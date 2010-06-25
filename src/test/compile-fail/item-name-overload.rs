85a701c8f668fc03e6340682956e7ca7d9cf54bc
Roy Frostig <rfrostig@mozilla.com>
2010-06-25 01:26:44 -0700

    Add testcases for item name conflict at the same path level.

---
+++ src/test/compile-fail/item-name-overload.rs
@@ -0,0 +1,8 @@
+// -*- rust -*-
+
+mod foo {
+  fn bar[T](T f) -> int { ret 17; }
+  type bar[U, T] = tup(int, U, T);
+}
+
+fn main() {}
---
+++ src/test/run-pass/item-name-overload.rs
@@ -0,0 +1,11 @@
+// -*- rust -*-
+
+mod foo {
+  fn baz() {}
+}
+
+mod bar {
+  fn baz() {}
+}
+
+fn main() {}
