74775eff7e0405939ef01a79d07133f8c31142d5
Graydon Hoare <graydon@mozilla.com>
2010-07-02 12:32:28 -0700

    Box the args entering the vec-drop test.

--- src/test/run-pass/vec-drop.rs
+++ src/test/run-pass/vec-drop.rs
@@ -1,4 +1,4 @@
 fn main() {
   // This just tests whether the vec leaks its members.
-  let vec[@tup(int,int)] pvec = vec(tup(1,2),tup(3,4),tup(5,6));
+  let vec[@tup(int,int)] pvec = vec(@tup(1,2),@tup(3,4),@tup(5,6));
 }
