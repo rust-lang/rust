2eba370aa2b65484b028316c9715f216012d2810
Graydon Hoare <graydon@mozilla.com>
2010-07-13 14:27:23 -0700

    Fix the next typo in mlist-cycle.rs; still doesn't work.

--- src/test/run-pass/mlist-cycle.rs
+++ src/test/run-pass/mlist-cycle.rs
@@ -8,7 +8,7 @@ type list = tag(link(@cell), nil());
 fn main() {
   let @cell first = tup(@nil());
   let @cell second = tup(@link(first));
-  first._0 = link(second);
+  first._0 = @link(second);
   std.sys.rustrt.gc();
   let @cell third = tup(@nil());
 }
