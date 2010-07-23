fe82978980c42df1288a804e53b32ebf66fd201c
Graydon Hoare <graydon@mozilla.com>
2010-07-23 12:32:12 -0700

    Modify testcase to match new syntax and un-XFAIL mutable-vec-drop.rs.

--- src/test/run-pass/mutable-vec-drop.rs
+++ src/test/run-pass/mutable-vec-drop.rs
@@ -1,4 +1,7 @@
 fn main() {
   // This just tests whether the vec leaks its members.
-  let vec[mutable @tup(int,int)] pvec = vec(tup(1,2),tup(3,4),tup(5,6));
+
+  let vec[mutable @tup(int,int)] pvec =
+    // FIXME: vec constructor syntax is slated to change.
+    vec[mutable](@tup(1,2), @tup(3,4), @tup(5,6));
 }
