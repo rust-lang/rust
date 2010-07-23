b5e46ac2a0f03c9657c26228b736fc9fc32d8b00
Graydon Hoare <graydon@mozilla.com>
2010-07-23 12:22:31 -0700

    Two more testcases for overwriting already-live box-like fields in structures, without leaking.

---
+++ src/test/run-pass/box-in-tup.rs
@@ -0,0 +1,4 @@
+fn main() {
+  let tup(mutable @int) i = tup(mutable @10);
+  i._0 = @11;
+}
---
+++ src/test/run-pass/vec-in-tup.rs
@@ -0,0 +1,4 @@
+fn main() {
+  let tup(mutable vec[int]) i = tup(mutable vec(1,2,3));
+  i._0 = vec(4,5,6);
+}
