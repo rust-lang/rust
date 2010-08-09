8395d46163a056f7b48f50d979004786d6282fe0
Michael Bebenita <mbebenita@mozilla.com>
2010-08-09 07:01:20 -0700

    Changed seemingly incorrect stk_field_valgrind_id.

--- src/boot/be/abi.ml
+++ src/boot/be/abi.ml
@@ -41,7 +41,7 @@ let box_gc_header_size = 4;;
 
 let box_gc_malloc_return_adjustment = 3;;
 
-let stk_field_valgrind_id = 0 + 1;;
+let stk_field_valgrind_id = 0;;
 let stk_field_limit = stk_field_valgrind_id + 1;;
 let stk_field_data = stk_field_limit + 1;;
 
