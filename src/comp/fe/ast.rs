5297582ba2e639368fe2ac65036408441cf8a25c
Graydon Hoare <graydon@mozilla.com>
2010-08-12 10:27:50 -0700

    Add mostly-empty AST module to rustc.

---
+++ src/comp/fe/ast.rs
@@ -0,0 +1,12 @@
+type crate = rec( str filename );
+
+//
+// Local Variables:
+// mode: rust
+// fill-column: 78;
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// buffer-file-coding-system: utf-8-unix
+// compile-command: "make -k -C ../.. 2>&1 | sed -e 's/\\/x\\//x:\\//g'";
+// End:
+//
