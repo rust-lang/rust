bc0d1fbae538fbb673fd1d03e0efcf89874ed88d
Graydon Hoare <graydon@mozilla.com>
2010-08-12 10:29:23 -0700

    Add modelines and related emacs chatter to rustc.

--- src/comp/driver/rustc.rs
+++ src/comp/driver/rustc.rs
@@ -19,4 +19,17 @@ fn main(vec[str] args) -> () {
     i += 1;
   }
 
+
 }
+
+
+//
+// Local Variables:
+// mode: rust
+// fill-column: 78;
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// buffer-file-coding-system: utf-8-unix
+// compile-command: "make -k -C ../.. 2>&1 | sed -e 's/\\/x\\//x:\\//g'";
+// End:
+//
