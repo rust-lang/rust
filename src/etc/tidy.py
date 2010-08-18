724d7235e546fb79009800700fd74328f8171b8c
Patrick Walton <pcwalton@mimiga.net>
2010-08-18 15:24:44 -0700

    Revert "Don't complain about \r when core.autocrlf is on in Git"
    This reverts commit 828afaa2fa4cc9e3e53bda0ae3073abfcfa151ca.

--- src/etc/tidy.py
+++ src/etc/tidy.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python
 
-import sys, fileinput, subprocess
+import sys, fileinput
 
 err=0
 cols=78
-
-try:
-    result=subprocess.check_output([ "git", "config", "core.autocrlf" ])
-    autocrlf=result.strip() == b"true"
-except CalledProcessError:
-    autocrlf=False
 
 def report_err(s):
     global err
@@ -20,11 +14,10 @@ for line in fileinput.input(openhook=fileinput.hook_encoded("utf-8")):
     if line.find('\t') != -1 and fileinput.filename().find("Makefile") == -1:
         report_err("tab character")
 
-    if not autocrlf and line.find('\r') != -1:
+    if line.find('\r') != -1:
         report_err("CR character")
 
-    line_len = len(line)-2 if autocrlf else len(line)-1
-    if line_len > cols:
+    if len(line)-1 > cols:
         report_err("line longer than %d chars" % cols)
 
 
