49f668dff306ae76ad83f2f82c032940f723b26c
Graydon Hoare <graydon@mozilla.com>
2010-08-18 11:34:47 -0700

    Add mod common to rustc.

---
+++ src/comp/util/common.rs
@@ -0,0 +1,15 @@
+
+type ty_mach = tag( ty_i8(), ty_i16(), ty_i32(), ty_i64(),
+                    ty_u8(), ty_u16(), ty_u32(), ty_u64(),
+                    ty_f32(), ty_f16() );
+
+//
+// Local Variables:
+// mode: rust
+// fill-column: 78;
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// buffer-file-coding-system: utf-8-unix
+// compile-command: "make -k -C ../.. 2>&1 | sed -e 's/\\/x\\//x:\\//g'";
+// End:
+//
