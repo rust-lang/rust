14262c73119349fd5452db0811a577febbc77670
Graydon Hoare <graydon@mozilla.com>
2010-08-24 09:09:04 -0700

    Rename lib tests, enable lib-int.rs using _str.eq for now.

---
+++ src/test/run-pass/lib-int.rs
@@ -0,0 +1,15 @@
+use std;
+
+import std._int;
+import std._str.eq;
+
+fn test_to_str() {
+  check (eq(_int.to_str(0, 10u), "0"));
+  check (eq(_int.to_str(1, 10u), "1"));
+  check (eq(_int.to_str(-1, 10u), "-1"));
+  check (eq(_int.to_str(255, 16u), "ff"));
+}
+
+fn main() {
+  test_to_str();
+}
