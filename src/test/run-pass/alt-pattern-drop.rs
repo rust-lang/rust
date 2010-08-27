5e77e784f006e36c06252d9beccbd7893eddac73
Roy Frostig <rfrostig@mozilla.com>
2010-08-27 13:27:28 -0700

    Have alt-pattern drop the slots it initializes when binding slot patterns.  Add a testcase for this as well.

---
+++ src/test/run-pass/alt-pattern-drop.rs
@@ -0,0 +1,19 @@
+// -*- rust -*-
+
+use std;
+import std._str;
+
+type t = tag(make_t(str), clam());
+
+fn main() {
+  let str s = "hi";     // ref up
+  let t x = make_t(s);  // ref up
+
+  alt (x) {
+    case (make_t(y)) { log y; }  // ref up and ref down
+    case (_) { log "?"; }
+  }
+
+  log _str.refcount(s);
+  check (_str.refcount(s) == 2u);
+}
