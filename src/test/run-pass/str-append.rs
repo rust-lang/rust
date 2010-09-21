dda16f807c2b1ce7d2ee6e510bee75e6a8dea30c
Roy Frostig <rfrostig@mozilla.com>
2010-09-20 17:42:14 -0700

    Add issue #163 testcase to str-append testcase.

--- src/test/run-pass/str-append.rs
+++ src/test/run-pass/str-append.rs
@@ -1,8 +1,30 @@
 // -*- rust -*-
 
-fn main() {
+use std;
+import std._str;
+
+fn test1() {
   let str s = "hello";
   s += "world";
   log s;
   check(s.(9) == ('d' as u8));
+}
+
+fn test2() {
+  // This tests for issue #163
+
+  let str ff = "abc";
+  let str a = ff + "ABC" + ff;
+  let str b = "ABC" + ff + "ABC";
+
+  log a;
+  log b;
+
+  check (_str.eq(a, "abcABCabc"));
+  check (_str.eq(b, "ABCabcABC"));
+}
+
+fn main() {
+  test1();
+  test2();
 }
