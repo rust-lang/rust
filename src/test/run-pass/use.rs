54024ae780d613b40159283a687301d8992308c1
Rafael Ávila de Espíndola <espindola@Rafael-Avila-de-Espindolas-MacBook-Pro.local>
2010-12-24 23:25:02 -0500

    Parse 'import' directives in rustc.

--- src/test/run-pass/use.rs
+++ src/test/run-pass/use.rs
@@ -3,11 +3,18 @@ use libc();
 use zed(name = "std");
 use bar(name = "std", ver = "0.0.1");
 
+import std._str;
+import x = std._str;
+
+
 mod baz {
   use std;
   use libc();
   use zed(name = "std");
   use bar(name = "std", ver = "0.0.1");
+
+  import std._str;
+  import x = std._str;
 }
 
 fn main() {
