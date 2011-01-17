58490d7fa0a39188915f80e04ef3cf7d6eaf3c90
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-01-17 16:17:09 -0500

    Fix the last known (to me) bug in import resolution. We were not properly dropping the inner part of the environment when an intermediate item resolved in an outer scope.

---
+++ src/test/run-pass/import7.rs
@@ -0,0 +1,19 @@
+import bar.baz;
+import foo.zed;
+mod foo {
+  mod zed {
+    fn baz() {
+      log "baz";
+    }
+  }
+}
+mod bar {
+  import zed.baz;
+  mod foo {
+    mod zed {
+    }
+  }
+}
+fn main(vec[str] args) {
+  baz();
+}
