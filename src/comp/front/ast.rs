41b7d6d74da54b80419ab093303dd356fae957ec
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-01-18 15:23:11 -0500

    One last refactoring of the import handling:
    * Create an import resolving stage. Currently this involves a copy of the ast,
      we can probably revisit this once we revisit doing full copies of the ast in
      general.
    
    * Don't repeat work. Once we resolve a import, put it on a hash table and use
      it next time we are asked for it. This solves a O(n^2) behaviour in
      degenerated cases.
    
    * Once import resolution is done, the target of an import is stored on the
      import itself.

--- src/comp/front/ast.rs
+++ src/comp/front/ast.rs
@@ -229,7 +229,7 @@ type variant = rec(str name, vec[variant_arg] args, def_id id, ann ann);
 type view_item = spanned[view_item_];
 tag view_item_ {
     view_item_use(ident, vec[@meta_item], def_id);
-    view_item_import(vec[ident], def_id);
+    view_item_import(vec[ident], def_id, option.t[def]);
 }
 
 type item = spanned[item_];
@@ -247,7 +247,7 @@ fn index_view_item(mod_index index, @view_item it) {
         case(ast.view_item_use(?id, _, _)) {
             index.insert(id, ast.mie_view_item(it));
         }
-        case(ast.view_item_import(?ids,_)) {
+        case(ast.view_item_import(?ids,_,_)) {
             auto len = _vec.len[ast.ident](ids);
             auto last_id = ids.(len - 1u);
             index.insert(last_id, ast.mie_view_item(it));
