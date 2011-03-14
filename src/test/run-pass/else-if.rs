87323573772619a99b07b4610639c6ec9c69c670
Brian Anderson <andersrb@gmail.com>
2011-03-14 19:57:12 -0400

    Add some nesting to the else-if test case

--- src/test/run-pass/else-if.rs
+++ src/test/run-pass/else-if.rs
@@ -16,4 +16,27 @@ fn main() {
     check(true);
   }
 
+  if (1 == 2) {
+    check(false);
+  } else if (2 == 2) {
+    if (1 == 1) {
+      check(true);
+    } else {
+      if (2 == 1) {
+        check(false);
+      } else {
+        check(false);
+      }
+    }
+  }
+
+  if (1 == 2) {
+    check(false);
+  } else {
+    if (1 == 2) {
+      check(false);
+    } else {
+      check(true);
+    }
+  }
 }
