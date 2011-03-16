6a6a30b7924d2a12a73cf3243a736c0efe26c6b4
Graydon Hoare <graydon@mozilla.com>
2011-03-16 16:45:41 -0700

    Remove uses of 'break' in std lib; rustc doesn't support it yet, this is easier for now.

--- src/lib/posix_fs.rs
+++ src/lib/posix_fs.rs
@@ -9,7 +9,10 @@ impure fn list_dir(str path) -> vec[str] {
   let vec[str] result = vec();
   while (true) {
     auto ent = os.libc.readdir(dir);
-    if (ent as int == 0) {break;}
+    if (ent as int == 0) {
+        os.libc.closedir(dir);
+        ret result;
+    }
     _vec.push[str](result, rustrt.rust_dirent_filename(ent));
   }
   os.libc.closedir(dir);
