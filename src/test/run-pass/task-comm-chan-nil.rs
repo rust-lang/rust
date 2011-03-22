48830768b94224b1cadb36c86bbe1ed78883abed
Brian Anderson <andersrb@gmail.com>
2011-03-21 21:30:32 -0400

    Add a regression test for channels of nil, which happens to work in rustc, but not in rustboot

---
+++ src/test/run-pass/task-comm-chan-nil.rs
@@ -0,0 +1,17 @@
+// -*- rust -*-
+
+// rustboot can't transmit nils across channels because they don't have
+// any size, but rustc currently can because they do have size. Whether
+// or not this is desirable I don't know, but here's a regression test.
+
+impure fn main() {
+  let port[()] po = port();
+  let chan[()] ch = chan(po);
+
+  ch <| ();
+
+  let () n;
+  n <- po;
+
+  check (n == ());
+}
