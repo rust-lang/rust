2335954da3237810013e277fcb45c4dc46182c1f
Brian Anderson <andersrb@gmail.com>
2011-03-27 18:51:25 -0400

    Handle structural types as the result of if expressions

---
+++ src/test/run-pass/expr-if-struct.rs
@@ -0,0 +1,24 @@
+// xfail-boot
+// -*- rust -*-
+
+// Tests for if as expressions returning structural types
+
+fn test_rec() {
+  auto res = if (true) { rec(i = 100) } else { rec(i = 101) };
+  check (res == rec(i = 100));
+}
+
+fn test_tag() {
+  tag mood {
+    happy;
+    sad;
+  }
+
+  auto res = if (true) { happy } else { sad };
+  check (res == happy);
+}
+
+fn main() {
+  test_rec();
+  test_tag();
+}
