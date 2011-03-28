efb738153c7248601a9a1b294b0ad8fd9c27023c
Brian Anderson <andersrb@gmail.com>
2011-03-28 00:46:16 -0400

    Add an XFAILed test for treating alts as expressions

---
+++ src/test/run-pass/expr-alt.rs
@@ -0,0 +1,31 @@
+// xfail-boot
+// xfail-stage0
+// -*- rust -*-
+
+// Tests for using alt as an expression
+
+fn test() {
+  let bool res = alt (true) {
+    case (true) {
+      true
+    }
+    case (false) {
+      false
+    }
+  };
+  check (res);
+
+  res = alt(false) {
+    case (true) {
+      false
+    }
+    case (false) {
+      true
+    }
+  };
+  check (res);
+}
+
+fn main() {
+  test();
+}
