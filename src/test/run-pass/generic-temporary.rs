0910a7323ca861e833d5a92a9bcf0f844699ca7c
Graydon Hoare <graydon@mozilla.com>
2011-04-13 13:21:06 -0700

    Add generic-temporary.rs minimal test showing cause of lib-deque.rs failure on stage0.

---
+++ src/test/run-pass/generic-temporary.rs
@@ -0,0 +1,21 @@
+
+// xfail-stage0
+
+fn mk() -> int {
+  ret 1;
+}
+
+fn chk(&int a) {
+  log a;
+  check (a == 1);
+}
+
+fn apply[T](fn() -> T produce, fn(&T) consume) {
+  consume(produce());
+}
+
+fn main() {
+  let (fn()->int) produce = mk;
+  let (fn(&int)) consume = chk;
+  apply[int](produce, consume);
+}
