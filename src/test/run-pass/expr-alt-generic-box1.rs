330fdd2b045343bd07620876cf3db553d46ded6c
Brian Anderson <andersrb@gmail.com>
2011-04-13 21:46:20 -0400

    Fix a typo in test expr-alt-generic-box1.rs

--- src/test/run-pass/expr-alt-generic-box1.rs
+++ src/test/run-pass/expr-alt-generic-box1.rs
@@ -14,7 +14,7 @@ fn test_generic[T](@T expected, &compare[T] eq) {
 
 fn test_box() {
   fn compare_box(@bool b1, @bool b2) -> bool {
-    ret *b1 == b2;
+    ret *b1 == *b2;
   }
   auto eq = bind compare_box(_, _);
   test_generic[bool](@true, eq);
