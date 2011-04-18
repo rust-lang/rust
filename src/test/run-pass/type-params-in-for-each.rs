8a6590a75f4a9ac95512a47163cb5396158e9018
Graydon Hoare <graydon@mozilla.com>
2011-04-18 12:20:40 -0700

    Remove use of 'use' from type-params-in-for-each.rs, put out darwin fire.

--- src/test/run-pass/type-params-in-for-each.rs
+++ src/test/run-pass/type-params-in-for-each.rs
@@ -1,8 +1,14 @@
-use std;
-import std._uint;
+
+iter range(uint lo, uint hi) -> uint {
+    auto lo_ = lo;
+    while (lo_ < hi) {
+        put lo_;
+        lo_ += 1u;
+    }
+}
 
 fn create_index[T](vec[tup(T, uint)] index, fn(&T) -> uint hash_fn) {
-    for each (uint i in _uint.range(0u, 256u)) {
+    for each (uint i in range(0u, 256u)) {
         let vec[T] bucket = vec();
     }
 }
