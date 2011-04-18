9ab42038bb13d3c8b01faa9c88e660e4e590fb00
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-04-18 14:49:08 -0400

    Change the makefiles to use -c.

--- src/rustllvm/RustWrapper.cpp
+++ src/rustllvm/RustWrapper.cpp
@@ -53,6 +53,7 @@ extern "C" void LLVMRustWriteOutputFile(LLVMPassManagerRef PMR, LLVMModuleRef M,
                                         LLVMCodeGenFileType FileType) {
   InitializeAllTargets();
   InitializeAllAsmPrinters();
+  InitializeAllAsmParsers();
   TargetMachine::setRelocationModel(Reloc::PIC_);
   std::string Err;
   const Target *TheTarget = TargetRegistry::lookupTarget(triple, Err);
