3b047bad6e1e33e460ead379534bb95829248a6a
Graydon Hoare <graydon@mozilla.com>
2011-04-22 02:07:31 +0000

    New minimal test for current stage2 blocker.

---
+++ src/test/run-pass/foreach-box-drop.rs
@@ -0,0 +1,17 @@
+// xfail-stage0
+
+obj ob[K](K k) {
+  iter foo() -> @tup(K) {
+    put @tup(k);
+  }
+}
+
+fn x(&ob[str] o) {
+  for each (@tup(str) i in o.foo()) {
+  }
+}
+
+fn main() {
+  auto o = ob[str]("hi" + "there");
+  x(o);
+}
