30df202cdaf328302c849d136d339534f284963b
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-04-21 17:39:04 -0700

    Support all expression forms in typestate
    Added support for self_method, cont, chan, port, recv, send, be,
    do_while, spawn, and ext; handled break and cont correctly.
    (However, there are no non-xfailed test cases for ext or spawn in
    stage0 currently.)
    
    Although the standard library compiles and all test cases pass with
    typestate enabled, I left typestate checking disabled as rustc
    terminates abnormally when building the standard library if so,
    even though it does generate code correctly.

---
+++ src/test/compile-fail/break-uninit2.rs
@@ -0,0 +1,22 @@
+// xfail-boot
+// xfail-stage0
+// error-pattern:Unsatisfied precondition
+
+fn foo() -> int {
+  let int x;
+  let int i;
+
+  do {
+    i = 0;
+    break;
+    x = 0;
+  } while (1 != 2);
+
+  log(x);
+
+  ret 17;
+}
+
+fn main() {
+  log(foo());
+}
