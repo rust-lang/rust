f0e1bb75d138522b3c64dfd2c16645cc45041d69
Graydon Hoare <graydon@mozilla.com>
2011-04-28 14:50:03 -0700

    Change vec-ref-count to handle 1 or 2, so long as non-leaky and non-creeping. un-XFAIL.

--- src/test/run-pass/vec-ref-count.rs
+++ src/test/run-pass/vec-ref-count.rs
@@ -1,11 +1,12 @@
-// xfail-stage0
-
 use std;
 import std._vec;
 
 fn main() {
     auto v = vec(1, 2, 3);
-    check (_vec.refcount[int](v) == 1u);
-    check (_vec.refcount[int](v) == 1u);
+    log_err _vec.refcount[int](v);
+    log_err _vec.refcount[int](v);
+    log_err _vec.refcount[int](v);
+    check (_vec.refcount[int](v) == 1u || _vec.refcount[int](v) == 2u);
+    check (_vec.refcount[int](v) == 1u || _vec.refcount[int](v) == 2u);
 }
 
