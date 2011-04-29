96d4577376f8bff0b3773d3cbc4fb1aa3abebcc8
Patrick Walton <pcwalton@mimiga.net>
2011-04-28 18:23:39 -0700

    test: Add a test case for vec growth

---
+++ src/test/run-pass/vec-growth.rs
@@ -0,0 +1,15 @@
+// xfail-stage0
+
+fn main() {
+    auto v = vec(1);
+    v += vec(2);
+    v += vec(3);
+    v += vec(4);
+    v += vec(5);
+    check (v.(0) == 1);
+    check (v.(1) == 2);
+    check (v.(2) == 3);
+    check (v.(3) == 4);
+    check (v.(4) == 5);
+}
+
