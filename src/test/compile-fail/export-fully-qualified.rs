f25e6783657cde34e25aa8ba105cd43fe9bda787
Brian Anderson <andersrb@gmail.com>
2011-05-01 16:29:52 -0400

    Add a regression test for use of unexported fully-qualified paths
    An unexported foo.baz can't be resolved from inside foo when called as foo.baz
    instead of just baz. This behavior may want to change eventually.

---
+++ src/test/compile-fail/export-fully-qualified.rs
@@ -0,0 +1,21 @@
+// xfail-boot
+// error-pattern: unresolved name
+
+// In this test baz isn't resolved when called as foo.baz even though
+// it's called from inside foo. This is somewhat surprising and may
+// want to change eventually.
+
+mod foo {
+
+  export bar;
+
+  fn bar() {
+    foo.baz();
+  }
+
+  fn baz() {
+  }
+}
+
+fn main() {
+}
