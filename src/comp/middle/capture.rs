9432626b686782f87e54960a4c019fd7c83ad7f7
Marijn Haverbeke <marijnh@gmail.com>
2011-05-05 15:03:37 +0200

    Eradicate fold from capture.rs
    The pass now uses walk.

--- src/comp/middle/capture.rs
+++ src/comp/middle/capture.rs
@@ -5,107 +5,102 @@ import std.option;
 import std.option.some;
 import std.option.none;
 import std._int;
+import std._vec;
 import util.common;
 
 type fn_id_of_local = std.map.hashmap[ast.def_id, ast.def_id];
-type env = rec(option.t[ast.def_id] current_context, // fn or obj
+type env = rec(mutable vec[ast.def_id] current_context, // fn or obj
                fn_id_of_local idmap,
                session.session sess);
 
-fn update_env_for_item(&env e, @ast.item i) -> env {
+fn current_context(&env e) -> ast.def_id {
+    ret e.current_context.(_vec.len(e.current_context) - 1u);
+}
+
+fn enter_item(@env e, @ast.item i) {
     alt (i.node) {
         case (ast.item_fn(?name, _, _, ?id, _)) {
-            ret rec(current_context = some(id) with e);
+            _vec.push(e.current_context, id);
         }
         case (ast.item_obj(_, _, _, ?ids, _)) {
-            ret rec(current_context = some(ids.ty) with e);
+            _vec.push(e.current_context, ids.ty);
         }
-        case (_) {
-            ret e;
-        }
+        case (_) {}
     }
 }
 
-fn update_env_for_expr(&env e, @ast.expr x) -> env {
+fn leave_item(@env e, @ast.item i) {
+    alt (i.node) {
+        case (ast.item_fn(?name, _, _, ?id, _)) {
+            _vec.pop(e.current_context);
+        }
+        case (ast.item_obj(_, _, _, ?ids, _)) {
+            _vec.pop(e.current_context);
+        }
+        case (_) {}
+    }
+}
+
+fn walk_expr(@env e, @ast.expr x) {
     alt (x.node) {
         case (ast.expr_for(?d, _, _, _)) {
             alt (d.node) {
                 case (ast.decl_local(?local)) {
-                    auto curr_context =
-                        option.get[ast.def_id](e.current_context);
-                    e.idmap.insert(local.id, curr_context);
+                    e.idmap.insert(local.id, current_context(*e));
                 }
-                case (_) {
-                }
+                case (_) { }
             }
         }
         case (ast.expr_for_each(?d, _, _, _)) {
             alt (d.node) {
                 case (ast.decl_local(?local)) {
-                    auto curr_context =
-                        option.get[ast.def_id](e.current_context);
-                    e.idmap.insert(local.id, curr_context);
+                    e.idmap.insert(local.id, current_context(*e));
                 }
-                case (_) {
-                }
+                case (_) { }
+            }
+        }
+        case (ast.expr_path(_, ?def, _)) {
+            auto local_id;
+            alt (option.get(def)) {
+                case (ast.def_local(?id)) { local_id = id; }
+                case (_) { ret; }
+            }
+
+            auto df = ast.def_id_of_def(option.get(def));
+            auto def_context = option.get(e.idmap.find(df));
+
+            if (current_context(*e) != def_context) {
+                e.sess.span_err(x.span,
+                                "attempted dynamic environment-capture");
             }
         }
         case (_) { }
     }
-    ret e;
 }
 
-fn update_env_for_block(&env e, &ast.block b) -> env {
-    auto curr_context = option.get[ast.def_id](e.current_context);
-
+fn walk_block(@env e, &ast.block b) {
     for each (@tup(ast.ident, ast.block_index_entry) it in
               b.node.index.items()) {
         alt (it._1) {
             case (ast.bie_local(?local)) {
-                e.idmap.insert(local.id, curr_context);
+                e.idmap.insert(local.id, current_context(*e));
             }
-            case (_) {
-            }
+            case (_) { }
         }
     }
-
-    ret e;
-}
-
-fn fold_expr_path(&env e, &ast.span sp, &ast.path p, &option.t[ast.def] d,
-                  ast.ann a) -> @ast.expr {
-    auto local_id;
-    alt (option.get[ast.def](d)) {
-        case (ast.def_local(?id)) {
-            local_id = id;
-        }
-        case (_) {
-            ret @fold.respan[ast.expr_](sp, ast.expr_path(p, d, a));
-        }
-    }
-
-    auto curr_context = option.get[ast.def_id](e.current_context);
-    auto x = ast.def_id_of_def(option.get[ast.def](d));
-    auto def_context = option.get[ast.def_id](e.idmap.find(x));
-
-    if (curr_context != def_context) {
-        e.sess.span_err(sp, "attempted dynamic environment-capture");
-    }
-
-    ret @fold.respan[ast.expr_](sp, ast.expr_path(p, d, a));
 }
 
 fn check_for_captures(session.session sess, @ast.crate crate) {
-    let fold.ast_fold[env] fld = fold.new_identity_fold[env]();
-    fld = @rec( update_env_for_item = bind update_env_for_item(_,_),
-                update_env_for_block = bind update_env_for_block(_,_),
-                update_env_for_expr = bind update_env_for_expr(_,_),
-                fold_expr_path = bind fold_expr_path(_,_,_,_,_)
-                with *fld);
-    auto idmap = common.new_def_hash[ast.def_id]();
-    auto e = rec(current_context = none[ast.def_id], idmap = idmap,
-                 sess = sess);
-    fold.fold_crate[env](e, fld, crate);
+    let vec[ast.def_id] curctx = vec();
+    auto env = @rec(mutable current_context = curctx,
+                    idmap = common.new_def_hash[ast.def_id](),
+                    sess = sess);
+    auto visitor = rec(visit_item_pre = bind enter_item(env, _),
+                       visit_item_post = bind leave_item(env, _),
+                       visit_block_pre = bind walk_block(env, _),
+                       visit_expr_pre = bind walk_expr(env, _)
+                       with walk.default_visitor());
+    walk.walk_crate(visitor, *crate);
 }
 
 // Local Variables:
