10a2093d37bcf13f375a7c3637f4c227b7a21b0c
Graydon Hoare <graydon@mozilla.com>
2011-05-06 16:54:50 -0700

    Failure to re-stage changes strikes again.

--- src/lib/GenericOS.rs
+++ src/lib/GenericOS.rs
@@ -1,6 +1,6 @@
 fn getenv(str n) -> Option.t[str] {
   auto s = OS.libc.getenv(Str.buf(n));
-  if (s == 0 as Str.sbuf) {
+  if ((s as int) == 0) {
     ret Option.none[str];
   } else {
     ret Option.some[str](Str.str_from_cstr(s));
