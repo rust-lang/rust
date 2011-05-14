fb76c25729ae6cf2c755d7515830a27f9b0971ec
Brian Anderson <andersrb@gmail.com>
2011-05-14 03:21:08 -0400

    WIP

---
+++ src/test/run-pass/compare.rs
@@ -0,0 +1,21 @@
+use std;
+import std::option;
+import std::option::none;
+
+tag sty {
+    ty_nil;
+}
+
+type raw_t = rec(sty struct,
+                 option::t[str] cname,
+                 uint hash);
+
+fn mk_raw_ty(sty st, &option::t[str] cname) -> raw_t {
+  ret rec(struct=st,
+	  cname=cname,
+	  hash=0u);
+}
+
+fn main() {
+  mk_raw_ty(ty_nil, none[str]);
+}
