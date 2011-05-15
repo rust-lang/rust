b42bb2cff249862d22c89af09a912fbb0a8d8862
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-05-14 19:03:08 -0700

    Two tests that the typechecker correctly unifies type arguments in patterns with their expected tag types

---
+++ src/test/compile-fail/pattern-tyvar-2.rs
@@ -0,0 +1,27 @@
+// -*- rust -*-
+// xfail-stage0
+
+use std;
+import std::option;
+import std::option::some;
+
+// error-pattern: mismatched types
+
+tag bar {
+  t1((), option::t[vec[int]]);
+  t2;
+}
+
+fn foo(bar t) -> int {
+  alt (t) {
+    case (t1(_, some(?x))) {
+      ret (x * 3);
+    }
+    case (_) {
+      fail;
+    }
+  }
+}
+
+fn main() {
+}
