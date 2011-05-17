1fdc75ee7cf1d498a413287b5444cdb4242c04c6
Paul Stansifer <paul.stansifer@gmail.com>
2011-05-16 18:05:21 -0700

    Parser fix: 'fail' was eating the next token that came after it.

---
+++ src/test/compile-fail/fail-simple.rs
@@ -0,0 +1,6 @@
+// -*- rust -*-
+
+// error-pattern: after expression but found
+fn main() {
+  fail @ ;
+}
---
+++ src/test/run-pass/parse-fail.rs
@@ -0,0 +1,8 @@
+// -*- rust -*-
+
+fn dont_call_me() {
+  fail;
+  log 1;
+}
+
+fn main() {}
