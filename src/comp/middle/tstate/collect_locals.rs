b624b43e4774d1dd421979946a09a36162a0aff1
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-05-16 17:49:46 -0700

    Beginning of Operation Eliminate Folds
    Started by rewriting collect_locals to use walk instead of fold

--- src/comp/middle/tstate/collect_locals.rs
+++ src/comp/middle/tstate/collect_locals.rs
@@ -19,12 +19,12 @@ import front::ast::decl;
 import front::ast::decl_local;
 import front::ast::def_id;
 import front::ast::ident;
+
 import middle::fold::span;
 import middle::fold::respan;
-import middle::fold::new_identity_fold;
-import middle::fold::fold_block;
-import middle::fold::fold_fn;
-import middle::fold::fold_crate;
+import middle::walk::walk_crate;
+import middle::walk::walk_fn;
+import middle::walk::ast_visitor;
 
 import aux::fn_info;
 import aux::var_info;
@@ -36,19 +36,17 @@ fn var_is_local(def_id v, fn_info m) -> bool {
   ret (m.vars.contains_key(v));
 }
 
-fn collect_local(&@vec[tup(ident, def_id)] vars, &span sp, &@local loc)
-    -> @decl {
+fn collect_local(&@vec[tup(ident, def_id)] vars, &@local loc) -> () {
     log("collect_local: pushing " + loc.ident);
-    vec::push[tup(ident, def_id)](*vars, tup(loc.ident, loc.id));
-    ret @respan(sp, decl_local(loc));
+    _vec::push[tup(ident, def_id)](*vars, tup(loc.ident, loc.id));
 }
 
 fn find_locals(_fn f) -> @vec[tup(ident,def_id)] {
   auto res = @vec::alloc[tup(ident,def_id)](0u);
 
-  auto fld = new_identity_fold[@vec[tup(ident, def_id)]]();
-  fld = @rec(fold_decl_local = bind collect_local(_,_,_) with *fld);
-  auto ignore = fold_fn[@vec[tup(ident, def_id)]](res, fld, f);
+  auto visitor = walk::default_visitor();
+  visitor = rec(visit_decl_pre=bind collect_local(res,_) with visitor);
+  walk_fn(visitor, f);
 
   ret res;
 }
@@ -83,40 +81,47 @@ fn mk_fn_info(_fn f, def_id f_id, ident f_name) -> fn_info {
     ret res;
 }
 
-/* extends mk_fn_info to a function item, side-effecting the map fi from
-   function IDs to fn_info maps */
-fn mk_fn_info_item_fn(&crate_ctxt ccx, &span sp, &ident i, &_fn f,
-                 &vec[ty_param] ty_params, &def_id id, &ann a) -> @item {
-    auto f_inf = mk_fn_info(f, id, i);
-    ccx.fm.insert(id, f_inf);
-    //  log_err("inserting: " + i);
-    ret @respan(sp, item_fn(i, f, ty_params, id, a));
-}
-
-/* extends mk_fn_info to an obj item, side-effecting the map fi from
-   function IDs to fn_info maps */
-fn mk_fn_info_item_obj(&crate_ctxt ccx, &span sp, &ident i, &_obj o,
-                       &vec[ty_param] ty_params,
-                       &obj_def_ids odid, &ann a) -> @item {
-    auto all_methods = vec::clone[@method](o.methods);
-    plus_option[@method](all_methods, o.dtor);
-    auto f_inf;
-    for (@method m in all_methods) {
+/* extends mk_fn_info to an item, side-effecting the map fi from
+   function IDs to fn_info maps
+   only looks at function and object items. */
+fn mk_fn_info_item (&crate_ctxt ccx, &@item i) -> () {
+  alt (i.node) {
+    case (item_fn(?i,?f,?ty_params,?id,?a)) {
+      auto f_inf = mk_fn_info(f, id, i);
+      ccx.fm.insert(id, f_inf);
+    }
+    case (item_obj(?i,?o,?ty_params,?odid,?a)) {
+      auto all_methods = _vec::clone[@method](o.methods);
+      plus_option[@method](all_methods, o.dtor);
+      auto f_inf;
+      for (@method m in all_methods) {
         f_inf = mk_fn_info(m.node.meth, m.node.id, m.node.ident);
         ccx.fm.insert(m.node.id, f_inf);
+      }
     }
-    ret @respan(sp, item_obj(i, o, ty_params, odid, a));
+    case (_) { ret; }
+  }
 }
-
 
 /* initializes the global fn_info_map (mapping each function ID, including
    nested locally defined functions, onto a mapping from local variable name
    to bit number) */
 fn mk_f_to_fn_info(&crate_ctxt ccx, @crate c) -> () {
+  let ast_visitor vars_visitor = walk::default_visitor();
+  vars_visitor = rec(visit_item_post=bind mk_fn_info_item(ccx,_)
+		     with vars_visitor);
 
-  auto fld = new_identity_fold[crate_ctxt]();
-  fld = @rec(fold_item_fn  = bind mk_fn_info_item_fn(_,_,_,_,_,_,_),
-             fold_item_obj = bind mk_fn_info_item_obj(_,_,_,_,_,_,_)
-               with *fld);
-  fold_crate[crate_ctxt](ccx, fld, c);
+  walk_crate(vars_visitor, *c);
 }
+
+//
+// Local Variables:
+// mode: rust
+// fill-column: 78;
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// buffer-file-coding-system: utf-8-unix
+// compile-command: "make -k -C $RBUILD 2>&1 | sed -e 's/\\/x\\//x:\\//g'";
+// End:
+//
+
