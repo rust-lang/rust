550667a538a2b140be8e2be9aef327d66d31c3e0
Graydon Hoare <graydon@mozilla.com>
2011-05-16 19:05:08 -0700

    Fix snap rules to depend more correctly on rustc$(X) not rustc.

--- mk/snap.mk
+++ mk/snap.mk
@@ -1,10 +1,10 @@
 
-snap-stage1: stage1/rustc stage1/glue.o stage1/$(CFG_STDLIB)
+snap-stage1: stage1/rustc$(X) stage1/glue.o stage1/$(CFG_STDLIB)
 	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage1
 
-snap-stage2: stage2/rustc stage2/glue.o stage2/$(CFG_STDLIB)
+snap-stage2: stage2/rustc$(X) stage2/glue.o stage2/$(CFG_STDLIB)
 	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage2
 
-snap-stage3: stage3/rustc stage3/glue.o stage3/$(CFG_STDLIB)
+snap-stage3: stage3/rustc$(X) stage3/glue.o stage3/$(CFG_STDLIB)
 	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage3
 
