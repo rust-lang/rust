51e1ce292d0a64630df1764bb3f71b885aff7116
Eric Holk <eholk@mozilla.com>
2011-05-18 11:38:41 -0700

    Added a couple of test cases for sending messages. One works as expected, the other succeeds unexpectedly.

---
+++ src/test/run-fail/trivial-message2.rs
@@ -0,0 +1,14 @@
+/*
+  This program should hang on the r <- po line.
+ */
+
+fn main() {
+    let port[int] po = port();
+    let chan[int] ch = chan(po);
+
+    auto r <- po;
+
+    ch <| 42;
+
+    log_err r;
+}
