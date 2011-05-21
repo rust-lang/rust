264c3b5be40ac4e7537d614dc019ec5651f5addb
Brian Anderson <andersrb@gmail.com>
2011-05-20 20:56:14 -0400

    stdlib: Use if/alt expressions in std::generic_os

--- src/lib/generic_os.rs
+++ src/lib/generic_os.rs
@@ -1,9 +1,9 @@
 fn getenv(str n) -> option::t[str] {
   auto s = os::libc::getenv(str::buf(n));
-  if ((s as int) == 0) {
-    ret option::none[str];
+  ret if ((s as int) == 0) {
+    option::none[str]
   } else {
-    ret option::some[str](str::str_from_cstr(s));
-  }
+    option::some[str](str::str_from_cstr(s))
+  };
 }
 
