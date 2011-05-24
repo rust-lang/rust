c22100ab2dd428dd4bf96c93ae70312593589fbd
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-05-23 19:44:59 -0700

    Un-XFAIL vector-no-ann
    and update it to reflect the new friendly error message

--- src/test/compile-fail/vector-no-ann.rs
+++ src/test/compile-fail/vector-no-ann.rs
@@ -1,10 +1,6 @@
 // xfail-stage0
-// xfail-stage1
-// xfail-stage2
-// xfail-stage3
 
-// error-pattern:3:13:3:14
+// error-pattern:Ambiguous type
 fn main() -> () {
   auto foo = [];
 }
-// this checks that span_err gets used
