d569a71b0b4bac30a4b15b10d95d839e915cd0b3
Graydon Hoare <graydon@mozilla.com>
2011-06-01 16:55:45 -0700

    make 'reformat' do libstd as well.

--- mk/pp.mk
+++ mk/pp.mk
@@ -1,7 +1,10 @@
+
+PP_INPUTS := $(wildcard $(addprefix $(S)src/lib/,*.rs */*.rs)) \
+             $(wildcard $(addprefix $(S)src/comp/,*.rs */*.rs */*/*.rs))
+
 reformat: $(SREQ1)
 	@$(call E, reformat [stage1]: $@)
-	for i in $(wildcard $(addprefix $(S)src/comp/, \
-                          *.rs */*.rs */*/*.rs));  \
+	for i in $(PP_INPUTS);  \
     do $(call CFG_RUN_TARG,stage1, stage1/rustc$(X)) \
        --pretty $$i >$$i.tmp && mv $$i.tmp $$i; \
     done
