92e246a1ee268233805bc4887228e3d6649b13d1
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-06-03 15:13:10 -0400

    Parse pointers in metadata.

--- src/comp/front/creader.rs
+++ src/comp/front/creader.rs
@@ -105,6 +105,7 @@ fn parse_ty(@pstate st, str_def sd) -> ty::t {
         }
         case ('p') { ret ty::mk_param(st.tcx, parse_int(st) as uint); }
         case ('@') { ret ty::mk_box(st.tcx, parse_mt(st, sd)); }
+        case ('*') { ret ty::mk_ptr(st.tcx, parse_mt(st, sd)); }
         case ('V') { ret ty::mk_vec(st.tcx, parse_mt(st, sd)); }
         case ('a') { ret ty::mk_task(st.tcx); }
         case ('P') { ret ty::mk_port(st.tcx, parse_ty(st, sd)); }
