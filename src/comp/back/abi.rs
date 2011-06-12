ac206068d9288faa9ee35aca3d1f5bb0268f46d4
Patrick Walton <pcwalton@mimiga.net>
2011-06-11 19:42:48 -0700

    rustc: Bump the default interior vector size up to 64 bytes

--- src/comp/back/abi.rs
+++ src/comp/back/abi.rs
@@ -66,7 +66,7 @@ const int closure_elt_target = 1;
 const int closure_elt_bindings = 2;
 const int closure_elt_ty_params = 3;
 
-const uint ivec_default_size = 16u;
+const uint ivec_default_size = 64u;
 
 const uint ivec_elt_len = 0u;
 const uint ivec_elt_alen = 1u;
