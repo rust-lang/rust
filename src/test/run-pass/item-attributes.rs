697fdaac24c0f973da8569b1249d8c778e510082
Brian Anderson <banderson@mozilla.com>
2011-06-14 16:29:46 -0700

    test: Add test cases for outer attributes on all item types

--- src/test/run-pass/item-attributes.rs
+++ src/test/run-pass/item-attributes.rs
@@ -1,8 +1,21 @@
 // xfail-stage0
 
 #[attr = "val"]
+const int x = 10;
+
+#[attr = "val"]
 mod mod1 {
 }
+
+#[attr = "val"]
+native "rust" mod rustrt {
+}
+
+#[attr = "val"]
+type t = obj { };
+
+#[attr = "val"]
+obj o() { }
 
 fn main() {
 }
