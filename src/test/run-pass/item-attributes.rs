95bdedf12d5f7e0e5bd2fc015e845449c3d1741d
Brian Anderson <banderson@mozilla.com>
2011-06-15 10:28:11 -0700

    test: Add missing fn test cases to run-pass/item-attributes.rs
    Issue #487

--- src/test/run-pass/item-attributes.rs
+++ src/test/run-pass/item-attributes.rs
@@ -1,14 +1,16 @@
-
-
-
 // xfail-stage0
+
 mod test_single_attr_outer {
 
     #[attr = "val"]
     const int x = 10;
 
     #[attr = "val"]
-    mod mod1 { }
+    fn f() {}
+
+    #[attr = "val"]
+    mod mod1 {
+    }
 
     #[attr = "val"]
     native "rust" mod rustrt { }
@@ -29,7 +31,12 @@ mod test_multi_attr_outer {
 
     #[attr1 = "val"]
     #[attr2 = "val"]
-    mod mod1 { }
+    fn f() {}
+
+    #[attr1 = "val"]
+    #[attr2 = "val"]
+    mod mod1 {
+    }
 
     #[attr1 = "val"]
     #[attr2 = "val"]
