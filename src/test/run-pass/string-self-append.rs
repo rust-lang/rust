12d9d07a84db3726b69255141899d5df4148211c
Michael Sullivan <sully@msully.net>
2011-06-27 11:41:16 -0700

    Improve the tests for vector self appending.

--- src/test/run-pass/string-self-append.rs
+++ src/test/run-pass/string-self-append.rs
@@ -1,17 +1,17 @@
 // xfail-stage0
 
 use std;
-import std::vec;
+import std::str;
 
 fn main()
 {
     // Make sure we properly handle repeated self-appends.
-    let vec[int] a = [0];
+    let str a = "A";
     auto i = 20;
     auto expected_len = 1u;
     while (i > 0) {
-        log_err vec::len(a);
-        assert (vec::len(a) == expected_len);
+        log_err str::byte_len(a);
+        assert (str::byte_len(a) == expected_len);
         a += a;
         i -= 1;
         expected_len *= 2u;
