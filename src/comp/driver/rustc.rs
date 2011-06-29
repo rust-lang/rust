54f618128e7e833db039f96d196084ea21fe1bb7
Rafael Ávila de Espíndola <respindola@mozilla.com>
2011-06-29 13:25:52 -0400

    Revert ""Try" push without -lssp on windows to see what needs it."
    This reverts commit 47b3e087e945a2a8fcfd1179e333746d294c1259.

--- src/comp/driver/rustc.rs
+++ src/comp/driver/rustc.rs
@@ -410,7 +410,7 @@ fn main(vec[str] args) {
         alt (sess.get_targ_cfg().os) {
             case (session::os_win32) {
                 shared_cmd = "-shared";
-                gcc_args = common_args;
+                gcc_args = common_args + ["-lssp"];
             }
             case (session::os_macos) {
                 shared_cmd = "-dynamiclib";
