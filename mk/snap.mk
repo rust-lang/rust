cd799a0a7a06cdef8c56fa4cbb66d7a8f64bc879
Graydon Hoare <graydon@mozilla.com>
2011-06-29 15:12:32 -0700

    Tidy up snap makefile.

--- mk/snap.mk
+++ mk/snap.mk
@@ -1,10 +1,10 @@
 
 snap-stage1: stage1/rustc$(X) stage1/glue.o stage1/lib/$(CFG_STDLIB)
-	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage1
+	$(S)src/etc/make-snapshot.py stage1
 
 snap-stage2: stage2/rustc$(X) stage2/glue.o stage2/lib/$(CFG_STDLIB)
-	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage2
+	$(S)src/etc/make-snapshot.py stage2
 
 snap-stage3: stage3/rustc$(X) stage3/glue.o stage3/lib/$(CFG_STDLIB)
-	$(CFG_SRC_DIR)src/etc/make-snapshot.py stage3
+	$(S)src/etc/make-snapshot.py stage3
 
