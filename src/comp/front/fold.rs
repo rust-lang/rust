1f0c25406c8842dcb4c3832b14388dca3ccf35c8
Brian Anderson <banderson@mozilla.com>
2011-06-30 00:22:38 -0700

    Fold the crate-level module in front::fold

--- src/comp/front/fold.rs
+++ src/comp/front/fold.rs
@@ -131,7 +131,8 @@ fn noop_fold_crate(&crate_ c, ast_fold fld) -> crate_ {
     auto fold_attribute = bind fold_attribute_(_,fold_meta_item);
 
     ret rec(directives=map(fld.fold_crate_directive, c.directives),
-            module=c.module, attrs=map(fold_attribute, c.attrs),
+            module=fld.fold_mod(c.module),
+            attrs=map(fold_attribute, c.attrs),
             config=map(fold_meta_item, c.config));
 }
 
