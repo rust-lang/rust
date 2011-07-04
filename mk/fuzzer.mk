ccf6112f1546ee381df11793bf013ebc95829b74
Brian Anderson <banderson@mozilla.com>
2011-07-04 15:30:28 -0700

    Link the fuzzer crate to librustc again

--- mk/fuzzer.mk
+++ mk/fuzzer.mk
@@ -3,6 +3,7 @@
 FUZZER_CRATE := $(S)src/fuzzer/fuzzer.rc
 FUZZER_INPUTS := $(wildcard $(addprefix $(S)src/fuzzer/, *.rs))
 
-stage1/fuzzer$(X): $(FUZZER_CRATE) $(FUZZER_INPUTS) $(SREQ1)
+stage1/fuzzer$(X): $(FUZZER_CRATE) $(FUZZER_INPUTS) $(SREQ1) \
+                   stage1/lib/$(CFG_LIBRUSTC)
 	@$(call E, compile_and_link: $@)
 	$(STAGE1) -o $@ $<
