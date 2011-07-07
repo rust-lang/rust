2c234fdc97414990006d26f76f598814f95fc147
Brian Anderson <banderson@mozilla.com>
2011-07-07 15:08:31 -0700

    Also copy rt and rustllvm to stage0 for now
    Once we've updated the snapshots to include them we can remove these rules

--- mk/stage0.mk
+++ mk/stage0.mk
@@ -1,4 +1,15 @@
-stage0/rustc$(X): $(S)src/snapshots.txt $(S)src/etc/get-snapshot.py $(MKFILES)
+# FIXME: Copying rt and rustllvm to stage0 is transitional until the snapshot
+# that begins including them
+stage0/lib/$(CFG_RUNTIME):	rt/$(CFG_RUNTIME)
+	@$(call E, cp: $@)
+	$(Q)cp $< $@
+
+stage0/lib/$(CFG_RUSTLLVM):	rustllvm/$(CFG_RUSTLLVM)
+	@$(call E, cp: $@)
+	$(Q)cp $< $@
+
+stage0/rustc$(X): $(S)src/snapshots.txt $(S)src/etc/get-snapshot.py $(MKFILES) \
+                  stage0/lib/$(CFG_RUNTIME) stage0/lib/$(CFG_RUSTLLVM)
 	@$(call E, fetch: $@)
 	$(Q)$(S)src/etc/get-snapshot.py
 	$(Q)touch $@
