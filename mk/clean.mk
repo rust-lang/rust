301f6aaa31594f996ae70ed84cf4e2ba7d8b3030
Brian Anderson <banderson@mozilla.com>
2011-07-07 17:22:39 -0700

    Add missing rules to 'make clean'

--- mk/clean.mk
+++ mk/clean.mk
@@ -14,12 +14,17 @@ clean:
 	$(Q)rm -f $(CRATE_DEPFILES:%.d=%.d.tmp)
 	$(Q)rm -f $(GENERATED)
 	$(Q)rm -f stage0/rustc$(X) stage0/lib/$(CFG_STDLIB) stage0/lib/glue*
+	$(Q)rm -f stage0/lib/$(CFG_RUNTIME) stage0/lib/$(CFG_RUSTLLVM)
 	$(Q)rm -f stage1/rustc$(X) stage1/lib/$(CFG_STDLIB) stage1/lib/glue*
+	$(Q)rm -f stage1/lib/$(CFG_RUNTIME) stage1/lib/$(CFG_RUSTLLVM)
 	$(Q)rm -f stage2/rustc$(X) stage2/lib/$(CFG_STDLIB) stage2/lib/glue*
+	$(Q)rm -f stage2/lib/$(CFG_RUNTIME) stage2/lib/$(CFG_RUSTLLVM)
 	$(Q)rm -f stage3/rustc$(X) stage3/lib/$(CFG_STDLIB) stage3/lib/glue*
+	$(Q)rm -f stage3/lib/$(CFG_RUNTIME) stage3/lib/$(CFG_RUSTLLVM)
 	$(Q)rm -f rustllvm/$(CFG_RUSTLLVM) rustllvm/rustllvmbits.a
 	$(Q)rm -f rt/$(CFG_RUNTIME)
 	$(Q)rm -f rt/main.o
+	$(Q)rm -f rt/main.ll
 	$(Q)rm -Rf $(PKG_NAME)-*.tar.gz dist
 	$(Q)rm -f $(foreach ext,o a d bc s exe,$(wildcard stage*/*.$(ext)))
 	$(Q)rm -Rf $(foreach ext,out out.tmp                      \
