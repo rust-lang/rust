ad1c0e6308c8e60536c801f8fc8889e8fba4b348
Brian Anderson <banderson@mozilla.com>
2011-07-18 14:59:44 -0700

    Reformulate x86stdcall test so it works with check-fast
    Hopefully this puts out the burning win32 tinderbox

--- src/test/run-pass/x86stdcall.rs
+++ src/test/run-pass/x86stdcall.rs
@@ -1,18 +1,17 @@
 // xfail-stage0
 
 #[cfg(target_os = "win32")]
-mod m {
-    native "x86stdcall" mod kernel32 {
-        fn SetLastError(uint err);
-        fn GetLastError() -> uint;
-    }
+native "x86stdcall" mod kernel32 {
+  fn SetLastError(uint err);
+  fn GetLastError() -> uint;
+}
 
-    fn main() {
-        auto expected = 10u;
-        kernel32::SetLastError(expected);
-        auto actual = kernel32::GetLastError();
-        assert expected == actual;
-    }
+#[cfg(target_os = "win32")]
+fn main() {
+  auto expected = 10u;
+  kernel32::SetLastError(expected);
+  auto actual = kernel32::GetLastError();
+  assert expected == actual;
 }
 
 #[cfg(target_os = "macos")]
