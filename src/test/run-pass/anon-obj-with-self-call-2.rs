a33bc56e43adbdfce78c63337c175053038f3362
Lindsey Kuper <lkuper@mozilla.com>
2011-07-19 12:48:06 -0700

    Reduced test case for issue #540.

---
+++ src/test/run-pass/anon-obj-with-self-call-2.rs
@@ -0,0 +1,22 @@
+//xfail-stage0
+//xfail-stage1
+//xfail-stage2
+
+// Reduced test case for issue #540.
+fn main() {
+    obj a() {
+        fn foo() -> int {
+            ret 2;
+        }
+    }
+
+    auto my_a = a();
+    auto my_b = obj {
+        fn baz() -> int {
+            ret self.foo();
+        }
+        with my_a
+    };
+
+    assert (my_b.baz() == 2);
+}
