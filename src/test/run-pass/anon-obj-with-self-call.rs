fefeebce24eb429309d1aac8e61d97b6d814867d
Lindsey Kuper <lkuper@mozilla.com>
2011-07-22 13:10:44 -0700

    Getting rid of obsolete comments.

--- src/test/run-pass/anon-obj-with-self-call.rs
+++ src/test/run-pass/anon-obj-with-self-call.rs
@@ -13,9 +13,6 @@ fn main() {
 
     auto my_a = a();
 
-    // Extending an object with a new method that contains a simple
-    // self-call.  Adding support for this is issue #540.
-
     auto my_b = obj {
         fn baz() -> int {
             ret self.foo();
