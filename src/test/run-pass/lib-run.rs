1348a383c39a083424280ab4f4d20b622e1769a6
Brian Anderson <banderson@mozilla.com>
2011-07-21 19:52:27 -0700

    Enable run-pass/lib-run on win32. Closes #714

--- src/test/run-pass/lib-run.rs
+++ src/test/run-pass/lib-run.rs
@@ -4,18 +4,11 @@ use std;
 import std::run;
 
 // Regression test for memory leaks
-// FIXME (714) Why does this fail on win32?
-
-#[cfg(target_os = "linux")]
-#[cfg(target_os = "macos")]
 fn test_leaks() {
   run::run_program("echo", []);
   run::start_program("echo", []);
   run::program_output("echo", []);
 }
-
-#[cfg(target_os = "win32")]
-fn test_leaks() {}
 
 fn main() {
   test_leaks();
