cb024253765c9537d1fa1f789caa4d3f9afe03e8
Michael Sullivan <sully@msully.net>
2011-07-21 18:07:10 -0700

    More missing things in make clean...

--- mk/clean.mk
+++ mk/clean.mk
@@ -16,19 +16,23 @@ clean:
 	$(Q)rm -f stage0/rustc$(X) stage0/lib/glue* stage0/lib/main.o
 	$(Q)rm -f stage0/lib/$(CFG_RUNTIME) stage0/lib/$(CFG_STDLIB)
 	$(Q)rm -f stage0/$(CFG_RUNTIME) stage0/$(CFG_STDLIB)
+	$(Q)rm -f stage0/lib/libstd.rlib
 	$(Q)rm -f stage0/$(CFG_RUSTLLVM) stage0/lib/intrinsics.bc
 	$(Q)rm -f stage1/rustc$(X) stage1/lib/glue* stage1/lib/main.o
 	$(Q)rm -f stage1/lib/$(CFG_RUNTIME) stage1/lib/$(CFG_STDLIB)
 	$(Q)rm -f stage1/$(CFG_RUNTIME) stage1/$(CFG_STDLIB)
 	$(Q)rm -f stage1/$(CFG_RUSTLLVM) stage1/lib/intrinsics.bc
+	$(Q)rm -f stage1/lib/libstd.rlib
 	$(Q)rm -f stage2/rustc$(X) stage2/lib/glue* stage2/lib/main.o
 	$(Q)rm -f stage2/lib/$(CFG_RUNTIME) stage2/lib/$(CFG_STDLIB)
 	$(Q)rm -f stage2/$(CFG_RUNTIME) stage2/$(CFG_STDLIB)
 	$(Q)rm -f stage2/$(CFG_RUSTLLVM) stage2/lib/intrinsics.bc
+	$(Q)rm -f stage2/lib/libstd.rlib
 	$(Q)rm -f stage3/rustc$(X) stage3/lib/glue* stage3/lib/main.o
 	$(Q)rm -f stage3/lib/$(CFG_RUNTIME) stage3/lib/$(CFG_STDLIB)
 	$(Q)rm -f stage3/$(CFG_RUNTIME) stage3/$(CFG_STDLIB)
 	$(Q)rm -f stage3/$(CFG_RUSTLLVM) stage3/lib/intrinsics.bc
+	$(Q)rm -f stage3/lib/libstd.rlib
 	$(Q)rm -f stage1/fuzzer stage1/lib/$(CFG_LIBRUSTC)
 	$(Q)rm -f rustllvm/$(CFG_RUSTLLVM) rustllvm/rustllvmbits.a
 	$(Q)rm -f rt/$(CFG_RUNTIME)
