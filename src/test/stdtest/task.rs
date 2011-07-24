b82bedb28ed0c3d474edc254fe0ebb5e577b7ca4
Brian Anderson <banderson@mozilla.com>
2011-07-23 18:41:48 -0700

    Reindent lib-task.rs

--- src/test/stdtest/task.rs
+++ src/test/stdtest/task.rs
@@ -7,28 +7,28 @@ fn test_sleep() { task::sleep(1000000u); }
 
 #[test]
 fn test_unsupervise() {
-  fn f() {
-    task::unsupervise();
-    fail;
-  }
-  spawn f();
+    fn f() {
+        task::unsupervise();
+        fail;
+    }
+    spawn f();
 }
 
 #[test]
 fn test_join() {
-  fn winner() {
-  }
+    fn winner() {
+    }
 
-  auto wintask = spawn winner();
+    auto wintask = spawn winner();
 
-  assert task::join(wintask) == task::tr_success;
+    assert task::join(wintask) == task::tr_success;
 
-  fn failer() {
-    task::unsupervise();
-    fail;
-  }
+    fn failer() {
+        task::unsupervise();
+        fail;
+    }
 
-  auto failtask = spawn failer();
+    auto failtask = spawn failer();
 
-  assert task::join(failtask) == task::tr_failure;
+    assert task::join(failtask) == task::tr_failure;
 }
