0e3ee39c41462652f41993a5610265abea6daa96
Marijn Haverbeke <marijnh@gmail.com>
2011-07-27 14:19:29 +0200

    Add fuzzer to reformat make target

--- mk/pp.mk
+++ mk/pp.mk
@@ -2,7 +2,8 @@
 PP_INPUTS = $(wildcard $(addprefix $(S)src/lib/,*.rs */*.rs)) \
             $(wildcard $(addprefix $(S)src/comp/,*.rs */*.rs */*/*.rs)) \
             $(wildcard $(S)src/test/*/*.rs   \
-                       $(S)src/test/*/*/*.rs)
+                       $(S)src/test/*/*/*.rs) \
+            $(wildcard $(S)src/fuzzer/*.rs)
 
 PP_INPUTS_FILTERED = $(shell echo $(PP_INPUTS) | xargs grep -L no-reformat)
 
