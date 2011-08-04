491ed7f12c2c9adf186599ac5294cb24825ad36b
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2011-08-04 16:20:53 -0700

    Port the fuzzer and tests to ivec type [T] syntax.

--- src/test/pretty/ivec-type.rs
+++ src/test/pretty/ivec-type.rs
@@ -1,9 +1,5 @@
 // pp-exact:ivec-type.pp
 
-fn f1(x: int[]) { }
+fn f1(x: [int]) { }
 
 fn g1() { f1(~[1, 2, 3]); }
-
-fn f2(x: [int]) { }
-
-fn g2() { f2(~[1, 2, 3]); }
--- src/test/run-pass/ivec-tag.rs
+++ src/test/run-pass/ivec-tag.rs
@@ -1,14 +1,14 @@
 use std;
 
-fn producer(c: chan[u8[]]) {
+fn producer(c: chan[[u8]]) {
     c <| ~[1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8,
            8u8, 9u8, 10u8, 11u8, 12u8, 13u8 ];
 }
 
 fn main() {
-    let p: port[u8[]] = port();
+    let p: port[[u8]] = port();
     let prod: task = spawn producer(chan(p));
 
-    let data: u8[];
+    let data: [u8];
     p |> data;
 }
