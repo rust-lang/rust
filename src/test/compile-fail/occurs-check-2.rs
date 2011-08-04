2baaeab784a1f5c95deb8bd0c3a9745a969431cc
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-08-03 18:06:57 -0700

    Implement the occurs check
    In the writeback phase, the typechecker now checks that it isn't
    replacing a type variable T with a type that contains T. It
    also does an occurs check in do_autoderef in order to avoid
    getting into an infinite chain of derefs.
    
    I'm a bit worried that there are more places where the occurs
    check needs to happen where I'm not doing it now, though.
    
    Closes #768

---
+++ src/test/compile-fail/occurs-check-2.rs
@@ -0,0 +1,5 @@
+// error-pattern: Type inference failed because I could not find
+fn main() {
+    let f = @f;
+    f();
+}
---
+++ src/test/compile-fail/occurs-check.rs
@@ -0,0 +1,4 @@
+// error-pattern: Type inference failed because I could not find
+fn main() {
+    let f = @f;
+}
