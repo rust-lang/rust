f918418953b2b295eb22970a1d68293d83081ef5
Brian Anderson <banderson@mozilla.com>
2011-08-05 15:47:47 -0700

    Fix a type in the libuv mingw makefile

--- src/rt/libuv/config-mingw.mk
+++ src/rt/libuv/config-mingw.mk
@@ -24,7 +24,7 @@ CC ?= $(PREFIX)gcc
 AR ?= $(PREFIX)ar
 E=.exe
 
-CFLAGS+=$(CPPFLAGS) -g --std=gnu89 -D_WIN32_WINNT=0x0501 -I$(S)src/ares/config_win32
+CFLAGS+=$(CPPFLAGS) -g --std=gnu89 -D_WIN32_WINNT=0x0501 -I$(S)/src/ares/config_win32
 LINKFLAGS=-lm
 
 CARES_OBJS += src/ares/windows_port.o
