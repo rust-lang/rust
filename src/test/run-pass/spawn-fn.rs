9742148e255abe4ed1e629c6e7af57d0c94fc0ed
Brian Anderson <banderson@mozilla.com>
2011-08-10 22:48:17 -0700

    Bring run-pass/spawn-fn up to date and un-XFAIL

--- src/test/run-pass/spawn-fn.rs
+++ src/test/run-pass/spawn-fn.rs
@@ -1,7 +1,7 @@
-// xfail-stage1
-// xfail-stage2
-// xfail-stage3
 // -*- rust -*-
+
+use std;
+import std::task::yield;
 
 fn x(s: str, n: int) { log s; log n; }
 
@@ -10,5 +10,5 @@ fn main() {
     spawn x("hello from second spawned fn", 66);
     spawn x("hello from third spawned fn", 67);
     let i: int = 30;
-    while i > 0 { i = i - 1; log "parent sleeping"; yield; }
+    while i > 0 { i = i - 1; log "parent sleeping"; yield(); }
 }
