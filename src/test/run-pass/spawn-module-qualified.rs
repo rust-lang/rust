d4a17caf1e6ee30a93edf6174ea5d41e3c2e3418
Brian Anderson <banderson@mozilla.com>
2011-08-10 22:45:43 -0700

    Bring run-pass/spawn-module-qualified up to date and un-XFAIL

--- src/test/run-pass/spawn-module-qualified.rs
+++ src/test/run-pass/spawn-module-qualified.rs
@@ -1,12 +1,12 @@
-// xfail-stage1
-// xfail-stage2
-// xfail-stage3
+use std;
+import std::task::join;
+
 fn main() {
-  auto x = spawn m::child(10);
-  join x;
+  let x = spawn m::child(10);
+  join(x);
 }
 mod m {
-  fn child(int i) {
+  fn child(i: int) {
     log i;
   }
 }
