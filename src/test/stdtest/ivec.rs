a76165f67068a6c5cadc29bb7ffece0613fe50f7
Brian Anderson <banderson@mozilla.com>
2011-08-11 21:08:19 -0700

    Move the ivec::reverse tests to stdtest

--- src/test/stdtest/ivec.rs
+++ src/test/stdtest/ivec.rs
@@ -343,6 +343,26 @@ fn test_position_pred() {
     assert (position_pred(is_eighteen, v1) == option::none[uint]);
 }
 
+#[test]
+fn reverse_and_reversed() {
+    let v: [mutable int] = ~[mutable 10, 20];
+    assert (v.(0) == 10);
+    assert (v.(1) == 20);
+    ivec::reverse(v);
+    assert (v.(0) == 20);
+    assert (v.(1) == 10);
+    let v2 = ivec::reversed[int](~[10, 20]);
+    assert (v2.(0) == 20);
+    assert (v2.(1) == 10);
+    v.(0) = 30;
+    assert (v2.(0) == 20);
+    // Make sure they work with 0-length vectors too.
+
+    let v4 = ivec::reversed[int](~[]);
+    let v3: [mutable int] = ~[mutable];
+    ivec::reverse[int](v3);
+}
+
 // Local Variables:
 // mode: rust;
 // fill-column: 78;
