c06d95f1788f7c3b708f0be1fc452c35abf03841
Brian Anderson <banderson@mozilla.com>
2011-08-11 20:46:50 -0700

    Remove references to std::vec from compile-fail/cross-crate-glob-collision.rs

--- src/test/compile-fail/cross-crate-glob-collision.rs
+++ src/test/compile-fail/cross-crate-glob-collision.rs
@@ -1,13 +1,13 @@
-// error-pattern:'swap' is glob-imported from multiple different modules
+// error-pattern:'member' is glob-imported from multiple different modules
 // issue #482
 
 use std;
 // expecting swap to be defined in vec
-import std::vec::*;
+import std::ivec::*;
 import alternate_supplier::*;
 
 mod alternate_supplier {
-    fn swap() { }
+    fn member() { }
 }
 
-fn main() { swap() }
+fn main() { member() }
