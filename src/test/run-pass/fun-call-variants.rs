d681f062c721a31574daaa15b81eba4be85cce2d
Brian Anderson <banderson@mozilla.com>
2011-08-18 21:27:22 -0700

    Clean up run-pass/fun-call-variants
    Uncomment parts of the test that work correctly now, enable pretty testing

--- src/test/run-pass/fun-call-variants.rs
+++ src/test/run-pass/fun-call-variants.rs
@@ -1,6 +1,3 @@
-// xfail-pretty
-
-
 // -*- rust -*-
 fn ho(f: fn(int) -> int ) -> int { let n: int = f(3); ret n; }
 
@@ -9,13 +6,10 @@ fn direct(x: int) -> int { ret x + 1; }
 fn main() {
     let a: int =
         direct(3); // direct
-                   //let int b = ho(direct); // indirect unbound
-
+    let b: int = ho(direct); // indirect unbound
 
     let c: int =
         ho(bind direct(_)); // indirect bound
-                            //assert (a == b);
-                            //assert (b == c);
-
-
+    assert (a == b);
+    assert (b == c);
 }
