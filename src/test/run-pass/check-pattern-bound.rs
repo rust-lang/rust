844e2d7d1cff43bd4f7ace853af8d09ff15e5ab5
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-08-26 00:46:53 -0700

    Test case for checks on pattern-bound vars

---
+++ src/test/run-pass/check-pattern-bound.rs
@@ -0,0 +1,16 @@
+use std;
+import std::option::*;
+
+pure fn p(x:int) -> bool { true }
+
+fn f(x:int) : p(x) { }
+
+fn main() {
+    alt some(5) {
+      some(y) {
+        check p(y);
+        f(y);
+      }
+      _ { fail "yuck"; }
+    }
+}
