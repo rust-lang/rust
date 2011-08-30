4fc72c21302d907a17affeaa4b72e114474410b0
Tim Chevalier <chevalier@alum.wellesley.edu>
2011-08-30 16:43:12 -0700

    XFAIL zip-same-length -- that's ok, since it's a new test

--- src/test/run-pass/zip-same-length.rs
+++ src/test/run-pass/zip-same-length.rs
@@ -1,23 +1,25 @@
+// xfail-stage1
+// xfail-stage2
+// xfail-stage3
 // In this case, the code should compile and should
 // succeed at runtime
 use std;
 import std::uint;
+import std::u8;
 
 import std::vec::*;
 
 fn main() {
     let a = 'a' as u8, j = 'j' as u8, k = 1u, l = 10u;
     // Silly, but necessary
-    check le_u8(a, j);
+    check u8::le(a, j);
     check uint::le(k, l);
     let chars = enum_chars(a, j);
     let ints  = enum_uints(k, l);
 
-/*
     check same_length(chars, ints);
     let ps = zip(chars, ints);
 
     assert (head(ps) == ('a', 1u));
     assert (last(ps) == ('j' as u8, 10u));
-*/
 }
