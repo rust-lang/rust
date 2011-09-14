69eda46af880847f8edc194da9c69974e1e746ef
Brian Anderson <banderson@mozilla.com>
2011-09-14 15:51:46 -0700

    Add a run-pass test for linked failure

---
+++ src/test/run-pass/linked-failure.rs
@@ -0,0 +1,24 @@
+// -*- rust -*-
+
+// error-pattern:1 == 2
+use std;
+import std::task;
+import std::comm::port;
+import std::comm::recv;
+
+fn child() { assert (1 == 2); }
+
+fn parent() {
+    // Since this task isn't supervised it won't bring down the whole
+    // process
+    task::unsupervise();
+    let p = port::<int>();
+    let f = child;
+    task::spawn(f);
+    let x = recv(p);
+}
+
+fn main() {
+    let f = parent;
+    task::spawn(f);
+}
