c0ffd2ea26c657ccab0a467c6843b7ce4300ef63
Graydon Hoare <graydon@mozilla.com>
2011-09-13 17:32:18 -0700

    Print perf measurement outputs on completion of 'perf' target.

--- mk/perf.mk
+++ mk/perf.mk
@@ -2,11 +2,13 @@
 ifdef CFG_PERF_TOOL
 rustc-perf$(X): stage2/rustc$(X)
 	@$(call E, perf compile: $@)
-	$(PERF_STAGE1) -L stage2 -o $@ $(COMPILER_CRATE)
-	rm -f $@
+	$(PERF_STAGE1) -L stage2 -o $@ $(COMPILER_CRATE) >rustc-perf.err 2>&1
+	$(Q)rm -f $@
 else
 rustc-perf$(X): stage2/rustc$(X)
-	touch $@
+	$(Q)touch $@
 endif
 
 perf: check-stage2-perf rustc-perf$(X)
+	$(Q)find test/perf -name \*.err | xargs cat
+	$(Q)cat rustc-perf.err
