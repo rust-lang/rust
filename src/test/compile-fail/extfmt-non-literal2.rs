7e046edebbd03ecc33e267085df426fd5bba27a8
Brian Anderson <banderson@mozilla.com>
2011-09-19 18:00:41 -0700

    Revert "xfail some tests that hang (instead of segfaulting) since eafb6789a2"
    This reverts commit 5f44a1356e2b0adce5157f5e331ab9a55e891b0a.
    
    Issue #936

--- src/test/compile-fail/extfmt-non-literal2.rs
+++ src/test/compile-fail/extfmt-non-literal2.rs
@@ -1,8 +1,5 @@
 // error-pattern: literal
 
-// xfail-test (issue #936)
-
-
 fn main() {
     // #fmt's first argument must be a literal.  Hopefully this
     // restriction can be eased eventually to just require a
