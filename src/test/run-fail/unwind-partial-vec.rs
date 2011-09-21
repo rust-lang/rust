956bc69330cef83977c6f0031fc53efc44542560
Brian Anderson <banderson@mozilla.com>
2011-09-21 10:56:28 -0700

    Add more unwind tests for failure during construction of allocated things

---
+++ src/test/run-fail/unwind-partial-vec.rs
@@ -0,0 +1,18 @@
+// error-pattern:fail
+
+fn f() -> [int] { fail; }
+
+// Voodoo. In unwind-alt we had to do this to trigger the bug. Might
+// have been to do with memory allocation patterns.
+fn prime() {
+    @0;
+}
+
+fn partial() {
+    let x = [[0], f(), [0]];
+}
+
+fn main() {
+    prime();
+    partial();
+}
