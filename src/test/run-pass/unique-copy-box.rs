3bb70359140062125405c6f592bd23dc34280e4b
Brian Anderson <banderson@mozilla.com>
2011-09-22 14:39:57 -0700

    Add a test for unique boxes containing shared boxes
    Issue #409

---
+++ src/test/run-pass/unique-copy-box.rs
@@ -0,0 +1,12 @@
+use std;
+import std::sys::rustrt::refcount;
+
+fn main() {
+    let i = ~@1;
+    let j = ~@2;
+    let rc1 = refcount(*i);
+    let j = i;
+    let rc2 = refcount(*i);
+    log_err #fmt("rc1: %u rc2: %u", rc1, rc2);
+    assert rc1 + 1u == rc2;
+}
