f8007b5535938be4f8b774177bc96dda2e9f5a71
Patrick Walton <pcwalton@mimiga.net>
2011-09-23 11:02:58 -0700

    rt: Remove the problematic min()/max()/align()/abs() macros from isaac/standard.h, as they're unused

--- src/rt/isaac/standard.h
+++ src/rt/isaac/standard.h
@@ -38,18 +38,6 @@ typedef                 int  word;  /* fastest type available */
 #define bis(target,mask)  ((target) |=  (mask))
 #define bic(target,mask)  ((target) &= ~(mask))
 #define bit(target,mask)  ((target) &   (mask))
-#ifndef min
-# define min(a,b) (((a)<(b)) ? (a) : (b))
-#endif /* min */
-#ifndef max
-# define max(a,b) (((a)<(b)) ? (b) : (a))
-#endif /* max */
-#ifndef align
-# define align(a) (((ub4)a+(sizeof(void *)-1))&(~(sizeof(void *)-1)))
-#endif /* align */
-#ifndef abs
-# define abs(a)   (((a)>0) ? (a) : -(a))
-#endif
 #define TRUE  1
 #define FALSE 0
 #define SUCCESS 0  /* 1 on VAX */
