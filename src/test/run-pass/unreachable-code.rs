7b1a3bb8e6296541f0200e42d54a77363709a9bd
Jesse Ruderman <jruderman@gmail.com>
2011-09-23 15:01:19 -0700

    Add more unreachable-code tests. Closes #935

--- src/test/run-pass/unreachable-code.rs
+++ src/test/run-pass/unreachable-code.rs
@@ -19,6 +19,12 @@ iter put_break() -> int {
 
 fn log_fail() { log_err fail; }
 
+fn log_ret() { log_err ret; }
+
+fn log_break() { while true { log_err break; } }
+
+fn log_cont() { do { log_err cont; } while false }
+
 fn ret_ret() -> int { ret (ret 2) + 3; }
 
 fn ret_guard() {
