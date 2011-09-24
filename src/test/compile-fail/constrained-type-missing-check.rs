c29f1af17e49ae01d865f443ea0ff2f92bf3a888
Jesse Ruderman <jruderman@gmail.com>
2011-09-24 16:11:50 -0700

    Remove irrelevant parts of test

--- src/test/compile-fail/constrained-type-missing-check.rs
+++ src/test/compile-fail/constrained-type-missing-check.rs
@@ -2,10 +2,6 @@
 // xfail-test
 // error-pattern:Unsatisfied precondition
 
-tag list { cons(int, @list); nil; }
-
-type bubu = {x: int, y: int};
-
 pure fn less_than(x: int, y: int) -> bool { ret x < y; }
 
 type ordered_range = {low: int, high: int} : less_than(low, high);
