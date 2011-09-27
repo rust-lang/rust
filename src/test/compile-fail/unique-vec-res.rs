4bffa5e13a5a103a3ced7e868bd2eb673755a7e6
Brian Anderson <banderson@mozilla.com>
2011-09-27 14:24:18 -0700

    Add an xfailed test for bogus vector addition of typarams

---
+++ src/test/compile-fail/unique-vec-res.rs
@@ -0,0 +1,21 @@
+// xfail-test
+// expected error: mismatched kinds
+
+resource r(i: @mutable int) {
+    *i = *i + 1;
+}
+
+fn f<@T>(i: [T], j: [T]) {
+    // Shouldn't be able to do this copy of j
+    let k = i + j;
+}
+
+fn main() {
+    let i1 = @mutable 0;
+    let i2 = @mutable 1;
+    let r1 <- [~r(i1)];
+    let r2 <- [~r(i2)];
+    f(r1, r2);
+    log_err *i1;
+    log_err *i2;
+}
