df19b7a13a7e55b73be9fc633db6ec76c3793658
Brian Anderson <banderson@mozilla.com>
2011-09-26 23:24:53 -0700

    Test for issue #980

---
+++ src/test/run-pass/issue-980.rs
@@ -0,0 +1,13 @@
+tag maybe_pointy {
+    no_pointy;
+    yes_pointy(@pointy);
+}
+
+type pointy = {
+    mutable x : maybe_pointy
+};
+
+fn main() {
+    let m = @{ mutable x : no_pointy };
+    m.x = yes_pointy(m);
+}
