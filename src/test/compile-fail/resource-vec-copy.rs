b49f4689f54f8ab6b3d7350d4902236a78e5852f
Brian Anderson <banderson@mozilla.com>
2011-09-26 23:45:51 -0700

    Add two xfailed tests for invalid copies into vectors

---
+++ src/test/compile-fail/resource-vec-copy.rs
@@ -0,0 +1,18 @@
+// xfail-test
+// error-pattern:mismatched kind
+
+resource r(i: @mutable int) {
+    *i = *i + 1;
+}
+
+fn main() {
+    let a = @mutable 0;
+    {
+        let i <- r(a);
+        // Can't copy into here
+        let j <- [i];
+    }
+    log_err *a;
+    // this is no good
+    assert *a == 2;
+}
