8217e4bd3821767131f9fb56c1f42aaf71889c43
Brian Anderson <banderson@mozilla.com>
2011-09-27 15:22:06 -0700

    Add xfailed tests that resources can't be copied into various things

---
+++ src/test/compile-fail/copy-res-into-rec.rs
@@ -0,0 +1,16 @@
+// xfail-test
+// error-pattern:mismatched kinds
+resource r(i: @mutable int) {
+    *i = *i + 1;
+}
+
+fn main() {
+    let i = @mutable 0;
+    {
+        let j <- r(i);
+        // No no no no no
+        let k <- {x: j};
+    }
+    log_err *i;
+    assert *i == 2;
+}
---
+++ src/test/compile-fail/copy-res-into-tag.rs
@@ -0,0 +1,20 @@
+// xfail-test
+// error-pattern:mismatched kinds
+resource r(i: @mutable int) {
+    *i = *i + 1;
+}
+
+tag t {
+    t0(r);
+}
+
+fn main() {
+    let i = @mutable 0;
+    {
+        let j <- r(i);
+        // No no no no no
+        let k <- t0(j);
+    }
+    log_err *i;
+    assert *i == 2;
+}
