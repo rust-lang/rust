e71d17ffa67627042d665771f7fa9dd2f3a7a662
Brian Anderson <banderson@mozilla.com>
2011-09-30 16:27:18 -0700

    Refix the snapshot rules

--- mk/snap.mk
+++ mk/snap.mk
@@ -3,10 +3,10 @@ snap-stage1: $(HOST_BIN1)/rustc$(X) $(HOST_LIB1)/$(CFG_RUNTIME) \
 	$(HOST_LIB1)/$(CFG_RUSTLLVM) $(HOST_LIB1)/$(CFG_STDLIB)
 	$(S)src/etc/make-snapshot.py stage1
 
-snap-stage1: $(HOST_BIN2)/rustc$(X) $(HOST_LIB2)/$(CFG_RUNTIME) \
+snap-stage2: $(HOST_BIN2)/rustc$(X) $(HOST_LIB2)/$(CFG_RUNTIME) \
 	$(HOST_LIB2)/$(CFG_RUSTLLVM) $(HOST_LIB2)/$(CFG_STDLIB)
-	$(S)src/etc/make-snapshot.py stage1
+	$(S)src/etc/make-snapshot.py stage2
 
-snap-stage1: $(HOST_BIN3)/rustc$(X) $(HOST_LIB3)/$(CFG_RUNTIME) \
+snap-stage3: $(HOST_BIN3)/rustc$(X) $(HOST_LIB3)/$(CFG_RUNTIME) \
 	$(HOST_LIB3)/$(CFG_RUSTLLVM) $(HOST_LIB3)/$(CFG_STDLIB)
-	$(S)src/etc/make-snapshot.py stage1
+	$(S)src/etc/make-snapshot.py stage3
