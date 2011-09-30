196cc6de2e517d4a265990409132e6064e487f4f
Brian Anderson <banderson@mozilla.com>
2011-09-29 18:30:00 -0700

    Teach compiletest to construct the PATH variable correctly on win32

--- src/test/compiletest/util.rs
+++ src/test/compiletest/util.rs
@@ -10,7 +10,9 @@ fn make_new_path(path: str) -> str {
     // Windows just uses PATH as the library search path, so we have to
     // maintain the current value while adding our own
     alt getenv(lib_path_env_var()) {
-      option::some(curr) { #fmt["%s:%s", path, curr] }
+      option::some(curr) {
+        #fmt["%s%s%s", path, path_div(), curr]
+      }
       option::none. { path }
     }
 }
@@ -23,6 +25,13 @@ fn lib_path_env_var() -> str { "DYLD_LIBRARY_PATH" }
 
 #[cfg(target_os = "win32")]
 fn lib_path_env_var() -> str { "PATH" }
+
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "macos")]
+fn path_div() -> str { ":" }
+
+#[cfg(target_os = "win32")]
+fn path_div() -> str { ";" }
 
 fn logv(config: config, s: str) {
     log s;
