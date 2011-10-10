816548ba8dc76ac3015ef8af7e74271f9cd6ea23
Niko Matsakis <niko@alum.mit.edu>
2011-10-10 11:59:20 -0700

    continue to annotate trans functions as unsafe where neccessary

--- src/comp/middle/trans_objects.rs
+++ src/comp/middle/trans_objects.rs
@@ -569,7 +569,7 @@ fn create_backwarding_vtbl(cx: @local_ctxt, sp: span, inner_obj_ty: ty::t,
 // finish_vtbl: Given a vector of vtable entries, create the table in
 // read-only memory and return a pointer to it.
 fn finish_vtbl(cx: @local_ctxt, llmethods: [ValueRef], name: str) ->
-   ValueRef {
+   ValueRef unsafe {
     let vtbl = C_struct(llmethods);
     let vtbl_name = mangle_internal_name_by_path(cx.ccx, cx.path + [name]);
     let gvar =
@@ -619,7 +619,7 @@ fn begin_fn(cx: @local_ctxt, sp: span, m: @ty::method,
 // returns the value returned from that call.
 fn process_bkwding_mthd(cx: @local_ctxt, sp: span, m: @ty::method,
                         ty_params: [ast::ty_param], outer_obj_ty: ty::t,
-                        _additional_field_tys: [ty::t]) -> ValueRef {
+                        _additional_field_tys: [ty::t]) -> ValueRef unsafe {
 
     let llbackwarding_fn = begin_fn(cx, sp, m, ty_params, "backwarding_fn");
     let fcx = new_fn_ctxt(cx, sp, llbackwarding_fn);
@@ -724,7 +724,7 @@ fn process_bkwding_mthd(cx: @local_ctxt, sp: span, m: @ty::method,
 fn process_fwding_mthd(cx: @local_ctxt, sp: span, m: @ty::method,
                        ty_params: [ast::ty_param], inner_obj_ty: ty::t,
                        backwarding_vtbl: ValueRef,
-                       additional_field_tys: [ty::t]) -> ValueRef {
+                       additional_field_tys: [ty::t]) -> ValueRef unsafe {
 
     // Create a new function context and block context for the function,
     // holding onto a pointer to the first block.
