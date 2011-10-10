84e98f4f652c8b33b89c238e12052b8b0d9246d2
Brian Anderson <banderson@mozilla.com>
2011-10-10 14:03:25 -0700

    Add a test for higher-order bare functions
    Issue #1022

---
+++ src/test/run-pass/fn-bare-assign.rs
@@ -0,0 +1,15 @@
+fn# f(i: int, &called: bool) {
+    assert i == 10;
+    called = true;
+}
+
+fn# g(f: fn#(int, &bool), &called: bool) {
+    f(10, called);
+}
+
+fn main() {
+    let called = false;
+    let h = f;
+    g(h, called);
+    assert called == true;
+}
