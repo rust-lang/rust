90c240208985aff48c8bf09574c0cb1d79868593
Brian Anderson <banderson@mozilla.com>
2011-10-10 18:19:01 -0700

    Add a test that uses a hypothetical bare-fn spawn signature
    Issue #1022

---
+++ src/test/run-pass/fn-bare-spawn.rs
@@ -0,0 +1,16 @@
+// This is what the signature to spawn should look like with bare functions
+
+fn spawn<~T>(val: T, f: fn#(T)) {
+    f(val);
+}
+
+fn# f(&&i: int) {
+    assert i == 100;
+}
+
+fn main() {
+    spawn(100, f);
+    spawn(100, fn#(&&i: int) {
+        assert i == 100;
+    });
+}
