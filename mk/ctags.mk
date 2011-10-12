e1c470c9ed9ea316c4ad77604331ee3eca0b53d9
Niko Matsakis <niko@alum.mit.edu>
2011-10-12 12:10:21 -0700

    new targets, TAGS.emacs and TAGS.vi. they must be manually made.

---
+++ mk/ctags.mk
@@ -0,0 +1,12 @@
+######################################################################
+# TAGS file creation.  No dependency tracking, just do it on demand.
+# Requires Exuberant Ctags: http://ctags.sourceforge.net/index.html
+######################################################################
+
+CTAGS_OPTS=--options=${CFG_SRC_DIR}/mk/ctags.rust -R ${CFG_SRC_DIR}/src
+
+TAGS.emacs:
+	ctags -e -f $@ ${CTAGS_OPTS}
+
+TAGS.vi:
+	ctags -f $@ ${CTAGS_OPTS}
