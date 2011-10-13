60cfa91897b90d37ef85068f93c01b2b99c30e10
Brian Anderson <banderson@mozilla.com>
2011-10-13 12:16:26 -0700

    Add a test that generic bare functions can be bound
    Issue #1022

---
+++ src/test/run-pass/fn-bare-bind-generic.rs
@@ -0,0 +1,10 @@
+fn# f<T>(i: T, j: T, k: T) {
+    assert i == j;
+    assert j != k;
+}
+
+fn main() {
+    // Binding a bare function turns it into a shared closure
+    let g: fn() = bind f(10, 10, 20);
+    g();
+}
