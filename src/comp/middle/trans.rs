b5157c25c29c4544ed33574523c603b9347310f9
Brian Anderson <banderson@mozilla.com>
2011-10-14 15:16:44 -0700

    Revert "Remove the environment argument from bare functions"
    This reverts commit 1b0f1f0b79fb29d28bcabb7a8468f4a5a95c0712.

--- src/comp/middle/trans.rs
+++ src/comp/middle/trans.rs
@@ -98,16 +98,7 @@ fn type_of_fn(cx: @crate_ctxt, sp: span, proto: ast::proto,
     // Arg 2: Env (closure-bindings / self-obj)
     if is_method {
         atys += [T_ptr(cx.rust_object_type)];
-    } else {
-        alt proto {
-          ast::proto_bare. {
-            // Bare functions have no environment
-          }
-          _ {
-            atys += [T_opaque_closure_ptr(*cx)];
-          }
-        }
-    }
+    } else { atys += [T_opaque_closure_ptr(*cx)]; }
 
     // Args >3: ty params, if not acquired via capture...
     if !is_method {
@@ -3560,15 +3551,7 @@ fn trans_bind_thunk(cx: @local_ctxt, sp: span, incoming_fty: ty::t,
     }
 
     // Set up the three implicit arguments to the thunk.
-    let llargs: [ValueRef] = alt ty::ty_fn_proto(ccx.tcx, outgoing_fty) {
-      ast::proto_bare. {
-        // Bare functions don't take an environment
-        [llretptr, fcx.lltaskptr]
-      }
-      _ {
-        [llretptr, fcx.lltaskptr, lltargetenv]
-      }
-    };
+    let llargs: [ValueRef] = [llretptr, fcx.lltaskptr, lltargetenv];
 
     // Copy in the type parameters.
     let i: uint = 0u;
@@ -3854,12 +3837,7 @@ fn trans_args(cx: @block_ctxt, outer_cx: @block_ctxt, llenv: ValueRef,
     llargs += [bcx.fcx.lltaskptr];
 
     // Arg 2: Env (closure-bindings / self-obj)
-    alt ty::ty_fn_proto(tcx, fn_ty) {
-      ast::proto_bare. { }
-      _ {
-        llargs += [llenv];
-      }
-    }
+    llargs += [llenv];
 
     // Args >3: ty_params ...
     llargs += lltydescs;
@@ -5112,7 +5090,6 @@ fn new_fn_ctxt_w_id(cx: @local_ctxt, sp: span, llfndecl: ValueRef,
                     id: ast::node_id, rstyle: ast::ret_style)
     -> @fn_ctxt {
     let llbbs = mk_standard_basic_blocks(llfndecl);
-    // FIXME: llenv is not correct for bare functions
     ret @{llfn: llfndecl,
           lltaskptr: llvm::LLVMGetParam(llfndecl, 1u),
           llenv: llvm::LLVMGetParam(llfndecl, 2u),
@@ -5143,13 +5120,6 @@ fn new_fn_ctxt(cx: @local_ctxt, sp: span, llfndecl: ValueRef) -> @fn_ctxt {
     ret new_fn_ctxt_w_id(cx, sp, llfndecl, -1, ast::return_val);
 }
 
-fn implicit_args_for_fn(proto: ast::proto) -> uint {
-    alt proto {
-      ast::proto_bare. { 2u }
-      _ { 3u }
-    }
-}
-
 // NB: must keep 4 fns in sync:
 //
 //  - type_of_fn
@@ -5167,8 +5137,10 @@ fn implicit_args_for_fn(proto: ast::proto) -> uint {
 fn create_llargs_for_fn_args(cx: @fn_ctxt, proto: ast::proto,
                              ty_self: option::t<ty::t>, ret_ty: ty::t,
                              args: [ast::arg], ty_params: [ast::ty_param]) {
-    // Skip the implicit arguments
-    let arg_n = implicit_args_for_fn(proto);
+    // Skip the implicit arguments 0, 1, and 2.  TODO: Pull out 3u and define
+    // it as a constant, since we're using it in several places in trans this
+    // way.
+    let arg_n = 3u;
     alt ty_self {
       some(tt) { cx.llself = some::<val_self_pair>({v: cx.llenv, t: tt}); }
       none. {
