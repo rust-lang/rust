eba88ef3ea4a50247961075c1487650dbcf86262
David Rajchenbach-Teller <dteller@mozilla.com>
2011-10-17 14:43:53 +0200

    [Lib] u32.rs: created

---
+++ src/lib/u32.rs
@@ -0,0 +1,13 @@
+pure fn max_value() -> u32 { ret 4294967296u32; }
+pure fn min_value() -> u32 { ret 0u32; }
+
+//
+// Local Variables:
+// mode: rust
+// fill-column: 78;
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// buffer-file-coding-system: utf-8-unix
+// compile-command: "make -k -C $RBUILD 2>&1 | sed -e 's/\\/x\\//x:\\//g'";
+// End:
+//
