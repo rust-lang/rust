b53825dea6e36ebae1b876a4bcffcfef2d60875f
Niko Matsakis <niko@alum.mit.edu>
2011-10-20 10:18:39 -0700

    remove unnecessary && from various file routines

--- src/lib/win32_fs.rs
+++ src/lib/win32_fs.rs
@@ -1,8 +1,8 @@
 
 
 native "c-stack-cdecl" mod rustrt {
-    fn rust_list_files(&&path: str) -> [str];
-    fn rust_file_is_dir(&&path: str) -> int;
+    fn rust_list_files(path: str) -> [str];
+    fn rust_file_is_dir(path: str) -> int;
 }
 
 fn list_dir(path: str) -> [str] {
