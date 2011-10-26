1b75e5c315f00e6a10b1eca0a2f501107fd8063e
Brian Anderson <banderson@mozilla.com>
2011-10-26 11:55:28 -0700

    Use web-style paragraphs in std docs

--- mk/docs.mk
+++ mk/docs.mk
@@ -20,15 +20,19 @@ docsnap: doc/rust.pdf
 	@$(call E, snap: doc/rust-$(shell date +"%Y-%m-%d")-snap.pdf)
 	$(Q)mv $< doc/rust-$(shell date +"%Y-%m-%d")-snap.pdf
 
-doc/std/index.html: nd/std/Languages.txt nd/std/Topics.txt \
+doc/std/index.html: nd/std/Languages.txt nd/std/Topics.txt nd/std/std.css \
                     $(STDLIB_CRATE) $(STDLIB_INPUTS)
 	@$(call E, naturaldocs: $@)
-	naturaldocs -i $(S)src/lib -o HTML doc/std -p nd/std -r
+	naturaldocs -i $(S)src/lib -o HTML doc/std -p nd/std -r -s Default std
 
 nd/std/Languages.txt: $(S)doc/Languages.txt
 	@$(call E, cp: $@)
 	$(Q)cp $< $@
 
 nd/std/Topics.txt: $(S)doc/Topics.txt
+	@$(call E, cp: $@)
+	$(Q)cp $< $@
+
+nd/std/std.css: $(S)doc/std.css
 	@$(call E, cp: $@)
 	$(Q)cp $< $@
