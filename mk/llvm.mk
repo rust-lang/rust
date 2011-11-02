fed44acb407b71bafedbb6bf5bcab90ba1ce10c2
Brian Anderson <banderson@mozilla.com>
2011-11-01 17:09:44 -0700

    LLVM build cleanup

--- mk/llvm.mk
+++ mk/llvm.mk
@@ -1,5 +1,7 @@
-.PHONY: $(CFG_LLVM_INST_DIR)/bin/llc
+# This is just a rough approximation of LLVM deps
+LLVM_DEPS:=$(wildcard $(addprefix $(CFG_LLVM_SRC_DIR)/, \
+                        * */*h */*/*h */*/*/*h */*cpp */*/*cpp */*/*/*cpp))
 
-$(CFG_LLVM_INST_DIR)/bin/llc:
+$(LLVM_CONFIG): $(LLVM_DEPS)
 	@$(call E, make: llvm)
 	$(Q)make -C $(CFG_LLVM_BUILD_DIR)
