8857657640f16da938de9e24cc4adf6960e29784
Brian Anderson <banderson@mozilla.com>
2011-11-07 19:35:39 -0800

    Rename rust_uv.cpp to rust_aio.cpp

--- mk/rt.mk
+++ mk/rt.mk
@@ -48,7 +48,7 @@ RUNTIME_CS_$(1) := \
               rt/rust_chan.cpp \
               rt/rust_port.cpp \
               rt/rust_upcall.cpp \
-              rt/rust_uv.cpp \
+              rt/rust_aio.cpp \
               rt/rust_log.cpp \
               rt/rust_timer.cpp \
               rt/circular_buffer.cpp \
