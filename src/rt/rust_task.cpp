69911c2bc1a4271edf8db63dbdf6847654922a69
Niko Matsakis <niko@alum.mit.edu>
2011-11-11 15:28:16 -0800

    temp. disable cycle coll. on x86_64

--- src/rt/rust_task.cpp
+++ src/rt/rust_task.cpp
@@ -167,7 +167,9 @@ void task_start_wrapper(spawn_args *a)
         failed = true;
     }
 
+#   ifndef __x86_64__ // FIXME: temp. hack on X86-64 (NDM)
     cc::do_cc(task);
+#   endif
 
     rust_closure_env* env = (rust_closure_env*)a->a3;
     if(env) {
