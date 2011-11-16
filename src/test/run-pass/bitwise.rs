f47f92235acd84e42cce4e31b36f507a460b0f53
Brian Anderson <banderson@mozilla.com>
2011-11-15 17:01:06 -0800

    Make run-pass/bitwise 64-bit compatible

--- src/test/run-pass/bitwise.rs
+++ src/test/run-pass/bitwise.rs
@@ -1,8 +1,18 @@
-
-
-
 // -*- rust -*-
-fn main() {
+
+#[cfg(target_arch = "x86")]
+fn target() {
+    assert (-1000 >> 3 == 536870787);
+}
+
+#[cfg(target_arch = "x86_64")]
+fn target() {
+    // FIXME (974) Can't lex this as a single integer
+    assert (-1000 >> 3 == 23058430 * 1000000000 * 100
+            + 92 * 100000000 + 13693827);
+}
+
+fn general() {
     let a: int = 1;
     let b: int = 2;
     a ^= b;
@@ -18,5 +28,9 @@ fn main() {
     assert (0xf0 >> 4 == 0xf);
     assert (-16 >>> 2 == -4);
     assert (0b1010_1010 | 0b0101_0101 == 0xff);
-    assert (-1000 >> 3 == 536870787);
+}
+
+fn main() {
+    general();
+    target();
 }
