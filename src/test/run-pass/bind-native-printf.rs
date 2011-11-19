12a67af81df4f05d9bc9f6634f615a69098f1d04
Niko Matsakis <niko@alum.mit.edu>
2011-11-18 16:15:03 -0800

    add test for issue #1174

---
+++ src/test/run-pass/bind-native-printf.rs
@@ -0,0 +1,16 @@
+// From #1174:
+
+use std;
+
+#[link_name = ""]
+native mod libc {
+    fn printf(s: *u8, a: int); /* A tenuous definition. */
+}
+
+fn main() {
+    let b = std::str::bytes("%d\n");
+    let b8 = unsafe { std::vec::unsafe::to_ptr(b) };
+    libc::printf(b8, 4);
+    let a = bind libc::printf(b8, 5);
+    a(); /* core dump */
+}
