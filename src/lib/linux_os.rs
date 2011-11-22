9dd4789d80b1313bb39daa8c8973e63a60b07b94
Elly Jones <elly@leptoquark.net>
2011-11-22 17:09:35 -0500

    Add fs::rmdir() and tempfile/gen_str() tests.

--- src/lib/linux_os.rs
+++ src/lib/linux_os.rs
@@ -52,6 +52,7 @@ native mod libc {
     fn waitpid(pid: pid_t, &status: c_int, options: c_int) -> pid_t;
     fn readlink(path: str::sbuf, buf: str::sbuf, bufsize: size_t) -> ssize_t;
     fn mkdir(path: str::sbuf, mode: int) -> int;
+    fn rmdir(path: str::sbuf) -> int;
 }
 
 mod libc_constants {
