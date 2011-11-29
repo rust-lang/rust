821298737a123da856ad9d994343371c02c97a2b
Niko Matsakis <niko@alum.mit.edu>
2011-11-28 22:05:22 -0800

    fix dependencies for snap-stage[23]

--- mk/snap.mk
+++ mk/snap.mk
@@ -2,8 +2,8 @@
 snap-stage1: $(HSREQ1_H_$(CFG_HOST_TRIPLE))
 	$(S)src/etc/make-snapshot.py stage1 $(CFG_HOST_TRIPLE)
 
-snap-stage2: $(HSREQ2_H_$(CFG_HOST_TRIPLE)
+snap-stage2: $(HSREQ2_H_$(CFG_HOST_TRIPLE))
 	$(S)src/etc/make-snapshot.py stage2 $(CFG_HOST_TRIPLE)
 
-snap-stage3: $(HSREQ3_H_$(CFG_HOST_TRIPLE)
+snap-stage3: $(HSREQ3_H_$(CFG_HOST_TRIPLE))
 	$(S)src/etc/make-snapshot.py stage3 $(CFG_HOST_TRIPLE)
