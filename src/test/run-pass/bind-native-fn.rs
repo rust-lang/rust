fdebd1e2ef440312ac791d505af7dd089efa2499
Niko Matsakis <niko@alum.mit.edu>
2011-11-30 15:55:28 -0800

    adjust and un-xfail

---
+++ src/test/run-pass/bind-native-fn.rs
@@ -0,0 +1,21 @@
+// From #1174:
+// xfail-test bots are crashing on this on x86_64
+
+use std;
+import std::str;
+import std::ctypes::*;
+
+#[link_name = ""]
+native mod libc {
+    fn write(fd: c_int, buf: *u8, nbyte: size_t);
+}
+
+fn main() {
+    let s = "hello world\n";
+    let b = str::bytes(s);
+    let l = str::byte_len(s);
+    let b8 = unsafe { std::vec::unsafe::to_ptr(b) };
+    libc::write(0i32, b8, l);
+    let a = bind libc::write(0i32, _, _);
+    a(b8, l);
+}
