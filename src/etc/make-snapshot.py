1b7f1415d629d984a7e839a269e88ca6cfb743a0
Stefan Plantikow <stefan.plantikow@googlemail.com>
2011-12-01 03:33:39 +0100

    build: added CFG_INSTALL_SNAP for automatically activating test snapshots
    This is only guaranteed to work when building in the top-level dir

--- src/etc/make-snapshot.py
+++ src/etc/make-snapshot.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 import snapshot, sys
-print(snapshot.make_snapshot(sys.argv[1], sys.argv[2]))
+
+if len(sys.argv) == 2:
+    print(snapshot.make_snapshot(sys.argv[1], sys.argv[2], ""))
+else:
+    print(snapshot.make_snapshot(sys.argv[1], sys.argv[2], sys.argv[3]))
