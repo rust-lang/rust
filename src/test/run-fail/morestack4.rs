471b4d6e58baba9ee880c8c8f4183bcf363708cf
Brian Anderson <banderson@mozilla.com>
2011-12-17 13:51:35 -0800

    rt: Use a DWARF CFI scheme that works on mac in __morestack

---
+++ src/test/run-fail/morestack4.rs
@@ -0,0 +1,28 @@
+// xfail-test
+// error-pattern:explicit failure
+// compile-flags:--stack-growth
+
+// Just testing unwinding
+
+use std;
+
+native mod rustrt {
+    fn set_min_stack(size: uint);
+}
+
+fn getbig_and_fail(&&i: int) {
+    let r = and_then_get_big_again(@0);
+    if i != 0 {
+        getbig_and_fail(i - 1);
+    } else {
+        fail;
+    }
+}
+
+resource and_then_get_big_again(_i: @int) {
+}
+
+fn main() {
+    rustrt::set_min_stack(256u);
+    task::spawn(1, getbig_and_fail);
+}
