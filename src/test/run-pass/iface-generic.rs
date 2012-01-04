b02a77d6b471fe76e8c88ea8462a5251f7744ca3
Marijn Haverbeke <marijnh@gmail.com>
2012-01-04 17:28:16 +0100

    Add test for generic iface methods
    Issue #1227

---
+++ src/test/run-pass/iface-generic.rs
@@ -0,0 +1,33 @@
+iface to_str {
+    fn to_str() -> str;
+}
+impl of to_str for int {
+    fn to_str() -> str { int::str(self) }
+}
+impl of to_str for str {
+    fn to_str() -> str { self }
+}
+
+iface map<T> {
+    fn map<U>(f: block(T) -> U) -> [U];
+}
+impl <T> of map<T> for [T] {
+    fn map<U>(f: block(T) -> U) -> [U] {
+        let r = [];
+        for x in self { r += [f(x)]; }
+        r
+    }
+}
+
+fn foo<U, T: map<U>>(x: T) -> [str] {
+    x.map({|_e| "hi" })
+}
+fn bar<U: to_str, T: map<U>>(x: T) -> [str] {
+    x.map({|_e| _e.to_str() })
+}
+
+fn main() {
+    assert foo([1]) == ["hi"];
+    assert bar::<int, [int]>([4, 5]) == ["4", "5"];
+    assert bar::<str, [str]>(["x", "y"]) == ["x", "y"];
+}
