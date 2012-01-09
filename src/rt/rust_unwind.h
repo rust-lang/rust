26536e69151d5f8e083d8165421039b409c93ee3
Haitao Li <lihaitao@gmail.com>
2012-01-09 15:28:18 +0800

    rt: Fix build with clang on non-Mac

--- src/rt/rust_unwind.h
+++ src/rt/rust_unwind.h
@@ -6,14 +6,19 @@
 #ifdef __APPLE__
 #include <libunwind.h>
 
-typedef int _Unwind_Action;
 typedef void _Unwind_Context;
-typedef void _Unwind_Exception;
 typedef int _Unwind_Reason_Code;
 
 #else
 
 #include <unwind.h>
+
+#endif
+
+#if (defined __APPLE__) || (defined __clang__)
+
+typedef int _Unwind_Action;
+typedef void _Unwind_Exception;
 
 #endif
 
