31bb6a60bb59d40c97ab3f68a313d199429bf4ef
Niko Matsakis <niko@alum.mit.edu>
2012-01-13 13:56:50 -0800

    Fixes #1499: a test that shape code can walk explicit disr values

---
+++ src/test/run-pass/tag-disr-val-shape.rs
@@ -0,0 +1,14 @@
+tag color {
+    red = 0xff0000;
+    green = 0x00ff00;
+    blue = 0x0000ff;
+    black = 0x000000;
+    white = 0xFFFFFF;
+}
+
+fn main() {
+    assert uint::to_str(red as uint, 10u) == #fmt["%?", red];
+    assert uint::to_str(green as uint, 10u) == #fmt["%?", green];
+    assert uint::to_str(white as uint, 10u) == #fmt["%?", white];
+}
+
