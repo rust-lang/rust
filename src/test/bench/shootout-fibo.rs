492dba8bfe8b5d906bce6eb0e90e095c168863ec
Brian Anderson <banderson@mozilla.com>
2012-01-14 19:00:59 -0800

    bench: Update shootout-fibo for performance

--- src/test/bench/shootout-fibo.rs
+++ src/test/bench/shootout-fibo.rs
@@ -1,19 +1,20 @@
+use std;
 
-
-// -*- rust -*-
 fn fib(n: int) -> int {
-
-
-    // Several of the posted 'benchmark' versions of this compute the
-    // wrong Fibonacci numbers, of course.
-    if n == 0 {
-        ret 0;
-    } else { if n <= 2 { ret 1; } else { ret fib(n - 1) + fib(n - 2); } }
+    if n < 2 {
+        ret 1;
+    } else {
+        ret fib(n - 1) + fib(n - 2);
+    }
 }
 
-fn main() {
-    assert (fib(8) == 21);
-    assert (fib(15) == 610);
-    log(debug, fib(8));
-    log(debug, fib(15));
+fn main(args: [str]) {
+    // FIXME: #1527
+    sys::set_min_stack(1000000u);
+    let n = if vec::len(args) == 2u {
+        int::from_str(args[1])
+    } else {
+        40
+    };
+    std::io::println(#fmt("%d\n", fib(n)));
 }
