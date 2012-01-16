2d36a71aee17b6dad04a184541cebbb8e3901a5a
Kevin Atkinson <kevina@cs.utah.edu>
2012-01-15 19:56:20 -0700

    Update pretty printer to print out disr. values.
    Partly fixes issue #1510.  "rustc --pretty=typed" fails.

---
+++ src/test/run-pass/enum-disr-val-pretty.rs
@@ -0,0 +1,16 @@
+// pp-exact
+
+enum color { red = 1; green; blue; imaginary = -1; }
+
+fn main() {
+    test_color(red, 1, "red");
+    test_color(green, 2, "green");
+    test_color(blue, 3, "blue");
+    test_color(imaginary, -1, "imaginary");
+}
+
+fn test_color(color: color, val: int, name: str) unsafe{
+    assert (color as int == val);
+    assert (color as float == val as float);
+}
+
