f3c42636357a2757d9a01871580cb660418ca77e
Brian Anderson <banderson@mozilla.com>
2012-01-15 21:50:55 -0800

    rustdoc: Move parse_compound_fndoc into mod attr_parser

---
+++ src/rustdoc/attr_parser.rs
@@ -0,0 +1,67 @@
+export parse_compound_fndoc;
+
+#[doc(
+  brief = "Parses function docs from a complex #[doc] attribute.",
+  desc = "Supported attributes:
+
+* `brief`: Brief description
+* `desc`: Long description
+* `return`: Description of return value
+* `args`: List of argname = argdesc pairs
+",
+  args(items = "Doc attribute contents"),
+  return = "Parsed function docs."
+)]
+fn parse_compound_fndoc(items: [@ast::meta_item]) -> doc::fndoc {
+    let brief = none;
+    let desc = none;
+    let return = none;
+    let argdocs = map::new_str_hash::<str>();
+    let argdocsfound = none;
+    for item: @ast::meta_item in items {
+        alt item.node {
+            ast::meta_name_value("brief", {node: ast::lit_str(value),
+                                           span: _}) {
+                brief = some(value);
+            }
+            ast::meta_name_value("desc", {node: ast::lit_str(value),
+                                              span: _}) {
+                desc = some(value);
+            }
+            ast::meta_name_value("return", {node: ast::lit_str(value),
+                                            span: _}) {
+                return = some(value);
+            }
+            ast::meta_list("args", args) {
+                argdocsfound = some(args);
+            }
+            _ { }
+        }
+    }
+
+    alt argdocsfound {
+        none. { }
+        some(ds) {
+            for d: @ast::meta_item in ds {
+                alt d.node {
+                    ast::meta_name_value(key, {node: ast::lit_str(value),
+                                               span: _}) {
+                        argdocs.insert(key, value);
+                    }
+                }
+            }
+        }
+    }
+
+    let _brief = alt brief {
+        some(_b) { _b }
+        none. { "_undocumented_" }
+    };
+
+    ~{
+        name: "todo",
+        brief: _brief,
+        desc: desc,
+        return: return,
+        args: argdocs }
+}
--- src/rustdoc/rustdoc.rs
+++ src/rustdoc/rustdoc.rs
@@ -23,72 +23,6 @@ type rustdoc = {
 };
 
 #[doc(
-  brief = "Parses function docs from a complex #[doc] attribute.",
-  desc = "Supported attributes:
-
-* `brief`: Brief description
-* `desc`: Long description
-* `return`: Description of return value
-* `args`: List of argname = argdesc pairs
-",
-  args(items = "Doc attribute contents"),
-  return = "Parsed function docs."
-)]
-fn parse_compound_fndoc(items: [@ast::meta_item]) -> doc::fndoc {
-    let brief = none;
-    let desc = none;
-    let return = none;
-    let argdocs = map::new_str_hash::<str>();
-    let argdocsfound = none;
-    for item: @ast::meta_item in items {
-        alt item.node {
-            ast::meta_name_value("brief", {node: ast::lit_str(value),
-                                           span: _}) {
-                brief = some(value);
-            }
-            ast::meta_name_value("desc", {node: ast::lit_str(value),
-                                              span: _}) {
-                desc = some(value);
-            }
-            ast::meta_name_value("return", {node: ast::lit_str(value),
-                                            span: _}) {
-                return = some(value);
-            }
-            ast::meta_list("args", args) {
-                argdocsfound = some(args);
-            }
-            _ { }
-        }
-    }
-
-    alt argdocsfound {
-        none. { }
-        some(ds) {
-            for d: @ast::meta_item in ds {
-                alt d.node {
-                    ast::meta_name_value(key, {node: ast::lit_str(value),
-                                               span: _}) {
-                        argdocs.insert(key, value);
-                    }
-                }
-            }
-        }
-    }
-
-    let _brief = alt brief {
-        some(_b) { _b }
-        none. { "_undocumented_" }
-    };
-
-    ~{
-        name: "todo",
-        brief: _brief,
-        desc: desc,
-        return: return,
-        args: argdocs }
-}
-
-#[doc(
   brief = "Documents a single crate item.",
   args(rd = "Rustdoc context",
        item = "AST item to document")
@@ -109,7 +43,8 @@ fn doc_item(rd: rustdoc, item: @ast::item) {
                 });
             }
             ast::meta_list("doc", docs) {
-                _fndoc = some(parse_compound_fndoc(docs));
+                _fndoc = some(
+                    attr_parser::parse_compound_fndoc(docs));
             }
         }
     }
