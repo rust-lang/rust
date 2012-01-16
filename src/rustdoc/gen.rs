d928952191bbdaec8f8dcf6ce9803d9f11aa4f5b
Brian Anderson <banderson@mozilla.com>
2012-01-16 00:33:36 -0800

    rustdoc: Write crate name

--- src/rustdoc/gen.rs
+++ src/rustdoc/gen.rs
@@ -3,6 +3,19 @@ type ctxt = {
     w: io::writer
 };
 
+fn write_markdown(
+    doc: doc::cratedoc,
+    _crate: @ast::crate,
+    writer: io::writer
+) {
+    let ctxt = {
+        ps: pprust::rust_printer(writer),
+        w: writer
+    };
+
+    write_header(ctxt, doc.topmod.name);
+}
+
 #[doc(
   brief = "Generate a crate document header.",
   args(rd = "Rustdoc context",
@@ -44,5 +57,27 @@ fn write_fndoc(ctxt: ctxt, ident: str, doc: doc::fndoc, decl: ast::fn_decl) {
     alt doc.return {
         some(_r) { ctxt.w.write_line(_r); }
         none. { }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    fn write_markdown_str(
+        doc: doc::cratedoc,
+        crate: @ast::crate
+    ) -> str {
+        let buffer = io::mk_mem_buffer();
+        let writer = io::mem_buffer_writer(buffer);
+        write_markdown(doc, crate, writer);
+        ret io::mem_buffer_str(buffer);
+    }
+
+    #[test]
+    fn write_markdown_should_write_crate_header() {
+        let source = "";
+        let ast = parse::from_str(source);
+        let doc = extract::extract(ast, "belch");
+        let markdown = write_markdown_str(doc, ast);
+        assert str::contains(markdown, "# Crate belch\n");
     }
 }
