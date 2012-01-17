ab86642e35220cd1469b9eb288ab084625d77c5d
Brian Anderson <banderson@mozilla.com>
2012-01-16 21:56:18 -0800

    rustdoc: Don't use by copy in fst/snd to work around segv
    Issue #1546

--- src/rustdoc/util.rs
+++ src/rustdoc/util.rs
@@ -1,10 +1,10 @@
-
-fn fst<T, U>(+pair: (T, U)) -> T {
+// FIXME #1546: Would rather write fst<T, U>(+pair: (T, U)) -> T
+fn fst<T:copy, U:copy>(pair: (T, U)) -> T {
     let (t, _) = pair;
     ret t;
 }
 
-fn snd<T, U>(+pair: (T, U)) -> U {
+fn snd<T:copy, U:copy>(pair: (T, U)) -> U {
     let (_, u) = pair;
     ret u;
 }
