2aabccc3ba7d4cf4da622761b266da1d6836ec3d
Brian Anderson <banderson@mozilla.com>
2012-01-20 19:57:45 -0800

    doc: Remove extract-tests.js and run-tests.sh
    The tutorial tests are run as part of the test suite now.

--- doc/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f fragments/*.rs
-mkdir -p fragments
-node extract-tests.js $1
-for F in `ls fragments/*.rs`; do
-  $RUSTC $F > /dev/null
-  if [[ $? != 0 ]] ; then echo $F; fi
-done
