4168101b039d2e0dfea5f066ea13047ff9ed4ad9
Brian Anderson <banderson@mozilla.com>
2012-01-25 12:44:13 -0800

    test: Un-xfail run-pass/tag-auto-disr-val-shape

--- src/test/run-pass/tag-auto-disr-val-shape.rs
+++ src/test/run-pass/tag-auto-disr-val-shape.rs
@@ -1,10 +1,9 @@
-// xfail-test
-
 enum color { red, green, blue, black, white, }
 
 fn main() {
-    assert (uint::to_str(red as uint, 10) == #fmt["%?", red]);
-    assert (uint::to_str(green as uint, 10) == #fmt["%?", green]);
-    assert (uint::to_str(white as uint, 10) == #fmt["%?", white]);
+    // Ideally we would print the name of the variant, not the number
+    assert (uint::to_str(red as uint, 10u) == #fmt["%?", red]);
+    assert (uint::to_str(green as uint, 10u) == #fmt["%?", green]);
+    assert (uint::to_str(white as uint, 10u) == #fmt["%?", white]);
 }
 
