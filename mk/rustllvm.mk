ee19e4344196847350a89630554ef1e321972f62
Jyun-Yan You <jyyou@cs.nctu.edu.tw>
2012-01-26 16:51:15 +0800

    Revert "let clang build llvm, fix rustllvm linking error"
    This reverts commit 8524a5258f61e5b2765c9ab2185500a9186b92ea.

--- mk/rustllvm.mk
+++ mk/rustllvm.mk
@@ -37,7 +37,7 @@ rustllvm/$(1)/$(CFG_RUSTLLVM): $$(RUSTLLVM_OBJS_OBJS_$(1)) \
 
 rustllvm/$(1)/%.o: rustllvm/%.cpp $$(MKFILE_DEPS) $$(LLVM_CONFIG_$(1))
 	@$$(call E, compile: $$@)
-	$$(Q)$$(call CFG_COMPILE_C_$(1), $$@, $$(subst -I, -iquote , $$(LLVM_CXXFLAGS_$(1))) $$(RUSTLLVM_INCS_$(1))) $$<
+	$$(Q)$$(call CFG_COMPILE_C_$(1), $$@, $$(LLVM_CXXFLAGS_$(1)) $$(RUSTLLVM_INCS_$(1))) $$<
 endef
 
 # Instantiate template for all stages
