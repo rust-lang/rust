768e7889c03323f9d777cf19bfd0b4f55cd70120
Paul Woolcock <pwoolcoc+github@gmail.com>
2012-01-29 19:43:11 -0500

    Alter/remove tests that include/concern ternary
    3 tests, pretty/block-disambig.rs, run-pass/operator-overloading.rs,
    and run-pass/weird-exprs.rs, all included the ternary operator.  These
    were changed to use the if-then-else construct instead.
    
    2 tests, run-pass/block-arg-in-ternary.rs and run-pass/ternary.rs, were
    only there because of the ternary operator, and were removed.

--- src/test/run-pass/block-arg-in-ternary.rs
+++
@@ -1,6 +0,0 @@
-// Allow block arguments with ternary... why not, no chance of ambig.
-fn main() {
-    let v = [-1f, 1f];
-    let foo = vec::any(v) { |e| float::is_negative(e) } ? true : false;
-    assert foo;
-}
