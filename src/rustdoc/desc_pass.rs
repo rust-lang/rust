f4aba18ae7490385a17adf6ad4d2240856bd895b
Brian Anderson <banderson@mozilla.com>
2012-01-30 19:36:58 -0800

    rustdoc: Apply generic string ops to ifaces

--- src/rustdoc/desc_pass.rs
+++ src/rustdoc/desc_pass.rs
@@ -20,7 +20,8 @@ fn run(
         fold_const: fold_const,
         fold_fn: fold_fn,
         fold_enum: fold_enum,
-        fold_res: fold_res
+        fold_res: fold_res,
+        fold_iface: fold_iface
         with *fold::default_seq_fold(op)
     });
     fold.fold_crate(fold, doc)
@@ -99,6 +100,32 @@ fn fold_res(fold: fold::fold<op>, doc: doc::resdoc) -> doc::resdoc {
     }
 }
 
+fn fold_iface(fold: fold::fold<op>, doc: doc::ifacedoc) -> doc::ifacedoc {
+    {
+        brief: maybe_apply_op(fold.ctxt, doc.brief),
+        desc: maybe_apply_op(fold.ctxt, doc.desc),
+        methods: vec::map(doc.methods) {|doc|
+            {
+                brief: maybe_apply_op(fold.ctxt, doc.brief),
+                desc: maybe_apply_op(fold.ctxt, doc.desc),
+                args: vec::map(doc.args) {|doc|
+                    {
+                        desc: maybe_apply_op(fold.ctxt, doc.desc)
+                        with doc
+                    }
+                },
+                return: {
+                    desc: maybe_apply_op(fold.ctxt, doc.return.desc)
+                    with doc.return
+                },
+                failure: maybe_apply_op(fold.ctxt, doc.failure)
+                with doc
+            }
+        }
+        with doc
+    }
+}
+
 #[test]
 fn should_execute_op_on_enum_brief() {
     let source = "#[doc(brief = \" a \")] enum a { b }";
@@ -157,4 +184,74 @@ fn should_execute_op_on_resource_args() {
     let doc = attr_pass::mk_pass()(srv, doc);
     let doc = mk_pass(str::trim)(srv, doc);
     assert doc.topmod.resources()[0].args[0].desc == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_brief() {
+    let source = "#[doc(brief = \" a \")] iface i { fn a(); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].brief == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_desc() {
+    let source = "#[doc(desc = \" a \")] iface i { fn a(); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].desc == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_method_brief() {
+    let source = "iface i { #[doc(brief = \" a \")] fn a(); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].methods[0].brief == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_method_desc() {
+    let source = "iface i { #[doc(desc = \" a \")] fn a(); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].methods[0].desc == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_method_args() {
+    let source = "iface i { #[doc(args(a = \" a \"))] fn a(a: bool); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].methods[0].args[0].desc == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_method_return() {
+    let source = "iface i { #[doc(return = \" a \")] fn a() -> int; }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].methods[0].return.desc == some("a");
+}
+
+#[test]
+fn should_execute_op_on_iface_method_failure_condition() {
+    let source = "iface i { #[doc(failure = \" a \")] fn a(); }";
+    let srv = astsrv::mk_srv_from_str(source);
+    let doc = extract::from_srv(srv, "");
+    let doc = attr_pass::mk_pass()(srv, doc);
+    let doc = mk_pass(str::trim)(srv, doc);
+    assert doc.topmod.ifaces()[0].methods[0].failure == some("a");
 }
