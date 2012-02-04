a608613ff51f5112659582c44399313d5e294728
Kevin Cantu <me@kevincantu.org>
2012-02-04 03:16:59 -0800

    Making vec::reserve reserve precisely the size given (untested)

--- src/rt/rust_builtin.cpp
+++ src/rt/rust_builtin.cpp
@@ -101,7 +101,7 @@ extern "C" CDECL void
 vec_reserve_shared(type_desc* ty, rust_vec** vp,
                    size_t n_elts) {
     rust_task *task = rust_scheduler::get_task();
-    reserve_vec(task, vp, n_elts * ty->size);
+    reserve_vec_exact(task, vp, n_elts * ty->size);
 }
 
 /**
