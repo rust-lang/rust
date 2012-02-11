13d743093b55dae44772ebdf21194058a33d3bb6
Niko Matsakis <niko@alum.mit.edu>
2012-02-11 10:14:28 -0800

    rewrite to use ctypes types

--- src/test/run-pass/native2.rs
+++ src/test/run-pass/native2.rs
@@ -16,7 +16,7 @@ native mod zed { }
 #[abi = "cdecl"]
 #[nolink]
 native mod libc {
-    fn write(fd: int, buf: *u8, count: uint) -> int;
+    fn write(fd: int, buf: *u8, count: ctypes::size_t) -> ctypes::ssize_t;
 }
 
 #[abi = "cdecl"]
