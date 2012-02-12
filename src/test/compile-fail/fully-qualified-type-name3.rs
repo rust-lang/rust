a7641e99e8e6830e4242cfe441a867eb6e955694
Niko Matsakis <niko@alum.mit.edu>
2012-02-11 17:14:29 -0800

    remove leading ::, new tests

---
+++ src/test/compile-fail/fully-qualified-type-name3.rs
@@ -0,0 +1,12 @@
+// Test that we use fully-qualified type names in error messages.
+
+type T1 = uint;
+type T2 = int;
+
+fn bar(x: T1) -> T2 {
+    ret x;
+    //!^ ERROR mismatched types: expected `T2` but found `T1`
+}
+
+fn main() {
+}
