0451b4822545325915db241cf5c83641846b0220
Brian Anderson <banderson@mozilla.com>
2012-02-14 00:43:45 -0800

    rt: Don't kill tasks while they are in a callback from C

---
+++ src/test/run-fail/crust-fail.rs
@@ -0,0 +1,31 @@
+// error-pattern:explicit failure
+// Testing that runtime failure doesn't cause callbacks to abort abnormally.
+// Instead the failure will be delivered after the callbacks return.
+
+native mod rustrt {
+    fn rust_dbg_call(cb: *u8,
+                     data: ctypes::uintptr_t) -> ctypes::uintptr_t;
+}
+
+crust fn cb(data: ctypes::uintptr_t) -> ctypes::uintptr_t {
+    if data == 1u {
+        data
+    } else {
+        count(data - 1u) + count(data - 1u)
+    }
+}
+
+fn count(n: uint) -> uint {
+    task::yield();
+    rustrt::rust_dbg_call(cb, n)
+}
+
+fn main() {
+    iter::repeat(10u) {||
+        task::spawn {||
+            let result = count(5u);
+            #debug("result = %?", result);
+            fail;
+        };
+    }
+}
