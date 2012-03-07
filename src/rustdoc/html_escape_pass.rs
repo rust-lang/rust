e536e8a93354cd2b4e5ecc87eb0fdf30b835859c
Brian Anderson <banderson@mozilla.com>
2012-03-06 20:16:53 -0800

    rustdoc: Don't escape characters between backticks

--- src/rustdoc/html_escape_pass.rs
+++ src/rustdoc/html_escape_pass.rs
@@ -7,6 +7,20 @@ fn mk_pass() -> pass {
 }
 
 fn escape(s: str) -> str {
+    let parts = str::split_char(s, '`');
+    let i = 0;
+    let parts = vec::map(parts) {|part|
+        i += 1;
+        if i % 2 != 0 {
+            escape_(part)
+        } else {
+            part
+        }
+    };
+    ret str::connect(parts, "`");
+}
+
+fn escape_(s: str) -> str {
     let s = str::replace(s, "&", "&amp;");
     let s = str::replace(s, "<", "&lt;");
     let s = str::replace(s, ">", "&gt;");
@@ -21,4 +35,10 @@ fn test() {
     assert escape("&") == "&amp;";
     assert escape("\"") == "&quot;";
     assert escape("<>&\"") == "&lt;&gt;&amp;&quot;";
+}
+
+#[test]
+fn should_not_escape_characters_in_backticks() {
+    // Markdown will quote things in backticks itself
+    assert escape("<`<`<`<`<") == "&lt;`<`&lt;`<`&lt;";
 }
