0972571cddcbbaa5fbf9e17dcc2732e820e5cb46
Patrick Walton <pcwalton@mimiga.net>
2012-03-15 18:13:12 -0700

    test: Add a test case for self regions and typeclass implementations

---
+++ src/test/run-pass/regions-self-impls.rs
@@ -0,0 +1,11 @@
+type clam = { chowder: &int };
+
+impl clam for clam {
+    fn get_chowder() -> &self.int { ret self.chowder; }
+}
+
+fn main() {
+    let clam = { chowder: &3 };
+    log(debug, *clam.get_chowder());
+}
+
