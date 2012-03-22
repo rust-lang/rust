140151f3f99fb7d8a4422af13e4368e3f3ede792
Brian Anderson <banderson@mozilla.com>
2012-03-22 15:35:22 -0700

    rustc: Move codemap to rustsyntax

---
+++ src/rustc/syntax.rs
@@ -0,0 +1,12 @@
+// Temporary wrapper modules for migrating syntax to its own crate
+import rustsyntax::codemap;
+export codemap;
+
+export ast;
+export ast_util;
+export visit;
+export fold;
+export parse;
+export ext;
+export print;
+export util;
