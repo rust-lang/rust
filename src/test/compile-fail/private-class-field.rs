2112c391cd13a67cfbf9064b17a51640add5053e
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-03-26 10:04:46 -0700

    Remove stray comment

--- src/test/compile-fail/private-class-field.rs
+++ src/test/compile-fail/private-class-field.rs
@@ -13,12 +13,3 @@ fn main() {
   let nyan : cat = cat(52u, 99);
   assert (nyan.meows == 52u);
 }
-/*
-  other tests:
-  not ok to refer to a private method outside a class
-  ok to refer to private method within a class
-  can't assign to a non-mutable var
-  can't assign to a method
-
-  all the same tests, cross-crate
- */
