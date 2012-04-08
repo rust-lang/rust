0094ffd99b09f2fc8c639df28b12e10211cff6ce
Brian Anderson <banderson@mozilla.com>
2012-04-07 17:28:09 -0700

    test: Add test for mismatched nominal types when using multiple crate vers

--- src/test/auxiliary/crateresolve5-1.rs
+++ src/test/auxiliary/crateresolve5-1.rs
@@ -7,4 +7,10 @@ fn structural() -> { name: str, val: int } {
     { name: "crateresolve5", val: 10 }
 }
 
+enum e {
+    e_val
+}
+
+fn nominal() -> e { e_val }
+
 fn f() -> int { 10 }
---
+++ src/test/compile-fail/crateresolve5.rs
@@ -0,0 +1,12 @@
+// xfail-fast
+// aux-build:crateresolve5-1.rs
+// aux-build:crateresolve5-2.rs
+// error-pattern:mismatched types
+
+use cr5_1 (name = "crateresolve5", vers = "0.1");
+use cr5_2 (name = "crateresolve5", vers = "0.2");
+
+fn main() {
+    // Nominal types from two multiple versions of a crate are different types
+    assert cr5_1::nominal() == cr5_2::nominal();
+}
