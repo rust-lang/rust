f34eae8802b327c23984bfef02312ce1c64a6581
Graydon Hoare <graydon@mozilla.com>
2012-04-10 18:34:21 -0700

    Translate slice-strings and make fixed-strings carry their null.

--- src/test/run-pass/estr-slice.rs
+++ src/test/run-pass/estr-slice.rs
@@ -1,8 +1,7 @@
-// xfail-test
 fn main() {
-    let x : str/& = "hello";
-    let mut y = "there";
+    let x = "hello"/&;
+    let mut y = "there"/&;
     y = x;
-    assert y[1] == 'h' as u8;
-    assert y[4] == 'e' as u8;
+    assert y[0] == 'h' as u8;
+    assert y[4] == 'o' as u8;
 }
