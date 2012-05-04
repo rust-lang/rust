95a2b5d42f1acb96b035f21f192b0fab6cb2af6a
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-05-04 13:42:13 -0700

    Add test case that checks that class members must be prefixed with "self"
    Closes #2289.

---
+++ src/test/compile-fail/class-missing-self.rs
@@ -0,0 +1,15 @@
+class cat {
+  priv {
+    let mut meows : uint;
+    fn sleep() { loop{} }
+    fn meow() {
+      #error("Meow");
+      meows += 1u; //! ERROR unresolved name
+      sleep();     //! ERROR unresolved name
+    }
+  }
+
+  new(in_x : uint) { self.meows = in_x; }
+}
+
+ fn main() { }
