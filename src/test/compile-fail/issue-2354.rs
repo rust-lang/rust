1460ea43d27373be9c5af5e344ab1a667be5e81f
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-05-07 14:42:24 -0700

    Add xfailed test case for #2354

---
+++ src/test/compile-fail/issue-2354.rs
@@ -0,0 +1,18 @@
+// xfail-test
+/*
+  Ideally, the error about the missing close brace in foo would be reported
+  near the corresponding open brace. But currently it's reported at the end.
+  xfailed for now (see Issue #2354)
+ */
+fn foo() { //! ERROR this open brace is not closed
+  alt some(x) {
+      some(y) { fail; }
+      none    { fail; }
+}
+
+fn bar() {
+    let mut i = 0;
+    while (i < 1000) {}
+}
+
+fn main() {}
