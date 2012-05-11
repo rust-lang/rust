a6b9fa0cd1a6eb29a127e05d10910d8b8f194f59
Niko Matsakis <niko@alum.mit.edu>
2012-05-11 14:42:31 -0700

    new test for loaning out an index

---
+++ src/test/compile-fail/borrowck-loan-vec-content.rs
@@ -0,0 +1,36 @@
+// compile-flags:--borrowck=err
+
+// Here we check that it is allowed to lend out an element of a
+// (locally rooted) mutable, unique vector, and that we then prevent
+// modifications to the contents.
+
+fn takes_imm_elt(_v: &int, f: fn()) {
+    f();
+}
+
+fn has_mut_vec_and_does_not_try_to_change_it() {
+    let v = [mut 1, 2, 3];
+    takes_imm_elt(&v[0]) {||
+    }
+}
+
+fn has_mut_vec_but_tries_to_change_it() {
+    let v = [mut 1, 2, 3];
+    takes_imm_elt(&v[0]) {|| //! NOTE loan of mutable vec content granted here
+        v[1] = 4; //! ERROR assigning to mutable vec content prohibited due to outstanding loan
+    }
+}
+
+fn takes_const_elt(_v: &const int, f: fn()) {
+    f();
+}
+
+fn has_mut_vec_and_tries_to_change_it() {
+    let v = [mut 1, 2, 3];
+    takes_const_elt(&const v[0]) {||
+        v[1] = 4;
+    }
+}
+
+fn main() {
+}
