efa2252cfcabe6aec34861d06fa10d0c174b8556
Niko Matsakis <niko@alum.mit.edu>
2012-05-14 08:23:11 -0700

    add a test that current fails b/c we do not preserve boxes

---
+++ src/test/run-pass/borrowck-preserve-box.rs
@@ -0,0 +1,22 @@
+// compile-flags:--borrowck=err
+// exec-env:RUST_POISON_ON_FREE=1
+
+fn borrow(x: &int, f: fn(x: &int)) {
+    let before = *x;
+    f(x);
+    let after = *x;
+    assert before == after;
+}
+
+fn main() {
+    let mut x = @3;
+    borrow(x) {|b_x|
+        assert *b_x == 3;
+        assert ptr::addr_of(*x) == ptr::addr_of(*b_x);
+        x = @22;
+
+        #debug["ptr::addr_of(*b_x) = %x", ptr::addr_of(*b_x) as uint];
+        assert *b_x == 3;
+        assert ptr::addr_of(*x) != ptr::addr_of(*b_x);
+    }
+}
