846dfbc922b5b8c2c7de8da0cbb912bb89ff046e
Brian Anderson <banderson@mozilla.com>
2012-05-21 18:35:00 -0700

    build: Use snapshot core

--- mk/target.mk
+++ mk/target.mk
@@ -9,7 +9,7 @@
 # the snapshot runtime (resp. corelib) rather than the runtime
 # (resp. corelib) from the working directory.
 USE_SNAPSHOT_RUNTIME=1
-USE_SNAPSHOT_CORELIB=0
+USE_SNAPSHOT_CORELIB=1
 
 define TARGET_STAGE_N
 
