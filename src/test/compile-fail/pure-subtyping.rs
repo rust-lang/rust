5b8abec92f6099fe0603d321a0fb5847bf283cbd
Niko Matsakis <niko@alum.mit.edu>
2012-05-30 11:56:50 -0700

    test for pure fn vs impure fn etc subtyping

---
+++ src/test/compile-fail/pure-subtyping.rs
@@ -0,0 +1,22 @@
+// Test rules governing higher-order pure fns.
+
+fn assign_to_pure(x: pure fn(), y: fn(), z: unsafe fn()) {
+    let a: pure fn() = x;
+    let b: pure fn() = y; //! ERROR expected pure fn but found impure fn
+    let c: pure fn() = z; //! ERROR expected pure fn but found unsafe fn
+}
+
+fn assign_to_impure(x: pure fn(), y: fn(), z: unsafe fn()) {
+    let h: fn() = x;
+    let i: fn() = y;
+    let j: fn() = z; //! ERROR expected impure fn but found unsafe fn
+}
+
+fn assign_to_unsafe(x: pure fn(), y: fn(), z: unsafe fn()) {
+    let m: unsafe fn() = x;
+    let n: unsafe fn() = y;
+    let o: unsafe fn() = z;
+}
+
+fn main() {
+}
