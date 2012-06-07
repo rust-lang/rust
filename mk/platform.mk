8f5f45bfe143b667a4d3bb41963ed130b13fdb7f
Jyun-Yan You <jyyou@cs.nctu.edu.tw>
2012-06-07 16:38:10 +0800

    fix compiletest deadlock on freebsd

--- mk/platform.mk
+++ mk/platform.mk
@@ -33,12 +33,6 @@ ifneq ($(findstring freebsd,$(CFG_OSTYPE)),)
   CFG_DEF_SUFFIX := .bsd.def
   CFG_INSTALL_NAME =
   CFG_PERF_TOOL := /usr/bin/time
-
-  # FIXME (1825): We're deadlocking on FreeBSD
-  ifndef RUST_THREADS
-    RUST_THREADS=1
-    export RUST_THREADS
-  endif
 endif
 
 ifneq ($(findstring linux,$(CFG_OSTYPE)),)
