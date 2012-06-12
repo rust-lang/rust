eadd74b5b69c36921963c2f1da7685bafcb723d8
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-06-12 16:41:11 -0700

    Test case for previous commit
    (basically a stripped-down version of comm, and a "driver" that
    constructs a new port)

---
+++ src/test/run-pass/leaky_comm.rs
@@ -0,0 +1,18 @@
+// aux-build:test_comm.rs
+
+use test_comm;
+
+fn main() {
+  let p = test_comm::port();
+  
+  alt none::<int> {
+      none {}
+      some(_) {
+  if test_comm::recv(p) == 0 {
+      #error("floop");
+  }
+  else {
+      #error("bloop");
+  }
+      }}
+}
