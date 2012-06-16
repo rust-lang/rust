5c9f414a85ae8f3448a27f56274afc861974cffe
Eric Holk <eric.holk@gmail.com>
2012-06-15 22:00:04 -0400

    id_visitor should handle expr_index and expr_assign_op as well.

--- src/libsyntax/ast_util.rs
+++ src/libsyntax/ast_util.rs
@@ -454,8 +454,9 @@ fn id_visitor(vfn: fn@(node_id)) -> visit::vt<()> {
 
         visit_expr: fn@(e: @expr) {
             vfn(e.id);
-            alt e.node {
-              expr_unary(*) | expr_binary(*) | expr_index(*) {
+            alt e.node { 
+              expr_index(*) | expr_assign_op(*) |
+              expr_unary(*) | expr_binary(*) {
                 vfn(ast_util::op_expr_callee_id(e));
               }
               _ { /* fallthrough */ }
