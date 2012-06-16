f0d8e978a94efe228e2ce9ca89bb4044be040c04
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-06-15 17:32:59 -0700

    Add test for #2633. Xfailed.

---
+++ src/test/run-pass/issue-2633.rs
@@ -0,0 +1,18 @@
+// Currently segfaults
+// xfail-test
+class cat {
+    let mut meow: fn@();
+    new() { self.meow = fn@() { #error("meow"); };}
+}
+
+type kitty_info = {kitty: cat};
+
+// Code compiles and runs successfully if we add a + before the first arg
+fn nyan(kitty: cat, _kitty_info: kitty_info) {
+    kitty.meow();
+}
+
+fn main() {
+    let mut kitty = cat();
+    nyan(kitty, {kitty: kitty});
+}
