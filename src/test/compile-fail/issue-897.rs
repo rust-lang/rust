8cc1149130dab62a70e2eea5070d85bff07b1a03
Lindsey Kuper <lindsey@rockstargirl.org>
2012-06-18 13:41:52 -0700

    Adding `i` suffixes so cfail tests keep failing after suffix inference

--- src/test/compile-fail/issue-897.rs
+++ src/test/compile-fail/issue-897.rs
@@ -1,5 +1,5 @@
 fn f() -> ! {
-    ret 42; //! ERROR expected `_|_` but found `int`
+    ret 42i; //! ERROR expected `_|_` but found `int`
     fail; //! WARNING unreachable statement
 }
 fn main() { }
