3246723bf79ffc20e91936440cebc71ddcfd0a66
Eric Holk <eric.holk@gmail.com>
2012-06-22 15:35:06 -0700

    Adding an issue number to vec-add.rs

--- src/test/compile-fail/vec-add.rs
+++ src/test/compile-fail/vec-add.rs
@@ -1,8 +1,8 @@
 // xfail-test
 
-// FIXME: + should allow immutable or mutable vectors on the right
-// hand side in all cases. We are getting compiler errors about this
-// now, so I'm xfailing the test for now. -eholk
+// FIXME (Issue #2711): + should allow immutable or mutable vectors on
+// the right hand side in all cases. We are getting compiler errors
+// about this now, so I'm xfailing the test for now. -eholk
 
 fn add(i: [int], m: [mut int], c: [const int]) {
 
