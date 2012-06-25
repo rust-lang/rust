fe9bbeca1a0945b7d465e6e85211368098d40c5d
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-06-25 13:16:47 -0700

    Add test cases for issue 2445

---
+++ src/test/run-pass/issue-2445-b.rs
@@ -0,0 +1,18 @@
+class c1<T: copy> {
+  let x: T;
+  new(x: T) {self.x = x;}
+
+    fn f1(x: int) {
+    }
+}
+
+impl i1<T: copy> for c1<T> {
+    fn f2(x: int) {
+    }
+}
+
+
+fn main() {
+    c1::<int>(3).f1(4);
+    c1::<int>(3).f2(4);
+}
