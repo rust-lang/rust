007b9d9acd8b04ad2e1984aeba79c2031dec0e45
Brian Anderson <banderson@mozilla.com>
2012-06-25 21:38:27 -0700

    Update combine-tests.py for new vec syntax

--- src/etc/combine-tests.py
+++ src/etc/combine-tests.py
@@ -30,6 +30,8 @@ for t in os.listdir(run_pass):
                 "xfail-fast" in s or
                 "xfail-win32" in s):
             stage2_tests.append(t)
+            if "main(args: [str]/~)" in s:
+                take_args[t] = True
             if "main(args: [str])" in s:
                 take_args[t] = True
         f.close()
