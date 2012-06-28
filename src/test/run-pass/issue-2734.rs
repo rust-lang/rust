b2b498183ad6326fec9c0c953c44b43668a74d1f
Ben Blum <bblum@andrew.cmu.edu>
2012-06-28 19:26:12 -0400

    Add test case for issue #2734 (xfailed)

---
+++ src/test/run-pass/issue-2734.rs
@@ -0,0 +1,15 @@
+// xfail-test
+iface hax { } 
+impl <A> of hax for A { } 
+
+fn perform_hax<T>(x: @T) -> hax {
+    x as hax 
+}
+
+fn deadcode() {
+    perform_hax(@"deadcode");
+}
+
+fn main() {
+    let _ = perform_hax(@42);
+}
