42f00ce2a54c325862e1c4d8140bd2a082ad2cd4
Armin Ronacher <armin.ronacher@active-4.com>
2012-07-03 01:32:05 +0100

    Added testcase for the missing do compile note

---
+++ src/test/compile-fail/missing-do.rs
@@ -0,0 +1,9 @@
+// Regression test for issue #2783
+
+fn foo(f: fn()) { f() }
+
+fn main() {
+    "" || 42; //! ERROR binary operation || cannot be applied to type `str`
+    foo || {}; //! ERROR binary operation || cannot be applied to type `extern fn(fn())`
+    //!^ NOTE did you forget the 'do' keyword for the call?
+}
