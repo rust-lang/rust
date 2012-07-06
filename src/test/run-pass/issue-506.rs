9b2d988df0ffefab9ade36763b7bf41b5862b105
Brian Anderson <banderson@mozilla.com>
2012-07-05 21:33:11 -0700

    Fix test that was using rust_task_allow_kill incorrectly

--- src/test/run-pass/issue-506.rs
+++ src/test/run-pass/issue-506.rs
@@ -1,5 +1,7 @@
 /*
   A reduced test case for Issue #506, provided by Rob Arnold.
+
+  Testing spawning foreign functions
 */
 
 use std;
@@ -7,7 +9,7 @@ import task;
 
 #[abi = "cdecl"]
 extern mod rustrt {
-    fn rust_task_allow_kill();
+    fn unsupervise();
 }
 
-fn main() { task::spawn(rustrt::rust_task_allow_kill); }
+fn main() { task::spawn(rustrt::unsupervise); }
