7b03832c958826b27ea77df91f2d2ac276bb7411
Eric Holk <eric.holk@gmail.com>
2012-07-05 23:14:27 -0700

    Updating tests to use pipes.

--- src/libsyntax/ext/pipes/parse_proto.rs
+++ src/libsyntax/ext/pipes/parse_proto.rs
@@ -10,11 +10,10 @@ impl proto_parser for parser {
     fn parse_proto(id: ident) -> protocol {
         let proto = protocol(id);
 
-        self.expect(token::LBRACE);
-
-        while self.token != token::RBRACE {
-            self.parse_state(proto);
-        }
+        self.parse_unspanned_seq(token::LBRACE,
+                                 token::RBRACE,
+                                 {sep: none, trailing_sep_allowed: false},
+                                 |self| self.parse_state(proto));
 
         ret proto;
     }
@@ -35,29 +34,44 @@ impl proto_parser for parser {
           _ { fail }
         };
 
-        let state = proto.add_state(id, dir);
-        // TODO: add typarams too.
-
-        self.expect(token::LBRACE);
-
-        while self.token != token::RBRACE {
-            let mname = self.parse_ident();
-
-            // TODO: parse data
-
-            self.expect(token::RARROW);
-
-            let next = self.parse_ident();
-            // TODO: parse next types
-
-            state.add_message(mname, ~[], next, ~[]);
-
-            alt copy self.token {
-              token::COMMA { self.bump() }
-              token::RBRACE { }
-              _ { fail }
-            }
+        let typarms = if self.token == token::LT {
+            self.parse_ty_params()
         }
-        self.bump();
+        else { ~[] };
+
+        let state = proto.add_state_poly(id, dir, typarms);
+
+        // parse the messages
+        self.parse_unspanned_seq(
+            token::LBRACE, token::RBRACE,
+            {sep: some(token::COMMA), trailing_sep_allowed: true},
+            |self| {
+                let mname = self.parse_ident();
+
+                let args = if self.token == token::LPAREN {
+                    self.parse_unspanned_seq(token::LPAREN,
+                                             token::RPAREN,
+                                             {sep: some(token::COMMA),
+                                              trailing_sep_allowed: true},
+                                             |p| p.parse_ty(false))
+                }
+                else { ~[] };
+
+                self.expect(token::RARROW);
+
+                let next = self.parse_ident();
+
+                let ntys = if self.token == token::LT {
+                    self.parse_unspanned_seq(token::LT,
+                                             token::GT,
+                                             {sep: some(token::COMMA),
+                                              trailing_sep_allowed: true},
+                                             |p| p.parse_ty(false))
+                }
+                else { ~[] };
+
+                state.add_message(mname, args, next, ntys);
+
+            });
     }
 }
--- src/test/run-pass/pipe-select.rs
+++ src/test/run-pass/pipe-select.rs
@@ -1,72 +1,22 @@
+// xfail-pretty
+
 use std;
 import std::timer::sleep;
 import std::uv;
 
 import pipes::{recv, select};
 
-// Compiled by pipec
-mod oneshot {
-    fn init() -> (client::waiting, server::waiting) { pipes::entangle() }
-    enum waiting { signal(server::signaled), }
-    enum signaled { }
-    mod client {
-        fn signal(-pipe: waiting) -> signaled {
-            let (c, s) = pipes::entangle();
-            let message = oneshot::signal(s);
-            pipes::send(pipe, message);
-            c
-        }
-        type waiting = pipes::send_packet<oneshot::waiting>;
-        type signaled = pipes::send_packet<oneshot::signaled>;
+proto! oneshot {
+    waiting:send {
+        signal -> signaled
     }
-    mod server {
-        impl recv for waiting {
-            fn recv() -> extern fn(-waiting) -> oneshot::waiting {
-                fn recv(-pipe: waiting) -> oneshot::waiting {
-                    option::unwrap(pipes::recv(pipe))
-                }
-                recv
-            }
-        }
-        type waiting = pipes::recv_packet<oneshot::waiting>;
-        impl recv for signaled {
-            fn recv() -> extern fn(-signaled) -> oneshot::signaled {
-                fn recv(-pipe: signaled) -> oneshot::signaled {
-                    option::unwrap(pipes::recv(pipe))
-                }
-                recv
-            }
-        }
-        type signaled = pipes::recv_packet<oneshot::signaled>;
-    }
+
+    signaled:send { }
 }
 
-mod stream {
-    fn init<T: send>() -> (client::stream<T>, server::stream<T>) {
-        pipes::entangle()
-    }
-    enum stream<T: send> { send(T, server::stream<T>), }
-    mod client {
-        fn send<T: send>(+pipe: stream<T>, +x_0: T) -> stream<T> {
-            {
-                let (c, s) = pipes::entangle();
-                let message = stream::send(x_0, s);
-                pipes::send(pipe, message);
-                c
-            }
-        }
-        type stream<T: send> = pipes::send_packet<stream::stream<T>>;
-    }
-    mod server {
-        impl recv<T: send> for stream<T> {
-            fn recv() -> extern fn(+stream<T>) -> stream::stream<T> {
-                fn recv<T: send>(+pipe: stream<T>) -> stream::stream<T> {
-                    option::unwrap(pipes::recv(pipe))
-                }
-                recv
-            }
-        }
-        type stream<T: send> = pipes::recv_packet<stream::stream<T>>;
+proto! stream {
+    stream:send<T:send> {
+        send(T) -> stream<T>
     }
 }
 
