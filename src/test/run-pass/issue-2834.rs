801e049617b25c36f048a20f59ec6f461397d265
Eric Holk <eric.holk@gmail.com>
2012-07-09 12:36:57 -0700

    Added a test case for issue #2834.

---
+++ src/test/run-pass/issue-2834.rs
@@ -0,0 +1,21 @@
+// Test case for issue #2843.
+//
+// xfail-test
+
+proto! streamp {
+    open:send<T: send> {
+        data(T) -> open<T>
+    }
+}
+
+fn rendezvous() {
+    let (c, s) = streamp::init();
+    let streams: ~[streamp::client::open<int>] = ~[c];
+
+    #error("%?", streams[0]);
+}
+
+fn main(args: ~[str]) {
+    //os::getenv("FOO");
+    rendezvous();
+}
