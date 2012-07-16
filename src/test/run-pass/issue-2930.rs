053bdcac2d1b6872fe54b6c928910a3eba8eca67
Eric Holk <eric.holk@gmail.com>
2012-07-16 15:12:53 -0700

    Test case for #2930

---
+++ src/test/run-pass/issue-2930.rs
@@ -0,0 +1,31 @@
+// xfail-pretty
+// xfail-win32
+// xfail-test
+
+// Generated from pipe compiler
+mod stream {
+    fn init<T: send>() -> (client::stream<T>, server::stream<T>) {
+        pipes::entangle()
+    }
+    enum stream<T: send> { send(T, server::stream<T>), }
+    mod client {
+        fn send<T: send>(+pipe: stream<T>, +x_0: T) -> stream<T> {
+            {
+                let (c, s) = pipes::entangle();
+                let message = stream::send(x_0, s);
+                pipes::send(pipe, message);
+                c
+            }
+        }
+        type stream<T: send> = pipes::send_packet<stream::stream<T>>;
+    }
+    mod server {
+        type stream<T: send> = pipes::recv_packet<stream::stream<T>>;
+    }
+}
+
+fn main() {
+    let (bc, _bp) = stream::init();
+
+    stream::client::send(bc, "abc");
+}
