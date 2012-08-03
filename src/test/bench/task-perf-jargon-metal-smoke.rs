138b78f785c7861d3b3ca6c31540a36d1fec5cc4
Ben Blum <bblum@andrew.cmu.edu>
2012-08-03 19:26:25 -0400

    Add task-perf-linked-failure.rs

--- src/test/bench/task-perf-jargon-metal-smoke.rs
+++ src/test/bench/task-perf-jargon-metal-smoke.rs
@@ -3,12 +3,12 @@
 // Each child task has to enlist as a descendant in each of its ancestor
 // groups, but that shouldn't have to happen for already-dead groups.
 //
-// The filename is a reference; google it in quotes.
+// The filename is a song reference; google it in quotes.
 
 fn child_generation(gens_left: uint) {
     // This used to be O(n^2) in the number of generations that ever existed.
     // With this code, only as many generations are alive at a time as tasks
-    // alive at a time, 
+    // alive at a time,
     do task::spawn_supervised {
         if gens_left & 1 == 1 {
             task::yield(); // shake things up a bit
