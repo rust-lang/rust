a52f6d26db810b947f5ff515ffae18a31e9479c0
Brian Anderson <banderson@mozilla.com>
2012-08-04 17:41:04 -0700

    Fix closure pretty-print tests

--- src/test/pretty/fn-types.rs
+++ src/test/pretty/fn-types.rs
@@ -1,7 +1,6 @@
 // pp-exact
 
 fn from_foreign_fn(x: extern fn()) { }
-fn from_closure(x: fn()) { }
 fn from_stack_closure(x: fn&()) { }
 fn from_box_closure(x: fn@()) { }
 fn from_unique_closure(x: fn~()) { }
