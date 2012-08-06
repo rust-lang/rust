2ed9b8b737b81737c4caec71efdfa8df69d1ee65
Eric Holk <eric.holk@gmail.com>
2012-08-06 14:34:53 -0700

    Use a proper protocol definition instead of the already-expanded code.

--- src/test/run-pass/issue-2930.rs
+++ src/test/run-pass/issue-2930.rs
@@ -1,22 +1,6 @@
-// Generated from pipe compiler
-mod stream {
-    fn init<T: send>() -> (client::stream<T>, server::stream<T>) {
-        pipes::entangle()
-    }
-    enum stream<T: send> { send(T, server::stream<T>), }
-    mod client {
-        fn send<T: send>(+pipe: stream<T>, +x_0: T) -> stream<T> {
-            {
-                let (c, s) = pipes::entangle();
-                let message = stream::send(x_0, s);
-                pipes::send(pipe, message);
-                c
-            }
-        }
-        type stream<T: send> = pipes::send_packet<stream::stream<T>>;
-    }
-    mod server {
-        type stream<T: send> = pipes::recv_packet<stream::stream<T>>;
+proto! stream {
+    stream:send<T:send> {
+        send(T) -> stream<T>
     }
 }
 
