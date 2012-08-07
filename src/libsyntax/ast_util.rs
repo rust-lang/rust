4a6226975602770829b7349c6fcf869f3d0b757c
Patrick Walton <pcwalton@mimiga.net>
2012-08-07 15:54:59 -0700

    libsyntax: Allow users of the visitor to visit struct defs

--- src/libsyntax/ast_util.rs
+++ src/libsyntax/ast_util.rs
@@ -565,6 +565,10 @@ fn id_visitor(vfn: fn@(node_id)) -> visit::vt<()> {
         visit_trait_method: fn@(_ty_m: trait_method) {
         },
 
+        visit_struct_def: fn@(_sd: struct_def, _id: ident, _tps: ~[ty_param],
+                              _id: node_id) {
+        },
+
         visit_class_item: fn@(c: @class_member) {
             match c.node {
               instance_var(_, _, _, id,_) => vfn(id),
