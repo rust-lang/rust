ffe74b7a3b7647019af2a0962a3dcd8db72a4952
Brian Anderson <banderson@mozilla.com>
2012-08-14 15:13:47 -0700

    test: Use a mod that exists in view-item-at-top

--- src/test/compile-fail/view-items-at-top.rs
+++ src/test/compile-fail/view-items-at-top.rs
@@ -3,7 +3,7 @@ use std;
 fn f() {
 }
 
-import std::io::println;    //~ ERROR view items must be declared at the top
+import std::net;    //~ ERROR view items must be declared at the top
 
 fn main() {
 }
