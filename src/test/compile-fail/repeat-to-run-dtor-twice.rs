1ed94a56744c0ed4056c22da297d84222f4327f2
Patrick Walton <pcwalton@mimiga.net>
2012-08-17 16:12:07 -0700

    rustc: Prevent destructors from being run twice with the repeated vector syntax

---
+++ src/test/compile-fail/repeat-to-run-dtor-twice.rs
@@ -0,0 +1,16 @@
+// Tests that one can't run a destructor twice with the repeated vector
+// literal syntax.
+
+struct Foo {
+    x: int;
+
+    drop {
+        io::println("Goodbye!");
+    }
+}
+
+fn main() {
+    let a = Foo { x: 3 };
+    let b = [ a, ..5 ];     //~ ERROR copying a noncopyable value
+}
+
