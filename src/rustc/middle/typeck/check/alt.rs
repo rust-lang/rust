36addc20b735dcbb582437c78d5f3523367cf4c7
Ben Blum <bblum@andrew.cmu.edu>
2012-08-20 18:25:05 -0400

    remove fixme in check::alt

--- src/rustc/middle/typeck/check/alt.rs
+++ src/rustc/middle/typeck/check/alt.rs
@@ -235,8 +235,6 @@ fn check_pat(pcx: pat_ctxt, pat: @ast::pat, expected: ty::t) {
       ast::pat_struct(path, fields, etc) => {
         // Grab the class data that we care about.
         let class_fields, class_id, substitutions;
-        // FIXME(#3217) If this were "match structure_of...", this bug causes
-        // it to not live long enough for 'substitutions'.
         let structure = structure_of(fcx, pat.span, expected);
         match structure {
             ty::ty_class(cid, ref substs) => {
