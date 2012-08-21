f5332769d58c5e8d9fee36a7e1f8faa4f5c1b861
Ben Blum <bblum@andrew.cmu.edu>
2012-08-21 15:05:57 -0400

    Convert atomic intrinsics away from old argument modes (partial #3200)

--- src/rustc/middle/trans/type_use.rs
+++ src/rustc/middle/trans/type_use.rs
@@ -64,7 +64,7 @@ fn type_uses_for(ccx: @crate_ctxt, fn_id: def_id, n_tps: uint)
         none    => ccx.sess.bug(fmt!{"type_uses_for: unbound item ID %?",
                                      fn_id_loc})
     };
-    match check map_node {
+    match map_node {
       ast_map::node_item(@{node: item_fn(_, _, body), _}, _) |
       ast_map::node_method(@{body, _}, _, _) => {
         handle_body(cx, body);
@@ -81,7 +81,7 @@ fn type_uses_for(ccx: @crate_ctxt, fn_id: def_id, n_tps: uint)
       ast_map::node_foreign_item(i@@{node: foreign_item_fn(_, _), _},
                                  abi, _) => {
         if abi == foreign_abi_rust_intrinsic {
-            let flags = match check *i.ident {
+            let flags = match *i.ident {
               ~"size_of" |  ~"pref_align_of" | ~"min_align_of" |
               ~"init" |  ~"reinterpret_cast" |
               ~"move_val" | ~"move_val_init" => {
@@ -90,6 +90,11 @@ fn type_uses_for(ccx: @crate_ctxt, fn_id: def_id, n_tps: uint)
               ~"get_tydesc" | ~"needs_drop" => {
                 use_tydesc
               }
+              // NB: new intrinsics
+              ~"atomic_xchg"     | ~"atomic_xadd"     | ~"atomic_xsub" |
+              ~"atomic_xchg_acq" | ~"atomic_xadd_acq" | ~"atomic_xsub_acq" |
+              ~"atomic_xchg_rel" | ~"atomic_xadd_rel" | ~"atomic_xsub_rel" |
+              // old intrinsics
               ~"atomic_xchng" | ~"atomic_add" | ~"atomic_sub" |
               ~"atomic_xchng_acq" | ~"atomic_add_acq" | ~"atomic_sub_acq" |
               ~"atomic_xchng_rel" | ~"atomic_add_rel" | ~"atomic_sub_rel" => {
@@ -98,6 +103,7 @@ fn type_uses_for(ccx: @crate_ctxt, fn_id: def_id, n_tps: uint)
               ~"visit_tydesc" | ~"forget" | ~"addr_of" => {
                 0u
               }
+              _ => fail ~"unknown intrinsic in type_use"
             };
             for uint::range(0u, n_tps) |n| { cx.uses[n] |= flags;}
         }
@@ -108,7 +114,7 @@ fn type_uses_for(ccx: @crate_ctxt, fn_id: def_id, n_tps: uint)
       ast_map::node_dtor(_, dtor, _, _) => {
         handle_body(cx, dtor.node.body);
       }
-
+      _ => fail ~"unknown node type in type_use"
     }
     let uses = vec::from_mut(copy cx.uses);
     ccx.type_use_cache.insert(fn_id, uses);
