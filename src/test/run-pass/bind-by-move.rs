ae513bdb4cd12e500ecc7eb6c259a244bfaba1bd
Ben Blum <bblum@andrew.cmu.edu>
2012-08-22 20:37:59 -0400

    WIP: Add an xfailed test for bind-by-move (#2329)

---
+++ src/test/run-pass/bind-by-move.rs
@@ -0,0 +1,13 @@
+// xfail-test
+use std;
+import std::arc;
+fn dispose(+_x: arc::arc<bool>) unsafe { }
+
+fn main() {
+    let p = arc::arc(true);
+    let x = some(p);
+    match move x {
+        some(move z) => { dispose(z); },
+        none => fail
+    }
+}
