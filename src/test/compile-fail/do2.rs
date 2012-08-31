3462bb6a462253333758b12a8cb1ee72981329cb
Graydon Hoare <graydon@mozilla.com>
2012-08-30 18:57:58 -0700

    Fix some more test breakage.

--- src/test/compile-fail/do2.rs
+++ src/test/compile-fail/do2.rs
@@ -7,5 +7,7 @@
 fn f(f: fn@(int) -> bool) -> bool { f(10i) }
 
 fn main() {
-    assert do f() |i| { i == 10i } == 10i; //~ ERROR: expected `bool` but found `int`
+    assert do f() |i| { i == 10i } == 10i;
+    //~^ ERROR: expected `bool` but found `int`
+    //~^^ ERROR: expected `bool` but found `int`
 }
