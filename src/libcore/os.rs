a544b83788cad14f61e9d39262356a9d9bd5bdf0
Brian Anderson <banderson@mozilla.com>
2012-09-13 14:33:15 -0700

    core: Fix stray usage of vec::unsafe on FreeBSD

--- src/libcore/os.rs
+++ src/libcore/os.rs
@@ -394,7 +394,7 @@ fn self_exe_path() -> Option<Path> {
                 let mib = ~[CTL_KERN as c_int,
                            KERN_PROC as c_int,
                            KERN_PROC_PATHNAME as c_int, -1 as c_int];
-                sysctl(vec::unsafe::to_ptr(mib), vec::len(mib) as c_uint,
+                sysctl(vec::raw::to_ptr(mib), vec::len(mib) as c_uint,
                        buf as *mut c_void, ptr::mut_addr_of(sz),
                        ptr::null(), 0u as size_t) == (0 as c_int)
             }
