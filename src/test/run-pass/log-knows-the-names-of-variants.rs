e0845eb589c69ebc5f4c572cc7d43b1d9bbc4b9c
Niko Matsakis <niko@alum.mit.edu>
2012-09-13 11:10:05 -0700

    repr: emit closing `)` for enum variants, test that nullary variants names print

--- src/test/run-pass/log-knows-the-names-of-variants.rs
+++ src/test/run-pass/log-knows-the-names-of-variants.rs
@@ -4,8 +4,13 @@ enum foo {
   c,
 }
 
+enum bar {
+  d, e, f
+}
+
 fn main() {
     assert ~"a(22)" == fmt!("%?", a(22u));
     assert ~"b(~\"hi\")" == fmt!("%?", b(~"hi"));
     assert ~"c" == fmt!("%?", c);
+    assert ~"d" == fmt!("%?", d);
 }
