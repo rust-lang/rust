ea5e3d21ff5ef8b4d30b588d0cd65859ffa3bab1
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-09-18 22:33:49 -0700

    Make moves explicit in doc examples
    Had to remove the buffalo example. It was awkward to update for
    explicit moves.

---
+++ src/test/run-pass/issue-3668.rs
@@ -0,0 +1,13 @@
+struct P { child: Option<@mut P> }
+trait PTrait {
+   fn getChildOption() -> Option<@P>;
+}
+
+impl P: PTrait {
+   fn getChildOption() -> Option<@P> {
+       const childVal: @P = self.child.get();
+       fail;
+   }
+}
+
+fn main() {}
