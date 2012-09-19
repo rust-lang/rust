68e9ae116028330a261f91a334188c20371d09c4
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-09-18 22:55:21 -0700

    Make moves explicit in rfail tests

--- src/test/run-fail/unwind-box-res.rs
+++ src/test/run-fail/unwind-box-res.rs
@@ -20,7 +20,7 @@ fn r(v: *int) -> r {
 fn main() unsafe {
     let i1 = ~0;
     let i1p = cast::reinterpret_cast(&i1);
-    cast::forget(i1);
+    cast::forget(move i1);
     let x = @r(i1p);
     failfn();
     log(error, x);
