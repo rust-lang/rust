3e9284d12873da6c7c4c6057f07856580422221c
Brian Anderson <banderson@mozilla.com>
2012-09-20 15:37:43 -0700

    core: Demode sys

--- src/test/run-pass/fixed_length_vec_glue.rs
+++ src/test/run-pass/fixed_length_vec_glue.rs
@@ -1,6 +1,6 @@
 fn main() {
     let arr = [1,2,3]/3;
     let struc = {a: 13u8, b: arr, c: 42};
-    let s = sys::log_str(struc);
+    let s = sys::log_str(&struc);
     assert(s == ~"{ a: 13, b: [ 1, 2, 3 ], c: 42 }");
 }
