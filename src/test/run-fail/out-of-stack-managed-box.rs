b10e575217101af680ab5729382724189f1d5a62
Brian Anderson <banderson@mozilla.com>
2012-09-21 21:48:33 -0700

    Add tests for out-of-stack box leak #2555

---
+++ src/test/run-fail/out-of-stack-managed-box.rs
@@ -0,0 +1,13 @@
+// NB: Not sure why this works. I expect the box argument to leak when
+// we run out of stack. Maybe the box annihilator works it out?
+
+// error-pattern:ran out of stack
+fn main() {
+    eat(move @0);
+}
+
+fn eat(
+    +a: @int
+) {
+    eat(move a)
+}
