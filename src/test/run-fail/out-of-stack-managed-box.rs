92752a462a055d6478bd96dab37a740514992106
Brian Anderson <banderson@mozilla.com>
2012-09-23 17:12:59 -0700

    xfail-test run-fail/out-of-stack-managed-box

--- src/test/run-fail/out-of-stack-managed-box.rs
+++ src/test/run-fail/out-of-stack-managed-box.rs
@@ -1,3 +1,5 @@
+// xfail-test iloops with optimizations on
+
 // NB: Not sure why this works. I expect the box argument to leak when
 // we run out of stack. Maybe the box annihilator works it out?
 
