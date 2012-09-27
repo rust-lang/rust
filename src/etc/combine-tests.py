25715bd59eb339fd37f979a425651802702b4618
Graydon Hoare <graydon@mozilla.com>
2012-09-26 17:20:33 -0700

    Fix check-fast for the change to main's signature.

--- src/etc/combine-tests.py
+++ src/etc/combine-tests.py
@@ -30,7 +30,7 @@ for t in os.listdir(run_pass):
                 "xfail-fast" in s or
                 "xfail-win32" in s):
             stage2_tests.append(t)
-            if "main(args: ~[~str])" in s:
+            if "fn main(args:" in s or "fn main(++args:" in s:
                 take_args[t] = True
         f.close()
 
