22495650dbdeb195bf43b6ad22e17b5002b4cf18
Graydon Hoare <graydon@mozilla.com>
2012-10-01 14:08:34 -0700

    De-export option and option_iter. Part of #3583.

--- src/libcore/iter-trait/option.rs
+++ src/libcore/iter-trait/option.rs
@@ -1,14 +1,14 @@
 #[allow(non_camel_case_types)]
-type IMPL_T<A> = Option<A>;
+pub type IMPL_T<A> = Option<A>;
 
-pure fn EACH<A>(self: &IMPL_T<A>, f: fn(v: &A) -> bool) {
+pub pure fn EACH<A>(self: &IMPL_T<A>, f: fn(v: &A) -> bool) {
     match *self {
       None => (),
       Some(ref a) => { f(a); }
     }
 }
 
-pure fn SIZE_HINT<A>(self: &IMPL_T<A>) -> Option<uint> {
+pub pure fn SIZE_HINT<A>(self: &IMPL_T<A>) -> Option<uint> {
     match *self {
       None => Some(0),
       Some(_) => Some(1)
