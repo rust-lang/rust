738cd00f1d3b40a4856785e5a96b2ca4b0cc4e7f
Brian Anderson <banderson@mozilla.com>
2012-10-04 14:09:42 -0700

    rt: Remove CHECK_CLAIMS

--- src/rt/rust_env.h
+++ src/rt/rust_env.h
@@ -9,7 +9,6 @@ struct rust_env {
     size_t min_stack_size;
     size_t max_stack_size;
     char* logspec;
-    bool check_claims;
     bool detailed_leaks;
     char* rust_seed;
     bool poison_on_free;
