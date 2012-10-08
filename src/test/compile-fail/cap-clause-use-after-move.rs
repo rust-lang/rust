bbda995bfe03680b6d621c6ab809ddb145125c3a
Niko Matsakis <niko@alum.mit.edu>
2012-10-08 08:38:03 -0700

    cap-clause-use-after-move: modernize error msg

--- src/test/compile-fail/cap-clause-use-after-move.rs
+++ src/test/compile-fail/cap-clause-use-after-move.rs
@@ -1,7 +1,5 @@
-// error-pattern:Unsatisfied precondition constraint
-
 fn main() {
     let x = 5;
     let _y = fn~(move x) { };
-    let _z = x; //< error: Unsatisfied precondition constraint
+    let _z = x; //~ ERROR use of moved variable: `x`
 }
