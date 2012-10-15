23bec6c65747e44397ec4a9eb040e8df62c5e5e4
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-10-15 14:17:16 -0700

    Un-xfail test for #3521; it works now
    Closes #3521

--- src/test/compile-fail/issue-3521.rs
+++ src/test/compile-fail/issue-3521.rs
@@ -1,9 +1,8 @@
-// xfail-test
 fn main() {
     let foo = 100;
 
     enum Stuff {
-        Bar = foo
+        Bar = foo //~ ERROR attempt to use a non-constant value in a constant
     }
 
     log(error, Bar);
