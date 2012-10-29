a369a7881f2196e359fb9d4cb20cf5d2bb0da6f8
Patrick Walton <pcwalton@mimiga.net>
2012-10-29 14:11:56 -0700

    rustc: Implement typechecking for automatically-derived enums

---
+++ src/test/compile-fail/enum-deriving-incomplete.rs
@@ -0,0 +1,24 @@
+trait MyEq {
+    pure fn eq(other: &self) -> bool;
+}
+
+struct A {
+    x: int
+}
+
+enum B {
+    C(A),
+    D(A),
+    E(A)
+}
+
+impl B : MyEq;
+//~^ ERROR cannot automatically derive
+//~^^ ERROR cannot automatically derive
+//~^^^ ERROR cannot automatically derive
+
+fn main() {
+    let c = C(A { x: 15 });
+    assert c.eq(&c);
+}
+
