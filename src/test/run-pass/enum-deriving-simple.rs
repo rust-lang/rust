2443e118b53b1173d8775a1abf219baad8b057b7
Patrick Walton <pcwalton@mimiga.net>
2012-10-29 16:21:15 -0700

    rustc: Translate "deriving" for monomorphic intra-crate enums. r=brson

---
+++ src/test/run-pass/enum-deriving-simple.rs
@@ -0,0 +1,32 @@
+trait MyEq {
+    pure fn eq(other: &self) -> bool;
+}
+
+struct A {
+    x: int
+}
+
+enum B {
+    C(A),
+    D(A),
+    E(A)
+}
+
+impl A : MyEq {
+    pure fn eq(other: &A) -> bool {
+        unsafe { io::println("in eq"); }
+        self.x == other.x
+    }
+}
+
+impl B : MyEq;
+
+fn main() {
+    let c = C(A { x: 15 });
+    let d = D(A { x: 30 });
+    let e = C(A { x: 30 });
+    assert c.eq(&c);
+    assert !c.eq(&d);
+    assert !c.eq(&e);
+}
+
