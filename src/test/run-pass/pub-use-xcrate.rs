e2f33e6883fcce83d33aa5375e5ea018759b245a
Brian Anderson <banderson@mozilla.com>
2012-11-02 15:57:47 -0700

    Add xfailed test for #3908

---
+++ src/test/run-pass/pub-use-xcrate.rs
@@ -0,0 +1,14 @@
+// xfail-test Issue #3908
+// aux-build:pub_use_xcrate1.rs
+// aux-build:pub_use_xcrate2.rs
+
+extern mod pub_use_xcrate2;
+
+use pub_use_xcrate2::Foo;
+
+fn main() {
+    let foo: Foo = Foo {
+        name: 0
+    };
+}
+
