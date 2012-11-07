d20899057fabc9c43b798b298ce3559b8549ad84
Brian Anderson <banderson@mozilla.com>
2012-11-07 15:03:12 -0800

    Move a few test defs to core
    This will be needed to turn core's unit tests into self tests

---
+++ src/libcore/private/test.rs
@@ -0,0 +1,22 @@
+/*! Test definitions. The actual test runner is in std */
+
+// The name of a test. By convention this follows the rules for rust
+// paths; i.e. it should be a series of identifiers seperated by double
+// colons. This way if some test runner wants to arrange the tests
+// hierarchically it may.
+pub type TestName = ~str;
+
+// A function that runs a test. If the function returns successfully,
+// the test succeeds; if the function fails then the test fails. We
+// may need to come up with a more clever definition of test in order
+// to support isolation of tests into tasks.
+pub type TestFn = fn~();
+
+// The definition of a single test. A test runner will run a list of
+// these.
+pub type TestDesc = {
+    name: TestName,
+    testfn: TestFn,
+    ignore: bool,
+    should_fail: bool
+};
--- src/libstd/test.rs
+++ src/libstd/test.rs
@@ -21,26 +21,9 @@ extern mod rustrt {
     fn rust_sched_threads() -> libc::size_t;
 }
 
-// The name of a test. By convention this follows the rules for rust
-// paths; i.e. it should be a series of identifiers seperated by double
-// colons. This way if some test runner wants to arrange the tests
-// hierarchically it may.
-pub type TestName = ~str;
-
-// A function that runs a test. If the function returns successfully,
-// the test succeeds; if the function fails then the test fails. We
-// may need to come up with a more clever definition of test in order
-// to support isolation of tests into tasks.
-pub type TestFn = fn~();
-
-// The definition of a single test. A test runner will run a list of
-// these.
-pub type TestDesc = {
-    name: TestName,
-    testfn: TestFn,
-    ignore: bool,
-    should_fail: bool
-};
+pub type TestName = core::private::test::TestName;
+pub type TestFn = core::private::test::TestFn;
+pub type TestDesc = core::private::test::TestDesc;
 
 // The default console test runner. It accepts the command line
 // arguments and a vector of test_descs (generated at compile time).
