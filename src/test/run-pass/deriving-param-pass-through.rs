29e10c91fe1d2b854b4db7a4cb3c12e3ea8dbddc
Patrick Walton <pcwalton@mimiga.net>
2012-11-14 13:07:32 -0800

    librustc: Pass through extra parameters in automatically-derived methods. r=brson

---
+++ src/test/run-pass/deriving-param-pass-through.rs
@@ -0,0 +1,31 @@
+trait Trait {
+    #[derivable]
+    fn f(x: int, y: &str);
+}
+
+impl int : Trait {
+    fn f(x: int, y: &str) {
+        assert x == 42;
+        assert y == "hello";
+    }
+}
+
+impl float : Trait {
+    fn f(x: int, y: &str) {
+        assert x == 42;
+        assert y == "hello";
+    }
+}
+
+struct Foo {
+    x: int,
+    y: float
+}
+
+impl Foo : Trait;
+
+fn main() {
+    let a: Foo = Foo { x: 1, y: 2.0 };
+    a.f(42, "hello");
+}
+
