9d6ab9ae2326ef130a1a43876f49f00933d234e6
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-11-15 18:59:46 -0800

    Update error message and un-xfail test

--- src/test/compile-fail/regions-var-type-out-of-scope.rs
+++ src/test/compile-fail/regions-var-type-out-of-scope.rs
@@ -1,12 +1,11 @@
-// xfail-test
-
 fn foo(cond: bool) {
     // Here we will infer a type that uses the
     // region of the if stmt then block:
-    let mut x; //~ ERROR foo
+    let mut x;
 
     if cond {
-        x = &3;
+        x = &3; //~ ERROR illegal borrow: borrowed value does not live long enough
+        assert (*x == 3);
     }
 }
 
