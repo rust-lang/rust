29b72dad8d3ce1a2108720226159cb427e6a27fb
Kevin Cantu <me@kevincantu.org>
2012-11-20 23:33:31 -0800

    Add improvements to insert_with_key
    This commit adds a lower-level implementation of the generic
    `insert_with_key` which I expect to be faster. Now insert could be
    defined with insert_with_key, too, although I'm not sure we want to do that.
    
    This also clarifies the tests a bit and adds an `insert_with` function.

--- src/test/bench/shootout-k-nucleotide-pipes.rs
+++ src/test/bench/shootout-k-nucleotide-pipes.rs
@@ -69,7 +69,7 @@ fn find(mm: HashMap<~[u8], uint>, key: ~str) -> uint {
 // given a map, increment the counter for a key
 fn update_freq(mm: HashMap<~[u8], uint>, key: &[u8]) {
     let key = vec::slice(key, 0, key.len());
-    mm.insert_with_key(|k,v,v1| {v + v1}, key, 1);
+    mm.insert_with(key, 1, |v,v1| { v+v1 });
 }
 
 // given a ~[u8], for each window call a function
