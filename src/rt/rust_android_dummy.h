4fb4be8f174c9d19c8ec6702070ef55987f73cb3
ILyoan <ilyoan@gmail.com>
2012-11-29 19:36:03 +0900

    More android support Conflicts: 	Makefile.in 	cmakeFiles/rustllvm.cmake

--- src/rt/rust_android_dummy.h
+++ src/rt/rust_android_dummy.h
@@ -2,4 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "execinfo.h"
+#ifndef _RUST_ANDROID_DUMMY_H
+#define _RUST_ANDROID_DUMMY_H
+
+int backtrace (void **__array, int __size);
+
+char **backtrace_symbols (void *__const *__array, int __size);
+
+void backtrace_symbols_fd (void *__const *__array, int __size, int __fd);
+
+#endif
+
