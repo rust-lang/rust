54ae377ec26ed47bbb627bdcb58bb10658cf03c4
Patrick Walton <pcwalton@mimiga.net>
2012-11-29 10:59:49 -0800

    librustc: Implement explicit @self and ~self for objects. r=nmatsakis

---
+++ src/test/run-pass/explicit-self-objects-box.rs
@@ -0,0 +1,24 @@
+trait Foo {
+    fn f(@self);
+}
+
+struct S {
+    x: int
+}
+
+impl S : Foo {
+    fn f(@self) {
+        assert self.x == 3;
+    }
+}
+
+fn main() {
+    let x = @S { x: 3 };
+    let y = x as @Foo;
+    y.f();
+    y.f();
+    y.f();
+    y.f();
+}
+
+
