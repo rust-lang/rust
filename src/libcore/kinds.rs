350895da399e4ef26d952ada0b2ed1fdc74bb3dd
Brian Anderson <banderson@mozilla.com>
2012-11-30 01:31:56 -0800

    core: Add docs about kind traits

--- src/libcore/kinds.rs
+++ src/libcore/kinds.rs
@@ -1,9 +1,35 @@
-//! The kind traits
+/*!
+The kind traits
 
-#[lang="const"]
-pub trait Const {
-    // Empty.
-}
+Rust types can be classified in vairous useful ways according to
+intrinsic properties of the type. These classifications, often called
+'kinds', are represented as traits.
+
+They cannot be implemented by user code, but are instead implemented
+by the compiler automatically for the types to which they apply.
+
+The 4 kinds are
+
+* Copy - types that may be copied without allocation. This includes
+  scalar types and managed pointers, and exludes owned pointers. It
+  also excludes types that implement `Drop`.
+
+* Send - owned types and types containing owned types.  These types
+  may be transferred across task boundaries.
+
+* Const - types that are deeply immutable. Const types are used for
+  freezable data structures.
+
+* Owned - types that do not contain borrowed pointers. Note that this
+  meaning of 'owned' conflicts with 'owned pointers'. The two notions
+  of ownership are different.
+
+`Copy` types include both implicitly copyable types that the compiler
+will copy automatically and non-implicitly copyable types that require
+the `copy` keyword to copy. Types that do not implement `Copy` may
+instead implement `Clone`.
+
+*/
 
 #[lang="copy"]
 pub trait Copy {
@@ -12,6 +38,11 @@ pub trait Copy {
 
 #[lang="send"]
 pub trait Send {
+    // Empty.
+}
+
+#[lang="const"]
+pub trait Const {
     // Empty.
 }
 
