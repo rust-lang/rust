ca963251d0f459a7236c8acac74e688b73cdebd8
Jakub Wieczorek <jakubw@jakubw.net>
2012-12-03 23:57:06 +0000

    Add one more test for determining unreachability

--- src/test/compile-fail/alt-vec-unreachable.rs
+++ src/test/compile-fail/alt-vec-unreachable.rs
@@ -1,7 +1,14 @@
 fn main() {
+    let x: ~[(int, int)] = ~[];
+    match x {
+        [(1, 2), (2, 3), _] => (),
+        [a, _, (3, 4)] => (), //~ ERROR unreachable pattern
+        _ => ()
+    }
+
     match [~"foo", ~"bar", ~"baz"] {
         [a, _, _, .._] => { io::println(a); }
-        [~"foo", ~"bar"] => { } //~ ERROR unreachable pattern
+        [~"foo", ~"bar", ~"baz", ~"foo", ~"bar"] => { } //~ ERROR unreachable pattern
         _ => { }
     }
 
