e8309cdb42ad184facc4304e7a4cb8fe6bffd510
Graydon Hoare <graydon@mozilla.com>
2012-12-03 16:48:01 -0800

    Update license, add license boilerplate to most files. Remainder will follow.

--- src/librustc/middle/typeck/check/alt.rs
+++ src/librustc/middle/typeck/check/alt.rs
@@ -1,3 +1,13 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use syntax::print::pprust;
 use syntax::ast_util::{walk_pat};
 use pat_util::{pat_is_binding, pat_is_const, pat_is_variant_or_struct};
