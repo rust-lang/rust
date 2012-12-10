a2889a94a5ce8bd252a4b62c1a9ecfd8a39391b2
Graydon Hoare <graydon@mozilla.com>
2012-12-10 15:44:02 -0800

    Add license boilerplate to more files.

--- src/rustllvm/RustWrapper.cpp
+++ src/rustllvm/RustWrapper.cpp
@@ -1,10 +1,13 @@
-//===- RustWrapper.cpp - Rust wrapper for core functions --------*- C++ -*-===
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //===----------------------------------------------------------------------===
 //
 // This file defines alternate interfaces to core functions that are more
