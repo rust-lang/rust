e9e27fa96d760704ce31bc444d3ff3d05a6c6990
Patrick Walton <pcwalton@mimiga.net>
2012-12-10 12:37:50 -0800

    librustc: Fix bug preventing cross-crate struct destructuring from working. rs=bugfix

---
+++ src/test/run-pass/struct-destructuring-cross-crate.rs
@@ -0,0 +1,9 @@
+// xfail-fast
+extern mod struct_destructuring_cross_crate;
+
+fn main() {
+    let x = struct_destructuring_cross_crate::S { x: 1, y: 2 };
+    let struct_destructuring_cross_crate::S { x: a, y: b } = x;
+    assert a == 1;
+    assert b == 2;
+}
