12c32e944d3dbadb6a114a6e0bc51b065b4f42c1
Graydon Hoare <graydon@mozilla.com>
2012-12-10 15:44:02 -0800

    Add license boilerplate to more files.

--- src/rt/rust_env.cpp
+++ src/rt/rust_env.cpp
@@ -1,3 +1,13 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // The runtime wants to pull a number of variables out of the
 // environment but calling getenv is not threadsafe, so every value
 // that might come from the environment is loaded here, once, during
