6439f2d54645915ce57f4c229811719e8e498151
Tim Chevalier <chevalier@alum.wellesley.edu>
2012-12-11 13:10:30 -0800

    Avoid extra error for type mismatches in patterns
    When a type error has already occurred, don't call ty::subst,
    which may ICE due to the mismatch in the number of type params
    involved.
    
    I'm deeming this too small to review.
    
    Closes #3680

--- src/test/compile-fail/issue-3680.rs
+++ src/test/compile-fail/issue-3680.rs
@@ -8,8 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-test
-fn f() {
+fn main() {
     match None {
             Err(_) => () //~ ERROR expected `core::result
     }
