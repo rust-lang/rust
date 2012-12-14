8979695496eb6247758a1a5f0a4f3cdf62b79453
Brian Anderson <banderson@mozilla.com>
2012-12-14 15:10:24 -0800

    Fix broken tests

--- src/test/run-fail/unwind-misc-1.rs
+++ src/test/run-fail/unwind-misc-1.rs
@@ -20,7 +20,7 @@ fn main() {
     let mut arr = ~[];
     for uint::range(0u, 10u) |i| {
         arr += ~[@~"key stuff"];
-        map.insert(arr, arr + ~[@~"value stuff"]);
+        map.insert(copy arr, arr + ~[@~"value stuff"]);
         if arr.len() == 5 {
             fail;
         }
