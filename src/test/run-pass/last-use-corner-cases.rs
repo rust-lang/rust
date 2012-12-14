bbf648dbe247ac71b50b6d9d3ef1ed172152ff68
Patrick Walton <pcwalton@mimiga.net>
2012-12-13 18:08:52 -0800

    test: Fix last-use-corner-cases. rs=bustage

--- src/test/run-pass/last-use-corner-cases.rs
+++ src/test/run-pass/last-use-corner-cases.rs
@@ -20,13 +20,13 @@ fn main() {
 
     // But only when it really is the last use
     let q = ~20;
-    let f = fn@() -> *int { ptr::addr_of(&(*q)) };
+    let f = fn@(copy q) -> *int { ptr::addr_of(&(*q)) };
     assert ptr::addr_of(&(*q)) != f();
 
     // Ensure function arguments and box arguments interact sanely.
     fn call_me(x: fn() -> int, y: ~int) { assert x() == *y; }
     let q = ~30;
-    call_me({|| *q}, q);
+    call_me({|copy q| *q}, q);
 
     // Check that no false positives are found in loops.
     let mut q = ~40, p = 10;
