959aee994119ca3403332e3e79fdda445b341fdb
Jakub Wieczorek <jakubw@jakubw.net>
2012-12-15 23:13:55 +0000

    Add more tests

---
+++ src/test/compile-fail/alt-vec-illegal-tail-element-loan.rs
@@ -0,0 +1,13 @@
+fn a() -> &int {
+    let vec = [1, 2, 3, 4];
+    let tail = match vec {
+        [a, ..tail] => &tail[0], //~ ERROR illegal borrow
+        _ => fail ~"foo"
+    };
+    move tail
+}
+
+fn main() {
+    let fifth = a();
+    io::println(fmt!("%d", *fifth));
+}
