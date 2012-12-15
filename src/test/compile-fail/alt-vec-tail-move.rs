6c83fe4c24b7c0e6c6ff6916c8b4a9d9973e46a9
Jakub Wieczorek <jakubw@jakubw.net>
2012-12-15 23:13:55 +0000

    Add more tests

---
+++ src/test/compile-fail/alt-vec-tail-move.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a = [mut 1, 2, 3, 4];
+    let _ = match a {
+        [1, 2, ..move tail] => tail,
+        _ => core::util::unreachable()
+    };
+    a[0] = 0; //~ ERROR: use of moved variable
+}
---
+++ src/test/run-pass/vec-matching-legal-tail-element-borrow.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = &[1, 2, 3, 4, 5];
+    if !x.is_empty() {
+        let el = match x {
+            [1, ..ref tail] => &tail[0], 
+            _ => core::util::unreachable()
+        };
+        io::println(fmt!("%d", *el));
+    }
+}
