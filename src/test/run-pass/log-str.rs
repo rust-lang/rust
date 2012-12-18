44af3614adf8b1f06474d87e1cae7e82275bc762
Graydon Hoare <graydon@mozilla.com>
2012-12-18 11:12:35 -0800

    fix 4 repr tests due to differences in spacing. r=burningtree.

--- src/test/run-pass/log-str.rs
+++ src/test/run-pass/log-str.rs
@@ -10,8 +10,8 @@
 
 fn main() {
     let act = sys::log_str(&~[1, 2, 3]);
-    assert ~"~[ 1, 2, 3 ]" == act;
+    assert ~"~[1, 2, 3]" == act;
 
     let act = fmt!("%?/%6?", ~[1, 2, 3], ~"hi");
-    assert act == ~"~[ 1, 2, 3 ]/ ~\"hi\"";
+    assert act == ~"~[1, 2, 3]/ ~\"hi\"";
 }
