658420aa43bf2898e4cf11d6a4ce076a5fc44877
ILyoan <ilyoan@gmail.com>
2012-12-20 19:19:41 +0900

    formatting

--- src/librustc/back/link.rs
+++ src/librustc/back/link.rs
@@ -639,10 +639,10 @@ fn output_dll_filename(os: session::os, lm: &link_meta) -> ~str {
         session::os_macos =>
             macos::dll_prefix() + libname + macos::dll_suffix(),
         session::os_freebsd =>
-            freebsd::dll_prefix() + libname + macos::dll_suffix(),
+            freebsd::dll_prefix() + libname + freebsd::dll_suffix(),
         session::os_linux =>
-            freebsd::dll_prefix() + libname + macos::dll_suffix(),
-        session::os_win32 => 
+            linux::dll_prefix() + libname + linux::dll_suffix(),
+        session::os_win32 =>
             win32::dll_prefix() + libname + win32::dll_suffix(),
     }
 }
@@ -665,8 +665,7 @@ fn link_binary(sess: Session,
 
     let output = if sess.building_library {
         let long_libname = output_dll_filename(sess.targ_cfg.os, &lm);
-        
-		debug!("link_meta.name:  %s", lm.name);
+        debug!("link_meta.name:  %s", lm.name);
         debug!("long_libname: %s", long_libname);
         debug!("out_filename: %s", out_filename.to_str());
         debug!("dirname(out_filename): %s", out_filename.dir_path().to_str());
