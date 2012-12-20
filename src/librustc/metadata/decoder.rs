61570b648d4eb350dc3bb5416e1f23dac6e01e82
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2012-12-20 12:52:53 -0800

    Remove the cfg(stageN)-specific serialize code.

--- src/librustc/metadata/decoder.rs
+++ src/librustc/metadata/decoder.rs
@@ -25,10 +25,6 @@ use reader = std::ebml::reader;
 use std::ebml;
 use std::map::HashMap;
 use std::map;
-#[cfg(stage0)]
-use std::serialize::traits::Decodable;
-#[cfg(stage1)]
-#[cfg(stage2)]
 use std::serialize::Decodable;
 use syntax::ast_map;
 use syntax::attr;
