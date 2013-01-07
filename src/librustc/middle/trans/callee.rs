fe360c1cea5f8d8e94a30a54e73f11166f23bc77
Niko Matsakis <niko@alum.mit.edu>
2013-01-07 10:16:30 -0800

    For `for` loop bodies, translate the type of the closure based on the expr_loop_body expression and not the inner closure

--- src/librustc/middle/trans/callee.rs
+++ src/librustc/middle/trans/callee.rs
@@ -638,8 +638,8 @@ fn trans_arg_expr(bcx: block,
                     let arg_ty = expr_ty(bcx, arg_expr);
                     let proto = ty::ty_fn_proto(arg_ty);
                     let bcx = closure::trans_expr_fn(
-                        bcx, proto, decl, /*bad*/copy *body, blk.id, cap,
-                        Some(ret_flag), expr::SaveIn(scratch));
+                        bcx, proto, decl, /*bad*/copy *body, arg_expr.id,
+                        blk.id, cap, Some(ret_flag), expr::SaveIn(scratch));
                     DatumBlock {bcx: bcx,
                                 datum: Datum {val: scratch,
                                               ty: scratch_ty,
