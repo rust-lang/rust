1153c69719c4ec46e86b12427d021a36b8ffa093
Graydon Hoare <graydon@mozilla.com>
2013-01-09 08:04:18 -0800

    core: fix libc linux fallout from 2db3abddcd67

--- src/libcore/libc.rs
+++ src/libcore/libc.rs
@@ -313,9 +313,9 @@ pub mod types {
             }
             pub mod posix01 {
                 use libc::types::os::arch::c95::{c_int, c_long, time_t};
-                use libc::consts::os::arch::posix88::{dev_t, gid_t, ino_t};
-                use libc::consts::os::arch::posix98::{mode_t, off_t};
-                use libc::consts::os::arch::posix98::{uid_t};
+                use libc::types::os::arch::posix88::{dev_t, gid_t, ino_t};
+                use libc::types::os::arch::posix88::{mode_t, off_t};
+                use libc::types::os::arch::posix88::{uid_t};
 
                 pub type nlink_t = u64;
                 pub type blksize_t = i64;
