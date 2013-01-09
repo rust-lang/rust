rust-lang/rust

commit 2db3abddcd6784dc5529081c6d831c972abbe755

librustc: Make unqualified identifier searches terminate at the nearest module scope. r=tjc

src/compiletest/procsrv.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use core::prelude::*;
+
 use io;
 use io::{ReaderUtil, WriterUtil};
 use libc;

commit 896dcd4eb38bd3f224e046dacc634b1e13f49e96

compiletest: more win32 fallout from 2db3abd

src/compiletest/procsrv.rs
@@ -20,6 +20,7 @@ use run::spawn_process;
 use pipes;
 use str;
 use task;
+use vec;
 
 export run;
 