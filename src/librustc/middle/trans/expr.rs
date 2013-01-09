rust-lang/rust

commit 2db3abddcd6784dc5529081c6d831c972abbe755

librustc: Make unqualified identifier searches terminate at the nearest module scope. r=tjc

src/librustc/middle/trans/expr.rs
@@ -111,6 +111,7 @@ lvalues are *never* stored by value.
 
 */
 
+use core::prelude::*;
 
 use lib::llvm::ValueRef;
 use middle::resolve;

commit 06b2205cbe26fe412ed3a2c2f82cbd4bcdb4195a

rustc: more fallout from 2db3abd

src/librustc/middle/trans/expr.rs
@@ -133,6 +133,8 @@ use util::common::indenter;
 use util::ppaux::ty_to_str;
 
 use syntax::print::pprust::{expr_to_str};
+use syntax::ast;
+use syntax::ast::spanned;
 
 // The primary two functions for translating expressions:
 export trans_to_datum, trans_into;