f8283ca734784861c04376fb453c382aef2f54c1
Tim Chevalier <chevalier@alum.wellesley.edu>
2013-01-10 13:44:27 -0800

    tests: Add two test cases, one xfailed

---
+++ src/test/run-pass/issue-3847.rs
@@ -0,0 +1,22 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+mod buildings {
+    pub struct Tower { height: uint }
+}
+
+fn main() {
+    let sears = buildings::Tower { height: 1451 };
+    let h: uint = match sears {
+        buildings::Tower { height: h } => { h }
+    };
+
+    io::println(h.to_str());
+}
