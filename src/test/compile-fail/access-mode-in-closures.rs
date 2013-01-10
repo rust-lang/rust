89ed595e306a7f391b561ee8a5a1e7aca24bf534
Niko Matsakis <niko@alum.mit.edu>
2013-01-10 13:57:38 -0800

    correct expected error msgs in various tests rs=breakage

--- src/test/compile-fail/access-mode-in-closures.rs
+++ src/test/compile-fail/access-mode-in-closures.rs
@@ -1,9 +1,9 @@
 enum sty = ~[int];
 
-fn unpack(unpack: &fn(v: &sty) -> ~[int]) {}
+fn unpack(_unpack: &fn(v: &sty) -> ~[int]) {}
 
 fn main() {
-    let foo = unpack(|s| {
+    let _foo = unpack(|s| {
         // Test that `s` is moved here.
         match *s { sty(v) => v } //~ ERROR moving out of dereference of immutable & pointer
     });
