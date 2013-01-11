5336dfbc14f3e10dd84f888be98cafe140896301
John Clements <clements@racket-lang.org>
2013-01-11 12:47:14 -0800

    s/moved variable/moved value/

--- src/test/compile-fail/no-capture-arc.rs
+++ src/test/compile-fail/no-capture-arc.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern: use of moved variable
+// error-pattern: use of moved value
 
 extern mod std;
 use std::arc;
