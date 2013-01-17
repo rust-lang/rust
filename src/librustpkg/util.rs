1f083490421c4425b8cfc4465e24f9dbc7c7256c
Zack Corr <zack@z0w0.me>
2013-01-17 19:05:19 +1000

    rustpkg: Finish parsing package scripts and finish boilerplate

--- src/librustpkg/util.rs
+++ src/librustpkg/util.rs
@@ -1,7 +1,7 @@
 use core::*;
 use rustc::metadata::filesearch;
 use semver::Version;
-use std::net::url;
+use std::term;
 
 pub fn root() -> Path {
     match filesearch::get_rustpkg_root() {
@@ -38,6 +38,46 @@ pub fn parse_vers(vers: ~str) -> Version {
         Some(vers) => vers,
         None => fail ~"could not parse version: invalid"
     }
+}
+
+pub fn need_dir(s: &Path) {
+    if !os::path_is_dir(s) && !os::make_dir(s, 493_i32) {
+        fail fmt!("can't create dir: %s", s.to_str());
+    }
+}
+
+pub fn info(msg: ~str) {
+    let out = io::stdout();
+
+    if term::color_supported() {
+        term::fg(out, term::color_green);
+        out.write_str(~"info: ");
+        term::reset(out);
+        out.write_line(msg);
+    } else { out.write_line(~"info: " + msg); }
+}
+
+pub fn warn(msg: ~str) {
+    let out = io::stdout();
+
+    if term::color_supported() {
+        term::fg(out, term::color_yellow);
+        out.write_str(~"warning: ");
+        term::reset(out);
+        out.write_line(msg);
+    }else { out.write_line(~"warning: " + msg); }
+}
+
+pub fn error(msg: ~str) {
+    let out = io::stdout();
+
+    if term::color_supported() {
+        term::fg(out, term::color_red);
+        out.write_str(~"error: ");
+        term::reset(out);
+        out.write_line(msg);
+    }
+    else { out.write_line(~"error: " + msg); }
 }
 
 #[test]
