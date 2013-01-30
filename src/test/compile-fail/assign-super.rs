097c8aefea1feca5c3f5ff93025e9477cc6fbb0e
Ben Striegel <ben.striegel@gmail.com>
2013-01-29 21:04:59 -0500

    RIMOV, round 2
    Running the same command, again:
    
    find ./ -type f -name "*.rs" -exec sed -i "s/let mut\(.*\)\[mut[ ]\?/let
    mut\1\[/g" {} \;

--- src/test/compile-fail/assign-super.rs
+++ src/test/compile-fail/assign-super.rs
@@ -9,7 +9,7 @@
 // except according to those terms.
 
 fn main() {
-    let mut x: ~[mut int] = ~[3];
+    let mut x: ~[int] = ~[3];
     let y: ~[int] = ~[3];
     x = y; //~ ERROR values differ in mutability
 }
