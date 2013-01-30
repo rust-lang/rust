a8ff91a630880fabc047d3d3d9383da2a9d59378
Ben Striegel <ben.striegel@gmail.com>
2013-01-29 21:02:53 -0500

    Removing interior mut on vectors, round 1
    find ./ -type f -name "*.rs" -exec sed -i "s/let mut\(.*\)\[mut[ ]\?/let
    mut\1\[/g" {} \;

--- src/test/compile-fail/assign-super.rs
+++ src/test/compile-fail/assign-super.rs
@@ -9,7 +9,7 @@
 // except according to those terms.
 
 fn main() {
-    let mut x: ~[mut int] = ~[mut 3];
+    let mut x: ~[mut int] = ~[3];
     let y: ~[int] = ~[3];
     x = y; //~ ERROR values differ in mutability
 }
