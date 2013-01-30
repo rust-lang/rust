ee8e2056269fd9c659317dd45b0c284aa7c7295b
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2013-01-30 10:04:11 -0800

    core: export from_str::FromStr from prelude.rs

--- src/libcore/prelude.rs
+++ src/libcore/prelude.rs
@@ -26,6 +26,7 @@ pub use result::{Result, Ok, Err};
 pub use clone::Clone;
 pub use cmp::{Eq, Ord};
 pub use container::{Container, Mutable, Map, Set};
+pub use from_str::FromStr;
 pub use hash::Hash;
 pub use iter::{BaseIter, ExtendedIter, EqIter, CopyableIter};
 pub use iter::{CopyableOrderedIter, CopyableNonstrictIter, Times};
