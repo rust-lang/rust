040035cd08f9c250d523c9832b25275153d11c3c
Graydon Hoare <graydon@mozilla.com>
2013-01-30 17:07:35 -0800

    test: fix compile-fail tests somehow missed from local 'make check', r=burningtree.

--- src/test/compile-fail/trait-test-2.rs
+++ src/test/compile-fail/trait-test-2.rs
@@ -13,7 +13,7 @@ impl int: bar { fn dup() -> int { self } fn blah<X>() {} }
 impl uint: bar { fn dup() -> uint { self } fn blah<X>() {} }
 
 fn main() {
-    10.dup::<int>(); //~ ERROR does not take type parameters
-    10.blah::<int, int>(); //~ ERROR incorrect number of type parameters
+    10i.dup::<int>(); //~ ERROR does not take type parameters
+    10i.blah::<int, int>(); //~ ERROR incorrect number of type parameters
     (10 as bar).dup(); //~ ERROR contains a self-type
 }
