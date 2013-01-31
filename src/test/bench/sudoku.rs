767d2c0a97e56525af9ab3229851760f7b74970a
Patrick Walton <pcwalton@mimiga.net>
2013-01-30 21:46:47 -0800

    test: De-export sudoku. rs=burningtree

--- src/test/bench/sudoku.rs
+++ src/test/bench/sudoku.rs
@@ -29,16 +29,14 @@ use io::{ReaderUtil, WriterUtil};
 // If called without arguments, sudoku solves a built-in example sudoku
 //
 
-export grid_t, read_grid, solve_grid, write_grid;
-
 // internal type of sudoku grids
 type grid = ~[~[mut u8]];
 
 // exported type of sudoku grids
-enum grid_t { grid_ctor(grid), }
+pub enum grid_t { grid_ctor(grid), }
 
 // read a sudoku problem from file f
-fn read_grid(f: io::Reader) -> grid_t {
+pub fn read_grid(f: io::Reader) -> grid_t {
     assert f.read_line() == ~"9,9"; /* assert first line is exactly "9,9" */
 
     let g = vec::from_fn(10u, {|_i|
@@ -56,7 +54,7 @@ fn read_grid(f: io::Reader) -> grid_t {
 }
 
 // solve sudoku grid
-fn solve_grid(g: grid_t) {
+pub fn solve_grid(g: grid_t) {
     fn next_color(g: grid, row: u8, col: u8, start_color: u8) -> bool {
         if start_color < 10u8 {
             // colors not yet used
@@ -126,7 +124,7 @@ fn solve_grid(g: grid_t) {
     }
 }
 
-fn write_grid(f: io::Writer, g: grid_t) {
+pub fn write_grid(f: io::Writer, g: grid_t) {
     for u8::range(0u8, 9u8) |row| {
         f.write_str(fmt!("%u", (*g)[row][0] as uint));
         for u8::range(1u8, 9u8) |col| {
