676e0290ed4d306e6d7b517de1409c109309a0b2
Brian Anderson <banderson@mozilla.com>
2013-02-03 18:15:43 -0800

    core: Add rt mod and add the new scheduler code

---
+++ src/libcore/rt/stack.rs
@@ -0,0 +1,49 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use vec;
+
+pub struct StackSegment {
+    buf: ~[u8]
+}
+
+pub impl StackSegment {
+    static fn new(size: uint) -> StackSegment {
+        // Crate a block of uninitialized values
+        let mut stack = vec::with_capacity(size);
+        unsafe {
+            vec::raw::set_len(&mut stack, size);
+        }
+
+        StackSegment {
+            buf: stack
+        }
+    }
+
+    fn end(&self) -> *uint {
+        unsafe {
+            vec::raw::to_ptr(self.buf).offset(self.buf.len()) as *uint
+        }
+    }
+}
+
+pub struct StackPool(());
+
+impl StackPool {
+
+    static fn new() -> StackPool { StackPool(()) }
+
+    fn take_segment(&self, min_size: uint) -> StackSegment {
+        StackSegment::new(min_size)
+    }
+
+    fn give_segment(&self, _stack: StackSegment) {
+    }
+}
