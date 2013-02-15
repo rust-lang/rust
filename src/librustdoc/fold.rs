b71aed1de3bc25542a20c4daa6e7fb3d48bbcf03
Luqman Aden <laden@mozilla.com>
2013-02-15 00:37:08 -0800

    librustdoc: Get rid of `move`.

--- src/librustdoc/fold.rs
+++ src/librustdoc/fold.rs
@@ -87,25 +87,25 @@ fn mk_fold<T>(
     fold_struct: FoldStruct<T>
 ) -> Fold<T> {
     Fold {
-        ctxt: move ctxt,
-        fold_doc: move fold_doc,
-        fold_crate: move fold_crate,
-        fold_item: move fold_item,
-        fold_mod: move fold_mod,
-        fold_nmod: move fold_nmod,
-        fold_fn: move fold_fn,
-        fold_const: move fold_const,
-        fold_enum: move fold_enum,
-        fold_trait: move fold_trait,
-        fold_impl: move fold_impl,
-        fold_type: move fold_type,
-        fold_struct: move fold_struct
+        ctxt: ctxt,
+        fold_doc: fold_doc,
+        fold_crate: fold_crate,
+        fold_item: fold_item,
+        fold_mod: fold_mod,
+        fold_nmod: fold_nmod,
+        fold_fn: fold_fn,
+        fold_const: fold_const,
+        fold_enum: fold_enum,
+        fold_trait: fold_trait,
+        fold_impl: fold_impl,
+        fold_type: fold_type,
+        fold_struct: fold_struct
     }
 }
 
 pub fn default_any_fold<T: Clone>(ctxt: T) -> Fold<T> {
     mk_fold(
-        move ctxt,
+        ctxt,
         |f, d| default_seq_fold_doc(f, d),
         |f, d| default_seq_fold_crate(f, d),
         |f, d| default_seq_fold_item(f, d),
@@ -123,7 +123,7 @@ pub fn default_any_fold<T: Clone>(ctxt: T) -> Fold<T> {
 
 pub fn default_seq_fold<T: Clone>(ctxt: T) -> Fold<T> {
     mk_fold(
-        move ctxt,
+        ctxt,
         |f, d| default_seq_fold_doc(f, d),
         |f, d| default_seq_fold_crate(f, d),
         |f, d| default_seq_fold_item(f, d),
@@ -141,7 +141,7 @@ pub fn default_seq_fold<T: Clone>(ctxt: T) -> Fold<T> {
 
 pub fn default_par_fold<T: Clone>(ctxt: T) -> Fold<T> {
     mk_fold(
-        move ctxt,
+        ctxt,
         |f, d| default_seq_fold_doc(f, d),
         |f, d| default_seq_fold_crate(f, d),
         |f, d| default_seq_fold_item(f, d),
