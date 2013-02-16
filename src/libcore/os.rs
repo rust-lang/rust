74edcfe55d1c54764a2afb166d2023505dd60bf1
Graydon Hoare <graydon@mozilla.com>
2013-02-15 17:28:51 -0800

    attempt to fix BSD breakage

--- src/libcore/os.rs
+++ src/libcore/os.rs
@@ -375,7 +375,7 @@ pub fn self_exe_path() -> Option<Path> {
                            KERN_PROC as c_int,
                            KERN_PROC_PATHNAME as c_int, -1 as c_int];
                 sysctl(vec::raw::to_ptr(mib), vec::len(mib) as c_uint,
-                       buf, &mut sz, ptr::null(),
+                       buf as *mut c_void, &mut sz, ptr::null(),
                        0u as size_t) == (0 as c_int)
             }
         }
