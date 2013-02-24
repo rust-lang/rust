f9f942bb142a36ce4c94181ae8d7f8dcf4160700
Patrick Walton <pcwalton@mimiga.net>
2013-02-23 23:33:50 -0800

    test: Fix comments in pretty-printed test. rs=burningtree

--- src/test/run-fail/bug-2470-bounds-check-overflow.rs
+++ src/test/run-fail/bug-2470-bounds-check-overflow.rs
@@ -20,8 +20,8 @@ fn main() {
 
     let x = ~[1u,2u,3u];
     do vec::as_imm_buf(x) |p, _len| {
-        let base = p as uint;                     // base = 0x1230 say
-        let idx = base / sys::size_of::<uint>();  // idx  = 0x0246 say
+        let base = p as uint;
+        let idx = base / sys::size_of::<uint>();
         error!("ov1 base = 0x%x", base);
         error!("ov1 idx = 0x%x", idx);
         error!("ov1 sizeof::<uint>() = 0x%x", sys::size_of::<uint>());
