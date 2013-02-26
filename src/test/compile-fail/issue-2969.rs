28641228910dbedd738cbf4a70ad81b9ec5fd6e0
Patrick Walton <pcwalton@mimiga.net>
2013-02-25 17:28:40 -0800

    test: De-[mut] (remove all mutable arrays from) the tests. rs=demuting

--- src/test/compile-fail/issue-2969.rs
+++ src/test/compile-fail/issue-2969.rs
@@ -12,7 +12,7 @@
 fn main()
 {
 // See #2969 -- error message should be improved
-   let mut x = [mut 1, 2, 4];
+   let mut x = [1, 2, 4];
    let v : &int = &x[2];
    x[2] = 6;
    assert *v == 6;
