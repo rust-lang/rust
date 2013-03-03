431e756fd72df1c092e71f6e605e82385a9c6881
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2013-03-03 09:01:28 -0800

    rustdoc: change paragraphs fn to take &str

--- src/librustdoc/desc_to_brief_pass.rs
+++ src/librustdoc/desc_to_brief_pass.rs
@@ -142,7 +142,7 @@ fn parse_desc(desc: ~str) -> Option<~str> {
 }
 
 fn first_sentence(s: ~str) -> Option<~str> {
-    let paras = paragraphs(copy s);
+    let paras = paragraphs(s);
     if !paras.is_empty() {
         let first_para = vec::head(paras);
         Some(str::replace(first_sentence_(first_para), ~"\n", ~" "))
@@ -182,7 +182,7 @@ fn first_sentence_(s: ~str) -> ~str {
     }
 }
 
-fn paragraphs(s: ~str) -> ~[~str] {
+fn paragraphs(s: &str) -> ~[~str] {
     let lines = str::lines_any(s);
     let mut whitespace_lines = 0;
     let mut accum = ~"";
