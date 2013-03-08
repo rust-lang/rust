f73a15fd584b8ccec4a4a804501993d9b4bbf335
Patrick Walton <pcwalton@mimiga.net>
2013-03-08 12:10:48 -0800

    test: Fix tests. rs=tests

--- src/libcore/dlist.rs
+++ src/libcore/dlist.rs
@@ -507,7 +507,7 @@ impl<T> BaseIter<T> for @mut DList<T> {
     * allow for e.g. breadth-first search with in-place enqueues), but
     * removing the current node is forbidden.
     */
-    pure fn each(&self, f: fn(v: &T) -> bool) {
+    pure fn each(&self, f: &fn(v: &T) -> bool) {
         let mut link = self.peek_n();
         while option::is_some(&link) {
             let nobe = option::get(link);
--- src/test/compile-fail/regions-fn-subtyping.rs
+++ src/test/compile-fail/regions-fn-subtyping.rs
@@ -17,41 +17,41 @@ fn test_fn<T>(_x: &x/T, _y: &y/T, _z: &z/T) {
     // subtype::<T1>(of::<T2>()) will typecheck
     // iff T1 <: T2.
 
-    subtype::<fn(&a/T)>(
-        of::<fn(&a/T)>());
+    subtype::<&fn(&a/T)>(
+        of::<&fn(&a/T)>());
 
-    subtype::<fn(&a/T)>(
-        of::<fn(&b/T)>());
+    subtype::<&fn(&a/T)>(
+        of::<&fn(&b/T)>());
 
-    subtype::<fn(&b/T)>(
-        of::<fn(&x/T)>());
+    subtype::<&fn(&b/T)>(
+        of::<&fn(&x/T)>());
 
-    subtype::<fn(&x/T)>(
-        of::<fn(&b/T)>());  //~ ERROR mismatched types
+    subtype::<&fn(&x/T)>(
+        of::<&fn(&b/T)>());  //~ ERROR mismatched types
 
-    subtype::<fn(&a/T, &b/T)>(
-        of::<fn(&a/T, &a/T)>());
+    subtype::<&fn(&a/T, &b/T)>(
+        of::<&fn(&a/T, &a/T)>());
 
-    subtype::<fn(&a/T, &a/T)>(
-        of::<fn(&a/T, &b/T)>()); //~ ERROR mismatched types
+    subtype::<&fn(&a/T, &a/T)>(
+        of::<&fn(&a/T, &b/T)>()); //~ ERROR mismatched types
 
-    subtype::<fn(&a/T, &b/T)>(
-        of::<fn(&x/T, &y/T)>());
+    subtype::<&fn(&a/T, &b/T)>(
+        of::<&fn(&x/T, &y/T)>());
 
-    subtype::<fn(&x/T, &y/T)>(
-        of::<fn(&a/T, &b/T)>()); //~ ERROR mismatched types
+    subtype::<&fn(&x/T, &y/T)>(
+        of::<&fn(&a/T, &b/T)>()); //~ ERROR mismatched types
 
-    subtype::<fn(&x/T) -> @fn(&a/T)>(
-        of::<fn(&x/T) -> @fn(&a/T)>());
+    subtype::<&fn(&x/T) -> @fn(&a/T)>(
+        of::<&fn(&x/T) -> @fn(&a/T)>());
 
-    subtype::<fn(&a/T) -> @fn(&a/T)>(
-        of::<fn(&a/T) -> @fn(&b/T)>()); //~ ERROR mismatched types
+    subtype::<&fn(&a/T) -> @fn(&a/T)>(
+        of::<&fn(&a/T) -> @fn(&b/T)>()); //~ ERROR mismatched types
 
-    subtype::<fn(&a/T) -> @fn(&a/T)>(
-        of::<fn(&x/T) -> @fn(&b/T)>()); //~ ERROR mismatched types
+    subtype::<&fn(&a/T) -> @fn(&a/T)>(
+        of::<&fn(&x/T) -> @fn(&b/T)>()); //~ ERROR mismatched types
 
-    subtype::<fn(&a/T) -> @fn(&b/T)>(
-        of::<fn(&a/T) -> @fn(&a/T)>());
+    subtype::<&fn(&a/T) -> @fn(&b/T)>(
+        of::<&fn(&a/T) -> @fn(&a/T)>());
 }
 
 fn main() {}
