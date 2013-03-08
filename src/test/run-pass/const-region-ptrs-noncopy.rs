122e4a28b83ca0d3b3aef9b39eed414da54c4523
Jed Davis <jld@panix.com>
2013-03-08 11:29:47 -0800

    Don't copy const data to do an autoderef+autoref.

---
+++ src/test/run-pass/const-region-ptrs-noncopy.rs
@@ -0,0 +1,18 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+type Big = [u64 * 8];
+struct Pair { a: int, b: &'self Big }
+const x: &'static Big = &([13, 14, 10, 13, 11, 14, 14, 15]);
+const y: &'static Pair<'static> = &Pair {a: 15, b: x};
+
+pub fn main() {
+    fail_unless!(ptr::addr_of(x) == ptr::addr_of(y.b));
+}
