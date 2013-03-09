9df3a28b0f5aaab34842118083b75a03efa8dc53
Andrew Paseltiner <apaseltiner@gmail.com>
2013-03-09 12:14:12 -0500

    core: implement Clone for primitive types

--- src/libcore/clone.rs
+++ src/libcore/clone.rs
@@ -19,3 +19,31 @@ impl Clone for () {
     #[inline(always)]
     fn clone(&self) -> () { () }
 }
+
+macro_rules! clone_impl(
+    ($t:ty) => {
+        impl Clone for $t {
+            #[inline(always)]
+            fn clone(&self) -> $t { *self }
+        }
+    }
+)
+
+clone_impl!(int)
+clone_impl!(i8)
+clone_impl!(i16)
+clone_impl!(i32)
+clone_impl!(i64)
+
+clone_impl!(uint)
+clone_impl!(u8)
+clone_impl!(u16)
+clone_impl!(u32)
+clone_impl!(u64)
+
+clone_impl!(float)
+clone_impl!(f32)
+clone_impl!(f64)
+
+clone_impl!(bool)
+clone_impl!(char)
