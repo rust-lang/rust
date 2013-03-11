070137ce905d0177e8d112385f1d8dc7b2407006
Seo Sanghyeon <sanxiyn@gmail.com>
2013-03-11 19:53:41 +0900

    Add one more test for vector destructuring

--- src/test/run-pass/vec-matching.rs
+++ src/test/run-pass/vec-matching.rs
@@ -1,4 +1,4 @@
-pub fn main() {
+fn a() {
     let x = [1];
     match x {
         [_, _, _, _, _, .._] => ::core::util::unreachable(),
@@ -10,4 +10,45 @@ pub fn main() {
         }
         [] => ::core::util::unreachable()
     }
+}
+
+fn b() {
+    let x = [1, 2, 3];
+    match x {
+        [a, b, ..c] => {
+            fail_unless!(a == 1);
+            fail_unless!(b == 2);
+            fail_unless!(c == &[3]);
+        }
+        _ => fail!()
+    }
+    match x {
+        [..a, b, c] => {
+            fail_unless!(a == &[1]);
+            fail_unless!(b == 2);
+            fail_unless!(c == 3);
+        }
+        _ => fail!()
+    }
+    match x {
+        [a, ..b, c] => {
+            fail_unless!(a == 1);
+            fail_unless!(b == &[2]);
+            fail_unless!(c == 3);
+        }
+        _ => fail!()
+    }
+    match x {
+        [a, b, c] => {
+            fail_unless!(a == 1);
+            fail_unless!(b == 2);
+            fail_unless!(c == 3);
+        }
+        _ => fail!()
+    }
+}
+
+pub fn main() {
+    a();
+    b();
 }
