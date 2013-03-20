4a6b6c45f8f2db34a0740633e3134d59e6325d3f
Graydon Hoare <graydon@mozilla.com>
2013-03-20 12:49:22 -0700

    core: add Reader, Writer, ReaderUtil, WriterUtil to prelude. Close #4182.

--- src/librustdoc/markdown_writer.rs
+++ src/librustdoc/markdown_writer.rs
@@ -34,21 +34,21 @@ pub type Writer = ~fn(v: WriteInstr);
 pub type WriterFactory = ~fn(page: doc::Page) -> Writer;
 
 pub trait WriterUtils {
-    fn write_str(&self, +str: ~str);
-    fn write_line(&self, +str: ~str);
-    fn write_done(&self);
+    fn put_str(&self, +str: ~str);
+    fn put_line(&self, +str: ~str);
+    fn put_done(&self);
 }
 
 impl WriterUtils for Writer {
-    fn write_str(&self, str: ~str) {
+    fn put_str(&self, str: ~str) {
         (*self)(Write(str));
     }
 
-    fn write_line(&self, str: ~str) {
-        self.write_str(str + ~"\n");
+    fn put_line(&self, str: ~str) {
+        self.put_str(str + ~"\n");
     }
 
-    fn write_done(&self) {
+    fn put_done(&self) {
         (*self)(Done)
     }
 }
