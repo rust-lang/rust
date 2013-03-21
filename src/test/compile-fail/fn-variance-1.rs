02c49b32caaf9ed23a347ede9ea47743717e94d7
Patrick Walton <pcwalton@mimiga.net>
2013-03-21 15:39:17 -0700

    libsyntax: Remove `@const` from the language

--- src/test/compile-fail/fn-variance-1.rs
+++ src/test/compile-fail/fn-variance-1.rs
@@ -11,7 +11,6 @@
 #[legacy_modes];
 
 fn takes_mut(&&x: @mut int) { }
-fn takes_const(&&x: @const int) { }
 fn takes_imm(&&x: @int) { }
 
 fn apply<T>(t: T, f: &fn(T)) {
@@ -20,10 +19,8 @@ fn apply<T>(t: T, f: &fn(T)) {
 
 fn main() {
     apply(@3, takes_mut); //~ ERROR (values differ in mutability)
-    apply(@3, takes_const);
     apply(@3, takes_imm);
 
     apply(@mut 3, takes_mut);
-    apply(@mut 3, takes_const);
     apply(@mut 3, takes_imm); //~ ERROR (values differ in mutability)
 }
