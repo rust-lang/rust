b96765179ec16bc306bf0f1cc76c7ff67ea36670
Brian Anderson <banderson@mozilla.com>
2013-03-27 15:24:50 -0700

    core: Add rt::context for figuring out what runtime services are available
    Conflicts:
    	src/libcore/rt/sched/mod.rs

--- src/libcore/rt/sched/local.rs
+++ src/libcore/rt/sched/local.rs
@@ -10,6 +10,7 @@
 
 //! Access to the thread-local Scheduler
 
+use prelude::*;
 use ptr::mut_null;
 use libc::c_void;
 use cast::transmute;
@@ -39,6 +40,16 @@ pub fn take() -> ~Scheduler {
     }
 }
 
+/// Check whether there is a thread-local Scheduler attached to the running thread
+pub fn exists() -> bool {
+    unsafe {
+        match maybe_tls_key() {
+            Some(key) => tls::get(key).is_not_null(),
+            None => false
+        }
+    }
+}
+
 /// Borrow a mutable reference to the thread-local Scheduler
 /// # Safety Note
 /// Because this leaves the Scheduler in thread-local storage it is possible
@@ -60,10 +71,31 @@ pub unsafe fn borrow() -> &mut Scheduler {
 }
 
 fn tls_key() -> tls::Key {
+    maybe_tls_key().get()
+}
+
+fn maybe_tls_key() -> Option<tls::Key> {
     unsafe {
         let key: *mut c_void = rust_get_sched_tls_key();
         let key: &mut tls::Key = transmute(key);
-        return *key;
+        let key = *key;
+        // Check that the key has been initialized.
+
+        // NB: This is a little racy because, while the key is
+        // initalized under a mutex and it's assumed to be initalized
+        // in the Scheduler ctor by any thread that needs to use it,
+        // we are not accessing the key under a mutex.  Threads that
+        // are not using the new Scheduler but still *want to check*
+        // whether they are running under a new Scheduler may see a 0
+        // value here that is in the process of being initialized in
+        // another thread. I think this is fine since the only action
+        // they could take if it was initialized would be to check the
+        // thread-local value and see that it's not set.
+        if key != 0 {
+            return Some(key);
+        } else {
+            return None;
+        }
     }
 }
 
