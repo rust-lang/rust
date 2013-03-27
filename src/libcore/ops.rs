9c1bbc586c0ca808ea074e982e159051ae404554
Daniel Micay <danielmicay@gmail.com>
2013-03-27 03:07:55 -0400

    ops: add a docstring

--- src/libcore/ops.rs
+++ src/libcore/ops.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// Core operators
+//! Traits for the built-in operators
 
 #[lang="drop"]
 pub trait Drop {
