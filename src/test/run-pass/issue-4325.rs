c417ce777c81d35476d9caffdc29bdf50d186164
Tim Chevalier <chevalier@alum.wellesley.edu>
2013-03-28 19:24:17 -0700

    testsuite: Add test case for #4325

---
+++ src/test/run-pass/issue-4325.rs
@@ -0,0 +1,25 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+struct Node<'self, T> {
+  val: T,
+  next: Option<&'self Node<'self, T>>
+}
+
+impl<'self, T> Node<'self, T> {
+  fn get(&self) -> &'self T {
+    match self.next {
+      Some(ref next) => next.get(),
+      None => &self.val
+    }
+  }
+}
+
+fn main() {}
