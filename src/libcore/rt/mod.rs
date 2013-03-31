eadd358b2a35a64ed91565ce436469089aa9e718
Luqman Aden <me@luqman.ca>
2013-03-30 19:59:21 -0700

    Correct type signature for start lang item.

--- src/libcore/rt/mod.rs
+++ src/libcore/rt/mod.rs
@@ -48,6 +48,7 @@ mod context;
 mod thread;
 pub mod env;
 
+#[cfg(stage0)]
 pub fn start(main: *u8, _argc: int, _argv: *c_char, _crate_map: *u8) -> int {
     use self::sched::{Scheduler, Task};
     use self::uvio::UvEventLoop;
@@ -67,3 +68,22 @@ pub fn start(main: *u8, _argc: int, _argv: *c_char, _crate_map: *u8) -> int {
     }
 }
 
+#[cfg(not(stage0))]
+pub fn start(main: *u8, _argc: int, _argv: **c_char, _crate_map: *u8) -> int {
+    use self::sched::{Scheduler, Task};
+    use self::uvio::UvEventLoop;
+
+    let loop_ = ~UvEventLoop::new();
+    let mut sched = ~Scheduler::new(loop_);
+    let main_task = ~do Task::new(&mut sched.stack_pool) {
+        // XXX: Can't call a C function pointer from Rust yet
+        unsafe { rust_call_nullary_fn(main) };
+    };
+    sched.task_queue.push_back(main_task);
+    sched.run();
+    return 0;
+
+    extern {
+        fn rust_call_nullary_fn(f: *u8);
+    }
+}
