85f3100c4bc36666d9951b4f39ce884df9206ed0
Jens Nockert <jens@nockert.se>
2013-04-05 01:35:32 +0200

    The nomangle attribute is not needed and does not exist

--- src/libcore/unstable/intrinsics.rs
+++ src/libcore/unstable/intrinsics.rs
@@ -67,85 +67,71 @@ pub extern "rust-intrinsic" {
 
 #[nolink]
 pub extern mod llvm {
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.sqrt.f32"]
     pub fn sqrtf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.sqrt.f64"]
     pub fn sqrtf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.powi.f32"]
     pub fn powif32(a: f32, x: i32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.powi.f64"]
     pub fn powif64(a: f64, x: i32) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.sin.f32"]
     pub fn sinf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.sin.f64"]
     pub fn sinf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cos.f32"]
     pub fn cosf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cos.f64"]
     pub fn cosf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.pow.f32"]
     pub fn powf32(a: f32, x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.pow.f64"]
     pub fn powf64(a: f64, x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.exp.f32"]
     pub fn expf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.exp.f64"]
     pub fn expf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.log.f32"]
     pub fn logf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.log.f64"]
@@ -155,7 +141,6 @@ pub extern mod llvm {
     pub fn exp2f32(x: f32) -> f32;
     pub fn exp2f64(x: f64) -> f64;
 
-
     pub fn log10f32(x: f32) -> f32;
     pub fn log10f64(x: f64) -> f64;
 
@@ -163,151 +148,126 @@ pub extern mod llvm {
     pub fn log2f64(x: f64) -> f64;
     */
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.fma.f32"]
     pub fn fmaf32(a: f32, b: f32, c: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.fma.f64"]
     pub fn fmaf64(a: f64, b: f64, c: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.fabs.f32"]
     pub fn fabsf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.fabs.f64"]
     pub fn fabsf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.floor.f32"]
     pub fn floorf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.floor.f64"]
     pub fn floorf64(x: f64) -> f64;
 
     /* NOTE: Needs LLVM 3.3
-    #[nomangle]
     #[rust_stack]
     #[link_name="llvm.ceil.f32"]
     pub fn ceilf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[link_name="llvm.ceil.f64"]
     pub fn ceilf64(x: f64) -> f64;
 
-    #[nomangle]
     #[rust_stack]
     #[link_name="llvm.trunc.f32"]
     pub fn truncf32(x: f32) -> f32;
 
-    #[nomangle]
     #[rust_stack]
     #[link_name="llvm.trunc.f64"]
     pub fn truncf64(x: f64) -> f64;
     */
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctpop.i8"]
     pub fn ctpop8(x: i8) -> i8;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctpop.i16"]
     pub fn ctpop16(x: i16) -> i16;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctpop.i32"]
     pub fn ctpop32(x: i32) -> i32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctpop.i64"]
     pub fn ctpop64(x: i64) -> i64;
 
-    /* NOTE: Needs bool constants
-    #[nomangle]
+    /* NOTE: Needs i1 constants
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctlz.i8"]
     pub fn ctlz8(x: i8, is_zero_undef: bool) -> i8;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctlz.i16"]
     pub fn ctlz16(x: i16, is_zero_undef: bool) -> i16;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctlz.i32"]
     pub fn ctlz32(x: i32, is_zero_undef: bool) -> i32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.ctlz.i64"]
     pub fn ctlz64(x: i64, is_zero_undef: bool) -> i64;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cttz.i8"]
     pub fn cttz8(x: i8, is_zero_undef: bool) -> i8;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cttz.i16"]
     pub fn cttz16(x: i16, is_zero_undef: bool) -> i16;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cttz.i32"]
     pub fn cttz32(x: i32, is_zero_undef: bool) -> i32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.cttz.i64"]
     pub fn cttz64(x: i64, is_zero_undef: bool) -> i64;
     */
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.bswap.i16"]
     pub fn bswap16(x: i16) -> i16;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.bswap.i32"]
     pub fn bswap32(x: i32) -> i32;
 
-    #[nomangle]
     #[rust_stack]
     #[inline(always)]
     #[link_name="llvm.bswap.i64"]
