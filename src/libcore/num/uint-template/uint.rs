353ce872e287edf3764ca416d2a8cd8e1f54329b
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-04-13 23:44:35 +1000

    Generate NumCast impls and tests using macros

--- src/libcore/num/uint-template/uint.rs
+++ src/libcore/num/uint-template/uint.rs
@@ -10,8 +10,6 @@
 
 //! Operations and constants for `uint`
 
-use num::NumCast;
-
 pub use self::inst::{
     div_ceil, div_round, div_floor, iterate,
     next_power_of_two
@@ -208,73 +206,4 @@ pub mod inst {
         for ten.times { accum += 1; }
         assert!((accum == 10));
     }
-}
-
-impl NumCast for uint {
-    /**
-     * Cast `n` to a `uint`
-     */
-    #[inline(always)]
-    fn from<N:NumCast>(n: N) -> uint { n.to_uint() }
-
-    #[inline(always)] fn to_u8(&self)    -> u8    { *self as u8    }
-    #[inline(always)] fn to_u16(&self)   -> u16   { *self as u16   }
-    #[inline(always)] fn to_u32(&self)   -> u32   { *self as u32   }
-    #[inline(always)] fn to_u64(&self)   -> u64   { *self as u64   }
-    #[inline(always)] fn to_uint(&self)  -> uint  { *self          }
-
-    #[inline(always)] fn to_i8(&self)    -> i8    { *self as i8    }
-    #[inline(always)] fn to_i16(&self)   -> i16   { *self as i16   }
-    #[inline(always)] fn to_i32(&self)   -> i32   { *self as i32   }
-    #[inline(always)] fn to_i64(&self)   -> i64   { *self as i64   }
-    #[inline(always)] fn to_int(&self)   -> int   { *self as int   }
-
-    #[inline(always)] fn to_f32(&self)   -> f32   { *self as f32   }
-    #[inline(always)] fn to_f64(&self)   -> f64   { *self as f64   }
-    #[inline(always)] fn to_float(&self) -> float { *self as float }
-}
-
-#[test]
-fn test_numcast() {
-    assert!((20u   == 20u.to_uint()));
-    assert!((20u8  == 20u.to_u8()));
-    assert!((20u16 == 20u.to_u16()));
-    assert!((20u32 == 20u.to_u32()));
-    assert!((20u64 == 20u.to_u64()));
-    assert!((20i   == 20u.to_int()));
-    assert!((20i8  == 20u.to_i8()));
-    assert!((20i16 == 20u.to_i16()));
-    assert!((20i32 == 20u.to_i32()));
-    assert!((20i64 == 20u.to_i64()));
-    assert!((20f   == 20u.to_float()));
-    assert!((20f32 == 20u.to_f32()));
-    assert!((20f64 == 20u.to_f64()));
-
-    assert!((20u == NumCast::from(20u)));
-    assert!((20u == NumCast::from(20u8)));
-    assert!((20u == NumCast::from(20u16)));
-    assert!((20u == NumCast::from(20u32)));
-    assert!((20u == NumCast::from(20u64)));
-    assert!((20u == NumCast::from(20i)));
-    assert!((20u == NumCast::from(20i8)));
-    assert!((20u == NumCast::from(20i16)));
-    assert!((20u == NumCast::from(20i32)));
-    assert!((20u == NumCast::from(20i64)));
-    assert!((20u == NumCast::from(20f)));
-    assert!((20u == NumCast::from(20f32)));
-    assert!((20u == NumCast::from(20f64)));
-
-    assert!((20u == num::cast(20u)));
-    assert!((20u == num::cast(20u8)));
-    assert!((20u == num::cast(20u16)));
-    assert!((20u == num::cast(20u32)));
-    assert!((20u == num::cast(20u64)));
-    assert!((20u == num::cast(20i)));
-    assert!((20u == num::cast(20i8)));
-    assert!((20u == num::cast(20i16)));
-    assert!((20u == num::cast(20i32)));
-    assert!((20u == num::cast(20i64)));
-    assert!((20u == num::cast(20f)));
-    assert!((20u == num::cast(20f32)));
-    assert!((20u == num::cast(20f64)));
 }
