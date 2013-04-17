e6c3c371a554de6079c1c0b27885b8792431e516
Tim Chevalier <chevalier@alum.wellesley.edu>
2013-04-17 13:53:48 -0700

    rustpkg: Add a few tests
    There's no test runner for rustpkg yet; just sketching out a few
    basic test scenarios. pass/ contains packages that should compile
    successfully, whereas fail/ contains packages that should fail
    to build.

---
+++ src/librustpkg/testsuite/pass/fancy-lib/bar.rs
@@ -0,0 +1,3 @@
+pub fn assert_true() {
+    assert!(true);
+}
---
+++ src/librustpkg/testsuite/pass/fancy-lib/pkg.rs
@@ -0,0 +1,17 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+pub fn main() {
+    let cwd = os::getcwd();
+    debug!("cwd = %s", cwd.to_str());
+    let file = io::file_writer(&Path(~"fancy-lib/build/generated.rs"),
+                               [io::Create]).get();
+    file.write_str("pub fn wheeeee() { for [1, 2, 3].each() |_| { assert!(true); } }");
+}
