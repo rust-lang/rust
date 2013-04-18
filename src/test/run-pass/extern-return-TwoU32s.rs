8a6d6dd868371e2ffc035dc508fb255bb9cbc8a0
Brian Anderson <banderson@mozilla.com>
2013-04-18 14:10:13 -0700

    rustc: Fix struct returns on x86 mac for 8-byte structs
    Mac appears to follow the same ABI as MSVC. This fixes one case
    but leaves others broken, like windows.

--- src/test/run-pass/extern-return-TwoU32s.rs
+++ src/test/run-pass/extern-return-TwoU32s.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-macos Broken on mac i686
-
 struct TwoU32s {
     one: u32, two: u32
 }
