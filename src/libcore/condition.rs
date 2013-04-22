94c7c474475c8ae117257e18f67dd9c7522c9fc9
Brian Anderson <banderson@mozilla.com>
2013-04-22 14:43:02 -0700

    Make conditions public. #6009

--- src/libcore/condition.rs
+++ src/libcore/condition.rs
@@ -192,4 +192,25 @@ mod test {
 
         assert!(trapped);
     }
+
+    // Issue #6009
+    mod m {
+        condition! {
+            sadness: int -> int;
+        }
+
+        mod n {
+            use super::sadness;
+
+            #[test]
+            fn test_conditions_are_public() {
+                let mut trapped = false;
+                do sadness::cond.trap(|_| {
+                    0
+                }).in {
+                    sadness::cond.raise(0);
+                }
+            }
+        }
+    }
 }
