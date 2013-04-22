795031e8bc21bfd03602a641801c394991ccef46
Jed Davis <jld@panix.com>
2013-04-22 08:49:21 -0700

    Add some tests for nullable-pointer enums

---
+++ src/test/run-pass/nullable-pointer-size.rs
@@ -0,0 +1,44 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+enum E<T> { Thing(int, T), Nothing((), ((), ()), [i8, ..0]) }
+struct S<T>(int, T);
+
+// These are macros so we get useful assert messages.
+
+macro_rules! check_option {
+    ($T:ty) => {
+        assert!(sys::size_of::<Option<$T>>() == sys::size_of::<$T>());
+    }
+}
+
+macro_rules! check_fancy {
+    ($T:ty) => {
+        assert!(sys::size_of::<E<$T>>() == sys::size_of::<S<$T>>());
+    }
+}
+
+macro_rules! check_type {
+    ($T:ty) => {{
+        check_option!($T);
+        check_fancy!($T);
+    }}
+}
+
+pub fn main() {
+    check_type!(&'static int);
+    check_type!(~int);
+    check_type!(@int);
+    check_type!(~str);
+    check_type!(@str);
+    check_type!(~[int]);
+    check_type!(@[int]);
+    check_type!(extern fn());
+}
