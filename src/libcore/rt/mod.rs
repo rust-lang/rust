9fc01ab9cef2b33a53122c232532480db8160216
Brian Anderson <banderson@mozilla.com>
2013-04-23 19:21:37 -0700

    core::rt: Add more I/O docs

--- src/libcore/rt/mod.rs
+++ src/libcore/rt/mod.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+/*! The Rust runtime, including the scheduler and I/O interface */
+
 #[doc(hidden)];
 
 use libc::c_char;
