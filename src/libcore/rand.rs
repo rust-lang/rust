08b7d4cdbab33928ba98c366b41650e753d69611
Philipp Brüschweiler <blei42@gmail.com>
2013-04-28 13:31:49 +0200

    rand: Fix infinite recursion
    `self` has type `&@Rand`, so `*self` will be of type `@Rand` which causes
    this same impl to be called again.

--- src/libcore/rand.rs
+++ src/libcore/rand.rs
@@ -690,7 +690,7 @@ pub fn task_rng() -> @IsaacRng {
 
 // Allow direct chaining with `task_rng`
 impl<R: Rng> Rng for @R {
-    fn next(&self) -> u32 { (*self).next() }
+    fn next(&self) -> u32 { (**self).next() }
 }
 
 /**
