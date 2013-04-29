cd18b882845ffa99f3a8f541664420e929904e7f
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-04-29 22:15:58 +1000

    Merge Exponential and Hyperbolic traits
    The Hyperbolic Functions are trivially implemented in terms of `exp`, so it's  simpler to group them the Exponential trait. In the future these would have default implementations.

--- src/libcore/num/f32.rs
+++ src/libcore/num/f32.rs
@@ -425,9 +425,7 @@ impl Exponential for f32 {
 
     #[inline(always)]
     fn log10(&self) -> f32 { log10(*self) }
-}
 
-impl Hyperbolic for f32 {
     #[inline(always)]
     fn sinh(&self) -> f32 { sinh(*self) }
 
