7894cf68dab405e8bcfb2c4e4c956c419e5e13f4
James Miller <bladeon@gmail.com>
2013-05-09 22:23:38 +1200

    Add uninit intrinsic

--- src/librustc/middle/trans/type_use.rs
+++ src/librustc/middle/trans/type_use.rs
@@ -118,7 +118,7 @@ pub fn type_uses_for(ccx: @CrateContext, fn_id: def_id, n_tps: uint)
         if abi.is_intrinsic() {
             let flags = match *cx.ccx.sess.str_of(i.ident) {
                 ~"size_of"  | ~"pref_align_of" | ~"min_align_of" |
-                ~"init"     | ~"transmute"     | ~"move_val"     |
+                ~"uninit"   | ~"init" | ~"transmute" | ~"move_val" |
                 ~"move_val_init" => use_repr,
 
                 ~"get_tydesc" | ~"needs_drop" => use_tydesc,
