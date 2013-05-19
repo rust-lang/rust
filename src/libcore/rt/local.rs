bf27b9a48a01e4656149940ffa8cc80c361fa585
Brian Anderson <banderson@mozilla.com>
2013-05-19 15:25:35 -0700

    core::rt: Make local_sched a wrapper around Local

--- src/libcore/rt/local.rs
+++ src/libcore/rt/local.rs
@@ -8,10 +8,21 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use rt::sched::Scheduler;
+use rt::local_ptr;
+
 pub trait Local {
     fn put_local(value: ~Self);
     fn take_local() -> ~Self;
-    fn exists() -> bool;
-    fn borrow(f: &fn(&mut Self));
-    fn unsafe_borrow() -> *mut Self;
+    fn exists_local() -> bool;
+    fn borrow_local(f: &fn(&mut Self));
+    unsafe fn unsafe_borrow_local() -> *mut Self;
+}
+
+impl Local for Scheduler {
+    fn put_local(value: ~Scheduler) { unsafe { local_ptr::put(value) }}
+    fn take_local() -> ~Scheduler { unsafe { local_ptr::take() } }
+    fn exists_local() -> bool { local_ptr::exists() }
+    fn borrow_local(f: &fn(&mut Scheduler)) { unsafe { local_ptr::borrow(f) } }
+    unsafe fn unsafe_borrow_local() -> *mut Scheduler { local_ptr::unsafe_borrow() }
 }
