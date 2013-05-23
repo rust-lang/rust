d7e43aa779f3a82614766af41adb5828d9282b5a
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2013-05-23 09:39:38 -0700

    cleanup warnings from librustdoc

--- src/librustdoc/extract.rs
+++ src/librustdoc/extract.rs
@@ -16,9 +16,7 @@ use astsrv;
 use doc::ItemUtils;
 use doc;
 
-use core::local_data::local_data_get;
 use syntax::ast;
-use syntax;
 use syntax::parse::token::{ident_interner};
 use syntax::parse::token;
 
