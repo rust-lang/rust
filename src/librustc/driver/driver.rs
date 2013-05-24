da71b0c7b997acc5a7c8ad1f4a879e2cbbae02a4
Patrick Walton <pcwalton@mimiga.net>
2013-05-23 19:12:16 -0700

    librustc: Redo the unsafe checker and make unsafe methods not callable from safe code

--- src/librustc/driver/driver.rs
+++ src/librustc/driver/driver.rs
@@ -262,6 +262,9 @@ pub fn compile_rest(sess: Session,
         time(time_passes, ~"privacy checking", ||
              middle::privacy::check_crate(ty_cx, &method_map, crate));
 
+        time(time_passes, ~"effect checking", ||
+             middle::effect::check_crate(ty_cx, method_map, crate));
+
         time(time_passes, ~"loop checking", ||
              middle::check_loop::check_crate(ty_cx, crate));
 
