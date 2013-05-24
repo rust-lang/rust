f2125434d86669d3086094397855f80c2bf636ac
Olivier Saut <osaut@airpost.net>
2013-05-24 21:38:12 +0200

    Remove the get function
    Rust is now preferring methods over functions and there is no legacy with ARC

--- src/test/compile-fail/no-capture-arc.rs
+++ src/test/compile-fail/no-capture-arc.rs
@@ -1,4 +1,4 @@
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2012-2013 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -18,11 +18,11 @@ fn main() {
     let arc_v = arc::ARC(v);
 
     do task::spawn() {
-        let v = *arc::get(&arc_v);
+        let v = arc_v.get();
         assert_eq!(v[3], 4);
     };
 
-    assert_eq!((*arc::get(&arc_v))[2], 3);
+    assert_eq!((arc_v.get())[2], 3);
 
     info!(arc_v);
 }
