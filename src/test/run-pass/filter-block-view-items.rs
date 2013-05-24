ac6c15aecea4bb9a328cf8f80e1cbb9f0104d98b
Alex Crichton <alex@alexcrichton.com>
2013-05-24 17:29:54 -0500

    Actually filter view_items in blocks

---
+++ src/test/run-pass/filter-block-view-items.rs
@@ -0,0 +1,15 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+fn main() {
+    // Make sure that this view item is filtered out because otherwise it would
+    // trigger a compilation error
+    #[cfg(not_present)] use foo = bar;
+}
