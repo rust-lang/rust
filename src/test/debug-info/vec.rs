6759ce4fd2083595193c93c3fd72383d24a73a5e
James Miller <bladeon@gmail.com>
2013-06-21 00:23:42 +1200

    _Actually_ xfail debuginfo tests

--- src/test/debug-info/vec.rs
+++ src/test/debug-info/vec.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail
+// xfail-test
 
 // compile-flags:-Z extra-debug-info
 // debugger:set print pretty off
