109b81bb0109c66dcf3dbd91eabbbfa4dd7eb92a
Corey Richardson <corey@octayn.net>
2013-06-22 14:43:15 -0400

    Fix/annotate a variety of xfails

--- src/test/run-pass/labeled-break.rs
+++ src/test/run-pass/labeled-break.rs
@@ -8,9 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-fast
-// xfail-test
-
 pub fn main() {
     'foo: loop {
         loop {
