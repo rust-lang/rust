e1b8c6758099b634ea9033f97ddcd3fa88b5abeb
Palmer Cox <p@lmercox.com>
2013-06-23 18:57:59 -0400

    Improve the SHA-1 implementation
    * Rename struct Sha1State to Sha1
    * Remove all use of @ types
    * Use fixed length vectors
    * Move all of the inner functions from inside sha1() to top level, private functions
    * Sha1 instances are now created via Sha1::new()
    * Update all constant names to uppercase
    * Remove unecessary assert_eq!s
    * Remove check_vec_eq() helper function; use vec::eq() instead

--- src/libextra/workcache.rs
+++ src/libextra/workcache.rs
@@ -13,7 +13,7 @@
 use core::prelude::*;
 
 use json;
-use sha1;
+use sha1::Sha1;
 use serialize::{Encoder, Encodable, Decoder, Decodable};
 use sort;
 
@@ -248,13 +248,13 @@ fn json_decode<T:Decodable<json::Decoder>>(s: &str) -> T {
 }
 
 fn digest<T:Encodable<json::Encoder>>(t: &T) -> ~str {
-    let mut sha = sha1::sha1();
+    let mut sha = Sha1::new();
     sha.input_str(json_encode(t));
     sha.result_str()
 }
 
 fn digest_file(path: &Path) -> ~str {
-    let mut sha = sha1::sha1();
+    let mut sha = Sha1::new();
     let s = io::read_whole_file_str(path);
     sha.input_str(*s.get_ref());
     sha.result_str()
