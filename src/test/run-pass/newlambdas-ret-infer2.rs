911a09d0bd87ebb34fac29164fd53926d05c19db
Corey Richardson <corey@octayn.net>
2013-06-24 08:05:41 -0400

    Un-xfail working test

--- src/test/run-pass/newlambdas-ret-infer2.rs
+++ src/test/run-pass/newlambdas-ret-infer2.rs
@@ -8,7 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-test ~fn is not inferred
 // Test that the lambda kind is inferred correctly as a return
 // expression
 
