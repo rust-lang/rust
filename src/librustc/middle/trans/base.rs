203bbe031dca1eb407f303bd5b0f6872eebf3c01
John Clements <clements@racket-lang.org>
2013-07-11 22:58:14 -0700

    whitespace, reindentation, and comments only

--- src/librustc/middle/trans/base.rs
+++ src/librustc/middle/trans/base.rs
@@ -2760,7 +2760,8 @@ pub fn declare_intrinsics(llmod: ModuleRef) -> HashMap<&'static str, ValueRef> {
 
 pub fn declare_dbg_intrinsics(llmod: ModuleRef, intrinsics: &mut HashMap<&'static str, ValueRef>) {
     ifn!(intrinsics, "llvm.dbg.declare", [Type::metadata(), Type::metadata()], Type::void());
-    ifn!(intrinsics, "llvm.dbg.value",   [Type::metadata(), Type::i64(), Type::metadata()], Type::void());
+    ifn!(intrinsics,
+         "llvm.dbg.value",   [Type::metadata(), Type::i64(), Type::metadata()], Type::void());
 }
 
 pub fn trap(bcx: @mut Block) {
