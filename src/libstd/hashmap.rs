7060de47e38e51e0316e6e054bee7c9a13e2d2cb
blake2-ppc <blake2-ppc>
2013-07-17 21:32:49 +0200

    Fix warnings in libstd and librusti tests

--- src/libstd/hashmap.rs
+++ src/libstd/hashmap.rs
@@ -863,7 +863,7 @@ pub type SetAlgebraIter<'self, T> =
 
 #[cfg(test)]
 mod test_map {
-    use container::{Container, Map, Set};
+    use container::{Container, Map};
     use option::{None, Some};
     use super::*;
     use uint;
@@ -1104,7 +1104,7 @@ mod test_map {
 #[cfg(test)]
 mod test_set {
     use super::*;
-    use container::{Container, Map, Set};
+    use container::Container;
     use vec::ImmutableEqVector;
     use uint;
 
