a177ff8d3c88b0ec1299060bd131c41807578b35
Alex Crichton <alex@alexcrichton.com>
2013-08-01 01:53:41 -0700

    Resolve globals having different addresses across crates
    * All globals marked as `pub` won't have the `internal` linkage type set
    * All global references across crates are forced to use the address of the
      global in the other crate via an external reference.

---
+++ src/test/run-pass/xcrate-static-addresses.rs
@@ -0,0 +1,27 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// xfail-fast
+// aux-build:xcrate_static_addresses.rs
+
+extern mod xcrate_static_addresses;
+
+use other = xcrate_static_addresses;
+
+pub fn main() {
+    other::verify_same(&other::global);
+
+    // Previously this fail'd because there were two addresses that were being
+    // used when declaring constants.
+    do other::test::cond.trap(|_| {
+    }).inside {
+        other::raise();
+    }
+}
