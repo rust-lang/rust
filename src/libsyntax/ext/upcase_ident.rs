f4debc9086d82edec27fd43bdabb5ea362ea17d1
Youngmin Yoo <youngmin.yoo@samsung.com>
2013-08-01 14:07:21 +0900

    Add upcase_ident.rs for ident string to upper

---
+++ src/libsyntax/ext/upcase_ident.rs
@@ -0,0 +1,45 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use ast;
+use codemap::span;
+use ext::base::*;
+use ext::base;
+use parse::token;
+use parse::token::{str_to_ident};
+
+pub fn expand_syntax_ext(cx: @ExtCtxt, sp: span, tts: &[ast::token_tree])
+    -> base::MacResult {
+    let mut res_str = ~"";
+    for tts.iter().advance |elem| {
+        match *elem {
+            ast::tt_tok(_, token::IDENT(ident,_)) => res_str.push_str(cx.str_of(ident)),
+                _ => cx.span_fatal(sp, "upcase_ident! requires ident args.")
+        }
+    }
+    res_str = res_str.to_ascii().to_upper().to_str_ascii();
+
+    let res = str_to_ident(res_str);
+
+    let e = @ast::expr {
+        id: cx.next_id(),
+        node: ast::expr_path(
+            ast::Path {
+                 span: sp,
+                 global: false,
+                 idents: ~[res],
+                 rp: None,
+                 types: ~[],
+            }
+        ),
+        span: sp,
+    };
+    MRExpr(e)
+}
