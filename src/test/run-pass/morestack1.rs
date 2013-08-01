1d256ba6e83c9f779e8c9a5346eb5df95ef589b2
Brian Anderson <banderson@mozilla.com>
2013-07-31 18:11:18 -0700

    test: Disable morestack tests
    The new scheduler does not yet implement split stacks

--- src/test/run-pass/morestack1.rs
+++ src/test/run-pass/morestack1.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// xfail-test newsched transition
+
 fn getbig(i: int) {
     if i != 0 {
         getbig(i - 1);
