650d91e647c15d76d474ffdb691fddc30afaa064
David Halperin <halperin.dr@gmail.com>
2013-08-02 17:53:59 -0400

    Add backticks around trait name in conflicting implementations error message

--- src/test/compile-fail/conflicting-implementations.rs
+++ src/test/compile-fail/conflicting-implementations.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern: conflicting implementations for trait Foo
+// error-pattern: conflicting implementations for trait `Foo`
 trait Foo {
 }
 
