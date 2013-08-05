e5fb4c4359de527c6807b64cd7855c9fb1b3176a
Huon Wilson <dbau.pp+github@gmail.com>
2013-08-05 19:53:42 +1000

    testsuite: remove incorrect section of 2 test cases. (Also, &const is disappearing.)
    Fixes #7304.

--- src/test/run-pass/coerce-reborrow-imm-vec-rcvr.rs
+++ src/test/run-pass/coerce-reborrow-imm-vec-rcvr.rs
@@ -1,9 +1,3 @@
-/* FIXME #7302
-fn foo(v: &const [uint]) -> ~[uint] {
-    v.to_owned()
-}
-*/
-
 fn bar(v: &mut [uint]) -> ~[uint] {
     v.to_owned()
 }
@@ -14,7 +8,6 @@ fn bip(v: &[uint]) -> ~[uint] {
 
 pub fn main() {
     let mut the_vec = ~[1u, 2, 3, 100];
-//    assert_eq!(the_vec.clone(), foo(the_vec));
     assert_eq!(the_vec.clone(), bar(the_vec));
     assert_eq!(the_vec.clone(), bip(the_vec));
 }
