b743dbe0ec85a666ad717a7d75dcd3b5cff1846c
Daniel Micay <danielmicay@gmail.com>
2013-08-10 13:42:53 -0400

    move `strdup_uniq` lang item to std::str

--- src/libstd/unstable/lang.rs
+++ src/libstd/unstable/lang.rs
@@ -93,12 +93,6 @@ pub unsafe fn check_not_borrowed(a: *u8,
     borrowck::check_not_borrowed(a, file, line)
 }
 
-#[lang="strdup_uniq"]
-#[inline]
-pub unsafe fn strdup_uniq(ptr: *c_uchar, len: uint) -> ~str {
-    str::raw::from_buf_len(ptr, len)
-}
-
 #[lang="annihilate"]
 pub unsafe fn annihilate() {
     ::cleanup::annihilate()
