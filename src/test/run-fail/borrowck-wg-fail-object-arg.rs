afb20775d1836c79975dfb9889fdeffe0c8baf58
Niko Matsakis <niko@alum.mit.edu>
2013-08-13 14:59:33 -0400

    Fix freezing of @mut Objects when passing as argument

---
+++ src/test/run-fail/borrowck-wg-fail-object-arg.rs
@@ -0,0 +1,22 @@
+// error-pattern:borrowed
+
+trait Foo {
+    fn foo(&self, @mut int);
+}
+
+impl Foo for int {
+    fn foo(&self, x: @mut int) {
+        *x += *self;
+    }
+}
+
+fn it_takes_two(f: &Foo, g: &mut Foo) {
+}
+
+fn main() {
+    let x = @mut 3_i;
+    let y = x as @mut Foo;
+    let z = y;
+
+    it_takes_two(y, z);
+}
