783c6a1fbf9c44bc58e3ba9224d5920db1de91f0
Alex Crichton <alex@alexcrichton.com>
2013-08-12 20:18:47 -0700

    Add a bunch of tests for closed issues
    Closes #3907
    Closes #5493
    Closes #4464
    Closes #4759
    Closes #5666
    Closes #5884
    Closes #5926
    Closes #6318
    Closes #6557
    Closes #6898
    Closes #6919
    Closes #7222

---
+++ src/test/compile-fail/issue-3907.rs
@@ -0,0 +1,30 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// aux-build:issue_3907.rs
+extern mod issue_3907;
+
+type Foo = issue_3907::Foo; //~ ERROR: reference to trait
+
+struct S {
+    name: int
+}
+
+impl Foo for S { //~ ERROR: Foo is not a trait
+    fn bar() { }
+}
+
+fn main() {
+    let s = S {
+        name: 0
+    };
+    s.bar();
+}
+
