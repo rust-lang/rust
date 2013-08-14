0932ab336ffd9c53d3a164e0f94fcab2206c3fee
Nick Desaulniers <ndesaulniers@mozilla.com>
2013-08-13 17:06:27 -0700

    Remove unused automatic cfg bindings Fixes #7169

--- src/librustc/rustc.rs
+++ src/librustc/rustc.rs
@@ -252,7 +252,7 @@ pub fn run_compiler(args: &~[~str], demitter: diagnostic::Emitter) {
     let sess = build_session(sopts, demitter);
     let odir = getopts::opt_maybe_str(matches, "out-dir").map_move(|o| Path(o));
     let ofile = getopts::opt_maybe_str(matches, "o").map_move(|o| Path(o));
-    let cfg = build_configuration(sess, binary, &input);
+    let cfg = build_configuration(sess);
     let pretty = do getopts::opt_default(matches, "pretty", "normal").map_move |a| {
         parse_pretty(sess, a)
     };
--- src/librustdoc/parse.rs
+++ src/librustdoc/parse.rs
@@ -10,8 +10,6 @@
 
 //! AST-parsing helpers
 
-
-use rustc::driver::driver::{file_input, str_input};
 use rustc::driver::driver;
 use rustc::driver::session;
 use syntax::ast;
@@ -29,14 +27,15 @@ pub fn from_str(source: @str) -> @ast::Crate {
 
 pub fn from_file_sess(sess: session::Session, file: &Path) -> @ast::Crate {
     parse::parse_crate_from_file(
-        file, cfg(sess, file_input((*file).clone())), sess.parse_sess)
+        file, cfg(sess), sess.parse_sess)
 }
 
 pub fn from_str_sess(sess: session::Session, source: @str) -> @ast::Crate {
     parse::parse_crate_from_source_str(
-        @"-", source, cfg(sess, str_input(source)), sess.parse_sess)
+        @"-", source, cfg(sess), sess.parse_sess)
 }
 
-fn cfg(sess: session::Session, input: driver::input) -> ast::CrateConfig {
-    driver::build_configuration(sess, @"rustdoc", &input)
+fn cfg(sess: session::Session) -> ast::CrateConfig {
+    driver::build_configuration(sess)
 }
+
