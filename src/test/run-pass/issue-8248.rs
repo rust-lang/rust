1764e207fb2cec0c656e1acd0e07eefb9e6ac7b9
Alex Crichton <alex@alexcrichton.com>
2013-08-15 14:22:36 -0700

    Add even more tests
    Closes #8248
    Closes #8249
    Closes #8398
    Closes #8401

---
+++ src/test/run-pass/issue-8248.rs
@@ -0,0 +1,21 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+trait A {}
+struct B;
+impl A for B {}
+
+fn foo(_: &mut A) {}
+
+fn main() {
+    let mut b = B;
+    foo(&mut b as &mut A);
+}
+
