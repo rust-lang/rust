393a130b3da254e7ac02931d7c4a74d02af41b5c
Tim Chevalier <chevalier@alum.wellesley.edu>
2013-08-16 21:22:06 -0700

    Testsuite: Test for #7013. Closes #7013

---
+++ src/test/compile-fail/issue-7013.rs
@@ -0,0 +1,43 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+extern mod extra;
+use extra::rc::RcMut;
+
+trait Foo
+{
+    fn set(&mut self, v: RcMut<A>);
+}
+
+struct B
+{
+    v: Option<RcMut<A>>
+}
+
+impl Foo for B
+{
+    fn set(&mut self, v: RcMut<A>)
+    {
+        self.v = Some(v);
+    }
+}
+
+struct A
+{
+    v: ~Foo,
+}
+
+fn main()
+{
+    let a = A {v: ~B{v: None} as ~Foo}; //~ ERROR cannot pack type `~B`, which does not fulfill `Send`
+    let v = RcMut::from_freeze(a); //~ ERROR instantiating a type parameter with an incompatible type
+    let w = v.clone();
+    v.with_mut_borrow(|p| {p.v.set(w.clone());})
+}
