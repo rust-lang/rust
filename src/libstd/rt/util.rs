6eccbee581930e6853c06446b0869beb21d647c9
Brian Anderson <banderson@mozilla.com>
2013-08-19 10:10:27 -0700

    std::rt: Enforce sanity a while longer
    I'm not comfortable turning off rtassert! yet

--- src/libstd/rt/util.rs
+++ src/libstd/rt/util.rs
@@ -20,7 +20,8 @@ use unstable::atomics::{AtomicInt, INIT_ATOMIC_INT, SeqCst};
 use unstable::running_on_valgrind;
 
 // Indicates whether we should perform expensive sanity checks, including rtassert!
-pub static ENFORCE_SANITY: bool = !cfg!(rtopt) || cfg!(rtdebug) || cfg!(rtassert);
+// XXX: Once the runtime matures remove the `true` below to turn off rtassert, etc.
+pub static ENFORCE_SANITY: bool = true || !cfg!(rtopt) || cfg!(rtdebug) || cfg!(rtassert);
 
 /// Get the number of cores available
 pub fn num_cpus() -> uint {
