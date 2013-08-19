af876b4643a3d47338c5d08e007e2a4de9fdf050
Alex Crichton <alex@alexcrichton.com>
2013-08-18 17:11:45 -0700

    Upgrade libuv to the current master (again)
    This is a reopening of the libuv-upgrade part of #8645. Hopefully this won't
    cause random segfaults all over the place. The windows regression in testing
    should also be fixed (it shouldn't build the whole compiler twice).
    
    A notable difference from before is that gyp is now a git submodule instead of
    always git-cloned at make time. This allows bundling for releases more easily.
    
    Closes #8850

--- mk/dist.mk
+++ mk/dist.mk
@@ -12,7 +12,7 @@ PKG_ICO = $(S)src/etc/pkg/rust-logo.ico
 PKG_EXE = $(PKG_DIR)-install.exe
 endif
 
-PKG_GITMODULES := $(S)src/libuv $(S)src/llvm
+PKG_GITMODULES := $(S)src/libuv $(S)src/llvm $(S)src/gyp
 
 PKG_FILES := \
     $(S)COPYRIGHT                              \
