40bd1ecd27de3776774edab59f5027972d4b5b06
Brian Anderson <banderson@mozilla.com>
2013-08-27 12:24:50 -0700

    rustc: Fix cstack lint for default methods. Closes #8753

---
+++ src/test/run-pass/lint-cstack.rs
@@ -0,0 +1,27 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::libc;
+
+extern {
+    fn rust_get_test_int() -> libc::intptr_t;
+}
+
+trait A {
+    #[fixed_stack_segment]
+    fn foo() {
+        unsafe {
+            rust_get_test_int();
+        }
+    }
+}
+
+fn main() {
+}
