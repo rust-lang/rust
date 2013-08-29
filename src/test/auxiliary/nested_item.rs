2c1d6568f253a2310a0b4a06064b2cf0271aa6be
Alex Crichton <alex@alexcrichton.com>
2013-08-28 23:28:06 -0700

    Fix a bug with statics inside blocks in generic fns
    Whenever a generic function was encountered, only the top-level items were
    recursed upon, even though the function could contain items inside blocks or
    nested inside of other expressions. This fixes the existing code from traversing
    just the top level items to using a Visitor to deeply recurse and find any items
    which need to be translated.
    
    This was uncovered when building code with --lib, because the encode_symbol
    function would panic once it found that an item hadn't been translated.
    
    Closes #8134

---
+++ src/test/auxiliary/nested_item.rs
@@ -0,0 +1,40 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// original problem
+fn foo<T>() -> int {
+    {
+        static foo: int = 2;
+        foo
+    }
+}
+
+// issue 8134
+struct Foo;
+impl<T> Foo {
+    pub fn foo(&self) {
+        static X: uint = 1;
+    }
+}
+
+// issue 8134
+pub struct Parser<T>;
+impl<T: std::iterator::Iterator<char>> Parser<T> {
+    fn in_doctype(&mut self) {
+        static DOCTYPEPattern: [char, ..6] = ['O', 'C', 'T', 'Y', 'P', 'E'];
+    }
+}
+
+struct Bar;
+impl<T> Foo {
+    pub fn bar(&self) {
+        static X: uint = 1;
+    }
+}
