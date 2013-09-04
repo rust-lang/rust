b49e9fa794addc197e58743bdc120cb9740b73c0
Daniel Micay <danielmicay@gmail.com>
2013-09-04 17:05:31 -0400

    forbid cast as bool
    This is currently unsound since `bool` is represented as `i8`. It will
    become sound when `bool` is stored as `i8` but always used as `i1`.
    
    However, the current behaviour will always be identical to `x & 1 != 0`,
    so there's no need for it. It's also surprising, since `x != 0` is the
    expected behaviour.
    
    Closes #7311

--- src/test/run-pass/cast.rs
+++ src/test/run-pass/cast.rs
@@ -18,6 +18,5 @@ pub fn main() {
     assert_eq!(i as u8, 'Q' as u8);
     assert_eq!(i as u8 as i8, 'Q' as u8 as i8);
     assert_eq!(0x51u8 as char, 'Q');
-    assert_eq!(true, 1 as bool);
     assert_eq!(0 as u32, false as u32);
 }
