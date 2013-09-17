233e0fb205c2daaf44181a5331e610c1c42e245d
Benjamin Herr <ben@0x539.de>
2013-09-17 22:13:47 +0200

    pp: also print bounds in paths with no generic params
    Since 3b6314c3 the pretty printer seems to only print trait bounds for
    `ast::ty_path(...)`s that have a generics arguments list. That seems
    wrong, so let's always print them.
    
    Closes #9253, un-xfails test for #7673.

--- src/test/run-pass/issue-7673-cast-generically-implemented-trait.rs
+++ src/test/run-pass/issue-7673-cast-generically-implemented-trait.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-pretty #9253 pretty printer doesn't preserve the bounds on trait objects
-
 /*
 
 #7673 Polymorphically creating traits barely works
