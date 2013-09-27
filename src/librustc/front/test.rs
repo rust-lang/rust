319016b2586bd6643f4b5feee365e4b79d1690a0
Brian Anderson <banderson@mozilla.com>
2013-09-26 18:18:42 -0700

    Update version numbers to 0.9-pre

--- src/librustc/front/test.rs
+++ src/librustc/front/test.rs
@@ -279,7 +279,7 @@ fn mk_std(cx: &TestCtxt) -> ast::view_item {
                                             path_node(~[id_extra]),
                                             ast::DUMMY_NODE_ID))])
     } else {
-        let mi = attr::mk_name_value_item_str(@"vers", @"0.8");
+        let mi = attr::mk_name_value_item_str(@"vers", @"0.9-pre");
         ast::view_item_extern_mod(id_extra, None, ~[mi], ast::DUMMY_NODE_ID)
     };
     ast::view_item {
