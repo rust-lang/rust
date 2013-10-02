8284df9e7c3b26897d65935eaa7acee1a956a450
Huon Wilson <dbau.pp+github@gmail.com>
2013-10-02 14:43:15 +1000

    syntax: indicate an error when a macro ignores trailing tokens.
    That is, only a single expression or item gets parsed, so if there are
    any extra tokens (e.g. the start of another item/expression) the user
    should be told, rather than silently dropping them.
    
    An example:
    
        macro_rules! foo {
            () => {
                println("hi");
                println("bye);
            }
        }
    
    would expand to just `println("hi")`, which is almost certainly not
    what the programmer wanted.
    
    Fixes #8012.

---
+++ src/test/compile-fail/macro-incomplete-parse.rs
@@ -0,0 +1,26 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+macro_rules! ignored_item {
+    () => {
+        fn foo() {}
+        fn bar() {} //~ ERROR macro expansion ignores token `fn`
+    }
+}
+
+macro_rules! ignored_expr {
+    () => ( 1, 2 ) //~ ERROR macro expansion ignores token `,`
+}
+
+ignored_item!()
+
+fn main() {
+    ignored_expr!()
+}
