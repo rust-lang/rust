bd19eacf84a5c62ff806d943e22b08cb41c7c1bf
Jason Toffaletti <jason@topsy.com>
2013-10-07 00:07:04 -0700

    add multi-producer multi-consumer bounded queue to use for sleeper list

--- src/libstd/rt/sleeper_list.rs
+++ src/libstd/rt/sleeper_list.rs
@@ -11,84 +11,37 @@
 //! Maintains a shared list of sleeping schedulers. Schedulers
 //! use this to wake each other up.
 
-use container::Container;
-use vec::OwnedVector;
-use option::{Option, Some, None};
-use cell::Cell;
-use unstable::sync::{UnsafeArc, LittleLock};
 use rt::sched::SchedHandle;
+use rt::mpmc_bounded_queue::Queue;
+use option::*;
 use clone::Clone;
 
 pub struct SleeperList {
-    priv state: UnsafeArc<State>
-}
-
-struct State {
-    count: uint,
-    stack: ~[SchedHandle],
-    lock: LittleLock
+    priv q: Queue<SchedHandle>,
 }
 
 impl SleeperList {
     pub fn new() -> SleeperList {
-        SleeperList {
-            state: UnsafeArc::new(State {
-                count: 0,
-                stack: ~[],
-                lock: LittleLock::new()
-            })
-        }
+        SleeperList{q: Queue::with_capacity(8*1024)}
     }
 
-    pub fn push(&mut self, handle: SchedHandle) {
-        let handle = Cell::new(handle);
-        unsafe {
-            let state = self.state.get();
-            do (*state).lock.lock {
-                (*state).count += 1;
-                (*state).stack.push(handle.take());
-            }
-        }
+    pub fn push(&mut self, value: SchedHandle) {
+        assert!(self.q.push(value))
     }
 
     pub fn pop(&mut self) -> Option<SchedHandle> {
-        unsafe {
-            let state = self.state.get();
-            do (*state).lock.lock {
-                if !(*state).stack.is_empty() {
-                    (*state).count -= 1;
-                    Some((*state).stack.pop())
-                } else {
-                    None
-                }
-            }
-        }
+        self.q.pop()
     }
 
-    /// A pop that may sometimes miss enqueued elements, but is much faster
-    /// to give up without doing any synchronization
     pub fn casual_pop(&mut self) -> Option<SchedHandle> {
-        unsafe {
-            let state = self.state.get();
-            // NB: Unsynchronized check
-            if (*state).count == 0 { return None; }
-            do (*state).lock.lock {
-                if !(*state).stack.is_empty() {
-                    // NB: count is also protected by the lock
-                    (*state).count -= 1;
-                    Some((*state).stack.pop())
-                } else {
-                    None
-                }
-            }
-        }
+        self.q.pop()
     }
 }
 
 impl Clone for SleeperList {
     fn clone(&self) -> SleeperList {
         SleeperList {
-            state: self.state.clone()
+            q: self.q.clone()
         }
     }
 }
