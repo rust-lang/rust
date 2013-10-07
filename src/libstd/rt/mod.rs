81a1fb2931a44e47b61e00ec07aa34fee89a6c93
Jason Toffaletti <jason@topsy.com>
2013-10-07 00:07:04 -0700

    add multi-producer multi-consumer bounded queue to use for sleeper list

--- src/libstd/rt/mod.rs
+++ src/libstd/rt/mod.rs
@@ -109,6 +109,9 @@ mod message_queue;
 /// A mostly lock-free multi-producer, single consumer queue.
 mod mpsc_queue;
 
+/// A lock-free multi-producer, multi-consumer bounded queue.
+mod mpmc_bounded_queue;
+
 /// A parallel data structure for tracking sleeping schedulers.
 mod sleeper_list;
 
