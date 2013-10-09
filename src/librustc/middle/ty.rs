e538c95e4d5c7df475772639e983770e6425da32
Geoff Hill <geoff@geoffhill.org>
2013-10-09 15:36:26 -0700

    Typeck: Disallow scalar casts to bare_fn.
    Bare functions are another example of a scalar but non-numeric
    type (like char) that should be handled separately in casts.
    This disallows expressions like `0 as extern "Rust" fn() -> int;`.
    
    It might be advantageous to allow casts between bare functions
    and raw pointers in unsafe code in the future, to pass function
    pointers between Rust and C.
    
    Closes #8728

--- src/librustc/middle/ty.rs
+++ src/librustc/middle/ty.rs
@@ -2539,6 +2539,13 @@ pub fn type_is_char(ty: t) -> bool {
     }
 }
 
+pub fn type_is_bare_fn(ty: t) -> bool {
+    match get(ty).sty {
+        ty_bare_fn(*) => true,
+        _ => false
+    }
+}
+
 pub fn type_is_fp(ty: t) -> bool {
     match get(ty).sty {
       ty_infer(FloatVar(_)) | ty_float(_) => true,
