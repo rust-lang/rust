b512f49319af3b40217a964b5f00cac987235c8e
Daniel Micay <danielmicay@gmail.com>
2013-10-14 21:51:03 -0400

    add an `abort` intrinsic
    This should be preferred to the libc `abort` function.

--- src/libstd/unstable/intrinsics.rs
+++ src/libstd/unstable/intrinsics.rs
@@ -171,6 +171,9 @@ pub trait TyVisitor {
 }
 
 extern "rust-intrinsic" {
+    /// Abort the execution of the process.
+    #[cfg(not(stage0))]
+    pub fn abort() -> !;
 
     /// Atomic compare and exchange, sequentially consistent.
     pub fn atomic_cxchg(dst: &mut int, old: int, src: int) -> int;
