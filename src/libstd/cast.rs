26c90a2ae78bd45213acbc1bd7f612366fe07878
Kevin Ballard <kevin@sb.org>
2013-10-20 00:06:37 -0700

    Add functions cast::transmute_slice, cast::transmute_mut_slice

--- src/libstd/cast.rs
+++ src/libstd/cast.rs
@@ -13,6 +13,7 @@
 use ptr::RawPtr;
 use mem;
 use unstable::intrinsics;
+use unstable::raw::Slice;
 
 /// Casts the value at `src` to U. The two types must have the same length.
 #[cfg(target_word_size = "32")]
@@ -111,6 +112,30 @@ pub unsafe fn transmute_mut_region<'a,'b,T>(ptr: &'a mut T) -> &'b mut T {
     transmute(ptr)
 }
 
+/// Coerce a slice of one type into a slice of another.
+/// This function modifies the length of the resulting slice appropriately.
+///
+/// Note that if the coercion leaves some bytes from the original slice un-addressable
+/// with the new slice, then this function will not be able to round-trip back to the
+/// original slice. For example, if a &[u8] of length 16 is transmuted to a &[[u8,..3]],
+/// which will have length 5, transmuting back to a &[u8] will produce a slice of length 15.
+#[inline]
+pub unsafe fn transmute_slice<'a,L,G>(thing: &'a [L]) -> &'a [G] {
+    let mut slice: Slice<G> = transmute(thing);
+    let bytes = slice.len * mem::nonzero_size_of::<L>();
+    // NB: the previous operation cannot overflow. A slice cannot possibly hold more memory than
+    // is possible to allocate, and uint can represent all valid allocation sizes.
+    slice.len = bytes / mem::nonzero_size_of::<G>();
+    transmute(slice)
+}
+
+/// Coerce a mutable slice of one type into a mutable slice of another.
+/// This function modifies the length of the resulting slice appropriately.
+#[inline]
+pub unsafe fn transmute_mut_slice<'a,L,G>(thing: &'a mut [L]) -> &'a mut [G] {
+    transmute(transmute_slice::<L,G>(thing))
+}
+
 /// Transforms lifetime of the second pointer to match the first.
 #[inline]
 pub unsafe fn copy_lifetime<'a,S,T>(_ptr: &'a S, ptr: &T) -> &'a T {
@@ -138,6 +163,7 @@ pub unsafe fn copy_lifetime_vec<'a,S,T>(_ptr: &'a [S], ptr: &T) -> &'a T {
 mod tests {
     use cast::{bump_box_refcount, transmute};
     use unstable::raw;
+    use container::Container;
 
     #[test]
     fn test_transmute_copy() {
@@ -173,6 +199,34 @@ mod tests {
     fn test_transmute2() {
         unsafe {
             assert_eq!(~[76u8], transmute(~"L"));
+        }
+    }
+
+    #[test]
+    fn test_transmute_slice() {
+        use cast::transmute_slice;
+        use unstable::raw::Vec;
+
+        unsafe {
+            let u8ary = [0u8, ..16];
+            let u32ary = [0u32, ..4];
+            assert_eq!(4, transmute_slice::<u8,u32>(u8ary).len());
+            assert_eq!(16, transmute_slice::<u32,u8>(u32ary).len());
+
+            let threeary = [[0u8, ..3], ..5];
+            assert_eq!(5, transmute_slice::<u8, [u8,..3]>(u8ary).len());
+            assert_eq!(15, transmute_slice::<[u8,..3],u8>(threeary).len());
+
+            // ensure the repr for a Vec uses nonzero sizes, since we rely on that
+            let vec: *Vec<()> = transmute(~[()]);
+            assert_eq!(1, (*vec).fill);
+            transmute::<*Vec<()>,~[()]>(vec); // don't leak it
+
+            let unitary = [(), ..16];
+            assert_eq!(16, transmute_slice::<u32,()>(u32ary).len());
+            assert_eq!(16, transmute_slice::<u8,()>(u8ary).len());
+            assert_eq!(4, transmute_slice::<(),u32>(unitary).len());
+            assert_eq!(16, transmute_slice::<(),u8>(unitary).len());
         }
     }
 }
