79c169296747987567396e53a4909338f4c9ffcb
Joshua Yanovski <pythonesque@gmail.com>
2013-10-29 23:34:43 -0700

    Special-casing iter_vec_raw to use a counter for zero-size types (Closes #9890).

---
+++ src/test/run-pass/zero-size-type-destructors.rs
@@ -0,0 +1,29 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+static mut destructions : int = 3;
+
+pub fn foo() {
+    #[unsafe_no_drop_flag]
+    struct Foo;
+
+    impl Drop for Foo {
+        fn drop(&mut self) {
+          unsafe { destructions -= 1 };
+        }
+    };
+
+    let _x = [Foo, Foo, Foo];
+}
+
+pub fn main() {
+  foo();
+  assert!((unsafe { destructions } == 0));
+}
