681fda016979786da5d60a5c6d7fcf5c6c649c86
Alex Crichton <alex@alexcrichton.com>
2013-10-31 20:47:23 -0700

    Reduce the aggressiveness of reachability
    Previously, all functions called by a reachable function were considered
    reachable, but this is only the case if the original function was possibly
    inlineable (if it's type generic or #[inline]-flagged).

---
+++ src/test/auxiliary/linkage-visibility.rs
@@ -0,0 +1,36 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::unstable::dynamic_lib::DynamicLibrary;
+
+#[no_mangle]
+pub fn foo() { bar(); }
+
+pub fn foo2<T>() {
+    fn bar2() {
+        bar();
+    }
+    bar2();
+}
+
+#[no_mangle]
+fn bar() { }
+
+#[no_mangle]
+fn baz() { }
+
+pub fn test() {
+    let lib = DynamicLibrary::open(None).unwrap();
+    unsafe {
+        assert!(lib.symbol::<int>("foo").is_ok());
+        assert!(lib.symbol::<int>("baz").is_err());
+        assert!(lib.symbol::<int>("bar").is_err());
+    }
+}
