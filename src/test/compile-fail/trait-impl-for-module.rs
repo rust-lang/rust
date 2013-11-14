e1d1ad34f7d28eeb2af2e17097615b74498a1511
Leah Hanson <astrieanna@gmail.com>
2013-11-13 21:29:37 -0600

    Fix issue #8763
    * added case & better error message for "impl trait for module"
    * used better way to print the module
    * switched from //error-pattern to //~ ERROR
    * added compile-fail test trait-impl-for-module.rs
    * revised compile-fail test trait-or-new-type-instead
        (the error message for the modified test is still unclear, but that's a different bug)
    * added FIXME to trait-or-new-type-instead

--- src/test/compile-fail/trait-impl-for-module.rs
+++ src/test/compile-fail/trait-impl-for-module.rs
@@ -1,4 +1,4 @@
-// Copyright 2012-2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,9 +8,14 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// FIXME(#8767) bad error message; Option is not a module
-impl<T> Option<T> { //~ERROR found module name used as a type
-    pub fn foo(&self) { }
+mod a {
 }
 
-fn main() { }
+trait A {
+}
+
+impl A for a { //~ERROR found module name used as a type
+}
+
+fn main() {
+}
