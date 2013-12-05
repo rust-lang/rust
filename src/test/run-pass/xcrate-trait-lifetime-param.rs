d99efe84dfd8aedec08da42d34d140991b8991c7
David Renshaw <dwrenshaw@gmail.com>
2013-12-04 21:13:31 -0500

    encode trait lifetime params in metadata to allow cross-crate usage

---
+++ src/test/run-pass/xcrate-trait-lifetime-param.rs
@@ -0,0 +1,26 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// xfail-fast
+// aux-build:xcrate-trait-lifetime-param.rs
+
+extern mod other(name = "xcrate-trait-lifetime-param");
+
+struct Reader<'a> {
+    b : &'a [u8]
+}
+
+impl <'a> other::FromBuf<'a> for Reader<'a> {
+    fn from_buf(b : &'a [u8]) -> Reader<'a> {
+        Reader { b : b }
+    }
+}
+
+pub fn main () {}
