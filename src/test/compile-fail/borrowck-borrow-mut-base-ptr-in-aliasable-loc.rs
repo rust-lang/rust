e7e9b0bb772af270d18a0ed10bbe1a0868af0c85
Niko Matsakis <niko@alum.mit.edu>
2013-12-11 06:40:37 -0500

    More small test case fixes. grr. cc #9629.

--- src/test/compile-fail/borrowck-borrow-mut-base-ptr-in-aliasable-loc.rs
+++ src/test/compile-fail/borrowck-borrow-mut-base-ptr-in-aliasable-loc.rs
@@ -1,3 +1,13 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // Test that attempt to reborrow an `&mut` pointer in an aliasable
 // location yields an error.
 //
@@ -7,7 +17,7 @@ use std::util::swap;
 
 fn foo(t0: & &mut int) {
     let t1 = t0;
-    let p: &int = &**t0; //~ ERROR cannot borrow an `&mut` in a `&` pointer
+    let p: &int = &**t0;
     **t1 = 22; //~ ERROR cannot assign
 }
 
