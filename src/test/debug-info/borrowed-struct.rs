0be3279a0db157f913f927d08b02554d897f7830
Brian Anderson <banderson@mozilla.com>
2013-12-13 15:45:08 -0800

    Ignore failing Android debug-info tests. #10381

--- src/test/debug-info/borrowed-struct.rs
+++ src/test/debug-info/borrowed-struct.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// xfail-android: FIXME(#10381)
+
 // compile-flags:-Z extra-debug-info
 // debugger:rbreak zzz
 // debugger:run
