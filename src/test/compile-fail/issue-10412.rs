5a52f1fd18cb6dbb2a002698b4df1b4a6b3afb43
Alex Crichton <alex@alexcrichton.com>
2013-12-16 17:04:02 -0800

    Adding tests for closed issues
    Closes #5806
    Closes #5950
    Closes #7178
    Closes #8259
    Closes #8578
    Closes #8851
    Closes #9129
    Closes #10412

---
+++ src/test/compile-fail/issue-10412.rs
@@ -0,0 +1,31 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+
+trait Serializable<'self, T> { //~ ERROR: no longer a special lifetime
+    fn serialize(val : &'self T) -> ~[u8];
+    fn deserialize(repr : &[u8]) -> &'self T;
+}
+
+impl<'self> Serializable<str> for &'self str {
+    fn serialize(val : &'self str) -> ~[u8] {
+        ~[1]
+    }
+    fn deserialize(repr: &[u8]) -> &'self str {
+        "hi"
+    }
+}
+
+fn main() {
+    println("hello");
+    let x = ~"foo";
+    let y = x;
+    println(y);
+}
---
+++ src/test/run-pass/issue-9129.rs
@@ -0,0 +1,41 @@
+// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// xfail-pretty
+
+#[feature(managed_boxes, macro_rules)];
+
+pub trait bomb { fn boom(@self, Ident); }
+pub struct S;
+impl bomb for S { fn boom(@self, _: Ident) { } }
+
+pub struct Ident { name: uint }
+
+// macro_rules! int3( () => ( unsafe { asm!( "int3" ); } ) )
+macro_rules! int3( () => ( { } ) )
+
+fn Ident_new() -> Ident {
+    int3!();
+    Ident {name: 0x6789ABCD }
+}
+
+pub fn light_fuse(fld: @bomb) {
+    int3!();
+    let f = || {
+        int3!();
+        fld.boom(Ident_new()); // *** 1
+    };
+    f();
+}
+
+pub fn main() {
+    let b = @S as @bomb;
+    light_fuse(b);
+}
