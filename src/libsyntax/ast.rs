25e334169ab0ad91ef389bdc592e8170a29425f9
Patrick Walton <pcwalton@mimiga.net>
2013-12-27 16:39:39 -0800

    libsyntax: De-`@mut` `SCTable::rename_memo`

--- src/libsyntax/ast.rs
+++ src/libsyntax/ast.rs
@@ -91,7 +91,7 @@ pub type SyntaxContext = u32;
 pub struct SCTable {
     table: RefCell<~[SyntaxContext_]>,
     mark_memo: RefCell<HashMap<(SyntaxContext,Mrk),SyntaxContext>>,
-    rename_memo: HashMap<(SyntaxContext,Ident,Name),SyntaxContext>
+    rename_memo: RefCell<HashMap<(SyntaxContext,Ident,Name),SyntaxContext>>,
 }
 
 // NB: these must be placed in any SCTable...
