d04564157f448fa97d5a928e414b1e33202719ce
kud1ing <github@kudling.de>
2014-01-12 10:35:10 +0100

    more backticks

--- src/test/compile-fail/binop-bitxor-str.rs
+++ src/test/compile-fail/binop-bitxor-str.rs
@@ -8,6 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern:^ cannot be applied to type `~str`
+// error-pattern:`^` cannot be applied to type `~str`
 
 fn main() { let x = ~"a" ^ ~"b"; }
--- src/test/compile-fail/estr-subtyping.rs
+++ src/test/compile-fail/estr-subtyping.rs
@@ -16,19 +16,19 @@ fn wants_slice(x: &str) { }
 
 fn has_box(x: @str) {
    wants_box(x);
-   wants_uniq(x); //~ ERROR str storage differs: expected ~ but found @
+   wants_uniq(x); //~ ERROR str storage differs: expected `~` but found `@`
    wants_slice(x);
 }
 
 fn has_uniq(x: ~str) {
-   wants_box(x); //~ ERROR str storage differs: expected @ but found ~
+   wants_box(x); //~ ERROR str storage differs: expected `@` but found `~`
    wants_uniq(x);
    wants_slice(x);
 }
 
 fn has_slice(x: &str) {
-   wants_box(x); //~ ERROR str storage differs: expected @ but found &
-   wants_uniq(x); //~ ERROR str storage differs: expected ~ but found &
+   wants_box(x); //~ ERROR str storage differs: expected `@` but found `&`
+   wants_uniq(x); //~ ERROR str storage differs: expected `~` but found `&`
    wants_slice(x);
 }
 
