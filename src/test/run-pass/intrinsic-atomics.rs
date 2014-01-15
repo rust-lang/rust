c027bc9f1666688602f22f3c7a3539a0d7c6cff4
Alex Crichton <alex@alexcrichton.com>
2014-01-15 15:32:44 -0800

    Add an AtomicU64 type to std::sync::atomics
    This also generalizes all atomic intrinsics over T so we'll be able to add u8
    atomics if we really feel the need to (do we really want to?)

--- src/test/run-pass/intrinsic-atomics.rs
+++ src/test/run-pass/intrinsic-atomics.rs
@@ -10,27 +10,27 @@
 
 mod rusti {
     extern "rust-intrinsic" {
-        pub fn atomic_cxchg(dst: &mut int, old: int, src: int) -> int;
-        pub fn atomic_cxchg_acq(dst: &mut int, old: int, src: int) -> int;
-        pub fn atomic_cxchg_rel(dst: &mut int, old: int, src: int) -> int;
+        pub fn atomic_cxchg<T>(dst: &mut T, old: T, src: T) -> T;
+        pub fn atomic_cxchg_acq<T>(dst: &mut T, old: T, src: T) -> T;
+        pub fn atomic_cxchg_rel<T>(dst: &mut T, old: T, src: T) -> T;
 
-        pub fn atomic_load(src: &int) -> int;
-        pub fn atomic_load_acq(src: &int) -> int;
+        pub fn atomic_load<T>(src: &T) -> T;
+        pub fn atomic_load_acq<T>(src: &T) -> T;
 
-        pub fn atomic_store(dst: &mut int, val: int);
-        pub fn atomic_store_rel(dst: &mut int, val: int);
+        pub fn atomic_store<T>(dst: &mut T, val: T);
+        pub fn atomic_store_rel<T>(dst: &mut T, val: T);
 
-        pub fn atomic_xchg(dst: &mut int, src: int) -> int;
-        pub fn atomic_xchg_acq(dst: &mut int, src: int) -> int;
-        pub fn atomic_xchg_rel(dst: &mut int, src: int) -> int;
+        pub fn atomic_xchg<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xchg_acq<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xchg_rel<T>(dst: &mut T, src: T) -> T;
 
-        pub fn atomic_xadd(dst: &mut int, src: int) -> int;
-        pub fn atomic_xadd_acq(dst: &mut int, src: int) -> int;
-        pub fn atomic_xadd_rel(dst: &mut int, src: int) -> int;
+        pub fn atomic_xadd<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xadd_acq<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xadd_rel<T>(dst: &mut T, src: T) -> T;
 
-        pub fn atomic_xsub(dst: &mut int, src: int) -> int;
-        pub fn atomic_xsub_acq(dst: &mut int, src: int) -> int;
-        pub fn atomic_xsub_rel(dst: &mut int, src: int) -> int;
+        pub fn atomic_xsub<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xsub_acq<T>(dst: &mut T, src: T) -> T;
+        pub fn atomic_xsub_rel<T>(dst: &mut T, src: T) -> T;
     }
 }
 
