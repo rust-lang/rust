351d0ffaa1027a3173c1a95e006c2efcc3d2ac02
Alex Crichton <alex@alexcrichton.com>
2014-01-16 10:12:31 -0800

    Force all lang items to be reachable
    This prevents linker errors as found in #11591
    
    Closes #11591

---
+++ src/test/run-pass/lang-item-public.rs
@@ -0,0 +1,38 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// aux-build:lang-item-public.rs
+// ignore-fast
+// ignore-android
+
+#[no_std];
+
+extern crate lang_lib = "lang-item-public";
+
+#[cfg(target_os = "linux")]
+#[link(name = "c")]
+extern {}
+
+#[cfg(target_os = "android")]
+#[link(name = "c")]
+extern {}
+
+#[cfg(target_os = "freebsd")]
+#[link(name = "execinfo")]
+extern {}
+
+#[cfg(target_os = "macos")]
+#[link(name = "System")]
+extern {}
+
+#[start]
+fn main(_: int, _: **u8) -> int {
+    1 % 1
+}
