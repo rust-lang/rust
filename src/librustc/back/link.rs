88e3b67ec4fd620d623cc61121b1c44ad3097c02
Alex Crichton <alex@alexcrichton.com>
2014-01-15 16:54:11 -0800

    Enable the +v7 feature on Android by default
    With the recently added double word CAS functionality on 32-bit ARM (enabled via
    a 64-bit atomic instruction in LLVM IR), without some extra features enabled
    LLVM lowers code to function calls which emulate atomic instructions. With the
    v7 feature enabled, proper 64-bit CAS instructions are used on 32-bit arm.
    
    I've been told that v7 for arm is what we should have been doing anyway. This is
    overridable by providing some other non-empty feature string.

--- src/librustc/back/link.rs
+++ src/librustc/back/link.rs
@@ -97,6 +97,7 @@ pub mod write {
     use lib::llvm::{ModuleRef, TargetMachineRef, PassManagerRef};
     use lib;
     use util::common::time;
+    use syntax::abi;
 
     use std::c_str::ToCStr;
     use std::io;
@@ -104,6 +105,24 @@ pub mod write {
     use std::path::Path;
     use std::run;
     use std::str;
+
+    // On android, we by default compile for armv7 processors. This enables
+    // things like double word CAS instructions (rather than emulating them)
+    // which are *far* more efficient. This is obviously undesirable in some
+    // cases, so if any sort of target feature is specified we don't append v7
+    // to the feature list.
+    fn target_feature<'a>(sess: &'a Session) -> &'a str {
+        match sess.targ_cfg.os {
+            abi::OsAndroid => {
+                if "" == sess.opts.target_feature {
+                    "+v7"
+                } else {
+                    sess.opts.target_feature.as_slice()
+                }
+            }
+            _ => sess.opts.target_feature.as_slice()
+        }
+    }
 
     pub fn run_passes(sess: Session,
                       trans: &CrateTranslation,
@@ -130,7 +149,7 @@ pub mod write {
 
             let tm = sess.targ_cfg.target_strs.target_triple.with_c_str(|T| {
                 sess.opts.target_cpu.with_c_str(|CPU| {
-                    sess.opts.target_feature.with_c_str(|Features| {
+                    target_feature(&sess).with_c_str(|Features| {
                         llvm::LLVMRustCreateTargetMachine(
                             T, CPU, Features,
                             lib::llvm::CodeModelDefault,
