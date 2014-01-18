22117259c05075c162b0ae7282e0e3043f95cf64
Corey Richardson <corey@octayn.net>
2014-01-18 15:20:55 -0500

    maybe fix windows

--- src/libgreen/stack.rs
+++ src/libgreen/stack.rs
@@ -94,7 +94,7 @@ fn protect_last_page(stack: &MemoryMap) -> bool {
         let last_page = stack.data as *mut c_void;
         let old_prot: DWORD = 0;
         VirtualProtect(last_page, page_size() as SIZE_T, PAGE_NOACCESS,
-                       &mut old_prot as *mut LPDWORD) != 0
+                       &mut old_prot as LPDWORD) != 0
     }
 }
 
