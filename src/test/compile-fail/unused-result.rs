70c1efd1f6fbc0eabe165aeb52619ac9cf5c6110
Alex Crichton <alex@alexcrichton.com>
2014-01-23 09:43:29 -0800

    Implement an unused_result lint
    I attempted to implement the lint in two steps. My first attempt was a
    default-warn lint about *all* unused results. While this attempt did indeed find
    many possible bugs, I felt that the false-positive rate was too high to be
    turned on by default for all of Rust.
    
    My second attempt was to make unused-result a default-allow lint, but allow
    certain types to opt-in to the notion of "you must use this". For example, the
    Result type is now flagged with #[must_use]. This lint about "must use" types is
    warn by default (it's different from unused-result).
    
    The unused_must_use lint had a 100% hit rate in the compiler, but there's not
    that many places that return Result right now. I believe that this lint is a
    crucial step towards moving away from conditions for I/O (because all I/O will
    return Result by default). I'm worried that this lint is a little too specific
    to Result itself, but I believe that the false positive rate for the
    unused_result lint is too high to make it useful when turned on by default.

---
+++ src/test/compile-fail/unused-result.rs
@@ -0,0 +1,40 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[deny(unused_result, unused_must_use)];
+#[allow(dead_code)];
+
+#[must_use]
+enum MustUse { Test }
+
+fn foo<T>() -> T { fail!() }
+
+fn bar() -> int { return foo::<int>(); }
+fn baz() -> MustUse { return foo::<MustUse>(); }
+
+#[allow(unused_result)]
+fn test() {
+    foo::<int>();
+    foo::<MustUse>(); //~ ERROR: unused result which must be used
+}
+
+#[allow(unused_result, unused_must_use)]
+fn test2() {
+    foo::<int>();
+    foo::<MustUse>();
+}
+
+fn main() {
+    foo::<int>(); //~ ERROR: unused result
+    foo::<MustUse>(); //~ ERROR: unused result which must be used
+
+    let _ = foo::<int>();
+    let _ = foo::<MustUse>();
+}
