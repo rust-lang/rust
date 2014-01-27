be4a11cf51df0b03b7c624c01eacafd2fa27f1e9
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-01-27 21:13:47 +0100

    test case for issue #10031.

---
+++ src/test/run-pass/issue-10031.rs
@@ -0,0 +1,17 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// xfail-fast - check-fast doesn't understand aux-build
+// aux-build:issue_10031_aux.rs
+extern mod issue_10031_aux;
+
+pub fn main() {
+    let _ = issue_10031_aux::Wrap(());
+}
