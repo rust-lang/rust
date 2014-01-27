8cd935f52a9af8620f608e1baad94282f038a864
Alex Crichton <alex@alexcrichton.com>
2014-01-27 12:45:48 -0800

    Upgrade LLVM
    This upgrade brings commit by @eddyb to help optimizations of virtual calls in
    a few places (https://github.com/llvm-mirror/llvm/commit/6d2bd95) as well as a
    commit by @c-a to *greatly* improve the runtime of the optimization passes
    (https://github.com/rust-lang/llvm/pull/3).
    
    Nice work to these guys!

--- src/librustc/lib/llvm.rs
+++ src/librustc/lib/llvm.rs
@@ -1762,6 +1762,8 @@ pub mod llvm {
         pub fn LLVMRustArchiveReadSection(AR: ArchiveRef, name: *c_char,
                                           out_len: *mut size_t) -> *c_char;
         pub fn LLVMRustDestroyArchive(AR: ArchiveRef);
+
+        pub fn LLVMRustSetDLLExportStorageClass(V: ValueRef);
     }
 }
 
