9a3583f06d7086c8eca70ae5770ecce1a74680be
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-01-30 16:35:17 +1100

    Make next_power_of_two generic for unsigned integers
    Also rename `next_power_of_two_opt` to `checked_next_power_of_two`.

--- src/libstd/hashmap.rs
+++ src/libstd/hashmap.rs
@@ -388,7 +388,7 @@ impl<K: Hash + Eq, V> HashMap<K, V> {
     pub fn reserve_at_least(&mut self, n: uint) {
         if n > self.buckets.len() {
             let buckets = n * 4 / 3 + 1;
-            self.resize(uint::next_power_of_two(buckets));
+            self.resize(num::next_power_of_two(buckets));
         }
     }
 
