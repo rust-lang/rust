d2802db1610350238f5ad5ff57fb9f68e03db1d8
David Manescu <david.manescu@gmail.com>
2014-01-30 23:36:05 +1100

    Handle attributes on cross-crate tuple-structs correctly
    Fixes #11741

--- src/test/compile-fail/simd-experimental.rs
+++ src/test/compile-fail/simd-experimental.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// xfail-test FIXME #11741 tuple structs ignore stability attributes
-
 #[deny(experimental)];
 
 use std::unstable::simd;
