2053b695953e530ebddeba2b8c7547858bc2e43d
Alex Crichton <alex@alexcrichton.com>
2014-01-30 14:37:30 -0800

    green: Remove io_error usage

--- src/libgreen/macros.rs
+++ src/libgreen/macros.rs
@@ -56,16 +56,17 @@ pub fn dumb_println(args: &fmt::Arguments) {
 
     struct Stderr;
     impl io::Writer for Stderr {
-        fn write(&mut self, data: &[u8]) {
+        fn write(&mut self, data: &[u8]) -> io::IoResult<()> {
             unsafe {
                 libc::write(libc::STDERR_FILENO,
                             data.as_ptr() as *libc::c_void,
                             data.len() as libc::size_t);
             }
+            Ok(()) // just ignore the result
         }
     }
     let mut w = Stderr;
-    fmt::writeln(&mut w as &mut io::Writer, args);
+    let _ = fmt::writeln(&mut w as &mut io::Writer, args);
 }
 
 pub fn abort(msg: &str) -> ! {
