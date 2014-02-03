8a1dda92ba348c93540fbe06f1a9f17111d1a8f3
Alex Crichton <alex@alexcrichton.com>
2014-02-03 10:12:26 -0800

    Adding tests for closed issues
    Closes #5521
    Closes #9396
    Closes #10714

---
+++ src/test/run-pass/issue-5521.rs
@@ -0,0 +1,26 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// aux-build:issue-5521.rs
+// xfail-fast
+
+#[feature(managed_boxes)];
+
+extern mod foo = "issue-5521";
+
+fn foo(a: foo::map) {
+    if false {
+        fail!();
+    } else {
+        let _b = a.get(&2);
+    }
+}
+
+fn main() {}
