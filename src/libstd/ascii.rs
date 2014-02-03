40583775e3093cb1e3226bc64c157587b639af0a
chromatic <chromatic@wgz.org>
2014-02-02 16:32:52 -0800

    Removed prelude::* from libstd files.
    This replaces the imports from the prelude with the re-exported symbols.

--- src/libstd/ascii.rs
+++ src/libstd/ascii.rs
@@ -18,9 +18,9 @@ use str::OwnedStr;
 use container::Container;
 use cast;
 use iter::Iterator;
-use vec::{ImmutableVector, MutableVector, Vector};
+use vec::{ImmutableVector,MutableVector,Vector};
 use to_bytes::IterBytes;
-use option::{Option, Some, None};
+use option::{Option,Some,None};
 
 /// Datatype to hold one ascii character. It wraps a `u8`, with the highest bit always zero.
 #[deriving(Clone, Eq, Ord, TotalOrd, TotalEq)]
