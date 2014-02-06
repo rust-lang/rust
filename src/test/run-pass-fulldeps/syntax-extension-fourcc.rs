d75480e28251f741facf1e6f7d0cb3e59272464a
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-02-06 17:41:49 -0200

    Default fourcc! to big-endian.
    It was decided that a consistent result across platforms would be the
    most useful and least surprising. A "target" option has been added to
    get the old behaviour of using the target platform's endianess.

--- src/test/run-pass-fulldeps/syntax-extension-fourcc.rs
+++ src/test/run-pass-fulldeps/syntax-extension-fourcc.rs
@@ -19,22 +19,25 @@
 extern mod fourcc;
 
 static static_val: u32 = fourcc!("foo ");
-static static_val_le: u32 = fourcc!("foo ", little);
 static static_val_be: u32 = fourcc!("foo ", big);
+static static_val_le: u32 = fourcc!("foo ", little);
+static static_val_target: u32 = fourcc!("foo ", target);
 
 fn main() {
-    let val = fourcc!("foo ");
-    let exp = if cfg!(target_endian = "big") { 0x666f6f20u32 } else { 0x206f6f66u32 };
-    assert_eq!(val, exp);
-
     let val = fourcc!("foo ", big);
     assert_eq!(val, 0x666f6f20u32);
+    assert_eq!(val, fourcc!("foo "));
 
     let val = fourcc!("foo ", little);
     assert_eq!(val, 0x206f6f66u32);
 
+    let val = fourcc!("foo ", target);
     let exp = if cfg!(target_endian = "big") { 0x666f6f20u32 } else { 0x206f6f66u32 };
-    assert_eq!(static_val, exp);
-    assert_eq!(static_val_le, 0x206f6f66u32);
+    assert_eq!(val, exp);
+
     assert_eq!(static_val_be, 0x666f6f20u32);
+    assert_eq!(static_val, static_val_be);
+    assert_eq!(static_val_le, 0x206f6f66u32);
+    let exp = if cfg!(target_endian = "big") { 0x666f6f20u32 } else { 0x206f6f66u32 };
+    assert_eq!(static_val_target, exp);
 }
