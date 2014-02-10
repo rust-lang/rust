f5a2332e597d16f31cd1e32cbbf6edf320c0eee5
mr.Shu <mr@shu.io>
2014-02-10 15:36:31 +0100

    Changed NonCamelCaseTypes lint to warn by default
    Added allow(non_camel_case_types) to librustc where necesary
    
    Tried to fix problems with non_camel_case_types outside rustc
    
    fixed failing tests
    
    Docs updated
    
    Moved #[allow(non_camel_case_types)] a level higher.
    
    markdown.rs reverted
    
    Fixed timer that was failing tests
    
    Fixed another timer

--- src/libnative/io/file.rs
+++ src/libnative/io/file.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -9,6 +9,8 @@
 // except according to those terms.
 
 //! Blocking posix-based file I/O
+
+#[allow(non_camel_case_types)];
 
 use std::sync::arc::UnsafeArc;
 use std::c_str::CString;
--- src/librustc/middle/trans/type_of.rs
+++ src/librustc/middle/trans/type_of.rs
@@ -8,6 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#[allow(non_camel_case_types)];
 
 use middle::trans::adt;
 use middle::trans::common::*;
