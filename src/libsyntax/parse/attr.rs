e21c2b1a3c5610ce5319c36aeff88c194196e78d
Daniel Fagnan <dnfagnan@gmail.com>
2014-02-25 19:27:32 -0700

    Fixed formatting error. Commented out the warnings for the time being.

--- src/libsyntax/parse/attr.rs
+++ src/libsyntax/parse/attr.rs
@@ -92,7 +92,8 @@ impl ParserAttr for Parser {
                         self.fatal("An inner attribute was not permitted in this context.");
                     }
                 } else {
-                    self.warn("The syntax for inner attributes have changed. Use `#![lang(foo)]` instead.");
+                    //self.warn("The syntax for inner attributes have changed.
+                    //    Use `#![lang(foo)]` instead.");
                 }
 
                 // #![lang(foo)]
@@ -119,7 +120,7 @@ impl ParserAttr for Parser {
             // need to bump the token.
             ast::AttrInner
         } else if permit_inner && self.token == token::SEMI {
-            self.warn("This uses the old attribute syntax. Semicolons are not longer required.");
+            //self.warn("This uses the old attribute syntax. Semicolons are not longer required.");
             self.bump();
             ast::AttrInner
         } else {
