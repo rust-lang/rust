6f5ae1679a8302f6f5795b0c1572fbe58b91e071
Alex Crichton <alex@alexcrichton.com>
2014-03-04 17:27:43 -0800

    native: Stop using readdir()
    This function is not threadsafe, and is deprecated in favor of the threadsafe
    readdir_r variant.
    
    Closes #12692

--- src/rt/rust_builtin.c
+++ src/rt/rust_builtin.c
@@ -279,9 +279,14 @@ rust_opendir(char *dirname) {
     return opendir(dirname);
 }
 
-struct dirent*
-rust_readdir(DIR *dirp) {
-    return readdir(dirp);
+int
+rust_readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result) {
+    return readdir_r(dirp, entry, result);
+}
+
+int
+rust_dirent_t_size() {
+    return sizeof(struct dirent);
 }
 
 #else
@@ -292,6 +297,10 @@ rust_opendir() {
 
 void
 rust_readdir() {
+}
+
+void
+rust_dirent_t_size() {
 }
 
 #endif
