d3393506ccaba4bc897f63a52f21ec3455b2ad3b
Alex Crichton <alex@alexcrichton.com>
2014-03-06 09:55:35 -0800

    test: Add some tests for closed issues
    Closes #6738
    Closes #7061
    Closes #7899
    Closes #9719
    Closes #10028
    Closes #10228
    Closes #10401
    Closes #11192
    Closes #11508
    Closes #11529
    Closes #11873
    Closes #11925

---
+++ src/test/auxiliary/issue-11529.rs
@@ -0,0 +1,11 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+pub struct A<'a>(&'a int);
---
+++ src/test/run-pass/issue-9719.rs
@@ -0,0 +1,43 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+mod a {
+    pub enum Enum<T> {
+        A(T),
+    }
+
+    pub trait X {}
+    impl X for int {}
+
+    pub struct Z<'a>(Enum<&'a X>);
+    fn foo() { let x = 42; let z = Z(A(&x as &X)); let _ = z; }
+}
+
+mod b {
+    trait X {}
+    impl X for int {}
+    struct Y<'a>{
+        x:Option<&'a X>,
+    }
+
+    fn bar() {
+        let x = 42;
+        let _y = Y { x: Some(&x as &X) };
+    }
+}
+
+mod c {
+    pub trait X { fn f(&self); }
+    impl X for int { fn f(&self) {} }
+    pub struct Z<'a>(Option<&'a X>);
+    fn main() { let x = 42; let z = Z(Some(&x as &X)); let _ = z; }
+}
+
+pub fn main() {}
