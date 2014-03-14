42847c8b1e076172bac9873f769f722e19ef945f
Brian Anderson <banderson@mozilla.com>
2014-03-13 17:30:33 -0700

    Use ignore-freebsd for tests broken on FreeBSD

--- src/test/run-pass/class-cast-to-trait.rs
+++ src/test/run-pass/class-cast-to-trait.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// ignore-test FIXME fails on BSD
+// ignore-freebsd FIXME fails on BSD
 
 #[feature(managed_boxes)];
 
