cda33346d0e9e4d1ff26163583831f96d42a4bc0
Huon Wilson <dbau.pp+github@gmail.com>
2014-03-18 23:14:08 +1100

    syntax: allow `trace_macros!` and `log_syntax!` in item position.
    Previously
    
        trace_macros!(true)
        fn main() {}
    
    would complain about `trace_macros` being an expression macro in item
    position. This is a pointless limitation, because the macro is purely
    compile-time, with no runtime effect. (And similarly for log_syntax.)
    
    This also changes the behaviour of `trace_macros!` very slightly, it
    used to be equivalent to
    
        macro_rules! trace_macros {
            (true $($_x: tt)*) => { true };
            (false $($_x: tt)*) => { false }
        }
    
    I.e. you could invoke it with arbitrary trailing arguments, which were
    ignored. It is changed to accept only exactly `true` or `false` (with no
    trailing arguments) and expands to `()`.

--- src/libsyntax/ext/trace_macros.rs
+++ src/libsyntax/ext/trace_macros.rs
@@ -12,33 +12,21 @@ use ast;
 use codemap::Span;
 use ext::base::ExtCtxt;
 use ext::base;
-use parse::lexer::{new_tt_reader, Reader};
-use parse::parser::Parser;
-use parse::token::keywords;
+use parse::token::{keywords, is_keyword};
 
 pub fn expand_trace_macros(cx: &mut ExtCtxt,
                            sp: Span,
                            tt: &[ast::TokenTree])
                         -> base::MacResult {
-    let sess = cx.parse_sess();
-    let cfg = cx.cfg();
-    let tt_rdr = new_tt_reader(&cx.parse_sess().span_diagnostic,
-                               None,
-                               tt.iter().map(|x| (*x).clone()).collect());
-    let mut rust_parser = Parser(sess, cfg.clone(), tt_rdr.dup());
-
-    if rust_parser.is_keyword(keywords::True) {
-        cx.set_trace_macros(true);
-    } else if rust_parser.is_keyword(keywords::False) {
-        cx.set_trace_macros(false);
-    } else {
-        cx.span_err(sp, "trace_macros! only accepts `true` or `false`");
-        return base::MacResult::dummy_expr(sp);
+    match tt {
+        [ast::TTTok(_, ref tok)] if is_keyword(keywords::True, tok) => {
+            cx.set_trace_macros(true);
+        }
+        [ast::TTTok(_, ref tok)] if is_keyword(keywords::False, tok) => {
+            cx.set_trace_macros(false);
+        }
+        _ => cx.span_err(sp, "trace_macros! accepts only `true` or `false`"),
     }
 
-    rust_parser.bump();
-
-    let mut rust_parser = Parser(sess, cfg, tt_rdr.dup());
-    let result = rust_parser.parse_expr();
-    base::MRExpr(result)
+    base::MacResult::dummy_any(sp)
 }
