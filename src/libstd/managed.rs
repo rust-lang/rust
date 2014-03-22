f5abd164243a6edf141807567d4c5064d03b3900
Daniel Micay <danielmicay@gmail.com>
2014-03-22 18:45:47 -0400

    make std::managed private
    This removes two tests built on `managed::refcount`, but these issues
    are well-covered elsewhere for non-managed types.

--- src/libstd/managed.rs
+++ src/libstd/managed.rs
@@ -12,13 +12,6 @@
 
 #[cfg(not(test))] use cmp::*;
 
-/// Returns the refcount of a shared box (as just before calling this)
-#[inline]
-pub fn refcount<T>(t: @T) -> uint {
-    use raw::Repr;
-    unsafe { (*t.repr()).ref_count - 1 }
-}
-
 /// Determine if two shared boxes point to the same object
 #[inline]
 pub fn ptr_eq<T>(a: @T, b: @T) -> bool {
@@ -65,15 +58,4 @@ fn test() {
     assert!((ptr_eq::<int>(y, y)));
     assert!((!ptr_eq::<int>(x, y)));
     assert!((!ptr_eq::<int>(y, x)));
-}
-
-#[test]
-fn refcount_test() {
-    use clone::Clone;
-
-    let x = @3;
-    assert_eq!(refcount(x), 1);
-    let y = x.clone();
-    assert_eq!(refcount(x), 2);
-    assert_eq!(refcount(y), 2);
 }
