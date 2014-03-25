2bebe7ac467f235f3195d9d887e5bdab99f8fc0e
Sven <bvssvni@gmail.com>
2014-03-25 09:18:47 +0100

    Fixed camel case algorithm.

---
+++ src/test/run-pass/lint-non-camel-case-with-numbers-after-underscore.rs
@@ -0,0 +1,19 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// This is ok because we often use underscores to separate numbers
+
+#[forbid(non_camel_case_types)];
+
+type Foo_1_2 = int;
+type _Bar = int;
+type __Baz__1 = int;
+
+pub fn main() { }
