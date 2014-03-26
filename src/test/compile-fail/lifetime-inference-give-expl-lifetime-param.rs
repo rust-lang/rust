6c45dbe2458842ad07783271415656e47a8c0933
Kiet Tran <ktt3ja@gmail.com>
2014-03-26 19:12:50 -0400

    Support lifetime suggestion for method
    This includes a change to the way lifetime names are generated. Say we
    figure that `[#0, 'a, 'b]` have to be the same lifetimes, then instead
    of just generating a new lifetime `'c` like before to replace them, we
    would reuse `'a`. This is done so that when the lifetime name comes
    from an impl, we don't give something that's completely off, and we
    don't have to do much work to figure out where the name came from. For
    example, for the following code snippet:
    
    ```rust
    struct Baz<'x> {
        bar: &'x int
    }
    
    impl<'x> Baz<'x> {
        fn baz1(&self) -> &int {
            self.bar
        }
    }
    ```
    
    `[#1, 'x]` (where `#1` is BrAnon(1) and refers to lifetime of `&int`)
    have to be marked the same lifetime. With the old method, we would
    generate a new lifetime `'a` and suggest `fn baz1(&self) -> &'a int`
    or `fn baz1<'a>(&self) -> &'a int`, both of which are wrong.

--- src/test/compile-fail/lifetime-inference-give-expl-lifetime-param.rs
+++ src/test/compile-fail/lifetime-inference-give-expl-lifetime-param.rs
@@ -17,7 +17,7 @@ fn foo1(x: &Foo) -> &int {
 }
 
 fn foo2<'a, 'b>(x: &'a Foo) -> &'b int {
-//~^ NOTE: consider using an explicit lifetime parameter as shown: fn foo2<'c>(x: &'c Foo) -> &'c int
+//~^ NOTE: consider using an explicit lifetime parameter as shown: fn foo2<'a>(x: &'a Foo) -> &'a int
     &x.bar //~ ERROR: cannot infer
 }
 
@@ -28,7 +28,7 @@ fn foo3(x: &Foo) -> (&int, &int) {
 }
 
 fn foo4<'a, 'b>(x: &'a Foo) -> (&'b int, &'a int, &int) {
-//~^ NOTE: consider using an explicit lifetime parameter as shown: fn foo4<'c>(x: &'c Foo) -> (&'c int, &'c int, &'c int)
+//~^ NOTE: consider using an explicit lifetime parameter as shown: fn foo4<'a>(x: &'a Foo) -> (&'a int, &'a int, &'a int)
     (&x.bar, &x.bar, &x.bar) //~ ERROR: cannot infer
     //~^ ERROR: cannot infer
 }
@@ -48,7 +48,7 @@ fn bar1(x: &Bar) -> (&int, &int, &int) {
 }
 
 fn bar2<'a, 'b, 'c>(x: &Bar<'a, 'b, 'c>) -> (&int, &int, &int) {
-//~^ NOTE: consider using an explicit lifetime parameter as shown: fn bar2<'d, 'e, 'a, 'c>(x: &'e Bar<'a, 'd, 'c>) -> (&'d int, &'e int, &'e int)
+//~^ NOTE: consider using an explicit lifetime parameter as shown: fn bar2<'d, 'a, 'b, 'c>(x: &'d Bar<'a, 'b, 'c>) -> (&'b int, &'d int, &'d int)
     (x.bar, &x.baz, &x.baz) //~ ERROR: mismatched types
     //~^ ERROR: cannot infer
     //~^^ ERROR: cannot infer
@@ -62,8 +62,28 @@ fn cat<'x>(x: Cat<'x, Dog>) -> &int {
 }
 
 fn cat2<'x, 'y>(x: Cat<'x, Dog<'y>>) -> &int {
-//~^ NOTE: consider using an explicit lifetime parameter as shown: fn cat2<'a, 'x>(x: Cat<'x, Dog<'a>>) -> &'a int
+//~^ NOTE: consider using an explicit lifetime parameter as shown: fn cat2<'x, 'y>(x: Cat<'x, Dog<'y>>) -> &'y int
     x.t.dog //~ ERROR: mismatched types
+}
+
+struct Baz<'x> {
+    bar: &'x int
+}
+
+impl<'x> Baz<'x> {
+    fn baz1(&self) -> &int {
+    //~^ NOTE: consider using an explicit lifetime parameter as shown: fn baz1(&self) -> &'x int
+        self.bar //~ ERROR: mismatched types
+    }
+}
+
+impl<'a> Baz<'a> {
+    fn baz2(&self, x: &int) -> (&int, &int) {
+    //~^ NOTE: consider using an explicit lifetime parameter as shown: fn baz2<'b>(&self, x: &'b int) -> (&'a int, &'b int)
+        (self.bar, x) //~ ERROR: cannot infer
+        //~^ ERROR: mismatched types
+        //~^^ ERROR: mismatched types
+    }
 }
 
 fn main() {}
