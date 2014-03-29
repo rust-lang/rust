ecacca3ab425ebdffa2119e8b93d061e5095709c
Richo Healey <richo@psych0tik.net>
2014-03-29 23:33:26 +1100

    Fix downstream users of enum_set

--- src/librustc/middle/ty.rs
+++ src/librustc/middle/ty.rs
@@ -54,7 +54,7 @@ use syntax::{ast, ast_map};
 use syntax::owned_slice::OwnedSlice;
 use syntax::abi::AbiSet;
 use syntax;
-use collections::enum_set::{EnumSet, CLike};
+use std::enum_set::{EnumSet, CLike};
 
 pub type Disr = u64;
 
