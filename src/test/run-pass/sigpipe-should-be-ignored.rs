9d6605c5316e49633236ecd1025dacc05a618868
Alex Crichton <alex@alexcrichton.com>
2014-04-03 13:02:50 -0700

    Remove ignore-fast that has cropped up

--- src/test/run-pass/sigpipe-should-be-ignored.rs
+++ src/test/run-pass/sigpipe-should-be-ignored.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// ignore-fast
-
 // Be sure that when a SIGPIPE would have been received that the entire process
 // doesn't die in a ball of fire, but rather it's gracefully handled.
 
