e79a7809fa9576d3d027ea2f7c63a41ee6ccd325
Richard Diamond <wichard@hahbee.co>
2014-04-10 19:01:25 -0500

    Add implementations of FromStr for Os && Architecture. Additionally added some deriving types to both.

--- src/libsyntax/abi.rs
+++ src/libsyntax/abi.rs
@@ -9,9 +9,24 @@
 // except according to those terms.
 
 use std::fmt;
+use std::from_str;
 
-#[deriving(Eq)]
+#[deriving(Eq, Hash, Clone, TotalEq)]
 pub enum Os { OsWin32, OsMacos, OsLinux, OsAndroid, OsFreebsd, }
+
+impl from_str::FromStr for Os {
+    fn from_str(s: &str) -> Option<Os> {
+        match s {
+            "mingw32" => Some(OsWin32),
+            "win32"   => Some(OsWin32),
+            "darwin"  => Some(OsMacos),
+            "android" => Some(OsAndroid),
+            "linux"   => Some(OsLinux),
+            "freebsd" => Some(OsFreebsd),
+            _ => None,
+        }
+    }
+}
 
 #[deriving(Eq, TotalEq, Hash, Encodable, Decodable, Clone)]
 pub enum Abi {
@@ -33,7 +48,7 @@ pub enum Abi {
 }
 
 #[allow(non_camel_case_types)]
-#[deriving(Eq)]
+#[deriving(Eq, Clone, Hash, TotalEq)]
 pub enum Architecture {
     // NB. You cannot change the ordering of these
     // constants without adjusting IntelBits below.
@@ -43,6 +58,18 @@ pub enum Architecture {
     Arm,
     Mips
 }
+impl from_str::FromStr for Architecture {
+    fn from_str(s: &str) -> Option<Architecture> {
+        match s {
+            "i386" | "i486" | "i586" | "i686" | "i786" => Some(X86),
+            "x86_64" => Some(X86_64),
+            "arm" | "xscale" | "thumb" => Some(Arm),
+            "mips" => Some(Mips),
+            _ => None,
+        }
+    }
+}
+
 
 static IntelBits: u32 = (1 << (X86 as uint)) | (1 << (X86_64 as uint));
 static ArmBits: u32 = (1 << (Arm as uint));
