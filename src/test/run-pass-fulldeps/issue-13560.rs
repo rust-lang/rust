4ba94e2c24faf7b327567f67a7cf1e49cf0ffcce
Alex Crichton <alex@alexcrichton.com>
2014-04-16 11:42:22 -0700

    rustc: Don't allocate a cnum to syntax crates
    Syntax-only crates are no longer registered with the cstore, so there's no need
    to allocate crate numbers to them. This ends up leaving gaps in the crate
    numbering scheme which is not expected in the rest of the compiler.
    
    Closes #13560

---
+++ src/test/run-pass-fulldeps/issue-13560.rs
@@ -0,0 +1,24 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// aux-build:issue-13560-1.rs
+// aux-build:issue-13560-2.rs
+// aux-build:issue-13560-3.rs
+// ignore-stage1
+// ignore-android
+// ignore-cross-compile #12102
+
+// Regression test for issue #13560, the test itself is all in the dependent
+// libraries. The fail which previously failed to compile is the one numbered 3.
+
+extern crate t2 = "issue-13560-2";
+extern crate t3 = "issue-13560-3";
+
+fn main() {}
