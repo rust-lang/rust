0e1c41e47a3577707e8e14dbd1d48f349bcce126
Ryan Mulligan <ryan@ryantm.com>
2014-04-20 18:54:16 -0700

    remove meaningless sentence and update copyright.

--- src/libstd/cast.rs
+++ src/libstd/cast.rs
@@ -1,6 +1,6 @@
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// Copyright 2012-2013 The Rust Project Developers.
+// See the COPYRIGHT file at the top-level directory of this 
+// distribution and at // http://rust-lang.org/COPYRIGHT.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
@@ -28,8 +28,7 @@ pub unsafe fn transmute_copy<T, U>(src: &T) -> U {
  * Move a thing into the void
  *
  * The forget function will take ownership of the provided value but neglect
- * to run any required cleanup or memory-management operations on it. This
- * can be used for various acts of magick.
+ * to run any required cleanup or memory-management operations on it.
  */
 #[inline]
 pub unsafe fn forget<T>(thing: T) { intrinsics::forget(thing); }
