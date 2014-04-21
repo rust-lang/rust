0b6a4387230b1f586f86bb996e33e7adf8f70c2a
Ryan Mulligan <ryan@ryantm.com>
2014-04-20 20:07:55 -0700

    fix copyright message based on `make check`

--- src/libstd/cast.rs
+++ src/libstd/cast.rs
@@ -1,6 +1,6 @@
-// Copyright 2012-2013 The Rust Project Developers.
-// See the COPYRIGHT file at the top-level directory of this 
-// distribution and at // http://rust-lang.org/COPYRIGHT.
+// Copyright 2012-2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
