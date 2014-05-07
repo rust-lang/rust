2aa42533772a6b3cd406c34f0de12327320cb875
Brian Anderson <banderson@mozilla.com>
2014-05-07 12:12:24 -0700

    std: Small doc tweaks

--- src/libcore/cell.rs
+++ src/libcore/cell.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! Types dealing with dynamic mutability
+//! Types that provide interior mutability.
 
 use clone::Clone;
 use cmp::Eq;
