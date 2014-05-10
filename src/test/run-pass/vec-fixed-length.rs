9f7caed2024268f6de16f99b6696d191f3ca3228
klutzy <klutzytheklutzy@gmail.com>
2014-05-10 17:30:55 +0900

    rustllvm: Add LLVMRustArrayType
    LLVM internally uses `uint64_t` for array size, but the corresponding
    C API (`LLVMArrayType`) uses `unsigned int` so ths value is truncated.
    Therefore rustc generates wrong type for fixed-sized large vector e.g.
    `[0 x i8]` for `[0u8, ..(1 << 32)]`.
    
    This patch adds `LLVMRustArrayType` function for `uint64_t` support.

--- src/test/run-pass/vec-fixed-length.rs
+++ src/test/run-pass/vec-fixed-length.rs
@@ -8,7 +8,19 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::mem::size_of;
+
 pub fn main() {
     let x: [int, ..4] = [1, 2, 3, 4];
-    println!("{}", x[0]);
+    assert_eq!(x[0], 1);
+    assert_eq!(x[1], 2);
+    assert_eq!(x[2], 3);
+    assert_eq!(x[3], 4);
+
+    assert_eq!(size_of::<[u8, ..4]>(), 4u);
+
+    // FIXME #10183
+    if cfg!(target_word_size = "64") {
+        assert_eq!(size_of::<[u8, ..(1 << 32)]>(), (1u << 32));
+    }
 }
