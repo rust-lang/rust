3f482cac03dec028966e2a16c59965b50ff1761b
Tom Lee <github@tomlee.co>
2014-05-12 21:48:02 -0700

    Fix field ordering of addrinfo struct on Android

--- src/liblibc/lib.rs
+++ src/liblibc/lib.rs
@@ -430,8 +430,19 @@ pub mod types {
                     pub ai_socktype: c_int,
                     pub ai_protocol: c_int,
                     pub ai_addrlen: socklen_t,
+
+                    #[cfg(target_os = "linux")]
                     pub ai_addr: *sockaddr,
+
+                    #[cfg(target_os = "linux")]
                     pub ai_canonname: *c_char,
+
+                    #[cfg(target_os = "android")]
+                    pub ai_canonname: *c_char,
+
+                    #[cfg(target_os = "android")]
+                    pub ai_addr: *sockaddr,
+
                     pub ai_next: *addrinfo,
                 }
                 pub struct sockaddr_un {
