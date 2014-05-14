ef44b5ac3826c66aa8731a25c1fdb6adf07dcbc5
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-05-14 12:36:03 +0200

    A test case for a bug I found in the new SVH while reviewing it.
    Namely: non-pub `use` declarations *are* significant to the SVH
    computation, since they can change which traits are part of the method
    resolution step, and thus affect which methods get called from the
    (potentially inlined) code.

---
+++ src/test/compile-fail/svh-use-trait.rs
@@ -0,0 +1,27 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// note that these aux-build directives must be in this order
+// aux-build:svh-uta-base.rs
+// aux-build:svh-utb.rs
+// aux-build:svh-uta-change-use-trait.rs
+
+//! "compile-fail/svh-uta-trait.rs" is checking that we detect a
+//! change from `use foo::TraitB` to use `foo::TraitB` in the hash
+//! (SVH) computation (#14132), since that will affect method
+//! resolution.
+
+extern crate uta;
+extern crate utb; //~ ERROR: found possibly newer version of crate `uta` which `utb` depends
+//~^ NOTE: perhaps this crate needs to be recompiled
+
+fn main() {
+    utb::foo()
+}
