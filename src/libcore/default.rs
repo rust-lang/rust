6ecf4230027ff2dde7cc424e1b05adb56208e018
Kevin Ballard <kevin@sb.org>
2014-05-14 20:06:21 -0700

    Extend #[deriving(Default)] to support specified field defaults
    When deriving the Default trait, individual struct fields can be marked
    up with a `#[default="expr"]` attribute to indicate that this field
    should be given the results of `expr` instead of `Default::default()`.
    
    Example:
    
        #[deriving(Default)]
        struct Foo {
            #[default="42"]
            x: int,
            #[default="Ok(())"]
            err: IoResult<()>
        }

--- src/libcore/default.rs
+++ src/libcore/default.rs
@@ -9,6 +9,26 @@
 // except according to those terms.
 
 //! The `Default` trait for types which may have meaningful default values
+//!
+//! # Deriving
+//!
+//! This trait is derivable on structs using `#[deriving(Default)]`. When
+//! deriving this trait, every field is initialized to `Default::default()`.
+//! This can be overridden with the `#[default="expr"]` attribute on the field
+//! itself. For example:
+//!
+//! ```rust
+//! # use std::default::Default;
+//! #[deriving(Default,Eq,Show)]
+//! struct Frob {
+//!     #[default="42"]
+//!     x: uint,
+//!     y: uint
+//! }
+//!
+//! let frob: Frob = Default::default();
+//! assert_eq!(frob, Frob { x: 42, y: 0 });
+//! ```
 
 /// A trait that types which have a useful default value should implement.
 pub trait Default {
--- src/libsyntax/ext/deriving/default.rs
+++ src/libsyntax/ext/deriving/default.rs
@@ -8,11 +8,15 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use ast;
 use ast::{MetaItem, Item, Expr};
+use attr::{AttrMetaMethods, AttributeMethods};
 use codemap::Span;
 use ext::base::ExtCtxt;
 use ext::build::AstBuilder;
 use ext::deriving::generic::*;
+use ext::quote::rt::ExtParseUtils;
+use fold::Folder;
 use parse::token::InternedString;
 
 pub fn expand_deriving_default(cx: &mut ExtCtxt,
@@ -52,24 +56,35 @@ fn default_substructure(cx: &mut ExtCtxt, trait_span: Span, substr: &Substructur
         cx.ident_of("Default"),
         cx.ident_of("default")
     );
-    let default_call = |span| cx.expr_call_global(span, default_ident.clone(), Vec::new());
+    let default_call = |cx: &mut ExtCtxt, span| cx.expr_call_global(span, default_ident.clone(),
+                                                                    Vec::new());
 
     return match *substr.fields {
-        StaticStruct(_, ref summary) => {
+        StaticStruct(struct_def, ref summary) => {
             match *summary {
                 Unnamed(ref fields) => {
                     if fields.is_empty() {
                         cx.expr_ident(trait_span, substr.type_ident)
                     } else {
-                        let exprs = fields.iter().map(|sp| default_call(*sp)).collect();
+                        let exprs = fields.iter().zip(struct_def.fields.iter()).map(|(sp, field)| {
+                            match get_default_attr_expr(cx, field) {
+                                None => default_call(cx, *sp),
+                                Some(e) => e
+                            }
+                        }).collect();
                         cx.expr_call_ident(trait_span, substr.type_ident, exprs)
                     }
                 }
                 Named(ref fields) => {
-                    let default_fields = fields.iter().map(|&(ident, span)| {
-                        cx.field_imm(span, ident, default_call(span))
+                    let exprs = fields.iter().zip(struct_def.fields.iter())
+                                      .map(|(&(ident, span), field)| {
+                        let expr = match get_default_attr_expr(cx, field) {
+                            None => default_call(cx, span),
+                            Some(e) => e
+                        };
+                        cx.field_imm(span, ident, expr)
                     }).collect();
-                    cx.expr_struct_ident(trait_span, substr.type_ident, default_fields)
+                    cx.expr_struct_ident(trait_span, substr.type_ident, exprs)
                 }
             }
         }
@@ -80,4 +95,42 @@ fn default_substructure(cx: &mut ExtCtxt, trait_span: Span, substr: &Substructur
         }
         _ => cx.span_bug(trait_span, "Non-static method in `deriving(Default)`")
     };
+}
+
+fn get_default_attr_expr(cx: &mut ExtCtxt, field: &ast::StructField) -> Option<@Expr> {
+    let attrs = field.node.attrs.as_slice();
+    attrs.iter().find(|at| at.name().get() == "default").and_then(|at| {
+        match at.meta().node {
+            ast::MetaNameValue(_, ref v) => {
+                match v.node {
+                    ast::LitStr(ref s, _) => {
+                        let s = s.get().to_strbuf();
+                        let expr = cx.parse_expr(s);
+                        let mut folder = SpanFolder { sp: v.span };
+                        Some(folder.fold_expr(expr))
+
+                    }
+                    _ => {
+                        cx.span_err(v.span,
+                            "non-string literals are not allowed in `#[default]` attribute");
+                        None
+                    }
+                }
+            }
+            _ => {
+                cx.span_err(at.span, "`#[default]` attribute must have a value");
+                None
+            }
+        }
+    })
+}
+
+struct SpanFolder {
+    sp: Span
+}
+
+impl Folder for SpanFolder {
+    fn new_span(&mut self, _sp: Span) -> Span {
+        self.sp
+    }
 }
