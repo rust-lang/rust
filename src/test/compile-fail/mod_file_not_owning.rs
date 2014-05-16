97e5ca37296809a5b651374824f46144905b39f5
Alex Crichton <alex@alexcrichton.com>
2014-05-16 14:23:04 -0700

    syntax: Tighten search paths for inner modules
    This is an implementation of RFC 16. A module can now only be loaded if the
    module declaring `mod name;` "owns" the current directory. A module is
    considered as owning its directory if it meets one of the following criteria:
    
    * It is the top-level crate file
    * It is a `mod.rs` file
    * It was loaded via `#[path]`
    * It was loaded via `include!`
    * The module was declared via an inline `mod foo { ... }` statement
    
    For example, this directory structure is now invalid
    
        // lib.rs
        mod foo;
    
        // foo.rs
        mod bar;
    
        // bar.rs;
        fn bar() {}
    
    With this change `foo.rs` must be renamed to `foo/mod.rs`, and `bar.rs` must be
    renamed to `foo/bar.rs`. This makes it clear that `bar` is a submodule of `foo`,
    and can only be accessed through `foo`.
    
    RFC: 0016-module-file-system-hierarchy
    Closes #14180
    
    [breaking-change]

--- src/test/compile-fail/mod_file_not_owning.rs
+++ src/test/compile-fail/mod_file_not_owning.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,13 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#[path = "circular_modules_hello.rs"]
-mod circular_modules_hello; //~ERROR: circular modules
+// error-pattern: cannot declare a new module at this location
 
-pub fn hi_str() -> StrBuf {
-  "Hi!".to_owned()
-}
+mod mod_file_not_owning_aux1;
 
-fn main() {
-    circular_modules_hello::say_hello();
-}
+fn main() {}
