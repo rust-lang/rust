a1075bc0220b0a10127c016f565973f17d3ea780
Alex Crichton <alex@alexcrichton.com>
2014-05-21 19:55:39 -0700

    Change static.rust-lang.org to doc.rust-lang.org
    The new documentation site has shorter urls, gzip'd content, and index.html
    redirecting functionality.

--- src/libhexfloat/lib.rs
+++ src/libhexfloat/lib.rs
@@ -42,7 +42,7 @@ fn main() {
 #![license = "MIT/ASL2"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://static.rust-lang.org/doc/master")]
+       html_root_url = "http://doc.rust-lang.org/")]
 
 #![deny(deprecated_owned_vector)]
 #![feature(macro_registrar, managed_boxes)]
