6f2e5cc87b03a29d3e598904ac815ea8314c4038
Alex Crichton <alex@alexcrichton.com>
2014-05-22 22:02:34 -0700

    std: Add doc(noinline) to the prelude reexports

--- src/libstd/prelude.rs
+++ src/libstd/prelude.rs
@@ -38,55 +38,62 @@
 //! `drop`, `spawn`, and `channel`.
 
 // Reexported core operators
-pub use kinds::{Copy, Send, Sized, Share};
-pub use ops::{Add, Sub, Mul, Div, Rem, Neg, Not};
-pub use ops::{BitAnd, BitOr, BitXor};
-pub use ops::{Drop, Deref, DerefMut};
-pub use ops::{Shl, Shr, Index};
-pub use option::{Option, Some, None};
-pub use result::{Result, Ok, Err};
+#[doc(noinline)] pub use kinds::{Copy, Send, Sized, Share};
+#[doc(noinline)] pub use ops::{Add, Sub, Mul, Div, Rem, Neg, Not};
+#[doc(noinline)] pub use ops::{BitAnd, BitOr, BitXor};
+#[doc(noinline)] pub use ops::{Drop, Deref, DerefMut};
+#[doc(noinline)] pub use ops::{Shl, Shr, Index};
+#[doc(noinline)] pub use option::{Option, Some, None};
+#[doc(noinline)] pub use result::{Result, Ok, Err};
 
 // Reexported functions
-pub use from_str::from_str;
-pub use iter::range;
-pub use mem::drop;
+#[doc(noinline)] pub use from_str::from_str;
+#[doc(noinline)] pub use iter::range;
+#[doc(noinline)] pub use mem::drop;
 
 // Reexported types and traits
 
-pub use ascii::{Ascii, AsciiCast, OwnedAsciiCast, AsciiStr, IntoBytes};
-pub use c_str::ToCStr;
-pub use char::Char;
-pub use clone::Clone;
-pub use cmp::{Eq, Ord, TotalEq, TotalOrd, Ordering, Less, Equal, Greater, Equiv};
-pub use container::{Container, Mutable, Map, MutableMap, Set, MutableSet};
-pub use iter::{FromIterator, Extendable};
-pub use iter::{Iterator, DoubleEndedIterator, RandomAccessIterator, CloneableIterator};
-pub use iter::{OrdIterator, MutableDoubleEndedIterator, ExactSize};
-pub use num::{Num, NumCast, CheckedAdd, CheckedSub, CheckedMul};
-pub use num::{Signed, Unsigned};
-pub use num::{Primitive, Int, Float, FloatMath, ToPrimitive, FromPrimitive};
-pub use option::Expect;
-pub use owned::Box;
-pub use path::{GenericPath, Path, PosixPath, WindowsPath};
-pub use ptr::RawPtr;
-pub use io::{Buffer, Writer, Reader, Seek};
-pub use str::{Str, StrVector, StrSlice, OwnedStr, IntoMaybeOwned};
-pub use str::{StrAllocating};
-pub use to_str::{ToStr, IntoStr};
-pub use tuple::{Tuple1, Tuple2, Tuple3, Tuple4};
-pub use tuple::{Tuple5, Tuple6, Tuple7, Tuple8};
-pub use tuple::{Tuple9, Tuple10, Tuple11, Tuple12};
-pub use slice::{CloneableVector, ImmutableCloneableVector, MutableCloneableVector};
-pub use slice::{ImmutableVector, MutableVector};
-pub use slice::{ImmutableEqVector, ImmutableTotalOrdVector, MutableTotalOrdVector};
-pub use slice::{Vector, VectorVector, OwnedVector, MutableVectorAllocating};
-pub use string::String;
-pub use vec::Vec;
+#[doc(noinline)] pub use ascii::{Ascii, AsciiCast, OwnedAsciiCast, AsciiStr};
+#[doc(noinline)] pub use ascii::IntoBytes;
+#[doc(noinline)] pub use c_str::ToCStr;
+#[doc(noinline)] pub use char::Char;
+#[doc(noinline)] pub use clone::Clone;
+#[doc(noinline)] pub use cmp::{Eq, Ord, TotalEq, TotalOrd};
+#[doc(noinline)] pub use cmp::{Ordering, Less, Equal, Greater, Equiv};
+#[doc(noinline)] pub use container::{Container, Mutable, Map, MutableMap};
+#[doc(noinline)] pub use container::{Set, MutableSet};
+#[doc(noinline)] pub use iter::{FromIterator, Extendable, ExactSize};
+#[doc(noinline)] pub use iter::{Iterator, DoubleEndedIterator};
+#[doc(noinline)] pub use iter::{RandomAccessIterator, CloneableIterator};
+#[doc(noinline)] pub use iter::{OrdIterator, MutableDoubleEndedIterator};
+#[doc(noinline)] pub use num::{Num, NumCast, CheckedAdd, CheckedSub, CheckedMul};
+#[doc(noinline)] pub use num::{Signed, Unsigned, Primitive, Int, Float};
+#[doc(noinline)] pub use num::{FloatMath, ToPrimitive, FromPrimitive};
+#[doc(noinline)] pub use option::Expect;
+#[doc(noinline)] pub use owned::Box;
+#[doc(noinline)] pub use path::{GenericPath, Path, PosixPath, WindowsPath};
+#[doc(noinline)] pub use ptr::RawPtr;
+#[doc(noinline)] pub use io::{Buffer, Writer, Reader, Seek};
+#[doc(noinline)] pub use str::{Str, StrVector, StrSlice, OwnedStr};
+#[doc(noinline)] pub use str::{IntoMaybeOwned, StrAllocating};
+#[doc(noinline)] pub use to_str::{ToStr, IntoStr};
+#[doc(noinline)] pub use tuple::{Tuple1, Tuple2, Tuple3, Tuple4};
+#[doc(noinline)] pub use tuple::{Tuple5, Tuple6, Tuple7, Tuple8};
+#[doc(noinline)] pub use tuple::{Tuple9, Tuple10, Tuple11, Tuple12};
+#[doc(noinline)] pub use slice::{CloneableVector, ImmutableCloneableVector};
+#[doc(noinline)] pub use slice::{MutableCloneableVector, MutableTotalOrdVector};
+#[doc(noinline)] pub use slice::{ImmutableVector, MutableVector};
+#[doc(noinline)] pub use slice::{ImmutableEqVector, ImmutableTotalOrdVector};
+#[doc(noinline)] pub use slice::{Vector, VectorVector, OwnedVector};
+#[doc(noinline)] pub use slice::MutableVectorAllocating;
+#[doc(noinline)] pub use string::String;
+#[doc(noinline)] pub use vec::Vec;
 
 // Reexported runtime types
-pub use comm::{sync_channel, channel, SyncSender, Sender, Receiver};
-pub use task::spawn;
+#[doc(noinline)] pub use comm::{sync_channel, channel};
+#[doc(noinline)] pub use comm::{SyncSender, Sender, Receiver};
+#[doc(noinline)] pub use task::spawn;
 
 // Reexported statics
 #[cfg(not(test))]
-pub use gc::GC;
+#[doc(noinline)] pub use gc::GC;
