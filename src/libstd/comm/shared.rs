41b65d39ab5b4fae52cbcc370b464ba370bedb87
Valerii Hiora <valerii.hiora@gmail.com>
2014-05-24 08:01:23 +0300

    Fixes problems on systems with opaque mutex
    On some systems (iOS for example) mutex is represented by
    opaque data structure which doesn't play well with simple
    data copy. Therefore mutex should be initialized from
    magic static value and filled by OS only when it landed RC.

--- src/libstd/comm/shared.rs
+++ src/libstd/comm/shared.rs
@@ -66,7 +66,8 @@ pub enum Failure {
 }
 
 impl<T: Send> Packet<T> {
-    // Creation of a packet *must* be followed by a call to inherit_blocker
+    // Creation of a packet *must* be followed by a call to postinit_lock
+    // and later by inherit_blocker
     pub fn new() -> Packet<T> {
         let p = Packet {
             queue: mpsc::Queue::new(),
@@ -78,9 +79,16 @@ impl<T: Send> Packet<T> {
             sender_drain: atomics::AtomicInt::new(0),
             select_lock: unsafe { NativeMutex::new() },
         };
-        // see comments in inherit_blocker about why we grab this lock
-        unsafe { p.select_lock.lock_noguard() }
         return p;
+    }
+
+    // This function should be used after newly created Packet
+    // was wrapped with an Arc
+    // In other case mutex data will be duplicated while clonning
+    // and that could cause problems on platforms where it is
+    // represented by opaque data structure
+    pub fn postinit_lock(&mut self) {
+        unsafe { self.select_lock.lock_noguard() }
     }
 
     // This function is used at the creation of a shared packet to inherit a
