94dc67ad441a6f975b9f1121882afcb2358a8a92
Alex Crichton <alex@alexcrichton.com>
2014-05-28 19:53:37 -0700

    rustdoc: Create anchor pages for primitive types
    This commit adds support in rustdoc to recognize the `#[doc(primitive = "foo")]`
    attribute. This attribute indicates that the current module is the "owner" of
    the primitive type `foo`. For rustdoc, this means that the doc-comment for the
    module is the doc-comment for the primitive type, plus a signal to all
    downstream crates that hyperlinks for primitive types will be directed at the
    crate containing the `#[doc]` directive.
    
    Additionally, rustdoc will favor crates closest to the one being documented
    which "implements the primitive type". For example, documentation of libcore
    links to libcore for primitive types, but documentation for libstd and beyond
    all links to libstd for primitive types.
    
    This change involves no compiler modifications, it is purely a rustdoc change.
    The landing pages for the primitive types primarily serve to show a list of
    implemented traits for the primitive type itself.
    
    The primitive types documented includes both strings and slices in a semi-ad-hoc
    way, but in a way that should provide at least somewhat meaningful
    documentation.
    
    Closes #14474

--- src/libstd/num/i64.rs
+++ src/libstd/num/i64.rs
@@ -10,6 +10,8 @@
 
 //! Operations and constants for signed 64-bits integers (`i64` type)
 
+#![doc(primitive = "i64")]
+
 use from_str::FromStr;
 use num::{ToStrRadix, FromStrRadix};
 use num::strconv;
