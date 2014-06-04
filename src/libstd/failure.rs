5ec36c358f74fe83332231e774ea20a21d165120
Alex Crichton <alex@alexcrichton.com>
2014-06-03 19:11:49 -0700

    std: Extract librustrt out of libstd
    As part of the libstd facade efforts, this commit extracts the runtime interface
    out of the standard library into a standalone crate, librustrt. This crate will
    provide the following services:
    
    * Definition of the rtio interface
    * Definition of the Runtime interface
    * Implementation of the Task structure
    * Implementation of task-local-data
    * Implementation of task failure via unwinding via libunwind
    * Implementation of runtime initialization and shutdown
    * Implementation of thread-local-storage for the local rust Task
    
    Notably, this crate avoids the following services:
    
    * Thread creation and destruction. The crate does not require the knowledge of
      an OS threading system, and as a result it seemed best to leave out the
      `rt::thread` module from librustrt. The librustrt module does depend on
      mutexes, however.
    * Implementation of backtraces. There is no inherent requirement for the runtime
      to be able to generate backtraces. As will be discussed later, this
      functionality continues to live in libstd rather than librustrt.
    
    As usual, a number of architectural changes were required to make this crate
    possible. Users of "stable" functionality will not be impacted by this change,
    but users of the `std::rt` module will likely note the changes. A list of
    architectural changes made is:
    
    * The stdout/stderr handles no longer live directly inside of the `Task`
      structure. This is a consequence of librustrt not knowing about `std::io`.
      These two handles are now stored inside of task-local-data.
    
      The handles were originally stored inside of the `Task` for perf reasons, and
      TLD is not currently as fast as it could be. For comparison, 100k prints goes
      from 59ms to 68ms (a 15% slowdown). This appeared to me to be an acceptable
      perf loss for the successful extraction of a librustrt crate.
    
    * The `rtio` module was forced to duplicate more functionality of `std::io`. As
      the module no longer depends on `std::io`, `rtio` now defines structures such
      as socket addresses, addrinfo fiddly bits, etc. The primary change made was
      that `rtio` now defines its own `IoError` type. This type is distinct from
      `std::io::IoError` in that it does not have an enum for what error occurred,
      but rather a platform-specific error code.
    
      The native and green libraries will be updated in later commits for this
      change, and the bulk of this effort was put behind updating the two libraries
      for this change (with `rtio`).
    
    * Printing a message on task failure (along with the backtrace) continues to
      live in libstd, not in librustrt. This is a consequence of the above decision
      to move the stdout/stderr handles to TLD rather than inside the `Task` itself.
      The unwinding API now supports registration of global callback functions which
      will be invoked when a task fails, allowing for libstd to register a function
      to print a message and a backtrace.
    
      The API for registering a callback is experimental and unsafe, as the
      ramifications of running code on unwinding is pretty hairy.
    
    * The `std::unstable::mutex` module has moved to `std::rt::mutex`.
    
    * The `std::unstable::sync` module has been moved to `std::rt::exclusive` and
      the type has been rewritten to not internally have an Arc and to have an RAII
      guard structure when locking. Old code should stop using `Exclusive` in favor
      of the primitives in `libsync`, but if necessary, old code should port to
      `Arc<Exclusive<T>>`.
    
    * The local heap has been stripped down to have fewer debugging options. None of
      these were tested, and none of these have been used in a very long time.
    
    [breaking-change]

---
+++ src/libstd/failure.rs
@@ -0,0 +1,102 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use alloc::owned::Box;
+use any::{Any, AnyRefExt};
+use fmt;
+use io::{Writer, IoResult};
+use kinds::Send;
+use option::{Some, None};
+use result::Ok;
+use rt::backtrace;
+use rt::{Stderr, Stdio};
+use rustrt::local::Local;
+use rustrt::task::Task;
+use str::Str;
+use string::String;
+
+// Defined in this module instead of io::stdio so that the unwinding
+local_data_key!(pub local_stderr: Box<Writer:Send>)
+
+impl Writer for Stdio {
+    fn write(&mut self, bytes: &[u8]) -> IoResult<()> {
+        fn fmt_write<F: fmt::FormatWriter>(f: &mut F, bytes: &[u8]) {
+            let _ = f.write(bytes);
+        }
+        fmt_write(self, bytes);
+        Ok(())
+    }
+}
+
+pub fn on_fail(obj: &Any:Send, file: &'static str, line: uint) {
+    let msg = match obj.as_ref::<&'static str>() {
+        Some(s) => *s,
+        None => match obj.as_ref::<String>() {
+            Some(s) => s.as_slice(),
+            None => "Box<Any>",
+        }
+    };
+    let mut err = Stderr;
+
+    // It is assumed that all reasonable rust code will have a local task at
+    // all times. This means that this `exists` will return true almost all of
+    // the time. There are border cases, however, when the runtime has
+    // *almost* set up the local task, but hasn't quite gotten there yet. In
+    // order to get some better diagnostics, we print on failure and
+    // immediately abort the whole process if there is no local task
+    // available.
+    if !Local::exists(None::<Task>) {
+        let _ = writeln!(&mut err, "failed at '{}', {}:{}", msg, file, line);
+        if backtrace::log_enabled() {
+            let _ = backtrace::write(&mut err);
+        } else {
+            let _ = writeln!(&mut err, "run with `RUST_BACKTRACE=1` to \
+                                        see a backtrace");
+        }
+        return
+    }
+
+    // Peel the name out of local task so we can print it. We've got to be sure
+    // that the local task is in TLS while we're printing as I/O may occur.
+    let (name, unwinding) = {
+        let mut t = Local::borrow(None::<Task>);
+        (t.name.take(), t.unwinder.unwinding())
+    };
+    {
+        let n = name.as_ref().map(|n| n.as_slice()).unwrap_or("<unnamed>");
+
+        match local_stderr.replace(None) {
+            Some(mut stderr) => {
+                // FIXME: what to do when the task printing fails?
+                let _ = writeln!(stderr,
+                                 "task '{}' failed at '{}', {}:{}\n",
+                                 n, msg, file, line);
+                if backtrace::log_enabled() {
+                    let _ = backtrace::write(stderr);
+                }
+                local_stderr.replace(Some(stderr));
+            }
+            None => {
+                let _ = writeln!(&mut err, "task '{}' failed at '{}', {}:{}",
+                                 n, msg, file, line);
+                if backtrace::log_enabled() {
+                    let _ = backtrace::write(&mut err);
+                }
+            }
+        }
+
+        // If this is a double failure, make sure that we printed a backtrace
+        // for this failure.
+        if unwinding && !backtrace::log_enabled() {
+            let _ = backtrace::write(&mut err);
+        }
+    }
+    Local::borrow(None::<Task>).name = name;
+}
