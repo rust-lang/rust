0f91cb8012e92de3e540cbb0c24a99612bbfdeeb
Patrick Walton <pcwalton@mimiga.net>
2014-06-12 14:08:44 -0700

    librustc: Forbid `transmute` from being called on types whose size is only known post-monomorphization, and report `transmute` errors before the code is generated for that `transmute`.
    This can break code that looked like:
    
        unsafe fn f<T>(x: T) {
            let y: int = transmute(x);
        }
    
    Change such code to take a type parameter that has the same size as the
    type being transmuted to.
    
    Closes #12898.
    
    [breaking-change]

---
+++ src/test/compile-fail/transmute-different-sizes.rs
@@ -0,0 +1,27 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Tests that `transmute` cannot be called on types of different size.
+
+use std::mem::transmute;
+
+unsafe fn f() {
+    let _: i8 = transmute(16i16);
+    //~^ ERROR transmute called on types with different sizes
+}
+
+unsafe fn g<T>(x: &T) {
+    let _: i8 = transmute(x);
+    //~^ ERROR transmute called on types with different sizes
+}
+
+fn main() {}
+
+
