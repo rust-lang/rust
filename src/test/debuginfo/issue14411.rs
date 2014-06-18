369b070d1dfdfe2535ff08aeb1576b6dbba6405b
Michael Woerister <michaelwoerister@posteo>
2014-06-18 17:56:21 +0200

    debuginfo: Add test case for issue 14411.

---
+++ src/test/debuginfo/issue14411.rs
@@ -0,0 +1,25 @@
+// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// ignore-android: FIXME(#10381)
+
+// compile-flags:-g
+
+// No debugger interaction required: just make sure it compiles without
+// crashing.
+
+fn test(a: &Vec<u8>) {
+  print!("{}", a.len());
+}
+
+pub fn main() {
+  let data = vec!();
+  test(&data);
+}
