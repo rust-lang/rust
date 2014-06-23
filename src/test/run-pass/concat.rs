08b906dcf69a6ddc08971d2234f1e0913206a93a
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-06-23 15:51:40 +0000

    Allow trailing comma in `concat!`
    (And in other extensions implemented with `get_exprs_from_tts` function).

--- src/test/run-pass/concat.rs
+++ src/test/run-pass/concat.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -11,6 +11,8 @@
 pub fn main() {
     assert_eq!(format!(concat!("foo", "bar", "{}"), "baz"), "foobarbaz".to_string());
     assert_eq!(format!(concat!()), "".to_string());
+    // check trailing comma is allowed in concat
+    assert_eq!(concat!("qux", "quux",).to_string(), "quxquux".to_string());
 
     assert_eq!(
         concat!(1, 2i, 3u, 4f32, 4.0, 'a', true, ()),
