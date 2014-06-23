7fb9134f22cc4a6a397ec07aaa957a9c6a7e2875
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2014-06-23 19:10:40 -0400

    std: micro-optimize hashing Gc types

--- src/libstd/gc.rs
+++ src/libstd/gc.rs
@@ -88,6 +88,7 @@ impl<T: Default + 'static> Default for Gc<T> {
 impl<T: 'static> raw::Repr<*const raw::Box<T>> for Gc<T> {}
 
 impl<S: hash::Writer, T: hash::Hash<S> + 'static> hash::Hash<S> for Gc<T> {
+    #[inline]
     fn hash(&self, s: &mut S) {
         (**self).hash(s)
     }
