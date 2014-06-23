3fe2a3926592db905173cc9d7d655c618da3c285
Steve Klabnik <steve@steveklabnik.com>
2014-06-23 14:04:54 -0400

    Remove the cheat sheet.
    Rust by Example is far better.
    
    Fixes #14380.

--- mk/docs.mk
+++ mk/docs.mk
@@ -28,7 +28,7 @@
 ######################################################################
 DOCS := index intro tutorial guide-ffi guide-macros guide-lifetimes \
 	guide-tasks guide-container guide-pointers guide-testing \
-	guide-runtime complement-bugreport complement-cheatsheet \
+	guide-runtime complement-bugreport \
 	complement-lang-faq complement-design-faq complement-project-faq rust \
     rustdoc guide-unsafe
 
