313e20f5e6ff1901d96fd859083402c313d0aaa1
Alexei Sholik <alcosholik@gmail.com>
2014-06-25 12:12:28 +0300

    Fix wrong type name: IoError<()> -> IoResult<()>

--- src/libstd/fmt.rs
+++ src/libstd/fmt.rs
@@ -185,7 +185,7 @@ struct. In order to help with this, the `Formatter` struct also provides some
 helper methods.
 
 Additionally, the return value of this function is `fmt::Result` which is a
-typedef to `Result<(), IoError>` (also known as `IoError<()>`). Formatting
+typedef to `Result<(), IoError>` (also known as `IoResult<()>`). Formatting
 implementations should ensure that they return errors from `write!` correctly
 (propagating errors upward).
 
