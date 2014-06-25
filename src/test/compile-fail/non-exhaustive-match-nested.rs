b7e442c976be75e556613b1b01636329968661d8
Jakub Wieczorek <jakub@jakub.cc>
2014-06-25 19:07:37 +0200

    Change exhaustiveness analysis to permit multiple constructors per pattern
    Slice patterns are different from the rest in that a single slice pattern
    does not have a distinct constructor if it contains a variable-length subslice
    pattern. For example, the pattern [a, b, ..tail] can match a slice of length 2, 3, 4
    and so on.
    
    As a result, the decision tree for exhaustiveness and redundancy analysis should
    explore each of those constructors separately to determine if the pattern could be useful
    when specialized for any of them.

--- src/test/compile-fail/non-exhaustive-match-nested.rs
+++ src/test/compile-fail/non-exhaustive-match-nested.rs
@@ -1,4 +1,4 @@
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2012-2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -11,10 +11,19 @@
 enum t { a(u), b }
 enum u { c, d }
 
+fn match_nested_vecs<'a, T>(l1: Option<&'a [T]>, l2: Result<&'a [T], ()>) -> &'static str {
+    match (l1, l2) { //~ ERROR non-exhaustive patterns: `(Some([]), Err(_))` not covered
+        (Some([]), Ok([])) => "Some(empty), Ok(empty)",
+        (Some([_, ..]), Ok(_)) | (Some([_, ..]), Err(())) => "Some(non-empty), any",
+        (None, Ok([])) | (None, Err(())) | (None, Ok([_])) => "None, Ok(less than one element)",
+        (None, Ok([_, _, ..])) => "None, Ok(at least two elements)"
+    }
+}
+
 fn main() {
-  let x = a(c);
-  match x { //~ ERROR non-exhaustive patterns: `a(c)` not covered
-      a(d) => { fail!("hello"); }
-      b => { fail!("goodbye"); }
+    let x = a(c);
+    match x { //~ ERROR non-exhaustive patterns: `a(c)` not covered
+        a(d) => { fail!("hello"); }
+        b => { fail!("goodbye"); }
     }
 }
