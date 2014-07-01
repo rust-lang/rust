812637e683ef1092cdd62704ab53f0b4c5aabe19
Alex Crichton <alex@alexcrichton.com>
2014-07-01 11:51:41 -0700

    test: Fix tests for crate_id removal
    This involved removing some tests whose functionality was removed such as many
    of the crateresolve tests

--- src/test/run-make/multiple-versions/foo.rs
+++ src/test/run-make/multiple-versions/foo.rs
@@ -1,4 +1,4 @@
-// Copyright 2012-2014 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,12 +8,4 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// aux-build:crateresolve1-1.rs
-// aux-build:crateresolve1-2.rs
-// aux-build:crateresolve1-3.rs
-
-extern crate crateresolve1 = "crateresolve1#0.2";
-
-pub fn main() {
-    assert_eq!(crateresolve1::f(), 20);
-}
+pub fn foo() {}
