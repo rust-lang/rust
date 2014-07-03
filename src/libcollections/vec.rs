6ed5d835665af89c66ea8ec7f57130f92dc9b76f
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2014-07-03 08:40:08 -0700

    collections: grow should use the overflow-checked reserve_additional

--- src/libcollections/vec.rs
+++ src/libcollections/vec.rs
@@ -253,8 +253,7 @@ impl<T: Clone> Vec<T> {
     /// assert_eq!(vec, vec!("hello", "world", "world"));
     /// ```
     pub fn grow(&mut self, n: uint, value: &T) {
-        let new_len = self.len() + n;
-        self.reserve(new_len);
+        self.reserve_additional(n);
         let mut i: uint = 0u;
 
         while i < n {
