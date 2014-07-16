707cf47ac814c7af124dcfe378f97c25eb6b4511
Alex Crichton <alex@alexcrichton.com>
2014-07-16 10:28:42 -0700

    Register new snapshots

--- src/libsyntax/diagnostics/macros.rs
+++ src/libsyntax/diagnostics/macros.rs
@@ -10,36 +10,10 @@
 
 #![macro_escape]
 
-// NOTE: remove after next snapshot
-#[cfg(stage0)]
-#[macro_export]
-macro_rules! __register_diagnostic(
-    ($code:tt, $description:tt) => ();
-    ($code:tt) => ()
-)
-
 #[macro_export]
 macro_rules! register_diagnostic(
     ($code:tt, $description:tt) => (__register_diagnostic!($code, $description));
     ($code:tt) => (__register_diagnostic!($code))
-)
-
-// NOTE: remove after next snapshot
-#[cfg(stage0)]
-#[macro_export]
-macro_rules! __build_diagnostic_array(
-    ($name:ident) => {
-        pub static $name: [(&'static str, &'static str), ..0] = [];
-    }
-)
-
-// NOTE: remove after next snapshot
-#[cfg(stage0)]
-#[macro_export]
-macro_rules! __diagnostic_used(
-    ($code:ident) => {
-        ()
-    }
 )
 
 #[macro_export]
