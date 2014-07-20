6ebbc6c4a3c82f7321e84b15abc9228ef1bdd68c
Alex Crichton <alex@alexcrichton.com>
2014-07-20 12:08:09 -0700

    rustc: Append platform exe suffix to output files
    Closes #15828

--- src/librustc/back/link.rs
+++ src/librustc/back/link.rs
@@ -937,7 +937,16 @@ pub fn filename_for_input(sess: &Session,
         config::CrateTypeStaticlib => {
             out_filename.with_filename(format!("lib{}.a", libname))
         }
-        config::CrateTypeExecutable => out_filename.clone(),
+        config::CrateTypeExecutable => {
+            match sess.targ_cfg.os {
+                abi::OsWin32 => out_filename.with_extension("exe"),
+                abi::OsMacos |
+                abi::OsLinux |
+                abi::OsAndroid |
+                abi::OsFreebsd |
+                abi::OsiOS => out_filename.clone(),
+            }
+        }
     }
 }
 
