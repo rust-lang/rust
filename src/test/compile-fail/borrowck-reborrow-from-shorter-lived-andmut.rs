1c15e9efeb2f816f2615042bd9e8125ebf48bcfa
Niko Matsakis <niko@alum.mit.edu>
2014-07-22 05:43:19 -0400

    Fix regionck to account for the uniqueness requirements on ref mut reborrows

--- src/test/compile-fail/borrowck-reborrow-from-shorter-lived-andmut.rs
+++ src/test/compile-fail/borrowck-reborrow-from-shorter-lived-andmut.rs
@@ -17,7 +17,7 @@ struct S<'a> {
 
 fn copy_borrowed_ptr<'a,'b>(p: &'a mut S<'b>) -> S<'b> {
     S { pointer: &mut *p.pointer }
-    //~^ ERROR lifetime of `p` is too short to guarantee its contents can be safely reborrowed
+    //~^ ERROR cannot infer
 }
 
 fn main() {
