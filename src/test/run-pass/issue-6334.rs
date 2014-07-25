08f033b8b73a00180955096f2c03df187f9b2060
Jason Fager <jfager@gmail.com>
2014-07-25 16:19:04 -0400

    Sort trait bounds.
    Closes #6334

--- src/test/run-pass/issue-6334.rs
+++ src/test/run-pass/issue-6334.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// ignore-test
-
 // Tests that everything still compiles and runs fine even when
 // we reorder the bounds.
 
