e9d7b92d4d3193635804a00269d14e3a36e07c26
koalazen <sylvain.mellak@gmail.com>
2014-07-26 00:29:06 +0200

    libcore::iter Refactor Skip::next
    Simplifying the code of that method
    Adding benchmarks for that method
    Fixing behaviour of the method which sets self.n to zero even when
    the iterator is consumed before skipping n values

--- src/libcoretest/iter.rs
+++ src/libcoretest/iter.rs
@@ -13,6 +13,7 @@ use core::iter::order::*;
 use core::uint;
 use core::cmp;
 use core::num;
+use test::Bencher;
 
 #[test]
 fn test_lt() {
@@ -842,3 +843,24 @@ fn test_iterate() {
     assert_eq!(it.next(), Some(4u));
     assert_eq!(it.next(), Some(8u));
 }
+
+// Some benchmarks
+
+#[bench]
+fn bench_skip(b: &mut Bencher) {
+    let xs = [0u, 1, 2, 3, 5, 13, 15, 16, 17, 19, 20, 30];
+    b.iter(|| xs.iter().skip(5).next());
+}
+
+#[bench]
+fn bench_skip_zero(b: &mut Bencher) {
+    let xs = [0u, 1, 2, 3, 5, 13, 15, 16, 17, 19, 20, 30];
+    b.iter(|| xs.iter().skip(0).next());
+}
+
+#[bench]
+fn bench_skip_reach_end(b: &mut Bencher) {
+    let xs = [0u, 1, 2, 3, 5, 13, 15, 16, 17, 19, 20, 30];
+    b.iter(|| xs.iter().skip(20).next());
+}
+
