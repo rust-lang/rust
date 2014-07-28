8876ce44c59bbb20006ae5beccc9f6954e7d1e4f
Corey Richardson <corey@octayn.net>
2014-07-28 01:02:31 -0700

    rustc: encode is_sugared_doc on ast::Attribute

--- src/librustc/metadata/common.rs
+++ src/librustc/metadata/common.rs
@@ -231,3 +231,5 @@ pub static tag_unboxed_closure_type: uint = 0x97;
 pub static tag_struct_fields: uint = 0x98;
 pub static tag_struct_field: uint = 0x99;
 pub static tag_struct_field_id: uint = 0x9a;
+
+pub static tag_attribute_is_sugared_doc: uint = 0x9b;
