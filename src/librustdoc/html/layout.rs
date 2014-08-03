1c6ee3f27d854e74d0549a49cb1bbe0b241ed7d7
Tom Jakubowski <tom@crystae.net>
2014-08-02 18:17:21 -0700

    rustdoc: Add generator meta tag to API docs
    This is consistent with the meta tag in Markdown docs run through
    rustdoc.

--- src/librustdoc/html/layout.rs
+++ src/librustdoc/html/layout.rs
@@ -39,6 +39,7 @@ r##"<!DOCTYPE html>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="The {krate} library documentation.">
+    <meta name="generator" content="rustdoc">
 
     <title>{title}</title>
 
