e44673f245a16e2434f43e9c77bdca1cb82f04bc
klutzy <klutzytheklutzy@gmail.com>
2014-08-12 23:23:10 +0900

    regex: Enable test on Windows
    Fixes #13725

--- src/test/compile-fail-fulldeps/syntax-extension-regex-unused-static.rs
+++ src/test/compile-fail-fulldeps/syntax-extension-regex-unused-static.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// FIXME(#13725) windows needs fixing.
-// ignore-windows
 // ignore-stage1
 
 #![feature(phase)]
