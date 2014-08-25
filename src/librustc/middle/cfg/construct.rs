43074775d4370608c447f6af4a0cf8a592b6ee9e
Kevin Ballard <kevin@sb.org>
2014-08-24 19:08:48 -0700

    Desugar 'if let' into the appropriate 'match'

--- src/librustc/middle/cfg/construct.rs
+++ src/librustc/middle/cfg/construct.rs
@@ -222,6 +222,8 @@ impl<'a, 'tcx> CFGBuilder<'a, 'tcx> {
                 self.add_node(expr.id, [then_exit, else_exit])           // 4, 5
             }
 
+            ast::ExprIfLet(..) => fail!("non-desugared ExprIfLet"),
+
             ast::ExprWhile(ref cond, ref body, _) => {
                 //
                 //         [pred]
