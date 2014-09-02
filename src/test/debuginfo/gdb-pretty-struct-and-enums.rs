b090905b1750983bf41094e7a940a333324e3e10
Alex Crichton <alex@alexcrichton.com>
2014-09-02 08:34:00 -0700

    test: Ignore failing gdb pretty tests
    These tests are blocking a linux nightly and a new snapshot, so ignore them for
    now. Their tracking issue is #16919.

--- src/test/debuginfo/gdb-pretty-struct-and-enums.rs
+++ src/test/debuginfo/gdb-pretty-struct-and-enums.rs
@@ -8,6 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// ignore-test FIXME(#16919)
 // ignore-tidy-linelength
 // ignore-lldb
 // ignore-android: FIXME(#10381)
