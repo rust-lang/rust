6349a61231b8c0571cf3db97a68c7215e04a8791
Niko Matsakis <niko@alum.mit.edu>
2014-09-12 10:54:08 -0400

    Port coherence to use the new trait matching code

--- src/librustc/middle/dead.rs
+++ src/librustc/middle/dead.rs
@@ -470,7 +470,7 @@ impl<'a, 'tcx> DeadVisitor<'a, 'tcx> {
         match self.tcx.inherent_impls.borrow().find(&local_def(id)) {
             None => (),
             Some(impl_list) => {
-                for impl_did in impl_list.borrow().iter() {
+                for impl_did in impl_list.iter() {
                     for item_did in impl_items.get(impl_did).iter() {
                         if self.live_symbols.contains(&item_did.def_id()
                                                                .node) {
