d3117a09d82532d497795fa2a69c7a7af914770a
Alexis Beingessner <a.beingessner@gmail.com>
2014-09-14 23:25:08 -0400

    impl ExactSize for vec::MoveItems

--- src/libcollections/vec.rs
+++ src/libcollections/vec.rs
@@ -1662,6 +1662,8 @@ impl<T> DoubleEndedIterator<T> for MoveItems<T> {
     }
 }
 
+impl<T> ExactSize<T> for MoveItems<T> {}
+
 #[unsafe_destructor]
 impl<T> Drop for MoveItems<T> {
     fn drop(&mut self) {
