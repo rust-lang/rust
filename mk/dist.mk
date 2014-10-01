c5d2ed54a364776b1d3b7552bd474191df3619fb
Aaron Turon <aturon@mozilla.com>
2014-09-30 23:10:10 -0700

    Remove libuv, gyp
    This commit removes the libuv and gyp submodules, as well as all build
    infrastructure related to them.
    
    For more context, see the [runtime removal
    RFC](https://github.com/rust-lang/rfcs/pull/230)
    
    [breaking-change]

--- mk/dist.mk
+++ mk/dist.mk
@@ -35,7 +35,7 @@ LICENSE.txt: $(S)COPYRIGHT $(S)LICENSE-APACHE $(S)LICENSE-MIT
 
 PKG_TAR = dist/$(PKG_NAME).tar.gz
 
-PKG_GITMODULES := $(S)src/libuv $(S)src/llvm $(S)src/gyp $(S)src/compiler-rt \
+PKG_GITMODULES := $(S)src/llvm $(S)src/compiler-rt \
 		  $(S)src/rt/hoedown $(S)src/jemalloc
 PKG_FILES := \
     $(S)COPYRIGHT                              \
