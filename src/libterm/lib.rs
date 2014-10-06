199641bffabc52d0d9b59415407c19b98182784e
Alex Crichton <alex@alexcrichton.com>
2014-10-06 16:31:39 -0700

    term: Convert statics to constants

--- src/libterm/lib.rs
+++ src/libterm/lib.rs
@@ -155,23 +155,23 @@ pub mod color {
     /// Number for a terminal color
     pub type Color = u16;
 
-    pub static BLACK:   Color = 0u16;
-    pub static RED:     Color = 1u16;
-    pub static GREEN:   Color = 2u16;
-    pub static YELLOW:  Color = 3u16;
-    pub static BLUE:    Color = 4u16;
-    pub static MAGENTA: Color = 5u16;
-    pub static CYAN:    Color = 6u16;
-    pub static WHITE:   Color = 7u16;
+    pub const BLACK:   Color = 0u16;
+    pub const RED:     Color = 1u16;
+    pub const GREEN:   Color = 2u16;
+    pub const YELLOW:  Color = 3u16;
+    pub const BLUE:    Color = 4u16;
+    pub const MAGENTA: Color = 5u16;
+    pub const CYAN:    Color = 6u16;
+    pub const WHITE:   Color = 7u16;
 
-    pub static BRIGHT_BLACK:   Color = 8u16;
-    pub static BRIGHT_RED:     Color = 9u16;
-    pub static BRIGHT_GREEN:   Color = 10u16;
-    pub static BRIGHT_YELLOW:  Color = 11u16;
-    pub static BRIGHT_BLUE:    Color = 12u16;
-    pub static BRIGHT_MAGENTA: Color = 13u16;
-    pub static BRIGHT_CYAN:    Color = 14u16;
-    pub static BRIGHT_WHITE:   Color = 15u16;
+    pub const BRIGHT_BLACK:   Color = 8u16;
+    pub const BRIGHT_RED:     Color = 9u16;
+    pub const BRIGHT_GREEN:   Color = 10u16;
+    pub const BRIGHT_YELLOW:  Color = 11u16;
+    pub const BRIGHT_BLUE:    Color = 12u16;
+    pub const BRIGHT_MAGENTA: Color = 13u16;
+    pub const BRIGHT_CYAN:    Color = 14u16;
+    pub const BRIGHT_WHITE:   Color = 15u16;
 }
 
 /// Terminal attributes
