2288f332301b9e22db2890df256322650a7f3445
Brian Anderson <banderson@mozilla.com>
2014-10-06 16:19:37 -0700

    Update html_root_url for 0.12.0 release

--- src/libgraphviz/lib.rs
+++ src/libgraphviz/lib.rs
@@ -273,7 +273,7 @@ pub fn main() {
 #![license = "MIT/ASL2"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://doc.rust-lang.org/master/")]
+       html_root_url = "http://doc.rust-lang.org/0.12.0/")]
 
 use std::io;
 use std::str;
--- src/libterm/lib.rs
+++ src/libterm/lib.rs
@@ -46,7 +46,7 @@
 #![crate_type = "dylib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://doc.rust-lang.org/master/",
+       html_root_url = "http://doc.rust-lang.org/0.12.0/",
        html_playground_url = "http://play.rust-lang.org/")]
 
 #![allow(unknown_features)]
