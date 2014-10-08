261a40519e48ce9d2efd4dac686ae5b59ba87768
Brian Koropoff <bkoropoff@gmail.com>
2014-10-08 00:08:30 -0700

    Add regression test for issue #17403

---
+++ src/test/compile-fail/regions-return-ref-to-upvar-issue-17403.rs
@@ -0,0 +1,30 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Test that closures cannot subvert aliasing restrictions
+
+#![feature(overloaded_calls, unboxed_closures)]
+
+fn main() {
+    // Unboxed closure case
+    {
+        let mut x = 0u;
+        let mut f = |&mut:| &mut x; //~ ERROR cannot infer
+        let x = f();
+        let y = f();
+    }
+    // Boxed closure case
+    {
+        let mut x = 0u;
+        let f = || &mut x; //~ ERROR cannot infer
+        let x = f();
+        let y = f();
+    }
+}
