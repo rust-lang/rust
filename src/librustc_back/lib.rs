40bf10f73924a04dc786a0d363b49f995a7a872d
Brian Anderson <banderson@mozilla.com>
2014-10-09 10:47:22 -0700

    Use the same html_root_url for all docs

--- src/librustc_back/lib.rs
+++ src/librustc_back/lib.rs
@@ -29,7 +29,7 @@
 #![crate_type = "rlib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-      html_root_url = "http://doc.rust-lang.org/")]
+      html_root_url = "http://doc.rust-lang.org/nightly/")]
 
 #![allow(unknown_features)]
 #![feature(globs, phase, macro_rules, slicing_syntax)]
--- src/librustc_llvm/lib.rs
+++ src/librustc_llvm/lib.rs
@@ -20,7 +20,7 @@
 #![crate_type = "rlib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
-       html_root_url = "http://doc.rust-lang.org/")]
+       html_root_url = "http://doc.rust-lang.org/nightly/")]
 
 #![feature(globs)]
 #![feature(link_args)]
