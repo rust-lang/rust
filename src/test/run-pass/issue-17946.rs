6b23ab689f88fcefc9014d371fc3a8c8b92f9830
Alejandro Sánchez <contacto@alsanchez.es>
2014-10-12 13:16:29 +0200

    Fix formatting

--- src/test/run-pass/issue-17946.rs
+++ src/test/run-pass/issue-17946.rs
@@ -1,7 +1,7 @@
 // Copyright 2014 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
-// 
+//
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
@@ -13,7 +13,7 @@ use std::io::fs::PathExtensions;
 
 fn main() {
     let tmp_dir = io::TempDir::new("").unwrap();
-    let tmp_file = tmp_dir.path().join("file"); 
+    let tmp_file = tmp_dir.path().join("file");
     io::fs::File::create(&tmp_file).unwrap();
     io::fs::change_file_times(&tmp_file, 10000999, 20000999).unwrap();
     let stat = tmp_file.stat().unwrap();
