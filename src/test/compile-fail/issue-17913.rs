9ce86cf09daf28edc5339075f683413babf1fc39
Ariel Ben-Yehuda <ariel.byd@gmail.com>
2014-10-15 01:53:37 +0300

    add some tests

---
+++ src/test/compile-fail/issue-17913.rs
@@ -0,0 +1,17 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// error-pattern: too big for the current ABI
+
+fn main() {
+    let n = 0u;
+    let a = box [&n,..0xF000000000000000u];
+    println!("{}", a[0xFFFFFFu]);
+}
