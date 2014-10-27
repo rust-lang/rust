e02b6d17486ecef8541d03bb6a38c52d1a35b339
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-10-27 12:55:16 +0100

    destructor checker (dropck).
    Largely adapted from pcwalton's original branch, with following
    notable modifications:
    
    Use `regionck::type_must_outlive` to generate `SafeDestructor`
    constraints.  (this plugged some soundness holes in the analysis).
    
    Avoid exponential time blowup on compile-fail/huge-struct.rs by
    keeping the breadcrumbs until end of traversal.
    
    Avoid premature return from regionck::visit_expr.
    
    Factored drop-checking code out into dropck module.
    
    Added `SafeDestructor` to enum `SubregionOrigin` (for error reporting).
    
    ----
    
    Since this imposes restrictions on the lifetimes used in types with
    destructors, this is a (wait for it)
    
    [breaking-change]

--- src/librustc/middle/infer/error_reporting.rs
+++ src/librustc/middle/infer/error_reporting.rs
@@ -242,7 +242,7 @@ impl<'a, 'tcx> ErrorReporting<'tcx> for InferCtxt<'a, 'tcx> {
                     }
                 }
                 SubSupConflict(var_origin, _, sub_r, _, sup_r) => {
-                    debug!("processing SubSupConflict");
+                    debug!("processing SubSupConflict sub: {:?} sup: {:?}", sub_r, sup_r);
                     match free_regions_from_same_fn(self.tcx, sub_r, sup_r) {
                         Some(ref same_frs) => {
                             var_origins.push(var_origin);
@@ -707,6 +707,23 @@ impl<'a, 'tcx> ErrorReporting<'tcx> for InferCtxt<'a, 'tcx> {
                     self.tcx,
                     "type is only valid for ",
                     sup,
+                    "");
+            }
+            infer::SafeDestructor(span) => {
+                self.tcx.sess.span_err(
+                    span,
+                    "unsafe use of destructor: destructor might be called \
+                     while references are dead");
+                // FIXME (22171): terms "super/subregion" are suboptimal
+                note_and_explain_region(
+                    self.tcx,
+                    "superregion: ",
+                    sup,
+                    "");
+                note_and_explain_region(
+                    self.tcx,
+                    "subregion: ",
+                    sub,
                     "");
             }
             infer::BindingTypeIsNotValidAtDecl(span) => {
@@ -1628,6 +1645,12 @@ impl<'a, 'tcx> ErrorReportingHelpers<'tcx> for InferCtxt<'a, 'tcx> {
                     span,
                     &format!("...so that the declared lifetime parameter bounds \
                                 are satisfied")[]);
+            }
+            infer::SafeDestructor(span) => {
+                self.tcx.sess.span_note(
+                    span,
+                    "...so that references are valid when the destructor \
+                     runs")
             }
         }
     }
