7ba7bc56039c154a5947beb73e8ced31be7b12d1
gamazeps <gamaz3ps@gmail.com>
2014-10-30 19:00:47 +0100

    Doc: Clears up trim_char doc
    Closes #18451

--- src/libcore/str.rs
+++ src/libcore/str.rs
@@ -1535,7 +1535,7 @@ pub trait StrSlice<'a> {
     /// ```
     fn ends_with(&self, needle: &str) -> bool;
 
-    /// Returns a string with characters that match `to_trim` removed.
+    /// Returns a string with characters that match `to_trim` removed from the left and the right.
     ///
     /// # Arguments
     ///
