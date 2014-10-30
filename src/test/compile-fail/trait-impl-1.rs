af922608d2ba1f8dc27089fe50571ff408849dde
Nick Cameron <ncameron@mozilla.com>
2014-10-30 15:41:07 +1300

    tests

---
+++ src/test/compile-fail/trait-impl-1.rs
@@ -0,0 +1,26 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Test calling methods on an impl for a bare trait. This test checks that the
+// trait impl is only applied to a trait object, not concrete types which implement
+// the trait.
+
+trait T {}
+
+impl<'a> T+'a {
+    fn foo(&self) {}
+}
+
+impl T for int {}
+
+fn main() {
+    let x = &42i;
+    x.foo(); //~ERROR: type `&int` does not implement any method in scope named `foo`
+}
