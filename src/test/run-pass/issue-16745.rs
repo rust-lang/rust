90b6404f200aa1f8d60167aa725d2a9d63a03b99
Jakub Bukaj <jakub@jakub.cc>
2014-11-02 15:49:35 +0100

    Make const_eval of literals more type aware
    Fixes #16745.
    Fixes #17631.

---
+++ src/test/run-pass/issue-16745.rs
@@ -0,0 +1,26 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+fn is_whitespace(b: u8) -> bool {
+    match b {
+        b' '  => true,
+        b'\t' => true,
+        b'\n' => true,
+        0x0B  => true,
+        0x0C  => true,
+        b'\r' => true,
+        _     => false,
+    }
+}
+
+fn main() {
+    assert!(is_whitespace('\n' as u8));
+    assert!(!is_whitespace('f' as u8));
+}
