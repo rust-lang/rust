bc086790c1d2aab4786d43952bc623093e0b6577
Alex Crichton <alex@alexcrichton.com>
2014-11-02 23:10:09 -0800

    Test fixes and rebase conflicts

--- src/test/run-pass/macro-include-items.rs
+++ src/test/run-pass/macro-include-items.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// ignore-pretty
+
 fn bar() {}
 
 include!(concat!("", "", "../auxiliary/", "macro-include-items-item.rs"))
