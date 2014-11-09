1afed0df3f5ccd860b0a3b2780be4f39d9221cd3
Brian Koropoff <bkoropoff@gmail.com>
2014-11-08 21:49:29 -0800

    Fix upvars sometimes not being marked as used mutably
    Drill down the loan path for McDeclared references as well since
    it might lead to an upvar.  Closes #18769

--- src/librustc/middle/borrowck/gather_loans/mod.rs
+++ src/librustc/middle/borrowck/gather_loans/mod.rs
@@ -395,10 +395,10 @@ impl<'a, 'tcx> GatherLoanCtxt<'a, 'tcx> {
             LpUpvar(ty::UpvarId{ var_id: local_id, closure_expr_id: _ }) => {
                 self.tcx().used_mut_nodes.borrow_mut().insert(local_id);
             }
-            LpExtend(ref base, mc::McInherited, _) => {
+            LpExtend(ref base, mc::McInherited, _) |
+            LpExtend(ref base, mc::McDeclared, _) => {
                 self.mark_loan_path_as_mutated(&**base);
             }
-            LpExtend(_, mc::McDeclared, _) |
             LpExtend(_, mc::McImmutable, _) => {
                 // Nothing to do.
             }
