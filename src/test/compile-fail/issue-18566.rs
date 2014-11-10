67a694443efb5db3da7c6f4bfbb1ae18c915a75d
Murarth <murarth@gmail.com>
2014-11-10 16:47:32 -0700

    Add regression test for #18566

---
+++ src/test/compile-fail/issue-18566.rs
@@ -0,0 +1,33 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(tuple_indexing)]
+
+struct MyPtr<'a>(&'a mut uint);
+impl<'a> Deref<uint> for MyPtr<'a> {
+    fn deref<'b>(&'b self) -> &'b uint { self.0 }
+}
+
+trait Tr {
+    fn poke(&self, s: &mut uint);
+}
+
+impl Tr for uint {
+    fn poke(&self, s: &mut uint)  {
+        *s = 2;
+    }
+}
+
+fn main() {
+    let s = &mut 1u;
+
+    MyPtr(s).poke(s);
+    //~^ ERROR cannot borrow `*s` as mutable more than once at a time
+}
