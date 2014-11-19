883b8340683461d67b4653a83e8add6062246f91
Manish Goregaokar <manishsmail@gmail.com>
2014-11-20 00:18:41 +0530

    Example for match_if_let

---
+++ examples/match_if_let.rs
@@ -0,0 +1,23 @@
+#![feature(phase)]
+
+#[phase(plugin)]
+extern crate rust_clippy;
+
+
+fn main(){
+    let x = Some(1u);
+    match x {
+        Some(y) => println!("{}", y),
+        _ => ()
+    }
+    // Not linted
+    match x {
+        Some(y) => println!("{}", y),
+        None => ()
+    }
+    let z = (1u,1u);
+    match z {
+        (2...3, 7...9) => println!("{}", z),
+        _ => {}
+    }
+}
