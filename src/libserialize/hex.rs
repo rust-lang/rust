d252cd8fe1c97e0a63c03b435db05dd884e78d95
Alex Crichton <alex@alexcrichton.com>
2014-11-20 10:11:15 -0800

    std: Align `raw` modules with unsafe conventions
    This commit is an implementation of [RFC 240][rfc] when applied to the standard
    library. It primarily deprecates the entirety of `string::raw`, `vec::raw`,
    `slice::raw`, and `str::raw` in favor of associated functions, methods, and
    other free functions. The detailed renaming is:
    
    * slice::raw::buf_as_slice => slice::with_raw_buf
    * slice::raw::mut_buf_as_slice => slice::with_raw_mut_buf
    * slice::shift_ptr => deprecated with no replacement
    * slice::pop_ptr => deprecated with no replacement
    * str::raw::from_utf8 => str::from_utf8_unchecked
    * str::raw::c_str_to_static_slice => str::from_c_str
    * str::raw::slice_bytes => deprecated for slice_unchecked (slight semantic diff)
    * str::raw::slice_unchecked => str.slice_unchecked
    * string::raw::from_parts => String::from_raw_parts
    * string::raw::from_buf_len => String::from_raw_buf_len
    * string::raw::from_buf => String::from_raw_buf
    * string::raw::from_utf8 => String::from_utf8_unchecked
    * vec::raw::from_buf => Vec::from_raw_buf
    
    All previous functions exist in their `#[deprecated]` form, and the deprecation
    messages indicate how to migrate to the newer variants.
    
    [rfc]: https://github.com/rust-lang/rfcs/blob/master/text/0240-unsafe-api-location.md
    [breaking-change]
    
    Closes #17863

--- src/libserialize/hex.rs
+++ src/libserialize/hex.rs
@@ -15,7 +15,6 @@
 pub use self::FromHexError::*;
 
 use std::fmt;
-use std::string;
 use std::error;
 
 /// A trait for converting a value to hexadecimal encoding
@@ -51,7 +50,7 @@ impl ToHex for [u8] {
         }
 
         unsafe {
-            string::raw::from_utf8(v)
+            String::from_utf8_unchecked(v)
         }
     }
 }
@@ -100,7 +99,7 @@ impl FromHex for str {
      * Convert any hexadecimal encoded string (literal, `@`, `&`, or `~`)
      * to the byte values it encodes.
      *
-     * You can use the `String::from_utf8` function in `std::string` to turn a
+     * You can use the `String::from_utf8` function to turn a
      * `Vec<u8>` into a string with characters corresponding to those values.
      *
      * # Example
--- src/libstd/ascii.rs
+++ src/libstd/ascii.rs
@@ -21,7 +21,7 @@ use mem;
 use option::{Option, Some, None};
 use slice::{SlicePrelude, AsSlice};
 use str::{Str, StrPrelude};
-use string::{mod, String, IntoString};
+use string::{String, IntoString};
 use vec::Vec;
 
 /// Datatype to hold one ascii character. It wraps a `u8`, with the highest bit always zero.
@@ -328,9 +328,7 @@ impl AsciiStr for [Ascii] {
 impl IntoString for Vec<Ascii> {
     #[inline]
     fn into_string(self) -> String {
-        unsafe {
-            string::raw::from_utf8(self.into_bytes())
-        }
+        unsafe { String::from_utf8_unchecked(self.into_bytes()) }
     }
 }
 
@@ -391,13 +389,13 @@ impl AsciiExt<String> for str {
     #[inline]
     fn to_ascii_upper(&self) -> String {
         // Vec<u8>::to_ascii_upper() preserves the UTF-8 invariant.
-        unsafe { string::raw::from_utf8(self.as_bytes().to_ascii_upper()) }
+        unsafe { String::from_utf8_unchecked(self.as_bytes().to_ascii_upper()) }
     }
 
     #[inline]
     fn to_ascii_lower(&self) -> String {
         // Vec<u8>::to_ascii_lower() preserves the UTF-8 invariant.
-        unsafe { string::raw::from_utf8(self.as_bytes().to_ascii_lower()) }
+        unsafe { String::from_utf8_unchecked(self.as_bytes().to_ascii_lower()) }
     }
 
     #[inline]
@@ -410,13 +408,13 @@ impl OwnedAsciiExt for String {
     #[inline]
     fn into_ascii_upper(self) -> String {
         // Vec<u8>::into_ascii_upper() preserves the UTF-8 invariant.
-        unsafe { string::raw::from_utf8(self.into_bytes().into_ascii_upper()) }
+        unsafe { String::from_utf8_unchecked(self.into_bytes().into_ascii_upper()) }
     }
 
     #[inline]
     fn into_ascii_lower(self) -> String {
         // Vec<u8>::into_ascii_lower() preserves the UTF-8 invariant.
-        unsafe { string::raw::from_utf8(self.into_bytes().into_ascii_lower()) }
+        unsafe { String::from_utf8_unchecked(self.into_bytes().into_ascii_lower()) }
     }
 }
 
