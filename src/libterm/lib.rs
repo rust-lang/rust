0b78522294fabecb0dcf9d2b8b186c336ceda33d
Niko Matsakis <niko@alum.mit.edu>
2014-11-20 19:45:42 -0500

    Fix odd example where bounds were permitted and then ignored

--- src/libterm/lib.rs
+++ src/libterm/lib.rs
@@ -115,7 +115,7 @@ pub fn stdout() -> Option<Box<Terminal<WriterWrapper> + Send>> {
 #[cfg(not(windows))]
 /// Return a Terminal wrapping stderr, or None if a terminal couldn't be
 /// opened.
-pub fn stderr() -> Option<Box<Terminal<WriterWrapper> + Send> + Send> {
+pub fn stderr() -> Option<Box<Terminal<WriterWrapper> + Send>> {
     TerminfoTerminal::new(WriterWrapper {
         wrapped: box std::io::stderr() as Box<Writer + Send>,
     })
