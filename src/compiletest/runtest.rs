16844198979f574e53a8c3cad460a1f37754fef2
Alex Crichton <alex@alexcrichton.com>
2014-11-21 14:15:33 -0800

    Register new snapshots

--- src/compiletest/runtest.rs
+++ src/compiletest/runtest.rs
@@ -7,7 +7,7 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-#[cfg(not(stage0))]
+
 use self::TargetLocation::*;
 
 use common::Config;
