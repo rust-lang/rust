79d9bebf49ec56078439816f4856dc4e47aa6523
Steven Fackler <sfackler@gmail.com>
2014-11-25 10:00:46 -0800

    Fix xcrate enum namespacing
    Closes #19293

--- src/test/run-pass/struct_variant_xc_match.rs
+++ src/test/run-pass/struct_variant_xc_match.rs
@@ -11,7 +11,7 @@
 // aux-build:struct_variant_xc_aux.rs
 extern crate struct_variant_xc_aux;
 
-use struct_variant_xc_aux::{StructVariant, Variant};
+use struct_variant_xc_aux::Enum::{StructVariant, Variant};
 
 pub fn main() {
     let arg = match (StructVariant { arg: 42 }) {
