1f1c5d54bfd09285816c24d9f7e54d360787c7c7
Steven Fackler <sfackler@gmail.com>
2014-12-05 20:22:35 -0800

    Ignore issue #16671 test on android (again)
    Seems to be blocking forever

--- src/test/run-pass/issue-16671.rs
+++ src/test/run-pass/issue-16671.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// DON'T REENABLE THIS UNLESS YOU'VE ACTUALLY FIXED THE UNDERLYING ISSUE
+// ignore-android seems to block forever
 
 #![forbid(warnings)]
 
