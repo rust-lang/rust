2193799c1ae0716fb75be1dcaa07c06756d55f17
P1start <rewi-github@whanau.org>
2014-12-06 13:17:30 +1300

    Make the naming lints only warn on names with upper/lowercase equivalents
    Closes #21735.

--- src/test/compile-fail/lint-non-uppercase-statics.rs
+++ src/test/compile-fail/lint-non-uppercase-statics.rs
@@ -11,6 +11,6 @@
 #![forbid(non_upper_case_globals)]
 #![allow(dead_code)]
 
-static foo: isize = 1; //~ ERROR static constant `foo` should have an uppercase name such as `FOO`
+static foo: isize = 1; //~ ERROR static constant `foo` should have an upper case name such as `FOO`
 
 fn main() { }
