17a9c2764f4713bb1a040afd535af5a30d02a828
Jorge Aparicio <japaricious@gmail.com>
2014-12-09 17:47:11 -0500

    libcore: allow deprecated `valN` methods on doc tests

--- src/libcore/tuple/mod.rs
+++ src/libcore/tuple/mod.rs
@@ -38,9 +38,12 @@
 //! Using methods:
 //!
 //! ```
+//! #[allow(deprecated)]
+//! # fn main() {
 //! let pair = ("pi", 3.14f64);
 //! assert_eq!(pair.val0(), "pi");
 //! assert_eq!(pair.val1(), 3.14f64);
+//! # }
 //! ```
 //!
 //! Using traits implemented for tuples:
