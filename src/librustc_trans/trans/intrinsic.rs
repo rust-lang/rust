ccd88c523597e2e33a32e92175659d8f5e9a475b
Joshua Yanovski <pythonesque@gmail.com>
2014-12-14 01:44:24 -0800

    Add LLVM's unordered intrinsic to Rust.

--- src/librustc_trans/trans/intrinsic.rs
+++ src/librustc_trans/trans/intrinsic.rs
@@ -518,6 +518,7 @@ pub fn trans_intrinsic_call<'a, 'blk, 'tcx>(mut bcx: Block<'blk, 'tcx>,
                 llvm::SequentiallyConsistent
             } else {
                 match split[2] {
+                    "unordered" => llvm::Unordered,
                     "relaxed" => llvm::Monotonic,
                     "acq"     => llvm::Acquire,
                     "rel"     => llvm::Release,
