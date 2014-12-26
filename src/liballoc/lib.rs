efaa43ade5cbc6636c955581384324c8f7ace940
John Ericson <Ericson2314@Yahoo.com>
2014-12-26 23:28:04 +0100

    liballoc's "external_funcs" and "external_crate" are now features
    This allows the vanilla libary to built for kernel use with Cargo.

--- src/liballoc/lib.rs
+++ src/liballoc/lib.rs
@@ -70,7 +70,7 @@
 #[macro_use]
 extern crate core;
 
-#[cfg(all(not(external_funcs), not(external_crate)))]
+#[cfg(all(not(feature = "external_funcs"), not(feature = "external_crate")))]
 extern crate libc;
 
 // Allow testing this library
