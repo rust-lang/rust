d4da75892219ee8fed5fc8801a37ffe82520083d
Seo Sanghyeon <sanxiyn@gmail.com>
2014-12-27 19:43:14 +0900

    Use -Z unstable-options for span debugger

--- src/librustc_driver/driver.rs
+++ src/librustc_driver/driver.rs
@@ -138,7 +138,7 @@ pub fn phase_1_parse_input(sess: &Session, cfg: ast::CrateConfig, input: &Input)
         krate.encode(&mut json).unwrap();
     }
 
-    if sess.show_span() {
+    if sess.opts.show_span.is_some() {
         syntax::show_span::run(sess.diagnostic(), &krate);
     }
 
@@ -542,7 +542,7 @@ pub fn stop_after_phase_1(sess: &Session) -> bool {
         debug!("invoked with --parse-only, returning early from compile_input");
         return true;
     }
-    if sess.show_span() {
+    if sess.opts.show_span.is_some() {
         return true;
     }
     return sess.opts.debugging_opts & config::AST_JSON_NOEXPAND != 0;
