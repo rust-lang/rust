fe4c8dc2a2089e629e461128c069c613323f8d0c
Valerii Hiora <valerii.hiora@gmail.com>
2014-12-28 08:31:23 +0200

    Fixes invalid LLVM data layout for aggregate data types
    According to http://llvm.org/docs/LangRef.html#data-layout correct syntax
    for data layout is `a:<abi>:<pref>` so it looks like  `a0:<abi>:<pref>` is
    either a typo or outdated syntax (as it goes back pretty deep in time)

--- src/librustc_back/target/i686_apple_darwin.rs
+++ src/librustc_back/target/i686_apple_darwin.rs
@@ -18,7 +18,7 @@ pub fn target() -> Target {
         data_layout: "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16\
                       -i32:32:32-i64:32:64\
                       -f32:32:32-f64:32:64-v64:64:64\
-                      -v128:128:128-a0:0:64-f80:128:128\
+                      -v128:128:128-a:0:64-f80:128:128\
                       -n8:16:32".to_string(),
         llvm_target: "i686-apple-darwin".to_string(),
         target_endian: "little".to_string(),
