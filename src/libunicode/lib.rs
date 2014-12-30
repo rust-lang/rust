abdeefdbcc96e0f270a4f74892589e1e6cb9b928
Huon Wilson <dbau.pp+github@gmail.com>
2014-12-30 12:04:12 +1100

    Remove deprecated functionality from `char`.

--- src/libunicode/lib.rs
+++ src/libunicode/lib.rs
@@ -58,9 +58,7 @@ mod u_str;
 /// however the converse is not always true due to the above range limits
 /// and, as such, should be performed via the `from_u32` function..
 pub mod char {
-    pub use core::char::{MAX, from_u32};
-    pub use core::char::{from_digit};
-    pub use core::char::Char;
+    pub use core::char::{MAX, from_u32, from_digit, Char};
 
     pub use normalize::{decompose_canonical, decompose_compatible, compose};
 
