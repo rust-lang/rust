d578bea2ced87cd171dbbbbfdcd2bb03f28c9a9e
David Creswick <dcrewi@gyrae.net>
2014-12-31 16:20:54 -0600

    fix "make install"
    There seems to be a problem introduced by
    8b3c67690c4747b9fadfef407e6261524fb03f8a that causes "make install"
    to fail when the build is not configured to skip doc building.

--- mk/install.mk
+++ mk/install.mk
@@ -28,7 +28,11 @@ endif
 # Remove tmp files because it's a decent amount of disk space
 	$(Q)rm -R tmp/dist
 
+ifeq ($(CFG_DISABLE_DOCS),)
+prepare_install: dist/$(PKG_NAME)-$(CFG_BUILD).tar.gz dist/$(DOC_PKG_NAME)-$(CFG_BUILD).tar.gz | tmp/empty_dir
+else
 prepare_install: dist/$(PKG_NAME)-$(CFG_BUILD).tar.gz | tmp/empty_dir
+endif
 
 uninstall:
 ifeq (root user, $(USER) $(patsubst %,user,$(SUDO_USER)))
