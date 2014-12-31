11382001e5f678c2e047f33cf0034ab1ae4c0ddd
Jorge Aparicio <japaricious@gmail.com>
2014-12-30 20:48:21 -0500

    rustc_back: unbox closures used in let bindings

--- src/librustc_back/target/mod.rs
+++ src/librustc_back/target/mod.rs
@@ -217,7 +217,7 @@ impl Target {
 
         let handler = diagnostic::default_handler(diagnostic::Auto, None);
 
-        let get_req_field = |name: &str| {
+        let get_req_field = |&: name: &str| {
             match obj.find(name)
                      .map(|s| s.as_string())
                      .and_then(|os| os.map(|s| s.to_string())) {
