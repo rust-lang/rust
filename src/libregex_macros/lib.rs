885a4a5604063b41d4d30ce3136ada97316417b5
Jorge Aparicio <japaricious@gmail.com>
2015-01-02 08:16:40 -0500

    regex_macros: fix fallout

--- src/libregex_macros/lib.rs
+++ src/libregex_macros/lib.rs
@@ -603,7 +603,7 @@ fn exec<'t>(which: ::regex::native::MatchKind, input: &'t str,
     // Converts `xs` to a `[x1, x2, .., xN]` expression by calling `to_expr`
     // on each element in `xs`.
     fn vec_expr<T, It, F>(&self, xs: It, mut to_expr: F) -> P<ast::Expr> where
-        It: Iterator<T>,
+        It: Iterator<Item=T>,
         F: FnMut(&ExtCtxt, T) -> P<ast::Expr>,
     {
         let exprs = xs.map(|x| to_expr(self.cx, x)).collect();
