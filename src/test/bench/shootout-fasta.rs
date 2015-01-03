ce8f748b4cd64317c33d64ccf3a6127931980d90
Jorge Aparicio <japaricious@gmail.com>
2015-01-03 17:29:27 -0500

    add feature gate to some benchmarks

--- src/test/bench/shootout-fasta.rs
+++ src/test/bench/shootout-fasta.rs
@@ -38,7 +38,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 // OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#![feature(slicing_syntax)]
+#![feature(associated_types, slicing_syntax)]
 
 use std::cmp::min;
 use std::io::{BufferedWriter, File};
