f7f5d0958b079a893678af2f439ef6a735365b18
FakeKane <andrewyli@gmail.com>
2015-01-05 16:46:46 -0500

    removing whitespace

--- src/libstd/tuple.rs
+++ src/libstd/tuple.rs
@@ -28,13 +28,13 @@
 //! * `Default`
 //!
 //! # Examples
-//! 
+//!
 //! Accessing elements of a tuple at specified indices:
-//! 
+//!
 //! ```
 //! let x = ("colorless",  "green", "ideas", "sleep", "furiously");
 //! assert_eq!(x.3, "sleep");
-//! 
+//!
 //! let v = (3i, 3i);
 //! let u = (1i, -5i);
 //! assert_eq!(v.0 * u.0 + v.1 * u.1, -12i);
