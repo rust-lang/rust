85e8fe3374e6891372a837567f0663dfcead9142
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-01-06 23:14:00 -0500

    Added lint for structs with zero fields in FFI blocks

--- src/librustc/lint/builtin.rs
+++ src/librustc/lint/builtin.rs
@@ -426,6 +426,24 @@ impl<'a, 'tcx> ImproperCTypesVisitor<'a, 'tcx> {
                                       representation annotation in foreign module, consider \
                                       adding a #[repr(...)] attribute to the type");
                 }
+
+                // FIXME: DO THIS CORRECTLY
+                if let ty_struct(...) = tty.sty {
+                    let ty_fields = ty::ty_to_def_id(tty).map(|ty_id| {
+                        ty::lookup_struct_fields(self.cx.tcx, ty_id)
+                    });
+                    match ty_fields {
+                        Some(ty_fields_really) => {
+                            if ty_fields_really.is_empty() {
+                                self.cx.span_lint(IMPROPER_CTYPES, sp,
+                                        "found struct with zero fields in foreign \
+                                        module, invalid C struct unless GCC extensions \
+                                        enabled");
+                            }
+                        },
+                        _ => unreachable!("struct should have fields!");
+                    }
+                }
             }
             _ => ()
         }
