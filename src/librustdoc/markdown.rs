bd58e3eb331f3a2ca03593b8661a13db1ed77100
Seth Faxon <seth.faxon@gmail.com>
2015-01-12 16:51:31 -0800

    Make the Rust Book more mobile friendly

--- src/librustdoc/markdown.rs
+++ src/librustdoc/markdown.rs
@@ -91,6 +91,7 @@ pub fn render(input: &str, mut output: Path, matches: &getopts::Matches,
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="generator" content="rustdoc">
     <title>{title}</title>
 
