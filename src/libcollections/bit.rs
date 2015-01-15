bc03bf87191c4770c8f584b30b78690121cd9904
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-15 22:56:53 +0200

    Revert stability for Bitv and BitvSet
    The collections were promoted to stable by mistake and do not match RFC 509.
    This reverts the stability back to unstable.
    
    [breaking-change] since previously stable API became unstable.
    
    Fixes #21193

--- src/libcollections/bit.rs
+++ src/libcollections/bit.rs
@@ -156,7 +156,7 @@ static FALSE: bool = false;
 /// println!("{:?}", bv);
 /// println!("total bits set to true: {}", bv.iter().filter(|x| *x).count());
 /// ```
-#[stable]
+#[unstable = "RFC 509"]
 pub struct Bitv {
     /// Internal representation of the bit vector
     storage: Vec<u32>,
@@ -1107,7 +1107,7 @@ impl<'a> RandomAccessIterator for Iter<'a> {
 /// assert!(bv[3]);
 /// ```
 #[derive(Clone)]
-#[stable]
+#[unstable = "RFC 509"]
 pub struct BitvSet {
     bitv: Bitv,
 }
