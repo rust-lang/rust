bbc1c749130062e521e9afa41d21deabe6486b77
Flavio Percoco <flaper87@gmail.com>
2015-01-15 01:40:33 +0100

    Forbid impls for builtin traits on types that are not structs/enums

---
+++ src/test/compile-fail/coherence-impls-builtin.rs
@@ -0,0 +1,37 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::marker::Send;
+
+enum TestE {
+  A
+}
+
+struct MyType;
+
+unsafe impl Send for TestE {}
+unsafe impl Send for MyType {}
+unsafe impl Send for (MyType, MyType) {}
+//~^ ERROR builtin traits can only be implemented on structs or enums
+
+unsafe impl Send for &'static MyType {}
+//~^ ERROR builtin traits can only be implemented on structs or enums
+
+unsafe impl Send for [MyType] {}
+//~^ ERROR builtin traits can only be implemented on structs or enums
+
+unsafe impl Send for &'static [MyType] {}
+//~^ ERROR builtin traits can only be implemented on structs or enums
+
+fn is_send<T: Send>() {}
+
+fn main() {
+    is_send::<(MyType, TestE)>();
+}
