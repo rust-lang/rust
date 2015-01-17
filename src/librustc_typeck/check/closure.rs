ab410cd66d1b8880fa0b744986fc9f70fa859dea
Brian Anderson <banderson@mozilla.com>
2015-01-17 13:36:59 -0800

    Add error codes to rustc_typeck

--- src/librustc_typeck/check/closure.rs
+++ src/librustc_typeck/check/closure.rs
@@ -50,10 +50,9 @@ pub fn check_expr_closure<'a,'tcx>(fcx: &FnCtxt<'a,'tcx>,
 
                     check_unboxed_closure(fcx, expr, kind, decl, body, None);
 
-                    fcx.ccx.tcx.sess.span_err(
-                        expr.span,
+                    span_err!(fcx.ccx.tcx.sess, expr.span, E0187,
                         "can't infer the \"kind\" of the closure, explicitly annotate it. e.g. \
-                        `|&:| {}`");
+                        `|&:| {{}}`");
                 },
                 Some((sig, kind)) => {
                     check_unboxed_closure(fcx, expr, kind, decl, body, Some(sig));
