9a4401fe828f2f29c359bc3c83f6f3b7eed27d83
Michael Sproul <micsproul@gmail.com>
2015-01-17 17:20:24 -0800

    Add some extended errors.

--- src/libsyntax/diagnostics/macros.rs
+++ src/libsyntax/diagnostics/macros.rs
@@ -59,3 +59,9 @@ macro_rules! register_diagnostics {
     )
 }
 
+#[macro_export]
+macro_rules! register_long_diagnostics {
+    ($($code:tt: $description:tt),*) => (
+        $(register_diagnostic! { $code, $description })*
+    )
+}
