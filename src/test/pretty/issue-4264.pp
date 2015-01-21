718badb99c7bd4db9731ed8d9567b4a114a28c07
Steve Klabnik <steve@steveklabnik.com>
2015-01-21 16:17:19 -0500

    remove copyright lines
    Fixes #9070.

--- src/test/pretty/issue-4264.pp
+++ src/test/pretty/issue-4264.pp
@@ -1,12 +1,4 @@
-#![no_std]
-#[macro_use]
-extern crate "std" as std;
-#[prelude_import]
 use std::prelude::v1::*;
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
