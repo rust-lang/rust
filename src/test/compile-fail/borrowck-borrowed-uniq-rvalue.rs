72002c6c4c7e29c65ead5ca9b4bcb334d59e038a
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-01-26 15:21:24 +0100

    accommodate new scoping rules in test/compile-fail.

--- src/test/compile-fail/borrowck-borrowed-uniq-rvalue.rs
+++ src/test/compile-fail/borrowck-borrowed-uniq-rvalue.rs
@@ -16,10 +16,11 @@ extern crate collections;
 use std::collections::HashMap;
 
 fn main() {
+    let tmp;
     let mut buggy_map: HashMap<usize, &usize> = HashMap::new();
     buggy_map.insert(42, &*box 1); //~ ERROR borrowed value does not live long enough
 
     // but it is ok if we use a temporary
-    let tmp = box 2;
+    tmp = box 2;
     buggy_map.insert(43, &*tmp);
 }
