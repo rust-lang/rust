d056ed0224d29b895ad565d648299147dfadde59
Brian Anderson <banderson@mozilla.com>
2015-01-30 14:57:21 -0800

    testparser has long lines

--- src/grammar/testparser.py
+++ src/grammar/testparser.py
@@ -9,6 +9,9 @@
 # <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
+
+# ignore-tidy-linelength
+
 import sys
 
 import os
