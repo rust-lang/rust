fbe78e46eb4a4d7118f96f93b35a3e846f064036
wonyong kim <wonyong.kim@samsung.com>
2015-01-30 16:38:25 +0900

    Enable some tests for android

--- src/test/run-pass/capturing-logging.rs
+++ src/test/run-pass/capturing-logging.rs
@@ -8,7 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// ignore-android (FIXME #11419)
 // exec-env:RUST_LOG=info
 
 #![allow(unknown_features)]
