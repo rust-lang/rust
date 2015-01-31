1935bbd91321b4ebb61b21a300f43234a0be6433
Edward Wang <edward.yu.wang@gmail.com>
2015-02-01 02:49:12 +0800

    Make use of a binary operator's RHS type for LHS inference
    For "symmetric" binary operators, meaning the types of two side must be
    equal, if the type of LHS doesn't know yet but RHS does, use that as an
    hint to infer LHS' type.
    
    Closes #21634

--- src/libsyntax/ast_util.rs
+++ src/libsyntax/ast_util.rs
@@ -102,6 +102,20 @@ pub fn is_by_value_binop(b: BinOp_) -> bool {
     }
 }
 
+/// Returns `true` if the binary operator is symmetric in the sense that LHS
+/// and RHS must have the same type. So the type of LHS can serve as an hint
+/// for the type of RHS and vice versa.
+pub fn is_symmetric_binop(b: BinOp_) -> bool {
+    match b {
+        BiAdd | BiSub | BiMul | BiDiv | BiRem |
+        BiBitXor | BiBitAnd | BiBitOr |
+        BiEq | BiLt | BiLe | BiNe | BiGt | BiGe => {
+            true
+        }
+        _ => false
+    }
+}
+
 /// Returns `true` if the unary operator takes its argument by value
 pub fn is_by_value_unop(u: UnOp) -> bool {
     match u {
