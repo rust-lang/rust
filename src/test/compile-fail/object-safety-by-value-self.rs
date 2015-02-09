8f5d22593328d18bd140e7ab10bdeccd0690928a
Niko Matsakis <niko@alum.mit.edu>
2015-02-09 08:54:34 -0500

    Extend object safety so that methods with Sized:Self are exempt.

--- src/test/compile-fail/object-safety-by-value-self.rs
+++ src/test/compile-fail/object-safety-by-value-self.rs
@@ -9,7 +9,8 @@
 // except according to those terms.
 
 // Check that we correctly prevent users from making trait objects
-// from traits with a `fn(self)` method.
+// from traits with a `fn(self)` method, unless `where Self : Sized`
+// is present on the method.
 
 trait Bar {
     fn bar(self);
@@ -17,6 +18,11 @@ trait Bar {
 
 trait Baz {
     fn baz(self: Self);
+}
+
+trait Quux {
+    // Legal because of the where clause:
+    fn baz(self: Self) where Self : Sized;
 }
 
 fn make_bar<T:Bar>(t: &T) -> &Bar {
@@ -41,6 +47,14 @@ fn make_baz_explicit<T:Baz>(t: &T) -> &Baz {
     t as &Baz
         //~^ ERROR `Baz` is not object-safe
         //~| NOTE method `baz` has a receiver type of `Self`
+}
+
+fn make_quux<T:Quux>(t: &T) -> &Quux {
+    t
+}
+
+fn make_quux_explicit<T:Quux>(t: &T) -> &Quux {
+    t as &Quux
 }
 
 fn main() {
