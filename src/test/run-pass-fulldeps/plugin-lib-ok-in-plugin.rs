b7683fc02b35cafc849b7097556f4f67c3adcfe9
Keegan McAllister <kmcallister@mozilla.com>
2015-02-11 22:33:07 -0800

    Warn when linking a plugin into a non-plugin crate
    Fixes #22202.

--- src/test/run-pass-fulldeps/plugin-lib-ok-in-plugin.rs
+++ src/test/run-pass-fulldeps/plugin-lib-ok-in-plugin.rs
@@ -9,19 +9,18 @@
 // except according to those terms.
 
 // aux-build:macro_crate_test.rs
+// aux-build:plugin_with_plugin_lib.rs
 // ignore-stage1
 // ignore-cross-compile
 //
 // macro_crate_test will not compile on a cross-compiled target because
 // libsyntax is not compiled for it.
 
-#![allow(plugin_as_library)]
+#![deny(plugin_as_library)]
 #![feature(plugin)]
 #![plugin(macro_crate_test)]
-
-extern crate macro_crate_test;
+#![plugin(plugin_with_plugin_lib)]
 
 fn main() {
     assert_eq!(1, make_a_1!());
-    macro_crate_test::foo();
 }
