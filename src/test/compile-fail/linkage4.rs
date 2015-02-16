96c41635b997a222ed868719b5575657d6850b02
Manish Goregaokar <manishsmail@gmail.com>
2015-02-17 00:49:42 +0530

    fix linkage tests

--- src/test/compile-fail/linkage4.rs
+++ src/test/compile-fail/linkage4.rs
@@ -10,6 +10,6 @@
 
 #[linkage = "external"]
 static foo: isize = 0;
-//~^ ERROR: the `linkage` attribute is experimental and not portable
+//~^^ ERROR: the `linkage` attribute is experimental and not portable
 
 fn main() {}
