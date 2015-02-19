0643494bc4d093dd660044b86c1d43820c40daf8
bombless <bombless@126.com>
2015-02-20 03:10:31 +0800

    Fix tests

--- src/test/compile-fail/issue-22426-2.rs
+++ src/test/compile-fail/issue-22426-2.rs
@@ -8,4 +8,5 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-fn a(B<) {} //~ ERROR unexpected token `<`
+fn a(B<) {}
+   //~^ error: unexpected token: `<`
