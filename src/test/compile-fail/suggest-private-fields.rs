3419d511991ae418ec55af594b303f37ffdc46c4
Junseok Lee <lee.junseok@berkeley.edu>
2015-02-18 17:58:15 -0800

    added local crate struct with priv field to test

--- src/test/compile-fail/suggest-private-fields.rs
+++ src/test/compile-fail/suggest-private-fields.rs
@@ -4,10 +4,23 @@ extern crate "struct-field-privacy" as xc;
 
 use xc::B;
 
+struct A {
+    pub a: u32,
+    b: u32,
+}
+
 fn main () {
+    // external crate struct
     let k = B {
         aa: 20, //~ ERROR structure `struct-field-privacy::B` has no field named `aa`
         //~^ HELP did you mean `a`?
         bb: 20, //~ ERROR structure `struct-field-privacy::B` has no field named `bb`
+    };
+    // local crate struct
+    let l = A {
+        aa: 20, //~ ERROR structure `A` has no field named `aa`
+        //~^ HELP did you mean `a`?
+        bb: 20, //~ ERROR structure `A` has no field named `bb`
+        //~^ HELP did you mean `b`?
     };
 }
