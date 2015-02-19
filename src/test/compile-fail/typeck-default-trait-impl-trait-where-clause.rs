dd0c64f760d9586d4effc8434707b16c1ba3d788
Niko Matsakis <niko@alum.mit.edu>
2015-02-19 10:30:45 -0500

    some comments and nits

--- src/test/compile-fail/typeck-default-trait-impl-trait-where-clause.rs
+++ src/test/compile-fail/typeck-default-trait-impl-trait-where-clause.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// ignore-tidy-linelength
+
 // Test that when a `..` impl applies, we also check that any
 // supertrait conditions are met.
 
