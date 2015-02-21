d360b76a85c9e04b3eb651da68aed9f3ffe05841
Flavio Percoco <flaper87@gmail.com>
2015-02-21 01:01:48 +0100

    allow negative impls for traits that have a default impl

--- src/test/compile-fail/typeck-negative-impls-builtin.rs
+++ src/test/compile-fail/typeck-negative-impls-builtin.rs
@@ -17,6 +17,6 @@ trait TestTrait {
 }
 
 impl !TestTrait for TestType {}
-//~^ ERROR  negative impls are currently allowed just for `Send` and `Sync`
+//~^ ERROR negative impls are only allowed for traits with default impls (e.g., `Send` and `Sync`)
 
 fn main() {}
