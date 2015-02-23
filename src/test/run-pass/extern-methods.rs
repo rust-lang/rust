f17a5220cb71c4f67476738fbbef803265505131
Manish Goregaokar <manishsmail@gmail.com>
2015-02-23 18:43:43 +0530

    Add marker trait to test (fixup #22538)

--- src/test/run-pass/extern-methods.rs
+++ src/test/run-pass/extern-methods.rs
@@ -7,7 +7,10 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-trait A {
+
+use std::marker::MarkerTrait;
+
+trait A : MarkerTrait {
     extern "fastcall" fn test1(i: i32);
     extern fn test2(i: i32);
 }
