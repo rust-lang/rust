f66a3f7bba378d4a308e02a860a11ab5f9f8e4ad
Keegan McAllister <kmcallister@mozilla.com>
2015-02-27 11:14:42 -0800

    Replace MacExpr / MacPat / MacItems with MacEager
    MacEager is a MacResult implementation for the common case where you've already
    built each form of AST that you might return.
    
    Fixes #17637. Based on #18814.
    
    This is a [breaking-change] for syntax extensions:
    
      * MacExpr::new becomes MacEager::expr.
    
      * MacPat::new becomes MacEager::pat.
    
      * MacItems::new becomes MacEager::items. It takes a SmallVector directly,
        not an iterator.

--- src/test/auxiliary/roman_numerals.rs
+++ src/test/auxiliary/roman_numerals.rs
@@ -19,7 +19,7 @@ extern crate rustc;
 use syntax::codemap::Span;
 use syntax::parse::token;
 use syntax::ast::{TokenTree, TtToken};
-use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacExpr};
+use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacEager};
 use syntax::ext::build::AstBuilder;  // trait for expr_usize
 use rustc::plugin::Registry;
 
@@ -61,7 +61,7 @@ fn expand_rn(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
         }
     }
 
-    MacExpr::new(cx.expr_usize(sp, total))
+    MacEager::expr(cx.expr_usize(sp, total))
 }
 
 #[plugin_registrar]
