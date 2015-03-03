a6a7e52bbee0ea70f71db289fb62fff7b5e4ef33
Alex Crichton <alex@alexcrichton.com>
2015-03-03 13:05:55 -0800

    libc: Move features behind a cfg_attr gate
    This same source is being built in the Cargo ecosystem and hence needs to build
    on stable Rust as well. This commit places the `no_std` attribute along with the
    `no_std` feature behind a `cfg_attr` flag so they are not processed when
    compiled on crates.io

--- src/liblibc/lib.rs
+++ src/liblibc/lib.rs
@@ -10,13 +10,10 @@
 
 #![crate_name = "libc"]
 #![crate_type = "rlib"]
-#![cfg_attr(not(feature = "cargo-build"),
-            unstable(feature = "libc"))]
-#![cfg_attr(not(feature = "cargo-build"), feature(staged_api))]
+#![cfg_attr(not(feature = "cargo-build"), unstable(feature = "libc"))]
+#![cfg_attr(not(feature = "cargo-build"), feature(staged_api, core, no_std))]
 #![cfg_attr(not(feature = "cargo-build"), staged_api)]
-#![cfg_attr(not(feature = "cargo-build"), feature(core))]
-#![feature(no_std)]
-#![no_std]
+#![cfg_attr(not(feature = "cargo-build"), no_std)]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/nightly/",
