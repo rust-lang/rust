de52403295e66ee413f1085e8b9defe7d81b9751
Ryan Prichard <ryan.prichard@gmail.com>
2015-03-12 04:09:12 -0700

    Avoid passing -L "" to rustc.
    Currently, target.mk passes -L "" when LLVM_STDCPP_LOCATION_$(2) is empty.
    
    This fixes #23287.

--- mk/target.mk
+++ mk/target.mk
@@ -85,7 +85,7 @@ $$(TLIB$(1)_T_$(2)_H_$(3))/stamp.$(4): \
 		$$(RUST_LIB_FLAGS_ST$(1)) \
 		-L "$$(RT_OUTPUT_DIR_$(2))" \
 		-L "$$(LLVM_LIBDIR_$(2))" \
-		-L "$$(dir $$(LLVM_STDCPP_LOCATION_$(2)))" \
+		$$(LLVM_STDCPP_RUSTFLAGS_$(2)) \
 		$$(RUSTFLAGS_$(4)) \
 		--out-dir $$(@D) \
 		-C extra-filename=-$$(CFG_FILENAME_EXTRA) \
