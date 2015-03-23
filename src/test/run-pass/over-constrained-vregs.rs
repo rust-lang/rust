bb9d210c99ab248e81598d70c39c3968ab9d09eb
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-03-23 07:04:15 -0700

    Fix shift-overflow in very old run-pass test.

--- src/test/run-pass/over-constrained-vregs.rs
+++ src/test/run-pass/over-constrained-vregs.rs
@@ -11,7 +11,7 @@
 // Regression test for issue #152.
 pub fn main() {
     let mut b: uint = 1_usize;
-    while b <= 32_usize {
+    while b < std::mem::size_of::<usize>() {
         0_usize << b;
         b <<= 1_usize;
         println!("{}", b);
