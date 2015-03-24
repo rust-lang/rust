bb36529f2051844c27557911245315d856fd4a05
Niko Matsakis <niko@alum.mit.edu>
2015-03-24 17:28:28 -0400

    Add test case for #22743.
    Fixes #22743.
    Fixes #19035.
    Fixes #22099.
    
    (Those all seem to be exactly the same scenario.)

---
+++ src/test/run-pass/binops-issue-22743.rs
@@ -0,0 +1,32 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::ops::Mul;
+
+#[derive(Copy)]
+pub struct Foo {
+    x: f64,
+}
+
+impl Mul<Foo> for f64 {
+    type Output = Foo;
+
+    fn mul(self, rhs: Foo) -> Foo {
+        // intentionally do something that is not *
+        Foo { x: self + rhs.x }
+    }
+}
+
+pub fn main() {
+    let f: Foo = Foo { x: 5.0 };
+    let val: f64 = 3.0;
+    let f2: Foo = val * f;
+    assert_eq!(f2.x, 8.0);
+}
