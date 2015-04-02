eaea188d9674c8b9eba36dec5f20277a6fe7ae43
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-04-02 17:29:22 +0200

    Do not suggest `#![feature(...)]` if we are in beta or stable channel.
    Fix #23973

--- src/libsyntax/feature_gate.rs
+++ src/libsyntax/feature_gate.rs
@@ -409,6 +409,12 @@ impl<'a> Context<'a> {
 
 pub fn emit_feature_err(diag: &SpanHandler, feature: &str, span: Span, explain: &str) {
     diag.span_err(span, explain);
+
+    // #23973: do not suggest `#![feature(...)]` if we are in beta/stable
+    match option_env!("CFG_RELEASE_CHANNEL") {
+        Some("stable") | Some("beta") => return,
+        _ => {}
+    }
     diag.fileline_help(span, &format!("add #![feature({})] to the \
                                    crate attributes to enable",
                                   feature));
@@ -416,6 +422,12 @@ pub fn emit_feature_err(diag: &SpanHandler, feature: &str, span: Span, explain: 
 
 pub fn emit_feature_warn(diag: &SpanHandler, feature: &str, span: Span, explain: &str) {
     diag.span_warn(span, explain);
+
+    // #23973: do not suggest `#![feature(...)]` if we are in beta/stable
+    match option_env!("CFG_RELEASE_CHANNEL") {
+        Some("stable") | Some("beta") => return,
+        _ => {}
+    }
     if diag.handler.can_emit_warnings {
         diag.fileline_help(span, &format!("add #![feature({})] to the \
                                        crate attributes to silence this warning",
