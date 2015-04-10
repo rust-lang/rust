59b8bacbf66f82a46278b5da1e909f02a86c44d3
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-04-10 19:02:01 +0200

    Test case for 64-bit corner cases where truncation occurred before prior commit.

--- src/test/run-pass/issue-15523-big.rs
+++ src/test/run-pass/issue-15523-big.rs
@@ -11,41 +11,38 @@
 // Issue 15523: derive(PartialOrd) should use the provided
 // discriminant values for the derived ordering.
 //
-// This is checking the basic functionality.
+// This test is checking corner cases that arise when you have
+// 64-bit values in the variants.
 
 #[derive(PartialEq, PartialOrd)]
-enum E1 {
+#[repr(u64)]
+enum Eu64 {
+    Pos2 = 2,
+    PosMax = !0,
+    Pos1 = 1,
+}
+
+#[derive(PartialEq, PartialOrd)]
+#[repr(i64)]
+enum Ei64 {
     Pos2 = 2,
     Neg1 = -1,
-    Pos1 = 1,
-}
-
-#[derive(PartialEq, PartialOrd)]
-#[repr(u8)]
-enum E2 {
-    Pos2 = 2,
-    PosMax = !0 as u8,
-    Pos1 = 1,
-}
-
-#[derive(PartialEq, PartialOrd)]
-#[repr(i8)]
-enum E3 {
-    Pos2 = 2,
-    Neg1 = -1_i8,
+    NegMin = 1 << 63,
+    PosMax = !(1 << 63),
     Pos1 = 1,
 }
 
 fn main() {
-    assert!(E1::Pos2 > E1::Pos1);
-    assert!(E1::Pos1 > E1::Neg1);
-    assert!(E1::Pos2 > E1::Neg1);
+    assert!(Eu64::Pos2 > Eu64::Pos1);
+    assert!(Eu64::Pos2 < Eu64::PosMax);
+    assert!(Eu64::Pos1 < Eu64::PosMax);
 
-    assert!(E2::Pos2 > E2::Pos1);
-    assert!(E2::Pos1 < E2::PosMax);
-    assert!(E2::Pos2 < E2::PosMax);
 
-    assert!(E3::Pos2 > E3::Pos1);
-    assert!(E3::Pos1 > E3::Neg1);
-    assert!(E3::Pos2 > E3::Neg1);
+    assert!(Ei64::Pos2 > Ei64::Pos1);
+    assert!(Ei64::Pos2 > Ei64::Neg1);
+    assert!(Ei64::Pos1 > Ei64::Neg1);
+    assert!(Ei64::Pos2 > Ei64::NegMin);
+    assert!(Ei64::Pos1 > Ei64::NegMin);
+    assert!(Ei64::Pos2 < Ei64::PosMax);
+    assert!(Ei64::Pos1 < Ei64::PosMax);
 }
