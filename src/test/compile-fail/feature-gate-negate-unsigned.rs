c1dc42c34cdccb19de2c20d27c20080d7eefdabc
Luke Gallagher <luke@hypergeometric.net>
2015-04-16 22:48:31 +1000

    Add feature gate tests
    As part of the audit for #22820 the following feature gate tests have
    been added:
    
    * `negate_unsigned`
    * `on_unimplemented`
    * `optin_builtin_traits`
    * `plugin`
    * `rustc_attrs`
    * `slice_patterns`

---
+++ src/test/compile-fail/feature-gate-negate-unsigned.rs
@@ -0,0 +1,17 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Test that negating unsigned integers is gated by `negate_unsigned` feature
+// gate
+
+const MAX: usize = -1;
+//~^ ERROR unary negation of unsigned integers may be removed in the future
+
+fn main() {}
