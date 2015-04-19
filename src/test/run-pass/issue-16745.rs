4cf9e2db5e49be870b0a6a1bf040995d0321b45d
Richo Healey <richo@psych0tik.net>
2015-04-19 00:25:13 -0700

    test: Add an assertion to the #16745 testcase

--- src/test/run-pass/issue-16745.rs
+++ src/test/run-pass/issue-16745.rs
@@ -10,10 +10,11 @@
 
 fn main() {
     const X: u8 = 0;
-    match 0u8 {
-        X => { },
-        b'\t' => { },
-        1u8 => { },
-        _ => { },
-    }
+    let out: u8 = match 0u8 {
+        X => 99,
+        b'\t' => 1,
+        1u8 => 2,
+        _ => 3,
+    };
+    assert_eq!(out, 99);
 }
