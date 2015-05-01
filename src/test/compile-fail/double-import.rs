db9d01842450fe07f77ca97d9a68d105366f407e
Theo Belaire <theo.belaire@gmail.com>
2015-04-30 22:46:15 -0400

    Fixed some nits

--- src/test/compile-fail/double-import.rs
+++ src/test/compile-fail/double-import.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -10,6 +10,8 @@
 #![feature(no_std)]
 #![no_std]
 
+// This tests that conflicting imports shows both `use` lines
+// when reporting the error.
 
 mod sub1 {
     fn foo() {} // implementation 1
