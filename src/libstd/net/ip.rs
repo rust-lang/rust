2d27eaa04130ce980446c4b55aca0907f27d6665
Tshepang Lekhonkhobe <tshepang@gmail.com>
2015-05-04 23:05:24 +0200

    doc: fix markup

--- src/libstd/net/ip.rs
+++ src/libstd/net/ip.rs
@@ -142,7 +142,8 @@ impl Ipv4Addr {
 
     /// Returns true if this address is in a range designated for documentation.
     ///
-    /// This is defined in RFC 5737
+    /// This is defined in RFC 5737:
+    ///
     /// - 192.0.2.0/24 (TEST-NET-1)
     /// - 198.51.100.0/24 (TEST-NET-2)
     /// - 203.0.113.0/24 (TEST-NET-3)
