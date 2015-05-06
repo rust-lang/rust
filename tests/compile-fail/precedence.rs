17bcf0e86519fa6147dec7271660e278cda98404
llogiq <bogusandre@gmail.com>
2015-05-06 12:59:08 +0200

    New lint: precedence, see issue #41

---
+++ tests/compile-fail/precedence.rs
@@ -0,0 +1,15 @@
+#![feature(plugin)]
+#![plugin(clippy)]
+
+#[deny(precedence)]
+#[allow(eq_op)]
+fn main() {
+	format!("{} vs. {}", 1 << 2 + 3, (1 << 2) + 3); //~ERROR
+    format!("{} vs. {}", 1 + 2 << 3, 1 + (2 << 3)); //~ERROR
+    format!("{} vs. {}", 4 >> 1 + 1, (4 >> 1) + 1); //~ERROR
+    format!("{} vs. {}", 1 + 3 >> 2, 1 + (3 >> 2)); //~ERROR
+    format!("{} vs. {}", 1 ^ 1 - 1, (1 ^ 1) - 1);   //~ERROR
+    format!("{} vs. {}", 3 | 2 - 1, (3 | 2) - 1);   //~ERROR
+    format!("{} vs. {}", 3 & 5 - 2, (3 & 5) - 2);   //~ERROR
+
+}
