b24433f36dbc6ac8540e5b961fafdbb4fa155298
llogiq <bogusandre@gmail.com>
2015-05-08 06:01:41 +0200

    added test for issue #31

--- tests/compile-fail/box_vec.rs
+++ tests/compile-fail/box_vec.rs
@@ -7,6 +7,11 @@ pub fn test(foo: Box<Vec<bool>>) { //~ ERROR You seem to be trying to use Box<Ve
     println!("{:?}", foo.get(0))
 }
 
+pub fn test2(foo: Box<Fn(Vec<u32>)>) { // pass if #31 is fixed
+	foo(vec![1, 2, 3])
+}
+
 fn main(){
     test(Box::new(Vec::new()));
+    test2(Box::new(|v| println!("{:?}", v)));
 }
