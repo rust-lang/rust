e8ee33d43442b6d3ac331eb05dff0960825c2e2f
Alex Crichton <alex@alexcrichton.com>
2015-05-11 12:24:56 -0700

    rt: Clean up to build with cl.exe
    * Detect the #define _MSC_VER in addition to __WIN32__
    * Don't include valgrind.h for windows
    * Remove unused `rust_valgrind_stack_{un,}register` functions
    * Add stub definition for `rust_running_on_valgrind` for windows
    * Conditionally define `rust_dbg_extern_empty_struct` as empty structures are
      not allowed by cl.exe apparently.

--- src/rt/rust_test_helpers.c
+++ src/rt/rust_test_helpers.c
@@ -135,6 +135,8 @@ struct ManyInts {
     struct TwoU8s arg6;
 };
 
+// MSVC doesn't allow empty structs or unions
+#ifndef _MSC_VER
 struct Empty {
 };
 
@@ -148,6 +150,7 @@ rust_dbg_extern_empty_struct(struct ManyInts v1, struct Empty e, struct ManyInts
     assert(v1.arg6.one == v2.arg6.one + 1);
     assert(v1.arg6.two == v2.arg6.two + 1);
 }
+#endif
 
 intptr_t
 rust_get_test_int() {
