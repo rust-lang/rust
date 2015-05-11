f2614f5858fed10e180102def32c60f180e46496
Kevin Ballard <kevin@sb.org>
2015-05-11 12:37:31 -0700

    Avoid returning a slice with a null pointer from Iter.as_slice()
    core::slice::Iter.ptr can be null when iterating a slice of zero-sized
    elements, but the pointer value used for the slice itself cannot. Handle
    this case by always returning a dummy pointer for slices of zero-sized
    elements.

--- src/test/run-pass/slice-of-zero-size-elements.rs
+++ src/test/run-pass/slice-of-zero-size-elements.rs
@@ -10,7 +10,25 @@
 
 // compile-flags: -C debug-assertions
 
+#![feature(core)]
+
 use std::slice;
+
+fn foo<T>(v: &[T]) -> Option<&[T]> {
+    let mut it = v.iter();
+    for _ in 0..5 {
+        let _ = it.next();
+    }
+    Some(it.as_slice())
+}
+
+fn foo_mut<T>(v: &mut [T]) -> Option<&mut [T]> {
+    let mut it = v.iter_mut();
+    for _ in 0..5 {
+        let _ = it.next();
+    }
+    Some(it.into_slice())
+}
 
 pub fn main() {
     // In a slice of zero-size elements the pointer is meaningless.
@@ -24,11 +42,19 @@ pub fn main() {
     assert!(it.nth(5).is_some());
     assert_eq!(it.count(), 4);
 
+    // Converting Iter to a slice should never have a null pointer
+    assert!(foo(slice).is_some());
+
+    // Test mutable iterators as well
     let slice: &mut [()] = unsafe { slice::from_raw_parts_mut(-5isize as *mut (), 10) };
     assert_eq!(slice.len(), 10);
     assert_eq!(slice.iter_mut().count(), 10);
 
-    let mut it = slice.iter_mut();
-    assert!(it.nth(5).is_some());
-    assert_eq!(it.count(), 4);
+    {
+        let mut it = slice.iter_mut();
+        assert!(it.nth(5).is_some());
+        assert_eq!(it.count(), 4);
+    }
+
+    assert!(foo_mut(slice).is_some())
 }
