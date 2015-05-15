0e21beb761e98d30a09c48e110eb0bb3d18fc5ea
Alex Crichton <alex@alexcrichton.com>
2015-05-15 16:04:01 -0700

    libs: Move favicon URLs to HTTPS
    Helps prevent mixed content warnings if accessing docs over HTTPS.
    
    Closes #25459

--- src/librustc_trans/lib.rs
+++ src/librustc_trans/lib.rs
@@ -22,7 +22,7 @@
 #![crate_type = "dylib"]
 #![crate_type = "rlib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-      html_favicon_url = "http://www.rust-lang.org/favicon.ico",
+      html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
       html_root_url = "http://doc.rust-lang.org/nightly/")]
 
 #![feature(alloc)]
