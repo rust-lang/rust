ba894959c402dfb35042e85143b8503b71549ae6
Xuefeng Wu <benewu@gmail.com>
2015-05-16 16:56:00 +0800

    fix typo for copyright year and trait capitalise

--- src/test/compile-fail/issue-24968.rs
+++ src/test/compile-fail/issue-24968.rs
@@ -1,4 +1,4 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -9,7 +9,7 @@
 // except according to those terms.
 
 fn foo(_: Self) {
-    //~^ ERROR use of `Self` outside of an impl or Trait
+    //~^ ERROR use of `Self` outside of an impl or trait
 }
 
 fn main() {}
