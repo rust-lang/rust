1f8453ab73ef40a33e04c95349c6d3c638e576ae
llogiq <bogusandre@gmail.com>
2015-05-18 10:41:15 +0200

    mut_mut now more robust (thanks to Manishearth, see issue #9)

--- tests/compile-fail/mut_mut.rs
+++ tests/compile-fail/mut_mut.rs
@@ -7,8 +7,13 @@ fn fun(x : &mut &mut u32) -> bool { //~ERROR
 }
 
 #[deny(mut_mut)]
+#[allow(unused_mut, unused_variables)]
 fn main() {
 	let mut x = &mut &mut 1u32; //~ERROR
+	{
+		let mut y = &mut x; //~ERROR
+	}
+	
 	if fun(x) {
 		let y : &mut &mut &mut u32 = &mut &mut &mut 2; 
 			 //~^ ERROR
