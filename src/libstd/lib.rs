ef35097cdc5808dee4f83a68d631d992fcd67ba9
Geoffrey Thomas <geofft@ldpreload.com>
2015-05-22 17:30:06 -0400

    sys/unix/c.rs: Remove unused code
    It looks like a lot of this dated to previous incarnations of the io
    module, etc., and went unused in the reworking leading up to 1.0. Remove
    everything we're not actively using (except for signal handling, which
    will be reworked in the next commit).

--- src/libstd/lib.rs
+++ src/libstd/lib.rs
@@ -148,8 +148,6 @@
 #![feature(vec_push_all)]
 #![feature(wrapping)]
 #![feature(zero_one)]
-#![cfg_attr(all(unix, not(target_os = "macos"), not(target_os = "ios")),
-            feature(num_bits_bytes))]
 #![cfg_attr(windows, feature(str_utf16))]
 #![cfg_attr(test, feature(float_from_str_radix, range_inclusive, float_extras))]
 #![cfg_attr(test, feature(test, rustc_private, float_consts))]
