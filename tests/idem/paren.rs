1db6fa0fe5d8ea9ddef2d5610c2485d9ec3a3cc5
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2015-05-24 22:07:18 +0200

    Add idem test for paren

---
+++ tests/idem/paren.rs
@@ -0,0 +1,16 @@
+// Test parenthesis
+
+fn foo() {
+    let very_long_variable_name = (a + first + simple + test);
+    let very_long_variable_name = (write + something + here + to + fill + the + line + 12 + 34 + 567
+                                   );
+    let very_long_variable_name = (write + something + here + to + fill + the + line + 12 + 34 + 567
+                                   + 78);
+    let very_long_variable_name = (write + something + here + to + fill + the + line + 12 + 34 + 567
+                                   + 78 + fill + another + line + AAAA + BBBBBBB + CCCCCCCCCCCCCCCCC
+                                   );
+    let very_long_variable_name = (write + something + here + to + fill + the + line + 12 + 34 + 567
+                                   + 78 + fill + another + line + AAAA + BBBBBBB + CCCCCCCCCCCCCCC +
+                                   DDDDDDD + EEEEEE);
+
+}
