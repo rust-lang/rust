e28f45833434b3a3b03ec78b2540e10aa7c102e0
Richard Diamond <wichard@vitalitystudios.com>
2015-06-09 18:52:18 -0500

    `librustc_back` changes for PNaCl.

---
+++ src/librustc_back/le32.rs
@@ -0,0 +1,26 @@
+// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use target_strs;
+use syntax::abi;
+use std::vec::Vec;
+
+pub fn get_target_strs(target_triple: String, _target_os: abi::Os) -> target_strs::t {
+    return target_strs::t {
+        module_asm: "".to_string(),
+
+        data_layout: "e-i1:8:8-i8:8:8-i16:16:16-i32:32:32-\
+                      i64:64:64-f32:32:32-f64:64:64-p:32:32:32-v128:32:32".to_string(),
+
+        target_triple: target_triple,
+
+        cc_args: Vec::new(),
+    };
+}
