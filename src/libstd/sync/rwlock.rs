e1b6a6384ed8997c614cd132c75ec8167cd36346
Alex Crichton <alex@alexcrichton.com>
2015-06-10 18:43:08 -0700

    std: Stabilize the sync_poison feature
    These accessor/constructor methods for a `PoisonError` are quite standard for a
    wrapper type and enable manipulation of the underlying type.

--- src/libstd/sync/rwlock.rs
+++ src/libstd/sync/rwlock.rs
@@ -253,7 +253,7 @@ impl<T: ?Sized> RwLock<T> {
     /// time.  You should not trust a `false` value for program correctness
     /// without additional synchronization.
     #[inline]
-    #[unstable(feature = "sync_poison")]
+    #[stable(feature = "sync_poison", since = "1.2.0")]
     pub fn is_poisoned(&self) -> bool {
         self.inner.poison.get()
     }
