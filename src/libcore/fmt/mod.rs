41dcb563549b6bea97ca75f62543090ab59fcc4c
Richo Healey <richo@psych0tik.net>
2015-06-15 20:50:14 -0700

    core: impl LowerHex and UpperHex for [u8]

--- src/libcore/fmt/mod.rs
+++ src/libcore/fmt/mod.rs
@@ -1175,6 +1175,20 @@ impl<T: Debug> Debug for [T] {
     }
 }
 
+#[stable(feature = "rust1", since = "1.2.0")]
+impl<T: LowerHex + Debug> LowerHex for [T] {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+#[stable(feature = "rust1", since = "1.2.0")]
+impl<T: UpperHex + Debug> UpperHex for [T] {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
 #[stable(feature = "rust1", since = "1.0.0")]
 impl Debug for () {
     fn fmt(&self, f: &mut Formatter) -> Result {
