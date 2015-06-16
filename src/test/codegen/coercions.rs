cabd0682e9aa406cca2541c3c5d420d100487b04
Björn Steinbrink <bsteinbr@gmail.com>
2015-06-16 10:53:06 +0200

    Avoid deref/ref cycles for no-op coercions between unsafe pointers
    Unlike coercing from reference to unsafe pointer, coercing between two
    unsafe pointers doesn't need an AutoDerefRef, because there is no region
    that regionck would need to know about.
    
    In unoptimized libcore, this reduces the number of "auto_deref" allocas
    from 174 to 4.

---
+++ src/test/codegen/coercions.rs
@@ -0,0 +1,27 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// compile-flags: -C no-prepopulate-passes
+
+static X: i32 = 5;
+
+// CHECK-LABEL: @raw_ptr_to_raw_ptr_noop
+// CHECK-NOT: alloca
+#[no_mangle]
+pub fn raw_ptr_to_raw_ptr_noop() -> *const i32{
+    &X as *const i32
+}
+
+// CHECK-LABEL: @reference_to_raw_ptr_noop
+// CHECK-NOT: alloca
+#[no_mangle]
+pub fn reference_to_raw_ptr_noop() -> *const i32 {
+    &X
+}
