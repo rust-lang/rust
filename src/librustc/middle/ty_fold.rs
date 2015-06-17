ef85338175cb322fa07846d20eec91c2800a98e6
Niko Matsakis <niko@alum.mit.edu>
2015-06-17 10:02:32 -0400

    Code up the new lifetime default rules, but leave them disabled for now.

--- src/librustc/middle/ty_fold.rs
+++ src/librustc/middle/ty_fold.rs
@@ -369,6 +369,9 @@ impl<'tcx> TypeFoldable<'tcx> for ty::ObjectLifetimeDefault {
             ty::ObjectLifetimeDefault::Ambiguous =>
                 ty::ObjectLifetimeDefault::Ambiguous,
 
+            ty::ObjectLifetimeDefault::BaseDefault =>
+                ty::ObjectLifetimeDefault::BaseDefault,
+
             ty::ObjectLifetimeDefault::Specific(r) =>
                 ty::ObjectLifetimeDefault::Specific(r.fold_with(folder)),
         }
