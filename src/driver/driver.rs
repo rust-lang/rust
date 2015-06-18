b74278c22eff1ad8b21bb87f1c32d7f4c3db67bc
Richard Diamond <wichard@vitalitystudios.com>
2015-06-18 07:10:11 -0500

    Allow the Rust tools to run on PNaCl.

--- src/driver/driver.rs
+++ src/driver/driver.rs
@@ -17,4 +17,14 @@ extern crate rustdoc as this;
 #[cfg(rustc)]
 extern crate rustc_driver as this;
 
+#[cfg_attr(target_os = "nacl", main_link_name = "nacl_main")]
 fn main() { this::main() }
+
+#[cfg(target_os = "nacl")]
+#[link(name = "ppapi_cpp", kind = "static")]
+#[link(name = "ppapi_simple_cpp", kind = "static")]
+#[link(name = "ppapi_stub", kind = "static")]
+#[link(name = "cli_main", kind = "static")]
+#[link(name = "tar", kind = "static")]
+#[link(name = "nacl_spawn", kind = "static")]
+extern { }
