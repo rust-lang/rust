00130cff99f88e13fec87378bdf476cfea6aa147
Eljay <lee@leejeffery.co.uk>
2015-06-21 16:02:05 +0100

    Fix `missing_docs` lint for const and static.

--- src/librustc_lint/builtin.rs
+++ src/librustc_lint/builtin.rs
@@ -1612,6 +1612,8 @@ impl LintPass for MissingDoc {
                 }
                 return
             },
+            ast::ItemConst(..) => "a constant",
+            ast::ItemStatic(..) => "a static",
             _ => return
         };
 
