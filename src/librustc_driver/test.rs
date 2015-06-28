5c3753f6b457dbc8e4cf0e8ec2d085d03f310911
Jared Roesch <roeschinc@gmail.com>
2015-06-28 14:08:50 -0700

    Fix librustc_driver/test.rs

--- src/librustc_driver/test.rs
+++ src/librustc_driver/test.rs
@@ -140,7 +140,7 @@ fn test_env<F>(source_string: &str,
                                lang_items,
                                stability::Index::new(krate),
                                |tcx| {
-        let infcx = infer::new_infer_ctxt(tcx);
+        let infcx = infer::new_infer_ctxt(tcx, &tcx.tables, None);
         body(Env { infcx: &infcx });
         let free_regions = FreeRegionMap::new();
         infcx.resolve_regions_and_report_errors(&free_regions, ast::CRATE_NODE_ID);
