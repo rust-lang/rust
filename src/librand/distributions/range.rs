7dc50b1a075da8cbfb707ea02f935c872c2652cc
Ulrik Sverdrup <root@localhost>
2015-07-01 12:05:57 +0200

    Hide SampleRange trait from docs

--- src/librand/distributions/range.rs
+++ src/librand/distributions/range.rs
@@ -58,6 +58,7 @@ impl<Sup: SampleRange> IndependentSample<Sup> for Range<Sup> {
 /// The helper trait for types that have a sensible way to sample
 /// uniformly between two values. This should not be used directly,
 /// and is only to facilitate `Range`.
+#[doc(hidden)]
 pub trait SampleRange {
     /// Construct the `Range` object that `sample_range`
     /// requires. This should not ever be called directly, only via
