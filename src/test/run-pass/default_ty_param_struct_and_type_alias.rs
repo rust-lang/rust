88ed5f7c36b26c646740b98a30d7d03c024deb89
Jared Roesch <roeschinc@gmail.com>
2015-07-12 21:02:16 -0700

    Clean up test cases

--- src/test/run-pass/default_ty_param_struct_and_type_alias.rs
+++ src/test/run-pass/default_ty_param_struct_and_type_alias.rs
@@ -1,7 +1,15 @@
-use std::marker::PhantomData;
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
 
-trait TypeEq<A> {}
-impl<A> TypeEq<A> for A {}
+use std::marker::PhantomData;
 
 struct DeterministicHasher;
 struct RandomHasher;
