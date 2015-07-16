d68b152c3e2feb6ee18bdf2c992098376dbb528c
Alex Crichton <alex@alexcrichton.com>
2015-07-16 11:59:53 -0700

    std: Be resilient to failure in pthread_getattr_np
    This can fail on linux for various reasons, such as the /proc filesystem not
    being mounted. There are already many cases where we can't set up stack guards,
    so just don't worry about this case and communicate that no guard was enabled.
    
    I've confirmed that this allows the compiler to run in a chroot without /proc
    mounted.
    
    Closes #22642

--- src/libstd/sys/common/thread_info.rs
+++ src/libstd/sys/common/thread_info.rs
@@ -18,7 +18,7 @@ use thread::Thread;
 use thread::LocalKeyState;
 
 struct ThreadInfo {
-    stack_guard: usize,
+    stack_guard: Option<usize>,
     thread: Thread,
 }
 
@@ -33,7 +33,7 @@ impl ThreadInfo {
         THREAD_INFO.with(move |c| {
             if c.borrow().is_none() {
                 *c.borrow_mut() = Some(ThreadInfo {
-                    stack_guard: 0,
+                    stack_guard: None,
                     thread: NewThread::new(None),
                 })
             }
@@ -47,10 +47,10 @@ pub fn current_thread() -> Option<Thread> {
 }
 
 pub fn stack_guard() -> Option<usize> {
-    ThreadInfo::with(|info| info.stack_guard)
+    ThreadInfo::with(|info| info.stack_guard).and_then(|o| o)
 }
 
-pub fn set(stack_guard: usize, thread: Thread) {
+pub fn set(stack_guard: Option<usize>, thread: Thread) {
     THREAD_INFO.with(|c| assert!(c.borrow().is_none()));
     THREAD_INFO.with(move |c| *c.borrow_mut() = Some(ThreadInfo{
         stack_guard: stack_guard,
