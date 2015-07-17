cee5f786c108f2f07b199fb1e46bf285c22e9b17
Alex Crichton <alex@alexcrichton.com>
2015-07-17 08:17:43 -0700

    Register new snapshots
    These new snapshots contain the knowledge of how to build the new triples of
    32-bit MSVC and 32-bit FreeBSD, both of which should soon start having
    nightlies/auto builders!
    
    This does not currently register bitrig/freebsd snapshots but I believe those
    will be retroactively added in the near future.

--- src/liballoc/lib.rs
+++ src/liballoc/lib.rs
@@ -135,20 +135,3 @@ pub fn oom() -> ! {
     //                allocate.
     unsafe { core::intrinsics::abort() }
 }
-
-// FIXME(#14344): When linking liballoc with libstd, this library will be linked
-//                as an rlib (it only exists as an rlib). It turns out that an
-//                optimized standard library doesn't actually use *any* symbols
-//                from this library. Everything is inlined and optimized away.
-//                This means that linkers will actually omit the object for this
-//                file, even though it may be needed in the future.
-//
-//                To get around this for now, we define a dummy symbol which
-//                will never get inlined so the stdlib can call it. The stdlib's
-//                reference to this symbol will cause this library's object file
-//                to get linked in to libstd successfully (the linker won't
-//                optimize it out).
-#[doc(hidden)]
-#[unstable(feature = "issue_14344_fixme")]
-#[cfg(stage0)]
-pub fn fixme_14344_be_sure_to_link_to_collections() {}
