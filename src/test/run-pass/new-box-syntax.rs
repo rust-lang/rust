79dab1f90a0c5c4efd535ce886cab319c637b5ce
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-07-22 15:32:17 +0200

    refine set of allowed warnings in `new-box-syntax.rs` test.

--- src/test/run-pass/new-box-syntax.rs
+++ src/test/run-pass/new-box-syntax.rs
@@ -13,7 +13,7 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-#![allow(warnings)]
+#![allow(dead_code, unused_variables)]
 #![feature(box_syntax, box_heap)]
 #![feature(placement_in_syntax)]
 
