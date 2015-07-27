0b2ca6778abdb6d43dd8351619e9fbd44c55892f
Alex Crichton <alex@alexcrichton.com>
2015-07-27 10:14:48 -0700

    test: Fix lto-syntax-extension
    The functionality this was testing was removed somewhere along the line, and
    this commit restores what it was testing.
    
    Closes #20586

--- src/test/auxiliary/lto-syntax-extension-plugin.rs
+++ src/test/auxiliary/lto-syntax-extension-plugin.rs
@@ -1,4 +1,4 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,9 +8,14 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// force-host
+
+#![feature(plugin_registrar)]
 #![feature(rustc_private)]
 
-extern crate lib;
-#[macro_use] extern crate log;
+extern crate rustc;
 
-fn main() {}
+use rustc::plugin::Registry;
+
+#[plugin_registrar]
+pub fn plugin_registrar(_reg: &mut Registry) {}
