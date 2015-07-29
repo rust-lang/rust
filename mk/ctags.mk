aba01982dea01116ef6e250b29bd06f9d8b0f9f4
Chris Morgan <me@chrismorgan.info>
2015-07-30 06:35:42 +1000

    Update the ctags rules and targets.
    As there’s no C++ runtime any more there’s really no point in having
    anything but Rust tags being made.
    
    I’ve also taken the liberty of excluding the compiler parts of this in
    the `librust%,,` pattern substitution. Whether or not this is “correct”
    will depend on whether you want tags for the compiler or for general
    use. For myself, I want it for general use.
    
    I’m not sure how much people use the tags files anyway. I definitely do,
    but with Racer existing the tags files aren’t quite so necessary.

--- mk/ctags.mk
+++ mk/ctags.mk
@@ -15,24 +15,11 @@
 
 .PHONY: TAGS.emacs TAGS.vi
 
-# This is using a blacklist approach, probably more durable than a whitelist.
-# We exclude: external dependencies (llvm, rt/{msvc,vg}),
-# tests (compiletest, test) and a couple of other things (rt/arch, etc)
-CTAGS_LOCATIONS=$(patsubst ${CFG_SRC_DIR}src/llvm,, \
-				$(patsubst ${CFG_SRC_DIR}src/compiletest,, \
-				$(patsubst ${CFG_SRC_DIR}src/test,, \
-				$(patsubst ${CFG_SRC_DIR}src/etc,, \
-				$(patsubst ${CFG_SRC_DIR}src/rt,, \
-				$(patsubst ${CFG_SRC_DIR}src/rt/arch,, \
-				$(patsubst ${CFG_SRC_DIR}src/rt/msvc,, \
-				$(patsubst ${CFG_SRC_DIR}src/rt/vg,, \
-				$(wildcard ${CFG_SRC_DIR}src/*) $(wildcard ${CFG_SRC_DIR}src/rt/*) \
-				))))))))
-CTAGS_OPTS=--options="${CFG_SRC_DIR}src/etc/ctags.rust" --languages=-javascript --recurse ${CTAGS_LOCATIONS}
-# We could use `--languages=Rust`, but there is value in producing tags for the
-# C++ parts of the code base too (at the time of writing, those are .h and .cpp
-# files in src/rt, src/rt/sync and src/rustllvm); we mainly just want to
-# exclude the external dependencies.
+CTAGS_LOCATIONS=$(wildcard ${CFG_SRC_DIR}src/lib*)
+CTAGS_LOCATIONS=$(patsubst ${CFG_SRC_DIR}src/librust%,, \
+                $(patsubst ${CFG_SRC_DIR}src/lib%test,, \
+				$(wildcard ${CFG_SRC_DIR}src/lib*))) ${CFG_SRC_DIR}src/libtest
+CTAGS_OPTS=--options="${CFG_SRC_DIR}src/etc/ctags.rust" --languages=Rust --recurse ${CTAGS_LOCATIONS}
 
 TAGS.emacs:
 	ctags -e -f $@ ${CTAGS_OPTS}
