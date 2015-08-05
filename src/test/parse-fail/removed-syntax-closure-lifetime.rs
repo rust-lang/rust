c72c0b465203f6563795192a007384ba92a0ad6c
Jared Roesch <roeschinc@gmail.com>
2015-08-05 14:18:29 -0700

    Fix expected parse error

--- src/test/parse-fail/removed-syntax-closure-lifetime.rs
+++ src/test/parse-fail/removed-syntax-closure-lifetime.rs
@@ -10,4 +10,5 @@
 
 // compile-flags: -Z parse-only
 
-type closure = Box<lt/fn()>; //~ ERROR expected one of `(`, `+`, `,`, `::`, `<`, or `>`, found `/`
+type closure = Box<lt/fn()>;
+//~^ ERROR expected one of `!`, `(`, `+`, `,`, `::`, `<`, or `>`, found `/`
