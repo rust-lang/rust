38231a68cbb5fe55496456f4b85c71d91822e4da
Niko Matsakis <niko@alum.mit.edu>
2015-08-07 10:59:28 -0400

    Fallout in tests -- break the object safety part into a separate file because error will be in future reported by wfcheck, which runs in a later stage than coherence

--- src/test/compile-fail/coherence-impl-trait-for-trait-object-safe.rs
+++ src/test/compile-fail/coherence-impl-trait-for-trait-object-safe.rs
@@ -11,17 +11,9 @@
 // Test that we give suitable error messages when the user attempts to
 // impl a trait `Trait` for its own object type.
 
-trait Foo { fn dummy(&self) { } }
-trait Bar: Foo { }
-trait Baz: Bar { }
-
-// Supertraits of Baz are not legal:
-impl Foo for Baz { }   //~ ERROR E0371
-impl Bar for Baz { }   //~ ERROR E0371
-impl Baz for Baz { }   //~ ERROR E0371
-
-// But other random traits are:
-trait Other { }
-impl Other for Baz { } // OK, Other not a supertrait of Baz
+// If the trait is not object-safe, we give a more tailored message
+// because we're such schnuckels:
+trait NotObjectSafe { fn eq(&self, other: Self); }
+impl NotObjectSafe for NotObjectSafe { } //~ ERROR E0372
 
 fn main() { }
