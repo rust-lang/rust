38e8d2bc060607ebf9a37919752e1de15f1794fa
Georg Brandl <georg@python.org>
2015-08-12 13:58:55 +0200

    methods: move misc.StrToStringPass to MethodsPass

--- src/methods.rs
+++ src/methods.rs
@@ -11,16 +11,19 @@ declare_lint!(pub OPTION_UNWRAP_USED, Warn,
               "Warn on using unwrap() on an Option value");
 declare_lint!(pub RESULT_UNWRAP_USED, Allow,
               "Warn on using unwrap() on a Result value");
+declare_lint!(pub STR_TO_STRING, Warn,
+              "Warn when a String could use to_owned() instead of to_string()");
 
 impl LintPass for MethodsPass {
     fn get_lints(&self) -> LintArray {
-        lint_array!(OPTION_UNWRAP_USED, RESULT_UNWRAP_USED)
+        lint_array!(OPTION_UNWRAP_USED, RESULT_UNWRAP_USED, STR_TO_STRING)
     }
 
     fn check_expr(&mut self, cx: &Context, expr: &Expr) {
         if let ExprMethodCall(ref ident, _, ref args) = expr.node {
+            let ref obj_ty = walk_ptrs_ty(cx.tcx.expr_ty(&*args[0])).sty;
             if ident.node.name == "unwrap" {
-                if let ty::TyEnum(did, _) = walk_ptrs_ty(cx.tcx.expr_ty(&*args[0])).sty {
+                if let ty::TyEnum(did, _) = *obj_ty {
                     if match_def_path(cx, did.did, &["core", "option", "Option"]) {
                         span_lint(cx, OPTION_UNWRAP_USED, expr.span,
                                   "used unwrap() on an Option value. If you don't want \
@@ -32,6 +35,11 @@ impl LintPass for MethodsPass {
                                   "used unwrap() on a Result value. Graceful handling \
                                    of Err values is preferred.");
                     }
+                }
+            }
+            else if ident.node.name == "to_string" {
+                if let ty::TyStr = *obj_ty {
+                    span_lint(cx, STR_TO_STRING, expr.span, "`str.to_owned()` is faster");
                 }
             }
         }
