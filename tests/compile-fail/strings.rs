f67175b4cd4c10366950fc0af7b46c85967e51d7
Andre Bogus <bogusandre@gmail.com>
2015-08-12 21:39:42 +0200

    fixed error messages in compile-fail test

--- tests/compile-fail/strings.rs
+++ tests/compile-fail/strings.rs
@@ -7,11 +7,11 @@ fn add_only() { // ignores assignment distinction
 	let mut x = "".to_owned();
 
     for _ in (1..3) {
-        x = x + "."; //~ERROR you add something to a string.
+        x = x + "."; //~ERROR you added something to a string.
     }
     
     let y = "".to_owned();
-    let z = y + "..."; //~ERROR you add something to a string.
+    let z = y + "..."; //~ERROR you added something to a string.
     
     assert_eq!(&x, &z);
 }
@@ -21,7 +21,7 @@ fn add_assign_only() {
 	let mut x = "".to_owned();
 
     for _ in (1..3) {
-        x = x + "."; //~ERROR you assign the result of adding something to this string.
+        x = x + "."; //~ERROR you assigned the result of adding something to this string.
     }
     
     let y = "".to_owned();
@@ -35,11 +35,11 @@ fn both() {
     let mut x = "".to_owned();
 
     for _ in (1..3) {
-        x = x + "."; //~ERROR you assign the result of adding something to this string.
+        x = x + "."; //~ERROR you assigned the result of adding something to this string.
     }
     
     let y = "".to_owned();
-    let z = y + "..."; //~ERROR you add something to a string.
+    let z = y + "..."; //~ERROR you added something to a string.
     
     assert_eq!(&x, &z);
 }
