f1da4efbafb6f89646d31c483007bafc81aab62c
Vadim Chugunov <vadimcn@gmail.com>
2015-08-11 23:19:38 -0700

    Fix #26803

--- src/etc/make-win-dist.py
+++ src/etc/make-win-dist.py
@@ -45,7 +45,7 @@ def make_win_dist(rust_root, gcc_root, target_triple):
         elif key == "libraries":
             lib_path.extend(val.lstrip(' =').split(';'))
 
-    target_tools = ["gcc.exe", "ld.exe", "ar.exe", "dlltool.exe", "windres.exe"]
+    target_tools = ["gcc.exe", "ld.exe", "ar.exe", "dlltool.exe"]
 
     rustc_dlls = ["libstdc++-6.dll"]
     if target_triple.startswith("i686-"):
