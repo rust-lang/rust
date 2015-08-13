f4b5d215332da92256bcc4d0c4b9f8b7ef6d6f72
llogiq <bogusandre@gmail.com>
2015-08-13 15:46:00 +0200

    added a few unit tests to trim_multiline

---
+++ tests/trim_multiline.rs
@@ -0,0 +1,38 @@
+/// test the multiline-trim function
+#[allow(plugin_as_library)]
+extern crate clippy;
+
+use clippy::utils::trim_multiline;
+
+#[test]
+fn test_single_line() {
+    assert_eq!("", trim_multiline("".into(), false));
+    assert_eq!("...", trim_multiline("...".into(), false));
+    assert_eq!("...", trim_multiline("    ...".into(), false));
+    assert_eq!("...", trim_multiline("\t...".into(), false));
+    assert_eq!("...", trim_multiline("\t\t...".into(), false));
+}
+
+#[test]
+fn test_block() {
+    assert_eq!("\
+if x {
+    y
+} else {
+    z
+}", trim_multiline("    if x {
+        y
+    } else {
+        z
+    }".into(), false));
+    assert_eq!("\
+if x {
+\ty
+} else {
+\tz
+}", trim_multiline("    if x {
+    \ty
+    } else {
+    \tz
+    }".into(), false));
+}
