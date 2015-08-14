5d99ebec72cdedc0e76103b9a93bd9199f9be0ef
llogiq <bogusandre@gmail.com>
2015-08-14 14:21:05 +0200

    fixed false positives (at the cost of some false negatives)

--- tests/compile-fail/eta.rs
+++ tests/compile-fail/eta.rs
@@ -4,18 +4,31 @@
 #![deny(redundant_closure)]
 
 fn main() {
-    let a = |a, b| foo(a, b);
+    let a = Some(1u8).map(|a| foo(a));
     //~^ ERROR redundant closure found. Consider using `foo` in its place
-    let c = |a, b| {1+2; foo}(a, b);
+    meta(|a| foo(a));
+    //~^ ERROR redundant closure found. Consider using `foo` in its place
+    let c = Some(1u8).map(|a| {1+2; foo}(a));
     //~^ ERROR redundant closure found. Consider using `{ 1 + 2; foo }` in its place
-    let d = |a, b| foo((|c, d| foo2(c,d))(a,b), b);
-    //~^ ERROR redundant closure found. Consider using `foo2` in its place
+    let d = Some(1u8).map(|a| foo((|b| foo2(b))(a))); //is adjusted?
+    all(&[1, 2, 3], &&2, |x, y| below(x, y)); //is adjusted
 }
 
-fn foo(_: u8, _: u8) {
+fn meta<F>(f: F) where F: Fn(u8) {
+    f(1u8)
+}
+
+fn foo(_: u8) {
 
 }
 
-fn foo2(_: u8, _: u8) -> u8 {
+fn foo2(_: u8) -> u8 {
     1u8
 }
+
+fn all<X, F>(x: &[X], y: &X, f: F) -> bool
+where F: Fn(&X, &X) -> bool {
+    x.iter().all(|e| f(e, y))
+}
+
+fn below(x: &u8, y: &u8) -> bool { x < y }
