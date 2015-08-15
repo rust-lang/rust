44f41063dd241320cbe19b078862fc29eb66c097
Ariel Ben-Yehuda <ariel.byd@gmail.com>
2015-08-16 01:20:36 +0300

    use the correct substs when checking struct patterns
    also, make sure this is tested.

--- src/test/run-pass/struct-aliases.rs
+++ src/test/run-pass/struct-aliases.rs
@@ -8,6 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::mem;
 
 struct S {
     x: isize,
@@ -15,6 +16,13 @@ struct S {
 }
 
 type S2 = S;
+
+struct S3<U,V> {
+    x: U,
+    y: V
+}
+
+type S4<U> = S3<U, char>;
 
 fn main() {
     let s = S2 {
@@ -30,4 +38,34 @@ fn main() {
             assert_eq!(y, 2);
         }
     }
+    // check that generics can be specified from the pattern
+    let s = S4 {
+        x: 4,
+        y: 'a'
+    };
+    match s {
+        S4::<u8> {
+            x: x,
+            y: y
+        } => {
+            assert_eq!(x, 4);
+            assert_eq!(y, 'a');
+            assert_eq!(mem::size_of_val(&x), 1);
+        }
+    };
+    // check that generics can be specified from the constructor
+    let s = S4::<u16> {
+        x: 5,
+        y: 'b'
+    };
+    match s {
+        S4 {
+            x: x,
+            y: y
+        } => {
+            assert_eq!(x, 5);
+            assert_eq!(y, 'b');
+            assert_eq!(mem::size_of_val(&x), 2);
+        }
+    };
 }
