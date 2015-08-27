595fda0de7fda252559b717e2e56b3b1eef1961e
Alex Burka <aburka@seas.upenn.edu>
2015-08-27 02:20:00 -0400

    fix some more unstable issue annotations

--- src/libcore/macros.rs
+++ src/libcore/macros.rs
@@ -239,7 +239,8 @@ macro_rules! writeln {
 /// ```
 #[macro_export]
 #[unstable(feature = "core",
-           reason = "relationship with panic is unclear")]
+           reason = "relationship with panic is unclear",
+           issue = "27701")]
 macro_rules! unreachable {
     () => ({
         panic!("internal error: entered unreachable code")
@@ -256,7 +257,8 @@ macro_rules! unreachable {
 /// message `"not yet implemented"` when executed.
 #[macro_export]
 #[unstable(feature = "core",
-           reason = "relationship with panic is unclear")]
+           reason = "relationship with panic is unclear",
+           issue = "27701")]
 macro_rules! unimplemented {
     () => (panic!("not yet implemented"))
 }
