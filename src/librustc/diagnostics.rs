3db19f1facf4afea03a23c41891724fed7d39a96
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-09-04 18:09:16 +0200

    Add span_help for E0002

--- src/librustc/diagnostics.rs
+++ src/librustc/diagnostics.rs
@@ -44,7 +44,7 @@ E0002: r##"
 This error indicates that an empty match expression is invalid because the type
 it is matching on is non-empty (there exist values of this type). In safe code
 it is impossible to create an instance of an empty type, so empty match
-expressions are almost never desired.  This error is typically fixed by adding
+expressions are almost never desired. This error is typically fixed by adding
 one or more cases to the match expression.
 
 An example of an empty type is `enum Empty { }`. So, the following will work:
