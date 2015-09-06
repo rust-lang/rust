a8a8dfbe1a0aa917fb392bb8f3c037ef6a1ef27f
christopherdumas <christopherdumas@me.com>
2015-09-05 20:23:26 -0700

    Line longer that 80 chars.

--- src/librustc_typeck/diagnostics.rs
+++ src/librustc_typeck/diagnostics.rs
@@ -1535,8 +1535,8 @@ For information on the design of the orphan rules, see [RFC 1023].
 
 E0118: r##"
 Rust can't find a base type for an implementation you are providing, or the type
-cannot have an implementation. For example, a typedef can't have an implementation,
-since it isn't its own type (this was done in PR #6087):
+cannot have an implementation. For example, a typedef can't have an
+implementation, since it isn't its own type (this was done in PR #6087):
 
 ```
 type NineString = [char, ..9]
