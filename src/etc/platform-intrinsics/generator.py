9aa1289a67b6682568b94c6de74d411cdf87d9ee
Andrea Canciani <ranma42@gmail.com>
2015-09-12 17:05:29 +0200

    Add a comment to explain the `#[inline(never)]` annotation
    and regenerate the platform intrinsics source files.

--- src/etc/platform-intrinsics/generator.py
+++ src/etc/platform-intrinsics/generator.py
@@ -737,6 +737,8 @@ use {{Intrinsic, i, i_, u, u_, f, v, v_, agg, p, void}};
 use IntrinsicDef::Named;
 use rustc::middle::ty;
 
+// The default inlining settings trigger a pathological behaviour in
+// LLVM, which causes makes compilation very slow. See #28273.
 #[inline(never)]
 pub fn find<'tcx>(_tcx: &ty::ctxt<'tcx>, name: &str) -> Option<Intrinsic> {{
     if !name.starts_with("{0}") {{ return None }}
