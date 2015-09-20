40a6f11f764096b9c2164532776b90507a25a1d7
Lee Jeffery <lee@leejeffery.co.uk>
2015-09-20 11:35:08 +0100

    Miscellaneous cleanup for old issues.

--- src/libcoretest/cell.rs
+++ src/libcoretest/cell.rs
@@ -248,15 +248,14 @@ fn unsafe_cell_unsized() {
     assert_eq!(unsafe { &mut *cell.get() }, comp);
 }
 
-// FIXME(#25351) needs deeply nested coercions of DST structs.
-// #[test]
-// fn refcell_unsized() {
-//     let cell: &RefCell<[i32]> = &RefCell::new([1, 2, 3]);
-//     {
-//         let b = &mut *cell.borrow_mut();
-//         b[0] = 4;
-//         b[2] = 5;
-//     }
-//     let comp: &mut [i32] = &mut [4, 2, 5];
-//     assert_eq!(&*cell.borrow(), comp);
-// }
+#[test]
+fn refcell_unsized() {
+    let cell: &RefCell<[i32]> = &RefCell::new([1, 2, 3]);
+    {
+        let b = &mut *cell.borrow_mut();
+        b[0] = 4;
+        b[2] = 5;
+    }
+    let comp: &mut [i32] = &mut [4, 2, 5];
+    assert_eq!(&*cell.borrow(), comp);
+}
