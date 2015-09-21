75648cd0c8eaf4d5c2f2012076f26911da536472
Sebastian Wicki <gandro@gmx.net>
2015-09-21 19:16:24 +0200

    Add support for the rumprun unikernel
    For most parts, rumprun currently looks like NetBSD, as they share the same
    libc and drivers. However, being a unikernel, rumprun does not support
    process management, signals or virtual memory, so related functions
    might fail at runtime. Stack guards are disabled exactly for this reason.
    
    Code for rumprun is always cross-compiled, it uses always static
    linking and needs a custom linker.

---
+++ src/librustc_back/target/x86_64_rumprun_netbsd.rs
@@ -0,0 +1,35 @@
+// Copyright 2014-2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use target::Target;
+
+pub fn target() -> Target {
+    let mut base = super::netbsd_base::opts();
+    base.pre_link_args.push("-m64".to_string());
+    base.linker = "x86_64-rumprun-netbsd-gcc".to_string();
+    base.ar = "x86_64-rumprun-netbsd-ar".to_string();
+
+    base.dynamic_linking = false;
+    base.has_rpath = false;
+    base.position_independent_executables = false;
+    base.disable_redzone = true;
+    base.no_default_libraries = false;
+
+    Target {
+        llvm_target: "x86_64-rumprun-netbsd".to_string(),
+        target_endian: "little".to_string(),
+        target_pointer_width: "64".to_string(),
+        arch: "x86_64".to_string(),
+        target_os: "netbsd".to_string(),
+        target_env: "".to_string(),
+        target_vendor: "rumprun".to_string(),
+        options: base,
+    }
+}
