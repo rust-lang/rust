223a7600376c9c6df41078c1fc647cee0a0ef34e
Manish Goregaokar <manishsmail@gmail.com>
2015-09-21 14:21:30 +0530

    Make function pointers implement traits for up to 12 parameters
    (12 was chosen to be consistent with what we do for tuples)
    
    Fixes #28559

--- src/libcore/ptr.rs
+++ src/libcore/ptr.rs
@@ -385,6 +385,13 @@ fnptr_impls_args! { A, B }
 fnptr_impls_args! { A, B, C }
 fnptr_impls_args! { A, B, C, D }
 fnptr_impls_args! { A, B, C, D, E }
+fnptr_impls_args! { A, B, C, D, E, F }
+fnptr_impls_args! { A, B, C, D, E, F, G }
+fnptr_impls_args! { A, B, C, D, E, F, G, H }
+fnptr_impls_args! { A, B, C, D, E, F, G, H, I }
+fnptr_impls_args! { A, B, C, D, E, F, G, H, I, J }
+fnptr_impls_args! { A, B, C, D, E, F, G, H, I, J, K }
+fnptr_impls_args! { A, B, C, D, E, F, G, H, I, J, K, L }
 
 // Comparison for pointers
 #[stable(feature = "rust1", since = "1.0.0")]
