97f2a325644eb88b50fb5b52e09933535fa29c40
Andrew Paseltiner <apaseltiner@gmail.com>
2015-09-23 10:33:36 -0400

    Optimize `Vec::clone_from`
    Before:
    
    test dst_bigger::src_100_dst_1000::clone      ... bench:  34 ns/iter (+/- 1)
    test dst_bigger::src_100_dst_1000::clone_from ... bench:  75 ns/iter (+/- 3)
    
    test dst_bigger::src_10_dst_100::clone        ... bench:  25 ns/iter (+/- 0)
    test dst_bigger::src_10_dst_100::clone_from   ... bench:   9 ns/iter (+/- 1)
    
    test eq::src_1000_dst_1000::clone             ... bench: 105 ns/iter (+/- 2)
    test eq::src_1000_dst_1000::clone_from        ... bench: 593 ns/iter (+/- 21)
    
    test eq::src_100_dst_100::clone               ... bench:  34 ns/iter (+/- 1)
    test eq::src_100_dst_100::clone_from          ... bench:  75 ns/iter (+/- 1)
    
    test src_bigger::src_1000_dst_100::clone      ... bench: 103 ns/iter (+/- 5)
    test src_bigger::src_1000_dst_100::clone_from ... bench: 148 ns/iter (+/- 5)
    
    test src_bigger::src_100_dst_10::clone        ... bench:  34 ns/iter (+/- 1)
    test src_bigger::src_100_dst_10::clone_from   ... bench:  20 ns/iter (+/- 0)
    
    After:
    
    test dst_bigger::src_100_dst_1000::clone      ... bench:  34 ns/iter (+/- 2)
    test dst_bigger::src_100_dst_1000::clone_from ... bench:  15 ns/iter (+/- 1)
    
    test dst_bigger::src_10_dst_100::clone        ... bench:  26 ns/iter (+/- 1)
    test dst_bigger::src_10_dst_100::clone_from   ... bench:   7 ns/iter (+/- 0)
    
    test eq::src_1000_dst_1000::clone             ... bench: 103 ns/iter (+/- 1)
    test eq::src_1000_dst_1000::clone_from        ... bench:  85 ns/iter (+/- 4)
    
    test eq::src_100_dst_100::clone               ... bench:  34 ns/iter (+/- 2)
    test eq::src_100_dst_100::clone_from          ... bench:  15 ns/iter (+/- 1)
    
    test src_bigger::src_1000_dst_100::clone      ... bench: 103 ns/iter (+/- 4)
    test src_bigger::src_1000_dst_100::clone_from ... bench:  90 ns/iter (+/- 2)
    
    test src_bigger::src_100_dst_10::clone        ... bench:  34 ns/iter (+/- 2)
    test src_bigger::src_100_dst_10::clone_from   ... bench:  20 ns/iter (+/- 0)
    
    Closes #28601.

--- src/libcollections/lib.rs
+++ src/libcollections/lib.rs
@@ -62,7 +62,7 @@
 #![feature(unsafe_no_drop_flag, filling_drop)]
 #![feature(decode_utf16)]
 #![feature(utf8_error)]
-#![cfg_attr(test, feature(rand, test))]
+#![cfg_attr(test, feature(clone_from_slice, rand, test))]
 
 #![feature(no_std)]
 #![no_std]
