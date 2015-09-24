938be69c4cdae8d53fa546933e6dc1406b9eafb4
Alex Crichton <alex@alexcrichton.com>
2015-09-24 08:38:48 -0700

    std: Update MatchIndices to return a subslice
    This commit updates the `MatchIndices` and `RMatchIndices` iterators to follow
    the same pattern as the `chars` and `char_indices` iterators. The `matches`
    iterator currently yield `&str` elements, so the `MatchIndices` iterator now
    yields the index of the match as well as the `&str` that matched (instead of
    start/end indexes).
    
    cc #27743

--- src/libcollectionstest/str.rs
+++ src/libcollectionstest/str.rs
@@ -1047,7 +1047,7 @@ fn test_pattern_deref_forward() {
 fn test_empty_match_indices() {
     let data = "aä中!";
     let vec: Vec<_> = data.match_indices("").collect();
-    assert_eq!(vec, [(0, 0), (1, 1), (3, 3), (6, 6), (7, 7)]);
+    assert_eq!(vec, [(0, ""), (1, ""), (3, ""), (6, ""), (7, "")]);
 }
 
 #[test]
@@ -1477,7 +1477,7 @@ generate_iterator_test! {
 
 generate_iterator_test! {
     double_ended_match_indices {
-        ("a1b2c3", char::is_numeric) -> [(1, 2), (3, 4), (5, 6)];
+        ("a1b2c3", char::is_numeric) -> [(1, "1"), (3, "2"), (5, "3")];
     }
     with str::match_indices, str::rmatch_indices;
 }
