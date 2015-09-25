c6e1b12a58b33ecfe7793d39b28827e05d3106f5
Ben S <ogham@users.noreply.github.com>
2015-09-25 12:29:47 +0100

    Change the first line of the println macro doc
    This makes the first lines of the print! and println! macros
    different. Previously, they would show up exactly the same in the
    documentation for the macros in libstd [1], with nothing about how
    one of them also prints a newline.
    
    [1]: https://doc.rust-lang.org/stable/std/#macros

--- src/libstd/macros.rs
+++ src/libstd/macros.rs
@@ -98,7 +98,7 @@ macro_rules! print {
     ($($arg:tt)*) => ($crate::io::_print(format_args!($($arg)*)));
 }
 
-/// Macro for printing to the standard output.
+/// Macro for printing to the standard output, with a newline.
 ///
 /// Use the `format!` syntax to write data to the standard output.
 /// See `std::fmt` for more information.
