c5ab8d62e304e546ae685880272edd0ab949e69d
Pyriphlegethon <pyriphlegethon.github@gmail.com>
2015-09-30 18:00:14 +0200

    Fix tests

--- tests/compile-fail/mut_reference.rs
+++ tests/compile-fail/mut_reference.rs
@@ -1,8 +1,11 @@
 #![feature(plugin)]
 #![plugin(clippy)]
 
+#![allow(unused_variable)]
+
 fn takes_an_immutable_reference(a: &i32) {
 }
+
 
 fn takes_a_mutable_reference(a: &mut i32) {
 }
@@ -32,7 +35,7 @@ fn main() {
     // Functions
     takes_an_immutable_reference(&42);
     takes_a_mutable_reference(&mut 42);
-    let mut a = &mut 42;
+    let a = &mut 42;
     takes_an_immutable_reference(a);
     
     // Methods
