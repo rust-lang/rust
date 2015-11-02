79033f1a8dc673efd2cd4486b453ba00632270ca
Vadim Chugunov <vadimcn@gmail.com>
2015-11-02 11:42:20 -0800

    Merged windows and unix `find_libdir()`

--- src/librustc/metadata/filesearch.rs
+++ src/librustc/metadata/filesearch.rs
@@ -258,7 +258,6 @@ pub fn rust_path() -> Vec<PathBuf> {
 }
 
 // The name of the directory rustc expects libraries to be located.
-#[cfg(unix)]
 fn find_libdir(sysroot: &Path) -> String {
     // FIXME: This is a quick hack to make the rustc binary able to locate
     // Rust libraries in Linux environments where libraries might be installed
@@ -290,11 +289,6 @@ fn find_libdir(sysroot: &Path) -> String {
     fn secondary_libdir_name() -> String {
         "lib".to_string()
     }
-}
-
-#[cfg(windows)]
-fn find_libdir(_sysroot: &Path) -> String {
-    "lib".to_string()
 }
 
 // The name of rustc's own place to organize libraries.
