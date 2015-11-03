e0af0caaca2b49ba57ae931686234b4df25289db
Amanieu d'Antras <amanieu@gmail.com>
2015-11-03 18:13:03 +0000

    Add test for inline asm indirect memory operands

---
+++ src/test/run-pass/asm-indirect-memory.rs
@@ -0,0 +1,39 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(asm)]
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+fn read(ptr: &u32) -> u32 {
+    let out: u32;
+    unsafe {
+        asm!("mov $1, $0" : "=r" (out) : "*m" (ptr));
+    }
+    out
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+fn write(ptr: &mut u32, val: u32) {
+    unsafe {
+        asm!("mov $1, $0" :: "=*m" (ptr), "r" (val));
+    }
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub fn main() {
+    let a = 1;
+    let mut b = 2;
+    assert_eq!(read(&a), 1);
+    write(&mut b, 3);
+    assert_eq!(b, 3);
+}
+
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+pub fn main() {}
