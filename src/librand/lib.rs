86c55e7d99507192390c7e42c735046499b83663
Kevin Butler <haqkrs@gmail.com>
2015-11-03 16:04:40 +0000

    librand: deny warnings in doctests

--- src/librand/lib.rs
+++ src/librand/lib.rs
@@ -23,7 +23,8 @@
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
        html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
        html_root_url = "https://doc.rust-lang.org/nightly/",
-       html_playground_url = "https://play.rust-lang.org/")]
+       html_playground_url = "https://play.rust-lang.org/",
+       test(attr(deny(warnings))))]
 #![no_std]
 #![staged_api]
 #![unstable(feature = "rand",
