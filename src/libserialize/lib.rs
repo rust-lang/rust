943cb64ea2db301c6b3b2a6e6dbc271b9b13256f
Kevin Butler <haqkrs@gmail.com>
2015-11-03 16:17:57 +0000

    libserialize: deny warnings in doctests

--- src/libserialize/lib.rs
+++ src/libserialize/lib.rs
@@ -26,7 +26,8 @@ Core encoding and decoding interfaces.
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
        html_root_url = "https://doc.rust-lang.org/nightly/",
-       html_playground_url = "https://play.rust-lang.org/")]
+       html_playground_url = "https://play.rust-lang.org/",
+       test(attr(allow(unused_variables), deny(warnings))))]
 
 #![feature(box_syntax)]
 #![feature(collections)]
