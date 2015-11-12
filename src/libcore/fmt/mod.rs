83c3125383090d6d7630694422e1f1937d71ce8f
Erik Davidson <erik@erikd.org>
2015-11-12 15:03:36 -0600

    Fix the manual implementation example for the Debug trait so it matches the given output

--- src/libcore/fmt/mod.rs
+++ src/libcore/fmt/mod.rs
@@ -328,7 +328,7 @@ impl<'a> Display for Arguments<'a> {
 ///
 /// impl fmt::Debug for Point {
 ///     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-///         write!(f, "({}, {})", self.x, self.y)
+///         write!(f, "Point {{ x: {}, y: {} }}", self.x, self.y)
 ///     }
 /// }
 ///
