8042fdd17c2556614db5496681cbc5c24b3757f7
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2015-11-29 15:40:29 +0300

    Merge `OwnedSlice` into `ptr::P`

--- src/libsyntax/ptr.rs
+++ src/libsyntax/ptr.rs
@@ -36,17 +36,39 @@
 //!   implementation changes (using a special thread-local heap, for example).
 //!   Moreover, a switch to, e.g. `P<'a, T>` would be easy and mostly automated.
 
-use std::fmt::{self, Display, Debug};
-use std::hash::{Hash, Hasher};
+use std::fmt;
+use std::iter::FromIterator;
 use std::ops::Deref;
 use std::ptr;
-
+use std::slice;
+use std::vec;
 use serialize::{Encodable, Decodable, Encoder, Decoder};
 
 /// An owned smart pointer.
-pub struct P<T> {
+#[derive(PartialEq, Eq, Hash)]
+pub struct P<T: ?Sized> {
     ptr: Box<T>
 }
+
+// ----------------------------------------------------------------------------
+// Common impls
+
+impl<T: ?Sized> Deref for P<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.ptr
+    }
+}
+
+impl<T: ?Sized + fmt::Debug> fmt::Debug for P<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&self.ptr, fmt)
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Impls for one boxed element `P<T>`
 
 #[allow(non_snake_case)]
 /// Construct a `P<T>` from a `T` value.
@@ -78,49 +100,21 @@ impl<T: 'static> P<T> {
     }
 }
 
-impl<T> Deref for P<T> {
-    type Target = T;
-
-    fn deref<'a>(&'a self) -> &'a T {
-        &*self.ptr
-    }
-}
-
 impl<T: 'static + Clone> Clone for P<T> {
     fn clone(&self) -> P<T> {
         P((**self).clone())
     }
 }
 
-impl<T: PartialEq> PartialEq for P<T> {
-    fn eq(&self, other: &P<T>) -> bool {
-        **self == **other
-    }
-}
-
-impl<T: Eq> Eq for P<T> {}
-
-impl<T: Debug> Debug for P<T> {
+impl<T: fmt::Display> fmt::Display for P<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(&**self, f)
-    }
-}
-impl<T: Display> Display for P<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&**self, f)
+        fmt::Display::fmt(&**self, f)
     }
 }
 
-#[stable(feature = "rust1", since = "1.0.0")]
 impl<T> fmt::Pointer for P<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Pointer::fmt(&self.ptr, f)
-    }
-}
-
-impl<T: Hash> Hash for P<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        (**self).hash(state);
     }
 }
 
@@ -133,5 +127,111 @@ impl<T: 'static + Decodable> Decodable for P<T> {
 impl<T: Encodable> Encodable for P<T> {
     fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
         (**self).encode(s)
+    }
+}
+
+// ----------------------------------------------------------------------------
+// Impls for boxed array `P<[T]>` (ex-`OwnedSlice`)
+
+impl<T> P<[T]> {
+    pub fn new() -> Self {
+        P { ptr: Box::new([]) as Box<[T]> }
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `P::new` instead")]
+    pub fn empty() -> P<[T]> {
+        P { ptr: Box::new([]) as Box<[T]> }
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `P::from` instead")]
+    pub fn from_vec(v: Vec<T>) -> P<[T]> {
+        P { ptr: v.into_boxed_slice() }
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `P::into` instead")]
+    pub fn into_vec(self) -> Vec<T> {
+        self.ptr.into_vec()
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `&owned_slice[..]` instead")]
+    pub fn as_slice<'a>(&'a self) -> &'a [T] {
+        &*self.ptr
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `P::into_iter` instead")]
+    pub fn move_iter(self) -> vec::IntoIter<T> {
+        self.ptr.into_vec().into_iter()
+    }
+
+    #[unstable(feature = "rustc_private", issue = "0")]
+    #[rustc_deprecated(since = "1.6.0", reason = "use `iter().map(f).collect()` instead")]
+    pub fn map<U, F: FnMut(&T) -> U>(&self, f: F) -> P<[U]> {
+        self.iter().map(f).collect()
+    }
+}
+
+impl<T: Clone> Clone for P<[T]> {
+    fn clone(&self) -> Self {
+        P { ptr: self.ptr.clone() }
+    }
+}
+
+impl<T> From<Vec<T>> for P<[T]> {
+    fn from(v: Vec<T>) -> Self {
+        P { ptr: v.into_boxed_slice() }
+    }
+}
+
+impl<T> Into<Vec<T>> for P<[T]> {
+    fn into(self) -> Vec<T> {
+        self.ptr.into_vec()
+    }
+}
+
+impl<T> FromIterator<T> for P<[T]> {
+    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+        P::from(iter.into_iter().collect::<Vec<_>>())
+    }
+}
+
+impl<T> IntoIterator for P<[T]> {
+    type Item = T;
+    type IntoIter = vec::IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.ptr.into_vec().into_iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a P<[T]> {
+    type Item = &'a T;
+    type IntoIter = slice::Iter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.ptr.iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut P<[T]> {
+    type Item = &'a mut T;
+    type IntoIter = slice::IterMut<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.ptr.iter_mut()
+    }
+}
+
+impl<T: Encodable> Encodable for P<[T]> {
+    fn encode<E: Encoder>(&self, s: &mut E) -> Result<(), E::Error> {
+        Encodable::encode(&self.ptr, s)
+    }
+}
+
+impl<T: Decodable> Decodable for P<[T]> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Self, D::Error> {
+        Ok(P { ptr: try!(Decodable::decode(d)) })
     }
 }
