34d0e8e68ff29d1509b1bc35f4d38477706298a4
Sean Griffin <sean@seantheprogrammer.com>
2015-12-02 13:55:35 -0700

    Ensure two `SystemTime`s are equal when nanos add to exactly 1B
    Currently if you add a duration which should lead to 0 nanos and 1
    additional second, we end up with no additional seconds, and 1000000000
    nanos.

--- src/libstd/time/mod.rs
+++ src/libstd/time/mod.rs
@@ -303,6 +303,10 @@ mod tests {
         let eighty_years = second * 60 * 60 * 24 * 365 * 80;
         assert_almost_eq!(a - eighty_years + eighty_years, a);
         assert_almost_eq!(a - (eighty_years * 10) + (eighty_years * 10), a);
+
+        let one_second_from_epoch = UNIX_EPOCH + Duration::new(1, 0);
+        let one_second_from_epoch2 = UNIX_EPOCH + Duration::new(0, 999_999_999) + Duration::new(0, 1);
+        assert_eq!(one_second_from_epoch, one_second_from_epoch2);
     }
 
     #[test]
