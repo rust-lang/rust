92fba6bd2c7c5b1a08ff1399ae8357e51399a1c0
Devon Hollowood <devonhollowood@gmail.com>
2015-12-12 21:35:35 -0800

    Make clippy tests compatible with new lint

--- tests/compile-fail/for_loop.rs
+++ tests/compile-fail/for_loop.rs
@@ -16,7 +16,8 @@ impl Unrelated {
 
 #[deny(needless_range_loop, explicit_iter_loop, iter_next_loop, reverse_range_loop, explicit_counter_loop)]
 #[deny(unused_collect)]
-#[allow(linkedlist,shadow_unrelated,unnecessary_mut_passed, cyclomatic_complexity)]
+#[allow(linkedlist,shadow_unrelated,unnecessary_mut_passed, cyclomatic_complexity,
+        used_underscore_binding)]
 fn main() {
     let mut vec = vec![1, 2, 3, 4];
     let vec2 = vec![1, 2, 3, 4];
