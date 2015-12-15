088d92a9e60d1d2240cf481d947d83d41543cbb6
Seo Sanghyeon <sanxiyn@gmail.com>
2015-12-15 16:23:18 +0900

    Mark libsyntax_ext unstable

--- src/libsyntax_ext/lib.rs
+++ src/libsyntax_ext/lib.rs
@@ -17,7 +17,12 @@
        html_favicon_url = "https://doc.rust-lang.org/favicon.ico",
        html_root_url = "https://doc.rust-lang.org/nightly/")]
 
+#![cfg_attr(stage0, feature(custom_attribute))]
+#![cfg_attr(stage0, staged_api)]
+#![unstable(feature = "rustc_private", issue = "27812")]
+
 #![feature(rustc_private)]
+#![feature(staged_api)]
 #![feature(str_char)]
 
 extern crate fmt_macros;
