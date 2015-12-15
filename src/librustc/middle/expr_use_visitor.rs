56681918bf991c31ae75a3010284d9cc160a425b
Niko Matsakis <niko@alum.mit.edu>
2015-12-15 11:15:59 -0500

    Workaround stage0 bug

--- src/librustc/middle/expr_use_visitor.rs
+++ src/librustc/middle/expr_use_visitor.rs
@@ -276,7 +276,7 @@ enum PassArgs {
 }
 
 impl<'d,'t,'a,'tcx> ExprUseVisitor<'d,'t,'a,'tcx> {
-    pub fn new(delegate: &'d mut (Delegate<'tcx>),
+    pub fn new(delegate: &'d mut Delegate<'tcx>,
                typer: &'t infer::InferCtxt<'a, 'tcx>)
                -> ExprUseVisitor<'d,'t,'a,'tcx> where 'tcx:'a+'d
     {
