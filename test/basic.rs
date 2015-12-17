2e38c5ba29954ca6a10a1f235c30a6b3771c70ff
Scott Olson <scott@solson.me>
2015-12-17 14:03:26 -0600

    Add (commented) test for basic use of std::option::Option.

--- test/basic.rs
+++ test/basic.rs
@@ -113,13 +113,22 @@ enum MyOption<T> {
 }
 
 #[miri_run(expected = "Int(13)")]
-fn match_opt_some() -> i32 {
+fn match_my_opt_some() -> i32 {
     let x = MyOption::Some { data: 13 };
     match x {
         MyOption::Some { data } => data,
         MyOption::None => 42,
     }
 }
+
+// #[miri_run(expected = "Int(13)")]
+// fn match_opt_some() -> i32 {
+//     let x = Some(13);
+//     match x {
+//         Some(data)  => data,
+//         None => 42,
+//     }
+// }
 
 // #[miri_run(expected = "Int(42)")]
 // fn match_opt_none() -> i32 {
