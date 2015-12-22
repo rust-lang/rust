3c07d66a5b2b90fa5273fdd6f3fe5d839e3c0fcd
Ticki <Ticki@users.noreply.github.com>
2015-12-22 09:10:55 +0100

    Move the is_empty method to Path instead
    Tests

--- src/libstd/path.rs
+++ src/libstd/path.rs
@@ -1147,15 +1147,6 @@ impl PathBuf {
         self.inner
     }
 
-    /// Checks if the path buffer is empty. On Windows, it will return false if the inner string is
-    /// invalid unicode. On Unix, this is a no-op.
-    pub fn is_empty(&self) -> bool {
-        if let Some(b) = self.inner.to_bytes() {
-            b.is_empty()
-        } else {
-            true
-        }
-    }
 }
 
 #[stable(feature = "rust1", since = "1.0.0")]
@@ -1868,6 +1859,27 @@ impl Path {
     #[stable(feature = "path_ext", since = "1.5.0")]
     pub fn is_dir(&self) -> bool {
         fs::metadata(self).map(|m| m.is_dir()).unwrap_or(false)
+    }
+
+    /// Checks if the path buffer is empty. On Windows, it will return false if the inner string is
+    /// invalid unicode. On Unix, this is a no-op.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::path::Path;
+    ///
+    /// let path = Path::new("/tmp/foo.rs");
+    ///
+    /// assert!(!path.is_empty());
+    /// ```
+    #[unstable(feature = "path_extras", reason = "recently added", issue = "30259")]
+    pub fn is_empty(&self) -> bool {
+        if let Some(b) = self.inner.to_bytes() {
+            b.is_empty()
+        } else {
+            true
+        }
     }
 }
 
@@ -3196,6 +3208,17 @@ mod tests {
             tfn!(r"foo\..", "bar", r"foo\..\bar");
             tfn!(r"\", "foo", r"\foo");
         }
+    }
+
+    #[test]
+    pub fn is_empty() {
+        let path = Path::new("/tmp/foo.rs");
+        let mut path_buf = PathBuf::new();
+
+        assert!(!path.is_empty());
+        assert!(path_buf.is_empty());
+        path_buf.push("catsarecute");
+        assert!(!path_buf.is_empty());
     }
 
     #[test]
