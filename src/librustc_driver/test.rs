2c3f0123e9e3da95cf32ba789b11c522ae3c9afc
Niko Matsakis <niko@alum.mit.edu>
2015-12-22 19:46:51 -0500

    only insert global predicates into the global cache once we've completely proven them to be true

--- src/librustc_driver/test.rs
+++ src/librustc_driver/test.rs
@@ -139,7 +139,7 @@ fn test_env<F>(source_string: &str,
                                lang_items,
                                stability::Index::new(krate),
                                |tcx| {
-                                   let infcx = infer::new_infer_ctxt(tcx, &tcx.tables, None, false);
+                                   let infcx = infer::new_infer_ctxt(tcx, &tcx.tables, None);
                                    body(Env { infcx: &infcx });
                                    let free_regions = FreeRegionMap::new();
                                    infcx.resolve_regions_and_report_errors(&free_regions,
