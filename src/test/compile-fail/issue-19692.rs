182ace679e67e0a7b6d0105339720cb5d18f422b
Niko Matsakis <niko@alum.mit.edu>
2016-01-07 13:51:16 -0500

    Change error scheme so that if projection fails we generate `A::B` instead of `TyError`

--- src/test/compile-fail/issue-19692.rs
+++ src/test/compile-fail/issue-19692.rs
@@ -12,7 +12,7 @@ struct Homura;
 
 fn akemi(homura: Homura) {
     let Some(ref madoka) = Some(homura.kaname()); //~ ERROR no method named `kaname` found
-    madoka.clone(); //~ ERROR the type of this value must be known in this context
+    madoka.clone();
 }
 
 fn main() { }
