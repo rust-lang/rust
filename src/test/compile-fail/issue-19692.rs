17a9657d87e2afbf51e2f25fd6784cdf996c50fe
Niko Matsakis <niko@alum.mit.edu>
2016-01-08 20:16:30 -0500

    permit coercions if `[error]` is found in either type

--- src/test/compile-fail/issue-19692.rs
+++ src/test/compile-fail/issue-19692.rs
@@ -12,7 +12,7 @@ struct Homura;
 
 fn akemi(homura: Homura) {
     let Some(ref madoka) = Some(homura.kaname()); //~ ERROR no method named `kaname` found
-    madoka.clone();
+    madoka.clone(); //~ ERROR the type of this value must be known
 }
 
 fn main() { }
