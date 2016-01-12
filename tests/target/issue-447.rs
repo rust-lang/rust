4da91e7df24e833a5800631319f300a304d2f981
DarkDrek <wackerei@gmail.com>
2016-01-12 01:09:08 +0100

    Handle more possible comment position for if else Extended the test with the new possiblecomment positions

--- tests/target/issue-447.rs
+++ tests/target/issue-447.rs
@@ -1,7 +1,39 @@
 fn main() {
-    if cond {
+    if
+    // shouldn't be dropped
+    // shouldn't be dropped
+    cond
+    // shouldn't be dropped
+    // shouldn't be dropped
+    {
     }
-    // This shouldn't be dropped
-    else {
+    // shouldn't be dropped
+    // shouldn't be dropped
+    else
+    // shouldn't be dropped
+    // shouldn't be dropped
+    if
+    // shouldn't be dropped
+    // shouldn't be dropped
+    cond
+    // shouldn't be dropped
+    // shouldn't be dropped
+    {
+    }
+    // shouldn't be dropped
+    // shouldn't be dropped
+    else
+    // shouldn't be dropped
+    // shouldn't be dropped
+    {
+    }
+
+    if
+    // shouldn't be dropped
+    // shouldn't be dropped
+    let Some(x) = y
+    // shouldn't be dropped
+    // shouldn't be dropped
+    {
     }
 }
