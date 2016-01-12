1d39aed701a44113cbde2729b64562332f15d0b2
Michael Sproul <micsproul@gmail.com>
2016-01-13 10:38:25 +1100

    Add an impl for Box<Error> from &str.

--- src/test/run-pass/string-box-error.rs
+++ src/test/run-pass/string-box-error.rs
@@ -15,4 +15,6 @@ use std::error::Error;
 fn main() {
     let _err1: Box<Error + Send + Sync> = From::from("test".to_string());
     let _err2: Box<Error> = From::from("test".to_string());
+    let _err3: Box<Error + Send + Sync + 'static> = From::from("test");
+    let _err4: Box<Error> = From::from("test");
 }
