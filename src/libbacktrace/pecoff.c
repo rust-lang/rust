55e2b7e1b4606ae0bc684293f011b7006d1f1258
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2016-01-15 19:24:50 +0300

    libbacktrace: Reapply Rust-specific changes
    New:
    Fix for https://github.com/rust-lang/rust/issues/28447
    
    Merged:
    openbsd support: https://github.com/rust-lang/rust/commit/fcb30a0b67b1bd4acbc3422ff74fac5d031ae1ae
    bitrig integration: https://github.com/rust-lang/rust/commit/cd8f31759fd04b9ce9c903fa00cae81dc5bba546
    
    Not merged:
    https://github.com/rust-lang/rust/commit/d4fc3ec208186f0a9de8d05a6802c95b07e058fd
    @Diggsey says this change was unintended (https://github.com/rust-lang/rust/pull/30666#issuecomment-168616892)

--- src/libbacktrace/pecoff.c
+++ src/libbacktrace/pecoff.c
@@ -602,7 +602,7 @@ coff_add (struct backtrace_state *state, int descriptor,
   const b_coff_section_header *sects;
   struct backtrace_view str_view;
   int str_view_valid;
-  size_t str_size;
+  uint32_t str_size;
   off_t str_off;
   struct backtrace_view syms_view;
   off_t syms_off;
