8c7a3ac0dd27b385816d45181be65be04819c736
Jonathan S <gereeter+code@gmail.com>
2016-01-17 11:26:11 -0600

    Expand the macro in variance-btree-invariant-types.rs to make compiletest recognize that it should error

--- src/test/compile-fail/variance-btree-invariant-types.rs
+++ src/test/compile-fail/variance-btree-invariant-types.rs
@@ -12,45 +12,52 @@
 
 use std::collections::btree_map::{IterMut, OccupiedEntry, VacantEntry};
 
-macro_rules! test_invariant {
-    { $m:ident $t:ident } => {
-        mod $m {
-            use std::collections::btree_map::{IterMut, OccupiedEntry, VacantEntry};
-
-            fn not_covariant_key<'a, 'min,'max>(v: $t<'a, &'max (), ()>)
-                                                -> $t<'a, &'min (), ()>
-                where 'max : 'min
-            {
-                v //~ ERROR mismatched types
-            }
-
-            fn not_contravariant_key<'a, 'min,'max>(v: $t<'a, &'min (), ()>)
-                                                    -> $t<'a, &'max (), ()>
-                where 'max : 'min
-            {
-                v //~ ERROR mismatched types
-            }
-
-            fn not_covariant_val<'a, 'min,'max>(v: $t<'a, (), &'max ()>)
-                                                -> $t<'a, (), &'min ()>
-                where 'max : 'min
-            {
-                v //~ ERROR mismatched types
-            }
-
-            fn not_contravariant_val<'a, 'min,'max>(v: $t<'a, (), &'min ()>)
-                                                    -> $t<'a, (), &'max ()>
-                where 'max : 'min
-            {
-                v //~ ERROR mismatched types
-            }
-        }
-    }
+fn iter_cov_key<'a, 'new>(v: IterMut<'a, &'static (), ()>) -> IterMut<'a, &'new (), ()> {
+    v //~ ERROR mismatched types
+}
+fn iter_cov_val<'a, 'new>(v: IterMut<'a, (), &'static ()>) -> IterMut<'a, (), &'new ()> {
+    v //~ ERROR mismatched types
+}
+fn iter_contra_key<'a, 'new>(v: IterMut<'a, &'new (), ()>) -> IterMut<'a, &'static (), ()> {
+    v //~ ERROR mismatched types
+}
+fn iter_contra_val<'a, 'new>(v: IterMut<'a, (), &'new ()>) -> IterMut<'a, (), &'static ()> {
+    v //~ ERROR mismatched types
 }
 
-test_invariant! { foo IterMut }
-test_invariant! { bar OccupiedEntry }
-test_invariant! { baz VacantEntry }
+fn occ_cov_key<'a, 'new>(v: OccupiedEntry<'a, &'static (), ()>)
+                         -> OccupiedEntry<'a, &'new (), ()> {
+    v //~ ERROR mismatched types
+}
+fn occ_cov_val<'a, 'new>(v: OccupiedEntry<'a, (), &'static ()>)
+                         -> OccupiedEntry<'a, (), &'new ()> {
+    v //~ ERROR mismatched types
+}
+fn occ_contra_key<'a, 'new>(v: OccupiedEntry<'a, &'new (), ()>)
+                            -> OccupiedEntry<'a, &'static (), ()> {
+    v //~ ERROR mismatched types
+}
+fn occ_contra_val<'a, 'new>(v: OccupiedEntry<'a, (), &'new ()>)
+                            -> OccupiedEntry<'a, (), &'static ()> {
+    v //~ ERROR mismatched types
+}
+
+fn vac_cov_key<'a, 'new>(v: VacantEntry<'a, &'static (), ()>)
+                         -> VacantEntry<'a, &'new (), ()> {
+    v //~ ERROR mismatched types
+}
+fn vac_cov_val<'a, 'new>(v: VacantEntry<'a, (), &'static ()>)
+                         -> VacantEntry<'a, (), &'new ()> {
+    v //~ ERROR mismatched types
+}
+fn vac_contra_key<'a, 'new>(v: VacantEntry<'a, &'new (), ()>)
+                            -> VacantEntry<'a, &'static (), ()> {
+    v //~ ERROR mismatched types
+}
+fn vac_contra_val<'a, 'new>(v: VacantEntry<'a, (), &'new ()>)
+                            -> VacantEntry<'a, (), &'static ()> {
+    v //~ ERROR mismatched types
+}
 
 #[rustc_error]
 fn main() { }
