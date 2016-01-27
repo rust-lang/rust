a1ac3125de6ae7cb0ffffd845df28b3ba3872a19
llogiq <bogusandre@gmail.com>
2016-01-27 20:13:15 +0100

    fixed and extended tests

--- tests/compile-fail/methods.rs
+++ tests/compile-fail/methods.rs
@@ -308,11 +308,18 @@ fn starts_with() {
 fn use_extend_from_slice() {
     let mut v : Vec<&'static str> = vec![];
     v.extend(&["Hello", "World"]); //~ERROR use of `extend`
-    v.extend(&vec!["Some", "more"]); //~ERROR use of `extend`
+    v.extend(&vec!["Some", "more"]);
+    //~^ERROR use of `extend`
+    //~| HELP try this
+    //~| SUGGESTION v.extend_from_slice(&vec!["Some", "more"]);
+    
     v.extend(vec!["And", "even", "more"].iter()); //~ERROR use of `extend`
     let o : Option<&'static str> = None;
     v.extend(o);
     v.extend(Some("Bye"));
     v.extend(vec!["Not", "like", "this"]);
-    v.extend(["Nor", "this"].iter());
+    v.extend(["But", "this"].iter()); 
+    //~^ERROR use of `extend
+    //~| HELP try this
+    //~| SUGGESTION v.extend_from_slice(&["But", "this"]);
 }
