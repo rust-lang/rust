54767f8471bb94b156d00d5061ab77269ed57672
Alex Crichton <alex@alexcrichton.com>
2016-01-29 12:36:45 -0800

    mk: Fix cross-compiling to armv7-unknown-linux-gnu
    The cross prefix was not likely the actual compiler that needed to be used, but
    rather the standard `arm-linux-gnueabihf-gcc` compiler can just be used with
    `-march=armv7`.

--- mk/cfg/armv7-unknown-linux-gnueabihf.mk
+++ mk/cfg/armv7-unknown-linux-gnueabihf.mk
@@ -1,5 +1,5 @@
 # armv7-unknown-linux-gnueabihf configuration
-CROSS_PREFIX_armv7-unknown-linux-gnueabihf=armv7-unknown-linux-gnueabihf-
+CROSS_PREFIX_armv7-unknown-linux-gnueabihf=arm-linux-gnueabihf-
 CC_armv7-unknown-linux-gnueabihf=gcc
 CXX_armv7-unknown-linux-gnueabihf=g++
 CPP_armv7-unknown-linux-gnueabihf=gcc -E
@@ -8,8 +8,8 @@ CFG_LIB_NAME_armv7-unknown-linux-gnueabihf=lib$(1).so
 CFG_STATIC_LIB_NAME_armv7-unknown-linux-gnueabihf=lib$(1).a
 CFG_LIB_GLOB_armv7-unknown-linux-gnueabihf=lib$(1)-*.so
 CFG_LIB_DSYM_GLOB_armv7-unknown-linux-gnueabihf=lib$(1)-*.dylib.dSYM
-CFG_JEMALLOC_CFLAGS_armv7-unknown-linux-gnueabihf := -D__arm__ $(CFLAGS)
-CFG_GCCISH_CFLAGS_armv7-unknown-linux-gnueabihf := -Wall -g -fPIC -D__arm__ $(CFLAGS)
+CFG_JEMALLOC_CFLAGS_armv7-unknown-linux-gnueabihf := -D__arm__ $(CFLAGS) -march=armv7
+CFG_GCCISH_CFLAGS_armv7-unknown-linux-gnueabihf := -Wall -g -fPIC -D__arm__ $(CFLAGS) -march=armv7
 CFG_GCCISH_CXXFLAGS_armv7-unknown-linux-gnueabihf := -fno-rtti $(CXXFLAGS)
 CFG_GCCISH_LINK_FLAGS_armv7-unknown-linux-gnueabihf := -shared -fPIC -g
 CFG_GCCISH_DEF_FLAG_armv7-unknown-linux-gnueabihf := -Wl,--export-dynamic,--dynamic-list=
