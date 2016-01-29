a17fb64fcecdc42c5e25dd1745e517db881f14e1
Björn Steinbrink <bsteinbr@gmail.com>
2016-01-29 11:11:24 +0100

    Workaround LLVM optimizer bug by not marking &mut pointers as noalias
    LLVM's memory dependence analysis doesn't properly account for calls
    that could unwind and thus effectively act as a branching point. This
    can lead to stores that are only visible when the call unwinds being
    removed, possibly leading to calls to drop() functions with b0rked
    memory contents.
    
    As there is no fix for this in LLVM yet and we want to keep
    compatibility to current LLVM versions anyways, we have to workaround
    this bug by omitting the noalias attribute on &mut function arguments.
    Benchmarks suggest that the performance loss by this change is very
    small.
    
    Thanks to @RalfJung for pushing me towards not removing too many
    noalias annotations and @alexcrichton for helping out with the test for
    this bug.
    
    Fixes #29485

--- src/librustc_trans/trans/attributes.rs
+++ src/librustc_trans/trans/attributes.rs
@@ -265,7 +265,7 @@ pub fn from_fn_type<'a, 'tcx>(ccx: &CrateContext<'a, 'tcx>, fn_type: ty::Ty<'tcx
                 // on memory dependencies rather than pointer equality
                 let interior_unsafe = mt.ty.type_contents(ccx.tcx()).interior_unsafe();
 
-                if mt.mutbl == hir::MutMutable || !interior_unsafe {
+                if mt.mutbl != hir::MutMutable && !interior_unsafe {
                     attrs.arg(idx, llvm::Attribute::NoAlias);
                 }
 
