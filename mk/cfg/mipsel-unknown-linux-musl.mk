74086cf05be43ff4d87a6cd6e75709ad76e64a1e
Jorge Aparicio <japaricious@gmail.com>
2016-01-30 03:24:24 -0500

    revert changes used for local testing

--- mk/cfg/mipsel-unknown-linux-musl.mk
+++ mk/cfg/mipsel-unknown-linux-musl.mk
@@ -1,8 +1,8 @@
 # mipsel-unknown-linux-musl configuration
-CC_mipsel-unknown-linux-musl=mipsel-openwrt-linux-gcc
-CXX_mipsel-unknown-linux-musl=mipsel-openwrt-linux-g++
-CPP_mipsel-unknown-linux-musl=mipsel-openwrt-linux-gcc
-AR_mipsel-unknown-linux-musl=mipsel-openwrt-linux-ar
+CC_mipsel-unknown-linux-musl=mipsel-linux-musl-gcc
+CXX_mipsel-unknown-linux-musl=mipsel-linux-musl-g++
+CPP_mipsel-unknown-linux-musl=mipsel-linux-musl-gcc
+AR_mipsel-unknown-linux-musl=mipsel-linux-musl-ar
 CFG_LIB_NAME_mipsel-unknown-linux-musl=lib$(1).so
 CFG_STATIC_LIB_NAME_mipsel-unknown-linux-musl=lib$(1).a
 CFG_LIB_GLOB_mipsel-unknown-linux-musl=lib$(1)-*.so
