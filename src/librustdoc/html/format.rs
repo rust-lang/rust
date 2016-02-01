bfac659f90d70cecddc285fa1bc86c3b92c50d7c
Brandon W Maister <quodlibetor@gmail.com>
2016-02-01 13:05:37 -0500

    Decide to hide constness only in fn/method renders
    ConstnessSpace has no knowledge of the type of item it's modifying, so
    hide the constness a level up.

--- src/librustdoc/html/format.rs
+++ src/librustdoc/html/format.rs
@@ -657,9 +657,6 @@ impl fmt::Display for UnsafetySpace {
 
 impl fmt::Display for ConstnessSpace {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if option_env!("CFG_DISABLE_UNSTABLE_FEATURES").is_some() {
-            return Ok(());
-        }
         match self.get() {
             hir::Constness::Const => write!(f, "const "),
             hir::Constness::NotConst => Ok(())
