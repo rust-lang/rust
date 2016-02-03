e9dfc94d263eeb7c4ce25a50619cc2e0cdc32b66
Guillaume Bonnet <guillaumebonnet10@gmail.com>
2016-02-03 16:08:18 +0100

    compiler-rt: Handle -Werror=* arguments in CFLAGS

--- mk/rt.mk
+++ mk/rt.mk
@@ -245,7 +245,7 @@ COMPRT_AR_$(1) := $$(AR_$(1))
 # We chomp -Werror here because GCC warns about the type signature of
 # builtins not matching its own and the build fails. It's a bit hacky,
 # but what can we do, we're building libclang-rt using GCC ......
-COMPRT_CFLAGS_$(1) := $$(subst -Werror,,$$(CFG_GCCISH_CFLAGS_$(1))) -std=c99
+COMPRT_CFLAGS_$(1) := $$(filter-out -Werror -Werror=*,$$(CFG_GCCISH_CFLAGS_$(1))) -std=c99
 
 # FreeBSD Clang's packaging is problematic; it doesn't copy unwind.h to
 # the standard include directory. This should really be in our changes to
