45e716e51cd8b7f982645bfefb129e7618e241b3
Alex Burka <aburka@seas.upenn.edu>
2016-02-03 19:58:07 -0500

    test #[derive(FromPrimitive)] triggers custom-derive error

--- src/test/compile-fail/deriving-primitive.rs
+++ src/test/compile-fail/deriving-primitive.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,27 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use std::num::FromPrimitive;
-use std::isize;
+#[derive(FromPrimitive)] //~ERROR `#[derive]` for custom traits is not stable
+enum Foo {}
 
-#[derive(FromPrimitive)]
-struct A { x: isize }
-//~^^ ERROR `FromPrimitive` cannot be derived for structs
-//~^^^ ERROR `FromPrimitive` cannot be derived for structs
+fn main() {}
 
-#[derive(FromPrimitive)]
-struct B(isize);
-//~^^ ERROR `FromPrimitive` cannot be derived for structs
-//~^^^ ERROR `FromPrimitive` cannot be derived for structs
-
-#[derive(FromPrimitive)]
-enum C { Foo(isize), Bar(usize) }
-//~^^ ERROR `FromPrimitive` cannot be derived for enums with non-unit variants
-//~^^^ ERROR `FromPrimitive` cannot be derived for enums with non-unit variants
-
-#[derive(FromPrimitive)]
-enum D { Baz { x: isize } }
-//~^^ ERROR `FromPrimitive` cannot be derived for enums with non-unit variants
-//~^^^ ERROR `FromPrimitive` cannot be derived for enums with non-unit variants
-
-pub fn main() {}
