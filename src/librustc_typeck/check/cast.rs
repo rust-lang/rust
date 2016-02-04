9024d5be3e178f1738e03e0fabca5d22f31fd68c
Jexell <Jexell@users.noreply.github.com>
2016-02-04 23:41:38 +0000

    Removed an unnecessary space.

--- src/librustc_typeck/check/cast.rs
+++ src/librustc_typeck/check/cast.rs
@@ -346,7 +346,7 @@ impl<'tcx> CastCheck<'tcx> {
     {
         // array-ptr-cast.
 
-	if  !(m_expr.mutbl == hir::MutImmutable && m_cast.mutbl == hir::MutMutable) {
+	if !(m_expr.mutbl == hir::MutImmutable && m_cast.mutbl == hir::MutMutable) {
             if let ty::TyArray(ety, _) = m_expr.ty.sty {
                 // Due to the limitations of LLVM global constants,
                 // region pointers end up pointing at copies of
