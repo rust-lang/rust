eb41fe6140091b9faf1bbb67f9f4d2882eea8da4
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-02-05 09:35:00 +0100

    refactor `MirPass` to always require a tcx

--- src/librustc_mir/mir_map.rs
+++ src/librustc_mir/mir_map.rs
@@ -22,12 +22,13 @@ extern crate rustc_front;
 use build;
 use graphviz;
 use pretty;
-use transform::{simplify_cfg, MirPass};
+use transform::simplify_cfg;
 use rustc::dep_graph::DepNode;
 use rustc::mir::repr::Mir;
 use hair::cx::Cx;
 use std::fs::File;
 
+use rustc::mir::transform::MirPass;
 use rustc::mir::mir_map::MirMap;
 use rustc::middle::infer;
 use rustc::middle::region::CodeExtentData;
@@ -147,7 +148,7 @@ impl<'a, 'm, 'tcx> Visitor<'tcx> for InnerDump<'a,'m,'tcx> {
 
         match build_mir(Cx::new(&infcx), implicit_arg_tys, id, span, decl, body) {
             Ok(mut mir) => {
-                simplify_cfg::SimplifyCfg::new().run_on_mir(&mut mir);
+                simplify_cfg::SimplifyCfg::new().run_on_mir(&mut mir, self.tcx);
 
                 let meta_item_list = self.attr
                                          .iter()
