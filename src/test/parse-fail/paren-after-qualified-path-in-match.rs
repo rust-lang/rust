847a0d21507faeca6413782c12e9071ca44c97e8
Nick Cameron <ncameron@mozilla.com>
2016-02-10 16:11:27 +1300

    Some error recovery in the parser

--- src/test/parse-fail/paren-after-qualified-path-in-match.rs
+++ src/test/parse-fail/paren-after-qualified-path-in-match.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// compile-flags: -Z parse-only
+
 fn foo() {
     match x {
         <T as Trait>::Type(2) => (),
