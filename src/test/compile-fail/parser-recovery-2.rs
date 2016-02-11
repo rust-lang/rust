4a225a707d5ecc150259941fb84ce9b4528fe22b
Nick Cameron <ncameron@mozilla.com>
2016-02-11 14:50:19 +1300

    Tests

---
+++ src/test/compile-fail/parser-recovery-2.rs
@@ -0,0 +1,22 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Test that we can recover from mismatched braces in the parser.
+
+trait Foo {
+    fn bar() {
+        let x = foo(); //~ ERROR unresolved name `foo`
+    ) //~ ERROR incorrect close delimiter: `)`
+}
+
+fn main() {
+    let x = y.;  //~ ERROR unexpected token
+                 //~^ ERROR unresolved name `y`
+}
