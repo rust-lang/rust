5172745da761842a40a625c804f5e0360586996d
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-02-12 06:42:44 +0000

    Warn instead of error when using an inaccessable extern crate

--- src/test/compile-fail/extern-crate-visibility.rs
+++ src/test/compile-fail/extern-crate-visibility.rs
@@ -8,16 +8,27 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![feature(rustc_attrs)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 mod foo {
     extern crate core;
-    pub use self::core as reexported_core; // Check that private extern crates can be reexported
 }
 
-// Check that private crates cannot be used from outside their modules
-use foo::core; //~ ERROR module `core` is inaccessible
-use foo::core::cell; //~ ERROR
+// Check that private crates can be used from outside their modules, albeit with warnings
+use foo::core; //~ WARN extern crate `core` is private
+//~^ WARN this was previously accepted by the compiler but is being phased out
+use foo::core::cell; //~ WARN extern crate `core` is private
+//~^ WARN this was previously accepted by the compiler but is being phased out
 
-fn main() {
+fn f() {
+    foo::core::cell::Cell::new(0); //~ WARN extern crate `core` is private
+    //~^ WARN this was previously accepted by the compiler but is being phased out
+
     use foo::*;
     mod core {} // Check that private crates are not glob imported
 }
+
+#[rustc_error]
+fn main() {} //~ ERROR compilation successful
