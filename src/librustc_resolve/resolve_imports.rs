b8b74fe36a58682ffda2ba22e517d749b55f0bbf
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-02-14 08:46:54 +0000

    Rename Module field children to resolutions

--- src/librustc_resolve/resolve_imports.rs
+++ src/librustc_resolve/resolve_imports.rs
@@ -406,7 +406,7 @@ impl<'a, 'b:'a, 'tcx:'b> ImportResolver<'a, 'b, 'tcx> {
         match (&value_result, &type_result) {
             (&Indeterminate, _) | (_, &Indeterminate) => return Indeterminate,
             (&Failed(_), &Failed(_)) => {
-                let children = target_module.children.borrow();
+                let children = target_module.resolutions.borrow();
                 let names = children.keys().map(|&(ref name, _)| name);
                 let lev_suggestion = match find_best_match_for_name(names, &source.as_str(), None) {
                     Some(name) => format!(". Did you mean to use `{}`?", name),
