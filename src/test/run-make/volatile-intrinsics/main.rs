f5cddd1baf34daeb1efeee3cfdbeaaa42d194d02
Amanieu d'Antras <amanieu@gmail.com>
2016-02-19 17:40:36 +0000

    Add tests for read_volatile and write_volatile

--- src/test/run-make/volatile-intrinsics/main.rs
+++ src/test/run-make/volatile-intrinsics/main.rs
@@ -8,14 +8,20 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(core_intrinsics)]
+#![feature(core_intrinsics, volatile)]
 
 use std::intrinsics::{volatile_load, volatile_store};
+use std::ptr::{read_volatile, write_volatile};
 
 pub fn main() {
     unsafe {
         let mut i : isize = 1;
         volatile_store(&mut i, 2);
         assert_eq!(volatile_load(&i), 2);
+    }
+    unsafe {
+        let mut i : isize = 1;
+        write_volatile(&mut i, 2);
+        assert_eq!(read_volatile(&i), 2);
     }
 }
