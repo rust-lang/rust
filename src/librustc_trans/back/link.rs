a6a9194e09df2da40369487bf8494064c6a7795c
Ralph Giles <giles@mozilla.com>
2016-02-19 15:09:31 -0800

    Use an early return.
    If there's nothing to do, returning early saves an indentation
    level and reduces the amount of state one needs to track reading
    the code.

--- src/librustc_trans/back/link.rs
+++ src/librustc_trans/back/link.rs
@@ -826,14 +826,16 @@ fn link_staticlib(sess: &Session, objects: &[PathBuf], out_filename: &Path,
 }
 
 fn report_link_line(sess: &Session, native_libs: Vec<(NativeLibraryKind, String)>) {
-    if !native_libs.is_empty() {
-        sess.note_without_error(
-            "link against the following native artifacts when linking against \
-                                 this static library");
-        sess.note_without_error(
-            "the order and any duplication can be significant on some \
-                                 platforms, and so may need to be preserved");
+    if native_libs.is_empty() {
+        return;
     }
+
+    sess.note_without_error(
+        "link against the following native artifacts when linking against \
+                             this static library");
+    sess.note_without_error(
+        "the order and any duplication can be significant on some \
+                             platforms, and so may need to be preserved");
 
     for &(kind, ref lib) in &native_libs {
         let name = match kind {
