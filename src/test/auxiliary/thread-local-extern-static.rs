6ebe03bf50d0233f8f6bfe716b78bed0065c720c
Michael Neumann <mneumann@ntecs.de>
2016-02-21 13:10:51 +0100

    Run thread-local-extern-static test only on supported platforms.

--- src/test/auxiliary/thread-local-extern-static.rs
+++ src/test/auxiliary/thread-local-extern-static.rs
@@ -9,8 +9,9 @@
 // except according to those terms.
 
 #![feature(thread_local)]
+#![feature(cfg_target_thread_local)]
 #![crate_type = "lib"]
 
 #[no_mangle]
-#[thread_local]
+#[cfg_attr(target_thread_local, thread_local)]
 pub static FOO: u32 = 3;
