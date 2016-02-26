3c9a26853cca8783892d5b2373e9de63b5f488b5
Manish Goregaokar <manishsmail@gmail.com>
2016-02-26 17:05:46 +0530

    fixup #31878

--- src/libstd/path.rs
+++ src/libstd/path.rs
@@ -226,7 +226,7 @@ mod platform {
                     }
                     _ => (),
                 }
-            } else if path.get(1) == Some(b':') {
+            } else if path.get(1) == Some(& b':') {
                 // C:
                 let c = path[0];
                 if c.is_ascii() && (c as char).is_alphabetic() {
