5865b1d5e1bca0de7d9b5e66f457bb73e14e67a6
Niko Matsakis <niko@alum.mit.edu>
2016-03-01 22:05:54 -0500

    convert coherence-cow to use revisions

--- src/test/compile-fail/coherence-cow.rs
+++ src/test/compile-fail/coherence-cow.rs
@@ -8,20 +8,30 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// revisions: a b c
+
 // aux-build:coherence_lib.rs
+
+// pretty-expanded FIXME #23616
 
 // Test that the `Pair` type reports an error if it contains type
 // parameters, even when they are covered by local types. This test
 // was originally intended to test the opposite, but the rules changed
 // with RFC 1023 and this became illegal.
 
-// pretty-expanded FIXME #23616
-
 extern crate coherence_lib as lib;
 use lib::{Remote,Pair};
 
 pub struct Cover<T>(T);
 
-impl<T> Remote for Pair<Cover<T>,T> { } //~ ERROR E0210
+#[cfg(a)]
+impl<T> Remote for Pair<T,Cover<T>> { } //[a]~ ERROR E0210
+
+#[cfg(b)]
+impl<T> Remote for Pair<Cover<T>,T> { } //[b]~ ERROR E0210
+
+#[cfg(c)]
+impl<T,U> Remote for Pair<Cover<T>,U> { }
+//[c]~^ ERROR type parameter `T` must be used as the type parameter for some local type
 
 fn main() { }
