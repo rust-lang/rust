ee4b607dfbc5f857eb1c500f2f2ff84062feb926
Alex Burka <aburka@seas.upenn.edu>
2016-03-03 14:55:24 -0500

    derive: assume enum repr defaults to isize
    It was originally intended to be i32, but it isn't.
    
    Fixes #31886.

--- src/libsyntax_ext/deriving/generic/mod.rs
+++ src/libsyntax_ext/deriving/generic/mod.rs
@@ -758,7 +758,7 @@ impl<'a> TraitDef<'a> {
 
 fn find_repr_type_name(diagnostic: &Handler,
                        type_attrs: &[ast::Attribute]) -> &'static str {
-    let mut repr_type_name = "i32";
+    let mut repr_type_name = "isize";
     for a in type_attrs {
         for r in &attr::find_repr_attrs(diagnostic, a) {
             repr_type_name = match *r {
