40285ca717f687f8c1df8f28e53fb97392ac6eb5
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2016-03-06 15:54:44 +0300

    Apply visit_path to import prefixes by default

--- src/librustc_incremental/calculate_svh.rs
+++ src/librustc_incremental/calculate_svh.rs
@@ -401,10 +401,6 @@ mod svh_visitor {
             SawPath.hash(self.st); visit::walk_path(self, path)
         }
 
-        fn visit_path_list_item(&mut self, prefix: &'a Path, item: &'a PathListItem) {
-            SawPath.hash(self.st); visit::walk_path_list_item(self, prefix, item)
-        }
-
         fn visit_block(&mut self, b: &'a Block) {
             SawBlock.hash(self.st); visit::walk_block(self, b)
         }
