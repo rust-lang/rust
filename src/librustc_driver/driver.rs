2998564615df006440ff4cbb6dc00a91c8a17007
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-03-06 05:46:24 +0000

    Refactor away `ExternalExports`

--- src/librustc_driver/driver.rs
+++ src/librustc_driver/driver.rs
@@ -761,7 +761,6 @@ pub fn phase_3_run_analysis_passes<'tcx, F, R>(sess: &'tcx Session,
         freevars,
         export_map,
         trait_map,
-        external_exports,
         glob_map,
     } = time(time_passes,
              "resolution",
@@ -822,9 +821,7 @@ pub fn phase_3_run_analysis_passes<'tcx, F, R>(sess: &'tcx Session,
 
         analysis.access_levels =
             time(time_passes, "privacy checking", || {
-                rustc_privacy::check_crate(tcx,
-                                           &analysis.export_map,
-                                           external_exports)
+                rustc_privacy::check_crate(tcx, &analysis.export_map)
             });
 
         // Do not move this check past lint
