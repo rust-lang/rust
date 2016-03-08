e9cb96a56ab6dc883703928825ec7587db74dc1e
Alex Crichton <alex@alexcrichton.com>
2016-03-08 11:20:16 -0800

    rustdoc: Don't inline all impls all at once
    Right now whenever rustdoc inlines a struct or enum from another crate it ends
    up inlining *all* `impl` items found in the other crate at the same time. The
    rationale for this was to discover all trait impls which are otherwise not
    probed for. This unfortunately picks up a lot of impls of public traits for
    private types, causing lots of broken links.
    
    This commit instead hoards all of those inlined impls into a temporary storage
    location which is then selectively drawn from whenever we inline a new type.
    This should ensure that we still inline all relevant impls while avoiding all
    private ones.

--- src/librustdoc/core.rs
+++ src/librustdoc/core.rs
@@ -56,7 +56,7 @@ pub struct DocContext<'a, 'tcx: 'a> {
     pub external_traits: RefCell<Option<HashMap<DefId, clean::Trait>>>,
     pub external_typarams: RefCell<Option<HashMap<DefId, String>>>,
     pub inlined: RefCell<Option<HashSet<DefId>>>,
-    pub populated_crate_impls: RefCell<HashSet<ast::CrateNum>>,
+    pub all_crate_impls: RefCell<HashMap<ast::CrateNum, Vec<clean::Item>>>,
     pub deref_trait_did: Cell<Option<DefId>>,
 }
 
@@ -179,7 +179,7 @@ pub fn run_core(search_paths: SearchPaths, cfgs: Vec<String>, externs: Externs,
             external_typarams: RefCell::new(Some(HashMap::new())),
             external_paths: RefCell::new(Some(HashMap::new())),
             inlined: RefCell::new(Some(HashSet::new())),
-            populated_crate_impls: RefCell::new(HashSet::new()),
+            all_crate_impls: RefCell::new(HashMap::new()),
             deref_trait_did: Cell::new(None),
         };
         debug!("crate: {:?}", ctxt.map.krate());
