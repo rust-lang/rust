d4d6260ef1f21668194d4b3423d4b0b155e0b08e
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-03-09 19:27:36 +0100

    don't be a breaking change, even in presence of overflowing literals

--- src/test/run-pass/const-negation.rs
+++ src/test/run-pass/const-negation.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![feature(stmt_expr_attributes)]
+
 #[deny(const_err)]
 
 fn main() {
@@ -18,7 +20,10 @@ fn main() {
     assert_eq!(::std::i32::MIN as u64, 0xffffffff80000000);
     assert_eq!(-2147483648isize as u64, 0xffffffff80000000);
     assert_eq!(::std::i64::MIN as u64, 0x8000000000000000);
+    #[cfg(target_pointer_width = "64")]
     assert_eq!(-9223372036854775808isize as u64, 0x8000000000000000);
+    #[cfg(target_pointer_width = "32")]
+    assert_eq!(-9223372036854775808isize as u64, 0);
     const J: usize = ::std::i32::MAX as usize;
     const K: usize = -1i32 as u32 as usize;
     const L: usize = ::std::i32::MIN as usize;
