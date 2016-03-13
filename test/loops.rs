b756aecee7a7b1813fb5c12cf11d9f7d7007a546
Scott Olson <scott@solson.me>
2016-03-13 08:31:29 -0600

    Uncomment now-working test.

--- test/loops.rs
+++ test/loops.rs
@@ -1,15 +1,15 @@
 #![feature(custom_attribute)]
 #![allow(dead_code, unused_attributes)]
 
-// #[miri_run(expected = "Int(3628800)")]
-// fn factorial_loop() -> i64 {
-//     let mut product = 1;
-//     let mut i = 1;
+#[miri_run]
+fn factorial_loop() -> i64 {
+    let mut product = 1;
+    let mut i = 1;
 
-//     while i <= 10 {
-//         product *= i;
-//         i += 1;
-//     }
+    while i <= 10 {
+        product *= i;
+        i += 1;
+    }
 
-//     product
-// }
+    product
+}
