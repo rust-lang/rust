7eddb4e92a71e3842cf8f5ca5edacd02e22bd1b1
Scott Olson <scott@solson.me>
2016-03-14 23:25:13 -0600

    Test the unstable box syntax.

--- test/heap.rs
+++ test/heap.rs
@@ -1,7 +1,12 @@
-#![feature(custom_attribute)]
+#![feature(custom_attribute, box_syntax)]
 #![allow(dead_code, unused_attributes)]
 
 #[miri_run]
-fn make_box() -> Box<i32> {
-    Box::new(42)
+fn make_box() -> Box<(i16, i16)> {
+    Box::new((1, 2))
+}
+
+#[miri_run]
+fn make_box_syntax() -> Box<(i16, i16)> {
+    box (1, 2)
 }
