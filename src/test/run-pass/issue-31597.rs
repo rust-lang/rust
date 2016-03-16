96d9408dd9c932d5302c357d37f1a0262dcb51a9
Jonas Schievink <jonas@schievink.net>
2016-03-16 13:22:35 +0100

    Resolve conflicts and extend the test

--- src/test/run-pass/issue-31597.rs
+++ src/test/run-pass/issue-31597.rs
@@ -20,4 +20,18 @@ impl Make for () {
     fn make() -> Self::Out {}
 }
 
+// Also make sure we don't hit an ICE when the projection can't be known
+fn f<T: Make>() -> <T as Make>::Out { loop {} }
+
+// ...and that it works with a blanket impl
+trait Tr {
+    type Assoc;
+}
+
+impl<T: Make> Tr for T {
+    type Assoc = ();
+}
+
+fn g<T: Make>() -> <T as Tr>::Assoc { }
+
 fn main() {}
