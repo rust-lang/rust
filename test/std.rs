3cb200a2bacc1dc9cb83cf065235d28c2e2681a9
Scott Olson <scott@solson.me>
2016-03-21 04:18:30 -0600

    Add tests involving Rc, Arc, and Cell.

---
+++ test/std.rs
@@ -0,0 +1,19 @@
+#![feature(custom_attribute, box_syntax)]
+#![allow(dead_code, unused_attributes)]
+
+#[miri_run]
+fn rc_cell() -> i32 {
+    use std::rc::Rc;
+    use std::cell::Cell;
+    let r = Rc::new(Cell::new(42));
+    let x = r.get();
+    r.set(x + x);
+    r.get()
+}
+
+#[miri_run]
+fn arc() -> i32 {
+    use std::sync::Arc;
+    let a = Arc::new(42);
+    *a
+}
