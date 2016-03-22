4bfef5c1f6c8c728aa7dc0e2cce22bfd0b195a03
Niko Matsakis <niko@alum.mit.edu>
2016-03-22 07:37:35 -0400

    Error for where-clause with unconstrained late-bound lifetimes

--- src/librustc/ty/fold.rs
+++ src/librustc/ty/fold.rs
@@ -387,7 +387,8 @@ impl<'tcx> TyCtxt<'tcx> {
         where T : TypeFoldable<'tcx>
     {
         let mut collector = LateBoundRegionsCollector::new();
-        value.skip_binder().visit_with(&mut collector);
+        let result = value.skip_binder().visit_with(&mut collector);
+        assert!(!result); // should never have stopped early
         collector.regions
     }
 
@@ -667,7 +668,7 @@ impl<'tcx> TypeVisitor<'tcx> for LateBoundRegionsCollector {
             }
             _ => { }
         }
-        true
+        false
     }
 }
 
