814477a8935bb2ee666cc74f861e21cd4d1fa57a
Niko Matsakis <niko@alum.mit.edu>
2016-03-24 10:03:50 -0400

    Revert "workarounds to make link guards work on windows"
    This reverts commit b52004d202ddfffa100d4963216e1673a0be0b95.

--- src/rtstartup/rsbegin.rs
+++ src/rtstartup/rsbegin.rs
@@ -23,19 +23,8 @@
 // of other runtime components (registered via yet another special image section).
 
 #![crate_type="rlib"]
-#![feature(no_core, lang_items, optin_builtin_traits)]
-#![no_core]
+#![no_std]
 #![allow(non_camel_case_types)]
-
-#[lang="sized"]
-trait Sized {}
-
-#[lang="copy"]
-trait Copy {}
-
-#[lang="sync"]
-trait Sync {}
-impl Sync for .. {}
 
 #[cfg(all(target_os="windows", target_arch = "x86", target_env="gnu"))]
 pub mod eh_frames
