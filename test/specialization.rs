71b94c9a5d68bcbb4667efb6edc054eda8be5665
Scott Olson <scott@solson.me>
2016-03-28 17:59:48 -0600

    Add a basic test for specialization.

---
+++ test/specialization.rs
@@ -0,0 +1,19 @@
+#![feature(custom_attribute, specialization)]
+#![allow(dead_code, unused_attributes)]
+
+trait IsUnit {
+    fn is_unit() -> bool;
+}
+
+impl<T> IsUnit for T {
+    default fn is_unit() -> bool { false }
+}
+
+impl IsUnit for () {
+    fn is_unit() -> bool { true }
+}
+
+#[miri_run]
+fn specialization() -> (bool, bool) {
+    (i32::is_unit(), <()>::is_unit())
+}
