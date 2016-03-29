0b12f9a41a66e8636036397a21f993560133b386
Alex Crichton <alex@alexcrichton.com>
2016-03-29 13:14:52 -0700

    rustbuild: Migrate tidy checks to Rust
    This commit rewrites all of the tidy checks we have, namely:
    
    * featureck
    * errorck
    * tidy
    * binaries
    
    into Rust under a new `tidy` tool inside of the `src/tools` directory. This at
    the same time deletes all the corresponding Python tidy checks so we can be sure
    to only have one source of truth for all the tidy checks.
    
    cc #31590

---
+++ src/tools/tidy/src/bins.rs
@@ -0,0 +1,45 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Tidy check to ensure that there are no binaries checked into the source tree
+//! by accident.
+//!
+//! In the past we've accidentally checked in test binaries and such which add a
+//! huge amount of bloat to the git history, so it's good to just ensure we
+//! don't do that again :)
+
+use std::path::Path;
+
+// All files are executable on Windows, so just check on Unix
+#[cfg(windows)]
+pub fn check(_path: &Path, _bad: &mut bool) {}
+
+#[cfg(unix)]
+pub fn check(path: &Path, bad: &mut bool) {
+    use std::fs;
+    use std::os::unix::prelude::*;
+
+    super::walk(path,
+                &mut |path| super::filter_dirs(path) || path.ends_with("src/etc"),
+                &mut |file| {
+        let filename = file.file_name().unwrap().to_string_lossy();
+        let extensions = [".py", ".sh"];
+        if extensions.iter().any(|e| filename.ends_with(e)) {
+            return
+        }
+
+        let metadata = t!(fs::metadata(&file));
+        if metadata.mode() & 0o111 != 0 {
+            println!("binary checked into source: {}", file.display());
+            *bad = true;
+        }
+    })
+}
+
