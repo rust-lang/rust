425db89ea9de4985dc725ca657f87480ce2206fb
Michael Woerister <michaelwoerister@posteo.net>
2016-03-29 12:12:12 -0400

    Add run-make test for stable symbol-name generation.

---
+++ src/test/run-make/stable-symbol-names/stable-symbol-names1.rs
@@ -0,0 +1,22 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![crate_type="rlib"]
+
+pub fn some_test_function<T>(t: T) -> T {
+  t
+}
+
+pub fn user() {
+  some_test_function(0u32);
+  some_test_function("abc");
+  let x = 2u64;
+  some_test_function(&x);
+}
