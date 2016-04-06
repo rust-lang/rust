2d5196147fed8bc1b84725bcdf8e3744fd806819
Scott Olson <scott@solson.me>
2016-04-06 04:24:35 -0600

    Add test for comparing ptrs into different allocs.

--- test/errors.rs
+++ test/errors.rs
@@ -3,10 +3,17 @@
 
 #[miri_run]
 fn overwriting_part_of_relocation_makes_the_rest_undefined() -> i32 {
-    let mut p: *const i32 = &42;
+    let mut p = &42;
     unsafe {
-        let ptr = &mut p as *mut *const i32 as *mut u32;
-        *ptr = 123;
-        *p
+        let ptr: *mut _ = &mut p;
+        *(ptr as *mut u32) = 123;
     }
+    *p
+}
+
+#[miri_run]
+fn pointers_to_different_allocations_are_unorderable() -> bool {
+    let x: *const u8 = &1;
+    let y: *const u8 = &2;
+    x < y
 }
