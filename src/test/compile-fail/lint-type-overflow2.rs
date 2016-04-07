ad6e82668016acb2157f8112e3db3c3e075844b2
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-04-07 10:10:11 +0200

    split tests to check for early and late issues separately

--- src/test/compile-fail/lint-type-overflow2.rs
+++ src/test/compile-fail/lint-type-overflow2.rs
@@ -19,4 +19,8 @@ fn main() {
     let x =  3.40282348e+38_f32; //~ error: literal out of range for f32
     let x = -1.7976931348623159e+308_f64; //~ error: literal out of range for f64
     let x =  1.7976931348623159e+308_f64; //~ error: literal out of range for f64
+    let x: f32 = -3.40282348e+38; //~ error: literal out of range for f32
+    let x: f32 =  3.40282348e+38; //~ error: literal out of range for f32
+    let x: f64 = -1.7976931348623159e+308; //~ error: literal out of range for f64
+    let x: f64 =  1.7976931348623159e+308; //~ error: literal out of range for f64
 }
