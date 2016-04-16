f06bab77586e377a24393b9e2e49d1bb7003dc0b
Eduard Burtescu <edy.burt@gmail.com>
2016-04-16 21:51:26 +0300

    debuginfo: argument and upvar names for MIR.

--- src/test/debuginfo/var-captured-in-stack-closure.rs
+++ src/test/debuginfo/var-captured-in-stack-closure.rs
@@ -69,7 +69,7 @@
 // lldb-command:print *owned
 // lldb-check:[...]$9 = 6
 
-#![feature(unboxed_closures, box_syntax, rustc_attrs, stmt_expr_attributes)]
+#![feature(unboxed_closures, box_syntax)]
 #![allow(unused_variables)]
 #![feature(omit_gdb_pretty_printer_section)]
 #![omit_gdb_pretty_printer_section]
@@ -80,7 +80,6 @@ struct Struct {
     c: usize
 }
 
-#[rustc_no_mir] // FIXME(#31005) MIR debuginfo is missing captures.
 fn main() {
     let mut variable = 1;
     let constant = 2;
@@ -95,9 +94,7 @@ fn main() {
     let owned: Box<_> = box 6;
 
     {
-        let mut first_closure =
-        #[rustc_no_mir] // FIXME(#31005) MIR debuginfo is missing captures.
-        || {
+        let mut first_closure = || {
             zzz(); // #break
             variable = constant + a_struct.a + struct_ref.a + *owned;
         };
@@ -106,9 +103,7 @@ fn main() {
     }
 
     {
-        let mut second_closure =
-        #[rustc_no_mir] // FIXME(#31005) MIR debuginfo is missing captures.
-        || {
+        let mut second_closure = || {
             zzz(); // #break
             variable = constant + a_struct.a + struct_ref.a + *owned;
         };
