af069484503f65ca3ca1f4c7fec28f84c11a35a0
Eduard Burtescu <edy.burt@gmail.com>
2016-04-18 15:38:45 +0300

    Make data-layout mandatory in target specs.

--- src/librustc_back/target/i686_pc_windows_gnu.rs
+++ src/librustc_back/target/i686_pc_windows_gnu.rs
@@ -22,6 +22,7 @@ pub fn target() -> Target {
         llvm_target: "i686-pc-windows-gnu".to_string(),
         target_endian: "little".to_string(),
         target_pointer_width: "32".to_string(),
+        data_layout: "e-m:x-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32".to_string(),
         arch: "x86".to_string(),
         target_os: "windows".to_string(),
         target_env: "gnu".to_string(),
