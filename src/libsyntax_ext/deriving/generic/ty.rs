27f2ca1cf576cbe3d4aa36922e6562f850ddc769
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2016-04-22 23:43:14 +0300

    Remove some old code from libsyntax

--- src/libsyntax_ext/deriving/generic/ty.rs
+++ src/libsyntax_ext/deriving/generic/ty.rs
@@ -169,15 +169,14 @@ impl<'a> Ty<'a> {
                    -> ast::Path {
         match *self {
             Self_ => {
-                let self_params = self_generics.ty_params.map(|ty_param| {
+                let self_params = self_generics.ty_params.iter().map(|ty_param| {
                     cx.ty_ident(span, ty_param.ident)
-                });
+                }).collect();
                 let lifetimes = self_generics.lifetimes.iter()
                                                        .map(|d| d.lifetime)
                                                        .collect();
 
-                cx.path_all(span, false, vec!(self_ty), lifetimes,
-                            self_params.into_vec(), Vec::new())
+                cx.path_all(span, false, vec![self_ty], lifetimes, self_params, Vec::new())
             }
             Literal(ref p) => {
                 p.to_path(cx, span, self_ty, self_generics)
