3acee3b6c5381d84fe4ad68e077517bd0975333d
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-04-26 11:18:48 +0200

    const_err lint all constant expressions

--- src/test/compile-fail/lint-exceeding-bitshifts.rs
+++ src/test/compile-fail/lint-exceeding-bitshifts.rs
@@ -16,46 +16,64 @@
 fn main() {
       let n = 1u8 << 7;
       let n = 1u8 << 8;   //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1u16 << 15;
       let n = 1u16 << 16; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1u32 << 31;
       let n = 1u32 << 32; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1u64 << 63;
       let n = 1u64 << 64; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1i8 << 7;
       let n = 1i8 << 8;   //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1i16 << 15;
       let n = 1i16 << 16; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1i32 << 31;
       let n = 1i32 << 32; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1i64 << 63;
       let n = 1i64 << 64; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
 
       let n = 1u8 >> 7;
       let n = 1u8 >> 8;   //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1u16 >> 15;
       let n = 1u16 >> 16; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1u32 >> 31;
       let n = 1u32 >> 32; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1u64 >> 63;
       let n = 1u64 >> 64; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1i8 >> 7;
       let n = 1i8 >> 8;   //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1i16 >> 15;
       let n = 1i16 >> 16; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1i32 >> 31;
       let n = 1i32 >> 32; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
       let n = 1i64 >> 63;
       let n = 1i64 >> 64; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
 
       let n = 1u8;
       let n = n << 7;
       let n = n << 8; //~ ERROR: bitshift exceeds the type's number of bits
 
       let n = 1u8 << -8; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift by a negative amount
 
       let n = 1u8 << (4+3);
       let n = 1u8 << (4+4); //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
 
       #[cfg(target_pointer_width = "32")]
       const BITS: usize = 32;
@@ -63,11 +81,14 @@ fn main() {
       const BITS: usize = 64;
 
       let n = 1_isize << BITS; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
       let n = 1_usize << BITS; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift left with overflow
 
 
       let n = 1i8<<(1isize+-1);
 
       let n = 1i64 >> [63][0];
       let n = 1i64 >> [64][0]; //~ ERROR: bitshift exceeds the type's number of bits
+      //~^ WARN: attempted to shift right with overflow
 }
