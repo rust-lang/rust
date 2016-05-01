728cd03c57f4f8dc3620fcb174c1a08984c0e987
Tamir Duberstein <tamird@gmail.com>
2016-05-01 17:41:28 -0400

    rustc_back: use a common musl base

--- src/librustc_back/target/i686_unknown_linux_musl.rs
+++ src/librustc_back/target/i686_unknown_linux_musl.rs
@@ -8,30 +8,13 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// See x86_64_unknown_linux_musl for explanation of arguments
-
 use target::Target;
 
 pub fn target() -> Target {
-    let mut base = super::linux_base::opts();
+    let mut base = super::linux_musl_base::opts();
     base.cpu = "pentium4".to_string();
     base.pre_link_args.push("-m32".to_string());
     base.pre_link_args.push("-Wl,-melf_i386".to_string());
-
-    base.pre_link_args.push("-nostdlib".to_string());
-    base.pre_link_args.push("-static".to_string());
-    base.pre_link_args.push("-Wl,--eh-frame-hdr".to_string());
-
-    base.pre_link_args.push("-Wl,-(".to_string());
-    base.post_link_args.push("-Wl,-)".to_string());
-
-    base.pre_link_objects_exe.push("crt1.o".to_string());
-    base.pre_link_objects_exe.push("crti.o".to_string());
-    base.post_link_objects.push("crtn.o".to_string());
-
-    base.dynamic_linking = false;
-    base.has_rpath = false;
-    base.position_independent_executables = false;
 
     Target {
         llvm_target: "i686-unknown-linux-musl".to_string(),
