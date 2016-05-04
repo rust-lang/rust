26aed4392b245f899fa2ea3138276445c246bfd6
Jonathan Turner <jturner@mozilla.com>
2016-05-04 13:29:26 -0700

    Plumb through more debug output for failing tests

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -1019,7 +1019,7 @@ fn check_expected_errors(revision: Option<&str>,
     let expect_note = expected_errors.iter().any(|ee| ee.kind == Some(ErrorKind::Note));
 
     // Parse the JSON output from the compiler and extract out the messages.
-    let actual_errors = json::parse_output(&file_name, &proc_res.stderr);
+    let actual_errors = json::parse_output(&file_name, &proc_res.stderr, &proc_res);
     let mut unexpected = 0;
     let mut not_found = 0;
     let mut found = vec![false; expected_errors.len()];
@@ -1111,7 +1111,7 @@ struct ProcArgs {
     args: Vec<String>,
 }
 
-struct ProcRes {
+pub struct ProcRes {
     status: Status,
     stdout: String,
     stderr: String,
@@ -1576,7 +1576,7 @@ fn fatal(revision: Option<&str>, err: &str) -> ! {
     error(revision, err); panic!();
 }
 
-fn fatal_proc_rec(revision: Option<&str>, err: &str, proc_res: &ProcRes) -> ! {
+pub fn fatal_proc_rec(revision: Option<&str>, err: &str, proc_res: &ProcRes) -> ! {
     error(revision, err);
     print!("\
 status: {}\n\
