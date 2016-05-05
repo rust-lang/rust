3984ec1e99d86d0fb6671a85985e2ce5a7efe2a6
Tshepang Lekhonkhobe <tshepang@gmail.com>
2016-05-05 23:55:08 +0200

    doc: mut not needed

--- src/libstd/io/mod.rs
+++ src/libstd/io/mod.rs
@@ -182,7 +182,7 @@
 //!
 //! # fn foo() -> io::Result<()> {
 //! let f = try!(File::open("foo.txt"));
-//! let mut reader = BufReader::new(f);
+//! let reader = BufReader::new(f);
 //!
 //! for line in reader.lines() {
 //!     let line = try!(line);
