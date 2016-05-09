d460597e3373c7ec2a54f64faf6a1f8874b958bd
Eduard Burtescu <edy.burt@gmail.com>
2016-05-09 22:52:01 +0300

    trans: monomorphize varidiac call argument types.

--- src/librustc_trans/mir/block.rs
+++ src/librustc_trans/mir/block.rs
@@ -213,7 +213,8 @@ impl<'bcx, 'tcx> MirContext<'bcx, 'tcx> {
 
                 let extra_args = &args[sig.inputs.len()..];
                 let extra_args = extra_args.iter().map(|op_arg| {
-                    self.mir.operand_ty(bcx.tcx(), op_arg)
+                    let op_ty = self.mir.operand_ty(bcx.tcx(), op_arg);
+                    bcx.monomorphize(&op_ty)
                 }).collect::<Vec<_>>();
                 let fn_ty = callee.direct_fn_type(bcx.ccx(), &extra_args);
 
