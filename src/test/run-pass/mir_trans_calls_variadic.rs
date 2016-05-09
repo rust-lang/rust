52de8a78265ad6a9371ee6b59c6f7f0e57f39b93
Eduard Burtescu <edy.burt@gmail.com>
2016-05-09 22:52:01 +0300

    trans: monomorphize varidiac call argument types.

--- src/test/run-pass/mir_trans_calls_variadic.rs
+++ src/test/run-pass/mir_trans_calls_variadic.rs
@@ -16,17 +16,17 @@ extern {
 }
 
 #[rustc_mir]
-fn test(a: i64, b: i64, c: i64, d: i64, e: i64, f: i64) -> i64 {
+fn test<T, U>(a: i64, b: i64, c: i64, d: i64, e: i64, f: T, g: U) -> i64 {
     unsafe {
         rust_interesting_average(6, a, a as f64,
                                     b, b as f64,
                                     c, c as f64,
                                     d, d as f64,
                                     e, e as f64,
-                                    f, f as f64) as i64
+                                    f, g) as i64
     }
 }
 
 fn main(){
-    assert_eq!(test(10, 20, 30, 40, 50, 60), 70);
+    assert_eq!(test(10, 20, 30, 40, 50, 60_i64, 60.0_f64), 70);
 }
