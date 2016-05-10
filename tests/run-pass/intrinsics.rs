78caee20c7e15f286147fc1a35fb0682cb046ab9
Scott Olson <scott@solson.me>
2016-05-09 21:03:53 -0600

    Add test for size_of_val::<str>.

--- tests/run-pass/intrinsics.rs
+++ tests/run-pass/intrinsics.rs
@@ -10,4 +10,5 @@ fn main() {
     assert_eq!(size_of_val(&42), 4);
     assert_eq!(size_of_val(&[] as &[i32]), 0);
     assert_eq!(size_of_val(&[1, 2, 3] as &[i32]), 12);
+    assert_eq!(size_of_val("foobar"), 6);
 }
