a181d2fad466b716006e0ad8b68c022a84e27494
Tshepang Lekhonkhobe <tshepang@gmail.com>
2016-05-12 23:25:33 +0200

    doc: line these comments up
    Looks more nice, and same is done with prior examples

--- src/libcollections/fmt.rs
+++ src/libcollections/fmt.rs
@@ -81,7 +81,7 @@
 //!
 //! ```
 //! format!("{argument}", argument = "test");   // => "test"
-//! format!("{name} {}", 1, name = 2);        // => "2 1"
+//! format!("{name} {}", 1, name = 2);          // => "2 1"
 //! format!("{a} {c} {b}", a="a", b='b', c=3);  // => "a 3 b"
 //! ```
 //!
