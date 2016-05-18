f8a3cb4f5d0e1defbbf4ec0af0803355f4cfe172
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-05-18 01:50:29 +0000

    Test that a feature gated cfg variable in a `cfg_attr` on an unconfigured item is allowed

--- src/test/compile-fail/expanded-cfg.rs
+++ src/test/compile-fail/expanded-cfg.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(rustc_attrs)]
+#![feature(custom_attribute, rustc_attrs)]
 
 macro_rules! mac {
     {} => {
@@ -16,6 +16,9 @@ macro_rules! mac {
         mod m {
             #[lang_item]
             fn f() {}
+
+            #[cfg_attr(target_thread_local, custom)]
+            fn g() {}
         }
     }
 }
