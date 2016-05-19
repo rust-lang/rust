93fb14ff427bc9196af87edc3d8be5aaf4fb6c11
ggomez <guillaume1.gomez@gmail.com>
2016-05-19 14:00:43 +0200

    Add new error code tests

---
+++ src/test/compile-fail/E0053.rs
@@ -0,0 +1,24 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+trait Foo {
+    fn foo(x: u16);
+    fn bar(&self);
+}
+
+struct Bar;
+
+impl Foo for Bar {
+    fn foo(x: i16) { } //~ ERROR E0053
+    fn bar(&mut self) { } //~ ERROR E0053
+}
+
+fn main() {
+}
