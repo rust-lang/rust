70e71c59482ae12367aee7a7c3b40b661a8e82f7
Robert Habermeier <rphmeier@gmail.com>
2016-05-19 15:52:33 -0400

    update ExactSizeIterator docs to match iter doc changes

--- src/libcore/iter/traits.rs
+++ src/libcore/iter/traits.rs
@@ -473,15 +473,18 @@ impl<'a, I: DoubleEndedIterator + ?Sized> DoubleEndedIterator for &'a mut I {
 /// impl ExactSizeIterator for Counter {
 ///     // We already have the number of iterations, so we can use it directly.
 ///     fn len(&self) -> usize {
-///         self.count
+///         (5usize).saturating_sub(self.count)
 ///     }
 /// }
 ///
 /// // And now we can use it!
 ///
-/// let counter = Counter::new();
+/// let mut counter = Counter::new();
+/// assert_eq!(5, counter.len());
 ///
-/// assert_eq!(0, counter.len());
+/// // The length goes down by 1 after we get the next element.
+/// let _ = counter.next().unwrap();
+/// assert_eq!(4, counter.len());
 /// ```
 #[stable(feature = "rust1", since = "1.0.0")]
 pub trait ExactSizeIterator: Iterator {
