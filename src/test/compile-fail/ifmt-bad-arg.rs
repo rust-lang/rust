eab01eec9bb63017099d9fe8dd00dcf644a4fa8a
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-05-19 22:28:14 +0000

    Fix fallout in tests

--- src/test/compile-fail/ifmt-bad-arg.rs
+++ src/test/compile-fail/ifmt-bad-arg.rs
@@ -50,8 +50,4 @@ fn main() {
 
     format!("foo } bar"); //~ ERROR: unmatched `}` found
     format!("foo }"); //~ ERROR: unmatched `}` found
-
-    format!();          //~ ERROR: requires at least a format string argument
-    format!("" 1);      //~ ERROR: expected token: `,`
-    format!("", 1 1);   //~ ERROR: expected token: `,`
 }
