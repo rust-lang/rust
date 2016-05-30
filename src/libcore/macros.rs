dac8c08d34bf4b2afb8b5515d6292c837463c2ec
Mitsunori Komatsu <komamitsu@gmail.com>
2016-05-31 01:53:14 +0900

    Add message argument to `assert_eq` macro

--- src/libcore/macros.rs
+++ src/libcore/macros.rs
@@ -94,7 +94,18 @@ macro_rules! assert_eq {
                 }
             }
         }
-    })
+    });
+    ($left:expr , $right:expr, $($arg:tt)*) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    panic!("assertion failed: `(left == right)` \
+                           (left: `{:?}`, right: `{:?}`): {}", left_val, right_val,
+                           format_args!($($arg)*))
+                }
+            }
+        }
+    });
 }
 
 /// Ensure that a boolean expression is `true` at runtime.
