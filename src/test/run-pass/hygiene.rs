b8e020d243f2f607f6d1d85841454af715971cb0
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-06-20 12:13:05 +0000

    Add more hygiene tests

--- src/test/run-pass/hygiene.rs
+++ src/test/run-pass/hygiene.rs
@@ -18,4 +18,27 @@ fn main() {
 
     let x = 1;
     foo!();
+
+    g();
+}
+
+fn g() {
+    let x = 0;
+    macro_rules! m { ($x:ident) => {
+        macro_rules! m2 { () => { ($x, x) } }
+        let x = 1;
+        macro_rules! m3 { () => { ($x, x) } }
+    } }
+
+    let x = 2;
+    m!(x);
+
+    let x = 3;
+    assert_eq!(m2!(), (2, 0));
+    assert_eq!(m3!(), (2, 1));
+
+    let x = 4;
+    m!(x);
+    assert_eq!(m2!(), (4, 0));
+    assert_eq!(m3!(), (4, 1));
 }
