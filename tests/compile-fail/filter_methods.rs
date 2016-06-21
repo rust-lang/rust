eef439cb78d7efbf24856c81a78e2b38ebfbc189
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-06-21 13:48:56 +0200

    add tests

---
+++ tests/compile-fail/filter_methods.rs
@@ -0,0 +1,15 @@
+#![feature(plugin)]
+#![plugin(clippy)]
+
+#![deny(clippy, clippy_pedantic)]
+fn main() {
+    let _: Vec<_> = vec![5; 6].into_iter() //~ERROR called `filter(p).map(q)` on an Iterator
+                              .filter(|&x| x == 0)
+                              .map(|x| x * 2)
+                              .collect();
+
+    let _: Vec<_> = vec![5i8; 6].into_iter() //~ERROR called `filter(p).flat_map(q)` on an Iterator
+                                .filter(|&x| x == 0)
+                                .flat_map(|x| x.checked_mul(2))
+                                .collect();
+}
