ce1146ac5f4dee7eee135e2ea2a6d3d317b2d36a
Will Crichton <wcrichto@stanford.edu>
2016-06-30 16:32:13 -0400

    Moved LLVM cleanup to after `after_llvm` phase

--- src/librustc_driver/driver.rs
+++ src/librustc_driver/driver.rs
@@ -225,6 +225,8 @@ pub fn compile_input(sess: &Session,
                             phase5_result);
     phase5_result?;
 
+    write::cleanup_llvm(&trans);
+
     phase_6_link_output(sess, &trans, &outputs);
 
     Ok(())
