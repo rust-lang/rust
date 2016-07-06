ad053d66fec4e2cdea2a93621caa303029b3a1e9
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-07-06 17:55:05 +0200

    change the block and stmt position after a function call returns
    previously we moved to the target block *before* calling a function, so when inspecting
    the stack, it appeared as if we were in the first statement of the next block.

--- tests/compile-fail/cast_fn_ptr.rs
+++ tests/compile-fail/cast_fn_ptr.rs
@@ -1,9 +1,9 @@
 fn main() {
     fn f() {}
 
-    let g = unsafe { //~ ERROR tried to call a function of type
+    let g = unsafe {
         std::mem::transmute::<fn(), fn(i32)>(f)
     };
 
-    g(42)
+    g(42) //~ ERROR tried to call a function of type
 }
