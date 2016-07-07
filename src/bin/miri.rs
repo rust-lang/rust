a7d3a85d9e22e978e8156c2ad57b9d3c4a67dd13
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-07-07 11:20:09 +0200

    infer type of the various limits

--- src/bin/miri.rs
+++ src/bin/miri.rs
@@ -53,9 +53,9 @@ impl<'a> CompilerCalls<'a> for MiriCompilerCalls {
                         match item.node {
                             MetaItemKind::NameValue(ref name, ref value) => {
                                 match &**name {
-                                    "memory_size" => memory_size = extract_str(value).parse::<u64>().expect("not a number"),
-                                    "step_limit" => step_limit = extract_str(value).parse::<u64>().expect("not a number"),
-                                    "stack_limit" => stack_limit = extract_str(value).parse::<u64>().expect("not a number"),
+                                    "memory_size" => memory_size = extract_str(value).parse().expect("not a number"),
+                                    "step_limit" => step_limit = extract_str(value).parse().expect("not a number"),
+                                    "stack_limit" => stack_limit = extract_str(value).parse().expect("not a number"),
                                     _ => state.session.span_err(item.span, "unknown miri attribute"),
                                 }
                             }
