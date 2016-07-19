e8c54edfe1899b3296095a9e1b24ff8101084841
mcarton <cartonmartin+git@gmail.com>
2016-07-19 22:42:09 +0200

    Fix deploy.sh II

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -39,6 +39,16 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" != "$SOURCE_BRANC
     exit 0
 fi
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in github/deploy_key.enc -out github/deploy_key -d
+chmod 600 github/deploy_key
+eval $(ssh-agent -s)
+ssh-add github/deploy_key
+
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
@@ -52,16 +62,6 @@ fi
 
 git add .
 git commit -m "Automatic deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval $(ssh-agent -s)
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push "$SSH_REPO" "$TARGET_BRANCH"
