115592530548c623c205185e1d3b89ce7ce05b18
mcarton <cartonmartin+git@gmail.com>
2016-07-19 22:48:52 +0200

    Fix deploy.sh III

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -44,10 +44,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in github/deploy_key.enc -out github/deploy_key -d
-chmod 600 github/deploy_key
+openssl aes-256-cbc -K "$ENCRYPTED_KEY" -iv "$ENCRYPTED_IV" -in .github/deploy_key.enc -out .github/deploy_key -d
+chmod 600 .github/deploy_key
 eval $(ssh-agent -s)
-ssh-add github/deploy_key
+ssh-add .github/deploy_key
 
 # Now let's go have some fun with the cloned repo
 cd out
