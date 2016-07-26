9ffd0fe5a75b8eb640f0bd94385c7b5f675c7e95
Jorge Aparicio <japaricious@gmail.com>
2016-07-26 16:17:14 -0500

    arm-musl targets now use cfg(env = "musl")

--- src/liballoc_jemalloc/lib.rs
+++ src/liballoc_jemalloc/lib.rs
@@ -36,9 +36,7 @@ use libc::{c_int, c_void, size_t};
 #[cfg_attr(target_os = "android", link(name = "gcc"))]
 #[cfg_attr(all(not(windows),
                not(target_os = "android"),
-               not(target_env = "musl"),
-               not(target_env = "musleabi"),
-               not(target_env = "musleabihf")),
+               not(target_env = "musl")),
            link(name = "pthread"))]
 #[cfg(not(cargobuild))]
 extern "C" {}
