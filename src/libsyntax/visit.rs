69e27614c6b7317dde84eec728b85607b1064f3b
Andrew Cann <shum@canndrew.org>
2016-08-03 02:29:14 +0800

    Minor fixup.

--- src/libsyntax/visit.rs
+++ src/libsyntax/visit.rs
@@ -4,8 +4,7 @@
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your,
-//                 "The `!` type is experimental");
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
