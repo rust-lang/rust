67e7e44d4e408e55952f754d1fa8b2cbc5e0fc5f
Kenneth Adam Miller <kennethadammiller@gmail.com>
2016-08-03 14:23:24 -0400

    Rebasing changes onto upstream rust/master failed.
    The new type expected from each target is now TargetResult, which I
    didn't expect to change. This fixes the compile error.

--- src/librustc_back/target/x86_64_unknown_linux_uclibc.rs
+++ src/librustc_back/target/x86_64_unknown_linux_uclibc.rs
@@ -8,14 +8,14 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use target::Target;
+use target::{Target, TargetResult};
 
-pub fn target() -> Target {
+pub fn target() -> TargetResult {
     let mut base = super::linux_base::opts();
     base.cpu = "x86-64".to_string();
     base.max_atomic_width = 64;
     base.pre_link_args.push("-m64".to_string());
-    Target {
+    Ok(Target {
         llvm_target: "x86_64-unknown-linux-uclibc".to_string(),
         data_layout: "e-m:e-i64:64-f80:128-n8:16:32:64-S128".to_string(),
         target_endian: "little".to_string(),
@@ -25,5 +25,5 @@ pub fn target() -> Target {
         target_env: "uclibc".to_string(),
         target_vendor: "unknown".to_string(),
         options: base,
-    }
+    })
 }
