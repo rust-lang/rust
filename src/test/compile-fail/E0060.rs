0e756840f0625f06a5e150528820e96316f7929f
William Lee <wlee@mochify.com>
2016-08-04 08:07:14 -0400

    Tidying up some of the line spacing / code formatting for NOTE/ERROR annotation to match other files.

--- src/test/compile-fail/E0060.rs
+++ src/test/compile-fail/E0060.rs
@@ -13,7 +13,8 @@ extern "C" {
 }
 
 fn main() {
-    unsafe { printf(); } //~ ERROR E0060
-                         //~| NOTE expected at least 1 parameter
-                         //~| NOTE the following parameter type was expected
+    unsafe { printf(); } 
+    //~^ ERROR E0060
+    //~| NOTE expected at least 1 parameter
+    //~| NOTE the following parameter type was expected
 }
