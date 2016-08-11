80beeb35744f97d65759fec662043cbe34857e8b
Panashe M. Fundira <fundirap@gmail.com>
2016-08-11 14:03:38 -0400

    Add additional error note

--- src/test/compile-fail/E0033.rs
+++ src/test/compile-fail/E0033.rs
@@ -16,6 +16,7 @@ fn main() {
     let trait_obj: &SomeTrait = SomeTrait; //~ ERROR E0425
                                            //~^ ERROR E0038
                                            //~| method `foo` has no receiver
+                                           //~| NOTE the trait `SomeTrait` cannot be made into an object
 
     let &invalid = trait_obj;
     //~^ ERROR E0033
