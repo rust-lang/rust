b8b7cbeabd94dbc046ae35efa5524a3fac382071
Jorge Aparicio <japaricious@gmail.com>
2016-08-13 18:20:52 -0500

    qemu-ppc64le still doesn't work

--- library/compiler-builtins/ci/env.sh
+++ library/compiler-builtins/ci/env.sh
@@ -47,6 +47,7 @@ case $TARGET in
     powerpc64le-unknown-linux-gnu)
         export PREFIX=powerpc64le-linux-gnu-
         export QEMU_LD_PREFIX=/usr/powerpc64le-linux-gnu
+        export RUN_TESTS=n
         ;;
     thumbv*-none-eabi)
         export CARGO=xargo
