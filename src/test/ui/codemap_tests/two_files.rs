d52eb1ab0ce5a8a1c5e86b7d606c4eac5d4447ea
Seo Sanghyeon <sanxiyn@gmail.com>
2016-08-16 16:52:30 +0900

    RUST_NEW_ERROR_FORMAT is no more

--- src/test/ui/codemap_tests/two_files.rs
+++ src/test/ui/codemap_tests/two_files.rs
@@ -8,7 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// rustc-env:RUST_NEW_ERROR_FORMAT
 include!("two_files_data.rs");
 
 struct Baz { }
