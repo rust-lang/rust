b6f320a495d2d144b22c5a5fcfda1488e3d5133b
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-08-16 14:36:48 +0200

    address nits and improve docs

--- tests/compile-fail/module_inception.rs
+++ tests/compile-fail/module_inception.rs
@@ -5,11 +5,12 @@
 mod foo {
     mod bar {
         mod bar { //~ ERROR module has the same name as its containing module
-
+            mod foo {}
         }
+        mod foo {}
     }
     mod foo { //~ ERROR module has the same name as its containing module
-
+        mod bar {}
     }
 }
 
