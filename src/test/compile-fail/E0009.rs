d01bfb122e195696e72178e967191999c8745df4
Jacob <jacob@yola.com>
2016-08-17 10:21:15 -0700

    Remove trailing white space

--- src/test/compile-fail/E0009.rs
+++ src/test/compile-fail/E0009.rs
@@ -12,7 +12,7 @@ fn main() {
     struct X { x: (), }
     let x = Some((X { x: () }, X { x: () }));
     match x {
-        Some((y, ref z)) => {}, 
+        Some((y, ref z)) => {},
         //~^ ERROR E0009
         //~| NOTE by-move pattern here
         //~| NOTE both by-ref and by-move used
