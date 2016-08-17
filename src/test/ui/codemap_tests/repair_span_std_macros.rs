050cbe63f2a9fc37f4c6301fbb792bfa5dc37a52
Jonathan Turner <jturner@mozilla.com>
2016-08-17 07:20:04 -0700

    Replace local backtrace with def-use, repair std macro spans

--- src/test/ui/codemap_tests/repair_span_std_macros.rs
+++ src/test/ui/codemap_tests/repair_span_std_macros.rs
@@ -8,15 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern: requires at least a format string argument
-// error-pattern: in this expansion
-
-// error-pattern: expected token: `,`
-// error-pattern: in this expansion
-// error-pattern: in this expansion
-
 fn main() {
-    format!();
-    format!("" 1);
-    format!("", 1 1);
+    let x = vec![];
 }
