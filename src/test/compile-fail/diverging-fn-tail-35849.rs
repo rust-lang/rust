93a2faceb52ffcd1602c31b9af5bd801cd6a8c8b
Alex Burka <durka42@gmail.com>
2016-08-21 22:45:25 +0000

    typeck: use NoExpectation to check return type of diverging fn
    This fixes #35849, a regression introduced by the typeck refactoring
    around TyNever/!.

--- src/test/compile-fail/diverging-fn-tail-35849.rs
+++ src/test/compile-fail/diverging-fn-tail-35849.rs
@@ -1,4 +1,4 @@
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,19 +8,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// Test that we can use a ! for an argument of type !
-
-// ignore-test FIXME(durka) can't be done with the current liveness code
-// error-pattern:wowzers!
-
-#![feature(never_type)]
-#![allow(unreachable_code)]
-
-fn foo(x: !) -> ! {
-    x
+fn _converge() -> ! { //~ ERROR computation may converge
+    42
 }
 
-fn main() {
-    foo(panic!("wowzers!"))
-}
+fn main() { }
 
