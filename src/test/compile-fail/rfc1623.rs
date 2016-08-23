e0eb1ba0db9c832a0385f31710edddd675feee14
Andre Bogus <bogusandre@gmail.com>
2016-08-23 21:36:19 +0200

    fixed and extended tests, however...
    ...there is still one confusing thing – see the _BAZ functions, which
    appear to be elided in the `compile-fail` test and defaulted in the
    ´run-pass` test (if you uncomment line 73).

--- src/test/compile-fail/rfc1623.rs
+++ src/test/compile-fail/rfc1623.rs
@@ -13,10 +13,21 @@
 fn non_elidable<'a, 'b>(a: &'a u8, b: &'b u8) -> &'a u8 { a }
 
 // the boundaries of elision
-static NON_ELIDABLE_FN : &fn(&u8, &u8) -> &u8 = 
-//~^ERROR: missing lifetime specifier
+static NON_ELIDABLE_FN : &fn(&u8, &u8) -> &u8 =
+//^ERROR: missing lifetime specifier
         &(non_elidable as fn(&u8, &u8) -> &u8);
 
+type Baz<'a> = fn(&'a [u8]) -> Option<u8>;
+
+fn baz(e: &[u8]) -> Option<u8> { e.first().map(|x| *x) }
+
+static STATIC_BAZ : &Baz<'static> = &(baz as Baz);
+const CONST_BAZ : &Baz<'static> = &(baz as Baz);
+
 fn main() {
-    // nothing to do here
+    let y = [1u8, 2, 3];
+
+    //surprisingly this appears to work, so lifetime < `'static` is valid
+    assert_eq!(Some(1), STATIC_BAZ(y));
+    assert_eq!(Some(1), CONST_BAZ(y));
 }
