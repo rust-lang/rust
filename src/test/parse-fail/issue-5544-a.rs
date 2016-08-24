112c1276ac6e7bb7d04a9d94f3395835a54d0e4d
Simonas Kazlauskas <git@kazlauskas.me>
2016-08-24 22:38:14 +0300

    Fix parse-fail and compile-fail tests

--- src/test/parse-fail/issue-5544-a.rs
+++ src/test/parse-fail/issue-5544-a.rs
@@ -11,6 +11,6 @@
 // compile-flags: -Z parse-only
 
 fn main() {
-    let __isize = 18446744073709551616; // 2^64
+    let __isize = 340282366920938463463374607431768211456; // 2^128
     //~^ ERROR int literal is too large
 }
--- src/test/parse-fail/issue-5544-b.rs
+++ src/test/parse-fail/issue-5544-b.rs
@@ -11,6 +11,6 @@
 // compile-flags: -Z parse-only
 
 fn main() {
-    let __isize = 0xff_ffff_ffff_ffff_ffff;
+    let __isize = 0xffff_ffff_ffff_ffff_ffff_ffff_ffff_ffff_ff;
     //~^ ERROR int literal is too large
 }
