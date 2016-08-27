b13e64b0f4ae075703fcc764530321bc0ffca84c
Jorge Aparicio <japaricious@gmail.com>
2016-08-27 01:39:29 -0500

    fix cross compilation of std

--- src/liballoc_jemalloc/lib.rs
+++ src/liballoc_jemalloc/lib.rs
@@ -77,7 +77,8 @@ const MIN_ALIGN: usize = 8;
 #[cfg(all(any(target_arch = "x86",
               target_arch = "x86_64",
               target_arch = "aarch64",
-              target_arch = "powerpc64")))]
+              target_arch = "powerpc64",
+              target_arch = "mips64")))]
 const MIN_ALIGN: usize = 16;
 
 // MALLOCX_ALIGN(a) macro
