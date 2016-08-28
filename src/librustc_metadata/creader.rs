65249a5431bbc3bf6b0f0894aefcc28c76eb17bb
Gavin Baker <gavinb@antonym.org>
2016-08-28 22:55:39 +1000

    E0459 Update error format #35933
    - Fixes #35933
    - Part of #35233
    
    r? @jonathandturner

--- src/librustc_metadata/creader.rs
+++ src/librustc_metadata/creader.rs
@@ -977,8 +977,9 @@ impl<'a> LocalCrateReader<'a> {
             let n = match n {
                 Some(n) => n,
                 None => {
-                    span_err!(self.sess, m.span, E0459,
-                              "#[link(...)] specified without `name = \"foo\"`");
+                    struct_span_err!(self.sess, m.span, E0459,
+                                     "#[link(...)] specified without `name = \"foo\"`")
+                        .span_label(m.span, &format!("missing `name` argument")).emit();
                     InternedString::new("foo")
                 }
             };
