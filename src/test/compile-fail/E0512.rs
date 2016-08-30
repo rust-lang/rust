1c077f4b772d89845572fdfed6c2c58ad45a4aa0
Mateusz Bocian <matbocian91@gmail.com>
2016-08-30 00:24:16 -0400

    #36107 - Updated E0512 error message format

--- src/test/compile-fail/E0512.rs
+++ src/test/compile-fail/E0512.rs
@@ -12,4 +12,5 @@ fn takes_u8(_: u8) {}
 
 fn main() {
     unsafe { takes_u8(::std::mem::transmute(0u16)); } //~ ERROR E0512
+                                                      //~| transmuting between 16 bits and 8 bits
 }
