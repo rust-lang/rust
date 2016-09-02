39a85d3f46cbd94eb9d4cd8516c6d8849669ca47
Federico Ravasio <ravasio.federico@gmail.com>
2016-09-02 11:44:46 +0200

    E0493: showing a label where the destructor is defined.

--- src/test/compile-fail/E0493.rs
+++ src/test/compile-fail/E0493.rs
@@ -14,6 +14,15 @@ struct Foo {
 
 impl Drop for Foo {
     fn drop(&mut self) {}
+    //~^ NOTE destructor defined here
+}
+
+struct Bar {
+    a: u32
+}
+
+impl Drop for Bar {
+    fn drop(&mut self) {}
 }
 
 const F : Foo = Foo { a : 0 };
