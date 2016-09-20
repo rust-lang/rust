ae5765e97d74cfa5c43481d5c084b161d58174a8
mcarton <cartonmartin+git@gmail.com>
2016-09-20 18:05:00 +0200

    Don't ICE when a float can't be parsed

--- src/test/compile-fail/issue-31109.rs
+++ src/test/compile-fail/issue-31109.rs
@@ -12,4 +12,5 @@ fn main() {
     // FIXME(#31407) this error should go away, but in the meantime we test that it
     // is accompanied by a somewhat useful error message.
     let _: f64 = 1234567890123456789012345678901234567890e-340; //~ ERROR could not evaluate float
+    //~ ERROR unimplemented constant expression: could not evaluate float literal
 }
