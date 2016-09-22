a5b15eef7106bcf385e07116786d58895fd97fbf
Jorge Aparicio <japaricious@gmail.com>
2016-09-22 11:56:34 -0500

    fix build.rs
    we don't want to match musleabihf targets

--- library/compiler-builtins/build.rs
+++ library/compiler-builtins/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    if env::var("TARGET").unwrap().ends_with("hf") {
+    if env::var("TARGET").unwrap().ends_with("gnueabihf") {
         println!("cargo:rustc-cfg=gnueabihf")
     }
 }
