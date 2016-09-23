056b0c1cf2c55510f8b1e4698d31b71dce00c3b7
mcarton <cartonmartin+git@gmail.com>
2016-09-23 19:51:32 +0200

    Remove redundant labels in tests

--- tests/run-pass/deprecated.rs
+++ tests/run-pass/deprecated.rs
@@ -2,11 +2,11 @@
 #![plugin(clippy)]
 
 #[warn(str_to_string)]
-//~^WARNING: warning: lint str_to_string has been removed: using `str::to_string`
+//~^WARNING: lint str_to_string has been removed: using `str::to_string`
 #[warn(string_to_string)]
-//~^WARNING: warning: lint string_to_string has been removed: using `string::to_string`
+//~^WARNING: lint string_to_string has been removed: using `string::to_string`
 #[warn(unstable_as_slice)]
-//~^WARNING: warning: lint unstable_as_slice has been removed: `Vec::as_slice` has been stabilized
+//~^WARNING: lint unstable_as_slice has been removed: `Vec::as_slice` has been stabilized
 #[warn(unstable_as_mut_slice)]
-//~^WARNING: warning: lint unstable_as_mut_slice has been removed: `Vec::as_mut_slice` has been stabilized
+//~^WARNING: lint unstable_as_mut_slice has been removed: `Vec::as_mut_slice` has been stabilized
 fn main() {}
