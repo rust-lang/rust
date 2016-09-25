1a6fc8b7b840fb381b1aefa35b9b3d4c736bae50
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2016-09-24 23:38:56 -0500

    Add support for the Haiku operating system on x86 and x86_64 machines
    * Hand rebased from Niels original work on 1.9.0

---
+++ src/librustc_back/target/x86_64_unknown_haiku.rs
@@ -0,0 +1,33 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use target::Target;
+use target::TargetOptions;
+use std::default::Default;
+
+pub fn target() -> Target {
+    Target {
+        llvm_target: "x86_64-unknown-haiku".to_string(),
+        target_endian: "little".to_string(),
+        target_pointer_width: "64".to_string(),
+        data_layout: "e-m:e-i64:64-f80:128-n8:16:32:64-S128".to_string(),
+        arch: "x86_64".to_string(),
+        target_os: "haiku".to_string(),
+        target_env: "".to_string(),
+        target_vendor: "unknown".to_string(),
+        options: TargetOptions {
+            linker: "cc".to_string(),
+            dynamic_linking: true,
+            executables: true,
+            has_rpath: true,
+            .. Default::default()
+        },
+    }
+}
--- src/libstd/sys/unix/fd.rs
+++ src/libstd/sys/unix/fd.rs
@@ -104,9 +104,9 @@ impl FileDesc {
         // resolve so we at least compile this.
         //
         // [1]: http://comments.gmane.org/gmane.linux.lib.musl.general/2963
-        #[cfg(target_os = "android")]
+        #[cfg(any(target_os = "android", target_os = "haiku"))]
         use libc::F_DUPFD as F_DUPFD_CLOEXEC;
-        #[cfg(not(target_os = "android"))]
+        #[cfg(not(any(target_os = "android", target_os="haiku")))]
         use libc::F_DUPFD_CLOEXEC;
 
         let make_filedesc = |fd| {
