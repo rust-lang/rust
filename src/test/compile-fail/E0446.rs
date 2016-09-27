c0f29fdd58666df69352c66ecbc3be68b500da27
Jonathan Turner <jturner@mozilla.com>
2016-09-26 17:05:43 -0700

    Update E0446 label with improved wording

--- src/test/compile-fail/E0446.rs
+++ src/test/compile-fail/E0446.rs
@@ -12,7 +12,7 @@ mod Foo {
     struct Bar(u32);
 
     pub fn bar() -> Bar { //~ ERROR E0446
-                          //~| NOTE private type can't be public
+                          //~| NOTE can't leak private type
         Bar(0)
     }
 }
