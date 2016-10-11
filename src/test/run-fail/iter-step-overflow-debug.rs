a23032d34969ec3db26356e53f6b5e040273e178
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-11 15:58:27 -0400

    replace MIN/MAX constants with min_value/max_value functions
    MIN and MAX were breaking the build for me. min_value and max_value do not.

--- src/test/run-fail/iter-step-overflow-debug.rs
+++ src/test/run-fail/iter-step-overflow-debug.rs
@@ -14,13 +14,13 @@ use std::panic;
 
 fn main() {
     let r = panic::catch_unwind(|| {
-        let mut it = u8::MAX..;
+        let mut it = u8::max_value()..;
         it.next();
     });
     assert!(r.is_err());
 
     let r = panic::catch_unwind(|| {
-        let mut it = i8::MAX..;
+        let mut it = i8::max_value()..;
         it.next();
     });
     assert!(r.is_err());
