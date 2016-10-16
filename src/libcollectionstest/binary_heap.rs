57f8aa254a335c0e9fd0bf2fa816b21ff8463e97
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2016-10-16 15:48:22 +0530

    run rustfmt on libcollectionstest

--- src/libcollectionstest/binary_heap.rs
+++ src/libcollectionstest/binary_heap.rs
@@ -299,5 +299,7 @@ fn test_extend_specialization() {
 
 #[allow(dead_code)]
 fn assert_covariance() {
-    fn drain<'new>(d: Drain<'static, &'static str>) -> Drain<'new, &'new str> { d }
+    fn drain<'new>(d: Drain<'static, &'static str>) -> Drain<'new, &'new str> {
+        d
+    }
 }
