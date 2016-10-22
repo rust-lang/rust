cb26d5503ec5ccceeedd6861dd406f174a9af93e
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-22 16:27:31 -0400

    unpwrap iterators in tests

--- src/test/run-fail/iter-step-overflow-debug.rs
+++ src/test/run-fail/iter-step-overflow-debug.rs
@@ -15,13 +15,13 @@ use std::panic;
 fn main() {
     let r = panic::catch_unwind(|| {
         let mut it = u8::max_value()..;
-        it.next();
+        it.next().unwrap();
     });
     assert!(r.is_err());
 
     let r = panic::catch_unwind(|| {
         let mut it = i8::max_value()..;
-        it.next();
+        it.next().unwrap();
     });
     assert!(r.is_err());
 }
