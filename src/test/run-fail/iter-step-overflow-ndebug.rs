d3d07f653923561dd906d34e16dead747c20edb4
Matthew Piziak <matthew.piziak@gmail.com>
2016-10-22 16:27:31 -0400

    unpwrap iterators in tests

--- src/test/run-fail/iter-step-overflow-ndebug.rs
+++ src/test/run-fail/iter-step-overflow-ndebug.rs
@@ -12,8 +12,8 @@
 
 fn main() {
     let mut it = u8::max_value()..;
-    assert_eq!(it.next(), u8::min_value());
+    assert_eq!(it.next().unwrap(), u8::min_value());
 
     let mut it = i8::max_value()..;
-    assert_eq!(it.next(), i8::min_value());
+    assert_eq!(it.next().unwrap(), i8::min_value());
 }
