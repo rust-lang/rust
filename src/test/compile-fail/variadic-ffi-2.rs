869003607f78bf9b40be80f201458235be35782f
Tim Neumann <mail@timnn.me>
2016-10-25 19:56:36 +0200

    adapt tests

--- src/test/compile-fail/variadic-ffi-2.rs
+++ src/test/compile-fail/variadic-ffi-2.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-fn baz(f: extern "stdcall" fn(usize, ...)) {
+fn baz(f: extern "cdecl" fn(usize, ...)) {
     //~^ ERROR: variadic function must have C calling convention
     f(22, 44);
 }
