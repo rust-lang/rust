34d14df2c72759e96425827186bc79e9e4d1f87b
Mark-Simulacrum <mark.simulacrum@gmail.com>
2016-10-27 21:24:24 -0600

    Track errors from resolving the crate.

--- src/librustc_driver/driver.rs
+++ src/librustc_driver/driver.rs
@@ -766,8 +766,7 @@ pub fn phase_2_configure_and_expand<'a, F>(sess: &Session,
         // don't perform `after_expand` until after import resolution.
         after_expand(&krate)?;
 
-        resolver.resolve_crate(&krate);
-        Ok(())
+        sess.track_errors(|| resolver.resolve_crate(&krate))
     })?;
 
     // Lower ast -> hir.
