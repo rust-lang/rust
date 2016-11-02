8f19d5c3f6e9e6b37a63175875eae6c11c56822e
Matthew Piziak <matthew.piziak@gmail.com>
2016-11-02 10:11:53 -0400

    add max value from iterator

--- src/test/run-pass/iter-step-overflow-debug.rs
+++ src/test/run-pass/iter-step-overflow-debug.rs
@@ -15,12 +15,14 @@ use std::panic;
 fn main() {
     let r = panic::catch_unwind(|| {
         let mut it = u8::max_value()..;
+        it.next().unwrap(); // 255
         it.next().unwrap();
     });
     assert!(r.is_err());
 
     let r = panic::catch_unwind(|| {
         let mut it = i8::max_value()..;
+        it.next().unwrap(); // 127
         it.next().unwrap();
     });
     assert!(r.is_err());
--- src/test/run-pass/iter-step-overflow-ndebug.rs
+++ src/test/run-pass/iter-step-overflow-ndebug.rs
@@ -12,8 +12,10 @@
 
 fn main() {
     let mut it = u8::max_value()..;
+    assert_eq!(it.next().unwrap(), 255);
     assert_eq!(it.next().unwrap(), u8::min_value());
 
     let mut it = i8::max_value()..;
+    assert_eq!(it.next().unwrap(), 127);
     assert_eq!(it.next().unwrap(), i8::min_value());
 }
