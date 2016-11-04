2937b993973cf2faa494ebc73cfefa1ba8d95a62
est31 <MTest31@outlook.com>
2016-11-04 22:37:51 +0100

    Add error when proc_macro_derive is used not on functions
    Fixes #37590

--- src/test/compile-fail-fulldeps/proc-macro/illegal-proc-macro-derive-use.rs
+++ src/test/compile-fail-fulldeps/proc-macro/illegal-proc-macro-derive-use.rs
@@ -18,4 +18,10 @@ pub fn foo(a: proc_macro::TokenStream) -> proc_macro::TokenStream {
     a
 }
 
+// Issue #37590
+#[proc_macro_derive(Foo)]
+//~^ ERROR: the `#[proc_macro_derive]` attribute may only be used on bare functions
+pub struct Foo {
+}
+
 fn main() {}
