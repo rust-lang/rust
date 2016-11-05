7a9e7fbd674d6a7d49091f9822b35a36826f5308
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2016-11-05 05:31:35 +0000

    Add a regression test and organize tests.

--- src/test/compile-fail/directory_ownership/unowned_mod_with_path.rs
+++ src/test/compile-fail/directory_ownership/unowned_mod_with_path.rs
@@ -1,4 +1,4 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,10 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// compile-flags: -Z parse-only
-
 // error-pattern: cannot declare a new module at this location
 
-mod mod_file_not_owning_aux1;
-
-fn main() {}
+// This is not a directory owner since the file name is not "mod.rs".
+#[path = "mod_file_not_owning_aux1.rs"]
+mod foo;
