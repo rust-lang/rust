cfae1e9fda47587aa87ed7c1123001b7897315b4
Oliver 'ker' Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-11-10 17:06:39 +0100

    fix ice in `len_zero` lint when type has no inherent impls at all
    fixes #1336

---
+++ tests/ice_exacte_size.rs
@@ -0,0 +1,17 @@
+#![feature(plugin)]
+#![plugin(clippy)]
+#![deny(clippy)]
+
+#[allow(dead_code)]
+struct Foo;
+
+impl Iterator for Foo {
+    type Item = ();
+
+    fn next(&mut self) -> Option<()> {
+        let _ = self.len() == 0;
+        unimplemented!()
+    }
+}
+
+impl ExactSizeIterator for Foo { }
