f71c31c0e866f87e7aaafc315b042c077ff302b5
Oliver 'ker' Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-11-13 21:30:03 +0100

    cannot index into non-fat-pointers

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -734,16 +734,12 @@ impl<'a, 'tcx> EvalContext<'a, 'tcx> {
             ty::TyRef(_, ty::TypeAndMut { ty, .. }) |
             ty::TyRawPtr(ty::TypeAndMut { ty, .. }) |
             ty::TyBox(ty) => {
-                if self.type_is_sized(ty) {
-                    assert_eq!(field_index, 0);
-                    Ok(self.tcx.mk_imm_ptr(self.tcx.types.never))
-                } else {
-                    match (field_index, &ty.sty) {
-                        (1, &ty::TySlice(_)) => Ok(self.tcx.types.usize),
-                        (1, &ty::TyTrait(_)) |
-                        (0, _) => Ok(self.tcx.mk_imm_ptr(self.tcx.types.never)),
-                        _ => bug!("invalid fat pointee type"),
-                    }
+                assert!(!self.type_is_sized(ty));
+                match (field_index, &self.tcx.struct_tail(ty).sty) {
+                    (1, &ty::TySlice(_)) => Ok(self.tcx.types.usize),
+                    (1, &ty::TyTrait(_)) |
+                    (0, _) => Ok(self.tcx.mk_imm_ptr(self.tcx.types.u8)),
+                    _ => bug!("invalid fat pointee type"),
                 }
             }
             _ => Err(EvalError::Unimplemented(format!("can't handle type: {:?}, {:?}", ty, ty.sty))),
