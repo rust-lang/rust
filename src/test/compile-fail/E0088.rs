92abce2addffb8394406fda764e83e81ebd29d08
Omer Sheikh <ojsheikh@gmail.com>
2016-11-16 20:56:57 -0500

    Use span of first unexpected lifetime in E0088.

--- src/test/compile-fail/E0088.rs
+++ src/test/compile-fail/E0088.rs
@@ -14,7 +14,7 @@ fn g<'a>() {}
 fn main() {
     f::<'static>(); //~ ERROR E0088
     //~^ unexpected lifetime parameter
-    
+
     g::<'static, 'static>(); //~ ERROR E0088
     //~^ unexpected lifetime parameters
 }
