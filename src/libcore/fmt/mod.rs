2121118f5401d84975f7910e0b71b32e74b990ec
Andrew Cann <shum@canndrew.org>
2016-11-23 17:13:12 +0800

    Revert libcore changes

--- src/libcore/fmt/mod.rs
+++ src/libcore/fmt/mod.rs
@@ -166,9 +166,7 @@ pub struct Formatter<'a> {
 // NB. Argument is essentially an optimized partially applied formatting function,
 // equivalent to `exists T.(&T, fn(&T, &mut Formatter) -> Result`.
 
-struct Void {
-    _private: (),
-}
+enum Void {}
 
 /// This struct represents the generic "argument" which is taken by the Xprintf
 /// family of functions. It contains a function to format the given value. At
