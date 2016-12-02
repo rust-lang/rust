dd2a2bf2590f534163b96b2c3fa29fff0f4dcefb
Jonathan Turner <jturner@mozilla.com>
2016-12-01 16:33:48 -0800

    Point arg num mismatch errors back to their definition

--- src/test/compile-fail/issue-4935.rs
+++ src/test/compile-fail/issue-4935.rs
@@ -11,6 +11,7 @@
 // Regression test for issue #4935
 
 fn foo(a: usize) {}
+//~^ defined here
 fn main() { foo(5, 6) }
 //~^ ERROR this function takes 1 parameter but 2 parameters were supplied
-//~| NOTE the following parameter type was expected
+//~| NOTE expected 1 parameter
