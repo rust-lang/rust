fe15880beb3359bcf6d4d5bd8f527a553e2e86bf
mcarton <cartonmartin+git@gmail.com>
2016-12-02 20:54:05 +0100

    Revert "down to for-loops not comparing as equal"
    This reverts commit cf1fa94be1e98dba2ce1755e1c86acef50daf599.

--- tests/compile-fail/copies.rs
+++ tests/compile-fail/copies.rs
@@ -224,18 +224,16 @@ fn if_same_then_else() -> Result<&'static str, ()> {
     }
 
     if true {
-        //~^NOTE same as this
         if let Some(42) = None {}
     }
-    else { //~ERROR this `if` has identical blocks
+    else {
         if let Option::Some(42) = None {}
     }
 
     if true {
-        //~^NOTE same as this
         if let Some(42) = None::<u8> {}
     }
-    else { //~ERROR this `if` has identical blocks
+    else {
         if let Some(42) = None {}
     }
 
