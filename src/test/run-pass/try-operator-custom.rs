97b92e153440267918fe5f92f8b8f8764f7693d1
Ulrik Sverdrup <bluss@users.noreply.github.com>
2016-12-11 18:30:56 +0100

    Adapt the `x?` test for QuestionMark / Try

--- src/test/run-pass/try-operator-custom.rs
+++ src/test/run-pass/try-operator-custom.rs
@@ -8,33 +8,25 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(question_mark, question_mark_carrier)]
+#![feature(question_mark, question_mark_qm, question_mark_try)]
 
-use std::ops::Carrier;
+use std::ops::QuestionMark;
+use std::ops::Try;
 
-enum MyResult<T, U> {
-    Awesome(T),
-    Terrible(U)
+#[derive(PartialEq)]
+enum MyResult<U, V> {
+    Awesome(U),
+    Terrible(V)
 }
 
-impl<U, V> Carrier for MyResult<U, V> {
-    type Success = U;
-    type Error = V;
-
-    fn from_success(u: U) -> MyResult<U, V> {
-        MyResult::Awesome(u)
-    }
-
-    fn from_error(e: V) -> MyResult<U, V> {
-        MyResult::Terrible(e)
-    }
-
-    fn translate<T>(self) -> T
-        where T: Carrier<Success=U, Error=V>
-    {
+impl<U, V, X, Y> QuestionMark<MyResult<X, Y>> for MyResult<U, V>
+    where Y: From<V>,
+{
+    type Continue = U;
+    fn question_mark(self) -> Try<Self::Continue, MyResult<X, Y>> {
         match self {
-            MyResult::Awesome(u) => T::from_success(u),
-            MyResult::Terrible(e) => T::from_error(e),
+            MyResult::Awesome(u) => Try::Continue(u),
+            MyResult::Terrible(e) => Try::Done(MyResult::Terrible(Y::from(e))),
         }
     }
 }
@@ -43,31 +35,28 @@ fn f(x: i32) -> Result<i32, String> {
     if x == 0 {
         Ok(42)
     } else {
-        let y = g(x)?;
+        let y = Err(String::new())?;
         Ok(y)
     }
 }
 
 fn g(x: i32) -> MyResult<i32, String> {
-    let _y = f(x - 1)?;
-    MyResult::Terrible("Hello".to_owned())
+    if x == 0 {
+        return MyResult::Awesome(42);
+    } else {
+        let _y = i()?;
+        MyResult::Awesome(1)
+    }
 }
 
-fn h() -> MyResult<i32, String> {
-    let a: Result<i32, &'static str> = Err("Hello");
-    let b = a?;
-    MyResult::Awesome(b)
-}
-
-fn i() -> MyResult<i32, String> {
-    let a: MyResult<i32, &'static str> = MyResult::Terrible("Hello");
+fn i() -> MyResult<u32, String> {
+    let a: MyResult<u32, &'static str> = MyResult::Terrible("Hello");
     let b = a?;
     MyResult::Awesome(b)
 }
 
 fn main() {
-    assert!(f(0) == Ok(42));
-    assert!(f(10) == Err("Hello".to_owned()));
-    let _ = h();
+    assert!(g(0) == MyResult::Awesome(42));
+    assert!(g(10) == MyResult::Terrible("Hello".to_owned()));
     let _ = i();
 }
