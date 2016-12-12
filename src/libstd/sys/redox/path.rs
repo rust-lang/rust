0e619d5572aeee64daf5fb40ad86da87349b511c
Jeremy Soller <jackpot51@gmail.com>
2016-12-12 15:57:19 -0700

    Fix tidy checks

--- src/libstd/sys/redox/path.rs
+++ src/libstd/sys/redox/path.rs
@@ -24,7 +24,8 @@ pub fn is_verbatim_sep(b: u8) -> bool {
 pub fn parse_prefix(path: &OsStr) -> Option<Prefix> {
     if let Some(path_str) = path.to_str() {
         if let Some(_i) = path_str.find(':') {
-            //TODO: Redox specific prefix Some(Prefix::Verbatim(OsStr::new(&path_str[..i])))
+            // FIXME: Redox specific prefix
+            // Some(Prefix::Verbatim(OsStr::new(&path_str[..i])))
             None
         } else {
             None
