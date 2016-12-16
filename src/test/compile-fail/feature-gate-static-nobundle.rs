a071d36e9623885f0a99af1a0659391e2c7e092c
Vadim Chugunov <vadimcn@gmail.com>
2016-12-15 23:46:21 -0800

    Feature gate

--- src/test/compile-fail/feature-gate-static-nobundle.rs
+++ src/test/compile-fail/feature-gate-static-nobundle.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,16 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![crate_type = "rlib"]
-#![feature(static_nobundle)]
-
-#[link(name = "foo", kind = "static-nobundle")]
-extern {
-    pub fn func();
-}
-
-pub fn wrapped_func() {
-    unsafe {
-        func();
-    }
-}
+#[link(name="foo", kind="static-nobundle")]
+//~^ ERROR: kind="static-nobundle" is feature gated
+extern {}
