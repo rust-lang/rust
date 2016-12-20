01157e6b3cf3acd2c555d36e272c7ad05d837868
Jeremy Soller <jackpot51@gmail.com>
2016-12-20 14:30:27 -0700

    Link openlibm only in libstd

--- src/libstd/build.rs
+++ src/libstd/build.rs
@@ -62,6 +62,8 @@ fn main() {
         println!("cargo:rustc-link-lib=magenta");
         println!("cargo:rustc-link-lib=mxio");
         println!("cargo:rustc-link-lib=launchpad"); // for std::process
+    } else if target.contains("redox") {
+        println!("cargo:rustc-link-lib=openlibm");
     }
 }
 
