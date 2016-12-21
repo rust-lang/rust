48f2b2c79e3734aa18828760282ac676724644db
Alex Crichton <alex@alexcrichton.com>
2016-12-20 18:51:41 -0800

    mk: Fix compile with makefiles
    A tweak was made to dependencies in #38451 but the makefiles weren't updated to
    accompany this. Instead of trying to integerate the `build_helper` crate into
    the makefiles (which currently isn't present) this commit takes the approach of
    just duplicating the required logic, which should be small enough for now.

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate build_helper;
-
 use common::Config;
 use common::{CompileFail, ParseFail, Pretty, RunFail, RunPass, RunPassValgrind};
 use common::{Codegen, DebugInfoLldb, DebugInfoGdb, Rustdoc, CodegenUnits};
@@ -2110,7 +2108,16 @@ actual:\n\
         }
         self.create_dir_racy(&tmpdir);
 
-        let mut cmd = Command::new(build_helper::make(&self.config.host));
+        let host = &self.config.host;
+        let make = if host.contains("bitrig") || host.contains("dragonfly") ||
+            host.contains("freebsd") || host.contains("netbsd") ||
+            host.contains("openbsd") {
+            "gmake"
+        } else {
+            "make"
+        };
+
+        let mut cmd = Command::new(make);
         cmd.current_dir(&self.testpaths.file)
            .env("TARGET", &self.config.target)
            .env("PYTHON", &self.config.docck_python)
