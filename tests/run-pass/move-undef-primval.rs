d6e35fe46fbb170c882277cff3532caec04e4940
Scott Olson <scott@solson.me>
2016-12-21 17:26:52 -0800

    Add test for #95.

---
+++ tests/run-pass/move-undef-primval.rs
@@ -0,0 +1,12 @@
+struct Foo {
+    _inner: i32,
+}
+
+fn main() {
+    unsafe {
+        let foo = Foo {
+            _inner: std::mem::uninitialized(),
+        };
+        let _bar = foo;
+    }
+}
