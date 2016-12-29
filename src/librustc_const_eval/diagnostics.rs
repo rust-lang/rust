4136ba072e1b8ff5c06de2c3dc4b72773ae1b3e5
Andrew Cann <shum@canndrew.org>
2016-12-29 12:38:02 +0800

    Remove E0001 diagnostic

--- src/librustc_const_eval/diagnostics.rs
+++ src/librustc_const_eval/diagnostics.rs
@@ -15,31 +15,6 @@
 // In vim you can `:set tw=80` and use `gq` to wrap paragraphs. Use `:set tw=0` to disable.
 register_long_diagnostics! {
 
-E0001: r##"
-This error suggests that the expression arm corresponding to the noted pattern
-will never be reached as for all possible values of the expression being
-matched, one of the preceding patterns will match.
-
-This means that perhaps some of the preceding patterns are too general, this
-one is too specific or the ordering is incorrect.
-
-For example, the following `match` block has too many arms:
-
-```compile_fail,E0001
-match Some(0) {
-    Some(bar) => {/* ... */}
-    x => {/* ... */} // This handles the `None` case
-    _ => {/* ... */} // All possible cases have already been handled
-}
-```
-
-`match` blocks have their patterns matched in order, so, for example, putting
-a wildcard arm above a more specific arm will make the latter arm irrelevant.
-
-Ensure the ordering of the match arm is correct and remove any superfluous
-arms.
-"##,
-
 E0002: r##"
 ## Note: this error code is no longer emitted by the compiler.
 
