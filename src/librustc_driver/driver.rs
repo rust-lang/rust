a8491587e7fbba54b32a8500ae52385c1004d527
Kyle Aleshire <kjaleshire@gmail.com>
2017-01-04 18:23:19 -0600

    Warn that the name is ignored if a link target is emitted

--- src/librustc_driver/driver.rs
+++ src/librustc_driver/driver.rs
@@ -1335,9 +1335,10 @@ pub fn build_output_filenames(input: &Input,
                                            .values()
                                            .filter(|a| a.is_none())
                                            .count();
-            let ofile = if unnamed_output_types > 1 {
-                sess.warn("ignoring specified output filename because multiple outputs were \
-                           requested");
+            let ofile = if unnamed_output_types > 1 &&
+                            sess.opts.output_types.contains_key(&OutputType::Exe) {
+                sess.warn("ignoring output name requested with -o for \"link\" output because \
+                           multiple outputs were requested");
                 None
             } else {
                 Some(out_file.clone())
