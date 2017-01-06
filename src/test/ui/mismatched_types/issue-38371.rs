ca4c5f17b3ad5e30ef7b361708779a5d8fb22594
Esteban Küber <esteban@kuber.com.ar>
2017-01-05 17:40:26 -0800

    add test case

--- src/test/ui/mismatched_types/issue-38371.rs
+++ src/test/ui/mismatched_types/issue-38371.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -7,20 +7,28 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+#![feature(slice_patterns)]
+
 
 struct Foo {
 }
 
-fn foo(&foo: Foo) {  // illegal syntax
+fn foo(&foo: Foo) {
 }
 
-fn bar(foo: Foo) {  // legal
+fn bar(foo: Foo) {
 }
 
-fn qux(foo: &Foo) {  // legal
+fn qux(foo: &Foo) {
 }
 
-fn zar(&foo: &Foo) {  // legal
+fn zar(&foo: &Foo) {
+}
+
+fn agh(&&bar: &u32) {
+}
+
+fn ugh(&[bar]: &u32) {
 }
 
 fn main() {}
