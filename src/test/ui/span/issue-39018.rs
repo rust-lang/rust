b54f593cffc40e9d07650b36629e60c48da6b11d
Michael Gattozzi <mgattozzi@gmail.com>
2017-01-14 15:25:33 -0500

    Add clearer error message using `&str + &str`
    This is the first part of #39018. One of the common things for new users
    coming from more dynamic languages like JavaScript, Python or Ruby is to
    use `+` to concatenate strings. However, this doesn't work that way in
    Rust unless the first type is a `String`. This commit adds a check for
    this use case and outputs a new error as well as a suggestion to guide
    the user towards the desired behavior. It also adds a new test case to
    test the output of the error.

---
+++ src/test/ui/span/issue-39018.rs
@@ -0,0 +1,23 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+pub fn main() {
+    let x = "Hello " + "World!";
+
+    // Make sure that the span outputs a warning
+    // for not having an implementation for std::ops::Add
+    // that won't output for the above string concatenation
+    let y = World::Hello + World::Goodbye;
+}
+
+enum World {
+    Hello,
+    Goodbye,
+}
