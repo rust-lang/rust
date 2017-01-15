4bdad4f4d6b36860d5bc5a4b8bda06598e55d5b6
Christopher Armstrong <radix@twistedmatrix.com>
2017-01-14 18:59:33 -0600

    Minor improvement to strange grammar in E0525

--- src/librustc/diagnostics.rs
+++ src/librustc/diagnostics.rs
@@ -1618,8 +1618,7 @@ fn cookie() -> ! { // error: definition of an unknown language item: `cookie`
 "##,
 
 E0525: r##"
-A closure was attempted to get used whereas it doesn't implement the expected
-trait.
+A closure was used where it didn't implement the expected trait.
 
 Erroneous code example:
 
