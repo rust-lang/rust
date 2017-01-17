27d47f970ad6e0b6833a3fe1c194c6bb0db4fd87
Andre Bogus <bogusandre@gmail.com>
2017-01-17 16:12:09 +0100

    rustfmt tests to fix build

--- tests/issue-825.rs
+++ tests/issue-825.rs
@@ -7,8 +7,8 @@
 fn rust_type_id(name: String) {
     if "bool" == &name[..] || "uint" == &name[..] || "u8" == &name[..] || "u16" == &name[..] ||
        "u32" == &name[..] || "f32" == &name[..] || "f64" == &name[..] || "i8" == &name[..] ||
-       "i16" == &name[..] || "i32" == &name[..] || "i64" == &name[..] ||
-       "Self" == &name[..] || "str" == &name[..] {
+       "i16" == &name[..] || "i32" == &name[..] ||
+       "i64" == &name[..] || "Self" == &name[..] || "str" == &name[..] {
         unreachable!();
     }
 }
