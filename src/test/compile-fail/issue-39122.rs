5c58653e46b73bbc2bb6811be2eefa4b6675c942
Chris Stankus <cdstankus@gmail.com>
2017-01-18 00:16:19 -0600

    E0122 should apply with where clauses

--- src/test/compile-fail/issue-39122.rs
+++ src/test/compile-fail/issue-39122.rs
@@ -1,4 +1,4 @@
-// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,7 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-trait Tr {}
-type Huh<T> where T: Tr = isize; //~  ERROR type parameter `T` is unused
-                                 //~| WARNING E0122
-fn main() {}
+type Foo<T: std::ops::Add> = T; //~ WARNING E0122
+
+type Bar<T> where T: std::ops::Add = T; //~ WARNING E0122
