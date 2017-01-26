662fef63603a9600ff2e4a712373dea220d2e517
Gaëtan Cassiers <gaetan.cassiers@gmail.com>
2017-01-26 21:15:58 +0100

    Move platform-specific implementation of libstd/f{32,64}.rs
    This removes exceptions in the tidy lint 'pal'.

---
+++ src/libstd/sys/redox/f32.rs
@@ -0,0 +1,78 @@
+// Copyright 2012-2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![allow(dead_code)]
+
+use intrinsics;
+
+pub mod cmath {
+    use libc::{c_float, c_int};
+
+    #[link_name = "m"]
+    extern {
+        pub fn lgammaf_r(n: c_float, sign: &mut c_int) -> c_float;
+        pub fn hypotf(x: c_float, y: c_float) -> c_float;
+        pub fn acosf(n: c_float) -> c_float;
+        pub fn asinf(n: c_float) -> c_float;
+        pub fn atan2f(a: c_float, b: c_float) -> c_float;
+        pub fn atanf(n: c_float) -> c_float;
+        pub fn coshf(n: c_float) -> c_float;
+        pub fn frexpf(n: c_float, value: &mut c_int) -> c_float;
+        pub fn ldexpf(x: c_float, n: c_int) -> c_float;
+        pub fn sinhf(n: c_float) -> c_float;
+        pub fn tanf(n: c_float) -> c_float;
+        pub fn tanhf(n: c_float) -> c_float;
+    }
+}
+
+#[inline]
+pub fn floor(x: f32) -> f32 {
+    unsafe { intrinsics::floorf32(x) }
+}
+
+#[inline]
+pub fn ceil(x: f32) -> f32 {
+    unsafe { intrinsics::ceilf32(x) }
+}
+
+#[inline]
+pub fn powf(x: f32, n: f32) -> f32 {
+    unsafe { intrinsics::powf32(x, n) }
+}
+
+#[inline]
+pub fn exp(x: f32) -> f32 {
+    unsafe { intrinsics::expf32(x) }
+}
+
+#[inline]
+pub fn ln(x: f32) -> f32 {
+    unsafe { intrinsics::logf32(x) }
+}
+
+#[inline]
+pub fn log2(x: f32) -> f32 {
+    unsafe { intrinsics::log2f32(x) }
+}
+
+#[inline]
+pub fn log10(x: f32) -> f32 {
+    unsafe { intrinsics::log10f32(x) }
+}
+
+#[inline]
+pub fn sin(x: f32) -> f32 {
+    unsafe { intrinsics::sinf32(x) }
+}
+
+#[inline]
+pub fn cos(x: f32) -> f32 {
+    unsafe { intrinsics::cosf32(x) }
+}
