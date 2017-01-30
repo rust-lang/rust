93e3f634b096129227c3863b76a1f303716122f4
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-01-30 23:18:29 +0300

    Fix debuginfo scope issue with `Box`

--- src/librustc_trans/debuginfo/mod.rs
+++ src/librustc_trans/debuginfo/mod.rs
@@ -400,7 +400,7 @@ pub fn create_function_debug_context<'a, 'tcx>(cx: &CrateContext<'a, 'tcx>,
                 // Only "class" methods are generally understood by LLVM,
                 // so avoid methods on other types (e.g. `<*mut T>::null`).
                 match impl_self_ty.sty {
-                    ty::TyAdt(..) => {
+                    ty::TyAdt(def, ..) if !def.is_box() => {
                         Some(type_metadata(cx, impl_self_ty, syntax_pos::DUMMY_SP))
                     }
                     _ => None
