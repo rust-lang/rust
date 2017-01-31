a3d83e5c8f9bfd504e5c6a6bbc6df8d2f451eee1
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-01-31 10:51:52 +0100

    can't call `assert_eq` on packed struct fields since that takes references

--- tests/run-pass/packed_struct.rs
+++ tests/run-pass/packed_struct.rs
@@ -9,6 +9,11 @@ fn main() {
         a: 42,
         b: 99,
     };
-    assert_eq!(x.a, 42);
-    assert_eq!(x.b, 99);
+    let a = x.a;
+    let b = x.b;
+    assert_eq!(a, 42);
+    assert_eq!(b, 99);
+    // can't do `assert_eq!(x.a, 42)`, because `assert_eq!` takes a reference
+    assert_eq!({x.a}, 42);
+    assert_eq!({x.b}, 99);
 }
