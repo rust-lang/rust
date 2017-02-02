c061464f20b25684751948bae2c4f8d6df1de0cb
scott-linder <scott.b.linder@wmich.edu>
2017-02-02 10:55:27 -0500

    Add more exhaustive tests for borrow_box

--- tests/compile-fail/borrow_box.rs
+++ tests/compile-fail/borrow_box.rs
@@ -4,11 +4,22 @@
 #![deny(clippy)]
 #![allow(boxed_local)]
 #![allow(blacklisted_name)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
-pub fn test(foo: &Box<bool>) { //~ ERROR you seem to be trying to use `&Box<T>`
+pub fn test1(foo: &Box<bool>) { //~ ERROR you seem to be trying to use `&Box<T>`
     println!("{:?}", foo)
 }
 
+pub fn test2() {
+    let foo: &Box<bool>; //~ ERROR you seem to be trying to use `&Box<T>`
+}
+
+struct Test3<'a> {
+    foo: &'a Box<bool> //~ ERROR you seem to be trying to use `&Box<T>`
+}
+
 fn main(){
-    test(&Box::new(false));
+    test1(&Box::new(false));
+    test2();
 }
