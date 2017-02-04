bc400e406774266d7df8bc9d3cc9e47dade1140c
Vadim Chugunov <vadimcn@gmail.com>
2017-02-03 18:25:20 -0800

    Don't link "nobundle" libs which had  already been included in upstream crate.

--- src/test/run-make/static-nobundle/aaa.c
+++ src/test/run-make/static-nobundle/aaa.c
@@ -1,4 +1,4 @@
-// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,9 +8,4 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-extern crate bar;
-
-fn main() {
-    unsafe { bar::func(); }
-    bar::wrapped_func();
-}
+void native_func() {}
