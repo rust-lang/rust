2d91e7aab8e91410d504ebfa48d16fca36b04614
Josh Driver <keeperofdakeys@gmail.com>
2017-02-06 22:14:38 +1030

    Refactor macro resolution errors + add derive macro suggestions

--- src/test/compile-fail/issue-11692-2.rs
+++ src/test/compile-fail/issue-11692-2.rs
@@ -8,20 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// Test macro_undefined issue
-
-mod m {
-    #[macro_export]
-    macro_rules! kl {
-        () => ()
-    }
-}
-
 fn main() {
-    k!();
-    //~^ ERROR cannot find macro `k!` in this scope
-    //~^^ HELP did you mean `kl!`?
-    kl!();
-    //~^ ERROR cannot find macro `kl!` in this scope
-    //~^^ HELP have you added the `#[macro_use]` on the module/import?
+    concat!(test!());
+    //~^ ERROR cannot find macro `test!` in this scope
 }
