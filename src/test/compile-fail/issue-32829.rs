rust-lang/rust

commit f6a243d231910a42822d4a287f6183698474022a

Add regression test for issue #32829
Closes #32829

src/test/compile-fail/issue-32829.rs
@@ -1,0 +1,17 @@
+// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// error-pattern: calls in statics are limited
+
+static S : u64 = { { panic!("foo"); 0 } };
+
+fn main() {
+    println!("{:?}", S);
+}

commit 102fc6ad38dd903169188752cb564a2722327f40

unify test files for issue #32829
issue-32829.rs was introduced in f6a243d2 (2 June 2016), but
issue32829.rs was introduced in 94a05526 (9 May 2016). We don't really
need both files; all the functionality is being exercised here.

src/test/compile-fail/issue-32829.rs
@@ -8,10 +8,81 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern: calls in statics are limited
+#![feature(const_fn)]
 
-static S : u64 = { { panic!("foo"); 0 } };
+const bad: u32 = {
+    {
+        5; //~ ERROR: blocks in constants are limited to items and tail expressions
+        0
+    }
+};
 
-fn main() {
-    println!("{:?}", S);
-}
+const bad_two: u32 = {
+    {
+        invalid();
+        //~^ ERROR: blocks in constants are limited to items and tail expressions
+        //~^^ ERROR: calls in constants are limited to constant functions, struct and enum
+        0
+    }
+};
+
+const bad_three: u32 = {
+    {
+        valid();
+        //~^ ERROR: blocks in constants are limited to items and tail expressions
+        0
+    }
+};
+
+static bad_four: u32 = {
+    {
+        5; //~ ERROR: blocks in statics are limited to items and tail expressions
+        0
+    }
+};
+
+static bad_five: u32 = {
+    {
+        invalid();
+        //~^ ERROR: blocks in statics are limited to items and tail expressions
+        //~^^ ERROR: calls in statics are limited to constant functions, struct and enum
+        0
+    }
+};
+
+static bad_six: u32 = {
+    {
+        valid();
+        //~^ ERROR: blocks in statics are limited to items and tail expressions
+        0
+    }
+};
+
+static mut bad_seven: u32 = {
+    {
+        5; //~ ERROR: blocks in statics are limited to items and tail expressions
+        0
+    }
+};
+
+static mut bad_eight: u32 = {
+    {
+        invalid();
+        //~^ ERROR: blocks in statics are limited to items and tail expressions
+        //~^^ ERROR: calls in statics are limited to constant functions, struct and enum
+        0
+    }
+};
+
+static mut bad_nine: u32 = {
+    {
+        valid();
+        //~^ ERROR: blocks in statics are limited to items and tail expressions
+        0
+    }
+};
+
+fn invalid() {}
+const fn valid() {}
+
+fn main() {}

src/test/compile-fail/issue32829.rs
@@ -1,88 +1,0 @@
-// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-#![feature(const_fn)]
-
-const bad : u32 = {
-    {
-        5; //~ ERROR: blocks in constants are limited to items and tail expressions
-        0
-    }
-};
-
-const bad_two : u32 = {
-    {
-        invalid();
-        //~^ ERROR: blocks in constants are limited to items and tail expressions
-        //~^^ ERROR: calls in constants are limited to constant functions, struct and enum
-        0
-    }
-};
-
-const bad_three : u32 = {
-    {
-        valid();
-        //~^ ERROR: blocks in constants are limited to items and tail expressions
-        0
-    }
-};
-
-static bad_four : u32 = {
-    {
-        5; //~ ERROR: blocks in statics are limited to items and tail expressions
-        0
-    }
-};
-
-static bad_five : u32 = {
-    {
-        invalid();
-        //~^ ERROR: blocks in statics are limited to items and tail expressions
-        //~^^ ERROR: calls in statics are limited to constant functions, struct and enum
-        0
-    }
-};
-
-static bad_six : u32 = {
-    {
-        valid();
-        //~^ ERROR: blocks in statics are limited to items and tail expressions
-        0
-    }
-};
-
-static mut bad_seven : u32 = {
-    {
-        5; //~ ERROR: blocks in statics are limited to items and tail expressions
-        0
-    }
-};
-
-static mut bad_eight : u32 = {
-    {
-        invalid();
-        //~^ ERROR: blocks in statics are limited to items and tail expressions
-        //~^^ ERROR: calls in statics are limited to constant functions, struct and enum
-        0
-    }
-};
-
-static mut bad_nine : u32 = {
-    {
-        valid();
-        //~^ ERROR: blocks in statics are limited to items and tail expressions
-        0
-    }
-};
-
-
-fn invalid() {}
-const fn valid() {}
-
-fn main() {}