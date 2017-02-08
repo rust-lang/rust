e23fc79d257095b1347dab58e369d55063857f15
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-02-08 15:38:48 +0100

    silence some style warning

--- tests/run-pass/issue-5917.rs
+++ tests/run-pass/issue-5917.rs
@@ -10,8 +10,8 @@
 
 
 struct T (&'static [isize]);
-static t : T = T (&[5, 4, 3]);
+static STATIC : T = T (&[5, 4, 3]);
 pub fn main () {
-    let T(ref v) = t;
+    let T(ref v) = STATIC;
     assert_eq!(v[0], 5);
 }
