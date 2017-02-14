4f32eaa14cf8f06952cb5900483a907b5d66c6b6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-02-14 14:33:57 +0100

    Improve invalid call on non-function error message

--- src/test/compile-fail/empty-struct-unit-expr.rs
+++ src/test/compile-fail/empty-struct-unit-expr.rs
@@ -24,10 +24,10 @@ enum E {
 fn main() {
     let e2 = Empty2(); //~ ERROR expected function, found `Empty2`
     let e4 = E::Empty4();
-    //~^ ERROR `E::Empty4` is being called, but it is not a function
+    //~^ ERROR expected a function, but `E::Empty4` is `E`
     //~| HELP did you mean to write `E::Empty4`?
     let xe2 = XEmpty2(); //~ ERROR expected function, found `empty_struct::XEmpty2`
     let xe4 = XE::XEmpty4();
-    //~^ ERROR `XE::XEmpty4` is being called, but it is not a function
+    //~^ ERROR expected a function, but `XE::XEmpty4` is `empty_struct::XE`
     //~| HELP did you mean to write `XE::XEmpty4`?
 }
