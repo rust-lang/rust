40aaa65734d72b7aabda3cd0925b0119fb6d5a0a
Alex Crichton <alex@alexcrichton.com>
2017-02-15 08:53:18 -0800

    test: Verify all sysroot crates are unstable
    As we continue to add more crates to the compiler and use them to implement
    various features we want to be sure we're not accidentally expanding the API
    surface area of the compiler! To that end this commit adds a new `run-make` test
    which will attempt to `extern crate foo` all crates in the sysroot, verifying
    that they're all unstable.
    
    This commit discovered that the `std_shim` and `test_shim` crates were
    accidentally stable and fixes the situation by deleting those shims. The shims
    are no longer necessary due to changes in Cargo that have happened since they
    were originally incepted.

--- src/bootstrap/doc.rs
+++ src/bootstrap/doc.rs
@@ -152,7 +152,7 @@ pub fn std(build: &Build, stage: u32, target: &str) {
 
     let mut cargo = build.cargo(&compiler, Mode::Libstd, target, "doc");
     cargo.arg("--manifest-path")
-         .arg(build.src.join("src/rustc/std_shim/Cargo.toml"))
+         .arg(build.src.join("src/libstd/Cargo.toml"))
          .arg("--features").arg(build.std_features());
 
     // We don't want to build docs for internal std dependencies unless
@@ -198,7 +198,7 @@ pub fn test(build: &Build, stage: u32, target: &str) {
 
     let mut cargo = build.cargo(&compiler, Mode::Libtest, target, "doc");
     cargo.arg("--manifest-path")
-         .arg(build.src.join("src/rustc/test_shim/Cargo.toml"));
+         .arg(build.src.join("src/libtest/Cargo.toml"));
     build.run(&mut cargo);
     cp_r(&out_dir, &out)
 }
