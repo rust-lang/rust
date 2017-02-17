cf902c44514014ce2a6c1f57742dc3f5ce12c622
Esteban Küber <esteban@kuber.com.ar>
2017-02-17 14:31:59 -0800

    Highlight and simplify mismatched types
    Shorten mismatched types errors by replacing subtypes that are not
    different with `_`, and highlighting only the subtypes that are
    different.
    
    Given a file
    
    ```rust
    struct X<T1, T2> {
        x: T1,
        y: T2,
    }
    
    fn foo() -> X<X<String, String>, String> {
        X { x: X {x: "".to_string(), y: 2}, y: "".to_string()}
    }
    
    fn bar() -> Option<String> {
        "".to_string()
    }
    ```
    
    provide the following output
    
    ```rust
    error[E0308]: mismatched types
      --> file.rs:6:5
       |
     6 |     X { x: X {x: "".to_string(), y: 2}, y: "".to_string()}
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ expected struct `std::string::String`, found {integer}
       |
       = note: expected type `X<X<_, std::string::String>, _>`
                                     ^^^^^^^^^^^^^^^^^^^   // < highlighted
                  found type `X<X<_, {integer}>, _>`
                                     ^^^^^^^^^             // < highlighted
    
    error[E0308]: mismatched types
      --> file.rs:6:5
       |
    10 |     "".to_string()
       |     ^^^^^^^^^^^^^^ expected struct `std::option::Option`, found `std::string::String`
       |
       = note: expected type `Option<std::string::String>`
                              ^^^^^^^                   ^  // < highlighted
                  found type `std::string::String`
    ```

---
+++ src/test/ui/mismatched_types/abridged.rs
@@ -0,0 +1,61 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+enum Bar {
+    Qux,
+    Zar,
+}
+
+struct Foo {
+    bar: usize,
+}
+
+struct X<T1, T2> {
+    x: T1,
+    y: T2,
+}
+
+fn a() -> Foo {
+    Some(Foo { bar: 1 })
+}
+
+fn a2() -> Foo {
+    Ok(Foo { bar: 1})
+}
+
+fn b() -> Option<Foo> {
+    Foo { bar: 1 }
+}
+
+fn c() -> Result<Foo, Bar> {
+    Foo { bar: 1 }
+}
+
+fn d() -> X<X<String, String>, String> {
+    X {
+        x: X {
+            x: "".to_string(),
+            y: 2,
+        },
+        y: 3,
+    }
+}
+
+fn e() -> X<X<String, String>, String> {
+    X {
+        x: X {
+            x: "".to_string(),
+            y: 2,
+        },
+        y: "".to_string(),
+    }
+}
+
+fn main() {}
