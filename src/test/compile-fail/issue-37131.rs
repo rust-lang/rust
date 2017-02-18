9322a38534c9ad4e6c6a048580ffd68e842d1077
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-02-19 02:28:38 +0300

    Use ARM instead of SystemZ for testing not installed targets

--- src/test/compile-fail/issue-37131.rs
+++ src/test/compile-fail/issue-37131.rs
@@ -11,8 +11,8 @@
 // Tests that compiling for a target which is not installed will result in a helpful
 // error message.
 
-// compile-flags: --target=s390x-unknown-linux-gnu
-// ignore s390x
+// compile-flags: --target=thumbv6m-none-eabi
+// ignore-arm
 
 // error-pattern:target may not be installed
 fn main() { }
