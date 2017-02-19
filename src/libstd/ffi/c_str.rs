ca9ef409834e1e18c7f1bd12009359ba2e6cfb14
lukaramu <lukaramu@gmail.com>
2017-02-19 15:36:28 +0100

    added Error and Display impl for std::ffi::FromBytesWithNulError

--- src/libstd/ffi/c_str.rs
+++ src/libstd/ffi/c_str.rs
@@ -455,6 +455,20 @@ impl From<NulError> for io::Error {
     }
 }
 
+#[unstable(feature = "frombyteswithnulerror_impls", reason = "recently added", issue = "39925")]
+impl Error for FromBytesWithNulError {
+    fn description(&self) -> &str {
+        "data provided is not null terminated or contains an interior nul byte"
+    }
+}
+
+#[unstable(feature = "frombyteswithnulerror_impls", reason = "recently added", issue = "39925")]
+impl fmt::Display for FromBytesWithNulError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.description().fmt(f)
+    }
+}
+
 impl IntoStringError {
     /// Consumes this error, returning original `CString` which generated the
     /// error.
