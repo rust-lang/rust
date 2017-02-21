89d98a8732f53ef8b319682bc6c665c72838874e
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-02-21 11:18:05 +0100

    Simplify calling `update-all-references.sh`

--- tests/ui-posix/update-all-references.sh
+++ tests/ui-posix/update-all-references.sh
@@ -18,14 +18,11 @@
 #
 # See all `update-references.sh`, if you just want to update a single test.
 
-if [[ "$1" == "--help" || "$1" == "-h" || "$1" == "" || "$2" != "" ]]; then
-    echo "usage: $0 <build-directory>"
-    echo ""
-    echo "For example:"
-    echo "   $0 ../../../build/x86_64-apple-darwin/test/ui"
+if [[ "$1" == "--help" || "$1" == "-h" || "$1" == "" ]]; then
+    echo "usage: $0"
 fi
 
-BUILD_DIR=$PWD/$1
+BUILD_DIR=$PWD/target/debug/test_build_base
 MY_DIR=$(dirname $0)
 cd $MY_DIR
 find . -name '*.rs' | xargs ./update-references.sh $BUILD_DIR
