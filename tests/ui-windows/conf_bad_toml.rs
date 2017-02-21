ff655a7dbc90c24537dbfeaf4693c54617745bcf
Oliver Schneider <oli-obk@users.noreply.github.com>
2017-02-21 12:12:08 +0100

    Ui windows test paths are wrong

--- tests/ui-windows/conf_bad_toml.rs
+++ tests/ui-windows/conf_bad_toml.rs
@@ -1,6 +1,6 @@
 // error-pattern: error reading Clippy's configuration file
 
 #![feature(plugin)]
-#![plugin(clippy(conf_file="./tests/compile-fail/conf_bad_toml.toml"))]
+#![plugin(clippy(conf_file="./tests/ui-windows/conf_bad_toml.toml"))]
 
 fn main() {}
