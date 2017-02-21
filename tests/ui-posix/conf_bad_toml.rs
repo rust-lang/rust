25e230419c642e925f168e473f6bec7c34bdf252
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-02-21 12:19:55 +0100

    Merge system independent ui-tests back into tests/ui

--- tests/ui-posix/conf_bad_toml.rs
+++
@@ -1,6 +0,0 @@
-// error-pattern: error reading Clippy's configuration file
-
-#![feature(plugin)]
-#![plugin(clippy(conf_file="./tests/ui-posix/conf_bad_toml.toml"))]
-
-fn main() {}
--- tests/ui-windows/conf_bad_type.rs
+++
@@ -1,6 +0,0 @@
-// error-pattern: error reading Clippy's configuration file: `blacklisted-names` is expected to be a `Vec < String >` but is a `integer`
-
-#![feature(plugin)]
-#![plugin(clippy(conf_file="./tests/ui-windows/conf_bad_type.toml"))]
-
-fn main() {}
