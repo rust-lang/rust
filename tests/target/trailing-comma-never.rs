fbb2370d66e35029e6e6545f12831d591b1e0cae
Nick Cameron <ncameron@mozilla.com>
2017-02-24 10:43:53 +1300

    Some tests

---
+++ tests/target/trailing-comma-never.rs
@@ -0,0 +1,31 @@
+// rustfmt-trailing_comma: Never
+
+enum X {
+    A,
+    B
+}
+
+enum Y {
+    A,
+    B
+}
+
+enum TupX {
+    A(u32),
+    B(i32, u16)
+}
+
+enum TupY {
+    A(u32),
+    B(i32, u16)
+}
+
+enum StructX {
+    A { s: u16 },
+    B { u: u32, i: i32 }
+}
+
+enum StructY {
+    A { s: u16 },
+    B { u: u32, i: i32 }
+}
