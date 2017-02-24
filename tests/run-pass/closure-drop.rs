31c81ac3227e8f4aa4a16f88ebf5af6414ee9d0e
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-02-24 10:42:11 +0100

    Merge `move_fn_closure` run-passtest into `closure-drop`

--- tests/run-pass/closure-drop.rs
+++ tests/run-pass/closure-drop.rs
@@ -13,17 +13,12 @@ fn f<T: FnOnce()>(t: T) {
 fn main() {
     let mut ran_drop = false;
     {
-        // FIXME: v is a temporary hack to force the below closure to be a FnOnce-only closure
-        // (with sig fn(self)). Without it, the closure sig would be fn(&self) which requires a
-        // shim to call via FnOnce::call_once, and Miri's current shim doesn't correctly call
-        // destructors.
-        let v = vec![1];
         let x = Foo(&mut ran_drop);
-        let g = move || {
-            let _ = x;
-            drop(v); // Force the closure to be FnOnce-only by using a capture by-value.
-        };
-        f(g);
+        // this closure never by val uses its captures
+        // so it's basically a fn(&self)
+        // the shim used to not drop the `x`
+        let x = move || { let _ = x; };
+        f(x);
     }
     assert!(ran_drop);
 }
