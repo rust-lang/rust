8d33c099c6a7bdd13ba621415f6114e786acf4d6
Arnavion <arnavion@gmail.com>
2017-02-24 01:28:28 -0800

    Add rust-analysis manifest
    Originally in buildbot as https://github.com/rust-lang/rust-buildbot/commit/f92708e72446abf35b5e98c020ffa50b8705014b and https://github.com/rust-lang/rust-buildbot/commit/d7235e8d60da4ef5f37aa9967c6cf79d6bd0cc06

--- src/tools/build-manifest/src/main.rs
+++ src/tools/build-manifest/src/main.rs
@@ -270,6 +270,15 @@ impl Builder {
                 target: "*".to_string(),
             });
 
+            if self.channel == "nightly" {
+                for target in TARGETS {
+                    extensions.push(Component {
+                        pkg: "rust-analysis".to_string(),
+                        target: target.to_string(),
+                    });
+                }
+            }
+
             pkg.target.insert(host.to_string(), Target {
                 available: true,
                 url: Some(self.url("rust", host)),
