57726b8199d4252cea0014c8351c4fcef34835ea
topecongiro <seuchida@gmail.com>
2017-02-28 19:35:04 +0900

    Add compile test for cfg_target_has_atomic

--- src/tools/tidy/src/features.rs
+++ src/tools/tidy/src/features.rs
@@ -168,7 +168,6 @@ pub fn check(path: &Path, bad: &mut bool) {
     // FIXME get this whitelist empty.
     let whitelist = vec![
         "abi_ptx", "simd",
-        "cfg_target_has_atomic",
         "stmt_expr_attributes",
         "cfg_target_thread_local", "unwind_attributes",
     ];
