b5e889791a5ec8cb06224cf07273be8c84192698
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-03-17 00:47:32 +0300

    Refactor parsing of trait object types

--- src/test/compile-fail/trait-object-macro-matcher.rs
+++ src/test/compile-fail/trait-object-macro-matcher.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,9 +8,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-mod foo {
-    type T = ();
-    struct S1(pub(foo) (), pub(T), pub(crate) (), pub(((), T)));
-    struct S2(pub((foo)) ()); //~ ERROR expected `,`, found `(`
-                              //~| ERROR expected one of `;` or `where`, found `(`
+// `ty` matcher accepts trait object types
+
+macro_rules! m {
+    ($t: ty) => ( let _: $t; )
+}
+
+fn main() {
+    m!(Copy + Send + 'static); //~ ERROR the trait `std::marker::Copy` cannot be made into an object
 }
--- src/test/parse-fail/bounds-obj-parens.rs
+++ src/test/parse-fail/bounds-obj-parens.rs
@@ -10,6 +10,6 @@
 
 // compile-flags: -Z parse-only
 
-type A = Box<(Fn(D::Error) -> E) + 'static + Send + Sync>; // OK
+type A = Box<(Fn(D::Error) -> E) + 'static + Send + Sync>; // OK (but see #39318)
 
 FAIL //~ ERROR
