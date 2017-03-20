c291f3e508b63154028fd9321d6f28b394cf3ad5
Michael Woerister <michaelwoerister@posteo.net>
2017-03-20 18:09:59 +0100

    Move some constants to rustc::ich.

--- src/librustc/ich/mod.rs
+++ src/librustc/ich/mod.rs
@@ -15,3 +15,20 @@ pub use self::caching_codemap_view::CachingCodemapView;
 mod fingerprint;
 mod def_path_hash;
 mod caching_codemap_view;
+
+pub const ATTR_DIRTY: &'static str = "rustc_dirty";
+pub const ATTR_CLEAN: &'static str = "rustc_clean";
+pub const ATTR_DIRTY_METADATA: &'static str = "rustc_metadata_dirty";
+pub const ATTR_CLEAN_METADATA: &'static str = "rustc_metadata_clean";
+pub const ATTR_IF_THIS_CHANGED: &'static str = "rustc_if_this_changed";
+pub const ATTR_THEN_THIS_WOULD_NEED: &'static str = "rustc_then_this_would_need";
+
+pub const IGNORED_ATTRIBUTES: &'static [&'static str] = &[
+    "cfg",
+    ATTR_IF_THIS_CHANGED,
+    ATTR_THEN_THIS_WOULD_NEED,
+    ATTR_DIRTY,
+    ATTR_CLEAN,
+    ATTR_DIRTY_METADATA,
+    ATTR_CLEAN_METADATA
+];
