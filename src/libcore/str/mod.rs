64cd0bebab1a9023dc5a4bbc38f9e6820629fbb9
Donnie Bishop <donnie.a.bishop@gmail.com>
2017-03-25 12:22:23 -0400

    Remove trailing whitespace

--- src/libcore/str/mod.rs
+++ src/libcore/str/mod.rs
@@ -43,28 +43,28 @@ pub mod pattern;
 /// ```
 /// use std::str::FromStr;
 /// use std::num::ParseIntError;
-/// 
+///
 /// #[derive(Debug, PartialEq)]
 /// struct Point {
 ///     x: i32,
 ///     y: i32
 /// }
-/// 
+///
 /// impl FromStr for Point {
 ///     type Err = ParseIntError;
-/// 
+///
 ///     fn from_str(s: &str) -> Result<Self, Self::Err> {
 ///         let coords: Vec<&str> = s.trim_matches(|p| p == '(' || p == ')' )
 ///                                  .split(",")
 ///                                  .collect();
-/// 
+///
 ///         let x_fromstr = try!(coords[0].parse::<i32>());
 ///         let y_fromstr = try!(coords[1].parse::<i32>());
-/// 
+///
 ///         Ok(Point { x: x_fromstr, y: y_fromstr })
 ///     }
 /// }
-/// 
+///
 /// let p = Point::from_str("(1,2)");
 /// assert_eq!(p.unwrap(), Point{ x: 1, y: 2} )
 /// ```
