805704c3469853b923c61744eed64a79c7879170
Niko Matsakis <niko@alum.mit.edu>
2017-03-30 08:54:28 -0400

    update tests slightly

--- src/test/run-pass/overlap-permitted-for-marker-traits-neg.rs
+++ src/test/run-pass/overlap-permitted-for-marker-traits-neg.rs
@@ -8,29 +8,13 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// Tests for RFC 1268: we allow overlapping impls of marker traits,
-// that is, traits without items. In this case, a type `T` is
-// `MyMarker` if it is either `Debug` or `Display`.
-
 #![feature(overlapping_marker_traits)]
 #![feature(optin_builtin_traits)]
 
-use std::fmt::{Debug, Display};
-
-trait MyMarker {}
-
-impl<T: Debug> MyMarker for T {}
-impl<T: Display> MyMarker for T {}
-
-fn foo<T: MyMarker>(t: T) -> T {
-    t
-}
+// Overlapping negative impls for `MyStruct` are permitted:
+struct MyStruct;
+impl !Send for MyStruct {}
+impl !Send for MyStruct {}
 
 fn main() {
-    // Debug && Display:
-    assert_eq!(1, foo(1));
-    assert_eq!(2.0, foo(2.0));
-
-    // Debug && !Display:
-    assert_eq!(vec![1], foo(vec![1]));
 }
