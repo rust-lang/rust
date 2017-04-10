e192fb3d892f04e90a78378bd772cc003304a887
Jorge Aparicio <japaricious@gmail.com>
2017-04-10 09:55:52 -0500

    explain why we have a fake cfail test

--- src/test/compile-fail/feature-gate-linker-flavor.rs
+++ src/test/compile-fail/feature-gate-linker-flavor.rs
@@ -8,6 +8,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// This is a fake compile fail test as there's no way to generate a
+// `#![feature(linker_flavor)]` error. The only reason we have a `linker_flavor`
+// feature gate is to be able to document `-Z linker-flavor` in the unstable
+// book
+
 #[used]
 fn foo() {}
 //~^^ ERROR the `#[used]` attribute is an experimental feature
