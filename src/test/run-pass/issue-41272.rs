65b04fa06869cecfbb8d76a8c8a2456aa1f3686e
Suchith J N <suchithjn22@gmail.com>
2017-04-15 17:21:53 +0530

    Fixed aesthetics and test

--- src/test/run-pass/issue-41272.rs
+++ src/test/run-pass/issue-41272.rs
@@ -14,10 +14,16 @@ impl Foo {
     fn bar(&mut self) -> bool { true }
 }
 
-/* This causes E0301. By fixing issue #41272 this problem should vanish */
-fn iflet_issue(foo: &mut Foo) {
+fn error(foo: &mut Foo) {
     if let Some(_) = Some(true) {
     } else if foo.bar() {}
+}
+
+fn ok(foo: &mut Foo) {
+    if let Some(_) = Some(true) {
+    } else {
+        if foo.bar() {}
+    }
 }
 
 fn main() {}
