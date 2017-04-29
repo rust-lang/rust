23522f6cabf9fc5803411c3dec4a90c56cc3dab2
Ralf Jung <post@ralfj.de>
2017-04-29 19:15:59 +0200

    need to pick a new feature name

--- src/libstd/sync/mutex.rs
+++ src/libstd/sync/mutex.rs
@@ -153,7 +153,7 @@ pub struct MutexGuard<'a, T: ?Sized + 'a> {
 
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<'a, T: ?Sized> !Send for MutexGuard<'a, T> { }
-#[stable(feature = "rust1", since = "1.18.0")]
+#[stable(feature = "mutexguard", since = "1.18.0")]
 unsafe impl<'a, T: ?Sized + Sync> Sync for MutexGuard<'a, T> { }
 
 impl<T> Mutex<T> {
