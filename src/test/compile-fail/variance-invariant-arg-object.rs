2490ee5080ef1810fc137c676f4906d65a407dde
Niko Matsakis <niko@alum.mit.edu>
2017-05-02 21:25:15 -0400

    correct various error messages that changed
    The new messages seem universally better. I think these result because
    we recognize that we are in an invariant context more often.

--- src/test/compile-fail/variance-invariant-arg-object.rs
+++ src/test/compile-fail/variance-invariant-arg-object.rs
@@ -18,14 +18,14 @@ fn get_min_from_max<'min, 'max>(v: Box<Get<&'max i32>>)
                                 -> Box<Get<&'min i32>>
     where 'max : 'min
 {
-    v //~ ERROR cannot infer an appropriate lifetime
+    v //~ ERROR mismatched types
 }
 
 fn get_max_from_min<'min, 'max, G>(v: Box<Get<&'min i32>>)
                                    -> Box<Get<&'max i32>>
     where 'max : 'min
 {
-    v //~ ERROR cannot infer an appropriate lifetime
+    v //~ ERROR mismatched types
 }
 
 fn main() { }
