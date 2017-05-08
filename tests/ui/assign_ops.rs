f1434d202348fa0c4aec5c2dba276fe85df943c0
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-08 12:31:48 +0100

    clean tests/ui/assign_ops.rs
    When all //~ were removed from tests, these were replaced with empty
    lines to leave the numbers in the expected stderr untouched.
    
    Cleaning the empty lines for clarity.

--- tests/ui/assign_ops.rs
+++ tests/ui/assign_ops.rs
@@ -6,46 +6,18 @@
 fn main() {
     let mut i = 1i32;
     i += 2;
-
-
     i += 2 + 17;
-
-
     i -= 6;
-
-
     i -= 2 - 1;
-
-
-
     i *= 5;
-
-
     i *= 1+5;
-
-
     i /= 32;
-
-
     i /= 32 | 5;
-
-
     i /= 32 / 5;
-
-
     i %= 42;
-
-
     i >>= i;
-
-
     i <<= 9 + 6 - 7;
-
-
     i += 1 << 5;
-
-
-
 }
 
 #[allow(dead_code, unused_assignments)]
@@ -53,29 +25,13 @@ fn main() {
 fn bla() {
     let mut a = 5;
     a = a + 1;
-
-
     a = 1 + a;
-
-
     a = a - 1;
-
-
     a = a * 99;
-
-
     a = 42 * a;
-
-
     a = a / 2;
-
-
     a = a % 5;
-
-
     a = a & 1;
-
-
     a = 1 - a;
     a = 5 / a;
     a = 42 % a;
