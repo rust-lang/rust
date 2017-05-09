e7e7231fdaa639edebd9e8f76580ae44379dcf66
Felix Raimundo <felix.raimundo@tweag.io>
2017-05-09 19:02:43 +0200

    Fix  link

--- src/libstd/thread/mod.rs
+++ src/libstd/thread/mod.rs
@@ -594,21 +594,21 @@ pub fn park() {
     *guard = false;
 }
 
-/// Use [park_timeout].
+/// Use [`park_timeout`].
 ///
 /// Blocks unless or until the current thread's token is made available or
 /// the specified duration has been reached (may wake spuriously).
 ///
-/// The semantics of this function are equivalent to [`park`][park] except
+/// The semantics of this function are equivalent to [`park`] except
 /// that the thread will be blocked for roughly no longer than `dur`. This
 /// method should not be used for precise timing due to anomalies such as
 /// preemption or platform differences that may not cause the maximum
 /// amount of time waited to be precisely `ms` long.
 ///
-/// See the [park documentation][park] for more detail.
+/// See the [park documentation][`park`] for more detail.
 ///
-/// [park_timeout]: fn.park_timeout.html
-/// [park]: ../../std/thread/fn.park.html
+/// [`park_timeout`]: fn.park_timeout.html
+/// [`park`]: ../../std/thread/fn.park.html
 #[stable(feature = "rust1", since = "1.0.0")]
 #[rustc_deprecated(since = "1.6.0", reason = "replaced by `std::thread::park_timeout`")]
 pub fn park_timeout_ms(ms: u32) {
