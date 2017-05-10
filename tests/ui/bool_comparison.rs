5865c2aacd73041a6a6d73c05483c24e57719769
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-10 15:48:58 +0100

    clean tests/ui/bool_comparison.rs
    Cleaning the empty lines for clarity.

--- tests/ui/bool_comparison.rs
+++ tests/ui/bool_comparison.rs
@@ -5,19 +5,7 @@
 fn main() {
     let x = true;
     if x == true { "yes" } else { "no" };
-
-
-
     if x == false { "yes" } else { "no" };
-
-
-
     if true == x { "yes" } else { "no" };
-
-
-
     if false == x { "yes" } else { "no" };
-
-
-
 }
