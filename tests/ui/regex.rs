bf6d4b7129485715639a29bdf65f800c66d090bc
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-11 15:41:40 +0100

    clean tests/ui/regex.rs
    Cleaning the empty lines for clarity.

--- tests/ui/regex.rs
+++ tests/ui/regex.rs
@@ -14,23 +14,15 @@ const NOT_A_REAL_REGEX : &'static str = "foobar";
 
 fn syntax_error() {
     let pipe_in_wrong_position = Regex::new("|");
-
     let pipe_in_wrong_position_builder = RegexBuilder::new("|");
-
     let wrong_char_ranice = Regex::new("[z-a]");
-
     let some_unicode = Regex::new("[é-è]");
-
 
     let some_regex = Regex::new(OPENING_PAREN);
 
-
     let binary_pipe_in_wrong_position = BRegex::new("|");
-
     let some_binary_regex = BRegex::new(OPENING_PAREN);
-
     let some_binary_regex_builder = BRegexBuilder::new(OPENING_PAREN);
-
 
     let closing_paren = ")";
     let not_linted = Regex::new(closing_paren);
@@ -46,12 +38,10 @@ fn syntax_error() {
 
     let set_error = RegexSet::new(&[
         OPENING_PAREN,
-
         r"[a-z]+\.(com|org|net)",
     ]);
     let bset_error = BRegexSet::new(&[
         OPENING_PAREN,
-
         r"[a-z]+\.(com|org|net)",
     ]);
 }
@@ -59,48 +49,26 @@ fn syntax_error() {
 fn trivial_regex() {
     let trivial_eq = Regex::new("^foobar$");
 
-
-
     let trivial_eq_builder = RegexBuilder::new("^foobar$");
-
-
 
     let trivial_starts_with = Regex::new("^foobar");
 
-
-
     let trivial_ends_with = Regex::new("foobar$");
-
-
 
     let trivial_contains = Regex::new("foobar");
 
-
-
     let trivial_contains = Regex::new(NOT_A_REAL_REGEX);
 
-
-
     let trivial_backslash = Regex::new("a\\.b");
-
-
 
     // unlikely corner cases
     let trivial_empty = Regex::new("");
 
-
-
     let trivial_empty = Regex::new("^");
-
-
 
     let trivial_empty = Regex::new("^$");
 
-
-
     let binary_trivial_empty = BRegex::new("^$");
-
-
 
     // non-trivial regexes
     let non_trivial_dot = Regex::new("a.b");
