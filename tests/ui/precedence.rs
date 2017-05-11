ec343e1524bc2eb7e3c022e53177e40c96d2e18f
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-11 15:32:42 +0100

    clean tests/ui/precedence.rs
    Cleaning the empty lines for clarity.

--- tests/ui/precedence.rs
+++ tests/ui/precedence.rs
@@ -6,33 +6,14 @@
 #[allow(eq_op)]
 fn main() {
     1 << 2 + 3;
-
-
     1 + 2 << 3;
-
-
     4 >> 1 + 1;
-
-
     1 + 3 >> 2;
-
-
     1 ^ 1 - 1;
-
-
     3 | 2 - 1;
-
-
     3 & 5 - 2;
-
-
-
     -1i32.abs();
-
-
     -1f32.abs();
-
-
 
     // These should not trigger an error
     let _ = (-1i32).abs();
