2389f9e94c4ac493fef3701ac197a61e091c1db0
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-11 12:29:21 +0100

    clean tests/ui/functions.rs
    Cleaning the empty lines for clarity.

--- tests/ui/functions.rs
+++ tests/ui/functions.rs
@@ -9,7 +9,6 @@
 fn good(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool) {}
 
 fn bad(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool, _eight: ()) {
-
 }
 
 // don't lint extern fns
@@ -19,7 +18,6 @@ pub trait Foo {
     fn good(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool);
     fn bad(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool, _eight: ());
 
-
     fn ptr(p: *const u8);
 }
 
@@ -28,7 +26,6 @@ pub struct Bar;
 impl Bar {
     fn good_method(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool) {}
     fn bad_method(_one: u32, _two: u32, _three: &str, _four: bool, _five: f32, _six: f32, _seven: bool, _eight: ()) {}
-
 }
 
 // ok, we don’t want to warn implementations
@@ -38,11 +35,8 @@ impl Foo for Bar {
 
     fn ptr(p: *const u8) {
         println!("{}", unsafe { *p });
-
         println!("{:?}", unsafe { p.as_ref() });
-
         unsafe { std::ptr::read(p) };
-
     }
 }
 
@@ -54,11 +48,8 @@ fn private(p: *const u8) {
 
 pub fn public(p: *const u8) {
     println!("{}", unsafe { *p });
-
     println!("{:?}", unsafe { p.as_ref() });
-
     unsafe { std::ptr::read(p) };
-
 }
 
 impl Bar {
@@ -68,11 +59,8 @@ impl Bar {
 
     pub fn public(self, p: *const u8) {
         println!("{}", unsafe { *p });
-
         println!("{:?}", unsafe { p.as_ref() });
-
         unsafe { std::ptr::read(p) };
-
     }
 
     pub fn public_ok(self, p: *const u8) {
