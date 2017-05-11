6f3679b8e4b1e9ae34850e15d43a2b9846703e3a
Luis de Bethencourt <luisbg@osg.samsung.com>
2017-05-11 16:16:19 +0100

    clean tests/ui/zero_div_zero.rs
    Cleaning the empty lines for clarity.

--- tests/ui/zero_div_zero.rs
+++ tests/ui/zero_div_zero.rs
@@ -5,13 +5,9 @@
 #[deny(zero_divided_by_zero)]
 fn main() {
     let nan = 0.0 / 0.0;
-
     let f64_nan = 0.0 / 0.0f64;
-
     let other_f64_nan = 0.0f64 / 0.0;
-
     let one_more_f64_nan = 0.0f64/0.0f64;
-
     let zero = 0.0;
     let other_zero = 0.0;
     let other_nan = zero / other_zero; // fine - this lint doesn't propegate constants.
