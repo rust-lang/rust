7e623796b05966034a8c446c2224ddfe437aa537
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-05-18 00:22:52 +0300

    Stabilize unions with `Copy` fields and no destructor

--- src/test/compile-fail/union/union-empty.rs
+++ src/test/compile-fail/union/union-empty.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(untagged_unions)]
-
 union U {} //~ ERROR unions cannot have zero fields
 
 fn main() {}
