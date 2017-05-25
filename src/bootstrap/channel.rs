696af75e634c5b8a4ec79f1bb89dd9153cf88334
Brian Anderson <banderson@mozilla.com>
2017-05-25 17:16:20 +0000

    Bump prerelease version

--- src/bootstrap/channel.rs
+++ src/bootstrap/channel.rs
@@ -28,7 +28,7 @@ pub const CFG_RELEASE_NUM: &'static str = "1.18.0";
 // An optional number to put after the label, e.g. '.2' -> '-beta.2'
 // Be sure to make this starts with a dot to conform to semver pre-release
 // versions (section 9)
-pub const CFG_PRERELEASE_VERSION: &'static str = ".3";
+pub const CFG_PRERELEASE_VERSION: &'static str = ".4";
 
 pub struct GitInfo {
     inner: Option<Info>,
