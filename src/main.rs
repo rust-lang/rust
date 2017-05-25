f887d55a0da741bc34d80eeeeab007f535681a4c
Inokentiy Babushkin <twk@twki.de>
2017-05-25 20:06:58 +0200

    Initial (boring) version of compilation driver

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+#![feature(box_syntax)]
+#![feature(rustc_private)]
+
+extern crate getopts;
+extern crate rustc;
+extern crate rustc_driver;
+extern crate rustc_errors;
+extern crate syntax;
+
+use rustc::session::{config, Session};
+use rustc::session::config::{Input, ErrorOutputType};
+
+use rustc_driver::{driver, CompilerCalls, RustcDefaultCalls, Compilation};
+
+use std::path::PathBuf;
+
+use syntax::ast;
+
+struct SemVerVerCompilerCalls {
+    default: RustcDefaultCalls,
+}
+
+impl SemVerVerCompilerCalls {
+    pub fn new() -> SemVerVerCompilerCalls {
+        SemVerVerCompilerCalls { default: RustcDefaultCalls }
+    }
+}
+
+impl<'a> CompilerCalls<'a> for SemVerVerCompilerCalls {
+    fn early_callback(&mut self,
+                      matches: &getopts::Matches,
+                      sopts: &config::Options,
+                      cfg: &ast::CrateConfig,
+                      descriptions: &rustc_errors::registry::Registry,
+                      output: ErrorOutputType)
+                      -> Compilation {
+        self.default
+            .early_callback(matches, sopts, cfg, descriptions, output)
+    }
+
+    fn no_input(&mut self,
+                matches: &getopts::Matches,
+                sopts: &config::Options,
+                cfg: &ast::CrateConfig,
+                odir: &Option<PathBuf>,
+                ofile: &Option<PathBuf>,
+                descriptions: &rustc_errors::registry::Registry)
+                -> Option<(Input, Option<PathBuf>)> {
+        self.default
+            .no_input(matches, sopts, cfg, odir, ofile, descriptions)
+    }
+
+    fn late_callback(&mut self,
+                     matches: &getopts::Matches,
+                     sess: &Session,
+                     input: &Input,
+                     odir: &Option<PathBuf>,
+                     ofile: &Option<PathBuf>)
+                     -> Compilation {
+        self.default
+            .late_callback(matches, sess, input, odir, ofile)
+    }
+
+    fn build_controller(&mut self,
+                        sess: &Session,
+                        matches: &getopts::Matches)
+                        -> driver::CompileController<'a> {
+        let mut controller = self.default.build_controller(sess, matches);
+
+        controller
+    }
+}
+
+pub fn main() {}
