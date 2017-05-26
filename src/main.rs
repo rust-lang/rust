fc1f381888df60cfee7bdd09be46581983490d56
Inokentiy Babushkin <twk@twki.de>
2017-05-26 18:19:51 +0200

    Blatantly clippy-esque cargo subcommand infrastructure
    This should be a first step to do the following things:
    * dump the relevant items from the current crate, after building
      all the dependencies
    * download the most recent stable version of the current crate
    * kick off a custom driver performing all the necessary work

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,22 @@ use rustc::session::config::{Input, ErrorOutputType};
 use rustc_driver::{driver, CompilerCalls, RustcDefaultCalls, Compilation};
 
 use std::io::{self, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use syntax::ast;
 
 struct SemVerVerCompilerCalls {
     default: RustcDefaultCalls,
+    enabled: bool
 }
 
 impl SemVerVerCompilerCalls {
-    pub fn new() -> SemVerVerCompilerCalls {
-        SemVerVerCompilerCalls { default: RustcDefaultCalls }
+    pub fn new(enabled: bool) -> SemVerVerCompilerCalls {
+        SemVerVerCompilerCalls {
+            default: RustcDefaultCalls,
+            enabled: enabled,
+        }
     }
 }
 
@@ -69,9 +74,11 @@ impl<'a> CompilerCalls<'a> for SemVerVerCompilerCalls {
                         -> driver::CompileController<'a> {
         let mut controller = self.default.build_controller(sess, matches);
 
-        let old_callback = std::mem::replace(&mut controller.after_hir_lowering.callback,
-                                             box |_| {});
-        controller.after_hir_lowering.callback = box move |state| { old_callback(state); };
+        if self.enabled {
+            let old_callback = std::mem::replace(&mut controller.after_hir_lowering.callback,
+                                                 box |_| {});
+            controller.after_hir_lowering.callback = box move |state| { old_callback(state); };
+        }
 
         controller
     }
@@ -117,6 +124,8 @@ pub fn main() {
            .map(|arg| arg == "semver")
            .unwrap_or(false) {
         // first run (we blatantly copy clippy's code structure here)
+        // we are being run as `cargo semver`
+
         let manifest_path_arg = std::env::args()
             .skip(2)
             .find(|val| val.starts_with("--manifest-path="));
@@ -129,24 +138,134 @@ pub fn main() {
                 .write_fmt(format_args!("error: could not obtain cargo metadata.\n"));
             std::process::exit(1);
         };
-    } else {
-        // second run TODO: elaborate etc
-    }
 
-    rustc_driver::in_rustc_thread(|| {
-        let args: Vec<String> = std::env::args().collect(); // TODO: look at clippy here
+        let manifest_path = manifest_path_arg.map(|arg| PathBuf::from(
+                Path::new(&arg["--manifest-path=".len()..])));
 
-        // let checks_enabled = std::env::args().any(|s| s == "-Zno-trans");
+        let current_dir = std::env::current_dir();
 
-        let mut cc = SemVerVerCompilerCalls::new(); // TODO: use `checks_enabled`
-        // TODO: the second result is a `Session` - maybe we'll need it
-        let (result, _) = rustc_driver::run_compiler(&args, &mut cc, None, None);
+        let package_index = metadata
+            .packages
+            .iter()
+            .position(|package| {
+                let package_manifest_path = Path::new(&package.manifest_path);
+                if let Some(ref path) = manifest_path {
+                    package_manifest_path == path
+                } else {
+                    let current_dir = current_dir
+                        .as_ref()
+                        .expect("could not read current directory");
+                    let package_manifest_directory = package_manifest_path
+                        .parent()
+                        .expect("could not find parent directory of package manifest");
+                    package_manifest_directory == current_dir
+                }
+            })
+            .expect("could not find matching package");
 
-        if let Err(count) = result {
-            if count > 0 {
-                std::process::exit(1);
+        let package = metadata.packages.remove(package_index);
+
+        for target in package.targets {
+            let args = std::env::args().skip(2);
+
+            if let Some(first) = target.kind.get(0) {
+                if target.kind.len() > 1 || first.ends_with("lib") {
+                    if let Err(code) = process(std::iter::once("--lib".to_owned()).chain(args)) {
+                        std::process::exit(code);
+                    }
+                } else if ["bin", "example", "test", "bench"].contains(&&**first) {
+                    if let Err(code) = process(vec![format!("--{}", first), target.name]
+                                                   .into_iter()
+                                                   .chain(args)) {
+                        std::process::exit(code);
+                    }
+                }
+            } else {
+                panic!("badly formatted cargo metadata: target::kind is an empty array");
             }
         }
-    })
-            .expect("rustc thread failed");
+    } else {
+        // second run: we're being run by `cargo rustc` as we set it up to happen
+
+        let home = option_env!("RUSTUP_HOME");
+        let toolchain = option_env!("RUSTUP_TOOLCHAIN");
+        let sys_root = if let (Some(home), Some(toolchain)) = (home, toolchain) {
+            format!("{}/toolchains/{}", home, toolchain)
+        } else {
+            option_env!("SYSROOT")
+                .map(|s| s.to_owned())
+                .or_else(|| {
+                    Command::new("rustc")
+                        .arg("--print")
+                        .arg("sysroot")
+                        .output()
+                        .ok()
+                        .and_then(|out| String::from_utf8(out.stdout).ok())
+                        .map(|s| s.trim().to_owned())
+                })
+                .expect("need to specify SYSROOT env var during compilation, or use rustup")
+        };
+
+        rustc_driver::in_rustc_thread(|| {
+            // make it possible to call `cargo-semver` directly without having to pass
+            // --sysroot or anything
+            let args: Vec<String> = if std::env::args().any(|s| s == "--sysroot") {
+                std::env::args().collect()
+            } else {
+                std::env::args()
+                    .chain(Some("--sysroot".to_owned()))
+                    .chain(Some(sys_root))
+                    .collect()
+            };
+
+            // this check ensures that dependencies are built but not checked and the final
+            // crate is checked but not built
+            let checks_enabled = std::env::args().any(|s| s == "-Zno-trans");
+
+            let mut cc = SemVerVerCompilerCalls::new(checks_enabled);
+            // TODO: the second result is a `Session` - maybe we'll need it
+            let (result, _) = rustc_driver::run_compiler(&args, &mut cc, None, None);
+
+            if let Err(count) = result {
+                if count > 0 {
+                    std::process::exit(1);
+                }
+            }
+        })
+        .expect("rustc thread failed");
+    }
+}
+
+fn process<I>(old_args: I) -> Result<(), i32>
+    where I: Iterator<Item = String>
+{
+    let mut args = vec!["rustc".to_owned()];
+
+    let found_dashes = old_args.fold(false, |mut found, arg| {
+        found |= arg == "--";
+        args.push(arg);
+        found
+    });
+
+    if !found_dashes {
+        args.push("--".to_owned());
+    }
+
+    args.push("-Zno-trans".to_owned());
+
+    let path = std::env::current_exe().expect("current executable path invalid");
+
+    let exit_status = std::process::Command::new("cargo")
+        .args(&args)
+        .env("RUSTC", path)
+        .spawn()
+        .expect("could not run cargo")
+        .wait()
+        .expect("failed to wait for cargo?");
+
+    if exit_status.success() {
+        Ok(())
+    } else {
+        Err(exit_status.code().unwrap_or(-1))
+    }
 }
