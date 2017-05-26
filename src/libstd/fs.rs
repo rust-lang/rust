e66388d21f4de5bb48529bf529f54315a0800fb1
king6cong <king6cong@gmail.com>
2017-05-26 19:21:29 +0800

    doc rewording

--- src/libstd/fs.rs
+++ src/libstd/fs.rs
@@ -1545,7 +1545,7 @@ pub fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
 /// determined to not exist) are outlined by `fs::create_dir`.
 ///
 /// Notable exception is made for situations where any of the directories
-/// specified in the `path` could not be created as it was created concurrently.
+/// specified in the `path` could not be created as it was being created concurrently.
 /// Such cases are considered success. In other words: calling `create_dir_all`
 /// concurrently from multiple threads or processes is guaranteed to not fail
 /// due to race itself.
