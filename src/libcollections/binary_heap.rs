f5421367a2d72d5da415d2bbf97538099ff3ade0
Stjepan Glavina <stjepang@gmail.com>
2017-05-27 17:00:11 +0200

    Docs: impls of PartialEq/PartialOrd/Ord must agree

--- src/libcollections/binary_heap.rs
+++ src/libcollections/binary_heap.rs
@@ -42,8 +42,11 @@
 //! // instead of a max-heap.
 //! impl Ord for State {
 //!     fn cmp(&self, other: &State) -> Ordering {
-//!         // Notice that the we flip the ordering here
+//!         // Notice that the we flip the ordering on costs.
+//!         // In case of a tie we compare positions - this step is necessary
+//!         // to make implementations of `PartialEq` and `Ord` consistent.
 //!         other.cost.cmp(&self.cost)
+//!             .then_with(|| self.position.cmp(&other.position))
 //!     }
 //! }
 //!
