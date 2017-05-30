adec9a83d685fd96f7d180714fb93f5563b08cf0
Inokentiy Babushkin <twk@twki.de>
2017-05-30 15:19:49 +0200

    Added path dumping
    In addition to the other data we already extract from the compiler's
    internal representation, we now construct a map from paths to the
    definition IDs. With custom {Partial,}Eq and Hash instances on a wrapper
    type, it should be possible to perform matching across crate boundaries
    now. Also note that some of the data structures we currently generate
    might very well be superfluous and probably will be removed in the
    future.

--- src/check/export_map.rs
+++ src/check/export_map.rs
@@ -1,9 +1,11 @@
 use rustc::hir::def::Def;
 use rustc::hir::def_id::*;
+use rustc::hir::map::definitions::DefPath;
 use rustc::middle::cstore::CrateStore;
 use rustc::ty::Visibility::Public;
 
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::{Hash, Hasher};
 
 pub type VisitedModSet = HashSet<DefId>;
 
@@ -12,57 +14,93 @@ pub type ItemSet = HashSet<DefId>;
 pub type ModMap = HashMap<DefId, Vec<DefId>>;
 
 #[derive(Debug)]
+pub struct Path {
+    inner: DefPath,
+}
+
+impl Path {
+    pub fn new(inner: DefPath) -> Path {
+        Path {
+            inner: inner,
+        }
+    }
+
+    pub fn get_inner(&self) -> &DefPath {
+        &self.inner
+    }
+}
+
+impl PartialEq for Path {
+    fn eq(&self, other: &Path) -> bool {
+        self.inner.data == other.get_inner().data
+    }
+}
+
+impl Eq for Path {}
+
+impl Hash for Path {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.inner.data.hash(state);
+    }
+}
+
+pub type PathMap = HashMap<Path, DefId>;
+
+#[derive(Debug)]
 pub struct ExportMap {
-    visited_mods: VisitedModSet,
+    visited: VisitedModSet,
     items: ItemSet,
     exports: ModMap,
+    paths: PathMap,
     root: DefId,
 }
 
 impl ExportMap {
     pub fn new(root: DefId, cstore: &CrateStore) -> ExportMap {
-        let (visited, items, exports) = walk_mod(root, cstore);
+        let mut visited = HashSet::new();
+        let mut items = HashSet::new();
+        let mut exports = HashMap::new();
+        let mut paths = HashMap::new();
+
+        let mut mod_queue = VecDeque::new();
+        mod_queue.push_back(root);
+
+        while let Some(mod_id) = mod_queue.pop_front() {
+            let mut children = cstore.item_children(mod_id);
+            let mut current_children = Vec::new();
+
+            for child in
+                children
+                    .drain(..)
+                    .filter(|c| cstore.visibility(c.def.def_id()) == Public) {
+                match child.def {
+                    Def::Mod(submod_id) =>
+                        if !visited.contains(&submod_id) {
+                            visited.insert(submod_id);
+                            current_children.push(submod_id);
+                            mod_queue.push_back(submod_id);
+                        } else {
+                            current_children.push(submod_id);
+                        },
+                    def => {
+                        let def_id = def.def_id();
+                        items.insert(def_id);
+                        paths.insert(Path::new(cstore.def_path(def_id)), def_id);
+                        println!("{}", cstore.def_path(def_id).data.len());
+                        current_children.push(def_id);
+                    },
+                }
+            }
+
+            exports.insert(mod_id, current_children);
+        }
+
         ExportMap {
-            visited_mods: visited,
+            visited: visited,
             items: items,
             exports: exports,
+            paths: paths,
             root: root,
         }
     }
-}
-
-fn walk_mod(root: DefId, cstore: &CrateStore) -> (VisitedModSet, ItemSet, ModMap) {
-    let mut visited = HashSet::new();
-    let mut items = HashSet::new();
-    let mut exports = HashMap::new();
-
-    let mut mod_queue = VecDeque::new();
-    mod_queue.push_back(root);
-
-    while let Some(mod_id) = mod_queue.pop_front() {
-        let mut children = cstore.item_children(mod_id);
-        let mut current_children = Vec::new();
-
-        for child in children.drain(..).filter(|c| cstore.visibility(c.def.def_id()) == Public) {
-            match child.def {
-                Def::Mod(submod_id) =>
-                    if !visited.contains(&submod_id) {
-                        visited.insert(submod_id);
-                        current_children.push(submod_id);
-                        mod_queue.push_back(submod_id);
-                    } else {
-                        current_children.push(submod_id);
-                    },
-                def => {
-                    let def_id = def.def_id();
-                    items.insert(def_id);
-                    current_children.push(def_id);
-                },
-            }
-        }
-
-        exports.insert(mod_id, current_children);
-    }
-
-    (visited, items, exports)
 }
