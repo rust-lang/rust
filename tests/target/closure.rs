2dc1de93075a11fc8f4f5e7bf04fedc088eb06fb
Nick Cameron <ncameron@mozilla.com>
2017-06-01 17:34:21 +1200

    Update a test

--- tests/target/closure.rs
+++ tests/target/closure.rs
@@ -109,10 +109,8 @@ fn foo() {
 
 fn issue1405() {
     open_raw_fd(fd, b'r').and_then(|file| {
-                                       Capture::new_raw(None, |_, err| unsafe {
-            raw::pcap_fopen_offline(file, err)
-        })
-                                   });
+        Capture::new_raw(None, |_, err| unsafe { raw::pcap_fopen_offline(file, err) })
+    });
 }
 
 fn issue1466() {
