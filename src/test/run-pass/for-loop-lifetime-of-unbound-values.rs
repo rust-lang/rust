8d65dd62b17892102231101a7e58de0a20b5dd21
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2017-06-14 19:26:42 +0200

    Fix test formatting

--- src/test/run-pass/for-loop-lifetime-of-unbound-values.rs
+++ src/test/run-pass/for-loop-lifetime-of-unbound-values.rs
@@ -1,11 +1,21 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::cell::Cell;
 
 struct Flag<'a>(&'a Cell<bool>);
 
 impl<'a> Drop for Flag<'a> {
-	fn drop(&mut self) {
-	    self.0.set(false)
-	}
+    fn drop(&mut self) {
+        self.0.set(false)
+    }
 }
 
 fn main() {
@@ -16,7 +26,7 @@ fn main() {
     }
     // The Flag value should be dead outside of the loop
     assert_eq!(alive2.get(), false);
-    
+
     let alive = Cell::new(true);
     for _ in std::iter::once(Flag(&alive)) {
         // The Flag value should be alive in the for loop body even if it wasn't
