d5fd8fef67a0a0b4d30dd8b23052c380632ae0e2
Niko Matsakis <niko@alum.mit.edu>
2017-06-15 12:27:15 -0400

    explain purpose of test

--- src/test/compile-fail/for-loop-unconstrained-element-type.rs
+++ src/test/compile-fail/for-loop-unconstrained-element-type.rs
@@ -8,6 +8,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// Test that `for` loops don't introduce artificial
+// constraints on the type of the binding (`i`).
+// Subtle changes in the desugaring can cause the
+// type of elements in the vector to (incorrectly) 
+// fallback to `!` or `()`.
+
 fn main() {
     for i in Vec::new() { } //~ ERROR type annotations needed
 }
