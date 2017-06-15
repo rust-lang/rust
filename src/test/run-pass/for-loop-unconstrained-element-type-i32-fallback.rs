202263d7dccc12a4bf633b56d9e2ef2d5ff976c0
Niko Matsakis <niko@alum.mit.edu>
2017-06-15 12:31:45 -0400

    Create for-loop-unconstrained-element-type-i32-fallback.rs

--- src/test/run-pass/for-loop-unconstrained-element-type-i32-fallback.rs
+++ src/test/run-pass/for-loop-unconstrained-element-type-i32-fallback.rs
@@ -8,6 +8,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// Test that the type of `sum` falls back to `i32` here,
+// and that the for loop desugaring doesn't inferfere with
+// that.
+
 fn main() {
     let mut sum = 0;
     for i in Vec::new() {
