7ec4d14acdcb18948953e11453e51627679f9955
Fourchaux <jprodi04@gmail.com>
2017-06-22 02:49:26 +0200

    fix wrapping

--- src/libstd/panicking.rs
+++ src/libstd/panicking.rs
@@ -221,7 +221,8 @@ impl<'a> PanicInfo<'a> {
     ///
     /// panic::set_hook(Box::new(|panic_info| {
     ///     if let Some(location) = panic_info.location() {
-    ///        println!("panic occurred in file '{}' at line {}", location.file(), location.line());
+    ///         println!("panic occurred in file '{}' at line {}", 
+    ///                  location.file(), location.line());
     ///     } else {
     ///         println!("panic occurred but can't get location information...");
     ///     }
