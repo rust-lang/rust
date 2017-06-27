3ec4588cf02c4dfc75d0b64bdc619caebee9a97a
Inokentiy Babushkin <twk@twki.de>
2017-06-27 11:23:24 +0200

    Added fn tests.

--- tests/examples/fn.rs
+++ tests/examples/fn.rs
@@ -1,7 +1,21 @@
 pub mod old {
     pub fn abc() {}
+
+    pub fn bcd() {}
+
+    pub fn cde() {}
+
+    pub fn def(_: u8) {}
 }
 
 pub mod new {
     pub fn abc() {}
+
+    pub fn bcd(_: u8) {}
+
+    pub fn cde() -> u16 { // TODO: maybe make this case TechnicallyBreaking
+        0xcde
+    }
+
+    pub fn def() {}
 }
