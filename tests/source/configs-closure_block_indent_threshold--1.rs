0199e08367ab09d0e6194cd0c8f9dbfeab8b53a2
topecongiro <seuchida@gmail.com>
2017-06-28 10:56:29 +0900

    Use correct one line budget in rewrite_closure_block

---
+++ tests/source/configs-closure_block_indent_threshold--1.rs
@@ -0,0 +1,5 @@
+// rustfmt-closure_block_indent_threshold: -1
+
+fn issue1755() {
+    b.iter(|| dfkljdklgjdlkfgjdlkgjldkjfgkdjgldjfgkljdfklgjlkdjfgkljdflkgjlkdfjgdjfsas::expect("parse"));
+}
---
+++ tests/target/configs-closure_block_indent_threshold--1.rs
@@ -0,0 +1,7 @@
+// rustfmt-closure_block_indent_threshold: -1
+
+fn issue1755() {
+    b.iter(|| {
+        dfkljdklgjdlkfgjdlkgjldkjfgkdjgldjfgkljdfklgjlkdjfgkljdflkgjlkdfjgdjfsas::expect("parse")
+    });
+}
