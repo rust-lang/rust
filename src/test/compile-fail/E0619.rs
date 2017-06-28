76d54c09b795fbc94f81bc7806ea2f1268bb2000
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-06-29 00:02:21 +0200

    Add E0619 error explanation

--- src/test/compile-fail/E0619.rs
+++ src/test/compile-fail/E0619.rs
@@ -1,4 +1,4 @@
-// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,11 +8,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-fn main() {
-    let x;
-
-    match x {
-        (..) => {} //~ ERROR E0619
-        _ => {}
-    }
+#![feature(intrinsics)]
+extern "rust-intrinsic" {
+    pub static breakpoint : unsafe extern "rust-intrinsic" fn();
+    //~^ ERROR intrinsic must be a function [E0619]
 }
+fn main() { unsafe { breakpoint(); } }
