b1d93a595c4a9ea648fee189b13cbdea994f9826
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-06-29 13:46:07 +0200

    Add needless_borrowed_ref example.

---
+++ clippy_tests/examples/needless_borrowed_ref.rs
@@ -0,0 +1,9 @@
+#![feature(plugin)]
+#![plugin(clippy)]
+
+#[warn(needless_borrowed_reference)]
+fn main() {
+    let mut v = Vec::<String>::new();
+    let _ = v.iter_mut().filter(|&ref a| a.is_empty());
+}
+
