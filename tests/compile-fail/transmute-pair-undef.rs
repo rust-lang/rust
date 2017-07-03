bdcdb605a49d1e543aac88c1b8ff53274fe1b489
Ralf Jung <post@ralfj.de>
2017-07-03 16:16:05 -0700

    fix test on i686

--- tests/compile-fail/transmute-pair-undef.rs
+++ tests/compile-fail/transmute-pair-undef.rs
@@ -1,17 +1,20 @@
 #![feature(core_intrinsics)]
 
+use std::mem;
+
 fn main() {
     let x: Option<Box<[u8]>> = unsafe {
         let z = std::intrinsics::add_with_overflow(0usize, 0usize);
         std::mem::transmute::<(usize, bool), Option<Box<[u8]>>>(z)
     };
     let y = &x;
-    // Now read this bytewise.  There should be 9 def bytes followed by 7 undef bytes (the padding after the bool) in there.
+    // Now read this bytewise.  There should be (ptr_size+1) def bytes followed by (ptr_size-1) undef bytes (the padding after the bool) in there.
     let z : *const u8 = y as *const _ as *const _;
-    for i in 0..9 {
+    let first_undef = mem::size_of::<usize>() as isize + 1;
+    for i in 0..first_undef {
         let byte = unsafe { *z.offset(i) };
         assert_eq!(byte, 0);
     }
-    let v = unsafe { *z.offset(9) };
+    let v = unsafe { *z.offset(first_undef) };
     if v == 0 {} //~ ERROR attempted to read undefined bytes
 }
