ea730ab20f72196a683f313cbd2797456eb28b3e
Ralf Jung <post@ralfj.de>
2017-07-10 15:58:47 -0700

    update tests for new allocator API

--- tests/compile-fail/deallocate-twice.rs
+++ tests/compile-fail/deallocate-twice.rs
@@ -1,14 +1,17 @@
-#![feature(alloc, heap_api)]
+#![feature(alloc, allocator_api)]
 
 extern crate alloc;
+
+use alloc::heap::Heap;
+use alloc::allocator::*;
 
 // error-pattern: tried to deallocate with a pointer not to the beginning of an existing object
 
 use alloc::heap::*;
 fn main() {
     unsafe {
-        let x = allocate(1, 1);
-        deallocate(x, 1, 1);
-        deallocate(x, 1, 1);
+        let x = Heap.alloc(Layout::from_size_align_unchecked(1, 1)).unwrap();
+        Heap.dealloc(x, Layout::from_size_align_unchecked(1, 1));
+        Heap.dealloc(x, Layout::from_size_align_unchecked(1, 1));
     }
 }
