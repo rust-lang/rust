b9bb367665c080aab0d79bbcb94bbc1e68805233
Inokentiy Babushkin <twk@twki.de>
2017-07-11 12:15:34 +0200

    First round of refactorings on the cargo plugin.

--- src/bin/cargo_semver.rs
+++ src/bin/cargo_semver.rs
@@ -7,13 +7,14 @@ use crates_io::{Crate, Registry};
 
 use cargo::exit_with_error;
 use cargo::core::{Package, PackageId, Source, SourceId, Workspace};
-use cargo::ops::{compile, Compilation, CompileMode, CompileOptions};
+use cargo::ops::{compile, CompileMode, CompileOptions};
 use cargo::sources::registry::RegistrySource;
 use cargo::util::{human, CargoResult, CliError};
 use cargo::util::config::Config;
 use cargo::util::important_paths::find_root_manifest_for_wd;
 
 use std::io::Write;
+use std::path::PathBuf;
 use std::process::{Stdio, Command};
 
 /// Given a crate name, try to locate the corresponding crate on `crates.io`.
@@ -35,15 +36,82 @@ fn exact_search(query: &str) -> CargoResult<Crate> {
         })
 }
 
-/// Compile a crate given it's workspace.
-///
-/// The results returned are then used to locate the build artefacts, which in turn are linked
-/// together for the actual analysis.
-fn generate_rlib<'a>(config: &'a Config,
-                     workspace: &Workspace<'a>)
-                     -> CargoResult<Compilation<'a>> {
-    let opts = CompileOptions::default(config, CompileMode::Build);
-    compile(workspace, &opts).map(|c| c)
+/// A specification of package name and version.
+struct NameAndVersion<'a> {
+    /// The package's name.
+    name: &'a str,
+    /// The package's version, as a semver-string.
+    version: &'a str,
+}
+
+/// A specification of a package source to fetch remote packages from.
+struct SourceInfo<'a> {
+    /// The source id to be used.
+    id: SourceId,
+    /// The registry to be used.
+    registry: RegistrySource<'a>,
+}
+
+impl<'a> SourceInfo<'a> {
+    /// Construct a new source info for `crates.io`.
+    fn new(config: &'a Config) -> CargoResult<SourceInfo<'a>> {
+        let source_id = SourceId::crates_io(config)?;
+        let registry = RegistrySource::remote(&source_id, config);
+        Ok(SourceInfo {
+            id: source_id,
+            registry: registry,
+        })
+    }
+}
+
+/// A specification of a package and it's workspace.
+struct WorkInfo<'a> {
+    /// The package to be compiled.
+    package: Package,
+    /// The package's workspace.
+    workspace: Workspace<'a>,
+}
+
+impl<'a> WorkInfo<'a> {
+    /// Construct a package/workspace pair for the local directory.
+    fn local(config: &'a Config) -> CargoResult<WorkInfo<'a>> {
+        let manifest_path = find_root_manifest_for_wd(None, config.cwd())?;
+
+        Ok(WorkInfo {
+            package: Package::for_path(&manifest_path, config)?,
+            workspace: Workspace::new(&manifest_path, config)?,
+        })
+    }
+
+    /// Construct a package/workspace pair by fetching the package of a specified name and
+    /// version.
+    fn remote(config: &'a Config, source: &mut SourceInfo<'a>, info: NameAndVersion)
+        -> CargoResult<WorkInfo<'a>>
+    {
+        let package_id = PackageId::new(info.name, info.version, &source.id)?;
+        let package = source.registry.download(&package_id)?;
+        let workspace = Workspace::ephemeral(package.clone(), config, None, false)?;
+
+        Ok(WorkInfo {
+            package: package,
+            workspace: workspace,
+        })
+    }
+
+    /// Obtain the paths to the rlib produced and to the output directory for dependencies.
+    fn rlib_and_dep_output(&self, config: &'a Config, name: &str)
+        -> CargoResult<(PathBuf, PathBuf)>
+    {
+        let opts = CompileOptions::default(config, CompileMode::Build);
+        let compilation = compile(&self.workspace, &opts)?;
+        let rlib = compilation.libraries[self.package.package_id()]
+            .iter()
+            .find(|t| t.0.name() == name)
+            .ok_or_else(|| human("lost a build artifact"))?;
+
+        Ok((rlib.1.clone(), compilation.deps_output))
+    }
+
 }
 
 /// Perform the heavy lifting.
@@ -52,51 +120,47 @@ fn generate_rlib<'a>(config: &'a Config,
 /// build it as well.
 ///
 /// TODO:
-/// * split this up
-/// * give some structure to the build artefact gathering
 /// * possibly reduce the complexity by investigating where some of the info can be sourced from
 /// in a more direct fashion
+/// * add proper support to compare two arbitrary versions
 fn do_main() -> CargoResult<()> {
+    use std::env::var;
     let config = Config::default()?;
+    let mut source = SourceInfo::new(&config)?;
 
-    let manifest_path = find_root_manifest_for_wd(None, config.cwd())?;
+    let current =
+        if let (Ok(n), Ok(v)) = (var("RUST_SEMVER_NAME"), var("RUST_SEMVER_CURRENT")) {
+            let info = NameAndVersion { name: &n, version: &v };
+            WorkInfo::remote(&config, &mut source, info)?
+        } else {
+            WorkInfo::local(&config)?
+        };
 
-    let local_package = Package::for_path(&manifest_path, &config)?;
-    let local_workspace = Workspace::new(&manifest_path, &config)?;
-    let local_compilation = generate_rlib(&config, &local_workspace)?;
+    let name = current.package.name().to_owned();
 
-    let name = local_package.name();
+    let (stable, stable_version) = if let Ok(v) = var("RUST_SEMVER_STABLE") {
+        let info = NameAndVersion { name: &name, version: &v };
+        let work_info = WorkInfo::remote(&config, &mut source, info)?;
+        (work_info, v.clone())
+    } else {
+        let stable_crate = exact_search(&name)?;
+        let info = NameAndVersion { name: &name, version: &stable_crate.max_version };
+        let work_info = WorkInfo::remote(&config, &mut source, info)?;
+        (work_info, stable_crate.max_version.clone())
+    };
 
-    let local_rlib = local_compilation.libraries[local_package.package_id()]
-        .iter()
-        .find(|t| t.0.name() == name)
-        .ok_or_else(|| human("lost a build artifact"))?;
-
-    let source_id = SourceId::crates_io(&config)?;
-    let mut registry_source = RegistrySource::remote(&source_id, &config);
-
-    let remote_crate = exact_search(name)?;
-
-    let package_id = PackageId::new(name, &remote_crate.max_version, &source_id)?;
-
-    let stable_package = registry_source.download(&package_id)?;
-    let stable_workspace = Workspace::ephemeral(stable_package, &config, None, false)?;
-    let stable_compilation = generate_rlib(&config, &stable_workspace)?;
-
-    let stable_rlib = stable_compilation.libraries[&package_id]
-        .iter()
-        .find(|t| t.0.name() == name)
-        .ok_or_else(|| human("lost a build artifact"))?;
+    let (current_rlib, current_deps_output) = current.rlib_and_dep_output(&config, &name)?;
+    let (stable_rlib, stable_deps_output) = stable.rlib_and_dep_output(&config, &name)?;
 
     let mut child = Command::new("rust-semverver")
         .arg("--crate-type=lib")
-        .args(&["--extern", &*format!("old={}", stable_rlib.1.display())])
-        .args(&[format!("-L{}", stable_compilation.deps_output.display())])
-        .args(&["--extern", &*format!("new={}", local_rlib.1.display())])
-        .args(&[format!("-L{}", local_compilation.deps_output.display())])
+        .args(&["--extern", &*format!("old={}", stable_rlib.display())])
+        .args(&[format!("-L{}", stable_deps_output.display())])
+        .args(&["--extern", &*format!("new={}", current_rlib.display())])
+        .args(&[format!("-L{}", current_deps_output.display())])
         .arg("-")
         .stdin(Stdio::piped())
-        .env("RUST_SEMVER_CRATE_VERSION", &remote_crate.max_version)
+        .env("RUST_SEMVER_CRATE_VERSION", stable_version)
         .spawn()
         .map_err(|e| human(format!("could not spawn rustc: {}", e)))?;
 
