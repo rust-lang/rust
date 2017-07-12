4a03e45169beb9a2d69518436ecc7850401cce50
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-07-12 09:29:18 +0200

    Add tests for #113
    resolves #113

---
+++ tests/compile-fail/transmute_fat.rs
@@ -0,0 +1,13 @@
+#![feature(i128_type)]
+
+fn main() {
+    #[cfg(target_pointer_width="64")]
+    let bad = unsafe {
+        std::mem::transmute::<&[u8], u128>(&[1u8])
+    };
+    #[cfg(target_pointer_width="32")]
+    let bad = unsafe {
+        std::mem::transmute::<&[u8], u64>(&[1u8])
+    };
+    bad + 1; //~ ERROR a raw memory access tried to access part of a pointer value as raw bytes
+}
