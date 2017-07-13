7701ff2f898b24cb17391b2378a9c9fe7272aa72
Oliver Schneider <git-no-reply-9879165716479413131@oli-obk.de>
2017-07-13 17:25:27 +0200

    Remove duplicate test

--- tests/compile-fail/double_free.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let x = Box::new(42);
-    {
-        let bad_box: Box<i32> = unsafe { std::ptr::read(&x) };
-        drop(bad_box);
-    }
-    drop(x); //~ ERROR dangling pointer was dereferenced
-}
