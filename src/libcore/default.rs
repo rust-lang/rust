caf125f414cc7bcdacdbfeab5c3b62eba772c8a3
Valentin Brandl <vbrandl@riseup.net>
2017-07-15 17:34:37 +0200

    Rephrase the doc string

--- src/libcore/default.rs
+++ src/libcore/default.rs
@@ -136,23 +136,23 @@ macro_rules! default_impl {
     }
 }
 
-default_impl! { (), (), "Defaults to `()`" }
-default_impl! { bool, false, "Defaults to `false`" }
-default_impl! { char, '\x00', "Defaults to `\\x00`" }
+default_impl! { (), (), "Returns the default value of `()`" }
+default_impl! { bool, false, "Returns the default value of `false`" }
+default_impl! { char, '\x00', "Returns the default value of `\\x00`" }
 
-default_impl! { usize, 0, "Defaults to `0`" }
-default_impl! { u8, 0, "Defaults to `0`" }
-default_impl! { u16, 0, "Defaults to `0`" }
-default_impl! { u32, 0, "Defaults to `0`" }
-default_impl! { u64, 0, "Defaults to `0`" }
-default_impl! { u128, 0, "Defaults to `0`" }
+default_impl! { usize, 0, "Returns the default value of `0`" }
+default_impl! { u8, 0, "Returns the default value of `0`" }
+default_impl! { u16, 0, "Returns the default value of `0`" }
+default_impl! { u32, 0, "Returns the default value of `0`" }
+default_impl! { u64, 0, "Returns the default value of `0`" }
+default_impl! { u128, 0, "Returns the default value of `0`" }
 
-default_impl! { isize, 0, "Defaults to `0`" }
-default_impl! { i8, 0, "Defaults to `0`" }
-default_impl! { i16, 0, "Defaults to `0`" }
-default_impl! { i32, 0, "Defaults to `0`" }
-default_impl! { i64, 0, "Defaults to `0`" }
-default_impl! { i128, 0, "Defaults to `0`" }
+default_impl! { isize, 0, "Returns the default value of `0`" }
+default_impl! { i8, 0, "Returns the default value of `0`" }
+default_impl! { i16, 0, "Returns the default value of `0`" }
+default_impl! { i32, 0, "Returns the default value of `0`" }
+default_impl! { i64, 0, "Returns the default value of `0`" }
+default_impl! { i128, 0, "Returns the default value of `0`" }
 
-default_impl! { f32, 0.0f32, "Defaults to `0.0`" }
-default_impl! { f64, 0.0f64, "Defaults to `0.0`" }
+default_impl! { f32, 0.0f32, "Returns the default value of `0.0`" }
+default_impl! { f64, 0.0f64, "Returns the default value of `0.0`" }
