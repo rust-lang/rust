5f37110e5e1ae14a19aa7ee036f27e47e08ee73d
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-07-16 02:02:34 +0300

    Compile `compiler_builtins` with `abort` panic strategy

--- src/bootstrap/bin/rustc.rs
+++ src/bootstrap/bin/rustc.rs
@@ -150,7 +150,12 @@ fn main() {
         // This... is a bit of a hack how we detect this. Ideally this
         // information should be encoded in the crate I guess? Would likely
         // require an RFC amendment to RFC 1513, however.
-        if crate_name == "panic_abort" {
+        //
+        // `compiler_builtins` are unconditionally compiled with panic=abort to
+        // workaround undefined references to `rust_eh_unwind_resume` generated
+        // otherwise, see issue https://github.com/rust-lang/rust/issues/43095.
+        if crate_name == "panic_abort" ||
+           crate_name == "compiler_builtins" && stage != "0" {
             cmd.arg("-C").arg("panic=abort");
         }
 
