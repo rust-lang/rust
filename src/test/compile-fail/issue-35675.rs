eb7f429ea54db7111b9c1570ce08c3851727d2e5
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-07-17 10:16:08 +0200

    Move resolve diagnostic instability to compile-fail
    The produced paths aren't stable between builds, since
    reporting paths inside resolve, before resolve is finished
    might produce paths resolved to type aliases instead of
    the concrete type.
    
    Compile-fail tests can match just parts of messages, so they
    don't "suffer" from this issue.
    
    This is just a workaround, the instability should be fixed
    in the future.

--- src/test/compile-fail/issue-35675.rs
+++ src/test/compile-fail/issue-35675.rs
@@ -12,14 +12,13 @@
 enum Fruit { //~ HELP possible candidate is found in another module, you can import it into scope
     //~^ HELP possible candidate is found in another module, you can import it into scope
     Apple(i64),
-    //~^ HELP there is an enum variant `Fruit::Apple`, did you mean to use `Fruit`?
-    //~| HELP there is an enum variant `Fruit::Apple`, did you mean to use `Fruit`?
     Orange(i64),
 }
 
 fn should_return_fruit() -> Apple {
     //~^ ERROR cannot find type `Apple` in this scope
     //~| NOTE not found in this scope
+    //~| HELP you can try using the variant's enum
     Apple(5)
     //~^ ERROR cannot find function `Apple` in this scope
     //~| NOTE not found in this scope
@@ -27,15 +26,33 @@ fn should_return_fruit() -> Apple {
 
 fn should_return_fruit_too() -> Fruit::Apple {
     //~^ ERROR expected type, found variant `Fruit::Apple`
+    //~| HELP you can try using the variant's enum
     //~| NOTE not a type
     Apple(5)
     //~^ ERROR cannot find function `Apple` in this scope
     //~| NOTE not found in this scope
 }
 
+fn foo() -> Ok {
+    //~^ ERROR expected type, found variant `Ok`
+    //~| NOTE not a type
+    //~| HELP there is an enum variant
+    //~| HELP there is an enum variant
+    Ok(())
+}
+
 fn bar() -> Variant3 {
     //~^ ERROR cannot find type `Variant3` in this scope
+    //~| HELP you can try using the variant's enum
     //~| NOTE not found in this scope
+}
+
+fn qux() -> Some {
+    //~^ ERROR expected type, found variant `Some`
+    //~| NOTE not a type
+    //~| HELP there is an enum variant
+    //~| HELP there is an enum variant
+    Some(1)
 }
 
 fn main() {}
@@ -45,7 +62,6 @@ mod x {
         Variant1,
         Variant2(),
         Variant3(usize),
-        //~^ HELP there is an enum variant `x::Enum::Variant3`, did you mean to use `x::Enum`?
         Variant4 {},
     }
 }
