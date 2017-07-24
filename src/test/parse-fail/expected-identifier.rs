7ccb754e343916cf9e356436b3fad4d38d0710ab
Guillaume Gomez <guillaume1.gomez@gmail.com>
2017-07-24 15:01:29 +0200

    Add help in case ref and mut are switched

---
+++ src/test/parse-fail/expected-identifier.rs
@@ -0,0 +1,17 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// compile-flags: -Z parse-only
+
+fn main() {
+    let mut ref y = &x; //~ ERROR expected identifier, found keyword `ref`
+                        //~| HELP if you want to make a mutable reference, write:
+                        //~^^ ERROR expected one of `:`, `;`, `=`, or `@`, found `y`
+}
