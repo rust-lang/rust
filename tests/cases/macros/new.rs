53c389e6ab78adc4a60f8235dc3f1d4cad9de310
Inokentiy Babushkin <twk@twki.de>
2017-07-25 12:29:19 +0200

    Added more tests for macros. Closes #15.
    It seems the issues we had have been resolved upstream.

--- tests/cases/macros/new.rs
+++ tests/cases/macros/new.rs
@@ -1,6 +1,8 @@
 #![feature(decl_macro, use_extern_macros)]
 pub struct Item;
 
+pub macro foo() { Item }
+
 #[macro_export]
 macro_rules! bar {
     () => {
@@ -8,7 +10,17 @@ macro_rules! bar {
     }
 }
 
-#[allow(dead_code)]
-fn abc() -> Item {
+pub macro baz() { Item }
+
+pub macro quux1() { Item }
+
+#[macro_export]
+macro_rules! quux2 {
+    () => {
+        Item
+    }
+}
+
+pub fn abc() -> Item {
     bar!()
 }
--- tests/cases/macros/old.rs
+++ tests/cases/macros/old.rs
@@ -1,8 +1,26 @@
 #![feature(decl_macro, use_extern_macros)]
 pub struct Item;
 
+pub macro foo() { Item }
+
 pub macro bar() { Item }
 
-fn abc() -> Item {
+#[macro_export]
+macro_rules! baz {
+    () => {
+        Item
+    }
+}
+
+pub macro qux1() { Item }
+
+#[macro_export]
+macro_rules! qux2 {
+    () => {
+        Item
+    }
+}
+
+pub fn abc() -> Item {
     bar!()
 }
