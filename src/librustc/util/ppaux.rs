35e78b5cddc04c6bd13da2a1290d27cfb8ae8db8
Niko Matsakis <niko@alum.mit.edu>
2017-07-31 07:40:24 +0300

    change skolemizations to use universe index

--- src/librustc/util/ppaux.rs
+++ src/librustc/util/ppaux.rs
@@ -786,7 +786,7 @@ define_print! {
                 }
 
                 ty::ReSkolemized(id, ref bound_region) => {
-                    write!(f, "ReSkolemized({}, {:?})", id.index, bound_region)
+                    write!(f, "ReSkolemized({:?}, {:?})", id, bound_region)
                 }
 
                 ty::ReEmpty => write!(f, "ReEmpty"),
