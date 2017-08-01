19a9441f855495665691eb8b72fc51f8ab2d5f9a
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-08-01 13:11:48 +0200

    Update to latest rustc diagnostic output

--- clippy_tests/examples/trailing_zeros.rs
+++ clippy_tests/examples/trailing_zeros.rs
@@ -5,6 +5,6 @@
 fn main() {
     let x: i32 = 42;
     let _ = #[clippy(author)] (x & 0b1111 == 0);  // suggest trailing_zeros
-    let _ = x & 0b11111 == 0; // suggest trailing_zeros
-    let _ = x & 0b11010 == 0; // do not lint
+    let _ = x & 0b1_1111 == 0; // suggest trailing_zeros
+    let _ = x & 0b1_1010 == 0; // do not lint
 }
