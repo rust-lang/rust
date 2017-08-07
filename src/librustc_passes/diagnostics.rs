78d1442808fa318c782e7f8249dab27126254930
Zack M. Davis <code@zackmdavis.net>
2017-08-07 10:38:16 -0700

    extended information for E0571 break with value in non-`loop` loop

--- src/librustc_passes/diagnostics.rs
+++ src/librustc_passes/diagnostics.rs
@@ -223,6 +223,40 @@ To fix this, add a label specifying which loop is being broken out of:
 ```
 'foo: while break 'foo {}
 ```
+"##,
+
+E0571: r##"
+A `break` statement with an argument appeared in a non-`loop` loop.
+
+Example of erroneous code:
+
+```compile_fail,E0571
+# let mut i = 1;
+# fn satisfied(n: usize) -> bool { n % 23 == 0 }
+let result = while true {
+    if satisfied(i) {
+        break 2*i; // error: `break` with value from a `while` loop
+    }
+    i += 1;
+};
+```
+
+The `break` statement can take an argument (which will be the value of the loop
+expression if the `break` statement is executed) in `loop` loops, but not
+`for`, `while`, or `while let` loops.
+
+Make sure `break value;` statements only occur in `loop` loops:
+
+```
+# let mut i = 1;
+# fn satisfied(n: usize) -> bool { n % 23 == 0 }
+let result = loop { // ok!
+    if satisfied(i) {
+        break 2*i;
+    }
+    i += 1;
+};
+```
 "##
 }
 
@@ -230,5 +264,4 @@ register_diagnostics! {
     E0226, // only a single explicit lifetime bound is permitted
     E0472, // asm! is unsupported on this target
     E0561, // patterns aren't allowed in function pointer types
-    E0571, // `break` with a value in a non-`loop`-loop
 }
