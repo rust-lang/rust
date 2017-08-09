e69a2aba18f0da8e004c0417a574fe001489a0fc
topecongiro <seuchida@gmail.com>
2017-08-10 08:13:20 +0900

    Add error_on_line_overflow_comments config option

--- src/config.rs
+++ src/config.rs
@@ -501,6 +501,7 @@ create_config! {
          via the --file-lines option";
     max_width: usize, 100, "Maximum width of each line";
     error_on_line_overflow: bool, true, "Error if unable to get all lines within max_width";
+    error_on_line_overflow_comments: bool, true, "Error if unable to get comments within max_width";
     tab_spaces: usize, 4, "Number of spaces per tab";
     fn_call_width: usize, 60,
         "Maximum width of the args of a function call before falling back to vertical formatting";
---
+++ tests/target/configs-error_on_line_overflow_comment-false.rs
@@ -0,0 +1,7 @@
+// rustfmt-error_on_line_overflow_comments: false
+// Error on line overflow comment
+
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+fn main() {
+    // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+}
