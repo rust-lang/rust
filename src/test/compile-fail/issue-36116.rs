804459bdca28010137990220e617a6b6cbab18d0
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-08-11 02:30:08 +0300

    Issue warnings for unnecessary path disambiguators

--- src/test/compile-fail/issue-36116.rs
+++ src/test/compile-fail/issue-36116.rs
@@ -13,13 +13,24 @@
 #![feature(rustc_attrs)]
 #![allow(unused)]
 
+macro_rules! m {
+    ($p: path) => {
+        let _ = $p(0);
+        let _: $p;
+    }
+}
+
 struct Foo<T> {
     _a: T,
 }
 
+struct S<T>(T);
+
 fn f() {
-    let f = Some(Foo { _a: 42 }).map(|a| a as Foo::<i32>);
-    let g: Foo::<i32> = Foo { _a: 42 };
+    let f = Some(Foo { _a: 42 }).map(|a| a as Foo::<i32>); //~ WARN unnecessary path disambiguator
+    let g: Foo::<i32> = Foo { _a: 42 }; //~ WARN unnecessary path disambiguator
+
+    m!(S::<u8>); // OK, no warning
 }
 
 #[rustc_error]
