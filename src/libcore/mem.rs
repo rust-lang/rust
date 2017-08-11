faadd35ee5074aa85fa6e2078650b3e6f1fd4662
Tobias Bucher <tobiasbucher5991@gmail.com>
2017-08-11 03:42:36 +0200

    Add `#[inline]` to `mem::unreachable`

--- src/libcore/mem.rs
+++ src/libcore/mem.rs
@@ -949,6 +949,7 @@ impl<T: ::fmt::Debug> ::fmt::Debug for ManuallyDrop<T> {
 /// NB: This is very different from the `unreachable!()` macro: Unlike the
 /// macro, which panics when it is executed, it is *undefined behavior* to
 /// reach code marked with this function.
+#[inline]
 #[unstable(feature = "unreachable", issue = "43751")]
 pub unsafe fn unreachable() -> ! {
     intrinsics::unreachable()
