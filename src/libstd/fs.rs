10a479ebe170398e70f872a7afa222d00a9c8f53
Corey Farwell <coreyf@rwell.org>
2017-08-15 10:13:35 -0400

    Clarify 'writable'-changing behavior of `set_readonly`.
    Fixes https://github.com/rust-lang/rust/issues/41984.

--- src/libstd/fs.rs
+++ src/libstd/fs.rs
@@ -934,7 +934,11 @@ impl Permissions {
     #[stable(feature = "rust1", since = "1.0.0")]
     pub fn readonly(&self) -> bool { self.0.readonly() }
 
-    /// Modifies the readonly flag for this set of permissions.
+    /// Modifies the readonly flag for this set of permissions. If the
+    /// `readonly` argument is `true`, using the resulting `Permission` will
+    /// update file permissions to forbid writing. Conversely, if it's `false`,
+    /// using the resulting `Permission` will update file permissions to allow
+    /// writing.
     ///
     /// This operation does **not** modify the filesystem. To modify the
     /// filesystem use the `fs::set_permissions` function.
