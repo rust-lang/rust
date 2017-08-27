907d2c73103817b1dfa1ce0dedf57114fdd87c80
Ralf Jung <post@ralfj.de>
2017-08-27 18:18:32 +0200

    rustc #43457 got fixed, we can enable more tests

--- tests/compile-fail/panic.rs
+++ tests/compile-fail/panic.rs
@@ -1,6 +1,3 @@
-// FIXME: Due to https://github.com/rust-lang/rust/issues/43457 we have to disable validation
-// compile-flags: -Zmir-emit-validate=0
-
 //error-pattern: the evaluated program panicked
 
 fn main() {
