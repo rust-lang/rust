e28f4a8e3e9de1cc745f57e78d0e75c078c91ef3
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-08-28 14:10:59 +0200

    Add a regression test for #313

---
+++ tests/run-pass/btreemap.rs
@@ -0,0 +1,14 @@
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub enum Foo {
+    A(&'static str),
+    _B,
+    _C,
+}
+
+pub fn main() {
+    let mut b = std::collections::BTreeSet::new();
+    b.insert(Foo::A("\'"));
+    b.insert(Foo::A("/="));
+    b.insert(Foo::A("#"));
+    b.insert(Foo::A("0o"));
+}
