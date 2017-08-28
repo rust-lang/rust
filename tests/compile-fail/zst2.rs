02a0f0f8146690df717bd868dd833b6bcb0b7a3a
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2017-08-28 14:59:16 +0200

    Turn off validation for some compile-fail tests

--- tests/compile-fail/zst2.rs
+++ tests/compile-fail/zst2.rs
@@ -1,3 +1,5 @@
+// FIXME: Probably failing due to https://github.com/solson/miri/issues/296
+// compile-flags: -Zmir-emit-validate=0
 // error-pattern: the evaluated program panicked
 
 #[derive(Debug)]
