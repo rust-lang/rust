b53b853129e4a05994e7029e2c45d74141a13961
Jonathan A. Kollasch <jakllsch@kollasch.net>
2017-08-31 09:17:50 -0500

    bootstrap: use shasum(1) on NetBSD build hosts
    NetBSD doesn't ship with sha256sum.  The openssl build will probably
    try to use perl anyway, so using perl's shasum is reasonable.

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -367,7 +367,7 @@ impl Step for Openssl {
             if !ok {
                 panic!("failed to download openssl source")
             }
-            let mut shasum = if target.contains("apple") {
+            let mut shasum = if target.contains("apple") || build.build.contains("netbsd") {
                 let mut cmd = Command::new("shasum");
                 cmd.arg("-a").arg("256");
                 cmd
