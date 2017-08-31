90aa66bfc2e90de39e10ad3ed6d08b534f1efd02
Jonathan A. Kollasch <jakllsch@kollasch.net>
2017-08-31 09:22:39 -0500

    bootstrap: use tar -z on extract
    Some tar(1) programs—such as NetBSD's—do not automatically decompress.

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -387,7 +387,7 @@ impl Step for Openssl {
         let dst = build.openssl_install_dir(target).unwrap();
         drop(fs::remove_dir_all(&obj));
         drop(fs::remove_dir_all(&dst));
-        build.run(Command::new("tar").arg("xf").arg(&tarball).current_dir(&out));
+        build.run(Command::new("tar").arg("zxf").arg(&tarball).current_dir(&out));
 
         let mut configure = Command::new("perl");
         configure.arg(obj.join("Configure"));
