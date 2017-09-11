eb26d5b7c928646983c53bcbd33ce578acbf238f
Alex Crichton <alex@alexcrichton.com>
2017-09-11 11:01:48 -0700

    rustbuild: Fix a distribution bug with rustdoc
    Apparently `File::create` was called when there was an existing hard link or the
    like, causing an existing file to get accidentally truncated!
    
    Closes #44487

--- src/bootstrap/dist.rs
+++ src/bootstrap/dist.rs
@@ -900,6 +900,7 @@ impl Step for PlainSourceTarball {
 fn install(src: &Path, dstdir: &Path, perms: u32) {
     let dst = dstdir.join(src.file_name().unwrap());
     t!(fs::create_dir_all(dstdir));
+    drop(fs::remove_file(&dst));
     {
         let mut s = t!(fs::File::open(&src));
         let mut d = t!(fs::File::create(&dst));
