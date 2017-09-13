c1630973d07fb9fcbf790808408392c549c266c5
Ralf Jung <post@ralfj.de>
2017-09-13 17:23:37 +0200

    Disable the address-of-ZST test for now; it is buggy as it disagrees with rustc

--- tests/compile-fail/zst3.rs
+++
@@ -1,9 +0,0 @@
-// error-pattern: the evaluated program panicked
-
-#[derive(Debug)]
-struct A;
-
-fn main() {
-    // can't use assert_eq, b/c that will try to print the pointer addresses with full MIR enabled
-    assert!(&A as *const A == &A as *const A);
-}
