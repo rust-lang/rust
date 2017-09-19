c66003d2593dae01455aa7d55309ae5a64361ebd
gnzlbg <gonzalobg88@gmail.com>
2017-09-19 18:27:00 +0200

    [bmi] add module-level docs to bmi and bmi2 modules

--- library/stdarch/src/x86/bmi.rs
+++ library/stdarch/src/x86/bmi.rs
@@ -1,3 +1,12 @@
+//! Bit Manipulation Instruction (BMI) Set 1.0.
+//!
+//! The reference is [Intel 64 and IA-32 Architectures Software Developer's
+//! Manual Volume 2: Instruction Set Reference,
+//! A-Z](http://www.intel.de/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf).
+//!
+//! [Wikipedia](https://en.wikipedia.org/wiki/Bit_Manipulation_Instruction_Sets#BMI1_.28Bit_Manipulation_Instruction_Set_1.29)
+//! provides a quick overview of the available instructions.
+
 #[allow(dead_code)]
 extern "platform-intrinsic" {
     fn x86_bmi_bextr_32(x: u32, y: u32) -> u32;
--- library/stdarch/src/x86/bmi2.rs
+++ library/stdarch/src/x86/bmi2.rs
@@ -1,3 +1,12 @@
+//! Bit Manipulation Instruction (BMI) Set 2.0.
+//!
+//! The reference is [Intel 64 and IA-32 Architectures Software Developer's
+//! Manual Volume 2: Instruction Set Reference,
+//! A-Z](http://www.intel.de/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf).
+//!
+//! [Wikipedia](https://en.wikipedia.org/wiki/Bit_Manipulation_Instruction_Sets#BMI2_.28Bit_Manipulation_Instruction_Set_2.29)
+//! provides a quick overview of the available instructions.
+
 /// Unsigned multiply without affecting flags.
 ///
 /// Unsigned multiplication of `a` with `b` returning a pair `(lo, hi)` with
