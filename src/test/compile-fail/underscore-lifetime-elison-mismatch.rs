06926b6298693a1fd6ad85073fc38b26cf6d6fbf
Taylor Cramer <cramertj@google.com>
2017-09-19 16:43:18 -0700

    Add tests for multiple underscore and non-underscore lifetimes

--- src/test/compile-fail/underscore-lifetime-elison-mismatch.rs
+++ src/test/compile-fail/underscore-lifetime-elison-mismatch.rs
@@ -10,25 +10,6 @@
 
 #![feature(underscore_lifetimes)]
 
-struct Foo<'a>(&'a u8);
+fn foo(x: &mut Vec<&'_ u8>, y: &'_ u8) { x.push(y); } //~ ERROR lifetime mismatch
 
-fn foo<'_> //~ ERROR invalid lifetime parameter name: `'_`
-(_: Foo<'_>) {}
-
-trait Meh<'a> {}
-impl<'a> Meh<'a> for u8 {}
-
-fn meh() -> Box<for<'_> Meh<'_>> //~ ERROR invalid lifetime parameter name: `'_`
-//~^ ERROR missing lifetime specifier
-//~^^ ERROR missing lifetime specifier
-{
-  Box::new(5u8)
-}
-
-fn foo2(_: &'_ u8, y: &'_ u8) -> &'_ u8 { y } //~ ERROR missing lifetime specifier
-
-fn main() {
-    let x = 5;
-    foo(Foo(&x));
-    let _ = meh();
-}
+fn main() {}
