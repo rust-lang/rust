66eea5e662db32dcdd233c48b3cddd691c2cb8fa
Manish Goregaokar <manishsmail@gmail.com>
2017-09-25 19:52:20 -0700

    Fix dogfood needless-borrow

--- clippy_lints/src/needless_pass_by_value.rs
+++ clippy_lints/src/needless_pass_by_value.rs
@@ -144,7 +144,7 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for NeedlessPassByValue {
                         let TyPath(QPath::Resolved(_, ref path)) = input.node,
                         let Some(elem_ty) = path.segments.iter()
                             .find(|seg| seg.name == "Vec")
-                            .and_then(|ref ps| ps.parameters.as_ref())
+                            .and_then(|ps| ps.parameters.as_ref())
                             .map(|params| &params.types[0]),
                     ], {
                         let slice_ty = format!("&[{}]", snippet(cx, elem_ty.span, "_"));
