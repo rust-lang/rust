12a8bc6eb916c3b8a61ad813c2e15aac8184c357
Daniel Klauer <daniel.c.klauer@web.de>
2017-09-30 13:48:12 +0200

    rustc: Use 16bit c_int for msp430
    Fix regression from c2fe69b9, where main() signature was changed from
    using 16bit isize to 32bit c_int for argc parameter/result.

--- src/test/codegen/abi-main-signature-32bit-c-int.rs
+++ src/test/codegen/abi-main-signature-32bit-c-int.rs
@@ -11,6 +11,9 @@
 // Checks the signature of the implicitly generated native main()
 // entry point. It must match C's `int main(int, char **)`.
 
+// This test is for targets with 32bit c_int only.
+// ignore-msp430
+
 fn main() {
 }
 
