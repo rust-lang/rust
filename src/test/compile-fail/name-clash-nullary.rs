db0be32fec539ab57c446433cb186f979af40413
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-10-05 22:54:34 +0300

    resolve: Use same rules for disambiguating fresh bindings in `match` and `let`

--- src/test/compile-fail/name-clash-nullary.rs
+++ src/test/compile-fail/name-clash-nullary.rs
@@ -11,7 +11,7 @@
 use std::option::*;
 
 fn main() {
-  let None: isize = 42; //~ ERROR let bindings cannot shadow unit variants
+  let None: isize = 42; //~ ERROR mismatched types
   log(debug, None);
   //~^ ERROR cannot find function `log` in this scope
   //~| ERROR cannot find value `debug` in this scope
