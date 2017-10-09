52cb6fc9367f3aed373d8edda7c7fa52ed9f4c25
Felix S. Klock II <pnkfelix@pnkfx.org>
2017-10-09 15:50:56 +0200

    Moved remaining AST-borrowck diagnostic definitions to `rustc_mir` crate.

--- src/librustc_borrowck/lib.rs
+++ src/librustc_borrowck/lib.rs
@@ -16,7 +16,6 @@
 #![allow(non_camel_case_types)]
 
 #![feature(quote)]
-#![feature(rustc_diagnostic_macros)]
 
 #[macro_use] extern crate log;
 #[macro_use] extern crate syntax;
@@ -33,14 +32,8 @@ extern crate rustc_mir;
 pub use borrowck::check_crate;
 pub use borrowck::build_borrowck_dataflow_data_for_fn;
 
-// NB: This module needs to be declared first so diagnostics are
-// registered before they are used.
-mod diagnostics;
-
 mod borrowck;
 
 pub mod graphviz;
 
 pub use borrowck::provide;
-
-__build_diagnostic_array! { librustc_borrowck, DIAGNOSTICS }
