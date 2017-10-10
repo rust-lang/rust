16a478368c8dcc0c0ee47372a9f663b23d28b097
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-10-10 21:20:57 +0300

    Fix breakage from `dyn Trait`

--- src/types.rs
+++ src/types.rs
@@ -652,7 +652,7 @@ impl Rewrite for ast::TraitRef {
 impl Rewrite for ast::Ty {
     fn rewrite(&self, context: &RewriteContext, shape: Shape) -> Option<String> {
         match self.node {
-            ast::TyKind::TraitObject(ref bounds) => bounds.rewrite(context, shape),
+            ast::TyKind::TraitObject(ref bounds, ..) => bounds.rewrite(context, shape),
             ast::TyKind::Ptr(ref mt) => {
                 let prefix = match mt.mutbl {
                     Mutability::Mutable => "*mut ",
