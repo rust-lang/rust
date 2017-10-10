6d94167014e1738486601e05bdfb5cdef812d18f
Cameron Steffen <CSteffen@trustwave.com>
2017-10-09 23:15:19 -0500

    move cstring tests

--- tests/ui/methods.rs
+++ tests/ui/methods.rs
@@ -453,10 +453,3 @@ fn str_extend_chars() {
     let f = HasChars;
     s.extend(f.chars());
 }
-
-#[allow(result_unwrap_used)]
-fn temporary_cstring() {
-    use std::ffi::CString;
-
-    CString::new("foo").unwrap().as_ptr();
-}
