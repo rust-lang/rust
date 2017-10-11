9da400965f64d725a2d30383b3bb066f628c6ae0
Alex Crichton <alex@alexcrichton.com>
2017-10-11 11:28:02 -0400

    Attempt to fix CI (#108)
    Need to bring codegen units back to only one for now

--- library/stdarch/ci/run.sh
+++ library/stdarch/ci/run.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
+# FIXME(rust-lang/rust#45201) shouldn't need to specify one codegen unit
+export RUSTFLAGS="$RUSTFLAGS -C codegen-units=1"
+
 cargo test --target $TARGET
 cargo test --release --target $TARGET
