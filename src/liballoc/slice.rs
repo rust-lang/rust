843181172892af2fdb7755e77c39227aba252c83
whitequark <whitequark@whitequark.org>
2017-10-15 13:15:41 +0000

    Bring back slice::ref_slice as slice::from_ref.
    These functions were deprecated and removed in 1.5, but such simple
    functionality shouldn't require using unsafe code, and it isn't
    cluttering libstd too much.

--- src/liballoc/slice.rs
+++ src/liballoc/slice.rs
@@ -119,6 +119,8 @@ pub use core::slice::{SplitN, RSplitN, SplitNMut, RSplitNMut};
 pub use core::slice::{RSplit, RSplitMut};
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use core::slice::{from_raw_parts, from_raw_parts_mut};
+#[stable(feature = "from_ref", since = "1.22.0")]
+pub use core::slice::{from_ref, from_ref_mut};
 #[unstable(feature = "slice_get_slice", issue = "35729")]
 pub use core::slice::SliceIndex;
 
