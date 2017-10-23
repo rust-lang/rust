24a2c14733604132135b2ab598e7a1cf95d8b3ab
Alex Burka <aburka@seas.upenn.edu>
2017-10-23 15:20:58 -0400

    remove if_let_chain

--- clippy_lints/src/utils/mod.rs
+++ clippy_lints/src/utils/mod.rs
@@ -37,63 +37,6 @@ pub use self::hir_utils::{SpanlessEq, SpanlessHash};
 
 pub type MethodArgs = HirVec<P<Expr>>;
 
-/// Produce a nested chain of if-lets and ifs from the patterns:
-///
-/// ```rust,ignore
-/// if_let_chain! {[
-///     let Some(y) = x,
-///     y.len() == 2,
-///     let Some(z) = y,
-/// ], {
-///     block
-/// }}
-/// ```
-///
-/// becomes
-///
-/// ```rust,ignore
-/// if let Some(y) = x {
-///     if y.len() == 2 {
-///         if let Some(z) = y {
-///             block
-///         }
-///     }
-/// }
-/// ```
-#[macro_export]
-macro_rules! if_let_chain {
-    ([let $pat:pat = $expr:expr, $($tt:tt)+], $block:block) => {
-        if let $pat = $expr {
-           if_let_chain!{ [$($tt)+], $block }
-        }
-    };
-    ([let $pat:pat = $expr:expr], $block:block) => {
-        if let $pat = $expr {
-           $block
-        }
-    };
-    ([let $pat:pat = $expr:expr,], $block:block) => {
-        if let $pat = $expr {
-           $block
-        }
-    };
-    ([$expr:expr, $($tt:tt)+], $block:block) => {
-        if $expr {
-           if_let_chain!{ [$($tt)+], $block }
-        }
-    };
-    ([$expr:expr], $block:block) => {
-        if $expr {
-           $block
-        }
-    };
-    ([$expr:expr,], $block:block) => {
-        if $expr {
-           $block
-        }
-    };
-}
-
 pub mod higher;
 
 /// Returns true if the two spans come from differing expansions (i.e. one is
