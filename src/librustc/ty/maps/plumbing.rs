c1436c3aa52c25fdbaa174bfe28df318d3ce879d
Wesley Wiser <wwiser@gmail.com>
2017-10-23 18:10:29 -0400

    Handle eval_always queries in force()
    Part of #45238

--- src/librustc/ty/maps/plumbing.rs
+++ src/librustc/ty/maps/plumbing.rs
@@ -433,10 +433,17 @@ macro_rules! define_maps {
                 profq_msg!(tcx, ProfileQueriesMsg::ProviderBegin);
                 let res = tcx.cycle_check(span, Query::$name(key), || {
                     tcx.sess.diagnostic().track_diagnostics(|| {
-                        tcx.dep_graph.with_task(dep_node,
-                                                tcx,
-                                                key,
-                                                Self::compute_result)
+                        if dep_node.kind.is_eval_always() {
+                            tcx.dep_graph.with_eval_always_task(dep_node,
+                                                                tcx,
+                                                                key,
+                                                                Self::compute_result)
+                        } else {
+                            tcx.dep_graph.with_task(dep_node,
+                                                    tcx,
+                                                    key,
+                                                    Self::compute_result)
+                        }
                     })
                 })?;
                 profq_msg!(tcx, ProfileQueriesMsg::ProviderEnd);
