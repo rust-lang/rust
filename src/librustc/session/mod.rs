cfc916ebf8a9039e321924ed111727c2fcfb3e70
Laurent Arnoud <laurent@spkdev.net>
2017-10-27 19:31:33 +0200

    Fix tidy error line longer than 100 chars

--- src/librustc/session/mod.rs
+++ src/librustc/session/mod.rs
@@ -778,7 +778,9 @@ pub fn build_session_(sopts: config::Options,
 
     let working_dir = match env::current_dir() {
         Ok(dir) => dir.to_string_lossy().into_owned(),
-        Err(e) => panic!(p_s.span_diagnostic.fatal(&format!("Current directory is invalid: {}", e))),
+        Err(e) => {
+            panic!(p_s.span_diagnostic.fatal(&format!("Current directory is invalid: {}", e)))
+        }
     };
     let working_dir = file_path_mapping.map_prefix(working_dir);
 
