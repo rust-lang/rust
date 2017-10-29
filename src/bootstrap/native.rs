aa4726cd37b0cf2a7f968274ad019df7a6a64b4c
Michael Neumann <mneumann@ntecs.de>
2017-10-29 18:42:04 +0100

    Support Dragonfly when building Openssl

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -451,6 +451,7 @@ impl Step for Openssl {
             "x86_64-apple-darwin" => "darwin64-x86_64-cc",
             "x86_64-linux-android" => "linux-x86_64",
             "x86_64-unknown-freebsd" => "BSD-x86_64",
+            "x86_64-unknown-dragonfly" => "BSD-x86_64",
             "x86_64-unknown-linux-gnu" => "linux-x86_64",
             "x86_64-unknown-linux-musl" => "linux-x86_64",
             "x86_64-unknown-netbsd" => "BSD-x86_64",
