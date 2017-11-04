33867579238ea94a1d22d1da344d6e484cacd9da
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-11-04 03:01:56 +0300

    Move some tests to UI

--- src/test/ui/const-pattern-irrefutable.rs
+++ src/test/ui/const-pattern-irrefutable.rs
@@ -20,10 +20,7 @@ const a: u8 = 2;
 
 fn main() {
     let a = 4; //~ ERROR refutable pattern in local binding: `_` not covered
-               //~^ NOTE pattern `_` not covered
     let c = 4; //~ ERROR refutable pattern in local binding: `_` not covered
-               //~^ NOTE pattern `_` not covered
     let d = 4; //~ ERROR refutable pattern in local binding: `_` not covered
-               //~^ NOTE pattern `_` not covered
     fn f() {} // Check that the `NOTE`s still work with an item here (c.f. issue #35115).
 }
