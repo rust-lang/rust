881c5b5a6dd8d22a184c5f2cfb2f054ad0d2a113
Seiichi Uchida <seuchida@gmail.com>
2017-11-11 23:14:01 +0900

    Add a test for #2021

--- tests/target/match.rs
+++ tests/target/match.rs
@@ -463,3 +463,15 @@ fn issue_2099() {
 
     match x {}
 }
+
+// #2021
+impl<'tcx> Const<'tcx> {
+    pub fn from_constval<'a>() -> Const<'tcx> {
+        let val = match *cv {
+            ConstVal::Variant(_) | ConstVal::Aggregate(..) | ConstVal::Unevaluated(..) => bug!(
+                "MIR must not use `{:?}` (aggregates are expanded to MIR rvalues)",
+                cv
+            ),
+        };
+    }
+}
