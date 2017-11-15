95c7325ac2aa89043e2ab39b86a3dc1615bee5ce
topecongiro <seuchida@gmail.com>
2017-11-15 15:46:00 +0900

    Do not get tricked by a nested comment

--- src/lists.rs
+++ src/lists.rs
@@ -572,10 +572,12 @@ where
             let comment_end = match self.inner.peek() {
                 Some(..) => {
                     let mut block_open_index = post_snippet.find("/*");
-                    // check if it really is a block comment (and not //*)
+                    // check if it really is a block comment (and not `//*` or a nested comment)
                     if let Some(i) = block_open_index {
-                        if i > 0 && &post_snippet[i - 1..i] == "/" {
-                            block_open_index = None;
+                        match post_snippet.find("/") {
+                            Some(j) if j < i => block_open_index = None,
+                            _ if i > 0 && &post_snippet[i - 1..i] == "/" => block_open_index = None,
+                            _ => (),
                         }
                     }
                     let newline_index = post_snippet.find('\n');
