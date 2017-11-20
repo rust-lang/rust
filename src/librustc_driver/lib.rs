8affcb7bd4a6f012941b07e5fb53dce44da24107
Tobias Bucher <tobiasbucher5991@gmail.com>
2017-11-20 22:16:38 +0100

    Clarify what `-D warnings` or `-F warnings` does
    They set all lints currently on the warning level to `deny` or `forbid`,
    respectively.

--- src/librustc_driver/lib.rs
+++ src/librustc_driver/lib.rs
@@ -980,7 +980,7 @@ Available lint options:
     println!("Lint groups provided by rustc:\n");
     println!("    {}  {}", padded("name"), "sub-lints");
     println!("    {}  {}", padded("----"), "---------");
-    println!("    {}  {}", padded("warnings"), "all built-in lints");
+    println!("    {}  {}", padded("warnings"), "all lints activated to the warning level");
 
     let print_lint_groups = |lints: Vec<(&'static str, Vec<lint::LintId>)>| {
         for (name, to) in lints {
