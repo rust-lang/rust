f891d04c87aa3ade9d60d9c3ca6bc582ec221613
Nick Cameron <ncameron@mozilla.com>
2017-11-24 14:46:33 +1300

    Some tests I renamed in a previous commit and forgot to git add

---
+++ tests/source/configs-same_line_attributes-false.rs
@@ -0,0 +1,26 @@
+// rustfmt-same_line_attributes: false
+// Option to place attributes on the same line as fields and variants where possible
+
+enum Lorem {
+    #[ serde(skip_serializing) ]
+    Ipsum,
+    #[ serde(skip_serializing) ]
+    Dolor,
+    #[ serde(skip_serializing) ]
+    Amet,
+}
+
+struct Lorem {
+    #[ serde(rename = "Ipsum") ]
+    ipsum: usize,
+    #[ serde(rename = "Dolor") ]
+    dolor: usize,
+    #[ serde(rename = "Amet") ]
+    amet: usize,
+}
+
+// #1943
+pub struct Bzip2 {
+    # [ serde (rename = "level") ]
+     level: i32 ,
+}
---
+++ tests/source/configs-same_line_attributes-true.rs
@@ -0,0 +1,20 @@
+// rustfmt-same_line_attributes: true
+// Option to place attributes on the same line as fields and variants where possible
+
+enum Lorem {
+    #[ serde(skip_serializing) ]
+    Ipsum,
+    #[ serde(skip_serializing) ]
+    Dolor,
+    #[ serde(skip_serializing) ]
+    Amet,
+}
+
+struct Lorem {
+    #[ serde(rename = "Ipsum") ]
+    ipsum: usize,
+    #[ serde(rename = "Dolor") ]
+    dolor: usize,
+    #[ serde(rename = "Amet") ]
+    amet: usize,
+}
