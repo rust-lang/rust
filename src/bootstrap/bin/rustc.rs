rust-lang/rust

commit 794ada06e7ad370ec48b4de69e43eb59d69543c9

add `reachable_only` to save-analysis config

src/bootstrap/bin/rustc.rs
@@ -183,7 +183,8 @@ fn main() {
         if env::var("RUSTC_SAVE_ANALYSIS") == Ok("api".to_string()) {
             cmd.arg("-Zsave-analysis");
             cmd.env("RUST_SAVE_ANALYSIS_CONFIG",
-                    "{\"output_file\": null,\"full_docs\": false,\"pub_only\": true,\
+                    "{\"output_file\": null,\"full_docs\": false,\
+                     \"pub_only\": true,\"reachable_only\": false,\
                      \"distro_crate\": true,\"signatures\": false,\"borrow_data\": false}");
         }
 

commit 68baaa1c401a4bf90f8f3ce67e907e08714527d7

Add a missing field to the RLS config
Fixes bug introduced in #46011 and reported in #46244

src/bootstrap/bin/rustc.rs
@@ -185,7 +185,8 @@ fn main() {
             cmd.env("RUST_SAVE_ANALYSIS_CONFIG",
                     "{\"output_file\": null,\"full_docs\": false,\
                      \"pub_only\": true,\"reachable_only\": false,\
-                     \"distro_crate\": true,\"signatures\": false,\"borrow_data\": false}");
+                     \"distro_crate\": true,\"signatures\": false,\"borrow_data\": false,\
+                     \"reachable_only\": false}");
         }
 
         // Dealing with rpath here is a little special, so let's go into some