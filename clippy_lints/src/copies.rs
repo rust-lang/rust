f8dbd32433bce7e63f48604423ba8d88e2bf47aa
laurent <laurent.mazare@gmail.com>
2017-11-29 20:42:37 +0000

    Add a couple small tests to the match-same-arm lint.

--- clippy_lints/src/copies.rs
+++ clippy_lints/src/copies.rs
@@ -203,14 +203,10 @@ fn lint_match_arms(cx: &LateContext, expr: &Expr) {
                     db.span_note(i.body.span, "same as this");
 
                     // Note: this does not use `span_suggestion` on purpose: there is no clean way
-                    // to
-                    // remove the other arm. Building a span and suggest to replace it to "" makes
-                    // an
-                    // even more confusing error message. Also in order not to make up a span for
-                    // the
-                    // whole pattern, the suggestion is only shown when there is only one pattern.
-                    // The
-                    // user should know about `|` if they are already using it…
+                    // to remove the other arm. Building a span and suggest to replace it to ""
+                    // makes an even more confusing error message. Also in order not to make up a
+                    // span for the whole pattern, the suggestion is only shown when there is only
+                    // one pattern. The user should know about `|` if they are already using it…
 
                     if i.pats.len() == 1 && j.pats.len() == 1 {
                         let lhs = snippet(cx, i.pats[0].span, "<pat1>");
--- tests/ui/matches.rs
+++ tests/ui/matches.rs
@@ -277,6 +277,26 @@ fn match_wild_err_arm() {
         Ok(_) => println!("ok"),
         Err(_) => {unreachable!();}
     }
+
+    // no warning because of the guard
+    match x {
+        Ok(x) if x*x == 64 => println!("ok"),
+        Ok(_) => println!("ok"),
+        Err(_) => println!("err")
+    }
+
+    match (x, Some(1i32)) {
+        (Ok(x), Some(_)) => println!("ok {}", x),
+        (Ok(_), Some(x)) => println!("ok {}", x),
+        _ => println!("err")
+    }
+
+    // no warning because of the different types for x
+    match (x, Some(1.0f64)) {
+        (Ok(x), Some(_)) => println!("ok {}", x),
+        (Ok(_), Some(x)) => println!("ok {}", x),
+        _ => println!("err")
+    }
 }
 
 fn main() {
