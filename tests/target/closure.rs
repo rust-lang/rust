9ea3e65df6ee614f44dcfc57f5c37ea5b499fbee
Seiichi Uchida <seuchida@gmail.com>
2017-11-30 06:10:59 +0900

    Update tests

--- tests/target/closure.rs
+++ tests/target/closure.rs
@@ -123,7 +123,9 @@ fn foo() {
 
 fn issue1405() {
     open_raw_fd(fd, b'r').and_then(|file| {
-        Capture::new_raw(None, |_, err| unsafe { raw::pcap_fopen_offline(file, err) })
+        Capture::new_raw(None, |_, err| unsafe {
+            raw::pcap_fopen_offline(file, err)
+        })
     });
 }
 
@@ -174,8 +176,9 @@ fn issue1329() {
 }
 
 fn issue325() {
-    let f =
-        || unsafe { xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx };
+    let f = || unsafe {
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    };
 }
 
 fn issue1697() {
