8dbe79ae5e2e299959b9dbd20074d348266a91ed
Jeffrey Seyfried <jeffrey.seyfried@gmail.com>
2017-12-06 18:30:20 -0800

    Fix fallout in tests.

--- src/test/pretty/issue-4264.pp
+++ src/test/pretty/issue-4264.pp
@@ -40,7 +40,7 @@ pub fn bar() ({
 
 
                   ((::fmt::format as
-                       for<'r> fn(std::fmt::Arguments<'r>) -> std::string::String {std::fmt::format})(((<::std::fmt::Arguments>::new_v1
+                       for<'r> fn(std::fmt::Arguments<'r>) -> std::string::String {std::fmt::format})(((<::fmt::Arguments>::new_v1
                                                                                                            as
                                                                                                            fn(&[&str], &[std::fmt::ArgumentV1<'_>]) -> std::fmt::Arguments<'_> {std::fmt::Arguments<'_>::new_v1})((&([("test"
                                                                                                                                                                                                                           as
