6d78bd5fdc5c5ade539e221d7e91603538199452
Seiichi Uchida <seuchida@gmail.com>
2017-12-09 20:21:34 +0900

    Add a test for error_on_line_overflow_strings

---
+++ tests/target/configs-error_on_line_overflow_strings-false.rs
@@ -0,0 +1,9 @@
+// rustfmt-error_on_line_overflow_strings: false
+// Suppress an error on line overflow strings.
+
+fn main() {
+    let x = "                                                                                                  ";
+    let a = "
+  
+";
+}
