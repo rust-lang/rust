0909ecbac7680a6fb235eda5f4a63f8816ee1357
Seiichi Uchida <seuchida@gmail.com>
2017-12-10 00:21:49 +0900

    Add tests for macro in pattern position

--- tests/source/macros.rs
+++ tests/source/macros.rs
@@ -284,3 +284,19 @@ impl Foo {
     /// foo
     pub fn foo(&self) -> Bar<foo!(   )> {}
 }
+
+// #819
+fn macro_in_pattern_position () {
+    let x = match y {
+        foo!(  ) => (),
+        bar!(            a, b,
+                         c) => (),
+        bar!(a
+             , b
+             , c
+             ,) => (),
+        baz!( 1 + 2 + 3, quux.kaas(  )
+        ) => (),
+        quux!(AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB) => (),
+    };
+}
