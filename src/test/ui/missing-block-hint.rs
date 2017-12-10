d4e51a8fb286444a8c276a05a1c3ba6ba8ca6576
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2017-12-10 22:47:55 +0300

    Move compile-fail tests with NOTE/HELP annotations to UI

--- src/test/ui/missing-block-hint.rs
+++ src/test/ui/missing-block-hint.rs
@@ -9,16 +9,12 @@
 // except according to those terms.
 
 fn main() {
-    let tup = (0, 1, 2);
-    // the case where we show a suggestion
-    let _ = tup[0];
-    //~^ ERROR cannot index into a value of type
-    //~| HELP to access tuple elements, use
-    //~| SUGGESTION tup.0
-
-    // the case where we show just a general hint
-    let i = 0_usize;
-    let _ = tup[i];
-    //~^ ERROR cannot index into a value of type
-    //~| HELP to access tuple elements, use tuple indexing syntax (e.g. `tuple.0`)
+    {
+        if (foo) => {} //~ ERROR expected `{`, found `=>`
+    }
+    {
+        if (foo)
+            bar; //~ ERROR expected `{`, found `bar`
+                 //~^ HELP try placing this code inside a block
+    }
 }
