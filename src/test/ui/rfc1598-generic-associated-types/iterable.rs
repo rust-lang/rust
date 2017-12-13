b90e9c9b2cf04920c08569953c4e3ee5e339524b
Sunjay Varma <varma.sunjay@gmail.com>
2017-12-13 18:50:22 -0500

    Added test to make sure we can refer to the declared traits of a generic associated type

--- src/test/ui/rfc1598-generic-associated-types/iterable.rs
+++ src/test/ui/rfc1598-generic-associated-types/iterable.rs
@@ -10,12 +10,19 @@
 
 #![feature(generic_associated_types)]
 
+use std::ops::Deref;
+
 //FIXME(#44265): "lifetime parameters are not allowed on this type" errors will be addressed in a
 //follow-up PR
 
 trait Iterable {
     type Item<'a>;
     type Iter<'a>: Iterator<Item = Self::Item<'a>>;
+    //~^ ERROR lifetime parameters are not allowed on this type [E0110]
+
+    // This weird type tests that we can use universal function call syntax to access the Item on
+    // Self::Iter which we have declared to be an Iterator
+    type Iter2<'a>: Deref<Target = <Self::Iter<'a> as Iterator>::Item>;
     //~^ ERROR lifetime parameters are not allowed on this type [E0110]
 
     fn iter<'a>(&'a self) -> Self::Iter<'a>;
