0cc573a9eea1a3f96e85c58a1acd586175d98796
Michael Hewson <michael@michaelhewson.ca>
2017-12-14 18:44:28 +0100

    better variable names in tests

--- src/test/run-pass/arbitrary_self_types_raw_pointer_trait.rs
+++ src/test/run-pass/arbitrary_self_types_raw_pointer_trait.rs
@@ -49,22 +49,22 @@ fn main() {
     assert_eq!("I'm an i32!", null_i32.foo());
     assert_eq!("I'm a u32!", null_u32.foo());
 
-    let bar_i32 = 5i32;
-    let bar_i32_thin = &bar_i32 as *const i32;
-    assert_eq!("I'm an i32!", bar_i32_thin.foo());
-    assert_eq!(5, unsafe { bar_i32_thin.bar() });
-    assert_eq!(5, unsafe { (&bar_i32_thin as *const *const i32).complicated() });
-    let bar_i32_fat = bar_i32_thin as *const Foo;
-    assert_eq!("I'm an i32!", bar_i32_fat.foo());
-    assert_eq!(5, unsafe { bar_i32_fat.bar() });
+    let valid_i32 = 5i32;
+    let valid_i32_thin = &valid_i32 as *const i32;
+    assert_eq!("I'm an i32!", valid_i32_thin.foo());
+    assert_eq!(5, unsafe { valid_i32_thin.bar() });
+    assert_eq!(5, unsafe { (&valid_i32_thin as *const *const i32).complicated() });
+    let valid_i32_fat = valid_i32_thin as *const Foo;
+    assert_eq!("I'm an i32!", valid_i32_fat.foo());
+    assert_eq!(5, unsafe { valid_i32_fat.bar() });
 
-    let bar_u32 = 18u32;
-    let bar_u32_thin = &bar_u32 as *const u32;
-    assert_eq!("I'm a u32!", bar_u32_thin.foo());
-    assert_eq!(18, unsafe { bar_u32_thin.bar() });
-    assert_eq!(18, unsafe { (&bar_u32_thin as *const *const u32).complicated() });
-    let bar_u32_fat = bar_u32_thin as *const Foo;
-    assert_eq!("I'm a u32!", bar_u32_fat.foo());
-    assert_eq!(18, unsafe { bar_u32_fat.bar() });
+    let valid_u32 = 18u32;
+    let valid_u32_thin = &valid_u32 as *const u32;
+    assert_eq!("I'm a u32!", valid_u32_thin.foo());
+    assert_eq!(18, unsafe { valid_u32_thin.bar() });
+    assert_eq!(18, unsafe { (&valid_u32_thin as *const *const u32).complicated() });
+    let valid_u32_fat = valid_u32_thin as *const Foo;
+    assert_eq!("I'm a u32!", valid_u32_fat.foo());
+    assert_eq!(18, unsafe { valid_u32_fat.bar() });
 
 }
