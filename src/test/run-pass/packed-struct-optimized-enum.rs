087f1c23a70f889ea157c68b9db36c524e95ba8f
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2017-12-18 15:06:34 +0200

    rustc: ensure optimized enums have a properly aligned size.

--- src/test/run-pass/packed-struct-optimized-enum.rs
+++ src/test/run-pass/packed-struct-optimized-enum.rs
@@ -16,14 +16,21 @@ impl<T: Copy> Clone for Packed<T> {
     fn clone(&self) -> Self { *self }
 }
 
-fn main() {
-    let one = (Some(Packed((&(), 0))), true);
+fn sanity_check_size<T: Copy>(one: T) {
     let two = [one, one];
     let stride = (&two[1] as *const _ as usize) - (&two[0] as *const _ as usize);
+    assert_eq!(stride, std::mem::size_of_val(&one));
+}
 
+fn main() {
     // This can fail if rustc and LLVM disagree on the size of a type.
     // In this case, `Option<Packed<(&(), u32)>>` was erronously not
     // marked as packed despite needing alignment `1` and containing
     // its `&()` discriminant, which has alignment larger than `1`.
-    assert_eq!(stride, std::mem::size_of_val(&one));
+    sanity_check_size((Some(Packed((&(), 0))), true));
+
+    // In #46769, `Option<(Packed<&()>, bool)>` was found to have
+    // pointer alignment, without actually being aligned in size.
+    // E.g. on 64-bit platforms, it had alignment `8` but size `9`.
+    sanity_check_size(Some((Packed(&()), true)));
 }
