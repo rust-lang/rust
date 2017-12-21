c428b7d9c1e771d7a7184c99fd8c6471a00ddf2e
David Wood <david@davidtw.co>
2017-12-21 18:56:48 +0000

    Converted moves-based-on-type-tuple test and added MIR borrowck comparison.

--- src/test/ui/moves-based-on-type-tuple.rs
+++ src/test/ui/moves-based-on-type-tuple.rs
@@ -10,9 +10,14 @@
 
 #![feature(box_syntax)]
 
+// compile-flags: -Z emit-end-regions -Z borrowck=compare
+
 fn dup(x: Box<isize>) -> Box<(Box<isize>,Box<isize>)> {
-    box (x, x) //~ ERROR use of moved value
+    box (x, x)
+    //~^ use of moved value: `x` (Ast) [E0382]
+    //~| use of moved value: `x` (Mir) [E0382]
 }
+
 fn main() {
     dup(box 3);
 }
