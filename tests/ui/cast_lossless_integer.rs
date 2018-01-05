fded77d85e948bb7896f8c73e0a89b7a0199869c
Philipp Hansch <dev@phansch.net>
2018-01-06 00:23:28 +0100

    Split up casting UI tests
    Part of #2038

---
+++ tests/ui/cast_lossless_integer.rs
@@ -0,0 +1,24 @@
+
+#[warn(cast_lossless)]
+#[allow(no_effect, unnecessary_operation)]
+fn main() {
+    // Test cast_lossless with casts to integer types
+    1i8 as i16;
+    1i8 as i32;
+    1i8 as i64;
+    1u8 as i16;
+    1u8 as i32;
+    1u8 as i64;
+    1u8 as u16;
+    1u8 as u32;
+    1u8 as u64;
+    1i16 as i32;
+    1i16 as i64;
+    1u16 as i32;
+    1u16 as i64;
+    1u16 as u32;
+    1u16 as u64;
+    1i32 as i64;
+    1u32 as i64;
+    1u32 as u64;
+}
