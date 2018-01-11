bae2988e6f12560ebe86bbff9daa75a7e81d59dc
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-01-11 19:49:26 +0100

    Fix yield-while-local-borrowed.rs test

--- src/test/ui/generator/yield-while-local-borrowed.rs
+++ src/test/ui/generator/yield-while-local-borrowed.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// compile-flags: -Z borrowck=compare
+
 #![feature(generators, generator_trait)]
 
 use std::ops::{GeneratorState, Generator};
@@ -19,7 +21,9 @@ fn borrow_local_inline() {
     // (This error occurs because the region shows up in the type of
     // `b` and gets extended by region inference.)
     let mut b = move || {
-        let a = &3;
+        let a = &mut 3;
+        //~^ ERROR borrow may still be in use when generator yields (Ast)
+        //~| ERROR borrow may still be in use when generator yields (Mir)
         yield();
         println!("{}", a);
     };
@@ -30,7 +34,7 @@ fn borrow_local_inline_done() {
     // No error here -- `a` is not in scope at the point of `yield`.
     let mut b = move || {
         {
-            let a = &3;
+            let a = &mut 3;
         }
         yield();
     };
@@ -45,7 +49,9 @@ fn borrow_local() {
     let mut b = move || {
         let a = 3;
         {
-            let b = &a; //~ ERROR
+            let b = &a;
+            //~^ ERROR borrow may still be in use when generator yields (Ast)
+            //~| ERROR borrow may still be in use when generator yields (Mir)
             yield();
             println!("{}", b);
         }
