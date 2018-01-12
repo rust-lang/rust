8fda46477a724fb163ce1759917d77b76be041b0
Robin Kruppe <robin.kruppe@gmail.com>
2018-01-12 23:32:14 +0100

    Compute LLVM argument indices correctly in face of padding
    Closes #47278

--- src/librustc_trans/mir/mod.rs
+++ src/librustc_trans/mir/mod.rs
@@ -402,6 +402,9 @@ fn arg_local_refs<'a, 'tcx>(bcx: &Builder<'a, 'tcx>,
             for i in 0..tupled_arg_tys.len() {
                 let arg = &mircx.fn_ty.args[idx];
                 idx += 1;
+                if arg.pad.is_some() {
+                    llarg_idx += 1;
+                }
                 arg.store_fn_arg(bcx, &mut llarg_idx, place.project_field(bcx, i));
             }
 
