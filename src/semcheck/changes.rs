bbcc1005c0720e561631cf9163167abef90a284e
Inokentiy Babushkin <twk@twki.de>
2018-01-14 23:55:26 +0100

    Fixed build with newest rust nightly.

--- src/semcheck/changes.rs
+++ src/semcheck/changes.rs
@@ -735,7 +735,7 @@ pub mod tests {
             let b: u32 = Arbitrary::arbitrary(g);
             DefId_(DefId {
                 krate: CrateNum::new(a as usize),
-                index: DefIndex::from_u32(b),
+                index: DefIndex::from_raw_u32(b),
             })
         }
     }
