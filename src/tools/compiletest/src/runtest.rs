ebfa6c709aecb12a164695912785643d916e75fb
Mark Mansi <markm@cs.wisc.edu>
2018-01-14 21:11:40 -0600

    Change the --unpretty flag to -Z unpretty
    -Z unpretty no longer requires -Z unstable-options. Also, I mildly
    changed the syntax of the flag to match the other -Z flags. All uses of
    the flag take the form `unpretty=something` where something can either
    `string` or `string=string` (see the help messages of the CLI).

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -466,8 +466,7 @@ impl<'test> TestCx<'test> {
         let mut rustc = Command::new(&self.config.rustc_path);
         rustc
             .arg("-")
-            .arg("-Zunstable-options")
-            .args(&["--unpretty", &pretty_type])
+            .args(&["-Z", &format!("unpretty={}", pretty_type)])
             .args(&["--target", &self.config.target])
             .arg("-L")
             .arg(&aux_dir)
