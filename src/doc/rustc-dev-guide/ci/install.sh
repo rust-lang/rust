444fa3a721f32159d6e9a13d5e2db841582d9cf1
Niko Matsakis <niko@alum.mit.edu>
2018-01-19 14:48:35 -0500

    improvements to travis setup -- cache, use command -v

--- src/doc/rustc-dev-guide/ci/install.sh
+++ src/doc/rustc-dev-guide/ci/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-cargo install cargo install mdbook --vers "0.0.28"
+if command -v mdbook >/dev/null 2>&1; then
+    cargo install mdbook --vers "0.0.28"
+fi
 
 if command -v ghp-import >/dev/null 2>&1; then
     pip install ghp-import
