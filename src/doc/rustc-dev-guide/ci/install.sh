1513fa9c92cf33ac1ab315dc6bbd6110ea62be1c
Niko Matsakis <niko@alum.mit.edu>
2018-01-23 10:58:54 -0500

    invert the sense of `command -v`

--- src/doc/rustc-dev-guide/ci/install.sh
+++ src/doc/rustc-dev-guide/ci/install.sh
@@ -2,15 +2,15 @@
 set -ex
 
 if command -v mdbook >/dev/null 2>&1; then
+    echo "mdbook already installed at $(command -v mdbook)"
+else
     echo "installing mdbook"
     cargo install mdbook --vers "0.0.28"
-else
-    echo "mdbook already installed at $(which mdbook)"
 fi
 
 if command -v ghp-import >/dev/null 2>&1; then
+    echo "ghp-import already installed at $(which ghp-import)"
+else
     echo "installing ghp-import"
     pip install ghp-import
-else 
-    echo "ghp-import already installed at $(which ghp-import)"
 fi
