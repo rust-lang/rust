39a09d577be63a033334ca5e2cd8a3e0a9016d69
Niko Matsakis <niko@alum.mit.edu>
2018-01-23 21:45:25 -0500

    try to use travis's builtin deploy workflow

--- src/doc/rustc-dev-guide/ci/github_pages.sh
+++ src/doc/rustc-dev-guide/ci/github_pages.sh
@@ -6,7 +6,6 @@ BOOK_DIR=book
 # Only upload the built book to github pages if it's a commit to master
 if [ "$TRAVIS_BRANCH" = master -a "$TRAVIS_PULL_REQUEST" = false ]; then
     mdbook build 
-    ghp-import -p $BOOK_DIR
 else
     echo Skipping 'mdbook build' because this is not master or this is just a PR.
 fi
