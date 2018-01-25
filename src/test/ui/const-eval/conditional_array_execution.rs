cf056615a85a14f7a07f30f9e2a7ec3d13e9237c
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2018-01-25 14:15:12 +0100

    Add test for attempting to use array indexing for conditional const eval

---
+++ src/test/ui/const-eval/conditional_array_execution.rs
@@ -0,0 +1,17 @@
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+const X: u32 = 5;
+const Y: u32 = 6;
+const FOO: u32 = [X - Y, Y - X][(X < Y) as usize]; //~ E0080
+
+fn main() {
+    println!("{}", FOO); //~ E0080
+}
