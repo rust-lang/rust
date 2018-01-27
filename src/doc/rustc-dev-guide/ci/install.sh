173a1083ea65b498d27ede37d841b02a4c5e7e65
Michael Bryan <michaelfbryan@gmail.com>
2018-01-27 14:36:23 +0800

    Updated CI to install and use mdbook-linkcheck

--- src/doc/rustc-dev-guide/ci/install.sh
+++ src/doc/rustc-dev-guide/ci/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -ex
 
-if command -v mdbook >/dev/null 2>&1; then
-    echo "mdbook already installed at $(command -v mdbook)"
-else
-    echo "installing mdbook"
-    cargo install mdbook --vers "0.0.28"
-fi
+function cargo_install() {
+  local name=$1
+  local version=$2
+
+  if command -v $name >/dev/null 2>&1; then
+    echo "$name is already installed at $(command -v $name)"
+  else
+    echo "Installing $name"
+    cargo install $name --version $version
+  fi
+}
+
+cargo_install mdbook 0.1.1
+cargo_install mdbook-linkcheck 0.1.0
 
 if command -v ghp-import >/dev/null 2>&1; then
     echo "ghp-import already installed at $(which ghp-import)"
