f4f79038d103c77e36b9b4eb74f532a7f9598234
Christopher Durham <cad97@cad97.com>
2018-01-28 05:08:25 -0500

    Block Comments
    closes #7

--- src/lexer/comments.rs
+++ src/lexer/comments.rs
@@ -14,12 +14,35 @@ pub(crate) fn scan_shebang(ptr: &mut Ptr) -> bool {
     }
 }
 
+pub(crate) fn scan_block_comment(ptr: &mut Ptr) -> Option<SyntaxKind> {
+    if ptr.next_is('*') {
+        ptr.bump();
+        let mut depth: u32 = 1;
+        while depth > 0 {
+            if ptr.next_is('*') && ptr.nnext_is('/') {
+                depth -= 1;
+                ptr.bump();
+                ptr.bump();
+            } else if ptr.next_is('/') && ptr.nnext_is('*') {
+                depth += 1;
+                ptr.bump();
+                ptr.bump();
+            } else if ptr.bump().is_none() {
+                break;
+            }
+        }
+        Some(COMMENT)
+    } else {
+        None
+    }
+}
+
 pub(crate) fn scan_comment(ptr: &mut Ptr) -> Option<SyntaxKind> {
     if ptr.next_is('/') {
         bump_until_eol(ptr);
         Some(COMMENT)
     } else {
-        None
+        scan_block_comment(ptr)
     }
 }
 
