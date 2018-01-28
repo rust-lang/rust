6f8d263e876106a3c05a05b93fb78a4de07b37f5
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-01-29 01:59:34 +0200

    tests: replace "lvalue" terminology with "place".

--- src/test/run-pass/issue-18845.rs
+++ src/test/run-pass/issue-18845.rs
@@ -11,7 +11,7 @@
 // This used to generate invalid IR in that even if we took the
 // `false` branch we'd still try to free the Box from the other
 // arm. This was due to treating `*Box::new(9)` as an rvalue datum
-// instead of as an lvalue.
+// instead of as a place.
 
 fn test(foo: bool) -> u8 {
     match foo {
