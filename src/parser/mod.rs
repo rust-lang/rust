c74eaf1fdfbe027379ab4a4df715a917bcbffef1
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-02-04 13:56:51 +0300

    Simplify

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,14 +10,12 @@ use self::event::Event;
 
 /// Parse a sequence of tokens into the representative node tree
 pub fn parse(text: String, tokens: &[Token]) -> File {
-    let events = parse_into_events(&text, tokens);
+    let events = {
+        let mut parser = parser::Parser::new(&text, tokens);
+        grammar::file(&mut parser);
+        parser.into_events()
+    };
     event::to_file(text, tokens, events)
-}
-
-pub(crate) fn parse_into_events<'t>(text: &'t str, raw_tokens: &'t [Token]) -> Vec<Event> {
-    let mut parser = parser::Parser::new(text, raw_tokens);
-    grammar::file(&mut parser);
-    parser.into_events()
 }
 
 fn is_insignificant(kind: SyntaxKind) -> bool {
