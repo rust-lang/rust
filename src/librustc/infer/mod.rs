afb4010dba6e3ee8f2aa9d7ecfe87405a6c87311
Sean Griffin <sean@seantheprogrammer.com>
2018-02-08 13:14:24 -0700

    Wrap `InferCtxt::universe` in a cell
    We'll need this in order to start tracking skolemizatoins here, and it's
    easier to update all the field accesses now rather than later.

--- src/librustc/infer/mod.rs
+++ src/librustc/infer/mod.rs
@@ -191,7 +191,7 @@ pub struct InferCtxt<'a, 'gcx: 'a+'tcx, 'tcx: 'a> {
     /// part of the root universe. So this would only get incremented
     /// when we enter into a higher-ranked (`for<..>`) type or trait
     /// bound.
-    pub universe: ty::UniverseIndex,
+    universe: Cell<ty::UniverseIndex>,
 }
 
 /// A map returned by `skolemize_late_bound_regions()` indicating the skolemized
@@ -464,7 +464,7 @@ impl<'a, 'gcx, 'tcx> InferCtxtBuilder<'a, 'gcx, 'tcx> {
             err_count_on_creation: tcx.sess.err_count(),
             in_snapshot: Cell::new(false),
             region_obligations: RefCell::new(vec![]),
-            universe: ty::UniverseIndex::ROOT,
+            universe: Cell::new(ty::UniverseIndex::ROOT),
         }))
     }
 }
@@ -850,7 +850,7 @@ impl<'a, 'gcx, 'tcx> InferCtxt<'a, 'gcx, 'tcx> {
     pub fn next_ty_var_id(&self, diverging: bool, origin: TypeVariableOrigin) -> TyVid {
         self.type_variables
             .borrow_mut()
-            .new_var(self.universe, diverging, origin)
+            .new_var(self.universe(), diverging, origin)
     }
 
     pub fn next_ty_var(&self, origin: TypeVariableOrigin) -> Ty<'tcx> {
@@ -882,7 +882,7 @@ impl<'a, 'gcx, 'tcx> InferCtxt<'a, 'gcx, 'tcx> {
     pub fn next_region_var(&self, origin: RegionVariableOrigin)
                            -> ty::Region<'tcx> {
         let region_var = self.borrow_region_constraints()
-            .new_region_var(self.universe, origin);
+            .new_region_var(self.universe(), origin);
         self.tcx.mk_region(ty::ReVar(region_var))
     }
 
@@ -920,7 +920,7 @@ impl<'a, 'gcx, 'tcx> InferCtxt<'a, 'gcx, 'tcx> {
                             -> Ty<'tcx> {
         let ty_var_id = self.type_variables
                             .borrow_mut()
-                            .new_var(self.universe,
+                            .new_var(self.universe(),
                                      false,
                                      TypeVariableOrigin::TypeParameterDefinition(span, def.name));
 
@@ -1334,6 +1334,10 @@ impl<'a, 'gcx, 'tcx> InferCtxt<'a, 'gcx, 'tcx> {
         self.selection_cache.clear();
         self.evaluation_cache.clear();
         self.projection_cache.borrow_mut().clear();
+    }
+
+    fn universe(&self) -> ty::UniverseIndex {
+        self.universe.get()
     }
 }
 
