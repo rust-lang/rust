6feb0dd9824e81c2ee9a2389e53e564d25bdca2d
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2018-02-09 15:23:51 +0100

    Fixes #2426 (if_same_then_else false positive)

---
+++ tests/run-pass/if_same_then_else.rs
@@ -0,0 +1,13 @@
+#![deny(if_same_then_else)]
+
+fn main() {}
+
+pub fn foo(a: i32, b: i32) -> Option<&'static str> {
+    if a == b {
+        None
+    } else if a > b {
+        Some("a pfeil b")
+    } else {
+        None
+    }
+}
