20dcc72127b53e1b7388f430f7ed26144fe5dcf4
csmoe <35686186+csmoe@users.noreply.github.com>
2018-02-14 11:06:08 +0800

    inform type annotations

--- src/test/compile-fail/match-vec-mismatch.rs
+++ src/test/compile-fail/match-vec-mismatch.rs
@@ -43,6 +43,6 @@ fn main() {
 fn another_fn_to_avoid_suppression() {
     match Default::default()
     {
-        [] => {}  //~ ERROR the type of this value
+        [] => {}  //~ ERROR type annotations needed
     };
 }
