63843d8de4bb4c6ab2c5ad2708847abdff886099
Wesley Wiser <wwiser@gmail.com>
2018-02-15 20:38:26 -0500

    Fix how paths are printed by error messages during bootstrap
    Fixes #47355

--- src/bootstrap/bootstrap.py
+++ src/bootstrap/bootstrap.py
@@ -720,7 +720,7 @@ def bootstrap():
                 registry = 'https://example.com'
 
                 [source.vendored-sources]
-                directory = '{}/src/vendor'
+                directory = '{}/vendor'
             """.format(build.rust_root))
     else:
         if os.path.exists('.cargo'):
