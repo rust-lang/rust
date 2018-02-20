f3d1a0cec2222b6cad88ddaa3159235950c4dc7d
bootandy <bootandy@gmail.com>
2018-02-20 12:37:30 -0500

    Add newlines in unwrap_or ui test

--- tests/ui/unwrap_or.rs
+++ tests/ui/unwrap_or.rs
@@ -3,3 +3,9 @@
 fn main() {
     let s = Some(String::from("test string")).unwrap_or("Fail".to_string()).len();
 }
+
+fn new_lines() {
+    let s = Some(String::from("test string"))
+        .unwrap_or("Fail".to_string())
+        .len();
+}
