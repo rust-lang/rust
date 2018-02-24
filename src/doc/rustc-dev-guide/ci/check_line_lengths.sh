b5bd9cbb9b8b2228858808dd674626f6cac8b05e
Alexander Regueiro <alexreg@me.com>
2018-02-24 01:43:36 +0000

    Ignore line check on certain types of lines or in certain blocks.

--- src/doc/rustc-dev-guide/ci/check_line_lengths.sh
+++ src/doc/rustc-dev-guide/ci/check_line_lengths.sh
@@ -3,17 +3,23 @@
 echo "Checking line lengths in all source files <= $MAX_LINE_LENGTH chars..."
 
 echo "Offending files and lines:"
-(( success = 1 ))
+(( bad_lines = 0 ))
+(( inside_block = 0 ))
 for file in "$@" ; do
   echo "$file"
   (( line_no = 0 ))
   while IFS="" read -r line || [[ -n "$line" ]] ; do
     (( line_no++ ))
-    if (( "${#line}" > $MAX_LINE_LENGTH )) ; then
-      (( success = 0 ))
+    if [[ "$line" =~ ^'```' ]] ; then
+      (( inside_block = !$inside_block ))
+      continue
+    fi
+    if ! (( $inside_block )) && ! [[ "$line" =~ " | "|"://"|\[\^[^\ ]+\]: ]] && (( "${#line}" > $MAX_LINE_LENGTH )) ; then
+      (( bad_lines++ ))
       echo -e "\t$line_no : $line"
     fi
   done < "$file"
 done
 
-(( $success )) && echo "No offending lines found."
+echo "$bad_lines offending lines found."
+(( $bad_lines == 0 ))
