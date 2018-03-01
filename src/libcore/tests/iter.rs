11fefeb61c959c6a964120036992aed34f9d4282
Scott McMurray <scottmcm@users.noreply.github.com>
2018-03-01 02:17:50 -0800

    Add a Zip::nth test for side effects

--- src/libcore/tests/iter.rs
+++ src/libcore/tests/iter.rs
@@ -162,6 +162,26 @@ fn test_zip_nth() {
 }
 
 #[test]
+fn test_zip_nth_side_effects() {
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+    let value = [1, 2, 3, 4, 5, 6].iter().cloned()
+        .map(|n| {
+            a.push(n);
+            n * 10
+        })
+        .zip([2, 3, 4, 5, 6, 7, 8].iter().cloned().map(|n| {
+            b.push(n * 100);
+            n * 1000
+        }))
+        .skip(1)
+        .nth(3);
+    assert_eq!(value, Some((50, 6000)));
+    assert_eq!(a, vec![1, 2, 3, 4, 5]);
+    assert_eq!(b, vec![200, 300, 400, 500, 600]);
+}
+
+#[test]
 fn test_iterator_step_by() {
     // Identity
     let mut it = (0..).step_by(1).take(3);
