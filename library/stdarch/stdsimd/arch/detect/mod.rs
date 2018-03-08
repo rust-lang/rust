26fd3bb5a9f05e06695a5d19b16229739ca089d8
gnzlbg <gnzlbg@users.noreply.github.com>
2018-03-08 09:59:21 +0100

    better error messages for target-feature detection macros (#352)
    Better error messages for target-feature detection macros

--- library/stdarch/stdsimd/arch/detect/mod.rs
+++ library/stdarch/stdsimd/arch/detect/mod.rs
@@ -36,3 +36,72 @@ pub use self::arch::Feature;
 pub fn check_for(x: Feature) -> bool {
     cache::test(x as u32, arch::detect_features)
 }
+
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+#[macro_export]
+#[unstable(feature = "stdsimd", issue = "0")]
+macro_rules! is_x86_feature_detected {
+    ($t:tt) => {
+        compile_error!(r#"
+is_x86_feature_detected can only be used on x86 and x86_64 targets.
+You can prevent it from being used in other architectures by
+guarding it behind a cfg(target_arch) as follows:
+
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
+        if is_x86_feature_detected(...) { ... }
+    }
+"#)
+    };
+}
+
+#[cfg(not(target_arch = "arm"))]
+#[macro_export]
+#[unstable(feature = "stdsimd", issue = "0")]
+macro_rules! is_arm_feature_detected {
+    ($t:tt) => {
+        compile_error!(r#"
+is_arm_feature_detected can only be used on ARM targets.
+You can prevent it from being used in other architectures by
+guarding it behind a cfg(target_arch) as follows:
+
+    #[cfg(target_arch = "arm")] {
+        if is_arm_feature_detected(...) { ... }
+    }
+"#)
+    };
+}
+
+#[cfg(not(target_arch = "aarch64"))]
+#[macro_export]
+#[unstable(feature = "stdsimd", issue = "0")]
+macro_rules! is_aarch64_feature_detected {
+    ($t:tt) => {
+        compile_error!(r#"
+is_aarch64_feature_detected can only be used on AArch64 targets.
+You can prevent it from being used in other architectures by
+guarding it behind a cfg(target_arch) as follows:
+
+    #[cfg(target_arch = "aarch64")] {
+        if is_aarch64_feature_detected(...) { ... }
+    }
+"#)
+    };
+}
+
+#[cfg(not(target_arch = "powerpc64"))]
+#[macro_export]
+#[unstable(feature = "stdsimd", issue = "0")]
+macro_rules! is_powerpc64_feature_detected {
+    ($t:tt) => {
+        compile_error!(r#"
+is_powerpc64_feature_detected can only be used on PowerPC64 targets.
+You can prevent it from being used in other architectures by
+guarding it behind a cfg(target_arch) as follows:
+
+    #[cfg(target_arch = "powerpc64")] {
+        if is_powerpc64_feature_detected(...) { ... }
+    }
+"#)
+    };
+}
+
