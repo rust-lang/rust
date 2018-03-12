036e0d7943f274fc3269a9cd67d2c922e397fcaf
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-12 20:45:35 +0100

    librustc_trans: disable profiling pre-inlining.
    It destroys performance actually.
    
    Signed-off-by: Emilio Cobos Álvarez <emilio@crisal.io>

--- src/librustc_trans/llvm_util.rs
+++ src/librustc_trans/llvm_util.rs
@@ -61,6 +61,7 @@ unsafe fn configure_llvm(sess: &Session) {
         add("rustc"); // fake program name
         if sess.time_llvm_passes() { add("-time-passes"); }
         if sess.print_llvm_passes() { add("-debug-pass=Structure"); }
+        add("-disable-preinline");
 
         for arg in &sess.opts.cg.llvm_args {
             add(&(*arg));
