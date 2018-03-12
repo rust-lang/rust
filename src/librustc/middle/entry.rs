b08e6d305f8366c8fa7e08d829bb0c6939124759
Eric Huss <eric@huss.org>
2018-03-12 13:23:12 -0700

    Add suggestion where to add main function.

--- src/librustc/middle/entry.rs
+++ src/librustc/middle/entry.rs
@@ -178,6 +178,9 @@ fn configure_main(this: &mut EntryContext, crate_name: &str) {
             err.emit();
             this.session.abort_if_errors();
         } else {
+            if let Some(ref filename) = this.session.local_crate_source_file {
+                err.note(&format!("consider adding a main function to {}", filename.display()));
+            }
             if this.session.teach(&err.get_code().unwrap()) {
                 err.note("If you don't know the basics of Rust, you can go look to the Rust Book \
                           to get started: https://doc.rust-lang.org/book/");
