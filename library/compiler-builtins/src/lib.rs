b90fa1e14fa86fe402c6d1c1e08384947c19a668
Mark Mansi <markm@cs.wisc.edu>
2018-03-18 14:07:01 -0500

    Put back feature

--- library/compiler-builtins/src/lib.rs
+++ library/compiler-builtins/src/lib.rs
@@ -17,6 +17,7 @@
 #![feature(linkage)]
 #![feature(lang_items)]
 #![allow(unused_features)]
+#![feature(i128_type)]
 #![no_builtins]
 #![cfg_attr(feature = "compiler-builtins", feature(staged_api))]
 #![cfg_attr(feature = "compiler-builtins",
