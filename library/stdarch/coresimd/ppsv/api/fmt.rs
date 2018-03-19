f107499c51420ab8a00f99a2b07c5ce6ad6d3de6
gnzlbg <gnzlbg@users.noreply.github.com>
2018-03-19 15:44:57 +0100

    add missing fmt implementations (#380)

--- library/stdarch/coresimd/ppsv/api/fmt.rs
+++ library/stdarch/coresimd/ppsv/api/fmt.rs
@@ -19,6 +19,45 @@ macro_rules! impl_hex_fmt {
                 write!(f, ")")
             }
         }
+        impl ::fmt::UpperHex for $id {
+            fn fmt(&self, f: &mut ::fmt::Formatter)
+                   -> ::fmt::Result {
+                write!(f, "{}(", stringify!($id))?;
+                for i in 0..$id::lanes() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    self.extract(i).fmt(f)?;
+                }
+                write!(f, ")")
+            }
+        }
+        impl ::fmt::Octal for $id {
+            fn fmt(&self, f: &mut ::fmt::Formatter)
+                   -> ::fmt::Result {
+                write!(f, "{}(", stringify!($id))?;
+                for i in 0..$id::lanes() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    self.extract(i).fmt(f)?;
+                }
+                write!(f, ")")
+            }
+        }
+        impl ::fmt::Binary for $id {
+            fn fmt(&self, f: &mut ::fmt::Formatter)
+                   -> ::fmt::Result {
+                write!(f, "{}(", stringify!($id))?;
+                for i in 0..$id::lanes() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    self.extract(i).fmt(f)?;
+                }
+                write!(f, ")")
+            }
+        }
     }
 }
 
@@ -41,6 +80,63 @@ macro_rules! test_hex_fmt_impl {
                 assert_eq!(s.len(), $id::lanes());
                 for (index, ss) in s.into_iter().enumerate() {
                     assert_eq!(ss, format!("{:#x}", vec.extract(index)));
+                }
+            }
+        }
+        #[test]
+        fn upper_hex_fmt() {
+            use ::std::prelude::v1::*;
+            use ::coresimd::simd::$id;
+            for &i in [$($values),+].iter() {
+                let vec = $id::splat(i as $elem_ty);
+
+                let s = format!("{:#X}", vec);
+                let beg = format!("{}(", stringify!($id));
+                assert!(s.starts_with(&beg));
+                assert!(s.ends_with(")"));
+                let s: Vec<String> = s.replace(&beg, "").replace(")", "").split(",")
+                    .map(|v| v.trim().to_string()).collect();
+                assert_eq!(s.len(), $id::lanes());
+                for (index, ss) in s.into_iter().enumerate() {
+                    assert_eq!(ss, format!("{:#X}", vec.extract(index)));
+                }
+            }
+        }
+        #[test]
+        fn octal_fmt() {
+            use ::std::prelude::v1::*;
+            use ::coresimd::simd::$id;
+            for &i in [$($values),+].iter() {
+                let vec = $id::splat(i as $elem_ty);
+
+                let s = format!("{:#o}", vec);
+                let beg = format!("{}(", stringify!($id));
+                assert!(s.starts_with(&beg));
+                assert!(s.ends_with(")"));
+                let s: Vec<String> = s.replace(&beg, "").replace(")", "").split(",")
+                    .map(|v| v.trim().to_string()).collect();
+                assert_eq!(s.len(), $id::lanes());
+                for (index, ss) in s.into_iter().enumerate() {
+                    assert_eq!(ss, format!("{:#o}", vec.extract(index)));
+                }
+            }
+        }
+        #[test]
+        fn binary_fmt() {
+            use ::std::prelude::v1::*;
+            use ::coresimd::simd::$id;
+            for &i in [$($values),+].iter() {
+                let vec = $id::splat(i as $elem_ty);
+
+                let s = format!("{:#b}", vec);
+                let beg = format!("{}(", stringify!($id));
+                assert!(s.starts_with(&beg));
+                assert!(s.ends_with(")"));
+                let s: Vec<String> = s.replace(&beg, "").replace(")", "").split(",")
+                    .map(|v| v.trim().to_string()).collect();
+                assert_eq!(s.len(), $id::lanes());
+                for (index, ss) in s.into_iter().enumerate() {
+                    assert_eq!(ss, format!("{:#b}", vec.extract(index)));
                 }
             }
         }
