fcca22cb4072097fc2cd1ae78ff84c7d59aacda2
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-03-20 22:19:52 +0200

    tests: move all proc_macro tests from -fulldeps.

--- src/bootstrap/test.rs
+++ src/bootstrap/test.rs
@@ -839,12 +839,6 @@ host_test!(CompileFailFullDeps {
     suite: "compile-fail-fulldeps"
 });
 
-host_test!(IncrementalFullDeps {
-    path: "src/test/incremental-fulldeps",
-    mode: "incremental",
-    suite: "incremental-fulldeps"
-});
-
 host_test!(Rustdoc {
     path: "src/test/rustdoc",
     mode: "rustdoc",
