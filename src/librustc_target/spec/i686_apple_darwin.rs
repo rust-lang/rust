969449f236bf8fe47b5bc90d4f02a3db00a80643
Simonas Kazlauskas <git@kazlauskas.me>
2018-03-24 21:54:21 +0200

    Don’t eliminate frame pointers on apple by default

--- src/librustc_target/spec/i686_apple_darwin.rs
+++ src/librustc_target/spec/i686_apple_darwin.rs
@@ -16,6 +16,7 @@ pub fn target() -> TargetResult {
     base.max_atomic_width = Some(64);
     base.pre_link_args.insert(LinkerFlavor::Gcc, vec!["-m32".to_string()]);
     base.stack_probes = true;
+    base.eliminate_frame_pointer = false;
 
     Ok(Target {
         llvm_target: "i686-apple-darwin".to_string(),
