9e6991ce498e81b25854263696138f3b465865d7
Alexander Ronald Altman <alexanderaltman@me.com>
2018-03-25 01:29:57 -0500

    Modify tests

--- src/test/compile-fail/rfc-1937-termination-trait/termination-trait-main-i32.rs
+++ src/test/compile-fail/rfc-1937-termination-trait/termination-trait-main-i32.rs
@@ -10,7 +10,7 @@
 
 fn main() -> i32 {
 //~^ ERROR `main` has invalid return type `i32`
-//~| NOTE `main` can only return types that implement std::process::Termination
+//~| NOTE `main` can only return types that implement `std::process::Termination`
 //~| HELP consider using `()`, or a `Result`
     0
 }
