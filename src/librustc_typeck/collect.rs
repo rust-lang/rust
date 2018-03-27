38d48ef53778674baa99dd0a3a193cec78f74e63
Alex Crichton <alex@alexcrichton.com>
2018-03-27 11:42:04 -0700

    rustc: Forbid #[inline(always)] with #[target_feature]
    Once a target feature is enabled for a function that means that it in general
    can't be inlined into other functions which don't have that target feature
    enabled. This can cause both safety and LLVM issues if we were to actually
    inline it, so `#[inline(always)]` both can't be respected and would be an error
    if we did so!
    
    Today LLVM doesn't inline functions with different `#[target_feature]`
    annotations, but it turns out that if one is tagged with `#[inline(always)]`
    it'll override this and cause scary LLVM error to arise!
    
    This commit fixes this issue by forbidding these two attributes to be used in
    conjunction with one another.
    
    cc rust-lang-nursery/stdsimd#404

--- src/librustc_typeck/collect.rs
+++ src/librustc_typeck/collect.rs
@@ -1769,6 +1769,7 @@ fn trans_fn_attrs<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, id: DefId) -> TransFnAt
 
     let whitelist = tcx.target_features_whitelist(LOCAL_CRATE);
 
+    let mut inline_span = None;
     for attr in attrs.iter() {
         if attr.check_name("cold") {
             trans_fn_attrs.flags |= TransFnAttrFlags::COLD;
@@ -1800,6 +1801,7 @@ fn trans_fn_attrs<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, id: DefId) -> TransFnAt
                     }
                     MetaItemKind::List(ref items) => {
                         mark_used(attr);
+                        inline_span = Some(attr.span);
                         if items.len() != 1 {
                             span_err!(tcx.sess.diagnostic(), attr.span, E0534,
                                         "expected one argument");
@@ -1851,6 +1853,19 @@ fn trans_fn_attrs<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, id: DefId) -> TransFnAt
         } else if attr.check_name("linkage") {
             if let Some(val) = attr.value_str() {
                 trans_fn_attrs.linkage = Some(linkage_by_name(tcx, id, &val.as_str()));
+            }
+        }
+    }
+
+    // If a function uses #[target_feature] it can't be inlined into general
+    // purpose functions as they wouldn't have the right target features
+    // enabled. For that reason we also forbid #[inline(always)] as it can't be
+    // respected.
+    if trans_fn_attrs.target_features.len() > 0 {
+        if trans_fn_attrs.inline == InlineAttr::Always {
+            if let Some(span) = inline_span {
+                tcx.sess.span_err(span, "cannot use #[inline(always)] with \
+                                         #[target_feature]");
             }
         }
     }
