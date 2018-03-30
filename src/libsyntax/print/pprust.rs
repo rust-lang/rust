679657b863c2a53a3052d8af9defbce48e12db10
Oliver Schneider <git-no-reply-9879165716479413131@oli-obk.de>
2018-03-30 13:06:34 +0200

    Inject the `compiler_builtins` crate whenever the `core` crate is injected

--- src/libsyntax/print/pprust.rs
+++ src/libsyntax/print/pprust.rs
@@ -94,7 +94,7 @@ pub fn print_crate<'a>(cm: &'a CodeMap,
                        is_expanded: bool) -> io::Result<()> {
     let mut s = State::new_from_input(cm, sess, filename, input, out, ann, is_expanded);
 
-    if is_expanded && !std_inject::injected_crate_name().is_none() {
+    if is_expanded && std_inject::injected_crate_name().is_some() {
         // We need to print `#![no_std]` (and its feature gate) so that
         // compiling pretty-printed source won't inject libstd again.
         // However we don't want these attributes in the AST because
