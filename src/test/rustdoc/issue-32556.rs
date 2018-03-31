42bc0712f127906bca9063a6659c18aea2ddd9da
Phlosioneer <mattmdrr2@gmail.com>
2018-03-31 16:08:58 -0400

    Remove whitespace

--- src/test/rustdoc/issue-32556.rs
+++ src/test/rustdoc/issue-32556.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -9,7 +9,7 @@
 // except according to those terms.
 
 /// Blah blah blah
-/// ``` ignore
+/// ```ignore
 /// bad_assert!();
 /// ```
 pub fn foo() {}
