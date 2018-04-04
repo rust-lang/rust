238583a50f0edc6991091dc3e9e6cae835a856ae
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-04-04 16:16:53 +0200

    Fix anchors issue when everything is collapsed

--- src/librustdoc/html/static/storage.js
+++ src/librustdoc/html/static/storage.js
@@ -19,10 +19,11 @@ function onEach(arr, func) {
     if (arr && arr.length > 0 && func) {
         for (var i = 0; i < arr.length; i++) {
             if (func(arr[i]) === true) {
-                break;
+                return true;
             }
         }
     }
+    return false;
 }
 
 function updateLocalStorage(name, value) {
