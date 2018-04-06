e18a83be8846010f3dbb1ef96b86941210517cc9
gaurikholkar <f2013002@goa.bits-pilani.ac.in>
2018-04-06 23:03:20 +0530

    fix ui test

--- src/test/ui/nll/issue-47388.rs
+++ src/test/ui/nll/issue-47388.rs
@@ -15,6 +15,6 @@ struct FancyNum {
 fn main() {
     let mut fancy = FancyNum{ num: 5 };
     let fancy_ref = &(&mut fancy);
-    fancy_ref.num = 6; //~^ ERROR E0594
+    fancy_ref.num = 6; //~ ERROR E0594
     println!("{}", fancy_ref.num);
 }
