5abe34832d80feca118b09aac3077200c3092763
Philipp Hansch <dev@phansch.net>
2018-04-07 10:23:27 +0200

    Split up match_bool UI tests

--- tests/ui/matches.rs
+++ tests/ui/matches.rs
@@ -24,48 +24,6 @@ fn unwrap_addr() -> Option<&'static ExprNode> {
     }
 }
 
-fn match_bool() {
-    let test: bool = true;
-
-    match test {
-        true => 0,
-        false => 42,
-    };
-
-    let option = 1;
-    match option == 1 {
-        true => 1,
-        false => 0,
-    };
-
-    match test {
-        true => (),
-        false => { println!("Noooo!"); }
-    };
-
-    match test {
-        false => { println!("Noooo!"); }
-        _ => (),
-    };
-
-    match test && test {
-        false => { println!("Noooo!"); }
-        _ => (),
-    };
-
-    match test {
-        false => { println!("Noooo!"); }
-        true => { println!("Yes!"); }
-    };
-
-    // Not linted
-    match option {
-        1 ... 10 => 1,
-        11 ... 20 => 2,
-        _ => 3,
-    };
-}
-
 fn ref_pats() {
     {
         let v = &Some(0);
