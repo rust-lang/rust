90e7d93d6cb333fea13383aa34d93b4fde7fbffc
Philipp Hansch <dev@phansch.net>
2018-04-07 12:52:18 +0200

    Fix nonminimal_bool false positive
    It was checking any is_ok, is_err, is_some, is_none method for negation
    but it should only perform the check for the built-in types, not custom
    types.

--- tests/ui/booleans.rs
+++ tests/ui/booleans.rs
@@ -57,3 +57,60 @@ fn methods_with_negation() {
     let _ = (!c ^ c) || !a.is_some();
     let _ = !c ^ c || !a.is_some();
 }
+
+// Simplified versions of https://github.com/rust-lang-nursery/rust-clippy/issues/2638
+// nonminimal_bool should only check the built-in Result and Some type, not
+// any other types like the following.
+enum CustomResultOk<E> { Ok, Err(E) }
+enum CustomResultErr<E> { Ok, Err(E) }
+enum CustomSomeSome<T> { Some(T), None }
+enum CustomSomeNone<T> { Some(T), None }
+
+impl<E> CustomResultOk<E> {
+    pub fn is_ok(&self) -> bool { true }
+}
+
+impl<E> CustomResultErr<E> {
+    pub fn is_err(&self) -> bool { true }
+}
+
+impl<T> CustomSomeSome<T> {
+    pub fn is_some(&self) -> bool { true }
+}
+
+impl<T> CustomSomeNone<T> {
+    pub fn is_none(&self) -> bool { true }
+}
+
+fn dont_warn_for_custom_methods_with_negation() {
+    let res = CustomResultOk::Err("Error");
+    // Should not warn and suggest 'is_err()' because the type does not
+    // implement is_err().
+    if !res.is_ok() { }
+
+    let res = CustomResultErr::Err("Error");
+    // Should not warn and suggest 'is_ok()' because the type does not
+    // implement is_ok().
+    if !res.is_err() { }
+
+    let res = CustomSomeSome::Some("thing");
+    // Should not warn and suggest 'is_none()' because the type does not
+    // implement is_none().
+    if !res.is_some() { }
+
+    let res = CustomSomeNone::Some("thing");
+    // Should not warn and suggest 'is_some()' because the type does not
+    // implement is_some().
+    if !res.is_none() { }
+}
+
+// Only Built-in Result and Some types should suggest the negated alternative
+fn warn_for_built_in_methods_with_negation() {
+    let res: Result<usize, usize> = Ok(1);
+    if !res.is_ok() { }
+    if !res.is_err() { }
+
+    let res = Some(1);
+    if !res.is_some() { }
+    if !res.is_none() { }
+}
