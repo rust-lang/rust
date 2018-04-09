53718d2ef1729c987531dc94971c905815daef55
Mark Simulacrum <mark.simulacrum@gmail.com>
2018-04-09 11:21:43 -0600

    Allow incorrectly reported unused attribute warning

--- src/rustc/rustc.rs
+++ src/rustc/rustc.rs
@@ -13,6 +13,7 @@
 
 // Set the stack size at link time on Windows. See rustc_driver::in_rustc_thread
 // for the rationale.
+#[allow(unused_attributes)]
 #[cfg_attr(all(windows, target_env = "msvc"), link_args = "/STACK:16777216")]
 // We only build for msvc and gnu now, but we use a exhaustive condition here
 // so we can expect either the stack size to be set or the build fails.
