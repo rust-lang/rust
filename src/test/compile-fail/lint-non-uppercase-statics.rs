6e0089ea7776eea84fb26690b590074710247146
varkor <github@varkor.com>
2018-04-10 22:30:23 +0100

    Do not uppercase-lint no_mangle statics

--- src/test/compile-fail/lint-non-uppercase-statics.rs
+++ src/test/compile-fail/lint-non-uppercase-statics.rs
@@ -16,4 +16,7 @@ static foo: isize = 1; //~ ERROR static variable `foo` should have an upper case
 static mut bar: isize = 1;
         //~^ ERROR static variable `bar` should have an upper case name such as `BAR`
 
+#[no_mangle]
+pub static extern_foo: isize = 1; // OK, because #[no_mangle] supersedes the warning
+
 fn main() { }
