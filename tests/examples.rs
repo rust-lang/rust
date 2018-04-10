9d83cae89cae7337bf476b50068c74e2df9e7900
Inokentiy Babushkin <twk@twki.de>
2018-04-10 13:45:12 +0200

    Restructured test hierarchy a bit.

--- tests/examples.rs
+++ tests/examples.rs
@@ -1,130 +1,132 @@
-use std::env;
-use std::fs::File;
-use std::os::unix::io::{AsRawFd, FromRawFd};
-use std::path::Path;
-use std::process::{Command, Stdio};
+mod features {
+    use std::env;
+    use std::fs::File;
+    use std::os::unix::io::{AsRawFd, FromRawFd};
+    use std::path::Path;
+    use std::process::{Command, Stdio};
 
-macro_rules! test {
-    ($name:ident) => {
-        #[test]
-        fn $name() {
-            let mut success = true;
+    macro_rules! test {
+        ($name:ident) => {
+            #[test]
+            fn $name() {
+                let mut success = true;
 
-            let current_dir = env::current_dir().expect("could not determine current dir");
-            let subst = format!("s#{}#$REPO_PATH#g", current_dir.to_str().unwrap());
-            let path = Path::new("tests/cases").join(stringify!($name));
+                let current_dir = env::current_dir().expect("could not determine current dir");
+                let subst = format!("s#{}#$REPO_PATH#g", current_dir.to_str().unwrap());
+                let path = Path::new("tests/cases").join(stringify!($name));
 
-            let out_file = path.join("stdout");
+                let out_file = path.join("stdout");
 
-            let old_rlib = path.join("libold.rlib").to_str().unwrap().to_owned();
-            let new_rlib = path.join("libnew.rlib").to_str().unwrap().to_owned();
+                let old_rlib = path.join("libold.rlib").to_str().unwrap().to_owned();
+                let new_rlib = path.join("libnew.rlib").to_str().unwrap().to_owned();
 
-            {
-                let stdout = File::create(&out_file).expect("could not create `stdout` file");
+                {
+                    let stdout = File::create(&out_file).expect("could not create `stdout` file");
 
-                success &= Command::new("rustc")
-                    .args(&["--crate-type=lib", "-o", &old_rlib])
-                    .arg(path.join("old.rs"))
-                    .env("RUST_BACKTRACE", "full")
-                    .stdin(Stdio::null())
+                    success &= Command::new("rustc")
+                        .args(&["--crate-type=lib", "-o", &old_rlib])
+                        .arg(path.join("old.rs"))
+                        .env("RUST_BACKTRACE", "full")
+                        .stdin(Stdio::null())
+                        .status()
+                        .expect("could not run rustc")
+                        .success();
+
+                    assert!(success, "couldn't compile old");
+
+                    success &= Command::new("rustc")
+                        .args(&["--crate-type=lib", "-o", &new_rlib])
+                        .arg(path.join("new.rs"))
+                        .env("RUST_BACKTRACE", "full")
+                        .stdin(Stdio::null())
+                        .status()
+                        .expect("could not run rustc")
+                        .success();
+
+                    assert!(success, "couldn't compile new");
+
+                    let mut sed_child = Command::new("sed")
+                        .arg(&subst)
+                        .stdin(Stdio::piped())
+                        .stdout(stdout)
+                        .spawn()
+                        .expect("could not run sed");
+
+                    let (err_pipe, out_pipe) = if let Some(ref stdin) = sed_child.stdin {
+                        let fd = stdin.as_raw_fd();
+                        unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
+                    } else {
+                        panic!("could not pipe to sed");
+                    };
+
+                    success &= Command::new("./target/debug/rust-semverver")
+                        .args(&["--crate-type=lib", "-Zverbose",
+                                "--extern", &format!("old={}", old_rlib),
+                                "--extern", &format!("new={}", new_rlib),
+                                "tests/helper/test.rs"])
+                        .env("RUST_BACKTRACE", "full")
+                        .env("RUST_SEMVER_CRATE_VERSION", "1.0.0")
+                        .stdin(Stdio::null())
+                        .stdout(out_pipe)
+                        .stderr(err_pipe)
+                        .status()
+                        .expect("could not run rust-semverver")
+                        .success();
+
+                    assert!(success, "rust-semverver");
+
+                    success &= sed_child.wait().expect("could not wait for sed child").success();
+                }
+
+                assert!(success, "sed");
+
+                eprintln!("path: {}", out_file.to_str().unwrap());
+                success &= Command::new("git")
+                    .args(&["diff", "--exit-code", out_file.to_str().unwrap()])
+                    .env("PAGER", "")
                     .status()
-                    .expect("could not run rustc")
+                    .expect("could not run git diff")
                     .success();
 
-                assert!(success, "couldn't compile old");
+                assert!(success, "git");
 
-                success &= Command::new("rustc")
-                    .args(&["--crate-type=lib", "-o", &new_rlib])
-                    .arg(path.join("new.rs"))
-                    .env("RUST_BACKTRACE", "full")
-                    .stdin(Stdio::null())
+                Command::new("rm")
+                    .args(&[&old_rlib, &new_rlib])
                     .status()
-                    .expect("could not run rustc")
-                    .success();
-
-                assert!(success, "couldn't compile new");
-
-                let mut sed_child = Command::new("sed")
-                    .arg(&subst)
-                    .stdin(Stdio::piped())
-                    .stdout(stdout)
-                    .spawn()
-                    .expect("could not run sed");
-
-                let (err_pipe, out_pipe) = if let Some(ref stdin) = sed_child.stdin {
-                    let fd = stdin.as_raw_fd();
-                    unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
-                } else {
-                    panic!("could not pipe to sed");
-                };
-
-                success &= Command::new("./target/debug/rust-semverver")
-                    .args(&["--crate-type=lib", "-Zverbose",
-                            "--extern", &format!("old={}", old_rlib),
-                            "--extern", &format!("new={}", new_rlib),
-                            "tests/helper/test.rs"])
-                    .env("RUST_BACKTRACE", "full")
-                    .env("RUST_SEMVER_CRATE_VERSION", "1.0.0")
-                    .stdin(Stdio::null())
-                    .stdout(out_pipe)
-                    .stderr(err_pipe)
-                    .status()
-                    .expect("could not run rust-semverver")
-                    .success();
-
-                assert!(success, "rust-semverver");
-
-                success &= sed_child.wait().expect("could not wait for sed child").success();
+                    .expect("could not run rm");
             }
-
-            assert!(success, "sed");
-
-            eprintln!("path: {}", out_file.to_str().unwrap());
-            success &= Command::new("git")
-                .args(&["diff", "--exit-code", out_file.to_str().unwrap()])
-                .env("PAGER", "")
-                .status()
-                .expect("could not run git diff")
-                .success();
-
-            assert!(success, "git");
-
-            Command::new("rm")
-                .args(&[&old_rlib, &new_rlib])
-                .status()
-                .expect("could not run rm");
         }
     }
-}
 
-test!(addition);
-test!(addition_path);
-test!(addition_use);
-test!(bounds);
-test!(circular);
-test!(consts);
-test!(enums);
-test!(func);
-test!(func_local_items);
-test!(infer);
-test!(infer_regress);
-test!(inherent_impls);
-test!(issue_34);
-test!(issue_50);
-test!(kind_change);
-test!(macros);
-test!(max_priv);
-test!(mix);
-test!(pathologic_paths);
-test!(pub_use);
-test!(regions);
-test!(removal);
-test!(removal_path);
-test!(removal_use);
-test!(sealed_traits);
-test!(structs);
-test!(swap);
-test!(traits);
-test!(trait_impls);
-test!(trait_objects);
-test!(ty_alias);
+    test!(addition);
+    test!(addition_path);
+    test!(addition_use);
+    test!(bounds);
+    test!(circular);
+    test!(consts);
+    test!(enums);
+    test!(func);
+    test!(func_local_items);
+    test!(infer);
+    test!(infer_regress);
+    test!(inherent_impls);
+    test!(issue_34);
+    test!(issue_50);
+    test!(kind_change);
+    test!(macros);
+    test!(max_priv);
+    test!(mix);
+    test!(pathologic_paths);
+    test!(pub_use);
+    test!(regions);
+    test!(removal);
+    test!(removal_path);
+    test!(removal_use);
+    test!(sealed_traits);
+    test!(structs);
+    test!(swap);
+    test!(traits);
+    test!(trait_impls);
+    test!(trait_objects);
+    test!(ty_alias);
+}
--- tests/full.rs
+++ tests/full.rs
@@ -1,100 +1,101 @@
-use std::env;
-use std::fs::File;
-#[cfg(not(windows))]
-use std::os::unix::io::{AsRawFd, FromRawFd};
-use std::path::Path;
-use std::process::{Command, Stdio};
+#[cfg(not(target_os = "windows"))]
+mod full {
+    use std::env;
+    use std::fs::File;
+    use std::os::unix::io::{AsRawFd, FromRawFd};
+    use std::path::Path;
+    use std::process::{Command, Stdio};
 
-macro_rules! full_test {
-    ($name:ident, $crate_name:expr, $old_version:expr, $new_version:expr) => {
-        #[cfg(not(windows))]
-        #[test]
-        fn $name() {
-            let mut success = true;
+    macro_rules! full_test {
+        ($name:ident, $crate_name:expr, $old_version:expr, $new_version:expr) => {
+            #[test]
+            fn $name() {
+                let mut success = true;
 
-            let old_version = concat!($crate_name, ":", $old_version);
-            let new_version = concat!($crate_name, ":", $new_version);
+                let old_version = concat!($crate_name, ":", $old_version);
+                let new_version = concat!($crate_name, ":", $new_version);
 
-            let prog = concat!(r#"
-# wait for the actual output
-/^version bump/ {
-    doprint = 1;
-}
-
-{
-    # check the environ for filtering
-    if (ENVIRON["RUST_LOG"] == "debug")
+                let prog = concat!(r#"
+    # wait for the actual output
+    /^version bump/ {
         doprint = 1;
+    }
 
-    # skip compilation info
-    if (!doprint)
-        next;
+    {
+        # check the environ for filtering
+        if (ENVIRON["RUST_LOG"] == "debug")
+            doprint = 1;
 
-    # sanitize paths
-    gsub(/-->.*"#, $crate_name, r#"/, "--> "#, $crate_name, r#"", $0);
-    print;
-}"#, $crate_name, $crate_name);
-            let out_file = Path::new("tests/full_cases")
-                .join(concat!($crate_name, "-", $old_version, "-", $new_version));
+        # skip compilation info
+        if (!doprint)
+            next;
 
-            if let Some(path) = env::var_os("PATH") {
-                let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-                let current_dir = env::current_dir().expect("could not determine current dir");
-                paths.insert(0, current_dir.join("target/debug"));
-                let new_path = env::join_paths(paths).unwrap();
-                env::set_var("PATH", &new_path);
-            } else {
-                eprintln!("no path!");
+        # sanitize paths
+        gsub(/-->.*"#, $crate_name, r#"/, "--> "#, $crate_name, r#"", $0);
+        print;
+    }"#, $crate_name, $crate_name);
+                let out_file = Path::new("tests/full_cases")
+                    .join(concat!($crate_name, "-", $old_version, "-", $new_version));
+
+                if let Some(path) = env::var_os("PATH") {
+                    let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+                    let current_dir = env::current_dir().expect("could not determine current dir");
+                    paths.insert(0, current_dir.join("target/debug"));
+                    let new_path = env::join_paths(paths).unwrap();
+                    env::set_var("PATH", &new_path);
+                } else {
+                    eprintln!("no path!");
+                }
+
+                let stdout = File::create(&out_file).expect("could not create `stdout` file");
+                let out_file = out_file.to_str().unwrap();
+
+                let mut awk_child = Command::new("awk")
+                    .arg(&prog)
+                    .stdin(Stdio::piped())
+                    .stdout(stdout)
+                    .spawn()
+                    .expect("could not run awk");
+
+                let (err_pipe, out_pipe) = if let Some(ref stdin) = awk_child.stdin {
+                    let fd = stdin.as_raw_fd();
+                    unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
+                } else {
+                    panic!("could not pipe to awk");
+                };
+
+                success &= Command::new("./target/debug/cargo-semver")
+                    .args(&["-S", &old_version, "-C", &new_version])
+                    .env("RUST_BACKTRACE", "full")
+                    .stdin(Stdio::null())
+                    .stdout(out_pipe)
+                    .stderr(err_pipe)
+                    .status()
+                    .expect("could not run cargo semver")
+                    .success();
+
+                assert!(success, "cargo semver");
+
+                success &= awk_child.wait().expect("could not wait for awk child").success();
+
+                assert!(success, "awk");
+
+                success &= Command::new("git")
+                    .args(&["diff", "--exit-code", out_file])
+                    .env("PAGER", "")
+                    .status()
+                    .expect("could not run git diff")
+                    .success();
+
+                assert!(success, "git");
             }
-
-            let stdout = File::create(&out_file).expect("could not create `stdout` file");
-            let out_file = out_file.to_str().unwrap();
-
-            let mut awk_child = Command::new("awk")
-                .arg(&prog)
-                .stdin(Stdio::piped())
-                .stdout(stdout)
-                .spawn()
-                .expect("could not run awk");
-
-            let (err_pipe, out_pipe) = if let Some(ref stdin) = awk_child.stdin {
-                let fd = stdin.as_raw_fd();
-                unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
-            } else {
-                panic!("could not pipe to awk");
-            };
-
-            success &= Command::new("./target/debug/cargo-semver")
-                .args(&["-S", &old_version, "-C", &new_version])
-                .env("RUST_BACKTRACE", "full")
-                .stdin(Stdio::null())
-                .stdout(out_pipe)
-                .stderr(err_pipe)
-                .status()
-                .expect("could not run cargo semver")
-                .success();
-
-            assert!(success, "cargo semver");
-
-            success &= awk_child.wait().expect("could not wait for awk child").success();
-
-            assert!(success, "awk");
-
-            success &= Command::new("git")
-                .args(&["diff", "--exit-code", out_file])
-                .env("PAGER", "")
-                .status()
-                .expect("could not run git diff")
-                .success();
-
-            assert!(success, "git");
         }
     }
-}
 
-full_test!(log, "log", "0.3.4", "0.3.8");
-full_test!(libc, "libc", "0.2.28", "0.2.31");
-// full_test!(mozjs, "mozjs", "0.2.0", "0.3.0");
-// full_test!(rand, "rand", "0.3.10", "0.3.16");
-// full_test!(serde_pre, "serde", "0.7.0", "1.0.0");
-// full_test!(serde_post, "serde", "1.0.0", "1.0.8");
+    full_test!(log, "log", "0.3.4", "0.3.8");
+    full_test!(libc, "libc", "0.2.28", "0.2.31");
+    // full_test!(mozjs, "mozjs", "0.2.0", "0.3.0");
+    // full_test!(rand, "rand", "0.3.10", "0.3.16");
+    // full_test!(serde_pre, "serde", "0.7.0", "1.0.0");
+    // full_test!(serde_post, "serde", "1.0.0", "1.0.8");
+}
