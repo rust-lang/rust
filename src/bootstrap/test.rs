a3ed2abed7bfb432168eb33492af71ebb16724d9
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-04-15 13:05:14 +0200

    Fix empty tests

--- src/bootstrap/test.rs
+++ src/bootstrap/test.rs
@@ -919,10 +919,8 @@ impl Step for Compiletest {
             if build.config.rust_debuginfo_tests {
                 flags.push("-g".to_string());
             }
-            flags.push("-Zmiri -Zunstable-options".to_string());
-        } else {
-            flags.push("-Zunstable-options".to_string());
         }
+        flags.push("-Zunstable-options".to_string());
         flags.push(build.config.cmd.rustc_args().join(" "));
 
         if let Some(linker) = build.linker(target) {
