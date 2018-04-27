78b141d937610e6a2bb345057d205bb9f9a2b980
Nathan <nathan@users.noreply.github.com>
2018-04-27 02:40:19 -0400

    Fix missing line comments in {drop,forget}_copy examples

--- clippy_lints/src/drop_forget_ref.rs
+++ clippy_lints/src/drop_forget_ref.rs
@@ -60,7 +60,7 @@ declare_clippy_lint! {
 /// ```rust
 /// let x:i32 = 42;   // i32 implements Copy
 /// std::mem::drop(x) // A copy of x is passed to the function, leaving the
-/// original unaffected
+/// // original unaffected
 /// ```
 declare_clippy_lint! {
     pub DROP_COPY,
@@ -87,7 +87,7 @@ declare_clippy_lint! {
 /// ```rust
 /// let x:i32 = 42;     // i32 implements Copy
 /// std::mem::forget(x) // A copy of x is passed to the function, leaving the
-/// original unaffected
+/// // original unaffected
 /// ```
 declare_clippy_lint! {
     pub FORGET_COPY,
