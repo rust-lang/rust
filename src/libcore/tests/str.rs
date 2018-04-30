4fab1674c3db2cf4b2d1546a7120d8d56a85c355
Michael Lamparski <diagonaldevice@gmail.com>
2018-04-30 07:36:56 -0400

    update libcore's comment about str tests

--- src/libcore/tests/str.rs
+++ src/libcore/tests/str.rs
@@ -8,4 +8,4 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// All `str` tests live in collectionstests::str
+// All `str` tests live in liballoc/tests
