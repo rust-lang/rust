160063aad2206a35e6312bfaa159f0f4475af0ae
F001 <changchun.fan@qq.com>
2018-05-05 16:34:43 +0800

    make `String::new()` const

--- src/test/run-pass/collections-const-new.rs
+++ src/test/run-pass/collections-const-new.rs
@@ -8,10 +8,15 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// Test that Vec::new() can be used for constants
+// Test several functions can be used for constants
+// 1. Vec::new()
+// 2. String::new()
 
 #![feature(const_vec_new)]
+#![feature(const_string_new)]
 
 const MY_VEC: Vec<usize> = Vec::new();
+
+const MY_STRING: String = String::new();
 
 pub fn main() {}
