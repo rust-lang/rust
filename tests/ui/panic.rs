db4e7ac725689c3006a2817f373fb6ce9e64d29f
flip1995 <uwdkn@student.kit.edu>
2018-05-06 17:26:47 +0200

    panic_params: don't lint escaped squigglies

--- tests/ui/panic.rs
+++ tests/ui/panic.rs
@@ -11,6 +11,8 @@ fn missing() {
     } else {
         assert!(true, "here be missing values: {}");
     }
+
+    panic!("{{{this}}}");
 }
 
 fn ok_single() {
@@ -41,6 +43,16 @@ fn ok_nomsg() {
     assert!(if 1 == ONE { ONE == 1 } else { false });
 }
 
+fn ok_escaped() {
+    panic!("{{ why should this not be ok? }}");
+    panic!(" or {{ that ?");
+    panic!(" or }} this ?");
+    panic!(" {or {{ that ?");
+    panic!(" }or }} this ?");
+    panic!("{{ test }");
+    panic!("{case }}");
+}
+
 fn main() {
     missing();
     ok_single();
@@ -48,4 +60,5 @@ fn main() {
     ok_bracket();
     ok_inner();
     ok_nomsg();
+    ok_escaped();
 }
