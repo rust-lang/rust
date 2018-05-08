61c58c2f6710913a315c4c13dfc3b3cb088af18a
Oliver Schneider <git-no-reply-9879165716479413131@oli-obk.de>
2018-05-08 17:29:09 +0200

    Fix a typo in a comment

--- src/test/run-pass/ctfe/const-fn-destructuring-arg.rs
+++ src/test/run-pass/ctfe/const-fn-destructuring-arg.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// test that certain things are disallowed in constant functionssignatures
+// test that certain things are disallowed in constant functions
 
 #![feature(const_fn, const_let)]
 
