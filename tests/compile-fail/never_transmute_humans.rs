569792acbc6fccc13a7cb7377948912f6bba0aab
bjorn3 <bjorn3@users.noreply.github.com>
2018-05-09 17:30:16 +0200

    Address some review comments

---
+++ tests/compile-fail/never_transmute_humans.rs
@@ -0,0 +1,18 @@
+// This should fail even without validation
+// compile-fail causes rustc ICE: rust-lang/rust#50570
+// compile-flags: -Zmir-emit-validate=0
+
+#![feature(never_type)]
+#![allow(unreachable_code)]
+#![allow(unused_variables)]
+
+struct Human;
+
+fn main() {
+    let x: ! = unsafe {
+        std::mem::transmute::<Human, !>(Human) //~ ERROR entered unreachable code
+    };
+    f(x)
+}
+
+fn f(x: !) -> ! { x }
--- tests/compile-fail/repeat2.rs
+++
@@ -1,7 +0,0 @@
-// ignore-test
-
-fn main() {
-    let data: [u8; 1024*1024*1024] = [42; 1024*1024*1024];
-    //~^ ERROR: tried to allocate
-    assert_eq!(data.len(), 1024*1024*1024);
-}
