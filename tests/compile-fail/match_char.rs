94754de60010dba845ea9c299c9616dbc2022c86
bjorn3 <bjorn3@users.noreply.github.com>
2018-05-09 19:24:25 +0200

    Convert legitimate failing errors to the new error format

--- tests/compile-fail/match_char.rs
+++ tests/compile-fail/match_char.rs
@@ -1,6 +1,7 @@
 fn main() {
     assert!(std::char::from_u32(-1_i32 as u32).is_none());
-    match unsafe { std::mem::transmute::<i32, char>(-1) } { //~ERROR tried to interpret an invalid 32-bit value as a char: 4294967295
+    match unsafe { std::mem::transmute::<i32, char>(-1) } { //~ ERROR constant evaluation error [E0080]
+        //~^ NOTE tried to interpret an invalid 32-bit value as a char: 4294967295
         'a' => {},
         'b' => {},
         _ => {},
--- tests/compile-fail/repeat.rs
+++
@@ -1,6 +0,0 @@
-// error-pattern the type `[u8;
-
-fn main() {
-    let data: [u8; std::usize::MAX] = [42; std::usize::MAX];
-    assert_eq!(data.len(), 1024);
-}
