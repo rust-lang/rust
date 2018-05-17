7676982e90120cf4edacc6b1ed8a8cebce12b97c
Kyle Stachowicz <kylestach99@gmail.com>
2018-05-17 16:57:46 -0700

    Revert "Add lint checks for unused loop labels"
    This functionality is being reimplemented in the resolver phase
    
    This reverts commit 503a69e844970476b27bf1ac7be951bb22194f50.

--- src/librustc_lint/lib.rs
+++ src/librustc_lint/lib.rs
@@ -110,7 +110,6 @@ pub fn register_builtins(store: &mut lint::LintStore, sess: Option<&Session>) {
 
     add_early_builtin_with_new!(sess,
                                 DeprecatedAttr,
-                                UnusedLabel,
                                 );
 
     add_builtin!(sess,
@@ -178,7 +177,6 @@ pub fn register_builtins(store: &mut lint::LintStore, sess: Option<&Session>) {
                     UNUSED_DOC_COMMENT,
                     UNUSED_EXTERN_CRATES,
                     UNUSED_FEATURES,
-                    UNUSED_LABEL,
                     UNUSED_PARENS);
 
     add_lint_group!(sess,
