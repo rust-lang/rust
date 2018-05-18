5fb987fff840783e1818ca93651037df574f3d6d
Nick Cameron <ncameron@mozilla.com>
2018-05-18 17:11:02 +1200

    Fixup integration tests

--- ci/integration.sh
+++ ci/integration.sh
@@ -21,7 +21,7 @@ cargo install --force
 echo "Integration tests for: ${INTEGRATION}"
 
 function check_fmt {
-    cargo fmt --all -v -- --error-on-unformatted &> rustfmt_output
+    cargo fmt --all -v &> rustfmt_output
     if [[ $? != 0 ]]; then
         cat rustfmt_output
         return 1
