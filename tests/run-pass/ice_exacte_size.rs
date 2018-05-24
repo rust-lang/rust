dc8d29be4ac405e61b6c403b0ea09809e55b8efa
Michael A. Plikk <michael@aptoma.com>
2018-05-24 16:30:26 +0200

    Allow unimplemented in other tests

--- tests/run-pass/ice_exacte_size.rs
+++ tests/run-pass/ice_exacte_size.rs
@@ -6,6 +6,7 @@ struct Foo;
 impl Iterator for Foo {
     type Item = ();
 
+    #[allow(unimplemented)]
     fn next(&mut self) -> Option<()> {
         let _ = self.len() == 0;
         unimplemented!()
