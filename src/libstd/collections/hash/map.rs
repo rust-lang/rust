fd4593fcdff233d0658162ec7859f988c0656393
Alexis Beingessner <a.beingessner@gmail.com>
2018-05-25 16:03:50 -0400

    fixup Debug bounds

--- src/libstd/collections/hash/map.rs
+++ src/libstd/collections/hash/map.rs
@@ -2254,7 +2254,7 @@ impl<'a, K, V> Debug for RawEntryBuilderHashed<'a, K, V> {
 }
 
 #[unstable(feature = "hash_raw_entry", issue = "42069")]
-impl<'a, K, V> Debug for RawEntry<'a, K, V> {
+impl<'a, K: Debug, V: Debug> Debug for RawEntry<'a, K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             RawEntry::Vacant(ref v) => {
@@ -2272,7 +2272,7 @@ impl<'a, K, V> Debug for RawEntry<'a, K, V> {
 }
 
 #[unstable(feature = "hash_raw_entry", issue = "42069")]
-impl<'a, K, V> Debug for RawOccupiedEntry<'a, K, V> {
+impl<'a, K: Debug, V: Debug> Debug for RawOccupiedEntry<'a, K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.debug_struct("RawOccupiedEntry")
          .field("key", self.key())
