5ef15cd71fc81c5e08a58756b314ad0973c27947
QuietMisdreavus <grey@quietmisdreavus.net>
2018-05-29 14:37:46 -0500

    tag private modules with doc(cfg)

--- library/stdarch/coresimd/mod.rs
+++ library/stdarch/coresimd/mod.rs
@@ -146,24 +146,31 @@ pub mod arch {
 mod simd_llvm;
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64", dox))]
+#[doc(cfg(any(target_arch = "x86", target_arch = "x86_64")))]
 mod x86;
 #[cfg(any(target_arch = "x86_64", dox))]
+#[doc(cfg(target_arch = "x86_64"))]
 mod x86_64;
 
 #[cfg(any(target_arch = "aarch64", dox))]
+#[doc(cfg(target_arch = "aarch64"))]
 mod aarch64;
 #[cfg(any(target_arch = "arm", target_arch = "aarch64", dox))]
+#[doc(cfg(any(target_arch = "arm", target_arch = "aarch64")))]
 mod arm;
 #[cfg(target_arch = "wasm32")]
 mod wasm32;
 
 #[cfg(any(target_arch = "mips", target_arch = "mips64", dox))]
+#[doc(cfg(any(target_arch = "mips", target_arch = "mips64")))]
 mod mips;
 
 #[cfg(any(target_arch = "powerpc", target_arch = "powerpc64", dox))]
+#[doc(cfg(any(target_arch = "powerpc", target_arch = "powerpc64")))]
 mod powerpc;
 
 #[cfg(any(target_arch = "powerpc64", dox))]
+#[doc(cfg(target_arch = "powerpc64"))]
 mod powerpc64;
 
 mod nvptx;
