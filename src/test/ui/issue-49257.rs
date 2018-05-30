cbc70a0d68bae1d439eb679676c499d543e61396
Esteban Küber <esteban@kuber.com.ar>
2018-05-29 22:31:00 -0700

    Improve diagnostics for incorrect `..` usage
    When using `..` somewhere other than the end, parse the rest of the
    pattern correctly while still emitting an error.
    
    Add suggestions to either remove trailing `,` or moving the `..` to the
    end.

--- src/test/ui/issue-49257.rs
+++ src/test/ui/issue-49257.rs
@@ -17,5 +17,8 @@ struct Point { x: u8, y: u8 }
 
 fn main() {
     let p = Point { x: 0, y: 0 };
+    let Point { .., y, } = p; //~ ERROR expected `}`, found `,`
     let Point { .., y } = p; //~ ERROR expected `}`, found `,`
+    let Point { .., } = p; //~ ERROR expected `}`, found `,`
+    let Point { .. } = p;
 }
