402aa0ff157d48175747812b594087deb027a151
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-06-03 00:48:17 +0200

    Add even more tests

--- src/test/rustdoc/keyword.rs
+++ src/test/rustdoc/keyword.rs
@@ -16,6 +16,9 @@
 // @has foo/index.html '//a[@href="keyword.match.html"]' 'match'
 // @has foo/keyword.match.html '//a[@class="keyword"]' 'match'
 // @has foo/keyword.match.html '//section[@id="main"]//div[@class="docblock"]//p' 'this is a test!'
+// @!has foo/index.html '//a/@href' 'foo/index.html'
+// @!has foo/foo/index.html
+// @!has-dir foo/foo
 #[doc(keyword = "match")]
 /// this is a test!
 mod foo{}
