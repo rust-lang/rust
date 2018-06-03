8b5f962762b500a1428e9d6937d964922a233fa1
Linus Färnstrand <faern@faern.net>
2018-06-03 11:18:24 +0200

    Ignore i128 test on asmjs

--- src/test/run-pass/const-endianess.rs
+++ src/test/run-pass/const-endianess.rs
@@ -16,12 +16,17 @@ use test::black_box as b;
 
 const BE_U32: u32 = 55u32.to_be();
 const LE_U32: u32 = 55u32.to_le();
-const BE_U128: u128 = 999999u128.to_be();
-const LE_I128: i128 = -999999i128.to_le();
+
 
 fn main() {
     assert_eq!(BE_U32, b(55u32).to_be());
     assert_eq!(LE_U32, b(55u32).to_le());
-    assert_eq!(BE_U128, b(999999u128).to_be());
-    assert_eq!(LE_I128, b(-999999i128).to_le());
+
+    #[cfg(not(target_arch = "asmjs"))]
+    {
+        const BE_U128: u128 = 999999u128.to_be();
+        const LE_I128: i128 = -999999i128.to_le();
+        assert_eq!(BE_U128, b(999999u128).to_be());
+        assert_eq!(LE_I128, b(-999999i128).to_le());
+    }
 }
