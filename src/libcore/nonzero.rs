530d7bc5174e93682a38b22ac7ff4e3569bcd813
Simon Sapin <simon.sapin@exyr.org>
2018-06-06 13:30:35 +0200

    Add #[repr(transparent)] to some libcore types
    * `UnsafeCell`
    * `Cell`
    * `NonZero*`
    * `NonNull`
    * `Unique`

--- src/libcore/nonzero.rs
+++ src/libcore/nonzero.rs
@@ -16,6 +16,7 @@ use ops::CoerceUnsized;
 /// NULL or 0 that might allow certain optimizations.
 #[lang = "non_zero"]
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[repr(transparent)]
 pub(crate) struct NonZero<T>(pub(crate) T);
 
 impl<T: CoerceUnsized<U>, U> CoerceUnsized<NonZero<U>> for NonZero<T> {}
