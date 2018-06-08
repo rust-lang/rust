9d7613abeec1db5613c331d9e69e102f9b22f844
Matthew Jasper <mjjasper1@gmail.com>
2018-06-08 17:00:49 +0100

    Fix a typo

--- src/test/ui/issue-50825.rs
+++ src/test/ui/issue-50825.rs
@@ -10,7 +10,7 @@
 
 // run-pass
 // regression test for issue #50825
-// Make sure that the built-in bound {integer}: Sized is prefered over
+// Make sure that the built-in bound {integer}: Sized is preferred over
 // the u64: Sized bound in the where clause.
 
 fn foo(y: &[()])
