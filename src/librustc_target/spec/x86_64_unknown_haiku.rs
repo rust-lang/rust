bbe45ce584436ea23aeb9aec5871f7813f8c7476
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2018-06-09 14:08:45 +0000

    Haiku: make it explicit that Haiku uses position independent executables on x86_64
    With the switch to gcc 7 the linker scripts don't always explicitly
    add the `-pie` flag anymore. This creates build failures on x86_64.
    Interestingly enough, adding this flag on i686 will lead to executables
    that fail to run when ASLR is enabled, so let's keep it x86_64 only.

--- src/librustc_target/spec/x86_64_unknown_haiku.rs
+++ src/librustc_target/spec/x86_64_unknown_haiku.rs
@@ -16,6 +16,8 @@ pub fn target() -> TargetResult {
     base.max_atomic_width = Some(64);
     base.pre_link_args.insert(LinkerFlavor::Gcc, vec!["-m64".to_string()]);
     base.stack_probes = true;
+    // This option is required to build executables on Haiku x86_64
+    base.position_independent_executables = true;
 
     Ok(Target {
         llvm_target: "x86_64-unknown-haiku".to_string(),
