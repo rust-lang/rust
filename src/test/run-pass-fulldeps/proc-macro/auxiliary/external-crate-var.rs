d0c59e2c86c7e19e5060fe1eb0cde253579b64f6
Michael Lamparski <diagonaldevice@gmail.com>
2018-06-11 19:29:12 -0400

    add tests for $crate in QSelf paths

--- src/test/run-pass-fulldeps/proc-macro/auxiliary/external-crate-var.rs
+++ src/test/run-pass-fulldeps/proc-macro/auxiliary/external-crate-var.rs
@@ -8,64 +8,44 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// aux-build:double.rs
-// aux-build:external-crate-var.rs
-// ignore-stage1
+pub struct ExternFoo;
 
-#![allow(unused)]
-
-#[macro_use]
-extern crate double;
-#[macro_use]
-extern crate external_crate_var;
-
-struct Foo;
-
-trait Trait {
+pub trait ExternTrait {
     const CONST: u32;
     type Assoc;
 }
 
-impl Trait for Foo {
+impl ExternTrait for ExternFoo {
     const CONST: u32 = 0;
-    type Assoc = Foo;
+    type Assoc = ExternFoo;
 }
 
-macro_rules! local { () => {
-    // derive_Double outputs secondary copies of each definition
-    // to test what the proc_macro sees.
+#[macro_export]
+macro_rules! external { () => {
     mod bar {
         #[derive(Double)]
-        struct Bar($crate::Foo);
+        struct Bar($crate::ExternFoo);
     }
 
     mod qself {
         #[derive(Double)]
-        struct QSelf(<::Foo as $crate::Trait>::Assoc);
+        struct QSelf(<$crate::ExternFoo as $crate::ExternTrait>::Assoc);
     }
 
     mod qself_recurse {
         #[derive(Double)]
-        struct QSelfRecurse(<<$crate::Foo as $crate::Trait>::Assoc as $crate::Trait>::Assoc);
+        struct QSelfRecurse(<
+            <$crate::ExternFoo as $crate::ExternTrait>::Assoc
+            as $crate::ExternTrait>::Assoc
+        );
     }
 
     mod qself_in_const {
         #[derive(Double)]
         #[repr(u32)]
         enum QSelfInConst {
-            Variant = <::Foo as $crate::Trait>::CONST,
+            Variant = <$crate::ExternFoo as $crate::ExternTrait>::CONST,
         }
     }
 } }
 
-mod local {
-    local!();
-}
-
-// and now repeat the above tests, using a macro defined in another crate
-
-mod external {
-    external!{}
-}
-
-fn main() {}
