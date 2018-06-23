dc7d77f88e213214d144643612939d52431396c9
varkor <github@varkor.com>
2018-06-23 13:21:09 +0100

    Add a test for break

--- src/test/ui/closure-array-break-length.rs
+++ src/test/ui/closure-array-break-length.rs
@@ -12,4 +12,6 @@ fn main() {
     |_: [_; continue]| {}; //~ ERROR: `continue` outside of loop
 
     while |_: [_; continue]| {} {} //~ ERROR: `break` or `continue` with no label
+
+    while |_: [_; break]| {} {} //~ ERROR: `break` or `continue` with no label
 }
