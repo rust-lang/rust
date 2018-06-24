d36302da53d398e35d8eacc8cef1c43d1d95359c
Dylan MacKenzie <ecstaticmorse@gmail.com>
2018-06-24 13:53:27 -0700

    Add a UI test for #50637
    This test relies on the fact that restrictions on expressions in `const
    fn` do not apply when computing array lengths. It is more difficult to
    statically analyze than the simple `loop{}` mentioned in #50637.
    
    This test should be updated to ignore the warning after #49980 is resolved.

---
+++ src/test/ui/const-eval/infinite_loop.rs
@@ -0,0 +1,25 @@
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(const_let)]
+
+fn main() {
+    // Tests the Collatz conjecture with an incorrect base case (0 instead of 1).
+    // The value of `n` will loop indefinitely (4 - 2 - 1 - 4).
+    let _ = [(); {
+        //~^ WARNING Constant evaluating a complex constant, this might take some time
+        //~| ERROR could not evaluate repeat length
+        let mut n = 113383; // #20 in A006884
+        while n != 0 { //~ ERROR constant contains unimplemented expression type
+            n = if n % 2 == 0 { n/2 } else { 3*n + 1 };
+        }
+        n
+    }];
+}
