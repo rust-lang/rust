57f7f22fe46a08c197e4cd1d2e78df9de4c00798
Anirudh Balaji <anirudhb@users.noreply.github.com>
2018-06-24 15:31:03 -0700

    Make line-breaking more consistent.

--- src/libcore/slice/mod.rs
+++ src/libcore/slice/mod.rs
@@ -1541,10 +1541,9 @@ impl<T> [T] {
     /// let src = [1, 2, 3, 4];
     /// let mut dst = [0, 0];
     ///
-    /// // Note: the slices must be the same length, so
-    /// // you can slice the source to be the same size.
-    /// // Here we slice the source, four elements, to two, the same size
-    /// // as the destination slice. It *will* panic if we don't do this.
+    /// // Note: the slices must be the same length, so you can slice the 
+    /// // source to be the same size. Here we slice the source, four elements,
+    /// // to two, the same size as the destination slice. It *will* panic if we don't do this.
     /// dst.clone_from_slice(&src[2..]);
     ///
     /// assert_eq!(src, [1, 2, 3, 4]);
@@ -1611,10 +1610,9 @@ impl<T> [T] {
     /// let src = [1, 2, 3, 4];
     /// let mut dst = [0, 0];
     ///
-    /// // Note: the slices must be the same length, so
-    /// // you can slice the source to be the same size.
-    /// // Here we slice the source, four elements, to two, the same size
-    /// // as the destination slice. It *will* panic if we don't do this.
+    /// // Note: the slices must be the same length, so you can slice the 
+    /// // source to be the same size. Here we slice the source, four elements,
+    /// // to two, the same size as the destination slice. It *will* panic if we don't do this.
     /// dst.copy_from_slice(&src[2..]);
     ///
     /// assert_eq!(src, [1, 2, 3, 4]);
