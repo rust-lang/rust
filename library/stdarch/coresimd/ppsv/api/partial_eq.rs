625c31200abb392883ac32c3c57e66b8b9aa1022
Brandon Falk <bfalk@gl.lan>
2018-06-29 16:25:04 -0700

    Fix PartialEq issue #511

--- library/stdarch/coresimd/ppsv/api/partial_eq.rs
+++ library/stdarch/coresimd/ppsv/api/partial_eq.rs
@@ -10,7 +10,7 @@ macro_rules! impl_partial_eq {
             }
             #[inline]
             fn ne(&self, other: &Self) -> bool {
-                $id::ne(*self, *other).all()
+                $id::ne(*self, *other).any()
             }
         }
     };
@@ -24,6 +24,18 @@ macro_rules! test_partial_eq {
             use coresimd::simd::*;
 
             let a = $id::splat($false);
+            let b = $id::splat($true);
+
+            assert!(a != b);
+            assert!(!(a == b));
+            assert!(a == a);
+            assert!(!(a != a));
+
+            // Test further to make sure comparisons work with non-splatted
+            // values.
+            // This is to test the fix for #511
+
+            let a = $id::splat($false).replace(0, $true);
             let b = $id::splat($true);
 
             assert!(a != b);
