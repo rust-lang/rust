c613aa5a95a83ec77b4b0af1e74019cb8aca75b4
Matthew Jasper <mjjasper1@gmail.com>
2018-06-30 11:02:38 +0100

    Improve error messages when assigning to a local that starts initialized

--- src/test/compile-fail/immut-function-arguments.rs
+++ src/test/compile-fail/immut-function-arguments.rs
@@ -13,12 +13,12 @@
 
 fn f(y: Box<isize>) {
     *y = 5; //[ast]~ ERROR cannot assign
-            //[mir]~^ ERROR cannot assign twice
+            //[mir]~^ ERROR cannot assign
 }
 
 fn g() {
     let _frob = |q: Box<isize>| { *q = 2; }; //[ast]~ ERROR cannot assign
-    //[mir]~^ ERROR cannot assign twice
+    //[mir]~^ ERROR cannot assign
 }
 
 fn main() {}
