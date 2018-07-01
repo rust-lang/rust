3ba0b3082178cc2ea9640991ce5523ae9525bb5a
Richard Dodd (dodj) <richard.o.dodd@gmail.com>
2018-07-01 16:12:38 +0000

    Update array.rs

--- src/libcore/array.rs
+++ src/libcore/array.rs
@@ -260,7 +260,7 @@ array_impls! {
      0  1  2  3  4  5  6  7  8  9
     10 11 12 13 14 15 16 17 18 19
     20 21 22 23 24 25 26 27 28 29
-    30 31 32 64 128 256 512 1024
+    30 31 32 48 64 128 256 512 1024
 }
 
 // The Default impls cannot be generated using the array_impls! macro because
