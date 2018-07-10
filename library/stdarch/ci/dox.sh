8d8d81aa352ed25a52742d503d6b76bed25d53e9
Luca Barbato <lu_zero@gentoo.org>
2018-07-10 23:39:44 +0200

    Drop the not really supported PowerPC 32bit target
    The LLVM backend has known issues and even for them the main
    development target is PowerPC 64bit Little Endian.

--- library/stdarch/ci/dox.sh
+++ library/stdarch/ci/dox.sh
@@ -40,7 +40,7 @@ dox i686 i686-unknown-linux-gnu
 dox x86_64 x86_64-unknown-linux-gnu
 dox arm armv7-unknown-linux-gnueabihf
 dox aarch64 aarch64-unknown-linux-gnu
-dox powerpc powerpc-unknown-linux-gnu
+# dox powerpc powerpc-unknown-linux-gnu
 dox powerpc64le powerpc64le-unknown-linux-gnu
 dox mips mips-unknown-linux-gnu
 dox mips64 mips64-unknown-linux-gnuabi64
