99102f4bcd8bc66acfc3362423bf72d3b54cee02
Steve Klabnik <steve@steveklabnik.com>
2018-07-11 07:18:28 -0700

    fix some doc links (#520)

--- library/stdarch/coresimd/arm/mod.rs
+++ library/stdarch/coresimd/arm/mod.rs
@@ -3,9 +3,7 @@
 //! The reference for NEON is [ARM's NEON Intrinsics Reference][arm_ref]. The
 //! [ARM's NEON Intrinsics Online Database][arm_dat] is also useful.
 //!
-//! [arm_ref]:
-//! http://infocenter.arm.com/help/topic/com.arm.doc.
-//! ihi0073a/IHI0073A_arm_neon_intrinsics_ref.pdf
+//! [arm_ref]: http://infocenter.arm.com/help/topic/com.arm.doc.ihi0073a/IHI0073A_arm_neon_intrinsics_ref.pdf
 //! [arm_dat]: https://developer.arm.com/technologies/neon/intrinsics
 #![allow(non_camel_case_types)]
 
