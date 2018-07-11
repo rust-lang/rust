6cfd49e8dd8c09122c8b4f98ca3e7a33080e6e80
ljedrz <ljedrz@gmail.com>
2018-07-11 16:08:38 +0200

    add a missing `dyn`

--- src/librustc_data_structures/sync.rs
+++ src/librustc_data_structures/sync.rs
@@ -268,7 +268,7 @@ cfg_if! {
             t.into_par_iter()
         }
 
-        pub type MetadataRef = OwningRef<Box<Erased + Send + Sync>, [u8]>;
+        pub type MetadataRef = OwningRef<Box<dyn Erased + Send + Sync>, [u8]>;
 
         /// This makes locks panic if they are already held.
         /// It is only useful when you are running in a single thread
