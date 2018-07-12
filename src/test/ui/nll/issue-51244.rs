323df7b5049f6026a5bed335e2d7bb1df89b47ef
ashtneoi <ashtneoi@gmail.com>
2018-07-12 13:11:47 -0700

    Bless tests and update ERROR

--- src/test/ui/nll/issue-51244.rs
+++ src/test/ui/nll/issue-51244.rs
@@ -12,5 +12,6 @@
 
 fn main() {
     let ref my_ref @ _ = 0;
-    *my_ref = 0; //~ ERROR cannot assign to data in a `&` reference [E0594]
+    *my_ref = 0;
+    //~^ ERROR cannot assign to `*my_ref` which is behind a `&` reference [E0594]
 }
