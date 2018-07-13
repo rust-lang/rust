f556a9bbb422e86c76334fe3fe0a3c0382707946
Jorge Aparicio <jorge@japaric.io>
2018-07-12 20:19:42 -0500

    test source importing this crate

--- library/compiler-builtins/libm/ci/script.sh
+++ library/compiler-builtins/libm/ci/script.sh
@@ -1,11 +1,21 @@
 set -euxo pipefail
 
 main() {
+    # quick check
+    cargo check
+
+    # check that we can source import libm into compiler-builtins
+    cargo check --package cb
+
+    # generate tests
     cargo run --package test-generator --target x86_64-unknown-linux-musl
+
     if cargo fmt --version >/dev/null 2>&1; then
         # nicer syntax error messages (if any)
         cargo fmt
     fi
+
+    # run tests
     cross test --target $TARGET --release
 
     # TODO need to fix overflow issues (cf. issue #4)
