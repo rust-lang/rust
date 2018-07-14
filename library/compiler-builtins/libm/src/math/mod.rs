a7c456363394954ef27cd3db80e476e7349518c0
C Jones <code@calebjones.net>
2018-07-14 05:48:39 -0400

    Use separate imports instead of {}-grouped imports for better merges
    Previously every merge would cause conflicts because the {}-group would re-flow,
    which the merge algorithm can't handle. This will hopefully make rebases and
    merges go more smoothly while everyone is still adding new modules.

--- library/compiler-builtins/libm/src/math/mod.rs
+++ library/compiler-builtins/libm/src/math/mod.rs
@@ -35,12 +35,33 @@ mod truncf;
 
 //mod service;
 
-pub use self::{
-    ceilf::ceilf, expf::expf, fabs::fabs, fabsf::fabsf, floor::floor, floorf::floorf, fmodf::fmodf,
-    hypot::hypot, hypotf::hypotf, log::log, log10::log10, log10f::log10f, log1p::log1p,
-    log1pf::log1pf, log2::log2, log2f::log2f, logf::logf, powf::powf, round::round, roundf::roundf,
-    scalbn::scalbn, scalbnf::scalbnf, sqrt::sqrt, sqrtf::sqrtf, trunc::trunc, truncf::truncf,
-};
+// Use separated imports instead of {}-grouped imports for easier merging.
+pub use self::ceilf::ceilf;
+pub use self::expf::expf;
+pub use self::fabs::fabs;
+pub use self::fabsf::fabsf;
+pub use self::floor::floor;
+pub use self::floorf::floorf;
+pub use self::fmodf::fmodf;
+pub use self::hypot::hypot;
+pub use self::hypotf::hypotf;
+pub use self::log::log;
+pub use self::log10::log10;
+pub use self::log10f::log10f;
+pub use self::log1p::log1p;
+pub use self::log1pf::log1pf;
+pub use self::log2::log2;
+pub use self::log2f::log2f;
+pub use self::logf::logf;
+pub use self::powf::powf;
+pub use self::round::round;
+pub use self::roundf::roundf;
+pub use self::scalbn::scalbn;
+pub use self::scalbnf::scalbnf;
+pub use self::sqrt::sqrt;
+pub use self::sqrtf::sqrtf;
+pub use self::trunc::trunc;
+pub use self::truncf::truncf;
 
 fn isnanf(x: f32) -> bool {
     x.to_bits() & 0x7fffffff > 0x7f800000
