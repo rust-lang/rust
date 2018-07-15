7e7c21492f257d688f08c19b206224198c93e6ae
Sébastien Marie <semarie@online.fr>
2018-07-15 09:58:58 +0200

    tidy: avoid long line

--- src/bootstrap/dist.rs
+++ src/bootstrap/dist.rs
@@ -1910,7 +1910,8 @@ impl Step for LlvmTools {
         /* run only if llvm-config isn't used */
         if let Some(config) = builder.config.target_config.get(&target) {
             if let Some(ref _s) = config.llvm_config {
-                builder.info(&format!("Skipping LlvmTools stage{} ({}): external LLVM", stage, target));
+                builder.info(&format!("Skipping LlvmTools stage{} ({}): external LLVM",
+                    stage, target));
                 return None;
             }
         }
