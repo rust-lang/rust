10b65fa603f600dee57f14f8b67f2d7c391de2cd
O01eg <o01eg@yandex.ru>
2018-07-16 19:17:14 +0300

    Revert some changes from #51917 to fix #52317.

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -825,7 +825,7 @@ impl<'a> Builder<'a> {
             cargo.env("RUSTC_ERROR_FORMAT", error_format);
         }
         if cmd != "build" && cmd != "check" && want_rustdoc {
-            cargo.env("RUSTDOC_LIBDIR", &libdir);
+            cargo.env("RUSTDOC_LIBDIR", self.sysroot_libdir(compiler, self.config.build));
         }
 
         if mode.is_tool() {
