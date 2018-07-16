fb6e8f2433f5c6efe984f41bf6ecfd0b57031fee
QuietMisdreavus <grey@quietmisdreavus.net>
2018-07-16 16:25:34 -0500

    normalize test output so it can be run from repo root

--- src/test/rustdoc-ui/failed-doctest-output.rs
+++ src/test/rustdoc-ui/failed-doctest-output.rs
@@ -9,15 +9,19 @@
 // except according to those terms.
 
 // Issue #51162: A failed doctest was not printing its stdout/stderr
+// FIXME: if/when the output of the test harness can be tested on its own, this test should be
+// adapted to use that, and that normalize line can go away
 
 // compile-flags:--test
-// disable-ui-testing-normalization
+// normalize-stdout-test: "src/test/rustdoc-ui" -> "$$DIR"
 
+// doctest fails at runtime
 /// ```
 /// panic!("oh no");
 /// ```
 pub struct SomeStruct;
 
+// doctest fails at compile time
 /// ```
 /// no
 /// ```
