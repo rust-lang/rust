d11c668a3900fcbad336eb470da8748a04000253
Ralf Jung <post@ralfj.de>
2018-07-16 08:39:06 +0200

    no longer run rustc on the tests; instead make sure we actually deny all warnings
    Also fix the fallout from that

--- tests/compile-fail-fullmir/reallocate-change-alloc.rs
+++ tests/compile-fail-fullmir/reallocate-change-alloc.rs
@@ -8,7 +8,7 @@ use std::alloc::*;
 fn main() {
     unsafe {
         let x = Global.alloc(Layout::from_size_align_unchecked(1, 1)).unwrap();
-        let _y = Global.realloc(x, Layout::from_size_align_unchecked(1, 1), 1).unwrap();
+        Global.realloc(x, Layout::from_size_align_unchecked(1, 1), 1).unwrap();
         let _z = *(x.as_ptr() as *mut u8); //~ ERROR constant evaluation error
         //~^ NOTE dangling pointer was dereferenced
     }
