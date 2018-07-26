c490151b16a07ccd261af44b4ddf5832ceda8c5a
Ralf Jung <post@ralfj.de>
2018-07-26 17:58:37 +0200

    memrchr currently does not work

--- cargo-miri-test/src/main.rs
+++ cargo-miri-test/src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let buf = &[1,2,3,4];
     let n = <BigEndian as ByteOrder>::read_u32(buf);
     assert_eq!(n, 0x01020304);
-    println!("{:#x}", n);
+    //println!("{:#x}", n); FIXME enable once memrchr works in miri
     eprintln!("standard error");
 }
