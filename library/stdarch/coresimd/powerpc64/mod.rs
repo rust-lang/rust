4db62cdf71fc3dd3e3006691fdddff1e0a65496a
Luca Barbato <luca.barbato@gmail.com>
2018-07-26 02:30:57 +0200

    Move the vsx code to powerpc (#538)
    Even if it is unlikely to happen the VSX instructions can be executed in
    32bit mode just as well.

--- library/stdarch/coresimd/powerpc64/mod.rs
+++ library/stdarch/coresimd/powerpc64/mod.rs
@@ -6,6 +6,3 @@
 //! [64-Bit ELF V2 ABI Specification - Power Architecture]: http://openpowerfoundation.org/wp-content/uploads/resources/leabi/leabi-20170510.pdf
 
 pub use coresimd::powerpc::*;
-
-mod vsx;
-pub use self::vsx::*;
