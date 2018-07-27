429ae93c09055a1fde14ef4cbf807bdfcbe65584
toidiu <apoorv@toidiu.com>
2018-07-26 20:32:01 -0400

    some comment about not infering static lifetimes test

--- src/test/ui/rfc-2093-infer-outlives/dont-infer-static.rs
+++ src/test/ui/rfc-2093-infer-outlives/dont-infer-static.rs
@@ -12,8 +12,14 @@
 
 #![feature(infer_outlives_requirements)]
 
+/*
+ * We don't infer `T: 'static` outlives relationships by default.
+ * Instead an additional feature gate `infer_static_outlives_requirements`
+ * is required.
+ */
+
 struct Foo<U> {
-    bar: Bar<U> //~ ERROR 16:5: 16:16: the parameter type `U` may not live long enough [E0310]
+    bar: Bar<U> //~ ERROR 22:5: 22:16: the parameter type `U` may not live long enough [E0310]
 }
 struct Bar<T: 'static> {
     x: T,
