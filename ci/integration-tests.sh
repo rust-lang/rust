a2343bfe7f2bcf1eba034a3a151a7a3a23c4ccc8
Matthias Krüger <matthias.krueger@famsik.de>
2018-07-28 12:14:35 +0200

    integration tests: add more clippy warnings for greater coverage integration/base tests: add a few code comments

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -8,7 +8,8 @@ git clone --depth=1 https://github.com/${INTEGRATION}.git checkout
 cd checkout
 
 function check() {
-  RUST_BACKTRACE=full cargo clippy --all-targets --all-features -- --cap-lints warn &> clippy_output
+# run clippy on a project, try to be verbose and trigger as many warnings as possible for greater coverage
+  RUST_BACKTRACE=full cargo clippy --all-targets --all-features -- --cap-lints warn -W clippy_pedantic -W clippy_nursery  &> clippy_output
   cat clippy_output
   ! cat clippy_output | grep -q "internal compiler error\|query stack during panic"
   if [[ $? != 0 ]]; then
