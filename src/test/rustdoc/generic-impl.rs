e0f8f871f6bbec2008af5697b8975ffe12d4571d
Guillaume Gomez <guillaume1.gomez@gmail.com>
2018-07-29 16:47:31 +0200

    Strengthen tests

--- src/test/rustdoc/generic-impl.rs
+++ src/test/rustdoc/generic-impl.rs
@@ -17,6 +17,7 @@ pub struct Bar;
 
 // @has foo/struct.Foo.html '//h3[@id="impl-ToString"]//code' 'impl<T> ToString for T'
 pub struct Foo;
+// @has foo/struct.Foo.html '//div[@class="sidebar-links"]/a[@href="#impl-ToString"]' 'ToString'
 
 impl fmt::Display for Foo {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
