1538b36c8042f22f7c3fe191016c8469991c1c89
Ralf Jung <post@ralfj.de>
2018-07-31 20:27:36 +0200

    make sure that StorageDead invalidates the backing store

---
+++ tests/compile-fail/storage_dead_dangling.rs
@@ -0,0 +1,20 @@
+static mut LEAK: usize = 0;
+
+fn fill(v: &mut i32) {
+    unsafe { LEAK = v as *mut _ as usize; }
+}
+
+fn evil() {
+    let v = unsafe { &mut *(LEAK as *mut i32) };
+    let _x = *v; //~ ERROR dangling pointer was dereferenced
+}
+
+fn main() {
+    let _y;
+    {
+        let mut x = 0i32;
+        fill(&mut x);
+        _y = x;
+    }
+    evil();
+}
