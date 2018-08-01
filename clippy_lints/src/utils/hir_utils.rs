98dbce4fe4a6f63a4d1f9e2fdd6b2752ed097af4
Michael Wright <mikerite@lavabit.com>
2018-08-01 06:32:36 +0200

    Fix E0502 warnings
    Fixes #2982

--- clippy_lints/src/utils/hir_utils.rs
+++ clippy_lints/src/utils/hir_utils.rs
@@ -448,7 +448,8 @@ impl<'a, 'tcx: 'a> SpanlessHash<'a, 'tcx> {
                     CaptureClause::CaptureByValue => 0,
                     CaptureClause::CaptureByRef => 1,
                 }.hash(&mut self.s);
-                self.hash_expr(&self.cx.tcx.hir.body(eid).value);
+                let value = &self.cx.tcx.hir.body(eid).value;
+                self.hash_expr(value);
             },
             ExprKind::Field(ref e, ref f) => {
                 let c: fn(_, _) -> _ = ExprKind::Field;
@@ -515,7 +516,8 @@ impl<'a, 'tcx: 'a> SpanlessHash<'a, 'tcx> {
                 self.hash_expr(e);
                 let full_table = self.tables;
                 self.tables = self.cx.tcx.body_tables(l_id.body);
-                self.hash_expr(&self.cx.tcx.hir.body(l_id.body).value);
+                let value = &self.cx.tcx.hir.body(l_id.body).value;
+                self.hash_expr(value);
                 self.tables = full_table;
             },
             ExprKind::Ret(ref e) => {
