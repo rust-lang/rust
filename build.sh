5c4a8b0ca05ef677b13a7b86ed4d56431021c021
bjorn3 <bjorn3@users.noreply.github.com>
2018-08-08 19:42:11 +0200

    Make sure build.sh exits with non-zero error code on errors

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ RUSTC="rustc -Zcodegen-backend=$(pwd)/../target/debug/librustc_codegen_cranelift
 $RUSTC mini_core.rs --crate-name mini_core &&
 $RUSTC example.rs &&
 $RUSTC mini_core_hello_world.rs &&
-$RUSTC ../target/libcore/src/libcore/lib.rs
+$RUSTC ../target/libcore/src/libcore/lib.rs &&
 
 rm *.rlib
