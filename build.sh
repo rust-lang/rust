f900b99e27f07cd3ce3dcca8214f01e2ba71832c
bjorn3 <bjorn3@users.noreply.github.com>
2018-08-09 10:55:13 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -12,11 +12,12 @@ else
    exit 1
 fi
 
-RUSTC="rustc -Zcodegen-backend=$(pwd)/../target/debug/librustc_codegen_cranelift.$dylib_ext -L crate=. --crate-type lib"
+RUSTC="rustc -Zcodegen-backend=$(pwd)/../target/debug/librustc_codegen_cranelift.$dylib_ext -L crate=. --crate-type lib -Og"
 
 $RUSTC mini_core.rs --crate-name mini_core &&
 $RUSTC example.rs &&
 $RUSTC mini_core_hello_world.rs &&
-$RUSTC ../target/libcore/src/libcore/lib.rs &&
+$RUSTC ../target/libcore/src/libcore/lib.rs 2>&1 | (head -n 10; tail -n 100) ||
+(rm *.rlib; exit 1)
 
 rm *.rlib
