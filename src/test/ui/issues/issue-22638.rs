dc5a6571a685189039feb9512dbcb599c1ef4102
David Wood <david@davidtw.co>
2018-08-09 12:48:31 +0200

    Fixed truncated path not being normalized.

--- src/test/ui/issues/issue-22638.rs
+++ src/test/ui/issues/issue-22638.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// normalize-stderr-test: "<\[closure@.+`" -> "$$CLOSURE`"
+
 #![allow(unused)]
 
 #![recursion_limit = "20"]
