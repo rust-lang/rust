0070b46626407f2e815993d46aef2b2637c2a4ed
Havvy (Ryan Scheel) <ryan.havvy@gmail.com>
2018-08-11 13:02:49 -0700

    Fix indent

--- src/libstd/process.rs
+++ src/libstd/process.rs
@@ -390,7 +390,7 @@ impl fmt::Debug for ChildStderr {
 ///
 /// let mut echo_hello = Command::new("sh");
 /// echo_hello.arg("-c")
-///            .arg("echo hello");
+///           .arg("echo hello");
 /// let hello_1 = echo_hello.output().expect("failed to execute process");
 /// let hello_2 = echo_hello.output().expect("failed to execute process");
 /// ```
