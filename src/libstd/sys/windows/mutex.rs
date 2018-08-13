8a72954d7a900736ce85f05cfa8c0df1c013acaa
Ralf Jung <post@ralfj.de>
2018-08-13 21:15:42 +0200

    Window Mutex: make sure we properly initialize the SRWLock

--- src/libstd/sys/windows/mutex.rs
+++ src/libstd/sys/windows/mutex.rs
@@ -58,6 +58,8 @@ pub unsafe fn raw(m: &Mutex) -> c::PSRWLOCK {
 impl Mutex {
     pub const fn new() -> Mutex {
         Mutex {
+            // This works because SRWLOCK_INIT is a NULL pointer, so we are also properly
+            // initializing an SRWLOCK here.
             lock: AtomicUsize::new(0),
             held: UnsafeCell::new(false),
         }
