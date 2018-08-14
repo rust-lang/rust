98a5b24ef7eb05ae52d63ce9e050d6ac144ba345
Ralf Jung <post@ralfj.de>
2018-08-14 20:09:07 +0200

    test some more things around packed structs

--- tests/run-pass/packed_struct.rs
+++ tests/run-pass/packed_struct.rs
@@ -45,6 +45,43 @@ fn test_unsizing() {
     let _unused = &arr_unaligned; // forcing an allocation, which could also yield "unaligned write"-errors
 }
 
+fn test_drop() {
+    struct Wrap(u32);
+    impl Drop for Wrap {
+        fn drop(&mut self) {
+            // Do an (aligned) load
+            let _test = self.0;
+            // For the fun of it, test alignment
+            assert_eq!(&self.0 as *const _ as usize % std::mem::align_of::<u32>(), 0);
+        }
+    }
+
+    #[repr(packed,C)]
+    struct Packed<T> {
+        f1: u8, // this should move the second field to something not very aligned
+        f2: T,
+    }
+
+    let p = Packed { f1: 42, f2: Wrap(23) };
+    drop(p);
+}
+
+fn test_inner_packed() {
+    // Even if just the inner struct is packed, accesses to the outer field can get unaligned.
+    // Make sure that works.
+    #[repr(packed)]
+    #[derive(Clone,Copy)]
+    struct Inner(u32);
+
+    #[derive(Clone,Copy)]
+    struct Outer(u8, Inner);
+
+    let o = Outer(0, Inner(42));
+    let _x = o.1;
+    let _y = (o.1).0;
+    let _o2 = o.clone();
+}
+
 fn main() {
     let mut x = S {
         a: 42,
@@ -64,4 +101,6 @@ fn main() {
     test(Test2 { x: 0, other: &Test1 { x: 0, other: &42 }});
 
     test_unsizing();
+    test_drop();
+    test_inner_packed();
 }
