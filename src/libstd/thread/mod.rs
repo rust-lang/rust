34b65db8420ecfec94126483c7c170c270fd2ead
Ralf Jung <post@ralfj.de>
2018-08-15 15:22:54 +0200

    document effect of join on memory ordering

--- src/libstd/thread/mod.rs
+++ src/libstd/thread/mod.rs
@@ -1308,13 +1308,17 @@ impl<T> JoinHandle<T> {
         &self.0.thread
     }
 
-    /// Waits for the associated thread to finish.
+    /// Waits for the associated thread to finish. In terms of [atomic memory orderings],
+    /// the completion of the associated thread synchronizes with this function returning.
+    /// In other words, all operations performed by that thread are ordered before all
+    /// operations that happen after `join` returns.
     ///
     /// If the child thread panics, [`Err`] is returned with the parameter given
     /// to [`panic`].
     ///
     /// [`Err`]: ../../std/result/enum.Result.html#variant.Err
     /// [`panic`]: ../../std/macro.panic.html
+    /// [atomic memory orderings]: ../../std/sync/atomic/index.html
     ///
     /// # Panics
     ///
