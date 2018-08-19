b7772e6c9e27cb4bbdcb735789b937dc63ca189b
Mazdak Farrokhzad <twingoow@gmail.com>
2018-08-19 21:06:43 +0200

    convert-id: tests for const gating.

---
+++ src/test/ui/rfc-2306/convert-id-const-with-gate.rs
@@ -0,0 +1,20 @@
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// This test should pass since we've opted into 'identity' as an
+// unstable const fn.
+
+// compile-pass
+
+#![feature(convert_id, const_convert_id)]
+
+fn main() {
+    const _FOO: u8 = ::std::convert::identity(42u8);
+}
