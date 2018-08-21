89574a6fc7c660172030724a5d1e8c0ef83094a4
Niko Matsakis <niko@alum.mit.edu>
2018-08-21 19:23:30 -0400

    resolve type variables in the custom type op pathway

--- src/librustc/traits/query/type_op/custom.rs
+++ src/librustc/traits/query/type_op/custom.rs
@@ -106,7 +106,8 @@ fn scrape_region_constraints<'gcx, 'tcx, R>(
         infcx.tcx,
         region_obligations
             .iter()
-            .map(|(_, r_o)| (r_o.sup_type, r_o.sub_region)),
+            .map(|(_, r_o)| (r_o.sup_type, r_o.sub_region))
+            .map(|(ty, r)| (infcx.resolve_type_vars_if_possible(&ty), r)),
         &region_constraint_data,
     );
 
