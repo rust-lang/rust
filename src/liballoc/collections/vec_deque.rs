f8d5ed47e50fa964878263572f583d8a96ddc910
MaloJaffre <jaffre.malo@gmail.com>
2018-08-22 10:22:21 +0200

    Add a test for issue #53529

--- src/liballoc/collections/vec_deque.rs
+++ src/liballoc/collections/vec_deque.rs
@@ -2967,4 +2967,21 @@ mod tests {
         }
     }
 
+    #[test]
+    fn issue_53529() {
+        use boxed::Box;
+
+        let mut dst = VecDeque::new();
+        dst.push_front(Box::new(1));
+        dst.push_front(Box::new(2));
+        assert_eq!(*dst.pop_back().unwrap(), 1);
+
+        let mut src = VecDeque::new();
+        src.push_front(Box::new(2));
+        dst.append(&mut src);
+        for a in dst {
+            assert_eq!(*a, 2);
+        }
+    }
+
 }
