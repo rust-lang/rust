6c101422514104dc7dbca13bab7a4e8b3b06f030
Alex Crichton <alex@alexcrichton.com>
2018-08-22 11:17:36 -0700

    Update LLVM submodule
    This commit updates the LLVM submodule to the current trunk of LLVM itself. This
    brings a few notable improvements for the wasm target:
    
    * Support for wasm atomic instructions is greatly improved
    * Renamed memory wasm intrinsics are fully supported
    * LLD has fixed a quadratic execution bug with large numbers of relocations in
      wasm files.
    
    The compiler-rt submodule has been updated in tandem as well.

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -145,6 +145,7 @@ impl Step for Llvm {
            .define("LLVM_INCLUDE_EXAMPLES", "OFF")
            .define("LLVM_INCLUDE_TESTS", "OFF")
            .define("LLVM_INCLUDE_DOCS", "OFF")
+           .define("LLVM_INCLUDE_BENCHMARKS", "OFF")
            .define("LLVM_ENABLE_ZLIB", "OFF")
            .define("WITH_POLLY", "OFF")
            .define("LLVM_ENABLE_TERMINFO", "OFF")
