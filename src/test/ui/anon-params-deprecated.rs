88037a558711e5182535c577de91429445586634
Mark Mansi <markm@cs.wisc.edu>
2018-08-22 15:07:27 -0500

    Add/update tests for 2015, 2018, rustfix

--- src/test/ui/anon-params-deprecated.rs
+++ src/test/ui/anon-params-deprecated.rs
@@ -8,17 +8,21 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![forbid(anonymous_parameters)]
+#![warn(anonymous_parameters)]
 // Test for the anonymous_parameters deprecation lint (RFC 1685)
 
+// compile-pass
+// edition:2015
+// run-rustfix
+
 trait T {
-    fn foo(i32); //~ ERROR anonymous parameters are deprecated
+    fn foo(i32); //~ WARNING anonymous parameters are deprecated
                  //~| WARNING hard error
 
     fn bar_with_default_impl(String, String) {}
-    //~^ ERROR anonymous parameters are deprecated
+    //~^ WARNING anonymous parameters are deprecated
     //~| WARNING hard error
-    //~| ERROR anonymous parameters are deprecated
+    //~| WARNING anonymous parameters are deprecated
     //~| WARNING hard error
 }
 
