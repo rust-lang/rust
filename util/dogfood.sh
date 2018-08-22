efeed9aefc172315fd840d27514069f7c0d27329
flip1995 <9744647+flip1995@users.noreply.github.com>
2018-08-22 17:47:54 +0200

    Remove unused code

--- util/dogfood.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf target*/*so
-cargo build --lib && cp -R target target_recur && cargo rustc --lib -- -Zextra-plugins=clippy -Ltarget_recur/debug -Dclippy_pedantic -Dclippy || exit 1
-rm -rf target_recur
-
