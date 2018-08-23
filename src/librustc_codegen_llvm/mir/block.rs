a6d011a98668cf672db2de8aae42f3654df4787f
Jorge Aparicio <jorge@japaric.io>
2018-08-23 16:18:28 +0200

    adapt to change in Session API

--- src/librustc_codegen_llvm/mir/block.rs
+++ src/librustc_codegen_llvm/mir/block.rs
@@ -466,7 +466,7 @@ impl FunctionCx<'a, 'll, 'tcx> {
                 if (intrinsic == Some("init") || intrinsic == Some("uninit")) &&
                     bx.cx.layout_of(sig.output()).abi.is_uninhabited()
                 {
-                    let loc = bx.sess().codemap().lookup_char_pos(span.lo());
+                    let loc = bx.sess().source_map().lookup_char_pos(span.lo());
                     let filename = Symbol::intern(&loc.file.name.to_string()).as_str();
                     let filename = C_str_slice(bx.cx, filename);
                     let line = C_u32(bx.cx, loc.line as u32);
