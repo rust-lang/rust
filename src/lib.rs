e66f012d7921a409e436ff5b13bd7538e33f9be5
bjorn3 <bjorn3@users.noreply.github.com>
2018-08-26 16:28:00 +0200

    Remove stabilized feature gate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(rustc_private, macro_at_most_once_rep, iterator_find_map)]
+#![feature(rustc_private, macro_at_most_once_rep)]
 #![allow(intra_doc_link_resolution_failure)]
 
 extern crate syntax;
