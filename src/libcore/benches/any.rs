50057ee3a364d51f5dee611ebd1fd3a05ac77ff5
Matthias Krüger <matthias.krueger@famsik.de>
2018-08-29 12:18:05 +0200

    bench: libcore: fix build failure of any.rs benchmark (use "dyn Any")

--- src/libcore/benches/any.rs
+++ src/libcore/benches/any.rs
@@ -15,7 +15,7 @@ use test::{Bencher, black_box};
 fn bench_downcast_ref(b: &mut Bencher) {
     b.iter(|| {
         let mut x = 0;
-        let mut y = &mut x as &mut Any;
+        let mut y = &mut x as &mut dyn Any;
         black_box(&mut y);
         black_box(y.downcast_ref::<isize>() == Some(&0));
     });
