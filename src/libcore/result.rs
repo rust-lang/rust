71b16d8350d6f3dcc9b1d624de9332d99ea3acd0
Isaac van Bakel <ivb@vanbakel.io>
2018-08-29 14:58:27 +0100

    Corrected bad typing in Result::map_or_else doc
    The error value now includes the type of the success.
    The success value now includes the type of the error.

--- src/libcore/result.rs
+++ src/libcore/result.rs
@@ -487,10 +487,10 @@ impl<T, E> Result<T, E> {
     /// ```
     /// let k = 21;
     ///
-    /// let x = Ok("foo");
+    /// let x : Result<_, &str> = Ok("foo");
     /// assert_eq!(x.map_or_else(|e| k * 2, |v| v.len()), 3);
     ///
-    /// let x = Err("bar");
+    /// let x : Result<&str, _> = Err("bar");
     /// assert_eq!(x.map_or_else(|e| k * 2, |v| v.len()), 42);
     /// ```
     #[inline]
