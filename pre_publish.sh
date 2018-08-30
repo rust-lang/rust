559b2f871f9907eab4be3cc58e118ff37a5468d0
Philipp Hansch <dev@phansch.net>
2018-08-30 07:33:53 +0200

    Remove git diffing part
    Because we no bump versions

--- pre_publish.sh
+++ pre_publish.sh
@@ -4,13 +4,6 @@ set -e
 
 ./util/update_lints.py
 
-git status --short | sort | grep -v README.md | grep -v helper.txt | sort > helper.txt
-
-# abort if the files differ
-diff "publish.files" "helper.txt"
-
-rm helper.txt
-
 # add all changed files
 git add .
 git commit -m "Bump the version"
