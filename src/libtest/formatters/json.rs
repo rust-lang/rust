347c6b76a001a73c994b0abd5f1f7205cd922ac2
Charlie Andrews <charlie@cloudflare.com>
2018-08-31 14:42:01 -0500

    Make json test output formatter represent "test_count" as num

--- src/libtest/formatters/json.rs
+++ src/libtest/formatters/json.rs
@@ -50,7 +50,7 @@ impl<T: Write> JsonFormatter<T> {
 impl<T: Write> OutputFormatter for JsonFormatter<T> {
     fn write_run_start(&mut self, test_count: usize) -> io::Result<()> {
         self.write_message(&*format!(
-            r#"{{ "type": "suite", "event": "started", "test_count": "{}" }}"#,
+            r#"{{ "type": "suite", "event": "started", "test_count": {} }}"#,
             test_count
         ))
     }
