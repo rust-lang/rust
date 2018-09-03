20318ebc22ea25bd6150e1c0a3657854d16accbf
Philipp Hansch <dev@phansch.net>
2018-09-03 22:27:35 +0200

    Cleanup old min_version stuff
    This cleans up a few leftover things after
    https://github.com/rust-lang-nursery/rust-clippy/pull/3018

--- build.rs
+++ build.rs
@@ -1,18 +1,3 @@
-//! This build script ensures that Clippy is not compiled with an
-//! incompatible version of rust. It will panic with a descriptive
-//! error message instead.
-//!
-//! We specifially want to ensure that Clippy is only built with a
-//! rustc version that is newer or equal to the one specified in the
-//! `min_version.txt` file.
-//!
-//! `min_version.txt` is in the repo but also in the `.gitignore` to
-//! make sure that it is not updated manually by accident. Only CI
-//! should update that file.
-//!
-//! This build script was originally taken from the Rocket web framework:
-//! https://github.com/SergioBenitez/Rocket
-
 use std::env;
 
 fn main() {
