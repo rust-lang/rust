bac0eb2f37e13fb3491507a8deaa344afb8e2de9
Jorge Aparicio <jorge@japaric.io>
2018-09-03 16:52:43 +0200

    whitelist some ARM features

--- src/librustc_codegen_llvm/llvm_util.rs
+++ src/librustc_codegen_llvm/llvm_util.rs
@@ -86,10 +86,14 @@ unsafe fn configure_llvm(sess: &Session) {
 // array, leading to crashes.
 
 const ARM_WHITELIST: &[(&str, Option<&str>)] = &[
+    ("aclass", Some("arm_target_feature")),
     ("mclass", Some("arm_target_feature")),
     ("rclass", Some("arm_target_feature")),
     ("dsp", Some("arm_target_feature")),
     ("neon", Some("arm_target_feature")),
+    ("v5te", Some("arm_target_feature")),
+    ("v6k", Some("arm_target_feature")),
+    ("v6t2", Some("arm_target_feature")),
     ("v7", Some("arm_target_feature")),
     ("vfp2", Some("arm_target_feature")),
     ("vfp3", Some("arm_target_feature")),
