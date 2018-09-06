5af06768a9b80afd9c1b8bbed695152d4ac5d9db
Mark Rousskov <mark.simulacrum@gmail.com>
2018-09-06 10:47:41 -0600

    Remove crate_visibility_modifier from 2018 edition

--- src/test/ui/rust-2018/edition-lint-nested-empty-paths.rs
+++ src/test/ui/rust-2018/edition-lint-nested-empty-paths.rs
@@ -10,7 +10,7 @@
 
 // run-rustfix
 
-#![feature(rust_2018_preview)]
+#![feature(rust_2018_preview, crate_visibility_modifier)]
 #![deny(absolute_paths_not_starting_with_crate)]
 #![allow(unused_imports)]
 #![allow(dead_code)]
