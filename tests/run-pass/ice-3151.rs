43549ebbf88f46012f898ddd759ba1906c1c80c9
Pascal Seitz <pascal.seitz@gmail.com>
2018-09-08 15:30:50 +0200

    fixes #3151 by skipping the lint instead of crashing

---
+++ tests/run-pass/ice-3151.rs
@@ -0,0 +1,13 @@
+#[derive(Clone)]
+pub struct HashMap<V, S> {
+    hash_builder: S,
+    table: RawTable<V>,
+}
+
+#[derive(Clone)]
+pub struct RawTable<V> {
+    size: usize,
+    val: V
+}
+
+fn main() {}
