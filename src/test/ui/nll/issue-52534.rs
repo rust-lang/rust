97bbcabef15309ba9ab9bc9d365d4524ce26fc09
David Wood <david@davidtw.co>
2018-09-17 10:48:44 +0200

    Added multiple parameter closure test.
    New test has multiple parameters in a closure with longer names in order
    to clarify the issues relating to odd spans.

--- src/test/ui/nll/issue-52534.rs
+++ src/test/ui/nll/issue-52534.rs
@@ -14,9 +14,17 @@
 fn foo(_: impl FnOnce(&u32) -> &u32) {
 }
 
+fn baz(_: impl FnOnce(&u32, u32) -> &u32) {
+}
+
 fn bar() {
     let x = 22;
     foo(|a| &x)
+}
+
+fn foobar() {
+    let y = 22;
+    baz(|first, second| &y)
 }
 
 fn main() { }
