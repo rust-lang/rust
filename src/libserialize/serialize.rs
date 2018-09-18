99f05e800ed806b6612f334d0e1d00343a911724
Nicholas Nethercote <nnethercote@mozilla.com>
2018-09-18 17:03:24 +1000

    Improve handling of type bounds in `bit_set.rs`.
    Currently, `BitSet` doesn't actually know its own domain size; it just
    knows how many words it contains. To improve things, this commit makes
    the following changes.
    
    - It changes `BitSet` and `SparseBitSet` to store their own domain size,
      and do more precise bounds and same-size checks with it. It also
      changes the signature of `BitSet::to_string()` (and puts it within
      `impl ToString`) now that the domain size need not be passed in from
      outside.
    
    - It uses `derive(RustcDecodable, RustcEncodable)` for `BitSet`. This
      required adding code to handle `PhantomData` in `libserialize`.
    
    - As a result, it removes the domain size from `HybridBitSet`, making a
      lot of that code nicer.
    
    - Both set_up_to() and clear_above() were overly general, working with
      arbitrary sizes when they are only needed for the domain size. The
      commit removes the former, degeneralizes the latter, and removes the
      (overly general) tests.
    
    - Changes `GrowableBitSet::grow()` to `ensure()`, fixing a bug where a
      (1-based) domain size was confused with a (0-based) element index.
    
    - Changes `BitMatrix` to store its row count, and do more precise bounds
      checks with it.
    
    - Changes `ty_params` in `select.rs` from a `BitSet` to a
      `GrowableBitSet` because it repeatedly failed the new, more precise
      bounds checks. (Changing the type was simpler than computing an
      accurate domain size.)
    
    - Various other minor improvements.

--- src/libserialize/serialize.rs
+++ src/libserialize/serialize.rs
@@ -16,6 +16,7 @@ Core encoding and decoding interfaces.
 
 use std::borrow::Cow;
 use std::intrinsics;
+use std::marker::PhantomData;
 use std::path;
 use std::rc::Rc;
 use std::cell::{Cell, RefCell};
@@ -544,6 +545,19 @@ impl Encodable for () {
 impl Decodable for () {
     fn decode<D: Decoder>(d: &mut D) -> Result<(), D::Error> {
         d.read_nil()
+    }
+}
+
+impl<T> Encodable for PhantomData<T> {
+    fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
+        s.emit_unit()
+    }
+}
+
+impl<T> Decodable for PhantomData<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<PhantomData<T>, D::Error> {
+        d.read_nil()?;
+        Ok(PhantomData)
     }
 }
 
