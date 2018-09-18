c6bb2e7223d467e124d7e8a36c94d608ea4846b0
bjorn3 <bjorn3@users.noreply.github.com>
2018-09-18 18:41:41 +0200

    Improve prepare_libcore.sh

--- prepare_libcore.sh
+++ prepare_libcore.sh
@@ -1,11 +1,14 @@
 #!/bin/bash --verbose
+set -e
+
 SRC_DIR="target/libcore"
-rm -rf $SRC_DIR &&
-mkdir -p $SRC_DIR/src &&
+rm -rf $SRC_DIR
+mkdir -p $SRC_DIR/src
 cp -r $(dirname $(rustup which rustc))/../lib/rustlib/src/rust/src/libcore $SRC_DIR/src/libcore || (echo "Please install rust-src component"; exit 1)
-cd $SRC_DIR || exit 1
-git init || exit 1
-git add . || exit 1
-git commit -m "Initial commit" -q || exit 1
-git apply ../../000*.patch || exit 1
+cd $SRC_DIR
+git init
+git add .
+git commit -m "Initial commit" -q
+git apply ../../000*.patch
+
 echo "Successfully prepared libcore for building"
