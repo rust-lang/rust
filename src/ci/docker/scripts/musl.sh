97723db7cf2e19dfd51721d5361c898b52f5a57f
Adrian Budau <adbudau@bitdefender.com>
2018-09-21 17:03:13 +0300

    Update musl to 1.1.20.

--- src/ci/docker/scripts/musl.sh
+++ src/ci/docker/scripts/musl.sh
@@ -32,17 +32,11 @@ shift
 
 export CFLAGS="-fPIC $CFLAGS"
 
-# FIXME: remove the patch when updating to 1.1.20
-MUSL=musl-1.1.19
+MUSL=musl-1.1.20
 
 # may have been downloaded in a previous run
 if [ ! -d $MUSL ]; then
   curl https://www.musl-libc.org/releases/$MUSL.tar.gz | tar xzf -
-  # Patch to fix https://github.com/rust-lang/rust/issues/48967
-  cd $MUSL && \
-    curl "https://git.musl-libc.org/cgit/musl/patch/?id=610c5a8524c3d6cd3ac5a5f1231422e7648a3791" |\
-    patch -p1 && \
-    cd -
 fi
 
 cd $MUSL
