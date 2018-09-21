ce782b934b96940495b619e1b3149cb058c6d089
Alex Crichton <alex@alexcrichton.com>
2018-09-21 10:05:23 -0700

    Ignore new test on Windows

--- src/test/incremental-fulldeps/issue-54059.rs
+++ src/test/incremental-fulldeps/issue-54059.rs
@@ -11,6 +11,7 @@
 // aux-build:issue_54059.rs
 // ignore-stage1
 // ignore-wasm32-bare no libc for ffi testing
+// ignore-windows - dealing with weird symbols issues on dylibs isn't worth it
 // revisions: rpass1
 
 extern crate issue_54059;
