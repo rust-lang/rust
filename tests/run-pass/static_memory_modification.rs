f18cb40e4ce0d0fa7ce60c17d2783edffde783f2
Ralf Jung <post@ralfj.de>
2018-09-23 09:14:55 +0200

    test mutating a non-mut static with interior mutability

--- tests/run-pass/static_memory_modification.rs
+++ tests/run-pass/static_memory_modification.rs
@@ -1,8 +1,14 @@
+use std::sync::atomic::{Ordering, AtomicUsize};
+
 static mut X: usize = 5;
+static Y: AtomicUsize = AtomicUsize::new(5);
 
 fn main() {
     unsafe {
         X = 6;
         assert_eq!(X, 6);
     }
+
+    Y.store(6, Ordering::Relaxed);
+    assert_eq!(Y.load(Ordering::Relaxed), 6);
 }
