a7cce470b6c6524249b25449d85b12ce62aef4af
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2018-10-04 17:31:35 +0400

    Fix two UI tests with locale-dependent output

--- src/test/ui/macros/macros-nonfatal-errors.rs
+++ src/test/ui/macros/macros-nonfatal-errors.rs
@@ -1,15 +1,4 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
-// normalize-stderr-test: "The system cannot find the file specified\." -> "No such file or directory"
-// ignore-tidy-linelength
+// normalize-stderr-test: "existed:.*\(" -> "existed: $$FILE_NOT_FOUND_MSG ("
 
 // test that errors in a (selection) of macros don't kill compilation
 // immediately, so that we get more errors listed at a time.
