8b3d2073fa8b1f48a27081944c1a7010a8d7b7b1
Philipp Hansch <dev@phansch.net>
2018-10-04 22:26:54 +0200

    Only run tests if pointer width is 64bit
    If the pointer width of the architechture is 32bit or something else,
    then the tests will most likely produce different results.

--- tests/ui/fn_to_numeric_cast_with_truncation.rs
+++ tests/ui/fn_to_numeric_cast_with_truncation.rs
@@ -1,3 +1,4 @@
+// only-64bit
 #![feature(tool_lints)]
 
 #![warn(clippy::fn_to_numeric_cast_with_truncation)]
@@ -13,11 +14,12 @@ fn test_fn_to_numeric_cast_with_truncation() {
     let _ = foo as u16;
     let _ = foo as u32;
 
-    // TODO: Is it bad to have these tests?
-    // Running the tests on a different architechture will
-    // produce different results
+    // These should not lint, because because casting to these types
+    // does not truncate the function pointer address.
     let _ = foo as u64;
     let _ = foo as i64;
+    let _ = foo as u128;
+    let _ = foo as i128;
 }
 
 fn main() {}
