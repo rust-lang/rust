7e5a13163ec0a724e67faf222d2f5b0872dc48d1
Jonas Schievink <jonasschievink@gmail.com>
2018-10-04 14:53:16 +0200

    Convert issue-49376.rs to compile-pass

--- src/test/ui/impl-trait/issue-49376.rs
+++ src/test/ui/impl-trait/issue-49376.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// run-pass
+// compile-pass
 
 // Tests for nested self-reference which caused a stack overflow.
 
