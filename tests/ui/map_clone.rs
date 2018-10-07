9bd4e5469e1ecb7d98602ba1d805872faa8bf2c9
Ryan Cumming <etaoins@gmail.com>
2018-10-08 06:20:32 +1100

    Don't suggest cloned() for map Box deref
    Boxes are a bit magic in that they need to use `*` to get an owned value
    out of the box. They implement `Deref` but that only returns a
    reference. This means an easy way to convert an `Option<Box<T>>` to an
    `<Option<T>` is:
    
    ```
    box_option.map(|b| *b)
    ```
    
    However, since b36bb0a6 the `map_clone` lint is detecting this as an
    attempt to copy the box. Fix by excluding boxes completely from the
    deref part of this lint.
    
    Fixes #3274

--- tests/ui/map_clone.rs
+++ tests/ui/map_clone.rs
@@ -16,4 +16,5 @@ fn main() {
     let _: Vec<i8> = vec![5_i8; 6].iter().map(|x| *x).collect();
     let _: Vec<String> = vec![String::new()].iter().map(|x| x.clone()).collect();
     let _: Vec<u32> = vec![42, 43].iter().map(|&x| x).collect();
+    let _: Option<u64> = Some(Box::new(16)).map(|b| *b);
 }
