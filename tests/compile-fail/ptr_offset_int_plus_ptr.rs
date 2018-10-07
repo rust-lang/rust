59eb3dbdae487f67d81c2e5d46dcbd636c9d0cf9
Ralf Jung <post@ralfj.de>
2018-10-07 10:35:34 +0200

    use Box::into_raw

--- tests/compile-fail/ptr_offset_int_plus_ptr.rs
+++ tests/compile-fail/ptr_offset_int_plus_ptr.rs
@@ -1,7 +1,7 @@
 // error-pattern: pointer value as raw bytes
 
 fn main() {
-    let ptr = &mut *Box::new(0u32) as *mut u32;
+    let ptr = Box::into_raw(Box::new(0u32));
     // Can't start with an integer pointer and get to something usable
     unsafe {
         let _ = (1 as *mut u8).offset(ptr as isize);
