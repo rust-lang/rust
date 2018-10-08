ae130b0889181e63b96bc32d5ce17b432f9225b1
Scott McMurray <scottmcm@users.noreply.github.com>
2018-10-08 00:00:58 -0700

    Move an underscore-lifetime error test to an IHLE success test

--- src/test/ui/impl-header-lifetime-elision/inherent-impl.rs
+++ src/test/ui/impl-header-lifetime-elision/inherent-impl.rs
@@ -8,25 +8,12 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// compile-pass
+
 struct Foo<'a>(&'a u8);
-struct Baz<'a>(&'_ &'a u8); //~ ERROR missing lifetime specifier
 
-fn foo<'_> //~ ERROR cannot be used here
-(_: Foo<'_>) {}
-
-trait Meh<'a> {}
-impl<'a> Meh<'a> for u8 {}
-
-fn meh() -> Box<for<'_> Meh<'_>> //~ ERROR cannot be used here
-//~^ ERROR missing lifetime specifier
-{
-  Box::new(5u8)
+impl Foo<'_> {
+    fn x() {}
 }
 
-fn foo2(_: &'_ u8, y: &'_ u8) -> &'_ u8 { y } //~ ERROR missing lifetime specifier
-
-fn main() {
-    let x = 5;
-    foo(Foo(&x));
-    let _ = meh();
-}
+fn main() {}
