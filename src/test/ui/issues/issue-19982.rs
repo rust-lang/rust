faf68f473ff70743c316e91632c4616ea62f820f
Scott McMurray <scottmcm@users.noreply.github.com>
2018-10-07 23:48:51 -0700

    Hooray! 19982 finally works the way people wanted in December 2014 :)

--- src/test/ui/issues/issue-19982.rs
+++ src/test/ui/issues/issue-19982.rs
@@ -8,10 +8,25 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![feature(unboxed_closures)]
+// compile-pass
 
+#![feature(fn_traits, unboxed_closures)]
+
+#[allow(dead_code)]
 struct Foo;
 
-impl Fn<(&(),)> for Foo { } //~ ERROR missing lifetime specifier
+impl Fn<(&(),)> for Foo {
+    extern "rust-call" fn call(&self, (_,): (&(),)) {}
+}
+
+impl FnMut<(&(),)> for Foo {
+    extern "rust-call" fn call_mut(&mut self, (_,): (&(),)) {}
+}
+
+impl FnOnce<(&(),)> for Foo {
+    type Output = ();
+
+    extern "rust-call" fn call_once(self, (_,): (&(),)) {}
+}
 
 fn main() {}
