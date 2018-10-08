3a405b65d61a5ad3176c223e4b7226a43fcd186e
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2018-10-08 22:36:47 +0100

    Add tslint and prettier to ci

--- editors/code/src/config.ts
+++ editors/code/src/config.ts
@@ -3,21 +3,21 @@ import * as vscode from 'vscode';
 import { Server } from './server';
 
 export class Config {
-    public highlightingOn = true;
+  public highlightingOn = true;
 
-    constructor() {
-        vscode.workspace.onDidChangeConfiguration((_) => this.userConfigChanged());
-        this.userConfigChanged();
+  constructor() {
+    vscode.workspace.onDidChangeConfiguration((_) => this.userConfigChanged());
+    this.userConfigChanged();
+  }
+
+  public userConfigChanged() {
+    const config = vscode.workspace.getConfiguration('ra-lsp');
+    if (config.has('highlightingOn')) {
+      this.highlightingOn = config.get('highlightingOn') as boolean;
     }
 
-    public userConfigChanged() {
-        const config = vscode.workspace.getConfiguration('ra-lsp');
-        if (config.has('highlightingOn')) {
-            this.highlightingOn = config.get('highlightingOn') as boolean;
-        }
-
-        if (!this.highlightingOn && Server) {
-            Server.highlighter.removeHighlights();
-        }
+    if (!this.highlightingOn && Server) {
+      Server.highlighter.removeHighlights();
     }
+  }
 }
