a090edbc03b95eb21f75675c026e674d646cae74
Ralf Jung <post@ralfj.de>
2018-10-10 09:06:44 +0200

    explain a test

--- tests/run-pass/ref-invalid-ptr.rs
+++ tests/run-pass/ref-invalid-ptr.rs
@@ -1,7 +1,9 @@
 fn main() {
     let x = 2usize as *const u32;
+    // this is not aligned, but we immediately cast it to a raw ptr so that must be okay
     let _y = unsafe { &*x as *const u32 };
 
     let x = 0usize as *const u32;
+    // this is NULL, but we immediately cast it to a raw ptr so that must be okay
     let _y = unsafe { &*x as *const u32 };
 }
