6391b7ab352d0ae79a6ed547efac39b7f8c6f644
Otavio Salvador <otavio@ossystems.com.br>
2018-10-11 10:21:15 -0300

    rewrite: adjust comment as it refers to `is_if_else_block`
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- src/rewrite.rs
+++ src/rewrite.rs
@@ -40,7 +40,7 @@ pub struct RewriteContext<'a> {
     pub inside_macro: RefCell<bool>,
     // Force block indent style even if we are using visual indent style.
     pub use_block: RefCell<bool>,
-    // When `format_if_else_cond_comment` is true, unindent the comment on top
+    // When `is_if_else_block` is true, unindent the comment on top
     // of the `else` or `else if`.
     pub is_if_else_block: RefCell<bool>,
     // When rewriting chain, veto going multi line except the last element
