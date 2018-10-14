4dcf49121cf4b195929099cedac19a489d403eb4
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-10-14 17:45:35 +0200

    Use platform independent types

--- src/test/ui/issues/issue-54410.rs
+++ src/test/ui/issues/issue-54410.rs
@@ -1,6 +1,5 @@
-use std::os::raw::c_char;
 extern "C" {
-    pub static mut symbol: [c_char];
+    pub static mut symbol: [i8];
     //~^ ERROR the size for values of type `[i8]` cannot be known at compilation time
 }
 
