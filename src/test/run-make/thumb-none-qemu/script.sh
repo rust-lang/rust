8d2b2ee5c2ea2e7de7bb14aeaae2c6f3b75a0305
Hideki Sekine <sekineh@me.com>
2018-10-16 20:56:02 +0900

    [ci] run-make/thumb-none-qemu: add example crate.

--- src/test/run-make/thumb-none-qemu/script.sh
+++ src/test/run-make/thumb-none-qemu/script.sh
@@ -1,16 +1,13 @@
 set -exuo pipefail
 
-CRATE=cortex-m-rt
-CRATE_URL=https://github.com/rust-embedded/cortex-m-rt
-CRATE_SHA1=62972c8a89ff54b76f9ef0d600c1fcf7a233aabd
+CRATE=example
 
 env | sort
 mkdir -p $WORK_DIR
 pushd $WORK_DIR
     rm -rf $CRATE || echo OK
-    bash -x $HERE/../git_clone_sha1.sh $CRATE $CRATE_URL $CRATE_SHA1
+    cp -a $HERE/example .
     pushd $CRATE
-        $CARGO run --target $TARGET --example qemu           | grep "x = 42"
-        $CARGO run --target $TARGET --example qemu --release | grep "x = 42"
+        $CARGO run --target $TARGET
     popd
 popd
