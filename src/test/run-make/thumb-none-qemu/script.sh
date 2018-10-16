f40fd4bace651be6fad51723b139dbebbf00f883
Hideki Sekine <sekineh@me.com>
2018-10-16 21:07:44 +0900

    [ci] run both debug and release

--- src/test/run-make/thumb-none-qemu/script.sh
+++ src/test/run-make/thumb-none-qemu/script.sh
@@ -8,6 +8,7 @@ pushd $WORK_DIR
     rm -rf $CRATE || echo OK
     cp -a $HERE/example .
     pushd $CRATE
-        $CARGO run --target $TARGET
+        $CARGO run --target $TARGET           | grep "x = 42"
+        $CARGO run --target $TARGET --release | grep "x = 42"
     popd
 popd
