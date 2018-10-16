42297390386a9b3f55fec7299b27ee51f41796e3
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-10-16 17:07:41 +0200

    Add ignore-compare-mode-nll to some run-pass tests that become compile-fail under #54986.

--- src/test/run-pass/issues/issue-26996.rs
+++ src/test/run-pass/issues/issue-26996.rs
@@ -9,6 +9,20 @@
 // except according to those terms.
 
 // run-pass
+
+// This test is bogus (i.e. should be compile-fail) during the period
+// where #54986 is implemented and #54987 is *not* implemented. For
+// now: just ignore it under nll
+//
+// ignore-compare-mode-nll
+
+// This test is checking that the write to `c.0` (which has been moved out of)
+// won't overwrite the state in `c2`.
+//
+// That's a fine thing to test when this code is accepted by the
+// compiler, and this code is being transcribed accordingly into
+// the ui test issue-21232-partial-init-and-use.rs
+
 fn main() {
     let mut c = (1, "".to_owned());
     match c {
