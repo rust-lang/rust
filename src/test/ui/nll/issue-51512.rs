5b748434afe19c8ec8091413e843df9b3679da3e
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-10-16 17:10:04 +0200

    updates to expected output for other ui tests.

--- src/test/ui/nll/issue-51512.rs
+++ src/test/ui/nll/issue-51512.rs
@@ -15,5 +15,5 @@ fn main() {
     let range = 0..1;
     let r = range;
     let x = range.start;
-    //~^ ERROR use of moved value: `range.start` [E0382]
+    //~^ ERROR use of moved value: `range` [E0382]
 }
