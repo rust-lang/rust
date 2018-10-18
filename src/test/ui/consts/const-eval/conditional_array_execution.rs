d5d5e8c5f52ccc5f88f8cf691118e364e64bf75b
Niko Matsakis <niko@alum.mit.edu>
2018-10-18 05:58:20 -0400

    lowering casts in constants now creates multiple uses
    This can trigger more errors than before. Not sure what is the best
    fix here.

--- src/test/ui/consts/const-eval/conditional_array_execution.rs
+++ src/test/ui/consts/const-eval/conditional_array_execution.rs
@@ -14,9 +14,10 @@ const X: u32 = 5;
 const Y: u32 = 6;
 const FOO: u32 = [X - Y, Y - X][(X < Y) as usize];
 //~^ WARN this constant cannot be used
+//~| ERROR
 
 fn main() {
     println!("{}", FOO);
-    //~^ ERROR erroneous constant used
-    //~| E0080
+    //~^ ERROR
+    //~| ERROR
 }
