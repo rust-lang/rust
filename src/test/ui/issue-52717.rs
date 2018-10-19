978dc3d66f5e9a3bea733aca664e25b845fa0f27
Pramod Bisht <pramodpsb@outlook.com>
2018-10-19 10:33:49 +0000

    Fixed: Multiple errors on single typo in match pattern
    Here we have fixed the case where we were throwing two diagnostic
    messages `E0026` and `E0027` for same case like this
    
    Example
    error[E0026]: variant `A::A` does not have a field named `fob`
      --> src/test/ui/issue-52717.rs:20:12
       |
    20 |     A::A { fob } => { println!("{}", fob); }
       |            ^^^ variant `A::A` does not have this field
    
    error[E0027]: pattern does not mention field `foo`
      --> src/test/ui/issue-52717.rs:20:5
       |
    20 |     A::A { fob } => { println!("{}", fob); }
       |     ^^^^^^^^^^^^ missing field `foo`
    
    error: aborting due to 2 previous errors
    
    Here above we can see that both `E0026` and `E0027` are depicting
    same thing.
    
    So, to fix this issue, we are simply checking element of
    `inexistent_fields` is there any value lies in
    `unmentioned_fields` using Levenshtein algorithm, if does
    then for that case we are simply deleting element from
    `unmentioned_fields`. More or less now instead of showing
    separate message in `E0027` we are giving extra hint on `E0026`
    
    Address: #52717

--- src/test/ui/issue-52717.rs
+++ src/test/ui/issue-52717.rs
@@ -1,4 +1,4 @@
-// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -7,16 +7,15 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-
-enum MyOption<T> {
-    MySome(T),
-    MyNone,
+enum A {
+  A {
+    foo: usize,
+  }
 }
 
 fn main() {
-    match MyOption::MySome(42) {
-        MyOption::MySome { x: 42 } => (),
-        //~^ ERROR variant `MyOption::MySome` does not have a field named `x`
-        _ => (),
-    }
+  let x = A::A { foo: 3 };
+  match x {
+    A::A { fob } => { println!("{}", fob); }
+  }
 }
