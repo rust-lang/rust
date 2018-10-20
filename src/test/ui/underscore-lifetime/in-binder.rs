18f7db3d69377ebf85b6ff3a319445411f4b16b1
Scott McMurray <scottmcm@users.noreply.github.com>
2018-10-19 22:57:41 -0700

    `impl<'_> IceCube<'_> {}` is now only one error in both editions

--- src/test/ui/underscore-lifetime/in-binder.rs
+++ src/test/ui/underscore-lifetime/in-binder.rs
@@ -2,44 +2,34 @@
 //
 // Regression test for #52098.
 
-// revisions: Rust2015 Rust2018
-//[Rust2018] edition:2018
-
 struct IceCube<'a> {
     v: Vec<&'a char>
 }
 
 impl<'_> IceCube<'_> {}
-//[Rust2015]~^ ERROR `'_` cannot be used here
-//[Rust2015]~| ERROR missing lifetime specifier
-//[Rust2018]~^^^ ERROR `'_` cannot be used here
+//~^ ERROR `'_` cannot be used here
 
 struct Struct<'_> {
-//[Rust2015]~^ ERROR `'_` cannot be used here
-//[Rust2018]~^^ ERROR `'_` cannot be used here
+//~^ ERROR `'_` cannot be used here
     v: Vec<&'static char>
 }
 
 enum Enum<'_> {
-//[Rust2015]~^ ERROR `'_` cannot be used here
-//[Rust2018]~^^ ERROR `'_` cannot be used here
+//~^ ERROR `'_` cannot be used here
     Variant
 }
 
 union Union<'_> {
-//[Rust2015]~^ ERROR `'_` cannot be used here
-//[Rust2018]~^^ ERROR `'_` cannot be used here
+//~^ ERROR `'_` cannot be used here
     a: u32
 }
 
 trait Trait<'_> {
-//[Rust2015]~^ ERROR `'_` cannot be used here
-//[Rust2018]~^^ ERROR `'_` cannot be used here
+//~^ ERROR `'_` cannot be used here
 }
 
 fn foo<'_>() {
-    //[Rust2015]~^ ERROR `'_` cannot be used here
-    //[Rust2018]~^^ ERROR `'_` cannot be used here
+    //~^ ERROR `'_` cannot be used here
 }
 
 fn main() {}
