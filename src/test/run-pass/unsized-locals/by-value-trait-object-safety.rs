4fd895a6b835c2d0ef6292ef39f3fb13f6f14650
Masaki Hara <ackie.h.gmai@gmail.com>
2018-10-24 23:18:15 +0900

    Add autoderef tests for by-value trait objects.

--- src/test/run-pass/unsized-locals/by-value-trait-object-safety.rs
+++ src/test/run-pass/unsized-locals/by-value-trait-object-safety.rs
@@ -26,4 +26,8 @@ impl Foo for A {
 fn main() {
     let x = *(Box::new(A) as Box<dyn Foo>);
     assert_eq!(x.foo(), format!("hello"));
+
+    // I'm not sure whether we want this to work
+    let x = Box::new(A) as Box<dyn Foo>;
+    assert_eq!(x.foo(), format!("hello"));
 }
