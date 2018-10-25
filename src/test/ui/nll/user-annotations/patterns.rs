ad3c6078567df892b8f806564986c6bf67b40b36
Felix S. Klock II <pnkfelix@pnkfx.org>
2018-10-25 15:45:41 +0200

    Add test for normalization during field-lookup on patterns with ascribed types.
    As a drive-by, also added test analogous to existing
    static_to_a_to_static_through_tuple, but now apply to a struct instead
    of a tuple.

--- src/test/ui/nll/user-annotations/patterns.rs
+++ src/test/ui/nll/user-annotations/patterns.rs
@@ -41,6 +41,18 @@ fn struct_no_initializer() {
     y = &x; //~ ERROR
 }
 
+
+fn struct_no_initializer_must_normalize() {
+    trait Indirect { type Assoc; }
+    struct StaticU32;
+    impl Indirect for StaticU32 { type Assoc = &'static u32; }
+    struct Single2<T: Indirect> { value: <T as Indirect>::Assoc }
+
+    let x = 22;
+    let Single2 { value: mut _y }: Single2<StaticU32>;
+    _y = &x; //~ ERROR
+}
+
 fn variable_with_initializer() {
     let x = 22;
     let y: &'static u32 = &x; //~ ERROR
@@ -110,6 +122,11 @@ fn static_to_a_to_static_through_tuple<'a>(x: &'a u32) -> &'static u32 {
 
 
     let (y, _z): (&'a u32, u32) = (&22, 44);
+    y //~ ERROR
+}
+
+fn static_to_a_to_static_through_struct<'a>(_x: &'a u32) -> &'static u32 {
+    let Single { value: y }: Single<&'a u32> = Single { value: &22 };
     y //~ ERROR
 }
 
