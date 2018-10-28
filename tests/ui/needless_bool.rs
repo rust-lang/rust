7cfde9cfa95b0c466efe867e064d05fd8adea568
Giorgio Gambino <gambnio.giorgio@gmail.com>
2018-10-28 15:37:39 +0100

    Fix #3335: bool_comparison triggers 3 times on same code

--- tests/ui/needless_bool.rs
+++ tests/ui/needless_bool.rs
@@ -8,9 +8,31 @@
 // except according to those terms.
 
 
-
-
 #![warn(clippy::needless_bool)]
+
+use std::cell::Cell;
+
+macro_rules! bool_comparison_trigger {
+    ($($i:ident: $def:expr, $stb:expr );+  $(;)*) => (
+
+        #[derive(Clone)]
+        pub struct Trigger {
+            $($i: (Cell<bool>, bool, bool)),+
+        }
+
+        #[allow(dead_code)]
+        impl Trigger {
+            pub fn trigger(&self, key: &str) -> bool {
+                $(
+                    if let stringify!($i) = key {
+                        return self.$i.1 && self.$i.2 == $def;
+                    }
+                 )+
+                false
+            }
+        }
+    )
+}
 
 #[allow(clippy::if_same_then_else)]
 fn main() {
@@ -28,6 +50,9 @@ fn main() {
     bool_ret5(x, x);
     bool_ret4(x);
     bool_ret6(x, x);
+    needless_bool(x);
+    needless_bool2(x);
+    needless_bool3(x);
 }
 
 #[allow(clippy::if_same_then_else, clippy::needless_return)]
@@ -58,4 +83,24 @@ fn bool_ret4(x: bool) -> bool {
 #[allow(clippy::needless_return)]
 fn bool_ret6(x: bool, y: bool) -> bool {
     if x && y { return false } else { return true };
+}
+
+fn needless_bool(x: bool) {
+   if x  == true { };
+}
+
+fn needless_bool2(x: bool) {
+   if x  == false { };
+}
+
+fn needless_bool3(x: bool) {
+    
+    bool_comparison_trigger! {
+        test_one:   false, false;
+        test_three: false, false;
+        test_two:   true, true;
+    }
+    
+    if x == true { };
+    if x == false { };
 }
