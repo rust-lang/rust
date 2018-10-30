e5af43d4262d66a1dc850d765da99f0fc3c783d2
Philipp Hansch <dev@phansch.net>
2018-10-30 21:36:52 +0100

    UI test cleanup: Extract match_overlapping_arm tests

--- tests/ui/matches.rs
+++ tests/ui/matches.rs
@@ -74,65 +74,6 @@ fn ref_pats() {
     }
 }
 
-fn overlapping() {
-    const FOO : u64 = 2;
-
-    match 42 {
-        0 ... 10 => println!("0 ... 10"),
-        0 ... 11 => println!("0 ... 11"),
-        _ => (),
-    }
-
-    match 42 {
-        0 ... 5 => println!("0 ... 5"),
-        6 ... 7 => println!("6 ... 7"),
-        FOO ... 11 => println!("0 ... 11"),
-        _ => (),
-    }
-
-    match 42 {
-        2 => println!("2"),
-        0 ... 5 => println!("0 ... 5"),
-        _ => (),
-    }
-
-    match 42 {
-        2 => println!("2"),
-        0 ... 2 => println!("0 ... 2"),
-        _ => (),
-    }
-
-    match 42 {
-        0 ... 10 => println!("0 ... 10"),
-        11 ... 50 => println!("11 ... 50"),
-        _ => (),
-    }
-
-    match 42 {
-        2 => println!("2"),
-        0 .. 2 => println!("0 .. 2"),
-        _ => (),
-    }
-
-    match 42 {
-        0 .. 10 => println!("0 .. 10"),
-        10 .. 50 => println!("10 .. 50"),
-        _ => (),
-    }
-
-    match 42 {
-        0 .. 11 => println!("0 .. 11"),
-        0 ... 11 => println!("0 ... 11"),
-        _ => (),
-    }
-
-    if let None = Some(42) {
-        // nothing
-    } else if let None = Some(42) {
-        // another nothing :-)
-    }
-}
-
 fn match_wild_err_arm() {
     let x: Result<i32, &str> = Ok(3);
 
