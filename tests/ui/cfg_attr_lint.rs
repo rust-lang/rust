7df7a0a86ebf290f6f2650955f2479aaaa39beef
flip1995 <hello@philkrones.com>
2018-11-01 20:06:25 +0100

    Add tests from rustfmt::skip test file

--- tests/ui/cfg_attr_lint.rs
+++ tests/ui/cfg_attr_lint.rs
@@ -1,9 +1,30 @@
-#![feature(tool_lints)]
+// Copyright 2014-2018 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(stmt_expr_attributes)]
 
 #![warn(clippy::deprecated_cfg_attr)]
 
 // This doesn't get linted, see known problems
 #![cfg_attr(rustfmt, rustfmt_skip)]
+
+#[rustfmt::skip]
+trait Foo
+{
+fn foo(
+);
+}
+
+fn skip_on_statements() {
+    #[cfg_attr(rustfmt, rustfmt::skip)]
+    5+3;
+}
 
 #[cfg_attr(rustfmt, rustfmt_skip)]
 fn main() {
