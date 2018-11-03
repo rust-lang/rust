cca50701d9b798c821bb4c5c4a20eb02d8d5f3a8
Philipp Hansch <dev@phansch.net>
2018-11-03 12:59:40 +0100

    Improve clippy_dev help text

--- clippy_dev/src/main.rs
+++ clippy_dev/src/main.rs
@@ -19,12 +19,17 @@ fn main() {
     let matches = App::new("Clippy developer tooling")
         .subcommand(
             SubCommand::with_name("update_lints")
-                .about("Update the lint list")
+                .about("Makes sure that:\n \
+                       * the lint count in README.md is correct\n \
+                       * the changelog contains markdown link references at the bottom\n \
+                       * all lints groups include the correct lints\n \
+                       * lint modules in `clippy_lints/*` are visible in `src/lib.rs` via `pub mod`\n \
+                       * all lints are registered in the lint store")
                 .arg(
                     Arg::with_name("print-only")
                         .long("print-only")
                         .short("p")
-                        .help("Print a table of lints to STDOUT. Does not modify any files."),
+                        .help("Print a table of lints to STDOUT. This does not include deprecated and internal lints. (Does not modify any files)"),
                 )
         )
         .get_matches();
