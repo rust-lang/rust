80f914d47b9f5e23533ec6e01794f4c404094f35
Camille GILLOT <gillot.camille@gmail.com>
2018-11-04 11:27:09 +0100

    test: Add test for issues 45510 and 18952.
    Those tests are directly taken from the comments on the bug reports.

---
+++ src/test/run-pass/issue-45510.rs
@@ -0,0 +1,27 @@
+// Test overloaded resolution of fn_traits.
+
+#![feature(fn_traits)]
+#![feature(unboxed_closures)]
+
+struct Ishmael;
+struct Maybe;
+struct CallMe;
+
+impl FnOnce<(Ishmael,)> for CallMe {
+    type Output = ();
+    extern "rust-call" fn call_once(self, _args: (Ishmael,)) -> () {
+        println!("Split your lungs with blood and thunder!");
+    }
+}
+
+impl FnOnce<(Maybe,)> for CallMe {
+    type Output = ();
+    extern "rust-call" fn call_once(self, _args: (Maybe,)) -> () {
+        println!("So we just met, and this is crazy");
+    }
+}
+
+fn main() {
+    CallMe(Ishmael);
+    CallMe(Maybe);
+}
