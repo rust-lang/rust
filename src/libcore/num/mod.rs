b60efc1574fdba2504fdc903990a00893fd6d295
Ethan Brierley <ethanboxx@gmail.com>
2018-11-05 21:57:19 +0000

    Continue to try to make travis happy by adding a issue number

--- src/libcore/num/mod.rs
+++ src/libcore/num/mod.rs
@@ -4773,7 +4773,8 @@ pub struct ParseIntError {
 
 #[unstable(feature = "int_error_matching",
            reason = "it can be useful to match errors when making error messages \
-                     for integer parsing")]
+                     for integer parsing",
+           issue = "22639")]
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum IntErrorKind {
