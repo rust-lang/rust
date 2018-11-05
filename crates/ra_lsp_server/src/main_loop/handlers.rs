a83ed374d069e28c5e2715384fdd0a4c2c10aeea
Jeremy A. Kolb <jkolb@ara.com>
2018-11-05 17:09:45 -0500

    Remove unused variable

--- crates/ra_lsp_server/src/main_loop/handlers.rs
+++ crates/ra_lsp_server/src/main_loop/handlers.rs
@@ -487,7 +487,6 @@ pub fn handle_hover(
 
     for (file_id, symbol) in world.analysis().approximately_resolve_symbol(position)? {
         let range = symbol.node_range.conv_with(&line_index);
-        let name = symbol.name.to_string();
         let comment = world.analysis.doc_comment_for(file_id, symbol)?;
 
         if comment.is_some() {
