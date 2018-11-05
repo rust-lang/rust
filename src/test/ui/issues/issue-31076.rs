9fb2e0618e3a718ae12cd45b5ee39f9076ff3e79
jsirs <44773712+jsirs@users.noreply.github.com>
2018-11-05 15:54:10 +0200

    update test to include concrete type (i32)

--- src/test/ui/issues/issue-31076.rs
+++ src/test/ui/issues/issue-31076.rs
@@ -12,4 +12,6 @@ impl Add<i32> for i32 {}
 fn main() {
     let x = 5 + 6;
     //~^ ERROR binary operation `+` cannot be applied to type `{integer}`
+    let y = 5i32 + 6i32;
+    //~^ ERROR binary operation `+` cannot be applied to type `i32`
 }
