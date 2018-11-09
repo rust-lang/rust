401cb6bb539e95733290d1b0487f682f48b3f9cd
QuietMisdreavus <grey@quietmisdreavus.net>
2018-11-08 19:06:16 -0600

    don't inline `pub use some_crate` unless directly asked to

--- src/test/rustdoc/src-links-external.rs
+++ src/test/rustdoc/src-links-external.rs
@@ -18,6 +18,7 @@
 extern crate src_links_external;
 
 // @has foo/bar/index.html '//a/@href' '../../src/src_links_external/src-links-external.rs.html#11'
+#[doc(inline)]
 pub use src_links_external as bar;
 
 // @has foo/bar/struct.Foo.html '//a/@href' '../../src/src_links_external/src-links-external.rs.html#11'
