ebf03363f2d2385bc2248549cd93fca42779699a
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-13 15:36:05 +0100

    Properly test for int pointers in fat pointers to str slices of zero chars

--- src/test/ui/consts/int_ptr_for_zst_slices.rs
+++ src/test/ui/consts/int_ptr_for_zst_slices.rs
@@ -1,3 +1,6 @@
-const FOO: &str = unsafe { &*(1_usize as *const [u8; 0] as *const [u8] as *const [str]) };
+#![feature(const_raw_ptr_deref)]
+
+const FOO: &str = unsafe { &*(1_usize as *const [u8; 0] as *const [u8] as *const str) };
+//~^ ERROR it is undefined behaviour to use this value
 
 fn main() {}
