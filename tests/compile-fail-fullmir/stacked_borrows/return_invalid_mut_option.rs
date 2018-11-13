c54dcf59aed7f44260df41a800e7855436a205eb
Ralf Jung <post@ralfj.de>
2018-11-13 13:03:01 +0100

    add some tests for retagging inside tuples and options

---
+++ tests/compile-fail-fullmir/stacked_borrows/return_invalid_mut_option.rs
@@ -0,0 +1,11 @@
+// Make sure that we cannot return a `&mut` that got already invalidated, not even in an `Option`.
+fn foo(x: &mut (i32, i32)) -> Option<&mut i32> {
+    let xraw = x as *mut (i32, i32);
+    let ret = Some(unsafe { &mut (*xraw).1 });
+    let _val = unsafe { *xraw }; // invalidate xref
+    ret //~ ERROR does not exist on the stack
+}
+
+fn main() {
+    foo(&mut (1, 2));
+}
