c040a483bcb522b60865130ea5e94e2ef2915f94
Olivier Goffart <ogoffart@woboq.com>
2018-11-14 18:09:54 +0100

    Remove extern and some return value as an attempt to make the test pass on more platforms

--- src/test/codegen/union-abi.rs
+++ src/test/codegen/union-abi.rs
@@ -26,15 +26,15 @@ pub struct i64x4(i64, i64, i64, i64);
 #[derive(Copy, Clone)]
 pub union UnionI64x4{ a:(), b: i64x4 }
 
-// CHECK: define <4 x i64> @test_UnionI64x4(<4 x i64> %arg0)
+// CHECK: define void @test_UnionI64x4(<4 x i64>* {{.*}} %arg0)
 #[no_mangle]
-pub extern fn test_UnionI64x4(_: UnionI64x4) -> UnionI64x4 { loop {} }
+pub fn test_UnionI64x4(_: UnionI64x4) { loop {} }
 
 pub union UnionI64x4_{ a: i64x4, b: (), c:i64x4, d: Unhab, e: ((),()), f: UnionI64x4 }
 
-// CHECK: define <4 x i64> @test_UnionI64x4_(<4 x i64> %arg0)
+// CHECK: define void @test_UnionI64x4_(<4 x i64>* {{.*}} %arg0)
 #[no_mangle]
-pub extern fn test_UnionI64x4_(_: UnionI64x4_) -> UnionI64x4_ { loop {} }
+pub fn test_UnionI64x4_(_: UnionI64x4_) { loop {} }
 
 pub union UnionI64x4I64{ a: i64x4, b: i64 }
 
@@ -53,19 +53,19 @@ pub union UnionF32{a:f32}
 
 // CHECK: define float @test_UnionF32(float %arg0)
 #[no_mangle]
-pub extern fn test_UnionF32(_: UnionF32) -> UnionF32 { loop {} }
+pub fn test_UnionF32(_: UnionF32) -> UnionF32 { loop {} }
 
 pub union UnionF32F32{a:f32, b:f32}
 
 // CHECK: define float @test_UnionF32F32(float %arg0)
 #[no_mangle]
-pub extern fn test_UnionF32F32(_: UnionF32F32) -> UnionF32F32 { loop {} }
+pub fn test_UnionF32F32(_: UnionF32F32) -> UnionF32F32 { loop {} }
 
 pub union UnionF32U32{a:f32, b:u32}
 
 // CHECK: define i32 @test_UnionF32U32(i32)
 #[no_mangle]
-pub extern fn test_UnionF32U32(_: UnionF32U32) -> UnionF32U32 { loop {} }
+pub fn test_UnionF32U32(_: UnionF32U32) -> UnionF32U32 { loop {} }
 
 pub union UnionU128{a:u128}
 // CHECK: define i128 @test_UnionU128(i128 %arg0)
