bd1d518f9229c38a437eb3329b5e9c8042ef1912
ljedrz <ljedrz@gmail.com>
2018-11-15 21:50:35 +0100

    data_structures: introduce HybridSortedMap

--- src/librustc_data_structures/sorted_map.rs
+++ src/librustc_data_structures/sorted_map.rs
@@ -8,9 +8,13 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use stable_hasher::{HashStable, StableHasher, ToStableHashKey, StableHasherResult};
+
 use std::borrow::Borrow;
 use std::cmp::Ordering;
+use std::collections::btree_map::{self, BTreeMap};
 use std::convert::From;
+use std::iter::FromIterator;
 use std::mem;
 use std::ops::{RangeBounds, Bound, Index, IndexMut};
 
@@ -27,6 +31,9 @@ use std::ops::{RangeBounds, Bound, Index, IndexMut};
 pub struct SortedMap<K: Ord, V> {
     data: Vec<(K,V)>
 }
+
+unsafe impl<K: Ord + Sync, V: Sync> Sync for SortedMap<K, V> { }
+unsafe impl<K: Ord + Send, V: Send> Send for SortedMap<K, V> { }
 
 impl<K: Ord, V> SortedMap<K, V> {
 
@@ -135,6 +142,11 @@ impl<K: Ord, V> SortedMap<K, V> {
     #[inline]
     pub fn len(&self) -> usize {
         self.data.len()
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
     }
 
     #[inline]
@@ -247,6 +259,19 @@ impl<K: Ord, V> SortedMap<K, V> {
 
         (start, end)
     }
+
+    fn entry(&mut self, key: K) -> Entry<K, V> {
+        match self.lookup_index_for(&key) {
+            Ok(index) => {
+                unsafe {
+                    Entry::Occupied(self.data.get_unchecked_mut(index))
+                }
+            }
+            Err(index) => {
+                Entry::Vacant(key, index, self)
+            }
+        }
+    }
 }
 
 impl<K: Ord, V> IntoIterator for SortedMap<K, V> {
@@ -282,6 +307,380 @@ impl<K: Ord, V, I: Iterator<Item=(K, V)>> From<I> for SortedMap<K, V> {
         SortedMap {
             data
         }
+    }
+}
+
+// FIXME: duplicates the code of impl From<Iterator> for SortedMap<K, V>; it's probably a good
+// idea to remove that piece of code and adjust places where it's used.
+impl<K: Ord, V> FromIterator<(K, V)> for SortedMap<K, V> {
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        let mut data: Vec<(K, V)> = iter.into_iter().collect();
+        data.sort_unstable_by(|&(ref k1, _), &(ref k2, _)| k1.cmp(k2));
+        data.dedup_by(|&mut (ref k1, _), &mut (ref k2, _)| {
+            k1.cmp(k2) == Ordering::Equal
+        });
+        SortedMap {
+            data
+        }
+    }
+}
+
+pub enum Entry<'a, K: Ord + 'a, V: 'a> {
+    // FIXME: change to a mutable reference to a newly created empty spot? This would probably
+    // require some unsafe pointer magic, though.
+    Vacant(K, usize, &'a mut SortedMap<K, V>),
+    Occupied(&'a mut (K, V))
+}
+
+const SMALL_MAX: usize = 50;
+
+/// A sorted map which uses a vector-backed storage in case of a small number (up to a maximum of
+/// SMALL_MAX) of elements in order to take advantage of performance benefits it provides over a
+/// BTreeMap in that scale.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, RustcEncodable,
+         RustcDecodable)]
+pub enum HybridSortedMap<K: Ord, V> {
+    Small(SortedMap<K, V>),
+    Big(BTreeMap<K, V>)
+}
+
+unsafe impl<K: Ord + Sync, V: Sync> Sync for HybridSortedMap<K, V> { }
+unsafe impl<K: Ord + Send, V: Send> Send for HybridSortedMap<K, V> { }
+
+impl<K: Ord, V> Default for HybridSortedMap<K, V> {
+    fn default() -> Self {
+        HybridSortedMap::Small(SortedMap::new())
+    }
+}
+
+impl<K: Ord, V> HybridSortedMap<K, V> {
+    pub fn new() -> HybridSortedMap<K, V> {
+        HybridSortedMap::Small(SortedMap::new())
+    }
+
+    pub fn from_presorted_elements(elements: Vec<(K, V)>) -> HybridSortedMap<K, V> {
+        if elements.len() <= SMALL_MAX {
+            HybridSortedMap::Small(SortedMap::from_presorted_elements(elements))
+        } else {
+            HybridSortedMap::Big(elements.into_iter().collect())
+        }
+    }
+
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        match self {
+            HybridSortedMap::Small(small) => {
+                if small.len() < SMALL_MAX {
+                    small.insert(key, value)
+                } else if let Some(ref mut v) = small.get_mut(&key) {
+                    // If the key is already there, the value gets replaced, so
+                    // the length is not a concern.
+                    Some(mem::replace(v, value)) // return the previous value
+                } else {
+                    // FIXME: remove this replace
+                    let small = mem::replace(small, SortedMap::new());
+                    let big = small.into_iter().chain(Some((key, value))).collect();
+                    *self = HybridSortedMap::Big(big);
+                    None // the key was not present, we already checked that earlier
+                }
+            },
+            HybridSortedMap::Big(big) => {
+                big.insert(key, value)
+            }
+        }
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        match self {
+            HybridSortedMap::Small(small) => small.remove(key),
+            HybridSortedMap::Big(big) => big.remove(key)
+        }
+    }
+
+    pub fn get(&self, key: &K) -> Option<&V> {
+        match self {
+            HybridSortedMap::Small(small) => small.get(key),
+            HybridSortedMap::Big(big) => big.get(key)
+        }
+    }
+
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        match self {
+            HybridSortedMap::Small(small) => small.get_mut(key),
+            HybridSortedMap::Big(big) => big.get_mut(key)
+        }
+    }
+
+    pub fn clear(&mut self) {
+        match self {
+            HybridSortedMap::Small(small) => small.clear(),
+            HybridSortedMap::Big(big) => big.clear()
+        }
+    }
+
+    pub fn iter(&self) -> HybridSortedMapIter<K, V> {
+        match self {
+            HybridSortedMap::Small(small) => HybridSortedMapIter::Small(small.iter()),
+            HybridSortedMap::Big(big) => HybridSortedMapIter::Big(big.iter())
+        }
+    }
+
+    pub fn into_iter(self) -> HybridSortedMapIntoIter<K, V> {
+        match self {
+            HybridSortedMap::Small(small) => HybridSortedMapIntoIter::Small(small.into_iter()),
+            HybridSortedMap::Big(big) => HybridSortedMapIntoIter::Big(big.into_iter())
+        }
+    }
+
+    pub fn keys(&self) -> HybridSortedMapKeys<K, V> {
+        match self {
+            HybridSortedMap::Small(small) => HybridSortedMapKeys::Small(small.iter()),
+            HybridSortedMap::Big(big) => HybridSortedMapKeys::Big(big.keys())
+        }
+    }
+
+    pub fn values(&self) -> HybridSortedMapValues<K, V> {
+        match self {
+            HybridSortedMap::Small(small) => HybridSortedMapValues::Small(small.iter()),
+            HybridSortedMap::Big(big) => HybridSortedMapValues::Big(big.values())
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        match self {
+            HybridSortedMap::Small(small) => small.len(),
+            HybridSortedMap::Big(big) => big.len()
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match self {
+            HybridSortedMap::Small(small) => small.is_empty(),
+            HybridSortedMap::Big(big) => big.is_empty()
+        }
+    }
+
+    pub fn contains_key<Q>(&self, key: &Q) -> bool
+        where K: Borrow<Q>,
+              Q: Ord + Borrow<K> + ?Sized
+    {
+        match self {
+            HybridSortedMap::Small(small) => {
+                let k: &K = key.borrow();
+                small.get(k).is_some()
+            },
+            HybridSortedMap::Big(big) => {
+                big.get(key).is_some()
+            }
+        }
+    }
+
+    pub fn entry(&mut self, key: K) -> HybridSortedMapEntry<K, V> {
+        match self {
+            HybridSortedMap::Small(small) => HybridSortedMapEntry::Small(small.entry(key)),
+            HybridSortedMap::Big(big) => HybridSortedMapEntry::Big(big.entry(key))
+        }
+    }
+}
+
+pub enum HybridSortedMapIter<'a, K: 'a, V: 'a> {
+    Small(::std::slice::Iter<'a, (K, V)>),
+    Big(::std::collections::btree_map::Iter<'a, K, V>)
+}
+
+impl<'a, K: 'a, V: 'a> Iterator for HybridSortedMapIter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<((&'a K, &'a V))> {
+        match self {
+            HybridSortedMapIter::Small(ref mut small) => {
+                if let Some((k, v)) = small.next() {
+                    Some((&k, &v))
+                } else {
+                    None
+                }
+            },
+            HybridSortedMapIter::Big(ref mut big) => big.next()
+        }
+    }
+}
+
+pub enum HybridSortedMapIntoIter<K, V> {
+    Small(::std::vec::IntoIter<(K, V)>),
+    Big(::std::collections::btree_map::IntoIter<K, V>)
+}
+
+impl<K, V> Iterator for HybridSortedMapIntoIter<K, V> {
+    type Item = (K, V);
+
+    fn next(&mut self) -> Option<(K, V)> {
+        match self {
+            HybridSortedMapIntoIter::Small(ref mut small) => small.next(),
+            HybridSortedMapIntoIter::Big(ref mut big) => big.next()
+        }
+    }
+}
+
+impl<K: Ord, V> IntoIterator for HybridSortedMap<K, V> {
+    type Item = (K, V);
+    type IntoIter = HybridSortedMapIntoIter<K, V>;
+
+    fn into_iter(self) -> HybridSortedMapIntoIter<K, V> {
+        self.into_iter()
+    }
+}
+
+impl<'a, K: Ord + 'a, V: 'a> IntoIterator for &'a HybridSortedMap<K, V> {
+    type Item = (&'a K, &'a V);
+    type IntoIter = HybridSortedMapIter<'a, K, V>;
+
+    fn into_iter(self) -> HybridSortedMapIter<'a, K, V> {
+        self.iter()
+    }
+}
+
+impl<K: Ord, V> FromIterator<(K, V)> for HybridSortedMap<K, V> {
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> HybridSortedMap<K, V> {
+        let iter = iter.into_iter();
+        let (low, _) = iter.size_hint();
+
+        if low > 0 {
+            if low <= SMALL_MAX {
+                HybridSortedMap::Small(SortedMap::from_iter(iter))
+            } else {
+                HybridSortedMap::Big(BTreeMap::from_iter(iter))
+            }
+        } else {
+            let mut map = HybridSortedMap::new();
+            for (k, v) in iter.into_iter() {
+                map.insert(k, v);
+            }
+            map
+        }
+    }
+}
+
+pub enum HybridSortedMapKeys<'a, K: 'a, V: 'a> {
+    Small(::std::slice::Iter<'a, (K, V)>),
+    Big(::std::collections::btree_map::Keys<'a, K, V>)
+}
+
+impl<'a, K: 'a, V: 'a> Iterator for HybridSortedMapKeys<'a, K, V> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<(&'a K)> {
+        match self {
+            HybridSortedMapKeys::Small(ref mut small) => {
+                if let Some((k, _)) = small.next() {
+                    Some(&k)
+                } else {
+                    None
+                }
+            },
+            HybridSortedMapKeys::Big(ref mut big) => big.next()
+        }
+    }
+}
+
+pub enum HybridSortedMapValues<'a, K: 'a, V: 'a> {
+    Small(::std::slice::Iter<'a, (K, V)>),
+    Big(::std::collections::btree_map::Values<'a, K, V>)
+}
+
+impl<'a, K: 'a, V: 'a> Iterator for HybridSortedMapValues<'a, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<(&'a V)> {
+        match self {
+            HybridSortedMapValues::Small(ref mut small) => {
+                if let Some((_, v)) = small.next() {
+                    Some(&v)
+                } else {
+                    None
+                }
+            },
+            HybridSortedMapValues::Big(ref mut big) => big.next()
+        }
+    }
+}
+
+impl<'a, K, Q, V> Index<&'a Q> for HybridSortedMap<K, V>
+    where K: Ord + Borrow<Q>, // FIXME: SortedMap restriction
+          Q: Ord + Borrow<K> + ?Sized
+{
+    type Output = V;
+
+    fn index(&self, key: &Q) -> &V {
+        match self {
+            HybridSortedMap::Small(small) => {
+                let k: &K = key.borrow();
+                small.get(k).expect("no entry found for key")
+            },
+            HybridSortedMap::Big(big) => {
+                big.get(key).expect("no entry found for key")
+            }
+        }
+    }
+}
+
+pub enum HybridSortedMapEntry<'a, K: Ord + 'a, V: 'a> {
+    Small(Entry<'a, K, V>),
+    Big(btree_map::Entry<'a, K, V>)
+}
+
+impl<'a, K: Ord + 'a, V: Default + 'a> HybridSortedMapEntry<'a, K, V> {
+    pub fn or_default(self) -> &'a mut V {
+        match self {
+            HybridSortedMapEntry::Small(entry) => {
+                match entry {
+                    Entry::Occupied((_, v)) => {
+                        v
+                    },
+                    Entry::Vacant(k, index, small) => {
+                        small.data.insert(index, (k, Default::default()));
+                        &mut small.data[index].1
+                    }
+                }
+            },
+            HybridSortedMapEntry::Big(entry) => {
+                entry.or_default()
+            }
+        }
+    }
+}
+
+impl<'a, K: Ord + 'a, V: 'a> HybridSortedMapEntry<'a, K, V> {
+    pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V {
+        match self {
+            HybridSortedMapEntry::Small(entry) => {
+                match entry {
+                    Entry::Occupied((_, v)) => {
+                        v
+                    },
+                    Entry::Vacant(k, index, small) => {
+                        small.data.insert(index, (k, default()));
+                        &mut small.data[index].1
+                    }
+                }
+            },
+            HybridSortedMapEntry::Big(entry) => {
+                entry.or_insert_with(default)
+            }
+        }
+    }
+}
+
+impl<K, V, HCX> HashStable<HCX> for HybridSortedMap<K, V>
+    where K: Ord + ToStableHashKey<HCX>,
+          V: HashStable<HCX>,
+{
+    fn hash_stable<W: StableHasherResult>(&self,
+                                          hcx: &mut HCX,
+                                          hasher: &mut StableHasher<W>) {
+        let mut entries: Vec<_> = self.iter()
+                                      .map(|(k, v)| (k.to_stable_hash_key(hcx), v))
+                                      .collect();
+        entries.sort_unstable_by(|&(ref sk1, _), &(ref sk2, _)| sk1.cmp(sk2));
+        entries.hash_stable(hcx, hasher);
     }
 }
 
