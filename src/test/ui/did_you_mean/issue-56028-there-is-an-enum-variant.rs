3986c964481a048100565c8d30b1937ec2eb516d
Zack M. Davis <code@zackmdavis.net>
2018-11-18 22:21:38 -0800

    enum type instead of variant suggestion unification
    Weirdly, we were deciding between a help note and a structured
    suggestion based on whether the import candidate span was a dummy—but
    we weren't using that span in any case! The dummy-ness of the span
    (which appears to be a matter of this-crate vs. other-crate
    definition) isn't the right criterion by which we should decide
    whether it's germane to mention that "there is an enum variant";
    instead, let's use the someness of `def` (which is used as the
    `has_unexpected_resolution` argument to `error_code`).
    
    Since `import_candidate_to_paths` has no other callers, we are free to
    stop returning the span and rename the function. By using
    `span_suggestions_`, we leverage the max-suggestions output limit
    already built in to the emitter, thus resolving #56028.
    
    In the matter of message wording, "you can" is redundant (and perhaps
    too informal); prefer the imperative.

---
+++ src/test/ui/did_you_mean/issue-56028-there-is-an-enum-variant.rs
@@ -0,0 +1,13 @@
+enum PutDown { Set }
+enum AffixHeart { Set }
+enum CauseToBe { Set }
+enum Determine { Set }
+enum TableDishesAction { Set }
+enum Solidify { Set }
+enum UnorderedCollection { Set }
+
+fn setup() -> Set { Set }
+
+fn main() {
+    setup();
+}
