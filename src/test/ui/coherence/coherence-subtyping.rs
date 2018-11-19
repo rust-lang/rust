57f76be0310742273e1d7656e1e4d1846fb65e04
Niko Matsakis <niko@alum.mit.edu>
2018-11-19 10:47:34 -0500

    address tmandry nits

--- src/test/ui/coherence/coherence-subtyping.rs
+++ src/test/ui/coherence/coherence-subtyping.rs
@@ -1,40 +1,14 @@
 // Test that two distinct impls which match subtypes of one another
 // yield coherence errors (or not) depending on the variance.
 
-trait Contravariant {
+trait TheTrait {
     fn foo(&self) { }
 }
 
-impl Contravariant for for<'a,'b> fn(&'a u8, &'b u8) -> &'a u8 {
+impl TheTrait for for<'a,'b> fn(&'a u8, &'b u8) -> &'a u8 {
 }
 
-impl Contravariant for for<'a> fn(&'a u8, &'a u8) -> &'a u8 {
-    //~^ ERROR
-}
-
-///////////////////////////////////////////////////////////////////////////
-
-trait Covariant {
-    fn foo(&self) { }
-}
-
-impl Covariant for for<'a,'b> fn(&'a u8, &'b u8) -> &'a u8 {
-}
-
-impl Covariant for for<'a> fn(&'a u8, &'a u8) -> &'a u8 {
-    //~^ ERROR
-}
-
-///////////////////////////////////////////////////////////////////////////
-
-trait Invariant {
-    fn foo(&self) { }
-}
-
-impl Invariant for for<'a,'b> fn(&'a u8, &'b u8) -> &'a u8 {
-}
-
-impl Invariant for for<'a> fn(&'a u8, &'a u8) -> &'a u8 {
+impl TheTrait for for<'a> fn(&'a u8, &'a u8) -> &'a u8 {
     //~^ ERROR
 }
 
--- src/test/ui/hrtb/hrtb-exists-forall-fn.rs
+++ src/test/ui/hrtb/hrtb-exists-forall-fn.rs
@@ -3,21 +3,16 @@
 // In particular, we test this pattern in trait solving, where it is not connected
 // to any part of the source code.
 
-trait Trait<T> {}
-
 fn foo<'a>() -> fn(&'a u32) {
     panic!()
 }
 
 fn main() {
-    // Here, proving that `(): Trait<for<'b> fn(&'b u32)>` uses the impl:
+    // Here, proving that `fn(&'a u32) <: for<'b> fn(&'b u32)`:
     //
-    // - The impl provides the clause `forall<'a> { (): Trait<fn(&'a u32)> }`
-    // - We instantiate `'a` existentially to get `(): Trait<fn(&?a u32)>`
-    // - We unify `fn(&?a u32)` with `for<'b> fn(&'b u32)`
-    //   - This requires (among other things) instantiating `'b` universally,
-    //     yielding `fn(&!b u32)`, in a fresh universe U1
-    //   - So we get `?a = !b` but the universe U0 assigned to `?a` cannot name `!b`.
+    // - instantiates `'b` with a placeholder `!b`,
+    // - requires that `&!b u32 <: &'a u32` and hence that `!b: 'a`,
+    // - but we can never know this.
 
     let _: for<'b> fn(&'b u32) = foo(); //~ ERROR mismatched types
 }
