cb2eeb1ac9d9f06185d8ef13732595be7d26a664
gnzlbg <gonzalobg88@gmail.com>
2018-11-21 15:19:44 +0100

    Travis should test OSX

---
+++ ci/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -ex
+
+cargo build
+cargo test --verbose
+
+# install
+mkdir -p ~/rust/cargo/bin
+cp target/debug/cargo-semver ~/rust/cargo/bin
+cp target/debug/rust-semverver ~/rust/cargo/bin
+
+# become semververver
+eval "current_version=$(grep -e '^version = .*$' Cargo.toml | cut -d ' ' -f 3)"
+PATH=~/rust/cargo/bin:$PATH cargo semver | tee semver_out
+(head -n 1 semver_out | grep "\-> $current_version") || (echo "versioning mismatch" && return 1)
