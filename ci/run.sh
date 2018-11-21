ffbfd781c186236d83fd02890b886d80bed81656
gnzlbg <gonzalobg88@gmail.com>
2018-11-21 17:44:40 +0100

    Fix exit of ci/run.sh script

--- ci/run.sh
+++ ci/run.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+export RUST_TEST_THREADS=1
+export RUST_BACKTRACE=full
+#export RUST_TEST_NOCAPTURE=1
+
 cargo build
 cargo test --verbose
 
@@ -11,6 +15,17 @@ cp target/debug/cargo-semver ~/rust/cargo/bin
 cp target/debug/rust-semverver ~/rust/cargo/bin
 
 # become semververver
-current_version=$(grep -e '^version = .*$' Cargo.toml | cut -d ' ' -f 3)
 PATH=~/rust/cargo/bin:$PATH cargo semver | tee semver_out
-(head -n 1 semver_out | grep "\\-> $current_version") || (echo "versioning mismatch" && return 1)
+current_version="$(grep -e '^version = .*$' Cargo.toml | cut -d ' ' -f 3)"
+current_version="${current_version%\"}"
+current_version="${current_version#\"}"
+result="$(head -n 1 semver_out)"
+if echo "$result" | grep -- "-> $current_version"; then
+    echo "version ok"
+    exit 0
+else
+    echo "versioning mismatch"
+    cat semver_out
+    echo "versioning mismatch"
+    exit 1
+fi
