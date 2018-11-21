e05b61ccd8fcb4bb4fd12085fc13522b940e7618
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-21 11:47:16 +0100

    Fix a comment

--- src/test/ui/consts/partial_qualif.rs
+++ src/test/ui/consts/partial_qualif.rs
@@ -4,7 +4,7 @@ use std::cell::Cell;
 
 const FOO: &(Cell<usize>, bool) = {
     let mut a = (Cell::new(0), false);
-    a.1 = true; // resets `qualif(a)` to `qualif(true)`
+    a.1 = true; // sets `qualif(a)` to `qualif(a) | qualif(true)`
     &{a} //~ ERROR cannot borrow a constant which may contain interior mutability
 };
 
