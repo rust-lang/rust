f44186342a3e3d1e3d4f6acfb248595e878b0ee1
gnzlbg <gonzalobg88@gmail.com>
2018-11-21 15:03:54 +0100

    Refactor full tests and add OSX support

--- tests/full.rs
+++ tests/full.rs
@@ -3,25 +3,19 @@ mod full {
     use std::env;
     use std::fs::File;
     use std::os::unix::io::{AsRawFd, FromRawFd};
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
     use std::process::{Command, Stdio};
 
-    macro_rules! full_test {
-        ($name:ident, $crate_name:expr, $old_version:expr, $new_version:expr) => {
-            #[test]
-            fn $name() {
-                let mut success = true;
+    fn test_full(crate_name: &str, old_version: &str, new_version: &str) {
+        let mut success = true;
 
-                let old_version = concat!($crate_name, ":", $old_version);
-                let new_version = concat!($crate_name, ":", $new_version);
-
-                let prog = concat!(r#"
+        let prog = format!(r#"
     # wait for the actual output
-    /^version bump/ {
+    /^version bump/ {{
         doprint = 1;
-    }
+    }}
 
-    {
+    {{
         # check the environ for filtering
         if (ENVIRON["RUST_LOG"] == "debug")
             doprint = 1;
@@ -31,69 +25,91 @@ mod full {
             next;
 
         # sanitize paths
-        gsub(/-->.*"#, $crate_name, r#"/, "--> "#, $crate_name, r#"", $0);
+        gsub(/-->.*{crate_name}/, "--> {crate_name}", $0);
         print;
-    }"#, $crate_name, $crate_name);
-                let out_file = Path::new("tests/full_cases")
-                    .join(concat!($crate_name, "-", $old_version, "-", $new_version));
+    }}{crate_name}{crate_name}"#, crate_name = crate_name);
 
-                if let Some(path) = env::var_os("PATH") {
-                    let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-                    let current_dir = env::current_dir().expect("could not determine current dir");
-                    paths.insert(0, current_dir.join("target/debug"));
-                    let new_path = env::join_paths(paths).unwrap();
-                    env::set_var("PATH", &new_path);
-                } else {
-                    eprintln!("no path!");
-                }
+        eprintln!("prog:\n{}",prog);
 
-                let stdout = File::create(&out_file).expect("could not create `stdout` file");
-                let out_file = out_file.to_str().unwrap();
+        let base_out_file = Path::new("tests/full_cases")
+            .join(format!("{}-{}-{}", crate_name, old_version, new_version));
 
-                let mut awk_child = Command::new("awk")
-                    .arg(&prog)
-                    .stdin(Stdio::piped())
-                    .stdout(stdout)
-                    .spawn()
-                    .expect("could not run awk");
+        let out_file = if cfg!(target_os = "macos") {
+            let p: PathBuf = format!("{}.osx", base_out_file.display()).into();
+            if p.exists() { p } else { base_out_file }
+        } else {
+            base_out_file
+        };
+        assert!(out_file.exists());
 
-                let (err_pipe, out_pipe) = if let Some(ref stdin) = awk_child.stdin {
-                    let fd = stdin.as_raw_fd();
-                    unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
-                } else {
-                    panic!("could not pipe to awk");
-                };
+        if let Some(path) = env::var_os("PATH") {
+            let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+            let current_dir = env::current_dir().expect("could not determine current dir");
+            paths.insert(0, current_dir.join("target/debug"));
+            let new_path = env::join_paths(paths).unwrap();
+            env::set_var("PATH", &new_path);
+        } else {
+            eprintln!("no path!");
+        }
 
-                success &= Command::new("./target/debug/cargo-semver")
-                    .args(&["-S", &old_version, "-C", &new_version])
-                    .env("RUST_BACKTRACE", "full")
-                    .stdin(Stdio::null())
-                    .stdout(out_pipe)
-                    .stderr(err_pipe)
-                    .status()
-                    .expect("could not run cargo semver")
-                    .success();
+        let stdout = File::create(&out_file).expect("could not create `stdout` file");
+        let out_file = out_file.to_str().unwrap();
 
-                assert!(success, "cargo semver");
+        let mut awk_child = Command::new("awk")
+            .arg(&prog)
+            .stdin(Stdio::piped())
+            .stdout(stdout)
+            .spawn()
+            .expect("could not run awk");
 
-                success &= awk_child.wait().expect("could not wait for awk child").success();
+        let (err_pipe, out_pipe) = if let Some(ref stdin) = awk_child.stdin {
+            let fd = stdin.as_raw_fd();
+            unsafe { (Stdio::from_raw_fd(fd), Stdio::from_raw_fd(fd)) }
+        } else {
+            panic!("could not pipe to awk");
+        };
 
-                assert!(success, "awk");
+        let old_version = format!("{}:{}", crate_name, old_version);
+        let new_version = format!("{}:{}", crate_name, new_version);
 
-                success &= Command::new("git")
-                    .args(&["diff", "--exit-code", out_file])
+        success &= Command::new("./target/debug/cargo-semver")
+            .args(&["-S", &old_version, "-C", &new_version])
+            .env("RUST_BACKTRACE", "full")
+            .stdin(Stdio::null())
+            .stdout(out_pipe)
+            .stderr(err_pipe)
+            .status()
+            .expect("could not run cargo semver")
+            .success();
+
+        assert!(success, "cargo semver");
+
+        success &= awk_child.wait().expect("could not wait for awk child").success();
+
+        assert!(success, "awk");
+
+        success &= Command::new("git")
+            .args(&["diff", "--exit-code", out_file])
                     .env("PAGER", "")
                     .status()
                     .expect("could not run git diff")
                     .success();
 
-                assert!(success, "git");
+        assert!(success, "git");
+
+    }
+
+    macro_rules! full_test {
+        ($name:ident, $crate_name:expr, $old_version:expr, $new_version:expr) => {
+            #[test]
+            fn $name() {
+                test_full($crate_name, $old_version, $new_version);
             }
         }
     }
 
-    // full_test!(log, "log", "0.3.4", "0.3.8");
-    // full_test!(libc, "libc", "0.2.28", "0.2.31");
+    full_test!(log, "log", "0.3.4", "0.3.8");
+    full_test!(libc, "libc", "0.2.28", "0.2.31");
     // full_test!(mozjs, "mozjs", "0.2.0", "0.3.0");
     // full_test!(rand, "rand", "0.3.10", "0.3.16");
     // full_test!(serde_pre, "serde", "0.7.0", "1.0.0");
