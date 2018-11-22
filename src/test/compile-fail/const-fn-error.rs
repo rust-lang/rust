df2123cff2b1e6e4c7dfc0560ccf0eec3ae67a65
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-22 17:19:55 +0100

    Update compile-fail test

--- src/test/compile-fail/const-fn-error.rs
+++ src/test/compile-fail/const-fn-error.rs
@@ -14,8 +14,6 @@ const X : usize = 2;
 
 const fn f(x: usize) -> usize {
     let mut sum = 0;
-    //~^ let bindings in constant functions are unstable
-    //~| statements in constant functions are unstable
     for i in 0..x {
         //~^ ERROR E0015
         //~| ERROR E0019
