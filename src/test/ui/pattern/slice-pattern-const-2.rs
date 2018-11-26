28cc3405a8be556d16e29f4a7bb6f8ad05b12420
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-26 09:56:39 +0100

    r/\t/    /

--- src/test/ui/pattern/slice-pattern-const-2.rs
+++ src/test/ui/pattern/slice-pattern-const-2.rs
@@ -1,11 +1,11 @@
 // compile-pass
 
 fn main() {
-	let s = &[0x00; 4][..]; //Slice of any value
-	const MAGIC_TEST: &[u32] = &[4, 5, 6, 7]; //Const slice to pattern match with
-	match s {
-		MAGIC_TEST => (),
-		[0x00, 0x00, 0x00, 0x00] => (),
-		_ => (),
-	}
+    let s = &[0x00; 4][..]; //Slice of any value
+    const MAGIC_TEST: &[u32] = &[4, 5, 6, 7]; //Const slice to pattern match with
+    match s {
+        MAGIC_TEST => (),
+        [0x00, 0x00, 0x00, 0x00] => (),
+        _ => (),
+    }
 }
--- src/test/ui/pattern/slice-pattern-const.rs
+++ src/test/ui/pattern/slice-pattern-const.rs
@@ -1,11 +1,11 @@
 // compile-pass
 
 fn main() {
-	let s = &[0x00; 4][..]; //Slice of any value
-	const MAGIC_TEST: &[u8] = b"TEST"; //Const slice to pattern match with
-	match s {
-		MAGIC_TEST => (),
-		[0x00, 0x00, 0x00, 0x00] => (),
-		_ => (),
-	}
+    let s = &[0x00; 4][..]; //Slice of any value
+    const MAGIC_TEST: &[u8] = b"TEST"; //Const slice to pattern match with
+    match s {
+        MAGIC_TEST => (),
+        [0x00, 0x00, 0x00, 0x00] => (),
+        _ => (),
+    }
 }
