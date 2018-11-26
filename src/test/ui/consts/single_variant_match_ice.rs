866664c8bb1a3f1eb7fa60c8d8243535675ea78f
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-26 10:26:54 +0100

    Add a test for single variant matches

--- src/test/ui/consts/single_variant_match_ice.rs
+++ src/test/ui/consts/single_variant_match_ice.rs
@@ -2,6 +2,14 @@ enum Foo {
     Prob,
 }
 
+const FOO: u32 = match Foo::Prob {
+    Foo::Prob => 42, //~ ERROR unimplemented expression type
+};
+
+const BAR: u32 = match Foo::Prob {
+    x => 42, //~ ERROR unimplemented expression type
+};
+
 impl Foo {
     pub const fn as_val(&self) -> u8 {
         use self::Foo::*;
