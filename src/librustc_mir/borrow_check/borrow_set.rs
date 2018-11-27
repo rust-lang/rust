4beb9f5d10dcad96e59c18e702d83704c37e68ea
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-11-27 04:08:36 +0200

    rustc_mir: try visit_local instead of visit_place in GatherBorrows.

--- src/librustc_mir/borrow_check/borrow_set.rs
+++ src/librustc_mir/borrow_check/borrow_set.rs
@@ -238,16 +238,14 @@ impl<'a, 'gcx, 'tcx> Visitor<'tcx> for GatherBorrows<'a, 'gcx, 'tcx> {
         self.super_assign(block, assigned_place, rvalue, location)
     }
 
-    fn visit_place(
+    fn visit_local(
         &mut self,
-        place: &mir::Place<'tcx>,
+        &local: &mir::Local,
         context: PlaceContext<'tcx>,
         location: Location,
     ) {
-        self.super_place(place, context, location);
-
         // We found a use of some temporary TEMP...
-        if let Place::Local(temp) = place {
+        if let Place::Local(temp) = &Place::Local(local) {
             // ... check whether we (earlier) saw a 2-phase borrow like
             //
             //     TMP = &mut place
