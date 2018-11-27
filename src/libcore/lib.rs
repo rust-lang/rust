25d1c073d199bfc58af311e8e84da173e22c6f44
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-11-27 11:00:44 +0100

    Remove unused feature gate from `libcore`

--- src/libcore/lib.rs
+++ src/libcore/lib.rs
@@ -120,7 +120,6 @@
 #![feature(const_slice_len)]
 #![feature(const_str_as_bytes)]
 #![feature(const_str_len)]
-#![cfg_attr(stage0, feature(const_let))]
 #![feature(const_int_rotate)]
 #![feature(const_int_wrapping)]
 #![feature(const_int_sign)]
