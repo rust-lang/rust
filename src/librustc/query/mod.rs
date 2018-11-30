5751fcc3b129a31981e9e24ee0ad063b9c7574ff
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-11-30 22:58:06 +0100

    Update all_traits

--- src/librustc/query/mod.rs
+++ src/librustc/query/mod.rs
@@ -850,7 +850,7 @@ rustc_queries! {
         /// A vector of every trait accessible in the whole crate
         /// (i.e., including those from subcrates). This is used only for
         /// error reporting.
-        query all_traits(_: CrateNum) -> Lrc<Vec<DefId>> {
+        query all_traits(_: CrateNum) -> &'tcx [DefId] {
             desc { "fetching all foreign and local traits" }
         }
     }
