28ca35fd132156b1c02110547975385a06ba52f7
Dan Robertson <dan@dlrobertson.com>
2018-11-30 17:37:23 +0000

    tests: Simplify VaList run-make test
    The va_list tests were too complex and were causing some spurious
    test failures on Windows

--- src/test/run-make-fulldeps/c-link-to-rust-va-list-fn/checkrust.rs
+++ src/test/run-make-fulldeps/c-link-to-rust-va-list-fn/checkrust.rs
@@ -16,127 +16,65 @@ extern crate libc;
 
 use libc::{c_char, c_double, c_int, c_long, c_longlong};
 use std::ffi::VaList;
-use std::slice;
-use std::ffi::CStr;
+use std::ffi::{CString, CStr};
 
-#[repr(C)]
-#[derive(Clone, Copy, Debug)]
-pub enum AnswerType {
-    Double,
-    Long,
-    LongLong,
-    Int,
-    Byte,
-    CStr,
-    Skip,
-}
-
-#[repr(C)]
-pub union AnswerData {
-    pub double: c_double,
-    pub long: c_long,
-    pub longlong: c_longlong,
-    pub int: c_int,
-    pub byte: c_char,
-    pub cstr: *const c_char,
-    pub skip_ty: AnswerType,
-}
-
-#[repr(C)]
-pub struct Answer {
-    tag: AnswerType,
-    data: AnswerData,
-}
-
-#[no_mangle]
-pub unsafe fn compare_answers(answers: &[Answer], mut ap: VaList) -> usize {
-    for (i, answer) in answers.iter().enumerate() {
-        match answer {
-            Answer { tag: AnswerType::Double, data: AnswerData { double: d } } => {
-                let tmp = ap.arg::<c_double>();
-                if d.floor() != tmp.floor() {
-                    println!("Double: {} != {}", d, tmp);
-                    return i + 1;
-                }
-            }
-            Answer { tag: AnswerType::Long, data: AnswerData { long: l } } => {
-                let tmp =  ap.arg::<c_long>();
-                if *l != tmp {
-                    println!("Long: {} != {}", l, tmp);
-                    return i + 1;
-                }
-            }
-            Answer { tag: AnswerType::LongLong, data: AnswerData { longlong: l } } => {
-                let tmp =  ap.arg::<c_longlong>();
-                if *l != tmp {
-                    println!("Long Long: {} != {}", l, tmp);
-                    return i + 1;
-                }
-            }
-            Answer { tag: AnswerType::Int, data: AnswerData { int: n } } => {
-                let tmp = ap.arg::<c_int>();
-                if *n != tmp {
-                    println!("Int: {} != {}", n, tmp);
-                    return i + 1;
-                }
-            }
-            Answer { tag: AnswerType::Byte, data: AnswerData { byte: b } } => {
-                let tmp = ap.arg::<c_char>();
-                if *b != tmp {
-                    println!("Byte: {} != {}", b, tmp);
-                    return i + 1;
-                }
-            }
-            Answer { tag: AnswerType::CStr, data: AnswerData { cstr: c0 } } => {
-                let c1 = ap.arg::<*const c_char>();
-                let cstr0 = CStr::from_ptr(*c0);
-                let cstr1 = CStr::from_ptr(c1);
-                if cstr0 != cstr1 {
-                    println!("C String: {:?} != {:?}", cstr0, cstr1);
-                    return i + 1;
-                }
-            }
-            _ => {
-                println!("Unknown type!");
-                return i + 1;
-            }
+macro_rules! continue_if {
+    ($cond:expr) => {
+        if !($cond) {
+            return 0xff;
         }
     }
-    return 0;
+}
+
+unsafe fn compare_c_str(ptr: *const c_char, val: &str) -> bool {
+    let cstr0 = CStr::from_ptr(ptr);
+    let cstr1 = CString::new(val).unwrap();
+    &*cstr1 == cstr0
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn check_rust(argc: usize, answers: *const Answer, ap: VaList) -> usize {
-    let slice = slice::from_raw_parts(answers, argc);
-    compare_answers(slice, ap)
+pub unsafe extern "C" fn check_list_0(mut ap: VaList) -> usize {
+    continue_if!(ap.arg::<c_longlong>() == 1);
+    continue_if!(ap.arg::<c_int>() == 2);
+    continue_if!(ap.arg::<c_longlong>() == 3);
+    0
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn check_rust_copy(argc: usize, answers: *const Answer,
-                                         mut ap: VaList) -> usize {
-    let slice = slice::from_raw_parts(answers, argc);
-    let mut skip_n = 0;
-    for (i, answer) in slice.iter().enumerate() {
-        match answer {
-            Answer { tag: AnswerType::Skip, data: AnswerData { skip_ty } } => {
-                match skip_ty {
-                    AnswerType::Double => { ap.arg::<c_double>(); }
-                    AnswerType::Long => { ap.arg::<c_long>(); }
-                    AnswerType::LongLong => { ap.arg::<c_longlong>(); }
-                    AnswerType::Int => { ap.arg::<c_int>(); }
-                    AnswerType::Byte => { ap.arg::<c_char>(); }
-                    AnswerType::CStr => { ap.arg::<*const c_char>(); }
-                    _ => { return i; }
-                };
-            }
-            _ => {
-                skip_n = i;
-                break;
-            }
+pub unsafe extern "C" fn check_list_1(mut ap: VaList) -> usize {
+    continue_if!(ap.arg::<c_int>() == -1);
+    continue_if!(ap.arg::<c_char>() == 'A' as c_char);
+    continue_if!(ap.arg::<c_char>() == '4' as c_char);
+    continue_if!(ap.arg::<c_char>() == ';' as c_char);
+    continue_if!(ap.arg::<c_int>() == 0x32);
+    continue_if!(ap.arg::<c_int>() == 0x10000001);
+    continue_if!(compare_c_str(ap.arg::<*const c_char>(), "Valid!"));
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn check_list_2(mut ap: VaList) -> usize {
+    continue_if!(ap.arg::<c_double>().floor() == 3.14f64.floor());
+    continue_if!(ap.arg::<c_long>() == 12);
+    continue_if!(ap.arg::<c_char>() == 'a' as c_char);
+    continue_if!(ap.arg::<c_double>().floor() == 6.18f64.floor());
+    continue_if!(compare_c_str(ap.arg::<*const c_char>(), "Hello"));
+    continue_if!(ap.arg::<c_int>() == 42);
+    continue_if!(compare_c_str(ap.arg::<*const c_char>(), "World"));
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn check_list_copy_0(mut ap: VaList) -> usize {
+    continue_if!(ap.arg::<c_double>().floor() == 6.28f64.floor());
+    continue_if!(ap.arg::<c_int>() == 16);
+    continue_if!(ap.arg::<c_char>() == 'A' as c_char);
+    continue_if!(compare_c_str(ap.arg::<*const c_char>(), "Skip Me!"));
+    ap.copy(|mut ap| {
+        if compare_c_str(ap.arg::<*const c_char>(), "Correct") {
+            0
+        } else {
+            0xff
         }
-    }
-
-    ap.copy(|ap| {
-        compare_answers(&slice[skip_n..], ap)
     })
 }
