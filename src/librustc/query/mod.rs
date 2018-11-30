bf50c64083e7e9f9685e1c588dd9b93094190a26
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-11-30 20:02:56 +0100

    Update used_trait_imports

--- src/librustc/query/mod.rs
+++ src/librustc/query/mod.rs
@@ -356,7 +356,7 @@ rustc_queries! {
     }
 
     Other {
-        query used_trait_imports(_: DefId) -> Lrc<DefIdSet> {}
+        query used_trait_imports(_: DefId) -> &'tcx DefIdSet {}
     }
 
     TypeChecking {
