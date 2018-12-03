12492a9e42a1bd151a069cfe32b23d320f26565a
Sebastian Malton <sebastian@malton.name>
2018-12-03 12:59:44 -0500

    fixed diagnostic text

--- src/librustc_mir/diagnostics.rs
+++ src/librustc_mir/diagnostics.rs
@@ -335,6 +335,8 @@ match Some(42) {
 "##,
 
 E0162: r##"
+#### Note: this error code is no longer emitted by the compiler.
+
 An if-let pattern attempts to match the pattern, and enters the body if the
 match was successful. If the match is irrefutable (when it cannot fail to
 match), use a regular `let`-binding instead. For instance:
@@ -362,6 +364,8 @@ println!("{}", x);
 "##,
 
 E0165: r##"
+#### Note: this error code is no longer emitted by the compiler.
+
 A while-let pattern attempts to match the pattern, and enters the body if the
 match was successful. If the match is irrefutable (when it cannot fail to
 match), use a regular `let`-binding inside a `loop` instead. For instance:
