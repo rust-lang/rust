1192b3933c30aa23c460d0aca17534ff3f4d342b
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-12-04 23:04:23 +0100

    dbg

--- src/librustc/ty/query/plumbing.rs
+++ src/librustc/ty/query/plumbing.rs
@@ -109,7 +109,8 @@ impl<'a, 'tcx, Q: QueryDescription<'tcx>> JobOwner<'a, 'tcx, Q> {
     ///
     /// This function is inlined because that results in a noticeable speedup
     /// for some compile-time benchmarks.
-    #[inline(always)]
+    //#[inline(always)]
+    // FIXME: Remove inline(never)
     pub(super) fn try_get(
         tcx: TyCtxt<'a, 'tcx, '_>,
         span: Span,
