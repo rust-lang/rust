ed64b1927b4e929e905f778870a9f53d75216b34
varkor <github@varkor.com>
2018-12-05 23:21:56 +0100

    Fix precise_pointer_size_matching tests on all platforms

--- src/test/ui/precise_pointer_size_matching.rs
+++ src/test/ui/precise_pointer_size_matching.rs
@@ -1,3 +1,10 @@
+// normalize-stderr-32bit: "-2147483648isize" -> "$$ISIZE_MIN"
+// normalize-stderr-64bit: "-9223372036854775808isize" -> "$$ISIZE_MIN"
+// normalize-stderr-32bit: "2147483647isize" -> "$$ISIZE_MAX"
+// normalize-stderr-64bit: "9223372036854775807isize" -> "$$ISIZE_MAX"
+// normalize-stderr-32bit: "4294967295usize" -> "$$USIZE_MAX"
+// normalize-stderr-64bit: "18446744073709551615usize" -> "$$USIZE_MAX"
+
 #![feature(precise_pointer_size_matching)]
 #![feature(exclusive_range_pattern)]
 
