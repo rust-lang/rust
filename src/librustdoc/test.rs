a3f7f97205dc158875228f607effc4f5e35438f8
Wesley Norris <repnop@outlook.com>
2018-12-08 14:22:08 -0500

    Fix tidy error.

--- src/librustdoc/test.rs
+++ src/librustdoc/test.rs
@@ -284,8 +284,16 @@ fn run_test(test: &str, cratename: &str, filename: &FileName, line: usize,
 
         let outdir = Mutex::new(
             if let Some(mut path) = persist_doctests {
-                path.push(format!("{}_{}", filename.to_string().rsplit('/').next().unwrap().replace(".", "_"), line));
-                std::fs::create_dir_all(&path).expect("Couldn't create directory for doctest executables");
+                path.push(format!("{}_{}",
+                    filename
+                        .to_string()
+                        .rsplit('/')
+                        .next()
+                        .unwrap()
+                        .replace(".", "_"), line)
+                );
+                std::fs::create_dir_all(&path)
+                    .expect("Couldn't create directory for doctest executables");
 
                 DirState::Perm(path)
             } else {
