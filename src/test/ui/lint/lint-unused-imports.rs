5ef71508fec1e2f89b257f6b6b65e5ca3295d658
Pietro Albini <pietro@pietroalbini.org>
2018-12-09 17:40:49 +0100

    unused_imports: update tests

--- src/test/ui/lint/lint-unused-imports.rs
+++ src/test/ui/lint/lint-unused-imports.rs
@@ -6,7 +6,7 @@ use bar::c::cc as cal;
 use std::mem::*;            // shouldn't get errors for not using
                             // everything imported
 use std::fmt::{};
-//~^ ERROR unused import: `use std::fmt::{};`
+//~^ ERROR unused import: `std::fmt::{}`
 
 // Should get errors for both 'Some' and 'None'
 use std::option::Option::{Some, None};
