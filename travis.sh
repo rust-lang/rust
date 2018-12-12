ee2b5bb6bbd8867bc964a25a464278d61e5ff112
Ralf Jung <post@ralfj.de>
2018-12-12 19:52:49 +0100

    travis: explain what we are doing

--- travis.sh
+++ travis.sh
@@ -10,17 +10,21 @@ else
   FOREIGN_TARGET=i686-unknown-linux-gnu
 fi
 
-# Build and install miri
+echo "Build and install miri"
 cargo build --release --all-features --all-targets &&
 cargo install --all-features --force --path .
+echo
 
-# Get ourselves a MIR-full libstd for the host and a foreign architecture
+echo "Get ourselves a MIR-full libstd for the host and a foreign architecture"
 cargo miri setup &&
 cargo miri setup --target "$FOREIGN_TARGET"
+echo
 
-# Test miri with full MIR, on the host and other architectures
+echo "Test miri with full MIR, on the host and other architectures"
 MIRI_SYSROOT=$MIRI_SYSROOT_BASE/HOST cargo test --release --all-features &&
 MIRI_SYSROOT=$MIRI_SYSROOT_BASE MIRI_TARGET=$FOREIGN_TARGET cargo test --release --all-features
+echo
 
-# Test cargo integration
+echo "Test cargo integration"
 (cd test-cargo-miri && MIRI_SYSROOT=$MIRI_SYSROOT_BASE/HOST ./run-test.py)
+echo
