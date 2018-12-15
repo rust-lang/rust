e6c53328015ebff467c960431f6335f07575c35b
Alexander Regueiro <alexreg@me.com>
2018-12-15 17:31:46 +0000

    Corrected expected test err messages.

--- src/test/ui/issues/issue-56199.rs
+++ src/test/ui/issues/issue-56199.rs
@@ -5,18 +5,18 @@ struct Bar {}
 impl Foo {
     fn foo() {
         let _ = Self;
-        //~^ ERROR the `Self` constructor can only be used with tuple structs
+        //~^ ERROR the `Self` constructor can only be used with tuple or unit structs
         let _ = Self();
-        //~^ ERROR the `Self` constructor can only be used with tuple structs
+        //~^ ERROR the `Self` constructor can only be used with tuple or unit structs
     }
 }
 
 impl Bar {
     fn bar() {
         let _ = Self;
-        //~^ ERROR the `Self` constructor can only be used with tuple structs
+        //~^ ERROR the `Self` constructor can only be used with tuple or unit structs
         let _ = Self();
-        //~^ ERROR the `Self` constructor can only be used with tuple structs
+        //~^ ERROR the `Self` constructor can only be used with tuple or unit structs
     }
 }
 
