3d0bcd28e2cb27829e1ae22efadb03fcc61b76ae
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2018-12-16 16:18:45 +0300

    privacy: Use common `DefId` visiting infra for all privacy visitors

--- src/test/ui/error-codes/E0445.rs
+++ src/test/ui/error-codes/E0445.rs
@@ -4,12 +4,9 @@ trait Foo {
 
 pub trait Bar : Foo {}
 //~^ ERROR private trait `Foo` in public interface [E0445]
-//~| NOTE can't leak private trait
 pub struct Bar2<T: Foo>(pub T);
 //~^ ERROR private trait `Foo` in public interface [E0445]
-//~| NOTE can't leak private trait
 pub fn foo<T: Foo> (t: T) {}
 //~^ ERROR private trait `Foo` in public interface [E0445]
-//~| NOTE can't leak private trait
 
 fn main() {}
