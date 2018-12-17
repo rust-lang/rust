a4968a72ef3ac2ace13511268d31afaeb3b78637
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-12-17 14:20:42 +0100

    Fix a recently introduces regression

---
+++ src/test/ui/consts/static_mut_containing_mut_ref2.rs
@@ -0,0 +1,8 @@
+#![feature(const_let)]
+
+static mut STDERR_BUFFER_SPACE: u8 = 0;
+
+pub static mut STDERR_BUFFER: () = unsafe { *(&mut STDERR_BUFFER_SPACE) = 42; };
+//~^ references in statics may only refer to immutable values
+
+fn main() {}
