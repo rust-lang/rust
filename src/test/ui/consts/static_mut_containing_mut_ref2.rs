f6f28d89e221fc03c021cf079816da0fc6ac07dc
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-12-17 14:36:10 +0100

    Also test projections

--- src/test/ui/consts/static_mut_containing_mut_ref2.rs
+++ src/test/ui/consts/static_mut_containing_mut_ref2.rs
@@ -3,6 +3,6 @@
 static mut STDERR_BUFFER_SPACE: u8 = 0;
 
 pub static mut STDERR_BUFFER: () = unsafe { *(&mut STDERR_BUFFER_SPACE) = 42; };
-//~^ references in statics may only refer to immutable values
+//~^ ERROR references in statics may only refer to immutable values
 
 fn main() {}
