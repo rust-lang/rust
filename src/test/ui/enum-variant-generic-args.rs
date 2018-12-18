89a33ddebf4a2f850068574302b865bd7ef8a2b3
Alexander Regueiro <alexreg@me.com>
2018-12-18 00:40:22 +0000

    Added regression test for using generic parameters on modules.

--- src/test/ui/enum-variant-generic-args.rs
+++ src/test/ui/enum-variant-generic-args.rs
@@ -7,22 +7,22 @@ type AliasFixed = Enum<()>;
 impl<T> Enum<T> {
     fn ts_variant() {
         Self::TSVariant::<()>(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
         Self::<()>::TSVariant(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
         Self::<()>::TSVariant::<()>(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
-        //~^^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
+        //~^^ ERROR type arguments are not allowed on this entity [E0109]
     }
 
     fn s_variant() {
         Self::SVariant::<()>(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
         Self::<()>::SVariant(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
         Self::<()>::SVariant::<()>(());
-        //~^ ERROR type parameters are not allowed on this type [E0109]
-        //~^^ ERROR type parameters are not allowed on this type [E0109]
+        //~^ ERROR type arguments are not allowed on this entity [E0109]
+        //~^^ ERROR type arguments are not allowed on this entity [E0109]
     }
 }
 
@@ -30,36 +30,36 @@ fn main() {
     // Tuple struct variant
 
     Enum::<()>::TSVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
 
     Alias::TSVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     Alias::<()>::TSVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
 
     AliasFixed::TSVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     AliasFixed::<()>::TSVariant(());
     //~^ ERROR wrong number of type arguments: expected 0, found 1 [E0107]
     AliasFixed::<()>::TSVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     //~^^ ERROR wrong number of type arguments: expected 0, found 1 [E0107]
 
     // Struct variant
 
     Enum::<()>::SVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
 
     Alias::SVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     Alias::<()>::SVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
 
     AliasFixed::SVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     AliasFixed::<()>::SVariant(());
     //~^ ERROR wrong number of type arguments: expected 0, found 1 [E0107]
     AliasFixed::<()>::SVariant::<()>(());
-    //~^ ERROR type parameters are not allowed on this type [E0109]
+    //~^ ERROR type arguments are not allowed on this entity [E0109]
     //~^^ ERROR wrong number of type arguments: expected 0, found 1 [E0107]
 }
--- src/test/ui/rfc1598-generic-associated-types/generic_associated_type_undeclared_lifetimes.rs
+++ src/test/ui/rfc1598-generic-associated-types/generic_associated_type_undeclared_lifetimes.rs
@@ -3,20 +3,20 @@
 
 use std::ops::Deref;
 
-// FIXME(#44265): "lifetime parameters are not allowed on this type" errors will be addressed in a
+// FIXME(#44265): "lifetime arguments are not allowed on this entity" errors will be addressed in a
 // follow-up PR.
 
 trait Iterable {
     type Item<'a>;
     type Iter<'a>: Iterator<Item = Self::Item<'a>>
-    //~^ ERROR lifetime parameters are not allowed on this type [E0110]
+    //~^ ERROR lifetime arguments are not allowed on this entity [E0110]
         + Deref<Target = Self::Item<'b>>;
     //~^ ERROR undeclared lifetime
-    //~| ERROR lifetime parameters are not allowed on this type [E0110]
+    //~| ERROR lifetime arguments are not allowed on this entity [E0110]
 
     fn iter<'a>(&'a self) -> Self::Iter<'undeclared>;
     //~^ ERROR undeclared lifetime
-    //~| ERROR lifetime parameters are not allowed on this type [E0110]
+    //~| ERROR lifetime arguments are not allowed on this entity [E0110]
 }
 
 fn main() {}
