66f0e42b4e6fd719099fe327d2731abd5b66ed41
Alex Crichton <alex@alexcrichton.com>
2018-12-18 13:32:09 -0800

    ci: Upgrade Travis to Xenial
    In theory we shouldn't require trusty so long as docker continues to
    work!

--- src/ci/docker/run.sh
+++ src/ci/docker/run.sh
@@ -19,10 +19,12 @@ travis_time_start
 
 if [ -f "$docker_dir/$image/Dockerfile" ]; then
     if [ "$CI" != "" ]; then
-      cksum=$(find $docker_dir/$image $docker_dir/scripts -type f | \
+      hash_key=/tmp/.docker-hash-key.txt
+      find $docker_dir/$image $docker_dir/scripts -type f | \
         sort | \
-        xargs cat | \
-        sha512sum | \
+        xargs cat >> $hash_key
+      docker --version >> $hash_key
+      cksum=$(sha512sum $hash_key | \
         awk '{print $1}')
       s3url="s3://$SCCACHE_BUCKET/docker/$cksum"
       url="https://s3-us-west-1.amazonaws.com/$SCCACHE_BUCKET/docker/$cksum"
