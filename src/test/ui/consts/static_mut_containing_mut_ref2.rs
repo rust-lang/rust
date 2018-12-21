6ed596ebe245df1a8de8a609222c8e217508f6dd
Oliver Scherer <github35764891676564198441@oli-obk.de>
2018-12-21 13:39:45 +0100

    Update tests to changes on master

--- src/test/ui/consts/static_mut_containing_mut_ref2.rs
+++ src/test/ui/consts/static_mut_containing_mut_ref2.rs
@@ -4,5 +4,6 @@ static mut STDERR_BUFFER_SPACE: u8 = 0;
 
 pub static mut STDERR_BUFFER: () = unsafe { *(&mut STDERR_BUFFER_SPACE) = 42; };
 //~^ ERROR references in statics may only refer to immutable values
+//~| ERROR static contains unimplemented expression type
 
 fn main() {}
