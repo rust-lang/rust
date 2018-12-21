b890d517430a8f691b8ffa738c05327df8690e8a
Alex Crichton <alex@alexcrichton.com>
2018-12-21 08:49:59 -0800

    rustc: Rewrite platform intrinsics crate
    This commit rewrites (updates) the `rustc_platform_intrinsics` crate
    in this repository. This crate was previously procedurally generated
    from a number of JSON files and was intended to be the basis for how we
    bind SIMD in Rust. This crate is quite old at this point and is actually
    only used very little. The `stdsimd` crate and `std::arch` modules are
    the "new" way for stabilizing SIMD intrinsics.
    
    Despite this, however, the crate isn't useless! Most LLVM intrinsics are
    called directly in the `stdsimd` crate using the `link_llvm_intrinsics`
    feature, but not all intrinsics can be expressed directly in Rust.
    Instead some intrinsics, notably those that return LLVM aggregates, need
    to be compiler-defined to ensure they're declared with the correct
    signature.
    
    A good example of this is the x86 rdrand/rdseed family of LLVM
    intrinsics, all of which return an aggregate of some form. The stdsimd
    functions which provide these x86 intrinsics link to functions defined
    by this crate, so we need to keep those!
    
    Adding all that together, this commit performs the following tasks:
    
    * Deletes the now-outdated `src/etc/platform-intrinsics` infrastructure
    * Deletes all `src/librustc_platform_intrinsics/*` intrinsics that are
      otherwise compatible to be defined in Rust itself. This includes
      everything that doesn't deal with aggregates basically. Empty
      architecture files are removed.
    * The style of `src/librustc_platform_intrinsics/` was updated slightly
      to make it a bit easier to handwrite. The `x86` rdrand/rdseed
      intrinsics and some `aarch64` intrinsics which return aggregates have
      all survived to continue to be defined by this crate.
    
    Closes #41885

--- src/librustc_platform_intrinsics/lib.rs
+++ src/librustc_platform_intrinsics/lib.rs
@@ -10,12 +10,10 @@
 
 #![allow(nonstandard_style)]
 
-#![feature(nll)]
-
+#[derive(Copy, Clone)]
 pub struct Intrinsic {
     pub inputs: &'static [&'static Type],
     pub output: &'static Type,
-
     pub definition: IntrinsicDef,
 }
 
@@ -29,6 +27,7 @@ pub enum Type {
     Aggregate(bool, &'static [&'static Type]),
 }
 
+#[derive(Copy, Clone)]
 pub enum IntrinsicDef {
     Named(&'static str),
 }
@@ -44,89 +43,53 @@ static U64: Type = Type::Integer(false, 64, 64);
 static F32: Type = Type::Float(32);
 static F64: Type = Type::Float(64);
 
-static I32_8: Type = Type::Integer(true, 32, 8);
-
 static I8x8: Type = Type::Vector(&I8, None, 8);
 static U8x8: Type = Type::Vector(&U8, None, 8);
 static I8x16: Type = Type::Vector(&I8, None, 16);
 static U8x16: Type = Type::Vector(&U8, None, 16);
-static I8x32: Type = Type::Vector(&I8, None, 32);
-static U8x32: Type = Type::Vector(&U8, None, 32);
-static I8x64: Type = Type::Vector(&I8, None, 64);
-static U8x64: Type = Type::Vector(&U8, None, 64);
-static I8x128: Type = Type::Vector(&I8, None, 128);
-static U8x128: Type = Type::Vector(&U8, None, 128);
-static I8x256: Type = Type::Vector(&I8, None, 256);
-static U8x256: Type = Type::Vector(&U8, None, 256);
 
 static I16x4: Type = Type::Vector(&I16, None, 4);
 static U16x4: Type = Type::Vector(&U16, None, 4);
 static I16x8: Type = Type::Vector(&I16, None, 8);
 static U16x8: Type = Type::Vector(&U16, None, 8);
-static I16x16: Type = Type::Vector(&I16, None, 16);
-static U16x16: Type = Type::Vector(&U16, None, 16);
-static I16x32: Type = Type::Vector(&I16, None, 32);
-static U16x32: Type = Type::Vector(&U16, None, 32);
-static I16x64: Type = Type::Vector(&I16, None, 64);
-static U16x64: Type = Type::Vector(&U16, None, 64);
-static I16x128: Type = Type::Vector(&I16, None, 128);
-static U16x128: Type = Type::Vector(&U16, None, 128);
 
 static I32x2: Type = Type::Vector(&I32, None, 2);
 static U32x2: Type = Type::Vector(&U32, None, 2);
 static I32x4: Type = Type::Vector(&I32, None, 4);
 static U32x4: Type = Type::Vector(&U32, None, 4);
-static I32x8: Type = Type::Vector(&I32, None, 8);
-static U32x8: Type = Type::Vector(&U32, None, 8);
-static I32x16: Type = Type::Vector(&I32, None, 16);
-static U32x16: Type = Type::Vector(&U32, None, 16);
-static I32x32: Type = Type::Vector(&I32, None, 32);
-static U32x32: Type = Type::Vector(&U32, None, 32);
-static I32x64: Type = Type::Vector(&I32, None, 64);
-static U32x64: Type = Type::Vector(&U32, None, 64);
 
 static I64x1: Type = Type::Vector(&I64, None, 1);
 static U64x1: Type = Type::Vector(&U64, None, 1);
 static I64x2: Type = Type::Vector(&I64, None, 2);
 static U64x2: Type = Type::Vector(&U64, None, 2);
-static I64x4: Type = Type::Vector(&I64, None, 4);
-static U64x4: Type = Type::Vector(&U64, None, 4);
 
 static F32x2: Type = Type::Vector(&F32, None, 2);
 static F32x4: Type = Type::Vector(&F32, None, 4);
-static F32x8: Type = Type::Vector(&F32, None, 8);
 static F64x1: Type = Type::Vector(&F64, None, 1);
 static F64x2: Type = Type::Vector(&F64, None, 2);
-static F64x4: Type = Type::Vector(&F64, None, 4);
 
-static I32x4_F32: Type = Type::Vector(&I32, Some(&F32), 4);
-static I32x8_F32: Type = Type::Vector(&I32, Some(&F32), 8);
-static I64x2_F64: Type = Type::Vector(&I64, Some(&F64), 2);
-static I64x4_F64: Type = Type::Vector(&I64, Some(&F64), 4);
-
-static VOID: Type = Type::Void;
+macro_rules! intrinsics {
+    ($name:ident, $prefix:expr, $($suffix:expr => $expr:expr,)*) => ({
+        if !$name.starts_with($prefix) { return None }
+        Some(match &$name[$prefix.len()..] {
+            $($suffix => {
+                static I: Intrinsic = $expr;
+                I
+            })*
+            _ => return None,
+        })
+    })
+}
 
 mod x86;
-mod arm;
 mod aarch64;
-mod nvptx;
-mod hexagon;
-mod powerpc;
 
 impl Intrinsic {
     pub fn find(name: &str) -> Option<Intrinsic> {
         if name.starts_with("x86_") {
             x86::find(name)
-        } else if name.starts_with("arm_") {
-            arm::find(name)
         } else if name.starts_with("aarch64_") {
             aarch64::find(name)
-        } else if name.starts_with("nvptx_") {
-            nvptx::find(name)
-        } else if name.starts_with("Q6_") {
-            hexagon::find(name)
-        } else if name.starts_with("powerpc_") {
-            powerpc::find(name)
         } else {
             None
         }
