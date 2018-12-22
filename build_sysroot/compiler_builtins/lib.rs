6e5d9abe9cd0aba8e1c97528d4d91732f63dc58e
bjorn3 <bjorn3@users.noreply.github.com>
2018-12-22 10:54:38 +0100

    Rustup to rustc 1.33.0-nightly (e40548bc4 2018-12-21)

--- build_sysroot/compiler_builtins/lib.rs
+++ build_sysroot/compiler_builtins/lib.rs
@@ -1,3 +1,9 @@
-#![feature(compiler_builtins)]
+#![feature(compiler_builtins, staged_api)]
 #![compiler_builtins]
 #![no_std]
+
+#![unstable(
+    feature = "compiler_builtins_lib",
+    reason = "Compiler builtins. Will never become stable.",
+    issue = "0"
+)]
