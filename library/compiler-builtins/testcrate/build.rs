fe9b17acec9b1e0d2028ab997ca5768708bd2867
Lokathor <zefria@gmail.com>
2018-12-21 18:47:03 -0700

    Rust and LLVM don't spec the same output types

--- library/compiler-builtins/testcrate/build.rs
+++ library/compiler-builtins/testcrate/build.rs
@@ -778,7 +778,7 @@ fn main() {
     
     // count leading zeros
     gen(|a: MyU64| {
-            Some((a.0 as usize).leading_zeros())
+            Some((a.0 as usize).leading_zeros() as usize)
         },
         "{
             builtins::int::__clzsi2(a as usize)
