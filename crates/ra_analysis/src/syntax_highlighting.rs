072028e67996162f5a9da14cfd59ed64de5e8729
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-12-28 13:27:30 +0300

    add macro-call node

--- crates/ra_analysis/src/syntax_highlighting.rs
+++ crates/ra_analysis/src/syntax_highlighting.rs
@@ -7,6 +7,8 @@ use crate::{
 };
 
 pub(crate) fn highlight(db: &RootDatabase, file_id: FileId) -> Cancelable<Vec<HighlightedRange>> {
-    let file = db.source_file(file_id);
-    Ok(ra_editor::highlight(&file))
+    let source_file = db.source_file(file_id);
+    let mut res = ra_editor::highlight(&source_file);
+    for node in source_file.syntax().descendants() {}
+    Ok(res)
 }
