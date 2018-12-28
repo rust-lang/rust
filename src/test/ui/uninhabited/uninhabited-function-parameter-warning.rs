2c8f042c204f22166fda3498a1dbaa56a37de6a9
varkor <github@varkor.com>
2018-12-28 18:52:15 +0000

    Add tests for privately/publicly uninhabited types in function signatures

--- src/test/ui/uninhabited/uninhabited-function-parameter-warning.rs
+++ src/test/ui/uninhabited/uninhabited-function-parameter-warning.rs
@@ -2,9 +2,29 @@
 
 enum Void {}
 
+mod hide {
+    pub struct PrivatelyUninhabited(::Void);
+
+    pub struct PubliclyUninhabited(pub ::Void);
+}
+
+// Check that functions with (publicly) uninhabited parameters trigger a lint.
+
 fn foo(a: (), b: Void) { //~ ERROR functions with parameters of uninhabited types are uncallable
     a
 }
+
+fn bar(a: (), b: hide::PrivatelyUninhabited) { // ok
+    a
+}
+
+fn baz(a: (), b: hide::PubliclyUninhabited) {
+    //~^ ERROR functions with parameters of uninhabited types are uncallable
+    a
+}
+
+// Check that trait methods with uninhabited parameters do not trigger a lint
+// (at least for now).
 
 trait Foo {
     fn foo(a: Self);
