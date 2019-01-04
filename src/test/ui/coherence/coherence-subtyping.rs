d758e4db783618daef974d067fdf6cfb285f48d5
Georg Semmler <georg_semmler_05@web.de>
2019-01-04 10:19:22 +0100

    Update tests changed by rebase

--- src/test/ui/coherence/coherence-subtyping.rs
+++ src/test/ui/coherence/coherence-subtyping.rs
@@ -1,6 +1,10 @@
 // Test that two distinct impls which match subtypes of one another
 // yield coherence errors (or not) depending on the variance.
 
+// revisions: old re
+
+#![cfg_attr(re, feature(re_rebalance_coherence))]
+
 trait TheTrait {
     fn foo(&self) { }
 }
@@ -9,7 +13,8 @@ impl TheTrait for for<'a,'b> fn(&'a u8, &'b u8) -> &'a u8 {
 }
 
 impl TheTrait for for<'a> fn(&'a u8, &'a u8) -> &'a u8 {
-    //~^ ERROR
+    //[old]~^ ERROR
+    //[re]~^^ ERROR
 }
 
 fn main() { }
