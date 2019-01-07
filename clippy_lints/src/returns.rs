38d4ac7ceaf8ace26864e847c8280dc66410587c
Philipp Hansch <dev@phansch.net>
2019-01-07 22:33:18 +0100

    Remove all copyright license headers
    Discussion previously happened in https://github.com/rust-lang/rust/pull/43498

--- clippy_lints/src/returns.rs
+++ clippy_lints/src/returns.rs
@@ -1,12 +1,3 @@
-// Copyright 2014-2018 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 use crate::utils::{in_macro, match_path_ast, snippet_opt, span_lint_and_then, span_note_and_lint};
 use if_chain::if_chain;
 use rustc::lint::{in_external_macro, EarlyContext, EarlyLintPass, LintArray, LintContext, LintPass};
