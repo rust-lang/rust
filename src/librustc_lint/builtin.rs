3cf3ed787740a82b4f3ab76db454218177a7a08a
Yuki Okushi <huyuumi.dev@gmail.com>
2019-01-09 21:13:42 +0900

    Separate the description on different lines

--- src/librustc_lint/builtin.rs
+++ src/librustc_lint/builtin.rs
@@ -229,7 +229,9 @@ impl UnsafeCode {
 impl<'a, 'tcx> LateLintPass<'a, 'tcx> for UnsafeCode {
     fn check_attribute(&mut self, cx: &LateContext, attr: &ast::Attribute) {
         if attr.check_name("allow_internal_unsafe") {
-            self.report_unsafe(cx, attr.span, "`allow_internal_unsafe` allows defining macros using unsafe without triggering the `unsafe_code` lint at their call site");
+            self.report_unsafe(cx, attr.span, "`allow_internal_unsafe` allows defining \
+                                               macros using unsafe without triggering \
+                                               the `unsafe_code` lint at their call site");
         }
     }
 
