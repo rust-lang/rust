2c57d1d256b84d3bd7f4a1d9613091bd9ec3ca02
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-01-14 19:51:00 +0100

    Add regression test

--- src/test/ui/consts/match_ice.rs
+++ src/test/ui/consts/match_ice.rs
@@ -1,11 +1,10 @@
-// compile-pass
 // https://github.com/rust-lang/rust/issues/53708
 
 struct S;
 
 fn main() {
     const C: &S = &S;
-    match C {
-        C => {}
+    match C { //~ ERROR non-exhaustive
+        C => {} // this is a common bug around constants and references in patterns
     }
 }
