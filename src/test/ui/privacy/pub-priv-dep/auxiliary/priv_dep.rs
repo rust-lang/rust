12f9b796ff9a551d540390647043f4bcd1728838
Aaron Hill <aa1ronham@gmail.com>
2019-01-13 20:37:27 -0500

    Improve UI tests

--- src/test/ui/privacy/pub-priv-dep/auxiliary/priv_dep.rs
+++ src/test/ui/privacy/pub-priv-dep/auxiliary/priv_dep.rs
@@ -1,1 +1,2 @@
 pub struct OtherType;
+pub trait OtherTrait {}
--- src/test/ui/privacy/pub-priv-dep/pub-priv1.rs
+++ src/test/ui/privacy/pub-priv-dep/pub-priv1.rs
@@ -1,11 +1,16 @@
  // aux-build:priv_dep.rs
+ // aux-build:pub_dep.rs
+ // compile-flags: --extern-public=pub_dep
 #![feature(public_private_dependencies)]
 #![deny(leaked_private_dependency)]
 
 // This crate is a private dependency
 extern crate priv_dep;
+// This crate is a public dependenct
+extern crate pub_dep;
 
-use priv_dep::OtherType;
+use priv_dep::{OtherType, OtherTrait};
+use pub_dep::PubType;
 
 // Type from private dependency used in private
 // type - this is fine
@@ -17,7 +22,8 @@ pub struct PublicType {
     pub field: OtherType,
     //~^ ERROR type `priv_dep::OtherType` from private dependency 'priv_dep' in public interface [leaked_private_dependency]
     //~| WARNING this was previously accepted
-    priv_field: OtherType,
+    priv_field: OtherType, // Private field - this is fine
+    pub other_field: PubType // Type from public dependency - this is fine
 }
 
 impl PublicType {
@@ -27,5 +33,12 @@ impl PublicType {
 
     fn priv_fn(param: OtherType) {}
 }
+
+pub trait MyPubTrait {
+    type Foo: OtherTrait;
+}
+//~^^^ ERROR trait `priv_dep::OtherTrait` from private dependency 'priv_dep' in public interface [leaked_private_dependency]
+//~| WARNING this was previously accepted
+
 
 fn main() {}
