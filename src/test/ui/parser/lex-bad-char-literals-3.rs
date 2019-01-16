d33ee3fefa3fad88fee81b5abb316e94c387d72e
Yuki Okushi <huyuumi.dev@gmail.com>
2019-01-16 09:28:26 +0900

    Fix tests

--- src/test/ui/parser/lex-bad-char-literals-3.rs
+++ src/test/ui/parser/lex-bad-char-literals-3.rs
@@ -1,7 +1,6 @@
 // This test needs to the last one appearing in this file as it kills the parser
 static c: char =
     '●●' //~ ERROR: character literal may only contain one codepoint
-         //~| ERROR: mismatched types
 ;
 
 fn main() {}
--- src/test/ui/parser/lex-bad-char-literals-5.rs
+++ src/test/ui/parser/lex-bad-char-literals-5.rs
@@ -2,7 +2,6 @@
 // This test needs to the last one appearing in this file as it kills the parser
 static c: char =
     '\x10\x10'  //~ ERROR: character literal may only contain one codepoint
-                //~| ERROR: mismatched types
 ;
 
 fn main() {}
