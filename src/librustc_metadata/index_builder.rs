22ad7cdf87633160dc48bb3e1b556386c5acc1a5
ljedrz <ljedrz@gmail.com>
2019-01-18 15:17:32 +0100

    metadata: HirId-ify

--- src/librustc_metadata/index_builder.rs
+++ src/librustc_metadata/index_builder.rs
@@ -185,7 +185,7 @@ macro_rules! read_hir {
     ($t:ty) => {
         impl<'tcx> DepGraphRead for &'tcx $t {
             fn read(&self, tcx: TyCtxt) {
-                tcx.hir().read(self.id);
+                tcx.hir().read_by_hir_id(self.hir_id);
             }
         }
     }
@@ -215,10 +215,10 @@ impl<T> DepGraphRead for Untracked<T> {
 /// HIR node that doesn't carry its own id. This will allow an
 /// arbitrary `T` to be passed in, but register a read on the given
 /// node-id.
-pub struct FromId<T>(pub ast::NodeId, pub T);
+pub struct FromId<T>(pub hir::HirId, pub T);
 
 impl<T> DepGraphRead for FromId<T> {
     fn read(&self, tcx: TyCtxt) {
-        tcx.hir().read(self.0);
+        tcx.hir().read_by_hir_id(self.0);
     }
 }
