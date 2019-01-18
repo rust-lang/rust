078e447e5a355a6686370f16e49e2e66f38980ef
ljedrz <ljedrz@gmail.com>
2019-01-18 15:20:51 +0100

    traits: HirId-ify

--- src/librustc_traits/type_op.rs
+++ src/librustc_traits/type_op.rs
@@ -1,6 +1,7 @@
 use rustc::infer::at::ToTrace;
 use rustc::infer::canonical::{Canonical, QueryResponse};
 use rustc::infer::InferCtxt;
+use rustc::hir;
 use rustc::hir::def_id::DefId;
 use rustc::mir::ProjectionKind;
 use rustc::mir::tcx::PlaceTy;
@@ -20,7 +21,6 @@ use rustc::ty::{
 };
 use rustc_data_structures::sync::Lrc;
 use std::fmt;
-use syntax::ast;
 use syntax_pos::DUMMY_SP;
 
 crate fn provide(p: &mut Providers) {
@@ -74,7 +74,7 @@ impl AscribeUserTypeCx<'me, 'gcx, 'tcx> {
         self.infcx
             .partially_normalize_associated_types_in(
                 DUMMY_SP,
-                ast::CRATE_NODE_ID,
+                hir::CRATE_HIR_ID,
                 self.param_env,
                 &value,
             )
