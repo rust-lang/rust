87d24e1fc998269d530e0fd88ba1c89d2171fb55
daxpedda <daxpedda@users.noreply.github.com>
2019-01-21 13:59:49 +0100

    Actually check for constants.

--- clippy_lints/src/arithmetic.rs
+++ clippy_lints/src/arithmetic.rs
@@ -1,3 +1,4 @@
+use crate::consts::constant_simple;
 use crate::utils::span_lint;
 use rustc::hir;
 use rustc::lint::{LateContext, LateLintPass, LintArray, LintPass};
@@ -92,7 +93,13 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for Arithmetic {
                 }
             },
             hir::ExprKind::Unary(hir::UnOp::UnNeg, arg) => {
-                if cx.tables.expr_ty(arg).is_floating_point() {
+                let ty = cx.tables.expr_ty(arg);
+                if ty.is_integral() {
+                    if constant_simple(cx, cx.tables, expr).is_none() {
+                        span_lint(cx, INTEGER_ARITHMETIC, expr.span, "integer arithmetic detected");
+                        self.expr_span = Some(expr.span);
+                    }
+                } else if ty.is_floating_point() {
                     span_lint(cx, FLOAT_ARITHMETIC, expr.span, "floating-point arithmetic detected");
                     self.expr_span = Some(expr.span);
                 }
