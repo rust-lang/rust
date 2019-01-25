e4120ee67864524c503ee2f59b357ce93a43980b
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2019-01-26 00:36:50 +0300

    Address review comments and cleanup code

--- src/test/ui/issues/issue-56411.rs
+++ src/test/ui/issues/issue-56411.rs
@@ -3,14 +3,14 @@ macro_rules! import {
         $(
             mod $name;
             pub use self::$name;
-            //~^ ERROR the name `issue_56411` is defined multiple times
-            //~| ERROR `issue_56411` is private, and cannot be re-exported
+            //~^ ERROR the name `issue_56411_aux` is defined multiple times
+            //~| ERROR `issue_56411_aux` is private, and cannot be re-exported
 
         )*
     }
 }
 
-import!(issue_56411);
+import!(issue_56411_aux);
 
 fn main() {
     println!("Hello, world!");
