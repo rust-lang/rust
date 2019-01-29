c7ae44c0e2aad23e6ca74893a3e3ed1a060c0ff0
Alex Hamilton <alex.hamilton@ou.edu>
2019-01-29 12:23:11 -0600

    wildcard_match_arm: format test.

--- tests/ui/wildcard_match_arm.rs
+++ tests/ui/wildcard_match_arm.rs
@@ -2,35 +2,35 @@
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum Color {
-	Red,
-	Green,
-	Blue,
-	Rgb(u8, u8, u8),
-	Cyan,
+    Red,
+    Green,
+    Blue,
+    Rgb(u8, u8, u8),
+    Cyan,
 }
 
 impl Color {
-	fn is_monochrome(self) -> bool {
-		match self {
-			Color::Red | Color::Green | Color::Blue => true,
-			Color::Rgb(r, g, b) => r | g == 0 || r | b == 0 || g | b == 0,
-			Color::Cyan => false,
-		}
-	}
+    fn is_monochrome(self) -> bool {
+        match self {
+            Color::Red | Color::Green | Color::Blue => true,
+            Color::Rgb(r, g, b) => r | g == 0 || r | b == 0 || g | b == 0,
+            Color::Cyan => false,
+        }
+    }
 }
 
 fn main() {
-	let color = Color::Rgb(0, 0, 127);
-	match color {
-		Color::Red => println!("Red"),
-		_ => eprintln!("Not red"),
-	};
-	match color {
-		Color::Red => {},
-		Color::Green => {},
-		Color::Blue => {},
-		Color::Cyan => {},
-		c if c.is_monochrome() => {},
-		Color::Rgb(_, _, _) => {},
-	};
+    let color = Color::Rgb(0, 0, 127);
+    match color {
+        Color::Red => println!("Red"),
+        _ => eprintln!("Not red"),
+    };
+    match color {
+        Color::Red => {},
+        Color::Green => {},
+        Color::Blue => {},
+        Color::Cyan => {},
+        c if c.is_monochrome() => {},
+        Color::Rgb(_, _, _) => {},
+    };
 }
