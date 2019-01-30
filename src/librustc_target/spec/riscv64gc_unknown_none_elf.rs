de3e93c68307851b167a644f209fb573fabe6757
Jonathan Behrens <fintelia@gmail.com>
2019-01-30 16:29:52 -0500

    Change target to riscv64gc-unknown-none-elf and remove copyright notice

--- src/librustc_target/spec/riscv64gc_unknown_none_elf.rs
+++ src/librustc_target/spec/riscv64gc_unknown_none_elf.rs
@@ -1,19 +1,9 @@
-// Copyright 2018 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 use spec::{LinkerFlavor, LldFlavor, PanicStrategy,
            Target, TargetOptions, TargetResult};
 
 pub fn target() -> TargetResult {
     Ok(Target {
-        data_layout: "e-m:e-i64:64-n64-S128".to_string(),
+        data_layout: "e-m:e-p:64:64-i64:64-i128:128-n64-S128".to_string(),
         llvm_target: "riscv64".to_string(),
         target_endian: "little".to_string(),
         target_pointer_width: "64".to_string(),
