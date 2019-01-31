d2a1e0715088d61ab40b64d0fd8ad296251ccfa1
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-01-31 13:59:25 +0300

    more expand boilerplate

--- crates/ra_macros/src/mbe_expander.rs
+++ crates/ra_macros/src/mbe_expander.rs
@@ -1,5 +1,26 @@
+use rustc_hash::FxHashMap;
+use smol_str::SmolStr;
+
 use crate::{mbe, tt};
 
-pub fn exapnd(rules: &mbe::MacroRules, input: tt::Subtree) -> Option<tt::Subtree> {
-    Some(input)
+pub fn exapnd(rules: &mbe::MacroRules, input: &tt::Subtree) -> Option<tt::Subtree> {
+    rules.rules.iter().find_map(|it| expand_rule(it, input))
+}
+
+fn expand_rule(rule: &mbe::Rule, input: &tt::Subtree) -> Option<tt::Subtree> {
+    let bidings = match_lhs(&rule.lhs, input)?;
+    expand_rhs(&rule.rhs, &bindings)
+}
+
+#[derive(Debug, Default)]
+struct Bindings {
+    inner: FxHashMap<SmolStr, tt::TokenTree>,
+}
+
+fn match_lhs(pattern: &mbe::TokenTree, input: &tt::Subtree) -> Option<Bindings> {
+    Some(Bindings::default())
+}
+
+fn expand_rhs(template: &mbe::TokenTree, bindings: &Bindings) -> Option<tt::Subtree> {
+    None
 }
