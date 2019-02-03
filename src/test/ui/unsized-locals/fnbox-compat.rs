10097261a76073d703d572418998589217ec97dc
Masaki Hara <ackie.h.gmai@gmail.com>
2019-02-03 22:08:34 +0900

    Fix failing tests.

--- src/test/ui/unsized-locals/fnbox-compat.rs
+++ src/test/ui/unsized-locals/fnbox-compat.rs
@@ -4,9 +4,10 @@ use std::boxed::FnBox;
 
 fn call_it<T>(f: Box<dyn FnBox(&i32) -> T>) -> T {
     f(&42)
+    //~^ERROR implementation of `std::ops::FnOnce` is not general enough
 }
 
 fn main() {
     let s = "hello".to_owned();
-    assert_eq!(&call_it(Box::new(|| s)) as &str, "hello");
+    assert_eq!(&call_it(Box::new(|_| s)) as &str, "hello");
 }
