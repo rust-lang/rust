0b3d8de2a08a066fc853d5afb0f9b89905609f98
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-02-04 11:18:12 +0100

    Consistent attribute argument suggestion

--- src/libsyntax/feature_gate.rs
+++ src/libsyntax/feature_gate.rs
@@ -1091,7 +1091,7 @@ pub const BUILTIN_ATTRIBUTES: &[(&str, AttributeType, AttributeTemplate, Attribu
                                               stable",
                                              cfg_fn!(profiler_runtime))),
 
-    ("allow_internal_unstable", Normal, template!(Word, List: "feat1, feat2"),
+    ("allow_internal_unstable", Normal, template!(Word, List: "feat1, feat2, ..."),
                                               Gated(Stability::Unstable,
                                               "allow_internal_unstable",
                                               EXPLAIN_ALLOW_INTERNAL_UNSTABLE,
