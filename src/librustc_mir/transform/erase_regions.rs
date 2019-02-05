d18d6145fe3873aeded43111c47bb43e96bcae43
Santiago Pastorino <spastorino@gmail.com>
2019-02-05 15:47:35 +0100

    Remove EraseRegionsVisitor since mir implements TypeFoldable

--- src/librustc_mir/transform/erase_regions.rs
+++ src/librustc_mir/transform/erase_regions.rs
@@ -4,49 +4,9 @@
 //! NOTE:  We do NOT erase regions of statements that are relevant for
 //! "types-as-contracts"-validation, namely, AcquireValid, ReleaseValid
 
-use rustc::ty::subst::Substs;
-use rustc::ty::{self, Ty, TyCtxt};
+use rustc::ty::TyCtxt;
 use rustc::mir::*;
-use rustc::mir::visit::{MutVisitor, TyContext};
 use transform::{MirPass, MirSource};
-
-struct EraseRegionsVisitor<'a, 'tcx: 'a> {
-    tcx: TyCtxt<'a, 'tcx, 'tcx>,
-}
-
-impl<'a, 'tcx> EraseRegionsVisitor<'a, 'tcx> {
-    pub fn new(tcx: TyCtxt<'a, 'tcx, 'tcx>) -> Self {
-        EraseRegionsVisitor {
-            tcx,
-        }
-    }
-}
-
-impl<'a, 'tcx> MutVisitor<'tcx> for EraseRegionsVisitor<'a, 'tcx> {
-    fn visit_ty(&mut self, ty: &mut Ty<'tcx>, _: TyContext) {
-        *ty = self.tcx.erase_regions(ty);
-        self.super_ty(ty);
-    }
-
-    fn visit_region(&mut self, region: &mut ty::Region<'tcx>, _: Location) {
-        *region = self.tcx.types.re_erased;
-    }
-
-    fn visit_const(&mut self, constant: &mut &'tcx ty::LazyConst<'tcx>, _: Location) {
-        *constant = self.tcx.erase_regions(constant);
-    }
-
-    fn visit_substs(&mut self, substs: &mut &'tcx Substs<'tcx>, _: Location) {
-        *substs = self.tcx.erase_regions(substs);
-    }
-
-    fn visit_statement(&mut self,
-                       block: BasicBlock,
-                       statement: &mut Statement<'tcx>,
-                       location: Location) {
-        self.super_statement(block, statement, location);
-    }
-}
 
 pub struct EraseRegions;
 
@@ -55,6 +15,6 @@ impl MirPass for EraseRegions {
                           tcx: TyCtxt<'a, 'tcx, 'tcx>,
                           _: MirSource,
                           mir: &mut Mir<'tcx>) {
-        EraseRegionsVisitor::new(tcx).visit_mir(mir);
+        *mir = tcx.erase_regions(mir);
     }
 }
