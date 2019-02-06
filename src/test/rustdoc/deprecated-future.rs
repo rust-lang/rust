c8752415dc0775960accc5f6ae11be782910b3c6
varkor <github@varkor.com>
2019-02-06 14:17:27 +0100

    Add rustdoc index page test for future deprecation attributes

--- src/test/rustdoc/deprecated-future.rs
+++ src/test/rustdoc/deprecated-future.rs
@@ -1,5 +1,7 @@
 #![feature(deprecated)]
 
+// @has deprecated_future/index.html '//*[@class="stab deprecated"]' \
+//      'Deprecated'
 // @has deprecated_future/struct.S.html '//*[@class="stab deprecated"]' \
 //      'Deprecated since 99.99.99: effectively never'
 #[deprecated(since = "99.99.99", note = "effectively never")]
