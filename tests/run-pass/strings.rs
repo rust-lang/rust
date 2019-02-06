e5972c38a3322e8ab98aead64d75f63fe4c7c75d
Ralf Jung <post@ralfj.de>
2019-02-06 11:47:32 +0100

    test mutable string slice indexing

--- tests/run-pass/strings.rs
+++ tests/run-pass/strings.rs
@@ -18,10 +18,17 @@ fn fat_pointer_on_32_bit() {
     Some(5).expect("foo");
 }
 
+fn str_indexing() {
+    let mut x = "Hello".to_string();
+    let _v = &mut x[..3];  // Test IndexMut on String.
+}
+
 fn main() {
     assert_eq!(empty(), "");
     assert_eq!(hello(), "Hello, world!");
     assert_eq!(hello_bytes(), b"Hello, world!");
     assert_eq!(hello_bytes_fat(), b"Hello, world!");
+
     fat_pointer_on_32_bit(); // Should run without crashing.
+    str_indexing();
 }
