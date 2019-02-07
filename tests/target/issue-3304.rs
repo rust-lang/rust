109394536dd05f51559a9b70a2efa018166c5061
Evgenii <evgeniy@nspcc.ru>
2019-02-07 12:12:36 +0300

    add more tests

--- tests/target/issue-3304.rs
+++ tests/target/issue-3304.rs
@@ -1,7 +1,18 @@
 // rustfmt-error_on_line_overflow: true
 
+#[rustfmt::skip] use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen;
+#[rustfmt::skip]
+
+use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen;
+
 macro_rules! test_macro {
     ($($id:ident),*) => {};
+}
+
+macro_rules! test_macro2 {
+    ($($id:ident),*) => {
+        1
+    };
 }
 
 fn main() {
@@ -10,3 +21,22 @@ fn main() {
     
     test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
 }
+
+fn test_local() {
+    #[rustfmt::skip] let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+    #[rustfmt::skip]
+    
+    let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+}
+
+fn test_expr(_: [u32]) -> u32 {
+    #[rustfmt::skip] test_expr([9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999]);
+    #[rustfmt::skip]
+    
+    test_expr([9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999])
+}
+
+#[rustfmt::skip] mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen; }
+#[rustfmt::skip]
+
+mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen; }
