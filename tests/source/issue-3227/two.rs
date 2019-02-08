7d9a2ef96d8106fe399a0e97fc4087a76361c4e2
rchaser53 <tayoshizawa29@gmail.com>
2019-02-08 21:39:11 +0900

    version/2: Align loop and while formatting
    The loop and while formatting was diverting as  `loop`
    was not being moved to a new, indented block, as `while`
    was.
    
    This commit fixes this inconsistency but pins it to version 2
    to avoid changing existing code.

---
+++ tests/source/issue-3227/two.rs
@@ -0,0 +1,13 @@
+// rustfmt-version: Two
+
+fn main() {
+    thread::spawn(|| {
+        while true {
+            println!("iteration");
+        }
+    });
+
+    thread::spawn(|| loop {
+        println!("iteration");
+    });
+}
