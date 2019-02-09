e70e6eb273d449e78349dde9a0980414df102c06
Hirokazu Hata <h.hata.ai.t@gmail.com>
2019-02-09 15:55:17 +0900

    deny rust_2018_idioms

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![deny(rust_2018_idioms)]
+
 #[macro_use]
 extern crate derive_new;
 #[cfg(test)]
