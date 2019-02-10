83a87fb7b6378eb928d0fd3e38fbab3579dc520c
Philipp Hansch <dev@phansch.net>
2019-02-10 10:19:24 +0100

    UI test cleanup: Extract match_same_arms tests

--- tests/ui/copies.rs
+++ tests/ui/copies.rs
@@ -5,7 +5,6 @@
     clippy::eq_op,
     clippy::needless_continue,
     clippy::needless_return,
-    clippy::never_loop,
     clippy::no_effect,
     clippy::zero_divided_by_zero,
     clippy::unused_unit
@@ -16,64 +15,15 @@ fn foo() -> bool {
     unimplemented!()
 }
 
-struct Foo {
-    bar: u8,
-}
-
 pub enum Abc {
     A,
     B,
     C,
 }
 
-#[warn(clippy::if_same_then_else)]
 #[warn(clippy::match_same_arms)]
 #[allow(clippy::unused_unit)]
-fn if_same_then_else() -> Result<&'static str, ()> {
-    if true {
-        Foo { bar: 42 };
-        0..10;
-        ..;
-        0..;
-        ..10;
-        0..=10;
-        foo();
-    } else {
-        //~ ERROR same body as `if` block
-        Foo { bar: 42 };
-        0..10;
-        ..;
-        0..;
-        ..10;
-        0..=10;
-        foo();
-    }
-
-    if true {
-        Foo { bar: 42 };
-    } else {
-        Foo { bar: 43 };
-    }
-
-    if true {
-        ();
-    } else {
-        ()
-    }
-
-    if true {
-        0..10;
-    } else {
-        0..=10;
-    }
-
-    if true {
-        foo();
-        foo();
-    } else {
-        foo();
-    }
-
+fn match_same_arms() {
     let _ = match 42 {
         42 => {
             foo();
@@ -101,129 +51,6 @@ fn if_same_then_else() -> Result<&'static str, ()> {
         Abc::B => 1,
         _ => 0, //~ ERROR match arms have same body
     };
-
-    if true {
-        foo();
-    }
-
-    let _ = if true {
-        42
-    } else {
-        //~ ERROR same body as `if` block
-        42
-    };
-
-    if true {
-        for _ in &[42] {
-            let foo: &Option<_> = &Some::<u8>(42);
-            if true {
-                break;
-            } else {
-                continue;
-            }
-        }
-    } else {
-        //~ ERROR same body as `if` block
-        for _ in &[42] {
-            let foo: &Option<_> = &Some::<u8>(42);
-            if true {
-                break;
-            } else {
-                continue;
-            }
-        }
-    }
-
-    if true {
-        let bar = if true { 42 } else { 43 };
-
-        while foo() {
-            break;
-        }
-        bar + 1;
-    } else {
-        //~ ERROR same body as `if` block
-        let bar = if true { 42 } else { 43 };
-
-        while foo() {
-            break;
-        }
-        bar + 1;
-    }
-
-    if true {
-        let _ = match 42 {
-            42 => 1,
-            a if a > 0 => 2,
-            10..=15 => 3,
-            _ => 4,
-        };
-    } else if false {
-        foo();
-    } else if foo() {
-        let _ = match 42 {
-            42 => 1,
-            a if a > 0 => 2,
-            10..=15 => 3,
-            _ => 4,
-        };
-    }
-
-    if true {
-        if let Some(a) = Some(42) {}
-    } else {
-        //~ ERROR same body as `if` block
-        if let Some(a) = Some(42) {}
-    }
-
-    if true {
-        if let (1, .., 3) = (1, 2, 3) {}
-    } else {
-        //~ ERROR same body as `if` block
-        if let (1, .., 3) = (1, 2, 3) {}
-    }
-
-    if true {
-        if let (1, .., 3) = (1, 2, 3) {}
-    } else {
-        if let (.., 3) = (1, 2, 3) {}
-    }
-
-    if true {
-        if let (1, .., 3) = (1, 2, 3) {}
-    } else {
-        if let (.., 4) = (1, 2, 3) {}
-    }
-
-    if true {
-        if let (1, .., 3) = (1, 2, 3) {}
-    } else {
-        if let (.., 1, 3) = (1, 2, 3) {}
-    }
-
-    if true {
-        if let Some(42) = None {}
-    } else {
-        if let Option::Some(42) = None {}
-    }
-
-    if true {
-        if let Some(42) = None::<u8> {}
-    } else {
-        if let Some(42) = None {}
-    }
-
-    if true {
-        if let Some(42) = None::<u8> {}
-    } else {
-        if let Some(42) = None::<u32> {}
-    }
-
-    if true {
-        if let Some(a) = Some(42) {}
-    } else {
-        if let Some(a) = Some(43) {}
-    }
 
     let _ = match 42 {
         42 => foo(),
@@ -271,33 +98,6 @@ fn if_same_then_else() -> Result<&'static str, ()> {
         _ => 0,
     };
 
-    let _ = if true {
-        0.0
-    } else {
-        //~ ERROR same body as `if` block
-        0.0
-    };
-
-    let _ = if true {
-        -0.0
-    } else {
-        //~ ERROR same body as `if` block
-        -0.0
-    };
-
-    let _ = if true { 0.0 } else { -0.0 };
-
-    // Different NaNs
-    let _ = if true { 0.0 / 0.0 } else { std::f32::NAN };
-
-    // Same NaNs
-    let _ = if true {
-        std::f32::NAN
-    } else {
-        //~ ERROR same body as `if` block
-        std::f32::NAN
-    };
-
     let _ = match Some(()) {
         Some(()) => 0.0,
         None => -0.0,
@@ -308,50 +108,6 @@ fn if_same_then_else() -> Result<&'static str, ()> {
         (None, Some(a)) => bar(a), // bindings have different types
         _ => (),
     }
-
-    if true {
-        try!(Ok("foo"));
-    } else {
-        //~ ERROR same body as `if` block
-        try!(Ok("foo"));
-    }
-
-    if true {
-        let foo = "";
-        return Ok(&foo[0..]);
-    } else if false {
-        let foo = "bar";
-        return Ok(&foo[0..]);
-    } else {
-        let foo = "";
-        return Ok(&foo[0..]);
-    }
-
-    // false positive if_same_then_else, let(x,y) vs let(y,x), see #3559
-    if true {
-        let foo = "";
-        let (x, y) = (1, 2);
-        return Ok(&foo[x..y]);
-    } else {
-        let foo = "";
-        let (y, x) = (1, 2);
-        return Ok(&foo[x..y]);
-    }
 }
 
 fn main() {}
-
-// Issue #2423. This was causing an ICE
-fn func() {
-    if true {
-        f(&[0; 62]);
-        f(&[0; 4]);
-        f(&[0; 3]);
-    } else {
-        f(&[0; 62]);
-        f(&[0; 6]);
-        f(&[0; 6]);
-    }
-}
-
-fn f(val: &[u8]) {}
