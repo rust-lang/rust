76403e574231ae54dc7ccf227750af907ea13db3
gnzlbg <gonzalobg88@gmail.com>
2019-02-10 09:59:07 +0100

    Use pattern to match attributes

--- src/test/codegen/ffi-returns-twice.rs
+++ src/test/codegen/ffi-returns-twice.rs
@@ -2,14 +2,10 @@
 #![crate_type = "lib"]
 #![feature(ffi_returns_twice)]
 
-extern {
-    // CHECK-LABEL: @foo()
-    // CHECK: attributes #1 = { {{.*}}returns_twice{{.*}} }
-    #[no_mangle]
-    #[ffi_returns_twice]
-    pub fn foo();
-}
+pub fn bar() { unsafe { foo() } }
 
-pub fn bar() {
-    unsafe { foo() }
+extern {
+    #[ffi_returns_twice] pub fn foo();
 }
+// CHECK: declare void @foo(){{.*}}#1{{.*}}
+// CHECK: attributes #1 = { {{.*}}returns_twice{{.*}} }
