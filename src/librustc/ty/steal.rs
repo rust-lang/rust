82d7e33972db9dfa1f35e2374ed8165bc05cb736
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-02-13 13:13:08 +0100

    Remove `stolen`

--- src/librustc/ty/steal.rs
+++ src/librustc/ty/steal.rs
@@ -43,8 +43,4 @@ impl<T> Steal<T> {
         let value = value_ref.take();
         value.expect("attempt to read from stolen value")
     }
-
-    pub fn stolen(&self) -> bool {
-        self.value.borrow().is_none()
-    }
 }
