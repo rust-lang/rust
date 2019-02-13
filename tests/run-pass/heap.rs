8466f78e83799ddee5f9bfd19f883c627036d096
Ralf Jung <post@ralfj.de>
2019-02-13 19:47:51 +0100

    ignore overaligned tests on Windows (because, of course, Windows' API is broken here)

--- tests/run-pass/heap.rs
+++ tests/run-pass/heap.rs
@@ -1,7 +1,4 @@
 #![feature(box_syntax)]
-#![feature(allocator_api)]
-
-use std::alloc::{Global, Alloc, Layout, System};
 
 fn make_box() -> Box<(i16, i16)> {
     Box::new((1, 2))
@@ -30,31 +27,8 @@ fn allocate_reallocate() {
     assert_eq!(s.capacity(), 9);
 }
 
-fn check_overalign_requests<T: Alloc>(mut allocator: T) {
-    let size = 8;
-    let align = 16; // greater than size
-    let iterations = 1; // Miri is deterministic, no need to try many times
-    unsafe {
-        let pointers: Vec<_> = (0..iterations).map(|_| {
-            allocator.alloc(Layout::from_size_align(size, align).unwrap()).unwrap()
-        }).collect();
-        for &ptr in &pointers {
-            assert_eq!((ptr.as_ptr() as usize) % align, 0,
-                       "Got a pointer less aligned than requested")
-        }
-
-        // Clean up
-        for &ptr in &pointers {
-            allocator.dealloc(ptr, Layout::from_size_align(size, align).unwrap())
-        }
-    }
-}
-
 fn main() {
     assert_eq!(*make_box(), (1, 2));
     assert_eq!(*make_box_syntax(), (1, 2));
     allocate_reallocate();
-
-    check_overalign_requests(System);
-    check_overalign_requests(Global);
 }
