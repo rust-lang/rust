edd9e5b7b12ed1093a87ba51e25443f7e1098c4f
Ralf Jung <post@ralfj.de>
2019-02-13 17:22:06 +0100

    test BTree a bit more

--- tests/run-pass/btreemap.rs
+++ tests/run-pass/btreemap.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap, BTreeSet};
+
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub enum Foo {
     A(&'static str),
@@ -6,11 +8,22 @@ pub enum Foo {
 }
 
 pub fn main() {
-    let mut b = std::collections::BTreeSet::new();
+    let mut b = BTreeSet::new();
     b.insert(Foo::A("\'"));
     b.insert(Foo::A("/="));
     b.insert(Foo::A("#"));
     b.insert(Foo::A("0o"));
     assert!(b.remove(&Foo::A("/=")));
     assert!(!b.remove(&Foo::A("/=")));
+
+    // Also test a lower-alignment type, where the NodeHeader overlaps with
+    // the keys.
+    let mut b = BTreeSet::new();
+    b.insert(1024);
+    b.insert(7);
+
+    let mut b = BTreeMap::new();
+    b.insert("bar", 1024);
+    b.insert("baz", 7);
+    for _val in b.iter_mut() {}
 }
