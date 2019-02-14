72afe51d81dd0824a009e378e9142ca7236806aa
Simon Heath <icefox@dreamquest.io>
2019-02-14 09:19:03 -0500

    Slowly figuring out how rustdoc actually works.
    Unfortunately trying to run doctests on my local machine is
    not really faster than letting Travis do it...

--- src/libcore/convert.rs
+++ src/libcore/convert.rs
@@ -413,6 +413,9 @@ pub trait TryInto<T>: Sized {
 /// As described, [`i32`] implements `TryFrom<i64>`:
 ///
 /// ```
+/// #![feature(try_from)]
+/// use std::convert::TryFrom;
+///
 /// let big_number = 1_000_000_000_000i64;
 /// // Silently truncates `big_number`, requires detecting
 /// // and handling the truncation after the fact.
