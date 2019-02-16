bd18cc5708328600a94a02444caf27a5fb6ca20c
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-02-16 12:36:23 +0100

    Remove an intermediate value from discriminant reading

--- src/librustc_mir/interpret/operand.rs
+++ src/librustc_mir/interpret/operand.rs
@@ -508,7 +508,7 @@ impl<'a, 'mir, 'tcx, M: Machine<'a, 'mir, 'tcx>> EvalContext<'a, 'mir, 'tcx, M> 
 
     // Evaluate a place with the goal of reading from it.  This lets us sometimes
     // avoid allocations.
-    fn eval_place_to_op(
+    pub(super) fn eval_place_to_op(
         &self,
         mir_place: &mir::Place<'tcx>,
         layout: Option<TyLayout<'tcx>>,
