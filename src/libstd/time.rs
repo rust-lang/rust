8e219e7eb5ed554e0602476edfbbee5720e5c36e
Stjepan Glavina <stjepang@gmail.com>
2019-02-20 14:21:15 +0100

    Turn duration consts into associated consts

--- src/libstd/time.rs
+++ src/libstd/time.rs
@@ -23,9 +23,6 @@ use sys_common::mutex::Mutex;
 #[stable(feature = "time", since = "1.3.0")]
 pub use core::time::Duration;
 
-#[unstable(feature = "duration_constants", issue = "57391")]
-pub use core::time::{SECOND, MILLISECOND, MICROSECOND, NANOSECOND};
-
 /// A measurement of a monotonically nondecreasing clock.
 /// Opaque and useful only with `Duration`.
 ///
