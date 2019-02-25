41d4ec606b8027e1f92180c80aaa72aa5afe84bb
varkor <github@varkor.com>
2019-02-25 23:56:10 +0000

    Add a test for #22892

---
+++ src/test/ui/asm/invalid-inline-asm.rs
@@ -0,0 +1,9 @@
+#![feature(asm)]
+
+fn main() {
+    let byte = 0;
+    let port = 0x80;
+
+    unsafe { asm!("out %al, %dx" :: "a" (byte), "d" (port) :: "volatile"); }
+    //~^ ERROR couldn't allocate input reg for constraint 'a'
+}
