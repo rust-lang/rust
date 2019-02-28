39412ca9695dbf965c69f6f673c0e49313d3f1fb
Mark Rousskov <mark.simulacrum@gmail.com>
2019-02-27 17:21:31 -0700

    Permit unwinding through FFI by default
    See #58794 for context.

--- src/librustc_mir/build/mod.rs
+++ src/librustc_mir/build/mod.rs
@@ -502,7 +502,7 @@ fn should_abort_on_panic(tcx: TyCtxt<'_>, fn_def_id: DefId, abi: Abi) -> bool {
     // This is a special case: some functions have a C abi but are meant to
     // unwind anyway. Don't stop them.
     match unwind_attr {
-        None => true,
+        None => false, // FIXME(#58794)
         Some(UnwindAttr::Allowed) => false,
         Some(UnwindAttr::Aborts) => true,
     }
