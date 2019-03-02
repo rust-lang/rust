a5c464aea4d044c9e0f362bc4f3afd4fedcee822
Esteban Küber <esteban@kuber.com.ar>
2019-03-01 22:35:21 -0800

    Reduce test case

--- src/test/ui/issues/issue-58856-1.rs
+++ src/test/ui/issues/issue-58856-1.rs
@@ -1,12 +1,6 @@
-struct A;
-
 impl A {
-    fn b(self> {}
+    fn b(self>
     //~^ ERROR expected one of `)`, `,`, or `:`, found `>`
 }
-
-// verify that mismatched delimiters get emitted
-fn foo(] {}
-//~^ ERROR incorrect close delimiter
 
 fn main() {}
