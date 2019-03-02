72f0ca51ef3d946a0a43eadad7b938001186572b
Alexander Regueiro <alex@noldorin.com>
2019-03-02 15:16:36 +0000

    Nit

--- src/test/pretty/fn-variadic.rs
+++ src/test/pretty/fn-variadic.rs
@@ -1,6 +1,6 @@
 // Check that `fn foo(x: i32, ...)` does not print as `fn foo(x: i32, ..., ...)`.
 // See issue #58853.
-//
+
 // pp-exact
 #![feature(c_variadic)]
 
@@ -12,4 +12,4 @@ pub unsafe extern "C" fn bar(_: i32, mut ap: ...) -> usize {
     ap.arg::<usize>()
 }
 
-fn main() { }
+fn main() {}
