fc379ee9d146b682850187b08d45a34a33981ca4
Linus Färnstrand <faern@faern.net>
2019-03-03 14:05:14 +0100

    Move at_exit over to RawMutex

--- src/libstd/sys_common/at_exit_imp.rs
+++ src/libstd/sys_common/at_exit_imp.rs
@@ -4,7 +4,7 @@
 
 use crate::ptr;
 use crate::mem;
-use crate::sys_common::mutex::Mutex;
+use crate::sync::RawMutex;
 
 type Queue = Vec<Box<dyn FnOnce()>>;
 
@@ -12,9 +12,7 @@ type Queue = Vec<Box<dyn FnOnce()>>;
 // on poisoning and this module needs to operate at a lower level than requiring
 // the thread infrastructure to be in place (useful on the borders of
 // initialization/destruction).
-// We never call `LOCK.init()`, so it is UB to attempt to
-// acquire this mutex reentrantly!
-static LOCK: Mutex = Mutex::new();
+static LOCK: RawMutex = RawMutex::new();
 static mut QUEUE: *mut Queue = ptr::null_mut();
 
 const DONE: *mut Queue = 1_usize as *mut _;
