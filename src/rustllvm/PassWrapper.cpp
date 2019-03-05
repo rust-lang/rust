2d0e47e28f1ca84e190f319e5bcdd4b7ba5d2b7a
Scott Mabin <MabezDev@gmail.com>
2019-03-05 15:20:30 +0000

    The xtensa triples are recognized by the compiler. Need to implement the calling convention, target -> call -> abi

--- src/rustllvm/PassWrapper.cpp
+++ src/rustllvm/PassWrapper.cpp
@@ -251,6 +251,14 @@ void LLVMRustAddLastExtensionPasses(
 #define SUBTARGET_HEXAGON
 #endif
 
+#ifdef LLVM_COMPONENT_XTENSA
+#define SUBTARGET_XTENSA SUBTARGET(XTENSA)
+#else
+#define SUBTARGET_XTENSA
+#endif
+
+
+
 #define GEN_SUBTARGETS                                                         \
   SUBTARGET_X86                                                                \
   SUBTARGET_ARM                                                                \
@@ -262,6 +270,7 @@ void LLVMRustAddLastExtensionPasses(
   SUBTARGET_SPARC                                                              \
   SUBTARGET_HEXAGON                                                            \
   SUBTARGET_RISCV                                                              \
+  SUBTARGET_XTENSA                                                              \
 
 #define SUBTARGET(x)                                                           \
   namespace llvm {                                                             \
