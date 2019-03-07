6caaf3715a556998d9f123b4a9b1aa51d75c41ed
Pietro Albini <pietro@pietroalbini.org>
2019-03-07 16:35:30 +0100

    some more tests

--- src/ci/run.sh
+++ src/ci/run.sh
@@ -3,15 +3,28 @@
 set -e
 
 if [[ "${INFINITE_SLEEP}" -eq 1 ]]; then
-    echo "Sleeping for 3.5 hours y'all"
+    if ! [[ "${INSTALL_STEP}" -eq 1 ]]; then
+        echo "Sleeping for 200 minutes y'all"
+        i=0
+        while [[ "$i" -le 200 ]]; do
+            echo "$i minutes passed"
+            i=$((i+1))
+            sleep 60
+        done
+        echo "Awoke!"
+    else
+        echo "skipping timeout on the install phase"
+    fi
+elif [[ "${INFINITE_SLEEP}" -eq 2 ]]; then
+    echo "Sleeping for 100 minutes y'all"
     i=0
-    while [[ "$i" -le 210 ]]; do
+    while [[ "$i" -le 100 ]]; do
         echo "$i minutes passed"
         i=$((i+1))
         sleep 60
     done
     echo "Awoke!"
-elif [[ "${INFINITE_SLEEP}" -eq 2 ]]; then
+elif [[ "${INFINITE_SLEEP}" -eq 3 ]]; then
     while true; do
         echo "foo"
         sleep 60
