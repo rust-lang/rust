d2f33b8a88e525d4832e47353396cf0c91dff4c0
Pietro Albini <pietro@pietroalbini.org>
2019-03-07 08:15:47 +0100

    don't do the infinite loop on travis

--- src/ci/run.sh
+++ src/ci/run.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-bash -c "while true; do echo foo; sleep 60; done" &
-
 if [[ "${INFINITE_SLEEP}" -eq 1 ]]; then
+    bash -c "while true; do echo foo; sleep 60; done" &
+
     echo "Sleeping for 3.5 hours y'all"
     sleep 12600
     echo "Awoke!"
 elif [[ "${INFINITE_SLEEP}" -eq 2 ]]; then
+    bash -c "while true; do echo foo; sleep 60; done" &
+
     while true; do
         sleep 60000000
     done
