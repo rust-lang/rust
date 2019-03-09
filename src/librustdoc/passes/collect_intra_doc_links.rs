a4ea08420cde12ea0943cafb1505e512c5820f70
Manish Goregaokar <manishsmail@gmail.com>
2019-03-08 21:12:12 -0800

    Avoid some common false positives in intra doc link checking

--- src/librustdoc/passes/collect_intra_doc_links.rs
+++ src/librustdoc/passes/collect_intra_doc_links.rs
@@ -291,6 +291,12 @@ impl<'a, 'tcx, 'rcx> DocFolder for LinkCollector<'a, 'tcx, 'rcx> {
             if ori_link.contains('/') {
                 continue;
             }
+
+            // [] is mostly likely not supposed to be a link
+            if ori_link.is_empty() {
+                continue;
+            }
+
             let link = ori_link.replace("`", "");
             let (def, fragment) = {
                 let mut kind = PathKind::Unknown;
