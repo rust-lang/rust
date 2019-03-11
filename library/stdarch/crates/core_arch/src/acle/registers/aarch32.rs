5450e5248fca69c8dd9b54c6e4e5ad81e117c2ca
Lokathor <zefria@gmail.com>
2019-03-11 01:25:35 -0600

    add the double quotes

--- library/stdarch/crates/core_arch/src/acle/registers/aarch32.rs
+++ library/stdarch/crates/core_arch/src/acle/registers/aarch32.rs
@@ -1,5 +1,5 @@
 /// Application Program Status Register
 pub struct APSR;
 
-#[cfg(any(not(target_feature = thumb-mode), target_feature = v6t2))]
+#[cfg(any(not(target_feature = thumb-mode), target_feature = "v6t2"))]
 rsr!(APSR);
