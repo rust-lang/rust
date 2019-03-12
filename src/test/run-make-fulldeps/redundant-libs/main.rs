32969e9f57a7fc472addd40eb0d0fb7e7b00888f
Douglas Creager <dcreager@dcreager.net>
2019-03-12 16:01:13 -0400

    Move comment describing test case

--- src/test/run-make-fulldeps/redundant-libs/main.rs
+++ src/test/run-make-fulldeps/redundant-libs/main.rs
@@ -8,15 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// rustc will remove one of the two redundant references to foo below.  Depending on which one gets
-// removed, we'll get a linker error on SOME platforms (like Linux).  On these platforms, when a
-// library is referenced, the linker will only pull in the symbols needed _at that point in time_.
-// If a later library depends on additional symbols from the library, they will not have been
-// pulled in, and you'll get undefined symbols errors.
-//
-// So in this example, we need to ensure that rustc keeps the _later_ reference to foo, and not the
-// former one.
-
 extern "C" {
     fn bar();
     fn baz();
