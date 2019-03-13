81b876b6a3e3e489fdad8514f69b67264ab1d338
Esteban Küber <esteban@kuber.com.ar>
2019-03-13 16:47:55 -0700

    Hide "type ascription is experimental error" unless it's the only one
    In order to minimize the verbosity of common syntax errors that are parsed
    as type ascription, hide the feature gate error unless there are no other
    errors being emitted by the parser.

--- src/test/ui/suggestions/type-ascription-instead-of-let.rs
+++ src/test/ui/suggestions/type-ascription-instead-of-let.rs
@@ -4,7 +4,6 @@ fn main() {
     let closure_annotated = |value: i32| -> i32 {
         temp: i32 = fun(5i32);
         //~^ ERROR cannot find value `temp` in this scope
-        //~| ERROR type ascription is experimental
         temp + value + 1
         //~^ ERROR cannot find value `temp` in this scope
     };
--- src/test/ui/suggestions/type-ascription-instead-of-path.rs
+++ src/test/ui/suggestions/type-ascription-instead-of-path.rs
@@ -2,5 +2,4 @@ fn main() {
     std:io::stdin();
     //~^ ERROR failed to resolve: use of undeclared type or module `io`
     //~| ERROR expected value, found module
-    //~| ERROR type ascription is experimental
 }
