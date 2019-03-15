fab71dd0469840c349c236d8c3caa89fbbfb8fc5
Anthony Ramine <n.oxyde@gmail.com>
2019-03-15 12:03:59 +0100

    Revert "Deprecate std::cell::RefCell::borrow_state"
    This reverts commit dc2d5058e999abb18ab2686b4e3e385ec6e36666.

--- src/libcore/cell.rs
+++ src/libcore/cell.rs
@@ -572,8 +572,6 @@ pub struct RefCell<T: ?Sized> {
 /// An enumeration of values returned from the `state` method on a `RefCell<T>`.
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 #[unstable(feature = "borrow_state", issue = "27733")]
-#[rustc_deprecated(since = "1.15.0", reason = "use `try_borrow` instead")]
-#[allow(deprecated)]
 pub enum BorrowState {
     /// The cell is currently being read, there is at least one active `borrow`.
     Reading,
@@ -788,8 +786,6 @@ impl<T: ?Sized> RefCell<T> {
     /// }
     /// ```
     #[unstable(feature = "borrow_state", issue = "27733")]
-    #[rustc_deprecated(since = "1.15.0", reason = "use `try_borrow` instead")]
-    #[allow(deprecated)]
     #[inline]
     pub fn borrow_state(&self) -> BorrowState {
         let borrow = self.borrow.get();
