664391c5f862c905a0753f7fe6de588981f10482
Mateusz Mikuła <matti@marinelayer.io>
2019-03-18 12:30:22 +0100

    Drop range_contains feature

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -5,7 +5,6 @@
 #![feature(rustc_private)]
 #![feature(slice_patterns)]
 #![feature(stmt_expr_attributes)]
-#![feature(range_contains)]
 #![allow(clippy::missing_docs_in_private_items)]
 #![recursion_limit = "256"]
 #![warn(rust_2018_idioms, trivial_casts, trivial_numeric_casts)]
