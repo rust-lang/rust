853ae8d931c3fe4cd303edf7d80271c1930b9654
Ralf Jung <post@ralfj.de>
2019-03-19 09:46:11 +0100

    fix some uses I missed

--- src/test/codegen/box-maybe-uninit.rs
+++ src/test/codegen/box-maybe-uninit.rs
@@ -12,5 +12,8 @@ pub fn box_uninitialized() -> Box<MaybeUninit<usize>> {
     // CHECK-NOT: alloca
     // CHECK-NOT: memcpy
     // CHECK-NOT: memset
-    Box::new(MaybeUninit::uninitialized())
+    Box::new(MaybeUninit::uninit())
 }
+
+// FIXME: add a test for a bigger box. Currently broken, see
+// https://github.com/rust-lang/rust/issues/58201.
