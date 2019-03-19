f8bffad4f1f0650ae56e17221d9443f9c2a6ab20
kennytm <kennytm@gmail.com>
2019-03-20 04:30:49 +0800

    Use `curl` instead of `wget` in install-x86_64-redox.sh

--- src/ci/docker/dist-various-1/install-x86_64-redox.sh
+++ src/ci/docker/dist-various-1/install-x86_64-redox.sh
@@ -3,5 +3,5 @@
 
 set -ex
 
-wget -O - https://static.redox-os.org/toolchain/x86_64-unknown-redox/relibc-install.tar.gz | \
+curl https://static.redox-os.org/toolchain/x86_64-unknown-redox/relibc-install.tar.gz | \
 tar --extract --gzip --directory /usr/local
