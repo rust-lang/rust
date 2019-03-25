58a04f06cb6522040af4d9352c3a25dd66547acc
varkor <github@varkor.com>
2019-03-25 16:48:57 +0000

    Propitiate tidy

--- src/librustc_typeck/check/mod.rs
+++ src/librustc_typeck/check/mod.rs
@@ -3229,7 +3229,8 @@ impl<'a, 'gcx, 'tcx> FnCtxt<'a, 'gcx, 'tcx> {
             Some(ret) => ret,
             None => return Vec::new()
         };
-        let expect_args = self.fudge_inference_if_ok(&RegionVariableOrigin::Coercion(call_span), || {
+        let origin = RegionVariableOrigin::Coercion(call_span);
+        let expect_args = self.fudge_inference_if_ok(&origin, || {
             // Attempt to apply a subtyping relationship between the formal
             // return type (likely containing type variables if the function
             // is polymorphic) and the expected return type.
