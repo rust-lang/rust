97f200533058ee40b1b4ba01a6996a994ce8c171
gnzlbg <gonzalobg88@gmail.com>
2019-03-26 17:13:03 +0100

    Add unstable cfg_if! macro to libcore

---
+++ src/libcore/tests/cfg_if.rs
@@ -0,0 +1,52 @@
+#![feature(cfg_if)]
+
+cfg_if! {
+    if #[cfg(test)] {
+        use core::option::Option as Option2;
+        fn works1() -> Option2<u32> { Some(1) }
+    } else {
+        fn works1() -> Option<u32> { None }
+    }
+}
+
+cfg_if! {
+    if #[cfg(foo)] {
+        fn works2() -> bool { false }
+    } else if #[cfg(test)] {
+        fn works2() -> bool { true }
+    } else {
+        fn works2() -> bool { false }
+    }
+}
+
+cfg_if! {
+    if #[cfg(foo)] {
+        fn works3() -> bool { false }
+    } else {
+        fn works3() -> bool { true }
+    }
+}
+
+cfg_if! {
+    if #[cfg(test)] {
+        use core::option::Option as Option3;
+        fn works4() -> Option3<u32> { Some(1) }
+    }
+}
+
+cfg_if! {
+    if #[cfg(foo)] {
+        fn works5() -> bool { false }
+    } else if #[cfg(test)] {
+        fn works5() -> bool { true }
+    }
+}
+
+#[test]
+fn it_works() {
+    assert!(works1().is_some());
+    assert!(works2());
+    assert!(works3());
+    assert!(works4().is_some());
+    assert!(works5());
+}
