f35f2dfe80ea1350376d2f527fefd1bf538a5b90
gnzlbg <gonzalobg88@gmail.com>
2019-03-26 19:47:12 +0100

    Enable cfg_if feature for libstd

--- src/libstd/lib.rs
+++ src/libstd/lib.rs
@@ -241,6 +241,7 @@
 #![feature(bind_by_move_pattern_guards)]
 #![feature(box_syntax)]
 #![feature(c_variadic)]
+#![feature(cfg_if)]
 #![feature(cfg_target_has_atomic)]
 #![feature(cfg_target_thread_local)]
 #![feature(char_error_internals)]
