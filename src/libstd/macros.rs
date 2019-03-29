9240092fe3bf32803ac9f9a247a098844d6f4780
Christian <chris_veenman@hotmail.com>
2019-03-29 16:18:24 +0100

    Adjusted the indentation.

--- src/libstd/macros.rs
+++ src/libstd/macros.rs
@@ -231,8 +231,9 @@ macro_rules! eprintln {
 /// to give up ownership, you can instead borrow with `dbg!(&expr)`
 /// for some expression `expr`.
 ///
-/// The `dbg!` macro works exactly the same in release builds. This is useful when debugging issues
-/// that only occur in release builds or when debugging in release mode is significantly faster.
+/// The `dbg!` macro works exactly the same in release builds.
+/// This is useful when debugging issues that only occur in release builds or when debugging in
+/// release mode is significantly faster.
 ///
 /// Note that the macro is intended as a debugging tool and therefore you
 /// should avoid having uses of it in version control for longer periods.
