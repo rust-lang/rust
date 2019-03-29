1bea823069d5a56e560500e25f2f2d0a0cd23ece
bjorn3 <bjorn3@users.noreply.github.com>
2019-03-29 19:25:14 +0100

    [DEBUG] Add objdump invocation

--- test.sh
+++ test.sh
@@ -7,6 +7,8 @@ mkdir -p target/out/clif
 echo "[BUILD] mini_core"
 $RUSTC example/mini_core.rs --crate-name mini_core --crate-type dylib
 
+objdump -section-headers target/out/libmini_core.dylib
+
 echo "[BUILD] example"
 $RUSTC example/example.rs --crate-type lib
 
