6f4df8c0c20969a1059a4b0cdf63e4ac33447e1b
Mazdak Farrokhzad <twingoow@gmail.com>
2019-03-30 10:30:57 +0100

    libstd: deny(elided_lifetimes_in_paths), fixes in cloudabi

--- src/libstd/sys/cloudabi/backtrace.rs
+++ src/libstd/sys/cloudabi/backtrace.rs
@@ -33,8 +33,9 @@ impl fmt::Display for UnwindError {
                  // tracing
 pub fn unwind_backtrace(frames: &mut [Frame]) -> io::Result<(usize, BacktraceContext)> {
     let mut cx = Context { idx: 0, frames };
-    let result_unwind =
-        unsafe { uw::_Unwind_Backtrace(trace_fn, &mut cx as *mut Context as *mut libc::c_void) };
+    let result_unwind = unsafe {
+        uw::_Unwind_Backtrace(trace_fn, &mut cx as *mut Context<'_> as *mut libc::c_void)
+    };
     // See libunwind:src/unwind/Backtrace.c for the return values.
     // No, there is no doc.
     match result_unwind {
