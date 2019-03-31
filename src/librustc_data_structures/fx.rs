b4092e811aab56f192bfbd2c2858228cf061dcab
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-03-31 21:30:41 +0200

    Test ahash

--- src/librustc_data_structures/fx.rs
+++ src/librustc_data_structures/fx.rs
@@ -1,1 +1,10 @@
-pub use rustc_hash::{FxHasher, FxHashMap, FxHashSet};
+use std::hash::BuildHasherDefault;
+use std::collections::hash_map::HashMap;
+use std::collections::hash_set::HashSet;
+pub use ahash::AHasher as FxHasher;
+
+/// Type alias for a hashmap using the `fx` hash algorithm.
+pub type FxHashMap<K, V> = HashMap<K, V, BuildHasherDefault<FxHasher>>;
+
+/// Type alias for a hashmap using the `fx` hash algorithm.
+pub type FxHashSet<V> = HashSet<V, BuildHasherDefault<FxHasher>>;
