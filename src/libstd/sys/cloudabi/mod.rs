e0fd309d0a5ee2fa9ee73a69a217d13c80303a58
Linus Färnstrand <faern@faern.net>
2019-04-01 11:43:09 +0200

    Make cloudabi::abi module public inside libstd

--- src/libstd/sys/cloudabi/mod.rs
+++ src/libstd/sys/cloudabi/mod.rs
@@ -1,6 +1,7 @@
 use crate::io::ErrorKind;
 use crate::mem;
 
+pub mod abi;
 #[path = "../unix/alloc.rs"]
 pub mod alloc;
 pub mod args;
@@ -20,8 +21,6 @@ pub mod thread_local;
 pub mod time;
 
 pub use crate::sys_common::os_str_bytes as os_str;
-
-mod abi;
 
 mod shims;
 pub use self::shims::*;
