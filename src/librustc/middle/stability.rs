ee0a6b37382cd339fe4bb5b8d9e9d5795573e649
gnzlbg <gonzalobg88@gmail.com>
2019-04-02 09:48:14 +0200

    Display name of crate requiring rustc_private

--- src/librustc/middle/stability.rs
+++ src/librustc/middle/stability.rs
@@ -429,12 +429,15 @@ impl<'a, 'tcx> Index<'tcx> {
             // while maintaining the invariant that all sysroot crates are unstable
             // by default and are unable to be used.
             if tcx.sess.opts.debugging_opts.force_unstable_if_unmarked {
-                let reason = "this crate is being loaded from the sysroot, an \
-                              unstable location; did you mean to load this crate \
-                              from crates.io via `Cargo.toml` instead?";
+                let reason = format!(
+                    "crate \"{}\" is being loaded from the sysroot, an \
+                     unstable location; did you mean to load this crate \
+                     from crates.io via `Cargo.toml` instead?",
+                     tcx.crate_name(LOCAL_CRATE)
+                );
                 let stability = tcx.intern_stability(Stability {
                     level: attr::StabilityLevel::Unstable {
-                        reason: Some(Symbol::intern(reason)),
+                        reason: Some(Symbol::intern(&reason)),
                         issue: 27812,
                     },
                     feature: Symbol::intern("rustc_private"),
