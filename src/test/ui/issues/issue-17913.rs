3a39b431e87807bea03d17df98ddb71f82bfc6d5
Ralf Jung <post@ralfj.de>
2019-04-09 22:08:57 +0200

    normalize away spurious error

--- src/test/ui/issues/issue-17913.rs
+++ src/test/ui/issues/issue-17913.rs
@@ -1,6 +1,10 @@
 // normalize-stderr-test "\[&usize; \d+\]" -> "[&usize; N]"
 // error-pattern: too big for the current architecture
 
+// FIXME https://github.com/rust-lang/rust/issues/59774
+// normalize-stderr-test "thread.*panicked.*Metadata module not compiled.*\n" -> ""
+// normalize-stderr-test "note:.*RUST_BACKTRACE=1.*\n" -> ""
+
 #![feature(box_syntax)]
 
 #[cfg(target_pointer_width = "64")]
