60c49bcb5520ed679287bb99437884cb6f946115
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-04-09 10:03:02 +0200

    Remove some dummy dependencies

--- src/librustc/lib.rs
+++ src/librustc/lib.rs
@@ -89,10 +89,8 @@ extern crate serialize as rustc_serialize;
 
 #[macro_use] extern crate smallvec;
 
-// Note that librustc doesn't actually depend on these crates, see the note in
-// `Cargo.toml` for this crate about why these are here.
-#[allow(unused_extern_crates)]
-extern crate flate2;
+// Use the test crate here so we depend on getopts through it. This allow tools to link to both
+// librustc_driver and libtest.
 #[allow(unused_extern_crates)]
 extern crate test;
 
