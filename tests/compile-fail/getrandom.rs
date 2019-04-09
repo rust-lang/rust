5530d295ad2c4900c3c8ab80abb5b5258b99446b
Aaron Hill <aa1ronham@gmail.com>
2019-04-08 21:36:35 -0400

    Simplify cast using as_mut_ptr()

--- tests/compile-fail/getrandom.rs
+++ tests/compile-fail/getrandom.rs
@@ -4,7 +4,7 @@ extern crate libc;
 fn main() {
     let mut buf = [0u8; 5];
     unsafe {
-        libc::syscall(libc::SYS_getrandom, &mut buf as &mut [u8] as *mut [u8] as *mut u8 as *mut libc::c_void, 5, 0);
+        libc::syscall(libc::SYS_getrandom, buf.as_mut_ptr() as *mut libc::c_void, 5, 0);
         //~^ ERROR constant evaluation error: miri does not support random number generators in deterministic mode!
     }
 }
