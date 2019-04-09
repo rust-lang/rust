3a35b7e6c711a700efa591b0ef4a20ac864913e7
Ralf Jung <post@ralfj.de>
2019-04-09 22:08:57 +0200

    normalize away spurious error

--- src/test/ui/linkage2.rs
+++ src/test/ui/linkage2.rs
@@ -1,3 +1,7 @@
+// FIXME https://github.com/rust-lang/rust/issues/59774
+// normalize-stderr-test "thread.*panicked.*Metadata module not compiled.*\n" -> ""
+// normalize-stderr-test "note:.*RUST_BACKTRACE=1.*\n" -> ""
+
 #![feature(linkage)]
 
 extern {
