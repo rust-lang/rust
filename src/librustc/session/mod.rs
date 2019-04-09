9017d46901700a4b877f47b2c715841557b388a1
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-04-09 10:19:53 +0200

    Make trait_methods_not_found use a lock

--- src/librustc/session/mod.rs
+++ src/librustc/session/mod.rs
@@ -166,7 +166,7 @@ pub struct Session {
     pub driver_lint_caps: FxHashMap<lint::LintId, lint::Level>,
 
     /// `Span`s of trait methods that weren't found to avoid emitting object safety errors
-    pub trait_methods_not_found: OneThread<RefCell<FxHashSet<Span>>>,
+    pub trait_methods_not_found: Lock<FxHashSet<Span>>,
 }
 
 pub struct PerfStats {
@@ -1236,7 +1236,7 @@ fn build_session_(
         has_global_allocator: Once::new(),
         has_panic_handler: Once::new(),
         driver_lint_caps,
-        trait_methods_not_found: OneThread::new(RefCell::new(Default::default())),
+        trait_methods_not_found: Lock::new(Default::default()),
     };
 
     validate_commandline_args_with_session_available(&sess);
