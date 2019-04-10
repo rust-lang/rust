4af7cf37d46d614e72e791c13153ef9a706716e9
Kampfkarren <boynedmaster@gmail.com>
2019-04-10 14:39:51 -0700

    Fix tests, I think

--- src/test/ui/try-block/try-block-catch.rs
+++ src/test/ui/try-block/try-block-catch.rs
@@ -5,5 +5,6 @@
 fn main() {
     let res: Option<bool> = try {
         true
-    } catch { }; //~ ERROR `try {} catch` is not a valid syntax
+    } catch { };
+    //~^ ERROR `try {} catch` is not a valid syntax
 }
