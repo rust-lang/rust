581c1ab730f36bfd5ed0d45862de768cb88b70ac
Ralf Jung <post@ralfj.de>
2019-04-15 23:09:14 +0200

    normalize away some Windows-only panic printing

--- src/test/ui/proc-macro/invalid-punct-ident-3.rs
+++ src/test/ui/proc-macro/invalid-punct-ident-3.rs
@@ -1,5 +1,9 @@
 // aux-build:invalid-punct-ident.rs
 
+// FIXME https://github.com/rust-lang/rust/issues/59998
+// normalize-stderr-windows "thread.*panicked.*proc_macro_server.rs.*\n" -> ""
+// normalize-stderr-windows "note:.*RUST_BACKTRACE=1.*\n" -> ""
+
 #[macro_use]
 extern crate invalid_punct_ident;
 
