28c4397b28277cbbfb8f41aa007b9cac0e6b826f
Ralf Jung <post@ralfj.de>
2019-04-16 10:55:46 +0200

    this panic occurs not just on Windows, normalize it away everywhere

--- src/test/ui/proc-macro/invalid-punct-ident-3.rs
+++ src/test/ui/proc-macro/invalid-punct-ident-3.rs
@@ -1,8 +1,8 @@
 // aux-build:invalid-punct-ident.rs
 
 // FIXME https://github.com/rust-lang/rust/issues/59998
-// normalize-stderr-windows "thread.*panicked.*proc_macro_server.rs.*\n" -> ""
-// normalize-stderr-windows "note:.*RUST_BACKTRACE=1.*\n" -> ""
+// normalize-stderr-test "thread.*panicked.*proc_macro_server.rs.*\n" -> ""
+// normalize-stderr-test "note:.*RUST_BACKTRACE=1.*\n" -> ""
 
 #[macro_use]
 extern crate invalid_punct_ident;
