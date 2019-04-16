af2b42de0a43c0bf656e290e73a431acd033e79d
Ralf Jung <post@ralfj.de>
2019-04-16 19:09:37 +0200

    fix compile-fail test

--- tests/compile-fail/ptr_eq_integer.rs
+++ tests/compile-fail/ptr_eq_integer.rs
@@ -1,8 +1,6 @@
-use std::mem;
-
 fn main() {
     let b = Box::new(0);
     let x = &*b as *const i32;
     // We cannot compare this with a non-NULL integer. After all, these *could* be equal (with the right base address).
-    assert!(x != mem::align_of::<i32>() as *const i32); //~ ERROR invalid arithmetic on pointers
+    assert!(x != 64 as *const i32); //~ ERROR invalid arithmetic on pointers
 }
