c9a2616e44c4bced021f3599fe14c2177f27cadd
David Wood <david@davidtw.co>
2019-04-21 18:49:02 +0100

    Add existing behaviour test for deref suggestions.
    This commit adds a test that demonstrates the current behaviour where
    suggestions to add a dereference aren't given for non-references.

---
+++ src/test/ui/suggestions/issue-59819.rs
@@ -0,0 +1,20 @@
+#![allow(warnings)]
+
+// Test that suggestion to add `*` characters applies to implementations of `Deref` as well as
+// references.
+
+struct Foo(i32);
+
+impl std::ops::Deref for Foo {
+    type Target = i32;
+    fn deref(&self) -> &i32 {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = Foo(42);
+    let y: i32 = x; //~ ERROR mismatched types
+    let a = &42;
+    let b: i32 = a; //~ ERROR mismatched types
+}
