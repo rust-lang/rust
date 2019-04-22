7ab1bfd692c2679278c5dc978b04dfb16dfae470
David Wood <david@davidtw.co>
2019-04-22 10:16:47 +0100

    Only make suggestion when type is `Copy`.
    This commit makes the suggestion to dereference when a type implements
    `Deref` only apply if the dereference would succeed (ie. the type is
    `Copy`, otherwise a borrow check error would occur).

--- src/test/ui/suggestions/issue-59819.rs
+++ src/test/ui/suggestions/issue-59819.rs
@@ -7,9 +7,18 @@
 
 struct Foo(i32);
 
+struct Bar(String);
+
 impl std::ops::Deref for Foo {
     type Target = i32;
-    fn deref(&self) -> &i32 {
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::Deref for Bar {
+    type Target = String;
+    fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
@@ -19,4 +28,8 @@ fn main() {
     let y: i32 = x; //~ ERROR mismatched types
     let a = &42;
     let b: i32 = a; //~ ERROR mismatched types
+
+    // Do not make a suggestion when adding a `*` wouldn't actually fix the issue:
+    let f = Bar("bar".to_string());
+    let g: String = f; //~ ERROR mismatched types
 }
