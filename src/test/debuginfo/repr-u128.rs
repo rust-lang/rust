e21077c1512395cdaa98f2815abbd34aeb7ce524
Yuki OKUSHI <huyuumi.dev@gmail.com>
2019-04-25 07:20:19 +0900

    WIP: fix debuginfo test

--- src/test/debuginfo/repr-u128.rs
+++ src/test/debuginfo/repr-u128.rs
@@ -2,12 +2,19 @@
 //min-system-llvm-version 8.0
 
 //compile-flags: -g -C no-prepopulate-passes
+
+// === GDB TESTS ===================================================================================
+
+// gdb-command: run
+
+
 #![feature(repr128)]
 
 #[repr(u128)]
 pub enum Foo {
     Lo,
     Hi = 1 << 64,
+    Bar = 18_446_745_000_000_000_123,
 }
 
 fn main() {
