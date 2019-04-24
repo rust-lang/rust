0fbe49d8a6ce6ca54780c59f1d547311a26fbedc
Philipp Hansch <dev@phansch.net>
2019-04-24 08:01:16 +0200

    UI test cleanup: Extract needless_range_loop tests

--- tests/ui/for_loop.rs
+++ tests/ui/for_loop.rs
@@ -35,70 +35,7 @@ impl Unrelated {
 #[allow(clippy::many_single_char_names, unused_variables, clippy::into_iter_on_array)]
 fn main() {
     const MAX_LEN: usize = 42;
-
     let mut vec = vec![1, 2, 3, 4];
-    let vec2 = vec![1, 2, 3, 4];
-    for i in 0..vec.len() {
-        println!("{}", vec[i]);
-    }
-
-    for i in 0..vec.len() {
-        let i = 42; // make a different `i`
-        println!("{}", vec[i]); // ok, not the `i` of the for-loop
-    }
-
-    for i in 0..vec.len() {
-        let _ = vec[i];
-    }
-
-    // ICE #746
-    for j in 0..4 {
-        println!("{:?}", STATIC[j]);
-    }
-
-    for j in 0..4 {
-        println!("{:?}", CONST[j]);
-    }
-
-    for i in 0..vec.len() {
-        println!("{} {}", vec[i], i);
-    }
-    for i in 0..vec.len() {
-        // not an error, indexing more than one variable
-        println!("{} {}", vec[i], vec2[i]);
-    }
-
-    for i in 0..vec.len() {
-        println!("{}", vec2[i]);
-    }
-
-    for i in 5..vec.len() {
-        println!("{}", vec[i]);
-    }
-
-    for i in 0..MAX_LEN {
-        println!("{}", vec[i]);
-    }
-
-    for i in 0..=MAX_LEN {
-        println!("{}", vec[i]);
-    }
-
-    for i in 5..10 {
-        println!("{}", vec[i]);
-    }
-
-    for i in 5..=10 {
-        println!("{}", vec[i]);
-    }
-
-    for i in 5..vec.len() {
-        println!("{} {}", vec[i], i);
-    }
-
-    for i in 5..10 {
-        println!("{} {}", vec[i], i);
-    }
 
     for i in 10..0 {
         println!("{}", i);
