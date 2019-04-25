6d7c7940b581e8a7f311327e9b48203ce97b8137
varkor <github@varkor.com>
2019-04-25 23:29:58 +0100

    Add comment explaining restriction

--- src/test/ui/const-generics/const-param-type-depends-on-type-param.rs
+++ src/test/ui/const-generics/const-param-type-depends-on-type-param.rs
@@ -1,7 +1,13 @@
 #![feature(const_generics)]
 //~^ WARN the feature `const_generics` is incomplete and may cause the compiler to crash
 
-pub struct Dependent<T, const X: T>([(); X]); //~ ERROR const parameters
-//~^ ERROR parameter `T` is never used
+// Currently, const parameters cannot depend on type parameters, because there is no way to
+// enforce the `structural_match` property on an arbitrary type parameter. This restriction
+// may be relaxed in the future. See https://github.com/rust-lang/rfcs/pull/2000 for more
+// details.
+
+pub struct Dependent<T, const X: T>([(); X]);
+//~^ ERROR const parameters cannot depend on type parameters
+//~^^ ERROR parameter `T` is never used
 
 fn main() {}
