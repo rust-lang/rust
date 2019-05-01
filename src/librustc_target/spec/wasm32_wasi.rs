1fe3ce1c42b8125f4f93dd3c3a5d5eed8c0cb651
Dan Gohman <sunfish@mozilla.com>
2019-04-30 19:37:25 -0700

    Omit the vendor component in the WASI triple
    This renames wasm32-unknown-wasi to wasm32-wasi, omitting the vendor
    component. This follows aarch64-linux-android, x86_64-fuchsia, and others in
    omitting the vendor field, which has the advantage of aligning with the
    [multiarch tuple](https://wiki.debian.org/Multiarch/Tuples), and of being
    less noisy.

--- src/librustc_target/spec/wasm32_wasi.rs
+++ src/librustc_target/spec/wasm32_wasi.rs
@@ -1,4 +1,4 @@
-//! The `wasm32-unknown-wasi` target is a new and still (as of March 2019)
+//! The `wasm32-wasi` target is a new and still (as of April 2019) an
 //! experimental target. The definition in this file is likely to be tweaked
 //! over time and shouldn't be relied on too much.
 //!
@@ -13,14 +13,14 @@
 //! serve two use cases here with this target:
 //!
 //! * First, we want Rust usage of the target to be as hassle-free as possible,
-//!   ideally avoiding the need to configure and install a local
-//!   wasm32-unknown-wasi toolchain.
+//!   ideally avoiding the need to configure and install a local wasm32-wasi
+//!   toolchain.
 //!
 //! * Second, one of the primary use cases of LLVM's new wasm backend and the
 //!   wasm support in LLD is that any compiled language can interoperate with
-//!   any other. To that the `wasm32-unknown-wasi` target is the first with a
-//!   viable C standard library and sysroot common definition, so we want Rust
-//!   and C/C++ code to interoperate when compiled to `wasm32-unknown-unknown`.
+//!   any other. To that the `wasm32-wasi` target is the first with a viable C
+//!   standard library and sysroot common definition, so we want Rust and C/C++
+//!   code to interoperate when compiled to `wasm32-unknown-unknown`.
 //!
 //! You'll note, however, that the two goals above are somewhat at odds with one
 //! another. To attempt to solve both use cases in one go we define a target
@@ -39,8 +39,8 @@
 //! necessary.
 //!
 //! All in all, by default, no external dependencies are required. You can
-//! compile `wasm32-unknown-wasi` binaries straight out of the box. You can't,
-//! however, reliably interoperate with C code in this mode (yet).
+//! compile `wasm32-wasi` binaries straight out of the box. You can't, however,
+//! reliably interoperate with C code in this mode (yet).
 //!
 //! ## Interop with C required
 //!
@@ -53,17 +53,17 @@
 //!
 //! 2. If you're using rustc to build a linked artifact then you'll need to
 //!    specify `-C linker` to a `clang` binary that supports
-//!    `wasm32-unknown-wasi` and is configured with the `wasm32-unknown-wasi`
-//!    sysroot. This will cause Rust code to be linked against the libc.a that
-//!    the specified `clang` provides.
+//!    `wasm32-wasi` and is configured with the `wasm32-wasi` sysroot. This
+//!    will cause Rust code to be linked against the libc.a that the specified
+//!    `clang` provides.
 //!
 //! 3. If you're building a staticlib and integrating Rust code elsewhere, then
 //!    compiling with `-C target-feature=-crt-static` is all you need to do.
 //!
 //! You can configure the linker via Cargo using the
-//! `CARGO_TARGET_WASM32_UNKNOWN_WASI_LINKER` env var. Be sure to also set
-//! `CC_wasm32-unknown-wasi` if any crates in the dependency graph are using
-//! the `cc` crate.
+//! `CARGO_TARGET_WASM32_WASI_LINKER` env var. Be sure to also set
+//! `CC_wasm32-wasi` if any crates in the dependency graph are using the `cc`
+//! crate.
 //!
 //! ## Remember, this is all in flux
 //!
@@ -82,7 +82,7 @@ pub fn target() -> Result<Target, String> {
         .pre_link_args
         .entry(LinkerFlavor::Gcc)
         .or_insert(Vec::new())
-        .push("--target=wasm32-unknown-wasi".to_string());
+        .push("--target=wasm32-wasi".to_string());
 
     // When generating an executable be sure to put the startup object at the
     // front so the main function is correctly hooked up.
@@ -98,13 +98,13 @@ pub fn target() -> Result<Target, String> {
     options.crt_static_respected = true;
 
     Ok(Target {
-        llvm_target: "wasm32-unknown-wasi".to_string(),
+        llvm_target: "wasm32-wasi".to_string(),
         target_endian: "little".to_string(),
         target_pointer_width: "32".to_string(),
         target_c_int_width: "32".to_string(),
         target_os: "wasi".to_string(),
         target_env: String::new(),
-        target_vendor: "unknown".to_string(),
+        target_vendor: String::new(),
         data_layout: "e-m:e-p:32:32-i64:64-n32:64-S128".to_string(),
         arch: "wasm32".to_string(),
         linker_flavor: LinkerFlavor::Lld(LldFlavor::Wasm),
