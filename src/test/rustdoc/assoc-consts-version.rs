8fc6e420d16dc882f2047e6ec1b981cac5ef0d14
Andy Russell <arussell123@gmail.com>
2019-05-03 12:55:31 -0400

    use span instead of div for since version

--- src/test/rustdoc/assoc-consts-version.rs
+++ src/test/rustdoc/assoc-consts-version.rs
@@ -1,5 +1,3 @@
-// ignore-tidy-linelength
-
 #![crate_name = "foo"]
 
 #![feature(staged_api)]
@@ -10,7 +8,8 @@
 pub struct SomeStruct;
 
 impl SomeStruct {
-    // @has 'foo/struct.SomeStruct.html' '//*[@id="associatedconstant.SOME_CONST"]//div[@class="since"]' '1.1.2'
+    // @has 'foo/struct.SomeStruct.html' \
+    //   '//*[@id="associatedconstant.SOME_CONST"]//span[@class="since"]' '1.1.2'
     #[stable(since="1.1.2", feature="rust2")]
     pub const SOME_CONST: usize = 0;
 }
