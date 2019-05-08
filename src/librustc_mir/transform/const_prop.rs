45214edf9e46459010d9853fe370c9c1e2cd4f35
Wesley Wiser <wwiser@gmail.com>
2019-05-08 06:42:41 -0400

    Run const propagation at O2

--- src/librustc_mir/transform/const_prop.rs
+++ src/librustc_mir/transform/const_prop.rs
@@ -639,7 +639,7 @@ impl<'b, 'a, 'tcx> MutVisitor<'tcx> for ConstPropagator<'b, 'a, 'tcx> {
                             assert!(self.places[local].is_none());
                             self.places[local] = Some(value);
 
-                            if self.tcx.sess.opts.debugging_opts.mir_opt_level >= 3 {
+                            if self.tcx.sess.opts.debugging_opts.mir_opt_level >= 2 {
                                 self.replace_with_const(rval, value, statement.source_info.span);
                             }
                         }
