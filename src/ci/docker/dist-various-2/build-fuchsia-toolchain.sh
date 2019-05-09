8f9e90b9cd4d377c8a880dc9b66932bcaf1da72d
Pietro Albini <pietro@pietroalbini.org>
2019-05-09 11:32:41 +0200

    ci: use our own mirror for fuchsia's zircon repository
    The canonical repository on fuchsia.googlesource.com is not accessible
    anymore, neither for anonymous access nor logged in access. This commit
    switches our CI to fetch the repository from our own mirror.

--- src/ci/docker/dist-various-2/build-fuchsia-toolchain.sh
+++ src/ci/docker/dist-various-2/build-fuchsia-toolchain.sh
@@ -10,7 +10,7 @@ pushd zircon > /dev/null
 
 # Download sources
 git init
-git remote add origin https://fuchsia.googlesource.com/zircon
+git remote add origin https://github.com/rust-lang-nursery/mirror-google-fuchsia-zircon
 git fetch --depth=1 origin $ZIRCON
 git reset --hard FETCH_HEAD
 
