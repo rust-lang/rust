dcfe380de4821b2122c64a5d25553eb6ea714a20
Michael Wright <mikerite@lavabit.com>
2019-05-09 05:55:24 +0200

    Remove unused attrs in while_let_on_iterator tests

--- tests/ui/while_let_on_iterator.rs
+++ tests/ui/while_let_on_iterator.rs
@@ -1,5 +1,5 @@
-#![warn(clippy::while_let_loop, clippy::empty_loop, clippy::while_let_on_iterator)]
-#![allow(dead_code, clippy::never_loop, unused, clippy::cognitive_complexity)]
+#![warn(clippy::while_let_on_iterator)]
+#![allow(clippy::never_loop, clippy::cognitive_complexity)]
 
 fn main() {
     let mut iter = 1..20;
