14d948c5601ce5a4970bf8f1fc3d16d302289488
pJunger <pJunger@users.noreply.github.com>
2019-05-12 20:42:13 +0200

    Registered lint.

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -576,6 +576,7 @@ pub fn register_plugins(reg: &mut rustc_plugin::Registry<'_>, conf: &Conf) {
     reg.register_late_lint_pass(box missing_const_for_fn::MissingConstForFn);
     reg.register_late_lint_pass(box transmuting_null::TransmutingNull);
     reg.register_late_lint_pass(box path_buf_push_overwrite::PathBufPushOverwrite);
+    reg.register_late_lint_pass(box checked_conversions::CheckedConversions);
 
     reg.register_lint_group("clippy::restriction", Some("clippy_restriction"), vec![
         arithmetic::FLOAT_ARITHMETIC,
