908d97d03ce4d20287163d91a71695f03f3a906e
varkor <github@varkor.com>
2019-05-12 22:28:26 +0100

    Update test output

--- src/test/ui/closures/closure-array-break-length.rs
+++ src/test/ui/closures/closure-array-break-length.rs
@@ -2,6 +2,8 @@ fn main() {
     |_: [_; continue]| {}; //~ ERROR: `continue` outside of loop
 
     while |_: [_; continue]| {} {} //~ ERROR: `continue` outside of loop
+    //~^ ERROR mismatched types
 
     while |_: [_; break]| {} {} //~ ERROR: `break` outside of loop
+    //~^ ERROR mismatched types
 }
