6c3862594210cc0a5e60fbad32d3aab7a4fb512c
Jeremy Fitzhardinge <jsgf@fb.com>
2019-05-21 12:38:46 -0700

    Make -Zemit-artifact-notifications also emit the artifact type
    This is easier for tooling to handle than trying to reverse-engineer it from the filename extension.

--- src/librustc_errors/emitter.rs
+++ src/librustc_errors/emitter.rs
@@ -56,7 +56,7 @@ pub trait Emitter {
     /// Emit a notification that an artifact has been output.
     /// This is currently only supported for the JSON format,
     /// other formats can, and will, simply ignore it.
-    fn emit_artifact_notification(&mut self, _path: &Path) {}
+    fn emit_artifact_notification(&mut self, _path: &Path, _artifact_type: &str) {}
 
     /// Checks if should show explanations about "rustc --explain"
     fn should_show_explain(&self) -> bool {
