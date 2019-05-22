a4b9a03362f626db617d3a67685d3a9ba9f1c683
Mazdak Farrokhzad <twingoow@gmail.com>
2019-05-22 05:52:47 +0200

    Fix inaccurate comments in '?' Kleene operator tests.

--- src/test/ui/macros/macro-at-most-once-rep-2018.rs
+++ src/test/ui/macros/macro-at-most-once-rep-2018.rs
@@ -1,4 +1,4 @@
-// Tests that `?` is a Kleene op and not a macro separator in the 2018 edition.
+// Tests that `?` is a Kleene op and not a macro separator in the 2015 edition.
 
 // edition:2018
 
@@ -6,6 +6,7 @@ macro_rules! foo {
     ($(a)?) => {};
 }
 
+// The Kleene op `?` does not admit a separator before it.
 macro_rules! baz {
     ($(a),?) => {}; //~ERROR the `?` macro repetition operator
 }
