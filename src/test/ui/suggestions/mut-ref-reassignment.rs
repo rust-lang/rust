7fbbcfaafd08023cc2296481425e16a02ccd3f1a
Esteban Küber <esteban@kuber.com.ar>
2019-05-23 10:32:01 -0700

    Add regression test for negative case

--- src/test/ui/suggestions/mut-ref-reassignment.rs
+++ src/test/ui/suggestions/mut-ref-reassignment.rs
@@ -1,5 +1,17 @@
-fn change_opt(opt: &mut Option<String>){
+fn suggestion(opt: &mut Option<String>) {
+    opt = None; //~ ERROR mismatched types
+}
+
+fn no_suggestion(opt: &mut Result<String, ()>) {
     opt = None //~ ERROR mismatched types
+}
+
+fn suggestion2(opt: &mut Option<String>) {
+    opt = Some(String::new())//~ ERROR mismatched types
+}
+
+fn no_suggestion2(opt: &mut Option<String>) {
+    opt = Some(42)//~ ERROR mismatched types
 }
 
 fn main() {}
