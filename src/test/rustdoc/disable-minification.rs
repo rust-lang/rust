834261fd8696dffda1115afdf2215dbcd53975ae
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-05-24 10:43:15 +0200

    Add test for minification disablement

---
+++ src/test/rustdoc/disable-minification.rs
@@ -0,0 +1,10 @@
+// compile-flags:--disable-minification
+
+// @has search-index.js
+// @!has - 'var R'
+
+/// The quick brown fox jumps over the lazy dog.
+pub struct SomeStruct;
+
+/// The quick brown fox jumps over the lazy dog.
+pub fn some_fn() {}
