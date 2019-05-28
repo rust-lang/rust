a1d1d7a2c696c2afeff2ea206621a32d77fa49dc
memoryruins <memoryruinsmusic@gmail.com>
2019-05-28 14:45:27 -0400

    Update test/ui/parser for bare_trait_object warnings

--- src/test/ui/parser/bounds-obj-parens.rs
+++ src/test/ui/parser/bounds-obj-parens.rs
@@ -1,5 +1,7 @@
 // compile-pass
 
+#![allow(bare_trait_objects)]
+
 type A = Box<(Fn(u8) -> u8) + 'static + Send + Sync>; // OK (but see #39318)
 
 fn main() {}
--- src/test/ui/parser/trait-object-trait-parens.rs
+++ src/test/ui/parser/trait-object-trait-parens.rs
@@ -5,8 +5,11 @@ fn f<T: (Copy) + (?Sized) + (for<'a> Trait<'a>)>() {}
 fn main() {
     let _: Box<(Copy) + (?Sized) + (for<'a> Trait<'a>)>;
     //~^ ERROR `?Trait` is not permitted in trait object types
+    //~| WARN trait objects without an explicit `dyn` are deprecated
     let _: Box<(?Sized) + (for<'a> Trait<'a>) + (Copy)>;
+    //~^ WARN trait objects without an explicit `dyn` are deprecated
     let _: Box<(for<'a> Trait<'a>) + (Copy) + (?Sized)>;
     //~^ ERROR use of undeclared lifetime name `'a`
     //~| ERROR `?Trait` is not permitted in trait object types
+    //~| WARN trait objects without an explicit `dyn` are deprecated
 }
