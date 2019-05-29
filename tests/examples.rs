0d7eb17bb9a804537f2e3dc93dad5dbb4b231641
Inokentiy Babushkin <twk@twki.de>
2019-05-29 20:16:09 +0000

    Moved away from `git-diff`-based feature tests.
    Akin to full tests, we now have a relatively clean rust pipeline running
    rust-semverver and friends, and performing all output transformations
    and diffing without shelling out to git and overwriting checked-out
    expected outputs in the process.

--- tests/examples.rs
+++ tests/examples.rs
@@ -1,139 +1,128 @@
 mod features {
     use std::{
-        env, fs, io,
+        env,
+        fs::{read_to_string, File},
+        io::{BufRead, Write},
         path::Path,
         process::{Command, Stdio},
     };
 
-    fn test_example(path: &Path, out_file: &Path, expected_result: bool) {
+    fn test_example2(name: &str, path: &Path, expected_path: &Path, expected_result: bool) {
         let old_rlib = path.join("libold.rlib").to_str().unwrap().to_owned();
         let new_rlib = path.join("libnew.rlib").to_str().unwrap().to_owned();
 
-        {
-            let stdout = fs::File::create(&out_file).expect("could not create `stdout` file");
-            let stderr = stdout
-                .try_clone()
-                .expect("could not create `stderr` file by cloning `stdout`");
+        let target_args = std::env::var("TEST_TARGET").map(|t| ["--target".to_string(), t]);
 
-            let target_args = std::env::var("TEST_TARGET").map(|t| ["--target".to_string(), t]);
-
-            let mut cmd = Command::new("rustc");
-            cmd.args(&["--crate-type=lib", "-o", &old_rlib])
-                .arg(path.join("old.rs"))
-                .env("RUST_BACKTRACE", "full")
-                .stdin(Stdio::null());
-
-            if let Ok(target_args) = &target_args {
-                cmd.args(target_args);
-            }
-
-            let rustc_old_result = cmd.status().expect("could not run rustc on old").success();
-            assert!(rustc_old_result, "couldn't compile old");
-
-            let mut cmd = Command::new("rustc");
-            cmd.args(&["--crate-type=lib", "-o", &new_rlib])
-                .arg(path.join("new.rs"))
-                .env("RUST_BACKTRACE", "full")
-                .stdin(Stdio::null());
-
-            if let Ok(target_args) = &target_args {
-                cmd.args(target_args);
-            }
-
-            let rustc_new_result = cmd.status().expect("could not run rustc on new").success();
-            assert!(rustc_new_result, "couldn't compile new");
-
-            let mut cmd = Command::new(
-                Path::new(".")
-                    .join("target")
-                    .join("debug")
-                    .join("rust-semverver")
-                    .to_str()
-                    .unwrap(),
-            );
-            cmd.args(&[
-                "--crate-type=lib",
-                "-Zverbose",
-                "--extern",
-                &format!("old={}", old_rlib),
-                "--extern",
-                &format!("new={}", new_rlib),
-                Path::new("tests")
-                    .join("helper")
-                    .join("test.rs")
-                    .to_str()
-                    .unwrap(),
-            ])
+        let mut cmd = Command::new("rustc");
+        cmd.args(&["--crate-type=lib", "-o", &old_rlib])
+            .arg(path.join("old.rs"))
             .env("RUST_BACKTRACE", "full")
-            .env("RUST_SEMVER_CRATE_VERSION", "1.0.0")
-            .stdin(Stdio::null())
-            .stdout(Stdio::from(stdout))
-            .stderr(Stdio::from(stderr));
+            .stdin(Stdio::null());
 
-            if let Ok(target_args) = &target_args {
-                cmd.args(target_args);
-            }
-
-            if out_file.to_str().unwrap().contains("stdout_api_guidelines") {
-                cmd.env("RUST_SEMVER_API_GUIDELINES", "true");
-            }
-
-            let rustsemverver_result = cmd
-                .status()
-                .expect("could not run rust-semverver")
-                .success();
-            assert_eq!(
-                rustsemverver_result, expected_result,
-                "rust-semverver returned an unexpected exit status"
-            );
-
-            {
-                // replace root path with with $REPO_PATH
-                use self::io::{Read, Write};
-                let current_dir = env::current_dir().expect("could not determine current dir");
-                let mut contents = {
-                    let mut f = fs::File::open(&out_file).expect("file not found");
-                    let mut contents = String::new();
-                    f.read_to_string(&mut contents)
-                        .expect("something went wrong reading the file");
-                    contents
-                };
-
-                contents = contents.replace(current_dir.to_str().unwrap(), "$REPO_PATH");
-
-                if cfg!(target_os = "windows") {
-                    let mut lines = Vec::new();
-
-                    for line in contents.lines() {
-                        if line.contains("$REPO_PATH") {
-                            lines.push(line.replace('\\', "/"));
-                        } else {
-                            lines.push(line.to_owned());
-                        }
-                    }
-                    lines.push(String::new());
-                    contents = lines.join("\r\n");
-                }
-
-                let mut file = fs::File::create(&out_file).expect("cannot create file");
-                file.write_all(contents.as_bytes())
-                    .expect("cannot write to file");
-            }
+        if let Ok(target_args) = &target_args {
+            cmd.args(target_args);
         }
 
-        let git_result = Command::new("git")
-            .args(&[
-                "diff",
-                "--ignore-space-at-eol",
-                "--exit-code",
-                out_file.to_str().unwrap(),
-            ])
-            .env("PAGER", "")
-            .status()
-            .expect("could not run git diff")
-            .success();
+        let rustc_old_result = cmd.status().expect("could not run rustc on old").success();
+        assert!(rustc_old_result, "couldn't compile old");
 
-        assert!(git_result, "git reports unexpected diff");
+        let mut cmd = Command::new("rustc");
+        cmd.args(&["--crate-type=lib", "-o", &new_rlib])
+            .arg(path.join("new.rs"))
+            .env("RUST_BACKTRACE", "full")
+            .stdin(Stdio::null());
+
+        if let Ok(target_args) = &target_args {
+            cmd.args(target_args);
+        }
+
+        let rustc_new_result = cmd.status().expect("could not run rustc on new").success();
+        assert!(rustc_new_result, "couldn't compile new");
+
+        let mut cmd = Command::new(
+            Path::new(".")
+                .join("target")
+                .join("debug")
+                .join("rust-semverver")
+                .to_str()
+                .unwrap(),
+        );
+        cmd.args(&[
+            "--crate-type=lib",
+            "-Zverbose",
+            "--extern",
+            &format!("old={}", old_rlib),
+            "--extern",
+            &format!("new={}", new_rlib),
+            Path::new("tests")
+                .join("helper")
+                .join("test.rs")
+                .to_str()
+                .unwrap(),
+        ])
+        .env("RUST_BACKTRACE", "full")
+        .env("RUST_SEMVER_CRATE_VERSION", "1.0.0")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+
+        if let Ok(target_args) = &target_args {
+            cmd.args(target_args);
+        }
+
+        if expected_path.to_str().unwrap().contains("stdout_api_guidelines") {
+            cmd.env("RUST_SEMVER_API_GUIDELINES", "true");
+        }
+
+        let output = cmd.output().expect("could not run rust-semverver");
+
+        let expected_output =
+            read_to_string(&expected_path).expect("could not read expected output from file");
+
+        let new_output = output
+            .stdout
+            .lines()
+            .chain(output.stderr.lines())
+            .map(|r| r.expect("could not read line from rust-semverver output"))
+            .map(|line| {
+                // sanitize paths for reproducibility
+                (match line.find("-->") {
+                    Some(idx) => {
+                        let (start, end) = line.split_at(idx);
+                        match end.find(name) {
+                            Some(idx) => format!("{}--> {}", start, end.split_at(idx).1),
+                            None => line,
+                        }
+                    }
+                    None => line,
+                }) + "\n"
+            })
+            .map(|l| if cfg!(target_os = "windows") {
+                l.replace('\\', "/")
+            } else {
+                l
+            })
+            .collect::<String>();
+
+        if expected_output != new_output {
+            eprintln!("rust-semverver failed to produce the expected result");
+
+            let new_path = Path::new(&env::var("OUT_DIR").unwrap()).join(name);
+            let mut new_file = File::create(&new_path).unwrap();
+            new_file.write_all(new_output.as_bytes()).unwrap();
+
+            eprintln!(
+                "For details, try this command: \n\n    diff {} {}\n\n",
+                expected_path.display(),
+                new_path.display()
+            );
+            panic!("unexpected output diff");
+        }
+
+        assert_eq!(
+            output.status.success(), expected_result,
+            "rust-semverver returned an unexpected exit status"
+        );
 
         Command::new("rm")
             .args(&[&old_rlib, &new_rlib])
@@ -146,11 +135,11 @@ mod features {
             #[test]
             fn $name() {
                 let path = Path::new("tests").join("cases").join(stringify!($name));
-                test_example(&path, &path.join("stdout"), $result);
+                test_example2(stringify!($name), &path, &path.join("stdout"), $result);
 
                 if path.join("stdout_api_guidelines").exists() {
                     eprintln!("api-guidelines");
-                    test_example(&path, &path.join("stdout_api_guidelines"), $result);
+                    test_example2(stringify!($name), &path, &path.join("stdout_api_guidelines"), $result);
                 }
             }
         };
