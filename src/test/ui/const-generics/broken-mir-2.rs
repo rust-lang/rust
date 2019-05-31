6485b6b2283080522bf952339e67c2aea38d02c2
CrLF0710 <crlf0710@gmail.com>
2019-05-31 18:06:46 +0800

    Change expected output of one test case.

--- src/test/ui/const-generics/broken-mir-2.rs
+++ src/test/ui/const-generics/broken-mir-2.rs
@@ -1,9 +1,11 @@
+// run-pass
+
 #![feature(const_generics)]
 //~^ WARN the feature `const_generics` is incomplete and may cause the compiler to crash
 
 use std::fmt::Debug;
 
 #[derive(Debug)]
-struct S<T: Debug, const N: usize>([T; N]); //~ ERROR `[T; _]` doesn't implement `std::fmt::Debug`
+struct S<T: Debug, const N: usize>([T; N]);
 
 fn main() {}
