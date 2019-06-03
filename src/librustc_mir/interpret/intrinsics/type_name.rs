dfb8dbd923f3d5c8b39b13fab21ba5e347a9dc7f
TankhouseAle <51239665+TankhouseAle@users.noreply.github.com>
2019-06-02 21:10:12 -0400

    Make Tidy happy
    I feel like the function name is good, so just knocking it down a line seems like the right way to go.

--- src/librustc_mir/interpret/intrinsics/type_name.rs
+++ src/librustc_mir/interpret/intrinsics/type_name.rs
@@ -228,7 +228,8 @@ pub fn type_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, ty: Ty<'tcx>) -> &'tcx t
 }
 
 /// Directly returns an `Allocation` containing an absolute path representation of the given type.
-pub(super) fn alloc_type_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, ty: Ty<'tcx>) -> &'tcx Allocation {
+pub(super) 
+fn alloc_type_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, ty: Ty<'tcx>) -> &'tcx Allocation {
     let path = AbsolutePathPrinter { tcx, path: String::new() }.print_type(ty).unwrap().path;
     let alloc = Allocation::from_byte_aligned_bytes(path.into_bytes());
     tcx.intern_const_alloc(alloc)
