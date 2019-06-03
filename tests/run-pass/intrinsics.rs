1ceb81b34552c0ff466f4ffaa00fe2f87cb5e16c
Dylan MacKenzie <ecstaticmorse@gmail.com>
2019-06-02 21:20:13 -0700

    Use in-core implementation of `type_name`.
    We bump `rust-version` to pick up the new impl from
    https://github.com/rust-lang/rust/pull/61498 and add a test.

--- tests/run-pass/intrinsics.rs
+++ tests/run-pass/intrinsics.rs
@@ -1,3 +1,6 @@
+#![feature(core_intrinsics)]
+
+use std::intrinsics::type_name;
 use std::mem::{size_of, size_of_val};
 
 fn main() {
@@ -7,4 +10,6 @@ fn main() {
     assert_eq!(size_of_val(&[] as &[i32]), 0);
     assert_eq!(size_of_val(&[1, 2, 3] as &[i32]), 12);
     assert_eq!(size_of_val("foobar"), 6);
+
+    assert_eq!(unsafe { type_name::<Option<i32>>() }, "core::option::Option<i32>");
 }
