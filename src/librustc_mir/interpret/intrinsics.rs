a58a557e454f43a3a4119d79fa4da1a5ca073a08
TankhouseAle <51239665+TankhouseAle@users.noreply.github.com>
2019-06-02 21:56:19 -0400

    Don't need to use Pointer if we do name_id.into()
    Within `tag_static_base_pointer`, that is.
    
    The separate `id_ptr` variable *does* seem to be necessary in general, however.
    
    Trying to do `name_id.into()` *directly* inside of `Scalar::Ptr()` in this context fails to compile, as `rustc` wants this super-specific specialization of `Pointer` which it apparently doesn't get from that.

--- src/librustc_mir/interpret/intrinsics.rs
+++ src/librustc_mir/interpret/intrinsics.rs
@@ -11,7 +11,7 @@ use rustc::mir::interpret::{
 };
 
 use super::{
-    Machine, PlaceTy, OpTy, InterpretCx, Immediate, Pointer
+    Machine, PlaceTy, OpTy, InterpretCx, Immediate
 };
 
 mod type_name;
@@ -82,7 +82,7 @@ impl<'a, 'mir, 'tcx, M: Machine<'a, 'mir, 'tcx>> InterpretCx<'a, 'mir, 'tcx, M> 
             "type_name" => {
                 let alloc = alloc_type_name(self.tcx.tcx, substs.type_at(0));
                 let name_id = self.tcx.alloc_map.lock().create_memory_alloc(alloc);
-                let id_ptr = self.memory.tag_static_base_pointer(Pointer::new(name_id, Size::ZERO));
+                let id_ptr = self.memory.tag_static_base_pointer(name_id.into());
                 let alloc_len = alloc.bytes.len() as u64;
                 let name_val = Immediate::new_slice(Scalar::Ptr(id_ptr), alloc_len, self);
                 self.write_immediate(name_val, dest)?;
