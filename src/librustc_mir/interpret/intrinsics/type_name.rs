9b2d6482a0143410c143448ebf62da3f703fde9e
TankhouseAle <51239665+TankhouseAle@users.noreply.github.com>
2019-06-03 10:09:32 -0400

    Maybe it will merge this way?

--- src/librustc_mir/interpret/intrinsics/type_name.rs
+++ src/librustc_mir/interpret/intrinsics/type_name.rs
@@ -210,6 +210,16 @@ impl Write for AbsolutePathPrinter<'_, '_> {
     }
 }
 
+/// Directly returns an `Allocation` containing an absolute path representation of the given type.
+pub(super) fn alloc_type_name<'a, 'tcx>(
+    tcx: TyCtxt<'a, 'tcx, 'tcx>,
+    ty: Ty<'tcx>
+) -> &'tcx Allocation {
+    let path = AbsolutePathPrinter { tcx, path: String::new() }.print_type(ty).unwrap().path;
+    let alloc = Allocation::from_byte_aligned_bytes(path.into_bytes());
+    tcx.intern_const_alloc(alloc)
+}
+
 /// Produces an absolute path representation of the given type. See also the documentation on
 /// `std::any::type_name`
 pub fn type_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, ty: Ty<'tcx>) -> &'tcx ty::Const<'tcx> {
@@ -222,14 +232,4 @@ pub fn type_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, ty: Ty<'tcx>) -> &'tcx t
         },
         ty: tcx.mk_static_str(),
     })
-}
-
-/// Directly returns an `Allocation` containing an absolute path representation of the given type.
-pub(super) fn alloc_type_name<'a, 'tcx>(
-    tcx: TyCtxt<'a, 'tcx, 'tcx>,
-    ty: Ty<'tcx>
-) -> &'tcx Allocation {
-    let path = AbsolutePathPrinter { tcx, path: String::new() }.print_type(ty).unwrap().path;
-    let alloc = Allocation::from_byte_aligned_bytes(path.into_bytes());
-    tcx.intern_const_alloc(alloc)
 }
