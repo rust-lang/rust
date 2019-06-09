bed189791ef9b900788c40354a2ebe5d0427027a
Mazdak Farrokhzad <twingoow@gmail.com>
2019-06-09 05:52:55 +0200

    type-alias-enum-variants-panic: harden + describe the test.

--- src/test/ui/type-alias-enum-variants/type-alias-enum-variants-panic.rs
+++ src/test/ui/type-alias-enum-variants/type-alias-enum-variants-panic.rs
@@ -1,15 +1,23 @@
 // ignore-tidy-linelength
 
+// Check that creating/matching on an enum variant through an alias with
+// the wrong braced/unit form is caught as an error.
+
 #![allow(unreachable_code)]
 
-enum Enum { Variant {} }
+enum Enum { Braced {}, Unit, Tuple() }
 type Alias = Enum;
 
 fn main() {
-    Alias::Variant;
-    //~^ ERROR expected unit struct/variant or constant, found struct variant `<Alias>::Variant` [E0533]
-    let Alias::Variant = panic!();
-    //~^ ERROR expected unit struct/variant or constant, found struct variant `<Alias>::Variant` [E0533]
-    let Alias::Variant(..) = panic!();
-    //~^ ERROR expected tuple struct/variant, found struct variant `<Alias>::Variant` [E0164]
+    Alias::Braced;
+    //~^ ERROR expected unit struct/variant or constant, found struct variant `<Alias>::Braced` [E0533]
+    let Alias::Braced = panic!();
+    //~^ ERROR expected unit struct/variant or constant, found struct variant `<Alias>::Braced` [E0533]
+    let Alias::Braced(..) = panic!();
+    //~^ ERROR expected tuple struct/variant, found struct variant `<Alias>::Braced` [E0164]
+
+    Alias::Unit();
+    //~^ ERROR expected function, found enum variant `<Alias>::Unit`
+    let Alias::Unit() = panic!();
+    //~^ ERROR expected tuple struct/variant, found unit variant `<Alias>::Unit` [E0164]
 }
