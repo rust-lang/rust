c0c48a6677fbee62db3816643200fb2a755d715b
Mazdak Farrokhzad <twingoow@gmail.com>
2019-06-09 03:59:18 +0200

    type-alias-enum-variants-priority-3: describe the test.

--- src/test/ui/type-alias-enum-variants/type-alias-enum-variants-priority-3.rs
+++ src/test/ui/type-alias-enum-variants/type-alias-enum-variants-priority-3.rs
@@ -1,3 +1,6 @@
+// Check that the compiler will resolve `<E>::V` to the variant `V` in the type namespace
+// but will reject this because `enum` variants do not exist in the type namespace.
+
 enum E {
     V
 }
