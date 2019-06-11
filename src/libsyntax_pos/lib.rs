6fdcc8281a93c3aa032df245ca5d0342adcb01b1
Niko Matsakis <niko@alum.mit.edu>
2019-06-11 19:09:15 -0400

    remove hacks that are no longer needed

--- src/libsyntax_pos/lib.rs
+++ src/libsyntax_pos/lib.rs
@@ -227,14 +227,6 @@ impl SpanData {
     }
 }
 
-// The interner is pointed to by a thread local value which is only set on the main thread
-// with parallelization is disabled. So we don't allow `Span` to transfer between threads
-// to avoid panics and other errors, even though it would be memory safe to do so.
-#[cfg(not(parallel_compiler))]
-impl !Send for Span {}
-#[cfg(not(parallel_compiler))]
-impl !Sync for Span {}
-
 impl PartialOrd for Span {
     fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
         PartialOrd::partial_cmp(&self.data(), &rhs.data())
