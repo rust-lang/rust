2057136326bfac61b29b7fac87289728f105ffe3
Niko Matsakis <niko@alum.mit.edu>
2019-06-12 10:47:04 -0400

    update the async-fn-multiple-lifetimes test

--- src/test/ui/async-await/async-fn-multiple-lifetimes.rs
+++ src/test/ui/async-await/async-fn-multiple-lifetimes.rs
@@ -1,11 +1,13 @@
 // edition:2018
+// run-pass
 
-#![feature(arbitrary_self_types, async_await, await_macro, pin)]
+// Test that we can use async fns with multiple arbitrary lifetimes.
+
+#![feature(arbitrary_self_types, async_await, await_macro)]
 
 use std::ops::Add;
 
 async fn multiple_named_lifetimes<'a, 'b>(_: &'a u8, _: &'b u8) {}
-//~^ ERROR ambiguous lifetime bound in `async fn`
 
 async fn multiple_hrtb_and_single_named_lifetime_ok<'c>(
     _: impl for<'a> Add<&'a u8>,
@@ -14,6 +16,5 @@ async fn multiple_hrtb_and_single_named_lifetime_ok<'c>(
 ) {}
 
 async fn multiple_elided_lifetimes(_: &u8, _: &u8) {}
-//~^ ambiguous lifetime bound in `async fn`
 
 fn main() {}
