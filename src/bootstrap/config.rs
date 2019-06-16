ca2dd22a1b9a92cacfd9ae3f3eb742640102cdf8
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-06-16 20:30:52 +0200

    Revert "test"
    This reverts commit 220058bf35fdf17e78b499e0df195000fca42f48.

--- src/bootstrap/config.rs
+++ src/bootstrap/config.rs
@@ -567,7 +567,7 @@ impl Config {
             set(&mut config.lld_enabled, rust.lld);
             set(&mut config.lldb_enabled, rust.lldb);
             set(&mut config.llvm_tools_enabled, rust.llvm_tools);
-            config.rustc_parallel = rust.parallel_compiler.unwrap_or(true);
+            config.rustc_parallel = rust.parallel_compiler.unwrap_or(false);
             config.rustc_default_linker = rust.default_linker.clone();
             config.musl_root = rust.musl_root.clone().map(PathBuf::from);
             config.save_toolstates = rust.save_toolstates.clone().map(PathBuf::from);
