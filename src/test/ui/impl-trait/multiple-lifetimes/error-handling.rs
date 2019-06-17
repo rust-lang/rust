5d44bebb32b12687638a93cde551ee8919c305c1
Niko Matsakis <niko@alum.mit.edu>
2019-06-17 11:38:38 -0400

    bless test output

--- src/test/ui/impl-trait/multiple-lifetimes/error-handling.rs
+++ src/test/ui/impl-trait/multiple-lifetimes/error-handling.rs
@@ -8,9 +8,9 @@ struct CopyIfEq<T, U>(T, U);
 impl<T: Copy> Copy for CopyIfEq<T, T> {}
 
 existential type E<'a, 'b>: Sized;
-//~^ ERROR lifetime may not live long enough
 
 fn foo<'a, 'b, 'c>(x: &'static i32, mut y: &'a i32) -> E<'b, 'c> {
+    //~^ ERROR lifetime may not live long enough
     let v = CopyIfEq::<*mut _, *mut _>(&mut {x}, &mut y);
     let u = v;
     let _: *mut &'a i32 = u.1;
