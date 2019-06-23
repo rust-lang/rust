4dc188a60eb19dc770db071a5e368e90b5a1fab3
Ralf Jung <post@ralfj.de>
2019-06-23 17:26:12 +0200

    adjust for refactored memory pointer checks

--- tests/compile-fail/unaligned_ptr_cast1.rs
+++ tests/compile-fail/unaligned_ptr_cast1.rs
@@ -2,8 +2,8 @@
 // compile-flags: -Zmiri-disable-validation
 
 fn main() {
-    let x = &2u16;
-    let x = x as *const _ as *const u32;
+    let x = [2u16, 3, 4]; // Make it big enough so we don't get an out-of-bounds error.
+    let x = &x[0] as *const _ as *const u32;
     // This must fail because alignment is violated
     let _x = unsafe { *x }; //~ ERROR tried to access memory with alignment 2, but alignment 4 is required
 }
