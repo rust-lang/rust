4d65aa8f20ac80f7517af15f2dec8936325119c6
Ralf Jung <post@ralfj.de>
2019-06-24 08:40:45 +0200

    expand and better explain alignment check tests

--- tests/compile-fail/unaligned_ptr1.rs
+++ tests/compile-fail/unaligned_ptr1.rs
@@ -4,6 +4,6 @@
 fn main() {
     let x = [2u16, 3, 4]; // Make it big enough so we don't get an out-of-bounds error.
     let x = &x[0] as *const _ as *const u32;
-    // This must fail because alignment is violated
+    // This must fail because alignment is violated: the allocation's base is not sufficiently aligned.
     let _x = unsafe { *x }; //~ ERROR tried to access memory with alignment 2, but alignment 4 is required
 }
