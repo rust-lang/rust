bb63f30b210579d3978964f2dcb17020023a41ed
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2019-06-29 17:51:20 +0300

    Improve documentation for built-in macros

--- src/libcore/lib.rs
+++ src/libcore/lib.rs
@@ -75,6 +75,7 @@
 #![feature(const_fn)]
 #![feature(const_fn_union)]
 #![feature(custom_inner_attributes)]
+#![feature(decl_macro)]
 #![feature(doc_cfg)]
 #![feature(doc_spotlight)]
 #![feature(extern_types)]
