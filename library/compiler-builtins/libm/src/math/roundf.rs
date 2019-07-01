1011d29c556d8abc41748c66f19ce8b22be7a6ca
Igor null <m1el.2027@gmail.com>
2019-07-01 17:05:46 +0300

    Fixed rounding to negative zero

--- library/compiler-builtins/libm/src/math/roundf.rs
+++ library/compiler-builtins/libm/src/math/roundf.rs
@@ -12,12 +12,12 @@ pub fn roundf(mut x: f32) -> f32 {
     if e >= 0x7f + 23 {
         return x;
     }
-    if i >> 31 != 0 {
-        x = -x;
-    }
     if e < 0x7f - 1 {
         force_eval!(x + TOINT);
         return 0.0 * x;
+    }
+    if i >> 31 != 0 {
+        x = -x;
     }
     y = x + TOINT - TOINT - x;
     if y > 0.5f32 {
@@ -32,4 +32,9 @@ pub fn roundf(mut x: f32) -> f32 {
     } else {
         y
     }
+}
+
+#[test]
+fn negative_zero() {
+    assert_eq!(roundf(-0.0_f32).to_bits(), (-0.0_f32).to_bits());
 }
