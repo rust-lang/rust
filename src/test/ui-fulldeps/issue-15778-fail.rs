cd5214c37ea3e28a2edf3bae6206ca94188343df
Samy Kacimi <samy.kacimi@icloud.com>
2019-07-01 23:53:39 +0200

    bless ui-fulldeps tests

--- src/test/ui-fulldeps/issue-15778-fail.rs
+++ src/test/ui-fulldeps/issue-15778-fail.rs
@@ -2,7 +2,7 @@
 // ignore-stage1
 // compile-flags: -D crate-not-okay
 
-#![feature(plugin)] //~ ERROR crate is not marked with #![crate_okay]
+#![feature(plugin)] //~ ERROR crate is not marked with `#![crate_okay]`
 #![plugin(lint_for_crate)]
 
 pub fn main() { }
