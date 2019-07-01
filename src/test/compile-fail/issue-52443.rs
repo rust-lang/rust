baa9efb1786ccf62b3808375af65d6ae6ac56e98
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2019-07-01 19:26:36 +0300

    rustc_mir: follow FalseUnwind's real_target edge in qualify_consts.

--- src/test/compile-fail/issue-52443.rs
+++ src/test/compile-fail/issue-52443.rs
@@ -1,10 +1,14 @@
 fn main() {
     [(); & { loop { continue } } ]; //~ ERROR mismatched types
     [(); loop { break }]; //~ ERROR mismatched types
-    [(); {while true {break}; 0}]; //~ ERROR constant contains unimplemented expression type
-    //~^ WARN denote infinite loops with
-    [(); { for _ in 0usize.. {}; 0}]; //~ ERROR calls in constants are limited to constant functions
+    [(); {while true {break}; 0}];
     //~^ ERROR constant contains unimplemented expression type
+    //~| ERROR constant contains unimplemented expression type
+    //~| WARN denote infinite loops with
+    [(); { for _ in 0usize.. {}; 0}];
+    //~^ ERROR calls in constants are limited to constant functions
+    //~| ERROR references in constants may only refer to immutable values
+    //~| ERROR constant contains unimplemented expression type
     //~| ERROR constant contains unimplemented expression type
     //~| ERROR evaluation of constant value failed
 }
