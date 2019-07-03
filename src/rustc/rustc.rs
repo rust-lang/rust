abd53f90e92b26b960e82f0f07dd7566582cb83f
gnzlbg <gonzalobg88@gmail.com>
2019-07-03 17:16:48 +0200

    Switch rustc allocator to mimalloc

--- src/rustc/rustc.rs
+++ src/rustc/rustc.rs
@@ -13,22 +13,22 @@ fn main() {
 
         #[used]
         static _F1: unsafe extern fn(usize, usize) -> *mut c_void =
-            jemalloc_sys::calloc;
+            mimalloc_sys::calloc;
         #[used]
         static _F2: unsafe extern fn(*mut *mut c_void, usize, usize) -> c_int =
-            jemalloc_sys::posix_memalign;
+            mimalloc_sys::posix_memalign;
         #[used]
         static _F3: unsafe extern fn(usize, usize) -> *mut c_void =
-            jemalloc_sys::aligned_alloc;
+            mimalloc_sys::aligned_alloc;
         #[used]
         static _F4: unsafe extern fn(usize) -> *mut c_void =
-            jemalloc_sys::malloc;
+            mimalloc_sys::malloc;
         #[used]
         static _F5: unsafe extern fn(*mut c_void, usize) -> *mut c_void =
-            jemalloc_sys::realloc;
+            mimalloc_sys::realloc;
         #[used]
         static _F6: unsafe extern fn(*mut c_void) =
-            jemalloc_sys::free;
+            mimalloc_sys::free;
     }
 
     rustc_driver::set_sigpipe_handler();
