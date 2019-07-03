05762eb876f40c192a3c497b8634b668d280b033
Benjamin Schultzer <benjamin@schultzer.com>
2019-07-03 14:15:29 -0700

    Only return the fp value.
    Signed-off-by: Benjamin Schultzer <benjamin@schultzer.com>

--- library/compiler-builtins/libm/src/math/remainderf.rs
+++ library/compiler-builtins/libm/src/math/remainderf.rs
@@ -1,5 +1,6 @@
 #[inline]
 #[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
-pub fn remainderf(x: f32, y: f32) -> (f32, i32) {
-    super::remquof(x, y)
+pub fn remainderf(x: f32, y: f32) -> f32 {
+    let (result, _) = super::remquof(x, y);
+    result
 }
