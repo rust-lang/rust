69b9287e0210704fcd22ed4fb0134d49d293b712
gnzlbg <gonzalobg88@gmail.com>
2019-07-04 14:47:40 +0200

    Use proper symbol names

--- src/rustc/rustc.rs
+++ src/rustc/rustc.rs
@@ -13,22 +13,23 @@ fn main() {
 
         #[used]
         static _F1: unsafe extern fn(usize, usize) -> *mut c_void =
-            mimalloc_sys::calloc;
-        #[used]
-        static _F2: unsafe extern fn(*mut *mut c_void, usize, usize) -> c_int =
-            mimalloc_sys::posix_memalign;
+            mimalloc_sys::mi_calloc;
+        // NOTE: this symbol calls mi_malloc_aligned
+        // #[used]
+        // static _F2: unsafe extern fn(*mut *mut c_void, usize, usize) -> c_int =
+        //   mimalloc_sys::mi_posix_memalign;
         #[used]
         static _F3: unsafe extern fn(usize, usize) -> *mut c_void =
-            mimalloc_sys::aligned_alloc;
+            mimalloc_sys::mi_malloc_aligned;
         #[used]
         static _F4: unsafe extern fn(usize) -> *mut c_void =
-            mimalloc_sys::malloc;
+            mimalloc_sys::mi_malloc;
         #[used]
         static _F5: unsafe extern fn(*mut c_void, usize) -> *mut c_void =
-            mimalloc_sys::realloc;
+            mimalloc_sys::mi_realloc;
         #[used]
         static _F6: unsafe extern fn(*mut c_void) =
-            mimalloc_sys::free;
+            mimalloc_sys::mi_free;
     }
 
     rustc_driver::set_sigpipe_handler();
