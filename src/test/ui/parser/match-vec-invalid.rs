91c8b53f458ef6efb3640a359dba4015848fd9d1
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-08 01:58:28 +0200

    --bless tests due to new subslice syntax.

--- src/test/ui/parser/match-vec-invalid.rs
+++ src/test/ui/parser/match-vec-invalid.rs
@@ -1,7 +1,12 @@
 fn main() {
     let a = Vec::new();
     match a {
-        [1, tail @ .., tail @ ..] => {}, //~ ERROR: expected one of `,` or `@`, found `..`
+        [1, tail @ .., tail @ ..] => {},
+        //~^ ERROR identifier `tail` is bound more than once in the same pattern
+        //~| ERROR subslice patterns are unstable
+        //~| ERROR subslice patterns are unstable
+        //~| ERROR `..` can only be used once per slice pattern
+        //~| ERROR expected an array or slice, found `std::vec::Vec<_>`
         _ => ()
     }
 }
