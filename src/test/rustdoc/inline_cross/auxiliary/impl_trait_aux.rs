cc6dbb4f237ae0d84db5994cd075065efe05306b
Shotaro Yamada <sinkuu@sinkuu.xyz>
2019-07-08 20:45:59 +0900

    Fix tidy

--- src/test/rustdoc/inline_cross/auxiliary/impl_trait_aux.rs
+++ src/test/rustdoc/inline_cross/auxiliary/impl_trait_aux.rs
@@ -2,7 +2,10 @@ use std::ops::Deref;
 
 pub fn func<'a>(_x: impl Clone + Into<Vec<u8>> + 'a) {}
 
-pub fn func2<T>(_x: impl Deref<Target = Option<T>> + Iterator<Item = T>, _y: impl Iterator<Item = u8>) {}
+pub fn func2<T>(
+    _x: impl Deref<Target = Option<T>> + Iterator<Item = T>,
+    _y: impl Iterator<Item = u8>,
+) {}
 
 pub fn func3(_x: impl Iterator<Item = impl Iterator<Item = u8>> + Clone) {}
 
