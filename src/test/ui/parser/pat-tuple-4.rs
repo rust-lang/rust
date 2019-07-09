2411134c78706c1bb852da2fba4d90567ce4a5f3
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-09 08:44:46 +0200

    Update tests wrt. recovery of range patterns.

--- src/test/ui/parser/pat-tuple-4.rs
+++ src/test/ui/parser/pat-tuple-4.rs
@@ -1,5 +1,10 @@
 fn main() {
     match 0 {
-        (.. pat) => {} //~ ERROR expected one of `)` or `,`, found `pat`
+        (.. pat) => {}
+        //~^ ERROR `..X` range patterns are not supported
+        //~| ERROR arbitrary expressions aren't allowed in patterns
+        //~| ERROR cannot find value `pat` in this scope
+        //~| ERROR exclusive range pattern syntax is experimental
+        //~| ERROR only char and numeric types are allowed in range patterns
     }
 }
