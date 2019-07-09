e46b4a82995f7a01bbb12385ae27ed476aa89379
gnzlbg <gonzalobg88@gmail.com>
2019-07-09 17:33:01 +0200

    Floating-point environment APIs are private

--- library/compiler-builtins/libm/src/math/fenv.rs
+++ library/compiler-builtins/libm/src/math/fenv.rs
@@ -1,33 +1,33 @@
 // src: musl/src/fenv/fenv.c
 /* Dummy functions for archs lacking fenv implementation */
 
-pub const FE_UNDERFLOW: i32 = 0;
-pub const FE_INEXACT: i32 = 0;
+pub(crate) const FE_UNDERFLOW: i32 = 0;
+pub(crate) const FE_INEXACT: i32 = 0;
 
-pub const FE_TONEAREST: i32 = 0;
-pub const FE_TOWARDZERO: i32 = 0;
+pub(crate) const FE_TONEAREST: i32 = 0;
+pub(crate) const FE_TOWARDZERO: i32 = 0;
 
 #[inline]
-pub fn feclearexcept(_mask: i32) -> i32 {
+pub(crate) fn feclearexcept(_mask: i32) -> i32 {
     0
 }
 
 #[inline]
-pub fn feraiseexcept(_mask: i32) -> i32 {
+pub(crate) fn feraiseexcept(_mask: i32) -> i32 {
     0
 }
 
 #[inline]
-pub fn fetestexcept(_mask: i32) -> i32 {
+pub(crate) fn fetestexcept(_mask: i32) -> i32 {
     0
 }
 
 #[inline]
-pub fn fegetround() -> i32 {
+pub(crate) fn fegetround() -> i32 {
     FE_TONEAREST
 }
 
 #[inline]
-pub fn fesetround(_r: i32) -> i32 {
+pub(crate) fn fesetround(_r: i32) -> i32 {
     0
 }
