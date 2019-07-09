557c81b3c6e4312e1e875e5256ecc5b3867d5777
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-09 08:44:46 +0200

    Update tests wrt. recovery of range patterns.

--- src/test/ui/exclusive-range/exclusive_range_pattern_syntax_collision2.rs
+++ src/test/ui/exclusive-range/exclusive_range_pattern_syntax_collision2.rs
@@ -2,7 +2,11 @@
 
 fn main() {
     match [5..4, 99..105, 43..44] {
-        [_, 99..] => {}, //~ ERROR unexpected token: `]`
+        [_, 99..] => {},
+        //~^ ERROR `X..` range patterns are not supported
+        //~| ERROR arbitrary expressions aren't allowed in patterns
+        //~| ERROR pattern requires 2 elements but array has 3
+        //~| ERROR only char and numeric types are allowed in range patterns
         _ => {},
     }
 }
