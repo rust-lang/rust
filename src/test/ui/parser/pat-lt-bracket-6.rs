8a481efc8b2eb34c142171169485b0e65eaec859
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-09 06:30:03 +0200

    Account for better recovery in two cases.

--- src/test/ui/parser/pat-lt-bracket-6.rs
+++ src/test/ui/parser/pat-lt-bracket-6.rs
@@ -1,3 +1,6 @@
 fn main() {
     let Test(&desc[..]) = x; //~ ERROR: expected one of `)`, `,`, or `@`, found `[`
+    //~^ ERROR cannot find value `x` in this scope
+    //~| ERROR cannot find tuple struct/variant `Test` in this scope
+    //~| ERROR subslice patterns are unstable
 }
