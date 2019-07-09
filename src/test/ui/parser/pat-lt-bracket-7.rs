06e5ae5c825f79c4f0fa7c0cbcf43f74751fc7d4
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-09 06:30:03 +0200

    Account for better recovery in two cases.

--- src/test/ui/parser/pat-lt-bracket-7.rs
+++ src/test/ui/parser/pat-lt-bracket-7.rs
@@ -1,3 +1,5 @@
 fn main() {
     for thing(x[]) in foo {} //~ ERROR: expected one of `)`, `,`, or `@`, found `[`
+    //~^ ERROR cannot find value `foo` in this scope
+    //~| ERROR cannot find tuple struct/variant `thing` in this scope
 }
