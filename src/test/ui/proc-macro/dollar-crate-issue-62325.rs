4cb67c0f1c429a1a1e2ec8c3e02714557feeb60d
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2019-07-09 22:21:55 +0300

    Add a test case with `$crate` from other crate

--- src/test/ui/proc-macro/dollar-crate-issue-62325.rs
+++ src/test/ui/proc-macro/dollar-crate-issue-62325.rs
@@ -1,6 +1,7 @@
 // check-pass
 // edition:2018
 // aux-build:test-macros.rs
+// aux-build:dollar-crate-external.rs
 
 // Anonymize unstable non-dummy spans while still showing dummy spans `0..0`.
 // normalize-stdout-test "bytes\([^0]\w*\.\.(\w+)\)" -> "bytes(LO..$1)"
@@ -10,6 +11,7 @@
 
 #[macro_use]
 extern crate test_macros;
+extern crate dollar_crate_external;
 
 type S = u8;
 
@@ -19,5 +21,7 @@ macro_rules! m { () => {
 }}
 
 m!();
+
+dollar_crate_external::issue_62325!();
 
 fn main() {}
