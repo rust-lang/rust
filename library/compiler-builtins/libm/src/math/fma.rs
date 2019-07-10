42e0c8c0a1cabb24b48f56470aea1656a47f9cc7
gnzlbg <gonzalobg88@gmail.com>
2019-07-10 12:59:36 +0200

    Add FMA tests that cause it to segfault

--- library/compiler-builtins/libm/src/math/fma.rs
+++ library/compiler-builtins/libm/src/math/fma.rs
@@ -205,3 +205,22 @@ pub fn fma(x: f64, y: f64, z: f64) -> f64 {
     }
     scalbn(r, e)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn fma_segfault() {
+        // These two inputs cause fma to segfault on release due to overflow:
+        assert_eq!(
+            fma(
+                -0.0000000000000002220446049250313,
+                -0.0000000000000002220446049250313,
+                -0.0000000000000002220446049250313
+            ),
+            -0.00000000000000022204460492503126,
+        );
+
+        assert_eq!(fma(-0.992, -0.992, -0.992), -0.00793599999988632,);
+    }
+}
