0f66ce65dbdef43e8459a3bc771d3fe05ad2063d
Delan Azabani <delan@azabani.com>
2019-07-12 13:32:39 +1000

    test E0133 when calling free/impl `async unsafe fn` in safe code

---
+++ src/test/ui/async-await/async-unsafe-fn-call-in-safe.rs
@@ -0,0 +1,16 @@
+// edition:2018
+
+#![feature(async_await, async_closure)]
+
+struct S;
+
+impl S {
+    async unsafe fn f() {}
+}
+
+async unsafe fn f() {}
+
+fn main() {
+    S::f(); //~ ERROR call to unsafe function is unsafe
+    f(); //~ ERROR call to unsafe function is unsafe
+}
