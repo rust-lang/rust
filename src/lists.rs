71289e1d230d19f223e45b0abe75ae976f567900
rChaser53 <tayoshizawa29@gmail.com>
2019-07-14 22:16:47 +0900

    fix 'extra comma inserted due to comment' (#3677)

--- src/lists.rs
+++ src/lists.rs
@@ -612,7 +612,11 @@ pub(crate) fn extract_post_comment(
         post_snippet[1..].trim_matches(white_space)
     } else if post_snippet.starts_with(separator) {
         post_snippet[separator.len()..].trim_matches(white_space)
-    } else if post_snippet.ends_with(',') && !post_snippet.trim().starts_with("//") {
+    }
+    // not comment or over two lines
+    else if post_snippet.ends_with(',')
+        && (!post_snippet.trim().starts_with("//") || post_snippet.trim().contains('\n'))
+    {
         post_snippet[..(post_snippet.len() - 1)].trim_matches(white_space)
     } else {
         post_snippet
