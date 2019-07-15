a4a7bb9a3f72505efb6ca9856af45287c8685d5e
Matthew Jasper <mjjasper1@gmail.com>
2019-07-15 22:26:45 +0100

    Make pretty-expanded-hygiene a `ui` test
    `normalize-stdout-test` removes the need for Make, and it can be updated
    with `--bless` this way

--- src/test/ui/hygiene/unpretty-debug.rs
+++ src/test/ui/hygiene/unpretty-debug.rs
@@ -1,3 +1,9 @@
+// check-pass
+// compile-flags: -Zunpretty=expanded,hygiene
+
+// Don't break whenever Symbol numbering changes
+// normalize-stdout-test "\d+#" -> "0#"
+
 // minimal junk
 #![feature(no_core)]
 #![no_core]
