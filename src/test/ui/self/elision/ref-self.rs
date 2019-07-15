f916aa995a40e4b23712f5f176eef287b2fa09cd
Taiki Endo <te316e89@gmail.com>
2019-07-16 01:09:25 +0900

    Add test for multiple ref-self

--- src/test/ui/self/elision/ref-self.rs
+++ src/test/ui/self/elision/ref-self.rs
@@ -1,9 +1,18 @@
 #![feature(arbitrary_self_types)]
 #![allow(non_snake_case)]
 
+use std::marker::PhantomData;
+use std::ops::Deref;
 use std::pin::Pin;
 
 struct Struct { }
+
+struct Wrap<T, P>(T, PhantomData<P>);
+
+impl<T, P> Deref for Wrap<T, P> {
+    type Target = T;
+    fn deref(&self) -> &T { &self.0 }
+}
 
 impl Struct {
     // Test using `&self` sugar:
@@ -31,6 +40,10 @@ impl Struct {
     }
 
     fn box_pin_ref_Self(self: Box<Pin<&Self>>, f: &u32) -> &u32 {
+        f //~ ERROR lifetime mismatch
+    }
+
+    fn wrap_ref_Self_Self(self: Wrap<&Self, Self>, f: &u8) -> &u8 {
         f //~ ERROR lifetime mismatch
     }
 }
