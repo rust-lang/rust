aab9edc68ab034f94bc956e145d155eb6d26fb3d
Taiki Endo <te316e89@gmail.com>
2019-07-16 00:48:28 +0900

    Minor clean up

--- src/test/ui/self/elision/struct.rs
+++ src/test/ui/self/elision/struct.rs
@@ -8,26 +8,24 @@ use std::rc::Rc;
 struct Struct { }
 
 impl Struct {
-    // Test using `&mut Struct` explicitly:
-
     fn ref_Struct(self: Struct, f: &u32) -> &u32 {
-        f //~ ERROR lifetime mismatch
+        f
     }
 
     fn box_Struct(self: Box<Struct>, f: &u32) -> &u32 {
-        f //~ ERROR lifetime mismatch
+        f
     }
 
     fn rc_Struct(self: Rc<Struct>, f: &u32) -> &u32 {
-        f //~ ERROR lifetime mismatch
+        f
     }
 
     fn box_box_Struct(self: Box<Box<Struct>>, f: &u32) -> &u32 {
-        f //~ ERROR lifetime mismatch
+        f
     }
 
     fn box_rc_Struct(self: Box<Rc<Struct>>, f: &u32) -> &u32 {
-        f //~ ERROR lifetime mismatch
+        f
     }
 }
 
