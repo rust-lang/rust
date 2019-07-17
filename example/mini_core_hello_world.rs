834a3bf49c9e6cc422a0028926654d46dc5749ba
bjorn3 <bjorn3@users.noreply.github.com>
2019-07-17 20:45:54 +0200

    [WIP]

--- example/mini_core_hello_world.rs
+++ example/mini_core_hello_world.rs
@@ -117,22 +117,123 @@ impl<T: ?Sized, U: ?Sized> CoerceUnsized<Unique<U>> for Unique<T> where T: Unsiz
 fn take_f32(_f: f32) {}
 fn take_unique(_u: Unique<()>) {}
 
-fn checked_div_i128(lhs: i128, rhs: i128) -> Option<i128> {
-    if rhs == 0 || (lhs == -170141183460469231731687303715884105728 && rhs == -1) {
-        None
-    } else {
-        Some(unsafe { intrinsics::unchecked_div(lhs, rhs) })
-    }
-}
-
-fn checked_div_u128(lhs: u128, rhs: u128) -> Option<u128> {
-    match rhs {
-        0 => None,
-        rhs => Some(unsafe { intrinsics::unchecked_div(lhs, rhs) })
-    }
-}
-
 fn main() {
-    checked_div_i128(0i128, 2i128);
-    checked_div_u128(0u128, 2u128);
+    take_unique(Unique {
+        pointer: 0 as *const (),
+        _marker: PhantomData,
+    });
+    take_f32(0.1);
+
+    //return;
+
+    unsafe {
+        printf("Hello %s\n\0" as *const str as *const i8, "printf\0" as *const str as *const i8);
+
+        let hello: &[u8] = b"Hello\0" as &[u8; 6];
+        let ptr: *const u8 = hello as *const [u8] as *const u8;
+        puts(ptr);
+
+        let world: Box<&str> = box "World!\0";
+        puts(*world as *const str as *const u8);
+        world as Box<SomeTrait>;
+
+        assert_eq!(intrinsics::bitreverse(0b10101000u8), 0b00010101u8);
+
+        assert_eq!(intrinsics::bswap(0xabu8), 0xabu8);
+        assert_eq!(intrinsics::bswap(0xddccu16), 0xccddu16);
+        assert_eq!(intrinsics::bswap(0xffee_ddccu32), 0xccdd_eeffu32);
+        assert_eq!(intrinsics::bswap(0x1234_5678_ffee_ddccu64), 0xccdd_eeff_7856_3412u64);
+
+        assert_eq!(intrinsics::size_of_val(hello) as u8, 6);
+
+        let chars = &['C', 'h', 'a', 'r', 's'];
+        let chars = chars as &[char];
+        assert_eq!(intrinsics::size_of_val(chars) as u8, 4 * 5);
+
+        let a: &dyn SomeTrait = &"abc\0";
+        a.object_safe();
+
+        assert_eq!(intrinsics::size_of_val(a) as u8, 16);
+        assert_eq!(intrinsics::size_of_val(&0u32) as u8, 4);
+
+        assert_eq!(intrinsics::min_align_of::<u16>() as u8, 2);
+        assert_eq!(intrinsics::min_align_of_val(&a) as u8, intrinsics::min_align_of::<&str>() as u8);
+
+        assert!(!intrinsics::needs_drop::<u8>());
+        assert!(intrinsics::needs_drop::<NoisyDrop>());
+
+        Unique {
+            pointer: 0 as *const &str,
+            _marker: PhantomData,
+        } as Unique<dyn SomeTrait>;
+
+        struct MyDst<T: ?Sized>(T);
+
+        intrinsics::size_of_val(&MyDst([0u8; 4]) as &MyDst<[u8]>);
+
+        struct Foo {
+            x: u8,
+            y: !,
+        }
+
+        unsafe fn zeroed<T>() -> T {
+            intrinsics::init::<T>()
+        }
+
+        unsafe fn uninitialized<T>() -> T {
+            MaybeUninit { uninit: () }.value
+        }
+
+        zeroed::<(u8, u8)>();
+        #[allow(unreachable_code)]
+        {
+            if false {
+                zeroed::<!>();
+                zeroed::<Foo>();
+                uninitialized::<Foo>();
+            }
+        }
+    }
+
+    let _ = box NoisyDrop {
+        text: "Boxed outer got dropped!\0",
+        inner: NoisyDropInner,
+    } as Box<SomeTrait>;
+
+    const FUNC_REF: Option<fn()> = Some(main);
+    match FUNC_REF {
+        Some(_) => {},
+        None => assert!(false),
+    }
+
+    match Ordering::Less {
+        Ordering::Less => {},
+        _ => assert!(false),
+    }
+
+    [NoisyDropInner, NoisyDropInner];
+
+    let x = &[0u32, 42u32] as &[u32];
+    match x {
+        [] => assert_eq!(0u32, 1),
+        [_, ref y..] => assert_eq!(&x[1] as *const u32 as usize, &y[0] as *const u32 as usize),
+    }
+
+    assert_eq!(((|()| 42u8) as fn(()) -> u8)(()), 42);
+
+    extern {
+        #[linkage = "weak"]
+        static ABC: *const u8;
+    }
+
+    {
+        extern {
+            #[linkage = "weak"]
+            static ABC: *const u8;
+        }
+    }
+
+    unsafe { assert_eq!(ABC as usize, 0); }
+
+    &mut (|| Some(0 as *const ())) as &mut FnMut() -> Option<*const ()>;
 }
--- example/std_example.rs
+++ example/std_example.rs
@@ -1,7 +1,25 @@
 #![feature(core_intrinsics)]
 
 use std::io::Write;
+use std::intrinsics;
+
+fn checked_div_i128(lhs: i128, rhs: i128) -> Option<i128> {
+    if rhs == 0 || (lhs == -170141183460469231731687303715884105728 && rhs == -1) {
+        None
+    } else {
+        Some(unsafe { intrinsics::unchecked_div(lhs, rhs) })
+    }
+}
+
+fn checked_div_u128(lhs: u128, rhs: u128) -> Option<u128> {
+    match rhs {
+        0 => None,
+        rhs => Some(unsafe { intrinsics::unchecked_div(lhs, rhs) })
+    }
+}
 
 fn main() {
+    checked_div_i128(0i128, 2i128);
+    checked_div_u128(0u128, 2u128);
     assert_eq!(1u128 + 2, 3);
 }
