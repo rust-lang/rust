2cc373ac54e7d244cea22b305cefc1c2d08d81f6
uHOOCCOOHu <hooccooh1896@gmail.com>
2019-07-19 23:03:34 +0800

    Fix fmt

--- tests/ui/issue_4266.rs
+++ tests/ui/issue_4266.rs
@@ -5,19 +5,33 @@
 async fn sink1<'a>(_: &'a str) {} // lint
 async fn sink1_elided(_: &str) {} // ok
 
-async fn one_to_one<'a>(s: &'a str) -> &'a str { s } // lint
-async fn one_to_one_elided(s: &str) -> &str { s } // ok
-async fn all_to_one<'a>(a: &'a str, _b: &'a str) -> &'a str { a } // ok
+// lint
+async fn one_to_one<'a>(s: &'a str) -> &'a str {
+    s
+}
+
+// ok
+async fn one_to_one_elided(s: &str) -> &str {
+    s
+}
+
+// ok
+async fn all_to_one<'a>(a: &'a str, _b: &'a str) -> &'a str {
+    a
+}
+
 // async fn unrelated(_: &str, _: &str) {} // Not allowed in async fn
 
 // #3988
 struct Foo;
 impl Foo {
-    pub async fn foo(&mut self) {} // ok
+    // ok
+    pub async fn foo(&mut self) {}
 }
 
 // rust-lang/rust#61115
-async fn print(s: &str) { // ok
+// ok
+async fn print(s: &str) {
     println!("{}", s);
 }
 
