fe2b5bbe6d151ae4713b3bddb4b386f94757435c
Esteban Küber <esteban@kuber.com.ar>
2019-07-23 12:51:34 -0700

    review comments

--- src/libsyntax/parse/parser.rs
+++ src/libsyntax/parse/parser.rs
@@ -7672,7 +7672,8 @@ impl<'a> Parser<'a> {
             &mut self.token_cursor.frame.last_token
         } else if self.token_cursor.stack.get(prev).is_none() {
             // This can happen due to a bad interaction of two unrelated recovery mechanisms with
-            // mismatched delimiters *and* recovery lookahead on `pub ident(` likely typo (#62881)
+            // mismatched delimiters *and* recovery lookahead on the likely typo `pub ident(`
+            // (#62881).
             return Ok((ret?, TokenStream::new(vec![])));
         } else {
             &mut self.token_cursor.stack[prev].last_token
@@ -7686,8 +7687,8 @@ impl<'a> Parser<'a> {
                 debug!("collect_tokens: {}", msg);
                 self.sess.span_diagnostic.delay_span_bug(self.token.span, &msg);
                 // This can happen due to a bad interaction of two unrelated recovery mechanisms
-                // with mismatched delimiters *and* recovery lookahead on `pub ident(` likely typo
-                // (#62895, different but similar to the case above)
+                // with mismatched delimiters *and* recovery lookahead on the likely typo
+                // `pub ident(` (#62895, different but similar to the case above).
                 return Ok((ret?, TokenStream::new(vec![])));
             }
         };
