7fdfe8b85476f5178df651dd836d556c2bf7917f
Mazdak Farrokhzad <twingoow@gmail.com>
2019-07-24 11:36:17 +0200

    Refer to #50072 re. hack.

--- src/test/ui/type-alias-enum-variants/self-in-enum-definition.rs
+++ src/test/ui/type-alias-enum-variants/self-in-enum-definition.rs
@@ -1,7 +1,7 @@
 #[repr(u8)]
 enum Alpha {
     V1 = 41,
-    V2 = Self::V1 as u8 + 1, // OK -- but why?
+    V2 = Self::V1 as u8 + 1, // OK; See #50072.
     V3 = Self::V1 {} as u8 + 2, //~ ERROR cycle detected when const-evaluating
 }
 
