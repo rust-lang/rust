9cfda48906c14fe12f5e51820427be5bb95283b4
Ralf Jung <post@ralfj.de>
2019-07-25 00:06:47 +0200

    improve error when CTFE does ptr-int-cast; update tests

--- src/test/ui/consts/const-eval/issue-52442.rs
+++ src/test/ui/consts/const-eval/issue-52442.rs
@@ -1,5 +1,5 @@
 fn main() {
     [();  { &loop { break } as *const _ as usize } ];
     //~^ ERROR casting pointers to integers in constants is unstable
-    //~| ERROR it is undefined behavior to use this value
+    //~| ERROR evaluation of constant value failed
 }
