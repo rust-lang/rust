18bf9dd4b7e9834bc6981ee413f025f3b2f7e386
Aaron Hill <aa1ronham@gmail.com>
2019-07-28 17:57:04 -0400

    Properly check the defining scope of existential types
    Fixes #52632
    
    Existential types (soon to be 'impl trait' aliases) can either be
    delcared at a top-level crate/module scope, or within another item such
    as an fn. Previously, we were handling the second case incorrectly when
    recursively searching for defining usages - we would check children of
    the item, but not the item itself. This lead to us missing closures
    that consituted a defining use of the existential type, as their opaque
    type instantiations are stored in the TypeckTables of their parent
    function.
    
    This commit ensures that we explicitly visit the defining item itself,
    not just its children.

---
+++ src/test/ui/existential-type/issue-52843.rs
@@ -0,0 +1,11 @@
+#![feature(existential_type)]
+
+use std::fmt::Debug;
+
+fn main() {
+    existential type Existential: Debug;
+    fn _unused() -> Existential { String::new() }
+    //~^ ERROR: concrete type differs from previous defining existential type use
+    let null = || -> Existential { 0 };
+    println!("{:?}", null());
+}
