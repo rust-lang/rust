0cf43291d7007fa8edc31e0d7dda84acc453d86d
Ralf Jung <post@ralfj.de>
2019-07-29 15:34:40 +0200

    also test error messages

--- src/test/ui/consts/const-eval/ub-ref.rs
+++ src/test/ui/consts/const-eval/ub-ref.rs
@@ -1,3 +1,4 @@
+// ignore-tidy-linelength
 #![feature(const_transmute)]
 #![allow(const_err)] // make sure we cannot allow away the errors tested here
 
@@ -5,6 +6,7 @@ use std::mem;
 
 const UNALIGNED: &u16 = unsafe { mem::transmute(&[0u8; 4]) };
 //~^ ERROR it is undefined behavior to use this value
+//~^^ type validation failed: encountered unaligned reference (required 2 byte alignment but found 1)
 
 const NULL: &u16 = unsafe { mem::transmute(0usize) };
 //~^ ERROR it is undefined behavior to use this value
