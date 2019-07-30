7336626ea5bcc83b8fd3ef63cf1f40af558076c3
eupn <36292692+eupn@users.noreply.github.com>
2019-07-30 13:34:58 +0700

    Apply suggestions from review by varkor
    Co-Authored-By: varkor <github@varkor.com>

--- src/librustdoc/core.rs
+++ src/librustdoc/core.rs
@@ -402,7 +402,7 @@ pub fn run_core(options: RustdocOptions) -> (clean::Crate, RenderInfo, RenderOpt
             fn report_deprecated_attr(name: &str, diag: &errors::Handler) {
                 let mut msg = diag.struct_warn(&format!("the `#![doc({})]` attribute is \
                                                          considered deprecated", name));
-                msg.warn("please see issue \
+                msg.warn("see issue \
                     #44136 <https://github.com/rust-lang/rust/issues/44136>");
 
                 if name == "no_default_passes" {
