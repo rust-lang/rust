1c11d7b1d87501687cda5efae9188e883ea3b92c
Roman Stoliar <rizakrko@rambler.ru>
2019-07-30 03:46:38 +0300

    Hide comments in rust section of doc comments

--- crates/ra_lsp_server/src/markdown.rs
+++ crates/ra_lsp_server/src/markdown.rs
@@ -1,7 +1,11 @@
-pub(crate) fn mark_fenced_blocks_as_rust(src: &str) -> String {
+pub(crate) fn format_docs(src: &str) -> String {
     let mut processed_lines = Vec::new();
     let mut in_code_block = false;
     for line in src.lines() {
+        if in_code_block && line.trim_start().starts_with("# ") {
+            continue;
+        }
+
         if line.starts_with("```") {
             in_code_block ^= true
         }
@@ -22,8 +26,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_codeblock_adds_rust() {
+    fn test_format_docs_adds_rust() {
         let comment = "```\nfn some_rust() {}\n```";
-        assert_eq!(mark_fenced_blocks_as_rust(comment), "```rust\nfn some_rust() {}\n```");
+        assert_eq!(format_docs(comment), "```rust\nfn some_rust() {}\n```");
+    }
+
+    #[test]
+    fn test_format_docs_skips_comments_in_rust_block() {
+        let comment = "```rust\n # skip1\n# skip2\n#stay1\nstay2\n```";
+        assert_eq!(format_docs(comment), "```rust\n#stay1\nstay2\n```");
+    }
+
+    #[test]
+    fn test_format_docs_keeps_comments_outside_of_rust_block() {
+        let comment = " # stay1\n# stay2\n#stay3\nstay4";
+        assert_eq!(format_docs(comment), comment);
     }
 }
