ca0ef0fcf66b0fe913c19a3a00729af4494866e6
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2019-08-01 03:35:26 +0300

    librustc: Unconfigure tests during normal build

--- src/librustc/lib.rs
+++ src/librustc/lib.rs
@@ -80,6 +80,9 @@ extern crate libc;
 // librustc_driver and libtest.
 extern crate test as _;
 
+#[cfg(test)]
+mod tests;
+
 #[macro_use]
 mod macros;
 
@@ -137,19 +140,6 @@ pub mod util {
 
 // Allows macros to refer to this crate as `::rustc`
 extern crate self as rustc;
-
-// FIXME(#27438): right now the unit tests of librustc don't refer to any actual
-//                functions generated in librustc_data_structures (all
-//                references are through generic functions), but statics are
-//                referenced from time to time. Due to this bug we won't
-//                actually correctly link in the statics unless we also
-//                reference a function, so be sure to reference a dummy
-//                function.
-#[test]
-fn noop() {
-    rustc_data_structures::__noop_fix_for_27438();
-}
-
 
 // Build the diagnostics array at the end so that the metadata includes error use sites.
 __build_diagnostic_array! { librustc, DIAGNOSTICS }
