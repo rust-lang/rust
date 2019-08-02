6a14411b7633ecaf70dc9ae0112678487a9b0d64
Mazdak Farrokhzad <twingoow@gmail.com>
2019-08-02 03:45:29 +0200

    Remove async_await feature gate tests.

--- src/test/ui/feature-gates/feature-gate-async-await-2015-edition.rs
+++
@@ -1,9 +0,0 @@
-// edition:2015
-
-async fn foo() {} //~ ERROR `async fn` is not permitted in the 2015 edition
-                  //~^ ERROR async fn is unstable
-
-fn main() {
-    let _ = async {}; //~ ERROR cannot find struct, variant or union type `async`
-    let _ = async || { true }; //~ ERROR cannot find value `async` in this scope
-}
