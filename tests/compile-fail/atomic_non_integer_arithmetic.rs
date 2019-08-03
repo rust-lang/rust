a41ec9aacbdc4798f9efeb8b24fc12d26e04bf7d
Ralf Jung <post@ralfj.de>
2019-08-03 20:54:57 +0200

    adjust error messages in tests

--- tests/compile-fail/atomic_non_integer_arithmetic.rs
+++
@@ -1,9 +0,0 @@
-#![feature(core_intrinsics)]
-
-pub fn main() {
-    let mut z: f64 = 1.0;
-    unsafe {
-        ::std::intrinsics::atomic_xadd(&mut z, 2.0);
-        //~^ ERROR: Atomic arithmetic operations only work on integer types
-    }
-}
