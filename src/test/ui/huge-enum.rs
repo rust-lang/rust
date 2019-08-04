f621f890a6e427b6074f0a8561fe1c95feff743d
Esteban Küber <esteban@kuber.com.ar>
2019-08-04 13:14:53 -0700

    revert change to single test

--- src/test/ui/huge-enum.rs
+++ src/test/ui/huge-enum.rs
@@ -1,3 +1,6 @@
+// normalize-stderr-test "std::option::Option<\[u32; \d+\]>" -> "TYPE"
+// normalize-stderr-test "\[u32; \d+\]" -> "TYPE"
+
 // FIXME https://github.com/rust-lang/rust/issues/59774
 // normalize-stderr-test "thread.*panicked.*Metadata module not compiled.*\n" -> ""
 // normalize-stderr-test "note:.*RUST_BACKTRACE=1.*\n" -> ""
