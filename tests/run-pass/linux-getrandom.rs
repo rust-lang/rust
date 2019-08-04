6b087d25362a3461f76cdee5a62fb76d2d8023fd
Aaron Hill <aa1ronham@gmail.com>
2019-08-04 15:53:49 -0400

    Add test

---
+++ tests/run-pass/linux-getrandom.rs
@@ -0,0 +1,12 @@
+// only-linux: Uses Linux-only APIs
+
+#![feature(rustc_private)]
+extern crate libc;
+
+fn main() {
+    let mut buf = [0u8; 5];
+    unsafe {
+		assert_eq!(libc::syscall(libc::SYS_getrandom, 0 as *mut libc::c_void, 0 as libc::size_t, 0 as libc::c_uint), 0);
+        assert_eq!(libc::syscall(libc::SYS_getrandom, buf.as_mut_ptr() as *mut libc::c_void, 5 as libc::size_t, 0 as libc::c_uint), 5);
+    }
+}
