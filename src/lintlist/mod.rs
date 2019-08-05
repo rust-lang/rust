a922f800aff150b1b2b8093ff54df40736b82332
flip1995 <hello@philkrones.com>
2019-08-05 13:24:31 +0200

    Run update_lints and fmt

--- src/lintlist/mod.rs
+++ src/lintlist/mod.rs
@@ -6,7 +6,7 @@ pub use lint::Lint;
 pub use lint::LINT_LEVELS;
 
 // begin lint list, do not remove this comment, it’s used in `update_lints`
-pub const ALL_LINTS: [Lint; 309] = [
+pub const ALL_LINTS: [Lint; 310] = [
     Lint {
         name: "absurd_extreme_comparisons",
         group: "correctness",
