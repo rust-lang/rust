5c45420bdad8d3bf950ff3c46e1856348a7ddc7b
Wesley Wiser <wwiser@gmail.com>
2019-08-04 22:23:32 -0400

    Changed tests

--- src/test/ui/consts/array-literal-index-oob.rs
+++ src/test/ui/consts/array-literal-index-oob.rs
@@ -2,5 +2,4 @@ fn main() {
     &{[1, 2, 3][4]};
     //~^ ERROR index out of bounds
     //~| ERROR reaching this expression at runtime will panic or abort
-    //~| ERROR this expression will panic at runtime
 }
