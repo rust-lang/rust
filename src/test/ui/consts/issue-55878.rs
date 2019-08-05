ce8510a21445c1443d2657968c474dc658a0abf9
Esteban Küber <esteban@kuber.com.ar>
2019-08-04 21:18:05 -0700

    fix tests

--- src/test/ui/consts/issue-55878.rs
+++ src/test/ui/consts/issue-55878.rs
@@ -1,4 +1,7 @@
-// error-pattern: the type `[u8; 18446744073709551615]` is too big for the current architecture
+// normalize-stderr-64bit "18446744073709551615" -> "SIZE"
+// normalize-stderr-32bit "4294967295" -> "SIZE"
+
+// error-pattern: is too big for the current architecture
 fn main() {
     println!("Size: {}", std::mem::size_of::<[u8; std::u64::MAX as usize]>());
 }
