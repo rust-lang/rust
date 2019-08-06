64469ac344114197815b835c98fa37408ef74c00
Esteban Küber <esteban@kuber.com.ar>
2019-08-06 11:23:32 -0700

    review comments

--- src/test/ui/huge-array-simple-64.rs
+++ src/test/ui/huge-array-simple-64.rs
@@ -1,4 +1,4 @@
-// ignore-64bit
+// ignore-32bit
 
 // FIXME https://github.com/rust-lang/rust/issues/59774
 // normalize-stderr-test "thread.*panicked.*Metadata module not compiled.*\n" -> ""
@@ -6,6 +6,6 @@
 #![allow(exceeding_bitshifts)]
 
 fn main() {
-    let _fat: [u8; (1<<31)+(1<<15)] = //~ ERROR too big for the current architecture
-        [0; (1u32<<31) as usize +(1u32<<15) as usize];
+    let _fat: [u8; (1<<61)+(1<<31)] = //~ ERROR too big for the current architecture
+        [0; (1u64<<61) as usize +(1u64<<31) as usize];
 }
--- src/test/ui/issues/issue-15919-64.rs
+++ src/test/ui/issues/issue-15919-64.rs
@@ -7,3 +7,6 @@
 fn main() {
     let x = [0usize; 0xffff_ffff_ffff_ffff]; //~ ERROR too big
 }
+
+// These two tests need to have different literals, as we can't rely on conditional compilation
+// for them while retaining the same spans/lines.
