3144b0aa04e9a1857cfb50ba556f4634c56a81e7
Esteban Küber <esteban@kuber.com.ar>
2019-08-06 16:51:17 -0700

    review comment: reword test comment

--- src/test/ui/issues/issue-15919-32.rs
+++ src/test/ui/issues/issue-15919-32.rs
@@ -8,5 +8,5 @@ fn main() {
     let x = [0usize; 0xffff_ffff]; //~ ERROR too big
 }
 
-// These two tests need to have different literals, as we can't rely on conditional compilation
-// for them while retaining the same spans/lines.
+// This and the -64 version of this test need to have different literals, as we can't rely on
+// conditional compilation for them while retaining the same spans/lines.
