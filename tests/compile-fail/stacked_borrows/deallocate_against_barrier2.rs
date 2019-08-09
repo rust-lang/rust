b93629262dc66361387fd045e53bd01e9e45311b
Ralf Jung <post@ralfj.de>
2019-08-09 19:21:41 +0200

    test that even &Cell must be dereferencable

---
+++ tests/compile-fail/stacked_borrows/deallocate_against_barrier2.rs
@@ -0,0 +1,17 @@
+// error-pattern: deallocating while item is protected
+
+use std::cell::Cell;
+
+// Check that even `&Cell` are dereferencable.
+// Also see <https://github.com/rust-lang/rust/issues/55005>.
+fn inner(x: &Cell<i32>, f: fn(&Cell<i32>)) {
+    // `f` may mutate, but it may not deallocate!
+    f(x)
+}
+
+fn main() {
+    inner(Box::leak(Box::new(Cell::new(0))), |x| {
+        let raw = x as *const _ as *mut Cell<i32>;
+        drop(unsafe { Box::from_raw(raw) });
+    });
+}
