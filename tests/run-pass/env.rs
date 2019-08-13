f451fe21bdae10709b98406544c61689e1691163
Christian Poveda <christianpoveda@protonmail.com>
2019-08-13 16:17:53 -0500

    Test env isolation

--- tests/run-pass/env.rs
+++ tests/run-pass/env.rs
@@ -6,4 +6,6 @@ fn main() {
     assert_eq!(env::var("MIRI_TEST"), Err(env::VarError::NotPresent));
     env::set_var("MIRI_TEST", "the answer");
     assert_eq!(env::var("MIRI_TEST"), Ok("the answer".to_owned()));
+    // Test that miri environment is isolated when communication is disabled.
+    assert!(env::var("MIRI_ENV_VAR_TEST").is_err());
 }
