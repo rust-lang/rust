2fe5e2cea91030737f38df81e5f82e652927491f
Jeremy Stucki <stucki.jeremy@gmail.com>
2019-08-13 21:50:52 +0200

    Update test

--- tests/ui/unnecessary_flat_map.rs
+++ tests/ui/unnecessary_flat_map.rs
@@ -1,13 +1,14 @@
 // run-rustfix
 
+#![allow(unused_imports)]
 #![warn(clippy::flat_map_identity)]
 
 use std::convert;
 
 fn main() {
     let iterator = [[0, 1], [2, 3], [4, 5]].iter();
-    iterator.flat_map(|x| x);
+    let _ = iterator.flat_map(|x| x);
 
     let iterator = [[0, 1], [2, 3], [4, 5]].iter();
-    iterator.flat_map(convert::identity);
+    let _ = iterator.flat_map(convert::identity);
 }
