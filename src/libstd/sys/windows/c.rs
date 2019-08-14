f584f92071e8a7a1e825a2a0486fae024149ad06
newpavlov <newpavlov@gmail.com>
2019-08-14 19:39:12 +0300

    remove unused extern fns

--- src/libstd/sys/windows/c.rs
+++ src/libstd/sys/windows/c.rs
@@ -664,8 +664,6 @@ if #[cfg(not(target_vendor = "uwp"))] {
     pub const TOKEN_READ: DWORD = 0x20008;
 
     extern "system" {
-        #[link_name = "SystemFunction036"]
-        pub fn RtlGenRandom(RandomBuffer: *mut u8, RandomBufferLength: ULONG) -> BOOLEAN;
 
         pub fn ReadConsoleW(hConsoleInput: HANDLE,
                             lpBuffer: LPVOID,
@@ -724,8 +722,6 @@ if #[cfg(target_vendor = "uwp")] {
                                             fileInfoClass: FILE_INFO_BY_HANDLE_CLASS,
                                             lpFileInformation: LPVOID,
                                             dwBufferSize: DWORD) -> BOOL;
-        pub fn BCryptGenRandom(hAlgorithm: LPVOID, pBuffer: *mut u8,
-                               cbBuffer: ULONG, dwFlags: ULONG) -> LONG;
     }
 }
 }
