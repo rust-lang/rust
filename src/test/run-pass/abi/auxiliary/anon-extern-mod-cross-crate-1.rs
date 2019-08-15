4a8ff5bb7c31c90a5be3e4d6a2e7e7211b4c8745
Kevin Per <kevin.per@protonmail.com>
2019-08-15 17:07:38 +0200

    Remove `run-pass` directory #62593

--- src/test/run-pass/abi/auxiliary/anon-extern-mod-cross-crate-1.rs
+++
@@ -1,9 +0,0 @@
-#![crate_name="anonexternmod"]
-#![feature(rustc_private)]
-
-extern crate libc;
-
-#[link(name = "rust_test_helpers", kind = "static")]
-extern {
-    pub fn rust_get_test_int() -> libc::intptr_t;
-}
