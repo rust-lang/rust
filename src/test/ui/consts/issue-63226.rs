e688352d2b6f4ad6fc931b84ff87b124433305af
Yuki Okushi <huyuumi.dev@gmail.com>
2019-08-15 16:59:03 +0900

    Adjust regression test

--- src/test/ui/consts/issue-63226.rs
+++ src/test/ui/consts/issue-63226.rs
@@ -2,6 +2,8 @@
 // compile-flags:--extern issue_63226
 // edition:2018
 // build-pass
+// A regression test for issue #63226.
+// Checks if `const fn` is marked as reachable.
 
 use issue_63226::VTable;
 
