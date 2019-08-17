72d9fe8b0e4940b4314f190dd14235fa83046338
Ralf Jung <post@ralfj.de>
2019-08-17 16:48:08 +0200

    less &

--- src/librustc_lint/builtin.rs
+++ src/librustc_lint/builtin.rs
@@ -1914,13 +1914,13 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for InvalidValue {
                 if let hir::ExprKind::Path(ref qpath) = path_expr.node {
                     let def_id = cx.tables.qpath_res(qpath, path_expr.hir_id).opt_def_id()?;
 
-                    if cx.match_def_path(def_id, &ZEROED_PATH) {
+                    if cx.match_def_path(def_id, ZEROED_PATH) {
                         return Some(InitKind::Zeroed);
                     }
-                    if cx.match_def_path(def_id, &UININIT_PATH) {
+                    if cx.match_def_path(def_id, UININIT_PATH) {
                         return Some(InitKind::Uninit);
                     }
-                    if cx.match_def_path(def_id, &TRANSMUTE_PATH) {
+                    if cx.match_def_path(def_id, TRANSMUTE_PATH) {
                         if is_zero(&args[0]) {
                             return Some(InitKind::Zeroed);
                         }
