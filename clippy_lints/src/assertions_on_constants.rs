439284b58400be98036117d053815eb24472fee7
Lzu Tao <taolzu@gmail.com>
2019-08-18 01:46:44 +0700

    Make code cleaner and more readable

--- clippy_lints/src/assertions_on_constants.rs
+++ clippy_lints/src/assertions_on_constants.rs
@@ -34,39 +34,37 @@ declare_lint_pass!(AssertionsOnConstants => [ASSERTIONS_ON_CONSTANTS]);
 impl<'a, 'tcx> LateLintPass<'a, 'tcx> for AssertionsOnConstants {
     fn check_expr(&mut self, cx: &LateContext<'a, 'tcx>, e: &'tcx Expr) {
         let mut is_debug_assert = false;
-        let debug_assert_not_in_macro_or_desugar = |span: Span| {
-            is_debug_assert = true;
-            // Check that `debug_assert!` itself is not inside a macro
-            !in_macro_or_desugar(span)
-        };
-        if_chain! {
-            if let Some(assert_span) = is_direct_expn_of(e.span, "assert");
-            if !in_macro_or_desugar(assert_span)
-                || is_direct_expn_of(assert_span, "debug_assert")
-                    .map_or(false, debug_assert_not_in_macro_or_desugar);
-            if let ExprKind::Unary(_, ref lit) = e.node;
-            if let Some(bool_const) = constant(cx, cx.tables, lit);
-            then {
-                match bool_const.0 {
-                    Constant::Bool(true) => {
-                        span_help_and_lint(
-                            cx,
-                            ASSERTIONS_ON_CONSTANTS,
-                            e.span,
-                            "`assert!(true)` will be optimized out by the compiler",
-                            "remove it"
-                        );
-                    },
-                    Constant::Bool(false) if !is_debug_assert => {
-                        span_help_and_lint(
-                            cx,
-                            ASSERTIONS_ON_CONSTANTS,
-                            e.span,
-                            "`assert!(false)` should probably be replaced",
-                            "use `panic!()` or `unreachable!()`"
-                        );
-                    },
-                    _ => (),
+        if let Some(assert_span) = is_direct_expn_of(e.span, "assert") {
+            if in_macro_or_desugar(assert_span) {
+                return;
+            }
+            if let Some(debug_assert_span) = is_direct_expn_of(assert_span, "debug_assert") {
+                if in_macro_or_desugar(debug_assert_span) {
+                    return;
+                }
+                is_debug_assert = true;
+            }
+            if let ExprKind::Unary(_, ref lit) = e.node {
+                if let Some((bool_const, _)) = constant(cx, cx.tables, lit) {
+                    if let Constant::Bool(is_true) bool_const {
+                        if is_true {
+                            span_help_and_lint(
+                                cx,
+                                ASSERTIONS_ON_CONSTANTS,
+                                e.span,
+                                "`assert!(true)` will be optimized out by the compiler",
+                                "remove it"
+                            );
+                        } else if !is_debug_assert {
+                            span_help_and_lint(
+                                cx,
+                                ASSERTIONS_ON_CONSTANTS,
+                                e.span,
+                                "`assert!(false)` should probably be replaced",
+                                "use `panic!()` or `unreachable!()`"
+                            );
+                        }
+                    }
                 }
             }
         }
