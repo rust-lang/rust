a94c60c94eb06eaa28985d4fe98b32468d320f95
newpavlov <newpavlov@gmail.com>
2019-08-20 18:39:47 +0300

    add tracking issue

--- src/libstd/os/linux/syscall/mod.rs
+++ src/libstd/os/linux/syscall/mod.rs
@@ -1,5 +1,5 @@
 //! Raw syscall functions.
-#![unstable(feature = "linux_syscall", issue = "0")]
+#![unstable(feature = "linux_syscall", issue = "63748")]
 #![cfg(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))]
 
 #[cfg(target_arch = "x86")]
@@ -12,35 +12,35 @@
 #[path="aarch64"] mod platform;
 
 /// Execute syscall with 0 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall0(n: usize) -> usize {
     platform::syscall0(n)
 }
 
 /// Execute syscall with 1 argument.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
     platform::syscall1(n, a1)
 }
 
 /// Execute syscall with 2 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
     platform::syscall2(n, a1, a2)
 }
 
 /// Execute syscall with 3 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     platform::syscall3(n, a1, a2, a3)
 }
 
 /// Execute syscall with 4 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall4(
     n: usize, a1: usize, a2: usize, a3: usize, a4: usize,
@@ -49,7 +49,7 @@ pub unsafe fn syscall4(
 }
 
 /// Execute syscall with 5 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall5(
     n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize,
@@ -58,7 +58,7 @@ pub unsafe fn syscall5(
 }
 
 /// Execute syscall with 6 arguments.
-#[unstable(feature = "linux_syscall", issue = "0")]
+#[unstable(feature = "linux_syscall", issue = "63748")]
 #[inline(always)]
 pub unsafe fn syscall6(
     n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize, a6: usize,
