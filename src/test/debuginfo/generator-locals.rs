c1b59d86b43d4f009aad2f77c674324f80fe1afc
Edd Barrett <vext01@gmail.com>
2019-08-20 14:21:33 +0100

    Fix non-determinism in the test.
    Since the second break-point is not anchored in any way, the compiler is
    free to re-order code and give different outcomes.

--- src/test/debuginfo/generator-locals.rs
+++ src/test/debuginfo/generator-locals.rs
@@ -13,16 +13,9 @@
 // gdb-check:$3 = 7
 // gdb-command:continue
 // gdb-command:print a
-// gdb-check:$4 = 7
+// gdb-check:$4 = 8
 // gdb-command:print c
 // gdb-check:$5 = 6
-// gdb-command:print e
-// gdb-check:$6 = 8
-// gdb-command:continue
-// gdb-command:print a
-// gdb-check:$7 = 8
-// gdb-command:print c
-// gdb-check:$8 = 6
 
 // === LLDB TESTS ==================================================================================
 
@@ -38,20 +31,10 @@
 // lldbr-check:(int) d = 7
 // lldb-command:continue
 // lldb-command:print a
-// lldbg-check:(int) $3 = 7
-// lldbr-check:(int) a = 7
-// lldb-command:print c
-// lldbg-check:(int) $4 = 6
-// lldbr-check:(int) c = 6
-// lldb-command:print e
-// lldbg-check:(int) $5 = 8
-// lldbr-check:(int) e = 8
-// lldb-command:continue
-// lldb-command:print a
-// lldbg-check:(int) $6 = 8
+// lldbg-check:(int) $3 = 8
 // lldbr-check:(int) a = 8
 // lldb-command:print c
-// lldbg-check:(int) $7 = 6
+// lldbg-check:(int) $4 = 6
 // lldbr-check:(int) c = 6
 
 #![feature(omit_gdb_pretty_printer_section, generators, generator_trait)]
@@ -71,7 +54,6 @@ fn main() {
         a = d;
 
         let e = 8; // Live across zero yield points
-        _zzz(); // #break
         a = e;
 
         yield;
