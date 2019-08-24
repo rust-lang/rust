a80ab3a2f85240ec2f9bda00f7cae1abc50865ef
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-08-24 23:45:47 +0200

    Generate version file if it doesn't exist

--- src/bootstrap/doc.rs
+++ src/bootstrap/doc.rs
@@ -375,7 +375,7 @@ impl Step for Standalone {
                up_to_date(&footer, &html) &&
                up_to_date(&favicon, &html) &&
                up_to_date(&full_toc, &html) &&
-               up_to_date(&version_info, &html) &&
+               (builder.config.dry_run || up_to_date(&version_info, &html)) &&
                (builder.config.dry_run || up_to_date(&rustdoc, &html)) {
                 continue
             }
