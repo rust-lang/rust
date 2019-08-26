edcfc261f994399c4063c878c95cbdc78f7cc302
Ralf Jung <post@ralfj.de>
2019-08-26 18:07:55 +0200

    remove a useless test

--- src/test/codegen/nounwind-extern.rs
+++
@@ -1,9 +0,0 @@
-// compile-flags: -O
-
-#![crate_type = "lib"]
-
-// The `nounwind` attribute does not get added by rustc; it is present here because LLVM
-// analyses determine that this function does not unwind.
-
-// CHECK: Function Attrs: norecurse nounwind
-pub extern fn foo() {}
