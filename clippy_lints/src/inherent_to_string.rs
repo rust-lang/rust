1dca950ac4c608263dbf208d923c426e5c1ea500
Yuki Okushi <huyuumi.dev@gmail.com>
2019-08-28 04:23:26 +0900

    Fix `inherent_to_string` false positive

--- clippy_lints/src/inherent_to_string.rs
+++ clippy_lints/src/inherent_to_string.rs
@@ -104,6 +104,7 @@ impl<'a, 'tcx> LateLintPass<'a, 'tcx> for InherentToString {
             if impl_item.ident.name.as_str() == "to_string";
             let decl = &signature.decl;
             if decl.implicit_self.has_implicit_self();
+            if decl.inputs.len() == 1;
 
             // Check if return type is String
             if match_type(cx, return_ty(cx, impl_item.hir_id), &paths::STRING);
