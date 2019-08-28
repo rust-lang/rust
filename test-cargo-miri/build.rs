71aae4ffa5a8ee36f09953d1b85c7b5c2818e0c6
Ralf Jung <post@ralfj.de>
2019-08-28 09:15:31 +0200

    test that build scripts do not run in Miri

---
+++ test-cargo-miri/build.rs
@@ -0,0 +1,15 @@
+#![feature(asm)]
+
+fn not_in_miri() -> i32 {
+    // Inline assembly definitely does not work in Miri.
+    let dummy = 42;
+    unsafe {
+        asm!("" : : "r"(&dummy));
+    }
+    return dummy;
+}
+
+fn main() {
+    not_in_miri();
+    println!("cargo:rerun-if-changed=build.rs");
+}
