c1cec3b247f022b9e3a88c6afe7d1fe8cc459a76
Christian Poveda <christianpoveda@protonmail.com>
2019-08-28 17:31:57 -0500

    Exclude env vars using flag

--- src/eval.rs
+++ src/eval.rs
@@ -42,7 +42,7 @@ pub fn create_ecx<'mir, 'tcx: 'mir>(
         MemoryExtra::new(StdRng::seed_from_u64(config.seed.unwrap_or(0)), config.validate),
     );
     // Complete initialization.
-    EnvVars::init(&mut ecx);
+    EnvVars::init(&mut ecx, config.excluded_env_vars);
 
     // Setup first stack-frame
     let main_instance = ty::Instance::mono(ecx.tcx.tcx, main_id);
