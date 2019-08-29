127311b75efd47a6f54aca49523c050566fc9823
Artyom Pavlov <newpavlov@gmail.com>
2019-08-29 18:08:13 +0000

    whitelist wasi crate

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -172,6 +172,7 @@ const WHITELIST: &[Crate<'_>] = &[
     Crate("vcpkg"),
     Crate("version_check"),
     Crate("void"),
+    Crate("wasi"),
     Crate("winapi"),
     Crate("winapi-build"),
     Crate("winapi-i686-pc-windows-gnu"),
