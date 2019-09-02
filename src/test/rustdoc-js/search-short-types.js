cb84aa4744c7a6120d8311806912240275d04960
Shiqing <shiqing-thu18@yandex.com>
2019-09-02 21:03:35 +0800

    Improve searching in rustdoc and add tests

--- src/test/rustdoc-js/search-short-types.js
+++ src/test/rustdoc-js/search-short-types.js
@@ -3,6 +3,8 @@ const QUERY = 'P';
 const EXPECTED = {
     'others': [
         { 'path': 'search_short_types', 'name': 'P' },
+        { 'path': 'search_short_types::VeryLongTypeName', 'name': 'p' },
         { 'path': 'search_short_types', 'name': 'Ap' },
+        { 'path': 'search_short_types::VeryLongTypeName', 'name': 'ap' },
     ],
 };
