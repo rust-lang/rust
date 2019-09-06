e079f2d70d5813775bff2e23ed942f76474c1e0e
Lokathor <zefria@gmail.com>
2019-09-06 15:20:05 -0600

    Update build.rs

--- library/compiler-builtins/build.rs
+++ library/compiler-builtins/build.rs
@@ -8,6 +8,9 @@ fn main() {
 
     println!("cargo:compiler-rt={}", cwd.join("compiler-rt").display());
 
+    // Activate libm's unstable features to make full use of Nightly.
+    println!("rustc-cfg=libm/unstable");
+
     // Emscripten's runtime includes all the builtins
     if target.contains("emscripten") {
         return;
