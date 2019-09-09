20a26055b7afa500e1b00c6e5a3d03a1208c1d00
Mazdak Farrokhzad <twingoow@gmail.com>
2019-09-09 17:05:41 +0200

    pacify tidy.

--- src/test/ui/refutable-pattern-errors.rs
+++ src/test/ui/refutable-pattern-errors.rs
@@ -1,4 +1,4 @@
-// ignore-line-length
+// ignore-tidy-linelength
 
 fn func((1, (Some(1), 2..=3)): (isize, (Option<isize>, isize))) { }
 //~^ ERROR refutable pattern in function argument: `(_, _)` not covered
