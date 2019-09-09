801e1b745c751a4ac02712ca6a7660cba575dbe9
gnzlbg <gonzalobg88@gmail.com>
2019-09-09 20:46:31 +0200

    Add more tests

--- src/test/ui/abi/abort-on-c-abi.rs
+++ src/test/ui/abi/abort-on-c-abi.rs
@@ -22,6 +22,26 @@ extern "C" fn panic_in_ffi() {
     panic!("Test");
 }
 
+#[unwind(allowed)]
+unsafe extern "C" fn unsafe_panic_allow_in_ffi() {
+    panic!("Test");
+}
+
+#[unwind(aborts)]
+unsafe extern "C" fn unsafe_abort_in_ffi() {
+    panic!("Test");
+}
+
+#[unwind(allowed)]
+extern "C" fn nopanic_in_ffi() {
+    panic!("Test");
+}
+
+#[unwind(aborts)]
+extern "C" fn abort_in_ffi() {
+    panic!("Test");
+}
+
 fn test() {
     // A safe extern "C" function that panics should abort the process:
     let _ = panic::catch_unwind(|| panic_in_ffi() );
@@ -31,10 +51,36 @@ fn test() {
     let _ = io::stdout().flush();
 }
 
+fn test2() {
+    // A safe extern "C" function that panics should abort the process:
+    let _ = panic::catch_unwind(|| abort_in_ffi() );
+
+    // If the process did not abort, the panic escaped FFI:
+    io::stdout().write(b"This should never be printed.\n");
+    let _ = io::stdout().flush();
+}
+
+fn test3() {
+    // An unsafe #[unwind(abort)] extern "C" function that panics should abort the process:
+    let _ = panic::catch_unwind(|| unsafe { unsafe_abort_in_ffi() });
+
+    // If the process did not abort, the panic escaped FFI:
+    io::stdout().write(b"This should never be printed.\n");
+    let _ = io::stdout().flush();
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 && args[1] == "test" {
-        return test();
+    if args.len() > 1 {
+        if args[1] == "test" {
+            return test();
+        }
+        if args[1] == "test2" {
+            return test2();
+        }
+        if args[1] == "test3" {
+            return test3();
+        }
     }
 
     let mut p = Command::new(&args[0])
@@ -43,6 +89,22 @@ fn main() {
                         .arg("test").spawn().unwrap();
     assert!(!p.wait().unwrap().success());
 
+    let mut p = Command::new(&args[0])
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .arg("test2").spawn().unwrap();
+    assert!(!p.wait().unwrap().success());
+
+    let mut p = Command::new(&args[0])
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .arg("test3").spawn().unwrap();
+    assert!(!p.wait().unwrap().success());
+
     // An unsafe extern "C" function that panics should let the panic escape:
     assert!(panic::catch_unwind(|| unsafe { unsafe_panic_in_ffi() }).is_err());
+    assert!(panic::catch_unwind(|| unsafe { unsafe_panic_allow_in_ffi() }).is_err());
+
+    // A safe extern "C" unwind(allows) that panics should let the panic escape:
+    assert!(panic::catch_unwind(|| nopanic_in_ffi()).is_err());
 }
