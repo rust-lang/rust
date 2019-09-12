612ef5f518198448c43959a6416b9da2964f9167
Niko Matsakis <niko@alum.mit.edu>
2019-09-12 17:04:32 -0400

    add new tests for re_rebalance_coherence

---
+++ src/test/ui/coherence/impl[t]-foreign[local]-for-fundamental[t].rs
@@ -0,0 +1,21 @@
+#![feature(re_rebalance_coherence)]
+
+// compile-flags:--crate-name=test
+// aux-build:coherence_lib.rs
+// run-pass
+
+extern crate coherence_lib as lib;
+use lib::*;
+use std::rc::Rc;
+
+struct Local;
+
+impl<T> Remote1<Local> for Box<T> {
+    // FIXME(#64412) -- this is expected to error
+}
+
+impl<T> Remote1<Local> for &T {
+    // FIXME(#64412) -- this is expected to error
+}
+
+fn main() {}
