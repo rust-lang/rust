0cf9c5b6643d51d0981b0f18bc6a71890b667009
Wesley Wiser <wwiser@gmail.com>
2019-09-13 08:40:43 -0400

    Respond to code review feedback and fix tidy

--- src/librustc_mir/interpret/eval_context.rs
+++ src/librustc_mir/interpret/eval_context.rs
@@ -134,7 +134,9 @@ impl<'tcx, Tag: Copy + 'static> LocalState<'tcx, Tag> {
     pub fn access(&self) -> InterpResult<'tcx, Operand<Tag>> {
         match self.value {
             LocalValue::Dead => throw_unsup!(DeadLocal),
-            LocalValue::Uninitialized => throw_unsup!(UninitializedLocal),
+            LocalValue::Uninitialized =>
+                // this is reachable from ConstProp
+                throw_unsup!(UninitializedLocal),
             LocalValue::Live(val) => Ok(val),
         }
     }
