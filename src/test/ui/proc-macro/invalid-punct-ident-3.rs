dab68131d314e6608c547311dfca1655ac8da886
Jonas Schievink <jonasschievink@gmail.com>
2019-09-14 23:02:22 +0200

    Try to fix the test output normalization

--- src/test/ui/proc-macro/invalid-punct-ident-3.rs
+++ src/test/ui/proc-macro/invalid-punct-ident-3.rs
@@ -3,11 +3,11 @@
 // FIXME https://github.com/rust-lang/rust/issues/59998
 // normalize-stderr-test "thread.*panicked.*proc_macro_server.rs.*\n" -> ""
 // normalize-stderr-test "note:.*RUST_BACKTRACE=1.*\n" -> ""
-// normalize-stderr-test "error: internal compiler error.*\n" -> ""
-// normalize-stderr-test "note:.*unexpectedly panicked.*\n" -> ""
-// normalize-stderr-test "note: we would appreciate a bug report.*\n" -> ""
-// normalize-stderr-test "note: compiler flags.*\n" -> ""
-// normalize-stderr-test "note: rustc.*running on.*\n" -> ""
+// normalize-stderr-test "\nerror: internal compiler error.*\n\n" -> ""
+// normalize-stderr-test "note:.*unexpectedly panicked.*\n\n" -> ""
+// normalize-stderr-test "note: we would appreciate a bug report.*\n\n" -> ""
+// normalize-stderr-test "note: compiler flags.*\n\n" -> ""
+// normalize-stderr-test "note: rustc.*running on.*\n\n" -> ""
 
 #[macro_use]
 extern crate invalid_punct_ident;
