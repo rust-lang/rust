636b3543c27e1eba301605afd2143fc96fd14431
Aleksey Kladov <aleksey.kladov@gmail.com>
2019-09-22 20:36:35 +0300

    docstring for mbe module

--- src/libsyntax/ext/mbe.rs
+++ src/libsyntax/ext/mbe.rs
@@ -1,3 +1,8 @@
+//! This module implements declarative macros: old `macro_rules` and the newer
+//! `macro`. Declarative macros are also known as "macro by example", and that's
+//! why we call this module `mbe`. For external documentation, prefer the
+//! official terminology: "declarative macros".
+
 crate mod transcribe;
 crate mod macro_check;
 crate mod macro_parser;
