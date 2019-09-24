75d8199ab1f41be3f58b5f5331eb642714f42732
gnzlbg <gonzalobg88@gmail.com>
2019-09-24 16:48:17 +0200

    Add a fail test

---
+++ src/test/ui/consts/const-eval/const_eval-simd_fail.rs
@@ -0,0 +1,20 @@
+// compile-flags: -Zunleash-the-miri-inside-of-you
+#![feature(repr_simd)]
+#![feature(platform_intrinsics)]
+#![allow(non_camel_case_types)]
+
+#[repr(simd)] struct i8x1(i8);
+
+extern "platform-intrinsic" {
+    fn simd_insert<T, U>(x: T, idx: u32, val: U) -> T;
+}
+
+const fn foo(x: i8x1) -> i8 {
+    // 42 is a i16 that does not fit in a i8
+    unsafe { simd_insert(x, 0_u32, 42_i16) }.0  //~ ERROR
+}
+
+fn main() {
+    const V: i8x1 = i8x1(13);
+    const X: i8 = foo(V);
+}
