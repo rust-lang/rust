bdaa90ceb20a42405f9c7fee31133e70aab1bf7d
Christian Poveda <christianpoveda@protonmail.com>
2019-09-25 11:12:46 -0500

    Add FIXME to file reading test

--- tests/run-pass/file_read.rs
+++ tests/run-pass/file_read.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
+    // FIXME: create the file and delete it when `rm` is implemented.
     let mut file = File::open("./tests/hello.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
