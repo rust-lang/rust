9a2cc54e11e919acf83827609ed0fb0d07bf0778
Mazdak Farrokhzad <twingoow@gmail.com>
2019-09-26 07:50:39 +0200

    cargotest: bump iron

--- src/tools/cargotest/main.rs
+++ src/tools/cargotest/main.rs
@@ -17,8 +17,8 @@ const TEST_REPOS: &'static [Test] = &[
     Test {
         name: "iron",
         repo: "https://github.com/iron/iron",
-        sha: "21c7dae29c3c214c08533c2a55ac649b418f2fe3",
-        lock: Some(include_str!("lockfiles/iron-Cargo.lock")),
+        sha: "cf056ea5e8052c1feea6141e40ab0306715a2c33",
+        lock: None,
         packages: &[],
     },
     Test {
