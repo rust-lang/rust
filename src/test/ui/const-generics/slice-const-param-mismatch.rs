54bad930304a1f7009296e6cfc2f90a008189b1d
ben <benlewisj@gmail.com>
2019-09-29 08:39:48 +1300

    Add a couple more test cases, including non-ascii strings.

--- src/test/ui/const-generics/slice-const-param-mismatch.rs
+++ src/test/ui/const-generics/slice-const-param-mismatch.rs
@@ -5,7 +5,10 @@ struct ConstString<const T: &'static str>;
 struct ConstBytes<const T: &'static [u8]>;
 
 pub fn main() {
+    let _: ConstString<"Hello"> = ConstString::<"Hello">;
     let _: ConstString<"Hello"> = ConstString::<"World">; //~ ERROR mismatched types
+    let _: ConstString<"ℇ㇈↦"> = ConstString::<"ℇ㇈↦">;
+    let _: ConstString<"ℇ㇈↦"> = ConstString::<"ℇ㇈↥">; //~ ERROR mismatched types
     let _: ConstBytes<b"AAA"> = ConstBytes::<{&[0x41, 0x41, 0x41]}>;
     let _: ConstBytes<b"AAA"> = ConstBytes::<b"BBB">; //~ ERROR mismatched types
 }
--- src/test/ui/const-generics/slice-const-param.rs
+++ src/test/ui/const-generics/slice-const-param.rs
@@ -13,6 +13,7 @@ pub fn function_with_bytes<const BYTES: &'static [u8]>() -> &'static [u8] {
 
 pub fn main() {
     assert_eq!(function_with_str::<"Rust">(), "Rust");
+    assert_eq!(function_with_str::<"ℇ㇈↦">(), "ℇ㇈↦");
     assert_eq!(function_with_bytes::<b"AAAA">(), &[0x41, 0x41, 0x41, 0x41]);
     assert_eq!(function_with_bytes::<{&[0x41, 0x41, 0x41, 0x41]}>(), b"AAAA");
 }
