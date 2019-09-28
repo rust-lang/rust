9328ee045e9cc91c1490db97718616022c731b25
Mahmut Bulut <vertexclique@gmail.com>
2019-09-28 16:11:28 +0200

    Make tidy happy

--- src/test/ui/consts/const-eval/const-eval-intrinsic-promotion.rs
+++ src/test/ui/consts/const-eval/const-eval-intrinsic-promotion.rs
@@ -1,4 +1,5 @@
 #![feature(core_intrinsics)]
 fn main() {
-    let x: &'static usize = &std::intrinsics::size_of::<i32>(); //~ ERROR temporary value dropped while borrowed
+    let x: &'static usize =
+        &std::intrinsics::size_of::<i32>(); //~ ERROR temporary value dropped while borrowed
 }
