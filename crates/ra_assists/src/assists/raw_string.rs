6195096fb4af0158a2cd512e3c30b211366b36ab
Geobert Quach <geobert@protonmail.com>
2019-10-01 21:36:14 +0100

    feat(assists): Even smarter with hashes
    Count `"#*` streak only, extract the counting in a function, unit test this function

--- crates/ra_assists/src/assists/raw_string.rs
+++ crates/ra_assists/src/assists/raw_string.rs
@@ -28,17 +28,7 @@ pub(crate) fn make_raw_string(mut ctx: AssistCtx<impl HirDatabase>) -> Option<As
         if error.is_err() {
             eprintln!("Error unescaping string");
         } else {
-            let mut max_hash_streak = 0;
-            unescaped.chars().fold(0, |acc, c| {
-                if c == '#' {
-                    acc + 1
-                } else {
-                    if acc > max_hash_streak {
-                        max_hash_streak = acc;
-                    }
-                    0
-                }
-            });
+            let max_hash_streak = count_hashes(&unescaped);
             let mut hashes = String::with_capacity(max_hash_streak + 1);
             for _ in 0..hashes.capacity() {
                 hashes.push('#');
@@ -50,6 +40,19 @@ pub(crate) fn make_raw_string(mut ctx: AssistCtx<impl HirDatabase>) -> Option<As
         }
     });
     ctx.build()
+}
+
+fn count_hashes(s: &str) -> usize {
+    let indexes: Vec<_> = s.match_indices("\"#").map(|(i, _)| i).collect();
+    let mut max_hash_streak = 0usize;
+    for idx in indexes {
+        let (_, sub) = s.split_at(idx + 1);
+        let nb_hash = sub.chars().take_while(|c| *c == '#').count();
+        if nb_hash > max_hash_streak {
+            max_hash_streak = nb_hash;
+        }
+    }
+    max_hash_streak
 }
 
 fn find_usual_string_range(s: &str) -> Option<TextRange> {
@@ -165,12 +168,31 @@ string"#;
             "###,
             r####"
             fn f() {
-                let s = <|>r###"#random##
+                let s = <|>r#"#random##
+string"#;
+            }
+            "####,
+        )
+    }
+
+    #[test]
+    fn make_raw_string_closing_hashes_inside_works() {
+        check_assist(
+            make_raw_string,
+            r###"
+            fn f() {
+                let s = <|>"#random\"##\nstring";
+            }
+            "###,
+            r####"
+            fn f() {
+                let s = <|>r###"#random"##
 string"###;
             }
             "####,
         )
     }
+
     #[test]
     fn make_raw_string_nothing_to_unescape_works() {
         check_assist(
@@ -409,5 +431,15 @@ string"###;
             }
             "#,
         );
+    }
+
+    #[test]
+    fn count_hashes_test() {
+        assert_eq!(0, count_hashes("abc"));
+        assert_eq!(0, count_hashes("###"));
+        assert_eq!(1, count_hashes("\"#abc"));
+        assert_eq!(0, count_hashes("#abc"));
+        assert_eq!(2, count_hashes("#ab\"##c"));
+        assert_eq!(4, count_hashes("#ab\"##\"####c"));
     }
 }
