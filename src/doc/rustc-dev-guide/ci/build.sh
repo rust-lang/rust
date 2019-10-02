3e2d38d34153861f0ae6947e6329307a9afdee03
Amanjeev Sethi <aj@amanjeev.com>
2019-10-02 18:06:30 -0400

    Fix (build CI script): fails early if linkcheck fails

--- src/doc/rustc-dev-guide/ci/build.sh
+++ src/doc/rustc-dev-guide/ci/build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -x
 
+set -o pipefail
+
 output=$(mktemp)
 
 mkdir -p book/
 cp -r $HOME/linkcheck/ book/
 RUST_LOG=mdbook_linkcheck=debug mdbook-linkcheck -s 2>&1 | tee -a $output
+result=${PIPESTATUS[0]}
 cp -r book/linkcheck $HOME/
 
 mdbook build
 
-result=${PIPESTATUS[0]}
-
 # if passed, great!
 if [ "$result" -eq "0" ] ; then
+    echo "Linkchecks passed"
     exit 0 ;
+else
+    echo "Linkchecks failed"
+    exit 1 ;
 fi
