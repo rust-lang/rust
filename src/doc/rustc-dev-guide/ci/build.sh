96b9ee07587db17e278b46c529bc4fea010e7cda
Amanjeev Sethi <aj@amanjeev.com>
2019-10-02 20:12:26 -0400

    Refactor (build CI script): minimizes the script, thanks to mark-i-m

--- src/doc/rustc-dev-guide/ci/build.sh
+++ src/doc/rustc-dev-guide/ci/build.sh
@@ -1,22 +1,10 @@
 #!/bin/bash -x
 
-set -o pipefail
-
-output=$(mktemp)
+set -e
 
 mkdir -p book/
 cp -r $HOME/linkcheck/ book/
-RUST_LOG=mdbook_linkcheck=debug mdbook-linkcheck -s 2>&1 | tee -a $output
-result=${PIPESTATUS[0]}
+RUST_LOG=mdbook_linkcheck=debug mdbook-linkcheck -s
 cp -r book/linkcheck $HOME/
 
 mdbook build
-
-# if passed, great!
-if [ "$result" -eq "0" ] ; then
-    echo "Linkchecks passed"
-    exit 0 ;
-else
-    echo "Linkchecks failed"
-    exit 1 ;
-fi
