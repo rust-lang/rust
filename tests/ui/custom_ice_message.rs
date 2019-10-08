5029dfe403c35e63352f9e1f10eb62ee2851fa90
Philipp Hansch <dev@phansch.net>
2019-10-08 21:25:10 +0200

    Use exec_env to set backtrace level and normalize output

--- tests/ui/custom_ice_message.rs
+++ tests/ui/custom_ice_message.rs
@@ -1,3 +1,7 @@
+// exec-env:RUST_BACKTRACE=0
+// normalize-stderr-test: "Clippy version: .*" -> "Clippy version: foo"
+// normalize-stderr-test: "internal_lints.rs.*" -> "internal_lints.rs:1:1"
+
 #![deny(clippy::internal)]
 
 fn should_trigger_an_ice_in_clippy() {}
