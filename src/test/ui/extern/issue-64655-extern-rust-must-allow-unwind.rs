63d67fa273f6b4409623b1dc9517ee5aade18cd2
Ralf Jung <post@ralfj.de>
2019-10-11 09:57:44 +0200

    ignore-emcscripten as it does not support threads

--- src/test/ui/extern/issue-64655-extern-rust-must-allow-unwind.rs
+++ src/test/ui/extern/issue-64655-extern-rust-must-allow-unwind.rs
@@ -1,5 +1,6 @@
 // run-pass
 // ignore-wasm32-bare compiled with panic=abort by default
+// ignore-emscripten no threads support
 
 // rust-lang/rust#64655: with panic=unwind, a panic from a subroutine
 // should still run destructors as it unwinds the stack. However,
