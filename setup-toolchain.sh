dce46325b5845508054d934ebabbf862e32ee2e4
Lzu Tao <taolzu@gmail.com>
2019-10-11 15:53:55 +0700

    Rustup for rustc-dev

--- setup-toolchain.sh
+++ setup-toolchain.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Set up the appropriate rustc toolchain
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 
 if ! command -v rustup-toolchain-install-master > /dev/null; then
   cargo install rustup-toolchain-install-master --debug
 fi
 
-RUSTC_HASH=$(git ls-remote https://github.com/rust-lang/rust.git master | awk '{print $1}')
-rustup-toolchain-install-master -f -n master "$RUSTC_HASH"
+rustup-toolchain-install-master -f -n master -c rustc-dev
 rustup override set master
