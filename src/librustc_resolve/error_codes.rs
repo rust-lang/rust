10236f1cee5d08e1f74d270fa37cd20fca6b4077
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-10-15 13:52:49 +0200

    Add long error explanation for E0577

--- src/librustc_resolve/error_codes.rs
+++ src/librustc_resolve/error_codes.rs
@@ -1661,6 +1661,33 @@ match eco {
 ```
 "##,
 
+E0577: r##"
+Something other than a module was found in visibility scope.
+
+Erroneous code example:
+
+```compile_fail,E0577,edition2018
+pub struct Sea;
+
+pub (in crate::Sea) struct Shark; // error!
+
+fn main() {}
+```
+
+`Sea` is not a module, therefore, it is invalid. To fix this error, we need to
+replace `Sea` with a module.
+
+Please note that the visibility scope can only be applied on ancestors!
+
+```edition2018
+pub mod Sea {
+    pub (in crate::Sea) struct Shark; // ok!
+}
+
+fn main() {}
+```
+"##,
+
 E0603: r##"
 A private item was used outside its scope.
 
@@ -1791,6 +1818,5 @@ struct Foo<X = Box<Self>> {
     E0573,
     E0575,
     E0576,
-    E0577,
     E0578,
 }
