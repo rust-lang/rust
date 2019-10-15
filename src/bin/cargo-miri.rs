e06ce728ca49fc647ea1dfbb609116d76385446c
YOUNGSUK_KIM <ykim837@gatech.edu>
2019-10-14 20:57:57 -0400

    Ralf Jung's great idea!

--- src/bin/cargo-miri.rs
+++ src/bin/cargo-miri.rs
@@ -333,11 +333,11 @@ path = "lib.rs"
         None => true,
         Some(target) => target == rustc_version::version_meta().unwrap().host,
     };
-    let sysroot = if is_host { dir.join("HOST") } else { PathBuf::from(dir) }; 
-    
+    let sysroot = if is_host { dir.join("HOST") } else { PathBuf::from(dir) };
+
     std::env::set_var("MIRI_SYSROOT", &sysroot); // pass the env var to the processes we spawn, which will turn it into "--sysroot" flags
     if print_env {
-        println!("MIRI_SYSROOT={:?}", &sysroot); // for Windows users, prints path with backslashes escaped.
+        println!("MIRI_SYSROOT='{}'", sysroot.display().to_string().replace('\'', r#"'"'"'"#));
     } else if !ask_user {
         println!("A libstd for Miri is now available in `{}`.", sysroot.display());
     }
