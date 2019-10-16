add21c44a1617c902fdec358fdd6be6d67e9158f
csmoe <csmoe@msn.com>
2019-10-17 00:57:18 +0800

    add debuginfo in generator_interior

--- src/librustc/hir/mod.rs
+++ src/librustc/hir/mod.rs
@@ -1833,6 +1833,16 @@ impl fmt::Display for YieldSource {
     }
 }
 
+impl core::convert::From<GeneratorKind> for YieldSource {
+    fn from(gen_kind: GeneratorKind) -> Self {
+        match gen_kind {
+            // Guess based on the kind of the current generator.
+            GeneratorKind::Gen => Self::Yield,
+            GeneratorKind::Async(_) => Self::Await,
+        }
+    }
+}
+
 #[derive(Copy, Clone, RustcEncodable, RustcDecodable, Debug, HashStable)]
 pub enum CaptureClause {
     CaptureByValue,
