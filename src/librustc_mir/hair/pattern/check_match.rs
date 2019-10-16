c4deea212d19b19fa11fb4e29dd5c7f1359543a5
Nicholas Nethercote <nnethercote@mozilla.com>
2019-10-16 17:22:35 +1100

    Avoid unnecessary arena allocations in `expand_pattern()`.
    `expand_pattern()` has two callsites. One of them needs arena
    allocation, but the other does not.
    
    This commit moves the arena allocation out of the function. This avoids
    the allocation of many 4 KiB page arena chunks that only hold a single
    small allocation. It reduces the number of bytes allocated by up to 2%
    for various benchmarks, albeit without only a very small improvement in
    runtime.

--- src/librustc_mir/hair/pattern/check_match.rs
+++ src/librustc_mir/hair/pattern/check_match.rs
@@ -153,7 +153,8 @@ impl<'tcx> MatchVisitor<'_, 'tcx> {
                         self.tables
                     );
                     patcx.include_lint_checks();
-                    let pattern = expand_pattern(cx, patcx.lower_pattern(&pat));
+                    let pattern =
+                        cx.pattern_arena.alloc(expand_pattern(cx, patcx.lower_pattern(&pat))) as &_;
                     if !patcx.errors.is_empty() {
                         patcx.report_inlining_errors(pat.span);
                         have_errors = true;
@@ -252,8 +253,9 @@ impl<'tcx> MatchVisitor<'_, 'tcx> {
             patcx.include_lint_checks();
             let pattern = patcx.lower_pattern(pat);
             let pattern_ty = pattern.ty;
+            let pattern = expand_pattern(cx, pattern);
             let pats: Matrix<'_, '_> = vec![smallvec![
-                expand_pattern(cx, pattern)
+                &pattern
             ]].into_iter().collect();
 
             let witnesses = match check_not_useful(cx, pattern_ty, &pats) {
