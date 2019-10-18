11011013f2b609b6cf58c96555b9e31dfd19d7ee
Esteban Küber <esteban@kuber.com.ar>
2019-10-17 19:00:19 -0700

    Refer to "associated functions" instead of "static methods"

--- src/librustc_resolve/error_codes.rs
+++ src/librustc_resolve/error_codes.rs
@@ -1013,7 +1013,12 @@ fn h1() -> i32 {
 "##,
 
 E0424: r##"
-The `self` keyword was used in a static method.
+The `self` keyword was used inside of an associated function instead of inside
+of a method. Associated functions have no "`self` receiver" argument, and are
+equivalent to regular functions which exist in the namespace of a trait.
+Methods, on the other hand, have a `self` reciver argument, like `self`,
+`&self`, `&mut self` or `self: &mut Pin<Self>` (this last one is an example of
+an ["abitrary `self` type"](https://github.com/rust-lang/rust/issues/44874)).
 
 Erroneous code example:
 
@@ -1021,25 +1026,26 @@ Erroneous code example:
 struct Foo;
 
 impl Foo {
-    fn bar(self) {}
+    // `bar` is a method, because it has a receiver argument.
+    fn bar(&self) {}
 
+    // `foo` is an associated function, because it has no receiver argument.
     fn foo() {
-        self.bar(); // error: `self` is not available in a static method.
+        self.bar(); // error: `self` is not available in an associated function
     }
 }
 ```
 
-Please check if the method's argument list should have contained `self`,
-`&self`, or `&mut self` (in case you didn't want to create a static
-method), and add it if so. Example:
+Check if the associated function's argument list should have contained a `self`
+receiver for it to be a method, and add it if so. Example:
 
 ```
 struct Foo;
 
 impl Foo {
-    fn bar(self) {}
+    fn bar(&self) {}
 
-    fn foo(self) {
+    fn foo(self) { // `foo` is now a method.
         self.bar(); // ok!
     }
 }
