38fd74f22af7eec30d11d421bcbeaa891c7f7436
Ralf Jung <post@ralfj.de>
2019-10-19 15:52:08 +0200

    show the proper diagnostics

--- src/test/ui/consts/miri_unleashed/non_const_fn.rs
+++ src/test/ui/consts/miri_unleashed/non_const_fn.rs
@@ -1,11 +1,12 @@
 // compile-flags: -Zunleash-the-miri-inside-of-you
-#![allow(const_err)]
+#![warn(const_err)]
 
 // A test demonstrating that we prevent calling non-const fn during CTFE.
 
 fn foo() {}
 
 const C: () = foo(); //~ WARN: skipping const checks
+//~^ WARN any use of this value will cause an error
 
 fn main() {
     println!("{:?}", C); //~ ERROR: evaluation of constant expression failed
