db0078706b47f7ee0e492a15995ae55ae70c086d
Pietro Albini <pietro@pietroalbini.org>
2019-10-22 16:40:54 +0200

    ci: split install-msys2 step into two separate scripts

--- src/ci/scripts/install-msys2.sh
+++ src/ci/scripts/install-msys2.sh
@@ -23,18 +23,4 @@ if isWindows; then
     mkdir -p "${SYSTEM_WORKFOLDER}/msys2/home/${USERNAME}"
 
     ciCommandAddPath "${SYSTEM_WORKFOLDER}/msys2/usr/bin"
-    export PATH="${SYSTEM_WORKFOLDER}/msys2/usr/bin:${PATH}"
-
-    pacman -S --noconfirm --needed base-devel ca-certificates make diffutils tar
-
-    # FIXME(#65767): workaround msys bug, step 2
-    pacman -U --noconfirm --noprogressbar mingw-w64-$arch-ca-certificates-20180409-1-any.pkg.tar.xz
-    rm mingw-w64-$arch-ca-certificates-20180409-1-any.pkg.tar.xz
-
-    # Make sure we use the native python interpreter instead of some msys equivalent
-    # one way or another. The msys interpreters seem to have weird path conversions
-    # baked in which break LLVM's build system one way or another, so let's use the
-    # native version which keeps everything as native as possible.
-    cp C:/Python27amd64/python.exe C:/Python27amd64/python2.7.exe
-    ciCommandAddPath "C:\\Python27amd64"
 fi
