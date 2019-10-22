a239c8dfb29c20f29fc3b30959f654d41fb0dd7f
Yuki Okushi <huyuumi.dev@gmail.com>
2019-10-22 17:00:47 +0900

    Add test for issue-41366

---
+++ src/test/ui/closures/issue-41366.rs
@@ -0,0 +1,13 @@
+trait T<'x> {
+    type V;
+}
+
+impl<'g> T<'g> for u32 {
+    type V = u16;
+}
+
+fn main() {
+    (&|_|()) as &dyn for<'x> Fn(<u32 as T<'x>>::V);
+    //~^ ERROR: type mismatch in closure arguments
+    //~| ERROR: type mismatch resolving
+}
