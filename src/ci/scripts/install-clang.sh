c854c4482b678c765a14c390565133dbadcc5490
Pietro Albini <pietro@pietroalbini.org>
2019-10-28 10:03:55 +0100

    ci: fix execution condition for install-clang

--- src/ci/scripts/install-clang.sh
+++ src/ci/scripts/install-clang.sh
@@ -17,7 +17,7 @@ if isMacOS; then
     # Configure `AR` specifically so rustbuild doesn't try to infer it as
     # `clang-ar` by accident.
     ciCommandSetEnv AR "ar"
-elif isWindows; then
+elif isWindows && [[ -z ${MINGW_URL+x} ]]; then
     # If we're compiling for MSVC then we, like most other distribution builders,
     # switch to clang as the compiler. This'll allow us eventually to enable LTO
     # amongst LLVM and rustc. Note that we only do this on MSVC as I don't think
