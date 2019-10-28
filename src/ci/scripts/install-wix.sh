7e051236b02ae6d924f24e080784fcf10cb8c4d3
Pietro Albini <pietro@pietroalbini.org>
2019-10-28 15:09:18 +0100

    ci: fix wrong path for wix being set

--- src/ci/scripts/install-wix.sh
+++ src/ci/scripts/install-wix.sh
@@ -8,10 +8,10 @@ IFS=$'\n\t'
 source "$(cd "$(dirname "$0")" && pwd)/../shared.sh"
 
 if isWindows; then
+    ciCommandSetEnv WIX "$(pwd)/wix"
+
     curl -O "${MIRRORS_BASE}/wix311-binaries.zip"
     mkdir -p wix/bin
     cd wix/bin
     7z x ../../wix311-binaries.zip
-
-    ciCommandSetEnv WIX "$(pwd)/wix"
 fi
