880f9058d2b30dd4f32b9a1b86499a258ee4e188
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2019-10-29 21:46:25 +0200

    rustc: compute FnAbi's for virtual calls through FnAbi::of_instance.

--- src/librustc_codegen_llvm/mono_item.rs
+++ src/librustc_codegen_llvm/mono_item.rs
@@ -43,7 +43,7 @@ impl PreDefineMethods<'tcx> for CodegenCx<'ll, 'tcx> {
         assert!(!instance.substs.needs_infer() &&
                 !instance.substs.has_param_types());
 
-        let fn_abi = FnAbi::of_instance(self, instance);
+        let fn_abi = FnAbi::of_instance(self, instance, &[]);
         let lldecl = self.declare_fn(symbol_name, &fn_abi);
         unsafe { llvm::LLVMRustSetLinkage(lldecl, base::linkage_to_llvm(linkage)) };
         let attrs = self.tcx.codegen_fn_attrs(instance.def_id());
