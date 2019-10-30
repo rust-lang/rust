53c2c04d63b3c043ec5b1b4f401e04868a1139a4
Pietro Albini <pietro@pietroalbini.org>
2019-10-30 12:35:55 +0100

    ci: remove the MINGW_DIR and MINGW_ARCHIVE env vars

--- src/ci/scripts/install-mingw.sh
+++ src/ci/scripts/install-mingw.sh
@@ -27,6 +27,9 @@ IFS=$'\n\t'
 
 source "$(cd "$(dirname "$0")" && pwd)/../shared.sh"
 
+MINGW_ARCHIVE_32="i686-6.3.0-release-posix-dwarf-rt_v5-rev2.7z"
+MINGW_ARCHIVE_64="x86_64-6.3.0-release-posix-seh-rt_v5-rev2.7z"
+
 if isWindows; then
     if [[ "${CUSTOM_MINGW-0}" -ne 1 ]]; then
         arch=i686
@@ -37,9 +40,15 @@ if isWindows; then
             mingw-w64-$arch-gcc mingw-w64-$arch-python2
         ciCommandAddPath "${SYSTEM_WORKFOLDER}/msys2/mingw${MSYS_BITS}/bin"
     else
-        curl -o mingw.7z "${MIRRORS_BASE}/${MINGW_ARCHIVE}"
+        mingw_archive="${MINGW_ARCHIVE_32}"
+        if [[ "${MSYS_BITS}" = "64" ]]; then
+            mingw_archive="${MINGW_ARCHIVE_64}"
+        fi
+        mingw_dir="mingw${MSYS_BITS}"
+
+        curl -o mingw.7z "${MIRRORS_BASE}/${mingw_archive}"
         7z x -y mingw.7z > /dev/null
-        curl -o "${MINGW_DIR}/bin/gdborig.exe" "${MIRRORS_BASE}/2017-04-20-${MSYS_BITS}bit-gdborig.exe"
-        ciCommandAddPath "$(pwd)/${MINGW_DIR}/bin"
+        curl -o "${mingw_dir}/bin/gdborig.exe" "${MIRRORS_BASE}/2017-04-20-${MSYS_BITS}bit-gdborig.exe"
+        ciCommandAddPath "$(pwd)/${mingw_dir}/bin"
     fi
 fi
