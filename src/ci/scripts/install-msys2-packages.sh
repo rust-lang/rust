48d6510f6f0b44d4cdd3ebd0699d7ad703de6169
Pietro Albini <pietro@pietroalbini.org>
2019-10-30 09:13:26 +0100

    ci: revert msys2 ca-certificates hack
    The hack was added because upstream msys2 broke the ca-certificates
    package, but since then it has been fixed. This reverts CI to use the
    upstream package.

--- src/ci/scripts/install-msys2-packages.sh
+++ src/ci/scripts/install-msys2-packages.sh
@@ -8,14 +8,6 @@ source "$(cd "$(dirname "$0")" && pwd)/../shared.sh"
 if isWindows; then
     pacman -S --noconfirm --needed base-devel ca-certificates make diffutils tar
 
-    # FIXME(#65767): workaround msys bug, step 2
-    arch=i686
-    if [ "$MSYS_BITS" = "64" ]; then
-      arch=x86_64
-    fi
-    pacman -U --noconfirm --noprogressbar mingw-w64-$arch-ca-certificates-20180409-1-any.pkg.tar.xz
-    rm mingw-w64-$arch-ca-certificates-20180409-1-any.pkg.tar.xz
-
     # Make sure we use the native python interpreter instead of some msys equivalent
     # one way or another. The msys interpreters seem to have weird path conversions
     # baked in which break LLVM's build system one way or another, so let's use the
