fcf0f886d46050ecbb64c068d62725b1b4ef1aca
Ralf Jung <post@ralfj.de>
2019-11-03 10:15:55 +0100

    adjust tests

--- tests/compile-fail/exact_div2.rs
+++ tests/compile-fail/exact_div2.rs
@@ -1,5 +1,5 @@
 #![feature(core_intrinsics)]
 fn main() {
     // divison with a remainder
-    unsafe { std::intrinsics::exact_div(2u16, 3); } //~ ERROR Scalar(0x0002) cannot be divided by Scalar(0x0003) without remainder
+    unsafe { std::intrinsics::exact_div(2u16, 3); } //~ ERROR 2 cannot be divided by 3 without remainder
 }
