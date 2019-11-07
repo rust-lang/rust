1e4c2ae622549108a55fcbdaeffdbf6d56ace3ac
Alex Crichton <alex@alexcrichton.com>
2019-11-07 09:00:25 -0800

    Update clang to build LLVM to 9.0.0
    This also ensure that we're using the same clang version for all our
    major platforms instead of 8.0 on Linux and 7.0 on OSX/Windows.

--- src/ci/scripts/install-clang.sh
+++ src/ci/scripts/install-clang.sh
@@ -9,10 +9,10 @@ IFS=$'\n\t'
 source "$(cd "$(dirname "$0")" && pwd)/../shared.sh"
 
 if isMacOS; then
-    curl -f "${MIRRORS_BASE}/clang%2Bllvm-7.0.0-x86_64-apple-darwin.tar.xz" | tar xJf -
+    curl -f "${MIRRORS_BASE}/clang%2Bllvm-9.0.0-x86_64-darwin-apple.tar.xz" | tar xJf -
 
-    ciCommandSetEnv CC "$(pwd)/clang+llvm-7.0.0-x86_64-apple-darwin/bin/clang"
-    ciCommandSetEnv CXX "$(pwd)/clang+llvm-7.0.0-x86_64-apple-darwin/bin/clang++"
+    ciCommandSetEnv CC "$(pwd)/clang+llvm-9.0.0-x86_64-darwin-apple/bin/clang"
+    ciCommandSetEnv CXX "$(pwd)/clang+llvm-9.0.0-x86_64-darwin-apple/bin/clang++"
 
     # Configure `AR` specifically so rustbuild doesn't try to infer it as
     # `clang-ar` by accident.
@@ -27,17 +27,18 @@ elif isWindows && [[ -z ${MINGW_URL+x} ]]; then
     # Note that the LLVM installer is an NSIS installer
     #
     # Original downloaded here came from
-    # http://releases.llvm.org/7.0.0/LLVM-7.0.0-win64.exe
-    # That installer was run through `wine` on Linux and then the resulting
-    # installation directory (found in `$HOME/.wine/drive_c/Program Files/LLVM`) was
-    # packaged up into a tarball. We've had issues otherwise that the installer will
-    # randomly hang, provide not a lot of useful information, pollute global state,
-    # etc. In general the tarball is just more confined and easier to deal with when
-    # working with various CI environments.
+    # http://releases.llvm.org/9.0.0/LLVM-9.0.0-win64.exe
+    # That installer was run through `wine ./installer.exe /S /NCRC` on Linux
+    # and then the resulting installation directory (found in
+    # `$HOME/.wine/drive_c/Program Files/LLVM`) was packaged up into a tarball.
+    # We've had issues otherwise that the installer will randomly hang, provide
+    # not a lot of useful information, pollute global state, etc. In general the
+    # tarball is just more confined and easier to deal with when working with
+    # various CI environments.
 
     mkdir -p citools
     cd citools
-    curl -f "${MIRRORS_BASE}/LLVM-7.0.0-win64.tar.gz" | tar xzf -
+    curl -f "${MIRRORS_BASE}/LLVM-9.0.0-win64.tar.gz" | tar xzf -
     ciCommandSetEnv RUST_CONFIGURE_ARGS \
         "${RUST_CONFIGURE_ARGS} --set llvm.clang-cl=$(pwd)/clang-rust/bin/clang-cl.exe"
 fi
