c1fd75005e18e713940b05e95efc5dc32385ea8b
Michael Wright <mikerite@lavabit.com>
2019-11-11 06:22:50 +0200

    Fix breakage due to rust-lang/rust#65324

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -31,6 +31,8 @@ extern crate rustc_index;
 #[allow(unused_extern_crates)]
 extern crate rustc_mir;
 #[allow(unused_extern_crates)]
+extern crate rustc_parse;
+#[allow(unused_extern_crates)]
 extern crate rustc_target;
 #[allow(unused_extern_crates)]
 extern crate rustc_typeck;
