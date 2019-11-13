9e28e9c545ab86f314ed242922f3d83e69319e45
Camille GILLOT <gillot.camille@gmail.com>
2019-11-13 20:47:41 +0100

    Create TypeFoldable derive proc-macro.

--- src/librustc_macros/src/lib.rs
+++ src/librustc_macros/src/lib.rs
@@ -9,6 +9,7 @@ use synstructure::decl_derive;
 use proc_macro::TokenStream;
 
 mod hash_stable;
+mod type_foldable;
 mod query;
 mod symbols;
 
@@ -23,3 +24,4 @@ pub fn symbols(input: TokenStream) -> TokenStream {
 }
 
 decl_derive!([HashStable, attributes(stable_hasher)] => hash_stable::hash_stable_derive);
+decl_derive!([TypeFoldable, attributes(type_foldable)] => type_foldable::type_foldable_derive);
