9e1b210c6ae98b2df5362c2d1ced67616105d575
Wesley Wiser <wwiser@gmail.com>
2019-11-14 06:17:41 -0500

    Respond to review feedback

--- src/test/ui/consts/issue-66342.rs
+++ src/test/ui/consts/issue-66342.rs
@@ -1,5 +1,7 @@
 // check-pass
 
+// Checks that the compiler does not actually try to allocate 4 TB during compilation and OOM crash.
+
 fn foo() -> [u8; 4 * 1024 * 1024 * 1024 * 1024] {
     unimplemented!()
 }
