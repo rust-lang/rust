a5f57f98ca4c0afb1c292da6676a1273ce95f0e0
memoryruins <memoryruinsmusic@gmail.com>
2019-11-15 06:53:11 -0500

    Heed the advice of rustc's into_iter() warning

--- crates/ra_hir_expand/src/quote.rs
+++ crates/ra_hir_expand/src/quote.rs
@@ -241,10 +241,8 @@ mod tests {
         // }
         let struct_name = mk_ident("Foo");
         let fields = [mk_ident("name"), mk_ident("id")];
-        let fields = fields
-            .into_iter()
-            .map(|it| quote!(#it: self.#it.clone(), ).token_trees.clone())
-            .flatten();
+        let fields =
+            fields.iter().map(|it| quote!(#it: self.#it.clone(), ).token_trees.clone()).flatten();
 
         let list = tt::Subtree { delimiter: tt::Delimiter::Brace, token_trees: fields.collect() };
 
