23d41d8e04ab895da01fc38dcdbad41599946a16
Matthew Jasper <mjjasper1@gmail.com>
2019-11-17 15:24:12 +0000

    Temp: Appease tidy

--- src/librustc_mir/build/scope.rs
+++ src/librustc_mir/build/scope.rs
@@ -789,7 +789,10 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
             }
 
             if scope.region_scope == region_scope {
-                let region_scope_span = region_scope.span(self.hir.tcx(), &self.hir.region_scope_tree);
+                let region_scope_span = region_scope.span(
+                    self.hir.tcx(),
+                    &self.hir.region_scope_tree,
+                );
                 // Attribute scope exit drops to scope's closing brace.
                 let scope_end = self.hir.tcx().sess.source_map().end_point(region_scope_span);
 
