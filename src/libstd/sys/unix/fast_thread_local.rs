ab6cb0197188f0c842d7dd2e378777f0bc0f6240
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2019-11-19 20:43:06 +0100

    HermitCore support is moved to sys/hermit, remove obsolete statement in sys/unix

--- src/libstd/sys/unix/fast_thread_local.rs
+++ src/libstd/sys/unix/fast_thread_local.rs
@@ -10,7 +10,7 @@
 // fallback implementation to use as well.
 //
 // Due to rust-lang/rust#18804, make sure this is not generic!
-#[cfg(any(target_os = "linux", target_os = "fuchsia", target_os = "hermit", target_os = "redox",
+#[cfg(any(target_os = "linux", target_os = "fuchsia", target_os = "redox",
           target_os = "emscripten"))]
 pub unsafe fn register_dtor(t: *mut u8, dtor: unsafe extern fn(*mut u8)) {
     use crate::mem;
