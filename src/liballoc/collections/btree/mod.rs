3cf724d0c122f08ccbb3a9f77cb0ad888d8bebf0
Stein Somers <git@steinsomers.be>
2019-11-19 13:55:55 +0100

    Bundle and document 6 BTreeMap navigation algorithms

--- src/liballoc/collections/btree/mod.rs
+++ src/liballoc/collections/btree/mod.rs
@@ -1,4 +1,5 @@
 pub mod map;
+mod navigate;
 mod node;
 mod search;
 pub mod set;
@@ -10,4 +11,15 @@ trait Recover<Q: ?Sized> {
     fn get(&self, key: &Q) -> Option<&Self::Key>;
     fn take(&mut self, key: &Q) -> Option<Self::Key>;
     fn replace(&mut self, key: Self::Key) -> Option<Self::Key>;
+}
+
+#[inline(always)]
+pub unsafe fn unwrap_unchecked<T>(val: Option<T>) -> T {
+    val.unwrap_or_else(|| {
+        if cfg!(debug_assertions) {
+            panic!("'unchecked' unwrap on None in BTreeMap");
+        } else {
+            core::intrinsics::unreachable();
+        }
+    })
 }
