a596ff36b55a37e7a74abd0504ff895a3d2fba6f
The8472 <git@infinite-source.de>
2019-11-21 01:01:35 +0100

    exercise more of the in-place pipeline in the bench

--- library/alloc/benches/vec.rs
+++ library/alloc/benches/vec.rs
@@ -513,7 +513,14 @@ impl Drop for Droppable {
 #[bench]
 fn bench_in_place_collect_droppable(b: &mut test::Bencher) {
     let v: Vec<Droppable> = std::iter::repeat_with(|| Droppable(0)).take(1000).collect();
-    b.iter(|| v.clone().into_iter().skip(100).collect::<Vec<_>>())
+    b.iter(|| {
+        v.clone()
+            .into_iter()
+            .skip(100)
+            .enumerate()
+            .map(|(i, e)| Droppable(i ^ e.0))
+            .collect::<Vec<_>>()
+    })
 }
 
 #[bench]
