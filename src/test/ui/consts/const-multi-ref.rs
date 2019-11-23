5a3ddcd771e9df8b5ed47b7aad22bf526bfd8b13
Dylan MacKenzie <ecstaticmorse@gmail.com>
2019-11-22 16:14:24 -0800

    Update test with improved diagnostics
    Now, `derived_from_illegal_borrow` is also applied to reborrows, so we
    don't show the user a useless error message.

--- src/test/ui/consts/const-multi-ref.rs
+++ src/test/ui/consts/const-multi-ref.rs
@@ -1,8 +1,11 @@
+// Ensure that we point the user to the erroneous borrow but not to any subsequent borrows of that
+// initial one.
+
 const _X: i32 = {
     let mut a = 5;
-    let p = &mut a;      //~ ERROR references in constants may only refer to immutable values
+    let p = &mut a; //~ ERROR references in constants may only refer to immutable values
 
-    let reborrow = {p};  //~ ERROR references in constants may only refer to immutable values
+    let reborrow = {p};
     let pp = &reborrow;
     let ppp = &pp;
     ***ppp
