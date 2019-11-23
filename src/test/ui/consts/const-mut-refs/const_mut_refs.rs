e31a1368fda9c5b4b4bc39e69754b19d36dff8c2
Christian Poveda <christianpoveda@protonmail.com>
2019-11-23 15:00:14 -0500

    Extend test for const_mut_refs feature

--- src/test/ui/consts/const-mut-refs/const_mut_refs.rs
+++ src/test/ui/consts/const-mut-refs/const_mut_refs.rs
@@ -6,12 +6,31 @@ struct Foo {
     x: usize
 }
 
-const fn bar(foo: &mut Foo) -> usize {
+const fn foo() -> Foo {
+    Foo { x: 0 }
+}
+
+impl Foo {
+    const fn bar(&mut self) -> usize {
+        self.x = 1;
+        self.x
+    }
+
+}
+
+const fn baz(foo: &mut Foo) -> usize {
     let x = &mut foo.x;
-    *x = 1;
+    *x = 2;
     *x
 }
 
+const fn bazz(foo: &mut Foo) -> usize {
+    foo.x = 3;
+    foo.x
+}
+
 fn main() {
-    let _: [(); bar(&mut Foo { x: 0 })] = [(); 1];
+    let _: [(); foo().bar()] = [(); 1];
+    let _: [(); baz(&mut foo())] = [(); 2];
+    let _: [(); bazz(&mut foo())] = [(); 3];
 }
