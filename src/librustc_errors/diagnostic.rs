d92355c1db65035e508020e746c141c79dfdbd99
Esteban Küber <esteban@kuber.com.ar>
2019-11-23 16:01:20 -0800

    Highlight parts of fn in type errors
    When a type error arises between two fn items, fn pointers or tuples,
    highlight only the differing parts of each.

--- src/librustc_errors/diagnostic.rs
+++ src/librustc_errors/diagnostic.rs
@@ -52,6 +52,13 @@ impl DiagnosticStyledString {
     pub fn push_highlighted<S: Into<String>>(&mut self, t: S) {
         self.0.push(StringPart::Highlighted(t.into()));
     }
+    pub fn push<S: Into<String>>(&mut self, t: S, highlight: bool) {
+        if highlight {
+            self.0.push(StringPart::Highlighted(t.into()));
+        } else {
+            self.0.push(StringPart::Normal(t.into()));
+        }
+    }
     pub fn normal<S: Into<String>>(t: S) -> DiagnosticStyledString {
         DiagnosticStyledString(vec![StringPart::Normal(t.into())])
     }
