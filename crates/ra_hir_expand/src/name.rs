67d3600f59684b4fe3eabe7036fe7e7ce4db3257
Jeremy Kolb <kjeremy@gmail.com>
2019-11-24 19:01:51 -0500

    Expand compile_error!

--- crates/ra_hir_expand/src/name.rs
+++ crates/ra_hir_expand/src/name.rs
@@ -144,5 +144,6 @@ pub const BOX_TYPE: Name = Name::new_inline_ascii(3, b"Box");
 // Builtin Macros
 pub const FILE_MACRO: Name = Name::new_inline_ascii(4, b"file");
 pub const COLUMN_MACRO: Name = Name::new_inline_ascii(6, b"column");
+pub const COMPILE_ERROR_MACRO: Name = Name::new_inline_ascii(13, b"compile_error");
 pub const LINE_MACRO: Name = Name::new_inline_ascii(4, b"line");
 pub const STRINGIFY_MACRO: Name = Name::new_inline_ascii(9, b"stringify");
