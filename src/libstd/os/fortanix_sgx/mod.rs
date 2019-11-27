60091ee826b071a122ad4ef4ae833591e1db553b
David Tolnay <dtolnay@gmail.com>
2019-11-27 10:28:21 -0800

    Format libstd/os with rustfmt
    This commit applies rustfmt with rust-lang/rust's default settings to
    files in src/libstd/os *that are not involved in any currently open PR*
    to minimize merge conflicts. THe list of files involved in open PRs was
    determined by querying GitHub's GraphQL API with this script:
    https://gist.github.com/dtolnay/aa9c34993dc051a4f344d1b10e4487e8
    
    With the list of files from the script in outstanding_files, the
    relevant commands were:
    
        $ find src/libstd/os -name '*.rs' \
            | xargs rustfmt --edition=2018 --unstable-features --skip-children
        $ rg libstd/os outstanding_files | xargs git checkout --
    
    Repeating this process several months apart should get us coverage of
    most of the rest of the files.
    
    To confirm no funny business:
    
        $ git checkout $THIS_COMMIT^
        $ git show --pretty= --name-only $THIS_COMMIT \
            | xargs rustfmt --edition=2018 --unstable-features --skip-children
        $ git diff $THIS_COMMIT  # there should be no difference

--- src/libstd/os/fortanix_sgx/mod.rs
+++ src/libstd/os/fortanix_sgx/mod.rs
@@ -21,19 +21,20 @@ pub mod usercalls {
 
     /// Lowest-level interfaces to usercalls and usercall ABI type definitions.
     pub mod raw {
+        pub use crate::sys::abi::usercalls::raw::{
+            accept_stream, alloc, async_queues, bind_stream, close, connect_stream, exit, flush,
+            free, insecure_time, launch_thread, read, read_alloc, send, wait, write,
+        };
         pub use crate::sys::abi::usercalls::raw::{do_usercall, Usercalls as UsercallNrs};
-        pub use crate::sys::abi::usercalls::raw::{accept_stream, alloc, async_queues, bind_stream,
-                                           close, connect_stream, exit, flush, free, insecure_time,
-                                           launch_thread, read, read_alloc, send, wait, write};
 
         // fortanix-sgx-abi re-exports
-        pub use crate::sys::abi::usercalls::raw::{ByteBuffer, FifoDescriptor, Return, Usercall};
         pub use crate::sys::abi::usercalls::raw::Error;
-        pub use crate::sys::abi::usercalls::raw::{EV_RETURNQ_NOT_EMPTY, EV_UNPARK,
-                                           EV_USERCALLQ_NOT_FULL, FD_STDERR, FD_STDIN, FD_STDOUT,
-                                           RESULT_SUCCESS, USERCALL_USER_DEFINED, WAIT_INDEFINITE,
-                                           WAIT_NO};
+        pub use crate::sys::abi::usercalls::raw::{ByteBuffer, FifoDescriptor, Return, Usercall};
         pub use crate::sys::abi::usercalls::raw::{Fd, Result, Tcs};
+        pub use crate::sys::abi::usercalls::raw::{
+            EV_RETURNQ_NOT_EMPTY, EV_UNPARK, EV_USERCALLQ_NOT_FULL, FD_STDERR, FD_STDIN, FD_STDOUT,
+            RESULT_SUCCESS, USERCALL_USER_DEFINED, WAIT_INDEFINITE, WAIT_NO,
+        };
     }
 }
 
@@ -42,7 +43,7 @@ pub mod mem {
     pub use crate::sys::abi::mem::*;
 }
 
-pub use crate::sys::ext::{io, arch, ffi};
+pub use crate::sys::ext::{arch, ffi, io};
 
 /// Functions for querying thread-related information.
 pub mod thread {
