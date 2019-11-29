34080fb6a6a848c6781eb57f55737c27968cf7ec
Alexander Regueiro <alexreg@me.com>
2019-11-29 21:52:06 +0000

    Reblessed some tests.

--- src/test/ui/bad/bad-sized.rs
+++ src/test/ui/bad/bad-sized.rs
@@ -1,8 +1,12 @@
+// ignore-tidy-linelength
+
 trait Trait {}
 
 pub fn main() {
     let x: Vec<dyn Trait + Sized> = Vec::new();
-    //~^ ERROR only auto traits can be used as additional traits in a trait object
-    //~| ERROR the size for values of type
-    //~| ERROR the size for values of type
+    //~^ ERROR only auto traits can be used as additional traits in a trait object [E0225]
+    //~| ERROR the size for values of type `dyn std::marker::Sized` cannot be known at compilation time [E0277]
+    //~| the trait `std::marker::Sized` cannot be made into an object [E0038]
+    //~| ERROR the size for values of type `dyn std::marker::Sized` cannot be known at compilation time [E0277]
+    //~| the trait `std::marker::Sized` cannot be made into an object [E0038]
 }
--- src/test/ui/issues/issue-32963.rs
+++ src/test/ui/issues/issue-32963.rs
@@ -2,10 +2,11 @@ use std::mem;
 
 trait Misc {}
 
-fn size_of_copy<T: Copy+?Sized>() -> usize { mem::size_of::<T>() }
+fn size_of_copy<T: Copy + ?Sized>() -> usize { mem::size_of::<T>() }
 
 fn main() {
     size_of_copy::<dyn Misc + Copy>();
-    //~^ ERROR only auto traits can be used as additional traits in a trait object
-    //~| ERROR the trait bound `dyn Misc: std::marker::Copy` is not satisfied
+    //~^ ERROR only auto traits can be used as additional traits in a trait object [E0225]
+    //~| ERROR the trait bound `dyn std::marker::Copy: std::marker::Copy` is not satisfied [E0277]
+    //~| ERROR the trait `std::marker::Copy` cannot be made into an object [E0038]
 }
