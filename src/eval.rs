e77258322c71091afe23ecb64461243174e7fd49
Ralf Jung <post@ralfj.de>
2019-11-30 11:48:34 +0100

    some error classes should be impossible

--- src/eval.rs
+++ src/eval.rs
@@ -217,6 +217,8 @@ pub fn eval_main<'tcx>(tcx: TyCtxt<'tcx>, main_id: DefId, config: MiriConfig) ->
                 }
                 err_unsup!(NoMirFor(..)) =>
                     format!("{}. Did you set `MIRI_SYSROOT` to a Miri-enabled sysroot? You can prepare one with `cargo miri setup`.", e),
+                InterpError::Panic(_) | InterpError::InvalidProgram(_) =>
+                    bug!("This error should be impossible in Miri: {}", e),
                 _ => e.to_string()
             };
             e.print_backtrace();
