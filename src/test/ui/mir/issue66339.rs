c14ad54e8486b5b7d4c126f0f1086a170e870e07
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-12-05 10:40:24 +0300

    const-prop: Restrict scalar pair propagation
    We now only propagate a scalar pair if the Rvalue is a tuple with two
    scalars. This for example avoids propagating a (u8, u8) value when
    Rvalue has type `((), u8, u8)` (see the regression test). While this is
    a correct thing to do, implementation is tricky and will be done later.
    
    Fixes #66971
    Fixes #66339
    Fixes #67019

---
+++ src/test/ui/mir/issue66339.rs
@@ -0,0 +1,13 @@
+// compile-flags: -Z mir-opt-level=2
+// build-pass
+
+// This used to ICE in const-prop
+
+fn foo() {
+    let bar = |_| { };
+    let _ = bar("a");
+}
+
+fn main() {
+    foo();
+}
