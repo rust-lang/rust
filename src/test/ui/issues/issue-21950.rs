83c5b4ec40ceadfbcf3c558f06e04c48f01c1e52
Esteban Küber <esteban@kuber.com.ar>
2019-12-13 11:37:31 -0800

    Avoid output dependency on std spans

--- src/test/ui/issues/issue-21950.rs
+++ src/test/ui/issues/issue-21950.rs
@@ -1,8 +1,13 @@
-use std::ops::Add;
+trait Add<Rhs=Self> {
+    type Output;
+}
+
+impl Add for i32 {
+    type Output = i32;
+}
 
 fn main() {
-    let x = &10 as
-            &dyn Add;
-            //~^ ERROR E0393
-            //~| ERROR E0191
+    let x = &10 as &dyn Add;
+    //~^ ERROR E0393
+    //~| ERROR E0191
 }
