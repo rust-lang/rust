75353753cdcb993c277ce1d8bb366c708eabe2c6
Omer Ben-Amram <omerbenamram@gmail.com>
2019-12-15 17:10:39 +0200

    `npm run fix`

--- editors/code/src/notifications/publish_decorations.ts
+++ editors/code/src/notifications/publish_decorations.ts
@@ -9,14 +9,12 @@ export interface PublishDecorationsParams {
 }
 
 export function handle(params: PublishDecorationsParams) {
-    const targetEditor = vscode.window.visibleTextEditors.find(
-        editor => {
-            const unescapedUri = unescape(editor.document.uri.toString());
-            // Unescaped URI looks like:
-            // file:///c:/Workspace/ra-test/src/main.rs
-            return unescapedUri === params.uri
-        }
-    );
+    const targetEditor = vscode.window.visibleTextEditors.find(editor => {
+        const unescapedUri = unescape(editor.document.uri.toString());
+        // Unescaped URI looks like:
+        // file:///c:/Workspace/ra-test/src/main.rs
+        return unescapedUri === params.uri;
+    });
 
     if (!Server.config.highlightingOn || !targetEditor) {
         return;
