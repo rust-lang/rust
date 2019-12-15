427b1c33e993b55136ac8323600f9e3c714df3f4
Mazdak Farrokhzad <twingoow@gmail.com>
2019-12-15 04:09:47 +0100

    clarify bind-by-move-neither-can-livee..

--- src/test/ui/pattern/bindings-after-at/bind-by-move-neither-can-live-while-the-other-survives-1.rs
+++ src/test/ui/pattern/bindings-after-at/bind-by-move-neither-can-live-while-the-other-survives-1.rs
@@ -1,3 +1,7 @@
+// This test is taken directly from #16053.
+// It checks that you cannot use an AND-pattern (`binding @ pat`)
+// where one side is by-ref and the other is by-move.
+
 #![feature(bindings_after_at)]
 //~^ WARN the feature `bindings_after_at` is incomplete and may cause the compiler to crash
 
@@ -7,6 +11,26 @@ fn main() {
     let x = Some(X { x: () });
     match x {
         Some(ref _y @ _z) => { }, //~ ERROR cannot bind by-move and by-ref in the same pattern
+        None => panic!()
+    }
+
+    let x = Some(X { x: () });
+    match x {
+        Some(_z @ ref _y) => { }, //~ ERROR cannot bind by-move with sub-bindings
+        //~^ ERROR borrow of moved value
+        None => panic!()
+    }
+
+    let mut x = Some(X { x: () });
+    match x {
+        Some(ref mut _y @ _z) => { }, //~ ERROR cannot bind by-move and by-ref in the same pattern
+        None => panic!()
+    }
+
+    let mut x = Some(X { x: () });
+    match x {
+        Some(_z @ ref mut _y) => { }, //~ ERROR cannot bind by-move with sub-bindings
+        //~^ ERROR borrow of moved value
         None => panic!()
     }
 }
