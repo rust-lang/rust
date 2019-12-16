856079bcaa2303fe941c44d6787139e8929bd871
bjorn3 <bjorn3@users.noreply.github.com>
2019-12-16 21:47:13 +0100

    Fix testing when lldb is not installed

--- test.sh
+++ test.sh
@@ -36,9 +36,9 @@ $RUSTC example/example.rs --crate-type lib
 echo "[AOT] mini_core_hello_world"
 $RUSTC example/mini_core_hello_world.rs --crate-name mini_core_hello_world --crate-type bin -g
 ./target/out/mini_core_hello_world abc bcd
-if lldb -v; then
-(echo "break set -n main"; echo "run"; sleep 1; echo "si -c 10"; sleep 1; echo "frame variable") | lldb -- ./target/out/mini_core_hello_world abc bcd
-fi
+lldb -v && \
+    (echo "break set -n main"; echo "run"; sleep 1; echo "si -c 10"; sleep 1; echo "frame variable") \
+    | lldb -- ./target/out/mini_core_hello_world abc bcd
 
 exit 1
 
