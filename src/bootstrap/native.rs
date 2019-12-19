e44fc4577fdf5e269db0c7f574ac8d125067ccd8
Matthew Healy <matthew.healy@soundcloud.com>
2019-12-19 20:42:01 +0100

    Skip LLVM rebuild when skip-rebuild is true

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -70,6 +70,15 @@ impl Step for Llvm {
         let done_stamp = out_dir.join("llvm-finished-building");
 
         if done_stamp.exists() {
+            if builder.config.llvm_skip_rebuild {
+                builder.info(
+                    "Warning: \
+                    Using a potentially stale build of LLVM; \
+                    This may not behave well.",
+                );
+                return build_llvm_config;
+            }
+
             if let Some(llvm_commit) = llvm_info.sha() {
                 let done_contents = t!(fs::read(&done_stamp));
 
