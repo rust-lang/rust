5548609204ce0563cf6c164a9dd95a74a0160932
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-12-20 22:41:13 +0100

    Add const unstability attributes
    These are needed for rustc to be able to correctly handle stability of constness of intrinsics. Without either `rustc_const_unstable` or `rustc_const_stable` an intrinsic is not const evaluable at all.

--- library/stdarch/crates/core_arch/src/simd_llvm.rs
+++ library/stdarch/crates/core_arch/src/simd_llvm.rs
@@ -25,8 +25,10 @@ extern "platform-intrinsic" {
     pub fn simd_shuffle64<T, U>(x: T, y: T, idx: [u32; 64]) -> U;
     #[rustc_args_required_const(2)]
     pub fn simd_shuffle128<T, U>(x: T, y: T, idx: [u32; 128]) -> U;
-
+    
+    #[rustc_const_unstable(feature = "const_simd_insert", issue = "0")]
     pub fn simd_insert<T, U>(x: T, idx: u32, val: U) -> T;
+    #[rustc_const_unstable(feature = "const_simd_extract", issue = "0")]
     pub fn simd_extract<T, U>(x: T, idx: u32) -> U;
     //pub fn simd_select
     pub fn simd_bitmask<T, U>(x: T) -> U;
