6d7c6d7384233b58ba98090797f7c45b8040a68d
Mazdak Farrokhzad <twingoow@gmail.com>
2019-12-20 22:39:20 +0100

    into: simplify AddressOf logic after rebase

--- src/librustc_mir/build/expr/into.rs
+++ src/librustc_mir/build/expr/into.rs
@@ -300,16 +300,11 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                 mutability,
                 arg,
             } => {
-                let address_of = match mutability {
-                    hir::Mutability::Not => Rvalue::AddressOf(
-                        Mutability::Not,
-                        unpack!(block = this.as_read_only_place(block, arg)),
-                    ),
-                    hir::Mutability::Mut => Rvalue::AddressOf(
-                        Mutability::Mut,
-                        unpack!(block = this.as_place(block, arg)),
-                    ),
+                let place = match mutability {
+                    hir::Mutability::Not => this.as_read_only_place(block, arg),
+                    hir::Mutability::Mut => this.as_place(block, arg),
                 };
+                let address_of = Rvalue::AddressOf(mutability, unpack!(block = place));
                 this.cfg.push_assign(block, source_info, destination, address_of);
                 block.unit()
             }
