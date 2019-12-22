e56a86162ceeb285c48132a103a6559f4d406ba7
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-12-22 21:42:50 +0100

    Show `const_err` lints

--- src/test/ui/consts/miri_unleashed/const_refers_to_static.rs
+++ src/test/ui/consts/miri_unleashed/const_refers_to_static.rs
@@ -1,5 +1,5 @@
 // compile-flags: -Zunleash-the-miri-inside-of-you
-#![allow(const_err)]
+#![warn(const_err)]
 
 #![feature(const_raw_ptr_deref)]
 
@@ -14,19 +14,19 @@ const BOO: &usize = { //~ ERROR undefined behavior to use this value
 
 const FOO: usize = {
     static FOO: AtomicUsize = AtomicUsize::new(0);
-    FOO.fetch_add(1, Ordering::Relaxed) // FIXME: this should error
+    FOO.fetch_add(1, Ordering::Relaxed) //~ WARN any use of this value will cause an error
     //~^ WARN skipping const checks
     //~| WARN skipping const checks
 };
 
 const BAR: usize = {
     static FOO: AtomicUsize = AtomicUsize::new(0);
-    unsafe { *(&FOO as *const _ as *const usize) } // FIXME: this should error
+    unsafe { *(&FOO as *const _ as *const usize) } //~ WARN any use of this value will cause an err
     //~^ WARN skipping const checks
 };
 
 static mut MUTABLE: u32 = 0;
-const BAD: u32 = unsafe { MUTABLE }; // FIXME: this should error
+const BAD: u32 = unsafe { MUTABLE }; //~ WARN any use of this value will cause an error
 //~^ WARN skipping const checks
 
 // ok some day perhaps
