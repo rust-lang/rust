7d5f36a1e5a277da8e1419557ba1edcfe94c6f07
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-12-23 23:11:03 +0100

    A cycle error on a diverging function is now a const stack overflow again

--- src/test/ui/consts/uninhabited-const-issue-61744.rs
+++ src/test/ui/consts/uninhabited-const-issue-61744.rs
@@ -5,15 +5,15 @@ pub const unsafe fn fake_type<T>() -> T {
 }
 
 pub const unsafe fn hint_unreachable() -> ! {
-    fake_type() //~ ERROR cycle detected when const-evaluating `hint_unreachable` [E0391]
+    fake_type() //~ ERROR evaluation of constant value failed
 }
 
 trait Const {
-    const CONSTANT: i32 = unsafe { fake_type() };
+    const CONSTANT: i32 = unsafe { fake_type() }; //~ ERROR any use of this value will cause an err
 }
 
-impl <T> Const for T {}
+impl<T> Const for T {}
 
 pub fn main() -> () {
-    dbg!(i32::CONSTANT);
+    dbg!(i32::CONSTANT); //~ ERROR erroneous constant used
 }
