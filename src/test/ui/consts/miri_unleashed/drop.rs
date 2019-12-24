db20f248ebcd2a6cb5230b3bde7f400a380b0b87
Ralf Jung <post@ralfj.de>
2019-12-24 12:20:36 +0100

    add test for dropping in const

---
+++ src/test/ui/consts/miri_unleashed/drop.rs
@@ -0,0 +1,19 @@
+// compile-flags: -Zunleash-the-miri-inside-of-you
+#![deny(const_err)]
+
+use std::mem::ManuallyDrop;
+
+fn main() {}
+
+static TEST_OK: () = {
+    let v: Vec<i32> = Vec::new();
+    let _v = ManuallyDrop::new(v);
+};
+
+// Make sure we catch executing bad drop functions.
+// The actual error is located in `real_drop_in_place` so we can't capture it with the
+// error annotations here.
+static TEST_BAD: () = {
+    let _v: Vec<i32> = Vec::new();
+    //~^ WARN skipping const check
+};
