6177e6df7e212340ad194319d698f321b44bb35a
Christian Poveda <git@christianpoveda.xyz>
2019-12-24 11:21:00 -0500

    provide correct name for shim

--- src/shims/foreign_items.rs
+++ src/shims/foreign_items.rs
@@ -494,7 +494,7 @@ pub trait EvalContextExt<'mir, 'tcx: 'mir>: crate::MiriEvalContextExt<'mir, 'tcx
                 this.write_scalar(Scalar::from_int(result, dest.layout.size), dest)?;
             }
 
-            "stat64" => {
+            "stat$INODE64" => {
                 let result = this.stat(args[0], args[1])?;
                 this.write_scalar(Scalar::from_int(result, dest.layout.size), dest)?;
             }
