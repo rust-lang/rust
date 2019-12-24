f507403517299765b6d628fd677c6d880899b83c
Mazdak Farrokhzad <twingoow@gmail.com>
2019-12-24 22:32:36 +0100

    define_id_collections -> rustc_data_structures

--- src/librustc/util/nodemap.rs
+++ src/librustc/util/nodemap.rs
@@ -4,12 +4,7 @@ use crate::hir::def_id::DefId;
 use crate::hir::{HirId, ItemLocalId};
 use syntax::ast;
 
-macro_rules! define_id_collections {
-    ($map_name:ident, $set_name:ident, $key:ty) => {
-        pub type $map_name<T> = rustc_data_structures::fx::FxHashMap<$key, T>;
-        pub type $set_name = rustc_data_structures::fx::FxHashSet<$key>;
-    };
-}
+use rustc_data_structures::define_id_collections;
 
 define_id_collections!(NodeMap, NodeSet, ast::NodeId);
 define_id_collections!(DefIdMap, DefIdSet, DefId);
