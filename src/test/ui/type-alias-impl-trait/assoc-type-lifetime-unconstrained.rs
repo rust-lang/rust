79439f85eaadd71376c6b39dc152cca2cf80a6c2
Matthew Jasper <mjjasper1@gmail.com>
2019-12-26 23:41:32 +0000

    Check associated opaque types don't use unconstrained lifetimes

--- src/test/ui/type-alias-impl-trait/assoc-type-lifetime-unconstrained.rs
+++ src/test/ui/type-alias-impl-trait/assoc-type-lifetime-unconstrained.rs
@@ -1,10 +1,9 @@
-// Tests that we still detect defining usages when
-// lifetimes are used in an associated opaque type
-// check-pass
+// Tests that we don't allow unconstrained lifetime parameters in impls when
+// the lifetime is used in an associated opaque type.
 
 #![feature(type_alias_impl_trait)]
 
-trait UnwrapItemsExt<'a> {
+trait UnwrapItemsExt {
     type Iter;
     fn unwrap_items(self) -> Self::Iter;
 }
@@ -15,7 +14,8 @@ trait MyTrait<'a> {}
 
 impl<'a> MyTrait<'a> for MyStruct {}
 
-impl<'a, I> UnwrapItemsExt<'a> for I {
+impl<'a, I> UnwrapItemsExt for I {
+    //~^ ERROR the lifetime parameter `'a` is not constrained
     type Iter = impl MyTrait<'a>;
 
     fn unwrap_items(self) -> Self::Iter {
