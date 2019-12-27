4aef81eb85a47ff123db8204c59c610fa28f8693
Oliver Scherer <github35764891676564198441@oli-obk.de>
2019-12-27 14:26:05 +0100

    Remove `to_ptr` uses

--- src/stacked_borrows.rs
+++ src/stacked_borrows.rs
@@ -507,7 +507,7 @@ trait EvalContextPrivExt<'mir, 'tcx: 'mir>: crate::MiriEvalContextExt<'mir, 'tcx
     ) -> InterpResult<'tcx> {
         let this = self.eval_context_mut();
         let protector = if protect { Some(this.frame().extra.call_id) } else { None };
-        let ptr = place.ptr.to_ptr().expect("we should have a proper pointer");
+        let ptr = place.ptr.assert_ptr();
         trace!(
             "reborrow: {} reference {:?} derived from {:?} (pointee {}): {:?}, size {}",
             kind,
