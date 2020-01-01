f474c0084a23207f89bd082b441bfb4701c8c2dc
LeSeulArtichaut <leseulartichaut@gmail.com>
2020-01-02 00:48:58 +0100

    Added test

---
+++ src/test/ui/match/match-same-name-enum-variant.rs
@@ -0,0 +1,33 @@
+// Test for issue #67776: binding named the same as enum variant
+// should report a warning even when matching against a borrow
+
+// check-pass
+
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+
+enum Foo {
+    Bar,
+    Baz,
+}
+
+
+fn fn2(e: Foo) {
+    match e {
+        Bar => println!("A"),
+        //~^ WARNING named the same as one of the variants of the type `Foo`
+        Baz => println!("B"),
+        //~^ WARNING named the same as one of the variants of the type `Foo`
+    }
+}
+
+fn fn1(e: &Foo) {
+    match e {
+        Bar => println!("A"),
+        //~^ WARNING named the same as one of the variants of the type `Foo`
+        Baz => println!("B"),
+        //~^ WARNING named the same as one of the variants of the type `Foo`
+    }
+}
+
+fn main() {}
