171b1bb59c5cb6b41532ceaf7d353a229aeeab81
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2020-01-04 00:16:37 +0100

    Add a new misc checking 3 block

--- src/librustc_interface/passes.rs
+++ src/librustc_interface/passes.rs
@@ -844,31 +844,33 @@ fn analysis(tcx: TyCtxt<'_>, cnum: CrateNum) -> Result<()> {
         );
     });
 
-    parallel!(
-        {
-            tcx.ensure().privacy_access_levels(LOCAL_CRATE);
-        },
-        {
-            sess.time("MIR_borrow_checking", || {
-                tcx.par_body_owners(|def_id| tcx.ensure().mir_borrowck(def_id));
-            });
-        },
-        {
-            sess.time("dumping_chalk_like_clauses", || {
-                rustc_traits::lowering::dump_program_clauses(tcx);
-            });
-        },
-        {
-            sess.time("MIR_effect_checking", || {
-                for def_id in tcx.body_owners() {
-                    mir::transform::check_unsafety::check_unsafety(tcx, def_id)
-                }
-            });
-        },
-        {
-            sess.time("layout_testing", || layout_test::test_layout(tcx));
-        }
-    );
+    sess.time("misc_checking_3", || {
+        parallel!(
+            {
+                tcx.ensure().privacy_access_levels(LOCAL_CRATE);
+            },
+            {
+                sess.time("MIR_borrow_checking", || {
+                    tcx.par_body_owners(|def_id| tcx.ensure().mir_borrowck(def_id));
+                });
+            },
+            {
+                sess.time("dumping_chalk_like_clauses", || {
+                    rustc_traits::lowering::dump_program_clauses(tcx);
+                });
+            },
+            {
+                sess.time("MIR_effect_checking", || {
+                    for def_id in tcx.body_owners() {
+                        mir::transform::check_unsafety::check_unsafety(tcx, def_id)
+                    }
+                });
+            },
+            {
+                sess.time("layout_testing", || layout_test::test_layout(tcx));
+            }
+        );
+    });
 
     // Avoid overwhelming user with errors if borrow checking failed.
     // I'm not sure how helpful this is, to be honest, but it avoids a
@@ -879,7 +881,7 @@ fn analysis(tcx: TyCtxt<'_>, cnum: CrateNum) -> Result<()> {
         return Err(ErrorReported);
     }
 
-    sess.time("misc_checking_3", || {
+    sess.time("misc_checking_4", || {
         parallel!(
             {
                 tcx.ensure().check_private_in_public(LOCAL_CRATE);
