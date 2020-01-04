12545c75ff9c9ad72ad50bbbb3cc1f5c04c8037a
Laurent Bonnans <bonnans.l@gmail.com>
2020-01-05 00:58:41 +0100

    Handle multiple error fix suggestions carefuly
    The existing code seems to assume that substitutions spans are disjoint,
    which is not always the case.
    
    In the example:
    
        pub trait AAAA {}
        pub trait B {}
        pub trait C {}
        pub type T<P: AAAA + B + C> = P;
    
    , we get three substituions starting from ':' and ending respectively at
    the end of each trait token.
    
    With the former offset calculation, this would cause `underline_start` to
    eventually become negative before being converted to `usize`...
    
    The new version may report erroneous results for non perfectly overlapping
    substitutions but I don't know if such examples exist. Alternatively, we
    could detect these cases and trim out overlapping substitutions.

---
+++ src/test/ui/type/issue-67690-type-alias-bound-diagnostic-crash.rs
@@ -0,0 +1,8 @@
+// Regression test for issue #67690
+// Rustc endless loop out-of-memory and consequent SIGKILL in generic new type
+
+// check-pass
+pub type T<P: Send + Send + Send> = P;
+//~^ WARN bounds on generic parameters are not enforced in type aliases
+
+fn main() {}
