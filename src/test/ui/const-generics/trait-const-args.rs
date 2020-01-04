a47983631f3659892a969496bfb896ea0cb494d0
Ben Lewis <benlewisj@gmail.com>
2020-01-04 13:42:02 +1300

    Add lazy normalization tests

---
+++ src/test/ui/const-generics/trait-const-args.rs
@@ -0,0 +1,29 @@
+// check-pass
+
+#![feature(const_generics)]
+//~^ WARN the feature `const_generics` is incomplete and may cause the compiler to crash
+
+struct Const<const N: usize>;
+trait Foo<const N: usize> {}
+
+impl<const N: usize> Foo<{N}> for Const<{N}> {}
+
+fn foo_impl(_: impl Foo<3>) {}
+
+fn foo_explicit<T: Foo<3>>(_: T) {}
+
+fn foo_where<T>(_: T) where T: Foo<3> {}
+
+fn main() {
+    // FIXME this causes a stack overflow in rustc
+    // foo_impl(Const);
+    foo_impl(Const::<3>);
+
+    // FIXME this causes a stack overflow in rustc
+    // foo_explicit(Const);
+    foo_explicit(Const::<3>);
+
+    // FIXME this causes a stack overflow in rustc
+    // foo_where(Const);
+    foo_where(Const::<3>);
+}
