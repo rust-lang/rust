56a0aec07fa998e43702216a7e18133af481c076
Camille GILLOT <gillot.camille@gmail.com>
2020-01-05 19:52:56 +0100

    Move subst_and_normalize_erasing_regionsto rustc::ty.

--- src/librustc/traits/codegen/mod.rs
+++ src/librustc/traits/codegen/mod.rs
@@ -8,7 +8,6 @@ use crate::traits::{
     FulfillmentContext, Obligation, ObligationCause, SelectionContext, TraitEngine, Vtable,
 };
 use crate::ty::fold::TypeFoldable;
-use crate::ty::subst::{Subst, SubstsRef};
 use crate::ty::{self, TyCtxt};
 
 /// Attempts to resolve an obligation to a vtable. The result is
@@ -74,31 +73,6 @@ pub fn codegen_fulfill_obligation<'tcx>(
         info!("Cache miss: {:?} => {:?}", trait_ref, vtable);
         vtable
     })
-}
-
-impl<'tcx> TyCtxt<'tcx> {
-    /// Monomorphizes a type from the AST by first applying the
-    /// in-scope substitutions and then normalizing any associated
-    /// types.
-    pub fn subst_and_normalize_erasing_regions<T>(
-        self,
-        param_substs: SubstsRef<'tcx>,
-        param_env: ty::ParamEnv<'tcx>,
-        value: &T,
-    ) -> T
-    where
-        T: TypeFoldable<'tcx>,
-    {
-        debug!(
-            "subst_and_normalize_erasing_regions(\
-             param_substs={:?}, \
-             value={:?}, \
-             param_env={:?})",
-            param_substs, value, param_env,
-        );
-        let substituted = value.subst(self, param_substs);
-        self.normalize_erasing_regions(param_env, substituted)
-    }
 }
 
 // # Global Cache
