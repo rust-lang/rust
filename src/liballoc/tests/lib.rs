503d06b90dfd8f6055c586c488dbd3a741e9f0c2
dylan_DPC <dylan.dpc@gmail.com>
2020-01-06 23:28:47 +0530

    oh the one that was left behind

--- src/liballoc/tests/lib.rs
+++ src/liballoc/tests/lib.rs
@@ -11,7 +11,6 @@
 #![feature(associated_type_bounds)]
 #![feature(binary_heap_into_iter_sorted)]
 #![feature(binary_heap_drain_sorted)]
-#![feature(vec_remove_item)]
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
