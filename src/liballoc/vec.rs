e6d95ce0b88bddd9cbb54bd272ba7c48601ba7a0
Grachev Mikhail <work@mgrachev.com>
2020-01-06 15:18:03 +0300

    Formatting an example for method Vec.retain

--- src/liballoc/vec.rs
+++ src/liballoc/vec.rs
@@ -1054,7 +1054,7 @@ impl<T> Vec<T> {
     ///
     /// ```
     /// let mut vec = vec![1, 2, 3, 4];
-    /// vec.retain(|&x| x%2 == 0);
+    /// vec.retain(|&x| x % 2 == 0);
     /// assert_eq!(vec, [2, 4]);
     /// ```
     ///
