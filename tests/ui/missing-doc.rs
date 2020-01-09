77e5a1b22765d983ac43193edd22b83a6d7d723e
Yuki Okushi <huyuumi.dev@gmail.com>
2020-01-09 09:04:53 +0900

    Split up `missing-doc` ui test

--- tests/ui/missing-doc.rs
+++ tests/ui/missing-doc.rs
@@ -2,29 +2,13 @@
 // When denying at the crate level, be sure to not get random warnings from the
 // injected intrinsics by the compiler.
 #![allow(dead_code)]
-#![feature(associated_type_defaults, global_asm)]
+#![feature(global_asm)]
 
 //! Some garbage docs for the crate here
 #![doc = "More garbage"]
 
 type Typedef = String;
 pub type PubTypedef = String;
-
-struct Foo {
-    a: isize,
-    b: isize,
-}
-
-pub struct PubFoo {
-    pub a: isize,
-    b: isize,
-}
-
-#[allow(clippy::missing_docs_in_private_items)]
-pub struct PubFoo2 {
-    pub a: isize,
-    pub c: isize,
-}
 
 mod module_no_dox {}
 pub mod pub_module_no_dox {}
@@ -35,69 +19,6 @@ pub fn foo2() {}
 fn foo3() {}
 #[allow(clippy::missing_docs_in_private_items)]
 pub fn foo4() {}
-
-/// dox
-pub trait A {
-    /// dox
-    fn foo(&self);
-    /// dox
-    fn foo_with_impl(&self) {}
-}
-
-#[allow(clippy::missing_docs_in_private_items)]
-trait B {
-    fn foo(&self);
-    fn foo_with_impl(&self) {}
-}
-
-pub trait C {
-    fn foo(&self);
-    fn foo_with_impl(&self) {}
-}
-
-#[allow(clippy::missing_docs_in_private_items)]
-pub trait D {
-    fn dummy(&self) {}
-}
-
-/// dox
-pub trait E {
-    type AssociatedType;
-    type AssociatedTypeDef = Self;
-
-    /// dox
-    type DocumentedType;
-    /// dox
-    type DocumentedTypeDef = Self;
-    /// dox
-    fn dummy(&self) {}
-}
-
-impl Foo {
-    pub fn foo() {}
-    fn bar() {}
-}
-
-impl PubFoo {
-    pub fn foo() {}
-    /// dox
-    pub fn foo1() {}
-    fn foo2() {}
-    #[allow(clippy::missing_docs_in_private_items)]
-    pub fn foo3() {}
-}
-
-#[allow(clippy::missing_docs_in_private_items)]
-trait F {
-    fn a();
-    fn b(&self);
-}
-
-// should need to redefine documentation for implementations of traits
-impl F for Foo {
-    fn a() {}
-    fn b(&self) {}
-}
 
 // It sure is nice if doc(hidden) implies allow(missing_docs), and that it
 // applies recursively
