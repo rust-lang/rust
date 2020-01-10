7ba25acd7a4a119fcfdb6beb58d3958647236b30
Lzu Tao <taolzu@gmail.com>
2020-01-10 10:34:35 +0000

    Revert "Rollup merge of #67727 - Dylan-DPC:stabilise/remove_item, r=alexcrichton"
    This reverts commit 4ed415b5478c74094c2859abfddb959588cd6bb1, reversing
    changes made to 3cce950743e8aa74a4378dfdefbbc80223a00865.

--- src/librustc/lib.rs
+++ src/librustc/lib.rs
@@ -50,6 +50,7 @@
 #![feature(thread_local)]
 #![feature(trace_macros)]
 #![feature(trusted_len)]
+#![feature(vec_remove_item)]
 #![feature(stmt_expr_attributes)]
 #![feature(integer_atomics)]
 #![feature(test)]
--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -10,6 +10,7 @@
 #![feature(nll)]
 #![feature(set_stdio)]
 #![feature(test)]
+#![feature(vec_remove_item)]
 #![feature(ptr_offset_from)]
 #![feature(crate_visibility_modifier)]
 #![feature(const_fn)]
