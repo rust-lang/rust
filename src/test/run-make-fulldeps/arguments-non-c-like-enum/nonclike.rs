26bb0f15e7e97bc93385296c2932193fe6da6300
John VanEnk <jvanenk@fastly.com>
2020-01-10 17:59:18 -0800

    Add similar examples that work to each test.

--- src/test/run-make-fulldeps/arguments-non-c-like-enum/nonclike.rs
+++ src/test/run-make-fulldeps/arguments-non-c-like-enum/nonclike.rs
@@ -1,7 +1,23 @@
 #![crate_type = "lib"]
 #![crate_name = "nonclike"]
 
-#[repr(C,u8)]
+#[repr(C, u8)]
+pub enum TT {
+    AA(u64, u64),
+    BB,
+}
+
+#[no_mangle]
+pub extern "C" fn tt_add(a: TT, b: TT) -> u64 {
+    match (a, b) {
+        (TT::AA(a1, b1), TT::AA(a2, b2)) => a1 + a2 + b1 + b2,
+        (TT::AA(a1, b1), TT::BB) => a1 + b1,
+        (TT::BB, TT::AA(a1, b1)) => a1 + b1,
+        _ => 0,
+    }
+}
+
+#[repr(C, u8)]
 pub enum T {
     A(u64),
     B,
@@ -9,7 +25,7 @@ pub enum T {
 
 #[no_mangle]
 pub extern "C" fn t_add(a: T, b: T) -> u64 {
-    match (a,b) {
+    match (a, b) {
         (T::A(a), T::A(b)) => a + b,
         (T::A(a), T::B) => a,
         (T::B, T::A(b)) => b,
