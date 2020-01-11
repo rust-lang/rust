8afd8e7425ef45094ede1dc290ce0572a69947be
John VanEnk <jvanenk@fastly.com>
2020-01-10 17:59:18 -0800

    Add similar examples that work to each test.

--- src/test/run-make-fulldeps/return-non-c-like-enum/nonclike.rs
+++ src/test/run-make-fulldeps/return-non-c-like-enum/nonclike.rs
@@ -1,6 +1,17 @@
 #![crate_type = "lib"]
 #![crate_name = "nonclike"]
 
+#[repr(C, u8)]
+pub enum TT {
+    AA(u64, u64),
+    BB,
+}
+
+#[no_mangle]
+pub extern "C" fn tt_new(a: u64, b: u64) -> TT {
+    TT::AA(a, b)
+}
+
 #[repr(C,u8)]
 pub enum T {
     A(u64),
