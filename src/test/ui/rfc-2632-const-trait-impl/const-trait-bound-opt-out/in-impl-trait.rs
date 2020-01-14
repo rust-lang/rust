23ea42cfd14e185f3af2232083318fdca90a4b6c
Dylan MacKenzie <ecstaticmorse@gmail.com>
2020-01-13 20:30:34 -0800

    Update tests

--- src/test/ui/rfc-2632-const-trait-impl/const-trait-bound-opt-out/in-impl-trait.rs
+++ src/test/ui/rfc-2632-const-trait-impl/const-trait-bound-opt-out/in-impl-trait.rs
@@ -8,18 +8,14 @@ impl T for S {}
 
 fn rpit() -> impl ?const T { S }
 //~^ ERROR `?const` is not permitted in `impl Trait`
-//~| ERROR `?const` on trait bounds is not yet implemented
 
 fn apit(_: impl ?const T) {}
 //~^ ERROR `?const` is not permitted in `impl Trait`
-//~| ERROR `?const` on trait bounds is not yet implemented
 
 fn rpit_assoc_bound() -> impl IntoIterator<Item: ?const T> { Some(S) }
 //~^ ERROR `?const` is not permitted in `impl Trait`
-//~| ERROR `?const` on trait bounds is not yet implemented
 
 fn apit_assoc_bound(_: impl IntoIterator<Item: ?const T>) {}
 //~^ ERROR `?const` is not permitted in `impl Trait`
-//~| ERROR `?const` on trait bounds is not yet implemented
 
 fn main() {}
