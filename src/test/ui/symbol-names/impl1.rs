30dba975400054424dd0ff31bea9fd312bc68d2c
Ben Lewis <benlewisj@gmail.com>
2020-01-14 20:41:14 +1300

    Normalize symbol hash in ui test for legacy symbol mangling, as it's dependent on the number of bits within consts.

--- src/test/ui/symbol-names/impl1.rs
+++ src/test/ui/symbol-names/impl1.rs
@@ -3,6 +3,8 @@
 // revisions: legacy v0
 //[legacy]compile-flags: -Z symbol-mangling-version=legacy
     //[v0]compile-flags: -Z symbol-mangling-version=v0
+//[legacy]normalize-stderr-32-bit: "hdb62078998ce7ea8" -> "SYMBOL_HASH"
+//[legacy]normalize-stderr-64bit: "h62e540f14f879d56" -> "SYMBOL_HASH"
 
 #![feature(optin_builtin_traits, rustc_attrs)]
 #![allow(dead_code)]
