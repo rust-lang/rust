0b60f1f2ae54b0bdb1623606db5dd18da1e80517
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2020-01-16 22:19:55 +0300

    Ignore some tests on platforms without libstd spans

--- src/test/ui/issues/issue-38857.rs
+++ src/test/ui/issues/issue-38857.rs
@@ -1,3 +1,8 @@
+// FIXME: missing sysroot spans (#53081)
+// ignore-i586-unknown-linux-gnu
+// ignore-i586-unknown-linux-musl
+// ignore-i686-unknown-linux-musl
+
 fn main() {
     let a = std::sys::imp::process::process_common::StdioPipes { ..panic!() };
     //~^ ERROR failed to resolve: could not find `imp` in `sys` [E0433]
