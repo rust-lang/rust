5022dd3b854792dd72d396e1586aa5b5c09975b2
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-01-16 14:26:43 +0100

    Extend url in titles test

--- src/test/rustdoc/remove-url-from-headings.rs
+++ src/test/rustdoc/remove-url-from-headings.rs
@@ -3,10 +3,15 @@
 // @has foo/fn.foo.html
 // !@has - '//a[@href="http://a.a"]'
 // @has - '//a[@href="#implementing-stuff-somewhere"]' 'Implementing stuff somewhere'
+// @has - '//a[@href="#another-one-urg"]' 'Another one urg'
 
 /// fooo
 ///
 /// # Implementing [stuff](http://a.a) somewhere
 ///
 /// hello
+///
+/// # Another [one][two] urg
+///
+/// [two]: http://a.a
 pub fn foo() {}
