33d637f2ddb5e5eab6d5dfaf75042dfc1a58d241
Florian Diebold <florian.diebold@freiheit.com>
2020-01-17 16:59:02 +0100

    Fix inference for shift operators
    Fixes #2602.

--- crates/ra_hir_ty/src/op.rs
+++ crates/ra_hir_ty/src/op.rs
@@ -1,13 +1,21 @@
-//! FIXME: write short doc here
-use hir_def::expr::{BinaryOp, CmpOp};
+//! Helper functions for binary operator type inference.
+use hir_def::expr::{ArithOp, BinaryOp, CmpOp};
 
 use super::{InferTy, Ty, TypeCtor};
 use crate::ApplicationTy;
 
-pub(super) fn binary_op_return_ty(op: BinaryOp, rhs_ty: Ty) -> Ty {
+pub(super) fn binary_op_return_ty(op: BinaryOp, lhs_ty: Ty, rhs_ty: Ty) -> Ty {
     match op {
         BinaryOp::LogicOp(_) | BinaryOp::CmpOp(_) => Ty::simple(TypeCtor::Bool),
         BinaryOp::Assignment { .. } => Ty::unit(),
+        BinaryOp::ArithOp(ArithOp::Shl) | BinaryOp::ArithOp(ArithOp::Shr) => match lhs_ty {
+            Ty::Apply(ApplicationTy { ctor, .. }) => match ctor {
+                TypeCtor::Int(..) | TypeCtor::Float(..) => lhs_ty,
+                _ => Ty::Unknown,
+            },
+            Ty::Infer(InferTy::IntVar(..)) | Ty::Infer(InferTy::FloatVar(..)) => lhs_ty,
+            _ => Ty::Unknown,
+        },
         BinaryOp::ArithOp(_) => match rhs_ty {
             Ty::Apply(ApplicationTy { ctor, .. }) => match ctor {
                 TypeCtor::Int(..) | TypeCtor::Float(..) => rhs_ty,
@@ -36,6 +44,7 @@ pub(super) fn binary_op_rhs_expectation(op: BinaryOp, lhs_ty: Ty) -> Ty {
                 _ => Ty::Unknown,
             }
         }
+        BinaryOp::ArithOp(ArithOp::Shl) | BinaryOp::ArithOp(ArithOp::Shr) => Ty::Unknown,
         BinaryOp::CmpOp(CmpOp::Ord { .. })
         | BinaryOp::Assignment { op: Some(_) }
         | BinaryOp::ArithOp(_) => match lhs_ty {
