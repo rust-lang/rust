643a708d05458830e7e185adfb3e74464d6d699d
Aaron Hill <aa1ronham@gmail.com>
2020-01-18 16:58:05 -0500

    Unconditionally enable never-type fallback for Crater

--- src/librustc/ty/context.rs
+++ src/librustc/ty/context.rs
@@ -2286,7 +2286,8 @@ impl<'tcx> TyCtxt<'tcx> {
 
     #[inline]
     pub fn mk_diverging_default(self) -> Ty<'tcx> {
-        if self.features().never_type_fallback { self.types.never } else { self.types.unit }
+        self.types.never
+        //if self.features().never_type_fallback { self.types.never } else { self.types.unit }
     }
 
     #[inline]
