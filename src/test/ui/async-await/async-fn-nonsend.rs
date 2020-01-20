a804a455289cfa6d39ec92903959c4614c48d080
Mark Rousskov <mark.simulacrum@gmail.com>
2020-01-20 12:17:12 -0500

    Fix UI test
    fmt::Formatter is still not Send/Sync, but the UI test emitted two errors, for
    the dyn Write and the Void inside Formatter. As of this PR, the Void is now
    gone, but the dyn Write remains.

--- src/test/ui/async-await/async-fn-nonsend.rs
+++ src/test/ui/async-await/async-fn-nonsend.rs
@@ -2,15 +2,15 @@
 // edition:2018
 // compile-flags: --crate-type lib
 
-use std::{
-    cell::RefCell,
-    fmt::Debug,
-    rc::Rc,
-};
+use std::{cell::RefCell, fmt::Debug, rc::Rc};
 
-fn non_sync() -> impl Debug { RefCell::new(()) }
+fn non_sync() -> impl Debug {
+    RefCell::new(())
+}
 
-fn non_send() -> impl Debug { Rc::new(()) }
+fn non_send() -> impl Debug {
+    Rc::new(())
+}
 
 fn take_ref<T>(_: &T) {}
 
@@ -53,5 +53,4 @@ pub fn pass_assert() {
     //~^ ERROR future cannot be sent between threads safely
     assert_send(non_sync_with_method_call());
     //~^ ERROR future cannot be sent between threads safely
-    //~^^ ERROR future cannot be sent between threads safely
 }
