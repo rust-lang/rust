72792f6bc5cc3c81b53fa8e71304b0f7321f8e22
TomasKralCZ <tomas@kral.hk>
2020-01-20 10:08:36 +0100

    changed 'descendants()' to 'children()'

--- crates/ra_syntax/src/ast/extensions.rs
+++ crates/ra_syntax/src/ast/extensions.rs
@@ -236,10 +236,7 @@ impl ast::LetStmt {
     }
 
     pub fn eq_token(&self) -> Option<SyntaxToken> {
-        self.syntax()
-            .descendants_with_tokens()
-            .find(|t| t.kind() == EQ)
-            .and_then(|it| it.into_token())
+        self.syntax().children_with_tokens().find(|t| t.kind() == EQ).and_then(|it| it.into_token())
     }
 }
 
