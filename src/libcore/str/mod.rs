2dbb6faae09c53ac990b5cf9fc290018e96ba3fe
Heinz N. Gies <heinz@licenser.net>
2020-01-22 22:49:41 +0100

    Fix formating again

--- src/libcore/str/mod.rs
+++ src/libcore/str/mod.rs
@@ -1536,8 +1536,8 @@ mod avx2 {
     use crate::arch::x86::{
         __m256i, _mm256_add_epi8, _mm256_alignr_epi8, _mm256_and_si256, _mm256_cmpeq_epi8,
         _mm256_cmpgt_epi8, _mm256_loadu_si256, _mm256_or_si256, _mm256_permute2x128_si256,
-        _mm256_set1_epi8, _mm256_setr_epi8, _mm256_setzero_si256,
-        _mm256_shuffle_epi8, _mm256_srli_epi16, _mm256_subs_epu8, _mm256_testz_si256,
+        _mm256_set1_epi8, _mm256_setr_epi8, _mm256_setzero_si256, _mm256_shuffle_epi8,
+        _mm256_srli_epi16, _mm256_subs_epu8, _mm256_testz_si256,
     };
     #[cfg(target_arch = "x86_64")]
     use crate::arch::x86_64::{
