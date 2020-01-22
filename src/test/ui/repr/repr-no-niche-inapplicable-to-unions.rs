712c4336892cf79c72ff9c59d2488282000edd1e
Felix S. Klock II <pnkfelix@pnkfx.org>
2020-01-22 18:50:38 -0500

    tests for `#[repr(no_niche)]`.

---
+++ src/test/ui/repr/repr-no-niche-inapplicable-to-unions.rs
@@ -0,0 +1,14 @@
+#![feature(no_niche)]
+
+use std::num::NonZeroU8 as N8;
+use std::num::NonZeroU16 as N16;
+
+#[repr(no_niche)]
+pub union Cloaked1 { _A: N16 }
+//~^^ ERROR attribute should be applied to struct or enum [E0517]
+
+#[repr(no_niche)]
+pub union Cloaked2 { _A: N16, _B: (u8, N8) }
+//~^^ ERROR attribute should be applied to struct or enum [E0517]
+
+fn main() { }
