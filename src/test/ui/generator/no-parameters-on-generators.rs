b30bf57d7cefd731f1cd84389fdc88167a4702da
Jonas Schievink <jonasschievink@gmail.com>
2020-01-25 02:29:09 +0100

    Adjust tests to type inference changes
    This makes some error messages ungreat, but those seem to be preexisting
    bugs that also apply to closures / return position `impl Trait` in
    general.

--- src/test/ui/generator/no-parameters-on-generators.rs
+++ src/test/ui/generator/no-parameters-on-generators.rs
@@ -1,8 +1,10 @@
 #![feature(generators)]
 
 fn main() {
-    let gen = |start| { //~ ERROR generators cannot have explicit parameters
+    let gen = |start| {
         //~^ ERROR type inside generator must be known in this context
         yield;
+        //~^ ERROR type inside generator must be known in this context
+        //~| ERROR type inside generator must be known in this context
     };
 }
