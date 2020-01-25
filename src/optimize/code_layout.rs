d821f154c5c6f46f5e461a28a7fd1ea7a2351620
bjorn3 <bjorn3@users.noreply.github.com>
2020-01-25 17:55:48 +0100

    Disable the code_layout optimization
    When compiling libcore, it causes ebb params to be dropped for a certain function

--- src/optimize/code_layout.rs
+++ src/optimize/code_layout.rs
@@ -11,6 +11,8 @@
 use crate::prelude::*;
 
 pub(super) fn optimize_function(ctx: &mut Context, cold_ebbs: &EntitySet<Ebb>) {
+    return; // FIXME add ebb arguments back
+
     // FIXME Move the ebb in place instead of remove and append once
     // bytecodealliance/cranelift#1339 is implemented.
 
