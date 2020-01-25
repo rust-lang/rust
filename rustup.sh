eec023eb8529fac3f1b91d45d82609526ef3452f
bjorn3 <bjorn3@users.noreply.github.com>
2020-01-25 12:01:35 +0100

    Rustup to rustc 1.42.0-nightly (c2d141df5 2020-01-24)

--- rustup.sh
+++ rustup.sh
@@ -5,14 +5,14 @@ set -e
 case $1 in
     "prepare")
         # FIXME Automatically detect latest nightly
-        read -p "Date of nightly to use (aka yesterday GMT): " TOOLCHAIN
+        read -p "Date of nightly to use: " TOOLCHAIN
 
         echo "=> Installing new nightly"
         rustup toolchain install --profile minimal nightly-${TOOLCHAIN} # Sanity check to see if the nightly exists
         echo nightly-${TOOLCHAIN} > rust-toolchain
 
         echo "=> Uninstalling all old nighlies"
-        for nightly in $(rustup toolchain list | grep nightly- | grep -v $TOOLCHAIN); do
+        for nightly in $(rustup toolchain list | grep nightly | grep -v $TOOLCHAIN | grep -v nightly-x86_64); do
             rustup toolchain uninstall $nightly
         done
 
