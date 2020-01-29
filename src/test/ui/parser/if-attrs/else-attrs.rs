0103e57ac06be692a2b0fb7fdf1bf99e38a1e4c7
Aaron Hill <aa1ronham@gmail.com>
2020-01-29 17:20:39 -0500

    Test that attrs on 'else' and 'else if' don't parse
    We may want to change this, but it would be a much larger change than
    this PR is making.

---
+++ src/test/ui/parser/if-attrs/else-attrs.rs
@@ -0,0 +1,25 @@
+#[cfg(FALSE)]
+fn if_else_parse_error() {
+    if true {
+    } #[attr] else if false { //~ ERROR expected
+    }
+}
+
+#[cfg(FALSE)]
+fn else_attr_ifparse_error() {
+    if true {
+    } else #[attr] if false { //~ ERROR expected
+    } else {
+    }
+}
+
+#[cfg(FALSE)]
+fn else_parse_error() {
+    if true {
+    } else if false {
+    } #[attr] else { //~ ERROR expected
+    }
+}
+
+fn main() {
+}
