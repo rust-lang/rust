c04195da9f47e8090bc49bad605765e9a2a721b4
Camille GILLOT <gillot.camille@gmail.com>
2020-01-31 23:06:06 +0100

    Move get_lang_items query in librustc_passes.

--- src/librustc/middle/weak_lang_items.rs
+++ src/librustc/middle/weak_lang_items.rs
@@ -141,7 +141,7 @@ impl<'a, 'tcx, 'v> Visitor<'v> for Context<'a, 'tcx> {
     }
 
     fn visit_foreign_item(&mut self, i: &hir::ForeignItem<'_>) {
-        if let Some((lang_item, _)) = lang_items::extract(&i.attrs) {
+        if let Some((lang_item, _)) = rustc_lang_items::lang_items::extract(&i.attrs) {
             self.register(lang_item, i.span);
         }
         intravisit::walk_foreign_item(self, i)
