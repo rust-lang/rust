ae7a3ea42dcdfc907e6fee14a5cc45d1c4745a2b
jumbatm <jumbatm@gmail.com>
2020-02-06 20:22:25 +1000

    Fix stage2 test failures from call to span_lint.
    span_lint was removed. Callers should use the `lint` method now, and
    call `set_span` within the closure passed to this method.

--- src/test/ui-fulldeps/auxiliary/lint-for-crate.rs
+++ src/test/ui-fulldeps/auxiliary/lint-for-crate.rs
@@ -5,13 +5,15 @@
 
 extern crate rustc_driver;
 extern crate rustc_hir;
-#[macro_use] extern crate rustc_lint;
-#[macro_use] extern crate rustc_session;
+#[macro_use]
+extern crate rustc_lint;
+#[macro_use]
+extern crate rustc_session;
 extern crate rustc_span;
 extern crate syntax;
 
-use rustc_lint::{LateContext, LintContext, LintPass, LateLintPass, LintArray};
 use rustc_driver::plugin::Registry;
+use rustc_lint::{LateContext, LateLintPass, LintArray, LintContext, LintPass};
 use rustc_span::symbol::Symbol;
 use syntax::attr;
 
@@ -26,8 +28,11 @@ declare_lint_pass!(Pass => [CRATE_NOT_OKAY]);
 impl<'a, 'tcx> LateLintPass<'a, 'tcx> for Pass {
     fn check_crate(&mut self, cx: &LateContext, krate: &rustc_hir::Crate) {
         if !attr::contains_name(&krate.attrs, Symbol::intern("crate_okay")) {
-            cx.span_lint(CRATE_NOT_OKAY, krate.span,
-                         "crate is not marked with #![crate_okay]");
+            cx.lint(CRATE_NOT_OKAY, |lint| {
+                lint.build("crate is not marked with #![crate_okay]")
+                    .set_span(krate.span)
+                    .emit()
+            });
         }
     }
 }
