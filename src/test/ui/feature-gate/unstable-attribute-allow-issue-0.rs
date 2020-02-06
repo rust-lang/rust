b5c82b6fbf4bfa570e6afb637f6886aba0f9b0b4
Yuki Okushi <huyuumi.dev@gmail.com>
2020-02-06 16:19:39 +0900

    Forbid using `0` as issue number

--- src/test/ui/feature-gate/unstable-attribute-allow-issue-0.rs
+++ src/test/ui/feature-gate/unstable-attribute-allow-issue-0.rs
@@ -4,10 +4,10 @@
 #![stable(feature = "stable_test_feature", since = "1.0.0")]
 
 #[unstable(feature = "unstable_test_feature", issue = "0")]
-fn unstable_issue_0() {}
+fn unstable_issue_0() {} //~^ ERROR `issue` must be a non-zero numeric string or "none"
 
 #[unstable(feature = "unstable_test_feature", issue = "none")]
 fn unstable_issue_none() {}
 
 #[unstable(feature = "unstable_test_feature", issue = "something")]
-fn unstable_issue_not_allowed() {} //~^ ERROR `issue` must be a numeric string or "none"
+fn unstable_issue_not_allowed() {} //~^ ERROR `issue` must be a non-zero numeric string or "none"
