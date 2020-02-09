fa5a3c35dcda3c82d0c10837cdcbf747e833eabd
Matthew Jasper <mjjasper1@gmail.com>
2020-02-09 13:19:06 +0000

    Don't parse `mut a @ b` as `mut a @ mut b`

---
+++ src/test/ui/pattern/bindings-after-at/nested-binding-modes-ref.rs
@@ -0,0 +1,13 @@
+#![feature(bindings_after_at)]
+
+fn main() {
+    let ref is_ref @ is_val = 42;
+    *is_ref;
+    *is_val;
+    //~^ ERROR cannot be dereferenced
+
+    let is_val @ ref is_ref = 42;
+    *is_ref;
+    *is_val;
+    //~^ ERROR cannot be dereferenced
+}
