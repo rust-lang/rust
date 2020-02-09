f4f781d5cfd7a572006a8721c7998d13c378647d
flip1995 <hello@philkrones.com>
2020-02-09 12:23:13 +0100

    Filter prelude glob imports

--- clippy_lints/src/wildcard_imports.rs
+++ clippy_lints/src/wildcard_imports.rs
@@ -76,6 +76,8 @@ impl LateLintPass<'_, '_> for WildcardImports {
         if_chain! {
             if !in_macro(item.span);
             if let ItemKind::Use(use_path, UseKind::Glob) = &item.kind;
+            // don't lint prelude glob imports
+            if !use_path.segments.iter().last().map_or(false, |ps| ps.ident.as_str() == "prelude");
             let used_imports = cx.tcx.names_imported_by_glob_use(item.hir_id.owner_def_id());
             if !used_imports.is_empty(); // Already handled by `unused_imports`
             then {
