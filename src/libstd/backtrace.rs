c999c3ffa6fb931d115069995304e24a212372e6
Jane Lusby <jlusby@yaah.dev>
2020-02-11 08:39:27 -0800

    remove intentionally failing test

--- src/libstd/backtrace.rs
+++ src/libstd/backtrace.rs
@@ -408,21 +408,3 @@ impl Capture {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn debug_backtrace_fmt() {
-        let bt = Backtrace::capture();
-        eprintln!("uncaptured: {:?}", bt);
-        let bt = Backtrace::force_capture();
-        eprintln!("captured: {:?}", bt);
-        eprintln!("display print: {}", bt);
-        eprintln!("display print alt: {:#}", bt);
-        eprintln!("resolved: {:?}", bt);
-        eprintln!("resolved alt: {:#?}", bt);
-        unimplemented!();
-    }
-}
