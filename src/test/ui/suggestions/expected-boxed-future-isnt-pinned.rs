80cdb0af7dd27471e1e4a4362e2473a9331a5fdd
Esteban Küber <esteban@kuber.com.ar>
2020-02-12 11:11:55 -0800

    Account for `Box::new(impl Future)` and emit help `use Box::pin`

--- src/test/ui/suggestions/expected-boxed-future-isnt-pinned.rs
+++ src/test/ui/suggestions/expected-boxed-future-isnt-pinned.rs
@@ -1,5 +1,4 @@
 // edition:2018
-// run-rustfix
 #![allow(dead_code)]
 use std::future::Future;
 use std::pin::Pin;
@@ -10,6 +9,18 @@ type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 fn foo<F: Future<Output=i32> + Send + 'static>(x: F) -> BoxFuture<'static, i32> {
     // We could instead use an `async` block, but this way we have no std spans.
     x //~ ERROR mismatched types
+}
+fn bar<F: Future<Output=i32> + Send + 'static>(x: F) -> BoxFuture<'static, i32> {
+    Box::new(x) //~ ERROR mismatched types
+    //~^ HELP use `Box::pin`
+}
+fn baz<F: Future<Output=i32> + Send + 'static>(x: F) -> BoxFuture<'static, i32> {
+    Pin::new(x) //~ ERROR mismatched types
+    //~^ ERROR the trait bound
+}
+fn qux<F: Future<Output=i32> + Send + 'static>(x: F) -> BoxFuture<'static, i32> {
+    Pin::new(Box::new(x)) //~ ERROR mismatched types
+    //~^ ERROR the trait bound
 }
 
 fn main() {}
