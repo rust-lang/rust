c7aadcfa567f363e302ac797a4805668eb7bc77c
Ben Lewis <benlewisj@gmail.com>
2020-02-15 12:57:46 +1300

    Change `const_field` and `const_caller_location` to return `ConstValue` instead of `Const` as the type in the returned const isn't needed.

--- src/librustc_codegen_llvm/intrinsic.rs
+++ src/librustc_codegen_llvm/intrinsic.rs
@@ -193,8 +193,7 @@ impl IntrinsicCallMethods<'tcx> for Builder<'a, 'll, 'tcx> {
                     .tcx
                     .const_eval_instance(ty::ParamEnv::reveal_all(), instance, None)
                     .unwrap();
-                let const_ = ty::Const { val: ty::ConstKind::Value(ty_name), ty: ret_ty };
-                OperandRef::from_const(self, &const_).immediate_or_packed_pair(self)
+                OperandRef::from_const(self, ty_name, ret_ty).immediate_or_packed_pair(self)
             }
             "init" => {
                 let ty = substs.type_at(0);
