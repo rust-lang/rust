415218fc8d590063a8c5cd403ff890fb91c0bd2c
Ralf Jung <post@ralfj.de>
2020-02-15 11:47:11 +0100

    expand assoc-const test a bit, just to be sure

--- src/test/ui/consts/issue-69020.rs
+++ src/test/ui/consts/issue-69020.rs
@@ -8,10 +8,22 @@
 use std::i32;
 
 pub trait Foo {
-    const N: i32;
+    const NEG: i32;
+    const ADD: i32;
+    const DIV: i32;
+    const OOB: i32;
 }
 
+// These constants cannot be evaluated already (they depend on `T::N`), so
+// they can just be linted like normal run-time code.  But codegen works
+// a bit different in const context, so this test makes sure that we still catch overflow.
 impl<T: Foo> Foo for Vec<T> {
-    const N: i32 = -i32::MIN + T::N;
+    const NEG: i32 = -i32::MIN + T::NEG;
     //~^ ERROR arithmetic operation will overflow
+    const ADD: i32 = (i32::MAX+1) + T::ADD;
+    //~^ ERROR arithmetic operation will overflow
+    const DIV: i32 = (1/0) + T::DIV;
+    //~^ ERROR operation will panic
+    const OOB: i32 = [1][1] + T::OOB;
+    //~^ ERROR operation will panic
 }
