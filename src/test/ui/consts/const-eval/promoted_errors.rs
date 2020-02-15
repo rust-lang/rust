bd485223149e35c1c4e6b3ecf58653e38de4b6c9
Ralf Jung <post@ralfj.de>
2020-02-15 10:47:27 +0100

    fix tests, and use variants to test debug and release builds together

--- src/test/ui/consts/const-eval/promoted_errors.rs
+++ src/test/ui/consts/const-eval/promoted_errors.rs
@@ -1,23 +1,28 @@
+// revisions: debug opt opt_with_overflow_checks
+//[debug]compile-flags: -C opt-level=0
+//[opt]compile-flags: -O
+//[opt_with_overflow_checks]compile-flags: -C overflow-checks=on -O
+
 // build-pass
 // ignore-pass (emit codegen-time warnings and verify that they are indeed warnings and not errors)
-// compile-flags: -O
 
-#![warn(const_err)]
+#![warn(const_err, overflow, panic)]
 
 fn main() {
     println!("{}", 0u32 - 1);
+    //[opt_with_overflow_checks,debug]~^ WARN [overflow]
     let _x = 0u32 - 1;
-    //~^ WARN const_err
+    //~^ WARN [overflow]
     println!("{}", 1 / (1 - 1));
-    //~^ WARN attempt to divide by zero [const_err]
-    //~| WARN const_err
+    //~^ WARN [panic]
+    //~| WARN panic or abort [const_err]
     //~| WARN erroneous constant used [const_err]
     let _x = 1 / (1 - 1);
-    //~^ WARN const_err
+    //~^ WARN [panic]
     println!("{}", 1 / (false as u32));
-    //~^ WARN attempt to divide by zero [const_err]
-    //~| WARN const_err
+    //~^ WARN [panic]
+    //~| WARN panic or abort [const_err]
     //~| WARN erroneous constant used [const_err]
     let _x = 1 / (false as u32);
-    //~^ WARN const_err
+    //~^ WARN [panic]
 }
