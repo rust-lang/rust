fbd6a17c525b1bfc61ca1579d98db6d4cdfb5afc
Mazdak Farrokhzad <twingoow@gmail.com>
2020-02-15 22:36:03 +0100

    ast: tweak AssocItemKind::Macro comment

--- src/libsyntax/ast.rs
+++ src/libsyntax/ast.rs
@@ -2644,6 +2644,6 @@ pub enum AssocItemKind {
     Fn(FnSig, Generics, Option<P<Block>>),
     /// A type.
     TyAlias(Generics, GenericBounds, Option<P<Ty>>),
-    /// A macro expanding to an item.
+    /// A macro expanding to items.
     Macro(Mac),
 }
