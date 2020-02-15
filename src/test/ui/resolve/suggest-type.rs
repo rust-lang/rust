8e0cf4048f9f123fc9fb3179bbeaad704b467e2e
Philipp Hansch <dev@phansch.net>
2020-02-15 11:16:04 +0100

    Remove tests testing Levenshtein distance

--- src/test/ui/resolve/suggest-type.rs
+++ src/test/ui/resolve/suggest-type.rs
@@ -8,12 +8,6 @@ fn main() {
     let _ = Cstring::new("hello").unwrap();
     //~^ ERROR failed to resolve: use of undeclared type or module `Cstring`
 
-    let _ = CStRiNg::new("hello").unwrap();
-    //~^ ERROR failed to resolve: use of undeclared type or module `CStRiNg`
-
-    let _ = cStrIng::new("hello").unwrap();
-    //~^ ERROR failed to resolve: use of undeclared type or module `cStrIng`
-
     let _ = foO::bar();
     //~^ ERROR failed to resolve: use of undeclared type or module `foO`
 }
