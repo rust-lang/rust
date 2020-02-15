9863a9bb1b44a22d40bb8aa9b514356728c98b77
Philipp Hansch <dev@phansch.net>
2020-02-15 14:29:34 +0100

    Add error annotations for existing tests

--- src/test/ui/resolve/suggest-type.rs
+++ src/test/ui/resolve/suggest-type.rs
@@ -7,7 +7,11 @@ mod foo {
 fn main() {
     let _ = Cstring::new("hello").unwrap();
     //~^ ERROR failed to resolve: use of undeclared type or module `Cstring`
+    //~| HELP a struct with a similar name exists
+    //~| SUGGESTION CString
 
     let _ = foO::bar();
     //~^ ERROR failed to resolve: use of undeclared type or module `foO`
+    //~| HELP a module with a similar name exists
+    //~| SUGGESTION foo
 }
