5dd646435ba882574e677acf58113880a6570949
David Ross <daboross@daboross.net>
2020-02-15 16:21:59 -0800

    Fix related type ascription tests.

--- src/test/ui/type/ascription/issue-60933.rs
+++ src/test/ui/type/ascription/issue-60933.rs
@@ -1,4 +1,6 @@
 fn main() {
     let u: usize = std::mem:size_of::<u32>();
-    //~^ ERROR expected one of
+    //~^ ERROR casts cannot be followed by a function call
+    //~| ERROR expected value, found module `std::mem` [E0423]
+    //~| ERROR cannot find type `size_of` in this scope [E0412]
 }
