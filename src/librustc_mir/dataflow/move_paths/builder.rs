cc92f22ef79ead0bb593bd1945c4882328176610
Jonas Schievink <jonasschievink@gmail.com>
2020-02-17 21:20:48 +0100

    Reorder yield visitation order to match call

--- src/librustc_mir/dataflow/move_paths/builder.rs
+++ src/librustc_mir/dataflow/move_paths/builder.rs
@@ -381,9 +381,9 @@ impl<'b, 'a, 'tcx> Gatherer<'b, 'a, 'tcx> {
             }
 
             TerminatorKind::Yield { ref value, resume_arg: ref place, .. } => {
+                self.gather_operand(value);
                 self.create_move_path(place);
                 self.gather_init(place.as_ref(), InitKind::Deep);
-                self.gather_operand(value);
             }
 
             TerminatorKind::Drop { ref location, target: _, unwind: _ } => {
