533422fcce7f0887f37b69a30f96216cff79afa1
Krishna Sai Veera Reddy <veerareddy@email.arizona.edu>
2020-02-18 08:04:02 -0800

    Add `LOG2_10` and `LOG10_2` to `approx_const` lint

--- clippy_lints/src/approx_const.rs
+++ clippy_lints/src/approx_const.rs
@@ -37,7 +37,7 @@ declare_clippy_lint! {
 }
 
 // Tuples are of the form (constant, name, min_digits)
-const KNOWN_CONSTS: [(f64, &str, usize); 16] = [
+const KNOWN_CONSTS: [(f64, &str, usize); 18] = [
     (f64::E, "E", 4),
     (f64::FRAC_1_PI, "FRAC_1_PI", 4),
     (f64::FRAC_1_SQRT_2, "FRAC_1_SQRT_2", 5),
@@ -52,6 +52,8 @@ const KNOWN_CONSTS: [(f64, &str, usize); 16] = [
     (f64::LN_2, "LN_2", 5),
     (f64::LOG10_E, "LOG10_E", 5),
     (f64::LOG2_E, "LOG2_E", 5),
+    (f64::LOG2_10, "LOG2_10", 5),
+    (f64::LOG10_2, "LOG10_2", 5),
     (f64::PI, "PI", 3),
     (f64::SQRT_2, "SQRT_2", 5),
 ];
