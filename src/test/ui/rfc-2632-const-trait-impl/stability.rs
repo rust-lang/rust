e26324b445c2a532690de7bb86e0f9347f90496b
Dylan MacKenzie <ecstaticmorse@gmail.com>
2020-02-18 15:45:54 -0800

    Update tests

--- src/test/ui/rfc-2632-const-trait-impl/stability.rs
+++ src/test/ui/rfc-2632-const-trait-impl/stability.rs
@@ -6,6 +6,17 @@
 
 pub struct Int(i32);
 
+#[stable(feature = "rust1", since = "1.0.0")]
+#[rustc_const_stable(feature = "rust1", since = "1.0.0")]
+impl const std::ops::Sub for Int {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        //~^ ERROR trait methods cannot be stable const fn
+        Int(self.0 - rhs.0)
+    }
+}
+
 #[rustc_const_unstable(feature = "const_add", issue = "none")]
 impl const std::ops::Add for Int {
     type Output = Self;
@@ -28,6 +39,5 @@ pub const fn foo() -> Int {
 pub const fn bar() -> Int {
     Int(1i32) + Int(2i32)
 }
-
 
 fn main() {}
