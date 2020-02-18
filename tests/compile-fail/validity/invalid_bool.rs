88c45f9891ede9d5b6643783f51f722fcf817d08
Ralf Jung <post@ralfj.de>
2020-02-18 09:32:02 +0100

    adjust for rustc changes

--- tests/compile-fail/validity/invalid_bool.rs
+++ tests/compile-fail/validity/invalid_bool.rs
@@ -1,3 +1,3 @@
 fn main() {
-    let _b = unsafe { std::mem::transmute::<u8, bool>(2) }; //~ ERROR encountered 2, but expected something less or equal to 1
+    let _b = unsafe { std::mem::transmute::<u8, bool>(2) }; //~ ERROR encountered 2, but expected a boolean
 }
--- tests/compile-fail/validity/invalid_char.rs
+++ tests/compile-fail/validity/invalid_char.rs
@@ -1,6 +1,6 @@
 fn main() {
     assert!(std::char::from_u32(-1_i32 as u32).is_none());
-    let _val = match unsafe { std::mem::transmute::<i32, char>(-1) } { //~ ERROR encountered 4294967295, but expected something less or equal to 1114111
+    let _val = match unsafe { std::mem::transmute::<i32, char>(-1) } { //~ ERROR encountered 4294967295, but expected a valid unicode codepoint
         'a' => {true},
         'b' => {false},
         _ => {true},
