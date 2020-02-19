b00f6745c4df0e979dd53dcbf43ededf5728e349
Aaron Hill <aa1ronham@gmail.com>
2020-02-19 10:48:13 -0500

    Remove recovery test

--- src/test/ui/parser/recovery-attr-on-if.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    #[attr] if true {};
-    //~^ ERROR cannot find attribute
-    #[attr] if true {};
-    //~^ ERROR cannot find attribute
-    let _recovery_witness: () = 0; //~ ERROR mismatched types
-}
