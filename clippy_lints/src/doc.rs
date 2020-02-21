8abe4365f3eab021bde47ef9ffebfb573918729d
Yuki Okushi <huyuumi.dev@gmail.com>
2020-02-21 16:24:58 +0900

    Use generator own DefId

--- clippy_lints/src/doc.rs
+++ clippy_lints/src/doc.rs
@@ -234,7 +234,7 @@ fn lint_for_missing_headers<'a, 'tcx>(
                 if implements_trait(cx, ret_ty, future, &[]);
                 if let ty::Opaque(_, subs) = ret_ty.kind;
                 if let Some(gen) = subs.types().next();
-                if let ty::Generator(_, subs, _) = gen.kind;
+                if let ty::Generator(def_id, subs, _) = gen.kind;
                 if match_type(cx, subs.as_generator().return_ty(def_id, cx.tcx), &paths::RESULT);
                 then {
                     span_lint(
