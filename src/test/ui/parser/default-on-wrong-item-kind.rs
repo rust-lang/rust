dfc1d6b82d4ad136bac71a76571778ddee4f3da7
Mazdak Farrokhzad <twingoow@gmail.com>
2020-02-22 06:57:31 +0100

    parse: use `parse_item_common` in `parse_foreign_item`.

--- src/test/ui/parser/default-on-wrong-item-kind.rs
+++ src/test/ui/parser/default-on-wrong-item-kind.rs
@@ -1,5 +1,6 @@
 // Test parsing for `default` where it doesn't belong.
 // Specifically, we are interested in kinds of items or items in certain contexts.
+// Also test item kinds in `extern` blocks and associated contexts which are not allowed there.
 
 fn main() {}
 
@@ -23,4 +24,39 @@ mod free_items {
     default::foo::bar!();
     default macro foo {} //~ ERROR item cannot be `default`
     default macro_rules! foo {} //~ ERROR item cannot be `default`
+}
+
+#[cfg(FALSE)]
+extern "C" {
+    default extern crate foo; //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default use foo; //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default static foo: u8; //~ ERROR item cannot be `default`
+    default const foo: u8; //~ ERROR item cannot be `default`
+    //~^ ERROR extern items cannot be `const`
+    default fn foo(); //~ ERROR item cannot be `default`
+    default mod foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default extern "C" {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default type foo = u8; //~ ERROR item cannot be `default`
+    default enum foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default struct foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default union foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default trait foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default trait foo = Ord; //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default impl foo {}
+    //~^ ERROR item kind not supported in `extern` block
+    default!();
+    default::foo::bar!();
+    default macro foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
+    default macro_rules! foo {} //~ ERROR item cannot be `default`
+    //~^ ERROR item kind not supported in `extern` block
 }
