a796af7a7685a21e8c43f93aa5fb3007cd847253
jumbatm <jumbatm@gmail.com>
2020-02-23 01:43:47 +1000

    Fail on multiple declarations of `main`.
    Previously, when inserting the entry function, we only checked for
    duplicate _definitions_ of `main`.  However, it's possible to cause
    problems even only having a duplicate _declaration_. For example,
    shadowing `main` using an extern block isn't caught by the current
    check, and causes an assertion failure down the line in in LLVM code.

--- src/test/ui/duplicate/dupe-symbols-7.rs
+++ src/test/ui/duplicate/dupe-symbols-7.rs
@@ -1,7 +1,7 @@
 // build-fail
 
 //
-// error-pattern: entry symbol `main` defined multiple times
+// error-pattern: entry symbol `main` declared multiple times
 
 // FIXME https://github.com/rust-lang/rust/issues/59774
 // normalize-stderr-test "thread.*panicked.*Metadata module not compiled.*\n" -> ""
---
+++ src/test/ui/duplicate/dupe-symbols-8.rs
@@ -0,0 +1,12 @@
+// build-fail
+// error-pattern: entry symbol `main` declared multiple times
+//
+// See #67946.
+
+#![allow(warnings)]
+fn main() {
+    extern "Rust" {
+     fn main();
+    }
+    unsafe { main(); }
+}
