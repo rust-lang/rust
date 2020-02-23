5892d0d010b44a2c5815c8f5d1bead1b99608979
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-02-23 18:23:52 +0100

    Add tests for new of variables

---
+++ src/test/rustdoc-js-std/return-specific-literal.js
@@ -0,0 +1,10 @@
+const QUERY = 'struct:"string"';
+
+const EXPECTED = {
+    'in_args': [
+        { 'path': 'std::string::String', 'name': 'ne' },
+    ],
+    'returned': [
+        { 'path': 'std::string::String', 'name': 'add' },
+    ],
+};
