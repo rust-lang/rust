8de1ec9ce0a53b8ad9987e923e833c82fda05603
Kornel <kornel@geekhood.net>
2020-02-27 16:28:27 +0000

    Make error message clearer about creating new module

--- src/test/ui/parser/mod_file_not_exist_windows.rs
+++ src/test/ui/parser/mod_file_not_exist_windows.rs
@@ -1,7 +1,7 @@
 // only-windows
 
 mod not_a_real_file; //~ ERROR file not found for module `not_a_real_file`
-//~^ HELP name the file either not_a_real_file.rs or not_a_real_file\mod.rs inside the directory
+//~^ HELP to create the module `not_a_real_file`, create file
 
 fn main() {
     assert_eq!(mod_file_aux::bar(), 10);
