db0d03229c54e3dead1cd8afde62e1ba9b1707bc
Ralf Jung <post@ralfj.de>
2020-03-04 09:50:26 +0100

    fix allocator tests

--- tests/compile-fail/reallocate-change-alloc.rs
+++ tests/compile-fail/reallocate-change-alloc.rs
@@ -7,7 +7,7 @@ use std::alloc::{AllocRef, Layout};
 
 fn main() {
     unsafe {
-        let x = Global.alloc(Layout::from_size_align_unchecked(1, 1)).unwrap();
+        let x = Global.alloc(Layout::from_size_align_unchecked(1, 1)).unwrap().0;
         Global.realloc(x, Layout::from_size_align_unchecked(1, 1), 1).unwrap();
         let _z = *(x.as_ptr() as *mut u8); //~ ERROR dangling pointer was dereferenced
     }
