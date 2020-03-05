6bfff8a30356e3dd7eaa1c6b9d08e1e7d978d5c1
Matthias Krüger <matthias.krueger@famsik.de>
2020-03-05 22:01:12 +0100

    rustdoc: Use .any(p) instead of find(p).is_some(). (clippy::search_is_some)

--- src/librustdoc/html/render.rs
+++ src/librustdoc/html/render.rs
@@ -3401,10 +3401,8 @@ fn render_assoc_items(
         let deref_impl =
             traits.iter().find(|t| t.inner_impl().trait_.def_id() == c.deref_trait_did);
         if let Some(impl_) = deref_impl {
-            let has_deref_mut = traits
-                .iter()
-                .find(|t| t.inner_impl().trait_.def_id() == c.deref_mut_trait_did)
-                .is_some();
+            let has_deref_mut =
+                traits.iter().any(|t| t.inner_impl().trait_.def_id() == c.deref_mut_trait_did);
             render_deref_methods(w, cx, impl_, containing_item, has_deref_mut);
         }
 
@@ -3816,7 +3814,7 @@ fn render_impl(
     ) {
         for trait_item in &t.items {
             let n = trait_item.name.clone();
-            if i.items.iter().find(|m| m.name == n).is_some() {
+            if i.items.iter().any(|m| m.name == n) {
                 continue;
             }
             let did = i.trait_.as_ref().unwrap().def_id().unwrap();
