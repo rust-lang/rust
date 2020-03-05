0f7f30711e4eb2f5f84fe198f325e512e404ee0c
Jacob Meyers <jacobmeyers065@gmail.com>
2020-03-04 21:13:57 -0500

    add lint on File::read_to_string and File::read_to_end

---
+++ tests/ui/verbose_file_reads.rs
@@ -0,0 +1,30 @@
+#![warn(clippy::verbose_file_reads)]
+use std::env::temp_dir;
+use std::fs::File;
+use std::io::Read;
+
+struct Struct;
+// To make sure we only warn on File::{read_to_end, read_to_string} calls
+impl Struct {
+    pub fn read_to_end(&self) {}
+
+    pub fn read_to_string(&self) {}
+}
+
+fn main() -> std::io::Result<()> {
+    let mut path = temp_dir();
+    path.push("test.txt");
+    let file = File::create(&path)?;
+    // Lint shouldn't catch this
+    let s = Struct;
+    s.read_to_end();
+    s.read_to_string();
+    // Should catch this
+    let mut f = File::open(&path)?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+    // ...and this
+    let mut string_buffer = String::new();
+    f.read_to_string(&mut string_buffer)?;
+    Ok(())
+}
