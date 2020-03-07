803de3188c59ecc782db46e3fc9e9f668e260be9
Mazdak Farrokhzad <twingoow@gmail.com>
2020-03-07 19:41:24 +0100

    submod_path: use id.span

--- src/test/ui/directory_ownership/macro-expanded-mod.rs
+++ src/test/ui/directory_ownership/macro-expanded-mod.rs
@@ -1,7 +1,9 @@
 // Test that macro-expanded non-inline modules behave correctly
 
 macro_rules! mod_decl {
-    ($i:ident) => { mod $i; } //~ ERROR Cannot declare a non-inline module inside a block
+    ($i:ident) => {
+        mod $i;
+    };
 }
 
 mod macro_expanded_mod_helper {
@@ -9,5 +11,5 @@ mod macro_expanded_mod_helper {
 }
 
 fn main() {
-    mod_decl!(foo);
+    mod_decl!(foo); //~ ERROR Cannot declare a non-inline module inside a block
 }
