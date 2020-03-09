fc8be08a8e786514819ffff7c4239879afb8ea3c
Yuki Okushi <huyuumi.dev@gmail.com>
2020-03-09 09:11:58 +0900

    Add test for issue-57200

---
+++ src/test/ui/impl-trait/issue-57200.rs
@@ -0,0 +1,14 @@
+// Regression test for #57200
+
+#![feature(impl_trait_in_bindings)]
+#![allow(incomplete_features)]
+
+fn bug<'a, 'b, T>()
+where
+    'a: 'b,
+{
+    let f: impl Fn(&'a T) -> &'b T = |x| x;
+    //~^ ERROR: lifetimes in impl Trait types in bindings are not currently supported
+}
+
+fn main() {}
