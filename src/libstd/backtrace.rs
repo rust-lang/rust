a2364dc85fb505487c272988b8c5073a3f6fef2a
David Tolnay <dtolnay@gmail.com>
2020-03-09 12:23:19 -0700

    Write backtrace fmt test using relative paths
    For some reason the absolute paths were formatted differently on the
    armhf-gnu target.
    
        thread '<unnamed>' panicked at 'assertion failed: `(left == right)`
          left: `"Backtrace [\n    { fn: \"__rust_maybe_catch_panic\" },\n    { fn: \"std::rt::lang_start_internal\", file: \"./rust/rt.rs\", line: 300 },\n    { fn: \"std::rt::lang_start\", file: \"./rust/rt.rs\", line: 400 },\n]"`,
         right: `"Backtrace [\n    { fn: \"__rust_maybe_catch_panic\" },\n    { fn: \"std::rt::lang_start_internal\", file: \"/rust/rt.rs\", line: 300 },\n    { fn: \"std::rt::lang_start\", file: \"/rust/rt.rs\", line: 400 },\n]"`', src/libstd/backtrace.rs:486:5

--- src/libstd/backtrace.rs
+++ src/libstd/backtrace.rs
@@ -445,7 +445,7 @@ fn test_debug() {
                     frame: RawFrame::Fake,
                     symbols: vec![BacktraceSymbol {
                         name: Some(b"std::backtrace::Backtrace::create".to_vec()),
-                        filename: Some(BytesOrWide::Bytes(b"/rust/backtrace.rs".to_vec())),
+                        filename: Some(BytesOrWide::Bytes(b"rust/backtrace.rs".to_vec())),
                         lineno: Some(100),
                     }],
                 },
@@ -462,12 +462,12 @@ fn test_debug() {
                     symbols: vec![
                         BacktraceSymbol {
                             name: Some(b"std::rt::lang_start_internal".to_vec()),
-                            filename: Some(BytesOrWide::Bytes(b"/rust/rt.rs".to_vec())),
+                            filename: Some(BytesOrWide::Bytes(b"rust/rt.rs".to_vec())),
                             lineno: Some(300),
                         },
                         BacktraceSymbol {
                             name: Some(b"std::rt::lang_start".to_vec()),
-                            filename: Some(BytesOrWide::Bytes(b"/rust/rt.rs".to_vec())),
+                            filename: Some(BytesOrWide::Bytes(b"rust/rt.rs".to_vec())),
                             lineno: Some(400),
                         },
                     ],
@@ -479,8 +479,8 @@ fn test_debug() {
     #[rustfmt::skip]
     let expected = "Backtrace [\
     \n    { fn: \"__rust_maybe_catch_panic\" },\
-    \n    { fn: \"std::rt::lang_start_internal\", file: \"/rust/rt.rs\", line: 300 },\
-    \n    { fn: \"std::rt::lang_start\", file: \"/rust/rt.rs\", line: 400 },\
+    \n    { fn: \"std::rt::lang_start_internal\", file: \"rust/rt.rs\", line: 300 },\
+    \n    { fn: \"std::rt::lang_start\", file: \"rust/rt.rs\", line: 400 },\
     \n]";
 
     assert_eq!(format!("{:#?}", backtrace), expected);
