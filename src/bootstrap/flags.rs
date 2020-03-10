9f734c978c4c32eea552d45fef582804c1d2b9bf
Chris Simpkins <git.simpkins@gmail.com>
2020-03-09 22:04:59 -0400

    Add documentation of tool testing with x.py script

--- src/bootstrap/flags.rs
+++ src/bootstrap/flags.rs
@@ -359,7 +359,7 @@ Arguments:
                 subcommand_help.push_str(
                     "\n
 Arguments:
-    This subcommand accepts a number of paths to directories to tests that
+    This subcommand accepts a number of paths to test directories that
     should be compiled and run. For example:
 
         ./x.py test src/test/ui
@@ -367,10 +367,14 @@ Arguments:
         ./x.py test src/libstd --stage 0 --no-doc
         ./x.py test src/test/ui --bless
         ./x.py test src/test/ui --compare-mode nll
-
+    
     Note that `test src/test/* --stage N` does NOT depend on `build src/rustc --stage N`;
     just like `build src/libstd --stage N` it tests the compiler produced by the previous
     stage.
+
+    Execute tool tests with a tool name argument:
+
+        ./x.py test tidy
 
     If no arguments are passed then the complete artifacts for that stage are
     compiled and tested.
