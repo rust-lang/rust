6eb05c4a14bf233b1a6774074b526b54420deb9d
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-03-13 18:02:04 +0100

    Simplify

--- crates/ra_assists/src/handlers/add_missing_impl_members.rs
+++ crates/ra_assists/src/handlers/add_missing_impl_members.rs
@@ -142,8 +142,8 @@ fn add_missing_impl_members_inner(
         let n_existing_items = impl_item_list.impl_items().count();
         let source_scope = sema.scope_for_def(trait_);
         let target_scope = sema.scope(impl_item_list.syntax());
-        let ast_transform = QualifyPaths::new(&target_scope, &source_scope, sema.db)
-            .or(SubstituteTypeParams::for_trait_impl(&source_scope, sema.db, trait_, impl_node));
+        let ast_transform = QualifyPaths::new(&target_scope, &source_scope)
+            .or(SubstituteTypeParams::for_trait_impl(&source_scope, trait_, impl_node));
         let items = missing_items
             .into_iter()
             .map(|it| ast_transform::apply(&*ast_transform, it))
