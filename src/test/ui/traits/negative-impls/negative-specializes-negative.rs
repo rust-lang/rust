d3866a452b32c4b41990b18431c078bb325de25b
Niko Matsakis <niko@alum.mit.edu>
2020-03-13 14:43:34 -0400

    add test for negative specializes negative

---
+++ src/test/ui/traits/negative-impls/negative-specializes-negative.rs
@@ -0,0 +1,13 @@
+#![feature(specialization)]
+#![feature(negative_impls)]
+
+// Test a negative impl that "specializes" another negative impl.
+//
+// run-pass
+
+trait MyTrait {}
+
+impl<T> !MyTrait for T {}
+impl !MyTrait for u32 {}
+
+fn main() {}
