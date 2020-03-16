9e7dbb1abd8ca502749d734ef25ba3359dc7d11b
Emil Lauridsen <mine809@gmail.com>
2020-03-16 14:22:25 +0100

    Remove unused config from VSCode ext

--- editors/code/src/client.ts
+++ editors/code/src/client.ts
@@ -42,7 +42,6 @@ export async function createClient(config: Config, serverPath: string): Promise<
             excludeGlobs: config.excludeGlobs,
             useClientWatching: config.useClientWatching,
             featureFlags: config.featureFlags,
-            additionalOutDirs: config.additionalOutDirs,
             withSysroot: config.withSysroot,
             cargoFeatures: config.cargoFeatures,
             rustfmtArgs: config.rustfmtArgs,
