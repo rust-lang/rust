0760803c06ff4128405ea22e390625938a15bcb2
DutchGhost <kasper199914@gmail.com>
2020-03-16 14:45:37 +0100

    rather than removing const_forget_box, stick an attribute on it and explain it cant be called in ctfe yet

--- src/libcore/tests/mem.rs
+++ src/libcore/tests/mem.rs
@@ -134,4 +134,16 @@ fn test_discriminant_send_sync() {
 fn test_const_forget() {
     const _: () = forget(0i32);
     const _: () = forget(Vec::<Vec<Box<i32>>>::new());
+
+    // Writing this function signature without const-forget
+    // triggers compiler errors:
+    // 1) That we use a non-const fn inside a const fn
+    // 2) without the forget, it complains about the destructor of Box
+    //
+    // FIXME: this method cannot be called in const-eval yet, as Box isn't
+    // const constructable
+    #[allow(unused)]
+    const fn const_forget_box<T: ?Sized>(x: Box<T>) {
+        forget(x);
+    }
 }
