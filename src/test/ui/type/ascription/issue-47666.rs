0a8db690a4cffc687bddcaabc762e3e8746adec6
Mazdak Farrokhzad <twingoow@gmail.com>
2020-03-17 08:55:28 +0100

    nix panictry! in ParserAnyMacro::make

--- src/test/ui/type/ascription/issue-47666.rs
+++ src/test/ui/type/ascription/issue-47666.rs
@@ -1,5 +1,7 @@
 fn main() {
     let _ = Option:Some(vec![0, 1]); //~ ERROR expected type, found
+    //~^ ERROR expected value, found enum `Option`
+    //~| ERROR expected type, found variant `Some`
 }
 
 // This case isn't currently being handled gracefully due to the macro invocation.
