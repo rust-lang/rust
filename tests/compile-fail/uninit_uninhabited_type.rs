0826899ca04af6700269bde37137ab41d2f4dae5
Elichai Turkel <elichai.turkel@gmail.com>
2020-03-18 13:18:08 +0200

    Remove uninhabit/zeroed tests to a new test file for abort checking

---
+++ tests/compile-fail/uninit_uninhabited_type.rs
@@ -0,0 +1,7 @@
+ // error-pattern: the evaluated program aborted execution: attempted to instantiate uninhabited type `!`
+#![feature(never_type)]
+
+#[allow(deprecated, invalid_value)]
+fn main() {
+    unsafe { std::mem::uninitialized::<!>() };
+}
