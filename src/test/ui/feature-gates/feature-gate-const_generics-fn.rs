0c9028606b19a98bd6ba30c836af1574d3825205
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-03-19 10:51:53 +0100

    added `gate-test-const_generics`

--- src/test/ui/feature-gates/feature-gate-const_generics-fn.rs
+++ src/test/ui/feature-gates/feature-gate-const_generics-fn.rs
@@ -1,3 +1,5 @@
+// gate-test-const_generics
+
 fn foo<const X: ()>() {} //~ ERROR const generics are unstable
 
 fn main() {}
--- src/test/ui/feature-gates/feature-gate-const_generics-struct.rs
+++ src/test/ui/feature-gates/feature-gate-const_generics-struct.rs
@@ -1,3 +1,5 @@
+// gate-test-const_generics
+
 struct Foo<const X: usize>([(); X]); //~ ERROR const generics are unstable
 
 fn main() {}
