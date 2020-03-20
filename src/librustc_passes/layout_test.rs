7b49678f5f6e473e69c4a737acd1acc1b8d9a0f1
Ralf Jung <post@ralfj.de>
2020-03-20 23:22:36 +0100

    fmt

--- src/librustc_passes/layout_test.rs
+++ src/librustc_passes/layout_test.rs
@@ -30,10 +30,10 @@ impl ItemLikeVisitor<'tcx> for LayoutTest<'tcx> {
         let item_def_id = self.tcx.hir().local_def_id(item.hir_id);
 
         match item.kind {
-            ItemKind::TyAlias(..) |
-            ItemKind::Enum(..) |
-            ItemKind::Struct(..) |
-            ItemKind::Union(..) => {
+            ItemKind::TyAlias(..)
+            | ItemKind::Enum(..)
+            | ItemKind::Struct(..)
+            | ItemKind::Union(..) => {
                 for attr in self.tcx.get_attrs(item_def_id).iter() {
                     if attr.check_name(sym::rustc_layout) {
                         self.dump_layout_of(item_def_id, item, attr);
