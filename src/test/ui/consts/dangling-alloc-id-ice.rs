ee403d6fc3a5d64d5f4bfa700de132da93acb6dc
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2020-03-21 00:24:59 +0100

    Update test

--- src/test/ui/consts/dangling-alloc-id-ice.rs
+++ src/test/ui/consts/dangling-alloc-id-ice.rs
@@ -1,11 +1,14 @@
+// run-pass
 // https://github.com/rust-lang/rust/issues/55223
 
 union Foo<'a> {
+    //~^ WARN union is never used
     y: &'a (),
     long_live_the_unit: &'static (),
 }
 
-const FOO: &() = { //~ ERROR any use of this value will cause an error
+const FOO: &() = {
+    //~^ WARN constant item is never used
     let y = ();
     unsafe { Foo { y: &y }.long_live_the_unit }
 };
