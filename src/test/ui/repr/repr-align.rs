3f1b79707a080bde63ff9aba3a5c4006a197fc32
Amanieu d'Antras <amanieu@gmail.com>
2020-03-20 18:21:00 +0000

    Fix test

--- src/test/ui/repr/repr-align.rs
+++ src/test/ui/repr/repr-align.rs
@@ -8,11 +8,11 @@ struct S0(i32);
                    //~| ERROR: invalid `repr(align)` attribute: not a power of two
 struct S1(i32);
 
-#[repr(align(4294967296))] //~ ERROR: invalid `repr(align)` attribute: larger than 2^29
-                           //~| ERROR: invalid `repr(align)` attribute: larger than 2^29
+#[repr(align(4294967296))] //~ ERROR: invalid `repr(align)` attribute: larger than 4096
+                           //~| ERROR: invalid `repr(align)` attribute: larger than 4096
 struct S2(i32);
 
-#[repr(align(536870912))] // ok: this is the largest accepted alignment
+#[repr(align(4096))] // ok: this is the largest accepted alignment
 struct S3(i32);
 
 #[repr(align(16.0))] //~ ERROR: invalid `repr(align)` attribute: not an unsuffixed integer
@@ -23,11 +23,11 @@ enum E0 { A, B }
                    //~| ERROR: invalid `repr(align)` attribute: not a power of two
 enum E1 { A, B }
 
-#[repr(align(4294967296))] //~ ERROR: invalid `repr(align)` attribute: larger than 2^29
-                           //~| ERROR: invalid `repr(align)` attribute: larger than 2^29
+#[repr(align(4294967296))] //~ ERROR: invalid `repr(align)` attribute: larger than 4096
+                           //~| ERROR: invalid `repr(align)` attribute: larger than 4096
 enum E2 { A, B }
 
-#[repr(align(536870912))] // ok: this is the largest accepted alignment
+#[repr(align(4096))] // ok: this is the largest accepted alignment
 enum E3 { A, B }
 
 fn main() {}
