82fe7716765c665565ff668c440303fe21345890
Ralf Jung <post@ralfj.de>
2020-03-21 23:26:44 +0100

    also cross-test Windows from Linux, macOS

--- travis.sh
+++ travis.sh
@@ -35,7 +35,13 @@ function run_tests {
 run_tests
 # cross-test 32bit Linux from everywhere
 MIRI_TEST_TARGET=i686-unknown-linux-gnu run_tests
+
 if [ "$TRAVIS_OS_NAME" == linux ]; then
   # cross-test 64bit macOS from Linux
   FOREIGN_TARGET=x86_64-apple-darwin run_tests
+  # cross-test 32bit Windows from Linux
+  FOREIGN_TARGET=i686-pc-windows-msvc run_tests
+elif [ "$TRAVIS_OS_NAME" == osx ]; then
+  # cross-test 64bit Windows from macOS
+  FOREIGN_TARGET=x86_64-pc-windows-msvc run_tests
 fi
