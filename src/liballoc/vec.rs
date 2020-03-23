6556549fa6d74791d1e3e155ae8a4d1480ea4481
Ralf Jung <post@ralfj.de>
2020-03-23 13:40:40 +0100

    fix Vec::extend invalidating unrelated pointers

--- src/liballoc/vec.rs
+++ src/liballoc/vec.rs
@@ -2122,8 +2122,9 @@ where
         self.reserve(slice.len());
         unsafe {
             let len = self.len();
+            let dst_slice = slice::from_raw_parts_mut(self.as_mut_ptr().add(len), slice.len());
+            dst_slice.copy_from_slice(slice);
             self.set_len(len + slice.len());
-            self.get_unchecked_mut(len..).copy_from_slice(slice);
         }
     }
 }
