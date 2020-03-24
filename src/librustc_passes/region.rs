e7f8895359365403a955468156a35bf26a45e0bf
Niko Matsakis <niko@alum.mit.edu>
2020-03-24 15:20:19 -0400

    save/restore `pessimistic_yield` when entering bodies
    This flag is used to make the execution order around `+=` operators
    pessimistic. Failure to save/restore the flag was causing independent
    async blocks to effect one another, leading to strange ICEs and failed
    assumptions.

--- src/librustc_passes/region.rs
+++ src/librustc_passes/region.rs
@@ -720,6 +720,7 @@ impl<'tcx> Visitor<'tcx> for RegionResolutionVisitor<'tcx> {
         let outer_ec = mem::replace(&mut self.expr_and_pat_count, 0);
         let outer_cx = self.cx;
         let outer_ts = mem::take(&mut self.terminating_scopes);
+        let outer_pessimistic_yield = mem::replace(&mut self.pessimistic_yield, false);
         self.terminating_scopes.insert(body.value.hir_id.local_id);
 
         if let Some(root_id) = self.cx.root_id {
@@ -771,6 +772,7 @@ impl<'tcx> Visitor<'tcx> for RegionResolutionVisitor<'tcx> {
         self.expr_and_pat_count = outer_ec;
         self.cx = outer_cx;
         self.terminating_scopes = outer_ts;
+        self.pessimistic_yield = outer_pessimistic_yield;
     }
 
     fn visit_arm(&mut self, a: &'tcx Arm<'tcx>) {
