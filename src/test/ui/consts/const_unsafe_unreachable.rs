b2d1c5a9a9229423831667119823ec6e3dfd0727
q2p <q2p@users.noreply.github.com>
2020-03-24 16:42:54 +0700

    Added `std::intrinsics::unreachable()` test.

--- src/test/ui/consts/const_unsafe_unreachable.rs
+++ src/test/ui/consts/const_unsafe_unreachable.rs
@@ -1,18 +1,28 @@
 // run-pass
 
+#![feature(core_intrinsics)]
 #![feature(const_fn)]
 #![feature(const_if_match)]
 #![feature(const_unreachable_unchecked)]
 
-const unsafe fn foo(x: u8) -> u8 {
+const unsafe fn f_std_hint_unreachable(x: u8) -> u8 {
     match x {
         42 => 34,
         _ => std::hint::unreachable_unchecked(),
     }
 }
 
-const BAR:u8 = unsafe { foo(42) };
+const unsafe fn f_std_intrinsics_unreachable(x: u8) -> u8 {
+    match x {
+        17 => 22,
+        _ => std::intrinsics::unreachable(),
+    }
+}
+
+const FOO:u8 = unsafe { f_std_hint_unreachable(42) };
+const BAR:u8 = unsafe { f_std_intrinsics_unreachable(17) };
 
 fn main() {
-    assert_eq!(BAR, 34);
+    assert_eq!(FOO, 34);
+    assert_eq!(BAR, 22);
 }
