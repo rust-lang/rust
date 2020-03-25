b65bcd3a6d5e072fe89992996a99b782b9c54a4c
Valentin Churavy <v.churavy@gmail.com>
2020-03-25 14:35:11 -0400

    update CI

--- enzyme/julia/src/opt.jl
+++ enzyme/julia/src/opt.jl
@@ -6,22 +6,20 @@ using LLVM
 using LLVM.Interop
 import Libdl
 
-function __init__()
-    llvmver = LLVM.version().major
-    if haskey(ENV, "ENZYME_PATH")
-        enzyme_path = ENV["ENZYME_PATH"]
-    else
-        error("Please set the environment variable ENZYME_PATH")
-    end
-    global libenzyme = joinpath(enzyme_path, "LLVMEnzyme-$(llvmver).$(Libdl.dlext)")
+llvmver = LLVM.version().major
+if haskey(ENV, "ENZYME_PATH")
+    enzyme_path = ENV["ENZYME_PATH"]
+else
+    error("Please set the environment variable ENZYME_PATH")
+end
+const libenzyme = abspath(joinpath(enzyme_path, "LLVMEnzyme-$(llvmver).$(Libdl.dlext)"))
 
-    if !isfile(libenzyme)
-        error("$(libenzyme) does not exist, Please specify a correct path in ENZYME_PATH, and restart Julia.")
-    end
+if !isfile(libenzyme)
+    error("$(libenzyme) does not exist, Please specify a correct path in ENZYME_PATH, and restart Julia.")
+end
 
-    if Libdl.dlopen_e(libenzyme) in (C_NULL, nothing)
-        error("$(libenzyme) cannot be opened, Please specify a correct path in ENZYME_PATH, and restart Julia.")
-    end
+if Libdl.dlopen_e(libenzyme) in (C_NULL, nothing)
+    error("$(libenzyme) cannot be opened, Please specify a correct path in ENZYME_PATH, and restart Julia.")
 end
 
 function enzyme!(pm)
