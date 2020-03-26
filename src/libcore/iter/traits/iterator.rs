a9a2a319afbeb7e2a54f343a9360bd9bc4863b4f
dylan_DPC <dylan.dpc@gmail.com>
2020-03-26 23:18:37 +0100

    fix docs

--- src/libcore/iter/traits/iterator.rs
+++ src/libcore/iter/traits/iterator.rs
@@ -2015,6 +2015,9 @@ pub trait Iterator {
     /// Find the maximum value:
     ///
     /// ```
+    /// #![feature(iterator_fold_self)]
+    /// use std::cmp::Ordering;
+    ///
     /// fn find_max<I>(iter: I) -> Option<I::Item>
     ///     where I: Iterator,
     ///           I::Item: Ord,
@@ -2027,9 +2030,9 @@ pub trait Iterator {
     ///     })
     /// }
     /// let a = [10, 20, 5, -23, 0];
-    /// let b = [];
+    /// let b: [u32; 0] = [];
     ///
-    /// assert_eq!(find_max(a.iter()), Some(20));
+    /// assert_eq!(find_max(a.iter()), Some(&20));
     /// assert_eq!(find_max(b.iter()), None);
     /// ```
     #[inline]
