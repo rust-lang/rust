fa621f80fabe2c9d36fb2af57785e184fb331939
Edwin Cheng <edwin0cheng@gmail.com>
2020-03-27 05:08:26 +0800

    Fix test

--- crates/ra_proc_macro/src/rpc.rs
+++ crates/ra_proc_macro/src/rpc.rs
@@ -252,7 +252,7 @@ mod tests {
         assert_eq!(task.macro_body, back.macro_body);
 
         let result = ExpansionResult { expansion: tt.clone() };
-        let json = serde_json::to_string(&task).unwrap();
+        let json = serde_json::to_string(&result).unwrap();
         let back: ExpansionResult = serde_json::from_str(&json).unwrap();
 
         assert_eq!(result, back);
