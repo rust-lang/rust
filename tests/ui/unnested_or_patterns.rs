d6136b92a203207bf25ee2f3c7c56ca9c881e92b
Mazdak Farrokhzad <twingoow@gmail.com>
2020-03-28 13:08:57 +0100

    split unnested_or_patterns test

--- tests/ui/unnested_or_patterns.rs
+++ tests/ui/unnested_or_patterns.rs
@@ -30,12 +30,4 @@ fn main() {
     }
     if let S { x: 0, y } | S { y, x: 1 } = (S { x: 0, y: 1 }) {}
     if let S { x: 0, y, .. } | S { y, x: 1 } = (S { x: 0, y: 1 }) {}
-    if let Some(Some(0)) | Some(Some(1)) = None {}
-    if let Some(Some(0)) | Some(Some(1) | Some(2)) = None {}
-    if let Some(Some(0 | 1) | Some(2)) | Some(Some(3) | Some(4)) = None {}
-    if let Some(Some(0) | Some(1 | 2)) = None {}
-    if let ((0,),) | ((1,) | (2,),) = ((0,),) {}
-    if let 0 | (1 | 2) = 0 {}
-    if let box (0 | 1) | (box 2 | box (3 | 4)) = Box::new(0) {}
-    if let box box 0 | box (box 2 | box 4) = Box::new(Box::new(0)) {}
 }
