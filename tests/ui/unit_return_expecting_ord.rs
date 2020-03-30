126790999a128a880ca276c49afd2927a66ffbbe
Matthias Krüger <matthias.krueger@famsik.de>
2020-03-30 11:02:14 +0200

    new lint: Returning unit from closures expecting Ord
    This lint catches cases where the last statement of a closure expecting
    an instance of Ord has a trailing semi-colon. It compiles since the
    closure ends up return () which also implements Ord but causes
    unexpected results in cases such as sort_by_key.
    
    Fixes #5080
    
    reprise: rebase, update and address all concerns

---
+++ tests/ui/unit_return_expecting_ord.rs
@@ -0,0 +1,36 @@
+#![warn(clippy::unit_return_expecting_ord)]
+#![allow(clippy::needless_return)]
+#![allow(clippy::unused_unit)]
+#![feature(is_sorted)]
+
+struct Struct {
+    field: isize,
+}
+
+fn double(i: isize) -> isize {
+    i * 2
+}
+
+fn unit(_i: isize) {}
+
+fn main() {
+    let mut structs = vec![Struct { field: 2 }, Struct { field: 1 }];
+    structs.sort_by_key(|s| {
+        double(s.field);
+    });
+    structs.sort_by_key(|s| double(s.field));
+    structs.is_sorted_by_key(|s| {
+        double(s.field);
+    });
+    structs.is_sorted_by_key(|s| {
+        if s.field > 0 {
+            ()
+        } else {
+            return ();
+        }
+    });
+    structs.sort_by_key(|s| {
+        return double(s.field);
+    });
+    structs.sort_by_key(|s| unit(s.field));
+}
