9ebbfa4186fe2dc0c846ba79bb0e65ad84e3b650
leo60228 <iakornfeld@gmail.com>
2020-03-31 11:27:45 -0400

    horizon is (currently) no_std

--- src/bootstrap/config.rs
+++ src/bootstrap/config.rs
@@ -244,7 +244,7 @@ pub struct Target {
 impl Target {
     pub fn from_triple(triple: &str) -> Self {
         let mut target: Self = Default::default();
-        if triple.contains("-none") || triple.contains("nvptx") {
+        if triple.contains("-none") || triple.contains("nvptx") || triple.contains("horizon") {
             target.no_std = true;
         }
         target
