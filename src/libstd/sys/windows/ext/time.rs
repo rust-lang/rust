f90a9419e2fa32ec0bd9818852b952499b2b7ede
JOE1994 <joseph942010@gmail.com>
2020-03-31 10:45:03 -0400

    adjust formatting to pass tidy-check

--- src/libstd/sys/windows/ext/time.rs
+++ src/libstd/sys/windows/ext/time.rs
@@ -9,7 +9,7 @@ use crate::sys::c;
 /// Counter value is a high resolution (<1us) time stamp that can be used for time-interval measurements.
 #[stable(feature = "windows_profileapi", since = "1.43.0")]
 pub fn QueryPerformanceCounter(qpc_value: &mut i64) -> crate::io::Result<i32> {
-    cvt( unsafe { c::QueryPerformanceCounter(qpc_value) } )
+    cvt(unsafe { c::QueryPerformanceCounter(qpc_value) })
 }
 
 /// Stores the frequency of the performance counter to the memory pointed by the argument pointer.
@@ -17,5 +17,5 @@ pub fn QueryPerformanceCounter(qpc_value: &mut i64) -> crate::io::Result<i32> {
 /// Therefore, the frequency need only be queried upon application initialization, and the result can be cached.
 #[stable(feature = "windows_profileapi", since = "1.43.0")]
 pub fn QueryPerformanceFrequency(frequency: &mut i64) -> crate::io::Result<i32> {
-    cvt( unsafe { c::QueryPerformanceFrequency(frequency) } )
+    cvt(unsafe { c::QueryPerformanceFrequency(frequency) })
 }
