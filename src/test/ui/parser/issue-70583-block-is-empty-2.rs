053a696ade1ba05508e099b56fe46ace264c169a
Kevin Per <kevin.per@protonmail.com>
2020-04-02 07:17:46 +0000

    Writing tests

---
+++ src/test/ui/parser/issue-70583-block-is-empty-2.rs
@@ -0,0 +1,14 @@
+pub enum ErrorHandled {
+    Reported,
+    TooGeneric,
+}
+
+impl ErrorHandled {
+    pub fn assert_reported(self) {
+        match self {
+            ErrorHandled::Reported => {}} 
+                                     //^~ ERROR this block is empty, you might have not mean to close it
+            ErrorHandled::TooGeneric => panic!(),
+        }
+    }
+} //~ ERROR unexpected closing delimiter: `}`
