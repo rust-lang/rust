77f38dc284c3adab8c529e478fe8b2fa5e4a82d3
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2020-04-12 07:16:38 +0300

    ty: convert `ErrorHandled::Reported` to `ConstKind::Error`.

--- src/test/ui/closures/closure-array-break-length.rs
+++ src/test/ui/closures/closure-array-break-length.rs
@@ -2,8 +2,6 @@ fn main() {
     |_: [_; continue]| {}; //~ ERROR: `continue` outside of a loop
 
     while |_: [_; continue]| {} {} //~ ERROR: `continue` outside of a loop
-    //~^ ERROR mismatched types
 
     while |_: [_; break]| {} {} //~ ERROR: `break` outside of a loop
-    //~^ ERROR mismatched types
 }
