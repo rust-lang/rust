50eb39757e471ed50ab34862af776f044c88222d
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-04-13 22:32:40 +0200

    allow const generics in const fn

--- src/test/ui/const-generics/const-fn-with-const-param.rs
+++ src/test/ui/const-generics/const-fn-with-const-param.rs
@@ -1,11 +1,11 @@
+// run-pass
 #![feature(const_generics)]
 //~^ WARN the feature `const_generics` is incomplete and may cause the compiler to crash
 
 const fn const_u32_identity<const X: u32>() -> u32 {
-    //~^ ERROR const parameters are not permitted in const functions
     X
 }
 
 fn main() {
-    println!("{:?}", const_u32_identity::<18>());
+    assert_eq!(const_u32_identity::<18>(), 18);
 }
