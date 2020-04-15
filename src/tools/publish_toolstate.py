6d0fbd1c7ce63080662e46eb043632bc501f5b37
Pietro Albini <pietro@pietroalbini.org>
2020-04-15 18:19:02 +0200

    update tool maintainers

--- src/tools/publish_toolstate.py
+++ src/tools/publish_toolstate.py
@@ -40,7 +40,7 @@ MAINTAINERS = {
         'adamgreig', 'andre-richter', 'jamesmunns', 'korken89',
         'ryankurte', 'thejpster', 'therealprof',
     },
-    'edition-guide': {'ehuss', 'Centril', 'steveklabnik'},
+    'edition-guide': {'ehuss', 'steveklabnik'},
     'rustc-dev-guide': {'mark-i-m', 'spastorino', 'amanjeev', 'JohnTitor'},
 }
 
