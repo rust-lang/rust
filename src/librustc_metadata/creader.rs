9f23b2d36bf31b89a107b83a37665af08f6c0fb3
Amanieu d'Antras <amanieu@gmail.com>
2020-04-17 21:21:49 +0100

    Add an option to inhibit automatic injection of profiler_builtins

--- src/librustc_metadata/creader.rs
+++ src/librustc_metadata/creader.rs
@@ -686,7 +686,9 @@ impl<'a> CrateLoader<'a> {
     }
 
     fn inject_profiler_runtime(&mut self) {
-        if self.sess.opts.debugging_opts.profile || self.sess.opts.cg.profile_generate.enabled() {
+        if (self.sess.opts.debugging_opts.profile || self.sess.opts.cg.profile_generate.enabled())
+            && !self.sess.opts.debugging_opts.no_profiler_runtime
+        {
             info!("loading profiler");
 
             let name = Symbol::intern("profiler_builtins");
