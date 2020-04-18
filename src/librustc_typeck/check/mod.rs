14119a2a05532a719628682f620137aa6a55cd8c
marmeladema <xademax@gmail.com>
2020-04-18 17:10:50 +0100

    Accept `LocalDefId` as key for `check_trait_item_well_formed` query

--- src/librustc_typeck/check/mod.rs
+++ src/librustc_typeck/check/mod.rs
@@ -758,8 +758,8 @@ fn check_item_well_formed(tcx: TyCtxt<'_>, def_id: LocalDefId) {
     wfcheck::check_item_well_formed(tcx, def_id);
 }
 
-fn check_trait_item_well_formed(tcx: TyCtxt<'_>, def_id: DefId) {
-    wfcheck::check_trait_item(tcx, def_id.expect_local());
+fn check_trait_item_well_formed(tcx: TyCtxt<'_>, def_id: LocalDefId) {
+    wfcheck::check_trait_item(tcx, def_id);
 }
 
 fn check_impl_item_well_formed(tcx: TyCtxt<'_>, def_id: DefId) {
