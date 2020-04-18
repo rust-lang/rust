fe7216d4214c4c3c4580a74eef5534e37a944495
Esteban Küber <esteban@kuber.com.ar>
2020-04-18 16:07:29 -0700

    review comment

--- src/librustc_resolve/late/lifetimes.rs
+++ src/librustc_resolve/late/lifetimes.rs
@@ -2387,6 +2387,7 @@ impl<'a, 'tcx> LifetimeContext<'a, 'tcx> {
         let mut err = self.report_missing_lifetime_specifiers(span, lifetime_refs.len());
 
         if let Some(params) = error {
+            // If there's no lifetime available, suggest `'static`.
             if self.report_elision_failure(&mut err, params) && lifetime_names.is_empty() {
                 lifetime_names.insert(ast::Ident::from_str("'static"));
             }
@@ -2405,7 +2406,7 @@ impl<'a, 'tcx> LifetimeContext<'a, 'tcx> {
         &mut self,
         db: &mut DiagnosticBuilder<'_>,
         params: &[ElisionFailureInfo],
-    ) -> bool {
+    ) -> bool /* add `'static` lifetime to lifetime list */ {
         let mut m = String::new();
         let len = params.len();
 
