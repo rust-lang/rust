efe30de0e2b45c9e3178a14229aef7a6c7184050
marmeladema <xademax@gmail.com>
2020-04-18 01:10:15 +0100

    Accept `LocalDefId` as keyt for `names_imported_by_glob_use`
    and `maybe_unused_trait_import` queries

--- src/librustc_middle/query/mod.rs
+++ src/librustc_middle/query/mod.rs
@@ -1030,17 +1030,19 @@ rustc_queries! {
         query upvars(_: DefId) -> Option<&'tcx FxIndexMap<hir::HirId, hir::Upvar>> {
             eval_always
         }
-        query maybe_unused_trait_import(_: DefId) -> bool {
+        query maybe_unused_trait_import(def_id: LocalDefId) -> bool {
             eval_always
+            desc { |tcx| "maybe_unused_trait_import for `{}`", tcx.def_path_str(def_id.to_def_id()) }
         }
         query maybe_unused_extern_crates(_: CrateNum)
             -> &'tcx [(DefId, Span)] {
             eval_always
             desc { "looking up all possibly unused extern crates" }
         }
-        query names_imported_by_glob_use(_: DefId)
+        query names_imported_by_glob_use(def_id: LocalDefId)
             -> &'tcx FxHashSet<ast::Name> {
             eval_always
+            desc { |tcx| "names_imported_by_glob_use for `{}`", tcx.def_path_str(def_id.to_def_id()) }
         }
 
         query stability_index(_: CrateNum) -> &'tcx stability::Index<'tcx> {
