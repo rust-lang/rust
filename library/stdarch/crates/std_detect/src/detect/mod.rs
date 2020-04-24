a69b5ec7aebb3eee651d5aba779461483b9d1f33
Tobias Kortkamp <t@tobik.me>
2020-04-24 07:01:25 +0200

    Unbreak non-x86 build on FreeBSD
    error[E0432]: unresolved import `self::arm::check_for`
      --> src/libstd/../stdarch/crates/std_detect/src/detect/os/freebsd/mod.rs:11:17
       |
    11 |         pub use self::arm::check_for;
       |                 ^^^^^^^^^^^^^^^^^^^^ no `check_for` in `std_detect::detect::os::arm`
    
    error[E0425]: cannot find value `detect_features` in module `self::os`
       --> src/libstd/../stdarch/crates/std_detect/src/detect/mod.rs:121:37
        |
    121 |     cache::test(x as u32, self::os::detect_features)
        |                                     ^^^^^^^^^^^^^^^ not found in `self::os`
        |
    help: possible candidate is found in another module, you can import it into scope
        |
    20  | use crate::std_detect::detect::os::arm::detect_features;

--- library/stdarch/crates/std_detect/src/detect/mod.rs
+++ library/stdarch/crates/std_detect/src/detect/mod.rs
@@ -100,7 +100,7 @@ cfg_if! {
     } else if #[cfg(all(target_os = "linux", feature = "use_std"))] {
         #[path = "os/linux/mod.rs"]
         mod os;
-    } else if #[cfg(target_os = "freebsd")] {
+    } else if #[cfg(all(target_os = "freebsd", feature = "use_std"))] {
         #[cfg(target_arch = "aarch64")]
         #[path = "os/aarch64.rs"]
         mod aarch64;
