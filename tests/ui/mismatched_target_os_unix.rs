303e7d1af8f1a2eb0c584e94f4392104227a9392
Eduardo Broto <ebroto@tutanota.com>
2020-04-26 21:26:19 +0200

    Split tests in unix/non-unix

--- tests/ui/mismatched_target_os_unix.rs
+++ tests/ui/mismatched_target_os_unix.rs
@@ -3,8 +3,6 @@
 #![warn(clippy::mismatched_target_os)]
 #![allow(unused)]
 
-// unix
-
 #[cfg(linux)]
 fn linux() {}
 
@@ -38,6 +36,12 @@ fn fuchsia() {}
 #[cfg(haiku)]
 fn haiku() {}
 
+#[cfg(illumos)]
+fn illumos() {}
+
+#[cfg(l4re)]
+fn l4re() {}
+
 #[cfg(redox)]
 fn redox() {}
 
@@ -47,30 +51,12 @@ fn solaris() {}
 #[cfg(vxworks)]
 fn vxworks() {}
 
-// non-unix
-
-#[cfg(cloudabi)]
-fn cloudabi() {}
-
-#[cfg(hermit)]
-fn hermit() {}
-
-#[cfg(wasi)]
-fn wasi() {}
-
-#[cfg(none)]
-fn none() {}
-
 // list with conditions
-#[cfg(all(not(any(windows, linux)), freebsd))]
+#[cfg(all(not(any(solaris, linux)), freebsd))]
 fn list() {}
-
-// windows is a valid target family, should be ignored
-#[cfg(windows)]
-fn windows() {}
 
 // correct use, should be ignored
 #[cfg(target_os = "freebsd")]
-fn freebsd() {}
+fn correct() {}
 
 fn main() {}
