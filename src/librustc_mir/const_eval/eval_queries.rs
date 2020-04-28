e8ffa2182bc41f3511a214a8ae6dad85d9d60e79
Ralf Jung <post@ralfj.de>
2020-04-28 23:48:22 +0200

    better document const-pattern dynamic soundness checks, and fix a soundness hole

--- src/librustc_mir/const_eval/eval_queries.rs
+++ src/librustc_mir/const_eval/eval_queries.rs
@@ -193,7 +193,7 @@ fn validate_and_turn_into_const<'tcx>(
                     mplace.into(),
                     path,
                     &mut ref_tracking,
-                    /*may_ref_to_static*/ is_static,
+                    /*may_ref_to_static*/ ecx.memory.extra.can_access_statics,
                 )?;
             }
         }
