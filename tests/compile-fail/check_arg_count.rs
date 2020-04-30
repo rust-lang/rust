f46d1974313c83886508088ff1beac3a107c3790
Chase Albert <thaoeuns@gmail.com>
2020-04-30 18:16:56 -0400

    Test for too many args.

--- tests/compile-fail/check_arg_count.rs
+++ tests/compile-fail/check_arg_count.rs
@@ -4,4 +4,5 @@ use std::intrinsics;
 
 fn main() {
     unsafe { intrinsics::forget(); } //~ ERROR this function takes 1 argument but 0 arguments were supplied
+    unsafe { intrinsics::forget(1, 2); } //~ ERROR this function takes 1 argument but 2 arguments were supplied
 }
