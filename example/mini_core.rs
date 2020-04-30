12c92a32f63130c18ba06d7320d2b28a2bd76625
bjorn3 <bjorn3@users.noreply.github.com>
2020-04-30 11:28:48 +0200

    Rustup to rustc 1.45.0-nightly (fa51f810e 2020-04-29)

--- example/mini_core.rs
+++ example/mini_core.rs
@@ -85,6 +85,12 @@ unsafe impl<T: ?Sized> Freeze for *mut T {}
 unsafe impl<T: ?Sized> Freeze for &T {}
 unsafe impl<T: ?Sized> Freeze for &mut T {}
 
+#[lang = "structural_peq"]
+pub trait StructuralPartialEq {}
+
+#[lang = "structural_teq"]
+pub trait StructuralEq {}
+
 #[lang = "not"]
 pub trait Not {
     type Output;
