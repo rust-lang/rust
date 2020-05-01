1a704a517a46c84ca6d347b863ea12173f55b8ec
Ralf Jung <post@ralfj.de>
2020-05-01 10:29:39 +0200

    rustup, adjust error messages

--- tests/compile-fail/validity/execute_memory.rs
+++ tests/compile-fail/validity/execute_memory.rs
@@ -3,6 +3,6 @@
 fn main() {
     let x = box 42;
     unsafe {
-        let _f = std::mem::transmute::<Box<i32>, fn()>(x); //~ ERROR encountered a pointer, but expected a function pointer
+        let _f = std::mem::transmute::<Box<i32>, fn()>(x); //~ ERROR expected a function pointer
     }
 }
