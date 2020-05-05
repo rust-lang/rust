f741f2cc714b90a224f0b8739c6d57107bf372cc
Chase Albert <thaoeuns@gmail.com>
2020-05-04 23:22:00 -0400

    Correct the test.

--- tests/compile-fail/check_arg_count.rs
+++
@@ -1,8 +0,0 @@
-#![feature(core_intrinsics)]
-
-use std::intrinsics;
-
-fn main() {
-    unsafe { intrinsics::forget(); } //~ ERROR this function takes 1 argument but 0 arguments were supplied
-    unsafe { intrinsics::forget(1, 2); } //~ ERROR this function takes 1 argument but 2 arguments were supplied
-}
