15154a882d2c60d7aa21ceb6ec0c79c488ccf2e0
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2020-05-07 15:37:06 -0400

    Use fp64 detection instead of OS blacklist

--- library/stdarch/crates/core_arch/src/mips/mod.rs
+++ library/stdarch/crates/core_arch/src/mips/mod.rs
@@ -1,13 +1,10 @@
 //! MIPS
 
-// Building this module (even if unused) for non-fp64 targets such as the Sony
-// PSP fails with an LLVM error. There doesn't seem to be a good way to detect
-// fp64 support as it is sometimes implied by the target cpu, so
-// `#[cfg(target_feature = "fp64")]` will unfortunately not work. This is a
-// fairly conservative workaround that only disables MSA intrinsics for the PSP.
-#[cfg(not(target_os = "psp"))]
+// Building this module (even if unused) for non-fp64 targets fails with an LLVM
+// error.
+#[cfg(target_feature = "fp64")]
 mod msa;
-#[cfg(not(target_os = "psp"))]
+#[cfg(target_feature = "fp64")]
 pub use self::msa::*;
 
 #[cfg(test)]
