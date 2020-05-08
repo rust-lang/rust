ac1fb93fce892f52a5b563b2fcb364f6a603fc0d
Amanieu d'Antras <amanieu@gmail.com>
2020-05-08 16:37:57 +0100

    Fix feature gate tests

--- src/test/ui/feature-gates/feature-gate-asm.rs
+++ src/test/ui/feature-gates/feature-gate-asm.rs
@@ -2,7 +2,9 @@
 
 fn main() {
     unsafe {
-        asm!(""); //~ ERROR inline assembly is not stable enough
-        llvm_asm!(""); //~ ERROR inline assembly is not stable enough
+        asm!("");
+        //~^ ERROR inline assembly is not stable enough
+        llvm_asm!("");
+        //~^ ERROR LLVM-style inline assembly will never be stabilized
     }
 }
