c035299548afdef98c7d820ddafa144dd3e36439
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-05-19 16:29:13 +0200

    tests

---
+++ src/test/ui/const-generics/issues/issue-69654-run-pass.rs
@@ -0,0 +1,19 @@
+#![feature(const_generics)]
+#![allow(incomplete_features, unused_braces)]
+
+trait Bar<T> {}
+impl<T> Bar<T> for [u8; {7}] {}
+
+struct Foo<const N: usize> {}
+impl<const N: usize> Foo<N>
+where
+    [u8; N]: Bar<[(); N]>,
+{
+    fn foo() {}
+}
+
+fn main() {
+    Foo::foo();
+    //~^ ERROR no function or associated item named `foo`
+    // FIXME(const_generics): The above should not error
+}
