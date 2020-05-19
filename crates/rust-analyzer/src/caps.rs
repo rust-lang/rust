6bf4fc27d923207df36d06da295d815d768f3a0b
kjeremy <kjeremy@gmail.com>
2020-05-19 11:56:51 -0400

    LSP: Two stage initialization
    Fills in server information.
    
    Derives CodeAction capabilities from the client. If code action literals
    are unsupported we fall back to the "simple support" which just sends back
    commands (this is already supported in our config). The difference being
    that we did not adjust our server capabilities so that if the client was
    checking for `CodeActionProvider: "true"` in the response that would have failed.

--- crates/rust-analyzer/src/caps.rs
+++ crates/rust-analyzer/src/caps.rs
@@ -4,16 +4,47 @@ use std::env;
 use crate::semantic_tokens;
 
 use lsp_types::{
-    CallHierarchyServerCapability, CodeActionOptions, CodeActionProviderCapability,
-    CodeLensOptions, CompletionOptions, DocumentOnTypeFormattingOptions,
-    FoldingRangeProviderCapability, ImplementationProviderCapability, RenameOptions,
-    RenameProviderCapability, SaveOptions, SelectionRangeProviderCapability,
-    SemanticTokensDocumentProvider, SemanticTokensLegend, SemanticTokensOptions,
-    ServerCapabilities, SignatureHelpOptions, TextDocumentSyncCapability, TextDocumentSyncKind,
-    TextDocumentSyncOptions, TypeDefinitionProviderCapability, WorkDoneProgressOptions,
+    CallHierarchyServerCapability, ClientCapabilities, CodeActionOptions,
+    CodeActionProviderCapability, CodeLensOptions, CompletionOptions,
+    DocumentOnTypeFormattingOptions, FoldingRangeProviderCapability,
+    ImplementationProviderCapability, RenameOptions, RenameProviderCapability, SaveOptions,
+    SelectionRangeProviderCapability, SemanticTokensDocumentProvider, SemanticTokensLegend,
+    SemanticTokensOptions, ServerCapabilities, SignatureHelpOptions, TextDocumentSyncCapability,
+    TextDocumentSyncKind, TextDocumentSyncOptions, TypeDefinitionProviderCapability,
+    WorkDoneProgressOptions,
 };
 
-pub fn server_capabilities() -> ServerCapabilities {
+pub fn server_capabilities(client_caps: &ClientCapabilities) -> ServerCapabilities {
+    let mut code_action_provider = CodeActionProviderCapability::Simple(true);
+
+    match client_caps.text_document.as_ref() {
+        Some(it) => {
+            match it.code_action.as_ref().and_then(|c| c.code_action_literal_support.as_ref()) {
+                Some(_literal_support) => {
+                    code_action_provider =
+                        CodeActionProviderCapability::Options(CodeActionOptions {
+                            // Advertise support for all built-in CodeActionKinds.
+                            // Ideally we would base this off of the client capabilities
+                            // but the client is supposed to fall back gracefully for unknown values.
+                            code_action_kinds: Some(vec![
+                                lsp_types::code_action_kind::EMPTY.to_string(),
+                                lsp_types::code_action_kind::QUICKFIX.to_string(),
+                                lsp_types::code_action_kind::REFACTOR.to_string(),
+                                lsp_types::code_action_kind::REFACTOR_EXTRACT.to_string(),
+                                lsp_types::code_action_kind::REFACTOR_INLINE.to_string(),
+                                lsp_types::code_action_kind::REFACTOR_REWRITE.to_string(),
+                                lsp_types::code_action_kind::SOURCE.to_string(),
+                                lsp_types::code_action_kind::SOURCE_ORGANIZE_IMPORTS.to_string(),
+                            ]),
+                            work_done_progress_options: Default::default(),
+                        });
+                }
+                None => {}
+            }
+        }
+        None => {}
+    };
+
     ServerCapabilities {
         text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
             open_close: Some(true),
@@ -45,20 +76,7 @@ pub fn server_capabilities() -> ServerCapabilities {
         document_highlight_provider: Some(true),
         document_symbol_provider: Some(true),
         workspace_symbol_provider: Some(true),
-        code_action_provider: Some(CodeActionProviderCapability::Options(CodeActionOptions {
-            // Advertise support for all built-in CodeActionKinds
-            code_action_kinds: Some(vec![
-                lsp_types::code_action_kind::EMPTY.to_string(),
-                lsp_types::code_action_kind::QUICKFIX.to_string(),
-                lsp_types::code_action_kind::REFACTOR.to_string(),
-                lsp_types::code_action_kind::REFACTOR_EXTRACT.to_string(),
-                lsp_types::code_action_kind::REFACTOR_INLINE.to_string(),
-                lsp_types::code_action_kind::REFACTOR_REWRITE.to_string(),
-                lsp_types::code_action_kind::SOURCE.to_string(),
-                lsp_types::code_action_kind::SOURCE_ORGANIZE_IMPORTS.to_string(),
-            ]),
-            work_done_progress_options: Default::default(),
-        })),
+        code_action_provider: Some(code_action_provider),
         code_lens_provider: Some(CodeLensOptions { resolve_provider: Some(true) }),
         document_formatting_provider: Some(true),
         document_range_formatting_provider: None,
