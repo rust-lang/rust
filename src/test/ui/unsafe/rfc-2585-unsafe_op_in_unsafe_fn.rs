925d5ac45f4a44dd61f1ebd4b7bd3cc89fcb2b5f
LeSeulArtichaut <leseulartichaut@gmail.com>
2020-05-21 23:53:12 +0200

    Fix and bless tests

--- src/test/ui/unsafe/rfc-2585-unsafe_op_in_unsafe_fn.rs
+++ src/test/ui/unsafe/rfc-2585-unsafe_op_in_unsafe_fn.rs
@@ -1,5 +1,5 @@
 #![feature(unsafe_block_in_unsafe_fn)]
-#![warn(unsafe_op_in_unsafe_fn)]
+#![deny(unsafe_op_in_unsafe_fn)]
 #![deny(unused_unsafe)]
 #![deny(safe_packed_borrows)]
 
@@ -14,18 +14,34 @@ pub struct Packed {
 
 const PACKED: Packed = Packed { data: &0 };
 
-unsafe fn foo() {
+unsafe fn deny_level() {
     unsf();
-    //~^ WARNING call to unsafe function is unsafe and requires unsafe block
+    //~^ ERROR call to unsafe function is unsafe and requires unsafe block
     *PTR;
-    //~^ WARNING dereference of raw pointer is unsafe and requires unsafe block
+    //~^ ERROR dereference of raw pointer is unsafe and requires unsafe block
     VOID = ();
-    //~^ WARNING use of mutable static is unsafe and requires unsafe block
-    &PACKED.data; // the level for the `safe_packed_borrows` lint is ignored
-    //~^ WARNING borrow of packed field is unsafe and requires unsafe block
+    //~^ ERROR use of mutable static is unsafe and requires unsafe block
+    &PACKED.data;
+    //~^ ERROR borrow of packed field is unsafe and requires unsafe block
+    //~| WARNING this was previously accepted by the compiler but is being phased out
 }
 
-unsafe fn bar() {
+// Check that `unsafe_op_in_unsafe_fn` works starting from the `warn` level.
+#[warn(unsafe_op_in_unsafe_fn)]
+#[deny(warnings)]
+unsafe fn warning_level() {
+    unsf();
+    //~^ ERROR call to unsafe function is unsafe and requires unsafe block
+    *PTR;
+    //~^ ERROR dereference of raw pointer is unsafe and requires unsafe block
+    VOID = ();
+    //~^ ERROR use of mutable static is unsafe and requires unsafe block
+    &PACKED.data;
+    //~^ ERROR borrow of packed field is unsafe and requires unsafe block
+    //~| WARNING this was previously accepted by the compiler but is being phased out
+}
+
+unsafe fn explicit_block() {
     // no error
     unsafe {
         unsf();
@@ -35,13 +51,25 @@ unsafe fn bar() {
     }
 }
 
-unsafe fn baz() {
+unsafe fn two_explicit_blocks() {
     unsafe { unsafe { unsf() } }
     //~^ ERROR unnecessary `unsafe` block
 }
 
+#[warn(safe_packed_borrows)]
+unsafe fn warn_packed_borrows() {
+    &PACKED.data;
+    //~^ WARNING borrow of packed field is unsafe and requires unsafe block
+    //~| WARNING this was previously accepted by the compiler but is being phased out
+}
+
+#[allow(safe_packed_borrows)]
+unsafe fn allow_packed_borrows() {
+    &PACKED.data; // `safe_packed_borrows` is allowed, no error
+}
+
 #[allow(unsafe_op_in_unsafe_fn)]
-unsafe fn qux() {
+unsafe fn allow_level() {
     // lint allowed -> no error
     unsf();
     *PTR;
@@ -52,7 +80,26 @@ unsafe fn qux() {
     //~^ ERROR unnecessary `unsafe` block
 }
 
+unsafe fn nested_allow_level() {
+    #[allow(unsafe_op_in_unsafe_fn)]
+    {
+        // lint allowed -> no error
+        unsf();
+        *PTR;
+        VOID = ();
+        &PACKED.data;
+
+        unsafe { unsf() }
+        //~^ ERROR unnecessary `unsafe` block
+    }
+}
+
 fn main() {
-    unsf()
-    //~^ ERROR call to unsafe function is unsafe and requires unsafe function or block
+    unsf();
+    //~^ ERROR call to unsafe function is unsafe and requires unsafe block
+    #[allow(unsafe_op_in_unsafe_fn)]
+    {
+        unsf();
+        //~^ ERROR call to unsafe function is unsafe and requires unsafe function or block
+    }
 }
