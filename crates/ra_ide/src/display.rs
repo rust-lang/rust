5043c9ce9c9d07263305ef0e560b8ce506593748
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-05-22 19:04:39 +0200

    Modify hover info to show module path separated by a line

--- crates/ra_ide/src/display.rs
+++ crates/ra_ide/src/display.rs
@@ -79,14 +79,14 @@ pub(crate) fn rust_code_markup_with_doc(
     doc: Option<&str>,
     mod_path: Option<&str>,
 ) -> String {
-    let mut buf = "```rust\n".to_owned();
+    let mut buf = "".to_owned();
 
     if let Some(mod_path) = mod_path {
         if !mod_path.is_empty() {
-            format_to!(buf, "{}\n\n", mod_path);
+            format_to!(buf, "{}\n___\n", mod_path);
         }
     }
-    format_to!(buf, "{}\n```", code);
+    format_to!(buf, "```rust\n\n{}\n```", code);
 
     if let Some(doc) = doc {
         format_to!(buf, "\n\n{}", doc);
