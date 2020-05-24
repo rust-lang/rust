19cd9103d85f51fc84f262b979f2877525d6a124
Jonas Schievink <jonasschievink@gmail.com>
2020-05-24 18:22:04 +0200

    Implement a destination propagation pass

--- src/librustc_mir/lib.rs
+++ src/librustc_mir/lib.rs
@@ -6,6 +6,7 @@ Rust MIR: a lowered representation of Rust.
 
 #![feature(nll)]
 #![feature(in_band_lifetimes)]
+#![feature(bindings_after_at)]
 #![feature(bool_to_option)]
 #![feature(box_patterns)]
 #![feature(box_syntax)]
