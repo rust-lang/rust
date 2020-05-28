2602e951c0301e6502e835b1842e3e144d10c306
Samrat Man Singh <samratmansingh@gmail.com>
2020-05-28 11:24:47 +0530

    Handle `read`s on STDIN

--- src/shims/posix/foreign_items.rs
+++ src/shims/posix/foreign_items.rs
@@ -67,7 +67,18 @@ pub trait EvalContextExt<'mir, 'tcx: 'mir>: crate::MiriEvalContextExt<'mir, 'tcx
                 let buf = this.read_scalar(buf)?.not_undef()?;
                 let count = this.read_scalar(count)?.to_machine_usize(this)?;
                 let result = if fd == 0 {
-                    throw_unsup_format!("reading from stdin is not implemented")
+                    use std::io::{self, Read};
+
+                    let mut buffer = String::new();
+                    let res = io::stdin().read_to_string(&mut buffer);
+
+                    match res {
+                        Ok(bytes) => {
+                            this.memory.write_bytes(buf, buffer.bytes())?;
+                            i64::try_from(bytes).unwrap()
+                        },
+                        Err(_) => -1,
+                    }
                 } else if fd == 1 || fd == 2 {
                     throw_unsup_format!("cannot read from stdout/stderr")
                 } else {
