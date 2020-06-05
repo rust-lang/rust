5bb2351826f5ef9bb0e5f1c2dcead776da05da3a
Rich Kadel <richkadel@google.com>
2020-06-04 17:52:27 -0700

    removed experiments for cleaner github PR

--- src/test/codegen/coverage-experiments/src/just_main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("hello world! (should be covered)");
-}
--- src/test/codegen/coverage-experiments/src/match_without_increment.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let a = 1;
-    let b = 10;
-    let _result = match a < b { true => true, _ => false, };
-}
