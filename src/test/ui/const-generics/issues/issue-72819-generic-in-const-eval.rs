ab7355a8860b1a16672f3678aebf64cadc53ab18
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-06-06 12:00:23 -0700

    fixup! Add regression test for const generic ICE in #72819

--- src/test/ui/const-generics/issues/issue-72819-generic-in-const-eval.rs
+++ src/test/ui/const-generics/issues/issue-72819-generic-in-const-eval.rs
@@ -1,7 +1,8 @@
 #![feature(const_generics)]
 #![allow(incomplete_features)]
 struct Arr<const N: usize>
-where Assert::<{N < usize::max_value() / 2}>: IsTrue, //~ ERROR constant expression depends on a generic parameter
+where Assert::<{N < usize::max_value() / 2}>: IsTrue,
+//~^ ERROR constant expression depends on a generic parameter
 {
 }
 
