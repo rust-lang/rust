e9b67d281a3a1e740217ded7df9fc292f5ba6559
Amanieu d'Antras <amanieu@gmail.com>
2020-06-06 18:26:15 +0100

    Fix link error with #[thread_local] introduced by #71192

---
+++ src/test/ui/tls.rs
@@ -0,0 +1,12 @@
+// run-pass
+
+#![feature(thread_local)]
+
+#[thread_local]
+static S: u32 = 222;
+
+fn main() {
+    let local = &S as *const u32 as usize;
+    let foreign = std::thread::spawn(|| &S as *const u32 as usize).join().unwrap();
+    assert_ne!(local, foreign);
+}
