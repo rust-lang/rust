7b0906b2e96fee11544c55ee56228bab782f1401
alamb <andrew@nerdnetworks.org>
2020-06-08 06:04:34 -0400

    Fix small typo in docs for std::mem::drop

--- src/libcore/mem/mod.rs
+++ src/libcore/mem/mod.rs
@@ -808,7 +808,7 @@ pub fn replace<T>(dest: &mut T, mut src: T) -> T {
 
 /// Disposes of a value.
 ///
-/// This does call the argument's implementation of [`Drop`][drop].
+/// This does so by calling the argument's implementation of [`Drop`][drop].
 ///
 /// This effectively does nothing for types which implement `Copy`, e.g.
 /// integers. Such values are copied and _then_ moved into the function, so the
