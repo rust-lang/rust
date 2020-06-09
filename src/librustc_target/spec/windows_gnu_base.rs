ffebd8ac8aa290cff345a407a734c5fdf0785fdb
Mateusz Mikuła <mati865@gmail.com>
2020-06-09 23:57:16 +0200

    Reoder order in which MinGW libs are linked

--- src/librustc_target/spec/windows_gnu_base.rs
+++ src/librustc_target/spec/windows_gnu_base.rs
@@ -20,9 +20,9 @@ pub fn opts() -> TargetOptions {
     late_link_args.insert(
         LinkerFlavor::Gcc,
         vec![
+            "-lmsvcrt".to_string(),
             "-lmingwex".to_string(),
             "-lmingw32".to_string(),
-            "-lmsvcrt".to_string(),
             // mingw's msvcrt is a weird hybrid import library and static library.
             // And it seems that the linker fails to use import symbols from msvcrt
             // that are required from functions in msvcrt in certain cases. For example
