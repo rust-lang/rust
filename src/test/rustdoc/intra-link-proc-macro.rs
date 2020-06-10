34c6b38e68f2e88bcc6c943494a7c05d35a71f17
Manish Goregaokar <manishsmail@gmail.com>
2020-06-10 09:11:17 -0700

    Add tests for macro@ and derive@

--- src/test/rustdoc/intra-link-proc-macro.rs
+++ src/test/rustdoc/intra-link-proc-macro.rs
@@ -1,6 +1,5 @@
 // aux-build:intra-link-proc-macro-macro.rs
 // build-aux-docs
-// @has intra_link_proc_macro/index.html
 #![deny(intra_doc_link_resolution_failure)]
 
 extern crate intra_link_proc_macro_macro;
@@ -9,6 +8,7 @@ extern crate intra_link_proc_macro_macro;
 pub use intra_link_proc_macro_macro::{DeriveA, attr_a};
 use intra_link_proc_macro_macro::{DeriveB, attr_b};
 
+// @has intra_link_proc_macro/struct.Foo.html
 // @has - '//a/@href' '../intra_link_proc_macro/derive.DeriveA.html'
 // @has - '//a/@href' '../intra_link_proc_macro/attr.attr_a.html'
 // @has - '//a/@href' '../intra_link_proc_macro/trait.DeriveTrait.html'
@@ -16,6 +16,12 @@ use intra_link_proc_macro_macro::{DeriveB, attr_b};
 // @has - '//a/@href' '../intra_link_proc_macro_macro/attr.attr_b.html'
 /// Link to [DeriveA], [attr_a], [DeriveB], [attr_b], [DeriveTrait]
 pub struct Foo;
+
+// @has intra_link_proc_macro/struct.Bar.html
+// @has - '//a/@href' '../intra_link_proc_macro/derive.DeriveA.html'
+// @has - '//a/@href' '../intra_link_proc_macro/attr.attr_a.html'
+/// Link to [deriveA](derive@DeriveA) [attr](macro@attr_a)
+pub struct Bar;
 
 // this should not cause ambiguity errors
 pub trait DeriveTrait {}
