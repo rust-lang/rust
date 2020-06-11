879693e63c19b474153473e11f96ab9c321739db
Matthew Jasper <mjjasper1@gmail.com>
2020-06-11 18:14:57 +0100

    Move complex inline test to own file

--- crates/ra_syntax/test_data/parser/inline/ok/0003_where_pred_for.rs
+++ crates/ra_syntax/test_data/parser/inline/ok/0003_where_pred_for.rs
@@ -2,19 +2,3 @@ fn for_trait<F>()
 where
    for<'a> F: Fn(&'a str)
 { }
-fn for_ref<F>()
-where
-   for<'a> &'a F: Debug
-{ }
-fn for_parens<F>()
-where
-   for<'a> (&'a F): Fn(&'a str)
-{ }
-fn for_slice<F>()
-where
-   for<'a> [&'a F]: Eq
-{ }
-fn for_qpath<T>(_t: &T)
-where
-    for<'a> <&'a T as Baz>::Foo: Iterator
-{ }
