7c5b66f3287c95df7e1aaca1d2a0576eed69f7dd
Camelid <camelidcamel@gmail.com>
2020-06-15 12:11:49 -0700

    Update duplicate options test

--- src/test/ui/asm/duplicate-options.rs
+++ src/test/ui/asm/duplicate-options.rs
@@ -6,19 +6,13 @@ fn main() {
     unsafe {
         asm!("", options(nomem, nomem));
         //~^ ERROR the `nomem` option was already provided
-        //~| HELP remove this option
         asm!("", options(att_syntax, att_syntax));
         //~^ ERROR the `att_syntax` option was already provided
-        //~| HELP remove this option
         asm!("", options(nostack, att_syntax), options(nostack));
         //~^ ERROR the `nostack` option was already provided
-        //~| HELP remove this option
         asm!("", options(nostack, nostack), options(nostack), options(nostack));
         //~^ ERROR the `nostack` option was already provided
-        //~| HELP remove this option
         //~| ERROR the `nostack` option was already provided
-        //~| HELP remove this option
         //~| ERROR the `nostack` option was already provided
-        //~| HELP remove this option
     }
 }
