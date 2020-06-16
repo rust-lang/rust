9f50f84ef106c7f521d1322ec39562610339f74d
root <aszenz@gmail.com>
2020-06-16 18:14:32 +0000

    break long line for formatting

--- src/rustllvm/PassWrapper.cpp
+++ src/rustllvm/PassWrapper.cpp
@@ -425,7 +425,11 @@ extern "C" void LLVMRustPrintTargetFeatures(LLVMTargetMachineRef TM) {
   for (auto &Feature : FeatTable)
     printf("    %-*s - %s.\n", MaxFeatLen, Feature.Key, Feature.Desc);
   printf("Rust-specific features:\n");
-  printf("    %-*s - %s.\n", MaxFeatLen, "crt-static", "Enables libraries with C Run-time Libraries(CRT) to be statically linked");
+  printf("    %-*s - %s.\n", 
+    MaxFeatLen, 
+    "crt-static", 
+    "Enables libraries with C Run-time Libraries(CRT) to be statically linked"
+  );
   printf("\n");
 
   printf("Use +feature to enable a feature, or -feature to disable it.\n"
