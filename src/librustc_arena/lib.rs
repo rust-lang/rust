1b92d592b5c7f8a3d399e5b9005f4b316b316ef1
Matthew Jasper <mjjasper1@gmail.com>
2020-06-16 19:29:53 +0100

    Explain unused macro param

--- src/librustc_arena/lib.rs
+++ src/librustc_arena/lib.rs
@@ -602,6 +602,10 @@ macro_rules! which_arena_for_type {
 
 #[macro_export]
 macro_rules! declare_arena {
+    // This macro has to take the same input as
+    // `impl_arena_allocatable_decoders` which requires a second version of
+    // each type. We ignore that type until we can fix
+    // `impl_arena_allocatable_decoders`.
     ([], [$($a:tt $name:ident: $ty:ty, $_gen_ty:ty;)*], $tcx:lifetime) => {
         #[derive(Default)]
         pub struct Arena<$tcx> {
