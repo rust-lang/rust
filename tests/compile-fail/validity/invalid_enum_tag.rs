2940da9d1f22982b237cf7c64d7e9e5829397f88
Ralf Jung <post@ralfj.de>
2020-06-20 11:48:42 +0200

    bump Rust

---
+++ tests/compile-fail/validity/invalid_enum_tag.rs
@@ -0,0 +1,8 @@
+#[repr(C)]
+pub enum Foo {
+    A, B, C, D
+}
+
+fn main() {
+    let _f = unsafe { std::mem::transmute::<i32, Foo>(42) }; //~ ERROR encountered 0x0000002a, but expected a valid enum tag
+}
