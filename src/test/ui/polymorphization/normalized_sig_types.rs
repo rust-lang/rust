f52c72948aa1dd718cc1f168d21c91c584c0a662
David Wood <david@davidtw.co>
2020-06-22 14:00:27 +0100

    ty: normalize fn sigs before subst
    This commit normalizes function signatures for instances before
    substituting, a workaround for polymorphization considering
    parameters unused when they show up in the signature, but not the
    body (due to being normalized).
    
    Unfortunately, this causes test output to change with the parallel
    compiler only.
    
    Signed-off-by: David Wood <david@davidtw.co>

---
+++ src/test/ui/polymorphization/normalized_sig_types.rs
@@ -0,0 +1,25 @@
+// build-pass
+
+pub trait ParallelIterator: Sized {
+    fn drive<C: Consumer<()>>(_: C) {
+        C::into_folder();
+    }
+}
+
+pub trait Consumer<T>: Sized {
+    type Result;
+    fn into_folder() -> Self::Result;
+}
+
+impl ParallelIterator for () {}
+
+impl<F: Fn(), T> Consumer<T> for F {
+    type Result = ();
+    fn into_folder() -> Self::Result {
+        unimplemented!()
+    }
+}
+
+fn main() {
+    <()>::drive(|| ());
+}
