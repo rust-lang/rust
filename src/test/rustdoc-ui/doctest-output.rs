608807934d41168cb30c6eee6442fe29251e40f0
Andy Russell <arussell123@gmail.com>
2020-06-28 13:06:33 -0400

    use outermost invocation span for doctest names
    Fixes #70090.

--- src/test/rustdoc-ui/doctest-output.rs
+++ src/test/rustdoc-ui/doctest-output.rs
@@ -1,3 +1,5 @@
+// edition:2018
+// aux-build:extern_macros.rs
 // compile-flags:--test --test-args=--test-threads=1
 // normalize-stdout-test: "src/test/rustdoc-ui" -> "$$DIR"
 // check-pass
@@ -6,10 +8,20 @@
 //! assert_eq!(1 + 1, 2);
 //! ```
 
+extern crate extern_macros as macros;
+
+use macros::attrs_on_struct;
+
 pub mod foo {
 
     /// ```
     /// assert_eq!(1 + 1, 2);
     /// ```
     pub fn bar() {}
+}
+
+attrs_on_struct! {
+    /// ```
+    /// assert!(true);
+    /// ```
 }
