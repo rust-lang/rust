a7c408d6ac74f4fef732d740e95e1d2504613bb6
Aaron Hill <aa1ronham@gmail.com>
2020-06-29 22:02:57 -0400

    Normalize symbol ids to 0 in test stdout
    The number of symbols we allocate (even early on) seems to be platform
    dependent. We only care about hygiene for the purposes of this test,
    so just set all of the symbol ids to zero

--- src/test/ui/proc-macro/meta-macro-hygiene.rs
+++ src/test/ui/proc-macro/meta-macro-hygiene.rs
@@ -3,7 +3,9 @@
 // edition:2018
 // compile-flags: -Z span-debug -Z unpretty=expanded,hygiene
 // check-pass
-
+// normalize-stdout-test "\d+#" -> "0#"
+// ^ We don't care about symbol ids, so set them all to 0
+// in the stdout
 extern crate meta_macro;
 
 fn main() {
