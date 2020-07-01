52bdaaa0edb2824af1610b67664f06580335fd78
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2020-07-01 14:12:49 +0300

    Add some requested tests

--- src/test/ui/tuple/index-float.rs
+++ src/test/ui/tuple/index-float.rs
@@ -4,6 +4,7 @@ fn main() {
     let tuple = (((),),);
 
     let _ = tuple. 0.0; // OK, whitespace
+    let _ = tuple.0. 0; // OK, whitespace
 
     let _ = tuple./*special cases*/0.0; // OK, comment
 }
