9209a992c9fa8a9421466f8119aa121fee2223e4
Laurențiu Nicola <lnicola@dend.ro>
2020-07-06 14:29:19 +0300

    Disable ES module interop

--- editors/code/tests/unit/index.ts
+++ editors/code/tests/unit/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
-import Mocha from 'mocha';
-import glob from 'glob';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
 
 export function run(): Promise<void> {
     // Create the mocha test
