08394eb12174ad14aeef739a876d1456187b4d66
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-07-07 10:46:24 +0200

    update test

--- src/test/ui/const-generics/type-dependent/issue-61936.rs
+++ src/test/ui/const-generics/type-dependent/issue-61936.rs
@@ -20,11 +20,13 @@ struct ArrayWindows<'a, T, const N: usize> {
 impl <'a, T: Clone, const N: usize> Iterator for ArrayWindows<'a, T, N> {
     type Item = [T; N];
     fn next(&mut self) -> Option<Self::Item> {
+        // Note: this is unsound for some `T` and not meant as an example
+        // on how to implement `ArrayWindows`.
         let mut res = unsafe{ std::mem::zeroed() };
         let mut ptr = &mut res as *mut [T; N] as *mut T;
 
         for i in 0..N {
-            match self.slice[i..].get(i) {
+            match self.slice[self.idx..].get(i) {
                 None => return None,
                 Some(elem) => unsafe { std::ptr::write_volatile(ptr, elem.clone())},
             };
@@ -39,7 +41,7 @@ impl <'a, T: Clone, const N: usize> Iterator for ArrayWindows<'a, T, N> {
 const FOUR: usize = 4;
 
 fn main() {
-    let v: Vec<usize> = vec![100; 0usize];
+    let v: Vec<usize> = vec![0; 100];
 
     for array in v.as_slice().array_windows::<FOUR>() {
         assert_eq!(array, [0, 0, 0, 0])
