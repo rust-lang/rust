600b8247a8008b8e4fb2f6b6f61207ba85aba363
Nicholas Nethercote <nnethercote@mozilla.com>
2020-07-10 10:14:55 +1000

    Rename `sym::item_context` as `sym::ItemContext`.
    Because it represents the symbol `ItemContext`, and `sym` identifiers
    are supposed to match the actual symbol whenever possible.

--- src/librustc_trait_selection/traits/error_reporting/on_unimplemented.rs
+++ src/librustc_trait_selection/traits/error_reporting/on_unimplemented.rs
@@ -126,7 +126,7 @@ impl<'a, 'tcx> InferCtxtExt<'tcx> for InferCtxt<'a, 'tcx> {
 
         let mut flags = vec![];
         flags.push((
-            sym::item_context,
+            sym::ItemContext,
             self.describe_enclosure(obligation.cause.body_id).map(|s| s.to_owned()),
         ));
 
