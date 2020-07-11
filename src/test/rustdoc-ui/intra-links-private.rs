ed53de029cf84651423007ec68305d6e772c819f
Dennis Hamester <dennis.hamester@gmail.com>
2020-07-11 19:39:02 +0200

    rustdoc: Always warn when linking from public to private items
    Change the logic such that linking from a public to a private item always
    triggers intra_doc_link_resolution_failure. Previously, the warning was
    not emitted when --document-private-items is passed.
    
    Also don't rely anymore on the item's visibility, which would falsely trigger
    the lint now that the check for --document-private-items is gone.

--- src/test/rustdoc-ui/intra-links-private.rs
+++ src/test/rustdoc-ui/intra-links-private.rs
@@ -1,10 +1,9 @@
 // check-pass
 // revisions: public private
 // [private]compile-flags: --document-private-items
-#![cfg_attr(private, deny(intra_doc_link_resolution_failure))]
 
 /// docs [DontDocMe]
-//[public]~^ WARNING public documentation for `DocMe` links to private item `DontDocMe`
+//~^ WARNING public documentation for `DocMe` links to private item `DontDocMe`
 // FIXME: for [private] we should also make sure the link was actually generated
 pub struct DocMe;
 struct DontDocMe;
