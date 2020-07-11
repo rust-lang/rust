325b727e1bd35dc81d5cb5757c94e4acf67f961b
Caleb Zulawski <caleb.zulawski@gmail.com>
2020-07-11 10:29:35 -0400

    Emit warnings for misplaced attributes used by some crates

--- src/test/ui/macros/issue-68060.rs
+++ src/test/ui/macros/issue-68060.rs
@@ -4,10 +4,13 @@ fn main() {
             #[target_feature(enable = "")]
             //~^ ERROR: attribute should be applied to a function
             //~| ERROR: the feature named `` is not valid for this target
+            //~| NOTE: `` is not valid for this target
             #[track_caller]
             //~^ ERROR: attribute should be applied to function [E0739]
             //~| ERROR: `#[track_caller]` requires Rust ABI [E0737]
             |_| (),
+            //~^ NOTE: not a function
+            //~| NOTE: not a function
         )
         .next();
 }
