2257a48cd36c61f887b881edf44df7fd260a52fc
kjeremy <kjeremy@gmail.com>
2020-07-14 11:03:44 -0400

    Use more explicity type for save registration

--- crates/rust-analyzer/src/main_loop.rs
+++ crates/rust-analyzer/src/main_loop.rs
@@ -122,29 +122,33 @@ impl GlobalState {
             );
         };
 
-        let registration_options = lsp_types::TextDocumentRegistrationOptions {
-            document_selector: Some(vec![
-                lsp_types::DocumentFilter {
-                    language: None,
-                    scheme: None,
-                    pattern: Some("**/*.rs".into()),
-                },
-                lsp_types::DocumentFilter {
-                    language: None,
-                    scheme: None,
-                    pattern: Some("**/Cargo.toml".into()),
-                },
-                lsp_types::DocumentFilter {
-                    language: None,
-                    scheme: None,
-                    pattern: Some("**/Cargo.lock".into()),
-                },
-            ]),
+        let save_registration_options = lsp_types::TextDocumentSaveRegistrationOptions {
+            include_text: Some(false),
+            text_document_registration_options: lsp_types::TextDocumentRegistrationOptions {
+                document_selector: Some(vec![
+                    lsp_types::DocumentFilter {
+                        language: None,
+                        scheme: None,
+                        pattern: Some("**/*.rs".into()),
+                    },
+                    lsp_types::DocumentFilter {
+                        language: None,
+                        scheme: None,
+                        pattern: Some("**/Cargo.toml".into()),
+                    },
+                    lsp_types::DocumentFilter {
+                        language: None,
+                        scheme: None,
+                        pattern: Some("**/Cargo.lock".into()),
+                    },
+                ]),
+            },
         };
+
         let registration = lsp_types::Registration {
             id: "textDocument/didSave".to_string(),
             method: "textDocument/didSave".to_string(),
-            register_options: Some(serde_json::to_value(registration_options).unwrap()),
+            register_options: Some(serde_json::to_value(save_registration_options).unwrap()),
         };
         self.send_request::<lsp_types::request::RegisterCapability>(
             lsp_types::RegistrationParams { registrations: vec![registration] },
