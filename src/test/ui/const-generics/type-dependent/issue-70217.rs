137ca05ccd2ef6f0bb5583f943f4395272a170c9
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-07-16 12:03:00 +0200

    add regression test for #70217

---
+++ src/test/ui/const-generics/type-dependent/issue-70217.rs
@@ -0,0 +1,16 @@
+// check-pass
+#![feature(const_generics)]
+#![allow(incomplete_features)]
+
+struct Struct<const N: usize>;
+
+impl<const N: usize> Struct<N> {
+    fn method<const M: usize>(&self) {}
+}
+
+fn test<const N: usize, const M: usize>(x: Struct<N>) {
+    Struct::<N>::method::<M>(&x);
+    x.method::<N>();
+}
+
+fn main() {}
