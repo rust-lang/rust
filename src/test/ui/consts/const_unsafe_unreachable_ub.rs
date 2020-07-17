c45e9c86ca5e1aa20ec8ec9904c4ad9a33a072e5
Nazım Can Altınova <canaltinova@gmail.com>
2020-07-17 22:03:33 +0200

    Add a test for const unsafe_unreachable that triggers UB

---
+++ src/test/ui/consts/const_unsafe_unreachable_ub.rs
@@ -0,0 +1,15 @@
+#![feature(const_fn)]
+#![feature(const_unreachable_unchecked)]
+
+const unsafe fn foo(x: bool) -> bool {
+    match x {
+        true => true,
+        false => std::hint::unreachable_unchecked(),
+    }
+}
+
+const BAR: bool = unsafe { foo(false) };
+
+fn main() {
+  assert_eq!(BAR, true);
+}
