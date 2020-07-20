05bb6e6bdb1894de5803f729339a631a9222499f
JarredAllen <jarredallen73@gmail.com>
2020-07-20 08:58:55 -0700

    Create test for wanted behavior

--- tests/ui/needless_collect.rs
+++ tests/ui/needless_collect.rs
@@ -8,6 +8,9 @@ use std::collections::{BTreeSet, HashMap, HashSet};
 #[allow(unused_variables, clippy::iter_cloned_collect)]
 fn main() {
     let sample = [1; 5];
+    let indirect_with_into_iter = sample.iter().collect::<Vec<_>>();
+    let indirect_with_iter = sample.iter().collect::<Vec<_>>();;
+    let indirect_negative = sample.iter().collect::<Vec<_>>();;
     let len = sample.iter().collect::<Vec<_>>().len();
     if sample.iter().collect::<Vec<_>>().is_empty() {
         // Empty
@@ -18,4 +21,8 @@ fn main() {
     sample.iter().collect::<HashSet<_>>().len();
     // Neither should this
     sample.iter().collect::<BTreeSet<_>>().len();
+    indirect_with_into_iter.into_iter().map(|x| (x, x+1)).collect::<HashMap<_, _>>();
+    indirect_with_iter.iter().map(|x| (x, x+1)).collect::<HashMap<_, _>>();
+    indirect_negative.iter().map(|x| (x, x+1)).collect::<HashMap<_, _>>();
+    indirect_negative.iter().map(|x| (x, x+1)).collect::<HashMap<_, _>>();
 }
