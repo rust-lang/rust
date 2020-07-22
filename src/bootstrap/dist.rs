b747a33abdf4c9d21b604853bc6dee105e622209
Mark Rousskov <mark.simulacrum@gmail.com>
2020-07-21 21:41:49 -0400

    Revert "include backtrace folder in rust-src component"
    This reverts commit d7a36d8964c927863faef5d3b42da08f37e5896c.

--- src/bootstrap/dist.rs
+++ src/bootstrap/dist.rs
@@ -1005,7 +1005,6 @@ impl Step for Src {
         // (essentially libstd and all of its path dependencies)
         let std_src_dirs = [
             "src/build_helper",
-            "src/backtrace/src",
             "src/liballoc",
             "src/libcore",
             "src/libpanic_abort",
