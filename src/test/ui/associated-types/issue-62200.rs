dc7d1156bef37b2fe38c3b5c83919dd8f0e39414
Steven Malis <smmalis37@gmail.com>
2020-07-22 22:54:06 -0700

    Don't ICE on unconstrained anonymous lifetimes inside associated types.

---
+++ src/test/ui/associated-types/issue-62200.rs
@@ -0,0 +1,14 @@
+struct S {}
+
+trait T<'a> {
+    type A;
+}
+
+impl T<'_> for S {
+    type A = u32;
+}
+
+fn foo(x: impl Fn(<S as T<'_>>::A) -> <S as T<'_>>::A) {}
+//~^ ERROR binding for associated type `Output` references an anonymous lifetime
+
+fn main() {}
