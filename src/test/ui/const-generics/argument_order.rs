5f1eea93a9340d5f2243c5467b4234bd0c14e7a5
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-07-23 14:30:01 +0200

    test usage

--- src/test/ui/const-generics/argument_order.rs
+++ src/test/ui/const-generics/argument_order.rs
@@ -13,4 +13,7 @@ struct AlsoBad<const N: usize, 'a, T, 'b, const M: usize, U> {
     b: &'b U,
 }
 
-fn main() { }
+fn main() {
+    let _: AlsoBad<7, 'static, u32, 'static, 17, u16>;
+    //~^ ERROR lifetime provided when a type was expected
+ }
