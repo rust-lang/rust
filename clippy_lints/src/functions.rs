e57aafe33f338208e612ecb816a948d28f2c3741
Matthias Krüger <matthias.krueger@famsik.de>
2020-07-23 15:06:33 +0200

    too-many-lines: make lint adhere to lint message convention

--- clippy_lints/src/functions.rs
+++ clippy_lints/src/functions.rs
@@ -374,7 +374,7 @@ impl<'tcx> Functions {
         }
 
         if line_count > self.max_lines {
-            span_lint(cx, TOO_MANY_LINES, span, "This function has a large number of lines.")
+            span_lint(cx, TOO_MANY_LINES, span, "this function has a large number of lines")
         }
     }
 
