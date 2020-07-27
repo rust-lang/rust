952fd0ce5867ea65c8d760ae66e900d49669a719
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-07-27 16:41:27 +0200

    update tests

--- src/test/ui/const-generics/params-in-ct-in-ty-param-lazy-norm.rs
+++ src/test/ui/const-generics/params-in-ct-in-ty-param-lazy-norm.rs
@@ -6,5 +6,6 @@ struct Foo<T, U = [u8; std::mem::size_of::<T>()]>(T, U);
 // FIXME(const_generics:defaults): We still don't know how to we deal with type defaults.
 struct Bar<T = [u8; N], const N: usize>(T);
 //~^ ERROR constant values inside of type parameter defaults
+//~| ERROR type parameters with a default
 
 fn main() {}
