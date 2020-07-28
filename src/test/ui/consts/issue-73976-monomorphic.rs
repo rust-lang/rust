a99d2cbfe7072218c67fbfe1fda2ea55f46c46f2
Ashley Mannix <ashleymannix@live.com.au>
2020-07-28 13:33:08 +1000

    remove unstable const_type_id feature

--- src/test/ui/consts/issue-73976-monomorphic.rs
+++ src/test/ui/consts/issue-73976-monomorphic.rs
@@ -5,7 +5,6 @@
 // will be properly rejected. This test will ensure that monomorphic use of these
 // would not be wrongly rejected in patterns.
 
-#![feature(const_type_id)]
 #![feature(const_type_name)]
 
 use std::any::{self, TypeId};
