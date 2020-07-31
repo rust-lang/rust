c5114549d74f6092517af6ea630ec5a26317ae93
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2020-07-31 18:04:13 +0800

    Add the proper tests

--- src/test/ui/statics/static-promotion.rs
+++ src/test/ui/statics/static-promotion.rs
@@ -12,12 +12,23 @@ struct A<T: 'static>(&'static T);
 struct B<T: 'static + ?Sized> {
     x: &'static T,
 }
+static STR: &'static [u8] = b"hi";
 static C: A<B<B<[u8]>>> = {
     A(&B {
-        x: &B { x: b"hi" as &[u8] },
+        x: &B { x: STR },
     })
 };
 
+pub struct Slice(&'static [i32]);
+
+static CONTENT: i32 = 42;
+pub static CONTENT_MAP: Slice = Slice(&[CONTENT]);
+
+pub static FOO: (i32, i32) = (42, 43);
+pub static CONTENT_MAP2: Slice = Slice(&[FOO.0]);
+
 fn main() {
     assert_eq!(b"hi", C.0.x.x);
+    assert_eq!(&[42], CONTENT_MAP.0);
+    assert_eq!(&[42], CONTENT_MAP2.0);
 }
