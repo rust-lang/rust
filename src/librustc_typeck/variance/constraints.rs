96b5dee9ab3b4b0c8f5086011b7a15b7c4aed48c
kadmin <julianknodt@gmail.com>
2020-07-31 21:33:55 +0000

    Added in explicit check for the type being matched

--- src/librustc_typeck/variance/constraints.rs
+++ src/librustc_typeck/variance/constraints.rs
@@ -161,7 +161,13 @@ impl<'a, 'tcx> ConstraintContext<'a, 'tcx> {
                 self.add_constraints_from_sig(current_item, tcx.fn_sig(def_id), self.covariant);
             }
 
-            _ => {}
+            ty::Error(_) => {}
+            _ => {
+                span_bug!(
+                    tcx.def_span(def_id),
+                    "`build_constraints_for_item` unsupported for this item"
+                );
+            }
         }
     }
 
