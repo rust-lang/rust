cc36c3d140b82757c5ffe94c71232d2f32fcf2a4
Yuki Okushi <huyuumi.dev@gmail.com>
2020-08-06 05:21:03 +0900

    Add a FIXME comment

--- src/test/ui/higher-rank-trait-bounds/issue-59311.rs
+++ src/test/ui/higher-rank-trait-bounds/issue-59311.rs
@@ -2,6 +2,10 @@
 // rust-lang/rust/issues/71546#issuecomment-620638437
 // as they seem to have the same cause.
 
+// FIXME: It's not clear that this code ought to report
+// an error, but the regression test is here to ensure
+// that it does not ICE. See discussion on #74889 for details.
+
 pub trait T {
     fn t<F: Fn()>(&self, _: F) {}
 }
