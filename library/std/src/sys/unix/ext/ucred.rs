be2637aba7e12474a7044b5ed9ba4a6978d46462
Joe Ellis <joe.ellis@arm.com>
2020-08-05 12:19:05 +0100

    Add basic test for Unix peer credentials

--- library/std/src/sys/unix/ext/ucred.rs
+++ library/std/src/sys/unix/ext/ucred.rs
@@ -87,3 +87,23 @@ pub mod impl_bsd {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::os::unix::net::UnixStream;
+    use libc::{getegid, geteuid};
+
+    #[test]
+    fn test_socket_pair() {
+        // Create two connected sockets and get their peer credentials. They should be equal.
+        let (sock_a, sock_b) = UnixStream::pair().unwrap();
+        let (cred_a, cred_b) = (sock_a.peer_cred().unwrap(), sock_b.peer_cred().unwrap());
+        assert_eq!(cred_a, cred_b);
+
+        // Check that the UID and GIDs match up.
+        let uid = unsafe { geteuid() };
+        let gid = unsafe { getegid() };
+        assert_eq!(cred_a.uid, uid);
+        assert_eq!(cred_a.gid, gid);
+    }
+}
