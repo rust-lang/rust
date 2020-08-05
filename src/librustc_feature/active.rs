375bccb8b3fa2a517e6e19e24da25cad0413987d
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-08-05 18:27:54 +0200

    add min_const_generics feature gate

--- src/librustc_feature/active.rs
+++ src/librustc_feature/active.rs
@@ -579,6 +579,9 @@ declare_features! (
     /// Alloc calling `transmute` in const fn
     (active, const_fn_transmute, "1.46.0", Some(53605), None),
 
+    /// The smallest useful subset of `const_generics`.
+    (active, min_const_generics, "1.46.0", None, None),
+
     // -------------------------------------------------------------------------
     // feature-group-end: actual feature gates
     // -------------------------------------------------------------------------
