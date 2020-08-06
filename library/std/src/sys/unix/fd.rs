888bc07c6bee3ed96b6f94f83e13a66828858ad4
Tomasz Miąsko <tomasz.miasko@gmail.com>
2020-08-06 00:00:00 +0000

    Keep stdout open in limit_vector_count test

--- library/std/src/sys/unix/fd.rs
+++ library/std/src/sys/unix/fd.rs
@@ -283,12 +283,12 @@ impl Drop for FileDesc {
 #[cfg(test)]
 mod tests {
     use super::{FileDesc, IoSlice};
+    use core::mem::ManuallyDrop;
 
     #[test]
     fn limit_vector_count() {
-        let stdout = FileDesc { fd: 1 };
+        let stdout = ManuallyDrop::new(FileDesc { fd: 1 });
         let bufs = (0..1500).map(|_| IoSlice::new(&[])).collect::<Vec<_>>();
-
         assert!(stdout.write_vectored(&bufs).is_ok());
     }
 }
