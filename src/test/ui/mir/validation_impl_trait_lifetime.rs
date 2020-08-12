ccbcd2d81cfc16ab814f6ea24088a9810a92e2e1
Ralf Jung <post@ralfj.de>
2020-08-12 08:48:06 +0200

    please tidy

--- src/test/ui/mir/validation_impl_trait_lifetime.rs
+++ src/test/ui/mir/validation_impl_trait_lifetime.rs
@@ -1,5 +1,6 @@
 // build-pass
-//! This used to fail MIR validation due to the types on both sides of an assignment not being equal.
+//! This used to fail MIR validation due to the types on both sides of
+//! an assignment not being equal.
 //! The failure doesn't occur with a check-only build.
 fn iter_slice<'a, T>(xs: &'a [T]) -> impl Iterator<Item = &'a T> {
     xs.iter()
