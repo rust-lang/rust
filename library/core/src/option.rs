b02c4049cb629437b4363b76816542f0b5fba337
Camelid <37223377+camelid@users.noreply.github.com>
2020-08-15 09:31:14 -0700

    Fix link
    Intra-doc links don't work yet in this case: #74489

--- library/core/src/option.rs
+++ library/core/src/option.rs
@@ -1501,6 +1501,8 @@ unsafe impl<A> TrustedLen for IterMut<'_, A> {}
 /// The iterator yields one value if the [`Option`] is a [`Some`], otherwise none.
 ///
 /// This `struct` is created by the [`Option::into_iter`] function.
+///
+/// [`Option::into_iter`]: enum.Option.html#method.into_iter
 #[derive(Clone, Debug)]
 #[stable(feature = "rust1", since = "1.0.0")]
 pub struct IntoIter<A> {
