1fe2e2941d93da397c8ace5f410c0ab3dc5a3049
Tomasz Miąsko <tomasz.miasko@gmail.com>
2020-08-15 00:00:00 +0000

    MatchBranchSimplification: fix equal const bool assignments
    The match branch simplification is applied when target blocks contain
    statements that are either equal or perform a const bool assignment with
    different values to the same place.
    
    Previously, when constructing new statements, only statements from a
    single block had been examined. This lead to a misoptimization when
    statements are equal because the assign the *same* const bool value to
    the same place.
    
    Fix the issue by examining statements from both blocks when deciding on
    replacement.

--- src/test/mir-opt/matches_reduce_branches.rs
+++ src/test/mir-opt/matches_reduce_branches.rs
@@ -1,5 +1,6 @@
 // EMIT_MIR_FOR_EACH_BIT_WIDTH
 // EMIT_MIR matches_reduce_branches.foo.MatchBranchSimplification.diff
+// EMIT_MIR matches_reduce_branches.bar.MatchBranchSimplification.diff
 
 fn foo(bar: Option<()>) {
     if matches!(bar, None) {
@@ -7,7 +8,35 @@ fn foo(bar: Option<()>) {
     }
 }
 
+fn bar(i: i32) -> (bool, bool, bool, bool) {
+    let a;
+    let b;
+    let c;
+    let d;
+
+    match i {
+        7 => {
+            a = false;
+            b = true;
+            c = false;
+            d = true;
+            ()
+        }
+        _ => {
+            a = true;
+            b = false;
+            c = false;
+            d = true;
+            ()
+        }
+    };
+
+    (a, b, c, d)
+}
+
+
 fn main() {
   let _ = foo(None);
   let _ = foo(Some(()));
+  let _ = bar(0);
 }
