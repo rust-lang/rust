a90aa762563a3220c7615a2d16c0c26e8a852fe6
Aaron Hill <aa1ronham@gmail.com>
2020-08-17 19:31:37 -0400

    [DO NOT MERGE] Enable parallel compiler by default
    It's been over a year since we last tried this - let's see what the
    performance looks like now.

--- src/bootstrap/config.rs
+++ src/bootstrap/config.rs
@@ -657,7 +657,7 @@ impl Config {
             set(&mut config.use_lld, rust.use_lld);
             set(&mut config.lld_enabled, rust.lld);
             set(&mut config.llvm_tools_enabled, rust.llvm_tools);
-            config.rustc_parallel = rust.parallel_compiler.unwrap_or(false);
+            config.rustc_parallel = rust.parallel_compiler.unwrap_or(true);
             config.rustc_default_linker = rust.default_linker.clone();
             config.musl_root = rust.musl_root.clone().map(PathBuf::from);
             config.save_toolstates = rust.save_toolstates.clone().map(PathBuf::from);
