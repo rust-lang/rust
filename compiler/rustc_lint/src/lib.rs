37463d46993a5e40c97752b81a362988e3152bb1
Nathan Whitaker <nathan.whitaker01@gmail.com>
2020-08-18 11:25:21 -0400

    Uplift temporary-cstring-as-ptr into rustc

--- compiler/rustc_lint/src/lib.rs
+++ compiler/rustc_lint/src/lib.rs
@@ -49,6 +49,7 @@ mod early;
 mod internal;
 mod late;
 mod levels;
+mod methods;
 mod non_ascii_idents;
 mod nonstandard_style;
 mod passes;
@@ -72,6 +73,7 @@ use rustc_span::Span;
 use array_into_iter::ArrayIntoIter;
 use builtin::*;
 use internal::*;
+use methods::*;
 use non_ascii_idents::*;
 use nonstandard_style::*;
 use redundant_semicolon::*;
@@ -157,6 +159,7 @@ macro_rules! late_lint_passes {
                 MissingDebugImplementations: MissingDebugImplementations::default(),
                 ArrayIntoIter: ArrayIntoIter,
                 ClashingExternDeclarations: ClashingExternDeclarations::new(),
+                TemporaryCStringAsPtr: TemporaryCStringAsPtr,
             ]
         );
     };
