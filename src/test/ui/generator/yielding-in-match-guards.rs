df127b86cb90470d8ceebdd880db8ad4c372f91b
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2020-08-19 09:04:40 +0800

    switch the test to an actual MCVE

--- src/test/ui/generator/yielding-in-match-guards.rs
+++ src/test/ui/generator/yielding-in-match-guards.rs
@@ -2,28 +2,23 @@
 // edition:2018
 
 // This test is derived from
-// https://github.com/rust-lang/rust/issues/74961#issuecomment-666893845
-// by @SNCPlay42
+// https://github.com/rust-lang/rust/issues/72651#issuecomment-668720468
 
 // This test demonstrates that, in `async fn g()`,
 // indeed a temporary borrow `y` from `x` is live
 // while `f().await` is being evaluated.
-// Thus, `&'_ A` should be included in type signature
+// Thus, `&'_ u8` should be included in type signature
 // of the underlying generator.
 
-#[derive(PartialEq, Eq)]
-struct A;
+async fn f() -> u8 { 1 }
 
-async fn f() -> A {
-    A
-}
-
-async fn g() {
-    let x = A;
+pub async fn g(x: u8) {
     match x {
-        y if f().await == y => {}
-        _ => {}
+        y if f().await == y => (),
+        _ => (),
     }
 }
 
-fn main() {}
+fn main() {
+    let _ = g(10);
+}
