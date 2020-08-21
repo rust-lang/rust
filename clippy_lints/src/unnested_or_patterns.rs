146e352db4eade947f6f358cd38c308e95783d7b
Eduardo Broto <ebroto@tutanota.com>
2020-08-22 00:59:42 +0200

    run cargo dev fmt

--- clippy_lints/src/unnested_or_patterns.rs
+++ clippy_lints/src/unnested_or_patterns.rs
@@ -340,7 +340,7 @@ fn take_pat(from: &mut Pat) -> Pat {
         id: DUMMY_NODE_ID,
         kind: Wild,
         span: DUMMY_SP,
-        tokens: None
+        tokens: None,
     };
     mem::replace(from, dummy)
 }
