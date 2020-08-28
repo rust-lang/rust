4edf33e8ad4604218899e3900901ecb9816de689
Esteban Küber <esteban@kuber.com.ar>
2020-08-28 14:01:46 -0700

    Add tests for turbofish suggestions

---
+++ src/test/ui/suggestions/appropriate-type-param-turbofish.rs
@@ -0,0 +1,45 @@
+mod a {
+    fn foo() {
+        vec![1, 2, 3].into_iter().collect(); //~ ERROR type annotations needed
+    }
+    fn bar() {
+        vec!["a", "b", "c"].into_iter().collect(); //~ ERROR type annotations needed
+    }
+    fn qux() {
+        vec!['a', 'b', 'c'].into_iter().collect(); //~ ERROR type annotations needed
+    }
+}
+mod b {
+    fn foo() {
+        let _ = vec![1, 2, 3].into_iter().collect(); //~ ERROR type annotations needed
+    }
+    fn bar() {
+        let _ = vec!["a", "b", "c"].into_iter().collect(); //~ ERROR type annotations needed
+    }
+    fn qux() {
+        let _ = vec!['a', 'b', 'c'].into_iter().collect(); //~ ERROR type annotations needed
+    }
+}
+
+trait T: Sized {
+    fn new() -> Self;
+}
+fn x<X: T>() -> X {
+    T::new()
+}
+struct S;
+impl T for S {
+    fn new() -> Self {
+        S
+    }
+}
+
+fn foo() {
+    x(); //~ ERROR type annotations needed
+}
+
+fn bar() {
+    let _ = x(); //~ ERROR type annotations needed
+}
+
+fn main() {}
