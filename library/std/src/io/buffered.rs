c91e764d51732399ee64fba2547e49609b90d715
Nathan West <Lucretiel@gmail.com>
2020-08-27 22:55:58 -0400

    Once again, x.py tidy

--- library/std/src/io/buffered.rs
+++ library/std/src/io/buffered.rs
@@ -1408,11 +1408,7 @@ mod tests {
     // rustfmt-on-save.
     impl Read for ShortReader {
         fn read(&mut self, _: &mut [u8]) -> io::Result<usize> {
-            if self.lengths.is_empty() {
-                Ok(0)
-            } else {
-                Ok(self.lengths.remove(0))
-            }
+            if self.lengths.is_empty() { Ok(0) } else { Ok(self.lengths.remove(0)) }
         }
     }
 
