a9919e0faeaae81a5befbc819d497b3d89af8de4
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-08-30 08:54:18 -0400

    Changed the implementation of IntoIter::last()
    Simply does what next() does except it goes straight to the end of the
    array.
    
    Woopsie

--- library/alloc/src/vec.rs
+++ library/alloc/src/vec.rs
@@ -2956,11 +2956,23 @@ impl<T> Iterator for IntoIter<T> {
             if mem::size_of::<T>() == 0 { mem::zeroed() } else { ptr::read(self.ptr.add(i)) }
         }
     #[inline]
-    fn last(self) -> Option<Self::Item>
-    where
-        Self: Sized,
-    {
-        if self.ptr == self.end { None } else { unsafe { Some(ptr::read(self.end.offset(-1))) } }
+    fn last(mut self) -> Option<T> {
+        unsafe {
+            if self.ptr as *const _ == self.end {
+                None
+            } else {
+                if mem::size_of::<T>() == 0 {
+                    // Immediately marches to end of the iterator.
+                    self.ptr = self.end;
+                    // Make up a value of this ZST.
+                    Some(mem::zeroed())
+                } else {
+                    // Immediately marches to end of the iterator.
+                    self.ptr = self.end;
+                    Some(ptr::read(self.ptr.offset(-1)))
+                }
+            }
+        }
     }
 }
 
