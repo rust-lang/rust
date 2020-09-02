b71e627b26a5ef932c9e1c10813a500a70211810
Tyson Nottingham <tgnottingham@gmail.com>
2020-09-01 21:50:07 -0700

    incr-comp: hash span end line/column
    Hash both the length and the end location (line/column) of a span. If we
    hash only the length, for example, then two otherwise equal spans with
    different end locations will have the same hash. This can cause a
    problem during incremental compilation wherein a previous result for a
    query that depends on the end location of a span will be incorrectly
    reused when the end location of the span it depends on has changed. A
    similar analysis applies if some query depends specifically on the
    length of the span, but we only hash the end location. So hash both.
    
    Fix #46744, fix #59954, fix #63161, fix #73640, fix #73967, fix #74890, fix #75900

---
+++ src/test/run-make/incr-prev-body-beyond-eof/a.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // foo must be used.
+    foo();
+}
+
+fn foo() {
+    // foo's span in a.rs and b.rs must be identical
+    // with respect to start line/column and length.
+    assert_eq!(1, 1);
+
+
+
+
+}
