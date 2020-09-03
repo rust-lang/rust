4f859fbcfc9080687f4a52a96e0ec98290e02f19
Christiaan Dirkx <christiaan@dirkx.email>
2020-09-04 00:13:25 +0200

    Move const tests for `Option` to `library\core`
    Part of #76268

--- library/core/tests/option.rs
+++ library/core/tests/option.rs
@@ -356,3 +356,19 @@ fn test_replace() {
     assert_eq!(x, Some(3));
     assert_eq!(old, None);
 }
+
+#[test]
+fn option_const() {
+    // test that the methods of `Option` are usable in a const context
+
+    const OPTION: Option<usize> = Some(32);
+
+    const REF: Option<&usize> = OPTION.as_ref();
+    assert_eq!(REF, Some(&32));
+
+    const IS_SOME: bool = OPTION.is_some();
+    assert!(IS_SOME);
+
+    const IS_NONE: bool = OPTION.is_none();
+    assert!(!IS_NONE);
+}
