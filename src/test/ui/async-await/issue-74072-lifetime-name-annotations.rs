61b52a33b3a5a170f7760ba3e3efaac25e5085ef
SNCPlay42 <SNCPlay42@gmail.com>
2020-09-08 05:35:24 +0100

    use RegionNameHighlight for async fn and closure returns

--- src/test/ui/async-await/issue-74072-lifetime-name-annotations.rs
+++ src/test/ui/async-await/issue-74072-lifetime-name-annotations.rs
@@ -18,6 +18,14 @@ pub fn async_closure(x: &mut i32) -> impl Future<Output=&i32> {
     })()
 }
 
+pub fn async_closure_explicit_return_type(x: &mut i32) -> impl Future<Output=&i32> {
+    (async move || -> &i32 {
+        let y = &*x;
+        *x += 1; //~ ERROR cannot assign to `*x` because it is borrowed
+        y
+    })()
+}
+
 pub fn async_block(x: &mut i32) -> impl Future<Output=&i32> {
     async move {
         let y = &*x;
