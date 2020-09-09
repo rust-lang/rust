74e07198a08fea62ab2fba9ae258f4d118d7dffe
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-09-09 20:10:23 +0200

    fix test on 32 bit systems

--- src/test/ui/const_evaluatable/function-call.rs
+++ src/test/ui/const_evaluatable/function-call.rs
@@ -1,8 +1,9 @@
 // check-pass
-#![warn(const_evaluatable_unchecked)]
 
 const fn foo<T>() -> usize {
-    if std::mem::size_of::<*mut T>() < 8 { // size of *mut T does not depend on T
+    // We might instead branch on `std::mem::size_of::<*mut T>() < 8` here,
+    // which would cause this function to fail on 32 bit systems.
+    if false {
         std::mem::size_of::<T>()
     } else {
         8
