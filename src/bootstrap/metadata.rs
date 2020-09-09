2ffef99fefe349c41cecc1216263b2bfde9e739d
bjorn3 <bjorn3@users.noreply.github.com>
2020-09-09 20:50:34 +0200

    Use a separate workspace for the sysroot

--- src/bootstrap/metadata.rs
+++ src/bootstrap/metadata.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 use std::process::Command;
 
-use build_helper::output;
 use serde::Deserialize;
 
 use crate::cache::INTERNER;
@@ -37,7 +36,33 @@ pub fn build(build: &mut Build) {
         .arg("--no-deps")
         .arg("--manifest-path")
         .arg(build.src.join("Cargo.toml"));
-    let output = output(&mut cargo);
+    let output = build_helper::output(&mut cargo);
+    let output: Output = serde_json::from_str(&output).unwrap();
+    for package in output.packages {
+        if package.source.is_none() {
+            let name = INTERNER.intern_string(package.name);
+            let mut path = PathBuf::from(package.manifest_path);
+            path.pop();
+            let deps = package
+                .dependencies
+                .into_iter()
+                .filter(|dep| dep.source.is_none())
+                .map(|dep| INTERNER.intern_string(dep.name))
+                .collect();
+            build.crates.insert(name, Crate { name, id: package.id, deps, path });
+        }
+    }
+
+    // Run `cargo metadata` to figure out what crates we're testing.
+    let mut cargo = Command::new(&build.initial_cargo);
+    cargo
+        .arg("metadata")
+        .arg("--format-version")
+        .arg("1")
+        .arg("--no-deps")
+        .arg("--manifest-path")
+        .arg(build.src.join("library/Cargo.toml"));
+    let output = build_helper::output(&mut cargo);
     let output: Output = serde_json::from_str(&output).unwrap();
     for package in output.packages {
         if package.source.is_none() {
