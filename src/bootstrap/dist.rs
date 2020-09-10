3bef430128b896bc7b45abd6f13bfb0b8819e4db
bjorn3 <bjorn3@users.noreply.github.com>
2020-09-10 09:28:37 +0200

    Fix vendoring

--- src/bootstrap/dist.rs
+++ src/bootstrap/dist.rs
@@ -1118,6 +1118,8 @@ impl Step for PlainSourceTarball {
             let mut cmd = Command::new(&builder.initial_cargo);
             cmd.arg("vendor")
                 .arg("--sync")
+                .arg(builder.src.join("./library/Cargo.yoml"))
+                .arg("--sync")
                 .arg(builder.src.join("./src/tools/rust-analyzer/Cargo.toml"))
                 .current_dir(&plain_dst_src);
             builder.run(&mut cmd);
