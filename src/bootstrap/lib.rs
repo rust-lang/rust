0be66d7f30ba31638e14c00bd2303dd0327964f4
Gus Wynn <guswynn@gmail.com>
2020-09-11 09:37:51 -0700

    just max_level_info

--- src/bootstrap/lib.rs
+++ src/bootstrap/lib.rs
@@ -548,7 +548,7 @@ impl Build {
         // if its unset, if debug_assertions is on, then debug_logging will also be on
         // as well as tracing *ignoring* this feature when debug_assertions is on
         if !self.config.rust_debug_logging {
-            features.push_str(" release_max_level_info");
+            features.push_str(" max_level_info");
         }
 
         features
