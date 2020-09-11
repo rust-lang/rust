63f3dcefb950d9d9f63b8a71de39d990a742b675
Tomasz Miąsko <tomasz.miasko@gmail.com>
2020-09-11 00:00:00 +0000

    copy-prop: Add test case for missed optimization due const prop failure
    Currently a failure to completely propagate a constant inhibits propagation of
    any subsequent locals. Add a test case demonstrating the problem before
    resolving it.

--- src/test/mir-opt/copy_propagation_arg.rs
+++ src/test/mir-opt/copy_propagation_arg.rs
@@ -30,10 +30,25 @@ fn arg_src(mut x: i32) -> i32 {
     y
 }
 
+// EMIT_MIR copy_propagation_arg.maybe.CopyPropagation.diff
+fn maybe(m: &[bool], s1: String) {
+    if m[0] {
+        let s2 = s1;
+        if m[1] {
+            let s3 = s2;
+            if m[2] {
+                let s4 = s3;
+                &s4;
+            }
+        }
+    }
+}
+
 fn main() {
     // Make sure the function actually gets instantiated.
     foo(0);
     bar(0);
     baz(0);
     arg_src(0);
+    maybe(&[true, false, true], "Hello!".to_owned());
 }
