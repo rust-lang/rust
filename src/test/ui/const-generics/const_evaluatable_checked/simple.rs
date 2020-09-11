9abc6bd28df390279b9f477538a778edf38ffd80
Hameer Abbasi <einstein.edison@gmail.com>
2020-09-11 11:42:32 +0200

    Add revisions to const generic const_evaluatable_checked tests.

--- src/test/ui/const-generics/const_evaluatable_checked/simple.rs
+++ src/test/ui/const-generics/const_evaluatable_checked/simple.rs
@@ -1,8 +1,12 @@
-// run-pass
-#![feature(const_generics, const_evaluatable_checked)]
+// [full] run-pass
+// revisions: full min
+#![cfg_attr(full, feature(const_generics))]
+#![cfg_attr(min, feature(min_const_generics))]
+#![feature(const_evaluatable_checked)]
 #![allow(incomplete_features)]
 
 type Arr<const N: usize> = [u8; N - 1];
+//[min]~^ ERROR generic parameters must not be used inside of non trivial constant values
 
 fn test<const N: usize>() -> Arr<N> where Arr<N>: Default {
     Default::default()
