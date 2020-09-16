d0ddbb9d0d0d6b9b6adb51b259819ec270421642
Robin Schoonover <robin@cornhooves.org>
2020-09-15 21:10:50 -0600

    Extend testing of rc_buffer lint

--- tests/ui/rc_buffer.rs
+++ tests/ui/rc_buffer.rs
@@ -1,13 +1,26 @@
+#![warn(clippy::rc_buffer)]
+
+use std::cell::RefCell;
 use std::ffi::OsString;
 use std::path::PathBuf;
 use std::rc::Rc;
 
-#[warn(clippy::rc_buffer)]
 struct S {
-    a: Rc<String>,
-    b: Rc<PathBuf>,
-    c: Rc<Vec<u8>>,
-    d: Rc<OsString>,
+    // triggers lint
+    bad1: Rc<String>,
+    bad2: Rc<PathBuf>,
+    bad3: Rc<Vec<u8>>,
+    bad4: Rc<OsString>,
+    // does not trigger lint
+    good1: Rc<RefCell<String>>,
 }
+
+// triggers lint
+fn func_bad1(_: Rc<String>) {}
+fn func_bad2(_: Rc<PathBuf>) {}
+fn func_bad3(_: Rc<Vec<u8>>) {}
+fn func_bad4(_: Rc<OsString>) {}
+// does not trigger lint
+fn func_good1(_: Rc<RefCell<String>>) {}
 
 fn main() {}
