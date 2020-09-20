4b5e78052a030783a609ea0d9c5ad401261fc44d
Ralf Jung <post@ralfj.de>
2020-09-20 15:55:36 +0200

    rustup; support panic=abort on Windows

--- tests/compile-fail/panic/panic_abort4.rs
+++ tests/compile-fail/panic/panic_abort4.rs
@@ -1,6 +1,5 @@
 // error-pattern: the evaluated program aborted execution
 // compile-flags: -C panic=abort
-// ignore-windows: windows panics via inline assembly (FIXME)
 
 fn main() {
     core::panic!("{}-panicking from libcore", 42);
