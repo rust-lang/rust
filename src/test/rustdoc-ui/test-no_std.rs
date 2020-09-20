3ca187ef51b8f40930fd9f36bde8327c0c12e2dc
Jakob Schikowski <jakob.schikowski@gmx.de>
2020-09-20 23:28:59 +0200

    libtest: Print the total time taken to execute a test suite

--- src/test/rustdoc-ui/test-no_std.rs
+++ src/test/rustdoc-ui/test-no_std.rs
@@ -1,5 +1,6 @@
 // compile-flags:--test
 // normalize-stdout-test: "src/test/rustdoc-ui" -> "$$DIR"
+// normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
 // check-pass
 
 #![no_std]
