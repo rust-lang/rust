5fdf436ad96099bb327b072e2c555533845478c9
Josh Triplett <josh@joshtriplett.org>
2020-09-20 21:19:34 -0700

    unwind: Move linux-gnu library linking to lib.rs and libc
    This unifies it with the handling of `target-feature=+crt-static` on
    other platforms, and allows for supporting static glibc in the future.

--- library/unwind/src/lib.rs
+++ library/unwind/src/lib.rs
@@ -42,6 +42,10 @@ cfg_if::cfg_if! {
 #[link(name = "gcc_s", cfg(not(target_feature = "crt-static")))]
 extern "C" {}
 
+#[cfg(all(target_os = "linux", target_env = "gnu", not(feature = "llvm-libunwind")))]
+#[link(name = "gcc_s", cfg(not(target_feature = "crt-static")))]
+extern "C" {}
+
 #[cfg(target_os = "redox")]
 #[link(name = "gcc_eh", kind = "static-nobundle", cfg(target_feature = "crt-static"))]
 #[link(name = "gcc_s", cfg(not(target_feature = "crt-static")))]
