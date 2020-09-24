daf976f6129e5fb16effc48bf91853548774e235
Oliver Scherer <github35764891676564198441@oli-obk.de>
2020-09-24 17:11:47 +0200

    Revert a test change to make sure it's still testing the original issue

--- src/test/ui/pattern/issue-71042-opaquely-typed-constant-used-in-pattern.rs
+++ src/test/ui/pattern/issue-71042-opaquely-typed-constant-used-in-pattern.rs
@@ -4,7 +4,7 @@
 fn main() {
     const C: impl Copy = 0;
     match C {
-        C => {} //~ ERROR: `impl Copy` cannot be used in patterns
+        C | //~ ERROR: `impl Copy` cannot be used in patterns
         _ => {}
     }
 }
