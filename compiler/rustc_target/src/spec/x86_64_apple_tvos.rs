c29a29cba099aef1e05c83970decdcbedaabd69a
Eric Huss <eric@huss.org>
2020-09-25 12:07:46 -0700

    Defer Apple SDKROOT detection to link time.

--- compiler/rustc_target/src/spec/x86_64_apple_tvos.rs
+++ compiler/rustc_target/src/spec/x86_64_apple_tvos.rs
@@ -1,8 +1,8 @@
-use super::apple_sdk_base::{opts, AppleOS, Arch};
+use super::apple_sdk_base::{opts, Arch};
 use crate::spec::{LinkerFlavor, Target, TargetOptions, TargetResult};
 
 pub fn target() -> TargetResult {
-    let base = opts(Arch::X86_64, AppleOS::iOS)?;
+    let base = opts(Arch::X86_64);
     Ok(Target {
         llvm_target: "x86_64-apple-tvos".to_string(),
         target_endian: "little".to_string(),
