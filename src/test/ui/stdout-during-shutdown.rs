6b8b9c4adbd7d8d53d730a3c01d154dec3bd179f
Mara Bos <m-ou.se@m-ou.se>
2020-09-26 11:47:26 +0200

    Disable stdout-during-shutdown test on emscripten.

--- src/test/ui/stdout-during-shutdown.rs
+++ src/test/ui/stdout-during-shutdown.rs
@@ -1,5 +1,10 @@
 // run-pass
 // check-run-results
+// ignore-emscripten
+
+// Emscripten doesn't flush its own stdout buffers on exit, which would fail
+// this test. So this test is disabled on this platform.
+// See https://emscripten.org/docs/getting_started/FAQ.html#what-does-exiting-the-runtime-mean-why-don-t-atexit-s-run
 
 #![feature(rustc_private)]
 
