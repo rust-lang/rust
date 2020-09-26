77ef1b36a1b4657a3e1d5f072b8b66f35823546c
Ivan Tham <pickfire@riseup.net>
2020-09-26 18:32:12 +0800

    Slightly improve comment for spec from iter doc
    Rewrap, fix weird casing and make it clearer (at least to me).

--- library/alloc/src/vec.rs
+++ library/alloc/src/vec.rs
@@ -2188,14 +2188,16 @@ impl<T> Drop for InPlaceDrop<T> {
 
 impl<T> SpecFromIter<T, IntoIter<T>> for Vec<T> {
     fn from_iter(iterator: IntoIter<T>) -> Self {
-        // A common case is passing a vector into a function which immediately
-        // re-collects into a vector. We can short circuit this if the IntoIter
-        // has not been advanced at all.
-        // When it has been advanced We can also reuse the memory and move the data to the front.
-        // But we only do so when the resulting Vec wouldn't have more unused capacity
-        // than creating it through the generic FromIterator implementation would. That limitation
-        // is not strictly necessary as Vec's allocation behavior is intentionally unspecified.
-        // But it is a conservative choice.
+        // It is common to pass a vector to a function which immediately
+        // re-collects as a vector. We can short circuit this if the IntoIter
+        // has not been advanced.
+        //
+        // If it has been advanced, we can reuse the memory by moving the
+        // data to the front. But we only do so when the resulting Vec
+        // would have less unused capacity than extending a new Vec with the
+        // generic from_iter would. This limitation is not strictly necessary
+        // as Vec's allocation is intentionally unspecified.
+        // This is a conservative choice.
         let has_advanced = iterator.buf.as_ptr() as *const _ != iterator.ptr;
         if !has_advanced || iterator.len() >= iterator.cap / 2 {
             unsafe {
