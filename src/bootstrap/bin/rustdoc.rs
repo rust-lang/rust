351f850d33e0a000c8fd8b43e71e03d91f470383
Joshua Nelson <jyn514@gmail.com>
2020-09-29 18:20:21 -0400

    Remove unused --cfg stageN

--- src/bootstrap/bin/rustdoc.rs
+++ src/bootstrap/bin/rustdoc.rs
@@ -11,7 +11,6 @@ fn main() {
     let args = env::args_os().skip(1).collect::<Vec<_>>();
     let rustdoc = env::var_os("RUSTDOC_REAL").expect("RUSTDOC_REAL was not set");
     let libdir = env::var_os("RUSTDOC_LIBDIR").expect("RUSTDOC_LIBDIR was not set");
-    let stage = env::var("RUSTC_STAGE").expect("RUSTC_STAGE was not set");
     let sysroot = env::var_os("RUSTC_SYSROOT").expect("RUSTC_SYSROOT was not set");
 
     use std::str::FromStr;
@@ -26,8 +25,6 @@ fn main() {
 
     let mut cmd = Command::new(rustdoc);
     cmd.args(&args)
-        .arg("--cfg")
-        .arg(format!("stage{}", stage))
         .arg("--sysroot")
         .arg(&sysroot)
         .env(bootstrap::util::dylib_path_var(), env::join_paths(&dylib_path).unwrap());
