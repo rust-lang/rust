aea3f8dbc94fcb3faf4125a2dbc4fa2aaa0fb005
Ivan Tham <pickfire@riseup.net>
2020-10-02 01:21:36 +0800

    Remove trailing whitespace in iter chain doc

--- library/core/src/iter/adapters/chain.rs
+++ library/core/src/iter/adapters/chain.rs
@@ -12,7 +12,7 @@ use crate::usize;
 /// ```
 /// use std::iter::Chain;
 /// use std::slice::Iter;
-/// 
+///
 /// let a1 = [1, 2, 3];
 /// let a2 = [4, 5, 6];
 /// let iter: Chain<Iter<_>, Iter<_>> = a1.iter().chain(a2.iter());
