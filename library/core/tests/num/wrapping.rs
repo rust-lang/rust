4e973966b95e5157754f91216456a3c37a1472b2
Jubilee Young <workingjubilee@gmail.com>
2020-10-02 11:40:57 -0700

    Remove unnecessary mod-cfg

--- library/core/tests/num/wrapping.rs
+++ library/core/tests/num/wrapping.rs
@@ -60,20 +60,17 @@ macro_rules! wrapping_test {
     };
 }
 
-#[cfg(tests)]
-mod tests {
-    wrapping_test!(i8, i8::MIN, i8::MAX);
-    wrapping_test!(i16, i16::MIN, i16::MAX);
-    wrapping_test!(i32, i32::MIN, i32::MAX);
-    wrapping_test!(i64, i64::MIN, i64::MAX);
-    #[cfg(not(target_os = "emscripten"))]
-    wrapping_test!(i128, i128::MIN, i128::MAX);
-    wrapping_test!(isize, isize::MIN, isize::MAX);
-    wrapping_test!(u8, u8::MIN, u8::MAX);
-    wrapping_test!(u16, u16::MIN, u16::MAX);
-    wrapping_test!(u32, u32::MIN, u32::MAX);
-    wrapping_test!(u64, u64::MIN, u64::MAX);
-    #[cfg(not(target_os = "emscripten"))]
-    wrapping_test!(u128, u128::MIN, u128::MAX);
-    wrapping_test!(usize, usize::MIN, usize::MAX);
-}
+wrapping_test!(i8, i8::MIN, i8::MAX);
+wrapping_test!(i16, i16::MIN, i16::MAX);
+wrapping_test!(i32, i32::MIN, i32::MAX);
+wrapping_test!(i64, i64::MIN, i64::MAX);
+#[cfg(not(target_os = "emscripten"))]
+wrapping_test!(i128, i128::MIN, i128::MAX);
+wrapping_test!(isize, isize::MIN, isize::MAX);
+wrapping_test!(u8, u8::MIN, u8::MAX);
+wrapping_test!(u16, u16::MIN, u16::MAX);
+wrapping_test!(u32, u32::MIN, u32::MAX);
+wrapping_test!(u64, u64::MIN, u64::MAX);
+#[cfg(not(target_os = "emscripten"))]
+wrapping_test!(u128, u128::MIN, u128::MAX);
+wrapping_test!(usize, usize::MIN, usize::MAX);
