5e90909f38d2921a463efea4a3656bc7a6c05709
Tim Vermeulen <tvermeulen@me.com>
2020-10-02 02:47:01 +0200

    Add tests

--- library/core/src/slice/iter/macros.rs
+++ library/core/src/slice/iter/macros.rs
@@ -188,7 +188,7 @@ macro_rules! iterator {
             #[inline]
             fn advance_by(&mut self, n: usize) -> Result<(), usize>{
                 let advance = cmp::min(n, len!(self));
-                // SAFETY: `advance` does not exceed `self.len()` by construction
+                // SAFETY: By construction, `advance` does not exceed `self.len()`.
                 unsafe { self.post_inc_start(advance as isize) };
                 if advance == n { Ok(()) } else { Err(advance) }
             }
@@ -383,7 +383,7 @@ macro_rules! iterator {
             #[inline]
             fn advance_back_by(&mut self, n: usize) -> Result<(), usize> {
                 let advance = cmp::min(n, len!(self));
-                // SAFETY: `advance` does not exceed `self.len()` by construction
+                // SAFETY: By construction, `advance` does not exceed `self.len()`.
                 unsafe { self.pre_dec_end(advance as isize) };
                 if advance == n { Ok(()) } else { Err(advance) }
             }
