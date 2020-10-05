c8d25af6985c2c6be388fc198c2c7d50eb553fc4
Camelid <camelidcamel@gmail.com>
2020-10-05 15:07:27 -0700

    Fixup

--- compiler/rustc_trait_selection/src/opaque_types.rs
+++ compiler/rustc_trait_selection/src/opaque_types.rs
@@ -41,13 +41,10 @@ pub struct OpaqueTypeDecl<'tcx> {
     /// The span of this particular definition of the opaque type. So
     /// for example:
     ///
-    /// ```
+    /// ```ignore (incomplete snippet)
     /// type Foo = impl Baz;
     /// fn bar() -> Foo {
     /// //          ^^^ This is the span we are looking for!
-    /// // ...
-    /// # unimplemented!()
-    /// }
     /// ```
     ///
     /// In cases where the fn returns `(impl Trait, impl Trait)` or
