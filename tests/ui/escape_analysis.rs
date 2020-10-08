418cde0389d47628b32e533c47e64874fd1050fb
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-10-08 09:06:19 -0300

    clippy_lint: Fix typo (now -> not)

--- tests/ui/escape_analysis.rs
+++ tests/ui/escape_analysis.rs
@@ -177,5 +177,5 @@ mod issue_3739 {
 
 /// Issue #5542
 ///
-/// This shouldn't warn for `boxed_local` as it is a function implemented in C.
-pub extern "C" fn do_now_warn_me(_c_pointer: Box<String>) -> () {}
+/// This shouldn't warn for `boxed_local` as it is a function to be used in C.
+pub extern "C" fn do_not_warn_me(_c_pointer: Box<String>) -> () {}
