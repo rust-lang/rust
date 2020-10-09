36d9b72354560528f07796a8a09b339bdcf37d53
Vojtech Kral <vojtech@kral.hk>
2020-10-09 13:06:04 +0200

    liballoc: VecDeque: Add binary search functions

--- library/alloc/tests/lib.rs
+++ library/alloc/tests/lib.rs
@@ -20,6 +20,7 @@
 #![feature(inplace_iteration)]
 #![feature(iter_map_while)]
 #![feature(int_bits_const)]
+#![feature(vecdeque_binary_search)]
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
