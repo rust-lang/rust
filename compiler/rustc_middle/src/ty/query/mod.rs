8071c0a401bbdd4132b6162b564410a2e6e01df9
Camille GILLOT <gillot.camille@gmail.com>
2020-10-11 10:34:13 +0200

    Make QueryEngine opaque to TyCtxt.

--- compiler/rustc_middle/src/ty/query/mod.rs
+++ compiler/rustc_middle/src/ty/query/mod.rs
@@ -44,6 +44,7 @@ use rustc_hir::def_id::{CrateNum, DefId, DefIdMap, DefIdSet, LocalDefId};
 use rustc_hir::lang_items::{LangItem, LanguageItems};
 use rustc_hir::{Crate, ItemLocalId, TraitCandidate};
 use rustc_index::{bit_set::FiniteBitSet, vec::IndexVec};
+use rustc_serialize::opaque;
 use rustc_session::config::{EntryFnType, OptLevel, OutputFilenames, SymbolManglingVersion};
 use rustc_session::utils::NativeLibKind;
 use rustc_session::CrateDisambiguator;
