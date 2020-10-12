874a57403947ded8bb53adcac123ea272d32e7f2
Benedikt Terhechte <terhechte@gmail.com>
2020-10-12 07:54:45 +0200

    Renamed symbol

--- compiler/rustc_target/src/spec/aarch64_apple_ios_macabi.rs
+++ compiler/rustc_target/src/spec/aarch64_apple_ios_macabi.rs
@@ -1,9 +1,9 @@
 use super::apple_sdk_base::{opts, Arch};
-use crate::spec::{LinkerFlavor, Target, TargetOptions, TargetResult};
+use crate::spec::{LinkerFlavor, Target, TargetOptions};
 
-pub fn target() -> TargetResult {
+pub fn target() -> Target {
     let base = opts(Arch::Arm64_macabi);
-    Ok(Target {
+    Target {
         llvm_target: "arm64-apple-ios14.2-macabi".to_string(),
         target_endian: "little".to_string(),
         target_pointer_width: "64".to_string(),
@@ -31,5 +31,5 @@ pub fn target() -> TargetResult {
                 .to_string(),
             ..base
         },
-    })
+    }
 }
