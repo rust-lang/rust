660480ac913a2e6173488e114a90bc04f07f64c1
Esteban Küber <esteban@kuber.com.ar>
2020-10-13 23:16:00 -0700

    Avoid suggesting inside macro call
    Specifically avoid non-sense suggestion when calling `panic!` with
    something `!Send`.

--- compiler/rustc_trait_selection/src/traits/error_reporting/mod.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/mod.rs
@@ -1537,6 +1537,7 @@ impl<'a, 'tcx> InferCtxtPrivExt<'tcx> for InferCtxt<'a, 'tcx> {
                     let generics = self.tcx.generics_of(*def_id);
                     if generics.params.iter().any(|p| p.name != kw::SelfUpper)
                         && !snippet.ends_with('>')
+                        && !span.from_expansion()
                     {
                         // FIXME: To avoid spurious suggestions in functions where type arguments
                         // where already supplied, we check the snippet to make sure it doesn't
