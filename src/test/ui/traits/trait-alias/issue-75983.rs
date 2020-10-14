59cc9de039ca251599b007cdf32b721bdd3f4578
Yuki Okushi <huyuumi.dev@gmail.com>
2020-10-15 08:36:24 +0900

    Add test for issue-75983

---
+++ src/test/ui/traits/trait-alias/issue-75983.rs
@@ -0,0 +1,17 @@
+// check-pass
+
+#![feature(trait_alias)]
+
+struct Bar;
+trait Foo {}
+impl Foo for Bar {}
+
+trait Baz = Foo where Bar: Foo;
+
+fn new() -> impl Baz {
+    Bar
+}
+
+fn main() {
+    let _ = new();
+}
