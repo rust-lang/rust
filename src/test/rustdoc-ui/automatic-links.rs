e833cd302bb7f88dc47846b17f3ba2cc7d47d4ac
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-10-14 15:11:55 +0200

    Improve automatic_links globally

--- src/test/rustdoc-ui/automatic-links.rs
+++ src/test/rustdoc-ui/automatic-links.rs
@@ -10,8 +10,40 @@
 /// [http://c.com][http://c.com]
 pub fn a() {}
 
+/// https://somewhere.com
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com/a
+//~^ ERROR this URL is not a hyperlink
+/// https://www.somewhere.com
+//~^ ERROR this URL is not a hyperlink
+/// https://www.somewhere.com/a
+//~^ ERROR this URL is not a hyperlink
+/// https://subdomain.example.com
+//~^ ERROR not a hyperlink
+/// https://somewhere.com?
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com/a?
+//~^ ERROR this URL is not a hyperlink
 /// https://somewhere.com?hello=12
-//~^ ERROR won't be a link as is
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com/a?hello=12
+//~^ ERROR this URL is not a hyperlink
+/// https://example.com?hello=12#xyz
+//~^ ERROR this URL is not a hyperlink
+/// https://example.com/a?hello=12#xyz
+//~^ ERROR this URL is not a hyperlink
+/// https://example.com#xyz
+//~^ ERROR this URL is not a hyperlink
+/// https://example.com/a#xyz
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com?hello=12&bye=11
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com/a?hello=12&bye=11
+//~^ ERROR this URL is not a hyperlink
+/// https://somewhere.com?hello=12&bye=11#xyz
+//~^ ERROR this URL is not a hyperlink
+/// hey! https://somewhere.com/a?hello=12&bye=11#xyz
+//~^ ERROR this URL is not a hyperlink
 pub fn c() {}
 
 /// <https://somewhere.com>
@@ -20,3 +52,9 @@ pub fn c() {}
 ///
 /// [b]: http://b.com
 pub fn everything_is_fine_here() {}
+
+#[allow(automatic_links)]
+pub mod foo {
+    /// https://somewhere.com/a?hello=12&bye=11#xyz
+    pub fn bar() {}
+}
