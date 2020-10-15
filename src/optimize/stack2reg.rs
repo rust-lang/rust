1c272b1224fabfa307942fc76eb14b7b7bddd040
bjorn3 <bjorn3@users.noreply.github.com>
2020-10-15 18:44:22 +0200

    Fix a compilation error in debug mode

--- src/optimize/stack2reg.rs
+++ src/optimize/stack2reg.rs
@@ -182,7 +182,10 @@ impl<'a> OptimizeContext<'a> {
     }
 }
 
-pub(super) fn optimize_function(ctx: &mut Context) {
+pub(super) fn optimize_function(
+    ctx: &mut Context,
+    #[cfg_attr(not(debug_assertions), allow(unused_variables))] clif_comments: &mut crate::pretty_clif::CommentWriter,
+) {
     combine_stack_addr_with_load_store(&mut ctx.func);
 
     let mut opt_ctx = OptimizeContext::for_context(ctx);
