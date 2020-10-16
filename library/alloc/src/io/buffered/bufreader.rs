e7d3f248e930b655ccbc4c673d05809a6a58b15a
Yonggang Luo <luoyonggang@gmail.com>
2020-10-16 15:20:09 +0800

    fixes unsafe

--- library/alloc/src/io/buffered/bufreader.rs
+++ library/alloc/src/io/buffered/bufreader.rs
@@ -305,7 +305,7 @@ impl<R: Read> Read for BufReader<R> {
 
     // we can't skip unconditionally because of the large buffer case in read.
     unsafe fn initializer(&self) -> Initializer {
-        unsafe { (*self).initializer() }
+        unsafe { self.inner.initializer() }
     }
 }
 
