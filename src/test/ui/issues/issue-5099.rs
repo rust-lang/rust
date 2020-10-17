e701ae376a040d9f5614cda3be6fabffe884b432
Jan Verbeek <jan.verbeek@posteo.nl>
2020-10-17 13:36:59 +0200

    Suggest correct place to add `self` parameter when inside closure
    It would incorrectly suggest adding it as a parameter to the closure instead of the
    containing function.

--- src/test/ui/issues/issue-5099.rs
+++ src/test/ui/issues/issue-5099.rs
@@ -5,6 +5,9 @@ trait B <A> {
     fn b(x: i32) {
         this.b(x); //~ ERROR cannot find value `this` in this scope
     }
+    fn c() {
+        let _ = || this.a; //~ ERROR cannot find value `this` in this scope
+    }
 }
 
 fn main() {}
