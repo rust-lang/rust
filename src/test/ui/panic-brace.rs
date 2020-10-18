14dcf0a8ac632e6073ec979da72e286a57c50f4c
Mara Bos <m-ou.se@m-ou.se>
2020-10-18 23:36:00 +0200

    Test for formating placeholders in panic_fmt lint test.

--- src/test/ui/panic-brace.rs
+++ src/test/ui/panic-brace.rs
@@ -4,6 +4,6 @@
 fn main() {
     panic!("here's a brace: {"); //~ WARN Panic message contains a brace
     std::panic!("another one: }"); //~ WARN Panic message contains a brace
-    core::panic!("Hello { { {"); //~ WARN Panic message contains a brace
-    assert!(false, "} } }..."); //~ WARN Panic message contains a brace
+    core::panic!("Hello {}"); //~ WARN Panic message contains an unused formatting placeholder
+    assert!(false, "{:03x} bla"); //~ WARN Panic message contains an unused formatting placeholder
 }
