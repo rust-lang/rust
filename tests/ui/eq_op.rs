16b5f37b5a23f475d0d94efea764c57e4572f63f
ThibsG <Thibs@debian.com>
2020-10-19 17:31:41 +0200

    Split `eq_op` ui tests to avoid file limit error in CI

--- tests/ui/eq_op.rs
+++ tests/ui/eq_op.rs
@@ -60,8 +60,6 @@ fn main() {
     const B: u32 = 10;
     const C: u32 = A / B; // ok, different named constants
     const D: u32 = A / A;
-
-    check_assert_identical_args();
 }
 
 #[rustfmt::skip]
@@ -86,59 +84,4 @@ fn check_ignore_macro() {
     check_if_named_foo!(foo);
     // checks if the lint ignores macros with `!` operator
     !bool_macro!(1) && !bool_macro!("");
-}
-
-macro_rules! assert_in_macro_def {
-    () => {
-        let a = 42;
-        assert_eq!(a, a);
-        assert_ne!(a, a);
-        debug_assert_eq!(a, a);
-        debug_assert_ne!(a, a);
-    };
-}
-
-// lint identical args in assert-like macro invocations (see #3574)
-fn check_assert_identical_args() {
-    // lint also in macro definition
-    assert_in_macro_def!();
-
-    let a = 1;
-    let b = 2;
-
-    // lint identical args in `assert_eq!`
-    assert_eq!(a, a);
-    assert_eq!(a + 1, a + 1);
-    // ok
-    assert_eq!(a, b);
-    assert_eq!(a, a + 1);
-    assert_eq!(a + 1, b + 1);
-
-    // lint identical args in `assert_ne!`
-    assert_ne!(a, a);
-    assert_ne!(a + 1, a + 1);
-    // ok
-    assert_ne!(a, b);
-    assert_ne!(a, a + 1);
-    assert_ne!(a + 1, b + 1);
-
-    // lint identical args in `debug_assert_eq!`
-    debug_assert_eq!(a, a);
-    debug_assert_eq!(a + 1, a + 1);
-    // ok
-    debug_assert_eq!(a, b);
-    debug_assert_eq!(a, a + 1);
-    debug_assert_eq!(a + 1, b + 1);
-
-    // lint identical args in `debug_assert_ne!`
-    debug_assert_ne!(a, a);
-    debug_assert_ne!(a + 1, a + 1);
-    // ok
-    debug_assert_ne!(a, b);
-    debug_assert_ne!(a, a + 1);
-    debug_assert_ne!(a + 1, b + 1);
-
-    let my_vec = vec![1; 5];
-    let mut my_iter = my_vec.iter();
-    assert_ne!(my_iter.next(), my_iter.next());
 }
