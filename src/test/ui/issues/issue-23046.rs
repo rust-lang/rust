c146e8c54fa0b99defd1ba77ad4d6505c491f4f3
SNCPlay42 <SNCPlay42@gmail.com>
2020-10-19 17:58:44 +0100

    revert workaround #73027

--- src/test/ui/issues/issue-23046.rs
+++ src/test/ui/issues/issue-23046.rs
@@ -14,7 +14,7 @@ pub fn let_<'var, VAR, F: for<'v> Fn(Expr<'v, VAR>) -> Expr<'v, VAR>>
 }
 
 fn main() {
-    let ex = |x| {
-        let_(add(x,x), |y| { //~ ERROR type annotations needed
+    let ex = |x| { //~ ERROR type annotations needed
+        let_(add(x,x), |y| {
             let_(add(x, x), |x|x)})};
 }
