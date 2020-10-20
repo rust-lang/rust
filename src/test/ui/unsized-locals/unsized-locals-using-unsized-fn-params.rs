a3470b13ab8cfba6b698aa0ae0e9591a76d40ba7
Santiago Pastorino <spastorino@gmail.com>
2020-10-20 13:08:40 -0300

    Test that unsized locals fail when turning unsized_fn_params feature flag on

---
+++ src/test/ui/unsized-locals/unsized-locals-using-unsized-fn-params.rs
@@ -0,0 +1,7 @@
+#![feature(unsized_fn_params)]
+
+fn main() {
+    let foo: Box<[u8]> = Box::new(*b"foo");
+    let _foo: [u8] = *foo;
+    //~^ ERROR: the size for values of type `[u8]` cannot be known at compilation time [E0277]
+}
