8b8b6392ba1c1e41c4f235bce1669953c9cd3b72
Santiago Pastorino <spastorino@gmail.com>
2020-10-20 21:52:49 -0300

    Add comment explaining why in these tests unsized locals are not accepted

--- src/test/ui/unsized-locals/unsized-local-pat.rs
+++ src/test/ui/unsized-locals/unsized-local-pat.rs
@@ -1,6 +1,8 @@
 #![feature(box_patterns)]
 #![feature(unsized_fn_params)]
 
+// Ensure that even with unsized_fn_params, unsized types in parameter patterns are not accepted.
+
 #[allow(dead_code)]
 fn f1(box box _b: Box<Box<[u8]>>) {}
 //~^ ERROR: the size for values of type `[u8]` cannot be known at compilation time [E0277]
--- src/test/ui/unsized-locals/unsized-locals-using-unsized-fn-params.rs
+++ src/test/ui/unsized-locals/unsized-locals-using-unsized-fn-params.rs
@@ -1,5 +1,7 @@
 #![feature(unsized_fn_params)]
 
+// Ensure that even with unsized_fn_params, unsized locals are not accepted.
+
 fn main() {
     let foo: Box<[u8]> = Box::new(*b"foo");
     let _foo: [u8] = *foo;
