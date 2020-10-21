cc9ab1cd58e55de4e60cfaf884e89a9d66563b38
Santiago Pastorino <spastorino@gmail.com>
2020-10-21 08:23:40 -0300

    Merge unsized locals pat tests

--- src/test/ui/unsized-locals/unsized-locals-using-unsized-fn-params.rs
+++ src/test/ui/unsized-locals/unsized-locals-using-unsized-fn-params.rs
@@ -1,6 +1,12 @@
+#![feature(box_patterns)]
 #![feature(unsized_fn_params)]
 
-// Ensure that even with unsized_fn_params, unsized locals are not accepted.
+#[allow(dead_code)]
+fn f1(box box _b: Box<Box<[u8]>>) {}
+//~^ ERROR: the size for values of type `[u8]` cannot be known at compilation time [E0277]
+
+fn f2((_x, _y): (i32, [i32])) {}
+//~^ ERROR: the size for values of type `[i32]` cannot be known at compilation time [E0277]
 
 fn main() {
     let foo: Box<[u8]> = Box::new(*b"foo");
