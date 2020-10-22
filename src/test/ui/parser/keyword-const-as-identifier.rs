24fa10f30ba7f7611c91037d49e5ba1cfb222583
Camelid <camelidcamel@gmail.com>
2020-10-22 13:04:59 -0700

    Update tests

--- src/test/ui/parser/keyword-const-as-identifier.rs
+++ src/test/ui/parser/keyword-const-as-identifier.rs
@@ -1,3 +1,5 @@
 fn main() {
-    let const = "foo"; //~ ERROR expected `{`, found `=`
+    let const = "foo";
+    //~^ ERROR expected `{`, found `=`
+    //~| ERROR inline-const is experimental [E0658]
 }
