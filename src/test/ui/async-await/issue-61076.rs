28f02fbf3e26738047135a577622a313a1c3bfb3
Esteban Küber <esteban@kuber.com.ar>
2020-10-23 10:54:34 -0700

    Suggest calling await on method call and field access
    When encountering a failing method or field resolution on a `Future`,
    look at the `Output` and try the same operation on it. If successful,
    suggest calling `.await` on the `Future`.
    
    This had already been introduced in #72784, but at some point they
    stopped working.

--- src/test/ui/async-await/issue-61076.rs
+++ src/test/ui/async-await/issue-61076.rs
@@ -40,6 +40,14 @@ async fn foo() -> Result<(), ()> {
 
 async fn bar() -> Result<(), ()> {
     foo()?; //~ ERROR the `?` operator can only be applied to values that implement `Try`
+    //~^ NOTE the `?` operator cannot be applied to type `impl Future`
+    //~| HELP the trait `Try` is not implemented for `impl Future`
+    //~| NOTE required by `into_result`
+    //~| HELP consider `await`ing on the `Future`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
     Ok(())
 }
 
@@ -48,25 +56,42 @@ async fn struct_() -> Struct {
 }
 
 async fn tuple() -> Tuple {
+    //~^ NOTE the `Output` of this `async fn`'s expected opaque type
     Tuple(1i32)
 }
 
 async fn baz() -> Result<(), ()> {
     let t = T;
     t?; //~ ERROR the `?` operator can only be applied to values that implement `Try`
+    //~^ NOTE the `?` operator cannot be applied to type `T`
+    //~| HELP the trait `Try` is not implemented for `T`
+    //~| NOTE required by `into_result`
+    //~| HELP consider `await`ing on the `Future`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
+    //~| NOTE in this expansion of desugaring of operator `?`
+
 
     let _: i32 = tuple().0; //~ ERROR no field `0`
+    //~^ HELP consider `await`ing on the `Future`
+    //~| NOTE field not available in `impl Future`
 
     let _: i32 = struct_().a; //~ ERROR no field `a`
+    //~^ HELP consider `await`ing on the `Future`
+    //~| NOTE field not available in `impl Future`
 
     struct_().method(); //~ ERROR no method named
-
+    //~^ NOTE method not found in `impl Future`
+    //~| HELP consider `await`ing on the `Future`
     Ok(())
 }
 
 async fn match_() {
-    match tuple() {
+    match tuple() { //~ HELP consider `await`ing on the `Future`
         Tuple(_) => {} //~ ERROR mismatched types
+        //~^ NOTE expected opaque type, found struct `Tuple`
+        //~| NOTE expected opaque type `impl Future`
     }
 }
 
