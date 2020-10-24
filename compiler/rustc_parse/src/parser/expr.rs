27732bfac3763cae1016a2dcaa39225e5d868048
Gurwinder Singh <vargwin@gmail.com>
2020-10-24 18:16:52 +0530

    Perform the lookahead once instead of twice

--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -2026,14 +2026,13 @@ impl<'a> Parser<'a> {
         self.look_ahead(1, |t| t.is_ident())
             && (
                 // `{ ident, ` cannot start a block.
-                self.look_ahead(2, |t| t == &token::Comma)
-                    || self.look_ahead(2, |t| t == &token::Colon)
-                        && (
-                            // `{ ident: token, ` cannot start a block.
-                            self.look_ahead(4, |t| t == &token::Comma) ||
-                // `{ ident: ` cannot start a block unless it's a type ascription `ident: Type`.
-                self.look_ahead(3, |t| !t.can_begin_type())
-                        )
+                self.look_ahead(2, |t| t == &token::Comma || t == &token::Colon)
+                    && (
+                        // `{ ident: token, ` cannot start a block.
+                        self.look_ahead(4, |t| t == &token::Comma) ||
+                            // `{ ident: ` cannot start a block unless it's a type ascription `ident: Type`.
+                            self.look_ahead(3, |t| !t.can_begin_type())
+                    )
             )
     }
 
