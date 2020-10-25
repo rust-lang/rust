ddcc4f241e1e9987b050067f4303d0449774b36e
Ralf Jung <post@ralfj.de>
2020-10-25 10:00:50 +0100

    rustup; make panic output less dependent on stdlib internals

--- tests/run-pass/panic/panic1.rs
+++ tests/run-pass/panic/panic1.rs
@@ -1,8 +1,8 @@
 // rustc-env: RUST_BACKTRACE=1
 // compile-flags: -Zmiri-disable-isolation
-// normalize-stderr-test ".*/(rust|checkout)/library/" -> "RUSTLIB/"
-// normalize-stderr-test "RUSTLIB/(.*):\d+:\d+ "-> "RUSTLIB/$1:LL:COL "
+// normalize-stderr-test "at .*/(rust|checkout)/library/.*" -> "at RUSTLIB/$$FILE:LL:COL"
 // normalize-stderr-test "::<.*>" -> ""
+
 
 fn main() {
     std::panic!("panicking from libstd");
