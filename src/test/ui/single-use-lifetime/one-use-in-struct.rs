cc468c0829dd159a50359441989c06834faa6a00
Olivia Crain <olivia@olivia.dev>
2020-10-24 19:22:53 -0500

    Use check-pass in single-use-lifetime ui test suite

--- src/test/ui/single-use-lifetime/one-use-in-struct.rs
+++ src/test/ui/single-use-lifetime/one-use-in-struct.rs
@@ -2,27 +2,26 @@
 // even when they are only used once (since to not use a named
 // lifetime is illegal!)
 //
-// build-pass (FIXME(62277): could be check-pass?)
+// check-pass
 
 #![deny(single_use_lifetimes)]
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
 struct Foo<'f> {
-    data: &'f u32
+    data: &'f u32,
 }
 
 enum Bar<'f> {
-    Data(&'f u32)
+    Data(&'f u32),
 }
 
-trait Baz<'f> { }
+trait Baz<'f> {}
 
 // `Derive`d impls shouldn't trigger a warning, either (Issue #53738).
-
 #[derive(Debug)]
 struct Quux<'a> {
     priors: &'a u32,
 }
 
-fn main() { }
+fn main() {}
