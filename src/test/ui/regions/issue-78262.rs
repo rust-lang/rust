4ec396ea5dd7bddfaa667766ab6cd8824c8028da
Yuki Okushi <huyuumi.dev@gmail.com>
2020-10-25 11:43:26 +0900

    Test with NLL explicitly

--- src/test/ui/regions/issue-78262.rs
+++ src/test/ui/regions/issue-78262.rs
@@ -1,3 +1,7 @@
+// revisions: nll default
+// ignore-compare-mode-nll
+//[nll]compile-flags: -Z borrowck=mir
+
 trait TT {}
 
 impl dyn TT {
@@ -5,5 +9,6 @@ impl dyn TT {
 }
 
 fn main() {
-    let f = |x: &dyn TT| x.func(); //~ ERROR: mismatched types
+    let f = |x: &dyn TT| x.func(); //[default]~ ERROR: mismatched types
+    //[nll]~^ ERROR: borrowed data escapes outside of closure
 }
