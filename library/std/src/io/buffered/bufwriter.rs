a4ba179bdddcabae1947eb307ec017d576ae4b3a
Michele Lacchia <michelelacchia@gmail.com>
2020-10-26 11:13:47 +0100

    fix(docs): typo in BufWriter documentation

--- library/std/src/io/buffered/bufwriter.rs
+++ library/std/src/io/buffered/bufwriter.rs
@@ -15,7 +15,7 @@ use crate::io::{
 /// *repeated* write calls to the same file or network socket. It does not
 /// help when writing very large amounts at once, or writing just one or a few
 /// times. It also provides no advantage when writing to a destination that is
-/// in memory, like a [`Vec`]<u8>`.
+/// in memory, like a [`Vec`]`<u8>`.
 ///
 /// It is critical to call [`flush`] before `BufWriter<W>` is dropped. Though
 /// dropping will attempt to flush the contents of the buffer, any errors
