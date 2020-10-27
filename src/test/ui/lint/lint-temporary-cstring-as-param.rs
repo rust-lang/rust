572cd358d379e4e641ad44bc693cdf48ceaf3581
Nathan Whitaker <nathan.whitaker01@gmail.com>
2020-10-27 10:10:30 -0400

    Fix test

--- src/test/ui/lint/lint-temporary-cstring-as-param.rs
+++ src/test/ui/lint/lint-temporary-cstring-as-param.rs
@@ -1,8 +1,9 @@
 #![deny(temporary_cstring_as_ptr)]
 
 use std::ffi::CString;
+use std::os::raw::c_char;
 
-fn some_function(data: *const i8) {}
+fn some_function(data: *const c_char) {}
 
 fn main() {
     some_function(CString::new("").unwrap().as_ptr());
