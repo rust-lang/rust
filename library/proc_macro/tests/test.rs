59061c53fcceb2c52df3cf4a86e6703ba4eb2a30
David Tolnay <dtolnay@gmail.com>
2020-11-01 09:34:22 -0800

    Add Ident PartialEq smoke test

--- library/proc_macro/tests/test.rs
+++ library/proc_macro/tests/test.rs
@@ -1,6 +1,6 @@
 #![feature(proc_macro_span)]
 
-use proc_macro::LineColumn;
+use proc_macro::{Ident, LineColumn};
 
 #[test]
 fn test_line_column_ord() {
@@ -9,4 +9,15 @@ fn test_line_column_ord() {
     let line1_column0 = LineColumn { line: 1, column: 0 };
     assert!(line0_column0 < line0_column1);
     assert!(line0_column1 < line1_column0);
+}
+
+#[test]
+fn test_ident_eq() {
+    // Good enough if it typechecks, since proc_macro::Ident can't exist in a test.
+    fn _check(ident: &Ident, string: String) {
+        let _ = ident == "serde";
+        let _ = *ident == "serde";
+        let _ = *ident == string;
+        let _ = *ident == &&&string;
+    }
 }
