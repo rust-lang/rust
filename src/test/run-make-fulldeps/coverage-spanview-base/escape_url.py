cdbce109264511d4943f9fb1a5ef8feca6778a91
Rich Kadel <richkadel@google.com>
2020-11-02 15:01:30 -0800

    fix cross-platform test bugs
    More portable way to make python 2/3 portable.
    
    Strip Args line (with hardcoded paths) from debug counters output.
    
    Ignore diff failures from llvm-cov debug output files ("counters"
    files), since generic function instantiations will appear in those files
    with mangled names. (Sadly, the demangler is apparently not applied to
    the debug output.)

--- src/test/run-make-fulldeps/coverage-spanview-base/escape_url.py
+++ src/test/run-make-fulldeps/coverage-spanview-base/escape_url.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-import six
 
 # Support python 2 or 3
-from six.moves.urllib.parse import quote
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib import quote
 
 # Converts the input string into a valid URL parameter string.
 print (quote(' '.join(sys.argv[1:])))
