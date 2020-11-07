c0c0597e099a6c6b1f3b47e839a630c5bc97aab9
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2020-11-07 14:34:44 +0300

    Update recently added targets

--- compiler/rustc_target/src/spec/mipsel_unknown_none.rs
+++ compiler/rustc_target/src/spec/mipsel_unknown_none.rs
@@ -9,17 +9,13 @@ use crate::spec::{PanicStrategy, Target, TargetOptions};
 pub fn target() -> Target {
     Target {
         llvm_target: "mipsel-unknown-none".to_string(),
-        target_endian: "little".to_string(),
         pointer_width: 32,
-        target_c_int_width: "32".to_string(),
         data_layout: "e-m:m-p:32:32-i8:8:32-i16:16:32-i64:64-n32-S64".to_string(),
         arch: "mips".to_string(),
-        target_os: "none".to_string(),
-        target_env: String::new(),
-        target_vendor: String::new(),
-        linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
 
         options: TargetOptions {
+            target_vendor: String::new(),
+            linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
             cpu: "mips32r2".to_string(),
             features: "+mips32r2,+soft-float,+noabicalls".to_string(),
             max_atomic_width: Some(32),
