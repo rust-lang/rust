73351109f573d0311f5aafcbeebdabac8c9e5db2
Tyson Nottingham <tgnottingham@gmail.com>
2020-11-08 20:28:03 -0800

    incr-comp: add clarifying comments to incr-prev-body-beyond-eof test

--- src/test/run-make/incr-prev-body-beyond-eof/a.rs
+++ src/test/run-make/incr-prev-body-beyond-eof/a.rs
@@ -3,9 +3,11 @@ fn main() {
     foo();
 }
 
+// For this test to operate correctly, foo's body must start on exactly the same
+// line and column and have the exact same length in bytes in a.rs and b.rs. In
+// a.rs, the body must end on a line number which does not exist in b.rs.
+// Basically, avoid modifying this file, including adding or removing whitespace!
 fn foo() {
-    // foo's span in a.rs and b.rs must be identical
-    // with respect to start line/column and length.
     assert_eq!(1, 1);
 
 
