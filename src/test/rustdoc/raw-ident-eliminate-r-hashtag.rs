f5e67b5ee1d442ddfc580224c69f260a07b8840b
Alexis Bourget <alexis.bourget@gmail.com>
2020-11-12 01:05:27 +0100

    Add a test for r# identifiers

---
+++ src/test/rustdoc/raw-ident-eliminate-r-hashtag.rs
@@ -0,0 +1,17 @@
+pub mod internal {
+    pub struct r#mod;
+
+    /// See [name], [other name]
+    ///
+    /// [name]: mod
+    /// [other name]: ../internal/struct.mod.html
+    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
+    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'other name'
+    pub struct B;
+}
+
+/// See [name].
+///
+/// [name]: internal::mod
+// @has 'raw_ident_eliminate_r_hashtag/struct.A.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
+struct A;
