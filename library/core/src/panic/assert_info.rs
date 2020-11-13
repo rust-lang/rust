7f8140e64aa84a24edf63ba0787bfa7ed70e107c
Maarten de Vries <maarten@de-vri.es>
2020-11-13 12:15:02 +0100

    Add more documentation to `AssertInfo` and related types.

--- library/core/src/panic/assert_info.rs
+++ library/core/src/panic/assert_info.rs
@@ -9,22 +9,41 @@ pub struct AssertInfo<'a> {
     pub message: Option<crate::fmt::Arguments<'a>>,
 }
 
+/// Details about the expression that failed an assertion.
 #[derive(Debug)]
 pub enum Assertion<'a> {
-    /// The assertion is a boolean assertion.
+    /// The failed assertion is a boolean expression.
+    ///
+    /// This variant is only used for expressions that can't be described more specifically
+    /// by another variant.
     Bool(BoolAssertion),
-    /// The assertion is a binary comparison assertion.
+
+    /// The failed assertion is a binary comparison expression.
+    ///
+    /// This is used by `assert_eq!()`, `assert_ne!()` and expressions like
+    /// `assert!(x > 10)`.
     Binary(BinaryAssertion<'a>),
 }
 
 /// Information about a failed boolean assertion.
+///
+/// The expression was asserted to be true, but it evaluated to false.
+///
+/// This struct is only used for assertion failures that can't be described more specifically
+/// by another assertion type.
 #[derive(Debug)]
 pub struct BoolAssertion {
-    /// The expression that was evaluated.
+    /// The expression that was evaluated to false.
     pub expr: &'static str,
 }
 
 /// Information about a failed binary comparison assertion.
+///
+/// The left expression was compared with the right expression using `op`,
+/// and the comparison evaluted to false.
+///
+/// This struct is used for `assert_eq!()`, `assert_ne!()` and expressions like
+/// `assert!(x > 10)`.
 #[derive(Debug)]
 pub struct BinaryAssertion<'a> {
     /// The operator used to compare left and right.
