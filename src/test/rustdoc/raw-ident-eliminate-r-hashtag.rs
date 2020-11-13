309d863e372227f9a3eed89e46cba0ff3b1ea8c8
Alexis Bourget <alexis.bourget@gmail.com>
2020-11-13 14:58:21 +0100

    Fix wrong XPath

--- src/test/rustdoc/raw-ident-eliminate-r-hashtag.rs
+++ src/test/rustdoc/raw-ident-eliminate-r-hashtag.rs
@@ -1,5 +1,7 @@
 // ignore-tidy-linelength
 
+#![crate_type="lib"]
+
 pub mod internal {
     // @has 'raw_ident_eliminate_r_hashtag/internal/struct.mod.html'
     pub struct r#mod;
@@ -8,13 +10,13 @@ pub mod internal {
     ///
     /// [name]: mod
     /// [other name]: crate::internal::mod
-    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
-    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'other name'
+    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//*a[@href="../../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
+    // @has 'raw_ident_eliminate_r_hashtag/internal/struct.B.html' '//*a[@href="../../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'other name'
     pub struct B;
 }
 
 /// See [name].
 ///
 /// [name]: internal::mod
-// @has 'raw_ident_eliminate_r_hashtag/struct.A.html' '//a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
-struct A;
+// @has 'raw_ident_eliminate_r_hashtag/struct.A.html' '//*a[@href="../raw_ident_eliminate_r_hashtag/internal/struct.mod.html"]' 'name'
+pub struct A;
