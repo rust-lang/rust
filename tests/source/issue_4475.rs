eed826f8a7f90406f43d1bccb3876bcf6ef9f85f
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-11-13 21:42:37 -0600

    tests: attributed comment-only blocks

---
+++ tests/source/issue_4475.rs
@@ -0,0 +1,27 @@
+fn main() {
+    #[cfg(debug_assertions)]
+    { println!("DEBUG"); }
+}
+
+fn main() {
+    #[cfg(feature = "foo")]
+    {
+        /*
+        let foo = 0
+        */
+    }
+}
+
+fn main() {
+    #[cfg(feature = "foo")]
+    { /* let foo = 0; */ }
+}
+
+fn main() {
+    #[foo]
+    #[bar]
+    #[baz]
+    {
+        // let foo = 0;
+    }
+}
