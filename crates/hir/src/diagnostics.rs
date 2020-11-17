c868f0255f677a4cedb62c27af96758fffdd6f8f
Lukas Wirth <lukastw97@gmail.com>
2020-11-17 16:14:45 +0100

    Fill the diagnostic code field in publish_diagnostics

--- crates/hir/src/diagnostics.rs
+++ crates/hir/src/diagnostics.rs
@@ -1,6 +1,8 @@
 //! FIXME: write short doc here
 pub use hir_def::diagnostics::{InactiveCode, UnresolvedModule};
-pub use hir_expand::diagnostics::{Diagnostic, DiagnosticSink, DiagnosticSinkBuilder};
+pub use hir_expand::diagnostics::{
+    Diagnostic, DiagnosticCode, DiagnosticSink, DiagnosticSinkBuilder,
+};
 pub use hir_ty::diagnostics::{
     IncorrectCase, MismatchedArgCount, MissingFields, MissingMatchArms, MissingOkInTailExpr,
     NoSuchField,
