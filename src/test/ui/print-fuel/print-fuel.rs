1e524fb36c0d1d20495b67ccb232f123d7a11891
cjkenn <cjkenn@users.noreply.github.com>
2020-11-18 11:27:37 -0500

    change print-fuel ui test to check-pass

--- src/test/ui/print-fuel/print-fuel.rs
+++ src/test/ui/print-fuel/print-fuel.rs
@@ -3,7 +3,7 @@
 
 // (#55495: The --error-format is to sidestep an issue in our test harness)
 // compile-flags: -C opt-level=0 --error-format human -Z print-fuel=foo
-// build-pass (FIXME(62277): could be check-pass?)
+// check-pass
 
 struct S1(u8, u16, u8);
 struct S2(u8, u16, u8);
