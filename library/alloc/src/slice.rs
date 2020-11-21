fe526f4b583e3d6d95d29f6e271812b35ed804ea
Mara Bos <m-ou.se@m-ou.se>
2020-11-21 11:49:19 +0100

    Avoid generating realloc call in []::to_vec.
    extend_from_slice already pre-allocates. Using with_capacity will end up
    with slightly worse code, as the `realloc` call doesn't get optimized
    away.

--- library/alloc/src/slice.rs
+++ library/alloc/src/slice.rs
@@ -156,7 +156,7 @@ mod hack {
     where
         T: Clone,
     {
-        let mut vec = Vec::with_capacity(s.len());
+        let mut vec = Vec::new();
         vec.extend_from_slice(s);
         vec
     }
