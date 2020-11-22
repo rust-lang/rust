2ee89144e21c10a95b411ff72235da6e29c04a94
Niko Matsakis <niko@alum.mit.edu>
2020-11-22 09:58:58 -0500

    introduce new fallback algorithm
    We now fallback type variables using the following rules:
    
    * Construct a coercion graph `A -> B` where `A` and `B` are unresolved
      type variables or the `!` type.
    * Let D be those variables that are reachable from `!`.
    * Let N be those variables that are reachable from a variable not in
    D.
    * All variables in (D \ N) fallback to `!`.
    * All variables in (D & N) fallback to `()`.

---
+++ src/test/ui/never_type/diverging-fallback-no-leak.rs
@@ -0,0 +1,15 @@
+#![feature(never_type_fallback)]
+
+fn make_unit() {}
+
+trait Test {}
+impl Test for i32 {}
+impl Test for () {}
+
+fn unconstrained_arg<T: Test>(_: T) {}
+
+fn main() {
+    // Here the type variable falls back to `!`,
+    // and hence we get a type error:
+    unconstrained_arg(return); //~ ERROR trait bound `!: Test` is not satisfied
+}
