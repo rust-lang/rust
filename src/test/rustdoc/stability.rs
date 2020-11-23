caf6c5790a858893c1d32ed2054c9577d12e7493
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-11-23 13:20:16 +0100

    Rename "stability" CSS class to "item-info"

--- src/test/rustdoc/stability.rs
+++ src/test/rustdoc/stability.rs
@@ -4,7 +4,7 @@
 
 pub struct Unstable {
     // @has stability/struct.Unstable.html \
-    //      '//div[@class="stability"]//div[@class="stab unstable"]' \
+    //      '//div[@class="item-info"]//div[@class="stab unstable"]' \
     //      'This is a nightly-only experimental API'
     // @count stability/struct.Unstable.html '//span[@class="stab unstable"]' 0
     pub foo: u32,
