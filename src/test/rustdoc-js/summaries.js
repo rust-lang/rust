f0cf5a974ec0730a512fa241dacf6e2c5659625f
Camelid <camelidcamel@gmail.com>
2020-11-24 11:18:45 -0800

    Add more rustdoc-js test cases

--- src/test/rustdoc-js/summaries.js
+++ src/test/rustdoc-js/summaries.js
@@ -1,7 +1,21 @@
-const QUERY = 'summaries';
+// ignore-tidy-linelength
 
-const EXPECTED = {
-    'others': [
-        { 'path': '', 'name': 'summaries', 'desc': 'This <em>summary</em> has a link and <code>code</code>.' },
-    ],
-};
+const QUERY = ['summaries', 'summaries::Sidebar', 'summaries::Sidebar2'];
+
+const EXPECTED = [
+    {
+        'others': [
+           { 'path': '', 'name': 'summaries', 'desc': 'This <em>summary</em> has a link and <code>code</code>.' },
+        ],
+    },
+    {
+        'others': [
+            { 'path': 'summaries', 'name': 'Sidebar', 'desc': 'This <code>code</code> will be rendered in a code tag.' },
+        ],
+    },
+    {
+        'others': [
+            { 'path': 'summaries', 'name': 'Sidebar2', 'desc': '' },
+        ],
+    },
+];
