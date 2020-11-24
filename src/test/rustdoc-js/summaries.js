b9035194c0c434d89ec776176a88406cc4af66cc
Camelid <camelidcamel@gmail.com>
2020-11-23 20:33:41 -0800

    Add rustdoc-js test
    Finally!

---
+++ src/test/rustdoc-js/summaries.js
@@ -0,0 +1,7 @@
+const QUERY = 'summaries';
+
+const EXPECTED = {
+    'others': [
+        { 'path': '', 'name': 'summaries', 'desc': 'This <em>summary</em> has a link and <code>code</code>.' },
+    ],
+};
