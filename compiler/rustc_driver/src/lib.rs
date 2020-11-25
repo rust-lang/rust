e54c0600b4ba25b0f7af582d9878bfd5c88736bc
flip1995 <philipp.krones@embecosm.com>
2020-11-25 13:16:36 +0100

    Reword message at the end of the output if not run with a lint tool

--- compiler/rustc_driver/src/lib.rs
+++ compiler/rustc_driver/src/lib.rs
@@ -961,7 +961,7 @@ Available lint options:
 
     match (loaded_plugins, plugin.len(), plugin_groups.len()) {
         (false, 0, _) | (false, _, 0) => {
-            println!("Compiler plugins can provide additional lints and lint groups.");
+            println!("Lint tools like Clippy can provide additional lints and lint groups.");
         }
         (false, ..) => panic!("didn't load lint plugins but got them anyway!"),
         (true, 0, 0) => println!("This crate does not load any lint plugins or lint groups."),
