0b64110b10c9ef5f971aab4b05975dee922a7bc3
Aaron Hill <aa1ronham@gmail.com>
2020-11-25 16:03:10 -0500

    Resolve inference variables before trying to remove overloaded indexing
    Fixes #79152
    
    This code was already set up to handle indexing an array. However, it
    appears that we never end up with an inference variable for the slice
    case, so the missing call to `resolve_vars_if_possible` had no effect
    until now.

---
+++ src/test/ui/consts/issue-79152-const-array-index.rs
@@ -0,0 +1,11 @@
+// check-pass
+// Regression test for issue #79152
+//
+// Tests that we can index an array in a const function
+
+const fn foo() {
+    let mut array = [[0; 1]; 1];
+    array[0][0] = 1;
+}
+
+fn main() {}
