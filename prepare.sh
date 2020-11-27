5988bbd24aa87732bfa1d111ba00bcdaa22c481a
bjorn3 <bjorn3@users.noreply.github.com>
2020-11-27 18:05:05 +0100

    Revert "Use the new component dependency option of the rust-toolchain file"
    This reverts commit 648caced6eb0d23c31758f69f2f44a7d748526b9.
    
    Rustup on github actions isn't yet updated

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --verbose
 set -e
 
+rustup component add rust-src rustc-dev llvm-tools-preview
 ./build_sysroot/prepare_sysroot_src.sh
 cargo install hyperfine || echo "Skipping hyperfine install"
 
