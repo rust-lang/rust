04cd4b17bb04031ecf7c7e9b734d22ea74d8f114
Kirill Bulatov <mail4score@gmail.com>
2020-11-27 18:13:02 +0200

    Fix the profiling string
    Co-authored-by: Laurențiu Nicola <lnicola@users.noreply.github.com>

--- crates/assists/src/utils/insert_use.rs
+++ crates/assists/src/utils/insert_use.rs
@@ -95,7 +95,7 @@ pub fn insert_use<'a>(
     path: ast::Path,
     merge: Option<MergeBehaviour>,
 ) -> SyntaxRewriter<'a> {
-    let _p = profile::span("mod_path_to_ast");
+    let _p = profile::span("insert_use");
     let mut rewriter = SyntaxRewriter::default();
     let use_item = make::use_(make::use_tree(path.clone(), None, None, false));
     // merge into existing imports if possible
