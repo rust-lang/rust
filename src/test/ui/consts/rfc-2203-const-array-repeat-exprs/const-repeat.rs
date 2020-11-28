7f3e18cc2b24214e10df5434972f4f7cd461fa98
Ralf Jung <post@ralfj.de>
2020-11-28 17:32:48 +0100

    make sure [CONST; N] drops N times

--- src/test/ui/consts/rfc-2203-const-array-repeat-exprs/const-repeat.rs
+++ src/test/ui/consts/rfc-2203-const-array-repeat-exprs/const-repeat.rs
@@ -1,4 +1,4 @@
-// check-pass
+// run-pass
 
 // Repeating a *constant* of non-Copy type (not just a constant expression) is already stable.
 
@@ -8,6 +8,20 @@ pub fn bar() -> [Vec<i32>; 2] {
     [EMPTY; 2]
 }
 
+struct Bomb;
+
+impl Drop for Bomb {
+    fn drop(&mut self) {
+        panic!("BOOM!");
+    }
+}
+
+const BOOM: Bomb = Bomb;
+
 fn main() {
-    let x = bar();
+    let _x = bar();
+
+    // Make sure the destructor does not get called for empty arrays. `[CONST; N]` should
+    // instantiate (and then later drop) the const exactly `N` times.
+    let _x = [BOOM; 0];
 }
