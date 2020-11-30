0e207888391fb8b55fa75d19259812b6cb97a75c
Cameron Steffen <cam.steffen94@gmail.com>
2020-11-29 18:21:21 -0600

    Split tests

--- tests/ui/collapsible_match.rs
+++ tests/ui/collapsible_match.rs
@@ -95,45 +95,6 @@ fn lint_cases(opt_opt: Option<Option<u32>>, res_opt: Result<Option<u32>, String>
         },
         None => return,
     }
-
-    // if guards on outer match
-    {
-        match res_opt {
-            Ok(val) if make() => match val {
-                Some(n) => foo(n),
-                _ => return,
-            },
-            _ => return,
-        }
-        match res_opt {
-            Ok(val) => match val {
-                Some(n) => foo(n),
-                _ => return,
-            },
-            _ if make() => return,
-            _ => return,
-        }
-    }
-
-    // macro
-    {
-        macro_rules! mac {
-            ($outer:expr => $pat:pat, $e:expr => $inner_pat:pat, $then:expr) => {
-                match $outer {
-                    $pat => match $e {
-                        $inner_pat => $then,
-                        _ => return,
-                    },
-                    _ => return,
-                }
-            };
-        }
-        // Lint this since the patterns are not defined by the macro.
-        // Allows the lint to work on if_chain! for example.
-        // Fixing the lint requires knowledge of the specific macro, but we optimistically assume that
-        // there is still a better way to write this.
-        mac!(res_opt => Ok(val), val => Some(n), foo(n));
-    }
 }
 
 fn negative_cases(res_opt: Result<Option<u32>, String>, res_res: Result<Result<u32, String>, String>) {
