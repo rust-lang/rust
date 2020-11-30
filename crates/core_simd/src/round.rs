3d9bbf9b86f9f41d9ea5fe649343f3d763003866
Jubilee Young <workingjubilee@gmail.com>
2020-11-30 12:52:30 -0800

    Use simd_{floor,ceil} intrinsics for round.rs

--- crates/core_simd/src/round.rs
+++ crates/core_simd/src/round.rs
@@ -1,60 +1,23 @@
 macro_rules! implement {
     {
         impl $type:ident {
-            int_type = $int_type:ident,
-            floor = $floor_intrinsic:literal,
-            ceil = $ceil_intrinsic:literal,
-            round = $round_intrinsic:literal,
-            trunc = $trunc_intrinsic:literal,
+            int_type = $int_type:ident
         }
     } => {
         mod $type {
-            #[allow(improper_ctypes)]
-            extern "C" {
-                #[link_name = $floor_intrinsic]
-                fn floor_intrinsic(x: crate::$type) -> crate::$type;
-                #[link_name = $ceil_intrinsic]
-                fn ceil_intrinsic(x: crate::$type) -> crate::$type;
-                #[link_name = $round_intrinsic]
-                fn round_intrinsic(x: crate::$type) -> crate::$type;
-                #[link_name = $trunc_intrinsic]
-                fn trunc_intrinsic(x: crate::$type) -> crate::$type;
-            }
-
             impl crate::$type {
                 /// Returns the largest integer less than or equal to each lane.
                 #[must_use = "method returns a new vector and does not mutate the original value"]
                 #[inline]
                 pub fn floor(self) -> Self {
-                    unsafe { floor_intrinsic(self) }
+                    unsafe { crate::intrinsics::simd_floor(self) }
                 }
 
                 /// Returns the smallest integer greater than or equal to each lane.
                 #[must_use = "method returns a new vector and does not mutate the original value"]
                 #[inline]
                 pub fn ceil(self) -> Self {
-                    unsafe { ceil_intrinsic(self) }
-                }
-
-                /// Returns the nearest integer to each lane. Round half-way cases away from 0.0.
-                #[must_use = "method returns a new vector and does not mutate the original value"]
-                #[inline]
-                pub fn round(self) -> Self {
-                    unsafe { round_intrinsic(self) }
-                }
-
-                /// Returns the integer part of each lane.
-                #[must_use = "method returns a new vector and does not mutate the original value"]
-                #[inline]
-                pub fn trunc(self) -> Self {
-                    unsafe { trunc_intrinsic(self) }
-                }
-
-                /// Returns the fractional part of each lane.
-                #[must_use = "method returns a new vector and does not mutate the original value"]
-                #[inline]
-                pub fn fract(self) -> Self {
-                    self - self.trunc()
+                    unsafe { crate::intrinsics::simd_ceil(self) }
                 }
 
                 /// Rounds toward zero and converts to the same-width integer type, assuming that
@@ -84,70 +47,42 @@ macro_rules! implement {
 
 implement! {
     impl f32x2 {
-        int_type = i32x2,
-        floor = "llvm.floor.v2f32",
-        ceil = "llvm.ceil.v2f32",
-        round = "llvm.round.v2f32",
-        trunc = "llvm.trunc.v2f32",
+        int_type = i32x2
     }
 }
 
 implement! {
     impl f32x4 {
-        int_type = i32x4,
-        floor = "llvm.floor.v4f32",
-        ceil = "llvm.ceil.v4f32",
-        round = "llvm.round.v4f32",
-        trunc = "llvm.trunc.v4f32",
+        int_type = i32x4
     }
 }
 
 implement! {
     impl f32x8 {
-        int_type = i32x8,
-        floor = "llvm.floor.v8f32",
-        ceil = "llvm.ceil.v8f32",
-        round = "llvm.round.v8f32",
-        trunc = "llvm.trunc.v8f32",
+        int_type = i32x8
     }
 }
 
 implement! {
     impl f32x16 {
-        int_type = i32x16,
-        floor = "llvm.floor.v16f32",
-        ceil = "llvm.ceil.v16f32",
-        round = "llvm.round.v16f32",
-        trunc = "llvm.trunc.v16f32",
+        int_type = i32x16
     }
 }
 
 implement! {
     impl f64x2 {
-        int_type = i64x2,
-        floor = "llvm.floor.v2f64",
-        ceil = "llvm.ceil.v2f64",
-        round = "llvm.round.v2f64",
-        trunc = "llvm.trunc.v2f64",
+        int_type = i64x2
     }
 }
 
 implement! {
     impl f64x4 {
-        int_type = i64x4,
-        floor = "llvm.floor.v4f64",
-        ceil = "llvm.ceil.v4f64",
-        round = "llvm.round.v4f64",
-        trunc = "llvm.trunc.v4f64",
+        int_type = i64x4
     }
 }
 
 implement! {
     impl f64x8 {
-        int_type = i64x8,
-        floor = "llvm.floor.v8f64",
-        ceil = "llvm.ceil.v8f64",
-        round = "llvm.round.v8f64",
-        trunc = "llvm.trunc.v8f64",
+        int_type = i64x8
     }
 }
