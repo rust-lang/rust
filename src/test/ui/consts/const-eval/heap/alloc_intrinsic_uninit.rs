528355c541b2f5d54d91a642355e39e01d93c75f
Vishnunarayan K I <appukuttancr@gmail.com>
2020-12-01 15:39:25 +0530

    add const_allocate intrisic

---
+++ src/test/ui/consts/const-eval/heap/alloc_intrinsic_uninit.rs
@@ -0,0 +1,10 @@
+// compile-test
+#![feature(core_intrinsics)]
+#![feature(const_heap)]
+#![feature(const_raw_ptr_deref)]
+#![feature(const_mut_refs)]
+use std::intrinsics;
+
+const BAR: &i32 = unsafe { &*(intrinsics::const_allocate(4, 4) as *mut i32) };
+//~^ error: it is undefined behavior to use this value
+fn main() {}
