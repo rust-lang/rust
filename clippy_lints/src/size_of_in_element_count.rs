c1a5329475d041dbeb077ecda6ae71f690b4bcc1
unknown <abramlujan@gmail.com>
2020-11-30 21:54:50 -0300

    Add more functions to size_of_in_element_count Specifically ptr::{sub, wrapping_sub, add, wrapping_add, offset, wrapping_offset} and slice::{from_raw_parts, from_raw_parts_mut} The lint now also looks for size_of calls through casts (Since offset takes an isize)

--- clippy_lints/src/size_of_in_element_count.rs
+++ clippy_lints/src/size_of_in_element_count.rs
@@ -54,31 +54,40 @@ fn get_size_of_ty(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) -> Option<Ty<'tc
         ExprKind::Binary(op, left, right) if BinOpKind::Mul == op.node || BinOpKind::Div == op.node => {
             get_size_of_ty(cx, left).or_else(|| get_size_of_ty(cx, right))
         },
+        ExprKind::Cast(expr, _) => get_size_of_ty(cx, expr),
         _ => None,
     }
 }
 
-const FUNCTIONS: [[&str; 3]; 6] = [
-    paths::COPY_NONOVERLAPPING,
-    paths::COPY,
-    paths::WRITE_BYTES,
-    paths::PTR_SWAP_NONOVERLAPPING,
-    paths::PTR_SLICE_FROM_RAW_PARTS,
-    paths::PTR_SLICE_FROM_RAW_PARTS_MUT,
-    ];
-const METHODS: [&str; 5] = [
-    "write_bytes",
-    "copy_to",
-    "copy_from",
-    "copy_to_nonoverlapping",
-    "copy_from_nonoverlapping",
-    ];
 fn get_pointee_ty_and_count_expr(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) -> Option<(Ty<'tcx>, &'tcx Expr<'tcx>)> {
+    const FUNCTIONS: [&[&str]; 8] = [
+        &paths::COPY_NONOVERLAPPING,
+        &paths::COPY,
+        &paths::WRITE_BYTES,
+        &paths::PTR_SWAP_NONOVERLAPPING,
+        &paths::PTR_SLICE_FROM_RAW_PARTS,
+        &paths::PTR_SLICE_FROM_RAW_PARTS_MUT,
+        &paths::SLICE_FROM_RAW_PARTS,
+        &paths::SLICE_FROM_RAW_PARTS_MUT,
+    ];
+    const METHODS: [&str; 11] = [
+        "write_bytes",
+        "copy_to",
+        "copy_from",
+        "copy_to_nonoverlapping",
+        "copy_from_nonoverlapping",
+        "add",
+        "wrapping_add",
+        "sub",
+        "wrapping_sub",
+        "offset",
+        "wrapping_offset",
+    ];
+
     if_chain! {
         // Find calls to ptr::{copy, copy_nonoverlapping}
         // and ptr::{swap_nonoverlapping, write_bytes},
-        if let ExprKind::Call(func, args) = expr.kind;
-        if let [.., count] = args;
+        if let ExprKind::Call(func, [.., count]) = expr.kind;
         if let ExprKind::Path(ref func_qpath) = func.kind;
         if let Some(def_id) = cx.qpath_res(func_qpath, func.hir_id).opt_def_id();
         if FUNCTIONS.iter().any(|func_path| match_def_path(cx, def_id, func_path));
@@ -91,13 +100,12 @@ fn get_pointee_ty_and_count_expr(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>) -
     };
     if_chain! {
         // Find calls to copy_{from,to}{,_nonoverlapping} and write_bytes methods
-        if let ExprKind::MethodCall(method_path, _, args, _) = expr.kind;
-        if let [ptr_self, _, count] = args;
+        if let ExprKind::MethodCall(method_path, _, [ptr_self, .., count], _) = expr.kind;
         let method_ident = method_path.ident.as_str();
         if METHODS.iter().any(|m| *m == &*method_ident);
 
         // Get the pointee type
-        if let ty::RawPtr(TypeAndMut { ty: pointee_ty, mutbl:_mutability }) =
+        if let ty::RawPtr(TypeAndMut { ty: pointee_ty, .. }) =
             cx.typeck_results().expr_ty(ptr_self).kind();
         then {
             return Some((pointee_ty, count));
@@ -115,7 +123,7 @@ impl<'tcx> LateLintPass<'tcx> for SizeOfInElementCount {
              instead of a count of elements of T";
 
         if_chain! {
-            // Find calls to unsafe copy functions and get
+            // Find calls to functions with an element count parameter and get
             // the pointee type and count parameter expression
             if let Some((pointee_ty, count_expr)) = get_pointee_ty_and_count_expr(cx, expr);
 
