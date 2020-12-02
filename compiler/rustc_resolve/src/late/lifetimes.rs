cdcce115043a19b2bbc154e56b59fa2f5017bbbc
Yuki Okushi <huyuumi.dev@gmail.com>
2020-12-02 11:39:42 +0900

    Tweak diagnostics on shadowing lifetimes/labels

--- compiler/rustc_resolve/src/late/lifetimes.rs
+++ compiler/rustc_resolve/src/late/lifetimes.rs
@@ -1098,7 +1098,7 @@ fn signal_shadowing_problem(tcx: TyCtxt<'_>, name: Symbol, orig: Original, shado
         )
     };
     err.span_label(orig.span, "first declared here");
-    err.span_label(shadower.span, format!("lifetime {} already in scope", name));
+    err.span_label(shadower.span, format!("{} `{}` already in scope", orig.kind.desc(), name));
     err.emit();
 }
 
