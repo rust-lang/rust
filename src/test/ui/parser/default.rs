21c157442c1e97745b91b8c3286ceabbc9f7be3f
Ivan Tham <pickfire@riseup.net>
2020-12-08 00:32:13 +0800

    Add pub as optional check_front_matter
    async-pub check created a regression for default

--- src/test/ui/parser/default.rs
+++ src/test/ui/parser/default.rs
@@ -1,4 +1,3 @@
-// ignore-tidy-linelength
 // Test successful and unsuccessful parsing of the `default` contextual keyword
 
 #![feature(specialization)]
@@ -22,7 +21,8 @@ impl Foo for u16 {
 
 impl Foo for u32 { //~ ERROR not all trait items implemented, missing: `foo`
     default pub fn foo<T: Default>() -> T { T::default() }
-    //~^ ERROR expected one of `async`, `const`, `extern`, `fn`, `pub`, `unsafe`, or `use`, found keyword `pub`
+    //~^ ERROR `default` is not followed by an item
+    //~| ERROR non-item in item list
 }
 
 fn main() {}
