a87ce05e1f539b242ee1baa28df891ff1700cb1f
Jennifer Wills <wills.jenniferg@gmail.com>
2020-12-07 23:57:30 -0500

    bless tests

--- src/test/ui/borrowck/borrowck-closures-unique.rs
+++ src/test/ui/borrowck/borrowck-closures-unique.rs
@@ -5,6 +5,7 @@
 // multiple borrows.
 
 
+#![feature(rustc_attrs)]
 
 fn get(x: &isize) -> isize {
     *x
@@ -29,7 +30,7 @@ fn b(x: &mut isize) {
 
 fn c(x: &mut isize) {
     let c1 = || get(x);
-    let c2 = || { get(x); set(x); }; //~ ERROR closure requires unique access to `x`
+    let c2 = #[rustc_capture_analysis] || { get(x); set(x); }; //~ ERROR closure requires unique access to `x`
     c1;
 }
 
