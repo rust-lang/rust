35006eba79b9a445c4ebec66f1a93b3170398e0e
ivan770 <leshenko.ivan770@gmail.com>
2020-12-09 08:22:13 +0000

    Apply rustfmt changes

--- crates/hir_ty/src/diagnostics/expr.rs
+++ crates/hir_ty/src/diagnostics/expr.rs
@@ -334,11 +334,12 @@ impl<'a, 'b> ExprValidator<'a, 'b> {
             None => return,
         };
 
-        let possible_tail_ty = if let Some(possible_tail_ty) = self.infer.type_of_expr.get(possible_tail_id) {
-            possible_tail_ty
-        } else {
-            return;
-        };
+        let possible_tail_ty =
+            if let Some(possible_tail_ty) = self.infer.type_of_expr.get(possible_tail_id) {
+                possible_tail_ty
+            } else {
+                return;
+            };
 
         if mismatch.actual != Ty::unit() || mismatch.expected != *possible_tail_ty {
             return;
@@ -346,10 +347,8 @@ impl<'a, 'b> ExprValidator<'a, 'b> {
 
         let (_, source_map) = db.body_with_source_map(self.owner.into());
         if let Ok(source_ptr) = source_map.expr_syntax(possible_tail_id) {
-            self.sink.push(RemoveThisSemicolon {
-                file: source_ptr.file_id,
-                expr: source_ptr.value,
-            });
+            self.sink
+                .push(RemoveThisSemicolon { file: source_ptr.file_id, expr: source_ptr.value });
         }
     }
 }
