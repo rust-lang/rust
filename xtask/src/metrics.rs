75543d8ca19515cbcf0a3090150fafebc41bb249
Jonas Schievink <jonasschievink@gmail.com>
2020-12-11 16:50:17 +0100

    Revert "Fix memory usage metrics"

--- xtask/src/metrics.rs
+++ xtask/src/metrics.rs
@@ -81,9 +81,7 @@ impl Metrics {
     }
     fn measure_analysis_stats_path(&mut self, name: &str, path: &str) -> Result<()> {
         eprintln!("\nMeasuring analysis-stats/{}", name);
-        let output =
-            cmd!("./target/release/rust-analyzer analysis-stats --quiet --memory-usage {path}")
-                .read()?;
+        let output = cmd!("./target/release/rust-analyzer analysis-stats --quiet {path}").read()?;
         for (metric, value, unit) in parse_metrics(&output) {
             self.report(&format!("analysis-stats/{}/{}", name, metric), value, unit.into());
         }
