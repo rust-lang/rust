5ce3f4c16636b261a8ce9ddfbbb30896338b9e37
Camelid <camelidcamel@gmail.com>
2020-12-11 19:52:51 -0800

    Resolve enum field visibility correctly
    Previously, this code treated enum fields' visibility as if they were
    struct fields. However, that's not correct because the visibility of a
    struct field with `ast::VisibilityKind::Inherited` is private to the
    module it's defined in, whereas the visibility of an *enum* field with
    `ast::VisibilityKind::Inherited` is the visibility of the enum it
    belongs to.

---
+++ src/test/ui/issues/issue-79593.rs
@@ -0,0 +1,29 @@
+mod foo {
+    pub struct Pub { private: () }
+
+    pub enum Enum {
+        Variant { x: (), y: () },
+        Other
+    }
+
+    fn correct() {
+        Pub {};
+        //~^ ERROR missing field `private` in initializer of `Pub`
+        Enum::Variant { x: () };
+        //~^ ERROR missing field `y` in initializer of `Enum`
+    }
+}
+
+fn correct() {
+    foo::Pub {};
+    //~^ ERROR cannot construct `Pub` with struct literal syntax due to inaccessible fields
+}
+
+fn wrong() {
+    foo::Enum::Variant { x: () };
+    //~^ ERROR missing field `y` in initializer of `Enum`
+    foo::Enum::Variant { };
+    //~^ ERROR missing fields `x`, `y` in initializer of `Enum`
+}
+
+fn main() {}
