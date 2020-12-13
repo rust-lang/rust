9d7eae2ca4ec87d1b27dce8b34c6bb68cffe857a
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2020-12-13 16:13:03 +0800

    Update rc.rs

--- library/alloc/src/rc.rs
+++ library/alloc/src/rc.rs
@@ -743,12 +743,10 @@ impl<T: ?Sized> Rc<T> {
         unsafe { Self::from_ptr(rc_ptr) }
     }
     
-    /// Obtains a new reference two a raw [`Rc`] pointer, without taking ownership.
+    /// Obtains a new reference to a raw [`Rc`] pointer, without taking ownership.
     #[unstable(feature = "rc_clone_raw", issue = "48108")]
     pub unsafe fn clone_raw(ptr: *const T) -> Self {
-        let result = unsafe {
-            Rc::from_raw(ptr)
-        };
+        let result = unsafe { Rc::from_raw(ptr) };
         
         forget(result.clone());
         
