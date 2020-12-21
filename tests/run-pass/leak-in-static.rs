3d151c8b62dbb9b4234387fcd6e4a6eb87a07ef0
Ralf Jung <post@ralfj.de>
2020-12-21 11:33:46 +0100

    leak checker: also test AtomicPtr stored via 'swap'

--- tests/run-pass/leak-in-static.rs
+++ tests/run-pass/leak-in-static.rs
@@ -12,5 +12,9 @@ fn main() {
     {
         static LEAK: AtomicPtr<usize> = AtomicPtr::new(ptr::null_mut());
         LEAK.store(Box::into_raw(Box::new(0usize)), Ordering::SeqCst);
+
+        static LEAK2: AtomicPtr<usize> = AtomicPtr::new(ptr::null_mut());
+        // Make sure this also works when using 'swap'.
+        LEAK2.swap(Box::into_raw(Box::new(0usize)), Ordering::SeqCst);
     }
 }
