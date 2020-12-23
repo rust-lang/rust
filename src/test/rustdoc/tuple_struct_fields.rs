bf0fd2f649f1917f24a17c85a67b4a21ad330dc0
rslint <rdambrosio016@gmail.com>
2020-12-22 22:25:17 -0500

    Chore: test for no rendering if the field is not public

--- src/test/rustdoc/tuple_struct_fields.rs
+++ src/test/rustdoc/tuple_struct_fields.rs
@@ -1,9 +1,11 @@
 // @has tuple_struct_fields/struct.Tooople.html
 // @has - //span '0: usize'
 // @has - 'Wow! i love tuple fields!'
+// @!has - 'I should be invisible'
 
 pub struct Tooople(
     /// Wow! i love tuple fields!
     pub usize,
+    /// I should be invisible
     u8,
 );
