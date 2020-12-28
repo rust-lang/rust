2c13c68de5dc4d47877069cf65b1f60a5962fe71
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-28 23:07:20 +0100

    New rustdoc lint to respect -Dwarnings correctly
    This adds a new lint to `rustc` that is used in rustdoc when a code
    block is empty or cannot be parsed as valid Rust code.
    
    Previously this was unconditionally a warning. As such some
    documentation comments were (unknowingly) abusing this to pass despite
    the `-Dwarnings` used when compiling `rustc`, this should not be the
    case anymore.

--- compiler/rustc_typeck/src/check/upvar.rs
+++ compiler/rustc_typeck/src/check/upvar.rs
@@ -320,7 +320,7 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
     ///
     /// InferBorrowKind results in a structure like this:
     ///
-    /// ```
+    /// ```text
     /// {
     ///       Place(base: hir_id_s, projections: [], ....) -> {
     ///                                                            capture_kind_expr: hir_id_L5,
@@ -345,7 +345,7 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
     /// ```
     ///
     /// After the min capture analysis, we get:
-    /// ```
+    /// ```text
     /// {
     ///       hir_id_s -> [
     ///            Place(base: hir_id_s, projections: [], ....) -> {
