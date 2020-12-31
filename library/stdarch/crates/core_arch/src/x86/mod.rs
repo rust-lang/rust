bd91c5e9cf31d5a6dd0ee4d9bf713c304195ffa5
tmiasko <tomasz.miasko@gmail.com>
2020-12-31 17:21:46 +0100

    Remove no longer necessary bootstrap guards (#975)

--- library/stdarch/crates/core_arch/src/x86/mod.rs
+++ library/stdarch/crates/core_arch/src/x86/mod.rs
@@ -675,27 +675,19 @@ pub use self::avx512cd::*;
 mod avx512ifma;
 pub use self::avx512ifma::*;
 
-#[cfg(not(bootstrap))]
 mod avx512bitalg;
-#[cfg(not(bootstrap))]
 pub use self::avx512bitalg::*;
 
-#[cfg(not(bootstrap))]
 mod avx512gfni;
-#[cfg(not(bootstrap))]
 pub use self::avx512gfni::*;
 
 mod avx512vpopcntdq;
 pub use self::avx512vpopcntdq::*;
 
-#[cfg(not(bootstrap))]
 mod avx512vaes;
-#[cfg(not(bootstrap))]
 pub use self::avx512vaes::*;
 
-#[cfg(not(bootstrap))]
 mod avx512vpclmulqdq;
-#[cfg(not(bootstrap))]
 pub use self::avx512vpclmulqdq::*;
 
 mod bt;
