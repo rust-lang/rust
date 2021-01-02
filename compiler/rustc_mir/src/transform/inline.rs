e4aa99fe7afaabf846d9c67dc68364e7661a0bde
Rich Kadel <richkadel@google.com>
2021-01-02 09:40:15 -0800

    Inlining enabled by -mir-opt-level > 1 is incompatible with coverage
    Fixes: #80060
    
    Also adds additional test cases for coverage of doctests.

--- compiler/rustc_mir/src/transform/inline.rs
+++ compiler/rustc_mir/src/transform/inline.rs
@@ -41,6 +41,15 @@ impl<'tcx> MirPass<'tcx> for Inline {
             return;
         }
 
+        if tcx.sess.opts.debugging_opts.instrument_coverage {
+            // Since `Inline` happens after `InstrumentCoverage`, the function-specific coverage
+            // counters can be invalidated, such as by merging coverage counter statements from
+            // a pre-inlined function into a different function. This kind of change is invalid,
+            // so inlining must be skipped. Note: This check is performed here so inlining can
+            // be disabled without preventing other optimizations (regardless of `mir_opt_level`).
+            return;
+        }
+
         if inline(tcx, body) {
             debug!("running simplify cfg on {:?}", body.source);
             CfgSimplifier::new(body).simplify();
