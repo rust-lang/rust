e33a205bdfb0fbbbad80f49e4d39180dc78087bf
max-heller <max.a.heller@gmail.com>
2021-01-03 20:34:07 -0500

    primitive disambiguator tests

---
+++ src/test/rustdoc/intra-doc/incompatible-primitive-disambiguator.rs
@@ -0,0 +1,2 @@
+#![deny(broken_intra_doc_links)]
+//! [struct@str::trim] //~ ERROR incompatible link
---
+++ src/test/rustdoc/intra-doc/primitive-disambiguators.rs
@@ -0,0 +1,4 @@
+#![deny(broken_intra_doc_links)]
+// @has primitive_disambiguators/index.html
+// @has - '//a/@href' 'https://doc.rust-lang.org/nightly/std/primitive.str.html#method.trim'
+//! [str::trim()]
