a35655621a7a7c0d41093e177855f46bce75fa30
Ryan Levick <ryan.levick@gmail.com>
2021-01-08 12:09:29 +0100

    Fix tidy errors

--- src/test/ui/lint/noop-method-call.rs
+++ src/test/ui/lint/noop-method-call.rs
@@ -21,19 +21,22 @@ impl<T> Deref for DerefExample<T> {
 
 fn main() {
     let foo = &Foo(1u32);
-    let foo_clone: &Foo<u32> = foo.clone(); //~ WARNING call to method that does nothing [noop_method_call]
+    let foo_clone: &Foo<u32> = foo.clone();
+    //~^ WARNING call to method that does nothing [noop_method_call]
 
     let bar = &Bar(1u32);
     let bar_clone: Bar<u32> = bar.clone();
 
     let deref = &&DerefExample(12u32);
-    let derefed: &DerefExample<u32> = deref.deref(); //~ WARNING call to method that does nothing [noop_method_call]
+    let derefed: &DerefExample<u32> = deref.deref();
+    //~^ WARNING call to method that does nothing [noop_method_call]
 
     let deref = &DerefExample(12u32);
     let derefed: &u32 = deref.deref();
 
     let a = &&Foo(1u32);
-    let borrowed: &Foo<u32> = a.borrow(); //~ WARNING call to method that does nothing [noop_method_call]
+    let borrowed: &Foo<u32> = a.borrow();
+    //~^ WARNING call to method that does nothing [noop_method_call]
 }
 
 fn generic<T>(foo: &Foo<T>) {
