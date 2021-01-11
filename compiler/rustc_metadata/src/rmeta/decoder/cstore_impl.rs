8e7cbc28a65df0e979b6052db7f4f8d26fecd9d9
Tyson Nottingham <tgnottingham@gmail.com>
2021-01-11 13:31:43 -0800

    Prevent potential bug resulting from changing crate_hash query name

--- compiler/rustc_metadata/src/rmeta/decoder/cstore_impl.rs
+++ compiler/rustc_metadata/src/rmeta/decoder/cstore_impl.rs
@@ -47,7 +47,8 @@ macro_rules! provide {
                 // External query providers call `crate_hash` in order to register a dependency
                 // on the crate metadata. The exception is `crate_hash` itself, which obviously
                 // doesn't need to do this (and can't, as it would cause a query cycle).
-                if stringify!($name) != "crate_hash" && $tcx.dep_graph.is_fully_enabled() {
+                use rustc_middle::dep_graph::DepKind;
+                if DepKind::$name != DepKind::crate_hash && $tcx.dep_graph.is_fully_enabled() {
                     $tcx.ensure().crate_hash($def_id.krate);
                 }
 
