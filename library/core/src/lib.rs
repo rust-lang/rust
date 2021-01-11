ff1cfbe3b7f1b7ce4fd60b312bdd4e3599d3db9c
Folyd <lyshuhow@gmail.com>
2021-01-11 12:41:33 +0800

    Add iter_at_least and iter_at_most feature flags to core/lib.rs

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -165,6 +165,8 @@
 #![feature(no_niche)] // rust-lang/rust#68303
 #![feature(unsafe_block_in_unsafe_fn)]
 #![feature(int_error_matching)]
+#![feature(iter_at_least)]
+#![feature(iter_at_most)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
 #[prelude_import]
