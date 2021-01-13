8b184ff1b8106bb88d3cf0cc6c54eaa5b30b3a5a
Tomasz Miąsko <tomasz.miasko@gmail.com>
2021-01-13 00:00:00 +0000

    Remove storage markers if they won't be used during code generation
    The storage markers constitute a substantial portion of all MIR
    statements. At the same time, for builds without any optimizations,
    the storage markers have no further use during and after MIR
    optimization phase.
    
    If storage markers are not necessary for code generation, remove them.

---
+++ src/test/mir-opt/remove_storage_markers.rs
@@ -0,0 +1,11 @@
+// Checks that storage markers are removed at opt-level=0.
+//
+// compile-flags: -C opt-level=0 -Coverflow-checks=off
+
+// EMIT_MIR remove_storage_markers.main.RemoveStorageMarkers.diff
+fn main() {
+    let mut sum = 0;
+    for i in 0..10 {
+        sum += i;
+    }
+}
