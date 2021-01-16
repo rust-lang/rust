6bcaba9f51cc345435cb8fdf085fecda7eeeed61
Scott McMurray <scottmcm@users.noreply.github.com>
2021-01-16 14:52:48 -0800

    Try ignore-debug in the codegen test
    This fixed things the last time I had a problem like this.  And plausibly will here too -- the check it's failing on is for the high bit being set in the length of the slice, which is a check that's only in a debug_assert.

--- src/test/codegen/slice-as_chunks.rs
+++ src/test/codegen/slice-as_chunks.rs
@@ -1,6 +1,7 @@
 // no-system-llvm
 // compile-flags: -O
 // only-64bit (because the LLVM type of i64 for usize shows up)
+// ignore-debug: the debug assertions get in the way
 
 #![crate_type = "lib"]
 #![feature(slice_as_chunks)]
