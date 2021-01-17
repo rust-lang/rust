9949d9e417ff563c3cf7fbadb3ae6d865ab08c53
hyd-dev <yd-huang@outlook.com>
2021-01-17 23:09:04 +0800

    Remove `#![feature(const_generics)]` and `#![allow(incomplete_features)]`

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,6 @@
 #![warn(rust_2018_idioms)]
 #![allow(clippy::cast_lossless)]
 
-#![allow(incomplete_features)]
-#![feature(const_generics)]
-
 extern crate rustc_attr;
 extern crate rustc_apfloat;
 extern crate rustc_ast;
