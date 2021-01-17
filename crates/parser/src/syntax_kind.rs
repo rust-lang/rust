b97447b12546c8c87defca997ab0489a004b9bf8
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-01-17 16:50:03 +0300

    Add missing inlines

--- crates/parser/src/syntax_kind.rs
+++ crates/parser/src/syntax_kind.rs
@@ -6,6 +6,7 @@ mod generated;
 pub use self::generated::SyntaxKind;
 
 impl From<u16> for SyntaxKind {
+    #[inline]
     fn from(d: u16) -> SyntaxKind {
         assert!(d <= (SyntaxKind::__LAST as u16));
         unsafe { std::mem::transmute::<u16, SyntaxKind>(d) }
@@ -13,12 +14,14 @@ impl From<u16> for SyntaxKind {
 }
 
 impl From<SyntaxKind> for u16 {
+    #[inline]
     fn from(k: SyntaxKind) -> u16 {
         k as u16
     }
 }
 
 impl SyntaxKind {
+    #[inline]
     pub fn is_trivia(self) -> bool {
         matches!(self, SyntaxKind::WHITESPACE | SyntaxKind::COMMENT)
     }
