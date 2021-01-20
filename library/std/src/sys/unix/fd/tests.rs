456baa0cc9a0212e821724a1004111f215940b34
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2021-01-20 16:29:26 -0500

    Fix file descriptor ranges for fuchsia
    Fuchsia does not restrict the range of valid values for a file descriptor, so
    the previous change that marked -1 as an invalid file descriptor does not
    apply to fuchsia.

--- library/std/src/sys/unix/fd/tests.rs
+++ library/std/src/sys/unix/fd/tests.rs
@@ -3,7 +3,16 @@ use core::mem::ManuallyDrop;
 
 #[test]
 fn limit_vector_count() {
-    let stdout = ManuallyDrop::new(unsafe { FileDesc { fd: 1 } });
+    let stdout = ManuallyDrop::new(
+        #[cfg(not(target_os = "fuchsia"))]
+        {
+            unsafe { FileDesc { fd: 1 } }
+        },
+        #[cfg(target_os = "fuchsia")]
+        {
+            FileDesc { fd: 1 }
+        },
+    );
     let bufs = (0..1500).map(|_| IoSlice::new(&[])).collect::<Vec<_>>();
     assert!(stdout.write_vectored(&bufs).is_ok());
 }
