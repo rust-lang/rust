d0c1405564024d109ec22b130584d74fcb82d153
Lzu Tao <taolzu@gmail.com>
2021-01-21 01:42:37 +0000

    Document why cannot use concat! in dbg!
    Co-authored-by: Miguel Ojeda <ojeda@users.noreply.github.com>

--- library/std/src/macros.rs
+++ library/std/src/macros.rs
@@ -282,6 +282,10 @@ macro_rules! eprintln {
 #[macro_export]
 #[stable(feature = "dbg_macro", since = "1.32.0")]
 macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `eprintln!`
+    // will be malformed.
     () => {
         $crate::eprintln!("[{}:{}]", $crate::file!(), $crate::line!());
     };
