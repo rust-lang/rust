c37c4213543a727870ca862d25446b3c66d8e399
Rune Tynan <runetynan@gmail.com>
2021-01-23 23:17:31 -0500

    fmt

--- src/tools/jsondocck/src/main.rs
+++ src/tools/jsondocck/src/main.rs
@@ -149,15 +149,17 @@ fn get_commands(template: &str) -> Result<Vec<Command>, ()> {
             }
         }
 
-        let args = cap.name("args")
-            .map_or(Some(vec![]), |m| shlex::split(m.as_str()));
+        let args = cap.name("args").map_or(Some(vec![]), |m| shlex::split(m.as_str()));
 
         let args = match args {
             Some(args) => args,
             None => {
                 print_err(
-                    &format!("Invalid arguments to shlex::split: `{}`", cap.name("args").unwrap().as_str()),
-                    lineno
+                    &format!(
+                        "Invalid arguments to shlex::split: `{}`",
+                        cap.name("args").unwrap().as_str()
+                    ),
+                    lineno,
                 );
                 errors = true;
                 continue;
