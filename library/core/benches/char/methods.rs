8acb5665072f499effeb77eb2c60fd9cf74a2955
Miccah Castorina <m.castorina93@gmail.com>
2021-01-24 20:29:44 -0600

    Add to_lowercase and to_uppercase char benchmarks

--- library/core/benches/char/methods.rs
+++ library/core/benches/char/methods.rs
@@ -45,3 +45,13 @@ fn bench_to_ascii_uppercase(b: &mut Bencher) {
 fn bench_to_ascii_lowercase(b: &mut Bencher) {
     b.iter(|| CHARS.iter().cycle().take(10_000).map(|c| c.to_ascii_lowercase()).min())
 }
+
+#[bench]
+fn bench_char_to_uppercase(b: &mut Bencher) {
+    b.iter(|| (0..=255).cycle().take(10_000).map(|b| char::from(b).to_uppercase()).count())
+}
+
+#[bench]
+fn bench_char_to_lowercase(b: &mut Bencher) {
+    b.iter(|| (0..=255).cycle().take(10_000).map(|b| char::from(b).to_lowercase()).count())
+}
