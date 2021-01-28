eaefe4a230d7c32d7c99da437ee8b87860f28704
Dan Aloni <alonid@gmail.com>
2021-01-28 18:01:36 +0200

    path trimming: ignore type aliases

--- src/test/ui/issues/issue-21332.rs
+++ src/test/ui/issues/issue-21332.rs
@@ -4,7 +4,7 @@ impl Iterator for S {
     type Item = i32;
     fn next(&mut self) -> Result<i32, i32> { Ok(7) }
     //~^ ERROR method `next` has an incompatible type for trait
-    //~| expected enum `Option`, found enum `std::result::Result`
+    //~| expected enum `Option`, found enum `Result`
 }
 
 fn main() {}
