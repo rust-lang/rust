a9abf6f08663dce7b69cac826ca943149a6b093b
Hirochika Matsumoto <matsujika@gmail.com>
2021-01-29 16:28:53 +0900

    Add test for match expression

--- src/test/ui/suggestions/field-access.rs
+++ src/test/ui/suggestions/field-access.rs
@@ -12,4 +12,12 @@ fn main() {
     if let B::Fst = a {};
     //~^ ERROR mismatched types [E0308]
     // note: you might have meant to use field `b` of type `B`
+    match a {
+        B::Fst => (),
+        B::Snd => (),
+    }
+    //~^^^ ERROR mismatched types [E0308]
+    // note: you might have meant to use field `b` of type `B`
+    //~^^^^ ERROR mismatched types [E0308]
+    // note: you might have meant to use field `b` of type `B`
 }
