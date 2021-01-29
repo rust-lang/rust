5e983d7b3f03e9243d905e0579f32be00170c9af
Dhruv Jauhar <dhruvjhr@gmail.com>
2021-01-28 23:22:49 -0500

    Add a test for syntax like: ..t.s

--- src/test/ui/closures/2229_closure_analysis/run_pass/fru_syntax.rs
+++ src/test/ui/closures/2229_closure_analysis/run_pass/fru_syntax.rs
@@ -8,22 +8,38 @@
 //~| NOTE: `#[warn(incomplete_features)]` on by default
 //~| NOTE: see issue #53488 <https://github.com/rust-lang/rust/issues/53488>
 
+#[derive(Clone)]
 struct S {
     a: String,
     b: String,
 }
 
+struct T {
+    a: String,
+    s: S,
+}
+
 fn main() {
     let a = String::new();
     let b = String::new();
+    let c = String::new();
     let s = S {a, b};
+    let t = T {
+        a: c,
+        s: s.clone()
+    };
 
     let c = || {
         let s2 = S {
-            a: format!("New a"),
+            a: format!("New s2"),
             ..s
         };
+        let s3 = S {
+            a: format!("New s3"),
+            ..t.s
+        };
         println!("{} {}", s2.a, s2.b);
+        println!("{} {} {}", s3.a, s3.b, t.a);
     };
 
     c();
