b763d9a40c526f5f72c0668d26f69241e7a42511
Nikita Popov <nikita.ppv@gmail.com>
2021-02-03 18:11:28 +0100

    Build both Python 2 and Python 3 on x86 dist builders
    Python 2 is needed for Clang 10, Python 3 for LLVM 12.
    
    The Python 2 dependency could be removed by upgrading to Clang 11,
    but that causes linker errors of unclear origin.

--- src/ci/docker/host-x86_64/dist-x86_64-linux/build-python.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/build-python.sh
@@ -3,7 +3,8 @@
 set -ex
 source shared.sh
 
-curl https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz | \
+VERSION=$1
+curl https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz | \
   tar xzf -
 
 mkdir python-build
@@ -12,10 +13,10 @@ cd python-build
 # Gotta do some hackery to tell python about our custom OpenSSL build, but other
 # than that fairly normal.
 CFLAGS='-I /rustroot/include' LDFLAGS='-L /rustroot/lib -L /rustroot/lib64' \
-    hide_output ../Python-2.7.12/configure --prefix=/rustroot
+    hide_output ../Python-$VERSION/configure --prefix=/rustroot
 hide_output make -j10
 hide_output make install
 
 cd ..
 rm -rf python-build
-rm -rf Python-2.7.12
+rm -rf Python-$VERSION
