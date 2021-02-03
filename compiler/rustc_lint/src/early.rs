37cb9d30fa457cd56e775175e684a0763bf4a3cb
Jethro Beekman <jethro@fortanix.com>
2021-02-03 09:40:59 +0100

    Really fix early lints inside an async desugaring

--- compiler/rustc_lint/src/early.rs
+++ compiler/rustc_lint/src/early.rs
@@ -219,10 +219,10 @@ impl<'a, T: EarlyLintPass> ast_visit::Visitor<'a> for EarlyContextAndPass<'a, T>
 
         // Explicitly check for lints associated with 'closure_id', since
         // it does not have a corresponding AST node
-        if let ast::ExprKind::Closure(_, asyncness, ..) = e.kind {
-            if let ast::Async::Yes { closure_id, .. } = asyncness {
-                self.check_id(closure_id);
-            }
+        match e.kind {
+            ast::ExprKind::Closure(_, ast::Async::Yes { closure_id, .. }, ..)
+            | ast::ExprKind::Async(_, closure_id, ..) => self.check_id(closure_id),
+            _ => {}
         }
     }
 
