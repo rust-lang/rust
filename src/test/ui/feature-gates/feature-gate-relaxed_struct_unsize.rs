031cce8cfc7fef922989e8b820da236ee17e016a
Bastian Kauschke <bastian_kauschke@hotmail.de>
2021-02-03 23:56:47 +0100

    add `relaxed_struct_unsize` feature gate

--- src/test/ui/feature-gates/feature-gate-relaxed_struct_unsize.rs
+++ src/test/ui/feature-gates/feature-gate-relaxed_struct_unsize.rs
@@ -1,5 +1,3 @@
-#![feature(relaxed_struct_unsize)]
-// run-pass
 // Test that we allow unsizing even if there is an unchanged param in the
 // field getting unsized.
 struct A<T, U: ?Sized + 'static>(T, B<T, U>);
@@ -7,6 +5,6 @@ struct B<T, U: ?Sized>(T, U);
 
 fn main() {
     let x: A<[u32; 1], [u32; 1]> = A([0; 1], B([0; 1], [0; 1]));
-    let y: &A<[u32; 1], [u32]> = &x;
+    let y: &A<[u32; 1], [u32]> = &x; //~ ERROR mismatched types
     assert_eq!(y.1.1.len(), 1);
 }
