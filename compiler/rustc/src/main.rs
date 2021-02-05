6fc64b2ba6650a0aef60ef3048977ed81eb30c2c
jq-rs <jq-rs@mles.io>
2021-02-05 12:49:03 +0200

    [WIP]  Swap jemalloc to mimalloc
    * Swap jemalloc to mimalloc
    * Fix aligned alloc.
    * Bump libmimalloc-sys.
    * Include lock-file.

--- compiler/rustc/src/main.rs
+++ compiler/rustc/src/main.rs
@@ -7,23 +7,23 @@ fn main() {
     // dynamic libraries. That means to pull in jemalloc we need to actually
     // reference allocation symbols one way or another (as this file is the only
     // object code in the rustc executable).
-    #[cfg(feature = "jemalloc-sys")]
+    #[cfg(feature = "libmimalloc-sys")]
     {
         use std::os::raw::{c_int, c_void};
 
         #[used]
-        static _F1: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::calloc;
+        static _F1: unsafe extern "C" fn(usize, usize) -> *mut c_void = libmimalloc_sys::mi_calloc;
         #[used]
         static _F2: unsafe extern "C" fn(*mut *mut c_void, usize, usize) -> c_int =
-            jemalloc_sys::posix_memalign;
+            libmimalloc_sys::mi_posix_memalign;
         #[used]
-        static _F3: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::aligned_alloc;
+        static _F3: unsafe extern "C" fn(usize, usize) -> *mut c_void = libmimalloc_sys::mi_aligned_alloc;
         #[used]
-        static _F4: unsafe extern "C" fn(usize) -> *mut c_void = jemalloc_sys::malloc;
+        static _F4: unsafe extern "C" fn(usize) -> *mut c_void = libmimalloc_sys::mi_malloc;
         #[used]
-        static _F5: unsafe extern "C" fn(*mut c_void, usize) -> *mut c_void = jemalloc_sys::realloc;
+        static _F5: unsafe extern "C" fn(*mut c_void, usize) -> *mut c_void = libmimalloc_sys::mi_realloc;
         #[used]
-        static _F6: unsafe extern "C" fn(*mut c_void) = jemalloc_sys::free;
+        static _F6: unsafe extern "C" fn(*mut c_void) = libmimalloc_sys::mi_free;
     }
 
     rustc_driver::set_sigpipe_handler();
