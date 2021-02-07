7b92a55ff700019908c137493435c12d50a2d8e0
Gowri <gowri.sankar@aligent.com.au>
2021-02-08 09:57:16 +1030

    fix: add for keyword in completion #7588

--- crates/completion/src/completions/keyword.rs
+++ crates/completion/src/completions/keyword.rs
@@ -88,6 +88,7 @@ pub(crate) fn complete_expr_keyword(acc: &mut Completions, ctx: &CompletionConte
         add_keyword(ctx, acc, "loop", "loop {$0}");
         add_keyword(ctx, acc, "if", "if $0 {}");
         add_keyword(ctx, acc, "if let", "if let $1 = $0 {}");
+        add_keyword(ctx, acc, "for", "for $0 in $1 {}");
     }
 
     if ctx.if_is_prev || ctx.block_expr_parent {
