1d35960fe1898dd370c360bbf60ca4fa89bbf93d
Santiago Pastorino <spastorino@gmail.com>
2021-02-07 01:44:29 -0300

    Create ui test for -Ztreat-err-as-bug delay_span_bug

---
+++ src/test/ui/treat-err-as-bug/delay_span_bug.rs
@@ -0,0 +1,11 @@
+// compile-flags: -Ztreat-err-as-bug
+// failure-status: 101
+// error-pattern: aborting due to `-Z treat-err-as-bug=1`
+// error-pattern: [trigger_delay_span_bug] trigger a delay span bug
+// normalize-stderr-test "note: .*\n\n" -> ""
+// normalize-stderr-test "thread 'rustc' panicked.*\n" -> ""
+
+#![feature(rustc_attrs)]
+
+#[rustc_error(delay_span_bug_from_inside_query)]
+fn main() {}
