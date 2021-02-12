67fb96c537993673a47ee91f073523e6778809b3
Joshua Nelson <jyn514@gmail.com>
2021-02-11 19:15:16 -0500

    Fix private intra-doc warnings on associated items
    The issue was that the `kind, id` override was previously only being
    considered for the disambiguator check, not the privacy check. This uses
    the same ID for both.

--- src/test/rustdoc-ui/intra-doc/private.rs
+++ src/test/rustdoc-ui/intra-doc/private.rs
@@ -2,8 +2,13 @@
 // revisions: public private
 // [private]compile-flags: --document-private-items
 
-/// docs [DontDocMe]
+/// docs [DontDocMe] [DontDocMe::f]
 //~^ WARNING public documentation for `DocMe` links to private item `DontDocMe`
+//~| WARNING public documentation for `DocMe` links to private item `DontDocMe::f`
 // FIXME: for [private] we should also make sure the link was actually generated
 pub struct DocMe;
 struct DontDocMe;
+
+impl DontDocMe {
+    fn f() {}
+}
