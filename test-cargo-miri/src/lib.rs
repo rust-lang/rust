51c6f51c36b95420d9748b0d9d372ccbbaafc93d
hyd-dev <yd-huang@outlook.com>
2021-02-14 22:38:56 +0800

    Use the `issue-1567` dependency in the test

--- test-cargo-miri/src/lib.rs
+++ test-cargo-miri/src/lib.rs
@@ -3,6 +3,7 @@
 /// assert!(cargo_miri_test::make_true());
 /// ```
 pub fn make_true() -> bool {
+    issue_1567::use_the_dependency();
     issue_1705::use_the_dependency();
     issue_1691::use_me()
 }
