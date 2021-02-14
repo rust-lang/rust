8c6b267d4481d8f17893221297c61ed17a43449d
Ralf Jung <post@ralfj.de>
2021-02-14 17:57:29 +0100

    use min_const_generics to avoid incomplete_features (#976)

--- library/stdarch/crates/core_arch/src/lib.rs
+++ library/stdarch/crates/core_arch/src/lib.rs
@@ -2,13 +2,11 @@
 #![allow(improper_ctypes_definitions)]
 #![allow(dead_code)]
 #![allow(unused_features)]
-#![allow(incomplete_features)]
 #![feature(
     asm,
     const_fn,
     const_fn_union,
     const_fn_transmute,
-    const_generics,
     custom_inner_attributes,
     link_llvm_intrinsics,
     platform_intrinsics,
