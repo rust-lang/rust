0cc6d2e6ad3c2a82f3726ddfbb6f7cf1d39a1835
Michael Howell <michael@notriddle.com>
2021-02-14 01:32:31 -0700

    Add test case for local macro (which is different from foreign crate macros)

--- src/test/ui/typeck/issue-81943.rs
+++ src/test/ui/typeck/issue-81943.rs
@@ -2,4 +2,8 @@ fn f<F: Fn(i32)>(f: F) { f(0); }
 fn main() {
   f(|x| dbg!(x)); //~ERROR
   f(|x| match x { tmp => { tmp } }); //~ERROR
+  macro_rules! d {
+    ($e:expr) => { match $e { x => { x } } }
+  }
+  f(|x| d!(x)); //~ERROR
 }
