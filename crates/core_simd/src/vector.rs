27f094f5ee216b499ee9e666a7f6bdba361443f9
Jubilee Young <workingjubilee@gmail.com>
2021-02-16 17:31:44 -0800

    Nominate base files

--- crates/core_simd/src/vector.rs
+++ crates/core_simd/src/vector.rs
@@ -1,29 +1,31 @@
-mod vectors_f32;
 mod vectors_f64;
 mod vectors_i128;
 mod vectors_i16;
 mod vectors_i32;
 mod vectors_i64;
 mod vectors_i8;
-mod vectors_isize;
 mod vectors_u128;
 mod vectors_u16;
 mod vectors_u32;
 mod vectors_u64;
 mod vectors_u8;
-mod vectors_usize;
 
-pub use vectors_f32::*;
 pub use vectors_f64::*;
 pub use vectors_i128::*;
 pub use vectors_i16::*;
 pub use vectors_i32::*;
 pub use vectors_i64::*;
 pub use vectors_i8::*;
-pub use vectors_isize::*;
 pub use vectors_u128::*;
 pub use vectors_u16::*;
 pub use vectors_u32::*;
 pub use vectors_u64::*;
 pub use vectors_u8::*;
-pub use vectors_usize::*;
+
+mod float;
+mod int;
+mod uint;
+
+pub use float::*;
+pub use int::*;
+pub use uint::*;
