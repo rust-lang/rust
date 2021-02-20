01b74c5b43a3e738661245739730bc6cca1f1692
Scott McMurray <scottmcm@users.noreply.github.com>
2021-02-19 23:27:24 -0800

    Update the UI tests to the current outputs
    Thank you to whomever got the error messages using the exported names ♥

--- src/test/ui/try-block/try-block-bad-type.rs
+++ src/test/ui/try-block/try-block-bad-type.rs
@@ -4,7 +4,7 @@
 
 pub fn main() {
     let res: Result<u32, std::array::TryFromSliceError> = try {
-        Err("")?; //~ ERROR the trait bound `std::array::TryFromSliceError: From<&str>` is not satisfied
+        Err("")?; //~ ERROR `?` couldn't convert the error to `TryFromSliceError`
         5
     };
 
@@ -15,7 +15,7 @@ pub fn main() {
     let res: Result<i32, i32> = try { }; //~ ERROR type mismatch
 
     let res: () = try { };
-    //~^ ERROR the trait bound `(): Try2021` is not satisfied
+    //~^ ERROR the trait bound `(): Try` is not satisfied
 
-    let res: i32 = try { 5 }; //~ ERROR the trait bound `i32: Try2021` is not satisfied
+    let res: i32 = try { 5 }; //~ ERROR the trait bound `i32: Try` is not satisfied
 }
