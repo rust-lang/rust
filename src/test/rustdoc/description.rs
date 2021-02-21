dcf49916e4fac986b602f345b60b712cd536494d
Michael Howell <michael@notriddle.com>
2021-02-20 17:50:01 -0700

    Fix formatting for description rustdoc UI tests

--- src/test/rustdoc/description.rs
+++ src/test/rustdoc/description.rs
@@ -1,11 +1,14 @@
 #![crate_name = "foo"]
 //! # Description test crate
 //!
-//! This is the contents of the test crate docstring. It should not show up in the description.
+//! This is the contents of the test crate docstring.
+//! It should not show up in the description.
 
-// @matches 'foo/index.html' '//meta[@name="description"]/@content' 'Description test crate'
+// @matches 'foo/index.html' '//meta[@name="description"]/@content' \
+//   'Description test crate'
 
-// @matches 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' 'First paragraph description.'
+// @matches 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' \
+//   'First paragraph description.'
 /// First paragraph description.
 ///
 /// Second paragraph should not show up.
@@ -13,7 +16,7 @@ pub mod foo_mod {
     pub struct __Thing {}
 }
 
-// @matches 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' 'Only paragraph.'
+// @matches 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' \
+//   'Only paragraph.'
 /// Only paragraph.
 pub fn foo_fn() {}
-
--- src/test/rustdoc/description_default.rs
+++ src/test/rustdoc/description_default.rs
@@ -1,12 +1,14 @@
 #![crate_name = "foo"]
 
-// @matches 'foo/index.html' '//meta[@name="description"]/@content' 'API documentation for the Rust `foo` crate.'
+// @matches 'foo/index.html' '//meta[@name="description"]/@content' \
+//   'API documentation for the Rust `foo` crate.'
 
-// @matches 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' 'API documentation for the Rust `foo_mod` mod in crate `foo`.'
+// @matches 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' \
+//   'API documentation for the Rust `foo_mod` mod in crate `foo`.'
 pub mod foo_mod {
     pub struct __Thing {}
 }
 
-// @matches 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' 'API documentation for the Rust `foo_fn` fn in crate `foo`.'
+// @matches 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' \
+//   'API documentation for the Rust `foo_fn` fn in crate `foo`.'
 pub fn foo_fn() {}
-
