6008a657f877b25f72e54bdf6ae09405205c7c0f
Valentin Churavy <v.churavy@gmail.com>
2021-02-21 16:37:00 -0500

    make the next Enzyme tag 1.6 only

--- .packaging/build_tarballs.jl
+++ .packaging/build_tarballs.jl
@@ -42,11 +42,11 @@ products = Product[
 ]
 
 dependencies = [
-    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"9.0.1")),
+    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"11.0.1")),
 #    Dependency(PackageSpec(name="libLLVM_jll", version=v"9.0.1"))
 ]
 
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version=v"8", julia_compat="1.5")
+               preferred_gcc_version=v"8", julia_compat="1.6")
