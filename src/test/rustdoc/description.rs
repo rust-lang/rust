fd5a710092fe1e7668c55999c12104c122da732a
Michael Howell <michael@notriddle.com>
2021-02-20 17:51:41 -0700

    Use has for non-regexes

--- src/test/rustdoc/description.rs
+++ src/test/rustdoc/description.rs
@@ -4,10 +4,10 @@
 //! This is the contents of the test crate docstring.
 //! It should not show up in the description.
 
-// @matches 'foo/index.html' '//meta[@name="description"]/@content' \
+// @has 'foo/index.html' '//meta[@name="description"]/@content' \
 //   'Description test crate'
 
-// @matches 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' \
+// @has 'foo/foo_mod/index.html' '//meta[@name="description"]/@content' \
 //   'First paragraph description.'
 /// First paragraph description.
 ///
@@ -16,7 +16,7 @@ pub mod foo_mod {
     pub struct __Thing {}
 }
 
-// @matches 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' \
+// @has 'foo/fn.foo_fn.html' '//meta[@name="description"]/@content' \
 //   'Only paragraph.'
 /// Only paragraph.
 pub fn foo_fn() {}
