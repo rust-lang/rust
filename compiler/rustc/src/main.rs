ebc9533d085fd377286a27c6ce9c956897d28983
jq-rs <jq-rs@mles.io>
2021-02-22 09:34:21 +0200

    Jemalloc updated to tikv-jemalloc-sys for reference.

--- compiler/rustc/src/main.rs
+++ compiler/rustc/src/main.rs
@@ -7,23 +7,25 @@ fn main() {
     // dynamic libraries. That means to pull in jemalloc we need to actually
     // reference allocation symbols one way or another (as this file is the only
     // object code in the rustc executable).
-    #[cfg(feature = "jemalloc-sys")]
+    #[cfg(feature = "tikv-jemalloc-sys")]
     {
         use std::os::raw::{c_int, c_void};
 
         #[used]
-        static _F1: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::calloc;
+        static _F1: unsafe extern "C" fn(usize, usize) -> *mut c_void = tikv_jemalloc_sys::calloc;
         #[used]
         static _F2: unsafe extern "C" fn(*mut *mut c_void, usize, usize) -> c_int =
-            jemalloc_sys::posix_memalign;
+            tikv_jemalloc_sys::posix_memalign;
         #[used]
-        static _F3: unsafe extern "C" fn(usize, usize) -> *mut c_void = jemalloc_sys::aligned_alloc;
+        static _F3: unsafe extern "C" fn(usize, usize) -> *mut c_void =
+            tikv_jemalloc_sys::aligned_alloc;
         #[used]
-        static _F4: unsafe extern "C" fn(usize) -> *mut c_void = jemalloc_sys::malloc;
+        static _F4: unsafe extern "C" fn(usize) -> *mut c_void = tikv_jemalloc_sys::malloc;
         #[used]
-        static _F5: unsafe extern "C" fn(*mut c_void, usize) -> *mut c_void = jemalloc_sys::realloc;
+        static _F5: unsafe extern "C" fn(*mut c_void, usize) -> *mut c_void =
+            tikv_jemalloc_sys::realloc;
         #[used]
-        static _F6: unsafe extern "C" fn(*mut c_void) = jemalloc_sys::free;
+        static _F6: unsafe extern "C" fn(*mut c_void) = tikv_jemalloc_sys::free;
     }
 
     rustc_driver::set_sigpipe_handler();
