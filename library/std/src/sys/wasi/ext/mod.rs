132ec261b0d215f77b71eeee3d814cd304fc1a29
Dan Gohman <dev@sunfishcode.online>
2021-02-22 14:21:43 -0800

    Enable API documentation for `std::os::wasi`.
    This adds API documentation support for `std::os::wasi` modeled after
    how `std::os::unix` works, so that WASI can be documented [here] along
    with the other platforms.
    
    [here]: https://doc.rust-lang.org/stable/std/os/index.html
    
    Two changes of particular interest:
    
     - This changes the `AsRawFd` for `io::Stdin` for WASI to return
       `libc::STDIN_FILENO` instead of `sys::stdio::Stdin.as_raw_fd()` (and
       similar for `Stdout` and `Stderr`), which matches how the `unix`
       version works. `STDIN_FILENO` etc. may not always be explicitly
       reserved at the WASI level, but as long as we have Rust's `std` and
       `libc`, I think it's reasonable to guarantee that we'll always use
       `libc::STDIN_FILENO` for stdin.
    
     - This duplicates the `osstr2str` utility function, rather than
       trying to share it across all the configurations that need it.

--- library/std/src/sys/wasi/ext/mod.rs
+++ library/std/src/sys/wasi/ext/mod.rs
@@ -1,4 +1,32 @@
+//! Platform-specific extensions to `std` for WASI.
+//!
+//! Provides access to platform-level information on WASI, and exposes
+//! WASI-specific functions that would otherwise be inappropriate as
+//! part of the core `std` library.
+//!
+//! It exposes more ways to deal with platform-specific strings (`OsStr`,
+//! `OsString`), allows to set permissions more granularly, extract low-level
+//! file descriptors from files and sockets, and has platform-specific helpers
+//! for spawning processes.
+//!
+//! # Examples
+//!
+//! ```no_run
+//! use std::fs::File;
+//! use std::os::wasi::prelude::*;
+//!
+//! fn main() -> std::io::Result<()> {
+//!     let f = File::create("foo.txt")?;
+//!     let fd = f.as_raw_fd();
+//!
+//!     // use fd with native WASI bindings
+//!
+//!     Ok(())
+//! }
+//! ```
+
 #![deny(unsafe_op_in_unsafe_fn)]
+#![doc(cfg(target_os = "wasi"))]
 
 pub mod ffi;
 pub mod fs;
