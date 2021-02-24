da60303a8e3335239ed3ec7ae66a2c9eda37ec61
Lzu Tao <taolzu@gmail.com>
2021-02-24 09:54:27 +0000

    Add codegen test for array comparision opt

---
+++ src/test/assembly/62531-array-cmp.rs
@@ -0,0 +1,21 @@
+// Test to make sure that asm for array comparison on x86 optimized.
+
+// min-llvm-version: 11
+// assembly-output: emit-asm
+// compile-flags: -C opt-level=2
+// only-x86_64
+
+#![crate_type = "lib"]
+
+// CHECK-LABEL: compare
+// CHECK: movb $1, %al
+// CHECK-NEXT: retq
+#[no_mangle]
+pub fn compare() -> bool {
+    let bytes = 12.5f32.to_ne_bytes();
+    bytes == if cfg!(target_endian = "big") {
+        [0x41, 0x48, 0x00, 0x00]
+    } else {
+        [0x00, 0x00, 0x48, 0x41]
+    }
+}
