c41347cbc62095d112cfd1b5901f0bccdee0ffc0
Ariadne Conill <ariadne@dereferenced.org>
2021-02-26 17:47:37 +0000

    spec: dynmusl: unset CRT-related options

--- compiler/rustc_target/src/spec/i586_unknown_linux_dynmusl.rs
+++ compiler/rustc_target/src/spec/i586_unknown_linux_dynmusl.rs
@@ -5,6 +5,9 @@ pub fn target() -> Target {
 
     base.llvm_target = "i586-unknown-linux-musl".to_string();
     base.options.crt_static_default = false;
+    base.options.pre_link_objects_fallback = None;
+    base.options.post_link_objects_fallback = None;
+    base.options.crt_objects_fallback = None;
 
     base
 }
