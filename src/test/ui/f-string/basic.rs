200f3b6c19909159720cee90e7212eedd90b01a4
Callum Tolley <cgtrolley@gmail.com>
2021-02-28 14:45:49 +0000

    Change basic test

--- src/test/ui/f-string/basic.rs
+++ src/test/ui/f-string/basic.rs
@@ -3,6 +3,6 @@
 pub fn main() {
     let a = 2;
     let b = 4;
-    let _c = f"12{34}56";//f"a ({a}) + b ({b}) = {a + b}";
+    let _c = f"a ({a}) + b ({b}) = {a + b}";
     assert_eq!(_c, "a (2) + b (4) = 6");
 }
