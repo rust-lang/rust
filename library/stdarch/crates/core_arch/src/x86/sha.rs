93143cddb24458fce41291b5f966999d59f715c3
Rémy Rakic <remy.rakic+github@gmail.com>
2021-03-04 21:31:46 +0100

    convert `_mm_sha1rnds4_epu32` to const generics

--- library/stdarch/crates/core_arch/src/x86/sha.rs
+++ library/stdarch/crates/core_arch/src/x86/sha.rs
@@ -66,25 +66,18 @@ pub unsafe fn _mm_sha1nexte_epu32(a: __m128i, b: __m128i) -> __m128i {
 /// Performs four rounds of SHA1 operation using an initial SHA1 state (A,B,C,D)
 /// from `a` and some pre-computed sum of the next 4 round message values
 /// (unsigned 32-bit integers), and state variable E from `b`, and return the
-/// updated SHA1 state (A,B,C,D). `func` contains the logic functions and round
+/// updated SHA1 state (A,B,C,D). `FUNC` contains the logic functions and round
 /// constants.
 ///
 /// [Intel's documentation](https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=_mm_sha1rnds4_epu32)
 #[inline]
 #[target_feature(enable = "sha")]
-#[cfg_attr(test, assert_instr(sha1rnds4, func = 0))]
-#[rustc_args_required_const(2)]
+#[cfg_attr(test, assert_instr(sha1rnds4, FUNC = 0))]
+#[rustc_legacy_const_generics(2)]
 #[stable(feature = "simd_x86", since = "1.27.0")]
-pub unsafe fn _mm_sha1rnds4_epu32(a: __m128i, b: __m128i, func: i32) -> __m128i {
-    let a = a.as_i32x4();
-    let b = b.as_i32x4();
-    macro_rules! call {
-        ($imm2:expr) => {
-            sha1rnds4(a, b, $imm2)
-        };
-    }
-    let ret = constify_imm2!(func, call);
-    transmute(ret)
+pub unsafe fn _mm_sha1rnds4_epu32<const FUNC: i32>(a: __m128i, b: __m128i) -> __m128i {
+    static_assert_imm2!(FUNC);
+    transmute(sha1rnds4(a.as_i32x4(), b.as_i32x4(), FUNC as i8))
 }
 
 /// Performs an intermediate calculation for the next four SHA256 message values
@@ -179,19 +172,19 @@ mod tests {
         let a = _mm_set_epi64x(0xe9b5dba5b5c0fbcf, 0x71374491428a2f98);
         let b = _mm_set_epi64x(0xab1c5ed5923f82a4, 0x59f111f13956c25b);
         let expected = _mm_set_epi64x(0x32b13cd8322f5268, 0xc54420862bd9246f);
-        let r = _mm_sha1rnds4_epu32(a, b, 0);
+        let r = _mm_sha1rnds4_epu32::<0>(a, b);
         assert_eq_m128i(r, expected);
 
         let expected = _mm_set_epi64x(0x6d4c43e56a3c25d9, 0xa7e00fb775cbd3fe);
-        let r = _mm_sha1rnds4_epu32(a, b, 1);
+        let r = _mm_sha1rnds4_epu32::<1>(a, b);
         assert_eq_m128i(r, expected);
 
         let expected = _mm_set_epi64x(0xb304e383c01222f4, 0x66f6b3b1f89d8001);
-        let r = _mm_sha1rnds4_epu32(a, b, 2);
+        let r = _mm_sha1rnds4_epu32::<2>(a, b);
         assert_eq_m128i(r, expected);
 
         let expected = _mm_set_epi64x(0x8189b758bfabfa79, 0xdb08f6e78cae098b);
-        let r = _mm_sha1rnds4_epu32(a, b, 3);
+        let r = _mm_sha1rnds4_epu32::<3>(a, b);
         assert_eq_m128i(r, expected);
     }
 
