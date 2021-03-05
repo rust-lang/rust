13201ca35b363a8778e20e082dde4af04ef84e92
Rémy Rakic <remy.rakic+github@gmail.com>
2021-03-06 00:44:26 +0100

    convert `_mm_mask_i32gather_ps` to const generics

--- library/stdarch/crates/core_arch/src/x86/avx2.rs
+++ library/stdarch/crates/core_arch/src/x86/avx2.rs
@@ -1203,24 +1203,19 @@ pub unsafe fn _mm_i32gather_ps<const SCALE: i32>(slice: *const f32, offsets: __m
 /// [Intel's documentation](https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=_mm_mask_i32gather_ps)
 #[inline]
 #[target_feature(enable = "avx2")]
-#[cfg_attr(test, assert_instr(vgatherdps, scale = 1))]
-#[rustc_args_required_const(4)]
+#[cfg_attr(test, assert_instr(vgatherdps, SCALE = 1))]
+#[rustc_legacy_const_generics(4)]
 #[stable(feature = "simd_x86", since = "1.27.0")]
-pub unsafe fn _mm_mask_i32gather_ps(
+pub unsafe fn _mm_mask_i32gather_ps<const SCALE: i32>(
     src: __m128,
     slice: *const f32,
     offsets: __m128i,
     mask: __m128,
-    scale: i32,
 ) -> __m128 {
+    static_assert_imm8_scale!(SCALE);
     let offsets = offsets.as_i32x4();
     let slice = slice as *const i8;
-    macro_rules! call {
-        ($imm8:expr) => {
-            pgatherdps(src, slice, offsets, mask, $imm8)
-        };
-    }
-    constify_imm8_gather!(scale, call)
+    pgatherdps(src, slice, offsets, mask, SCALE as i8)
 }
 
 /// Returns values from `slice` at offsets determined by `offsets * scale`,
@@ -5627,12 +5622,11 @@ mod tests {
             j += 1.0;
         }
         // A multiplier of 4 is word-addressing for f32s
-        let r = _mm_mask_i32gather_ps(
+        let r = _mm_mask_i32gather_ps::<4>(
             _mm_set1_ps(256.0),
             arr.as_ptr(),
             _mm_setr_epi32(0, 16, 64, 96),
             _mm_setr_ps(-1.0, -1.0, -1.0, 0.0),
-            4,
         );
         assert_eq_m128(r, _mm_setr_ps(0.0, 16.0, 64.0, 256.0));
     }
