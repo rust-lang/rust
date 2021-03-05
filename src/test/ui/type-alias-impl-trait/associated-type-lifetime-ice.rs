55533011377c648f41467d23a882bdd84779ab16
Oli Scherer <github35764891676564198441@oli-obk.de>
2021-03-05 11:16:37 +0000

    Make regression test succeed as long as it ICEs

--- src/test/ui/type-alias-impl-trait/associated-type-lifetime-ice.rs
+++ src/test/ui/type-alias-impl-trait/associated-type-lifetime-ice.rs
@@ -1,4 +1,8 @@
-// check-pass
+// failure-status: 101
+// rustc-env:RUST_BACKTRACE=0
+// normalize-stderr-test "note: .*\n\n" -> ""
+// normalize-stderr-test "thread 'rustc' panicked.*\n" -> ""
+
 // compile-flags: --crate-type=rlib
 
 // Regression test for https://github.com/rust-lang/rust/issues/78450
@@ -22,7 +26,7 @@ trait Associated {
 impl<'a, T: Associated<A = &'a ()>> AssociatedImpl for S<T> {
     type ImplTrait = impl core::fmt::Debug;
 
-    fn f() -> Self::ImplTrait {
+    fn f() -> Self::ImplTrait { //~ ERROR unexpected concrete region in borrowck: ReEarlyBound(0, 'a)
         ()
     }
 }
