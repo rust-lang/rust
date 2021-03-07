9d5d669b77f8c100f3e01a7bd5924d30c8bad152
Manish Goregaokar <manishsmail@gmail.com>
2021-03-07 14:19:50 -0800

    diagnostics: Differentiate between edition meanings of ::foo in resolve diagnostics for ::foo::Bar

--- src/test/ui/editions-crate-root-2018.rs
+++ src/test/ui/editions-crate-root-2018.rs
@@ -7,6 +7,13 @@ mod inner {
     fn crate_inner(_: crate::nonexistant::Foo) {
         //~^ ERROR failed to resolve: maybe a missing crate `nonexistant`?
     }
+
+    fn bare_global(_: ::nonexistant) {
+        //~^ ERROR cannot find crate `nonexistant` in the list of imported crates
+    }
+    fn bare_crate(_: crate::nonexistant) {
+        //~^ ERROR cannot find type `nonexistant` in the crate root
+    }
 }
 
 fn main() {
