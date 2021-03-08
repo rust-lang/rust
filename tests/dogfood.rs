2d53b6b82412a18734478c0086090dc3a2b5c5cc
hyd-dev <yd-huang@outlook.com>
2021-03-08 18:29:36 +0800

    Move `test_no_deps_ignores_path_deps_in_workspaces()` out of `dogfood_subprojects()`

--- tests/dogfood.rs
+++ tests/dogfood.rs
@@ -46,28 +46,46 @@ fn dogfood_clippy() {
     assert!(output.status.success());
 }
 
-#[test]
-fn dogfood_subprojects() {
-    fn test_no_deps_ignores_path_deps_in_workspaces() {
-        if cargo::is_rustc_test_suite() {
-            return;
-        }
-        let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        let target_dir = root.join("target").join("dogfood");
-        let cwd = root.join("clippy_workspace_tests");
+fn test_no_deps_ignores_path_deps_in_workspaces() {
+    if cargo::is_rustc_test_suite() {
+        return;
+    }
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let target_dir = root.join("target").join("dogfood");
+    let cwd = root.join("clippy_workspace_tests");
 
-        // Make sure we start with a clean state
-        Command::new("cargo")
-            .current_dir(&cwd)
-            .env("CARGO_TARGET_DIR", &target_dir)
-            .arg("clean")
-            .args(&["-p", "subcrate"])
-            .args(&["-p", "path_dep"])
-            .output()
-            .unwrap();
+    // Make sure we start with a clean state
+    Command::new("cargo")
+        .current_dir(&cwd)
+        .env("CARGO_TARGET_DIR", &target_dir)
+        .arg("clean")
+        .args(&["-p", "subcrate"])
+        .args(&["-p", "path_dep"])
+        .output()
+        .unwrap();
 
-        // `path_dep` is a path dependency of `subcrate` that would trigger a denied lint.
-        // Make sure that with the `--no-deps` argument Clippy does not run on `path_dep`.
+    // `path_dep` is a path dependency of `subcrate` that would trigger a denied lint.
+    // Make sure that with the `--no-deps` argument Clippy does not run on `path_dep`.
+    let output = Command::new(&*CLIPPY_PATH)
+        .current_dir(&cwd)
+        .env("CLIPPY_DOGFOOD", "1")
+        .env("CARGO_INCREMENTAL", "0")
+        .arg("clippy")
+        .args(&["-p", "subcrate"])
+        .arg("--")
+        .arg("--no-deps")
+        .arg("-Cdebuginfo=0") // disable debuginfo to generate less data in the target dir
+        .args(&["--cfg", r#"feature="primary_package_test""#])
+        .output()
+        .unwrap();
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    assert!(output.status.success());
+
+    let lint_path_dep = || {
+        // Test that without the `--no-deps` argument, `path_dep` is linted.
         let output = Command::new(&*CLIPPY_PATH)
             .current_dir(&cwd)
             .env("CLIPPY_DOGFOOD", "1")
@@ -75,7 +93,6 @@ fn dogfood_subprojects() {
             .arg("clippy")
             .args(&["-p", "subcrate"])
             .arg("--")
-            .arg("--no-deps")
             .arg("-Cdebuginfo=0") // disable debuginfo to generate less data in the target dir
             .args(&["--cfg", r#"feature="primary_package_test""#])
             .output()
@@ -84,69 +101,52 @@ fn dogfood_subprojects() {
         println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
         println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 
+        assert!(!output.status.success());
+        assert!(
+            String::from_utf8(output.stderr)
+                .unwrap()
+                .contains("error: empty `loop {}` wastes CPU cycles")
+        );
+    };
+
+    // Make sure Cargo is aware of the removal of `--no-deps`.
+    lint_path_dep();
+
+    let successful_build = || {
+        let output = Command::new(&*CLIPPY_PATH)
+            .current_dir(&cwd)
+            .env("CLIPPY_DOGFOOD", "1")
+            .env("CARGO_INCREMENTAL", "0")
+            .arg("clippy")
+            .args(&["-p", "subcrate"])
+            .arg("--")
+            .arg("-Cdebuginfo=0") // disable debuginfo to generate less data in the target dir
+            .output()
+            .unwrap();
+        println!("status: {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
         assert!(output.status.success());
 
-        let lint_path_dep = || {
-            // Test that without the `--no-deps` argument, `path_dep` is linted.
-            let output = Command::new(&*CLIPPY_PATH)
-                .current_dir(&cwd)
-                .env("CLIPPY_DOGFOOD", "1")
-                .env("CARGO_INCREMENTAL", "0")
-                .arg("clippy")
-                .args(&["-p", "subcrate"])
-                .arg("--")
-                .arg("-Cdebuginfo=0") // disable debuginfo to generate less data in the target dir
-                .args(&["--cfg", r#"feature="primary_package_test""#])
-                .output()
-                .unwrap();
-            println!("status: {}", output.status);
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        output
+    };
 
-            assert!(!output.status.success());
-            assert!(
-                String::from_utf8(output.stderr)
-                    .unwrap()
-                    .contains("error: empty `loop {}` wastes CPU cycles")
-            );
-        };
+    // Trigger a sucessful build, so Cargo would like to cache the build result.
+    successful_build();
 
-        // Make sure Cargo is aware of the removal of `--no-deps`.
-        lint_path_dep();
+    // Make sure there's no spurious rebuild when nothing changes.
+    let stderr = String::from_utf8(successful_build().stderr).unwrap();
+    assert!(!stderr.contains("Compiling"));
+    assert!(!stderr.contains("Checking"));
+    assert!(stderr.contains("Finished"));
 
-        let successful_build = || {
-            let output = Command::new(&*CLIPPY_PATH)
-                .current_dir(&cwd)
-                .env("CLIPPY_DOGFOOD", "1")
-                .env("CARGO_INCREMENTAL", "0")
-                .arg("clippy")
-                .args(&["-p", "subcrate"])
-                .arg("--")
-                .arg("-Cdebuginfo=0") // disable debuginfo to generate less data in the target dir
-                .output()
-                .unwrap();
-            println!("status: {}", output.status);
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    // Make sure Cargo is aware of the new `--cfg` flag.
+    lint_path_dep();
+}
 
-            assert!(output.status.success());
-
-            output
-        };
-
-        // Trigger a sucessful build, so Cargo would like to cache the build result.
-        successful_build();
-
-        // Make sure there's no spurious rebuild when nothing changes.
-        let stderr = String::from_utf8(successful_build().stderr).unwrap();
-        assert!(!stderr.contains("Compiling"));
-        assert!(!stderr.contains("Checking"));
-        assert!(stderr.contains("Finished"));
-
-        // Make sure Cargo is aware of the new `--cfg` flag.
-        lint_path_dep();
-    }
-
+#[test]
+fn dogfood_subprojects() {
     // run clippy on remaining subprojects and fail the test if lint warnings are reported
     if cargo::is_rustc_test_suite() {
         return;
