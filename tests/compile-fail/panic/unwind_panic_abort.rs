15465a58812f7af4f5d27eb87da191b344d281b5
Ralf Jung <post@ralfj.de>
2021-03-14 17:10:45 +0100

    don't ICE when we unwind despite panic=abort

---
+++ tests/compile-fail/panic/unwind_panic_abort.rs
@@ -0,0 +1,11 @@
+// compile-flags: -Cpanic=abort
+
+//! Unwinding despite `-C panic=abort` is an error.
+
+extern "Rust" {
+    fn miri_start_panic(payload: *mut u8) -> !;
+}
+
+fn main() {
+    unsafe { miri_start_panic(&mut 0); } //~ ERROR unwinding despite panic=abort
+}
