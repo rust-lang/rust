eb3eb22f5edf080c1747fd41eee8b7b6d1ba49bb
Felix S. Klock II <pnkfelix@pnkfx.org>
2021-03-15 16:39:41 -0400

    Revert PR 81473 to resolve (on mainline) issues 81626 and 81658.
    Revert "Add missing brace"
    
    This reverts commit 85ad773049536d7fed9a94ae0ac74f97135c8655.
    
    Revert "Simplify base_expr"
    
    This reverts commit 899aae465eb4ef295dc1eeb2603f744568e0768c.
    
    Revert "Warn write-only fields"
    
    This reverts commit d3c69a4c0dd98af2611b7553d1a65afef6a6ccb0.

--- compiler/rustc_passes/src/dead.rs
+++ compiler/rustc_passes/src/dead.rs
@@ -285,11 +285,6 @@ impl<'tcx> Visitor<'tcx> for MarkSymbolVisitor<'tcx> {
             hir::ExprKind::MethodCall(..) => {
                 self.lookup_and_handle_method(expr.hir_id);
             }
-            hir::ExprKind::Assign(ref left, ref right, ..) => {
-                self.handle_assign(left);
-                self.visit_expr(right);
-                return;
-            }
             hir::ExprKind::Field(ref lhs, ..) => {
                 self.handle_field_access(&lhs, expr.hir_id);
             }
