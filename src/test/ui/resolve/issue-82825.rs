b3ac42b873ff342e6d2871680efb708bdb172fa4
b-naber <bn263@gmx.de>
2021-03-16 17:09:13 +0100

    add test

---
+++ src/test/ui/resolve/issue-82825.rs
@@ -0,0 +1,21 @@
+// check-pass
+
+trait Trait {
+    fn static_call(&self) where Self: Sized;
+    
+    fn maybe_dynamic_call(&self) {
+        unimplemented!("unsupported maybe_dynamic_call");
+    }
+}
+
+impl<T: ?Sized + Trait> Trait for &T {
+    fn static_call(&self) where Self: Sized {
+        (**self).maybe_dynamic_call();
+    }
+}
+
+fn foo(x: &dyn Trait) {
+    x.static_call();
+}
+
+fn main() {}
