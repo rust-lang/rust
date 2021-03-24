d9e2d8d6652fef745a5658178a6f85fc809f6453
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2021-03-24 19:44:23 +0000

    Rename `span` to `source`
    Caused by
    https://github.com/rust-lang/rust/commit/b0659f9b1bfb92626c40dabceb3268f88bb26224#diff-ede26372490522288745c5b3df2b6b2a1cc913dcd09b29af3a49935afe00c7e6L464-R464

--- src/test/rustdoc-json/reexport/in_root_and_mod_pub.rs
+++ src/test/rustdoc-json/reexport/in_root_and_mod_pub.rs
@@ -7,14 +7,14 @@ pub mod foo {
     pub struct Bar;
 }
 
-// @set root_import_id = - "$.index[*][?(@.inner.span=='foo::Bar')].id"
-// @is - "$.index[*][?(@.inner.span=='foo::Bar')].inner.id" $bar_id
+// @set root_import_id = - "$.index[*][?(@.inner.source=='foo::Bar')].id"
+// @is - "$.index[*][?(@.inner.source=='foo::Bar')].inner.id" $bar_id
 // @has - "$.index[*][?(@.name=='in_root_and_mod_pub')].inner.items[*]" $root_import_id
 pub use foo::Bar;
 
 pub mod baz {
-    // @set baz_import_id = - "$.index[*][?(@.inner.span=='crate::foo::Bar')].id"
-    // @is - "$.index[*][?(@.inner.span=='crate::foo::Bar')].inner.id" $bar_id
+    // @set baz_import_id = - "$.index[*][?(@.inner.source=='crate::foo::Bar')].id"
+    // @is - "$.index[*][?(@.inner.source=='crate::foo::Bar')].inner.id" $bar_id
     // @has - "$.index[*][?(@.name=='baz')].inner.items[*]" $baz_import_id
     pub use crate::foo::Bar;
 }
