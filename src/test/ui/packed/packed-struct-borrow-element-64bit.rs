1ab05c13dde13d08a2e1990960582cb9970e90a2
Ralf Jung <post@ralfj.de>
2021-03-28 13:54:27 +0200

    adjust old test

--- src/test/ui/packed/packed-struct-borrow-element-64bit.rs
+++ src/test/ui/packed/packed-struct-borrow-element-64bit.rs
@@ -1,18 +1,7 @@
 // run-pass (note: this is spec-UB, but it works for now)
+// ignore-32bit (needs `usize` to be 8-aligned to reproduce all the errors below)
 #![allow(dead_code)]
 // ignore-emscripten weird assertion?
-
-#[repr(packed)]
-struct Foo1 {
-    bar: u8,
-    baz: usize
-}
-
-#[repr(packed(2))]
-struct Foo2 {
-    bar: u8,
-    baz: usize
-}
 
 #[repr(C, packed(4))]
 struct Foo4C {
@@ -21,12 +10,7 @@ struct Foo4C {
 }
 
 pub fn main() {
-    let foo = Foo1 { bar: 1, baz: 2 };
-    let brw = &foo.baz; //~WARN reference to packed field is unaligned
-    //~^ previously accepted
-    assert_eq!(*brw, 2);
-
-    let foo = Foo2 { bar: 1, baz: 2 };
+    let foo = Foo4C { bar: 1, baz: 2 };
     let brw = &foo.baz; //~WARN reference to packed field is unaligned
     //~^ previously accepted
     assert_eq!(*brw, 2);
