f80b9acf204888585b254e760d13ac4c37af2254
Hameer Abbasi <einstein.edison@gmail.com>
2021-03-31 09:33:45 +0000

    Add 32bit.stderr files.

--- src/test/ui/consts/const-points-to-static.rs
+++ src/test/ui/consts/const-points-to-static.rs
@@ -7,7 +7,7 @@ const TEST: &u8 = &MY_STATIC;
 //~^ ERROR it is undefined behavior to use this value
 //~| NOTE  encountered a reference pointing to a static variable
 //~| NOTE undefined behavior
-//~| NOTE  the raw bytes of the constant (size: 8, align: 8)
+//~| NOTE the raw bytes of the constant
 
 static MY_STATIC: u8 = 4;
 
