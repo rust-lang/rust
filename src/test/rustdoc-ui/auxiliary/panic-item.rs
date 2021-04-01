cc69c652072ed8c544b288b2c45a183e57dd052d
Joshua Nelson <jyn514@gmail.com>
2021-03-31 22:33:07 -0400

    Don't load all extern crates unconditionally
    Instead, only load the crates that are linked to with intra-doc links.
    
    This doesn't help very much with any of rustdoc's fundamental issues
    with freezing the resolver, but it at least fixes a stable-to-stable
    regression, and makes the crate loading model somewhat more consistent
    with rustc's.

---
+++ src/test/rustdoc-ui/auxiliary/panic-item.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![feature(lang_items)]
+
+use core::panic::PanicInfo;
+use core::sync::atomic::{self, Ordering};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        atomic::compiler_fence(Ordering::SeqCst);
+    }
+}
+
+#[lang = "eh_personality"]
+fn foo() {}
