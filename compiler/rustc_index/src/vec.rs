3cfa0a0dff610e228819579709c035c9a1d4310b
Wilco Kusee <wilcokusee@gmail.com>
2021-04-03 10:00:09 +0200

    Remove nightly features in rustc_type_ir

--- compiler/rustc_index/src/vec.rs
+++ compiler/rustc_index/src/vec.rs
@@ -124,7 +124,7 @@ macro_rules! newtype_index {
 
             #[inline]
             $v const fn from_usize(value: usize) -> Self {
-                assert!(value <= ($max as usize));
+                [()][(value > ($max as usize)) as usize];
                 unsafe {
                     Self::from_u32_unchecked(value as u32)
                 }
@@ -132,7 +132,7 @@ macro_rules! newtype_index {
 
             #[inline]
             $v const fn from_u32(value: u32) -> Self {
-                assert!(value <= $max);
+                [()][(value > $max) as usize];
                 unsafe {
                     Self::from_u32_unchecked(value)
                 }
