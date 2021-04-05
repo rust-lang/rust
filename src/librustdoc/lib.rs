0f3efe25b9c02c6b70cf68798ff0626023246c80
Aliénore Bouttefeux <77335613+ABouttefeux@users.noreply.github.com>
2021-04-05 17:53:45 +0200

    Update src/librustdoc/lib.rs from suggestion
    Co-authored-by: Ivan Tham <pickfire@riseup.net>

--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -536,7 +536,7 @@ fn opts() -> Vec<RustcOptGroup> {
                 "[unversioned-shared-resources,toolchain-shared-resources,invocation-specific]",
             )
         }),
-        unstable("no-run", |o| o.optflag("", "no-run", "Compile, but don't run doc tests")),
+        unstable("no-run", |o| o.optflag("", "no-run", "Compile doc tests without running it")),
     ]
 }
 
