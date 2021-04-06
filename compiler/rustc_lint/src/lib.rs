389100921ab840bc235b3487d8b519197dec77c1
Aliénore Bouttefeux <alienore.bouttefeux@gmail.com>
2021-04-06 22:01:00 +0200

    add lint deref_nullptr

--- compiler/rustc_lint/src/lib.rs
+++ compiler/rustc_lint/src/lib.rs
@@ -206,6 +206,7 @@ macro_rules! late_lint_mod_passes {
                 UnreachablePub: UnreachablePub,
                 ExplicitOutlivesRequirements: ExplicitOutlivesRequirements,
                 InvalidValue: InvalidValue,
+                DerefNullPtr: DerefNullPtr,
             ]
         );
     };
