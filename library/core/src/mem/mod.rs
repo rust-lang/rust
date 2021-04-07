1876c7e54f82b970b65a7e41e41b68873fec6ea1
Joshua Nelson <jyn514@gmail.com>
2021-04-07 09:46:13 -0400

    Add `free` as a doc alias for `drop`

--- library/core/src/mem/mod.rs
+++ library/core/src/mem/mod.rs
@@ -886,7 +886,7 @@ pub const fn replace<T>(dest: &mut T, src: T) -> T {
 /// ```
 ///
 /// [`RefCell`]: crate::cell::RefCell
-#[doc(alias = "delete")]
+#[doc(alias("delete", "free"))]
 #[inline]
 #[stable(feature = "rust1", since = "1.0.0")]
 pub fn drop<T>(_x: T) {}
