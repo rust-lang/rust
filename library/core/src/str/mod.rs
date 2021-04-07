fbfde7eaaf6e95ec186c311d5044281d7b2415c6
Count Count <countvoncount123456@gmail.com>
2021-04-07 08:07:09 +0200

    Style only: merge with other pub use statement

--- library/core/src/str/mod.rs
+++ library/core/src/str/mod.rs
@@ -69,10 +69,7 @@ pub use iter::SplitAsciiWhitespace;
 pub use iter::SplitInclusive;
 
 #[unstable(feature = "str_internals", issue = "none")]
-pub use validations::next_code_point;
-
-#[unstable(feature = "str_internals", issue = "none")]
-pub use validations::utf8_char_width;
+pub use validations::{next_code_point, utf8_char_width};
 
 use iter::MatchIndicesInternal;
 use iter::SplitInternal;
