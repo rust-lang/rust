0019ca9141d0e397534df87a6a7c4c7ece2646d7
pierwill <pierwill@users.noreply.github.com>
2021-04-07 14:47:01 -0500

    Fix outdated crate names in compiler docs
    Changes `librustc_X` to `rustc_X`, only in documentation comments.
    Plain code comments are left unchanged.
    
    Also fix incorrect file paths.

--- compiler/rustc_middle/src/mir/interpret/error.rs
+++ compiler/rustc_middle/src/mir/interpret/error.rs
@@ -46,7 +46,7 @@ static_assert_size!(InterpErrorInfo<'_>, 8);
 /// Packages the kind of error we got from the const code interpreter
 /// up with a Rust-level backtrace of where the error occurred.
 /// Thsese should always be constructed by calling `.into()` on
-/// a `InterpError`. In `librustc_mir::interpret`, we have `throw_err_*`
+/// a `InterpError`. In `rustc_mir::interpret`, we have `throw_err_*`
 /// macros for this.
 #[derive(Debug)]
 pub struct InterpErrorInfo<'tcx>(Box<InterpErrorInfoInner<'tcx>>);
