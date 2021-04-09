0d7167698fcb957d98ccfb77729196bf33442841
Esteban Küber <esteban@kuber.com.ar>
2021-04-08 19:12:00 -0700

    Avoid `;` -> `,` recovery and unclosed `}` recovery from being too verbose
    Those two recovery attempts have a very bad interaction that causes too
    unnecessary output. Add a simple gate to avoid interpreting a `;` as a
    `,` when there are unclosed braces.

--- src/test/ui/parser/use-unclosed-brace.rs
+++ src/test/ui/parser/use-unclosed-brace.rs
@@ -1,26 +1,12 @@
+// error-pattern: expected one of `,`, `::`, `as`, or `}`, found `;`
+// error-pattern: this file contains an unclosed delimiter
+// error-pattern: expected item, found `}`
 use foo::{bar, baz;
-//~^ ERROR expected one of `,`, `::`, `as`, or `}`, found `;`
 
 use std::fmt::Display;
-//~^ ERROR expected identifier, found keyword `use`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `std`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `;`
 
 mod bar { }
-//~^ ERROR expected identifier, found keyword `mod`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `bar`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `{`
 
 mod baz { }
-//~^ ERROR expected identifier, found keyword `mod`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `{`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `baz`
-//~| ERROR expected one of `,` or `}`, found keyword `mod`
 
 fn main() {}
-//~^ ERROR expected identifier, found keyword `fn`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `(`
-//~| ERROR expected one of `,` or `}`, found keyword `fn`
-//~| ERROR expected one of `,`, `::`, `as`, or `}`, found `main`
-
-//~ ERROR this file contains an unclosed delimiter
