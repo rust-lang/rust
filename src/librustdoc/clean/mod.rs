2dfd0bfe1087fef83f26b52b32c8b54fb621b6b9
Michael Howell <michael@notriddle.com>
2021-04-10 14:22:06 -0700

    rustdoc: clean up and test macro visibility print
    This fixes the overly-complex invariant mentioned in
    <https://github.com/rust-lang/rust/pull/83237#issuecomment-815346570>,
    where the macro source can't have any links in it only because the
    cache hasn't been populated yet.

--- src/librustdoc/clean/mod.rs
+++ src/librustdoc/clean/mod.rs
@@ -2287,14 +2287,14 @@ impl Clean<Item> for (&hir::MacroDef<'_>, Option<Symbol>) {
             if matchers.len() <= 1 {
                 format!(
                     "{}macro {}{} {{\n    ...\n}}",
-                    vis.print_with_space(cx.tcx, def_id, &cx.cache),
+                    vis.to_src_with_space(cx.tcx, def_id),
                     name,
                     matchers.iter().map(|span| span.to_src(cx)).collect::<String>(),
                 )
             } else {
                 format!(
                     "{}macro {} {{\n{}}}",
-                    vis.print_with_space(cx.tcx, def_id, &cx.cache),
+                    vis.to_src_with_space(cx.tcx, def_id),
                     name,
                     matchers
                         .iter()
