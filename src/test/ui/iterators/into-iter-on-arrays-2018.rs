3dab4e22d43c1ec724b23d301c81bdc2b99d50e7
Josh Stone <jistone@redhat.com>
2021-04-12 16:07:28 -0700

    Skip into_iter() for arrays before 2021

---
+++ src/test/ui/iterators/into-iter-on-arrays-2018.rs
@@ -0,0 +1,17 @@
+// check-pass
+
+use std::array::IntoIter;
+use std::slice::Iter;
+
+fn main() {
+    let array = [0; 10];
+
+    // Before 2021, the method dispatched to `IntoIterator for &[T]`,
+    // which we continue to support for compatibility.
+    let _: Iter<'_, i32> = array.into_iter();
+    //~^ WARNING this method call currently resolves to `<&[T; N] as IntoIterator>::into_iter`
+    //~| WARNING this was previously accepted by the compiler but is being phased out
+
+    // But you can always use the trait method explicitly as an array.
+    let _: IntoIter<i32, 10> = IntoIterator::into_iter(array);
+}
---
+++ src/test/ui/iterators/into-iter-on-arrays-2021.rs
@@ -0,0 +1,15 @@
+// check-pass
+// edition:2021
+// compile-flags: -Zunstable-options
+
+use std::array::IntoIter;
+
+fn main() {
+    let array = [0; 10];
+
+    // In 2021, the method dispatches to `IntoIterator for [T; N]`.
+    let _: IntoIter<i32, 10> = array.into_iter();
+
+    // And you can always use the trait method explicitly as an array.
+    let _: IntoIter<i32, 10> = IntoIterator::into_iter(array);
+}
