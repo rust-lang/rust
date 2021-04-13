bf8b15f5533c852e69ba9051c96bbcac862bdfbb
Amanieu d'Antras <amanieu@gmail.com>
2021-04-13 04:32:11 +0100

    Bump stdarch submodule

--- src/test/ui/consts/const_arg_wrapper.rs
+++ src/test/ui/consts/const_arg_wrapper.rs
@@ -1,12 +1,10 @@
-// only-x86_64
+#![feature(rustc_attrs)]
 
-#[cfg(target_arch = "x86")]
-use std::arch::x86::*;
-#[cfg(target_arch = "x86_64")]
-use std::arch::x86_64::*;
+#[rustc_args_required_const(0)]
+fn foo(_imm8: i32) {}
 
-unsafe fn pclmul(a: __m128i, b: __m128i, imm8: i32) -> __m128i {
-    _mm_clmulepi64_si128(a, b, imm8) //~ ERROR argument 3 is required to be a constant
+fn bar(imm8: i32) {
+    foo(imm8) //~ ERROR argument 1 is required to be a constant
 }
 
 fn main() {}
