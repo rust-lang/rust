891b1f4bf3e7e2bc541ac4b442753b77e969928f
Guillaume Gomez <guillaume1.gomez@gmail.com>
2021-04-13 22:51:56 +0200

    Generate links for modules as well

--- src/test/rustdoc/check-source-code-urls-to-def.rs
+++ src/test/rustdoc/check-source-code-urls-to-def.rs
@@ -2,10 +2,11 @@
 
 #![crate_name = "foo"]
 
+// @has 'src/foo/check-source-code-urls-to-def.rs.html'
+
+// @has - '//a[@href="../../src/foo/auxiliary/source-code-bar.rs.html#1-17"]' 'bar'
 #[path = "auxiliary/source-code-bar.rs"]
 pub mod bar;
-
-// @has 'src/foo/check-source-code-urls-to-def.rs.html'
 
 // @count - '//a[@href="../../src/foo/auxiliary/source-code-bar.rs.html#5-7"]' 4
 use bar::Bar;
@@ -22,13 +23,13 @@ impl Foo {
 fn babar() {}
 
 // @has - '//a[@href="https://doc.rust-lang.org/nightly/alloc/string/struct.String.html"]' 'String'
-// @count - '//a[@href="../../src/foo/check-source-code-urls-to-def.rs.html#16"]' 5
+// @count - '//a[@href="../../src/foo/check-source-code-urls-to-def.rs.html#17"]' 5
 pub fn foo(a: u32, b: &str, c: String, d: Foo, e: bar::Bar) {
     let x = 12;
     let y: Foo = Foo;
     let z: Bar = bar::Bar { field: Foo };
     babar();
-    // @has - '//a[@href="../../src/foo/check-source-code-urls-to-def.rs.html#19"]' 'hello'
+    // @has - '//a[@href="../../src/foo/check-source-code-urls-to-def.rs.html#20"]' 'hello'
     y.hello();
 }
 
