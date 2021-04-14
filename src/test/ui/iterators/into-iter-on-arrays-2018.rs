4d089a41b1faec8b9d4f2a6cc3efcef72eb2d6fe
Josh Stone <jistone@redhat.com>
2021-04-14 12:05:24 -0700

    Test array into_iter with more wrapper types

--- src/test/ui/iterators/into-iter-on-arrays-2018.rs
+++ src/test/ui/iterators/into-iter-on-arrays-2018.rs
@@ -2,6 +2,8 @@
 // edition:2018
 
 use std::array::IntoIter;
+use std::ops::Deref;
+use std::rc::Rc;
 use std::slice::Iter;
 
 fn main() {
@@ -17,6 +19,21 @@ fn main() {
     //~^ WARNING this method call currently resolves to `<&[T; N] as IntoIterator>::into_iter`
     //~| WARNING this was previously accepted by the compiler but is being phased out
 
+    // The `array_into_iter` lint doesn't cover other wrappers that deref to an array.
+    let _: Iter<'_, i32> = Rc::new(array).into_iter();
+    let _: Iter<'_, i32> = Array(array).into_iter();
+
     // But you can always use the trait method explicitly as an array.
     let _: IntoIter<i32, 10> = IntoIterator::into_iter(array);
+}
+
+/// User type that dereferences to an array.
+struct Array([i32; 10]);
+
+impl Deref for Array {
+    type Target = [i32; 10];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
