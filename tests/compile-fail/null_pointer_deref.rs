648638976af2f7331a61921997b0703edb2f4212
Aliénore Bouttefeux <alienore.bouttefeux@gmail.com>
2021-04-15 10:00:39 +0200

    allow deref of null ptr in test

--- tests/compile-fail/null_pointer_deref.rs
+++ tests/compile-fail/null_pointer_deref.rs
@@ -1,3 +1,4 @@
+#[allow(deref_nullptr)]
 fn main() {
     let x: i32 = unsafe { *std::ptr::null() }; //~ ERROR inbounds test failed: 0x0 is not a valid pointer
     panic!("this should never print: {}", x);
