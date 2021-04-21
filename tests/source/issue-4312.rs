84ff0013c25cd96995f61253b0b8da74e32d12ea
ChinYing-Li <chinying.li@mail.utoronto.ca>
2021-04-21 14:08:57 +0800

    Add the case in duplicate issue (#4806) to the idempotent tests

--- tests/source/issue-4312.rs
+++ tests/source/issue-4312.rs
@@ -1,3 +1,4 @@
+// issue 4312
 fn main() {
     /* " */
     println!("Hello, world!");
@@ -5,4 +6,17 @@ fn main() {
     println!("Hello, world!");
     /* " abc */
     println!("Hello, world!");
+    let y = 4;
+    let x = match 1 + y == 3 {
+        True => 3,
+        False => 4,
+        /* " unreachable */
+    };
+}
+
+// issue 4806
+enum X {
+    A,
+    B,
+    /*"*/
 }
