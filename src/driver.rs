32351d6b9f7caa2fd197a23a190fb32392ab9dfd
Cameron Steffen <cam.steffen94@gmail.com>
2021-04-28 14:29:13 -0500

    Remove leftover plugin conf_file code

--- src/driver.rs
+++ src/driver.rs
@@ -106,7 +106,7 @@ impl rustc_driver::Callbacks for ClippyCallbacks {
                 (previous)(sess, lint_store);
             }
 
-            let conf = clippy_lints::read_conf(&[], sess);
+            let conf = clippy_lints::read_conf(sess);
             clippy_lints::register_plugins(lint_store, sess, &conf);
             clippy_lints::register_pre_expansion_lints(lint_store);
             clippy_lints::register_renamed(lint_store);
