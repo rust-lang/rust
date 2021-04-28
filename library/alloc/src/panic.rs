701e3a45a9b8d2eb32be1c44ac3ef780abb4075f
David Tolnay <dtolnay@gmail.com>
2021-04-28 10:08:29 -0700

    Fix comment referring to formerly-above code

--- library/alloc/src/panic.rs
+++ library/alloc/src/panic.rs
@@ -2,7 +2,7 @@ use crate::rc::Rc;
 use crate::sync::Arc;
 use core::panic::{RefUnwindSafe, UnwindSafe};
 
-// not covered via the Shared impl above b/c the inner contents use
+// not covered via the Shared impl in libcore b/c the inner contents use
 // Cell/AtomicUsize, but the usage here is unwind safe so we can lift the
 // impl up one level to Arc/Rc itself
 #[stable(feature = "catch_unwind", since = "1.9.0")]
