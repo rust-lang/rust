6145051eeebeba030cef3aa01a99683b84ff24fc
Christiaan Dirkx <christiaan@dirkx.email>
2021-04-29 16:10:32 +0200

    Replace `sys_common::util::dumb_print` with `rterr!`

--- library/std/src/sys_common/util.rs
+++
@@ -1,9 +0,0 @@
-use crate::fmt;
-use crate::io::prelude::*;
-use crate::sys::stdio::panic_output;
-
-pub fn dumb_print(args: fmt::Arguments<'_>) {
-    if let Some(mut out) = panic_output() {
-        let _ = out.write_fmt(args);
-    }
-}
