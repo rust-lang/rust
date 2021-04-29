236705f3c30905623f97dc8887c83db520eb27c1
Christiaan Dirkx <christiaan@dirkx.email>
2021-04-29 16:05:10 +0200

    Replace `sys_common::util::report_overflow` with `rterr!`

--- library/std/src/sys_common/util.rs
+++ library/std/src/sys_common/util.rs
@@ -1,18 +1,9 @@
 use crate::fmt;
 use crate::io::prelude::*;
 use crate::sys::stdio::panic_output;
-use crate::thread;
 
 pub fn dumb_print(args: fmt::Arguments<'_>) {
     if let Some(mut out) = panic_output() {
         let _ = out.write_fmt(args);
     }
-}
-
-#[allow(dead_code)] // stack overflow detection not enabled on all platforms
-pub unsafe fn report_overflow() {
-    dumb_print(format_args!(
-        "\nthread '{}' has overflowed its stack\n",
-        thread::current().name().unwrap_or("<unknown>")
-    ));
 }
