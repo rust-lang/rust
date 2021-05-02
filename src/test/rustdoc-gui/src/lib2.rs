bc888ba840c8273e8d1d83157c693a9df49f5ebf
Guillaume Gomez <guillaume1.gomez@gmail.com>
2021-05-02 18:59:31 +0200

    Add sidebar GUI tests

---
+++ src/test/rustdoc-gui/src/lib2.rs
@@ -0,0 +1,11 @@
+pub mod module {
+    pub mod sub_module {
+        pub mod sub_sub_module {
+            pub fn foo() {}
+        }
+        pub fn bar() {}
+    }
+    pub fn whatever() {}
+}
+
+pub fn foobar() {}
