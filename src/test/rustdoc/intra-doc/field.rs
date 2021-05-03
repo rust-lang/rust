4029a03b345ae9fcc8e981ac181f00332b7711ed
Joshua Nelson <jyn514@gmail.com>
2021-05-03 14:57:51 -0400

    Make match in `register_res` easier to read
    - Don't duplicate DefKind -> ItemType handling; that's a good way to get bugs
    - Use exhaustive match
    - Add comments
    
    This found that register_res is very wrong in at least one way: if it
    registers a Res for `Variant`, it should also register one for `Field`.
    But I don't know whether the one for Variant should be removed or Field
    added. Maybe someone has ideas?

---
+++ src/test/rustdoc/intra-doc/field.rs
@@ -0,0 +1,4 @@
+// @has field/index.html '//a[@href="https://doc.rust-lang.org/nightly/core/ops/range/struct.Range.html#structfield.start"]' 'start'
+// @has field/index.html '//a[@href="https://doc.rust-lang.org/nightly/std/io/error/enum.ErrorKind.html#variant.NotFound"]' 'not_found'
+//! [start][std::ops::Range::start]
+//! [not_found][std::io::ErrorKind::NotFound]
