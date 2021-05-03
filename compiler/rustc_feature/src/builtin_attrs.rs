3584c1dd0cb501916c8bc6fd452864b261068beb
Rich Kadel <richkadel@google.com>
2021-05-03 08:47:43 -0700

    Disallows `#![feature(no_coverage)]` on stable and beta
    using allow_internal_unstable (as recommended)
    
    Fixes: #84836
    
    ```shell
    $ ./build/x86_64-unknown-linux-gnu/stage1/bin/rustc     src/test/run-make-fulldeps/coverage/no_cov_crate.rs
    error[E0554]: `#![feature]` may not be used on the dev release channel
     --> src/test/run-make-fulldeps/coverage/no_cov_crate.rs:2:1
      |
    2 | #![feature(no_coverage)]
      | ^^^^^^^^^^^^^^^^^^^^^^^^
    
    error: aborting due to previous error
    
    For more information about this error, try `rustc --explain E0554`.
    ```

--- compiler/rustc_feature/src/builtin_attrs.rs
+++ compiler/rustc_feature/src/builtin_attrs.rs
@@ -273,13 +273,7 @@ pub const BUILTIN_ATTRIBUTES: &[BuiltinAttribute] = &[
         template!(List: "address, memory, thread"),
         experimental!(no_sanitize)
     ),
-    ungated!(
-        // Not exclusively gated at the crate level (though crate-level is
-        // supported). The feature can alternatively be enabled on individual
-        // functions.
-        no_coverage, AssumedUsed,
-        template!(Word),
-    ),
+    gated!(no_coverage, AssumedUsed, template!(Word), experimental!(no_coverage)),
 
     // FIXME: #14408 assume docs are used since rustdoc looks at them.
     ungated!(doc, AssumedUsed, template!(List: "hidden|inline|...", NameValueStr: "string")),
