25c66a13a9b9a646e247f8ed15828d621cdf30fa
Cameron Steffen <cam.steffen94@gmail.com>
2021-05-05 10:33:14 -0500

    Suggest lint groups

--- src/test/ui/lint/lint-unknown-lint.rs
+++ src/test/ui/lint/lint-unknown-lint.rs
@@ -6,4 +6,8 @@
                    //~| HELP did you mean
                    //~| SUGGESTION dead_code
 
+#![deny(rust_2018_idiots)] //~ ERROR unknown lint
+                           //~| HELP did you mean
+                           //~| SUGGESTION rust_2018_idioms
+
 fn main() {}
