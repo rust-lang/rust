65cd051b4a883327e8d286eaab1a171f3f904c92
Ralf Jung <post@ralfj.de>
2021-05-08 16:28:29 +0200

    stabilize const_fn_unsize

--- src/test/ui/consts/const_fn_unsize.rs
+++ src/test/ui/consts/const_fn_unsize.rs
@@ -1,16 +1,14 @@
-// gate-test-const_fn_unsize
-
-// revisions: stock gated
-
-#![feature(rustc_attrs)]
-#![cfg_attr(gated, feature(const_fn_unsize))]
+// check-pass
 
 use std::ptr::NonNull;
 
 const fn test() {
     let _x = NonNull::<[i32; 0]>::dangling() as NonNull<[i32]>;
-    //[stock]~^ unsizing cast
 }
 
-#[rustc_error]
-fn main() {} //[gated]~ fatal error triggered by #[rustc_error]
+// Regression test for #75118.
+pub const fn dangling_slice<T>() -> NonNull<[T]> {
+    NonNull::<[T; 0]>::dangling()
+}
+
+fn main() {}
