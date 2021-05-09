efc8f65b852b7ce5203ce22f54066a0e647019a5
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2021-05-09 05:26:59 +0430

    Try to fix issue 68049

--- compiler/rustc_mir/src/borrow_check/diagnostics/mutability_errors.rs
+++ compiler/rustc_mir/src/borrow_check/diagnostics/mutability_errors.rs
@@ -6,7 +6,7 @@ use rustc_middle::mir::{Mutability, Place, PlaceRef, ProjectionElem};
 use rustc_middle::ty::{self, Ty, TyCtxt};
 use rustc_middle::{
     hir::place::PlaceBase,
-    mir::{self, ClearCrossCrate, Local, LocalDecl, LocalInfo, Location},
+    mir::{self, ClearCrossCrate, Local, LocalDecl, LocalInfo, LocalKind, Location},
 };
 use rustc_span::source_map::DesugaringKind;
 use rustc_span::symbol::{kw, Symbol};
@@ -424,15 +424,108 @@ impl<'a, 'tcx> MirBorrowckCtxt<'a, 'tcx> {
 
                         match label {
                             Some((true, err_help_span, suggested_code)) => {
-                                err.span_suggestion(
-                                    err_help_span,
-                                    &format!(
-                                        "consider changing this to be a mutable {}",
-                                        pointer_desc
-                                    ),
-                                    suggested_code,
-                                    Applicability::MachineApplicable,
-                                );
+                                /// User cannot make signature of a trait mutable
+                                /// without changing the trait. So we find if this
+                                /// error belongs to a trait and if so we move
+                                /// suggestion to the trait or disable it if it is
+                                /// out of scope of this crate
+                                let (is_trait_sig, local_trait) = {
+                                    if self.body.local_kind(local) != LocalKind::Arg {
+                                        (false, None)
+                                    } else {
+                                        let hir_map = self.infcx.tcx.hir();
+                                        let my_hir = hir_map.local_def_id_to_hir_id(
+                                            self.body.source.def_id().as_local().unwrap(),
+                                        );
+                                        match hir_map.find(hir_map.get_parent_node(my_hir)) {
+                                            Some(Node::Item(hir::Item {
+                                                kind:
+                                                    hir::ItemKind::Impl(hir::Impl {
+                                                        of_trait:
+                                                            Some(hir::TraitRef {
+                                                                path:
+                                                                    hir::Path {
+                                                                        res:
+                                                                            hir::def::Res::Def(_, td),
+                                                                        ..
+                                                                    },
+                                                                ..
+                                                            }),
+                                                        ..
+                                                    }),
+                                                ..
+                                            })) => {
+                                                (true, td.as_local().and_then(|tld| {
+                                                    let h = hir_map.local_def_id_to_hir_id(tld);
+                                                    match hir_map.find(h) {
+                                                        Some(Node::Item(hir::Item {
+                                                            kind: hir::ItemKind::Trait(
+                                                                _, _, _, _,
+                                                                items
+                                                            ),
+                                                            ..
+                                                        })) => {
+                                                            let mut f_in_trait_opt = None;
+                                                            for hir::TraitItemRef { id: fi, kind: k, .. } in *items {
+                                                                let hi = fi.hir_id();
+                                                                if !matches!(k, hir::AssocItemKind::Fn { .. }) {
+                                                                    continue;
+                                                                }
+                                                                if hir_map.name(hi) != hir_map.name(my_hir) {
+                                                                    continue;
+                                                                }
+                                                                f_in_trait_opt = Some(hi);
+                                                                break;
+                                                            }
+                                                            f_in_trait_opt.and_then(|f_in_trait| {
+                                                                match hir_map.find(f_in_trait) {
+                                                                    Some(Node::TraitItem(hir::TraitItem {
+                                                                        kind: hir::TraitItemKind::Fn(hir::FnSig {
+                                                                            decl: hir::FnDecl {
+                                                                                inputs,
+                                                                                ..
+                                                                            },
+                                                                            ..
+                                                                        }, _),
+                                                                        ..
+                                                                    })) => {
+                                                                        let hir::Ty { span, .. } = inputs[local.index() - 1];
+                                                                        Some(span)
+                                                                    },
+                                                                    _ => None,
+                                                                }
+                                                            })
+                                                            //Some(hir_map.span(h))
+                                                        }
+                                                        _ => None
+                                                    }
+                                                }))
+                                            }
+                                            _ => (false, None),
+                                        }
+                                    }
+                                };
+                                if !is_trait_sig {
+                                    err.span_suggestion(
+                                        err_help_span,
+                                        &format!(
+                                            "consider changing this to be a mutable {}",
+                                            pointer_desc
+                                        ),
+                                        suggested_code,
+                                        Applicability::MachineApplicable,
+                                    );
+                                } else if let Some(x) = local_trait {
+                                    err.span_suggestion(
+                                        x,
+                                        &format!(
+                                            "consider changing that to be a mutable {}",
+                                            pointer_desc
+                                        ),
+                                        suggested_code,
+                                        Applicability::MachineApplicable,
+                                    );
+                                }
                             }
                             Some((false, err_label_span, message)) => {
                                 err.span_label(err_label_span, &message);
