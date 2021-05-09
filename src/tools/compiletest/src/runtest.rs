4ce2e335d9bd0a4ef60429f5d57f119bdb562492
Aliénore Bouttefeux <alienore.bouttefeux@gmail.com>
2021-05-09 14:46:00 +0200

    fix compiletest to search for two dash and run make fulldeps test

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -2638,7 +2638,7 @@ impl<'test> TestCx<'test> {
         let mut tested = 0;
         for _ in res.stdout.split('\n').filter(|s| s.starts_with("test ")).inspect(|s| {
             let tmp: Vec<&str> = s.split(" - ").collect();
-            if tmp.len() == 2 {
+            if tmp.len() == 3 {
                 let path = tmp[0].rsplit("test ").next().unwrap();
                 if let Some(ref mut v) = files.get_mut(&path.replace('\\', "/")) {
                     tested += 1;
