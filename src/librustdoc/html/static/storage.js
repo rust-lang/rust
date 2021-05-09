f510e412e93958e099334de22de375d26264e767
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2021-05-09 13:49:22 -0700

    rustdoc: remove explicit boolean comparisons.
    For boolean variables it's shorter and more readable to check the value
    directly, or negate it with `!`.
    
    In a couple of cases I reordered an if/else pair because it made the
    initial `if` statement simpler.
    
    Removed unused isType parameter from two functions.

--- src/librustdoc/html/static/storage.js
+++ src/librustdoc/html/static/storage.js
@@ -59,15 +59,15 @@ function onEach(arr, func, reversed) {
     if (arr && arr.length > 0 && func) {
         var length = arr.length;
         var i;
-        if (reversed !== true) {
-            for (i = 0; i < length; ++i) {
-                if (func(arr[i]) === true) {
+        if (reversed) {
+            for (i = length - 1; i >= 0; --i) {
+                if (func(arr[i])) {
                     return true;
                 }
             }
         } else {
-            for (i = length - 1; i >= 0; --i) {
-                if (func(arr[i]) === true) {
+            for (i = 0; i < length; ++i) {
+                if (func(arr[i])) {
                     return true;
                 }
             }
@@ -111,7 +111,7 @@ function switchTheme(styleElem, mainStyleElem, newTheme, saveTheme) {
 
     // If this new value comes from a system setting or from the previously
     // saved theme, no need to save it.
-    if (saveTheme === true) {
+    if (saveTheme) {
         updateLocalStorage("rustdoc-theme", newTheme);
     }
 
@@ -131,7 +131,7 @@ function switchTheme(styleElem, mainStyleElem, newTheme, saveTheme) {
             return true;
         }
     });
-    if (found === true) {
+    if (found) {
         styleElem.href = newHref;
     }
 }
