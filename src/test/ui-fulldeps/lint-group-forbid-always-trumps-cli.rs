80ae9ef14641e0cff52d55a0a8cb5eb2d96fb317
Fabian Wolff <fabian.wolff@alumni.ethz.ch>
2021-05-14 20:36:44 +0200

    Fix test case failure in ui-fulldeps suite

--- src/test/ui-fulldeps/lint-group-forbid-always-trumps-cli.rs
+++ src/test/ui-fulldeps/lint-group-forbid-always-trumps-cli.rs
@@ -1,7 +1,10 @@
 // aux-build:lint-group-plugin-test.rs
-// compile-flags: -F unused -A unused
+// compile-flags: -F unused -D forbidden_lint_groups -A unused -Z deduplicate-diagnostics=yes
+//~^^ ERROR: allow(unused) incompatible
+//~| WARNING: this was previously accepted
+//~| ERROR: allow(unused) incompatible
+//~| WARNING: this was previously accepted
 
 fn main() {
     let x = 1;
-    //~^ ERROR unused variable: `x`
 }
