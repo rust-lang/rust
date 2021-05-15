9719031dd9e2c546da32710d6e816e3bfa7f0151
Ximin Luo <infinity0@pwned.gg>
2021-05-15 15:05:06 +0100

    bootstrap: ensure host std when cross-compiling tools, fixes #85320

--- src/bootstrap/tool.rs
+++ src/bootstrap/tool.rs
@@ -52,7 +52,10 @@ impl Step for ToolBuild {
         let is_optional_tool = self.is_optional_tool;
 
         match self.mode {
-            Mode::ToolRustc => builder.ensure(compile::Rustc { compiler, target }),
+            Mode::ToolRustc => {
+                builder.ensure(compile::Std { compiler, target: compiler.host });
+                builder.ensure(compile::Rustc { compiler, target });
+            }
             Mode::ToolStd => builder.ensure(compile::Std { compiler, target }),
             Mode::ToolBootstrap => {} // uses downloaded stage0 compiler libs
             _ => panic!("unexpected Mode for tool build"),
