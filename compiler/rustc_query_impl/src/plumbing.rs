8ed82ebb2ad82b1cb95b7f68482a3a6d201a7462
Camille GILLOT <gillot.camille@gmail.com>
2021-05-15 10:55:53 +0200

    Only test if key is local.

--- compiler/rustc_query_impl/src/plumbing.rs
+++ compiler/rustc_query_impl/src/plumbing.rs
@@ -389,8 +389,7 @@ macro_rules! define_queries {
             fn compute_fn(tcx: QueryCtxt<'tcx>, key: &Self::Key) ->
                 fn(TyCtxt<'tcx>, Self::Key) -> Self::Value
             {
-                let is_local = key.query_crate() == LOCAL_CRATE;
-                if is_local {
+                if key.query_crate_is_local() {
                     tcx.queries.local_providers.$name
                 } else {
                     tcx.queries.extern_providers.$name
