83459081bacfe30a593fc3cc95c029b11ed67468
Jack Huey <jack.huey@umassmed.edu>
2021-05-15 23:18:59 -0400

    Remove legacy for trait object mangling test and replace crate hash

--- src/test/ui/symbol-names/trait-objects.rs
+++ src/test/ui/symbol-names/trait-objects.rs
@@ -1,9 +1,10 @@
 // Ensure that trait objects don't include more than one binder. See #83611
 
 // build-fail
-// revisions: legacy v0
-//[legacy]compile-flags: -Z symbol-mangling-version=legacy
-    //[v0]compile-flags: -Z symbol-mangling-version=v0
+// revisions: v0
+//[v0]compile-flags: -Z symbol-mangling-version=v0
+//[v0]normalize-stderr-test: "Cs.*?_" -> "CRATE_HASH"
+//[v0]normalize-stderr-test: "core\[.*?\]" -> "core[HASH]"
 
 #![feature(rustc_attrs)]
 
@@ -13,10 +14,7 @@ trait Bar {
 
 impl Bar for &dyn FnMut(&u8) {
     #[rustc_symbol_name]
-    //[legacy]~^ ERROR symbol-name
-    //[legacy]~| ERROR demangling
-    //[legacy]~| ERROR demangling-alt
-    //[v0]~^^^^ ERROR symbol-name
+    //[v0]~^ ERROR symbol-name
     //[v0]~| ERROR demangling
     //[v0]~| ERROR demangling-alt
     fn method(&self) {}
@@ -28,10 +26,7 @@ trait Foo {
 
 impl Foo for &(dyn FnMut(&u8) + for<'b> Send) {
     #[rustc_symbol_name]
-    //[legacy]~^ ERROR symbol-name
-    //[legacy]~| ERROR demangling
-    //[legacy]~| ERROR demangling-alt
-    //[v0]~^^^^ ERROR symbol-name
+    //[v0]~^ ERROR symbol-name
     //[v0]~| ERROR demangling
     //[v0]~| ERROR demangling-alt
     fn method(&self) {}
@@ -43,10 +38,7 @@ trait Baz {
 
 impl Baz for &(dyn for<'b> Send + FnMut(&u8)) {
     #[rustc_symbol_name]
-    //[legacy]~^ ERROR symbol-name
-    //[legacy]~| ERROR demangling
-    //[legacy]~| ERROR demangling-alt
-    //[v0]~^^^^ ERROR symbol-name
+    //[v0]~^ ERROR symbol-name
     //[v0]~| ERROR demangling
     //[v0]~| ERROR demangling-alt
     fn method(&self) {}
