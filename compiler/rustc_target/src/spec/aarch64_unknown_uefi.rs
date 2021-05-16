b648ac62ca35738a7ef02cefad87f323b7d90dca
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-05-16 11:09:54 +0800

    Update aarch64_unknown_uefi.rs

--- compiler/rustc_target/src/spec/aarch64_unknown_uefi.rs
+++ compiler/rustc_target/src/spec/aarch64_unknown_uefi.rs
@@ -1,1 +1,22 @@
+// Copyright 2021 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
+// This defines the aarch64 target for UEFI systems as described in the UEFI specification. See the
+// uefi-base module for generic UEFI options. On aarch64 systems (mostly called "arm64" in the spec)
+// UEFI systems always run in long-mode, have the interrupt-controller pre-configured and force a
+// single-CPU execution.
+// The win64 ABI is used. It differs from the sysv64 ABI, so we must use a windows target with
+// LLVM. "aarch64-unknown-windows" is used to get the minimal subset of windows-specific features.
+
+use spec::{LinkerFlavor, LldFlavor, Target, TargetResult};
+
+pub fn target() -> TargetResult {
+
+}
