393ce98b32ced200fc443ca411edcb3383bcdef9
Ralf Jung <post@ralfj.de>
2021-05-23 12:37:52 +0200

    fix a Stacked Borrows test whose output changed

--- tests/compile-fail/stacked_borrows/static_memory_modification.rs
+++ tests/compile-fail/stacked_borrows/static_memory_modification.rs
@@ -3,6 +3,6 @@ static X: usize = 5;
 #[allow(mutable_transmutes)]
 fn main() {
     let _x = unsafe {
-        std::mem::transmute::<&usize, &mut usize>(&X) //~ ERROR borrow stack
+        std::mem::transmute::<&usize, &mut usize>(&X) //~ ERROR writing to alloc0 which is read-only
     };
 }
