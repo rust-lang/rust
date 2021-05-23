10bafe1975e53769180701508e2b8cd3a3b34a0e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2021-05-23 08:09:39 -0700

    Remove toggle for "undocumented items."
    Per discussion in #84326. For trait implementations, this was
    misleading: the items actually do have documentation (but it comes from
    the trait definition).
    
    For both trait implementations and trait implementors, this was
    redundant: in both of those cases, the items are default-hidden by
    different toggle at the level above.
    
    Update tests: Remove XPath selectors that over-specified on details tag,
    in cases that weren't testing toggles. Add an explicit test for toggles
    on methods. Rename item-hide-threshold to toggle-item-contents for
    consistency.

--- src/test/rustdoc/toggle-trait-fn.rs
+++ src/test/rustdoc/toggle-trait-fn.rs
@@ -1,11 +1,23 @@
 #![crate_name = "foo"]
 
+// Trait methods with documentation should be wrapped in a <details> toggle with an appropriate
+// summary. Trait methods with no documentation should not be wrapped.
+//
 // @has foo/trait.Foo.html
-// @!has - '//details[@class="rustdoc-toggle"]//code' 'bar'
-// @has - '//code' 'bar'
-// @has - '//details[@class="rustdoc-toggle"]//code' 'foo'
+// @has -  '//details[@class="rustdoc-toggle"]//summary//code' 'is_documented()'
+// @!has - '//details[@class="rustdoc-toggle"]//summary//code' 'not_documented()'
+// @has -  '//details[@class="rustdoc-toggle"]//*[@class="docblock"]' 'is_documented is documented'
+// @has -  '//details[@class="rustdoc-toggle"]//summary//code' 'is_documented_optional()'
+// @!has - '//details[@class="rustdoc-toggle"]//summary//code' 'not_documented_optional()'
+// @has -  '//details[@class="rustdoc-toggle"]//*[@class="docblock"]' 'is_documented_optional is documented'
 pub trait Foo {
-    fn bar() -> ();
-    /// hello
-    fn foo();
+    fn not_documented();
+
+    /// is_documented is documented
+    fn is_documented();
+
+    fn not_documented_optional() {}
+
+    /// is_documented_optional is documented
+    fn is_documented_optional() {}
 }
