9cf2170a79c73f32e1442120242729dc3af992f0
Alessandro Decina <alessandro.d@gmail.com>
2021-05-29 22:17:02 +1000

    BPF: fix #[target_feature(enable = "alu32")]

---
+++ src/test/codegen/bpf-alu32.rs
@@ -0,0 +1,12 @@
+// compile-flags: --emit=asm --target bpfel-unknown-none
+// only-bpf
+#![crate_type = "lib"]
+#![feature(bpf_target_feature)]
+#![no_std]
+
+#[no_mangle]
+#[target_feature(enable = "alu32")]
+// CHECK: define i8 @foo(i8 returned %arg) unnamed_addr #0 {
+pub unsafe fn foo(arg: u8) -> u8 {
+    arg
+}
