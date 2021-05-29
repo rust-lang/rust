bd8e5ce4b9353ad68b2180bf9f26e2d4cfcaf0af
Alessandro Decina <alessandro.d@gmail.com>
2021-05-29 22:18:30 +1000

    BPF: abi: extend args/ret to 32 bits
    Let LLVM extend to 64 bits when alu32 is not enabled

--- compiler/rustc_target/src/abi/call/bpf.rs
+++ compiler/rustc_target/src/abi/call/bpf.rs
@@ -1,11 +1,11 @@
-// see BPFCallingConv.td
+// see https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/BPF/BPFCallingConv.td
 use crate::abi::call::{ArgAbi, FnAbi};
 
 fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
     if ret.layout.is_aggregate() || ret.layout.size.bits() > 64 {
         ret.make_indirect();
     } else {
-        ret.extend_integer_width_to(64);
+        ret.extend_integer_width_to(32);
     }
 }
 
@@ -13,7 +13,7 @@ fn classify_arg<Ty>(arg: &mut ArgAbi<'_, Ty>) {
     if arg.layout.is_aggregate() || arg.layout.size.bits() > 64 {
         arg.make_indirect();
     } else {
-        arg.extend_integer_width_to(64);
+        arg.extend_integer_width_to(32);
     }
 }
 
