160bc68cae213dc204037880c529125442557205
hyd-dev <yd-huang@outlook.com>
2021-05-31 07:59:51 +0800

    Move the test to `src/main.rs`

--- test-cargo-miri/src/lib.rs
+++ test-cargo-miri/src/lib.rs
@@ -1,21 +1,6 @@
-extern crate exported_symbol;
-
 /// Doc-test test
 /// ```rust
 /// assert!(cargo_miri_test::make_true());
-/// // Repeat calls to make sure the `Instance` cache is not broken.
-/// for _ in 0..3 {
-///     extern "Rust" {
-///         fn exported_symbol() -> i32;
-///         fn make_true() -> bool;
-///     }
-///     assert_eq!(unsafe { exported_symbol() }, 123456);
-///     assert!(unsafe { make_true() });
-/// }
-/// ```
-/// ```compile_fail
-/// // Make sure `exported_symbol_dep` is not a direct dependency for doctests.
-/// use exported_symbol_dep;
 /// ```
 /// ```rust,no_run
 /// assert!(!cargo_miri_test::make_true());
