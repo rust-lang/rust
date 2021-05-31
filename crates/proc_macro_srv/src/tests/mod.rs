cfcadcb2959d2dbaf7d95e8b9a33c48b75480e9f
Jonas Schievink <jonasschievink@gmail.com>
2021-05-31 17:32:56 +0200

    Simplify proc_macro_srv tests

--- crates/proc_macro_srv/src/tests/mod.rs
+++ crates/proc_macro_srv/src/tests/mod.rs
@@ -2,64 +2,86 @@
 
 #[macro_use]
 mod utils;
-use test_utils::assert_eq_text;
+use expect_test::expect;
 use utils::*;
 
 #[test]
-fn test_derive_serialize_proc_macro() {
+fn test_derive_empty() {
+    assert_expand("DeriveEmpty", r#"struct S;"#, expect![[r#"SUBTREE $"#]]);
+}
+
+#[test]
+fn test_derive_error() {
     assert_expand(
-        "serde_derive",
-        "Serialize",
-        "1.0",
-        r"struct Foo {}",
-        include_str!("fixtures/test_serialize_proc_macro.txt"),
+        "DeriveError",
+        r#"struct S;"#,
+        expect![[r##"
+            SUBTREE $
+              IDENT   compile_error 4294967295
+              PUNCH   ! [alone] 4294967295
+              SUBTREE () 4294967295
+                LITERAL "#[derive(DeriveError)] struct S ;" 4294967295
+              PUNCH   ; [alone] 4294967295"##]],
     );
 }
 
 #[test]
-fn test_derive_serialize_proc_macro_failed() {
+fn test_fn_like_macro() {
     assert_expand(
-        "serde_derive",
-        "Serialize",
-        "1.0",
-        r"struct {}",
-        r##"
-SUBTREE $
-  IDENT   compile_error 4294967295
-  PUNCH   ! [alone] 4294967295
-  SUBTREE {} 4294967295
-    LITERAL "expected identifier" 4294967295
-"##,
+        "fn_like_noop",
+        r#"ident, 0, 1, []"#,
+        expect![[r#"
+            SUBTREE $
+              IDENT   ident 4294967295
+              PUNCH   , [alone] 4294967295
+              LITERAL 0 4294967295
+              PUNCH   , [alone] 4294967295
+              LITERAL 1 4294967295
+              PUNCH   , [alone] 4294967295
+              SUBTREE [] 4294967295"#]],
     );
 }
 
 #[test]
-fn test_derive_proc_macro_list() {
-    let res = list("serde_derive", "1").join("\n");
-
-    assert_eq_text!(
-        r#"Serialize [CustomDerive]
-Deserialize [CustomDerive]"#,
-        &res
+fn test_attr_macro() {
+    // Corresponds to
+    //    #[proc_macro_test::attr_error(some arguments)]
+    //    mod m {}
+    assert_expand_attr(
+        "attr_error",
+        r#"mod m {}"#,
+        r#"some arguments"#,
+        expect![[r##"
+            SUBTREE $
+              IDENT   compile_error 4294967295
+              PUNCH   ! [alone] 4294967295
+              SUBTREE () 4294967295
+                LITERAL "#[attr_error(some arguments)] mod m {}" 4294967295
+              PUNCH   ; [alone] 4294967295"##]],
     );
 }
 
-/// Tests that we find and classify non-derive macros correctly.
+/// Tests that we find and classify all proc macros correctly.
 #[test]
 fn list_test_macros() {
-    let res = list("proc_macro_test", "0.0.0").join("\n");
+    let res = list().join("\n");
 
-    assert_eq_text!(
-        r#"function_like_macro [FuncLike]
-attribute_macro [Attr]
-DummyTrait [CustomDerive]"#,
-        &res
-    );
+    expect![[r#"
+        fn_like_noop [FuncLike]
+        fn_like_panic [FuncLike]
+        fn_like_error [FuncLike]
+        attr_noop [Attr]
+        attr_panic [Attr]
+        attr_error [Attr]
+        DeriveEmpty [CustomDerive]
+        DerivePanic [CustomDerive]
+        DeriveError [CustomDerive]"#]]
+    .assert_eq(&res);
 }
 
 #[test]
 fn test_version_check() {
-    let path = fixtures::dylib_path("proc_macro_test", "0.0.0");
+    let path = fixtures::proc_macro_test_dylib_path();
     let info = proc_macro_api::read_dylib_info(&path).unwrap();
     assert!(info.version.1 >= 50);
 }
--- crates/proc_macro_srv/src/tests/utils.rs
+++ crates/proc_macro_srv/src/tests/utils.rs
@@ -2,9 +2,9 @@
 
 use crate::dylib;
 use crate::ProcMacroSrv;
+use expect_test::Expect;
 use proc_macro_api::ListMacrosTask;
 use std::str::FromStr;
-use test_utils::assert_eq_text;
 
 pub mod fixtures {
     use cargo_metadata::Message;
@@ -12,9 +12,11 @@ pub mod fixtures {
     use std::process::Command;
 
     // Use current project metadata to get the proc-macro dylib path
-    pub fn dylib_path(crate_name: &str, version: &str) -> std::path::PathBuf {
+    pub fn proc_macro_test_dylib_path() -> std::path::PathBuf {
+        let name = "proc_macro_test";
+        let version = "0.0.0";
         let command = Command::new(toolchain::cargo())
-            .args(&["check", "--tests", "--message-format", "json"])
+            .args(&["build", "-p", name, "--message-format", "json"])
             .output()
             .unwrap()
             .stdout;
@@ -23,7 +25,7 @@ pub mod fixtures {
             match message.unwrap() {
                 Message::CompilerArtifact(artifact) => {
                     if artifact.target.kind.contains(&"proc-macro".to_string()) {
-                        let repr = format!("{} {}", crate_name, version);
+                        let repr = format!("{} {}", name, version);
                         if artifact.package_id.repr.starts_with(&repr) {
                             return PathBuf::from(&artifact.filenames[0]);
                         }
@@ -33,7 +35,7 @@ pub mod fixtures {
             }
         }
 
-        panic!("No proc-macro dylib for {} found!", crate_name);
+        panic!("No proc-macro dylib for {} found!", name);
     }
 }
 
@@ -41,23 +43,26 @@ fn parse_string(code: &str) -> Option<crate::rustc_server::TokenStream> {
     Some(crate::rustc_server::TokenStream::from_str(code).unwrap())
 }
 
-pub fn assert_expand(
-    crate_name: &str,
-    macro_name: &str,
-    version: &str,
-    ra_fixture: &str,
-    expect: &str,
-) {
-    let path = fixtures::dylib_path(crate_name, version);
-    let expander = dylib::Expander::new(&path).unwrap();
-    let fixture = parse_string(ra_fixture).unwrap();
-
-    let res = expander.expand(macro_name, &fixture.into_subtree(), None).unwrap();
-    assert_eq_text!(&expect.trim(), &format!("{:?}", res));
+pub fn assert_expand(macro_name: &str, ra_fixture: &str, expect: Expect) {
+    assert_expand_impl(macro_name, ra_fixture, None, expect);
 }
 
-pub fn list(crate_name: &str, version: &str) -> Vec<String> {
-    let path = fixtures::dylib_path(crate_name, version);
+pub fn assert_expand_attr(macro_name: &str, ra_fixture: &str, attr_args: &str, expect: Expect) {
+    assert_expand_impl(macro_name, ra_fixture, Some(attr_args), expect);
+}
+
+fn assert_expand_impl(macro_name: &str, input: &str, attr: Option<&str>, expect: Expect) {
+    let path = fixtures::proc_macro_test_dylib_path();
+    let expander = dylib::Expander::new(&path).unwrap();
+    let fixture = parse_string(input).unwrap();
+    let attr = attr.map(|attr| parse_string(attr).unwrap().into_subtree());
+
+    let res = expander.expand(macro_name, &fixture.into_subtree(), attr.as_ref()).unwrap();
+    expect.assert_eq(&format!("{:?}", res));
+}
+
+pub fn list() -> Vec<String> {
+    let path = fixtures::proc_macro_test_dylib_path();
     let task = ListMacrosTask { lib: path };
     let mut srv = ProcMacroSrv::default();
     let res = srv.list_macros(&task).unwrap();
