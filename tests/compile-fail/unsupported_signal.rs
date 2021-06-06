0ece55d748a04aba9c4950937de226c259630719
Ralf Jung <post@ralfj.de>
2021-06-06 10:33:46 +0200

    expand comment

--- tests/compile-fail/unsupported_signal.rs
+++ tests/compile-fail/unsupported_signal.rs
@@ -1,4 +1,5 @@
 //! `signal()` is special on Linux and macOS that it's only supported within libstd.
+//! The implementation is not complete enough to permit user code to call it.
 // ignore-windows: No libc on Windows
 #![feature(rustc_private)]
 
