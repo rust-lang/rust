46377c48a400bc39110fb9a930c2c200ceac7cd2
Cameron Steffen <cam.steffen94@gmail.com>
2021-06-07 17:43:35 -0500

    Add unnecessary unsafe test

--- src/test/ui/unsafe/unsafe-around-compiler-generated-unsafe.rs
+++ src/test/ui/unsafe/unsafe-around-compiler-generated-unsafe.rs
@@ -8,4 +8,7 @@ fn main() {
     let _ = async {
         unsafe { async {}.await; } //~ ERROR unnecessary `unsafe`
     };
+
+    // `format_args!` expands with a compiler-generated unsafe block
+    unsafe { println!("foo"); } //~ ERROR unnecessary `unsafe`
 }
