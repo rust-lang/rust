a867dd4c7e65beb1453fcff9e252e7ab19f0e1d0
Richard Cobbe <ricobbe@microsoft.com>
2021-06-08 13:56:06 -0700

    Add support for raw-dylib with stdcall, fastcall functions on i686-pc-windows-msvc.

---
+++ src/test/ui/rfc-2627-raw-dylib/unsupported-abi.rs
@@ -0,0 +1,13 @@
+// only-x86_64-pc-windows-msvc
+// compile-flags: --crate-type lib --emit link
+#![allow(incomplete_features)]
+#![feature(raw_dylib)]
+#[link(name = "foo", kind = "raw-dylib")]
+extern "stdcall" {
+    fn f(x: i32);
+    //~^ ERROR ABI not supported by `#[link(kind = "raw-dylib")]` on this architecture
+}
+
+pub fn lib_main() {
+    unsafe { f(42); }
+}
