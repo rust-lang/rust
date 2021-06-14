da2ed6f768452777c467a0d1e1a77fb92d9165c0
hyd-dev <yd-huang@outlook.com>
2021-06-14 20:56:03 +0800

    Don't report UB for `#[no_mangle]` on associated functions

--- test-cargo-miri/exported-symbol-dep/src/lib.rs
+++ test-cargo-miri/exported-symbol-dep/src/lib.rs
@@ -2,3 +2,12 @@
 fn exported_symbol() -> i32 {
     123456
 }
+
+pub struct AssocFn;
+
+impl AssocFn {
+    #[no_mangle]
+    pub fn assoc_fn_as_exported_symbol() -> i32 {
+        -123456
+    }
+}
