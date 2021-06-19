bc243a7f5597dbc6bd6947e9d799ed9fba428634
Yuki Okushi <yuki.okushi@huawei.com>
2021-06-19 11:46:14 +0900

    Refactor `PatternError` structure

--- src/test/ui/non-constant-in-const-path.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let x = 0;
-    match 1 {
-        0 ..= x => {}
-        //~^ ERROR runtime values cannot be referenced in patterns
-    };
-}
--- src/test/ui/pattern/issue-68394-let-pat-runtime-value.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let x = 255u8;
-    let 0u8..=x = 0;
-    //~^ ERROR runtime values cannot be referenced in patterns
-}
