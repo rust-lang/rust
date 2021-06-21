ad8a30e2ab39e0be12cb614f37f0cb0b78ba44cc
Ian Jackson <ijackson@chiark.greenend.org.uk>
2021-06-21 23:53:37 +0100

    it captures things (but debugging and junk)

--- src/tools/errorkind-os-codes/src/main.rs
+++ src/tools/errorkind-os-codes/src/main.rs
@@ -2,10 +2,11 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::mem;
 
 use regex::Regex;
 
-fn some_codes(src: &str, fn_start: &str, scope_retxt: &str) -> HashMap<String, String> {
+fn some_codes(src: &str, fn_start: &str, scope_retxt: &str) -> HashMap<String, Vec<String>> {
     let f = File::open(src).expect(&format!("failed to open {}", src));
     let f = BufReader::new(f);
     let mut f = f.lines().zip(1..);
@@ -18,11 +19,15 @@ fn some_codes(src: &str, fn_start: &str, scope_retxt: &str) -> HashMap<String, S
         panic!("failed to find {:?} in {}", fn_start, src);
     };
 
-    let ignore_re = Regex::new(r#"^ *(?:use |match| _ *=> | +\}|  } *| *$"#).unwrap();
+    let ignore_re = Regex::new(r#"^ *(?:use |match| _ *=> | +\}|  } *| *$)"#).unwrap();
     let ent_retxt = format!(r#"{}\w+\b"#, scope_retxt);
     let lhs_retxt = format!(r#"^ *\|? +({}(?: *\| *{})*)"#, &ent_retxt, &ent_retxt);
     let part_re = Regex::new(&format!(r#"({}) *\|? *$"#, &lhs_retxt)).unwrap();
-    let full_re = Regex::new(&format!(r#"({}) *=> *(?:return)? *(\w+),? *$#"#, &lhs_retxt)).unwrap();
+    let full_retxt = format!(r#"({}) *=> *(?:return)? *(?P<k>\w+),? *$"#, &lhs_retxt);
+    let full_re = Regex::new(&full_retxt).unwrap();
+    eprintln!("lhs_retxt  {}", &lhs_retxt);
+    eprintln!("full_retxt {}", &full_retxt);
+
 
     let mut current = String::new();
     let mut out = HashMap::new();
@@ -41,16 +46,17 @@ fn some_codes(src: &str, fn_start: &str, scope_retxt: &str) -> HashMap<String, S
             more_current(&mut current, &m);
         } else if let Some(m) = full_re.captures(&l) {
             more_current(&mut current, &m);
-            let key = m.get(2).unwrap().as_str();
-            for e in current.split('|') {
+            let key = m.name("k").unwrap().as_str();
+dbg!(&current, &key);
+            for e in mem::take(&mut current).split('|') {
                 let e = e.trim();
                 if e.len() == 0 { continue }
-                if let Some(_) = out.insert(key.to_owned(), e.to_owned()) {
-                    panic!("duplicate mapping in {} for {:?}", src, &e);
-                }
+                out.entry(key.to_owned())
+                    .or_insert(vec![])
+                    .push(e.to_owned())
             }
         } else {
-            panic!("{}:{}: uncategorisable line", src, lno);
+            panic!("{}:{}: uncategorisable line {:?}", src, lno, &l);
         }
     }
     out
