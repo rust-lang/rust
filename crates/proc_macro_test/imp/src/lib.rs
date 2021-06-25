c27fdc75fad67381e43cd7ce7e92a59f5e8fe087
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-06-25 10:32:13 +0300

    internal: add cloning macro fixture
    Macro that deep clone the tokens but otherwise preserves source
    locations and hygiene info is an interesting case for IDE support. Lets
    have this, although we don't actively use it at the moment.

--- crates/proc_macro_test/imp/src/lib.rs
+++ crates/proc_macro_test/imp/src/lib.rs
@@ -1,6 +1,6 @@
 //! Exports a few trivial procedural macros for testing.
 
-use proc_macro::TokenStream;
+use proc_macro::{Group, Ident, Punct, TokenStream, TokenTree};
 
 #[proc_macro]
 pub fn fn_like_noop(args: TokenStream) -> TokenStream {
@@ -15,6 +15,11 @@ pub fn fn_like_panic(args: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn fn_like_error(args: TokenStream) -> TokenStream {
     format!("compile_error!(\"fn_like_error!({})\");", args).parse().unwrap()
+}
+
+#[proc_macro]
+pub fn fn_like_clone_tokens(args: TokenStream) -> TokenStream {
+    clone_stream(args)
 }
 
 #[proc_macro_attribute]
@@ -45,4 +50,25 @@ pub fn derive_panic(item: TokenStream) -> TokenStream {
 #[proc_macro_derive(DeriveError)]
 pub fn derive_error(item: TokenStream) -> TokenStream {
     format!("compile_error!(\"#[derive(DeriveError)] {}\");", item).parse().unwrap()
+}
+
+fn clone_stream(ts: TokenStream) -> TokenStream {
+    ts.into_iter().map(clone_tree).collect()
+}
+
+fn clone_tree(t: TokenTree) -> TokenTree {
+    match t {
+        TokenTree::Group(orig) => {
+            let mut new = Group::new(orig.delimiter(), clone_stream(orig.stream()));
+            new.set_span(orig.span());
+            TokenTree::Group(new)
+        }
+        TokenTree::Ident(orig) => TokenTree::Ident(Ident::new(&orig.to_string(), orig.span())),
+        TokenTree::Punct(orig) => {
+            let mut new = Punct::new(orig.as_char(), orig.spacing());
+            new.set_span(orig.span());
+            TokenTree::Punct(new)
+        }
+        TokenTree::Literal(_orig) => unimplemented!(),
+    }
 }
