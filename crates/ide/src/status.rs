d85fa40d58df2055491ccb04d6e570b9933a0da1
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-29 20:32:33 +0100

    Update crates/ide/src/status.rs
    Co-authored-by: Laurențiu Nicola <lnicola@users.noreply.github.com>

--- crates/ide/src/status.rs
+++ crates/ide/src/status.rs
@@ -42,7 +42,7 @@ pub(crate) fn status(db: &RootDatabase, file_id: Option<FileId>) -> String {
     format_to!(buf, "{} (Macros)\n", macro_syntax_tree_stats(db));
     format_to!(buf, "{} in total\n", memory_usage());
     if env::var("RA_COUNT").is_ok() {
-        format_to!(buf, "\nCounts:\n{}", count.to_string());
+        format_to!(buf, "\nCounts:\n{}", count);
     }
 
     if let Some(file_id) = file_id {
