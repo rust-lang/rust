32fa96486a5a9969d6151cf436dca4a31f8da292
Aman Arora <me@aman-arora.com>
2021-06-29 03:33:24 -0400

    Edition 2021 for tests

--- src/test/ui/closures/2229_closure_analysis/optimization/edge_case_run_pass.rs
+++ src/test/ui/closures/2229_closure_analysis/optimization/edge_case_run_pass.rs
@@ -1,8 +1,4 @@
-#![feature(capture_disjoint_fields)]
-//~^ WARNING: the feature `capture_disjoint_fields` is incomplete
-//~| NOTE: `#[warn(incomplete_features)]` on by default
-//~| NOTE: see issue #53488 <https://github.com/rust-lang/rust/issues/53488>
-
+// edition:2021
 // run-pass
 
 #![allow(unused)]
