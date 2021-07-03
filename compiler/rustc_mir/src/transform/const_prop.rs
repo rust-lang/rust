d83c46ffcc57eab48249476344982b8f5ae1f263
Smittyvb <me@smitop.com>
2021-07-03 11:15:14 -0400

    add note about MAX_ALLOC_LIMIT
    Co-authored-by: Ralf Jung <post@ralfj.de>

--- compiler/rustc_mir/src/transform/const_prop.rs
+++ compiler/rustc_mir/src/transform/const_prop.rs
@@ -181,7 +181,7 @@ impl<'mir, 'tcx> ConstPropMachine<'mir, 'tcx> {
 
 impl<'mir, 'tcx> interpret::Machine<'mir, 'tcx> for ConstPropMachine<'mir, 'tcx> {
     compile_time_machine!(<'mir, 'tcx>);
-    const PANIC_ON_ALLOC_FAIL: bool = true; // all allocations are small
+    const PANIC_ON_ALLOC_FAIL: bool = true; // all allocations are small (see `MAX_ALLOC_LIMIT`)
 
     type MemoryKind = !;
 
