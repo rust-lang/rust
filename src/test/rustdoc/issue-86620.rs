fc97fbeef3cbd21d94a321faead3ee64b1a14a61
François Mockers <mockersf@gmail.com>
2021-07-05 01:30:48 +0200

    add mcve as test

---
+++ src/test/rustdoc/issue-86620.rs
@@ -0,0 +1,8 @@
+// aux-build:issue-86620-1.rs
+
+extern crate issue_86620_1;
+
+use issue_86620_1::*;
+
+// @!has issue_86620/struct.S.html '//div[@id="method.vzip"]//a[@class="fnname"]/@href' #tymethod.vzip
+pub struct S;
