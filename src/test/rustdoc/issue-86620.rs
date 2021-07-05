441a350e2c15f88b550921f911edc9e039164f15
François Mockers <mockersf@gmail.com>
2021-07-05 02:09:20 +0200

    add check on anchor link

--- src/test/rustdoc/issue-86620.rs
+++ src/test/rustdoc/issue-86620.rs
@@ -5,4 +5,5 @@ extern crate issue_86620_1;
 use issue_86620_1::*;
 
 // @!has issue_86620/struct.S.html '//div[@id="method.vzip"]//a[@class="fnname"]/@href' #tymethod.vzip
+// @has issue_86620/struct.S.html '//div[@id="method.vzip"]//a[@class="anchor"]/@href' #method.vzip
 pub struct S;
