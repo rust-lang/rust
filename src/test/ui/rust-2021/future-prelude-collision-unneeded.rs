10d6b34d32e377798868586d2d8e25d972cbeb0d
Mara Bos <m-ou.se@m-ou.se>
2021-07-07 13:25:48 +0000

    Add generic types to prelude collision lint test.

--- src/test/ui/rust-2021/future-prelude-collision-unneeded.rs
+++ src/test/ui/rust-2021/future-prelude-collision-unneeded.rs
@@ -23,6 +23,28 @@ impl Hey for X {
     }
 }
 
+struct Y<T>(T);
+
+impl Hey for Y<i32> {
+    fn from_iter(_: i32) -> Self {
+        Y(0)
+    }
+}
+
+struct Z<T>(T);
+
+impl Hey for Z<i32> {
+    fn from_iter(_: i32) -> Self {
+        Z(0)
+    }
+}
+
+impl std::iter::FromIterator<u32> for Z<u32> {
+    fn from_iter<T: IntoIterator<Item = u32>>(_: T) -> Self {
+        todo!()
+    }
+}
+
 fn main() {
     // See https://github.com/rust-lang/rust/issues/86633
     let s = S;
@@ -30,4 +52,7 @@ fn main() {
 
     // See https://github.com/rust-lang/rust/issues/86902
     X::from_iter(1);
+    Y::from_iter(1);
+    Y::<i32>::from_iter(1);
+    Z::<i32>::from_iter(1);
 }
