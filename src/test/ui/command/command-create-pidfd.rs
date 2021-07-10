4a832d32f232a68acdabfd29e526d2a4b6366a1c
Dominik Stolz <d.stolz@tum.de>
2021-07-10 12:58:30 +0200

    Check whether clone3 syscall exists in pidfd test

--- src/test/ui/command/command-create-pidfd.rs
+++ src/test/ui/command/command-create-pidfd.rs
@@ -2,10 +2,28 @@
 // only-linux - pidfds are a linux-specific concept
 
 #![feature(linux_pidfd)]
-use std::os::linux::process::{CommandExt, ChildExt};
+#![feature(rustc_private)]
+
+extern crate libc;
+
+use std::io::Error;
+use std::os::linux::process::{ChildExt, CommandExt};
 use std::process::Command;
 
+fn has_clone3() -> bool {
+    let res = unsafe { libc::syscall(libc::SYS_clone3, 0, 0) };
+    let err = (res == -1)
+        .then(|| Error::last_os_error())
+        .expect("probe syscall should not succeed");
+    err.raw_os_error() != Some(libc::ENOSYS)
+}
+
 fn main() {
+    // pidfds require the clone3 syscall
+    if !has_clone3() {
+        return;
+    }
+
     // We don't assert the precise value, since the standard library
     // might have opened other file descriptors before our code runs.
     let _ = Command::new("echo")
