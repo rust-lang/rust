ba1e13fa668ee8e634030c90714a091d8dbfc73a
Santiago Pastorino <spastorino@gmail.com>
2021-07-14 19:00:48 -0300

    Revert "structural_match: non-structural-match ty closures"
    Reverts #73353

--- compiler/rustc_mir_build/src/thir/pattern/const_to_pat.rs
+++ compiler/rustc_mir_build/src/thir/pattern/const_to_pat.rs
@@ -133,9 +133,6 @@ impl<'a, 'tcx> ConstToPat<'a, 'tcx> {
                     traits::NonStructuralMatchTy::Generator => {
                         "generators cannot be used in patterns".to_string()
                     }
-                    traits::NonStructuralMatchTy::Closure => {
-                        "closures cannot be used in patterns".to_string()
-                    }
                     traits::NonStructuralMatchTy::Param => {
                         bug!("use of a constant whose type is a parameter inside a pattern")
                     }
