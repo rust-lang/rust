1006175e22bf55ebdabca83ce91777852668d277
Joshua Nelson <jyn514@gmail.com>
2021-07-13 21:41:05 -0400

    Remove useless alias from `rustc_span` to itself

--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -53,7 +53,7 @@ extern crate rustc_parse;
 extern crate rustc_passes;
 extern crate rustc_resolve;
 extern crate rustc_session;
-extern crate rustc_span as rustc_span;
+extern crate rustc_span;
 extern crate rustc_target;
 extern crate rustc_trait_selection;
 extern crate rustc_typeck;
