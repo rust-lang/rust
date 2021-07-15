4c0ff4db80d2c6cb4dc204f65f34ce2246e27bfe
Maarten de Vries <maarten@de-vri.es>
2021-07-15 21:25:11 +0200

    Show discriminant before overflow in diagnostic.

--- src/test/ui/enum/enum-discrim-autosizing.rs
+++ src/test/ui/enum/enum-discrim-autosizing.rs
@@ -4,8 +4,10 @@
 // so force the repr.
 #[cfg_attr(not(target_pointer_width = "32"), repr(i32))]
 enum Eu64 {
-    Au64 = 0,
-    Bu64 = 0x8000_0000_0000_0000 //~ERROR already exists
+    Au64 = 0, //~NOTE first use of `0`
+    Bu64 = 0x8000_0000_0000_0000
+    //~^ ERROR discriminant value `0` already exists
+    //~| NOTE enum already has `0` (overflowed from `9223372036854775808`)
 }
 
 fn main() {}
