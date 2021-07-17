afd892a549bb4b9bdbe676bc0b3f9899a750d0a3
lcnr <rust@lcnr.de>
2021-07-17 18:48:07 +0200

    update `TypeFlags` to deal with missing ct substs

--- clippy_utils/src/lib.rs
+++ clippy_utils/src/lib.rs
@@ -1581,7 +1581,7 @@ pub fn fn_has_unsatisfiable_preds(cx: &LateContext<'_>, did: DefId) -> bool {
         .predicates_of(did)
         .predicates
         .iter()
-        .filter_map(|(p, _)| if p.is_global() { Some(*p) } else { None });
+        .filter_map(|(p, _)| if p.is_global(cx.tcx) { Some(*p) } else { None });
     traits::impossible_predicates(
         cx.tcx,
         traits::elaborate_predicates(cx.tcx, predicates)
