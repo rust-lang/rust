cf26458376f47c659c663b1735c395a94a475e5f
Ralf Jung <post@ralfj.de>
2021-07-17 14:43:32 +0200

    test for negative offsets

---
+++ tests/compile-fail/intrinsics/out_of_bounds_ptr_3.rs
@@ -0,0 +1,7 @@
+// error-pattern: pointer to 1 byte starting at offset -1 is out-of-bounds
+fn main() {
+    let v = [0i8; 4];
+    let x = &v as *const i8;
+    let x = unsafe { x.offset(-1) };
+    panic!("this should never print: {:?}", x);
+}
