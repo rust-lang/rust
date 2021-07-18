4156473bca0cf1991383cd3643e1a6d23e38957f
robojumper <robojumper@gmail.com>
2021-07-18 12:21:09 +0200

    Add test for unsupported bound relaxation with incorrect behavior

---
+++ src/test/ui/issues/issue-87199.rs
@@ -0,0 +1,21 @@
+// Regression test for issue #87199, where attempting to relax a bound
+// other than the only supported `?Sized` would still cause the compiler
+// to assume that the `Sized` bound was relaxed.
+
+// check-fail
+
+// Check that these function definitions only emit warnings, not errors
+fn arg<T: ?Send>(_: T) {}
+//~^ warning: default bound relaxed for a type parameter, but this does nothing
+//~^^ the size for values of type `T`
+fn ref_arg<T: ?Send>(_: &T) {}
+//~^ warning: default bound relaxed for a type parameter, but this does nothing
+fn ret() -> impl Iterator<Item = ()> + ?Send { std::iter::empty() }
+//~^ warning: default bound relaxed for a type parameter, but this does nothing
+//~^^ the size for values of type `impl Iterator+?Sized` cannot be known
+
+// Check that there's no `?Sized` relaxation!
+fn main() {
+    ref_arg::<i32>(&5);
+    ref_arg::<[i32]>(&[5]);
+}
