b6e9d069eb950226c8c75d2b516d8575eb6cf54e
Felix S. Klock II <pnkfelix@pnkfx.org>
2021-07-21 22:57:10 -0400

    Allow some temporarily dead code.
    I expect these two methods to come back very soon; noise of removing them to satisfy lint seems wrong.

--- compiler/rustc_passes/src/dead.rs
+++ compiler/rustc_passes/src/dead.rs
@@ -134,6 +134,7 @@ impl<'tcx> MarkSymbolVisitor<'tcx> {
         }
     }
 
+    #[allow(dead_code)] // FIXME(81658): should be used + lint reinstated after #83171 relands.
     fn handle_assign(&mut self, expr: &'tcx hir::Expr<'tcx>) {
         if self
             .typeck_results()
@@ -150,6 +151,7 @@ impl<'tcx> MarkSymbolVisitor<'tcx> {
         }
     }
 
+    #[allow(dead_code)] // FIXME(81658): should be used + lint reinstated after #83171 relands.
     fn check_for_self_assign(&mut self, assign: &'tcx hir::Expr<'tcx>) {
         fn check_for_self_assign_helper(
             tcx: TyCtxt<'tcx>,
