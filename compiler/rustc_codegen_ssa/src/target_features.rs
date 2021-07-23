c97c216efd542fbec5a7ccf9555f3cebb3198cec
Simonas Kazlauskas <git@kazlauskas.me>
2021-07-23 16:19:22 +0300

    Direct users towards using Rust feature names in CLI
    If they are trying to use features rustc doesn't yet know about,
    request a feature request.
    
    Additionally, also warn against using feature names without leading `+`
    or `-` signs.

--- compiler/rustc_codegen_ssa/src/target_features.rs
+++ compiler/rustc_codegen_ssa/src/target_features.rs
@@ -4,6 +4,9 @@ use rustc_session::Session;
 use rustc_span::symbol::sym;
 use rustc_span::symbol::Symbol;
 
+/// Features that control behaviour of rustc, rather than the codegen.
+pub const RUSTC_SPECIFIC_FEATURES: &[&str] = &["crt-static"];
+
 // When adding features to the below lists
 // check whether they're named already elsewhere in rust
 // e.g. in stdarch and whether the given name matches LLVM's
