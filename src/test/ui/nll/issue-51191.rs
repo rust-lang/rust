e8238a78df844692ae6918643e202756183c4fde
ibraheemdev <ibrah1440@gmail.com>
2021-07-25 10:23:48 -0400

    suggest removing unnecessary \&mut as help message

--- src/test/ui/nll/issue-51191.rs
+++ src/test/ui/nll/issue-51191.rs
@@ -1,13 +1,15 @@
+#![allow(unconditional_recursion)]
+
 struct Struct;
 
 impl Struct {
     fn bar(self: &mut Self) {
-        //~^ WARN function cannot return without recursing
         (&mut self).bar();
         //~^ ERROR cannot borrow `self` as mutable, as it is not declared as mutable [E0596]
+        //~^^ HELP try removing `&mut` here
     }
 
-    fn imm(self) {
+    fn imm(self) { //~ HELP consider changing this to be mutable
         (&mut self).bar();
         //~^ ERROR cannot borrow `self` as mutable, as it is not declared as mutable [E0596]
     }
@@ -25,7 +27,8 @@ impl Struct {
     fn mtblref(&mut self) {
         (&mut self).bar();
         //~^ ERROR cannot borrow `self` as mutable, as it is not declared as mutable [E0596]
+        //~^^ HELP try removing `&mut` here
     }
 }
 
-fn main () {}
+fn main() {}
