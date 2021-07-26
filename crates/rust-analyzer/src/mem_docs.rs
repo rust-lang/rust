891867b1f1aa352c96c1c4416a2846efdcc01670
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-07-26 21:18:22 +0300

    fix: correctly update diagnostics when files are opened and closed
    Basically, this tracks the changes to `subscriptions` we use when
    issuing a publish_diagnostics.

--- crates/rust-analyzer/src/mem_docs.rs
+++ crates/rust-analyzer/src/mem_docs.rs
@@ -1,5 +1,7 @@
 //! In-memory document information.
 
+use std::mem;
+
 use rustc_hash::FxHashMap;
 use vfs::VfsPath;
 
@@ -10,6 +12,7 @@ use vfs::VfsPath;
 #[derive(Default, Clone)]
 pub(crate) struct MemDocs {
     mem_docs: FxHashMap<VfsPath, DocumentData>,
+    added_or_removed: bool,
 }
 
 impl MemDocs {
@@ -17,12 +20,14 @@ impl MemDocs {
         self.mem_docs.contains_key(path)
     }
     pub(crate) fn insert(&mut self, path: VfsPath, data: DocumentData) -> Result<(), ()> {
+        self.added_or_removed = true;
         match self.mem_docs.insert(path, data) {
             Some(_) => Err(()),
             None => Ok(()),
         }
     }
     pub(crate) fn remove(&mut self, path: &VfsPath) -> Result<(), ()> {
+        self.added_or_removed = true;
         match self.mem_docs.remove(path) {
             Some(_) => Ok(()),
             None => Err(()),
@@ -32,11 +37,15 @@ impl MemDocs {
         self.mem_docs.get(path)
     }
     pub(crate) fn get_mut(&mut self, path: &VfsPath) -> Option<&mut DocumentData> {
+        // NB: don't set `self.added_or_removed` here, as that purposefully only
+        // tracks changes to the key set.
         self.mem_docs.get_mut(path)
     }
-
     pub(crate) fn iter(&self) -> impl Iterator<Item = &VfsPath> {
         self.mem_docs.keys()
+    }
+    pub(crate) fn take_changes(&mut self) -> bool {
+        mem::replace(&mut self.added_or_removed, false)
     }
 }
 
