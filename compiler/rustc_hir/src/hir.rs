6b6ad781f8cae57aef80d35b1c5b7f36f239ed62
Deadbeef <ent3rm4n@gmail.com>
2021-07-26 12:06:55 +0800

    Fix call-generic-method-nonconst test

--- compiler/rustc_hir/src/hir.rs
+++ compiler/rustc_hir/src/hir.rs
@@ -3261,7 +3261,7 @@ impl<'hir> Node<'hir> {
         }
     }
 
-    /// Returns `Constness::Const` when this node is a const fn/impl.
+    /// Returns `Constness::Const` when this node is a const fn/impl/item.
     pub fn constness(&self) -> Constness {
         match self {
             Node::Item(Item {
@@ -3277,6 +3277,10 @@ impl<'hir> Node<'hir> {
                 ..
             })
             | Node::Item(Item { kind: ItemKind::Impl(Impl { constness, .. }), .. }) => *constness,
+
+            Node::Item(Item { kind: ItemKind::Const(..), .. })
+            | Node::TraitItem(TraitItem { kind: TraitItemKind::Const(..), .. })
+            | Node::ImplItem(ImplItem { kind: ImplItemKind::Const(..), .. }) => Constness::Const,
 
             _ => Constness::NotConst,
         }
