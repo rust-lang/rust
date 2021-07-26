8c2a1e8e432d092896cf0487eca5de324fc06bfc
Deadbeef <ent3rm4n@gmail.com>
2021-07-26 16:53:36 +0800

    allow incomplete features for now

--- library/alloc/tests/lib.rs
+++ library/alloc/tests/lib.rs
@@ -25,6 +25,8 @@
 #![feature(string_remove_matches)]
 #![feature(const_btree_new)]
 #![feature(const_trait_impl)]
+// FIXME remove this when const_trait_impl is not incomplete anymore
+#![allow(incomplete_features)]
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
