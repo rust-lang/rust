0f90e99857546c6f3b868113851620d8aa88dc26
Deadbeef <ent3rm4n@gmail.com>
2021-07-26 12:29:18 +0800

    Fix tests

--- src/test/ui/consts/const-eval/issue-49296.rs
+++ src/test/ui/consts/const-eval/issue-49296.rs
@@ -1,8 +1,10 @@
 // issue-49296: Unsafe shenigans in constants can result in missing errors
 
 #![feature(const_fn_trait_bound)]
+#![feature(const_trait_bound_opt_out)]
+#![allow(incomplete_features)]
 
-const unsafe fn transmute<T: Copy, U: Copy>(t: T) -> U {
+const unsafe fn transmute<T: ?const Copy, U: ?const Copy>(t: T) -> U {
     #[repr(C)]
     union Transmute<T: Copy, U: Copy> {
         from: T,
