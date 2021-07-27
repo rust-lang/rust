b518dc7a316b172f474be97e2139cb36b1b15f2f
Jethro Beekman <jethro@fortanix.com>
2021-07-27 12:11:20 +0200

    Add warning to SGX mutex implementation

--- library/std/src/sys/sgx/mutex.rs
+++ library/std/src/sys/sgx/mutex.rs
@@ -8,6 +8,7 @@ pub struct Mutex {
     inner: SpinMutex<WaitVariable<bool>>,
 }
 
+// not movable: see UnsafeList implementation
 pub type MovableMutex = Box<Mutex>;
 
 // Implementation according to “Operating Systems: Three Easy Pieces”, chapter 28
