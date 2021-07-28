17b2f92e44b65caa5e057c51560336311caf2fb4
Esteban Küber <esteban@kuber.com.ar>
2021-07-28 10:08:39 -0700

    Tweak borrowing suggestion in `for` loop

--- src/test/ui/suggestions/for-i-in-vec.rs
+++ src/test/ui/suggestions/for-i-in-vec.rs
@@ -3,11 +3,14 @@
 
 struct Foo {
     v: Vec<u32>,
+    h: std::collections::HashMap<i32, i32>,
 }
 
 impl Foo {
     fn bar(&self) {
         for _ in self.v { //~ ERROR cannot move out of `self.v` which is behind a shared reference
+        }
+        for _ in self.h { //~ ERROR cannot move out of `self.h` which is behind a shared reference
         }
     }
 }
