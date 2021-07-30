9a2e3f3a8e032ae7b39c33bc7cc8f25ebbef8a8e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2021-07-30 16:01:49 -0400

    Recommend `swap_remove` in `Vec::remove` docs

--- library/alloc/src/vec/mod.rs
+++ library/alloc/src/vec/mod.rs
@@ -1360,6 +1360,12 @@ impl<T, A: Allocator> Vec<T, A> {
     /// Removes and returns the element at position `index` within the vector,
     /// shifting all elements after it to the left.
     ///
+    /// Note: Because this shifts over the remaining elements, it has a
+    /// worst-case performance of O(n). If you don't need the order of elements
+    /// to be preserved, use [`swap_remove`] instead.
+    ///
+    /// [`swap_remove`]: Vec::swap_remove
+    ///
     /// # Panics
     ///
     /// Panics if `index` is out of bounds.
