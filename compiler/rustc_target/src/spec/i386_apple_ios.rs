8f65d154c8d6ea020f74acdb7ab7a62818c022af
Patrick Amrein <amrein@ubique.ch>
2021-08-02 15:26:24 +0200

    allow specifying an ios version for the llvm target

--- compiler/rustc_target/src/spec/i386_apple_ios.rs
+++ compiler/rustc_target/src/spec/i386_apple_ios.rs
@@ -4,7 +4,7 @@ use crate::spec::{StackProbeType, Target, TargetOptions};
 pub fn target() -> Target {
     let base = opts("ios", Arch::I386);
     Target {
-        llvm_target: "i386-apple-ios".to_string(),
+        llvm_target: super::apple_base::ios_sim_llvm_target("i386"),
         pointer_width: 32,
         data_layout: "e-m:o-p:32:32-p270:32:32-p271:32:32-p272:64:64-\
             f64:32:64-f80:128-n8:16:32-S128"
