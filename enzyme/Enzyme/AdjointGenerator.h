a92fcb341fe4d9d5203681b678f361fe8a474956
Ludger Paehler <ludger.paehler@tum.de>
2021-08-03 02:46:48 +0200

    Restore to the previous condition.

--- enzyme/Enzyme/AdjointGenerator.h
+++ enzyme/Enzyme/AdjointGenerator.h
@@ -6392,7 +6392,7 @@ public:
       // may load uncacheable data)
       //    Store and reload it
       if (Mode != DerivativeMode::ReverseModeCombined && subretused &&
-          (orig->mayWriteToMemory() || !gutils->legalRecompute(orig, ValueToValueMapTy(), nullptr))) {
+          (orig->doesNotAccessMemory()) {
         if (!gutils->unnecessaryIntermediates.count(orig)) {
           gutils->cacheForReverse(BuilderZ, newCall,
                                   getIndex(orig, CacheType::Self));
