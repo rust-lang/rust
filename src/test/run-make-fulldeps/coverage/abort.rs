bb68c66c40b7df475c79042d78dab9be3bb7ea00
Alex Crichton <alex@alexcrichton.com>
2021-08-04 07:43:28 -0700

    Fix assertions in `coverage-reports` test
    Update some `C-unwind` bits and then

--- src/test/run-make-fulldeps/coverage/abort.rs
+++ src/test/run-make-fulldeps/coverage/abort.rs
@@ -17,16 +17,11 @@ fn main() -> Result<(), u8> {
             might_abort(false);
         }
         // See discussion (below the `Notes` section) on coverage results for the closing brace.
-        if countdown < 5 {
-            might_abort(false);
-        }
-        // Counts for different regions on one line.
+        if countdown < 5 { might_abort(false); } // Counts for different regions on one line.
         // For the following example, the closing brace is the last character on the line.
         // This shows the character after the closing brace is highlighted, even if that next
         // character is a newline.
-        if countdown < 5 {
-            might_abort(false);
-        }
+        if countdown < 5 { might_abort(false); }
         countdown -= 1;
     }
     Ok(())
