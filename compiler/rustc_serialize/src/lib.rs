823e6f161a977030b9350078e08088a1888eaec4
est31 <MTest31@outlook.com>
2021-08-05 04:53:51 +0200

    Remove box syntax from rustc_serialize

--- compiler/rustc_serialize/src/lib.rs
+++ compiler/rustc_serialize/src/lib.rs
@@ -9,7 +9,6 @@ Core encoding and decoding interfaces.
     html_playground_url = "https://play.rust-lang.org/",
     test(attr(allow(unused_variables), deny(warnings)))
 )]
-#![feature(box_syntax)]
 #![feature(never_type)]
 #![feature(nll)]
 #![feature(associated_type_bounds)]
