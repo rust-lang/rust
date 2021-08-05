45bfc7fe2be4686e996136c6d55610ab1171b0ec
John Schug <john.ips.schug@gmail.com>
2021-08-05 16:55:52 -0700

    Override more Iterator methods in ArrayChunks

--- library/core/src/iter/adapters/array_chunks.rs
+++ library/core/src/iter/adapters/array_chunks.rs
@@ -1,6 +1,53 @@
 use core::iter::FusedIterator;
 use core::mem::{self, MaybeUninit};
+use core::ops::{ControlFlow, Try};
 use core::ptr;
+
+#[derive(Debug)]
+struct Guard<T, const N: usize> {
+    arr: [MaybeUninit<T>; N],
+    init: usize,
+}
+
+impl<T, const N: usize> Guard<T, N> {
+    fn new() -> Self {
+        Self { arr: MaybeUninit::uninit_array(), init: 0 }
+    }
+
+    // SAFETY: The entire array must have been properly initialized.
+    unsafe fn assume_init_read(&mut self) -> [T; N] {
+        debug_assert_eq!(self.init, N);
+        // Reset the tracked initialization state.
+        self.init = 0;
+        // SAFETY: This is safe: `MaybeUninit<T>` is guaranteed to have the same layout
+        // as `T` and per the function's contract the entire array has been initialized.
+        unsafe { mem::transmute_copy(&self.arr) }
+    }
+}
+
+impl<T: Clone, const N: usize> Clone for Guard<T, N> {
+    fn clone(&self) -> Self {
+        let mut new = Self::new();
+        // SAFETY: this raw slice contains only the initialized objects.
+        let src = unsafe { MaybeUninit::slice_assume_init_ref(&self.arr[..self.init]) };
+        MaybeUninit::write_slice_cloned(&mut new.arr[..self.init], src);
+        new.init = self.init;
+        new
+    }
+}
+
+impl<T, const N: usize> Drop for Guard<T, N> {
+    fn drop(&mut self) {
+        debug_assert!(self.init <= N);
+
+        let init_part = &mut self.arr[..self.init];
+        // SAFETY: this raw slice will contain only the initialized objects
+        // that have not been dropped or moved.
+        unsafe {
+            ptr::drop_in_place(MaybeUninit::slice_assume_init_mut(init_part));
+        }
+    }
+}
 
 /// An iterator that yields the elements of another iterator in
 /// chunks of size `N`.
@@ -10,28 +57,15 @@ use core::ptr;
 ///
 /// [`array_chunks`]: Iterator::array_chunks
 #[unstable(feature = "iter_array_chunks", issue = "none")]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ArrayChunks<I: Iterator, const N: usize> {
     iter: I,
-    buffer: [MaybeUninit<I::Item>; N],
-    init: usize,
-}
-
-#[unstable(feature = "iter_array_chunks", issue = "none")]
-impl<I: Iterator, const N: usize> Drop for ArrayChunks<I, N> {
-    fn drop(&mut self) {
-        // SAFETY: This is safe: `remainder_mut` returns exactly the sub-slice
-        // of elements that were initialized but not yielded and so have yet
-        // to be dropped.
-        unsafe {
-            ptr::drop_in_place(self.remainder_mut());
-        }
-    }
+    guard: Guard<I::Item, N>,
 }
 
 impl<I: Iterator, const N: usize> ArrayChunks<I, N> {
     pub(in crate::iter) fn new(iter: I) -> Self {
-        Self { iter, init: 0, buffer: MaybeUninit::uninit_array() }
+        Self { iter, guard: Guard::new() }
     }
 
     /// Returns the remainder of the elements yielded by the original
@@ -40,7 +74,7 @@ impl<I: Iterator, const N: usize> ArrayChunks<I, N> {
     #[unstable(feature = "iter_array_chunks", issue = "none")]
     pub fn remainder(&self) -> &[I::Item] {
         // SAFETY: We know that all elements before `init` are properly initialized.
-        unsafe { MaybeUninit::slice_assume_init_ref(&self.buffer[..self.init]) }
+        unsafe { MaybeUninit::slice_assume_init_ref(&self.guard.arr[..self.guard.init]) }
     }
 
     /// Returns the remainder of the elements yielded by the original
@@ -49,7 +83,7 @@ impl<I: Iterator, const N: usize> ArrayChunks<I, N> {
     #[unstable(feature = "iter_array_chunks", issue = "none")]
     pub fn remainder_mut(&mut self) -> &mut [I::Item] {
         // SAFETY: We know that all elements before `init` are properly initialized.
-        unsafe { MaybeUninit::slice_assume_init_mut(&mut self.buffer[..self.init]) }
+        unsafe { MaybeUninit::slice_assume_init_mut(&mut self.guard.arr[..self.guard.init]) }
     }
 }
 
@@ -58,14 +92,72 @@ impl<I: Iterator, const N: usize> Iterator for ArrayChunks<I, N> {
     type Item = [I::Item; N];
 
     fn next(&mut self) -> Option<Self::Item> {
-        while self.init < N {
-            self.buffer[self.init] = MaybeUninit::new(self.iter.next()?);
-            self.init += 1;
+        while self.guard.init < N {
+            self.guard.arr[self.guard.init] = MaybeUninit::new(self.iter.next()?);
+            self.guard.init += 1;
         }
-        self.init = 0;
-        // SAFETY: This is safe: `MaybeUninit<T>` is guaranteed to have the same layout
-        // as `T` and the entire array has just been initialized.
-        unsafe { Some(mem::transmute_copy(&self.buffer)) }
+        // SAFETY: The entire array has just been initialized.
+        unsafe { Some(self.guard.assume_init_read()) }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (lower, upper) = self.iter.size_hint();
+        (lower / N, upper.map(|x| x / N))
+    }
+
+    fn advance_by(&mut self, n: usize) -> Result<(), usize> {
+        let res = match n.checked_mul(N) {
+            Some(n) => self.iter.advance_by(n),
+            None => self.iter.advance_by(usize::MAX).and(Err(usize::MAX)),
+        };
+        res.map_err(|k| k / N)
+    }
+
+    fn try_fold<Acc, Fold, R>(&mut self, init: Acc, mut fold: Fold) -> R
+    where
+        Self: Sized,
+        Fold: FnMut(Acc, Self::Item) -> R,
+        R: Try<Output = Acc>,
+    {
+        let mut guard = mem::replace(&mut self.guard, Guard::new());
+        let result = self.iter.try_fold(init, |mut acc, x| {
+            guard.arr[guard.init] = MaybeUninit::new(x);
+            guard.init += 1;
+
+            if guard.init == N {
+                // SAFETY: The entire array has just been initialized.
+                let arr = unsafe { guard.assume_init_read() };
+                acc = fold(acc, arr).branch()?;
+            }
+            ControlFlow::Continue(acc)
+        });
+
+        if guard.init != 0 {
+            self.guard = guard;
+        }
+
+        match result {
+            ControlFlow::Continue(acc) => R::from_output(acc),
+            ControlFlow::Break(res) => R::from_residual(res),
+        }
+    }
+
+    fn fold<Acc, Fold>(self, init: Acc, mut fold: Fold) -> Acc
+    where
+        Fold: FnMut(Acc, Self::Item) -> Acc,
+    {
+        let mut guard = Guard::<_, N>::new();
+        self.iter.fold(init, |mut acc, x| {
+            guard.arr[guard.init] = MaybeUninit::new(x);
+            guard.init += 1;
+
+            if guard.init == N {
+                // SAFETY: The entire array has just been initialized.
+                let arr = unsafe { guard.assume_init_read() };
+                acc = fold(acc, arr);
+            }
+            acc
+        })
     }
 }
 
--- library/core/tests/iter/adapters/array_chunks.rs
+++ library/core/tests/iter/adapters/array_chunks.rs
@@ -1,10 +1,16 @@
 use core::cell::Cell;
 use core::iter::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct DropBomb<'a> {
     dropped: bool,
     counter: &'a Cell<usize>,
+}
+
+impl<'a> DropBomb<'a> {
+    fn new(counter: &'a Cell<usize>) -> Self {
+        Self { dropped: false, counter }
+    }
 }
 
 impl Drop for DropBomb<'_> {
@@ -15,6 +21,34 @@ impl Drop for DropBomb<'_> {
         self.dropped = true;
         self.counter.set(self.counter.get() + 1);
     }
+}
+
+#[test]
+fn test_iterator_array_chunks_clone() {
+    let mut iter = (0..=10).array_chunks::<4>();
+    let mut iter2 = iter.clone();
+    for (x, y) in iter.by_ref().zip(iter2.by_ref()) {
+        assert_eq!(x, y);
+    }
+    assert_eq!(iter.remainder(), &[8, 9, 10]);
+    assert_eq!(iter2.remainder(), &[]);
+    assert_eq!(iter2.next(), None);
+    assert_eq!(iter2.remainder(), &[8, 9, 10]);
+
+    let counter = Cell::new(0);
+    let mut iter = once(DropBomb::new(&counter)).cycle().take(11).array_chunks::<3>();
+    let mut iter2 = iter.clone();
+    for (i, (_x, _y)) in iter.by_ref().zip(iter2.by_ref()).enumerate() {
+        assert_eq!(counter.get(), i * 6);
+    }
+    assert_eq!(counter.get(), 18);
+    drop(iter);
+    assert_eq!(counter.get(), 21);
+    assert_eq!(iter2.remainder().len(), 0);
+    assert!(iter2.next().is_none());
+    assert_eq!(iter2.remainder().len(), 2);
+    drop(iter2);
+    assert_eq!(counter.get(), 24);
 }
 
 #[test]
@@ -36,8 +70,7 @@ fn test_iterator_array_chunks_remainder() {
 #[test]
 fn test_iterator_array_chunks_drop() {
     let counter = Cell::new(0);
-    let create =
-        |n| (0..n).map(|_| DropBomb { dropped: false, counter: &counter }).array_chunks::<3>();
+    let create = |n| (0..n).map(|_| DropBomb::new(&counter)).array_chunks::<3>();
 
     let iter = create(5);
     assert_eq!(counter.get(), 0);
@@ -63,4 +96,65 @@ fn test_iterator_array_chunks_drop() {
     assert_eq!(iter.remainder().len(), 2);
     drop(iter);
     assert_eq!(counter.get(), 5);
+}
+
+#[test]
+fn test_iterator_array_chunks_try_fold() {
+    let mut iter = (0..=10).array_chunks::<3>();
+    let result = iter.try_fold(0, |acc, arr| {
+        assert_eq!(arr, [acc * 3, (acc * 3) + 1, (acc * 3) + 2]);
+        if acc == 2 { Err(acc) } else { Ok(acc + 1) }
+    });
+    assert_eq!(result, Err(2));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.remainder(), &[9, 10]);
+
+    let mut iter = (0..10).array_chunks::<2>();
+    let result: Result<_, ()> = iter.try_fold(0, |acc, arr| {
+        assert_eq!(arr, [acc * 2, (acc * 2) + 1]);
+        Ok(acc + 1)
+    });
+    assert_eq!(result, Ok(5));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.remainder(), &[]);
+
+    let counter = Cell::new(0);
+    let mut iter = (0..=10).map(|_| DropBomb::new(&counter)).array_chunks::<3>();
+    let result = iter.try_fold(0, |acc, _arr| {
+        assert_eq!(counter.get(), acc * 3);
+        if acc == 1 { Err(acc) } else { Ok(acc + 1) }
+    });
+    assert_eq!(result, Err(1));
+    assert_eq!(iter.remainder().len(), 0);
+    assert_eq!(counter.get(), 6);
+    drop(iter);
+    assert_eq!(counter.get(), 6);
+
+    counter.set(0);
+    let mut iter = (0..=10).map(|_| DropBomb::new(&counter)).array_chunks::<3>();
+    let result: Result<_, ()> = iter.try_fold(0, |acc, _arr| {
+        assert_eq!(counter.get(), acc * 3);
+        Ok(acc + 1)
+    });
+    assert_eq!(result, Ok(3));
+    assert_eq!(iter.remainder().len(), 2);
+    assert_eq!(counter.get(), 9);
+    drop(iter);
+    assert_eq!(counter.get(), 11);
+}
+
+#[test]
+fn test_iterator_array_chunks_fold() {
+    let result = (0..10).array_chunks::<3>().fold(0, |acc, arr| {
+        assert_eq!(arr, [acc * 3, (acc * 3) + 1, (acc * 3) + 2]);
+        acc + 1
+    });
+    assert_eq!(result, 3);
+
+    let counter = Cell::new(0);
+    (0..10).map(|_| DropBomb::new(&counter)).array_chunks::<3>().fold(0, |acc, _arr| {
+        assert_eq!(counter.get(), acc * 3);
+        acc + 1
+    });
+    assert_eq!(counter.get(), 10);
 }
