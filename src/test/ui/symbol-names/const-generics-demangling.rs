f8810ee171d1549cfb4082d4e1289704aad722e9
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2021-08-11 00:05:25 +0300

    Update rustc-demangle to 0.1.21.

--- src/test/ui/symbol-names/const-generics-demangling.rs
+++ src/test/ui/symbol-names/const-generics-demangling.rs
@@ -6,7 +6,7 @@ pub struct Unsigned<const F: u8>;
 
 #[rustc_symbol_name]
 //~^ ERROR symbol-name(_RMCsno73SFvQKx_1cINtB0_8UnsignedKhb_E)
-//~| ERROR demangling(<c[464da6a86cb672f]::Unsigned<11: u8>>)
+//~| ERROR demangling(<c[464da6a86cb672f]::Unsigned<11u8>>)
 //~| ERROR demangling-alt(<c::Unsigned<11>>)
 impl Unsigned<11> {}
 
@@ -14,7 +14,7 @@ pub struct Signed<const F: i16>;
 
 #[rustc_symbol_name]
 //~^ ERROR symbol-name(_RMs_Csno73SFvQKx_1cINtB2_6SignedKsn98_E)
-//~| ERROR demangling(<c[464da6a86cb672f]::Signed<-152: i16>>)
+//~| ERROR demangling(<c[464da6a86cb672f]::Signed<-152i16>>)
 //~| ERROR demangling-alt(<c::Signed<-152>>)
 impl Signed<-152> {}
 
@@ -22,7 +22,7 @@ pub struct Bool<const F: bool>;
 
 #[rustc_symbol_name]
 //~^ ERROR symbol-name(_RMs0_Csno73SFvQKx_1cINtB3_4BoolKb1_E)
-//~| ERROR demangling(<c[464da6a86cb672f]::Bool<true: bool>>)
+//~| ERROR demangling(<c[464da6a86cb672f]::Bool<true>>)
 //~| ERROR demangling-alt(<c::Bool<true>>)
 impl Bool<true> {}
 
@@ -30,7 +30,7 @@ pub struct Char<const F: char>;
 
 #[rustc_symbol_name]
 //~^ ERROR symbol-name(_RMs1_Csno73SFvQKx_1cINtB3_4CharKc2202_E)
-//~| ERROR demangling(<c[464da6a86cb672f]::Char<'∂': char>>)
+//~| ERROR demangling(<c[464da6a86cb672f]::Char<'∂'>>)
 //~| ERROR demangling-alt(<c::Char<'∂'>>)
 impl Char<'∂'> {}
 
