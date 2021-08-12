f1860d190131c643c3f5db97fdbcb69adc57a789
Mara Bos <m-ou.se@m-ou.se>
2021-08-12 17:35:37 +0200

    Include attributes in removal span for unused imports.

--- compiler/rustc_resolve/src/check_unused.rs
+++ compiler/rustc_resolve/src/check_unused.rs
@@ -98,7 +98,7 @@ impl<'a, 'b> UnusedImportCheckVisitor<'a, 'b> {
 
 impl<'a, 'b> Visitor<'a> for UnusedImportCheckVisitor<'a, 'b> {
     fn visit_item(&mut self, item: &'a ast::Item) {
-        self.item_span = item.span;
+        self.item_span = item.span_with_attributes();
 
         // Ignore is_public import statements because there's no way to be sure
         // whether they're used or not. Also ignore imports with a dummy span
