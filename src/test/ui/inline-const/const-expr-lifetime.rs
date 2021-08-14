f6375f8321c7f607628bee4ad0cdb5c72a6afce7
Gary Guo <gary@garyguo.net>
2021-08-14 04:09:30 +0100

    Add test for issue 78174

---
+++ src/test/ui/inline-const/const-expr-lifetime.rs
@@ -0,0 +1,11 @@
+// run-pass
+
+#![allow(incomplete_features)]
+#![feature(inline_const)]
+
+// rust-lang/rust#78174: ICE: "cannot convert ReErased to a region vid"
+
+fn main() {
+    let foo = const { "foo" };
+    assert_eq!(foo, "foo");
+}
