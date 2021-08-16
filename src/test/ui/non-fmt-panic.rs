82d530577b3366cc62e175771e85139590275a55
Mara Bos <m-ou.se@m-ou.se>
2021-08-16 17:33:42 +0200

    Add tests for non_fmt_panics in generic functions.

--- src/test/ui/non-fmt-panic.rs
+++ src/test/ui/non-fmt-panic.rs
@@ -56,4 +56,25 @@ fn main() {
     }
     panic!("{}"); // OK
     panic!(S); // OK
+
+    a(1);
+    b(1);
+    c(1);
+    d(1);
+}
+
+fn a<T: Send + 'static>(v: T) {
+    panic!(v); //~ WARN panic message is not a string literal
+}
+
+fn b<T: std::fmt::Debug + Send + 'static>(v: T) {
+    panic!(v); //~ WARN panic message is not a string literal
+}
+
+fn c<T: std::fmt::Display + Send + 'static>(v: T) {
+    panic!(v); //~ WARN panic message is not a string literal
+}
+
+fn d<T: std::fmt::Display + std::fmt::Debug + Send + 'static>(v: T) {
+    panic!(v); //~ WARN panic message is not a string literal
 }
