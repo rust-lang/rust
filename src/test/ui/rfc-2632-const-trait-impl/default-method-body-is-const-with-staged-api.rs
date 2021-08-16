85abdf07578f14b9c07b3aed9f6b23b1bbf23f8f
Deadbeef <ent3rm4n@gmail.com>
2021-08-16 14:46:08 +0000

    Add ui test

---
+++ src/test/ui/rfc-2632-const-trait-impl/default-method-body-is-const-with-staged-api.rs
@@ -0,0 +1,18 @@
+// check-pass
+
+// This was an ICE, because the compiler ensures the
+// function to be const when performing const checking,
+// but functions marked with the attribute are not const
+// *and* subject to const checking.
+
+#![feature(staged_api)]
+#![feature(const_trait_impl)]
+#![feature(const_fn_trait_bound)]
+#![stable(since = "1", feature = "foo")]
+
+trait Tr {
+    #[default_method_body_is_const]
+    fn a() {}
+}
+
+fn main() {}
