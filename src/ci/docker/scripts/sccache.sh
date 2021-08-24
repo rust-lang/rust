21d8d5f88101ce15c2072e83d1ccbf7533895b11
Mark Rousskov <mark.simulacrum@gmail.com>
2021-08-24 17:15:14 -0400

    Bump sccache used in CI to v0.2.15
    This skips bumping Windows sccache because we run into compilation failures when
    doing so (-m32 not supported by clang-cl?). Not clear on cause, but seems
    easiest to just hold back.
    
    This should avoid PGO-related failures encountered on Linux, and more broadly
    seems like a good idea on other platforms as well (though it is likely not
    necessary right this moment).

--- src/ci/docker/scripts/sccache.sh
+++ src/ci/docker/scripts/sccache.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+
+# ignore-tidy-linelength
+
 set -ex
 
 case "$(uname -m)" in
     x86_64)
-        url="https://ci-mirrors.rust-lang.org/rustc/2018-04-02-sccache-x86_64-unknown-linux-musl"
+        url="https://ci-mirrors.rust-lang.org/rustc/2021-08-24-sccache-v0.2.15-x86_64-unknown-linux-musl"
         ;;
     aarch64)
-        url="https://ci-mirrors.rust-lang.org/rustc/2019-12-17-sccache-aarch64-unknown-linux-gnu"
+        url="https://ci-mirrors.rust-lang.org/rustc/2021-08-25-sccache-v0.2.15-aarch64-unknown-linux-musl"
         ;;
     *)
         echo "unsupported architecture: $(uname -m)"
