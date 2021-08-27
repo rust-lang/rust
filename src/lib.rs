e9eac6bd93624775209ca7a9c5e7f2ccf13248f5
bjorn3 <bjorn3@users.noreply.github.com>
2021-08-27 15:17:17 +0200

    Remove unnecessary feature gates
    One wasn't necessary anymore at all. Others are only necessary when jit mode is enabled.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![feature(rustc_private, decl_macro, never_type, hash_drain_filter, vec_into_raw_parts, once_cell)]
+#![feature(rustc_private, decl_macro)]
+#![cfg_attr(feature = "jit", feature(never_type, vec_into_raw_parts, once_cell))]
 #![warn(rust_2018_idioms)]
 #![warn(unused_lifetimes)]
 #![warn(unreachable_pub)]
