ee02c8e20a5098e77251bb07cca5fe9b932c84d7
Deadbeef <ent3rm4n@gmail.com>
2021-08-28 15:53:26 +0000

    Add test cases

--- src/test/ui/rfc-2632-const-trait-impl/trait-where-clause.rs
+++ src/test/ui/rfc-2632-const-trait-impl/trait-where-clause.rs
@@ -6,28 +6,35 @@ trait Bar {}
 trait Foo {
     fn a();
     fn b() where Self: ~const Bar;
+    fn c<T: ~const Bar>();
 }
 
 const fn test1<T: ~const Foo + Bar>() {
     T::a();
     T::b();
     //~^ ERROR the trait bound
+    T::c::<T>();
+    //~^ ERROR the trait bound
 }
 
 const fn test2<T: ~const Foo + ~const Bar>() {
     T::a();
     T::b();
+    T::c::<T>();
 }
 
 fn test3<T: Foo>() {
     T::a();
     T::b();
     //~^ ERROR the trait bound
+    T::c::<T>();
+    //~^ ERROR the trait bound
 }
 
 fn test4<T: Foo + Bar>() {
     T::a();
     T::b();
+    T::c::<T>();
 }
 
 fn main() {}
