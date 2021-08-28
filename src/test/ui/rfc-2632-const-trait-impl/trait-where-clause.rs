523490e94ad6aabcc53a30919889ce60b4f327ee
Deadbeef <ent3rm4n@gmail.com>
2021-08-28 07:37:30 +0000

    Allow `~const` bounds on trait assoc functions

---
+++ src/test/ui/rfc-2632-const-trait-impl/trait-where-clause.rs
@@ -0,0 +1,33 @@
+#![feature(const_fn_trait_bound)]
+#![feature(const_trait_impl)]
+
+trait Bar {}
+
+trait Foo {
+    fn a();
+    fn b() where Self: ~const Bar;
+}
+
+const fn test1<T: ~const Foo + Bar>() {
+    T::a();
+    T::b();
+    //~^ ERROR the trait bound
+}
+
+const fn test2<T: ~const Foo + ~const Bar>() {
+    T::a();
+    T::b();
+}
+
+fn test3<T: Foo>() {
+    T::a();
+    T::b();
+    //~^ ERROR the trait bound
+}
+
+fn test4<T: Foo + Bar>() {
+    T::a();
+    T::b();
+}
+
+fn main() {}
