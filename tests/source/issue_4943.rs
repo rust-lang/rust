d19f69cd7106d8e84d4e92dad4d94249128a9d35
Eric <e.lim0322@gmail.com>
2021-08-30 20:59:45 +1200

    test: add missing source for #4943

--- tests/source/issue_4943.rs
+++ tests/source/issue_4943.rs
@@ -2,8 +2,7 @@
 
 impl SomeStruct {
     fn process<T>(v: T) -> <Self as GAT>::R<T>
-    where
-        Self: GAT<R<T> = T>,
+    where Self: GAT<R<T> = T>
     {
         SomeStruct::do_something(v)
     }
