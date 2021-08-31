5390ea46440d051085c0cf06f64cb2720bb41678
Katherine Philip <kurerunoshikaku@yahoo.com>
2021-08-31 08:28:51 -0700

    Move  to the top of file

--- library/core/tests/slice.rs
+++ library/core/tests/slice.rs
@@ -1,5 +1,6 @@
 use core::cell::Cell;
 use core::cmp::Ordering;
+use core::mem::MaybeUninit;
 use core::result::Result::{Err, Ok};
 
 #[test]
@@ -2148,8 +2149,6 @@ fn test_slice_run_destructors() {
 #[test]
 fn test_slice_fill_with_uninit() {
     // This should not UB. See #87891
-    use core::mem::MaybeUninit;
-
     let mut a = [MaybeUninit::<u8>::uninit(); 10];
     a.fill(MaybeUninit::uninit());
 }
