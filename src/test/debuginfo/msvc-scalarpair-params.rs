4e8701e6200d114d9576f8cdf9721b0a81de0118
Wesley Wiser <wesleywiser@microsoft.com>
2021-09-01 15:56:32 -0400

    Fix debuginfo for ScalarPair abi parameters
    Mark all of these as locals so the debugger does not try to interpret
    them as being a pointer to the value. This extends the approach used in
    PR #81898.

--- src/test/debuginfo/msvc-scalarpair-params.rs
+++ src/test/debuginfo/msvc-scalarpair-params.rs
@@ -6,14 +6,14 @@
 // cdb-command: dx r1
 // cdb-check:r1               : (0xa..0xc) [Type: core::ops::range::Range<u32>]
 // cdb-command: dx r2
-// cdb-check:r2               : 0x14 [Type: core::ops::range::Range<u64> *]
+// cdb-check:r2               : (0x14..0x1e) [Type: core::ops::range::Range<u64>]
 
 // cdb-command: g
 
 // cdb-command: dx r1
 // cdb-check:r1               : (0x9..0x64) [Type: core::ops::range::Range<u32>]
 // cdb-command: dx r2
-// cdb-check:r2               : 0xc [Type: core::ops::range::Range<u64> *]
+// cdb-check:r2               : (0xc..0x5a) [Type: core::ops::range::Range<u64>]
 
 // cdb-command: g
 
@@ -22,8 +22,9 @@
 // cdb-check:    [variant]        : Some
 // cdb-check:    [+0x004] __0              : 0x4d2 [Type: [...]]
 // cdb-command: dx o2
-// cdb-check:o2               : 0x1 [Type: enum$<core::option::Option<u64> > *]
-// cdb-check:    [variant]
+// cdb-check:o2               : Some [Type: enum$<core::option::Option<u64> >]
+// cdb-check:    [variant]        : Some
+// cdb-check:    [+0x008] __0              : 0x162e [Type: unsigned __int64]
 
 // cdb-command: g
 
@@ -32,22 +33,22 @@
 // cdb-check:    [0]              : 0xa [Type: unsigned int]
 // cdb-check:    [1]              : 0x14 [Type: unsigned int]
 // cdb-command: dx t2
-// cdb-check:t2               : 0x1e [Type: tuple$<u64,u64> *]
-// cdb-check:    [0]              : Unable to read memory at Address 0x1e
-// cdb-check:    [1]              : Unable to read memory at Address 0x26
+// cdb-check:t2               : (0x1e, 0x28) [Type: tuple$<u64,u64>]
+// cdb-check:    [0]              : 0x1e [Type: unsigned __int64]
+// cdb-check:    [1]              : 0x28 [Type: unsigned __int64]
 
 // cdb-command: g
 
 // cdb-command: dx s
 // cdb-check:s                : "this is a static str" [Type: str]
-// cdb-check:    [len]            : 0x14 [Type: unsigned __int64]
+// cdb-check:    [len]            : 0x14 [Type: unsigned [...]]
 // cdb-check:    [chars]
 
 // cdb-command: g
 
 // cdb-command: dx s
 // cdb-check:s                : { len=0x5 } [Type: slice$<u8>]
-// cdb-check:    [len]            : 0x5 [Type: unsigned __int64]
+// cdb-check:    [len]            : 0x5 [Type: unsigned [...]]
 // cdb-check:    [0]              : 0x1 [Type: unsigned char]
 // cdb-check:    [1]              : 0x2 [Type: unsigned char]
 // cdb-check:    [2]              : 0x3 [Type: unsigned char]
