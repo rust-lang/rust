3a334a0e159141d948a26ae15a6288287ae06a7b
Sasha Pourcelot <sasha.pourcelot@iomentum.com>
2021-09-02 20:13:40 +0200

    Add comment about ruby_style_closure status

--- src/test/ui/expr/malformed_closure/ruby_style_closure.rs
+++ src/test/ui/expr/malformed_closure/ruby_style_closure.rs
@@ -1,3 +1,11 @@
+// Part of issue #27300.
+// The problem here is that ruby-style closures are parsed as blocks whose
+// first statement is a closure. See the issue for more details:
+// https://github.com/rust-lang/rust/issues/27300
+
+// Note: this test represents what the compiler currently emits. The error
+// message will be improved later.
+
 fn main() {
     let p = Some(45).and_then({
         //~^ expected a `FnOnce<({integer},)>` closure, found `Option<_>`
