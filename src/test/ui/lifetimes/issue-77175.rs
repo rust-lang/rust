30f2695392a61760186e3b017a76c808b65e2d3b
Stefano Buliani <stefano.buliani@gmail.com>
2021-09-03 22:35:12 -0700

    Updated test comment To add historical context on the issue

--- src/test/ui/lifetimes/issue-77175.rs
+++ src/test/ui/lifetimes/issue-77175.rs
@@ -2,7 +2,12 @@
 // edition:2018
 // check-pass
 
-// error: lifetime parameter `'a` only used once
+// Prior to the fix, the compiler complained that the 'a lifetime was only used
+// once. This was obviously wrong since the lifetime is used twice: For the s3
+// parameter and the return type. The issue was caused by the compiler
+// desugaring the async function into a generator that uses only a single
+// lifetime, which then the validator complained about becauase of the
+// single_use_lifetimes constraints.
 async fn bar<'a>(s1: String, s2: &'_ str, s3: &'a str) -> &'a str {
     s3
 }
