021b8ff8bd3d351f098847c3507ffb2c6c722aa3
Guillaume Gomez <guillaume.gomez@huawei.com>
2021-09-08 15:09:47 +0200

    Add tests to ensure that <table> don't break doc blocks width anymore

---
+++ src/test/rustdoc/table-in-docblock.rs
@@ -0,0 +1,16 @@
+#![crate_name = "foo"]
+
+// @has foo/struct.Foo.html
+// @count - '//*[@class="docblock"]/div/table' 2
+// @!has - '//*[@class="docblock"]/table'
+/// | hello | hello2 |
+/// | ----- | ------ |
+/// | data  | data2  |
+pub struct Foo;
+
+impl Foo {
+    /// | hello | hello2 |
+    /// | ----- | ------ |
+    /// | data  | data2  |
+    pub fn foo(&self) {}
+}
