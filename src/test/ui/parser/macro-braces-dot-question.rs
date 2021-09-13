7d8d7a03412f6db1402f2713ee96a2040cfb24d2
Mara Bos <m-ou.se@m-ou.se>
2021-09-13 17:50:59 +0200

    Add negative test in macro-braces-dot-question.rs.

--- src/test/ui/parser/macro-braces-dot-question.rs
+++ src/test/ui/parser/macro-braces-dot-question.rs
@@ -5,5 +5,7 @@ use std::io::Write;
 fn main() -> Result<(), std::io::Error> {
     vec! { 1, 2, 3 }.len();
     write! { vec![], "" }?;
+    println!{""}
+    [0]; // separate statement, not indexing into the result of println.
     Ok(())
 }
