af9fd37cd9a003cc26c07abdbd82473d501f95f1
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-09-18 15:22:49 +0300

    internal: minimize use_tree parser tests
    The code here is intentionally dense and does exactly what is written.
    Explaining semantic difference between Rust 2015 and 2018 doesn't help
    with understanding syntax. Better to just add more targeted tests.

--- crates/syntax/test_data/parser/inline/ok/0043_use_alias.rs
+++
@@ -1,8 +0,0 @@
-use some::path as some_name;
-use some::{
- other::path as some_other_name,
- different::path as different_name,
- yet::another::path,
- running::out::of::synonyms::for_::different::*
-};
-use Trait as _;
---
+++ crates/syntax/test_data/parser/inline/ok/0176_use_tree_alias.rs
@@ -0,0 +1,2 @@
+use std as stdlib;
+use Trait as _;
