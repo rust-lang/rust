afb7472bcca54bffcab6841345cbf58e9f5dfa33
Oli Scherer <github35764891676564198441@oli-obk.de>
2021-09-18 09:34:06 +0200

    Add a doc comment to infer_projection

--- compiler/rustc_infer/src/infer/projection.rs
+++ compiler/rustc_infer/src/infer/projection.rs
@@ -7,6 +7,12 @@ use super::type_variable::{TypeVariableOrigin, TypeVariableOriginKind};
 use super::InferCtxt;
 
 impl<'a, 'tcx> InferCtxt<'a, 'tcx> {
+    /// Instead of normalizing an associated type projection,
+    /// this function generates an inference variable and registers
+    /// an obligation that this inference variable must be the result
+    /// of the given projection. This allows us to proceed with projections
+    /// while they cannot be resolved yet due to missing information or
+    /// simply due to the lack of access to the trait resolution machinery.
     pub fn infer_projection(
         &self,
         param_env: ty::ParamEnv<'tcx>,
