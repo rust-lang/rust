80654c3d93e7bb7d8b53a88be45c3e31ea480cdd
Mike Leany <55358344+mikeleany@users.noreply.github.com>
2021-09-18 08:21:52 -0600

    Fix code formatting.

--- compiler/rustc_target/src/spec/x86_64_unknown_none_elf.rs
+++ compiler/rustc_target/src/spec/x86_64_unknown_none_elf.rs
@@ -22,7 +22,8 @@ pub fn target() -> Target {
     Target {
         llvm_target: "x86_64-unknown-none-elf".to_string(),
         pointer_width: 64,
-        data_layout: "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128".to_string(),
+        data_layout: "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+            .to_string(),
         arch: "x86_64".to_string(),
         options: opts,
     }
