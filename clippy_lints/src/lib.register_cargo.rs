e6747df5cd4232d26747a556c263d0a5d9c72414
Michael Wright <mikerite@lavabit.com>
2021-09-18 06:43:39 +0200

    Fix lint register code format
    Also change the generation functions to return `String` instead of
    `Vec<String>`. This makes sense now as the updates aren't line oriented
    anymore.

--- clippy_lints/src/lib.register_cargo.rs
+++ clippy_lints/src/lib.register_cargo.rs
@@ -3,9 +3,9 @@
 // Manual edits will be overwritten.
 
 store.register_group(true, "clippy::cargo", Some("clippy_cargo"), vec![
-LintId::of(cargo_common_metadata::CARGO_COMMON_METADATA),
-LintId::of(feature_name::NEGATIVE_FEATURE_NAMES),
-LintId::of(feature_name::REDUNDANT_FEATURE_NAMES),
-LintId::of(multiple_crate_versions::MULTIPLE_CRATE_VERSIONS),
-LintId::of(wildcard_dependencies::WILDCARD_DEPENDENCIES),
+    LintId::of(cargo_common_metadata::CARGO_COMMON_METADATA),
+    LintId::of(feature_name::NEGATIVE_FEATURE_NAMES),
+    LintId::of(feature_name::REDUNDANT_FEATURE_NAMES),
+    LintId::of(multiple_crate_versions::MULTIPLE_CRATE_VERSIONS),
+    LintId::of(wildcard_dependencies::WILDCARD_DEPENDENCIES),
 ])
