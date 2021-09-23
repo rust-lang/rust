aafa0a78bee297e36612f7e17efe2ce1287a832e
Tim McNamara <code@timmcnamara.co.nz>
2021-09-23 22:53:23 +1200

    Improve grammar and remove guarantee that no allocation will occur

--- library/alloc/src/boxed.rs
+++ library/alloc/src/boxed.rs
@@ -1455,12 +1455,8 @@ impl<T, const N: usize> TryFrom<Box<[T]>> for Box<[T; N]> {
     ///
     /// # Errors
     ///
-<<<<<<< HEAD
     /// Returns the old `Box<[T]>` in the `Err` variant if 
     /// `boxed_slice.len()` does not equal `N`.
-=======
-    /// Returns the old `Box<[T]>` in the `Err` variant if `boxed_slice.len()` does not equal `N`.
->>>>>>> Correctly document the error behavior
     fn try_from(boxed_slice: Box<[T]>) -> Result<Self, Self::Error> {
         if boxed_slice.len() == N {
             Ok(unsafe { Box::from_raw(Box::into_raw(boxed_slice) as *mut [T; N]) })
