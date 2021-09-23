7a3e450df45d2e40cff0ab6a4928084d6f61e993
Wesley Wiser <wesleywiser@microsoft.com>
2021-09-23 15:15:51 -0400

    Add tests to verify the drop order of fields in fully captured upvars

---
+++ src/test/ui/closures/2229_closure_analysis/preserve_field_drop_order2.rs
@@ -0,0 +1,28 @@
+// run-pass
+// check-run-results
+// revisions: twenty_eighteen twenty_twentyone
+// [twenty_eighteen]compile-flags: --edition 2018
+// [twenty_twentyone]compile-flags: --edition 2021
+
+#[derive(Debug)]
+struct Dropable(String);
+
+impl Drop for Dropable {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0)
+    }
+}
+
+#[derive(Debug)]
+struct A {
+    x: Dropable,
+    y: Dropable,
+}
+
+fn main() {
+    let a = A { x: Dropable(format!("x")), y: Dropable(format!("y")) };
+
+    let c = move || println!("{:?} {:?}", a.y, a.x);
+    
+    c();
+}
