d63e0f0e477654cb39036eb6f8b45e2dd6f4d371
Takashi Idobe <idobetakashi@gmail.com>
2021-09-23 17:58:02 -0500

    Add time complexities to linked_list.rs

--- library/alloc/src/collections/linked_list.rs
+++ library/alloc/src/collections/linked_list.rs
@@ -631,6 +631,8 @@ impl<T> LinkedList<T> {
     /// Returns `true` if the `LinkedList` contains an element equal to the
     /// given value.
     ///
+    /// This operation should compute in *O*(*n*) time.
+    ///
     /// # Examples
     ///
     /// ```
@@ -655,6 +657,8 @@ impl<T> LinkedList<T> {
 
     /// Provides a reference to the front element, or `None` if the list is
     /// empty.
+    /// 
+    /// This operation should compute in *O*(*1*) time.
     ///
     /// # Examples
     ///
@@ -675,6 +679,8 @@ impl<T> LinkedList<T> {
 
     /// Provides a mutable reference to the front element, or `None` if the list
     /// is empty.
+    /// 
+    /// This operation should compute in *O*(*1*) time.
     ///
     /// # Examples
     ///
@@ -701,6 +707,8 @@ impl<T> LinkedList<T> {
 
     /// Provides a reference to the back element, or `None` if the list is
     /// empty.
+    /// 
+    /// This operation should compute in *O*(*1*) time.
     ///
     /// # Examples
     ///
@@ -721,6 +729,8 @@ impl<T> LinkedList<T> {
 
     /// Provides a mutable reference to the back element, or `None` if the list
     /// is empty.
+    /// 
+    /// This operation should compute in *O*(*1*) time.
     ///
     /// # Examples
     ///
