945ada5d9fef5af7c38ee5b497843ae13c999f36
Camille GILLOT <gillot.camille@gmail.com>
2021-09-26 15:54:02 +0200

    Update ui-fulldeps.

--- src/test/ui-fulldeps/hash-stable-is-unstable.rs
+++ src/test/ui-fulldeps/hash-stable-is-unstable.rs
@@ -2,9 +2,9 @@
 
 extern crate rustc_data_structures;
 //~^ use of unstable library feature 'rustc_private'
-extern crate rustc_middle;
-//~^ use of unstable library feature 'rustc_private'
 extern crate rustc_macros;
+//~^ use of unstable library feature 'rustc_private'
+extern crate rustc_query_system;
 //~^ use of unstable library feature 'rustc_private'
 
 use rustc_macros::HashStable;
