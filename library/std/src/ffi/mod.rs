86b3dd9e0abb0126f0718a8cade40d14b63fcb46
Nathan Stocks <cleancut@github.com>
2021-09-26 15:38:51 -0600

    stabilize CString::from_vec_with_nul[_unchecked]

--- library/std/src/ffi/mod.rs
+++ library/std/src/ffi/mod.rs
@@ -145,7 +145,7 @@
 
 #[stable(feature = "cstr_from_bytes", since = "1.10.0")]
 pub use self::c_str::FromBytesWithNulError;
-#[unstable(feature = "cstring_from_vec_with_nul", issue = "73179")]
+#[stable(feature = "cstring_from_vec_with_nul", since = "1.57.0")]
 pub use self::c_str::FromVecWithNulError;
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use self::c_str::{CStr, CString, IntoStringError, NulError};
