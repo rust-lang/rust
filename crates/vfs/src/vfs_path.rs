51b1f7707b0742d22241562158c8a2cf22e684d4
龙方淞 <longfangsong@icloud.com>
2021-09-28 10:20:35 +0800

    Update crates/vfs/src/vfs_path.rs
    Co-authored-by: Lukas Wirth <lukastw97@gmail.com>

--- crates/vfs/src/vfs_path.rs
+++ crates/vfs/src/vfs_path.rs
@@ -359,9 +359,7 @@ impl VirtualPath {
             }
             path = &path["../".len()..]
         }
-        while path.starts_with("./") {
-            path = &path["./".len()..]
-        }
+        path = path.trim_start_matches("./");
         res.0 = format!("{}/{}", res.0, path);
         Some(res)
     }
