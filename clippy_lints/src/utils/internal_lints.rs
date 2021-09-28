c2b8882cef03ed7813ac4e3162c3c3e22869f618
flip1995 <philipp.krones@embecosm.com>
2021-09-28 17:40:06 +0100

    Cleanup of rustup changes

--- clippy_lints/src/utils/internal_lints.rs
+++ clippy_lints/src/utils/internal_lints.rs
@@ -1222,5 +1222,10 @@ fn if_chain_local_span(cx: &LateContext<'_>, local: &Local<'_>, if_chain_span: S
     let sm = cx.sess().source_map();
     let span = sm.span_extend_to_prev_str(span, "let", false);
     let span = sm.span_extend_to_next_char(span, ';', false);
-    Span::new(span.lo() - BytePos(3), span.hi() + BytePos(1), span.ctxt(), None)
+    Span::new(
+        span.lo() - BytePos(3),
+        span.hi() + BytePos(1),
+        span.ctxt(),
+        span.parent(),
+    )
 }
