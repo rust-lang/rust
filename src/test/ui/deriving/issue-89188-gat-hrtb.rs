34d95d9090c300b5ad8612b732ed947a67c6666b
Audun Halland <audun.halland@pm.me>
2021-09-29 20:17:33 +0200

    Additional tests (from jackh726)

--- src/test/ui/deriving/issue-89188-gat-hrtb.rs
+++ src/test/ui/deriving/issue-89188-gat-hrtb.rs
@@ -11,4 +11,27 @@ trait CallWithShim: Sized {
 #[derive(Clone)]
 struct ShimMethod<T: CallWithShim + 'static>(pub &'static dyn for<'s> Fn(&'s mut T::Shim<'s>));
 
+trait CallWithShim2: Sized {
+    type Shim<T>;
+}
+
+struct S<'s>(&'s ());
+
+#[derive(Clone)]
+struct ShimMethod2<T: CallWithShim2 + 'static>(pub &'static dyn for<'s> Fn(&'s mut T::Shim<S<'s>>));
+
+trait Trait<'s, 't> {}
+
+#[derive(Clone)]
+struct ShimMethod3<T: CallWithShim2 + 'static>(
+    pub &'static dyn for<'s> Fn(&'s mut T::Shim<dyn for<'t> Trait<'s, 't>>),
+);
+
+trait Trait2 {
+    type As;
+}
+
+#[derive(Clone)]
+struct ShimMethod4<T: Trait2 + 'static>(pub &'static dyn for<'s> Fn(&'s mut T::As));
+
 pub fn main() {}
