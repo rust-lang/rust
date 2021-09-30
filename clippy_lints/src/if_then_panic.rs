4babfae9cba0409d2eed2c5caa623fae0445a50e
surechen <chenshuo17@huawei.com>
2021-09-30 19:06:42 +0800

    fix issue #7731
    Make if_then_else handle situation of cond.kind = ExprKind::DropTemps(ExprKind::Binary(BinOpKind::And || BinOpKind::Or, left, right), ..) =

--- clippy_lints/src/if_then_panic.rs
+++ clippy_lints/src/if_then_panic.rs
@@ -3,7 +3,7 @@ use clippy_utils::higher::PanicExpn;
 use clippy_utils::is_expn_of;
 use clippy_utils::source::snippet_with_applicability;
 use rustc_errors::Applicability;
-use rustc_hir::{Block, Expr, ExprKind, StmtKind, UnOp};
+use rustc_hir::{BinOpKind, Block, Expr, ExprKind, StmtKind, UnOp};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 
@@ -74,10 +74,38 @@ impl LateLintPass<'_> for IfThenPanic {
                 };
                 let mut applicability = Applicability::MachineApplicable;
                 let sugg = snippet_with_applicability(cx, span, "..", &mut applicability);
-
-                let cond_sugg =
-                if let ExprKind::DropTemps(Expr{kind: ExprKind::Unary(UnOp::Not, not_expr), ..}) = cond.kind {
-                    snippet_with_applicability(cx, not_expr.span, "..", &mut applicability).to_string()
+                //let mut cond_sugg = format!("!{}", snippet_with_applicability(cx, cond.span, "..", &mut applicability));
+                let cond_sugg = if let ExprKind::DropTemps(e, ..) = cond.kind {
+                    if let Expr{kind: ExprKind::Unary(UnOp::Not, not_expr), ..} = e {
+                        snippet_with_applicability(cx, not_expr.span, "..", &mut applicability).to_string()
+                    } else if let Expr{kind: ExprKind::Binary(op, left, right), ..} = e {//BinOp{BinOpKind::And, ..}
+                        match op.node {
+                            BinOpKind::And | BinOpKind::Or => {
+                                let left_span =  {
+                                    if let Expr{kind: ExprKind::Unary(UnOp::Not, not_expr), ..} = left {
+                                        snippet_with_applicability(cx, not_expr.span, "..", &mut applicability).to_string()
+                                    } else {
+                                        format!("!{}", snippet_with_applicability(cx, left.span, "..", &mut applicability))
+                                    }
+                                };
+                                let right_span = {
+                                    if let Expr{kind: ExprKind::Unary(UnOp::Not, not_expr), ..} = right {
+                                        snippet_with_applicability(cx, not_expr.span, "..", &mut applicability).to_string()
+                                    } else {
+                                        format!("!{}", snippet_with_applicability(cx, right.span, "..", &mut applicability))
+                                    }
+                                };
+                                if op.node == BinOpKind::And {
+                                  format!("{} || {}", left_span, right_span)
+                                } else  {
+                                  format!("{} && {}", left_span, right_span)
+                                }
+                            }
+                            _ => format!("!({})", snippet_with_applicability(cx, cond.span, "..", &mut applicability))
+                        }
+                    } else {
+                        format!("!{}", snippet_with_applicability(cx, cond.span, "..", &mut applicability))
+                    }
                 } else {
                     format!("!{}", snippet_with_applicability(cx, cond.span, "..", &mut applicability))
                 };
