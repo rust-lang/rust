87241e909933b30bb31af4edf58d1a331589df3c
Audun Halland <audun.halland@pm.me>
2021-10-02 11:26:10 +0200

    Expand one test (from jackh726)

--- src/test/ui/deriving/issue-89188-gat-hrtb.rs
+++ src/test/ui/deriving/issue-89188-gat-hrtb.rs
@@ -20,11 +20,13 @@ struct S<'s>(&'s ());
 #[derive(Clone)]
 struct ShimMethod2<T: CallWithShim2 + 'static>(pub &'static dyn for<'s> Fn(&'s mut T::Shim<S<'s>>));
 
-trait Trait<'s, 't> {}
+trait Trait<'s, 't, 'u> {}
 
 #[derive(Clone)]
 struct ShimMethod3<T: CallWithShim2 + 'static>(
-    pub &'static dyn for<'s> Fn(&'s mut T::Shim<dyn for<'t> Trait<'s, 't>>),
+    pub  &'static dyn for<'s> Fn(
+        &'s mut T::Shim<dyn for<'t> Fn(&'s mut T::Shim<dyn for<'u> Trait<'s, 't, 'u>>)>,
+    ),
 );
 
 trait Trait2 {
