4acef9ac19133e7cd3a0648ab6395c7c4eb3031f
Gary Guo <gary@garyguo.net>
2021-10-03 16:03:34 +0100

    Add unit test for issue 78132

---
+++ src/test/ui/inline-const/const-match-pat-inference.rs
@@ -0,0 +1,12 @@
+// check-pass
+
+#![feature(inline_const)]
+#![allow(incomplete_features)]
+
+fn main() {
+    match 1u64 {
+        0 => (),
+        const { 0 + 1 } => (),
+        const { 2 - 1 } ..= const { u64::MAX } => (),
+    }
+}
