97f0b3cb52d526d906422cb37f7e273c369f735d
Cameron Steffen <cam.steffen94@gmail.com>
2021-10-04 08:20:23 -0500

    Revert "Add unnecessary unsafe test"
    This reverts commit 46377c48a400bc39110fb9a930c2c200ceac7cd2.

--- src/test/ui/unsafe/unsafe-around-compiler-generated-unsafe.rs
+++ src/test/ui/unsafe/unsafe-around-compiler-generated-unsafe.rs
@@ -8,7 +8,4 @@ fn main() {
     let _ = async {
         unsafe { async {}.await; } //~ ERROR unnecessary `unsafe`
     };
-
-    // `format_args!` expands with a compiler-generated unsafe block
-    unsafe { println!("foo"); } //~ ERROR unnecessary `unsafe`
 }
