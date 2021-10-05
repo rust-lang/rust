9af75a824f0d1d07e111ef68c3198568a2b6a513
Noah Lev <camelidcamel@gmail.com>
2021-10-05 13:13:06 -0700

    rustup
    Update to the `HEAD` commit of rust-lang/rust and fix test failure.

--- tests/compile-fail/erroneous_const.rs
+++ tests/compile-fail/erroneous_const.rs
@@ -2,7 +2,6 @@
 //! (https://github.com/rust-lang/miri/issues/1382)
 // Inlining changes the error location
 // compile-flags: -Zmir-opt-level=0
-#![feature(const_panic)]
 #![feature(never_type)]
 #![warn(warnings, const_err)]
 
