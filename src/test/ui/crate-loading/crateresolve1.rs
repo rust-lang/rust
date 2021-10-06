2219e0bb0a9510e9843f4845c57451527bb97633
Noah Lev <camelidcamel@gmail.com>
2021-10-05 20:23:40 -0700

    Test crate loading error stderr
    And remove E0464 from test-exemption list, since it now has a full test.

--- src/test/ui/crate-loading/crateresolve1.rs
+++ src/test/ui/crate-loading/crateresolve1.rs
@@ -1,10 +1,12 @@
-// dont-check-compiler-stderr
 // aux-build:crateresolve1-1.rs
 // aux-build:crateresolve1-2.rs
 // aux-build:crateresolve1-3.rs
-// error-pattern:multiple matching crates for `crateresolve1`
+
+// normalize-stderr-test: "\.so" -> ".dylib"
+// normalize-stderr-test: "\.dll" -> ".dylib"
 
 extern crate crateresolve1;
+//~^ ERROR multiple matching crates for `crateresolve1`
 
 fn main() {
 }
