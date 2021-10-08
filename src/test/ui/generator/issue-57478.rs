4be32f896a7fc7e9db8b92132b147870bd57bc9b
Eric Holk <ericholk@microsoft.com>
2021-10-08 15:09:20 -0700

    Add test case for #57478

---
+++ src/test/ui/generator/issue-57478.rs
@@ -0,0 +1,14 @@
+#![feature(negative_impls, generators)]
+
+struct Foo;
+impl !Send for Foo {}
+
+fn main() {
+    assert_send(|| {
+        let guard = Foo;
+        drop(guard);
+        yield;
+    })
+}
+
+fn assert_send<T: Send>(_: T) {}
