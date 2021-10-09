3827b6451c8cfbab9ec1f21bb385eabfede74022
Tom Farmer <tfarmer7980@gmail.com>
2021-10-09 13:13:15 +0100

    Update invalid crate attributes, add help message
    tidy run
    
    update invalid crate attributes, improve error
    
    update test outputs
    
    de-capitalise error
    
    update tests
    
    Update invalid crate attributes, add help message
    
    Update - generate span without using BytePos
    
    Add correct dependancies
    
    Update - generate suggestion without BytePos
    
    Tidy run
    
    update tests
    
    Generate Suggestion without BytePos
    
    Add all builtin attributes
    
    add err builtin inner attr at top of crate
    
    fix tests
    
    add err builtin inner attr at top of crate
    
    tidy fix
    
    add err builtin inner attr at top of crate

--- src/test/ui/derives/issue-36617.rs
+++ src/test/ui/derives/issue-36617.rs
@@ -1,3 +1,16 @@
 #![derive(Copy)] //~ ERROR cannot determine resolution for the attribute macro `derive`
+//~^ ERROR `derive` attribute cannot be used at crate level
+
+#![test]//~ ERROR cannot determine resolution for the attribute macro `test`
+//~^ ERROR `test` attribute cannot be used at crate level
+
+#![test_case]//~ ERROR cannot determine resolution for the attribute macro `test_case`
+//~^ ERROR `test_case` attribute cannot be used at crate level
+
+#![bench]//~ ERROR cannot determine resolution for the attribute macro `bench`
+//~^ ERROR `bench` attribute cannot be used at crate level
+
+#![global_allocator]//~ ERROR cannot determine resolution for the attribute macro `global_allocator`
+//~^ ERROR `global_allocator` attribute cannot be used at crate level
 
 fn main() {}
