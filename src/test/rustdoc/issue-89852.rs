7b9189aef301bd0e6cb2ec50835d3ce15eee051f
Loïc BRANSTETT <loic.branstett@epitech.eu>
2021-10-15 16:54:31 +0200

    Rework the equivalent test to work with sidebar-items.js

--- src/test/rustdoc/issue-89852.rs
+++ src/test/rustdoc/issue-89852.rs
@@ -3,12 +3,12 @@
 #![no_core]
 #![feature(no_core)]
 
-// @count issue_89852/index.html '//*[@class="macro"]' 2
-// @has - '//*[@class="macro"]/@href' 'macro.repro.html'
+// @matches 'issue_89852/sidebar-items.js' '"repro"'
+// @!matches 'issue_89852/sidebar-items.js' '"repro".*"repro"'
+
 #[macro_export]
 macro_rules! repro {
     () => {};
 }
 
-// @!has issue_89852/macro.repro.html '//*[@class="macro"]/@content' 'repro2'
 pub use crate::repro as repro2;
