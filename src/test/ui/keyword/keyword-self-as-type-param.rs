a72dd4a5b973a3d94876ebea66e428fabaff00b0
Hirochika Matsumoto <git@hkmatsumoto.com>
2021-10-19 01:31:23 +0900

    Explain why `Self` is invalid in generic parameters

--- src/test/ui/keyword/keyword-self-as-type-param.rs
+++ src/test/ui/keyword/keyword-self-as-type-param.rs
@@ -1,10 +1,10 @@
 // Regression test of #36638.
 
 struct Foo<Self>(Self);
-//~^ ERROR expected identifier, found keyword `Self`
-//~^^ ERROR E0392
+//~^ ERROR unexpected keyword `Self` in generic parameters
+//~| ERROR recursive type `Foo` has infinite size
 
 trait Bar<Self> {}
-//~^ ERROR expected identifier, found keyword `Self`
+//~^ ERROR unexpected keyword `Self` in generic parameters
 
 fn main() {}
