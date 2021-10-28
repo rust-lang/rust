1bf47532489a9a35368ae96f1e3acf691e8b7af1
Logan Collins <logan@lcspace.net>
2021-10-28 17:39:51 -0600

    add feature(split_rinclusive) to testing crate

--- library/alloc/tests/lib.rs
+++ library/alloc/tests/lib.rs
@@ -25,6 +25,7 @@
 #![feature(const_btree_new)]
 #![feature(const_default_impls)]
 #![feature(const_trait_impl)]
+#![feature(split_rinclusive)]
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
