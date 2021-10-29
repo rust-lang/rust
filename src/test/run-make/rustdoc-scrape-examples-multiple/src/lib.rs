b8ecc9fefa4c9c763cf74e9c9659ecc00f30ba3a
Will Crichton <wcrichto@cs.stanford.edu>
2021-10-29 13:21:50 -0700

    Fix rare ICE during typeck in rustdoc scrape_examples

--- src/test/run-make/rustdoc-scrape-examples-multiple/src/lib.rs
+++ src/test/run-make/rustdoc-scrape-examples-multiple/src/lib.rs
@@ -1,4 +1,6 @@
 // @has foobar/fn.ok.html '//*[@class="docblock scraped-example-list"]//*[@class="prev"]' ''
 // @has foobar/fn.ok.html '//*[@class="more-scraped-examples"]' ''
+// @has src/ex/ex.rs.html
+// @has foobar/fn.ok.html '//a[@href="../src/ex/ex.rs.html#2"]' ''
 
 pub fn ok() {}
