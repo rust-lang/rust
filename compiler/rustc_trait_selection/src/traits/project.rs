851f2b2f969131442fc6290580d7c3632a3a24ae
Aaron Hill <aa1ronham@gmail.com>
2021-10-30 18:53:00 -0500

    Use SsoHashSet

--- compiler/rustc_trait_selection/src/traits/project.rs
+++ compiler/rustc_trait_selection/src/traits/project.rs
@@ -20,7 +20,7 @@ use super::{Normalized, NormalizedTy, ProjectionCacheEntry, ProjectionCacheKey};
 use crate::infer::type_variable::{TypeVariableOrigin, TypeVariableOriginKind};
 use crate::infer::{InferCtxt, InferOk, LateBoundRegionConversionTime};
 use crate::traits::error_reporting::InferCtxtExt as _;
-use rustc_data_structures::fx::FxHashSet;
+use rustc_data_structures::sso::SsoHashSet;
 use rustc_data_structures::stack::ensure_sufficient_stack;
 use rustc_errors::ErrorReported;
 use rustc_hir::def_id::DefId;
@@ -945,7 +945,7 @@ fn opt_normalize_projection_type<'a, 'b, 'tcx>(
                 Normalized { value: projected_ty, obligations: projected_obligations }
             };
 
-            let mut deduped: FxHashSet<_> = Default::default();
+            let mut deduped: SsoHashSet<_> = Default::default();
             result
                 .obligations
                 .drain_filter(|sub_obligation| !deduped.insert(sub_obligation.clone()));
