e45ca8b905acaff89c31043c71911d815cb1063f
Hans Kratz <hans@appfour.com>
2021-10-31 20:49:57 +0100

    Use ubuntu image to download openssl, curl sources, cacert.pem

--- src/ci/docker/host-x86_64/dist-x86_64-linux/build-openssl.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/build-openssl.sh
@@ -3,21 +3,14 @@
 set -ex
 source shared.sh
 
-VERSION=1.0.2k
+tar xzf openssl.tar.gz
 
-# This needs to be downloaded directly from S3, it can't go through the CDN.
-# That's because the CDN is backed by CloudFront, which requires SNI and TLSv1
-# (without paying an absurd amount of money).
-URL=https://rust-lang-ci-mirrors.s3-us-west-1.amazonaws.com/rustc/openssl-$VERSION.tar.gz
-
-curl $URL | tar xzf -
-
-cd openssl-$VERSION
+cd openssl-*
 hide_output ./config --prefix=/rustroot shared -fPIC
 hide_output make -j$(nproc)
 hide_output make install
 cd ..
-rm -rf openssl-$VERSION
+rm -rf openssl-*
 
 # Make the system cert collection available to the new install.
 ln -nsf /etc/pki/tls/cert.pem /rustroot/ssl/
