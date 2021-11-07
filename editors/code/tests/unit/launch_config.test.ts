1fe48a7184cd94667d0dbfb755b6e8c8bba60f53
Laurențiu Nicola <lnicola@dend.ro>
2021-11-07 10:00:58 +0200

    Avoid deprecated methods in client-side tests

--- editors/code/tests/unit/launch_config.test.ts
+++ editors/code/tests/unit/launch_config.test.ts
@@ -7,22 +7,22 @@ suite('Launch configuration', () => {
         test('A binary', async () => {
             const args = Cargo.artifactSpec(["build", "--package", "pkg_name", "--bin", "pkg_name"]);
 
-            assert.deepEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "pkg_name", "--message-format=json"]);
-            assert.deepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "pkg_name", "--message-format=json"]);
+            assert.deepStrictEqual(args.filter, undefined);
         });
 
         test('One of Multiple Binaries', async () => {
             const args = Cargo.artifactSpec(["build", "--package", "pkg_name", "--bin", "bin1"]);
 
-            assert.deepEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "bin1", "--message-format=json"]);
-            assert.deepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "bin1", "--message-format=json"]);
+            assert.deepStrictEqual(args.filter, undefined);
         });
 
         test('A test', async () => {
             const args = Cargo.artifactSpec(["test", "--package", "pkg_name", "--lib", "--no-run"]);
 
-            assert.deepEqual(args.cargoArgs, ["test", "--package", "pkg_name", "--lib", "--no-run", "--message-format=json"]);
-            assert.notDeepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["test", "--package", "pkg_name", "--lib", "--no-run", "--message-format=json"]);
+            assert.notDeepStrictEqual(args.filter, undefined);
         });
     });
 
@@ -30,23 +30,23 @@ suite('Launch configuration', () => {
         test('A binary', async () => {
             const args = Cargo.artifactSpec(["run", "--package", "pkg_name", "--bin", "pkg_name"]);
 
-            assert.deepEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "pkg_name", "--message-format=json"]);
-            assert.deepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "pkg_name", "--message-format=json"]);
+            assert.deepStrictEqual(args.filter, undefined);
         });
 
 
         test('One of Multiple Binaries', async () => {
             const args = Cargo.artifactSpec(["run", "--package", "pkg_name", "--bin", "bin2"]);
 
-            assert.deepEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "bin2", "--message-format=json"]);
-            assert.deepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["build", "--package", "pkg_name", "--bin", "bin2", "--message-format=json"]);
+            assert.deepStrictEqual(args.filter, undefined);
         });
 
         test('A test', async () => {
             const args = Cargo.artifactSpec(["test", "--package", "pkg_name", "--lib"]);
 
-            assert.deepEqual(args.cargoArgs, ["test", "--package", "pkg_name", "--lib", "--message-format=json", "--no-run"]);
-            assert.notDeepEqual(args.filter, undefined);
+            assert.deepStrictEqual(args.cargoArgs, ["test", "--package", "pkg_name", "--lib", "--message-format=json", "--no-run"]);
+            assert.notDeepStrictEqual(args.filter, undefined);
         });
     });
 });
