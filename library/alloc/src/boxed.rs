db4e60b29f863bbf78351b7f095b221ffe7b93a6
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2021-11-09 07:13:53 -0500

    document Box and box_free connection

--- library/alloc/src/boxed.rs
+++ library/alloc/src/boxed.rs
@@ -169,6 +169,9 @@ use crate::vec::Vec;
 #[lang = "owned_box"]
 #[fundamental]
 #[stable(feature = "rust1", since = "1.0.0")]
+// The declaration of the `Box` struct must be kept in sync with the
+// `alloc::alloc::box_free` function or ICEs will happen. See the comment
+// on `box_free` for more details.
 pub struct Box<
     T: ?Sized,
     #[unstable(feature = "allocator_api", issue = "32838")] A: Allocator = Global,
