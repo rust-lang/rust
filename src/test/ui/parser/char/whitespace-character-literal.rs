65e02be7e43cc5c772f8c4d72a0aecd63c8c4b00
5225225 <5225225@mailbox.org>
2021-11-13 12:46:22 +0000

    Suggest removing the non-printing characters

--- src/test/ui/parser/char/whitespace-character-literal.rs
+++ src/test/ui/parser/char/whitespace-character-literal.rs
@@ -2,8 +2,9 @@
 // characters in it contains a note about non-printing characters.
 
 fn main() {
-    // <hair space>x<zero width space>
     let _hair_space_around = ' x​';
     //~^ ERROR: character literal may only contain one codepoint
     //~| NOTE: there are non-printing characters, the full sequence is `\u{200a}x\u{200b}`
+    //~| HELP: consider removing the non-printing characters
+    //~| SUGGESTION: x
 }
