4f2b1c06506cc1277edf4fa46ed84f4e9e9dc671
Esteban Kuber <esteban@kuber.com.ar>
2021-11-16 03:03:00 +0000

    Remove unnecessary argument

--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -1153,7 +1153,7 @@ impl<'a> Parser<'a> {
     /// Assuming we have just parsed `.`, continue parsing into an expression.
     fn parse_dot_suffix(&mut self, self_arg: P<Expr>, lo: Span) -> PResult<'a, P<Expr>> {
         if self.token.uninterpolated_span().rust_2018() && self.eat_keyword(kw::Await) {
-            return Ok(self.mk_await_expr(self_arg, lo));
+            return Ok(self.mk_await_expr(self_arg));
         }
 
         let fn_span_lo = self.token.span;
@@ -2838,7 +2838,7 @@ impl<'a> Parser<'a> {
         ExprKind::Call(f, args)
     }
 
-    fn mk_await_expr(&mut self, self_arg: P<Expr>, _lo: Span) -> P<Expr> {
+    fn mk_await_expr(&mut self, self_arg: P<Expr>) -> P<Expr> {
         let span = self.prev_token.span;
         let await_expr = self.mk_expr(span, ExprKind::Await(self_arg), AttrVec::new());
         self.recover_from_await_method_call();
