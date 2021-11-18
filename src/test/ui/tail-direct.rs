41d9abd76cd514aca23e3409fe6896a3a7d61d1a
Caio <c410.f3r@gmail.com>
2021-11-18 12:09:34 -0300

    Move some tests to more reasonable directories

--- src/test/ui/tail-direct.rs
+++
@@ -1,7 +0,0 @@
-// run-pass
-
-pub fn main() { assert!((even(42))); assert!((odd(45))); }
-
-fn even(n: isize) -> bool { if n == 0 { return true; } else { return odd(n - 1); } }
-
-fn odd(n: isize) -> bool { if n == 0 { return false; } else { return even(n - 1); } }
