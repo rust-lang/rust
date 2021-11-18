f6db19044f53a2065dec622b259d54ce898e85c6
David Tolnay <dtolnay@gmail.com>
2021-11-18 10:01:57 -0800

    Add ui test of ptr range coercion

--- src/test/ui/iterators/ranges.rs
+++ src/test/ui/iterators/ranges.rs
@@ -7,3 +7,9 @@ fn main() {
     for _ in 0..=10 {}
     for _ in 0.. {}
 }
+
+fn references_do_not_coerce_to_ptr_range(start: &i32, end: &i32) {
+    // Better not turn into Range<*const i32>.
+    for _ in start..end {}
+    //~^ ERROR E0277
+}
