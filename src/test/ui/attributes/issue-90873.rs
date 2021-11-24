7f5d3fff4f138cd5aaeea2a3ec4be44da6e8c308
Camille GILLOT <gillot.camille@gmail.com>
2021-11-24 23:07:13 +0100

    Do not visit attributes in `LateResolutionVisitor`.

--- src/test/ui/attributes/issue-90873.rs
+++ src/test/ui/attributes/issue-90873.rs
@@ -3,3 +3,7 @@
 //~| cannot find attribute `u` in this scope
 //~| `main` function not found in crate `issue_90873`
 //~| missing type for `static` item
+
+#![a={impl std::ops::Neg for i8 {}}]
+//~^ ERROR unexpected token
+//~| ERROR cannot find attribute `a` in this scope
