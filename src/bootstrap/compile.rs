320c47ef1c222962cfb26764de34cd8ffd688b0e
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-11-26 16:47:28 -0500

    Fix bootstrap failing to copy rust-llvm-dwp when cross-compiling rustc

--- src/bootstrap/compile.rs
+++ src/bootstrap/compile.rs
@@ -1161,9 +1161,16 @@ impl Step for Assemble {
             let dst_exe = exe("rust-llvm-dwp", target_compiler.host);
             let llvm_config_bin = builder.ensure(native::Llvm { target: target_compiler.host });
             if !builder.config.dry_run {
-                let llvm_bin_dir = output(Command::new(llvm_config_bin).arg("--bindir"));
-                let llvm_bin_dir = Path::new(llvm_bin_dir.trim());
-                builder.copy(&llvm_bin_dir.join(&src_exe), &libdir_bin.join(&dst_exe));
+                if target_compiler.host == build_compiler.host {
+                    let llvm_bin_dir = output(Command::new(llvm_config_bin).arg("--bindir"));
+                    let llvm_bin_dir = Path::new(llvm_bin_dir.trim());
+                    builder.copy(&llvm_bin_dir.join(&src_exe), &libdir_bin.join(&dst_exe));
+                } else {
+                    // if cross-compiling don't copy the llvm directory for the target instead
+                    let llvm_bin_dir = format!("build/{}/llvm/build/bin", target_compiler.host);
+                    let llvm_bin_dir = Path::new(&llvm_bin_dir);
+                    builder.copy(&llvm_bin_dir.join(&src_exe), &libdir_bin.join(&dst_exe));
+                }
             }
         }
 
