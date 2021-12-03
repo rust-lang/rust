e24045e5873a19690bda6a07a63912adfc375b13
Tobias Kortkamp <t@tobik.me>
2021-12-03 18:48:53 +0100

    Explain why libatomic is not needed on FreeBSD riscv64
    From Jessica Clarke (jrtc27@)

--- src/bootstrap/native.rs
+++ src/bootstrap/native.rs
@@ -250,8 +250,13 @@ impl Step for Llvm {
         }
 
         if !target.contains("freebsd") && target.starts_with("riscv") {
-            // In RISC-V, using C++ atomics require linking to `libatomic` but the LLVM build
-            // system check cannot detect this. Therefore it is set manually here.
+            // RISC-V GCC erroneously requires linking against
+            // `libatomic` when using 1-byte and 2-byte C++
+            // atomics but the LLVM build system check cannot
+            // detect this. Therefore it is set manually here.
+            // FreeBSD uses Clang as its system compiler and
+            // provides no libatomic in its base system so does
+            // not want this.
             if !builder.config.llvm_tools_enabled {
                 cfg.define("CMAKE_EXE_LINKER_FLAGS", "-latomic");
             } else {
