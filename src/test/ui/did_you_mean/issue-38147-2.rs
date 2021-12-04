97bf7b934e740b8b94aaf0e75a17f995444e44b6
Lucas Kent <rubickent@gmail.com>
2021-12-04 18:57:31 +1100

    Improve suggestion to change struct field to &mut

--- src/test/ui/did_you_mean/issue-38147-2.rs
+++ src/test/ui/did_you_mean/issue-38147-2.rs
@@ -1,11 +1,16 @@
 struct Bar<'a> {
-    s: &'a String
+    s: &'a String,
+    // use wonky spaces to ensure we are creating the span correctly
+    longer_name:   &   'a     Vec<u8>
 }
 
 impl<'a> Bar<'a> {
     fn f(&mut self) {
         self.s.push('x');
         //~^ ERROR cannot borrow `*self.s` as mutable, as it is behind a `&` reference
+
+        self.longer_name.push(13);
+        //~^ ERROR cannot borrow `*self.longer_name` as mutable, as it is behind a `&` reference
     }
 }
 
