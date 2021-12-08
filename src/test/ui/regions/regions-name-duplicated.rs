85cbd0bc185c4b9b36162a638c9ec3915df265b2
Camille GILLOT <gillot.camille@gmail.com>
2021-12-08 22:40:16 +0100

    Bless tests.

--- src/test/ui/regions/regions-name-duplicated.rs
+++ src/test/ui/regions/regions-name-duplicated.rs
@@ -1,5 +1,7 @@
-struct Foo<'a, 'a> { //~ ERROR lifetime name `'a` declared twice
-    x: &'a isize
+struct Foo<'a, 'a> {
+    //~^ ERROR lifetime name `'a` declared twice
+    //~| ERROR parameter `'a` is never used [E0392]
+    x: &'a isize,
 }
 
 fn main() {}
