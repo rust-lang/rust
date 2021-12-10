a310dfb4e304c74456501438dc4f68eb64df583e
Esteban Kuber <esteban@kuber.com.ar>
2021-12-10 03:01:37 +0000

    review comment: change wording of suggestion

--- compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
@@ -896,7 +896,7 @@ impl<'a, 'tcx> InferCtxtExt<'tcx> for InferCtxt<'a, 'tcx> {
                     // it is from the local crate.
                     err.span_suggestion_verbose(
                         expr.span.shrink_to_hi().with_hi(span.hi()),
-                        "do not `.await` the expression",
+                        "remove the `.await`",
                         String::new(),
                         Applicability::MachineApplicable,
                     );
