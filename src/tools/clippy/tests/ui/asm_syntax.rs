ead68f0e5cc0d7f1bd2694d401b5b8662851cebb
Amanieu d'Antras <amanieu@gmail.com>
2021-12-13 00:00:51 +0000

    Fix clippy tests

--- src/tools/clippy/tests/ui/asm_syntax.rs
+++ src/tools/clippy/tests/ui/asm_syntax.rs
@@ -1,11 +1,10 @@
 // only-x86_64
 // ignore-aarch64
 
-#![feature(asm)]
-
 #[warn(clippy::inline_asm_x86_intel_syntax)]
 mod warn_intel {
     pub(super) unsafe fn use_asm() {
+        use std::arch::asm;
         asm!("");
         asm!("", options());
         asm!("", options(nostack));
@@ -17,6 +16,7 @@ mod warn_intel {
 #[warn(clippy::inline_asm_x86_att_syntax)]
 mod warn_att {
     pub(super) unsafe fn use_asm() {
+        use std::arch::asm;
         asm!("");
         asm!("", options());
         asm!("", options(nostack));
