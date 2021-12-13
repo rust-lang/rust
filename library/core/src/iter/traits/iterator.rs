715c562d71f18ceb077482fc22339b3a73816b4e
Jonas Berlin <xkr47@outerspace.dyndns.org>
2021-12-13 13:52:17 +0200

    [ReviewFix] Linguistics

--- library/core/src/iter/traits/iterator.rs
+++ library/core/src/iter/traits/iterator.rs
@@ -3029,7 +3029,7 @@ pub trait Iterator {
     /// Instead of stopping at [`None`], the iterator will instead start again,
     /// from the beginning. After iterating again, it will start at the
     /// beginning again. And again. And again. Forever. Note that in case the
-    /// original iterator is empty the resulting iterator will also be empty.
+    /// original iterator is empty, the resulting iterator will also be empty.
     ///
     /// # Examples
     ///
