6b7fcf720a714b07ab8dc5d21aac03ade62bbdcc
Michael Howell <michael@notriddle.com>
2021-12-14 22:44:27 -0700

    fix(rustc_lint): better detect when parens are necessary
    Fixes #90807

--- compiler/rustc_lint/src/unused.rs
+++ compiler/rustc_lint/src/unused.rs
@@ -476,8 +476,11 @@ trait UnusedDelimLint {
 
         lhs_needs_parens
             || (followed_by_block
-                && match inner.kind {
+                && match &inner.kind {
                     ExprKind::Ret(_) | ExprKind::Break(..) | ExprKind::Yield(..) => true,
+                    ExprKind::Range(_lhs, Some(rhs), _limits) => {
+                        !classify::expr_requires_semi_to_be_stmt(&rhs)
+                    }
                     _ => parser::contains_exterior_struct_lit(&inner),
                 })
     }
