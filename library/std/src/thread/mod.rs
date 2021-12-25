d66a9e16bae86863cfdbd033b49b672da34bdde1
Laurence Tratt <laurie@tratt.net>
2021-12-25 15:18:55 +0000

    Language tweak.

--- library/std/src/thread/mod.rs
+++ library/std/src/thread/mod.rs
@@ -967,8 +967,8 @@ pub fn park_timeout(dur: Duration) {
 ///
 /// A `ThreadId` is an opaque object that uniquely identifies each thread
 /// created during the lifetime of a process. `ThreadId`s are guaranteed not to
-/// be reused, even if a thread dies. `ThreadId`s are under the control of
-/// Rust's standard library and there may not be any relationship between
+/// be reused, even when a thread terminates. `ThreadId`s are under the control
+/// of Rust's standard library and there may not be any relationship between
 /// `ThreadId` and the underlying platform's notion of a thread identifier --
 /// the two concepts cannot, therefore, be used interchangeably. A `ThreadId`
 /// can be retrieved from the [`id`] method on a [`Thread`].
