e6e46055df8707e4780510f5f3794b4fbee07203
bjorn3 <bjorn3@users.noreply.github.com>
2021-12-31 16:57:33 +0100

    Remove unused extern crate rustc_symbol_mangling

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,6 @@ extern crate rustc_metadata;
 extern crate rustc_middle;
 extern crate rustc_session;
 extern crate rustc_span;
-extern crate rustc_symbol_mangling;
 extern crate rustc_target;
 
 // This prevents duplicating functions and statics that are already part of the host rustc process.
