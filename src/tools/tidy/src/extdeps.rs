0a7dbb689ae8b43e30764f2d484f22122cce1d77
Nicholas Nethercote <n.nethercote@gmail.com>
2022-01-05 12:08:18 +1100

    More hacks, trying to make this work on CI.

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -4,7 +4,10 @@ use std::fs;
 use std::path::Path;
 
 /// List of allowed sources for packages.
-const ALLOWED_SOURCES: &[&str] = &["\"registry+https://github.com/rust-lang/crates.io-index\""];
+const ALLOWED_SOURCES: &[&str] = &[
+    "\"registry+https://github.com/rust-lang/crates.io-index\"",
+    r#""https://github.com/nnethercote/hashbrown?branch=add-is_empty-checks#dec8e8cc""#,
+];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
 /// workspace `Cargo.toml`.
