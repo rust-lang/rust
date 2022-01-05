439efc144321c912546caacb9ef5d5061ffafd53
Mark Rousskov <mark.simulacrum@gmail.com>
2022-01-05 15:33:10 -0500

    Remove portable_simd
    Stand-in for a backport of "Sync portable-simd to remove autosplats #91484".

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -323,7 +323,6 @@
 #![feature(panic_internals)]
 #![feature(panic_unwind)]
 #![feature(pin_static_ref)]
-#![cfg_attr(not(bootstrap), feature(portable_simd))]
 #![feature(prelude_import)]
 #![feature(ptr_internals)]
 #![feature(rustc_attrs)]
@@ -475,9 +474,6 @@ pub use core::pin;
 pub use core::ptr;
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use core::result;
-#[unstable(feature = "portable_simd", issue = "86656")]
-#[cfg(not(bootstrap))]
-pub use core::simd;
 #[unstable(feature = "async_stream", issue = "79024")]
 pub use core::stream;
 #[stable(feature = "i128", since = "1.26.0")]
