e18b23b7f494d57090be94351e92c1d69251f1a9
Noah Lev <camelidcamel@gmail.com>
2022-01-06 13:59:01 -0800

    Move two intra-doc-link tests into the `intra-doc` folder

--- src/test/rustdoc/intra-doc/prim-self.rs
+++ src/test/rustdoc/intra-doc/prim-self.rs
@@ -7,7 +7,7 @@
 #[lang = "usize"]
 /// [Self::f]
 /// [Self::MAX]
-// @has intra_link_prim_self/primitive.usize.html
+// @has prim_self/primitive.usize.html
 // @has - '//a[@href="primitive.usize.html#method.f"]' 'Self::f'
 // @has - '//a[@href="primitive.usize.html#associatedconstant.MAX"]' 'Self::MAX'
 impl usize {
