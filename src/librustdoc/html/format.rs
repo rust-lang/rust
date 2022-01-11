95bd87f849ebbd6a6d4bc24c318c40986a78b4f4
Muhammad Falak R Wani <falakreyaz@gmail.com>
2022-01-11 13:09:05 +0530

    librustdoc: impl core::fmt::Write for rustdoc::html::render::Buffer
    Signed-off-by: Muhammad Falak R Wani <falakreyaz@gmail.com>

--- src/librustdoc/html/format.rs
+++ src/librustdoc/html/format.rs
@@ -64,6 +64,12 @@ crate struct Buffer {
     buffer: String,
 }
 
+impl core::fmt::Write for Buffer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.buffer.write_str(s)
+    }
+}
+
 impl Buffer {
     crate fn empty_from(v: &Buffer) -> Buffer {
         Buffer { for_html: v.for_html, buffer: String::new() }
