474e091160a5704ba6c07dee2d7aa789736ca857
Rune Tynan <runetynan@gmail.com>
2022-01-13 14:46:04 -0500

    Move FIXME to if statement

--- src/librustdoc/json/mod.rs
+++ src/librustdoc/json/mod.rs
@@ -171,10 +171,6 @@ impl<'tcx> FormatRenderer<'tcx> for JsonRenderer<'tcx> {
     /// the hashmap because certain items (traits and types) need to have their mappings for trait
     /// implementations filled out before they're inserted.
     fn item(&mut self, item: clean::Item) -> Result<(), Error> {
-        // FIXME(CraftSpider): We skip children of local blanket implementations, as we'll have
-        //     already seen the actual generic impl, and the generated ones don't need documenting.
-        //     This is necessary due to the visibility, return type, and self arg of the generated
-        //     impls not quite matching, and will no longer be necessary when the mismatch is fixed.
         let local_blanket_impl = match item.def_id {
             clean::ItemId::Blanket { impl_id, .. } => impl_id.is_local(),
             clean::ItemId::Auto { .. }
@@ -183,6 +179,10 @@ impl<'tcx> FormatRenderer<'tcx> for JsonRenderer<'tcx> {
         };
 
         // Flatten items that recursively store other items
+        // FIXME(CraftSpider): We skip children of local blanket implementations, as we'll have
+        //     already seen the actual generic impl, and the generated ones don't need documenting.
+        //     This is necessary due to the visibility, return type, and self arg of the generated
+        //     impls not quite matching, and will no longer be necessary when the mismatch is fixed.
         if !local_blanket_impl {
             item.kind.inner_items().for_each(|i| self.item(i.clone()).unwrap());
         }
