aff5296c78438759f9ea2b6d6f24ddab25d05407
David Tolnay <dtolnay@gmail.com>
2022-01-14 14:33:58 -0800

    Move test of macro-generated macro with linebreak to snapshot

--- src/test/rustdoc/macro-generated-macro.rs
+++ src/test/rustdoc/macro-generated-macro.rs
@@ -20,10 +20,5 @@ make_macro!(attributes #![no_std] #[cfg(feature = "alloc")]);
 // @has - //pre '(<= fn {} () { foo[0] } =>) => { ... };'
 make_macro!(groups fn {}() {foo[0]});
 
-// @has macro_generated_macro/macro.linebreak.html //pre 'macro_rules! linebreak {'
-// @has - //pre '    ('
-// @has - //pre '        <= 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25'
-// @has - //pre '        26 27 28 =>'
-// @has - //pre '    ) => { ... };'
-// @has - //pre '};'
+// @snapshot macro_linebreak_pre macro_generated_macro/macro.linebreak.html //pre
 make_macro!(linebreak 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28);
