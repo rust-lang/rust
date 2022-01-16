af87248037a8c86f580c469ce7d2a10c0b12f2fd
Ivor Wanders <ivor@iwanders.net>
2022-01-16 11:44:10 -0500

    Update unit tests to match more limited hint about escaping.

--- src/test/ui/conditional-compilation/cfg-arg-invalid-6.rs
+++ src/test/ui/conditional-compilation/cfg-arg-invalid-6.rs
@@ -1,3 +1,3 @@
 // compile-flags: --cfg a{
-// error-pattern: invalid `--cfg` argument: `a{` (expected `key` or `key="value"`, ensure escaping is appropriate for your shell, try 'key="value"' or key=\"value\")
+// error-pattern: invalid `--cfg` argument: `a{` (expected `key` or `key="value"`)
 fn main() {}
