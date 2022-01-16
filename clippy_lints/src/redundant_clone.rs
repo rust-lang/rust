94dd91524b4ad99dcb31ac250398b557bbbc7c96
Cameron Steffen <cam.steffen94@gmail.com>
2022-01-16 15:58:54 -0600

    Format clippy

--- clippy_lints/src/redundant_clone.rs
+++ clippy_lints/src/redundant_clone.rs
@@ -624,10 +624,7 @@ impl<'a, 'tcx> mir::visit::Visitor<'tcx> for PossibleBorrowerVisitor<'a, 'tcx> {
                 .flat_map(HybridBitSet::iter)
                 .collect();
 
-            if ContainsRegion
-                .visit_ty(self.body.local_decls[*dest].ty)
-                .is_break()
-            {
+            if ContainsRegion.visit_ty(self.body.local_decls[*dest].ty).is_break() {
                 mutable_variables.push(*dest);
             }
 
