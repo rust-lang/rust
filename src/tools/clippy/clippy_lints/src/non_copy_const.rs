1a6312ee0f667d0c31ceb37b7304e86c924899fd
Cameron Steffen <cam.steffen94@gmail.com>
2022-01-16 15:58:54 -0600

    Format clippy

--- src/tools/clippy/clippy_lints/src/non_copy_const.rs
+++ src/tools/clippy/clippy_lints/src/non_copy_const.rs
@@ -188,10 +188,7 @@ fn is_value_unfrozen_expr<'tcx>(cx: &LateContext<'tcx>, hir_id: HirId, def_id: D
 
     let result = cx.tcx.const_eval_resolve(
         cx.param_env,
-        ty::Unevaluated::new(
-            ty::WithOptConstParam::unknown(def_id),
-            substs,
-        ),
+        ty::Unevaluated::new(ty::WithOptConstParam::unknown(def_id), substs),
         None,
     );
     is_value_unfrozen_raw(cx, result, ty)
--- src/tools/clippy/clippy_utils/src/consts.rs
+++ src/tools/clippy/clippy_utils/src/consts.rs
@@ -413,10 +413,7 @@ impl<'a, 'tcx> ConstEvalLateContext<'a, 'tcx> {
                     .tcx
                     .const_eval_resolve(
                         self.param_env,
-                        ty::Unevaluated::new(
-                            ty::WithOptConstParam::unknown(def_id),
-                            substs,
-                        ),
+                        ty::Unevaluated::new(ty::WithOptConstParam::unknown(def_id), substs),
                         None,
                     )
                     .ok()
