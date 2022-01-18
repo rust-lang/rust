f562b248306adbb6b95d2e6a5ce5c7ef30db144a
David Tolnay <dtolnay@gmail.com>
2022-01-18 11:11:06 -0800

    Convert macro-generated-macro snapshot to /text() using PR 92914

--- src/test/rustdoc/macro-generated-macro.rs
+++ src/test/rustdoc/macro-generated-macro.rs
@@ -20,5 +20,5 @@ make_macro!(attributes #![no_std] #[cfg(feature = "alloc")]);
 // @has - //pre '(<= fn {} () { foo[0] } =>) => { ... };'
 make_macro!(groups fn {}() {foo[0]});
 
-// @snapshot macro_linebreak_pre macro_generated_macro/macro.linebreak.html //pre
+// @snapshot macro_linebreak_pre macro_generated_macro/macro.linebreak.html //pre/text()
 make_macro!(linebreak 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28);
