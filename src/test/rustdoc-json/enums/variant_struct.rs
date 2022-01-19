ab239cc7499c137cc6d4b3b6e9a01b78f7bd3b07
Martin Nordholts <enselic@gmail.com>
2022-01-19 22:03:08 +0100

    src/test/rustdoc-json: Check for `struct_field`s in `variant_tuple_struct.rs`
    The presence of `struct_field`s is being checked for already in
    `variant_struct.rs`. We should also check for them in `variant_tuple_struct.rs`.

--- src/test/rustdoc-json/enums/variant_struct.rs
+++ src/test/rustdoc-json/enums/variant_struct.rs
@@ -2,8 +2,8 @@
 // @has - "$.index[*][?(@.name=='EnumStruct')].kind" \"enum\"
 pub enum EnumStruct {
     // @has - "$.index[*][?(@.name=='VariantS')].inner.variant_kind" \"struct\"
-    // @has - "$.index[*][?(@.name=='x')]"
-    // @has - "$.index[*][?(@.name=='y')]"
+    // @has - "$.index[*][?(@.name=='x')].kind" \"struct_field\"
+    // @has - "$.index[*][?(@.name=='y')].kind" \"struct_field\"
     VariantS {
         x: u32,
         y: String,
