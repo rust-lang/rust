9c0141a4907e9255c5583304def3c16d357a5a14
Deadbeef <ent3rm4n@gmail.com>
2022-01-23 00:13:42 +0800

    Bless rustdoc test

--- src/test/rustdoc/const-generics/generic_const_exprs.rs
+++ src/test/rustdoc/const-generics/generic_const_exprs.rs
@@ -3,5 +3,5 @@
 #![allow(incomplete_features)]
 // make sure that `ConstEvaluatable` predicates dont cause rustdoc to ICE #77647
 // @has foo/struct.Ice.html '//pre[@class="rust struct"]' \
-//      'pub struct Ice<const N: usize> where [(); N + 1]: ;'
+//      'pub struct Ice<const N: usize>;'
 pub struct Ice<const N: usize> where [(); N + 1]:;
