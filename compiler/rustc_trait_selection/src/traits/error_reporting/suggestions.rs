5518d1931d4035a207438d625464dce9c3171d54
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-01-27 16:22:22 +0000

    Guess head span of async blocks

--- compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
@@ -1680,6 +1680,7 @@ impl<'a, 'tcx> InferCtxtExt<'tcx> for InferCtxt<'a, 'tcx> {
             ));
 
             let original_span = err.span.primary_span().unwrap();
+            let original_span = self.tcx.sess.source_map().guess_head_span(original_span);
             let mut span = MultiSpan::from_span(original_span);
 
             let message = outer_generator
