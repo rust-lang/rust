4d2e9651060c984d18cf8863ceacf215ec00446c
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-01-27 16:34:28 +0000

    Make a span more useful

--- compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/suggestions.rs
@@ -1328,6 +1328,7 @@ impl<'a, 'tcx> InferCtxtExt<'tcx> for InferCtxt<'a, 'tcx> {
             ty::Generator(..) => "generator",
             _ => "function",
         };
+        let span = self.tcx.sess.source_map().guess_head_span(span);
         let mut err = struct_span_err!(
             self.tcx.sess,
             span,
