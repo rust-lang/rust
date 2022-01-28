7a7144f4136897605842886b1ca51515e75d654e
woppopo <woppopo@protonmail.com>
2022-01-28 17:27:33 +0900

    test_const_allocate_at_runtime

--- library/core/tests/intrinsics.rs
+++ library/core/tests/intrinsics.rs
@@ -83,7 +83,16 @@ fn test_hints_in_const_contexts() {
 
 #[cfg(not(bootstrap))]
 #[test]
-fn test_const_dealocate_at_runtime() {
+fn test_const_allocate_at_runtime() {
+    use core::intrinsics::const_allocate;
+    unsafe {
+        assert!(const_allocate(4, 4).is_null());
+    }
+}
+
+#[cfg(not(bootstrap))]
+#[test]
+fn test_const_deallocate_at_runtime() {
     use core::intrinsics::const_deallocate;
     const X: &u32 = &42u32;
     let x = &0u32;
