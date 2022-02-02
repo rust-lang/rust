9d857d95ba08764da67d9088906356f91d3b3e80
Tyler Mandry <tmandry@gmail.com>
2022-02-02 22:48:09 +0000

    Make llvm-libunwind a per-target option

--- src/bootstrap/compile.rs
+++ src/bootstrap/compile.rs
@@ -177,7 +177,7 @@ fn copy_third_party_objects(
     }
 
     if target == "x86_64-fortanix-unknown-sgx"
-        || builder.config.llvm_libunwind == LlvmLibunwind::InTree
+        || builder.config.llvm_libunwind(target) == LlvmLibunwind::InTree
             && (target.contains("linux") || target.contains("fuchsia"))
     {
         let libunwind_path =
