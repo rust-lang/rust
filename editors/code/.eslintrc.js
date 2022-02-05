76ecf9a17d7f80e648c0a89921f937239e324ffd
Jack Clayton <jackclayto@gmail.com>
2022-02-06 06:52:51 +0800

    Fix eslint errors

--- editors/code/.eslintrc.js
+++ editors/code/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": "tsconfig.json",
+        "project": "tsconfig.eslint.json",
+        "tsconfigRootDir": __dirname,
         "sourceType": "module"
     },
     "plugins": [
