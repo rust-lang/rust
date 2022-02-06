8b7b0a0e49cf3e313d0432d1f6e8ce20253fef0a
Michael Goulet <michael@errs.io>
2022-02-06 13:53:38 -0800

    always cache result from mir_borrowck

--- compiler/rustc_middle/src/query/mod.rs
+++ compiler/rustc_middle/src/query/mod.rs
@@ -836,13 +836,14 @@ rustc_queries! {
     /// additional requirements that the closure's creator must verify.
     query mir_borrowck(key: LocalDefId) -> &'tcx mir::BorrowCheckResult<'tcx> {
         desc { |tcx| "borrow-checking `{}`", tcx.def_path_str(key.to_def_id()) }
-        cache_on_disk_if(tcx) { tcx.is_typeck_child(key.to_def_id()) }
+        cache_on_disk_if { true }
     }
     query mir_borrowck_const_arg(key: (LocalDefId, DefId)) -> &'tcx mir::BorrowCheckResult<'tcx> {
         desc {
             |tcx| "borrow-checking the const argument`{}`",
             tcx.def_path_str(key.0.to_def_id())
         }
+        cache_on_disk_if { true }
     }
 
     /// Gets a complete map from all types to their inherent impls.
