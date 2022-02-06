a476ec8bd0b1925d349383110b75ff51567d7534
Guillaume Gomez <guillaume.gomez@huawei.com>
2022-02-06 22:51:47 +0100

    Update rustdoc test

--- src/test/rustdoc/strip-block-doc-comments-stars.rs
+++ src/test/rustdoc/strip-block-doc-comments-stars.rs
@@ -1,6 +1,6 @@
 #![crate_name = "foo"]
 
-// The goal of this test is to answer that it won't be generated as a list because
+// The goal of this test is to ensure that it won't be generated as a list because
 // block doc comments can have their lines starting with a star.
 
 // @has foo/fn.foo.html
