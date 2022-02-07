4b3fdd7e52af164e967deda1d42685043e293982
pushkine <pushkine.github@gmail.com>
2022-02-07 16:04:51 +0100

    move file

--- src/test/ui/parser/assoc-oddities-1.rs
+++ src/test/ui/parser/assoc-oddities-1.rs
@@ -6,6 +6,6 @@ fn main() {
     x = if true { 1 } else { 0 } as *mut _;
     // however this does not parse and probably should fail to retain compat?
     // N.B., `..` here is arbitrary, failure happens/should happen ∀ops that aren’t `=`
-    // see assoc-oddities-2 and ../assoc-oddities-3
+    // see assoc-oddities-2 and assoc-oddities-3
     ..if c { a } else { b }[n]; //~ ERROR expected one of
 }
