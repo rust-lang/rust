b64a8222ab1868490bbcadc160a8c868b2ce5d13
David Tolnay <dtolnay@gmail.com>
2022-02-07 21:34:04 -0800

    Bless use tree pretty print test

--- src/test/pretty/use-tree.rs
+++ src/test/pretty/use-tree.rs
@@ -7,11 +7,17 @@ use ::std::fmt::{self, Debug, Display, Write as _};
 
 use core::option::Option::*;
 
-use core::{cmp::{Eq, Ord, PartialEq, PartialOrd},
+use core::{
+    cmp::{Eq, Ord, PartialEq, PartialOrd},
     convert::{AsMut, AsRef, From, Into},
-    iter::{DoubleEndedIterator, ExactSizeIterator, Extend, FromIterator,
-    IntoIterator, Iterator},
-    marker::{Copy as Copy, Send as Send, Sized as Sized, Sync as Sync, Unpin
-    as U}, ops::{*, Drop, Fn, FnMut, FnOnce}};
+    iter::{
+        DoubleEndedIterator, ExactSizeIterator, Extend, FromIterator,
+        IntoIterator, Iterator,
+    },
+    marker::{
+        Copy as Copy, Send as Send, Sized as Sized, Sync as Sync, Unpin as U,
+    },
+    ops::{*, Drop, Fn, FnMut, FnOnce},
+};
 
 fn main() {}
