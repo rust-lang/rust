9a52401d8ede6d4ec767d5630bc68d1c5c7fb4be
Jacob Pratt <jacob@jhpratt.dev>
2022-02-08 04:52:11 -0500

    Add and use stability helper methods
    This avoids an ambiguity (when reading) where `.level.is_stable()` is
    not immediately clear whether it is general stability or const
    stability.

--- src/librustdoc/clean/inline.rs
+++ src/librustdoc/clean/inline.rs
@@ -351,7 +351,7 @@ crate fn build_impl(
             }
 
             if let Some(stab) = tcx.lookup_stability(did) {
-                if stab.level.is_unstable() && stab.feature == sym::rustc_private {
+                if stab.is_unstable() && stab.feature == sym::rustc_private {
                     return;
                 }
             }
@@ -380,7 +380,7 @@ crate fn build_impl(
             }
 
             if let Some(stab) = tcx.lookup_stability(did) {
-                if stab.level.is_unstable() && stab.feature == sym::rustc_private {
+                if stab.is_unstable() && stab.feature == sym::rustc_private {
                     return;
                 }
             }
