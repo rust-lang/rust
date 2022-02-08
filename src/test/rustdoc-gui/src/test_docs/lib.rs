78064549bed873fbf324982edcdadf9a89aa555f
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2022-02-07 22:36:51 -0800

    rustdoc: fix spacing of non-toggled impl blocks
    We recently removed the "up here" arrows on item-infos, and adjusted
    vertical spacing so that even without the arrow, it would be visually
    clear which item the item-info belonged to. The new CSS styles for
    vertical spacing only applied to toggles, though. This missed
    non-toggled impl blocks - for instance, those without any methods, like
    https://doc.rust-lang.org/nightly/std/marker/trait.Send.html#implementors.
    The result was lists of implementors that were spaced too closely. This
    PR fixes the spacing by making it apply to non-toggled impl blocks as
    well.
    
    This also fixes an issue where item-infos were displayed too far below
    their items. That was a result of display: table on .item-info .stab.
    Changed that to display: inline-block.

--- src/test/rustdoc-gui/src/test_docs/lib.rs
+++ src/test/rustdoc-gui/src/test_docs/lib.rs
@@ -260,3 +260,14 @@ impl HeavilyDocumentedUnion {
 macro_rules! heavily_documented_macro {
     () => {};
 }
+
+pub trait EmptyTrait1 {}
+pub trait EmptyTrait2 {}
+pub trait EmptyTrait3 {}
+
+pub struct HasEmptyTraits{}
+
+impl EmptyTrait1 for HasEmptyTraits {}
+impl EmptyTrait2 for HasEmptyTraits {}
+#[doc(cfg(feature = "some-feature"))]
+impl EmptyTrait3 for HasEmptyTraits {}
