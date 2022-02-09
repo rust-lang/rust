36b495f3cf23a1f235482ce7f81f0f4be614bb85
Nicholas Nethercote <n.nethercote@gmail.com>
2022-02-10 00:47:48 +1100

    Introduce `ChunkedBitSet` and use it for some dataflow analyses.
    This reduces peak memory usage significantly for some programs with very
    large functions, such as:
    - `keccak`, `unicode_normalization`, and `match-stress-enum`, from
      the `rustc-perf` benchmark suite;
    - `http-0.2.6` from crates.io.
    
    The new type is used in the analyses where the bitsets can get huge
    (e.g. 10s of thousands of bits): `MaybeInitializedPlaces`,
    `MaybeUninitializedPlaces`, and `EverInitializedPlaces`.
    
    Some refactoring was required in `rustc_mir_dataflow`. All existing
    analysis domains are either `BitSet` or a trivial wrapper around
    `BitSet`, and access in a few places is done via `Borrow<BitSet>` or
    `BorrowMut<BitSet>`. Now that some of these domains are `ClusterBitSet`,
    that no longer works. So this commit replaces the `Borrow`/`BorrowMut`
    usage with a new trait `BitSetExt` containing the needed bitset
    operations. The impls just forward these to the underlying bitset type.
    This required fiddling with trait bounds in a few places.
    
    The commit also:
    - Moves `static_assert_size` from `rustc_data_structures` to
      `rustc_index` so it can be used in the latter; the former now
      re-exports it so existing users are unaffected.
    - Factors out some common "clear excess bits in the final word"
      functionality in `bit_set.rs`.
    - Uses `fill` in a few places instead of loops.

--- compiler/rustc_data_structures/src/macros.rs
+++ compiler/rustc_data_structures/src/macros.rs
@@ -1,11 +1,3 @@
-/// Type size assertion. The first argument is a type and the second argument is its expected size.
-#[macro_export]
-macro_rules! static_assert_size {
-    ($ty:ty, $size:expr) => {
-        const _: [(); $size] = [(); ::std::mem::size_of::<$ty>()];
-    };
-}
-
 #[macro_export]
 macro_rules! enum_from_u32 {
     ($(#[$attr:meta])* pub enum $name:ident {
--- compiler/rustc_index/src/lib.rs
+++ compiler/rustc_index/src/lib.rs
@@ -1,11 +1,21 @@
 #![feature(allow_internal_unstable)]
 #![feature(bench_black_box)]
 #![feature(extend_one)]
-#![feature(min_specialization)]
-#![feature(step_trait)]
-#![feature(test)]
 #![feature(let_else)]
+#![feature(min_specialization)]
+#![feature(new_uninit)]
+#![feature(step_trait)]
+#![feature(stmt_expr_attributes)]
+#![feature(test)]
 
 pub mod bit_set;
 pub mod interval;
 pub mod vec;
+
+/// Type size assertion. The first argument is a type and the second argument is its expected size.
+#[macro_export]
+macro_rules! static_assert_size {
+    ($ty:ty, $size:expr) => {
+        const _: [(); $size] = [(); ::std::mem::size_of::<$ty>()];
+    };
+}
