f606a5807cc954dc9f01f5a943f8479bdee96e09
bjorn3 <bjorn3@users.noreply.github.com>
2022-02-16 12:43:07 +0100

    Rustup to rustc 1.60.0-nightly (09cb29c64 2022-02-15)

--- build_system/prepare.rs
+++ build_system/prepare.rs
@@ -92,7 +92,7 @@ fn prepare_sysroot() {
     clone_repo(
         "build_sysroot/compiler-builtins",
         "https://github.com/rust-lang/compiler-builtins.git",
-        "0.1.68",
+        "0.1.70",
     );
     apply_patches("compiler-builtins", Path::new("build_sysroot/compiler-builtins"));
 }
