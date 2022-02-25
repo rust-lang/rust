ccef93fb891ac2f3426d9f4a733841db256919a9
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-02-25 15:27:11 +0000

    jsondocck: better error for when @set matches multiple items

--- src/tools/jsondocck/src/main.rs
+++ src/tools/jsondocck/src/main.rs
@@ -277,7 +277,7 @@ fn check_command(command: Command, cache: &mut Cache) -> Result<(), CkError> {
             assert_eq!(
                 results.len(),
                 1,
-                "Didn't get 1 result for `{}`: got {:?}",
+                "Expected 1 match for `{}` (because of @set): matched to {:?}",
                 command.args[3],
                 results
             );
