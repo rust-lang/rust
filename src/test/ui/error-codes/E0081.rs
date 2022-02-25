5b58c6feb87face0118bd36b61f58d137dd625e7
lightning1141 <lightning1141@gmail.com>
2022-02-25 21:12:04 +0800

    fix E0081 error message due to lit_to_const behavior change

--- src/test/ui/error-codes/E0081.rs
+++ src/test/ui/error-codes/E0081.rs
@@ -10,10 +10,10 @@ enum Enum {
 #[repr(u8)]
 enum EnumOverflowRepr {
     P = 257,
-    //~^ NOTE first use of `1` (overflowed from `257`)
+    //~^ NOTE first use of `255` (overflowed from `257`)
     X = 513,
-    //~^ ERROR discriminant value `1` already exists
-    //~| NOTE enum already has `1` (overflowed from `513`)
+    //~^ ERROR discriminant value `255` already exists
+    //~| NOTE enum already has `255` (overflowed from `513`)
 }
 
 fn main() {
