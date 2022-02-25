4aa92aff054c8e89c61ac06551a405af828ade70
Loïc BRANSTETT <lolo.branstett@numericable.fr>
2022-02-25 16:10:26 +0100

    Add well known values to --check-cfg implementation

--- src/test/ui/check-cfg/empty-values.rs
+++ src/test/ui/check-cfg/empty-values.rs
@@ -1,6 +1,10 @@
-// Check that a an empty values() pass
+// Check warning for unexpected cfg value
 //
 // check-pass
 // compile-flags: --check-cfg=values() -Z unstable-options
+
+#[cfg(test = "value")]
+//~^ WARNING unexpected `cfg` condition value
+pub fn f() {}
 
 fn main() {}
