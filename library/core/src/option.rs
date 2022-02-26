082fe5cb6b5b07203e10f00d74d913292d3ae9e7
Edward Nicholes Jr <GameDevFox@gmail.com>
2022-02-26 01:08:53 -0600

    Remove feature attributes

--- library/core/src/option.rs
+++ library/core/src/option.rs
@@ -1405,8 +1405,6 @@ impl<T> Option<T> {
     /// assert_eq!(x.xor(y), None);
     /// ```
     #[inline]
-    #[stable(feature = "option_xor", since = "1.37.0")]
-    #[rustc_const_unstable(feature = "const_option_ext", issue = "91930")]
     pub const fn xor(self, optb: Option<T>) -> Option<T>
     where
         T: ~const Drop,
