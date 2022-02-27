bfa7d44823717c30bc21abc1ca3675d0b78c80a2
DrMeepster <19316085+DrMeepster@users.noreply.github.com>
2022-02-27 00:48:17 -0800

    fix box icing when it has aggregate abi

---
+++ src/test/ui/box/issue-81270-ice.rs
@@ -0,0 +1,22 @@
+// check-pass
+#![feature(allocator_api)]
+
+use std::alloc::Allocator;
+
+struct BigAllocator([usize; 2]);
+
+unsafe impl Allocator for BigAllocator {
+    fn allocate(
+        &self,
+        _: std::alloc::Layout,
+    ) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+        todo!()
+    }
+    unsafe fn deallocate(&self, _: std::ptr::NonNull<u8>, _: std::alloc::Layout) {
+        todo!()
+    }
+}
+
+fn main() {
+    Box::new_in((), BigAllocator([0; 2]));
+}
