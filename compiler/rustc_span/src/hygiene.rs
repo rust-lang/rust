e77e4fcf898ca4b2aa2041d2cbac200ed0f11b5b
Camille GILLOT <gillot.camille@gmail.com>
2022-02-27 16:09:52 +0100

    Only create a single expansion for each inline integration.

--- compiler/rustc_span/src/hygiene.rs
+++ compiler/rustc_span/src/hygiene.rs
@@ -874,19 +874,13 @@ impl Span {
     /// other compiler-generated code to set per-span properties like allowed unstable features.
     /// The returned span belongs to the created expansion and has the new properties,
     /// but its location is inherited from the current span.
-    pub fn fresh_expansion(self, expn_data: ExpnData, ctx: impl HashStableContext) -> Span {
-        self.fresh_expansion_with_transparency(expn_data, Transparency::Transparent, ctx)
-    }
-
-    pub fn fresh_expansion_with_transparency(
-        self,
-        expn_data: ExpnData,
-        transparency: Transparency,
-        ctx: impl HashStableContext,
-    ) -> Span {
-        let expn_id = LocalExpnId::fresh(expn_data, ctx).to_expn_id();
+    pub fn fresh_expansion(self, expn_id: LocalExpnId) -> Span {
         HygieneData::with(|data| {
-            self.with_ctxt(data.apply_mark(SyntaxContext::root(), expn_id, transparency))
+            self.with_ctxt(data.apply_mark(
+                SyntaxContext::root(),
+                expn_id.to_expn_id(),
+                Transparency::Transparent,
+            ))
         })
     }
 
@@ -903,7 +897,8 @@ impl Span {
             allow_internal_unstable,
             ..ExpnData::default(ExpnKind::Desugaring(reason), self, edition, None, None)
         };
-        self.fresh_expansion(expn_data, ctx)
+        let expn_id = LocalExpnId::fresh(expn_data, ctx);
+        self.fresh_expansion(expn_id)
     }
 }
 
--- src/test/ui/const_prop/inline_spans.rs
+++ src/test/ui/const_prop/inline_spans.rs
@@ -5,10 +5,14 @@
 
 fn main() {
     let _ = add(u8::MAX, 1);
-    //~^ ERROR this arithmetic operation will overflow
+    //~^ NOTE in this expansion of inlined source
+    //~| NOTE in this expansion of inlined source
 }
 
 #[inline(always)]
 fn add(x: u8, y: u8) -> u8 {
     x + y
+    //~^ ERROR this arithmetic operation will overflow
+    //~| NOTE attempt to compute `u8::MAX + 1_u8`, which would overflow
+    //~| NOTE `#[deny(arithmetic_overflow)]` on by default
 }
