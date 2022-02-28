1f51b548746b37195e3e445ef66a7d9423301b4d
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-02-28 14:26:46 -0500

    update Enzyme to LLVM platform tag (#538)

--- .packaging/build_tarballs.jl
+++ .packaging/build_tarballs.jl
@@ -1,16 +1,24 @@
 using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
 
-include("../../fancy_toys.jl")
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 
 name = "Enzyme"
-repo = "https://github.com/wsmoses/Enzyme.git"
+repo = "https://github.com/EnzymeAD/Enzyme.git"
 
 auto_version = "%ENZYME_VERSION%"
 version = VersionNumber(split(auto_version, "/")[end])
 
+llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1"]
+
 # Collection of sources required to build attr
 sources = [GitSource(repo, "%ENZYME_HASH%")]
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # Bash recipe for building across all platforms
 script = raw"""
@@ -39,43 +47,50 @@ cmake -B build -S enzyme -GNinja ${CMAKE_FLAGS[@]}
 ninja -C build -j ${nproc} install
 """
 
-function configure(julia_version, llvm_version)
-    # These are the platforms we will build for by default, unless further
-    # platforms are passed in on the command line
-    platforms = expand_cxxstring_abis(supported_platforms(; experimental=julia_version>=v"1.7"))
+augment_platform_block = """
+    using Base.BinaryPlatforms
+    $(LLVM.augment)
+    function augment_platform!(platform::Platform)
+        augment_llvm!(platform)
+    end"""
 
-    foreach(platforms) do p
-        BinaryPlatforms.add_tag!(p.tags, "julia_version", string(julia_version))
-    end
+# determine exactly which tarballs we should build
+builds = []
+for llvm_version in llvm_versions, llvm_assertions in (false, true)
+    # Dependencies that must be installed before this package can be built
+    llvm_name = llvm_assertions ? "LLVM_full_assert_jll" : "LLVM_full_jll"
+    dependencies = [
+        BuildDependency(PackageSpec(name=llvm_name, version=llvm_version))
+    ]
 
     # The products that we will ensure are always built
     products = Product[
-        LibraryProduct(["libEnzyme-$(llvm_version.major)", "libEnzyme"], :libEnzyme),
+        LibraryProduct(["libEnzyme-$(llvm_version.major)", "libEnzyme"], :libEnzyme, dont_dlopen=true),
     ]
 
+    for platform in platforms
+        augmented_platform = deepcopy(platform)
+        augmented_platform[LLVM.platform_name] = LLVM.platform(llvm_version, llvm_assertions)
 
-    dependencies = [
-        BuildDependency(get_addable_spec("LLVM_full_jll", llvm_version))
-    #    Dependency(PackageSpec(name="libLLVM_jll", version=v"9.0.1")) is given through julia_version tag
-    ]
-
-    return platforms, products, dependencies
+        should_build_platform(triplet(augmented_platform)) || continue
+        push!(builds, (;
+            dependencies, products,
+            platforms=[augmented_platform],
+        ))
+    end
 end
 
-# TODO: Don't require build-id on LLVM version
-supported = (
-    (v"1.6", v"11.0.1+3"),
-    (v"1.7", v"12.0.0+0"),
-    (v"1.8", v"13.0.0+3"),
-)
+# don't allow `build_tarballs` to override platform selection based on ARGS.
+# we handle that ourselves by calling `should_build_platform`
+non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
 
+# `--register` should only be passed to the latest `build_tarballs` invocation
+non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
 
-for (julia_version, llvm_version) in supported
-    platforms, products, dependencies = configure(julia_version, llvm_version)
-
-    any(should_build_platform.(triplet.(platforms))) || continue
-
-    # Build the tarballs.
-    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-                preferred_gcc_version=v"8", julia_compat="1.6")
+for (i,build) in enumerate(builds)
+    build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
+                   name, version, sources, script,
+                   build.platforms, build.products, build.dependencies;
+                   preferred_gcc_version=v"8", julia_compat="1.6",
+                   augment_platform_block)
 end
