786c831d01c9456c40dd171023e3e644545121d1
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-01 20:40:40 +0900

    Join the actual span.

--- src/test/ui/proc-macro/auxiliary/span-join.rs
+++ src/test/ui/proc-macro/auxiliary/span-join.rs
@@ -6,12 +6,14 @@ use proc_macro::{Ident, JoinError, Span, TokenStream};
 
 #[proc_macro]
 fn hygiene_differ(input: TokenStream) -> TokenStream {
-    let ident = input.into_iter().next();
+    let call_site_ident = input.into_iter().next();
 
     let mixed_site_ident = Ident::new("other", Span::mixed_site());
 
-    let e =
-        ident.join(mixed_site_ident).expect_err("successfully joined despite different hygiene");
+    let e = call_site_ident
+        .span()
+        .join(mixed_site_ident.span())
+        .expect_err("successfully joined despite different hygiene");
 
     assert_eq(e, JoinError::DifferentHygiene);
 
