bdba89733e719e12b8fdd362d4c1e397f7c51436
Amanieu d'Antras <amanieu@gmail.com>
2022-03-01 00:54:28 +0000

    Update tests for sym support in global_asm!

--- src/test/assembly/asm/global_asm.rs
+++ src/test/assembly/asm/global_asm.rs
@@ -1,11 +1,18 @@
 // only-x86_64
+// only-linux
 // assembly-output: emit-asm
 // compile-flags: -C llvm-args=--x86-asm-syntax=intel
 
-#![feature(asm_const)]
+#![feature(asm_const, asm_sym)]
 #![crate_type = "rlib"]
 
 use std::arch::global_asm;
+
+#[no_mangle]
+fn my_func() {}
+
+#[no_mangle]
+static MY_STATIC: i32 = 0;
 
 // CHECK: mov eax, eax
 global_asm!("mov eax, eax");
@@ -13,3 +20,7 @@ global_asm!("mov eax, eax");
 global_asm!("mov ebx, {}", const 5);
 // CHECK: mov ecx, 5
 global_asm!("movl ${}, %ecx", const 5, options(att_syntax));
+// CHECK: call my_func
+global_asm!("call {}", sym my_func);
+// CHECK: lea rax, [rip + MY_STATIC]
+global_asm!("lea rax, [rip + {}]", sym MY_STATIC);
