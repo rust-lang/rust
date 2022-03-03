86b4658c569d59452863b3161acd78aaa30abb86
Dan Gohman <dev@sunfishcode.online>
2022-03-03 14:41:10 -0800

    Generalize `get_nullable_type` to allow types where null is all-ones.
    Generalize get_nullable_type to accept types that have an all-ones bit
    pattern as their sentry "null" value.
    
    This will allow [`OwnedFd`], [`BorrowedFd`], [`OwnedSocket`], and
    [`BorrowedSocket`] to be marked with
    `#[rustc_nonnull_optimization_guaranteed]`, which will allow
    `Option<OwnedFd>`, `Option<BorrowedFd>`, `Option<OwnedSocket>`, and
    `Option<BorrowedSocket>` to be used in FFI declarations, as described
    in the [I/O safety RFC].
    
    For example, it will allow a function like `open` on Unix and `WSASocketW`
    on Windows to be declared using `Option<OwnedFd>` and `Option<OwnedSocket>`
    return types, respectively.
    
    The actual change to add `#[rustc_nonnull_optimization_guaranteed]`
    to the abovementioned types will be a separate PR, as it'll depend on
    having this patch in the stage0 compiler.
    
    Also, update the diagnostics to mention that "niche optimizations" are
    used in libstd as well as libcore, as `rustc_layout_scalar_valid_range_start`
    and `rustc_layout_scalar_valid_range_end` are already in use in libstd.
    
    [`OwnedFd`]: https://github.com/rust-lang/rust/blob/c9dc44be24c58ff13ce46416c4b97ab5c1bd8429/library/std/src/os/fd/owned.rs#L49
    [`BorrowedFd`]: https://github.com/rust-lang/rust/blob/c9dc44be24c58ff13ce46416c4b97ab5c1bd8429/library/std/src/os/fd/owned.rs#L29
    [`OwnedSocket`]: https://github.com/rust-lang/rust/blob/c9dc44be24c58ff13ce46416c4b97ab5c1bd8429/library/std/src/os/windows/io/socket.rs#L51
    [`BorrowedSocket`]: https://github.com/rust-lang/rust/blob/c9dc44be24c58ff13ce46416c4b97ab5c1bd8429/library/std/src/os/windows/io/socket.rs#L29
    [I/O safety RFC]: https://github.com/rust-lang/rfcs/blob/master/text/3128-io-safety.md#ownedfd-and-borrowedfdfd-1

--- compiler/rustc_feature/src/builtin_attrs.rs
+++ compiler/rustc_feature/src/builtin_attrs.rs
@@ -606,17 +606,17 @@ pub const BUILTIN_ATTRIBUTES: &[BuiltinAttribute] = &[
     rustc_attr!(
         rustc_layout_scalar_valid_range_start, Normal, template!(List: "value"), ErrorFollowing,
         "the `#[rustc_layout_scalar_valid_range_start]` attribute is just used to enable \
-        niche optimizations in libcore and will never be stable",
+        niche optimizations in libcore and libstd and will never be stable",
     ),
     rustc_attr!(
         rustc_layout_scalar_valid_range_end, Normal, template!(List: "value"), ErrorFollowing,
         "the `#[rustc_layout_scalar_valid_range_end]` attribute is just used to enable \
-        niche optimizations in libcore and will never be stable",
+        niche optimizations in libcore and libstd and will never be stable",
     ),
     rustc_attr!(
         rustc_nonnull_optimization_guaranteed, Normal, template!(Word), WarnFollowing,
         "the `#[rustc_nonnull_optimization_guaranteed]` attribute is just used to enable \
-        niche optimizations in libcore and will never be stable",
+        niche optimizations in libcore and libstd and will never be stable",
     ),
 
     // ==========================================================================
