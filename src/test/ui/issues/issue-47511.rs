9e03d7ddbe46492ea9487dd52b3c4024a5823099
marmeladema <xademax@gmail.com>
2022-03-04 22:47:56 +0100

    Add known-bug directive to issue #47511 test case

--- src/test/ui/issues/issue-47511.rs
+++ src/test/ui/issues/issue-47511.rs
@@ -1,14 +1,15 @@
+// check-fail
+// known-bug
+
 // Regression test for #47511: anonymous lifetimes can appear
 // unconstrained in a return type, but only if they appear just once
 // in the input, as the input to a projection.
 
 fn f(_: X) -> X {
-    //~^ ERROR return type references an anonymous lifetime
     unimplemented!()
 }
 
 fn g<'a>(_: X<'a>) -> X<'a> {
-    //~^ ERROR return type references lifetime `'a`, which is not constrained
     unimplemented!()
 }
 
