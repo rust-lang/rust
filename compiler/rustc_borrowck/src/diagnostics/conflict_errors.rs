2ed5f257c22d9a039131623f8badce137cae0002
Maybe Waffle <waffle.lapkin@gmail.com>
2022-03-08 22:17:05 +0400

    Move code that finds missing bounds into a function

--- compiler/rustc_borrowck/src/diagnostics/conflict_errors.rs
+++ compiler/rustc_borrowck/src/diagnostics/conflict_errors.rs
@@ -292,44 +292,9 @@ impl<'cx, 'tcx> MirBorrowckCtxt<'cx, 'tcx> {
                     .as_local()
                     .and_then(|def_id| tcx.hir().get_generics(def_id))
                 {
-                    let predicates: Result<Vec<_>, _> = tcx.infer_ctxt().enter(|infcx| {
-                        let mut fulfill_cx =
-                            <dyn rustc_infer::traits::TraitEngine<'_>>::new(infcx.tcx);
-
-                        let copy_did = infcx.tcx.lang_items().copy_trait().unwrap();
-                        let cause = ObligationCause::new(
-                            span,
-                            self.mir_hir_id(),
-                            rustc_infer::traits::ObligationCauseCode::MiscObligation,
-                        );
-                        fulfill_cx.register_bound(
-                            &infcx,
-                            self.param_env,
-                            // Erase any region vids from the type, which may not be resolved
-                            infcx.tcx.erase_regions(ty),
-                            copy_did,
-                            cause,
-                        );
-                        // Select all, including ambiguous predicates
-                        let errors = fulfill_cx.select_all_or_error(&infcx);
-
-                        // Only emit suggestion if all required predicates are on generic
-                        errors
-                            .into_iter()
-                            .map(|err| match err.obligation.predicate.kind().skip_binder() {
-                                PredicateKind::Trait(predicate) => {
-                                    match predicate.self_ty().kind() {
-                                        ty::Param(param_ty) => Ok((
-                                            generics.type_param(param_ty, tcx),
-                                            predicate.trait_ref.print_only_trait_path().to_string(),
-                                        )),
-                                        _ => Err(()),
-                                    }
-                                }
-                                _ => Err(()),
-                            })
-                            .collect()
-                    });
+                    let copy_did = tcx.lang_items().copy_trait().unwrap();
+                    let predicates =
+                        self.try_find_missing_generic_bounds(ty, copy_did, generics, span);
 
                     if let Ok(predicates) = predicates {
                         suggest_constraining_type_params(
@@ -2255,6 +2220,53 @@ impl<'cx, 'tcx> MirBorrowckCtxt<'cx, 'tcx> {
                 None
             }
         }
+    }
+
+    /// Tries to find bounds on `generics` that satisfy `ty: required_trait_did` bound.
+    fn try_find_missing_generic_bounds(
+        &self,
+        ty: Ty<'tcx>,
+        required_trait_did: DefId,
+        generics: &'tcx ty::Generics,
+        obligation_cause_span: Span,
+    ) -> Result<Vec<(&'tcx ty::GenericParamDef, String)>, ()> {
+        let tcx = self.infcx.tcx;
+        let predicates: Result<Vec<_>, _> = tcx.infer_ctxt().enter(|infcx| {
+            let mut fulfill_cx = <dyn rustc_infer::traits::TraitEngine<'_>>::new(infcx.tcx);
+
+            let cause = ObligationCause::new(
+                obligation_cause_span,
+                self.mir_hir_id(),
+                rustc_infer::traits::ObligationCauseCode::MiscObligation,
+            );
+            fulfill_cx.register_bound(
+                &infcx,
+                self.param_env,
+                // Erase any region vids from the type, which may not be resolved
+                infcx.tcx.erase_regions(ty),
+                required_trait_did,
+                cause,
+            );
+            // Select all, including ambiguous predicates
+            let errors = fulfill_cx.select_all_or_error(&infcx);
+
+            // Only emit suggestion if all required predicates are on generic
+            errors
+                .into_iter()
+                .map(|err| match err.obligation.predicate.kind().skip_binder() {
+                    PredicateKind::Trait(predicate) => match predicate.self_ty().kind() {
+                        ty::Param(param_ty) => Ok((
+                            generics.type_param(param_ty, tcx),
+                            predicate.trait_ref.print_only_trait_path().to_string(),
+                        )),
+                        _ => Err(()),
+                    },
+                    _ => Err(()),
+                })
+                .collect()
+        });
+
+        predicates
     }
 }
 
