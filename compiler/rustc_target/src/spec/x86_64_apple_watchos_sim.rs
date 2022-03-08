44cb1417833b19aecdb8ed2c5bd9941d1e65b20c
Vladimir Michael Eatwell <dimir@fb.com>
2022-03-08 16:06:15 +0000

    [watchos] fix formatting

--- compiler/rustc_target/src/spec/x86_64_apple_watchos_sim.rs
+++ compiler/rustc_target/src/spec/x86_64_apple_watchos_sim.rs
@@ -10,7 +10,8 @@ pub fn target() -> Target {
     Target {
         llvm_target,
         pointer_width: 64,
-        data_layout: "e-m:o-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128".to_string(),
+        data_layout: "e-m:o-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+            .to_string(),
         arch: "x86_64".to_string(),
         options: TargetOptions {
             max_atomic_width: Some(64),
