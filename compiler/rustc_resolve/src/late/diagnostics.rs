6eebe901ffa377d58351c9328efa2195eb130a27
Yoav Lavi <yoavlavi122@gmail.com>
2022-03-22 19:29:41 +0100

    revert formatting change

--- compiler/rustc_resolve/src/late/diagnostics.rs
+++ compiler/rustc_resolve/src/late/diagnostics.rs
@@ -1021,8 +1021,7 @@ impl<'a: 'ast, 'ast> LateResolutionVisitor<'a, '_, 'ast> {
             (Res::Def(DefKind::TyAlias, def_id), PathSource::Trait(_)) => {
                 err.span_label(span, "type aliases cannot be used as traits");
                 if self.r.session.is_nightly_build() {
-                    let msg =
-                        "you might have meant to use `#![feature(trait_alias)]` instead of a \
+                    let msg = "you might have meant to use `#![feature(trait_alias)]` instead of a \
                                `type` alias";
                     if let Some(span) = self.def_span(def_id) {
                         if let Ok(snip) = self.r.session.source_map().span_to_snippet(span) {
