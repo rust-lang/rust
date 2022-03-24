3275df31ea91d638fd75cc5f627c211de8bd8f71
Ralf Jung <post@ralfj.de>
2022-03-24 10:06:33 -0400

    rustup

--- tests/compile-fail/null_pointer_deref.rs
+++ tests/compile-fail/null_pointer_deref.rs
@@ -1,5 +1,5 @@
 #[allow(deref_nullptr)]
 fn main() {
-    let x: i32 = unsafe { *std::ptr::null() }; //~ ERROR null pointer is not a valid pointer for this operation
+    let x: i32 = unsafe { *std::ptr::null() }; //~ ERROR null pointer is not a valid pointer
     panic!("this should never print: {}", x);
 }
