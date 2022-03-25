553e10622769001c6f4808a9050bc2092076ace8
Tom Tromey <tom@tromey.com>
2022-03-25 11:39:42 -0600

    Undo drop-locations.rs change
    The compiler crashed in CI when compiling drop-locations.rs, so
    restore the ignore-test marker there.

--- src/test/debuginfo/drop-locations.rs
+++ src/test/debuginfo/drop-locations.rs
@@ -1,6 +1,6 @@
 // ignore-windows
 // ignore-android
-// ignore-lldb // Test temporarily ignored due to debuginfo tests being disabled, see PR 47155
+// ignore-test // Test temporarily ignored due to debuginfo tests being disabled, see PR 47155
 // min-lldb-version: 310
 
 #![allow(unused)]
