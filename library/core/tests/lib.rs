2a827635ba65e3d40af8643373e4584858eecc7d
Declan Kelly <dkelly.home@gmail.com>
2022-04-01 00:19:10 -0700

    Implement provenance preserving method on NonNull
    **Description**
     Add the `addr`, `with_addr, `map_addr` methods to the `NonNull` type,
     and map the address type to `NonZeroUsize`.
    
     **Motiviation**
     The `NonNull` type is useful for implementing pointer types which have
     the 0-niche. It is currently possible to implement these provenance
     preserving functions by calling `NonNull::as_ptr` and `new_unchecked`.
     The addition of these methods simply make it more ergonomic to use.
    
     **Testing**
     Added a unit test of a nonnull tagged pointer type. This is based on
     some real code I have elsewhere, that currently routes the pointer
     through a `NonZeroUsize` and back out to produce a usable pointer.

--- library/core/tests/lib.rs
+++ library/core/tests/lib.rs
@@ -87,6 +87,7 @@
 #![feature(int_roundings)]
 #![feature(slice_group_by)]
 #![feature(split_array)]
+#![feature(strict_provenance)]
 #![feature(trusted_random_access)]
 #![feature(unsize)]
 #![feature(unzip_option)]
