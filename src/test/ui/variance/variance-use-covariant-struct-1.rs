1c1d01eb495fb663f64766a5e7e1e23b9a2590e7
Jack Huey <31162821+jackh726@users.noreply.github.com>
2022-04-01 22:12:17 -0400

    More nll revisions

--- src/test/ui/variance/variance-use-covariant-struct-1.rs
+++ src/test/ui/variance/variance-use-covariant-struct-1.rs
@@ -1,13 +1,19 @@
 // Test that a covariant struct does not permit the lifetime of a
 // reference to be enlarged.
 
+// revisions: base nll
+// ignore-compare-mode-nll
+//[nll] compile-flags: -Z borrowck=mir
+
 struct SomeStruct<T>(T);
 
 fn foo<'min,'max>(v: SomeStruct<&'min ()>)
                   -> SomeStruct<&'max ()>
     where 'max : 'min
 {
-    v //~ ERROR mismatched types
+    v
+    //[base]~^ ERROR mismatched types
+    //[nll]~^^ ERROR lifetime may not live long enough
 }
 
 fn main() { }
