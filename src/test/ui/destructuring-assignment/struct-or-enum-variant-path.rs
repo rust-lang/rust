17f5c4d255323d331345ffe4cfebb959b1b5fa1a
Michael Goulet <michael@errs.io>
2022-04-01 20:24:32 -0700

    Fix unit struct/enum variant in destructuring assignment

---
+++ src/test/ui/destructuring-assignment/struct-or-enum-variant-path.rs
@@ -0,0 +1,34 @@
+// check-pass
+
+struct S;
+
+enum E {
+    V,
+}
+
+type A = E;
+
+fn main() {
+    let mut a;
+
+    (S, a) = (S, ());
+
+    (E::V, a) = (E::V, ());
+
+    (<E>::V, a) = (E::V, ());
+    (A::V, a) = (E::V, ());
+}
+
+impl S {
+    fn check() {
+        let a;
+        (Self, a) = (S, ());
+    }
+}
+
+impl E {
+    fn check() {
+        let a;
+        (Self::V, a) = (E::V, ());
+    }
+}
