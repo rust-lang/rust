635270d4ad0e8c1b99784d8d46199fe0d2e8f956
Cadu <cadu.coelho@gmail.com>
2022-04-03 13:48:10 -0300

    Better error message hinting about `cargo clippy`

--- crates/flycheck/src/lib.rs
+++ crates/flycheck/src/lib.rs
@@ -329,7 +329,7 @@ impl CargoActor {
             Ok(output) if output.status.success() => Ok(()),
             Ok(output)  => {
                 Err(io::Error::new(io::ErrorKind::Other, format!(
-                    "Cargo watcher failed, the command produced no valid metadata (exit code: {:?})",
+                    "Cargo watcher failed, the command produced no valid metadata (exit code: {:?})\nMake sure to have `cargo clippy` installed.",
                     output.status
                 )))
             }
