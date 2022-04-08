f8f144117da48b52f2c65292402dedcb1c59d9c1
infrandomness <infrandomness@gmail.com>
2022-04-08 21:54:44 +0200

    Swap span_lint for span_lint_and_sugg
    This implements a machine applicable suggestion to any matched usage of
    `.as_ref().take()``

--- clippy_lints/src/methods/needless_option_take.rs
+++ clippy_lints/src/methods/needless_option_take.rs
@@ -1,27 +1,30 @@
-use clippy_utils::diagnostics::span_lint;
+use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::ty::is_type_diagnostic_item;
+use rustc_errors::Applicability;
 use rustc_hir::Expr;
 use rustc_lint::LateContext;
 use rustc_span::sym;
 
 use super::NEEDLESS_OPTION_TAKE;
 
-pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>, _recv: &'tcx Expr<'_>) {
+pub(super) fn check<'tcx>(cx: &LateContext<'tcx>, expr: &'tcx Expr<'_>, recv: &'tcx Expr<'_>) {
     // Checks if expression type is equal to sym::Option and if the expr is not a syntactic place
     if is_expr_option(cx, expr) && !expr.is_syntactic_place_expr() {
-        span_lint(cx, OPTION_TAKE_ON_TEMPORARY, expr.span, "Format test");
+        let mut applicability = Applicability::MachineApplicable;
+        span_lint_and_sugg(
+            cx,
+            NEEDLESS_OPTION_TAKE,
+            expr.span,
+            "Called `Option::take()` on a temporary value",
+            "try",
+            format!(
+                "{}",
+                snippet_with_applicability(cx, recv.span, "..", &mut applicability)
+            ),
+            applicability,
+        );
     }
-    /*    if_chain! {
-        is_expr_option(cx, expr);
-        then {
-            span_lint(
-                cx,
-                NEEDLESS_OPTION_TAKE,
-                expr.span,
-                "Format test"
-            );
-        }
-    };*/
 }
 
 fn is_expr_option(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool {
