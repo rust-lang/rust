2f03767eef21ac1111afb6af969e64d7d159c753
Jakob Degen <jakob.e.degen@gmail.com>
2022-04-11 10:23:33 -0400

    Remove inlining cost of `Deinit` statements

--- compiler/rustc_mir_transform/src/inline.rs
+++ compiler/rustc_mir_transform/src/inline.rs
@@ -369,6 +369,7 @@ impl<'tcx> Inliner<'tcx> {
                 match stmt.kind {
                     StatementKind::StorageLive(_)
                     | StatementKind::StorageDead(_)
+                    | StatementKind::Deinit(_)
                     | StatementKind::Nop => {}
                     _ => cost += INSTR_COST,
                 }
