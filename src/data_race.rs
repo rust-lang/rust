ec1dc749a344522b080ec8162b9fd24fce0c507e
Ralf Jung <post@ralfj.de>
2022-04-18 10:20:11 -0400

    adjust for provenance cleanup

--- src/data_race.rs
+++ src/data_race.rs
@@ -999,7 +999,7 @@ trait EvalContextPrivExt<'mir, 'tcx: 'mir>: MiriEvalContextExt<'mir, 'tcx> {
         if let Some(data_race) = &this.machine.data_race {
             if data_race.multi_threaded.get() {
                 let size = place.layout.size;
-                let (alloc_id, base_offset, ptr) = this.ptr_get_alloc_id(place.ptr)?;
+                let (alloc_id, base_offset, _tag) = this.ptr_get_alloc_id(place.ptr)?;
                 // Load and log the atomic operation.
                 // Note that atomic loads are possible even from read-only allocations, so `get_alloc_extra_mut` is not an option.
                 let alloc_meta = &this.get_alloc_extra(alloc_id)?.data_race.as_ref().unwrap();
@@ -1007,7 +1007,7 @@ trait EvalContextPrivExt<'mir, 'tcx: 'mir>: MiriEvalContextExt<'mir, 'tcx> {
                     "Atomic op({}) with ordering {:?} on {:?} (size={})",
                     description,
                     &atomic,
-                    ptr,
+                    place.ptr,
                     size.bytes()
                 );
 
@@ -1039,7 +1039,7 @@ trait EvalContextPrivExt<'mir, 'tcx: 'mir>: MiriEvalContextExt<'mir, 'tcx> {
                     {
                         log::trace!(
                             "Updated atomic memory({:?}, size={}) to {:#?}",
-                            ptr,
+                            place.ptr,
                             size.bytes(),
                             range.atomic_ops
                         );
