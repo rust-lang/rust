512e95248adc0c8cc0ea0b7aa50d5bd1ccc5f376
CAD97 <cad97@cad97.com>
2022-04-30 10:06:19 -0500

    Add rustdoctest no_compile test

---
+++ src/test/rustdoc-ui/no_compile.rs
@@ -0,0 +1,25 @@
+// check-pass
+
+// compile-flags:--test --test-args=--include-ignored --test-args=--test-threads=1
+// normalize-stdout-test: "src/test/rustdoc-ui" -> "$$DIR"
+// normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
+
+/// These code blocks should be treated as rust code in documentation, but never
+/// treated as a test and compiled, even when testing ignored tests.
+///
+/// ```rust,no_compile
+/// // haha rustc
+/// i"have" 0utsmarted k#thee
+/// ```
+///
+/// ```no_compile
+/// // haha rustc
+/// i"have" 0utsmarted k#thee
+/// ```
+///
+/// For comparison, ignore does get run:
+///
+/// ```ignore
+/// println!("Hello, world");
+/// ```
+struct S;
