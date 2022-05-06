adab1350e76dea8a8af4d062fcad518980673643
Rémy Rakic <remy.rakic+github@gmail.com>
2022-05-06 23:20:04 +0200

    update jemalloc-sys to jemalloc v5.3

--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -70,9 +70,7 @@ extern crate test;
 // See docs in https://github.com/rust-lang/rust/blob/master/compiler/rustc/src/main.rs
 // about jemalloc.
 #[cfg(feature = "jemalloc")]
-extern crate tikv_jemalloc_sys;
-#[cfg(feature = "jemalloc")]
-use tikv_jemalloc_sys as jemalloc_sys;
+extern crate jemalloc_sys;
 
 use std::default::Default;
 use std::env::{self, VarError};
