cedc428a5fa988a3d7639a0fa45b726e0fd698ed
Ralf Jung <post@ralfj.de>
2022-05-07 15:01:25 +0200

    fix the layout of repr(align) enums

--- src/test/ui/aligned_enum_cast.rs
+++ src/test/ui/aligned_enum_cast.rs
@@ -11,5 +11,13 @@ enum Aligned {
 fn main() {
     let aligned = Aligned::Zero;
     let fo = aligned as u8;
-    println!("foo {}",fo);
+    println!("foo {}", fo);
+    println!("{}", tou8(Aligned::Zero));
+}
+
+#[inline(never)]
+fn tou8(al: Aligned) -> u8 {
+    // Cast behind a function call so ConstProp does not see it
+    // (so that we can test codegen).
+    al as u8
 }
