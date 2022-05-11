5c60951344d180f5a21ba550dae77847dcd92de0
Dan Gohman <dev@sunfishcode.online>
2022-05-11 09:29:08 -0700

    Fix attribute name.

--- library/std/src/os/windows/io/tests.rs
+++ library/std/src/os/windows/io/tests.rs
@@ -8,9 +8,9 @@ fn test_niche_optimizations_socket() {
     assert_eq!(size_of::<Option<OwnedSocket>>(), size_of::<RawSocket>());
     assert_eq!(size_of::<Option<BorrowedSocket<'static>>>(), size_of::<RawSocket>(),);
     unsafe {
-        #[cfg(target_pointer_size = "32")]
+        #[cfg(target_pointer_width = "32")]
         let (min, max) = (i32::MIN as u32, i32::MAX as u32);
-        #[cfg(target_pointer_size = "64")]
+        #[cfg(target_pointer_width = "64")]
         let (min, max) = (i64::MIN as u64, i64::MAX as u64);
 
         assert_eq!(OwnedSocket::from_raw_socket(min).into_raw_socket(), min);
