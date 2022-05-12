a315bb444678a31de0effcb3071c6a9c57d25219
Gary Guo <gary@garyguo.net>
2022-05-12 14:20:13 +0100

    Expand c-unwind-abi-panic-abort test

--- src/test/codegen/unwind-abis/c-unwind-abi-panic-abort.rs
+++ src/test/codegen/unwind-abis/c-unwind-abi-panic-abort.rs
@@ -6,17 +6,22 @@
 #![crate_type = "lib"]
 #![feature(c_unwind)]
 
-extern "C-unwind" {
-    fn may_unwind();
-}
-
 // CHECK: @rust_item_that_can_unwind() unnamed_addr #0
 #[no_mangle]
 pub unsafe extern "C-unwind" fn rust_item_that_can_unwind() {
+    // CHECK: call void @_ZN4core9panicking15panic_no_unwind
     may_unwind();
+}
+
+extern "C-unwind" {
+    // CHECK: @may_unwind() unnamed_addr #1
+    fn may_unwind();
 }
 
 // Now, make sure that the LLVM attributes for this functions are correct.  First, make
 // sure that the first item is correctly marked with the `nounwind` attribute:
 //
 // CHECK: attributes #0 = { {{.*}}nounwind{{.*}} }
+//
+// Now, check that foreign item is correctly marked without the `nounwind` attribute.
+// CHECK-NOT: attributes #1 = { {{.*}}nounwind{{.*}} }
