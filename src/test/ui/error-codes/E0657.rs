28c24cc4ebd4445be39c4079d819c12d48af9b8b
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-05-14 02:08:22 -0400

    result of blessing the ui tests. I skimmed these results and compared them against PR 96970 and it seems consistent but I have not inspected them carefully.

--- src/test/ui/error-codes/E0657.rs
+++ src/test/ui/error-codes/E0657.rs
@@ -8,7 +8,8 @@ impl<T> Id<T> for T {}
 
 fn free_fn_capture_hrtb_in_impl_trait()
     -> Box<for<'a> Id<impl Lt<'a>>>
-        //~^ ERROR `impl Trait` can only capture lifetimes bound at the fn or impl level [E0657]
+//~^ ERROR `impl Trait` can only capture lifetimes bound at the fn or impl level [E0657]
+//~| ERROR lifetime bounds on nested opaque types are not supported yet
 {
     Box::new(())
 }
@@ -17,7 +18,8 @@ struct Foo;
 impl Foo {
     fn impl_fn_capture_hrtb_in_impl_trait()
         -> Box<for<'a> Id<impl Lt<'a>>>
-            //~^ ERROR `impl Trait` can only capture lifetimes bound at the fn or impl level
+    //~^ ERROR `impl Trait` can only capture lifetimes bound at the fn or impl level
+    //~| ERROR lifetime bounds on nested opaque types are not supported yet
     {
         Box::new(())
     }
