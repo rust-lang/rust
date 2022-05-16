4cc0cc7eb3f35bc15967922a94245917e0a7c0e2
Dan Gohman <dev@sunfishcode.online>
2022-05-16 05:07:28 -0700

    Update how CC etc. are set in the wasi-libc Makefile.
    wasi-libc's Makefile changed how it detects the compiler to use; update
    Rust's script to set `CC` directly to the compiler it installs.

--- src/ci/docker/host-x86_64/dist-various-2/build-wasi-toolchain.sh
+++ src/ci/docker/host-x86_64/dist-various-2/build-wasi-toolchain.sh
@@ -5,13 +5,18 @@ set -ex
 # Originally from https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 curl https://ci-mirrors.rust-lang.org/rustc/2022-05-10-clang%2Bllvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz | \
   tar xJf -
-export PATH=`pwd`/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/bin:$PATH
+bin="$PWD/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04/bin"
 
 git clone https://github.com/WebAssembly/wasi-libc
 
 cd wasi-libc
 git reset --hard 9886d3d6200fcc3726329966860fc058707406cd
-make -j$(nproc) INSTALL_DIR=/wasm32-wasi install
+make -j$(nproc) \
+    CC="$bin/clang" \
+    NM="$bin/llvm-nm" \
+    AR="$bin/llvm-ar" \
+    INSTALL_DIR=/wasm32-wasi \
+    install
 
 cd ..
 rm -rf wasi-libc
