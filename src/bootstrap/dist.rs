7501995977d49238091b4ddee95c9595c4866ffe
Mark Rousskov <mark.simulacrum@gmail.com>
2022-05-20 18:56:21 -0400

    Bump LLVM fetched from CI to fix run-make

--- src/bootstrap/dist.rs
+++ src/bootstrap/dist.rs
@@ -2038,6 +2038,9 @@ impl Step for RustDev {
         tarball.set_overlay(OverlayKind::LLVM);
 
         let src_bindir = builder.llvm_out(target).join("bin");
+        // If updating this list, you likely want to change
+        // src/bootstrap/download-ci-llvm-stamp as well, otherwise local users
+        // will not pick up the extra file until LLVM gets bumped.
         for bin in &[
             "llvm-config",
             "llvm-ar",
