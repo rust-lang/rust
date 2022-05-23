c516ffa8e531290687b60bb8023fbccd3abe43ad
Michael Howell <michael@notriddle.com>
2022-05-23 12:06:16 -0700

    Adjust test case to account for 6c8a2d4

--- src/test/rustdoc-js/generics-impl.js
+++ src/test/rustdoc-js/generics-impl.js
@@ -49,8 +49,8 @@ const EXPECTED = [
         ],
     },
     {
-        // -> "Ddddddd"
-        'returned': [
+        // -> Ddddddd
+        'others': [
             { 'path': 'generics_impl::Ddddddd', 'name': 'hhhhhhh' },
         ],
     },
