d1aa6d32167be4b4916399f87520e3c3cea44aed
Jake Heinz <jh@discordapp.com>
2022-05-24 02:59:19 +0000

    vscode: fix extraEnv handling numeric values

--- editors/code/src/config.ts
+++ editors/code/src/config.ts
@@ -100,8 +100,9 @@ export class Config {
     get serverPath() {
         return this.get<null | string>("server.path") ?? this.get<null | string>("serverPath");
     }
-    get serverExtraEnv() {
-        return this.get<Env | null>("server.extraEnv") ?? {};
+    get serverExtraEnv(): Env {
+        const extraEnv = this.get<{[key: string]: string | number} | null>("server.extraEnv") ?? {};
+        return Object.fromEntries(Object.entries(extraEnv).map(([k, v]) => [k, typeof v !== "string" ? v.toString(): v]));
     }
     get traceExtension() {
         return this.get<boolean>("trace.extension");
