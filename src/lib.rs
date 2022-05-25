e5b824c535242866ee004e2c6ef8da5ad162b1ad
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-05-25 08:48:14 +0000

    Documentation never hurts

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
 //! The WIP stable interface to rustc internals.
+//! 
+//! For more information see https://github.com/rust-lang/project-stable-mir
+//! 
+//! # Note
+//!
+//! This API is still completely unstable and subject to change.
 
 #![doc(
     html_root_url = "https://doc.rust-lang.org/nightly/nightly-rustc/",
