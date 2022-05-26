c6cda15a164343b1820f374ba36a37c985fa1dab
hafeoz <95505675+hafeoz@users.noreply.github.com>
2022-05-26 23:13:37 +0100

    Remove few characters for tidy check to pass

--- src/test/rustdoc-ui/doc-comment-multi-line-attr.rs
+++ src/test/rustdoc-ui/doc-comment-multi-line-attr.rs
@@ -1,4 +1,4 @@
-// Regression test for #97440: Multiline (more than 2 lines) inner attribute triggers ICE during doctest
+// Regression test for #97440: Multiline inner attribute triggers ICE during doctest
 // compile-flags:--test
 // normalize-stdout-test: "src/test/rustdoc-ui" -> "$$DIR"
 // normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
