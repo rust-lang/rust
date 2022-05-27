f2a1b7b7724fef2876094f9b4a6c356345d7dd67
Esteban Küber <esteban@kuber.com.ar>
2022-05-27 10:48:12 -0700

    Modify `derive(Debug)` to use `Self` in struct literal to avoid redundant error
    #97343

--- src/test/ui/derives/issue-97343.rs
+++ src/test/ui/derives/issue-97343.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-#[derive(Debug)] //~ ERROR expected struct, variant or union type, found type parameter `Irrelevant`
+#[derive(Debug)]
 pub struct Irrelevant<Irrelevant> { //~ ERROR type arguments are not allowed for this type
     irrelevant: Irrelevant,
 }
