fc297306ee6ca4cb097ba766bdf0dfc7295d10bf
Michael Goulet <michael@errs.io>
2022-05-28 13:34:54 -0700

    Use type_is_copy_modulo_regions check in intrisicck

---
+++ src/test/ui/asm/issue-97490.rs
@@ -0,0 +1,10 @@
+// check-pass
+
+pub type Yes = extern "sysv64" fn(&'static u8) -> !;
+
+fn main() {
+    unsafe {
+        let yes = &6 as *const _ as *const Yes;
+        core::arch::asm!("call {}", in(reg) yes, options(noreturn));
+    }
+}
