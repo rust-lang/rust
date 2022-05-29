d2657e784fe080b818b65ad62594fbcaad9bd1aa
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-29 09:39:22 +0200

    The toolchain no longer changes to default - instead we will try and use a channel

--- build-wasix.sh
+++ build-wasix.sh
@@ -4,4 +4,4 @@ sudo apt install python ninja-build
 ./x.py build
 ./x.py build --stage 2
 rustup toolchain link wasix ./build/x86_64-unknown-linux-gnu/stage2
-rustup default wasix
+#rustup default wasix
