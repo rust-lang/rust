86e53f41b099f48e25fe53aa2d13101f17fae049
Ralf Jung <post@ralfj.de>
2022-05-30 10:31:12 +0200

    print reason for ignoring

--- ui_test/src/lib.rs
+++ ui_test/src/lib.rs
@@ -89,7 +89,7 @@ pub fn run_tests(config: Config) {
                     // Ignore file if only/ignore rules do (not) apply
                     if ignore_file(&comments, &target) {
                         ignored.fetch_add(1, Ordering::Relaxed);
-                        eprintln!("{} ... {}", path.display(), "ignored".yellow());
+                        eprintln!("{} ... {}", path.display(), "ignored (in-test comment)".yellow());
                         continue;
                     }
                     // Run the test for all revisions
