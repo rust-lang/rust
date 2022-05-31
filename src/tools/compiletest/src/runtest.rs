4e573145da8888d04a974bb16809396cd81e8c8d
lionelllohcd <lionellloh@gmail.com>
2022-05-30 18:59:47 -0700

    Pass --out-dir for run-pass tests in Compiletest

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -1905,13 +1905,15 @@ impl<'test> TestCx<'test> {
         match output_file {
             TargetLocation::ThisFile(path) => {
                 rustc.arg("-o").arg(path);
+                rustc.arg("--out-dir").arg(&self.config.build_base);
+
             }
             TargetLocation::ThisDirectory(path) => {
                 if is_rustdoc {
                     // `rustdoc` uses `-o` for the output directory.
                     rustc.arg("-o").arg(path);
                 } else {
-                    rustc.arg("--out-dir").arg(path);
+                    rustc.arg("--out-dir").arg(&self.config.build_base);
                 }
             }
         }
