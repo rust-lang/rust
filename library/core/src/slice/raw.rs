2aef6c5436d0d0b2f0800b1e870d467dafd4e505
Maybe Waffle <waffle.lapkin@gmail.com>
2022-05-31 23:12:28 +0400

    Fixup feature name to be more consistent with others
    `slice_from_mut_ptr_range_const` -> `const_slice_from_mut_ptr_range`,
    we usually have `const` in the front.

--- library/core/src/slice/raw.rs
+++ library/core/src/slice/raw.rs
@@ -264,7 +264,7 @@ pub const unsafe fn from_ptr_range<'a, T>(range: Range<*const T>) -> &'a [T] {
 ///
 /// [valid]: ptr#safety
 #[unstable(feature = "slice_from_ptr_range", issue = "89792")]
-#[rustc_const_unstable(feature = "slice_from_mut_ptr_range_const", issue = "89792")]
+#[rustc_const_unstable(feature = "const_slice_from_mut_ptr_range", issue = "89792")]
 pub const unsafe fn from_mut_ptr_range<'a, T>(range: Range<*mut T>) -> &'a mut [T] {
     // SAFETY: the caller must uphold the safety contract for `from_mut_ptr_range`.
     unsafe { from_raw_parts_mut(range.start, range.end.sub_ptr(range.start)) }
