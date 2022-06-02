97fcead24219bea0db1d2133ffd347760ec161d8
Maybe Waffle <waffle.lapkin@gmail.com>
2022-06-02 20:15:41 +0400

    --bless tests

--- src/test/ui/parser/recover-quantified-closure.rs
+++ src/test/ui/parser/recover-quantified-closure.rs
@@ -1,10 +1,13 @@
 fn main() {
     for<'a> |x: &'a u8| *x + 1;
-    //~^ ERROR cannot introduce explicit parameters for a closure
+    //~^ ERROR use of undeclared lifetime name `'a`
+    //~^^ ERROR `for<...>` binders for closures are experimental
+    //~^^^ ERROR `for<...>` binders for closures are not yet supported
 }
 
 enum Foo { Bar }
 fn foo(x: impl Iterator<Item = Foo>) {
     for <Foo>::Bar in x {}
     //~^ ERROR expected one of `move`, `static`, `|`
+    //~^^ ERROR `for<...>` binders for closures are experimental
 }
