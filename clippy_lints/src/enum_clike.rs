6d94f95a201582e6c99ffc54573cd06fdfd0c165
b-naber <bn263@gmx.de>
2022-06-03 20:42:35 +0200

    address review

--- clippy_lints/src/enum_clike.rs
+++ clippy_lints/src/enum_clike.rs
@@ -50,7 +50,7 @@ impl<'tcx> LateLintPass<'tcx> for UnportableVariant {
                         .tcx
                         .const_eval_poly(def_id.to_def_id())
                         .ok()
-                        .and_then(|val| Some(rustc_middle::mir::ConstantKind::from_value(val, ty)));
+                        .map(|val| rustc_middle::mir::ConstantKind::from_value(val, ty));
                     if let Some(Constant::Int(val)) = constant.and_then(|c| miri_to_const(cx.tcx, c)) {
                         if let ty::Adt(adt, _) = ty.kind() {
                             if adt.is_enum() {
