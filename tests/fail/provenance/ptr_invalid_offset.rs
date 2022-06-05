4a852126b979d6a04f5ce2a0b132dcc88002cea1
Ralf Jung <post@ralfj.de>
2022-06-05 11:47:39 -0400

    do not use int2ptr casts in strict provenance tests

--- tests/fail/provenance/ptr_invalid_offset.rs
+++ tests/fail/provenance/ptr_invalid_offset.rs
@@ -1,9 +1,11 @@
 // compile-flags: -Zmiri-strict-provenance
 // error-pattern: not a valid pointer
+#![feature(strict_provenance)]
 
 fn main() {
     let x = 22;
     let ptr = &x as *const _ as *const u8;
-    let roundtrip = ptr as usize as *const u8;
+    let roundtrip = std::ptr::invalid::<u8>(ptr as usize);
+    // Not even offsetting this is allowed.
     let _ = unsafe { roundtrip.offset(1) };
 }
