84edb76e2698c62d65605d18f1c2294007d3b3aa
Ralf Jung <post@ralfj.de>
2022-06-06 12:33:48 -0400

    make output bitwidth-independent

--- tests/fail/transmute_fat1.rs
+++ tests/fail/transmute_fat1.rs
@@ -1,4 +1,5 @@
 // error-pattern: type validation failed: encountered a pointer
+// normalize-stderr-test: "\[u8; (08|16)\]" -> "$$ARRAY"
 
 fn main() {
     #[cfg(target_pointer_width="64")]
@@ -7,7 +8,7 @@ fn main() {
     };
     #[cfg(target_pointer_width="32")]
     let bad = unsafe {
-        std::mem::transmute::<&[u8], [u8; 8]>(&[1u8])
+        std::mem::transmute::<&[u8], [u8; 08]>(&[1u8])
     };
     let _val = bad[0] + bad[bad.len()-1];
 }
