801725a77b17a0641fcf40a310f491d71b059d80
Takayuki Maeda <takoyaki0316@gmail.com>
2022-06-14 14:58:46 +0900

    suggest adding a `#[macro_export]` to a private macro

---
+++ src/test/ui/privacy/macro-private-reexport.rs
@@ -0,0 +1,11 @@
+// edition:2018
+
+mod foo {
+    macro_rules! bar {
+        () => {};
+    }
+
+    pub use bar as _; //~ ERROR `bar` is only public within the crate, and cannot be re-exported outside
+}
+
+fn main() {}
