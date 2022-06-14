9aa1ccdae85b9d5b1575f7a2e48cf31196462e0f
Sprite <SpriteOvO@gmail.com>
2022-06-15 03:06:28 +0800

    Fix `cfg(not)` and `cfg(all)` causing Rustdoc stab to disappear

--- src/test/rustdoc/doc-auto-cfg.rs
+++ src/test/rustdoc/doc-auto-cfg.rs
@@ -1,5 +1,4 @@
 #![feature(doc_auto_cfg)]
-
 #![crate_name = "foo"]
 
 // @has foo/fn.foo.html
@@ -12,3 +11,20 @@ pub fn foo() {}
 // @!has - '//*[@class="item-info"]/*[@class="stab portability"]' 'test'
 #[cfg(any(test, doc))]
 pub fn bar() {}
+
+// @has foo/fn.appear_1.html
+// @has - '//*[@class="item-info"]/*[@class="stab portability"]' 'doc'
+// @!has - '//*[@class="item-info"]/*[@class="stab portability"]' 'non-test'
+#[cfg(any(doc, not(test)))]
+pub fn appear_1() {} // issue #98065
+
+// @has foo/fn.appear_2.html
+// @has - '//*[@class="item-info"]/*[@class="stab portability"]' 'doc'
+// @!has - '//*[@class="item-info"]/*[@class="stab portability"]' 'test'
+#[cfg(any(doc, all(test)))]
+pub fn appear_2() {} // issue #98065
+
+// @has foo/fn.appear_3.html
+// @has - '//*[@class="item-info"]/*[@class="stab portability"]' 'doc'
+#[cfg(any(doc, all()))]
+pub fn appear_3() {} // issue #98065
