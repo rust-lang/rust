736dbefaf3bd9a7cd95b4a8e5a60cb317125904c
Rémy Rakic <remy.rakic+github@gmail.com>
2022-06-14 22:19:07 +0200

    make linux PGO script more generic
    This extracts the linux-isms into variables, so that the script can be
    extended to do PGO on windows. These variables will be overriden in a
    few spots, in windows-specific blocks.

--- src/ci/pgo.sh
+++ src/ci/pgo.sh
@@ -3,44 +3,69 @@
 
 set -euxo pipefail
 
+ci_dir=`cd $(dirname $0) && pwd`
+source "$ci_dir/shared.sh"
+
+# The root checkout, where the source is located
+CHECKOUT=/checkout
+
+DOWNLOADED_LLVM=/rustroot
+
+# The main directory where the build occurs, which can be different between linux and windows
+BUILD_ROOT=$CHECKOUT/obj
+
+# The various build artifacts used in other commands: to launch rustc builds, build the perf
+# collector, and run benchmarks to gather profiling data
+BUILD_ARTIFACTS=$BUILD_ROOT/build/$PGO_HOST
+RUSTC_STAGE_0=$BUILD_ARTIFACTS/stage0/bin/rustc
+CARGO_STAGE_0=$BUILD_ARTIFACTS/stage0/bin/cargo
+RUSTC_STAGE_2=$BUILD_ARTIFACTS/stage2/bin/rustc
+
+# Make sure we have a temporary PGO work folder
+PGO_TMP=/tmp/tmp-pgo
+mkdir -p $PGO_TMP
+rm -rf $PGO_TMP/*
+
+RUSTC_PERF=$PGO_TMP/rustc-perf
+
 # Compile several crates to gather execution PGO profiles.
 # Arg0 => profiles (Debug, Opt)
 # Arg1 => scenarios (Full, IncrFull, All)
 # Arg2 => crates (syn, cargo, ...)
 gather_profiles () {
-  cd /checkout/obj
+  cd $BUILD_ROOT
 
   # Compile libcore, both in opt-level=0 and opt-level=3
-  RUSTC_BOOTSTRAP=1 ./build/$PGO_HOST/stage2/bin/rustc \
-      --edition=2021 --crate-type=lib ../library/core/src/lib.rs
-  RUSTC_BOOTSTRAP=1 ./build/$PGO_HOST/stage2/bin/rustc \
-      --edition=2021 --crate-type=lib -Copt-level=3 ../library/core/src/lib.rs
+  RUSTC_BOOTSTRAP=1 $RUSTC_STAGE_2 \
+      --edition=2021 --crate-type=lib $CHECKOUT/library/core/src/lib.rs \
+      --out-dir $PGO_TMP
+  RUSTC_BOOTSTRAP=1 $RUSTC_STAGE_2 \
+      --edition=2021 --crate-type=lib -Copt-level=3 $CHECKOUT/library/core/src/lib.rs \
+      --out-dir $PGO_TMP
 
-  cd rustc-perf
+  cd $RUSTC_PERF
 
   # Run rustc-perf benchmarks
   # Benchmark using profile_local with eprintln, which essentially just means
   # don't actually benchmark -- just make sure we run rustc a bunch of times.
   RUST_LOG=collector=debug \
-  RUSTC=/checkout/obj/build/$PGO_HOST/stage0/bin/rustc \
+  RUSTC=$RUSTC_STAGE_0 \
   RUSTC_BOOTSTRAP=1 \
-  /checkout/obj/build/$PGO_HOST/stage0/bin/cargo run -p collector --bin collector -- \
-          profile_local \
-          eprintln \
-          /checkout/obj/build/$PGO_HOST/stage2/bin/rustc \
-          --id Test \
-          --profiles $1 \
-          --cargo /checkout/obj/build/$PGO_HOST/stage0/bin/cargo \
-          --scenarios $2 \
-          --include $3
+  $CARGO_STAGE_0 run -p collector --bin collector -- \
+      profile_local \
+      eprintln \
+      $RUSTC_STAGE_2 \
+      --id Test \
+      --profiles $1 \
+      --cargo $CARGO_STAGE_0 \
+      --scenarios $2 \
+      --include $3
 
-  cd /checkout/obj
+  cd $BUILD_ROOT
 }
 
-rm -rf /tmp/rustc-pgo
-
 # This path has to be absolute
-LLVM_PROFILE_DIRECTORY_ROOT=/tmp/llvm-pgo
+LLVM_PROFILE_DIRECTORY_ROOT=$PGO_TMP/llvm-pgo
 
 # We collect LLVM profiling information and rustc profiling information in
 # separate phases. This increases build time -- though not by a huge amount --
@@ -49,34 +74,34 @@ LLVM_PROFILE_DIRECTORY_ROOT=/tmp/llvm-pgo
 # LLVM IR PGO does not respect LLVM_PROFILE_FILE, so we have to set the profiling file
 # path through our custom environment variable. We include the PID in the directory path
 # to avoid updates to profile files being lost because of race conditions.
-LLVM_PROFILE_DIR=${LLVM_PROFILE_DIRECTORY_ROOT}/prof-%p python3 ../x.py build \
+LLVM_PROFILE_DIR=${LLVM_PROFILE_DIRECTORY_ROOT}/prof-%p python3 $CHECKOUT/x.py build \
     --target=$PGO_HOST \
     --host=$PGO_HOST \
     --stage 2 library/std \
     --llvm-profile-generate
 
 # Compile rustc perf
-cp -r /tmp/rustc-perf ./
-chown -R $(whoami): ./rustc-perf
-cd rustc-perf
+cp -r /tmp/rustc-perf $RUSTC_PERF
+chown -R $(whoami): $RUSTC_PERF
+cd $RUSTC_PERF
 
-# Build the collector ahead of time, which is needed to make sure the rustc-fake
+# Build rustc-perf's collector ahead of time, which is needed to make sure the rustc-fake
 # binary used by the collector is present.
-RUSTC=/checkout/obj/build/$PGO_HOST/stage0/bin/rustc \
+RUSTC=$RUSTC_STAGE_0 \
 RUSTC_BOOTSTRAP=1 \
-/checkout/obj/build/$PGO_HOST/stage0/bin/cargo build -p collector
+$CARGO_STAGE_0 build -p collector
 
 # Here we're profiling LLVM, so we only care about `Debug` and `Opt`, because we want to stress
 # codegen. We also profile some of the most prolific crates.
 gather_profiles "Debug,Opt" "Full" \
-"syn-1.0.89,cargo-0.60.0,serde-1.0.136,ripgrep-13.0.0,regex-1.5.5,clap-3.1.6,hyper-0.14.18"
+    "syn-1.0.89,cargo-0.60.0,serde-1.0.136,ripgrep-13.0.0,regex-1.5.5,clap-3.1.6,hyper-0.14.18"
 
-LLVM_PROFILE_MERGED_FILE=/tmp/llvm-pgo.profdata
+LLVM_PROFILE_MERGED_FILE=$PGO_TMP/llvm-pgo.profdata
 
 # Merge the profile data we gathered for LLVM
 # Note that this uses the profdata from the clang we used to build LLVM,
 # which likely has a different version than our in-tree clang.
-/rustroot/bin/llvm-profdata merge -o ${LLVM_PROFILE_MERGED_FILE} ${LLVM_PROFILE_DIRECTORY_ROOT}
+$DOWNLOADED_LLVM/bin/llvm-profdata merge -o ${LLVM_PROFILE_MERGED_FILE} ${LLVM_PROFILE_DIRECTORY_ROOT}
 
 echo "LLVM PGO statistics"
 du -sh ${LLVM_PROFILE_MERGED_FILE}
@@ -84,17 +109,20 @@ du -sh ${LLVM_PROFILE_DIRECTORY_ROOT}
 echo "Profile file count"
 find ${LLVM_PROFILE_DIRECTORY_ROOT} -type f | wc -l
 
+# We don't need the individual .profraw files now that they have been merged into a final .profdata
+rm -r $LLVM_PROFILE_DIRECTORY_ROOT
+
 # Rustbuild currently doesn't support rebuilding LLVM when PGO options
 # change (or any other llvm-related options); so just clear out the relevant
 # directories ourselves.
-rm -r ./build/$PGO_HOST/llvm ./build/$PGO_HOST/lld
+rm -r $BUILD_ARTIFACTS/llvm $BUILD_ARTIFACTS/lld
 
 # Okay, LLVM profiling is done, switch to rustc PGO.
 
 # The path has to be absolute
-RUSTC_PROFILE_DIRECTORY_ROOT=/tmp/rustc-pgo
+RUSTC_PROFILE_DIRECTORY_ROOT=$PGO_TMP/rustc-pgo
 
-python3 ../x.py build --target=$PGO_HOST --host=$PGO_HOST \
+python3 $CHECKOUT/x.py build --target=$PGO_HOST --host=$PGO_HOST \
     --stage 2 library/std \
     --rust-profile-generate=${RUSTC_PROFILE_DIRECTORY_ROOT}
 
@@ -105,13 +133,13 @@ python3 ../x.py build --target=$PGO_HOST --host=$PGO_HOST \
 # That's why we override the profile path to include the PID. This will produce many more profiling
 # files, but the resulting profile will produce a slightly faster rustc binary.
 LLVM_PROFILE_FILE=${RUSTC_PROFILE_DIRECTORY_ROOT}/default_%m_%p.profraw gather_profiles \
-  "Check,Debug,Opt" "All" \
-  "externs,ctfe-stress-5,cargo-0.60.0,token-stream-stress,match-stress,tuple-stress,diesel-1.4.8,bitmaps-3.1.0"
+    "Check,Debug,Opt" "All" \
+    "externs,ctfe-stress-5,cargo-0.60.0,token-stream-stress,match-stress,tuple-stress,diesel-1.4.8,bitmaps-3.1.0"
 
-RUSTC_PROFILE_MERGED_FILE=/tmp/rustc-pgo.profdata
+RUSTC_PROFILE_MERGED_FILE=$PGO_TMP/rustc-pgo.profdata
 
 # Merge the profile data we gathered
-./build/$PGO_HOST/llvm/bin/llvm-profdata \
+$BUILD_ARTIFACTS/llvm/bin/llvm-profdata \
     merge -o ${RUSTC_PROFILE_MERGED_FILE} ${RUSTC_PROFILE_DIRECTORY_ROOT}
 
 echo "Rustc PGO statistics"
@@ -120,10 +148,13 @@ du -sh ${RUSTC_PROFILE_DIRECTORY_ROOT}
 echo "Profile file count"
 find ${RUSTC_PROFILE_DIRECTORY_ROOT} -type f | wc -l
 
+# We don't need the individual .profraw files now that they have been merged into a final .profdata
+rm -r $RUSTC_PROFILE_DIRECTORY_ROOT
+
 # Rustbuild currently doesn't support rebuilding LLVM when PGO options
 # change (or any other llvm-related options); so just clear out the relevant
 # directories ourselves.
-rm -r ./build/$PGO_HOST/llvm ./build/$PGO_HOST/lld
+rm -r $BUILD_ARTIFACTS/llvm $BUILD_ARTIFACTS/lld
 
 # This produces the actual final set of artifacts, using both the LLVM and rustc
 # collected profiling data.
