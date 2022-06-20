42eeb58e5dc8d409d0f1db40486f9f8db4436913
Patrick Walton <pcwalton@fb.com>
2022-06-20 16:26:51 -0700

    Implement support for DWARF version 5.
    DWARF version 5 brings a number of improvements over version 4. Quoting from
    the announcement [1]:
    
    > Version 5 incorporates improvements in many areas: better data compression,
    > separation of debugging data from executable files, improved description of
    > macros and source files, faster searching for symbols, improved debugging
    > optimized code, as well as numerous improvements in functionality and
    > performance.
    
    On platforms where DWARF version 5 is supported (Linux, primarily), this commit
    adds support for it behind a new `-Z dwarf-version=5` flag.
    
    [1]: https://dwarfstd.org/Public_Review.php

--- compiler/rustc_interface/src/tests.rs
+++ compiler/rustc_interface/src/tests.rs
@@ -733,6 +733,7 @@ fn test_debugging_options_tracking_hash() {
     tracked!(dep_info_omit_d_target, true);
     tracked!(drop_tracking, true);
     tracked!(dual_proc_macros, true);
+    tracked!(dwarf_version, Some(5));
     tracked!(fewer_names, Some(true));
     tracked!(force_unstable_if_unmarked, true);
     tracked!(fuel, Some(("abc".to_string(), 99)));
