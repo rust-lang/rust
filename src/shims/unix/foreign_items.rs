84a02787d8cc81f10bc9119dec5239c687915210
infrandomness <infrandomness@gmail.com>
2022-06-27 01:13:13 +0200

    Address code review
    - Merge pthread_attr_getstack shim to unix/foreign_items.rs

--- src/shims/unix/foreign_items.rs
+++ src/shims/unix/foreign_items.rs
@@ -461,6 +461,28 @@ pub trait EvalContextExt<'mir, 'tcx: 'mir>: crate::MiriEvalContextExt<'mir, 'tcx
                 this.write_null(dest)?;
             }
 
+            // Querying system information
+            "pthread_attr_getstack" => {
+                // We don't support "pthread_attr_setstack", so we just pretend all stacks have the same values here.
+                let [attr_place, addr_place, size_place] =
+                    this.check_shim(abi, Abi::C { unwind: false }, link_name, args)?;
+                this.deref_operand(attr_place)?;
+                let addr_place = this.deref_operand(addr_place)?;
+                let size_place = this.deref_operand(size_place)?;
+
+                this.write_scalar(
+                    Scalar::from_uint(STACK_ADDR, this.pointer_size()),
+                    &addr_place.into(),
+                )?;
+                this.write_scalar(
+                    Scalar::from_uint(STACK_SIZE, this.pointer_size()),
+                    &size_place.into(),
+                )?;
+
+                // Return success (`0`).
+                this.write_null(dest)?;
+            }
+
             | "signal"
             | "sigaltstack"
             if this.frame_in_std() => {
--- src/shims/unix/freebsd/foreign_items.rs
+++ src/shims/unix/freebsd/foreign_items.rs
@@ -18,28 +18,6 @@ pub trait EvalContextExt<'mir, 'tcx: 'mir>: crate::MiriEvalContextExt<'mir, 'tcx
     ) -> InterpResult<'tcx, EmulateByNameResult<'mir, 'tcx>> {
         let this = self.eval_context_mut();
         match link_name.as_str() {
-            // Querying system information
-            "pthread_attr_getstack" => {
-                // We don't support "pthread_attr_setstack", so we just pretend all stacks have the same values here.
-                let [attr_place, addr_place, size_place] =
-                    this.check_shim(abi, Abi::C { unwind: false }, link_name, args)?;
-                this.deref_operand(attr_place)?;
-                let addr_place = this.deref_operand(addr_place)?;
-                let size_place = this.deref_operand(size_place)?;
-
-                this.write_scalar(
-                    Scalar::from_uint(STACK_ADDR, this.pointer_size()),
-                    &addr_place.into(),
-                )?;
-                this.write_scalar(
-                    Scalar::from_uint(STACK_SIZE, this.pointer_size()),
-                    &size_place.into(),
-                )?;
-
-                // Return success (`0`).
-                this.write_null(dest)?;
-            }
-
             // Linux's `pthread_getattr_np` equivalent
             "pthread_attr_get_np" if this.frame_in_std() => {
                 let [_thread, _attr] =
