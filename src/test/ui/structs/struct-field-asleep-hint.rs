d9cc4e2f232f96d766c58d9d0c31d5fd489da801
Soveu <marx.tomasz@gmail.com>
2022-06-26 22:01:52 +0200

    rustc_typeck: check if programmer has fallen asleep - unit test

---
+++ src/test/ui/structs/struct-field-asleep-hint.rs
@@ -0,0 +1,9 @@
+struct S { pre: i32 }
+impl S {
+    fn f(self) -> i32 {
+        self.prehopjpokppppppppppppppppppp
+        //~^ ERROR it seems like you've fallen asleep and hit your keyboard
+    }
+}
+
+fn main() {}
