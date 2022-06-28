49e6b7986a49a0ea1cd17a7853f26fe89883ef00
Gary Guo <gary@garyguo.net>
2022-06-28 17:18:01 +0100

    Fix test for non-prefer-dynamic target

--- src/test/ui/panic-runtime/need-unwind-got-abort.rs
+++ src/test/ui/panic-runtime/need-unwind-got-abort.rs
@@ -2,6 +2,7 @@
 // error-pattern:is incompatible with this crate's strategy of `abort`
 // aux-build:needs-unwind.rs
 // compile-flags:-C panic=abort
+// no-prefer-dynamic
 
 extern crate needs_unwind;
 
