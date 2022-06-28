2888e76d06483e505bf4956a08bc24d4e7764beb
Martin Nordholts <enselic@gmail.com>
2022-06-28 11:02:14 +0200

    test/rustdoc-json/assoc_type.rs: Maximize chance of detecting future ICEs

--- src/test/rustdoc-json/assoc_type.rs
+++ src/test/rustdoc-json/assoc_type.rs
@@ -4,6 +4,7 @@
 // @has - "$.index[*][?(@.name=='Trait')]"
 // @has - "$.index[*][?(@.name=='AssocType')]"
 // @has - "$.index[*][?(@.name=='S')]"
+// @has - "$.index[*][?(@.name=='S2')]"
 
 pub trait Trait {
     type AssocType;
@@ -14,3 +15,8 @@ impl<T> Trait for T {
 }
 
 pub struct S;
+
+/// Not needed for the #98547 ICE to occur, but added to maximize the chance of
+/// getting an ICE in the future. See
+/// <https://github.com/rust-lang/rust/pull/98548#discussion_r908219164>
+pub struct S2;
