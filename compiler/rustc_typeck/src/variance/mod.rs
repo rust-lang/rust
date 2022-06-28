f97326de454583d36fef2efbfeb1ea384b9e9a50
Yan Chen <ychen2@futurewei.com>
2022-06-28 12:46:42 -0700

    Fix #98260, added the test case

--- compiler/rustc_typeck/src/variance/mod.rs
+++ compiler/rustc_typeck/src/variance/mod.rs
@@ -37,6 +37,11 @@ fn crate_variances(tcx: TyCtxt<'_>, (): ()) -> CrateVariancesMap<'_> {
 }
 
 fn variances_of(tcx: TyCtxt<'_>, item_def_id: DefId) -> &[ty::Variance] {
+    // Skip items with no generics - there's nothing to infer in them.
+    if tcx.generics_of(item_def_id).count() == 0 {
+        return &[];
+    }
+
     match tcx.def_kind(item_def_id) {
         DefKind::Fn
         | DefKind::AssocFn
