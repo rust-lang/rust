9118087215b0cc129914a3fe1ef1f2ac2cb69156
Santiago Pastorino <spastorino@gmail.com>
2022-06-28 13:07:16 -0300

    Add APIT with lifetimes test

---
+++ src/test/ui/impl-trait/v2/sized_bounds.rs
@@ -0,0 +1,12 @@
+//run-pass
+#![deny(rust_2021_incompatible_closure_captures)]
+#![allow(unused_must_use)]
+#![feature(return_position_impl_trait_v2)]
+
+fn filter_try_fold(predicate: &mut impl FnMut() -> bool) -> impl FnMut() -> bool + '_ {
+    move || predicate()
+}
+
+fn main() {
+    filter_try_fold(&mut || true);
+}
