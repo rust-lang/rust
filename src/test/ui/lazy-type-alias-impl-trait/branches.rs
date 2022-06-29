77e88a7c7afce2389b16b385f494be2f836c43ff
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-06-29 08:27:14 +0000

    Add more tests

--- src/test/ui/lazy-type-alias-impl-trait/branches.rs
+++ src/test/ui/lazy-type-alias-impl-trait/branches.rs
@@ -1,7 +1,5 @@
 #![feature(type_alias_impl_trait)]
 
-// check-pass
-
 type Foo = impl std::fmt::Debug;
 
 fn foo(b: bool) -> Foo {
@@ -10,6 +8,18 @@ fn foo(b: bool) -> Foo {
     } else {
         std::iter::empty().collect()
     }
+}
+
+type Bar = impl std::fmt::Debug;
+
+fn bar(b: bool) -> Bar {
+    let x: Bar = if b {
+        vec![42_i32]
+    } else {
+        std::iter::empty().collect()
+        //~^ ERROR  a value of type `Bar` cannot be built from an iterator over elements of type `_`
+    };
+    x
 }
 
 fn main() {}
