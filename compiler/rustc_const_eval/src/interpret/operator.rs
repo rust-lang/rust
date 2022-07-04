0850bad94dab9499a6dde0f303a73220a21a2f71
Ralf Jung <post@ralfj.de>
2022-07-04 09:12:22 -0400

    extra assertion, extra sure

--- compiler/rustc_const_eval/src/interpret/operator.rs
+++ compiler/rustc_const_eval/src/interpret/operator.rs
@@ -32,6 +32,7 @@ impl<'mir, 'tcx: 'mir, M: Machine<'mir, 'tcx>> InterpCx<'mir, 'tcx, M> {
             let pair = Immediate::ScalarPair(val.into(), Scalar::from_bool(overflowed).into());
             self.write_immediate(pair, dest)?;
         } else {
+            assert!(self.tcx.sess.opts.debugging_opts.randomize_layout);
             // With randomized layout, `(int, bool)` might cease to be a `ScalarPair`, so we have to
             // do a component-wise write here. This code path is slower than the above because
             // `place_field` will have to `force_allocate` locals here.
