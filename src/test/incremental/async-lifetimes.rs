9715ea4562ef9eabcf326167a15305eba266b40a
Camille GILLOT <gillot.camille@gmail.com>
2022-07-06 10:56:35 +0200

    Add regression test.

---
+++ src/test/incremental/async-lifetimes.rs
@@ -0,0 +1,17 @@
+// revisions: rpass1 rpass2
+// edition:2021
+
+#![allow(unused)]
+
+struct Foo;
+
+impl Foo {
+    async fn f(&self, _: &&()) -> &() {
+        &()
+    }
+}
+
+#[cfg(rpass2)]
+enum Bar {}
+
+fn main() {}
