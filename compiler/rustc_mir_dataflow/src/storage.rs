2524a89ecd735edecd64c1dbd3201e12810da668
pierwill <pierwill@users.noreply.github.com>
2022-07-07 12:58:43 -0500

    iterate over localdecls

--- compiler/rustc_mir_dataflow/src/storage.rs
+++ compiler/rustc_mir_dataflow/src/storage.rs
@@ -1,5 +1,5 @@
 use rustc_index::bit_set::BitSet;
-use rustc_middle::mir::{self, Local};
+use rustc_middle::mir::{self, HasLocalDecls, Local};
 
 /// The set of locals in a MIR body that do not have `StorageLive`/`StorageDead` annotations.
 ///
@@ -10,12 +10,9 @@ use rustc_middle::mir::{self, Local};
 pub fn always_storage_live_locals(body: &mir::Body<'_>) -> BitSet<Local> {
     let mut always_live_locals = BitSet::new_filled(body.local_decls.len());
 
-    for block in body.basic_blocks() {
-        for statement in &block.statements {
-            use mir::StatementKind::{StorageDead, StorageLive};
-            if let StorageLive(l) | StorageDead(l) = statement.kind {
-                always_live_locals.remove(l);
-            }
+    for local in body.local_decls() {
+        if !local.always_storage_live {
+            always_live_locals.remove(local);
         }
     }
 
