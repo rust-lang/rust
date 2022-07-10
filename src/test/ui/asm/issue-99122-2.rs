3ac69d7476fdbc5fc279d28cf523dab6453d014f
Michael Goulet <michael@errs.io>
2022-07-10 18:55:38 +0000

    Fix sized check ICE in intrisicck

---
+++ src/test/ui/asm/issue-99122-2.rs
@@ -0,0 +1,18 @@
+// check-pass
+// This demonstrates why we need to erase regions before sized check in intrinsicck
+
+struct NoCopy;
+
+struct Wrap<'a, T, Tail: ?Sized>(&'a T, Tail);
+
+pub unsafe fn test() {
+    let i = NoCopy;
+    let j = Wrap(&i, ());
+    let pointer = &j as *const _;
+    core::arch::asm!(
+        "nop",
+        in("eax") pointer,
+    );
+}
+
+fn main() {}
