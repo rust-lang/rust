e65214785d85a4e2c76141e14eb38a55f1fd21d1
Deadbeef <ent3rm4n@gmail.com>
2022-07-10 13:31:58 +0000

    add more tests

--- src/test/ui/repr/auxiliary/repr-transparent-non-exhaustive.rs
+++ src/test/ui/repr/auxiliary/repr-transparent-non-exhaustive.rs
@@ -5,6 +5,14 @@ pub struct Private { _priv: () }
 #[non_exhaustive]
 pub struct NonExhaustive {}
 
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {}
+
+pub enum NonExhaustiveVariant {
+    #[non_exhaustive]
+    A,
+}
+
 pub struct ExternalIndirection<T> {
     pub x: T,
 }
