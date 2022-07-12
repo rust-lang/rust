cd3535d25664f02cbf66863a3ccfa682d8082629
Ralf Jung <post@ralfj.de>
2022-07-12 09:49:11 -0400

    test that we also find bad uses of mem::uninitialized

--- tests/fail/validity/uninit_float.rs
+++ tests/fail/validity/uninit_float.rs
@@ -1,6 +1,8 @@
+#![allow(deprecated)]
 // This test is adapted from https://github.com/rust-lang/miri/issues/1340#issue-600900312.
 
 fn main() {
-    let _val = unsafe { std::mem::MaybeUninit::<f32>::uninit().assume_init() };
+    // Deliberately using `mem::uninitialized` to make sure that despite all the mitigations, we consider this UB.
+    let _val: f32 = unsafe { std::mem::uninitialized() };
     //~^ ERROR: constructing invalid value at .value: encountered uninitialized bytes, but expected initialized bytes
 }
