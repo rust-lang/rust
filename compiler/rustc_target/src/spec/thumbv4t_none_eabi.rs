6c22b44537005ffd0cfb8513c5ac09fe9906e13f
Lokathor <zefria@gmail.com>
2022-07-13 23:37:08 -0600

    add missing imports.

--- compiler/rustc_target/src/spec/thumbv4t_none_eabi.rs
+++ compiler/rustc_target/src/spec/thumbv4t_none_eabi.rs
@@ -16,7 +16,7 @@
 //! The default link script is very likely wrong, so you should use
 //! `-Clink-arg=-Tmy_script.ld` to override that with a correct linker script.
 
-use crate::spec::{cvs, LinkerFlavor, Target, TargetOptions};
+use crate::spec::{cvs, LinkerFlavor, Target, TargetOptions, PanicStrategy, RelocModel};
 
 pub fn target() -> Target {
     Target {
