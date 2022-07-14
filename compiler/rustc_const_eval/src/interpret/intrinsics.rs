ed7303766138aa9c1ef79c877cb610ec604a62b8
Daniel Bevenius <daniel.bevenius@gmail.com>
2022-07-14 15:35:19 +0200

    Remove comment referring to constness.rs
    This commit removes the comment in emulate_intrinsic, which is
    currently referring to 'src/librustc_middle/ty/constness.rs'.

--- compiler/rustc_const_eval/src/interpret/intrinsics.rs
+++ compiler/rustc_const_eval/src/interpret/intrinsics.rs
@@ -132,8 +132,6 @@ impl<'mir, 'tcx: 'mir, M: Machine<'mir, 'tcx>> InterpCx<'mir, 'tcx, M> {
             Some(p) => p,
         };
 
-        // Keep the patterns in this match ordered the same as the list in
-        // `src/librustc_middle/ty/constness.rs`
         match intrinsic_name {
             sym::caller_location => {
                 let span = self.find_closest_untracked_caller_location();
