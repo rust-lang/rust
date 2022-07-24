c33fc24566da5e342e03183c07314c910b690d25
Ralf Jung <post@ralfj.de>
2022-07-24 08:37:29 -0400

    rustup

--- tests/fail/dangling_pointers/null_pointer_write_zst.rs
+++ tests/fail/dangling_pointers/null_pointer_write_zst.rs
@@ -1,6 +1,5 @@
 // Some optimizations remove ZST accesses, thus masking this UB.
 //@compile-flags: -Zmir-opt-level=0
-//@error-pattern: memory access failed: null pointer is a dangling pointer
 
 #[allow(deref_nullptr)]
 fn main() {
@@ -8,4 +7,5 @@ fn main() {
     // Also not assigning directly as that's array initialization, not assignment.
     let zst_val = [1u8; 0];
     unsafe { std::ptr::null_mut::<[u8; 0]>().write(zst_val) };
+    //~^ERROR: memory access failed: null pointer is a dangling pointer
 }
