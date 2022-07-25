2e7b4217781896f2856faa97f87d5a0fc24c53ce
Tomasz Miąsko <tomasz.miasko@gmail.com>
2022-07-25 00:00:00 +0000

    Diagnose missing make includes

--- src/test/run-make/coverage/coverage_tools.mk
+++ src/test/run-make/coverage/coverage_tools.mk
@@ -1,6 +1,6 @@
 # Common Makefile include for Rust `run-make/coverage-* tests. Include this
 # file with the line:
 #
-# -include ../coverage/coverage_tools.mk
+# include ../coverage/coverage_tools.mk
 
--include ../tools.mk
+include ../../run-make-fulldeps/tools.mk
