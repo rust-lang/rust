bb20b0f97fca69b7b45cb2b23fb824c99b3c6cbf
Gimgim <93856041+gimbles@users.noreply.github.com>
2022-07-26 15:25:09 +0530

    Add tests

--- src/test/ui/parser/public-instead-of-pub.rs
+++ src/test/ui/parser/public-instead-of-pub.rs
@@ -1,4 +1,6 @@
 // Checks what happens when `public` is used instead of the correct, `pub`
 // edition:2018
+// run-rustfix
 public struct MyStruct;
 //~^ ERROR 3:8: 3:14: expected one of `!` or `::`, found keyword `struct`
+//~^^ HELP write `pub` instead of `public` to make the item public
