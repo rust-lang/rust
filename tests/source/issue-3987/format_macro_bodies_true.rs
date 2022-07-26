a7801aac272ce50da191af86bab988284de534be
Yacin Tmimi <yacintmimi@gmail.com>
2022-07-25 21:38:09 -0400

    Add test for issue 3987
    Closes 3987
    
    It's unclear which commit resolved this issue, but it can no longer be
    reproduced.

---
+++ tests/source/issue-3987/format_macro_bodies_true.rs
@@ -0,0 +1,26 @@
+// rustfmt-format_macro_bodies: true
+
+// with comments
+macro_rules! macros {
+    () => {{
+        Struct {
+            field: (
+                42 + //comment 1
+                42
+                //comment 2
+            ),
+        };
+    }};
+}
+
+// without comments
+macro_rules! macros {
+    () => {{
+        Struct {
+            field: (
+                42 +
+                42
+            ),
+        };
+    }};
+}
