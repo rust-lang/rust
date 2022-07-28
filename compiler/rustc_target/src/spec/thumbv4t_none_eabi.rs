2eac6f30c8a1cd59dc640bf68bc58eb7fe656d61
Lokathor <zefria@gmail.com>
2022-07-28 10:58:42 -0600

    once again tidy was unhappy

--- compiler/rustc_target/src/spec/thumbv4t_none_eabi.rs
+++ compiler/rustc_target/src/spec/thumbv4t_none_eabi.rs
@@ -16,7 +16,9 @@
 //! The default link script is very likely wrong, so you should use
 //! `-Clink-arg=-Tmy_script.ld` to override that with a correct linker script.
 
-use crate::spec::{cvs, LinkerFlavor, FramePointer, PanicStrategy, RelocModel, Target, TargetOptions};
+use crate::spec::{
+    cvs, FramePointer, LinkerFlavor, PanicStrategy, RelocModel, Target, TargetOptions,
+};
 
 pub fn target() -> Target {
     Target {
