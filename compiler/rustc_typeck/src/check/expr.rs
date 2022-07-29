06f89b7924af4657e847742bf0e40eafaaee52f9
Takayuki Maeda <takoyaki0316@gmail.com>
2022-07-29 14:12:17 +0900

    implement `point_at_index_if_possible`

--- compiler/rustc_typeck/src/check/expr.rs
+++ compiler/rustc_typeck/src/check/expr.rs
@@ -2649,17 +2649,7 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
                     // two-phase not needed because index_ty is never mutable
                     self.demand_coerce(idx, idx_t, index_ty, None, AllowTwoPhase::No);
                     self.select_obligations_where_possible(false, |errors| {
-                        for error in errors {
-                            match error.obligation.predicate.kind().skip_binder() {
-                                ty::PredicateKind::Trait(predicate)
-                                    if self.tcx.is_diagnostic_item(
-                                        sym::SliceIndex,
-                                        predicate.trait_ref.def_id,
-                                    ) => {}
-                                _ => continue,
-                            }
-                            error.obligation.cause.span = idx.span;
-                        }
+                        self.point_at_index_if_possible(errors, idx.span)
                     });
                     element_ty
                 }
@@ -2701,6 +2691,22 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
                     self.tcx.ty_error()
                 }
             }
+        }
+    }
+
+    fn point_at_index_if_possible(
+        &self,
+        errors: &mut Vec<traits::FulfillmentError<'tcx>>,
+        span: Span,
+    ) {
+        for error in errors {
+            match error.obligation.predicate.kind().skip_binder() {
+                ty::PredicateKind::Trait(predicate)
+                    if self.tcx.is_diagnostic_item(sym::SliceIndex, predicate.trait_ref.def_id) => {
+                }
+                _ => continue,
+            }
+            error.obligation.cause.span = span;
         }
     }
 
