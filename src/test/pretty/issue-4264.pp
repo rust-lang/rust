15c5afd3b6ed6bc1f8fac7df77610638a8e85e5d
Frank Steffahn <frank.steffahn@stu.uni-kiel.de>
2022-07-30 01:10:14 +0200

    Bless changes in Pretty test output

--- src/test/pretty/issue-4264.pp
+++ src/test/pretty/issue-4264.pp
@@ -32,9 +32,9 @@ fn bar() ({
         ({
                 let res =
                     ((::alloc::fmt::format as
-                            for<'r> fn(Arguments<'r>) -> String {format})(((::core::fmt::Arguments::new_v1
+                            [fn item {format}: for<'r> fn(Arguments<'r>) -> String])(((::core::fmt::Arguments::new_v1
                                 as
-                                fn(&[&'static str], &[ArgumentV1]) -> Arguments {Arguments::new_v1})((&([("test"
+                                [fn item {Arguments::new_v1}: fn(&[&'static str], &[ArgumentV1]) -> Arguments])((&([("test"
                                             as &str)] as [&str; 1]) as &[&str; 1]),
                             (&([] as [ArgumentV1; 0]) as &[ArgumentV1; 0])) as
                             Arguments)) as String);
@@ -51,7 +51,7 @@ fn id<T>(x: T) -> T ({ (x as T) } as T)
 fn use_id() ({
         let _ =
             ((id::<[i32; (3 as usize)]> as
-                    fn([i32; 3]) -> [i32; 3] {id::<[i32; 3]>})(([(1 as i32),
-                        (2 as i32), (3 as i32)] as [i32; 3])) as [i32; 3]);
+                    [fn item {id::<[i32; 3]>}: fn([i32; 3]) -> [i32; 3]])(([(1
+                            as i32), (2 as i32), (3 as i32)] as [i32; 3])) as [i32; 3]);
     } as ())
 fn main() ({ } as ())
