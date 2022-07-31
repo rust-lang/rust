df13721863253f6298264165348a60dfb9783f94
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2022-07-31 10:03:44 +0000

    Remove workarounds for issue 59998

--- src/test/ui/proc-macro/invalid-punct-ident-2.rs
+++ src/test/ui/proc-macro/invalid-punct-ident-2.rs
@@ -1,9 +1,4 @@
 // aux-build:invalid-punct-ident.rs
-// ignore-stage1
-// only-linux
-//
-// FIXME: This should be a normal (stage1, all platforms) test in
-// src/test/ui/proc-macro once issue #59998 is fixed.
 
 #[macro_use]
 extern crate invalid_punct_ident;
