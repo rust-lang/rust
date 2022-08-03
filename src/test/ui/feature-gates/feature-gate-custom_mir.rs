a2f3649152f917b95c6c6d017aefa85ab15ec4c6
Jakob Degen <jakob.e.degen@gmail.com>
2022-08-03 04:30:13 -0700

    Add support for custom MIR parsing

---
+++ src/test/ui/feature-gates/feature-gate-custom_mir.rs
@@ -0,0 +1,12 @@
+#![feature(core_intrinsics)]
+
+extern crate core;
+
+#[custom_mir(dialect = "built")] //~ ERROR the `#[custom_mir]` attribute is just used for the Rust test suite
+pub fn foo(_x: i32) -> i32 {
+    0
+}
+
+fn main() {
+    assert_eq!(2, foo(2));
+}
