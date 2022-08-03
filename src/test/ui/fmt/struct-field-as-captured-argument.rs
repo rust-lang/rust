a0a2ec332621d35b92633dc4db0451e0c3cb4ab2
Takayuki Maeda <takoyaki0316@gmail.com>
2022-08-03 11:28:00 +0900

    add tests for `Debug` formatters and precision formatters

--- src/test/ui/fmt/struct-field-as-captured-argument.rs
+++ src/test/ui/fmt/struct-field-as-captured-argument.rs
@@ -12,4 +12,7 @@ fn main() {
     format!("{foo.field} {} {bar}", "aa"); //~ ERROR invalid format string: field access isn't supported
     format!("{foo.field} {} {1} {bar}", "aa", "bb"); //~ ERROR invalid format string: field access isn't supported
     format!("{foo.field} {} {baz}", "aa", baz = 3); //~ ERROR invalid format string: field access isn't supported
+    format!("{foo.field:?} {} {baz}", "aa", baz = 3); //~ ERROR invalid format string: field access isn't supported
+    format!("{foo.field:#?} {} {baz}", "aa", baz = 3); //~ ERROR invalid format string: field access isn't supported
+    format!("{foo.field:.3} {} {baz}", "aa", baz = 3); //~ ERROR invalid format string: field access isn't supported
 }
