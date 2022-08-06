a6cfabc88a79fe039a9c3d0c541f6517fb9767e2
Camille GILLOT <gillot.camille@gmail.com>
2022-08-06 19:42:52 +0200

    Avoid ICE in rustdoc.

---
+++ src/test/rustdoc/fn-bound.rs
@@ -0,0 +1,17 @@
+use std::iter::Peekable;
+
+pub struct Span<F: Fn(&i32)> {
+    inner: Peekable<ConditionalIterator<F>>,
+}
+
+struct ConditionalIterator<F> {
+    f: F,
+}
+
+impl<F: Fn(&i32)> Iterator for ConditionalIterator<F> {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
