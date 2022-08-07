8823634db8c4ee65b4a3d47ccb1d81891e33d0de
Camille GILLOT <gillot.camille@gmail.com>
2022-08-08 00:05:20 +0200

    Manually cleanup token stream when macro expansion aborts.

---
+++ src/test/ui/macros/syntax-error-recovery.rs
@@ -0,0 +1,17 @@
+macro_rules! values {
+    ($($token:ident($value:literal) $(as $inner:ty)? => $attr:meta,)*) => {
+        #[derive(Debug)]
+        pub enum TokenKind {
+            $(
+                #[$attr]
+                $token $($inner)? = $value,
+            )*
+        }
+    };
+}
+//~^^^^^ ERROR expected one of `(`, `,`, `=`, `{`, or `}`, found `(String)`
+//~| ERROR macro expansion ignores token `(String)` and any following
+
+values!(STRING(1) as (String) => cfg(test),);
+
+fn main() {}
