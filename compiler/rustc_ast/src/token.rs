863488dfd55c47ace05cc16ef9453831ca4335d5
yukang <moorekang@gmail.com>
2022-08-07 17:17:27 +0800

    parser will not give wrong help message for 'public'

--- compiler/rustc_ast/src/token.rs
+++ compiler/rustc_ast/src/token.rs
@@ -436,6 +436,26 @@ impl Token {
             || self == &OpenDelim(Delimiter::Parenthesis)
     }
 
+    /// Returns `true` if the token can appear at the start of a item
+    pub fn can_begin_item(&self) -> bool {
+        self.is_keyword(kw::Use)
+            || self.is_keyword(kw::Fn)
+            || self.is_keyword(kw::Extern)
+            || self.is_keyword(kw::Crate)
+            || self.is_keyword(kw::Mod)
+            || self.is_keyword(kw::Const)
+            || self.is_keyword(kw::Static)
+            || self.is_keyword(kw::Trait)
+            || self.is_keyword(kw::Impl)
+            || self.is_keyword(kw::Mod)
+            || self.is_keyword(kw::Type)
+            || self.is_keyword(kw::Enum)
+            || self.is_keyword(kw::Struct)
+            || self.is_keyword(kw::Union)
+            || self.is_keyword(kw::Macro)
+            || self == &OpenDelim(Delimiter::Parenthesis)
+    }
+
     /// Returns `true` if the token is any literal.
     pub fn is_lit(&self) -> bool {
         matches!(self.kind, Literal(..))
