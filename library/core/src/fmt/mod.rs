cc8259e4b6b1c8c390969ad24ce09ca502b55f69
Dan Vail <danielhvail@live.com>
2022-08-07 21:02:04 -0500

    Adding more verbose documentation for `std::fmt::Write`

--- library/core/src/fmt/mod.rs
+++ library/core/src/fmt/mod.rs
@@ -119,6 +119,13 @@ pub trait Write {
     ///
     /// This function will return an instance of [`Error`] on error.
     ///
+    /// Though it is possible for implementors of this trait to return an error, at the time
+    /// of writing these docs, no implementation of [`std::fmt::Write`] in the standard library
+    /// returns such an error.
+    ///
+    /// When working with external crates, it is advised to check the implementation of this
+    /// trait and anticipate any possible [`Error`]s.
+    ///
     /// # Examples
     ///
     /// ```
