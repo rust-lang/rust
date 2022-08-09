ccc558a9751074e6a075266192cb2e8393ba16a7
Maybe Waffle <waffle.lapkin@gmail.com>
2022-08-09 19:51:45 +0400

    make `ptr_mask` codegen test more specific

--- src/test/codegen/intrinsics/mask.rs
+++ src/test/codegen/intrinsics/mask.rs
@@ -1,7 +1,11 @@
 #![crate_type = "lib"]
 #![feature(core_intrinsics)]
 
+// CHECK-LABEL: @mask_ptr
+// CHECK-SAME: [[WORD:i[0-9]+]] %mask
+#[no_mangle]
 pub fn mask_ptr(ptr: *const u8, mask: usize) -> *const u8 {
-    // CHECK: llvm.ptrmask
+    // CHECK: call
+    // CHECK-SAME: @llvm.ptrmask.p0isVoid.[[WORD]](
     core::intrinsics::ptr_mask(ptr, mask)
 }
