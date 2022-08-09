2bd947998497c0bf2dc91152528eb654bbe6091b
Ralf Jung <post@ralfj.de>
2022-08-09 09:40:44 -0400

    compare with-flag to without-flag

--- src/test/ui/consts/extra-const-ub/detect-extra-ub.rs
+++ src/test/ui/consts/extra-const-ub/detect-extra-ub.rs
@@ -1,36 +1,38 @@
-// compile-flags: -Zextra-const-ub-checks
+// revisions: no_flag with_flag
+// [no_flag] check-pass
+// [with_flag] compile-flags: -Zextra-const-ub-checks
 #![feature(const_ptr_read)]
 
 use std::mem::transmute;
 
 const INVALID_BOOL: () = unsafe {
     let _x: bool = transmute(3u8);
-    //~^ ERROR: evaluation of constant value failed
-    //~| invalid value
+    //[with_flag]~^ ERROR: evaluation of constant value failed
+    //[with_flag]~| invalid value
 };
 
 const INVALID_PTR_IN_INT: () = unsafe {
     let _x: usize = transmute(&3u8);
-    //~^ ERROR: evaluation of constant value failed
-    //~| invalid value
+    //[with_flag]~^ ERROR: evaluation of constant value failed
+    //[with_flag]~| invalid value
 };
 
 const INVALID_SLICE_TO_USIZE_TRANSMUTE: () = unsafe {
     let x: &[u8] = &[0; 32];
     let _x: (usize, usize) = transmute(x);
-    //~^ ERROR: evaluation of constant value failed
-    //~| invalid value
+    //[with_flag]~^ ERROR: evaluation of constant value failed
+    //[with_flag]~| invalid value
 };
 
 const UNALIGNED_PTR: () = unsafe {
     let _x: &u32 = transmute(&[0u8; 4]);
-    //~^ ERROR: evaluation of constant value failed
-    //~| invalid value
+    //[with_flag]~^ ERROR: evaluation of constant value failed
+    //[with_flag]~| invalid value
 };
 
 const UNALIGNED_READ: () = {
-    INNER; //~ERROR any use of this value will cause an error
-    //~| previously accepted
+    INNER; //[with_flag]~ERROR any use of this value will cause an error
+    //[with_flag]~| previously accepted
     // There is an error here but its span is in the standard library so we cannot match it...
     // so we have this in a *nested* const, such that the *outer* const fails to use it.
     const INNER: () = unsafe {
