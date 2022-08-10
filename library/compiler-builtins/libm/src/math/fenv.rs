142261c91cba56c32c1554d67de8561d71d48631
Amanieu d'Antras <amanieu@gmail.com>
2022-08-10 01:11:33 +0100

    Fix fmaf not to depend on FE_TOWARDZERO
    Ported from upstream musl commit 4f3d346bffdf9ed2b1803653643dc31242490944
    
    Fixes rust-lang/libm#263

--- library/compiler-builtins/libm/src/math/fenv.rs
+++ library/compiler-builtins/libm/src/math/fenv.rs
@@ -5,7 +5,6 @@ pub(crate) const FE_UNDERFLOW: i32 = 0;
 pub(crate) const FE_INEXACT: i32 = 0;
 
 pub(crate) const FE_TONEAREST: i32 = 0;
-pub(crate) const FE_TOWARDZERO: i32 = 0;
 
 #[inline]
 pub(crate) fn feclearexcept(_mask: i32) -> i32 {
@@ -25,9 +24,4 @@ pub(crate) fn fetestexcept(_mask: i32) -> i32 {
 #[inline]
 pub(crate) fn fegetround() -> i32 {
     FE_TONEAREST
-}
-
-#[inline]
-pub(crate) fn fesetround(_r: i32) -> i32 {
-    0
 }
