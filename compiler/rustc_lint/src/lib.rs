eccdccf4ebd1b1389e409cccb3427b85bbe6166b
Urgau <urgau@numericable.fr>
2022-08-11 19:50:48 +0200

    Add warning against unexpected --cfg with --check-cfg

--- compiler/rustc_lint/src/lib.rs
+++ compiler/rustc_lint/src/lib.rs
@@ -144,6 +144,7 @@ macro_rules! early_lint_passes {
                 IncompleteFeatures: IncompleteFeatures,
                 RedundantSemicolons: RedundantSemicolons,
                 UnusedDocComment: UnusedDocComment,
+                UnexpectedCfgs: UnexpectedCfgs,
             ]
         );
     };
