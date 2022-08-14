f0d0573db1a791958938789b35309aab7323c284
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2022-08-14 19:11:31 +0300

    rustc_target: Do not specify some target options redundantly
    These values are already inherited

--- compiler/rustc_target/src/spec/android_base.rs
+++ compiler/rustc_target/src/spec/android_base.rs
@@ -4,7 +4,6 @@ pub fn opts() -> TargetOptions {
     let mut base = super::linux_base::opts();
     base.os = "android".into();
     base.default_dwarf_version = 2;
-    base.position_independent_executables = true;
     base.has_thread_local = false;
     // This is for backward compatibility, see https://github.com/rust-lang/rust/issues/49867
     // for context. (At that time, there was no `-C force-unwind-tables`, so the only solution
