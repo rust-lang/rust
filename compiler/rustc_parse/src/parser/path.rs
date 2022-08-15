e0f455e9a2432ed2bf4141345f7022e766c4fda0
Rageking8 <106309953+Rageking8@users.noreply.github.com>
2022-08-15 16:43:52 +0800

    Part 11

--- compiler/rustc_parse/src/parser/path.rs
+++ compiler/rustc_parse/src/parser/path.rs
@@ -609,7 +609,7 @@ impl<'a> Parser<'a> {
                     );
                     err.span_suggestion(
                         eq.to(before_next),
-                        &format!("remove the `=` if `{}` is a type", ident),
+                        &format!("remove the `=` if `{ident}` is a type"),
                         "",
                         Applicability::MaybeIncorrect,
                     )
