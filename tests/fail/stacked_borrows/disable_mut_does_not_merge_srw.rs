db43ee5714900900a52ad5050b77993288472ce2
Ralf Jung <post@ralfj.de>
2022-08-15 20:23:19 -0400

    defend test against overly smart Miri

--- tests/fail/stacked_borrows/disable_mut_does_not_merge_srw.rs
+++ tests/fail/stacked_borrows/disable_mut_does_not_merge_srw.rs
@@ -5,9 +5,14 @@ fn main() {
     unsafe {
         let mut mem = 0;
         let base = &mut mem as *mut i32; // the base pointer we build the rest of the stack on
-        let mutref = &mut *base;
-        let raw = mutref as *mut i32;
-        // in the stack, we now have [base, mutref, raw]
+        let raw = {
+            let mutref = &mut *base;
+            mutref as *mut i32
+        };
+        // In the stack, we now have [base, mutref, raw].
+        // We do this in a weird way where `mutref` is out of scope here, just in case
+        // Miri decides to get smart and argue that items for tags that are no longer
+        // used by any pointer stored anywhere in the machine can be removed.
         let _val = *base;
         // now mutref is disabled
         *base = 1;
