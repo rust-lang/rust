0307fa347466f58ccde75ffae314abf2cc19f803
yaraksan <arschwixxa123@gmail.com>
2022-08-18 20:05:17 +0200

    Add diagnostic lints to rustc_typeck

--- compiler/rustc_typeck/src/lib.rs
+++ compiler/rustc_typeck/src/lib.rs
@@ -55,6 +55,8 @@ This API is completely unstable and subject to change.
 
 */
 
+#![deny(rustc::untranslatable_diagnostic)]
+#![deny(rustc::diagnostic_outside_of_impl)]
 #![allow(rustc::potential_query_instability)]
 #![doc(html_root_url = "https://doc.rust-lang.org/nightly/nightly-rustc/")]
 #![feature(box_patterns)]
