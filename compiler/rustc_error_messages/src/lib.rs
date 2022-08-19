3becaafe89dba05e071d5a4e7e48717e06a98b3e
David Wood <david.wood@huawei.com>
2022-08-19 14:47:41 +0100

    incremental: migrate diagnostics
    Migrate the `rustc_incremental` crate's diagnostics to translatable
    diagnostic structs.
    
    Signed-off-by: David Wood <david.wood@huawei.com>

--- compiler/rustc_error_messages/src/lib.rs
+++ compiler/rustc_error_messages/src/lib.rs
@@ -37,6 +37,7 @@ fluent_messages! {
     builtin_macros => "../locales/en-US/builtin_macros.ftl",
     const_eval => "../locales/en-US/const_eval.ftl",
     expand => "../locales/en-US/expand.ftl",
+    incremental => "../locales/en-US/incremental.ftl",
     interface => "../locales/en-US/interface.ftl",
     lint => "../locales/en-US/lint.ftl",
     parser => "../locales/en-US/parser.ftl",
