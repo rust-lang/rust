ca753124088c0d57ca6728ee093363cbc1db5274
Maybe Waffle <waffle.lapkin@gmail.com>
2022-08-21 06:16:57 +0400

    fix `ptr_mask` codegen test wrt llvm opaque pointers

--- src/test/codegen/intrinsics/mask.rs
+++ src/test/codegen/intrinsics/mask.rs
@@ -6,6 +6,6 @@
 #[no_mangle]
 pub fn mask_ptr(ptr: *const u16, mask: usize) -> *const u16 {
     // CHECK: call
-    // CHECK-SAME: @llvm.ptrmask.p0i8.[[WORD]](
+    // CHECK-SAME: @llvm.ptrmask.{{p0|p0i8}}.[[WORD]]({{ptr|i8\*}} {{%ptr|%0}}, [[WORD]] %mask)
     core::intrinsics::ptr_mask(ptr, mask)
 }
