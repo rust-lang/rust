f20cc9ae4ea6e9b5226680e93229a53468022350
5225225 <5225225@mailbox.org>
2022-08-23 13:48:15 +0100

    Rename rustc_mir_dataflow diagnostic to mir_dataflow

--- compiler/rustc_mir_dataflow/src/errors.rs
+++ compiler/rustc_mir_dataflow/src/errors.rs
@@ -2,21 +2,21 @@ use rustc_macros::SessionDiagnostic;
 use rustc_span::{Span, Symbol};
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::path_must_end_in_filename)]
+#[diag(mir_dataflow::path_must_end_in_filename)]
 pub(crate) struct PathMustEndInFilename {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::unknown_formatter)]
+#[diag(mir_dataflow::unknown_formatter)]
 pub(crate) struct UnknownFormatter {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::duplicate_values_for)]
+#[diag(mir_dataflow::duplicate_values_for)]
 pub(crate) struct DuplicateValuesFor {
     #[primary_span]
     pub span: Span,
@@ -24,7 +24,7 @@ pub(crate) struct DuplicateValuesFor {
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::requires_an_argument)]
+#[diag(mir_dataflow::requires_an_argument)]
 pub(crate) struct RequiresAnArgument {
     #[primary_span]
     pub span: Span,
@@ -32,39 +32,39 @@ pub(crate) struct RequiresAnArgument {
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::stop_after_dataflow_ended_compilation)]
+#[diag(mir_dataflow::stop_after_dataflow_ended_compilation)]
 pub(crate) struct StopAfterDataFlowEndedCompilation;
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::peek_must_be_place_or_ref_place)]
+#[diag(mir_dataflow::peek_must_be_place_or_ref_place)]
 pub(crate) struct PeekMustBePlaceOrRefPlace {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::peek_must_be_not_temporary)]
+#[diag(mir_dataflow::peek_must_be_not_temporary)]
 pub(crate) struct PeekMustBeNotTemporary {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::peek_bit_not_set)]
+#[diag(mir_dataflow::peek_bit_not_set)]
 pub(crate) struct PeekBitNotSet {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::peek_argument_not_a_local)]
+#[diag(mir_dataflow::peek_argument_not_a_local)]
 pub(crate) struct PeekArgumentNotALocal {
     #[primary_span]
     pub span: Span,
 }
 
 #[derive(SessionDiagnostic)]
-#[diag(rustc_mir_dataflow::peek_argument_untracked)]
+#[diag(mir_dataflow::peek_argument_untracked)]
 pub(crate) struct PeekArgumentUntracked {
     #[primary_span]
     pub span: Span,
