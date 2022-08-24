7220f4a73847499d2684cff8363474420001bec3
Guillaume Gomez <guillaume.gomez@huawei.com>
2022-08-24 16:13:26 +0200

    Extend GUI test to check colors for all srclink elements

--- src/test/rustdoc-gui/src/staged_api/lib.rs
+++ src/test/rustdoc-gui/src/staged_api/lib.rs
@@ -7,4 +7,6 @@ pub struct Foo {}
 impl Foo {
     #[stable(feature = "some_feature", since = "1.3.5")]
     pub fn bar() {}
+    #[stable(feature = "some_other_feature", since = "1.3.6")]
+    pub fn yo() {}
 }
