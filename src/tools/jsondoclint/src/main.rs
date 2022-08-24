41d35a97f9f33e265de53b8f04abe307d7616641
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-08-24 23:18:53 +0100

    jsondocck: Find path to Id's not in index

--- src/tools/jsondoclint/src/main.rs
+++ src/tools/jsondoclint/src/main.rs
@@ -3,8 +3,10 @@ use std::env;
 use anyhow::{anyhow, bail, Result};
 use fs_err as fs;
 use rustdoc_json_types::{Crate, Id, FORMAT_VERSION};
+use serde_json::Value;
 
 pub(crate) mod item_kind;
+mod json_find;
 mod validator;
 
 #[derive(Debug)]
@@ -21,8 +23,10 @@ enum ErrorKind {
 
 fn main() -> Result<()> {
     let path = env::args().nth(1).ok_or_else(|| anyhow!("no path given"))?;
-    let contents = fs::read_to_string(path)?;
+    let contents = fs::read_to_string(&path)?;
     let krate: Crate = serde_json::from_str(&contents)?;
+    // TODO: Only load if nessessary.
+    let krate_json: Value = serde_json::from_str(&contents)?;
     assert_eq!(krate.format_version, FORMAT_VERSION);
 
     let mut validator = validator::Validator::new(&krate);
@@ -31,11 +35,29 @@ fn main() -> Result<()> {
     if !validator.errs.is_empty() {
         for err in validator.errs {
             match err.kind {
-                ErrorKind::NotFound => eprintln!("{}: Not Found", err.id.0),
+                ErrorKind::NotFound => {
+                    let sels =
+                        json_find::find_selector(&krate_json, &Value::String(err.id.0.clone()));
+                    match &sels[..] {
+                        [] => unreachable!(
+                            "id must be in crate, or it wouldn't be reported as not found"
+                        ),
+                        [sel] => eprintln!(
+                            "{} not in index or paths, but refered to at '{}'",
+                            err.id.0,
+                            json_find::to_jsonpath(&sel)
+                        ),
+                        [sel, ..] => eprintln!(
+                            "{} not in index or paths, but refered to at '{}' and more",
+                            err.id.0,
+                            json_find::to_jsonpath(&sel)
+                        ),
+                    }
+                }
                 ErrorKind::Custom(msg) => eprintln!("{}: {}", err.id.0, msg),
             }
         }
-        bail!("Errors validating json");
+        bail!("Errors validating json {path}");
     }
 
     Ok(())
