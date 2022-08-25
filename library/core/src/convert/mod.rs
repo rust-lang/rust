c4e921fb8ac0438e1c6c18666737966d9e5d20f7
Maurits van Riezen <12109031+mousetail@users.noreply.github.com>
2022-08-25 20:40:24 +0200

    Typo

--- library/core/src/convert/mod.rs
+++ library/core/src/convert/mod.rs
@@ -93,7 +93,7 @@ pub use num::FloatToInt;
 ///
 /// let iter = [Some(1), None, Some(3)].into_iter();
 /// let filtered = iter.filter_map(identity).collect::<Vec<_>>();
-/// // Equivalent, since Option impements IntoIterator:
+/// // Equivalent, since Option implements IntoIterator:
 /// // let filtered = iter.flatten().collect::<Vec<_>>();
 /// assert_eq!(vec![1, 3], filtered);
 /// ```
