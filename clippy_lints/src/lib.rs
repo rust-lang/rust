037f698147eeb97699c06a270e8774d2f7a6fbbc
Samuel Moelius <samuel.moelius@trailofbits.com>
2022-08-25 00:14:21 +0000

    `needless_borrow` uses `used_exactly_once`

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -38,7 +38,6 @@ extern crate rustc_infer;
 extern crate rustc_lexer;
 extern crate rustc_lint;
 extern crate rustc_middle;
-extern crate rustc_mir_dataflow;
 extern crate rustc_parse;
 extern crate rustc_session;
 extern crate rustc_span;
