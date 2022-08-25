575f6098da83cef33676ef67e39b73b2b910a042
SLASHLogin <loginmlgxd@gmail.com>
2022-08-25 15:34:30 +0200

    Port unknown feature diagnostic to the new framework

---
+++ compiler/rustc_codegen_llvm/src/errors.rs
@@ -0,0 +1,33 @@
+use rustc_errors::DiagnosticBuilder;
+use rustc_session::SessionDiagnostic;
+use rustc_errors::fluent;
+
+pub(crate) enum UnknownCTargetFeature {
+    UnknownFeaturePrefix { feature: String },
+    UnknownFeature { feature: String, rust_feature: Option<String> },
+}
+
+impl SessionDiagnostic<'_, ()> for UnknownCTargetFeature {
+    fn into_diagnostic(self, sess: &'_ rustc_session::parse::ParseSess) -> DiagnosticBuilder<'_, ()> {
+        match self {
+            UnknownCTargetFeature::UnknownFeaturePrefix { feature } => {
+                let mut diag = sess.struct_warn(fluent::codegen_llvm::unknown_ctarget_feature);
+                diag.set_arg("feature", feature);
+                diag.note(fluent::codegen_llvm::unknown_feature_prefix);
+                diag
+            }
+            UnknownCTargetFeature::UnknownFeature { feature, rust_feature } => {
+                let mut diag = sess.struct_warn(fluent::codegen_llvm::unknown_ctarget_feature);
+                diag.set_arg("feature", feature);
+                diag.note(fluent::codegen_llvm::unknown_feature);
+                if let Some(rust_feature) = rust_feature {
+                    diag.help(fluent::codegen_llvm::rust_feature);
+                    diag.set_arg("rust_feature", rust_feature);
+                } else {
+                    diag.note(fluent::codegen_llvm::unknown_feature_fill_request);
+                }
+                diag
+            }
+        }
+    }
+}
