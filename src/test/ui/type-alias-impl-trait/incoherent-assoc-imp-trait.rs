84e8ac29a6088aaf56e2f513fcf762672247b769
Katherine Philip <kurerunoshikaku@yahoo.com>
2022-08-24 19:14:18 -0700

    Rebase & bless more tests

--- src/test/ui/type-alias-impl-trait/incoherent-assoc-imp-trait.rs
+++ src/test/ui/type-alias-impl-trait/incoherent-assoc-imp-trait.rs
@@ -8,7 +8,9 @@ trait MyTrait {}
 impl MyTrait for () {}
 
 impl<F> FnOnce<()> for &F {
-    //~^ ERROR type parameter `F` must be used
+    //~^ ERROR type parameter `F` as argument to a fundamental type
+    // | must be used as the type parameter for some local type
+    // | (e.g., `MyStruct<F>`)
     type Output = impl MyTrait;
     extern "rust-call" fn call_once(self, _: ()) -> Self::Output {}
 }
