2741dd81adc8b69cb564d1502cf92948d3be3431
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-08-26 14:20:38 -0400

    ignore std/test/codegen/mem-replace-direct-memcpy.rs in the 1.64-beta.

--- src/test/codegen/mem-replace-direct-memcpy.rs
+++ src/test/codegen/mem-replace-direct-memcpy.rs
@@ -1,3 +1,15 @@
+// ignore-test
+
+// WHY IS THIS TEST BEING IGNORED:
+//
+// This test depends on characteristics of how the stdlib was compiled,
+// namely that sufficient inlining occurred to ensure that the call to
+// `std::mem::replace` boils down to just two calls of `llvm.memcpy`.
+//
+// But the MIR inlining policy is in flux as of 1.64-beta, and the intermittent
+// breakage of this test that results is causing problems for people trying to
+// do development.
+
 // This test ensures that `mem::replace::<T>` only ever calls `@llvm.memcpy`
 // with `size_of::<T>()` as the size, and never goes through any wrapper that
 // may e.g. multiply `size_of::<T>()` with a variable "count" (which is only
