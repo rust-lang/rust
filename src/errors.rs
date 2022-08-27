7277046d84d53a9b31abb4ef8d5b448afa4725f1
Ellis Hoag <ellis.sparky.hoag@gmail.com>
2022-08-26 20:23:50 -0700

    Add LinkageConstOrMutType

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,15 @@
 use rustc_macros::SessionDiagnostic;
+use rustc_span::Span;
 
 #[derive(SessionDiagnostic)]
 #[diag(codegen_gcc::ranlib_failure)]
 pub(crate) struct RanlibFailure {
     pub exit_code: Option<i32>
+}
+
+#[derive(SessionDiagnostic)]
+#[diag(codegen_gcc::linkage_const_or_mut_type)]
+pub(crate) struct LinkageConstOrMutType {
+    #[primary_span]
+    pub span: Span
 }
