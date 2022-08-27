4caedbae869855a19ca49455e2cd338ae3b8c1ef
est31 <MTest31@outlook.com>
2022-08-27 17:35:46 +0200

    Adjust label break value stabilization version to CURRENT_RUSTC_VERSION

--- compiler/rustc_feature/src/accepted.rs
+++ compiler/rustc_feature/src/accepted.rs
@@ -187,7 +187,7 @@ declare_features! (
     /// especially around globs and shadowing (RFC 1560).
     (accepted, item_like_imports, "1.15.0", Some(35120), None),
     /// Allows `'a: { break 'a; }`.
-    (accepted, label_break_value, "1.65.0", Some(48594), None),
+    (accepted, label_break_value, "CURRENT_RUSTC_VERSION", Some(48594), None),
     /// Allows `if/while p && let q = r && ...` chains.
     (accepted, let_chains, "1.64.0", Some(53667), None),
     /// Allows `break {expr}` with a value inside `loop`s.
