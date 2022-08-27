d39b683053a8c8fb654331cceb783674182c4252
Ralf Jung <post@ralfj.de>
2022-08-27 16:08:05 -0400

    dont rerun build script unnecessarily

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    // Don't rebuild miri when nothing changed.
+    println!("cargo:rerun-if-changed=build.rs");
     // Re-export the TARGET environment variable so it can
     // be accessed by miri.
     let target = std::env::var("TARGET").unwrap();
