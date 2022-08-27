445841cda32cf9fb95528cfe8d126d0a0e0cb608
Camille GILLOT <gillot.camille@gmail.com>
2022-08-27 12:21:02 +0200

    Compute explicit MIR params on THIR.

--- compiler/rustc_mir_build/src/build/matches/mod.rs
+++ compiler/rustc_mir_build/src/build/matches/mod.rs
@@ -568,7 +568,7 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
 
             _ => {
                 let place_builder = unpack!(block = self.as_place_builder(block, initializer));
-                self.place_into_pattern(block, irrefutable_pat, place_builder, true)
+                self.place_into_pattern(block, &irrefutable_pat, place_builder, true)
             }
         }
     }
