bc6483d11e692f01a4c9845212559017ab23ef4b
Deadbeef <ent3rm4n@gmail.com>
2022-08-28 04:03:11 +0000

    Prevent errors for stage0 rustc build

--- library/core/src/default.rs
+++ library/core/src/default.rs
@@ -99,7 +99,7 @@
 /// ```
 #[cfg_attr(not(test), rustc_diagnostic_item = "Default")]
 #[stable(feature = "rust1", since = "1.0.0")]
-#[const_trait]
+#[cfg_attr(not(bootstrap), const_trait)]
 pub trait Default: Sized {
     /// Returns the "default value" for a type.
     ///
