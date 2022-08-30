eaa00250ba29f5833f1195891eff10fff8099d4b
Alex Saveau <saveau.alexandre@gmail.com>
2022-08-29 23:09:25 -0400

    Add another MaybeUninit array test with const
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/test/codegen/issue-96274.rs
+++ src/test/codegen/issue-96274.rs
@@ -2,10 +2,16 @@
 // compile-flags: -O
 
 #![crate_type = "lib"]
+#![feature(inline_const)]
 
 use std::mem::MaybeUninit;
 
 pub fn maybe_uninit() -> [MaybeUninit<u8>; 3000] {
     // CHECK-NOT: memset
     [MaybeUninit::uninit(); 3000]
+}
+
+pub fn maybe_uninit_const<T>() -> [MaybeUninit<T>; 8192] {
+    // CHECK-NOT: memset
+    [const { MaybeUninit::uninit() }; 8192]
 }
