79d4f09942a2fc6f96f56f7ae85196c4aa7df56c
JeanCASPAR <55629512+JeanCASPAR@users.noreply.github.com>
2022-08-30 14:26:09 +0200

    Change fatal diagnostic to an error.
    Co-authored-by: Esteban Kuber <estebank@users.noreply.github.com>

--- compiler/rustc_ast_lowering/src/expr.rs
+++ compiler/rustc_ast_lowering/src/expr.rs
@@ -1264,7 +1264,13 @@ impl<'hir> LoweringContext<'_, 'hir> {
             (Some(..), Some(..), HalfOpen) => hir::LangItem::Range,
             (None, Some(..), Closed) => hir::LangItem::RangeToInclusive,
             (Some(..), Some(..), Closed) => unreachable!(),
-            (_, None, Closed) => self.tcx.sess.emit_fatal(InclusiveRangeWithNoEnd { span }),
+            (start, None, Closed) => {
+                self.tcx.sess.emit_err(InclusiveRangeWithNoEnd { span });
+                match start {
+                    Some(..) => hir::LangItem::RangeFrom,
+                    None => hir::LangItem::RangeFull,
+                }
+            }
         };
 
         let fields = self.arena.alloc_from_iter(
