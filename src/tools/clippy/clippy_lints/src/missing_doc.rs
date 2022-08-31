ed2a32f22cfb7e43a3d8c732ac8fa8007c061c5f
Michael Goulet <michael@errs.io>
2022-08-31 05:29:36 +0000

    Make clippy happy

--- src/tools/clippy/clippy_lints/src/missing_doc.rs
+++ src/tools/clippy/clippy_lints/src/missing_doc.rs
@@ -147,7 +147,8 @@ impl<'tcx> LateLintPass<'tcx> for MissingDoc {
             | hir::ItemKind::TraitAlias(..)
             | hir::ItemKind::TyAlias(..)
             | hir::ItemKind::Union(..)
-            | hir::ItemKind::OpaqueTy(..) => {},
+            | hir::ItemKind::OpaqueTy(..)
+            | hir::ItemKind::ImplTraitPlaceholder(..) => {},
             hir::ItemKind::ExternCrate(..)
             | hir::ItemKind::ForeignMod { .. }
             | hir::ItemKind::GlobalAsm(..)
