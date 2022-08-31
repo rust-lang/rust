1135ad31ab69d1d0e44e57750149447301b891c4
Ralf Jung <post@ralfj.de>
2022-08-31 16:05:40 +0200

    make shim_arg_size ptr-width-independent

--- tests/fail/shims/shim_arg_size.rs
+++ tests/fail/shims/shim_arg_size.rs
@@ -1,17 +1,10 @@
-//@stderr-per-bitwidth
-
 fn main() {
     extern "C" {
-        // Use the wrong type(ie. not the pointer width) for the `size`
-        // argument.
-        #[cfg(target_pointer_width = "64")]
-        fn malloc(size: u32) -> *mut std::ffi::c_void;
-
-        #[cfg(target_pointer_width = "32")]
-        fn malloc(size: u16) -> *mut std::ffi::c_void;
+        // Use the wrong type (ie. not `i32`) for the `c` argument.
+        fn memchr(s: *const std::ffi::c_void, c: u8, n: usize) -> *mut std::ffi::c_void;
     }
 
     unsafe {
-        let _p1 = malloc(42); //~ ERROR: Undefined Behavior: scalar size mismatch
+        memchr(std::ptr::null(), 0, 0); //~ ERROR: Undefined Behavior: scalar size mismatch
     };
 }
