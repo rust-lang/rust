4c75b32e9965acae7e9f7efa20fc505b1ca7a2a0
lcnr <rust@lcnr.de>
2022-09-08 15:56:32 +0200

    merge tests

--- src/test/ui/parser/issues/issue-14303-enum.rs
+++
@@ -1,6 +0,0 @@
-enum X<'a, T, 'b> {
-//~^ ERROR lifetime parameters must be declared prior to type or const parameters
-    A(&'a &'b T)
-}
-
-fn main() {}
--- src/test/ui/parser/issues/issue-14303-fn-def.rs
+++
@@ -1,4 +0,0 @@
-fn foo<'a, T, 'b>(x: &'a T) {}
-//~^ ERROR lifetime parameters must be declared prior to type or const parameters
-
-fn main() {}
