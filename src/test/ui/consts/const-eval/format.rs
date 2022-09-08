abb8906f15913b4ebabbb47b228417a9cdd23347
Mara Bos <m-ou.se@m-ou.se>
2022-09-08 12:30:07 +0200

    Update tests.

--- src/test/ui/consts/const-eval/format.rs
+++ src/test/ui/consts/const-eval/format.rs
@@ -1,21 +1,17 @@
 const fn failure() {
     panic!("{:?}", 0);
-    //~^ ERROR cannot call non-const formatting macro in constant functions
-    //~| ERROR erroneous constant used
-    //~| ERROR erroneous constant used
-    //~| WARN this was previously accepted by the compiler
-    //~| WARN this was previously accepted by the compiler
+    //~^ ERROR
+    //~| ERROR
+    //~| ERROR
+    //~| ERROR
 }
 
 const fn print() {
     println!("{:?}", 0);
-    //~^ ERROR cannot call non-const formatting macro in constant functions
-    //~| ERROR `Arguments::<'a>::new_v1` is not yet stable as a const fn
-    //~| ERROR cannot call non-const fn `_print` in constant functions
-    //~| ERROR erroneous constant used
-    //~| ERROR erroneous constant used
-    //~| WARN this was previously accepted by the compiler
-    //~| WARN this was previously accepted by the compiler
+    //~^ ERROR
+    //~| ERROR
+    //~| ERROR
+    //~| WARN
 }
 
 fn main() {}
