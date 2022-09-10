cdd0b56071cbc9247461bbd2e0551b46e653162f
Wim Looman <git@nemo157.com>
2022-09-10 13:31:08 +0200

    Test that both table headers and values are not wrapped

--- tests/target/issue-4210.rs
+++ tests/target/issue-4210.rs
@@ -7,9 +7,9 @@
 /// | val   | x                                                                           |
 pub struct Item;
 
-/// Table that is > 80 symbols:
+/// Table value that is > 80 symbols:
 ///
-/// | table | xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-/// |-------|-----------------------------------------------------------------------------
-/// | val   | x                                                                          
-pub struct Item;
+/// | table    | heading
+/// |----------|-----------------------------------------------------------------------------
+/// | long val | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+pub struct Item2;
