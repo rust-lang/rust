6a00c5cd5176ac6461623c9c958f2574fe2a745d
Chris Denton <christophersdenton@gmail.com>
2022-09-10 09:22:22 +0100

    Use `raw-dylib` in the std

--- library/std/src/sys/windows/c.rs
+++ library/std/src/sys/windows/c.rs
@@ -286,6 +286,7 @@ pub const STATUS_INVALID_PARAMETER: NTSTATUS = 0xc000000d_u32 as _;
 
 pub const STATUS_PENDING: NTSTATUS = 0x103 as _;
 pub const STATUS_END_OF_FILE: NTSTATUS = 0xC0000011_u32 as _;
+#[cfg(target_arch = "x86")]
 pub const STATUS_NOT_IMPLEMENTED: NTSTATUS = 0xC0000002_u32 as _;
 
 // Equivalent to the `NT_SUCCESS` C preprocessor macro.
@@ -843,7 +844,8 @@ if #[cfg(not(target_vendor = "uwp"))] {
         ) -> BOOL;
     }
 
-    #[link(name = "userenv")]
+    #[cfg_attr(not(target_arch = "x86"), link(name = "userenv", kind = "raw-dylib"))]
+    #[cfg_attr(target_arch = "x86", link(name = "userenv"))]
     extern "system" {
         // Allowed but unused by UWP
         pub fn GetUserProfileDirectoryW(
@@ -1158,7 +1160,8 @@ extern "system" {
     pub fn GetFileAttributesW(lpFileName: LPCWSTR) -> DWORD;
 }
 
-#[link(name = "ws2_32")]
+#[cfg_attr(not(target_arch = "x86"), link(name = "ws2_32", kind = "raw-dylib"))]
+#[cfg_attr(target_arch = "x86", link(name = "ws2_32"))]
 extern "system" {
     pub fn WSAStartup(wVersionRequested: WORD, lpWSAData: LPWSADATA) -> c_int;
     pub fn WSACleanup() -> c_int;
@@ -1251,7 +1254,8 @@ extern "system" {
     ) -> c_int;
 }
 
-#[link(name = "bcrypt")]
+#[cfg_attr(not(target_arch = "x86"), link(name = "bcrypt", kind = "raw-dylib"))]
+#[cfg_attr(target_arch = "x86", link(name = "bcrypt"))]
 extern "system" {
     // >= Vista / Server 2008
     // https://docs.microsoft.com/en-us/windows/win32/api/bcrypt/nf-bcrypt-bcryptgenrandom
@@ -1268,6 +1272,47 @@ extern "system" {
         dwflags: ULONG,
     ) -> NTSTATUS;
     pub fn BCryptCloseAlgorithmProvider(hAlgorithm: BCRYPT_ALG_HANDLE, dwFlags: ULONG) -> NTSTATUS;
+}
+
+#[cfg(not(target_arch = "x86"))]
+#[link(name = "ntdll", kind = "raw-dylib")]
+extern "system" {
+    pub fn NtCreateFile(
+        FileHandle: *mut HANDLE,
+        DesiredAccess: ACCESS_MASK,
+        ObjectAttributes: *const OBJECT_ATTRIBUTES,
+        IoStatusBlock: *mut IO_STATUS_BLOCK,
+        AllocationSize: *mut i64,
+        FileAttributes: ULONG,
+        ShareAccess: ULONG,
+        CreateDisposition: ULONG,
+        CreateOptions: ULONG,
+        EaBuffer: *mut c_void,
+        EaLength: ULONG,
+    ) -> NTSTATUS;
+    pub fn NtReadFile(
+        FileHandle: BorrowedHandle<'_>,
+        Event: HANDLE,
+        ApcRoutine: Option<IO_APC_ROUTINE>,
+        ApcContext: *mut c_void,
+        IoStatusBlock: &mut IO_STATUS_BLOCK,
+        Buffer: *mut crate::mem::MaybeUninit<u8>,
+        Length: ULONG,
+        ByteOffset: Option<&LARGE_INTEGER>,
+        Key: Option<&ULONG>,
+    ) -> NTSTATUS;
+    pub fn NtWriteFile(
+        FileHandle: BorrowedHandle<'_>,
+        Event: HANDLE,
+        ApcRoutine: Option<IO_APC_ROUTINE>,
+        ApcContext: *mut c_void,
+        IoStatusBlock: &mut IO_STATUS_BLOCK,
+        Buffer: *const u8,
+        Length: ULONG,
+        ByteOffset: Option<&LARGE_INTEGER>,
+        Key: Option<&ULONG>,
+    ) -> NTSTATUS;
+    pub fn RtlNtStatusToDosError(Status: NTSTATUS) -> ULONG;
 }
 
 // Functions that aren't available on every version of Windows that we support,
@@ -1310,6 +1355,7 @@ compat_fn_optional! {
 compat_fn_with_fallback! {
     pub static NTDLL: &CStr = ansi_str!("ntdll");
 
+    #[cfg(target_arch = "x86")]
     pub fn NtCreateFile(
         FileHandle: *mut HANDLE,
         DesiredAccess: ACCESS_MASK,
@@ -1325,6 +1371,7 @@ compat_fn_with_fallback! {
     ) -> NTSTATUS {
         STATUS_NOT_IMPLEMENTED
     }
+    #[cfg(target_arch = "x86")]
     pub fn NtReadFile(
         FileHandle: BorrowedHandle<'_>,
         Event: HANDLE,
@@ -1338,6 +1385,7 @@ compat_fn_with_fallback! {
     ) -> NTSTATUS {
         STATUS_NOT_IMPLEMENTED
     }
+    #[cfg(target_arch = "x86")]
     pub fn NtWriteFile(
         FileHandle: BorrowedHandle<'_>,
         Event: HANDLE,
@@ -1351,6 +1399,7 @@ compat_fn_with_fallback! {
     ) -> NTSTATUS {
         STATUS_NOT_IMPLEMENTED
     }
+    #[cfg(target_arch = "x86")]
     pub fn RtlNtStatusToDosError(
         Status: NTSTATUS
     ) -> ULONG {
