3673e34be916effcd3155a4256cee434fe6c92d6
est31 <MTest31@outlook.com>
2022-09-12 17:09:08 +0200

    Stabilize map_first_last

--- compiler/rustc_middle/src/lib.rs
+++ compiler/rustc_middle/src/lib.rs
@@ -33,7 +33,6 @@
 #![feature(get_mut_unchecked)]
 #![feature(generic_associated_types)]
 #![feature(if_let_guard)]
-#![feature(map_first_last)]
 #![feature(negative_impls)]
 #![feature(never_type)]
 #![feature(extern_types)]
