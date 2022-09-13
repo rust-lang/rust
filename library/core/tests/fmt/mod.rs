e45a26c916a305dc8f31a8465a17a22841099721
Mara Bos <m-ou.se@m-ou.se>
2022-09-14 01:24:33 +0200

    Update fmt estimated capacity tests.

--- library/core/tests/fmt/mod.rs
+++ library/core/tests/fmt/mod.rs
@@ -24,9 +24,11 @@ fn test_estimated_capacity() {
     assert_eq!(format_args!("").estimated_capacity(), 0);
     assert_eq!(format_args!("{}", "").estimated_capacity(), 0);
     assert_eq!(format_args!("Hello").estimated_capacity(), 5);
-    assert_eq!(format_args!("Hello, {}!", "").estimated_capacity(), 16);
+    #[cfg(not(bootstrap))]
+    assert_eq!(format_args!("Hello, {}!", "").estimated_capacity(), 0);
     assert_eq!(format_args!("{}, hello!", "World").estimated_capacity(), 0);
-    assert_eq!(format_args!("{}. 16-bytes piece", "World").estimated_capacity(), 32);
+    #[cfg(not(bootstrap))]
+    assert_eq!(format_args!("{}. 16-bytes piece", "World").estimated_capacity(), 0);
 }
 
 #[test]
