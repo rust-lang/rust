3a38d566bd222981b3292ba9d6870340a6d26958
est31 <MTest31@outlook.com>
2022-09-13 19:46:08 +0200

    Also replace the placeholder for the stable_features lint

--- compiler/rustc_passes/src/lib_features.rs
+++ compiler/rustc_passes/src/lib_features.rs
@@ -5,7 +5,7 @@
 //! collect them instead.
 
 use rustc_ast::{Attribute, MetaItemKind};
-use rustc_attr::VERSION_PLACEHOLDER;
+use rustc_attr::{rust_version_symbol, VERSION_PLACEHOLDER};
 use rustc_errors::struct_span_err;
 use rustc_hir::intravisit::Visitor;
 use rustc_middle::hir::nested_filter;
@@ -57,9 +57,7 @@ impl<'tcx> LibFeatureCollector<'tcx> {
                 }
 
                 if let Some(s) = since && s.as_str() == VERSION_PLACEHOLDER {
-                    let version = option_env!("CFG_VERSION").unwrap_or("<current>");
-                    let version = version.split(' ').next().unwrap();
-                    since = Some(Symbol::intern(&version));
+                    since = Some(rust_version_symbol());
                 }
 
                 if let Some(feature) = feature {
