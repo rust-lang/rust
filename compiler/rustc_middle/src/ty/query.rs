dddfb7db24e66891fec9f2abf2ac1477985f36ad
hanar3 <vh.morenodasilva@gmail.com>
2022-09-14 12:19:42 -0300

    Improve error message for unsupported crate

--- compiler/rustc_middle/src/ty/query.rs
+++ compiler/rustc_middle/src/ty/query.rs
@@ -275,10 +275,11 @@ macro_rules! define_callbacks {
             fn default() -> Self {
                 Providers {
                     $($name: |_, key| bug!(
-                        "`tcx.{}({:?})` unsupported by its crate; \
-                         perhaps the `{}` query was never assigned a provider function",
+                        "`tcx.{}({:?})` unsupported for {} crate; \
+                         perhaps the `{}` query was never assigned a provider function. Queries can be either made to the local crate, or the external crate. This error means you tried to use it for one that's not supported.", 
                         stringify!($name),
                         key,
+                        if key.query_crate_is_local() { "local" } : { "external" } ,
                         stringify!($name),
                     ),)*
                 }
