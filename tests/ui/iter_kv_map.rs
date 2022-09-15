5004f04eccabe954e489b4ca85ab0692a93ff3f0
Kartavya Vashishtha <sendtokartavya@gmail.com>
2022-09-15 09:46:01 +0530

    added identity block test
    added binding annotations for all lines

--- tests/ui/iter_kv_map.rs
+++ tests/ui/iter_kv_map.rs
@@ -3,6 +3,7 @@
 #![warn(clippy::iter_kv_map)]
 #![allow(clippy::redundant_clone)]
 #![allow(clippy::suspicious_map)]
+#![allow(clippy::map_identity)]
 
 use std::collections::{BTreeMap, HashMap};
 
@@ -23,18 +24,18 @@ fn main() {
     let _ = map.clone().into_iter().map(|(_, val)| val + 2).collect::<Vec<_>>();
 
     let _ = map.clone().iter().map(|(_, val)| val).collect::<Vec<_>>();
-    map.iter().map(|(key, _)| key).filter(|x| *x % 2 == 0).count();
+    let _ = map.iter().map(|(key, _)| key).filter(|x| *x % 2 == 0).count();
 
     // Don't lint
-    map.iter().filter(|(_, val)| *val % 2 == 0).map(|(key, _)| key).count();
-    map.iter().map(get_key).collect::<Vec<_>>();
+    let _ = map.iter().filter(|(_, val)| *val % 2 == 0).map(|(key, _)| key).count();
+    let _ = map.iter().map(get_key).collect::<Vec<_>>();
 
     // Linting the following could be an improvement to the lint
     // map.iter().filter_map(|(_, val)| (val % 2 == 0).then(val * 17)).count();
 
     // Lint
-    map.iter().map(|(key, _value)| key * 9).count();
-    map.iter().map(|(_key, value)| value * 17).count();
+    let _ = map.iter().map(|(key, _value)| key * 9).count();
+    let _ = map.iter().map(|(_key, value)| value * 17).count();
 
     let map: BTreeMap<u32, u32> = BTreeMap::new();
 
@@ -50,16 +51,16 @@ fn main() {
     let _ = map.clone().into_iter().map(|(_, val)| val + 2).collect::<Vec<_>>();
 
     let _ = map.clone().iter().map(|(_, val)| val).collect::<Vec<_>>();
-    map.iter().map(|(key, _)| key).filter(|x| *x % 2 == 0).count();
+    let _ = map.iter().map(|(key, _)| key).filter(|x| *x % 2 == 0).count();
 
     // Don't lint
-    map.iter().filter(|(_, val)| *val % 2 == 0).map(|(key, _)| key).count();
-    map.iter().map(get_key).collect::<Vec<_>>();
+    let _ = map.iter().filter(|(_, val)| *val % 2 == 0).map(|(key, _)| key).count();
+    let _ = map.iter().map(get_key).collect::<Vec<_>>();
 
     // Linting the following could be an improvement to the lint
     // map.iter().filter_map(|(_, val)| (val % 2 == 0).then(val * 17)).count();
 
     // Lint
-    map.iter().map(|(key, _value)| key * 9).count();
-    map.iter().map(|(_key, value)| value * 17).count();
+    let _ = map.iter().map(|(key, _value)| key * 9).count();
+    let _ = map.iter().map(|(_key, value)| value * 17).count();
 }
