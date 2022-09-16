48c1c1d19043120e9e89b3186d5d5286142f3066
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2022-09-16 19:02:45 +0800

    avoid duplicating StorageLive in let-else

--- compiler/rustc_mir_build/src/build/block.rs
+++ compiler/rustc_mir_build/src/build/block.rs
@@ -232,7 +232,8 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                         pattern,
                         UserTypeProjections::none(),
                         &mut |this, _, _, _, node, span, _, _| {
-                            this.storage_live_binding(block, node, span, OutsideGuard, false);
+                            this.storage_live_binding(block, node, span, OutsideGuard, true);
+                            this.schedule_drop_for_binding(node, span, OutsideGuard);
                         },
                     );
                     let failure = unpack!(
