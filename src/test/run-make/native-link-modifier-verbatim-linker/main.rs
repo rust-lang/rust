f55797ca8661cb164a19a9516a7fe694565e246e
Daniil Belov <70999565+BelovDV@users.noreply.github.com>
2022-09-19 16:23:45 +0300

    fix

--- src/test/run-make/native-link-modifier-verbatim-linker/main.rs
+++ src/test/run-make/native-link-modifier-verbatim-linker/main.rs
@@ -1,5 +1,3 @@
-extern crate rust_dep;
-
 extern "C" {
     fn local_native_f() -> i32;
 }
@@ -8,5 +6,4 @@ pub fn main() {
     unsafe {
         assert!(local_native_f() == 0);
     };
-    rust_dep::rust_dep()
 }
