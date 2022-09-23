e9d219e97c9b87e928d131ac071fef96e906c464
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-09-23 07:15:55 +0000

    Make the test actually show the problematic case

--- src/test/ui/type-alias-impl-trait/implied_lifetime_wf_check.rs
+++ src/test/ui/type-alias-impl-trait/implied_lifetime_wf_check.rs
@@ -1,18 +1,27 @@
-trait Mirror<'a> {
-    type Item;
+#![feature(type_alias_impl_trait)]
+
+// known-bug: #99840
+// this should not compile
+// check-pass
+
+type Alias = impl Sized;
+
+fn constrain() -> Alias {
+    1i32
 }
 
-impl<'a, T> Mirror<'a> for T {
-    type Item = T;
+trait HideIt {
+    type Assoc;
 }
 
-trait AnotherTrait {
-    type Blah;
+impl HideIt for () {
+    type Assoc = Alias;
 }
 
-impl<'a> AnotherTrait for <u32 as Mirror<'a>>::Item {
-    //~^ ERROR: the lifetime parameter `'a` is not constrained
-    type Blah = &'a u32;
-}
+pub trait Yay {}
+
+impl Yay for <() as HideIt>::Assoc {}
+// impl Yay for i32 {} // this already errors
+// impl Yay for u32 {} // this also already errors
 
 fn main() {}
