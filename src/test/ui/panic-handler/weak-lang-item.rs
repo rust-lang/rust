0be3cc82345b22c898fb46a93e69fa7b1f6c710f
Florian Bartels <Florian.Bartels@elektrobit.com>
2022-09-23 09:41:12 +0200

    ignore test cases when checking emscripten

--- src/test/ui/panic-handler/weak-lang-item.rs
+++ src/test/ui/panic-handler/weak-lang-item.rs
@@ -2,6 +2,7 @@
 // error-pattern: `#[panic_handler]` function required, but not found
 // error-pattern: language item required, but not found: `eh_personality`
 // needs-unwind since it affects the error output
+// ignore-emscripten missing eh_catch_typeinfo lang item
 
 #![no_std]
 
