90ec6f847fe54e751a045044dabb7e0d252981e3
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-09-23 07:12:59 +0000

    Show errors instead of hiding them due to an earlier error

--- src/test/ui/type-alias-impl-trait/implied_lifetime_wf_check3.rs
+++ src/test/ui/type-alias-impl-trait/implied_lifetime_wf_check3.rs
@@ -6,6 +6,7 @@ mod test_lifetime_param {
     fn assert_static<'a: 'static>() {}
     //~^ WARN: unnecessary lifetime parameter `'a`
     fn test<'a>() where Ty<'a>: 'static { assert_static::<'a>() }
+    //~^ ERROR: lifetime may not live long enough
 }
 
 mod test_higher_kinded_lifetime_param {
@@ -14,14 +15,14 @@ mod test_higher_kinded_lifetime_param {
     fn assert_static<'a: 'static>() {}
     //~^ WARN: unnecessary lifetime parameter `'a`
     fn test<'a>() where for<'b> Ty<'b>: 'a { assert_static::<'a>() }
+    //~^ ERROR: lifetime may not live long enough
 }
 
 mod test_higher_kinded_lifetime_param2 {
     fn assert_static<'a: 'static>() {}
     //~^ WARN: unnecessary lifetime parameter `'a`
     fn test<'a>() { assert_static::<'a>() }
-    // no error because all the other errors happen first and then we abort before
-    // emitting an error here.
+    //~^ ERROR: lifetime may not live long enough
 }
 
 mod test_type_param {
@@ -29,14 +30,14 @@ mod test_type_param {
     fn defining<A>(s: A) -> Ty<A> { s }
     fn assert_static<A: 'static>() {}
     fn test<A>() where Ty<A>: 'static { assert_static::<A>() }
+    //~^ ERROR: parameter type `A` may not live long enough
 }
 
-mod test_type_param_static {
-    type Ty<A> = impl Sized + 'static;
-    //~^ ERROR: the parameter type `A` may not live long enough
-    fn defining<A: 'static>(s: A) -> Ty<A> { s }
-    fn assert_static<A: 'static>() {}
-    fn test<A>() where Ty<A>: 'static { assert_static::<A>() }
+mod test_implied_from_fn_sig {
+    type Opaque<T: 'static> = impl Sized;
+    fn defining<T: 'static>() -> Opaque<T> {}
+    fn assert_static<T: 'static>() {}
+    fn test<T>(_: Opaque<T>) { assert_static::<T>(); }
 }
 
 fn main() {}
