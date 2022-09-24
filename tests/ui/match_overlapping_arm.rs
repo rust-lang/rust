5f6e1d397a940447af5e4279718668db473c683c
Urgau <urgau@numericable.fr>
2022-09-24 17:22:04 +0200

    Stabilize half_open_range_patterns

--- tests/ui/match_overlapping_arm.rs
+++ tests/ui/match_overlapping_arm.rs
@@ -1,5 +1,5 @@
 #![feature(exclusive_range_pattern)]
-#![feature(half_open_range_patterns)]
+
 #![warn(clippy::match_overlapping_arm)]
 #![allow(clippy::redundant_pattern_matching)]
 #![allow(clippy::if_same_then_else, clippy::equatable_if_let)]
