1ec3b506cc6e53c3f12198063f76c1047f78a4e1
Gimgim <93856041+gimbles@users.noreply.github.com>
2022-09-25 23:01:24 +0530

    Update option.rs

--- library/core/src/option.rs
+++ library/core/src/option.rs
@@ -840,14 +840,11 @@ impl<T> Option<T> {
     /// [`None`] on error.
     ///
     /// ```
-    /// let good_year_from_input = "1909";
-    /// let bad_year_from_input = "190blarg";
-    /// // Result::ok() converts a Result<T> to an Option<T>
-    /// let good_year = good_year_from_input.parse().ok().unwrap_or_default();
-    /// let bad_year = bad_year_from_input.parse().ok().unwrap_or_default();
+    /// let x: Option<u32> = None;
+    /// let y: Option<u32> = Some(42);
     ///
-    /// assert_eq!(1909, good_year);
-    /// assert_eq!(0, bad_year);
+    /// assert_eq!(x.unwrap_or_default(), 0);
+    /// assert_eq!(y.unwrap_or_default(), 42);
     /// ```
     ///
     /// [default value]: Default::default
