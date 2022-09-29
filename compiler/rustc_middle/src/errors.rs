03480e903cbc39514afc3ad4fe42bbe2a81d81d3
pierwill <pierwill@users.noreply.github.com>
2022-09-29 11:18:45 -0500

    Add err code and slug

--- compiler/rustc_middle/src/errors.rs
+++ compiler/rustc_middle/src/errors.rs
@@ -4,6 +4,7 @@ use rustc_session::{lint::Level, parse::ParseSess, SessionDiagnostic};
 use rustc_span::{Span, Symbol};
 
 #[derive(SessionDiagnostic)]
+#[error(code = "E0320", slug = "overflow-while-adding-drop-check-rules")]
 pub struct DropckOutlivesErr {
     span: Span,
     overflow_ty: Ty<'tcx>,
