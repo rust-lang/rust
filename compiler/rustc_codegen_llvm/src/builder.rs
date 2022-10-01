268e02c3875f8d0310a8eeb36007e7d80aa63cbd
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2022-10-01 18:22:46 +0000

    Remove type argument of array_alloca and rename to byte_array_alloca

--- compiler/rustc_codegen_llvm/src/builder.rs
+++ compiler/rustc_codegen_llvm/src/builder.rs
@@ -418,9 +418,10 @@ impl<'a, 'll, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'll, 'tcx> {
         }
     }
 
-    fn array_alloca(&mut self, ty: &'ll Type, len: &'ll Value, align: Align) -> &'ll Value {
+    fn byte_array_alloca(&mut self, len: &'ll Value, align: Align) -> &'ll Value {
         unsafe {
-            let alloca = llvm::LLVMBuildArrayAlloca(self.llbuilder, ty, len, UNNAMED);
+            let alloca =
+                llvm::LLVMBuildArrayAlloca(self.llbuilder, self.cx().type_i8(), len, UNNAMED);
             llvm::LLVMSetAlignment(alloca, align.bytes() as c_uint);
             alloca
         }
