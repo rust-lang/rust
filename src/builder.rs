413edf67713195ebbf57ba50aac84e9387559bbe
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2022-10-01 18:22:46 +0000

    Remove type argument of array_alloca and rename to byte_array_alloca

--- src/builder.rs
+++ src/builder.rs
@@ -655,7 +655,7 @@ impl<'a, 'gcc, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'gcc, 'tcx> {
         self.current_func().new_local(None, aligned_type, &format!("stack_var_{}", self.stack_var_count.get())).get_address(None)
     }
 
-    fn array_alloca(&mut self, _ty: Type<'gcc>, _len: RValue<'gcc>, _align: Align) -> RValue<'gcc> {
+    fn byte_array_alloca(&mut self, _len: RValue<'gcc>, _align: Align) -> RValue<'gcc> {
         unimplemented!();
     }
 
