299e10d7ad2601b24bbd5556de402be10cd46fe0
Camille GILLOT <gillot.camille@gmail.com>
2022-10-01 09:46:39 +0200

    Add FIXME.

--- compiler/rustc_lint/src/levels.rs
+++ compiler/rustc_lint/src/levels.rs
@@ -162,6 +162,7 @@ fn shallow_lint_levels_on(tcx: TyCtxt<'_>, owner: hir::OwnerId) -> ShallowLintLe
         [(local_id, _)] => levels.add_id(HirId { owner, local_id: *local_id }),
         // Otherwise, we need to visit the attributes in source code order, so we fetch HIR and do
         // a standard visit.
+        // FIXME(#102522) Just iterate on attrs once that iteration order matches HIR's.
         _ => match tcx.hir().expect_owner(owner) {
             hir::OwnerNode::Item(item) => levels.visit_item(item),
             hir::OwnerNode::ForeignItem(item) => levels.visit_foreign_item(item),
