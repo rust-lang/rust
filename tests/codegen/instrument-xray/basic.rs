0fef658ffeb0fdc858d6c111a992b570ed73c951
Oleksii Lozovskyi <me@ilammy.net>
2022-10-02 10:25:49 +0900

    Codegen tests for -Z instrument-xray
    Let's add at least some tests to verify that this option is accepted
    and produces expected LLVM attributes. More tests can be added later
    with attribute support.

---
+++ tests/codegen/instrument-xray/basic.rs
@@ -0,0 +1,8 @@
+// Checks that `-Z instrument-xray` produces expected instrumentation.
+//
+// compile-flags: -Z instrument-xray=always
+
+#![crate_type = "lib"]
+
+// CHECK: attributes #{{.*}} "function-instrument"="xray-always"
+pub fn function() {}
---
+++ tests/codegen/instrument-xray/options-combine.rs
@@ -0,0 +1,11 @@
+// Checks that `-Z instrument-xray` options can be specified multiple times.
+//
+// compile-flags: -Z instrument-xray=skip-exit
+// compile-flags: -Z instrument-xray=instruction-threshold=123
+// compile-flags: -Z instrument-xray=instruction-threshold=456
+
+#![crate_type = "lib"]
+
+// CHECK:      attributes #{{.*}} "xray-instruction-threshold"="456" "xray-skip-exit"
+// CHECK-NOT:  attributes #{{.*}} "xray-instruction-threshold"="123"
+pub fn function() {}
