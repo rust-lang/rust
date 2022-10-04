c72c6e01c8020c7a285b76a9b61934e3501b80f3
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-10-04 08:59:20 +0000

    Merge the `~const` and `impl const` checks and add some explanatory notes

--- src/test/ui/rfc-2632-const-trait-impl/const-impl-requires-const-trait.rs
+++ src/test/ui/rfc-2632-const-trait-impl/const-impl-requires-const-trait.rs
@@ -1,10 +1,9 @@
 #![feature(const_trait_impl)]
 
 pub trait A {}
-//~^ NOTE: this trait must be annotated with `#[const_trait]`
+//~^ HELP: mark `A` as const
 
 impl const A for () {}
-//~^ ERROR: const `impl`s must be for traits marked with `#[const_trait]`
-//~| ERROR: ~const can only be applied to `#[const_trait]` traits
+//~^ ERROR: const `impl` for trait `A` which is not marked with `#[const_trait]`
 
 fn main() {}
