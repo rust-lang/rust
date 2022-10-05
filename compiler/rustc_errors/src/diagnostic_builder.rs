fc0ba2c8b6c144a8bda60b8267fcbbdc8ed48084
Xiretza <xiretza@xiretza.xyz>
2022-10-05 21:40:56 +0200

    Use AddToDiagnostic for "use latest edition" help

--- compiler/rustc_errors/src/diagnostic_builder.rs
+++ compiler/rustc_errors/src/diagnostic_builder.rs
@@ -669,7 +669,6 @@ impl<'a, G: EmissionGuarantee> DiagnosticBuilder<'a, G> {
         sp: impl Into<MultiSpan>,
         msg: impl Into<SubdiagnosticMessage>,
     ) -> &mut Self);
-    forward!(pub fn help_use_latest_edition(&mut self,) -> &mut Self);
     forward!(pub fn set_is_lint(&mut self,) -> &mut Self);
 
     forward!(pub fn disable_suggestions(&mut self,) -> &mut Self);
