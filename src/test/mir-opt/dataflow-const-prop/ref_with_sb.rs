b9dbb81b5e4f62be117fd196723a85e45b84341d
Jannis Christopher Köhl <mail@koehl.dev>
2022-10-07 01:17:43 +0200

    Improve example used for SB tests

--- src/test/mir-opt/dataflow-const-prop/ref_with_sb.rs
+++ src/test/mir-opt/dataflow-const-prop/ref_with_sb.rs
@@ -4,11 +4,15 @@
 #[inline(never)]
 fn escape<T>(x: &T) {}
 
+#[inline(never)]
+fn some_function() {}
+
 // EMIT_MIR ref_with_sb.main.DataflowConstProp.diff
 fn main() {
     let mut a = 0;
     escape(&a);
     a = 1;
+    some_function();
     // With `-Zunsound-mir-opt`, this should be propagated
     // (because we assume Stacked Borrows).
     let b = a;
--- src/test/mir-opt/dataflow-const-prop/ref_without_sb.rs
+++ src/test/mir-opt/dataflow-const-prop/ref_without_sb.rs
@@ -3,11 +3,15 @@
 #[inline(never)]
 fn escape<T>(x: &T) {}
 
+#[inline(never)]
+fn some_function() {}
+
 // EMIT_MIR ref_without_sb.main.DataflowConstProp.diff
 fn main() {
     let mut a = 0;
     escape(&a);
     a = 1;
+    some_function();
     // Without `-Zunsound-mir-opt`, this should not be propagated
     // (because we do not assume Stacked Borrows).
     let b = a;
