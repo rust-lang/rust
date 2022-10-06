86a046f50238574c9e5b922be8b347fd5187e547
mejrs <>
2022-10-06 21:56:57 +0200

    Formatting

--- compiler/rustc_trait_selection/src/traits/error_reporting/mod.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/mod.rs
@@ -678,7 +678,7 @@ impl<'a, 'tcx> InferCtxtExt<'tcx> for InferCtxt<'a, 'tcx> {
                                 None,
                                 obligation.cause.body_id,
                             );
-                        } else if !suggested  {
+                        } else if !suggested {
                             // Can't show anything else useful, try to find similar impls.
                             // This is skipped for traits with type parameters
                             // because those suggestions are  noisy and unhelpful
