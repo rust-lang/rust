dcd92d7b8cd6dffc6037ac28d9ee0ef301df1374
clubby789 <jamie@hill-daniel.co.uk>
2022-10-10 15:08:40 +0100

    Update UI test

--- src/test/ui/suggestions/inner_type2.rs
+++ src/test/ui/suggestions/inner_type2.rs
@@ -22,5 +22,5 @@ fn main() {
     let item = std::mem::MaybeUninit::new(Struct { p: 42_u32 });
     item.method();
     //~^ ERROR no method named `method` found for union `MaybeUninit` in the current scope [E0599]
-    //~| HELP if this `MaybeUninit::<Struct<u32>>` has been initialized, use one of the `assume_init` methods to access the inner value
+    //~| HELP if this `MaybeUninit<Struct<u32>>` has been initialized, use one of the `assume_init` methods to access the inner value
 }
