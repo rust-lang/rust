e21fa6f5d16a487e16ff1555d3633f8d3698fce6
Christoph Herzog <chris@theduke.at>
2022-10-12 12:40:27 +0200

    Fix wasmer target specs.
    LinkerFlavor definitions have changed upstream.

--- compiler/rustc_target/src/spec/wasm32_wasmer_wasi.rs
+++ compiler/rustc_target/src/spec/wasm32_wasmer_wasi.rs
@@ -72,22 +72,22 @@
 //! you know what you're getting in to!
 
 use super::{wasm_base, TlsModel};
-use super::{crt_objects, LinkerFlavor, LldFlavor, Target};
+use super::{crt_objects, Cc, LinkerFlavor, Target};
 
 pub fn target() -> Target {
     let mut options = wasm_base::options();
 
     options.os = "wasi".into();
     options.vendor = "wasmer".into();
-    options.linker_flavor = LinkerFlavor::Lld(LldFlavor::Wasm);
-    options
-        .pre_link_args
-        .entry(LinkerFlavor::Gcc)
-        .or_insert(Vec::new())
-        .push("--target=wasm32-wasi".into());
+    options.add_pre_link_args(LinkerFlavor::WasmLld(Cc::Yes), &[
+        "--target=wasm32-wasi",
+        // We need shared memory for multithreading
+        "--shared-memory",
+        "--no-check-features"
+    ]);
 
-    options.pre_link_objects_fallback = crt_objects::pre_wasi_fallback();
-    options.post_link_objects_fallback = crt_objects::post_wasi_fallback();
+    options.pre_link_objects_self_contained = crt_objects::pre_wasi_self_contained();
+    options.post_link_objects_self_contained = crt_objects::post_wasi_self_contained();
 
     // WASI now supports multi-threading but not yet thread local support
     options.singlethread = false;
@@ -111,15 +111,10 @@ pub fn target() -> Target {
     // `args::args()` makes the WASI API calls itself.
     options.main_needs_argc_argv = false;
 
+    // WASIX enables more WASM features
     // TODO: Adding "+atomics" here seems to enable more of the multithreading however it does not yet
     //       work properly so more work is needed to finish this, otherwise this is very close to
     //       full networking support.
-    // We need shared memory for multithreading
-    let lld_args = options.pre_link_args.get_mut(&LinkerFlavor::Lld(LldFlavor::Wasm)).unwrap();
-    lld_args.push("--shared-memory".into());
-    lld_args.push("--no-check-features".into());
-
-    // WASIX enables more WASM features
     options.features = "+bulk-memory,+atomics,+mutable-globals,+sign-ext,+nontrapping-fptoint".into();
     
     Target {
--- compiler/rustc_target/src/spec/wasm64_wasmer_wasi.rs
+++ compiler/rustc_target/src/spec/wasm64_wasmer_wasi.rs
@@ -2,25 +2,22 @@
 //!
 
 use super::{wasm_base, TlsModel};
-use super::{crt_objects, LinkerFlavor, LldFlavor, Target};
+use super::{crt_objects, Cc, LinkerFlavor, Target};
 
 pub fn target() -> Target {
     let mut options = wasm_base::options();
     options.os = "wasi".into();
     options.vendor = "wasmer".into();
-    options.linker_flavor = LinkerFlavor::Lld(LldFlavor::Wasm);
-    let clang_args = options
-        .pre_link_args
-        .entry(LinkerFlavor::Gcc)
-        .or_insert(Vec::new());
+    options.add_pre_link_args(LinkerFlavor::WasmLld(Cc::Yes), &[
+        "-mwasm64",
+        "--target=wasm64-wasi",
+        // We need shared memory for multithreading
+        "--shared-memory",
+        "--no-check-features"
+    ]);
 
-    // Make sure clang uses LLD as its linker and is configured appropriately
-    // otherwise
-    clang_args.push("--target=wasm64-wasi".into());
-    
-
-    options.pre_link_objects_fallback = crt_objects::pre_wasi_fallback();
-    options.post_link_objects_fallback = crt_objects::post_wasi_fallback();
+    options.pre_link_objects_self_contained = crt_objects::pre_wasi_self_contained();
+    options.post_link_objects_self_contained = crt_objects::post_wasi_self_contained();
 
     // WASI now supports multi-threading but not yet thread local support
     options.singlethread = false;
@@ -44,19 +41,13 @@ pub fn target() -> Target {
     // `args::args()` makes the WASIX API calls itself.
     options.main_needs_argc_argv = false;
 
-    let lld_args = options.pre_link_args.get_mut(&LinkerFlavor::Lld(LldFlavor::Wasm)).unwrap();
-    lld_args.push("-mwasm64".into());
-
-    // TODO: Adding "+atomics" here seems to enable more of the multithreading however it does not yet
-    //       work properly so more work is needed to finish this, otherwise this is very close to
-    //       full networking support.
-    // We need shared memory for multithreading
-    lld_args.push("--shared-memory".into());
-    lld_args.push("--no-check-features".into());
 
     // Any engine that implements wasm64 will surely implement the rest of these
     // features since they were all merged into the official spec by the time
     // wasm64 was designed.
+    // TODO: Adding "+atomics" here seems to enable more of the multithreading however it does not yet
+    //       work properly so more work is needed to finish this, otherwise this is very close to
+    //       full networking support.
     options.features = "+bulk-memory,+atomics,+mutable-globals,+sign-ext,+nontrapping-fptoint".into();
 
     Target {
