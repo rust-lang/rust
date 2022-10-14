1ec96293ff26ec5d524ec04b58d42c7f2477e8ff
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2022-10-14 22:55:32 +0400

    Revert default for `-Zpacked_bundled_libs` back to false

--- compiler/rustc_session/src/options.rs
+++ compiler/rustc_session/src/options.rs
@@ -1431,7 +1431,7 @@ options! {
         "pass `-install_name @rpath/...` to the macOS linker (default: no)"),
     diagnostic_width: Option<usize> = (None, parse_opt_number, [UNTRACKED],
         "set the current output width for diagnostic truncation"),
-    packed_bundled_libs: bool = (true, parse_bool, [TRACKED],
+    packed_bundled_libs: bool = (false, parse_bool, [TRACKED],
         "change rlib format to store native libraries as archives"),
     panic_abort_tests: bool = (false, parse_bool, [TRACKED],
         "support compiling tests with panic=abort (default: no)"),
