3aef5e5889ffb4221f5806bf704f2e91637c4b02
Christoph Herzog <chris@theduke.at>
2022-10-15 15:23:12 +0200

    fix: Fix wasm64-wasmer-wasi target linker args
    Correctly specify the -mwasm64 arg.

--- compiler/rustc_target/src/spec/wasm64_wasmer_wasi.rs
+++ compiler/rustc_target/src/spec/wasm64_wasmer_wasi.rs
@@ -9,12 +9,12 @@ pub fn target() -> Target {
     options.os = "wasi".into();
     options.vendor = "wasmer".into();
     options.add_pre_link_args(LinkerFlavor::WasmLld(Cc::Yes), &[
-        "-mwasm64",
         "--target=wasm64-wasi",
         // We need shared memory for multithreading
         "--shared-memory",
         "--no-check-features"
     ]);
+    options.add_pre_link_args(LinkerFlavor::WasmLld(Cc::No), &["-mwasm64"]);
 
     options.pre_link_objects_self_contained = crt_objects::pre_wasi_self_contained();
     options.post_link_objects_self_contained = crt_objects::post_wasi_self_contained();
