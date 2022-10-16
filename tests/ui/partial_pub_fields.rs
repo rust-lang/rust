abd5db332173a5ef76a56f4fd18af421cf551c17
TennyZhuang <zty0826@gmail.com>
2022-10-16 16:57:31 +0800

    add many tests
    Signed-off-by: TennyZhuang <zty0826@gmail.com>

--- tests/ui/partial_pub_fields.rs
+++ tests/ui/partial_pub_fields.rs
@@ -2,8 +2,6 @@
 #![warn(clippy::partial_pub_fields)]
 
 fn main() {
-    // test code goes here
-
     use std::collections::HashMap;
 
     #[derive(Default)]
@@ -16,5 +14,27 @@ fn main() {
         pub r: u8,
         pub g: u8,
         b: u8,
+    }
+
+    pub struct Point(i32, pub i32);
+
+    pub struct Visibility {
+        r#pub: bool,
+        pub pos: u32,
+    }
+
+    // Don't lint on empty structs;
+    pub struct Empty1;
+    pub struct Empty2();
+    pub struct Empty3 {};
+
+    // Don't lint on structs with one field.
+    pub struct Single1(i32);
+    pub struct Single2(pub i32);
+    pub struct Single3 {
+        v1: i32,
+    }
+    pub struct Single4 {
+        pub v1: i32,
     }
 }
