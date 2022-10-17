bbac29c0950c780af5a9aea707d4ff30c046be45
Michael Howell <michael@notriddle.com>
2022-10-17 13:54:42 -0700

    rustdoc: remove CSS `@media (min-width: 701px)`
    The two rules within it can and should be done without the separate
    media query:
    
      * There ain't no rule saying a viewport can't be `700.5px` wide, since
        hardware pixels can be finer than CSS pixels.
    
      * The rule for the first example-wrap child should probably apply
        on mobile.
    
      * The rule for the source sidebar is overriden by the mobile rule
        setting `max-width: 100vw`, so it can be merged with the rest
        of the styles.

--- src/librustdoc/html/static/js/storage.js
+++ src/librustdoc/html/static/js/storage.js
@@ -10,7 +10,7 @@ window.currentTheme = document.getElementById("themeStyle");
 window.mainTheme = document.getElementById("mainThemeStyle");
 
 // WARNING: RUSTDOC_MOBILE_BREAKPOINT MEDIA QUERY
-// If you update this line, then you also need to update the two media queries with the same
+// If you update this line, then you also need to update the media query with the same
 // warning in rustdoc.css
 window.RUSTDOC_MOBILE_BREAKPOINT = 701;
 
