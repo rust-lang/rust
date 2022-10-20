b5cd01a8caa3c58e4593f5827b4bbf71ce37a095
chetankokil <All189@#>
2022-10-20 00:34:03 -0500

    made changes as per the suggestion

--- src/bootstrap/compile.rs
+++ src/bootstrap/compile.rs
@@ -1107,8 +1107,6 @@ impl Step for Sysroot {
         let host_dir = builder.out.join(&compiler.host.triple);
         let sysroot = if compiler.stage == 0 {
             host_dir.join("stage0-sysroot")
-        } else if builder.download_rustc() {
-            host_dir.join("ci-rustc-sysroot")
         } else {
             host_dir.join(format!("stage{}", compiler.stage))
         };
