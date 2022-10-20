002ce8c9182668cdd8403860ea496a12ce031560
Dan Gohman <dev@sunfishcode.online>
2022-10-20 08:04:19 -0700

    Mark `std::os::wasi::io::AsFd` etc. as stable.
    io_safety was stabilized in Rust 1.63, so mark the io_safety exports in
    `std::os::wasi::io` as stable.
    
    Fixes #103306.

--- library/std/src/os/wasi/io/mod.rs
+++ library/std/src/os/wasi/io/mod.rs
@@ -1,12 +1,12 @@
 //! WASI-specific extensions to general I/O primitives.
 
 #![deny(unsafe_op_in_unsafe_fn)]
-#![unstable(feature = "wasi_ext", issue = "71213")]
+#![stable(feature = "io_safety_wasi", since = "1.65.0")]
 
 mod fd;
 mod raw;
 
-#[unstable(feature = "wasi_ext", issue = "71213")]
+#[stable(feature = "io_safety_wasi", since = "1.65.0")]
 pub use fd::*;
-#[unstable(feature = "wasi_ext", issue = "71213")]
+#[stable(feature = "io_safety_wasi", since = "1.65.0")]
 pub use raw::*;
