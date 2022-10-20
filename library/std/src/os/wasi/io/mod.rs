e56b84e8440bedfa7854ed027324552035601114
Dan Gohman <dev@sunfishcode.online>
2022-10-20 08:04:19 -0700

    Mark `std::os::wasi::io::AsFd` etc. as stable.
    io_safety was stabilized in Rust 1.63, so mark the io_safety exports in
    `std::os::wasi::io` as stable.
    
    Fixes #103306.

--- library/std/src/os/wasi/io/mod.rs
+++ library/std/src/os/wasi/io/mod.rs
@@ -2,5 +2,5 @@
 
 #![unstable(feature = "wasi_ext", issue = "71213")]
 
-#[unstable(feature = "wasi_ext", issue = "71213")]
+#[stable(feature = "io_safety", since = "1.63.0")]
 pub use crate::os::fd::*;
