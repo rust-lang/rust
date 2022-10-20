184b115e76e1ed8be9a79b07b35ba785cdc95d16
chetankokil <All189@#>
2022-10-20 00:06:03 -0500

    reverted changes, as earlier changes was appropriate

--- src/bootstrap/compile.rs
+++ src/bootstrap/compile.rs
@@ -1106,11 +1106,11 @@ impl Step for Sysroot {
         let compiler = self.compiler;
         let host_dir = builder.out.join(&compiler.host.triple);
         let sysroot = if compiler.stage == 0 {
-            builder.out.join(&compiler.host.triple).join("stage0-sysroot")
+            host_dir.join("stage0-sysroot")
         } else if builder.download_rustc() {
-            builder.out.join(&compiler.host.triple).join("ci-rustc-sysroot")
+            host_dir.join("ci-rustc-sysroot")
         } else {
-            builder.out.join(&compiler.host.triple).join(format!("stage{}", compiler.stage))
+            host_dir.join(format!("stage{}", compiler.stage))
         };
         let _ = fs::remove_dir_all(&sysroot);
         t!(fs::create_dir_all(&sysroot));
