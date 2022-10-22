9f0503e4a60d73b6a8404e4f9c7678e162173305
Finn Bear <finnbearlabs@gmail.com>
2022-10-22 12:26:47 -0700

    Fix typo in docs of `String::leak`.

--- library/alloc/src/string.rs
+++ library/alloc/src/string.rs
@@ -1851,7 +1851,7 @@ impl String {
     }
 
     /// Consumes and leaks the `String`, returning a mutable reference to the contents,
-    /// `&'a mut str`.
+    /// `&'static mut str`.
     ///
     /// This is mainly useful for data that lives for the remainder of
     /// the program's life. Dropping the returned reference will cause a memory
