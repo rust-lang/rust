d2a37847809bc3df86afb4c65b5fe5f06d99ce20
BlackHoleFox <blackholefoxdev@gmail.com>
2022-10-23 15:33:01 -0500

    Fix x86_64-apple-tvos target to use the correct target_abi

--- compiler/rustc_target/src/spec/apple/tests.rs
+++ compiler/rustc_target/src/spec/apple/tests.rs
@@ -1,12 +1,16 @@
-use crate::spec::{aarch64_apple_ios_sim, aarch64_apple_watchos_sim, x86_64_apple_ios};
+use crate::spec::{
+    aarch64_apple_ios_sim, aarch64_apple_watchos_sim, x86_64_apple_ios, x86_64_apple_tvos,
+};
 
 #[test]
 fn simulator_targets_set_abi() {
     let all_sim_targets = [
         x86_64_apple_ios::target(),
+        x86_64_apple_tvos::target(),
         aarch64_apple_ios_sim::target(),
+        // Note: There is currently no ARM64 tvOS simulator target
         aarch64_apple_watchos_sim::target(),
-        // TODO: x86_64-apple-tvos and x86_64-apple-watchos-sim
+        // TODO: x86_64-apple-watchos-sim
     ];
 
     for target in all_sim_targets {
