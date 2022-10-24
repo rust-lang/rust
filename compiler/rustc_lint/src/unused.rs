a46af18cb1aedf7ccc5fbebd6f126c71da6cd4ee
yukang <moorekang@gmail.com>
2022-10-24 18:24:10 +0800

    fix parentheses surrounding spacing issue in parser

--- compiler/rustc_lint/src/unused.rs
+++ compiler/rustc_lint/src/unused.rs
@@ -568,8 +568,7 @@ trait UnusedDelimLint {
                 let sm = cx.sess().source_map();
                 let lo_replace =
                     if keep_space.0 &&
-                        let Ok(snip) = sm.span_to_snippet(lo.with_lo(lo.lo() - BytePos(1))) &&
-                        !snip.starts_with(" ") {
+                        let Ok(snip) = sm.span_to_prev_source(lo) && !snip.ends_with(" ") {
                         " ".to_string()
                         } else {
                             "".to_string()
@@ -577,8 +576,7 @@ trait UnusedDelimLint {
 
                 let hi_replace =
                     if keep_space.1 &&
-                        let Ok(snip) = sm.span_to_snippet(sm.next_point(hi)) &&
-                        !snip.starts_with(" ") {
+                        let Ok(snip) = sm.span_to_next_source(hi) && !snip.starts_with(" ") {
                         " ".to_string()
                         } else {
                             "".to_string()
