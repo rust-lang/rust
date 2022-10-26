f71741bac41a5373791958d4a0a36c84d83b1bd5
Patrick Walton <pcwalton@fb.com>
2022-10-26 15:12:19 -0700

    [rustc_ty_utils] Add the LLVM `noalias` parameter attribute to `drop_in_place` in certain cases.
    LLVM can make use of the `noalias` parameter attribute on the parameter to
    `drop_in_place` in areas like argument promotion. Because the Rust compiler
    fully controls the code for `drop_in_place`, it can soundly deduce parameter
    attributes on it. In the case of a value that has a programmer-defined Drop
    implementation, we know that the first thing `drop_in_place` will do is pass a
    pointer to the object to `Drop::drop`. `Drop::drop` takes `&mut`, so it must be
    guaranteed that there are no pointers to the object upon entering that
    function. Therefore, it should be safe to mark `noalias` there.
    
    With this patch, we mark `noalias` only when the type is a value with a
    programmer-defined Drop implementation. This is possibly overly conservative,
    but I thought that proceeding cautiously was best in this instance.

---
+++ tests/codegen/drop-in-place-noalias.rs
@@ -0,0 +1,34 @@
+// Tests that the compiler can mark `drop_in_place` as `noalias` when safe to do so.
+
+#![crate_type="lib"]
+
+use std::hint::black_box;
+
+// CHECK: define{{.*}}drop_in_place{{.*}}Foo{{.*}}({{.*}}noalias{{.*}})
+
+#[repr(C)]
+pub struct Foo {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+impl Drop for Foo {
+    #[inline(never)]
+    fn drop(&mut self) {
+        black_box(self.a);
+    }
+}
+
+extern {
+    fn bar();
+    fn baz(foo: Foo);
+}
+
+pub fn haha() {
+    let foo = Foo { a: 1, b: 2, c: 3 };
+    unsafe {
+        bar();
+        baz(foo);
+    }
+}
