ecfb33238c927633311aa9624963c406ef9b241b
Patrick Walton <pcwalton@fb.com>
2022-10-26 21:49:23 -0700

    Fix noalias box test

--- src/test/codegen/noalias-box-off.rs
+++ src/test/codegen/noalias-box-off.rs
@@ -3,6 +3,6 @@
 #![crate_type = "lib"]
 
 // CHECK-LABEL: @box_should_not_have_noalias_if_disabled(
-// CHECK-NOT: noalias
+// CHECK-NOT: noalias{{.*}}%
 #[no_mangle]
 pub fn box_should_not_have_noalias_if_disabled(_b: Box<u8>) {}
