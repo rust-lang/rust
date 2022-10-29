a258a543b84cb906556851cd77990766f16f70ae
Mark Rousskov <mark.simulacrum@gmail.com>
2022-10-29 11:20:07 -0400

    Add test validating successful backport

---
+++ src/test/ui/const-generics/issue-103243.rs
@@ -0,0 +1,35 @@
+// build-pass
+
+pub trait CSpace<const N: usize>: Sized {
+    type Traj;
+}
+
+pub trait FullTrajectory<T, S1, const N: usize> {}
+
+pub struct Const<const R: usize>;
+
+pub trait Obstacle<CS, const N: usize>
+where
+    CS: CSpace<N>,
+{
+    fn trajectory_free<FT, S1>(&self, t: &FT)
+    where
+        FT: FullTrajectory<CS::Traj, S1, N>;
+}
+
+// -----
+
+const N: usize = 4;
+
+struct ObstacleSpace2df32;
+
+impl<CS> Obstacle<CS, N> for ObstacleSpace2df32
+where
+    CS: CSpace<N>,
+{
+    fn trajectory_free<TF, S1>(&self, t: &TF)
+    where
+        TF: FullTrajectory<CS::Traj, S1, N>,
+    {
+    }
+}
