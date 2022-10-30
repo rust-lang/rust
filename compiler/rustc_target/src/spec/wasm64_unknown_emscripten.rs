ce33ba110a3986f6390074daab1db6894992e1e8
Christoph Herzog <chris@theduke.at>
2022-10-30 23:48:13 +0100

    Stub out emscripten config (NASTY QUICK FIX)

--- compiler/rustc_target/src/spec/wasm64_unknown_emscripten.rs
+++ compiler/rustc_target/src/spec/wasm64_unknown_emscripten.rs
@@ -1,51 +1,13 @@
-use super::{cvs, wasm_base};
-use super::{LinkArgs, LinkerFlavor, PanicStrategy, Target, TargetOptions};
+use super::wasm_base;
+use super::Target;
+
 
 pub fn target() -> Target {
-    let mut options = wasm_base::options();
-
-    let clang_args = options.pre_link_args.entry(LinkerFlavor::Gcc).or_default();
-    clang_args.push("-mwasm64".into());
-
-    // Rust really needs a way for users to specify exports and imports in
-    // the source code. --export-dynamic isn't the right tool for this job,
-    // however it does have the side effect of automatically exporting a lot
-    // of symbols, which approximates what people want when compiling for
-    // wasm32-unknown-unknown expect, so use it for now.
-    clang_args.push("--export-dynamic".into());
-
-    let mut post_link_args = LinkArgs::new();
-    post_link_args.insert(
-        LinkerFlavor::Em,
-        vec![
-            "-s".into(),
-            "ERROR_ON_UNDEFINED_SYMBOLS=1".into(),
-            "-s".into(),
-            "ASSERTIONS=1".into(),
-            "-s".into(),
-            "ABORTING_MALLOC=0".into(),
-            "-Wl,--fatal-warnings".into(),
-        ],
-    );
-
-    let opts = TargetOptions {
-        os: "emscripten".into(),
-        linker_flavor: LinkerFlavor::Em,
-        // emcc emits two files - a .js file to instantiate the wasm and supply platform
-        // functionality, and a .wasm file.
-        exe_suffix: ".js".into(),
-        linker: None,
-        is_like_emscripten: true,
-        panic_strategy: PanicStrategy::Unwind,
-        post_link_args,
-        families: cvs!["unix", "wasm"],
-        ..options
-    };
     Target {
         llvm_target: "wasm64-unknown-emscripten".into(),
         pointer_width: 64,
         data_layout: "e-m:e-p:64:64-p10:8:8-p20:8:8-i64:64-f128:64-n32:64-S128-ni:1:10:20".into(),
         arch: "wasm64".into(),
-        options: opts,
+        options: wasm_base::options(),
     }
 }
