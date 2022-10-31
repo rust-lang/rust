7b6ead2027d073613ff3f160f221f5d3a5941b0b
Camille GILLOT <gillot.camille@gmail.com>
2022-10-31 16:48:23 +0000

    Explain disabled span hashing.

--- compiler/rustc_span/src/hygiene.rs
+++ compiler/rustc_span/src/hygiene.rs
@@ -104,6 +104,10 @@ fn assert_default_hashing_controls<CTX: HashStableContext>(ctx: &CTX, msg: &str)
         // `-Z incremental-ignore-spans` option. Normally, this option is disabled,
         // which will cause us to require that this method always be called with `Span` hashing
         // enabled.
+        //
+        // Span hashing can also be disabled without `-Z incremental-ignore-spans`.
+        // This is the case for instance when building a hash for name mangling.
+        // Such configuration must not be used for metadata.
         HashingControls { hash_spans }
             if hash_spans == !ctx.unstable_opts_incremental_ignore_spans() => {}
         other => panic!("Attempted hashing of {msg} with non-default HashingControls: {:?}", other),
