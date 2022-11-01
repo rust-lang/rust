c1420974aa02b4b46c46774c9447a5aee4278160
Jakub Beránek <berykubik@gmail.com>
2022-11-01 19:36:25 +0100

    Revert "ci: Bring back ninja for dist builders"

--- src/ci/docker/host-x86_64/dist-x86_64-linux/build-clang.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/build-clang.sh
@@ -25,7 +25,6 @@ INC="/rustroot/include:/usr/include"
 # disable them. BOLT is used for optimizing LLVM.
 hide_output \
     cmake ../llvm \
-      -GNinja \
       -DCMAKE_C_COMPILER=/rustroot/bin/gcc \
       -DCMAKE_CXX_COMPILER=/rustroot/bin/g++ \
       -DCMAKE_BUILD_TYPE=Release \
@@ -40,8 +39,8 @@ hide_output \
       -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt;bolt" \
       -DC_INCLUDE_DIRS="$INC"
 
-hide_output ninja
-hide_output ninja install
+hide_output make -j$(nproc)
+hide_output make install
 
 cd ../..
 rm -rf llvm-project
