2ef8308687f57335e117fdfa7d92002cf6f53eda
b-naber <bn263@gmx.de>
2022-11-02 22:05:19 +0100

    add more tests

--- src/test/ui/mir/field-ty-ascription.rs
+++ src/test/ui/mir/field-ty-ascription.rs
@@ -2,6 +2,22 @@
 
 struct Foo<T>(T); // `T` is covariant.
 
+struct Bar<T> {
+    x: T,
+} // `T` is covariant.
+
+fn bar<'b>(x: Bar<for<'a> fn(&'a ())>) {
+    let Bar { x }: Bar<fn(&'b ())> = x;
+}
+
+fn bar_nested<'b>(x: Bar<Bar<for<'a> fn(&'a ())>>) {
+    let Bar { x: Bar { x } }: Bar<Bar<fn(&'b ())>> = x;
+}
+
+fn bar_foo_nested<'b>(x: Bar<Foo<for<'a> fn(&'a ())>>) {
+    let Bar { x: Foo ( x ) }: Bar<Foo<fn(&'b ())>> = x;
+}
+
 fn foo<'b>(x: Foo<for<'a> fn(&'a ())>) {
     let Foo(y): Foo<fn(&'b ())> = x;
 }
