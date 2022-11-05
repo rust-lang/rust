98c915279ef03a243f9adecd728885d8c2f995f3
Patrick Walton <pcwalton@fb.com>
2022-11-05 01:13:42 -0700

    Don't enable by default :)

--- compiler/rustc_session/src/options.rs
+++ compiler/rustc_session/src/options.rs
@@ -1466,7 +1466,7 @@ options! {
     print_llvm_passes: bool = (false, parse_bool, [UNTRACKED],
         "print the LLVM optimization passes being run (default: no)"),
     #[rustc_lint_opt_deny_field_access("use `Session::print_llvm_stats` instead of this field")]
-    print_llvm_stats: bool = (true, parse_bool, [UNTRACKED],
+    print_llvm_stats: bool = (false, parse_bool, [UNTRACKED],
         "print LLVM statistics (default: no)"),
     print_mono_items: Option<String> = (None, parse_opt_string, [UNTRACKED],
         "print the result of the monomorphization collection pass"),
