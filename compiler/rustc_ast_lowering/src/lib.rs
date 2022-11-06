769cc733b1663b48e1a1639bd8e8579ef795fbcd
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-11-06 17:47:06 +0100

    Remove allow(rustc::potential_query_instability) from rustc_ast_lowering

--- compiler/rustc_ast_lowering/src/lib.rs
+++ compiler/rustc_ast_lowering/src/lib.rs
@@ -34,7 +34,6 @@
 #![feature(let_chains)]
 #![feature(never_type)]
 #![recursion_limit = "256"]
-#![allow(rustc::potential_query_instability)]
 #![deny(rustc::untranslatable_diagnostic)]
 #![deny(rustc::diagnostic_outside_of_impl)]
 
