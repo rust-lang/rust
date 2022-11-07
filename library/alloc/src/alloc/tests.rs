ce06c8eb83ebc555b767f76a842b2c3e065fd3cc
Ralf Jung <post@ralfj.de>
2022-11-07 10:33:54 +0100

    run alloc benchmarks in Miri and fix UB

--- library/alloc/src/alloc/tests.rs
+++ library/alloc/src/alloc/tests.rs
@@ -22,7 +22,6 @@ fn allocate_zeroed() {
 }
 
 #[bench]
-#[cfg_attr(miri, ignore)] // isolated Miri does not support benchmarks
 fn alloc_owned_small(b: &mut Bencher) {
     b.iter(|| {
         let _: Box<_> = Box::new(10);
