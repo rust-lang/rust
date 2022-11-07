10dad67f89a320c315c6c758dffe0bee610cbbfa
Sean Cross <sean@xobs.io>
2022-11-07 11:06:38 +0800

    std: xous: add services support
    Xous has a concept of `services` that provide various features.
    Processes may connect to these services by name or by address. Most
    services require a name server in order to connect.
    
    Add a file with the most common services, and provide a way to connect
    to a service by querying the name server.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/os/xous/mod.rs
+++ library/std/src/os/xous/mod.rs
@@ -3,6 +3,9 @@
 
 pub mod ffi;
 
+#[stable(feature = "rust1", since = "1.0.0")]
+pub mod services;
+
 /// A prelude for conveniently writing platform-specific code.
 ///
 /// Includes all extension traits, and some important type definitions.
