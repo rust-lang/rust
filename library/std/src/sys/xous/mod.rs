46a4ec3a71a3e941b2d254c80f5780a2ed9eeff3
Sean Cross <sean@xobs.io>
2022-11-07 16:06:01 +0800

    std: xous: add thread_local_key
    Add an implementation of thread local storage. This uses a container
    that is pointed to by the otherwise-unsed `$tp` register. This container
    is allocated on-demand, so threads that use no TLS will not allocate
    this extra memory.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/sys/xous/mod.rs
+++ library/std/src/sys/xous/mod.rs
@@ -28,7 +28,6 @@ pub mod pipe;
 pub mod process;
 pub mod stdio;
 pub mod thread;
-#[path = "../unsupported/thread_local_key.rs"]
 pub mod thread_local_key;
 #[path = "../unsupported/thread_parking.rs"]
 pub mod thread_parking;
