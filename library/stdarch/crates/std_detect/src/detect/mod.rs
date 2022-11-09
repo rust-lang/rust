45e3039ee9618e8af0708467e34e8e92b9bee7ac
gendx <gendx@users.noreply.github.com>
2022-11-09 18:38:48 +0000

    Detect CPU features with Linux methods on Android for non-Intel CPUs. (#1351)
    Co-authored-by: Amanieu d'Antras <amanieu@gmail.com>

--- library/stdarch/crates/std_detect/src/detect/mod.rs
+++ library/stdarch/crates/std_detect/src/detect/mod.rs
@@ -47,7 +47,7 @@ cfg_if! {
         // On x86/x86_64 no OS specific functionality is required.
         #[path = "os/x86.rs"]
         mod os;
-    } else if #[cfg(all(target_os = "linux", feature = "libc"))] {
+    } else if #[cfg(all(any(target_os = "linux", target_os = "android"), feature = "libc"))] {
         #[path = "os/linux/mod.rs"]
         mod os;
     } else if #[cfg(all(target_os = "freebsd", feature = "libc"))] {
