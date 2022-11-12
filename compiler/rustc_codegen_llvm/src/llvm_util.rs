11224000e8e9468b3c2a7999aa7566d361d81a4c
Caleb Zulawski <caleb.zulawski@gmail.com>
2022-11-12 10:28:06 -0500

    Update compiler/rustc_codegen_llvm/src/llvm_util.rs
    Co-authored-by: Jubilee <46493976+workingjubilee@users.noreply.github.com>

--- compiler/rustc_codegen_llvm/src/llvm_util.rs
+++ compiler/rustc_codegen_llvm/src/llvm_util.rs
@@ -164,7 +164,7 @@ pub fn to_llvm_features<'a>(sess: &Session, s: &'a str) -> SmallVec<[&'a str; 2]
         ("x86", "bmi1") => smallvec!["bmi"],
         ("x86", "cmpxchg16b") => smallvec!["cx16"],
         // FIXME: These aliases are misleading, and should be removed before avx512_target_feature is
-        // stabilized. For now, they must be kept at a minimum because std::arch uses them.
+        // stabilized. They must remain until std::arch switches off them.
         // rust#100752
         ("x86", "avx512vaes") => smallvec!["vaes"],
         ("x86", "avx512gfni") => smallvec!["gfni"],
