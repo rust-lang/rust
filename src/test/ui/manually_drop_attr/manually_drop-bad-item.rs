8b0f5f8f6c989b93d9e0a5ee58217ffb59383401
Devin Jeanpierre <jeanpierreda@google.com>
2022-11-15 13:48:35 -0800

    Fix bad-item tests, which I apparently forgot to do

--- src/test/ui/manually_drop_attr/manually_drop-bad-item.rs
+++ src/test/ui/manually_drop_attr/manually_drop-bad-item.rs
@@ -1,8 +1,7 @@
 #![feature(manually_drop_attr)]
 #![forbid(unused_attributes)]
 #![manually_drop]
-//~^ ERROR `manually_drop` attribute cannot be used at crate level
-//~^^ ERROR attribute should be applied to a struct or enum
+//~^ ERROR attribute should be applied to a struct or enum
 
 #[manually_drop]
 //~^ ERROR attribute should be applied to a struct or enum
