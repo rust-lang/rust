fa99459b4286a2fb45dd1f224a9fc5788da84cfb
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-11-15 16:24:52 +0000

    Reintroduce the span printing in miri (plus point to spans where possible)

--- src/tools/miri/src/diagnostics.rs
+++ src/tools/miri/src/diagnostics.rs
@@ -364,7 +364,10 @@ fn report_msg<'tcx>(
         if is_local && idx > 0 {
             err.span_note(frame_info.span, &frame_info.to_string());
         } else {
-            err.note(&frame_info.to_string());
+            let sm = sess.source_map();
+            let lo = sm.lookup_char_pos(frame_info.span.lo());
+            let filename = sm.filename_for_diagnostics(&lo.file.name);
+            err.note(format!("{frame_info} at {}:{}:{}", filename, lo.line, lo.col.0 + 1));
         }
     }
 
