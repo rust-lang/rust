23744cd4ba57d2fc5c57459002282fe2d896193b
Lukas Wirth <lukastw97@gmail.com>
2022-11-15 19:22:00 +0100

    Use multi-span suggestions

--- clippy_lints/src/semicolon_block.rs
+++ clippy_lints/src/semicolon_block.rs
@@ -1,10 +1,9 @@
-use clippy_utils::diagnostics::span_lint_and_sugg;
-use clippy_utils::source::snippet_with_macro_callsite;
-use clippy_utils::{get_parent_expr_for_hir, get_parent_node};
+use clippy_utils::diagnostics::{multispan_sugg_with_applicability, span_lint_and_then};
 use rustc_errors::Applicability;
-use rustc_hir::{Block, Expr, ExprKind, Node, Stmt, StmtKind};
+use rustc_hir::{Block, Expr, ExprKind, Stmt, StmtKind};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
+use rustc_span::Span;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -65,69 +64,73 @@ declare_clippy_lint! {
 declare_lint_pass!(SemicolonBlock => [SEMICOLON_INSIDE_BLOCK, SEMICOLON_OUTSIDE_BLOCK]);
 
 impl LateLintPass<'_> for SemicolonBlock {
-    fn check_block(&mut self, cx: &LateContext<'_>, block: &Block<'_>) {
-        semicolon_inside_block(cx, block);
-        semicolon_outside_block(cx, block);
+    fn check_stmt(&mut self, cx: &LateContext<'_>, stmt: &Stmt<'_>) {
+        match stmt.kind {
+            StmtKind::Expr(Expr {
+                kind:
+                    ExprKind::Block(
+                        block @ Block {
+                            expr: None,
+                            stmts:
+                                &[
+                                    ..,
+                                    Stmt {
+                                        kind: StmtKind::Semi(expr),
+                                        span,
+                                        ..
+                                    },
+                                ],
+                            ..
+                        },
+                        _,
+                    ),
+                ..
+            }) if !block.span.from_expansion() => semicolon_outside_block(cx, block, expr, span),
+            StmtKind::Semi(Expr {
+                kind: ExprKind::Block(block @ Block { expr: Some(tail), .. }, _),
+                ..
+            }) if !block.span.from_expansion() => semicolon_inside_block(cx, block, tail, stmt.span),
+            _ => (),
+        }
     }
 }
 
-fn semicolon_inside_block(cx: &LateContext<'_>, block: &Block<'_>) {
-    if !block.span.from_expansion()
-        && let Some(tail) = block.expr
-        && let Some(block_expr @ Expr { kind: ExprKind::Block(_, _), ..}) = get_parent_expr_for_hir(cx, block.hir_id)
-        && let Some(Node::Stmt(Stmt { kind: StmtKind::Semi(_), span, .. })) = get_parent_node(cx.tcx, block_expr.hir_id)
-    {
-        let expr_snip = snippet_with_macro_callsite(cx, tail.span, "..");
+fn semicolon_inside_block(cx: &LateContext<'_>, block: &Block<'_>, tail: &Expr<'_>, semi_span: Span) {
+    let insert_span = tail.span.with_lo(tail.span.hi());
+    let remove_span = semi_span.with_lo(block.span.hi());
 
-        let mut suggestion: String = snippet_with_macro_callsite(cx, block.span, "..").to_string();
-
-        if let Some((expr_offset, _)) = suggestion.rmatch_indices(&*expr_snip).next() {
-            suggestion.insert(expr_offset + expr_snip.len(), ';');
-        } else {
-            return;
-        }
-
-        span_lint_and_sugg(
-            cx,
-            SEMICOLON_INSIDE_BLOCK,
-            *span,
-            "consider moving the `;` inside the block for consistent formatting",
-            "put the `;` here",
-            suggestion,
-            Applicability::MaybeIncorrect,
-        );
-    }
+    span_lint_and_then(
+        cx,
+        SEMICOLON_INSIDE_BLOCK,
+        semi_span,
+        "consider moving the `;` inside the block for consistent formatting",
+        |diag| {
+            multispan_sugg_with_applicability(
+                diag,
+                "put the `;` here",
+                Applicability::MachineApplicable,
+                [(remove_span, String::new()), (insert_span, ";".to_owned())],
+            );
+        },
+    );
 }
 
-fn semicolon_outside_block(cx: &LateContext<'_>, block: &Block<'_>) {
-    if !block.span.from_expansion()
-        && block.expr.is_none()
-        && let [.., Stmt { kind: StmtKind::Semi(expr), .. }] = block.stmts
-        && let Some(block_expr @ Expr { kind: ExprKind::Block(_, _), ..}) = get_parent_expr_for_hir(cx,block.hir_id)
-        && let Some(Node::Stmt(Stmt { kind: StmtKind::Expr(_), .. })) = get_parent_node(cx.tcx, block_expr.hir_id)
-    {
-        let expr_snip = snippet_with_macro_callsite(cx, expr.span, "..");
+fn semicolon_outside_block(cx: &LateContext<'_>, block: &Block<'_>, tail_stmt_expr: &Expr<'_>, semi_span: Span) {
+    let insert_span = block.span.with_lo(block.span.hi());
+    let remove_span = semi_span.with_lo(tail_stmt_expr.span.source_callsite().hi());
 
-        let mut suggestion: String = snippet_with_macro_callsite(cx, block.span, "..").to_string();
-
-        if let Some((expr_offset, _)) = suggestion.rmatch_indices(&*expr_snip).next()
-            && let Some(semi_offset) = suggestion[expr_offset + expr_snip.len()..].find(';')
-        {
-            suggestion.remove(expr_offset +  expr_snip.len() + semi_offset);
-        } else {
-            return;
-        }
-
-        suggestion.push(';');
-
-        span_lint_and_sugg(
-            cx,
-            SEMICOLON_OUTSIDE_BLOCK,
-            block.span,
-            "consider moving the `;` outside the block for consistent formatting",
-            "put the `;` outside the block",
-            suggestion,
-            Applicability::MaybeIncorrect,
-        );
-    }
+    span_lint_and_then(
+        cx,
+        SEMICOLON_OUTSIDE_BLOCK,
+        block.span,
+        "consider moving the `;` outside the block for consistent formatting",
+        |diag| {
+            multispan_sugg_with_applicability(
+                diag,
+                "put the `;` here",
+                Applicability::MachineApplicable,
+                [(remove_span, String::new()), (insert_span, ";".to_owned())],
+            );
+        },
+    );
 }
