9c3555d5c2dc3d1e5025c35db3873c2776054185
Florian Bartels <Florian.Bartels@elektrobit.com>
2022-11-17 11:25:28 +0100

    Remove periods from QNX/nto target names

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1244,8 +1244,8 @@ supported_targets! {
 
     ("mips64-openwrt-linux-musl", mips64_openwrt_linux_musl),
 
-    ("aarch64-unknown-nto-qnx7.1.0", aarch64_unknown_nto_qnx_710),
-    ("x86_64-pc-nto-qnx7.1.0", x86_64_pc_nto_qnx710),
+    ("aarch64-unknown-nto-qnx710", aarch64_unknown_nto_qnx_710),
+    ("x86_64-pc-nto-qnx710", x86_64_pc_nto_qnx710),
 }
 
 /// Cow-Vec-Str: Cow<'static, [Cow<'static, str>]>
