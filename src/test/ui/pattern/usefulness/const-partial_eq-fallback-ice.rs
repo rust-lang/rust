4d9451b1d1e334e90e6725d5577545eae4e28545
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-11-17 15:30:42 +0000

    Fix an ICE that I just made worse

---
+++ src/test/ui/pattern/usefulness/const-partial_eq-fallback-ice.rs
@@ -0,0 +1,20 @@
+#![allow(warnings)]
+
+struct MyType;
+
+impl PartialEq<usize> for MyType {
+    fn eq(&self, y: &usize) -> bool {
+        true
+    }
+}
+
+const CONSTANT: &&MyType = &&MyType;
+
+fn main() {
+    if let CONSTANT = &&MyType {
+        //~^ ERROR must be annotated with `#[derive(PartialEq, Eq)]`
+        println!("semantic!");
+    } else {
+        println!("structural!");
+    }
+}
