e0b1463e0a0133ae49fc31aab3e7f18882ff00b0
Caio <c410.f3r@gmail.com>
2022-11-19 08:25:20 -0300

    Remove unused

--- clippy_lints/src/operators/arithmetic_side_effects.rs
+++ clippy_lints/src/operators/arithmetic_side_effects.rs
@@ -10,10 +10,7 @@ use rustc_hir as hir;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_middle::ty::Ty;
 use rustc_session::impl_lint_pass;
-use rustc_span::{
-    source_map::{Span, Spanned},
-    sym,
-};
+use rustc_span::source_map::{Span, Spanned};
 
 const HARD_CODED_ALLOWED: &[&str] = &[
     "&str",
