7092b1ca0f22c06b2706cc7c22624fc3f8bd4c6b
Thalia Nero <chrisvn00@gmail.com>
2022-11-22 19:33:30 -0600

    Change to non-spanned note

--- compiler/rustc_hir_typeck/src/fn_ctxt/suggestions.rs
+++ compiler/rustc_hir_typeck/src/fn_ctxt/suggestions.rs
@@ -878,7 +878,7 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
             format!("impl {}", all_bounds_str),
             Applicability::MaybeIncorrect,
         );
-        err.span_note(fn_return.span(), "generic type variables are chosen by the caller");
+        err.note("generic type variables are chosen by the caller");
     }
 
     pub(in super::super) fn suggest_missing_break_or_return_expr(
