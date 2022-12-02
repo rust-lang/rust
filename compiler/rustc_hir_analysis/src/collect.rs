b740cdcf43f6d686f52d3f85100a0ff5ff0c043d
Tomasz Miąsko <tomasz.miasko@gmail.com>
2022-12-02 00:00:00 +0000

    Mark naked functions as never inline in codegen_fn_attrs
    Use code generation attributes to ensure that naked functions are never
    inline, replacing separate checks in MIR inliner and LLVM code
    generation.

--- compiler/rustc_hir_analysis/src/collect.rs
+++ compiler/rustc_hir_analysis/src/collect.rs
@@ -2075,6 +2075,7 @@ fn codegen_fn_attrs(tcx: TyCtxt<'_>, did: DefId) -> CodegenFnAttrs {
 
     if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::NAKED) {
         codegen_fn_attrs.flags |= CodegenFnAttrFlags::NO_COVERAGE;
+        codegen_fn_attrs.inline = InlineAttr::Never;
     }
 
     // Weak lang items have the same semantics as "std internal" symbols in the
