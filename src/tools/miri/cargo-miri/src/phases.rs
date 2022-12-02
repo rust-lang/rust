5fd4b84e20315dcdb0bf05bff8512cc9f78d7380
Ralf Jung <post@ralfj.de>
2022-12-02 13:56:42 +0100

    forward verbosity to cargo setup

--- src/tools/miri/cargo-miri/src/phases.rs
+++ src/tools/miri/cargo-miri/src/phases.rs
@@ -94,7 +94,7 @@ pub fn phase_cargo_miri(mut args: impl Iterator<Item = String>) {
     let target = target.as_ref().unwrap_or(host);
 
     // We always setup.
-    setup(&subcommand, target, &rustc_version);
+    setup(&subcommand, target, &rustc_version, verbose);
 
     // Invoke actual cargo for the job, but with different flags.
     // We re-use `cargo test` and `cargo run`, which makes target and binary handling very easy but
