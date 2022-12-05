b9b33d983d83fde3067a28fdd126fd84061e74c8
DebugSteven <debugsteven@gmail.com>
2022-12-05 16:18:43 -0700

    spawn x command and compare semvers

--- src/tools/tidy/src/x_version.rs
+++ src/tools/tidy/src/x_version.rs
@@ -1,17 +1,36 @@
-use std::process::Command;
+use semver::{BuildMetadata, Prerelease, Version};
+use std::io::ErrorKind;
+use std::process::{Command, Stdio};
 
-pub fn check(_bad: &mut bool) {
-    let result = Command::new("x").arg("--version").output();
-    let output = match result {
-        Ok(output) => output,
-        Err(_e) => todo!(),
+pub fn check(bad: &mut bool) {
+    let result = Command::new("x")
+        .arg("--version")
+        .stdout(Stdio::piped())
+        .spawn();
+    let child = match result {
+        Ok(child) => child,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => return (),
+            _ => return tidy_error!(bad, "{}", e),
+        },
     };
+
+    let output = child.wait_with_output().unwrap();
 
     if output.status.success() {
         let version = String::from_utf8_lossy(&output.stdout);
-        assert_eq!("0.1.0", version.trim_end());
+        let version = Version::parse(version.trim_end()).unwrap();
+        let expected = Version {
+            major: 0,
+            minor: 1,
+            patch: 0,
+            pre: Prerelease::new("").unwrap(),
+            build: BuildMetadata::EMPTY,
+        };
+        if version < expected {
+            return tidy_error!(bad, "Current version of x is {version} Consider updating to the newer version of x by running `cargo install --path src/tools/x`");
+        }
+    } else {
+        return tidy_error!(bad, "{}", output.status);
     }
-    // FIXME: throw some kind of tidy error when the version of x isn't
-    // greater than or equal to the version we'd expect.
-    //tidy_error!(bad, "Current version of x is {version} Consider updating to the newer version of x by running `cargo install --path src/tools/x`")
 }
