2bb42fab86a97fcbe09f791d406a7e9e22d54896
Jakob Degen <jakob.e.degen@gmail.com>
2022-12-06 01:18:24 -0800

    Retag argument to `drop_in_place` unconditionally

--- src/tools/miri/tests/fail/stacked_borrows/drop_in_place_retag.rs
+++ src/tools/miri/tests/fail/stacked_borrows/drop_in_place_retag.rs
@@ -3,16 +3,9 @@
 
 //@error-pattern: /retag .* for Unique permission .* only grants SharedReadOnly permission/
 
-#[repr(transparent)]
-struct HasDrop;
-
-impl Drop for HasDrop {
-    fn drop(&mut self) {}
-}
-
 fn main() {
     unsafe {
-        let x = (0u8, HasDrop);
+        let x = 0u8;
         let x = core::ptr::addr_of!(x);
         core::ptr::drop_in_place(x.cast_mut());
     }
