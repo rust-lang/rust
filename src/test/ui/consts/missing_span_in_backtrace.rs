8aff39130ff476f0949cd259478737762cc0bd30
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2022-12-07 09:19:49 +0000

    Avoid remapping paths back to `$SRC_DIR` in CI

--- src/test/ui/consts/missing_span_in_backtrace.rs
+++ src/test/ui/consts/missing_span_in_backtrace.rs
@@ -1,4 +1,4 @@
-// compile-flags: -Z simulate-remapped-rust-src-base=/rustc/xyz -Z translate-remapped-path-to-local-path=no
+// compile-flags: -Z simulate-remapped-rust-src-base=/rustc/xyz -Z translate-remapped-path-to-local-path=no -Z ui-testing=no
 // normalize-stderr-test "alloc[0-9]+" -> "ALLOC_ID"
 
 #![feature(const_swap)]
