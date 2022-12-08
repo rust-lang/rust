a99e97af97f5f86e1892ec2bf633105384ed8bee
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-12-08 01:30:07 -0500

    Add 0..=isize::MAX range metadata to size loads from vtables

--- src/test/codegen/dst-vtable-align-nonzero.rs
+++ src/test/codegen/dst-vtable-align-nonzero.rs
@@ -1,6 +1,7 @@
 // compile-flags: -O
 
 #![crate_type = "lib"]
+#![feature(core_intrinsics)]
 
 // This test checks that we annotate alignment loads from vtables with nonzero range metadata,
 // and that this allows LLVM to eliminate redundant `align >= 1` checks.
@@ -40,6 +41,23 @@ pub fn does_not_eliminate_runtime_check_when_align_2(
     // CHECK: {{icmp|llvm.umax}}
     // CHECK: ret
     &x.dst
+}
+
+// CHECK-LABEL: @align_load_from_align_of_val
+#[no_mangle]
+pub fn align_load_from_align_of_val(x: &dyn Trait) -> usize {
+    // CHECK: {{%[0-9]+}} = load [[USIZE]], {{.+}} !range [[RANGE_META]]
+    core::mem::align_of_val(x)
+}
+
+// CHECK-LABEL: @align_load_from_vtable_align_intrinsic
+#[no_mangle]
+pub unsafe fn align_load_from_vtable_align_intrinsic(x: &dyn Trait) -> usize {
+    let (data, vtable): (*const (), *const ()) = core::mem::transmute(x);
+    // CHECK: {{%[0-9]+}} = load [[USIZE]], {{.+}} !range [[RANGE_META]]
+    let align = core::intrinsics::vtable_align(vtable);
+    // make this function unique so it doesn't get merged with the previous
+    align + 1
 }
 
 // CHECK: [[RANGE_META]] = !{[[USIZE]] 1, [[USIZE]] 0}
