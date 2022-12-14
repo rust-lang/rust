0fb8d848f9d3b427fea754a5d944375e049f2eb4
Esteban Küber <esteban@kuber.com.ar>
2022-12-13 19:52:42 -0800

    Suggest `#[derive(Clone)]`

--- src/test/ui/suggestions/clone-on-unconstrained-borrowed-type-param.rs
+++ src/test/ui/suggestions/clone-on-unconstrained-borrowed-type-param.rs
@@ -3,6 +3,13 @@ fn wat<T>(t: &T) -> T {
     t.clone() //~ ERROR E0308
 }
 
+struct Foo;
+
+fn wut(t: &Foo) -> Foo {
+    t.clone() //~ ERROR E0308
+}
+
 fn main() {
     wat(&42);
+    wut(&Foo);
 }
