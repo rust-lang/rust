f2d03667914528a672c476b3718b0a0ead0736da
Thom Chiovoloni <thom@shift.click>
2022-12-13 20:53:14 -0800

    Use `expose_addr()` in `fmt::Pointer`

--- library/core/src/fmt/mod.rs
+++ library/core/src/fmt/mod.rs
@@ -2471,8 +2471,8 @@ impl Display for char {
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: ?Sized> Pointer for *const T {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        // Cast is needed here because `.addr()` requires `T: Sized`.
-        pointer_fmt_inner((*self as *const ()).addr(), f)
+        // Cast is needed here because `.expose_addr()` requires `T: Sized`.
+        pointer_fmt_inner((*self as *const ()).expose_addr(), f)
     }
 }
 
