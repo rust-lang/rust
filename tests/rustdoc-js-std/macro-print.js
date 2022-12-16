db558b46861ee2f812ee1cc2b03b796f691a246d
Michael Howell <michael@notriddle.com>
2022-12-16 13:19:38 -0700

    rustdoc: update search test cases

--- tests/rustdoc-js-std/macro-print.js
+++ tests/rustdoc-js-std/macro-print.js
@@ -3,8 +3,8 @@ const QUERY = 'macro:print';
 const EXPECTED = {
     'others': [
         { 'path': 'std', 'name': 'print' },
-        { 'path': 'std', 'name': 'eprint' },
         { 'path': 'std', 'name': 'println' },
+        { 'path': 'std', 'name': 'eprint' },
         { 'path': 'std', 'name': 'eprintln' },
     ],
 };
