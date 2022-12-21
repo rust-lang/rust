c74ca0ecc5ce1df21d2f331a7489388878de84c3
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2022-12-21 23:22:56 +0000

    Update track_caller tests; run fmt

--- src/test/ui/async-await/track-caller/async-closure-gate.rs
+++ src/test/ui/async-await/track-caller/async-closure-gate.rs
@@ -5,6 +5,5 @@
 fn main() {
     let _ = #[track_caller] async || {
         //~^ ERROR `#[track_caller]` on closures is currently unstable [E0658]
-        //~| ERROR `#[track_caller]` on closures is currently unstable [E0658]
     };
 }
