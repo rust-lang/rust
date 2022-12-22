f38bf5399d329497046f51fc06c0f27213de3044
Michael Goulet <michael@errs.io>
2022-12-22 17:59:26 +0000

    Take revision into account in src/test/ui incremental tests

--- src/tools/compiletest/src/common.rs
+++ src/tools/compiletest/src/common.rs
@@ -617,6 +617,6 @@ pub fn output_base_name(config: &Config, testpaths: &TestPaths, revision: Option
 
 /// Absolute path to the directory to use for incremental compilation. Example:
 ///   /path/to/build/host-triple/test/ui/relative/testname.mode/testname.inc
-pub fn incremental_dir(config: &Config, testpaths: &TestPaths) -> PathBuf {
-    output_base_name(config, testpaths, None).with_extension("inc")
+pub fn incremental_dir(config: &Config, testpaths: &TestPaths, revision: Option<&str>) -> PathBuf {
+    output_base_name(config, testpaths, revision).with_extension("inc")
 }
