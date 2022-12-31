a29425c6d41db7d356d4fc8acddc2e3a497961ab
Kathryn Long <squeeself@gmail.com>
2022-12-30 23:56:18 -0500

    Stabilize f16c_target_feature

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -238,7 +238,6 @@
 #![feature(arm_target_feature)]
 #![feature(avx512_target_feature)]
 #![feature(cmpxchg16b_target_feature)]
-#![feature(f16c_target_feature)]
 #![feature(hexagon_target_feature)]
 #![feature(mips_target_feature)]
 #![feature(powerpc_target_feature)]
@@ -247,6 +246,7 @@
 #![feature(sse4a_target_feature)]
 #![feature(tbm_target_feature)]
 #![feature(wasm_target_feature)]
+#![cfg_attr(bootstrap, feature(f16c_target_feature))]
 
 // allow using `core::` in intra-doc links
 #[allow(unused_extern_crates)]
