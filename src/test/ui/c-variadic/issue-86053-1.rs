db6d3b3c019daa22032887ba213e3b7b1c141b36
Michael Goulet <michael@errs.io>
2023-01-01 23:48:10 +0000

    Filter impl and where-clause candidates that reference errors

--- src/test/ui/c-variadic/issue-86053-1.rs
+++ src/test/ui/c-variadic/issue-86053-1.rs
@@ -2,7 +2,7 @@
 // error-pattern:unexpected `self` parameter in function
 // error-pattern:`...` must be the last argument of a C-variadic function
 // error-pattern:cannot find type `F` in this scope
-// error-pattern:in type `&'a &'b usize`, reference has a longer lifetime than the data it references
+
 
 #![feature(c_variadic)]
 #![crate_type="lib"]
