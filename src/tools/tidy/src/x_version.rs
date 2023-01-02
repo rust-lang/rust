e9ca6636e108cb4380d38a45c5ebb1d485079210
DebugSteven <debugsteven@gmail.com>
2023-01-02 16:36:29 -0700

    get latest x version from parsing cargo command

--- src/tools/tidy/src/x_version.rs
+++ src/tools/tidy/src/x_version.rs
@@ -1,4 +1,5 @@
-use semver::{BuildMetadata, Prerelease, Version};
+use semver::Version;
+use serde_json::Value;
 use std::io::ErrorKind;
 use std::process::{Command, Stdio};
 
@@ -33,20 +34,47 @@ pub fn check(bad: &mut bool) {
     if output.status.success() {
         let version = String::from_utf8_lossy(&output.stdout);
         let version = Version::parse(version.trim_end()).unwrap();
-        let expected = Version {
-            major: 0,
-            minor: 1,
-            patch: 0,
-            pre: Prerelease::new("").unwrap(),
-            build: BuildMetadata::EMPTY,
-        };
-        if version < expected {
+
+        if let Some(expected) = get_x_wrapper_version() {
+            if version < expected {
+                return tidy_error!(
+                    bad,
+                    "Current version of x is {version}, but the latest version is {expected}\nConsider updating to the newer version of x by running `cargo install --path src/tools/x`"
+                );
+            }
+        } else {
             return tidy_error!(
                 bad,
-                "Current version of x is {version}, but the latest version is {expected}\nConsider updating to the newer version of x by running `cargo install --path src/tools/x`"
+                "Unable to parse the latest version of `x` at `src/tools/x/Cargo.toml`"
             );
         }
     } else {
         return tidy_error!(bad, "failed to check version of `x`: {}", output.status);
     }
+}
+
+// Parse latest version out of `x` Cargo.toml
+fn get_x_wrapper_version() -> Option<Version> {
+    let cmd = Command::new("cargo")
+        .arg("metadata")
+        .args(["--no-deps", "--format-version", "1", "--manifest-path", "src/tools/x/Cargo.toml"])
+        .stdout(Stdio::piped())
+        .spawn();
+
+    let child = match cmd {
+        Ok(child) => child,
+        Err(e) => {
+            println!("failed to get version of `x`: {}", e);
+            return None;
+        }
+    };
+
+    let cargo_output = child.wait_with_output().unwrap();
+    let cargo_output_str =
+        String::from_utf8(cargo_output.stdout).expect("Unable to parse `src/tools/x/Cargo.toml`");
+
+    let v: Value = serde_json::from_str(&cargo_output_str).unwrap();
+    let vesrion_str = &v["packages"][0]["version"].as_str()?;
+
+    Some(Version::parse(vesrion_str).unwrap())
 }
