75b7c6c8ecc6865823e64ff6b2ab5b018c8ca15b
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2023-01-04 05:04:03 +0000

    Bless and update consts tests

--- tests/ui/consts/const-eval/issue-52475.rs
+++ tests/ui/consts/const-eval/issue-52475.rs
@@ -2,8 +2,8 @@ fn main() {
     let _ = [(); {
         let mut x = &0;
         let mut n = 0;
-        while n < 5 {
-            n = (n + 1) % 5; //~ ERROR evaluation of constant value failed
+        while n < 5 { //~ ERROR evaluation of constant value failed [E0080]
+            n = (n + 1) % 5;
             x = &0; // Materialize a new AllocId
         }
         0
