2b88d8bc1845385db6f786cb1f80e917af732463
Aaditya Dhruv <aadityadhruv@mailbox.org>
2023-01-05 16:14:03 +0530

    triagebot.rs fixes for blank struct

--- src/tools/tidy/src/triagebot.rs
+++ src/tools/tidy/src/triagebot.rs
@@ -1,21 +1,23 @@
-use std::path::Path;
 use crate::walk::{filter_dirs, walk};
+use serde::Deserialize;
+use std::path::Path;
+
+#[derive(Deserialize)]
+struct Config {}
 
 pub fn check(path: &Path, bad: &mut bool) {
-    walk(
-        path,
-        &mut |path| filter_dirs(path),
-        &mut |entry, contents| {
-            let file = entry.path();
-            let filename = file.file_name().unwrap();
-            if filename != "triagebot.toml" {
-                return;
+    walk(path, &mut |path| filter_dirs(path), &mut |entry, contents| {
+        let file = entry.path();
+        let filename = file.file_name().unwrap();
+        if filename != "triagebot.toml" {
+            return;
+        }
+        let conf: Result<Config, toml::de::Error> = toml::from_str(contents);
+        match conf {
+            Ok(_) => {}
+            Err(_err) => {
+                tidy_error!(bad, "{} is an invalid toml file", file.display())
             }
-            let conf : Config = toml::from_str(contents);
-            match conf {
-                Ok(_) => {},
-                Err(_err) => { tidy_error!(bad, "{} is an invalid toml file", file.display()) }
-            }
-        },
-    );
+        }
+    });
 }
