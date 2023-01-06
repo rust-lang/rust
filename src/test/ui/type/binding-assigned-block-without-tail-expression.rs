1fa6ada9dd3a17fc8c99b6b559a2835946803d21
Esteban Küber <esteban@kuber.com.ar>
2023-01-06 02:43:16 +0000

    Detect bindings assigned blocks without tail expressions in trait errors
    Address  #44173 for trait errors.

--- src/test/ui/type/binding-assigned-block-without-tail-expression.rs
+++ src/test/ui/type/binding-assigned-block-without-tail-expression.rs
@@ -1,3 +1,4 @@
+struct S;
 fn main() {
     let x = {
         println!("foo");
@@ -7,10 +8,15 @@ fn main() {
     let z = {
         "hi";
     };
+    let s = {
+        S;
+    };
     println!("{}", x); //~ ERROR E0277
     println!("{}", y); //~ ERROR E0277
     println!("{}", z); //~ ERROR E0277
+    println!("{}", s); //~ ERROR E0277
     let _: i32 = x; //~ ERROR E0308
     let _: i32 = y; //~ ERROR E0308
     let _: i32 = z; //~ ERROR E0308
+    let _: i32 = s; //~ ERROR E0308
 }
