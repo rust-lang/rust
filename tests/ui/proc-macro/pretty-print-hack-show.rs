496edf97c5c150967217f886e14d355c4f9890d0
Tim Neumann <timnn@google.com>
2023-01-06 10:37:53 +0000

    Update `rental` hack to work with remapped paths.

--- tests/ui/proc-macro/pretty-print-hack-show.rs
+++ tests/ui/proc-macro/pretty-print-hack-show.rs
@@ -1,5 +1,11 @@
 // aux-build:test-macros.rs
 // compile-flags: -Z span-debug
+// revisions: local remapped
+// [remapped]compile-flags: --remap-path-prefix={{src-base}}=remapped
+
+// The remapped paths are not normalized by compiletest.
+// normalize-stdout-test: "\\(proc-macro|pretty-print-hack)" -> "/$1"
+// normalize-stderr-test: "\\(proc-macro|pretty-print-hack)" -> "/$1"
 
 #![no_std] // Don't load unnecessary hygiene information from std
 extern crate std;
