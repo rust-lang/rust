504c832d0c171220b6bb9f087728cdfe5ae0c271
Max Fan <git@max.fan>
2023-01-12 17:58:17 -0500

    Comply with linter

--- tests/codegen/issue-97217.rs
+++ tests/codegen/issue-97217.rs
@@ -9,10 +9,10 @@
 pub fn issue97217() -> i32 {
     // drop_in_place should be inlined and never appear
     // CHECK-NOT: drop_in_place
-    
+
     // __rust_alloc should be optimized out
     // CHECK-NOT: __rust_alloc
-    
+
     let v1 = vec![5, 6, 7];
     let v1_iter = v1.iter();
     let total: i32 = v1_iter.sum();
