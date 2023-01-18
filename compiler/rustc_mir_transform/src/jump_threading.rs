e132cac3c45217f5f2b730ddd684fdd4700ffc4c
Camille GILLOT <gillot.camille@gmail.com>
2023-01-18 17:57:50 +0000

    Enable by default.

--- compiler/rustc_mir_transform/src/jump_threading.rs
+++ compiler/rustc_mir_transform/src/jump_threading.rs
@@ -60,7 +60,7 @@ const MAX_PLACES: usize = 100;
 
 impl<'tcx> MirPass<'tcx> for JumpThreading {
     fn is_enabled(&self, sess: &rustc_session::Session) -> bool {
-        sess.mir_opt_level() >= 4
+        sess.mir_opt_level() >= 2
     }
 
     #[instrument(skip_all level = "debug")]
