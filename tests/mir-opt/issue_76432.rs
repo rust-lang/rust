a9aed861acee892602a03be72624ab7895e1db61
Camille GILLOT <gillot.camille@gmail.com>
2023-01-21 20:51:21 +0000

    Reimplement NormalizeArrayLen.

--- tests/mir-opt/issue_76432.rs
+++ tests/mir-opt/issue_76432.rs
@@ -1,3 +1,4 @@
+// compile-flags: -Zmir-enable-passes=-NormalizeArrayLen
 // Check that we do not insert StorageDead at each target if StorageDead was never seen
 
 // EMIT_MIR issue_76432.test.SimplifyComparisonIntegral.diff
