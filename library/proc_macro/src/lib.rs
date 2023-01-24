43855d4f1c864d8a44cec66aa7fe38e2111ad2c3
Peter Kehl <peter.kehl@gmail.com>
2023-01-24 11:58:53 -0800

    CoAlloc: library/proc_macro (+related library/alloc): Experimenting with importing DEFAULT_COOP_PREFERRED

--- library/proc_macro/src/lib.rs
+++ library/proc_macro/src/lib.rs
@@ -33,6 +33,7 @@
 #![feature(min_specialization)]
 #![feature(strict_provenance)]
 #![recursion_limit = "256"]
+#![feature(global_co_alloc_def)]
 
 #[unstable(feature = "proc_macro_internals", issue = "27812")]
 #[doc(hidden)]
