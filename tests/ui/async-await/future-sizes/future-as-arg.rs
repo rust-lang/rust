ba685ee7532b0b2ff6d1a549807a203726019e91
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2023-01-31 17:04:18 +0000

    Update based on PR comments

--- tests/ui/async-await/future-sizes/future-as-arg.rs
+++ tests/ui/async-await/future-sizes/future-as-arg.rs
@@ -1,5 +1,6 @@
 // edition: 2021
 // run-pass
+// ignore-pass
 
 async fn test(_arg: [u8; 8192]) {}
 
@@ -8,12 +9,12 @@ async fn use_future(fut: impl std::future::Future<Output = ()>) {
 }
 
 fn main() {
-    let n = std::mem::size_of_val(&use_future(use_future(use_future(use_future(use_future(
+    let actual = std::mem::size_of_val(&use_future(use_future(use_future(use_future(use_future(
             use_future(use_future(use_future(use_future(use_future(test(
                 [0; 8192]
             ))))))
         ))))));
-    println!("size is: {n}");
     // Not putting an exact number in case it slightly changes over different commits
-    assert!(n > 8000000);
+    let expected = 8000000;
+    assert!(actual > expected, "expected: >{expected}, actual: {actual}");
 }
