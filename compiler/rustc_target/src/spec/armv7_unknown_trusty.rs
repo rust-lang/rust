12053c9f64c99db13761509b0a31118d3ac9a2ac
Nicole L <dlegare.1001@gmail.com>
2023-01-31 11:53:23 -0800

    Address review feedback

--- compiler/rustc_target/src/spec/armv7_unknown_trusty.rs
+++ compiler/rustc_target/src/spec/armv7_unknown_trusty.rs
@@ -7,23 +7,24 @@ pub fn target() -> Target {
         // It's important we use "gnueabi" and not "musleabi" here. LLVM uses it
         // to determine the calling convention and float ABI, and it doesn't
         // support the "musleabi" value.
-        llvm_target: "armv7-unknown-linux-gnueabi".into(),
+        llvm_target: "armv7-unknown-unknown-gnueabi".into(),
         pointer_width: 32,
         data_layout: "e-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64".into(),
         arch: "arm".into(),
         options: TargetOptions {
             abi: "eabi".into(),
-            features: "+v7,+thumb2,+soft-float,-neon".into(),
+            features: "+v7,+thumb2,+soft-float,-neon,+reserve-x18".into(),
             max_atomic_width: Some(64),
             mcount: "\u{1}mcount".into(),
             os: "trusty".into(),
-            env: "musl".into(),
             link_self_contained: LinkSelfContainedDefault::Musl,
             dynamic_linking: false,
             executables: true,
+            crt_static_default: true,
+            crt_static_respected: false,
             relro_level: RelroLevel::Full,
             panic_strategy: PanicStrategy::Abort,
-            position_independent_executables: true,
+            static_position_independent_executables: true,
 
             ..Default::default()
         },
