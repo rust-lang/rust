776918971d3320d6858da12112343753b1dd875e
Bryan Garza <1396101+bryangarza@users.noreply.github.com>
2023-02-01 21:09:24 +0000

    Update test to not trigger stack overflow

--- tests/ui/async-await/future-sizes/future-as-arg.rs
+++ tests/ui/async-await/future-sizes/future-as-arg.rs
@@ -1,19 +1,16 @@
 // edition: 2021
 // run-pass
 
-async fn test(_arg: [u8; 8192]) {}
+async fn test(_arg: [u8; 16]) {}
 
 async fn use_future(fut: impl std::future::Future<Output = ()>) {
     fut.await
 }
 
 fn main() {
-    let actual = std::mem::size_of_val(&use_future(use_future(use_future(use_future(use_future(
-            use_future(use_future(use_future(use_future(use_future(test(
-                [0; 8192]
-            ))))))
-        ))))));
-    // Not putting an exact number in case it slightly changes over different commits
-    let expected = 8000000;
+    let actual = std::mem::size_of_val(
+        &use_future(use_future(use_future(use_future(use_future(test([0; 16])))))));
+    // Not using an exact number in case it slightly changes over different commits
+    let expected = 550;
     assert!(actual > expected, "expected: >{expected}, actual: {actual}");
 }
