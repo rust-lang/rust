e05cbfe9dd8ce9077d90811c1ab81ba672165edf
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-02-02 17:37:01 +0000

    bug: nesting pattern types in enums doesn't use layout optimizations

--- tests/ui/type/pattern_types/range_patterns.rs
+++ tests/ui/type/pattern_types/range_patterns.rs
@@ -1,8 +1,15 @@
-#![feature(pattern_types)]
+#![feature(pattern_types, rustc_attrs)]
 #![allow(incomplete_features)]
 
-// check-pass
+#[rustc_layout(debug)]
+type X = std::num::NonZeroU32; //~ ERROR layout_of
+#[rustc_layout(debug)]
+type Y = u32 is 1..; //~ ERROR layout_of
+#[rustc_layout(debug)]
+type Z = Option<u32 is 1..>; //~ ERROR layout_of
+#[rustc_layout(debug)]
+type A = Option<std::num::NonZeroU32>; //~ ERROR layout_of
 
 fn main() {
-    let x: u32 is 1.. = todo!();
+    let x: u32 is 1.. = unsafe { std::mem::transmute(42_u32) };
 }
