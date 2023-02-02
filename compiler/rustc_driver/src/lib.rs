2b8f8922ee5852f97e92082b209c0f4d940a6edb
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2023-02-02 07:47:39 +0100

    Remove unneeded extern crate

--- compiler/rustc_driver/src/lib.rs
+++ compiler/rustc_driver/src/lib.rs
@@ -1,7 +1,4 @@
 // This crate is intentionally empty and a rexport of `rustc_driver_impl` to allow the code in
 // `rustc_driver_impl` to be compiled in parallel with other crates.
 
-#![allow(unused_extern_crates)]
-extern crate rustc_driver_impl;
-
 pub use rustc_driver_impl::*;
