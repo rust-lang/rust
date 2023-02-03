1bc80e87f199b82638a0a95f8d50fac61ffab7cc
Lorenz Schmidt <lorenz.schmidt@mailbox.org>
2023-02-03 09:57:14 +0100

    Only set inline attribute

--- compiler/rustc_codegen_llvm/src/attributes.rs
+++ compiler/rustc_codegen_llvm/src/attributes.rs
@@ -297,7 +297,7 @@ pub fn from_fn_attrs<'ll, 'tcx>(
     if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::FFI_CONST) {
         to_add.push(AttributeKind::ReadNone.create_attr(cx.llcx));
     }
-    if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::NAKED)  || autodiff_attrs.is_active(){
+    if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::NAKED) {
         to_add.push(AttributeKind::Naked.create_attr(cx.llcx));
     }
     if codegen_fn_attrs.flags.contains(CodegenFnAttrFlags::ALLOCATOR) {
