b7db632d1e4357f44ee1f30114de95e569cf1cd6
austaras <austaras@outlook.com>
2023-02-05 08:13:08 +0800

    Update test

--- src/tools/rust-analyzer/lib/smol_str/tests/test.rs
+++ src/tools/rust-analyzer/lib/smol_str/tests/test.rs
@@ -55,7 +55,7 @@ fn check_props(std_str: &str, smol: SmolStr) -> Result<(), proptest::test_runner
     prop_assert_eq!(smol.as_str(), std_str);
     prop_assert_eq!(smol.len(), std_str.len());
     prop_assert_eq!(smol.is_empty(), std_str.is_empty());
-    if smol.len() <= 22 {
+    if smol.len() <= 23 {
         prop_assert!(!smol.is_heap_allocated());
     }
     Ok(())
@@ -218,7 +218,7 @@ fn test_from_char_iterator() {
     // String which has too many characters to even consider inlining: Chars::size_hint uses
     // (`len` + 3) / 4. With `len` = 89, this results in 23, so `from_iter` will immediately
     // heap allocate
-    let raw: String = std::iter::repeat('a').take(22 * 4 + 1).collect();
+    let raw: String = std::iter::repeat('a').take(23 * 4 + 1).collect();
     let s: SmolStr = raw.chars().collect();
     assert_eq!(s.as_str(), raw);
     assert!(s.is_heap_allocated());
