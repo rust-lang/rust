ad3f9df22fa30dba083b1b2fc3c9aeda061ed3eb
Oskar Goldhahn <oskar@goldhahn.info>
2023-02-06 23:26:12 +0100

    sanity check

--- compiler/rustc_ty_utils/src/layout.rs
+++ compiler/rustc_ty_utils/src/layout.rs
@@ -59,6 +59,9 @@ fn layout_of<'tcx>(
 
     sanity_check_layout(&cx, &layout);
 
+    // FIXME: move this into the sanity check.
+    debug_assert_eq!(tcx.align_of(query).unwrap(), layout.align);
+
     Ok(layout)
 }
 
