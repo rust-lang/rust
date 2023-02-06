e58bb2acbd3a2c61d4c492c322e9c53ed3068489
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-02-06 16:58:05 +0000

    Use new abi for 128bit int to float cast intrinsics on Windows

--- src/lib.rs
+++ src/lib.rs
@@ -188,7 +188,7 @@ impl CodegenBackend for CraneliftCodegenBackend {
     }
 
     fn target_features(&self, _sess: &Session, _allow_unstable: bool) -> Vec<rustc_span::Symbol> {
-        vec![]
+        vec![Symbol::intern("llvm14-builtins-abi")]
     }
 
     fn print_version(&self) {
