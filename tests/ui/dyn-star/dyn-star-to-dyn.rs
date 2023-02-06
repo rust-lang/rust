57056d7f8f79bf96ad99f562d00001451dc8fb4a
Ralf Jung <post@ralfj.de>
2023-02-06 16:18:09 +0100

    slightly beef up dyn-star-to-dyn test

--- tests/ui/dyn-star/dyn-star-to-dyn.rs
+++ tests/ui/dyn-star/dyn-star-to-dyn.rs
@@ -1,9 +1,17 @@
-// build-pass
+// run-pass
 
 #![feature(dyn_star)]
 //~^ WARN the feature `dyn_star` is incomplete and may not be safe to use and/or cause compiler crashes
 
+use std::fmt::Debug;
+
 fn main() {
-    let x: dyn* Send = &();
-    let x = Box::new(x) as Box<dyn Send>;
+    let x: dyn* Debug = &42;
+    let x = Box::new(x) as Box<dyn Debug>;
+    assert_eq!("42", format!("{x:?}"));
+
+    // Also test opposite direction.
+    let x: Box<dyn Debug> = Box::new(42);
+    let x = &x as dyn* Debug;
+    assert_eq!("42", format!("{x:?}"));
 }
