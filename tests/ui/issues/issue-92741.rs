039f70e926a859669fb046df4bd8115cad850113
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-02-06 15:30:29 +0100

    Add more test cases to tests/ui/issues/issue-92741.rs

--- tests/ui/issues/issue-92741.rs
+++ tests/ui/issues/issue-92741.rs
@@ -4,3 +4,13 @@ fn foo() -> bool {
     mut
     if true { true } else { false }
 }
+
+fn bar() -> bool {
+    &  //~ ERROR 9:5: 10:40: mismatched types [E0308]
+    mut if true { true } else { false }
+}
+
+fn baz() -> bool {
+    & mut //~ ERROR 14:5: 15:36: mismatched types [E0308]
+    if true { true } else { false }
+}
