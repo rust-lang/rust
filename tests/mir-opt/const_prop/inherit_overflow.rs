e34caaf42dd10b3cb8d14e6e99c2c8687fe6342e
Camille GILLOT <gillot.camille@gmail.com>
2023-02-08 20:28:39 +0000

    Remove overflow checks from ConstProp.

--- tests/mir-opt/const_prop/inherit_overflow.rs
+++ tests/mir-opt/const_prop/inherit_overflow.rs
@@ -1,7 +1,7 @@
-// unit-test: DataflowConstProp
+// unit-test: ConstProp
 // compile-flags: -Zmir-enable-passes=+Inline
 
-// EMIT_MIR inherit_overflow.main.DataflowConstProp.diff
+// EMIT_MIR inherit_overflow.main.ConstProp.diff
 fn main() {
     // After inlining, this will contain a `CheckedBinaryOp`.
     // Propagating the overflow is ok as codegen will just skip emitting the panic.
