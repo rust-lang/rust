805a0ae2df4d85ec88497e32adcd1bb24a8729d1
Mara Bos <m-ou.se@m-ou.se>
2023-02-10 22:35:23 +0100

    Add more test cases for suspicious_command_arg_space.

--- tests/ui/suspicious_command_arg_space.rs
+++ tests/ui/suspicious_command_arg_space.rs
@@ -1,5 +1,10 @@
 fn main() {
-    std::process::Command::new("echo").arg("hello world").spawn().unwrap();
+    // Things it should warn about:
     std::process::Command::new("echo").arg("-n hello").spawn().unwrap();
     std::process::Command::new("cat").arg("--number file").spawn().unwrap();
+
+    // Things it should not warn about:
+    std::process::Command::new("echo").arg("hello world").spawn().unwrap();
+    std::process::Command::new("a").arg("--fmt=%a %b %c").spawn().unwrap();
+    std::process::Command::new("b").arg("-ldflags=-s -w").spawn().unwrap();
 }
