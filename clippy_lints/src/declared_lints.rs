8f56767c94a9ccadcd5cb6c495fe7f646e5221a7
Mara Bos <m-ou.se@m-ou.se>
2023-02-10 19:03:20 +0100

    Update lints.

--- clippy_lints/src/declared_lints.rs
+++ clippy_lints/src/declared_lints.rs
@@ -378,6 +378,7 @@ pub(crate) static LINTS: &[&crate::LintInfo] = &[
     crate::methods::SKIP_WHILE_NEXT_INFO,
     crate::methods::STABLE_SORT_PRIMITIVE_INFO,
     crate::methods::STRING_EXTEND_CHARS_INFO,
+    crate::methods::SUSPICIOUS_COMMAND_ARG_SPACE_INFO,
     crate::methods::SUSPICIOUS_MAP_INFO,
     crate::methods::SUSPICIOUS_SPLITN_INFO,
     crate::methods::SUSPICIOUS_TO_OWNED_INFO,
