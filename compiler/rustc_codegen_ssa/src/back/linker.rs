8c69f740dae426e7dc9e5ccac6cf2d3017f16832
Dan Gohman <dev@sunfishcode.online>
2023-02-14 13:10:09 -0800

    Change Wasm's `cdylib` etc. to be a "reactor".
    Use `--entry` `_initialize` on `cdylib` and similar library types on Wasm
    targets.
    
    This is a change in how constructor functions have at least sometimes been
    handled. It will mean that there's an exported `_initialize` function which
    calls the static constructors, and it must be called from the outside before
    any other export is accessed. Rust doesn't officially have static
    constructors, but C++ does and C has not-uncommonly-used extensions that do,
    and there are crates in Rust that provide static-constructor functionality.
    
    Some Wasm embeddings automatically call the `_initialize` function if
    present because it is part of the [WASI Preview1 application ABI], though
    not all do.
    
    This does not implement support for dynamic linking. The format and behavior
    of `cdylib` and similar outputs may change in future compiler versions,
    especially as Wasm gains a stable dynamic linking format.
    
    What this does do, is make `cdylib` work more like what many people have
    been assuming `cylib` does on Wasm. It produces a Wasm module that
    doesn't have a main function that you can instantiate and call exports
    on, but which now also supports static constructors by having an `_initialize`
    function that you or your engine must call first.
    
    [WASI Preview1 application ABI]: https://github.com/WebAssembly/WASI/blob/main/legacy/application-abi.md

--- compiler/rustc_codegen_ssa/src/back/linker.rs
+++ compiler/rustc_codegen_ssa/src/back/linker.rs
@@ -1207,10 +1207,9 @@ impl<'a> Linker for WasmLd<'a> {
             | LinkOutputKind::DynamicPicExe
             | LinkOutputKind::StaticNoPicExe
             | LinkOutputKind::StaticPicExe => {}
-            LinkOutputKind::DynamicDylib | LinkOutputKind::StaticDylib => {
-                self.cmd.arg("--no-entry");
-            }
-            LinkOutputKind::WasiReactorExe => {
+            LinkOutputKind::DynamicDylib
+            | LinkOutputKind::StaticDylib
+            | LinkOutputKind::WasiReactorExe => {
                 self.cmd.arg("--entry");
                 self.cmd.arg("_initialize");
             }
