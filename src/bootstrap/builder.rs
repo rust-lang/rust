238d8e3ec79bfc428cb60c64d88ad38bb38a1bd5
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-02-14 19:39:39 +0000

    Allow testing cg_clif using ./x.py test

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -737,6 +737,7 @@ impl<'a> Builder<'a> {
                 test::Incremental,
                 test::Debuginfo,
                 test::UiFullDeps,
+                test::CodegenCranelift,
                 test::Rustdoc,
                 test::RunCoverageRustdoc,
                 test::Pretty,
