d2ec961a675addb47b3307bdd2a5267e00d8cc3e
Alan Egerton <eggyal@gmail.com>
2023-02-26 00:40:02 +0000

    Use specialisation in ParamEnv traversable impls

--- compiler/rustc_middle/src/ty/mod.rs
+++ compiler/rustc_middle/src/ty/mod.rs
@@ -1627,17 +1627,31 @@ impl<'tcx> TypeFoldable<TyCtxt<'tcx>> for ParamEnv<'tcx> {
         folder: &mut F,
     ) -> Result<Self, F::Error> {
         Ok(ParamEnv::new(
-            self.caller_bounds().try_fold_with(folder)?,
-            self.reveal().try_fold_with(folder)?,
-            self.constness(),
+            rustc_type_ir::prefer_noop_traversal_if_applicable!(
+                { self.caller_bounds() }.try_fold_with(folder)
+            )?,
+            rustc_type_ir::prefer_noop_traversal_if_applicable!(
+                { self.reveal() }.try_fold_with(folder)
+            )?,
+            rustc_type_ir::prefer_noop_traversal_if_applicable!(
+                { self.constness() }.try_fold_with(folder)
+            )?,
         ))
     }
 }
 
 impl<'tcx> TypeVisitable<TyCtxt<'tcx>> for ParamEnv<'tcx> {
     fn visit_with<V: TypeVisitor<TyCtxt<'tcx>>>(&self, visitor: &mut V) -> ControlFlow<V::BreakTy> {
-        self.caller_bounds().visit_with(visitor)?;
-        self.reveal().visit_with(visitor)
+        rustc_type_ir::prefer_noop_traversal_if_applicable!(
+            { &self.caller_bounds() }.visit_with(visitor)
+        )?;
+        rustc_type_ir::prefer_noop_traversal_if_applicable!(
+            { &self.reveal() }.visit_with(visitor)
+        )?;
+        rustc_type_ir::prefer_noop_traversal_if_applicable!(
+            { &self.constness() }.visit_with(visitor)
+        )?;
+        ControlFlow::Continue(())
     }
 }
 
