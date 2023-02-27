8b186dfdb70ebfe722975970458c6230841e6911
clubby789 <jamie@hill-daniel.co.uk>
2023-02-27 13:22:43 +0000

    Add recovery for use of removed `box` syntax

--- tests/ui/parser/removed-syntax-box.rs
+++ tests/ui/parser/removed-syntax-box.rs
@@ -1,10 +1,14 @@
+// run-rustfix
+
 fn main() {
+    #[allow(dead_code)]
     struct T {
         a: u8,
         b: u8,
     }
-    let _ = box () //~ ERROR expected expression, found reserved keyword `box`
-    let _ = box 1;
-    let _ = box T { a: 12, b: 18 };
-    let _ = box [5; 30];
+    let _ = box (); //~ ERROR `box_syntax` has been removed
+    let _ = box 1; //~ ERROR `box_syntax` has been removed
+    let _ = box T { a: 12, b: 18 }; //~ ERROR `box_syntax` has been removed
+    let _ = box [5; 30]; //~ ERROR `box_syntax` has been removed
+    let _: Box<()> = box (); //~ ERROR `box_syntax` has been removed
 }
