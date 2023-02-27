e5189cc7e45991e1c6e9a4218c80d54441b346a9
Michael Goulet <michael@errs.io>
2023-02-27 20:47:06 +0000

    Nested impl traits trigger opaque_hidden_inferred_bound too much

--- tests/ui/impl-trait/nested-return-type2.rs
+++ tests/ui/impl-trait/nested-return-type2.rs
@@ -26,7 +26,6 @@ impl<R: Duh, F: FnMut() -> R> Trait for F {
 // Lazy TAIT would error out, but we inserted a hack to make it work again,
 // keeping backwards compatibility.
 fn foo() -> impl Trait<Assoc = impl Send> {
-    //~^ WARN opaque type `impl Trait<Assoc = impl Send>` does not satisfy its associated type bounds
     || 42
 }
 
