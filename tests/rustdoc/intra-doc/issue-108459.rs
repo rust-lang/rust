bf77fed46371c858eb2c903cbbe8f3d3f3530bd7
benediktwerner <1benediktwerner@gmail.com>
2023-02-27 13:05:35 +0100

    Fix and format test

--- tests/rustdoc/intra-doc/issue-108459.rs
+++ tests/rustdoc/intra-doc/issue-108459.rs
@@ -8,28 +8,30 @@ pub mod char {}
 /// See [S] and [`S`]
 pub struct MyStruct1;
 
-// Ensure that link texts are replaced correctly even if there are multiple links with the same target but different text
+// Ensure that link texts are replaced correctly even if there are multiple links with
+// the same target but different text
 
 /// See also [crate::char] and [mod@char] and [prim@char]
-// @has prim_precedence/struct.MyStruct2.html '//*[@href="char/index.html"]' 'crate::char'
+// @has issue-108459/struct.MyStruct2.html '//*[@href="char/index.html"]' 'crate::char'
 // @has - '//*[@href="char/index.html"]' 'char'
 // @has - '//*[@href="{{channel}}/std/primitive.char.html"]' 'char'
 pub struct MyStruct2;
 
 /// See also [mod@char] and [prim@char] and [crate::char]
-// @has prim_precedence/struct.MyStruct3.html '//*[@href="char/index.html"]' 'crate::char'
+// @has issue-108459/struct.MyStruct3.html '//*[@href="char/index.html"]' 'crate::char'
 // @has - '//*[@href="char/index.html"]' 'char'
 // @has - '//*[@href="{{channel}}/std/primitive.char.html"]' 'char'
 pub struct MyStruct3;
 
-// Ensure that links are correct even if there are multiple links with the same text but different targets
+// Ensure that links are correct even if there are multiple links with the same text but
+// different targets
 
 /// See also [char][mod@char] and [char][prim@char]
-// @has prim_precedence/struct.MyStruct4.html '//*[@href="char/index.html"]' 'char'
+// @has issue-108459/struct.MyStruct4.html '//*[@href="char/index.html"]' 'char'
 // @has - '//*[@href="{{channel}}/std/primitive.char.html"]' 'char'
 pub struct MyStruct4;
 
 /// See also [char][prim@char] and [char][crate::char]
-// @has prim_precedence/struct.MyStruct5.html '//*[@href="char/index.html"]' 'char'
+// @has issue-108459/struct.MyStruct5.html '//*[@href="char/index.html"]' 'char'
 // @has - '//*[@href="{{channel}}/std/primitive.char.html"]' 'char'
 pub struct MyStruct5;
