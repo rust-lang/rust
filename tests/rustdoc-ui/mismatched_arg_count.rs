d1b6aa6834744ec653a8de9f2f4ef25612e520e3
Patrik Kårlin <patrik.karlin@gmail.com>
2023-03-01 10:43:41 +0100

    rustdoc: remove excess from rustdoc test

--- tests/rustdoc-ui/mismatched_arg_count.rs
+++ tests/rustdoc-ui/mismatched_arg_count.rs
@@ -1,5 +1,3 @@
-// ensures that we don't ICE when there are too many args supplied to the alias.
-
 trait Trait<'a> {
     type Assoc;
 }
@@ -8,5 +6,3 @@ type Alias<'a, T> = <T as Trait<'a>>::Assoc;
 
 fn bar<'a, T: Trait<'a>>(_: Alias<'a, 'a, T>) {}
 //~^ error: type alias takes 1 lifetime argument but 2 lifetime arguments were supplied
-
-fn main() {}
