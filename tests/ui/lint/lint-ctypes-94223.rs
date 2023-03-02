c75b080d7d878740c49539413d1b000906d420d4
David Wood <david.wood@huawei.com>
2023-03-02 11:23:44 +0000

    lint/ctypes: multiple external fn-ptrs in ty
    Extend previous commit's support for checking for external fn-ptrs in
    internal fn types to report errors for multiple found fn-ptrs.
    
    Signed-off-by: David Wood <david.wood@huawei.com>

--- tests/ui/lint/lint-ctypes-94223.rs
+++ tests/ui/lint/lint-ctypes-94223.rs
@@ -3,3 +3,7 @@
 
 pub fn bad(f: extern "C" fn([u8])) {}
 //~^ ERROR `extern` fn uses type `[u8]`, which is not FFI-safe
+
+pub fn bad_twice(f: Result<extern "C" fn([u8]), extern "C" fn([u8])>) {}
+//~^ ERROR `extern` fn uses type `[u8]`, which is not FFI-safe
+//~^^ ERROR `extern` fn uses type `[u8]`, which is not FFI-safe
