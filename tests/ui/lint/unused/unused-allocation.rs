a90abd64fbb0764007d93d60823161f05f564259
Maybe Waffle <waffle.lapkin@gmail.com>
2023-03-03 19:02:35 +0000

    Remove `feature(box_syntax)` from unused allocation list test

--- tests/ui/lint/unused/unused-allocation.rs
+++ tests/ui/lint/unused/unused-allocation.rs
@@ -1,7 +1,7 @@
-#![feature(box_syntax)]
+#![feature(rustc_attrs, stmt_expr_attributes)]
 #![deny(unused_allocation)]
 
 fn main() {
-    _ = (box [1]).len(); //~ error: unnecessary allocation, use `&` instead
+    _ = (#[rustc_box] Box::new([1])).len(); //~ error: unnecessary allocation, use `&` instead
     _ = Box::new([1]).len(); //~ error: unnecessary allocation, use `&` instead
 }
