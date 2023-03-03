130bcfdb3d5f3dbc470466b8128afa91e48ba14d
Santiago Pastorino <spastorino@gmail.com>
2023-03-03 12:34:16 -0300

    Map to new synthesized assoc ty for RPITITs in astconv

--- compiler/rustc_hir_analysis/src/astconv/mod.rs
+++ compiler/rustc_hir_analysis/src/astconv/mod.rs
@@ -3049,10 +3049,17 @@ impl<'o, 'tcx> dyn AstConv<'tcx> + 'o {
             }
             &hir::TyKind::OpaqueDef(item_id, lifetimes, in_trait) => {
                 let opaque_ty = tcx.hir().item(item_id);
-                let def_id = item_id.owner_id.to_def_id();
 
                 match opaque_ty.kind {
                     hir::ItemKind::OpaqueTy(hir::OpaqueTy { origin, .. }) => {
+                        let local_def_id = item_id.owner_id.def_id;
+                        let def_id = if in_trait
+                            && tcx.sess.opts.unstable_opts.lower_impl_trait_in_trait_to_assoc_ty
+                        {
+                            tcx.associated_item_for_impl_trait_in_trait(local_def_id).to_def_id()
+                        } else {
+                            local_def_id.to_def_id()
+                        };
                         self.impl_trait_ty_to_ty(def_id, lifetimes, origin, in_trait)
                     }
                     ref i => bug!("`impl Trait` pointed to non-opaque type?? {:#?}", i),
