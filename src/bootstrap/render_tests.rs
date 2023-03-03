9a1ff1b95808c6639b8d00f46f675daafad566e7
Pietro Albini <pietro.albini@ferrous-systems.com>
2023-03-03 10:03:23 +0100

    handle non-json output in stdout

--- src/bootstrap/render_tests.rs
+++ src/bootstrap/render_tests.rs
@@ -105,12 +105,19 @@ impl<'a> Renderer<'a> {
                 break;
             }
 
-            self.render_message(match serde_json::from_str(&line) {
-                Ok(parsed) => parsed,
-                Err(err) => {
-                    panic!("failed to parse libtest json output; error: {err}, line: {line:?}");
-                }
-            });
+            let trimmed = line.trim();
+            if trimmed.starts_with("{") && trimmed.ends_with("}") {
+                self.render_message(match serde_json::from_str(&trimmed) {
+                    Ok(parsed) => parsed,
+                    Err(err) => {
+                        panic!("failed to parse libtest json output; error: {err}, line: {line:?}");
+                    }
+                });
+            } else {
+                // Handle non-JSON output, for example when --nocapture is passed.
+                print!("{line}");
+                let _ = std::io::stdout().flush();
+            }
         }
     }
 
