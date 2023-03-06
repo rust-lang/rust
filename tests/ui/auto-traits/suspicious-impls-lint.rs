5c0f55d508b49dbea5b5d69a41d998a00e7d95d2
Mu42 <mu001999@outlook.com>
2023-03-06 21:05:23 +0800

    Moves the negative impls into a separate test file

--- tests/ui/auto-traits/suspicious-impls-lint.rs
+++ tests/ui/auto-traits/suspicious-impls-lint.rs
@@ -1,4 +1,3 @@
-#![feature(negative_impls)]
 #![deny(suspicious_auto_trait_impls)]
 
 use std::marker::PhantomData;
@@ -22,9 +21,6 @@ struct ContainsVec<T>(Vec<T>);
 unsafe impl Send for ContainsVec<i32> {}
 //~^ ERROR
 //~| WARNING this will change its meaning
-impl !Send for ContainsVec<u32> {}
-//~^ ERROR
-//~| WARNING this will change its meaning
 
 struct TwoParams<T, U>(T, U);
 unsafe impl<T: Send, U: Send> Send for TwoParams<T, U> {} // ok
@@ -44,16 +40,10 @@ pub struct WithPhantomDataSend<T, U>(PhantomData<T>, U);
 unsafe impl<T> Send for WithPhantomDataSend<*const T, i8> {}
 //~^ ERROR
 //~| WARNING this will change its meaning
-impl<T> !Send for WithPhantomDataSend<*const T, u8> {}
-//~^ ERROR
-//~| WARNING this will change its meaning
 
 pub struct WithLifetime<'a, T>(&'a (), T);
 unsafe impl<T> Send for WithLifetime<'static, T> {} // ok
 unsafe impl<T> Sync for WithLifetime<'static, Vec<T>> {}
-//~^ ERROR
-//~| WARNING this will change its meaning
-impl<T> !Sync for WithLifetime<'static, Option<T>> {}
 //~^ ERROR
 //~| WARNING this will change its meaning
 
