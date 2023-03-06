9df8da8e04d04492d57603d69d1b9c330b4424f6
Rémy Rakic <remy.rakic+github@gmail.com>
2023-03-06 12:32:45 +0000

    fix ignore header in MSVC test

--- tests/ui/panic-runtime/unwind-tables-target-required.rs
+++ tests/ui/panic-runtime/unwind-tables-target-required.rs
@@ -1,7 +1,7 @@
 // Tests that the compiler errors if the user tries to turn off unwind tables
 // when they are required.
 //
-// only-x86_64-windows-msvc
+// only-x86_64-pc-windows-msvc
 // compile-flags: -C force-unwind-tables=no
 //
 // error-pattern: target requires unwind tables, they cannot be disabled with `-C force-unwind-tables=no`.
