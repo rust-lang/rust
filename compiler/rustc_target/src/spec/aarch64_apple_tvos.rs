abb191168222e57c2adb3874b7955b37d3021abb
Thom Chiovoloni <thom@shift.click>
2023-03-14 21:24:15 -0700

    Fix the tvOS targets to use the right LLVM target and respect the deployment target environment variables

--- compiler/rustc_target/src/spec/aarch64_apple_tvos.rs
+++ compiler/rustc_target/src/spec/aarch64_apple_tvos.rs
@@ -1,10 +1,10 @@
-use super::apple_base::{opts, Arch};
+use super::apple_base::{opts, tvos_llvm_target, Arch};
 use crate::spec::{FramePointer, Target, TargetOptions};
 
 pub fn target() -> Target {
     let arch = Arch::Arm64;
     Target {
-        llvm_target: "arm64-apple-tvos".into(),
+        llvm_target: tvos_llvm_target(arch).into(),
         pointer_width: 64,
         data_layout: "e-m:o-i64:64-i128:128-n32:64-S128".into(),
         arch: arch.target_arch(),
