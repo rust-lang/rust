76b0cf812b70b458cbae480af75d788a29a43adc
Michael Goulet <michael@errs.io>
2023-03-21 18:06:04 +0000

    Walk return-position impl trait in trait deeply in associated_item_def_ids

--- tests/ui/impl-trait/in-trait/auxiliary/rpitit.rs
+++ tests/ui/impl-trait/in-trait/auxiliary/rpitit.rs
@@ -2,12 +2,13 @@
 
 #![feature(return_position_impl_trait_in_trait)]
 
+use std::ops::Deref;
+
 pub trait Foo {
-    fn bar() -> impl Sized;
+    fn bar() -> impl Deref<Target = impl Sized>;
 }
 
 pub struct Foreign;
-
 impl Foo for Foreign {
-    fn bar() {}
+    fn bar() -> &'static () { &() }
 }
