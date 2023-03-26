257d9005b4e86078a6cea46e1e0958f0d10b537d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-03-26 11:02:56 +0000

    Also include MIR statements in clif ir dumps by default

--- src/base.rs
+++ src/base.rs
@@ -502,7 +502,6 @@ fn codegen_stmt<'tcx>(
 
     fx.set_debug_loc(stmt.source_info);
 
-    #[cfg(any())] // This is never true
     match &stmt.kind {
         StatementKind::StorageLive(..) | StatementKind::StorageDead(..) => {} // Those are not very useful
         _ => {
