eb7f64582dcd7c4136dee7521ba9347d889fbb58
Santiago Pastorino <spastorino@gmail.com>
2023-03-28 17:52:56 -0300

    Specialization involving RPITITs is broken so ignore the diagnostic differences for them

--- tests/ui/async-await/in-trait/dont-project-to-specializable-projection.rs
+++ tests/ui/async-await/in-trait/dont-project-to-specializable-projection.rs
@@ -1,5 +1,7 @@
 // edition: 2021
 // known-bug: #108309
+// [next] compile-flags: -Zlower-impl-trait-in-trait-to-assoc-ty
+// revisions: current next
 
 #![feature(async_fn_in_trait)]
 #![feature(min_specialization)]
