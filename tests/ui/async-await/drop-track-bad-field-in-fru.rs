67b57d65c378867fc6eb553173a86610150d7c22
Nikolai Vazquez <hello@nikolaivazquez.com>
2023-03-28 01:54:03 -0400

    Fix UI test for `Option` future

--- tests/ui/async-await/drop-track-bad-field-in-fru.rs
+++ tests/ui/async-await/drop-track-bad-field-in-fru.rs
@@ -5,6 +5,5 @@ fn main() {}
 
 async fn foo() {
     None { value: (), ..Default::default() }.await;
-    //~^ ERROR `Option<_>` is not a future
-    //~| ERROR variant `Option<_>::None` has no field named `value`
+    //~^ ERROR variant `Option<_>::None` has no field named `value`
 }
