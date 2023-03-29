d499bbb99d72c991f1d1691f83ffe96bcfafc80a
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2023-03-29 14:50:10 +0200

    Use #[cfg(target_thread_local)] on items

--- tests/ui/thread-local/auxiliary/tls-rlib.rs
+++ tests/ui/thread-local/auxiliary/tls-rlib.rs
@@ -3,11 +3,12 @@
 #![crate_type = "rlib"]
 #![feature(thread_local)]
 #![feature(cfg_target_thread_local)]
-#![cfg(target_thread_local)]
 
+#[cfg(target_thread_local)]
 #[thread_local]
 pub static BAR: bool = true;
 
+#[cfg(target_thread_local)]
 #[inline(never)]
 pub fn bar_addr() -> usize {
     &BAR as *const bool as usize
