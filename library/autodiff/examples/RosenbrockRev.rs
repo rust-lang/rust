cfe1789cef549da21159d255336f706cacef6999
Lorenz Schmidt <lorenz.schmidt@mailbox.org>
2023-03-30 18:32:42 +0200

    Fix rosenbrock function

--- library/autodiff/examples/RosenbrockRev.rs
+++ library/autodiff/examples/RosenbrockRev.rs
@@ -12,17 +12,6 @@ fn rosenbrock(#[dup] x: &[f64; 2]) -> f64 {
     res
 }
 
-#[autodiff(d_rosenbrock, Forward, DuplicatedNoNeed)]
-fn rosenbrock(#[dup] x: &[f64; 2]) -> f64 {
-    let mut res = 0.0;
-    for i in 0..(x.len() - 1) {
-        let a = x[i + 1] - x[i] * x[i];
-        let b = x[i] - 1.0;
-        res += 100.0 * a * a + b * b;
-    }
-    res
-}
-
 fn main() {
     let x = [3.14, 2.4];
     let output = rosenbrock(&x);
