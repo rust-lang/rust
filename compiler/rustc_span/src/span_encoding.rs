c0e4a3d6a3477340618bd9e0ac06f687fc867eeb
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-04-03 14:41:05 +0200

    a

--- compiler/rustc_span/src/span_encoding.rs
+++ compiler/rustc_span/src/span_encoding.rs
@@ -110,7 +110,7 @@ impl Span {
         if len <= MAX_LEN && ctxt2 <= MAX_CTXT {
             let len_or_tag = len as u16;
             debug_assert_eq!(len_or_tag & PARENT_MASK, 0);
-            assert!(base <= 0xFFFF_FFFF_FFFF_FFFE);
+            assert!(base <= 0xFFFF_FFFE);
 
             if let Some(parent) = parent {
                 // Inline format with parent.
@@ -138,7 +138,7 @@ impl Span {
         let index =
             with_span_interner(|interner| interner.intern(&SpanData { lo, hi, ctxt, parent }));
         let ctxt_or_tag = if ctxt2 <= MAX_CTXT { ctxt2 } else { CTXT_TAG } as u16;
-        assert!(index <= 0xFFFF_FFFF_FFFF_FFFE);
+        assert!(index <= 0xFFFF_FFFE);
         Span { base_or_index: unsafe { NonMaxU32(index) }, len_or_tag: LEN_TAG, ctxt_or_tag }
     }
 
