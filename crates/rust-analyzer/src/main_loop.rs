440889edec0bc4e2ca3b12f2d56f3eaff278c01b
Bruno Ortiz <brunortiz11@gmail.com>
2023-04-02 22:08:02 -0300

    fixing main_loop.rs

--- crates/rust-analyzer/src/main_loop.rs
+++ crates/rust-analyzer/src/main_loop.rs
@@ -655,7 +655,6 @@ impl GlobalState {
             .on_sync_mut::<lsp_ext::ReloadWorkspace>(handlers::handle_workspace_reload)
             .on_sync_mut::<lsp_ext::RebuildProcMacros>(handlers::handle_proc_macros_rebuild)
             .on_sync_mut::<lsp_ext::MemoryUsage>(handlers::handle_memory_usage)
-            .on_sync_mut::<lsp_ext::FetchDependencyGraph>(handlers::fetch_dependency_graph)
             .on_sync_mut::<lsp_ext::ShuffleCrateGraph>(handlers::handle_shuffle_crate_graph)
             .on_sync::<lsp_ext::JoinLines>(handlers::handle_join_lines)
             .on_sync::<lsp_ext::OnEnter>(handlers::handle_on_enter)
