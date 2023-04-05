b3b8ff39db6bc12f31d97bac30798c96ac9d0573
joboet <jonasboettiger@icloud.com>
2023-04-05 17:39:59 +0200

    std: use `OnceCell` instead of `take`/`set` dance

--- library/std/src/sys_common/thread_info.rs
+++ library/std/src/sys_common/thread_info.rs
@@ -1,41 +1,29 @@
 #![allow(dead_code)] // stack_guard isn't used right now on all platforms
 
-use crate::cell::Cell;
+use crate::cell::OnceCell;
 use crate::sys::thread::guard::Guard;
 use crate::thread::Thread;
 
 thread_local! {
-    static THREAD: Cell<Option<Thread>> = const { Cell::new(None) };
+    static THREAD: OnceCell<Thread> = const { OnceCell::new() };
     // Use a separate thread local for the stack guard page location.
     // Since `Guard` does not implement drop, this is always available
     // on systems with ELF-TLS, in particular during TLS destruction.
-    static STACK_GUARD: Cell<Option<Guard>> = const { Cell::new(None) };
+    static STACK_GUARD: OnceCell<Guard> = const { OnceCell::new() };
 }
 
 pub fn current_thread() -> Option<Thread> {
-    THREAD
-        .try_with(|thread| {
-            let t = thread.take().unwrap_or_else(|| Thread::new(None));
-            let t2 = t.clone();
-            thread.set(Some(t));
-            t2
-        })
-        .ok()
+    THREAD.try_with(|thread| thread.get_or_init(|| Thread::new(None)).clone()).ok()
 }
 
 pub fn stack_guard() -> Option<Guard> {
-    STACK_GUARD
-        .try_with(|guard| {
-            let g = guard.take();
-            let g2 = g.clone();
-            guard.set(g);
-            g2
-        })
-        .ok()
-        .flatten()
+    STACK_GUARD.try_with(|guard| guard.get().cloned()).ok().flatten()
 }
 
 pub fn set(stack_guard: Option<Guard>, thread: Thread) {
-    rtassert!(STACK_GUARD.replace(stack_guard).is_none());
-    rtassert!(THREAD.replace(Some(thread)).is_none());
+    #[allow(unreachable_patterns, unreachable_code)] // On some platforms, `Guard` is `!`.
+    if let Some(guard) = stack_guard {
+        rtassert!(STACK_GUARD.with(|s| s.set(guard)).is_ok());
+    }
+    rtassert!(THREAD.with(|t| t.set(thread)).is_ok());
 }
