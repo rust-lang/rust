b47a322ef11e541231b18c67dfe133dac8764b11
Michael Schubart <michael@schubart.net>
2023-04-06 13:45:50 +0100

    Add tests suggested by @llogiq

--- tests/ui/manual_slice_size_calculation.rs
+++ tests/ui/manual_slice_size_calculation.rs
@@ -22,6 +22,12 @@ fn main() {
     // False negatives:
     let _ = 5 * size_of::<i32>() * s_i32.len(); // Ok (MISSED OPPORTUNITY)
     let _ = size_of::<i32>() * 5 * s_i32.len(); // Ok (MISSED OPPORTUNITY)
+
+    let len = s_i32.len();
+    let size = size_of::<i32>();
+    let _ = len * size_of::<i32>(); // Ok (MISSED OPPORTUNITY)
+    let _ = s_i32.len() * size; // Ok (MISSED OPPORTUNITY)
+    let _ = len * size; // Ok (MISSED OPPORTUNITY)
 }
 
 const fn _const(s_i32: &[i32]) {
