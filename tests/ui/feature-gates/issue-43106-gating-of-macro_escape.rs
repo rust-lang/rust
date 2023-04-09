bc5ca04bfbb4b596bee99a1d56ecf5a22187dacf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-09 19:16:29 -0400

    spelling: deprecation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/ui/feature-gates/issue-43106-gating-of-macro_escape.rs
+++ tests/ui/feature-gates/issue-43106-gating-of-macro_escape.rs
@@ -1,5 +1,5 @@
 // Testing that crate-level `#![macro_escape]` is not gated beyond a
-// depecation warning.  This file sits on its own, because crate-level
+// deprecation warning.  This file sits on its own, because crate-level
 // `#![macro_escape]` is incompatible with crate-level `#![macro_use]`
 // already present in issue-43106-gating-of-builtin-attrs.
 
