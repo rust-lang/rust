65364cc654742b4e37b4f598e256791da85aaf9b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-09 20:49:39 -0400

    spelling: granular
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/bootstrap/render_tests.rs
+++ src/bootstrap/render_tests.rs
@@ -1,7 +1,7 @@
 //! This module renders the JSON output of libtest into a human-readable form, trying to be as
 //! similar to libtest's native output as possible.
 //!
-//! This is needed because we need to use libtest in JSON mode to extract granluar information
+//! This is needed because we need to use libtest in JSON mode to extract granular information
 //! about the executed tests. Doing so suppresses the human-readable output, and (compared to Cargo
 //! and rustc) libtest doesn't include the rendered human-readable output as a JSON field. We had
 //! to reimplement all the rendering logic in this module because of that.
