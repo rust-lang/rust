d348c301e262e154ad9576cdc16f829fdc7b3ed5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-09 23:17:56 -0400

    spelling: suggestion
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/ui/suggestions/sugg_with_positional_args_and_debug_fmt.rs
+++ tests/ui/suggestions/sugg_with_positional_args_and_debug_fmt.rs
@@ -1,4 +1,4 @@
-// When build the suggesttion take in consideration the `:?`
+// When build the suggestion take in consideration the `:?`
 // https://github.com/rust-lang/rust/issues/100648
 #![deny(warnings)]
 
