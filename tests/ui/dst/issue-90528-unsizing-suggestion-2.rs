1ddfdb8ccb6e2068ab791090d8240871fec3e153
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-09 20:50:09 -0400

    spelling: immutable
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/ui/dst/issue-90528-unsizing-suggestion-2.rs
+++ tests/ui/dst/issue-90528-unsizing-suggestion-2.rs
@@ -1,7 +1,7 @@
 // Issue #90528: provide helpful suggestions when a trait bound is unsatisfied
 // due to a missed unsizing coercion.
 //
-// This test exercises array variables and a trait implemented on immmutable slices.
+// This test exercises array variables and a trait implemented on immutable slices.
 
 trait Read {}
 
