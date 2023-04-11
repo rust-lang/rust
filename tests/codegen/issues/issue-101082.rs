83f525cc2897d3561e268d3ea2207cd49a1c1bff
Nikita Popov <npopov@redhat.com>
2023-04-11 17:19:07 +0200

    Make test compatible with 32-bit

--- tests/codegen/issues/issue-101082.rs
+++ tests/codegen/issues/issue-101082.rs
@@ -7,7 +7,7 @@
 #[no_mangle]
 pub fn test() -> usize {
     // CHECK-LABEL: @test(
-    // CHECK: ret i64 165
+    // CHECK: ret {{i64|i32}} 165
     let values = [23, 16, 54, 3, 60, 9];
     let mut acc = 0;
     for item in values {
