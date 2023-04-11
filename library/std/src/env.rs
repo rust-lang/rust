7d269633b1e254ad21f23994bbf04a6a8296abf7
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2023-04-11 04:13:35 +0200

    Break up long first paragraph
    Further referring to `var_os` as a "function" (like in `var`), rather than "method".

--- library/std/src/env.rs
+++ library/std/src/env.rs
@@ -236,11 +236,12 @@ fn _var(key: &OsStr) -> Result<String, VarError> {
 }
 
 /// Fetches the environment variable `key` from the current process, returning
-/// [`None`] if the environment variable isn't set. It may return `None` also
-/// if the environment variable's name contains
+/// [`None`] if the variable isn't set or if there is another error.
+///
+/// It may return `None` if the environment variable's name contains
 /// the equal sign character (`=`) or the NUL character.
 ///
-/// Note that the method will not check if the environment variable
+/// Note that this function will not check if the environment variable
 /// is valid Unicode. If you want to have an error on invalid UTF-8,
 /// use the [`var`] function instead.
 ///
