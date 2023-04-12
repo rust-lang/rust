40a7ec98bb9be7180541b037dd4b032de7a7553d
bindsdev <bindsdev@gmail.com>
2023-04-11 22:22:53 -0500

    add additional information to note for referencing packed field diagnostic

--- compiler/rustc_mir_transform/src/check_packed_ref.rs
+++ compiler/rustc_mir_transform/src/check_packed_ref.rs
@@ -56,8 +56,9 @@ impl<'tcx> Visitor<'tcx> for PackedRefChecker<'_, 'tcx> {
                         "reference to packed field is unaligned"
                     )
                     .note(
-                        "fields of packed structs are not properly aligned, and creating \
-                        a misaligned reference is undefined behavior (even if that \
+                        "packed structs are only aligned by one byte. therefore, fields of packed \
+                        structs are not properly aligned and many modern architectures may penalize this. \
+                        creating a misaligned reference is undefined behavior (even if that \
                         reference is never dereferenced)",
                     ).help(
                         "copy the field contents to a local variable, or replace the \
