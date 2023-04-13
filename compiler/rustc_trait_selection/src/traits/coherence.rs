e2f5a5a71fa2a55e976484b9e72f5aa425ebab62
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2023-04-13 15:23:54 -0600

    make tcx.impl_subject return EarlyBinder, remove bound_impl_subject, rename usages of bound_impl_subject to impl_subject

--- compiler/rustc_trait_selection/src/traits/coherence.rs
+++ compiler/rustc_trait_selection/src/traits/coherence.rs
@@ -306,7 +306,7 @@ fn negative_impl(tcx: TyCtxt<'_>, impl1_def_id: DefId, impl2_def_id: DefId) -> b
         &infcx,
         ObligationCause::dummy(),
         impl_env,
-        tcx.bound_impl_subject(impl1_def_id).subst_identity(),
+        tcx.impl_subject(impl1_def_id).subst_identity(),
     ) {
         Ok(s) => s,
         Err(err) => {
