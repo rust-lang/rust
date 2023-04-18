8d75a8f699c921724b1e01ce932de77d08533957
Michael Goulet <michael@errs.io>
2023-04-18 19:25:57 +0000

    Test downstream errors from bad index expr

--- tests/ui/typeck/bad-index-due-to-nested.rs
+++ tests/ui/typeck/bad-index-due-to-nested.rs
@@ -1,15 +1,27 @@
-use std::collections::HashMap;
+use std::hash::Hash;
+use std::marker::PhantomData;
+use std::ops::Index;
 
-pub struct Graph<V> {
-    node_index_map: HashMap<V, usize>,
+struct HashMap<K, V>(PhantomData<(K, V)>);
+
+impl<K, V> Index<&K> for HashMap<K, V>
+where
+    K: Hash,
+    V: Copy,
+{
+    type Output = V;
+
+    fn index(&self, k: &K) -> &V {
+        todo!()
+    }
 }
 
-impl<V> Graph<V> {
-    pub fn node_index(&self, node: V) -> usize {
-        self.node_index_map[&node]
-        //~^ ERROR the trait bound `V: Eq` is not satisfied
-        //~| ERROR the trait bound `V: Hash` is not satisfied
-    }
+fn index<'a, K, V>(map: &'a HashMap<K, V>, k: K) -> &'a V {
+    map[k]
+    //~^ ERROR the trait bound `K: Hash` is not satisfied
+    //~| ERROR the trait bound `V: Copy` is not satisfied
+    //~| ERROR mismatched types
+    //~| ERROR mismatched types
 }
 
 fn main() {}
