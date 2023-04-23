b4067660f6d15e31f28e06c983630dc011a24b97
Alex Macleod <alex@macleod.io>
2023-04-23 14:20:55 +0000

    Add a test that checks for old style test headers

--- tests/ui/expect_tool_lint_rfc_2383.rs
+++ tests/ui/expect_tool_lint_rfc_2383.rs
@@ -1,4 +1,3 @@
-// check-pass
 #![feature(lint_reasons)]
 //! This file tests the `#[expect]` attribute implementation for tool lints. The same
 //! file is used to test clippy and rustdoc. Any changes to this file should be synced
