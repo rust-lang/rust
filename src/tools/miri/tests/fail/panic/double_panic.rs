6f5554bdef72e859380f2b6f6da7f830f66c905a
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-04-24 17:00:49 +0000

    Hide backtrace from stderr files

--- src/tools/miri/tests/fail/panic/double_panic.rs
+++ src/tools/miri/tests/fail/panic/double_panic.rs
@@ -1,6 +1,8 @@
 //@error-pattern: the program aborted
 //@normalize-stderr-test: "\| +\^+" -> "| ^"
 //@normalize-stderr-test: "unsafe \{ libc::abort\(\) \}|crate::intrinsics::abort\(\);" -> "ABORT();"
+//@normalize-stderr-test: "\n  +[0-9]+:[^\n]+" -> "$1"
+//@normalize-stderr-test: "\n at [^\n]+" -> "$1"
 
 struct Foo;
 impl Drop for Foo {
