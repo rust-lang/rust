0aed507f1632703a0d3e123fd250fd2eba3ef446
Bruno Ortiz <brunortiz11@gmail.com>
2023-04-27 16:15:54 -0300

    fixing TS linting, removing import

--- editors/code/src/lsp_ext.ts
+++ editors/code/src/lsp_ext.ts
@@ -2,7 +2,6 @@
  * This file mirrors `crates/rust-analyzer/src/lsp_ext.rs` declarations.
  */
 
-import { Uri } from "vscode";
 import * as lc from "vscode-languageclient";
 
 // rust-analyzer overrides
