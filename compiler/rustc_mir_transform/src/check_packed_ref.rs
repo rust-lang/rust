599e6e6e65da4c7b3ab5c896083294a6b4864853
bindsdev <bindsdev@gmail.com>
2023-04-27 22:52:05 -0500

    split note into two

--- compiler/rustc_mir_transform/src/check_packed_ref.rs
+++ compiler/rustc_mir_transform/src/check_packed_ref.rs
@@ -57,9 +57,10 @@ impl<'tcx> Visitor<'tcx> for PackedRefChecker<'_, 'tcx> {
                     )
                     .note(
                         "packed structs are only aligned by one byte. therefore, fields of packed \
-                        structs are not properly aligned and many modern architectures may penalize this. \
-                        creating a misaligned reference is undefined behavior (even if that \
-                        reference is never dereferenced)",
+                        structs are not properly aligned and many modern architectures may penalize this."
+                    ).note(
+                        "creating a misaligned reference is undefined behavior (even if that \
+                        reference is never dereferenced)"
                     ).help(
                         "copy the field contents to a local variable, or replace the \
                         reference with a raw pointer and use `read_unaligned`/`write_unaligned` \
