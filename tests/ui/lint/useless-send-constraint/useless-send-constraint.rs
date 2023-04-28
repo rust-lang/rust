30af6f089161b3a158ffab909ee77f6c3761b4dd
John Kelly <johnharrykelly@gmail.com>
2023-04-28 20:21:32 +0100

    WIP tests

--- tests/ui/lint/useless-send-constraint/useless-send-constraint.rs
+++ tests/ui/lint/useless-send-constraint/useless-send-constraint.rs
@@ -1,10 +1,13 @@
-// check-pass
 #![warn(useless_send_constraint)]
 
 use std::any::Any;
 
 fn main() {}
 
-fn foo(_a: &(dyn Any + Send + Send)) {
+fn fine(_a: &dyn Any) {}
 
-}
+fn should_replace_with_any(_a: &(dyn Send)) {}
+
+fn should_remove_send(_a: &(dyn Any + Send)) {}
+
+fn should_remove_send_duplicate(_a: &(dyn Any + Send)) {}
