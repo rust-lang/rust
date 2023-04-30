dca02d619599433f952b7fcef88660dc4da886c8
Camille GILLOT <gillot.camille@gmail.com>
2023-04-30 20:48:29 +0000

    Enable DestinationPropagation by default.

--- compiler/rustc_mir_transform/src/lib.rs
+++ compiler/rustc_mir_transform/src/lib.rs
@@ -94,7 +94,6 @@ mod lower_slice_len;
 mod match_branches;
 mod multiple_return_terminators;
 mod normalize_array_len;
-mod nrvo;
 mod prettify;
 mod promote_consts;
 mod ref_prop;
@@ -609,13 +608,12 @@ fn run_optimization_passes<'tcx>(tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
             &jump_threading::JumpThreading,
             &early_otherwise_branch::EarlyOtherwiseBranch,
             &simplify_comparison_integral::SimplifyComparisonIntegral,
-            &dest_prop::DestinationPropagation,
             &o1(simplify_branches::SimplifyConstCondition::Final),
             &o1(remove_noop_landing_pads::RemoveNoopLandingPads),
             &o1(simplify::SimplifyCfg::Final),
             &copy_prop::CopyProp,
             &dead_store_elimination::DeadStoreElimination::Final,
-            &nrvo::RenameReturnPlace,
+            &dest_prop::DestinationPropagation,
             &simplify::SimplifyLocals::Final,
             &multiple_return_terminators::MultipleReturnTerminators,
             &deduplicate_blocks::DeduplicateBlocks,
