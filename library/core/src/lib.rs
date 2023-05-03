a1e5c65aa451473d1614fc04cf3c0e67247a17f1
Scott McMurray <scottmcm@users.noreply.github.com>
2023-05-02 17:23:54 -0700

    `assume` the runtime range of `align_offset`
    Found when I saw code with `align_to` having extraneous checks.

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -107,6 +107,7 @@
 #![feature(const_arguments_as_str)]
 #![feature(const_array_from_ref)]
 #![feature(const_array_into_iter_constructors)]
+#![feature(const_assume)]
 #![feature(const_bigint_helper_methods)]
 #![feature(const_black_box)]
 #![feature(const_caller_location)]
