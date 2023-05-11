b0b76a5db31c57aace6fd7c6302f02b3dd63fae0
Ralf Jung <post@ralfj.de>
2023-05-11 10:09:20 +0200

    fix deny_lint test

--- src/tools/miri/tests/fail/deny_lint.rs
+++ src/tools/miri/tests/fail/deny_lint.rs
@@ -1,6 +1,6 @@
 //@error-in-other-file: miri cannot be run on programs that fail compilation
 
-#![deny(warnings)]
+#![deny(warnings, unused)]
 
 struct Foo;
 //~^ ERROR: struct `Foo` is never constructed
