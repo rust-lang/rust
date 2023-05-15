0ebb5cbab62e42b2e71ff8bfe6e30a8a51b59d4b
David Tolnay <dtolnay@gmail.com>
2023-05-15 02:56:31 -0700

    Add test coverage of more varieties of panic temporaries

--- tests/ui/macros/panic-temporaries-2018.rs
+++ tests/ui/macros/panic-temporaries-2018.rs
@@ -1,7 +1,7 @@
 // check-pass
-// edition:2021
+// edition:2018
 
-#![allow(unreachable_code)]
+#![allow(non_fmt_panics, unreachable_code)]
 
 use std::fmt::{self, Display};
 use std::marker::PhantomData;
@@ -20,8 +20,8 @@ impl Display for NotSend {
 
 async fn f(_: u8) {}
 
-// Exercises this matcher in panic_2021:
-// ($($t:tt)+) => $crate::panicking::panic_fmt(...)
+// Exercises this matcher in panic_2015:
+// ($fmt:expr, $($arg:tt)+) => $crate::panicking::panic_fmt(...)
 async fn panic_fmt() {
     // Panic returns `!`, so the await is never reached, and in particular the
     // temporaries inside the formatting machinery are not still alive at the
@@ -35,9 +35,21 @@ async fn panic_display() {
     f(panic!("{}", NOT_SEND)).await;
 }
 
+// Exercises ($msg:expr) => $crate::panicking::panic_str($msg)
+async fn panic_str() {
+    f(panic!((NOT_SEND, "...").1)).await;
+}
+
+// Exercises ($msg:expr) => $crate::panicking::unreachable_display(&$msg)
+async fn unreachable_display() {
+    f(unreachable!(NOT_SEND)).await;
+}
+
 fn require_send(_: impl Send) {}
 
 fn main() {
     require_send(panic_fmt());
     require_send(panic_display());
+    require_send(panic_str());
+    require_send(unreachable_display());
 }
