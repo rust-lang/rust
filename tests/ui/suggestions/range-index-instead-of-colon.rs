730d299354f7ef09d8e30609110fc499e0e22d71
Hirochika Matsumoto <hirochika.k.matsumoto@gmail.com>
2023-05-16 14:53:05 +0900

    Address review feedbacks
    Also addressed merge conflicts upon rebasing.

--- tests/ui/suggestions/range-index-instead-of-colon.rs
+++ tests/ui/suggestions/range-index-instead-of-colon.rs
@@ -4,5 +4,4 @@ fn main() {
     &[1, 2, 3][1:2];
     //~^ ERROR: expected one of
     //~| HELP: you might have meant to make a slice with range index
-    //~| HELP: maybe write a path separator here
 }
