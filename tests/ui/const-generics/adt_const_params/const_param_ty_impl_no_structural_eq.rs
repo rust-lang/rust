a9fcb524ffbcccc0c0f56e9ecde431961a4619a9
Michael Goulet <michael@errs.io>
2023-05-17 03:00:19 +0000

    Impl ConstParamTy for tuples, make PartialStructuralEq a supertrait too

--- tests/ui/const-generics/adt_const_params/const_param_ty_impl_no_structural_eq.rs
+++ tests/ui/const-generics/adt_const_params/const_param_ty_impl_no_structural_eq.rs
@@ -9,9 +9,11 @@ struct CantParam(ImplementsConstParamTy);
 
 impl std::marker::ConstParamTy for CantParam {}
 //~^ error: the type `CantParam` does not `#[derive(Eq)]`
+//~| error: the type `CantParam` does not `#[derive(PartialEq)]`
 
 #[derive(std::marker::ConstParamTy)]
 //~^ error: the type `CantParamDerive` does not `#[derive(Eq)]`
+//~| error: the type `CantParamDerive` does not `#[derive(PartialEq)]`
 struct CantParamDerive(ImplementsConstParamTy);
 
 fn check<T: std::marker::ConstParamTy>() {}
