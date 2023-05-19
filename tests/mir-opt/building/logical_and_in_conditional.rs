a1847f980ec1f4a314c6da27d67c58798a16724a
Abdulaziz Ghuloum <aghuloum@gmail.com>
2023-05-19 14:06:57 +0200

    added test case from issue https://github.com/rust-lang/rust/issues/111583

--- tests/mir-opt/building/logical_and_in_conditional.rs
+++ tests/mir-opt/building/logical_and_in_conditional.rs
@@ -20,7 +20,18 @@ fn test_and(a: i32, b: i32, c: i32) {
     } {}
 }
 
+// https://github.com/rust-lang/rust/issues/111583
+// EMIT_MIR logical_and_in_conditional.function_from_issue_111583.built.after.mir
+fn function_from_issue_111583(z: f64) {
+    let mask = (1 << 38) - 1;
+    let mut ret = 0;
+    if (z.to_bits() >> 8) & mask == 0 && z % 0.0625 < 1e-13 {
+        ret += 1;
+    }
+}
+
 fn main() {
+    function_from_issue_111583(0.0);
     for a in 0..1 {
         for b in 0..1 {
             for c in 0..1 {
