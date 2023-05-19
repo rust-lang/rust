164d041e303e9c98daaf35301fa88ba6844277dd
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-05-19 15:48:43 +0000

    Stop creating intermediate places just to immediate convert them to operands

--- tests/codegen/const_scalar_pair.rs
+++ tests/codegen/const_scalar_pair.rs
@@ -1,10 +1,8 @@
 // compile-flags: --crate-type=lib -Copt-level=0 -Zmir-opt-level=0 -C debuginfo=2
 
-// CHECK: @0 = private unnamed_addr constant <{ [8 x i8] }> <{ [8 x i8] c"\01\00\00\00\02\00\00\00" }>, align 4
-
 #![feature(inline_const)]
 
 pub fn foo() -> (i32, i32) {
-    // CHECK: %0 = load i32, ptr @0, align 4
+    // CHECK: ret { i32, i32 } { i32 1, i32 2 }
     const { (1, 2) }
 }
