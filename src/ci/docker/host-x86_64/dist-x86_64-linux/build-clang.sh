0bd136785c67aaeb72ebf13acc99fee42548bfdf
Jakub Beránek <jakub.beranek@vsb.cz>
2023-05-20 18:05:01 +0200

    Use PGO LLVM build

--- src/ci/docker/host-x86_64/dist-x86_64-linux/build-clang.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/build-clang.sh
@@ -25,7 +25,7 @@ INC="/rustroot/include:/usr/include"
 # disable them. BOLT is used for optimizing LLVM.
 hide_output \
     cmake ../llvm \
-      -C ../clang/cmake/caches/BOLT.cmake \
+      -C ../clang/cmake/caches/PGO.cmake \
       -DCMAKE_C_COMPILER=/rustroot/bin/gcc \
       -DCMAKE_CXX_COMPILER=/rustroot/bin/g++ \
       -DCMAKE_BUILD_TYPE=Release \
@@ -40,7 +40,7 @@ hide_output \
       -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt;bolt" \
       -DC_INCLUDE_DIRS="$INC"
 
-hide_output make -j$(nproc) clang-bolt lld bolt compiler-rt
+hide_output make -j$(nproc) stage2 lld bolt compiler-rt
 hide_output make install
 
 cd ../..
