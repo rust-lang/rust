5f0b677b86204c4cd98cf51b33b7224340406719
Pietro Albini <pietro.albini@ferrous-systems.com>
2023-05-23 15:20:57 +0200

    codegen: add needs-unwind to tests that require it

--- tests/codegen/drop.rs
+++ tests/codegen/drop.rs
@@ -1,4 +1,5 @@
 // ignore-wasm32-bare compiled with panic=abort by default
+// needs-unwind - this test verifies the amount of drop calls when unwinding is used
 // compile-flags: -C no-prepopulate-passes
 
 #![crate_type = "lib"]
