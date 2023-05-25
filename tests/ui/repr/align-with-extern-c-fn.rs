8ae01d67676f07792c7f1f349c26a55e3b70fbbd
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-05-25 18:36:53 -0400

    repr

--- tests/ui/repr/align-with-extern-c-fn.rs
+++ tests/ui/repr/align-with-extern-c-fn.rs
@@ -1,17 +1,11 @@
 // run-pass
 
-#![allow(stable_features)]
-#![allow(unused_variables)]
-
 // #45662
 
-#![feature(repr_align)]
+#[repr(C, align(16))]
+pub struct A(i64);
 
-#[repr(align(16))]
-pub struct A(#[allow(unused_tuple_struct_fields)] i64);
-
-#[allow(improper_ctypes_definitions)]
-pub extern "C" fn foo(x: A) {}
+pub extern "C" fn foo(_x: A) {}
 
 fn main() {
     foo(A(0));
