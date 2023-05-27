aca61b2c07f2f7b644ca000385473af9de2d5f77
Wim Looman <git@nemo157.com>
2023-05-27 14:37:18 +0200

    Test that a couple more types of unsafe-ops get a wrapping unsafe block added

--- tests/ui/unsafe/wrapping-unsafe-block-sugg.rs
+++ tests/ui/unsafe/wrapping-unsafe-block-sugg.rs
@@ -9,4 +9,15 @@ pub unsafe fn foo() {
     unsf(); //~ ERROR call to unsafe function is unsafe
 }
 
+pub unsafe fn bar(x: *const i32) -> i32 {
+    let y = *x; //~ ERROR dereference of raw pointer is unsafe and requires unsafe block
+    y + *x //~ ERROR dereference of raw pointer is unsafe and requires unsafe block
+}
+
+static mut BAZ: i32 = 0;
+pub unsafe fn baz() -> i32 {
+    let y = BAZ; //~ ERROR use of mutable static is unsafe and requires unsafe block
+    y + BAZ //~ ERROR use of mutable static is unsafe and requires unsafe block
+}
+
 fn main() {}
