472230d192b8bace6ddbe825d68ccd27d0d5ef1d
Jubilee Young <workingjubilee@gmail.com>
2023-05-30 00:34:50 -0700

    Remove array_zip
    `[T; N]::zip` is "eager" but most zips are mapped.
    This causes poor optimization in generated code.
    This is a fundamental design issue and "zip" is
    "prime real estate" in terms of function names,
    so let's free it up again.

--- tests/codegen/autovectorize-f32x4.rs
+++ tests/codegen/autovectorize-f32x4.rs
@@ -1,7 +1,6 @@
 // compile-flags: -C opt-level=3 -Z merge-functions=disabled
 // only-x86_64
 #![crate_type = "lib"]
-#![feature(array_zip)]
 
 // CHECK-LABEL: @auto_vectorize_direct
 #[no_mangle]
@@ -30,14 +29,4 @@ pub fn auto_vectorize_loop(a: [f32; 4], b: [f32; 4]) -> [f32; 4] {
         c[i] = a[i] + b[i];
     }
     c
-}
-
-// CHECK-LABEL: @auto_vectorize_array_zip_map
-#[no_mangle]
-pub fn auto_vectorize_array_zip_map(a: [f32; 4], b: [f32; 4]) -> [f32; 4] {
-// CHECK: load <4 x float>
-// CHECK: load <4 x float>
-// CHECK: fadd <4 x float>
-// CHECK: store <4 x float>
-    a.zip(b).map(|(a, b)| a + b)
 }
