d4b388eb43719543aaa9680f5aa03b69d024ea38
MarcusGrass <marcus.grass@protonmail.com>
2023-05-31 17:46:05 +0200

    Add from_over_into replace for type in `Self` reference

--- tests/ui/from_over_into.rs
+++ tests/ui/from_over_into.rs
@@ -88,4 +88,14 @@ impl Into<Opaque> for IntoOpaque {
     fn into(self) -> Opaque {}
 }
 
+pub struct Lval<T>(T);
+
+pub struct Rval<T>(T);
+
+impl<T> Into<Rval<Self>> for Lval<T> {
+    fn into(self) -> Rval<Self> {
+        Rval(self)
+    }
+}
+
 fn main() {}
