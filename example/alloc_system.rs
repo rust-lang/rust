5277ea10b53373912582a3a3a5eaab94ff46ed5f
chenx97 <chenx97@aosc.io>
2023-06-02 11:18:26 +0800

    support for mips64r6 as a target_arch value

--- example/alloc_system.rs
+++ example/alloc_system.rs
@@ -17,6 +17,7 @@ const MIN_ALIGN: usize = 8;
               target_arch = "aarch64",
               target_arch = "loongarch64",
               target_arch = "mips64",
+              target_arch = "mips64r6",
               target_arch = "s390x",
               target_arch = "sparc64"))]
 const MIN_ALIGN: usize = 16;
