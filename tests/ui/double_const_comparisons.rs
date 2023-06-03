8f40d09e0f08273e8edbb22493d845029a92d58f
Morten Lohne <lohnemorten@gmail.com>
2023-06-03 13:36:12 +0200

    Add tests for const comparisons that compare two different types

--- tests/ui/double_const_comparisons.rs
+++ tests/ui/double_const_comparisons.rs
@@ -8,8 +8,37 @@
 const STATUS_BAD_REQUEST: u16 = 400;
 const STATUS_SERVER_ERROR: u16 = 500;
 
+struct Status {
+    code: u16,
+}
+
+impl PartialEq<u16> for Status {
+    fn eq(&self, other: &u16) -> bool {
+        self.code == *other
+    }
+}
+
+impl PartialOrd<u16> for Status {
+    fn partial_cmp(&self, other: &u16) -> Option<std::cmp::Ordering> {
+        self.code.partial_cmp(other)
+    }
+}
+
+impl PartialEq<Status> for u16 {
+    fn eq(&self, other: &Status) -> bool {
+        *self == other.code
+    }
+}
+
+impl PartialOrd<Status> for u16 {
+    fn partial_cmp(&self, other: &Status) -> Option<std::cmp::Ordering> {
+        self.partial_cmp(&other.code)
+    }
+}
+
 fn main() {
     let status_code = 500; // Value doesn't matter for the lint
+    let status = Status { code: status_code };
 
     status_code >= 400 && status_code < 500; // Correct
     status_code <= 400 && status_code > 500;
@@ -22,11 +51,23 @@ fn main() {
     status_code <= u16::MIN + 1 && status_code > STATUS_SERVER_ERROR;
     status_code < STATUS_SERVER_ERROR && status_code > STATUS_SERVER_ERROR;
 
+    // Comparing two different types, via the `impl PartialOrd<u16> for Status`
+    status < { 400 } && status > { 500 };
+    status < STATUS_BAD_REQUEST && status > STATUS_SERVER_ERROR;
+    status <= u16::MIN + 1 && status > STATUS_SERVER_ERROR;
+    status < STATUS_SERVER_ERROR && status > STATUS_SERVER_ERROR;
+
     // Yoda conditions
     500 <= status_code && 600 > status_code; // Correct
     500 <= status_code && status_code <= 600; // Correct
     500 >= status_code && 600 < status_code; // Incorrect
     500 >= status_code && status_code > 600; // Incorrect
+
+    // Yoda conditions, comparing two different types
+    500 <= status && 600 > status; // Correct
+    500 <= status && status <= 600; // Correct
+    500 >= status && 600 < status; // Incorrect
+    500 >= status && status > 600; // Incorrect
 
     // Expressions where one of the sides has no effect
     status_code < 200 && status_code <= 299;
