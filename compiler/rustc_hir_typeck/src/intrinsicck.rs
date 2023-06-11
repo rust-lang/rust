3bbc598d16bbaab62ec3f460d1832236908607d8
yukang <moorekang@gmail.com>
2023-06-11 17:03:26 +0800

    use bug! for overflow of u128

--- compiler/rustc_hir_typeck/src/intrinsicck.rs
+++ compiler/rustc_hir_typeck/src/intrinsicck.rs
@@ -87,7 +87,10 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
                 if let Some(v) = u128::from(size.bytes()).checked_mul(8) {
                     format!("{} bits", v)
                 } else {
-                    format!("{} bytes", size.bytes())
+                    // `u128` should definitely be able to hold the size of different architectures
+                    // larger sizes should be reported as error `are too big for the current architecture`
+                    // otherwise we have a bug somewhere
+                    bug!("{:?} overflow for u128", size)
                 }
             }
             Ok(SizeSkeleton::Generic(size)) => {
