802c1d5979683c8bf991122643ee2cdbdb51eec4
Wim Looman <git@nemo157.com>
2023-06-11 11:59:15 +0200

    Add test cases for suggestions with unsafe operations contained inside macros

--- tests/ui/unsafe/wrapping-unsafe-block-sugg.rs
+++ tests/ui/unsafe/wrapping-unsafe-block-sugg.rs
@@ -1,6 +1,9 @@
 // run-rustfix
+// aux-build:external_unsafe_macro.rs
 
 #![deny(unsafe_op_in_unsafe_fn)] //~ NOTE
+
+extern crate external_unsafe_macro;
 
 unsafe fn unsf() {}
 
@@ -33,6 +36,31 @@ pub unsafe fn baz() -> i32 {
     y + BAZ //~ ERROR use of mutable static is unsafe and requires unsafe block
     //~^ NOTE
     //~| NOTE
+}
+
+macro_rules! unsafe_macro { () => (unsf()) }
+//~^ ERROR call to unsafe function is unsafe
+//~| NOTE
+//~| NOTE
+//~| ERROR call to unsafe function is unsafe
+//~| NOTE
+//~| NOTE
+
+pub unsafe fn unsafe_in_macro() {
+    //~^ NOTE an unsafe function restricts its caller, but its body is safe by default
+    unsafe_macro!();
+    //~^ NOTE
+    //~| NOTE
+    unsafe_macro!();
+    //~^ NOTE
+    //~| NOTE
+}
+
+pub unsafe fn unsafe_in_external_macro() {
+    // FIXME: https://github.com/rust-lang/rust/issues/112504
+    // FIXME: ~^ NOTE an unsafe function restricts its caller, but its body is safe by default
+    external_unsafe_macro::unsafe_macro!();
+    external_unsafe_macro::unsafe_macro!();
 }
 
 fn main() {}
