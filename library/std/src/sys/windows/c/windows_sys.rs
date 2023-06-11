f7f25b0e2f32a3c6eb778a229631a9a5c3979f92
bdbai <bdbaiapp@163.com>
2023-06-11 22:50:30 +0800

    Add windows_sys typedef for Win ARM32

--- library/std/src/sys/windows/c/windows_sys.rs
+++ library/std/src/sys/windows/c/windows_sys.rs
@@ -4275,3 +4275,20 @@ impl ::core::clone::Clone for XSAVE_FORMAT {
         *self
     }
 }
+// Begin of ARM32 shim
+cfg_if::cfg_if! {
+if #[cfg(target_arch = "arm")] {
+#[repr(C)]
+pub struct WSADATA {
+    pub wVersion: u16,
+    pub wHighVersion: u16,
+    pub szDescription: [u8; 257],
+    pub szSystemStatus: [u8; 129],
+    pub iMaxSockets: u16,
+    pub iMaxUdpDg: u16,
+    pub lpVendorInfo: PSTR,
+}
+pub enum CONTEXT {}
+}
+}
+// End of ARM32 shim
