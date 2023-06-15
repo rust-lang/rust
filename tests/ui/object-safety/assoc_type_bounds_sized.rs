ce3cff47e0216e6fed4fb87e31d9d24307e6c49a
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-06-15 13:29:13 +0000

    Add more tests

--- tests/ui/object-safety/assoc_type_bounds_sized.rs
+++ tests/ui/object-safety/assoc_type_bounds_sized.rs
@@ -1,3 +1,6 @@
+//! This test checks that associated types only need to be
+//! mentioned in trait objects, if they don't require `Self: Sized`.
+
 // check-pass
 
 trait Foo {
@@ -7,5 +10,15 @@ trait Foo {
 }
 
 fn foo(_: &dyn Foo) {}
+
+trait Other: Sized {}
+
+trait Boo {
+    type Assoc
+    where
+        Self: Other;
+}
+
+fn boo(_: &dyn Boo) {}
 
 fn main() {}
