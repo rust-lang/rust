25e3785b8691e269e4e10bb8deeae0df205e22c1
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-06-16 12:46:28 +0000

    Make `unused_associated_type_bounds`'s lint level changeable

--- tests/ui/object-safety/assoc_type_bounds_sized_unnecessary.rs
+++ tests/ui/object-safety/assoc_type_bounds_sized_unnecessary.rs
@@ -11,4 +11,7 @@ fn foo(_: &dyn Foo<Bar = ()>) {}
 //~| WARN: unnecessary associated type bound for not object safe associated type
 //~| WARN: unnecessary associated type bound for not object safe associated type
 
+#[allow(unused_associated_type_bounds)]
+fn bar(_: &dyn Foo<Bar = ()>) {}
+
 fn main() {}
