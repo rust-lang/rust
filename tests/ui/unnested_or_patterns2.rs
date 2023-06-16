1d4afc5d8201e591e0019883c0c2c6a0ae93f13b
y21 <30553356+y21@users.noreply.github.com>
2023-06-16 21:39:02 +0200

    new lint: `manual_range_pattern`

--- tests/ui/unnested_or_patterns2.rs
+++ tests/ui/unnested_or_patterns2.rs
@@ -2,7 +2,12 @@
 
 #![feature(box_patterns)]
 #![warn(clippy::unnested_or_patterns)]
-#![allow(clippy::cognitive_complexity, clippy::match_ref_pats, clippy::needless_if)]
+#![allow(
+    clippy::cognitive_complexity,
+    clippy::match_ref_pats,
+    clippy::needless_if,
+    clippy::manual_range_pattern
+)]
 #![allow(unreachable_patterns, irrefutable_let_patterns, unused_variables)]
 
 fn main() {
