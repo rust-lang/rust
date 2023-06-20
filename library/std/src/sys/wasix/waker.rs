4ed0aa15892d72bc7997f59dd43cc489bf9f3742
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-06-21 04:12:38 +1000

    Modified the callback registration function

--- library/std/src/sys/wasix/waker.rs
+++ library/std/src/sys/wasix/waker.rs
@@ -1,63 +1,62 @@
 use crate::task::{Poll, Context, Waker};
 use crate::io;
+use crate::mem::ManuallyDrop;
 use wasi::x::Errno;
 
 // Callback when a waker is woken
 #[no_mangle]
 #[allow(fuzzy_provenance_casts)]
-pub extern "C" fn _waker_wake(waker: u64) {
-    let waker = waker as usize;
-    let waker = unsafe {
-        let waker = waker as *mut RawWaker;
-        Box::from_raw(waker)
+pub extern "C" fn _wake(waker0: i64, waker1: i64, waker2: i64, waker3: i64, waker4: i64, waker5: i64, waker6: i64, waker7: i64) {
+    let process = |waker: i64| {
+        if waker > 0 {
+            unsafe {
+                let waker = waker.abs() as *mut Waker;
+                ManuallyDrop::new(Box::from_raw(waker))
+            }.wake_by_ref();
+        } else if waker < 0 {
+            drop(unsafe {
+                let waker = waker.abs() as *mut Waker;
+                Box::from_raw(waker)
+            })
+        }
     };
-    waker.wake();
-}
-
-// Callback when a waker is dropped
-#[no_mangle]
-#[allow(fuzzy_provenance_casts)]
-pub extern "C" fn _waker_drop(waker: u64) {
-    let waker = waker as usize;
-    let waker = unsafe {
-        let waker = waker as *mut RawWaker;
-        Box::from_raw(waker)
-    };
-    drop(waker);
+    process(waker0);
+    process(waker1);
+    process(waker2);
+    process(waker3);
+    process(waker4);
+    process(waker5);
+    process(waker6);
+    process(waker7);
 }
 
 pub fn waker_register(cx: &mut crate::task::Context<'_>) -> RawWakerRef {
-    let waker = Box::new(RawWaker {
-        inner: cx.waker().clone()
-    });
-    let waker = Box::into_raw(waker) as *mut RawWaker;
-    let waker = waker as usize;
-    (waker as u64).into()
+    let waker = Box::new(cx.waker().clone());
+    let waker = Box::into_raw(waker) as *mut Waker;
+    RawWakerRef {
+        raw: Some(waker as u64)
+    }
 }
 
 pub struct RawWakerRef {
-    id: Option<u64>,
-}
-impl From<u64>
-for RawWakerRef {
-    fn from(val: u64) -> Self {
-        Self {
-            id: Some(val)
-        }
-    }
+    raw: Option<u64>,
 }
 impl Drop
 for RawWakerRef {
+    #[allow(fuzzy_provenance_casts)]
     fn drop(&mut self) {
-        if let Some(id) = self.id.take() {
-            _waker_drop(id);
+        if let Some(raw) = self.raw.take() {
+            drop(unsafe {
+                let waker = raw as *mut Waker;
+                Box::from_raw(waker)
+            })
         }
     }
 }
 impl Into<u64>
 for RawWakerRef {
     fn into(mut self) -> u64 {
-        self.id.take().unwrap()
+        self.raw.take().unwrap()
     }
 }
 
@@ -75,15 +74,5 @@ where F: FnOnce(RawWakerRef) -> Result<T, Errno> {
         Err(err) => {
             Poll::Ready(Err(super::err2io(err)))
         }
-    }
-}
-
-struct RawWaker {
-    inner: Waker,
-}
-
-impl RawWaker {
-    pub fn wake(self) {
-        self.inner.wake();
     }
 }
