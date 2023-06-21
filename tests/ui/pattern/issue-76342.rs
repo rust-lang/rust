e18e3761ced3695e9d7a920b223a49767dd4f1f6
b-naber <b_naber@gmx.de>
2023-06-21 18:01:31 +0000

    add test, bless tests

---
+++ tests/ui/pattern/issue-76342.rs
@@ -0,0 +1,16 @@
+// check-pass
+#![allow(unused_variables)]
+struct Zeroes;
+impl Into<[usize; 2]> for Zeroes {
+    fn into(self) -> [usize; 2] { [0; 2] }
+}
+impl Into<[usize; 3]> for Zeroes {
+    fn into(self) -> [usize; 3] { [0; 3] }
+}
+impl Into<[usize; 4]> for Zeroes {
+    fn into(self) -> [usize; 4] { [0; 4] }
+}
+fn main() {
+    let [a, b, c] = Zeroes.into(); // ERROR: type annotations needed
+    let [d, e, f]: [_; 3] = Zeroes.into(); // Works great
+}
