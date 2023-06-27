b0142f603dd171dccf822a868284c93b8f6f6f91
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-06-27 09:07:14 +0000

    Avoid calling queries during query stack printing

--- tests/ui/treat-err-as-bug/panic-causes-oom-112708.rs
+++ tests/ui/treat-err-as-bug/panic-causes-oom-112708.rs
@@ -1,8 +1,7 @@
 // compile-flags: -Ztreat-err-as-bug
 // dont-check-failure-status
 // error-pattern: aborting due to `-Z treat-err-as-bug=1`
-// normalize-stderr-test "note: .*\n\n" -> ""
-// normalize-stderr-test "thread 'rustc' panicked.*\n" -> ""
+// dont-check-compiler-stderr
 // rustc-env:RUST_BACKTRACE=0
 
 fn main() {
