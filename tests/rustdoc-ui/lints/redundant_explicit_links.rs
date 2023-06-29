e583318aa864ce0a3d53c339600d51a70d7b6440
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-06-30 02:35:44 +0800

    fix trailing whitespace

--- tests/rustdoc-ui/lints/redundant_explicit_links.rs
+++ tests/rustdoc-ui/lints/redundant_explicit_links.rs
@@ -4,7 +4,7 @@ pub fn dummy_target() {}
 
 /// [dummy_target](dummy_target)
 /// [`dummy_target`](dummy_target)
-/// 
+///
 /// [Vec](Vec)
 /// [`Vec`](Vec)
 /// [Vec](std::vec::Vec)
@@ -13,7 +13,7 @@ pub fn dummy_target() {}
 /// [`std::vec::Vec`](Vec)
 /// [std::vec::Vec](std::vec::Vec)
 /// [`std::vec::Vec`](std::vec::Vec)
-/// 
+///
 /// [usize](usize)
 /// [`usize`](usize)
 /// [usize](std::primitive::usize)
