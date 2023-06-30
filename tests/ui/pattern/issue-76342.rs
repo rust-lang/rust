39c2785aec339032632fc0c72a5c51017bfa54ae
b-naber <b_naber@gmx.de>
2023-06-30 11:42:37 +0000

    add tests for refutable patterns

--- tests/ui/pattern/issue-76342.rs
+++ tests/ui/pattern/issue-76342.rs
@@ -2,15 +2,21 @@
 #![allow(unused_variables)]
 struct Zeroes;
 impl Into<[usize; 2]> for Zeroes {
-    fn into(self) -> [usize; 2] { [0; 2] }
+    fn into(self) -> [usize; 2] {
+        [0; 2]
+    }
 }
 impl Into<[usize; 3]> for Zeroes {
-    fn into(self) -> [usize; 3] { [0; 3] }
+    fn into(self) -> [usize; 3] {
+        [0; 3]
+    }
 }
 impl Into<[usize; 4]> for Zeroes {
-    fn into(self) -> [usize; 4] { [0; 4] }
+    fn into(self) -> [usize; 4] {
+        [0; 4]
+    }
 }
 fn main() {
-    let [a, b, c] = Zeroes.into(); // ERROR: type annotations needed
-    let [d, e, f]: [_; 3] = Zeroes.into(); // Works great
+    let [a, b, c] = Zeroes.into();
+    let [d, e, f]: [_; 3] = Zeroes.into();
 }
---
+++ tests/ui/pattern/slice-pattern-refutable.rs
@@ -0,0 +1,34 @@
+#![allow(unused_variables)]
+
+struct Zeroes;
+
+impl Into<[usize; 3]> for Zeroes {
+    fn into(self) -> [usize; 3] {
+        [0; 3]
+    }
+}
+
+fn let_else() {
+    let [a, b, c] = Zeroes.into() else {
+        //~^ ERROR type annotations needed
+        unreachable!();
+    };
+}
+
+fn if_let() {
+    if let [a, b, c] = Zeroes.into() {
+        //~^ ERROR type annotations needed
+        unreachable!();
+    }
+}
+
+fn if_let_else() {
+    if let [a, b, c] = Zeroes.into() {
+        //~^ ERROR type annotations needed
+        unreachable!();
+    } else {
+        unreachable!();
+    }
+}
+
+fn main() {}
