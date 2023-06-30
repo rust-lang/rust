982f025cad0c6bf6cb7007f5cdb5f61cf40dee38
Michael Goulet <michael@errs.io>
2023-06-30 18:55:43 +0000

    use explicit predicates

--- compiler/rustc_hir_analysis/src/collect/predicates_of.rs
+++ compiler/rustc_hir_analysis/src/collect/predicates_of.rs
@@ -89,7 +89,8 @@ fn gather_explicit_predicates_of(tcx: TyCtxt<'_>, def_id: LocalDefId) -> ty::Gen
 
         Some(ImplTraitInTraitData::Impl { fn_def_id }) => {
             let assoc_item = tcx.associated_item(def_id);
-            let trait_assoc_predicates = tcx.predicates_of(assoc_item.trait_item_def_id.unwrap());
+            let trait_assoc_predicates =
+                tcx.explicit_predicates_of(assoc_item.trait_item_def_id.unwrap());
 
             let impl_assoc_identity_substs = InternalSubsts::identity_for_item(tcx, def_id);
             let impl_def_id = tcx.parent(fn_def_id);
