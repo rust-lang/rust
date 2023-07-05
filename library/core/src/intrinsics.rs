8a7f4495aa464901e3742c66c003c164e04c9d88
Jubilee Young <workingjubilee@gmail.com>
2023-07-05 09:38:19 -0700

    Describe write_bytes and copy sans C
    These intrinsics have a divergent meaning from C's own,
    so stop trying to tell people that they are the same.

--- library/core/src/intrinsics.rs
+++ library/core/src/intrinsics.rs
@@ -2681,17 +2681,14 @@ pub const unsafe fn copy_nonoverlapping<T>(src: *const T, dst: *mut T, count: us
 /// Copies `count * size_of::<T>()` bytes from `src` to `dst`. The source
 /// and destination may overlap.
 ///
-/// If the source and destination will *never* overlap,
-/// [`copy_nonoverlapping`] can be used instead.
-///
-/// `copy` is semantically equivalent to C's [`memmove`], but with the argument
-/// order swapped. Copying takes place as if the bytes were copied from `src`
-/// to a temporary array and then copied from the array to `dst`.
+/// `copy` may temporarily use additional space to copy bytes from `src` into an array, before
+/// copying them to `dst`. This lets `copy` correctly handle overlapping locations.
+/// If `src` and `dst` *never* overlap, [`copy_nonoverlapping`] may be used to avoid overhead.
+/// Note that in many cases, this additional overhead can be removed or reduced by the compiler,
+/// as this is an "intrinsic function" implemented directly through the compiler.
 ///
 /// The copy is "untyped" in the sense that data may be uninitialized or otherwise violate the
 /// requirements of `T`. The initialization state is preserved exactly.
-///
-/// [`memmove`]: https://en.cppreference.com/w/c/string/byte/memmove
 ///
 /// # Safety
 ///
@@ -2768,10 +2765,8 @@ pub const unsafe fn copy<T>(src: *const T, dst: *mut T, count: usize) {
 /// Sets `count * size_of::<T>()` bytes of memory starting at `dst` to
 /// `val`.
 ///
-/// `write_bytes` is similar to C's [`memset`], but sets `count *
-/// size_of::<T>()` bytes to `val`.
-///
-/// [`memset`]: https://en.cppreference.com/w/c/string/byte/memset
+/// Note that this means that if `T` is multiple bytes, each byte is set to the same value,
+/// which may be interpreted to mean a very different value when interpreted as `T`.
 ///
 /// # Safety
 ///
