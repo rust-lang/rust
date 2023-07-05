261c0231232287dec32329ceb67d61249ffc8d5e
Eric Mark Martin <ericmarkmartin@gmail.com>
2023-07-05 04:28:45 +0000

    rename constants in mir dump test

--- tests/mir-opt/building/issue_110508.rs
+++ tests/mir-opt/building/issue_110508.rs
@@ -1,13 +1,13 @@
-// EMIT_MIR issue_110508.{impl#0}-BAR1.built.after.mir
-// EMIT_MIR issue_110508.{impl#0}-BAR2.built.after.mir
+// EMIT_MIR issue_110508.{impl#0}-BAR.built.after.mir
+// EMIT_MIR issue_110508.{impl#0}-SELF_BAR.built.after.mir
 
 enum Foo {
     Bar(()),
 }
 
 impl Foo {
-    const BAR1: Foo = Foo::Bar(());
-    const BAR2: Foo = Self::Bar(());
+    const BAR: Foo = Foo::Bar(());
+    const SELF_BAR: Foo = Self::Bar(());
 }
 
 fn main() {}
