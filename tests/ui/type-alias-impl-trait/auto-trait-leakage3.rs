6e039656c5fd21bc5fa9d40903946a63c2d7743f
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-07-06 07:27:15 +0000

    Revert "Add some extra information to opaque type cycle errors"
    This reverts commit 9e98feb84c36f369b7b0f163e82304ee799b1aef, as it
    caused a performance regression.

--- tests/ui/type-alias-impl-trait/auto-trait-leakage3.rs
+++ tests/ui/type-alias-impl-trait/auto-trait-leakage3.rs
@@ -6,7 +6,6 @@
 mod m {
     pub type Foo = impl std::fmt::Debug;
     //~^ ERROR: cycle detected when computing type of `m::Foo::{opaque#0}` [E0391]
-    //~| ERROR: cycle detected when computing type of `m::Foo::{opaque#0}` [E0391]
 
     pub fn foo() -> Foo {
         22_u32
@@ -14,7 +13,6 @@ mod m {
 
     pub fn bar() {
         is_send(foo());
-        //~^ ERROR: cannot check whether the hidden type of `auto_trait_leakage3[211d]::m::Foo::{opaque#0}
     }
 
     fn is_send<T: Send>(_: T) {}
