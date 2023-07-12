c429a72db9515eb3244d6da8c09c15c520aee9db
nxya <nathacutlan@gmail.com>
2023-07-12 10:18:30 -0400

    add links to query documentation for E0391

--- tests/ui/associated-type-bounds/ambiguous-associated-type2.rs
+++ tests/ui/associated-type-bounds/ambiguous-associated-type2.rs
@@ -5,6 +5,7 @@ trait Bar<T> {
     type Item;
 }
 trait Baz: Foo + Bar<Self::Item> {}
-//~^ ERROR cycle detected when computing the super traits of `Baz` with associated type name `Item` [E0391]
+//~^ ERROR cycle detected when computing the super traits of `Baz` with associated type name `Item`. see https://rustc-dev-guide.rust-lang.org/overview.html#queries and https://rustc-dev-guide.rust-lang.org/query.html for more information. [E0391]
+
 
 fn main() {}
