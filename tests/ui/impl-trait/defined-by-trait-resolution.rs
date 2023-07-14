5a473bb693ea2fef509d4b7337537ee49ecb94b5
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2023-07-14 12:35:33 +0000

    allow opaques to be defined by trait queries

---
+++ tests/ui/impl-trait/defined-by-trait-resolution.rs
@@ -0,0 +1,12 @@
+//! The trait query `foo: Fn() -> u8` is a valid defining use of RPIT.
+
+// build-pass
+
+fn returns_u8(_: impl Fn() -> u8) {}
+
+pub fn foo() -> impl Sized {
+    returns_u8(foo);
+    0u8
+}
+
+fn main() {}
