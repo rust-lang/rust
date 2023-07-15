aa98c5d14e9ff560dd8539a3c4b61344c53fc8af
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-07-15 17:35:36 +0000

    Rewrite rmeta-rpass test to work with the new check for all crate sources being in sync

--- tests/run-make/rmeta-preferred/lib.rs
+++ tests/run-make/rmeta-preferred/lib.rs
@@ -1,4 +1,3 @@
-// run-pass
 // Test that using rlibs and rmeta dep crates work together. Specifically, that
 // there can be both an rmeta and an rlib file and rustc will prefer the rmeta
 // file.
@@ -7,12 +6,9 @@
 // case we want to make sure that the rlib isn't being used as that would cause
 // bugs in -Zbinary-dep-depinfo (see #68298).
 
-// aux-build:rmeta-rmeta.rs
-// aux-build:rmeta-rlib-rpass.rs
-
 extern crate rmeta_aux;
 use rmeta_aux::Foo;
 
-pub fn main() {
-    let _ = Foo { field2: 42 };
+pub fn foo() {
+    let _ = Foo { field: 42 };
 }
