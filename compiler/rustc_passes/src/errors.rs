08c77a6eb403768ed64ae567980887984f506ef9
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2023-07-16 18:18:38 +0800

    Add infrastructure `#[rustc_confusables]` attribute to allow targeted "no method" errors on standard library types
    The standard library developer can annotate methods on e.g.
    `BTreeSet::push` with `#[rustc_confusables("insert")]`. When the user
    mistypes `btreeset.push()`, `BTreeSet::insert` will be suggested if
    there are no other candidates to suggest.

--- compiler/rustc_passes/src/errors.rs
+++ compiler/rustc_passes/src/errors.rs
@@ -618,6 +618,38 @@ pub struct LinkOrdinal {
 }
 
 #[derive(Diagnostic)]
+#[diag(passes_confusables)]
+pub struct Confusables {
+    #[primary_span]
+    pub attr_span: Span,
+}
+
+#[derive(Diagnostic)]
+#[diag(passes_empty_confusables)]
+pub(crate) struct EmptyConfusables {
+    #[primary_span]
+    pub span: Span,
+}
+
+#[derive(Diagnostic)]
+#[diag(passes_incorrect_meta_item, code = "E0539")]
+pub(crate) struct IncorrectMetaItem {
+    #[primary_span]
+    pub span: Span,
+    #[subdiagnostic]
+    pub suggestion: IncorrectMetaItemSuggestion,
+}
+
+#[derive(Subdiagnostic)]
+#[multipart_suggestion(passes_incorrect_meta_item_suggestion, applicability = "maybe-incorrect")]
+pub(crate) struct IncorrectMetaItemSuggestion {
+    #[suggestion_part(code = "\"")]
+    pub lo: Span,
+    #[suggestion_part(code = "\"")]
+    pub hi: Span,
+}
+
+#[derive(Diagnostic)]
 #[diag(passes_stability_promotable)]
 pub struct StabilityPromotable {
     #[primary_span]
