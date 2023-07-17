e1e755311a6ae8a3d15dff80851d00a8b2881d82
b-naber <b_naber@gmx.de>
2023-07-17 21:58:37 +0000

    add more tests

--- tests/ui/destructuring-assignment/slice_destructure_fail.rs
+++ tests/ui/destructuring-assignment/slice_destructure_fail.rs
@@ -3,8 +3,6 @@ fn main() {
     [a, .., b, ..] = [0, 1]; //~ ERROR `..` can only be used once per slice pattern
     [a, a, b] = [1, 2];
     //~^ ERROR pattern requires 3 elements but array has 2
-    //~| ERROR mismatched types
     [_] = [1, 2];
     //~^ ERROR pattern requires 1 element but array has 2
-    //~| ERROR mismatched types
 }
