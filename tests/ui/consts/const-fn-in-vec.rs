86b112204ade0203d1c2272a523e26b4593fb60a
clubby789 <jamie@hill-daniel.co.uk>
2023-07-21 15:59:28 +0000

    Improve diagnostic for const ctors in array repeat expressions

--- tests/ui/consts/const-fn-in-vec.rs
+++ tests/ui/consts/const-fn-in-vec.rs
@@ -1,7 +1,11 @@
+static _MAYBE_STRINGS: [Option<String>; 5] = [None; 5];
+//~^ ERROR the trait bound `String: Copy` is not satisfied
+
 fn main() {
     // should hint to create an inline `const` block
     // or to create a new `const` item
-    let strings: [String; 5] = [String::new(); 5];
+    let _strings: [String; 5] = [String::new(); 5];
     //~^ ERROR the trait bound `String: Copy` is not satisfied
-    println!("{:?}", strings);
+    let _maybe_strings: [Option<String>; 5] = [None; 5];
+    //~^ ERROR the trait bound `String: Copy` is not satisfied
 }
