a4e1bf416d7dc2e6ed156ed09f7f3294c8eec48a
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2023-07-22 13:45:50 +0200

    compiler: Hermit targets: Sort base fields by declaration
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- compiler/rustc_target/src/spec/hermit_base.rs
+++ compiler/rustc_target/src/spec/hermit_base.rs
@@ -3,13 +3,13 @@ use crate::spec::{Cc, LinkerFlavor, Lld, PanicStrategy, TargetOptions, TlsModel}
 pub fn opts() -> TargetOptions {
     TargetOptions {
         os: "hermit".into(),
-        linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
         linker: Some("rust-lld".into()),
-        has_thread_local: true,
-        panic_strategy: PanicStrategy::Abort,
+        linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
+        tls_model: TlsModel::InitialExec,
         position_independent_executables: true,
         static_position_independent_executables: true,
-        tls_model: TlsModel::InitialExec,
+        has_thread_local: true,
+        panic_strategy: PanicStrategy::Abort,
         ..Default::default()
     }
 }
