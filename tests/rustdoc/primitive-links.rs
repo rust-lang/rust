1ef48b1bcf90eb760f1dc51e357418d19526fe37
Guillaume Gomez <guillaume.gomez@huawei.com>
2023-07-26 21:28:01 +0200

    Update rustdoc tests

--- tests/rustdoc/primitive-links.rs
+++ tests/rustdoc/primitive-links.rs
@@ -8,11 +8,10 @@
 pub struct Struct;
 
 pub trait Trait<Rhs = Self> {
-    // There should only be one `<a>` (not linking to reference primitive).
-    // @count - '//*[@id="tymethod.method"]/*[@class="code-header"]/a' 1
+    // @count - '//*[@id="tymethod.method"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.method"]/*[@class="code-header"]/a' 'method'
+    // @has - '//*[@id="tymethod.method"]/*[@class="code-header"]/a' '&'
     fn method(&self, other: &Rhs);
-    // There should only be one `<a>` (method and `*const`).
     // @count - '//*[@id="tymethod.method2"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.method2"]/*[@class="code-header"]/a' 'method2'
     // @has - '//*[@id="tymethod.method2"]/*[@class="code-header"]/a' '*const'
@@ -41,24 +40,25 @@ pub trait Trait<Rhs = Self> {
     // @has - '//*[@id="tymethod.tuple2"]/*[@class="code-header"]/a' 'tuple2'
     // @has - '//*[@id="tymethod.tuple2"]/*[@class="code-header"]/a' 'Struct'
     fn tuple2(&self, other: (Struct, Rhs));
-    // There should be only one `<a>` (just the method).
-    // @count - '//*[@id="tymethod.slice"]/*[@class="code-header"]/a' 1
+    // @count - '//*[@id="tymethod.slice"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.slice"]/*[@class="code-header"]/a' 'slice'
+    // @has - '//*[@id="tymethod.slice"]/*[@class="code-header"]/a' '&[Rhs]'
     fn slice(&self, other: &[Rhs]);
+    // @count - '//*[@id="tymethod.ref_array"]/*[@class="code-header"]/a' 2
+    // @has - '//*[@id="tymethod.ref_array"]/*[@class="code-header"]/a' 'ref_array'
+    // @has - '//*[@id="tymethod.ref_array"]/*[@class="code-header"]/a' '[Rhs; 2]'
+    fn ref_array(&self, other: &[Rhs; 2]);
     // There should be two `<a>` (method and `Struct`).
     // @count - '//*[@id="tymethod.slice2"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.slice2"]/*[@class="code-header"]/a' 'slice2'
     // @has - '//*[@id="tymethod.slice2"]/*[@class="code-header"]/a' 'Struct'
     fn slice2(&self, other: &[Struct]);
-    // There should be two `<a>` (the method and one for the array primitive).
     // @count - '//*[@id="tymethod.array"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.array"]/*[@class="code-header"]/a' 'array'
-    // @has - '//*[@id="tymethod.array"]/*[@class="code-header"]/a' '2'
+    // @has - '//*[@id="tymethod.array"]/*[@class="code-header"]/a' '[Rhs; 2]'
     fn array(&self, other: [Rhs; 2]);
-    // There should be two `<a>` (method, `Struct` and array primitive).
-    // @count - '//*[@id="tymethod.array2"]/*[@class="code-header"]/a' 3
+    // @count - '//*[@id="tymethod.array2"]/*[@class="code-header"]/a' 2
     // @has - '//*[@id="tymethod.array2"]/*[@class="code-header"]/a' 'array2'
     // @has - '//*[@id="tymethod.array2"]/*[@class="code-header"]/a' 'Struct'
-    // @has - '//*[@id="tymethod.array2"]/*[@class="code-header"]/a' '2'
     fn array2(&self, other: [Struct; 2]);
 }
