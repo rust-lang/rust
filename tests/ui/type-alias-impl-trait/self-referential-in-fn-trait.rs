ab01463f48f497cd89772363ffdbaa52def6cc2e
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-07-26 19:17:25 +0000

    Show that the opaque type works in the new solver and is actually instantiatable

--- tests/ui/type-alias-impl-trait/self-referential-in-fn-trait.rs
+++ tests/ui/type-alias-impl-trait/self-referential-in-fn-trait.rs
@@ -1,10 +1,25 @@
+//! This test checks that we do not
+//! end up with an infinite recursion,
+//! a cycle error or an overflow when
+//! encountering an opaque type that has
+//! an associated type that is just itself
+//! again.
+
 #![feature(type_alias_impl_trait)]
+// revisions: next old working
+//[next] compile-flags: -Ztrait-solver=next
+//[working] check-pass
 
 type Foo<'a> = impl Fn() -> Foo<'a>;
-//~^ ERROR: unconstrained opaque type
+//[old,next]~^ ERROR: unconstrained opaque type
 
 fn crash<'a>(_: &'a (), x: Foo<'a>) -> Foo<'a> {
     x
+}
+
+#[cfg(working)]
+fn foo<'a>() -> Foo<'a> {
+    foo
 }
 
 fn main() {}
