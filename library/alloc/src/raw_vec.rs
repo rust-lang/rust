2a6948b3d0ed271ef9e6cb2521bebeb732347ae3
Thom Chiovoloni <thom@shift.click>
2023-07-26 23:48:40 -0700

    Experiment with removing cold from rawvec

--- library/alloc/src/raw_vec.rs
+++ library/alloc/src/raw_vec.rs
@@ -280,7 +280,6 @@ impl<T, A: Allocator> RawVec<T, A> {
         // Therefore, we move all the resizing and error-handling logic from grow_amortized and
         // handle_reserve behind a call, while making sure that this function is likely to be
         // inlined as just a comparison and a call if the comparison fails.
-        #[cold]
         fn do_reserve_and_handle<T, A: Allocator>(
             slf: &mut RawVec<T, A>,
             len: usize,
