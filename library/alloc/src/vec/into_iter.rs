fd8ba7bc3cfcc72f79da33f74b19c905f0cbb835
The 8472 <git@infinite-source.de>
2023-07-29 16:08:24 +0200

    typo fix

--- library/alloc/src/vec/into_iter.rs
+++ library/alloc/src/vec/into_iter.rs
@@ -136,7 +136,7 @@ impl<T, A: Allocator> IntoIter<T, A> {
 
     /// Forgets to Drop the remaining elements while still allowing the backing allocation to be freed.
     pub(crate) fn forget_remaining_elements(&mut self) {
-        // For th ZST case, it is crucial that we mutate `end` here, not `ptr`.
+        // For the ZST case, it is crucial that we mutate `end` here, not `ptr`.
         // `ptr` must stay aligned, while `end` may be unaligned.
         self.end = self.ptr;
     }
