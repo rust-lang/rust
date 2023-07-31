efa470d0ae6707facf608500c2f7417312dd0f29
Sean Cross <sean@xobs.io>
2023-07-31 17:12:31 +0800

    std: xous: add support for time
    Add support for determining the current time. This connects to the
    ticktimer server in order to get the system uptime.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/sys/xous/mod.rs
+++ library/std/src/sys/xous/mod.rs
@@ -33,7 +33,6 @@ pub mod thread;
 pub mod thread_local_key;
 #[path = "../unsupported/thread_parking.rs"]
 pub mod thread_parking;
-#[path = "../unsupported/time.rs"]
 pub mod time;
 
 #[path = "../unsupported/common.rs"]
