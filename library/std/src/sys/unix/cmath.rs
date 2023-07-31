fcecaff16eeb3f11d84520d3ebcf9b762d0e48eb
Andrew Kane <andrew@ankane.org>
2023-07-31 07:41:50 -0700

    Add gamma and ln_gamma functions to f32 and f64

--- library/std/src/sys/unix/cmath.rs
+++ library/std/src/sys/unix/cmath.rs
@@ -30,4 +30,8 @@ extern "C" {
     pub fn tanf(n: f32) -> f32;
     pub fn tanh(n: f64) -> f64;
     pub fn tanhf(n: f32) -> f32;
+    pub fn tgamma(n: f64) -> f64;
+    pub fn tgammaf(n: f32) -> f32;
+    pub fn lgamma_r(n: f64, s: &mut i32) -> f64;
+    pub fn lgammaf_r(n: f32, s: &mut i32) -> f32;
 }
