9a963e80260f8fc26c502e30ffca43661e454ad0
David CARLIER <devnexen@gmail.com>
2023-08-05 16:15:05 +0100

    std: freebsd build update.
    since freebsd 11 had been removed, minimum is now 12.

--- library/std/build.rs
+++ library/std/build.rs
@@ -4,10 +4,8 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     let target = env::var("TARGET").expect("TARGET was not set");
     if target.contains("freebsd") {
-        if env::var("RUST_STD_FREEBSD_12_ABI").is_ok() {
-            println!("cargo:rustc-cfg=freebsd12");
-        } else if env::var("RUST_STD_FREEBSD_13_ABI").is_ok() {
-            println!("cargo:rustc-cfg=freebsd12");
+        println!("cargo:rustc-cfg=freebsd12");
+        if env::var("RUST_STD_FREEBSD_13_ABI").is_ok() {
             println!("cargo:rustc-cfg=freebsd13");
         }
     } else if target.contains("linux")
