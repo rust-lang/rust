4ab3e9d5b9b1f4163cc5d3208c0c3f7f99f3b2f6
Nicholas Nethercote <n.nethercote@gmail.com>
2023-08-09 15:24:06 +1000

    Add a failing case to `tests/ui/macros/macro-interpolation`.
    This test currently tests the successful paths for the
    `Interpolated`/`NtTy`/`Path` case in `parse_path_inner`, but it doesn't
    test the failure path.

--- tests/ui/macros/macro-interpolation.rs
+++ tests/ui/macros/macro-interpolation.rs
@@ -1,5 +1,3 @@
-// run-pass
-
 macro_rules! overly_complicated {
     ($fnname:ident, $arg:ident, $ty:ty, $body:block, $val:expr, $pat:pat, $res:path) =>
     ({
@@ -21,12 +19,14 @@ macro_rules! qpath {
 
     (ty, <$type:ty as $trait:ty>::$name:ident) => {
         <$type as $trait>::$name
+        //~^ ERROR expected identifier, found `!`
     };
 }
 
 pub fn main() {
     let _: qpath!(path, <str as ToOwned>::Owned);
     let _: qpath!(ty, <str as ToOwned>::Owned);
+    let _: qpath!(ty, <str as !>::Owned);
 
     assert!(overly_complicated!(f, x, Option<usize>, { return Some(x); },
                                Some(8), Some(y), y) == 8)
