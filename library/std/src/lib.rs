6c32a64962f591e6fd4cab72f63555b157684323
Sean Cross <sean@xobs.io>
2023-08-16 09:58:51 +0200

    std: enable feature(slice_ptr_len) on xous
    Xous passes slice pointers around in order to manipulate memory.
    This is feature-gated behind `slice_ptr_len`. Xous is currently
    the only target to use this feature, so gate it behind an OS flag.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -236,6 +236,7 @@
     feature(slice_index_methods, coerce_unsized, sgx_platform)
 )]
 #![cfg_attr(windows, feature(round_char_boundary))]
+#![cfg_attr(target_os = "xous", feature(slice_ptr_len))]
 //
 // Language features:
 // tidy-alphabetical-start
