4469dbe13a8e33f471ac383dd3569305ebf56f86
Tomasz Miąsko <tomasz.miasko@gmail.com>
2023-08-22 00:00:00 +0000

    Contents of reachable statics is reachable
    (cherry picked from commit 0383131f7f48c7ff65b982c4c07dd8a7e6d5ed5e)

--- tests/ui/cross-crate/static-init.rs
+++ tests/ui/cross-crate/static-init.rs
@@ -1,9 +1,11 @@
+// Regression test for #84455 and #115052.
 // run-pass
 // aux-build:static_init_aux.rs
 extern crate static_init_aux as aux;
 
 static V: &u32 = aux::V;
 static F: fn() = aux::F;
+static G: fn() = aux::G;
 
 fn v() -> *const u32 {
     V
@@ -12,4 +14,5 @@ fn v() -> *const u32 {
 fn main() {
     assert_eq!(aux::v(), crate::v());
     F();
+    G();
 }
