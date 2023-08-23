298ec5258f8e5f1211734bcb43c89aa184d3ca1b
Wesley Wiser <wesleywiser@microsoft.com>
2023-08-23 13:31:38 -0400

    Disable MIR SROA optimization by default
    Turn off the MIR SROA optimization by default to prevent incorrect
    debuginfo generation and rustc ICEs caused by invalid LLVM IR being
    created.
    
    Related to #115113

--- compiler/rustc_mir_transform/src/sroa.rs
+++ compiler/rustc_mir_transform/src/sroa.rs
@@ -12,7 +12,7 @@ pub struct ScalarReplacementOfAggregates;
 
 impl<'tcx> MirPass<'tcx> for ScalarReplacementOfAggregates {
     fn is_enabled(&self, sess: &rustc_session::Session) -> bool {
-        sess.mir_opt_level() >= 2
+        sess.mir_opt_level() >= 3
     }
 
     #[instrument(level = "debug", skip(self, tcx, body))]
