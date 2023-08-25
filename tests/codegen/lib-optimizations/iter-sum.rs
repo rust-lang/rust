72b01d5cca0604e1ea5818d90b9feefae92a0093
The 8472 <git@infinite-source.de>
2023-08-25 23:29:18 +0200

    Optimize Take::{fold, for_each} when wrapping TrustedRandomAccess iterators

---
+++ tests/codegen/lib-optimizations/iter-sum.rs
@@ -0,0 +1,14 @@
+// ignore-debug: the debug assertions get in the way
+// compile-flags: -O
+#![crate_type = "lib"]
+
+
+// Ensure that slice + take + sum gets vectorized.
+// Currently this relies on the slice::Iter::try_fold implementation
+// CHECK-LABEL: @slice_take_sum
+#[no_mangle]
+pub fn slice_take_sum(s: &[u64], l: usize) -> u64 {
+    // CHECK: vector.body:
+    // CHECK: ret
+    s.iter().take(l).sum()
+}
