7a653feffbe936eb2d1f88e47f28dd3ffd81d9eb
Santiago Pastorino <spastorino@gmail.com>
2023-08-28 14:55:08 -0300

    Remove stable_mir::generics_of/predicates_of those shouldn't be exposed

--- compiler/rustc_smir/src/stable_mir/mod.rs
+++ compiler/rustc_smir/src/stable_mir/mod.rs
@@ -109,14 +109,6 @@ pub fn trait_impl(trait_impl: &ImplDef) -> ImplTrait {
     with(|cx| cx.trait_impl(trait_impl))
 }
 
-pub fn generics_of(generic_def: &GenericDef) -> Generics {
-    with(|cx| cx.generics_of(generic_def))
-}
-
-pub fn predicates_of(trait_def: &TraitDef) -> GenericPredicates {
-    with(|cx| cx.predicates_of(trait_def))
-}
-
 pub trait Context {
     fn entry_fn(&mut self) -> Option<CrateItem>;
     /// Retrieve all items of the local crate that have a MIR associated with them.
