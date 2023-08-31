df19546bf7d62a45ae6287590691127f29706b24
John Kelly <johnharrykelly@gmail.com>
2023-08-31 14:17:58 +0100

    Address review comments

--- compiler/rustc_lint/src/unnecessary_send_constraint.rs
+++ compiler/rustc_lint/src/unnecessary_send_constraint.rs
@@ -5,19 +5,20 @@ use crate::hir;
 use crate::{lints::UnnecessarySendConstraintDiag, LateContext, LateLintPass};
 
 declare_lint! {
-    /// The `lint_unnecessary_send_constraint` lints unnecessary constraint of references to `Send`.
+    /// The `unnecessary_send_constraint` lints unnecessary constraint of references to `Send`.
     ///
     /// ### Example
     ///
     /// ```rust,compile_fail
-    /// fn foo(_: &(dyn Any + Send>) {}
+    /// #[deny(unnecessary_send_constraint)]
+    /// fn foo(_: &(dyn Any + Send)) {}
     /// ```
     ///
     /// {{produces}}
     ///
     /// ### Explanation
     ///
-    /// References cannot be sent across threads unless they have a `Sync` bound, so constraining them to `Send` without `Sync` is unnecessary.
+    /// Shared references cannot be sent across threads unless they have a `Sync` bound, so constraining them to `Send` without `Sync` is unnecessary.
     pub UNNECESSARY_SEND_CONSTRAINT,
     Warn,
     "constraining a reference to `Send` without `Sync` is unnecessary, consider removing it"
