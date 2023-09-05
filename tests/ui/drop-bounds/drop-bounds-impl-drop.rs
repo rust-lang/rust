6a0b1bc177198037b60fc35b0e96afc7d60ad0e1
ouz-a <ouz.agz@gmail.com>
2023-09-05 17:52:39 +0300

    make voldemort less cryptic

--- tests/ui/drop-bounds/drop-bounds-impl-drop.rs
+++ tests/ui/drop-bounds/drop-bounds-impl-drop.rs
@@ -2,6 +2,7 @@
 #![deny(drop_bounds)]
 // As a special exemption, `impl Drop` in the return position raises no error.
 // This allows a convenient way to return an unnamed drop guard.
+// Voldemort here refers to types that are unnameable.
 fn voldemort_type() -> impl Drop {
   struct Voldemort;
   impl Drop for Voldemort {
