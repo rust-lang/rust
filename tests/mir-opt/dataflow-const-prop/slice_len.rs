b7a925a7ce37c5163ce81abec0fdf8d848d34ae5
Camille GILLOT <gillot.camille@gmail.com>
2023-09-07 15:59:05 +0000

    Add test where slice is a const.

--- tests/mir-opt/dataflow-const-prop/slice_len.rs
+++ tests/mir-opt/dataflow-const-prop/slice_len.rs
@@ -5,5 +5,8 @@
 
 // EMIT_MIR slice_len.main.DataflowConstProp.diff
 fn main() {
-    (&[1u32, 2, 3] as &[u32])[1];
+    let local = (&[1u32, 2, 3] as &[u32])[1];
+
+    const SLICE: &[u32] = &[1, 2, 3];
+    let constant = SLICE[1];
 }
