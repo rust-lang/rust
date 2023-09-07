4da6eb2ec6141eb7b4172d62e48c277ea8a191fb
Michael Goulet <michael@errs.io>
2023-09-07 05:11:41 +0000

    Test showing it doesnt work

---
+++ tests/ui/resolve/suggest-import-without-clobbering-attrs.rs
@@ -0,0 +1,15 @@
+// run-rustfix
+// compile-flags: --cfg=whatever -Aunused
+
+#[cfg(whatever)]
+use y::Whatever;
+
+mod y {
+    pub(crate) fn z() {}
+    pub(crate) struct Whatever;
+}
+
+fn main() {
+    z();
+    //~^ ERROR cannot find function `z` in this scope
+}
