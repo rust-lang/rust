2325f9143bbe0dfa2565e437823d1f3950642b90
David Wood <david.wood@huawei.com>
2023-09-08 14:20:07 +0100

    target compliance: mipsel-sony-psx
    Signed-off-by: David Wood <david@davidtw.co>

---
+++ tests/assembly/targets/mipsel_sony_psx.rs
@@ -0,0 +1,17 @@
+// assembly-output: emit-asm
+// compile-flags: --target mipsel-sony-psx
+// needs-llvm-components: mips
+
+#![feature(no_core, lang_items)]
+#![no_std]
+#![no_core]
+#![crate_type = "lib"]
+
+#[lang = "sized"]
+trait Sized {}
+
+pub fn test() -> u8 {
+    42
+}
+
+// CHECK: .section
