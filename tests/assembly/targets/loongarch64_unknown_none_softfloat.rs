4354c653529b984b916dc63b75af7cceec006b16
David Wood <david.wood@huawei.com>
2023-09-08 14:40:55 +0100

    target compliance: loongarch64-unknown-none{,-softfloat}
    Signed-off-by: David Wood <david@davidtw.co>

---
+++ tests/assembly/targets/loongarch64_unknown_none_softfloat.rs
@@ -0,0 +1,17 @@
+// assembly-output: emit-asm
+// compile-flags: --target loongarch64-unknown-none-softfloat
+// needs-llvm-components: loongarch
+
+#![feature(no_core, lang_items)]
+#![no_std]
+#![no_core]
+#![crate_type = "lib"]
+
+#[lang = "sized"]
+trait Sized {}
+
+pub fn test() -> u8 {
+    42
+}
+
+// CHECK: .section
