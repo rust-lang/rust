387d4fbf9e3f5aeb80f59181ba83fc12b09d5d9d
David Wood <david.wood@huawei.com>
2023-09-11 16:24:16 +0100

    target compliance: x86_64-unknown-none
    Signed-off-by: David Wood <david@davidtw.co>

---
+++ tests/assembly/targets/x86_64_unknown_none.rs
@@ -0,0 +1,17 @@
+// assembly-output: emit-asm
+// compile-flags: --target x86_64-unknown-none
+// needs-llvm-components: x86
+
+#![feature(no_core, lang_items)]
+#![no_std]
+#![no_core]
+#![crate_type = "lib"]
+
+#[lang = "sized"]
+trait Sized {}
+
+pub fn test() -> u8 {
+    42
+}
+
+// CHECK: .section
