122d1cc8c332b4dd176e1475a063688e73d95168
Kai Luo <lkail@cn.ibm.com>
2023-09-13 16:10:26 +0800

    Adjust comment

--- src/bootstrap/bootstrap.py
+++ src/bootstrap/bootstrap.py
@@ -314,9 +314,9 @@ def default_build_triple(verbose):
         return 'x86_64-pc-windows-msvc'
     elif kernel == 'AIX':
         # `uname -m` returns the machine ID rather than machine hardware on AIX,
-        # so we are unable to use cputype to form triple. Since AIX 7.2 and
+        # so we are unable to use cputype to form triple. AIX 7.2 and
         # above supports 32-bit and 64-bit mode simultaneously and `uname -p`
-        # returns `powerpc`. Currently we only supports `powerpc64-ibm-aix` in
+        # returns `powerpc`, however we only supports `powerpc64-ibm-aix` in
         # rust on AIX. For above reasons, kerneltype_mapper and cputype_mapper
         # are not used to infer AIX's triple.
         return 'powerpc64-ibm-aix'
