4f903d0107441bcba6553226e0e0399ab031dd58
J.A.A.Tsimouris <jim@tsimouris.com>
2023-09-19 17:34:37 +0300

    Create rust_contributor_shell.nix

---
+++ nix-shell/rust_contributor_shell.nix
@@ -0,0 +1,24 @@
+# Rust Contributor Shell
+let
+  # Unstable Channel | Rolling Release
+  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) { };
+
+  packages = with pkgs; [
+    pkg-config
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    python3
+    git
+    libgcc
+    gnumake
+    curl
+    cmake
+    libstc
+    libstdc++5
+  ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+}
