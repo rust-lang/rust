25407bc0bb2fd4aa2c1a37c81a72084160d097fd
Nicholas Nethercote <n.nethercote@gmail.com>
2023-09-20 07:51:24 +1000

    Make `DroplessArena::alloc` call `DroplessArena::alloc_raw`.
    They're very similar.

--- compiler/rustc_arena/src/lib.rs
+++ compiler/rustc_arena/src/lib.rs
@@ -501,14 +501,7 @@ impl DroplessArena {
         assert!(!mem::needs_drop::<T>());
         assert!(mem::size_of::<T>() != 0);
 
-        let layout = Layout::new::<T>();
-        let mem = if let Some(a) = self.alloc_raw_without_grow(layout) {
-            a
-        } else {
-            // No free space left. Allocate a new chunk to satisfy the request.
-            // On failure the grow will panic or abort.
-            self.grow_and_alloc_raw(layout)
-        } as *mut T;
+        let mem = self.alloc_raw(Layout::new::<T>()) as *mut T;
 
         unsafe {
             // Write into uninitialized memory.
