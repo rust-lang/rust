ccf57997826581da83eff3a29cadc1c9fb728875
Guillaume Gomez <guillaume1.gomez@gmail.com>
2023-09-25 17:04:44 +0200

    Use cargo to build the build system binary

--- y.sh
+++ y.sh
@@ -2,6 +2,7 @@
 
 set -e
 echo "[BUILD] build system" 1>&2
-mkdir -p build_system/target
-rustc build_system/src/main.rs -o build_system/target/y -Cdebuginfo=1 --edition 2021
-exec ./build_system/target/y "$@"
+cd build_system
+cargo build --release
+cd ..
+./build_system/target/release/y $@
