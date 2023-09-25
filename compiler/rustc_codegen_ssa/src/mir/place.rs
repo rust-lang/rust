0cb6c3463519a3cfaf4e7b16a33efc4d02ec8a49
ouz-a <ouz.agz@gmail.com>
2023-09-25 15:04:37 +0300

    add bug! for unused opaquecast

--- compiler/rustc_codegen_ssa/src/mir/place.rs
+++ compiler/rustc_codegen_ssa/src/mir/place.rs
@@ -463,7 +463,9 @@ impl<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>> FunctionCx<'a, 'tcx, Bx> {
                 mir::ProjectionElem::Field(ref field, _) => {
                     cg_base.project_field(bx, field.index())
                 }
-                mir::ProjectionElem::OpaqueCast(ty) => cg_base.project_type(bx, ty),
+                mir::ProjectionElem::OpaqueCast(_) => {
+                    bug!("OpaqueCast should be unreachable here!")
+                }
                 mir::ProjectionElem::Index(index) => {
                     let index = &mir::Operand::Copy(mir::Place::from(index));
                     let index = self.codegen_operand(bx, index);
