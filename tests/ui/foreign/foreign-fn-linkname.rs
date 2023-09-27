ac9707d25cb05b43a4466dbf18ca56f1b463ea78
apekros <bayleyfoster@gmail.com>
2023-09-27 18:05:14 +1000

    Add regression test for rust-lang#56098

--- tests/ui/foreign/foreign-fn-linkname.rs
+++ tests/ui/foreign/foreign-fn-linkname.rs
@@ -2,6 +2,9 @@
 // ignore-wasm32-bare no libc to test ffi with
 // ignore-sgx no libc
 
+// Ensure no false positive on "unused extern crate" lint
+#![deny(unused_extern_crates)]
+
 #![feature(rustc_private)]
 
 extern crate libc;
