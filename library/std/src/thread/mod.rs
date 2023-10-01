351830086484f4b4c1744a7fbeb4d38bf6e6846a
Jakub Beránek <berykubik@gmail.com>
2023-10-01 21:58:05 +0200

    Fix default stack size in stdlib docs

--- library/std/src/thread/mod.rs
+++ library/std/src/thread/mod.rs
@@ -125,7 +125,7 @@
 //! ## Stack size
 //!
 //! The default stack size is platform-dependent and subject to change.
-//! Currently, it is 2 MiB on all Tier-1 platforms.
+//! Currently, it is 8 MiB on all Tier-1 platforms.
 //!
 //! There are two ways to manually specify the stack size for spawned threads:
 //!
