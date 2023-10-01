e683e3eeac5603296c328ff45c9e5bb911327756
Victor Song <vms2@rice.edu>
2023-09-30 22:57:54 -0500

    Don't lint `manual_non_exhaustive` when enum explicitly marked as `non_exhaustive`
    There are cases where users create a unit variant for the purposes
    of tracking the number of variants for an nonexhaustive enum.
    We should check if an enum is explicitly marked as nonexhaustive
    before reporting `manual_non_exhaustive` in these cases. Fixes #11583

--- clippy_lints/src/manual_non_exhaustive.rs
+++ clippy_lints/src/manual_non_exhaustive.rs
@@ -3,6 +3,7 @@ use clippy_utils::is_doc_hidden;
 use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::source::snippet_opt;
 use rustc_ast::ast::{self, VisibilityKind};
+use rustc_ast::attr;
 use rustc_data_structures::fx::FxHashSet;
 use rustc_errors::Applicability;
 use rustc_hir::def::{CtorKind, CtorOf, DefKind, Res};
@@ -158,7 +159,8 @@ impl<'tcx> LateLintPass<'tcx> for ManualNonExhaustiveEnum {
             let mut iter = def.variants.iter().filter_map(|v| {
                 (matches!(v.data, hir::VariantData::Unit(_, _))
                     && v.ident.as_str().starts_with('_')
-                    && is_doc_hidden(cx.tcx.hir().attrs(v.hir_id)))
+                    && is_doc_hidden(cx.tcx.hir().attrs(v.hir_id))
+                    && !attr::contains_name(cx.tcx.hir().attrs(item.hir_id()), sym::non_exhaustive))
                 .then_some((v.def_id, v.span))
             });
             if let Some((id, span)) = iter.next()
