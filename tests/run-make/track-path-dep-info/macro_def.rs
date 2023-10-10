fa67588da7114360a5505650661f9ef255f6ed9d
Bernhard Schuster <bernhard@ahoi.io>
2023-10-11 01:09:00 +0200

    fix test

--- tests/run-make/track-path-dep-info/macro_def.rs
+++ tests/run-make/track-path-dep-info/macro_def.rs
@@ -8,14 +8,14 @@ use std::str;
 
 #[proc_macro]
 pub fn access_tracked_paths(_: TokenStream) -> TokenStream {
-    assert!(tracked::path("emojis.txt").is_ok());
+    assert!(tracked_path::path("emojis.txt").is_ok());
 
     // currently only valid utf-8 paths are supported
     let invalid = [1_u8, 2,123, 254, 0, 0, 1, 1];
     let invalid: &str = unsafe {
         str::from_utf8_unchecked(&invalid[..])
     };
-    assert!(tracked::path(invalid).is_err());
+    assert!(tracked_path::path(invalid).is_err());
 
     TokenStream::new()
 }
