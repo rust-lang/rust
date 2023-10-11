de74b36e200ed129c36e1ed97baff3b5635774b8
Trevor Gross <tmgross@umich.edu>
2023-10-11 14:15:04 -0400

    Update target specification strings

--- compiler/rustc_target/src/spec/x86_64_unikraft_linux_musl.rs
+++ compiler/rustc_target/src/spec/x86_64_unikraft_linux_musl.rs
@@ -5,8 +5,8 @@ pub fn target() -> Target {
         llvm_target: "x86_64-unknown-linux-musl".into(),
         pointer_width: 64,
         arch: "x86_64".into(),
-        data_layout: "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
-            .into(),
+        data_layout:
+            "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-i128:128-f80:128-n8:16:32:64-S128".into(),
         options: TargetOptions {
             cpu: "x86-64".into(),
             plt_by_default: false,
