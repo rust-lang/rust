75081b71edfccf66779f3076e56f096b76944378
Camille GILLOT <gillot.camille@gmail.com>
2023-10-16 19:39:34 +0000

    FileCheck duplicate_switch_targets.

--- tests/mir-opt/instsimplify/duplicate_switch_targets.rs
+++ tests/mir-opt/instsimplify/duplicate_switch_targets.rs
@@ -1,14 +1,15 @@
-// skip-filecheck
+// unit-test: InstSimplify
+
 #![feature(custom_mir, core_intrinsics)]
 #![crate_type = "lib"]
 
 use std::intrinsics::mir::*;
 
-// unit-test: InstSimplify
-
-// EMIT_MIR instsimplify_duplicate_switch_targets.assert_zero.InstSimplify.diff
+// EMIT_MIR duplicate_switch_targets.assert_zero.InstSimplify.diff
 #[custom_mir(dialect = "runtime", phase = "post-cleanup")]
 pub unsafe fn assert_zero(x: u8) -> u8 {
+    // CHECK-LABEL: fn assert_zero(
+    // CHECK: switchInt({{.*}}) -> [0: {{bb.*}}, otherwise: {{bb.*}}]
     mir!(
         {
             match x {
