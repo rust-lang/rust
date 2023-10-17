7d0675125c948fbf78f8dbfaf8d6824c2263082e
Felix S Klock II <pnkfelix@pnkfx.org>
2023-10-17 11:10:33 -0400

    Update tests/run-make/wasm-builtins-import/main.rs
    clarify seemingly circular reference

--- tests/run-make/wasm-builtins-import/main.rs
+++ tests/run-make/wasm-builtins-import/main.rs
@@ -9,6 +9,6 @@ fn my_panic(_info: &core::panic::PanicInfo) -> ! {
 #[no_mangle]
 pub fn multer(a: i128, b: i128) -> i128 {
     // Trigger usage of the __multi3 compiler intrinsic which then leads to an imported function
-    // such as panic or __multi3 in case of a bug. We verify that no imports exist in our verifier.
+    // such as panic or __multi3 (externally defined) in case of a bug. We verify that no imports exist in our verifier.
     a * b
 }
