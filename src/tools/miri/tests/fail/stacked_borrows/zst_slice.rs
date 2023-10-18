73afc00cf99f9db95dc35b0bd33604c3879ca3e7
bendn <bend.n@outlook.com>
2023-10-19 06:46:28 +0700

    use `assume(idx < self.len())` in `[T]::get_unchecked`

--- src/tools/miri/tests/fail/stacked_borrows/zst_slice.rs
+++ src/tools/miri/tests/fail/stacked_borrows/zst_slice.rs
@@ -1,11 +1,10 @@
 //@compile-flags: -Zmiri-strict-provenance
-//@error-in-other-file: /retag .* tag does not exist in the borrow stack/
 
 fn main() {
     unsafe {
         let a = [1, 2, 3];
         let s = &a[0..0];
         assert_eq!(s.len(), 0);
-        assert_eq!(*s.get_unchecked(1), 2);
+        assert_eq!(*s.as_ptr().add(1), 2); //~ ERROR: /retag .* tag does not exist in the borrow stack/
     }
 }
