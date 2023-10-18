79bb50ab1e93339a447fb4a543ec098058324e93
León Orell Valerian Liehr <me@fmease.dev>
2023-10-18 11:03:28 +0200

    rustdoc: hide `#[repr(...)]` if it isn't part of the public ABI

--- tests/rustdoc/inline_cross/auxiliary/repr.rs
+++ tests/rustdoc/inline_cross/auxiliary/repr.rs
@@ -1,24 +1,58 @@
 #![feature(repr_simd)]
 
-#[repr(C, align(8))]
+#[repr(Rust)]
+pub struct ReprRust;
+
+#[repr(C, align(8))] // public
 pub struct ReprC {
-    field: u8,
-}
-#[repr(simd, packed(2))]
-pub struct ReprSimd {
-    field: u8,
-}
-#[repr(transparent)]
-pub struct ReprTransparent {
     pub field: u8,
 }
-#[repr(isize)]
+
+#[repr(C)] // private
+pub struct ReprCPrivField {
+    private: u8,
+    pub public: i8,
+}
+
+#[repr(align(4))] // private
+pub struct ReprAlignHiddenField {
+    #[doc(hidden)]
+    pub hidden: i16,
+}
+
+#[repr(simd, packed(2))] // public
+pub struct ReprSimd {
+    pub field: u8,
+}
+
+#[repr(transparent)] // public
+pub struct ReprTransparent {
+    pub field: u8, // non-1-ZST field
+}
+
+#[repr(isize)] // public
 pub enum ReprIsize {
     Bla,
 }
-#[repr(u8)]
+
+#[repr(u8)] // public
 pub enum ReprU8 {
     Bla,
+}
+
+#[repr(u32)] // public
+pub enum ReprU32 {
+    #[doc(hidden)]
+    Hidden,
+    Public,
+}
+
+#[repr(u64)] // private
+pub enum ReprU64HiddenVariants {
+    #[doc(hidden)]
+    A,
+    #[doc(hidden)]
+    B,
 }
 
 #[repr(transparent)] // private
