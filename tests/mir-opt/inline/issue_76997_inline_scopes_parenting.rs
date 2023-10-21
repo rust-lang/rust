bb695977de920f47eb87ccf7580bb04a6bd36350
Ryan Mehri <ryan.mehri1@gmail.com>
2023-10-20 21:57:08 -0700

    FileCheck inline_scopes_parenting

--- tests/mir-opt/inline/issue_76997_inline_scopes_parenting.rs
+++ tests/mir-opt/inline/issue_76997_inline_scopes_parenting.rs
@@ -1,8 +1,12 @@
-// skip-filecheck
 // Tests that MIR inliner can handle `SourceScopeData` parenting correctly. (#76997)
 
 // EMIT_MIR issue_76997_inline_scopes_parenting.main.Inline.after.mir
 fn main() {
+    // CHECK-LABEL: fn main(
+    // CHECK: scope 2
+    // CHECK-NEXT: debug x
+    // CHECK-NEXT: scope 3
+    // CHECK-NEXT: debug y
     let f = |x| { let y = x; y };
     f(())
 }
