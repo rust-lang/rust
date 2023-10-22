dc00d03a11337af94d943e6a52686da636f10760
dirreke <mingyang_ge@163.com>
2023-10-22 21:20:30 +0800

    add target csky-unknown-linux-gnuabiv2hf

--- compiler/rustc_target/src/spec/csky_unknown_linux_gnuabiv2hf.rs
+++ compiler/rustc_target/src/spec/csky_unknown_linux_gnuabiv2hf.rs
@@ -10,9 +10,10 @@ pub fn target() -> Target {
         data_layout: "e-m:e-S32-p:32:32-i32:32:32-i64:32:32-f32:32:32-f64:32:32-v64:32:32-v128:32:32-a:0:32-Fi32-n32".into(),
         arch: "csky".into(),
         options: TargetOptions {
-            abi: "abiv2".into(),
-            features: "+2e3,+3e7,+7e10,+cache,+dsp1e2,+dspe60,+e1,+e2,+edsp,+elrw,+hard-tp,+high-registers,+hwdiv,+mp,+mp1e2,+nvic,+trust".into(),
-            late_link_args: TargetOptions::link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-l:libatomic.a"]),
+            abi: "abiv2hf".into(),
+            cpu: "ck860fv".into(),
+            features: "+hard-float,+hard-float-abi,+2e3,+3e7,+7e10,+cache,+dsp1e2,+dspe60,+e1,+e2,+edsp,+elrw,+hard-tp,+high-registers,+hwdiv,+mp,+mp1e2,+nvic,+trust".into(),
+            late_link_args: TargetOptions::link_args(LinkerFlavor::Gnu(Cc::Yes, Lld::No), &["-l:libatomic.a", "-mhard-float"]),
             max_atomic_width: Some(32),
             ..super::linux_gnu_base::opts()
         },
