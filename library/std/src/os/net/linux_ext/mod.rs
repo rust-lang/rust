2ed49fc8c75fdf3b47a05840218b2c8489fea794
John Millikin <john@john-millikin.com>
2023-10-25 17:01:21 +0900

    Convert `Unix{Datagram,Stream}::{set_}passcred()` to per-OS traits
    These methods are the pre-stabilized API for obtaining peer credentials
    from an `AF_UNIX` socket, part of the `unix_socket_ancillary_data` feature.
    
    Their current behavior is to get/set one of the `SO_PASSCRED` (Linux),
    `LOCAL_CREDS_PERSISTENT` (FreeBSD), or `LOCAL_CREDS` (NetBSD) socket
    options. On other targets the `{set_}passcred()` methods do not exist.
    
    There are two problems with this approach:
    
    1. Having public methods only exist for certain targets isn't permitted
       in a stable `std` API.
    
    2. These options have generally similar purposes, but they are non-POSIX
       and their details can differ in subtle and surprising ways (such as
       whether they continue to be set after the next call to `recvmsg()`).
    
    Splitting into OS-specific extension traits is the preferred solution to
    both problems.

--- library/std/src/os/net/linux_ext/mod.rs
+++ library/std/src/os/net/linux_ext/mod.rs
@@ -5,6 +5,9 @@
 #[stable(feature = "unix_socket_abstract", since = "1.70.0")]
 pub(crate) mod addr;
 
+#[unstable(feature = "unix_socket_ancillary_data", issue = "76915")]
+pub(crate) mod socket;
+
 #[unstable(feature = "tcp_quickack", issue = "96256")]
 pub(crate) mod tcp;
 
