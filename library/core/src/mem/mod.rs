1a81b928e01689a726207406c554e52343fabd05
Carter Hunt Fogelman <chfogelman@gmail.com>
2023-10-25 13:51:46 -0700

    Refactor unsafe implementation of std::mem::replace
    The function now defers to mem::swap, which both
    eliminates the use of `unsafe` in the function, and
    also allows it (and mem::take) to benefit from
    any performance enhancements to mem::swap, such as
    the current use of ptr::swap_nonoverlapping for large
    types.

--- library/core/src/mem/mod.rs
+++ library/core/src/mem/mod.rs
@@ -908,15 +908,9 @@ pub fn take<T: Default>(dest: &mut T) -> T {
 #[must_use = "if you don't need the old value, you can just assign the new value directly"]
 #[rustc_const_unstable(feature = "const_replace", issue = "83164")]
 #[cfg_attr(not(test), rustc_diagnostic_item = "mem_replace")]
-pub const fn replace<T>(dest: &mut T, src: T) -> T {
-    // SAFETY: We read from `dest` but directly write `src` into it afterwards,
-    // such that the old value is not duplicated. Nothing is dropped and
-    // nothing here can panic.
-    unsafe {
-        let result = ptr::read(dest);
-        ptr::write(dest, src);
-        result
-    }
+pub const fn replace<T>(dest: &mut T, mut src: T) -> T {
+    swap(dest, &mut src);
+    src
 }
 
 /// Disposes of a value.
