b10902e7885df446365fa2da2e80d9515a9d17f4
Amanieu d'Antras <amanieu@gmail.com>
2023-10-26 12:09:35 +0100

    Fix 'since' in simd_avx512_types stability attribute

--- library/stdarch/crates/core_arch/src/x86/mod.rs
+++ library/stdarch/crates/core_arch/src/x86/mod.rs
@@ -258,7 +258,7 @@ types! {
     ///
     /// Note that this means that an instance of `__m512i` typically just means
     /// a "bag of bits" which is left up to interpretation at the point of use.
-    #[stable(feature = "simd_avx512_types", since = "CURRENT_RUSTC_VERSION")]
+    #[stable(feature = "simd_avx512_types", since = "1.72.0")]
     pub struct __m512i(i64, i64, i64, i64, i64, i64, i64, i64);
 
     /// 512-bit wide set of sixteen `f32` types, x86-specific
@@ -276,7 +276,7 @@ types! {
     /// Most intrinsics using `__m512` are prefixed with `_mm512_` and are
     /// suffixed with "ps" (or otherwise contain "ps"). Not to be confused with
     /// "pd" which is used for `__m512d`.
-    #[stable(feature = "simd_avx512_types", since = "CURRENT_RUSTC_VERSION")]
+    #[stable(feature = "simd_avx512_types", since = "1.72.0")]
     pub struct __m512(
         f32, f32, f32, f32, f32, f32, f32, f32,
         f32, f32, f32, f32, f32, f32, f32, f32,
@@ -297,7 +297,7 @@ types! {
     /// Most intrinsics using `__m512d` are prefixed with `_mm512_` and are
     /// suffixed with "pd" (or otherwise contain "pd"). Not to be confused with
     /// "ps" which is used for `__m512`.
-    #[stable(feature = "simd_avx512_types", since = "CURRENT_RUSTC_VERSION")]
+    #[stable(feature = "simd_avx512_types", since = "1.72.0")]
     pub struct __m512d(f64, f64, f64, f64, f64, f64, f64, f64);
 
     /// 128-bit wide set of eight `u16` types, x86-specific
