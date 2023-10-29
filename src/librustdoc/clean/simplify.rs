3784adcd64bb6a2a871dcb47de527cd6aa0953e4
Noah Lev <camelidcamel@gmail.com>
2023-10-29 01:44:12 -0400

    rustdoc: Use `ThinVec` in `GenericParamDefKind`
    This should hopefully reduce memory usage and improve performance since
    these vectors are often empty (and `GenericParamDefKind` is constructed
    *a lot*).

--- src/librustdoc/clean/simplify.rs
+++ src/librustdoc/clean/simplify.rs
@@ -145,7 +145,7 @@ pub(crate) fn move_bounds_to_generic_parameters(generics: &mut clean::Generics) 
                 ..
             }) = generics.params.iter_mut().find(|param| &param.name == arg)
         {
-            param_bounds.append(bounds);
+            param_bounds.extend(bounds.drain(..));
         } else if let WherePredicate::RegionPredicate { lifetime: Lifetime(arg), bounds } = &mut pred
             && let Some(GenericParamDef {
                 kind: GenericParamDefKind::Lifetime { outlives: param_bounds },
