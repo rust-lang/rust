5e6c313cafd636bf54973d64875ebeba635dc612
Havard Eidnes <he@NetBSD.org>
2023-10-29 13:53:24 +0000

    mipsel_unknown_netbsd.rs: fix indentation.

--- compiler/rustc_target/src/spec/mipsel_unknown_netbsd.rs
+++ compiler/rustc_target/src/spec/mipsel_unknown_netbsd.rs
@@ -7,15 +7,15 @@ pub fn target() -> Target {
     base.cpu = "mips32".into();
 
     Target {
-       llvm_target: "mipsel-unknown-netbsd".into(),
-       pointer_width: 32,
-       data_layout: "e-m:m-p:32:32-i8:8:32-i16:16:32-i64:64-n32-S64".into(),
-       arch: "mips".into(),
-       options: TargetOptions {
-           features: "+soft-float".into(),
-           mcount: "__mcount".into(),
-           endian: Endian::Little,
-           ..base
-       },
+        llvm_target: "mipsel-unknown-netbsd".into(),
+        pointer_width: 32,
+        data_layout: "e-m:m-p:32:32-i8:8:32-i16:16:32-i64:64-n32-S64".into(),
+        arch: "mips".into(),
+        options: TargetOptions {
+            features: "+soft-float".into(),
+            mcount: "__mcount".into(),
+            endian: Endian::Little,
+            ..base
+        },
     }
 }
