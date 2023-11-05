9a819ab8f7f69b5461d0a9a7f8b57f976de1ae2e
Ralf Jung <post@ralfj.de>
2023-11-05 16:58:47 +0100

    static mut: allow reference to arbitrary types, not just slices and arrays

--- tests/ui/consts/const-address-of-mut.rs
+++ tests/ui/consts/const-address-of-mut.rs
@@ -4,8 +4,6 @@ const A: () = { let mut x = 2; &raw mut x; };           //~ mutable pointer
 
 static B: () = { let mut x = 2; &raw mut x; };          //~ mutable pointer
 
-static mut C: () = { let mut x = 2; &raw mut x; };      //~ mutable pointer
-
 const fn foo() {
     let mut x = 0;
     let y = &raw mut x;                                 //~ mutable pointer
