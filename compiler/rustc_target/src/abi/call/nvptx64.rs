ecb84e20bae00ded5edc207a799bd8abeb225068
Kjetil Kjeka <kjetil@muybridge.com>
2023-11-07 14:54:37 +0100

    NVPTX: Avoid PassMode::Direct for args in C abi

--- compiler/rustc_target/src/abi/call/nvptx64.rs
+++ compiler/rustc_target/src/abi/call/nvptx64.rs
@@ -2,14 +2,16 @@ use crate::abi::call::{ArgAbi, FnAbi, PassMode, Reg, Size, Uniform};
 use crate::abi::{HasDataLayout, TyAbiInterface};
 
 fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
-    if ret.layout.is_aggregate() && ret.layout.size.bits() > 64 {
-        ret.make_indirect();
+    if !ret.layout.is_aggregate() && ret.layout.size.bits() < 32 {
+        ret.extend_integer_width_to(32);
     }
 }
 
 fn classify_arg<Ty>(arg: &mut ArgAbi<'_, Ty>) {
-    if arg.layout.is_aggregate() && arg.layout.size.bits() > 64 {
-        arg.make_indirect();
+    if arg.layout.is_aggregate() {
+        arg.make_indirect_byval(None);
+    } else if arg.layout.size.bits() < 32 {
+        arg.extend_integer_width_to(32);
     }
 }
 
