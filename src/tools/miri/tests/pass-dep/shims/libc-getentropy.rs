b7a98e359596f2d572e7f4735627e16d11e3031e
David Carlier <devnexen@gmail.com>
2023-11-12 21:08:47 +0000

    freebsd adding getentropy interception support

---
+++ src/tools/miri/tests/pass-dep/shims/libc-getentropy.rs
@@ -0,0 +1,11 @@
+//@only-target-freebsd
+
+fn main() {
+    let mut buf1 = [0u8; 256];
+    let mut buf2 = [0u8; 257];
+    unsafe {
+        assert_eq!(libc::getentropy(buf1.as_mut_ptr() as *mut libc::c_void, buf1.len()), 0);
+        assert_eq!(libc::getentropy(buf2.as_mut_ptr() as *mut libc::c_void, buf2.len()), -1);
+        assert_eq!(std::io::Error::last_os_error().raw_os_error().unwrap(), libc::EIO);
+    }
+}
