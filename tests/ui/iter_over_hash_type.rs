f8ea496495d2766536d7f0fe11c03d609e8ed837
Jacherr <jwc2002@outlook.com>
2023-11-13 16:48:52 +0000

    add tests for type-aliased hash types

--- tests/ui/iter_over_hash_type.rs
+++ tests/ui/iter_over_hash_type.rs
@@ -1,15 +1,20 @@
 //@aux-build:proc_macros.rs
-
+#![feature(rustc_private)]
 #![warn(clippy::iter_over_hash_type)]
 use std::collections::{HashMap, HashSet};
+
+extern crate rustc_data_structures;
 
 extern crate proc_macros;
 
 fn main() {
     let mut hash_set = HashSet::<i32>::new();
     let mut hash_map = HashMap::<i32, i32>::new();
+    let mut fx_hash_map = rustc_data_structures::fx::FxHashMap::<i32, i32>::default();
+    let mut fx_hash_set = rustc_data_structures::fx::FxHashMap::<i32, i32>::default();
     let vec = Vec::<i32>::new();
 
+    // test hashset
     for x in &hash_set {
         let _ = x;
     }
@@ -22,6 +27,8 @@ fn main() {
     for x in hash_set.drain() {
         let _ = x;
     }
+
+    // test hashmap
     for (x, y) in &hash_map {
         let _ = (x, y);
     }
@@ -41,6 +48,14 @@ fn main() {
         let _ = x;
     }
     for x in hash_map.drain() {
+        let _ = x;
+    }
+
+    // test type-aliased hashers
+    for x in fx_hash_set {
+        let _ = x;
+    }
+    for x in fx_hash_map {
         let _ = x;
     }
 
