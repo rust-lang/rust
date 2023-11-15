4138702621f32528fee8bf55eb1b8c0b99ac369b
bohan <bohan-zhang@foxmail.com>
2023-11-15 15:28:38 +0800

    discard invalid spans in external blocks

--- compiler/rustc_parse/src/errors.rs
+++ compiler/rustc_parse/src/errors.rs
@@ -1676,7 +1676,7 @@ pub(crate) struct ExternItemCannotBeConst {
     #[primary_span]
     pub ident_span: Span,
     #[suggestion(code = "static ", applicability = "machine-applicable")]
-    pub const_span: Span,
+    pub const_span: Option<Span>,
 }
 
 #[derive(Diagnostic)]
