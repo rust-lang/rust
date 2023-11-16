925111c2eb85681697155edbaf694f6693427a69
Zalathar <Zalathar@users.noreply.github.com>
2023-11-16 13:18:40 +1100

    Support multiple globs in static suggestions

--- src/tools/suggest-tests/src/static_suggestions.rs
+++ src/tools/suggest-tests/src/static_suggestions.rs
@@ -2,23 +2,23 @@ use crate::{sug, Suggestion};
 
 // FIXME: perhaps this could use `std::lazy` when it is stablizied
 macro_rules! static_suggestions {
-    ($( $glob:expr => [ $( $suggestion:expr ),*  $(,)? ] ),* $(,)? ) => {
-        pub(crate) const STATIC_SUGGESTIONS: ::once_cell::unsync::Lazy<Vec<(&'static str, Vec<Suggestion>)>>
-            = ::once_cell::unsync::Lazy::new(|| vec![ $( ($glob, vec![ $($suggestion),* ]) ),*]);
+    ($( [ $( $glob:expr ),* $(,)? ] => [ $( $suggestion:expr ),* $(,)? ] ),* $(,)? ) => {
+        pub(crate) const STATIC_SUGGESTIONS: ::once_cell::unsync::Lazy<Vec<(Vec<&'static str>, Vec<Suggestion>)>>
+            = ::once_cell::unsync::Lazy::new(|| vec![ $( (vec![ $($glob),* ], vec![ $($suggestion),* ]) ),*]);
     }
 }
 
 static_suggestions! {
-    "*.md" => [
+    ["*.md"] => [
         sug!("test", 0, ["linkchecker"]),
     ],
 
-    "compiler/*" => [
+    ["compiler/*"] => [
         sug!("check"),
         sug!("test", 1, ["tests/ui", "tests/run-make"]),
     ],
 
-    "src/librustdoc/*" => [
+    ["src/librustdoc/*"] => [
         sug!("test", 1, ["rustdoc"]),
     ],
 }
