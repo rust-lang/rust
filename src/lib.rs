4de5d372aab05f2c893ef83d08884e1ccfa72475
Nicholas Nethercote <n.nethercote@gmail.com>
2023-11-22 09:22:39 +1100

    Avoid need for `{D,Subd}iagnosticMessage` imports.
    The `fluent_messages!` macro produces uses of
    `crate::{D,Subd}iagnosticMessage`, which means that every crate using
    the macro must have this import:
    ```
    use rustc_errors::{DiagnosticMessage, SubdiagnosticMessage};
    ```
    
    This commit changes the macro to instead use
    `rustc_errors::{D,Subd}iagnosticMessage`, which avoids the need for the
    imports.

--- src/lib.rs
+++ src/lib.rs
@@ -101,7 +101,7 @@ use rustc_codegen_ssa::target_features::supported_target_features;
 use rustc_data_structures::fx::FxIndexMap;
 use rustc_data_structures::sync::IntoDynSyncSend;
 use rustc_codegen_ssa::traits::{CodegenBackend, ExtraBackendMethods, ThinBufferMethods, WriteBackendMethods};
-use rustc_errors::{DiagnosticMessage, ErrorGuaranteed, Handler, SubdiagnosticMessage};
+use rustc_errors::{ErrorGuaranteed, Handler};
 use rustc_fluent_macro::fluent_messages;
 use rustc_metadata::EncodedMetadata;
 use rustc_middle::dep_graph::{WorkProduct, WorkProductId};
