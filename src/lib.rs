8e13be084a316883b4aadabbc9ff08aaaf676aa6
Nicholas Nethercote <n.nethercote@gmail.com>
2023-11-22 09:53:07 +1100

    Use `rustc_fluent_macro::fluent_messages!` directly.
    Currently we always do this:
    ```
    use rustc_fluent_macro::fluent_messages;
    ...
    fluent_messages! { "./example.ftl" }
    ```
    But there is no need, we can just do this everywhere:
    ```
    rustc_fluent_macro::fluent_messages! { "./example.ftl" }
    ```
    which is shorter.

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,6 @@ use rustc_data_structures::fx::FxIndexMap;
 use rustc_data_structures::sync::IntoDynSyncSend;
 use rustc_codegen_ssa::traits::{CodegenBackend, ExtraBackendMethods, ThinBufferMethods, WriteBackendMethods};
 use rustc_errors::{ErrorGuaranteed, Handler};
-use rustc_fluent_macro::fluent_messages;
 use rustc_metadata::EncodedMetadata;
 use rustc_middle::dep_graph::{WorkProduct, WorkProductId};
 use rustc_middle::util::Providers;
@@ -116,7 +115,7 @@ use tempfile::TempDir;
 use crate::back::lto::ModuleBuffer;
 use crate::gcc_util::target_cpu;
 
-fluent_messages! { "../messages.ftl" }
+rustc_fluent_macro::fluent_messages! { "../messages.ftl" }
 
 pub struct PrintOnPanic<F: Fn() -> String>(pub F);
 
