84bc8f037afa470232b3171c5e547744c322be2b
Arlie Davis <ardavis@microsoft.com>
2023-11-22 10:17:50 -0800

    fix long lines

--- compiler/rustc_llvm/llvm-wrapper/SuppressLLVMWarnings.h
+++ compiler/rustc_llvm/llvm-wrapper/SuppressLLVMWarnings.h
@@ -5,9 +5,9 @@
 // to diagnose real problems when working on C++ code, so we suppress them.
 
 #ifdef _MSC_VER
-#pragma warning(disable:4530) // C++ exception handler used, but unwind semantics are not enabled. Specify /EHsc
+#pragma warning(disable:4530) // C++ exception handler used, but unwind semantics are not enabled.
 #pragma warning(disable:4624) // 'xxx': destructor was implicitly defined as deleted
-#pragma warning(disable:4244) // 'initializing': conversion from 'xxx' to 'yyy', possible loss of data
+#pragma warning(disable:4244) // conversion from 'xxx' to 'yyy', possible loss of data
 #endif
 
 #endif // _rustc_llvm_SuppressLLVMWarnings_h
