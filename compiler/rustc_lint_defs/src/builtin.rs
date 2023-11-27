535151ed03ba629393abcc6f927f38c1919ed70e
Michael Goulet <michael@errs.io>
2023-11-27 22:05:46 +0000

    Add comments

--- compiler/rustc_lint_defs/src/builtin.rs
+++ compiler/rustc_lint_defs/src/builtin.rs
@@ -1694,6 +1694,12 @@ declare_lint! {
     /// #[deny(unused_lifetimes)]
     ///
     /// pub fn foo<'a>() {}
+    ///
+    /// // `'a = 'static`, so all usages of `'a` can be replaced with `'static`
+    /// pub fn bar<'a: 'static>() {}
+    ///
+    /// // `'a = 'b`, so all usages of `'b` can be replaced with `'a`
+    /// pub fn bar<'a: 'b, 'b: 'a>() {}
     /// ```
     ///
     /// {{produces}}
