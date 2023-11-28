5161b22143bf9a726cfd69d19bef11552d0ed519
Tomasz Miąsko <tomasz.miasko@gmail.com>
2023-11-28 00:00:00 +0000

    Fix coroutine validation for mixed panic strategy
    Validation introduced in #113124 allows UnwindAction::Continue and
    TerminatorKind::Resume to occur only in functions with ABI that can
    unwind. The function ABI depends on the panic strategy, which can vary
    across crates.
    
    Usually MIR is built and validated in the same crate. The coroutine drop
    glue thus far was an exception. As a result validation could fail when
    mixing different panic strategies.
    
    Avoid the problem by executing AbortUnwindingCalls along with the
    validation.

---
+++ tests/ui/coroutine/auxiliary/unwind-aux.rs
@@ -0,0 +1,11 @@
+// compile-flags: -Cpanic=unwind  --crate-type=lib
+// no-prefer-dynamic
+// edition:2021
+
+#![feature(coroutines)]
+pub fn run<T>(a: T) {
+    let _ = move || {
+        drop(a);
+        yield;
+    };
+}
