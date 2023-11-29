ec12feaf5717e2c67011c54ee45bbc2511162bf8
Alona Enraght-Moony <code@alona.page>
2023-11-29 00:20:56 +0000

    const_eval: Allow std to stabley call const fns from deps.
    Because std's dependencies are build with `-Zforce-unstable-if-unmarked`
    normally, std is unable to call them in const-stable function. However,
    becuase they had no explicit feature gates, there was no way to use
    `rustc_allow_const_fn_unstable` to allow this.
    
    Therefor we add `#[rustc_allow_const_fn_unstable(any)]` to allow using
    these functions.
    
    The reson to do this is #102575, which relies on calling hashbrow function in
    const-stable std functions.

---
+++ tests/ui/stability-attribute/const-stable-cross-crate.rs
@@ -0,0 +1,22 @@
+// aux-build:normal-const-fn.rs
+// check-pass
+#![crate_type = "lib"]
+#![feature(staged_api)]
+#![feature(rustc_attrs)]
+#![feature(rustc_private)]
+#![allow(internal_features)]
+#![feature(rustc_allow_const_fn_unstable)]
+#![stable(feature = "stable_feature", since = "1.0.0")]
+
+extern crate normal_const_fn;
+
+// This ensures std can call const functions in it's deps that don't have
+// access to rustc_const_stable annotations (and hense don't have a feature)
+// gate.
+
+#[rustc_const_stable(feature = "stable_feature", since = "1.0.0")]
+#[stable(feature = "stable_feature", since = "1.0.0")]
+#[rustc_allow_const_fn_unstable(any)]
+pub const fn do_something() {
+    normal_const_fn::do_something_else()
+}
