cb0863475f53854114a1ebcebc02d9cd82d3dd95
Esteban Küber <esteban@kuber.com.ar>
2023-11-29 18:25:16 +0000

    fix rebase

--- tests/ui/pattern/pattern-bad-ref-box-order.rs
+++ tests/ui/pattern/pattern-bad-ref-box-order.rs
@@ -7,6 +7,8 @@ fn foo(f: Option<Box<i32>>) {
     match f {
         Some(ref box _i) => {},
         //~^ ERROR switch the order of `ref` and `box`
+        //~| ERROR expected one of `)`, `,`, or `|`, found `_i`
+        //~| ERROR this pattern has 2 fields, but the corresponding tuple variant has 1 field
         None => {}
     }
 }
