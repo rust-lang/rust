3323e4dc04e57cc64ac77dbff2f6bf50ac6832f0
Wesley Wiser <wesleywiser@microsoft.com>
2023-11-29 17:36:45 -0600

    Dispose llvm::TargetMachines prior to llvm::Context being disposed
    If the TargetMachine is disposed after the Context is disposed, it can
    lead to use after frees in some cases.
    
    I've observed this happening occasionally on code compiled for
    aarch64-pc-windows-msvc using `-Zstack-protector=strong` but other users
    have reported AVs from host aarch64-pc-windows-msvc compilers as well.

--- compiler/rustc_codegen_llvm/src/back/lto.rs
+++ compiler/rustc_codegen_llvm/src/back/lto.rs
@@ -25,6 +25,7 @@ use std::ffi::{CStr, CString};
 use std::fs::File;
 use std::io;
 use std::iter;
+use std::mem::ManuallyDrop;
 use std::path::Path;
 use std::slice;
 use std::sync::Arc;
@@ -734,7 +735,7 @@ pub unsafe fn optimize_thin_module(
     let llcx = llvm::LLVMRustContextCreate(cgcx.fewer_names);
     let llmod_raw = parse_module(llcx, module_name, thin_module.data(), &diag_handler)? as *const _;
     let mut module = ModuleCodegen {
-        module_llvm: ModuleLlvm { llmod_raw, llcx, tm },
+        module_llvm: ModuleLlvm { llmod_raw, llcx, tm: ManuallyDrop::new(tm) },
         name: thin_module.name().to_string(),
         kind: ModuleKind::Regular,
     };
