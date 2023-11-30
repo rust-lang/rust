4a1b4182feaf284d26511fc3dae8fefc7a2efd04
Jake Goulding <jake.goulding@gmail.com>
2023-11-30 08:22:31 -0500

    Rename `unused_tuple_struct_fields` in rustfmt
    Otherwise tests fail due to unknown lint and dead code warnings.

--- src/expr.rs
+++ src/expr.rs
@@ -1947,7 +1947,7 @@ fn rewrite_unary_op(
 }
 
 pub(crate) enum RhsAssignKind<'ast> {
-    Expr(&'ast ast::ExprKind, #[allow(unused_tuple_struct_fields)] Span),
+    Expr(&'ast ast::ExprKind, #[allow(dead_code)] Span),
     Bounds,
     Ty,
 }
