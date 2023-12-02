a22139f351761ebeaf46a9e1a387e5adcf5802eb
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-12-02 14:17:33 +0000

    Remove implicit #[no_mangle] for #[rustc_std_internal_symbol]

--- tests/codegen/vec-optimizes-away.rs
+++ tests/codegen/vec-optimizes-away.rs
@@ -5,7 +5,7 @@
 pub fn sum_me() -> i32 {
     // CHECK-LABEL: @sum_me
     // CHECK-NEXT: {{^.*:$}}
-    // CHECK-NEXT: {{.*}} load volatile i8, ptr @__rust_no_alloc_shim_is_unstable, align 1
+    // CHECK-NEXT: {{.*}} load volatile i8, ptr @{{.*}}__rust_no_alloc_shim_is_unstable, align 1
     // CHECK-NEXT: ret i32 6
     vec![1, 2, 3].iter().sum::<i32>()
 }
