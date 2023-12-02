30a95b7c0a97f0aa45c3b090548ba7977ba4791a
Camille GILLOT <gillot.camille@gmail.com>
2023-12-02 21:10:04 +0000

    FileCheck while_let_loops.

--- tests/mir-opt/const_prop/while_let_loops.rs
+++ tests/mir-opt/const_prop/while_let_loops.rs
@@ -1,8 +1,9 @@
-// skip-filecheck
 // unit-test: ConstProp
 // EMIT_MIR while_let_loops.change_loop_body.ConstProp.diff
 
 pub fn change_loop_body() {
+    // CHECK-LABEL: fn change_loop_body(
+    // CHECK: switchInt(const 0_isize)
     let mut _x = 0;
     while let Some(0u32) = None {
         _x = 1;
