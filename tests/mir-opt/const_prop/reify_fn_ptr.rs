343ef6a9cb8425e394643b735a9a4ef361067fe2
Camille GILLOT <gillot.camille@gmail.com>
2023-12-02 21:00:59 +0000

    FileCheck reify_fn_ptr.

--- tests/mir-opt/const_prop/reify_fn_ptr.rs
+++ tests/mir-opt/const_prop/reify_fn_ptr.rs
@@ -1,7 +1,10 @@
-// skip-filecheck
 // unit-test: ConstProp
 // EMIT_MIR reify_fn_ptr.main.ConstProp.diff
 
 fn main() {
+    // CHECK-LABEL: fn main(
+    // CHECK: [[ptr:_.*]] = main as fn() (PointerCoercion(ReifyFnPointer));
+    // CHECK: [[addr:_.*]] = move [[ptr]] as usize (PointerExposeAddress);
+    // CHECK: [[back:_.*]] = move [[addr]] as *const fn() (PointerFromExposedAddress);
     let _ = main as usize as *const fn();
 }
