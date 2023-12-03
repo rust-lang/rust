aa61b124b4eb87c41a75e5926267a0381dcd21c6
MiguelX413 <lbpgaming27@gmail.com>
2023-12-02 20:30:24 -0800

    Add docs to `core::iter::adapters::PeekMap`

--- library/core/src/iter/adapters/peek_map.rs
+++ library/core/src/iter/adapters/peek_map.rs
@@ -1,17 +1,39 @@
 use crate::iter::{FusedIterator, Peekable};
 
 /// An iterator that maps the values of `iter` with `f`.
+///
+/// This struct is created by the [`peek_map`] method on [`Peekable`]. See its
+/// documentation for more.
+///
+/// [`peek_map`]: Peekable::peek_map
+/// [`Peekable`]: struct.Peekable.html
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// #![feature(peek_map)]
+///
+/// let a = [1, 2, 3];
+/// let mut iter = a.into_iter().peekable().peek_map(|x, next| x * *next.unwrap_or(&1));
+///
+/// assert_eq!(iter.next(), Some(2));
+/// assert_eq!(iter.next(), Some(6));
+/// assert_eq!(iter.next(), Some(3));
+/// assert_eq!(iter.next(), None);
+/// ```
 #[must_use = "iterators are lazy and do nothing unless consumed"]
 #[unstable(feature = "peek_map", issue = "118474")]
 #[derive(Debug)]
-pub struct PeekMap<P, F> {
-    pub(crate) peekable: P,
+pub struct PeekMap<T, F> {
+    pub(crate) t: T,
     f: F,
 }
 
 impl<I: Iterator, F> PeekMap<Peekable<I>, F> {
     pub(in crate::iter) fn new(peekable: Peekable<I>, f: F) -> PeekMap<Peekable<I>, F> {
-        PeekMap { peekable, f }
+        PeekMap { t: peekable, f }
     }
 }
 
@@ -24,12 +46,12 @@ where
 
     #[inline]
     fn next(&mut self) -> Option<B> {
-        Some((&mut self.f)(self.peekable.next()?, self.peekable.peek()))
+        Some((&mut self.f)(self.t.next()?, self.t.peek()))
     }
 
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
-        self.peekable.size_hint()
+        self.t.size_hint()
     }
 }
 #[unstable(feature = "peek_map", issue = "118474")]
@@ -38,11 +60,11 @@ where
     F: FnMut(I::Item, Option<&I::Item>) -> B,
 {
     fn len(&self) -> usize {
-        self.peekable.len()
+        self.t.len()
     }
 
     fn is_empty(&self) -> bool {
-        self.peekable.is_empty()
+        self.t.is_empty()
     }
 }
 #[unstable(feature = "peek_map", issue = "118474")]
