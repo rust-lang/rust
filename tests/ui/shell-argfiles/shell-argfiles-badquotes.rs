fdd991f95b3effca449cb5b9f2770837bfc161d2
David Koloski <djkoloski@gmail.com>
2023-12-06 18:25:13 +0000

    Add support for shell argfiles

---
+++ tests/ui/shell-argfiles/shell-argfiles-badquotes.rs
@@ -0,0 +1,6 @@
+// Check to see if we can get parameters from an @argsfile file
+//
+// compile-flags: --cfg cmdline_set -Z shell-argfiles @shell:{{src-base}}/shell-argfiles/shell-argfiles-badquotes.args
+
+fn main() {
+}
