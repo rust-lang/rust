eb53721a34d1d910b900aa753e0a00dc72ef41ac
jyn <github@jyn.dev>
2023-12-07 22:54:41 -0500

    recurse into refs when comparing tys for diagnostics

--- tests/ui/unsafe/unsafe-trait-impl.rs
+++ tests/ui/unsafe/unsafe-trait-impl.rs
@@ -7,8 +7,8 @@ trait Foo {
 impl Foo for u32 {
     fn len(&self) -> u32 { *self }
     //~^ ERROR method `len` has an incompatible type for trait
-    //~| expected signature `unsafe fn(&u32) -> _`
-    //~| found signature `fn(&u32) -> _`
+    //~| expected signature `unsafe fn(&_) -> _`
+    //~| found signature `fn(&_) -> _`
 }
 
 fn main() { }
