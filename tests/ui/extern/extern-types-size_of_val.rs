5f5e6b94d8bdcc0780a980cd2213b8b355a4b73a
Ralf Jung <post@ralfj.de>
2023-12-11 13:38:14 +0100

    test that both size_of_val and align_of_val panic

--- tests/ui/extern/extern-types-size_of_val.rs
+++ tests/ui/extern/extern-types-size_of_val.rs
@@ -1,5 +1,6 @@
 // run-fail
 // check-run-results
+// revisions: size align
 // normalize-stderr-test "panicking\.rs:\d+:\d+:" -> "panicking.rs:"
 #![feature(extern_types)]
 
@@ -13,6 +14,9 @@ fn main() {
     let x: &A = unsafe { &*(1usize as *const A) };
 
     // These don't have a dynamic size, so this should panic.
-    assert_eq!(size_of_val(x), 0);
-    assert_eq!(align_of_val(x), 1);
+    if cfg!(size) {
+        assert_eq!(size_of_val(x), 0);
+    } else {
+        assert_eq!(align_of_val(x), 1);
+    }
 }
