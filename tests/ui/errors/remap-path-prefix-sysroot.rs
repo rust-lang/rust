fde286a7928d8092361c3fda65740054865d2c4a
Andy Wang <cbeuw.andy@gmail.com>
2023-12-11 22:45:24 +0000

    Update test

--- tests/ui/errors/remap-path-prefix-sysroot.rs
+++ tests/ui/errors/remap-path-prefix-sysroot.rs
@@ -1,12 +1,18 @@
-// run-fail
-// check-run-results
-
-// exec-env:RUST_BACKTRACE=full
 // revisions: with-remap without-remap
 // compile-flags: -g -Ztranslate-remapped-path-to-local-path=yes
 // [with-remap]compile-flags: --remap-path-prefix={{rust-src-base}}=remapped
 // [without-remap]compile-flags:
+// error-pattern: E0507
 
-fn main() {
-    Vec::<String>::with_capacity(!0);
+use std::thread;
+struct Worker {
+    thread: thread::JoinHandle<()>,
 }
+
+impl Drop for Worker {
+    fn drop(&mut self) {
+        self.thread.join().unwrap();
+    }
+}
+
+pub fn main(){}
