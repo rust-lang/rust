f3218b2a707aaf8de8ce2798865d3df4a7a0484a
Michael Goulet <michael@errs.io>
2023-12-11 21:08:05 +0000

    Make ConstKind and TyKind Copy

--- compiler/rustc_type_ir/src/ty_kind.rs
+++ compiler/rustc_type_ir/src/ty_kind.rs
@@ -111,6 +111,7 @@ pub enum AliasKind {
 #[derive(derivative::Derivative)]
 #[derivative(
     Clone(bound = ""),
+    Copy(bound = ""),
     PartialOrd(bound = ""),
     PartialOrd = "feature_allow_slow_enum",
     Ord(bound = ""),
