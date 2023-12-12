ff5934f85cfb98e16e7dea2cb3ca7f10620b2955
Andy Wang <cbeuw.andy@gmail.com>
2023-12-12 23:34:08 +0000

    Update tests

--- tests/ui/errors/remap-path-prefix-sysroot.rs
+++ tests/ui/errors/remap-path-prefix-sysroot.rs
@@ -4,6 +4,11 @@
 // [without-remap]compile-flags:
 // error-pattern: E0507
 
+// The $SRC_DIR*.rs:LL:COL normalisation doesn't kick in automatically
+// as the remapped revision will not begin with $SRC_DIR_REAL,
+// so we have to do it ourselves.
+// normalize-stderr-test: ".rs:\d+:\d+" -> ".rs:LL:COL"
+
 use std::thread;
 struct Worker {
     thread: thread::JoinHandle<()>,
