c5208518faa423b96e6d1fecaa6428a2fa4eb9d1
Jakub Beránek <berykubik@gmail.com>
2023-12-13 07:47:25 +0100

    Add `TargetSelection::is_windows` method

--- src/bootstrap/src/core/build_steps/dist.rs
+++ src/bootstrap/src/core/build_steps/dist.rs
@@ -1319,7 +1319,7 @@ impl Step for CodegenBackend {
                 return None;
             }
 
-            if self.compiler.host.contains("windows") {
+            if self.compiler.host.is_windows() {
                 builder.info(
                     "dist currently disabled for windows by rustc_codegen_cranelift. skipping",
                 );
@@ -1658,7 +1658,7 @@ impl Step for Extended {
             builder.run(&mut cmd);
         }
 
-        if target.contains("windows") {
+        if target.is_windows() {
             let exe = tmp.join("exe");
             let _ = fs::remove_dir_all(&exe);
 
