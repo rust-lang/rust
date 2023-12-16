2f1a8e2d7a641a398e9e02c8c99e80f6e44dce87
Ralf Jung <post@ralfj.de>
2023-12-16 16:24:25 +0100

    const-eval interner: from-scratch rewrite using mutability information from provenance rather than types

--- src/tools/miri/src/borrow_tracker/stacked_borrows/mod.rs
+++ src/tools/miri/src/borrow_tracker/stacked_borrows/mod.rs
@@ -830,7 +830,9 @@ trait EvalContextPrivExt<'mir: 'ecx, 'tcx: 'mir, 'ecx>: crate::MiriInterpCxExt<'
         let new_prov = this.sb_reborrow(place, size, new_perm, new_tag, info)?;
 
         // Adjust place.
-        Ok(place.clone().map_provenance(|_| new_prov))
+        // (If the closure gets called, that means the old provenance was `Some`, and hence the new
+        // one must also be `Some`.)
+        Ok(place.clone().map_provenance(|_| new_prov.unwrap()))
     }
 
     /// Retags an individual pointer, returning the retagged version.
