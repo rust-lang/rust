c00486c9bbfc9fa8f989715ea6cee1e324e2ea00
Pietro Albini <pietro@pietroalbini.org>
2023-12-21 15:39:15 +0100

    update version placeholders

--- library/alloc/src/sync.rs
+++ library/alloc/src/sync.rs
@@ -2194,7 +2194,7 @@ impl<T: Clone, A: Allocator + Clone> Arc<T, A> {
     /// assert!(ptr::eq(ptr, inner.as_ptr()));
     /// ```
     #[inline]
-    #[stable(feature = "arc_unwrap_or_clone", since = "CURRENT_RUSTC_VERSION")]
+    #[stable(feature = "arc_unwrap_or_clone", since = "1.76.0")]
     pub fn unwrap_or_clone(this: Self) -> T {
         Arc::try_unwrap(this).unwrap_or_else(|arc| (*arc).clone())
     }
