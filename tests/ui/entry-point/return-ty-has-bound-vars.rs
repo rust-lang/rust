d3f466a3a77d6d32e8e31ab9ccb61435938928a8
r0cky <mu001999@outlook.com>
2023-12-23 00:09:37 +0800

    Update test

--- tests/ui/entry-point/return-ty-has-bound-vars.rs
+++ tests/ui/entry-point/return-ty-has-bound-vars.rs
@@ -1,5 +1,3 @@
 // issue-119209
 
-type Foo<'a> = impl PartialEq; //~ERROR `impl Trait` in type aliases is unstable
-
-fn main<'a>(_: &'a i32) -> Foo<'a> {} //~ERROR `main` function return type is not allowed to have generic parameters
+fn main<'a>(_: &'a i32) -> &'a () { &() } //~ERROR `main` function return type is not allowed to have generic parameters
