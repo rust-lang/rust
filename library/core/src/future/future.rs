cd9425b8d1fe7c0c84e9743efc0223640ff4fe08
Georg Semmler <github@weiznich.de>
2023-12-22 14:30:12 +0100

    Replace some usage of `#[rustc_on_unimplemented]` with `#[diagnostic::on_unimplemented]`
    This commit replaces those `#[rustc_on_unimplemented]` attributes with
    their equivalent `#[diagnostic::on_unimplemented]` where this is
    supported (So no filter or any extended option)

--- library/core/src/future/future.rs
+++ library/core/src/future/future.rs
@@ -28,7 +28,7 @@ use crate::task::{Context, Poll};
 #[must_use = "futures do nothing unless you `.await` or poll them"]
 #[stable(feature = "futures_api", since = "1.36.0")]
 #[lang = "future_trait"]
-#[rustc_on_unimplemented(
+#[diagnostic::on_unimplemented(
     label = "`{Self}` is not a future",
     message = "`{Self}` is not a future",
     note = "{Self} must be a future or must implement `IntoFuture` to be awaited"
