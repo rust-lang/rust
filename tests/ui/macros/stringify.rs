ff6c7c5f6d920602319f65bfc95e8471bbcc4d8a
David Tolnay <dtolnay@gmail.com>
2023-12-27 17:30:54 -0800

    Fix pretty printer statement boundaries after braced macro call

--- tests/ui/macros/stringify.rs
+++ tests/ui/macros/stringify.rs
@@ -226,7 +226,7 @@ fn test_expr() {
     );
     c2_match_arm!(
         [ m! {} - 1 ],
-        "match () { _ => m! {} - 1, }",
+        "match () { _ => (m! {}) - 1, }", // parenthesis is redundant
         "match() { _ => m! {} - 1 }",
     );
 
@@ -748,7 +748,7 @@ fn test_stmt() {
     );
     c2_minus_one!(
         [ m! {} ],
-        "m! {} - 1;", // FIXME(dtolnay): needs parens, otherwise this is 2 separate statements
+        "(m! {}) - 1;",
         "m! {} - 1"
     );
 
