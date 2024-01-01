69cc9831553cf40c0ee63bfc614827c438c438bf
roife <roifewu@gmail.com>
2024-01-01 16:17:46 +0800

    Add tests for autofix in unnecessary_fallible_conversions

--- tests/ui/unnecessary_fallible_conversions.rs
+++ tests/ui/unnecessary_fallible_conversions.rs
@@ -3,4 +3,18 @@
 fn main() {
     let _: i64 = 0i32.try_into().unwrap();
     let _: i64 = 0i32.try_into().expect("can't happen");
+
+    let _ = i64::try_from(0i32).unwrap();
+    let _ = i64::try_from(0i32).expect("can't happen");
+
+    let _: i64 = i32::try_into(0).unwrap();
+    let _: i64 = i32::try_into(0i32).expect("can't happen");
+
+    let _ = <i64 as TryFrom<i32>>::try_from(0)
+        .unwrap();
+    let _ = <i64 as TryFrom<i32>>::try_from(0).
+        expect("can't happen");
+
+    let _: i64 = <i32 as TryInto<_>>::try_into(0).unwrap();
+    let _: i64 = <i32 as TryInto<_>>::try_into(0).expect("can't happen");
 }
