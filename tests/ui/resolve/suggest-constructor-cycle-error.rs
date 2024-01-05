339fa311ad8140cfb98a1c6080ea7e002b9ce3fa
Lukas Markeffsky <@>
2024-01-05 21:55:09 +0100

    fix cycle error for "use constructor" suggestion

--- tests/ui/resolve/suggest-constructor-cycle-error.rs
+++ tests/ui/resolve/suggest-constructor-cycle-error.rs
@@ -1,10 +1,10 @@
 // aux-build:suggest-constructor-cycle-error.rs
-//~^ cycle detected when getting the resolver for lowering [E0391]
 
 // Regression test for https://github.com/rust-lang/rust/issues/119625
 
 extern crate suggest_constructor_cycle_error as a;
 
 const CONST_NAME: a::Uuid = a::Uuid(());
+//~^ ERROR: cannot initialize a tuple struct which contains private fields [E0423]
 
 fn main() {}
