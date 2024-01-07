e44b11f6952659e7cb4e66ee3b5ff47bb7bc6c31
Michael Goulet <michael@errs.io>
2024-01-07 22:53:50 +0000

    ~const trait or projection bounds do not imply non-const bounds

--- tests/ui/rfcs/rfc-2632-const-trait-impl/effects/project.rs
+++ tests/ui/rfcs/rfc-2632-const-trait-impl/effects/project.rs
@@ -1,7 +1,12 @@
-// check-pass
+// known-bug: #110395
+// FIXME: effects
+
 #![feature(const_trait_impl, effects)]
 
-pub trait Owo<X = <Self as Uwu>::T> {}
+// This fails because `~const Uwu` doesn't imply (non-const) `Uwu`.
+
+// FIXME: #[const_trait]
+pub trait Owo<X = <Self as /* FIXME: ~const */ Uwu>::T> {}
 
 #[const_trait]
 pub trait Uwu: Owo {
