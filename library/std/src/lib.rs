c949dbdab1be3fab38acb166c4b0028190e6f46d
Amanieu d'Antras <amanieu@gmail.com>
2024-01-08 13:36:05 +0000

    Add stdarch_wasm_atomic_wait feature in std

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -262,6 +262,7 @@
 )]
 #![cfg_attr(any(windows, target_os = "uefi"), feature(round_char_boundary))]
 #![cfg_attr(target_os = "xous", feature(slice_ptr_len))]
+#![cfg_attr(target_family = "wasm", feature(stdarch_wasm_atomic_wait))]
 //
 // Language features:
 // tidy-alphabetical-start
