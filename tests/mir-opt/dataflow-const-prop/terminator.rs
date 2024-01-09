1adda9a170085823f43672e60c19be29a4deb18f
sfzhu93 <zhushuofei@gmail.com>
2024-01-08 20:21:23 -0800

    Add FileCheck to terminator.rs and tuple.rs

--- tests/mir-opt/dataflow-const-prop/terminator.rs
+++ tests/mir-opt/dataflow-const-prop/terminator.rs
@@ -1,12 +1,14 @@
-// skip-filecheck
 // EMIT_MIR_FOR_EACH_PANIC_STRATEGY
 // unit-test: DataflowConstProp
 
 fn foo(n: i32) {}
 
 // EMIT_MIR terminator.main.DataflowConstProp.diff
+
+// CHECK-LABEL: fn main
 fn main() {
     let a = 1;
     // Checks that we propagate into terminators.
+    // CHECK: {{_[0-9]+}} = foo(const 2_i32) -> [return: {{bb[0-9]+}}, unwind continue];
     foo(a + 1);
 }
