24aefa0e5d134526c8e29c5603dcb4d42637968a
sfzhu93 <zhushuofei@gmail.com>
2024-01-08 16:58:58 -0800

    Add FileCheck for if.rs, inherit_overflow.rs, issue_81605.rs

--- tests/mir-opt/dataflow-const-prop/issue_81605.rs
+++ tests/mir-opt/dataflow-const-prop/issue_81605.rs
@@ -1,9 +1,12 @@
-// skip-filecheck
 // unit-test: DataflowConstProp
 
 // EMIT_MIR issue_81605.f.DataflowConstProp.diff
+
+// CHECK-LABEL: fn f
 fn f() -> usize {
+    // CHECK: switchInt(const true) -> [0: {{bb[0-9]+}}, otherwise: {{bb[0-9]+}}];
     1 + if true { 1 } else { 2 }
+    // CHECK: _0 = const 2_usize;
 }
 
 fn main() {
