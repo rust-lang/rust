41b69aae912aa9a1a8d5ed9518e73d136b42e7ca
Urgau <urgau@numericable.fr>
2024-01-13 12:18:51 +0100

    Add way to express no-values with check-cfg

--- tests/ui/check-cfg/concat-values.rs
+++ tests/ui/check-cfg/concat-values.rs
@@ -1,6 +1,7 @@
 // check-pass
 // compile-flags: -Z unstable-options
 // compile-flags: --check-cfg=cfg(my_cfg,values("foo")) --check-cfg=cfg(my_cfg,values("bar"))
+// compile-flags: --check-cfg=cfg(my_cfg,values())
 
 #[cfg(my_cfg)]
 //~^ WARNING unexpected `cfg` condition value
@@ -9,5 +10,8 @@ fn my_cfg() {}
 #[cfg(my_cfg = "unk")]
 //~^ WARNING unexpected `cfg` condition value
 fn my_cfg() {}
+
+#[cfg(any(my_cfg = "foo", my_cfg = "bar"))]
+fn foo_and_bar() {}
 
 fn main() {}
