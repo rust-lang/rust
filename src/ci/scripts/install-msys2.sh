be435b5ebf6a4f1433f51ed9a5dc3f272e3877c4
Matt Harding <majaharding@gmail.com>
2024-01-15 22:58:09 +0000

    .

--- src/ci/scripts/install-msys2.sh
+++ src/ci/scripts/install-msys2.sh
@@ -71,5 +71,5 @@ if isWindows; then
     rm -r "/c/mingw64/"
     rm -r "/c/mingw32/"
 
-    echo "\nexport PATH=\"/c/hostedtoolcache/windows/Python/3.7.9/x64/:\$PATH\"\n" >> ~/.profile
+    echo "\nexport PATH=\"/c/hostedtoolcache/windows/Python/3.7.9/x64/:\$PATH\"\n" >> ~/.bashrc
 fi
