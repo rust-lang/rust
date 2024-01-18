ad392ff9ccd725131e180553610b6fc9f3bab25b
Michael Goulet <michael@errs.io>
2024-01-18 17:28:37 +0000

    Make sure refinement still works

--- tests/ui/async-await/in-trait/async-example-desugared-boxed.rs
+++ tests/ui/async-await/in-trait/async-example-desugared-boxed.rs
@@ -4,12 +4,15 @@
 use std::future::Future;
 use std::pin::Pin;
 
-trait MyTrait {
+#[allow(async_fn_in_trait)]
+pub trait MyTrait {
     async fn foo(&self) -> i32;
 }
 
 impl MyTrait for i32 {
+    #[warn(refining_impl_trait)]
     fn foo(&self) -> Pin<Box<dyn Future<Output = i32> + '_>> {
+        //~^ WARN impl trait in impl method signature does not match trait method signature
         Box::pin(async { *self })
     }
 }
--- tests/ui/async-await/in-trait/async-example-desugared-manual.rs
+++ tests/ui/async-await/in-trait/async-example-desugared-manual.rs
@@ -4,11 +4,12 @@
 use std::future::Future;
 use std::task::Poll;
 
-trait MyTrait {
+#[allow(async_fn_in_trait)]
+pub trait MyTrait {
     async fn foo(&self) -> i32;
 }
 
-struct MyFuture;
+pub struct MyFuture;
 impl Future for MyFuture {
     type Output = i32;
     fn poll(self: std::pin::Pin<&mut Self>, _: &mut std::task::Context<'_>) -> Poll<Self::Output> {
@@ -17,7 +18,9 @@ impl Future for MyFuture {
 }
 
 impl MyTrait for u32 {
+    #[warn(refining_impl_trait)]
     fn foo(&self) -> MyFuture {
+        //~^ WARN impl trait in impl method signature does not match trait method signature
         MyFuture
     }
 }
