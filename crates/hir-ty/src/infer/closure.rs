2a500d5280fa9cbf334dc766bf16f01819015561
Johann Hemmann <johann.hemmann@code.berlin>
2024-01-19 13:59:29 +0100

    for_kv_map

--- crates/hir-ty/src/infer/closure.rs
+++ crates/hir-ty/src/infer/closure.rs
@@ -1008,7 +1008,7 @@ impl InferenceContext<'_> {
         let mut deferred_closures = mem::take(&mut self.deferred_closures);
         let mut dependents_count: FxHashMap<ClosureId, usize> =
             deferred_closures.keys().map(|it| (*it, 0)).collect();
-        for (_, deps) in &self.closure_dependencies {
+        for deps in self.closure_dependencies.values() {
             for dep in deps {
                 *dependents_count.entry(*dep).or_default() += 1;
             }
