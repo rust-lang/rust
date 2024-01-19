7ca59285a656bc030450adde64e14b0963651586
Matt Harding <majaharding@gmail.com>
2024-01-19 22:36:06 +0000

    .

--- src/ci/scripts/install-mingw.sh
+++ src/ci/scripts/install-mingw.sh
@@ -39,6 +39,7 @@ if isWindows; then
     esac
 
     if [[ "${CUSTOM_MINGW-0}" -eq 0 ]]; then
+        pacman -S --noconfirm pactoys
         pacboy -S --noconfirm toolchain:p
         # Maybe even:
         # pacboy -S --noconfirm clang:p ...
