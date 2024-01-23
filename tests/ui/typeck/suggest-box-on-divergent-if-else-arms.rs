34f4f3da4f3e2ac8fa086c4d3a3c89d49d23a263
Esteban Küber <esteban@kuber.com.ar>
2024-01-23 04:42:26 +0000

    Suggest boxing both arms of if expr if that solves divergent arms involving `impl Trait`
    When encountering the following
    
    ```rust
    // run-rustfix
    trait Trait {}
    struct Struct;
    impl Trait for Struct {}
    fn foo() -> Box<dyn Trait> {
        Box::new(Struct)
    }
    fn bar() -> impl Trait {
        Struct
    }
    fn main() {
        let _ = if true {
            Struct
        } else {
            foo() //~ ERROR E0308
        };
        let _ = if true {
            foo()
        } else {
            Struct //~ ERROR E0308
        };
        let _ = if true {
            Struct
        } else {
            bar() // impl Trait
        };
        let _ = if true {
            bar() // impl Trait
        } else {
            Struct
        };
    }
    ```
    
    suggest boxing both arms
    
    ```rust
        let _ = if true {
            Box::new(Struct) as Box<dyn Trait>
        } else {
            Box::new(bar())
        };
        let _ = if true {
            Box::new(bar()) as Box<dyn Trait>
        } else {
            Box::new(Struct)
        };
    ```

--- tests/ui/typeck/suggest-box-on-divergent-if-else-arms.rs
+++ tests/ui/typeck/suggest-box-on-divergent-if-else-arms.rs
@@ -5,6 +5,9 @@ impl Trait for Struct {}
 fn foo() -> Box<dyn Trait> {
     Box::new(Struct)
 }
+fn bar() -> impl Trait {
+    Struct
+}
 fn main() {
     let _ = if true {
         Struct
@@ -13,6 +16,16 @@ fn main() {
     };
     let _ = if true {
         foo()
+    } else {
+        Struct //~ ERROR E0308
+    };
+    let _ = if true {
+        Struct
+    } else {
+        bar() //~ ERROR E0308
+    };
+    let _ = if true {
+        bar()
     } else {
         Struct //~ ERROR E0308
     };
