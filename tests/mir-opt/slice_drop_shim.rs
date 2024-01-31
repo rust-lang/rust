2116ed723d178313cac1ff829d5a7417bba1cd3b
Ben Kimock <kimockb@gmail.com>
2024-01-30 22:28:28 -0500

    Tweak a few mir-opt tests instead of using -Clink-dead-code

--- tests/mir-opt/slice_drop_shim.rs
+++ tests/mir-opt/slice_drop_shim.rs
@@ -1,6 +1,8 @@
 // skip-filecheck
-// compile-flags: -Zmir-opt-level=0
-
+// compile-flags: -Zmir-opt-level=0 -Clink-dead-code
+// mir-opt tests are always built as rlibs so that they seamlessly cross-compile,
+// so this test only produces MIR for the drop_in_place we're looking for
+// if we use -Clink-dead-code.
 
 // EMIT_MIR core.ptr-drop_in_place.[String].AddMovesForPackedDrops.before.mir
 fn main() {
