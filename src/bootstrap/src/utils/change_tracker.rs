ad0504b8344e239693e9520552e4cdd2fea30e35
Matthew Maurer <mmaurer@google.com>
2024-02-02 23:13:05 +0000

    Update Android testing to API 21, matching NDK 26
    We were running testing on API 18, which was already out of support for
    NDK 25, and some of the ancient behavior in that image was causing
    trouble when developing `rustc` features (#120326).
    
    Update to the current LTS NDK 26, and to its minimum supported API 21.
    
    Fixes: #120567

--- src/bootstrap/src/utils/change_tracker.rs
+++ src/bootstrap/src/utils/change_tracker.rs
@@ -190,4 +190,9 @@ pub const CONFIG_CHANGE_HISTORY: &[ChangeInfo] = &[
         severity: ChangeSeverity::Warning,
         summary: "`rust.lld` has a new default value of `true` on `x86_64-unknown-linux-gnu`. Starting at stage1, `rust-lld` will thus be this target's default linker. No config changes should be necessary.",
     },
+    ChangeInfo {
+        change_id: 120593,
+        severity: ChangeSeverity::Info,
+        summary: "Removed android-ndk r25b support in favor of android-ndk r26d.",
+    },
 ];
