60e497ba3bdc99af293197f6754db497d971c7cf
Jiri Bobek <jiri.bobek@gmail.com>
2024-02-08 16:55:49 +0100

    fix bug in 'simplify_branches.rs'

--- compiler/rustc_mir_transform/src/simplify_branches.rs
+++ compiler/rustc_mir_transform/src/simplify_branches.rs
@@ -19,23 +19,9 @@ impl<'tcx> MirPass<'tcx> for SimplifyConstCondition {
         let param_env = tcx.param_env_reveal_all_normalized(body.source.def_id());
         'blocks: for block in body.basic_blocks_mut() {
             for stmt in block.statements.iter_mut() {
-                if let StatementKind::Intrinsic(box ref intrinsic) = stmt.kind
-                    && let NonDivergingIntrinsic::Assume(discr) = intrinsic
-                    && let Operand::Constant(ref c) = discr
-                    && let Some(constant) = c.const_.try_eval_bool(tcx, param_env)
-                {
-                    if constant {
-                        stmt.make_nop();
-                    } else {
-                        block.statements.clear();
-                        block.terminator_mut().kind = TerminatorKind::Unreachable;
-                        continue 'blocks;
-                    }
-                }
                 if let StatementKind::Intrinsic(box ref intrinsic) = stmt.kind {
                     match intrinsic {
-                        NonDivergingIntrinsic::Assume(discr)
-                        | NonDivergingIntrinsic::Expect(discr, ..) => {
+                        NonDivergingIntrinsic::Assume(discr) => {
                             if let Operand::Constant(ref c) = discr
                                 && let Some(constant) = c.const_.try_eval_bool(tcx, param_env)
                             {
@@ -46,6 +32,14 @@ impl<'tcx> MirPass<'tcx> for SimplifyConstCondition {
                                     block.terminator_mut().kind = TerminatorKind::Unreachable;
                                     continue 'blocks;
                                 }
+                            }
+                        }
+                        NonDivergingIntrinsic::Expect(discr, ..) => {
+                            if let Operand::Constant(ref c) = discr
+                                && let Some(constant) = c.const_.try_eval_bool(tcx, param_env)
+                                && constant
+                            {
+                                stmt.make_nop();
                             }
                         }
                         _ => {}
