aea60b0cc78cf7b0d189c52f79563f1e4e82dfb8
Martin Nordholts <martin.nordholts@codetale.se>
2024-02-09 08:05:32 +0100

    unix_sigpipe: Replace `inherit` with `sig_dfl` in syntax tests
    The `sig_dfl` variant of the attribute is the most likely variant to be
    stabilized first, and thus to become the "most allowed" variant of the
    attribute. Because of this, it is the most appropriate variant to use in
    syntax tests, because even if the most allowed variant is used, the
    compiler shall still emit errors if it e.g. is used in the wrong places.

--- tests/ui/attributes/unix_sigpipe/unix_sigpipe-non-root-main.rs
+++ tests/ui/attributes/unix_sigpipe/unix_sigpipe-non-root-main.rs
@@ -1,7 +1,7 @@
 #![feature(unix_sigpipe)]
 
 mod m {
-    #[unix_sigpipe = "inherit"] //~ error: `unix_sigpipe` attribute can only be used on root `fn main()`
+    #[unix_sigpipe = "sig_dfl"] //~ error: `unix_sigpipe` attribute can only be used on root `fn main()`
     fn main() {}
 }
 
