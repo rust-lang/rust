0e89228bd7c3a8bde4ddd3b3f480f55286c4382d
Eric Huss <eric@huss.org>
2024-02-08 18:48:51 -0800

    [Beta 1.77] Fix bootstrapping from 1.76

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -309,6 +309,7 @@
 //
 // Library features (core):
 // tidy-alphabetical-start
+#![cfg_attr(bootstrap, feature(c_str_literals))]
 #![feature(char_internals)]
 #![feature(core_intrinsics)]
 #![feature(core_io_borrowed_buf)]
