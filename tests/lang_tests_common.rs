79241b8a4e511a4e59761477c0bdffea24bd7201
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-02-09 17:00:37 +0100

    Update tests to use `config.toml` instead

--- tests/lang_tests_common.rs
+++ tests/lang_tests_common.rs
@@ -7,6 +7,7 @@ use std::{
 
 use lang_tester::LangTester;
 use tempfile::TempDir;
+use boml::Toml;
 
 /// Controls the compile options (e.g., optimization level) used to compile
 /// test code.
@@ -20,8 +21,16 @@ pub fn main_inner(profile: Profile) {
     let tempdir = TempDir::new().expect("temp dir");
     let current_dir = current_dir().expect("current dir");
     let current_dir = current_dir.to_str().expect("current dir").to_string();
-    let gcc_path = include_str!("../gcc_path");
-    let gcc_path = gcc_path.trim();
+    let gcc_path = Toml::parse(include_str!("../config.toml"))
+        .expect("Failed to parse `config.toml`")
+        .get_string("gcc-path")
+        .expect("Missing `gcc-path` key in `config.toml`")
+        .to_string();
+    let gcc_path = Path::new(&gcc_path)
+        .canonicalize()
+        .expect("failed to get absolute path of `gcc-path`")
+        .display()
+        .to_string();
     env::set_var("LD_LIBRARY_PATH", gcc_path);
 
     fn rust_filter(filename: &Path) -> bool {
