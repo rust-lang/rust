1e4171bc6e4c47de88eb98f44837a733ef53ce57
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2024-02-09 22:42:16 +0900

    clippy: Enable `non_canonical_partial_ord_impl` rule

--- lib/la-arena/src/lib.rs
+++ lib/la-arena/src/lib.rs
@@ -70,7 +70,7 @@ impl<T> Ord for Idx<T> {
 
 impl<T> PartialOrd for Idx<T> {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        self.raw.partial_cmp(&other.raw)
+        Some(self.cmp(other))
     }
 }
 
