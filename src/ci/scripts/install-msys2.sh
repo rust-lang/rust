52e9dc32b9fdb3ad56430ba0f8ebef418d7a7754
Matt Harding <majaharding@gmail.com>
2024-02-13 07:33:43 +0000

    .

--- src/ci/scripts/install-msys2.sh
+++ src/ci/scripts/install-msys2.sh
@@ -12,18 +12,18 @@ if isWindows; then
     # subdirectory for each installed Python version.
     #
     # The -V flag of the sort command sorts the input by version number.
-    native_python_version="$(ls /c/hostedtoolcache/windows/Python | sort -Vr | head -n 1)"
+    # native_python_version="$(ls /c/hostedtoolcache/windows/Python | sort -Vr | head -n 1)"
 
-    # Make sure we use the native python interpreter instead of some msys equivalent
-    # one way or another. The msys interpreters seem to have weird path conversions
-    # baked in which break LLVM's build system one way or another, so let's use the
-    # native version which keeps everything as native as possible.
-    python_home="/c/hostedtoolcache/windows/Python/${native_python_version}/x64"
-    if ! [[ -f "${python_home}/python3.exe" ]]; then
-        cp "${python_home}/python.exe" "${python_home}/python3.exe"
-    fi
-    ciCommandAddPath "C:\\hostedtoolcache\\windows\\Python\\${native_python_version}\\x64"
-    ciCommandAddPath "C:\\hostedtoolcache\\windows\\Python\\${native_python_version}\\x64\\Scripts"
+    # # Make sure we use the native python interpreter instead of some msys equivalent
+    # # one way or another. The msys interpreters seem to have weird path conversions
+    # # baked in which break LLVM's build system one way or another, so let's use the
+    # # native version which keeps everything as native as possible.
+    # python_home="/c/hostedtoolcache/windows/Python/${native_python_version}/x64"
+    # if ! [[ -f "${python_home}/python3.exe" ]]; then
+    #     cp "${python_home}/python.exe" "${python_home}/python3.exe"
+    # fi
+    # ciCommandAddPath "C:\\hostedtoolcache\\windows\\Python\\${native_python_version}\\x64"
+    # ciCommandAddPath "C:\\hostedtoolcache\\windows\\Python\\${native_python_version}\\x64\\Scripts"
 
     # Install pacboy for easily installing packages
     pacman -S --noconfirm pactoys
