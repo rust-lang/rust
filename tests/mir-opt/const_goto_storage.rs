20e8d540f925c0ed3b08bfd71023288be47864e7
Erick Tryzelaar <etryzelaar@google.com>
2024-02-15 01:20:19 +0000

    Reverts rust-lang/rust#117206
    cc @cjgillot @tmiasko
    
    This revert is based on the following report of a regression caused by this PR:
    
    https://github.com/rust-lang/rust/issues/121094
    
    In accordance with the compiler team [revert policy], PRs that cause meaningful
    regressions should be reverted and re-landed once the regression has been fixed
    (and a regression test has been added, where appropriate).
    [revert policy]: https://forge.rust-lang.org/compiler/reviews.html#reverts
    
    Fear not! Regressions happen. Please rest assured that this does not
    represent a negative judgment of your contribution or ability to contribute
    positively to Rust in the future. We simply want to prioritize keeping existing
    use cases working, and keep the compiler more stable for everyone.
    
    r? compiler

---
+++ tests/mir-opt/const_goto_storage.rs
@@ -0,0 +1,22 @@
+// skip-filecheck
+// unit-test: ConstGoto
+
+// EMIT_MIR const_goto_storage.match_nested_if.ConstGoto.diff
+fn match_nested_if() -> bool {
+    let val = match () {
+        () if if if if true { true } else { false } { true } else { false } {
+            true
+        } else {
+            false
+        } =>
+            {
+                true
+            }
+        _ => false,
+    };
+    val
+}
+
+fn main() {
+    let _ = match_nested_if();
+}
