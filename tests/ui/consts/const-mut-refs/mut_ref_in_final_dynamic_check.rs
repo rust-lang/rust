d3fc69ae450530dffa0f307ef2cb669c7b0a1c3b
Ralf Jung <post@ralfj.de>
2024-02-16 11:35:20 +0100

    add test ensuring we detect a static actually having a mutable reference

--- tests/ui/consts/const-mut-refs/mut_ref_in_final_dynamic_check.rs
+++ tests/ui/consts/const-mut-refs/mut_ref_in_final_dynamic_check.rs
@@ -1,7 +1,10 @@
 // normalize-stderr-test "(the raw bytes of the constant) \(size: [0-9]*, align: [0-9]*\)" -> "$1 (size: $$SIZE, align: $$ALIGN)"
-// normalize-stderr-test "([0-9a-f][0-9a-f] |╾─*ALLOC[0-9]+(\+[a-z0-9]+)?(<imm>)?─*╼ )+ *│.*" -> "HEX_DUMP"
+// normalize-stderr-test "( 0x[0-9a-f][0-9a-f] │)? ([0-9a-f][0-9a-f] |__ |╾─*ALLOC[0-9]+(\+[a-z0-9]+)?(<imm>)?─*╼ )+ *│.*" -> " HEX_DUMP"
+// normalize-stderr-test "HEX_DUMP\s*\n\s*HEX_DUMP" -> "HEX_DUMP"
 #![feature(const_mut_refs, const_refs_to_static)]
 #![feature(raw_ref_op)]
+
+use std::sync::Mutex;
 
 // This file checks that our dynamic checks catch things that the static checks miss.
 // We do not have static checks for these, because we do not look into function bodies.
@@ -34,5 +37,12 @@ const fn helper_dangling() -> Option<&'static mut i32> { unsafe {
 } }
 const DANGLING: Option<&mut i32> = helper_dangling(); //~ ERROR encountered dangling pointer
 static DANGLING_STATIC: Option<&mut i32> = helper_dangling(); //~ ERROR encountered dangling pointer
+
+// Variant of the real-world case in <https://github.com/rust-lang/rust/issues/120450>.
+// Maybe we should allow this in the future (then the rest should move to `const_mut_refs.rs`),
+// but for now we reject it.
+static mut MUT_ARRAY: &mut [u8] = &mut [42];
+static MUTEX: Mutex<&mut [u8]> = Mutex::new(unsafe { &mut *MUT_ARRAY }); //~ ERROR it is undefined behavior to use this value
+//~^ encountered mutable reference
 
 fn main() {}
