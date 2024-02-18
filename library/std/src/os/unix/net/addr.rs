731b456ab2587015a28792ee1a44ea87334626b5
John Nunley <dev@notgull.net>
2024-02-18 12:53:48 -0800

    chore: Fix tidy
    Signed-off-by: John Nunley <dev@notgull.net>

--- library/std/src/os/unix/net/addr.rs
+++ library/std/src/os/unix/net/addr.rs
@@ -112,11 +112,12 @@ impl SocketAddr {
             // linux returns zero bytes of address
             len = sun_path_offset(&addr) as libc::socklen_t; // i.e., zero-length address
         } else if cfg!(target_os = "openbsd") {
-            // OpenBSD implements getsockname in a way where the 
+            // OpenBSD implements getsockname in a way where the
             // socket name's length is more than what the buffer
             // actually contains. Figure out the length for ourselves.
             // https://marc.info/?l=openbsd-bugs&m=170105481926736&w=2
-            let sun_path: &[u8] = unsafe { crate::mem::transmute::<&[libc::c_char], &[u8]>(&addr.sun_path) };
+            let sun_path: &[u8] =
+                unsafe { crate::mem::transmute::<&[libc::c_char], &[u8]>(&addr.sun_path) };
             len = crate::sys::memchr::memchr(0, sun_path)
                 .map_or(len, |new_len| (new_len + sun_path_offset(&addr)) as libc::socklen_t);
         }
