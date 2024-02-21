5fe4e226c99d20ed20c77f9eedca987fd7e942f2
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2024-02-21 00:48:37 +0000

    Fix tests/assembly/stack-protector/stack-protector-target-support.rs
    Previously the test seems to not actually test redox.

--- tests/assembly/stack-protector/stack-protector-target-support.rs
+++ tests/assembly/stack-protector/stack-protector-target-support.rs
@@ -4,7 +4,7 @@
 // revisions: r1 r2 r3 r4 r5 r6 r7 r8 r9 r10 r11 r12 r13 r14 r15 r16 r17 r18 r19 r20 r21 r22 r23
 // revisions: r24 r25 r26 r27 r28 r29 r30 r31 r32 r33     r35 r36 r37 r38 r39 r40 r41 r42 r43 r44
 // revisions: r45 r46 r47 r48 r49 r50 r51 r52 r53 r54 r55 r56 r57 r58 r59 r60 r61 r62 r63 r64 r65
-// revisions: r66 r67 r68 r69 r70 r71 r72 r73 r74 r75 r76 r77 r78 r79 r80 r81 r82 r83 r84
+// revisions: r66 r67 r68 r69 r70 r71 r72 r73 r74 r75 r76 r77 r78 r79 r80 r81 r82 r83 r84 r85
 // assembly-output: emit-asm
 // [r1] compile-flags: --target aarch64-unknown-linux-gnu
 // [r1] needs-llvm-components: aarch64
@@ -283,4 +283,5 @@ pub fn foo() {
     // r82: __stack_chk_fail
     // r83: __stack_chk_fail
     // r84: __stack_chk_fail
+    // r85: __stack_chk_fail
 }
