6f4354f6adc37ed889491cbfe6ed3e730e55fa91
dfireBird <me@dfirebird.dev>
2024-02-24 00:49:52 +0530

    add assist for filling fields by replacing ellipsis in record syntax

---
+++ crates/ide-assists/src/handlers/fill_record_pattern_fields.rs
@@ -0,0 +1,236 @@
+use syntax::{
+    ast::{self, make},
+    AstNode,
+};
+
+use crate::{AssistContext, AssistId, Assists};
+
+// Assist: fill_record_pattern_fields
+//
+// Fills fields by replacing rest pattern in record patterns.
+//
+// ```
+// struct Bar { y: Y, z: Z }
+//
+// fn foo(bar: Bar) {
+//     let Bar { ..$0 } = bar;
+// }
+// ```
+// ->
+// ```
+// struct Bar { y: Y, z: Z }
+//
+// fn foo(bar: Bar) {
+//     let Bar { y, z  } = bar;
+// }
+// ```
+pub(crate) fn fill_record_pattern_fields(acc: &mut Assists, ctx: &AssistContext<'_>) -> Option<()> {
+    let record_pat = ctx.find_node_at_offset::<ast::RecordPat>()?;
+
+    let ellipsis = record_pat.record_pat_field_list().and_then(|r| r.rest_pat())?;
+    if !ellipsis.syntax().text_range().contains_inclusive(ctx.offset()) {
+        return None;
+    }
+
+    let target_range = ellipsis.syntax().text_range();
+
+    let missing_fields = ctx.sema.record_pattern_missing_fields(&record_pat);
+
+    let old_field_list = record_pat.record_pat_field_list()?;
+    let new_field_list = make::record_pat_field_list(old_field_list.fields()).clone_for_update();
+    for (f, _) in missing_fields.iter() {
+        let field =
+            make::record_pat_field_shorthand(make::name_ref(&f.name(ctx.sema.db).to_smol_str()));
+        new_field_list.add_field(field.clone_for_update());
+    }
+
+    let old_range = ctx.sema.original_range_opt(old_field_list.syntax())?;
+    if old_range.file_id != ctx.file_id() {
+        return None;
+    }
+
+    acc.add(
+        AssistId("fill_record_pattern_fields", crate::AssistKind::RefactorRewrite),
+        "Fill structure fields",
+        target_range,
+        move |builder| builder.replace_ast(old_field_list, new_field_list),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::{check_assist, check_assist_not_applicable};
+
+    #[test]
+    fn fill_fields_enum_with_only_ellipsis() {
+        check_assist(
+            fill_record_pattern_fields,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B{ ..$0 } => true,
+    };
+}
+"#,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B{ y, z  } => true,
+    };
+}
+"#,
+        )
+    }
+
+    #[test]
+    fn fill_fields_enum_with_fields() {
+        check_assist(
+            fill_record_pattern_fields,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B{ y, ..$0 } => true,
+    };
+}
+"#,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B{ y, z } => true,
+    };
+}
+"#,
+        )
+    }
+
+    #[test]
+    fn fill_fields_struct_with_only_ellipsis() {
+        check_assist(
+            fill_record_pattern_fields,
+            r#"
+struct Bar {
+    y: Y,
+    z: Z,
+}
+
+fn foo(bar: Bar) {
+    let Bar { ..$0 } = bar;
+}
+"#,
+            r#"
+struct Bar {
+    y: Y,
+    z: Z,
+}
+
+fn foo(bar: Bar) {
+    let Bar { y, z  } = bar;
+}
+"#,
+        )
+    }
+
+    #[test]
+    fn fill_fields_struct_with_fields() {
+        check_assist(
+            fill_record_pattern_fields,
+            r#"
+struct Bar {
+    y: Y,
+    z: Z,
+}
+
+fn foo(bar: Bar) {
+    let Bar { y, ..$0 } = bar;
+}
+"#,
+            r#"
+struct Bar {
+    y: Y,
+    z: Z,
+}
+
+fn foo(bar: Bar) {
+    let Bar { y, z } = bar;
+}
+"#,
+        )
+    }
+
+    #[test]
+    fn not_applicable_when_not_in_ellipsis() {
+        check_assist_not_applicable(
+            fill_record_pattern_fields,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B{..}$0 => true,
+    };
+}
+"#,
+        );
+        check_assist_not_applicable(
+            fill_record_pattern_fields,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::B$0{..} => true,
+    };
+}
+"#,
+        );
+        check_assist_not_applicable(
+            fill_record_pattern_fields,
+            r#"
+enum Foo {
+    A(X),
+    B{y: Y, z: Z}
+}
+
+fn bar(foo: Foo) {
+    match foo {
+        Foo::A(_) => false,
+        Foo::$0B{..} => true,
+    };
+}
+"#,
+        );
+    }
+}
