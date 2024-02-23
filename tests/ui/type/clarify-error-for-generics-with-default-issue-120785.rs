39270575f0db7a76c8ed454e4e77ddd3dfe82de3
Veera <sveera.2001@gmail.com>
2024-02-23 13:22:14 -0500

    Update item order in test

--- tests/ui/type/clarify-error-for-generics-with-default-issue-120785.rs
+++ tests/ui/type/clarify-error-for-generics-with-default-issue-120785.rs
@@ -5,7 +5,7 @@ fn main() {
     let c: What<usize, String> = What(1, String::from("meow"));
     b = c; //~ ERROR mismatched types
 
-    let mut e: What<usize> = What(5, vec![1, 2, 3]);
-    let f: What<usize, Vec<String>> = What(1, vec![String::from("meow")]);
-    e = f; //~ ERROR mismatched types
+    let mut f: What<usize, Vec<String>> = What(1, vec![String::from("meow")]);
+    let e: What<usize> = What(5, vec![1, 2, 3]);
+    f = e; //~ ERROR mismatched types
 }
