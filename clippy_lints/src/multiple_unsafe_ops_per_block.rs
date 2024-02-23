013bf92dcc3a602bd9b82675fe86bf396e115224
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-02-23 23:29:09 +0000

    Add `nested` bool to `DefKind::Static`.
    Will be used in the next commit

--- clippy_lints/src/multiple_unsafe_ops_per_block.rs
+++ clippy_lints/src/multiple_unsafe_ops_per_block.rs
@@ -109,7 +109,7 @@ fn collect_unsafe_exprs<'tcx>(
             ExprKind::Path(QPath::Resolved(
                 _,
                 hir::Path {
-                    res: Res::Def(DefKind::Static{mt:Mutability::Mut}, _),
+                    res: Res::Def(DefKind::Static{mt:Mutability::Mut, ..}, _),
                     ..
                 },
             )) => {
@@ -149,7 +149,7 @@ fn collect_unsafe_exprs<'tcx>(
                     ExprKind::Path(QPath::Resolved(
                         _,
                         hir::Path {
-                            res: Res::Def(DefKind::Static{mt:Mutability::Mut}, _),
+                            res: Res::Def(DefKind::Static{mt:Mutability::Mut, ..}, _),
                             ..
                         }
                     ))
