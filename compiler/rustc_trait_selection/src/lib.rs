e1e02a27f43d829e3d0166e1d9c42a30eb330b7d
Jason Newcomb <jsnewcomb@pm.me>
2024-02-24 19:38:58 -0500

    Convert `ProofTreeVisitor` to use `VisitorResult`

--- compiler/rustc_trait_selection/src/lib.rs
+++ compiler/rustc_trait_selection/src/lib.rs
@@ -17,6 +17,7 @@
 #![allow(rustc::diagnostic_outside_of_impl)]
 #![allow(rustc::untranslatable_diagnostic)]
 #![feature(associated_type_bounds)]
+#![feature(associated_type_defaults)]
 #![feature(box_patterns)]
 #![feature(control_flow_enum)]
 #![feature(extract_if)]
