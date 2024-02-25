7d82dd0d31b426afaf88d307cc3fb7deddb44552
Lieselotte <52315535+she3py@users.noreply.github.com>
2024-02-25 22:22:09 +0100

    Add `ast::ExprKind::Dummy`

--- src/utils.rs
+++ src/utils.rs
@@ -497,6 +497,7 @@ pub(crate) fn is_block_expr(context: &RewriteContext<'_>, expr: &ast::Expr, repr
         | ast::ExprKind::Break(..)
         | ast::ExprKind::Cast(..)
         | ast::ExprKind::Continue(..)
+        | ast::ExprKind::Dummy
         | ast::ExprKind::Err
         | ast::ExprKind::Field(..)
         | ast::ExprKind::IncludedBytes(..)
