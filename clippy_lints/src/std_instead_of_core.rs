74cba639e9dc68b252f54db711be776a8bbe5c5e
MarcusGrass <marcus.grass@protonmail.com>
2024-03-03 15:30:49 +0100

    Dedup std_instead_of_core by using first segment span for uniqueness

--- clippy_lints/src/std_instead_of_core.rs
+++ clippy_lints/src/std_instead_of_core.rs
@@ -109,7 +109,7 @@ impl<'tcx> LateLintPass<'tcx> for StdReexports {
                     sym::core => (STD_INSTEAD_OF_CORE, "std", "core"),
                     sym::alloc => (STD_INSTEAD_OF_ALLOC, "std", "alloc"),
                     _ => {
-                        self.prev_span = path.span;
+                        self.prev_span = first_segment.ident.span;
                         return;
                     },
                 },
@@ -117,13 +117,13 @@ impl<'tcx> LateLintPass<'tcx> for StdReexports {
                     if cx.tcx.crate_name(def_id.krate) == sym::core {
                         (ALLOC_INSTEAD_OF_CORE, "alloc", "core")
                     } else {
-                        self.prev_span = path.span;
+                        self.prev_span = first_segment.ident.span;
                         return;
                     }
                 },
                 _ => return,
             };
-            if path.span != self.prev_span {
+            if first_segment.ident.span != self.prev_span {
                 span_lint_and_sugg(
                     cx,
                     lint,
@@ -133,7 +133,7 @@ impl<'tcx> LateLintPass<'tcx> for StdReexports {
                     replace_with.to_string(),
                     Applicability::MachineApplicable,
                 );
-                self.prev_span = path.span;
+                self.prev_span = first_segment.ident.span;
             }
         }
     }
