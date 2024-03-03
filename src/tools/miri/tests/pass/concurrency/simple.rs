8deb65160307c0cf71b07f5d930cba7ae6344bc1
Ralf Jung <post@ralfj.de>
2024-03-03 09:00:38 +0100

    move thread-panic tests to their own file; test getting the thread name

--- src/tools/miri/tests/pass/concurrency/simple.rs
+++ src/tools/miri/tests/pass/concurrency/simple.rs
@@ -45,23 +45,6 @@ fn create_move_out() {
     assert_eq!(result.len(), 6);
 }
 
-fn panic() {
-    let result = thread::spawn(|| panic!("Hello!")).join().unwrap_err();
-    let msg = result.downcast_ref::<&'static str>().unwrap();
-    assert_eq!(*msg, "Hello!");
-}
-
-fn panic_named() {
-    thread::Builder::new()
-        .name("childthread".to_string())
-        .spawn(move || {
-            panic!("Hello, world!");
-        })
-        .unwrap()
-        .join()
-        .unwrap_err();
-}
-
 // This is not a data race!
 fn shared_readonly() {
     use std::sync::Arc;
@@ -89,6 +72,4 @@ fn main() {
     create_move_in();
     create_move_out();
     shared_readonly();
-    panic();
-    panic_named();
 }
