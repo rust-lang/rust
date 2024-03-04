8364a0626038ab4d22018d5223c9adda4be3aa49
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-03-04 11:25:56 +0000

    Merge the impl trait in assoc type collector into the opaque type collector and use a runtime switch instead

--- tests/ui/type-alias-impl-trait/hidden_behind_struct_field3.rs
+++ tests/ui/type-alias-impl-trait/hidden_behind_struct_field3.rs
@@ -1,7 +1,6 @@
 //! This test demonstrates a bug where we accidentally
 //! detected opaque types in struct fields, but only if nested
 //! in projections of another opaque type.
-//@ check-pass
 
 #![feature(impl_trait_in_assoc_type)]
 
@@ -18,6 +17,7 @@ impl Trait for Bar {
     type Assoc = impl Iterator<Item = Foo>;
     fn foo() -> Self::Assoc {
         vec![Foo { field: () }].into_iter()
+        //~^ ERROR item constrains opaque type that is not in its signature
     }
 }
 
