523ab25418f5bf624bdb7752ec9d9ecba9b8aa4e
surechen <chenshuo17@huawei.com>
2024-03-04 14:25:51 +0800

    add test for #71450

--- tests/ui/lint/use-redundant/use-redundant-issue-78894.rs
+++ tests/ui/lint/use-redundant/use-redundant-issue-78894.rs
@@ -1,6 +1,15 @@
 //@ check-pass
 //@ edition:2018
+
 #![warn(unused_imports)]
+
+mod foo {
+    macro_rules! foo1 {
+        () => ();
+    }
+
+    pub(crate) use foo1;
+}
 
 fn main ()
 {
@@ -11,4 +20,29 @@ fn main ()
     }
 
     use bar;
+
+    mod m {
+        bar1!();
+
+        macro_rules! bar1 {
+            () => ();
+        }
+
+        use bar1;
+    }
+
+    {
+        foo::foo1!();
+    }
+
+    {
+        use foo::foo1;
+        foo1!();
+    }
+
+    {
+        use foo::foo1; //~ WARNING unused import: `foo::foo1`
+        foo::foo1!();
+    }
+
 }
