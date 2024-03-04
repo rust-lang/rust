e076c4dbf52acc4180cfb8f1a4ede268a93c4692
Andrew Zhogin <andrew.zhogin@gmail.com>
2024-03-04 12:15:33 +0700

    Subpart12 for async drop - tests

--- tests/ui/feature-gates/feature-gate-async-drop.rs
+++ tests/ui/feature-gates/feature-gate-async-drop.rs
@@ -1,6 +1,6 @@
 //@ edition: 2021
 
-use std::future::AsyncDrop; //~ ERROR  use of unstable library feature 'async_drop'
+use std::future::AsyncDrop; //~ ERROR  use of unstable library feature `async_drop`
 use std::pin::Pin;
 
 struct Foo {}
@@ -9,8 +9,8 @@ impl Drop for Foo {
     fn drop(&mut self) {}
 }
 
-impl AsyncDrop for Foo { //~ ERROR  use of unstable library feature 'async_drop'
-    async fn drop(self: Pin<&mut Self>) {} //~ ERROR  use of unstable library feature 'async_drop'
+impl AsyncDrop for Foo { //~ ERROR  use of unstable library feature `async_drop`
+    async fn drop(self: Pin<&mut Self>) {} //~ ERROR  use of unstable library feature `async_drop`
 }
 
 fn main() {}
