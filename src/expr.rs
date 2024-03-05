73c81495cdb03d1fb73dd4313466c146ca8f4d98
MarcusGrass <34198073+MarcusGrass@users.noreply.github.com>
2024-03-05 04:19:57 +0100

    Ensure space after binary exprs that ends with `.` before range expr
    Removing the space would lead to compilation errors.

--- src/expr.rs
+++ src/expr.rs
@@ -282,6 +282,9 @@ pub(crate) fn format_expr(
                 match lhs.kind {
                     ast::ExprKind::Lit(token_lit) => lit_ends_in_dot(&token_lit),
                     ast::ExprKind::Unary(_, ref expr) => needs_space_before_range(context, expr),
+                    ast::ExprKind::Binary(_, _, ref rhs_expr) => {
+                        needs_space_before_range(context, rhs_expr)
+                    }
                     _ => false,
                 }
             }
