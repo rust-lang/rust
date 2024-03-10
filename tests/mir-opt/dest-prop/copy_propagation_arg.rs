f19561d8a588c33fe711c63e2e31d81a1ca26257
CastilloDel <delcastillodelarosadaniel@gmail.com>
2024-03-10 16:33:53 +0100

    Run filecheck on copy_propagation.rs

--- tests/mir-opt/dest-prop/copy_propagation_arg.rs
+++ tests/mir-opt/dest-prop/copy_propagation_arg.rs
@@ -1,4 +1,3 @@
-// skip-filecheck
 // EMIT_MIR_FOR_EACH_PANIC_STRATEGY
 // Check that DestinationPropagation does not propagate an assignment to a function argument
 // (doing so can break usages of the original argument value)
@@ -9,18 +8,25 @@ fn dummy(x: u8) -> u8 {
 
 // EMIT_MIR copy_propagation_arg.foo.DestinationPropagation.diff
 fn foo(mut x: u8) {
+    // CHECK: foo
+    // CHECK-NOT: {{_.*}} = {{_.*}}
     // calling `dummy` to make a use of `x` that copyprop cannot eliminate
     x = dummy(x); // this will assign a local to `x`
 }
 
 // EMIT_MIR copy_propagation_arg.bar.DestinationPropagation.diff
 fn bar(mut x: u8) {
+    // CHECK: bar
+    // CHECK-NOT: {{_.*}} = {{_.*}}
     dummy(x);
     x = 5;
 }
 
 // EMIT_MIR copy_propagation_arg.baz.DestinationPropagation.diff
 fn baz(mut x: i32) -> i32 {
+    // CHECK: baz
+    // CHECK: {{_.*}} = {{_.*}}
+    // CHECK-NOT: {{_.*}} = {{_.*}}
     // self-assignment to a function argument should be eliminated
     x = x;
     x
@@ -28,6 +34,9 @@ fn baz(mut x: i32) -> i32 {
 
 // EMIT_MIR copy_propagation_arg.arg_src.DestinationPropagation.diff
 fn arg_src(mut x: i32) -> i32 {
+    // CHECK: arg_src
+    // CHECK: {{_.*}} = {{_.*}}
+    // CHECK-NOT: {{_.*}} = {{_.*}}
     let y = x;
     x = 123; // Don't propagate this assignment to `y`
     y
