b1d4cbc834431f545473ae594727dad6a82a34de
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-03-13 12:36:28 +0000

    Add regression test

--- tests/ui/consts/dangling_raw_ptr.rs
+++ tests/ui/consts/dangling_raw_ptr.rs
@@ -3,6 +3,16 @@ const FOO: *const u32 = { //~ ERROR it is undefined behavior
     &x
 };
 
+union Union {
+    ptr: *const u32
+}
+
+const BAR: Union = { //~ ERROR encountered dangling pointer in final value
+    let x = 42;
+    Union { ptr: &x }
+};
+
 fn main() {
     let x = FOO;
+    let x = BAR;
 }
