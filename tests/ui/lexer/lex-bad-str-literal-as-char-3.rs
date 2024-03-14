6f388ef1fb3964fb83bae5c277f9c83bbde4c76b
Esteban Küber <esteban@kuber.com.ar>
2024-03-14 00:22:52 +0000

    Extend test to trigger on 2015, 2018 and 2021 editions

--- tests/ui/lexer/lex-bad-str-literal-as-char-3.rs
+++ tests/ui/lexer/lex-bad-str-literal-as-char-3.rs
@@ -1,4 +1,8 @@
-//@ run-rustfix
+//@ revisions: rust2015 rust2018 rust2021
+//@[rust2018] edition:2018
+//@[rust2021] edition:2021
 fn main() {
-    println!('hello world'); //~ ERROR unterminated character literal
+    println!('hello world');
+    //[rust2015,rust2018,rust2021]~^ ERROR unterminated character literal
+    //[rust2021]~^^ ERROR prefix `world` is unknown
 }
