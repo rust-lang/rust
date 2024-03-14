576a0ba2c478adac866f5fc0fb4124282514d3ac
Benoît du Garreau <bdgdlm@outlook.com>
2024-03-14 11:20:59 +0100

    `VecDeque::read_to_string`: avoid making the slices contiguous

--- library/std/src/io/impls.rs
+++ library/std/src/io/impls.rs
@@ -464,17 +464,8 @@ impl<A: Allocator> Read for VecDeque<u8, A> {
 
     #[inline]
     fn read_to_string(&mut self, buf: &mut String) -> io::Result<usize> {
-        // We have to use a single contiguous slice because the `VecDequeue` might be split in the
-        // middle of an UTF-8 character.
-        let len = self.len();
-        let content = self.make_contiguous();
-        let string = str::from_utf8(content).map_err(|_| {
-            io::const_io_error!(ErrorKind::InvalidData, "stream did not contain valid UTF-8")
-        })?;
-        buf.try_reserve(len)?;
-        buf.push_str(string);
-        self.clear();
-        Ok(len)
+        // SAFETY: We only append to the buffer
+        unsafe { io::append_to_string(buf, |buf| self.read_to_end(buf)) }
     }
 }
 
