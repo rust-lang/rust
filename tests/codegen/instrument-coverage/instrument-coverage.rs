5330ccdb345b24be57f051c6659cd3b33a65fdca
Zalathar <Zalathar@users.noreply.github.com>
2024-03-16 17:02:30 +1100

    Use `-Zno-profiler-runtime` instead of `//@ needs-profiler-support`
    For PGO/coverage tests that don't need to build or run an actual artifact, we
    can use `-Zno-profiler-runtime` to run the test even when the profiler runtime
    is not available.

--- tests/codegen/instrument-coverage/instrument-coverage.rs
+++ tests/codegen/instrument-coverage/instrument-coverage.rs
@@ -1,6 +1,6 @@
 // Test that `-Cinstrument-coverage` creates expected __llvm_profile_filename symbol in LLVM IR.
 
-//@ needs-profiler-support
+//@ compile-flags: -Zno-profiler-runtime
 //@ revisions: default y yes on true_ all
 //@ [default] compile-flags: -Cinstrument-coverage
 //@ [y] compile-flags: -Cinstrument-coverage=y
@@ -9,8 +9,8 @@
 //@ [true_] compile-flags: -Cinstrument-coverage=true
 //@ [all] compile-flags: -Cinstrument-coverage=all
 
-// CHECK: @__llvm_profile_filename = {{.*}}"default_%m_%p.profraw\00"{{.*}}
-// CHECK: @__llvm_coverage_mapping
+// CHECK-DAG: @__llvm_coverage_mapping
+// CHECK-DAG: @__llvm_profile_filename = {{.*}}"default_%m_%p.profraw\00"{{.*}}
 
 #![crate_type = "lib"]
 
