8d59aaf735020434a9448742c77f745acb92f60f
Nadrieril <nadrieril+git@gmail.com>
2024-03-17 14:16:30 +0100

    Use in-tree rustc_pattern_analysis

--- crates/hir-ty/src/lib.rs
+++ crates/hir-ty/src/lib.rs
@@ -15,7 +15,10 @@ extern crate rustc_abi;
 #[cfg(not(feature = "in-rust-tree"))]
 extern crate ra_ap_rustc_abi as rustc_abi;
 
-// No need to use the in-tree one.
+#[cfg(feature = "in-rust-tree")]
+extern crate rustc_pattern_analysis;
+
+#[cfg(not(feature = "in-rust-tree"))]
 extern crate ra_ap_rustc_pattern_analysis as rustc_pattern_analysis;
 
 mod builder;
