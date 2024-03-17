e7d397024f6eebdd36bfa31ea8cf56496d348f7f
Petr Portnov <me@progrm-jarvis.ru>
2024-03-17 17:06:12 +0300

    chore(121952): echo comments on the `*_assign` methods

--- library/core/src/num/nonzero.rs
+++ library/core/src/num/nonzero.rs
@@ -851,6 +851,8 @@ macro_rules! nonzero_integer_signedness_dependent_impls {
 
         #[stable(feature = "nonzero_div_assign", since = "CURRENT_RUSTC_VERSION")]
         impl DivAssign<$Ty> for $Int {
+            /// This operation rounds towards zero,
+            /// truncating any fractional part of the exact result, and cannot panic.
             #[inline]
             fn div_assign(&mut self, other: $Ty) {
                 *self = *self / other;
@@ -872,6 +874,7 @@ macro_rules! nonzero_integer_signedness_dependent_impls {
 
         #[stable(feature = "nonzero_div_assign", since = "CURRENT_RUSTC_VERSION")]
         impl RemAssign<$Ty> for $Int {
+            /// This operation satisfies `n % d == n - (n / d) * d`, and cannot panic.
             #[inline]
             fn rem_assign(&mut self, other: $Ty) {
                 *self = *self % other;
