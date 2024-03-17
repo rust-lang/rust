f512f9e94903b81030672678592aff44ae777e20
Josh Stone <cuviper@gmail.com>
2024-03-17 13:10:49 -0700

    Use `pushd` and `popd`
    Co-authored-by: Urgau <3616612+Urgau@users.noreply.github.com>

--- src/ci/docker/host-x86_64/dist-x86_64-linux/build-gccjit.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/build-gccjit.sh
@@ -16,7 +16,7 @@ curl -L "$GIT_REPO/archive/$GIT_COMMIT.tar.gz" |
     tar -xz --transform "s/gcc-$GIT_COMMIT/gcc-src/"
 
 mkdir gcc-build gcc-install
-cd gcc-build
+pushd gcc-build
 
 # Building GCC.
 hide_output \
@@ -31,7 +31,7 @@ hide_output \
 hide_output make -j$(nproc)
 hide_output make install
 
-cd ..
+popd
 rm -rf gcc-src gcc-build
 ln -s /scripts/gcc-install/lib/libgccjit.so /usr/lib/x86_64-linux-gnu/libgccjit.so
 ln -s /scripts/gcc-install/lib/libgccjit.so /usr/lib/x86_64-linux-gnu/libgccjit.so.0
