935842bf0a7b3401355ea5285f1b8c6646a242c4
Veera <sveera.2001@gmail.com>
2024-03-18 11:17:27 -0400

    Add tests

---
+++ tests/ui/asm/fail-const-eval-issue-121099.rs
@@ -0,0 +1,10 @@
+//@ build-fail 
+#![feature(asm_const)]
+
+use std::arch::global_asm;
+
+fn main() {}
+
+global_asm!("/* {} */", const 1 << 500); //~ ERROR evaluation of constant value failed [E0080]
+
+global_asm!("/* {} */", const 1 / 0); //~ ERROR evaluation of constant value failed [E0080]
