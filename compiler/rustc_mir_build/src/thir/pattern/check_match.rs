e2bdd6d90f266f375281176d8bc48734e41e8493
Nadrieril <nadrieril+git@gmail.com>
2024-03-20 19:24:42 +0100

    Stabilize `min_exhaustive_patterns`

--- compiler/rustc_mir_build/src/thir/pattern/check_match.rs
+++ compiler/rustc_mir_build/src/thir/pattern/check_match.rs
@@ -688,9 +688,7 @@ impl<'p, 'tcx> MatchVisitor<'p, 'tcx> {
 
         // Emit an extra note if the first uncovered witness would be uninhabited
         // if we disregard visibility.
-        let witness_1_is_privately_uninhabited = if (self.tcx.features().exhaustive_patterns
-            || self.tcx.features().min_exhaustive_patterns)
-            && let Some(witness_1) = witnesses.get(0)
+        let witness_1_is_privately_uninhabited = if let Some(witness_1) = witnesses.get(0)
             && let ty::Adt(adt, args) = witness_1.ty().kind()
             && adt.is_enum()
             && let Constructor::Variant(variant_index) = witness_1.ctor()
