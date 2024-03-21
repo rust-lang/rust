81f04efb51d8bcb5437fcc30c47592cda5c6febd
Michael Baikov <manpacket@gmail.com>
2024-03-21 12:26:50 -0400

    Update tests to account for emitted obj

--- tests/ui/json/json-multiple.rs
+++ tests/ui/json/json-multiple.rs
@@ -1,5 +1,6 @@
 //@ build-pass
 //@ ignore-pass (different metadata emitted in different modes)
 //@ compile-flags: --json=diagnostic-short --json artifacts --error-format=json
+//@ normalize-stderr-test: "json_multiple\.[0-9a-zA-Z]+-cgu" -> "json_multiple.HASH-cgu"
 
 #![crate_type = "lib"]
--- tests/ui/json/json-options.rs
+++ tests/ui/json/json-options.rs
@@ -1,5 +1,6 @@
 //@ build-pass
 //@ ignore-pass (different metadata emitted in different modes)
 //@ compile-flags: --json=diagnostic-short,artifacts --error-format=json
+//@ normalize-stderr-test: "json_options\.[0-9a-zA-Z]+-cgu" -> "json_options.HASH-cgu"
 
 #![crate_type = "lib"]
