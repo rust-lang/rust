f1f287fadb91451b116ff00a2f8b9c32c5e1571b
Matthias Krüger <matthias.krueger@famsik.de>
2024-03-23 12:24:40 +0100

    add test for  ICE "raw ptr comparison should already be caught in the trait system" #105047
    Fixes #105047

---
+++ tests/ui/raw-ref-op/const-eval-compare-ice-105047.rs
@@ -0,0 +1,15 @@
+// issue: rust-lang/rust#105047
+// ICE raw ptr comparison should already be caught in the trait systems
+
+#![feature(raw_ref_op)]
+
+const RCZ: *const i32 = &raw const *&0;
+
+const fn f() {
+    if let RCZ = &raw const *&0 { }
+    //~^ WARN function pointers and raw pointers not derived from integers in patterns
+    //~^^ ERROR pointers cannot be reliably compared during const eval
+    //~^^^ WARN this was previously accepted by the compiler but is being phased out
+}
+
+fn main() {}
