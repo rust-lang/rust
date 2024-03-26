d7d5fc97341ebc127ad716053c3efe6e613ba3a7
Trevor Gross <tmgross@umich.edu>
2024-03-26 05:22:18 -0400

    Add basic trait impls for `f16` and `f128`
    Split off part of <https://github.com/rust-lang/rust/pull/122470> so the
    compiler doesn't ICE because it expects primitives to have some minimal
    traits.
    
    Fixes <https://github.com/rust-lang/rust/issues/123074>

--- library/core/src/cmp.rs
+++ library/core/src/cmp.rs
@@ -1486,7 +1486,7 @@ mod impls {
     }
 
     partial_eq_impl! {
-        bool char usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f32 f64
+        bool char usize u8 u16 u32 u64 u128 isize i8 i16 i32 i64 i128 f16 f32 f64 f128
     }
 
     macro_rules! eq_impl {
@@ -1539,7 +1539,7 @@ mod impls {
         }
     }
 
-    partial_ord_impl! { f32 f64 }
+    partial_ord_impl! { f16 f32 f64 f128 }
 
     macro_rules! ord_impl {
         ($($t:ty)*) => ($(
