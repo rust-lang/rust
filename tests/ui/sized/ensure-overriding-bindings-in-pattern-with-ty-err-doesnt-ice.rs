d54e9833e3513958a9e03c5f50b9496c618ebde5
Esteban Küber <esteban@kuber.com.ar>
2024-03-29 19:05:54 +0000

    Do not attempt to write `ty::Err` on binding that isn't from current HIR Owner
    Fix #123009.

---
+++ tests/ui/sized/ensure-overriding-bindings-in-pattern-with-ty-err-doesnt-ice.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let str::<{fn str() { let str::T>>::as_bytes; }}, T>::as_bytes;
+//~^ ERROR expected a pattern, found an expression
+//~| ERROR cannot find type `T` in this scope
+//~| ERROR type and const arguments are not allowed on builtin type `str`
+//~| ERROR expected unit struct, unit variant or constant, found associated function `str<, T>::as_bytes`
+}
