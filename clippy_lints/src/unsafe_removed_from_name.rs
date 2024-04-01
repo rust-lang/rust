f0f392781f2829aaf4368e28c2ba3e9bc0e6115f
Pietro Albini <pietro@pietroalbini.org>
2024-04-02 00:26:10 +0200

    store the span of the nested part of the use tree in the ast

--- clippy_lints/src/unsafe_removed_from_name.rs
+++ clippy_lints/src/unsafe_removed_from_name.rs
@@ -49,8 +49,8 @@ fn check_use_tree(use_tree: &UseTree, cx: &EarlyContext<'_>, span: Span) {
             unsafe_to_safe_check(old_name, new_name, cx, span);
         },
         UseTreeKind::Simple(None) | UseTreeKind::Glob => {},
-        UseTreeKind::Nested(ref nested_use_tree) => {
-            for (use_tree, _) in nested_use_tree {
+        UseTreeKind::Nested { ref items, .. } => {
+            for (use_tree, _) in items {
                 check_use_tree(use_tree, cx, span);
             }
         },
