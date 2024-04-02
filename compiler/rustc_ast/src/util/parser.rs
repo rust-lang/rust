4cb5643bd4b9a66f8c8b8fee2505a7f09b8ed10a
Michael Goulet <michael@errs.io>
2024-04-02 19:40:12 -0400

    Fix contains_exterior_struct_lit

--- compiler/rustc_ast/src/util/parser.rs
+++ compiler/rustc_ast/src/util/parser.rs
@@ -392,7 +392,8 @@ pub fn contains_exterior_struct_lit(value: &ast::Expr) -> bool {
         | ast::ExprKind::Cast(x, _)
         | ast::ExprKind::Type(x, _)
         | ast::ExprKind::Field(x, _)
-        | ast::ExprKind::Index(x, _, _) => {
+        | ast::ExprKind::Index(x, _, _)
+        | ast::ExprKind::Match(x, _, ast::MatchKind::Postfix) => {
             // &X { y: 1 }, X { y: 1 }.y
             contains_exterior_struct_lit(x)
         }
