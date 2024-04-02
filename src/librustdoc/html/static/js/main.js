1ca145778a4971d82c56025ece1824ec672428b9
mu001999 <mu001999@outlook.com>
2024-04-02 14:35:25 +0800

    Support type '/' to search

--- src/librustdoc/html/static/js/main.js
+++ src/librustdoc/html/static/js/main.js
@@ -438,6 +438,7 @@ function preLoadCss(cssUrl) {
 
             case "s":
             case "S":
+            case "/":
                 ev.preventDefault();
                 searchState.focus();
                 break;
@@ -1310,7 +1311,7 @@ function preLoadCss(cssUrl) {
 
         const shortcuts = [
             ["?", "Show this help dialog"],
-            ["S", "Focus the search field"],
+            ["S / /", "Focus the search field"],
             ["↑", "Move up in search results"],
             ["↓", "Move down in search results"],
             ["← / →", "Switch result tab (when results focused)"],
