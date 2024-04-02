17a06d9c27556ad944b58f2d98128e6d87b3115a
Jamie Cunliffe <Jamie.Cunliffe@arm.com>
2024-04-02 15:27:22 +0100

    Add ?Sized bound to some SIMD intrinsics.

--- compiler/rustc_hir_analysis/src/check/intrinsic.rs
+++ compiler/rustc_hir_analysis/src/check/intrinsic.rs
@@ -623,6 +623,7 @@ pub fn check_intrinsic_type(
             sym::simd_cast
             | sym::simd_as
             | sym::simd_cast_ptr
+            | sym::simd_reinterpret
             | sym::simd_expose_addr
             | sym::simd_from_exposed_addr => (2, 0, vec![param(0)], param(1)),
             sym::simd_bitmask => (2, 0, vec![param(0)], param(1)),
