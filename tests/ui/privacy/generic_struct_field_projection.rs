5d40b801dc935a1d7cfcc458915dd554c4e406ef
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-04-02 17:27:35 +0000

    Only inspect user-written predicates for privacy concerns
    (cherry picked from commit 83bd12c70fd34dece71bcc632ee3df64036ca1d8)

--- tests/ui/privacy/generic_struct_field_projection.rs
+++ tests/ui/privacy/generic_struct_field_projection.rs
@@ -1,11 +1,15 @@
 //! To determine all the types that need to be private when looking at `Struct`, we
-//! invoke `predicates_of` to also look at types in `where` bounds.
+//! used to invoke `predicates_of` to also look at types in `where` bounds.
 //! Unfortunately this also computes the inferred outlives bounds, which means for
 //! every field we check that if it is of type `&'a T` then `T: 'a` and if it is of
 //! struct type, we check that the struct satisfies its lifetime parameters by looking
 //! at its inferred outlives bounds. This means we end up with a `<Foo as Trait>::Assoc: 'a`
 //! in the outlives bounds of `Struct`. While this is trivially provable, privacy
-//! only sees `Foo` and `Trait` and determins that `Foo` is private and then errors.
+//! only sees `Foo` and `Trait` and determines that `Foo` is private and then errors.
+//! So now we invoke `explicit_predicates_of` to make sure we only care about user-written
+//! predicates.
+
+//@ check-pass
 
 mod baz {
     struct Foo;
@@ -20,7 +24,6 @@ mod baz {
 
     pub struct Bar<'a, T: Trait> {
         source: &'a T::Assoc,
-        //~^ ERROR: type `Foo` is private
     }
 
     pub struct Baz<'a> {
