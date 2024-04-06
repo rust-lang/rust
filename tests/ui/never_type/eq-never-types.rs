62d956fde5350da099acb760a790df3eb04e6e74
Maybe Waffle <waffle.lapkin@gmail.com>
2024-04-06 20:52:32 +0000

    Correctly change type when adding adjustments on top of `NeverToAny`

--- tests/ui/never_type/eq-never-types.rs
+++ tests/ui/never_type/eq-never-types.rs
@@ -1,15 +1,6 @@
-//@ known-bug: #120600
+//@ check-pass
 //
 // issue: rust-lang/rust#120600
-
-//@ failure-status: 101
-//@ normalize-stderr-test: "DefId\(.*?\]::" -> "DefId("
-//@ normalize-stderr-test: "(?m)note: we would appreciate a bug report.*\n\n" -> ""
-//@ normalize-stderr-test: "(?m)note: rustc.*running on.*\n\n" -> ""
-//@ normalize-stderr-test: "(?m)note: compiler flags.*\n\n" -> ""
-//@ normalize-stderr-test: "(?m)note: delayed at.*$" -> ""
-//@ normalize-stderr-test: "(?m)^ *\d+: .*\n" -> ""
-//@ normalize-stderr-test: "(?m)^ *at .*\n" -> ""
 
 #![allow(internal_features)]
 #![feature(never_type, rustc_attrs)]
