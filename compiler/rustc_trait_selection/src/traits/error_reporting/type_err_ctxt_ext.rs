0a88339a57557152bb32c4969f2f76955793133e
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-04-09 10:08:20 +0000

    Don't ICE for kind mismatches during error rendering

--- compiler/rustc_trait_selection/src/traits/error_reporting/type_err_ctxt_ext.rs
+++ compiler/rustc_trait_selection/src/traits/error_reporting/type_err_ctxt_ext.rs
@@ -1974,6 +1974,9 @@ impl<'tcx> TypeErrCtxt<'_, 'tcx> {
                     for (obligation_arg, impl_arg) in
                         std::iter::zip(obligation_trait_ref.args, impl_trait_ref.args)
                     {
+                        if (obligation_arg, impl_arg).references_error() {
+                            return false;
+                        }
                         if let Err(terr) =
                             ocx.eq(&ObligationCause::dummy(), param_env, impl_arg, obligation_arg)
                         {
