c0a9c8c9549662ec9199112cb59592ef2c617412
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-04-09 10:23:58 +0000

    Silence some follow-up errors on trait impls in case the trait has conflicting or otherwise incoherent impls

--- tests/ui/wf/conflicting-impls.rs
+++ tests/ui/wf/conflicting-impls.rs
@@ -5,7 +5,6 @@ struct Ty;
 impl TryFrom<Ty> for u8 {
     type Error = Ty;
     fn try_from(_: Ty) -> Result<Self, Self::Error> {
-        //~^ ERROR type annotations needed
         loop {}
     }
 }
@@ -14,7 +13,6 @@ impl TryFrom<Ty> for u8 {
     //~^ ERROR conflicting implementations of trait
     type Error = Ty;
     fn try_from(_: Ty) -> Result<Self, Self::Error> {
-        //~^ ERROR type annotations needed
         loop {}
     }
 }
