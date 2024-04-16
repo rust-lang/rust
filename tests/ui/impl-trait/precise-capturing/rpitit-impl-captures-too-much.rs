7b0e96218597fd29db2d2b7dacd884583afed32a
Michael Goulet <michael@errs.io>
2024-04-16 12:36:35 -0400

    Tests, suppress refine lint when errors

---
+++ tests/ui/impl-trait/precise-capturing/rpitit-impl-captures-too-much.rs
@@ -0,0 +1,14 @@
+#![feature(precise_capturing)]
+
+struct Invariant<'a>(&'a mut &'a mut ());
+
+trait Trait {
+    fn hello(self_: Invariant<'_>) -> impl Sized + use<Self>;
+}
+
+impl Trait for () {
+    fn hello(self_: Invariant<'_>) -> impl Sized + use<'_> {}
+    //~^ return type captures more lifetimes than trait definition
+}
+
+fn main() {}
