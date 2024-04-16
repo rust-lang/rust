1567d4d8504ed7bb57e77063fbc10a2d2f56c57a
Ben Kimock <kimockb@gmail.com>
2024-04-15 23:46:27 -0400

    Remove libc from more tests

--- tests/ui/rfcs/rfc-1014-stdout-existential-crisis/rfc-1014.rs
+++ tests/ui/rfcs/rfc-1014-stdout-existential-crisis/rfc-1014.rs
@@ -1,28 +1,27 @@
+// Test that println! to a closed stdout does not panic.
+// On Windows, close via CloseHandle.
 //@ run-pass
-#![allow(dead_code)]
 //@ ignore-sgx no libc
 
 #![feature(rustc_private)]
 
-extern crate libc;
-
-type DWORD = u32;
-type HANDLE = *mut u8;
-
-#[cfg(windows)]
-extern "system" {
-    fn GetStdHandle(which: DWORD) -> HANDLE;
-    fn CloseHandle(handle: HANDLE) -> i32;
-}
-
 #[cfg(windows)]
 fn close_stdout() {
+    type DWORD = u32;
+    type HANDLE = *mut u8;
+
+    extern "system" {
+        fn GetStdHandle(which: DWORD) -> HANDLE;
+        fn CloseHandle(handle: HANDLE) -> i32;
+    }
+
     const STD_OUTPUT_HANDLE: DWORD = -11i32 as DWORD;
     unsafe { CloseHandle(GetStdHandle(STD_OUTPUT_HANDLE)); }
 }
 
 #[cfg(not(windows))]
 fn close_stdout() {
+    extern crate libc;
     unsafe { libc::close(1); }
 }
 
