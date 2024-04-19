2f29f782549a22aecacc0ba3d525c2785e40401b
Esteban Küber <esteban@kuber.com.ar>
2024-04-19 04:26:42 +0000

    Suggest cloning captured binding in `move` closure
    ```
    error[E0507]: cannot move out of `bar`, a captured variable in an `FnMut` closure
      --> $DIR/borrowck-move-by-capture.rs:9:29
       |
    LL |     let bar: Box<_> = Box::new(3);
       |         --- captured outer variable
    LL |     let _g = to_fn_mut(|| {
       |                        -- captured by this `FnMut` closure
    LL |         let _h = to_fn_once(move || -> isize { *bar });
       |                             ^^^^^^^^^^^^^^^^   ----
       |                             |                  |
       |                             |                  variable moved due to use in closure
       |                             |                  move occurs because `bar` has type `Box<isize>`, which does not implement the `Copy` trait
       |                             `bar` is moved here
       |
    help: clone the value before moving it into the closure
       |
    LL ~         let value = bar.clone();
    LL ~         let _h = to_fn_once(move || -> isize { value });
       |
    ```

--- tests/ui/suggestions/option-content-move3.rs
+++ tests/ui/suggestions/option-content-move3.rs
@@ -1,10 +1,23 @@
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct NotCopyable;
+#[derive(Debug, Clone)]
+struct NotCopyableButCloneable;
 
 fn func<F: FnMut() -> H, H: FnMut()>(_: F) {}
 
-fn parse() {
-    let mut var = NotCopyable;
+fn foo() {
+    let var = NotCopyable;
+    func(|| {
+        // Shouldn't suggest `move ||.as_ref()` here
+        move || { //~ ERROR cannot move out of `var`
+            let x = var; //~ ERROR cannot move out of `var`
+            println!("{x:?}");
+        }
+    });
+}
+
+fn bar() {
+    let var = NotCopyableButCloneable;
     func(|| {
         // Shouldn't suggest `move ||.as_ref()` here
         move || { //~ ERROR cannot move out of `var`
