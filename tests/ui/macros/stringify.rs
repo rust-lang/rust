78c8dc123495f19b2da8e655cd5b95fff161c8c1
David Tolnay <dtolnay@gmail.com>
2024-04-20 11:23:01 -0700

    Fix redundant parens around braced macro call in match arms

--- tests/ui/macros/stringify.rs
+++ tests/ui/macros/stringify.rs
@@ -225,7 +225,7 @@ fn test_expr() {
     );
     c2_match_arm!(
         [ m! {} - 1 ],
-        "match () { _ => (m! {}) - 1, }", // parenthesis is redundant
+        "match () { _ => m! {} - 1, }",
         "match () { _ => m! {} - 1 }",
     );
 
