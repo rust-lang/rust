b756b11ae399413ec728e3a01393a4eba00be2fc
Jubilee Young <workingjubilee@gmail.com>
2024-04-21 18:39:48 -0700

    bootstrap: Describe build_steps::clean how 2024 knows it

--- src/bootstrap/src/core/build_steps/clean.rs
+++ src/bootstrap/src/core/build_steps/clean.rs
@@ -1,4 +1,4 @@
-//! Implementation of `make clean` in rustbuild.
+//! `./x.py clean`
 //!
 //! Responsible for cleaning out a build directory of all old and stale
 //! artifacts to prepare for a fresh build. Currently doesn't remove the
