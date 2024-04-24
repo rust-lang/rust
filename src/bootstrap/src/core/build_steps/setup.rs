f4e02a193e39958df1cee6abeefc62e50354fdea
Jubilee Young <workingjubilee@gmail.com>
2024-04-24 16:56:41 -0700

    bootstrap: reinterpret the main advantage of x.py setup

--- src/bootstrap/src/core/build_steps/setup.rs
+++ src/bootstrap/src/core/build_steps/setup.rs
@@ -1,8 +1,9 @@
 //! First time setup of a dev environment
 //!
 //! These are build-and-run steps for `./x.py setup`, which allows quickly setting up the directory
-//! for modifying, building, and running the compiler and library. The main convenience is to allow
-//! not having to painstakingly set every single option in config.toml.
+//! for modifying, building, and running the compiler and library. Running arbitrary configuration
+//! allows setting up things that cannot be simply captured inside the config.toml, in addition to
+//! leading people away from manually editing most of the config.toml values.
 
 use crate::core::builder::{Builder, RunConfig, ShouldRun, Step};
 use crate::t;
