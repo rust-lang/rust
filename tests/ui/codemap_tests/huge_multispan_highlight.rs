21e97a67ab4f9c8da08b22fb83b92383551a80d5
klensy <klensy@users.noreply.github.com>
2024-04-24 10:42:01 +0300

    tests: remove few ignore-stage2
    beta was branched long ago, so can be removed

--- tests/ui/codemap_tests/huge_multispan_highlight.rs
+++ tests/ui/codemap_tests/huge_multispan_highlight.rs
@@ -1,7 +1,5 @@
 //@ compile-flags: --error-format=human --color=always
 //@ ignore-windows
-// Temporary until next release:
-//@ ignore-stage2
 fn main() {
     let _ = match true {
         true => (
