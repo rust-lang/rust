984039f8a4e780a9cc9c22dd5f5ac44d5975b38b
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-04-25 08:55:39 +0000

    Test RUSTC_OVERRIDE_VERSION_STRING

---
+++ tests/ui/feature-gates/version_check.rs
@@ -0,0 +1,14 @@
+//@ run-pass
+use std::process::Command;
+
+fn main() {
+    let signalled_version = "Ceci n'est pas une rustc";
+    let version = Command::new(std::env::var_os("RUSTC").unwrap())
+        .env("RUSTC_OVERRIDE_VERSION_STRING", signalled_version)
+        .arg("--version")
+        .output()
+        .unwrap()
+        .stdout;
+    let version = std::str::from_utf8(&version).unwrap().strip_prefix("rustc ").unwrap().trim_end();
+    assert_ne!(version, signalled_version);
+}
