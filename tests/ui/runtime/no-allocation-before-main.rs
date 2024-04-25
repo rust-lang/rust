495594742decace2ea2f3c38a5f817c3bae0ea1a
GnomedDev <david2005thomas@gmail.com>
2024-04-25 21:25:04 +0100

    Only run no-allocation-before-main on linux-gnu.

--- tests/ui/runtime/no-allocation-before-main.rs
+++ tests/ui/runtime/no-allocation-before-main.rs
@@ -3,8 +3,11 @@
 //! The initial runtime should not allocate for performance/binary size reasons.
 //!
 //! -Cprefer-dynamic=no is required as otherwise #[global_allocator] does nothing.
+//! We only test linux-gnu as other targets currently need allocation for thread dtors.
 //@ run-pass
 //@ compile-flags: -Cprefer-dynamic=no
+//@ only-linux
+//@ only-gnu
 
 #[allow(dead_code)]
 #[path = "aborting-alloc.rs"]
