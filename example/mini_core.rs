2b875f09274c97df5f4d34cac98ad48cf5ce7bbc
Mark Rousskov <mark.simulacrum@gmail.com>
2024-04-29 09:27:34 -0400

    Step bootstrap cfgs

--- example/mini_core.rs
+++ example/mini_core.rs
@@ -420,7 +420,6 @@ pub fn panic(_msg: &'static str) -> ! {
 
 macro_rules! panic_const {
     ($($lang:ident = $message:expr,)+) => {
-        #[cfg(not(bootstrap))]
         pub mod panic_const {
             use super::*;
 
