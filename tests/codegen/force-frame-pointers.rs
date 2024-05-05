f301d087d4d5d0dfdc9049d0bf09d7dcfc3ac78e
Jubilee Young <workingjubilee@gmail.com>
2024-05-04 18:05:44 -0700

    test: for frame-pointer=non-leaf codegen opts

--- tests/codegen/force-frame-pointers.rs
+++ tests/codegen/force-frame-pointers.rs
@@ -1,6 +1,11 @@
-//@ compile-flags: -C no-prepopulate-passes -C force-frame-pointers=y -Copt-level=0
+//@ revisions: Always NonLeaf
+//@ [Always] compile-flags: -Cforce-frame-pointers=yes
+//@ [NonLeaf] compile-flags: -Cforce-frame-pointers=non-leaf
+//@ compile-flags: -Zunstable-options
+//@ compile-flags: -C no-prepopulate-passes -Copt-level=0
 
 #![crate_type = "lib"]
 
-// CHECK: attributes #{{.*}} "frame-pointer"="all"
+// Always: attributes #{{.*}} "frame-pointer"="all"
+// NonLeaf: attributes #{{.*}} "frame-pointer"="non-leaf"
 pub fn foo() {}
