b32efa1dcba804ef4dc156ee3fe53c914755a8e8
Matthew Maurer <mmaurer@google.com>
2024-05-07 19:26:59 +0000

    sanitizer: Fix scs-attr check test
    This test was not actually being run before - it only runs on targets
    with SCS enabled, which is limited to `aarch64-linux-android` at the
    moment. This means that the test has been sitting broken and needs to be
    fixed before we can enable `aarch64-linux-android` in testing.
    
    * Rename CHECK function names to match current crate name
    * Disable optimization to prevent the functions from being made MIR-only

--- tests/codegen/sanitizer/scs-attr-check.rs
+++ tests/codegen/sanitizer/scs-attr-check.rs
@@ -3,15 +3,17 @@
 //
 //@ needs-sanitizer-shadow-call-stack
 //@ compile-flags: -Zsanitizer=shadow-call-stack
+// With optimization, Rust may decide to make these functions MIR-only.
+//@ compile-flags: -C opt-level=0
 
 #![crate_type = "lib"]
 #![feature(no_sanitize)]
 
-// CHECK: ; sanitizer_scs_attr_check::scs
+// CHECK: ; scs_attr_check::scs
 // CHECK-NEXT: ; Function Attrs:{{.*}}shadowcallstack
 pub fn scs() {}
 
-// CHECK: ; sanitizer_scs_attr_check::no_scs
+// CHECK: ; scs_attr_check::no_scs
 // CHECK-NOT: ; Function Attrs:{{.*}}shadowcallstack
 #[no_sanitize(shadow_call_stack)]
 pub fn no_scs() {}
