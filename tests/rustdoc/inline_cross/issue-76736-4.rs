75e17c96a80dad1f497f8b6c24fd9456c83da1c5
Michael Howell <michael@notriddle.com>
2024-05-07 15:24:44 -0700

    rustdoc: use stability, instead of features, to decide what to show
    To decide if internal items should be inlined in a doc page,
    check if the crate is itself internal, rather than if it has
    the rustc_private feature flag. The standard library uses
    internal items, but is not itself internal and should not show
    internal items on its docs pages.
    
    (cherry picked from commit 6d6f67a98cbc399ac14a3f9a8ddf6ed30bb7fb83)

--- tests/rustdoc/inline_cross/issue-76736-4.rs
+++ tests/rustdoc/inline_cross/issue-76736-4.rs
@@ -4,15 +4,16 @@
 // https://github.com/rust-lang/rust/issues/124635
 
 #![crate_name = "foo"]
-#![feature(rustc_private)]
+#![feature(rustc_private, staged_api)]
+#![unstable(feature = "rustc_private", issue = "none")]
 
 extern crate issue_76736_1;
 extern crate issue_76736_2;
 
 // @has foo/struct.Foo.html
-// @!has - '//*[@class="impl"]//h3[@class="code-header"]' 'MaybeResult'
+// @has - '//*[@class="impl"]//h3[@class="code-header"]' 'MaybeResult'
 pub struct Foo;
 
 // @has foo/struct.Bar.html
-// @!has - '//*[@class="impl"]//h3[@class="code-header"]' 'MaybeResult'
+// @has - '//*[@class="impl"]//h3[@class="code-header"]' 'MaybeResult'
 pub use issue_76736_2::Bar;
