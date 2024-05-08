75f57670b03a33dd165685fe1f252969ef5c16ea
Ralf Jung <post@ralfj.de>
2024-05-08 17:22:35 +0200

    miri: rename intrinsic_fallback_checks_ub to intrinsic_fallback_is_spec

--- src/tools/miri/tests/fail/intrinsic_fallback_is_spec.rs
+++ src/tools/miri/tests/fail/intrinsic_fallback_is_spec.rs
@@ -10,5 +10,5 @@ pub const fn ptr_guaranteed_cmp<T>(ptr: *const T, other: *const T) -> u8 {
 
 fn main() {
     ptr_guaranteed_cmp::<()>(std::ptr::null(), std::ptr::null());
-    //~^ ERROR: can only use intrinsic fallback bodies that check UB.
+    //~^ ERROR: can only use intrinsic fallback bodies that exactly reflect the specification
 }
