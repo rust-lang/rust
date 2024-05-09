c90b6b8d2937cc116c635aa5c58f90afcc535248
Skgland <bennet.blessmann+github@googlemail.com>
2024-05-09 15:41:15 +0200

    stabilize `const_int_from_str`

--- library/core/tests/lib.rs
+++ library/core/tests/lib.rs
@@ -16,7 +16,6 @@
 #![feature(const_hash)]
 #![feature(const_heap)]
 #![feature(const_intrinsic_copy)]
-#![feature(const_int_from_str)]
 #![feature(const_maybe_uninit_as_mut_ptr)]
 #![feature(const_nonnull_new)]
 #![feature(const_pointer_is_aligned)]
