1f5837ae2506d5439b31195b7fcf784b9ee90d2b
Oneirical <manchot@videotron.ca>
2024-05-11 17:20:31 -0400

    rewrite c-link-to-rust-staticlib

---
+++ tests/run-make/c-link-to-rust-staticlib/rmake.rs
@@ -0,0 +1,20 @@
+// This test checks that C linking with Rust does not encounter any errors, with a static library.
+// See https://github.com/rust-lang/rust/issues/10434
+
+//@ ignore-cross-compile
+
+use run_make_support::{cc, extra_c_flags, extra_cxx_flags, run, rustc, static_lib};
+use std::fs;
+
+fn main() {
+    rustc().input("foo.rs").run();
+    cc().input("bar.c")
+        .input(static_lib("foo"))
+        .out_exe("bar")
+        .args(&extra_c_flags())
+        .args(&extra_cxx_flags())
+        .run();
+    run("bar");
+    fs::remove_file(static_lib("foo"));
+    run("bar");
+}
