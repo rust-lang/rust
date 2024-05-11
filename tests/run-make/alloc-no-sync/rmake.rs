50539da261b885d1d829f77484ef689435fd571c
Oneirical <manchot@videotron.ca>
2024-05-11 15:35:49 -0400

    rewrite alloc tests & remove import

---
+++ tests/run-make/alloc-no-sync/rmake.rs
@@ -0,0 +1,14 @@
+// This test checks that alloc can still compile correctly when the unstable no_sync feature is turned on.
+// See https://github.com/rust-lang/rust/pull/84266
+
+use run_make_support::rustc;
+
+fn main() {
+    rustc()
+        .edition("2021")
+        .arg("-Dwarnings")
+        .crate_type("rlib")
+        .input("../../../library/alloc/src/lib.rs")
+        .cfg("no_sync")
+        .run();
+}
