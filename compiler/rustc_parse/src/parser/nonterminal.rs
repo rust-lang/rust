3986ea0ea555ab6de8af46932da94c26aa1c28ee
Eric Holk <eric@theincredibleholk.org>
2024-05-17 11:27:18 -0700

    Update compiler/rustc_parse/src/parser/nonterminal.rs
    Co-authored-by: León Orell Valerian Liehr <me@fmease.dev>

--- compiler/rustc_parse/src/parser/nonterminal.rs
+++ compiler/rustc_parse/src/parser/nonterminal.rs
@@ -3,7 +3,6 @@ use rustc_ast::token::{self, Delimiter, Nonterminal, Nonterminal::*, Nonterminal
 use rustc_ast::HasTokens;
 use rustc_ast_pretty::pprust;
 use rustc_errors::PResult;
-use rustc_span::edition::Edition;
 use rustc_span::symbol::{kw, Ident};
 
 use crate::errors::UnexpectedNonterminal;
@@ -49,7 +48,7 @@ impl<'a> Parser<'a> {
                 token.can_begin_expr()
                 // This exception is here for backwards compatibility.
                 && !token.is_keyword(kw::Let)
-                && (token.span.edition() >= Edition::Edition2024 || !token.is_keyword(kw::Const))
+                && (token.span.edition().at_least_rust_2024() || !token.is_keyword(kw::Const))
             }
             NonterminalKind::Ty => token.can_begin_type(),
             NonterminalKind::Ident => get_macro_ident(token).is_some(),
