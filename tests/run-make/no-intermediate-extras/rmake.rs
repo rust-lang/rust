f0b8da4430971ce1a34848852849b8b4e3f20778
Oneirical <manchot@videotron.ca>
2024-05-17 13:29:43 -0400

    rewrite no-intermediate-extras

---
+++ tests/run-make/no-intermediate-extras/rmake.rs
@@ -0,0 +1,21 @@
+// When using the --test flag with an rlib, this used to generate
+// an unwanted .bc file, which should not exist. This test checks
+// that the bug causing the generation of this file has not returned.
+// See https://github.com/rust-lang/rust/issues/10973
+
+//@ ignore-cross-compile
+
+use run_make_support::{rustc, tmp_dir};
+
+fn main() {
+    rustc().crate_type("rlib").arg("--test").input("foo.rs").run(); 
+    match fs::remove_file(tmp_dir().join("foo.bc")) {
+        Ok(_) => {
+            println!("An unwanted .bc file was created by run-make/no-intermediate-extras.");
+            std::process::exit(0);
+        },
+        Err(e) => {
+            std::process::exit(1);
+        }
+    }
+}
