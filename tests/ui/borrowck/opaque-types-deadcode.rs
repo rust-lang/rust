1e7db2cd198dbd94e8842c6571a4cf64a92fef19
Santiago Pastorino <spastorino@gmail.com>
2024-05-19 11:08:21 -0300

    Add tests for unchecked region constraints for opaque types in dead code

---
+++ tests/ui/borrowck/opaque-types-deadcode.rs
@@ -0,0 +1,32 @@
+//@ compile-flags:-Zverbose-internals
+
+#![feature(rustc_attrs)]
+#![rustc_hidden_type_of_opaques]
+
+trait CallMeMaybe<'a, 'b> {
+    fn mk() -> Self;
+    fn subtype<T>(self, x: &'b T) -> &'a T;
+}
+
+struct Foo<'a, 'b: 'a>(&'a (), &'b ());
+impl<'a, 'b> CallMeMaybe<'a, 'b> for Foo<'a, 'b> {
+    fn mk() -> Self {
+        Foo(&(), &())
+    }
+
+    fn subtype<T>(self, x: &'b T) -> &'a T {
+        x
+    }
+}
+
+fn good_bye() -> ! {
+    panic!();
+}
+
+fn foo<'a, 'b: 'a>() -> impl CallMeMaybe<'a, 'b> {
+    //~^ ERROR: Foo<'{erased}, '{erased}>
+    good_bye();
+    Foo(&(), &())
+}
+
+fn main() {}
