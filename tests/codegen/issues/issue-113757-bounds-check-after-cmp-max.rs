f37c576dbd350adf0644ca9a2b83e28d9d57a076
Lzu Tao <taolzu@gmail.com>
2024-05-20 18:18:52 +0000

    add codegen test for #113757

---
+++ tests/codegen/issues/issue-113757-bounds-check-after-cmp-max.rs
@@ -0,0 +1,19 @@
+// in Rust 1.73, -O and opt-level=3 optimizes differently
+//@ compile-flags: -C opt-level=3
+//@ min-llvm-version: 17
+#![crate_type = "lib"]
+
+use std::cmp::max;
+
+#[no_mangle]
+// CHECK-LABEL: @foo
+// CHECK-NOT: slice_start_index_len_fail
+// CHECK-NOT: unreachable
+pub fn foo(v: &mut Vec<u8>, size: usize)-> Option<&mut [u8]> {
+    if v.len() > max(1, size) {
+        let start = v.len() - size;
+        Some(&mut v[start..])
+    } else {
+        None
+    }
+}
