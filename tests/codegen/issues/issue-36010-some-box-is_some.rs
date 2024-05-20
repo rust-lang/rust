95740fbfeddbc3e09a0a94cde4c3694cd5af17db
Lzu Tao <taolzu@gmail.com>
2024-05-20 21:50:31 +0000

    add codegen test for #36010

---
+++ tests/codegen/issues/issue-36010-some-box-is_some.rs
@@ -0,0 +1,29 @@
+#![crate_type = "lib"]
+
+//@ compile-flags: -O
+
+use std::mem;
+
+fn foo<T>(a: &mut T, b: T) -> bool {
+    let b = Some(mem::replace(a, b));
+    let ret = b.is_some();
+    mem::forget(b);
+    return ret
+}
+
+// CHECK-LABEL: @foo_u32
+// CHECK: store i32
+// CHECK-NEXT: ret i1
+#[no_mangle]
+pub fn foo_u32(a: &mut u32, b: u32) -> bool {
+    foo(a, b)
+}
+
+// CHECK-LABEL: @foo_box
+// CHECK: store ptr
+// CHECK-NEXT: ret i1
+#[no_mangle]
+pub fn foo_box(a: &mut Box<u32>, b: Box<u32>) -> bool {
+    foo(a, b)
+}
+
