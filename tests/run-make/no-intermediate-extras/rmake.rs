95c47d3396c324b32ffaedc2681a370107aeb6be
Oneirical <manchot@videotron.ca>
2024-05-20 21:30:48 -0400

    simplify

--- tests/run-make/no-intermediate-extras/rmake.rs
+++ tests/run-make/no-intermediate-extras/rmake.rs
@@ -10,13 +10,8 @@ use std::fs;
 
 fn main() {
     rustc().crate_type("rlib").arg("--test").input("foo.rs").run();
-    match fs::remove_file(tmp_dir().join("foo.bc")) {
-        Ok(_) => {
-            println!("An unwanted .bc file was created by run-make/no-intermediate-extras.");
-            std::process::exit(1);
-        }
-        Err(e) => {
-            std::process::exit(0);
-        }
-    }
+    assert!(
+        fs::remove_file(tmp_dir().join("foo.bc")).is_err(),
+        "An unwanted .bc file was created by run-make/no-intermediate-extras."
+    );
 }
