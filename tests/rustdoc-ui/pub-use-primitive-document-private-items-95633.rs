b5923a95a886588fd40fccd69f1ccca504234194
Michael Howell <michael@notriddle.com>
2024-05-21 21:21:26 -0700

    Move tests into appropriate subdirectories

--- tests/rustdoc-ui/pub-use-primitive-document-private-items-95633.rs
+++ tests/rustdoc-ui/pub-use-primitive-document-private-items-95633.rs
@@ -1,3 +1,4 @@
+//@ check-pass
 //@ compile-flags: --document-private-items
 
 // This ensures that no ICE is triggered when rustdoc is run on this code.
--- tests/rustdoc/inline_cross/reexport-with-anonymous-lifetime-98697.rs
+++ tests/rustdoc/inline_cross/reexport-with-anonymous-lifetime-98697.rs
@@ -1,4 +1,4 @@
-//@ aux-build:issue-98697-reexport-with-anonymous-lifetime.rs
+//@ aux-build:reexport-with-anonymous-lifetime-98697.rs
 //@ ignore-cross-compile
 #![crate_name = "foo"]
 
@@ -7,12 +7,12 @@
 //
 // https://github.com/rust-lang/rust/issues/98697
 
-extern crate issue_98697_reexport_with_anonymous_lifetime;
+extern crate reexport_with_anonymous_lifetime_98697;
 
 // @has foo/fn.repro.html '//pre[@class="rust item-decl"]/code' 'fn repro<F>()where F: Fn(&str)'
 // @!has foo/fn.repro.html '//pre[@class="rust item-decl"]/code' 'for<'
-pub use issue_98697_reexport_with_anonymous_lifetime::repro;
+pub use reexport_with_anonymous_lifetime_98697::repro;
 
 // @has foo/struct.Extra.html '//div[@id="trait-implementations-list"]//h3[@class="code-header"]' 'impl MyTrait<&Extra> for Extra'
 // @!has foo/struct.Extra.html '//div[@id="trait-implementations-list"]//h3[@class="code-header"]' 'impl<'
-pub use issue_98697_reexport_with_anonymous_lifetime::Extra;
+pub use reexport_with_anonymous_lifetime_98697::Extra;
