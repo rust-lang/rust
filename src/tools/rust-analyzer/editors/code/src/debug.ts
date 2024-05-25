09677b03ddbe2f1357a03d0f96c991e930df9176
Jonas Rinke <delphi1024@gmail.com>
2024-05-25 17:08:17 +0200

    Formatting

--- src/tools/rust-analyzer/editors/code/src/debug.ts
+++ src/tools/rust-analyzer/editors/code/src/debug.ts
@@ -194,8 +194,8 @@ function getCCppDebugConfig(
         args: runnable.args.executableArgs,
         cwd: runnable.args.cwd || runnable.args.workspaceRoot || ".",
         sourceFileMap,
-        environment: Object.entries(env).map(entry => {
-            return { "name": entry[0], "value": entry[1] };
+        environment: Object.entries(env).map((entry) => {
+            return { name: entry[0], value: entry[1] };
         }),
         // See https://github.com/rust-lang/rust-analyzer/issues/16901#issuecomment-2024486941
         osx: {
