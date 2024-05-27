ad5dce55e620db3f992421debafe66194e5210c9
Chris Denton <chris@chrisdenton.dev>
2024-05-27 13:46:41 +0000

    Move run-make windows_subsystem tests to ui tests

--- tests/run-make/windows-subsystem/rmake.rs
+++
@@ -1,8 +0,0 @@
-//@ ignore-cross-compile
-
-use run_make_support::rustc;
-
-fn main() {
-    rustc().input("windows.rs").run();
-    rustc().input("console.rs").run();
-}
--- tests/ui/windows-subsystem/console.rs
+++ tests/ui/windows-subsystem/console.rs
@@ -1,3 +1,4 @@
+//@ run-pass
 #![windows_subsystem = "console"]
 
 fn main() {}
