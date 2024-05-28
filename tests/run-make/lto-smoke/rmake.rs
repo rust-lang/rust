d9d013bec077600b8b0dc25e51191802d1e11cd9
Oneirical <manchot@videotron.ca>
2024-05-27 20:57:01 -0400

    rewrite lto-smoke to rmake

---
+++ tests/run-make/lto-smoke/rmake.rs
@@ -0,0 +1,13 @@
+// A simple smoke test to check that link time optimization
+// (LTO) works as intended, with its various flags turned on.
+// See https://github.com/rust-lang/rust/issues/10741
+
+//@ ignore-cross-compile
+
+fn main() {
+    let lto_flags = ["-Clto", "-Clto=yes", "-Clto=off", "-Clto=thin", "-Clto=fat"];
+    for flag in lto_flags {
+        rustc().input(lib.rs).run();
+        rustc().input(main.rs).arg(flag).run();
+    }
+}
