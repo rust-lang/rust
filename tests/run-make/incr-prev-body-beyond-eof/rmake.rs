59e2074c79aaea17ed1b4e16081983e5f03c6bd2
Oneirical <manchot@videotron.ca>
2024-05-28 15:54:09 -0400

    rewrite suspicious-library in rmake

--- tests/run-make/incr-prev-body-beyond-eof/rmake.rs
+++ tests/run-make/incr-prev-body-beyond-eof/rmake.rs
@@ -7,26 +7,21 @@
 // was hashed by rustc in addition to the span length, and the fix still
 // works.
 
-// FIXME: Ignore flags temporarily disabled for the test.
-// ignore-none
-// ignore-nvptx64-nvidia-cuda
+//@ ignore-none
+// reason: no-std is not supported
 
-use run_make_support::{rustc, target, tmp_dir};
+//@ ignore-nvptx64-nvidia-cuda
+// FIXME: can't find crate for `std`
+
+use run_make_support::{rustc, tmp_dir};
 use std::fs;
 
 fn main() {
-    fs::create_dir(tmp_dir().join("src"));
-    fs::create_dir(tmp_dir().join("incr"));
-    fs::copy("a.rs", tmp_dir().join("main.rs"));
-    rustc()
-        .incremental(tmp_dir().join("incr"))
-        .input(tmp_dir().join("src/main.rs"))
-        .target(target())
-        .run();
-    fs::copy("b.rs", tmp_dir().join("main.rs"));
-    rustc()
-        .incremental(tmp_dir().join("incr"))
-        .input(tmp_dir().join("src/main.rs"))
-        .target(target())
-        .run();
+    // FIXME(Oneirical): Use run_make_support::fs_wrapper here.
+    fs::create_dir(tmp_dir().join("src")).unwrap();
+    fs::create_dir(tmp_dir().join("incr")).unwrap();
+    fs::copy("a.rs", tmp_dir().join("src/main.rs")).unwrap();
+    rustc().incremental(tmp_dir().join("incr")).input(tmp_dir().join("src/main.rs")).run();
+    fs::copy("b.rs", tmp_dir().join("src/main.rs")).unwrap();
+    rustc().incremental(tmp_dir().join("incr")).input(tmp_dir().join("src/main.rs")).run();
 }
