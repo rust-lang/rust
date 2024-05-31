45da03541c0a07f63f1b8c15cccd2fc8692cca7e
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-05-31 09:51:10 +0000

    More tests

---
+++ tests/ui/impl-trait/unsized_coercion3.rs
@@ -0,0 +1,24 @@
+//! This test checks that opaque types get unsized instead of
+//! constraining their hidden type to a trait object.
+
+//@ revisions: next old
+//@[next] compile-flags: -Znext-solver
+
+trait Trait {}
+
+impl Trait for u32 {}
+
+fn hello() -> Box<impl Trait + ?Sized> {
+    if true {
+        let x = hello();
+        //[next]~^ ERROR: type mismatch resolving `impl Trait + ?Sized <: dyn Send`
+        let y: Box<dyn Send> = x;
+        //[old]~^ ERROR: the size for values of type `impl Trait + ?Sized` cannot be know
+        //[old]~| ERROR: cannot check whether the hidden type of opaque type satisfies auto traits
+    }
+    Box::new(1u32)
+    //[next]~^ ERROR: mismatched types
+    //[next]~| ERROR: the size for values of type `impl Trait + ?Sized` cannot be know
+}
+
+fn main() {}
