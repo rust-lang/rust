8c1777750bce6329a3ce1268c0f6f49b7842f7d2
Adrian Taylor <adetaylor@chromium.org>
2024-05-31 11:03:37 +0000

    Remove duplicative test.
    I think that this test is supposed to be a treereduced version of
    122903-1.rs, but is actually identical.

--- tests/crashes/122903-2.rs
+++
@@ -1,9 +0,0 @@
-//@ known-bug: #122903
-
-impl Struct {
-    async fn box_box_ref_Struct(
-        self: Box<Box<Self, impl FnMut(&mut Box<Box<Self, impl FnMut(&mut Self)>>)>>
-    ) -> &u32 {
-        f
-    }
-}
