2e11cb1228899bca167f0d0cce99a08be9c5da66
Jubilee Young <workingjubilee@gmail.com>
2024-05-31 23:16:26 -0700

    Treat extern static and static mut the same

--- compiler/rustc_mir_build/src/check_unsafety.rs
+++ compiler/rustc_mir_build/src/check_unsafety.rs
@@ -464,8 +464,8 @@ impl<'a, 'tcx> Visitor<'a, 'tcx> for UnsafetyVisitor<'a, 'tcx> {
                 if let ExprKind::StaticRef { def_id, .. } | ExprKind::ThreadLocalRef(def_id) =
                     self.thir[arg].kind
                 {
-                    if self.tcx.is_mutable_static(def_id) && allow_implicit_static_deref {
-                        // we're only taking the address of the implicit place expr, it's fine
+                    if allow_implicit_static_deref {
+                        // it's fine to take the addr of a place expression
                     } else if self.tcx.is_mutable_static(def_id) {
                         self.requires_unsafe(expr.span, UseOfMutableStatic);
                     } else if self.tcx.is_foreign_item(def_id) {
