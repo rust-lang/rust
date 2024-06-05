3a6b606ad9e6c9efc83bd9e7ab0025080d052bc0
Boxy <supbscripter@gmail.com>
2024-06-05 22:39:42 +0100

    remove const arg windows debug info tests

--- tests/debuginfo/function-names.rs
+++ tests/debuginfo/function-names.rs
@@ -76,9 +76,6 @@
 // Const generic parameter
 // cdb-command:x a!function_names::const_generic_fn*
 // cdb-check:[...] a!function_names::const_generic_fn_bool<false> (void)
-// cdb-check:[...] a!function_names::const_generic_fn_non_int<CONST$a70c39591cb5f53d> (void)
-// cdb-check:[...] a!function_names::const_generic_fn_unsigned_int<14> (void)
-// cdb-check:[...] a!function_names::const_generic_fn_signed_int<-7> (void)
 
 #![allow(unused_variables)]
 #![feature(omit_gdb_pretty_printer_section)]
