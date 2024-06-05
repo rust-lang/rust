dc91ad05e374c0f5de7f669e5003c2ab9ceaaf46
Zalathar <Zalathar@users.noreply.github.com>
2024-06-05 17:31:01 +1000

    Port `tests/run-make-fulldeps/obtain-borrowck` to ui-fulldeps

--- tests/ui-fulldeps/obtain-borrowck.rs
+++ tests/ui-fulldeps/obtain-borrowck.rs
@@ -1,3 +1,10 @@
+//@ edition: 2021
+//@ run-pass
+//@ check-run-results
+//@ run-flags: --sysroot {{sysroot-base}} --edition=2021 {{src-base}}/auxiliary/obtain-borrowck-input.rs
+//@ ignore-stage1 (requires matching sysroot built with in-tree compiler)
+// ignore-tidy-linelength
+
 #![feature(rustc_private)]
 
 //! This program implements a rustc driver that retrieves MIR bodies with
