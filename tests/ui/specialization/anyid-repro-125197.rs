4b188d9d667dfcc7ba4caf95e56cbb3a6697f292
Michael Goulet <michael@errs.io>
2024-06-07 15:58:05 -0400

    Only compute specializes query if specialization is enabled in the crate of the specialized impl

--- tests/ui/specialization/anyid-repro-125197.rs
+++ tests/ui/specialization/anyid-repro-125197.rs
@@ -5,7 +5,7 @@
 // actually participate in specialization. Since <https://github.com/rust-lang/rust/pull/122791>,
 // we don't treat inductive cycles as errors -- so we may need to winnow more pairs of impls, and
 // we try to winnow impls in favor of other impls. However, if we're *inside* the `specializes`
-// query, then may have a query cycle if we call `specializes` again! 
+// query, then may have a query cycle if we call `specializes` again!
 
 extern crate anyid_repro_125197;
 use anyid_repro_125197::AnyId;
