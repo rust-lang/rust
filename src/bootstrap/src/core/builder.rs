e61d3b8372f988ba8b6573deac2290d59d72b76f
onur-ozkan <work@onurozkan.dev>
2024-06-08 12:48:19 +0300

    fix `Builder::doc_rust_lang_org_channel`
    Previously, we were unable to use `rust.download-rustc` with the beta or stable
    channel settings through `rust.channel` due to breaking rustdoc UI tests.
    This was because when using a precompiled nightly compiler from CI, we must use the
    channel of precompiled compiler and ignore `rust.channel` from the configuration.
    
    This change addresses that issue in `Builder::doc_rust_lang_org_channel` and allows rustdoc
    UI tests to work with the precompiled compiler even if the channel specified in config.toml is
    "beta" or "stable".
    
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/bootstrap/src/core/builder.rs
+++ src/bootstrap/src/core/builder.rs
@@ -1038,14 +1038,26 @@ impl<'a> Builder<'a> {
     }
 
     pub fn doc_rust_lang_org_channel(&self) -> String {
-        let channel = match &*self.config.channel {
-            "stable" => &self.version,
-            "beta" => "beta",
-            "nightly" | "dev" => "nightly",
-            // custom build of rustdoc maybe? link to the latest stable docs just in case
-            _ => "stable",
+        // When using precompiled compiler from CI, we need to use CI rustc's channel and
+        // ignore `rust.channel` from the configuration. Otherwise most of the rustdoc tests
+        // will fail due to incompatible `DOC_RUST_LANG_ORG_CHANNEL`.
+        let channel = if let Some(commit) = self.config.download_rustc_commit() {
+            self.config
+                .read_file_by_commit(&PathBuf::from("src/ci/channel"), commit)
+                .trim()
+                .to_owned()
+        } else {
+            match &*self.config.channel {
+                "stable" => &self.version,
+                "beta" => "beta",
+                "nightly" | "dev" => "nightly",
+                // custom build of rustdoc maybe? link to the latest stable docs just in case
+                _ => "stable",
+            }
+            .to_owned()
         };
-        "https://doc.rust-lang.org/".to_owned() + channel
+
+        format!("https://doc.rust-lang.org/{channel}")
     }
 
     fn run_step_descriptions(&self, v: &[StepDescription], paths: &[PathBuf]) {
