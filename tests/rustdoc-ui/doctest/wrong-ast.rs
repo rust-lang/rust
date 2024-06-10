2fd8d1c1cee3bce5c088cd5537ec293f04d34e15
Guillaume Gomez <guillaume.gomez@huawei.com>
2024-06-10 22:58:24 +0200

    Add/update `rustdoc-ui` tests to check new merged doctests

--- tests/rustdoc-ui/doctest/wrong-ast.rs
+++ tests/rustdoc-ui/doctest/wrong-ast.rs
@@ -1,7 +1,7 @@
 //@ compile-flags:--test --test-args=--test-threads=1
 //@ normalize-stdout-test: "tests/rustdoc-ui/doctest" -> "$$DIR"
 //@ normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
-//@ normalize-stdout-test "wrong-ast.rs:\d+:\d+" -> "wrong-ast.rs:$$LINE:$$COL"
+//@ normalize-stdout-test ".rs:\d+:\d+" -> ".rs:$$LINE:$$COL"
 //@ failure-status: 101
 
 /// ```
