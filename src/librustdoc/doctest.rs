3f899ad8c95a641c85aa59b58bbc2169541cd94e
Guillaume Gomez <guillaume.gomez@huawei.com>
2024-06-11 00:01:59 +0200

    Simplify `has_main_fn` to be a boolean instead of a `Option<Span>`

--- src/librustdoc/doctest.rs
+++ src/librustdoc/doctest.rs
@@ -536,13 +536,17 @@ fn run_test(
         compiler.arg("--error-format=short");
         let input_file =
             doctest.test_opts.outdir.path().join(&format!("doctest_{}.rs", doctest.edition));
+            eprintln!("OUUUUUUUT>>>>>>> {input_file:?}");
         if std::fs::write(&input_file, &doctest.full_test_code).is_err() {
             // If we cannot write this file for any reason, we leave. All combined tests will be
             // tested as standalone tests.
             return Err(TestFailure::CompileError);
         }
         compiler.arg(input_file);
-        compiler.stderr(Stdio::null());
+        // compiler.stderr(Stdio::null());
+        let mut buffer = String::new();
+        eprintln!("Press ENTER");
+        let _ = std::io::stdin().read_line(&mut buffer);
     } else {
         compiler.arg("-");
         compiler.stdin(Stdio::piped());
@@ -764,7 +768,7 @@ struct CreateRunnableDoctests {
 
 impl CreateRunnableDoctests {
     fn new(rustdoc_options: RustdocOptions, opts: GlobalTestOptions) -> CreateRunnableDoctests {
-        let can_merge_doctests = rustdoc_options.edition >= Edition::Edition2024;
+        let can_merge_doctests = true;//rustdoc_options.edition >= Edition::Edition2024;
         CreateRunnableDoctests {
             standalone_tests: Vec::new(),
             mergeable_tests: FxHashMap::default(),
