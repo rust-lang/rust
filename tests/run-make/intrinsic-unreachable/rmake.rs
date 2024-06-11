2752a374c759f70dd879989b88f8b891e14d646b
Oneirical <manchot@videotron.ca>
2024-06-11 14:18:27 -0400

    rewrite intrinsic-unreachable to rmake

---
+++ tests/run-make/intrinsic-unreachable/rmake.rs
@@ -0,0 +1,19 @@
+// intrinsics::unreachable tells the compiler that a certain point in the code
+// is not reachable by any means, which enables some useful optimizations.
+// In this test, exit-unreachable contains this instruction and exit-ret does not,
+// which means the emitted artifacts should be shorter in length.
+// See https://github.com/rust-lang/rust/pull/16970
+
+//@ needs-asm-support
+//@ ignore-windows-msvc
+// Reason: Because of Windows exception handling, the code is not necessarily any shorter.
+
+use run_make_support::rustc;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() {
+    rustc().opt().emit("asm").input("exit-ret.rs").run();
+    rustc().opt().emit("asm").input("exit-unreachable.rs").run();
+    assert!(BufReader::new(File::open("exit-unreachable.s")).lines().count() < BufReader::new(File::open("exit-ret.s")).lines().count());
+}
