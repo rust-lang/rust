e3b3ac6e360836396635b128df87588a4b724e9b
Lzu Tao <taolzu@gmail.com>
2024-06-13 21:31:24 +0000

    cache propagated const

--- compiler/rustc_mir_transform/src/gvn.rs
+++ compiler/rustc_mir_transform/src/gvn.rs
@@ -1367,6 +1367,10 @@ impl<'tcx> VnState<'_, 'tcx> {
         assert!(!value.may_have_provenance(self.tcx, op.layout.size));
 
         let const_ = Const::Val(value, op.layout.ty);
+        // cache the propagated const
+        if let Some(new_index) = self.insert_constant(const_) {
+            self.values.swap_indices(index.as_usize(), new_index.as_usize());
+        }
         Some(ConstOperand { span: DUMMY_SP, user_ty: None, const_ })
     }
 
