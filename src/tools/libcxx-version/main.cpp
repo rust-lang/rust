8e8f867bdfe20a6cdc4b18aa775575f5dc6fc8ae
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-14 10:40:06 +0200

    Optimize libcxx-version for faster bootstrap startups

--- src/tools/libcxx-version/main.cpp
+++ src/tools/libcxx-version/main.cpp
@@ -5,22 +5,25 @@
 // It's nothing more than specifying the name of the standard library implementation (either libstdc++ or libc++)
 // and its major version.
 
-#include <iostream>
+#include <cstdio>
 
 int main() {
     #ifdef _GLIBCXX_RELEASE
-        std::cout << "libstdc++ version: " << _GLIBCXX_RELEASE << std::endl;
+        #define name "libstdc++"
+        #define version _GLIBCXX_RELEASE
     #elif defined(_LIBCPP_VERSION)
         // _LIBCPP_VERSION follows "XXYYZZ" format (e.g., 170001 for 17.0.1).
         // ref: https://github.com/llvm/llvm-project/blob/f64732195c1030ee2627ff4e4142038e01df1d26/libcxx/include/__config#L51-L54
         //
         // Since we use the major version from _GLIBCXX_RELEASE, we need to extract only the first 2 characters of _LIBCPP_VERSION
         // to provide the major version for consistency.
-        std::cout << "libc++ version: " << std::to_string(_LIBCPP_VERSION).substr(0, 2) << std::endl;
+        #define name "libc++"
+        #define version _LIBCPP_VERSION / 10000
     #else
-        std::cerr << "Coudln't recognize the standard library version." << std::endl;
-        return 1;
+        #error "Couldn't recognize the standard library version."
     #endif
+
+    printf("%s version: %d\n", name, version);
 
     return 0;
 }
