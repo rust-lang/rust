41dcdc030cd62f5e433a42fc1acb3ad41b529e90
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-06-14 09:20:05 +0000

    Add regression test

---
+++ tests/ui/statics/const_generics.rs
@@ -0,0 +1,22 @@
+//! check that we lose the information that `BAR` points to `FOO`
+//! when going through a const generic.
+
+//@ run-pass
+// With optimizations, LLVM will deduplicate the constant `X` whose
+// value is `&42` to just be a reference to the static. This is correct,
+// but obscures the issue we're trying to show.
+//@ compile-flags: -Copt-level=0
+
+#![feature(const_refs_to_static)]
+#![feature(adt_const_params)]
+#![allow(incomplete_features)]
+
+static FOO: usize = 42;
+const BAR: &usize = &FOO;
+fn foo<const X: &'static usize>() {
+    assert!(!std::ptr::eq(X, &FOO));
+}
+
+fn main() {
+    foo::<BAR>();
+}
