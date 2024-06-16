cf9510cd3382fb1a5b4902a1b0e68af3e3274931
joboet <jonasboettiger@icloud.com>
2024-06-16 13:14:01 +0200

    std: move `sys_common::backtrace` to `sys`

--- library/std/src/thread/mod.rs
+++ library/std/src/thread/mod.rs
@@ -539,7 +539,7 @@ impl Builder {
             let f = f.into_inner();
             set_current(their_thread);
             let try_result = panic::catch_unwind(panic::AssertUnwindSafe(|| {
-                crate::sys_common::backtrace::__rust_begin_short_backtrace(f)
+                crate::sys::backtrace::__rust_begin_short_backtrace(f)
             }));
             // SAFETY: `their_packet` as been built just above and moved by the
             // closure (it is an Arc<...>) and `my_packet` will be stored in the
