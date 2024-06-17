78998f3fea3aba636e50aa0a6abcafa51d66eff7
Oneirical <manchot@videotron.ca>
2024-06-17 16:11:14 -0400

    rewrite allocator-shim-circular-deps to ui test

--- tests/run-make/separate-link/rmake.rs
+++ tests/run-make/separate-link/rmake.rs
@@ -1,5 +1,5 @@
 // The compiler flags no-link (and by extension, link-only) used to be broken
-// due to changes in encoding/decoding. This was patched, and this test ensures
+// due to changes in encoding/decoding. This was patched, and this test checks
 // that these flags are not broken again, resulting in successful compilation.
 // See https://github.com/rust-lang/rust/issues/77857
 
