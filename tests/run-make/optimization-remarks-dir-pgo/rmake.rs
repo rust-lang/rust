e340e7b85d2a66897dd13aebc7b663582eb1c125
Oneirical <manchot@videotron.ca>
2024-06-19 14:30:47 -0400

    rewrite optimization-remarks-dir to rmake

--- tests/run-make/optimization-remarks-dir-pgo/rmake.rs
+++ tests/run-make/optimization-remarks-dir-pgo/rmake.rs
@@ -7,13 +7,23 @@
 //@ needs-profiler-support
 //@ ignore-cross-compile
 
-use run_make_support::{run, llvm_profdata, rustc, invalid_utf8_contains};
+use run_make_support::{invalid_utf8_contains, llvm_profdata, run, rustc};
 
 fn main() {
     rustc().profile_generate("profdata").opt().input("foo.rs").output("foo").run();
     run("foo");
-    llvm_profdata().merge().output("merged.profdata").input("profdata/default_15907418011457399462_0.profraw").run();
-    rustc().profile_use("merged.profdata").opt().input("foo.rs").arg("-Cremark=all").arg("-Zremark-dir=profiles").run();
+    llvm_profdata()
+        .merge()
+        .output("merged.profdata")
+        .input("profdata/default_15907418011457399462_0.profraw")
+        .run();
+    rustc()
+        .profile_use("merged.profdata")
+        .opt()
+        .input("foo.rs")
+        .arg("-Cremark=all")
+        .arg("-Zremark-dir=profiles")
+        .run();
     // Check that PGO hotness is included in the remark files
     invalid_utf8_contains("profiles/foo.cba44757bc0621b9-cgu.0.opt.opt.yaml", "Hotness");
 }
