84d9b67dabd195c21fd210150facd2a8eccac908
Guillaume Gomez <guillaume.gomez@huawei.com>
2024-06-19 15:54:08 +0200

    Add 2024 edition doctests to cover corner cases

---
+++ tests/rustdoc-ui/2024-doctests-checks.rs
@@ -0,0 +1,41 @@
+//@ check-pass
+//@ compile-flags: --test --test-args=--test-threads=1 -Zunstable-options --edition 2024
+//@ normalize-stdout-test: "tests/rustdoc-ui" -> "$$DIR"
+//@ normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
+//@ normalize-stdout-test "wrong-ast.rs:\d+:\d+" -> "wrong-ast.rs:$$LINE:$$COL"
+
+/// This one should fail: crate attributes should remain crate attributes
+/// in standalone doctests.
+///
+/// ```compile_fail
+/// #![deny(missing_docs)]
+///
+/// pub struct Bar;
+/// ```
+///
+/// This one should not impact the other merged doctests.
+///
+/// ```
+/// #![deny(unused)]
+/// ```
+///
+/// ```
+/// let x = 12;
+/// ```
+///
+/// This one should not be a merged doctest (because of `$crate`):
+///
+/// ```
+/// macro_rules! bla {
+///     () => {{
+///         $crate::foo();
+///     }}
+/// }
+///
+/// fn foo() {}
+///
+/// fn main() {
+///     bla!();
+/// }
+/// ```
+pub struct Foo;
