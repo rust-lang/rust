866832d94111c4aeca69b67d4eade5797e9be512
zhuyunxing <zhuyunxing.zyx@alibaba-inc.com>
2024-06-19 17:49:10 +0800

    coverage. Disable supporting mcdc on llvm-18

--- tests/coverage/mcdc/condition-limit.rs
+++ tests/coverage/mcdc/condition-limit.rs
@@ -1,7 +1,6 @@
 #![feature(coverage_attribute)]
 //@ edition: 2021
-//@ min-llvm-version: 18
-//@ ignore-llvm-version: 19 - 99
+//@ min-llvm-version: 19
 //@ compile-flags: -Zcoverage-options=mcdc
 //@ llvm-cov-flags: --show-branches=count --show-mcdc
 
