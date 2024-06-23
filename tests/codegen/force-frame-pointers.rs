c72a6ad8f2653d8da38a28520b6c5a2d64aa7aa5
Jubilee Young <workingjubilee@gmail.com>
2024-06-22 21:36:11 -0700

    test: ignore force-frame-pointers test on some targets
    all of these currently force stronger frame pointers, and
    currently the CLI does not override the target

--- tests/codegen/force-frame-pointers.rs
+++ tests/codegen/force-frame-pointers.rs
@@ -3,6 +3,13 @@
 //@ [NonLeaf] compile-flags: -Cforce-frame-pointers=non-leaf
 //@ compile-flags: -Zunstable-options
 //@ compile-flags: -C no-prepopulate-passes -Copt-level=0
+//@ [NonLeaf] ignore-illumos
+//@ [NonLeaf] ignore-openbsd
+//@ [NonLeaf] ignore-x86
+//@ [NonLeaf] ignore-x86_64-apple-darwin
+//@ [NonLeaf] ignore-windows-gnu
+//@ [NonLeaf] ignore-thumb
+// result is platform-dependent based on platform's frame pointer settings
 
 #![crate_type = "lib"]
 
