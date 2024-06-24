39bf1dcce5b0e80fbdbcde30e618821f5c9b0bf0
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2024-06-24 17:57:58 +0300

    Small fixme in core now that split_first has no codegen issues

--- library/core/src/num/dec2flt/parse.rs
+++ library/core/src/num/dec2flt/parse.rs
@@ -51,9 +51,7 @@ fn try_parse_19digits(s_ref: &mut &[u8], x: &mut u64) {
     let mut s = *s_ref;
 
     while *x < MIN_19DIGIT_INT {
-        // FIXME: Can't use s.split_first() here yet,
-        // see https://github.com/rust-lang/rust/issues/109328
-        if let [c, s_next @ ..] = s {
+        if let Some((c, s_next)) = s.split_first() {
             let digit = c.wrapping_sub(b'0');
 
             if digit < 10 {
