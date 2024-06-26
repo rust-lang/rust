d44732120cfd6f49d49bc333ad60c0539aedee19
Oneirical <manchot@videotron.ca>
2024-06-26 16:21:58 -0400

    rewrite symbols-include-type-name to rmake

--- tests/run-make/cdylib-fewer-symbols/rmake.rs
+++ tests/run-make/cdylib-fewer-symbols/rmake.rs
@@ -4,20 +4,18 @@
 // four such symbols are successfully hidden.
 // See https://github.com/rust-lang/rust/pull/45710
 
-//FIXME(Oneirical): try it on windows, restore ignore
-// See https://github.com/rust-lang/rust/pull/46207#issuecomment-347561753
-//FIXME(Oneirical): I also removed cross-compile ignore since there is no binary execution
+//@ ignore-cross-compile
+// Reason: The __rust_ symbol appears during cross-compilation.
 
 use run_make_support::{dynamic_lib_name, llvm_readobj, rustc};
 
 fn main() {
     // Compile a cdylib
     rustc().input("foo.rs").run();
-    let out = llvm_readobj().arg("--symbols").input(dynamic_lib_name("foo")).run().stdout_utf8();
-    let out = // All hidden symbols must be removed.
-        out.lines().filter(|&line| !line.trim().contains("HIDDEN")).collect::<Vec<_>>().join("\n");
-    assert!(!&out.contains("__rdl_"));
-    assert!(!&out.contains("__rde_"));
-    assert!(!&out.contains("__rg_"));
-    assert!(!&out.contains("__ruse_"));
+    let out =
+        llvm_readobj().arg("--dyn-symbols").input(dynamic_lib_name("foo")).run().stdout_utf8();
+    assert!(!&out.contains("__rdl_"), "{out}");
+    assert!(!&out.contains("__rde_"), "{out}");
+    assert!(!&out.contains("__rg_"), "{out}");
+    assert!(!&out.contains("__rust_"), "{out}");
 }
