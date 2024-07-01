4f67d2613e15801890a40631a29aa161540e7aa0
surechen <chenshuo17@huawei.com>
2024-07-01 12:57:10 +0800

    Make E0599's label more clear for field which is used like a method.
    fixes #127178

--- tests/ui/imports/unnamed_pub_trait.rs
+++ tests/ui/imports/unnamed_pub_trait.rs
@@ -12,5 +12,5 @@ fn main() {
     use unnamed_pub_trait_source::S;
     S.method();
     //~^ ERROR no method named `method` found for struct `S` in the current scope [E0599]
-    //~| HELP items from traits can only be used if the trait is in scope
+    //~| HELP items from traits can only be used if the trait is implemented and in scope
 }
