4c0cbaeb9e799362dd081ff8514f75e23ab8c2f5
Nicholas Nethercote <n.nethercote@gmail.com>
2024-07-04 10:16:51 +1000

    Remove `TokenStream::flattened` and `InvisibleOrigin::FlattenToken`.
    They are no longer needed.
    
    This does slightly worsen the error message for a single test, but that
    test contains code that is so badly broken that I'm not worried about
    it.

--- compiler/rustc_ast/src/token.rs
+++ compiler/rustc_ast/src/token.rs
@@ -30,10 +30,6 @@ pub enum InvisibleOrigin {
     // Converted from `proc_macro::Delimiter` in
     // `proc_macro::Delimiter::to_internal`, i.e. returned by a proc macro.
     ProcMacro,
-
-    // Converted from `TokenKind::NtLifetime` in `TokenStream::flatten_token`.
-    // Treated similarly to `ProcMacro`.
-    FlattenToken,
 }
 
 impl PartialEq for InvisibleOrigin {
@@ -130,9 +126,7 @@ impl Delimiter {
         match self {
             Delimiter::Parenthesis | Delimiter::Bracket | Delimiter::Brace => false,
             Delimiter::Invisible(InvisibleOrigin::MetaVar(_)) => false,
-            Delimiter::Invisible(InvisibleOrigin::FlattenToken | InvisibleOrigin::ProcMacro) => {
-                true
-            }
+            Delimiter::Invisible(InvisibleOrigin::ProcMacro) => true,
         }
     }
 
