2f0368c902e912242e86e49f140c666c712b79c7
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2024-07-05 08:28:09 +0000

    Remove `StructuredDiag`

--- compiler/rustc_hir_analysis/src/structured_errors.rs
+++ compiler/rustc_hir_analysis/src/structured_errors.rs
@@ -1,32 +1,3 @@
 mod wrong_number_of_generic_args;
 
 pub use self::wrong_number_of_generic_args::*;
-
-use rustc_errors::{Diag, ErrCode};
-use rustc_session::Session;
-
-pub trait StructuredDiag<'tcx> {
-    fn session(&self) -> &Session;
-
-    fn code(&self) -> ErrCode;
-
-    fn diagnostic(&self) -> Diag<'tcx> {
-        let err = self.diagnostic_common();
-
-        if self.session().teach(self.code()) {
-            self.diagnostic_extended(err)
-        } else {
-            self.diagnostic_regular(err)
-        }
-    }
-
-    fn diagnostic_common(&self) -> Diag<'tcx>;
-
-    fn diagnostic_regular(&self, err: Diag<'tcx>) -> Diag<'tcx> {
-        err
-    }
-
-    fn diagnostic_extended(&self, err: Diag<'tcx>) -> Diag<'tcx> {
-        err
-    }
-}
