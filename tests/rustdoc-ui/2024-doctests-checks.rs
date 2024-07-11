903d2db4d22e947354385ed2fff42d0816143cfc
Guillaume Gomez <guillaume.gomez@huawei.com>
2024-07-11 23:37:55 +0200

    Only keep "useful" code in `tests/rustdoc-ui/2024-doctests-checks.rs`

--- tests/rustdoc-ui/2024-doctests-checks.rs
+++ tests/rustdoc-ui/2024-doctests-checks.rs
@@ -4,26 +4,12 @@
 //@ normalize-stdout-test "finished in \d+\.\d+s" -> "finished in $$TIME"
 //@ normalize-stdout-test ".rs:\d+:\d+" -> ".rs:$$LINE:$$COL"
 
-/// This one should fail: crate attributes should remain crate attributes
-/// in standalone doctests.
-///
-/// ```compile_fail
-/// #![deny(missing_docs)]
-///
-/// pub struct Bar;
-/// ```
-///
-/// This one should not impact the other merged doctests.
-///
-/// ```
-/// #![deny(unused)]
-/// ```
-///
 /// ```
 /// let x = 12;
 /// ```
 ///
-/// This one should not be a merged doctest (because of `$crate`):
+/// This one should not be a merged doctest (because of `$crate`). The output
+/// will confirm it by displaying both merged and standalone doctest passes.
 ///
 /// ```
 /// macro_rules! bla {
