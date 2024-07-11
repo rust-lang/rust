01a266206b4c923a8fbbc49b3be7d630e48eef86
Oneirical <manchot@videotron.ca>
2024-07-11 16:00:15 -0400

    rewrite and rename issue-69368 to rmake

--- tests/run-make/link-eh-frame-terminator/rmake.rs
+++ tests/run-make/link-eh-frame-terminator/rmake.rs
@@ -4,17 +4,20 @@
 // after the fix in #85395.
 // See https://github.com/rust-lang/rust/issues/47551
 
-//FIXME(Oneirical): See if it works on anything other than only linux and 64 bit
-// maybe riscv64gc-unknown-linux-gnu
+//@ only-linux
+// Reason: the ZERO terminator is unique to the Linux architecture.
+//@ ignore-32bit
+// Reason: the usage of a large array in the test causes an out-of-memory
+// error on 32 bit systems.
 
-use run_make_support::{llvm_objdump, run, rustc};
+use run_make_support::{bin_name, llvm_objdump, run, rustc};
 
 fn main() {
     rustc().input("eh_frame-terminator.rs").run();
     run("eh_frame-terminator").assert_stdout_contains("1122334455667788");
     llvm_objdump()
         .arg("--dwarf=frames")
-        .input("eh_frame-terminator")
+        .input(bin_name("eh_frame-terminator"))
         .run()
         .assert_stdout_contains("ZERO terminator");
 }
