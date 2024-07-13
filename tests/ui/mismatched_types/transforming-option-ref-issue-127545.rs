9c1a9e03d5e001543a21eaff5d616de8cb4e2220
Lzu Tao <taolzu@gmail.com>
2024-07-13 00:54:00 +0000

    add test for Result<&T, _> where T; Deref

--- tests/ui/mismatched_types/transforming-option-ref-issue-127545.rs
+++ tests/ui/mismatched_types/transforming-option-ref-issue-127545.rs
@@ -12,3 +12,7 @@ pub fn bar(arg: Option<&Vec<i32>>) -> &[i32] {
 pub fn barzz<'a>(arg: Option<&'a Vec<i32>>, v: &'a [i32]) -> &'a [i32] {
     arg.unwrap_or(v) //~ ERROR 13:19: 13:20: mismatched types [E0308]
 }
+
+pub fn convert_result(arg: Result<&Vec<i32>, ()>) -> &[i32] {
+    arg.unwrap_or(&[]) //~ ERROR 17:19: 17:22: mismatched types [E0308]
+}
