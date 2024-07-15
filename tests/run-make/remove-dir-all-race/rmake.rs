e7ccf0b273a5b78be97c609871ba5d9178783ee2
binarycat <binarycat@envs.net>
2024-07-15 11:55:07 -0400

    run autoformatter

--- tests/run-make/remove-dir-all-race/rmake.rs
+++ tests/run-make/remove-dir-all-race/rmake.rs
@@ -2,9 +2,9 @@ use run_make_support::{
     fs_wrapper::{create_dir, write},
     run_in_tmpdir,
 };
+use std::fs::remove_dir_all;
 use std::thread;
 use std::time::Duration;
-use std::fs::remove_dir_all;
 
 fn main() {
     run_in_tmpdir(|| {
@@ -19,19 +19,19 @@ fn main() {
             });
 
             let t2 = thread::spawn(move || {
-				let _ = remove_dir_all("outer/inner");
-				let _ = remove_dir_all("outer/inner.txt");
-			});
+                let _ = remove_dir_all("outer/inner");
+                let _ = remove_dir_all("outer/inner.txt");
+            });
 
             t1.join().unwrap();
             t2.join().unwrap();
 
-			// trying to remove the top-level directory should
-			// still result in an error
-			let Err(err) = remove_dir_all("outer") else {
-				panic!("removing nonexistant dir did not result in an error");
-			};
-			assert_eq!(err.kind(), std::io::ErrorKind::NotFound);
+            // trying to remove the top-level directory should
+            // still result in an error
+            let Err(err) = remove_dir_all("outer") else {
+                panic!("removing nonexistant dir did not result in an error");
+            };
+            assert_eq!(err.kind(), std::io::ErrorKind::NotFound);
         }
     })
 }
