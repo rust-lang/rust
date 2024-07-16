3ba62f0a63c6b0092b6fec3bbc8877045073d827
Oneirical <manchot@videotron.ca>
2024-07-16 14:12:12 -0400

    rewrite unstable-flag-required to rmake

--- tests/run-make/missing-crate-dependency/rmake.rs
+++ tests/run-make/missing-crate-dependency/rmake.rs
@@ -3,12 +3,12 @@
 // in a dependency chain.
 // See https://github.com/rust-lang/rust/issues/12146
 
-use run_make_support::{fs_wrapper, rust_lib_name, rustc};
+use run_make_support::{rfs, rust_lib_name, rustc};
 
 fn main() {
     rustc().crate_type("rlib").input("crateA.rs").run();
     rustc().crate_type("rlib").input("crateB.rs").run();
-    fs_wrapper::remove_file(rust_lib_name("crateA"));
+    rfs::remove_file(rust_lib_name("crateA"));
     // Ensure that crateC fails to compile, as the crateA dependency is missing.
     rustc()
         .input("crateC.rs")
