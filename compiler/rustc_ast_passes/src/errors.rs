2f5a84ea16f232f8a0709ea567bca0cfd90b44cf
Michael Goulet <michael@errs.io>
2024-07-18 18:02:09 -0400

    Don't allow unsafe statics outside of extern blocks

--- compiler/rustc_ast_passes/src/errors.rs
+++ compiler/rustc_ast_passes/src/errors.rs
@@ -225,6 +225,13 @@ pub struct InvalidSafetyOnBareFn {
 }
 
 #[derive(Diagnostic)]
+#[diag(ast_passes_unsafe_static)]
+pub struct UnsafeStatic {
+    #[primary_span]
+    pub span: Span,
+}
+
+#[derive(Diagnostic)]
 #[diag(ast_passes_bound_in_context)]
 pub struct BoundInContext<'a> {
     #[primary_span]
