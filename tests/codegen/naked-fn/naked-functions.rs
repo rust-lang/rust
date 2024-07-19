9185e3cec07f07db06340733e9cf0e44d1988282
Folkert <folkert@folkertdev.nl>
2024-07-20 01:16:49 +0200

    test the new global asm output of naked functions

--- tests/codegen/naked-fn/naked-functions.rs
+++ tests/codegen/naked-fn/naked-functions.rs
@@ -4,26 +4,39 @@
 
 #![crate_type = "lib"]
 #![feature(naked_functions)]
-use std::arch::asm;
+use std::arch::{asm, global_asm};
+// CHECK: module asm ".intel_syntax"
+// CHECK: .pushsection .text.naked_empty,\22ax\22, @progbits
+// CHECK: .balign 4"
+// CHECK: .globl naked_empty"
+// CHECK: .hidden naked_empty"
+// CHECK: .type naked_empty, @function"
+// CHECK-LABEL: naked_empty:
+// CHECK: ret
+// CHECK: .popsection
+// CHECK: .att_syntax
 
-// CHECK: Function Attrs: naked
-// CHECK-NEXT: define{{.*}}void @naked_empty()
 #[no_mangle]
 #[naked]
 pub unsafe extern "C" fn naked_empty() {
-    // CHECK-NEXT: {{.+}}:
-    // CHECK-NEXT: call void asm
-    // CHECK-NEXT: unreachable
     asm!("ret", options(noreturn));
 }
 
-// CHECK: Function Attrs: naked
-// CHECK-NEXT: define{{.*}}i{{[0-9]+}} @naked_with_args_and_return(i64 %0, i64 %1)
+// CHECK: .intel_syntax
+// CHECK: .pushsection .text.naked_with_args_and_return,\22ax\22, @progbits
+// CHECK: .balign 4
+// CHECK: .globl naked_with_args_and_return
+// CHECK: .hidden naked_with_args_and_return
+// CHECK: .type naked_with_args_and_return, @function
+// CHECK-LABEL: naked_with_args_and_return:
+// CHECK: lea rax, [rdi + rsi]
+// CHECK: ret
+// CHECK: .size naked_with_args_and_return, . - naked_with_args_and_return
+// CHECK: .popsection
+// CHECK: .att_syntax
+
 #[no_mangle]
 #[naked]
 pub unsafe extern "C" fn naked_with_args_and_return(a: isize, b: isize) -> isize {
-    // CHECK-NEXT: {{.+}}:
-    // CHECK-NEXT: call void asm
-    // CHECK-NEXT: unreachable
     asm!("lea rax, [rdi + rsi]", "ret", options(noreturn));
 }
