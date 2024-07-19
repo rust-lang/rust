633f41de0903efb830753e2e373ac9666230eb54
León Orell Valerian Liehr <me@fmease.dev>
2024-07-19 16:50:47 +0200

    Update all rustdoc-json/ tests to use the new style

--- tests/rustdoc-json/reexport/rename_private.rs
+++ tests/rustdoc-json/reexport/rename_private.rs
@@ -1,10 +1,10 @@
 //@ edition:2018
 
-// @!has "$.index[*][?(@.name=='inner')]"
+//@ !has "$.index[*][?(@.name=='inner')]"
 mod inner {
-    // @has "$.index[*][?(@.name=='Public')]"
+    //@ has "$.index[*][?(@.name=='Public')]"
     pub struct Public;
 }
 
-// @is "$.index[*][?(@.inner.import)].inner.import.name" \"NewName\"
+//@ is "$.index[*][?(@.inner.import)].inner.import.name" \"NewName\"
 pub use inner::Public as NewName;
