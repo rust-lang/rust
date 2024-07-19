9a5e41c56fbd61c5af4626cbd03935115cfaaa38
yukang <moorekang@gmail.com>
2024-07-19 14:55:27 +0800

    add testcase for 127868

---
+++ tests/ui/parser/mismatched-delimiter-corner-case-issue-127868.rs
@@ -0,0 +1,7 @@
+// ignore-tidy-trailing-newlines
+// issue: rust-lang/rust#127868
+
+fn main() {
+    let a = [[[[[[[[[[[[[[[[[[[[1, {, (, [,;
+} //~ ERROR mismatched closing delimiter: `}`
+//~ ERROR this file contains an unclosed delimiter
