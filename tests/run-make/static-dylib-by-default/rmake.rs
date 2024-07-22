62fb491028229d94ecfc356771baec6f15936125
Oneirical <manchot@videotron.ca>
2024-07-22 16:34:32 -0400

    rewrite sanitizer-staticlib-link to rmake

--- tests/run-make/static-dylib-by-default/rmake.rs
+++ tests/run-make/static-dylib-by-default/rmake.rs
@@ -7,19 +7,29 @@
 
 //@ ignore-cross-compile
 // Reason: the compiled binary is executed
-//FIXME(Oneirical): try on msvc, not because of an ignore but because i did wonky things
 
 use run_make_support::{
-    cc, cwd, dynamic_lib_name, extra_c_flags, has_extension, rfs, run, rustc, shallow_find_files,
+    cc, cwd, dynamic_lib_name, extra_c_flags, has_extension, is_msvc, rfs, run, rustc,
+    shallow_find_files,
 };
 
 fn main() {
     rustc().input("foo.rs").run();
     rustc().input("bar.rs").run();
-    cc().input("main.c").out_exe("main").arg(dynamic_lib_name("bar")).args(extra_c_flags()).run();
+    // On msvc, dynamic libraries are compiled by rustc to:
+    // bar.dll     // dylib
+    // bar.dll.lib // import library for the dylib
+    // bar.dll.exp // export library for the dylib
+    // msvc's underlying link.exe requires the import library for the dynamic library as input.
+    // That is why the library is bar.dll.lib, not bar.dll.
+    let library = if is_msvc() { "bar.dll.lib" } else { &dynamic_lib_name("bar") };
+    cc().input("main.c").out_exe("main").arg(library).args(extra_c_flags()).run();
     for rlib in shallow_find_files(cwd(), |path| has_extension(path, "rlib")) {
         rfs::remove_file(rlib);
     }
     rfs::remove_file(dynamic_lib_name("foo"));
+    if is_msvc() {
+        rfs::remove_file("foo.dll.lib");
+    }
     run("main");
 }
