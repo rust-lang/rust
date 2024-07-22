4d834143ccf49f8d2b853f1a64e7ddadf3a16dd6
Michael Howell <michael@notriddle.com>
2024-07-22 12:59:34 -0700

    rustdoc: show code spans as `<code>` in TOC

--- tests/rustdoc/sidebar/top-toc-html.rs
+++ tests/rustdoc/sidebar/top-toc-html.rs
@@ -4,7 +4,7 @@
 #![feature(lazy_type_alias)]
 #![allow(incomplete_features)]
 
-//! # Basic [link](https://example.com) and *emphasis*
+//! # Basic [link](https://example.com) and *emphasis* and `code`
 //!
 //! This test case covers TOC entries with rich text inside.
 //! Rustdoc normally supports headers with links, but for the
@@ -12,9 +12,12 @@
 //!
 //! For consistency, emphasis is also filtered out.
 
-// @has foo/index.html
+//@ has foo/index.html
 // User header
-// @has - '//section[@id="TOC"]/h3' 'Sections'
-// @has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis"]' 'Basic link and emphasis'
-// @count - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis"]/em' 0
-// @count - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis"]/a' 0
+//@ has - '//section[@id="TOC"]/h3' 'Sections'
+//@ has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]/@title' 'Basic link and emphasis and `code`'
+//@ has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]' 'Basic link and emphasis and code'
+//@ count - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]/em' 0
+//@ count - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]/a' 0
+//@ count - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]/code' 1
+//@ has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#basic-link-and-emphasis-and-code"]/code' 'code'
--- tests/rustdoc/sidebar/top-toc-idmap.rs
+++ tests/rustdoc/sidebar/top-toc-idmap.rs
@@ -14,13 +14,13 @@
 //! in the `top-doc`, and the one that's not in the `top-doc`
 //! needs to match the one that isn't in the `top-toc`.
 
-// @has foo/index.html
+//@ has foo/index.html
 // User header
-// @has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#structs"]' 'Structs'
-// @has - '//details[@class="toggle top-doc"]/div[@class="docblock"]/h2[@id="structs"]' 'Structs'
+//@ has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#structs"]' 'Structs'
+//@ has - '//details[@class="toggle top-doc"]/div[@class="docblock"]/h2[@id="structs"]' 'Structs'
 // Built-in header
-// @has - '//section[@id="TOC"]/ul[@class="block"]/li/a[@href="#structs-1"]' 'Structs'
-// @has - '//section[@id="main-content"]/h2[@id="structs-1"]' 'Structs'
+//@ has - '//section[@id="TOC"]/ul[@class="block"]/li/a[@href="#structs-1"]' 'Structs'
+//@ has - '//section[@id="main-content"]/h2[@id="structs-1"]' 'Structs'
 
 /// # Fields
 /// ## Fields
@@ -29,15 +29,15 @@
 /// The difference between struct-like headers and module-like headers
 /// is strange, but not actually a problem as long as we're consistent.
 
-// @has foo/struct.MyStruct.html
+//@ has foo/struct.MyStruct.html
 // User header
-// @has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#fields-1"]' 'Fields'
-// @has - '//details[@class="toggle top-doc"]/div[@class="docblock"]/h2[@id="fields-1"]' 'Fields'
+//@ has - '//section[@id="TOC"]/ul[@class="block top-toc"]/li/a[@href="#fields-1"]' 'Fields'
+//@ has - '//details[@class="toggle top-doc"]/div[@class="docblock"]/h2[@id="fields-1"]' 'Fields'
 // Only one level of nesting
-// @count - '//section[@id="TOC"]/ul[@class="block top-toc"]//a' 2
+//@ count - '//section[@id="TOC"]/ul[@class="block top-toc"]//a' 2
 // Built-in header
-// @has - '//section[@id="TOC"]/h3/a[@href="#fields"]' 'Fields'
-// @has - '//section[@id="main-content"]/h2[@id="fields"]' 'Fields'
+//@ has - '//section[@id="TOC"]/h3/a[@href="#fields"]' 'Fields'
+//@ has - '//section[@id="main-content"]/h2[@id="fields"]' 'Fields'
 
 pub struct MyStruct {
     pub fields: i32,
