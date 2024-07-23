af6d49d97c6d787f11563b6e3bf80ae5411eda6d
Alex Macleod <alex@macleod.io>
2024-07-23 16:15:28 +0000

    Fix running compile-test under cargo nextest

--- tests/compile-test.rs
+++ tests/compile-test.rs
@@ -268,15 +268,6 @@ fn run_ui_cargo() {
 }
 
 fn main() {
-    // Support being run by cargo nextest - https://nexte.st/book/custom-test-harnesses.html
-    if env::args().any(|arg| arg == "--list") {
-        if !env::args().any(|arg| arg == "--ignored") {
-            println!("compile_test: test");
-        }
-
-        return;
-    }
-
     set_var("CLIPPY_DISABLE_DOCS_LINKS", "true");
     // The SPEEDTEST_* env variables can be used to check Clippy's performance on your PR. It runs the
     // affected test 1000 times and gets the average.
