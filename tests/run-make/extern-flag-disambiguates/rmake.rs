62dff66a6c2b13a962743b62122a2945766485ed
binarycat <binarycat@envs.net>
2024-07-23 18:38:42 -0400

    use shorter link and remove entries wrongly added in rebase

--- tests/run-make/extern-flag-disambiguates/rmake.rs
+++ tests/run-make/extern-flag-disambiguates/rmake.rs
@@ -14,7 +14,7 @@ use run_make_support::{cwd, run, rustc};
 //
 // Note that A.1 and A.2 are crates with the same name.
 
-// original Makefile at https://github.com/rust-lang/rust/commit/cc3c8bbfaf5af19caf3deb131a995a65ca4674f9#diff-23d3f6bcee0c974ae7c793ab6f87c454cffd1f5c9ec17ce86aaf2cf8bc6e5397
+// original Makefile at https://github.com/rust-lang/rust/issues/14469
 
 fn main() {
     rustc().metadata("1").extra_filename("-1").input("a.rs").run();
