28e0907111075dead53a931004b0ba6f1fe0d793
Ralf Jung <post@ralfj.de>
2024-07-24 20:55:43 +0200

    nontemporal_store: make sure that the intrinsic is truly just a hint

--- tests/codegen/intrinsics/nontemporal.rs
+++ tests/codegen/intrinsics/nontemporal.rs
@@ -1,13 +1,28 @@
 //@ compile-flags: -O
+//@ compile-flags: --target aarch64-unknown-linux-gnu
+//@ needs-llvm-components: aarch64
 
-#![feature(core_intrinsics)]
+#![feature(no_core, lang_items, intrinsics)]
+#![no_core]
 #![crate_type = "lib"]
+
+#[lang = "sized"]
+pub trait Sized {}
+#[lang = "copy"]
+pub trait Copy {}
+
+impl Copy for u32 {}
+impl<T> Copy for *mut T {}
+
+extern "rust-intrinsic" {
+    pub fn nontemporal_store<T>(ptr: *mut T, val: T);
+}
 
 #[no_mangle]
 pub fn a(a: &mut u32, b: u32) {
     // CHECK-LABEL: define{{.*}}void @a
     // CHECK: store i32 %b, ptr %a, align 4, !nontemporal
     unsafe {
-        std::intrinsics::nontemporal_store(a, b);
+        nontemporal_store(a, b);
     }
 }
