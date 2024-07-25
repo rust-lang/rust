8859da0bf2f1f3d447ce1939647e88a0302afb99
Folkert <folkert@folkertdev.nl>
2024-07-26 00:20:59 +0200

    add `run-rustfix` test for machine-applicable suggestion

---
+++ tests/ui/asm/unsupported-option.rs
@@ -0,0 +1,10 @@
+//@ run-rustfix
+
+use std::arch::global_asm;
+
+fn main() {}
+
+global_asm!("", options(att_syntax, nomem, readonly, noreturn, raw));
+//~^ ERROR the `nomem` option cannot be used with `global_asm!`
+//~| ERROR the `readonly` option cannot be used with `global_asm!`
+//~| ERROR the `noreturn` option cannot be used with `global_asm!`
