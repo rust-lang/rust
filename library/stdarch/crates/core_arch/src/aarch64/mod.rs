92b6a3abaedf20e9fd1a8ebd89e506947d4b1840
dheaton-arm <Damian.Heaton@arm.com>
2024-07-25 14:08:56 +0100

    Add MTE intrinsics
    Adds intrinsic functions to `core_arch::aarch64` for MTE, as per the
    ACLE:
    - __arm_mte_create_random_tag
    - __arm_mte_increment_tag
    - __arm_mte_exclude_tag
    - __arm_mte_set_tag
    - __arm_mte_get_tag
    - __arm_mte_ptrdiff
    
    These are unavailable without the `mte` target feature.

--- library/stdarch/crates/core_arch/src/aarch64/mod.rs
+++ library/stdarch/crates/core_arch/src/aarch64/mod.rs
@@ -6,6 +6,10 @@
 //! [arm_ref]: http://infocenter.arm.com/help/topic/com.arm.doc.ihi0073a/IHI0073A_arm_neon_intrinsics_ref.pdf
 //! [arm_dat]: https://developer.arm.com/technologies/neon/intrinsics
 
+mod mte;
+#[unstable(feature = "stdarch_aarch64_mte", issue = "129010")]
+pub use self::mte::*;
+
 // NEON intrinsics are currently broken on big-endian, so don't expose them. (#1484)
 #[cfg(target_endian = "little")]
 mod neon;
