3e4ddc39b8c47895b9086908ca4e9577a2c28142
Folkert <folkert@folkertdev.nl>
2024-07-27 19:08:00 +0200

    update aarch64 asm tests

--- tests/ui/asm/unsupported-option.rs
+++ tests/ui/asm/unsupported-option.rs
@@ -4,7 +4,7 @@ use std::arch::global_asm;
 
 fn main() {}
 
-global_asm!("", options(att_syntax, nomem, readonly, noreturn, raw));
+global_asm!("", options(nomem, readonly, noreturn, raw));
 //~^ ERROR the `nomem` option cannot be used with `global_asm!`
 //~| ERROR the `readonly` option cannot be used with `global_asm!`
 //~| ERROR the `noreturn` option cannot be used with `global_asm!`
