5d685d7650717d96f9cb7589c427bb632aae68ac
binarycat <binarycat@envs.net>
2024-08-02 11:46:38 -0400

    document new behavior in doc comment

--- library/std/src/fs.rs
+++ library/std/src/fs.rs
@@ -2491,6 +2491,8 @@ pub fn remove_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
 ///
 /// Consider ignoring the error if validating the removal is not required for your use case.
 ///
+/// [`io::ErrorKind::NotFound`] is only returned if no removal occurs.
+///
 /// [`fs::remove_file`]: remove_file
 /// [`fs::remove_dir`]: remove_dir
 ///
