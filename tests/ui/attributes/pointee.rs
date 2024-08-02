4058d49e39de3d04e6bfca4882e163dc4a5b3b05
Kaloyan Gangov <gangov1@gmail.com>
2024-08-03 00:03:45 +0300

    adds tests

---
+++ tests/ui/attributes/pointee.rs
@@ -0,0 +1,34 @@
+#![feature(pointee)]
+#![feature(stmt_expr_attributes)]
+#![deny(unused_attributes)]
+#![allow(dead_code)]
+
+fn invalid() {
+    #[pointee] //~ ERROR attribute should be applied to a function definition
+    {
+        1
+    };
+}
+
+#[pointee] //~ ERROR attribute should be applied to a function definition
+type InvalidTy = ();
+
+#[pointee] //~ ERROR attribute should be applied to a function definition
+mod invalid_module {}
+
+fn main() {
+    let _ = #[pointee] //~ ERROR attribute should be applied to a function definition
+    (|| 1);
+}
+
+#[pointee] //~ ERROR attribute should be applied to a function definition
+struct F;
+
+#[pointee] //~ ERROR attribute should be applied to a function definition
+impl F {
+    #[pointee]
+    fn valid(&self) {}
+}
+
+#[pointee]
+fn valid() {}
