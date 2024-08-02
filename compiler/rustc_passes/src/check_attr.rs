04e2511ef99e9a5312b5ced6fd8995d50a4f5e40
Kaloyan Gangov <gangov1@gmail.com>
2024-08-03 00:18:11 +0300

    adds a missing `:`

--- compiler/rustc_passes/src/check_attr.rs
+++ compiler/rustc_passes/src/check_attr.rs
@@ -158,7 +158,7 @@ impl<'tcx> CheckAttrVisitor<'tcx> {
                     self.check_rustc_std_internal_symbol(attr, span, target)
                 }
                 [sym::naked] => self.check_naked(hir_id, attr, span, target, attrs),
-                [sym:pointee] => self.check_pointee(hir_id, attr, span, target, attrs),
+                [sym::pointee] => self.check_pointee(hir_id, attr, span, target, attrs),
                 [sym::rustc_never_returns_null_ptr] => {
                     self.check_applied_to_fn_or_method(hir_id, attr, span, target)
                 }
