89d804b34b3168c746a4d0a45ab53c0687366313
Folkert <folkert@folkertdev.nl>
2024-08-04 16:45:48 +0200

    use `naked_asm!` in naked-function tests

--- tests/ui/feature-gates/feature-gate-naked_functions.rs
+++ tests/ui/feature-gates/feature-gate-naked_functions.rs
@@ -5,14 +5,14 @@ use std::arch::asm;
 #[naked]
 //~^ the `#[naked]` attribute is an experimental feature
 extern "C" fn naked() {
-    asm!("", options(noreturn))
+    naked_asm!("", options(noreturn))
     //~^ ERROR: requires unsafe
 }
 
 #[naked]
 //~^ the `#[naked]` attribute is an experimental feature
 extern "C" fn naked_2() -> isize {
-    asm!("", options(noreturn))
+    naked_asm!("", options(noreturn))
     //~^ ERROR: requires unsafe
 }
 
