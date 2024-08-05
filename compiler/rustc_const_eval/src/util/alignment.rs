f81549c9ca465e87ab6902dee8cdb4989ee50077
Michael Goulet <michael@errs.io>
2024-08-05 16:30:00 -0400

    Normalize struct tail properly in disalignment check

--- compiler/rustc_const_eval/src/util/alignment.rs
+++ compiler/rustc_const_eval/src/util/alignment.rs
@@ -22,7 +22,7 @@ where
     };
 
     let ty = place.ty(local_decls, tcx).ty;
-    let unsized_tail = || tcx.struct_tail_with_normalize(ty, |ty| ty, || {});
+    let unsized_tail = || tcx.struct_tail_erasing_lifetimes(ty, param_env);
     match tcx.layout_of(param_env.and(ty)) {
         Ok(layout)
             if layout.align.abi <= pack
