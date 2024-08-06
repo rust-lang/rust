764675e01aa4f32fd5a84e5f3b6a6e1ca8a1d31e
Veera <sveera.2001@gmail.com>
2024-08-06 14:06:29 -0400

    Add Tests

---
+++ tests/ui/inline-const/break-inside-inline-const-issue-128604.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let _ = ['a'; { break 2; 1 }];
+    //~^ ERROR `break` outside of a loop or labeled block
+    //~| HELP consider labeling this block to be able to break within it
+
+    const {
+        {
+            //~^ HELP consider labeling this block to be able to break within it
+            break;
+            //~^ ERROR `break` outside of a loop or labeled block
+        }
+    };
+
+    const {
+        break;
+        //~^ ERROR `break` outside of a loop or labeled block
+    };
+}
