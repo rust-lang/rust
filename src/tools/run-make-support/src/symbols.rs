c1d8892acf1d485e9b3bc63d09d987e9cd816d0e
binarycat <binarycat@envs.net>
2024-08-06 15:07:02 -0400

    fmt

--- src/tools/run-make-support/src/symbols.rs
+++ src/tools/run-make-support/src/symbols.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-use object::{self, Object, Symbol, ObjectSymbol, SymbolIterator};
+use object::{self, Object, ObjectSymbol, Symbol, SymbolIterator};
 
 /// Iterate through the symbols in an object file.
 ///
@@ -50,7 +50,11 @@ pub fn any_symbol_contains(path: impl AsRef<Path>, substrings: &[&str]) -> bool 
 /// The symbol names must match exactly.
 ///
 /// Panics if `path` is not a valid object file readable by the current user.
-pub fn missing_exact_symbols<'a>(path: impl AsRef<Path>, symbol_names: &[&'a str], pred: impl Fn(&Symbol<'_, '_>) -> bool) -> Vec<&'a str> {
+pub fn missing_exact_symbols<'a>(
+    path: impl AsRef<Path>,
+    symbol_names: &[&'a str],
+    pred: impl Fn(&Symbol<'_, '_>) -> bool,
+) -> Vec<&'a str> {
     let mut found = vec![false; symbol_names.len()];
     with_symbol_iter(path, |syms| {
         for sym in syms.filter(&pred) {
@@ -59,16 +63,19 @@ pub fn missing_exact_symbols<'a>(path: impl AsRef<Path>, symbol_names: &[&'a str
             }
         }
     });
-    return found.iter().enumerate()
-        .filter_map(|(i, found)| if !*found {
-            Some(symbol_names[i])
-        } else {
-            None
-        }).collect();
+    return found
+        .iter()
+        .enumerate()
+        .filter_map(|(i, found)| if !*found { Some(symbol_names[i]) } else { None })
+        .collect();
 }
 
 /// Assert that the symbol file contains all of the listed symbols and they all match the given predicate
-pub fn assert_contains_exact_symbols(path: impl AsRef<Path>, symbol_names: &[&str], pred: impl Fn(&Symbol<'_, '_>) -> bool) {
+pub fn assert_contains_exact_symbols(
+    path: impl AsRef<Path>,
+    symbol_names: &[&str],
+    pred: impl Fn(&Symbol<'_, '_>) -> bool,
+) {
     let missing = missing_exact_symbols(path.as_ref(), symbol_names, pred);
     if missing.len() > 0 {
         eprintln!("{} does not contain symbol(s): ", path.as_ref().display());
