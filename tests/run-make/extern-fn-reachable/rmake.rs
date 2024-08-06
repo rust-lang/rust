8663bbecd5e35042cf1a432098b793346e1844d7
binarycat <binarycat@envs.net>
2024-08-06 14:53:52 -0400

    refactor extern-fn-reachable to make sure symbols are exported

--- tests/run-make/extern-fn-reachable/rmake.rs
+++ tests/run-make/extern-fn-reachable/rmake.rs
@@ -1,8 +1,12 @@
 //@ ignore-cross-compile
 use run_make_support::rustc;
-use run_make_support::symbols::contains_exact_symbols;
-
+use run_make_support::symbols::assert_contains_exact_symbols;
+use run_make_support::object::ObjectSymbol;
 fn main() {
     rustc().input("dylib.rs").output("dylib.so").prefer_dynamic().run();
-    assert!(contains_exact_symbols("dylib.so", &["fun1", "fun2", "fun3", "fun4", "fun5"]));
+    assert_contains_exact_symbols(
+        "dylib.so",
+        &["fun1", "fun2", "fun3", "fun4", "fun5"],
+        |sym| dbg!(dbg!(sym).is_global()) && !dbg!(sym.is_undefined()),
+    );
 }
