e785219238e73f353f392afe30073180c5b2c5d6
Nicholas Nethercote <n.nethercote@gmail.com>
2024-08-08 17:18:20 +1000

    Shrink `TyKind::FnPtr`.
    By splitting the `FnSig` within `TyKind::FnPtr` into `FnSigTys` and
    `FnHeader`, which can be packed more efficiently. This reduces the size
    of the hot `TyKind` type from 32 bytes to 24 bytes on 64-bit platforms.
    This reduces peak memory usage by a few percent on some benchmarks. It
    also reduces cache misses and page faults similarly, though this doesn't
    translate to clear cycles or wall-time improvements on CI.

--- clippy_lints/src/multiple_unsafe_ops_per_block.rs
+++ clippy_lints/src/multiple_unsafe_ops_per_block.rs
@@ -130,7 +130,7 @@ fn collect_unsafe_exprs<'tcx>(
             ExprKind::Call(path_expr, _) => {
                 let sig = match *cx.typeck_results().expr_ty(path_expr).kind() {
                     ty::FnDef(id, _) => cx.tcx.fn_sig(id).skip_binder(),
-                    ty::FnPtr(sig) => sig,
+                    ty::FnPtr(sig_tys, hdr) => sig_tys.with(hdr),
                     _ => return Continue(Descend::Yes),
                 };
                 if sig.safety() == Safety::Unsafe {
