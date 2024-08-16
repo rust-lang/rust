c8e1cd9ced923157e95ef34dd8cf9406183e58c9
Chris Denton <chris@chrisdenton.dev>
2024-08-16 19:32:22 +0000

    WIP

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,6 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    r#""git+https://github.com/ChrisDenton/ar_archive_writer.git?branch=rename2#a5b424a6ef9d6416b299ac27c2dba224e2470253""#,
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
---
+++ tests/run-make/msvc-wholearchive/rmake.rs
@@ -0,0 +1,26 @@
+//@ only-msvc
+// Reason: this is testing the MSVC linker
+
+// This is a regression test for #129020
+// It ensures we can link a rust staticlib into DLL using the MSVC linker
+
+use run_make_support::run::cmd;
+use run_make_support::rustc;
+
+fn main() {
+    // FIXME: Make this test either work with or ignore LLVM.
+    if true {
+        return;
+    }
+    // Build the staticlib
+    rustc().crate_type("staticlib").input("static.rs").output("static.lib").run();
+    // Create an empty obj file (using the C compiler)
+    // Then use it to link in the staticlib using `/WHOLEARCHIVE` and produce a DLL.
+    // We call link.exe directly because this test is only for MSVC and not their LLVM equivalents
+    cmd("cl.exe").args(&["c.c", "-nologo", "-c", "-MT"]).run();
+    cmd("link.exe")
+        .args(&["c", "/WHOLEARCHIVE:./static.lib", "/dll", "/def:dll.def", "/out:dll.dll"])
+        // Import libs
+        .args(&["libcmt.lib", "kernel32.lib", "ws2_32.lib", "ntdll.lib", "userenv.lib"])
+        .run();
+}
