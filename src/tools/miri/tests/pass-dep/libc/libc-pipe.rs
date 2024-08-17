5d59bdefb44181c596917da27cfc6b8ff3b67505
Ralf Jung <post@ralfj.de>
2024-08-17 10:53:33 +0200

    test cleanup

--- src/tools/miri/tests/pass-dep/libc/libc-pipe.rs
+++ src/tools/miri/tests/pass-dep/libc/libc-pipe.rs
@@ -10,35 +10,31 @@ fn main() {
 
 fn test_pipe() {
     let mut fds = [-1, -1];
-    let mut res = unsafe { libc::pipe(fds.as_mut_ptr()) };
+    let res = unsafe { libc::pipe(fds.as_mut_ptr()) };
     assert_eq!(res, 0);
 
     // Read size == data available in buffer.
     let data = "12345".as_bytes().as_ptr();
-    res = unsafe { libc::write(fds[1], data as *const libc::c_void, 5).try_into().unwrap() };
+    let res = unsafe { libc::write(fds[1], data as *const libc::c_void, 5) };
     assert_eq!(res, 5);
     let mut buf3: [u8; 5] = [0; 5];
-    res = unsafe {
-        libc::read(fds[0], buf3.as_mut_ptr().cast(), buf3.len() as libc::size_t).try_into().unwrap()
-    };
+    let res = unsafe { libc::read(fds[0], buf3.as_mut_ptr().cast(), buf3.len() as libc::size_t) };
     assert_eq!(res, 5);
     assert_eq!(buf3, "12345".as_bytes());
 
     // Read size > data available in buffer.
     let data = "123".as_bytes().as_ptr();
-    res = unsafe { libc::write(fds[1], data as *const libc::c_void, 3).try_into().unwrap() };
+    let res = unsafe { libc::write(fds[1], data as *const libc::c_void, 3) };
     assert_eq!(res, 3);
     let mut buf4: [u8; 5] = [0; 5];
-    res = unsafe {
-        libc::read(fds[0], buf4.as_mut_ptr().cast(), buf4.len() as libc::size_t).try_into().unwrap()
-    };
+    let res = unsafe { libc::read(fds[0], buf4.as_mut_ptr().cast(), buf4.len() as libc::size_t) };
     assert_eq!(res, 3);
     assert_eq!(&buf4[0..3], "123".as_bytes());
 }
 
 fn test_pipe_threaded() {
     let mut fds = [-1, -1];
-    let mut res = unsafe { libc::pipe(fds.as_mut_ptr()) };
+    let res = unsafe { libc::pipe(fds.as_mut_ptr()) };
     assert_eq!(res, 0);
 
     let thread1 = thread::spawn(move || {
@@ -54,7 +50,7 @@ fn test_pipe_threaded() {
     // FIXME: we should yield here once blocking is implemented.
     //thread::yield_now();
     let data = "abcde".as_bytes().as_ptr();
-    res = unsafe { libc::write(fds[1], data as *const libc::c_void, 5).try_into().unwrap() };
+    let res = unsafe { libc::write(fds[1], data as *const libc::c_void, 5) };
     assert_eq!(res, 5);
     thread1.join().unwrap();
 
@@ -63,16 +59,13 @@ fn test_pipe_threaded() {
         // FIXME: we should yield here once blocking is implemented.
         //thread::yield_now();
         let data = "12345".as_bytes().as_ptr();
-        let res: i64 =
-            unsafe { libc::write(fds[1], data as *const libc::c_void, 5).try_into().unwrap() };
+        let res = unsafe { libc::write(fds[1], data as *const libc::c_void, 5) };
         assert_eq!(res, 5);
     });
     // FIXME: we should not yield here once blocking is implemented.
     thread::yield_now();
     let mut buf: [u8; 5] = [0; 5];
-    res = unsafe {
-        libc::read(fds[0], buf.as_mut_ptr().cast(), buf.len() as libc::size_t).try_into().unwrap()
-    };
+    let res = unsafe { libc::read(fds[0], buf.as_mut_ptr().cast(), buf.len() as libc::size_t) };
     assert_eq!(res, 5);
     assert_eq!(buf, "12345".as_bytes());
     thread2.join().unwrap();
@@ -81,7 +74,7 @@ fn test_pipe_threaded() {
 fn test_race() {
     static mut VAL: u8 = 0;
     let mut fds = [-1, -1];
-    let mut res = unsafe { libc::pipe(fds.as_mut_ptr()) };
+    let res = unsafe { libc::pipe(fds.as_mut_ptr()) };
     assert_eq!(res, 0);
     let thread1 = thread::spawn(move || {
         let mut buf: [u8; 1] = [0; 1];
@@ -99,7 +92,7 @@ fn test_race() {
     });
     unsafe { VAL = 1 };
     let data = "a".as_bytes().as_ptr();
-    res = unsafe { libc::write(fds[1], data as *const libc::c_void, 1).try_into().unwrap() };
+    let res = unsafe { libc::write(fds[1], data as *const libc::c_void, 1) };
     assert_eq!(res, 1);
     thread::yield_now();
     thread1.join().unwrap();
