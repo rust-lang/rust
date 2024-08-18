683ef6002b28091607bd33f8d9d76ab7dc5b42cc
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2024-08-18 21:00:40 +0300

    Bless a test for #70963

--- tests/ui/hygiene/panic-location.rs
+++ tests/ui/hygiene/panic-location.rs
@@ -4,8 +4,7 @@
 //@ normalize-stderr-test: ".rs:\d+:\d+" -> ".rs:LL:CC"
 //
 // Regression test for issue #70963
-// The captured stderr from this test reports a location
-// inside `VecDeque::with_capacity`, instead of `<::core::macros::panic macros>`
+// The reported panic location should not be `<::core::macros::panic macros>`.
 fn main() {
     std::collections::VecDeque::<String>::with_capacity(!0);
 }
