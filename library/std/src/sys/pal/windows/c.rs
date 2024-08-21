40481fc70a8626f9e58242b0fb673d90ade2f084
Geoffry Song <goffrie@gmail.com>
2024-08-21 15:34:51 -0700

    format

--- library/std/src/sys/pal/windows/c.rs
+++ library/std/src/sys/pal/windows/c.rs
@@ -112,16 +112,9 @@ if #[cfg(not(target_vendor = "uwp"))] {
 #[cfg(not(target_vendor = "win7"))]
 #[cfg_attr(
     target_arch = "x86",
-    link(
-        name = "bcryptprimitives",
-        kind = "raw-dylib",
-        import_name_type = "undecorated"
-    )
+    link(name = "bcryptprimitives", kind = "raw-dylib", import_name_type = "undecorated")
 )]
-#[cfg_attr(
-    not(target_arch = "x86"),
-    link(name = "bcryptprimitives", kind = "raw-dylib")
-)]
+#[cfg_attr(not(target_arch = "x86"), link(name = "bcryptprimitives", kind = "raw-dylib"))]
 extern "system" {
     pub fn ProcessPrng(pbdata: *mut u8, cbdata: usize) -> BOOL;
 }
