f6a0dbab46d3dbcdafa6737c15a4757889795fb5
Scott McMurray <scottmcm@users.noreply.github.com>
2024-08-23 12:21:20 -0700

    Update the MIRI tests

--- src/tools/miri/tests/fail/intrinsics/simd-reduce-invalid-bool.rs
+++ src/tools/miri/tests/fail/intrinsics/simd-reduce-invalid-bool.rs
@@ -4,11 +4,11 @@ use std::intrinsics::simd::simd_reduce_any;
 
 #[repr(simd)]
 #[allow(non_camel_case_types)]
-struct i32x2(i32, i32);
+struct i32x2([i32; 2]);
 
 fn main() {
     unsafe {
-        let x = i32x2(0, 1);
+        let x = i32x2([0, 1]);
         simd_reduce_any(x); //~ERROR: must be all-0-bits or all-1-bits
     }
 }
