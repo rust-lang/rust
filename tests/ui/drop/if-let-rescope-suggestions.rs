1544905b896b2b5b1e49da4c219cdbfbf803acba
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2024-08-25 01:55:41 +0800

    crater run

---
+++ tests/ui/drop/if-let-rescope-suggestions.rs
@@ -0,0 +1,25 @@
+//@ edition:2024
+//@ compile-flags: -Z validate-mir -Zunstable-options
+//@ run-rustfix
+
+#![deny(if_let_rescope)]
+#![feature(if_let_rescope)]
+
+struct Droppy;
+impl Drop for Droppy {
+    fn drop(&mut self) {
+        println!("dropped");
+    }
+}
+impl Droppy {
+    fn get_ref(&self) -> Option<&u8> {
+        None
+    }
+}
+
+fn do_something<T>(_: &T) {}
+
+fn main() {
+    do_something(if let Some(value) = Droppy.get_ref() { value } else { &0 });
+    //~^ ERROR: temporary value dropped while borrowed
+}
