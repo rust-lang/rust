8f178e48125cbadc66f965f55c6521c2262b6bf0
tiif <pekyuan@gmail.com>
2024-08-24 23:45:46 +0800

    Change timeout value

--- src/tools/miri/tests/pass-dep/tokio/sleep.rs
+++ src/tools/miri/tests/pass-dep/tokio/sleep.rs
@@ -6,7 +6,7 @@ use tokio::time::{sleep, Duration, Instant};
 #[tokio::main]
 async fn main() {
     let start = Instant::now();
-    sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_millis(100)).await;
     let time_elapsed = &start.elapsed().as_millis();
-    assert!((1000..1100).contains(time_elapsed), "{}", time_elapsed);
+    assert!((100..1000).contains(time_elapsed), "{}", time_elapsed);
 }
