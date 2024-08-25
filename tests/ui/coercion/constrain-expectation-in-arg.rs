824971f5ba6f5fed99a107ea61cec479aaa541a9
Michael Goulet <michael@errs.io>
2024-08-25 12:45:58 -0400

    Inline expected_inputs_for_expected_output into check_argument_types/check_expr_struct_fields
    (cherry picked from commit 95b9ecd6d671637e9e3db55ed31d06882d3cad4d)

--- tests/ui/coercion/constrain-expectation-in-arg.rs
+++ tests/ui/coercion/constrain-expectation-in-arg.rs
@@ -1,5 +1,10 @@
 //@ check-pass
 
+// Regression test for for #129286.
+// Makes sure that we don't have unconstrained type variables that come from
+// bivariant type parameters due to the way that we construct expectation types
+// when checking call expressions in HIR typeck.
+
 trait Trait {
     type Item;
 }
