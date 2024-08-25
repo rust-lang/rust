3bb0af3152d5a04e9ccb0cf2d66ddecf57e37b77
Trevor Gross <tmgross@umich.edu>
2024-08-25 01:30:17 -0500

    float: Add `f16` to `test-float-parse`
    This requires a fix to the subnormal test to cap the maximum allowed
    value within the maximum mantissa.

--- src/bootstrap/src/core/build_steps/test.rs
+++ src/bootstrap/src/core/build_steps/test.rs
@@ -3565,7 +3565,7 @@ impl Step for TestFloatParse {
         builder.ensure(tool::TestFloatParse { host: self.host });
 
         // Run any unit tests in the crate
-        let cargo_test = tool::prepare_tool_cargo(
+        let mut cargo_test = tool::prepare_tool_cargo(
             builder,
             compiler,
             Mode::ToolStd,
@@ -3575,6 +3575,7 @@ impl Step for TestFloatParse {
             SourceType::InTree,
             &[],
         );
+        cargo_test.allow_features("f16");
 
         run_cargo_test(cargo_test, &[], &[], crate_name, bootstrap_host, builder);
 
@@ -3589,6 +3590,7 @@ impl Step for TestFloatParse {
             SourceType::InTree,
             &[],
         );
+        cargo_run.allow_features("f16");
 
         if !matches!(env::var("FLOAT_PARSE_TESTS_NO_SKIP_HUGE").as_deref(), Ok("1") | Ok("true")) {
             cargo_run.args(["--", "--skip-huge"]);
