ae4c34492b86a655938f3a7787dec651830f6411
techvoyagerX <bytebender90@gmail.com>
2024-08-28 12:41:14 -0400

    Improve error handling and code clarity

--- tests/debuginfo/dummy_span.rs
+++ tests/debuginfo/dummy_span.rs
@@ -1,11 +1,8 @@
 //@ min-lldb-version: 310
-
 //@ compile-flags:-g
 
 // === GDB TESTS ===================================================================================
-
 // gdb-command:run 7
-
 // gdb-command:next
 // gdb-command:next
 // gdb-check:[...]#loc1[...]
@@ -13,9 +10,7 @@
 // gdb-check:[...]#loc2[...]
 
 // === LLDB TESTS ==================================================================================
-
 // lldb-command:run 7
-
 // lldb-command:next
 // lldb-command:next
 // lldb-command:frame select
@@ -27,19 +22,39 @@
 use std::env;
 use std::num::ParseIntError;
 
-fn main() -> Result<(), ParseIntError> {
-    let args = env::args();
-    let number_str = args.skip(1).next().unwrap();
-    let number = number_str.parse::<i32>()?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Collect command line arguments
+    let mut args = env::args().skip(1);
+
+    // Ensure an argument is provided
+    let number_str = match args.next() {
+        Some(arg) => arg,
+        None => {
+            eprintln!("Error: No number provided.");
+            return Err("No number provided.".into());
+        }
+    };
+
+    // Attempt to parse the argument as an integer
+    let number = match number_str.parse::<i32>() {
+        Ok(num) => num,
+        Err(e) => {
+            eprintln!("Error: Failed to parse '{}'. Details: {}", number_str, e);
+            return Err(Box::new(e));
+        }
+    };
+
     zzz(); // #break
+
+    // Check if the number is divisible by 7
     if number % 7 == 0 {
-        // This generates code with a dummy span for
-        // some reason. If that ever changes this
-        // test will not test what it wants to test.
+        // This generates code with a dummy span for some reason.
+        // If that ever changes, this test will not test what it intends to.
         return Ok(()); // #loc1
     }
+
     println!("{}", number);
     Ok(())
 } // #loc2
 
-fn zzz() { () }
+fn zzz() {}
