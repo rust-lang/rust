bbbe968d2058625c30a3f96d51cd6b380352b93f
Nicholas Nethercote <n.nethercote@gmail.com>
2024-08-30 15:57:43 +1000

    Remove an unnecessary `continue`.
    Nothing comes after it within the loop.

--- compiler/rustc_mir_transform/src/inline.rs
+++ compiler/rustc_mir_transform/src/inline.rs
@@ -158,7 +158,6 @@ impl<'tcx> Inliner<'tcx> {
             match self.try_inlining(caller_body, &callsite) {
                 Err(reason) => {
                     debug!("not-inlined {} [{}]", callsite.callee, reason);
-                    continue;
                 }
                 Ok(new_blocks) => {
                     debug!("inlined {}", callsite.callee);
