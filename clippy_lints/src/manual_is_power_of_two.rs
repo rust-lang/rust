6ecb48f6d0becb8ff365b787106a8be429b4637a
Isaac Bess <isaac.bess@outlook.com>
2024-09-02 17:01:20 -0700

    Fixed extra backtick
    Co-authored-by: Ruby Lazuli <general@patchmixolydic.com>

--- clippy_lints/src/manual_is_power_of_two.rs
+++ clippy_lints/src/manual_is_power_of_two.rs
@@ -10,8 +10,8 @@ use rustc_session::declare_lint_pass;
 
 declare_clippy_lint! {
     /// ### What it does
-    /// Checks for expressions like `x.count_ones() == 1` or `x & (x - 1) == 0` which are manual
-    /// reimplementations of `x.is_power_of_two()``
+    /// Checks for expressions like `x.count_ones() == 1` or `x & (x - 1) == 0`, which are manual
+    /// reimplementations of `x.is_power_of_two()`.
     /// ### Why is this bad?
     /// Manual reimplementations of `is_power_of_two` increase code complexity for little benefit.
     /// ### Example
