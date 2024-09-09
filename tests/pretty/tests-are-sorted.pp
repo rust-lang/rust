3dfc7d25c28771f00de542270412d85f3317bcb9
Urgau <urgau@numericable.fr>
2024-09-09 13:43:25 +0200

    Allow `missing_docs` lint on the generated test harness

--- tests/pretty/tests-are-sorted.pp
+++ tests/pretty/tests-are-sorted.pp
@@ -83,6 +83,7 @@ pub const a_test: test::TestDescAndFn =
 fn a_test() {}
 #[rustc_main]
 #[coverage(off)]
+#[allow(missing_docs)]
 pub fn main() -> () {
     extern crate test;
     test::test_main_static(&[&a_test, &m_test, &z_test])
