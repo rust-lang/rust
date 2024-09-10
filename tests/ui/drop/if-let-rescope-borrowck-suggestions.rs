89682a53131803c81c8d6c5f013d1bbe410c8ae1
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2024-09-11 04:07:13 +0800

    downgrade borrowck suggestion level due to possible span conflict

--- tests/ui/drop/if-let-rescope-borrowck-suggestions.rs
+++ tests/ui/drop/if-let-rescope-borrowck-suggestions.rs
@@ -1,6 +1,5 @@
 //@ edition: 2024
 //@ compile-flags: -Z validate-mir -Zunstable-options
-//@ run-rustfix
 
 #![feature(if_let_rescope)]
 #![deny(if_let_rescope)]
@@ -22,4 +21,13 @@ fn do_something<T>(_: &T) {}
 fn main() {
     do_something(if let Some(value) = Droppy.get_ref() { value } else { &0 });
     //~^ ERROR: temporary value dropped while borrowed
+    do_something(if let Some(value) = Droppy.get_ref() {
+        //~^ ERROR: temporary value dropped while borrowed
+        value
+    } else if let Some(value) = Droppy.get_ref() {
+        //~^ ERROR: temporary value dropped while borrowed
+        value
+    } else {
+        &0
+    });
 }
