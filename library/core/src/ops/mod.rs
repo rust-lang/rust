20d50af7e3dac034cf89ed36f318cfccc6d27143
Adrian Taylor <adetaylor@chromium.org>
2024-09-11 12:27:08 +0000

    Rename Receiver -> LegacyReceiver
    As part of the "arbitrary self types v2" project, we are going to
    replace the current `Receiver` trait with a new mechanism based on a
    new, different `Receiver` trait.
    
    This PR renames the old trait to get it out the way. Naming is hard.
    Options considered included:
    * HardCodedReceiver (because it should only be used for things in the
      standard library, and hence is sort-of hard coded)
    * LegacyReceiver
    * TargetLessReceiver
    * OldReceiver
    
    These are all bad names, but fortunately this will be temporary.
    Assuming the new mechanism proceeds to stabilization as intended, the
    legacy trait will be removed altogether.
    
    Although we expect this trait to be used only in the standard library,
    we suspect it may be in use elsehwere, so we're landing this change
    separately to identify any surprising breakages.
    
    It's known that this trait is used within the Rust for Linux project; a
    patch is in progress to remove their dependency.
    
    This is a part of the arbitrary self types v2 project,
    https://github.com/rust-lang/rfcs/pull/3519
    https://github.com/rust-lang/rust/issues/44874
    
    r? @wesleywiser

--- library/core/src/ops/mod.rs
+++ library/core/src/ops/mod.rs
@@ -168,8 +168,8 @@ pub use self::control_flow::ControlFlow;
 pub use self::coroutine::{Coroutine, CoroutineState};
 #[unstable(feature = "deref_pure_trait", issue = "87121")]
 pub use self::deref::DerefPure;
-#[unstable(feature = "receiver_trait", issue = "none")]
-pub use self::deref::Receiver;
+#[unstable(feature = "legacy_receiver_trait", issue = "none")]
+pub use self::deref::LegacyReceiver;
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use self::deref::{Deref, DerefMut};
 #[stable(feature = "rust1", since = "1.0.0")]
