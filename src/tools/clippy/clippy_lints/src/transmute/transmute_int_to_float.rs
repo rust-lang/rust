d195f80639715d54918a7cff171113ed25474d27
Trevor Spiteri <tspiteri@ieee.org>
2024-09-13 13:28:22 +0200

    Revert "stabilize const_float_bits_conv" for src/tools/clippy/clippy_lints
    This reverts the part of commit 19908ff7a37a9a431399bb6f2868efc22820f2b6 in
    subdirectory src/tools/clippy/clippy_lints.

--- src/tools/clippy/clippy_lints/src/transmute/transmute_int_to_float.rs
+++ src/tools/clippy/clippy_lints/src/transmute/transmute_int_to_float.rs
@@ -14,9 +14,10 @@ pub(super) fn check<'tcx>(
     from_ty: Ty<'tcx>,
     to_ty: Ty<'tcx>,
     arg: &'tcx Expr<'_>,
+    const_context: bool,
 ) -> bool {
     match (&from_ty.kind(), &to_ty.kind()) {
-        (ty::Int(_) | ty::Uint(_), ty::Float(_)) => {
+        (ty::Int(_) | ty::Uint(_), ty::Float(_)) if !const_context => {
             span_lint_and_then(
                 cx,
                 TRANSMUTE_INT_TO_FLOAT,
