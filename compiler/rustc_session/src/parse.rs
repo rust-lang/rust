25970803d022fd520db901bd683325b6e951fc0d
Vetle Rasmussen <vetle.rasmussen@muybridge.com>
2024-09-13 11:15:19 +0200

    Add set_dcx to ParseSess

--- compiler/rustc_session/src/parse.rs
+++ compiler/rustc_session/src/parse.rs
@@ -340,4 +340,8 @@ impl ParseSess {
     pub fn dcx(&self) -> DiagCtxtHandle<'_> {
         self.dcx.handle()
     }
+
+    pub fn set_dcx(&mut self, dcx: DiagCtxt) {
+        self.dcx = dcx;
+    }
 }
