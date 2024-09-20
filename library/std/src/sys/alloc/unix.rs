4957eda2c17ff43d52ece67b118ec304e5c41f11
B I Mohammed Abbas <bimohammadabbas@gmail.com>
2024-09-20 17:26:42 +0530

    Allow unused unsafe for vxworks in alligned_malloc to resolve build errors

--- library/std/src/sys/alloc/unix.rs
+++ library/std/src/sys/alloc/unix.rs
@@ -71,6 +71,7 @@ cfg_if::cfg_if! {
         }
     } else {
         #[inline]
+        #[cfg_attr(target_os = "vxworks", allow(unused_unsafe))]
         unsafe fn aligned_malloc(layout: &Layout) -> *mut u8 {
             let mut out = ptr::null_mut();
             // We prefer posix_memalign over aligned_alloc since it is more widely available, and
