a0228686d16baea8880320905a484cfbfe36d8bc
Jubilee Young <workingjubilee@gmail.com>
2024-09-27 13:17:27 -0700

    library: Stabilize `const_str_from_utf8_unchecked_mut`
    Const-stabilizes:
    - `str::from_utf8_unchecked_mut`

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -152,7 +152,6 @@
 #![feature(const_slice_from_ref)]
 #![feature(const_slice_split_at_mut)]
 #![feature(const_str_as_mut)]
-#![feature(const_str_from_utf8_unchecked_mut)]
 #![feature(const_strict_overflow_ops)]
 #![feature(const_swap)]
 #![feature(const_try)]
