0069649c3d23282942e44ee9d5d60546ae920070
Jacob Kiesel <jake@bitcrafters.co>
2024-09-29 01:22:30 -0600

    Add basic integration test for checksum-hash-algorithm feature

---
+++ tests/run-make/checksum-freshness/expected.d
@@ -0,0 +1,3 @@
+lib.d: lib.rs # checksum:blake3=5fd6328c93a3360bc580567aec10731ab3e5efc4a0f957b1bba0db0d27af3767 file_len:110
+
+lib.rs:
---
+++ tests/run-make/checksum-freshness/lib.rs
@@ -0,0 +1,5 @@
+// A basic library to be used in tests with no real purpose.
+
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
