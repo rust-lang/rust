2c23fa4411435ce1b8b50f1885a42c1a6c6f1ca4
GnomedDev <david2005thomas@gmail.com>
2024-09-30 12:52:38 +0100

    Patch out tidy check for allowed sources

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,6 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    r#""git+https://github.com/GnomedDev/thin-vec?branch=outline-capacity-overflow#3f61c4016972cf641c1dff6593e6e87ec94f0f59""#,
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
