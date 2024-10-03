4e1416868607ad7ebe9616fb459c0f6006f6c94e
Orion Gonzalez <ardi@ardis.dev>
2024-10-03 17:11:01 +0200

    update CONFIG_CHANGE_HISTORY

--- src/bootstrap/src/utils/change_tracker.rs
+++ src/bootstrap/src/utils/change_tracker.rs
@@ -270,4 +270,9 @@ pub const CONFIG_CHANGE_HISTORY: &[ChangeInfo] = &[
         severity: ChangeSeverity::Info,
         summary: "If `llvm.download-ci-llvm` is not defined, it defaults to `true`.",
     },
+    ChangeInfo {
+        change_id: 131181,
+        severity: ChangeSeverity::Info,
+        summary: "New option `build.compiletest-diff-tool` that adds support for a custom differ for compiletest",
+    },
 ];
