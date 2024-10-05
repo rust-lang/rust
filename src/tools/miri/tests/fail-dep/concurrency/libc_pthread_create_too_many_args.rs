885ec88f936c39769486776ab002627f6559ff03
Ralf Jung <post@ralfj.de>
2024-10-05 17:21:08 +0200

    avoid pthread_attr_t in tests

--- src/tools/miri/tests/fail-dep/concurrency/libc_pthread_create_too_many_args.rs
+++ src/tools/miri/tests/fail-dep/concurrency/libc_pthread_create_too_many_args.rs
@@ -12,12 +12,13 @@ extern "C" fn thread_start(_null: *mut libc::c_void, _x: i32) -> *mut libc::c_vo
 fn main() {
     unsafe {
         let mut native: libc::pthread_t = mem::zeroed();
-        let attr: libc::pthread_attr_t = mem::zeroed();
-        // assert_eq!(libc::pthread_attr_init(&mut attr), 0); FIXME: this function is not yet implemented.
         let thread_start: extern "C" fn(*mut libc::c_void, i32) -> *mut libc::c_void = thread_start;
         let thread_start: extern "C" fn(*mut libc::c_void) -> *mut libc::c_void =
             mem::transmute(thread_start);
-        assert_eq!(libc::pthread_create(&mut native, &attr, thread_start, ptr::null_mut()), 0);
+        assert_eq!(
+            libc::pthread_create(&mut native, ptr::null(), thread_start, ptr::null_mut()),
+            0
+        );
         assert_eq!(libc::pthread_join(native, ptr::null_mut()), 0);
     }
 }
