8a6137d9fb0a031580420cecc380281335d0cec6
eduardorittner <eduardorittnerc@gmail.com>
2024-10-06 19:03:43 -0300

    ./x test --bless

--- tests/ui/feature-gates/feature-gate-split-pattern.rs
+++ tests/ui/feature-gates/feature-gate-split-pattern.rs
@@ -1,5 +1,5 @@
 pub fn main() {
     let a = &[1, 2, 3, 4, 5];
     let b = &[2, 3];
-    let mut c = a.split_pattern(b);
+    let mut c = a.split_pattern(b); //~ ERROR use of unstable library feature 'split_pattern'
 }
