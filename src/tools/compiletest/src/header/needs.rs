ad7815c266fe00bdba4dfc0dc39a667d8dad9720
Zalathar <Zalathar@users.noreply.github.com>
2024-10-09 14:08:13 +1100

    No need to cache the `profiler_runtime` flag
    This cache struct entry was a relic from when profiler availability was
    communicated via an environment variable rather than a command-line flag.

--- src/tools/compiletest/src/header/needs.rs
+++ src/tools/compiletest/src/header/needs.rs
@@ -101,7 +101,7 @@ pub(super) fn handle_needs(
         },
         Need {
             name: "needs-profiler-runtime",
-            condition: cache.profiler_runtime,
+            condition: config.profiler_runtime,
             ignore_reason: "ignored when the profiler runtime is not available",
         },
         Need {
@@ -220,7 +220,6 @@ pub(super) struct CachedNeedsConditions {
     sanitizer_memtag: bool,
     sanitizer_shadow_call_stack: bool,
     sanitizer_safestack: bool,
-    profiler_runtime: bool,
     xray: bool,
     rust_lld: bool,
     dlltool: bool,
@@ -247,7 +246,6 @@ impl CachedNeedsConditions {
             sanitizer_memtag: sanitizers.contains(&Sanitizer::Memtag),
             sanitizer_shadow_call_stack: sanitizers.contains(&Sanitizer::ShadowCallStack),
             sanitizer_safestack: sanitizers.contains(&Sanitizer::Safestack),
-            profiler_runtime: config.profiler_runtime,
             xray: config.target_cfg().xray,
 
             // For tests using the `needs-rust-lld` directive (e.g. for `-Clink-self-contained=+linker`),
