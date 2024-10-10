eb76079911b3011e6c41bd69bc6536ad442c2cd2
Ralf Jung <post@ralfj.de>
2024-10-10 07:53:16 +0200

    epoll event adding: no need to join, there's no old clock here

--- src/tools/miri/src/shims/unix/linux/epoll.rs
+++ src/tools/miri/src/shims/unix/linux/epoll.rs
@@ -626,7 +626,7 @@ fn check_and_update_one_event_interest<'tcx>(
         let mut event_instance = EpollEventInstance::new(flags, epoll_event_interest.data);
         // If we are tracking data races, remember the current clock so we can sync with it later.
         ecx.release_clock(|clock| {
-            event_instance.clock.join(clock);
+            event_instance.clock.clone_from(clock);
         });
         // Triggers the notification by inserting it to the ready list.
         ready_list.insert(epoll_key, event_instance);
