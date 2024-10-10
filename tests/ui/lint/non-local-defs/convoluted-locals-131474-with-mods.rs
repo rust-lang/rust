783625079ee1b31fef993f3c11df49d705b2b2d8
Urgau <urgau@numericable.fr>
2024-10-10 14:22:20 +0200

    Consider outermost const-anon in non_local_def lint

---
+++ tests/ui/lint/non-local-defs/convoluted-locals-131474-with-mods.rs
@@ -0,0 +1,34 @@
+// This test check that no matter the nesting of const-anons and modules
+// we consider them as transparent.
+//
+// Similar to https://github.com/rust-lang/rust/issues/131474
+
+//@ check-pass
+
+pub mod tmp {
+    pub mod tmp {
+        pub struct Test;
+    }
+}
+
+const _: () = {
+    const _: () = {
+        const _: () = {
+            const _: () = {
+                impl tmp::tmp::Test {}
+            };
+        };
+    };
+};
+
+const _: () = {
+    const _: () = {
+        mod tmp {
+            pub(super) struct InnerTest;
+        }
+
+        impl tmp::InnerTest {}
+    };
+};
+
+fn main() {}
