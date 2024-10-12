a4be3b549b4013b7c3c3c0a555e07a7ef058c395
GnomedDev <david2005thomas@gmail.com>
2024-10-12 22:46:29 +0100

    [WIP] Make tidy be quiet temporarily

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,6 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    r#""git+https://github.com/GnomedDev/triomphe?branch=dropck-eyepatch#a9614f96c0eb04818cea5f98416b05ca34d9e383""#,
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
