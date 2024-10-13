ed008aa44885b45c392de3d576f6d85cadbe6e59
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2024-10-13 19:00:29 +0800

    the lint works up until 2021

--- compiler/rustc_lint_defs/src/builtin.rs
+++ compiler/rustc_lint_defs/src/builtin.rs
@@ -5007,7 +5007,7 @@ declare_lint! {
     /// Your discretion on the new drop order introduced by Edition 2024 is required.
     ///
     /// ### Example
-    /// ```rust,edition2024
+    /// ```rust,edition2021
     /// #![cfg_attr(not(bootstrap), feature(shorter_tail_lifetimes))] // Simplify this in bootstrap bump.
     /// #![warn(tail_expr_drop_order)]
     /// struct Droppy(i32);
@@ -5023,12 +5023,12 @@ declare_lint! {
     ///         println!("loud drop {}", self.0);
     ///     }
     /// }
-    /// fn edition_2024() -> i32 {
+    /// fn edition_2021() -> i32 {
     ///     let another_droppy = Droppy(0);
     ///     Droppy(1).get()
     /// }
     /// fn main() {
-    ///     edition_2024();
+    ///     edition_2021();
     /// }
     /// ```
     ///
