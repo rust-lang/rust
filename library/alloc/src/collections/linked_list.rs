3ed5d5590e12d972acf4ca6a503d0b00f233ad0e
Collin O'Connor <collinoc+dev@protonmail.com>
2024-10-16 19:52:50 -0500

    Fix predicate signatures in retain_mut docs

--- library/alloc/src/collections/linked_list.rs
+++ library/alloc/src/collections/linked_list.rs
@@ -1082,7 +1082,7 @@ impl<T, A: Allocator> LinkedList<T, A> {
 
     /// Retains only the elements specified by the predicate.
     ///
-    /// In other words, remove all elements `e` for which `f(&e)` returns false.
+    /// In other words, remove all elements `e` for which `f(&mut e)` returns false.
     /// This method operates in place, visiting each element exactly once in the
     /// original order, and preserves the order of the retained elements.
     ///
