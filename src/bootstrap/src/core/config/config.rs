24684612457fd2f2bb00265892f7de1ad9d50ae0
onur-ozkan <work@onurozkan.dev>
2024-10-17 15:15:34 +0300

    add test coverage for rustc "if-unchanged" paths
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/bootstrap/src/core/config/config.rs
+++ src/bootstrap/src/core/config/config.rs
@@ -37,7 +37,7 @@ use crate::utils::helpers::{self, exe, output, t};
 ///
 /// WARNING: Be cautious when adding paths to this list. If a path that influences the compiler build
 /// is added here, it will cause bootstrap to skip necessary rebuilds, which may lead to risky results.
-const RUSTC_IF_UNCHANGED_ALLOWED_PATHS: &[&str] = &[
+pub(crate) const RUSTC_IF_UNCHANGED_ALLOWED_PATHS: &[&str] = &[
     ":!.github",
     ":!.clang-format",
     ":!.editorconfig",
@@ -72,7 +72,7 @@ const RUSTC_IF_UNCHANGED_ALLOWED_PATHS: &[&str] = &[
 ];
 
 /// Holds subpaths from [`RUSTC_IF_UNCHANGED_ALLOWED_PATHS`] that should not be allowed.
-const RUSTC_IF_UNCHANGED_DENIED_SUBPATHS: &[&str] = &[
+pub(crate) const RUSTC_IF_UNCHANGED_DENIED_SUBPATHS: &[&str] = &[
     "src/stage0",
     "src/version",
     "src/ci/channel",
