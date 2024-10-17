7681617b182268bc0f3cb04c7d50de48e76e6c56
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-10-17 19:50:59 +0200

    Stop relying on hashmap iteration for hir stat printing
    Just because the code says it's OK does not mean that it actually is OK.
    Nodes with the same total size were not sorted, their order relied on
    hashmap iteration.

--- compiler/rustc_passes/src/hir_stats.rs
+++ compiler/rustc_passes/src/hir_stats.rs
@@ -122,7 +122,9 @@ impl<'k> StatCollector<'k> {
         // We will soon sort, so the initial order does not matter.
         #[allow(rustc::potential_query_instability)]
         let mut nodes: Vec<_> = self.nodes.iter().collect();
-        nodes.sort_by_key(|(_, node)| node.stats.count * node.stats.size);
+        nodes.sort_by_cached_key(|(label, node)| {
+            (node.stats.count * node.stats.size, label.to_owned())
+        });
 
         let total_size = nodes.iter().map(|(_, node)| node.stats.count * node.stats.size).sum();
 
