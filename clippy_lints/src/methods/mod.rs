91a458f45106082f31dea172156b9ad6400a9955
Philipp Krones <hello@philkrones.com>
2024-10-18 14:54:06 +0200

    Hotfix TRAIT_METHODS static->const

--- clippy_lints/src/methods/mod.rs
+++ clippy_lints/src/methods/mod.rs
@@ -5182,7 +5182,8 @@ impl ShouldImplTraitCase {
 }
 
 #[rustfmt::skip]
-static TRAIT_METHODS: [ShouldImplTraitCase; 30] = [
+#[expect(clippy::large_const_arrays, reason = "`Span` is not sync, so this can't be static")]
+const TRAIT_METHODS: [ShouldImplTraitCase; 30] = [
     ShouldImplTraitCase::new("std::ops::Add", "add",  2,  FN_HEADER,  SelfKind::Value,  OutType::Any, true),
     ShouldImplTraitCase::new("std::convert::AsMut", "as_mut",  1,  FN_HEADER,  SelfKind::RefMut,  OutType::Ref, true),
     ShouldImplTraitCase::new("std::convert::AsRef", "as_ref",  1,  FN_HEADER,  SelfKind::Ref,  OutType::Ref, true),
