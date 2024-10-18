1af9d11583ea7ce06f78594b7f15bb0ba8bb22b1
许杰友 Jieyou Xu (Joe) <39484203+jieyouxu@users.noreply.github.com>
2024-10-18 16:00:11 +0800

    Expand test coverage for deny-inside-forbid interactions

--- tests/ui/lint/deny-inside-forbid-ignored.rs
+++ tests/ui/lint/deny-inside-forbid-ignored.rs
@@ -1,5 +1,9 @@
-/// Ensure that using deny inside forbid is treated as a no-op,
-/// and does not override the level to deny.
+//! Ensure that using deny inside forbid is treated as a no-op, and does not override the level to
+//! deny.
+
+//@ revisions: source_only cli_forbid cli_forbid_warnings
+//@[cli_forbid] compile-flags: -F unsafe_code
+//@[cli_forbid_warnings] compile-flags: -F warnings
 
 #[forbid(unsafe_code)] // NO UNSAFE CODE IN HERE!!
 fn main() {
