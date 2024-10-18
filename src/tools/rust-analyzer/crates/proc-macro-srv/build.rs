ede2cdae32c8f9dce288959725e11897f1def689
Ralf Jung <post@ralfj.de>
2024-10-18 07:40:33 +0200

    proc-macro-srv: honor RUSTC_WRAPPER env var

--- src/tools/rust-analyzer/crates/proc-macro-srv/build.rs
+++ src/tools/rust-analyzer/crates/proc-macro-srv/build.rs
@@ -7,7 +7,14 @@ fn main() {
     println!("cargo::rustc-check-cfg=cfg(rust_analyzer)");
 
     let rustc = env::var("RUSTC").expect("proc-macro-srv's build script expects RUSTC to be set");
-    let output = Command::new(rustc).arg("--version").output().expect("rustc --version must run");
+    let mut cmd = if let Some(wrapper) = env::var_os("RUSTC_WRAPPER").filter(|w| !w.is_empty()) {
+        let mut cmd = Command::new(wrapper);
+        cmd.arg(rustc);
+        cmd
+    } else {
+        Command::new(rustc)
+    };
+    let output = cmd.arg("--version").output().expect("rustc --version must run");
     let version_string = std::str::from_utf8(&output.stdout[..])
         .expect("rustc --version output must be UTF-8")
         .trim();
