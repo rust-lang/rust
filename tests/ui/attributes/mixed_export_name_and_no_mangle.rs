72a2280449fa16611b9075c8485a423891fc54dd
Sven Kanoldt <sven@d34dl0ck.me>
2024-10-18 18:54:33 +0200

    fix: add `#[warn(unused_attributes)]` so that the lint triggers

--- tests/ui/attributes/mixed_export_name_and_no_mangle.rs
+++ tests/ui/attributes/mixed_export_name_and_no_mangle.rs
@@ -2,6 +2,7 @@
 //@ run-rustfix
 //@ check-pass
 
+#![warn(unused_attributes)]
 #[no_mangle]
 //~^ WARN the `#[no_mangle]` attribute may not be used in combination with `#[export_name]`
 #[export_name = "foo"]
