7782401c520ab1e744dbcf58d0f0254672da9026
Slanterns <slanterns.w@gmail.com>
2024-10-22 01:33:22 -0700

    add codegen test

--- tests/codegen/placement-new.rs
+++ tests/codegen/placement-new.rs
@@ -1,9 +1,11 @@
 //@ compile-flags: -O
+//@ compile-flags: -Zmerge-functions=disabled
 #![crate_type = "lib"]
 
 // Test to check that types with "complex" destructors, but trivial `Default` impls
 // are constructed directly into the allocation in `Box::default` and `Arc::default`.
 
+use std::rc::Rc;
 use std::sync::Arc;
 
 // CHECK-LABEL: @box_default_inplace
@@ -14,6 +16,16 @@ pub fn box_default_inplace() -> Box<(String, String)> {
     // CHECK-NOT: call void @llvm.memcpy
     // CHECK: ret ptr [[BOX]]
     Box::default()
+}
+
+// CHECK-LABEL: @rc_default_inplace
+#[no_mangle]
+pub fn rc_default_inplace() -> Rc<(String, String)> {
+    // CHECK-NOT: alloca
+    // CHECK: [[RC:%.*]] = {{.*}}call {{.*}}__rust_alloc(
+    // CHECK-NOT: call void @llvm.memcpy
+    // CHECK: ret ptr [[RC]]
+    Rc::default()
 }
 
 // CHECK-LABEL: @arc_default_inplace
