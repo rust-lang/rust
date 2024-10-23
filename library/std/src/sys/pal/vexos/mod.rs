508177c682e4b0c1d40e0a3d58da949249ee707a
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-10-23 15:44:39 -0500

    update to `vex-sdk` 0.23.0
    0.23.0 most notably removes uses of `#[no_mangle]` from symbols in `vex-sdk` to avoid conflicts with other versions of the crate.
    
    Symbols were originally `#[no_mangle]` for the purposes of compiling to a C static library, but this never ended up being useful and would be actively harmful to include in `libstd`.

--- library/std/src/sys/pal/vexos/mod.rs
+++ library/std/src/sys/pal/vexos/mod.rs
@@ -107,16 +107,16 @@ pub fn abort_internal() -> ! {
         // Force the serial buffer to flush
         while exit_time.elapsed() < FLUSH_TIMEOUT {
             vex_sdk::vexTasksRun();
-            
+
             // If the buffer has been fully flushed, exit the loop
             if vex_sdk::vexSerialWriteFree(stdio::STDIO_CHANNEL) == (stdio::STDOUT_BUF_SIZE as i32)
             {
                 break;
             }
         }
-        
+
         vex_sdk::vexSystemExitRequest();
-        
+
         loop {
             vex_sdk::vexTasksRun();
         }
