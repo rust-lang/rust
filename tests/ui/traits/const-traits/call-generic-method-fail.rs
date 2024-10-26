f2f67232a53b79b44c5b87fe5757ef1f18f4b590
Deadbeef <ent3rm4n@gmail.com>
2024-10-26 11:35:16 +0800

    Deny calls to non-`#[const_trait]` methods in MIR constck

--- tests/ui/traits/const-traits/call-generic-method-fail.rs
+++ tests/ui/traits/const-traits/call-generic-method-fail.rs
@@ -1,12 +1,10 @@
-//@ check-pass
 //@ compile-flags: -Znext-solver
 #![allow(incomplete_features)]
 #![feature(const_trait_impl, effects)]
 
 pub const fn equals_self<T: PartialEq>(t: &T) -> bool {
     *t == *t
-    // FIXME(effects) ~^ ERROR mismatched types
-    // FIXME(effects): diagnostic
+    //~^ ERROR cannot call non-const operator in constant functions
 }
 
 fn main() {}
