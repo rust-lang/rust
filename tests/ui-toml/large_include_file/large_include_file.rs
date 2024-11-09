223bffd0d2c7f26145d2e7861dbe5cbb0aff830e
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-11-09 13:51:46 +0100

    Fix `large_include_file` lint being triggered all the time by doc comments

--- tests/ui-toml/large_include_file/large_include_file.rs
+++ tests/ui-toml/large_include_file/large_include_file.rs
@@ -15,5 +15,9 @@ const TOO_BIG_INCLUDE_BYTES: &[u8; 654] = include_bytes!("too_big.txt");
 const TOO_BIG_INCLUDE_STR: &str = include_str!("too_big.txt");
 //~^ large_include_file
 
-#[doc = include_str!("too_big.txt")] //~ large_include_file
+#[doc = include_str!("too_big.txt")]
+//~^ large_include_file
+// Should not lint!
+// Regression test for <https://github.com/rust-lang/rust-clippy/issues/13670>.
+#[doc = include_str!("empty.txt")]
 fn main() {}
