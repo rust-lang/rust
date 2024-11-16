35beb09850763026a6011e7a6b277cb45f709f64
Felix Rath <felixm.rath@gmail.com>
2024-11-16 16:06:48 +0100

    fix: comment + unused warning

--- tests/incremental/derive_macro_expansion/proc_macro_unchanged.rs
+++ tests/incremental/derive_macro_expansion/proc_macro_unchanged.rs
@@ -2,13 +2,13 @@
 // HOWEVER, this test can currently only be checked manually,
 // by running it (through compiletest) with `-- --nocapture --verbose`.
 // The proc-macro (for `Nothing`) prints a message to stderr when invoked,
-// and this message should only be present during the second invocation
-// (which has `cfail2` set via cfg).
+// and this message should only be present during the first invocation,
+// because the cached result should be used for the second invocation.
 // FIXME(pr-time): Properly have the test check this, but how? UI-test that tests for `.stderr`?
 
 //@ aux-build:derive_nothing.rs
 //@ revisions:cfail1 cfail2
-//@ compile-flags: -Z query-dep-graph
+//@ compile-flags: -Z query-dep-graph -Zcache-all-derive-macros=true
 //@ build-pass
 
 #![feature(rustc_attrs)]
@@ -17,10 +17,12 @@
 #![crate_type = "rlib"]
 
 #![rustc_partition_codegened(module="proc_macro_unchanged-foo", cfg="cfail1")]
-#![rustc_partition_codegened(module="proc_macro_unchanged-foo", cfg="cfail2")]
+// #![rustc_partition_codegened(module="proc_macro_unchanged-foo", cfg="cfail2")]
 
 // `foo::nothing_mod` is created by the derive macro and doesn't change
-#![rustc_partition_reused(module="proc_macro_unchanged-foo", cfg="cfail2")]
+// BUG: this yields the same result with `-Zcache-all-derive-macros=false` (i.e., uncached),
+// not sure how to do this correctly.
+#![rustc_partition_reused(module="proc_macro_unchanged-foo-nothing_mod", cfg="cfail2")]
 
  #[macro_use]
  extern crate derive_nothing;
