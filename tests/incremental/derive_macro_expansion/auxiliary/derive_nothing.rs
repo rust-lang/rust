8610ec6b70f8e37a4484aa4807f98f722d235704
Felix Rath <felixm.rath@gmail.com>
2024-11-16 16:06:48 +0100

    fix: comment + unused warning

--- tests/incremental/derive_macro_expansion/auxiliary/derive_nothing.rs
+++ tests/incremental/derive_macro_expansion/auxiliary/derive_nothing.rs
@@ -7,7 +7,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Nothing)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(_input: TokenStream) -> TokenStream {
     eprintln!("invoked");
 
     return r#"
