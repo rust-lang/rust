808e44145498774ef72d92ec2e97725e14a5c00b
lcnr <rust@lcnr.de>
2024-11-20 19:59:26 +0100

    update crashes

--- tests/ui/type-alias-impl-trait/hidden-ty-mismatch-tainting-issue-120016.rs
+++ tests/ui/type-alias-impl-trait/hidden-ty-mismatch-tainting-issue-120016.rs
@@ -1,5 +1,4 @@
-//@ known-bug: #120016
-//@ compile-flags: -Zcrate-attr=feature(const_async_blocks) --edition=2021
+//@ compile-flags: --edition=2021
 
 #![feature(type_alias_impl_trait, const_async_blocks)]
 
@@ -7,11 +6,11 @@ struct Bug {
     V1: [(); {
         type F = impl std::future::Future<Output = impl Sized>;
         fn concrete_use() -> F {
-            //~^ ERROR to be a future that resolves to `u8`, but it resolves to `()`
             async {}
         }
         let f: F = async { 1 };
-        //~^ ERROR `async` blocks are not allowed in constants
+        //~^ ERROR concrete type differs from previous defining opaque type use
+        //~| ERROR concrete type differs from previous defining opaque type use
         1
     }],
 }
