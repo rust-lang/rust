bba35673869408839778949efcb223cf77597352
Zalathar <Zalathar@users.noreply.github.com>
2024-11-23 16:14:40 +1100

    Make profiler_builtins `#![no_core]` instead of just `#![no_std]`
    This crate doesn't contain any actual Rust code; it's just C/C++ code built and
    packaged in a Rust-friendly way.

--- library/profiler_builtins/src/lib.rs
+++ library/profiler_builtins/src/lib.rs
@@ -1,11 +1,12 @@
 // tidy-alphabetical-start
 #![allow(internal_features)]
+#![feature(no_core)]
 #![feature(profiler_runtime)]
 #![feature(staged_api)]
 // tidy-alphabetical-end
 
 // Other attributes:
-#![no_std]
+#![no_core]
 #![profiler_runtime]
 #![unstable(
     feature = "profiler_runtime_lib",
