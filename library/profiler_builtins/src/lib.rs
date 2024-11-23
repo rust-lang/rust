7d2d11b59588e5e75c666b3d2ce6187cbfa6ac7e
Zalathar <Zalathar@users.noreply.github.com>
2024-11-23 16:11:02 +1100

    Sort and separate lint/feature attributes in `profiler_builtins`

--- library/profiler_builtins/src/lib.rs
+++ library/profiler_builtins/src/lib.rs
@@ -1,11 +1,15 @@
-#![no_std]
+// tidy-alphabetical-start
+#![allow(internal_features)]
+#![allow(unused_features)]
 #![feature(profiler_runtime)]
+#![feature(staged_api)]
+// tidy-alphabetical-end
+
+// Other attributes:
+#![no_std]
 #![profiler_runtime]
 #![unstable(
     feature = "profiler_runtime_lib",
     reason = "internal implementation detail of rustc right now",
     issue = "none"
 )]
-#![allow(unused_features)]
-#![allow(internal_features)]
-#![feature(staged_api)]
