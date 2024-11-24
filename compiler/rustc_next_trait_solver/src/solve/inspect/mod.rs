ccc06c09b61d4b3d3246d430b6986bfe115c2069
Zalathar <Zalathar@users.noreply.github.com>
2024-11-24 13:14:22 +1100

    Rename `rustc_next_trait_solver::solve::inspect::build` to `builder`

--- compiler/rustc_next_trait_solver/src/solve/inspect/mod.rs
+++ compiler/rustc_next_trait_solver/src/solve/inspect/mod.rs
@@ -1,6 +1,6 @@
 pub use rustc_type_ir::solve::inspect::*;
 
-mod build;
-pub(in crate::solve) use build::*;
+mod builder;
+pub(in crate::solve) use builder::*;
 
 pub use crate::solve::eval_ctxt::canonical::instantiate_canonical_state;
