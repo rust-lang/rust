bd44b632a84030138a5f0133fa2ae7ad30fe8266
Michael Goulet <michael@errs.io>
2024-11-24 00:30:16 +0000

    Constify Drop and Destruct

--- library/core/src/marker.rs
+++ library/core/src/marker.rs
@@ -957,6 +957,7 @@ marker_impls! {
 #[lang = "destruct"]
 #[rustc_on_unimplemented(message = "can't drop `{Self}`", append_const_msg)]
 #[rustc_deny_explicit_impl(implement_via_object = false)]
+#[cfg_attr(not(bootstrap), const_trait)]
 pub trait Destruct {}
 
 /// A marker for tuple types.
