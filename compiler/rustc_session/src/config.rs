42b87819120577ace99466809be132674c1abe30
lcnr <rust@lcnr.de>
2024-11-26 14:03:00 +0100

    enable new solver everywhere

--- compiler/rustc_session/src/config.rs
+++ compiler/rustc_session/src/config.rs
@@ -886,7 +886,7 @@ pub struct NextSolverConfig {
 }
 impl Default for NextSolverConfig {
     fn default() -> Self {
-        NextSolverConfig { coherence: true, globally: false }
+        NextSolverConfig { coherence: true, globally: true }
     }
 }
 
