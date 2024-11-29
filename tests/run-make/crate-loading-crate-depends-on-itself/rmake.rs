2fb0b160c051f57a9e93c33d6ed972195a009e1d
Esteban Küber <esteban@kuber.com.ar>
2024-11-29 18:31:37 +0000

    Use rmake `diff` output in test
    (cherry picked from commit e97e15dea55d61d68732bc030be1a44d8a51a1e9)

--- tests/run-make/crate-loading-crate-depends-on-itself/rmake.rs
+++ tests/run-make/crate-loading-crate-depends-on-itself/rmake.rs
@@ -7,26 +7,25 @@
 // and traits of the different versions are mixed, we produce diagnostic output and not an ICE.
 // #133563
 
-use run_make_support::{rust_lib_name, rustc};
+use run_make_support::{diff, rust_lib_name, rustc};
 
 fn main() {
     rustc().input("foo-prev.rs").run();
 
-    rustc()
+    let out = rustc()
         .extra_filename("current")
         .metadata("current")
         .input("foo-current.rs")
         .extern_("foo", rust_lib_name("foo"))
         .run_fail()
-        .assert_stderr_contains(r#"
-note: there are multiple different versions of crate `foo` in the dependency graph
-  --> foo-current.rs:7:1
-   |
-4  | extern crate foo;
-   | ----------------- one version of crate `foo` is used here, as a direct dependency of the current crate
-5  |
-6  | pub struct Struct;
-   | ----------------- this type implements the required trait
-7  | pub trait Trait {}
-   | ^^^^^^^^^^^^^^^ this is the required trait"#);
+        .stderr_utf8();
+
+    // We don't remap the path of the `foo-prev` crate, so we remap it here.
+    let mut lines: Vec<_> = out.lines().collect();
+    for line in &mut lines {
+        if line.starts_with("  ::: ") {
+            *line = "  ::: foo-prev.rs:X:Y";
+        }
+    }
+    diff().expected_file("foo.stderr").actual_text("(rustc)", &lines.join("\n")).run();
 }
