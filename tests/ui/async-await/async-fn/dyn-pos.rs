fa7449d130c70bf96d1302ebd69c054227028ec3
Michael Goulet <michael@errs.io>
2024-11-30 02:47:40 +0000

    Do not create trait object type if missing associated types

--- tests/ui/async-await/async-fn/dyn-pos.rs
+++ tests/ui/async-await/async-fn/dyn-pos.rs
@@ -3,9 +3,6 @@
 #![feature(async_closure)]
 
 fn foo(x: &dyn async Fn()) {}
-//~^ ERROR the trait `AsyncFn` cannot be made into an object
-//~| ERROR the trait `AsyncFnMut` cannot be made into an object
-//~| ERROR the trait `AsyncFnMut` cannot be made into an object
-//~| ERROR the trait `AsyncFnMut` cannot be made into an object
+//~^ ERROR the trait `AsyncFnMut` cannot be made into an object
 
 fn main() {}
