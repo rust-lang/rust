cd025d79467abcbded86a4d3972f61bf40116aea
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2024-12-04 05:05:45 +0800

    stabilize derive(CoercePointee)

--- src/tools/miri/src/lib.rs
+++ src/tools/miri/src/lib.rs
@@ -14,7 +14,6 @@
 #![feature(strict_overflow_ops)]
 #![feature(pointer_is_aligned_to)]
 #![feature(unqualified_local_imports)]
-#![feature(derive_coerce_pointee)]
 #![feature(arbitrary_self_types)]
 // Configure clippy and other lints
 #![allow(
