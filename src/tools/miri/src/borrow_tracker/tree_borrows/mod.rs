JoJoDeveloping/miri

Properly fix #3846 by resetting parents on lazy node creation
This commit supplies a real fix, which makes retags more complicated, at the benefit of
making accesses more performant.

src/borrow_tracker/tree_borrows/mod.rs
@@ -297,6 +297,11 @@ trait EvalContextPrivExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
         )?;
         // Record the parent-child pair in the tree.
         tree_borrows.new_child(orig_tag, new_tag, new_perm.initial_state, range, span)?;
+        tree_borrows.update_last_accessed_after_retag(
+            new_tag,
+            new_perm.initial_state,
+            new_perm.protector.is_some(),
+        );
         drop(tree_borrows);
 
         // Also inform the data race model (but only if any bytes are actually affected).

rust-lang/rust

Co-authored-by: Ralf Jung <post@ralfj.de>

src/tools/miri/src/borrow_tracker/tree_borrows/mod.rs
@@ -9,6 +9,7 @@ use crate::concurrency::data_race::NaReadType;
 use crate::*;
 
 pub mod diagnostics;
+mod foreign_access_skipping;
 mod perms;
 mod tree;
 mod unimap;
@@ -296,7 +297,14 @@ trait EvalContextPrivExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
             this.machine.current_span(),
         )?;
         // Record the parent-child pair in the tree.
-        tree_borrows.new_child(orig_tag, new_tag, new_perm.initial_state, range, span)?;
+        tree_borrows.new_child(
+            orig_tag,
+            new_tag,
+            new_perm.initial_state,
+            range,
+            span,
+            new_perm.protector.is_some(),
+        )?;
         drop(tree_borrows);
 
         // Also inform the data race model (but only if any bytes are actually affected).