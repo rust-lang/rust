38b6058a966baf84ae543b63e7fff6d62009ce7c
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-06 09:06:51 +0000

    Remove all support for wasm's legacy ABI

--- compiler/rustc_session/src/config.rs
+++ compiler/rustc_session/src/config.rs
@@ -2936,7 +2936,7 @@ pub(crate) mod dep_tracking {
     use rustc_target::spec::{
         CodeModel, FramePointer, MergeFunctions, OnBrokenPipe, PanicStrategy, RelocModel,
         RelroLevel, SanitizerSet, SplitDebuginfo, StackProtector, SymbolVisibility, TargetTuple,
-        TlsModel, WasmCAbi,
+        TlsModel,
     };
 
     use super::{
@@ -3047,7 +3047,6 @@ pub(crate) mod dep_tracking {
         Polonius,
         InliningThreshold,
         FunctionReturn,
-        WasmCAbi,
         Align,
     );
 
