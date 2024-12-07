5a068cda1491f5e4c3f27d67afa0168994a7ed85
klensy <klensy@users.noreply.github.com>
2024-12-07 13:51:08 +0300

    fix few comment typos in compiler

--- compiler/rustc_lint_defs/src/builtin.rs
+++ compiler/rustc_lint_defs/src/builtin.rs
@@ -4480,7 +4480,7 @@ declare_lint! {
 
 declare_lint! {
     /// The `refining_impl_trait_reachable` lint detects `impl Trait` return
-    /// types in method signatures that are refined by a publically reachable
+    /// types in method signatures that are refined by a publicly reachable
     /// trait implementation, meaning the implementation adds information about
     /// the return type that is not present in the trait.
     ///
@@ -4521,7 +4521,7 @@ declare_lint! {
     ///
     /// `refining_impl_trait` is a lint group composed of two lints:
     ///
-    /// * `refining_impl_trait_reachable`, for refinements that are publically
+    /// * `refining_impl_trait_reachable`, for refinements that are publicly
     ///   reachable outside a crate, and
     /// * `refining_impl_trait_internal`, for refinements that are only visible
     ///    within a crate.
@@ -4577,7 +4577,7 @@ declare_lint! {
     ///
     /// `refining_impl_trait` is a lint group composed of two lints:
     ///
-    /// * `refining_impl_trait_reachable`, for refinements that are publically
+    /// * `refining_impl_trait_reachable`, for refinements that are publicly
     ///   reachable outside a crate, and
     /// * `refining_impl_trait_internal`, for refinements that are only visible
     ///    within a crate.
