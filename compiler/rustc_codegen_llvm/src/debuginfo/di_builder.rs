14397b701df8b4b8050bcc6f85fd2b6d232ed98b
Zalathar <Zalathar@users.noreply.github.com>
2024-12-07 21:27:04 +1100

    Use `LLVMDIBuilderCreateUnionType`

--- compiler/rustc_codegen_llvm/src/debuginfo/di_builder.rs
+++ compiler/rustc_codegen_llvm/src/debuginfo/di_builder.rs
@@ -1,6 +1,7 @@
 //! Safe wrappers for [`DIBuilder`] FFI functions.
 
 use libc::c_uint;
+use rustc_abi::{Align, Size};
 
 use crate::llvm::debuginfo::{DIBuilder, DIFlags};
 use crate::llvm::{self, Metadata};
@@ -18,6 +19,38 @@ impl<'ll> DIBuilder<'ll> {
                 parameter_types.as_ptr(),
                 parameter_types.len() as c_uint,
                 flags,
+            )
+        }
+    }
+
+    pub(crate) fn create_union_type(
+        &self,
+        scope: Option<&'ll Metadata>,
+        name: &str,
+        file_metadata: &'ll Metadata,
+        line_number: c_uint,
+        size: Size,
+        align: Align,
+        flags: DIFlags,
+        elements: &[&'ll Metadata],
+        unique_id: &str,
+    ) -> &'ll Metadata {
+        unsafe {
+            llvm::LLVMDIBuilderCreateUnionType(
+                self,
+                scope,
+                name.as_ptr(),
+                name.len(),
+                file_metadata,
+                line_number,
+                size.bits(),
+                align.bits() as u32,
+                flags,
+                elements.as_ptr(),
+                elements.len() as c_uint,
+                0, // ("Objective-C runtime version"; default is 0)
+                unique_id.as_ptr(),
+                unique_id.len(),
             )
         }
     }
