b30d9ebf2627efa37278e00b6ef714340cf224a6
Oli Scherer <github333195615777966@oli-obk.de>
2024-12-09 09:51:55 +0000

    Add regression tests

---
+++ tests/ui/asm/generic_const_simd_vec_len.rs
@@ -0,0 +1,18 @@
+//! This is a regression test to ensure that we emit a diagnostic pointing to the
+//! reason the type was rejected in inline assembly.
+
+#![feature(repr_simd)]
+
+#[repr(simd)]
+#[derive(Copy, Clone)]
+pub struct Foo<const C: usize>([u8; C]);
+
+pub unsafe fn foo<const C: usize>(a: Foo<C>) {
+    std::arch::asm!(
+        "movaps {src}, {src}",
+        src = in(xmm_reg) a,
+        //~^ ERROR: cannot use value of type `Foo<C>` for inline assembly
+    );
+}
+
+fn main() {}
