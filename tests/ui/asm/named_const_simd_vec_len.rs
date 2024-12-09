5370f1243e1d5cb18ede24935fcdcfa14f0584dd
Oli Scherer <github333195615777966@oli-obk.de>
2024-12-09 10:06:51 +0000

    Evaluate constants in SIMD vec lengths before rejecting them

--- tests/ui/asm/named_const_simd_vec_len.rs
+++ tests/ui/asm/named_const_simd_vec_len.rs
@@ -1,6 +1,8 @@
 //! This is a regression test to ensure that we evaluate
 //! SIMD vector length constants instead of assuming they are literals.
 
+//@check-pass
+
 #![feature(repr_simd)]
 
 const C: usize = 16;
@@ -13,7 +15,6 @@ pub unsafe fn foo(a: Foo) {
     std::arch::asm!(
         "movaps {src}, {src}",
         src = in(xmm_reg) a,
-        //~^ ERROR: cannot use value of type `Foo` for inline assembly
     );
 }
 
