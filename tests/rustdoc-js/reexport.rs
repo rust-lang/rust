8200c1e52e1f67cee0546b288a72154407cf1c0a
Michael Howell <michael@notriddle.com>
2024-12-12 13:26:09 -0700

    rustdoc-search: fix mismatched path when parent re-exported twice

--- tests/rustdoc-js/reexport.rs
+++ tests/rustdoc-js/reexport.rs
@@ -2,6 +2,15 @@
 // This is a DWIM case, since renaming the export probably means the intent is also different.
 // For the de-duplication case of exactly the same name, see reexport-dedup
 
+//@ aux-crate:equivalent=equivalent.rs
+//@ compile-flags: --extern equivalent
+//@ aux-build:equivalent.rs
+//@ build-aux-docs
+#[doc(inline)]
+pub extern crate equivalent;
+#[doc(inline)]
+pub use equivalent::Equivalent as NotEquivalent;
+
 pub mod fmt {
     pub struct Subscriber;
 }
