c3ed253040b4ea2dcb78cd1c1b9ea87eda2ba7d8
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-12-12 17:49:25 +0100

    don't run container as root

--- src/ci/docker/run.sh
+++ src/ci/docker/run.sh
@@ -282,8 +282,12 @@ else
     # LOCAL_USER_ID will map to a different subuid range on the host.
     # The "keep-id" mode maps the current UID directly into the container.
     args="$args --env NO_CHANGE_USER=1 --userns=keep-id"
-  else
+  else if [[ "$id" != 0 ]]; then
     args="$args --env LOCAL_USER_ID=$id"
+  else
+    # If we're running as root, we don't want to run the container as root,
+    # so we set id `1001` instead of `0`.
+    args="$args --env LOCAL_USER_ID=1001"
   fi
 fi
 
