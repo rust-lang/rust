6280032ec7ff726d708e7cf2dda8ad9e36bcbe90
Santiago Pastorino <spastorino@gmail.com>
2024-12-17 12:27:02 -0300

    Generate the right MIR for by use closures

--- tests/ui/ergonomic-clones/closure.rs
+++ tests/ui/ergonomic-clones/closure.rs
@@ -3,9 +3,18 @@
 
 #![feature(ergonomic_clones)]
 
-fn ergonomic_clone_closure() -> i32 {
+fn ergonomic_clone_closure_no_captures() -> i32 {
     let cl = use || {
         1
+    };
+    cl()
+}
+
+fn ergonomic_clone_closure_with_captures() -> String {
+    let s = String::from("hi");
+
+    let cl = use || {
+        s
     };
     cl()
 }
@@ -14,7 +23,7 @@ fn ergonomic_clone_async_closures() -> String {
     let s = String::from("hi");
 
     async use {
-        22
+        s
     };
 
     s
