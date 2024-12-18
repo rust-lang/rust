47573bf61ec0807cee3cf2c5c1ca88bb7d5b89cd
Folkert de Vries <folkert@folkertdev.nl>
2024-12-18 22:03:07 +0100

    add `-Zmin-function-alignment`

--- compiler/rustc_session/src/config.rs
+++ compiler/rustc_session/src/config.rs
@@ -2891,6 +2891,7 @@ pub(crate) mod dep_tracking {
     use std::num::NonZero;
     use std::path::PathBuf;
 
+    use rustc_abi::Align;
     use rustc_data_structures::fx::FxIndexMap;
     use rustc_data_structures::stable_hasher::Hash64;
     use rustc_errors::LanguageIdentifier;
@@ -3011,6 +3012,7 @@ pub(crate) mod dep_tracking {
         InliningThreshold,
         FunctionReturn,
         WasmCAbi,
+        Align,
     );
 
     impl<T1, T2> DepTrackingHash for (T1, T2)
