a60e45f45d1b54120b439796681ae5cf8b70111d
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-19 16:09:50 +0000

    [WIP] Use weak linkage instead of compiler generated shims
    This is still keeping the allocator shim for the oom handler and the
    __rust_no_alloc_shim_is_unstable symbol for now.
    
    TODO: Update comments everywhere and test on macOS and Windows

--- compiler/rustc_codegen_ssa/src/traits/backend.rs
+++ compiler/rustc_codegen_ssa/src/traits/backend.rs
@@ -104,7 +104,6 @@ pub trait ExtraBackendMethods:
         &self,
         tcx: TyCtxt<'tcx>,
         module_name: &str,
-        kind: AllocatorKind,
         alloc_error_handler_kind: AllocatorKind,
     ) -> Self::Module;
 
