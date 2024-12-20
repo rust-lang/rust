8b2b6359f97ee9ac8fb1b5c35e86211222d02574
Ralf Jung <post@ralfj.de>
2024-12-20 15:03:51 +0100

    mri: add track_caller to thread spawning methods for better backtraces

--- library/std/src/sys/pal/unix/thread.rs
+++ library/std/src/sys/pal/unix/thread.rs
@@ -45,6 +45,7 @@ unsafe impl Sync for Thread {}
 
 impl Thread {
     // unsafe: see thread::Builder::spawn_unchecked for safety requirements
+    #[cfg_attr(miri, track_caller)] // even without panics, this helps for Miri backtraces
     pub unsafe fn new(stack: usize, p: Box<dyn FnOnce()>) -> io::Result<Thread> {
         let p = Box::into_raw(Box::new(p));
         let mut native: libc::pthread_t = mem::zeroed();
