5e3e5b999a6ef5773921a9a29cd7a234e9f38e37
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-12-20 17:27:15 +0100

    fix

--- src/ci/run.sh
+++ src/ci/run.sh
@@ -22,6 +22,9 @@ if [ "$NO_CHANGE_USER" = "" ]; then
     export HOME=/home/user
     unset LOCAL_USER_ID
 
+    # Give ownership of the current directory to the user
+    chown -R user .
+
     # Ensure that runners are able to execute git commands in the worktree,
     # overriding the typical git protections. In our docker container we're running
     # as root, while the user owning the checkout is not root.
