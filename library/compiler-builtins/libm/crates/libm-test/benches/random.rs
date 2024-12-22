ba1d271158fb73b71955680dea6280bfa753cf2b
Trevor Gross <tmgross@umich.edu>
2024-12-22 23:56:45 +0000

    Rename associated type helpers, add `OpITy`
    Change the names to make them less ambiguous. Additionally add `OpITy`
    for accessing the same-sized integer of an operation's float type.

--- library/compiler-builtins/libm/crates/libm-test/benches/random.rs
+++ library/compiler-builtins/libm/crates/libm-test/benches/random.rs
@@ -26,7 +26,7 @@ macro_rules! musl_rand_benches {
 
                 #[cfg(feature = "build-musl")]
                 let musl_extra = MuslExtra {
-                    musl_fn: Some(musl_math_sys::$fn_name as libm_test::CFn<Op>),
+                    musl_fn: Some(musl_math_sys::$fn_name as libm_test::OpCFn<Op>),
                     skip_on_i586: $skip_on_i586
                 };
 
