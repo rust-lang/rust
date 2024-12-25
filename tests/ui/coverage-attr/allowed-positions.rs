9124662da3e0d573703c9bb07d5e3cf8d06f1b20
Zalathar <Zalathar@users.noreply.github.com>
2024-12-25 13:29:20 +1100

    Expand the main test for where the coverage attribute is allowed
    Some of these cases are also implicitly checked by other tests, but it's
    helpful to also explicitly list them in the main test.

--- tests/ui/coverage-attr/allowed-positions.rs
+++ tests/ui/coverage-attr/allowed-positions.rs
@@ -8,35 +8,68 @@
 #![warn(unused_attributes)]
 #![coverage(off)]
 
+#[coverage(off)]
+mod submod {}
+
 #[coverage(off)] //~ ERROR [E0788]
-trait Trait {
+type MyTypeAlias = ();
+
+#[coverage(off)] //~ ERROR [E0788]
+trait MyTrait {
     #[coverage(off)] //~ ERROR [E0788]
-    const X: u32;
+    const TRAIT_ASSOC_CONST: u32;
 
     #[coverage(off)] //~ ERROR [E0788]
-    type T;
-
-    type U;
+    type TraitAssocType;
 
     #[coverage(off)] //~ ERROR [E0788]
-    fn f(&self);
+    fn trait_method(&self);
+
+    #[coverage(off)]
+    fn trait_method_with_default(&self) {}
 
     #[coverage(off)] //~ ERROR [E0788]
-    fn g();
+    fn trait_assoc_fn();
 }
 
 #[coverage(off)]
-impl Trait for () {
-    const X: u32 = 0;
+impl MyTrait for () {
+    const TRAIT_ASSOC_CONST: u32 = 0;
 
     #[coverage(off)] //~ ERROR [E0788]
-    type T = Self;
+    type TraitAssocType = Self;
 
+    #[coverage(off)]
+    fn trait_method(&self) {}
+    #[coverage(off)]
+    fn trait_method_with_default(&self) {}
+    #[coverage(off)]
+    fn trait_assoc_fn() {}
+}
+
+trait HasAssocType {
+    type T;
+    fn constrain_assoc_type() -> Self::T;
+}
+
+impl HasAssocType for () {
     #[coverage(off)] //~ ERROR [E0788]
-    type U = impl Trait; //~ ERROR unconstrained opaque type
+    type T = impl Copy;
+    fn constrain_assoc_type() -> Self::T {}
+}
 
-    fn f(&self) {}
-    fn g() {}
+#[coverage(off)] //~ ERROR [E0788]
+struct MyStruct {
+    #[coverage(off)] //~ ERROR [E0788]
+    field: u32,
+}
+
+#[coverage(off)]
+impl MyStruct {
+    #[coverage(off)]
+    fn method(&self) {}
+    #[coverage(off)]
+    fn assoc_fn() {}
 }
 
 extern "C" {
@@ -45,12 +78,33 @@ extern "C" {
 
     #[coverage(off)] //~ ERROR [E0788]
     type T;
+
+    #[coverage(off)] //~ ERROR [E0788]
+    fn foreign_fn();
 }
 
 #[coverage(off)]
 fn main() {
     #[coverage(off)] //~ ERROR [E0788]
     let _ = ();
+
+    // Currently not allowed on let statements, even if they bind to a closure.
+    // It might be nice to support this as a special case someday, but trying
+    // to define the precise boundaries of that special case might be tricky.
+    #[coverage(off)] //~ ERROR [E0788]
+    let _let_closure = || ();
+
+    // In situations where attributes can already be applied to expressions,
+    // the coverage attribute is allowed on closure expressions.
+    let _closure_tail_expr = {
+        #[coverage(off)]
+        || ()
+    };
+
+    // Applying attributes to arbitrary expressions requires an unstable
+    // feature, but if that feature were enabled then this would be allowed.
+    let _closure_expr = #[coverage(off)] || ();
+    //~^ ERROR attributes on expressions are experimental [E0658]
 
     match () {
         #[coverage(off)] //~ ERROR [E0788]
