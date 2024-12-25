335f7f59c1a4ffa0483f936a905b25f2f58c0263
Ralf Jung <post@ralfj.de>
2024-12-25 15:58:57 +0100

    swap_typed_nonoverlapping: properly detect overlap even when swapping scalar values

--- src/tools/miri/tests/fail/intrinsics/typed-swap-invalid-scalar.rs
+++ src/tools/miri/tests/fail/intrinsics/typed-swap-invalid-scalar.rs
@@ -1,3 +1,4 @@
+//@revisions: left right
 #![feature(core_intrinsics)]
 #![feature(rustc_attrs)]
 
@@ -5,8 +6,9 @@ use std::intrinsics::typed_swap_nonoverlapping;
 use std::ptr::addr_of_mut;
 
 fn invalid_scalar() {
-    let mut a = 1_u8;
-    let mut b = 2_u8;
+    // We run the test twice, with either the left or the right side being invalid.
+    let mut a = if cfg!(left) { 2_u8} else { 1_u8 };
+    let mut b = if cfg!(right) { 3_u8} else { 1_u8 };
     unsafe {
         let a = addr_of_mut!(a).cast::<bool>();
         let b = addr_of_mut!(b).cast::<bool>();
--- src/tools/miri/tests/fail/intrinsics/typed-swap-overlap.rs
+++ src/tools/miri/tests/fail/intrinsics/typed-swap-overlap.rs
@@ -5,7 +5,7 @@ use std::intrinsics::typed_swap_nonoverlapping;
 use std::ptr::addr_of_mut;
 
 fn main() {
-    let mut a = [0_u8; 100];
+    let mut a = 0_u8;
     unsafe {
         let a = addr_of_mut!(a);
         typed_swap_nonoverlapping(a, a); //~ERROR: called on overlapping ranges
