e972aacac72410ca7902f5fe29de1784f1f33f0e
jyn <github@jyn.dev>
2024-12-29 18:08:25 -0500

    update for test directive rename; split out comparison code

--- tests/ui/backtrace/std-backtrace-skip-frames.rs
+++ tests/ui/backtrace/std-backtrace-skip-frames.rs
@@ -1,13 +1,7 @@
 /* This tests a lot of fiddly platform-specific impl details.
-   It will probably flake a lot for a while. Feel free to split it up into
-   different tests or add more `ignore-` directives. If at all possible, rather
-   than removing the assert that the different revisions behave the same, move
-   only the revisions that are failing into a separate test, so that the rest
-   are still kept the same.
-
-    TODO: this test is megacursed, either split it into a separate run-make test
-    that looks at the .stderr files or get `compare-mode` working
- */
+  It will probably flake a lot for a while. Feel free to split it up into
+  different tests or add more `ignore-` directives.
+*/
 
 //@ ignore-android FIXME #17520
 //@ ignore-wasm32 spawning processes is not supported
@@ -18,13 +12,11 @@
 
 //@ run-pass
 //@ check-run-results
-//@ normalize-stderr-test: "omitted [0-9]+ frames" -> "omitted N frames"
-//@ normalize-stderr-test: ".rs:[0-9]+:[0-9]+" -> ".rs:LL:CC"
+//@ normalize-stderr: "omitted [0-9]+ frames" -> "omitted N frames"
+//@ normalize-stderr: ".rs:[0-9]+:[0-9]+" -> ".rs:LL:CC"
 //@ error-pattern:stack backtrace:
 //@ regex-error-pattern:omitted [0-9]+ frames
 //@ error-pattern:main
-// NOTE: if this is missing it's probably because the check that .stderr files match failed.
-//@ error-pattern:finished all checks
 
 //@ exec-env:RUST_BACKTRACE=1
 //@ unset-exec-env:RUST_LIB_BACKTRACE
@@ -42,63 +34,20 @@
 //@[limited] compile-flags:-Cdebuginfo=limited
 //@[full] compile-flags:-Cdebuginfo=full
 
-use std::collections::BTreeMap;
-use std::env;
-use std::path::Path;
-
 fn main() {
-    // Make sure this comes first. Otherwise the error message when the check
-    // below fails prevents you from using --bless to see the actual output.
-    std::panic::catch_unwind(|| check_all_panics()).unwrap_err();
-
-    // compiletest generates a bunch of files for each revision. make sure they're all the same.
-    let mut files = BTreeMap::new();
-    let dir = Path::new(env!("SOURCE_DIR")).join("backtrace");
-    for file in std::fs::read_dir(dir).unwrap() {
-        let file = file.unwrap();
-        let name = file.file_name().into_string().unwrap();
-        if !file.file_type().unwrap().is_file() || !name.starts_with("std-backtrace-skip-frames.")
-                || !name.ends_with(".run.stderr") {
-            continue;
-        }
-        files.insert(name, std::fs::read_to_string(file.path()).unwrap());
-    }
-
-    let mut first_line_tables = None;
-    let mut first_full = None;
-
-    for (name, contents) in &files {
-        // These have different output. Rather than duplicating this whole test,
-        // just special-case them here.
-        let target = if name.contains(".full.") || name.contains(".limited.") {
-            &mut first_full
-        } else {
-            &mut first_line_tables
-        };
-        if let Some((target_name, target_contents)) = target {
-            if contents != *target_contents {
-                eprintln!("are you *sure* that you want {name} to have different backtrace output\
-                           than {target_name}?");
-                eprintln!("NOTE: this test is stateful; run \
-                           `rm tests/ui/backtrace/std-backtrace-skip-frames.*.stderr` to reset it");
-                std::process::exit(0);
-            }
-        } else {
-            // compiletest doesn't support negative matching for `error-pattern`. Do that here.
-            assert!(!contents.contains("FnOnce::call_once"));
-            *target = Some((name, contents));
-        }
-    }
-
-    // We need this so people don't --bless away the assertion failure by accident.
-    eprintln!("finished all checks");
+    check_all_panics();
 }
 
 fn check_all_panics() {
     // Spawn a bunch of threads and make sure all of them hide panic details we don't care about.
     let tests = [
-        unwrap_result, expect_result, unwrap_option, expect_option, explicit_panic, literal_panic,
-         /*panic_nounwind*/
+        unwrap_result,
+        expect_result,
+        unwrap_option,
+        expect_option,
+        explicit_panic,
+        literal_panic,
+        /*panic_nounwind*/
     ];
     for func in tests {
         std::thread::spawn(move || func()).join().unwrap_err();
@@ -106,7 +55,7 @@ fn check_all_panics() {
     std::thread::spawn(|| panic_fmt(3)).join().unwrap_err();
 
     // Finally, panic ourselves so we can make sure `lang_start`, etc. frames are hidden.
-    panic!();
+    std::panic::catch_unwind(|| panic!()).unwrap_err();
 }
 
 fn unwrap_result() {
@@ -141,5 +90,7 @@ fn panic_fmt(x: u32) {
 // TODO: separate test
 #[allow(dead_code)]
 fn panic_nounwind() {
-    unsafe { [0].get_unchecked(1); }
+    unsafe {
+        [0].get_unchecked(1);
+    }
 }
