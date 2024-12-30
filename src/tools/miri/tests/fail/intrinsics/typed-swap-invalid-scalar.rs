3c0c1386878812780349be38080470c0b4fcdda2
Ralf Jung <post@ralfj.de>
2024-12-30 19:30:08 +0100

    fmt
    Co-authored-by: scottmcm <scottmcm@users.noreply.github.com>

--- src/tools/miri/tests/fail/intrinsics/typed-swap-invalid-scalar.rs
+++ src/tools/miri/tests/fail/intrinsics/typed-swap-invalid-scalar.rs
@@ -7,8 +7,8 @@ use std::ptr::addr_of_mut;
 
 fn invalid_scalar() {
     // We run the test twice, with either the left or the right side being invalid.
-    let mut a = if cfg!(left) { 2_u8} else { 1_u8 };
-    let mut b = if cfg!(right) { 3_u8} else { 1_u8 };
+    let mut a = if cfg!(left) { 2_u8 } else { 1_u8 };
+    let mut b = if cfg!(right) { 3_u8 } else { 1_u8 };
     unsafe {
         let a = addr_of_mut!(a).cast::<bool>();
         let b = addr_of_mut!(b).cast::<bool>();
