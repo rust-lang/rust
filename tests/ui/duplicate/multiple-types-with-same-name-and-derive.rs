bb16267a65f00defdc5d484375c59f9d93dfa50e
dxsullivan <193140725+dxsullivan@users.noreply.github.com>
2024-12-31 23:46:39 +0800

    chore: fix typos
    Signed-off-by: dxsullivan <193140725+dxsullivan@users.noreply.github.com>

--- tests/ui/duplicate/multiple-types-with-same-name-and-derive.rs
+++ tests/ui/duplicate/multiple-types-with-same-name-and-derive.rs
@@ -1,6 +1,6 @@
 // Here, there are two types with the same name. One of these has a `derive` annotation, but in the
 // expansion these `impl`s are associated to the the *other* type. There is a suggestion to remove
-// unneded type parameters, but because we're now point at a type with no type parameters, the
+// unneeded type parameters, but because we're now point at a type with no type parameters, the
 // suggestion would suggest removing code from an empty span, which would ICE in nightly.
 //
 // issue: rust-lang/rust#108748
