4057797a3fccf8df17367630b98832eecca22260
Shunpoco <tkngsnsk313320@gmail.com>
2025-01-04 11:19:29 +0000

    Add FileCheck to cycle.rs
    Signed-off-by: Shunpoco <tkngsnsk313320@gmail.com>

--- tests/mir-opt/copy-prop/cycle.rs
+++ tests/mir-opt/copy-prop/cycle.rs
@@ -1,4 +1,3 @@
-// skip-filecheck
 // EMIT_MIR_FOR_EACH_PANIC_STRATEGY
 //! Tests that cyclic assignments don't hang CopyProp, and result in reasonable code.
 //@ test-mir-pass: CopyProp
@@ -8,6 +7,16 @@ fn val() -> i32 {
 
 // EMIT_MIR cycle.main.CopyProp.diff
 fn main() {
+    // CHECK-LABEL: fn main(
+    // CHECK: debug z => _2;
+    // CHECK-NOT: StorageLive(_2);
+    // CHECK: _2 = copy _1;
+    // CHECK-NOT: StorageLive(_3);
+    // CHECK-NOT: _3 = copy _2;
+    // CHECK-NOT: StorageLive(_4);
+    // CHECK-NOT: _4 = copy _3;
+    // CHECK-NOT: _1 = move _4;
+    // CHECK: _1 = copy _2;
     let mut x = val();
     let y = x;
     let z = y;
