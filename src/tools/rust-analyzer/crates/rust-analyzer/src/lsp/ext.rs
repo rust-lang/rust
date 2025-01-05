0389235a15ab0a210db1f3eed04ddbafe01d9bcc
Lukas Wirth <lukastw97@gmail.com>
2025-01-05 16:18:20 +0100

    fix: Be more permissive with completion resolve data

--- src/tools/rust-analyzer/crates/rust-analyzer/src/lsp/ext.rs
+++ src/tools/rust-analyzer/crates/rust-analyzer/src/lsp/ext.rs
@@ -1,5 +1,9 @@
 //! rust-analyzer extensions to the LSP.
 
+// Note when adding new resolve payloads, add a #[serde(default)] on boolean fields as some clients
+// might strip `false` values from the JSON payload due to their reserialization logic turning false
+// into null which will then cause them to be omitted in the resolve request. See https://github.com/rust-lang/rust-analyzer/issues/18767
+
 #![allow(clippy::disallowed_types)]
 
 use std::ops;
@@ -829,6 +833,7 @@ pub struct CompletionResolveData {
     pub version: Option<i32>,
     #[serde(skip_serializing_if = "Option::is_none", default)]
     pub trigger_character: Option<char>,
+    #[serde(default)]
     pub for_ref: bool,
     pub hash: String,
 }
