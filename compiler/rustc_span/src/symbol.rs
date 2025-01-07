3069aa6b4d959c081a7e9b37d66e6c45a2113060
Ben Kimock <kimockb@gmail.com>
2025-01-07 11:10:54 -0500

    Implement inline(usually)

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -2244,6 +2244,7 @@ symbols! {
         usize_legacy_fn_max_value,
         usize_legacy_fn_min_value,
         usize_legacy_mod,
+        usually,
         v8plus,
         va_arg,
         va_copy,
