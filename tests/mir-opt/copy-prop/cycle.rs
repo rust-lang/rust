30a656a9bd80a611305f956695042e68c8da279e
Shunpoco <tkngsnsk313320@gmail.com>
2025-01-08 22:09:18 +0000

    address review: add CHECKs in cycle.rs
    Signed-off-by: Shunpoco <tkngsnsk313320@gmail.com>

--- tests/mir-opt/copy-prop/cycle.rs
+++ tests/mir-opt/copy-prop/cycle.rs
@@ -8,15 +8,17 @@ fn val() -> i32 {
 // EMIT_MIR cycle.main.CopyProp.diff
 fn main() {
     // CHECK-LABEL: fn main(
-    // CHECK: debug z => _2;
-    // CHECK-NOT: StorageLive(_2);
-    // CHECK: _2 = copy _1;
+    // CHECK: debug x => [[x:_.*]];
+    // CHECK: debug y => [[y:_.*]];
+    // CHECK: debug z => [[y]];
+    // CHECK-NOT: StorageLive([[y]]);
+    // CHECK: [[y]] = copy [[x]];
     // CHECK-NOT: StorageLive(_3);
-    // CHECK-NOT: _3 = copy _2;
+    // CHECK-NOT: _3 = copy [[y]];
     // CHECK-NOT: StorageLive(_4);
     // CHECK-NOT: _4 = copy _3;
     // CHECK-NOT: _1 = move _4;
-    // CHECK: _1 = copy _2;
+    // CHECK: [[x]] = copy [[y]];
     let mut x = val();
     let y = x;
     let z = y;
