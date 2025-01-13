10d745c7f7f6dbff7feabc00269c7bcee4c9d680
Antoni Boucher <bouanto@zoho.com>
2025-01-13 08:53:59 -0500

    Do not run UI tests in the error-emitter directory

--- build_system/src/test.rs
+++ build_system/src/test.rs
@@ -876,8 +876,13 @@ fn contains_ui_error_patterns(file_path: &Path, keep_lto_tests: bool) -> Result<
             return Ok(true);
         }
     }
-    if file_path.display().to_string().contains("ambiguous-4-extern.rs") {
+    let file_path = file_path.display().to_string();
+    if file_path.contains("ambiguous-4-extern.rs") {
         eprintln!("nothing found for {file_path:?}");
+    }
+    // The files in this directory contain errors.
+    if file_path.contains("/error-emitter/") {
+        return Ok(true);
     }
     Ok(false)
 }
