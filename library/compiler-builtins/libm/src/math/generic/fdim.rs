bfbe919adfae9ce1b0dc17260db2dba097c3c37c
quaternic <57393910+quaternic@users.noreply.github.com>
2025-01-14 03:55:26 +0200

    Simplify and optimize `fdim` (#442)
    The cases with NaN arguments can be handled by the same x - y
    expression, and this generates much better code: https://godbolt.org/z/f3rnT8jx4.

--- library/compiler-builtins/libm/src/math/generic/fdim.rs
+++ library/compiler-builtins/libm/src/math/generic/fdim.rs
@@ -1,13 +1,5 @@
 use super::super::Float;
 
 pub fn fdim<F: Float>(x: F, y: F) -> F {
-    if x.is_nan() {
-        x
-    } else if y.is_nan() {
-        y
-    } else if x > y {
-        x - y
-    } else {
-        F::ZERO
-    }
+    if x <= y { F::ZERO } else { x - y }
 }
