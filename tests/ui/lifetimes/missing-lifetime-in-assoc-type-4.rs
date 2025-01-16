8ba2950fb6ebb3f5e7cf10afb79dda6f5ed61bba
Esteban Küber <esteban@kuber.com.ar>
2025-01-16 21:02:23 +0000

    Detect case of missing lifetime in assoc type
    When an associated type is missing a lifetime, point at its enclosing `impl`, whether it has or doesn't have lifetimes defined. If it does have a lifetime, suggest using it.
    
    ```
    error: in the trait associated type is declared without lifetime parameters, so using a borrowed type for them requires that lifetime to come from the implemented type
      --> $DIR/missing-lifetime-in-assoc-type-1.rs:8:17
       |
    LL | impl<'a> IntoIterator for &S {
       |     ---- there is a named lifetime specified on the impl block you could use
    ...
    LL |     type Item = &T;
       |                 ^ this lifetime must come from the implemented type
       |
    help: consider using the lifetime from the impl block
       |
    LL |     type Item = &'a T;
       |                  ++
    ```
    
    ```
    error: in the trait associated type is declared without lifetime parameters, so using a borrowed type for them requires that lifetime to come from the implemented type
      --> $DIR/missing-lifetime-in-assoc-type-2.rs:5:17
       |
    LL | impl IntoIterator for &S {
       |     - you could add a lifetime on the impl block, if the trait or the self type can have one
    LL |     type Item = &T;
       |                 ^ this lifetime must come from the implemented type
    ```

--- tests/ui/lifetimes/missing-lifetime-in-assoc-type-4.rs
+++ tests/ui/lifetimes/missing-lifetime-in-assoc-type-4.rs
@@ -5,7 +5,7 @@ impl IntoIterator for &S {
     type Item = &T;
     //~^ ERROR in the trait associated type
     type IntoIter<'a> = std::collections::btree_map::Values<'a, i32, T>;
-    //~^ ERROR lifetime parameters or bounds on type `IntoIter` do not match the trait declaration
+    //~^ ERROR lifetime parameters or bounds on associated type `IntoIter` do not match the trait declaration
 
     fn into_iter(self) -> Self::IntoIter {
         todo!()
