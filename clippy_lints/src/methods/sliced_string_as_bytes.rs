4e94d222912c2a36966ae845e8fd54e073896266
wowinter13 <vla-dy@yandex.ru>
2025-01-16 16:28:59 +0100

    nit: change placeholders

--- clippy_lints/src/methods/sliced_string_as_bytes.rs
+++ clippy_lints/src/methods/sliced_string_as_bytes.rs
@@ -14,8 +14,8 @@ pub(super) fn check(cx: &LateContext<'_>, expr: &Expr<'_>, recv: &Expr<'_>) {
         && (ty.is_str() || is_type_lang_item(cx, ty, LangItem::String))
     {
         let mut applicability = Applicability::MaybeIncorrect;
-        let stringish = snippet_with_applicability(cx, indexed.span, "..", &mut applicability);
-        let range = snippet_with_applicability(cx, index.span, "..", &mut applicability);
+        let stringish = snippet_with_applicability(cx, indexed.span, "_", &mut applicability);
+        let range = snippet_with_applicability(cx, index.span, "_", &mut applicability);
         span_lint_and_sugg(
             cx,
             SLICED_STRING_AS_BYTES,
