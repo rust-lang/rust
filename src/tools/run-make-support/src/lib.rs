9d6a1c980e7de879d6eb64c281aac3cdfdefce3f
jyn <github@jyn.dev>
2025-01-18 16:55:31 -0500

    Shorten linker output even more when `--verbose` is not present
    - Don't show environment variables. Seeing PATH is almost never useful, and it can be extremely long.
    - For .rlibs in the sysroot, replace crate hashes with a `"-*"` string. This will expand to the full crate name when pasted into the shell.
    - Move `.rlib` to outside the glob.
    - Abbreviate the sysroot path to `<sysroot>` wherever it appears in the arguments.
    
    This also adds an example of the linker output as a run-make test. Currently it only runs on x86_64-unknown-linux-gnu, because each platform has its own linker arguments. So that it's stable across machines, pass BUILD_ROOT as an argument through compiletest through to run-make tests.

--- src/tools/run-make-support/src/lib.rs
+++ src/tools/run-make-support/src/lib.rs
@@ -90,7 +90,7 @@ pub use artifact_names::{
 /// Path-related helpers.
 pub use path_helpers::{
     cwd, filename_contains, filename_not_in_denylist, has_extension, has_prefix, has_suffix,
-    not_contains, path, shallow_find_files, source_root,
+    not_contains, path, shallow_find_files, build_root, source_root,
 };
 
 /// Helpers for scoped test execution where certain properties are attempted to be maintained.
