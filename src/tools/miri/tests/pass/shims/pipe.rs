544695506c1d85628d39856ae912cbfeb112021d
Ralf Jung <post@ralfj.de>
2025-01-19 09:54:29 -0700

    fix location of pipe module

--- src/tools/miri/tests/pass/shims/pipe.rs
+++ src/tools/miri/tests/pass/shims/pipe.rs
@@ -1,10 +1,11 @@
 //@ignore-target: windows
 
 #![feature(anonymous_pipe)]
-use std::io::{Read, Write};
+
+use std::io::{Read, Write, pipe};
 
 fn main() {
-    let (mut ping_rx, mut ping_tx) = std::pipe::pipe().unwrap();
+    let (mut ping_rx, mut ping_tx) = pipe().unwrap();
     ping_tx.write(b"hello").unwrap();
     let mut buf: [u8; 5] = [0; 5];
     ping_rx.read(&mut buf).unwrap();
