950229cd7d96a69540604225674a79b77519b162
Folkert de Vries <folkert@folkertdev.nl>
2025-01-19 18:37:57 +0100

    add `vec_sub` and `vec_mul` for s390x

--- library/stdarch/crates/core_arch/src/s390x/vector.rs
+++ library/stdarch/crates/core_arch/src/s390x/vector.rs
@@ -146,9 +146,145 @@ mod sealed {
             #[inline]
             #[target_feature(enable = "vector")]
             unsafe fn vec_add(self, other: Self) -> Self::Result {
-                simd_add(self, other)
+                va_float(self, other)
             }
         }
+    }
+
+    #[unstable(feature = "stdarch_s390x", issue = "135681")]
+    pub trait VectorSub<Other> {
+        type Result;
+        unsafe fn vec_sub(self, other: Other) -> Self::Result;
+    }
+
+    macro_rules! impl_sub {
+        ($name:ident, $a:ty, $instr:ident) => {
+            impl_sub!($name, $a, $a, $a, $instr);
+        };
+        ($name:ident, $a:ty, $b:ty, $c:ty, $instr:ident) => {
+            #[inline]
+            #[target_feature(enable = "vector")]
+            #[cfg_attr(test, assert_instr($instr))]
+            pub unsafe fn $name(a: $a, b: $b) -> $c {
+                transmute(simd_sub(transmute(a), b))
+            }
+
+            #[unstable(feature = "stdarch_s390x", issue = "135681")]
+            impl VectorSub<$b> for $a {
+                type Result = $c;
+
+                #[inline]
+                #[target_feature(enable = "vector")]
+                unsafe fn vec_sub(self, other: $b) -> Self::Result {
+                    $name(self, other)
+                }
+            }
+        };
+    }
+
+    #[rustfmt::skip]
+    mod impl_sub {
+        use super::*;
+
+        impl_sub!(vs_sc, vector_signed_char, vsb);
+        impl_sub!(vs_uc, vector_unsigned_char, vsb);
+        impl_sub!(vs_sh, vector_signed_short, vsh);
+        impl_sub!(vs_uh, vector_unsigned_short, vsh);
+        impl_sub!(vs_sf, vector_signed_int, vsf);
+        impl_sub!(vs_uf, vector_unsigned_int, vsf);
+        impl_sub!(vs_sg, vector_signed_long_long, vsg);
+        impl_sub!(vs_ug, vector_unsigned_long_long, vsg);
+
+        impl_sub!(vs_sc_bc, vector_signed_char, vector_bool_char, vector_signed_char, vsb);
+        impl_sub!(vs_uc_bc, vector_unsigned_char, vector_bool_char, vector_unsigned_char, vsb);
+        impl_sub!(vs_sh_bh, vector_signed_short, vector_bool_short, vector_signed_short, vsh);
+        impl_sub!(vs_uh_bh, vector_unsigned_short, vector_bool_short, vector_unsigned_short, vsh);
+        impl_sub!(vs_sf_bf, vector_signed_int, vector_bool_int, vector_signed_int, vsf);
+        impl_sub!(vs_uf_bf, vector_unsigned_int, vector_bool_int, vector_unsigned_int, vsf);
+        impl_sub!(vs_sg_bg, vector_signed_long_long, vector_bool_long_long, vector_signed_long_long, vsg);
+        impl_sub!(vs_ug_bg, vector_unsigned_long_long, vector_bool_long_long, vector_unsigned_long_long, vsg);
+
+        impl_sub!(vs_bc_sc, vector_bool_char, vector_signed_char, vector_signed_char, vsb);
+        impl_sub!(vs_bc_uc, vector_bool_char, vector_unsigned_char, vector_unsigned_char, vsb);
+        impl_sub!(vs_bh_sh, vector_bool_short, vector_signed_short, vector_signed_short, vsh);
+        impl_sub!(vs_bh_uh, vector_bool_short, vector_unsigned_short, vector_unsigned_short, vsh);
+        impl_sub!(vs_bf_sf, vector_bool_int, vector_signed_int, vector_signed_int, vsf);
+        impl_sub!(vs_bf_uf, vector_bool_int, vector_unsigned_int, vector_unsigned_int, vsf);
+        impl_sub!(vs_bg_sg, vector_bool_long_long, vector_signed_long_long, vector_signed_long_long, vsg);
+        impl_sub!(vs_bg_ug, vector_bool_long_long, vector_unsigned_long_long, vector_unsigned_long_long, vsg);
+
+        impl_sub!(vs_double, vector_double, vfsdb);
+
+        #[inline]
+        #[target_feature(enable = "vector")]
+        // FIXME: "vfssb" is part of vector enhancements 1, add a test for it when possible
+        // #[cfg_attr(test, assert_instr(vfasb))]
+        pub unsafe fn vs_float(a: vector_float, b: vector_float) -> vector_float {
+            transmute(simd_sub(a, b))
+        }
+
+        #[unstable(feature = "stdarch_s390x", issue = "135681")]
+        impl VectorSub<Self> for vector_float {
+            type Result = Self;
+
+            #[inline]
+            #[target_feature(enable = "vector")]
+            unsafe fn vec_sub(self, other: Self) -> Self::Result {
+                vs_float(self, other)
+            }
+        }
+    }
+
+    #[unstable(feature = "stdarch_s390x", issue = "135681")]
+    pub trait VectorMul {
+        unsafe fn vec_mul(self, b: Self) -> Self;
+    }
+
+    macro_rules! impl_mul {
+        ($name:ident, $a:ty, std_simd) => {
+            #[unstable(feature = "stdarch_s390x", issue = "135681")]
+            impl VectorMul for $a {
+                #[inline]
+                #[target_feature(enable = "vector")]
+                unsafe fn vec_mul(self, other: Self) -> Self {
+                    transmute(simd_mul(transmute(self), other))
+                }
+            }
+        };
+        ($name:ident, $a:ty, $instr:ident) => {
+            #[inline]
+            #[target_feature(enable = "vector")]
+            #[cfg_attr(test, assert_instr($instr))]
+            pub unsafe fn $name(a: $a, b: $a) -> $a {
+                transmute(simd_mul(transmute(a), b))
+            }
+
+            #[unstable(feature = "stdarch_s390x", issue = "135681")]
+            impl VectorMul for $a {
+                #[inline]
+                #[target_feature(enable = "vector")]
+                unsafe fn vec_mul(self, other: Self) -> Self {
+                    $name(self, other)
+                }
+            }
+        };
+    }
+
+    #[rustfmt::skip]
+    mod impl_mul {
+        use super::*;
+
+        impl_mul!(vml_sc, vector_signed_char, vmlb);
+        impl_mul!(vml_uc, vector_unsigned_char, vmlb);
+        impl_mul!(vml_sh, vector_signed_short, vmlhw);
+        impl_mul!(vml_uh, vector_unsigned_short, vmlhw);
+        impl_mul!(vml_sf, vector_signed_int, vmlf);
+        impl_mul!(vml_uf, vector_unsigned_int, vmlf);
+        impl_mul!(vml_sg, vector_signed_long_long, std_simd);
+        impl_mul!(vml_ug, vector_unsigned_long_long, std_simd);
+
+        impl_mul!(vml_float, vector_float, std_simd);
+        impl_mul!(vml_double, vector_double, vfmdb);
     }
 }
 
@@ -163,6 +299,34 @@ where
     a.vec_add(b)
 }
 
+/// Vector pointwise subtraction.
+#[inline]
+#[target_feature(enable = "vector")]
+#[unstable(feature = "stdarch_s390x", issue = "135681")]
+pub unsafe fn vec_sub<T, U>(a: T, b: U) -> <T as sealed::VectorSub<U>>::Result
+where
+    T: sealed::VectorSub<U>,
+{
+    a.vec_sub(b)
+}
+
+/// Vector Multiply
+///
+/// ## Purpose
+/// Compute the products of corresponding elements of two vectors.
+///
+/// ## Result value
+/// Each element of r receives the product of the corresponding elements of a and b.
+#[inline]
+#[target_feature(enable = "vector")]
+#[unstable(feature = "stdarch_s390x", issue = "135681")]
+pub unsafe fn vec_mul<T>(a: T, b: T) -> T
+where
+    T: sealed::VectorMul,
+{
+    a.vec_mul(b)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -171,6 +335,33 @@ mod tests {
 
     use crate::core_arch::simd::*;
     use stdarch_test::simd_test;
+
+    macro_rules! test_vec_2 {
+        { $name: ident, $fn:ident, $ty: ident, [$($a:expr),+], [$($b:expr),+], [$($d:expr),+] } => {
+            test_vec_2! { $name, $fn, $ty -> $ty, [$($a),+], [$($b),+], [$($d),+] }
+        };
+        { $name: ident, $fn:ident, $ty: ident -> $ty_out: ident, [$($a:expr),+], [$($b:expr),+], [$($d:expr),+] } => {
+            #[simd_test(enable = "vector")]
+            unsafe fn $name() {
+                let a: s_t_l!($ty) = transmute($ty::new($($a),+));
+                let b: s_t_l!($ty) = transmute($ty::new($($b),+));
+
+                let d = $ty_out::new($($d),+);
+                let r : $ty_out = transmute($fn(a, b));
+                assert_eq!(d, r);
+            }
+         };
+         { $name: ident, $fn:ident, $ty: ident -> $ty_out: ident, [$($a:expr),+], [$($b:expr),+], $d:expr } => {
+            #[simd_test(enable = "vector")]
+            unsafe fn $name() {
+                let a: s_t_l!($ty) = transmute($ty::new($($a),+));
+                let b: s_t_l!($ty) = transmute($ty::new($($b),+));
+
+                let r : $ty_out = transmute($fn(a, b));
+                assert_eq!($d, r);
+            }
+         }
+   }
 
     #[simd_test(enable = "vector")]
     unsafe fn vec_add_i32x4_i32x4() {
@@ -181,4 +372,116 @@ mod tests {
         let z = vec_add(x, y);
         assert_eq!(i32x4::splat(5), transmute(z));
     }
+
+    macro_rules! test_vec_sub {
+        { $name: ident, $ty: ident, [$($a:expr),+], [$($b:expr),+], [$($d:expr),+] } => {
+            test_vec_2! {$name, vec_sub, $ty, [$($a),+], [$($b),+], [$($d),+] }
+        }
+    }
+
+    test_vec_sub! { test_vec_sub_f32x4, f32x4,
+    [-1.0, 0.0, 1.0, 2.0],
+    [2.0, 1.0, -1.0, -2.0],
+    [-3.0, -1.0, 2.0, 4.0] }
+
+    test_vec_sub! { test_vec_sub_f64x2, f64x2,
+    [-1.0, 0.0],
+    [2.0, 1.0],
+    [-3.0, -1.0] }
+
+    test_vec_sub! { test_vec_sub_i64x2, i64x2,
+    [-1, 0],
+    [2, 1],
+    [-3, -1] }
+
+    test_vec_sub! { test_vec_sub_u64x2, u64x2,
+    [0, 1],
+    [1, 0],
+    [u64::MAX, 1] }
+
+    test_vec_sub! { test_vec_sub_i32x4, i32x4,
+    [-1, 0, 1, 2],
+    [2, 1, -1, -2],
+    [-3, -1, 2, 4] }
+
+    test_vec_sub! { test_vec_sub_u32x4, u32x4,
+    [0, 0, 1, 2],
+    [2, 1, 0, 0],
+    [4294967294, 4294967295, 1, 2] }
+
+    test_vec_sub! { test_vec_sub_i16x8, i16x8,
+    [-1, 0, 1, 2, -1, 0, 1, 2],
+    [2, 1, -1, -2, 2, 1, -1, -2],
+    [-3, -1, 2, 4, -3, -1, 2, 4] }
+
+    test_vec_sub! { test_vec_sub_u16x8, u16x8,
+    [0, 0, 1, 2, 0, 0, 1, 2],
+    [2, 1, 0, 0, 2, 1, 0, 0],
+    [65534, 65535, 1, 2, 65534, 65535, 1, 2] }
+
+    test_vec_sub! { test_vec_sub_i8x16, i8x16,
+    [-1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2],
+    [2, 1, -1, -2, 2, 1, -1, -2, 2, 1, -1, -2, 2, 1, -1, -2],
+    [-3, -1, 2, 4, -3, -1, 2, 4, -3, -1, 2, 4, -3, -1, 2, 4] }
+
+    test_vec_sub! { test_vec_sub_u8x16, u8x16,
+    [0, 0, 1, 2, 0, 0, 1, 2, 0, 0, 1, 2, 0, 0, 1, 2],
+    [2, 1, 0, 0, 2, 1, 0, 0, 2, 1, 0, 0, 2, 1, 0, 0],
+    [254, 255, 1, 2, 254, 255, 1, 2, 254, 255, 1, 2, 254, 255, 1, 2] }
+
+    macro_rules! test_vec_mul {
+        { $name: ident, $ty: ident, [$($a:expr),+], [$($b:expr),+], [$($d:expr),+] } => {
+            test_vec_2! {$name, vec_mul, $ty, [$($a),+], [$($b),+], [$($d),+] }
+        }
+    }
+
+    test_vec_mul! { test_vec_mul_f32x4, f32x4,
+    [-1.0, 0.0, 1.0, 2.0],
+    [2.0, 1.0, -1.0, -2.0],
+    [-2.0, 0.0, -1.0, -4.0] }
+
+    test_vec_mul! { test_vec_mul_f64x2, f64x2,
+    [-1.0, 0.0],
+    [2.0, 1.0],
+    [-2.0, 0.0] }
+
+    test_vec_mul! { test_vec_mul_i64x2, i64x2,
+    [i64::MAX, -4],
+    [2, 3],
+    [i64::MAX.wrapping_mul(2), -12] }
+
+    test_vec_mul! { test_vec_mul_u64x2, u64x2,
+    [u64::MAX, 4],
+    [2, 3],
+    [u64::MAX.wrapping_mul(2), 12] }
+
+    test_vec_mul! { test_vec_mul_i32x4, i32x4,
+    [-1, 0, 1, 2],
+    [2, 1, -1, -2],
+    [-2, 0, -1, -4] }
+
+    test_vec_mul! { test_vec_mul_u32x4, u32x4,
+    [0, u32::MAX - 1, 1, 2],
+    [5, 6, 7, 8],
+    [0, 4294967284, 7, 16] }
+
+    test_vec_mul! { test_vec_mul_i16x8, i16x8,
+    [-1, 0, 1, 2, -1, 0, 1, 2],
+    [2, 1, -1, -2, 2, 1, -1, -2],
+    [-2, 0, -1, -4, -2, 0, -1, -4] }
+
+    test_vec_mul! { test_vec_mul_u16x8, u16x8,
+    [0, u16::MAX - 1, 1, 2, 3, 4, 5, 6],
+    [5, 6, 7, 8, 9, 8, 7, 6],
+    [0, 65524, 7, 16, 27, 32, 35, 36] }
+
+    test_vec_mul! { test_vec_mul_i8x16, i8x16,
+    [-1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2, -1, 0, 1, 2],
+    [2, 1, -1, -2, 2, 1, -1, -2, 2, 1, -1, -2, 2, 1, -1, -2],
+    [-2, 0, -1, -4, -2, 0, -1, -4, -2, 0, -1, -4, -2, 0, -1, -4] }
+
+    test_vec_mul! { test_vec_mul_u8x16, u8x16,
+    [0, u8::MAX - 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4],
+    [5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 0, u8::MAX, 1, 2, 3, 4],
+    [0, 244, 7, 16, 27, 32, 35, 36, 35, 32, 0, 248, 7, 12, 15, 16] }
 }
