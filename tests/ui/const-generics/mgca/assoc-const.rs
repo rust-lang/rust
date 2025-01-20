dd2819ffc93e13fdf862c7bc4092e03ee5d43f59
Noah Lev <camelidcamel@gmail.com>
2025-01-19 21:46:10 -0800

    Allow const paths with generics/qself under mgca

--- tests/ui/const-generics/mgca/assoc-const.rs
+++ tests/ui/const-generics/mgca/assoc-const.rs
@@ -3,12 +3,12 @@
 #![feature(min_generic_const_args)]
 #![allow(incomplete_features)]
 
-pub trait Tr {
+pub trait Tr<X> {
     #[type_const]
     const SIZE: usize;
 }
 
-fn mk_array<T: Tr>(_x: T) -> [(); T::SIZE] {
+fn mk_array<T: Tr<bool>>(_x: T) -> [(); <T as Tr<bool>>::SIZE] {
     [(); T::SIZE]
 }
 
