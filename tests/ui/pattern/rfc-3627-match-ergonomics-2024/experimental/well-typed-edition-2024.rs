f5567e1132c1dce184f34313715e3f22dc6a8e04
dianne <diannes.gm@gmail.com>
2025-01-21 02:19:27 -0800

    organize old well-typed-edition-2024 tests
    This doesn't (or at least shouldn't!) add, remove, or change any test
    cases. I've grouped them by which rule variants they test.

--- tests/ui/pattern/rfc-3627-match-ergonomics-2024/experimental/well-typed-edition-2024.rs
+++ tests/ui/pattern/rfc-3627-match-ergonomics-2024/experimental/well-typed-edition-2024.rs
@@ -11,16 +11,30 @@
 #![cfg_attr(structural2024, feature(ref_pat_eat_one_layer_2024_structural))]
 
 pub fn main() {
-    if let Some(Some(&x)) = &Some(&Some(0)) {
-        //[stable2021]~^ mismatched types
-        //[stable2021]~| expected integer, found `&_`
-        let _: u32 = x;
+    // Tests not using match ergonomics. These should always succeed with the same bindings.
+    if let Some(&Some(&mut ref x)) = Some(&Some(&mut 0)) {
+        let _: &u32 = x;
     }
+
+    // Tests for differences in how many layers of reference are eaten by reference patterns
     if let Some(Some(&x)) = &Some(Some(&0)) {
         #[cfg(stable2021)] let _: u32 = x;
         #[cfg(any(classic2024, structural2024))] let _: &u32 = x;
     }
+    if let Some(&Some(x)) = &mut Some(&Some(0)) {
+        // This additionally tests that `&` patterns can eat inherited `&mut` refs.
+        // This is possible on stable when the real reference being eaten is of a `&` type.
+        #[cfg(stable2021)] let _: u32 = x;
+        #[cfg(any(classic2024, structural2024))] let _: &u32 = x;
+    }
     if let Some(Some(&&x)) = &Some(Some(&0)) {
+        //[stable2021]~^ mismatched types
+        //[stable2021]~| expected integer, found `&_`
+        let _: u32 = x;
+    }
+
+    // Tests for eating a lone inherited reference
+    if let Some(Some(&x)) = &Some(&Some(0)) {
         //[stable2021]~^ mismatched types
         //[stable2021]~| expected integer, found `&_`
         let _: u32 = x;
@@ -35,41 +49,42 @@ pub fn main() {
         //[stable2021]~| expected integer, found `&mut _`
         let _: u32 = x;
     }
-    if let Some(&Some(&x)) = &mut Some(&Some(0)) {
-        //[stable2021]~^ mismatched types
-        //[stable2021]~| expected integer, found `&_`
-        let _: u32 = x;
-    }
-    if let Some(&Some(x)) = &mut Some(&Some(0)) {
-        #[cfg(stable2021)] let _: u32 = x;
-        #[cfg(any(classic2024, structural2024))] let _: &u32 = x;
-    }
-    if let Some(&Some(&mut ref x)) = Some(&Some(&mut 0)) {
-        let _: &u32 = x;
-    }
-    if let Some(&Some(&x)) = &Some(&mut Some(0)) {
-        //[stable2021]~^ mismatched types
-        //[stable2021]~| types differ in mutability
-        let _: u32 = x;
-    }
-    if let Some(&Some(&x)) = &Some(&Some(0)) {
-        //[stable2021]~^ mismatched types
-        //[stable2021]~| expected integer, found `&_`
-        let _: u32 = x;
-    }
-    if let Some(&Some(Some(&x))) = &Some(Some(&mut Some(0))) {
-        //[stable2021]~^ mismatched types
-        //[stable2021]~| expected `Option<&mut Option<{integer}>>`, found `&_`
-        let _: u32 = x;
-    }
+
+    // Tests for `&` patterns matching real `&mut` reference types
     if let Some(&Some(&x)) = Some(&Some(&mut 0)) {
         //[stable2021]~^ mismatched types
         //[stable2021]~| types differ in mutability
         let _: u32 = x;
     }
+
+    // Tests for eating only one layer and also eating a lone inherited reference
+    if let Some(&Some(&x)) = &Some(&Some(0)) {
+        //[stable2021]~^ mismatched types
+        //[stable2021]~| expected integer, found `&_`
+        let _: u32 = x;
+    }
+
+    // Tests for `&` matching a lone inherited possibly-`&mut` reference
+    if let Some(&Some(Some(&x))) = &Some(Some(&mut Some(0))) {
+        //[stable2021]~^ mismatched types
+        //[stable2021]~| expected `Option<&mut Option<{integer}>>`, found `&_`
+        let _: u32 = x;
+    }
     if let Some(&Some(x)) = &mut Some(Some(0)) {
         //[stable2021]~^ mismatched types
         //[stable2021]~| expected `Option<{integer}>`, found `&_`
+        let _: u32 = x;
+    }
+
+    // Tests eating one layer, eating a lone inherited ref, and `&` eating `&mut` (realness varies)
+    if let Some(&Some(&x)) = &Some(&mut Some(0)) {
+        //[stable2021]~^ mismatched types
+        //[stable2021]~| types differ in mutability
+        let _: u32 = x;
+    }
+    if let Some(&Some(&x)) = &mut Some(&Some(0)) {
+        //[stable2021]~^ mismatched types
+        //[stable2021]~| expected integer, found `&_`
         let _: u32 = x;
     }
 
