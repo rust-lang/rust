ed7cc3486cbd0a059fa5f7d7f9198ee1c5144c01
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2025-01-22 19:57:19 +0300

    Implement `VecDeque::pop_front_if` & `VecDeque::pop_back_if`

--- library/alloc/tests/lib.rs
+++ library/alloc/tests/lib.rs
@@ -38,6 +38,7 @@
 #![feature(str_as_str)]
 #![feature(strict_provenance_lints)]
 #![feature(vec_pop_if)]
+#![feature(vec_deque_pop_if)]
 #![feature(unique_rc_arc)]
 #![feature(macro_metavar_expr_concat)]
 #![allow(internal_features)]
