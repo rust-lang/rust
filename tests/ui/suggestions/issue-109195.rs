221b6214c05bae34f6814177bb26f2a88b358ca3
Zachary S <zasample18+github@gmail.com>
2025-01-21 23:57:15 -0600

    Add test that multiple impls works with E0223 similar-name suggestion.

--- tests/ui/suggestions/issue-109195.rs
+++ tests/ui/suggestions/issue-109195.rs
@@ -1,3 +1,13 @@
+struct Foo;
+
+impl Foo {
+    fn bar_baz() {}
+}
+
+impl Foo {
+    fn bar_quux() {}
+}
+
 fn main() {
     String::from::utf8;
     //~^ ERROR ambiguous associated type [E0223]
@@ -17,4 +27,13 @@ fn main() {
     str::from::utf8_mut();
     //~^ ERROR ambiguous associated type [E0223]
     //~| HELP if there were a trait named `Example` with associated type `from`
+    Foo::bar::baz;
+    //~^ ERROR ambiguous associated type [E0223]
+    //~| HELP there is an associated function with a similar name: `bar_baz`
+    Foo::bar::quux;
+    //~^ ERROR ambiguous associated type [E0223]
+    //~| HELP there is an associated function with a similar name: `bar_quux`
+    Foo::bar::fizz;
+    //~^ ERROR ambiguous associated type [E0223]
+    //~| HELP if there were a trait named `Example` with associated type `bar`
 }
