d8a216b866ff238589005fb1ea4c54644d8d24bf
Shunpoco <tkngsnsk313320@gmail.com>
2025-01-22 01:27:15 +0000

    address review: modify ICE-133063-never-arm-no-otherwise-block.rs
    - Use enum Void to avoid mismatched types error
    - We don't need to use if let to check the ICE
    
    Signed-off-by: Shunpoco <tkngsnsk313320@gmail.com>

--- tests/ui/rfcs/rfc-0000-never_patterns/ICE-133063-never-arm-no-otherwise-block.rs
+++ tests/ui/rfcs/rfc-0000-never_patterns/ICE-133063-never-arm-no-otherwise-block.rs
@@ -1,18 +1,13 @@
+#![feature(never_type)]
 #![feature(never_patterns)]
-#![feature(if_let_guard)]
 #![allow(incomplete_features)]
 
-fn split_last(_: &()) -> Option<(&i32, &i32)> {
-    None
-}
+enum Void {}
 
-fn assign_twice() {
+fn foo(x: Void) {
     loop {
-        match () {
-            (!| //~ ERROR: mismatched types
-            !) if let _ = split_last(&()) => {} //~ ERROR a never pattern is always unreachable
-            //~^ ERROR: mismatched types
-            //~^^ WARNING: irrefutable `if let` guard pattern [irrefutable_let_patterns]
+        match x {
+            (!|!) if false => {} //~ ERROR a never pattern is always unreachable
             _ => {}
         }
     }
