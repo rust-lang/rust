872dc4cc244e14792806b4da7e35ac0cb79fbb22
David Wood <david.wood2@arm.com>
2025-01-22 18:07:04 +0000

    tests: `PointeeSized` bounds with extern types
    These tests necessarily need to change now that `?Sized` is not
    sufficient to accept extern types and `PointeeSized` is now necessary. In
    addition, the `size_of_val`/`align_of_val` test can now be changed to
    expect an error.

--- tests/ui/extern/extern-types-not-sync-send.rs
+++ tests/ui/extern/extern-types-not-sync-send.rs
@@ -1,13 +1,15 @@
 // Make sure extern types are !Sync and !Send.
 
-#![feature(extern_types)]
+#![feature(extern_types, sized_hierarchy)]
+
+use std::marker::PointeeSized;
 
 extern "C" {
     type A;
 }
 
-fn assert_sync<T: ?Sized + Sync>() {}
-fn assert_send<T: ?Sized + Send>() {}
+fn assert_sync<T: PointeeSized + Sync>() {}
+fn assert_send<T: PointeeSized + Send>() {}
 
 fn main() {
     assert_sync::<A>();
