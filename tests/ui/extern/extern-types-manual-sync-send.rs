2a7fb8b8a9cf1ad0e8d4d1745927447c33fb2ae4
David Wood <david.wood2@arm.com>
2025-01-22 18:07:04 +0000

    tests: `PointeeSized` bounds with extern types
    These tests necessarily need to change now that `?Sized` is not
    sufficient to accept extern types and `PointeeSized` is now necessary. In
    addition, the `size_of_val`/`align_of_val` test can now be changed to
    expect an error.

--- tests/ui/extern/extern-types-manual-sync-send.rs
+++ tests/ui/extern/extern-types-manual-sync-send.rs
@@ -1,7 +1,9 @@
 //@ run-pass
 // Test that unsafe impl for Sync/Send can be provided for extern types.
 
-#![feature(extern_types)]
+#![feature(extern_types, sized_hierarchy)]
+
+use std::marker::PointeeSized;
 
 extern "C" {
     type A;
@@ -10,8 +12,8 @@ extern "C" {
 unsafe impl Sync for A {}
 unsafe impl Send for A {}
 
-fn assert_sync<T: ?Sized + Sync>() {}
-fn assert_send<T: ?Sized + Send>() {}
+fn assert_sync<T: PointeeSized + Sync>() {}
+fn assert_send<T: PointeeSized + Send>() {}
 
 fn main() {
     assert_sync::<A>();
