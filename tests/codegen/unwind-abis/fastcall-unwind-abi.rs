cf13708d1f3e8a07abd53147befca0b52f9d7358
David Wood <david.wood2@arm.com>
2025-01-23 11:25:12 +0000

    tests: `{Meta,Pointee}Sized` in non-minicore tests
    As before, add `MetaSized` and `PointeeSized` traits to all of the
    non-minicore `no_core` tests so that they don't fail for lack of
    language items.

--- tests/codegen/unwind-abis/fastcall-unwind-abi.rs
+++ tests/codegen/unwind-abis/fastcall-unwind-abi.rs
@@ -2,8 +2,15 @@
 //@ compile-flags: --target=i686-pc-windows-msvc --crate-type=rlib -Cno-prepopulate-passes
 #![no_core]
 #![feature(no_core, lang_items)]
+
+#[lang = "pointee_sized"]
+pub trait PointeeSized {}
+
+#[lang = "meta_sized"]
+pub trait MetaSized: PointeeSized {}
+
 #[lang = "sized"]
-trait Sized {}
+pub trait Sized: MetaSized {}
 
 // Test that `nounwind` attributes are correctly applied to exported `fastcall` and
 // `fastcall-unwind` extern functions. `fastcall-unwind` functions MUST NOT have this attribute. We
