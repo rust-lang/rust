d39ee71e0fcc8b9d9d7cdf4a579a3981b9b6b978
Marijn Schouten <hkBst@users.noreply.github.com>
2025-01-24 18:33:20 +0100

    add semicolons

--- library/std/src/f32.rs
+++ library/std/src/f32.rs
@@ -332,8 +332,8 @@ impl f32 {
     /// let abs_difference = (x.powf(2.0) - (x * x)).abs();
     /// assert!(abs_difference <= f32::EPSILON);
     ///
-    /// assert_eq!(f32::powf(1.0, f32::NAN), 1.0)
-    /// assert_eq!(f32::powf(f32::NAN, 0.0), 1.0)
+    /// assert_eq!(f32::powf(1.0, f32::NAN), 1.0);
+    /// assert_eq!(f32::powf(f32::NAN, 0.0), 1.0);
     /// ```
     #[rustc_allow_incoherent_impl]
     #[must_use = "method returns a new number and does not mutate the original value"]
