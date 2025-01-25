4a98907b8a9f7cabf758fa7241466a62735fba88
Trevor Gross <tmgross@umich.edu>
2025-01-25 04:18:44 +0000

    Remove remnants of the `checked` feature
    The Cargo feature `checked` was added in 410b0633a6b9 ("Overhaul tests")
    and later removed in e4ac1399062c ("swap stable to be unstable, checked
    is now debug_assertions"). However, there are a few remaining uses of
    `feature = "checked"` that did not get removed. Clean these up here.

--- library/compiler-builtins/libm/build.rs
+++ library/compiler-builtins/libm/build.rs
@@ -8,18 +8,13 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-check-cfg=cfg(assert_no_panic)");
 
-    println!("cargo:rustc-check-cfg=cfg(feature, values(\"checked\"))");
-
-    #[allow(unexpected_cfgs)]
-    if !cfg!(feature = "checked") {
-        let lvl = env::var("OPT_LEVEL").unwrap();
-        if lvl != "0" && !cfg!(debug_assertions) {
-            println!("cargo:rustc-cfg=assert_no_panic");
-        } else if env::var("ENSURE_NO_PANIC").is_ok() {
-            // Give us a defensive way of ensureing that no-panic is checked  when we
-            // expect it to be.
-            panic!("`assert_no_panic `was not enabled");
-        }
+    let lvl = env::var("OPT_LEVEL").unwrap();
+    if lvl != "0" && !cfg!(debug_assertions) {
+        println!("cargo:rustc-cfg=assert_no_panic");
+    } else if env::var("ENSURE_NO_PANIC").is_ok() {
+        // Give us a defensive way of ensureing that no-panic is checked  when we
+        // expect it to be.
+        panic!("`assert_no_panic `was not enabled");
     }
 
     configure::emit_libm_config(&cfg);
