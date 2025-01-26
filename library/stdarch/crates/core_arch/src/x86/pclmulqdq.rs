2348f153aec7c9a6fb37711e58688e2abab2ff18
usamoi <usamoi@outlook.com>
2025-01-26 20:56:50 +0800

    mark PCLMULQDQ intrinsics as safe
    Mark all PCLMULQDQ intrinsics as safe.

--- library/stdarch/crates/core_arch/src/x86/pclmulqdq.rs
+++ library/stdarch/crates/core_arch/src/x86/pclmulqdq.rs
@@ -28,9 +28,9 @@ unsafe extern "C" {
 #[cfg_attr(test, assert_instr(pclmul, IMM8 = 0))]
 #[rustc_legacy_const_generics(2)]
 #[stable(feature = "simd_x86", since = "1.27.0")]
-pub unsafe fn _mm_clmulepi64_si128<const IMM8: i32>(a: __m128i, b: __m128i) -> __m128i {
+pub fn _mm_clmulepi64_si128<const IMM8: i32>(a: __m128i, b: __m128i) -> __m128i {
     static_assert_uimm_bits!(IMM8, 8);
-    pclmulqdq(a, b, IMM8 as u8)
+    unsafe { pclmulqdq(a, b, IMM8 as u8) }
 }
 
 #[cfg(test)]
