cfdcfe418911c261a6b71eaa5166ee37d366fb40
Michael Goulet <michael@errs.io>
2025-01-27 19:15:37 +0000

    Eagerly detect coroutine recursion pre-mono when possible

--- tests/ui/impl-trait/recursive-coroutine-indirect.rs
+++ tests/ui/impl-trait/recursive-coroutine-indirect.rs
@@ -2,9 +2,6 @@
 //@ ignore-compare-mode-next-solver (explicit revisions)
 //@[next] compile-flags: -Znext-solver
 
-//@[next] build-fail
-// Deeply normalizing writeback results of opaques makes this into a post-mono error :(
-
 #![feature(coroutines)]
 #![allow(unconditional_recursion)]
 fn coroutine_hold() -> impl Sized {
