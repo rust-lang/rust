b866debf3cdbc30f71b0800684236213ebbb2d8c
David Tolnay <dtolnay@gmail.com>
2025-02-01 14:54:02 -0800

    Omit argument names from function pointers that do not have argument names

--- tests/rustdoc/fn-pointer-arg-name.rs
+++ tests/rustdoc/fn-pointer-arg-name.rs
@@ -5,7 +5,7 @@
 pub fn f(callback: fn(len: usize, foo: u32)) {}
 
 //@ has foo/fn.g.html
-//@ has - '//pre[@class="rust item-decl"]' 'pub fn g(_: fn(_: usize, _: u32))'
+//@ has - '//pre[@class="rust item-decl"]' 'pub fn g(_: fn(usize, u32))'
 pub fn g(_: fn(usize, _: u32)) {}
 
 //@ has foo/fn.mixed.html
