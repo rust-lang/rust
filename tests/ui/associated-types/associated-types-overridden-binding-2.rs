72bd174c43406068727e17cbf6fc2f4e1a36f6af
Michael Goulet <michael@errs.io>
2025-02-03 02:38:56 +0000

    Do not deduplicate list of associated types provided by dyn principal

--- tests/ui/associated-types/associated-types-overridden-binding-2.rs
+++ tests/ui/associated-types/associated-types-overridden-binding-2.rs
@@ -4,5 +4,5 @@ trait I32Iterator = Iterator<Item = i32>;
 
 fn main() {
     let _: &dyn I32Iterator<Item = u32> = &vec![42].into_iter();
-    //~^ ERROR expected `IntoIter<u32>` to be an iterator that yields `i32`, but it yields `u32`
+    //~^ ERROR conflicting associated type bounds
 }
