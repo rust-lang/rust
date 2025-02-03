c20e12e6a8ef91e4e51759073e74a9a04260ce66
David Wood <david.wood2@arm.com>
2025-02-03 15:54:25 +0000

    tests: bless remaining tests
    These tests just need blessing, they don't have any interesting behaviour
    changes.
    
    Some of these tests have new errors because `LegacyReceiver` cannot be
    proven to be implemented now that it is also testing for `MetaSized` -
    but this is just a consequence of the other errors in the test.

--- tests/ui/layout/unconstrained-param-ice-137308.rs
+++ tests/ui/layout/unconstrained-param-ice-137308.rs
@@ -3,14 +3,16 @@
 //! This used to ICE in layout computation, because `<u8 as A>::B` fails to normalize
 //! due to the unconstrained param on the impl.
 
-#![feature(rustc_attrs)]
+#![feature(rustc_attrs, sized_hierarchy)]
 #![crate_type = "lib"]
+
+use std::marker::PointeeSized;
 
 trait A {
     const B: usize;
 }
 
-impl<C: ?Sized> A for u8 { //~ ERROR: the type parameter `C` is not constrained
+impl<C: PointeeSized> A for u8 { //~ ERROR: the type parameter `C` is not constrained
     const B: usize = 42;
 }
 
