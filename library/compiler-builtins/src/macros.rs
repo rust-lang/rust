57572a583da932aed817b0677bf6a85d135dc920
Trevor Gross <tmgross@umich.edu>
2025-02-04 10:36:45 +0000

    Revert "Eliminate the use of `public_test_dep!`"
    [1] has not gone forward, so this needs to be reverted again in order to
    unblock a compiler-builtins upgrade that is necessary for the LLVM 20
    upgrade.
    
    This reverts commit a2494f14e99ae90c964f12bf0c059d63ccc07c2a.
    
    [1]: https://github.com/rust-lang/rust/pull/135501

--- library/compiler-builtins/src/macros.rs
+++ library/compiler-builtins/src/macros.rs
@@ -1,5 +1,21 @@
 //! Macros shared throughout the compiler-builtins implementation
 
+/// Changes the visibility to `pub` if feature "public-test-deps" is set
+#[cfg(not(feature = "public-test-deps"))]
+macro_rules! public_test_dep {
+    ($(#[$($meta:meta)*])* pub(crate) $ident:ident $($tokens:tt)*) => {
+        $(#[$($meta)*])* pub(crate) $ident $($tokens)*
+    };
+}
+
+/// Changes the visibility to `pub` if feature "public-test-deps" is set
+#[cfg(feature = "public-test-deps")]
+macro_rules! public_test_dep {
+    {$(#[$($meta:meta)*])* pub(crate) $ident:ident $($tokens:tt)*} => {
+        $(#[$($meta)*])* pub $ident $($tokens)*
+    };
+}
+
 /// The "main macro" used for defining intrinsics.
 ///
 /// The compiler-builtins library is super platform-specific with tons of crazy
