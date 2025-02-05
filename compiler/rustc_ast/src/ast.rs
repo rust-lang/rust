b0daa2e30d74182f3c90bf117d920d4683df2b61
Noah Lev <camelidcamel@gmail.com>
2025-02-04 20:08:58 -0500

    Relax overly strict mgca requirements

--- compiler/rustc_ast/src/ast.rs
+++ compiler/rustc_ast/src/ast.rs
@@ -123,6 +123,13 @@ impl Path {
     pub fn is_global(&self) -> bool {
         self.segments.first().is_some_and(|segment| segment.ident.name == kw::PathRoot)
     }
+
+    // FIXME: add docs
+    #[tracing::instrument(level = "debug", ret)]
+    pub fn is_potential_trivial_const_arg(&self, allow_mgca_arg: bool) -> bool {
+        allow_mgca_arg
+            || self.segments.len() == 1 && self.segments.iter().all(|seg| seg.args.is_none())
+    }
 }
 
 /// A segment of a path: an identifier, an optional lifetime, and a set of types.
@@ -1171,6 +1178,29 @@ pub struct Expr {
 }
 
 impl Expr {
+    // FIXME: update docs
+    /// Could this expr be either `N`, or `{ N }`, where `N` is a const parameter.
+    ///
+    /// If this is not the case, name resolution does not resolve `N` when using
+    /// `min_const_generics` as more complex expressions are not supported.
+    ///
+    /// Does not ensure that the path resolves to a const param, the caller should check this.
+    /// This also does not consider macros, so it's only correct after macro-expansion.
+    pub fn is_potential_trivial_const_arg(&self, allow_mgca_arg: bool) -> bool {
+        let this = self.maybe_unwrap_block();
+        if allow_mgca_arg {
+            matches!(this.kind, ExprKind::Path(..))
+        } else {
+            if let ExprKind::Path(None, path) = &this.kind
+                && path.is_potential_trivial_const_arg(allow_mgca_arg)
+            {
+                true
+            } else {
+                false
+            }
+        }
+    }
+
     /// Returns an expression with (when possible) *one* outter brace removed
     pub fn maybe_unwrap_block(&self) -> &Expr {
         if let ExprKind::Block(block, None) = &self.kind
