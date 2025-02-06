57b241532a605c9b531b755df4823e581ee715ca
jnyfah <jnyfaah@gmail.com>
2025-02-06 16:18:15 +0100

    collapsing if statement

--- src/tools/rust-analyzer/crates/ide/src/inlay_hints/bind_pat.rs
+++ src/tools/rust-analyzer/crates/ide/src/inlay_hints/bind_pat.rs
@@ -36,10 +36,8 @@ pub(super) fn hints(
                 if it.ty().is_some() {
                     return None;
                 }
-                if config.hide_closure_parameter_hints {
-                    if it.syntax().ancestors().any(|n| matches!(ast::Expr::cast(n), Some(ast::Expr::ClosureExpr(_)))) {
-                        return None;
-                    }
+                if config.hide_closure_parameter_hints && it.syntax().ancestors().any(|n| matches!(ast::Expr::cast(n), Some(ast::Expr::ClosureExpr(_)))) {
+                    return None;
                 }
                 Some(it.colon_token())
             },
