518893c052665c7ea0fde7e41ef30a1f477aac48
Jonathan Dönszelmann <jonathan@donsz.nl>
2025-02-06 16:02:11 +0100

    implement inline parser

--- compiler/rustc_attr_data_structures/src/attributes.rs
+++ compiler/rustc_attr_data_structures/src/attributes.rs
@@ -8,7 +8,7 @@ use thin_vec::ThinVec;
 
 use crate::{DefaultBodyStability, PartialConstStability, PrintAttribute, RustcVersion, Stability};
 
-#[derive(Copy, Clone, PartialEq, Encodable, Decodable, Debug, HashStable_Generic)]
+#[derive(Copy, Clone, PartialEq, Encodable, Decodable, Debug, HashStable_Generic, PrintAttribute)]
 pub enum InlineAttr {
     None,
     Hint,
@@ -189,6 +189,7 @@ pub enum AttributeKind {
         span: Span,
         comment: Symbol,
     },
+    Inline(InlineAttr, Span),
     MacroTransparency(Transparency),
     Repr(ThinVec<(ReprAttr, Span)>),
     Stability {
