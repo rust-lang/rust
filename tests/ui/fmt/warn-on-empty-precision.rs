6b1958995e6bb562c95b8fee86d55884d842df17
Marijn Schouten <mhkbst@gmail.com>
2025-02-06 12:15:06 +0100

    warn on empty precision

---
+++ tests/ui/fmt/warn-on-empty-precision.rs
@@ -0,0 +1,9 @@
+//@ run-pass
+fn main() {
+    let float = 0.123_456_789;
+    println!("Missing precision: {float:.} {float:3.} {float:_^12.}");
+    //~^ WARNING invalid format string: expected numerical precision after precision specifier
+    //~| WARNING invalid format string: expected numerical precision after precision specifier
+    //~| WARNING invalid format string: expected numerical precision after precision specifier
+    println!("Given precision: {float:.6} {float:3.15} {float:_^12.7}");
+}
