95b52d51eac77be1fcc624c2278dcaafa6989b0b
Jana Dönszelmann <jana@donsz.nl>
2025-02-09 22:50:03 +0100

    pretty print hir attributes

--- compiler/rustc_hir_pretty/src/lib.rs
+++ compiler/rustc_hir_pretty/src/lib.rs
@@ -16,7 +16,7 @@ use rustc_ast_pretty::pp::Breaks::{Consistent, Inconsistent};
 use rustc_ast_pretty::pp::{self, Breaks};
 use rustc_ast_pretty::pprust::state::MacHeader;
 use rustc_ast_pretty::pprust::{Comments, PrintState};
-use rustc_attr_parsing::AttributeKind;
+use rustc_attr_parsing::{AttributeKind, PrintAttribute};
 use rustc_hir::{
     BindingMode, ByRef, ConstArgKind, GenericArg, GenericBound, GenericParam, GenericParamKind,
     HirId, ImplicitSelfKind, LifetimeParamKind, Node, PatKind, PreciseCapturingArg, RangeEnd, Term,
@@ -117,7 +117,12 @@ impl<'a> State<'a> {
                 ));
                 self.hardbreak()
             }
-            _ => unimplemented!("pretty print parsed attributes"),
+            hir::Attribute::Parsed(pa) => {
+                self.word("#[attr=\"");
+                pa.print_attribute(self);
+                self.word("\")]");
+                self.hardbreak()
+            }
         }
     }
 
