97a062ef66f41f94d14370b30e81b031e84bba8d
Santiago Pastorino <spastorino@gmail.com>
2025-02-10 18:09:31 -0300

    Support nested use closures

---
+++ tests/ui/ergonomic-clones/closure/nested.rs
@@ -0,0 +1,20 @@
+//@ run-pass
+
+#![feature(ergonomic_clones)]
+
+use std::clone::UseCloned;
+
+#[derive(Clone)]
+struct Foo;
+
+impl UseCloned for Foo {}
+
+fn work(_: Box<Foo>) {}
+fn foo<F:FnOnce()>(_: F) {}
+
+pub fn main() {
+  let a = Box::new(Foo);
+  foo(use || { foo(use || { work(a) }) });
+  let x = use || { use || { Foo } };
+  let _y = x();
+}
