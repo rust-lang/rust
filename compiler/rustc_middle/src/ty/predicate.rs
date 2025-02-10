bd8fead23e54cc2e27389587576bdd7ad9f084cc
lcnr <rust@lcnr.de>
2025-02-10 14:45:24 +0100

    rework cycle handling
    A cycle was previously coinductive if all steps were coinductive.
    Change this to instead considerm cycles to be coinductive if they
    step through at least one where-bound of an impl of a coinductive
    trait goal.

--- compiler/rustc_middle/src/ty/predicate.rs
+++ compiler/rustc_middle/src/ty/predicate.rs
@@ -52,10 +52,6 @@ impl<'tcx> rustc_type_ir::inherent::Predicate<TyCtxt<'tcx>> for Predicate<'tcx> 
         self.as_clause()
     }
 
-    fn is_coinductive(self, interner: TyCtxt<'tcx>) -> bool {
-        self.is_coinductive(interner)
-    }
-
     fn allow_normalization(self) -> bool {
         self.allow_normalization()
     }
@@ -120,6 +116,8 @@ impl<'tcx> Predicate<'tcx> {
         Some(tcx.mk_predicate(kind))
     }
 
+    /// Only used by the old solver to decide whether a predicate is accepted
+    /// in a coinductive trait solver cycle.
     #[instrument(level = "debug", skip(tcx), ret)]
     pub fn is_coinductive(self, tcx: TyCtxt<'tcx>) -> bool {
         match self.kind().skip_binder() {
