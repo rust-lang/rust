f41052050fce7746eb5e9462c735d34da6539c5c
Eric Huss <eric@huss.org>
2025-02-10 11:33:50 -0800

    Enable s390x_target_feature in core
    This is needed for stdarch which now includes s390x target features.

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -211,6 +211,7 @@
 #![feature(powerpc_target_feature)]
 #![feature(riscv_target_feature)]
 #![feature(rtm_target_feature)]
+#![feature(s390x_target_feature)]
 #![feature(sha512_sm_x86)]
 #![feature(sse4a_target_feature)]
 #![feature(tbm_target_feature)]
