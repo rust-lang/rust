ee827bf83ee1d18c7df1a74c7b57b87a99eeca77
Chris Denton <chris@chrisdenton.dev>
2025-02-10 13:14:59 +0000

    Update install-mingw.sh

--- src/ci/scripts/install-mingw.sh
+++ src/ci/scripts/install-mingw.sh
@@ -10,9 +10,6 @@ MINGW_ARCHIVE_32="i686-14.1.0-release-posix-dwarf-msvcrt-rt_v12-rev0.7z"
 MINGW_ARCHIVE_64="x86_64-14.1.0-release-posix-seh-msvcrt-rt_v12-rev0.7z"
 
 if isWindows && isKnownToBeMingwBuild; then
-    msys2Path="c:/msys64"
-    mkdir -p "${msys2Path}/home/${USERNAME}"
-    ciCommandAddPath "${msys2Path}/usr/bin"
 
     case "${CI_JOB_NAME}" in
         *i686*)
