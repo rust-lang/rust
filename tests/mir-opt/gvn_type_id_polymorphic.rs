a061e7eb65161af1a994257f0619d0ee7da24306
Lukas Markeffsky <@>
2025-02-10 22:06:19 +0000

    fix ensure_monomorphic_enough
    (cherry picked from commit c1da4f1d3c4fc4beb5edcfa8a303a1dcbe27b65e)

--- tests/mir-opt/gvn_type_id_polymorphic.rs
+++ tests/mir-opt/gvn_type_id_polymorphic.rs
@@ -12,7 +12,7 @@ const fn type_id_of_val<T: 'static>(_: &T) -> u128 {
 // EMIT_MIR gvn_type_id_polymorphic.cursed_is_i32.GVN.diff
 fn cursed_is_i32<T: 'static>() -> bool {
     // CHECK-LABEL: fn cursed_is_i32(
-    // CHECK: _0 = const false;
+    // CHECK: _0 = Eq(const cursed_is_i32::<T>::{constant#0}, const cursed_is_i32::<T>::{constant#1});
     // CHECK-NEXT: return;
     (const { type_id_of_val(&generic::<T>) } == const { type_id_of_val(&generic::<i32>) })
 }
