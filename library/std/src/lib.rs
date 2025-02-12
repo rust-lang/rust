cafa646f21ec41d6b20b5495c25456c997206dc1
Jubilee Young <workingjubilee@gmail.com>
2025-02-11 17:25:26 -0800

    library: amend revert of extended_varargs_abi_support for beta diff
    And leave a comment on the unusual `cfg_attr`
    
    Co-authored-by: waffle <waffle.lapkin@gmail.com>

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -272,6 +272,9 @@
 //
 // Language features:
 // tidy-alphabetical-start
+
+// stabilization was reverted after it hit beta
+#![cfg_attr(not(bootstrap), feature(extended_varargs_abi_support))]
 #![feature(alloc_error_handler)]
 #![feature(allocator_internals)]
 #![feature(allow_internal_unsafe)]
@@ -289,7 +292,6 @@
 #![feature(doc_masked)]
 #![feature(doc_notable_trait)]
 #![feature(dropck_eyepatch)]
-#![feature(extended_varargs_abi_support)]
 #![feature(f128)]
 #![feature(f16)]
 #![feature(formatting_options)]
