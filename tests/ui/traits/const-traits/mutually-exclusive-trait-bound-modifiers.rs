7cef03d6ba7611846a5a6ee78373f119e94b5e63
David Wood <david.wood2@arm.com>
2025-02-13 17:48:34 +0000

    library/trait_sel: make `{Meta,}Sized` const
    Makes `{Meta,}Sized` into const traits and implements them in the trait
    solvers so everything is `Sized` except ADTs with the
    `rustc_non_const_sized` attribute.

--- tests/ui/traits/const-traits/mutually-exclusive-trait-bound-modifiers.rs
+++ tests/ui/traits/const-traits/mutually-exclusive-trait-bound-modifiers.rs
@@ -1,4 +1,4 @@
-#![feature(const_trait_impl)]
+#![feature(const_trait_impl, sized_hierarchy)]
 
 const fn maybe_const_maybe<T: ~const ?Sized>() {}
 //~^ ERROR `~const` trait not allowed with `?` trait polarity modifier
