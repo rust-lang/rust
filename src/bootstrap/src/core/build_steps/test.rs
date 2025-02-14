5afd2d9e492d962ba4a6d5e93e05d94ccd40cb64
许杰友 Jieyou Xu (Joe) <39484203+jieyouxu@users.noreply.github.com>
2025-02-15 03:31:59 +0800

    bootstrap: no longer pass `--git-hash`

--- src/bootstrap/src/core/build_steps/test.rs
+++ src/bootstrap/src/core/build_steps/test.rs
@@ -2125,10 +2125,6 @@ NOTE: if you're sure you want to do this, please open an issue as to why. In the
 
         cmd.arg("--channel").arg(&builder.config.channel);
 
-        if !builder.config.omit_git_hash {
-            cmd.arg("--git-hash");
-        }
-
         let git_config = builder.config.git_config();
         cmd.arg("--git-repository").arg(git_config.git_repository);
         cmd.arg("--nightly-branch").arg(git_config.nightly_branch);
