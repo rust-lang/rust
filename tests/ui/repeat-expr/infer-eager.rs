33d925aa19d4101471f6a3abeccfd7dc86cdf959
Boxy <rust@boxyuwu.dev>
2025-02-14 21:45:39 +0000

    Add inference constraining Copy impl test

---
+++ tests/ui/repeat-expr/infer-eager.rs
@@ -0,0 +1,21 @@
+//@ check-pass
+
+use std::marker::PhantomData;
+
+struct Foo<T>(PhantomData<T>);
+
+impl Clone for Foo<u8> {
+    fn clone(&self) -> Self {
+        Foo(PhantomData)
+    }
+}
+impl Copy for Foo<u8> {}
+
+fn extract<T, const N: usize>(_: [Foo<T>; N]) -> T {
+    loop {}
+}
+
+fn main() {
+    let x = [Foo(PhantomData); 2];
+    _ = extract(x).max(2);
+}
