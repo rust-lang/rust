193b93c116697760db93051a055cbb8a186dbfae
Michael Goulet <michael@errs.io>
2025-02-14 02:16:42 +0000

    Check for post-mono failures

--- compiler/rustc_monomorphize/src/mono_checks/mod.rs
+++ compiler/rustc_monomorphize/src/mono_checks/mod.rs
@@ -3,7 +3,7 @@
 //! checks in a way that is friendly to incremental compilation.
 
 use rustc_middle::query::Providers;
-use rustc_middle::ty::{Instance, TyCtxt};
+use rustc_middle::ty::{Instance, InstanceKind, TyCtxt};
 
 mod abi_check;
 mod move_check;
@@ -12,6 +12,11 @@ fn check_mono_item<'tcx>(tcx: TyCtxt<'tcx>, instance: Instance<'tcx>) {
     let body = tcx.instance_mir(instance.def);
     abi_check::check_feature_dependent_abi(tcx, instance, body);
     move_check::check_moves(tcx, instance, body);
+    if let InstanceKind::Item(def_id) = instance.def {
+        if tcx.instantiate_and_check_impossible_predicates((def_id, instance.args)) {
+            tcx.dcx().span_err(tcx.def_span(def_id), "post-mono");
+        }
+    }
 }
 
 pub(super) fn provide(providers: &mut Providers) {
