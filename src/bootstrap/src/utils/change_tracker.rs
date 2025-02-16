c7af90d4c325e460c14e46e32aff9a18c743ee97
bit-aloo <sshourya17@gmail.com>
2025-02-17 01:34:32 +0530

    Add change info to change tracker

--- src/bootstrap/src/utils/change_tracker.rs
+++ src/bootstrap/src/utils/change_tracker.rs
@@ -370,4 +370,9 @@ pub const CONFIG_CHANGE_HISTORY: &[ChangeInfo] = &[
         severity: ChangeSeverity::Info,
         summary: "The rust.description option has moved to build.description and rust.description is now deprecated.",
     },
+    ChangeInfo {
+        change_id: 137147,
+        severity: ChangeSeverity::Info,
+        summary: "New option `build.exclude` that adds support for excluding test.",
+    },
 ];
