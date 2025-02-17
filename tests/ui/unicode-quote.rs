1f9345d2143830891cc6a482e13552d2e2a75a14
Xelph <fierrosalinas.la@gmail.com>
2025-02-17 14:24:33 -0700

    Add comment to test

--- tests/ui/unicode-quote.rs
+++ tests/ui/unicode-quote.rs
@@ -1,5 +1,7 @@
+// Provide a useful error message when attempting to escape a non-ascii quotation mark.
+// <https://github.com/rust-lang/rust/issues/128858>
 fn main() {
     dbg!("Is this \“ a small sized quote or a big sized quote. \“ ");
-    //~^ 2:20: 2:21: unknown character escape: `\u{201c}`
-    //~^^ 2:65: 2:66: unknown character escape: `\u{201c}`
+    //~^ ERROR 4:20: 4:21: unknown character escape: `\u{201c}`
+    //~^^ ERROR 4:65: 4:66: unknown character escape: `\u{201c}`
 }
