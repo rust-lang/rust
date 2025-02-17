4d80eef3ff73e52061fa1e374f520c9788fe915c
Xelph <fierrosalinas.la@gmail.com>
2025-02-17 15:45:09 -0700

    Rework test

--- tests/ui/unicode-quote.rs
+++ tests/ui/unicode-quote.rs
@@ -1,7 +1,20 @@
 // Provide a useful error message when attempting to escape a non-ascii quotation mark.
 // <https://github.com/rust-lang/rust/issues/128858>
 fn main() {
-    dbg!("Is this \“ a small sized quote or a big sized quote. \“ ");
-    //~^ ERROR 4:20: 4:21: unknown character escape: `\u{201c}`
-    //~^^ ERROR 4:65: 4:66: unknown character escape: `\u{201c}`
+    dbg!("\″"); //U+2033
+    //~^ERROR 4:12: 4:13: unknown character escape: `\u{2033}`
+    dbg!("\ʺ"); //U+02BA
+    //~^ERROR 6:12: 6:13: unknown character escape: `\u{2ba}`
+    dbg!("\˝"); //U+02DD
+    //~^ERROR 8:12: 8:13: unknown character escape: `\u{2dd}`
+    dbg!("\̋"); //U+030B
+    //~^ERROR 10:12: 10:13: unknown character escape: `\u{30b}`
+    dbg!("\̎"); //U+030E
+    //~^ERROR 12:12: 12:13: unknown character escape: `\u{30e}`
+    dbg!("\״"); //U+05F4
+    //~^ERROR 14:12: 14:13: unknown character escape: `\u{5f4}`
+    dbg!("\“"); //U+201C
+    //~^ERROR 16:12: 16:13: unknown character escape: `\u{201c}`
+    dbg!("\”"); //U+201D
+    //~^ERROR 18:12: 18:13: unknown character escape: `\u{201d}`
 }
