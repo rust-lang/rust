999967a57dce987bbad353d152f03c3ef67d41f2
Trevor Gross <tmgross@umich.edu>
2025-02-18 00:27:30 +0000

    Rename `cfg_match!` to `cfg_select!`
    At [1] it was pointed out that `cfg_match!` syntax does not actually
    align well with match syntax, which is a possible source of confusion.
    The comment points out that usage is instead more similar to ecosystem
    `select!` macros. Rename `cfg_match!` to `cfg_select!` to match this.
    
    Tracking issue: https://github.com/rust-lang/rust/issues/115585
    
    [1]: https://github.com/rust-lang/rust/issues/115585#issuecomment-2346307605

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -100,7 +100,7 @@
 #![feature(bigint_helper_methods)]
 #![feature(bstr)]
 #![feature(bstr_internals)]
-#![feature(cfg_match)]
+#![feature(cfg_select)]
 #![feature(cfg_target_has_reliable_f16_f128)]
 #![feature(const_carrying_mul_add)]
 #![feature(const_eval_select)]
@@ -235,8 +235,8 @@ pub mod autodiff {
 #[unstable(feature = "contracts", issue = "128044")]
 pub mod contracts;
 
-#[unstable(feature = "cfg_match", issue = "115585")]
-pub use crate::macros::cfg_match;
+#[unstable(feature = "cfg_select", issue = "115585")]
+pub use crate::macros::cfg_select;
 
 #[macro_use]
 mod internal_macros;
