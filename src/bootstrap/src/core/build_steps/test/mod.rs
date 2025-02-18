34a17493b52ca7e28f941c88347b54cff3403ba2
许杰友 Jieyou Xu (Joe) <39484203+jieyouxu@users.noreply.github.com>
2025-02-18 20:19:22 +0800

    Break out compiletest self-test into its own `compiletest_self_tests` module

--- src/bootstrap/src/core/build_steps/test/mod.rs
+++ src/bootstrap/src/core/build_steps/test/mod.rs
@@ -37,6 +37,7 @@ use crate::{CLang, DocTests, GitRepo, Mode, PathSet, envify};
 
 mod book_tests;
 mod bootstrap_self_tests;
+mod compiletest_self_tests;
 mod devtool_tests;
 mod miri_tests;
 mod rustdoc_tests;
@@ -243,47 +244,6 @@ impl Step for Cargotest {
             .env("RUSTDOC", builder.rustdoc(compiler));
         add_rustdoc_cargo_linker_args(&mut cmd, builder, compiler.host, LldThreads::No);
         cmd.delay_failure().run(builder);
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct CompiletestTest {
-    host: TargetSelection,
-}
-
-impl Step for CompiletestTest {
-    type Output = ();
-
-    fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {
-        run.path("src/tools/compiletest")
-    }
-
-    fn make_run(run: RunConfig<'_>) {
-        run.builder.ensure(CompiletestTest { host: run.target });
-    }
-
-    /// Runs `cargo test` for compiletest.
-    fn run(self, builder: &Builder<'_>) {
-        let host = self.host;
-        let compiler = builder.compiler(builder.top_stage, host);
-
-        // We need `ToolStd` for the locally-built sysroot because
-        // compiletest uses unstable features of the `test` crate.
-        builder.ensure(compile::Std::new(compiler, host));
-        let mut cargo = tool::prepare_tool_cargo(
-            builder,
-            compiler,
-            // compiletest uses libtest internals; make it use the in-tree std to make sure it never breaks
-            // when std sources change.
-            Mode::ToolStd,
-            host,
-            Kind::Test,
-            "src/tools/compiletest",
-            SourceType::InTree,
-            &[],
-        );
-        cargo.allow_features("test");
-        run_cargo_test(cargo, &[], &[], "compiletest", "compiletest self test", host, builder);
     }
 }
 
