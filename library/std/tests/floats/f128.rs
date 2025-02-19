b190f717a776e18d7d95f0e00fd0109b8defc767
Calder Coalson <caldercoalson@gmail.com>
2025-02-19 12:08:31 -0800

    Expose algebraic floating point intrinsics

--- library/std/tests/floats/f128.rs
+++ library/std/tests/floats/f128.rs
@@ -985,6 +985,26 @@ fn test_total_cmp() {
 }
 
 #[test]
+fn test_algebraic() {
+    let a: f128 = 123.0;
+    let b: f128 = 456.0;
+
+    if cfg!(miri) {
+        assert_approx_eq!(a.algebraic_add(b), a + b);
+        assert_approx_eq!(a.algebraic_sub(b), a - b);
+        assert_approx_eq!(a.algebraic_mul(b), a * b);
+        assert_approx_eq!(a.algebraic_div(b), a / b);
+        assert_approx_eq!(a.algebraic_rem(b), a % b);
+    } else {
+        assert_eq!(a.algebraic_add(b), a + b);
+        assert_eq!(a.algebraic_sub(b), a - b);
+        assert_eq!(a.algebraic_mul(b), a * b);
+        assert_eq!(a.algebraic_div(b), a / b);
+        assert_eq!(a.algebraic_rem(b), a % b);
+    }
+}
+
+#[test]
 fn test_from() {
     assert_eq!(f128::from(false), 0.0);
     assert_eq!(f128::from(true), 1.0);
--- library/std/tests/floats/f32.rs
+++ library/std/tests/floats/f32.rs
@@ -915,3 +915,23 @@ fn test_total_cmp() {
     assert_eq!(Ordering::Less, (-s_nan()).total_cmp(&f32::INFINITY));
     assert_eq!(Ordering::Less, (-s_nan()).total_cmp(&s_nan()));
 }
+
+#[test]
+fn test_algebraic() {
+    let a: f32 = 123.0;
+    let b: f32 = 456.0;
+
+    if cfg!(miri) {
+        assert_approx_eq!(a.algebraic_add(b), a + b, 1e-2);
+        assert_approx_eq!(a.algebraic_sub(b), a - b, 1e-2);
+        assert_approx_eq!(a.algebraic_mul(b), a * b, 1e-1);
+        assert_approx_eq!(a.algebraic_div(b), a / b, 1e-5);
+        assert_approx_eq!(a.algebraic_rem(b), a % b, 1e-2);
+    } else {
+        assert_eq!(a.algebraic_add(b), a + b);
+        assert_eq!(a.algebraic_sub(b), a - b);
+        assert_eq!(a.algebraic_mul(b), a * b);
+        assert_eq!(a.algebraic_div(b), a / b);
+        assert_eq!(a.algebraic_rem(b), a % b);
+    }
+}
