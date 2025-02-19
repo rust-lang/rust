0046bab513e7fe56f9a1ca0fbbc20852b9ac8509
Esteban Küber <esteban@kuber.com.ar>
2025-02-19 19:46:34 +0000

    review comment: reword message and add detail to tests

--- tests/ui/consts/const_unsafe_unreachable_ub.rs
+++ tests/ui/consts/const_unsafe_unreachable_ub.rs
@@ -1,13 +1,14 @@
-//@ error-pattern: evaluation of constant value failed
-
 const unsafe fn foo(x: bool) -> bool {
     match x {
         true => true,
-        false => std::hint::unreachable_unchecked(),
+        false => std::hint::unreachable_unchecked(), //~ NOTE inside `foo`
     }
 }
 
 const BAR: bool = unsafe { foo(false) };
+//~^ ERROR evaluation of constant value failed
+//~| NOTE entering unreachable code
+//~| NOTE inside `unreachable_unchecked`
 
 fn main() {
     assert_eq!(BAR, true);
--- tests/ui/consts/miri_unleashed/abi-mismatch.rs
+++ tests/ui/consts/miri_unleashed/abi-mismatch.rs
@@ -5,7 +5,7 @@ const extern "C" fn c_fn() {}
 
 const fn call_rust_fn(my_fn: extern "Rust" fn()) {
     my_fn(); //~ NOTE inside `call_rust_fn`
-    //~^ NOTE the evaluated
+    //~^ NOTE the failure occurred here
 }
 
 static VAL: () = call_rust_fn(unsafe { std::mem::transmute(c_fn as extern "C" fn()) });
