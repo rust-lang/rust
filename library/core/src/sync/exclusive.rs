44e408032aefce8b626c01903a353359f1852c42
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-02-20 18:01:53 -0500

    Update exclusive.rs

--- library/core/src/sync/exclusive.rs
+++ library/core/src/sync/exclusive.rs
@@ -164,7 +164,7 @@ impl<T: ?Sized> Exclusive<T> {
 
 #[unstable(feature = "exclusive_wrapper", issue = "98407")]
 impl<T> From<T> for Exclusive<T> {
-    /// Creates a new `Exclusive` containing [`T`], wrapping the value in an `Exclusive`
+    /// Creates a new `Exclusive` wrapping `T`.
     #[inline]
     fn from(t: T) -> Self {
         Self::new(t)
