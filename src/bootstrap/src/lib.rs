d2203ad59c67a6acb2968ea77e1e9dea5530e518
onur-ozkan <work@onurozkan.dev>
2025-02-20 22:48:39 +0300

    skip submodule updating logics on tarballs
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/bootstrap/src/lib.rs
+++ src/bootstrap/src/lib.rs
@@ -481,6 +481,10 @@ impl Build {
         ),
     )]
     pub fn require_submodule(&self, submodule: &str, err_hint: Option<&str>) {
+        if self.rust_info().is_from_tarball() {
+            return;
+        }
+
         // When testing bootstrap itself, it is much faster to ignore
         // submodules. Almost all Steps work fine without their submodules.
         if cfg!(test) && !self.config.submodules() {
