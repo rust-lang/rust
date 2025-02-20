70017077e0d4710a9dd09e1d8f3b557079760787
Michael Goulet <michael@errs.io>
2025-02-20 02:21:58 +0000

    Check signature WF when lowering MIR body

--- tests/crashes/134654.rs
+++ tests/crashes/134654.rs
@@ -2,6 +2,9 @@
 //@ compile-flags: -Zmir-enable-passes=+GVN -Zmir-enable-passes=+Inline -Zvalidate-mir
 //@ only-x86_64
 
+#![feature(adt_const_params, unsized_const_params)]
+#![allow(incomplete_features)]
+
 fn function_with_bytes<const BYTES:
     &'static [u8; 0xa9008fb6c9d81e42_0e25730562a601c8_u128]>() -> &'static [u8] {
     BYTES
