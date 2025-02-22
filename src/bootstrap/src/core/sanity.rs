f76d1aecd6a5e239fa5c6b719440657cb13a3231
Jayden Qi <jaydeniqv2@gmail.com>
2025-02-22 13:03:50 -0800

    fix: wasm-bare targets compiling x86 builtins
    Added sanity check to bootstrap to hard error on wasm builds without
    clang, and changed distribution image `dist-various-2` to use clang to
    build for official targets.

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -326,6 +326,17 @@ than building it.
             .entry(*target)
             .or_insert_with(|| Target::from_triple(&target.triple));
 
+        // compiler-rt c fallbacks for wasm cannot be built with gcc
+        if target.contains("wasm") // bare metal targets without wasi sdk
+            && (build.config.optimized_compiler_builtins(*target)
+                || build.config.rust_std_features.contains("compiler-builtins-c"))
+        {
+            let is_clang = build.cc_tool(*target).is_like_clang();
+            if !is_clang {
+                panic!("only clang supports building c code for wasm targets");
+            }
+        }
+
         if (target.contains("-none-") || target.contains("nvptx"))
             && build.no_std(*target) == Some(false)
         {
