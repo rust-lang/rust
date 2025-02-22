eaeda23c0422c1c9e3530cd5da25d4c00618957d
Richard Dodd <richard.o.dodd@gmail.com>
2025-02-22 16:47:09 +0000

    ensure previously rejected tokens now accepted in macros

--- tests/ui/lexer/custom-suffixes-exponent-like.rs
+++ tests/ui/lexer/custom-suffixes-exponent-like.rs
@@ -7,4 +7,10 @@ const _C: f64 = 1e_______________0m;
 const _D: f64 = 1e_______________m;
     //~^ ERROR invalid suffix `e_______________m` for number literal
 
+// All the above patterns should not generate an error when used in a macro
+macro_rules! do_nothing {
+    ($($toks:tt)*) => {};
+}
+do_nothing!(1em 1e0m 1e_______________0m 1e_______________m);
+
 fn main() {}
