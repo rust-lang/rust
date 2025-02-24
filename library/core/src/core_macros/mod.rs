27c07311f25c8029bff43d2c49a466b51240d6b6
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2025-02-24 15:08:22 +0100

    Move debug_assert_matches back to its kin
    Needed for doc intra links

--- library/core/src/core_macros/mod.rs
+++ library/core/src/core_macros/mod.rs
@@ -194,55 +194,6 @@ pub macro assert_matches {
     },
 }
 
-/// Asserts that an expression matches the provided pattern.
-///
-/// This macro is generally preferable to `debug_assert!(matches!(value, pattern))`, because it can
-/// print the debug representation of the actual value shape that did not meet expectations. In
-/// contrast, using [`debug_assert!`] will only print that expectations were not met, but not why.
-///
-/// The pattern syntax is exactly the same as found in a match arm and the `matches!` macro. The
-/// optional if guard can be used to add additional checks that must be true for the matched value,
-/// otherwise this macro will panic.
-///
-/// On panic, this macro will print the value of the expression with its debug representation.
-///
-/// Like [`assert!`], this macro has a second form, where a custom panic message can be provided.
-///
-/// Unlike [`assert_matches!`], `debug_assert_matches!` statements are only enabled in non optimized
-/// builds by default. An optimized build will not execute `debug_assert_matches!` statements unless
-/// `-C debug-assertions` is passed to the compiler. This makes `debug_assert_matches!` useful for
-/// checks that are too expensive to be present in a release build but may be helpful during
-/// development. The result of expanding `debug_assert_matches!` is always type checked.
-///
-/// # Examples
-///
-/// ```
-/// use std::macros::debug_assert_matches;
-///
-/// let a = Some(345);
-/// let b = Some(56);
-/// debug_assert_matches!(a, Some(_));
-/// debug_assert_matches!(b, Some(_));
-///
-/// debug_assert_matches!(a, Some(345));
-/// debug_assert_matches!(a, Some(345) | None);
-///
-/// // debug_assert_matches!(a, None); // panics
-/// // debug_assert_matches!(b, Some(345)); // panics
-/// // debug_assert_matches!(b, Some(345) | None); // panics
-///
-/// debug_assert_matches!(a, Some(x) if x > 100);
-/// // debug_assert_matches!(a, Some(x) if x < 100); // panics
-/// ```
-#[stable(feature = "assert_matches", since = "CURRENT_RUSTC_VERSION")]
-#[allow_internal_unstable(assert_matches)]
-#[rustc_macro_transparency = "semitransparent"]
-pub macro debug_assert_matches($($arg:tt)*) {
-    if $crate::cfg!(debug_assertions) {
-        $crate::macros::assert_matches!($($arg)*);
-    }
-}
-
 /// A macro for defining `#[cfg]` match-like statements.
 ///
 /// It is similar to the `if/elif` C preprocessor macro by allowing definition of a cascade of
@@ -415,6 +366,55 @@ macro_rules! debug_assert_ne {
             $crate::assert_ne!($($arg)*);
         }
     };
+}
+
+/// Asserts that an expression matches the provided pattern.
+///
+/// This macro is generally preferable to `debug_assert!(matches!(value, pattern))`, because it can
+/// print the debug representation of the actual value shape that did not meet expectations. In
+/// contrast, using [`debug_assert!`] will only print that expectations were not met, but not why.
+///
+/// The pattern syntax is exactly the same as found in a match arm and the `matches!` macro. The
+/// optional if guard can be used to add additional checks that must be true for the matched value,
+/// otherwise this macro will panic.
+///
+/// On panic, this macro will print the value of the expression with its debug representation.
+///
+/// Like [`assert!`], this macro has a second form, where a custom panic message can be provided.
+///
+/// Unlike [`assert_matches!`], `debug_assert_matches!` statements are only enabled in non optimized
+/// builds by default. An optimized build will not execute `debug_assert_matches!` statements unless
+/// `-C debug-assertions` is passed to the compiler. This makes `debug_assert_matches!` useful for
+/// checks that are too expensive to be present in a release build but may be helpful during
+/// development. The result of expanding `debug_assert_matches!` is always type checked.
+///
+/// # Examples
+///
+/// ```
+/// use std::macros::debug_assert_matches;
+///
+/// let a = Some(345);
+/// let b = Some(56);
+/// debug_assert_matches!(a, Some(_));
+/// debug_assert_matches!(b, Some(_));
+///
+/// debug_assert_matches!(a, Some(345));
+/// debug_assert_matches!(a, Some(345) | None);
+///
+/// // debug_assert_matches!(a, None); // panics
+/// // debug_assert_matches!(b, Some(345)); // panics
+/// // debug_assert_matches!(b, Some(345) | None); // panics
+///
+/// debug_assert_matches!(a, Some(x) if x > 100);
+/// // debug_assert_matches!(a, Some(x) if x < 100); // panics
+/// ```
+#[stable(feature = "assert_matches", since = "CURRENT_RUSTC_VERSION")]
+#[allow_internal_unstable(assert_matches)]
+#[rustc_macro_transparency = "semitransparent"]
+pub macro debug_assert_matches($($arg:tt)*) {
+    if $crate::cfg!(debug_assertions) {
+        $crate::macros::assert_matches!($($arg)*);
+    }
 }
 
 /// Returns whether the given expression matches the provided pattern.
