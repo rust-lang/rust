660241d1d47ac939e88163bab747c853be82eac0
Yotam Ofek <yotam.ofek@gmail.com>
2025-02-24 13:28:55 +0000

    Fix test directives that were accidentally ignored

--- tests/run-make/rustdoc-themes/foo.rs
+++ tests/run-make/rustdoc-themes/foo.rs
@@ -1,4 +1,4 @@
-// @has test.css
-// @has foo/struct.Foo.html
-// @has - '//*[@name="rustdoc-vars"]/@data-themes' 'test'
+//@ has test.css
+//@ has foo/struct.Foo.html
+//@ has - '//*[@name="rustdoc-vars"]/@data-themes' 'test'
 pub struct Foo;
