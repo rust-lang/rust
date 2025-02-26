ded8d109a1c2e53b3242228a7a197823f23cc8e2
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-02-26 14:05:24 +0000

    Only run the symbols-all-mangled test on Linux
    Other platforms may add other unmangled symbols through crt1.o and such.

--- tests/run-make/symbols-all-mangled/rmake.rs
+++ tests/run-make/symbols-all-mangled/rmake.rs
@@ -1,4 +1,5 @@
 // Check that all symbols in cdylibs, staticlibs and bins are mangled
+//@ only-linux
 
 use run_make_support::object::read::{Object, ObjectSymbol};
 use run_make_support::{bin_name, dynamic_lib_name, object, rfs, rustc, static_lib_name};
