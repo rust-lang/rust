4bb9a73e0c354fbe41f55353bd037255855984cc
Scott McMurray <scottmcm@users.noreply.github.com>
2025-02-26 17:40:06 -0800

    Add more MIR tests around Option handling

--- tests/mir-opt/pre-codegen/simple_option_map.rs
+++ tests/mir-opt/pre-codegen/simple_option_map.rs
@@ -17,6 +17,11 @@ pub fn ezmap(x: Option<i32>) -> Option<i32> {
     map(x, |n| n + 1)
 }
 
+// EMIT_MIR simple_option_map.map_via_question_mark.PreCodegen.after.mir
+pub fn map_via_question_mark(x: Option<i32>) -> Option<i32> {
+    Some(x? + 1)
+}
+
 fn main() {
     assert_eq!(None, ezmap(None));
 }
