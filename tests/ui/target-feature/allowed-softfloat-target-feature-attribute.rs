7290f59d6da6e6a4fa6caff1d79763b9e4ecb396
David Wood <david.wood2@arm.com>
2025-02-27 23:36:31 +0000

    tests: `const {Meta,}Sized` in non-minicore
    Now that `MetaSized` and `Sized` are const traits, this must be reflected
    in tests without minicore which define these language items.

--- tests/ui/target-feature/allowed-softfloat-target-feature-attribute.rs
+++ tests/ui/target-feature/allowed-softfloat-target-feature-attribute.rs
@@ -1,16 +1,18 @@
 //@ compile-flags: --target=x86_64-unknown-none --crate-type=lib
 //@ needs-llvm-components: x86
 //@ build-pass
-#![feature(no_core, lang_items, x87_target_feature)]
+#![feature(no_core, lang_items, x87_target_feature, const_trait_impl)]
 #![no_core]
 
 #[lang = "pointeesized"]
 pub trait PointeeSized {}
 
 #[lang = "metasized"]
+#[const_trait]
 pub trait MetaSized: PointeeSized {}
 
 #[lang = "sized"]
+#[const_trait]
 pub trait Sized: MetaSized {}
 
 #[target_feature(enable = "x87")]
