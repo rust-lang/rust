643238f09e9e2785dc7449a73f0ad345ab6360e1
David Wood <david.wood2@arm.com>
2025-02-27 21:57:43 +0000

    tests: bless remaining tests
    These tests just need blessing, they don't have any interesting behaviour
    changes.

--- tests/ui/traits/next-solver/overflow/recursive-self-normalization.rs
+++ tests/ui/traits/next-solver/overflow/recursive-self-normalization.rs
@@ -14,6 +14,7 @@ fn test<T: Foo<Assoc = <T as Foo>::Assoc>>() {
     //~| ERROR overflow evaluating the requirement `<T as Foo>::Assoc == _`
     //~| ERROR overflow evaluating the requirement `<T as Foo>::Assoc == _`
     //~| ERROR overflow evaluating the requirement `<T as Foo>::Assoc: Sized`
+    //~| ERROR overflow evaluating the requirement `<T as Foo>::Assoc: const Sized`
     //~| ERROR overflow evaluating the requirement `<T as Foo>::Assoc: Bar`
 }
 
