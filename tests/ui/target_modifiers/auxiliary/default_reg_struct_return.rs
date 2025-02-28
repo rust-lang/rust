7fdd193e23c2c1afaa071ee167232c07bd2a4be8
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2025-02-28 16:48:58 +0300

    tests: Unignore target modifier tests on all platforms
    These tests can be `check-pass` and do not need dynamic libraries.
    Also remove other unnecessary stuff from them.

--- tests/ui/target_modifiers/auxiliary/default_reg_struct_return.rs
+++ tests/ui/target_modifiers/auxiliary/default_reg_struct_return.rs
@@ -1,20 +1,7 @@
-//@ compile-flags: --target i686-unknown-linux-gnu -Cpanic=abort
-// Auxiliary build problems with aarch64-apple:
-// Shared library linking cc seems to convert "-m32" flag into -arch armv4t
-// Auxiliary build problems with i686-mingw: linker `cc` not found
-//@ only-x86
-//@ ignore-windows
-//@ ignore-apple
+//@ no-prefer-dynamic
+//@ compile-flags: --target i686-unknown-linux-gnu
 //@ needs-llvm-components: x86
+
+#![feature(no_core)]
 #![crate_type = "rlib"]
 #![no_core]
-#![feature(no_core, lang_items, repr_simd)]
-
-#[lang = "sized"]
-trait Sized {}
-#[lang = "copy"]
-trait Copy {}
-
-pub fn somefun() {}
-
-pub struct S;
