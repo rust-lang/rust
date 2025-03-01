340a45282ac8a076b0b058ac066a8872d78302b3
Jiahao XU <Jiahao_XU@outlook.com>
2025-03-02 00:33:51 +1100

    Stablize feature `anonymous_pipe`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- library/std/src/sys/anonymous_pipe/unsupported.rs
+++ library/std/src/sys/anonymous_pipe/unsupported.rs
@@ -7,14 +7,14 @@ pub fn pipe() -> io::Result<(AnonPipe, AnonPipe)> {
     Err(io::Error::UNSUPPORTED_PLATFORM)
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeReader> for Stdio {
     fn from(pipe: PipeReader) -> Self {
         pipe.0.diverge()
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeWriter> for Stdio {
     fn from(pipe: PipeWriter) -> Self {
         pipe.0.diverge()
