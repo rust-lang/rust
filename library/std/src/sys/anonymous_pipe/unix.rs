7acb0e3ae1a61a8d5cccd289add21791ac9efdde
Jiahao XU <Jiahao_XU@outlook.com>
2025-03-02 00:33:51 +1100

    Stablize feature `anonymous_pipe`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- library/std/src/sys/anonymous_pipe/unix.rs
+++ library/std/src/sys/anonymous_pipe/unix.rs
@@ -12,88 +12,88 @@ pub fn pipe() -> io::Result<(AnonPipe, AnonPipe)> {
     anon_pipe().map(|(rx, wx)| (rx.into_inner(), wx.into_inner()))
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsFd for PipeReader {
     fn as_fd(&self) -> BorrowedFd<'_> {
         self.0.as_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsRawFd for PipeReader {
     fn as_raw_fd(&self) -> RawFd {
         self.0.as_raw_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeReader> for OwnedFd {
     fn from(pipe: PipeReader) -> Self {
         FileDesc::into_inner(pipe.0)
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl FromRawFd for PipeReader {
     unsafe fn from_raw_fd(raw_fd: RawFd) -> Self {
         unsafe { Self(FileDesc::from_raw_fd(raw_fd)) }
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl IntoRawFd for PipeReader {
     fn into_raw_fd(self) -> RawFd {
         self.0.into_raw_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeReader> for Stdio {
     fn from(pipe: PipeReader) -> Self {
         Self::from(OwnedFd::from(pipe))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsFd for PipeWriter {
     fn as_fd(&self) -> BorrowedFd<'_> {
         self.0.as_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsRawFd for PipeWriter {
     fn as_raw_fd(&self) -> RawFd {
         self.0.as_raw_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeWriter> for OwnedFd {
     fn from(pipe: PipeWriter) -> Self {
         FileDesc::into_inner(pipe.0)
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl FromRawFd for PipeWriter {
     unsafe fn from_raw_fd(raw_fd: RawFd) -> Self {
         unsafe { Self(FileDesc::from_raw_fd(raw_fd)) }
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl IntoRawFd for PipeWriter {
     fn into_raw_fd(self) -> RawFd {
         self.0.into_raw_fd()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeWriter> for Stdio {
     fn from(pipe: PipeWriter) -> Self {
         Self::from(OwnedFd::from(pipe))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<OwnedFd> for PipeReader {
     fn from(owned_fd: OwnedFd) -> Self {
         Self(FileDesc::from_inner(owned_fd))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<OwnedFd> for PipeWriter {
     fn from(owned_fd: OwnedFd) -> Self {
         Self(FileDesc::from_inner(owned_fd))
--- library/std/src/sys/anonymous_pipe/windows.rs
+++ library/std/src/sys/anonymous_pipe/windows.rs
@@ -23,92 +23,92 @@ pub fn pipe() -> io::Result<(AnonPipe, AnonPipe)> {
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsHandle for PipeReader {
     fn as_handle(&self) -> BorrowedHandle<'_> {
         self.0.as_handle()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsRawHandle for PipeReader {
     fn as_raw_handle(&self) -> RawHandle {
         self.0.as_raw_handle()
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl FromRawHandle for PipeReader {
     unsafe fn from_raw_handle(raw_handle: RawHandle) -> Self {
         unsafe { Self(Handle::from_raw_handle(raw_handle)) }
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl IntoRawHandle for PipeReader {
     fn into_raw_handle(self) -> RawHandle {
         self.0.into_raw_handle()
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeReader> for OwnedHandle {
     fn from(pipe: PipeReader) -> Self {
         Handle::into_inner(pipe.0)
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeReader> for Stdio {
     fn from(pipe: PipeReader) -> Self {
         Self::from(OwnedHandle::from(pipe))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsHandle for PipeWriter {
     fn as_handle(&self) -> BorrowedHandle<'_> {
         self.0.as_handle()
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl AsRawHandle for PipeWriter {
     fn as_raw_handle(&self) -> RawHandle {
         self.0.as_raw_handle()
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl FromRawHandle for PipeWriter {
     unsafe fn from_raw_handle(raw_handle: RawHandle) -> Self {
         unsafe { Self(Handle::from_raw_handle(raw_handle)) }
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl IntoRawHandle for PipeWriter {
     fn into_raw_handle(self) -> RawHandle {
         self.0.into_raw_handle()
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeWriter> for OwnedHandle {
     fn from(pipe: PipeWriter) -> Self {
         Handle::into_inner(pipe.0)
     }
 }
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<PipeWriter> for Stdio {
     fn from(pipe: PipeWriter) -> Self {
         Self::from(OwnedHandle::from(pipe))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<OwnedHandle> for PipeReader {
     fn from(owned_handle: OwnedHandle) -> Self {
         Self(Handle::from_inner(owned_handle))
     }
 }
 
-#[unstable(feature = "anonymous_pipe", issue = "127154")]
+#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
 impl From<OwnedHandle> for PipeWriter {
     fn from(owned_handle: OwnedHandle) -> Self {
         Self(Handle::from_inner(owned_handle))
