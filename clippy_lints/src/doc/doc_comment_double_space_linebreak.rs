60191cfd5ba18ef83800e4599a9a7e073e12d1c9
xFrednet <xFrednet@gmail.com>
2025-03-01 11:43:30 +0100

    `doc_comment_double_space_linebreak`: Use multi spans and suggestions

--- clippy_lints/src/doc/doc_comment_double_space_linebreak.rs
+++ clippy_lints/src/doc/doc_comment_double_space_linebreak.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::diagnostics::span_lint_and_then;
 use rustc_errors::Applicability;
 use rustc_lint::LateContext;
 use rustc_span::{BytePos, Span};
@@ -6,15 +6,28 @@ use rustc_span::{BytePos, Span};
 use super::DOC_COMMENT_DOUBLE_SPACE_LINEBREAK;
 
 pub fn check(cx: &LateContext<'_>, collected_breaks: &[Span]) {
-    for r_span in collected_breaks {
-        span_lint_and_sugg(
-            cx,
-            DOC_COMMENT_DOUBLE_SPACE_LINEBREAK,
-            r_span.with_hi(r_span.lo() + BytePos(2)),
-            "doc comment uses two spaces for a hard line break",
-            "replace this double space with a backslash",
-            "\\".to_owned(),
-            Applicability::MachineApplicable,
-        );
+    if collected_breaks.is_empty() {
+        return;
     }
+
+    let breaks: Vec<_> = collected_breaks
+        .iter()
+        .map(|span| span.with_hi(span.lo() + BytePos(2)))
+        .collect();
+
+    span_lint_and_then(
+        cx,
+        DOC_COMMENT_DOUBLE_SPACE_LINEBREAK,
+        breaks.clone(),
+        "doc comment uses two spaces for a hard line break",
+        |diag| {
+            let suggs: Vec<_> = breaks.iter().map(|span| (*span, "\\".to_string())).collect();
+            diag.tool_only_multipart_suggestion(
+                "replace this double space with a backslash:",
+                suggs,
+                Applicability::MachineApplicable,
+            );
+            diag.help("replace this double space with a backslash: `\\`");
+        },
+    );
 }
