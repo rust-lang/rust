1b21952f0279bf39d0f7188638f1a33dc16e5143
Scott McMurray <scottmcm@users.noreply.github.com>
2025-03-02 13:59:00 -0800

    Also add a MIR pre-codegen test for the derived `PartialOrd::le`

--- tests/mir-opt/pre-codegen/derived_ord.rs
+++ tests/mir-opt/pre-codegen/derived_ord.rs
@@ -6,4 +6,11 @@
 #[derive(PartialOrd, PartialEq)]
 pub struct MultiField(char, i16);
 
+// Because this isn't derived by the impl, it's not on the `{impl#0}-partial_cmp`,
+// and thus we need to call it to see what the inlined generic one produces.
+pub fn demo_le(a: &MultiField, b: &MultiField) -> bool {
+    *a <= *b
+}
+
 // EMIT_MIR derived_ord.{impl#0}-partial_cmp.PreCodegen.after.mir
+// EMIT_MIR derived_ord.demo_le.PreCodegen.after.mir
