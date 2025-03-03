dfed028e788702a313ad49754e5d7ec64ffa61cf
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-03-03 19:59:12 +0100

    Always allow rustdoc-json tests to contain long lines
    The rustdoc-json test syntax often requires very long lines, so the checks
    for long lines aren't really useful.

--- tests/rustdoc-json/fn_pointer/generics.rs
+++ tests/rustdoc-json/fn_pointer/generics.rs
@@ -1,5 +1,3 @@
-// ignore-tidy-linelength
-
 //@ count "$.index[*][?(@.name=='WithHigherRankTraitBounds')].inner.type_alias.type.function_pointer.sig.inputs[*]" 1
 //@ is "$.index[*][?(@.name=='WithHigherRankTraitBounds')].inner.type_alias.type.function_pointer.sig.inputs[0][0]" '"val"'
 //@ is "$.index[*][?(@.name=='WithHigherRankTraitBounds')].inner.type_alias.type.function_pointer.sig.inputs[0][1].borrowed_ref.lifetime" \"\'c\"
