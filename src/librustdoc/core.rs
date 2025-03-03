0c4eaa5c985f69d3b6255a56799ee30d29e08d54
Esteban Küber <esteban@kuber.com.ar>
2025-03-03 20:18:56 +0000

    Use default field values for `ErrorOutputType`
    Remove manual `Default` impl from `config::ErrorOutputType`.

--- src/librustdoc/core.rs
+++ src/librustdoc/core.rs
@@ -153,7 +153,7 @@ pub(crate) fn new_dcx(
         false,
     );
     let emitter: Box<DynEmitter> = match error_format {
-        ErrorOutputType::HumanReadable(kind, color_config) => {
+        ErrorOutputType::HumanReadable { kind, color_config } => {
             let short = kind.short();
             Box::new(
                 HumanEmitter::new(stderr_destination(color_config), fallback_bundle)
