0ad5959add1ae47cc165a1ae966c1036ac979369
Boxy <rust@boxyuwu.dev>
2025-03-06 02:21:19 +0000

    Impossible preds const arg test

---
+++ tests/ui/const-generics/backcompat/impossible_preds_repeat_expr_hack.rs
@@ -0,0 +1,39 @@
+// Check that we emit the `CONST_EVALUATABLE_UNCHECKED` FCW when a repeat expr
+// count depends on in-scope where clauses to prove an impossible bound but does
+// not otherwise depend on any in-scope generic parameters.
+
+pub trait Unimplemented<'a> {}
+
+trait Trait<T>
+where
+    for<'a> T: Unimplemented<'a>,
+{
+    const ASSOC: usize;
+}
+
+impl<T> Trait<T> for u8
+where
+    for<'a> T: Unimplemented<'a>,
+{
+    const ASSOC: usize = 1;
+}
+
+pub fn impossible_preds_repeat_expr_count()
+where
+    for<'a> (): Unimplemented<'a>,
+{
+    let _a: [(); 1] = [(); <u8 as Trait<()>>::ASSOC];
+}
+
+struct Foo<const N: usize>;
+
+pub fn impossible_preds_normal_arg()
+where
+    for<'a> (): Unimplemented<'a>,
+{
+    let _a: Foo<1> = Foo::<{ <u8 as Trait<()>>::ASSOC }>;
+    //~^ ERROR: the trait bound `for<'a> (): Unimplemented<'a>`
+    //~| ERROR: the trait bound `for<'a> (): Unimplemented<'a>`
+}
+
+fn main() {}
