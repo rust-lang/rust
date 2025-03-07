8814679a5410b8d9ce8385094ec8ec888395b1a5
Zequan Wu <zequanwu@google.com>
2025-03-06 22:52:20 -0800

    rename Triple to Target

--- compiler/rustc_llvm/llvm-wrapper/RustWrapper.cpp
+++ compiler/rustc_llvm/llvm-wrapper/RustWrapper.cpp
@@ -152,11 +152,11 @@ extern "C" LLVMContextRef LLVMRustContextCreate(bool shouldDiscardNames) {
 }
 
 extern "C" void LLVMRustSetNormalizedTarget(LLVMModuleRef M,
-                                            const char *Triple) {
+                                            const char *Target) {
 #if LLVM_VERSION_GE(21, 0)
-  unwrap(M)->setTargetTriple(llvm::Triple(Triple::normalize(Triple)));
+  unwrap(M)->setTargetTriple(Triple(Triple::normalize(Target)));
 #else
-  unwrap(M)->setTargetTriple(Triple::normalize(Triple));
+  unwrap(M)->setTargetTriple(Triple::normalize(Target));
 #endif
 }
 
