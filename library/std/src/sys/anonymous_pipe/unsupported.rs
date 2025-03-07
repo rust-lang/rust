6863a99841f81ea18b9a26d053fd33a9700a8345
Jiahao XU <Jiahao_XU@outlook.com>
2025-03-08 00:36:15 +1100

    Mv os-specific trait impl of `Pipe*` into `std::os::*`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- library/std/src/sys/anonymous_pipe/unsupported.rs
+++ library/std/src/sys/anonymous_pipe/unsupported.rs
@@ -1,22 +1,7 @@
-use crate::io::{self, PipeReader, PipeWriter};
-use crate::process::Stdio;
+use crate::io;
 pub use crate::sys::pipe::AnonPipe;
 
 #[inline]
 pub fn pipe() -> io::Result<(AnonPipe, AnonPipe)> {
     Err(io::Error::UNSUPPORTED_PLATFORM)
-}
-
-#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
-impl From<PipeReader> for Stdio {
-    fn from(pipe: PipeReader) -> Self {
-        pipe.0.diverge()
-    }
-}
-
-#[stable(feature = "anonymous_pipe", since = "CURRENT_RUSTC_VERSION")]
-impl From<PipeWriter> for Stdio {
-    fn from(pipe: PipeWriter) -> Self {
-        pipe.0.diverge()
-    }
 }
