1ceebfe8a08b6d1be68431e70db2dcdb2fac77c9
Urgau <urgau@numericable.fr>
2025-03-09 22:23:31 +0100

    Allow `invalid_null_arguments` in some tests

--- tests/ui/precondition-checks/copy.rs
+++ tests/ui/precondition-checks/copy.rs
@@ -3,6 +3,8 @@
 //@ error-pattern: unsafe precondition(s) violated: ptr::copy requires
 //@ revisions: null_src null_dst misaligned_src misaligned_dst
 
+#![allow(invalid_null_arguments)]
+
 use std::ptr;
 
 fn main() {
