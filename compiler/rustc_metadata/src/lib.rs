d577883f929b00b8a35204acef10e40f58f064d5
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2025-03-09 14:06:51 +0300

    metadata: Ignore sysroot when doing the manual native lib search in rustc

--- compiler/rustc_metadata/src/lib.rs
+++ compiler/rustc_metadata/src/lib.rs
@@ -35,8 +35,8 @@ pub mod locator;
 pub use creader::{DylibError, load_symbol_from_dylib};
 pub use fs::{METADATA_FILENAME, emit_wrapper_file};
 pub use native_libs::{
-    find_native_static_library, try_find_native_dynamic_library, try_find_native_static_library,
-    walk_native_lib_search_dirs,
+    NativeLibSearchFallback, find_native_static_library, try_find_native_dynamic_library,
+    try_find_native_static_library, walk_native_lib_search_dirs,
 };
 pub use rmeta::{EncodedMetadata, METADATA_HEADER, encode_metadata, rendered_const};
 
