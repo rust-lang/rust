f5dd3d13fc4685b2846f130f5e5b633c50cefc55
Nicole L <dlegare.1001@gmail.com>
2025-03-10 12:54:59 -0700

    Update Trusty support to account for recent libstd reorganization

--- library/std/src/sys/pal/trusty/mod.rs
+++ library/std/src/sys/pal/trusty/mod.rs
@@ -7,19 +7,12 @@ pub mod args;
 mod common;
 #[path = "../unsupported/env.rs"]
 pub mod env;
-#[path = "../unsupported/fs.rs"]
-pub mod fs;
-#[path = "../unsupported/io.rs"]
-pub mod io;
-#[path = "../unsupported/net.rs"]
-pub mod net;
 #[path = "../unsupported/os.rs"]
 pub mod os;
 #[path = "../unsupported/pipe.rs"]
 pub mod pipe;
 #[path = "../unsupported/process.rs"]
 pub mod process;
-pub mod stdio;
 #[path = "../unsupported/thread.rs"]
 pub mod thread;
 #[path = "../unsupported/time.rs"]
