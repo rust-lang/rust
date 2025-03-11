f7da7ba2330530842e6e349b792ea46fa55a57f5
Ding Xiang Fei <dingxiangfei2009@protonmail.ch>
2025-03-11 23:20:52 +0800

    changes to tests upfront

--- tests/ui/async-await/issue-74072-lifetime-name-annotations.rs
+++ tests/ui/async-await/issue-74072-lifetime-name-annotations.rs
@@ -9,27 +9,27 @@ pub async fn async_fn(x: &mut i32) -> &i32 {
     y
 }
 
-pub fn async_closure(x: &mut i32) -> impl Future<Output=&i32> {
+pub fn async_closure(x: &mut i32) -> impl Future<Output = &i32> {
     (async move || {
         //~^ ERROR lifetime may not live long enough
         //~| ERROR temporary value dropped while borrowed
         let y = &*x;
-        *x += 1; //~ ERROR cannot assign to `*x` because it is borrowed
+        *x += 1; //~ ERROR cannot assign to value because it is borrowed
         y
     })()
 }
 
-pub fn async_closure_explicit_return_type(x: &mut i32) -> impl Future<Output=&i32> {
+pub fn async_closure_explicit_return_type(x: &mut i32) -> impl Future<Output = &i32> {
     (async move || -> &i32 {
         //~^ ERROR lifetime may not live long enough
         //~| ERROR temporary value dropped while borrowed
         let y = &*x;
-        *x += 1; //~ ERROR cannot assign to `*x` because it is borrowed
+        *x += 1; //~ ERROR cannot assign to value because it is borrowed
         y
     })()
 }
 
-pub fn async_block(x: &mut i32) -> impl Future<Output=&i32> {
+pub fn async_block(x: &mut i32) -> impl Future<Output = &i32> {
     async move {
         let y = &*x;
         *x += 1; //~ ERROR cannot assign to `*x` because it is borrowed
