9882eca151a4c1c604597acd98072babb9e6ffcf
Oli Scherer <github333195615777966@oli-obk.de>
2025-03-11 08:41:15 +0000

    Remove some dead code

--- compiler/rustc_monomorphize/src/partitioning/autodiff.rs
+++ compiler/rustc_monomorphize/src/partitioning/autodiff.rs
@@ -21,11 +21,6 @@ fn adjust_activity_to_abi<'tcx>(tcx: TyCtxt<'tcx>, fn_ty: Ty<'tcx>, da: &mut Vec
     let mut new_positions = vec![];
     for (i, ty) in sig.inputs().iter().enumerate() {
         if let Some(inner_ty) = ty.builtin_deref(true) {
-            if ty.is_fn_ptr() {
-                // FIXME(ZuseZ4): add a nicer error, or just figure out how to support them,
-                // since Enzyme itself can handle them.
-                tcx.dcx().err("function pointers are currently not supported in autodiff");
-            }
             if inner_ty.is_slice() {
                 // We know that the length will be passed as extra arg.
                 if !da.is_empty() {
