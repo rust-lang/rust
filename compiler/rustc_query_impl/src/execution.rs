6661c27998c63a6f7a5662ac7a5b2814cee8657f
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2025-03-12 18:13:49 +0100

    Use `horde`'s `SyncTable` for default query caches and `CtxtInterners`

--- compiler/rustc_query_impl/src/execution.rs
+++ compiler/rustc_query_impl/src/execution.rs
@@ -3,6 +3,7 @@ use std::mem::ManuallyDrop;
 use std::num::NonZero;
 
 use rustc_data_structures::hash_table::Entry;
+use rustc_data_structures::sync::collect;
 use rustc_data_structures::{defer, outline, sharded, sync};
 use rustc_errors::FatalError;
 use rustc_middle::dep_graph::{
@@ -269,6 +270,10 @@ fn try_execute_query<'tcx, C: QueryCache, const INCR: bool>(
             // Tell the guard to insert `value` in the cache and remove the status entry from
             // `query.state`.
             job_guard.complete(&query.cache, value, dep_node_index);
+
+            // Periodic memory reclamation trigger.
+            // We do this after `complete` since the default caches may have memory to free due to table expansion.
+            collect::collect();
 
             (value, Some(dep_node_index))
         }
