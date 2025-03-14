c06fbc390c006a9c5cac27e64090dddb7dbbbfff
lcnr <rust@lcnr.de>
2025-03-14 12:47:08 +0100

    remove `feature(inline_const_pat)`

---
+++ tests/ui/inline-const/in-pat-recovery.rs
@@ -0,0 +1,11 @@
+// While `feature(inline_const_pat)` has been removed from the
+// compiler, we should still make sure that the resulting error
+// message is acceptable.
+fn main() {
+    match 1 {
+        const { 1 + 7 } => {}
+        //~^ `inline_const_pat` has been removed
+        2 => {}
+        _ => {}
+    }
+}
