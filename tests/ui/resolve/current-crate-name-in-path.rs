6313c2fcf36205360eeb0e8254196a94751b3907
Sloane Vaznova <sloane.vaznova@gmail.com>
2025-03-15 00:28:23 -0500

    Fix UI test annotations
    (The `help` annotation was missing due to an issue that, admittedly,
    resulted from a typo I stared at and didn't catch.)

--- tests/ui/resolve/current-crate-name-in-path.rs
+++ tests/ui/resolve/current-crate-name-in-path.rs
@@ -7,6 +7,7 @@ mod bar {
 
 use current_crate_name_in_path::bar::baz;
 //~^ ERROR failed to resolve: use of unresolved module or unlinked crate `current_crate_name_in_path`
+//~| HELP the current crate name can not be used in a path, use the keyword `crate` instead
 //~| SUGGESTION crate
 
 fn main() {}
