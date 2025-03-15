41f6339d70ce59f506838373c66cbdadb4c7fa3c
Scott McMurray <scottmcm@users.noreply.github.com>
2025-03-14 20:05:14 -0700

    This test is "fixed", though not in any meaningful way.  Intentionally *not* marking the issue as fixed, because it's really not.
    (There MIR tests really need to be regressed with MIR tests, not with UI tests.)

--- tests/ui/sized/peek-iter-of-str.rs
+++ tests/ui/sized/peek-iter-of-str.rs
@@ -1,5 +1,9 @@
-//@ known-bug: #136442
+//@ build-pass
 //@ compile-flags: -Copt-level=0 -Zmir-enable-passes=+Inline -Zmir-enable-passes=+JumpThreading --crate-type lib
+
+// Regression test for <https://github.com/rust-lang/rust/issues/136442>
+// Used to fail in MIR but #138526 (accidentally) worked around that.
+
 pub fn problem_thingy(items: &mut impl Iterator<Item = str>) {
     let mut peeker = items.peekable();
     match peeker.peek() {
