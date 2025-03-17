cc2ed5ae136fc1b0f6a0132b9623d5a837ded306
xizheyin <xizheyin@smail.nju.edu.cn>
2025-03-17 22:22:32 +0800

    Report span of test when should_panic test failed
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

---
+++ tests/ui/test-should-panic-failed-show-span.rs
@@ -0,0 +1,41 @@
+//@ run-fail
+//@ check-run-results
+//@ compile-flags: --test
+//@ run-flags: --test-threads=1
+
+#[test]
+#[should_panic]
+fn should_panic_with_any_message() {
+    panic!("Panic!");
+}
+
+#[test]
+#[should_panic = "message"]
+fn should_panic_with_message() {
+    panic!("message");
+}
+
+#[test]
+#[should_panic]
+fn should_panic_with_any_message_does_not_panic() {
+    // DON'T PANIC
+}
+
+
+#[test]
+#[should_panic = "message"]
+fn should_panic_with_message_does_not_panic() {
+    // DON'T PANIC
+}
+
+#[test]
+#[should_panic = "message"]
+fn should_panic_with_substring_panics_with_incorrect_string() {
+    panic!("ZOMGWTFBBQ");
+}
+
+#[test]
+#[should_panic = "message"]
+fn should_panic_with_substring_panics_with_non_string_value() {
+    panic!(123); //~ WARNING panic message is not a string literal
+}
