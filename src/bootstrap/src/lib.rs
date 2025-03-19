68aaa8d103f8d17dc9fc721a80ceb45d2ec9585d
Jakub Beránek <berykubik@gmail.com>
2025-03-19 14:40:38 +0100

    Allow unused code in tests
    To avoid working around some code being unused in tests due to it being stubbed out with `#[cfg(test)]`.

--- src/bootstrap/src/lib.rs
+++ src/bootstrap/src/lib.rs
@@ -15,6 +15,7 @@
 //!
 //! More documentation can be found in each respective module below, and you can
 //! also check out the `src/bootstrap/README.md` file for more information.
+#![cfg_attr(test, allow(unused))]
 
 use std::cell::{Cell, RefCell};
 use std::collections::{BTreeSet, HashMap, HashSet};
