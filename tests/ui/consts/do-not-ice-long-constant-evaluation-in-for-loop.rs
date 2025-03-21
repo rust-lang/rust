cd7a8f53dbb55537aef1b1ed8e28e0fbfa750908
Shunpoco <tkngsnsk313320@gmail.com>
2025-03-21 19:23:42 +0000

    Fix: early return if typeck_results is tainted by errors

--- tests/ui/consts/do-not-ice-long-constant-evaluation-in-for-loop.rs
+++ tests/ui/consts/do-not-ice-long-constant-evaluation-in-for-loop.rs
@@ -1,6 +1,6 @@
 // Issue-125323
 fn main() {
-    for _ in 0..0 { //~ ERROR type annotations needed [E0282]
+    for _ in 0..0 {
         [(); loop {}]; //~ ERROR constant evaluation is taking a long time
     }
 }
