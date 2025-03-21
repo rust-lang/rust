0ac6b409f673ab12c48d92238da8d1f402bdb909
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-03-21 11:37:18 +0100

    CI: use aws codebuild for dist-arm-linux job

--- src/ci/docker/run.sh
+++ src/ci/docker/run.sh
@@ -288,7 +288,7 @@ args="$args --privileged"
 # `LOCAL_USER_ID` (recognized in `src/ci/run.sh`) to ensure that files are all
 # read/written as the same user as the bare-metal user.
 if [ -f /.dockerenv ]; then
-  docker create -v /checkout --name checkout alpine:3.4 /bin/true
+  docker create -v /checkout --name checkout ghcr.io/rust-lang/alpine:3.4 /bin/true
   docker cp . checkout:/checkout
   args="$args --volumes-from checkout"
 else
@@ -296,7 +296,6 @@ else
   args="$args --volume $objdir:/checkout/obj"
   args="$args --volume $HOME/.cargo:/cargo"
   args="$args --volume /tmp/toolstate:/tmp/toolstate"
-
   id=$(id -u)
   if [[ "$id" != 0 && "$(docker version)" =~ Podman ]]; then
     # Rootless podman creates a separate user namespace, where an inner
@@ -307,6 +306,21 @@ else
     args="$args --env LOCAL_USER_ID=$id"
   fi
 fi
+
+# id=$(id -u)
+# if [[ "$id" != 0 && "$(docker version)" =~ Podman ]]; then
+#   # Rootless podman creates a separate user namespace, where an inner
+#   # LOCAL_USER_ID will map to a different subuid range on the host.
+#   # The "keep-id" mode maps the current UID directly into the container.
+#   args="$args --env NO_CHANGE_USER=1 --userns=keep-id"
+# elif [[ "$id" != 0 ]]; then
+#   # We are running in docker as non-root
+#   args="$args --env LOCAL_USER_ID=$id"
+# else
+#   # We are running as root. Since we don't want to run the container as root,
+#   # we set id `1001` instead of `0`.
+#   args="$args --env LOCAL_USER_ID=1001"
+# fi
 
 if [ "$dev" = "1" ]
 then
