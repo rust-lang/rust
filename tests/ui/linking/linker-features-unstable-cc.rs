9d33272f578aee6276955135660966f9cbeb2a9f
Rémy Rakic <remy.rakic+github@gmail.com>
2025-03-24 12:18:24 +0100

    add test for unstable values of `-Clink-self-contained`

--- tests/ui/linking/linker-features-unstable-cc.rs
+++ tests/ui/linking/linker-features-unstable-cc.rs
@@ -1,8 +1,13 @@
-// Check that -CLinker-features with anything else than lld requires -Zunstable-options.
+// Check that only `-C linker-features=[+-]lld` is stable on x64 linux, and that other linker
+// features require using `-Z unstable-options`.
 //
-//@ check-fail
+// Note that, currently, only `lld` is parsed on the CLI, but that other linker features can exist
+// internally (`cc`).
+//
 //@ compile-flags: --target=x86_64-unknown-linux-gnu -C linker-features=+cc --crate-type=rlib
 //@ needs-llvm-components: x86
 
 #![feature(no_core)]
 #![no_core]
+
+//~? ERROR incorrect value `+cc` for codegen option `linker-features` - a list of enabled (`+` prefix) and disabled (`-` prefix) features: `lld` was expected
