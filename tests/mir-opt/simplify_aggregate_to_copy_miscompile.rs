70daee7698ec5b9cd4d670e28f1c626f430ca496
dianqk <dianqk@dianqk.net>
2025-03-25 22:21:18 +0800

    Invalid dereferences for all non-local mutation

--- tests/mir-opt/simplify_aggregate_to_copy_miscompile.rs
+++ tests/mir-opt/simplify_aggregate_to_copy_miscompile.rs
@@ -9,15 +9,16 @@
 #![crate_type = "lib"]
 //@ test-mir-pass: GVN
 #![allow(internal_features)]
-#![feature(rustc_attrs, core_intrinsics)]
+#![feature(core_intrinsics, custom_mir, rustc_attrs)]
+
+use std::intrinsics::mir::*;
 
 // EMIT_MIR simplify_aggregate_to_copy_miscompile.foo.GVN.diff
-#[no_mangle]
 fn foo(v: &mut Option<i32>) -> Option<i32> {
     // CHECK-LABEL: fn foo(
     // CHECK-SAME: [[v:_.*]]: &mut Option<i32>
     // CHECK: [[v_alias_1:_.*]] = &(*_1)
-    // CHECK-NEXT: [[v_alias_2:_.*]] = get(move [[v_alias_1]])
+    // CHECK-NEXT: [[v_alias_2:_.*]] = get::<Option<i32>>(move [[v_alias_1]])
     // CHECK: (*[[v]]) = const Option::<i32>::None;
     // CHECK-NOT: _0 = copy (*[[v_alias_2]])
     // CHECK: _0 = Option::<i32>::Some
@@ -30,9 +31,31 @@ fn foo(v: &mut Option<i32>) -> Option<i32> {
     }
 }
 
-#[no_mangle]
+pub enum Value {
+    V0(i32),
+    V1(i32),
+}
+
+// EMIT_MIR simplify_aggregate_to_copy_miscompile.set_discriminant.GVN.diff
+#[custom_mir(dialect = "runtime", phase = "initial")]
+fn set_discriminant(v: &mut Value) -> Value {
+    // CHECK-LABEL: fn set_discriminant(
+    mir! {
+        let v_: &Value;
+        {
+            Call(v_ = get(v), ReturnTo(ret), UnwindUnreachable())
+        }
+        ret = {
+            let col: i32 = Field(Variant(*v_, 0), 0);
+            SetDiscriminant(*v, 1);
+            RET = Value::V0(col);
+            Return()
+        }
+    }
+}
+
 #[inline(never)]
 #[rustc_nounwind]
-fn get(v: &Option<i32>) -> &Option<i32> {
+fn get<T>(v: &T) -> &T {
     v
 }
