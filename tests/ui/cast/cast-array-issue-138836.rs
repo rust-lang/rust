a34c42fefabc774af2d8b2c5a5a100c0697d2ba9
xizheyin <xizheyin@smail.nju.edu.cn>
2025-03-26 11:31:58 +0800

    Expect an array when expected and acutal types are both arrays during cast
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- tests/ui/cast/cast-array-issue-138836.rs
+++ tests/ui/cast/cast-array-issue-138836.rs
@@ -1,5 +1,5 @@
 fn main() {
     let a: [u8; 3] = [1,2,3];
     let b = &a;
-    let c = b as *const [u32; 3]; //~ ERROR mismatched types [E0308]
+    let c = b as *const [u32; 3]; //~ ERROR casting `&[u8; 3]` as `*const [u32; 3]` is invalid
 }
