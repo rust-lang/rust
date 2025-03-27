3ea2e5600f289b91d66b129d1b3f1563859a726e
bendn <bend.n@outlook.com>
2025-03-27 13:45:16 +0700

    allow unnecessary transmutes

--- library/stdarch/crates/core_arch/src/lib.rs
+++ library/stdarch/crates/core_arch/src/lib.rs
@@ -43,6 +43,7 @@
 )]
 #![cfg_attr(test, feature(test, abi_vectorcall, stdarch_internal))]
 #![deny(clippy::missing_inline_in_public_items)]
+#![allow(unknown_lints, unnecessary_transmutes)]
 #![allow(
     clippy::identity_op,
     clippy::inline_always,
