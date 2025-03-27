2e119b293a52374a70b4204d9db9b083355a341c
joboet <jonasboettiger@icloud.com>
2025-03-27 14:20:25 +0100

    std: get rid of `sys_common::process`
    Move the public `CommandEnvs` into the `process` module (and make it a wrapper type for an internal iterator type) and everything else into `sys::process` as per #117276.

--- library/std/src/sys/process/unix/unsupported.rs
+++ library/std/src/sys/process/unix/unsupported.rs
@@ -18,13 +18,13 @@ impl Command {
         unsupported()
     }
 
-    pub fn output(&mut self) -> io::Result<(ExitStatus, Vec<u8>, Vec<u8>)> {
-        unsupported()
-    }
-
     pub fn exec(&mut self, _default: Stdio) -> io::Error {
         unsupported_err()
     }
+}
+
+pub fn output(_: &mut Command) -> io::Result<(ExitStatus, Vec<u8>, Vec<u8>)> {
+    unsupported()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
