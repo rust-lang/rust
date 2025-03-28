6537278e114a98ea6f2c80d7d7ffacb2868be770
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-03-29 00:54:22 +0100

    Update rustdoc tests

--- tests/rustdoc-ui/lints/doc_cfg_hide.rs
+++ tests/rustdoc-ui/lints/doc_cfg_hide.rs
@@ -1,7 +1,2 @@
-#![feature(doc_cfg_hide)]
-
-#![doc(cfg_hide = "test")] //~ ERROR
-#![doc(cfg_hide)] //~ ERROR
-
-#[doc(cfg_hide(doc))] //~ ERROR
-pub fn foo() {}
+#![doc(auto_cfg(hide = "test"))] //~ ERROR
+#![doc(auto_cfg(hide))] //~ ERROR
