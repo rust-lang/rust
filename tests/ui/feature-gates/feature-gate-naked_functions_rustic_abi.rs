41ddf8672231c1f8cfa0fc754c1653f151030b19
Folkert de Vries <folkert@folkertdev.nl>
2025-03-29 17:30:11 +0100

    Make `#[naked]` an unsafe attribute

--- tests/ui/feature-gates/feature-gate-naked_functions_rustic_abi.rs
+++ tests/ui/feature-gates/feature-gate-naked_functions_rustic_abi.rs
@@ -5,19 +5,19 @@
 
 use std::arch::naked_asm;
 
-#[naked]
+#[unsafe(naked)]
 pub unsafe fn rust_implicit() {
     //~^ ERROR `#[naked]` is currently unstable on `extern "Rust"` functions
     naked_asm!("ret");
 }
 
-#[naked]
+#[unsafe(naked)]
 pub unsafe extern "Rust" fn rust_explicit() {
     //~^ ERROR `#[naked]` is currently unstable on `extern "Rust"` functions
     naked_asm!("ret");
 }
 
-#[naked]
+#[unsafe(naked)]
 pub unsafe extern "rust-cold" fn rust_cold() {
     //~^ ERROR `#[naked]` is currently unstable on `extern "rust-cold"` functions
     naked_asm!("ret");
