93341ca1d0f8608ac95acf72230f01a0becd02af
Luca Cominardi <luca.cominardi@gmail.com>
2025-03-31 12:04:44 +0200

    Update tracking issue to 139166

--- library/std/src/net/udp.rs
+++ library/std/src/net/udp.rs
@@ -596,7 +596,7 @@ impl UdpSocket {
     /// let socket = UdpSocket::bind("[::1]:12345").expect("couldn't bind to address");
     /// socket.set_hop_limit_v6(88).expect("set_hop_limit_v6 call failed");
     /// ```
-    #[unstable(feature = "ipv6_hop_limit", issue = "47727")]
+    #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
     pub fn set_hop_limit_v6(&self, limit: u8) -> io::Result<()> {
         self.0.set_hop_limit_v6(limit)
     }
@@ -615,7 +615,7 @@ impl UdpSocket {
     /// socket.set_hop_limit_v6(88).expect("set_hop_limit_v6 call failed");
     /// assert_eq!(socket.hop_limit_v6().unwrap(), 88);
     /// ```
-    #[unstable(feature = "ipv6_hop_limit", issue = "47727")]
+    #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
     pub fn hop_limit_v6(&self) -> io::Result<u8> {
         self.0.hop_limit_v6()
     }
@@ -634,7 +634,7 @@ impl UdpSocket {
     /// let socket = UdpSocket::bind("[::1]:12345").expect("couldn't bind to address");
     /// socket.set_multicast_hop_limit_v6(88).expect("set_multicast_hop_limit_v6 call failed");
     /// ```
-    #[unstable(feature = "ipv6_hop_limit", issue = "47727")]
+    #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
     pub fn set_multicast_hop_limit_v6(&self, limit: u8) -> io::Result<()> {
         self.0.set_multicast_hop_limit_v6(limit)
     }
@@ -653,7 +653,7 @@ impl UdpSocket {
     /// socket.set_multicast_hop_limit_v6(88).expect("set_multicast_hop_limit_v6 call failed");
     /// assert_eq!(socket.multicast_hop_limit_v6().unwrap(), 88);
     /// ```
-    #[unstable(feature = "ipv6_hop_limit", issue = "47727")]
+    #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
     pub fn multicast_hop_limit_v6(&self) -> io::Result<u8> {
         self.0.multicast_hop_limit_v6()
     }
