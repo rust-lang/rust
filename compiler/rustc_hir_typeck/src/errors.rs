ab64774ec75dfb8ecbd048314c909dabffbb36b0
beetrees <b@beetr.ee>
2025-03-31 23:18:09 +0100

    Add FCW for unsuffixed float literal `f32` fallback

--- compiler/rustc_hir_typeck/src/errors.rs
+++ compiler/rustc_hir_typeck/src/errors.rs
@@ -971,3 +971,11 @@ pub(crate) struct RegisterTypeUnstable<'a> {
     pub span: Span,
     pub ty: Ty<'a>,
 }
+
+#[derive(LintDiagnostic)]
+#[diag(hir_typeck_float_literal_f32_fallback)]
+pub(crate) struct FloatLiteralF32Fallback {
+    pub literal: String,
+    #[suggestion(code = "{literal}_f32", applicability = "machine-applicable")]
+    pub span: Option<Span>,
+}
