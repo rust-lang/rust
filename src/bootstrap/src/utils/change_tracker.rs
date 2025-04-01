df5a0017b2e6ce0dd31089f788f509d66a16c36c
onur-ozkan <work@onurozkan.dev>
2025-04-01 09:59:28 +0300

    add change-entry
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/bootstrap/src/utils/change_tracker.rs
+++ src/bootstrap/src/utils/change_tracker.rs
@@ -396,4 +396,9 @@ pub const CONFIG_CHANGE_HISTORY: &[ChangeInfo] = &[
         severity: ChangeSeverity::Info,
         summary: "Added a new option `build.compiletest-use-stage0-libtest` to force `compiletest` to use the stage 0 libtest.",
     },
+    ChangeInfo {
+        change_id: 119899,
+        severity: ChangeSeverity::Warning,
+        summary: "Stage0 library no longer matches the in-tree library, which means stage1 compiler now uses the beta library.",
+    },
 ];
