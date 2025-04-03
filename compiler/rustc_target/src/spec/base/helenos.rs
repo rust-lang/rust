c8a564cb8857f8688ad19f342ac76dccb22d9844
Matěj Volf <git@mvolfik.com>
2025-04-03 16:20:57 +0200

    remove incorrect comment about PIE support

--- compiler/rustc_target/src/spec/base/helenos.rs
+++ compiler/rustc_target/src/spec/base/helenos.rs
@@ -5,10 +5,7 @@ pub(crate) fn opts() -> TargetOptions {
         os: "helenos".into(),
 
         dynamic_linking: true,
-        // FIXME: this actually is supported by HelenOS, but then we run into issues
-        // with linking libstartfiles.a (parts of which obviously can't be at randomized
-        // positions). The crt_* flags also have some effect on this.
-        // position_independent_executables: true,
+        has_rpath: true,
         relro_level: RelroLevel::Full,
         panic_strategy: PanicStrategy::Abort,
 
