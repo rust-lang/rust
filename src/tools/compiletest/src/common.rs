ecf9e204c942b8e9bd4f04c9e2873ba04036e7e6
Zalathar <Zalathar@users.noreply.github.com>
2025-04-03 20:58:34 +1100

    compiletest: Encapsulate all of the code that touches libtest

--- src/tools/compiletest/src/common.rs
+++ src/tools/compiletest/src/common.rs
@@ -9,9 +9,9 @@ use std::{fmt, iter};
 use build_helper::git::GitConfig;
 use semver::Version;
 use serde::de::{Deserialize, Deserializer, Error as _};
-use test::{ColorConfig, OutputFormat};
 
 pub use self::Mode::*;
+use crate::executor::{ColorConfig, OutputFormat};
 use crate::util::{PathBufExt, add_dylib_path};
 
 macro_rules! string_enum {
