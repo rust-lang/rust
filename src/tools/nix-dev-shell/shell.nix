ba1d9a94c21067046758428e524afff67a5b5c3b
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-04-02 20:33:37 -0400

    Deduplicate nix code And clean it up a little.

--- src/tools/nix-dev-shell/shell.nix
+++ src/tools/nix-dev-shell/shell.nix
@@ -1,18 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-let 
-  x = import ./x { inherit pkgs; };
+{pkgs ? import <nixpkgs> {}}: let
+  x = pkgs.callPackage ./x {};
 in
-pkgs.mkShell {
-  name = "rustc";
-  nativeBuildInputs = with pkgs; [
-    binutils cmake ninja pkg-config python3 git curl cacert patchelf nix
-  ];
-  buildInputs = with pkgs; [
-    openssl glibc.out glibc.static x
-  ];
-  # Avoid creating text files for ICEs.
-  RUSTC_ICE = "0";
-  # Provide `libstdc++.so.6` for the self-contained lld.
-  # Provide `libz.so.1`
-  LD_LIBRARY_PATH = "${with pkgs; lib.makeLibraryPath [stdenv.cc.cc.lib zlib]}";
-}
+  pkgs.mkShell {
+    name = "rustc-shell";
+
+    packages = [
+      pkgs.git
+      pkgs.nix
+      pkgs.glibc
+      x
+    ];
+
+    env = {
+      # Avoid creating text files for ICEs.
+      RUSTC_ICE = 0;
+    };
+  }
