781b92c3f1bbf88252c6de7fbddc386c0c0c26b4
Thalia Archibald <thalia@archibald.dev>
2025-04-05 18:18:31 -0700

    Implement AsRef<OsStr> for Cow<'_, Path>
    In #31751, `impl AsRef<Path> for Cow<'_, OsStr>` was added, but the
    converse was omitted and it wasn't mentioned in discussion. This seems
    to have been a simple oversight, as it otherwise implemented traits
    mutually between `OsStr` and `Path`.

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -941,7 +941,7 @@ impl<'test> TestCx<'test> {
             .arg("-L")
             .arg(aux_dir)
             .arg("-o")
-            .arg(out_dir.as_ref())
+            .arg(out_dir)
             .arg("--deny")
             .arg("warnings")
             .arg(&self.testpaths.file)
