2d5be243b8e049c24a583c8d7576abb116b1d1cf
David Wood <david.wood2@arm.com>
2025-04-09 16:49:28 +0000

    lint: don't consider sizedness in upcastable lint
    Adding a sizedness supertrait shouldn't require multiple vtables so
    shouldn't be linted against.

--- compiler/rustc_lint/src/multiple_supertrait_upcastable.rs
+++ compiler/rustc_lint/src/multiple_supertrait_upcastable.rs
@@ -46,7 +46,8 @@ impl<'tcx> LateLintPass<'tcx> for MultipleSupertraitUpcastable {
                 .tcx
                 .explicit_super_predicates_of(def_id)
                 .iter_identity_copied()
-                .filter_map(|(pred, _)| pred.as_trait_clause());
+                .filter_map(|(pred, _)| pred.as_trait_clause())
+                .filter(|pred| !cx.tcx.is_lang_item(pred.def_id(), hir::LangItem::MetaSized));
             if direct_super_traits_iter.count() > 1 {
                 cx.emit_span_lint(
                     MULTIPLE_SUPERTRAIT_UPCASTABLE,
