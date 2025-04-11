c191c55a9cd4c15a0230c6f91736c1502c09a62c
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-04-11 14:32:31 +0200

    Put back the `doc_cfg` code behind a nightly feature

--- compiler/rustc_ast_passes/src/feature_gate.rs
+++ compiler/rustc_ast_passes/src/feature_gate.rs
@@ -182,6 +182,7 @@ impl<'a> Visitor<'a> for PostExpansionVisitor<'a> {
 
                 gate_doc!(
                     "experimental" {
+                        cfg => doc_cfg
                         masked => doc_masked
                         notable_trait => doc_notable_trait
                     }
