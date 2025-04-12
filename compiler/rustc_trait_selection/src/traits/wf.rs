06242e0ddd454c05d1bca3f7f06378fb8fed9420
David Wood <david.wood2@arm.com>
2025-04-12 21:09:38 +0000

    trait_sel: skip `nominal_obligations` for `Sized`
    `nominal_obligations` calls `predicates_of` on a `Sized` obligation,
    effectively elaborating the trait and making the well-formedness checking
    machinery do a bunch of extra work checking a `MetaSized` obligation is
    well-formed, but given that both `Sized` and `MetaSized` are built-ins,
    if `Sized` is otherwise well-formed, so `MetaSized` will be.

--- compiler/rustc_trait_selection/src/traits/wf.rs
+++ compiler/rustc_trait_selection/src/traits/wf.rs
@@ -567,6 +567,14 @@ impl<'a, 'tcx> WfPredicates<'a, 'tcx> {
         def_id: DefId,
         args: GenericArgsRef<'tcx>,
     ) -> PredicateObligations<'tcx> {
+        // PERF: `Sized`'s predicates include `MetaSized`, but both are compiler implemented marker
+        // traits, so `MetaSized` will always be WF if `Sized` is WF and vice-versa. Determining
+        // the nominal obligations of `Sized` would in-effect just elaborate `MetaSized` and make
+        // the compiler do a bunch of work needlessly.
+        if self.tcx().is_lang_item(def_id, LangItem::Sized) {
+            return Default::default();
+        }
+
         let predicates = self.tcx().predicates_of(def_id);
         let mut origins = vec![def_id; predicates.predicates.len()];
         let mut head = predicates;
