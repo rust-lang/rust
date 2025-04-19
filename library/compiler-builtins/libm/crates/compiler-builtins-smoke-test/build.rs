806bb4fa6e35f65e63e1b96953fab68bfe5a67b4
Trevor Gross <tmgross@umich.edu>
2025-04-19 20:44:21 +0000

    libm: Remove compiler-builtins-smoke-test
    Since `libm` is now part of the `compiler-builtins` repo, the crate to
    test that they work together is no longer needed.

--- library/compiler-builtins/libm/crates/compiler-builtins-smoke-test/build.rs
+++
@@ -1,8 +0,0 @@
-#[path = "../../libm/configure.rs"]
-mod configure;
-
-fn main() {
-    println!("cargo:rerun-if-changed=../../libm/configure.rs");
-    let cfg = configure::Config::from_env();
-    configure::emit_libm_config(&cfg);
-}
