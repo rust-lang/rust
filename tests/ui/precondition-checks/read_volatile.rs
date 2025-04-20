656d4e5bfb17178e6beb7136fbe601535b57ea2e
Luigi Sartor Piucco <luigipiucco@gmail.com>
2025-04-20 18:43:54 -0300

    fix: don't panic on volatile access to null
    According to
    https://discourse.llvm.org/t/rfc-volatile-access-to-non-dereferenceable-memory-may-be-well-defined/86303/4,
    LLVM allows volatile operations on null and handles it correctly. This
    should be allowed in Rust as well, because I/O memory may be hard-coded
    to address 0 in some cases, like the AVR chip ATtiny1626.
    
    A test case that ensured a failure when passing null to volatile was
    removed, since it's now valid.
    
    Due to the addition of `maybe_is_aligned` to `ub_checks`,
    `maybe_is_aligned_and_not_null` was refactored to use it.

--- tests/ui/precondition-checks/read_volatile.rs
+++ tests/ui/precondition-checks/read_volatile.rs
@@ -1,7 +1,7 @@
 //@ run-fail
 //@ compile-flags: -Copt-level=3 -Cdebug-assertions=no -Zub-checks=yes
 //@ error-pattern: unsafe precondition(s) violated: ptr::read_volatile requires
-//@ revisions: null misaligned
+//@ revisions: misaligned
 
 #![allow(invalid_null_arguments)]
 
@@ -11,8 +11,6 @@ fn main() {
     let src = [0u16; 2];
     let src = src.as_ptr();
     unsafe {
-        #[cfg(null)]
-        ptr::read_volatile(ptr::null::<u8>());
         #[cfg(misaligned)]
         ptr::read_volatile(src.byte_add(1));
     }
