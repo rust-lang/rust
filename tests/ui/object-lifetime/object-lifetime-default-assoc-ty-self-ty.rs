0f8c502c4e64a347c3521ff4c427170f3bfe9387
León Orell Valerian Liehr <me@fmease.dev>
2025-04-20 22:21:01 +0200

    Propagate the object lifetime default to the self ty of resolved projections

--- tests/ui/object-lifetime/object-lifetime-default-assoc-ty-self-ty.rs
+++ tests/ui/object-lifetime/object-lifetime-default-assoc-ty-self-ty.rs
@@ -1,13 +1,15 @@
-// FIXME: Explainer.
-//@ known-bug: unknown
+// Check that we correctly deduce object lifetime defaults inside self types of qualified paths.
+//@ check-pass
+//@ revisions: bound clause
 
-trait Outer<'a>: 'a { type Ty; }
+#[cfg(bound)] trait Outer<'a>: 'a { type Ty; }
+#[cfg(clause)] trait Outer<'a> where Self: 'a { type Ty; }
 trait Inner {}
 
 impl<'a> Outer<'a> for dyn Inner + 'a { type Ty = &'a (); }
 
 fn f<'r>(x: <dyn Inner + 'r as Outer<'r>>::Ty) { g(x) }
-// FIXME: Deduce `dyn Inner + 'r` from bound `'a` on self ty param of trait `Outer`.
+// We deduce `dyn Inner + 'r` from bound `'a` on self ty param of trait `Outer`.
 fn g<'r>(x: <dyn Inner as Outer<'r>>::Ty) {}
 
 fn main() {}
