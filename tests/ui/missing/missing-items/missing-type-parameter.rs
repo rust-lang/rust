e94d1923ea6dfecdeb0daa48d32a115c502d9ea6
Kivooeo <Kivooeo123@gmail.com>
2025-04-20 15:30:27 +0500

    fix for #139999

--- tests/ui/missing/missing-items/missing-type-parameter.rs
+++ tests/ui/missing/missing-items/missing-type-parameter.rs
@@ -1,4 +1,19 @@
-fn foo<X>() { }
+fn foo<X>() {}
+
+fn do_something() -> Option<ForgotToImport> {
+    //~^ cannot find type `ForgotToImport` in this scope [E0412]
+    None
+}
+
+fn do_something_T() -> Option<T> {
+    //~^ cannot find type `T` in this scope [E0412]
+    None
+}
+
+fn do_something_Type() -> Option<Type> {
+    //~^ cannot find type `Type` in this scope [E0412]
+    None
+}
 
 fn main() {
     foo(); //~ ERROR type annotations needed
