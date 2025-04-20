13bf5f5bb4115cebe30c8b18935ad017c7fa3de1
Trevor Gross <tmgross@umich.edu>
2025-04-20 08:34:47 +0000

    ci: Use lowercase for bash locals, fix shellcheck

--- library/compiler-builtins/ci/miri.sh
+++ library/compiler-builtins/ci/miri.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -eux
 
 # We need Tree Borrows as some of our raw pointer patterns are not
 # compatible with Stacked Borrows.
@@ -7,10 +7,12 @@ export MIRIFLAGS="-Zmiri-tree-borrows"
 
 # One target that sets `mem-unaligned` and one that does not,
 # and a big-endian target.
-TARGETS=(x86_64-unknown-linux-gnu
+targets=(
+    x86_64-unknown-linux-gnu
     armv7-unknown-linux-gnueabihf
-    s390x-unknown-linux-gnu)
-for TARGET in "${TARGETS[@]}"; do
+    s390x-unknown-linux-gnu
+)
+for target in "${targets[@]}"; do
     # Only run the `mem` tests to avoid this taking too long.
-    cargo miri test --manifest-path builtins-test/Cargo.toml --features no-asm --target $TARGET -- mem
+    cargo miri test --manifest-path builtins-test/Cargo.toml --features no-asm --target "$target" -- mem
 done
