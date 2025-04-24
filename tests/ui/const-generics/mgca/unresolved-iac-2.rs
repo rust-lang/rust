34ce403addf99bd4959b83f3546992b263ff03d8
Boxy <rust@boxyuwu.dev>
2025-04-24 10:49:06 +0100

    Don't build `ParamEnv` and do trait solving in `ItemCtxt`s

--- tests/ui/const-generics/mgca/unresolved-iac-2.rs
+++ tests/ui/const-generics/mgca/unresolved-iac-2.rs
@@ -1,12 +1,14 @@
-//@ known-bug: #138131
 #![feature(min_generic_const_args)]
 #![feature(inherent_associated_types)]
+#![expect(incomplete_features)]
+
 struct Foo<'a> {
     x: &'a (),
 }
 
 impl<'a> Foo<'a> {
     fn foo(_: [u8; Foo::X]) {}
+    //~^ ERROR: associated constant `X` not found for `Foo<'_>` in the current scope
 }
 
 fn main() {}
