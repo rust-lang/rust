d166b303863369e12bd177476bc1c6232ddc21ff
tiif <pekyuan@gmail.com>
2025-04-28 14:48:46 +0000

    Setup unstable feature attr parser

--- compiler/rustc_attr_parsing/src/context.rs
+++ compiler/rustc_attr_parsing/src/context.rs
@@ -11,7 +11,7 @@ use rustc_hir::{AttrArgs, AttrItem, AttrPath, Attribute, HashIgnoredAttrId};
 use rustc_session::Session;
 use rustc_span::{DUMMY_SP, ErrorGuaranteed, Span, Symbol, sym};
 
-use crate::attributes::allow_unstable::{AllowConstFnUnstableParser, AllowInternalUnstableParser};
+use crate::attributes::allow_unstable::{AllowConstFnUnstableParser, AllowInternalUnstableParser, AllowUnstableFeatureParser};
 use crate::attributes::confusables::ConfusablesParser;
 use crate::attributes::deprecation::DeprecationParser;
 use crate::attributes::repr::ReprParser;
@@ -70,6 +70,7 @@ attribute_groups!(
         // tidy-alphabetical-start
         Combine<AllowConstFnUnstableParser>,
         Combine<AllowInternalUnstableParser>,
+        Combine<AllowUnstableFeatureParser>,
         Combine<ReprParser>,
         // tidy-alphabetical-end
 
--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -448,6 +448,7 @@ symbols! {
         allow_fail,
         allow_internal_unsafe,
         allow_internal_unstable,
+        allow_unstable_feature,
         altivec,
         alu32,
         always,
