21b2dc137854aedc403ca3252abdf47968c9116c
Ralf Jung <post@ralfj.de>
2025-04-28 15:17:33 +0200

    forbidden target feature tests: consolidate multiple tests in a single one with revisions

--- tests/ui/target-feature/forbidden-hardfloat-target-feature-flag-disable.rs
+++ tests/ui/target-feature/forbidden-hardfloat-target-feature-flag-disable.rs
@@ -1,7 +1,13 @@
 //! Ensure ABI-required features cannot be disabled via `-Ctarget-feature`.
-//@ compile-flags: --target=x86_64-unknown-linux-gnu --crate-type=lib
-//@ needs-llvm-components: x86
-//@ compile-flags: -Ctarget-feature=-x87
+//! Also covers the case of a feature indirectly disabling another via feature implications.
+//@ compile-flags: --crate-type=lib
+//@ revisions: x86 x86-implied aarch64
+//@[x86] compile-flags: --target=x86_64-unknown-linux-gnu -Ctarget-feature=-x87
+//@[x86] needs-llvm-components: x86
+//@[x86-implied] compile-flags: --target=x86_64-unknown-linux-gnu -Ctarget-feature=-sse
+//@[x86-implied] needs-llvm-components: x86
+//@[aarch64] compile-flags: --target=aarch64-unknown-linux-gnu -Ctarget-feature=-neon
+//@[aarch64] needs-llvm-components: aarch64
 // For now this is just a warning.
 //@ build-pass
 
@@ -11,5 +17,5 @@
 #[lang = "sized"]
 pub trait Sized {}
 
-//~? WARN target feature `x87` must be enabled to ensure that the ABI of the current target can be implemented correctly
-//~? WARN unstable feature specified for `-Ctarget-feature`: `x87`
+//~? WARN must be enabled to ensure that the ABI of the current target can be implemented correctly
+//[x86]~? WARN unstable feature specified for `-Ctarget-feature`
