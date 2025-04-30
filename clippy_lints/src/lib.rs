d49b8aa7d2290241c2ea3ee246f3ffad8486dd35
Alex Macleod <alex@macleod.io>
2025-04-30 13:12:29 +0000

    Remove `ClippyCtfe` pass

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -67,7 +67,6 @@ extern crate clippy_utils;
 
 mod utils;
 
-pub mod ctfe; // Very important lint, do not remove (rust#125116)
 pub mod declared_lints;
 pub mod deprecated_lints;
 
@@ -582,8 +581,6 @@ pub fn register_lints(store: &mut rustc_lint::LintStore, conf: &'static Conf) {
     let attr_storage = AttrStorage::default();
     let attrs = attr_storage.clone();
     store.register_early_pass(move || Box::new(AttrCollector::new(attrs.clone())));
-
-    store.register_late_pass(|_| Box::new(ctfe::ClippyCtfe));
 
     store.register_late_pass(move |_| Box::new(operators::arithmetic_side_effects::ArithmeticSideEffects::new(conf)));
     store.register_late_pass(|_| Box::new(utils::dump_hir::DumpHir));
