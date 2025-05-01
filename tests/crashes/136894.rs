a76a7e98a985c8a6c335f6d0388c2382ddb5723d
Boxy <rust@boxyuwu.dev>
2025-05-01 15:46:33 +0100

    Fix tests

--- tests/crashes/136894.rs
+++
@@ -1,8 +0,0 @@
-//@ known-bug: #136894
-#![feature(generic_const_exprs)]
-#![crate_type = "lib"]
-#![allow(incomplete_features, dead_code)]
-
-struct X<T>([(); f::<T>()]) where [(); f::<T>()]:;
-
-const fn f<T>() -> usize { panic!() }
