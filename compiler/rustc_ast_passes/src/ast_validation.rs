74a17fd049885f8b2c9d1e570a3afea364415d84
Urgau <urgau@numericable.fr>
2025-05-03 15:18:30 +0200

    Have `AstValidation` track a linting node id

--- compiler/rustc_ast_passes/src/ast_validation.rs
+++ compiler/rustc_ast_passes/src/ast_validation.rs
@@ -82,6 +82,8 @@ struct AstValidator<'a> {
     /// Used to ban explicit safety on foreign items when the extern block is not marked as unsafe.
     extern_mod_safety: Option<Safety>,
 
+    lint_node_id: NodeId,
+
     lint_buffer: &'a mut LintBuffer,
 }
 
@@ -839,6 +841,8 @@ impl<'a> Visitor<'a> for AstValidator<'a> {
             self.has_proc_macro_decls = true;
         }
 
+        let previous_lint_node_id = mem::replace(&mut self.lint_node_id, item.id);
+
         if let Some(ident) = item.kind.ident()
             && attr::contains_name(&item.attrs, sym::no_mangle)
         {
@@ -1128,6 +1132,8 @@ impl<'a> Visitor<'a> for AstValidator<'a> {
             }
             _ => visit::walk_item(self, item),
         }
+
+        self.lint_node_id = previous_lint_node_id;
     }
 
     fn visit_foreign_item(&mut self, fi: &'a ForeignItem) {
@@ -1694,6 +1700,7 @@ pub fn check_crate(
         outer_impl_trait_span: None,
         disallow_tilde_const: Some(TildeConstReason::Item),
         extern_mod_safety: None,
+        lint_node_id: CRATE_NODE_ID,
         lint_buffer: lints,
     };
     visit::walk_crate(&mut validator, krate);
