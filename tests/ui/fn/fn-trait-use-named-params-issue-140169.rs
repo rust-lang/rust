b922da3586ce01a4bf174db09624a1d0a424d5d9
xizheyin <xizheyin@smail.nju.edu.cn>
2025-05-06 15:45:04 +0800

    Use `parse_param_general` when parsing `(T, U)->R` in `parse_path_segment`
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>
    
    Co-authored-by: Vadim Petrochenkov <vadim.petrochenkov@gmail.com>

--- tests/ui/fn/fn-trait-use-named-params-issue-140169.rs
+++ tests/ui/fn/fn-trait-use-named-params-issue-140169.rs
@@ -1,6 +1,12 @@
-fn g(_: fn(a: u8)) {}
-fn x(_: impl Fn(u8, vvvv: u8)) {} //~ ERROR expected one of `!`, `(`, `)`, `+`, `,`, `::`, or `<`, found `:`
-fn y(_: impl Fn(aaaa: u8, u8)) {} //~ ERROR expected one of `!`, `(`, `)`, `+`, `,`, `::`, or `<`, found `:`
-fn z(_: impl Fn(aaaa: u8, vvvv: u8)) {} //~ ERROR expected one of `!`, `(`, `)`, `+`, `,`, `::`, or `<`, found `:`
+fn f1(_: fn(a: u8)) {}
+fn f2(_: impl Fn(u8, vvvv: u8)) {} //~ ERROR `Trait(...)` syntax does not support named parameters
+fn f3(_: impl Fn(aaaa: u8, u8)) {} //~ ERROR `Trait(...)` syntax does not support named parameters
+fn f4(_: impl Fn(aaaa: u8, vvvv: u8)) {}
+//~^ ERROR `Trait(...)` syntax does not support named parameters
+//~| ERROR `Trait(...)` syntax does not support named parameters
+fn f5(_: impl Fn(u8, ...)) {}
+//~^ ERROR `Trait(...)` syntax does not support c_variadic parameters
+fn f6(_: impl Fn(u8, #[allow(unused_attributes)] u8)) {}
+//~^ ERROR `Trait(...)` syntax does not support attributes in parameters
 
 fn main(){}
