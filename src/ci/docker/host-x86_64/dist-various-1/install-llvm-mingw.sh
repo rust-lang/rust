a7e39b28212cd435d6378320c84ececc4054ae8b
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-05-06 18:42:53 +0200

    Move *-windows-gnullvm targets to it's own Dockerfile and enable host tools

--- src/ci/docker/host-x86_64/dist-various-1/install-llvm-mingw.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-release_date=20240404
-archive=llvm-mingw-${release_date}-ucrt-ubuntu-20.04-x86_64.tar.xz
-curl -L https://github.com/mstorsjo/llvm-mingw/releases/download/${release_date}/${archive} | \
-tar --extract --lzma --strip 1 --directory /usr/local
