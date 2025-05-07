9196eb3dd1506048dcbbfd8504e26ac2b130940b
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-05-07 13:05:47 +0000

    Fix mini_core for panic=unwind

--- example/mini_core.rs
+++ example/mini_core.rs
@@ -521,8 +521,25 @@ fn panic_cannot_unwind() -> ! {
 }
 
 #[lang = "eh_personality"]
-fn eh_personality() -> ! {
+// FIXME personality signature depends on target
+fn eh_personality(
+    _version: i32,
+    _actions: i32,
+    _exception_class: u64,
+    _exception_object: *mut (),
+    _context: *mut (),
+) -> i32 {
     loop {}
+}
+
+#[lang = "panic_in_cleanup"]
+fn panic_in_cleanup() -> ! {
+    loop {}
+}
+
+#[link(name = "gcc_s")]
+extern "C" {
+    fn _Unwind_Resume(exc: *mut ()) -> !;
 }
 
 #[lang = "drop_in_place"]
