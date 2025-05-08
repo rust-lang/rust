88c1796384cca81ca80e728d45f6673f601493ff
xizheyin <xizheyin@smail.nju.edu.cn>
2025-05-08 21:12:02 +0800

    Use span before macro expansion in lint for-loops-over-falibles
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- tests/ui/lint/for-loops-over-falibles/macro-issue-140747.rs
+++ tests/ui/lint/for-loops-over-falibles/macro-issue-140747.rs
@@ -3,8 +3,8 @@
 fn main() {
     macro_rules! x {
         () => {
-            None::<i32> //~ ERROR for loop over an `Option`. This is more readably written as an `if let` statement [for_loops_over_fallibles]
+            None::<i32>
         };
     }
-    for _ in x! {} {}
+    for _ in x! {} {} //~ ERROR for loop over an `Option`. This is more readably written as an `if let` statement [for_loops_over_fallibles]
 }
