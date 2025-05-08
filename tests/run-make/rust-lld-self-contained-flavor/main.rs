d74189c1abc6f4732fc6d31bcbd908686a46592d
Rémy Rakic <remy.rakic+github@gmail.com>
2025-05-08 15:57:48 +0000

    add rmake test for `-Clink-self-contained=+linker -Clinker-flavor=ld.lld`

---
+++ tests/run-make/rust-lld-self-contained-flavor/main.rs
@@ -0,0 +1,6 @@
+// We're just trying to launch the linker to see which one rustc picks between system lld and
+// rust-lld. We don't need to link anything successfully though, so this is just a stub.
+
+#![feature(no_core)]
+#![no_core]
+#![no_main]
