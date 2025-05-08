b6c2a429efd85cbfa5f592c66faac6955729c6ea
xizheyin <xizheyin@smail.nju.edu.cn>
2025-05-08 18:09:29 +0800

    std: Make consistence between `From` and `Into`
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- library/core/src/convert/mod.rs
+++ library/core/src/convert/mod.rs
@@ -464,8 +464,8 @@ pub trait Into<T>: Sized {
 /// orphaning rules.
 /// See [`Into`] for more details.
 ///
-/// Prefer using [`Into`] over using `From` when specifying trait bounds on a generic function.
-/// This way, types that directly implement [`Into`] can be used as arguments as well.
+/// Prefer using [`Into`] over [`From`] when specifying trait bounds on a generic function
+/// to ensure that types that only implement [`Into`] can be used as well.
 ///
 /// The `From` trait is also very useful when performing error handling. When constructing a function
 /// that is capable of failing, the return type will generally be of the form `Result<T, E>`.
