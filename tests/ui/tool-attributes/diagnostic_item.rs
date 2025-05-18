eedbf8b72decc71c8b53447d5c6b0c64aad2363c
mejrs <59372212+mejrs@users.noreply.github.com>
2025-05-18 18:35:13 +0200

    refactor  `AttributeGate` and `rustc_attr!` to emit notes during feature checking

--- tests/ui/tool-attributes/diagnostic_item.rs
+++ tests/ui/tool-attributes/diagnostic_item.rs
@@ -1,3 +1,5 @@
-#[rustc_diagnostic_item = "foomp"] //~ ERROR compiler internal support for linting
+#[rustc_diagnostic_item = "foomp"]
+//~^ ERROR use of an internal attribute [E0658]
+//~| NOTE the `#[rustc_diagnostic_item]` attribute allows the compiler to reference types from the standard library for diagnostic purposes
 struct Foomp;
 fn main() {}
