a1d9cc4f0615efe47ed021d99c244becf4848c9b
Ruan Comelli <ruancomelli@gmail.com>
2025-05-19 18:34:32 -0300

    Add const support for float rounding methods
    Add const support for the float rounding methods floor, ceil, trunc,
    fract, round and round_ties_even.
    This works by moving the calculation logic from
    
         src/tools/miri/src/intrinsics/mod.rs
    
    into
    
         compiler/rustc_const_eval/src/interpret/intrinsics.rs.
    
    All relevant method definitions were adjusted to include the `const`
    keyword for all supported float types: f16, f32, f64 and f128.
    
    The constness is hidden behind the feature gate
    
         feature(const_float_round_methods)
    
    which is tracked in
    
         https://github.com/rust-lang/rust/issues/141555
    
    This commit is a squash of the following commits:
    - test: add tests that we expect to pass when float rounding becomes const
    - feat: make float rounding methods `const`
    - fix: replace `rustc_allow_const_fn_unstable(core_intrinsics)` attribute with `#[rustc_const_unstable(feature = "f128", issue = "116909")]` in `library/core/src/num/f128.rs`
    - revert: undo update to `library/stdarch`
    - refactor: replace multiple `float_<mode>_intrinsic` rounding methods with a single, parametrized one
    - fix: add `#[cfg(not(bootstrap))]` to new const method tests
    - test: add extra sign tests to check `+0.0` and `-0.0`
    - revert: undo accidental changes to `round` docs
    - fix: gate `const` float round method behind `const_float_round_methods`
    - fix: remove unnecessary `#![feature(const_float_methods)]`
    - fix: remove unnecessary `#![feature(const_float_methods)]` [2]
    - revert: undo changes to `tests/ui/consts/const-eval/float_methods.rs`
    - fix: adjust after rebase
    - test: fix float tests
    - test: add tests for `fract`
    - chore: add commented-out `const_float_round_methods` feature gates to `f16` and `f128`
    - fix: adjust NaN when rounding floats
    - chore: add FIXME comment for de-duplicating float tests
    - test: remove unnecessary test file `tests/ui/consts/const-eval/float_methods.rs`
    - test: fix tests after upstream simplification of how float tests are run

--- library/core/src/num/f32.rs
+++ library/core/src/num/f32.rs
@@ -1590,8 +1590,9 @@ pub mod math {
     /// [`f32::floor`]: ../../../std/primitive.f32.html#method.floor
     #[inline]
     #[unstable(feature = "core_float_math", issue = "137578")]
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
     #[must_use = "method returns a new number and does not mutate the original value"]
-    pub fn floor(x: f32) -> f32 {
+    pub const fn floor(x: f32) -> f32 {
         // SAFETY: intrinsic with no preconditions
         unsafe { intrinsics::floorf32(x) }
     }
@@ -1620,7 +1621,8 @@ pub mod math {
     #[doc(alias = "ceiling")]
     #[must_use = "method returns a new number and does not mutate the original value"]
     #[unstable(feature = "core_float_math", issue = "137578")]
-    pub fn ceil(x: f32) -> f32 {
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
+    pub const fn ceil(x: f32) -> f32 {
         // SAFETY: intrinsic with no preconditions
         unsafe { intrinsics::ceilf32(x) }
     }
@@ -1654,7 +1656,8 @@ pub mod math {
     #[inline]
     #[unstable(feature = "core_float_math", issue = "137578")]
     #[must_use = "method returns a new number and does not mutate the original value"]
-    pub fn round(x: f32) -> f32 {
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
+    pub const fn round(x: f32) -> f32 {
         // SAFETY: intrinsic with no preconditions
         unsafe { intrinsics::roundf32(x) }
     }
@@ -1687,7 +1690,8 @@ pub mod math {
     #[inline]
     #[unstable(feature = "core_float_math", issue = "137578")]
     #[must_use = "method returns a new number and does not mutate the original value"]
-    pub fn round_ties_even(x: f32) -> f32 {
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
+    pub const fn round_ties_even(x: f32) -> f32 {
         intrinsics::round_ties_even_f32(x)
     }
 
@@ -1717,7 +1721,8 @@ pub mod math {
     #[doc(alias = "truncate")]
     #[must_use = "method returns a new number and does not mutate the original value"]
     #[unstable(feature = "core_float_math", issue = "137578")]
-    pub fn trunc(x: f32) -> f32 {
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
+    pub const fn trunc(x: f32) -> f32 {
         // SAFETY: intrinsic with no preconditions
         unsafe { intrinsics::truncf32(x) }
     }
@@ -1746,8 +1751,9 @@ pub mod math {
     /// [`f32::fract`]: ../../../std/primitive.f32.html#method.fract
     #[inline]
     #[unstable(feature = "core_float_math", issue = "137578")]
+    #[rustc_const_unstable(feature = "const_float_round_methods", issue = "141555")]
     #[must_use = "method returns a new number and does not mutate the original value"]
-    pub fn fract(x: f32) -> f32 {
+    pub const fn fract(x: f32) -> f32 {
         x - trunc(x)
     }
 
