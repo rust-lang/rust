dc794f18a425a20563943ea4201b34ebea2aa017
Andrew Zhogin <andrew.zhogin@gmail.com>
2025-05-21 15:21:01 +0700

    When AsyncDrop impl is empty, sync drop generated in elaborator (Fixes #140974)

--- tests/ui/async-await/async-drop/elaborate-index-out-of-bounds.rs
+++ tests/ui/async-await/async-drop/elaborate-index-out-of-bounds.rs
@@ -1,5 +1,7 @@
-//@ known-bug: #140974
-//@edition:2021
+//@ edition: 2024
+// Ex-ICE: #140974
+#![crate_type = "lib"]
+#![allow(incomplete_features)]
 #![feature(async_drop)]
 use core::future::AsyncDrop;
 
@@ -10,5 +12,6 @@ impl Drop for HasIncompleteAsyncDrop {
     fn drop(&mut self) {}
 }
 impl AsyncDrop for HasIncompleteAsyncDrop {
+    //~^ ERROR: not all trait items implemented, missing: `drop` [E0046]
     // not implemented yet..
 }
