caf665e692d74085e28a62ce4497eb7f02c5f37f
Kyle Huey <khuey@kylehuey.com>
2025-05-21 16:21:00 -0700

    Use the fn_span when emitting function calls for better debug info.
    This especially improves the developer experience for long chains
    of function calls that span multiple lines, which is common with
    builder patterns, chains of iterator/future combinators, etc.

---
+++ tests/ui/panics/location-detail-unwrap-multiline.rs
@@ -0,0 +1,11 @@
+//@ run-fail
+//@ compile-flags: -Cstrip=none -Cdebuginfo=line-tables-only -Copt-level=0
+//@ exec-env:RUST_BACKTRACE=1
+//@ regex-error-pattern: location-detail-unwrap-multiline\.rs:10(:10)?\n
+//@ needs-unwind
+
+fn main() {
+    let opt: Option<u32> = None;
+    opt
+        .unwrap();
+}
