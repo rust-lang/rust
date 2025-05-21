ad212114443dceae557763fdf89dc9040065d330
bit-aloo <sshourya17@gmail.com>
2025-05-21 22:49:03 +0530

    The config file declare the central Config struct and house its top-level methods that interact across different config sections

--- src/bootstrap/src/core/config/mod.rs
+++ src/bootstrap/src/core/config/mod.rs
@@ -1,7 +1,12 @@
 #[expect(clippy::module_inception)]
 mod config;
 pub mod flags;
+pub mod parsing;
 #[cfg(test)]
 mod tests;
+pub mod toml;
+pub mod types;
 
 pub use config::*;
+pub use toml::ChangeId;
+pub use types::*;
