efdd43d4b4cd74f46c24786aa59e61f176994907
Max Siling <root@goldstein.lol>
2025-05-22 22:39:12 +0300

    Make into_parts methods on Vec associated functions
    This is more consistent with `Box::into_raw()` and clears out potential
    confusion about whether the method acts on a vector or on a slice.

--- library/alloc/src/vec/mod.rs
+++ library/alloc/src/vec/mod.rs
@@ -1,4 +1,4 @@
-//! A contiguous growable array type with heap-allocated contents, written
+//! A contiguous growable array type with heap-allocated contents, writtenalloc/vec/
 //! `Vec<T>`.
 //!
 //! Vectors have *O*(1) indexing, amortized *O*(1) push (to the end) and
@@ -1092,7 +1092,7 @@ impl<T, A: Allocator> Vec<T, A> {
     /// #![feature(vec_into_raw_parts)]
     /// let v: Vec<i32> = vec![-1, 0, 1];
     ///
-    /// let (ptr, len, cap) = v.into_raw_parts();
+    /// let (ptr, len, cap) = Vec::into_raw_parts(v);
     ///
     /// let rebuilt = unsafe {
     ///     // We can now make changes to the components, such as
@@ -1105,8 +1105,8 @@ impl<T, A: Allocator> Vec<T, A> {
     /// ```
     #[must_use = "losing the pointer will leak memory"]
     #[unstable(feature = "vec_into_raw_parts", reason = "new API", issue = "65816")]
-    pub fn into_raw_parts(self) -> (*mut T, usize, usize) {
-        let mut me = ManuallyDrop::new(self);
+    pub fn into_raw_parts(vec: Self) -> (*mut T, usize, usize) {
+        let mut me = ManuallyDrop::new(vec);
         (me.as_mut_ptr(), me.len(), me.capacity())
     }
 
@@ -1133,7 +1133,7 @@ impl<T, A: Allocator> Vec<T, A> {
     ///
     /// let v: Vec<i32> = vec![-1, 0, 1];
     ///
-    /// let (ptr, len, cap) = v.into_parts();
+    /// let (ptr, len, cap) = Vec::into_parts(v);
     ///
     /// let rebuilt = unsafe {
     ///     // We can now make changes to the components, such as
@@ -1147,8 +1147,8 @@ impl<T, A: Allocator> Vec<T, A> {
     #[must_use = "losing the pointer will leak memory"]
     #[unstable(feature = "box_vec_non_null", reason = "new API", issue = "130364")]
     // #[unstable(feature = "vec_into_raw_parts", reason = "new API", issue = "65816")]
-    pub fn into_parts(self) -> (NonNull<T>, usize, usize) {
-        let (ptr, len, capacity) = self.into_raw_parts();
+    pub fn into_parts(vec: Self) -> (NonNull<T>, usize, usize) {
+        let (ptr, len, capacity) = Self::into_raw_parts(vec);
         // SAFETY: A `Vec` always has a non-null pointer.
         (unsafe { NonNull::new_unchecked(ptr) }, len, capacity)
     }
@@ -1179,7 +1179,7 @@ impl<T, A: Allocator> Vec<T, A> {
     /// v.push(0);
     /// v.push(1);
     ///
-    /// let (ptr, len, cap, alloc) = v.into_raw_parts_with_alloc();
+    /// let (ptr, len, cap, alloc) = Vec::into_raw_parts_with_alloc(v);
     ///
     /// let rebuilt = unsafe {
     ///     // We can now make changes to the components, such as
@@ -1193,8 +1193,8 @@ impl<T, A: Allocator> Vec<T, A> {
     #[must_use = "losing the pointer will leak memory"]
     #[unstable(feature = "allocator_api", issue = "32838")]
     // #[unstable(feature = "vec_into_raw_parts", reason = "new API", issue = "65816")]
-    pub fn into_raw_parts_with_alloc(self) -> (*mut T, usize, usize, A) {
-        let mut me = ManuallyDrop::new(self);
+    pub fn into_raw_parts_with_alloc(vec: Self) -> (*mut T, usize, usize, A) {
+        let mut me = ManuallyDrop::new(vec);
         let len = me.len();
         let capacity = me.capacity();
         let ptr = me.as_mut_ptr();
@@ -1229,7 +1229,7 @@ impl<T, A: Allocator> Vec<T, A> {
     /// v.push(0);
     /// v.push(1);
     ///
-    /// let (ptr, len, cap, alloc) = v.into_parts_with_alloc();
+    /// let (ptr, len, cap, alloc) = Vec::into_parts_with_alloc(v);
     ///
     /// let rebuilt = unsafe {
     ///     // We can now make changes to the components, such as
@@ -1244,8 +1244,8 @@ impl<T, A: Allocator> Vec<T, A> {
     #[unstable(feature = "allocator_api", issue = "32838")]
     // #[unstable(feature = "box_vec_non_null", reason = "new API", issue = "130364")]
     // #[unstable(feature = "vec_into_raw_parts", reason = "new API", issue = "65816")]
-    pub fn into_parts_with_alloc(self) -> (NonNull<T>, usize, usize, A) {
-        let (ptr, len, capacity, alloc) = self.into_raw_parts_with_alloc();
+    pub fn into_parts_with_alloc(vec: Self) -> (NonNull<T>, usize, usize, A) {
+        let (ptr, len, capacity, alloc) = Vec::into_raw_parts_with_alloc(vec);
         // SAFETY: A `Vec` always has a non-null pointer.
         (unsafe { NonNull::new_unchecked(ptr) }, len, capacity, alloc)
     }
@@ -3139,7 +3139,7 @@ impl<T, A: Allocator, const N: usize> Vec<[T; N], A> {
     /// ```
     #[stable(feature = "slice_flatten", since = "1.80.0")]
     pub fn into_flattened(self) -> Vec<T, A> {
-        let (ptr, len, cap, alloc) = self.into_raw_parts_with_alloc();
+        let (ptr, len, cap, alloc) = Vec::into_raw_parts_with_alloc(self);
         let (new_len, new_cap) = if T::IS_ZST {
             (len.checked_mul(N).expect("vec len overflow"), usize::MAX)
         } else {
