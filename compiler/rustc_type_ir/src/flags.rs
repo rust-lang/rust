f96e6a60b4a09ac3e0f1110ba07d61c805e704d8
tiif <pekyuan@gmail.com>
2025-05-23 10:39:03 +0000

    fmt

--- compiler/rustc_type_ir/src/flags.rs
+++ compiler/rustc_type_ir/src/flags.rs
@@ -410,7 +410,7 @@ impl<I: Interner> FlagComputation<I> {
                 self.add_term(t1);
                 self.add_term(t2);
             }
-            ty::PredicateKind::Clause(ty::ClauseKind::UnstableFeature(_)) => todo!()
+            ty::PredicateKind::Clause(ty::ClauseKind::UnstableFeature(_)) => todo!(),
         }
     }
 
