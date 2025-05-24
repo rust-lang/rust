9beea6fe35a99dc3e3400733d20c0f18b2e7d012
Tomoaki Kobayashi <tomoaki.kobayashi.t3@alumni.tohoku.ac.jp>
2025-05-24 10:57:02 +0900

    Fix SubtypeFormat sorting  for unformmated code

--- tests/ui/errors/span-format_args-issue-140578.rs
+++ tests/ui/errors/span-format_args-issue-140578.rs
@@ -8,4 +8,25 @@ fn check_format_args_nl() {
   //~^ ERROR type annotations needed
 }
 
+fn check_multi1() {
+  println!("{:?} {:?} {a} {a:?}", [], [], a = 1 + 1);
+  //~^ ERROR type annotations needed
+}
+
+fn check_multi2() {
+  println!("{:?} {:?} {a} {a:?} {b:?}", [], [], a = 1 + 1, b = []);
+  //~^ ERROR type annotations needed
+}
+
+fn check_unformatted() {
+  println!("
+  {:?} {:?}
+{a}
+{a:?}",
+        [],
+ [],
+ //~^ ERROR type annotations needed
+a = 1 + 1);
+}
+
 fn main() {}
