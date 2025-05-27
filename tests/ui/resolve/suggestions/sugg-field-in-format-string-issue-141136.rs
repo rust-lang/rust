58fc81c84bf1a5eaedf59358a9d90a7493bbf03a
xizheyin <xizheyin@smail.nju.edu.cn>
2025-05-27 14:27:59 +0800

    Add testcase for sugg-field-in-format-string-issue-141136
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- tests/ui/resolve/suggestions/sugg-field-in-format-string-issue-141136.rs
+++ tests/ui/resolve/suggestions/sugg-field-in-format-string-issue-141136.rs
@@ -9,6 +9,7 @@ impl Foo {
         let _ = format!("{ x}"); //~ ERROR invalid format string: expected `}`, found `x`
         let _ = format!("{}", x); //~ ERROR cannot find value `x` in this scope [E0425]
         println!("{x}"); //~ ERROR cannot find value `x` in this scope [E0425]
+        let _ = {x}; //~ERROR cannot find value `x` in this scope [E0425]
     }
 }
 
