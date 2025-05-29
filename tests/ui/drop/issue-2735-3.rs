8fc1bed0c8b6aab76d07e7576b0c1c1f29b934bf
Jake Goulding <jake.goulding@gmail.com>
2025-05-29 09:38:44 -0400

    Reduce confusion of some drop order tests
    In addition to adhering to normal Rust casing idioms, I ran `rustfmt`.

--- tests/ui/drop/issue-2735-3.rs
+++ tests/ui/drop/issue-2735-3.rs
@@ -1,23 +1,20 @@
 //@ run-pass
-#![allow(non_camel_case_types)]
 
 use std::cell::Cell;
 
 // This test should behave exactly like issue-2735-2
-struct defer<'a> {
+struct Defer<'a> {
     b: &'a Cell<bool>,
 }
 
-impl<'a> Drop for defer<'a> {
+impl<'a> Drop for Defer<'a> {
     fn drop(&mut self) {
         self.b.set(true);
     }
 }
 
-fn defer(b: &Cell<bool>) -> defer<'_> {
-    defer {
-        b: b
-    }
+fn defer(b: &Cell<bool>) -> Defer<'_> {
+    Defer { b }
 }
 
 pub fn main() {
