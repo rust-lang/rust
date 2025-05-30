67e36af83ad63e935c7d4892a075ef196c98f1d4
Karol Zwolak <karolzwolak7@gmail.com>
2025-05-30 23:17:58 +0200

    add additional example in the error to showcase the behavior

--- tests/ui/lint/default-groups-issue-65464.rs
+++ tests/ui/lint/default-groups-issue-65464.rs
@@ -8,7 +8,15 @@
 // in such tests, and the we're testing a scenario with no modification to the default settings.
 
 fn main() {
+    // additional context is provided only if the level is not explicitly set
     let WrongCase = 1;
     //~^ WARN [non_snake_case]
     //~| NOTE `#[warn(non_snake_case)]` (part of `#[warn(nonstandard_style)]`) on by default
+
+    // unchanged message if the level is explicitly set
+    // even if the level is the same as the default
+    #[warn(nonstandard_style)] //~ NOTE the lint level is defined here
+    let WrongCase = 2;
+    //~^ WARN [non_snake_case]
+    //~| NOTE `#[warn(non_snake_case)]` implied by `#[warn(nonstandard_style)]`
 }
