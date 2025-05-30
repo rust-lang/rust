9b8d4c68c4ba928513b74cca077d3a7d3d68a0f8
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-05-30 14:56:15 +0200

    multiline

--- src/ci/scripts/update-docker.sh
+++ src/ci/scripts/update-docker.sh
@@ -22,7 +22,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "Add the repository to Apt sources"
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+  https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
