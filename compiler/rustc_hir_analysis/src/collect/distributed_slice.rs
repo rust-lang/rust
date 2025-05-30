9f5725c49ae24fe51d7105e8c8af6089d439d8a4
Jana Dönszelmann <jana@donsz.nl>
2025-05-30 10:49:06 +0200

    shuffle based on defpathhash

--- compiler/rustc_hir_analysis/src/collect/distributed_slice.rs
+++ compiler/rustc_hir_analysis/src/collect/distributed_slice.rs
@@ -1,3 +1,5 @@
+use rand::SeedableRng;
+use rand::seq::SliceRandom;
 use rustc_hir::def_id::{DefIdMap, LocalDefId};
 use rustc_hir::{DistributedSlice, ItemKind};
 use rustc_middle::ty::TyCtxt;
@@ -6,15 +8,28 @@ pub(super) fn distributed_slice_elements<'tcx>(
     tcx: TyCtxt<'tcx>,
     _: (),
 ) -> DefIdMap<Vec<LocalDefId>> {
-    let mut res = DefIdMap::<Vec<LocalDefId>>::default();
+    let mut slice_elements = DefIdMap::<Vec<LocalDefId>>::default();
 
     for i in tcx.hir_free_items() {
         let addition_def_id = i.owner_id.def_id;
         if let ItemKind::Const(.., DistributedSlice::Addition(declaration_def_id)) =
             tcx.hir_expect_item(addition_def_id).kind
         {
-            res.entry(declaration_def_id.to_def_id()).or_default().push(addition_def_id);
+            slice_elements.entry(declaration_def_id.to_def_id()).or_default().push(addition_def_id);
         }
+    }
+
+    let mut res = DefIdMap::<Vec<LocalDefId>>::default();
+
+    for (key, mut registered_values) in
+        tcx.with_stable_hashing_context(|hcx| slice_elements.into_sorted(&hcx, true))
+    {
+        // shuffle seeded by the defpathhash of the registry
+        let item_seed = tcx.def_path_hash(key).0.to_smaller_hash();
+        let mut rng = rand_xoshiro::Xoshiro128StarStar::seed_from_u64(item_seed.as_u64());
+        registered_values.as_mut_slice().shuffle(&mut rng);
+
+        res.insert(key, registered_values);
     }
 
     res
