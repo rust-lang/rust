0dd29e1a86683ec2ff439cd41c1a1765f617b520
Jubilee Young <workingjubilee@gmail.com>
2025-06-01 14:19:02 -0700

    tests: Update and bless cmse-nonsecure ABI tests

--- tests/ui/cmse-nonsecure/cmse-nonsecure-entry/gate_test.rs
+++ tests/ui/cmse-nonsecure/cmse-nonsecure-entry/gate_test.rs
@@ -2,8 +2,8 @@
 
 #[no_mangle]
 pub extern "C-cmse-nonsecure-entry" fn entry_function(input: u32) -> u32 {
-    //~^ ERROR [E0570]
-    //~| ERROR [E0658]
+    //~^ ERROR: is not a supported ABI for the current target [E0570]
+    //~| ERROR: ABI is experimental and subject to change [E0658]
     input + 6
 }
 
