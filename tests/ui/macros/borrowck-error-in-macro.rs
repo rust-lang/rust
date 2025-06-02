2e527f03d1c6b428cedbea52c1b644c4d0abe1c4
Jana Dönszelmann <jana@donsz.nl>
2025-06-02 11:15:20 +0200

    fix bug where borrowck tries to describe a name from a macro in another crate

--- tests/ui/macros/borrowck-error-in-macro.rs
+++ tests/ui/macros/borrowck-error-in-macro.rs
@@ -1,5 +1,7 @@
 //@ aux-build: borrowck-error-in-macro.rs
+//@ error-pattern: a call in this macro requires a mutable binding due to mutable borrow of `d`
 
 extern crate borrowck_error_in_macro as a;
 
 a::ice! {}
+//~^ ERROR cannot borrow value as mutable, as it is not declared as mutable
