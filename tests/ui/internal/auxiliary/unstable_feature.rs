3672c136c4bd022abf2cb6851fd98974c62b5f74
tiif <pekyuan@gmail.com>
2025-06-02 17:28:46 +0000

    Improve test

--- tests/ui/internal/auxiliary/unstable_feature.rs
+++ tests/ui/internal/auxiliary/unstable_feature.rs
@@ -1,18 +1,25 @@
-#![allow(internal_features)]
-
-#![feature(impl_stability)] 
-#![feature(trivial_bounds)] // TODO: figure out what is this
-
+#![allow(internal_features)] // Enabled to use #![feature(staged_api)] and #![feature(impl_stability)]
+#![feature(staged_api)] // Enabled to use  #![unstable(feature = "feat_foo", issue = "none")]
+#![feature(impl_stability)] // Enabled to use #[unstable_feature_bound(feat_foo)] 
+#![unstable(feature = "feat_foo", issue = "none")]
 
 pub trait Foo {
     fn foo();
 }
-
 pub struct Bar;
 
-#[unstable_feature_bound(feat_foo)]
+// Annotate the impl as unstable.
+#[unstable_feature_bound(feat_foo)] 
 impl Foo for Bar {
     fn foo() {}
 }
 
-fn main() {}
+// Use the unstable impl inside std/core.
+#[unstable_feature_bound(feat_foo)] 
+#[allow(dead_code)]
+fn bar() {
+    Bar::foo();
+}
+
+fn main() {
+}
--- tests/ui/internal/unstable-feature-bound.rs
+++ tests/ui/internal/unstable-feature-bound.rs
@@ -1,12 +1,12 @@
 //@ aux-build:unstable_feature.rs
 
-#![feature(impl_stability)]
-#![feature(trivial_bounds)]
+extern crate unstable_feature; //~ ERROR: use of unstable library feature `feat_foo`
+use unstable_feature::{Foo, Bar}; //~ ERROR: use of unstable library feature `feat_foo`
+//~^ ERROR: use of unstable library feature `feat_foo`
+//~^^ ERROR: use of unstable library feature `feat_foo`
 
-extern crate unstable_feature;
-use unstable_feature::{Foo, Bar};
-
-#[unstable_feature_bound(feat_foo)]
-fn main() { //~ ERROR: `main` function is not allowed to have a `where` clause
-    Bar::foo();
+fn main() { 
+    Bar::foo(); //~ ERROR: type annotations needed: cannot satisfy `unstable feature: `feat_foo``
+    //~^ ERROR: use of unstable library feature `feat_foo`
+    //~^^ ERROR: use of unstable library feature `feat_foo`
 }
