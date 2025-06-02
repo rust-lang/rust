d5a9a005188322cd08dca018402e1593d3a5ebd5
Andrew Zhogin <andrew.zhogin@gmail.com>
2025-06-03 03:27:21 +0700

    Fix for async drop inside async gen fn

--- tests/ui/async-await/async-drop/assign-incompatible-types.rs
+++ tests/ui/async-await/async-drop/assign-incompatible-types.rs
@@ -1,7 +1,8 @@
-//@ known-bug: #140530
+// ex-ice: #140530
 //@ edition: 2024
-
+//@ build-pass
 #![feature(async_drop, gen_blocks)]
+#![allow(incomplete_features)]
 async gen fn a() {
   _ = async {}
 }
