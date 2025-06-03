375eda2f856a1eac201f26213610f24d803444f8
Ohad Ravid <ohad.rv@gmail.com>
2025-06-03 07:36:50 +0300

    Added a mir-opt test for generating storage statements for scoped locals

---
+++ tests/mir-opt/copy-prop/issue_141649.rs
@@ -0,0 +1,27 @@
+// skip-filecheck
+// EMIT_MIR_FOR_EACH_PANIC_STRATEGY
+//@ test-mir-pass: CopyProp
+
+// EMIT_MIR issue_141649.main.CopyProp.diff
+fn main() {
+    struct S(usize, usize);
+    {
+        let s1 = S(1, 2);
+        drop(s1);
+    }
+    {
+        let s2 = S(3, 4);
+        drop(s2);
+    }
+
+    #[derive(Clone, Copy)]
+    struct C(usize, usize);
+    {
+        let c1 = C(1, 2);
+        drop(c1);
+    }
+    {
+        let c2 = C(3, 4);
+        drop(c2);
+    }
+}
