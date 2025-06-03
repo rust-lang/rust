108628ece59b320d7f5cb10d86254e28aabd23c2
tiif <pekyuan@gmail.com>
2025-06-03 12:07:55 +0000

    Make the type and trait stable

--- tests/ui/internal/auxiliary/unstable_feature.rs
+++ tests/ui/internal/auxiliary/unstable_feature.rs
@@ -2,15 +2,19 @@
 #![feature(staged_api)] // Enabled to use  #![unstable(feature = "feat_foo", issue = "none")]
 #![feature(impl_stability)] // Enabled to use #[unstable_feature_bound(feat_foo)] 
 #![allow(dead_code)]
-#![unstable(feature = "feat_foo", issue = "none" )]
+#![stable(feature = "a", since = "1.1.1" )]
 
+#[stable(feature = "a", since = "1.1.1" )]
 pub trait Foo {
+    #[stable(feature = "a", since = "1.1.1" )]
     fn foo();
 }
+#[stable(feature = "a", since = "1.1.1" )]
 pub struct Bar;
 
 // Annotate the impl as unstable.
 #[unstable_feature_bound(feat_foo)] 
+#[unstable(feature = "feat_foo", issue = "none" )]
 impl Foo for Bar {
     fn foo() {}
 }
--- tests/ui/internal/unstable-feature-bound.rs
+++ tests/ui/internal/unstable-feature-bound.rs
@@ -1,12 +1,8 @@
 //@ aux-build:unstable_feature.rs
 
-extern crate unstable_feature; //~ ERROR: use of unstable library feature `feat_foo`
-use unstable_feature::{Foo, Bar}; //~ ERROR: use of unstable library feature `feat_foo`
-//~^ ERROR: use of unstable library feature `feat_foo`
-//~^^ ERROR: use of unstable library feature `feat_foo`
+extern crate unstable_feature;
+use unstable_feature::{Foo, Bar};
 
 fn main() { 
     Bar::foo(); //~ ERROR: type annotations needed: cannot satisfy `unstable feature: `feat_foo``
-    //~^ ERROR: use of unstable library feature `feat_foo`
-    //~^^ ERROR: use of unstable library feature `feat_foo`
 }
