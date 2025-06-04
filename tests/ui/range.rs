ea2ea62fd1577cc67dc61d6b1b40e5f7427ed704
Samuel Tardieu <sam@rfc1149.net>
2025-06-04 13:40:50 +0200

    Note that using `enumerate()` will swap the arguments
    The autofix now:
    
    - includes the swapping of index and element in the closure in which the
      content will be consumed;
    - notes, with applicability `MaybeIncorrect` (because it will be), that
      the element and the index will be swapped.

--- tests/ui/range.rs
+++ tests/ui/range.rs
@@ -1,10 +1,22 @@
 #![allow(clippy::useless_vec)]
 #[warn(clippy::range_zip_with_len)]
 fn main() {
-    let v1 = vec![1, 2, 3];
-    let v2 = vec![4, 5];
+    let v1: Vec<u64> = vec![1, 2, 3];
+    let v2: Vec<u64> = vec![4, 5];
     let _x = v1.iter().zip(0..v1.len());
     //~^ range_zip_with_len
+
+    //~v range_zip_with_len
+    for (e, i) in v1.iter().zip(0..v1.len()) {
+        let _: &u64 = e;
+        let _: usize = i;
+    }
+
+    //~v range_zip_with_len
+    v1.iter().zip(0..v1.len()).for_each(|(e, i)| {
+        let _: &u64 = e;
+        let _: usize = i;
+    });
 
     let _y = v1.iter().zip(0..v2.len()); // No error
 }
