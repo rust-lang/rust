1d7c1b1b4f038219e434db5d38d08cc31b3dd186
Lukas Wirth <lukas.wirth@ferrous-systems.com>
2025-06-05 08:42:30 +0200

    Add more missing 2015 edition directives

--- tests/ui/traits/const-traits/mbe-dyn-const-2015.rs
+++ tests/ui/traits/const-traits/mbe-dyn-const-2015.rs
@@ -1,6 +1,7 @@
 // Ensure that the introduction of const trait bound didn't regress this code in Rust 2015.
 // See also `mbe-const-trait-bound-theoretical-regression.rs`.
 
+//@ edition: 2015
 //@ check-pass
 
 macro_rules! check {
