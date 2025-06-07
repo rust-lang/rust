52824052d7782762056aec11179e08ed015c0dd3
Ralf Jung <post@ralfj.de>
2025-06-07 14:14:09 +0200

    cci_inrinsic tets: do not use atomic intrinsic

--- tests/ui/intrinsics/auxiliary/cci_intrinsic.rs
+++ tests/ui/intrinsics/auxiliary/cci_intrinsic.rs
@@ -1,24 +1,12 @@
 #![feature(intrinsics, adt_const_params)]
 
-pub mod rusti {
-    use std::marker::ConstParamTy;
-
-    #[derive(Debug, ConstParamTy, PartialEq, Eq)]
-    pub enum AtomicOrdering {
-        // These values must match the compiler's `AtomicOrdering` defined in
-        // `rustc_middle/src/ty/consts/int.rs`!
-        Relaxed = 0,
-        Release = 1,
-        Acquire = 2,
-        AcqRel = 3,
-        SeqCst = 4,
-    }
-
+mod rusti {
     #[rustc_intrinsic]
-    pub unsafe fn atomic_xchg<T, const ORD: AtomicOrdering>(dst: *mut T, src: T) -> T;
+    pub unsafe fn size_of_val<T: ?Sized>(ptr: *const T) -> usize;
 }
 
+// A monomorphic function, inlined cross-crate, referencing an intrinsic.
 #[inline(always)]
-pub fn atomic_xchg_seqcst(dst: *mut isize, src: isize) -> isize {
-    unsafe { rusti::atomic_xchg::<_, { rusti::AtomicOrdering::SeqCst }>(dst, src) }
+pub fn size_of_val(val: &[u8]) -> usize {
+    unsafe { rusti::size_of_val(val) }
 }
