e90df0f43f123482862793385e5d939db01b75e4
Jeremy Smart <jeremy3141592@gmail.com>
2025-06-07 10:54:34 -0400

    fix tests

--- library/core/src/iter/traits/iterator.rs
+++ library/core/src/iter/traits/iterator.rs
@@ -2925,8 +2925,8 @@ pub const trait Iterator {
     ///
     /// ```
     /// #![feature(iter_contains)]
-    /// assert!([1, 2, 3].iter().contains(2));
-    /// assert!(![1, 2, 3].iter().contains(5));
+    /// assert!([1, 2, 3].iter().contains(&2));
+    /// assert!(![1, 2, 3].iter().contains(&5));
     /// ```
     ///
     /// [`Iterator::contains`] can be used where [`slice::contains`] cannot:
