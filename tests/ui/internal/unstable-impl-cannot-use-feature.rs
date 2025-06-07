2aec0ab4ef541bb03e786876324e4aff2c6b6303
tiif <pekyuan@gmail.com>
2025-06-07 16:29:31 +0000

    Combine some tests

--- tests/ui/internal/unstable-impl-cannot-use-feature.rs
+++ tests/ui/internal/unstable-impl-cannot-use-feature.rs
@@ -1,27 +1,30 @@
+//@ revisions: pass fail
+//@[pass] check-pass
+
 #![allow(internal_features)]
 #![feature(staged_api)]
 #![feature(impl_stability)]
 #![allow(dead_code)]
-#![feature(feat_foo)]
+#![cfg_attr(fail, feature(feat_foo))]
 #![unstable(feature = "feat_foo", issue = "none" )]
 
-/// In staged-api crate, impl that is marked with #[unstable_feature_bound(..)]
-/// cannot be enabled with #[feature(..)]
+/// In staged-api crate, using an unstable impl requires
+/// #[unstable_feature_bound(..)], not  #[feature(..)].
 
 pub trait Foo {
     fn foo();
 }
 pub struct Bar;
 
-// Annotate the impl as unstable.
 #[unstable_feature_bound(feat_foo)]
 impl Foo for Bar {
     fn foo() {}
 }
 
+#[cfg_attr(pass, unstable_feature_bound(feat_foo))]
 fn bar() {
     Bar::foo();
-    //~^ ERROR: cannot satisfy `unstable feature: `feat_foo``
+    //[fail]~^ ERROR: cannot satisfy `unstable feature: `feat_foo``
 }
 
 fn main() {}
