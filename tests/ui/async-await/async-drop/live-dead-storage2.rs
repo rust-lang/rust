d6a9081612e55b4d755163ce54d94ea0a96e4a2b
Andrew Zhogin <andrew.zhogin@gmail.com>
2025-06-08 21:32:04 +0700

    Async drop - fix for StorageLive/StorageDead codegen for pinned async drop future

--- tests/ui/async-await/async-drop/live-dead-storage2.rs
+++ tests/ui/async-await/async-drop/live-dead-storage2.rs
@@ -1,7 +1,11 @@
-//@ known-bug: #140531
-//@compile-flags: -Zlint-mir --crate-type lib
+// ex-ice: #140531
+//@ compile-flags: -Zlint-mir --crate-type lib
 //@ edition:2024
+//@ check-pass
+
 #![feature(async_drop)]
+#![allow(incomplete_features)]
+
 async fn call_once(f: impl AsyncFnOnce()) {
     let fut = Box::pin(f());
 }
