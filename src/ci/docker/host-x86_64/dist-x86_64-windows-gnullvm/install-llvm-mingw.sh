cacda770ef9507c82d86d3e5ef3c8a096d6157e2
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-06-09 23:43:35 +0200

    update llvm-mingw

--- src/ci/docker/host-x86_64/dist-x86_64-windows-gnullvm/install-llvm-mingw.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-windows-gnullvm/install-llvm-mingw.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-release_date=20250430
+release_date=20250528
 archive=llvm-mingw-${release_date}-ucrt-ubuntu-22.04-x86_64.tar.xz
 curl -L https://github.com/mstorsjo/llvm-mingw/releases/download/${release_date}/${archive} | \
 tar --extract --xz --strip 1 --directory /usr/local
