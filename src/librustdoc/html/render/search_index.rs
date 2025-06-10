5e8ebd5ecd8546591a6707ac9e1a3b8a64c72f76
binarycat <binarycat@envs.net>
2025-06-10 13:35:46 -0500

    rustdoc: IndexItem::{stability -> is_unstable}

--- src/librustdoc/html/render/search_index.rs
+++ src/librustdoc/html/render/search_index.rs
@@ -93,7 +93,7 @@ pub(crate) fn build_index(
                 ),
                 aliases: item.attrs.get_doc_aliases(),
                 deprecation: item.deprecation(tcx),
-                stability: item.stability(tcx),
+                is_unstable: item.stability(tcx).is_some_and(|x| x.is_unstable()),
             });
         }
     }
@@ -700,7 +700,7 @@ pub(crate) fn build_index(
                     // bitmasks always use 1-indexing for items, with 0 as the crate itself
                     deprecated.push(u32::try_from(index + 1).unwrap());
                 }
-                if item.is_unstable() {
+                if item.is_unstable {
                     unstable.push(u32::try_from(index + 1).unwrap());
                 }
             }
