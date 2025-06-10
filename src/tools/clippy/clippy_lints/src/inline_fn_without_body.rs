f50c732f747801bce8065b7bc22e461c950230d4
Jana Dönszelmann <jana@donsz.nl>
2025-06-10 08:48:56 +0200

    fix clippy

--- src/tools/clippy/clippy_lints/src/inline_fn_without_body.rs
+++ src/tools/clippy/clippy_lints/src/inline_fn_without_body.rs
@@ -1,6 +1,6 @@
 use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::sugg::DiagExt;
-use rustc_attr_parsing::{find_attr, AttributeKind};
+use rustc_attr_data_structures::{find_attr, AttributeKind};
 use rustc_errors::Applicability;
 use rustc_hir::{TraitFn, TraitItem, TraitItemKind};
 use rustc_lint::{LateContext, LateLintPass};
