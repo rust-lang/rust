b5b87d324efb1855f7d63b40eba8821d9fb822c6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-06-11 21:29:06 +0200

    Put negative implementors first and apply same ordering logic to foreign implementors

--- src/librustdoc/html/static/js/rustdoc.d.ts
+++ src/librustdoc/html/static/js/rustdoc.d.ts
@@ -467,7 +467,7 @@ declare namespace rustdoc {
      * Provied by generated `trait.impl` files.
      */
     type Implementors = {
-        [key: string]: Array<[string, number, Array<string>]>
+        [key: string]: Array<[string, 0|1, number, Array<string>]>
     }
 
     type TypeImpls = {
