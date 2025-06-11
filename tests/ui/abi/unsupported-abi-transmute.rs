03564e52253af10f8c04e05cfda468869e02c13d
Jubilee Young <workingjubilee@gmail.com>
2025-06-11 16:09:58 -0700

    fixup! tests: Adopt ABI transmute tests from crashtests

--- tests/ui/abi/unsupported-abi-transmute.rs
+++ tests/ui/abi/unsupported-abi-transmute.rs
@@ -1,7 +1,15 @@
-#![feature(abi_gpu_kernel)]
+//@ add-core-stubs
+//@ compile-flags: --crate-type=lib --target x86_64-unknown-none
+//@ needs-llvm-components: x86
+//@ edition: 2018
+#![no_core]
+#![feature(no_core, lang_items)]
+extern crate minicore;
+use minicore::*;
+
 // Check we error before unsupported ABIs reach codegen stages.
 
-fn main() {
-    let a = unsafe { core::mem::transmute::<usize, extern "gpu-kernel" fn(i32)>(4) }(2);
-    //~^ ERROR E0570
+fn anything() {
+    let a = unsafe { mem::transmute::<usize, extern "thiscall" fn(i32)>(4) }(2);
+    //~^ ERROR: is not a supported ABI for the current target [E0570]
 }
