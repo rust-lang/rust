ef38bc4ee6181eeee72457be208c520caeaea069
dianqk <dianqk@dianqk.net>
2025-06-11 21:38:52 +0800

    mir-opt: Eliminate dead ref statements

---
+++ tests/mir-opt/dead-store-elimination/ref.rs
@@ -0,0 +1,19 @@
+//@ test-mir-pass: DeadStoreElimination-initial
+
+pub struct Foo {
+    a: i32,
+    b: i64,
+    c: i32,
+}
+
+// EMIT_MIR ref.tuple.DeadStoreElimination-initial.diff
+pub fn tuple(v: (i32, &Foo)) -> i32 {
+    // CHECK-LABEL: fn tuple
+    // CHECK: debug _dead => [[dead:_[0-9]+]];
+    // CHECK: bb0:
+    // FIXME: Preserve `tmp` for debuginfo, but we can merge it into the debuginfo.
+    // CHECK: [[tmp:_[0-9]+]] = deref_copy (_1.1: &Foo);
+    // CHECK-NEXT: DBG: [[dead]] = &((*[[tmp]]).2: i32)
+    let _dead = &v.1.c;
+    v.1.a
+}
