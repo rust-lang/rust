d9d7f81d90b02a306a69e741e891950a35234b0e
tiif <pekyuan@gmail.com>
2025-06-11 12:51:50 +0000

    Fix codegen test

--- tests/ui/internal/auxiliary/unstable_impl_codegen_aux2.rs
+++ tests/ui/internal/auxiliary/unstable_impl_codegen_aux2.rs
@@ -1,18 +1,14 @@
 //@ aux-build:unstable_impl_codegen_aux1.rs
-
-#![allow(internal_features)]
-#![feature(staged_api)]
-#![feature(impl_stability)]
-#![stable(feature = "a", since = "1.1.1" )]
-#[feature(foo)]
+#![feature(foo)]
 
 extern crate unstable_impl_codegen_aux1 as aux;
+use aux::Trait;
 
 /// Upstream crate for unstable impl codegen test
 /// that depends on aux crate in
 /// unstable_impl_codegen_aux1.rs
 
-fn foo<T>(a:T) {
+pub fn foo<T: Trait>(a:T) {
     a.method();
 }
 
--- tests/ui/internal/unstable_impl_codegen.rs
+++ tests/ui/internal/unstable_impl_codegen.rs
@@ -1,8 +1,13 @@
 //@ aux-build:unstable_impl_codegen_aux2.rs
+//@ check-pass
+#![feature(foo)]
 
 /// Downstream crate for unstable impl codegen test
 /// that depends on upstream crate in
 /// unstable_impl_codegen_aux2.rs
+
+extern crate unstable_impl_codegen_aux2 as aux;
+use aux::foo;
 
 fn main() {
     foo(1_u8);
