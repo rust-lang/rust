9f50246741cd0350c6c2efd16cfd47c8ca9996e2
Jubilee Young <workingjubilee@gmail.com>
2025-06-12 11:34:45 -0700

    tests: Convert linkage-attr test to cross-compiling and bless

--- tests/ui/linkage-attr/raw-dylib/windows/unsupported-abi.rs
+++ tests/ui/linkage-attr/raw-dylib/windows/unsupported-abi.rs
@@ -1,6 +1,14 @@
-//@ only-x86_64
-//@ only-windows
+//@ add-core-stubs
+//@ compile-flags: --target x86_64-pc-windows-msvc
 //@ compile-flags: --crate-type lib --emit link
+//@ needs-llvm-components: x86
+#![no_core]
+#![feature(no_core)]
+extern crate minicore;
+
+// It may seem weird this is a cross-platform-testable thing, since doesn't it test linkage?
+// However the main thing we are testing is an *error*, so it works fine!
+
 #[link(name = "foo", kind = "raw-dylib")]
 extern "stdcall" {
 //~^ WARN: calling convention not supported on this target
