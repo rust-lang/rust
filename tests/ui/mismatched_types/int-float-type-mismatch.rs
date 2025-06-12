62f53e33ea65f1352f2bffd9068ab6358b8c7300
Kivooeo <Kivooeo123@gmail.com>
2025-06-12 21:51:23 +0500

    moved renamed docs stderr | integral-variable-unification-error.rs

--- tests/ui/mismatched_types/int-float-type-mismatch.rs
+++ tests/ui/mismatched_types/int-float-type-mismatch.rs
@@ -1,7 +1,9 @@
+//! Test type mismatch error when assigning float to integer variable
+
 fn main() {
     let mut x //~ NOTE expected due to the type of this binding
         =
-        2; //~ NOTE expected due to this value
+        2;    //~ NOTE expected due to this value
     x = 5.0;
     //~^ ERROR mismatched types
     //~| NOTE expected integer, found floating-point number
