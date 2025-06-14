3d5a96fdfcc1a95ba5fd019e715e9dd7dcd5d959
Nicholas Nethercote <n.nethercote@gmail.com>
2025-06-14 20:22:30 +1000

    Fix some comments.
    As per the previous commit, generic args here can only appear on the
    final segment. So make the comments obey that constraint.

--- src/rustdoc-json-types/lib.rs
+++ src/rustdoc-json-types/lib.rs
@@ -269,8 +269,8 @@ pub struct PolyTrait {
 /// A set of generic arguments provided to a path segment, e.g.
 ///
 /// ```text
-/// std::option::Option::<u32>::None
-///                      ^^^^^
+/// std::option::Option<u32>
+///                    ^^^^^
 /// ```
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -323,7 +323,7 @@ pub enum GenericArg {
     Const(Constant),
     /// A generic argument that's explicitly set to be inferred.
     /// ```text
-    /// std::vec::Vec::<_>::new()
+    /// std::vec::Vec::<_>
     ///                 ^
     /// ```
     Infer,
