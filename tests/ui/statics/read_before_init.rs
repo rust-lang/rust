cfc22cfffb7a75229752de6a76e2fc94791b5203
Oli Scherer <github333195615777966@oli-obk.de>
2025-06-16 10:52:29 +0000

    Ensure copy* intrinsics also perform the static self-init checks

--- tests/ui/statics/read_before_init.rs
+++ tests/ui/statics/read_before_init.rs
@@ -1,8 +1,15 @@
-//@ check-pass
+//! This test checks the one code path that does not go through
+//! the regular CTFE memory access (as an optimization). We forgot
+//! to duplicate the static item self-initialization check, allowing
+//! reading from the uninitialized static memory before it was
+//! initialized at the end of the static initializer.
+//!
+//! https://github.com/rust-lang/rust/issues/142532
 
 use std::mem::MaybeUninit;
 
 pub static X: (i32, MaybeUninit<i32>) = (1, foo(&X.0));
+//~^ ERROR: encountered static that tried to initialize itself with itself
 
 const fn foo(x: &i32) -> MaybeUninit<i32> {
     let mut temp = MaybeUninit::<i32>::uninit();
