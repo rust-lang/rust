aa0082bf1f1be28afb27d50f8533a085a44d505b
Jane Losare-Lusby <jlusby@yaah.dev>
2025-06-16 14:28:10 -0700

    Add test verifying that changing comments recompiles dependents

---
+++ tests/run-make/rdr-ignores-comments/rmake.rs
@@ -0,0 +1,24 @@
+// tests that changing the content of a comment will cause a change in the rmeta of a file
+use run_make_support::{rfs, rustc};
+use std::hash::{Hash, Hasher};
+use std::path::Path;
+
+fn main() {
+    let before = check_and_hash("before.rs");
+    let after = check_and_hash("after.rs");
+    dbg!(before, after);
+    assert_neq!(before, after);
+}
+
+fn check_and_hash<P>(filename: P) -> u64
+where
+    P: AsRef<Path>,
+{
+    rfs::rename(filename, "foo.rs");
+    rustc().input("foo.rs").emit("metadata").run();
+    // hash the output
+    let bytes = rfs::read("libfoo.rmeta");
+    let mut hasher = std::hash::DefaultHasher::new();
+    bytes.hash(&mut hasher);
+    hasher.finish()
+}
