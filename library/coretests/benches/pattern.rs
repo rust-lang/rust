dc18aaec42b7d0573aa5f0ccda6c2c74a6e6e0a3
Jubilee Young <workingjubilee@gmail.com>
2025-06-16 14:10:29 -0700

    library: Add a very simple microbenchmark for splitn

--- library/coretests/benches/pattern.rs
+++ library/coretests/benches/pattern.rs
@@ -39,3 +39,27 @@ fn ends_with_str(b: &mut Bencher) {
         }
     })
 }
+
+#[bench]
+fn splitn_on_http_response(b: &mut Bencher) {
+    fn parse_http(s: &str) -> Result<(&str, &str, &str), &str> {
+        let mut parts = s.splitn(3, ' ');
+        let version = parts.next().ok_or("No version")?;
+        let code = parts.next().ok_or("No status code")?;
+        let description = parts.next().ok_or("No description")?;
+        Ok((version, code, description))
+    }
+
+    let response = String::from("HTTP/1.1 418 I'm a teapot\r\n");
+    let mut res: (&str, &str, &str) = ("", "", "");
+    b.iter(|| {
+        for _ in 0..1024 {
+            res = black_box(match parse_http(black_box(&response)) {
+                Ok(data) => data,
+                Err(_) => {
+                    continue;
+                }
+            })
+        }
+    })
+}
