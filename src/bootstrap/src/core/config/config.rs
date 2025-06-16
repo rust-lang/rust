ac8920fa4c931a1df2de11734177d08dcbbf1029
Jakub Beránek <berykubik@gmail.com>
2025-06-16 17:12:42 +0200

    Do not allow building anything on stage 0

--- src/bootstrap/src/core/config/config.rs
+++ src/bootstrap/src/core/config/config.rs
@@ -1023,7 +1023,7 @@ impl Config {
             || install_stage.is_some()
             || check_stage.is_some()
             || bench_stage.is_some();
-        // See https://github.com/rust-lang/compiler-team/issues/326
+
         config.stage = match config.cmd {
             Subcommand::Check { .. } => flags_stage.or(check_stage).unwrap_or(0),
             Subcommand::Clippy { .. } | Subcommand::Fix => flags_stage.or(check_stage).unwrap_or(1),
@@ -1050,6 +1050,12 @@ impl Config {
             | Subcommand::Suggest { .. }
             | Subcommand::Vendor { .. } => flags_stage.unwrap_or(0),
         };
+
+        // Now check that the selected stage makes sense, and if not, print a warning and end
+        if let (0, Subcommand::Build) = (config.stage, &config.cmd) {
+            eprintln!("WARNING: cannot build anything on stage 0. Use at least stage 1.");
+            exit!(1);
+        }
 
         // CI should always run stage 2 builds, unless it specifically states otherwise
         #[cfg(not(test))]
