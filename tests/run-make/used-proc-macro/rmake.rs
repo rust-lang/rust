cb88811c5b8f0a5ef2c53431b370654df0768d3b
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-06-17 20:31:16 +0000

    Generate symbols.o for proc-macros too
    To ensure used statics are functioning correctly for proc-macros too.

---
+++ tests/run-make/used-proc-macro/rmake.rs
@@ -0,0 +1,17 @@
+// Test that #[used] statics are included in the final dylib for proc-macros too.
+
+//@ ignore-cross-compile
+//@ needs-crate-type: proc-macro
+//@ ignore-musl (FIXME: can't find `-lunwind`)
+
+use run_make_support::{dynamic_lib_name, llvm_readobj, rustc};
+
+fn main() {
+    rustc().input("dep.rs").run();
+    rustc().input("proc_macro.rs").run();
+    llvm_readobj()
+        .input(dynamic_lib_name("proc_macro"))
+        .arg("--all")
+        .run()
+        .assert_stdout_contains("VERY_IMPORTANT_SYMBOL");
+}
