634016478ec95c6ff933d32789e663ace78e8f82
Manuel Drehwald <git@manuel.drehwald.info>
2025-06-18 15:29:43 -0700

    add -Zoffload=Enable flag behind -Zunstable-options, to enable gpu (host) code generation

--- compiler/rustc_codegen_llvm/src/back/lto.rs
+++ compiler/rustc_codegen_llvm/src/back/lto.rs
@@ -668,6 +668,12 @@ pub(crate) fn run_pass_manager(
         write::llvm_optimize(cgcx, dcx, module, None, config, opt_level, opt_stage, stage)?;
     }
 
+    if enable_gpu && !thin {
+        let cx =
+            SimpleCx::new(module.module_llvm.llmod(), &module.module_llvm.llcx, cgcx.pointer_size);
+        crate::builder::gpu_offload::handle_gpu_code(cgcx, &cx);
+    }
+
     if cfg!(llvm_enzyme) && enable_ad && !thin {
         let cx =
             SimpleCx::new(module.module_llvm.llmod(), &module.module_llvm.llcx, cgcx.pointer_size);
