d374279ce20764d15b6ee9b615b72810fe941503
Trevor Gross <tmgross@umich.edu>
2025-06-19 06:12:22 +0000

    Migrate some tests away from `concat-idents`
    `concat_idents!` is in the process of being removed, but a few things it
    is used to test will still be relevant. Migrate these tests to something
    other than `concat_idents`.

--- tests/ui/issues/issue-50403.rs
+++ tests/ui/issues/issue-50403.rs
@@ -1,6 +1,12 @@
-#![feature(concat_idents)]
-#![expect(deprecated)] // concat_idents is deprecated
+// Issue 50403
+// Ensure that `concat` can't create empty identifiers
+// FIXME(macro_metavar_expr_concat): this error message could be improved
+
+macro_rules! empty {
+    () => { ${concat()} } //~ ERROR expected identifier or string literal
+                          //~^ERROR expected expression
+}
 
 fn main() {
-    let x = concat_idents!(); //~ ERROR `concat_idents!()` takes 1 or more arguments
+    let x = empty!();
 }
