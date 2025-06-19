f8ab9343b8e6f195be036bdd99884c8721876820
Trevor Gross <tmgross@umich.edu>
2025-06-19 06:12:22 +0000

    Migrate some tests away from `concat-idents`
    `concat_idents!` is in the process of being removed, but a few things it
    is used to test will still be relevant. Migrate these tests to something
    other than `concat_idents`.

--- tests/ui/issues/issue-32950.rs
+++ tests/ui/issues/issue-32950.rs
@@ -1,10 +1,12 @@
-#![feature(concat_idents)]
-#![expect(deprecated)] // concat_idents is deprecated
+// Issue: #32950
+// Ensure that using macros rather than a type doesn't break `derive`.
 
 #[derive(Debug)]
-struct Baz<T>(
-    concat_idents!(Foo, Bar) //~ ERROR `derive` cannot be used on items with type macros
-                             //~^ ERROR cannot find type `FooBar` in this scope
-);
+struct Nonsense<T> {
+    //~^ ERROR type parameter `T` is never used
+    should_be_vec_t: vec![T],
+    //~^ ERROR `derive` cannot be used on items with type macros
+    //~| ERROR expected type, found `expr` metavariable
+}
 
 fn main() {}
