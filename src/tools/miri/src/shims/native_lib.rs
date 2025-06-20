6a0976a5211b4132c079475d79e33bf8ebabc832
Nia Espera <a5b6@riseup.net>
2025-06-20 22:14:25 +0200

    cfg if

--- src/tools/miri/src/shims/native_lib.rs
+++ src/tools/miri/src/shims/native_lib.rs
@@ -232,12 +232,9 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
         let (ret, maybe_memevents) =
             this.call_native_with_args(link_name, dest, code_ptr, libffi_args)?;
 
-        #[cfg(target_os = "linux")]
-        if let Some(events) = maybe_memevents {
+        if cfg!(target_os = "linux") && let Some(events) = maybe_memevents {
             trace!("Registered FFI events:\n{events:#0x?}");
         }
-        #[cfg(not(target_os = "linux"))]
-        let _ = maybe_memevents; // Suppress the unused warning.
 
         this.write_immediate(*ret, dest)?;
         interp_ok(true)
