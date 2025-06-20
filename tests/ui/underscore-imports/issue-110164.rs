d82fb1e72df42c8a18e354f6a9cc778d19055a10
Esteban Küber <esteban@kuber.com.ar>
2025-06-20 19:20:16 +0000

    Emit a single error when importing a path with `_`
    When encountering `use _;`, `use _::*'` or similar, do not emit two errors for that single mistake. This also side-steps the issue of resolve errors suggesting adding a crate named `_` to `Cargo.toml`.

--- tests/ui/underscore-imports/issue-110164.rs
+++ tests/ui/underscore-imports/issue-110164.rs
@@ -7,16 +7,12 @@ use crate::*;
 //~^ ERROR unresolved import `crate::*`
 use _::a;
 //~^ ERROR expected identifier, found reserved identifier `_`
-//~| ERROR unresolved import `_`
 use _::*;
 //~^ ERROR expected identifier, found reserved identifier `_`
-//~| ERROR unresolved import `_`
 
 fn main() {
     use _::a;
     //~^ ERROR expected identifier, found reserved identifier `_`
-    //~| ERROR unresolved import `_`
     use _::*;
     //~^ ERROR expected identifier, found reserved identifier `_`
-    //~| ERROR unresolved import `_`
 }
