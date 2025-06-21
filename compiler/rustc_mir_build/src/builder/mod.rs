01dd29f2c0f9c58e2847617b407e50aec7839fe7
Camille GILLOT <gillot.camille@gmail.com>
2025-06-21 12:44:19 +0000

    Introduce VarBindingIntroduction.

--- compiler/rustc_mir_build/src/builder/mod.rs
+++ compiler/rustc_mir_build/src/builder/mod.rs
@@ -1054,7 +1054,10 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                                 opt_ty_info: param.ty_span,
                                 opt_match_place: Some((None, span)),
                                 pat_span: span,
-                                introductions: vec![(span, false)],
+                                introductions: vec![VarBindingIntroduction {
+                                    span,
+                                    is_shorthand: false,
+                                }],
                             }))
                         };
                     self.var_indices.insert(var, LocalsForNode::One(local));
