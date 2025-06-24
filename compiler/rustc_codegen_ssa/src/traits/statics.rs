35ccb266509600f67bf9c8dc35d8071a1f642a31
Oli Scherer <github333195615777966@oli-obk.de>
2025-06-24 14:38:37 +0000

    Remove const_data_from_alloc to make cg_ssa more amenable for cg_clif

--- compiler/rustc_codegen_ssa/src/traits/statics.rs
+++ compiler/rustc_codegen_ssa/src/traits/statics.rs
@@ -1,10 +1,10 @@
-use rustc_abi::Align;
 use rustc_hir::def_id::DefId;
+use rustc_middle::mir::interpret::ConstAllocation;
 
 use super::BackendTypes;
 
 pub trait StaticCodegenMethods: BackendTypes {
-    fn static_addr_of(&self, cv: Self::Value, align: Align, kind: Option<&str>) -> Self::Value;
+    fn static_addr_of(&self, alloc: ConstAllocation<'_>, kind: Option<&str>) -> Self::Value;
     fn get_value_name(&self, val: Self::Value) -> &[u8];
     fn set_value_name(&self, val: Self::Value, name: &[u8]);
     fn codegen_static(&mut self, def_id: DefId);
