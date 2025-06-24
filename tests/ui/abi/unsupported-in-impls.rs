12d05d868fd43d6d930d7d9f9648ae7443751003
Jubilee Young <workingjubilee@gmail.com>
2025-06-24 15:14:33 -0700

    tests: migrate unsupported-abi-transmute.rs to extern "rust-invalid"

--- tests/ui/abi/unsupported-in-impls.rs
+++ tests/ui/abi/unsupported-in-impls.rs
@@ -1,6 +1,9 @@
 // Test for https://github.com/rust-lang/rust/issues/86232
 // Due to AST-to-HIR lowering nuances, we used to allow unsupported ABIs to "leak" into the HIR
 // without being checked, as we would check after generating the ExternAbi.
+// Checking afterwards only works if we examine every HIR construct that contains an ExternAbi,
+// and those may be very different in HIR, even if they read the same in source.
+// This made it very easy to make mistakes.
 //
 // Here we test that an unsupported ABI in various impl-related positions will be rejected,
 // both in the original declarations and the actual implementations.
