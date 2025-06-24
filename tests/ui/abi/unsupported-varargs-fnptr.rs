78652b783dd1549d4782c2daba7d12214e001b54
Jubilee Young <workingjubilee@gmail.com>
2025-06-24 15:22:59 -0700

    tests: specify why extern "rust-invalid" cannot be used in varargs test

--- tests/ui/abi/unsupported-varargs-fnptr.rs
+++ tests/ui/abi/unsupported-varargs-fnptr.rs
@@ -1,5 +1,6 @@
 // FIXME(workingjubilee): add revisions and generalize to other platform-specific varargs ABIs,
 // preferably after the only-arch directive is enhanced with an "or pattern" syntax
+// NOTE: This deliberately tests an ABI that supports varargs, so no `extern "rust-invalid"`
 //@ only-x86_64
 
 // We have to use this flag to force ABI computation of an invalid ABI
