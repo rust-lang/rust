b067154d8fedef3d6b05903e40c5ff671ebf455d
Oli Scherer <github333195615777966@oli-obk.de>
2025-06-24 09:35:15 +0000

    Move `scalar_to_backend` to ssa

--- compiler/rustc_codegen_ssa/src/traits/statics.rs
+++ compiler/rustc_codegen_ssa/src/traits/statics.rs
@@ -5,7 +5,10 @@ use super::BackendTypes;
 
 pub trait StaticCodegenMethods: BackendTypes {
     fn static_addr_of(&self, cv: Self::Value, align: Align, kind: Option<&str>) -> Self::Value;
+    fn get_value_name(&self, val: Self::Value) -> &[u8];
+    fn set_value_name(&self, val: Self::Value, name: &[u8]);
     fn codegen_static(&mut self, def_id: DefId);
+    fn get_static(&self, def_id: DefId) -> Self::Value;
 }
 
 pub trait StaticBuilderMethods: BackendTypes {
