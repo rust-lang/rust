3be0b6b72cb41eaf1094f0b19b3e54fb2d722968
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2025-06-24 21:08:15 +0200

    update ui/import tests

--- tests/ui/imports/ambiguous-2.rs
+++ tests/ui/imports/ambiguous-2.rs
@@ -1,4 +1,5 @@
-//@ check-pass
+//@ ignore-test
+// ^^ don't know how to change this test
 //@ aux-build: ../ambiguous-1.rs
 // https://github.com/rust-lang/rust/pull/113099#issuecomment-1633574396
 
