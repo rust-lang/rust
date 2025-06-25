dcd0350aaa6972bd0a9e800f159fd6d0e0f175a3
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2025-06-25 13:30:12 +0200

    Fix feature-gate-format_args_nl UI test

--- tests/ui/feature-gates/feature-gate-format_args_nl.rs
+++ tests/ui/feature-gates/feature-gate-format_args_nl.rs
@@ -1,4 +1,4 @@
-use std::format_args_nl;
+use std::format_args_nl; //~ ERROR `format_args_nl` is only for internal language use
 
 fn main() {
     format_args_nl!(""); //~ ERROR `format_args_nl` is only for internal language use
