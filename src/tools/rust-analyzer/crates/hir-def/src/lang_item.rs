10fce1fb94425485845f047bea4200495b6e6c5b
Shoyu Vanilla <modulo641@gmail.com>
2025-06-25 23:23:11 +0900

    Backport new sized-hierarchy trait bounds in old ways

--- src/tools/rust-analyzer/crates/hir-def/src/lang_item.rs
+++ src/tools/rust-analyzer/crates/hir-def/src/lang_item.rs
@@ -304,6 +304,8 @@ impl LangItem {
 language_item_table! {
 //  Variant name,            Name,                     Getter method name,         Target                  Generic requirements;
     Sized,                   sym::sized,               sized_trait,                Target::Trait,          GenericRequirement::Exact(0);
+    MetaSized,               sym::meta_sized,          sized_trait,                Target::Trait,          GenericRequirement::Exact(0);
+    PointeeSized,            sym::pointee_sized,       sized_trait,                Target::Trait,          GenericRequirement::Exact(0);
     Unsize,                  sym::unsize,              unsize_trait,               Target::Trait,          GenericRequirement::Minimum(1);
     /// Trait injected by `#[derive(PartialEq)]`, (i.e. "Partial EQ").
     StructuralPeq,           sym::structural_peq,      structural_peq_trait,       Target::Trait,          GenericRequirement::None;
