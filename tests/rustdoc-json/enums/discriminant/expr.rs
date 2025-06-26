3fb092972785c8dcc243755955663ed4c8c182c2
Fluid <90795031+fluiderson@users.noreply.github.com>
2025-06-26 19:26:06 +0300

    `enums/discriminant/` is done

--- tests/rustdoc-json/enums/discriminant/expr.rs
+++ tests/rustdoc-json/enums/discriminant/expr.rs
@@ -1,30 +1,39 @@
 pub enum Foo {
-    //@ is "$.index[?(@.name=='Addition')].inner.variant.discriminant.value" '"0"'
-    //@ is "$.index[?(@.name=='Addition')].inner.variant.discriminant.expr" '"{ _ }"'
+    //@ arg addition .index[] | select(.name == "Addition").inner.variant.discriminant?
+    //@ jq $addition.value? == "0"
+    //@ jq $addition.expr? == "{ _ }"
     Addition = 0 + 0,
-    //@ is "$.index[?(@.name=='Bin')].inner.variant.discriminant.value" '"1"'
-    //@ is "$.index[?(@.name=='Bin')].inner.variant.discriminant.expr" '"0b1"'
+    //@ arg bin .index[] | select(.name == "Bin").inner.variant.discriminant?
+    //@ jq $bin.value? == "1"
+    //@ jq $bin.expr? == "0b1"
     Bin = 0b1,
-    //@ is "$.index[?(@.name=='Oct')].inner.variant.discriminant.value" '"2"'
-    //@ is "$.index[?(@.name=='Oct')].inner.variant.discriminant.expr" '"0o2"'
+    //@ arg oct .index[] | select(.name == "Oct").inner.variant.discriminant?
+    //@ jq $oct.value? == "2"
+    //@ jq $oct.expr? == "0o2"
     Oct = 0o2,
-    //@ is "$.index[?(@.name=='PubConst')].inner.variant.discriminant.value" '"3"'
-    //@ is "$.index[?(@.name=='PubConst')].inner.variant.discriminant.expr" '"THREE"'
+    //@ arg pub_const .index[] | select(.name == "PubConst").inner.variant.discriminant?
+    //@ jq $pub_const.value? == "3"
+    //@ jq $pub_const.expr? == "THREE"
     PubConst = THREE,
-    //@ is "$.index[?(@.name=='Hex')].inner.variant.discriminant.value" '"4"'
-    //@ is "$.index[?(@.name=='Hex')].inner.variant.discriminant.expr" '"0x4"'
+    //@ arg hex .index[] | select(.name == "Hex").inner.variant.discriminant?
+    //@ jq $hex.value? == "4"
+    //@ jq $hex.expr? == "0x4"
     Hex = 0x4,
-    //@ is "$.index[?(@.name=='Cast')].inner.variant.discriminant.value" '"5"'
-    //@ is "$.index[?(@.name=='Cast')].inner.variant.discriminant.expr" '"{ _ }"'
+    //@ arg cast .index[] | select(.name == "Cast").inner.variant.discriminant?
+    //@ jq $cast.value? == "5"
+    //@ jq $cast.expr? == "{ _ }"
     Cast = 5 as isize,
-    //@ is "$.index[?(@.name=='PubCall')].inner.variant.discriminant.value" '"6"'
-    //@ is "$.index[?(@.name=='PubCall')].inner.variant.discriminant.expr" '"{ _ }"'
+    //@ arg pub_call .index[] | select(.name == "PubCall").inner.variant.discriminant?
+    //@ jq $pub_call.value? == "6"
+    //@ jq $pub_call.expr? == "{ _ }"
     PubCall = six(),
-    //@ is "$.index[?(@.name=='PrivCall')].inner.variant.discriminant.value" '"7"'
-    //@ is "$.index[?(@.name=='PrivCall')].inner.variant.discriminant.expr" '"{ _ }"'
+    //@ arg priv_call .index[] | select(.name == "PrivCall").inner.variant.discriminant?
+    //@ jq $priv_call.value? == "7"
+    //@ jq $priv_call.expr? == "{ _ }"
     PrivCall = seven(),
-    //@ is "$.index[?(@.name=='PrivConst')].inner.variant.discriminant.value" '"8"'
-    //@ is "$.index[?(@.name=='PrivConst')].inner.variant.discriminant.expr" '"EIGHT"'
+    //@ arg priv_const .index[] | select(.name == "PrivConst").inner.variant.discriminant?
+    //@ jq $priv_const.value? == "8"
+    //@ jq $priv_const.expr? == "EIGHT"
     PrivConst = EIGHT,
 }
 
