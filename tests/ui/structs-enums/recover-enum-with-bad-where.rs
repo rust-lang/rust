4e51e67a24d940d3c8069062d07f663ee2034ac3
Michael Goulet <michael@errs.io>
2025-06-26 18:08:15 +0000

    Better recovery

--- tests/ui/structs-enums/recover-enum-with-bad-where.rs
+++ tests/ui/structs-enums/recover-enum-with-bad-where.rs
@@ -1,6 +1,8 @@
 pub enum Foo<T>
 where:
-//~^ ERROR expected one of `#`, `{`, lifetime, or type, found `:`
+//~^ ERROR unexpected colon after `where`
     T: Missing, {}
+//~^ ERROR cannot find trait `Missing` in this scope
+// (evidence that we continue parsing after the erroneous colon)
 
 fn main() {}
