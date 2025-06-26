d484bd159278bc6ca6aaf67ca73604d93e0c7d66
Rémy Rakic <remy.rakic+github@gmail.com>
2025-06-26 15:27:23 +0000

    add NLL-like imprecision examples
    These tests showcase the same imprecision as NLLs, unlike the datalog
    implementation, when using reachability as a liveness approximation.

---
+++ tests/ui/nll/polonius/iterating-updating-cursor-108704.rs
@@ -0,0 +1,47 @@
+// An example from #108704 of the linked-list cursor-like pattern of #46859/#48001, where the
+// polonius alpha analysis shows the same imprecision as NLLs, unlike the datalog implementation.
+
+//@ ignore-compare-mode-polonius (explicit revisions)
+//@ revisions: nll polonius legacy
+//@ [polonius] compile-flags: -Z polonius=next
+//@ [legacy] check-pass
+//@ [legacy] compile-flags: -Z polonius=legacy
+
+struct Root {
+    children: Vec<Node>,
+}
+
+struct Node {
+    name: String,
+    children: Vec<Node>,
+}
+
+fn merge_tree_ok(root: &mut Root, path: Vec<String>) {
+    let mut elements = &mut root.children;
+
+    for p in path.iter() {
+        for (idx, el) in elements.iter_mut().enumerate() {
+            if el.name == *p {
+                elements = &mut elements[idx].children;
+                break;
+            }
+        }
+    }
+}
+
+fn merge_tree_ko(root: &mut Root, path: Vec<String>) {
+    let mut elements = &mut root.children;
+
+    for p in path.iter() {
+        for (idx, el) in elements.iter_mut().enumerate() {
+            //[nll]~^ ERROR cannot borrow `*elements` as mutable more than once at a time
+            //[polonius]~^^ ERROR cannot borrow `*elements` as mutable more than once at a time
+            if el.name == *p {
+                elements = &mut el.children;
+                break;
+            }
+        }
+    }
+}
+
+fn main() {}
