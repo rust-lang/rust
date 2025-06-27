97d350d79806f1987b02565e055bbda7c8138b2a
Fluid <90795031+fluiderson@users.noreply.github.com>
2025-06-27 13:23:04 +0300

    `fns/` is done

--- tests/rustdoc-json/fns/async_return.rs
+++ tests/rustdoc-json/fns/async_return.rs
@@ -4,30 +4,30 @@
 
 use std::future::Future;
 
-//@ is "$.index[?(@.name=='get_int')].inner.function.sig.output.primitive" \"i32\"
-//@ is "$.index[?(@.name=='get_int')].inner.function.header.is_async" false
+//@ jq .index[] | select(.name == "get_int").inner.function | .sig?.output.primitive == "i32" and .header?.is_async == false
 pub fn get_int() -> i32 {
     42
 }
 
-//@ is "$.index[?(@.name=='get_int_async')].inner.function.sig.output.primitive" \"i32\"
-//@ is "$.index[?(@.name=='get_int_async')].inner.function.header.is_async" true
+//@ jq .index[] | select(.name == "get_int_async").inner.function | .sig?.output.primitive == "i32" and .header?.is_async == true
 pub async fn get_int_async() -> i32 {
     42
 }
 
-//@ is "$.index[?(@.name=='get_int_future')].inner.function.sig.output.impl_trait[0].trait_bound.trait.path" '"Future"'
-//@ is "$.index[?(@.name=='get_int_future')].inner.function.sig.output.impl_trait[0].trait_bound.trait.args.angle_bracketed.constraints[0].name" '"Output"'
-//@ is "$.index[?(@.name=='get_int_future')].inner.function.sig.output.impl_trait[0].trait_bound.trait.args.angle_bracketed.constraints[0].binding.equality.type.primitive"  \"i32\"
-//@ is "$.index[?(@.name=='get_int_future')].inner.function.header.is_async" false
+//@ arg get_int_future .index[] | select(.name == "get_int_future").inner.function
+//@ jq $get_int_future.sig?.output.impl_trait[]?.trait_bound.trait?.path == "Future"
+//@ jq $get_int_future.sig?.output.impl_trait[]?.trait_bound.trait?.args.angle_bracketed?.constraints?[].name == "Output"
+//@ jq $get_int_future.sig?.output.impl_trait[]?.trait_bound.trait?.args.angle_bracketed?.constraints?[].binding.equality.type?.primitive == "i32"
+//@ jq $get_int_future.header?.is_async == false
 pub fn get_int_future() -> impl Future<Output = i32> {
     async { 42 }
 }
 
-//@ is "$.index[?(@.name=='get_int_future_async')].inner.function.sig.output.impl_trait[0].trait_bound.trait.path" '"Future"'
-//@ is "$.index[?(@.name=='get_int_future_async')].inner.function.sig.output.impl_trait[0].trait_bound.trait.args.angle_bracketed.constraints[0].name" '"Output"'
-//@ is "$.index[?(@.name=='get_int_future_async')].inner.function.sig.output.impl_trait[0].trait_bound.trait.args.angle_bracketed.constraints[0].binding.equality.type.primitive" \"i32\"
-//@ is "$.index[?(@.name=='get_int_future_async')].inner.function.header.is_async" true
+//@ arg get_int_future_async .index[] | select(.name == "get_int_future_async").inner.function
+//@ jq $get_int_future_async.sig?.output.impl_trait[]?.trait_bound.trait?.path == "Future"
+//@ jq $get_int_future_async.sig?.output.impl_trait[]?.trait_bound.trait?.args.angle_bracketed?.constraints?[].name == "Output"
+//@ jq $get_int_future_async.sig?.output.impl_trait[]?.trait_bound.trait?.args.angle_bracketed?.constraints?[].binding.equality.type?.primitive == "i32"
+//@ jq $get_int_future_async.header?.is_async == true
 pub async fn get_int_future_async() -> impl Future<Output = i32> {
     async { 42 }
 }
