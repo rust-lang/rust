13bbc3203043e45619e5957672a973f988b66aa4
Ohad Ravid <ohad.rv@gmail.com>
2025-06-29 08:39:42 +0300

    Move `MaybeUninitializedLocals` to the ssa module

--- compiler/rustc_mir_dataflow/src/impls/mod.rs
+++ compiler/rustc_mir_dataflow/src/impls/mod.rs
@@ -6,7 +6,7 @@ mod storage_liveness;
 pub use self::borrowed_locals::{MaybeBorrowedLocals, borrowed_locals};
 pub use self::initialized::{
     EverInitializedPlaces, EverInitializedPlacesDomain, MaybeInitializedPlaces,
-    MaybeUninitializedLocals, MaybeUninitializedPlaces, MaybeUninitializedPlacesDomain,
+    MaybeUninitializedPlaces, MaybeUninitializedPlacesDomain,
 };
 pub use self::liveness::{
     MaybeLiveLocals, MaybeTransitiveLiveLocals, TransferFunction as LivenessTransferFunction,
