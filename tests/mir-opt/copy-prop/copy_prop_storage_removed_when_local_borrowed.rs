215d5746c3f40087829e6e673cb29b2f6df40783
Ohad Ravid <ohad.rv@gmail.com>
2025-06-29 09:12:20 +0300

    Added FileCheck to copy prop storage tests

--- tests/mir-opt/copy-prop/copy_prop_storage_removed_when_local_borrowed.rs
+++ tests/mir-opt/copy-prop/copy_prop_storage_removed_when_local_borrowed.rs
@@ -1,23 +1,26 @@
-// skip-filecheck
+//! Check that we remove the storage statements if one of the locals is borrowed,
+//! and the head isn't borrowed.
 //@ test-mir-pass: CopyProp
 
 #![feature(custom_mir, core_intrinsics, freeze)]
 
-// Check that we remove the storage statements if one of the locals is borrowed,
-// and the head isn't borrowed.
-
 use std::intrinsics::mir::*;
 use std::marker::Freeze;
 
-// EMIT_MIR copy_prop_borrowed_storage_not_removed.f.CopyProp.diff
+// EMIT_MIR copy_prop_storage_removed_when_local_borrowed.f.CopyProp.diff
 
 #[custom_mir(dialect = "runtime")]
 pub fn f<T: Copy + Freeze>(_1: (T, T)) -> T {
+    // CHECK-LABEL: fn f(
     mir! {
         let _2: T;
         let _3: T;
         let _4: &T;
+        // CHECK: bb0: {
         {
+            // CHECK-NOT: StorageLive(_2);
+            // CHECK: _4 = &_2;
+            // CHECK-NOT: StorageDead(_2);
             StorageLive(_2);
             _2 = _1.0;
             _3 = _2;
