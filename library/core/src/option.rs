7cd06dce3edc7c5d7b0bc46675424b62c4a38fa2
Esteban Küber <esteban@kuber.com.ar>
2025-07-01 17:15:26 +0000

    Set issue

--- library/core/src/option.rs
+++ library/core/src/option.rs
@@ -2111,7 +2111,7 @@ where
 impl<T> crate::clone::UseCloned for Option<T> where T: crate::clone::UseCloned {}
 
 #[stable(feature = "rust1", since = "1.0.0")]
-#[rustc_const_unstable(feature = "const_default", issue = "none")]
+#[rustc_const_unstable(feature = "const_default", issue = "67792")]
 impl<T> const Default for Option<T> {
     /// Returns [`None`][Option::None].
     ///
