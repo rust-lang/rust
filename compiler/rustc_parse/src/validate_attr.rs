02fb8317b5cdda98122d85067ebfd3f9ab3b6fc9
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2025-07-02 23:09:25 +0200

    Port `#[path]` to the new attribute parsing infrastructure
    Signed-off-by: Jonathan Brouwer <jonathantbrouwer@gmail.com>

--- compiler/rustc_parse/src/validate_attr.rs
+++ compiler/rustc_parse/src/validate_attr.rs
@@ -271,31 +271,33 @@ pub fn check_builtin_meta_item(
         if matches!(
             name,
             sym::inline
-            | sym::may_dangle
-            | sym::rustc_as_ptr
-            | sym::rustc_pub_transparent
-            | sym::rustc_const_stable_indirect
-            | sym::rustc_force_inline
-            | sym::rustc_confusables
-            | sym::rustc_skip_during_method_dispatch
-            | sym::repr
-            | sym::align
-            | sym::deprecated
-            | sym::optimize
-            | sym::cold
-            | sym::target_feature
-            | sym::rustc_allow_const_fn_unstable
-            | sym::naked
-            | sym::no_mangle
-            | sym::must_use
-            | sym::track_caller
-            | sym::link_name
-            | sym::export_name
-            | sym::rustc_macro_transparency
-            | sym::link_section
-            | sym::rustc_layout_scalar_valid_range_start
-            | sym::rustc_layout_scalar_valid_range_end
-        | sym::no_implicit_prelude) {
+                | sym::may_dangle
+                | sym::rustc_as_ptr
+                | sym::rustc_pub_transparent
+                | sym::rustc_const_stable_indirect
+                | sym::rustc_force_inline
+                | sym::rustc_confusables
+                | sym::rustc_skip_during_method_dispatch
+                | sym::repr
+                | sym::align
+                | sym::deprecated
+                | sym::optimize
+                | sym::cold
+                | sym::target_feature
+                | sym::rustc_allow_const_fn_unstable
+                | sym::naked
+                | sym::no_mangle
+                | sym::must_use
+                | sym::track_caller
+                | sym::link_name
+                | sym::export_name
+                | sym::rustc_macro_transparency
+                | sym::link_section
+                | sym::rustc_layout_scalar_valid_range_start
+                | sym::rustc_layout_scalar_valid_range_end
+                | sym::path
+                | sym::no_implicit_prelude
+        ) {
             return;
         }
         emit_malformed_attribute(psess, style, meta.span, name, template);
