4a6f6977f98026a9359a3b168b6d50045b1c07a8
binarycat <binarycat@envs.net>
2025-07-03 14:06:32 -0500

    tidy: update files_modified to take CiInfo

--- src/tools/tidy/src/rustdoc_json.rs
+++ src/tools/tidy/src/rustdoc_json.rs
@@ -14,7 +14,7 @@ pub fn check(src_path: &Path, ci_info: &crate::CiInfo, bad: &mut bool) {
     };
 
     // First we check that `src/rustdoc-json-types` was modified.
-    if !crate::files_modified(base_commit, |p| p == RUSTDOC_JSON_TYPES) {
+    if !crate::files_modified(ci_info, |p| p == RUSTDOC_JSON_TYPES) {
         // `rustdoc-json-types` was not modified so nothing more to check here.
         println!("`rustdoc-json-types` was not modified.");
         return;
