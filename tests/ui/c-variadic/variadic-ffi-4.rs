290c4dff3878555f27679e88c61f64b85a7e5732
beetrees <b@beetr.ee>
2025-07-04 17:44:15 +0100

    Rework `c_variadic`

--- tests/ui/c-variadic/variadic-ffi-4.rs
+++ tests/ui/c-variadic/variadic-ffi-4.rs
@@ -2,37 +2,30 @@
 #![no_std]
 #![feature(c_variadic)]
 
-use core::ffi::{VaList, VaListImpl};
+use core::ffi::VaList;
 
-pub unsafe extern "C" fn no_escape0<'f>(_: usize, ap: ...) -> VaListImpl<'f> {
+pub unsafe extern "C" fn no_escape0<'f>(_: usize, ap: ...) -> VaList<'f> {
     ap
     //~^ ERROR: lifetime may not live long enough
-    //~| ERROR: lifetime may not live long enough
 }
 
-pub unsafe extern "C" fn no_escape1(_: usize, ap: ...) -> VaListImpl<'static> {
+pub unsafe extern "C" fn no_escape1(_: usize, ap: ...) -> VaList<'static> {
     ap //~ ERROR: lifetime may not live long enough
 }
 
-pub unsafe extern "C" fn no_escape2(_: usize, ap: ...) {
-    let _ = ap.with_copy(|ap| ap); //~ ERROR: lifetime may not live long enough
-}
-
-pub unsafe extern "C" fn no_escape3(_: usize, mut ap0: &mut VaListImpl, mut ap1: ...) {
+pub unsafe extern "C" fn no_escape3(_: usize, mut ap0: &mut VaList, mut ap1: ...) {
     *ap0 = ap1;
     //~^ ERROR: lifetime may not live long enough
-    //~| ERROR: lifetime may not live long enough
 }
 
-pub unsafe extern "C" fn no_escape4(_: usize, mut ap0: &mut VaListImpl, mut ap1: ...) {
+pub unsafe extern "C" fn no_escape4(_: usize, mut ap0: &mut VaList, mut ap1: ...) {
     ap0 = &mut ap1;
     //~^ ERROR: `ap1` does not live long enough
     //~| ERROR: lifetime may not live long enough
     //~| ERROR: lifetime may not live long enough
 }
 
-pub unsafe extern "C" fn no_escape5(_: usize, mut ap0: &mut VaListImpl, mut ap1: ...) {
+pub unsafe extern "C" fn no_escape5(_: usize, mut ap0: &mut VaList, mut ap1: ...) {
     *ap0 = ap1.clone();
     //~^ ERROR: lifetime may not live long enough
-    //~| ERROR: lifetime may not live long enough
 }
