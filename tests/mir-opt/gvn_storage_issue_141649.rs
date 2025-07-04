fc029e447513077deb68152bc3580c562d3fc530
Ohad Ravid <ohad.rv@gmail.com>
2025-07-04 15:37:11 +0300

    Added another small test to gvn storage

--- tests/mir-opt/gvn_storage_issue_141649.rs
+++ tests/mir-opt/gvn_storage_issue_141649.rs
@@ -2,6 +2,7 @@
 //@ test-mir-pass: GVN
 // EMIT_MIR gvn_storage_issue_141649.f.GVN.diff
 // EMIT_MIR gvn_storage_issue_141649.f_borrowed.GVN.diff
+// EMIT_MIR gvn_storage_issue_141649.f_dead.GVN.diff
 
 #![feature(custom_mir, core_intrinsics)]
 
@@ -53,6 +54,29 @@ pub fn f_borrowed(_1: u32) {
             // Because `*_4` will be replaced with `_2`,
             // we have to remove the storage statements of `_2`.
             _5 = *_4;
+            Return()
+        }
+    }
+}
+
+#[custom_mir(dialect = "runtime")]
+pub fn f_dead(_1: u32) {
+    // CHECK-LABEL: fn f_dead(
+    mir! {
+        let _2: S;
+        let _3: S;
+        {
+            // CHECK-NOT: StorageLive(_2);
+            // CHECK: StorageLive(_3);
+            // CHECK: _3 = copy _2;
+            // CHECK: StorageDead(_3);
+            // CHECK-NOT: StorageDead(_2);
+            StorageLive(_2);
+            _2 = S(_1, 2);
+            StorageDead(_2);
+            StorageLive(_3);
+            _3 = S(_1, 2);
+            StorageDead(_3);
             Return()
         }
     }
