d48376462a12244d764871c5d6b650eacbe4e4ad
Oli Scherer <github333195615777966@oli-obk.de>
2025-07-04 12:54:34 +0000

    Rename GlobalAlloc::Type to TypeId

--- compiler/rustc_codegen_cranelift/src/constant.rs
+++ compiler/rustc_codegen_cranelift/src/constant.rs
@@ -175,7 +175,7 @@ pub(crate) fn codegen_const_value<'tcx>(
                             fx.module.declare_data_in_func(data_id, &mut fx.bcx.func);
                         fx.bcx.ins().global_value(fx.pointer_type, local_data_id)
                     }
-                    GlobalAlloc::Type { .. } => {
+                    GlobalAlloc::TypeId { .. } => {
                         return CValue::const_val(
                             fx,
                             layout,
@@ -367,7 +367,7 @@ fn define_all_allocs(tcx: TyCtxt<'_>, module: &mut dyn Module, cx: &mut Constant
                     GlobalAlloc::Memory(alloc) => alloc,
                     GlobalAlloc::Function { .. }
                     | GlobalAlloc::Static(_)
-                    | GlobalAlloc::Type { .. }
+                    | GlobalAlloc::TypeId { .. }
                     | GlobalAlloc::VTable(..) => {
                         unreachable!()
                     }
@@ -479,7 +479,7 @@ fn define_all_allocs(tcx: TyCtxt<'_>, module: &mut dyn Module, cx: &mut Constant
                         .principal()
                         .map(|principal| tcx.instantiate_bound_regions_with_erased(principal)),
                 ),
-                GlobalAlloc::Type { .. } => {
+                GlobalAlloc::TypeId { .. } => {
                     // Nothing to do, the bytes/offset of this pointer have already been written together with all other bytes,
                     // so we just need to drop this provenance.
                     continue;
--- compiler/rustc_middle/src/ty/print/pretty.rs
+++ compiler/rustc_middle/src/ty/print/pretty.rs
@@ -1779,7 +1779,7 @@ pub trait PrettyPrinter<'tcx>: Printer<'tcx> + fmt::Write {
                         }
                         Some(GlobalAlloc::Function { .. }) => p!("<function>"),
                         Some(GlobalAlloc::VTable(..)) => p!("<vtable>"),
-                        Some(GlobalAlloc::Type { .. }) => p!("<typeid>"),
+                        Some(GlobalAlloc::TypeId { .. }) => p!("<typeid>"),
                         None => p!("<dangling pointer>"),
                     }
                     return Ok(());
