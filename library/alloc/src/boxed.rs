49cc5483259a86baf388f12b24a4c28da58a172d
Vrtgs <vrtgs@vrtgs.xyz>
2025-07-04 16:40:56 +0300

    add fallback body to box_new intrinsic

--- library/alloc/src/boxed.rs
+++ library/alloc/src/boxed.rs
@@ -239,7 +239,9 @@ pub struct Box<
 /// This is the surface syntax for `box <expr>` expressions.
 #[rustc_intrinsic]
 #[unstable(feature = "liballoc_internals", issue = "none")]
-pub fn box_new<T>(x: T) -> Box<T>;
+pub fn box_new<T>(x: T) -> Box<T> {
+    Box::write(Box::new_uninit(), x)
+}
 
 impl<T> Box<T> {
     /// Allocates memory on the heap and then places `x` into it.
