2111525850863ec2f4c14c964ae37a761de64f63
xizheyin <xizheyin@smail.nju.edu.cn>
2025-07-04 20:54:45 +0800

    Add test false-sealed-traits-note.rs
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

---
+++ tests/ui/privacy/sealed-traits/false-sealed-traits-note.rs
@@ -0,0 +1,13 @@
+// when we impl TraitA for Struct, it can compile, we should not emit sealed traits note, see issue #143392
+
+mod inner {
+    pub trait TraitA {}
+
+    pub trait TraitB: TraitA {}
+}
+
+struct Struct;
+
+impl inner::TraitB for Struct {} //~ ERROR the trait bound `Struct: TraitA` is not satisfied [E0277]
+
+fn main(){}
