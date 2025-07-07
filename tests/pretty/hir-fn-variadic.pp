e18dc172064b0a5c14eb8b1e1c5efa35b46f2866
beetrees <b@beetr.ee>
2025-07-08 00:09:05 +0100

    `c_variadic`: Make `fn f(...) {}` error like `fn f(u32) {}` outside of `extern` blocks

--- tests/pretty/hir-fn-variadic.pp
+++ tests/pretty/hir-fn-variadic.pp
@@ -16,12 +16,12 @@ unsafe extern "C" fn bar(_: i32, mut va2: ...) -> usize { va2.arg::<usize>() }
 
 fn main() {
     fn g1(_: extern "C" fn(_: u8, va: ...)) { }
-    fn g2(_: extern "C" fn(_: u8, ...)) { }
+    fn g2(_: extern "C" fn(_: u8, _: ...)) { }
     fn g3(_: extern "C" fn(u8, va: ...)) { }
-    fn g4(_: extern "C" fn(u8, ...)) { }
+    fn g4(_: extern "C" fn(u8, _: ...)) { }
 
     fn g5(_: extern "C" fn(va: ...)) { }
-    fn g6(_: extern "C" fn(...)) { }
+    fn g6(_: extern "C" fn(_: ...)) { }
 
     {
         let _ =
