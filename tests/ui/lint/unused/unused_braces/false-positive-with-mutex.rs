ab221a4c2df91c877b697fe54df2436496a57d6c
xizheyin <xizheyin@smail.nju.edu.cn>
2025-07-07 17:37:30 +0800

    Add test tests/ui/lint/unused/unused_braces/false-positive-with-mutex.rs
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

---
+++ tests/ui/lint/unused/unused_braces/false-positive-with-mutex.rs
@@ -0,0 +1,35 @@
+// We should not emit unused braces warning for `Mutex`
+// See issue #143562
+
+#![warn(unused)]
+#![deny(warnings)]
+
+use std::sync::Mutex;
+
+struct Test {
+    a: i32,
+    b: i32,
+}
+
+struct VecVec {
+    v: Vec<i32>,
+}
+
+impl VecVec {
+    pub fn push(mut self, value: i32) -> Self {
+        self.v.push(value);
+        self
+    }
+}
+
+pub fn main() {
+    let test = Mutex::new(Test {
+        a: 12,
+        b: 34,
+    });
+    // the program will hang if this pair of braces is removed
+    let vec_vec = VecVec { v: Vec::new() }
+        .push({ test.lock().unwrap().a })  //~ ERROR unnecessary braces around method argument [unused_braces]
+        .push({ test.lock().unwrap().b }); //~ ERROR unnecessary braces around method argument [unused_braces]
+    println!("len: {}", vec_vec.v.len());
+}
