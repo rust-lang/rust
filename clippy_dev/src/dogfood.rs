Jarcho/rust-clippy

Add helper function for creating cargo commands.

clippy_dev/src/dogfood.rs
@@ -1,6 +1,5 @@
-use crate::utils::run_success;
+use crate::utils::{cargo_cmd, run_success};
 use itertools::Itertools;
-use std::process::Command;
 
 /// # Panics
 ///
@@ -9,7 +8,7 @@ use std::process::Command;
 pub fn dogfood(fix: bool, allow_dirty: bool, allow_staged: bool, allow_no_vcs: bool) {
     run_success(
         "cargo test",
-        Command::new("cargo")
+        cargo_cmd()
             .args(["test", "--test", "dogfood"])
             .args(["--features", "internal"])
             .args(["--", "dogfood_clippy", "--nocapture"])

clippy_dev/src/lint.rs
@@ -1,6 +1,6 @@
-use crate::utils::{cargo_clippy_path, run_success};
+use crate::utils::{cargo_clippy_path, cargo_cmd, run_success};
+use std::fs;
 use std::process::{self, Command};
-use std::{env, fs};
 
 pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>) {
     let is_file = match fs::metadata(path) {
@@ -14,7 +14,7 @@ pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>
     if is_file {
         run_success(
             "cargo run",
-            Command::new(env::var("CARGO").unwrap_or("cargo".into()))
+            cargo_cmd()
                 .args(["run", "--bin", "clippy-driver", "--"])
                 .args(["-L", "./target/debug"])
                 .args(["-Z", "no-codegen"])
@@ -25,10 +25,7 @@ pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>
                 .env("RUSTC_ICE", "0"),
         );
     } else {
-        run_success(
-            "cargo build",
-            Command::new(env::var("CARGO").unwrap_or("cargo".into())).arg("build"),
-        );
+        run_success("cargo build", cargo_cmd().arg("build"));
         run_success(
             "cargo clippy",
             Command::new(cargo_clippy_path())

clippy_dev/src/setup/toolchain.rs
@@ -1,10 +1,9 @@
-use crate::utils::run_success;
+use crate::utils::{cargo_cmd, run_success};
 use std::env::consts::EXE_SUFFIX;
 use std::env::current_dir;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 use walkdir::WalkDir;
 
 pub fn create(standalone: bool, force: bool, release: bool, name: &str) {
@@ -46,7 +45,7 @@ pub fn create(standalone: bool, force: bool, release: bool, name: &str) {
 
     run_success(
         "cargo build",
-        Command::new("cargo").arg("build").args(release.then_some("--release")),
+        cargo_cmd().arg("build").args(release.then_some("--release")),
     );
 
     install_bin("cargo-clippy", &dest, standalone, release);

rust-lang/rust

clippy_dev/src/dogfood.rs
@@ -1,6 +1,5 @@
-use crate::utils::run_exit_on_err;
+use crate::utils::{cargo_cmd, run_exit_on_err};
 use itertools::Itertools;
-use std::process::Command;
 
 /// # Panics
 ///
@@ -9,7 +8,7 @@ use std::process::Command;
 pub fn dogfood(fix: bool, allow_dirty: bool, allow_staged: bool, allow_no_vcs: bool) {
     run_exit_on_err(
         "cargo test",
-        Command::new("cargo")
+        cargo_cmd()
             .args(["test", "--test", "dogfood"])
             .args(["--features", "internal"])
             .args(["--", "dogfood_clippy", "--nocapture"])

clippy_dev/src/lint.rs
@@ -1,6 +1,6 @@
-use crate::utils::{cargo_clippy_path, run_exit_on_err};
+use crate::utils::{cargo_clippy_path, cargo_cmd, run_exit_on_err};
+use std::fs;
 use std::process::{self, Command};
-use std::{env, fs};
 
 pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>) {
     let is_file = match fs::metadata(path) {
@@ -14,7 +14,7 @@ pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>
     if is_file {
         run_exit_on_err(
             "cargo run",
-            Command::new(env::var("CARGO").unwrap_or("cargo".into()))
+            cargo_cmd()
                 .args(["run", "--bin", "clippy-driver", "--"])
                 .args(["-L", "./target/debug"])
                 .args(["-Z", "no-codegen"])
@@ -25,10 +25,7 @@ pub fn run<'a>(path: &str, edition: &str, args: impl Iterator<Item = &'a String>
                 .env("RUSTC_ICE", "0"),
         );
     } else {
-        run_exit_on_err(
-            "cargo build",
-            Command::new(env::var("CARGO").unwrap_or_else(|_| "cargo".into())).arg("build"),
-        );
+        run_exit_on_err("cargo build", cargo_cmd().arg("build"));
         run_exit_on_err(
             "cargo clippy",
             Command::new(cargo_clippy_path())

clippy_dev/src/setup/toolchain.rs
@@ -1,10 +1,9 @@
-use crate::utils::run_exit_on_err;
+use crate::utils::{cargo_cmd, run_exit_on_err};
 use std::env::consts::EXE_SUFFIX;
 use std::env::current_dir;
 use std::ffi::OsStr;
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 use walkdir::WalkDir;
 
 pub fn create(standalone: bool, force: bool, release: bool, name: &str) {
@@ -46,7 +45,7 @@ pub fn create(standalone: bool, force: bool, release: bool, name: &str) {
 
     run_exit_on_err(
         "cargo build",
-        Command::new("cargo").arg("build").args(release.then_some("--release")),
+        cargo_cmd().arg("build").args(release.then_some("--release")),
     );
 
     install_bin("cargo-clippy", &dest, standalone, release);