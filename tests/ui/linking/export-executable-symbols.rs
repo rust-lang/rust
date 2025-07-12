46616f37a7e81ef30e4172a265e37ea4b2e5e3e9
usamoi <usamoi@outlook.com>
2025-07-13 00:12:33 +0800

    pass --gc-sections if -Zexport-executable-symbols is enabled and improve tests

--- tests/ui/linking/export-executable-symbols.rs
+++ tests/ui/linking/export-executable-symbols.rs
@@ -1,14 +1,11 @@
 //@ run-pass
-//@ only-linux
-//@ only-gnu
-//@ compile-flags: -Zexport-executable-symbols
+//@ compile-flags: -Ctarget-feature=-crt-static -Zexport-executable-symbols
+//@ ignore-wasm
 //@ edition: 2024
 
 // Regression test for <https://github.com/rust-lang/rust/issues/101610>.
 
 #![feature(rustc_private)]
-
-extern crate libc;
 
 #[unsafe(no_mangle)]
 fn hack() -> u64 {
@@ -16,9 +13,31 @@ fn hack() -> u64 {
 }
 
 fn main() {
+    #[cfg(unix)]
     unsafe {
+        extern crate libc;
         let handle = libc::dlopen(std::ptr::null(), libc::RTLD_NOW);
         let ptr = libc::dlsym(handle, c"hack".as_ptr());
+        let ptr: Option<unsafe fn() -> u64> = std::mem::transmute(ptr);
+        if let Some(f) = ptr {
+            assert!(f() == 998244353);
+            println!("symbol `hack` is found successfully");
+        } else {
+            panic!("symbol `hack` is not found");
+        }
+    }
+    #[cfg(windows)]
+    unsafe {
+        type PCSTR = *const u8;
+        type HMODULE = *mut core::ffi::c_void;
+        type FARPROC = Option<unsafe extern "system" fn() -> isize>;
+        #[link(name = "kernel32", kind = "raw-dylib")]
+        unsafe extern "system" {
+            fn GetModuleHandleA(lpmodulename: PCSTR) -> HMODULE;
+            fn GetProcAddress(hmodule: HMODULE, lpprocname: PCSTR) -> FARPROC;
+        }
+        let handle = GetModuleHandleA(std::ptr::null_mut());
+        let ptr = GetProcAddress(handle, b"hack\0".as_ptr());
         let ptr: Option<unsafe fn() -> u64> = std::mem::transmute(ptr);
         if let Some(f) = ptr {
             assert!(f() == 998244353);
