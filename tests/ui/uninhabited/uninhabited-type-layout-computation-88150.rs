521a754d34444f4af4765fc5fabe76e29c693d18
Oneirical <manchot@videotron.ca>
2025-07-13 16:39:45 -0400

    Rehome tests/ui/issues/ tests [4/?]

---
+++ tests/ui/uninhabited/uninhabited-type-layout-computation-88150.rs
@@ -0,0 +1,22 @@
+// https://github.com/rust-lang/rust/issues/88150
+//@ run-pass
+//@ compile-flags:-C debuginfo=2
+//@ edition:2018
+
+use core::marker::PhantomData;
+
+pub struct Foo<T: ?Sized, A>(
+    PhantomData<(A, T)>,
+);
+
+enum Never {}
+
+impl<T: ?Sized> Foo<T, Never> {
+    fn new_foo() -> Foo<T, Never> {
+        Foo(PhantomData)
+    }
+}
+
+fn main() {
+    let _ = Foo::<[()], Never>::new_foo();
+}
