1aa2951c3e2fb8049bc0e60786d41a2da9658664
Oli Scherer <github333195615777966@oli-obk.de>
2025-07-16 15:56:56 +0000

    Validate transmutes in CTFE

--- tests/ui/consts/promotion-in-const-validation.rs
+++ tests/ui/consts/promotion-in-const-validation.rs
@@ -1,9 +1,9 @@
 fn foo() {}
 
 const _: &usize = unsafe { &std::mem::transmute(foo as fn()) };
-//~^ ERROR: constructing invalid value at .<deref>: encountered a pointer, but expected an integer
+//~^ ERROR: encountered a pointer, but expected an integer
 
 const _: usize = unsafe { std::mem::transmute(foo as fn()) };
-//~^ ERROR: unable to turn pointer into integer
+//~^ ERROR: encountered a pointer, but expected an integer
 
 fn main() {}
