496a9d6fab6b68ffd1728ca6c550e57cf0ce8ba5
Ralf Jung <post@ralfj.de>
2025-07-17 11:25:24 +0200

    stabilize varargs for system, sysv64, win64, efiapi, aapcs

--- tests/ui/feature-gates/feature-gate-c_variadic.rs
+++ tests/ui/feature-gates/feature-gate-c_variadic.rs
@@ -2,3 +2,9 @@
 
 pub unsafe extern "C" fn test(_: i32, ap: ...) { }
 //~^ ERROR C-variadic functions are unstable
+
+trait Trait {
+    unsafe extern "C" fn trait_test(_: i32, ap: ...) { }
+    //~^ ERROR C-variadic functions are unstable
+    //~| ERROR only foreign, `unsafe extern "C"`, or `unsafe extern "C-unwind"` functions may have a C-variadic arg
+}
