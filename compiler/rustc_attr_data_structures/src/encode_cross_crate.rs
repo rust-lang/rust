49ae52e3ffeb3d98f19845eba5eb9fa1408fa432
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2025-07-17 08:02:26 +0200

    Fix encoding of `link_section` and `no_mangle` cross crate

--- compiler/rustc_attr_data_structures/src/encode_cross_crate.rs
+++ compiler/rustc_attr_data_structures/src/encode_cross_crate.rs
@@ -40,9 +40,9 @@ impl AttributeKind {
             Fundamental { .. } => Yes,
             Ignore { .. } => No,
             Inline(..) => No,
-            LinkName { .. } => Yes,
+            LinkName { .. } => Yes, // Needed for rustdoc
             LinkOrdinal { .. } => No,
-            LinkSection { .. } => No,
+            LinkSection { .. } => Yes, // Needed for rustdoc
             LoopMatch(..) => No,
             MacroTransparency(..) => Yes,
             Marker(..) => No,
@@ -50,8 +50,8 @@ impl AttributeKind {
             MustUse { .. } => Yes,
             Naked(..) => No,
             NoImplicitPrelude(..) => No,
-            NoMangle(..) => No,
-            NonExhaustive(..) => Yes,
+            NoMangle(..) => Yes,      // Needed for rustdoc
+            NonExhaustive(..) => Yes, // Needed for rustdoc
             OmitGdbPrettyPrinterSection => No,
             Optimize(..) => No,
             ParenSugar(..) => No,
