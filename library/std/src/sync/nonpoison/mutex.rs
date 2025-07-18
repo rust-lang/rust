2f6dd51984b73cac9249524c34d979485ab03977
Connor Tsui <connor.tsui20@gmail.com>
2025-07-18 08:09:04 +0200

    update `mapped_lock_guard` features

--- library/std/src/sync/nonpoison/mutex.rs
+++ library/std/src/sync/nonpoison/mutex.rs
@@ -235,8 +235,7 @@ unsafe impl<T: ?Sized + Sync> Sync for MutexGuard<'_, T> {}
 #[must_not_suspend = "holding a MappedMutexGuard across suspend \
                       points can cause deadlocks, delays, \
                       and cause Futures to not implement `Send`"]
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 #[clippy::has_significant_drop]
 pub struct MappedMutexGuard<'a, T: ?Sized + 'a> {
     // NB: we use a pointer instead of `&'a mut T` to avoid `noalias` violations, because a
@@ -248,11 +247,9 @@ pub struct MappedMutexGuard<'a, T: ?Sized + 'a> {
     _variance: PhantomData<&'a mut T>,
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized> !Send for MappedMutexGuard<'_, T> {}
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 unsafe impl<T: ?Sized + Sync> Sync for MappedMutexGuard<'_, T> {}
 
 impl<T> Mutex<T> {
@@ -272,7 +269,7 @@ impl<T> Mutex<T> {
         Mutex { inner: sys::Mutex::new(), data: UnsafeCell::new(t) }
     }
 
-    // FIXME(connor): Add `lock_value_accessors` feature methods.
+    // FIXME: Add `lock_value_accessors` feature methods.
 }
 
 impl<T: ?Sized> Mutex<T> {
@@ -392,7 +389,7 @@ impl<T: ?Sized> Mutex<T> {
         self.data.get_mut()
     }
 
-    // FIXME(connor): Add `mutex_data_ptr` feature method.
+    // FIXME: Add `mutex_data_ptr` feature method.
 }
 
 #[unstable(feature = "nonpoison_mutex", issue = "134645")]
@@ -483,8 +480,7 @@ impl<'a, T: ?Sized> MutexGuard<'a, T> {
     /// This is an associated function that needs to be used as
     /// `MutexGuard::map(...)`. A method would interfere with methods of the
     /// same name on the contents of the `MutexGuard` used through `Deref`.
-    // #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-    #[unstable(feature = "nonpoison_mutex", issue = "134645")]
+    #[unstable(feature = "mapped_lock_guards", issue = "117108")]
     pub fn map<U, F>(orig: Self, f: F) -> MappedMutexGuard<'a, U>
     where
         F: FnOnce(&mut T) -> &mut U,
@@ -509,8 +505,7 @@ impl<'a, T: ?Sized> MutexGuard<'a, T> {
     /// `MutexGuard::try_map(...)`. A method would interfere with methods of the
     /// same name on the contents of the `MutexGuard` used through `Deref`.
     #[doc(alias = "filter_map")]
-    // #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-    #[unstable(feature = "nonpoison_mutex", issue = "134645")]
+    #[unstable(feature = "mapped_lock_guards", issue = "117108")]
     pub fn try_map<U, F>(orig: Self, f: F) -> Result<MappedMutexGuard<'a, U>, Self>
     where
         F: FnOnce(&mut T) -> Option<&mut U>,
@@ -531,8 +526,7 @@ impl<'a, T: ?Sized> MutexGuard<'a, T> {
     }
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized> Deref for MappedMutexGuard<'_, T> {
     type Target = T;
 
@@ -541,16 +535,14 @@ impl<T: ?Sized> Deref for MappedMutexGuard<'_, T> {
     }
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized> DerefMut for MappedMutexGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { self.data.as_mut() }
     }
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized> Drop for MappedMutexGuard<'_, T> {
     #[inline]
     fn drop(&mut self) {
@@ -560,16 +552,14 @@ impl<T: ?Sized> Drop for MappedMutexGuard<'_, T> {
     }
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized + fmt::Debug> fmt::Debug for MappedMutexGuard<'_, T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&**self, f)
     }
 }
 
-// #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-#[unstable(feature = "nonpoison_mutex", issue = "134645")]
+#[unstable(feature = "mapped_lock_guards", issue = "117108")]
 impl<T: ?Sized + fmt::Display> fmt::Display for MappedMutexGuard<'_, T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         (**self).fmt(f)
@@ -585,15 +575,14 @@ impl<'a, T: ?Sized> MappedMutexGuard<'a, T> {
     /// This is an associated function that needs to be used as
     /// `MappedMutexGuard::map(...)`. A method would interfere with methods of the
     /// same name on the contents of the `MutexGuard` used through `Deref`.
-    // #[unstable(feature = "mapped_lock_guards", issue = "117108")]
-    #[unstable(feature = "nonpoison_mutex", issue = "134645")]
+    #[unstable(feature = "mapped_lock_guards", issue = "117108")]
     pub fn map<U, F>(mut orig: Self, f: F) -> MappedMutexGuard<'a, U>
     where
         F: FnOnce(&mut T) -> &mut U,
         U: ?Sized,
     {
         // SAFETY: the conditions of `MutexGuard::new` were satisfied when the original guard
-        // was created, and have been upheld throughout `map` and/or `try_map`.
+        // was created, and have been upheld throughout `map` and/or `filter_map`.
         // The signature of the closure guarantees that it will not "leak" the lifetime of the reference
         // passed to it. If the closure panics, the guard will be dropped.
         let data = NonNull::from(f(unsafe { orig.data.as_mut() }));
@@ -608,17 +597,16 @@ impl<'a, T: ?Sized> MappedMutexGuard<'a, T> {
     /// The `Mutex` is already locked, so this cannot fail.
     ///
     /// This is an associated function that needs to be used as
-    /// `MappedMutexGuard::try_map(...)`. A method would interfere with methods of the
+    /// `MappedMutexGuard::filter_map(...)`. A method would interfere with methods of the
     /// same name on the contents of the `MutexGuard` used through `Deref`.
-    #[doc(alias = "filter_map")]
-    #[unstable(feature = "nonpoison_mutex", issue = "134645")]
-    pub fn try_map<U, F>(mut orig: Self, f: F) -> Result<MappedMutexGuard<'a, U>, Self>
+    #[unstable(feature = "mapped_lock_guards", issue = "117108")]
+    pub fn filter_map<U, F>(mut orig: Self, f: F) -> Result<MappedMutexGuard<'a, U>, Self>
     where
         F: FnOnce(&mut T) -> Option<&mut U>,
         U: ?Sized,
     {
         // SAFETY: the conditions of `MutexGuard::new` were satisfied when the original guard
-        // was created, and have been upheld throughout `map` and/or `try_map`.
+        // was created, and have been upheld throughout `map` and/or `filter_map`.
         // The signature of the closure guarantees that it will not "leak" the lifetime of the reference
         // passed to it. If the closure panics, the guard will be dropped.
         match f(unsafe { orig.data.as_mut() }) {
