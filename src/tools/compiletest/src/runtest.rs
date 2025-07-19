72f260dad4687f7d345c1ec51980c7909a68cdc8
Martin Nordholts <martin.nordholts@codetale.se>
2025-07-19 10:13:46 +0200

    TEMP: debug Windows .ll with bigger FileCheck input context

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -2050,7 +2050,7 @@ impl<'test> TestCx<'test> {
         filecheck.arg("--allow-unused-prefixes");
 
         // Provide more context on failures.
-        filecheck.args(&["--dump-input-context", "100"]);
+        filecheck.args(&["--dump-input-context", "1000000"]);
 
         // Add custom flags supplied by the `filecheck-flags:` test directive.
         filecheck.args(&self.props.filecheck_flags);
