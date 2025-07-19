38f810219695f9137b036faa6617d6583eaeb648
Maksim Bondarenkov <maksapple2306@gmail.com>
2025-07-19 09:46:20 +0300

    opt-dist: add an option for setting path to stage0 root
    in MSYS2 we have problems with stage0 for *-gnullvm hosts because prebuilt dist tarballs will be
    available starting from 1.90.0-beta. also this change helps to match bootstrap.toml config

--- src/tools/opt-dist/src/main.rs
+++ src/tools/opt-dist/src/main.rs
@@ -106,6 +106,10 @@ enum EnvironmentCmd {
         /// Set build artifacts dir. Must be the same as `build.build-dir` in bootstrap.toml
         #[arg(long, default_value = "build")]
         build_dir: Utf8PathBuf,
+
+        /// Path to custom stage0 root
+        #[arg(long)]
+        stage0_root: Option<Utf8PathBuf>,
     },
     /// Perform an optimized build on Linux CI, from inside Docker.
     LinuxCi {
@@ -143,6 +147,7 @@ fn create_environment(args: Args) -> anyhow::Result<(Environment, Vec<String>)> 
             run_tests,
             build_llvm,
             build_dir,
+            stage0_root,
         } => {
             let env = EnvironmentBuilder::default()
                 .host_tuple(target_triple)
@@ -159,6 +164,7 @@ fn create_environment(args: Args) -> anyhow::Result<(Environment, Vec<String>)> 
                 .run_tests(run_tests)
                 .fast_try_build(is_fast_try_build)
                 .build_llvm(build_llvm)
+                .stage0_root(stage0_root)
                 .build()?;
 
             (env, shared.build_args)
