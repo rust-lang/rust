e9fb744207b64ff60e7f3dcec3897d76131b165a
Nadrieril <nadrieril+git@gmail.com>
2025-07-20 14:41:27 +0200

    Add test

--- tests/ui/uninhabited/uninhabited-unstable-field.rs
+++ tests/ui/uninhabited/uninhabited-unstable-field.rs
@@ -1,11 +1,13 @@
 //@ aux-build: staged-api.rs
 //@ revisions: current exhaustive
-
-#![feature(exhaustive_patterns)]
+#![cfg_attr(exhaustive, feature(exhaustive_patterns))]
+#![feature(never_type)]
+#![feature(my_coro_state)] // Custom feature from `staged-api.rs`
+#![deny(unreachable_patterns)]
 
 extern crate staged_api;
 
-use staged_api::Foo;
+use staged_api::{Foo, MyCoroutineState};
 
 enum Void {}
 
@@ -23,7 +25,20 @@ fn demo2(x: Foo<Void>) {
 
 // Same as above, but for wildcard.
 fn demo3(x: Foo<Void>) {
-    match x { _ => {} }
+    match x {
+        _ => {}
+    }
+}
+
+fn unstable_enum(x: MyCoroutineState<i32, !>) {
+    match x {
+        //~^ ERROR non-exhaustive patterns
+        MyCoroutineState::Yielded(_) => {}
+    }
+    match x {
+        MyCoroutineState::Yielded(_) => {}
+        MyCoroutineState::Complete(_) => {}
+    }
 }
 
 fn main() {}
