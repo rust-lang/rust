8d311764b2985e08eb81e15c95789754f61783da
Camille GILLOT <gillot.camille@gmail.com>
2025-07-20 20:32:43 +0000

    Ignore parents when comparing raw identifier spans.

--- compiler/rustc_resolve/src/late.rs
+++ compiler/rustc_resolve/src/late.rs
@@ -2908,7 +2908,7 @@ impl<'a, 'ast, 'ra, 'tcx> LateResolutionVisitor<'a, 'ast, 'ra, 'tcx> {
                         .psess
                         .raw_identifier_spans
                         .iter()
-                        .any(|span| span == param.span());
+                        .any(|span| span == param.span().with_parent(None));
 
                     self.r
                         .dcx()
