b42f87c76e3bfbdfaaa464b370168094c998e803
yanglsh <yanglsh@shanghaitech.edu.cn>
2025-07-21 22:37:14 +0800

    fix: `missing_inline_in_public_items` fail to fulfill `expect` in `--test` build

--- clippy_lints/src/missing_inline.rs
+++ clippy_lints/src/missing_inline.rs
@@ -1,4 +1,5 @@
 use clippy_utils::diagnostics::{span_lint, span_lint_hir};
+use clippy_utils::fulfill_or_allowed;
 use rustc_hir::attrs::AttributeKind;
 use rustc_hir::def_id::DefId;
 use rustc_hir::{self as hir, Attribute, find_attr};
@@ -94,7 +95,14 @@ declare_lint_pass!(MissingInline => [MISSING_INLINE_IN_PUBLIC_ITEMS]);
 
 impl<'tcx> LateLintPass<'tcx> for MissingInline {
     fn check_item(&mut self, cx: &LateContext<'tcx>, it: &'tcx hir::Item<'_>) {
-        if it.span.in_external_macro(cx.sess().source_map()) || is_executable_or_proc_macro(cx) {
+        if it.span.in_external_macro(cx.sess().source_map()) {
+            return;
+        }
+
+        if is_executable_or_proc_macro(cx)
+            // Allow the lint if it is expected, when building with `--test`
+            && !(cx.sess().is_test_crate() && fulfill_or_allowed(cx, MISSING_INLINE_IN_PUBLIC_ITEMS, [it.hir_id()]))
+        {
             return;
         }
 
---
+++ tests/ui/missing_inline_test_crate.rs
@@ -0,0 +1,8 @@
+//@compile-flags: --test
+//@check-pass
+#![warn(clippy::missing_inline_in_public_items)]
+
+#[expect(clippy::missing_inline_in_public_items)]
+pub fn foo() -> u32 {
+    0
+}
