055483d468a6603064fad271b3436641ce5622af
Chris Denton <chris@chrisdenton.dev>
2025-07-22 18:10:24 +0000

    Update free-disk-space-windows.ps1

--- src/ci/scripts/free-disk-space-windows.ps1
+++ src/ci/scripts/free-disk-space-windows.ps1
@@ -6,28 +6,26 @@ Get-Volume | Out-String | Write-Output
 
 $available = $(Get-Volume C).SizeRemaining
 
-$dirs = 'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm',
+$dirs = 'Does not exit',
+'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\Llvm',
 'C:\rtools45', 'C:\ghcup', 'C:\Program Files (x86)\Android',
 'C:\Program Files\Google\Chrome', 'C:\Program Files (x86)\Microsoft\Edge',
 'C:\Program Files\Mozilla Firefox', 'C:\Program Files\MySQL', 'C:\Julia',
 'C:\Program Files\MongoDB', 'C:\Program Files\Azure Cosmos DB Emulator',
 'C:\Program Files\PostgreSQL', 'C:\Program Files\Unity Hub',
 'C:\Strawberry', 'C:\hostedtoolcache\windows\Java_Temurin-Hotspot_jdk',
-'C:\does not exist'
+'C:\does not exist', 'oh no'
 
 foreach ($dir in $dirs) {
-    Remove-Item -Recurse -Force -ErrorAction Continue $dir &
+    Start-ThreadJob { Remove-Item -Recurse -Force $dir }
 }
 
-# Wait for deletion to finish
-Get-Job -State Running | Wait-Job
-# Print any errors
-$warnings = Get-Job | Receive-Job -ErrorAction Continue
-foreach ($warning in $warnings) {
-    Write-Ouptut "::warning $warning"
+foreach ($job in Get-Job) {
+    Wait-Job $job
+    # Print if any, any.
+    Write-Output "::warning file=$PSCommandPath::$($job.Error)"
+    Remove-Job $job
 }
-# Cleanup finished jobs
-Get-Job | Remove-Job
 
 Get-Volume | Out-String | Write-Output
 
