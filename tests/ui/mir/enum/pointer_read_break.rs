34076b4648ff455586ca03c97f21bfeb49642cbc
Bastian Kersting <bkersting@google.com>
2025-07-23 09:45:56 +0000

    Extend the enum check to pointer and union reads
    This change extends the previously added enum discriminant check to
    enums read through a union or pointer. At the moment we only insert
    the check when transmuting to an enum. Although I hoped for it, this
    check isn't yet inserted for calls to `MaybeUninit::assume_init`,
    because the pass is running on polymorphic MIR and thus doesn't have
    the information yet to know whether the type that is read is an
    enum.

---
+++ tests/ui/mir/enum/pointer_read_break.rs
@@ -0,0 +1,18 @@
+//@ run-crash
+//@ compile-flags: -C debug-assertions
+//@ error-pattern: trying to construct an enum from an invalid value 0x1
+
+#[allow(dead_code)]
+#[repr(u16)]
+enum Single {
+    A,
+}
+
+fn main() {
+    let illegal_val: u16 = 1;
+    let illegal_val_ptr = &raw const illegal_val;
+    let foo: *const Single = unsafe { std::mem::transmute(illegal_val_ptr) };
+
+    let val: Single = unsafe { foo.read() };
+    println!("{}", val as u16);
+}
