e7992cf22ce62fdf3ea09d70bdba56d2042f2e05
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-07-24 03:06:28 +0200

    move tests in codegen to codegen-llvm

---
+++ tests/ui/explicit-tail-calls/recursion-no-tce.rs
@@ -0,0 +1,16 @@
+//@ run-fail
+
+use std::hint::black_box;
+
+pub fn count(curr: u64, top: u64) -> u64 {
+   if black_box(curr) >= top {
+        curr
+   } else {
+        count(curr + 1, top)
+   }
+}
+
+fn main() {
+    println!("{}",
+    count(0, black_box(1000000)));
+}
