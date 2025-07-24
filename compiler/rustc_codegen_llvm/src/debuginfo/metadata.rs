38aff5d5c962d7f12e6c32e421f000ddd2db2945
Walnut <39544927+Walnut356@users.noreply.github.com>
2025-07-24 04:06:33 -0500

    generate typedef nodes for ptr/ref and array

--- compiler/rustc_codegen_llvm/src/debuginfo/metadata.rs
+++ compiler/rustc_codegen_llvm/src/debuginfo/metadata.rs
@@ -131,6 +131,21 @@ fn build_fixed_size_array_di_node<'ll, 'tcx>(
         )
     };
 
+    if cpp_like_debuginfo(cx.tcx) {
+        let array_type_name = compute_debuginfo_type_name(cx.tcx, array_type, false);
+        di_node = unsafe {
+            llvm::LLVMRustDIBuilderCreateTypedef(
+                DIB(cx),
+                di_node,
+                array_type_name.as_c_char_ptr(),
+                array_type_name.len(),
+                unknown_file_metadata(cx),
+                UNKNOWN_LINE_NUMBER,
+                None,
+            )
+        };
+    }
+
     DINodeCreationResult::new(di_node, false)
 }
 
@@ -180,8 +195,19 @@ fn build_pointer_or_reference_di_node<'ll, 'tcx>(
                 pointer_align.abi,
                 &ptr_type_debuginfo_name,
             );
+            let typedefed_ptr = unsafe {
+                llvm::LLVMRustDIBuilderCreateTypedef(
+                    DIB(cx),
+                    di_node,
+                    ptr_type_debuginfo_name.as_c_char_ptr(),
+                    ptr_type_debuginfo_name.len(),
+                    unknown_file_metadata(cx),
+                    UNKNOWN_LINE_NUMBER,
+                    None,
+                )
+            };
 
-            DINodeCreationResult { di_node, already_stored_in_typemap: false }
+            DINodeCreationResult { di_node: typedefed_ptr, already_stored_in_typemap: false }
         }
         Some(wide_pointer_kind) => {
             type_map::build_type_with_children(
