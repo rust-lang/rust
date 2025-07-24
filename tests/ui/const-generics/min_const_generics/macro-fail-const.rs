272513868f0dbc76fc3d0f10adda86a013d51b5e
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-07-24 17:10:39 +0200

    Update ui tests with new macro early erroring

--- tests/ui/const-generics/min_const_generics/macro-fail-const.rs
+++ tests/ui/const-generics/min_const_generics/macro-fail-const.rs
@@ -1,24 +1,13 @@
-struct Example<const N: usize>;
-
-macro_rules! external_macro {
-  () => {{
-    //~^ ERROR expected type
-    const X: usize = 1337;
-    X
-  }}
-}
-
 trait Marker<const N: usize> {}
+struct Example<const N: usize>;
 impl<const N: usize> Marker<N> for Example<N> {}
 
 fn make_marker() -> impl Marker<gimme_a_const!(marker)> {
+  //~^ ERROR: type provided when a constant was expected
+  //~| ERROR: type provided when a constant was expected
   Example::<gimme_a_const!(marker)>
+  //~^ ERROR: type provided when a constant was expected
 }
-
-fn from_marker(_: impl Marker<{
-    #[macro_export]
-    macro_rules! inline { () => {{ 3 }} }; inline!()
-}>) {}
 
 fn main() {
   let _ok = Example::<{
@@ -27,12 +16,8 @@ fn main() {
       ($rusty: ident) => {{ let $rusty = 3; *&$rusty }}
       //~^ ERROR expected type
       //~| ERROR expected type
-    };
+    }
     gimme_a_const!(run)
   }>;
-
-  let _fail = Example::<external_macro!()>;
-
-  let _fail = Example::<gimme_a_const!()>;
-  //~^ ERROR: unexpected end of macro invocation
+  let _ok = Example::<{gimme_a_const!(marker)}>;
 }
