92fd27c0a392b63cf473eb36b83c9ee1596977f1
dianne <diannes.gm@gmail.com>
2025-07-25 10:03:11 -0700

    additional tests for `is` expressions' lexical scope
    Co-authored-by: Josh Triplett <josh@joshtriplett.org>

--- tests/ui/rfcs/rfc-3573-is/name-resolution-errors.rs
+++ tests/ui/rfcs/rfc-3573-is/name-resolution-errors.rs
@@ -11,17 +11,63 @@ fn main() {
         && x
     {
         x;
+    } else {
+        x; //~ ERROR cannot find value `x` in this scope
     }
     x; //~ ERROR cannot find value `x` in this scope
 
     // Elsewhere, `is`'s bindings only extend to the end of its `&&`-chain.
     if x //~ ERROR cannot find value `x` in this scope
-        && (is!(true is x) && x)
+        && (is!(true is x) && x && { x })
         && x //~ ERROR cannot find value `x` in this scope
         && (is!(true is x) || x) //~ ERROR cannot find value `x` in this scope
+        && (!is!(true is x) || x) //~ ERROR cannot find value `x` in this scope
+        && (is!(true is x) & x) //~ ERROR cannot find value `x` in this scope
     {
         x; //~ ERROR cannot find value `x` in this scope
     }
+
+    match is!(true is x) {
+        _ => { x; } //~ ERROR cannot find value `x` in this scope
+    }
+
+    match () {
+        () if is!(true is x) && x => { x; }
+        () if x => {} //~ ERROR cannot find value `x` in this scope
+    }
+
+    match () {
+        () if is!(true is x) && x => { x; }
+        () => { x; } //~ ERROR cannot find value `x` in this scope
+    }
+
+    fn f(_a: bool, _b: bool) {}
+    f(is!(true is x) && x, true);
+    f(is!(true is x), x); //~ ERROR cannot find value `x` in this scope
+    f(is!(true is x) && x, x); //~ ERROR cannot find value `x` in this scope
+
+    if !is!(true is x) {
+        x; //~ ERROR cannot find value `x` in this scope
+    } else {
+        x; //~ ERROR cannot find value `x` in this scope
+    }
+
+    if true
+        && if is!(true is x) && x { x } else { false }
+        && x //~ ERROR cannot find value `x` in this scope
+    {
+        x; //~ ERROR cannot find value `x` in this scope
+    }
+
+    while is!(true is x) {
+        x;
+        break;
+    }
+    x; //~ ERROR cannot find value `x` in this scope
+
+    is!(true is x);
+    x; //~ ERROR cannot find value `x` in this scope
+
     is!(true is x);
     x; //~ ERROR cannot find value `x` in this scope
 }
