a1e292a63a7ae8138114f1166f1aeb92061ea2bb
Weihang Lo <me@weihanglo.tw>
2025-07-25 14:44:13 -0400

    Update cargo
    ring is brought into Cargo transitively because Cargo now
    provides optional (unstable) reqwest HTTP backend.

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -135,6 +135,7 @@ const EXCEPTIONS_CARGO: ExceptionList = &[
     ("libz-rs-sys", "Zlib"),
     ("normalize-line-endings", "Apache-2.0"),
     ("openssl", "Apache-2.0"),
+    ("ring", "Apache-2.0 AND ISC"),
     ("ryu", "Apache-2.0 OR BSL-1.0"), // BSL is not acceptble, but we use it under Apache-2.0
     ("similar", "Apache-2.0"),
     ("sized-chunks", "MPL-2.0+"),
