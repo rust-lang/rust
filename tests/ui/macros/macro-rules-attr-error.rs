84915114e9aee2cc616fdaf47dd470d0a5325f28
Josh Triplett <josh@joshtriplett.org>
2025-07-28 03:00:24 -0700

    In error messages, don't assume attributes are always proc macros
    Now that `macro_rules` macros can define attribute rules, make sure
    error messages account for that.

---
+++ tests/ui/macros/macro-rules-attr-error.rs
@@ -0,0 +1,13 @@
+#![feature(macro_attr)]
+
+macro_rules! local_attr {
+    attr() { $($body:tt)* } => {
+        compile_error!(concat!("local_attr: ", stringify!($($body)*)));
+    };
+    //~^^ ERROR: local_attr
+}
+
+fn main() {
+    #[local_attr]
+    struct S;
+}
