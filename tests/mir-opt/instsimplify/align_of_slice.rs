8ef9233339c09d80e12b31c523082b94d9e09b44
Scott McMurray <scottmcm@users.noreply.github.com>
2025-07-27 23:39:06 -0700

    Simplify `align_of_val::<[T]>(…)` → `align_of::<T>()`

--- tests/mir-opt/instsimplify/align_of_slice.rs
+++ tests/mir-opt/instsimplify/align_of_slice.rs
@@ -7,7 +7,6 @@
 // EMIT_MIR align_of_slice.of_val_slice.InstSimplify-after-simplifycfg.diff
 pub fn of_val_slice<T>(slice: &[T]) -> usize {
     // CHECK-LABEL: fn of_val_slice(_1: &[T])
-    // CHECK: _2 = &raw const (*_1);
-    // CHECK: _0 = std::intrinsics::align_of_val::<[T]>(move _2)
+    // CHECK: _0 = AlignOf(T);
     unsafe { core::intrinsics::align_of_val(slice) }
 }
