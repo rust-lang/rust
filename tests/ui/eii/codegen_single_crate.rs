02656430fd22da55d845fd9419c806bfe274ea76
Jana Dönszelmann <jana@donsz.nl>
2025-07-28 10:35:42 +0200

    EII tests

---
+++ tests/ui/eii/codegen_single_crate.rs
@@ -0,0 +1,20 @@
+//@ run-pass
+//@ check-run-results
+// Tests whether calling EIIs works with the declaration in the same crate.
+#![feature(eii)]
+
+#[eii]
+fn hello(x: u64);
+
+#[hello]
+fn hello_impl(x: u64) {
+    println!("{x:?}")
+}
+
+// what you would write:
+fn main() {
+    // directly
+    hello_impl(21);
+    // through the alias
+    hello(42);
+}
---
+++ tests/ui/eii/duplicate/duplicate1.rs
@@ -0,0 +1,13 @@
+//@ no-prefer-dynamic
+//@ aux-build: impl1.rs
+//@ aux-build: impl2.rs
+// tests that EIIs error properly, even if the conflicting implementations live in another crate.
+#![feature(eii)]
+
+// has a span but in the other crate
+//~? ERROR multiple implementations of `#[eii1]`
+
+extern crate impl1;
+extern crate impl2;
+
+fn main() {}
