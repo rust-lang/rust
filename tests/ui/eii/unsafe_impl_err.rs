6fdb7f3e052d4160639435170073522bb0af081b
Jana Dönszelmann <jana@donsz.nl>
2025-07-28 12:46:03 +0200

    EII (builtin) macros in std

--- tests/ui/eii/unsafe_impl_err.rs
+++ tests/ui/eii/unsafe_impl_err.rs
@@ -5,11 +5,9 @@
 #![feature(rustc_attrs)]
 #![feature(eii_internals)]
 
-#[eii_macro_for(bar, "unsafe")]
-#[rustc_builtin_macro(eii_macro)]
-macro foo() {
-
-}
+#[eii_extern_target(bar, "unsafe")]
+#[rustc_builtin_macro(eii_shared_macro)]
+macro foo() {}
 
 unsafe extern "Rust" {
     safe fn bar(x: u64) -> u64;
--- tests/ui/eii/wrong_ty.rs
+++ tests/ui/eii/wrong_ty.rs
@@ -5,11 +5,9 @@
 #![feature(rustc_attrs)]
 #![feature(eii_internals)]
 
-#[eii_macro_for(bar)]
-#[rustc_builtin_macro(eii_macro)]
-macro foo() {
-
-}
+#[eii_extern_target(bar)]
+#[rustc_builtin_macro(eii_shared_macro)]
+macro foo() {}
 
 unsafe extern "Rust" {
     safe fn bar(x: u64) -> u64;
@@ -17,7 +15,7 @@ unsafe extern "Rust" {
 
 #[foo]
 fn other(x: usize) -> u64 {
-//~^ ERROR function `other` has a type that is incompatible with the declaration
+    //~^ ERROR function `other` has a type that is incompatible with the declaration
     3
 }
 
