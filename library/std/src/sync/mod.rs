2b38cb9ab7d866bbee7314a88fafd224e7e45470
Connor Tsui <connor.tsui20@gmail.com>
2025-07-29 11:10:17 +0200

    add experimental `oneshot` channel
    The `oneshot` channel is gated under the `oneshot_channel` feature.

--- library/std/src/sync/mod.rs
+++ library/std/src/sync/mod.rs
@@ -224,6 +224,8 @@ pub use self::poison::{MappedMutexGuard, MappedRwLockReadGuard, MappedRwLockWrit
 #[unstable(feature = "mpmc_channel", issue = "126840")]
 pub mod mpmc;
 pub mod mpsc;
+#[unstable(feature = "oneshot_channel", issue = "143674")]
+pub mod oneshot;
 
 #[unstable(feature = "sync_poison_mod", issue = "134646")]
 pub mod poison;
