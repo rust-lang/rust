ecf6d3c6ced41d71a09248fdc679309e39bae318
Trevor Gross <tmgross@umich.edu>
2025-07-30 09:45:53 -0500

    Simplify the configuration for no-panic
    Currently, attributes for `no-panic` are gated behind both the `test`
    config and `assert_no_panic`, because `no-panic` is a dev dependency (so
    only available with test configuration). However, we only emit
    `assert_no_panic` when the test config is also set anyway, so there
    isn't any need to gate on both.
    
    Replace gates on `all(test, assert_no_panic)` with only
    `assert_no_panic`. This is simpler, and also has the benefit that
    attempting to check for panics without `--test` errors.

--- library/compiler-builtins/libm/src/math/rem_pio2.rs
+++ library/compiler-builtins/libm/src/math/rem_pio2.rs
@@ -41,7 +41,7 @@ const PIO2_3T: f64 = 8.47842766036889956997e-32; /* 0x397B839A, 0x252049C1 */
 // use rem_pio2_large() for large x
 //
 // caller must handle the case when reduction is not needed: |x| ~<= pi/4 */
-#[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
+#[cfg_attr(assert_no_panic, no_panic::no_panic)]
 pub(crate) fn rem_pio2(x: f64) -> (i32, f64, f64) {
     let x1p24 = f64::from_bits(0x4170000000000000);
 
--- library/compiler-builtins/libm/src/math/rem_pio2_large.rs
+++ library/compiler-builtins/libm/src/math/rem_pio2_large.rs
@@ -221,7 +221,7 @@ const PIO2: [f64; 8] = [
 /// skip the part of the product that are known to be a huge integer (
 /// more accurately, = 0 mod 8 ). Thus the number of operations are
 /// independent of the exponent of the input.
-#[cfg_attr(all(test, assert_no_panic), no_panic::no_panic)]
+#[cfg_attr(assert_no_panic, no_panic::no_panic)]
 pub(crate) fn rem_pio2_large(x: &[f64], y: &mut [f64], e0: i32, prec: usize) -> i32 {
     // FIXME(rust-lang/rust#144518): Inline assembly would cause `no_panic` to fail
     // on the callers of this function. As a workaround, avoid inlining `floor` here
