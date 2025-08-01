7b13a509cc21dc49ae9c467038f04d9dd60994af
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-08-01 13:24:46 +0000

    Add a method to dump MIR in the middle of MIR building
    This makes it easier to debug issues with MIR building by inserting
    dump_for_debugging calls around the suspected code responsible for the
    bad MIR.

--- compiler/rustc_mir_build/src/builder/mod.rs
+++ compiler/rustc_mir_build/src/builder/mod.rs
@@ -789,6 +789,28 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
         builder
     }
 
+    #[allow(dead_code)]
+    fn dump_for_debugging(&self) {
+        let body = Body::new(
+            MirSource::item(self.def_id.to_def_id()),
+            self.cfg.basic_blocks.clone(),
+            self.source_scopes.clone(),
+            self.local_decls.clone(),
+            self.canonical_user_type_annotations.clone(),
+            self.arg_count.clone(),
+            self.var_debug_info.clone(),
+            self.fn_span.clone(),
+            self.coroutine.clone(),
+            None,
+        );
+        //body.coverage_info_hi = self.coverage_info.clone().map(|b| b.into_done());
+
+        use rustc_middle::mir::pretty;
+        let options = pretty::PrettyPrintMirOptions::from_cli(self.tcx);
+        pretty::write_mir_fn(self.tcx, &body, &mut |_, _| Ok(()), &mut std::io::stdout(), options)
+            .unwrap();
+    }
+
     fn finish(self) -> Body<'tcx> {
         let mut body = Body::new(
             MirSource::item(self.def_id.to_def_id()),
