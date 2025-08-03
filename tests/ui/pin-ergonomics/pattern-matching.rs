7a9bbfaea25cfce52b0381a532293309812e7cf6
Frank King <frankking1729@gmail.com>
2025-08-03 11:10:59 +0800

    Remove `!Unpin` related bounds

--- tests/ui/pin-ergonomics/pattern-matching.rs
+++ tests/ui/pin-ergonomics/pattern-matching.rs
@@ -8,25 +8,20 @@
 use std::pin::Pin;
 
 // This test verifies that a `&pin mut T` can be projected to a pinned
-// reference field `&pin mut T.U` when `T: !Unpin` or when `U: Unpin`.
+// reference field `&pin mut T.U`.
+// FIXME(pin_ergonomics): it is unsound to project `&pin mut T` to
+// `&pin mut T.U` when `U: !Unpin` but `T: Unpin`, or when there exists
+// `impl Drop for T` that takes a `&mut self` receiver.
 
 struct Foo<T, U> {
     x: T,
     y: U,
 }
 
-struct NegUnpinFoo<T, U> {
-    x: T,
-    y: U,
-}
-
-enum NegUnpinBar<T, U> {
+enum Bar<T, U> {
     Foo(T, U),
     Bar { x: T, y: U },
 }
-
-impl<T, U> !Unpin for NegUnpinFoo<T, U> {}
-impl<T, U> !Unpin for NegUnpinBar<T, U> {}
 
 trait IsPinMut {}
 trait IsPinConst {}
@@ -36,8 +31,7 @@ impl<T: ?Sized> IsPinConst for Pin<&T> {}
 fn assert_pin_mut<T: IsPinMut>(_: T) {}
 fn assert_pin_const<T: IsPinConst>(_: T) {}
 
-// Pinned references can be projected to pinned references of `Unpin` fields
-fn unpin2unpin<T: Unpin, U: Unpin>(foo_mut: Pin<&mut Foo<T, U>>, foo_const: Pin<&Foo<T, U>>) {
+fn foo<T: Unpin, U: Unpin>(foo_mut: Pin<&mut Foo<T, U>>, foo_const: Pin<&Foo<T, U>>) {
     let Foo { x, y } = foo_mut;
     //[normal]~^ ERROR mismatched types
     assert_pin_mut(x);
@@ -49,45 +43,14 @@ fn unpin2unpin<T: Unpin, U: Unpin>(foo_mut: Pin<&mut Foo<T, U>>, foo_const: Pin<
     assert_pin_const(y);
 }
 
-// Pinned references can be only projected to pinned references of `Unpin` fields
-fn unpin2partial_unpin<T: Unpin, U>(foo_mut: Pin<&mut Foo<T, U>>, foo_const: Pin<&Foo<T, U>>) {
-    let Foo { x, .. } = foo_mut;
-    //[normal]~^ ERROR mismatched types
-    assert_pin_mut(x);
-
-    let Foo { x, .. } = foo_const;
-    //[normal]~^ ERROR mismatched types
-    assert_pin_const(x);
-}
-
-// Pinned references of `!Unpin` types can be projected to pinned references
-fn neg_unpin2not_unpin<T, U>(
-    foo_mut: Pin<&mut NegUnpinFoo<T, U>>,
-    foo_const: Pin<&NegUnpinFoo<T, U>>,
-) {
-    let NegUnpinFoo { x, y } = foo_mut;
-    //[normal]~^ ERROR mismatched types
-    assert_pin_mut(x);
-    assert_pin_mut(y);
-
-    let NegUnpinFoo { x, y } = foo_const;
-    //[normal]~^ ERROR mismatched types
-    assert_pin_const(x);
-    assert_pin_const(y);
-}
-
-// Pinned references of `!Unpin` types can be projected to pinned references of `Unpin` fields
-fn neg_unpin2unpin<T: Unpin, U: Unpin>(
-    bar_mut: Pin<&mut NegUnpinBar<T, U>>,
-    bar_const: Pin<&NegUnpinBar<T, U>>,
-) {
+fn bar<T: Unpin, U: Unpin>(bar_mut: Pin<&mut Bar<T, U>>, bar_const: Pin<&Bar<T, U>>) {
     match bar_mut {
-        NegUnpinBar::Foo(x, y) => {
+        Bar::Foo(x, y) => {
             //[normal]~^ ERROR mismatched types
             assert_pin_mut(x);
             assert_pin_mut(y);
         }
-        _ if let NegUnpinBar::Bar { x, y } = bar_mut => {
+        _ if let Bar::Bar { x, y } = bar_mut => {
             //[normal]~^ ERROR mismatched types
             assert_pin_mut(x);
             assert_pin_mut(y);
@@ -95,12 +58,12 @@ fn neg_unpin2unpin<T: Unpin, U: Unpin>(
         _ => {}
     }
     match bar_const {
-        NegUnpinBar::Bar { x, y } => {
+        Bar::Bar { x, y } => {
             //[normal]~^ ERROR mismatched types
             assert_pin_const(x);
             assert_pin_const(y);
         }
-        _ if let NegUnpinBar::Foo(x, y) = bar_const => {
+        _ if let Bar::Foo(x, y) = bar_const => {
             //[normal]~^ ERROR mismatched types
             assert_pin_const(x);
             assert_pin_const(y);
@@ -109,45 +72,36 @@ fn neg_unpin2unpin<T: Unpin, U: Unpin>(
     }
 }
 
-fn neg_unpin_tuple<T, U>(
-    foo_mut: Pin<&mut (NegUnpinFoo<T, U>,)>,
-    foo_const: Pin<&(NegUnpinFoo<T, U>,)>,
-) {
-    let (NegUnpinFoo { x, y },) = foo_mut;
+fn pin_mut_tuple<T, U>(foo_mut: Pin<&mut (Foo<T, U>,)>, foo_const: Pin<&(Foo<T, U>,)>) {
+    let (Foo { x, y },) = foo_mut;
     //[normal]~^ ERROR mismatched types
     assert_pin_mut(x);
     assert_pin_mut(y);
-    let (NegUnpinFoo { x, y },) = foo_const;
+    let (Foo { x, y },) = foo_const;
     //[normal]~^ ERROR mismatched types
     assert_pin_const(x);
     assert_pin_const(y);
 }
 
-fn neg_unpin_array<T, U>(
-    foo_mut: Pin<&mut [NegUnpinFoo<T, U>; 1]>,
-    foo_const: Pin<&[NegUnpinFoo<T, U>; 1]>,
-) {
-    let [NegUnpinFoo { x, y }] = foo_mut;
-    //[normal]~^ ERROR expected an array or slice, found `Pin<&mut [NegUnpinFoo<T, U>; 1]>`
+fn pin_mut_array<T, U>(foo_mut: Pin<&mut [Foo<T, U>; 1]>, foo_const: Pin<&[Foo<T, U>; 1]>) {
+    let [Foo { x, y }] = foo_mut;
+    //[normal]~^ ERROR expected an array or slice, found `Pin<&mut [Foo<T, U>; 1]>`
     assert_pin_mut(x);
     assert_pin_mut(y);
-    let [NegUnpinFoo { x, y }] = foo_const;
-    //[normal]~^ ERROR expected an array or slice, found `Pin<&[NegUnpinFoo<T, U>; 1]>`
+    let [Foo { x, y }] = foo_const;
+    //[normal]~^ ERROR expected an array or slice, found `Pin<&[Foo<T, U>; 1]>`
     assert_pin_const(x);
     assert_pin_const(y);
 }
 
-fn neg_unpin_slice<T, U>(
-    foo_mut: Pin<&mut [NegUnpinFoo<T, U>]>,
-    foo_const: Pin<&[NegUnpinFoo<T, U>]>,
-) {
-    if let [NegUnpinFoo { x, y }] = foo_mut {
-        //[normal]~^ ERROR expected an array or slice, found `Pin<&mut [NegUnpinFoo<T, U>]>`
+fn pin_mut_slice<T, U>(foo_mut: Pin<&mut [Foo<T, U>]>, foo_const: Pin<&[Foo<T, U>]>) {
+    if let [Foo { x, y }] = foo_mut {
+        //[normal]~^ ERROR expected an array or slice, found `Pin<&mut [Foo<T, U>]>`
         assert_pin_mut(x);
         assert_pin_mut(y);
     }
-    if let [NegUnpinFoo { x, y }] = foo_const {
-        //[normal]~^ ERROR expected an array or slice, found `Pin<&[NegUnpinFoo<T, U>]>`
+    if let [Foo { x, y }] = foo_const {
+        //[normal]~^ ERROR expected an array or slice, found `Pin<&[Foo<T, U>]>`
         assert_pin_const(x);
         assert_pin_const(y);
     }
