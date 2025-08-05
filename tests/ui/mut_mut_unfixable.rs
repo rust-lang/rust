75b68606bc11529c2dd0e0f12dd317b74d40409c
Ada Alakbarova <ada.alakbarova@proton.me>
2025-08-05 14:12:41 +0200

    add an unfixable case
    as recommended in
    https://rust-lang.zulipchat.com/#narrow/channel/257328-clippy/topic/problem.20when.20suggestion.20only.20removes.20one.20.22level.22.20of.20a.20lint/near/532894687

--- tests/ui/mut_mut_unfixable.rs
+++ tests/ui/mut_mut_unfixable.rs
@@ -31,4 +31,12 @@ fn main() {
         //~^ mut_mut
         ***y + **x;
     }
+
+    if fun(x) {
+        // The lint will remove the extra `&mut`, but the result will still be a `&mut` of an expr
+        // of type `&mut _` (x), so the lint will fire again. That's because we've decided that
+        // doing both fixes in one run is not worth it, given how improbable code like this is.
+        let y = &mut &mut x;
+        //~^ mut_mut
+    }
 }
