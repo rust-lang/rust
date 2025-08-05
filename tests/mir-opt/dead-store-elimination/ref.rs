cd2784f8dfc9fcdffacd8c1795c09162d10c39ed
dianqk <dianqk@dianqk.net>
2025-08-05 21:17:01 +0800

    fixup! mir-opt: Eliminate dead ref statements
    Add a test case with the same local.
    
    ```
    // DBG: _1 = &_2
    _1 = &_3
    ```

--- tests/mir-opt/dead-store-elimination/ref.rs
+++ tests/mir-opt/dead-store-elimination/ref.rs
@@ -15,3 +15,17 @@ pub fn tuple(v: (i32, &Foo)) -> i32 {
     let _dead = &v.1.c;
     v.1.a
 }
+
+// EMIT_MIR ref.dead_first.DeadStoreElimination-initial.diff
+pub fn dead_first(v: &Foo) -> &i32 {
+    // CHECK-LABEL: fn dead_first
+    // CHECK: debug a => [[var_a:_[0-9]+]];
+    // CHECK: bb0:
+    // CHECK: DBG: [[var_a]] = &((*_1).2: i32)
+    // CHECK: [[tmp_4:_[0-9]+]] = &((*_1).0: i32)
+    // CHECK: [[tmp_3:_[0-9]+]] = &(*[[tmp_4]])
+    // CHECK: [[var_a]] = move [[tmp_3]]
+    let mut a = &v.c;
+    a = &v.a;
+    a
+}
