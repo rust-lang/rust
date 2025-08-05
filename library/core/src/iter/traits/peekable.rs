332bddfb892d3cf9a0587f488884044aae404b2c
Waleed Dahshan <waleedmast@gmail.com>
2025-08-05 18:23:41 +1000

    Implement new PeekableIterator for Iter

--- library/core/src/iter/traits/peekable.rs
+++ library/core/src/iter/traits/peekable.rs
@@ -1,34 +1,17 @@
 #[unstable(feature = "peekable_iterator", issue = "132973")]
 /// Iterators which inherently support `peek()` without needing to be wrapped by a `Peekable`.
 pub trait PeekableIterator: Iterator {
-    /// returns a reference to the `next()` value without advancing the iterator.
-    /// Just like `next()`, if there is a value, it returns a reference to it in Some()
-    /// if the iteration is finished, a `None` is returned
-    ///
-    /// # Examples
-    /// Basic usage:
-    /// ```
-    /// let xs = [1, 2, 3];
-    /// let mut iter = xs.iter();
-    ///
-    /// // peek() allows us to check the future value
-    /// assert_eq!(iter.peek(), Some(&&1));
-    /// assert_eq!(iter.next(), Some(&1));
-    ///
-    /// // peek() doesn't move the iterator forward
-    /// assert_eq!(iter.peek(), Some(&&2));
-    /// assert_eq!(iter.peek(), Some(&&2));
-    ///
-    /// ```
-    fn peek(&mut self) -> Option<&Self::Item>;
+    /// executes the closure with an Option containing `None` if the iterator is exhausted or Some(&Self::Item)
+    fn peek_with<T>(&mut self, func: impl for<'a> FnOnce(Option<&'a Self::Item>) -> T) -> T;
+
+    /// executes the closure on the next element without advancing the iterator, or returns None if the iterator is exhausted.
+    fn peek_map<T>(&mut self, func: impl for<'a> FnOnce(&'a Self::Item) -> T) -> Option<T> {
+        self.peek_with(|x| x.map(|y| func(y)))
+    }
 
     /// returns the `next()` element if a predicate holds true
     fn next_if(&mut self, func: impl FnOnce(&Self::Item) -> bool) -> Option<Self::Item> {
-        let Some(item) = self.peek() else {
-            return None;
-        };
-
-        if func(item) { self.next() } else { None }
+        self.peek_map(func).and_then(|_| self.next())
     }
 
     /// move forward and return the `next()` item if it is equal to the expected value
