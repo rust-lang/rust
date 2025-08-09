c503487a6f1402ed37051e435af0c410a3815b14
Josh Triplett <josh@joshtriplett.org>
2025-08-09 11:53:30 -0700

    Avoid abbreviating "numerator" as "numer", to allow catching typo "numer" elsewhere
    `typos.toml` has an exception for "numer", to avoid flagging its use as
    an abbreviation for "numerator". Remove the use of that abbrevation,
    spelling out "numerator" instead, and remove the exception, so that typo
    checks can find future instances of "numer" as a typo for "number".

--- library/std/src/sys_common/mod.rs
+++ library/std/src/sys_common/mod.rs
@@ -51,17 +51,16 @@ pub trait FromInner<Inner> {
     fn from_inner(inner: Inner) -> Self;
 }
 
-// Computes (value*numer)/denom without overflow, as long as both
-// (numer*denom) and the overall result fit into i64 (which is the case
-// for our time conversions).
+// Computes (value*numerator)/denom without overflow, as long as both (numerator*denom) and the
+// overall result fit into i64 (which is the case for our time conversions).
 #[allow(dead_code)] // not used on all platforms
-pub fn mul_div_u64(value: u64, numer: u64, denom: u64) -> u64 {
+pub fn mul_div_u64(value: u64, numerator: u64, denom: u64) -> u64 {
     let q = value / denom;
     let r = value % denom;
     // Decompose value as (value/denom*denom + value%denom),
-    // substitute into (value*numer)/denom and simplify.
-    // r < denom, so (denom*numer) is the upper bound of (r*numer)
-    q * numer + r * numer / denom
+    // substitute into (value*numerator)/denom and simplify.
+    // r < denom, so (denom*numerator) is the upper bound of (r*numerator)
+    q * numerator + r * numerator / denom
 }
 
 pub fn ignore_notfound<T>(result: crate::io::Result<T>) -> crate::io::Result<()> {
