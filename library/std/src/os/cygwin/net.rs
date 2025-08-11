3e98cc6bf8c58297f59af729454c6665425a86b7
王宇逸 <Strawberry_Str@hotmail.com>
2025-08-12 03:11:39 +0800

    Make "tcp_quickack" stable

--- library/std/src/os/cygwin/net.rs
+++ library/std/src/os/cygwin/net.rs
@@ -6,5 +6,5 @@
 pub use crate::os::net::linux_ext::addr::SocketAddrExt;
 #[unstable(feature = "unix_socket_ancillary_data", issue = "76915")]
 pub use crate::os::net::linux_ext::socket::UnixSocketExt;
-#[unstable(feature = "tcp_quickack", issue = "96256")]
+#[stable(feature = "tcp_quickack", since = "1.89.0")]
 pub use crate::os::net::linux_ext::tcp::TcpStreamExt;
