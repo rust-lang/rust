ebcbcc8b73cb6fa8aad42e258ca0382db68e7876
Cathal Mullan <contact@cathal.dev>
2025-08-13 13:18:06 +0100

    bootstrap: Fix jemalloc 64K page support for aarch64 tools

--- src/bootstrap/src/core/build_steps/compile.rs
+++ src/bootstrap/src/core/build_steps/compile.rs
@@ -1391,6 +1391,7 @@ pub fn rustc_cargo_env(builder: &Builder<'_>, cargo: &mut Cargo, target: TargetS
 
     // Build jemalloc on AArch64 with support for page sizes up to 64K
     // See: https://github.com/rust-lang/rust/pull/135081
+    // See also the "JEMALLOC_SYS_WITH_LG_PAGE" setting in the tool build step.
     if builder.config.jemalloc(target)
         && target.starts_with("aarch64")
         && env::var_os("JEMALLOC_SYS_WITH_LG_PAGE").is_none()
