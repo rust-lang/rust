11df77d15e3dc20757ec2ab717e4760167931656
Zachary S <zasample18+github@gmail.com>
2025-08-13 02:01:41 -0500

    Fix 5af8e4bcacd07c8e787da8924352bc9452dcc034 for rust-analyzer

--- src/tools/rust-analyzer/crates/hir-ty/src/mir/eval.rs
+++ src/tools/rust-analyzer/crates/hir-ty/src/mir/eval.rs
@@ -805,7 +805,7 @@ impl Evaluator<'_> {
                 ProjectionElem::Field(Either::Left(f)) => {
                     let layout = self.layout(&prev_ty)?;
                     let variant_layout = match &layout.variants {
-                        Variants::Single { .. } | Variants::Empty => &layout,
+                        Variants::Single { .. } | Variants::Empty { .. } => &layout,
                         Variants::Multiple { variants, .. } => {
                             &variants[match f.parent {
                                 hir_def::VariantId::EnumVariantId(it) => {
@@ -1628,8 +1628,8 @@ impl Evaluator<'_> {
             return Ok(0);
         };
         match &layout.variants {
-            Variants::Empty => unreachable!(),
-            Variants::Single { index } => {
+            Variants::Empty { .. } => unreachable!(),
+            Variants::Single { index, .. } => {
                 let r =
                     self.const_eval_discriminant(e.enum_variants(self.db).variants[index.0].0)?;
                 Ok(r)
@@ -1790,7 +1790,7 @@ impl Evaluator<'_> {
         }
         let layout = self.layout_adt(adt, subst)?;
         Ok(match &layout.variants {
-            Variants::Single { .. } | Variants::Empty => (layout.size.bytes_usize(), layout, None),
+            Variants::Single { .. } | Variants::Empty { .. } => (layout.size.bytes_usize(), layout, None),
             Variants::Multiple { variants, tag, tag_encoding, .. } => {
                 let enum_variant_id = match it {
                     VariantId::EnumVariantId(it) => it,
