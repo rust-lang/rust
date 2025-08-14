75b7d24a985ef9f6bab00414752dd74ceb47d114
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2025-08-14 12:38:35 +0200

    ci: clean windows disk space in background

--- src/ci/scripts/free-disk-space.sh
+++ src/ci/scripts/free-disk-space.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 script_dir=$(dirname "$0")
 
 if [[ "${RUNNER_OS:-}" == "Windows" ]]; then
-    pwsh $script_dir/free-disk-space-windows.ps1
+    python3 "$script_dir/free-disk-space-windows-start.py"
 else
     $script_dir/free-disk-space-linux.sh
 fi
