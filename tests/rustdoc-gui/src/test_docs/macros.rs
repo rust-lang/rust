add2aaf66702c96f0e158bd5a62b1272a74ad66e
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-08-15 15:19:57 +0200

    Add tests for attribute bang macros in rustdoc

--- tests/rustdoc-gui/src/test_docs/macros.rs
+++ tests/rustdoc-gui/src/test_docs/macros.rs
@@ -2,3 +2,10 @@
 macro_rules! a{ () => {}}
 #[macro_export]
 macro_rules! b{ () => {}}
+
+// An attribute bang macro.
+#[macro_export]
+macro_rules! attr_macro {
+    attr() () => {};
+    () => {};
+}
