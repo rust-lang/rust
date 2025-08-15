3de37a208ad7943e0a80b5ff22522d79ab62162b
Waffle Lapkin <waffle.lapkin@gmail.com>
2025-08-15 02:32:46 +0200

    remove `#![feature(never_type)]` from library

--- library/core/src/result.rs
+++ library/core/src/result.rs
@@ -1343,7 +1343,6 @@ impl<T, E> Result<T, E> {
     /// # Examples
     ///
     /// ```
-    /// # #![feature(never_type)]
     /// # #![feature(unwrap_infallible)]
     ///
     /// fn only_good_news() -> Result<String, !> {
@@ -1380,7 +1379,6 @@ impl<T, E> Result<T, E> {
     /// # Examples
     ///
     /// ```
-    /// # #![feature(never_type)]
     /// # #![feature(unwrap_infallible)]
     ///
     /// fn only_bad_news() -> Result<!, String> {
