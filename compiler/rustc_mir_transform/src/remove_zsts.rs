cad423ac4a4aa7531df36afa95c7a14203987e60
Camille Gillot <gillot.camille@gmail.com>
2025-08-15 22:14:02 +0000

    Perform RemoveZsts inside coroutines.

--- compiler/rustc_mir_transform/src/remove_zsts.rs
+++ compiler/rustc_mir_transform/src/remove_zsts.rs
@@ -12,11 +12,6 @@ impl<'tcx> crate::MirPass<'tcx> for RemoveZsts {
     }
 
     fn run_pass(&self, tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
-        // Avoid query cycles (coroutines require optimized MIR for layout).
-        if tcx.type_of(body.source.def_id()).instantiate_identity().is_coroutine() {
-            return;
-        }
-
         let typing_env = body.typing_env(tcx);
         let local_decls = &body.local_decls;
         let mut replacer = Replacer { tcx, typing_env, local_decls };
