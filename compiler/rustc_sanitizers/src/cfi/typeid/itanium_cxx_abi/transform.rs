09410d8b2d238d965dc6b46e061bdbcba1dcc8c6
Cameron Steffen <cam.steffen94@gmail.com>
2025-08-15 16:12:35 -0500

    Introduce trait_item_of

--- compiler/rustc_sanitizers/src/cfi/typeid/itanium_cxx_abi/transform.rs
+++ compiler/rustc_sanitizers/src/cfi/typeid/itanium_cxx_abi/transform.rs
@@ -469,8 +469,7 @@ fn implemented_method<'tcx>(
     let ancestor = if let Some(impl_id) = tcx.impl_of_assoc(instance.def_id()) {
         // Implementation in an `impl` block
         trait_ref = tcx.impl_trait_ref(impl_id)?;
-        let impl_method = tcx.associated_item(instance.def_id());
-        method_id = impl_method.trait_item_def_id?;
+        method_id = tcx.trait_item_of(instance.def_id())?;
         trait_method = tcx.associated_item(method_id);
         trait_id = trait_ref.skip_binder().def_id;
         impl_id
