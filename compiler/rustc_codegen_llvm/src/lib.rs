d429ed29c322b4512378da7f091a3a8846fa03e5
Deadbeef <ent3rm4n@gmail.com>
2025-08-16 22:08:41 +0800

    use declarative macro for `#[derive(TryFromU32)]`

--- compiler/rustc_codegen_llvm/src/lib.rs
+++ compiler/rustc_codegen_llvm/src/lib.rs
@@ -14,6 +14,7 @@
 #![feature(if_let_guard)]
 #![feature(impl_trait_in_assoc_type)]
 #![feature(iter_intersperse)]
+#![feature(macro_derive)]
 #![feature(rustdoc_internals)]
 #![feature(slice_as_array)]
 #![feature(try_blocks)]
@@ -73,6 +74,24 @@ mod va_arg;
 mod value;
 
 rustc_fluent_macro::fluent_messages! { "../messages.ftl" }
+
+macro_rules! TryFromU32 {
+    derive() ($(#[$meta:meta])* $vis:vis enum $Type:ident {$(
+        $(#[$varmeta:meta])*
+        $Variant:ident $(= $discr:expr)?
+    ),*$(,)?}) => {
+        impl ::core::convert::TryFrom<u32> for $Type {
+            type Error = u32;
+            #[allow(deprecated)] // Don't warn about deprecated variants.
+            fn try_from(value: u32) -> ::core::result::Result<$Type, Self::Error> {
+                $( if value == const { $Type::$Variant as u32 } { return Ok($Type::$Variant) } )*
+                Err(value)
+            }
+        }
+    }
+}
+
+pub(crate) use TryFromU32;
 
 #[derive(Clone)]
 pub struct LlvmCodegenBackend(());
