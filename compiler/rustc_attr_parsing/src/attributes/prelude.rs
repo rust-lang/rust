920a37cb1f1f796d5903294da254e5509790fc9b
Jana Dönszelmann <jana@donsz.nl>
2025-08-16 19:08:39 +0200

    split up context.rs

--- compiler/rustc_attr_parsing/src/attributes/prelude.rs
+++ compiler/rustc_attr_parsing/src/attributes/prelude.rs
@@ -1,23 +1,20 @@
 // parsing
+// templates
+pub(super) use rustc_feature::{AttributeTemplate, template};
+// data structures
+pub(super) use rustc_hir::attrs::AttributeKind;
+pub(super) use rustc_hir::lints::AttributeLintKind;
+pub(super) use rustc_hir::{MethodKind, Target};
+pub(super) use rustc_span::{DUMMY_SP, Ident, Span, Symbol, sym};
+pub(super) use thin_vec::ThinVec;
+
 pub(super) use crate::attributes::{
     AcceptMapping, AttributeOrder, AttributeParser, CombineAttributeParser, ConvertFn,
     NoArgsAttributeParser, OnDuplicate, SingleAttributeParser,
 };
-pub(super) use crate::parser::*;
-
 // contexts
 pub(super) use crate::context::{AcceptContext, FinalizeContext, Stage};
-
-// data structures
-pub(super) use rustc_hir::attrs::AttributeKind;
-pub(super) use rustc_hir::lints::AttributeLintKind;
-pub(super) use rustc_span::{DUMMY_SP, Ident, Span, Symbol, sym};
-pub(super) use thin_vec::ThinVec;
-
+pub(super) use crate::parser::*;
 // target checking
 pub(super) use crate::target_checking::Policy::{Allow, Error, Warn};
 pub(super) use crate::target_checking::{ALL_TARGETS, AllowedTargets};
-pub(super) use rustc_hir::{MethodKind, Target};
-
-// templates
-pub(super) use rustc_feature::{AttributeTemplate, template};
