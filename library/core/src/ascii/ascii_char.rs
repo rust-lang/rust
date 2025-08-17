88047428b6058a56fae8149af0f1c41ad4c6139e
Jules Bertholet <julesbertholet@quoi.xyz>
2025-08-17 14:10:19 -0400

    Add `impl AsMut<str> for AsciiChar`

--- library/core/src/ascii/ascii_char.rs
+++ library/core/src/ascii/ascii_char.rs
@@ -1177,6 +1177,18 @@ impl AsRef<str> for AsciiChar {
     }
 }
 
+#[unstable(feature = "ascii_char", issue = "110998")]
+impl AsMut<str> for AsciiChar {
+    #[inline(always)]
+    fn as_mut(&mut self) -> &mut str {
+        let ascii_ptr: *mut [Self] = crate::slice::from_mut(self);
+        let str_ptr = ascii_ptr as *mut str;
+        // SAFETY: Each ASCII codepoint in UTF-8 is encoded as one single-byte
+        // code unit having the same value as the ASCII byte.
+        unsafe { &mut *str_ptr }
+    }
+}
+
 impl [AsciiChar] {
     /// Views this slice of ASCII characters as a UTF-8 `str`.
     #[unstable(feature = "ascii_char", issue = "110998")]
