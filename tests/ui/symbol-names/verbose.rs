fa10fc4bcce70a30553eadc7335c2dd2e7be9ad1
Shunpoco <tkngsnsk313320@gmail.com>
2025-08-18 13:02:00 +0100

    Add -Awarnings flag on tests Those tests fail because wcrun on MacOS outputs. This is because they have -Zverbose-internals flag, Since it only happens on MacOS and we can't manage the 3rd party warning, ignoring the warning is better.
    Signed-off-by: Shunpoco <tkngsnsk313320@gmail.com>

--- tests/ui/symbol-names/verbose.rs
+++ tests/ui/symbol-names/verbose.rs
@@ -2,9 +2,10 @@
 // affected produced symbols making it impossible to link between crates
 // with a different value of the flag (for symbols involving generic
 // arguments equal to defaults of their respective parameters).
+// Add -Awarnings to ignore unexpected warnings on StdErr, especially from external tools.
 //
 //@ build-pass
-//@ compile-flags: -Zverbose-internals
+//@ compile-flags: -Zverbose-internals -Awarnings
 
 pub fn error(msg: String) -> Box<dyn std::error::Error> {
   msg.into()
--- tests/ui/type/issue-94187-verbose-type-name.rs
+++ tests/ui/type/issue-94187-verbose-type-name.rs
@@ -1,8 +1,9 @@
 // Ensure the output of `std::any::type_name` does not change based on `-Zverbose-internals`
+// Add -Awarnings to ignore unexpected warnings on StdErr, especially from external tools.
 //@ run-pass
 //@ edition: 2018
 //@ revisions: normal verbose
-//@ [verbose]compile-flags:-Zverbose-internals --verbose
+//@ [verbose]compile-flags:-Zverbose-internals --verbose -Awarnings
 
 use std::any::type_name;
 
