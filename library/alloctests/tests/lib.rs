23ea5dc8b87214aa33da0def7824e507eeee85e9
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2025-08-19 17:46:31 +0300

    Stabilize `char_max_len`

--- library/alloctests/tests/lib.rs
+++ library/alloctests/tests/lib.rs
@@ -4,7 +4,6 @@
 #![feature(assert_matches)]
 #![feature(btree_extract_if)]
 #![feature(wtf8_internals)]
-#![feature(char_max_len)]
 #![feature(cow_is_borrowed)]
 #![feature(core_intrinsics)]
 #![feature(downcast_unchecked)]
