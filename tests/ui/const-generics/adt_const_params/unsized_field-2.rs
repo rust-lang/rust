85dad03ac24088934906c84f84fec8d6a5281a79
tiif <pekyuan@gmail.com>
2025-08-19 12:25:42 +0000

    wip with boxy

--- tests/ui/const-generics/adt_const_params/unsized_field-2.rs
+++ tests/ui/const-generics/adt_const_params/unsized_field-2.rs
@@ -2,13 +2,13 @@
 #![feature(adt_const_params, unsized_const_params)]
 //~^ WARN: the feature `unsized_const_params` is incomplete
 
+
+// TODO: remove test
+
 extern crate unsized_const_param;
 
-#[derive(std::marker::ConstParamTy, Eq, PartialEq)]
-//~^ ERROR: the trait `ConstParamTy_` cannot be implemented for this type
-struct A(unsized_const_param::GenericNotUnsizedParam<&'static [u8]>);
 
-#[derive(std::marker::UnsizedConstParamTy, Eq, PartialEq)]
+#[derive(std::marker::ConstParamTy, Eq, PartialEq)]
 struct B(unsized_const_param::GenericNotUnsizedParam<&'static [u8]>);
 
 fn main() {}
