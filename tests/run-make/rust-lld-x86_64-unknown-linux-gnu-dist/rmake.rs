1a643205e3bcddedd897751f559a0b036a04fdf9
Rémy Rakic <remy.rakic+github@gmail.com>
2025-08-22 15:43:59 +0000

    adjust tests to leave lld in beta
    - stable tests to ensure lld is not used
    - splitting beta (where lld is on) behavior from stable (where it is
      off)

--- tests/run-make/rust-lld-x86_64-unknown-linux-gnu-dist/rmake.rs
+++ tests/run-make/rust-lld-x86_64-unknown-linux-gnu-dist/rmake.rs
@@ -1,7 +1,8 @@
-// Ensure that rust-lld is used as the default linker on `x86_64-unknown-linux-gnu`
-// dist artifacts and that it can also be turned off with a CLI flag.
+// Ensure that rust-lld is used as the default linker on `x86_64-unknown-linux-gnu` dist artifacts
+// (except on stable) and that it can also be turned off with a CLI flag.
 
 //@ only-dist
+//@ ignore-stable
 //@ only-x86_64-unknown-linux-gnu
 
 use run_make_support::linker::{assert_rustc_doesnt_use_lld, assert_rustc_uses_lld};
--- tests/run-make/rust-lld-x86_64-unknown-linux-gnu/rmake.rs
+++ tests/run-make/rust-lld-x86_64-unknown-linux-gnu/rmake.rs
@@ -1,11 +1,12 @@
-// Ensure that rust-lld is used as the default linker on `x86_64-unknown-linux-gnu`
-// and that it can also be turned off with a CLI flag.
+// Ensure that rust-lld is used as the default linker on `x86_64-unknown-linux-gnu` (except on
+// stable) and that it can also be turned off with a CLI flag.
 //
 // This version of the test checks that LLD is used by default when LLD is enabled in the
 // toolchain. There is a separate test that checks that LLD is used for dist artifacts
 // unconditionally.
 
 //@ needs-rust-lld
+//@ ignore-stable
 //@ only-x86_64-unknown-linux-gnu
 
 use run_make_support::linker::{assert_rustc_doesnt_use_lld, assert_rustc_uses_lld};
