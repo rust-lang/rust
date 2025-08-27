d4dc99bf0ee41fea141b3f5c7a0331f570c356ba
niacdoial <niac.notirl@gmail.com>
2025-08-27 22:30:22 +0200

    ImproperCTypes: change what type is blamed, use nested help messages
    A major change to the content of linting messages, but not where they
    occur.
    Now, the "uses type `_`" part of the message mentions the type directly
    visible where the error occurs, and the nested note/help messages trace
    the link to the actual source of the FFI-unsafety

--- tests/ui/extern/extern-C-str-arg-ice-80125.rs
+++ tests/ui/extern/extern-C-str-arg-ice-80125.rs
@@ -7,7 +7,7 @@ pub struct Struct(ExternCallback);
 
 #[no_mangle]
 pub extern "C" fn register_something(bind: ExternCallback) -> Struct {
-//~^ WARN `extern` fn uses type `str`, which is not FFI-safe
+// ^ FIXME: the error isn't seen here, but at least it's reported elsewhere
 //~^^ WARN `extern` fn uses type `Struct`, which is not FFI-safe
     Struct(bind)
 }
