c6078220ed71ef0630aad7217f4978f1d2d94f12
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2025-08-27 18:21:55 +0200

    fix oopsie

--- tests/ui/imports/import4.rs
+++ tests/ui/imports/import4.rs
@@ -1,3 +1,4 @@
-mod a { pub use crate::b::foo; } //~ ERROR unresolved import `crate::b`
+mod a { pub use crate::b::foo; } //~ ERROR unresolved import `crate::b::foo`
+mod b { pub use crate::a::foo; }
 
 fn main() { println!("loop"); }
