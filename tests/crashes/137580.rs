2c361f826756fa9c14e611c01b6f6717ecbd19f2
Takayuki Maeda <takoyaki0316@gmail.com>
2025-08-27 14:39:33 +0900

    remove old crash test

--- tests/crashes/137580.rs
+++
@@ -1,4 +0,0 @@
-//@ known-bug: #137580
-fn main() {
-    println!("%65536$", 1);
-}
--- tests/ui/macros/format-foreign-dollar-without-spec.rs
+++ tests/ui/macros/format-foreign-dollar-without-spec.rs
@@ -1,3 +1,4 @@
+// https://github.com/rust-lang/rust/issues/137580
 fn main() {
     println!("%65536$", 1);
     //~^ ERROR never used
