5b821b99f67bde7c061efab66244969fe38cf588
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2025-08-28 18:12:02 +0200

    disallow ambiguous names imported from external crates

--- tests/ui/imports/ambiguous-2.rs
+++ tests/ui/imports/ambiguous-2.rs
@@ -1,10 +1,9 @@
-//@ check-pass
 //@ aux-build: ../ambiguous-1.rs
 // https://github.com/rust-lang/rust/pull/113099#issuecomment-1633574396
 
 extern crate ambiguous_1;
 
 fn main() {
-    ambiguous_1::id();
+    ambiguous_1::id(); //~ ERROR cannot find function `id` in crate `ambiguous_1`
     //^ FIXME: `id` should be identified as an ambiguous item.
 }
--- tests/ui/imports/ambiguous-4.rs
+++ tests/ui/imports/ambiguous-4.rs
@@ -1,9 +1,8 @@
-//@ check-pass
 //@ aux-build: ../ambiguous-4-extern.rs
 
 extern crate ambiguous_4_extern;
 
 fn main() {
-    ambiguous_4_extern::id();
+    ambiguous_4_extern::id(); //~ ERROR cannot find function `id` in crate `ambiguous_4_extern`
     //^ FIXME: `id` should be identified as an ambiguous item.
 }
