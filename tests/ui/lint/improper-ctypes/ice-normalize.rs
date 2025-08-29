59de843f52624b7a6f4169e54521f982bdf9e9e6
niacdoial <niac.notirl@gmail.com>
2025-08-29 21:13:12 +0200

    ImproperCTypes: rename associated tests
    Rustc is trying to shift away from test names that are just issue
    numbers, so we add this as part of its effort, since this commit chain
    is moving and rewriting these files anyway.
    The directory containing all tests is also renamed.

--- tests/ui/lint/improper-ctypes/ice-normalize.rs
+++ tests/ui/lint/improper-ctypes/ice-normalize.rs
@@ -1,5 +1,6 @@
 //@ check-pass
 
+// Issue: https://github.com/rust-lang/rust/issues/113900
 // Extending `improper_ctypes` to check external-ABI fn-ptrs means that it can encounter
 // projections which cannot be normalized - unsurprisingly, this shouldn't crash the compiler.
 
