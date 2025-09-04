0a30c39c43d2e496d49461991b59e2e846d50b7a
Dawid Lachowicz <domdaw7@gmail.com>
2025-09-04 08:24:00 +0100

    Remove no longer used contract_checks intrinsic
    The contract_checks compiler flag is now used to determine
    if runtime contract checks should be enabled, as opposed
    to the compiler intrinsic as previously.

--- tests/ui/contracts/internal_machinery/contract-intrinsics.rs
+++ tests/ui/contracts/internal_machinery/contract-intrinsics.rs
@@ -1,29 +1,16 @@
-//@ revisions: default unchk_pass chk_pass chk_fail_ensures chk_fail_requires
+//@ revisions: default chk_fail_ensures chk_fail_requires
 //
 //@ [default] run-pass
-//@ [unchk_pass] run-pass
-//@ [chk_pass] run-pass
 //@ [chk_fail_requires] run-crash
 //@ [chk_fail_ensures] run-crash
-//
-//@ [unchk_pass] compile-flags: -Zcontract-checks=no
-//@ [chk_pass] compile-flags: -Zcontract-checks=yes
-//@ [chk_fail_requires] compile-flags: -Zcontract-checks=yes
-//@ [chk_fail_ensures] compile-flags: -Zcontract-checks=yes
-#![feature(cfg_contract_checks, contracts_internals, core_intrinsics)]
+#![feature(contracts_internals, core_intrinsics)]
 
 fn main() {
-    #[cfg(any(default, unchk_pass))] // default: disabled
-    assert_eq!(core::intrinsics::contract_checks(), false);
-
-    #[cfg(chk_pass)] // explicitly enabled
-    assert_eq!(core::intrinsics::contract_checks(), true);
-
     // always pass
     core::intrinsics::contract_check_requires(|| true);
 
     // always fail
-    #[cfg(any(chk_fail_requires))]
+    #[cfg(chk_fail_requires)]
     core::intrinsics::contract_check_requires(|| false);
 
     let doubles_to_two = { let old = 2; move |ret: &u32 | ret + ret == old };
@@ -31,6 +18,6 @@ fn main() {
     core::intrinsics::contract_check_ensures(Some(doubles_to_two), 1);
 
     // always fail
-    #[cfg(any(chk_fail_ensures))]
+    #[cfg(chk_fail_ensures)]
     core::intrinsics::contract_check_ensures(Some(doubles_to_two), 2);
 }
