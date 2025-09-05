719b14c3a73f84d1a1a64277bee37ce48f5bc929
Jieyou Xu <jieyouxu@outlook.com>
2025-09-05 21:24:45 +0800

    cg_clif: run `run-make-cargo` test suite

--- scripts/test_rustc_tests.sh
+++ scripts/test_rustc_tests.sh
@@ -166,5 +166,5 @@ index 073116933bd..c3e4578204d 100644
 EOF
 
 echo "[TEST] rustc test suite"
-./x.py test --stage 0 --test-args=--no-capture tests/{codegen-units,run-make,ui,incremental}
+./x.py test --stage 0 --test-args=--no-capture tests/{codegen-units,run-make,run-make-cargo,ui,incremental}
 popd
