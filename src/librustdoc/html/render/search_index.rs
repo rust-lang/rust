7ef932325b41dee6f59ef9d9beb72d244c3a0d1c
Benno Lossin <benno.lossin@proton.me>
2025-09-06 01:54:06 +0200

    rustdoc support for FRTs

--- src/librustdoc/html/render/search_index.rs
+++ src/librustdoc/html/render/search_index.rs
@@ -2077,6 +2077,7 @@ fn get_index_type_id(
         }
         // Not supported yet
         clean::Type::Pat(..)
+        | clean::Field(..)
         | clean::Generic(_)
         | clean::SelfTy
         | clean::ImplTrait(_)
