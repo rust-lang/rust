e802928376da0a8f1760c9510660df3c5fe046bb
Site Tester <sitetester2010@gmail.com>
2025-09-06 12:33:47 +0300

    Remove redundant comment

--- tests/ui/lint/issue-146164.rs
+++ tests/ui/lint/issue-146164.rs
@@ -3,7 +3,7 @@
 // that's already available through a glob re-export
 
 pub use std::option::*;
-use std::option::Option; // Should not warn - same item as glob import
+use std::option::Option;
 
 fn main() {
     let _x: Option<i32> = Some(42);
