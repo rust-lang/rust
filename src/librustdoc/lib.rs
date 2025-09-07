edc94e61841506c6957e1ee0ed57a2232fe339c4
cyrgani <ansgar.w.zielke@gmail.com>
2025-09-07 23:20:40 +0200

    mark `format_args_nl!` as `#[doc(hidden)]`

--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -10,7 +10,6 @@
 #![feature(box_patterns)]
 #![feature(debug_closure_helpers)]
 #![feature(file_buffered)]
-#![feature(format_args_nl)]
 #![feature(if_let_guard)]
 #![feature(iter_advance_by)]
 #![feature(iter_intersperse)]
