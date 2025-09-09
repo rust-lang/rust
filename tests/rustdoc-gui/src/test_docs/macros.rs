781b51190ce76ba581ea2e8ee937c046c2dee9ff
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-09-09 17:13:45 +0200

    Deduplicate "attributes" and "attribute macros" sections IDs

--- tests/rustdoc-gui/src/test_docs/macros.rs
+++ tests/rustdoc-gui/src/test_docs/macros.rs
@@ -3,7 +3,7 @@ macro_rules! a{ () => {}}
 #[macro_export]
 macro_rules! b{ () => {}}
 
-// An attribute bang macro.
+/// An attribute bang macro.
 #[macro_export]
 macro_rules! attr_macro {
     attr() () => {};
