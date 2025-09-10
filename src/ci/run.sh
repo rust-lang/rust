287e458daed03604799ba5e5493286388dc403cc
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-09-10 16:41:09 +0200

    Run rust tests with GCC backend

--- src/ci/run.sh
+++ src/ci/run.sh
@@ -188,7 +188,9 @@ else
   fi
 
   # Download GCC from CI on test builders
-  RUST_CONFIGURE_ARGS="$RUST_CONFIGURE_ARGS --set gcc.download-ci-gcc=true"
+  # FIXME: Temporarily commented out until the GCC build/download strategy is cleared out or
+  # made dependent on GCC submodule changes.
+  # RUST_CONFIGURE_ARGS="$RUST_CONFIGURE_ARGS --set gcc.download-ci-gcc=true"
 
   # download-rustc seems to be broken on CI after the stage0 redesign
   # Disable it until these issues are debugged and resolved
