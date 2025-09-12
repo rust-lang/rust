f2e1406f3b26f1aeca5ad91fdd7decbdf8ec3e0a
tiif <pekyuan@gmail.com>
2025-09-12 06:27:25 +0000

    Make the test case to be actually supported under adt_const_params

--- tests/ui/feature-gates/feature-gate-unsized-const-params.rs
+++ tests/ui/feature-gates/feature-gate-unsized-const-params.rs
@@ -1,4 +1,8 @@
-struct Foo<const N: [u8]>;
+#![feature(adt_const_params)]
+
+struct Bar(u8);
+
+struct Foo<const N: Bar>;
 //~^ ERROR: `[u8]` is forbidden as the type of a const generic parameter
 
 fn main() {}
