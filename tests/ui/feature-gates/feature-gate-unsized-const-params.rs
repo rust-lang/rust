652dcf493da804d7354c4c05c66a7a55274d263b
tiif <pekyuan@gmail.com>
2025-09-12 06:45:26 +0000

    Bring back the help message

--- tests/ui/feature-gates/feature-gate-unsized-const-params.rs
+++ tests/ui/feature-gates/feature-gate-unsized-const-params.rs
@@ -3,6 +3,6 @@
 struct Bar(u8);
 
 struct Foo<const N: Bar>;
-//~^ ERROR: `[u8]` is forbidden as the type of a const generic parameter
+//~^ ERROR: `Bar` must implement `ConstParamTy` to be used as the type of a const generic parameter
 
 fn main() {}
