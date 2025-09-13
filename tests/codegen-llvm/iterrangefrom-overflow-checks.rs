80e7831dbd5318ae74902f9f15673098a72ddb68
Peter Jaszkowiak <p.jaszkow@gmail.com>
2025-09-13 11:16:16 -0600

    fix test

--- tests/codegen-llvm/iterrangefrom-overflow-checks.rs
+++ tests/codegen-llvm/iterrangefrom-overflow-checks.rs
@@ -16,12 +16,12 @@ use std::range::{IterRangeFrom, RangeFrom};
 // CHECK-LABEL: @iterrangefrom_remainder(
 #[no_mangle]
 pub unsafe fn iterrangefrom_remainder(x: IterRangeFrom<i32>) -> RangeFrom<i32> {
-    //    DEBUG: i32 noundef %x.0
-    // NOCHECKS: i32 noundef returned %x.0
-    //    CHECK: i1 noundef zeroext %x.1
-    //    DEBUG: br i1
-    //    DEBUG: call core::panicking::panic_const::panic_const_add_overflow
-    //    DEBUG: unreachable
-    // NOCHECKS: ret i32 %x.0
+    //        DEBUG: i32 noundef %x
+    //     NOCHECKS: i32 noundef returned %x
+    //        DEBUG: br i1
+    //        DEBUG: call core::panicking::panic_const::panic_const_add_overflow
+    //        DEBUG: unreachable
+    // NOCHECKS-NOT: unreachable
+    //     NOCHECKS: ret i32 %x
     x.remainder()
 }
