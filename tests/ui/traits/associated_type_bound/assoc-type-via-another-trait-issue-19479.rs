05a5c7d0a104d3c3efbccb0aaf01b51495ad3080
omskscream <golovkov93@gmail.com>
2025-09-13 23:43:20 +0300

    clean up issue-19479 (assoc type from another trait)

--- tests/ui/traits/associated_type_bound/assoc-type-via-another-trait-issue-19479.rs
+++ tests/ui/traits/associated_type_bound/assoc-type-via-another-trait-issue-19479.rs
@@ -1,5 +1,11 @@
 //@ check-pass
 
+//! Tests that it's possible to define an associated type in a trait
+//! using an associated type from type parameter bound trait in a blanket implementation.
+//!
+//! # Context
+//! Original issue: https://github.com/rust-lang/rust/issues/19479
+
 trait Base {
     fn dummy(&self) { }
 }
