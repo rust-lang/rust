2a63fde0bcb1d6615dc245e8f6e6d7cad07857a7
Camille Gillot <gillot.camille@gmail.com>
2025-09-14 19:48:25 +0000

    Simplify condition graph.

--- tests/mir-opt/jump_threading.rs
+++ tests/mir-opt/jump_threading.rs
@@ -191,18 +191,22 @@ fn multiple_match(x: u8) -> u8 {
     mir! {
         {
             // CHECK: bb0: {
-            // CHECK:     switchInt(copy [[x:_.*]]) -> [3: bb9, otherwise: bb10];
+            // CHECK:     switchInt(copy [[x:_.*]]) -> [3: bb1, otherwise: bb2];
             match x { 3 => bb1, _ => bb2 }
         }
         bb1 = {
             // We know `x == 3`, so we can take `bb3`.
             // CHECK: bb1: {
+            // CHECK:     {{_.*}} = copy [[x]];
+            // CHECK:     goto -> bb3;
             let y = x;
             match y { 3 => bb3, _ => bb4 }
         }
         bb2 = {
             // We know `x != 3`, so we can take `bb6`.
             // CHECK: bb2: {
+            // CHECK:     [[z:_.*]] = copy [[x]];
+            // CHECK:     goto -> bb6;
             let z = x;
             match z { 3 => bb5, _ => bb6 }
         }
@@ -230,7 +234,7 @@ fn multiple_match(x: u8) -> u8 {
         bb6 = {
             // We know `z != 3`, so we CANNOT take `bb7`.
             // CHECK: bb6: {
-            // CHECK:     switchInt(copy [[z:_.*]]) -> [1: bb7, otherwise: bb8];
+            // CHECK:     switchInt(copy [[z]]) -> [1: bb7, otherwise: bb8];
             match z { 1 => bb7, _ => bb8 }
         }
         bb7 = {
@@ -247,14 +251,6 @@ fn multiple_match(x: u8) -> u8 {
             RET = 11;
             Return()
         }
-        // We know `x == 3`, so we can take `bb3`.
-        // CHECK: bb9: {
-        // CHECK:     {{_.*}} = copy [[x]];
-        // CHECK:     goto -> bb3;
-        // We know `x != 3`, so we can take `bb6`.
-        // CHECK: bb10: {
-        // CHECK:     [[z]] = copy [[x]];
-        // CHECK:     goto -> bb6;
     }
 }
 
@@ -273,14 +269,14 @@ fn duplicate_chain(x: bool) -> u8 {
         bb1 = {
             // CHECK: bb1: {
             // CHECK:     [[a:_.*]] = const 5_u8;
-            // CHECK:     goto -> bb7;
+            // CHECK:     goto -> bb3;
             a = 5;
             Goto(bb3)
         }
         bb2 = {
             // CHECK: bb2: {
             // CHECK:     [[a]] = const 5_u8;
-            // CHECK:     goto -> bb7;
+            // CHECK:     goto -> bb3;
             a = 5;
             Goto(bb3)
         }
@@ -294,7 +290,7 @@ fn duplicate_chain(x: bool) -> u8 {
         bb4 = {
             // CHECK: bb4: {
             // CHECK:     {{_.*}} = const 15_i32;
-            // CHECK:     switchInt(copy _2) -> [5: bb5, otherwise: bb6];
+            // CHECK:     goto -> bb5;
             let c = 15;
             match a { 5 => bb5, _ => bb6 }
         }
@@ -312,12 +308,6 @@ fn duplicate_chain(x: bool) -> u8 {
             RET = 9;
             Return()
         }
-        // CHECK: bb7: {
-        // CHECK-NEXT:     {{_.*}} = const 13_i32;
-        // CHECK-NEXT:     goto -> bb8;
-        // CHECK: bb8: {
-        // CHECK-NEXT:     {{_.*}} = const 15_i32;
-        // CHECK-NEXT:     goto -> bb5;
     }
 }
 
@@ -383,13 +373,13 @@ fn renumbered_bb(x: bool) -> u8 {
         let b: bool;
         {
             // CHECK: bb0: {
-            // CHECK:     switchInt({{.*}}) -> [1: bb8, otherwise: bb2];
+            // CHECK:     switchInt({{.*}}) -> [1: bb1, otherwise: bb2];
             b = false;
             match x { true => bb1, _ => bb2 }
         }
         bb1 = {
             // CHECK: bb1: {
-            // CHECK:     goto -> bb3;
+            // CHECK:     goto -> bb9;
             a = false;
             Goto(bb3)
         }
@@ -428,17 +418,14 @@ fn renumbered_bb(x: bool) -> u8 {
             RET = 11;
             Return()
         }
-        // Duplicate of bb1.
-        // CHECK: bb8: {
-        // CHECK:     goto -> bb10;
         // Duplicate of bb3.
-        // CHECK: bb9: {
+        // CHECK: bb8: {
         // CHECK:     switchInt(copy _2) -> [0: bb4, otherwise: bb5];
-        // Duplicate of bb9.
-        // CHECK: bb10: {
-        // CHECK:     goto -> bb11;
+        // Duplicate of bb8.
+        // CHECK: bb9: {
+        // CHECK:     goto -> bb10;
         // Duplicate of bb4.
-        // CHECK: bb11: {
+        // CHECK: bb10: {
         // CHECK:     goto -> bb6;
     }
 }
@@ -457,12 +444,12 @@ fn disappearing_bb(x: u8) -> u8 {
             // CHECK: bb0: {
             a = true;
             b = true;
-            // CHECK:     switchInt({{.*}}) -> [0: bb10, 1: bb10, 2: bb9, otherwise: bb2];
+            // CHECK:     switchInt({{.*}}) -> [0: bb3, 1: bb3, 2: bb1, otherwise: bb2];
             match x { 0 => bb3, 1 => bb3, 2 => bb1, _ => bb2 }
         }
         bb1 = {
             // CHECK: bb1: {
-            // CHECK:     goto -> bb4;
+            // CHECK:     goto -> bb10;
             b = false;
             Goto(bb4)
         }
@@ -473,7 +460,7 @@ fn disappearing_bb(x: u8) -> u8 {
         }
         bb3 = {
             // CHECK: bb3: {
-            // CHECK:     goto -> bb4;
+            // CHECK:     goto -> bb13;
             a = false;
             Goto(bb4)
         }
@@ -493,22 +480,18 @@ fn disappearing_bb(x: u8) -> u8 {
             Goto(bb6)
         }
         // CHECK: bb9: {
-        // CHECK:     goto -> bb12;
+        // CHECK:     switchInt(copy _3) -> [0: bb5, otherwise: bb7];
         // CHECK: bb10: {
-        // CHECK:     goto -> bb15;
+        // CHECK:     goto -> bb11;
         // CHECK: bb11: {
-        // CHECK:     switchInt(copy _3) -> [0: bb5, otherwise: bb7];
-        // CHECK: bb12: {
-        // CHECK:     goto -> bb13;
-        // CHECK: bb13: {
         // CHECK:     goto -> bb8;
-        // CHECK: bb14: {
+        // CHECK: bb12: {
         // CHECK:     switchInt(copy _3) -> [0: bb5, otherwise: bb7];
+        // CHECK: bb13: {
+        // CHECK:     goto -> bb14;
+        // CHECK: bb14: {
+        // CHECK:     goto -> bb15;
         // CHECK: bb15: {
-        // CHECK:     goto -> bb16;
-        // CHECK: bb16: {
-        // CHECK:     goto -> bb17;
-        // CHECK: bb17: {
         // CHECK:     goto -> bb6;
     }
 }
