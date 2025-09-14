266a6fbf755e07e468bd531eaa6838cac8f89186
Jeremy Smart <jeremy3141592@gmail.com>
2025-09-14 14:47:51 -0400

    add trait impls to proc_macro::Ident

--- library/proc_macro/src/lib.rs
+++ library/proc_macro/src/lib.rs
@@ -1032,7 +1032,7 @@ impl PartialEq<Punct> for char {
 }
 
 /// An identifier (`ident`).
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 #[stable(feature = "proc_macro_lib2", since = "1.29.0")]
 pub struct Ident(bridge::Ident<bridge::client::Span, bridge::client::Symbol>);
 
@@ -1097,10 +1097,7 @@ impl Ident {
 #[stable(feature = "proc_macro_lib2", since = "1.29.0")]
 impl fmt::Display for Ident {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.0.is_raw {
-            f.write_str("r#")?;
-        }
-        fmt::Display::fmt(&self.0.sym, f)
+        self.0.fmt(f)
     }
 }
 
@@ -1111,6 +1108,20 @@ impl fmt::Debug for Ident {
             .field("ident", &self.to_string())
             .field("span", &self.span())
             .finish()
+    }
+}
+
+#[stable(feature = "proc_macro_ident_impls", since = "CURRENT_RUSTC_VERSION")]
+impl PartialEq<String> for Ident {
+    fn eq(&self, other: &String) -> bool {
+        self.0 == *other
+    }
+}
+
+#[stable(feature = "proc_macro_ident_impls", since = "CURRENT_RUSTC_VERSION")]
+impl PartialEq<str> for Ident {
+    fn eq(&self, other: &str) -> bool {
+        self.0 == *other
     }
 }
 
