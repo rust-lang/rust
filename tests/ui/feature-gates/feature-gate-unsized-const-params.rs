7299e8fc4db506bb5ec3990e22b611c45df2f13d
tiif <pekyuan@gmail.com>
2025-09-15 14:15:29 +0000

    Fix feature gate tests

--- tests/ui/feature-gates/feature-gate-unsized-const-params.rs
+++ tests/ui/feature-gates/feature-gate-unsized-const-params.rs
@@ -1,8 +1,6 @@
-#![feature(adt_const_params)]
-
-struct Bar(u8);
-
-struct Foo<const N: Bar>;
-//~^ ERROR: `Bar` must implement `ConstParamTy` to be used as the type of a const generic parameter
+struct Foo<const N: [u8]>;
+//~^ ERROR: `[u8]` is forbidden as the type of a const generic parameter
+//~| HELP: add `#![feature(adt_const_params)]` to the crate
+//~| HELP: add `#![feature(unsized_const_params)]` to the crate
 
 fn main() {}
