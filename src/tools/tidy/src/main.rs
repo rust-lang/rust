b0010dd5aeb3b9837fdf92c121ebfdd1a07bce6f
Jakub Beránek <berykubik@gmail.com>
2025-09-15 13:16:25 +0200

    Add diagnostics context and migrate the `style` check to it

--- src/tools/tidy/src/main.rs
+++ src/tools/tidy/src/main.rs
@@ -9,9 +9,11 @@ use std::num::NonZeroUsize;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
 use std::thread::{self, ScopedJoinHandle, scope};
 use std::{env, process};
 
+use tidy::diagnostics::DiagCtx;
 use tidy::*;
 
 fn main() {
@@ -54,6 +56,8 @@ fn main() {
     let ci_info = CiInfo::new(&mut bad);
     let bad = std::sync::Arc::new(AtomicBool::new(bad));
 
+    let mut diag_ctx = DiagCtx::new(verbose);
+
     let drain_handles = |handles: &mut VecDeque<ScopedJoinHandle<'_, ()>>| {
         // poll all threads for completion before awaiting the oldest one
         for i in (0..handles.len()).rev() {
@@ -87,76 +91,73 @@ fn main() {
             (@ $p:ident, name=$name:expr $(, $args:expr)* ) => {
                 drain_handles(&mut handles);
 
+                let diag_ctx = diag_ctx.clone();
                 let handle = thread::Builder::new().name($name).spawn_scoped(s, || {
-                    let mut flag = false;
-                    $p::check($($args, )* &mut flag);
-                    if (flag) {
-                        bad.store(true, Ordering::Relaxed);
-                    }
+                    $p::check($($args, )* diag_ctx);
                 }).unwrap();
                 handles.push_back(handle);
             }
         }
 
-        check!(target_specific_tests, &tests_path);
+        // check!(target_specific_tests, &tests_path);
 
         // Checks that are done on the cargo workspace.
-        check!(deps, &root_path, &cargo, bless);
-        check!(extdeps, &root_path);
+        // check!(deps, &root_path, &cargo, bless);
+        // check!(extdeps, &root_path);
 
         // Checks over tests.
-        check!(tests_placement, &root_path);
-        check!(tests_revision_unpaired_stdout_stderr, &tests_path);
-        check!(debug_artifacts, &tests_path);
-        check!(ui_tests, &root_path, bless);
-        check!(mir_opt_tests, &tests_path, bless);
-        check!(rustdoc_gui_tests, &tests_path);
-        check!(rustdoc_css_themes, &librustdoc_path);
-        check!(rustdoc_templates, &librustdoc_path);
-        check!(rustdoc_json, &src_path, &ci_info);
-        check!(known_bug, &crashes_path);
-        check!(unknown_revision, &tests_path);
+        // check!(tests_placement, &root_path);
+        // check!(tests_revision_unpaired_stdout_stderr, &tests_path);
+        // check!(debug_artifacts, &tests_path);
+        // check!(ui_tests, &root_path, bless);
+        // check!(mir_opt_tests, &tests_path, bless);
+        // check!(rustdoc_gui_tests, &tests_path);
+        // check!(rustdoc_css_themes, &librustdoc_path);
+        // check!(rustdoc_templates, &librustdoc_path);
+        // check!(rustdoc_json, &src_path, &ci_info);
+        // check!(known_bug, &crashes_path);
+        // check!(unknown_revision, &tests_path);
 
         // Checks that only make sense for the compiler.
-        check!(error_codes, &root_path, &[&compiler_path, &librustdoc_path], verbose, &ci_info);
-        check!(fluent_alphabetical, &compiler_path, bless);
-        check!(fluent_period, &compiler_path);
-        check!(fluent_lowercase, &compiler_path);
-        check!(target_policy, &root_path);
-        check!(gcc_submodule, &root_path, &compiler_path);
+        // check!(error_codes, &root_path, &[&compiler_path, &librustdoc_path], verbose, &ci_info);
+        // check!(fluent_alphabetical, &compiler_path, bless);
+        // check!(fluent_period, &compiler_path);
+        // check!(fluent_lowercase, &compiler_path);
+        // check!(target_policy, &root_path);
+        // check!(gcc_submodule, &root_path, &compiler_path);
 
         // Checks that only make sense for the std libs.
-        check!(pal, &library_path);
+        // check!(pal, &library_path);
 
         // Checks that need to be done for both the compiler and std libraries.
-        check!(unit_tests, &src_path, false);
-        check!(unit_tests, &compiler_path, false);
-        check!(unit_tests, &library_path, true);
-
-        if bins::check_filesystem_support(&[&root_path], &output_directory) {
-            check!(bins, &root_path);
-        }
+        // check!(unit_tests, &src_path, false);
+        // check!(unit_tests, &compiler_path, false);
+        // check!(unit_tests, &library_path, true);
+        //
+        // if bins::check_filesystem_support(&[&root_path], &output_directory) {
+        //     check!(bins, &root_path);
+        // }
 
         check!(style, &src_path);
         check!(style, &tests_path);
         check!(style, &compiler_path);
         check!(style, &library_path);
 
-        check!(edition, &src_path);
-        check!(edition, &compiler_path);
-        check!(edition, &library_path);
-
-        check!(alphabetical, &root_manifest);
-        check!(alphabetical, &src_path);
-        check!(alphabetical, &tests_path);
-        check!(alphabetical, &compiler_path);
-        check!(alphabetical, &library_path);
-
-        check!(x_version, &root_path, &cargo);
-
-        check!(triagebot, &root_path);
-
-        check!(filenames, &root_path);
+        // check!(edition, &src_path);
+        // check!(edition, &compiler_path);
+        // check!(edition, &library_path);
+        //
+        // check!(alphabetical, &root_manifest);
+        // check!(alphabetical, &src_path);
+        // check!(alphabetical, &tests_path);
+        // check!(alphabetical, &compiler_path);
+        // check!(alphabetical, &library_path);
+        //
+        // check!(x_version, &root_path, &cargo);
+        //
+        // check!(triagebot, &root_path);
+        //
+        // check!(filenames, &root_path);
 
         let collected = {
             drain_handles(&mut handles);
@@ -175,24 +176,24 @@ fn main() {
             }
             r
         };
-        check!(unstable_book, &src_path, collected);
-
-        check!(
-            extra_checks,
-            &root_path,
-            &output_directory,
-            &ci_info,
-            &librustdoc_path,
-            &tools_path,
-            &npm,
-            &cargo,
-            bless,
-            extra_checks,
-            pos_args
-        );
+        // check!(unstable_book, &src_path, collected);
+        //
+        // check!(
+        //     extra_checks,
+        //     &root_path,
+        //     &output_directory,
+        //     &ci_info,
+        //     &librustdoc_path,
+        //     &tools_path,
+        //     &npm,
+        //     &cargo,
+        //     bless,
+        //     extra_checks,
+        //     pos_args
+        // );
     });
 
-    if bad.load(Ordering::Relaxed) {
+    if diag_ctx.into_conclusion() {
         eprintln!("some tidy checks failed");
         process::exit(1);
     }
