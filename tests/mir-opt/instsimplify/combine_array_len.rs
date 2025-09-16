de73af9ec47552dcade86e4ebc4edb8a68fddd2e
Camille Gillot <gillot.camille@gmail.com>
2025-09-16 22:44:35 +0000

    Add test.

--- tests/mir-opt/instsimplify/combine_array_len.rs
+++ tests/mir-opt/instsimplify/combine_array_len.rs
@@ -4,7 +4,16 @@
 // EMIT_MIR combine_array_len.norm2.InstSimplify-after-simplifycfg.diff
 fn norm2(x: [f32; 2]) -> f32 {
     // CHECK-LABEL: fn norm2(
-    // CHECK-NOT: Len(
+    // CHECK-NOT: PtrMetadata(
+    let a = x[0];
+    let b = x[1];
+    a * a + b * b
+}
+
+// EMIT_MIR combine_array_len.normN.InstSimplify-after-simplifycfg.diff
+fn normN<const N: usize>(x: [f32; N]) -> f32 {
+    // CHECK-LABEL: fn normN(
+    // CHECK-NOT: PtrMetadata(
     let a = x[0];
     let b = x[1];
     a * a + b * b
@@ -12,4 +21,5 @@ fn norm2(x: [f32; 2]) -> f32 {
 
 fn main() {
     assert_eq!(norm2([3.0, 4.0]), 5.0 * 5.0);
+    assert_eq!(normN([3.0, 4.0]), 5.0 * 5.0);
 }
