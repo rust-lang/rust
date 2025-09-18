0fd6f1113b7209d24d4954ef99165aba09ed27f7
Tomoaki Kobayashi <tomoaki.kobayashi.t3@alumni.tohoku.ac.jp>
2025-09-18 14:23:58 +0900

    Add test for unuseful span in type error in some format_args!() invocations

---
+++ tests/ui/errors/span-format_args-issue-140578.rs
@@ -0,0 +1,31 @@
+fn check_format_args() {
+  print!("{:?} {a} {a:?}", [], a = 1 + 1);
+  //~^ ERROR type annotations needed
+}
+
+fn check_format_args_nl() {
+  println!("{:?} {a} {a:?}", [], a = 1 + 1);
+  //~^ ERROR type annotations needed
+}
+
+fn check_multi1() {
+  println!("{:?} {:?} {a} {a:?}", [], [], a = 1 + 1);
+  //~^ ERROR type annotations needed
+}
+
+fn check_multi2() {
+  println!("{:?} {:?} {a} {a:?} {b:?}", [], [], a = 1 + 1, b = []);
+  //~^ ERROR type annotations needed
+}
+
+fn check_unformatted() {
+  println!(" //~ ERROR type annotations needed
+  {:?} {:?}
+{a}
+{a:?}",
+        [],
+ [],
+a = 1 + 1);
+}
+
+fn main() {}
