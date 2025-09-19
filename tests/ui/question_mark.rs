rust-lang/rust-clippy

fix: `question_mark` FP on variables used after

tests/ui/question_mark.rs
@@ -2,6 +2,7 @@
 #![allow(unreachable_code)]
 #![allow(dead_code)]
 #![allow(clippy::unnecessary_wraps)]
+#![allow(dropping_copy_types)]
 
 use std::sync::MutexGuard;
 
@@ -560,4 +561,18 @@ fn issue_13642(x: Option<i32>) -> Option<()> {
     };
 
     None
+}
+
+mod issue14894 {
+    struct CannotCopy {}
+
+    fn use_after_question_mark(do_something_else: impl FnOnce() -> Result<CannotCopy, ()>) -> Result<(), ()> {
+        let result = do_something_else();
+        if let Err(reason) = result {
+            return Err(reason);
+        }
+        drop(result);
+
+        Ok(())
+    }
 }

rust-lang/rust

tests/ui/question_mark.rs
@@ -583,3 +583,35 @@ fn issue_15679() -> Result<i32, String> {
 
     Ok(0)
 }
+
+mod issue14894 {
+    fn use_after_question_mark(do_something_else: impl Fn() -> Result<String, ()>) -> Result<(), ()> {
+        let result = do_something_else();
+        if let Err(reason) = result {
+            return Err(reason);
+        }
+        drop(result);
+
+        let result = do_something_else();
+        let x = match result {
+            //~^ question_mark
+            Ok(v) => v,
+            Err(e) => return Err(e),
+        };
+        drop(x);
+
+        Ok(())
+    }
+
+    #[expect(dropping_copy_types)]
+    fn use_after_question_mark_but_is_copy(do_something_else: impl Fn() -> Result<i32, ()>) -> Result<(), ()> {
+        let result = do_something_else();
+        if let Err(reason) = result {
+            //~^ question_mark
+            return Err(reason);
+        }
+        drop(result);
+
+        Ok(())
+    }
+}