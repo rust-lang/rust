3277a9ceca0e06201b1c7815573c36780d8bdb1e
Taiki Endo <te316e89@gmail.com>
2025-09-21 14:39:07 +0900

    rustc_target: Use +spe for powerpcspe targets

--- compiler/rustc_target/src/spec/targets/powerpc_unknown_linux_gnuspe.rs
+++ compiler/rustc_target/src/spec/targets/powerpc_unknown_linux_gnuspe.rs
@@ -25,7 +25,7 @@ pub(crate) fn target() -> Target {
         options: TargetOptions {
             cfg_abi: CfgAbi::Spe,
             endian: Endian::Big,
-            features: "+secure-plt,+msync".into(),
+            features: "+secure-plt,+msync,+spe".into(),
             mcount: "_mcount".into(),
             ..base
         },
