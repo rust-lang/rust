001b4f4a205cc593bb3b969b000c805d2cbc3913
Yonggang Luo <luoyonggang@gmail.com>
2025-09-22 21:39:25 +0800

    Avoid duplicate stable and unstable attributes between ffi/mod.rs and ffi/primitives.rs
    Split c_ptrdiff_t.md c_size_t.md c_ssize_t.md out of primitives.rs for consistence

--- library/core/src/ffi/primitives.rs
+++ library/core/src/ffi/primitives.rs
@@ -3,36 +3,25 @@
 //! This module is intentionally standalone to facilitate parsing when retrieving
 //! core C types.
 
-macro_rules! type_alias {
-    {
-      $Docfile:tt, $Alias:ident = $Real:ty;
-      $( $Cfg:tt )*
-    } => {
-        #[doc = include_str!($Docfile)]
-        $( $Cfg )*
-        #[stable(feature = "core_ffi_c", since = "1.64.0")]
-        pub type $Alias = $Real;
-    }
-}
+pub(super) type c_char = c_char_definition::c_char;
 
-type_alias! { "c_char.md", c_char = c_char_definition::c_char; #[doc(cfg(all()))] }
+pub(super) type c_schar = i8;
+pub(super) type c_uchar = u8;
 
-type_alias! { "c_schar.md", c_schar = i8; }
-type_alias! { "c_uchar.md", c_uchar = u8; }
-type_alias! { "c_short.md", c_short = i16; }
-type_alias! { "c_ushort.md", c_ushort = u16; }
+pub(super) type c_short = i16;
+pub(super) type c_ushort = u16;
 
-type_alias! { "c_int.md", c_int = c_int_definition::c_int; #[doc(cfg(all()))] }
-type_alias! { "c_uint.md", c_uint = c_int_definition::c_uint; #[doc(cfg(all()))] }
+pub(super) type c_int = c_int_definition::c_int;
+pub(super) type c_uint = c_int_definition::c_uint;
 
-type_alias! { "c_long.md", c_long = c_long_definition::c_long; #[doc(cfg(all()))] }
-type_alias! { "c_ulong.md", c_ulong = c_long_definition::c_ulong; #[doc(cfg(all()))] }
+pub(super) type c_long = c_long_definition::c_long;
+pub(super) type c_ulong = c_long_definition::c_ulong;
 
-type_alias! { "c_longlong.md", c_longlong = i64; }
-type_alias! { "c_ulonglong.md", c_ulonglong = u64; }
+pub(super) type c_longlong = i64;
+pub(super) type c_ulonglong = u64;
 
-type_alias! { "c_float.md", c_float = f32; }
-type_alias! { "c_double.md", c_double = f64; }
+pub(super) type c_float = f32;
+pub(super) type c_double = f64;
 
 mod c_char_definition {
     crate::cfg_select! {
@@ -150,26 +139,9 @@ mod c_long_definition {
     }
 }
 
-/// Equivalent to C's `size_t` type, from `stddef.h` (or `cstddef` for C++).
-///
-/// This type is currently always [`usize`], however in the future there may be
-/// platforms where this is not the case.
-#[unstable(feature = "c_size_t", issue = "88345")]
-pub type c_size_t = usize;
-
-/// Equivalent to C's `ptrdiff_t` type, from `stddef.h` (or `cstddef` for C++).
-///
-/// This type is currently always [`isize`], however in the future there may be
-/// platforms where this is not the case.
-#[unstable(feature = "c_size_t", issue = "88345")]
-pub type c_ptrdiff_t = isize;
-
-/// Equivalent to C's `ssize_t` (on POSIX) or `SSIZE_T` (on Windows) type.
-///
-/// This type is currently always [`isize`], however in the future there may be
-/// platforms where this is not the case.
-#[unstable(feature = "c_size_t", issue = "88345")]
-pub type c_ssize_t = isize;
+pub(super) type c_size_t = usize;
+pub(super) type c_ptrdiff_t = isize;
+pub(super) type c_ssize_t = isize;
 
 mod c_int_definition {
     crate::cfg_select! {
