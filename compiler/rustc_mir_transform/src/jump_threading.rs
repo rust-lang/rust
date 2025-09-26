d13a5b0a6c12837007c51f229cb8c58b614cfb5b
Camille Gillot <gillot.camille@gmail.com>
2025-09-26 20:44:06 +0000

    Handle self-loops too.

--- compiler/rustc_mir_transform/src/jump_threading.rs
+++ compiler/rustc_mir_transform/src/jump_threading.rs
@@ -842,9 +842,6 @@ fn maybe_loop_headers(body: &Body<'_>) -> DenseBitSet<BasicBlock> {
     let mut maybe_loop_headers = DenseBitSet::new_empty(body.basic_blocks.len());
     let mut visited = DenseBitSet::new_empty(body.basic_blocks.len());
     for (bb, bbdata) in traversal::postorder(body) {
-        let _new = visited.insert(bb);
-        debug_assert!(_new);
-
         // Post-order means we visit successors before the block for acyclic CFGs.
         // If the successor is not visited yet, consider it a loop header.
         for succ in bbdata.terminator().successors() {
@@ -852,6 +849,11 @@ fn maybe_loop_headers(body: &Body<'_>) -> DenseBitSet<BasicBlock> {
                 maybe_loop_headers.insert(succ);
             }
         }
+
+        // Only mark `bb` as visited after we checked the successors, in case we have a self-loop.
+        //     bb1: goto -> bb1;
+        let _new = visited.insert(bb);
+        debug_assert!(_new);
     }
 
     maybe_loop_headers
