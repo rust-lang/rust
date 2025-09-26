4f0462e0d4768304e2c7a1266b702d1e1369576b
Benno Lossin <benno.lossin@proton.me>
2025-09-26 15:04:09 +0200

    check for auto impls on field representing types

--- tests/ui/field_projections/send.rs
+++ tests/ui/field_projections/send.rs
@@ -1,6 +1,5 @@
 //@ revisions: old next
 //@ [next] compile-flags: -Znext-solver
-//@ run-pass
 #![feature(field_projections)]
 #![allow(incomplete_features)]
 use std::field::field_of;
@@ -12,10 +11,12 @@ struct Bar {
     bar_field: u32,
 }
 unsafe impl Send for field_of!(Bar, bar_field) {}
+//~^ ERROR: impls of auto traits for field representing types not supported
 
 fn is_send<T: Send>() {}
 
 fn main() {
     is_send::<field_of!(Bar, bar_field)>();
     is_send::<field_of!(Foo, field)>();
+    //~^ ERROR: `field_of!(Foo, field)` cannot be sent between threads safely [E0277]
 }
