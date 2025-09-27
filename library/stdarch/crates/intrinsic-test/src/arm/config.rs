62182b54662abfe929be98bdc1757505ced86434
Madhav Madhusoodanan <madhavmadhusoodanan@gmail.com>
2025-09-27 16:27:13 +0530

    fix: remove extra brackets for cast definition in arm/config.rs

--- library/stdarch/crates/intrinsic-test/src/arm/config.rs
+++ library/stdarch/crates/intrinsic-test/src/arm/config.rs
@@ -10,12 +10,12 @@ std::ostream& operator<<(std::ostream& os, float16_t value);
 #endif
 
 // T1 is the `To` type, T2 is the `From` type
-template<typename T1, typename T2> T1 cast(T2 x) {{
+template<typename T1, typename T2> T1 cast(T2 x) {
   static_assert(sizeof(T1) == sizeof(T2), "sizeof T1 and T2 must be the same");
-  T1 ret{{}};
+  T1 ret{};
   memcpy(&ret, &x, sizeof(T1));
   return ret;
-}}
+}
 "#;
 
 pub const POLY128_OSTREAM_DEF: &str = r#"
