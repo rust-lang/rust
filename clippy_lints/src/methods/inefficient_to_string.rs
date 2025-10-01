rust-lang/rust-clippy

Do not trigger `inefficient_to_string` after Rust 1.82
Starting with Rust version 1.82.0, the compiler generates similar code with
and without the `with_ref` cfg:

```rust
fn f(x: impl IntoIterator<Item = String>) {
    for y in x { println!("{y}"); }
}

fn main() {
    #[cfg(with_ref)]
    let a = ["foo", "bar"].iter().map(|&s| s.to_string());
    #[cfg(not(with_ref))]
    let a = ["foo", "bar"].iter().map(|s| s.to_string());
    f(a);
}
```

The generated code is strictly identical with `-O`, and identical modulo
some minor reordering without.

clippy_lints/src/methods/inefficient_to_string.rs
@@ -1,4 +1,5 @@
 use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::ty::{is_type_lang_item, peel_and_count_ty_refs};
 use rustc_errors::Applicability;
@@ -16,6 +17,7 @@ pub fn check(
     method_name: Symbol,
     receiver: &hir::Expr<'_>,
     args: &[hir::Expr<'_>],
+    msrv: Msrv,
 ) {
     if args.is_empty()
         && method_name == sym::to_string
@@ -26,6 +28,8 @@ pub fn check(
         && let self_ty = args.type_at(0)
         && let (deref_self_ty, deref_count, _) = peel_and_count_ty_refs(self_ty)
         && deref_count >= 1
+        // Since Rust 1.82, the specialized `ToString` is properly called
+        && !msrv.meets(cx, msrvs::SPECIALIZED_TO_STRING)
         && specializes_tostring(cx, deref_self_ty)
     {
         span_lint_and_then(

clippy_utils/src/msrvs.rs
@@ -28,7 +28,7 @@ msrv_aliases! {
     1,85,0 { UINT_FLOAT_MIDPOINT, CONST_SIZE_OF_VAL }
     1,84,0 { CONST_OPTION_AS_SLICE, MANUAL_DANGLING_PTR }
     1,83,0 { CONST_EXTERN_FN, CONST_FLOAT_BITS_CONV, CONST_FLOAT_CLASSIFY, CONST_MUT_REFS, CONST_UNWRAP }
-    1,82,0 { IS_NONE_OR, REPEAT_N, RAW_REF_OP }
+    1,82,0 { IS_NONE_OR, REPEAT_N, RAW_REF_OP, SPECIALIZED_TO_STRING }
     1,81,0 { LINT_REASONS_STABILIZATION, ERROR_IN_CORE, EXPLICIT_SELF_TYPE_ELISION, DURATION_ABS_DIFF }
     1,80,0 { BOX_INTO_ITER, LAZY_CELL }
     1,77,0 { C_STR_LITERALS }

rust-lang/rust

clippy_lints/src/methods/inefficient_to_string.rs
@@ -1,4 +1,5 @@
 use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::ty::{is_type_lang_item, peel_and_count_ty_refs};
 use rustc_errors::Applicability;
@@ -16,6 +17,7 @@ pub fn check(
     method_name: Symbol,
     receiver: &hir::Expr<'_>,
     args: &[hir::Expr<'_>],
+    msrv: Msrv,
 ) {
     if args.is_empty()
         && method_name == sym::to_string
@@ -26,6 +28,8 @@ pub fn check(
         && let self_ty = args.type_at(0)
         && let (deref_self_ty, deref_count, _) = peel_and_count_ty_refs(self_ty)
         && deref_count >= 1
+        // Since Rust 1.82, the specialized `ToString` is properly called
+        && !msrv.meets(cx, msrvs::SPECIALIZED_TO_STRING_FOR_REFS)
         && specializes_tostring(cx, deref_self_ty)
     {
         span_lint_and_then(

clippy_utils/src/msrvs.rs
@@ -28,7 +28,7 @@ msrv_aliases! {
     1,85,0 { UINT_FLOAT_MIDPOINT, CONST_SIZE_OF_VAL }
     1,84,0 { CONST_OPTION_AS_SLICE, MANUAL_DANGLING_PTR }
     1,83,0 { CONST_EXTERN_FN, CONST_FLOAT_BITS_CONV, CONST_FLOAT_CLASSIFY, CONST_MUT_REFS, CONST_UNWRAP }
-    1,82,0 { IS_NONE_OR, REPEAT_N, RAW_REF_OP }
+    1,82,0 { IS_NONE_OR, REPEAT_N, RAW_REF_OP, SPECIALIZED_TO_STRING_FOR_REFS }
     1,81,0 { LINT_REASONS_STABILIZATION, ERROR_IN_CORE, EXPLICIT_SELF_TYPE_ELISION, DURATION_ABS_DIFF }
     1,80,0 { BOX_INTO_ITER, LAZY_CELL }
     1,77,0 { C_STR_LITERALS }