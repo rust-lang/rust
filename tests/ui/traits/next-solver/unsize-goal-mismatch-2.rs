075142a5b5d7ec390c82888bf333999eb8457eee
Jana Dönszelmann <jana@donsz.nl>
2025-10-02 15:29:30 +0200

    prove both newly added tests are fixed by the changes

--- tests/ui/traits/next-solver/unsize-goal-mismatch-2.rs
+++ tests/ui/traits/next-solver/unsize-goal-mismatch-2.rs
@@ -1,13 +1,14 @@
-//@ check-pass
 //@ revisions: current next
 //@ ignore-compare-mode-next-solver (explicit revisions)
 //@[next] compile-flags: -Znext-solver
+//@[current] check-pass
 
 trait Super<T> {}
 trait Trait<T>: Super<T> + for<'hr> Super<&'hr ()> {}
 
 fn foo<'a>(x: Box<dyn Trait<&'a ()>>) -> Box<dyn Super<&'a ()>> {
     x
+    //[next]~^ ERROR type annotations needed: cannot satisfy `dyn Trait<&()>: Unsize<dyn Super<&()>>`
 }
 
 fn main() {}
