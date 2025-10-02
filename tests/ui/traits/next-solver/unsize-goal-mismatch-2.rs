3d9cb043dd10a1d189ac9bf35ee869c2fd5e3d48
Jana Dönszelmann <jana@donsz.nl>
2025-10-02 15:29:30 +0200

    prove both newly added tests are fixed by the changes

--- tests/ui/traits/next-solver/unsize-goal-mismatch-2.rs
+++ tests/ui/traits/next-solver/unsize-goal-mismatch-2.rs
@@ -1,7 +1,7 @@
-//@ check-pass
 //@ revisions: current next
 //@ ignore-compare-mode-next-solver (explicit revisions)
 //@[next] compile-flags: -Znext-solver
+//@[current] check-pass
 // Test from trait-system-refactor-initiative#241:
 // Used to ICE in mir typeck because of ambiguity in the new solver.
 // The wrong (first) trait bound was selected.
@@ -13,6 +13,7 @@ trait Trait<T>: Super<T> + for<'hr> Super<&'hr ()> {}
 
 fn foo<'a>(x: Box<dyn Trait<&'a ()>>) -> Box<dyn Super<&'a ()>> {
     x
+    //[next]~^ ERROR type annotations needed: cannot satisfy `dyn Trait<&()>: Unsize<dyn Super<&()>>`
 }
 
 fn main() {}
