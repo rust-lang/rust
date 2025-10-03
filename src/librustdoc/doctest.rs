e914a1a6e026d2c9db43300735a75ba25746e14c
Urgau <urgau@numericable.fr>
2025-10-03 11:57:04 +0200

    Respect `--error-format` in `rustdoc --test`

--- src/librustdoc/doctest.rs
+++ src/librustdoc/doctest.rs
@@ -174,6 +174,7 @@ pub(crate) fn run(dcx: DiagCtxtHandle<'_>, input: Input, options: RustdocOptions
         crate_name: options.crate_name.clone(),
         remap_path_prefix: options.remap_path_prefix.clone(),
         unstable_opts: options.unstable_opts.clone(),
+        error_format: options.error_format.clone(),
         ..config::Options::default()
     };
 
