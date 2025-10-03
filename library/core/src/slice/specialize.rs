115aeeebd222aafd3658eb7885d61d45d05b9151
The 8472 <git@infinite-source.de>
2025-10-03 13:43:11 +0200

    specialize slice::fill to use memset when possible
    LLVM generally can do this on its own, but it helps miri and other backends.

--- library/core/src/slice/specialize.rs
+++ library/core/src/slice/specialize.rs
@@ -15,9 +15,43 @@ impl<T: Clone> SpecFill<T> for [T] {
 }
 
 impl<T: Copy> SpecFill<T> for [T] {
-    fn spec_fill(&mut self, value: T) {
+    default fn spec_fill(&mut self, value: T) {
+        if size_of::<T>() == 1 {
+            // SAFETY: The pointer is derived from a reference, so it's writable.
+            // And we checked that T is 1 byte wide.
+            unsafe {
+                // use the intrinsic since it allows any T as long as it's 1 byte wide
+                crate::intrinsics::write_bytes(self.as_mut_ptr(), value, self.len());
+            }
+            return;
+        }
         for item in self.iter_mut() {
             *item = value;
         }
     }
 }
+
+macro spec_fill_int {
+    ($($type:ty)*) => {$(
+        impl SpecFill<$type> for [$type] {
+            #[inline]
+            fn spec_fill(&mut self, value: $type) {
+                if crate::intrinsics::is_val_statically_known(value) {
+                    let bytes = value.to_ne_bytes();
+                    if value == <$type>::from_ne_bytes([bytes[0]; size_of::<$type>()]) {
+                        // SAFETY: The pointer is derived from a reference, so it's writable.
+                        unsafe {
+                            crate::intrinsics::write_bytes(self.as_mut_ptr(), bytes[0], self.len());
+                        }
+                        return;
+                    }
+                }
+                for item in self.iter_mut() {
+                    *item = value;
+                }
+            }
+        }
+    )*}
+}
+
+spec_fill_int! { u16 i16 u32 i32 u64 i64 u128 i128 usize isize }
