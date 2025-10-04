456c0bb24b5eef9da02f4e072c99c0b5ec3fccdd
Camille Gillot <gillot.camille@gmail.com>
2025-10-04 17:49:24 +0000

    Perform DestinationPropagation on places.

--- tests/mir-opt/jump_threading.rs
+++ tests/mir-opt/jump_threading.rs
@@ -67,12 +67,11 @@ fn identity(x: Result<i32, i32>) -> Result<i32, i32> {
     // CHECK: bb5: {
     // CHECK:     switchInt(move _5) -> [0: bb2, 1: bb3, otherwise: bb1];
     // CHECK: bb6: {
-    // CHECK:     {{_.*}} = move (([[x]] as Err).0: i32);
-    // CHECK:     [[controlflow]] = ControlFlow::<Result<Infallible, i32>, i32>::Break(
+    // CHECK:     [[tmperr:_.*]] = Result::<Infallible, i32>::Err(copy (([[x]] as Err).0: i32));
+    // CHECK:     [[controlflow]] = ControlFlow::<Result<Infallible, i32>, i32>::Break(move [[tmperr]]);
     // CHECK:     goto -> bb8;
     // CHECK: bb7: {
-    // CHECK:     {{_.*}} = move (([[x]] as Ok).0: i32);
-    // CHECK:     [[controlflow]] = ControlFlow::<Result<Infallible, i32>, i32>::Continue(
+    // CHECK:     [[controlflow]] = ControlFlow::<Result<Infallible, i32>, i32>::Continue(copy (([[x]] as Ok).0: i32));
     // CHECK:     goto -> bb9;
     // CHECK: bb8: {
     // CHECK:     goto -> bb3;
