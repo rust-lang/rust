2071fc2bdb5ce464dde936d1c7c863f1b210acea
Ben Kimock <kimockb@gmail.com>
2025-10-04 23:35:19 -0400

    Make inliner cycle detection a fallible process

--- compiler/rustc_middle/src/query/mod.rs
+++ compiler/rustc_middle/src/query/mod.rs
@@ -1294,7 +1294,7 @@ rustc_queries! {
     }
 
     /// Return the set of (transitive) callees that may result in a recursive call to `key`.
-    query mir_callgraph_cyclic(key: LocalDefId) -> &'tcx UnordSet<LocalDefId> {
+    query mir_callgraph_cyclic(key: LocalDefId) -> &'tcx Option<UnordSet<LocalDefId>> {
         fatal_cycle
         arena_cache
         desc { |tcx|
