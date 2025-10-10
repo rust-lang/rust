137512379eef34a02cef8479a0ae5fe1f91ae5d4
Boxy Uwu <rust@boxyuwu.dev>
2025-10-10 18:03:53 +0100

    remove normalize call

--- compiler/rustc_hir_typeck/src/coercion.rs
+++ compiler/rustc_hir_typeck/src/coercion.rs
@@ -356,7 +356,7 @@ impl<'f, 'tcx> Coerce<'f, 'tcx> {
             let coercion = RegionVariableOrigin::Coercion(self.cause.span);
             let r_borrow = self.next_region_var(coercion);
             let autorefd_deref_ty = Ty::new_ref(self.tcx, r_borrow, deref_ty, mutbl_b);
-            
+
             // Note that we unify the autoref'd `Target` type with `b` rather than
             // the `Target` type with the pointee of `b`. This is necessary
             // to properly account for the differing variances of the pointees
@@ -804,9 +804,6 @@ impl<'f, 'tcx> Coerce<'f, 'tcx> {
         debug_assert!(self.shallow_resolve(a) == a);
         debug_assert!(self.shallow_resolve(b) == b);
 
-        let InferOk { value: b, mut obligations } =
-            self.at(&self.cause, self.param_env).normalize(b);
-
         match b.kind() {
             ty::FnPtr(_, b_hdr) => {
                 let mut a_sig = a.fn_sig(self.tcx);
@@ -838,9 +835,10 @@ impl<'f, 'tcx> Coerce<'f, 'tcx> {
                     }
                 }
 
-                let InferOk { value: a_sig, obligations: o1 } =
+                // FIXME: we shouldn't be normalizing here as coercion is inside of
+                // a probe. This can probably cause ICEs.
+                let InferOk { value: a_sig, mut obligations } =
                     self.at(&self.cause, self.param_env).normalize(a_sig);
-                obligations.extend(o1);
 
                 let InferOk { value, obligations: o2 } = self.coerce_from_safe_fn(
                     a_sig,
@@ -855,8 +853,8 @@ impl<'f, 'tcx> Coerce<'f, 'tcx> {
         }
     }
 
-    /// Attempts to coerce from the type of a non-capturing closure
-    /// into a function pointer.
+    /// Attempts to coerce from a closure to a function pointer. Fails
+    /// if the closure has any upvars.
     fn coerce_closure_to_fn(
         &self,
         a: Ty<'tcx>,
