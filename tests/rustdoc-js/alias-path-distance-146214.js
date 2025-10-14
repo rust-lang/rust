07072812ca00473176be7df5a0ed18517c9c0889
binarycat <binarycat@envs.net>
2025-10-14 15:16:11 -0500

    rustdoc: account for path distance in doc aliases

--- tests/rustdoc-js/alias-path-distance-146214.js
+++ tests/rustdoc-js/alias-path-distance-146214.js
@@ -3,7 +3,17 @@
 // consider path distance for doc aliases
 // regression test for <https://github.com/rust-lang/rust/issues/146214>
 
-const EXPECTED = {
-    'query': 'Foo::zzz',
-    'others': [{ 'path': 'alias_path_distance::Foo', 'name': 'baz' }],
-};
+const EXPECTED = [
+    {
+        'query': 'Foo::zzz',
+        'others': [
+            { 'path': 'alias_path_distance::Foo', 'name': 'baz' },
+        ],
+    },
+    {
+        'query': '"Foo::zzz"',
+        'others': [
+            { 'path': 'alias_path_distance::Foo', 'name': 'baz' },
+        ],
+    },
+];
