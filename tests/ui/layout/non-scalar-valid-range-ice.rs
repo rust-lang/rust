99488bf10a7050a523d69ea65c691c1d6b6499c2
Zalathar <Zalathar@users.noreply.github.com>
2025-10-16 16:29:21 +1100

    Regression test for 147761

---
+++ tests/ui/layout/non-scalar-valid-range-ice.rs
@@ -0,0 +1,19 @@
+//! Misused internal attributes should not be able to cause an ICE if
+//! `#![feature(rustc_attr)]` is not even enabled.
+//!
+//! Regression test for <https://github.com/rust-lang/rust/issues/147761>.
+
+//@ revisions: check build
+//@ compile-flags: --crate-type=lib
+//@ [check] check-fail
+//@ [build] build-fail
+//@ [build] dont-require-check-pass
+//@ [build] failure-status: 101
+
+fn array() {
+    //~v ERROR use of an internal attribute
+    #[rustc_layout_scalar_valid_range_start(1)]
+    struct NonZero<T>([T; 4]);
+    let nums = [1, 2, 3, 4];
+    let _ = unsafe { NonZero(nums) };
+}
