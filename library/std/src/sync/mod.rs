0758e191d518fc9dc6861e4c43ab6fc11bb9e678
Connor Tsui <connor.tsui20@gmail.com>
2025-10-17 11:02:47 -0400

    clean up some documentation
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- library/std/src/sync/mod.rs
+++ library/std/src/sync/mod.rs
@@ -142,7 +142,7 @@
 //!   most one thread at a time is able to access some data.
 //!
 //! - [`Once`]: Used for a thread-safe, one-time global initialization routine.
-//!   Mostly useful for implementing other types like `OnceLock`.
+//!   Mostly useful for implementing other types like [`OnceLock`].
 //!
 //! - [`OnceLock`]: Used for thread-safe, one-time initialization of a
 //!   variable, with potentially different initializers based on the caller.
