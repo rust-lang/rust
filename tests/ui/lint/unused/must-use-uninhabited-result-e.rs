d9fd1ced62b413155bbc10b491b298689dde811c
Waffle Lapkin <waffle.lapkin@gmail.com>
2025-10-18 19:39:30 +0200

    add a test for unused results with uninhabited `E`

---
+++ tests/ui/lint/unused/must-use-uninhabited-result-e.rs
@@ -0,0 +1,32 @@
+// This test checks for uninhabited error type in a result interacts with must
+// use lint. Ideally we don't want to lint on `Result<T, !>` as something that
+// must be used, because it's equivalent to `T`. However, we need to make sure
+// that if there are other reasons for the lint, we still emit it.
+//
+// See also: <https://github.com/rust-lang/rust/issues/65861>.
+//
+//@ aux-build:../../../pattern/usefulness/auxiliary/empty.rs
+//@ check-pass
+#![warn(unused)]
+#![feature(never_type)]
+
+extern crate empty;
+
+fn main() {
+    Ok::<_, std::convert::Infallible>(()); //~ warn: unused `Result` that must be used
+    Ok::<_, empty::EmptyForeignEnum>(()); //~ warn: unused `Result` that must be used
+    Ok::<_, empty::VisiblyUninhabitedForeignStruct>(()); //~ warn: unused `Result` that must be used
+    Ok::<_, empty::SecretlyUninhabitedForeignStruct>(()); //~ warn: unused `Result` that must be used
+    Ok::<_, !>(()); //~ warn: unused `Result` that must be used
+
+    Ok::<_, !>(Important); //~ warn: unused `Result` that must be used
+
+    very_important(); //~ warn: unused return value of `very_important` that must be used
+                      //~| warn: unused `Result` that must be used
+}
+
+#[must_use]
+struct Important;
+
+#[must_use]
+fn very_important() -> Result<(), !> { Ok(()) }
