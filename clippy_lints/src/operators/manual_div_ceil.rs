9ee9fd00d0ee8d3476d8f857a2042c62796a6bb2
Ada Alakbarova <ada.alakbarova@proton.me>
2025-10-19 23:57:41 +0200

    refactor(manual_div_ceil): move to under `operators/`

--- clippy_lints/src/operators/manual_div_ceil.rs
+++ clippy_lints/src/operators/manual_div_ceil.rs
@@ -1,4 +1,3 @@
-use clippy_config::Conf;
 use clippy_utils::diagnostics::span_lint_and_sugg;
 use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::sugg::{Sugg, has_enclosing_paren};
@@ -7,111 +6,69 @@ use rustc_ast::{BinOpKind, LitIntType, LitKind, UnOp};
 use rustc_data_structures::packed::Pu128;
 use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
-use rustc_lint::{LateContext, LateLintPass};
+use rustc_lint::LateContext;
 use rustc_middle::ty::{self};
-use rustc_session::impl_lint_pass;
 use rustc_span::source_map::Spanned;
 
-declare_clippy_lint! {
-    /// ### What it does
-    /// Checks for an expression like `(x + (y - 1)) / y` which is a common manual reimplementation
-    /// of `x.div_ceil(y)`.
-    ///
-    /// ### Why is this bad?
-    /// It's simpler, clearer and more readable.
-    ///
-    /// ### Example
-    /// ```no_run
-    /// let x: i32 = 7;
-    /// let y: i32 = 4;
-    /// let div = (x + (y - 1)) / y;
-    /// ```
-    /// Use instead:
-    /// ```no_run
-    /// #![feature(int_roundings)]
-    /// let x: i32 = 7;
-    /// let y: i32 = 4;
-    /// let div = x.div_ceil(y);
-    /// ```
-    #[clippy::version = "1.83.0"]
-    pub MANUAL_DIV_CEIL,
-    complexity,
-    "manually reimplementing `div_ceil`"
-}
+use super::MANUAL_DIV_CEIL;
 
-pub struct ManualDivCeil {
-    msrv: Msrv,
-}
+pub(super) fn check(cx: &LateContext<'_>, expr: &Expr<'_>, op: BinOpKind, lhs: &Expr<'_>, rhs: &Expr<'_>, msrv: Msrv) {
+    let mut applicability = Applicability::MachineApplicable;
 
-impl ManualDivCeil {
-    #[must_use]
-    pub fn new(conf: &'static Conf) -> Self {
-        Self { msrv: conf.msrv }
-    }
-}
-
-impl_lint_pass!(ManualDivCeil => [MANUAL_DIV_CEIL]);
-
-impl<'tcx> LateLintPass<'tcx> for ManualDivCeil {
-    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &Expr<'_>) {
-        let mut applicability = Applicability::MachineApplicable;
-
-        if let ExprKind::Binary(div_op, div_lhs, div_rhs) = expr.kind
-            && div_op.node == BinOpKind::Div
-            && check_int_ty_and_feature(cx, div_lhs)
-            && check_int_ty_and_feature(cx, div_rhs)
-            && let ExprKind::Binary(inner_op, inner_lhs, inner_rhs) = div_lhs.kind
-            && self.msrv.meets(cx, msrvs::DIV_CEIL)
+    if op == BinOpKind::Div
+        && check_int_ty_and_feature(cx, lhs)
+        && check_int_ty_and_feature(cx, rhs)
+        && let ExprKind::Binary(inner_op, inner_lhs, inner_rhs) = lhs.kind
+        && msrv.meets(cx, msrvs::DIV_CEIL)
+    {
+        // (x + (y - 1)) / y
+        if let ExprKind::Binary(sub_op, sub_lhs, sub_rhs) = inner_rhs.kind
+            && inner_op.node == BinOpKind::Add
+            && sub_op.node == BinOpKind::Sub
+            && check_literal(sub_rhs)
+            && check_eq_expr(cx, sub_lhs, rhs)
         {
-            // (x + (y - 1)) / y
-            if let ExprKind::Binary(sub_op, sub_lhs, sub_rhs) = inner_rhs.kind
-                && inner_op.node == BinOpKind::Add
-                && sub_op.node == BinOpKind::Sub
-                && check_literal(sub_rhs)
-                && check_eq_expr(cx, sub_lhs, div_rhs)
-            {
-                build_suggestion(cx, expr, inner_lhs, div_rhs, &mut applicability);
-                return;
-            }
+            build_suggestion(cx, expr, inner_lhs, rhs, &mut applicability);
+            return;
+        }
 
-            // ((y - 1) + x) / y
-            if let ExprKind::Binary(sub_op, sub_lhs, sub_rhs) = inner_lhs.kind
-                && inner_op.node == BinOpKind::Add
-                && sub_op.node == BinOpKind::Sub
-                && check_literal(sub_rhs)
-                && check_eq_expr(cx, sub_lhs, div_rhs)
-            {
-                build_suggestion(cx, expr, inner_rhs, div_rhs, &mut applicability);
-                return;
-            }
+        // ((y - 1) + x) / y
+        if let ExprKind::Binary(sub_op, sub_lhs, sub_rhs) = inner_lhs.kind
+            && inner_op.node == BinOpKind::Add
+            && sub_op.node == BinOpKind::Sub
+            && check_literal(sub_rhs)
+            && check_eq_expr(cx, sub_lhs, rhs)
+        {
+            build_suggestion(cx, expr, inner_rhs, rhs, &mut applicability);
+            return;
+        }
 
-            // (x + y - 1) / y
-            if let ExprKind::Binary(add_op, add_lhs, add_rhs) = inner_lhs.kind
-                && inner_op.node == BinOpKind::Sub
-                && add_op.node == BinOpKind::Add
-                && check_literal(inner_rhs)
-                && check_eq_expr(cx, add_rhs, div_rhs)
-            {
-                build_suggestion(cx, expr, add_lhs, div_rhs, &mut applicability);
-            }
+        // (x + y - 1) / y
+        if let ExprKind::Binary(add_op, add_lhs, add_rhs) = inner_lhs.kind
+            && inner_op.node == BinOpKind::Sub
+            && add_op.node == BinOpKind::Add
+            && check_literal(inner_rhs)
+            && check_eq_expr(cx, add_rhs, rhs)
+        {
+            build_suggestion(cx, expr, add_lhs, rhs, &mut applicability);
+        }
 
-            // (x + (Y - 1)) / Y
-            if inner_op.node == BinOpKind::Add && differ_by_one(inner_rhs, div_rhs) {
-                build_suggestion(cx, expr, inner_lhs, div_rhs, &mut applicability);
-            }
+        // (x + (Y - 1)) / Y
+        if inner_op.node == BinOpKind::Add && differ_by_one(inner_rhs, rhs) {
+            build_suggestion(cx, expr, inner_lhs, rhs, &mut applicability);
+        }
 
-            // ((Y - 1) + x) / Y
-            if inner_op.node == BinOpKind::Add && differ_by_one(inner_lhs, div_rhs) {
-                build_suggestion(cx, expr, inner_rhs, div_rhs, &mut applicability);
-            }
+        // ((Y - 1) + x) / Y
+        if inner_op.node == BinOpKind::Add && differ_by_one(inner_lhs, rhs) {
+            build_suggestion(cx, expr, inner_rhs, rhs, &mut applicability);
+        }
 
-            // (x - (-Y - 1)) / Y
-            if inner_op.node == BinOpKind::Sub
-                && let ExprKind::Unary(UnOp::Neg, abs_div_rhs) = div_rhs.kind
-                && differ_by_one(abs_div_rhs, inner_rhs)
-            {
-                build_suggestion(cx, expr, inner_lhs, div_rhs, &mut applicability);
-            }
+        // (x - (-Y - 1)) / Y
+        if inner_op.node == BinOpKind::Sub
+            && let ExprKind::Unary(UnOp::Neg, abs_div_rhs) = rhs.kind
+            && differ_by_one(abs_div_rhs, inner_rhs)
+        {
+            build_suggestion(cx, expr, inner_lhs, rhs, &mut applicability);
         }
     }
 }
