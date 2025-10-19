ff33ae8509a27c5ada3273b8f1889d0130879618
Maja Kądziołka <maya@compilercrim.es>
2025-10-19 16:13:50 +0200

    Add more variations from the PR thread

--- tests/ui/closures/2229_closure_analysis/deref-mut-in-pattern.rs
+++ tests/ui/closures/2229_closure_analysis/deref-mut-in-pattern.rs
@@ -4,6 +4,7 @@
 //@ check-pass
 #![allow(unused_assignments)]
 
+// Reading the length as part of a pattern captures the pointee.
 fn f() {
     let mut x: &mut [u8] = &mut [1, 2, 3];
     let c = || {
@@ -16,6 +17,7 @@ fn f() {
     c();
 }
 
+// Plain old deref as part of pattern behaves similarly
 fn g() {
     let mut x: &mut bool = &mut false;
     let mut t = true;
@@ -27,6 +29,23 @@ fn g() {
     };
     x = &mut t;
     c();
+}
+
+// Like f, but the lifetime implications are expressed in terms of
+// returning a closure.
+fn f2<'l: 's, 's>(x: &'s mut &'l [u8]) -> impl Fn() + 'l {
+    || match *x {
+        &[] => (),
+        _ => (),
+    }
+}
+
+// Related testcase that was already accepted before
+fn f3<'l: 's, 's>(x: &'s mut &'l [u8]) -> impl Fn() + 'l {
+    || match **x {
+        [] => (),
+        _ => (),
+    }
 }
 
 fn main() {
