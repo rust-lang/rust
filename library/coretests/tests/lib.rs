0e6131c9aad0668f08a4a08672c74ac140feae13
Karl Meakin <karl.meakin@arm.com>
2025-10-20 00:17:09 +0100

    refactor: make `unicode_data` tests normal tests
    Instead of generating a standalone executable to test `unicode_data`,
    generate normal tests in `coretests`. This ensures tests are always
    generated, and will be run as part of the normal testsuite.
    
    Also change the generated tests to loop over lookup tables, rather than
    generating a separate `assert_eq!()` statement for every codepoint. The
    old approach produced a massive (20,000 lines plus) file which took
    minutes to compile!

--- library/coretests/tests/lib.rs
+++ library/coretests/tests/lib.rs
@@ -111,6 +111,7 @@
 #![feature(try_find)]
 #![feature(try_trait_v2)]
 #![feature(uint_bit_width)]
+#![feature(unicode_internals)]
 #![feature(unsize)]
 #![feature(unwrap_infallible)]
 // tidy-alphabetical-end
