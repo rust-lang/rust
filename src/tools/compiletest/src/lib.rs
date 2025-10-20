84861fa7653cfd11c850bf06cc01daa9d5f45124
Zalathar <Zalathar@users.noreply.github.com>
2025-10-20 14:12:23 +1100

    Skip up-to-date checking for tests that are already ignored

--- src/tools/compiletest/src/lib.rs
+++ src/tools/compiletest/src/lib.rs
@@ -907,7 +907,10 @@ fn make_test(cx: &TestCollectorCx, collector: &mut TestCollector, testpaths: &Te
 
         // If a test's inputs haven't changed since the last time it ran,
         // mark it as ignored so that the executor will skip it.
-        if !cx.config.force_rerun && is_up_to_date(cx, testpaths, &early_props, revision) {
+        if !desc.ignore
+            && !cx.config.force_rerun
+            && is_up_to_date(cx, testpaths, &early_props, revision)
+        {
             desc.ignore = true;
             // Keep this in sync with the "up-to-date" message detected by bootstrap.
             // FIXME(Zalathar): Now that we are no longer tied to libtest, we could
