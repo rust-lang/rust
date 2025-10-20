43e378d9f6db3172054c188fb936f5de17d3abb3
T <simp4t7@gmail.com>
2025-10-20 10:11:56 +0000

    remove tempfile tests

--- src/tools/tidy/src/tidy_flags/tests.rs
+++ src/tools/tidy/src/tidy_flags/tests.rs
@@ -26,67 +26,67 @@ fn get_untracked(root_path: &PathBuf) -> HashSet<PathBuf> {
     untracked_files
 }
 
-#[test]
-//Creates a temp untracked file and checks that there are more untracked files than
-//previously.
-fn test_get_untracked() {
-    let temp_dir = tempdir_in(get_test_dir()).unwrap();
-    let temp_dir_path = temp_dir.path().to_path_buf();
+//#[test]
+////Creates a temp untracked file and checks that there are more untracked files than
+////previously.
+//fn test_get_untracked() {
+//let temp_dir = tempdir_in(get_test_dir()).unwrap();
+//let temp_dir_path = temp_dir.path().to_path_buf();
 
-    let num_untracked = get_untracked(&temp_dir_path).len();
+//let num_untracked = get_untracked(&temp_dir_path).len();
 
-    assert!(num_untracked == 0);
+//assert!(num_untracked == 0);
 
-    let _new_file = tempfile::NamedTempFile::new_in(&temp_dir_path).unwrap();
+//let _new_file = tempfile::NamedTempFile::new_in(&temp_dir_path).unwrap();
 
-    let new_num_untracked = get_untracked(&temp_dir_path).len();
+//let new_num_untracked = get_untracked(&temp_dir_path).len();
 
-    assert!(new_num_untracked == 1);
-}
+//assert!(new_num_untracked == 1);
+//}
 
-#[test]
-//Various checks for the walk function and interactions with `TidyFlags`.
-fn test_tidy_walk() {
-    let temp_dir = tempdir_in(get_test_dir()).unwrap();
-    let temp_dir_path = temp_dir.path().to_path_buf();
+//#[test]
+////Various checks for the walk function and interactions with `TidyFlags`.
+//fn test_tidy_walk() {
+//let temp_dir = tempdir_in(get_test_dir()).unwrap();
+//let temp_dir_path = temp_dir.path().to_path_buf();
 
-    let _file_guards: Vec<NamedTempFile> = (0..5)
-        .map(|_| tempfile::Builder::new().prefix("temp_file").tempfile_in(&temp_dir_path).unwrap())
-        .collect();
+//let _file_guards: Vec<NamedTempFile> = (0..5)
+//.map(|_| tempfile::Builder::new().prefix("temp_file").tempfile_in(&temp_dir_path).unwrap())
+//.collect();
 
-    //Checks that untracked files are included.
-    let mut tidy_flags = TidyFlags::new(&temp_dir_path, &[]);
-    tidy_flags.include_untracked = true;
+////Checks that untracked files are included.
+//let mut tidy_flags = TidyFlags::new(&temp_dir_path, &[]);
+//tidy_flags.include_untracked = true;
 
-    let mut file_count = 0;
-    walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
-        file_count += 1;
-    });
+//let mut file_count = 0;
+//walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
+//file_count += 1;
+//});
 
-    assert!(file_count == 5);
+//assert!(file_count == 5);
 
-    //Checks that untracked files are excluded.
-    tidy_flags.include_untracked = false;
+////Checks that untracked files are excluded.
+//tidy_flags.include_untracked = false;
 
-    let mut file_count = 0;
-    walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
-        file_count += 1;
-    });
+//let mut file_count = 0;
+//walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
+//file_count += 1;
+//});
 
-    assert!(file_count == 0);
+//assert!(file_count == 0);
 
-    //Default behavior include ALL files, including untracked. This is the previous behavior, so any
-    //downstream functions using `tidy::walk::walk` and now passing in `TidyFlags::default()` should
-    //be the same as before.
-    let tidy_flags = TidyFlags::default();
+////Default behavior include ALL files, including untracked. This is the previous behavior, so any
+////downstream functions using `tidy::walk::walk` and now passing in `TidyFlags::default()` should
+////be the same as before.
+//let tidy_flags = TidyFlags::default();
 
-    let mut file_count = 0;
-    walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
-        file_count += 1;
-    });
+//let mut file_count = 0;
+//walk(&temp_dir_path, &tidy_flags, |_, _| false, &mut |_, _| {
+//file_count += 1;
+//});
 
-    assert!(file_count == 5);
-}
+//assert!(file_count == 5);
+//}
 
 #[test]
 //Checks that the tidy walk function will include files in `tidy/tidy_flags/`.
