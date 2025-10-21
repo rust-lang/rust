58efee40ea4465df27f6a8be45a273de684a15e1
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2025-10-21 20:13:41 +0200

    Port `cfg!()` macro to the new attribute parsing system
    Signed-off-by: Jonathan Brouwer <jonathantbrouwer@gmail.com>

--- compiler/rustc_attr_parsing/src/lib.rs
+++ compiler/rustc_attr_parsing/src/lib.rs
@@ -106,7 +106,7 @@ mod target_checking;
 pub mod validate_attr;
 
 pub use attributes::cfg::{
-    CFG_TEMPLATE, EvalConfigResult, eval_config_entry, parse_cfg, parse_cfg_attr,
+    CFG_TEMPLATE, EvalConfigResult, eval_config_entry, parse_cfg, parse_cfg_attr, parse_cfg_entry,
 };
 pub use attributes::cfg_old::*;
 pub use attributes::util::{is_builtin_attr, is_doc_alias_attrs_contain_symbol, parse_version};
