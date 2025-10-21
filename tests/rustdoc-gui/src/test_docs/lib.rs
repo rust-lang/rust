fda393f1b00df18ae9ada6f83a84a1c0dd1c3f06
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-10-21 16:51:44 +0200

    Fix link to attr/derive bang macros

--- tests/rustdoc-gui/src/test_docs/lib.rs
+++ tests/rustdoc-gui/src/test_docs/lib.rs
@@ -9,6 +9,7 @@
 #![feature(doc_cfg)]
 #![feature(associated_type_defaults)]
 #![feature(macro_attr)]
+#![feature(macro_derive)]
 
 /*!
 Enable the feature <span class="stab portability"><code>some-feature</code></span> to enjoy
