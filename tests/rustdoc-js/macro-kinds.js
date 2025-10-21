6dce6010d9145c90baf12b468b68346e313f1a46
Guillaume Gomez <guillaume1.gomez@gmail.com>
2025-10-21 16:51:44 +0200

    Fix link to attr/derive bang macros

--- tests/rustdoc-js/macro-kinds.js
+++ tests/rustdoc-js/macro-kinds.js
@@ -4,15 +4,24 @@ const EXPECTED = [
     {
         'query': 'macro:macro',
         'others': [
-            { 'path': 'macro_kinds', 'name': 'macro1' },
-            { 'path': 'macro_kinds', 'name': 'macro3' },
+            { 'path': 'macro_kinds', 'name': 'macro1', 'href': '../macro_kinds/macro.macro1.html' },
+            { 'path': 'macro_kinds', 'name': 'macro3', 'href': '../macro_kinds/macro.macro3.html' },
         ],
     },
     {
         'query': 'attr:macro',
         'others': [
-            { 'path': 'macro_kinds', 'name': 'macro1' },
-            { 'path': 'macro_kinds', 'name': 'macro2' },
+            { 'path': 'macro_kinds', 'name': 'macro1', 'href': '../macro_kinds/macro.macro1.html' },
+            { 'path': 'macro_kinds', 'name': 'macro2', 'href': '../macro_kinds/attr.macro2.html' },
+        ],
+    },
+    {
+        'query': 'derive:macro',
+        'others': [
+            { 'path': 'macro_kinds', 'name': 'macro1', 'href': '../macro_kinds/macro.macro1.html' },
+            {
+                'path': 'macro_kinds', 'name': 'macro4', 'href': '../macro_kinds/derive.macro4.html'
+            },
         ],
     },
 ];
