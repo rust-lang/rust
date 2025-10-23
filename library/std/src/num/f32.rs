033711ce8d011d7eed296917eb274232c3b45c34
Trevor Gross <tmgross@umich.edu>
2025-10-23 17:04:03 -0400

    Stabilize `const_mul_add`
    Newly stable API:
    
        impl {f32, f64} {
            pub const fn mul_add(self, a: Self, b: Self) -> Self;
        }
    
    This includes making the intrinsics `fmaf{16,32,64,128}` const stable
    for indirect use, matching similar intrinsics.
    
    Closes: https://github.com/rust-lang/rust/issues/146724

--- library/std/src/num/f32.rs
+++ library/std/src/num/f32.rs
@@ -217,7 +217,7 @@ impl f32 {
     #[must_use = "method returns a new number and does not mutate the original value"]
     #[stable(feature = "rust1", since = "1.0.0")]
     #[inline]
-    #[rustc_const_unstable(feature = "const_mul_add", issue = "146724")]
+    #[rustc_const_stable(feature = "const_mul_add", since = "CURRENT_RUSTC_VERSION")]
     pub const fn mul_add(self, a: f32, b: f32) -> f32 {
         core::f32::math::mul_add(self, a, b)
     }
