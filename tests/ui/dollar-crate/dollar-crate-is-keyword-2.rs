f39a019bf37eea51da7116dbccc3c257838765ca
mu001999 <mu001999@outlook.com>
2025-10-26 19:49:41 +0800

    Rewrite build_reduced_graph_for_use_tree

--- tests/ui/dollar-crate/dollar-crate-is-keyword-2.rs
+++ tests/ui/dollar-crate/dollar-crate-is-keyword-2.rs
@@ -2,7 +2,7 @@ mod a {}
 
 macro_rules! m {
     () => {
-        use a::$crate; //~ ERROR unresolved import `a::$crate`
+        use a::$crate; //~ ERROR `$crate` in paths can only be used in start position
         use a::$crate::b; //~ ERROR `$crate` in paths can only be used in start position
         type A = a::$crate; //~ ERROR `$crate` in paths can only be used in start position
     }
