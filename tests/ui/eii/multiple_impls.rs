615acd8b4d7b854588ba5a4c4132bd39a78e0a4e
Jana Dönszelmann <jana@donsz.nl>
2025-10-27 14:54:54 +0100

    add tests for using a single impl for two EIIs

---
+++ tests/ui/eii/multiple_impls.rs
@@ -0,0 +1,23 @@
+//@ run-pass
+//@ check-run-results
+//@ ignore-backends: gcc
+// Tests whether one function could implement two EIIs.
+#![feature(eii)]
+
+#[eii]
+fn a(x: u64);
+
+#[eii]
+fn b(x: u64);
+
+#[a]
+#[b]
+fn implementation(x: u64) {
+    println!("{x:?}")
+}
+
+// what you would write:
+fn main() {
+    a(42);
+    b(42);
+}
