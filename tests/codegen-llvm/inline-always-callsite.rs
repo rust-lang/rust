95b6747a9de4d30b328c3300a27622b15c1c83e5
Jamie Cunliffe <Jamie.Cunliffe@arm.com>
2025-10-27 13:49:23 +0000

    Broken test to show alwaysinline attribute not being applied

---
+++ tests/codegen-llvm/inline-always-callsite.rs
@@ -0,0 +1,37 @@
+//@ add-core-stubs
+//@ compile-flags: --target aarch64-unknown-linux-gnu -Zinline-mir=no -C no-prepopulate-passes
+//@ needs-llvm-components: aarch64
+
+#![crate_type = "lib"]
+#![feature(no_core, lang_items, target_feature_inline_always)]
+#![no_core]
+
+extern crate minicore;
+use minicore::*;
+
+#[inline(always)]
+#[target_feature(enable = "neon")]
+#[no_mangle]
+pub fn single_target_feature() -> i32 {
+    42
+}
+
+#[inline(always)]
+#[target_feature(enable = "neon,i8mm")]
+#[no_mangle]
+// CHECK: define noundef i32 @multiple_target_features() unnamed_addr #1 {
+pub fn multiple_target_features() -> i32 {
+    // CHECK: %_0 = call noundef i32 @single_target_feature() #3
+    single_target_feature()
+}
+
+#[no_mangle]
+// CHECK: define noundef i32 @inherits_from_global() unnamed_addr #2 {
+pub fn inherits_from_global() -> i32 {
+    unsafe {
+        // CHECK: %_0 = call noundef i32 @single_target_feature() #3
+        single_target_feature()
+    }
+}
+
+// CHECK: attributes #3 = { nounwind }
