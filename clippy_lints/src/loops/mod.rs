bd22913666869c92b7be6217d439a87986f9afcb
Jason Newcomb <jsnewcomb@pm.me>
2025-10-28 03:48:01 -0400

    `unused_enumerate_index`: move to loops lint pass
    Move `unused_enumerate_index.rs` to `methods`.

--- clippy_lints/src/loops/mod.rs
+++ clippy_lints/src/loops/mod.rs
@@ -880,6 +880,16 @@ impl<'tcx> LateLintPass<'tcx> for Loops {
             missing_spin_loop::check(cx, condition, body);
             manual_while_let_some::check(cx, condition, body, span);
         }
+
+        if let ExprKind::MethodCall(path, recv, [arg], _) = expr.kind
+            && matches!(
+                path.ident.name,
+                sym::all | sym::any | sym::filter_map | sym::find_map | sym::flat_map | sym::for_each | sym::map
+            )
+            && !recv.span.from_expansion()
+        {
+            unused_enumerate_index::check_method(cx, expr, recv, arg);
+        }
     }
 }
 
@@ -908,7 +918,7 @@ impl Loops {
         same_item_push::check(cx, pat, arg, body, expr, self.msrv);
         manual_flatten::check(cx, pat, arg, body, span, self.msrv);
         manual_find::check(cx, pat, arg, body, span, expr);
-        unused_enumerate_index::check(cx, pat, arg, body);
+        unused_enumerate_index::check_loop(cx, pat, arg, body);
         char_indices_as_byte_indices::check(cx, pat, arg, body);
     }
 
