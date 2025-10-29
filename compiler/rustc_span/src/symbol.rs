63e07cfd5f0b6f87e98ea9757a1ccf0d3f463857
Waffle Lapkin <waffle.lapkin@gmail.com>
2025-10-29 19:25:57 +0100

    add semantics to `MaybeDangling`

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1409,6 +1409,7 @@ symbols! {
         maxnumf128,
         may_dangle,
         may_unwind,
+        maybe_dangling,
         maybe_uninit,
         maybe_uninit_uninit,
         maybe_uninit_zeroed,
--- tests/codegen-llvm/manually_drop_refs.rs
+++ tests/codegen-llvm/manually_drop_refs.rs
@@ -3,19 +3,19 @@
 
 use std::mem::ManuallyDrop;
 
-// CHECK: define noundef nonnull align 1 ptr @f(ptr noalias noundef nonnull readnone returned align 1 {{(captures\(ret: address, provenance\))?}} %x) unnamed_addr
+// CHECK: define noundef nonnull ptr @f(ptr noundef nonnull readnone returned {{(captures\(ret: address, provenance\) )?}}%x) unnamed_addr
 #[no_mangle]
 pub fn f(x: ManuallyDrop<Box<u8>>) -> ManuallyDrop<Box<u8>> {
     x
 }
 
-// CHECK: define noundef nonnull align 1 dereferenceable(1) ptr @g(ptr noalias noundef readonly returned align 1 {{(captures\(ret: address, read_provenance\))?}} dereferenceable(1) %x) unnamed_addr
+// CHECK: define noundef nonnull ptr @g(ptr noundef nonnull readnone returned {{(captures\(ret: address, provenance\) )?}}%x) unnamed_addr
 #[no_mangle]
 pub fn g(x: ManuallyDrop<&u8>) -> ManuallyDrop<&u8> {
     x
 }
 
-// CHECK: define noundef nonnull align 1 dereferenceable(1) ptr @h(ptr noalias noundef readnone returned align 1 {{(captures\(ret: address, provenance\))?}} dereferenceable(1) %x) unnamed_addr
+// CHECK: define noundef nonnull ptr @h(ptr noundef nonnull readnone returned {{(captures\(ret: address, provenance\) )?}}%x) unnamed_addr
 #[no_mangle]
 pub fn h(x: ManuallyDrop<&mut u8>) -> ManuallyDrop<&mut u8> {
     x
