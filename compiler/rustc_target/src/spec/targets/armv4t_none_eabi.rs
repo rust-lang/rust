38987c0b9a922d7401cfd44e617003b1ea631367
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2025-10-29 11:17:36 +0000

    Remove +atomics-32 for armv4t-none-eabi and armv5te-none-eabi
    Also rationalise the settings, and copy in the thumb base defaults,
    rather than just import them, because these aren't M-profile
    microcontrollers and may not want to always have the same settings.

--- compiler/rustc_target/src/spec/targets/armv4t_none_eabi.rs
+++ compiler/rustc_target/src/spec/targets/armv4t_none_eabi.rs
@@ -25,15 +25,6 @@ pub(crate) fn target() -> Target {
         },
         pointer_width: 32,
         arch: Arch::Arm,
-        /* Data layout args are '-' separated:
-         * little endian
-         * stack is 64-bit aligned (EABI)
-         * pointers are 32-bit
-         * i64 must be 64-bit aligned (EABI)
-         * mangle names with ELF style
-         * native integers are 32-bit
-         * All other elements are default
-         */
         data_layout: "e-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64".into(),
         options: TargetOptions {
             abi: Abi::Eabi,
@@ -43,15 +34,29 @@ pub(crate) fn target() -> Target {
             asm_args: cvs!["-mthumb-interwork", "-march=armv4t", "-mlittle-endian",],
             // Force-enable 32-bit atomics, which allows the use of atomic load/store only.
             // The resulting atomics are ABI incompatible with atomics backed by libatomic.
-            features: "+soft-float,+strict-align,+atomics-32".into(),
-            main_needs_argc_argv: false,
+            features: "+soft-float,+strict-align".into(),
             atomic_cas: false,
             has_thumb_interworking: true,
-            relocation_model: RelocModel::Static,
+            linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
+            linker: Some("rust-lld".into()),
+            // Because these devices have very little resources having an unwinder is too onerous so we
+            // default to "abort" because the "unwind" strategy is very rare.
             panic_strategy: PanicStrategy::Abort,
-            // From thumb_base, rust-lang/rust#44993.
+            // Similarly, one almost always never wants to use relocatable code because of the extra
+            // costs it involves.
+            relocation_model: RelocModel::Static,
+            // When this section is added a volatile load to its start address is also generated. This
+            // volatile load is a footgun as it can end up loading an invalid memory address, depending
+            // on how the user set up their linker scripts. This section adds pretty printer for stuff
+            // like std::Vec, which is not that used in no-std context, so it's best to left it out
+            // until we figure a way to add the pretty printers without requiring a volatile load cf.
+            // rust-lang/rust#44993.
             emit_debug_gdb_scripts: false,
-            // From thumb_base, GCC gives enums a minimum of 8 bits on no-os targets.
+            // LLVM is eager to trash the link register when calling `noreturn` functions, which
+            // breaks debugging. Preserve LR by default to prevent that from happening.
+            frame_pointer: FramePointer::Always,
+            // ARM supports multiple ABIs for enums, the linux one matches the default of 32 here
+            // but any arm-none or thumb-none target will be defaulted to 8 on GCC.
             c_enum_min_bits: Some(8),
             ..Default::default()
         },
--- compiler/rustc_target/src/spec/targets/armv5te_none_eabi.rs
+++ compiler/rustc_target/src/spec/targets/armv5te_none_eabi.rs
@@ -15,17 +15,7 @@ pub(crate) fn target() -> Target {
         },
         pointer_width: 32,
         arch: Arch::Arm,
-        /* Data layout args are '-' separated:
-         * little endian
-         * stack is 64-bit aligned (EABI)
-         * pointers are 32-bit
-         * i64 must be 64-bit aligned (EABI)
-         * mangle names with ELF style
-         * native integers are 32-bit
-         * All other elements are default
-         */
         data_layout: "e-m:e-p:32:32-Fi8-i64:64-v128:64:128-a:0:32-n32-S64".into(),
-
         options: TargetOptions {
             abi: Abi::Eabi,
             llvm_floatabi: Some(FloatAbi::Soft),
@@ -35,16 +25,31 @@ pub(crate) fn target() -> Target {
             // * use little-endian
             asm_args: cvs!["-mthumb-interwork", "-march=armv5te", "-mlittle-endian",],
             // minimum extra features, these cannot be disabled via -C
-            // Also force-enable 32-bit atomics, which allows the use of atomic load/store only.
-            // The resulting atomics are ABI incompatible with atomics backed by libatomic.
-            features: "+soft-float,+strict-align,+atomics-32".into(),
-            frame_pointer: FramePointer::MayOmit,
-            main_needs_argc_argv: false,
-            // don't have atomic compare-and-swap
+            features: "+soft-float,+strict-align".into(),
             atomic_cas: false,
             has_thumb_interworking: true,
-
-            ..base::thumb::opts()
+            linker_flavor: LinkerFlavor::Gnu(Cc::No, Lld::Yes),
+            linker: Some("rust-lld".into()),
+            // Because these devices have very little resources having an unwinder is too onerous so we
+            // default to "abort" because the "unwind" strategy is very rare.
+            panic_strategy: PanicStrategy::Abort,
+            // Similarly, one almost always never wants to use relocatable code because of the extra
+            // costs it involves.
+            relocation_model: RelocModel::Static,
+            // When this section is added a volatile load to its start address is also generated. This
+            // volatile load is a footgun as it can end up loading an invalid memory address, depending
+            // on how the user set up their linker scripts. This section adds pretty printer for stuff
+            // like std::Vec, which is not that used in no-std context, so it's best to left it out
+            // until we figure a way to add the pretty printers without requiring a volatile load cf.
+            // rust-lang/rust#44993.
+            emit_debug_gdb_scripts: false,
+            // LLVM is eager to trash the link register when calling `noreturn` functions, which
+            // breaks debugging. Preserve LR by default to prevent that from happening.
+            frame_pointer: FramePointer::Always,
+            // ARM supports multiple ABIs for enums, the linux one matches the default of 32 here
+            // but any arm-none or thumb-none target will be defaulted to 8 on GCC.
+            c_enum_min_bits: Some(8),
+            ..Default::default()
         },
     }
 }
