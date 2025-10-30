8762219b8fc5d3ab8d48345df6be45d6e63a20e6
Oli Scherer <github@oli-obk.de>
2025-10-30 09:51:50 +0000

    Fix deferred cast checks using the wrong body for determining constness

--- tests/ui/traits/const-traits/issue-103677.rs
+++ tests/ui/traits/const-traits/issue-103677.rs
@@ -1,10 +1,9 @@
-//@[stock] check-pass
+//@ check-pass
 //@ revisions: stock cnst
 #![cfg_attr(cnst, feature(const_trait_impl))]
 
 const _: fn(&String) = |s| {
     &*s as &str;
-    //[cnst]~^ ERROR: the trait bound `String: const Deref` is not satisfied
 };
 
 fn main() {}
