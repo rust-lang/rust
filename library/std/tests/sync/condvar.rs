3d5a40809c8322de933205a4d581059dc8adc3f3
Connor Tsui <connor.tsui20@gmail.com>
2025-10-31 15:12:46 -0400

    update `nonpoison::Condvar` to take guards by reference
    Since non-poisoning `Condvar` take non-poisoing `Mutex`es when
    `wait`ing, we do not need to take by ownership since a poison error
    cannot occur while we wait.
    
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- library/std/tests/sync/condvar.rs
+++ library/std/tests/sync/condvar.rs
@@ -19,8 +19,8 @@ nonpoison_and_poison_unwrap_test!(
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn notify_one() {
-    use std::sync::{Condvar, Mutex};
+fn poison_notify_one() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let m = Arc::new(Mutex::new(()));
     let m2 = m.clone();
@@ -39,8 +39,28 @@ fn notify_one() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn notify_all() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_notify_one() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let m = Arc::new(Mutex::new(()));
+    let m2 = m.clone();
+    let c = Arc::new(Condvar::new());
+    let c2 = c.clone();
+
+    let mut g = m.lock();
+    let _t = thread::spawn(move || {
+        let _g = m2.lock();
+        c2.notify_one();
+    });
+
+    c.wait(&mut g);
+    drop(g);
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_notify_all() {
+    use std::sync::poison::{Condvar, Mutex};
 
     const N: usize = 10;
 
@@ -78,8 +98,47 @@ fn notify_all() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn test_mutex_arc_condvar() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_notify_all() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    const N: usize = 10;
+
+    let data = Arc::new((Mutex::new(0), Condvar::new()));
+    let (tx, rx) = channel();
+    for _ in 0..N {
+        let data = data.clone();
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let &(ref lock, ref cond) = &*data;
+            let mut cnt = lock.lock();
+            *cnt += 1;
+            if *cnt == N {
+                tx.send(()).unwrap();
+            }
+            while *cnt != 0 {
+                cond.wait(&mut cnt);
+            }
+            tx.send(()).unwrap();
+        });
+    }
+    drop(tx);
+
+    let &(ref lock, ref cond) = &*data;
+    rx.recv().unwrap();
+    let mut cnt = lock.lock();
+    *cnt = 0;
+    cond.notify_all();
+    drop(cnt);
+
+    for _ in 0..N {
+        rx.recv().unwrap();
+    }
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_test_mutex_arc_condvar() {
+    use std::sync::poison::{Condvar, Mutex};
 
     struct Packet<T>(Arc<(Mutex<T>, Condvar)>);
 
@@ -113,8 +172,43 @@ fn test_mutex_arc_condvar() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_while() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_test_mutex_arc_condvar() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    struct Packet<T>(Arc<(Mutex<T>, Condvar)>);
+
+    let packet = Packet(Arc::new((Mutex::new(false), Condvar::new())));
+    let packet2 = Packet(packet.0.clone());
+
+    let (tx, rx) = channel();
+
+    let _t = thread::spawn(move || {
+        // Wait until our parent has taken the lock.
+        rx.recv().unwrap();
+        let &(ref lock, ref cvar) = &*packet2.0;
+
+        // Set the data to `true` and wake up our parent.
+        let mut guard = lock.lock();
+        *guard = true;
+        cvar.notify_one();
+    });
+
+    let &(ref lock, ref cvar) = &*packet.0;
+    let mut guard = lock.lock();
+    // Wake up our child.
+    tx.send(()).unwrap();
+
+    // Wait until our child has set the data to `true`.
+    assert!(!*guard);
+    while !*guard {
+        cvar.wait(&mut guard);
+    }
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_while() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let pair = Arc::new((Mutex::new(false), Condvar::new()));
     let pair2 = pair.clone();
@@ -136,8 +230,32 @@ fn wait_while() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_timeout_wait() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_wait_while() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = pair.clone();
+
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move || {
+        let &(ref lock, ref cvar) = &*pair2;
+        let mut started = lock.lock();
+        *started = true;
+        // We notify the condvar that the value has changed.
+        cvar.notify_one();
+    });
+
+    // Wait for the thread to start up.
+    let &(ref lock, ref cvar) = &*pair;
+    let mut guard = lock.lock();
+    cvar.wait_while(&mut guard, |started| !*started);
+    assert!(*guard);
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_timeout_wait() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let m = Arc::new(Mutex::new(()));
     let c = Arc::new(Condvar::new());
@@ -157,8 +275,29 @@ fn wait_timeout_wait() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_timeout_while_wait() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_wait_timeout_wait() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let m = Arc::new(Mutex::new(()));
+    let c = Arc::new(Condvar::new());
+
+    loop {
+        let mut g = m.lock();
+        let no_timeout = c.wait_timeout(&mut g, Duration::from_millis(1));
+        // spurious wakeups mean this isn't necessarily true
+        // so execute test again, if not timeout
+        if !no_timeout.timed_out() {
+            continue;
+        }
+
+        break;
+    }
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_timeout_while_wait() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let m = Arc::new(Mutex::new(()));
     let c = Arc::new(Condvar::new());
@@ -171,8 +310,22 @@ fn wait_timeout_while_wait() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_timeout_while_instant_satisfy() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_wait_timeout_while_wait() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let m = Arc::new(Mutex::new(()));
+    let c = Arc::new(Condvar::new());
+
+    let mut g = m.lock();
+    let wait = c.wait_timeout_while(&mut g, Duration::from_millis(1), |_| true);
+    // no spurious wakeups. ensure it timed-out
+    assert!(wait.timed_out());
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_timeout_while_instant_satisfy() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let m = Arc::new(Mutex::new(()));
     let c = Arc::new(Condvar::new());
@@ -185,8 +338,22 @@ fn wait_timeout_while_instant_satisfy() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_timeout_while_wake() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_wait_timeout_while_instant_satisfy() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let m = Arc::new(Mutex::new(()));
+    let c = Arc::new(Condvar::new());
+
+    let mut g = m.lock();
+    let wait = c.wait_timeout_while(&mut g, Duration::from_millis(0), |_| false);
+    // ensure it didn't time-out even if we were not given any time.
+    assert!(!wait.timed_out());
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_timeout_while_wake() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let pair = Arc::new((Mutex::new(false), Condvar::new()));
     let pair_copy = pair.clone();
@@ -211,8 +378,33 @@ fn wait_timeout_while_wake() {
 
 #[test]
 #[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
-fn wait_timeout_wake() {
-    use std::sync::{Condvar, Mutex};
+fn nonpoison_wait_timeout_while_wake() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair_copy = pair.clone();
+
+    let &(ref m, ref c) = &*pair;
+    let mut g = m.lock();
+    let _t = thread::spawn(move || {
+        let &(ref lock, ref cvar) = &*pair_copy;
+        let mut started = lock.lock();
+        thread::sleep(Duration::from_millis(1));
+        *started = true;
+        cvar.notify_one();
+    });
+
+    let wait =
+        c.wait_timeout_while(&mut g, Duration::from_millis(u64::MAX), |&mut notified| !notified);
+    // ensure it didn't time-out even if we were not given any time.
+    assert!(!wait.timed_out());
+    assert!(*g);
+}
+
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn poison_wait_timeout_wake() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let m = Arc::new(Mutex::new(()));
     let c = Arc::new(Condvar::new());
@@ -249,6 +441,46 @@ fn wait_timeout_wake() {
     }
 }
 
+#[test]
+#[cfg(not(any(target_os = "emscripten", target_os = "wasi")))] // No threads.
+fn nonpoison_wait_timeout_wake() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let m = Arc::new(Mutex::new(()));
+    let c = Arc::new(Condvar::new());
+
+    loop {
+        let mut g = m.lock();
+
+        let c2 = c.clone();
+        let m2 = m.clone();
+
+        let notified = Arc::new(AtomicBool::new(false));
+        let notified_copy = notified.clone();
+
+        let t = thread::spawn(move || {
+            let _g = m2.lock();
+            thread::sleep(Duration::from_millis(1));
+            notified_copy.store(true, Ordering::Relaxed);
+            c2.notify_one();
+        });
+
+        let timeout_res = c.wait_timeout(&mut g, Duration::from_millis(u64::MAX));
+        assert!(!timeout_res.timed_out());
+        // spurious wakeups mean this isn't necessarily true
+        // so execute test again, if not notified
+        if !notified.load(Ordering::Relaxed) {
+            t.join().unwrap();
+            continue;
+        }
+        drop(g);
+
+        t.join().unwrap();
+
+        break;
+    }
+}
+
 // Some platforms internally cast the timeout duration into nanoseconds.
 // If they fail to consider overflow during the conversion (I'm looking
 // at you, macOS), `wait_timeout` will return immediately and indicate a
@@ -256,8 +488,8 @@ fn wait_timeout_wake() {
 // `std` should guard against this by clamping the timeout.
 // See #37440 for context.
 #[test]
-fn timeout_nanoseconds() {
-    use std::sync::{Condvar, Mutex};
+fn poison_timeout_nanoseconds() {
+    use std::sync::poison::{Condvar, Mutex};
 
     let sent = Mutex::new(false);
     let cond = Condvar::new();
@@ -290,6 +522,43 @@ fn timeout_nanoseconds() {
                 .unwrap();
             assert!(!res.timed_out());
             guard = g;
+        }
+    })
+}
+
+#[test]
+fn nonpoison_timeout_nanoseconds() {
+    use std::sync::nonpoison::{Condvar, Mutex};
+
+    let sent = Mutex::new(false);
+    let cond = Condvar::new();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            // Sleep so that the other thread has a chance to encounter the
+            // timeout.
+            thread::sleep(Duration::from_secs(2));
+            sent.set(true);
+            cond.notify_all();
+        });
+
+        let mut guard = sent.lock();
+        // Loop until `sent` is set by the thread to guard against spurious
+        // wakeups. If the `wait_timeout` happens just before the signal by
+        // the other thread, such a spurious wakeup might prevent the
+        // miscalculated timeout from occurring, but this is basically just
+        // a smoke test anyway.
+        loop {
+            if *guard {
+                break;
+            }
+
+            // If there is internal overflow, this call will return almost
+            // immediately, before the other thread has reached the `notify_all`,
+            // and indicate a timeout.
+            let res = cond
+                .wait_timeout(&mut guard, Duration::from_secs(u64::MAX.div_ceil(1_000_000_000)));
+            assert!(!res.timed_out());
         }
     })
 }
