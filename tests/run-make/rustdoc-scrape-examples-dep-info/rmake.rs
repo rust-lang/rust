65aed3b6e63bc7ad9ed61785197f882bfc071089
Michael Howell <michael@notriddle.com>
2025-10-31 11:16:00 -0700

    Fix test on Windows

--- tests/run-make/rustdoc-scrape-examples-dep-info/rmake.rs
+++ tests/run-make/rustdoc-scrape-examples-dep-info/rmake.rs
@@ -10,10 +10,10 @@ fn main() {
         &["--emit=dep-info,invocation-specific"],
     );
 
-    let content = rfs::read_to_string("foobar.d");
+    let content = rfs::read_to_string("foobar.d").replace(r"\", "/");
     assert_contains(&content, "lib.rs:");
     assert_contains(&content, "rustdoc/ex.calls:");
 
-    let content = rfs::read_to_string("ex.d");
+    let content = rfs::read_to_string("ex.d").replace(r"\", "/");
     assert_contains(&content, "examples/ex.rs:");
 }
