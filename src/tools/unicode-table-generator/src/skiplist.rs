4aeb2970640cc81926f06b40ef9c7841064a4c28
Jieyou Xu <jieyouxu@outlook.com>
2025-11-03 19:39:50 +0800

    Revert "unicode_data refactors RUST-147622"
    This PR reverts RUST-147622 for several reasons:
    
    1. The RUST-147622 PR would format the generated core library code using
       an arbitrary `rustfmt` picked up from `PATH`, which will cause
       hard-to-debug failures when the `rustfmt` used to format the
       generated unicode data code versus the `rustfmt` used to format the
       in-tree library code.
    2. Previously, the `unicode-table-generator` tests were not run under CI
       as part of `coretests`, and since for `x86_64-gnu-aux` job we run
       library `coretests` with `miri`, the generated tests unfortunately
       caused an unacceptably large Merge CI time regression from ~2 hours
       to ~3.5 hours, making it the slowest Merge CI job (and thus the new
       bottleneck).
    3. This PR also has an unintended effect of causing a diagnostic
       regression (RUST-148387), though that's mostly an edge case not
       properly handled by `rustc` diagnostics.
    
    Given that these are three distinct causes with non-trivial fixes, I'm
    proposing to revert this PR to return us to baseline. This is not
    prejudice against relanding the changes with these issues addressed, but
    to alleviate time pressure to address these non-trivial issues.

--- src/tools/unicode-table-generator/src/skiplist.rs
+++ src/tools/unicode-table-generator/src/skiplist.rs
@@ -1,8 +1,8 @@
-use std::fmt::{self};
+use std::fmt::{self, Write as _};
 use std::ops::Range;
 
+use crate::fmt_list;
 use crate::raw_emitter::RawEmitter;
-use crate::writeln;
 
 /// This will get packed into a single u32 before inserting into the data set.
 #[derive(PartialEq)]
@@ -68,45 +68,79 @@ impl RawEmitter {
             assert!(inserted);
         }
 
+        writeln!(&mut self.file, "use super::ShortOffsetRunHeader;\n").unwrap();
+        writeln!(
+            &mut self.file,
+            "static SHORT_OFFSET_RUNS: [ShortOffsetRunHeader; {}] = [{}];",
+            short_offset_runs.len(),
+            fmt_list(short_offset_runs.iter())
+        )
+        .unwrap();
         self.bytes_used += 4 * short_offset_runs.len();
+        writeln!(
+            &mut self.file,
+            "static OFFSETS: [u8; {}] = [{}];",
+            coded_offsets.len(),
+            fmt_list(&coded_offsets)
+        )
+        .unwrap();
         self.bytes_used += coded_offsets.len();
 
         // The inlining in this code works like the following:
         //
-        // The `skip_search` function is always inlined into the parent `lookup_slow` fn,
+        // The `skip_search` function is always inlined into the parent `lookup` fn,
         // thus the compiler can generate optimal code based on the referenced `static`s.
         //
-        // The lower-bounds check is inlined into the caller, and slower-path
-        // `skip_search` is outlined into a separate `lookup_slow` fn.
-        assert!(first_code_point > 0x7f);
-        writeln!(self.file,
-            "use super::ShortOffsetRunHeader;
-
-            static SHORT_OFFSET_RUNS: [ShortOffsetRunHeader; {short_offset_runs_len}] = {short_offset_runs:?};
-            static OFFSETS: [u8; {coded_offset_len}] = {coded_offsets:?};
-
-            #[inline]
-            pub fn lookup(c: char) -> bool {{
-                debug_assert!(!c.is_ascii());
-                (c as u32) >= {first_code_point:#04x} && lookup_slow(c)
-            }}
-
-            #[inline(never)]
-            fn lookup_slow(c: char) -> bool {{
-                const {{
-                    assert!(SHORT_OFFSET_RUNS.last().unwrap().0 > char::MAX as u32);
-                    let mut i = 0;
-                    while i < SHORT_OFFSET_RUNS.len() {{
-                        assert!(SHORT_OFFSET_RUNS[i].start_index() < OFFSETS.len());
-                        i += 1;
-                    }}
-                }}
-                // SAFETY: We just ensured the last element of `SHORT_OFFSET_RUNS` is greater than `std::char::MAX`
-                // and the start indices of all elements in `SHORT_OFFSET_RUNS` are smaller than `OFFSETS.len()`.
-                unsafe {{ super::skip_search(c, &SHORT_OFFSET_RUNS, &OFFSETS) }}
-            }}",
-            short_offset_runs_len = short_offset_runs.len(),
-            coded_offset_len = coded_offsets.len(),
-        );
+        // In the case of ASCII optimization, the lower-bounds check is inlined into
+        // the caller, and slower-path `skip_search` is outlined into a separate `lookup_slow` fn.
+        //
+        // Thus, in both cases, the `skip_search` function is specialized for the `static`s,
+        // and outlined into the prebuilt `std`.
+        if first_code_point > 0x7f {
+            writeln!(&mut self.file, "#[inline]").unwrap();
+            writeln!(&mut self.file, "pub fn lookup(c: char) -> bool {{").unwrap();
+            writeln!(&mut self.file, "    debug_assert!(!c.is_ascii());").unwrap();
+            writeln!(&mut self.file, "    (c as u32) >= {first_code_point:#04x} && lookup_slow(c)")
+                .unwrap();
+            writeln!(&mut self.file, "}}").unwrap();
+            writeln!(&mut self.file).unwrap();
+            writeln!(&mut self.file, "#[inline(never)]").unwrap();
+            writeln!(&mut self.file, "fn lookup_slow(c: char) -> bool {{").unwrap();
+        } else {
+            writeln!(&mut self.file, "pub fn lookup(c: char) -> bool {{").unwrap();
+            writeln!(&mut self.file, "    debug_assert!(!c.is_ascii());").unwrap();
+        }
+        writeln!(&mut self.file, "    const {{").unwrap();
+        writeln!(
+            &mut self.file,
+            "        assert!(SHORT_OFFSET_RUNS.last().unwrap().0 > char::MAX as u32);",
+        )
+        .unwrap();
+        writeln!(&mut self.file, "        let mut i = 0;").unwrap();
+        writeln!(&mut self.file, "        while i < SHORT_OFFSET_RUNS.len() {{").unwrap();
+        writeln!(
+            &mut self.file,
+            "            assert!(SHORT_OFFSET_RUNS[i].start_index() < OFFSETS.len());",
+        )
+        .unwrap();
+        writeln!(&mut self.file, "            i += 1;").unwrap();
+        writeln!(&mut self.file, "        }}").unwrap();
+        writeln!(&mut self.file, "    }}").unwrap();
+        writeln!(
+            &mut self.file,
+            "    // SAFETY: We just ensured the last element of `SHORT_OFFSET_RUNS` is greater than `std::char::MAX`",
+        )
+        .unwrap();
+        writeln!(
+            &mut self.file,
+            "    // and the start indices of all elements in `SHORT_OFFSET_RUNS` are smaller than `OFFSETS.len()`.",
+        )
+        .unwrap();
+        writeln!(
+            &mut self.file,
+            "    unsafe {{ super::skip_search(c, &SHORT_OFFSET_RUNS, &OFFSETS) }}"
+        )
+        .unwrap();
+        writeln!(&mut self.file, "}}").unwrap();
     }
 }
