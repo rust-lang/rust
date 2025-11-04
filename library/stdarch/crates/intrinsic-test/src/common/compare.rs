a472b5dcd904c5dd25b3b84cfe862ccaf134acb0
Madhav Madhusoodanan <madhavmadhusoodanan@gmail.com>
2025-11-04 22:02:20 +0530

    chore: formatting checks

--- library/stdarch/crates/intrinsic-test/src/common/compare.rs
+++ library/stdarch/crates/intrinsic-test/src/common/compare.rs
@@ -1,4 +1,3 @@
-use super::cli::FailureReason;
 use itertools::Itertools;
 use rayon::prelude::*;
 use std::{collections::HashMap, process::Command};
@@ -21,12 +20,12 @@ pub fn compare_outputs(intrinsic_name_list: &Vec<String>, runner: &str, target: 
         .arg(format!("./target/{target}/release/intrinsic-test-programs"))
         .current_dir("rust_programs")
         .output();
-    
+
     let (c, rust) = match (c, rust) {
         (Ok(c), Ok(rust)) => (c, rust),
         a => panic!("{a:#?}"),
     };
-    
+
     if !c.status.success() {
         error!(
             "Failed to run C program.\nstdout: {stdout}\nstderr: {stderr}",
@@ -34,7 +33,7 @@ pub fn compare_outputs(intrinsic_name_list: &Vec<String>, runner: &str, target: 
             stderr = std::str::from_utf8(&c.stderr).unwrap_or(""),
         );
     }
-    
+
     if !rust.status.success() {
         error!(
             "Failed to run Rust program.\nstdout: {stdout}\nstderr: {stderr}",
@@ -51,15 +50,21 @@ pub fn compare_outputs(intrinsic_name_list: &Vec<String>, runner: &str, target: 
         .unwrap()
         .to_lowercase()
         .replace("-nan", "nan");
-    
-    let c_output_map = c.split("############")
+
+    let c_output_map = c
+        .split("############")
         .filter_map(|output| output.trim().split_once("\n"))
         .collect::<HashMap<&str, &str>>();
-    let rust_output_map = rust.split("############")
+    let rust_output_map = rust
+        .split("############")
         .filter_map(|output| output.trim().split_once("\n"))
         .collect::<HashMap<&str, &str>>();
 
-    let intrinsics = c_output_map.keys().chain(rust_output_map.keys()).unique().collect_vec();
+    let intrinsics = c_output_map
+        .keys()
+        .chain(rust_output_map.keys())
+        .unique()
+        .collect_vec();
     let intrinsics_diff_count = intrinsics
         .par_iter()
         .filter_map(|&&intrinsic| {
@@ -67,22 +72,28 @@ pub fn compare_outputs(intrinsic_name_list: &Vec<String>, runner: &str, target: 
             let c_output = c_output_map.get(intrinsic).unwrap();
             let rust_output = rust_output_map.get(intrinsic).unwrap();
             let diff = diff::lines(c_output, rust_output);
-            let diff_count = diff.into_iter().filter_map(|diff| match diff {
-                diff::Result::Left(c) => {
-                    println!("C: {c}");
-                    Some(c)
-                }
-                diff::Result::Right(rust) => {
-                    println!("Rust: {rust}");
-                    Some(rust)
-                }
-                diff::Result::Both(_, _) => None,
-            }).count();
+            let diff_count = diff
+                .into_iter()
+                .filter_map(|diff| match diff {
+                    diff::Result::Left(c) => {
+                        println!("C: {c}");
+                        Some(c)
+                    }
+                    diff::Result::Right(rust) => {
+                        println!("Rust: {rust}");
+                        Some(rust)
+                    }
+                    diff::Result::Both(_, _) => None,
+                })
+                .count();
             println!("****************************************************************");
             if diff_count > 0 {
                 Some(intrinsic)
-            } else { None }
-        }).count();
+            } else {
+                None
+            }
+        })
+        .count();
 
     println!(
         "{} differences found (tested {} intrinsics)",
