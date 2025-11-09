7ef62a803ccbbbb4c17197e0bddf0bb12f6fce3f
Ralf Jung <post@ralfj.de>
2025-11-09 12:55:51 +0100

    adjust clippy to fix some of the issues

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -2128,7 +2128,6 @@ symbols! {
         slice_from_raw_parts_mut,
         slice_from_ref,
         slice_get_unchecked,
-        slice_into_vec,
         slice_iter,
         slice_len_fn,
         slice_patterns,
