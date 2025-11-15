e1344da2feb8c5ec12a0f830523bf9b30215d644
Ada Alakbarova <ada.alakbarova@proton.me>
2025-11-15 08:19:33 +0100

    Handle the suggestion-less case inside `print_unchecked_duration_subtration_sugg`
    This doesn't change any functionality, but will make it easier to switch
    to a uniform message in the next commit. Also a nice simplification imo

--- clippy_lints/src/time_subtraction.rs
+++ clippy_lints/src/time_subtraction.rs
@@ -1,5 +1,5 @@
 use clippy_config::Conf;
-use clippy_utils::diagnostics::{span_lint, span_lint_and_sugg};
+use clippy_utils::diagnostics::{span_lint_and_sugg, span_lint_and_then};
 use clippy_utils::msrvs::{self, Msrv};
 use clippy_utils::res::{MaybeDef, MaybeTypeckRes};
 use clippy_utils::sugg::Sugg;
@@ -109,36 +109,16 @@ impl LateLintPass<'_> for UncheckedTimeSubtraction {
                     && !expr.span.from_expansion()
                     && self.msrv.meets(cx, msrvs::TRY_FROM)
                 {
-                    // For chained subtraction like (instant - dur1) - dur2, avoid suggestions
-                    if is_chained_time_subtraction(cx, lhs) {
-                        span_lint(
-                            cx,
-                            UNCHECKED_TIME_SUBTRACTION,
-                            expr.span,
-                            "unchecked subtraction of a 'Duration' from an 'Instant'",
-                        );
-                    } else {
-                        // instant - duration
-                        print_unchecked_duration_subtraction_sugg(cx, lhs, rhs, expr);
-                    }
+                    print_unchecked_duration_subtraction_sugg(cx, lhs, rhs, expr);
                 }
-            } else if lhs_ty.is_diag_item(cx, sym::Duration)
+            }
+            // duration - duration
+            else if lhs_ty.is_diag_item(cx, sym::Duration)
                 && rhs_ty.is_diag_item(cx, sym::Duration)
                 && !expr.span.from_expansion()
                 && self.msrv.meets(cx, msrvs::TRY_FROM)
             {
-                // For chained subtraction like (dur1 - dur2) - dur3, avoid suggestions
-                if is_chained_time_subtraction(cx, lhs) {
-                    span_lint(
-                        cx,
-                        UNCHECKED_TIME_SUBTRACTION,
-                        expr.span,
-                        "unchecked subtraction between 'Duration' values",
-                    );
-                } else {
-                    // duration - duration
-                    print_unchecked_duration_subtraction_sugg(cx, lhs, rhs, expr);
-                }
+                print_unchecked_duration_subtraction_sugg(cx, lhs, rhs, expr);
             }
         }
     }
@@ -200,17 +180,20 @@ fn print_unchecked_duration_subtraction_sugg(
         "unchecked subtraction between 'Duration' values"
     };
 
-    let mut applicability = Applicability::MachineApplicable;
-    let left_sugg = Sugg::hir_with_applicability(cx, left_expr, "<left>", &mut applicability);
-    let right_sugg = Sugg::hir_with_applicability(cx, right_expr, "<right>", &mut applicability);
+    span_lint_and_then(cx, UNCHECKED_TIME_SUBTRACTION, expr.span, lint_msg, |diag| {
+        // For chained subtraction, like `(dur1 - dur2) - dur3` or `(instant - dur1) - dur2`,
+        // avoid suggestions
+        if !is_chained_time_subtraction(cx, left_expr) {
+            let mut applicability = Applicability::MachineApplicable;
+            let left_sugg = Sugg::hir_with_applicability(cx, left_expr, "<left>", &mut applicability);
+            let right_sugg = Sugg::hir_with_applicability(cx, right_expr, "<right>", &mut applicability);
 
-    span_lint_and_sugg(
-        cx,
-        UNCHECKED_TIME_SUBTRACTION,
-        expr.span,
-        lint_msg,
-        "try",
-        format!("{}.checked_sub({}).unwrap()", left_sugg.maybe_paren(), right_sugg),
-        applicability,
-    );
+            diag.span_suggestion(
+                expr.span,
+                "try",
+                format!("{}.checked_sub({}).unwrap()", left_sugg.maybe_paren(), right_sugg),
+                applicability,
+            );
+        }
+    });
 }
