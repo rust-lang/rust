8f1b6056abc212c6a07a08dc0981d17e03fbdd0f
cyrgani <ansgar.w.zielke@gmail.com>
2025-11-18 21:55:55 +0100

    separate module

--- library/proc_macro/src/bridge/standalone/mod.rs
+++ library/proc_macro/src/bridge/standalone/mod.rs
@@ -1,13 +1,13 @@
 #![warn(warnings)]
+mod parsing;
+
 use std::cell::{Cell, RefCell};
 use std::ops::{Bound, Range};
 
 use crate::bridge::client::Symbol;
 use crate::bridge::fxhash::FxHashMap;
-use crate::bridge::{
-    self, DelimSpan, Diagnostic, ExpnGlobals, Group, LitKind, Punct, TokenTree, server,
-};
-use crate::{Delimiter, LEGAL_PUNCT_CHARS, get_hashes_str};
+use crate::bridge::{self, DelimSpan, Diagnostic, ExpnGlobals, Group, Punct, TokenTree, server};
+use crate::{Delimiter, LEGAL_PUNCT_CHARS};
 
 type Result<T> = std::result::Result<T, ()>;
 type Literal = bridge::Literal<Span, Symbol>;
@@ -96,128 +96,6 @@ thread_local! {
     static TRACKED_ENV_VARS: RefCell<FxHashMap<String, Option<String>>> = RefCell::new(FxHashMap::default());
 }
 
-fn parse_maybe_raw_str(
-    mut s: &str,
-    raw_variant: fn(u8) -> LitKind,
-    regular_variant: LitKind,
-) -> Result<Literal> {
-    let mut hash_count = None;
-
-    if s.starts_with('r') {
-        s = s.strip_prefix('r').unwrap();
-        let mut h = 0;
-        for c in s.chars() {
-            if c == '#' {
-                if h == u8::MAX {
-                    return Err(());
-                }
-                h += 1;
-            } else {
-                break;
-            }
-        }
-        hash_count = Some(h);
-        let hashes = get_hashes_str(h);
-        s = s.strip_prefix(hashes).unwrap();
-        s = s.strip_suffix(hashes).ok_or(())?;
-    }
-    let sym = parse_plain_str(s)?;
-
-    Ok(make_literal(if let Some(h) = hash_count { raw_variant(h) } else { regular_variant }, sym))
-}
-
-fn parse_char(s: &str) -> Result<Literal> {
-    if let Some(s) = s.strip_circumfix('\'', '\'') {
-        if s.chars().count() == 1 {
-            Ok(make_literal(LitKind::Char, Symbol::new(s)))
-        } else {
-            Err(())
-        }
-    } else {
-        Err(())
-    }
-}
-
-fn parse_plain_str(mut s: &str) -> Result<Symbol> {
-    s = s.strip_prefix("\"").ok_or(())?.strip_suffix('\"').ok_or(())?;
-    Ok(Symbol::new(s))
-}
-
-const INT_SUFFIXES: &[&str] =
-    &["u8", "i8", "u16", "i16", "u32", "i32", "u64", "i64", "u128", "i128"];
-const FLOAT_SUFFIXES: &[&str] = &["f16", "f32", "f64", "f128"];
-
-fn parse_numeral(mut s: &str) -> Result<Literal> {
-    for suffix in INT_SUFFIXES {
-        if s.ends_with(suffix) {
-            return parse_integer(s);
-        }
-    }
-    let is_negative = s.starts_with('-');
-    let non_negative = s.trim_prefix('-');
-    if non_negative.starts_with("0b")
-        || non_negative.starts_with("0o")
-        || non_negative.starts_with("0x")
-    {
-        return parse_integer(s);
-    }
-    for suffix in FLOAT_SUFFIXES {
-        if s.ends_with(suffix) {
-            // return parse_float(s);
-        }
-    }
-    let (s, suffix) = strip_number_suffix(s, FLOAT_SUFFIXES);
-
-    Ok(Literal { kind: LitKind::Float, symbol: Symbol::new(s), suffix, span: Span })
-}
-
-fn parse_integer(symbol: &str) -> Result<Literal> {
-    let is_negative = symbol.starts_with('-');
-    let s = symbol.trim_prefix('-');
-
-    let (s, valid_chars) = if let Some(s) = s.strip_prefix("0b") {
-        (s, '0'..='1')
-    } else if let Some(s) = s.strip_prefix("0o") {
-        (s, '0'..='7')
-    } else if let Some(s) = s.strip_prefix("0x") {
-        (s, '0'..='F')
-    } else {
-        (s, '0'..='9')
-    };
-
-    let (s, suffix) = strip_number_suffix(s, INT_SUFFIXES);
-
-    let mut any_found = false;
-    for c in s.chars() {
-        if c == '_' {
-            continue;
-        }
-        if valid_chars.contains(&c) {
-            any_found = true;
-            continue;
-        }
-        return Err(());
-    }
-    if !any_found {
-        return Err(());
-    }
-
-    Ok(Literal { kind: LitKind::Integer, symbol: Symbol::new(symbol), suffix, span: Span })
-}
-
-fn strip_number_suffix<'a>(s: &'a str, suffixes: &[&str]) -> (&'a str, Option<Symbol>) {
-    for suf in suffixes {
-        if let Some(new_s) = s.strip_suffix(suf) {
-            return (new_s, Some(Symbol::new(suf)));
-        }
-    }
-    (s, None)
-}
-
-fn make_literal(kind: LitKind, symbol: Symbol) -> Literal {
-    Literal { kind, symbol, suffix: None, span: Span }
-}
-
 impl server::FreeFunctions for NoRustc {
     fn injected_env_var(&mut self, var: &str) -> Option<String> {
         TRACKED_ENV_VARS.with_borrow(|vars| vars.get(var)?.clone())
@@ -231,27 +109,7 @@ impl server::FreeFunctions for NoRustc {
     fn track_path(&mut self, _path: &str) {}
 
     fn literal_from_str(&mut self, s: &str) -> Result<Literal> {
-        let mut chars = s.chars();
-        let first = chars.next().ok_or(())?;
-        let rest = &s[1..];
-        match first {
-            'b' => {
-                if chars.next() == Some('\'') {
-                    parse_char(rest).map(|mut lit| {
-                        lit.kind = LitKind::Byte;
-                        lit
-                    })
-                } else {
-                    parse_maybe_raw_str(rest, LitKind::ByteStrRaw, LitKind::ByteStr)
-                }
-            }
-            'c' => parse_maybe_raw_str(rest, LitKind::CStrRaw, LitKind::CStr),
-            'r' => parse_maybe_raw_str(rest, LitKind::StrRaw, LitKind::Str),
-            '0'..='9' | '-' => parse_numeral(s),
-            '\'' => parse_char(s),
-            '"' => Ok(make_literal(LitKind::Str, parse_plain_str(s)?)),
-            _ => Err(()),
-        }
+        parsing::literal_from_str(s)
     }
 
     fn emit_diagnostic(&mut self, _: Diagnostic<Self::Span>) {
