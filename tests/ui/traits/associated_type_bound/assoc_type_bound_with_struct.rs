0c4db6701b09c003994f1d37c4db3ba023519dcb
shua <gpg@isthisa.email>
2025-11-19 13:57:25 +0100

    update name in tests: ui, mir-opt, std, llvm cg

--- tests/ui/traits/associated_type_bound/assoc_type_bound_with_struct.rs
+++ tests/ui/traits/associated_type_bound/assoc_type_bound_with_struct.rs
@@ -2,22 +2,22 @@ trait Bar {
     type Baz;
 }
 
-struct Foo<T> where T: Bar, <T as Bar>::Baz: String { //~ ERROR expected trait, found struct
+struct Foo<T> where T: Bar, <T as Bar>::Baz: String { //~ ERROR expected trait, found
     t: T,
 }
 
-struct Qux<'a, T> where T: Bar, <&'a T as Bar>::Baz: String { //~ ERROR expected trait, found struct
+struct Qux<'a, T> where T: Bar, <&'a T as Bar>::Baz: String { //~ ERROR expected trait, found
     t: &'a T,
 }
 
-fn foo<T: Bar>(_: T) where <T as Bar>::Baz: String { //~ ERROR expected trait, found struct
+fn foo<T: Bar>(_: T) where <T as Bar>::Baz: String { //~ ERROR expected trait, found
 }
 
 fn qux<'a, T: Bar>(_: &'a T) where <&'a T as Bar>::Baz: String { //~ ERROR expected trait, found
 }
 
 fn issue_95327() where <u8 as Unresolved>::Assoc: String {}
-//~^ ERROR expected trait, found struct
+//~^ ERROR expected trait, found
 //~| ERROR cannot find trait `Unresolved` in this scope
 
 fn main() {}
