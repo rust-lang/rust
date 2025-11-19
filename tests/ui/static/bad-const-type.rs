b292386fdc4ba8d837d40188c771b1cf91273579
shua <gpg@isthisa.email>
2025-11-19 13:57:25 +0100

    update name in tests: ui, mir-opt, std, llvm cg

--- tests/ui/static/bad-const-type.rs
+++ tests/ui/static/bad-const-type.rs
@@ -1,5 +1,6 @@
 static i: String = 10;
-//~^ ERROR: mismatched types
-//~| NOTE: expected `String`, found integer
+//~^ ERROR mismatched types
+//~| NOTE expected `String`, found integer
 //~| NOTE: expected because
+//~| NOTE expected struct `String`
 fn main() { println!("{}", i); }
