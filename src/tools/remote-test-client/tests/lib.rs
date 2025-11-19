444f1c534b03cd2fbd29521fafc332605d857b49
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-11-19 13:21:13 -0500

    Test the `remote-test-client` timeout

--- src/tools/remote-test-client/tests/lib.rs
+++ src/tools/remote-test-client/tests/lib.rs
@@ -8,3 +8,18 @@ fn test_help() {
     let stdout = String::from_utf8(output.stdout.clone()).unwrap();
     assert!(stdout.trim().starts_with("Usage:"));
 }
+
+#[test]
+fn test_timeout() {
+    let mut cmd = assert_cmd::cargo::cargo_bin_cmd!();
+    cmd.env("TEST_DEVICE_CONNECT_TIMEOUT_SECONDS", "1");
+    cmd.env("TEST_DEVICE_ADDR", "127.69.69.69:6969");
+    cmd.args(["spawn-emulator", "dummy-target", "dummy-server", "dummy-tmpdir"]);
+
+    let assert = cmd.assert().failure();
+    let output = assert.get_output();
+
+    let stderr = String::from_utf8(output.stderr.clone()).unwrap();
+    let pass_msg = "Gave up trying to connect to test device";
+    assert!(stderr.contains(pass_msg), "Could not find `{pass_msg}` in `{stderr}`");
+}
