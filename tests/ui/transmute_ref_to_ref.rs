profetia/rust-clippy

fix: `transmute_ptr_to_ptr` wrongly unmangled macros

tests/ui/transmute_ref_to_ref.rs
@@ -18,3 +18,23 @@ fn main() {
         //~^ transmute_ptr_to_ptr
     }
 }
+
+fn wronglg_unmangled_macros(make_ptr: fn() -> *const u32) {
+    macro_rules! call {
+        ($x:expr) => {
+            $x()
+        };
+    }
+    macro_rules! take_ref {
+        ($x:expr) => {
+            &$x
+        };
+    }
+
+    unsafe {
+        let _: *const f32 = std::mem::transmute(call!(make_ptr));
+        //~^ transmute_ptr_to_ptr
+        let _: &f32 = std::mem::transmute(take_ref!(1u32));
+        //~^ transmute_ptr_to_ptr
+    }
+}

rust-lang/rust

tests/ui/transmute_ref_to_ref.rs
@@ -18,3 +18,23 @@ fn main() {
         //~^ transmute_ptr_to_ptr
     }
 }
+
+fn issue16104(make_ptr: fn() -> *const u32) {
+    macro_rules! call {
+        ($x:expr) => {
+            $x()
+        };
+    }
+    macro_rules! take_ref {
+        ($x:expr) => {
+            &$x
+        };
+    }
+
+    unsafe {
+        let _: *const f32 = std::mem::transmute(call!(make_ptr));
+        //~^ transmute_ptr_to_ptr
+        let _: &f32 = std::mem::transmute(take_ref!(1u32));
+        //~^ transmute_ptr_to_ptr
+    }
+}