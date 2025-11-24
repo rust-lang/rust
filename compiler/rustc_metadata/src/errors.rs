de2408aeedea7ca8c9d0792c329b15aef7c2f5e7
Ariel Ben-Yehuda <me@arielby.net>
2025-11-24 13:56:38 +0000

    Implement `-Z allow-partial-mitigations` (RFC 3855)
    This implements `-Z allow-partial-mitigations` as an unstable option,
    currently with support for control-flow-guard and stack-protector.
    
    As a difference from the RFC, we have `-Z allow-partial-mitigations=!foo`
    rather than `-Z deny-partial-mitigations=foo`, since I couldn't find an easy
    way to have an allow/deny pair of flags where the latter flag wins.
    
    To allow for stabilization, this is only enabled starting from the next edition. Maybe a
    better policy is possible (bikeshed).

--- compiler/rustc_metadata/src/errors.rs
+++ compiler/rustc_metadata/src/errors.rs
@@ -696,3 +696,21 @@ pub(crate) struct UnusedCrateDependency {
     pub extern_crate: Symbol,
     pub local_crate: Symbol,
 }
+
+#[derive(Diagnostic)]
+#[diag(
+    "your program uses the crate `{$extern_crate}`, that is not compiled with `{$mitigation_name}{$mitigation_level}` enabled"
+)]
+#[note(
+    "recompile `{$extern_crate}` with `{$mitigation_name}{$mitigation_level}` enabled, or use `-Z allow-partial-mitigations={$mitigation_name}` to allow creating an artifact that has the mitigation only partially enabled "
+)]
+#[help(
+    "it is possible to disable `-Z allow-partial-mitigations={$mitigation_name}` via `-Z deny-partial-mitigations={$mitigation_name}`"
+)]
+pub struct MitigationLessStrictInDependency {
+    #[primary_span]
+    pub span: Span,
+    pub mitigation_name: String,
+    pub mitigation_level: String,
+    pub extern_crate: Symbol,
+}
