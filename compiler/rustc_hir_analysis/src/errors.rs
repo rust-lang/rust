f0b30c4336ec0441debe2c2120b8364b9f45babf
Frank King <frankking1729@gmail.com>
2025-11-24 15:18:59 +0800

    Forbid manual `Unpin` impls for structurally pinned types

--- compiler/rustc_hir_analysis/src/errors.rs
+++ compiler/rustc_hir_analysis/src/errors.rs
@@ -1708,3 +1708,14 @@ pub(crate) struct AsyncDropWithoutSyncDrop {
     #[primary_span]
     pub span: Span,
 }
+
+#[derive(Diagnostic)]
+#[diag(hir_analysis_impl_unpin_for_pin_projected_type)]
+pub(crate) struct ImplUnpinForPinProjectedType {
+    #[primary_span]
+    #[label]
+    pub span: Span,
+    #[help]
+    pub adt_span: Span,
+    pub adt_name: Symbol,
+}
