372e20d1a9c36de6e8b404c6f132d1a5648838e7
Simon Gellis <simongellis@gmail.com>
2025-11-25 08:45:01 -0500

    Fix lints

--- tests/rustdoc-ui/lints/check.rs
+++ tests/rustdoc-ui/lints/check.rs
@@ -1,6 +1,6 @@
 //@ check-pass
 //@ compile-flags: -Z unstable-options --check
-//@ normalize-stderr: "nightly|beta|1\.[0-9][0-9]\.[0-9]" -> "$$CHANNEL"
+//@ normalize-stderr: "stable|nightly|beta|1\.[0-9][0-9]\.[0-9]" -> "$$CHANNEL"
 
 #![feature(rustdoc_missing_doc_code_examples)] //~ WARN no documentation found for this crate's top-level module
 //~^ WARN
--- tests/rustdoc-ui/lints/no-crate-level-doc-lint.rs
+++ tests/rustdoc-ui/lints/no-crate-level-doc-lint.rs
@@ -1,4 +1,4 @@
-//@ normalize-stderr: "nightly|beta|1\.[0-9][0-9]\.[0-9]" -> "$$CHANNEL"
+//@ normalize-stderr: "stable|nightly|beta|1\.[0-9][0-9]\.[0-9]" -> "$$CHANNEL"
 #![deny(rustdoc::missing_crate_level_docs)] //~ ERROR no documentation found for this crate's top-level module
 //^~ NOTE defined here
 
