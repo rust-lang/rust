57b57b8b56e5d709dd749ecea0129147801f74e1
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-11-27 19:30:17 +0000

    Improve test

--- src/tools/miri/tests/pass/target-cpu-implies-target-feature.rs
+++ src/tools/miri/tests/pass/target-cpu-implies-target-feature.rs
@@ -3,10 +3,14 @@
 //@compile-flags: -C target-cpu=x86-64-v4
 
 fn main() {
+    assert!(cfg!(target_feature = "avx2"));
+    assert!(cfg!(target_feature = "avx512bw"));
+    assert!(cfg!(target_feature = "avx512cd"));
+    assert!(cfg!(target_feature = "avx512dq"));
+    assert!(cfg!(target_feature = "avx512f"));
+    assert!(cfg!(target_feature = "avx512vl"));
     assert!(is_x86_feature_detected!("avx512bw"));
-    assert!(is_x86_feature_detected!("avx512cd"));
-    assert!(is_x86_feature_detected!("avx512dq"));
-    assert!(is_x86_feature_detected!("avx512f"));
-    assert!(is_x86_feature_detected!("avx512vl"));
+
+    assert!(cfg!(not(target_feature = "avx512vpopcntdq")));
     assert!(!is_x86_feature_detected!("avx512vpopcntdq"));
 }
