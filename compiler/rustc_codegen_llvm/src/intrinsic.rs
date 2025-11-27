b75ce434ba2e82192faba95eb4fc1a1f7889aa1b
Ralf Jung <post@ralfj.de>
2025-11-27 17:49:24 +0100

    Revert "use fallback impl in LLVM backend"
    This reverts commit a00db66b007ebee4815c9d821b39c5ba2a83df08.

--- compiler/rustc_codegen_llvm/src/intrinsic.rs
+++ compiler/rustc_codegen_llvm/src/intrinsic.rs
@@ -401,6 +401,8 @@ impl<'ll, 'tcx> IntrinsicCallBuilderMethods<'tcx> for Builder<'_, 'll, 'tcx> {
             | sym::ctpop
             | sym::bswap
             | sym::bitreverse
+            | sym::rotate_left
+            | sym::rotate_right
             | sym::saturating_add
             | sym::saturating_sub
             | sym::unchecked_funnel_shl
@@ -445,11 +447,19 @@ impl<'ll, 'tcx> IntrinsicCallBuilderMethods<'tcx> for Builder<'_, 'll, 'tcx> {
                     sym::bitreverse => {
                         self.call_intrinsic("llvm.bitreverse", &[llty], &[args[0].immediate()])
                     }
-                    sym::unchecked_funnel_shl | sym::unchecked_funnel_shr => {
-                        let is_left = name == sym::unchecked_funnel_shl;
+                    sym::rotate_left
+                    | sym::rotate_right
+                    | sym::unchecked_funnel_shl
+                    | sym::unchecked_funnel_shr => {
+                        let is_left = name == sym::rotate_left || name == sym::unchecked_funnel_shl;
                         let lhs = args[0].immediate();
-                        let rhs = args[1].immediate();
-                        let raw_shift = args[2].immediate();
+                        let (rhs, raw_shift) =
+                            if name == sym::rotate_left || name == sym::rotate_right {
+                                // rotate = funnel shift with first two args the same
+                                (lhs, args[1].immediate())
+                            } else {
+                                (args[1].immediate(), args[2].immediate())
+                            };
                         let llvm_name = format!("llvm.fsh{}", if is_left { 'l' } else { 'r' });
 
                         // llvm expects shift to be the same type as the values, but rust
