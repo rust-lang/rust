073f5e29f11092aa0635ba79e66ec8a3eac33e65
reddevilmidzy <midzy0228@gmail.com>
2025-12-02 11:25:36 +0900

    Updated `tests/ui/for-loop-while/iter-from-mac-call.rs`

--- tests/ui/for-loop-while/iter-from-mac-call.rs
+++ tests/ui/for-loop-while/iter-from-mac-call.rs
@@ -1,13 +1,12 @@
+//! Tests for trait/type errors when dereferencing via macro in a for loop.
+
 macro_rules! deref {
-    ($e:expr) => { *$e };
+    ($e:expr) => {
+        *$e
+    };
 }
 
-fn f1<'a>(mut iter: Box<dyn Iterator<Item=&'a mut u8>>) {
-    for item in deref!(iter) { *item = 0 }
-    //~^ ERROR `dyn Iterator<Item = &'a mut u8>` is not an iterator
-}
-
-fn f2(x: &mut i32) {
+fn f1(x: &mut i32) {
     for _item in deref!(x) {}
     //~^ ERROR `i32` is not an iterator
 }
@@ -15,12 +14,16 @@ fn f2(x: &mut i32) {
 struct Wrapped(i32);
 
 macro_rules! borrow_deref {
-    ($e:expr) => { &mut *$e };
+    ($e:expr) => {
+        &mut *$e
+    };
 }
 
-fn f3<'a>(mut iter: Box<dyn Iterator<Item=&'a mut i32>>) {
-    for Wrapped(item) in borrow_deref!(iter) { *item = 0 }
-    //~^ ERROR mismatched types
+fn f2<'a>(mut iter: Box<dyn Iterator<Item = &'a mut i32>>) {
+    for Wrapped(item) in borrow_deref!(iter) {
+        //~^ ERROR mismatched types
+        *item = 0
+    }
 }
 
 fn main() {}
