dfee0dad260cf22efd0d56e0b07021d0fc86a564
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2025-12-05 18:06:42 +0000

    Add test for misspelled feature name

--- tests/ui/feature-gates/unknown-feature.rs
+++ tests/ui/feature-gates/unknown-feature.rs
@@ -1,3 +1,14 @@
-#![feature(unknown_rust_feature)] //~ ERROR unknown feature
+#![feature(
+    unknown_rust_feature,
+    //~^ ERROR unknown feature
+    
+    // Typo for lang feature
+    associated_types_default,
+    //~^ ERROR unknown feature
+
+    // Typo for lib feature
+    core_intrnisics,
+    //~^ ERROR unknown feature
+)]
 
 fn main() {}
