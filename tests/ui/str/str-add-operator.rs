73ef996206aafdf416377e48d47bc2f2688ec3f7
reddevilmidzy <midzy0228@gmail.com>
2025-12-05 20:08:58 +0900

    Merged tests/ui/str/str-add-operator-2.rs with tests/ui/str/str-add-operator.rs

--- tests/ui/str/str-add-operator.rs
+++ tests/ui/str/str-add-operator.rs
@@ -1,6 +1,11 @@
+//! regression test for issue #47377, #47380
 // ignore-tidy-tab
 fn main() {
  	let b = "hello";
  	let _a = b + ", World!";
  	//~^ ERROR E0369
+
+    let b = "hello";
+    println!("🦀🦀🦀🦀🦀"); let _a = b + ", World!";
+    //~^ ERROR E0369
 }
