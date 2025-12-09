4b376b794fdd7e92c0e1b04aa56b64403f2310e7
Daria Sukhonina <dariasukhonina@gmail.com>
2025-12-09 17:30:06 +0300

    Finish draft impl of new cycle detection

--- compiler/rustc_data_structures/src/sync/branch_key.rs
+++ compiler/rustc_data_structures/src/sync/branch_key.rs
@@ -1,6 +1,6 @@
 use std::cmp;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct BranchKey(u128);
 
 impl BranchKey {
@@ -8,16 +8,14 @@ impl BranchKey {
         Self(0x80000000_00000000_00000000_00000000)
     }
 
-    fn bits_branch(self, branch_num: u128, bits: u32) -> BranchKey {
-        let trailing_zeroes = self.0.trailing_zeros();
-        let allocated_shift = trailing_zeroes
-            .checked_sub(bits)
-            .unwrap_or_else(|| panic!("query branch space is exhausted to fit {bits} bits"));
-        BranchKey(
-            self.0 & !(1 << trailing_zeroes)
+    fn bits_branch(self, branch_num: u128, bits: u32) -> Result<Self, BranchNestingError> {
+        let trailing_zeros = self.0.trailing_zeros();
+        let allocated_shift = trailing_zeros.checked_sub(bits).ok_or(BranchNestingError(()))?;
+        Ok(BranchKey(
+            self.0 & !(1 << trailing_zeros)
                 | (1 << allocated_shift)
                 | (branch_num << (allocated_shift + 1)),
-        )
+        ))
     }
 
     pub fn branch(self, branch_num: u128, branch_space: u128) -> BranchKey {
@@ -27,12 +25,23 @@ impl BranchKey {
         );
         // floor(log2(n - 1)) + 1 == ceil(log2(n))
         self.bits_branch(branch_num, (branch_space - 1).checked_ilog2().map_or(0, |b| b + 1))
+            .expect("query branch space is exhausted")
     }
 
     pub fn disjoint_cmp(self, other: Self) -> cmp::Ordering {
         self.0.cmp(&other.0)
     }
+
+    pub fn nest(self, then: Self) -> Result<Self, BranchNestingError> {
+        let trailing_zeros = then.0.trailing_zeros();
+        let branch_num = then.0.wrapping_shr(trailing_zeros + 1);
+        let bits = u128::BITS - trailing_zeros;
+        self.bits_branch(branch_num, bits)
+    }
 }
+
+#[derive(Debug)]
+pub struct BranchNestingError(());
 
 impl Default for BranchKey {
     fn default() -> Self {
