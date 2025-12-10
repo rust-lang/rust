039e1c0d115c65f1cc611d4f070c6fe2f1a4278e
joboet <jonasboettiger@icloud.com>
2025-12-11 00:31:46 +0100

    std: avoid tearing `dbg!` prints

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -469,7 +469,9 @@ extern crate std as realstd;
 
 // The standard macros that are not built-in to the compiler.
 #[macro_use]
-mod macros;
+#[doc(hidden)]
+#[unstable(feature = "std_internals", issue = "none")]
+pub mod macros;
 
 // The runtime entry point and a few unstable public functions used by the
 // compiler
