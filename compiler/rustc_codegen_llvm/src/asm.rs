d6e7c7d8f10b0e46ceb7205ef42f17a4a522c350
Flakebi <flakebi@t-online.de>
2025-12-11 02:47:45 +0100

    Add inline asm support for amdgpu
    Add support for inline assembly for the amdgpu backend (the
    amdgcn-amd-amdhsa target).
    Add register classes for `vgpr` (vector general purpose register) and
    `sgpr` (scalar general purpose register).
    The LLVM backend supports two more classes, `reg`, which is either VGPR
    or SGPR, up to the compiler to decide. As instructions often rely on a
    register being either a VGPR or SGPR for the assembly to be valid, reg
    doesn’t seem that useful (I struggled to write correct tests for it), so
    I didn’t end up adding it.
    The fourth register class is AGPRs, which only exist on some hardware
    versions (not the consumer ones) and they have restricted ways to write
    and read from them, which makes it hard to write a Rust variable into
    them. They could be used inside assembly blocks, but I didn’t add them
    as Rust register class.
    
    There is one change affecting general inline assembly code, that is
    `InlineAsmReg::name()` now returns a `Cow` instead of a `&'static str`.
    Because amdgpu has many registers, 256 VGPRs plus combinations of 2 or 4
    VGPRs, and I didn’t want to list hundreds of static strings, the amdgpu
    reg stores the register number(s) and a non-static String is generated
    at runtime for the register name.

--- compiler/rustc_codegen_llvm/src/asm.rs
+++ compiler/rustc_codegen_llvm/src/asm.rs
@@ -229,6 +229,7 @@ impl<'ll, 'tcx> AsmBuilderMethods<'tcx> for Builder<'_, 'll, 'tcx> {
                 InlineAsmArch::AArch64 | InlineAsmArch::Arm64EC | InlineAsmArch::Arm => {
                     constraints.push("~{cc}".to_string());
                 }
+                InlineAsmArch::Amdgpu => {}
                 InlineAsmArch::X86 | InlineAsmArch::X86_64 => {
                     constraints.extend_from_slice(&[
                         "~{dirflag}".to_string(),
@@ -645,6 +646,7 @@ fn reg_to_llvm(reg: InlineAsmRegOrRegClass, layout: Option<&TyAndLayout<'_>>) ->
             | Arm(ArmInlineAsmRegClass::dreg_low8)
             | Arm(ArmInlineAsmRegClass::qreg_low4) => "x",
             Arm(ArmInlineAsmRegClass::dreg) | Arm(ArmInlineAsmRegClass::qreg) => "w",
+            Amdgpu(class) => class.prefix(),
             Hexagon(HexagonInlineAsmRegClass::reg) => "r",
             Hexagon(HexagonInlineAsmRegClass::preg) => unreachable!("clobber-only"),
             LoongArch(LoongArchInlineAsmRegClass::reg) => "r",
@@ -746,6 +748,7 @@ fn modifier_to_llvm(
                 modifier
             }
         }
+        Amdgpu(_) => None,
         Hexagon(_) => None,
         LoongArch(_) => None,
         Mips(_) => None,
@@ -826,6 +829,7 @@ fn dummy_output_type<'ll>(cx: &CodegenCx<'ll, '_>, reg: InlineAsmRegClass) -> &'
         Arm(ArmInlineAsmRegClass::qreg)
         | Arm(ArmInlineAsmRegClass::qreg_low8)
         | Arm(ArmInlineAsmRegClass::qreg_low4) => cx.type_vector(cx.type_i64(), 2),
+        Amdgpu(_) => cx.type_i32(),
         Hexagon(HexagonInlineAsmRegClass::reg) => cx.type_i32(),
         Hexagon(HexagonInlineAsmRegClass::preg) => unreachable!("clobber-only"),
         LoongArch(LoongArchInlineAsmRegClass::reg) => cx.type_i32(),
