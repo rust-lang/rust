3062f507aac136c8bb442bf513d7fbf0849bf184
jyn <github@jyn.dev>
2025-12-13 10:25:11 -0500

    set messages=deny for crater

--- compiler/rustc_lint_defs/src/builtin.rs
+++ compiler/rustc_lint_defs/src/builtin.rs
@@ -4011,7 +4011,7 @@ declare_lint! {
     /// and actionable warning of similar quality to our other diagnostics. See this tracking
     /// issue for more details: <https://github.com/rust-lang/rust/issues/136096>.
     pub LINKER_MESSAGES,
-    Warn,
+    Deny,
     "warnings emitted at runtime by the target-specific linker program"
 }
 
