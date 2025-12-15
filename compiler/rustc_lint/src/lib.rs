a02a37f19054d8276c798762f13b00ef12ac47a0
xonx4l <sk797119@gmail.com>
2025-12-16 02:51:07 +0530

    feat:lint-black-box

--- compiler/rustc_lint/src/lib.rs
+++ compiler/rustc_lint/src/lib.rs
@@ -200,6 +200,7 @@ late_lint_methods!(
             InvalidFromUtf8: InvalidFromUtf8,
             VariantSizeDifferences: VariantSizeDifferences,
             PathStatements: PathStatements,
+            BlackBoxZstCalls: BlackBoxZstCalls,
             LetUnderscore: LetUnderscore,
             InvalidReferenceCasting: InvalidReferenceCasting,
             ImplicitAutorefs: ImplicitAutorefs,
