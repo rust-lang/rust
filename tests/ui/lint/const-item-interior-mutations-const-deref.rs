0aa11b5dd2abf908fc47c1d8d13abfa8b78c6117
Urgau <urgau@numericable.fr>
2025-12-19 17:55:19 +0100

    Add regression test for `const_item_interior_mutations` deref FP
    (cherry picked from commit 0bc29cec45412ee3cc68a1ef1556814885543d55)

---
+++ tests/ui/lint/const-item-interior-mutations-const-deref.rs
@@ -0,0 +1,30 @@
+// Regression test for <https://github.com/rust-lang/rust/issues/150157>
+//
+// We shouldn't lint on user types, including through deref.
+
+//@ check-pass
+
+use std::cell::Cell;
+use std::ops::Deref;
+
+// Cut down version of the issue reproducer without the thread local to just a Deref
+pub struct LocalKey<T> {
+    inner: T,
+}
+
+impl<T> Deref for LocalKey<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+const LOCAL_COUNT: LocalKey<Cell<usize>> = LocalKey { inner: Cell::new(8) };
+
+fn main() {
+    let count = LOCAL_COUNT.get();
+    //~^ WARN mutation of an interior mutable `const`
+    LOCAL_COUNT.set(count);
+    //~^ WARN mutation of an interior mutable `const`
+}
