a3d8159b7431ce59c9c62952e906099a734fef65
Imad <imdrdwn74@gmail.com>
2025-12-20 18:05:50 +0330

    parser: emit proper diagnostic for gen fn in fn pointer type - fixed missing import

--- compiler/rustc_parse/src/parser/item.rs
+++ compiler/rustc_parse/src/parser/item.rs
@@ -25,7 +25,7 @@ use super::{
     AttrWrapper, ExpKeywordPair, ExpTokenPair, FollowedByType, ForceCollect, Parser, PathStyle,
     Recovered, Trailing, UsePreAttrPos,
 };
-use crate::errors::{self, FnPointerCannotBeAsync, FnPointerCannotBeConst, MacroExpandsToAdtField};
+use crate::errors::{self, FnPointerCannotBeGen, FnPointerCannotBeAsync, FnPointerCannotBeConst, MacroExpandsToAdtField};
 use crate::{exp, fluent_generated as fluent};
 
 impl<'a> Parser<'a> {
