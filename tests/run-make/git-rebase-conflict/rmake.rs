aaff57082e680378c34ea4add7f7c3a250999ed1
Esteban Küber <esteban@kuber.com.ar>
2025-12-23 16:53:57 +0000

    Add the necessary `.git` contents to the tests

--- tests/run-make/git-rebase-conflict/rmake.rs
+++ tests/run-make/git-rebase-conflict/rmake.rs
@@ -5,10 +5,11 @@
 // that the standard error matches what is expected.
 // See https://github.com/rust-lang/rust/issues/66530
 
-use run_make_support::{diff, rustc};
+use run_make_support::{diff, rfs, rustc};
 
 fn main() {
     // We use `.rust` purely to avoid `fmt` check on a file with conflict markers
+    rfs::copy_dir_all("git", ".git");
     let file_out = rustc().input("main.rust").run_fail().stderr_utf8();
     diff().expected_file("file.stderr").actual_text("actual-file-stderr", file_out).run();
 }
