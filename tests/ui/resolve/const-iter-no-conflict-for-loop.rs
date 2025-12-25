1861011faa18ae2010cee90f52ac67f8a37173ff
reddevilmidzy <midzy0228@gmail.com>
2025-12-26 00:38:07 +0900

    add comment to const-iter-no-conflict-for-loop.rs

--- tests/ui/resolve/const-iter-no-conflict-for-loop.rs
+++ tests/ui/resolve/const-iter-no-conflict-for-loop.rs
@@ -1,3 +1,7 @@
+//! regression test for <https://github.com/rust-lang/rust/issues/27639>
+//! Ensure that a constant named `iter` does not
+//! interfere with the name resolution of the `iter` methods used internally
+//! by `for` loops
 //@ run-pass
 #![allow(dead_code)]
 #![allow(non_upper_case_globals)]
