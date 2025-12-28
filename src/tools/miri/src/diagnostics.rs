b3bdf0f0107fb951e83c4f44031e8dbd3cf8d788
Ralf Jung <post@ralfj.de>
2025-12-29 00:54:17 +0100

    tail calls: fix copying non-scalar arguments to callee

--- src/tools/miri/src/diagnostics.rs
+++ src/tools/miri/src/diagnostics.rs
@@ -493,7 +493,7 @@ pub fn report_result<'tcx>(
     for (i, frame) in ecx.active_thread_stack().iter().enumerate() {
         trace!("-------------------");
         trace!("Frame {}", i);
-        trace!("    return: {:?}", frame.return_place);
+        trace!("    return: {:?}", frame.return_place());
         for (i, local) in frame.locals.iter().enumerate() {
             trace!("    local {}: {:?}", i, local);
         }
