a4270ff0ce045e047a0b60fa6a3080af0d87b46e
Rémy Rakic <remy.rakic+github@gmail.com>
2025-12-29 23:25:03 +0000

    remove eager constraint conversion

--- compiler/rustc_borrowck/src/nll.rs
+++ compiler/rustc_borrowck/src/nll.rs
@@ -154,7 +154,7 @@ pub(crate) fn compute_regions<'tcx>(
     // If requested for `-Zpolonius=next`, convert NLL constraints to localized outlives constraints
     // and use them to compute loan liveness.
     let polonius_diagnostics = polonius_context.map(|polonius_context| {
-        polonius_context.compute_loan_liveness(infcx.tcx, &mut regioncx, body, borrow_set)
+        polonius_context.compute_loan_liveness(&mut regioncx, body, borrow_set)
     });
 
     // If requested: dump NLL facts, and run legacy polonius analysis.
