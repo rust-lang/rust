77552df630b00500f17d70f63e5ca3c4d8e97839
Benno Lossin <benno.lossin@proton.me>
2026-01-01 17:11:09 +0100

    test incoherent impls

---
+++ tests/ui/field_representing_types/incoherent-impl.rs
@@ -0,0 +1,28 @@
+//@ revisions: old next
+//@ [next] compile-flags: -Znext-solver
+//@ aux-build:extern-crate.rs
+#![allow(incomplete_features)]
+#![feature(field_projections)]
+extern crate extern_crate;
+
+use std::field::field_of;
+
+use extern_crate::{ForeignTrait, Point};
+
+pub trait MyTrait {}
+
+impl MyTrait for field_of!(Point, x) {}
+impl MyTrait for field_of!(Player, pos) {}
+impl MyTrait for field_of!((usize, usize), 0) {}
+
+pub struct Player {
+    pos: Point,
+}
+
+impl ForeignTrait for field_of!(Point, x) {}
+//~^ ERROR: only traits defined in the current crate can be implemented for arbitrary types [E0117]
+impl ForeignTrait for field_of!(Player, pos) {}
+impl ForeignTrait for field_of!((usize, usize), 0) {}
+//~^ ERROR: only traits defined in the current crate can be implemented for arbitrary types [E0117]
+
+fn main() {}
