4d2ce7d36b7c96f1778621a6b0865bcaa8872c54
Ralf Jung <post@ralfj.de>
2026-01-02 18:09:01 +0100

    use a single generic codepath for all return types

--- src/tools/miri/tests/native-lib/pass/aggregate_arguments.rs
+++ src/tools/miri/tests/native-lib/pass/aggregate_arguments.rs
@@ -1,6 +1,7 @@
 fn main() {
     test_pass_struct();
     test_pass_struct_complex();
+    test_return_struct();
 }
 
 /// Test passing a basic struct as an argument.
@@ -18,6 +19,23 @@ fn test_pass_struct() {
 
     let pass_me = PassMe { value: 42, other_value: 1337 };
     assert_eq!(unsafe { pass_struct(pass_me) }, 42 + 1337);
+}
+
+fn test_return_struct() {
+    // Exactly two fields, so that we hit the ScalarPair case.
+    #[repr(C)]
+    struct PassMe {
+        value: i32,
+        other_value: i64,
+    }
+
+    extern "C" {
+        fn return_struct(v: i32, ov: i64) -> PassMe;
+    }
+
+    let pass_me = unsafe { return_struct(1, 2) };
+    assert_eq!(pass_me.value, 1);
+    assert_eq!(pass_me.other_value, 2);
 }
 
 /// Test passing a more complex struct as an argument.
