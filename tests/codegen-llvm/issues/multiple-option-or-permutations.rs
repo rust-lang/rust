a2fcb0de187664d27aa6c312585bcf64ff1c909a
Ryan Ward <ryan.ward17@outlook.com>
2026-01-03 12:50:38 +1030

    fix: add `CHECK` directives to `ret` comments and be more pervasive with directive contents

--- tests/codegen-llvm/issues/multiple-option-or-permutations.rs
+++ tests/codegen-llvm/issues/multiple-option-or-permutations.rs
@@ -10,11 +10,11 @@ use std::num::NonZero;
 #[no_mangle]
 pub fn or_match_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
     // CHECK: start:
-    // CHECK-DAG: select i1 %0
-    // CHECK-DAG: or i1 %0
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // ret { i1, i8 }
+    // CHECK-DAG: [[A_OR_B:%.+]] = select i1 %0, i8 %1, i8 %optb.1
+    // CHECK-DAG: [[IS_SOME:%.+]] = or i1 {{%0, %optb.0|%optb.0, %0}}
+    // CHECK-NEXT: [[FLAG:%.+]] = insertvalue { i1, i8 } poison, i1 [[IS_SOME]], 0
+    // CHECK-NEXT: [[R:%.+]] = insertvalue { i1, i8 } [[FLAG]], i8 [[A_OR_B]], 1
+    // CHECK: ret { i1, i8 } [[R]]
     match opta {
         Some(x) => Some(x),
         None => optb,
@@ -26,11 +26,11 @@ pub fn or_match_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
 #[no_mangle]
 pub fn or_match_alt_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
     // CHECK: start:
-    // CHECK-DAG: select i1 %opta.0, i8 %opta.1, i8 %optb.1
-    // CHECK-DAG: or i1 {{%opta.0, %optb.0|%optb.0, %opta.0}}
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // ret { i1, i8 }
+    // CHECK-DAG: [[A_OR_B:%.+]] = select i1 %opta.0, i8 %opta.1, i8 %optb.1
+    // CHECK-DAG: [[IS_SOME:%.+]] = or i1 {{%opta.0, %optb.0|%optb.0, %opta.0}}
+    // CHECK-NEXT: [[FLAG:%.+]] = insertvalue { i1, i8 } poison, i1 [[IS_SOME]], 0
+    // CHECK-NEXT: [[R:%.+]] = insertvalue { i1, i8 } [[FLAG]], i8 [[A_OR_B]], 1
+    // CHECK: ret { i1, i8 } [[R]]
     match opta {
         Some(_) => opta,
         None => optb,
@@ -42,11 +42,11 @@ pub fn or_match_alt_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
 #[no_mangle]
 pub fn option_or_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
     // CHECK: start:
-    // CHECK-DAG: select i1
-    // CHECK-DAG: or i1
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // ret { i1, i8 }
+    // CHECK-DAG: [[A_OR_B:%.+]] = select i1 %opta.0, i8 %opta.1, i8 %optb.1
+    // CHECK-DAG: [[IS_SOME:%.+]] = or i1 {{%opta.0, %optb.0|%optb.0, %opta.0}}
+    // CHECK-NEXT: [[FLAG:%.+]] = insertvalue { i1, i8 } poison, i1 [[IS_SOME]], 0
+    // CHECK-NEXT: [[R:%.+]] = insertvalue { i1, i8 } [[FLAG]], i8 [[A_OR_B]], 1
+    // CHECK: ret { i1, i8 } [[R]]
     opta.or(optb)
 }
 
@@ -55,11 +55,11 @@ pub fn option_or_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
 #[no_mangle]
 pub fn if_some_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
     // CHECK: start:
-    // CHECK-DAG: select i1
-    // CHECK-DAG: or i1
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // CHECK-NEXT: insertvalue { i1, i8 }
-    // ret { i1, i8 }
+    // CHECK-DAG: [[A_OR_B:%.+]] = select i1 %opta.0, i8 %opta.1, i8 %optb.1
+    // CHECK-DAG: [[IS_SOME:%.+]] = or i1 {{%opta.0, %optb.0|%optb.0, %opta.0}}
+    // CHECK-NEXT: [[FLAG:%.+]] = insertvalue { i1, i8 } poison, i1 [[IS_SOME]], 0
+    // CHECK-NEXT: [[R:%.+]] = insertvalue { i1, i8 } [[FLAG]], i8 [[A_OR_B]], 1
+    // CHECK: ret { i1, i8 } [[R]]
     if opta.is_some() { opta } else { optb }
 }
 
@@ -70,9 +70,9 @@ pub fn if_some_u8(opta: Option<u8>, optb: Option<u8>) -> Option<u8> {
 #[no_mangle]
 pub fn or_match_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option<[u8; 1]> {
     // CHECK: start:
-    // CHECK-NEXT: trunc i16 %0 to i1
-    // CHECK-NEXT: select i1 %2, i16 %0, i16 %1
-    // ret i16
+    // CHECK-NEXT: [[SOME_A:%.+]] = trunc i16 %0 to i1
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[SOME_A]], i16 %0, i16 %1
+    // CHECK: ret i16 [[R]]
     match opta {
         Some(x) => Some(x),
         None => optb,
@@ -84,9 +84,9 @@ pub fn or_match_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option
 #[no_mangle]
 pub fn or_match_slice_alt_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option<[u8; 1]> {
     // CHECK: start:
-    // CHECK-NEXT: trunc i16 %0 to i1
-    // CHECK-NEXT: select i1 %2, i16 %0, i16 %1
-    // ret i16
+    // CHECK-NEXT: [[SOME_A:%.+]] = trunc i16 %0 to i1
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[SOME_A]], i16 %0, i16 %1
+    // CHECK: ret i16 [[R]]
     match opta {
         Some(_) => opta,
         None => optb,
@@ -98,9 +98,9 @@ pub fn or_match_slice_alt_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Op
 #[no_mangle]
 pub fn option_or_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option<[u8; 1]> {
     // CHECK: start:
-    // CHECK-NEXT: trunc i16 %0 to i1
-    // CHECK-NEXT: select i1 %2, i16 %0, i16 %1
-    // ret i16
+    // CHECK-NEXT: [[SOME_A:%.+]] = trunc i16 %0 to i1
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[SOME_A]], i16 %0, i16 %1
+    // CHECK: ret i16 [[R]]
     opta.or(optb)
 }
 
@@ -109,9 +109,9 @@ pub fn option_or_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Optio
 #[no_mangle]
 pub fn if_some_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option<[u8; 1]> {
     // CHECK: start:
-    // CHECK-NEXT: trunc i16 %0 to i1
-    // CHECK-NEXT: select i1 %2, i16 %0, i16 %1
-    // ret i16
+    // CHECK-NEXT: [[SOME_A:%.+]] = trunc i16 %0 to i1
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[SOME_A]], i16 %0, i16 %1
+    // CHECK: ret i16 [[R]]
     if opta.is_some() { opta } else { optb }
 }
 
@@ -122,9 +122,9 @@ pub fn if_some_slice_u8(opta: Option<[u8; 1]>, optb: Option<[u8; 1]>) -> Option<
 #[no_mangle]
 pub fn or_match_nz_u8(opta: Option<NonZero<u8>>, optb: Option<NonZero<u8>>) -> Option<NonZero<u8>> {
     // CHECK: start:
-    // CHECK-NEXT: [[NOT_A:%.*]] = icmp eq i8 %0, 0
-    // CHECK-NEXT: select i1 [[NOT_A]], i8 %optb, i8 %0
-    // ret i8
+    // CHECK-NEXT: [[NOT_A:%.+]] = icmp eq i8 %0, 0
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[NOT_A]], i8 %optb, i8 %0
+    // CHECK: ret i8 [[R]]
     match opta {
         Some(x) => Some(x),
         None => optb,
@@ -139,9 +139,9 @@ pub fn or_match_alt_nz_u8(
     optb: Option<NonZero<u8>>,
 ) -> Option<NonZero<u8>> {
     // CHECK: start:
-    // CHECK-NEXT: [[NOT_A:%.*]] = icmp eq i8 %opta, 0
-    // CHECK-NEXT: select i1 [[NOT_A]], i8 %optb, i8 %opta
-    // ret i8
+    // CHECK-NEXT: [[NOT_A:%.+]] = icmp eq i8 %opta, 0
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[NOT_A]], i8 %optb, i8 %opta
+    // CHECK: ret i8 [[R]]
     match opta {
         Some(_) => opta,
         None => optb,
@@ -156,9 +156,9 @@ pub fn option_or_nz_u8(
     optb: Option<NonZero<u8>>,
 ) -> Option<NonZero<u8>> {
     // CHECK: start:
-    // CHECK-NEXT: [[NOT_A:%.*]] = icmp eq i8 %opta, 0
-    // CHECK-NEXT: select i1 [[NOT_A]], i8 %optb, i8 %opta
-    // ret i8
+    // CHECK-NEXT: [[NOT_A:%.+]] = icmp eq i8 %opta, 0
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[NOT_A]], i8 %optb, i8 %opta
+    // CHECK: ret i8 [[R]]
     opta.or(optb)
 }
 
@@ -167,8 +167,8 @@ pub fn option_or_nz_u8(
 #[no_mangle]
 pub fn if_some_nz_u8(opta: Option<NonZero<u8>>, optb: Option<NonZero<u8>>) -> Option<NonZero<u8>> {
     // CHECK: start:
-    // CHECK-NEXT: [[NOT_A:%.*]] = icmp eq i8 %opta, 0
-    // CHECK-NEXT: select i1 [[NOT_A]], i8 %optb, i8 %opta
-    // ret i8
+    // CHECK-NEXT: [[NOT_A:%.+]] = icmp eq i8 %opta, 0
+    // CHECK-NEXT: [[R:%.+]] = select i1 [[NOT_A]], i8 %optb, i8 %opta
+    // CHECK: ret i8 [[R]]
     if opta.is_some() { opta } else { optb }
 }
