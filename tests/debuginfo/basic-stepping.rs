de505d24db776bf399b9932f673f3f3de8c591a0
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-05 08:08:02 +0100

    tests/debuginfo/basic-stepping.rs: Don't mix `compile-flags` with `ignore-*`
    We want directives nice and tidy.

--- tests/debuginfo/basic-stepping.rs
+++ tests/debuginfo/basic-stepping.rs
@@ -5,8 +5,10 @@
 //@ ignore-aarch64: Doesn't work yet.
 //@ ignore-loongarch64: Doesn't work yet.
 //@ ignore-riscv64: Doesn't work yet.
-//@ compile-flags: -g
 //@ ignore-backends: gcc
+
+// Debugger tests need debuginfo
+//@ compile-flags: -g
 
 //@ gdb-command: run
 // FIXME(#97083): Should we be able to break on initialization of zero-sized types?
