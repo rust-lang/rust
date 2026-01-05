646ad315ea428eae01918b6111e5f8e5aeae55ee
kotauskas <v.toncharov@gmail.com>
2026-01-05 14:25:32 +0300

    Add `FIXME` to `io::Error` destructor

--- library/std/src/io/error/repr_bitpacked.rs
+++ library/std/src/io/error/repr_bitpacked.rs
@@ -241,6 +241,10 @@ impl Drop for Repr {
     // meanwhile, is a code size increase by a factor of up to 5.4 in the case
     // of dropping multiple io::Results in the same function
     // (https://godbolt.org/z/8hfGchjsT).
+    //
+    // FIXME this is not the optimal place for this hint (notably, we cannot
+    // apply it to the 32-bit variant). See the discussion in
+    // https://github.com/rust-lang/rust/pull/149146
     #[inline(never)]
     fn drop(&mut self) {
         // Safety: We're a Repr, decode_repr is fine. The `Box::from_raw` is
