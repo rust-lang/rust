f805d1b210b5bd1e765e07903804b7cdea1d48ae
andjsrk <andjsrk0213@gmail.com>
2026-01-07 01:08:19 +0900

    format

--- tests/ui/repeat-expr/can-have-side-effects-consider-element.rs
+++ tests/ui/repeat-expr/can-have-side-effects-consider-element.rs
@@ -5,7 +5,8 @@
 // Test if `Expr::can_have_side_effects` considers element of repeat expressions.
 
 fn drop_repeat_in_arm_body() {
-    // Built-in lint `dropping_copy_types` relies on `Expr::can_have_side_effects` (See rust-clippy#9482)
+    // Built-in lint `dropping_copy_types` relies on
+    // `Expr::can_have_side_effects` (See rust-clippy#9482)
 
     match () {
         () => drop([0; 1]), // No side effects
