35f9fa2fa2ee987982f6dfc257e16bfda2c806bd
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-01-07 17:53:07 +0000

    Don't use extern "unadjusted" outside of LLVM intrinsics in tests

--- tests/codegen-llvm/simd/unpadded-simd.rs
+++ tests/codegen-llvm/simd/unpadded-simd.rs
@@ -1,19 +1,21 @@
 // Make sure that no 0-sized padding is inserted in structs and that
 // structs are represented as expected by Neon intrinsics in LLVM.
 // See #87254.
+//@ only-aarch64
+//@ compile-flags: -Cno-prepopulate-passes
 
 #![crate_type = "lib"]
-#![feature(repr_simd, abi_unadjusted)]
+#![feature(abi_unadjusted, link_llvm_intrinsics)]
 
-#[derive(Copy, Clone)]
-#[repr(simd)]
-pub struct int16x4_t(pub [i16; 4]);
+use std::arch::aarch64::int16x4x2_t;
 
-#[derive(Copy, Clone)]
-pub struct int16x4x2_t(pub int16x4_t, pub int16x4_t);
+unsafe extern "unadjusted" {
+    #[link_name = "llvm.aarch64.neon.ld1x2.v4i16.p0"]
+    fn vld1_s16_x2(t: *const i16) -> int16x4x2_t;
+}
 
-// CHECK: %int16x4x2_t = type { <4 x i16>, <4 x i16> }
 #[no_mangle]
-extern "unadjusted" fn takes_int16x4x2_t(t: int16x4x2_t) -> int16x4x2_t {
-    t
+unsafe extern "C" fn returns_int16x4x2_t(a: *const i16) -> int16x4x2_t {
+    // CHECK: call { <4 x i16>, <4 x i16> } @llvm.aarch64.neon.ld1x2.v4i16.p0
+    vld1_s16_x2(a)
 }
