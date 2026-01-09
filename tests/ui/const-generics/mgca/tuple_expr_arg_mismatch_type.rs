2f35424e716fec72ede2405d0de0a4a3fc63b733
mu001999 <mu001999@outlook.com>
2026-01-09 19:32:23 +0800

    Rename tests for const tuple properly

--- tests/ui/const-generics/mgca/tuple_expr_arg_mismatch_type.rs
+++ tests/ui/const-generics/mgca/tuple_expr_arg_mismatch_type.rs
@@ -2,7 +2,7 @@
 #![expect(incomplete_features)]
 
 pub fn takes_nested_tuple<const N: u32>() {
-    takes_nested_tuple::<{ () }> //~ ERROR expected u32, found const tuple
+    takes_nested_tuple::<{ () }> //~ ERROR expected `u32`, found const tuple
 }
 
 fn main() {}
