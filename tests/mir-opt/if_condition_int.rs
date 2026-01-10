5ee964049a9f0808bc0729fad79be446a2a987cb
dianqk <dianqk@dianqk.net>
2026-01-10 18:34:02 +0800

    Add FileCheck to if_condition_int.rs

--- tests/mir-opt/if_condition_int.rs
+++ tests/mir-opt/if_condition_int.rs
@@ -1,36 +1,76 @@
-// skip-filecheck
 //@ test-mir-pass: SimplifyComparisonIntegral
-// EMIT_MIR if_condition_int.opt_u32.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.opt_negative.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.opt_char.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.opt_i8.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.dont_opt_bool.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.opt_multiple_ifs.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.dont_remove_comparison.SimplifyComparisonIntegral.diff
-// EMIT_MIR if_condition_int.dont_opt_floats.SimplifyComparisonIntegral.diff
+// GVN simplifies FileCheck.
+//@ compile-flags: -Zmir-enable-passes=+GVN
 
+// EMIT_MIR if_condition_int.opt_u32.SimplifyComparisonIntegral.diff
 fn opt_u32(x: u32) -> u32 {
+    // CHECK-LABEL: fn opt_u32(
+    // FIXME: This should be copy.
+    // CHECK: switchInt(move _1) -> [42: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_u32;
     if x == 42 { 0 } else { 1 }
 }
 
+// EMIT_MIR if_condition_int.dont_opt_bool.SimplifyComparisonIntegral.diff
 // don't opt: it is already optimal to switch on the bool
 fn dont_opt_bool(x: bool) -> u32 {
+    // CHECK-LABEL: fn dont_opt_bool(
+    // CHECK: switchInt(copy _1) -> [0: [[BB2:bb.*]], otherwise: [[BB1:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_u32;
     if x { 0 } else { 1 }
 }
 
+// EMIT_MIR if_condition_int.opt_char.SimplifyComparisonIntegral.diff
 fn opt_char(x: char) -> u32 {
+    // CHECK-LABEL: fn opt_char(
+    // CHECK: switchInt(move _1) -> [120: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_u32;
     if x == 'x' { 0 } else { 1 }
 }
 
+// EMIT_MIR if_condition_int.opt_i8.SimplifyComparisonIntegral.diff
 fn opt_i8(x: i8) -> u32 {
+    // CHECK-LABEL: fn opt_i8(
+    // CHECK: switchInt(move _1) -> [42: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_u32;
     if x == 42 { 0 } else { 1 }
 }
 
+// EMIT_MIR if_condition_int.opt_negative.SimplifyComparisonIntegral.diff
 fn opt_negative(x: i32) -> u32 {
+    // CHECK-LABEL: fn opt_negative(
+    // CHECK: switchInt(move _1) -> [4294967254: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_u32;
     if x == -42 { 0 } else { 1 }
 }
 
+// EMIT_MIR if_condition_int.opt_multiple_ifs.SimplifyComparisonIntegral.diff
 fn opt_multiple_ifs(x: u32) -> u32 {
+    // CHECK-LABEL: fn opt_multiple_ifs(
+    // CHECK: switchInt(move _1) -> [42: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_u32;
+    // CHECK: [[BB2]]:
+    // CHECK: switchInt(move _1) -> [21: [[BB4:bb.*]], otherwise: [[BB3:bb.*]]];
+    // CHECK: [[BB3]]:
+    // CHECK: _0 = const 1_u32;
+    // CHECK: [[BB4]]:
+    // CHECK: _0 = const 2_u32;
     if x == 42 {
         0
     } else if x != 21 {
@@ -40,8 +80,18 @@ fn opt_multiple_ifs(x: u32) -> u32 {
     }
 }
 
+// EMIT_MIR if_condition_int.dont_remove_comparison.SimplifyComparisonIntegral.diff
 // test that we optimize, but do not remove the b statement, as that is used later on
 fn dont_remove_comparison(a: i8) -> i32 {
+    // CHECK-LABEL: fn dont_remove_comparison(
+    // CHECK: [[b:_.*]] = Eq(copy _1, const 17_i8);
+    // CHECK: switchInt(move _1) -> [17: [[BB1:bb.*]], otherwise: [[BB2:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: [[cast_1:_.*]] = copy [[b]] as i32 (IntToInt);
+    // CHECK: _0 = Add(const 100_i32, move [[cast_1]]);
+    // CHECK: [[BB2]]:
+    // CHECK: [[cast_2:_.*]] = copy [[b]] as i32 (IntToInt);
+    // CHECK: _0 = Add(const 10_i32, move [[cast_2]]);
     let b = a == 17;
     match b {
         false => 10 + b as i32,
@@ -49,8 +99,16 @@ fn dont_remove_comparison(a: i8) -> i32 {
     }
 }
 
+// EMIT_MIR if_condition_int.dont_opt_floats.SimplifyComparisonIntegral.diff
 // test that we do not optimize on floats
 fn dont_opt_floats(a: f32) -> i32 {
+    // CHECK-LABEL: fn dont_opt_floats(
+    // CHECK: [[cmp:_.*]] = Eq(copy _1, const -42f32);
+    // CHECK: switchInt(move [[cmp]]) -> [0: [[BB2:bb.*]], otherwise: [[BB1:bb.*]]];
+    // CHECK: [[BB1]]:
+    // CHECK: _0 = const 0_i32;
+    // CHECK: [[BB2]]:
+    // CHECK: _0 = const 1_i32;
     if a == -42.0 { 0 } else { 1 }
 }
 
