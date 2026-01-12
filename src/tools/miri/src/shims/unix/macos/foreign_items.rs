cc99a498ea02d8e479975865745b565c3d98b5e7
joboet <jonasboettiger@icloud.com>
2026-01-12 14:58:16 +0100

    miri: implement `mach_wait_until`

--- src/tools/miri/src/shims/unix/macos/foreign_items.rs
+++ src/tools/miri/src/shims/unix/macos/foreign_items.rs
@@ -116,6 +116,13 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                 this.write_scalar(result, dest)?;
             }
 
+            // FIXME: add a test that directly calls this function.
+            "mach_wait_until" => {
+                let [deadline] = this.check_shim_sig_lenient(abi, CanonAbi::C, link_name, args)?;
+                let result = this.mach_wait_until(deadline)?;
+                this.write_scalar(result, dest)?;
+            }
+
             // Access to command-line arguments
             "_NSGetArgc" => {
                 let [] = this.check_shim_sig_lenient(abi, CanonAbi::C, link_name, args)?;
