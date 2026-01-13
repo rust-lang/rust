71f8ea99fe5cfbca752ddcfb1c0b13453941a9e6
BD103 <59022059+BD103@users.noreply.github.com>
2026-01-13 12:19:37 -0500

    refactor: move tuples type info ui test to coretest

--- library/coretests/tests/mem/type_info.rs
+++ library/coretests/tests/mem/type_info.rs
@@ -21,3 +21,36 @@ fn test_arrays() {
         _ => unreachable!(),
     }
 }
+
+#[test]
+fn test_tuples() {
+    fn assert_tuple_arity<T: 'static, const N: usize>() {
+        match const { Type::of::<T>() }.kind {
+            TypeKind::Tuple(tup) => {
+                assert_eq!(tup.fields.len(), N);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    assert_tuple_arity::<(), 0>();
+    assert_tuple_arity::<(u8,), 1>();
+    assert_tuple_arity::<(u8, u8), 2>();
+
+    const {
+        match Type::of::<(u8, u8)>().kind {
+            TypeKind::Tuple(tup) => {
+                let [a, b] = tup.fields else { unreachable!() };
+
+                assert!(a.offset == 0);
+                assert!(b.offset == 1);
+
+                match (a.ty.info().kind, b.ty.info().kind) {
+                    (TypeKind::Leaf, TypeKind::Leaf) => {}
+                    _ => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
