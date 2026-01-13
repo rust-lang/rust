034a14943aae49f96bad0d3fde09df7e5440a127
Tobias Bucher <tobiasbucher5991@gmail.com>
2026-01-13 14:09:59 +0100

    Make `vec.extend(slice.iter().cloned())` do the same as `Vec::extend_from_slice`
    Remove the template parameter `T` from `SpecExtend`. It was previously
    only used to create a second trait hierarchy just for
    `Vec::extend_from_slice`.
    
    Calling `vec.extend(slice.iter().cloned())` now calls the same code
    thanks to specialization.
    
    I don't know of any plans to deprecate this method.

--- library/alloc/src/vec/spec_extend.rs
+++ library/alloc/src/vec/spec_extend.rs
@@ -1,16 +1,16 @@
 use core::clone::TrivialClone;
-use core::iter::TrustedLen;
-use core::slice::{self};
+use core::iter::{Cloned, TrustedLen};
+use core::slice;
 
 use super::{IntoIter, Vec};
 use crate::alloc::Allocator;
 
 // Specialization trait used for Vec::extend
-pub(super) trait SpecExtend<T, I> {
+pub(super) trait SpecExtend<I> {
     fn spec_extend(&mut self, iter: I);
 }
 
-impl<T, I, A: Allocator> SpecExtend<T, I> for Vec<T, A>
+impl<T, I, A: Allocator> SpecExtend<I> for Vec<T, A>
 where
     I: Iterator<Item = T>,
 {
@@ -19,7 +19,7 @@ where
     }
 }
 
-impl<T, I, A: Allocator> SpecExtend<T, I> for Vec<T, A>
+impl<T, I, A: Allocator> SpecExtend<I> for Vec<T, A>
 where
     I: TrustedLen<Item = T>,
 {
@@ -28,7 +28,7 @@ where
     }
 }
 
-impl<T, A1: Allocator, A2: Allocator> SpecExtend<T, IntoIter<T, A2>> for Vec<T, A1> {
+impl<T, A1: Allocator, A2: Allocator> SpecExtend<IntoIter<T, A2>> for Vec<T, A1> {
     fn spec_extend(&mut self, mut iterator: IntoIter<T, A2>) {
         unsafe {
             self.append_elements(iterator.as_slice() as _);
@@ -37,22 +37,13 @@ impl<T, A1: Allocator, A2: Allocator> SpecExtend<T, IntoIter<T, A2>> for Vec<T, 
     }
 }
 
-impl<'a, T: 'a, I, A: Allocator> SpecExtend<&'a T, I> for Vec<T, A>
-where
-    I: Iterator<Item = &'a T>,
-    T: Clone,
-{
-    default fn spec_extend(&mut self, iterator: I) {
-        self.spec_extend(iterator.cloned())
-    }
-}
-
-impl<'a, T: 'a, A: Allocator> SpecExtend<&'a T, slice::Iter<'a, T>> for Vec<T, A>
+impl<'a, T: 'a, I, A: Allocator> SpecExtend<Cloned<slice::Iter<'a, T>>> for Vec<T, A>
 where
     T: TrivialClone,
 {
-    fn spec_extend(&mut self, iterator: slice::Iter<'a, T>) {
+    fn spec_extend(&mut self, mut iterator: Cloned<slice::Iter<'a, T>>) {
+        let inner: &mut slice::Iter<'a, T> = unsafe { iterator.as_inner() };
         let slice = iterator.as_slice();
-        unsafe { self.append_elements(slice) };
+        unsafe { self.append_elements(slice); }
     }
 }
