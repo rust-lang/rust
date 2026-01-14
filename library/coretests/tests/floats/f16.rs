051f68ea44ba24a2e62c1636cfb80d4428f903d7
JohnCari <johnuelcarrion@gmail.com>
2026-01-14 11:42:31 -0400

    Remove test_from from f16.rs (moved to mod.rs)
    Port f16 From trait tests to unified float_test! macro in mod.rs.
    
    Part of rust-lang#141726

--- library/coretests/tests/floats/f16.rs
+++ library/coretests/tests/floats/f16.rs
@@ -22,14 +22,5 @@ const TOL_P4: f16 = 10.0;
 // FIXME(f16_f128,miri): many of these have to be disabled since miri does not yet support
 // the intrinsics.
 
-#[test]
-fn test_from() {
-    assert_biteq!(f16::from(false), 0.0);
-    assert_biteq!(f16::from(true), 1.0);
-    assert_biteq!(f16::from(u8::MIN), 0.0);
-    assert_biteq!(f16::from(42_u8), 42.0);
-    assert_biteq!(f16::from(u8::MAX), 255.0);
-    assert_biteq!(f16::from(i8::MIN), -128.0);
-    assert_biteq!(f16::from(42_i8), 42.0);
-    assert_biteq!(f16::from(i8::MAX), 127.0);
-}
+// NOTE: test_from has been moved to mod.rs using float_test! macro
+// See: from_bool, from_u8, from_i8 tests
