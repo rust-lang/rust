607c37797a868c7be11af0d7792dbc362b6519a2
dianne <diannes.gm@gmail.com>
2026-01-16 18:07:57 -0800

    add tests

---
+++ tests/ui/typeck/no-expectation-for-unop-operand.rs
@@ -0,0 +1,12 @@
+//! Test for <https://github.com/rust-lang/rust/issues/151202>: we shouldn't expect that the operand
+//! of `!` or `-` has the same type as the overall result of the operation, since `Not` and `Neg`
+//! impls can have differing operand and output types.
+
+fn main() {
+    // If we propagated the expected type of `i8` here to the `{&0i8}` operands, the blocks would
+    // force a coercion from `&i8` to `i8`, resulting in a type error.
+    let _: i8 = -{&0i8};
+    //~^ ERROR mismatched types
+    let _: i8 = !{&0i8};
+    //~^ ERROR mismatched types
+}
