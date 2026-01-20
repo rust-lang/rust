7d8a19ebb65767bd38099f0861c9a2db92bb3b50
Eddy (Eduard) Stefes <eddy@linux.ibm.com>
2026-01-20 15:26:35 +0100

    add a s390x-unknown-none-softfloat target

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1462,6 +1462,7 @@ supported_targets! {
     ("powerpc64le-unknown-linux-gnu", powerpc64le_unknown_linux_gnu),
     ("powerpc64le-unknown-linux-musl", powerpc64le_unknown_linux_musl),
     ("s390x-unknown-linux-gnu", s390x_unknown_linux_gnu),
+    ("s390x-unknown-none-softfloat", s390x_unknown_none_softfloat),
     ("s390x-unknown-linux-musl", s390x_unknown_linux_musl),
     ("sparc-unknown-linux-gnu", sparc_unknown_linux_gnu),
     ("sparc64-unknown-linux-gnu", sparc64_unknown_linux_gnu),
