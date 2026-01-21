007d0de353f89a29ca96c11d5e805ba5b743d2e4
Christian Poveda <christian.poveda@ferrous-systems.com>
2026-01-21 11:24:12 -0500

    Use edition ranges in `tests/ui/associated-consts/`
    Co-authored-by: Lukas Wirth <lukastw97@gmail.com>

--- tests/ui/associated-consts/associated-const-range-match-patterns.rs
+++ tests/ui/associated-consts/associated-const-range-match-patterns.rs
@@ -1,5 +1,7 @@
-//@ edition:2015..2021
-//@ run-pass
+//@ revisions: edition2015 edition2018
+//@[edition2015]edition:2015
+//@[edition2018]edition:2018..
+//@run-pass
 #![allow(dead_code, unreachable_patterns)]
 #![allow(ellipsis_inclusive_range_patterns)]
 
@@ -13,14 +15,17 @@ impl HasNum for Foo {
 }
 
 fn main() {
+    #[cfg(edition2015)]
     assert!(match 2 {
         Foo::NUM ... 3 => true,
         _ => false,
     });
+    #[cfg(edition2015)]
     assert!(match 0 {
         -1 ... <Foo as HasNum>::NUM => true,
         _ => false,
     });
+    #[cfg(edition2015)]
     assert!(match 1 {
         <Foo as HasNum>::NUM ... <Foo>::NUM => true,
         _ => false,
