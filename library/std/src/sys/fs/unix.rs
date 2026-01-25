7f1bf99f3f3dae9f7e738b05eaa04f9ba0c08eb2
vinDelphini <betadelphini.py@gmail.com>
2026-01-25 20:38:18 +0530

    Revert "removed FIXME comment"
    This reverts commit 7d472aa4eda37488ba7037e2b02d40350d32476e.

--- library/std/src/sys/fs/unix.rs
+++ library/std/src/sys/fs/unix.rs
@@ -195,6 +195,8 @@ cfg_has_statx! {{
             // if the syscall is usable.
             //
             // See: https://github.com/rust-lang/rust/issues/65662
+            //
+
             let err2 = cvt(statx(0, ptr::null(), 0, libc::STATX_BASIC_STATS | libc::STATX_BTIME, ptr::null_mut()))
                 .err()
                 .and_then(|e| e.raw_os_error());
