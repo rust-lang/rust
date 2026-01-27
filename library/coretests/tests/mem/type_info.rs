f2ee7144b2e0ef53da3ee9eb0f09a64cc9eb5e38
Katt <51190960+coolcatcoder@users.noreply.github.com>
2026-01-27 11:16:12 +0800

    Fix test.

--- library/coretests/tests/mem/type_info.rs
+++ library/coretests/tests/mem/type_info.rs
@@ -300,8 +300,8 @@ fn test_implements_trait() {
     impl Blah for Garlic {}
 
     const {
-        assert!(TypeId::of::<Garlic>().implements_trait::<dyn Blah>());
-        assert!(TypeId::of::<Garlic>().implements_trait::<dyn Blah + Send>());
-        assert!(!TypeId::of::<*const Box<Garlic>>().implements_trait::<dyn Sync>());
+        assert!(TypeId::of::<Garlic>().info().has_trait::<dyn Blah>());
+        assert!(TypeId::of::<Garlic>().info().has_trait::<dyn Blah + Send>());
+        assert!(!TypeId::of::<*const Box<Garlic>>().info().has_trait::<dyn Sync>());
     }
 }
