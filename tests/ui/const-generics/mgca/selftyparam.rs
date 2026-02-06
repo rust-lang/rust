022139f12ca3cdd2261c542bea651a2fad557312
Boxy <rust@boxyuwu.dev>
2026-02-06 16:18:10 +0000

    parse array lengths without mgca shenanigans

--- tests/ui/const-generics/mgca/selftyparam.rs
+++
@@ -1,9 +0,0 @@
-#![feature(min_generic_const_args)]
-#![expect(incomplete_features)]
-
-trait Tr<const N: usize> {
-    fn foo() -> [(); const { let _: Self; 1 }];
-    //~^ ERROR generic parameters
-}
-
-fn main() {}
