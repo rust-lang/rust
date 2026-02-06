715db45be13ed26ced2d23ef02ab669d85355c0b
xonx <119700621+xonx4l@users.noreply.github.com>
2026-02-06 17:42:56 +0000

    implement built-in candidate

---
+++ tests/ui/traits/next-solver/iterator-ice.rs
@@ -0,0 +1,11 @@
+//@ compile-flags: -Znext-solver=globally
+
+fn main() {
+    const { 
+        for _ in 1..5 {} 
+        //~^ ERROR cannot use `for` loop on `std::ops::Range<i32>` in constants
+        //~| ERROR `IntoIterator` is not yet stable as a const trait
+        //~| ERROR cannot use `for` loop on `std::ops::Range<i32>` in constants
+        //~| ERROR `Iterator` is not yet stable as a const trait
+    }
+}
