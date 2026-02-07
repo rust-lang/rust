b46780feacc2cb20b80a9cc3c3127b67e08a84e6
xonx <119700621+xonx4l@users.noreply.github.com>
2026-02-07 11:25:47 +0000

    fix ci

--- tests/ui/traits/next-solver/iterator-ice.rs
+++ tests/ui/traits/next-solver/iterator-ice.rs
@@ -1,11 +1,12 @@
 //@ compile-flags: -Znext-solver=globally
+//@ edition: 2021
 
 fn main() {
     const {
         for _ in 1..5 {}
-        //~^ ERROR cannot use `for` loop on `std::ops::Range<i32>` in constants
-        //~| ERROR `IntoIterator` is not yet stable as a const trait
-        //~| ERROR cannot use `for` loop on `std::ops::Range<i32>` in constants
-        //~| ERROR `Iterator` is not yet stable as a const trait
+        //~^ ERROR cannot use `for` loop
+        //~| ERROR `IntoIterator` is not yet stable
+        //~| ERROR cannot use `for` loop
+        //~| ERROR `Iterator` is not yet stable
     }
 }
