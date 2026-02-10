58335d639912ba14c36bcf8e98ccdb28fcdde62e
Maja Kądziołka <maya@compilercrim.es>
2026-02-10 15:10:04 +0100

    Don't ICE on layout error in vtable computation

---
+++ tests/ui/limits/vtable-try-as-dyn.rs
@@ -0,0 +1,14 @@
+// At the time of writing, vtable.rs would ICE only with debuginfo disabled, while this testcase,
+// originally reported as #152030, would ICE even with debuginfo enabled.
+//@ revisions: default debuginfo
+//@ compile-flags: --crate-type=lib --emit=mir
+//@[debuginfo] compile-flags: -C debuginfo=2
+#![feature(try_as_dyn)]
+
+trait Trait {}
+impl<T> Trait for T {}
+
+//~? ERROR: values of the type `[u8; usize::MAX]` are too big for the target architecture
+pub fn foo(x: &[u8; usize::MAX]) {
+    let _ = std::any::try_as_dyn::<[u8; usize::MAX], dyn Trait>(x);
+}
