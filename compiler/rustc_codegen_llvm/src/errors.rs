6753aec77c12dc9cc18ae7d69adbe031cd93e93c
Eddy (Eduard) Stefes <eddy@linux.ibm.com>
2026-02-10 09:18:07 +0100

    add rustc option -Zpacked-stack
    this enables packed-stack just as -mpacked-stack in clang and gcc.
    packed-stack is needed on s390x for kernel development.
    
    Co-authored-by: Ralf Jung <post@ralfj.de>

--- compiler/rustc_codegen_llvm/src/errors.rs
+++ compiler/rustc_codegen_llvm/src/errors.rs
@@ -206,3 +206,10 @@ pub(crate) struct FixedX18InvalidArch<'a> {
 #[derive(Diagnostic)]
 #[diag("`-Zsanitizer-kcfi-arity` requires LLVM 21.0.0 or later")]
 pub(crate) struct SanitizerKcfiArityRequiresLLVM2100;
+
+#[derive(Diagnostic)]
+#[diag("packedstack with backchain needs softfloat")]
+#[note(
+    "enabling both `-Zpacked-stack` and the `backchain` target feature is incompatible with the default s390x ABI. Switch to s390x-unknown-none-softfloat if you need both attributes"
+)]
+pub(crate) struct PackedStackBackchainNeedsSoftfloat;
