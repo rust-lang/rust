e325046d3be27586c425d80d6e616554d0b6f52e
Matthew Hughes <matthewhughes934@gmail.com>
2026-02-12 21:37:13 +0000

    Make comment wrapping indent aware
    When formatting 'missed' spans (I'm still not sure I understand exactly
    what 'missed' means).
    
    Previously, if `comment_width` was less than `max_width -
    $current_indent` we would format the comment ignoring the length of any
    indentation, so e.g. the following, with `comment_width=38` would not be
    formatted
    
        fn foo() {
            // The '.' is at 'comment_width' . A
    
            ()
        }
    
    This involved rewriting some existing tests, without impacting what
    they're meant to be asserting on. This emphasises that this patch _is
    not_ backwards compatible: while I believe the change to be correct
    (some tests had comments longer than `comment_width` that weren't being
    formatted). So there's a trade-off to be made between correctness and
    stability, since `comment_width` is still not stabilised this may be
    acceptable.
    
    Issue: #6801

--- tests/target/issue-5023.rs
+++ tests/target/issue-5023.rs
@@ -20,8 +20,9 @@ fn main() {
             .into_iter()
             .filter(|(xxx, xxx)| {
                 if let Some(x) = Some(1) {
-                    // xxxxxxxxxxxxxxxxxx, xxxxxxxxxxxx, xxxxxxxxxxxxxxxxxxxx xxx xxxxxxx, xxxxx xxx
-                    // xxxxxxxxxx. xxxxxxxxxxxxxxxx，xxxxxxxxxxxxxxxxx xxx xxxxxxx
+                    // xxxxxxxxxxxxxxxxxx, xxxxxxxxxxxx, xxxxxxxxxxxxxxxxxxxx
+                    // xxx xxxxxxx, xxxxx xxx xxxxxxxxxx.
+                    // xxxxxxxxxxxxxxxx，xxxxxxxxxxxxxxxxx xxx xxxxxxx
                     // 是sdfadsdfxxxxxxxxx，sdfaxxxxxx_xxxxx_masdfaonxxx，
                     if false {
                         return true;
