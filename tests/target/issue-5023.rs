a778e81e455bad9c7c0e8ee687554aad34468955
Matthew Hughes <matthewhughes934@gmail.com>
2026-02-13 20:26:47 +0000

    Add clearer test example for issue 5023

--- tests/target/issue-5023.rs
+++ tests/target/issue-5023.rs
@@ -1,5 +1,16 @@
 // rustfmt-wrap_comments: true
 
+// below we try and force a split at a byte in the middle of a multi-byte
+// character. The two parts of the first line are constructed such that:
+// 1) the entire line is longer than `$comment_width`
+// 2) the length of the second part is such that:
+//   `$comment_width - 3 + $length` is positive _and_ less than the number of
+//   bytes in the multi-byte char
+
+// xxxxxxxxxx
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// 是
+
 /// A comment to test special unicode characters on boundaries
 /// 是，是，是，是，是，是，是，是，是，是，是，是  it should break right here
 /// this goes to the next line
