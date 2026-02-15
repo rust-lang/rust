172ecbcab8dbdcf66f1e84c683cfb74773e7123f
Matthew Hughes <matthewhughes934@gmail.com>
2026-02-15 21:36:12 +0000

    Silence expected parser errors
    I find otherwise the test output can be a bit noisy, and in particular
    the error messages make me think some test somewhere failed when even
    when it's all passing, for example, one such removed error:
    
        error: mismatched closing delimiter: `}`
         --> tests/parser/unclosed-delims/issue_4466.rs:3:17
          |
        2 |     if true {
          |             - closing delimiter possibly meant for this
        3 |         println!("answer: {}", a_func();
          |                 ^ unclosed delimiter
        4 |     } else {
          |     ^ mismatched closing delimiter
    
    This also removes some noisy warnings like:
    
        warning: whitespace symbol '\u{a0}' is not skipped
         --> tests/target/string_lit_unicode_ws.rs:3:22
          |
        3 |       let str = "hello \
          |  ______________________^
        4 | |  world";
          | | ^ whitespace symbol '\u{a0}' is not skipped
          | |_|
          |

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -627,7 +627,9 @@ fn stdin_parser_panic_caught() {
     // See issue #3239.
     for text in ["{", "}"].iter().cloned().map(String::from) {
         let mut buf = vec![];
-        let mut session = Session::new(Default::default(), Some(&mut buf));
+        let mut config = Config::default();
+        config.set().show_parse_errors(false);
+        let mut session = Session::new(config, Some(&mut buf));
         let _ = session.format(Input::Text(text));
 
         assert!(session.has_parsing_errors());
