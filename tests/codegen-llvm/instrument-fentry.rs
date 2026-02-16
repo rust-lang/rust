1a433a256d9f3723efe81d74b3932ffe000771e1
Paul Murphy <murp@redhat.com>
2026-02-16 16:19:57 -0600

    Add -Zinstrument-fentry
    The behavior is very similar to `-Zinstrument-mcount`, except this
    option has no frame-pointer requirements, and is called before the
    call frame is set up.
    
    This only works on x86, x86-64, and s390x targets.

---
+++ tests/codegen-llvm/instrument-fentry.rs
@@ -0,0 +1,25 @@
+//@ add-minicore
+//@ compile-flags: -Z instrument-fentry -Copt-level=0
+//
+//@ revisions: x86_64-linux
+//@[x86_64-linux] compile-flags: --target=x86_64-unknown-linux-gnu
+//@[x86_64-linux] needs-llvm-components: x86
+//
+//@ revisions: x86-linux
+//@[x86-linux] compile-flags: --target=i686-unknown-linux-gnu
+//@[x86-linux] needs-llvm-components: x86
+//
+//@ revisions: s390x-linux
+//@[s390x-linux] compile-flags: --target=s390x-unknown-linux-gnu
+//@[s390x-linux] needs-llvm-components: systemz
+
+#![feature(no_core)]
+#![no_std]
+#![no_core]
+#![crate_type = "lib"]
+
+extern crate minicore;
+use minicore::*;
+
+// CHECK: attributes #{{.*}} "fentry-call"="true"
+pub fn foo() {}
