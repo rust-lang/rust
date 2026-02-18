88ceea4f4da48533e6838438353744ac44b351e4
Altug Bozkurt <altugbozkurt09@gmail.com>
2026-02-18 15:53:06 +0300

    Allow `#[link_section]` on foreign items
    Extend the existing stable `#[link_section]` attribute to work on
    `extern` statics and functions. This adds `ForeignStatic` and
    `ForeignFn` to the attribute's allowed targets, consistent with
    other foreign-item attributes like `link_name` and `linkage`.
    
    This is useful for BPF programs that need extern declarations
    placed in specific ELF sections for BTF generation, and for linker
    scripts on other targets.

--- compiler/rustc_codegen_llvm/src/consts.rs
+++ compiler/rustc_codegen_llvm/src/consts.rs
@@ -403,6 +403,10 @@ impl<'ll> CodegenCx<'ll, '_> {
             llvm::set_dllimport_storage_class(g);
         }
 
+        if self.tcx.is_foreign_item(def_id) {
+            base::set_link_section(g, fn_attrs);
+        }
+
         self.instances.borrow_mut().insert(instance, g);
         g
     }
