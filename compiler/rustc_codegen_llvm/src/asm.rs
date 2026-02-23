21d3e5747ca87a8f6776a8c6675464f1515ea292
Flakebi <flakebi@t-online.de>
2026-02-23 10:38:14 +0100

    Split amdgpu inline asm reg classes by size

--- compiler/rustc_codegen_llvm/src/asm.rs
+++ compiler/rustc_codegen_llvm/src/asm.rs
@@ -699,8 +699,13 @@ fn reg_to_llvm(reg: InlineAsmRegOrRegClass, layout: Option<&TyAndLayout<'_>>) ->
             | Arm(ArmInlineAsmRegClass::dreg_low8)
             | Arm(ArmInlineAsmRegClass::qreg_low4) => "x",
             Arm(ArmInlineAsmRegClass::dreg) | Arm(ArmInlineAsmRegClass::qreg) => "w",
-            Amdgpu(AmdgpuInlineAsmRegClass::sgpr) => "s",
-            Amdgpu(AmdgpuInlineAsmRegClass::vgpr) => "v",
+            Amdgpu(AmdgpuInlineAsmRegClass::sgpr32)
+            | Amdgpu(AmdgpuInlineAsmRegClass::sgpr64)
+            | Amdgpu(AmdgpuInlineAsmRegClass::sgpr128) => "s",
+            Amdgpu(AmdgpuInlineAsmRegClass::vgpr16)
+            | Amdgpu(AmdgpuInlineAsmRegClass::vgpr32)
+            | Amdgpu(AmdgpuInlineAsmRegClass::vgpr64)
+            | Amdgpu(AmdgpuInlineAsmRegClass::vgpr128) => "v",
             Hexagon(HexagonInlineAsmRegClass::reg) => "r",
             Hexagon(HexagonInlineAsmRegClass::reg_pair) => "r",
             Hexagon(HexagonInlineAsmRegClass::preg) => unreachable!("clobber-only"),
