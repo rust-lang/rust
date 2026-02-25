dc548c96e40d5515ca0eb082f53677406d10bb7a
joboet <jonasboettiger@icloud.com>
2026-02-25 13:59:48 +0100

    reference more reliable items in rustdoc tests

--- tests/rustdoc-html/reexport/overlapping-reexport-105735.rs
+++ tests/rustdoc-html/reexport/overlapping-reexport-105735.rs
@@ -1,22 +1,22 @@
-// Regression test to ensure that both `AtomicU8` items are displayed but not the re-export.
+// Regression test to ensure that both `Ordering` items are displayed but not the re-export.
 // https://github.com/rust-lang/rust/issues/105735
 
 #![crate_name = "foo"]
 #![no_std]
 
 //@ has 'foo/index.html'
-//@ has - '//dt/a[@class="struct"]' 'AtomicU8'
-//@ has - '//dt/a[@class="constant"]' 'AtomicU8'
+//@ has - '//dt/a[@class="enum"]' 'Ordering'
+//@ has - '//dt/a[@class="constant"]' 'Ordering'
 // We also ensure we don't have another item displayed.
 //@ count - '//*[@id="main-content"]/*[@class="section-header"]' 2
-//@ has - '//*[@id="main-content"]/*[@class="section-header"]' 'Structs'
+//@ has - '//*[@id="main-content"]/*[@class="section-header"]' 'Enums'
 //@ has - '//*[@id="main-content"]/*[@class="section-header"]' 'Constants'
 
 mod thing {
-    pub use core::sync::atomic::AtomicU8;
+    pub use core::cmp::Ordering;
 
     #[allow(non_upper_case_globals)]
-    pub const AtomicU8: () = ();
+    pub const Ordering: () = ();
 }
 
-pub use crate::thing::AtomicU8;
+pub use crate::thing::Ordering;
