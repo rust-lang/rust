19b3617dd418347e09aaee9229018b2a3cf6e249
Nicholas Nethercote <n.nethercote@gmail.com>
2026-02-27 16:39:23 +1100

    Remove a duplicated comment.
    This exact comment block also appears in
    `compiler/rustc_middle/src/queries.rs`, which is a better place for it.

--- compiler/rustc_middle/src/query/plumbing.rs
+++ compiler/rustc_middle/src/query/plumbing.rs
@@ -643,18 +643,6 @@ macro_rules! define_callbacks {
     };
 }
 
-// Each of these queries corresponds to a function pointer field in the
-// `Providers` struct for requesting a value of that type, and a method
-// on `tcx: TyCtxt` (and `tcx.at(span)`) for doing that request in a way
-// which memoizes and does dep-graph tracking, wrapping around the actual
-// `Providers` that the driver creates (using several `rustc_*` crates).
-//
-// The result type of each query must implement `Clone`, and additionally
-// `ty::query::values::Value`, which produces an appropriate placeholder
-// (error) value if the query resulted in a query cycle.
-// Queries marked with `cycle_fatal` do not need the latter implementation,
-// as they will raise an fatal error on query cycles instead.
-
 mod sealed {
     use rustc_hir::def_id::{LocalModDefId, ModDefId};
 
