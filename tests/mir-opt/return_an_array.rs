2b8b82feec6a03a4730e235ac27e7744617a5073
milosilo-dev <lazerache@gmail.com>
2026-02-28 13:38:29 +0000

    Addded file check to return_an_array.rs

--- tests/mir-opt/return_an_array.rs
+++ tests/mir-opt/return_an_array.rs
@@ -1,5 +1,3 @@
-// skip-filecheck
-// this tests move up progration, which is not yet implemented
 
 fn foo() -> [u8; 1024] {
     let x = [0; 1024];
@@ -7,3 +5,10 @@ fn foo() -> [u8; 1024] {
 }
 
 fn main() {}
+
+// File checks to confirm that the array is assigned and returned
+// In the mir output
+
+// CHECK: let mut _0: [u8; 1024]; 
+// CHECK: _0 = [const 0_u8; 1024];
+// CHECK: return;
