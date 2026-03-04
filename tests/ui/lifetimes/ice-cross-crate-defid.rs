ddd6d0d4b37cc0fbd3c14b26c3afae90be88b112
Shrey Naithani <mrytshplays@gmail.com>
2026-03-04 12:36:59 +0530

    Fix comment formatting

--- tests/ui/lifetimes/ice-cross-crate-defid.rs
+++ tests/ui/lifetimes/ice-cross-crate-defid.rs
@@ -1,7 +1,7 @@
 //@ aux-build:ice_trait.rs
 extern crate ice_trait;
 use ice_trait::ExternalTrait;
-// Forcing a lifetime error here, The compiler will try to explain 
+// Forcing a lifetime error here, The compiler will try to explain
 // This should forces it to look at the ExternalTrait definition in the other crate.
 fn test<'a, T: ExternalTrait>(a: &'a mut T) -> impl std::future::Future<Output = ()> + 'static {
     a.build_request()
