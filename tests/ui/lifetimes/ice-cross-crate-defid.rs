58e1785654987f1730b527a5b9b5f2fed9e0cbac
Shrey Naithani <mrytshplays@gmail.com>
2026-03-04 18:00:19 +0530

    Fix whitespace

--- tests/ui/lifetimes/ice-cross-crate-defid.rs
+++ tests/ui/lifetimes/ice-cross-crate-defid.rs
@@ -3,6 +3,6 @@
 extern crate ice_trait;
 use ice_trait::ExternalTrait;
 fn test<'a, T: ExternalTrait>(a: &'a mut T) -> impl std::future::Future<Output = ()> + 'static {
-    a.build_request() 
+    a.build_request()
 }
 fn main() {}
