f33e6d5149c7e40ef849450110b0ea876b84a890
Shrey Naithani <mrytshplays@gmail.com>
2026-03-04 17:29:25 +0530

    Update ice-cross-crate-defid.rs

--- tests/ui/lifetimes/ice-cross-crate-defid.rs
+++ tests/ui/lifetimes/ice-cross-crate-defid.rs
@@ -1,9 +1,8 @@
 //@ aux-build:ice_trait.rs
+//@ check-fail
 extern crate ice_trait;
 use ice_trait::ExternalTrait;
-// Forcing a lifetime error here, The compiler will try to explain
-// This should forces it to look at the ExternalTrait definition in the other crate.
 fn test<'a, T: ExternalTrait>(a: &'a mut T) -> impl std::future::Future<Output = ()> + 'static {
-    a.build_request()
+    a.build_request() 
 }
 fn main() {}
