b40965b90c55d63b620a1d705eed57bf58c42f9f
Shrey Naithani <mrytshplays@gmail.com>
2026-03-04 12:06:02 +0530

    Update lifetime test

--- tests/ui/lifetimes/ice-cross-crate-defid.rs
+++ tests/ui/lifetimes/ice-cross-crate-defid.rs
@@ -1,17 +1,10 @@
 //@ aux-build:ice_trait.rs
 extern crate ice_trait;
 use ice_trait::ExternalTrait;
-
-// This test ensures that the compiler doesn't crash (ICE) when explaining
-// lifetimes involving cross-crate traits. See gh-153375.
-
-struct LocalWrapper<T>(T);
-
-impl<T: ExternalTrait> LocalWrapper<T> {
-    fn do_thing(&mut self) {
-        // This exercises the note_and_explain logic on an external trait.
-        let _ = self.0.build_request();
-    }
+// Forcing a lifetime error here, The compiler will try to explain 
+// This should forces it to look at the ExternalTrait definition in the other crate.
+fn test<'a, T: ExternalTrait>(a: &'a mut T) -> impl std::future::Future<Output = ()> + 'static {
+    a.build_request() 
 }
 
 fn main() {}
