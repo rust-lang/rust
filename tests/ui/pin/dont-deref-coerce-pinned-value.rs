5276fcd28eaaf3edbaa170990de622f3a9fe0bb1
dianne <diannes.gm@gmail.com>
2026-03-05 10:28:12 -0800

    prevent deref coercions in `pin!`

--- tests/ui/pin/dont-deref-coerce-pinned-value.rs
+++ tests/ui/pin/dont-deref-coerce-pinned-value.rs
@@ -2,12 +2,12 @@
 //! expectation on `pin!`'s result, make sure we don't deref-coerce the argument to
 //! `Pin::new_unchecked` to get its type to match up. That violates the pinning invariant, leading
 //! to unsoundness!
-//@ check-pass
 
 use std::pin::{Pin, pin};
 
 fn wrong_pin<T>(data: &mut T, callback: impl FnOnce(Pin<&mut T>)) {
     callback(pin!(data));
+    //~^ ERROR: mismatched types
 }
 
 fn main() {}
