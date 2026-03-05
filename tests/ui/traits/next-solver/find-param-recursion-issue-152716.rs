fcddf7fa3cd4c9cfa8afa9f1aba830cea87971b0
Ada Bohm <ada@kreatrix.org>
2026-03-05 10:08:57 +0100

    Propagate certainty

--- tests/ui/traits/next-solver/find-param-recursion-issue-152716.rs
+++ tests/ui/traits/next-solver/find-param-recursion-issue-152716.rs
@@ -1,5 +1,4 @@
 //@ compile-flags: -Znext-solver
-//~^ ERROR overflow normalizing the associated type `<T as Proj<'b>>::Assoc` [E0275]
 
 // Regression test for <https://github.com/rust-lang/rust/issues/152716>.
 
@@ -11,6 +10,7 @@ fn foo<T>()
 where
     T: for<'a> Proj<'a, Assoc = for<'b> fn(<T as Proj<'b>>::Assoc)>,
     (): Trait<<T as Proj<'static>>::Assoc>
+    //~^ ERROR overflow evaluating the requirement `(): Trait<<T as Proj<'static>>::Assoc>` [E0275]
 {
 }
 
