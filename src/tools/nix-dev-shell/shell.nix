925e46f5a423aa1515bab7f17918f0f2ca8cde3a
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2026-03-06 13:53:24 +0100

    add rustup to nix shell

--- src/tools/nix-dev-shell/shell.nix
+++ src/tools/nix-dev-shell/shell.nix
@@ -16,6 +16,7 @@ pkgs.mkShell {
     pkgs.nix
     pkgs.glibc.out
     pkgs.glibc.static
+    pkgs.rustup
     x
     # Get the runtime deps of the x wrapper
   ] ++ lists.flatten (attrsets.attrValues env);
