3e0572e7b62a118f845b349f9426d0ca61b7ed68
Jana Dönszelmann <jana@donsz.nl>
2026-03-07 10:26:23 +0100

    improve diagnostics for attrs that *contain* the word rustc

--- tests/ui/attributes/rustc-in-attr-path.rs
+++
@@ -1,9 +0,0 @@
-mod rustc {
-    pub use std::prelude::v1::test;
-}
-
-#[crate::rustc::test]
-//~^ ERROR: attributes starting with `rustc` are reserved for use by the `rustc` compiler
-fn foo() {}
-
-fn main() {}
