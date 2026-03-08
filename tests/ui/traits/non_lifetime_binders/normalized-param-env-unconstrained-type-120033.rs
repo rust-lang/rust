1df1c8a4bc046c169a8f2d3775e7aabaac361a9e
Takayuki Maeda <takoyaki0316@gmail.com>
2026-03-09 01:41:56 +0900

    add self-referential param-env normalization regression
    avoid ICE on invalid param-env normalization
    
    remove 120033 crash test
    
    fix comments
    
    use rustc_no_implicit_bounds

---
+++ tests/ui/traits/non_lifetime_binders/normalized-param-env-unconstrained-type-120033.rs
@@ -0,0 +1,21 @@
+//@ compile-flags: --crate-type=lib
+
+// Regression test for <https://github.com/rust-lang/rust/issues/120033>
+
+#![feature(rustc_attrs)]
+#![feature(non_lifetime_binders)] //~ WARN the feature `non_lifetime_binders` is incomplete
+#![rustc_no_implicit_bounds]
+
+pub trait Foo<T> {
+    type Bar<K>;
+}
+
+pub struct Bar<T: ?AutoTrait> {} //~ ERROR cannot find trait `AutoTrait`
+
+pub fn f<T1, T2>()
+where
+    T1: for<T> Foo<usize, Bar = Bar<T>>, //~ ERROR missing generics for associated type `Foo::Bar`
+    //~| ERROR missing generics for associated type `Foo::Bar`
+    T2: for<L, T> Foo<usize, Bar<T> = T1::Bar<T>>,
+{
+}
