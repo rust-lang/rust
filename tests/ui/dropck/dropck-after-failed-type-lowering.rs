94c3811c81bd6c5a30291c06e76a0d07580ad764
Makai <m4kai410@gmail.com>
2026-03-09 01:35:01 +0800

    compute the result of a projection type with region errors

--- tests/ui/dropck/dropck-after-failed-type-lowering.rs
+++ tests/ui/dropck/dropck-after-failed-type-lowering.rs
@@ -3,11 +3,12 @@
 trait B {
     type C<'a>;
     fn d<E>() -> F<E> {
+              //~^ ERROR: the trait bound `E: B` is not satisfied
         todo!()
     }
 }
 struct F<G> {
-    h: Option<<G as B>::C>,
+    h: Option<<G as B>::C>, //~ ERROR: the trait bound `G: B` is not satisfied
     //~^ ERROR missing generics for associated type `B::C`
 }
 
