b18def152402d7371402bf5ccd21c6b179ff04bf
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-03-09 18:17:07 +0100

    Extend raw-dylib test for ELF with reloc models

--- tests/run-make/raw-dylib-elf/main.rs
+++ tests/run-make/raw-dylib-elf/main.rs
@@ -2,7 +2,7 @@
 #![feature(thread_local)]
 #![allow(incomplete_features)]
 
-use std::ffi::c_int;
+use std::ffi::{CStr, c_char, c_int, c_long};
 
 #[link(name = "library", kind = "raw-dylib")]
 unsafe extern "C" {
@@ -10,10 +10,20 @@ unsafe extern "C" {
     static mut global_variable: c_int;
     #[thread_local]
     static mut tls_variable: c_int;
+    safe static const_array: [c_long; 2];
+    safe static const_string: *const c_char;
 }
 
 fn main() {
     unsafe {
-        println!("{} {} {}", this_is_a_library_function(), global_variable, tls_variable);
+        println!(
+            "{} {} {} {} {} {}",
+            this_is_a_library_function(),
+            global_variable,
+            tls_variable,
+            const_array[0],
+            const_array[1],
+            CStr::from_ptr(const_string).to_str().unwrap(),
+        );
     }
 }
