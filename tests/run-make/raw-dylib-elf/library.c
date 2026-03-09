3531ed9c1d7e866cf2b2c238efaca7e5fc3f4b0b
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-03-09 18:17:07 +0100

    Extend raw-dylib test for ELF with reloc models

--- tests/run-make/raw-dylib-elf/library.c
+++ tests/run-make/raw-dylib-elf/library.c
@@ -1,5 +1,7 @@
 int global_variable = 3;
 __thread int tls_variable = 33;
+const long const_array[] = {795, 906};
+const char *const_string = "sums of three cubes";
 
 int this_is_a_library_function() {
     return 42;
