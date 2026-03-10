ferrocene/ferrocene

Allow running unit/integration tests without doc tests and benches

src/bootstrap/src/core/build_steps/test.rs
@@ -50,7 +50,7 @@ use crate::utils::helpers::{
     linker_args, linker_flags, t, target_supports_cranelift_backend, up_to_date,
 };
 use crate::utils::render_tests::{add_flags_and_try_run_tests, try_run_tests};
-use crate::{CLang, CodegenBackendKind, DocTests, GitRepo, Mode, PathSet, envify};
+use crate::{CLang, CodegenBackendKind, GitRepo, Mode, PathSet, TestTarget, envify};
 
 mod compiletest;
 
@@ -183,7 +183,7 @@ You can skip linkcheck with --skip src/tools/linkchecker"
         );
         run_cargo_test(cargo, &[], &[], "linkchecker self tests", bootstrap_host, builder);
 
-        if builder.doc_tests == DocTests::No {
+        if !builder.test_target.runs_doctests() {
             return;
         }
 
@@ -828,15 +828,14 @@ impl Step for CargoMiri {
 
         // We're not using `prepare_cargo_test` so we have to do this ourselves.
         // (We're not using that as the test-cargo-miri crate is not known to bootstrap.)
-        match builder.doc_tests {
-            DocTests::Yes => {}
-            DocTests::No => {
-                cargo.args(["--lib", "--bins", "--examples", "--tests", "--benches"]);
+        match builder.test_target {
+            TestTarget::AllTargets => {
+                cargo.args(["--lib", "--bins", "--examples", "--tests", "--benches"])
             }
-            DocTests::Only => {
-                cargo.arg("--doc");
-            }
-        }
+            TestTarget::Default => &mut cargo,
+            TestTarget::Doc => cargo.arg("--doc"),
+            TestTarget::Tests => cargo.arg("--tests"),
+        };
         cargo.arg("--").args(builder.config.test_args());
 
         // Finally, run everything.
@@ -1214,7 +1213,7 @@ impl Step for RustdocGUI {
 
     fn is_default_step(builder: &Builder<'_>) -> bool {
         builder.config.nodejs.is_some()
-            && builder.doc_tests != DocTests::Only
+            && builder.test_target != TestTarget::Doc
             && get_browser_ui_test_version(builder).is_some()
     }
 
@@ -1304,7 +1303,7 @@ impl Step for Tidy {
     }
 
     fn is_default_step(builder: &Builder<'_>) -> bool {
-        builder.doc_tests != DocTests::Only
+        builder.test_target != TestTarget::Doc
     }
 
     fn make_run(run: RunConfig<'_>) {
@@ -1860,7 +1859,7 @@ impl Step for Compiletest {
     }
 
     fn run(self, builder: &Builder<'_>) {
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::Doc {
             return;
         }
 
@@ -3017,15 +3016,12 @@ fn prepare_cargo_test(
         cargo.arg("--message-format=json");
     }
 
-    match builder.doc_tests {
-        DocTests::Only => {
-            cargo.arg("--doc");
-        }
-        DocTests::No => {
-            cargo.args(["--bins", "--examples", "--tests", "--benches"]);
-        }
-        DocTests::Yes => {}
-    }
+    match builder.test_target {
+        TestTarget::AllTargets => cargo.args(["--bins", "--examples", "--tests", "--benches"]),
+        TestTarget::Default => &mut cargo,
+        TestTarget::Doc => cargo.arg("--doc"),
+        TestTarget::Tests => cargo.arg("--tests"),
+    };
 
     for krate in crates {
         cargo.arg("-p").arg(krate);
@@ -3937,7 +3933,7 @@ impl Step for CodegenCranelift {
         let host = run.build_triple();
         let compilers = RustcPrivateCompilers::new(run.builder, run.builder.top_stage, host);
 
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::Doc {
             return;
         }
 
@@ -4058,7 +4054,7 @@ impl Step for CodegenGCC {
         let host = run.build_triple();
         let compilers = RustcPrivateCompilers::new(run.builder, run.builder.top_stage, host);
 
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::Doc {
             return;
         }
 

src/bootstrap/src/lib.rs
@@ -183,13 +183,21 @@ impl std::str::FromStr for CodegenBackendKind {
 }
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-pub enum DocTests {
-    /// Run normal tests and doc tests (default).
-    Yes,
-    /// Do not run any doc tests.
-    No,
+pub enum TestTarget {
+    /// Run unit, integration and doc tests (default).
+    Default,
+    /// Run unit, integration, doc tests, examples, bins, benchmarks (no doc tests).
+    AllTargets,
     /// Only run doc tests.
-    Only,
+    Doc,
+    /// Only run unit and integration tests.
+    Tests,
+}
+
+impl TestTarget {
+    fn runs_doctests(&self) -> bool {
+        matches!(self, TestTarget::Doc | TestTarget::Default)
+    }
 }
 
 pub enum GitRepo {
@@ -230,7 +238,7 @@ pub struct Build {
     in_tree_gcc_info: GitInfo,
     local_rebuild: bool,
     fail_fast: bool,
-    doc_tests: DocTests,
+    test_target: TestTarget,
     verbosity: usize,
 
     /// Build triple for the pre-compiled snapshot compiler.
@@ -560,7 +568,7 @@ impl Build {
             initial_sysroot: config.initial_sysroot.clone(),
             local_rebuild: config.local_rebuild,
             fail_fast: config.cmd.fail_fast(),
-            doc_tests: config.cmd.doc_tests(),
+            test_target: config.cmd.test_target(),
             verbosity: config.exec_ctx.verbosity as usize,
 
             host_target: config.host_target,

rust-lang/rust

src/bootstrap/src/core/build_steps/test.rs
@@ -41,7 +41,7 @@ use crate::utils::helpers::{
     linker_args, linker_flags, t, target_supports_cranelift_backend, up_to_date,
 };
 use crate::utils::render_tests::{add_flags_and_try_run_tests, try_run_tests};
-use crate::{CLang, CodegenBackendKind, DocTests, GitRepo, Mode, PathSet, envify};
+use crate::{CLang, CodegenBackendKind, GitRepo, Mode, PathSet, TestTarget, envify};
 
 mod compiletest;
 
@@ -174,7 +174,7 @@ You can skip linkcheck with --skip src/tools/linkchecker"
         );
         run_cargo_test(cargo, &[], &[], "linkchecker self tests", bootstrap_host, builder);
 
-        if builder.doc_tests == DocTests::No {
+        if !builder.test_target.runs_doctests() {
             return;
         }
 
@@ -807,15 +807,14 @@ impl Step for CargoMiri {
 
         // We're not using `prepare_cargo_test` so we have to do this ourselves.
         // (We're not using that as the test-cargo-miri crate is not known to bootstrap.)
-        match builder.doc_tests {
-            DocTests::Yes => {}
-            DocTests::No => {
-                cargo.args(["--lib", "--bins", "--examples", "--tests", "--benches"]);
+        match builder.test_target {
+            TestTarget::AllTargets => {
+                cargo.args(["--lib", "--bins", "--examples", "--tests", "--benches"])
             }
-            DocTests::Only => {
-                cargo.arg("--doc");
-            }
-        }
+            TestTarget::Default => &mut cargo,
+            TestTarget::DocOnly => cargo.arg("--doc"),
+            TestTarget::Tests => cargo.arg("--tests"),
+        };
         cargo.arg("--").args(builder.config.test_args());
 
         // Finally, run everything.
@@ -1193,7 +1192,7 @@ impl Step for RustdocGUI {
 
     fn is_default_step(builder: &Builder<'_>) -> bool {
         builder.config.nodejs.is_some()
-            && builder.doc_tests != DocTests::Only
+            && builder.test_target != TestTarget::DocOnly
             && get_browser_ui_test_version(builder).is_some()
     }
 
@@ -1283,7 +1282,7 @@ impl Step for Tidy {
     }
 
     fn is_default_step(builder: &Builder<'_>) -> bool {
-        builder.doc_tests != DocTests::Only
+        builder.test_target != TestTarget::DocOnly
     }
 
     fn make_run(run: RunConfig<'_>) {
@@ -1839,7 +1838,7 @@ impl Step for Compiletest {
     }
 
     fn run(self, builder: &Builder<'_>) {
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::DocOnly {
             return;
         }
 
@@ -2957,15 +2956,12 @@ fn prepare_cargo_test(
         cargo.arg("--message-format=json");
     }
 
-    match builder.doc_tests {
-        DocTests::Only => {
-            cargo.arg("--doc");
-        }
-        DocTests::No => {
-            cargo.args(["--bins", "--examples", "--tests", "--benches"]);
-        }
-        DocTests::Yes => {}
-    }
+    match builder.test_target {
+        TestTarget::AllTargets => cargo.args(["--bins", "--examples", "--tests", "--benches"]),
+        TestTarget::Default => &mut cargo,
+        TestTarget::DocOnly => cargo.arg("--doc"),
+        TestTarget::Tests => cargo.arg("--tests"),
+    };
 
     for krate in crates {
         cargo.arg("-p").arg(krate);
@@ -3834,7 +3830,7 @@ impl Step for CodegenCranelift {
         let host = run.build_triple();
         let compilers = RustcPrivateCompilers::new(run.builder, run.builder.top_stage, host);
 
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::DocOnly {
             return;
         }
 
@@ -3955,7 +3951,7 @@ impl Step for CodegenGCC {
         let host = run.build_triple();
         let compilers = RustcPrivateCompilers::new(run.builder, run.builder.top_stage, host);
 
-        if builder.doc_tests == DocTests::Only {
+        if builder.test_target == TestTarget::DocOnly {
             return;
         }
 

src/bootstrap/src/lib.rs
@@ -177,13 +177,21 @@ impl std::str::FromStr for CodegenBackendKind {
 }
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-pub enum DocTests {
-    /// Run normal tests and doc tests (default).
-    Yes,
-    /// Do not run any doc tests.
-    No,
+pub enum TestTarget {
+    /// Run unit, integration and doc tests (default).
+    Default,
+    /// Run unit, integration, doc tests, examples, bins, benchmarks (no doc tests).
+    AllTargets,
     /// Only run doc tests.
-    Only,
+    DocOnly,
+    /// Only run unit and integration tests.
+    Tests,
+}
+
+impl TestTarget {
+    fn runs_doctests(&self) -> bool {
+        matches!(self, TestTarget::DocOnly | TestTarget::Default)
+    }
 }
 
 pub enum GitRepo {
@@ -222,7 +230,7 @@ pub struct Build {
     in_tree_gcc_info: GitInfo,
     local_rebuild: bool,
     fail_fast: bool,
-    doc_tests: DocTests,
+    test_target: TestTarget,
     verbosity: usize,
 
     /// Build triple for the pre-compiled snapshot compiler.
@@ -540,7 +548,7 @@ impl Build {
             initial_sysroot: config.initial_sysroot.clone(),
             local_rebuild: config.local_rebuild,
             fail_fast: config.cmd.fail_fast(),
-            doc_tests: config.cmd.doc_tests(),
+            test_target: config.cmd.test_target(),
             verbosity: config.exec_ctx.verbosity as usize,
 
             host_target: config.host_target,