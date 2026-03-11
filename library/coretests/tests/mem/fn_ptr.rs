e043db865826ba5db695270b5422402981eb668b
teor <teor@riseup.net>
2026-03-11 17:34:32 +1000

    Impl TypeInfo for splat

--- library/coretests/tests/mem/fn_ptr.rs
+++ library/coretests/tests/mem/fn_ptr.rs
@@ -5,6 +5,7 @@ const STRING_TY: TypeId = const { TypeId::of::<String>() };
 const U8_TY: TypeId = const { TypeId::of::<u8>() };
 const _U8_REF_TY: TypeId = const { TypeId::of::<&u8>() };
 const UNIT_TY: TypeId = const { TypeId::of::<()>() };
+const TUPLE_STRING_U8_TY: TypeId = const { TypeId::of::<(String, u8)>() };
 
 #[test]
 fn test_fn_ptrs() {
@@ -14,6 +15,8 @@ fn test_fn_ptrs() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<fn()>().kind })
     else {
         panic!();
@@ -31,6 +34,8 @@ fn test_ref() {
             inputs: &[ty1, ty2],
             output,
             variadic: false,
+            is_splatted: false,
+            splatted_index: _,
         }) = (const { Type::of::<fn(&u8, &u8)>().kind })
         else {
             panic!();
@@ -61,6 +66,8 @@ fn test_unsafe() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<unsafe fn()>().kind })
     else {
         panic!();
@@ -75,6 +82,8 @@ fn test_abi() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<extern "Rust" fn()>().kind })
     else {
         panic!();
@@ -87,6 +96,8 @@ fn test_abi() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<extern "C" fn()>().kind })
     else {
         panic!();
@@ -99,6 +110,8 @@ fn test_abi() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<unsafe extern "system" fn()>().kind })
     else {
         panic!();
@@ -114,6 +127,8 @@ fn test_inputs() {
         inputs: &[ty1, ty2],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<fn(String, u8)>().kind })
     else {
         panic!();
@@ -128,6 +143,8 @@ fn test_inputs() {
         inputs: &[ty1, ty2],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<fn(val: String, p2: u8)>().kind })
     else {
         panic!();
@@ -145,6 +162,8 @@ fn test_output() {
         inputs: &[],
         output,
         variadic: false,
+        is_splatted: false,
+        splatted_index: _,
     }) = (const { Type::of::<fn() -> u8>().kind })
     else {
         panic!();
@@ -160,10 +179,57 @@ fn test_variadic() {
         inputs: [ty1],
         output,
         variadic: true,
+        is_splatted: false,
+        splatted_index: _,
     }) = &(const { Type::of::<extern "C" fn(u8, ...)>().kind })
     else {
         panic!();
     };
     assert_eq!(output, &UNIT_TY);
     assert_eq!(*ty1, U8_TY);
+}
+
+#[test]
+fn test_splat() {
+    #[rustfmt::skip]
+    let TypeKind::FnPtr(fn_ptr_ty) = &(const { Type::of::<fn(#[splat] (String, u8))>().kind }) else {
+        panic!();
+    };
+    let FnPtr {
+        unsafety: false,
+        abi: Abi::ExternRust,
+        inputs: [ty1],
+        output,
+        variadic: false,
+        is_splatted: true,
+        splatted_index: 0,
+    } = fn_ptr_ty
+    else {
+        panic!();
+    };
+    assert_eq!(output, &UNIT_TY);
+    assert_eq!(*ty1, TUPLE_STRING_U8_TY);
+    assert_eq!(fn_ptr_ty.splatted(), Some(0));
+}
+
+#[test]
+fn test_not_splat() {
+    let TypeKind::FnPtr(fn_ptr_ty) = &(const { Type::of::<fn((String, u8))>().kind }) else {
+        panic!();
+    };
+    let FnPtr {
+        unsafety: false,
+        abi: Abi::ExternRust,
+        inputs: [ty1],
+        output,
+        variadic: false,
+        is_splatted: false,
+        splatted_index: _,
+    } = fn_ptr_ty
+    else {
+        panic!();
+    };
+    assert_eq!(output, &UNIT_TY);
+    assert_eq!(*ty1, TUPLE_STRING_U8_TY);
+    assert_eq!(fn_ptr_ty.splatted(), None);
 }
