87d8f5885b4f289477e48090a86356302437cec9
Esteban Küber <esteban@kuber.com.ar>
2026-03-14 17:23:34 +0000

    Provide more context on type errors in const context
    - On `const` and `static` point at the type (like we do for let bindings)
    - On fn calls, point at const parameter in fn definition
    - On type, point at const parameter in type definition
    - On array type lengths, explain that array length is always `usize`
    - On enum variant discriminant, mention `repr`

--- tests/ui/issues/issue-31910.rs
+++ tests/ui/issues/issue-31910.rs
@@ -1,9 +1,10 @@
 enum Enum<T: Trait> {
-    //~^ ERROR `T` is never used
-    //~| NOTE unused type parameter
+    //~^ ERROR: `T` is never used
+    //~| NOTE: unused type parameter
     X = Trait::Number,
-    //~^ ERROR mismatched types
-    //~| NOTE expected `isize`, found `i32`
+    //~^ ERROR: mismatched types
+    //~| NOTE: expected `isize`, found `i32`
+    //~| NOTE: enum variant discriminant
 }
 
 trait Trait {
