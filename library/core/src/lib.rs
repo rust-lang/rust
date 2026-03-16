8155209828619951db645443e720e3a33c4046b9
Folkert de Vries <folkert@folkertdev.nl>
2026-03-16 23:28:20 +0100

    enable the `movrs` target feature in `core` and `std`

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -182,6 +182,7 @@
 #![feature(hexagon_target_feature)]
 #![feature(loongarch_target_feature)]
 #![feature(mips_target_feature)]
+#![feature(movrs_target_feature)]
 #![feature(nvptx_target_feature)]
 #![feature(powerpc_target_feature)]
 #![feature(riscv_target_feature)]
