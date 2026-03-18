4853fde45b6e3429f553974298da8ce18d191198
Esteban Küber <esteban@kuber.com.ar>
2026-03-18 18:09:47 +0000

    Unify lifetime and identifier parsing

--- tests/ui/lexer/emoji-in-lifetime.rs
+++ tests/ui/lexer/emoji-in-lifetime.rs
@@ -1,9 +1,20 @@
 // #141081
-fn bad_lifetime_name<'🐛🐛🐛family👨‍👩‍👧‍👦>(_: &'🐛🐛🐛family👨‍👩‍👧‍👦 ()) {}
-//~^ ERROR: lifetimes cannot contain emoji
-//~| ERROR: lifetimes cannot contain emoji
+fn bad_lifetime_name<
+    '🐛🐛🐛family👨‍👩‍👧‍👦,//~ ERROR: lifetimes cannot have emoji
+    '12, //~ ERROR: lifetimes cannot start with a number
+    'a🐛, //~ ERROR: lifetimes cannot have emoji
+    '1🐛, //~ ERROR: invalid lifetime name
+    '1, //~ ERROR: lifetimes cannot start with a number
+    'a‌b // bare zero-width-joiners are accepted as XID_Continue
+>() {}
+
+
+
+
+
+
 fn main() {
-    '🐛: { //~ ERROR: lifetimes cannot contain emoji
+    '🐛: { //~ ERROR: lifetimes cannot have emoji
         todo!();
     };
 }
