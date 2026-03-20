0941e187ef276a707025f01ce513cf594611e834
cyrgani <ansgar.w.zielke@gmail.com>
2026-03-20 09:17:48 +0000

    reformat merged files

--- tests/ui/structs-enums/classes-cross-crate.rs
+++ tests/ui/structs-enums/classes-cross-crate.rs
@@ -1,47 +1,50 @@
 //@ run-pass
 //@ aux-build:cci_class_4.rs
-#![allow(non_camel_case_types)]
 
 extern crate cci_class_4;
-use cci_class_4::kitties::{cat, cat_unnamed};
+use cci_class_4::*;
 
 fn simple_cross_crate() {
-  let nyan : cat = cat_unnamed(52, 99);
-  let kitty = cat_unnamed(1000, 2);
-  assert_eq!(nyan.how_hungry, 99);
-  assert_eq!(kitty.how_hungry, 2);
-  nyan.noop();
+    let nyan: Cat = cat_unnamed(52, 99);
+    let kitty = cat_unnamed(1000, 2);
+    assert_eq!(nyan.how_hungry, 99);
+    assert_eq!(kitty.how_hungry, 2);
+    nyan.noop();
 }
 
 fn cross_crate() {
     let mut nyan = cat(0_usize, 2, "nyan".to_string());
     nyan.eat();
     assert!(!nyan.eat());
-    for _ in 1_usize..10_usize { nyan.speak(); };
+    for _ in 1_usize..10_usize {
+        nyan.speak();
+    }
     assert!(nyan.eat());
 }
 
-
 fn print_out(thing: Box<dyn ToString>, expected: String) {
-  let actual = (*thing).to_string();
-  println!("{}", actual);
-  assert_eq!(actual.to_string(), expected);
+    let actual = (*thing).to_string();
+    println!("{}", actual);
+    assert_eq!(actual.to_string(), expected);
 }
 
 fn separate_impl() {
-  let nyan: Box<dyn ToString> = Box::new(cat(0, 2, "nyan".to_string())) as Box<dyn ToString>;
-  print_out(nyan, "nyan".to_string());
+    let nyan: Box<dyn ToString> = Box::new(cat(0, 2, "nyan".to_string())) as Box<dyn ToString>;
+    print_out(nyan, "nyan".to_string());
 }
 
-trait noisy {
+trait Noisy {
     fn speak(&mut self) -> isize;
 }
 
-impl noisy for cat {
-    fn speak(&mut self) -> isize { self.meow(); 0 }
+impl Noisy for Cat {
+    fn speak(&mut self) -> isize {
+        self.meow();
+        0
+    }
 }
 
-fn make_speak<C:noisy>(mut c: C) {
+fn make_speak<C: Noisy>(mut c: C) {
     c.speak();
 }
 
@@ -54,48 +57,46 @@ fn implement_traits() {
     }
 }
 
+struct Dog {
+    barks: usize,
 
-struct dog {
-  barks: usize,
-
-  volume: isize,
+    volume: isize,
 }
 
-impl dog {
+impl Dog {
     fn bark(&mut self) -> isize {
-      println!("Woof {} {}", self.barks, self.volume);
-      self.barks += 1_usize;
-      if self.barks % 3_usize == 0_usize {
-          self.volume += 1;
-      }
-      if self.barks % 10_usize == 0_usize {
-          self.volume -= 2;
-      }
-      println!("Grrr {} {}", self.barks, self.volume);
-      self.volume
+        println!("Woof {} {}", self.barks, self.volume);
+        self.barks += 1_usize;
+        if self.barks % 3_usize == 0_usize {
+            self.volume += 1;
+        }
+        if self.barks % 10_usize == 0_usize {
+            self.volume -= 2;
+        }
+        println!("Grrr {} {}", self.barks, self.volume);
+        self.volume
     }
 }
 
-impl noisy for dog {
+impl Noisy for Dog {
     fn speak(&mut self) -> isize {
         self.bark()
     }
 }
 
-fn dog() -> dog {
-    dog {
-        volume: 0,
-        barks: 0_usize
+fn dog() -> Dog {
+    Dog { volume: 0, barks: 0_usize }
+}
+
+fn annoy_neighbors(critter: &mut dyn Noisy) {
+    for _i in 0_usize..10 {
+        critter.speak();
     }
 }
 
-fn annoy_neighbors(critter: &mut dyn noisy) {
-    for _i in 0_usize..10 { critter.speak(); }
-}
-
 fn multiple_types() {
-    let mut nyan: cat = cat(0_usize, 2, "nyan".to_string());
-    let mut whitefang: dog = dog();
+    let mut nyan: Cat = cat(0_usize, 2, "nyan".to_string());
+    let mut whitefang: Dog = dog();
     annoy_neighbors(&mut nyan);
     annoy_neighbors(&mut whitefang);
     assert_eq!(nyan.meow_count(), 10_usize);
@@ -104,7 +105,7 @@ fn multiple_types() {
 
 fn cast_to_trait() {
     let mut nyan = cat(0, 2, "nyan".to_string());
-    let nyan: &mut dyn noisy = &mut nyan;
+    let nyan: &mut dyn Noisy = &mut nyan;
     nyan.speak();
 }
 
