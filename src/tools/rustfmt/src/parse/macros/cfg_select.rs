8c8a3d722060737405bd9b2bb325ae65b505d4d5
Yacin Tmimi <yacintmimi@gmail.com>
2026-03-21 23:42:38 -0400

    feat: parse `cfg_select!` within rustfmt
    `cfg_select!` parsing needs to be implemented in rustfmt right now
    because there's no good way to call `rustc_attr_parsing::parse_cfg_select`.

--- src/tools/rustfmt/src/parse/macros/cfg_select.rs
+++ src/tools/rustfmt/src/parse/macros/cfg_select.rs
@@ -1,25 +1,32 @@
 use std::panic::{AssertUnwindSafe, catch_unwind};
 
 use rustc_ast::ast;
-use rustc_ast::token::TokenKind;
+use rustc_ast::token;
+use rustc_ast::token::{Token, TokenKind};
+use rustc_ast::tokenstream::TokenStream;
 use rustc_parse::exp;
 use rustc_parse::parser::{AllowConstBlockItems, ForceCollect};
+use rustc_span::Span;
+use tracing::debug;
 
 use crate::parse::macros::build_stream_parser;
 use crate::parse::session::ParseSess;
+use crate::spanned::Spanned;
 
-pub(crate) fn parse_cfg_select<'a>(
+pub(crate) fn parse_items_from_cfg_select<'a>(
     psess: &'a ParseSess,
     mac: &'a ast::MacCall,
 ) -> Result<Vec<ast::Item>, &'static str> {
-    match catch_unwind(AssertUnwindSafe(|| parse_cfg_select_inner(psess, mac))) {
+    match catch_unwind(AssertUnwindSafe(|| {
+        parse_items_from_cfg_select_inner(psess, mac)
+    })) {
         Ok(Ok(items)) => Ok(items),
         Ok(err @ Err(_)) => err,
         Err(..) => Err("failed to parse cfg_select!"),
     }
 }
 
-fn parse_cfg_select_inner<'a>(
+fn parse_items_from_cfg_select_inner<'a>(
     psess: &'a ParseSess,
     mac: &'a ast::MacCall,
 ) -> Result<Vec<ast::Item>, &'static str> {
@@ -77,4 +84,101 @@ fn parse_cfg_select_inner<'a>(
     }
 
     Ok(items)
+}
+
+pub(crate) enum CfgSelectFormatPredicate {
+    Cfg(ast::MetaItemInner),
+    Wildcard(Span),
+}
+
+impl Spanned for CfgSelectFormatPredicate {
+    fn span(&self) -> rustc_span::Span {
+        match self {
+            Self::Cfg(meta_item_inner) => meta_item_inner.span(),
+            Self::Wildcard(span) => *span,
+        }
+    }
+}
+
+pub(crate) struct CfgSelectArm {
+    pub(crate) predicate: CfgSelectFormatPredicate,
+    pub(crate) arrow: Token,
+    pub(crate) expr: Box<ast::Expr>,
+    pub(crate) trailing_comma: Option<Span>,
+}
+
+impl PartialEq for &CfgSelectArm {
+    fn eq(&self, other: &Self) -> bool {
+        // consider the arms equal if they have the same span
+        self.span() == other.span()
+    }
+}
+
+impl Spanned for CfgSelectArm {
+    fn span(&self) -> Span {
+        self.predicate
+            .span()
+            .with_hi(if let Some(comma) = self.trailing_comma {
+                comma.hi()
+            } else {
+                self.expr.span.hi()
+            })
+    }
+}
+
+impl std::fmt::Debug for CfgSelectArm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.predicate {
+            CfgSelectFormatPredicate::Cfg(cfg_entry) => cfg_entry.fmt(f)?,
+            CfgSelectFormatPredicate::Wildcard(t) => t.fmt(f)?,
+        };
+        write!(f, "=> {:?}", self.expr)
+    }
+}
+
+// FIXME(ytmimi) would be nice if rustfmt didn't need to implement parsing logic on its own
+// and could instead just call rustc_attr_parsing::parse_cfg_select, but this is fine for now.
+pub(crate) fn parse_cfg_select(psess: &ParseSess, ts: TokenStream) -> Option<Vec<CfgSelectArm>> {
+    let mut cfg_select_predicates = vec![];
+    let mut parser = build_stream_parser(psess.inner(), ts);
+
+    while parser.token != token::Eof {
+        let predicate = if parser.eat_keyword(exp!(Underscore)) {
+            CfgSelectFormatPredicate::Wildcard(parser.prev_token.span)
+        } else {
+            let Ok(meta_item) = parser.parse_meta_item_inner().map_err(|e| e.cancel()) else {
+                debug!("Failed to parse cfg entry in cfg_select! predicate");
+                return None;
+            };
+            CfgSelectFormatPredicate::Cfg(meta_item)
+        };
+
+        if let Err(_) = parser.expect(exp!(FatArrow)) {
+            debug!("Expected to find a `=>` after cfg_selec! predicate.");
+            return None;
+        };
+
+        let arrow = parser.prev_token;
+
+        let Ok(expr) = parser.parse_expr().map_err(|e| e.cancel()) else {
+            debug!("Couldn't parse cfg_select! arm body after `=>`.");
+            return None;
+        };
+
+        let trailing_comma = if parser.eat(exp!(Comma)) {
+            Some(parser.prev_token.span)
+        } else {
+            None
+        };
+
+        let arm = CfgSelectArm {
+            predicate,
+            arrow,
+            expr,
+            trailing_comma,
+        };
+
+        cfg_select_predicates.push(arm);
+    }
+    Some(cfg_select_predicates)
 }
