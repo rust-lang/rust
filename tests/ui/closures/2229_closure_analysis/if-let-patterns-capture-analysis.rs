63c15e4bd5762307e6e72a74e328024fa5c4b029
Embers-of-the-Fire <stellarishs@163.com>
2026-03-22 19:14:07 +0800

    test: cover current if let closure capture behavior

---
+++ tests/ui/closures/2229_closure_analysis/if-let-patterns-capture-analysis.rs
@@ -0,0 +1,54 @@
+//@ edition:2021
+
+#![feature(rustc_attrs)]
+#![feature(stmt_expr_attributes)]
+#![allow(irrefutable_let_patterns)]
+
+enum SingleVariant {
+    Pair(i32, String),
+}
+
+fn if_let_closure() {
+    let variant = SingleVariant::Pair(1, "hello".into());
+
+    let c = #[rustc_capture_analysis]
+    || {
+    //~^ ERROR First Pass analysis includes:
+    //~| ERROR Min Capture analysis includes:
+        if let SingleVariant::Pair(ref n, s) = variant {
+            //~^ NOTE: Capturing variant[] -> Immutable
+            //~| NOTE: Capturing variant[(0, 0)] -> Immutable
+            //~| NOTE: Capturing variant[(1, 0)] -> ByValue
+            //~| NOTE: Min Capture variant[] -> ByValue
+            let _ = (n, s);
+        }
+    };
+
+    c();
+}
+
+fn match_closure() {
+    let variant = SingleVariant::Pair(1, "hello".into());
+
+    let c = #[rustc_capture_analysis]
+    || {
+    //~^ ERROR First Pass analysis includes:
+    //~| ERROR Min Capture analysis includes:
+        match variant {
+            //~^ NOTE: Capturing variant[(0, 0)] -> Immutable
+            //~| NOTE: Capturing variant[(1, 0)] -> ByValue
+            //~| NOTE: Min Capture variant[(0, 0)] -> Immutable
+            //~| NOTE: Min Capture variant[(1, 0)] -> ByValue
+            SingleVariant::Pair(ref n, s) => {
+                let _ = (n, s);
+            }
+        }
+    };
+
+    c();
+}
+
+fn main() {
+    if_let_closure();
+    match_closure();
+}
