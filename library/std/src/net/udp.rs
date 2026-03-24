be7a9dd4c8b5ac451c1433d1d08b54e021117a6a
Luca Cominardi <luca.cominardi@datadoghq.com>
2026-03-24 12:27:38 +0100

    fix: add cfg guards on unsupported wasi p2 and p3 targets

--- library/std/src/net/udp.rs
+++ library/std/src/net/udp.rs
@@ -600,6 +600,7 @@ impl UdpSocket {
     /// socket.set_hop_limit_v6(88).expect("set_hop_limit_v6 call failed");
     /// ```
     #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
+    #[cfg(not(all(target_os = "wasi", any(target_env = "p2", target_env = "p3"))))]
     pub fn set_hop_limit_v6(&self, limit: u8) -> io::Result<()> {
         self.0.set_hop_limit_v6(limit)
     }
@@ -619,6 +620,7 @@ impl UdpSocket {
     /// assert_eq!(socket.hop_limit_v6().unwrap(), 88);
     /// ```
     #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
+    #[cfg(not(all(target_os = "wasi", any(target_env = "p2", target_env = "p3"))))]
     pub fn hop_limit_v6(&self) -> io::Result<u8> {
         self.0.hop_limit_v6()
     }
@@ -638,6 +640,7 @@ impl UdpSocket {
     /// socket.set_multicast_hop_limit_v6(88).expect("set_multicast_hop_limit_v6 call failed");
     /// ```
     #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
+    #[cfg(not(all(target_os = "wasi", any(target_env = "p2", target_env = "p3"))))]
     pub fn set_multicast_hop_limit_v6(&self, limit: u8) -> io::Result<()> {
         self.0.set_multicast_hop_limit_v6(limit)
     }
@@ -657,6 +660,7 @@ impl UdpSocket {
     /// assert_eq!(socket.multicast_hop_limit_v6().unwrap(), 88);
     /// ```
     #[unstable(feature = "ipv6_hop_limit", issue = "139166")]
+    #[cfg(not(all(target_os = "wasi", any(target_env = "p2", target_env = "p3"))))]
     pub fn multicast_hop_limit_v6(&self) -> io::Result<u8> {
         self.0.multicast_hop_limit_v6()
     }
