2a543acbaf7739b547ca95c1d8d49758cfd510f4
Aryan Dubey <aryandubey@umass.edu>
2026-03-25 10:13:13 +0000

    Moved and rename issue-50411 to tests/ui/mir/inliner-double-elaborate
    * Move issue-50411 to tests/ui/mir/inliner-double-elaborate
    * Addded the link for the issue to inliner-double-elaborate.rs
    * Fix tidy issues
    * Renamed to inliner-double-elaborate.rs

--- tests/ui/mir/inliner-double-elaborate.rs
+++ tests/ui/mir/inliner-double-elaborate.rs
@@ -1,4 +1,4 @@
-// Regression test for #50411: the MIR inliner was causing problems
+// Regression test for https://github.com/rust-lang/rust/issues/50411: the MIR inliner was causing problems
 // here because it would inline promoted code (which had already had
 // elaborate-drops invoked on it) and then try to elaboate drops a
 // second time. Uncool.
