193126af4ecb7e13881c620776608449154777f7
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2026-03-25 17:20:17 +0100

    Give up if `ensure_sufficient_stack` is called more than 1000 times
    Something has definitely gone wrong at this point. This is over 5
    million stack frames, certainly more than we'd ever hit in successful
    code. It's actually too many stack frames for the default panic handler,
    or GDB, or LLDB, to print. Instead add a manual stack overflow handler
    which only prints the first and last hundred stack frames.
    
    We can't even have a normal `println!("{backtrace}")` because
    symbolizing that many frames too long. Instead, resolve the frame
    lazily.

--- compiler/rustc_data_structures/src/stack.rs
+++ compiler/rustc_data_structures/src/stack.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{AtomicU16, Ordering};
+
 // This is the amount of bytes that need to be left on the stack before increasing the size.
 // It must be at least as large as the stack required by any code that does not call
 // `ensure_sufficient_stack`.
@@ -11,6 +13,13 @@ const STACK_PER_RECURSION: usize = 1024 * 1024; // 1MB
 #[cfg(target_os = "aix")]
 const STACK_PER_RECURSION: usize = 16 * 1024 * 1024; // 16MB
 
+thread_local! {
+    static TIMES_GROWN: AtomicU16 = const { AtomicU16::new(0) };
+}
+
+// Give up if we expand the stack this many times and are still trying to recurse deeper.
+const MAX_STACK_GROWTH: u16 = 2000;
+
 /// Grows the stack on demand to prevent stack overflow. Call this in strategic locations
 /// to "break up" recursive calls. E.g. almost any call to `visit_expr` or equivalent can benefit
 /// from this.
@@ -18,5 +27,22 @@ const STACK_PER_RECURSION: usize = 16 * 1024 * 1024; // 16MB
 /// Should not be sprinkled around carelessly, as it causes a little bit of overhead.
 #[inline]
 pub fn ensure_sufficient_stack<R>(f: impl FnOnce() -> R) -> R {
-    stacker::maybe_grow(RED_ZONE, STACK_PER_RECURSION, f)
+    // if we can't guess the remaining stack (unsupported on some platforms) we immediately grow
+    // the stack and then cache the new stack size (which we do know now because we allocated it.
+    let enough_space = match stacker::remaining_stack() {
+        Some(remaining) => remaining >= RED_ZONE,
+        None => false,
+    };
+    if enough_space {
+        f()
+    } else {
+        let times = TIMES_GROWN.with(|times| times.fetch_add(1, Ordering::Relaxed));
+        if times > MAX_STACK_GROWTH {
+            // something is *definitely* wrong.
+            panic!("still not enough stack after {MAX_STACK_GROWTH} expansions of dynamic stack; infinite recursion?");
+        }
+        let out = stacker::grow(STACK_PER_RECURSION, f);
+        TIMES_GROWN.with(|times| times.fetch_sub(1, Ordering::Relaxed));
+        out
+    }
 }
