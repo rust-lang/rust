rust-lang/rust-clippy

fix(explicit_counter_loop): suggest `.take(n)` for `for _ in 0..n` counter loops

clippy_lints/src/loops/explicit_counter_loop.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 
 use super::{EXPLICIT_COUNTER_LOOP, IncrementVisitor, InitializeVisitor, make_iterator_snippet};
 use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::higher::Range;
 use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::sugg::{EMPTY, Sugg};
 use clippy_utils::{get_enclosing_block, is_integer_const, is_integer_literal_untyped};
@@ -82,6 +83,26 @@ pub(super) fn check<'tcx>(
                     }
                     snippet
                 });
+
+                // If the loop variable is unused and the range is `0..n`, suggest `(init..).take(n)`.
+                if pat_snippet == "_"
+                    && let Some(range) = Range::hir(cx, arg)
+                    && range.limits == RangeLimits::HalfOpen
+                    && range.start.is_none_or(|start| is_integer_const(cx, start, 0))
+                    && let Some(end) = range.end
+                {
+                    let end = snippet_with_applicability(cx, end.span, "..", &mut applicability);
+                    diag.span_suggestion(
+                        span,
+                        "consider using",
+                        format!(
+                            "{loop_label}for {name} in ({}).take({end})",
+                            initializer.range(&EMPTY, RangeLimits::HalfOpen)
+                        ),
+                        applicability,
+                    );
+                    return;
+                }
 
                 diag.span_suggestion(
                     span,

rust-lang/rust

clippy_lints/src/loops/explicit_counter_loop.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 
 use super::{EXPLICIT_COUNTER_LOOP, IncrementVisitor, InitializeVisitor, make_iterator_snippet};
 use clippy_utils::diagnostics::span_lint_and_then;
+use clippy_utils::higher::Range;
 use clippy_utils::source::snippet_with_applicability;
 use clippy_utils::sugg::{EMPTY, Sugg};
 use clippy_utils::{get_enclosing_block, is_integer_const, is_integer_literal_untyped};
@@ -82,6 +83,26 @@ pub(super) fn check<'tcx>(
                     }
                     snippet
                 });
+
+                // If the loop variable is unused and the range is `0..n`, suggest `(init..).take(n)`.
+                if pat_snippet == "_"
+                    && let Some(range) = Range::hir(cx, arg)
+                    && range.limits == RangeLimits::HalfOpen
+                    && range.start.is_some_and(|start| is_integer_const(cx, start, 0))
+                    && let Some(end) = range.end
+                {
+                    let end = snippet_with_applicability(cx, end.span, "..", &mut applicability);
+                    diag.span_suggestion(
+                        span,
+                        "consider using",
+                        format!(
+                            "{loop_label}for {name} in ({}).take({end})",
+                            initializer.range(&EMPTY, RangeLimits::HalfOpen)
+                        ),
+                        applicability,
+                    );
+                    return;
+                }
 
                 diag.span_suggestion(
                     span,