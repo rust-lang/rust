607b0620e2bfd8ee0c7ed42e83ec35aa88ed6984
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-03-27 22:21:22 +0100

    Analysis for externally implementable statics

--- compiler/rustc_builtin_macros/src/errors.rs
+++ compiler/rustc_builtin_macros/src/errors.rs
@@ -1117,8 +1117,24 @@ pub(crate) struct EiiExternTargetExpectedUnsafe {
 }
 
 #[derive(Diagnostic)]
-#[diag("`#[{$name}]` is only valid on functions")]
-pub(crate) struct EiiSharedMacroExpectedFunction {
+#[diag("`#[{$name}]` is only valid on functions and statics")]
+pub(crate) struct EiiSharedMacroTarget {
+    #[primary_span]
+    pub span: Span,
+    pub name: String,
+}
+
+#[derive(Diagnostic)]
+#[diag("`#[{$name}]` cannot be used on statics with a value")]
+pub(crate) struct EiiStaticDefault {
+    #[primary_span]
+    pub span: Span,
+    pub name: String,
+}
+
+#[derive(Diagnostic)]
+#[diag("`#[{$name}]` requires the name as an explicit argument when used on a static")]
+pub(crate) struct EiiStaticArgumentRequired {
     #[primary_span]
     pub span: Span,
     pub name: String,
