089ee5e5be2aa3fc9190cef795c80805d51be957
Orryson Yu Otuma <orrysonotuma@gmail.com>
2026-03-27 14:05:54 +0300

    Clarify that isqrt returns the principal (non-negative) square root

--- library/core/src/num/int_macros.rs
+++ library/core/src/num/int_macros.rs
@@ -1827,7 +1827,11 @@ macro_rules! int_impl {
             }
         }
 
-        /// Returns the square root of the number, rounded down.
+        /// Returns the integer square root of the number, rounded down.
+        ///
+        /// This function returns the **principal (non-negative) square root**.
+        /// For a given number `n`, although both `x` and `-x` satisfy `x^2 = n`,
+        /// this function always returns the non-negative value.
         ///
         /// Returns `None` if `self` is negative.
         ///
@@ -3122,7 +3126,11 @@ macro_rules! int_impl {
             }
         }
 
-        /// Returns the square root of the number, rounded down.
+        /// Returns the integer square root of the number, rounded down.
+        ///
+        /// This function returns the **principal (non-negative) square root**.
+        /// For a given number `n`, although both `x` and `-x` satisfy `x^2 = n`,
+        /// this function always returns the non-negative value.
         ///
         /// # Panics
         ///
