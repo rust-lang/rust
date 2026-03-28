6d8c10ebca0bcb628bfcfead2b62e5aa784faed5
Aryan Dubey <aryandubey@umass.edu>
2026-03-28 14:32:22 -0400

    Added description and orignal issue link to enum_u8_variant.rs

--- tests/ui/enum/enum_u8_variant.rs
+++ tests/ui/enum/enum_u8_variant.rs
@@ -1,4 +1,6 @@
 //@ run-pass
+//Test that checks pattern matching works correctly on a repr(u8) enum whose only variant contains an inner 'u8' field
+//https://github.com/rust-lang/rust/issues/34571
 #[repr(u8)]
 enum Foo {
     Foo(#[allow(dead_code)] u8),
