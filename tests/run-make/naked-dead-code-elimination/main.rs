4e80d2b29f766dc7dcb97cf12bccd6b3ac85c1cb
Folkert de Vries <folkert@folkertdev.nl>
2026-03-30 00:44:56 +0200

    fix macho section specifier & windows test

--- tests/run-make/naked-dead-code-elimination/main.rs
+++ tests/run-make/naked-dead-code-elimination/main.rs
@@ -1,9 +1,15 @@
+#![feature(cfg_target_object_format)]
 use std::arch::naked_asm;
 
 #[unsafe(naked)]
 #[unsafe(no_mangle)]
 extern "C" fn used() {
     naked_asm!("ret")
+}
+
+#[unsafe(no_mangle)]
+extern "C" fn unused_clothed() -> i32 {
+    42
 }
 
 #[unsafe(naked)]
@@ -13,10 +19,18 @@ extern "C" fn unused() {
 }
 
 #[unsafe(naked)]
-#[unsafe(link_section = "foobar")]
+#[cfg_attr(target_object_format = "macho", unsafe(link_section = "__TEXT,foobar"))]
+#[cfg_attr(not(target_object_format = "macho"), unsafe(link_section = ".foobar"))]
 #[unsafe(no_mangle)]
 extern "C" fn unused_link_section() {
     naked_asm!("ret")
+}
+
+#[cfg_attr(target_object_format = "macho", unsafe(link_section = "__TEXT,baz"))]
+#[cfg_attr(not(target_object_format = "macho"), unsafe(link_section = ".baz"))]
+#[unsafe(no_mangle)]
+extern "C" fn unused_link_section_clothed() -> i32 {
+    43
 }
 
 fn main() {
