dfa2e249154f2c57bedf5d50e549e32f1c87d6fc
mejrs <59372212+mejrs@users.noreply.github.com>
2026-03-30 22:49:45 +0200

    Enable diagnostic::on_const for local impls

---
+++ tests/ui/diagnostic_namespace/on_const/it_works_foreign.rs
@@ -0,0 +1,15 @@
+//@ aux-build: non_const_impl.rs
+#![crate_type = "lib"]
+
+extern crate non_const_impl;
+
+use non_const_impl::X;
+
+const _: () = {
+    let x = X;
+    x == x;
+    //~^ ERROR: their message
+    //~| NOTE: trait `PartialEq` is implemented but not `const`
+    //~| NOTE: their note
+    //~| NOTE: their other note
+};
