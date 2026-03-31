ebf22f83f10e17ec3840b23c799b68ce674d995f
yukang <moorekang@gmail.com>
2026-03-31 19:54:28 +0800

    fix unit test

--- tests/ui/lint/unused/overwritten-before-read-note-issue-154434.rs
+++
@@ -1,9 +0,0 @@
-//@ check-pass
-//@ compile-flags: -W unused-assignments
-
-fn main() {
-    let mut d = String::from("hello"); //~ WARN value assigned to `d` is never read
-    d = String::from("ahoy");
-
-    println!("{d}, world!");
-}
