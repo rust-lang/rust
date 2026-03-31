cb4025be4890042227502f3d2b559779748b56b6
Trevor Gross <tg@trevorgross.com>
2026-03-31 17:56:23 -0400

    c-b: Remove the `mangled-names` feature from `builtins-test`
    This isn't used anywhere in CI and there doesn't seem to be a reason
    that it would need to be toggled. Enable `mangled-names` on the
    `compiler-builtins` dependency by default and remove the from
    `builtins-test`.

--- library/compiler-builtins/builtins-test/tests/lse.rs
+++ library/compiler-builtins/builtins-test/tests/lse.rs
@@ -1,7 +1,7 @@
 #![allow(unused_features)]
 #![feature(decl_macro)] // so we can use pub(super)
 #![feature(macro_metavar_expr_concat)]
-#![cfg(all(target_arch = "aarch64", feature = "mangled-names"))]
+#![cfg(target_arch = "aarch64")]
 
 use std::sync::Mutex;
 
