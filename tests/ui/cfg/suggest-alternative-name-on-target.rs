1d79216182b79e2e93ec29e177095a696becab19
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2026-04-02 18:04:34 +0000

    `#[cfg]`: suggest alternative `target_` name when the value does not match

--- tests/ui/cfg/suggest-alternative-name-on-target.rs
+++ tests/ui/cfg/suggest-alternative-name-on-target.rs
@@ -5,34 +5,35 @@
 #[cfg(target_abi = "arm")]
 //~^ ERROR unexpected `cfg` condition value:
 //~| NOTE see <https://doc.rust-lang.org
-//~| NOTE expected values for `target_abi` are
+//~| HELP `arm` is an expected value for `target_arch`
 struct A;
 
 // target env used in `target_arch`
 #[cfg(target_arch = "gnu")]
 //~^ ERROR unexpected `cfg` condition value:
 //~| NOTE see <https://doc.rust-lang.org
-//~| NOTE expected values for `target_arch` are
+//~| HELP `gnu` is an expected value for `target_env`
 struct B;
 
 // target os used in `target_env`
 #[cfg(target_env = "openbsd")]
 //~^ ERROR unexpected `cfg` condition value:
 //~| NOTE see <https://doc.rust-lang.org
-//~| NOTE expected values for `target_env` are
+//~| HELP `openbsd` is an expected value for `target_os`
 struct C;
 
 // target abi used in `target_os`
 #[cfg(target_os = "eabi")]
 //~^ ERROR unexpected `cfg` condition value:
 //~| NOTE see <https://doc.rust-lang.org
-//~| NOTE expected values for `target_os` are
+//~| HELP `eabi` is an expected value for `target_abi`
 struct D;
 
 #[cfg(target_abi = "windows")]
 //~^ ERROR unexpected `cfg` condition value:
 //~| NOTE see <https://doc.rust-lang.org
-//~| NOTE expected values for `target_abi` are
+//~| HELP `windows` is an expected value for `target_os`
+//~| HELP `windows` is an expected value for `target_family`
 struct E;
 
 fn main() {}
