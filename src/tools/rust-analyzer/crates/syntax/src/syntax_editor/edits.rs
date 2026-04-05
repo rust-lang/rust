3dd6020f65d825b1d4ccf48ccd076fa7afb94941
bit-aloo <sshourya17@gmail.com>
2026-04-05 11:06:50 +0530

    update syntax with new syntaxeditor

--- src/tools/rust-analyzer/crates/syntax/src/syntax_editor/edits.rs
+++ src/tools/rust-analyzer/crates/syntax/src/syntax_editor/edits.rs
@@ -473,8 +473,9 @@ enum Foo {
     }
 
     fn check_add_variant(before: &str, expected: &str, variant: ast::Variant) {
-        let enum_ = ast_from_text::<ast::Enum>(before);
-        let mut editor = SyntaxEditor::new(enum_.syntax().clone());
+        let (mut editor, enum_) =
+            SyntaxEditor::new(ast_from_text::<ast::Enum>(before).syntax().clone());
+        let enum_ = ast::Enum::cast(enum_).unwrap();
         if let Some(it) = enum_.variant_list() {
             it.add_variant(&mut editor, &variant)
         }
