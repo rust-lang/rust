ddf9d4cfd5a4b20ad5bfaffd36fb8f4624a61ac8
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-04-06 19:08:23 +0200

    suppress unreachable code lint in automatically derived impls
    This is relevant for `derive(Clone, PartialEq)` on adts with fields of
    type never.

--- tests/ui/never_type/regress/builtin_derives_on_adts_with_never.rs
+++ tests/ui/never_type/regress/builtin_derives_on_adts_with_never.rs
@@ -6,20 +6,14 @@
 #![warn(unreachable_code)]
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-//~^ warn: unreachable
 pub struct S(!);
-//~^ warn: unreachable
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-//~^ warn: unreachable
 pub struct S2 {
     f: !,
-    //~^ warn: unreachable
 }
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-//~^ warn: unreachable
-//~| warn: unreachable
 pub enum E {
     E2(!),
     E3 { f: ! },
