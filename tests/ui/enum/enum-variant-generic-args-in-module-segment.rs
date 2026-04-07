d4173eca488fdb9d56920d54ba3a70e2f54762d3
León Orell Valerian Liehr <me@fmease.dev>
2026-04-08 01:03:35 +0200

    Verify that penultimate segment of enum variant path refers to enum if it has args

---
+++ tests/ui/enum/enum-variant-generic-args-in-module-segment.rs
@@ -0,0 +1,16 @@
+// Test that we don't assume that the penultimate segment of an enum variant path refers to the
+// enum just because it has generic arguments.
+//
+// issue: <https://github.com/rust-lang/rust/issues/154962>
+
+enum Enum<T> { Variant, Carry(T) }
+
+mod module { pub(super) use super::Enum::Variant; }
+
+fn main() {
+    let _ = module::<i32>::Variant;
+    //~^ ERROR type arguments are not allowed on module `module`
+
+    let _ = self::module::<()>::Variant {};
+    //~^ ERROR type arguments are not allowed on module `module`
+}
