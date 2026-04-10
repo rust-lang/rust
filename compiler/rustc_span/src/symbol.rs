d33560957ed6ed229d08f807631a4dcae4bf128b
Nik Revenco <pm@nikrev.com>
2026-04-10 16:07:39 +0100

    Extract `$$crate` into a separate, incomplete feature gate
    Co-authored-by: c410-f3r <c410-f3r@users.noreply.github.com>
    
    uwu

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1201,6 +1201,7 @@ symbols! {
         macro_literal_matcher,
         macro_metavar_expr,
         macro_metavar_expr_concat,
+        macro_metavar_expr_dollar_dollar_crate,
         macro_reexport,
         macro_use,
         macro_vis_matcher,
