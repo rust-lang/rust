a0301449b4664630435d6bac8287a9b49f78c00d
Peter Jaszkowiak <p.jaszkow@gmail.com>
2026-04-10 09:24:30 -0600

    test that Range loops optimize away
    but RangeInclusive loops do not

---
+++ tests/codegen-llvm/range-iter-loop-opts.rs
@@ -0,0 +1,60 @@
+// This test ensures that Range iterators are optimizable, to
+// the point that some loops can be entirely optimized out.
+
+//@ compile-flags: -Copt-level=3
+
+#![crate_type = "lib"]
+
+use std::ops::{Range, RangeInclusive};
+
+// CHECK-LABEL: @range_noop_loop(
+#[no_mangle]
+pub unsafe fn range_noop_loop() {
+    // CHECK-NEXT: start:
+    // CHECK-NEXT: ret void
+
+    // This loop should be optimized out entirely.
+    for _ in 0_u8..100 {
+        ()
+    }
+}
+
+// CHECK-LABEL: @range_count(
+#[no_mangle]
+pub unsafe fn range_count(s: u8, e: u8) -> usize {
+    //  CHECK-NOT: br {{.*}}
+    //      CHECK: ret i64
+
+    // This loop should be optimized to arithmetic.
+    let mut count = 0;
+    for _ in s..e {
+        count += 1;
+    }
+    count
+}
+
+// RangeInclusive currently cannot optimize the same way.
+
+// CHECK-LABEL: @rangeinclusive_noop_loop(
+#[no_mangle]
+pub unsafe fn rangeinclusive_noop_loop() {
+    //      CHECK: br {{.*}}
+    //      CHECK: ret void
+
+    for _ in 0_u8..=100 {
+        ()
+    }
+}
+
+// CHECK-LABEL: @rangeinclusive_count(
+#[no_mangle]
+pub unsafe fn rangeinclusive_count(s: u8, e: u8) -> usize {
+    //      CHECK: br {{.*}}
+    //      CHECK: ret i64
+
+    let mut count = 0;
+    for _ in s..=e {
+        count += 1;
+    }
+    count
+}
