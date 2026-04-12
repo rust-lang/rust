f35ea0eb8e04936b39f9e9d46cc4a33ab78006ab
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-04-12 12:39:09 +0200

    Properly check arguments of `#[rustc_allow_const_fn_unstable]`

--- tests/ui/attributes/args-checked.rs
+++ tests/ui/attributes/args-checked.rs
@@ -1,3 +1,5 @@
+#![feature(rustc_attrs)]
+
 #[inline(always = 5)]
 //~^ ERROR malformed
 #[inline(always(x, y, z))]
@@ -19,3 +21,9 @@ fn main() {
 macro_rules! m {
     () => {};
 }
+
+#[rustc_allow_const_fn_unstable(x = 5)]
+//~^ ERROR `rustc_allow_const_fn_unstable` expects feature names
+#[rustc_allow_const_fn_unstable(x(x, y, z))]
+//~^ ERROR `rustc_allow_const_fn_unstable` expects feature names
+const fn g() {}
