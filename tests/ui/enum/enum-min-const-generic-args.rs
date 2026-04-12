28594d7ebefed8d44e7570ac6e3b59263e64750a
JayanAXHF <sunil.chdry@gmail.com>
2026-04-12 18:48:27 +0530

    fix(mgca): Allow specifying generic args (of enum) on enum itself in unit & tuple variant constructions in (direct) const args

---
+++ tests/ui/enum/enum-min-const-generic-args.rs
@@ -0,0 +1,30 @@
+//@ run-pass
+
+#![feature(min_generic_const_args)]
+#![feature(adt_const_params, unsized_const_params)]
+#![allow(dead_code)]
+
+#[derive(PartialEq, Eq, std::marker::ConstParamTy)]
+enum Enum<T> {
+    Unit,
+    Tuple(),
+    Store(T),
+}
+
+// FIXME:  Ctor(Variant, Const)
+type const _: Enum<()> = Enum::<()>::Unit;
+
+// FIXME:  Ctor(Variant, Fn)
+type const _: Enum<()> = Enum::<()>::Tuple();
+
+// OK:  Variant
+type const _: Enum<()> = Enum::<()>::Unit {};
+
+
+type const _: Enum<()> = Enum::Unit::<()>; // OK
+type const _: Enum<()> = Enum::Tuple::<()>(); // OK
+
+type const _: Enum<()> = const { Enum::<()>::Unit }; // (OK)
+type const _: Enum<()> = const { Enum::<()>::Tuple() }; // (OK)
+
+fn main() {}
