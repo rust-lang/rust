d66d64fb460d8913b071b1593597afa061c64cf3
Jacob Adam <software@jacobadam.net>
2026-04-12 22:34:14 +0100

    Extend both tests with the example from https://github.com/rust-lang/rust/issues/103073

--- tests/mir-opt/building/match/aggregate_array_eq.rs
+++ tests/mir-opt/building/match/aggregate_array_eq.rs
@@ -13,3 +13,25 @@ pub fn array_match(x: [u8; 4]) -> bool {
     // CHECK-NOT: switchInt(copy _1[
     matches!(x, [1, 2, 3, 4])
 }
+
+pub enum MyEnum {
+    A,
+    B,
+    C,
+    D,
+}
+
+// Regression test for https://github.com/rust-lang/rust/issues/103073.
+// EMIT_MIR aggregate_array_eq.try_from_matched.built.after.mir
+pub fn try_from_matched(value: [u8; 4]) -> Result<MyEnum, ()> {
+    // CHECK-LABEL: fn try_from_matched(
+    // CHECK: <[u8; 4] as PartialEq>::eq
+    // CHECK-NOT: switchInt(copy (*_2)[
+    match &value {
+        b"ABCD" => Ok(MyEnum::A),
+        b"EFGH" => Ok(MyEnum::B),
+        b"IJKL" => Ok(MyEnum::C),
+        b"MNOP" => Ok(MyEnum::D),
+        _ => Err(()),
+    }
+}
