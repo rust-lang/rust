302710b7a7fde1466af0d06c92b77dd77e57469f
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-04-12 12:48:46 +0200

    Properly check arguments of `#[rustc_abi]`

--- compiler/rustc_attr_parsing/src/attributes/test_attrs.rs
+++ compiler/rustc_attr_parsing/src/attributes/test_attrs.rs
@@ -157,7 +157,7 @@ impl<S: Stage> SingleAttributeParser<S> for RustcAbiParser {
         let mut fail_incorrect_argument =
             |span| cx.adcx().expected_specific_argument(span, &[sym::assert_eq, sym::debug]);
 
-        let Some(arg) = arg.meta_item() else {
+        let Some(arg) = arg.meta_item_no_args() else {
             fail_incorrect_argument(args.span);
             return None;
         };
