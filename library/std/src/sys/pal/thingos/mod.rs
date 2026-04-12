9b431e30cb6e61721638f8183c0fbb4c8fdb8175
Travis Reed <tdreed@gmail.com>
2026-04-12 16:07:48 -0700

    fix: add compile-time error for unsupported unwind panic strategy in ThingOS std PAL

--- library/std/src/sys/pal/thingos/mod.rs
+++ library/std/src/sys/pal/thingos/mod.rs
@@ -1,5 +1,12 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
+// ThingOS targets are currently abort-only for panics.
+// Emit a clear compile-time error for any attempt to build std with unwind.
+#[cfg(panic = "unwind")]
+compile_error!(
+    "ThingOS std PAL is abort-only: panic=unwind is not supported for target_os=thingos"
+);
+
 pub mod common;
 pub mod futex;
 pub mod os;
