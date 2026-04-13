5c80d9031dcba3f8e49ce8fb131b6248f2249bca
Takayuki Maeda <takoyaki0316@gmail.com>
2026-04-14 02:08:02 +0900

    add a link to issue 154487

--- tests/ui/lint/unused-features/used-doc-cfg.rs
+++ tests/ui/lint/unused-features/used-doc-cfg.rs
@@ -1,5 +1,7 @@
 //@ check-pass
 //@ compile-flags: --check-cfg=cfg(feature,values("enabled_feature"))
+// Regression test for https://github.com/rust-lang/rust/issues/154487
+
 #![crate_type = "lib"]
 #![deny(unused_features)]
 #![feature(doc_cfg)]
