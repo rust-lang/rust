7027a6419fa61fb2fbdb72f532e35243f5a02dd8
Brian Oiko <oikobrian99@gmail.com>
2026-04-14 08:09:39 +0300

    Add failing UI test for invalid whitespace (zero width space)
    This adds a //@ check-fail test to ensure that disallowed whitespace
    characters like ZERO WIDTH SPACE are rejected by the Rust lexer.

---
+++ tests/ui/whitespace/invalid_whitespace.rs
@@ -0,0 +1,16 @@
+// This test ensures that the Rust lexer rejects invalid whitespace
+// characters that are not part of Unicode Pattern_White_Space.
+//
+// In this case, we use ZERO WIDTH SPACE (\u{200B}), which should
+// not be accepted as valid whitespace by the lexer.
+
+//@ check-fail
+
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    // The line below contains a ZERO WIDTH SPACE between `a` and `=`
+    let a​= x + y;
+    //~^ ERROR unknown start of token
+}
