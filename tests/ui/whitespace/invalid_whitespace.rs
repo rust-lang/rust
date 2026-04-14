a1eb2312c8afb040b2f266a3e6ca95ffbbdc7c3f
Brian Oiko <oikobrian99@gmail.com>
2026-04-14 23:01:13 +0300

    Correct whitespace in variable declaration
    Fix formatting issue by adding space around '=' in variable declaration.

--- tests/ui/whitespace/invalid_whitespace.rs
+++ tests/ui/whitespace/invalid_whitespace.rs
@@ -4,7 +4,7 @@
 fn main() {
     let x = 5;
     let y = 10;
-    let a=​ x + y;
+    let a=​x + y;
     //~^ ERROR unknown start of token: \u{200b}
     //~| HELP invisible characters like '\u{200b}' are not usually visible in text editors
 }
