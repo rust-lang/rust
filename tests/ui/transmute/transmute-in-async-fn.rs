70116e15f009f46f6774c18ee4da397caaa5229f
human9000 <human9000.2021@gmail.com>
2026-04-15 16:52:49 +0500

    Update transmute test for typeck child cases

--- tests/ui/transmute/transmute-in-async-fn.rs
+++ tests/ui/transmute/transmute-in-async-fn.rs
@@ -4,3 +4,9 @@ pub async fn a() -> u32 {
     unsafe { std::mem::transmute(1u64) }
     //~^error: cannot transmute between types of different sizes, or dependently-sized types
 }
+
+pub async fn b() -> u32 {
+    let closure = || unsafe { std::mem::transmute(1u64) };
+    //~^ ERROR: cannot transmute between types of different sizes, or dependently-sized types [E0512]
+    closure()
+}
