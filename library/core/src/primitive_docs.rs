39bdc73288f71fa05f92df144b718dc67cfdbe0e
Alice Ryhl <aliceryhl@google.com>
2026-04-16 08:42:44 +0000

    Take note about C back out
    This was too confusing. This section is about Rust-to-Rust calls, so it
    sounds like it's referring to how core::ffi::c_int and similar behave,
    but that is not the case (it's about how C does it, which differs from
    what core::ffi::c_int does), and either way it is off-topic for
    Rust-to-Rust calls.

--- library/core/src/primitive_docs.rs
+++ library/core/src/primitive_docs.rs
@@ -1908,10 +1908,8 @@ mod prim_ref {}
 /// CFI uses the name of the type for its compatibility check.
 ///
 /// When not using the `-Zsanitizer-cfi-normalize-integers` flag, the CFI sanitizer further
-/// restricts the rules by considering `usize` incompatible with the `uN` integer type of the same
-/// size, and similarly for `isize`. In addition, different C integer types are also given
-/// incompatible CFI types even if they boil down to the same size/signedness. Without this flag,
-/// there are some C integers that do not correspond to *any* Rust integer type.
+/// restricts the rules by considering `usize`/`isize` incompatible with the `uN`/`iN` integer type
+/// of the same size.
 ///
 /// As sanitizers are unstable, these rules may change in the future. This section only documents
 /// cases where CFI disagrees with the usual Rust ABI-compatibility rules, and is not meant to be a
