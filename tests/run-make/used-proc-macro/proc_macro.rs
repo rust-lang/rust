8830fc7c1f198028cdbaad68cee263f128cf55f0
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2026-04-17 14:22:07 +0200

    make tests/run-make/used-proc-macro more reliable
    the test was relying on the linker not discarding the `#[used]` symbol
    
    this commit modifies the proc macro to read the `#[used]` symbol to ensure it
    is not discarded by the linker

--- tests/run-make/used-proc-macro/proc_macro.rs
+++ tests/run-make/used-proc-macro/proc_macro.rs
@@ -1,3 +1,24 @@
 #![crate_type = "proc-macro"]
 
+use proc_macro::TokenStream;
+
 extern crate dep as _;
+extern crate proc_macro;
+
+#[proc_macro]
+pub fn mymacro(input: TokenStream) -> TokenStream {
+    extern "C" {
+        static VERY_IMPORTANT_SYMBOL: u32;
+    }
+
+    // read the symbol otherwise the _linker_ may discard it
+    // `#[used]` only preserves the symbol up to the compiler output (.o file)
+    // which is then passed to the linker. the linker is free to discard the
+    // `#[used]` symbol if it's not accessed/referred-to by other object
+    // files (crates)
+    let symbol_value = unsafe { core::ptr::addr_of!(VERY_IMPORTANT_SYMBOL).read_volatile() };
+
+    // the exact logic here is unimportant for the rmake check
+    let is_that_version = symbol_value == 12345;
+    if is_that_version { input } else { panic!() }
+}
