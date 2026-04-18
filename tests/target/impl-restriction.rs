7fe33db4682ca29ead12dda73c982a98daea4985
CoCo-Japan-pan <115922543+CoCo-Japan-pan@users.noreply.github.com>
2026-04-18 23:41:16 +0900

    Update `rustfmt`

--- tests/target/impl-restriction.rs
+++ tests/target/impl-restriction.rs
@@ -6,10 +6,10 @@ pub impl(in crate) trait Bar {}
 
 pub impl(in foo::bar) trait Baz {}
 
-pub const impl(self) trait QuxConst {}
+pub impl(self) const trait QuxConst {}
 
-pub auto impl(super) trait QuxAuto {}
+pub impl(super) auto trait QuxAuto {}
 
-pub unsafe impl(in crate) trait QuxUnsafe {}
+pub impl(in crate) unsafe trait QuxUnsafe {}
 
-pub const unsafe impl(in super::foo) trait QuxConstUnsafe {}
+pub impl(in super::foo) const unsafe trait QuxConstUnsafe {}
