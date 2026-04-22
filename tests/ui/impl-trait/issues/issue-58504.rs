a1d794b63f2f940438d201b8bd7511ec84cc8fd8
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-04-22 20:05:17 +0200

    bless tests

--- tests/ui/impl-trait/issues/issue-58504.rs
+++ tests/ui/impl-trait/issues/issue-58504.rs
@@ -1,12 +1,17 @@
-#![feature(coroutines, coroutine_trait, never_type)]
+#![feature(coroutines, coroutine_trait)]
 
 use std::ops::Coroutine;
 
-fn mk_gen() -> impl Coroutine<Return=!, Yield=()> {
-    #[coroutine] || { loop { yield; } }
+fn mk_gen() -> impl Coroutine<Return = !, Yield = ()> {
+    #[coroutine]
+    || {
+        loop {
+            yield;
+        }
+    }
 }
 
 fn main() {
-    let gens: [impl Coroutine<Return=!, Yield=()>;2] = [ mk_gen(), mk_gen() ];
+    let gens: [impl Coroutine<Return = !, Yield = ()>; 2] = [mk_gen(), mk_gen()];
     //~^ ERROR `impl Trait` is not allowed in the type of variable bindings
 }
