b7f337e4b2b40f3fb2c4fd46300a8b1a7adee438
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-04-23 18:01:21 +0200

    Replace windows-sys with a dummy crate in the library workspace
    This reduces the vendor size of the standard library from 37MB to 20MB.

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -543,7 +543,7 @@ const PERMITTED_STDLIB_DEPENDENCIES: &[&str] = &[
     "wasip2",
     "wasip3",
     "windows-link",
-    "windows-sys",
+    "windows-sys@0.61.100", // Enforce the usage of our dummy windows-sys patch. Keep version in sync.
     "wit-bindgen",
     // tidy-alphabetical-end
 ];
