9d3db4a9a0cacb9cff1fe945e0833e4c44a3df3e
EFanZh <efanzh@gmail.com>
2026-04-24 22:52:07 +0800

    Add LLVM codegen tests for new `Arc` implementation

--- tests/codegen-llvm/lib-optimizations/rc-arc-optimizations.rs
+++ tests/codegen-llvm/lib-optimizations/rc-arc-optimizations.rs
@@ -3,6 +3,7 @@
 #![crate_type = "lib"]
 
 use std::rc::{self, Rc};
+use std::sync::{self, Arc};
 
 // Ensures that we can create array of `Weak`s using `memset`.
 
@@ -15,7 +16,17 @@ pub fn array_of_rc_weak() -> [rc::Weak<u32>; 100] {
     [(); 100].map(|()| rc::Weak::new())
 }
 
-// Ensures that we convert `&Option<Rc<T>>` to `Option<&T>` without checking for `None`.
+#[no_mangle]
+pub fn array_of_sync_weak() -> [sync::Weak<u32>; 100] {
+    // CHECK-LABEL: @array_of_sync_weak(
+    // CHECK-NEXT: start:
+    // CHECK-NEXT: call void @llvm.memset.
+    // CHECK-NEXT: ret void
+    [(); 100].map(|()| sync::Weak::new())
+}
+
+// Ensures that we convert `&Option<Rc<T>>` and `&Option<Arc<T>>` to `Option<&T>` without checking
+// for `None`.
 
 #[no_mangle]
 pub fn option_rc_as_deref_no_cmp(rc: &Option<Rc<u32>>) -> Option<&u32> {
@@ -24,4 +35,13 @@ pub fn option_rc_as_deref_no_cmp(rc: &Option<Rc<u32>>) -> Option<&u32> {
     // CHECK-NEXT: %[[RC:.+]] = load ptr, ptr %rc
     // CHECK-NEXT: ret ptr %[[RC]]
     rc.as_deref()
+}
+
+#[no_mangle]
+pub fn option_arc_as_deref_no_cmp(arc: &Option<Arc<u32>>) -> Option<&u32> {
+    // CHECK-LABEL: @option_arc_as_deref_no_cmp(ptr
+    // CHECK-NEXT: start:
+    // CHECK-NEXT: %[[ARC:.+]] = load ptr, ptr %arc
+    // CHECK-NEXT: ret ptr %[[ARC]]
+    arc.as_deref()
 }
