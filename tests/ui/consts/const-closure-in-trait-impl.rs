d173b3068b7d0d771e5c256092d732161bfb471c
Oli Scherer <rust@oli-obk.de>
2026-04-25 09:47:07 +0200

    Add regression test

---
+++ tests/ui/consts/const-closure-in-trait-impl.rs
@@ -0,0 +1,25 @@
+#![feature(const_closures, const_destruct, const_trait_impl)]
+
+use std::marker::Destruct;
+use std::num::NonZero;
+
+const trait T {
+    fn a(&mut self, f: impl [const] Fn() + [const] Destruct);
+    fn b(&mut self);
+}
+
+struct S;
+
+impl const T for S {
+    fn a(&mut self, f: impl [const] Fn() + [const] Destruct) {
+        f()
+    }
+
+    fn b(&mut self) {
+        self.a(const || {});
+        //~^ ERROR: cannot use `const` closures outside of const contexts
+        //~| ERROR: [const] Fn()` is not satisfied
+    }
+}
+
+fn main() {}
