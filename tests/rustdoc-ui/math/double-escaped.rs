328864826e13e70bb9b8fc7ebcf15c8dabc8f849
Michael Howell <michael@notriddle.com>
2026-04-26 21:23:49 -0700

    rustdoc: implement LaTeX conversion with math-core

---
+++ tests/rustdoc-ui/math/double-escaped.rs
@@ -0,0 +1,8 @@
+//@ run-rustfix
+#![doc(syntax="+tex_math_dollars")]
+#![deny(rustdoc::invalid_math)]
+
+//! Let $f(x) \in \mathbb{Z}\[x\]$ be non-zero primitive squarefree polynomial
+//~^ ERROR unknown
+//~| HELP double-escaped
+//! of degree at least $1$$
