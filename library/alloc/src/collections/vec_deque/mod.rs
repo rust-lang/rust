ec8c18fdb83bbda486ad0e175dfc407727076341
Sidney Cammeresi <sac@cheesecake.org>
2026-05-02 19:35:54 -0700

    Un-copy-paste Dropper in VecDeque code

--- library/alloc/src/collections/vec_deque/mod.rs
+++ library/alloc/src/collections/vec_deque/mod.rs
@@ -133,21 +133,21 @@ impl<T: Clone, A: Allocator + Clone> Clone for VecDeque<T, A> {
     }
 }
 
+/// Runs the destructor for all items in the slice when it gets dropped (normally or
+/// during unwinding).
+struct Dropper<'a, T>(&'a mut [T]);
+
+impl<T> Drop for Dropper<'_, T> {
+    fn drop(&mut self) {
+        unsafe {
+            ptr::drop_in_place(self.0);
+        }
+    }
+}
+
 #[stable(feature = "rust1", since = "1.0.0")]
 unsafe impl<#[may_dangle] T, A: Allocator> Drop for VecDeque<T, A> {
     fn drop(&mut self) {
-        /// Runs the destructor for all items in the slice when it gets dropped (normally or
-        /// during unwinding).
-        struct Dropper<'a, T>(&'a mut [T]);
-
-        impl<'a, T> Drop for Dropper<'a, T> {
-            fn drop(&mut self) {
-                unsafe {
-                    ptr::drop_in_place(self.0);
-                }
-            }
-        }
-
         let (front, back) = self.as_mut_slices();
         unsafe {
             let _back_dropper = Dropper(back);
@@ -1433,18 +1433,6 @@ impl<T, A: Allocator> VecDeque<T, A> {
     #[doc(alias = "retain_front")]
     #[stable(feature = "deque_extras", since = "1.16.0")]
     pub fn truncate(&mut self, len: usize) {
-        /// Runs the destructor for all items in the slice when it gets dropped (normally or
-        /// during unwinding).
-        struct Dropper<'a, T>(&'a mut [T]);
-
-        impl<'a, T> Drop for Dropper<'a, T> {
-            fn drop(&mut self) {
-                unsafe {
-                    ptr::drop_in_place(self.0);
-                }
-            }
-        }
-
         // Safe because:
         //
         // * Any slice passed to `drop_in_place` is valid; the second case has
@@ -1499,18 +1487,6 @@ impl<T, A: Allocator> VecDeque<T, A> {
     #[doc(alias = "truncate_front")]
     #[stable(feature = "vec_deque_truncate_front", since = "CURRENT_RUSTC_VERSION")]
     pub fn retain_back(&mut self, len: usize) {
-        /// Runs the destructor for all items in the slice when it gets dropped (normally or
-        /// during unwinding).
-        struct Dropper<'a, T>(&'a mut [T]);
-
-        impl<'a, T> Drop for Dropper<'a, T> {
-            fn drop(&mut self) {
-                unsafe {
-                    ptr::drop_in_place(self.0);
-                }
-            }
-        }
-
         unsafe {
             if len >= self.len {
                 // No action is taken
