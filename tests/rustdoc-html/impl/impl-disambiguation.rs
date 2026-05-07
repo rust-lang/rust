69035d6151a5af25acf0e13ebb1e6edcbc4f5041
Qai Juang <237468078+qaijuang@users.noreply.github.com>
2026-05-07 09:37:09 -0400

    rustdoc: Disambiguate identical type names in impl headers

--- tests/rustdoc-html/impl/impl-disambiguation.rs
+++ tests/rustdoc-html/impl/impl-disambiguation.rs
@@ -28,3 +28,41 @@ impl Foo for mod1::Baz {}
 //@ has foo/trait.Foo.html '//*[@id="implementors-list"]//h3[@class="code-header"]' \
 //     "impl<'a> Foo for &'a foo::mod2::Baz"
 impl<'a> Foo for &'a mod2::Baz {}
+
+pub mod ops {
+    pub struct Range<T>(pub T);
+}
+
+pub mod range {
+    pub struct Range<T>(pub T);
+}
+
+// Regression test for https://github.com/rust-lang/rust/issues/154960.
+//@ has foo/range/struct.Range.html '//section[@class="impl"]//h3[@class="code-header"]' \
+//     "impl<T> From<foo::ops::Range<T>> for foo::range::Range<T>"
+impl<T> From<ops::Range<T>> for range::Range<T> {
+    fn from(range: ops::Range<T>) -> Self {
+        Self(range.0)
+    }
+}
+
+pub trait HasAssoc {
+    type Assoc;
+}
+
+pub trait ProjectionBound {}
+
+pub struct Projected<T>(pub T);
+
+// Regression test for https://github.com/rust-lang/rust/issues/154960.
+//@ has foo/struct.Projected.html '//section[@class="impl"]//h3[@class="code-header"]' \
+//     "impl<T> From<T> for Projected<T>where T: HasAssoc, T::Assoc: ProjectionBound,"
+impl<T> From<T> for Projected<T>
+where
+    T: HasAssoc,
+    T::Assoc: ProjectionBound,
+{
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
