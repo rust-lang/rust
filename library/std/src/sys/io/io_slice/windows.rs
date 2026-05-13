3a4475e91ffe7ec163bbcf32ad7591f3a871767e
Zac Harrold <zac@harrold.com.au>
2026-05-13 11:29:50 +1000

    Make `std::sys::io::io_slice` compatible with `core`
    - Unsafe core must be documented.
    - Types from dependencies must be inlined.
    - These types are only used within `core` so `pub` must be replaced with `pub(crate)`.

--- library/std/src/sys/io/io_slice/windows.rs
+++ library/std/src/sys/io/io_slice/windows.rs
@@ -1,30 +1,39 @@
 use crate::marker::PhantomData;
 use crate::slice;
-use crate::sys::c;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct WSABUF {
+    len: u32,
+    buf: *mut u8,
+}
 
 #[derive(Copy, Clone)]
 #[repr(transparent)]
-pub struct IoSlice<'a> {
-    vec: c::WSABUF,
+pub(crate) struct IoSlice<'a> {
+    vec: WSABUF,
     _p: PhantomData<&'a [u8]>,
 }
 
 impl<'a> IoSlice<'a> {
     #[inline]
-    pub fn new(buf: &'a [u8]) -> IoSlice<'a> {
+    pub(crate) fn new(buf: &'a [u8]) -> IoSlice<'a> {
         assert!(buf.len() <= u32::MAX as usize);
         IoSlice {
-            vec: c::WSABUF { len: buf.len() as u32, buf: buf.as_ptr() as *mut u8 },
+            vec: WSABUF { len: buf.len() as u32, buf: buf.as_ptr() as *mut u8 },
             _p: PhantomData,
         }
     }
 
     #[inline]
-    pub fn advance(&mut self, n: usize) {
+    pub(crate) fn advance(&mut self, n: usize) {
         if (self.vec.len as usize) < n {
             panic!("advancing IoSlice beyond its length");
         }
 
+        // SAFETY:
+        //  * `n <= len` as asserted above.
+        //  * The allocation pointed to by `buf` is valid up to `buf + len`.
         unsafe {
             self.vec.len -= n as u32;
             self.vec.buf = self.vec.buf.add(n);
@@ -32,33 +41,35 @@ impl<'a> IoSlice<'a> {
     }
 
     #[inline]
-    pub const fn as_slice(&self) -> &'a [u8] {
+    pub(crate) const fn as_slice(&self) -> &'a [u8] {
+        // SAFETY:
+        //  * `buf` and `len` come from a prior decomposition of a valid slice.
         unsafe { slice::from_raw_parts(self.vec.buf, self.vec.len as usize) }
     }
 }
 
 #[repr(transparent)]
-pub struct IoSliceMut<'a> {
-    vec: c::WSABUF,
+pub(crate) struct IoSliceMut<'a> {
+    vec: WSABUF,
     _p: PhantomData<&'a mut [u8]>,
 }
 
 impl<'a> IoSliceMut<'a> {
     #[inline]
-    pub fn new(buf: &'a mut [u8]) -> IoSliceMut<'a> {
+    pub(crate) fn new(buf: &'a mut [u8]) -> IoSliceMut<'a> {
         assert!(buf.len() <= u32::MAX as usize);
-        IoSliceMut {
-            vec: c::WSABUF { len: buf.len() as u32, buf: buf.as_mut_ptr() },
-            _p: PhantomData,
-        }
+        IoSliceMut { vec: WSABUF { len: buf.len() as u32, buf: buf.as_mut_ptr() }, _p: PhantomData }
     }
 
     #[inline]
-    pub fn advance(&mut self, n: usize) {
+    pub(crate) fn advance(&mut self, n: usize) {
         if (self.vec.len as usize) < n {
             panic!("advancing IoSliceMut beyond its length");
         }
 
+        // SAFETY:
+        //  * `n <= len` as asserted above.
+        //  * The allocation pointed to by `buf` is valid up to `buf + len`.
         unsafe {
             self.vec.len -= n as u32;
             self.vec.buf = self.vec.buf.add(n);
@@ -66,17 +77,23 @@ impl<'a> IoSliceMut<'a> {
     }
 
     #[inline]
-    pub fn as_slice(&self) -> &[u8] {
+    pub(crate) fn as_slice(&self) -> &[u8] {
+        // SAFETY:
+        //  * `buf` and `len` come from a prior decomposition of a valid slice.
         unsafe { slice::from_raw_parts(self.vec.buf, self.vec.len as usize) }
     }
 
     #[inline]
-    pub const fn into_slice(self) -> &'a mut [u8] {
+    pub(crate) const fn into_slice(self) -> &'a mut [u8] {
+        // SAFETY:
+        //  * `buf` and `len` come from a prior decomposition of a valid slice.
         unsafe { slice::from_raw_parts_mut(self.vec.buf, self.vec.len as usize) }
     }
 
     #[inline]
-    pub fn as_mut_slice(&mut self) -> &mut [u8] {
+    pub(crate) fn as_mut_slice(&mut self) -> &mut [u8] {
+        // SAFETY:
+        //  * `buf` and `len` come from a prior decomposition of a valid slice.
         unsafe { slice::from_raw_parts_mut(self.vec.buf, self.vec.len as usize) }
     }
 }
