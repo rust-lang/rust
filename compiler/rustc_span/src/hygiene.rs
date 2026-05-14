22ee24ef7303464fd25d6b82fe68bad9c7fb3f43
Matyas Susits <susits.matyas@gmail.com>
2026-05-14 10:11:26 +0200

    use SyntaxContext instead of raw u32 in HygieneEncodeContext::encode

--- compiler/rustc_span/src/hygiene.rs
+++ compiler/rustc_span/src/hygiene.rs
@@ -707,7 +707,7 @@ impl SyntaxContext {
     }
 
     #[inline]
-    pub(crate) const fn as_u32(self) -> u32 {
+    pub const fn as_u32(self) -> u32 {
         self.0
     }
 
@@ -1319,7 +1319,7 @@ impl HygieneEncodeContext {
     pub fn encode<T>(
         &self,
         encoder: &mut T,
-        mut encode_ctxt: impl FnMut(&mut T, u32, &SyntaxContextKey),
+        mut encode_ctxt: impl FnMut(&mut T, SyntaxContext, &SyntaxContextKey),
         mut encode_expn: impl FnMut(&mut T, ExpnId, &ExpnData, ExpnHash),
     ) {
         // When we serialize a `SyntaxContextData`, we may end up serializing
@@ -1344,7 +1344,7 @@ impl HygieneEncodeContext {
             });
             for (ctxt, ctxt_key) in all_ctxt_data {
                 if self.serialized_ctxts.lock().insert(ctxt) {
-                    encode_ctxt(encoder, ctxt.0, &ctxt_key);
+                    encode_ctxt(encoder, ctxt, &ctxt_key);
                 }
             }
 
