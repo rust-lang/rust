01f6bb52b347cadfff97843872cc76f608059e8d
Josh Stone <jistone@redhat.com>
2026-05-14 15:36:33 -0700

    Revert "Rollup merge of #154994 - dianne:no-dbg-temp, r=Mark-Simulacrum"
    This reverts commit 59b36a52837f112f4adfb496e493324ddfc5294c, reversing
    changes made to 197eb8c6c0a495f52d71c8e7bda98818010dee55.
    
    (cherry picked from commit 73931c4422b8fef480278afd9b4209fff5b1c0ff)

--- library/std/src/macros.rs
+++ library/std/src/macros.rs
@@ -369,8 +369,7 @@ macro_rules! dbg {
 /// E.g. `dbg_internal!(() () (1, 2))` expands into
 /// ```rust, ignore
 /// match (1, 2) {
-///     args => {
-///         let (tmp_1, tmp_2) = args;
+///     (tmp_1, tmp_2) => {
 ///         eprint!("...", &tmp_1, &tmp_2, /* some other arguments */);
 ///         (tmp_1, tmp_2)
 ///     }
@@ -386,9 +385,7 @@ pub macro dbg_internal {
         // of temporaries - https://stackoverflow.com/a/48732525/1063961
         // Always put the arguments in a tuple to avoid an unused parens lint on the pattern.
         match ($($processed,)+) {
-            // Move the entire tuple so it doesn't stick around as a temporary (#154988).
-            args => {
-                let ($($bound,)+) = args;
+            ($($bound,)+) => {
                 $crate::eprint!(
                     $crate::concat!($($piece),+),
                     $(
