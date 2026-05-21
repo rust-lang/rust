9466492a1a6dcda8848c079ba9eff6bcefc7b942
Zac Harrold <zac@harrold.com.au>
2026-05-21 21:44:29 +1000

    Move `std::io::append_to_string` to `alloc::io`

--- library/alloc/src/io/read.rs
+++ library/alloc/src/io/read.rs
@@ -1,4 +1,6 @@
-use crate::io::{BorrowedCursor, IoSliceMut, Result};
+use crate::io::{BorrowedCursor, Error, IoSliceMut, Result};
+use crate::string::String;
+use crate::vec::Vec;
 
 // Bare metal platforms usually have very small amounts of RAM
 // (in the order of hundreds of KB)
@@ -8,6 +10,57 @@ pub const DEFAULT_BUF_SIZE: usize = cfg_select! {
     target_os = "espidf" => { 512 },
     _ => { 8 * 1024 }
 };
+
+struct Guard<'a> {
+    buf: &'a mut Vec<u8>,
+    len: usize,
+}
+
+impl Drop for Guard<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            self.buf.set_len(self.len);
+        }
+    }
+}
+
+// Several `read_to_string` and `read_line` methods in the standard library will
+// append data into a `String` buffer, but we need to be pretty careful when
+// doing this. The implementation will just call `.as_mut_vec()` and then
+// delegate to a byte-oriented reading method, but we must ensure that when
+// returning we never leave `buf` in a state such that it contains invalid UTF-8
+// in its bounds.
+//
+// To this end, we use an RAII guard (to protect against panics) which updates
+// the length of the string when it is dropped. This guard initially truncates
+// the string to the prior length and only after we've validated that the
+// new contents are valid UTF-8 do we allow it to set a longer length.
+//
+// The unsafety in this function is twofold:
+//
+// 1. We're looking at the raw bytes of `buf`, so we take on the burden of UTF-8
+//    checks.
+// 2. We're passing a raw buffer to the function `f`, and it is expected that
+//    the function only *appends* bytes to the buffer. We'll get undefined
+//    behavior if existing bytes are overwritten to have non-UTF-8 data.
+#[doc(hidden)]
+#[unstable(feature = "core_io_internals", reason = "exposed only for libstd", issue = "none")]
+pub unsafe fn append_to_string<F>(buf: &mut String, f: F) -> Result<usize>
+where
+    F: FnOnce(&mut Vec<u8>) -> Result<usize>,
+{
+    let mut g = Guard { len: buf.len(), buf: unsafe { buf.as_mut_vec() } };
+    let ret = f(g.buf);
+
+    // SAFETY: the caller promises to only append data to `buf`
+    let appended = unsafe { g.buf.get_unchecked(g.len..) };
+    if str::from_utf8(appended).is_err() {
+        ret.and_then(|_| Err(Error::INVALID_UTF8))
+    } else {
+        g.len = g.buf.len();
+        ret
+    }
+}
 
 #[doc(hidden)]
 #[unstable(feature = "core_io_internals", reason = "exposed only for libstd", issue = "none")]
