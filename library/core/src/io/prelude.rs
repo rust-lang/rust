8a11a24b8b3ace297ae480a5fce0bbdb2d1a1198
Zac Harrold <zac@harrold.com.au>
2026-05-22 08:38:41 +1000

    Setup `core::io::prelude`

---
+++ library/core/src/io/prelude.rs
@@ -0,0 +1,12 @@
+//! The I/O Prelude.
+//!
+//! The purpose of this module is to alleviate imports of many common I/O traits
+//! by adding a glob import to the top of I/O heavy modules:
+//!
+//! ```
+//! # #![allow(unused_imports)]
+//! use std::io::prelude::*;
+//! ```
+
+#[stable(feature = "rust1", since = "1.0.0")]
+pub use crate::io::{Seek, Write};
