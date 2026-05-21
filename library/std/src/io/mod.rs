bc86e9f2786d2c3e9a351b71415097e460ffb01c
Zac Harrold <zac@harrold.com.au>
2026-05-21 11:18:11 +1000

    Move general IO tests to `alloctests`

--- library/std/src/io/mod.rs
+++ library/std/src/io/mod.rs
@@ -294,9 +294,6 @@
 
 #![stable(feature = "rust1", since = "1.0.0")]
 
-#[cfg(test)]
-mod tests;
-
 #[unstable(feature = "raw_os_error_ty", issue = "107792")]
 pub use alloc_crate::io::RawOsError;
 #[doc(hidden)]
