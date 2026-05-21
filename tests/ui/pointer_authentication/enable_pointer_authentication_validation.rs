c57d0b607026201f515fe5cbc217ad1d21f2ea72
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-05-21 13:11:35 +0000

    Document -Zpointer-authentication option

--- tests/ui/pointer_authentication/enable_pointer_authentication_validation.rs
+++ tests/ui/pointer_authentication/enable_pointer_authentication_validation.rs
@@ -1,13 +1,22 @@
+//@ ignore-backends: gcc
 //@ revisions: empty unprefixed all_unknown all_known mixed
 
-//@[empty] compile-flags: -Zpointer-authentication=
-//@[unprefixed] compile-flags: -Zpointer-authentication=auth-traps
-//@[all_unknown] compile-flags: -Zpointer-authentication=+I,+do,-not,-exist
+//@[empty] needs-llvm-components: aarch64
+//@[empty] compile-flags: --target aarch64-unknown-linux-pauthtest -Zpointer-authentication=
+//@[unprefixed] needs-llvm-components: aarch64
+//@[unprefixed] compile-flags: --target aarch64-unknown-linux-pauthtest -Zpointer-authentication=auth-traps
+//@[all_unknown] needs-llvm-components: aarch64
+//@[all_unknown] compile-flags: --target aarch64-unknown-linux-pauthtest -Zpointer-authentication=+I,+do,-not,-exist
 //@[all_known] check-pass
-//@[all_known] compile-flags: -Zpointer-authentication=+elf-got,-init-fini
-//@[mixed] compile-flags: -Zpointer-authentication=+elf-got,-imaginary
+//@[all_known] needs-llvm-components: aarch64
+//@[all_known] compile-flags: --target aarch64-unknown-linux-pauthtest -Zpointer-authentication=+elf-got,-init-fini
+//@[mixed] needs-llvm-components: aarch64
+//@[mixed] compile-flags: --target aarch64-unknown-linux-pauthtest -Zpointer-authentication=+elf-got,-imaginary
 
-fn main() {}
+#![feature(no_core)]
+#![no_std]
+#![no_main]
+#![no_core]
 
 //[empty]~? ERROR incorrect value `` for unstable option `pointer-authentication`
 //[unprefixed]~? ERROR incorrect value `auth-traps` for unstable option `pointer-authentication`
