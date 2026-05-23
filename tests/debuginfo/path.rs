6ed5bf2f529dc103a9cecfd237d445643c20ec30
fallofpheonix <ujjosing@gmail.com>
2026-05-24 00:43:32 +0530

    Remove po checks from debuginfo path test

--- tests/debuginfo/path.rs
+++ tests/debuginfo/path.rs
@@ -7,9 +7,9 @@
 //@ lldb-command:run
 
 //@ lldb-command:print pathbuf
-//@ lldb-check:[...] "/some/path" [...]
+//@ lldb-check:[...] "/some/path" { inner = "/some/path" { inner = { inner = size=10 { [0] = '/' [1] = 's' [2] = 'o' [3] = 'm' [4] = 'e' [5] = '/' [6] = 'p' [7] = 'a' [8] = 't' [9] = 'h' } } } }
 //@ lldb-command:print path
-//@ lldb-check:[...] "/some/path" [...]
+//@ lldb-check:[...] "/some/path" { data_ptr = [...] length = 10 }
 
 use std::path::Path;
 
