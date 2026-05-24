57fdf541a5e25a002bcbd66b5c49cd5427fcaa62
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-05-24 16:07:44 +0200

    Fix tests

--- tests/ui/panic-handler/panic-handler-std.rs
+++ tests/ui/panic-handler/panic-handler-std.rs
@@ -1,11 +1,13 @@
 //@ normalize-stderr: "loaded from .*libstd-.*.rmeta" -> "loaded from SYSROOT/libstd-*.rmeta"
 
+//~? ERROR multiple implementations of `#[panic_handler]`
+
 extern crate core;
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(info: PanicInfo) -> ! { //~ ERROR found duplicate lang item `panic_impl`
+fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
