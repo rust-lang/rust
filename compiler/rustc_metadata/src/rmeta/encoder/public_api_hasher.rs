7a2a60ca257908172e0c19728a092d3349ceb512
Matyas Susits <susits.matyas@gmail.com>
2026-05-25 18:26:09 +0200

    rdr: save whether public api hashing was used while encoding the metadata

--- compiler/rustc_metadata/src/rmeta/encoder/public_api_hasher.rs
+++ compiler/rustc_metadata/src/rmeta/encoder/public_api_hasher.rs
@@ -356,6 +356,7 @@ pub(crate) struct HashableCrateRoot {
 
     // FIXME do we need to hash this?
     pub(crate) specialization_enabled_in: bool,
+    pub(crate) public_api_hash_opt_enabled: bool,
 }
 
 impl HashableCrateRoot {
@@ -442,6 +443,7 @@ impl HashableCrateRoot {
             symbol_mangling_version: self.symbol_mangling_version,
 
             specialization_enabled_in: self.specialization_enabled_in,
+            public_api_hash_opt_enabled: self.public_api_hash_opt_enabled,
         }
     }
 }
