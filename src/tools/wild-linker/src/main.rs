dcf9cbce75ec9c5b2a1597168ce1aa19d33d74e3
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-05-26 14:01:07 +0200

    Update Wild to 0.9

--- src/tools/wild-linker/src/main.rs
+++ src/tools/wild-linker/src/main.rs
@@ -5,13 +5,15 @@ fn main() {
 }
 
 fn run() -> libwild::error::Result {
-    let args = libwild::Args::parse(|| std::env::args().skip(1))?;
+    let mut args = libwild::Args::new(|| std::env::args())?;
+    args.parse(|| std::env::args())?;
 
-    if args.should_fork() {
+    if libwild::should_fork(&args) {
         // Safety: We haven't spawned any threads yet.
         unsafe { libwild::run_in_subprocess(args) };
     } else {
         // Run the linker in this process without forking.
+        libwild::setup_tracing(&args)?;
         libwild::run(args)
     }
 }
