397f128743e5c448699713f161517e3f1dc4bb8f
Mahdi Ali-Raihan <asder8215@gmail.com>
2026-05-30 23:50:28 -0400

    Perform all memory allocations necessary for do_exec before forking; confstr_as_cstring added to get a CString value returned by confstr, added CStringArray::from_ptr, added default_path and shell_argv fields to unix Command, and refactored do_exec code

--- library/std/src/sys/process/unix/common.rs
+++ library/std/src/sys/process/unix/common.rs
@@ -10,6 +10,7 @@ use crate::ffi::{CStr, CString, OsStr, OsString};
 use crate::os::unix::prelude::*;
 use crate::path::Path;
 use crate::process::StdioPipes;
+use crate::sys::conf::confstr_as_cstring;
 use crate::sys::fd::FileDesc;
 use crate::sys::fs::File;
 #[cfg(not(target_os = "fuchsia"))]
@@ -86,7 +87,9 @@ cfg_select! {
 pub struct Command {
     program: CString,
     args: CStringArray,
+    shell_argv: CStringArray,
     env: CommandEnv,
+    default_path: CString,
 
     program_kind: ProgramKind,
     cwd: Option<CString>,
@@ -164,10 +167,20 @@ impl Command {
         let program = os2c(program, &mut saw_nul);
         let mut args = CStringArray::with_capacity(1);
         args.push(program.clone());
+        let mut shell_argv = CStringArray::with_capacity(args.len() + 2);
+        // Shell path value is implementation defined, so this could be a different
+        // value. Is there a platform dependent constant for this? In glibc it's
+        // _PATH_BSHELL, but that's not a constant available in libc
+        shell_argv.push(c"/bin/sh".into());
+        shell_argv.push(program.clone());
+        args.iter().for_each(|arg| shell_argv.push(arg.to_owned()));
+
         Command {
             program,
             args,
+            shell_argv,
             env: Default::default(),
+            default_path: confstr_as_cstring(libc::_CS_PATH).unwrap_or(c"".into()),
             program_kind,
             cwd: None,
             chroot: None,
@@ -267,6 +280,10 @@ impl Command {
         self.env.does_clear()
     }
 
+    pub fn get_default_path(&self) -> &CStr {
+        &self.default_path
+    }
+
     pub fn get_resolved_envs(&self) -> CommandResolvedEnvs {
         CommandResolvedEnvs::new(self.env.capture())
     }
@@ -277,6 +294,10 @@ impl Command {
 
     pub fn get_argv(&self) -> &CStringArray {
         &self.args
+    }
+
+    pub fn get_shell_argv(&self) -> &CStringArray {
+        &self.shell_argv
     }
 
     pub fn get_program_cstr(&self) -> &CStr {
