23144573a871726bca6e271ff80168cf5acba640
steamproof <93405617+pbkx@users.noreply.github.com>
2026-05-31 20:10:17 -0700

    borrowck: avoid ICE describing fields on generic params

--- compiler/rustc_borrowck/src/diagnostics/mod.rs
+++ compiler/rustc_borrowck/src/diagnostics/mod.rs
@@ -525,6 +525,7 @@ impl<'infcx, 'tcx> MirBorrowckCtxt<'_, 'infcx, 'tcx> {
                 ty::Array(ty, _) | ty::Slice(ty) => {
                     self.describe_field_from_ty(ty, field, variant_index, including_tuple_field)
                 }
+                ty::Param(_) => Some(field.index().to_string()),
                 ty::Closure(def_id, _) | ty::Coroutine(def_id, _) => {
                     // We won't be borrowck'ing here if the closure came from another crate,
                     // so it's safe to call `expect_local`.
