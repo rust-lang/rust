f629d561686b33d974b248c7b87416f5e3b19168
Manuel Drehwald <git@manuel.drehwald.info>
2026-05-31 18:59:02 -0700

    wip test

---
+++ tests/codegen-llvm/gpu_offload/explicit_memtransfer.rs
@@ -0,0 +1,23 @@
+#![feature(abi_gpu_kernel, gpu_offload, offload)]
+#![no_std]
+
+use core::offload::offload::*;
+
+#[cfg(target_os = "linux")]
+#[unsafe(no_mangle)]
+fn main() {
+    //println!("Hello, world!");
+    let mut x = [1234.0f64; 256];
+    let p: PreloadMut<[f64; 256]> = preload_mut(&mut x);
+    core::hint::black_box(p);
+    let y = [1234.0f64; 128];
+    let q: Preload<[f64; 128]> = preload(&y);
+    core::hint::black_box(q);
+}
+
+use core::offload::offload_kernel;
+
+//#[offload_kernel]
+//fn foo(a: &[f32], b: &[f32], c: *mut f32) {
+//    unsafe { *c = a[0] + b[0] };
+//}
