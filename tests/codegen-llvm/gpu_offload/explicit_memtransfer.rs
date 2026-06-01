555b339761b7bfe99b67ce6d852f919b46f90a1d
Manuel Drehwald <git@manuel.drehwald.info>
2026-05-31 19:00:55 -0700

    wip test

--- tests/codegen-llvm/gpu_offload/explicit_memtransfer.rs
+++ tests/codegen-llvm/gpu_offload/explicit_memtransfer.rs
@@ -9,10 +9,15 @@ fn main() {
     //println!("Hello, world!");
     let mut x = [1234.0f64; 256];
     let p: PreloadMut<[f64; 256]> = preload_mut(&mut x);
+    // The next line does not compile
+    //let p2: PreloadMut<[f64; 256]> = preload_mut(&mut x);
     core::hint::black_box(p);
     let y = [1234.0f64; 128];
     let q: Preload<[f64; 128]> = preload(&y);
-    core::hint::black_box(q);
+    let r: Preload<[f64; 128]> = preload(&y);
+    core::hint::black_box(&q);
+    core::hint::black_box(&r);
+    core::hint::black_box(&q);
 }
 
 use core::offload::offload_kernel;
