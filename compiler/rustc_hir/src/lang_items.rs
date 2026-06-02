449ec068c3e7b557fc4312038a5bbdbfb5edb596
Camille Gillot <gillot.camille@gmail.com>
2026-06-02 09:31:50 +0000

    Add required lang items.

--- compiler/rustc_hir/src/lang_items.rs
+++ compiler/rustc_hir/src/lang_items.rs
@@ -386,6 +386,13 @@ language_item_table! {
     AsyncIteratorPollNext,   sym::async_iterator_poll_next, async_iterator_poll_next, Target::Method(MethodKind::Trait { body: false }), GenericRequirement::Exact(0);
     IntoAsyncIterIntoIter,   sym::into_async_iter_into_iter, into_async_iter_into_iter, Target::Method(MethodKind::Trait { body: false }), GenericRequirement::Exact(0);
 
+    CoroutineIterator,       sym::CoroutineIterator,   coroutine_iterator,         Target::Struct,         GenericRequirement::Exact(1);
+    IterFromCoroutine,       sym::iter_from_coroutine, iter_from_coroutine,        Target::Fn,             GenericRequirement::Exact(1);
+    CoroutineFuture,         sym::CoroutineFuture,     coroutine_future,           Target::Struct,         GenericRequirement::Exact(1);
+    FutureFromCoroutine,     sym::future_from_coroutine, future_from_coroutine,    Target::Fn,             GenericRequirement::Exact(1);
+    CoroutineAsyncIterator,  sym::CoroutineAsyncIterator, coroutine_async_iterator, Target::Struct,         GenericRequirement::Exact(1);
+    AsyncIteratorFromCoroutine, sym::async_iterator_from_coroutine, async_iterator_from_coroutine, Target::Fn, GenericRequirement::Exact(2);
+
     Option,                  sym::Option,              option_type,                Target::Enum,           GenericRequirement::None;
     OptionSome,              sym::Some,                option_some_variant,        Target::Variant,        GenericRequirement::None;
     OptionNone,              sym::None,                option_none_variant,        Target::Variant,        GenericRequirement::None;
