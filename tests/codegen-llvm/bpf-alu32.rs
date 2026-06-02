1df9b8af17292672236493f5b6cc22cf15f8041a
vad <vad.sol@proton.me>
2026-06-02 14:25:48 +0200

    tests: codegen-llvm: Check whether feature is emitted in bpf-alu32 test
    Check whether the `"target-feature"` attribure was actually emitted in
    LLVM IR.

--- tests/codegen-llvm/bpf-alu32.rs
+++ tests/codegen-llvm/bpf-alu32.rs
@@ -12,6 +12,7 @@ use minicore::*;
 #[no_mangle]
 #[target_feature(enable = "alu32")]
 // CHECK: define {{.*}}i8 @foo(i8 {{.*}}%arg) unnamed_addr #0
+// CHECK: attributes #0 = { {{.*}}"target-features"="{{[^"]*}}+alu32{{.*}} }
 pub unsafe fn foo(arg: u8) -> u8 {
     arg
 }
