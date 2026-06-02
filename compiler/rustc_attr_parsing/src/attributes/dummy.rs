9bc4e33f4ccdfa77c027071d8a5683d51f512d0f
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-06-02 21:41:05 +0200

    Rewrite target checking of `rustc_dummy`

--- compiler/rustc_attr_parsing/src/attributes/dummy.rs
+++ compiler/rustc_attr_parsing/src/attributes/dummy.rs
@@ -5,20 +5,21 @@ use rustc_span::{Symbol, sym};
 use crate::attributes::{OnDuplicate, SingleAttributeParser};
 use crate::context::AcceptContext;
 use crate::parser::ArgParser;
-use crate::target_checking::{ALL_TARGETS, AllowedTargets};
+use crate::target_checking::AllowedTargets;
 use crate::unstable;
 
 pub(crate) struct RustcDummyParser;
 impl SingleAttributeParser for RustcDummyParser {
     const PATH: &[Symbol] = &[sym::rustc_dummy];
     const ON_DUPLICATE: OnDuplicate = OnDuplicate::Ignore;
-    const ALLOWED_TARGETS: AllowedTargets = AllowedTargets::AllowList(ALL_TARGETS);
+    const ALLOWED_TARGETS: AllowedTargets = AllowedTargets::ManuallyChecked;
     const TEMPLATE: AttributeTemplate = template!(Word); // Anything, really
     const STABILITY: AttributeStability =
         unstable!(rustc_attrs, "the `#[rustc_dummy]` attribute is used for rustc unit tests");
 
-    fn convert(_: &mut AcceptContext<'_, '_>, args: &ArgParser) -> Option<AttributeKind> {
+    fn convert(cx: &mut AcceptContext<'_, '_>, args: &ArgParser) -> Option<AttributeKind> {
         args.ignore_args();
+        cx.ignore_target_checks();
         Some(AttributeKind::RustcDummy)
     }
 }
