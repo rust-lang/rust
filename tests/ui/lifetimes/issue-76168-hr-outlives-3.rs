c631256928746d09cc42906c09418195dda04311
Camille Gillot <gillot.camille@gmail.com>
2026-06-03 13:05:04 +0000

    UI regressions.

--- tests/ui/lifetimes/issue-76168-hr-outlives-3.rs
+++ tests/ui/lifetimes/issue-76168-hr-outlives-3.rs
@@ -11,6 +11,7 @@ where
     F:,
     for<'a> <i32 as FnOnce<(&'a mut i32,)>>::Output: Future<Output = ()> + 'a,
 {
+    //~^ ERROR: expected an `FnOnce(&'a mut i32)` closure, found `i32`
     let mut i = 41;
     &mut i;
 }
