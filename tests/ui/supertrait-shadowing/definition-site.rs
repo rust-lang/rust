9407a97f90cc0af16dabd2ad63121e73cd9290fb
Amanieu d'Antras <amanieu@gmail.com>
2026-06-04 17:03:02 +0100

    Address review feedback for tests

--- tests/ui/supertrait-shadowing/definition-site.rs
+++ tests/ui/supertrait-shadowing/definition-site.rs
@@ -3,16 +3,26 @@
 
 trait SuperSuper {
     fn method();
+    const CONST: i32;
+    type Assoc;
 }
 
 trait Super: SuperSuper {
     fn method();
     //~^ ERROR trait item `method` from `Super` shadows identically named item
+    const CONST: i32;
+    //~^ ERROR trait item `CONST` from `Super` shadows identically named item
+    type Assoc;
+    //~^ ERROR trait item `Assoc` from `Super` shadows identically named item
 }
 
 trait Sub: Super {
     fn method();
     //~^ ERROR trait item `method` from `Sub` shadows identically named item
+    const CONST: i32;
+    //~^ ERROR trait item `CONST` from `Sub` shadows identically named item
+    type Assoc;
+    //~^ ERROR trait item `Assoc` from `Sub` shadows identically named item
 }
 
 fn main() {}
