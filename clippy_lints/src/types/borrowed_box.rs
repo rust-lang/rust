a991b843a633e434edad76a2e7a4ef0442d72069
Ada Alakbarova <ada.alakbarova@proton.me>
2026-06-06 17:07:37 +0200

    extend the let chain
    separate commit to improve diffs

--- clippy_lints/src/types/borrowed_box.rs
+++ clippy_lints/src/types/borrowed_box.rs
@@ -10,71 +10,61 @@ use rustc_lint::LateContext;
 use super::BORROWED_BOX;
 
 pub(super) fn check(cx: &LateContext<'_>, hir_ty: &hir::Ty<'_>, lt: &Lifetime, mut_ty: &MutTy<'_>) -> bool {
-    match mut_ty.ty.kind {
-        TyKind::Path(ref qpath) => {
-            let hir_id = mut_ty.ty.hir_id;
-            let def = cx.qpath_res(qpath, hir_id);
-            if let Some(def_id) = def.opt_def_id()
-                && Some(def_id) == cx.tcx.lang_items().owned_box()
-                && let QPath::Resolved(None, path) = *qpath
-                && let [ref bx] = *path.segments
-                && let Some(params) = bx.args
-                && params.parenthesized == hir::GenericArgsParentheses::No
-                && let Some(inner) = params.args.iter().find_map(|arg| match arg {
-                    GenericArg::Type(ty) => Some(ty),
-                    _ => None,
-                })
+    if let TyKind::Path(ref qpath) = mut_ty.ty.kind
+        && let hir_id = mut_ty.ty.hir_id
+        && let def = cx.qpath_res(qpath, hir_id)
+        && let Some(def_id) = def.opt_def_id()
+        && Some(def_id) == cx.tcx.lang_items().owned_box()
+        && let QPath::Resolved(None, path) = *qpath
+        && let [bx] = path.segments
+        && let Some(params) = bx.args
+        && params.parenthesized == hir::GenericArgsParentheses::No
+        && let Some(inner) = params.args.iter().find_map(|arg| match arg {
+            GenericArg::Type(ty) => Some(ty),
+            _ => None,
+        })
+        // Ignore `Box<Any>` types; see issue #1884 for details.
+        && !is_any_trait(cx, inner.as_unambig_ty())
+        // Ignore `&mut Box<T>` types; see issue #2907 for details.
+        && mut_ty.mutbl == Mutability::Not
+    {
+        let ltopt = if lt.is_anonymous() {
+            String::new()
+        } else {
+            format!("{} ", lt.ident)
+        };
+
+        // When trait objects or opaque types have lifetime or auto-trait bounds,
+        // we need to add parentheses to avoid a syntax error due to its ambiguity.
+        // Originally reported as the issue #3128.
+        let inner_snippet = snippet(cx, inner.span, "..");
+        let suggestion = match &inner.kind {
+            TyKind::TraitObject(bounds, lt_bound) if bounds.len() > 1 || !lt_bound.is_elided() => {
+                format!("&{ltopt}({inner_snippet})")
+            },
+            TyKind::Path(qpath)
+                if get_bounds_if_impl_trait(cx, qpath, inner.hir_id).is_some_and(|bounds| bounds.len() > 1) =>
             {
-                if is_any_trait(cx, inner.as_unambig_ty()) {
-                    // Ignore `Box<Any>` types; see issue #1884 for details.
-                    return false;
-                }
-
-                let ltopt = if lt.is_anonymous() {
-                    String::new()
-                } else {
-                    format!("{} ", lt.ident)
-                };
-
-                if mut_ty.mutbl == Mutability::Mut {
-                    // Ignore `&mut Box<T>` types; see issue #2907 for
-                    // details.
-                    return false;
-                }
-
-                // When trait objects or opaque types have lifetime or auto-trait bounds,
-                // we need to add parentheses to avoid a syntax error due to its ambiguity.
-                // Originally reported as the issue #3128.
-                let inner_snippet = snippet(cx, inner.span, "..");
-                let suggestion = match &inner.kind {
-                    TyKind::TraitObject(bounds, lt_bound) if bounds.len() > 1 || !lt_bound.is_elided() => {
-                        format!("&{ltopt}({inner_snippet})")
-                    },
-                    TyKind::Path(qpath)
-                        if get_bounds_if_impl_trait(cx, qpath, inner.hir_id).is_some_and(|bounds| bounds.len() > 1) =>
-                    {
-                        format!("&{ltopt}({inner_snippet})")
-                    },
-                    _ => format!("&{ltopt}{inner_snippet}"),
-                };
-                span_lint_and_sugg(
-                    cx,
-                    BORROWED_BOX,
-                    hir_ty.span,
-                    "you seem to be trying to use `&Box<T>`. Consider using just `&T`",
-                    "try",
-                    suggestion,
-                    // To make this `MachineApplicable`, at least one needs to check if it isn't a trait item
-                    // because the trait impls of it will break otherwise;
-                    // and there may be other cases that result in invalid code.
-                    // For example, type coercion doesn't work nicely.
-                    Applicability::Unspecified,
-                );
-                return true;
-            }
-            false
-        },
-        _ => false,
+                format!("&{ltopt}({inner_snippet})")
+            },
+            _ => format!("&{ltopt}{inner_snippet}"),
+        };
+        span_lint_and_sugg(
+            cx,
+            BORROWED_BOX,
+            hir_ty.span,
+            "you seem to be trying to use `&Box<T>`. Consider using just `&T`",
+            "try",
+            suggestion,
+            // To make this `MachineApplicable`, at least one needs to check if it isn't a trait item
+            // because the trait impls of it will break otherwise;
+            // and there may be other cases that result in invalid code.
+            // For example, type coercion doesn't work nicely.
+            Applicability::Unspecified,
+        );
+        true
+    } else {
+        false
     }
 }
 
