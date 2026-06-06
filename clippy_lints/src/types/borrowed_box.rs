8e7be63805251798ac6176faaa16e62acfcf35d8
Ada Alakbarova <ada.alakbarova@proton.me>
2026-06-06 17:07:37 +0200

    misc improvements
    - Do the cheaper `Mutability` test first.
    - Extract `Res` out of `Path`, since we're fetching the latter anyway.
      Avoids the call to `LateContext::qpath_res` which does the same thing.
    - `is_any_trait`: get diagnostic item once -- this is an expensive function
    - Move snippet creation inside `span_lint_and_then` -- improves laziness.

--- clippy_lints/src/types/borrowed_box.rs
+++ clippy_lints/src/types/borrowed_box.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::span_lint_and_sugg;
+use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::source::snippet;
 use clippy_utils::sym;
 use rustc_errors::Applicability;
@@ -10,12 +10,12 @@ use rustc_lint::LateContext;
 use super::BORROWED_BOX;
 
 pub(super) fn check(cx: &LateContext<'_>, hir_ty: &hir::Ty<'_>, lt: &Lifetime, mut_ty: &MutTy<'_>) -> bool {
-    if let TyKind::Path(ref qpath) = mut_ty.ty.kind
-        && let hir_id = mut_ty.ty.hir_id
-        && let def = cx.qpath_res(qpath, hir_id)
-        && let Some(def_id) = def.opt_def_id()
-        && Some(def_id) == cx.tcx.lang_items().owned_box()
+    // Ignore `&mut Box<T>` types; see issue #2907 for details.
+    if mut_ty.mutbl == Mutability::Not
+        && let TyKind::Path(ref qpath) = mut_ty.ty.kind
         && let QPath::Resolved(None, path) = *qpath
+        && let Some(def_id) = path.res.opt_def_id()
+        && Some(def_id) == cx.tcx.lang_items().owned_box()
         && let [bx] = path.segments
         && let Some(params) = bx.args
         && params.parenthesized == hir::GenericArgsParentheses::No
@@ -25,42 +25,45 @@ pub(super) fn check(cx: &LateContext<'_>, hir_ty: &hir::Ty<'_>, lt: &Lifetime, m
         })
         // Ignore `Box<Any>` types; see issue #1884 for details.
         && !is_any_trait(cx, inner.as_unambig_ty())
-        // Ignore `&mut Box<T>` types; see issue #2907 for details.
-        && mut_ty.mutbl == Mutability::Not
     {
-        let ltopt = if lt.is_anonymous() {
-            String::new()
-        } else {
-            format!("{} ", lt.ident)
-        };
-
-        // When trait objects or opaque types have lifetime or auto-trait bounds,
-        // we need to add parentheses to avoid a syntax error due to its ambiguity.
-        // Originally reported as the issue #3128.
-        let inner_snippet = snippet(cx, inner.span, "..");
-        let suggestion = match &inner.kind {
-            TyKind::TraitObject(bounds, lt_bound) if bounds.len() > 1 || !lt_bound.is_elided() => {
-                format!("&{ltopt}({inner_snippet})")
-            },
-            TyKind::Path(qpath)
-                if get_bounds_if_impl_trait(cx, qpath, inner.hir_id).is_some_and(|bounds| bounds.len() > 1) =>
-            {
-                format!("&{ltopt}({inner_snippet})")
-            },
-            _ => format!("&{ltopt}{inner_snippet}"),
-        };
-        span_lint_and_sugg(
+        span_lint_and_then(
             cx,
             BORROWED_BOX,
             hir_ty.span,
             "you seem to be trying to use `&Box<T>`. Consider using just `&T`",
-            "try",
-            suggestion,
-            // To make this `MachineApplicable`, at least one needs to check if it isn't a trait item
-            // because the trait impls of it will break otherwise;
-            // and there may be other cases that result in invalid code.
-            // For example, type coercion doesn't work nicely.
-            Applicability::Unspecified,
+            |diag| {
+                let ltopt = if lt.is_anonymous() {
+                    String::new()
+                } else {
+                    format!("{} ", lt.ident)
+                };
+
+                // When trait objects or opaque types have lifetime or auto-trait bounds,
+                // we need to add parentheses to avoid a syntax error due to its ambiguity.
+                // Originally reported as the issue #3128.
+                let inner_snippet = snippet(cx, inner.span, "..");
+                let suggestion = match &inner.kind {
+                    TyKind::TraitObject(bounds, lt_bound) if bounds.len() > 1 || !lt_bound.is_elided() => {
+                        format!("&{ltopt}({inner_snippet})")
+                    },
+                    TyKind::Path(qpath)
+                        if get_bounds_if_impl_trait(cx, qpath, inner.hir_id).is_some_and(|bounds| bounds.len() > 1) =>
+                    {
+                        format!("&{ltopt}({inner_snippet})")
+                    },
+                    _ => format!("&{ltopt}{inner_snippet}"),
+                };
+                diag.span_suggestion(
+                    hir_ty.span,
+                    "try",
+                    suggestion,
+                    // To make this `MachineApplicable`, at least one needs to check if it isn't a trait item
+                    // because the trait impls of it will break otherwise;
+                    // and there may be other cases that result in invalid code.
+                    // For example, type coercion doesn't work nicely.
+                    Applicability::Unspecified,
+                );
+            },
         );
         true
     } else {
@@ -70,15 +73,12 @@ pub(super) fn check(cx: &LateContext<'_>, hir_ty: &hir::Ty<'_>, lt: &Lifetime, m
 
 // Returns true if given type is `Any` trait.
 fn is_any_trait(cx: &LateContext<'_>, t: &hir::Ty<'_>) -> bool {
-    if let TyKind::TraitObject(traits, ..) = t.kind {
-        return traits.iter().any(|bound| {
-            if let Some(trait_did) = bound.trait_ref.trait_def_id()
-                && cx.tcx.is_diagnostic_item(sym::Any, trait_did)
-            {
-                return true;
-            }
-            false
-        });
+    if let TyKind::TraitObject(traits, ..) = t.kind
+        && let Some(any_did) = cx.tcx.get_diagnostic_item(sym::Any)
+    {
+        return traits
+            .iter()
+            .any(|bound| bound.trait_ref.trait_def_id() == Some(any_did));
     }
 
     false
@@ -87,8 +87,7 @@ fn is_any_trait(cx: &LateContext<'_>, t: &hir::Ty<'_>) -> bool {
 fn get_bounds_if_impl_trait<'tcx>(cx: &LateContext<'tcx>, qpath: &QPath<'_>, id: HirId) -> Option<GenericBounds<'tcx>> {
     if let Some(did) = cx.qpath_res(qpath, id).opt_def_id()
         && let Some(Node::GenericParam(generic_param)) = cx.tcx.hir_get_if_local(did)
-        && let GenericParamKind::Type { synthetic, .. } = generic_param.kind
-        && synthetic
+        && let GenericParamKind::Type { synthetic: true, .. } = generic_param.kind
         && let Some(generics) = cx.tcx.hir_get_generics(id.owner.def_id)
         && let Some(pred) = generics.bounds_for_param(did.expect_local()).next()
     {
