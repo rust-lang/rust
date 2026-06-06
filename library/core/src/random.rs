0e5198863f0d8e55acfd4553d9d27b245bd3cb01
Josh Triplett <josh@joshtriplett.org>
2026-06-06 10:41:05 -0700

    Rename `RandomSource` -> `Rng`, `DefaultRandomSource` -> `SystemRng`
    Per naming bikeshed in libs-api. `Rng` is shorter, and already
    established in the ecosystem. `System` makes it self-documenting that
    `SystemRng` gets its randomness from the system RNG.

--- library/core/src/random.rs
+++ library/core/src/random.rs
@@ -4,13 +4,13 @@ use crate::ops::RangeFull;
 
 /// A source of randomness.
 #[unstable(feature = "random", issue = "130703")]
-pub trait RandomSource {
+pub trait Rng {
     /// Fills `bytes` with random bytes.
     ///
     /// Note that calling `fill_bytes` multiple times is not equivalent to calling `fill_bytes` once
-    /// with a larger buffer. A `RandomSource` is allowed to return different bytes for those two
-    /// cases. For instance, this allows a `RandomSource` to generate a word at a time and throw
-    /// part of it away if not needed.
+    /// with a larger buffer. An `Rng` is allowed to return different bytes for those two cases. For
+    /// instance, this allows an `Rng` to generate a word at a time and throw part of it away if not
+    /// needed.
     fn fill_bytes(&mut self, bytes: &mut [u8]);
 }
 
@@ -18,17 +18,17 @@ pub trait RandomSource {
 #[unstable(feature = "random", issue = "130703")]
 pub trait Distribution<T> {
     /// Samples a random value from the distribution, using the specified random source.
-    fn sample(&self, source: &mut (impl RandomSource + ?Sized)) -> T;
+    fn sample(&self, source: &mut (impl Rng + ?Sized)) -> T;
 }
 
 impl<T, DT: Distribution<T>> Distribution<T> for &DT {
-    fn sample(&self, source: &mut (impl RandomSource + ?Sized)) -> T {
+    fn sample(&self, source: &mut (impl Rng + ?Sized)) -> T {
         (*self).sample(source)
     }
 }
 
 impl Distribution<bool> for RangeFull {
-    fn sample(&self, source: &mut (impl RandomSource + ?Sized)) -> bool {
+    fn sample(&self, source: &mut (impl Rng + ?Sized)) -> bool {
         let byte: u8 = RangeFull.sample(source);
         byte & 1 == 1
     }
@@ -37,7 +37,7 @@ impl Distribution<bool> for RangeFull {
 macro_rules! impl_primitive {
     ($t:ty) => {
         impl Distribution<$t> for RangeFull {
-            fn sample(&self, source: &mut (impl RandomSource + ?Sized)) -> $t {
+            fn sample(&self, source: &mut (impl Rng + ?Sized)) -> $t {
                 let mut bytes = (0 as $t).to_ne_bytes();
                 source.fill_bytes(&mut bytes);
                 <$t>::from_ne_bytes(bytes)
