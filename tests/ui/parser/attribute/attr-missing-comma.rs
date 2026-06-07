c865af60ec7899d6a9d3f4da04da51328a70de10
Aria Givens <aria@ariagivens.com>
2026-06-07 17:20:31 -0400

    Give multiple suggestions when multiple commas are missing in attribute argument list

--- tests/ui/parser/attribute/attr-missing-comma.rs
+++ tests/ui/parser/attribute/attr-missing-comma.rs
@@ -5,3 +5,10 @@ fn main() {}
     note = "note"
 )]
 fn f0() {}
+
+#[link(
+    name = "name" //~ ERROR attribute items not separated with `,`
+    kind = "static"
+    modifiers = "modifiers"
+)]
+fn f1() {}
