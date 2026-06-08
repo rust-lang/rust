e1c939975849ee7621f25f61e1b253af4c276f66
Adwin White <adwinw01@gmail.com>
2026-06-08 12:22:38 +0800

    bless tests

--- tests/ui/const-generics/mgca/type-const-free-anon-const-mismatch.rs
+++ tests/ui/const-generics/mgca/type-const-free-anon-const-mismatch.rs
@@ -4,6 +4,7 @@
 
 type const X: usize = const { N };
 //~^ ERROR type annotations needed
+//~| ERROR type annotations needed
 
 type const N: usize = "this isn't a usize";
 //~^ ERROR the constant `"this isn't a usize"` is not of type `usize`
--- tests/ui/lazy-type-alias/inherent-impls-overflow.rs
+++ tests/ui/lazy-type-alias/inherent-impls-overflow.rs
@@ -7,12 +7,12 @@
 
 type Loop = Loop;
 //[current]~^ ERROR overflow normalizing the type alias `Loop`
-//[next]~^^ ERROR type mismatch resolving `Loop normalizes-to _`
+//[next]~^^ ERROR overflow evaluating the requirement `Loop == _`
 
 impl Loop {}
 //[current]~^ ERROR overflow normalizing the type alias `Loop`
-//[next]~^^ ERROR type mismatch resolving `Loop normalizes-to _`
-//[next]~| ERROR type mismatch resolving `Loop normalizes-to _`
+//[next]~^^ ERROR overflow evaluating the requirement `Loop == _`
+//[next]~| ERROR overflow evaluating the requirement `Loop == _`
 
 type Poly0<T> = Poly1<(T,)>;
 //[current]~^ ERROR overflow normalizing the type alias `Poly0<(((((((...,),),),),),),)>`
