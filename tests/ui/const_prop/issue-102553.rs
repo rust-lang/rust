f86ad477f6fbfbe1261a9b70a3cd73b3e0524f23
qaijuang <237468078+qaijuang@users.noreply.github.com>
2026-06-09 07:32:19 -0400

    isolate regression test

--- tests/ui/const_prop/issue-102553.rs
+++ tests/ui/const_prop/issue-102553.rs
@@ -1,5 +1,5 @@
-//@ compile-flags: --crate-type=lib -Zmir-opt-level=3
-//@ build-pass
+//@ compile-flags: --crate-type=lib
+//@ check-pass
 
 pub trait Widget<E> {
     fn boxed<'w>(self) -> Box<dyn WidgetDyn<E> + 'w>
@@ -20,23 +20,5 @@ impl<E> Widget<E> for dyn WidgetDyn<E> + '_ {
         // trigger an ICE because it can never be called from actual code
         // (due to the trivially false where-clause predicate).
         Box::new(self)
-    }
-}
-
-// Regression test for https://github.com/rust-lang/rust/issues/156051.
-trait WidgetSize<E> {
-    fn size<'w>(self) -> usize
-    where
-        Self: Sized + 'w;
-}
-
-trait WidgetSizeDyn<E> {}
-
-impl<E> WidgetSize<E> for dyn WidgetSizeDyn<E> + '_ {
-    fn size<'w>(self) -> usize
-    where
-        Self: Sized + 'w,
-    {
-        core::mem::size_of::<Self>()
     }
 }
