95c0fef96598e2eb6deb834e81009b044cbe2efa
Camille Gillot <gillot.camille@gmail.com>
2026-06-10 12:56:44 +0000

    Remove useless call.

--- compiler/rustc_interface/src/passes.rs
+++ compiler/rustc_interface/src/passes.rs
@@ -1076,8 +1076,6 @@ pub fn emit_delayed_lints(tcx: TyCtxt<'_>) {
 /// Runs all analyses that we guarantee to run, even if errors were reported in earlier analyses.
 /// This function never fails.
 fn run_required_analyses(tcx: TyCtxt<'_>) {
-    tcx.ensure_done().early_lint_checks(());
-
     if tcx.sess.opts.unstable_opts.input_stats {
         rustc_passes::input_stats::print_hir_stats(tcx);
     }
