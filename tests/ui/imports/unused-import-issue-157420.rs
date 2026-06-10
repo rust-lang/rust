ea1e984ceb77d7572a74a981ff102993f4e8d1f9
Vadim Petrochenkov <vadim.petrochenkov@gmail.com>
2026-06-10 15:52:15 +0300

    Add a regression test for issue 157420

---
+++ tests/ui/imports/unused-import-issue-157420.rs
@@ -0,0 +1,19 @@
+//@ check-pass
+//@ edition: 2018..
+
+#![crate_type = "lib"] // needed to enable doc link collection
+#![warn(unused_imports)]
+
+pub use inner::*;
+use crate::outer::*; //~ WARN unused import: `crate::outer`
+
+mod outer {
+    pub mod inner {
+        pub trait Trait {} // must be a trait
+    }
+
+    pub use inner::*;
+}
+
+/// [A::assoc] // needed to force collection of traits in scope, without filter on assoc item name
+pub struct A;
