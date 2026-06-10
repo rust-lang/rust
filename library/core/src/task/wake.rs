c5c5e814c6933a1d4a71332f6de68ba1e30c42d7
XMH <970252187@qq.com>
2026-06-10 16:20:09 +0800

    Update wake.rs

--- library/core/src/task/wake.rs
+++ library/core/src/task/wake.rs
@@ -434,7 +434,7 @@ impl Waker {
     ///
     /// To avoid missed wakeups, runtimes must ensure that for any call to `wake`,
     /// there is a subsequent call to `poll` such that the call to `wake()` _happens-before_
-    /// the beginning of the invocation of `poll`. 
+    /// the beginning of the invocation of `poll`.
     /// In particular, this means that if a task self-wakes (invokes `wake` on itself during `poll`),
     /// then the `poll` must be invoked again because the call to `wake` _happens-after_ the beginning
     /// of the current invocation of `poll`.
