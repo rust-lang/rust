5b3e95d537e399b65a2fcb89e821b69e7efb6ccb
Oli Scherer <rust@oli-obk.de>
2026-06-12 11:19:25 +0200

    Add regression test

---
+++ tests/ui/comptime/comptime_impl.rs
@@ -0,0 +1,37 @@
+#![feature(rustc_attrs, const_trait_impl)]
+
+const trait Foo {
+    fn foo(&self);
+
+    fn bar(&self) {}
+}
+
+struct Bar;
+
+#[rustc_comptime]
+//~^ ERROR: cannot be used on inherent impl
+impl Bar {
+    fn boo(&self) {}
+}
+
+#[rustc_comptime]
+//~^ ERROR: cannot be used on trait impl
+impl Foo for Bar {
+    fn foo(&self) {
+        comptime_fn();
+        //~^ ERROR: comptime fns can only be called at compile time
+    }
+}
+
+#[rustc_comptime]
+fn comptime_fn() {}
+
+const _: () = {
+    Bar.boo();
+    Bar.foo();
+    //~^ ERROR: `Bar: const Foo` is not satisfied
+    Bar.bar();
+    //~^ ERROR: `Bar: const Foo` is not satisfied
+};
+
+fn main() {}
