f55d0f11b75e02b35387dc890edefdb6af7a8038
panstromek <panstromek@seznam.cz>
2026-06-12 15:19:35 +0200

    fix: make IndexVec compile on stable

--- compiler/rustc_index/src/vec.rs
+++ compiler/rustc_index/src/vec.rs
@@ -65,7 +65,15 @@ impl<I: Idx, T: Hash> Hash for IndexVec<I, T> {
     }
 }
 
+#[cfg(feature = "nightly")]
 unsafe impl<I: Idx, #[may_dangle] T> Drop for IndexVec<I, T> {
+    fn drop(&mut self) {
+        std::mem::take(self).into_vec();
+    }
+}
+
+#[cfg(not(feature = "nightly"))]
+impl<I: Idx, T> Drop for IndexVec<I, T> {
     fn drop(&mut self) {
         std::mem::take(self).into_vec();
     }
