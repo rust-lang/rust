5e016b66c3b14d2934eb64d344e91c2246578ed5
Tim Neumann <timnn@google.com>
2026-06-13 12:13:31 +0200

    Stabilize `#[my_macro] mod foo;` (part of `proc_macro_hygiene`)

--- tests/ui/proc-macro/module.rs
+++ tests/ui/proc-macro/module.rs
@@ -1,1 +1,4 @@
-//@ ignore-auxiliary (used by `./attributes-on-modules-fail.rs`)
+//@ ignore-auxiliary (used by `./attributes-on-modules-fail.rs` and `modules-in-input.rs`)
+
+// An item that can be referenced to verify the module has been properly loaded.
+pub const fn hello() {}
