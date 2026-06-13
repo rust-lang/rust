f6bc2c8a4308aac4e3370963845d408c332097be
Ada Alakbarova <ada.alakbarova@proton.me>
2026-06-14 00:01:34 +0200

    misc: wrap everything in `span_lint_and_then`
    ..and inline `msg`, to make next commit's diff a bit nicer

--- clippy_lints/src/methods/filter_next.rs
+++ clippy_lints/src/methods/filter_next.rs
@@ -1,4 +1,4 @@
-use clippy_utils::diagnostics::{span_lint, span_lint_and_then};
+use clippy_utils::diagnostics::span_lint_and_then;
 use clippy_utils::source::snippet;
 use clippy_utils::ty::implements_trait;
 use clippy_utils::{path_to_local_with_projections, sym};
@@ -35,39 +35,44 @@ pub(super) fn check(
     {
         return;
     }
-    let msg = format!(
-        "called `filter(..).{next_method}()` on an `{required_trait}`. \
+    span_lint_and_then(
+        cx,
+        FILTER_NEXT,
+        expr.span,
+        format!(
+            "called `filter(..).{next_method}()` on an `{required_trait}`. \
             This is more succinctly expressed by calling `.{find_method}(..)` instead",
-    );
-    // add note if not multi-line
-    let filter_snippet = snippet(cx, filter_arg.span, "..");
-    if filter_snippet.lines().count() <= 1 {
-        let iter_snippet = snippet(cx, recv.span, "..");
-        span_lint_and_then(cx, FILTER_NEXT, expr.span, msg, |diag| {
-            let (applicability, pat) = if let Some(id) = path_to_local_with_projections(recv)
-                && let Node::Pat(pat) = cx.tcx.hir_node(id)
-                && let PatKind::Binding(BindingMode(_, Mutability::Not), _, ident, _) = pat.kind
-            {
-                (Applicability::Unspecified, Some((pat.span, ident)))
-            } else {
-                (Applicability::MachineApplicable, None)
-            };
+        ),
+        |diag| {
+            // add note if not multi-line
+            let filter_snippet = snippet(cx, filter_arg.span, "..");
+            if filter_snippet.lines().count() <= 1 {
+                let iter_snippet = snippet(cx, recv.span, "..");
+                let (applicability, pat) = if let Some(id) = path_to_local_with_projections(recv)
+                    && let Node::Pat(pat) = cx.tcx.hir_node(id)
+                    && let PatKind::Binding(BindingMode(_, Mutability::Not), _, ident, _) = pat.kind
+                {
+                    (Applicability::Unspecified, Some((pat.span, ident)))
+                } else {
+                    (Applicability::MachineApplicable, None)
+                };
 
-            diag.span_suggestion(
-                expr.span,
-                "try",
-                format!("{iter_snippet}.{find_method}({filter_snippet})"),
-                applicability,
-            );
-
-            if let Some((pat_span, ident)) = pat {
-                diag.span_help(
-                    pat_span,
-                    format!("you will also need to make `{ident}` mutable, because `{find_method}` takes `&mut self`"),
+                diag.span_suggestion(
+                    expr.span,
+                    "try",
+                    format!("{iter_snippet}.{find_method}({filter_snippet})"),
+                    applicability,
                 );
+
+                if let Some((pat_span, ident)) = pat {
+                    diag.span_help(
+                        pat_span,
+                        format!(
+                            "you will also need to make `{ident}` mutable, because `{find_method}` takes `&mut self`"
+                        ),
+                    );
+                }
             }
-        });
-    } else {
-        span_lint(cx, FILTER_NEXT, expr.span, msg);
-    }
+        },
+    );
 }
