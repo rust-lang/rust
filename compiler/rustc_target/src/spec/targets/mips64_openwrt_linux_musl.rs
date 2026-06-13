ed576bd42bed8b401c4cd7c36e9b3cbb5fc20113
Xi Ruoyao <xry111@xry111.site>
2026-06-13 20:39:50 +0800

    mips: set llvm_args -mno-check-zero-division for all mips targets
    In rust interger divide by zero is defined to panic, thus the inserted
    conditional trap should never trigger as the program should have
    panicked if the divisor is zero.
    
    So disable the insertion of the redundant conditional trap.

--- compiler/rustc_target/src/spec/targets/mips64_openwrt_linux_musl.rs
+++ compiler/rustc_target/src/spec/targets/mips64_openwrt_linux_musl.rs
@@ -2,7 +2,7 @@
 
 use rustc_abi::Endian;
 
-use crate::spec::{Arch, CfgAbi, LlvmAbi, Target, TargetMetadata, TargetOptions, base};
+use crate::spec::{Arch, CfgAbi, LlvmAbi, Target, TargetMetadata, TargetOptions, base, cvs};
 
 pub(crate) fn target() -> Target {
     let mut base = base::linux_musl::opts();
@@ -28,6 +28,7 @@ pub(crate) fn target() -> Target {
             endian: Endian::Big,
             mcount: "_mcount".into(),
             llvm_abiname: LlvmAbi::N64,
+            llvm_args: cvs!["-mno-check-zero-division"],
             ..base
         },
     }
