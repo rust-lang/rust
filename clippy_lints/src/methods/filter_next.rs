f0974c97494e4297e43c527518332b79cf097f51
Ada Alakbarova <ada.alakbarova@proton.me>
2026-06-14 00:32:21 +0200

    misc improvements

--- clippy_lints/src/methods/filter_next.rs
+++ clippy_lints/src/methods/filter_next.rs
@@ -4,12 +4,12 @@ use clippy_utils::ty::implements_trait;
 use clippy_utils::{path_to_local_with_projections, sym};
 use rustc_ast::{BindingMode, Mutability};
 use rustc_errors::Applicability;
-use rustc_hir as hir;
+use rustc_hir::{Expr, Node, PatKind};
 use rustc_lint::LateContext;
 
 use super::FILTER_NEXT;
 
-#[derive(Copy, Clone)]
+#[derive(Clone, Copy)]
 pub(super) enum Direction {
     Forward,
     Backward,
@@ -17,15 +17,13 @@ pub(super) enum Direction {
 
 /// lint use of `filter().next()` for `Iterator` and `filter().next_back()` for
 /// `DoubleEndedIterator`
-pub(super) fn check<'tcx>(
-    cx: &LateContext<'tcx>,
-    expr: &'tcx hir::Expr<'_>,
-    recv: &'tcx hir::Expr<'_>,
-    filter_arg: &'tcx hir::Expr<'_>,
+pub(super) fn check(
+    cx: &LateContext<'_>,
+    expr: &Expr<'_>,
+    recv: &Expr<'_>,
+    filter_arg: &Expr<'_>,
     direction: Direction,
 ) {
-    // lint if caller of `.filter().next()` is an Iterator or `.filter().next_back()` is a
-    // DoubleEndedIterator
     let (required_trait, next_method, find_method) = match direction {
         Direction::Forward => (sym::Iterator, "next", "find"),
         Direction::Backward => (sym::DoubleEndedIterator, "next_back", "rfind"),
@@ -38,18 +36,17 @@ pub(super) fn check<'tcx>(
         return;
     }
     let msg = format!(
-        "called `filter(..).{next_method}()` on an `{}`. This is more succinctly expressed by calling \
-                   `.{find_method}(..)` instead",
-        required_trait.as_str()
+        "called `filter(..).{next_method}()` on an `{required_trait}`. \
+            This is more succinctly expressed by calling `.{find_method}(..)` instead",
     );
+    // add note if not multi-line
     let filter_snippet = snippet(cx, filter_arg.span, "..");
     if filter_snippet.lines().count() <= 1 {
         let iter_snippet = snippet(cx, recv.span, "..");
-        // add note if not multi-line
         span_lint_and_then(cx, FILTER_NEXT, expr.span, msg, |diag| {
             let (applicability, pat) = if let Some(id) = path_to_local_with_projections(recv)
-                && let hir::Node::Pat(pat) = cx.tcx.hir_node(id)
-                && let hir::PatKind::Binding(BindingMode(_, Mutability::Not), _, ident, _) = pat.kind
+                && let Node::Pat(pat) = cx.tcx.hir_node(id)
+                && let PatKind::Binding(BindingMode(_, Mutability::Not), _, ident, _) = pat.kind
             {
                 (Applicability::Unspecified, Some((pat.span, ident)))
             } else {
