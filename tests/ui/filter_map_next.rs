9f629d1b7eb2b53897ae3b367c5ce75540a9519f
Ada Alakbarova <ada.alakbarova@proton.me>
2026-06-14 13:49:33 +0200

    test: make the fixable file the default
    i.e. rename:
    - filter_map_next -> filter_map_next_unfixable
    - filter_map_next_fixable -> filter_map_next
    
    This is because having fixable lints is the common case

--- tests/ui/filter_map_next.rs
+++ tests/ui/filter_map_next.rs
@@ -3,18 +3,20 @@
 fn main() {
     let a = ["1", "lol", "3", "NaN", "5"];
 
-    #[rustfmt::skip]
-    let _: Option<u32> = vec![1, 2, 3, 4, 5, 6]
+    let element: Option<i32> = a.iter().filter_map(|s| s.parse().ok()).next();
     //~^ filter_map_next
+    assert_eq!(element, Some(1));
+}
 
+#[clippy::msrv = "1.29"]
+fn msrv_1_29() {
+    let a = ["1", "lol", "3", "NaN", "5"];
+    let _: Option<i32> = a.iter().filter_map(|s| s.parse().ok()).next();
+}
 
-        .into_iter()
-        .filter_map(|x| {
-            if x == 2 {
-                Some(x * 2)
-            } else {
-                None
-            }
-        })
-        .next();
+#[clippy::msrv = "1.30"]
+fn msrv_1_30() {
+    let a = ["1", "lol", "3", "NaN", "5"];
+    let _: Option<i32> = a.iter().filter_map(|s| s.parse().ok()).next();
+    //~^ filter_map_next
 }
