5807486f7fcc83fd0beb8b33fa0f33df862c17b5
bit-aloo <sshourya17@gmail.com>
2026-06-14 09:48:59 +0530

    Add tup expr arg unbounded assoc const test

---
+++ tests/ui/const-generics/mgca/tuple_expr_arg_unbounded_assoc_const.rs
@@ -0,0 +1,13 @@
+//@ check-pass
+#![feature(min_generic_const_args, adt_const_params)]
+
+// Regression test for an ICE in privacy checking while walking the `T` qself
+// of `T::ASSOC` inside a tuple const argument.
+
+fn takes_tuple<const N: ()>() {}
+
+fn generic_caller<T, const N: u32>() {
+    takes_tuple::<{ (N, T::ASSOC) }>;
+}
+
+fn main() {}
