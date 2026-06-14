5754ef52f05835cfa7c2ba54c14ef62f8db73339
Camille Gillot <gillot.camille@gmail.com>
2026-06-14 15:58:55 +0000

    Remove outdated comment.

--- compiler/rustc_trait_selection/src/traits/project.rs
+++ compiler/rustc_trait_selection/src/traits/project.rs
@@ -1640,9 +1640,6 @@ fn confirm_closure_candidate<'cx, 'tcx>(
     let closure_sig = match *self_ty.kind() {
         ty::Closure(_, args) => Unnormalized::new_wip(args.as_closure().sig()),
 
-        // Construct a "normal" `FnOnce` signature for coroutine-closure. This is
-        // basically duplicated with the `AsyncFnOnce::CallOnce` confirmation, but
-        // I didn't see a good way to unify those.
         ty::CoroutineClosure(def_id, args) => {
             let args = args.as_coroutine_closure();
             Unnormalized::new_wip(args.coroutine_closure_sig().map_bound(|sig| {
