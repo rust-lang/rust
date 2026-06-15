cc87a4e951ab6269eb58dafef5d834323a77215b
Nia Deckers <me@nia.gay>
2026-06-15 16:08:39 +0200

    fallible type ops

--- compiler/rustc_infer/src/traits/mod.rs
+++ compiler/rustc_infer/src/traits/mod.rs
@@ -20,7 +20,9 @@ use rustc_middle::ty::{self, Ty, TyCtxt, Upcast};
 use rustc_span::Span;
 use thin_vec::ThinVec;
 
-pub use self::engine::{FromSolverError, ScrubbedTraitError, TraitEngine, TraitErrors};
+pub use self::engine::{
+    EngineError, FromSolverError, ScrubbedTraitError, TraitEngine, TraitErrors,
+};
 pub(crate) use self::project::UndoLog;
 pub use self::project::{
     MismatchedProjectionTypes, Normalized, NormalizedTerm, ProjectionCache, ProjectionCacheEntry,
