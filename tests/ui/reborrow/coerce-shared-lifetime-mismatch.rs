01c2eb46d1e2f4409cc2bb6d3c2f6787426f1373
Pieter-Louis Schoeman <pl.schoeman44@gmail.com>
2026-06-15 11:58:30 +0200

    Rework reborrow test expectations

---
+++ tests/ui/reborrow/coerce-shared-lifetime-mismatch.rs
@@ -0,0 +1,24 @@
+#![feature(reborrow)]
+
+// The impl is accepted, but using it to coerce a local marker into a `'static`
+// target still requires the local borrow to live for `'static`.
+
+use std::marker::{CoerceShared, PhantomData, Reborrow};
+
+struct CustomMarker<'a>(PhantomData<&'a ()>);
+
+impl<'a> Reborrow for CustomMarker<'a> {}
+
+#[derive(Clone, Copy)]
+struct StaticMarkerRef<'a>(PhantomData<&'a ()>);
+
+impl<'a> CoerceShared<StaticMarkerRef<'static>> for CustomMarker<'a> {}
+//~^ ERROR
+
+fn method(_a: StaticMarkerRef<'static>) {}
+
+fn main() {
+    let a = CustomMarker(PhantomData);
+    method(a);
+    //~^ ERROR
+}
