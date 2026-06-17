be17d4bcd39edddeb897c04623de4e3e680b448d
Matyas Susits <susits.matyas@gmail.com>
2026-06-17 13:32:06 +0200

    rdr: add Ty and Predicate nodes to the reachability graph

--- compiler/rustc_metadata/src/rmeta/encoder/public_api_hasher.rs
+++ compiler/rustc_metadata/src/rmeta/encoder/public_api_hasher.rs
@@ -639,8 +639,8 @@ impl ReachabilityGraphHashes {
             Node::ExpnId(id) => self.expn.entry(id.expect_local()).or_insert(Fingerprint::ZERO),
             Node::SyntaxContext(id) => self.syntax.entry(*id).or_insert(Fingerprint::ZERO),
             Node::Span(_) => todo!(),
-            Node::Ty(_) => todo!(),
-            Node::Predicate(_) => todo!(),
+            Node::Ty(_) => unimplemented!(),
+            Node::Predicate(_) => unimplemented!(),
             Node::AllocId(_) => todo!(),
         };
         *current_hash = hash.combine_commutative(hash);
@@ -669,8 +669,8 @@ impl ReachabilityGraphHashes {
             }
             Node::SyntaxContext(id) => self.syntax.get(id).copied(),
             Node::Span(_) => todo!(),
-            Node::Ty(_) => todo!(),
-            Node::Predicate(_) => todo!(),
+            Node::Ty(_) => None,
+            Node::Predicate(_) => None,
             Node::AllocId(_) => todo!(),
         }
     }
@@ -875,8 +875,8 @@ fn build_public_hashes<'tcx>(
                 }
             }
             Node::SyntaxContext(_) => (),
-            Node::Ty(_) => todo!(),
-            Node::Predicate(_) => todo!(),
+            Node::Ty(_) => (),
+            Node::Predicate(_) => (),
             Node::Span(_) => todo!(),
             Node::AllocId(_) => todo!(),
         }
