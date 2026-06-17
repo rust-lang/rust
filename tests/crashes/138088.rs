f227e79e5ddee36e6e125dbb60ab5f33547380ad
bit-aloo <sshourya17@gmail.com>
2026-06-17 13:57:56 +0000

    Remove crashtest and mention the issue in the ui test

--- tests/crashes/138088.rs
+++
@@ -1,5 +0,0 @@
-//@ known-bug: #138088
-#![feature(min_generic_const_args)]
-trait Bar {
-    fn x(&self) -> [i32; Bar::x];
-}
--- tests/ui/const-generics/mgca/trait-assoc-fn-as-const-arg.rs
+++ tests/ui/const-generics/mgca/trait-assoc-fn-as-const-arg.rs
@@ -1,5 +1,7 @@
 #![feature(min_generic_const_args)]
 
+// Regression test for #138088. assoc functions cannot be lowered as const
+// args, and this should emit a regular diagnostic instead of ICEing.
 trait Bar {
     fn x(&self) -> [i32; Bar::x];
     //~^ ERROR function items cannot be used as const arguments
