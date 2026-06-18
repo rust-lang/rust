e3622c1a449b927ada555dc2adecbdf93d345499
Jules Bertholet <julesbertholet@quoi.xyz>
2026-06-17 20:30:09 -0400

    Don't escape U+FF9E and U+FF9F in `escape_debug_ext`

--- library/core/src/char/methods.rs
+++ library/core/src/char/methods.rs
@@ -486,8 +486,11 @@ impl char {
             '\r' => EscapeDebug::backslash(ascii::Char::SmallR),
             '\0' => EscapeDebug::backslash(ascii::Char::Digit0),
 
-            // ASCII fast path
-            '\x20'..='\x7E' => EscapeDebug::printable(self),
+            // ASCII fast path,
+            // plus U+FF9E HALFWIDTH KATAKANA VOICED SOUND MARK
+            // and U+FF9F HALFWIDTH KATAKANA SEMI-VOICED SOUND MARK
+            // which should not be escaped despite being grapheme extenders.
+            '\x20'..='\x7E' | '\u{FF9E}' | '\u{FF9F}' => EscapeDebug::printable(self),
 
             _ if self.is_control()
                 || self.is_private_use()
