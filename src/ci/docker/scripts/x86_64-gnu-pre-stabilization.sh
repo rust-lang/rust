1f15206477e4ab45d320f5163a3b62709fd01220
Rémy Rakic <remy.rakic@arm.com>
2026-06-19 11:51:21 +0200

    ensure the new solver bootstraps

--- src/ci/docker/scripts/x86_64-gnu-pre-stabilization.sh
+++ src/ci/docker/scripts/x86_64-gnu-pre-stabilization.sh
@@ -7,10 +7,12 @@ set -ex
 
 # 1. For the new trait solver, we want to:
 # - ensure it can build the standard library
+# - ensure it actually bootstraps
 #
-# FIXME: we also need to ensure it actually bootstraps.
+# We test both by building the _stage 2_ library with the new solver enabled
+# at stage 1 via rustflags.
 
-RUSTFLAGS_NOT_BOOTSTRAP="-Znext-solver=globally" ../x build library --stage 1
+RUSTFLAGS_NOT_BOOTSTRAP="-Znext-solver=globally" ../x build library --stage 2
 
 # 2. For the polonius alpha, we run the UI tests under the polonius
 # compare-mode.
