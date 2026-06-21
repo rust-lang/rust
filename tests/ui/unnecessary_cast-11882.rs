55dc5523e535ac76223bfd0dfe1adcb823cca2bd
Michael Howell <michael@notriddle.com>
2026-06-20 21:35:03 -0700

    Clean up unrelated tests

--- tests/ui/unnecessary_cast-11882.rs
+++ tests/ui/unnecessary_cast-11882.rs
@@ -1,4 +1,3 @@
-//@ignore-bitwidth: 32
 #![feature(const_trait_impl, const_ops)]
 #![warn(clippy::unnecessary_cast)]
 #![allow(unused, clippy::identity_op)]
@@ -7,84 +6,58 @@ const TEST: u64 = (!0 as u64).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
 const TEST2: u64 = (!0_u64 as u64).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const TEST3: u64 = (not(0) as u64).overflowing_shr(1_u32).0;
-const TEST4: u64 = (not(0_u64) as u64).overflowing_shr(1_u32).0;
+const TEST3: u64 = (!not(!0_u64) as u64).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const TEST5: u64 = (!not(!0_u64) as u64).overflowing_shr(1_u32).0;
+const TEST4: u64 = (!0 as u64 + 0).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const TEST6: u64 = (0xff_ff_ff_ff_ff_ff_ff_ff as u64).overflowing_shr(1_u32).0;
+const TEST5: u64 = (!(0 as u64 + 0)).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const TEST7: u64 = (!0 as u64 + 0).overflowing_shr(1_u32).0;
-//~^ unnecessary_cast
-const TEST8: u64 = (!(0 as u64 + 0)).overflowing_shr(1_u32).0;
-//~^ unnecessary_cast
-const TEST9: u64 = (!((0 + 0) as u64)).overflowing_shr(1_u32).0;
+const TEST6: u64 = (!((0 + 0) as u64)).overflowing_shr(1_u32).0;
 
 const CHK1: u64 = not(!0 as u64).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const CHK2: u64 = (not(!0) as u64).overflowing_shr(1_u32).0;
-const CHK3: u64 = (!not(0) as u64).overflowing_shr(1_u32).0;
-const CHK4: u64 = (!not(0_u64) as u64).overflowing_shr(1_u32).0;
+const CHK2: u64 = (!not(0_u64) as u64).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const CHK5: u64 = (not(!0_u64) as u64).overflowing_shr(1_u32).0;
+const CHK3: u64 = (!not(0 as u64)).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const CHK6: u64 = (!(not(0) as u64)).overflowing_shr(1_u32).0;
-const CHK7: u64 = (!not(0 as u64)).overflowing_shr(1_u32).0;
+const CHK4: u64 = not(!0 as u64 + 0).overflowing_shr(1_u32).0;
 //~^ unnecessary_cast
-const CHK8: u64 = not(!0 as u64 + 0).overflowing_shr(1_u32).0;
-//~^ unnecessary_cast
+
+// Make sure that the calculated values aren't changed by the fixes.
+const _: () = {
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST);
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST2);
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST3);
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST4);
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST5);
+    assert_eq!(0x7f_ff_ff_ff_ff_ff_ff_ffu64, TEST6);
+    assert_eq!(0, CHK1);
+    assert_eq!(0, CHK2);
+    assert_eq!(0, CHK3);
+    assert_eq!(0, CHK4);
+};
 
 fn main() {
-    // make sure that the calculated value doesn't change
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST2 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST3 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST4 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST5 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST6 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST7 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST8 as usize];
-    let _x: [(); 0x7f_ff_ff_ff_ff_ff_ff_ff] = [(); TEST9 as usize];
-
-    let _x: [(); 0] = [(); CHK1 as usize];
-    let _x: [(); 0] = [(); CHK2 as usize];
-    let _x: [(); 0] = [(); CHK3 as usize];
-    let _x: [(); 0] = [(); CHK4 as usize];
-    let _x: [(); 0] = [(); CHK5 as usize];
-    let _x: [(); 0] = [(); CHK6 as usize];
-    let _x: [(); 0] = [(); CHK7 as usize];
-    let _x: [(); 0] = [(); CHK8 as usize];
-
     // the non-const version of the tests
     let test: u64 = (!0 as u64).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
     let test2: u64 = (!0_u64 as u64).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let test3: u64 = (not(0) as u64).overflowing_shr(1_u32).0;
-    let test4: u64 = (not(0_u64) as u64).overflowing_shr(1_u32).0;
+    let test3: u64 = (!not(!0_u64) as u64).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let test5: u64 = (!not(!0_u64) as u64).overflowing_shr(1_u32).0;
+    let test4: u64 = (!0 as u64 + 0).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let test6: u64 = (0xff_ff_ff_ff_ff_ff_ff_ff as u64).overflowing_shr(1_u32).0;
+    let test5: u64 = (!(0 as u64 + 0)).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let test7: u64 = (!0 as u64 + 0).overflowing_shr(1_u32).0;
-    //~^ unnecessary_cast
-    let test8: u64 = (!(0 as u64 + 0)).overflowing_shr(1_u32).0;
-    //~^ unnecessary_cast
-    let test9: u64 = (!((0 + 0) as u64)).overflowing_shr(1_u32).0;
+    let test6: u64 = (!((0 + 0) as u64)).overflowing_shr(1_u32).0;
 
     let chk1: u64 = not(!0 as u64).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let chk2: u64 = (not(!0) as u64).overflowing_shr(1_u32).0;
-    let chk3: u64 = (!not(0) as u64).overflowing_shr(1_u32).0;
-    let chk4: u64 = (!not(0_u64) as u64).overflowing_shr(1_u32).0;
+    let chk2: u64 = (!not(0_u64) as u64).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let chk5: u64 = (not(!0_u64) as u64).overflowing_shr(1_u32).0;
+    let chk3: u64 = (!not(0 as u64)).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
-    let chk6: u64 = (!(not(0) as u64)).overflowing_shr(1_u32).0;
-    let chk7: u64 = (!not(0 as u64)).overflowing_shr(1_u32).0;
-    //~^ unnecessary_cast
-    let chk8: u64 = not(!0 as u64 + 0).overflowing_shr(1_u32).0;
+    let chk4: u64 = not(!0 as u64 + 0).overflowing_shr(1_u32).0;
     //~^ unnecessary_cast
 }
 
