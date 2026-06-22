f5c36e81abbcc9b7fb2faf0465908d1cad3ba0be
Takayuki Maeda <takoyaki0316@gmail.com>
2026-06-23 02:25:28 +0900

    support move expr in coroutine blocks

--- tests/ui/move-expr/parse-ambiguity-errors.rs
+++ tests/ui/move-expr/parse-ambiguity-errors.rs
@@ -5,7 +5,7 @@ fn main() {
     let x: bool = true;
     let y: bool = true;
     let _ = move(x) || y;
-    //~^ ERROR `move(expr)` is only supported in closures
+    //~^ ERROR `move(expr)` is only supported in closures, `async`, `gen`, and `async gen` blocks
 
     let x: bool = true;
     let y: bool = true;
