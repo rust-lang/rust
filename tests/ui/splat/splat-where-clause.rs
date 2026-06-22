513ca49f785385e31712dc0445b736c32a35b531
Ajay Singh <ajaykripa8736968359@gmail.com>
2026-06-22 11:49:50 +0530

    Add impl Tuple tests to splat-where-clause

--- tests/ui/splat/splat-where-clause.rs
+++ tests/ui/splat/splat-where-clause.rs
@@ -9,6 +9,10 @@ fn where_splat<T>(#[splat] _t: T) where T: std::marker::Tuple {}
 
 fn where_splat_with_extra<T>(#[splat] _t: T, _extra: u32) where T: std::marker::Tuple {}
 
+fn impl_tuple_splat(#[splat] _t: impl std::marker::Tuple) {}
+
+fn impl_tuple_splat_with_extra(#[splat] _t: impl std::marker::Tuple, _extra: u32) {}
+
 fn main() {
     // empty tuple
     where_splat();
@@ -28,4 +32,13 @@ fn main() {
     // with extra non-splatted arg
     where_splat_with_extra(1u32, 2i8, 42u32);
     where_splat_with_extra(1, 2, 42);
+
+    // impl Trait syntax variants
+    impl_tuple_splat();
+    impl_tuple_splat(1u32);
+    impl_tuple_splat(1u32, 2i8);
+    impl_tuple_splat(1, 2, 3.0);
+
+    impl_tuple_splat_with_extra(1u32, 2i8, 42u32);
+    impl_tuple_splat_with_extra(1, 2, 42);
 }
