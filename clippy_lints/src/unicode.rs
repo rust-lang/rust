04ac5087c240bfc7ba7911a9768c8c4dbb02f732
shulaoda <165626830+shulaoda@users.noreply.github.com>
2026-06-25 07:57:22 +0800

    `invisible_characters`: don't suggest a fix on raw strings

--- clippy_lints/src/unicode.rs
+++ clippy_lints/src/unicode.rs
@@ -111,17 +111,20 @@ fn check_str(cx: &LateContext<'_>, span: Span, id: HirId) {
     let is_raw = string.starts_with('r');
 
     if string.chars().any(|c| ['\u{200B}', '\u{ad}', '\u{2060}'].contains(&c)) {
-        #[expect(clippy::collapsible_span_lint_calls, reason = "rust-clippy#7797")]
         span_lint_and_then(cx, INVISIBLE_CHARACTERS, span, "invisible character detected", |diag| {
-            diag.span_suggestion(
-                span,
-                "consider replacing the string with",
-                string
-                    .replace('\u{200B}', "\\u{200B}")
-                    .replace('\u{ad}', "\\u{AD}")
-                    .replace('\u{2060}', "\\u{2060}"),
-                Applicability::MachineApplicable,
-            );
+            if is_raw {
+                diag.help("use a normal string literal instead of a raw one to escape the character");
+            } else {
+                diag.span_suggestion(
+                    span,
+                    "consider replacing the string with",
+                    string
+                        .replace('\u{200B}', "\\u{200B}")
+                        .replace('\u{ad}', "\\u{AD}")
+                        .replace('\u{2060}', "\\u{2060}"),
+                    Applicability::MachineApplicable,
+                );
+            }
         });
     }
 
