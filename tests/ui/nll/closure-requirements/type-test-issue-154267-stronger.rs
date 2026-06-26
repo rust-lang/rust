993b2c95161356aef76353aa33f02ba6fa2153dc
LorrensP-2158466 <lorrens.pantelis@student.uhasselt.be>
2026-06-26 15:44:53 +0200

    address concern of closure type-test prop pr

--- tests/ui/nll/closure-requirements/type-test-issue-154267-stronger.rs
+++ tests/ui/nll/closure-requirements/type-test-issue-154267-stronger.rs
@@ -1,7 +1,5 @@
-//@ check-pass
-//
-// Check that the compiler correctly detects that we don't need to propagate `T: 'b` and `T: 'c`
-// when doing the type tests of `constrain`. This is a stronger version of issue-154267.
+// Stronger version of `type-test-issue-154267` where `error` should fail because it does not have
+// an explicit `'a: 'b` bound.
 
 struct Arg<'a: 'b, 'b: 'd, 'c: 'd, 'd, T> {
     field: *mut (&'a (), &'b (), &'c (), &'d (), T),
@@ -14,9 +12,9 @@ impl<'a: 'b, 'b, 'c, 'd, T> Arg<'a, 'b, 'c, 'd, T> {
     {
     }
 }
-fn takes_closure<'a, 'b, 'c, T>(_: impl for<'d> FnOnce(Arg<'a, 'b, 'c, 'd, T>)) {}
+fn takes_closure<'a: 'b, 'b, 'c, T>(_: impl for<'d> FnOnce(Arg<'a, 'b, 'c, 'd, T>)) {}
 
 fn error<'a, 'b, 'c, T: 'a>() {
-    takes_closure::<'a, 'b, 'c, T>(|arg| arg.constrain());
+    takes_closure::<'a, 'b, 'c, T>(|arg| arg.constrain()); //~ ERROR: lifetime may not live long enough
 }
 fn main() {}
