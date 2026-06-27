4c28834a4db46994f3e1c3bfc06ef56a0305ecad
Miguel Marques <miguel.m.marques@tecnico.ulisboa.pt>
2026-06-27 23:36:25 +0100

    fix: Adapts new enum layout to work in all editions.
    The new implementation was only working for future edition, but since
    currently enum layout is not guaranteed, it adds the changes
    to all editions.
    
    Signed-off-by: Miguel Marques <miguel.m.marques@tecnico.ulisboa.pt>
    Co-authored-by: Vicente Gusmão <vicente.gusmao@tecnico.ulisboa.pt>

--- tests/ui/structs-enums/enum-niche-fill-around-tag.rs
+++ tests/ui/structs-enums/enum-niche-fill-around-tag.rs
@@ -1,13 +1,8 @@
-//@ revisions: old future
 //@ run-pass
-//@ [old] edition:2024
-//@ [future] edition:future
-//@ [future] compile-flags: -Z unstable-options
 
 #![allow(dead_code)]
-#![cfg_attr(future, feature(offset_of_enum))]
+#![feature(offset_of_enum)]
 
-#[cfg(future)]
 use std::mem::{align_of, offset_of};
 use std::mem::size_of;
 use std::num::NonZero;
@@ -77,64 +72,55 @@ enum ChooseFittingNiche {
 fn main() {
     assert_eq!(size_of::<Inner>(), 8);
 
-    #[cfg(old)]
-    {
-        assert_eq!(size_of::<Outer>(), 12);
-        assert_eq!(size_of::<RepackPadding>(), 12);
-    }
+    assert_eq!(size_of::<Outer>(), 8);
+    assert_eq!(size_of::<RepackPadding>(), 8);
 
-    #[cfg(future)]
-    {
-        assert_eq!(size_of::<Outer>(), 8);
-        assert_eq!(size_of::<RepackPadding>(), 8);
+    assert_eq!(size_of::<RepackedBase>(), 8);
+    assert_eq!(align_of::<RepackedBase>(), 4);
+    assert_eq!(offset_of!(RepackedBase, int32), 0);
+    assert_eq!(offset_of!(RepackedBase, boolean), 4);
 
-        assert_eq!(size_of::<RepackedBase>(), 8);
-        assert_eq!(align_of::<RepackedBase>(), 4);
-        assert_eq!(offset_of!(RepackedBase, int32), 0);
-        assert_eq!(offset_of!(RepackedBase, boolean), 4);
+    assert_eq!(size_of::<RepackedAroundNiche>(), 8);
+    assert_eq!(align_of::<RepackedAroundNiche>(), 4);
+    assert_eq!(size_of::<Option<RepackedAroundNiche>>(), 8);
+    assert_eq!(offset_of!(RepackedAroundNiche, A.0), 0);
+    assert_eq!(offset_of!(RepackedAroundNiche, B.1), 0);
+    assert_eq!(offset_of!(RepackedAroundNiche, B.2), 5);
+    assert_eq!(offset_of!(RepackedAroundNiche, B.0), 6);
 
-        assert_eq!(size_of::<RepackedAroundNiche>(), 8);
-        assert_eq!(align_of::<RepackedAroundNiche>(), 4);
-        assert_eq!(size_of::<Option<RepackedAroundNiche>>(), 8);
-        assert_eq!(offset_of!(RepackedAroundNiche, A.0), 0);
-        assert_eq!(offset_of!(RepackedAroundNiche, B.1), 0);
-        assert_eq!(offset_of!(RepackedAroundNiche, B.2), 5);
-        assert_eq!(offset_of!(RepackedAroundNiche, B.0), 6);
+    assert_eq!(size_of::<NestedRepackedAroundNiche>(), 8);
+    assert_eq!(align_of::<NestedRepackedAroundNiche>(), 4);
+    assert_eq!(size_of::<Option<NestedRepackedAroundNiche>>(), 8);
+    assert_eq!(offset_of!(NestedRepackedAroundNiche, A.0), 0);
+    assert_eq!(offset_of!(NestedRepackedAroundNiche, B.1), 0);
+    assert_eq!(offset_of!(NestedRepackedAroundNiche, B.2), 5);
+    assert_eq!(offset_of!(NestedRepackedAroundNiche, B.0), 6);
 
-        assert_eq!(size_of::<NestedRepackedAroundNiche>(), 8);
-        assert_eq!(align_of::<NestedRepackedAroundNiche>(), 4);
-        assert_eq!(size_of::<Option<NestedRepackedAroundNiche>>(), 8);
-        assert_eq!(offset_of!(NestedRepackedAroundNiche, A.0), 0);
-        assert_eq!(offset_of!(NestedRepackedAroundNiche, B.1), 0);
-        assert_eq!(offset_of!(NestedRepackedAroundNiche, B.2), 5);
-        assert_eq!(offset_of!(NestedRepackedAroundNiche, B.0), 6);
+    assert_eq!(size_of::<RepackedPair>(), 4);
+    assert_eq!(align_of::<RepackedPair>(), 2);
+    assert_eq!(offset_of!(RepackedPair, field2), 0);
+    assert_eq!(offset_of!(RepackedPair, field3), 2);
 
-        assert_eq!(size_of::<RepackedPair>(), 4);
-        assert_eq!(align_of::<RepackedPair>(), 2);
-        assert_eq!(offset_of!(RepackedPair, field2), 0);
-        assert_eq!(offset_of!(RepackedPair, field3), 2);
+    assert_eq!(size_of::<RepackedNestedAggregate>(), 8);
+    assert_eq!(align_of::<RepackedNestedAggregate>(), 4);
+    assert_eq!(size_of::<Option<RepackedNestedAggregate>>(), 8);
+    assert_eq!(offset_of!(RepackedNestedAggregate, B.0), 0);
+    assert_eq!(offset_of!(RepackedNestedAggregate, A.1), 0);
+    assert_eq!(offset_of!(RepackedNestedAggregate, A.0), 6);
 
-        assert_eq!(size_of::<RepackedNestedAggregate>(), 8);
-        assert_eq!(align_of::<RepackedNestedAggregate>(), 4);
-        assert_eq!(size_of::<Option<RepackedNestedAggregate>>(), 8);
-        assert_eq!(offset_of!(RepackedNestedAggregate, B.0), 0);
-        assert_eq!(offset_of!(RepackedNestedAggregate, A.1), 0);
-        assert_eq!(offset_of!(RepackedNestedAggregate, A.0), 6);
+    assert_eq!(size_of::<FittingNichePayload>(), 8);
+    assert_eq!(align_of::<FittingNichePayload>(), 4);
+    assert_eq!(offset_of!(FittingNichePayload, word), 0);
+    assert_eq!(offset_of!(FittingNichePayload, flag), 4);
 
-        assert_eq!(size_of::<FittingNichePayload>(), 8);
-        assert_eq!(align_of::<FittingNichePayload>(), 4);
-        assert_eq!(offset_of!(FittingNichePayload, word), 0);
-        assert_eq!(offset_of!(FittingNichePayload, flag), 4);
-
-        assert_eq!(size_of::<ChooseFittingNiche>(), 8);
-        assert_eq!(align_of::<ChooseFittingNiche>(), 4);
-        assert_eq!(size_of::<Option<ChooseFittingNiche>>(), 8);
-        assert_eq!(offset_of!(ChooseFittingNiche, WideNiche.0), 0);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.1), 0);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.2), 5);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.0), 6);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.1), 0);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.2), 5);
-        assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.0), 6);
-    }
+    assert_eq!(size_of::<ChooseFittingNiche>(), 8);
+    assert_eq!(align_of::<ChooseFittingNiche>(), 4);
+    assert_eq!(size_of::<Option<ChooseFittingNiche>>(), 8);
+    assert_eq!(offset_of!(ChooseFittingNiche, WideNiche.0), 0);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.1), 0);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.2), 5);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheFront.0), 6);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.1), 0);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.2), 5);
+    assert_eq!(offset_of!(ChooseFittingNiche, SmallNicheBack.0), 6);
 }
