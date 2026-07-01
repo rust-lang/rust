fe853279dbcaa8df29ffe1f02dcfd7ce5919ed50
Tim (Theemathas Chirananthavat) <theemathas@gmail.com>
2026-07-01 22:50:19 +0700

    Revert "Remove redundant dyn-compatibility check."
    This reverts commit 5fad8a6a2313aa0c8d90197b55a0c644396fbc20.

--- compiler/rustc_trait_selection/src/traits/select/mod.rs
+++ compiler/rustc_trait_selection/src/traits/select/mod.rs
@@ -771,10 +771,12 @@ impl<'cx, 'tcx> SelectionContext<'cx, 'tcx> {
                     Ok(EvaluatedToOkModuloRegions)
                 }
 
-                ty::PredicateKind::DynCompatible(_) => {
-                    bug!(
-                        "Obligation {obligation:?} should have been handled by fulfillment already."
-                    )
+                ty::PredicateKind::DynCompatible(trait_def_id) => {
+                    if self.tcx().is_dyn_compatible(trait_def_id) {
+                        Ok(EvaluatedToOk)
+                    } else {
+                        Ok(EvaluatedToErr)
+                    }
                 }
 
                 ty::PredicateKind::Clause(ty::ClauseKind::Projection(data)) => {
