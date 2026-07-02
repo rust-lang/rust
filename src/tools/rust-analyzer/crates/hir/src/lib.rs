23595b559ff0a26917203156852b206daad7772a
Ada Alakbarova <ada.alakbarova@ferrous-systems.com>
2026-07-02 21:59:17 +0200

    ensure that the macro is declarative beforehand
    removes the `never` branch

--- src/tools/rust-analyzer/crates/hir/src/lib.rs
+++ src/tools/rust-analyzer/crates/hir/src/lib.rs
@@ -1188,13 +1188,10 @@ fn emit_macro_def_diagnostics<'db>(
     m: Macro,
 ) {
     let id = db.macro_def(m.id);
-    if let hir_expand::db::TokenExpander::DeclarativeMacro(expander) = db.macro_expander(id)
+    if let hir_expand::MacroDefKind::Declarative(ast, _) = id.kind
+        && let expander = db.decl_macro_expander(id.krate, ast)
         && let Some(e) = expander.mac.err()
     {
-        let Some(ast) = id.ast_id().left() else {
-            never!("declarative expander for non decl-macro: {:?}", e);
-            return;
-        };
         let krate = HasModule::krate(&m.id, db);
         let edition = krate.data(db).edition;
         emit_def_diagnostic_(
