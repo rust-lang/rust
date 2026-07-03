8b8dde8712fc09071d34e27f9c54a2a909fb0874
Takayuki Maeda <takoyaki0316@gmail.com>
2026-07-03 17:58:30 +0900

    update open namespace test expectations

--- tests/ui/resolve/open-ns-2.rs
+++ tests/ui/resolve/open-ns-2.rs
@@ -1,3 +1,4 @@
+//@ build-pass
 //@ aux-crate: my_api=open-ns-my_api.rs
 //@ aux-crate: my_api::utils=open-ns-my_api_utils.rs
 //@ aux-crate: my_api::core=open-ns-my_api_core.rs
@@ -5,9 +6,7 @@
 //@ edition: 2024
 
 use my_api::core::{core_fn, core_fn2};
-//~^ ERROR unresolved import `my_api::core` [E0432]
 use my_api::utils::*;
-//~^ ERROR unresolved import `my_api::utils` [E0432]
 use my_api::*;
 
 fn main() {
