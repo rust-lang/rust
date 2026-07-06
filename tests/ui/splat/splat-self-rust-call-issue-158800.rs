c213b5f1c76fc74a002ce0af935be8ec9271a0cf
Yukang <moorekang@gmail.com>
2026-07-06 13:49:43 +0800

    Avoid underflow for splatted rust-call receivers

---
+++ tests/ui/splat/splat-self-rust-call-issue-158800.rs
@@ -0,0 +1,16 @@
+//! Regression test for `#[splat] self` in a rust-call method not ICEing in WF
+//! checking.
+
+#![feature(splat)]
+#![feature(unboxed_closures)]
+#![allow(incomplete_features)]
+
+impl<T> dyn FnOnce(T) -> () {
+    //~^ ERROR cannot define inherent `impl` for a type outside of the crate
+    extern "rust-call" fn call_once(#[splat] self) {}
+    //~^ ERROR `#[splat]` is not allowed in the arguments of functions with the `rust-call` ABI
+    //~| ERROR functions with the "rust-call" ABI must take a single non-self tuple argument
+    //~| ERROR the size for values of type `(dyn FnOnce(T) + 'static)` cannot be known
+}
+
+fn main() {}
