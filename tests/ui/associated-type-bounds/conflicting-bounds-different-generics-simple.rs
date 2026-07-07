8546c21dac83a3e2db7e80d2c5af5eb81fcb4bd6
Tim (Theemathas Chirananthavat) <theemathas@gmail.com>
2026-07-07 21:57:53 +0700

    Require coherence of supertrait associated item bounds for dyn-compability
    Fixes https://github.com/rust-lang/rust/issues/154662
    
    In a `dyn` type, if multiple bounds are specified via supertraits for
    the same associated item, then we previously accepted them if
    the relevant trait's generics are different,
    even if the bounds conflict.
    
    This was unsound, since those generics could end up being instantiated
    with identical concrete types, causing the `dyn` type to have
    two different "values" for the same bound.
    
    Thus, if a trait has multiple supertrait bounds for the same
    associated item, we check whether those bounds are coherent,
    similarly to how we check for overlap between impls (i.e., we check
    if the generics could be instantiated to be the same while the "values"
    of the bounds are different). If the bounds are incoherent, then we
    consider the trait to be dyn-incompatible.

--- tests/ui/associated-type-bounds/conflicting-bounds-different-generics-simple.rs
+++ tests/ui/associated-type-bounds/conflicting-bounds-different-generics-simple.rs
@@ -1,7 +1,3 @@
-//@ known-bug: #154662
-//@ failure-status: 101
-//@ compile-flags: --emit link
-
 // We currently accept conflicting associated type bounds with different generics,
 // which results in an ICE, since those generics can be instantiated with the
 // same concrete type.
@@ -16,8 +12,10 @@ trait Sub<T, U>: Super<T, Assoc = u32> + Super<U, Assoc = u64> {
 }
 
 fn foo<T, U>(x: Option<&dyn Sub<T, U>>) {
+    //~^ ERROR the trait `Sub` is not dyn compatible
     if false {
         x.unwrap().method();
+        //~^ ERROR the trait `Sub` is not dyn compatible
     }
 }
 
@@ -26,4 +24,5 @@ fn main() {
     // However, `dyn Sub<i16, i16>` has bounds for both `Assoc = u32` and `Assoc = u64`,
     // which is nonsense.
     foo::<i16, i16>(None);
+    //~^ ERROR the trait `Sub` is not dyn compatible
 }
