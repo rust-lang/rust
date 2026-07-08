f55de235dbbefa32f351aa110e6d9385d152abf9
Xuyang Zhang <huan9d0w@gmail.com>
2026-07-08 21:41:51 +0800

    Fix bootstrap submodule path prefix matching

--- src/bootstrap/src/utils/helpers.rs
+++ src/bootstrap/src/utils/helpers.rs
@@ -96,7 +96,11 @@ pub fn is_dylib(path: &Path) -> bool {
 
 /// Return the path to the containing submodule if available.
 pub fn submodule_path_of(builder: &Builder<'_>, path: &str) -> Option<String> {
-    let submodule_paths = builder.submodule_paths();
+    submodule_path_of_paths(builder.submodule_paths(), path)
+}
+
+fn submodule_path_of_paths(submodule_paths: &[String], path: &str) -> Option<String> {
+    let path = Path::new(path);
     submodule_paths.iter().find_map(|submodule_path| {
         if path.starts_with(submodule_path) { Some(submodule_path.to_string()) } else { None }
     })
