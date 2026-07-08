3c9b183b76abbc326798a96227d49fc40e6fd46b
zedddie <zedddie@protonmail.com>
2026-07-08 23:26:44 +0200

    bless batch

--- tests/ui/generics/unused-type-and-lifetime-param.rs
+++ tests/ui/generics/unused-type-and-lifetime-param.rs
@@ -1,3 +1,6 @@
+//! Regression test for <https://github.com/rust-lang/rust/issues/36299>.
+//! This used to ICE.
+
 struct Foo<'a, A> {}
 //~^ ERROR parameter `'a` is never used
 //~| ERROR parameter `A` is never used
--- tests/ui/macros/macro-in-array-len-expr.rs
+++ tests/ui/macros/macro-in-array-len-expr.rs
@@ -1,4 +1,7 @@
+//! Regression test for <https://github.com/rust-lang/rust/issues/36816>.
+//! Macro invocation in len position in arrays used to ICE.
 //@ run-pass
+
 macro_rules! m { () => { 1 } }
 macro_rules! n { () => { 1 + m!() } }
 
