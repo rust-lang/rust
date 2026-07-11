2200ed1c97f2d58772bd532e3fb4ffc8f95b27f3
Michael Howell <michael@notriddle.com>
2026-07-10 18:04:30 -0700

    Add requested description comment

--- tests/rustdoc-ui/lints/deny-cmd.rs
+++ tests/rustdoc-ui/lints/deny-cmd.rs
@@ -2,7 +2,8 @@
 //@[deny] compile-flags: -Dmissing_docs
 //@[allow] compile-flags: -Amissing_docs
 //@[allow] check-pass
-//! docs for crate
+
+//! Verify that the `-D` flag, passed to rustdoc, works as expected
 
 pub struct Foo;
 //[deny]~^ ERROR missing_docs
