4bc432455f30689e1a0cafa4b8ae5671396388c2
Dnreikronos <dnreikronos2@gmail.com>
2026-07-11 11:12:20 -0300

    Move region constraint tests into test module

--- compiler/rustc_type_ir/src/region_constraint.rs
+++ compiler/rustc_type_ir/src/region_constraint.rs
@@ -644,31 +644,7 @@ impl<I: Interner> TypeFolder<I> for EquatedRegionVarReplacer<I> {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::compute_equated_region_var_replacements_from;
-
-    #[test]
-    fn equated_region_var_replacements_follow_transitive_region_var_chains() {
-        const REVAR_1: u8 = 1;
-        const REVAR_2: u8 = 2;
-        const PLACEHOLDER: u8 = 3;
-
-        let region_outlives = [
-            (REVAR_1, REVAR_2),
-            (REVAR_2, REVAR_1),
-            (REVAR_2, PLACEHOLDER),
-            (PLACEHOLDER, REVAR_2),
-        ];
-
-        let replacements = compute_equated_region_var_replacements_from(
-            &region_outlives,
-            |r| matches!(r, REVAR_1 | REVAR_2),
-            |r| matches!(r, REVAR_1 | REVAR_2),
-        );
-
-        assert_eq!(replacements, vec![(REVAR_1, PLACEHOLDER), (REVAR_2, PLACEHOLDER)]);
-    }
-}
+mod tests;
 
 /// Filter our region constraints to not include constraints between region variables from `u` and
 /// other regions as those are always satisfied. This requires some care to handle correctly for example:
