75fddd925f31de2386510b04f85a8fdc973acb89
Predrag Gruevski <obi1kenobi82@gmail.com>
2026-07-11 01:03:53 -0400

    [perf] Enable inlining trait items that only contain inlined code.

--- tests/codegen-llvm/cross-crate-inlining/auxiliary/leaf.rs
+++ tests/codegen-llvm/cross-crate-inlining/auxiliary/leaf.rs
@@ -28,3 +28,51 @@ pub fn leaf_with_intrinsic(a: &[u64; 2], b: &[u64; 2]) -> bool {
 pub fn leaf_with_assert(a: i32, b: i32) -> i32 {
     a / b
 }
+
+pub trait FindToken<T> {
+    fn find_token(&self, token: T) -> bool;
+}
+
+// This function's optimized MIR contains only statically dispatched trait calls.
+// Their selected implementations are marked inline.
+impl FindToken<char> for &str {
+    fn find_token(&self, token: char) -> bool {
+        self.chars().any(|character| character == token)
+    }
+}
+
+pub struct NonInlineTraitCall;
+
+pub trait NonInlineTrait {
+    fn call(&self, value: u32) -> u32;
+}
+
+impl NonInlineTrait for NonInlineTraitCall {
+    #[inline(never)]
+    fn call(&self, value: u32) -> u32 {
+        value + 1
+    }
+}
+
+pub fn stem_with_non_inline_trait_call(value: u32) -> u32 {
+    <NonInlineTraitCall as NonInlineTrait>::call(&NonInlineTraitCall, value)
+}
+
+pub struct ExportedInlineTraitCall;
+
+pub trait ExportedInlineTrait {
+    fn exported_call(&self, value: u32) -> u32;
+}
+
+impl ExportedInlineTrait for ExportedInlineTraitCall {
+    #[expect(unused_attributes)]
+    #[inline]
+    #[no_mangle]
+    fn exported_call(&self, value: u32) -> u32 {
+        value + 2
+    }
+}
+
+pub fn stem_with_exported_inline_trait_call(value: u32) -> u32 {
+    <ExportedInlineTraitCall as ExportedInlineTrait>::exported_call(&ExportedInlineTraitCall, value)
+}
--- tests/codegen-llvm/cross-crate-inlining/leaf-inlining.rs
+++ tests/codegen-llvm/cross-crate-inlining/leaf-inlining.rs
@@ -6,6 +6,7 @@
 extern crate leaf;
 
 // Check that we inline a leaf cross-crate call
+// CHECK-LABEL: @leaf_outer(
 #[no_mangle]
 pub fn leaf_outer() -> String {
     // CHECK-NOT: call {{.*}}leaf_fn
@@ -13,6 +14,7 @@ pub fn leaf_outer() -> String {
 }
 
 // Check that we do not inline a non-leaf cross-crate call
+// CHECK-LABEL: @stem_outer(
 #[no_mangle]
 pub fn stem_outer() -> String {
     // CHECK: call {{.*}}stem_fn
@@ -20,6 +22,7 @@ pub fn stem_outer() -> String {
 }
 
 // Check that we inline functions that call intrinsics
+// CHECK-LABEL: @leaf_with_intrinsic_outer(
 #[no_mangle]
 pub fn leaf_with_intrinsic_outer(a: &[u64; 2], b: &[u64; 2]) -> bool {
     // CHECK-NOT: call {{.*}}leaf_with_intrinsic
@@ -27,10 +30,38 @@ pub fn leaf_with_intrinsic_outer(a: &[u64; 2], b: &[u64; 2]) -> bool {
 }
 
 // Check that we inline functions with assert terminators
+// CHECK-LABEL: @leaf_with_assert(
 #[no_mangle]
 pub fn leaf_with_assert(a: i32, b: i32) -> i32 {
     // CHECK-NOT: call {{.*}}leaf_with_assert
     // CHECK: sdiv i32 %a, %b
     // CHECK-NOT: call {{.*}}leaf_with_assert
     leaf::leaf_with_assert(a, b)
+}
+
+// Check that we inline functions with only statically dispatched trait calls.
+// The selected implementations are inline.
+// CHECK-LABEL: @leaf_with_inline_trait_calls(
+#[no_mangle]
+pub fn leaf_with_inline_trait_calls(input: &str, token: char) -> bool {
+    // CHECK-NOT: call {{.*}}find_token
+    <&str as leaf::FindToken<char>>::find_token(&input, token)
+}
+
+// Check that we do not inline functions with statically dispatched trait calls
+// when the selected implementations are not inline.
+// CHECK-LABEL: @stem_with_non_inline_trait_call(
+#[no_mangle]
+pub fn stem_with_non_inline_trait_call(value: u32) -> u32 {
+    // CHECK: call {{.*}}stem_with_non_inline_trait_call
+    leaf::stem_with_non_inline_trait_call(value)
+}
+
+// Check that we do not inline functions with statically dispatched trait calls
+// when the selected implementations are exported and therefore globally shared.
+// CHECK-LABEL: @stem_with_exported_inline_trait_call(
+#[no_mangle]
+pub fn stem_with_exported_inline_trait_call(value: u32) -> u32 {
+    // CHECK: call {{.*}}stem_with_exported_inline_trait_call
+    leaf::stem_with_exported_inline_trait_call(value)
 }
