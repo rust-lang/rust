rust-lang/rust-analyzer

Turn `BlockLoc` into a tracked struct, take 3

crates/hir-def/src/nameres.rs
@@ -76,7 +76,7 @@ use triomphe::Arc;
 use tt::TextRange;
 
 use crate::{
-    AstId, BlockId, BlockLoc, BuiltinDeriveImplId, ExternCrateId, FunctionId, FxIndexMap, Lookup,
+    AstId, BlockId, BlockIdLt, BuiltinDeriveImplId, ExternCrateId, FunctionId, FxIndexMap, Lookup,
     MacroCallStyles, MacroExpander, MacroId, ModuleId, ModuleIdLt, ProcMacroId, UseId,
     db::DefDatabase,
     item_scope::{BuiltinShadowMode, ItemScope},
@@ -273,12 +273,12 @@ struct BlockInfo {
     parent: ModuleId,
 }
 
-impl std::ops::Index<ModuleId> for DefMap {
+impl std::ops::Index<ModuleIdLt<'_>> for DefMap {
     type Output = ModuleData;
 
-    fn index(&self, id: ModuleId) -> &ModuleData {
+    fn index(&self, id: ModuleIdLt<'_>) -> &ModuleData {
         self.modules
-            .get(&id)
+            .get(&unsafe { id.to_static() })
             .unwrap_or_else(|| panic!("ModuleId not found in ModulesMap {:#?}: {id:#?}", self.root))
     }
 }
@@ -426,8 +426,10 @@ pub(crate) fn crate_local_def_map(db: &dyn DefDatabase, crate_id: Crate) -> DefM
 }
 
 #[salsa_macros::tracked(returns(ref))]
-pub fn block_def_map(db: &dyn DefDatabase, block_id: BlockId) -> DefMap {
-    let BlockLoc { ast_id, module } = *block_id.lookup(db);
+pub fn block_def_map<'db>(db: &'db dyn DefDatabase, block_id: BlockIdLt<'db>) -> DefMap {
+    let block_id = unsafe { block_id.to_static() };
+    let ast_id = block_id.ast_id(db);
+    let module = unsafe { block_id.module(db).to_static() };
 
     let visibility = Visibility::Module(module, VisibilityExplicitness::Implicit);
     let module_data =
@@ -615,7 +617,7 @@ impl DefMap {
 
     /// Returns the module containing `local_mod`, either the parent `mod`, or the module (or block) containing
     /// the block, if `self` corresponds to a block expression.
-    pub fn containing_module(&self, local_mod: ModuleId) -> Option<ModuleId> {
+    pub fn containing_module(&self, local_mod: ModuleIdLt<'_>) -> Option<ModuleId> {
         match self[local_mod].parent {
             Some(parent) => Some(parent),
             None => self.block.map(|BlockInfo { parent, .. }| parent),
@@ -720,11 +722,11 @@ impl DefMap {
     ///
     /// If `f` returns `Some(val)`, iteration is stopped and `Some(val)` is returned. If `f` returns
     /// `None`, iteration continues.
-    pub(crate) fn with_ancestor_maps<T>(
+    pub(crate) fn with_ancestor_maps<'db, T>(
         &self,
-        db: &dyn DefDatabase,
-        local_mod: ModuleId,
-        f: &mut dyn FnMut(&DefMap, ModuleId) -> Option<T>,
+        db: &'db dyn DefDatabase,
+        local_mod: ModuleIdLt<'db>,
+        f: &mut dyn FnMut(&DefMap, ModuleIdLt<'db>) -> Option<T>,
     ) -> Option<T> {
         if let Some(it) = f(self, local_mod) {
             return Some(it);
@@ -911,11 +913,13 @@ impl DerefMut for ModulesMap {
     }
 }
 
-impl Index<ModuleId> for ModulesMap {
+impl Index<ModuleIdLt<'_>> for ModulesMap {
     type Output = ModuleData;
 
-    fn index(&self, id: ModuleId) -> &ModuleData {
-        self.inner.get(&id).unwrap_or_else(|| panic!("ModuleId not found in ModulesMap: {id:#?}"))
+    fn index(&self, id: ModuleIdLt<'_>) -> &ModuleData {
+        self.inner
+            .get(&unsafe { id.to_static() })
+            .unwrap_or_else(|| panic!("ModuleId not found in ModulesMap: {id:#?}"))
     }
 }
 

crates/hir-def/src/visibility.rs
@@ -8,8 +8,8 @@ use la_arena::ArenaMap;
 use syntax::ast::{self, HasVisibility};
 
 use crate::{
-    AssocItemId, HasModule, ItemContainerId, LocalFieldId, ModuleId, TraitId, VariantId,
-    db::DefDatabase, nameres::DefMap, resolver::HasResolver, signatures::VariantFields,
+    AssocItemId, HasModule, ItemContainerId, LocalFieldId, ModuleId, ModuleIdLt, TraitId,
+    VariantId, db::DefDatabase, nameres::DefMap, resolver::HasResolver, signatures::VariantFields,
     src::HasSource,
 };
 
@@ -41,9 +41,13 @@ impl Visibility {
     }
 
     #[tracing::instrument(skip_all)]
-    pub fn is_visible_from(self, db: &dyn DefDatabase, from_module: ModuleId) -> bool {
+    pub fn is_visible_from<'db>(
+        self,
+        db: &'db dyn DefDatabase,
+        from_module: ModuleIdLt<'db>,
+    ) -> bool {
         let to_module = match self {
-            Visibility::Module(m, _) => m,
+            Visibility::Module(m, _) => unsafe { m.to_db(db) },
             Visibility::PubCrate(krate) => return from_module.krate(db) == krate,
             Visibility::Public => return true,
         };
@@ -59,11 +63,11 @@ impl Visibility {
         Self::is_visible_from_def_map_(db, def_map, to_module, from_module)
     }
 
-    pub(crate) fn is_visible_from_def_map(
+    pub(crate) fn is_visible_from_def_map<'db>(
         self,
-        db: &dyn DefDatabase,
-        def_map: &DefMap,
-        from_module: ModuleId,
+        db: &'db dyn DefDatabase,
+        def_map: &'db DefMap,
+        from_module: ModuleIdLt<'db>,
     ) -> bool {
         if cfg!(debug_assertions) {
             _ = def_map.modules[from_module];
@@ -89,11 +93,11 @@ impl Visibility {
         Self::is_visible_from_def_map_(db, def_map, to_module, from_module)
     }
 
-    fn is_visible_from_def_map_(
-        db: &dyn DefDatabase,
-        def_map: &DefMap,
-        mut to_module: ModuleId,
-        mut from_module: ModuleId,
+    fn is_visible_from_def_map_<'db>(
+        db: &'db dyn DefDatabase,
+        def_map: &'db DefMap,
+        mut to_module: ModuleIdLt<'db>,
+        mut from_module: ModuleIdLt<'db>,
     ) -> bool {
         debug_assert_eq!(to_module.krate(db), def_map.krate());
         // `to_module` might be the root module of a block expression. Those have the same

rust-lang/rust

src/tools/rust-analyzer/crates/hir-def/src/nameres.rs
@@ -76,7 +76,7 @@ use triomphe::Arc;
 use tt::TextRange;
 
 use crate::{
-    AstId, BlockId, BlockLoc, BuiltinDeriveImplId, ExternCrateId, FunctionId, FxIndexMap, Lookup,
+    AstId, BlockId, BlockIdLt, BuiltinDeriveImplId, ExternCrateId, FunctionId, FxIndexMap, Lookup,
     MacroCallStyles, MacroExpander, MacroId, ModuleId, ModuleIdLt, ProcMacroId, UseId,
     item_scope::{BuiltinShadowMode, ItemScope},
     item_tree::TreeId,
@@ -272,12 +272,12 @@ struct BlockInfo {
     parent: ModuleId,
 }
 
-impl std::ops::Index<ModuleId> for DefMap {
+impl std::ops::Index<ModuleIdLt<'_>> for DefMap {
     type Output = ModuleData;
 
-    fn index(&self, id: ModuleId) -> &ModuleData {
+    fn index(&self, id: ModuleIdLt<'_>) -> &ModuleData {
         self.modules
-            .get(&id)
+            .get(&unsafe { id.to_static() })
             .unwrap_or_else(|| panic!("ModuleId not found in ModulesMap {:#?}: {id:#?}", self.root))
     }
 }
@@ -425,8 +425,10 @@ pub(crate) fn crate_local_def_map(db: &dyn SourceDatabase, crate_id: Crate) -> D
 }
 
 #[salsa_macros::tracked(returns(ref))]
-pub fn block_def_map(db: &dyn SourceDatabase, block_id: BlockId) -> DefMap {
-    let BlockLoc { ast_id, module } = *block_id.lookup(db);
+pub fn block_def_map<'db>(db: &'db dyn SourceDatabase, block_id: BlockIdLt<'db>) -> DefMap {
+    let block_id = unsafe { block_id.to_static() };
+    let ast_id = block_id.ast_id(db);
+    let module = unsafe { block_id.module(db).to_static() };
 
     let visibility = Visibility::Module(module, VisibilityExplicitness::Implicit);
     let module_data =
@@ -614,7 +616,7 @@ impl DefMap {
 
     /// Returns the module containing `local_mod`, either the parent `mod`, or the module (or block) containing
     /// the block, if `self` corresponds to a block expression.
-    pub fn containing_module(&self, local_mod: ModuleId) -> Option<ModuleId> {
+    pub fn containing_module(&self, local_mod: ModuleIdLt<'_>) -> Option<ModuleId> {
         match self[local_mod].parent {
             Some(parent) => Some(parent),
             None => self.block.map(|BlockInfo { parent, .. }| parent),
@@ -725,11 +727,11 @@ impl DefMap {
     ///
     /// If `f` returns `Some(val)`, iteration is stopped and `Some(val)` is returned. If `f` returns
     /// `None`, iteration continues.
-    pub(crate) fn with_ancestor_maps<T>(
+    pub(crate) fn with_ancestor_maps<'db, T>(
         &self,
-        db: &dyn SourceDatabase,
-        local_mod: ModuleId,
-        f: &mut dyn FnMut(&DefMap, ModuleId) -> Option<T>,
+        db: &'db dyn SourceDatabase,
+        local_mod: ModuleIdLt<'db>,
+        f: &mut dyn FnMut(&DefMap, ModuleIdLt<'db>) -> Option<T>,
     ) -> Option<T> {
         if let Some(it) = f(self, local_mod) {
             return Some(it);
@@ -916,11 +918,13 @@ impl DerefMut for ModulesMap {
     }
 }
 
-impl Index<ModuleId> for ModulesMap {
+impl Index<ModuleIdLt<'_>> for ModulesMap {
     type Output = ModuleData;
 
-    fn index(&self, id: ModuleId) -> &ModuleData {
-        self.inner.get(&id).unwrap_or_else(|| panic!("ModuleId not found in ModulesMap: {id:#?}"))
+    fn index(&self, id: ModuleIdLt<'_>) -> &ModuleData {
+        self.inner
+            .get(&unsafe { id.to_static() })
+            .unwrap_or_else(|| panic!("ModuleId not found in ModulesMap: {id:#?}"))
     }
 }
 

src/tools/rust-analyzer/crates/hir-def/src/visibility.rs
@@ -8,8 +8,8 @@ use la_arena::ArenaMap;
 use syntax::ast::{self, HasVisibility};
 
 use crate::{
-    AssocItemId, HasModule, ItemContainerId, LocalFieldId, ModuleId, TraitId, VariantId,
-    nameres::DefMap, resolver::HasResolver, signatures::VariantFields, src::HasSource,
+    AssocItemId, HasModule, ItemContainerId, LocalFieldId, ModuleId, ModuleIdLt, TraitId,
+    VariantId, nameres::DefMap, resolver::HasResolver, signatures::VariantFields, src::HasSource,
 };
 
 pub use crate::item_tree::{RawVisibility, VisibilityExplicitness};
@@ -40,9 +40,13 @@ impl Visibility {
     }
 
     #[tracing::instrument(skip_all)]
-    pub fn is_visible_from(self, db: &dyn SourceDatabase, from_module: ModuleId) -> bool {
+    pub fn is_visible_from<'db>(
+        self,
+        db: &'db dyn SourceDatabase,
+        from_module: ModuleIdLt<'db>,
+    ) -> bool {
         let to_module = match self {
-            Visibility::Module(m, _) => m,
+            Visibility::Module(m, _) => unsafe { m.to_db(db) },
             Visibility::PubCrate(krate) => return from_module.krate(db) == krate,
             Visibility::Public => return true,
         };
@@ -58,11 +62,11 @@ impl Visibility {
         Self::is_visible_from_def_map_(db, def_map, to_module, from_module)
     }
 
-    pub(crate) fn is_visible_from_def_map(
+    pub(crate) fn is_visible_from_def_map<'db>(
         self,
-        db: &dyn SourceDatabase,
-        def_map: &DefMap,
-        from_module: ModuleId,
+        db: &'db dyn SourceDatabase,
+        def_map: &'db DefMap,
+        from_module: ModuleIdLt<'db>,
     ) -> bool {
         if cfg!(debug_assertions) {
             _ = def_map.modules[from_module];
@@ -88,11 +92,11 @@ impl Visibility {
         Self::is_visible_from_def_map_(db, def_map, to_module, from_module)
     }
 
-    fn is_visible_from_def_map_(
-        db: &dyn SourceDatabase,
-        def_map: &DefMap,
-        mut to_module: ModuleId,
-        mut from_module: ModuleId,
+    fn is_visible_from_def_map_<'db>(
+        db: &'db dyn SourceDatabase,
+        def_map: &'db DefMap,
+        mut to_module: ModuleIdLt<'db>,
+        mut from_module: ModuleIdLt<'db>,
     ) -> bool {
         debug_assert_eq!(to_module.krate(db), def_map.krate());
         // `to_module` might be the root module of a block expression. Those have the same