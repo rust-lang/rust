d19f2f899b37bc7873941d0cc05fa1882bfde87d
Crystal Durham <crystal.durham@canonical.com>
2026-07-13 15:02:43 -0400

    refactor darwin UserDirsExt

--- library/std/src/os/darwin/fs/dirs.rs
+++ library/std/src/os/darwin/fs/dirs.rs
@@ -93,20 +93,21 @@ pub trait UserDirsExt: Sized + Sealed {
 #[cfg(target_vendor = "apple")]
 impl UserDirsExt for UserDirs {
     fn sysdir() -> io::Result<Self> {
+        use libc::sysdir_search_path_directory_t::*;
+
         let mut dirs = UserDirs::new();
         let home =
             dirs.user_home().ok_or(const_error!(ErrorKind::NotFound, "no home directory"))?;
 
-        let caches = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_CACHES)?;
-        let application_support =
-            get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_APPLICATION_SUPPORT)?;
-        let desktop = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_DESKTOP)?;
-        let documents = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_DOCUMENT)?;
-        let downloads = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_DOWNLOADS)?;
-        let movies = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_MOVIES)?;
-        let music = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_MUSIC)?;
-        let pictures = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_PICTURES)?;
-        let public_share = get_user_sysdir(&home, sys::SYSDIR_DIRECTORY_SHARED_PUBLIC)?;
+        let caches = sys::get_user_dir(&home, SYSDIR_DIRECTORY_CACHES)?;
+        let application_support = sys::get_user_dir(&home, SYSDIR_DIRECTORY_APPLICATION_SUPPORT)?;
+        let desktop = sys::get_user_dir(&home, SYSDIR_DIRECTORY_DESKTOP)?;
+        let documents = sys::get_user_dir(&home, SYSDIR_DIRECTORY_DOCUMENT)?;
+        let downloads = sys::get_user_dir(&home, SYSDIR_DIRECTORY_DOWNLOADS)?;
+        let movies = sys::get_user_dir(&home, SYSDIR_DIRECTORY_MOVIES)?;
+        let music = sys::get_user_dir(&home, SYSDIR_DIRECTORY_MUSIC)?;
+        let pictures = sys::get_user_dir(&home, SYSDIR_DIRECTORY_PICTURES)?;
+        let public_share = sys::get_user_dir(&home, SYSDIR_DIRECTORY_SHARED_PUBLIC)?;
 
         dirs.home.cache = caches;
         // Apple puts config/data/state all in Application Support
@@ -126,80 +127,119 @@ impl UserDirsExt for UserDirs {
     }
 }
 
-/// Get the path for a system directory using `sysdir(3)`.
-#[cfg(target_vendor = "apple")]
-fn get_user_sysdir(
-    home: &Path,
-    kind: sys::sysdir_search_path_directory_t,
-) -> io::Result<Option<PathBuf>> {
-    // SAFETY: `kind` is a valid `sysdir_search_path_directory_t` value, and
-    //   `SYSDIR_DOMAIN_MASK_USER` is a valid `sysdir_search_path_domain_mask_t` value.
-    let state =
-        unsafe { sys::sysdir_start_search_path_enumeration(kind, sys::SYSDIR_DOMAIN_MASK_USER) };
-    if state == 0 {
-        // 0 directory paths produced
-        return Ok(None);
-    }
-
-    let mut path = [0u8; sys::PATH_MAX as usize];
-    // SAFETY: `state` is nonzero and comes from `sysdir_start_search_path_enumeration`
-    // SAFETY: sysdir_get_next_search_path_enumeration will write at most `PATH_MAX-1` bytes to `path`
-    let state = unsafe {
-        sys::sysdir_get_next_search_path_enumeration(state, path.as_mut_ptr() as *mut c_char)
-    };
-    if state == 0 {
-        // exactly 1 directory path produced
-        let Ok(path) = CStr::from_bytes_until_nul(&path) else {
-            return Err(const_error!(
-                ErrorKind::InvalidData,
-                "standard user directory path too long",
-            ));
-        };
-        // read as UTF-8
-        let Ok(path) = path.to_str() else {
-            return Err(const_error!(
-                ErrorKind::InvalidData,
-                "standard user directory path not valid UTF-8",
-            ));
-        };
-        // expand the `~` shorthand
-        return if path == "~" {
-            Ok(Some(home.into()))
-        } else if path.starts_with("~/") {
-            Ok(Some(home.join(&path[2..])))
-        } else if path.starts_with("~") {
-            Err(const_error!(
-                ErrorKind::InvalidData,
-                "standard user directory relative to different user",
-            ))
-        } else if path.starts_with('/') {
-            Ok(Some(path.into()))
-        } else {
-            Err(const_error!(ErrorKind::InvalidData, "standard user directory path not absolute"))
-        };
-    }
-
-    // 2+ directory paths produced
-    let mut state = state;
-    // exhaust iterator for cleanup; ignore the paths that get returned
-    while state != 0 {
-        // SAFETY: `state` is nonzero and is from a previous call to sysdir_get_next_search_path_enumeration
-        state = unsafe {
-            sys::sysdir_get_next_search_path_enumeration(state, path.as_mut_ptr() as *mut c_char)
-        };
-    }
-
-    Err(const_error!(ErrorKind::InvalidData, "multiple paths returned for standard user directory"))
-}
-
+/// Safer wrapper around the sysdir(3) API
 #[cfg(target_vendor = "apple")]
 mod sys {
-    pub use libc::sysdir_search_path_directory_t::*;
-    pub use libc::sysdir_search_path_domain_mask_t::*;
-    pub use libc::{
-        PATH_MAX, sysdir_get_next_search_path_enumeration, sysdir_search_path_directory_t,
-        sysdir_start_search_path_enumeration,
-    };
+    use crate::io::{self, ErrorKind, const_error};
+    use crate::path::{Path, PathBuf};
+
+    /// Get the path for a system directory using `sysdir(3)`.
+    pub fn get_user_dir(
+        home: &Path,
+        kind: libc::sysdir_search_path_directory_t,
+    ) -> io::Result<Option<PathBuf>> {
+        use libc::sysdir_search_path_domain_mask_t::SYSDIR_DOMAIN_MASK_USER;
+
+        // SAFETY: SYSDIR_DOMAIN_MASK_USER < SYSDIR_DOMAIN_MASK_ALL
+        let mut iter = unsafe { sys::Iter::new(home, kind, SYSDIR_DOMAIN_MASK_USER) };
+        let Some(path) = iter.next() else {
+            return Ok(None);
+        };
+
+        if iter.next().is_some() {
+            // more than one path returned (shouldn't happen for SYSDIR_DOMAIN_MASK_USER)
+            return Err(const_error!(
+                ErrorKind::InvalidData,
+                "multiple paths returned for standard user directory",
+            ));
+        }
+
+        Ok(Some(path?))
+    }
+
+    struct Iter<'a> {
+        home: &'a Path,
+        state: libc::sysdir_search_path_enumeration_state,
+    }
+
+    impl Drop for Iter<'_> {
+        fn drop(&mut self) {
+            for _ in self {}
+        }
+    }
+
+    impl<'a> Iter<'a> {
+        // SAFETY: `mask` must be <= `SYSDIR_DOMAIN_MASK_ALL`
+        pub unsafe fn new(
+            home: &'a Path,
+            kind: libc::sysdir_search_path_directory_t,
+            mask: libc::sysdir_search_path_domain_mask_t,
+        ) -> Self {
+            // SAFETY: forwarded to the caller
+            let state = unsafe { libc::sysdir_start_search_path_enumeration(kind, mask) };
+            Self { home, state }
+        }
+    }
+
+    impl Iterator for Iter<'_> {
+        type Item = io::Result<PathBuf>;
+
+        fn next(&mut self) -> Option<io::Result<PathBuf>> {
+            let mut buf = [0u8; libc::PATH_MAX as usize];
+            if self.state != 0 {
+                // SAFETY: `self.state` is nonzero and comes from prior sysdir_{start|get_next}_search_path_enumeration call
+                // SAFETY: sysdir_get_next_search_path_enumeration will write at most `PATH_MAX` bytes to `path`
+                self.state = unsafe {
+                    libc::sysdir_get_next_search_path_enumeration(
+                        self.state,
+                        buf.as_mut_ptr() as *mut libc::c_char,
+                    )
+                };
+            }
+
+            if self.state == 0 {
+                // exhausted
+                return None;
+            }
+
+            let Ok(path) = std::ffi::CStr::from_bytes_until_nul(&buf) else {
+                // should be impossible given username length limits, but be defensive
+                return Err(const_error!(
+                    ErrorKind::InvalidData,
+                    "standard user directory path too long",
+                ));
+            };
+
+            let Ok(path) = path.to_str() else {
+                // FIXME: is this possible, and if so, how should it be handled?
+                return Err(const_error!(
+                    ErrorKind::InvalidData,
+                    "standard user directory path not valid UTF-8",
+                ));
+            };
+
+            // expand `~` shorthand
+            Some(match path {
+                "~" => Ok(home.into()),
+                _ if path.starts_with("~/") => Ok(home.join(&path[2..])),
+                _ if path.starts_with("~") => Err(const_error!(
+                    ErrorKind::InvalidData,
+                    "standard user directory relative to different user",
+                )),
+                _ => {
+                    let path = PathBuf::from(path);
+                    if path.is_relative() {
+                        Err(const_error!(
+                            ErrorKind::InvalidData,
+                            "standard user directory path not absolute",
+                        ))
+                    } else {
+                        Ok(path)
+                    }
+                }
+            })
+        }
+    }
 }
 
 #[cfg(test)]
