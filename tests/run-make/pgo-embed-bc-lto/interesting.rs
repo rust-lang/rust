df6afdf60f37a0b552b764ccb7bf9ecae21039c7
Nikita Popov <npopov@redhat.com>
2026-07-14 14:29:17 +0200

    Adjust PGO tests for LLVM 23
    These now have additional !guid metadata.

--- tests/run-make/pgo-embed-bc-lto/interesting.rs
+++ tests/run-make/pgo-embed-bc-lto/interesting.rs
@@ -10,7 +10,7 @@ pub fn function_called_once() {
 }
 
 // CHECK-LABEL: @function_called_once
-// CHECK-SAME: !prof [[function_called_once_id:![0-9]+]] {
+// CHECK-SAME: !prof [[function_called_once_id:![0-9]+]]
 // CHECK: "CG Profile"
 // CHECK-NOT: "CG Profile"
 // CHECK-DAG: [[function_called_once_id]] = !{!"function_entry_count", i64 1}
