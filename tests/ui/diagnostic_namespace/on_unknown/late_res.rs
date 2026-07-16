b7a862c67814e04a09d93125ee46e127db5d0409
mejrs <59372212+mejrs@users.noreply.github.com>
2026-07-17 00:05:57 +0200

    Plug diagnostic::on_unknown into late resolution

--- tests/ui/diagnostic_namespace/on_unknown/late_res.rs
+++ tests/ui/diagnostic_namespace/on_unknown/late_res.rs
@@ -7,18 +7,18 @@ pub mod empty {}
 fn stuff(x: u32) {
     match x {
         empty::blah => {}
-        //~^ERROR cannot find unit struct, unit variant or constant `blah` in module `empty` [E0531]
+        //~^ERROR it works [E0531]
         _ => {}
     }
 
     println!("{}", empty::blah);
-    //~^ERROR cannot find value `blah` in module `empty` [E0425]
+    //~^ERROR it works [E0425]
 
     let x = [
         empty::blah,
-        //~^ERROR cannot find value `blah` in module `empty` [E0425]
+        //~^ERROR it works [E0425]
 
         empty::blah2,
-        //~^ERROR cannot find value `blah2` in module `empty` [E0425]
+        //~^ERROR it works [E0425]
     ];
 }
