3bf1942a236e6fcf7cacf8224c328080daea0d96
mejrs <59372212+mejrs@users.noreply.github.com>
2026-07-17 00:06:45 +0200

    Update late_res test

--- tests/ui/diagnostic_namespace/on_unknown/late_res.rs
+++ tests/ui/diagnostic_namespace/on_unknown/late_res.rs
@@ -1,24 +1,27 @@
 #![crate_type = "lib"]
 #![feature(diagnostic_on_unknown)]
 
-#[diagnostic::on_unknown(message = "it works")]
+#[diagnostic::on_unknown(
+    message = "it works `{This}` `{Unresolved}`",
+    label = "label it works",
+    note = "note it works"
+)]
 pub mod empty {}
 
 fn stuff(x: u32) {
     match x {
         empty::blah => {}
-        //~^ERROR it works [E0531]
+        //~^ERROR it works `empty` `blah` [E0531]
         _ => {}
     }
 
     println!("{}", empty::blah);
-    //~^ERROR it works [E0425]
+    //~^ERROR it works `empty` `blah` [E0425]
 
     let x = [
         empty::blah,
-        //~^ERROR it works [E0425]
-
+        //~^ERROR it works `empty` `blah` [E0425]
         empty::blah2,
-        //~^ERROR it works [E0425]
+        //~^ERROR it works `empty` `blah2` [E0425]
     ];
 }
