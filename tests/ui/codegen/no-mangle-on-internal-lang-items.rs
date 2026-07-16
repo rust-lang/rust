104c06c93cc133cddb04801d3f2a3296cb0c794e
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-07-16 16:21:10 +0200

    Bless several tests

--- tests/ui/codegen/no-mangle-on-internal-lang-items.rs
+++ tests/ui/codegen/no-mangle-on-internal-lang-items.rs
@@ -4,11 +4,7 @@
 #![feature(rustc_attrs)]
 
 #[rustc_std_internal_symbol]
-#[unsafe(no_mangle)] //~ERROR `#[no_mangle]` cannot be used on internal language items
-fn internal_lang_function () {
+#[unsafe(no_mangle)] //~ERROR `#[no_mangle]` and `#[export_name]` cannot be used on internal language items
+fn internal_lang_function() {}
 
-}
-
-fn main() {
-
-}
+fn main() {}
