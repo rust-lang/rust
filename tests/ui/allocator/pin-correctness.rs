c172f932fcfc5a8a349359eaae8b059e5ddec12c
Nia Deckers <me@nia.gay>
2026-07-17 19:43:37 +0200

    test for pin unsize coercion

---
+++ tests/ui/allocator/pin-correctness.rs
@@ -0,0 +1,30 @@
+#![feature(allocator_api)]
+
+use std::{
+    alloc::{AllocError, Allocator, Layout},
+    any::Any,
+    pin::Pin,
+    ptr::NonNull,
+};
+
+struct BadAlloc;
+unsafe impl Allocator for BadAlloc {
+    fn allocate(&self, _: Layout) -> Result<NonNull<[u8]>, AllocError> {
+        unimplemented!()
+    }
+    unsafe fn deallocate(&self, _: NonNull<u8>, _: Layout) {
+        unimplemented!()
+    }
+}
+
+#[expect(dead_code)]
+fn issue_159445_unsize_custom_alloc_box() {
+    // no requirements for the allocator, we are just pinning a `Box<impl Unpin>`
+    let base: Pin<Box<i32, BadAlloc>> = Pin::new(Box::new_in(1i32, BadAlloc));
+
+    // unsize coercion (must fail)
+    let _: Pin<Box<dyn Any, BadAlloc>> = base;
+    //~^ ERROR: the trait bound `BadAlloc: StaticAllocator` is not satisfied [E0277]
+}
+
+fn main() {}
