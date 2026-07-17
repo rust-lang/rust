4db4693ca84fcc89b32d05563d3285547d740f59
Ramon de C Valle <rcvalle@users.noreply.github.com>
2026-07-17 16:51:14 -0700

    Add the alloc_token_infer intrinsic
    Add the `alloc_token_infer` intrinsic (i.e., the Rust equivalent of
    the Clang `__builtin_infer_alloc_token` builtin, lowered to the
    `llvm.alloc.token.id` intrinsic) for allocator and arena implementers
    to query the token identifier for a given type at compile time (e.g.,
    `core::intrinsics::alloc_token_infer::<T>() -> usize`), so it can be
    passed to a token-enabled allocator interface directly.

--- compiler/rustc_hir_analysis/src/check/intrinsic.rs
+++ compiler/rustc_hir_analysis/src/check/intrinsic.rs
@@ -69,6 +69,7 @@ fn intrinsic_operation_unsafety(tcx: TyCtxt<'_>, intrinsic_id: LocalDefId) -> hi
         | sym::add_with_overflow
         | sym::aggregate_raw_ptr
         | sym::align_of
+        | sym::alloc_token_infer
         | sym::amdgpu_dispatch_ptr
         | sym::assert_inhabited
         | sym::assert_mem_uninitialized_valid
@@ -301,7 +302,9 @@ pub(crate) fn check_intrinsic_type(
         sym::amdgpu_dispatch_ptr => (0, 0, vec![], Ty::new_imm_ptr(tcx, tcx.types.unit)),
         sym::unreachable => (0, 0, vec![], tcx.types.never),
         sym::breakpoint => (0, 0, vec![], tcx.types.unit),
-        sym::size_of | sym::align_of | sym::variant_count => (1, 0, vec![], tcx.types.usize),
+        sym::size_of | sym::align_of | sym::variant_count | sym::alloc_token_infer => {
+            (1, 0, vec![], tcx.types.usize)
+        }
         sym::size_of_val | sym::align_of_val => {
             (1, 0, vec![Ty::new_imm_ptr(tcx, param(0))], tcx.types.usize)
         }
