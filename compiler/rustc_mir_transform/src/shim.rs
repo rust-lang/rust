e176864ce54248b7f483f63bcdc48c16af779a5d
Ralf Jung <post@ralfj.de>
2026-07-17 09:49:11 +0200

    fix grammar

--- compiler/rustc_mir_transform/src/shim.rs
+++ compiler/rustc_mir_transform/src/shim.rs
@@ -1074,7 +1074,7 @@ pub(super) fn build_adt_ctor(tcx: TyCtxt<'_>, ctor_id: DefId) -> Body<'_> {
     // so this would otherwise not get filled).
     body.set_mentioned_items(Vec::new());
 
-    // We don't pass any passes here, just to force a phase change to `Optimized`.
+    // We don't pass any passes here, we just force a phase change to `Optimized`.
     // Otherwise this bit of MIR will trigger assertions trying to detect MIR with an invalid phase.
     pm::run_passes_no_validate(
         tcx,
