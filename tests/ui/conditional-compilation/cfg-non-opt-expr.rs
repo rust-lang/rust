832b092a6ab662d37052ef282623cd0637f3f329
Usman Akinyemi <usmanakinyemi202@gmail.com>
2026-07-19 03:44:53 +0530

    Reject cfg on expressions that cannot be safely removed
    Previously, expressions behind stmt_expr_attributes were only rejected
    when the cfg condition evaluated to false. If the condition was true,
    the code compiled successfully.
    
    This meant code like an attributed binary operand could compile on one
    platform but fail on another, even though removing the operand would leave
    an invalid expression.
    
    This change always rejects cfg in expression positions where removing the
    expression would produce invalid code. Expression positions where removal is
    safe continue to work as before.
    
    Signed-off-by: Usman Akinyemi <usmanakinyemi202@gmail.com>

--- tests/ui/conditional-compilation/cfg-non-opt-expr.rs
+++ tests/ui/conditional-compilation/cfg-non-opt-expr.rs
@@ -1,11 +1,38 @@
 #![feature(stmt_expr_attributes)]
 #![feature(custom_test_frameworks)]
 
+#[derive(Clone)]
+enum E {
+    V1 = #[cfg(true)] 1,
+    //~^ ERROR removing an expression is not supported in this position
+    //~| ERROR removing an expression is not supported in this position
+    V2 = #[cfg_attr(true, cfg(true))] 2,
+    //~^ ERROR removing an expression is not supported in this position
+}
+
+macro_rules! mac {
+    ($expr:expr) => { $expr.clone() }
+}
+
 fn main() {
+    let _ = 1 + #[cfg(unix)] 2;
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = 1 + #[cfg(windows)] 2;
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = 1 + #[cfg(all())] 2;
+    //~^ ERROR removing an expression is not supported in this position
     let _ = #[cfg(false)] ();
     //~^ ERROR removing an expression is not supported in this position
     let _ = 1 + 2 + #[cfg(false)] 3;
     //~^ ERROR removing an expression is not supported in this position
     let _ = [1, 2, 3][#[cfg(false)] 1];
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = mac!(#[cfg(true)] 10);
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = #[cfg(true)] ();
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = 1 + 2 + #[cfg(true)] 3;
+    //~^ ERROR removing an expression is not supported in this position
+    let _ = [1, 2, 3][#[cfg(true)] 1];
     //~^ ERROR removing an expression is not supported in this position
 }
