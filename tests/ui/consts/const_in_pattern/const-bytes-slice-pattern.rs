3b999caee652bc1c9c8c9fc7f59eb925fd78f3f5
zedddie <zedddie@protonmail.com>
2026-07-20 01:59:14 +0200

    bless batch

--- tests/ui/consts/const_in_pattern/const-bytes-slice-pattern.rs
+++ tests/ui/consts/const_in_pattern/const-bytes-slice-pattern.rs
@@ -1,4 +1,7 @@
+//! Regression test for <https://github.com/rust-lang/rust/issues/51655>.
+//! This used to ICE.
 //@ check-pass
+
 #![allow(dead_code)]
 
 const PATH_DOT: &[u8] = &[b'.'];
--- tests/ui/structs-enums/call-tuple-struct-ctor-as-fn.rs
+++ tests/ui/structs-enums/call-tuple-struct-ctor-as-fn.rs
@@ -1,3 +1,5 @@
+//! Regression test for <https://github.com/rust-lang/rust/issues/5315>.
+//! Test calling tuple struct constructor doesn't cause segfault.
 //@ run-pass
 
 struct A(#[allow(dead_code)] bool);
