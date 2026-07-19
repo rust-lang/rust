44d9de29f242057908355ffaa3ca56095bd7fea5
Zac Harrold <zac@harrold.com.au>
2026-07-19 20:42:02 +1000

    Reduce API surface of `alloc::io` reexports from `core::io`

--- library/alloc/src/io/mod.rs
+++ library/alloc/src/io/mod.rs
@@ -23,10 +23,10 @@ pub use core::io::{
 };
 #[doc(hidden)]
 #[unstable(feature = "core_io_internals", reason = "exposed only for libstd", issue = "none")]
-pub use core::io::{
-    IoHandle, OsFunctions, SizeHint, WriteThroughCursor, chain, default_write_vectored,
-    slice_write, slice_write_all, slice_write_all_vectored, slice_write_vectored,
-    stream_len_default, take,
+pub use core::io::{IoHandle, OsFunctions, default_write_vectored, stream_len_default};
+use core::io::{
+    SizeHint, WriteThroughCursor, chain, slice_write, slice_write_all, slice_write_all_vectored,
+    slice_write_vectored, take,
 };
 
 use self::read::{append_to_string, default_read_buf_exact, default_read_exact};
