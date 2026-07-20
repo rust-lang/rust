30bc6d2f06c4641d10f44aae1f783d6e63c10e1c
Andrew Ghostuhin <me@torinasakura.name>
2026-07-21 01:03:16 +0300

    fix: improve missing value diagnostics for built-in attributes

--- tests/ui/const-generics/mgca/adt_expr_erroneuous_inits.rs
+++ tests/ui/const-generics/mgca/adt_expr_erroneuous_inits.rs
@@ -21,6 +21,6 @@ fn bar() {
     //~^ ERROR: cannot find struct, variant or union type `Fooo` in this scope
     //~| ERROR: struct expression with invalid base path
     accepts::<{ NonStruct { }}>();
-    //~^ ERROR: expected struct, variant or union type, found function `NonStruct`
+    //~^ ERROR: cannot find struct, variant or union type `NonStruct` in this scope
     //~| ERROR: struct expression with invalid base path
 }
