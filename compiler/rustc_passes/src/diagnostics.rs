147c13a5ac6be03507010f66d5233136173cda0a
mejrs <59372212+mejrs@users.noreply.github.com>
2026-07-20 16:50:53 +0200

    Move extern static mut linkage check to check_attr

--- compiler/rustc_passes/src/diagnostics.rs
+++ compiler/rustc_passes/src/diagnostics.rs
@@ -1190,3 +1190,15 @@ pub(crate) struct OnTypeErrorMalformedFormatLiterals {
 pub(crate) struct OnTypeErrorNotExactlyOneGeneric {
     pub count: usize,
 }
+
+#[derive(Diagnostic)]
+#[diag("extern mutable statics are incompatible with `#[linkage]`")]
+#[note(
+    "marking the extern static mutable would allow changing which \
+    symbol the static references rather than make the target of the \
+    symbol mutable"
+)]
+pub(crate) struct StaticMutLinkage {
+    #[primary_span]
+    pub span: Span,
+}
