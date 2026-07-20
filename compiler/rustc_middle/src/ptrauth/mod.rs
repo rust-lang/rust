4613587c5822545f81f0a5256f843f6d08f0f32f
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-07-20 11:44:28 +0000

    [PAC] Add unit tests for sip hash

--- compiler/rustc_middle/src/ptrauth/mod.rs
+++ compiler/rustc_middle/src/ptrauth/mod.rs
@@ -2,6 +2,6 @@ pub mod discriminator;
 pub mod llvm_siphash;
 
 pub use discriminator::{
-    FnPtrDiscriminatorSource, FnPtrTypeDiscriminatorInput, clone_discriminated_ptrauth_schema_for,
-    compute_fn_ptr_type_discriminator_for,
+    FnPtrDiscriminatorSource, FnPtrTypeDiscriminatorInput, ptrauth_clone_discriminated_schema_for,
+    ptrauth_compute_fn_ptr_type_discriminator_for,
 };
