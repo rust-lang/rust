2bb8a3b44ab94616e7757364c8e567f351e72de1
sgasho <SuganoShota1999@gmail.com>
2026-07-21 22:23:34 +0900

    Delete autodiff.sh and move the test command to the Dockerfile

--- src/ci/docker/scripts/autodiff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-../x.py test --stage 2 --no-fail-fast \
-    tests/codegen-llvm/autodiff \
-    tests/pretty/autodiff \
-    tests/ui/autodiff \
-    tests/ui/feature-gates/feature-gate-autodiff.rs
