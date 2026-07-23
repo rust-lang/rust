afb2e9cb0e58a8b46030dcd4d856e607afec7813
Folkert de Vries <folkert@folkertdev.nl>
2026-07-23 12:13:38 +0200

    use `minicore::simd::Simd` more

--- tests/ui/codegen/deprecated-llvm-intrinsic.rs
+++ tests/ui/codegen/deprecated-llvm-intrinsic.rs
@@ -3,17 +3,15 @@
 //@ compile-flags: --target aarch64-unknown-linux-gnu
 //@ needs-llvm-components: aarch64
 //@ ignore-backends: gcc
-#![feature(no_core, lang_items, link_llvm_intrinsics, abi_unadjusted, repr_simd, simd_ffi)]
+#![feature(no_core, lang_items, link_llvm_intrinsics, abi_unadjusted, simd_ffi)]
 #![no_std]
 #![no_core]
 #![allow(internal_features, non_camel_case_types, improper_ctypes)]
 #![crate_type = "lib"]
 
 extern crate minicore;
+use minicore::simd::i8x8;
 use minicore::*;
-
-#[repr(simd)]
-pub struct i8x8([i8; 8]);
 
 extern "unadjusted" {
     #[deny(deprecated_llvm_intrinsic)]
