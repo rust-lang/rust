c9ef9028783f08a567893c9897e60ffab1bda739
Ralf Jung <post@ralfj.de>
2026-07-24 15:19:22 +0200

    line-tables-only test: check that the line number matches the function name

--- tests/ui/backtrace/auxiliary/line-tables-only-helper.rs
+++ tests/ui/backtrace/auxiliary/line-tables-only-helper.rs
@@ -1,22 +1,22 @@
 //@ compile-flags: -Cstrip=none -Cdebuginfo=line-tables-only
 
 #[no_mangle]
-pub fn baz<F>(mut cb: F, data: u32) where F: FnMut(u32) {
+pub fn backtrace_with_baz_in_it<F>(mut cb: F, data: u32) where F: FnMut(u32) {
     cb(data);
 }
 
 #[no_mangle]
-pub fn bar<F>(cb: F, data: u32) where F: FnMut(u32) {
-    baz(cb, data);
+pub fn backtrace_with_bar_in_it<F>(cb: F, data: u32) where F: FnMut(u32) {
+    backtrace_with_baz_in_it(cb, data);
 }
 
 #[no_mangle]
-pub fn foo<F>(cb: F, data: u32) where F: FnMut(u32) {
-    bar(cb, data);
+pub fn backtrace_with_foo_in_it<F>(cb: F, data: u32) where F: FnMut(u32) {
+    backtrace_with_bar_in_it(cb, data);
 }
 
 pub fn capture_backtrace() -> std::backtrace::Backtrace {
     let mut bt = None;
-    foo(|_| bt = Some(std::backtrace::Backtrace::capture()), 42);
+    backtrace_with_foo_in_it(|_| bt = Some(std::backtrace::Backtrace::capture()), 42);
     bt.unwrap()
 }
