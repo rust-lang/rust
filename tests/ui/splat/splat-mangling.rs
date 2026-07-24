fbab3a660fb22122fb3fce9eb9ae5303cb822422
teor <teor@riseup.net>
2026-07-24 16:46:42 +1000

    Fix legacy splat symbol lengths for rename

--- tests/ui/splat/splat-mangling.rs
+++ tests/ui/splat/splat-mangling.rs
@@ -20,7 +20,7 @@ fn main() {
     #[rustfmt::skip]
     // FIXME(splat, legacy mangling): the first comma is in the wrong place
     #[rustc_dump_symbol_name]
-           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main66Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u8$C$u32$RP$$RP$$GT
+           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main70Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u8$C$u32$RP$$RP$$GT
            //[legacy]~| ERROR demangling(splat_mangling::main::Type<fn(, #[arg_splat](u8,u32))>::
            //[legacy]~| ERROR demangling-alt(splat_mangling::main::Type<fn(, #[arg_splat](u8,u32))>)
     //[v0,default]~^^^^ ERROR symbol-name(_RMNvCsCRATE_HASH_14splat_mangling4mainINtB<REF>_4TypeFwThmEEuE)
@@ -40,7 +40,7 @@ fn main() {
 
     #[rustfmt::skip]
     #[rustc_dump_symbol_name]
-           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main77Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$$LP$u8$C$u32$RP$$C$$RP$$RP$$GT
+           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main81Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$$LP$u8$C$u32$RP$$C$$RP$$RP$$GT
            //[legacy]~| ERROR demangling(splat_mangling::main::Type<fn(, #[arg_splat]((u8,u32),))>::
            //[legacy]~| ERROR demangling-alt(splat_mangling::main::Type<fn(, #[arg_splat]((u8,u32),))>)
     //[v0,default]~^^^^ ERROR symbol-name(_RMs0_NvCsCRATE_HASH_14splat_mangling4mainINtB<REF>_4TypeFwTThmEEEuE)
@@ -60,7 +60,7 @@ fn main() {
 
     #[rustfmt::skip]
     #[rustc_dump_symbol_name]
-           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main80Type$LT$$BP$const$u20$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u32$C$i8$RP$$RP$$GT
+           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main84Type$LT$$BP$const$u20$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u32$C$i8$RP$$RP$$GT
            //[legacy]~| ERROR demangling(splat_mangling::main::Type<*const fn(, #[arg_splat](u32,i8))>::
            //[legacy]~| ERROR demangling-alt(splat_mangling::main::Type<*const fn(, #[arg_splat](u32,i8))>)
     //[v0,default]~^^^^ ERROR symbol-name(_RMs2_NvCsCRATE_HASH_14splat_mangling4mainINtB<REF>_4TypePFwTmaEEuE)
@@ -80,7 +80,7 @@ fn main() {
 
     #[rustfmt::skip]
     #[rustc_dump_symbol_name]
-           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main72Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u32$C$i8$RP$$C$f64$RP$$GT
+           //[legacy]~^ ERROR symbol-name(_ZN14splat_mangling4main76Type$LT$fn$LP$$C$$u20$$u23$$u5b$arg_splat$u5d$$LP$u32$C$i8$RP$$C$f64$RP$$GT
            //[legacy]~| ERROR demangling(splat_mangling::main::Type<fn(, #[arg_splat](u32,i8),f64)>::
            //[legacy]~| ERROR demangling-alt(splat_mangling::main::Type<fn(, #[arg_splat](u32,i8),f64)>)
     //[v0,default]~^^^^ ERROR symbol-name(_RMs4_NvCsCRATE_HASH_14splat_mangling4mainINtB<REF>_4TypeFwTmaEdEuE)
