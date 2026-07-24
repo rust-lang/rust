60dac3d5f9246bc69266bd330c31f81ffb0c2cfa
Joao Roberto <dnreikronos2@gmail.com>
2026-07-24 10:43:01 -0300

    Keep file module token recovery minimal

--- tests/ui/proc-macro/auxiliary/inner_attr_file_mod_cfg_attr_child.rs
+++
@@ -1,8 +0,0 @@
-#![cfg_attr(enabled, test_macros::identity_attr, allow(dead_code))]
-#![cfg_attr(enabled, allow(unused_imports), test_macros::identity_attr)]
-#![cfg_attr(enabled, test_macros::identity_attr)]
-//! docs
-
-use std::fmt;
-
-fn unused() {}
