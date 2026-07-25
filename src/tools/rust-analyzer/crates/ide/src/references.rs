70847a5bc39e70decb27ddf526375d6febe61d54
Chayim Refael Friedman <chayimfr@gmail.com>
2026-07-25 23:33:29 +0300

    Remove format_args lowering for toolchains prior to 1.94.0
    We don't support them now since https://github.com/rust-lang/rust-analyzer/pull/22784.

--- src/tools/rust-analyzer/crates/ide/src/references.rs
+++ src/tools/rust-analyzer/crates/ide/src/references.rs
@@ -600,7 +600,7 @@ fn main() {
             false,
             false,
             expect![[r#"
-                Some Variant FileId(1) 6737..6769 6762..6766
+                Some Variant FileId(1) 6734..6766 6759..6763
 
                 FileId(0) 46..50
             "#]],
