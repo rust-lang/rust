2191fc155571ae14ceeb6ba685b29211e7ef8cb4
bit-aloo <sshourya17@gmail.com>
2026-07-25 06:23:27 +0000

    Revise generic const args WF test and split GAT case

--- tests/ui/type-alias/lack-of-wfcheck-generic-const-args.rs
+++ tests/ui/type-alias/lack-of-wfcheck-generic-const-args.rs
@@ -1,10 +1,12 @@
 // Demonstrate that generic_const_args changes the behavior for dyn trait aliases
 // with non-type associated consts: the associated const must be specified.
 
+//@ revisions: no_gca gca
 //@ compile-flags: -Znext-solver=globally
+//@ [no_gca] check-pass
 
-#![feature(generic_const_args, min_generic_const_args)]
-#![expect(incomplete_features)]
+#![cfg_attr(gca, feature(generic_const_args, min_generic_const_args))]
+#![cfg_attr(gca, expect(incomplete_features))]
 
 type UnsatTraitBound0 = [str]; // `str: Sized` unsatisfied
 type UnsatTraitBound1<T = Vec<str>> = T; // `str: Sized` unsatisfied
@@ -15,17 +17,10 @@ type Diverging = [(); panic!()]; // `panic!()` diverging
 type DynIncompat0 = dyn Sized; // `Sized` axiomatically dyn incompatible
 // issue: <https://github.com/rust-lang/rust/issues/153731>
 type DynIncompat1 = dyn HasAssocConst;
-//~^ ERROR the value of the associated constant `N` in `HasAssocConst` must be specified
-
-// * dyn incompatible due to GAT
-// * `'a: 'static`, `String: Copy` and `[u8]: Sized` unsatisfied, `loop {}` diverging
-type Several<'a> = dyn HasGenericAssocType<Type<'a, String, { loop {} }> = [u8]>;
+//[gca]~^ ERROR the value of the associated constant `N` in `HasAssocConst` must be specified
 
 trait HasAssocConst {
     const N: usize;
-}
-trait HasGenericAssocType {
-    type Type<'a: 'static, T: Copy, const N: usize>;
 }
 
 fn main() {}
