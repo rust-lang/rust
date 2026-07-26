69f4f98865d0bfd7a3823205cfb68c822f68400e
Folkert de Vries <folkert@folkertdev.nl>
2026-07-26 20:55:58 +0200

    make powerpc `Complex` ABI GCC-compatible

--- compiler/rustc_target/src/callconv/powerpc64.rs
+++ compiler/rustc_target/src/callconv/powerpc64.rs
@@ -4,7 +4,7 @@
 
 use rustc_abi::{HasDataLayout, TyAbiInterface};
 
-use crate::callconv::{Align, ArgAbi, FnAbi, Reg, RegKind, Uniform};
+use crate::callconv::{Align, ArgAbi, CastTarget, FnAbi, Reg, RegKind, Uniform};
 use crate::spec::{HasTargetSpec, LlvmAbi, Os};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -58,6 +58,13 @@ where
     }
     if !arg.layout.is_aggregate() {
         arg.extend_integer_width_to(64);
+        return;
+    }
+    if let Some(component) = arg.layout.complex_number(cx) {
+        if is_ret {
+            let reg = Reg { kind: component.reg_kind(), size: component.size() };
+            arg.cast_to(CastTarget::pair(reg, reg));
+        }
         return;
     }
 
