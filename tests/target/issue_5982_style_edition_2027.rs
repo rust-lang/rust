fbdafded756094d21e425253ba62bfe8d475620f
Jan Koscisz <koscis.j@gmail.com>
2026-07-27 15:52:04 +0200

    split tests

--- tests/target/issue_5982_style_edition_2027.rs
+++ tests/target/issue_5982_style_edition_2027.rs
@@ -1,8 +1,5 @@
 // rustfmt-style_edition: 2027
-// See https://github.com/rust-lang/rustfmt/issues/5982 and
-// https://github.com/rust-lang/rustfmt/issues/5920
 
-// https://github.com/rust-lang/rustfmt/issues/5982
 struct Foo {
     #[cfg(all())]
     something_long_enough_to_wrap_foo: i32,
@@ -38,30 +35,5 @@ fn example_with_comments() {
         something_long_enough_to_wrap_foo: 111,
         #[cfg(all())]
         something_long_enough_to_wrap_bar: 222,
-    };
-}
-
-// https://github.com/rust-lang/rustfmt/issues/5920
-struct Demo {
-    field_name_foo: (),
-    field_name_bar: (),
-    #[cfg(feature = "diagnostics")]
-    field_name_baz: (),
-    field_name_tux: (),
-}
-
-fn main() {
-    let Demo {
-        field_name_foo,
-        #[cfg(feature = "diagnostics")]
-        field_name_baz: _,
-        field_name_bar,
-        field_name_tux,
-    } = Demo {
-        field_name_foo: (),
-        field_name_bar: (),
-        #[cfg(feature = "diagnostics")]
-        field_name_baz: (),
-        field_name_tux: (),
     };
 }
