4c61167ed3d15ff21484b629e2988b5fa0740a5b
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2026-07-27 13:32:09 +0330

    tests: use explicit dyn trait object
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- tests/ui/traits/next-solver/lower-universe-resolved-const-issue-159703.rs
+++ tests/ui/traits/next-solver/lower-universe-resolved-const-issue-159703.rs
@@ -1,7 +1,4 @@
 //@ compile-flags: -Znext-solver=globally
-//@ edition: 2015
-
-#![allow(bare_trait_objects)]
 
 trait Foo {}
 
@@ -10,7 +7,7 @@ struct BarType<const N: usize>;
 impl<const N: usize> Foo for BarType {}
 //~^ ERROR missing generics for struct `BarType`
 
-fn a(x: &Foo) {
+fn a(x: &dyn Foo) {
     let bar = BarType;
     a(bar);
     //~^ ERROR mismatched types
