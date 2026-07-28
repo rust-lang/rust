4aadc68a8a49ea50ca3970d784e69d1dea3a40f7
Nicholas Nethercote <n.nethercote@gmail.com>
2026-07-29 09:56:00 +1000

    Improve CFG traversal in `iterate_to_fixpoint`
    `iterate_to_fixpoint` uses a FIFO worklist. It is seeded in a good order
    (reverse postorder for forward analyses, postorder for backward
    analyses). But when back edges are present, re-dirtied blocks are pushed
    to the back of the queue. Successor blocks of these re-dirtied blocks are
    then analyzed even though we know the input states may change later on.
    
    This commit switches to a new min-rank algorithm that always processes
    the earliest dirty block in dataflow order.
    
    On most code this makes little difference, but cranelift-codegen has one
    enormous function with over 18,000 basic blocks. The old algorithm
    required 1.5 million calls to `apply_effects_in_block` to reach a
    fixpoint for `EverInitializedPlaces`. The new algorithm requires 90,000.
    This results in a 17% reduction in instruction counts for a `Check Full`
    build.
    
    Note: the new algorithm also avoids doing anything for unreachable
    blocks in backward analyses. (The old algorithm computed states but
    never used them meaningfully.)

--- compiler/rustc_mir_dataflow/src/framework/mod.rs
+++ compiler/rustc_mir_dataflow/src/framework/mod.rs
@@ -32,12 +32,11 @@
 //!
 //! [gen-kill]: https://en.wikipedia.org/wiki/Data-flow_analysis#Bit_vector_problems
 
-use rustc_data_structures::work_queue::WorkQueue;
 use rustc_index::bit_set::{DenseBitSet, MixedBitSet};
 use rustc_index::{Idx, IndexVec};
 use rustc_middle::bug;
 use rustc_middle::mir::{
-    self, BasicBlock, BasicBlockData, CallReturnPlaces, Location, TerminatorEdges, traversal,
+    self, BasicBlock, BasicBlockData, CallReturnPlaces, Location, TerminatorEdges,
 };
 use rustc_middle::ty::TyCtxt;
 use tracing::error;
@@ -294,42 +293,81 @@ pub trait Analysis<'tcx> {
             bug!("`initialize_start_block` is not yet supported for backward dataflow analyses");
         }
 
-        let mut dirty_queue: WorkQueue<BasicBlock> = WorkQueue::with_none(body.basic_blocks.len());
+        // Forward analyses use a reverse postorder (`rpo`). Every reachable basic block has a
+        // *rank*: its position within `rpo`. Rank order is dataflow order: for every edge A -> B
+        // that is not a back edge, rank(A) < rank(B). This is independent of basic block numbering
+        // (which depends on the vagaries of CFG construction).
+        //
+        // The CFG traversal uses a "min-rank" algorithm. First, all reachable basic blocks are
+        // marked as dirty. The loop-head invariant is that `curr_rank` always points to the
+        // minimum-rank dirty block in `rpo`. Before processing that block we mark it as clean. If
+        // the processing dirties a block with a rank lower than or equal to `curr_rank` (via a
+        // back edge, which could be an edge-to-self) then `curr_rank` is set to that
+        // lower-or-equal rank. After the block is processed, if `curr_rank` doesn't point to a
+        // dirty block it is moved to the next dirty block, and we iterate again.
+        //
+        // This algorithm ensures each basic block is processed only after all its dirty
+        // predecessors (ignoring back edges). When a back edge dirties an earlier block we return
+        // to that earlier block immediately, which avoids processing later blocks with possibly
+        // soon-to-be-stale information. Loop-free code is processed in a single pass.
+        //
+        // Backward analyses: we want a postorder instead of a reverse postorder, but we also want
+        // to avoid the cost of adding a `postorder` field to `mir::basic_blocks::Cache`. We can
+        // fake a postorder traversal cheaply by using a reverse postorder and flipping the rank
+        // mapping. There is also one wrinkle involving unreachable blocks; see below.
 
-        if Self::Direction::IS_FORWARD {
-            for (bb, _) in traversal::reverse_postorder(body) {
-                dirty_queue.insert(bb);
-            }
-        } else {
-            // Reverse post-order on the reverse CFG may generate a better iteration order for
-            // backward dataflow analyses, but probably not enough to matter.
-            for (bb, _) in traversal::postorder(body) {
-                dirty_queue.insert(bb);
-            }
+        rustc_index::newtype_index! {
+            #[orderable]
+            #[debug_format = "bbr{}"]
+            struct BasicBlockRank {}
         }
 
-        // `state` is not actually used between iterations;
-        // this is just an optimization to avoid reallocating
-        // every iteration.
-        let mut state = self.bottom_value(body);
-        while let Some(bb) = dirty_queue.pop() {
-            // Set the state to the entry state of the block. This is equivalent to `state =
-            // entry_states[bb].clone()`, but it saves an allocation, thus improving compile times.
-            state.clone_from(&entry_states[bb]);
+        let rpo: &[BasicBlock] = body.basic_blocks.reverse_postorder();
+        let last = rpo.len() - 1;
 
-            Self::Direction::apply_effects_in_block(
-                &self,
-                body,
-                &mut state,
-                bb,
-                &body[bb],
-                |target: BasicBlock, state: &Self::Domain| {
-                    let set_changed = entry_states[target].join(state);
-                    if set_changed {
-                        dirty_queue.insert(target);
-                    }
-                },
-            );
+        let mut ranks: IndexVec<BasicBlock, Option<BasicBlockRank>> =
+            IndexVec::from_elem_n(None, body.basic_blocks.len());
+        for (i, &bb) in rpo.iter().enumerate() {
+            let rank = if Self::Direction::IS_FORWARD { i } else { last - i };
+            ranks[bb] = Some(BasicBlockRank::new(rank));
+        }
+
+        let mut dirty: DenseBitSet<BasicBlockRank> = DenseBitSet::new_filled(rpo.len());
+        let mut curr_rank = BasicBlockRank::ZERO;
+
+        // `state` is not actually used between iterations; this is just an optimization to avoid
+        // reallocating every iteration.
+        let mut state = self.bottom_value(body);
+
+        loop {
+            let i = curr_rank.as_usize();
+            let bb = rpo[if Self::Direction::IS_FORWARD { i } else { last - i }];
+            debug_assert!(dirty.contains(curr_rank)); // check invariant
+            dirty.remove(curr_rank); // invariant temporarily broken
+
+            state.clone_from(&entry_states[bb]);
+            let prop = |target: BasicBlock, state: &Self::Domain| {
+                // A backward analysis may encounter an unreachable block, because a predecessor
+                // of a reachable block may be unreachable. Ignore any such block. (In contrast, in
+                // a forward analysis any successor of a reachable block must be reachable.)
+                let target_rank = ranks[target];
+                if Self::Direction::IS_BACKWARD && target_rank.is_none() {
+                    return;
+                }
+                let target_rank = target_rank.unwrap();
+
+                let set_changed = entry_states[target].join(state);
+                if set_changed {
+                    dirty.insert(target_rank);
+                    curr_rank = curr_rank.min(target_rank);
+                }
+            };
+            Self::Direction::apply_effects_in_block(&self, body, &mut state, bb, &body[bb], prop);
+
+            match dirty.first_set_at_or_after(curr_rank) {
+                Some(rank) => curr_rank = rank, // broken invariant re-established
+                None => break,                  // no more dirty blocks; finish
+            }
         }
 
         let results = Results { analysis: self, entry_states };
