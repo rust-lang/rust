jrakibi/miri

add aarch64 SHA256 intrinsics
Implement shims for vsha256hq_u32, vsha256h2q_u32, vsha256su0q_u32 and vsha256su1q_u32

src/shims/aarch64.rs
@@ -3,7 +3,7 @@ use std::assert_matches;
 use rustc_middle::mir::BinOp;
 use rustc_span::Symbol;
 
-use crate::shims::math::compute_crc32;
+use crate::shims::math::{compute_crc32, sha256};
 use crate::*;
 
 impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
@@ -273,8 +273,163 @@ pub(super) trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                 this.write_scalar(Scalar::from_u128(result), &dest)?;
             }
 
+            // Used to implement the vsha256hq_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256hq_u32
+            "crypto.sha256h" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [abcd, efgh, wk] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (abcd, abcd_len) = this.project_to_simd(abcd)?;
+                let (efgh, efgh_len) = this.project_to_simd(efgh)?;
+                let (wk, wk_len) = this.project_to_simd(wk)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(abcd_len, 4);
+                assert_eq!(efgh_len, 4);
+                assert_eq!(wk_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let abcd: [u32; 4] = read_u32x4(this, &abcd)?;
+                let efgh: [u32; 4] = read_u32x4(this, &efgh)?;
+                let wk: [u32; 4] = read_u32x4(this, &wk)?;
+
+                let result = sha256h(abcd, efgh, wk);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256h2q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256h2q_u32
+            "crypto.sha256h2" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [efgh, abcd, wk] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (efgh, efgh_len) = this.project_to_simd(efgh)?;
+                let (abcd, abcd_len) = this.project_to_simd(abcd)?;
+                let (wk, wk_len) = this.project_to_simd(wk)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(efgh_len, 4);
+                assert_eq!(abcd_len, 4);
+                assert_eq!(wk_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let efgh: [u32; 4] = read_u32x4(this, &efgh)?;
+                let abcd: [u32; 4] = read_u32x4(this, &abcd)?;
+                let wk: [u32; 4] = read_u32x4(this, &wk)?;
+
+                let result = sha256h2(efgh, abcd, wk);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256su0q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256su0q_u32
+            "crypto.sha256su0" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [a, b] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (a, a_len) = this.project_to_simd(a)?;
+                let (b, b_len) = this.project_to_simd(b)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(a_len, 4);
+                assert_eq!(b_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let a: [u32; 4] = read_u32x4(this, &a)?;
+                let b: [u32; 4] = read_u32x4(this, &b)?;
+
+                let result = sha256su0(a, b);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256su1q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256su1q_u32
+            "crypto.sha256su1" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [a, b, c] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (a, a_len) = this.project_to_simd(a)?;
+                let (b, b_len) = this.project_to_simd(b)?;
+                let (c, c_len) = this.project_to_simd(c)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(a_len, 4);
+                assert_eq!(b_len, 4);
+                assert_eq!(c_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let a: [u32; 4] = read_u32x4(this, &a)?;
+                let b: [u32; 4] = read_u32x4(this, &b)?;
+                let c: [u32; 4] = read_u32x4(this, &c)?;
+
+                let result = sha256su1(a, b, c);
+
+                write_u32x4(this, &dest, result)?;
+            }
             _ => return interp_ok(EmulateItemResult::NotSupported),
         }
         interp_ok(EmulateItemResult::NeedsReturn)
     }
+}
+
+/// Reads a `[u32; 4]` array.
+fn read_u32x4<'c>(ecx: &mut MiriInterpCx<'c>, vec: &OpTy<'c>) -> InterpResult<'c, [u32; 4]> {
+    let mut res = [0; 4];
+    for (i, dst) in res.iter_mut().enumerate() {
+        let projected = &ecx.project_index(vec, i.try_into().unwrap())?;
+        *dst = ecx.read_scalar(projected)?.to_u32()?;
+    }
+    interp_ok(res)
+}
+
+fn write_u32x4<'c>(
+    ecx: &mut MiriInterpCx<'c>,
+    dest: &MPlaceTy<'c>,
+    val: [u32; 4],
+) -> InterpResult<'c, ()> {
+    for (i, part) in val.into_iter().enumerate() {
+        let projected = &ecx.project_index(dest, i.to_u64())?;
+        ecx.write_scalar(Scalar::from_u32(part), projected)?;
+    }
+    interp_ok(())
+}
+
+fn sha256su0(v0: [u32; 4], v1: [u32; 4]) -> [u32; 4] {
+    [
+        v0[0].wrapping_add(sha256::sigma0(v0[1])),
+        v0[1].wrapping_add(sha256::sigma0(v0[2])),
+        v0[2].wrapping_add(sha256::sigma0(v0[3])),
+        v0[3].wrapping_add(sha256::sigma0(v1[0])),
+    ]
+}
+
+fn sha256su1(v0: [u32; 4], v1: [u32; 4], v2: [u32; 4]) -> [u32; 4] {
+    let r0 = v0[0].wrapping_add(v1[1]).wrapping_add(sha256::sigma1(v2[2]));
+    let r1 = v0[1].wrapping_add(v1[2]).wrapping_add(sha256::sigma1(v2[3]));
+    let r2 = v0[2].wrapping_add(v1[3]).wrapping_add(sha256::sigma1(r0));
+    let r3 = v0[3].wrapping_add(v2[0]).wrapping_add(sha256::sigma1(r1));
+    [r0, r1, r2, r3]
+}
+
+// SHA256H/SHA256H2 do four compression rounds on the abcd/efgh layout.
+// https://developer.arm.com/architectures/instruction-sets/intrinsics/#f:@navigationhierarchiesinstructiongroup=[Cryptography,SHA256]
+fn sha256hash(abcd: [u32; 4], efgh: [u32; 4], wk: [u32; 4]) -> ([u32; 4], [u32; 4]) {
+    let mut state = [abcd[0], abcd[1], abcd[2], abcd[3], efgh[0], efgh[1], efgh[2], efgh[3]];
+    for &wk_i in &wk {
+        state = sha256::round(state, wk_i);
+    }
+    ([state[0], state[1], state[2], state[3]], [state[4], state[5], state[6], state[7]])
+}
+
+fn sha256h(abcd: [u32; 4], efgh: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
+    sha256hash(abcd, efgh, wk).0
+}
+
+// sha256h2 takes efgh as the first argument. abcd and efgh are swapped when calling sha256hash.
+fn sha256h2(efgh: [u32; 4], abcd: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
+    sha256hash(abcd, efgh, wk).1
 }

src/shims/x86/sha.rs
@@ -6,6 +6,7 @@
 
 use rustc_span::Symbol;
 
+use crate::shims::math::sha256;
 use crate::*;
 
 impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
@@ -110,110 +111,41 @@ pub(super) trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
     }
 }
 
-#[inline(always)]
-fn shr(v: [u32; 4], o: u32) -> [u32; 4] {
-    [v[0] >> o, v[1] >> o, v[2] >> o, v[3] >> o]
-}
-
-#[inline(always)]
-fn shl(v: [u32; 4], o: u32) -> [u32; 4] {
-    [v[0] << o, v[1] << o, v[2] << o, v[3] << o]
-}
-
-#[inline(always)]
-fn or(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [a[0] | b[0], a[1] | b[1], a[2] | b[2], a[3] | b[3]]
-}
-
-#[inline(always)]
-fn xor(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [a[0] ^ b[0], a[1] ^ b[1], a[2] ^ b[2], a[3] ^ b[3]]
-}
-
-#[inline(always)]
-fn add(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [
-        a[0].wrapping_add(b[0]),
-        a[1].wrapping_add(b[1]),
-        a[2].wrapping_add(b[2]),
-        a[3].wrapping_add(b[3]),
-    ]
-}
-
 fn sha256load(v2: [u32; 4], v3: [u32; 4]) -> [u32; 4] {
     [v3[3], v2[0], v2[1], v2[2]]
 }
 
 fn sha256_digest_round_x2(cdgh: [u32; 4], abef: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
-    macro_rules! big_sigma0 {
-        ($a:expr) => {
-            ($a.rotate_right(2) ^ $a.rotate_right(13) ^ $a.rotate_right(22))
-        };
-    }
-    macro_rules! big_sigma1 {
-        ($a:expr) => {
-            ($a.rotate_right(6) ^ $a.rotate_right(11) ^ $a.rotate_right(25))
-        };
-    }
-    macro_rules! bool3ary_202 {
-        ($a:expr, $b:expr, $c:expr) => {
-            $c ^ ($a & ($b ^ $c))
-        };
-    } // Choose, MD5F, SHA1C
-    macro_rules! bool3ary_232 {
-        ($a:expr, $b:expr, $c:expr) => {
-            ($a & $b) ^ ($a & $c) ^ ($b & $c)
-        };
-    } // Majority, SHA1M
-
+    // `sha256rnds2`: two rounds on the abef/cdgh permutation
+    // Ref: https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm_sha256rnds2
     let [_, _, wk1, wk0] = wk;
     let [a0, b0, e0, f0] = abef;
     let [c0, d0, g0, h0] = cdgh;
 
-    // a round
-    let x0 =
-        big_sigma1!(e0).wrapping_add(bool3ary_202!(e0, f0, g0)).wrapping_add(wk0).wrapping_add(h0);
-    let y0 = big_sigma0!(a0).wrapping_add(bool3ary_232!(a0, b0, c0));
-    let (a1, b1, c1, d1, e1, f1, g1, h1) =
-        (x0.wrapping_add(y0), a0, b0, c0, x0.wrapping_add(d0), e0, f0, g0);
+    let state = sha256::round([a0, b0, c0, d0, e0, f0, g0, h0], wk0);
+    let state = sha256::round(state, wk1);
 
-    // a round
-    let x1 =
-        big_sigma1!(e1).wrapping_add(bool3ary_202!(e1, f1, g1)).wrapping_add(wk1).wrapping_add(h1);
-    let y1 = big_sigma0!(a1).wrapping_add(bool3ary_232!(a1, b1, c1));
-    let (a2, b2, _, _, e2, f2, _, _) =
-        (x1.wrapping_add(y1), a1, b1, c1, x1.wrapping_add(d1), e1, f1, g1);
-
-    [a2, b2, e2, f2]
+    [state[0], state[1], state[4], state[5]]
 }
 
 fn sha256msg1(v0: [u32; 4], v1: [u32; 4]) -> [u32; 4] {
-    // sigma 0 on vectors
-    #[inline]
-    fn sigma0x4(x: [u32; 4]) -> [u32; 4] {
-        let t1 = or(shr(x, 7), shl(x, 25));
-        let t2 = or(shr(x, 18), shl(x, 14));
-        let t3 = shr(x, 3);
-        xor(xor(t1, t2), t3)
-    }
-
-    add(v0, sigma0x4(sha256load(v0, v1)))
+    let x = sha256load(v0, v1);
+    [
+        v0[0].wrapping_add(sha256::sigma0(x[0])),
+        v0[1].wrapping_add(sha256::sigma0(x[1])),
+        v0[2].wrapping_add(sha256::sigma0(x[2])),
+        v0[3].wrapping_add(sha256::sigma0(x[3])),
+    ]
 }
 
 fn sha256msg2(v4: [u32; 4], v3: [u32; 4]) -> [u32; 4] {
-    macro_rules! sigma1 {
-        ($a:expr) => {
-            $a.rotate_right(17) ^ $a.rotate_right(19) ^ ($a >> 10)
-        };
-    }
-
     let [x3, x2, x1, x0] = v4;
     let [w15, w14, _, _] = v3;
 
-    let w16 = x0.wrapping_add(sigma1!(w14));
-    let w17 = x1.wrapping_add(sigma1!(w15));
-    let w18 = x2.wrapping_add(sigma1!(w16));
-    let w19 = x3.wrapping_add(sigma1!(w17));
+    let w16 = x0.wrapping_add(sha256::sigma1(w14));
+    let w17 = x1.wrapping_add(sha256::sigma1(w15));
+    let w18 = x2.wrapping_add(sha256::sigma1(w16));
+    let w19 = x3.wrapping_add(sha256::sigma1(w17));
 
     [w19, w18, w17, w16]
 }

rust-lang/rust

src/tools/miri/src/intrinsics/aarch64.rs
@@ -1,7 +1,7 @@
 use rustc_middle::mir::BinOp;
 use rustc_span::Symbol;
 
-use crate::intrinsics::math::compute_crc32;
+use crate::intrinsics::math::{compute_crc32, sha256};
 use crate::*;
 
 impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
@@ -314,8 +314,163 @@ pub(super) trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                 this.write_scalar(Scalar::from_u128(result), &dest)?;
             }
 
+            // Used to implement the vsha256hq_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256hq_u32
+            "crypto.sha256h" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [abcd, efgh, wk] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (abcd, abcd_len) = this.project_to_simd(abcd)?;
+                let (efgh, efgh_len) = this.project_to_simd(efgh)?;
+                let (wk, wk_len) = this.project_to_simd(wk)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(abcd_len, 4);
+                assert_eq!(efgh_len, 4);
+                assert_eq!(wk_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let abcd: [u32; 4] = read_u32x4(this, &abcd)?;
+                let efgh: [u32; 4] = read_u32x4(this, &efgh)?;
+                let wk: [u32; 4] = read_u32x4(this, &wk)?;
+
+                let result = sha256h(abcd, efgh, wk);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256h2q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256h2q_u32
+            "crypto.sha256h2" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [efgh, abcd, wk] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (efgh, efgh_len) = this.project_to_simd(efgh)?;
+                let (abcd, abcd_len) = this.project_to_simd(abcd)?;
+                let (wk, wk_len) = this.project_to_simd(wk)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(efgh_len, 4);
+                assert_eq!(abcd_len, 4);
+                assert_eq!(wk_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let efgh: [u32; 4] = read_u32x4(this, &efgh)?;
+                let abcd: [u32; 4] = read_u32x4(this, &abcd)?;
+                let wk: [u32; 4] = read_u32x4(this, &wk)?;
+
+                let result = sha256h2(efgh, abcd, wk);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256su0q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256su0q_u32
+            "crypto.sha256su0" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [a, b] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (a, a_len) = this.project_to_simd(a)?;
+                let (b, b_len) = this.project_to_simd(b)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(a_len, 4);
+                assert_eq!(b_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let a: [u32; 4] = read_u32x4(this, &a)?;
+                let b: [u32; 4] = read_u32x4(this, &b)?;
+
+                let result = sha256su0(a, b);
+
+                write_u32x4(this, &dest, result)?;
+            }
+            // Used to implement the vsha256su1q_u32 function.
+            // https://developer.arm.com/architectures/instruction-sets/intrinsics/vsha256su1q_u32
+            "crypto.sha256su1" => {
+                this.expect_target_feature_for_intrinsic(link_name, "sha2")?;
+
+                let [a, b, c] = this.check_shim_sig_unadjusted(link_name, args)?;
+
+                let (a, a_len) = this.project_to_simd(a)?;
+                let (b, b_len) = this.project_to_simd(b)?;
+                let (c, c_len) = this.project_to_simd(c)?;
+                let (dest, dest_len) = this.project_to_simd(dest)?;
+
+                assert_eq!(a_len, 4);
+                assert_eq!(b_len, 4);
+                assert_eq!(c_len, 4);
+                assert_eq!(dest_len, 4);
+
+                let a: [u32; 4] = read_u32x4(this, &a)?;
+                let b: [u32; 4] = read_u32x4(this, &b)?;
+                let c: [u32; 4] = read_u32x4(this, &c)?;
+
+                let result = sha256su1(a, b, c);
+
+                write_u32x4(this, &dest, result)?;
+            }
             _ => return interp_ok(EmulateItemResult::NotSupported),
         }
         interp_ok(EmulateItemResult::NeedsReturn)
     }
+}
+
+/// Reads a `[u32; 4]` array.
+fn read_u32x4<'c>(ecx: &mut MiriInterpCx<'c>, vec: &OpTy<'c>) -> InterpResult<'c, [u32; 4]> {
+    let mut res = [0; 4];
+    for (i, dst) in res.iter_mut().enumerate() {
+        let projected = &ecx.project_index(vec, i.try_into().unwrap())?;
+        *dst = ecx.read_scalar(projected)?.to_u32()?;
+    }
+    interp_ok(res)
+}
+
+fn write_u32x4<'c>(
+    ecx: &mut MiriInterpCx<'c>,
+    dest: &MPlaceTy<'c>,
+    val: [u32; 4],
+) -> InterpResult<'c, ()> {
+    for (i, part) in val.into_iter().enumerate() {
+        let projected = &ecx.project_index(dest, i.to_u64())?;
+        ecx.write_scalar(Scalar::from_u32(part), projected)?;
+    }
+    interp_ok(())
+}
+
+fn sha256su0(v0: [u32; 4], v1: [u32; 4]) -> [u32; 4] {
+    [
+        v0[0].wrapping_add(sha256::sigma0(v0[1])),
+        v0[1].wrapping_add(sha256::sigma0(v0[2])),
+        v0[2].wrapping_add(sha256::sigma0(v0[3])),
+        v0[3].wrapping_add(sha256::sigma0(v1[0])),
+    ]
+}
+
+fn sha256su1(v0: [u32; 4], v1: [u32; 4], v2: [u32; 4]) -> [u32; 4] {
+    let r0 = v0[0].wrapping_add(v1[1]).wrapping_add(sha256::sigma1(v2[2]));
+    let r1 = v0[1].wrapping_add(v1[2]).wrapping_add(sha256::sigma1(v2[3]));
+    let r2 = v0[2].wrapping_add(v1[3]).wrapping_add(sha256::sigma1(r0));
+    let r3 = v0[3].wrapping_add(v2[0]).wrapping_add(sha256::sigma1(r1));
+    [r0, r1, r2, r3]
+}
+
+// SHA256H/SHA256H2 do four compression rounds on the abcd/efgh layout.
+// https://developer.arm.com/architectures/instruction-sets/intrinsics/#f:@navigationhierarchiesinstructiongroup=[Cryptography,SHA256]
+fn sha256hash(abcd: [u32; 4], efgh: [u32; 4], wk: [u32; 4]) -> ([u32; 4], [u32; 4]) {
+    let mut state = [abcd[0], abcd[1], abcd[2], abcd[3], efgh[0], efgh[1], efgh[2], efgh[3]];
+    for &wk_i in &wk {
+        state = sha256::round(state, wk_i);
+    }
+    ([state[0], state[1], state[2], state[3]], [state[4], state[5], state[6], state[7]])
+}
+
+fn sha256h(abcd: [u32; 4], efgh: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
+    sha256hash(abcd, efgh, wk).0
+}
+
+// sha256h2 takes efgh as the first argument. abcd and efgh are swapped when calling sha256hash.
+fn sha256h2(efgh: [u32; 4], abcd: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
+    sha256hash(abcd, efgh, wk).1
 }

src/tools/miri/src/intrinsics/x86/sha.rs
@@ -6,6 +6,7 @@
 
 use rustc_span::Symbol;
 
+use crate::intrinsics::math::sha256;
 use crate::*;
 
 impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
@@ -110,110 +111,41 @@ pub(super) trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
     }
 }
 
-#[inline(always)]
-fn shr(v: [u32; 4], o: u32) -> [u32; 4] {
-    [v[0] >> o, v[1] >> o, v[2] >> o, v[3] >> o]
-}
-
-#[inline(always)]
-fn shl(v: [u32; 4], o: u32) -> [u32; 4] {
-    [v[0] << o, v[1] << o, v[2] << o, v[3] << o]
-}
-
-#[inline(always)]
-fn or(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [a[0] | b[0], a[1] | b[1], a[2] | b[2], a[3] | b[3]]
-}
-
-#[inline(always)]
-fn xor(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [a[0] ^ b[0], a[1] ^ b[1], a[2] ^ b[2], a[3] ^ b[3]]
-}
-
-#[inline(always)]
-fn add(a: [u32; 4], b: [u32; 4]) -> [u32; 4] {
-    [
-        a[0].wrapping_add(b[0]),
-        a[1].wrapping_add(b[1]),
-        a[2].wrapping_add(b[2]),
-        a[3].wrapping_add(b[3]),
-    ]
-}
-
 fn sha256load(v2: [u32; 4], v3: [u32; 4]) -> [u32; 4] {
     [v3[3], v2[0], v2[1], v2[2]]
 }
 
 fn sha256_digest_round_x2(cdgh: [u32; 4], abef: [u32; 4], wk: [u32; 4]) -> [u32; 4] {
-    macro_rules! big_sigma0 {
-        ($a:expr) => {
-            ($a.rotate_right(2) ^ $a.rotate_right(13) ^ $a.rotate_right(22))
-        };
-    }
-    macro_rules! big_sigma1 {
-        ($a:expr) => {
-            ($a.rotate_right(6) ^ $a.rotate_right(11) ^ $a.rotate_right(25))
-        };
-    }
-    macro_rules! bool3ary_202 {
-        ($a:expr, $b:expr, $c:expr) => {
-            $c ^ ($a & ($b ^ $c))
-        };
-    } // Choose, MD5F, SHA1C
-    macro_rules! bool3ary_232 {
-        ($a:expr, $b:expr, $c:expr) => {
-            ($a & $b) ^ ($a & $c) ^ ($b & $c)
-        };
-    } // Majority, SHA1M
-
+    // `sha256rnds2`: two rounds on the abef/cdgh permutation
+    // Ref: https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm_sha256rnds2
     let [_, _, wk1, wk0] = wk;
     let [a0, b0, e0, f0] = abef;
     let [c0, d0, g0, h0] = cdgh;
 
-    // a round
-    let x0 =
-        big_sigma1!(e0).wrapping_add(bool3ary_202!(e0, f0, g0)).wrapping_add(wk0).wrapping_add(h0);
-    let y0 = big_sigma0!(a0).wrapping_add(bool3ary_232!(a0, b0, c0));
-    let (a1, b1, c1, d1, e1, f1, g1, h1) =
-        (x0.wrapping_add(y0), a0, b0, c0, x0.wrapping_add(d0), e0, f0, g0);
+    let state = sha256::round([a0, b0, c0, d0, e0, f0, g0, h0], wk0);
+    let state = sha256::round(state, wk1);
 
-    // a round
-    let x1 =
-        big_sigma1!(e1).wrapping_add(bool3ary_202!(e1, f1, g1)).wrapping_add(wk1).wrapping_add(h1);
-    let y1 = big_sigma0!(a1).wrapping_add(bool3ary_232!(a1, b1, c1));
-    let (a2, b2, _, _, e2, f2, _, _) =
-        (x1.wrapping_add(y1), a1, b1, c1, x1.wrapping_add(d1), e1, f1, g1);
-
-    [a2, b2, e2, f2]
+    [state[0], state[1], state[4], state[5]]
 }
 
 fn sha256msg1(v0: [u32; 4], v1: [u32; 4]) -> [u32; 4] {
-    // sigma 0 on vectors
-    #[inline]
-    fn sigma0x4(x: [u32; 4]) -> [u32; 4] {
-        let t1 = or(shr(x, 7), shl(x, 25));
-        let t2 = or(shr(x, 18), shl(x, 14));
-        let t3 = shr(x, 3);
-        xor(xor(t1, t2), t3)
-    }
-
-    add(v0, sigma0x4(sha256load(v0, v1)))
+    let x = sha256load(v0, v1);
+    [
+        v0[0].wrapping_add(sha256::sigma0(x[0])),
+        v0[1].wrapping_add(sha256::sigma0(x[1])),
+        v0[2].wrapping_add(sha256::sigma0(x[2])),
+        v0[3].wrapping_add(sha256::sigma0(x[3])),
+    ]
 }
 
 fn sha256msg2(v4: [u32; 4], v3: [u32; 4]) -> [u32; 4] {
-    macro_rules! sigma1 {
-        ($a:expr) => {
-            $a.rotate_right(17) ^ $a.rotate_right(19) ^ ($a >> 10)
-        };
-    }
-
     let [x3, x2, x1, x0] = v4;
     let [w15, w14, _, _] = v3;
 
-    let w16 = x0.wrapping_add(sigma1!(w14));
-    let w17 = x1.wrapping_add(sigma1!(w15));
-    let w18 = x2.wrapping_add(sigma1!(w16));
-    let w19 = x3.wrapping_add(sigma1!(w17));
+    let w16 = x0.wrapping_add(sha256::sigma1(w14));
+    let w17 = x1.wrapping_add(sha256::sigma1(w15));
+    let w18 = x2.wrapping_add(sha256::sigma1(w16));
+    let w19 = x3.wrapping_add(sha256::sigma1(w17));
 
     [w19, w18, w17, w16]
 }