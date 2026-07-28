ae0348320b4bbc48a8dbcd0f56bd0771fcbefcdc
Jules Bertholet <julesbertholet@quoi.xyz>
2026-07-28 17:14:58 -0400

    Lint against `c_void` parameters

--- compiler/rustc_lint/src/c_void.rs
+++ compiler/rustc_lint/src/c_void.rs
@@ -8,10 +8,12 @@ use rustc_lint_defs::{declare_lint, declare_lint_pass};
 use rustc_middle::ty;
 use rustc_span::Span;
 
-use crate::diagnostics::{CVoidConst, CVoidReference, CVoidReturn, CVoidStatic, ExternCVoidReturn};
+use crate::diagnostics::{
+    CVoidConst, CVoidParameter, CVoidReference, CVoidReturn, CVoidStatic, ExternCVoidReturn,
+};
 use crate::{LateContext, LateLintPass, LintContext};
 
-fn is_c_void(cx: &LateContext<'_>, ty: hir::Ty<'_>) -> bool {
+fn is_c_void(cx: &LateContext<'_>, ty: &hir::Ty<'_>) -> bool {
     if let hir::TyKind::Path(qpath) = ty.kind
         && let Res::Def(.., def_id) = cx.qpath_res(&qpath, ty.hir_id)
     {
@@ -27,6 +29,35 @@ fn is_c_void(cx: &LateContext<'_>, ty: hir::Ty<'_>) -> bool {
     } else {
         false
     }
+}
+
+declare_lint! {
+    /// The `c_void_parameters` lint detects the use of [`core::ffi::c_void`] as a parameter type.
+    ///
+    /// ### Example
+    ///
+    /// ```rust
+    /// use std::ffi::c_void;
+    ///
+    /// unsafe extern "C" {
+    ///     fn foo(_: c_void);
+    /// }
+    /// ```
+    ///
+    /// {{produces}}
+    ///
+    /// ### Explanation
+    ///
+    /// `c_void` is designed for use through a [`pointer`], equivalent to C's `void*` type. It is a
+    /// mistake to use it directly as a parameter type. If you intended to use a pointer type, use
+    /// `*mut c_void` or `*const c_void`.
+    ///
+    /// [`core::ffi::c_void`]: https://doc.rust-lang.org/core/ffi/enum.c_void.html
+    /// [`pointer`]: https://doc.rust-lang.org/core/primitive.pointer.html
+    /// [`()`]: https://doc.rust-lang.org/core/primitive.unit.html
+    pub C_VOID_PARAMETERS,
+    Warn,
+    "detects use of `c_void` as a parameter type"
 }
 
 declare_lint! {
@@ -59,9 +90,9 @@ declare_lint! {
     "detects use of `c_void` as a return type"
 }
 
-declare_lint_pass!(CVoidReturns => [C_VOID_RETURNS]);
+declare_lint_pass!(CVoidParamsAndReturns => [C_VOID_PARAMETERS, C_VOID_RETURNS]);
 
-impl<'tcx> LateLintPass<'tcx> for CVoidReturns {
+impl<'tcx> LateLintPass<'tcx> for CVoidParamsAndReturns {
     fn check_fn(
         &mut self,
         cx: &LateContext<'tcx>,
@@ -71,7 +102,7 @@ impl<'tcx> LateLintPass<'tcx> for CVoidReturns {
         _: Span,
         _: LocalDefId,
     ) {
-        check_decl_for_c_void_return(
+        check_fn_decl_for_c_void(
             cx,
             decl,
             !matches!(fn_kind, FnKind::ItemFn(.., hir::FnHeader { abi: ExternAbi::Rust, .. })),
@@ -80,26 +111,26 @@ impl<'tcx> LateLintPass<'tcx> for CVoidReturns {
 
     fn check_foreign_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx hir::ForeignItem<'tcx>) {
         if let hir::ForeignItemKind::Fn(sig, ..) = item.kind {
-            check_decl_for_c_void_return(cx, sig.decl, true);
+            check_fn_decl_for_c_void(cx, sig.decl, true);
         }
     }
 
     fn check_trait_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx hir::TraitItem<'tcx>) {
         if let hir::TraitItemKind::Fn(sig, ..) = item.kind {
-            check_decl_for_c_void_return(cx, sig.decl, false);
+            check_fn_decl_for_c_void(cx, sig.decl, false);
         }
     }
 
     fn check_ty(&mut self, cx: &LateContext<'tcx>, ty: &'tcx hir::Ty<'tcx, hir::AmbigArg>) {
         if let hir::TyKind::FnPtr(fn_ptr_ty) = ty.kind {
-            check_decl_for_c_void_return(cx, fn_ptr_ty.decl, fn_ptr_ty.abi != ExternAbi::Rust);
+            check_fn_decl_for_c_void(cx, fn_ptr_ty.decl, fn_ptr_ty.abi != ExternAbi::Rust);
         }
     }
 }
 
-fn check_decl_for_c_void_return(cx: &LateContext<'_>, decl: &hir::FnDecl<'_>, is_extern: bool) {
+fn check_fn_decl_for_c_void(cx: &LateContext<'_>, decl: &hir::FnDecl<'_>, is_extern: bool) {
     if let hir::FnRetTy::Return(output_ty) = decl.output
-        && is_c_void(cx, *output_ty)
+        && is_c_void(cx, output_ty)
     {
         let suggestion =
             cx.sess().source_map().span_extend_to_prev_char(decl.output.span(), ')', true);
@@ -108,6 +139,12 @@ fn check_decl_for_c_void_return(cx: &LateContext<'_>, decl: &hir::FnDecl<'_>, is
             cx.emit_span_lint(C_VOID_RETURNS, decl.output.span(), ExternCVoidReturn { suggestion });
         } else {
             cx.emit_span_lint(C_VOID_RETURNS, decl.output.span(), CVoidReturn { suggestion });
+        }
+    }
+
+    for param_ty in decl.inputs {
+        if is_c_void(cx, param_ty) {
+            cx.emit_span_lint(C_VOID_PARAMETERS, param_ty.span, CVoidParameter);
         }
     }
 }
@@ -146,7 +183,7 @@ declare_lint_pass!(CVoidReferences => [C_VOID_REFERENCES]);
 impl<'tcx> LateLintPass<'tcx> for CVoidReferences {
     fn check_ty(&mut self, cx: &LateContext<'tcx>, ty: &'tcx hir::Ty<'tcx, hir::AmbigArg>) {
         if let hir::TyKind::Ref(_, hir::MutTy { ty: pointee_ty, .. }) = ty.kind
-            && is_c_void(cx, *pointee_ty)
+            && is_c_void(cx, pointee_ty)
         {
             cx.emit_span_lint(C_VOID_REFERENCES, ty.span, CVoidReference);
         }
@@ -187,10 +224,10 @@ declare_lint_pass!(CVoidStatics => [C_VOID_STATICS]);
 impl<'tcx> LateLintPass<'tcx> for CVoidStatics {
     fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx rustc_hir::Item<'tcx>) {
         match item.kind {
-            hir::ItemKind::Static(_mut, _name, ty, _) if is_c_void(cx, *ty) => {
+            hir::ItemKind::Static(_mut, _name, ty, _) if is_c_void(cx, ty) => {
                 cx.emit_span_lint(C_VOID_STATICS, item.span, CVoidStatic { suggestion: ty.span })
             }
-            hir::ItemKind::Const(_name, _generics, ty, _) if is_c_void(cx, *ty) => {
+            hir::ItemKind::Const(_name, _generics, ty, _) if is_c_void(cx, ty) => {
                 cx.emit_span_lint(C_VOID_STATICS, item.span, CVoidConst)
             }
             _ => (),
@@ -203,7 +240,7 @@ impl<'tcx> LateLintPass<'tcx> for CVoidStatics {
         item: &'tcx rustc_hir::ForeignItem<'tcx>,
     ) {
         if let hir::ForeignItemKind::Static(ty, ..) = item.kind
-            && is_c_void(cx, *ty)
+            && is_c_void(cx, ty)
         {
             cx.emit_span_lint(C_VOID_STATICS, item.span, CVoidStatic { suggestion: ty.span })
         }
@@ -211,7 +248,7 @@ impl<'tcx> LateLintPass<'tcx> for CVoidStatics {
 
     fn check_trait_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx rustc_hir::TraitItem<'tcx>) {
         if let hir::TraitItemKind::Const(ty, ..) = item.kind
-            && is_c_void(cx, *ty)
+            && is_c_void(cx, ty)
         {
             cx.emit_span_lint(C_VOID_STATICS, item.span, CVoidConst)
         }
