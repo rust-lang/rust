e9451181988693c23512704aafe615f122f43950
Makro <mnlkroiss@gmail.com>
2026-07-28 11:05:57 +0000

    Distinguish the dep-graph index space from the live node count

--- compiler/rustc_middle/src/dep_graph/serialized.rs
+++ compiler/rustc_middle/src/dep_graph/serialized.rs
@@ -116,6 +116,9 @@ pub struct SerializedDepGraph {
     /// [`SerializedDepNodeIndex`] via the node's key fingerprint. See
     /// [`LazyNodeIndex`].
     reverse_index: LazyNodeIndex,
+    /// The number of nodes actually encoded, which is below [`Self::index_space_len`]
+    /// whenever a thread left part of its batch of indices unused.
+    live_node_count: usize,
     /// The number of previous compilation sessions. This is used to generate
     /// unique anon dep nodes per session.
     session_count: u64,
@@ -133,6 +136,7 @@ impl std::fmt::Debug for SerializedDepGraph {
             .field("edge_list_indices", &self.edge_list_indices)
             .field("edge_list_data", &self.edge_list_data)
             .field("reverse_index", &self.reverse_index)
+            .field("live_node_count", &self.live_node_count)
             .field("session_count", &self.session_count)
             .finish_non_exhaustive()
     }
@@ -251,9 +255,15 @@ impl SerializedDepGraph {
         self.value_fingerprints[dep_node_index]
     }
 
+    /// The number of dep-node indices, counting those that hold no node.
     #[inline]
-    pub fn node_count(&self) -> usize {
+    pub fn index_space_len(&self) -> usize {
         self.nodes.len()
+    }
+
+    #[inline]
+    pub fn live_node_count(&self) -> usize {
+        self.live_node_count
     }
 
     #[inline]
@@ -414,6 +424,7 @@ impl SerializedDepGraph {
             edge_list_indices,
             edge_list_data,
             reverse_index,
+            live_node_count: node_count,
             session_count,
             profiler: Some(profiler.clone()),
         })
@@ -892,14 +903,14 @@ impl GraphEncoder {
     pub(crate) fn new(
         sess: &Session,
         encoder: FileEncoder<'static>,
-        prev_node_count: usize,
+        prev_index_space_len: usize,
         previous: Arc<SerializedDepGraph>,
     ) -> Self {
         let retained_graph = sess
             .opts
             .unstable_opts
             .query_dep_graph
-            .then(|| Lock::new(RetainedDepGraph::new(prev_node_count)));
+            .then(|| Lock::new(RetainedDepGraph::new(prev_index_space_len)));
         let status = EncoderState::new(encoder, sess.opts.unstable_opts.incremental_info, previous);
         GraphEncoder { status, retained_graph, profiler: sess.prof.clone() }
     }
