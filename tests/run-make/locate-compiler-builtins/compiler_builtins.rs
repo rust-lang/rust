d1e25b4ac8952835a30f66f232858d6fc8e5cec0
Bryanskiy <ivakin.kir@gmail.com>
2026-07-29 21:43:53 +0300

    tests: remove test for compiler_builtins

--- tests/run-make/locate-compiler-builtins/compiler_builtins.rs
+++
@@ -1,9 +0,0 @@
-// We are `compiler_builtins`.
-#![feature(lang_items, no_core, compiler_builtins)]
-#![allow(internal_features)]
-#![compiler_builtins]
-#![no_std]
-#![no_core]
-#![crate_type = "rlib"]
-
-extern crate core;
