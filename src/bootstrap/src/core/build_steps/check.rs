f54409bae7b07c2c76f6dcfbd587367a76d287f9
Zalathar <Zalathar@users.noreply.github.com>
2026-07-30 20:30:44 +1000

    Remove method `Subcommand::kind`
    Every caller can either get the kind from `builder.kind`, or directly match
    against variants of `Subcommand`.
    
    `Builder::new` is now the sole mapping from `Subcommand` to `Kind`.

--- src/bootstrap/src/core/build_steps/check.rs
+++ src/bootstrap/src/core/build_steps/check.rs
@@ -88,7 +88,7 @@ impl CommandLineStep for Std {
             Mode::Std,
             SourceType::InTree,
             target,
-            builder.config.cmd.kind(),
+            builder.kind,
         );
 
         std_cargo(builder, target, &mut cargo, &self.crates);
@@ -98,7 +98,7 @@ impl CommandLineStep for Std {
         }
 
         let _guard = builder.msg(
-            builder.config.cmd.kind(),
+            builder.kind,
             format_args!("library artifacts{}", crate_description(&self.crates)),
             Mode::Std,
             build_compiler,
