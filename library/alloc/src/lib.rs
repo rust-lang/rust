20eb58fa1e00dd208ae49cbf11930819adc95942
Jacob Pratt <jacob@jhpratt.dev>
2026-07-31 23:34:44 -0400

    Remove final use of sealed traits from stdlib

--- library/alloc/src/lib.rs
+++ library/alloc/src/lib.rs
@@ -203,6 +203,7 @@
 #![feature(decl_macro)]
 #![feature(dropck_eyepatch)]
 #![feature(fundamental)]
+#![feature(impl_restriction)]
 #![feature(intrinsics)]
 #![feature(lang_items)]
 #![feature(min_specialization)]
