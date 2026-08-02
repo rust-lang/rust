3ebccb1de832249a66bdc8ddf81f7938a5dbd033
Folkert de Vries <folkert@folkertdev.nl>
2026-08-02 21:29:43 +0200

    sparc: pass aggregate arguments by reference
    this matches GCC and Clang.

--- compiler/rustc_target/src/callconv/sparc.rs
+++ compiler/rustc_target/src/callconv/sparc.rs
@@ -1,55 +1,38 @@
-use rustc_abi::{BackendRepr, Float, HasDataLayout, Primitive, Size, TyAbiInterface};
+use rustc_abi::{BackendRepr, Float, HasDataLayout, Primitive, TyAbiInterface};
 
-use crate::callconv::{ArgAbi, FnAbi, Reg, Uniform};
+use crate::callconv::{ArgAbi, FnAbi};
 
 fn is_long_double(repr: BackendRepr) -> bool {
     matches!(repr, BackendRepr::Scalar(scalar) if scalar.primitive() == Primitive::Float(Float::F128))
 }
 
-fn classify_ret<Ty, C>(cx: &C, ret: &mut ArgAbi<'_, Ty>, offset: &mut Size)
-where
-    C: HasDataLayout,
-{
+fn classify_ret<'a, Ty>(ret: &mut ArgAbi<'a, Ty>) {
     if is_long_double(ret.layout.backend_repr) || ret.layout.is_aggregate() {
         ret.make_indirect();
-        *offset += cx.data_layout().pointer_size();
     } else {
         ret.extend_integer_width_to(32);
     }
 }
 
-fn classify_arg<'a, Ty, C>(cx: &C, arg: &mut ArgAbi<'a, Ty>, offset: &mut Size)
+fn classify_arg<'a, Ty, C>(cx: &C, arg: &mut ArgAbi<'a, Ty>)
 where
     Ty: TyAbiInterface<'a, C> + Copy,
     C: HasDataLayout,
 {
     if !arg.layout.is_sized() {
-        // FIXME: Update offset?
         // Not touching this...
         return;
     }
-    let dl = cx.data_layout();
     if arg.layout.pass_indirectly_in_non_rustic_abis(cx) {
         arg.make_indirect();
-        *offset += dl.pointer_size();
         return;
     }
-    if is_long_double(arg.layout.backend_repr) {
-        arg.pass_by_stack_offset(None);
-        *offset += dl.pointer_size();
-        return;
-    }
-    let size = arg.layout.size;
-    let align = arg.layout.align.abi.max(dl.i32_align).min(dl.i64_align);
 
-    if arg.layout.is_aggregate() {
-        let pad_i32 = u8::from(!offset.is_aligned(align));
-        arg.cast_to_and_pad_i32(Uniform::new(Reg::i32(), size), pad_i32);
+    if arg.layout.is_aggregate() || is_long_double(arg.layout.backend_repr) {
+        arg.make_indirect();
     } else {
         arg.extend_integer_width_to(32);
     }
-
-    *offset = offset.align_to(align) + size.align_to(align);
 }
 
 pub(crate) fn compute_abi_info<'a, Ty, C>(cx: &C, fn_abi: &mut FnAbi<'a, Ty>)
@@ -57,19 +40,17 @@ where
     Ty: TyAbiInterface<'a, C> + Copy,
     C: HasDataLayout,
 {
-    let mut offset = Size::ZERO;
     if !fn_abi.ret.is_ignore() {
-        classify_ret(cx, &mut fn_abi.ret, &mut offset);
+        classify_ret(&mut fn_abi.ret);
     }
 
     for arg in fn_abi.args.iter_mut() {
         if arg.is_ignore() {
             if arg.layout.is_zst() {
                 arg.make_indirect_from_ignore();
-                offset += cx.data_layout().pointer_size();
             }
             continue;
         }
-        classify_arg(cx, arg, &mut offset);
+        classify_arg(cx, arg);
     }
 }
