926cb677535cd3e6184628903faecee12499c396
Michael Baikov <manpacket@gmail.com>
2026-08-03 06:46:53 -0400

    core: add try_next_code_point{,_reverse} internal functions
    @pacak:
    - made more things const fn
    - there was a (copy-paste?) error in try_finish_byte_sequence so I
      added a test that checks try_next_code_point(_reverse) with some
      values, including invalid ones.
    - reworded a few comments (passive voice, etc)
    
    Also different comments: since former is public and later is private due
    to historical reasons.
    
    > This is different than [`next_code_point`] in that it doesn't assume
    
    > This is different than `next_code_point_reverse` in that it doesn't assume

--- library/core/src/str/mod.rs
+++ library/core/src/str/mod.rs
@@ -59,7 +59,9 @@ pub use lossy::{Utf8Chunk, Utf8Chunks};
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use traits::FromStr;
 #[unstable(feature = "str_internals", issue = "none")]
-pub use validations::{next_code_point, utf8_char_width};
+pub use validations::{
+    next_code_point, try_next_code_point, try_next_code_point_reverse, utf8_char_width,
+};
 
 #[inline(never)]
 #[cold]
