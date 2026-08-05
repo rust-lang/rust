9308c04ad7e6d6dd9ac7fe9d8bb02a8538be0133
Urgau <urgau@numericable.fr>
2026-08-05 22:58:28 +0200

    Extend `undropped_manually_drops` lint to `drop_in_place`

--- tests/ui/lint/undropped_manually_drops.rs
+++ tests/ui/lint/undropped_manually_drops.rs
@@ -11,6 +11,14 @@ fn main() {
     drop(manual1); //~ ERROR calls to `std::mem::drop`
     drop({ manual3 }); //~ ERROR calls to `std::mem::drop`
 
+    let mut manual4 = std::mem::ManuallyDrop::new(S);
+    let mut manual5 = std::mem::ManuallyDrop::new(S);
+
+    unsafe {
+        std::ptr::drop_in_place(&raw mut manual4); //~ ERROR calls to `drop_in_place`
+        (&raw mut manual5).drop_in_place(); //~ ERROR calls to `drop_in_place`
+    }
+
     // These lines will drop `S` and should be okay.
     unsafe {
         std::mem::ManuallyDrop::drop(&mut std::mem::ManuallyDrop::new(S));
