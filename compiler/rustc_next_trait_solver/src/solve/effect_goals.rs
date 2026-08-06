3cf1de3004d1bca8c4913690939a31fa265eae2f
Nicholas Nethercote <n.nethercote@gmail.com>
2026-08-06 14:58:31 +1000

    Avoid another duplicate `goal.predicate.alias.trait_ref(cx)`
    It's expensive and is called unconditionally in
    `NormalizesTo::consider_impl_candidate`, which is always called within a
    loop. The result is invariant across the loop, so this commit hoists the
    call outside of `consider_impl_candidate` and passes the return value in.
    
    Notes:
    
    - At the hoist sites, the expression is `goal.predicate.trait_ref(cx)`;
      for `NormalizesTo` this dispatches to
      `goal.predicate.alias.trait_ref(cx)`.
    
    - Two other `GoalKind` methods are affected, but this doesn't
      change perf because they can get the `goal_trait_ref` cheaply.

--- compiler/rustc_next_trait_solver/src/solve/effect_goals.rs
+++ compiler/rustc_next_trait_solver/src/solve/effect_goals.rs
@@ -136,6 +136,7 @@ where
     fn consider_impl_candidate(
         ecx: &mut EvalCtxt<'_, D>,
         goal: Goal<I, Self>,
+        goal_trait_ref: ty::TraitRef<I>,
         impl_def_id: I::ImplId,
         then: impl FnOnce(&mut EvalCtxt<'_, D>, Certainty) -> QueryResultOrRerunNonErased<I>,
     ) -> Result<Candidate<I>, NoSolutionOrRerunNonErased> {
@@ -143,7 +144,7 @@ where
 
         let impl_trait_ref = cx.impl_trait_ref(impl_def_id);
         if !DeepRejectCtxt::relate_rigid_infer(ecx.cx())
-            .args_may_unify(goal.predicate.trait_ref.args, impl_trait_ref.skip_binder().args)
+            .args_may_unify(goal_trait_ref.args, impl_trait_ref.skip_binder().args)
         {
             return Err(NoSolution.into());
         }
@@ -170,7 +171,7 @@ where
             ecx.record_impl_args(impl_args);
             let impl_trait_ref = impl_trait_ref.instantiate(cx, impl_args).skip_norm_wip();
 
-            ecx.eq(goal.param_env, goal.predicate.trait_ref, impl_trait_ref)?;
+            ecx.eq(goal.param_env, goal_trait_ref, impl_trait_ref)?;
             let where_clause_bounds = cx
                 .clauses_of(impl_def_id.into())
                 .iter_instantiated(cx, impl_args)
