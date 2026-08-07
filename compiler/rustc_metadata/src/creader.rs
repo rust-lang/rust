e1cb6fc06c261c2e5f379312ddcf0561ff7a10b7
David Mládek <david.mladek.cz@gmail.com>
2026-08-07 22:49:53 +0200

    Queries: add `is_direct_public_dep` query

--- compiler/rustc_metadata/src/creader.rs
+++ compiler/rustc_metadata/src/creader.rs
@@ -568,6 +568,10 @@ impl CStore {
         }
     }
 
+    fn is_extern_private_dep(&self, externs: &Externs, name: Symbol) -> bool {
+        externs.get(name.as_str()).map(|e| e.is_private_dep).unwrap_or_default()
+    }
+
     fn register_crate<'tcx>(
         &mut self,
         tcx: TyCtxt<'tcx>,
@@ -584,6 +588,7 @@ impl CStore {
         let Library { source, metadata } = lib;
         let crate_root = metadata.get_root();
         let host_hash = host_lib.as_ref().map(|lib| lib.metadata.get_root().hash());
+        let extern_private_dep = self.is_extern_private_dep(&tcx.sess.opts.externs, name);
         let private_dep = self.is_private_dep(&tcx.sess.opts.externs, name, private_dep);
 
         // Claim this crate number and cache it
@@ -642,6 +647,7 @@ impl CStore {
             dep_kind,
             source,
             private_dep,
+            extern_private_dep,
             host_hash,
         );
 
