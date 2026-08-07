7c696e540ae20af639cfb926184d707da8092131
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-08-07 16:52:06 +0200

    bless ui tests

--- tests/ui/never_type/never-from-impl-is-not-reserved.rs
+++ tests/ui/never_type/never-from-impl-is-not-reserved.rs
@@ -1,5 +1,7 @@
-// check that the `for<T> T: From<!>` impl is reserved
+// check that the `for<T> T: From<!>` impl is not reserved anymore
 
+//@ check-pass
+//
 //@ revisions: current next
 //@ ignore-compare-mode-next-solver (explicit revisions)
 //@[next] compile-flags: -Znext-solver=coherence
@@ -10,7 +12,6 @@ pub struct MyFoo;
 pub trait MyTrait {}
 
 impl MyTrait for MyFoo {}
-// This will conflict with the first impl if we impl `for<T> T: From<!>`.
-impl<T> MyTrait for T where T: From<!> {} //~ ERROR conflicting implementation
+impl<T> MyTrait for T where T: From<!> {}
 
 fn main() {}
