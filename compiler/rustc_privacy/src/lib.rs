0b66210476b1d011e8e1b6dab562a418327cf4f3
David Mládek <david.mladek.cz@gmail.com>
2026-08-07 23:08:16 +0200

    privacy: add new visitor for hir walking for private dependencies in public interfaces

--- compiler/rustc_privacy/src/lib.rs
+++ compiler/rustc_privacy/src/lib.rs
@@ -21,9 +21,9 @@ use rustc_data_structures::indexmap::IndexSet;
 use rustc_data_structures::intern::Interned;
 use rustc_errors::{MultiSpan, listify};
 use rustc_hir::def::{CtorOf, DefKind, Res};
-use rustc_hir::def_id::{DefId, LocalDefId, LocalModId};
+use rustc_hir::def_id::{DefId, LOCAL_CRATE, LocalDefId, LocalModId};
 use rustc_hir::intravisit::{self, InferKind, Visitor};
-use rustc_hir::{self as hir, AmbigArg, ForeignItemId, ItemId, OwnerId, PatKind, find_attr};
+use rustc_hir::{self as hir, AmbigArg, ForeignItemId, ItemId, Node, OwnerId, PatKind, find_attr};
 use rustc_middle::middle::privacy::{EffectiveVisibilities, EffectiveVisibility, Level};
 use rustc_middle::query::Providers;
 use rustc_middle::ty::print::PrintTraitRefExt as _;
@@ -1898,4 +1898,84 @@ fn check_private_in_public(tcx: TyCtxt<'_>, mod_id: LocalModId) {
     let crate_items = tcx.hir_module_items(mod_id);
     let _ = crate_items.par_items(|id| Ok(checker.check_item(id)));
     let _ = crate_items.par_foreign_items(|id| Ok(checker.check_foreign_item(id)));
+
+    let mut private_in_public_visitor = SearchInterfaceForImportedItemsVisitor { tcx, checker };
+
+    tcx.hir_visit_all_item_likes_in_crate(&mut private_in_public_visitor);
+}
+
+struct SearchInterfaceForImportedItemsVisitor<'a, 'tcx> {
+    tcx: TyCtxt<'tcx>,
+    checker: PrivateItemsInPublicInterfacesChecker<'a, 'tcx>,
+}
+
+impl<'a, 'tcx> SearchInterfaceForImportedItemsVisitor<'a, 'tcx> {
+    fn check_path(&mut self, path: &hir::Path<'_>, kind: &'static str, hir_id: hir::HirId) {
+        let mut exporting_crate_num = LOCAL_CRATE;
+
+        let Some(enclosing_def) =
+            self.tcx.hir_parent_iter(hir_id).find_map(|(_, node)| match node {
+                Node::Field(field) => Some(field.def_id),
+                Node::Item(item) => Some(item.owner_id.def_id),
+                Node::ImplItem(impl_item) => Some(impl_item.owner_id.def_id),
+                Node::TraitItem(trait_item) => Some(trait_item.owner_id.def_id),
+                Node::ForeignItem(foreign_item) => Some(foreign_item.owner_id.def_id),
+                _ => None,
+            })
+        else {
+            return;
+        };
+
+        let Some(effective_visibility) = self.checker.get(enclosing_def) else {
+            return;
+        };
+
+        if !effective_visibility.is_public_at_level(Level::Reachable) {
+            return;
+        }
+
+        for segment in path.segments {
+            let Res::Def(_def_kind, def_id) = segment.res else {
+                return;
+            };
+            let exporting_module = def_id;
+            exporting_crate_num = exporting_module.krate;
+            if exporting_crate_num != LOCAL_CRATE {
+                break;
+            }
+        }
+
+        if exporting_crate_num == LOCAL_CRATE {
+            return;
+        }
+
+        if !self.tcx.is_direct_public_dep(exporting_crate_num) {
+            self.tcx.emit_node_span_lint(
+                lint::builtin::EXPORTED_PRIVATE_DEPENDENCIES,
+                hir_id,
+                path.span,
+                FromPrivateDependencyInPublicInterface {
+                    kind,
+                    descr: (&path.segments.last().unwrap().ident.name.as_str()).into(),
+                    krate: self.tcx.crate_name(exporting_crate_num),
+                },
+            );
+        }
+    }
+}
+
+impl<'a, 'tcx, 'v> intravisit::Visitor<'v> for SearchInterfaceForImportedItemsVisitor<'a, 'tcx> {
+    fn visit_ty(&mut self, t: &'v hir::Ty<'v, AmbigArg>) -> Self::Result {
+        if let hir::TyKind::Path(hir::QPath::Resolved(_hir_ty, path)) = t.kind {
+            self.check_path(path, "type", t.hir_id);
+        }
+
+        intravisit::walk_ty(self, t)
+    }
+
+    fn visit_trait_ref(&mut self, trait_ref: &'v rustc_hir::TraitRef<'v>) {
+        self.check_path(trait_ref.path, "trait", trait_ref.hir_ref_id);
+
+        intravisit::walk_trait_ref(self, trait_ref);
+    }
 }
