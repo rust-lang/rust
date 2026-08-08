9dcb499d179cdc95c38788b1bbab493d857b7dc8
SomeFlyingThing <306498559+SomeFlyingThing@users.noreply.github.com>
2026-08-08 16:27:06 -0700

    Fix VecDeque binary search codegen test

--- tests/codegen-llvm/vec-deque-binary-search-index-no-bound-check.rs
+++ tests/codegen-llvm/vec-deque-binary-search-index-no-bound-check.rs
@@ -3,13 +3,16 @@
 
 use std::collections::VecDeque;
 
+unsafe extern "C" {
+    safe fn vec_deque_binary_search_insertion_point_out_of_bounds();
+}
+
 // Make sure no bounds check is emitted when indexing with a successful search result.
 
 // CHECK-LABEL: @vec_deque_binary_search_index_no_bounds_check
 #[no_mangle]
 pub fn vec_deque_binary_search_index_no_bounds_check(deque: &VecDeque<u8>) -> u8 {
     // CHECK-NOT: expect_failed
-    // CHECK-NOT: panic
     if let Ok(index) = deque.binary_search_by(|element| element.cmp(&b'\\')) {
         deque[index]
     } else {
@@ -19,11 +22,14 @@ pub fn vec_deque_binary_search_index_no_bounds_check(deque: &VecDeque<u8>) -> u8
 
 // An unsuccessful search result is a valid insertion point.
 
-// CHECK-LABEL: @vec_deque_binary_search_insert_no_bounds_check
+// CHECK-LABEL: @vec_deque_binary_search_insertion_point_in_bounds
 #[no_mangle]
-pub fn vec_deque_binary_search_insert_no_bounds_check(deque: &mut VecDeque<u8>) {
-    // CHECK-NOT: panic
+pub fn vec_deque_binary_search_insertion_point_in_bounds(deque: &VecDeque<u8>) {
+    // CHECK-NOT: call void @vec_deque_binary_search_insertion_point_out_of_bounds
+    // CHECK: ret void
     if let Err(index) = deque.binary_search_by(|element| element.cmp(&b'\\')) {
-        deque.insert(index, b'\\');
+        if index > deque.len() {
+            vec_deque_binary_search_insertion_point_out_of_bounds()
+        }
     }
 }
