1cf12a5f1bbaeef1b7ba4b801f72fa3285f04e04
Vicente Gusmão <vicente.gusmao.2020@gmail.com>
2026-08-10 18:19:23 +0100

    Add explanation to regression test

--- tests/ui/traits/non_lifetime_binders/foreach-partial-eq-next-solver.rs
+++ tests/ui/traits/non_lifetime_binders/foreach-partial-eq-next-solver.rs
@@ -1,5 +1,14 @@
 //@ compile-flags: -Znext-solver=globally
 
+// Regression test for <https://github.com/rust-lang/rust/issues/151304>.
+//
+// This used to ICE with `-Znext-solver=globally`.
+// The ICE happened because the `PartialOrd` bound fails causing
+// diagnostics to replay the proof tree in order to find the
+// best nested-goal. During that replay, it needs to create a
+// fresh inference variable for the higher-ranked `T` but it was
+// creating it in the wrong universe.
+
 #![allow(incomplete_features)]
 #![feature(non_lifetime_binders)]
 
