9ac7293a8ac8c08381f6008c0a3b95d2a32ed1f1
mejrs <59372212+mejrs@users.noreply.github.com>
2026-08-10 10:45:22 +0200

    resolver diagnostics: don't swallow labels and point out  similar items as a note, not a label

--- tests/ui/diagnostic_namespace/on_unknown/late_res.rs
+++ tests/ui/diagnostic_namespace/on_unknown/late_res.rs
@@ -12,16 +12,40 @@ fn stuff(x: u32) {
     match x {
         empty::blah => {}
         //~^ ERROR it works `empty` `blah` [E0531]
+        //~| NOTE cannot find unit struct, unit variant or constant `blah` in module `empty`
+        //~| NOTE label it works
+        //~| NOTE note it works
         _ => {}
     }
 
     println!("{}", empty::blah);
     //~^ ERROR it works `empty` `blah` [E0425]
+    //~| NOTE cannot find value `blah` in module `empty`
+    //~| NOTE label it works
+    //~| NOTE note it works
 
     let x = [
         empty::blah,
         //~^ ERROR it works `empty` `blah` [E0425]
+        //~| NOTE cannot find value `blah` in module `empty`
+        //~| NOTE label it works
+        //~| NOTE note it works
         empty::blah2,
         //~^ ERROR it works `empty` `blah2` [E0425]
+        //~| NOTE cannot find value `blah2` in module `empty`
+        //~| NOTE label it works
+        //~| NOTE note it works
     ];
 }
+
+#[diagnostic::on_unknown(message = "message", label = "label", note = "note")]
+mod x {
+    const WHAT: u32 = 1;
+    //~^ NOTE similarly named constant `WHAT` defined here
+
+}
+const X: u32 = x::WAHT;
+//~^ ERROR message
+//~| NOTE note
+//~| NOTE label
+//~| NOTE cannot find value `WAHT` in module `x`
