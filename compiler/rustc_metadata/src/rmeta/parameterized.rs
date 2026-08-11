32aecb01255282a2c68d3edcf8cb848170b25ba8
Joao Roberto <dnreikronos2@gmail.com>
2026-08-11 15:21:28 -0300

    Represent live alias arguments as bitsets
    Store identity argument indices instead of bound generic arguments so
    callers can index concrete alias arguments without changing rigidness
    through instantiation.

--- compiler/rustc_metadata/src/rmeta/parameterized.rs
+++ compiler/rustc_metadata/src/rmeta/parameterized.rs
@@ -106,6 +106,7 @@ trivially_parameterized_over_tcx! {
     rustc_hir::def_id::DefIndex,
     rustc_hir::definitions::DefKey,
     rustc_index::bit_set::DenseBitSet<u32>,
+    rustc_index::bit_set::DenseBitSet<usize>,
     rustc_middle::metadata::AmbigModChild,
     rustc_middle::metadata::ModChild,
     rustc_middle::middle::codegen_fn_attrs::CodegenFnAttrs,
