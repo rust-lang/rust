aa42902b15db3273f4ac0f1ba3c1865b21e66717
Jose Torres <josetorrs7@gmail.com>
2026-08-11 22:25:13 -0400

    moving attribute for Clone

--- compiler/rustc_builtin_macros/src/deriving/clone.rs
+++ compiler/rustc_builtin_macros/src/deriving/clone.rs
@@ -104,7 +104,10 @@ pub(crate) fn expand_deriving_clone(
             explicit_self: true,
             nonself_args: Vec::new(),
             ret_ty: Self_,
-            attributes: thin_vec![cx.attr_word(sym::inline, span)],
+            attributes: thin_vec![
+                cx.attr_word(sym::inline, span),
+                cx.attr_word(sym::rustc_trivial_field_reads, span)
+            ],
             fieldless_variants_strategy: FieldlessVariantsStrategy::Default,
             combine_substructure: substructure,
         }],
