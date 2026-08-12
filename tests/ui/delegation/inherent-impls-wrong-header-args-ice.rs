f8727552f273814871ec7665b25fb1d9afcda59b
aerooneqq <aerooneq@yandex.ru>
2026-08-12 11:47:00 +0300

    Better handling of wrong args in inherent impl

--- tests/ui/delegation/inherent-impls-wrong-header-args-ice.rs
+++ tests/ui/delegation/inherent-impls-wrong-header-args-ice.rs
@@ -12,8 +12,7 @@ impl<'a, 'b, 'c, A, const C: usize> S<A, C> {
 
 trait Trait<'a, AA, BB> where Self: Sized {
     reuse S::foo_self;
-    //~^ ERROR: failed to generate delegation: mismatch between provided generic args in inherent impl and declared generics
-    //~| ERROR: this function takes 1 argument but 0 arguments were supplied
+    //~^ ERROR: this function takes 1 argument but 0 arguments were supplied
 }
 
 fn main() {}
