8c60f10165f5073dec547977e0483d325e104f6e
Trevor Gross <tg@trevorgross.com>
2026-08-12 15:37:34 -0400

    ci: Re-enable the musl build on ppc64le
    This may have been a qemu bug or something with `QEMU_CPU`, but appears
    resolved now. More context at [1].
    
    [1]: https://github.com/rust-lang/compiler-builtins/pull/1096

--- library/compiler-builtins/ci/run.sh
+++ library/compiler-builtins/ci/run.sh
@@ -151,10 +151,6 @@ case "$target" in
 
     # We can build musl on MinGW but running tests gets a stack overflow
     *windows-gnu*) ;;
-    # FIXME(#309): LE PPC crashes calling the musl version of some functions. It
-    # seems like a qemu bug but should be investigated further at some point.
-    # See <https://github.com/rust-lang/libm/issues/309>.
-    *powerpc64le*) ;;
 
     # Everything else gets musl enabled
     *) mflags+=(--features libm-test/build-musl) ;;
