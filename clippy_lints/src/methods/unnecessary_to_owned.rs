e1bb6a191f030cf9636c74c720b848c5d6caabc7
Ada Alakbarova <ada.alakbarova@ferrous-systems.com>
2026-08-12 09:53:15 +0200

    rename `TraitPredicate` to `TraitClause`

--- clippy_lints/src/methods/unnecessary_to_owned.rs
+++ clippy_lints/src/methods/unnecessary_to_owned.rs
@@ -17,7 +17,7 @@ use rustc_lint::LateContext;
 use rustc_middle::mir::Mutability;
 use rustc_middle::ty::adjustment::{Adjust, Adjustment, DerefAdjustKind, OverloadedDeref};
 use rustc_middle::ty::{
-    self, ClauseKind, GenericArg, GenericArgKind, GenericArgsRef, ParamTy, ProjectionClause, TraitPredicate, Ty,
+    self, ClauseKind, GenericArg, GenericArgKind, GenericArgsRef, ParamTy, ProjectionClause, TraitClause, Ty,
 };
 use rustc_span::Symbol;
 use rustc_trait_selection::traits::query::evaluate_obligation::InferCtxtExt as _;
@@ -473,12 +473,12 @@ fn get_callee_generic_args_and_args<'tcx>(
     None
 }
 
-/// Returns the `TraitPredicate`s and `ProjectionClause`s for a function's input type.
+/// Returns the `TraitClause`s and `ProjectionClause`s for a function's input type.
 fn get_input_traits_and_projections<'tcx>(
     cx: &LateContext<'tcx>,
     callee_def_id: DefId,
     input: Ty<'tcx>,
-) -> (Vec<TraitPredicate<'tcx>>, Vec<ProjectionClause<'tcx>>) {
+) -> (Vec<TraitClause<'tcx>>, Vec<ProjectionClause<'tcx>>) {
     let mut trait_predicates = Vec::new();
     let mut projection_predicates = Vec::new();
     for clause in cx.tcx.param_env(callee_def_id).caller_bounds() {
@@ -740,7 +740,7 @@ fn check_borrow_predicate<'tcx>(cx: &LateContext<'tcx>, expr: &Expr<'tcx>) {
         && let Some(borrow_id) = cx.tcx.get_diagnostic_item(sym::Borrow)
         && cx.tcx.clauses_of(method_def_id).clauses.iter().any(|(clause, _)| {
             if let ClauseKind::Trait(trait_pred) = clause.kind().skip_binder()
-                && trait_pred.polarity == ty::PredicatePolarity::Positive
+                && trait_pred.polarity == ty::ClausePolarity::Positive
                 && trait_pred.trait_ref.def_id == borrow_id
             {
                 true
