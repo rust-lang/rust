274997f1db52b3afa9882130b8e68ee0f453d263
aerooneqq <aerooneq@yandex.ru>
2026-08-12 10:28:18 +0300

    Fix tests

--- tests/ui/delegation/inherent-impls-structs.rs
+++ tests/ui/delegation/inherent-impls-structs.rs
@@ -50,7 +50,6 @@ impl<'a, A, B> Trait<'a, A, B> for X {
     reuse S::<(), 1>::foo_static::<'static, (), true> as foo_static_1;
     reuse S::foo_static::<'static, (), true> as foo_static_2;
     //~^ ERROR: type annotations needed [E0284]
-    //~| ERROR: type annotations needed [E0284]
     reuse S::<(), 1>::foo_static as foo_static_3;
     reuse S::<_, 1>::foo_static::<'static, _, _> as foo_static_4;
     //~^ ERROR: type annotations needed [E0284]
