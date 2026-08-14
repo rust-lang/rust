e94d71ff4f4f63a706f0fb6d8fa97c10f5828bf3
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-14 15:59:11 +0200

    feat: added feature gate test

---
+++ tests/ui/feature-gates/feature-gate-const_exact_size_iterator.rs
@@ -0,0 +1,17 @@
+#![feature(const_trait_impl)]
+#![feature(const_iter)]
+
+
+fn main() {}
+
+
+struct MyType;
+
+const impl Iterator for MyType {
+    type Item = ();
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+const impl ExactSizeIterator for MyType {}
