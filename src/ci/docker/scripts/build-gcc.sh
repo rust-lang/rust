0795af6b0cc547a4075b6f6b410a307d606e2af2
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-08-15 01:37:43 +0200

    Remove binutils installation to ensure it fails

--- src/ci/docker/scripts/build-gcc.sh
+++ src/ci/docker/scripts/build-gcc.sh
@@ -4,25 +4,6 @@ set -eux
 
 source shared.sh
 
-BINUTILS="2.47"
-curl https://ci-mirrors.rust-lang.org/rustc/gcc/binutils-$BINUTILS.tar.xz | xzcat | tar xf -
-mkdir binutils-build
-cd binutils-build
-hide_output ../binutils-$BINUTILS/configure --prefix=/rustroot
-hide_output make -j$(nproc)
-hide_output make install
-
-cd ..
-rm -rf binutils-build binutils-$BINUTILS
-
-if echo '.section .test,"awR",@progbits' | as - -o /dev/null 2>/dev/null; then
-    echo "binutils assembler supports SHF_GNU_RETAIN"
-else
-    echo "binutils assembler DOES NOT support SHF_GNU_RETAIN"
-    exit 1
-fi
-
-
 # Note: in the future when bumping to version 10.1.0, also take care of the sed block below.
 # This version is specified in the Dockerfile
 GCC=$GCC_VERSION
