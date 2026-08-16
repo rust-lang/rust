60b5f88cce0137355b4dba9deec2c82024239090
Ralf Jung <post@ralfj.de>
2026-08-16 10:59:10 +0200

    add another movable-generator regression test

--- src/tools/miri/tests/pass/generators.rs
+++ src/tools/miri/tests/pass/generators.rs
@@ -1,10 +1,15 @@
 //@edition: 2024
+//@revisions: stack tree tree_implicit_writes
+//@[tree_implicit_writes]compile-flags: -Zmiri-tree-borrows -Zmiri-tree-borrows-implicit-writes
+//@[tree]compile-flags: -Zmiri-tree-borrows
+
 #![feature(gen_blocks)]
 
 fn main() {
     basic();
     iterate();
     movable_gen();
+    movable_gen2();
 }
 
 fn basic() {
@@ -77,4 +82,34 @@ pub fn movable_gen() {
     assert_eq!(i.next(), Some(1));
     let mut j = i;
     assert_eq!(j.next(), None);
+}
+
+/// Regression test for <https://github.com/rust-lang/rust/issues/161026>.
+fn movable_gen2() {
+    // a struct that has a drop flag and contains a reference
+    struct DropMut<T: 'static>(&'static mut T);
+    impl<T: 'static> Drop for DropMut<T> {
+        fn drop(&mut self) {
+            drop(unsafe { Box::from_raw(self.0) });
+        }
+    }
+
+    let mut a = gen {
+        let b = DropMut(Box::leak(Box::new(1)));
+
+        // create a drop flag on `b`
+        let c;
+        if true {
+            c = b; // and ensure it's set to false
+        } else {
+            c = DropMut(Box::leak(Box::new(2)));
+        }
+
+        *c.0 = 3;
+        4.yield;
+        *c.0 = 5;
+    };
+    let _ = a.next();
+    let mut d = a;
+    let _ = d.next();
 }
