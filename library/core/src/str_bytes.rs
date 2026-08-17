ec2d2035b5f35c91b48281f15fbc8720153e30b5
Mikhail Baykov <manpacket@gmail.com>
2026-08-17 18:51:41 -0400

    core: skip emitting rejects if result doesn't cary it

--- library/core/src/str_bytes.rs
+++ library/core/src/str_bytes.rs
@@ -1716,8 +1716,10 @@ impl TwoWaySearcher {
             };
 
             if old_pos != self.position {
-                if let Some(ret) = R::rejecting(old_pos, self.position) {
-                    return ret;
+                if R::HAS_REJECTS {
+                    if let Some(ret) = R::rejecting(old_pos, self.position) {
+                        return ret;
+                    }
                 }
             }
 
@@ -1832,8 +1834,10 @@ impl TwoWaySearcher {
             };
 
             if old_end != self.end {
-                if let Some(ret) = R::rejecting(self.end, old_end) {
-                    return ret;
+                if R::HAS_REJECTS {
+                    if let Some(ret) = R::rejecting(self.end, old_end) {
+                        return ret;
+                    }
                 }
             }
 
