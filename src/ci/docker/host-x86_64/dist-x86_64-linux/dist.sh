6763ffb03158a653c24ad238e69540149d6106d3
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-08-18 11:59:14 +0200

    Move binutils new version download into `dist.sh`

--- src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
@@ -16,6 +16,29 @@ python3 ../x.py build --set rust.debug=true opt-dist
 # Use GCC for building GCC components, as it seems to behave badly when built with Clang
 # Only build GCC on full builds, not try builds
 if [ "${DIST_TRY_BUILD:-0}" == "0" ]; then
+    # We have to build our own binutils for the GCC build, because the default CentOS 7 binutils are
+    # too old, and they do not support `SHF_GNU_RETAIN`.
+    BINUTILS="2.47"
+    BINUTILS_ROOT_PATH="/binutils-install"
+    export BINUTILS_PATH="$BINUTILS_ROOT_PATH/bin"
+    curl https://ci-mirrors.rust-lang.org/rustc/gcc/binutils-$BINUTILS.tar.xz | xzcat | tar xf -
+    mkdir binutils-build
+    mkdir "$BINUTILS_ROOT_PATH"
+    cd binutils-build
+    hide_output ../binutils-$BINUTILS/configure --prefix="$BINUTILS_ROOT_PATH"
+    hide_output make -j$(nproc)
+    hide_output make install
+
+    cd ..
+    rm -rf binutils-build binutils-$BINUTILS
+
+    if echo '.section .test,"awR",@progbits' | "$BINUTILS_PATH"/as - -o /dev/null 2>/dev/null; then
+        echo "binutils assembler supports SHF_GNU_RETAIN"
+    else
+        echo "binutils assembler DOES NOT support SHF_GNU_RETAIN"
+        exit 1
+    fi
+
     PATH="$BINUTILS_PATH":$PATH CC=/rustroot/bin/cc CXX=/rustroot/bin/c++ python3 ../x.py dist \
       gcc-dev \
       gcc
