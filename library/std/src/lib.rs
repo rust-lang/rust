159d36c0d25be2bbb8333d4f494b7df7ec8015d3
Chris Denton <chris@chrisdenton.dev>
2026-08-19 02:41:43 +0000

    Allow lints on backtrace-rs

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -740,7 +740,7 @@ mod panicking;
 
 #[path = "../../backtrace/src/lib.rs"]
 #[allow(dead_code, unused_attributes, implicit_provenance_casts, unsafe_op_in_unsafe_fn)]
-#[allow(clippy::len_zero, clippy::needless_borrow)] // FIXME
+#[allow(clippy::len_zero, clippy::needless_borrow, clippy::filter_map_next)] // FIXME
 mod backtrace_rs;
 
 #[stable(feature = "cfg_select", since = "1.95.0")]
