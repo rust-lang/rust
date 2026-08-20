f541885f9474b11981ab16c75c6a8232d49a7070
James Barford-Evans <james.barford-evans@arm.com>
2026-08-20 14:11:54 +0100

    cfg test on `target_has_reliable_f16b`

--- tests/ui/float/f16b.rs
+++ tests/ui/float/f16b.rs
@@ -1,9 +1,7 @@
-//@ ignore-s390x
-//@ ignore-wasm
-//@ ignore-backends: gcc
+//@ compile-flags: --check-cfg=cfg(target_has_reliable_f16b)
 //@ run-pass
 
-#![feature(f16b)]
+#![feature(f16b, cfg_target_has_reliable_f16b)]
 
 extern crate core;
 
@@ -20,21 +18,23 @@ where
 }
 
 fn main() {
-    assert_traits::<f16b>();
+    if cfg!(target_has_reliable_f16b) {
+        assert_traits::<f16b>();
 
-    assert_eq!(size_of::<f16b>(), 2);
-    assert_eq!(align_of::<f16b>(), 2);
-    assert_eq!(f16b::default().to_bits(), 0);
-    assert_eq!(ONE_BITS, 0x3f80);
-    assert_eq!(f32::from(ONE).to_bits(), 0x3f80_0000);
+        assert_eq!(size_of::<f16b>(), 2);
+        assert_eq!(align_of::<f16b>(), 2);
+        assert_eq!(f16b::default().to_bits(), 0);
+        assert_eq!(ONE_BITS, 0x3f80);
+        assert_eq!(f32::from(ONE).to_bits(), 0x3f80_0000);
 
-    let two = f16b::from_bits(0x4000);
-    let negative_zero = f16b::from_bits(0x8000);
-    let nan = f16b::from_bits(0x7fc0);
-    assert!(ONE < two);
-    assert_eq!(f16b::from_bits(0), negative_zero);
-    assert!(nan != nan);
-    assert_eq!(format!("{ONE:?}"), "1.0");
-    assert_eq!(format!("{ONE:e}"), "1e0");
-    assert_eq!(format!("{ONE:E}"), "1E0");
+        let two = f16b::from_bits(0x4000);
+        let negative_zero = f16b::from_bits(0x8000);
+        let nan = f16b::from_bits(0x7fc0);
+        assert!(ONE < two);
+        assert_eq!(f16b::from_bits(0), negative_zero);
+        assert!(nan != nan);
+        assert_eq!(format!("{ONE:?}"), "1.0");
+        assert_eq!(format!("{ONE:e}"), "1e0");
+        assert_eq!(format!("{ONE:E}"), "1E0");
+    }
 }
