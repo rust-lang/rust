cddac3fae71643436ef72deb10c4ac89c8bad2c7
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-08-20 17:16:23 +0200

    Add gcc support for retain attribute check

--- src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
@@ -18,9 +18,20 @@ python3 ../x.py build --set rust.debug=true opt-dist
 if [ "${DIST_TRY_BUILD:-0}" == "0" ]; then
     # We add the binutils binary path to ensure that it uses the 2.47 version of binutils,
     # which is needed for the `retain` attribute feature.
-    PATH="/tmp/binutils-install/bin:$PATH" \
-        CC=/rustroot/bin/cc CXX=/rustroot/bin/c++ \
+    export PATH="/tmp/binutils-install/bin:$PATH"
+
+    CC=/rustroot/bin/cc CXX=/rustroot/bin/c++ \
         python3 ../x.py dist \
         gcc-dev \
         gcc
+
+    gcc_path="./build/$HOSTS/gcc/$HOSTS/install/bin/gcc"
+    if echo 'int x __attribute__((used, retain));' | "$gcc_path" -S -x c -o - - | grep -i '"a.*R"'; then
+        echo "retain attribute is supported"
+    else
+        echo "retain attribute is not supported"
+        # We display the generated asm just in case...
+        echo 'int x __attribute__((used, retain));' | "$gcc_path" -S -x c -o - -
+        exit 1
+    fi
 fi
