0e38313b5d7f1b36637d9787adc7b21d21114452
Jakub Chlanda <jchlanda@accesssoftek.com>
2026-08-20 06:47:34 +0000

    [PAC] Use Option<u64> in FnAbi's discriminator field

--- compiler/rustc_target/src/callconv/mod.rs
+++ compiler/rustc_target/src/callconv/mod.rs
@@ -701,7 +701,7 @@ pub struct FnAbi<'a, Ty> {
     /// Indicates if an unwind may happen across a call to this function.
     pub can_unwind: bool,
     /// Computed type discriminator for pointer authentication purpose.
-    pub ptrauth_discriminator: u64,
+    pub ptrauth_discriminator: Option<u64>,
 }
 
 // Needs to be a custom impl because of the bounds on the `TyAndLayout` debug impl.
@@ -1042,6 +1042,6 @@ mod size_asserts {
     use super::*;
     // tidy-alphabetical-start
     static_assert_size!(ArgAbi<'_, usize>, 64);
-    static_assert_size!(FnAbi<'_, usize>, 96);
+    static_assert_size!(FnAbi<'_, usize>, 104);
     // tidy-alphabetical-end
 }
