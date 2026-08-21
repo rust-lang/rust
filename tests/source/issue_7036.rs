e9b0f1ec52a9a90296a58528be27cf60e8af588f
AsthaMishra <mishraastha211@gmail.com>
2026-08-21 23:19:28 +0000

    Inconsistent formatting of doc comments in macros

---
+++ tests/source/issue_7036.rs
@@ -0,0 +1,21 @@
+// Doc comments inside macro calls should not prevent formatting.
+
+foo!(
+    /// Doc
+    A,
+      B,
+);
+
+foo!(
+    #[doc = ""]
+    /// Doc
+    A,
+      B,
+);
+
+foo!(
+    /// Doc
+    #[doc = ""]
+    A,
+      B,
+);
