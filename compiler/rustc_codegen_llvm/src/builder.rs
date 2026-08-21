7d0d2731eef519190d63231bcfe8e8a4e0fe3907
Antoni Boucher <bouanto@zoho.com>
2026-08-21 08:13:30 -0400

    Add missing direct return arguments

--- compiler/rustc_codegen_llvm/src/builder.rs
+++ compiler/rustc_codegen_llvm/src/builder.rs
@@ -2121,6 +2121,7 @@ impl<'a, 'll, 'tcx> Builder<'a, 'll, 'tcx> {
                     None,
                     None,
                     ubsan_handler,
+                    ReturnSlot::Direct,
                     &[diag_data, function_address, self.const_usize(0)],
                     None,
                     None,
