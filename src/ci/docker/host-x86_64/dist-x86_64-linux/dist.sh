2ad19ced642a53a547f2b137049c1b169be805d2
Ralf Jung <post@ralfj.de>
2026-08-22 12:18:44 +0200

    Revert "Auto merge of #161243 - GuillaumeGomez:binutils-install, r=Kobzol"
    This reverts commit b66219142c937562f2ff8e083be2a2b25b20199e, reversing
    changes made to 526c36b4c477994b7905ec6a084d6150e49076f5.

--- src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
+++ src/ci/docker/host-x86_64/dist-x86_64-linux/dist.sh
@@ -16,23 +16,7 @@ python3 ../x.py build --set rust.debug=true opt-dist
 # Use GCC for building GCC components, as it seems to behave badly when built with Clang
 # Only build GCC on full builds, not try builds
 if [ "${DIST_TRY_BUILD:-0}" == "0" ]; then
-    # We add the binutils binary path to ensure that it uses the 2.47 version of binutils,
-    # which is needed for the `retain` attribute feature.
-    export PATH="/tmp/binutils-install/bin:$PATH"
-
-    CC=/rustroot/bin/cc CXX=/rustroot/bin/c++ \
-        python3 ../x.py dist \
-        gcc-dev \
-        gcc
-
-    gcc_path="./build/$HOSTS/gcc/$HOSTS/install/bin/gcc"
-    if echo 'int x __attribute__((used, retain));' | \
-            "$gcc_path" -S -x c -o - - | grep -i '"a.*R"'; then
-        echo "retain attribute is supported"
-    else
-        echo "retain attribute is not supported"
-        # We display the generated asm just in case...
-        echo 'int x __attribute__((used, retain));' | "$gcc_path" -S -x c -o - -
-        exit 1
-    fi
+    CC=/rustroot/bin/cc CXX=/rustroot/bin/c++ python3 ../x.py dist \
+      gcc-dev \
+      gcc
 fi
