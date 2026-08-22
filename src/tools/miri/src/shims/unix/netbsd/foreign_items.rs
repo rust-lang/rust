8b32975a38e5fa83bbdd0ccf3e3ec3216e94ffab
Ralf Jung <post@ralfj.de>
2026-08-22 17:48:08 +0200

    support a few more basic things on netbsd

---
+++ src/tools/miri/src/shims/unix/netbsd/foreign_items.rs
@@ -0,0 +1,51 @@
+use rustc_middle::ty::Ty;
+use rustc_span::Symbol;
+use rustc_target::callconv::FnAbi;
+
+use crate::shims::unix::*;
+use crate::*;
+
+pub fn is_dyn_sym(_name: &str) -> bool {
+    false
+}
+
+impl<'tcx> EvalContextExt<'tcx> for crate::MiriInterpCx<'tcx> {}
+pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
+    fn emulate_foreign_item_inner(
+        &mut self,
+        link_name: Symbol,
+        abi: &FnAbi<'tcx, Ty<'tcx>>,
+        args: &[OpTy<'tcx>],
+        dest: &MPlaceTy<'tcx>,
+    ) -> InterpResult<'tcx, EmulateItemResult> {
+        let this = self.eval_context_mut();
+        match link_name.as_str() {
+            // Environment
+            "__unsetenv13" => {
+                let [name] = this.check_shim_sig(
+                    shim_sig!(extern "C" fn(*const _) -> i32),
+                    link_name,
+                    abi,
+                    args,
+                )?;
+                let result = this.unsetenv(name)?;
+                this.write_scalar(result, dest)?;
+            }
+
+            // Miscellaneous
+            "__errno" => {
+                let [] = this.check_shim_sig(
+                    shim_sig!(extern "C" fn() -> *mut _),
+                    link_name,
+                    abi,
+                    args,
+                )?;
+                let errno_place = this.last_error_place()?;
+                this.write_scalar(errno_place.to_ref(this).to_scalar(), dest)?;
+            }
+
+            _ => return interp_ok(EmulateItemResult::NotSupported),
+        }
+        interp_ok(EmulateItemResult::NeedsReturn)
+    }
+}
