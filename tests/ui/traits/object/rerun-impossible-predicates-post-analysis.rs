5298d0ef75469fd49c0aef0b5f6df385a4e1d9d3
lcnr <rust@lcnr.de>
2026-08-22 21:08:02 +0200

    add test

---
+++ tests/ui/traits/object/rerun-impossible-predicates-post-analysis.rs
@@ -0,0 +1,43 @@
+// run-pass
+//
+// Regression test for #161441. This is a next-solver bug fixed by #158993
+// which affected stable due to `impossible_predicates` already using the next-solver
+// by default.
+
+use std::marker::PhantomData;
+
+struct MyError;
+
+trait StreamingBody {
+    type BodyError;
+}
+struct Body;
+impl StreamingBody for Body {
+    type BodyError = MyError;
+}
+
+trait Service {
+    type Output;
+}
+struct HttpClientService;
+impl Service for HttpClientService {
+    type Output = Body;
+}
+
+trait Trait {
+    fn method(&self);
+}
+impl<F, R, ResBody> Trait for (F, PhantomData<R>)
+where
+    F: Fn() -> R,
+    HttpClientService: Service<Output = ResBody>,
+    ResBody: StreamingBody<BodyError: Sized>,
+{
+    fn method(&self) {}
+}
+
+fn inspect_websocket_message() -> impl Sized {}
+
+fn main() {
+    (&(inspect_websocket_message, PhantomData) as &dyn Trait).method();
+}
