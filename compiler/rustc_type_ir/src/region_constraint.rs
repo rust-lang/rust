dbb76aa8344d51835ee9e8ff219982da70a6339e
Joao Roberto <dnreikronos2@gmail.com>
2026-08-29 15:20:23 -0300

    Handle reflexive solver region constraints

--- compiler/rustc_type_ir/src/region_constraint.rs
+++ compiler/rustc_type_ir/src/region_constraint.rs
@@ -645,6 +645,10 @@ fn pull_region_outlives_constraints_out_of_universe<
                     pulled_constraints.push(Or::new_leaf(c.clone()));
                 }
                 RegionOutlives(region_1, region_2, ()) => {
+                    if region_1 == region_2 {
+                        continue;
+                    }
+
                     let region_1_u = max_universe(infcx, region_1);
                     let region_2_u = max_universe(infcx, region_2);
 
