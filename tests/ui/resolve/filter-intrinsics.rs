a002fca868c8a2efc3f01d7c85c2f76b452811a6
KevinA-cpu <kevinanderson1410@gmail.com>
2026-08-29 13:39:05 +0700

    fix:  ci

--- tests/ui/resolve/filter-intrinsics.rs
+++ tests/ui/resolve/filter-intrinsics.rs
@@ -3,10 +3,10 @@
 
 #![cfg_attr(gate_on, feature(core_intrinsics))]
 fn main() {
+//[gate_on]~^ HELP consider importing this function
     let _ = transmute::<usize>();
     //~^ ERROR cannot find function `transmute` in this scope
 
     let _ = fabs(1.0);
     //~^ ERROR cannot find function `fabs` in this scope
-    //[gate_on]~| HELP consider importing this function
 }
