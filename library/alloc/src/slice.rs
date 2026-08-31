9a4c35ec98557c41c495b823edebf169d0916f63
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2026-08-31 16:15:16 +0300

    Partially revert "Use `drop_guard` in some places in {core,alloc,std}"
    This partially reverts commit 4f404c1f7e1de1ec058a7729568aaa310a063959.

--- library/alloc/src/slice.rs
+++ library/alloc/src/slice.rs
@@ -408,30 +408,35 @@ impl<T> [T] {
         impl<T: Clone> ConvertVec for T {
             #[inline]
             default fn to_vec<A: Allocator>(s: &[Self], alloc: A) -> Vec<Self, A> {
-                use core::mem::DropGuard;
-
-                let mut guard = DropGuard::new(
-                    (0, Vec::with_capacity_in(s.len(), alloc)),
-                    |(num_init, mut vec)| {
+                struct DropGuard<'a, T, A: Allocator> {
+                    vec: &'a mut Vec<T, A>,
+                    num_init: usize,
+                }
+                impl<'a, T, A: Allocator> Drop for DropGuard<'a, T, A> {
+                    #[inline]
+                    fn drop(&mut self) {
                         // SAFETY:
                         // items were marked initialized in the loop below
-                        unsafe { vec.set_len(num_init) }
-                    },
-                );
-                let (num_init, vec) = &mut *guard;
-
-                let slots = vec.spare_capacity_mut();
+                        unsafe {
+                            self.vec.set_len(self.num_init);
+                        }
+                    }
+                }
+                let mut vec = Vec::with_capacity_in(s.len(), alloc);
+                let mut guard = DropGuard { vec: &mut vec, num_init: 0 };
+                let slots = guard.vec.spare_capacity_mut();
                 // .take(slots.len()) is necessary for LLVM to remove bounds checks
                 // and has better codegen than zip.
                 for (i, b) in s.iter().enumerate().take(slots.len()) {
-                    *num_init = i;
+                    guard.num_init = i;
                     slots[i].write(b.clone());
                 }
-
-                let (_, mut vec) = DropGuard::dismiss(guard);
+                core::mem::forget(guard);
                 // SAFETY:
                 // the vec was allocated and initialized above to at least this length.
-                unsafe { vec.set_len(s.len()) };
+                unsafe {
+                    vec.set_len(s.len());
+                }
                 vec
             }
         }
