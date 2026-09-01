fc4c9efa73beef9eb87e6e9fe3944f154c8b175d
Eva van Houten <opensource@evavh.net>
2026-09-01 14:05:36 +0200

    Minimize doc-crate-level test for clarity

--- tests/ui/attributes/doc-crate-level.rs
+++ tests/ui/attributes/doc-crate-level.rs
@@ -1,10 +1,4 @@
 #![feature(rustdoc_internals)]
-#![doc(fake_variadic)]
-//~^ ERROR `#![doc(fake_variadic = "...")]` isn't allowed as a crate-level attribute
-#![doc(alias = "test")]
-//~^ ERROR `#![doc(alias = "...")]` isn't allowed as a crate-level attribute
-#![doc(search_unbox)]
-//~^ ERROR `#![doc(search_unbox = "...")]` isn't allowed as a crate-level attribute
 
 #[doc(rust_logo)]
 //~^ ERROR crate-level attribute should be an inner attribute: add an exclamation mark: `#![doc]`
@@ -24,20 +18,4 @@
 //~^ ERROR crate-level attribute should be an inner attribute: add an exclamation mark: `#![doc]`
 fn function() {}
 
-#![doc(rust_logo)]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(html_favicon_url = "example.org")]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(html_logo_url = "example.org")]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(html_playground_url = "example.org")]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(issue_tracker_base_url = "example.org")]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(html_root_url = "example.org")]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(html_no_source)]
-//~^ ERROR an inner attribute is not permitted in this context
-#![doc(test(no_crate_inject))]
-//~^ ERROR an inner attribute is not permitted in this context
 fn main() {}
