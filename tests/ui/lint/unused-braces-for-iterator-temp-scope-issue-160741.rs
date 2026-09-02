fa18a90a403ad2b5875d1893c49ed44550ac1be7
Yukang <moorekang@gmail.com>
2026-09-02 18:08:57 +0800

    Remove wrong UnusedBraces lint for iterator loop in edition 2024

--- tests/ui/lint/unused-braces-for-iterator-temp-scope-issue-160741.rs
+++ tests/ui/lint/unused-braces-for-iterator-temp-scope-issue-160741.rs
@@ -1,6 +1,8 @@
 //! A block around a Rust 2024 `for` iterator expression may shorten the lifetime of temporaries.
 
-//@ edition: 2024
+//@ revisions: e2021 e2024
+//@[e2021] edition: 2021
+//@[e2024] edition: 2024
 //@ check-pass
 
 #![warn(unused_braces)]
@@ -16,7 +18,7 @@ fn main() {
     let data = Arc::new(Mutex::new(State { values: vec![1, 2, 3], total: 0 }));
 
     for value in { data.lock().unwrap().values.clone() } {
-        //~^ WARN unnecessary braces around `for` iterator expression
+        //[e2021]~^ WARN unnecessary braces around `for` iterator expression
         data.lock().unwrap().total += value;
     }
 }
