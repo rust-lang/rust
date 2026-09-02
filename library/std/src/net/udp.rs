e73ef829f6d3cb036fa69385a150e36a77c8ece9
Niclas Klugmann <niclas@klugmann.de>
2026-09-02 05:15:56 +0200

    don't use v6 for UDP SO_BROADCAST methods

--- library/std/src/net/udp.rs
+++ library/std/src/net/udp.rs
@@ -425,7 +425,7 @@ impl UdpSocket {
     /// ```no_run
     /// use std::net::UdpSocket;
     ///
-    /// let socket = UdpSocket::bind("[::1]:34254").expect("bind should succeed");
+    /// let socket = UdpSocket::bind("127.0.0.1:34254").expect("bind should succeed");
     /// socket.set_broadcast(false).expect("set_broadcast should succeed");
     /// ```
     #[stable(feature = "net2_mutators", since = "1.9.0")]
@@ -442,7 +442,7 @@ impl UdpSocket {
     /// ```no_run
     /// use std::net::UdpSocket;
     ///
-    /// let socket = UdpSocket::bind("[::1]:34254").expect("bind should succeed");
+    /// let socket = UdpSocket::bind("127.0.0.1:34254").expect("bind should succeed");
     /// socket.set_broadcast(false).expect("set_broadcast should succeed");
     /// assert_eq!(socket.broadcast().unwrap(), false);
     /// ```
