2de164566072db2354995fe44ac28968c16f867f
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-09-02 12:30:40 +0200

    Fix more ui tests

--- tests/ui/lint/forbid-error-capped.rs
+++ tests/ui/lint/forbid-error-capped.rs
@@ -1,12 +1,11 @@
-//@ check-pass
-
 // This checks that the forbid attribute checking is ignored when the forbidden
 // lint is capped.
 
+//@ check-pass
+//@ compile-flags: --cap-lints=warn -Fwarnings
+
 #![forbid(warnings)]
 #![allow(unused)]
-//~^ WARN allow(unused) incompatible with previous forbid
-//~| WARN previously accepted
 
 #[allow(unused)]
 mod bar {
