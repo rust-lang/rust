6e05fd8ff42acc015341083bf860b1627a50941d
Niclas Klugmann <niclas@klugmann.de>
2026-09-02 11:23:55 +0200

    remove [::1] comment since its value is not relevant and the meaning is covered by the constant

--- library/core/src/net/socket_addr.rs
+++ library/core/src/net/socket_addr.rs
@@ -204,7 +204,6 @@ impl SocketAddr {
     /// ```
     /// use std::net::{IpAddr, Ipv6Addr, SocketAddr};
     ///
-    /// // [::1]
     /// let mut socket = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 8080);
     /// // [2001:db8::1]
     /// socket.set_ip(IpAddr::V6(Ipv6Addr::new(0x2001, 0xdb8, 0, 0, 0, 0, 0, 1)));
