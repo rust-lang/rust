572f0343a7faf5bb5c78159eeddfca899c67b0bf
Zalathar <Zalathar@users.noreply.github.com>
2026-09-03 21:25:32 +1000

    Add a `#[diagnostic::on_unimplemented(..)]` hint to RefDecodable

--- compiler/rustc_middle/src/ty/codec/ref_decodable.rs
+++ compiler/rustc_middle/src/ty/codec/ref_decodable.rs
@@ -19,6 +19,9 @@ use crate::{mir, traits};
 ///
 /// Implementations of this trait will typically allocate into an arena or interner,
 /// e.g. see `impl_ref_decodable_into_arena!` in [`rustc_middle::arena`].
+#[diagnostic::on_unimplemented(
+    note = "consider adding `{Self}` to the list in `impl_ref_decodable_into_arena!`"
+)]
 pub trait RefDecodable<'tcx, D: TyDecoder<'tcx>>: PointeeSized {
     fn decode(d: &mut D) -> &'tcx Self;
 }
