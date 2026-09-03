25f51e1015ff4a8dac6b3b7afb76d47be4af882d
León Orell Valerian Liehr <me@fmease.dev>
2026-09-03 14:12:44 +0200

    Rename feature `builtin_syntax` to `internal_syntax`

--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -1915,7 +1915,7 @@ impl<'a> Parser<'a> {
         let span = lo.to(self.token.span);
         self.expect(exp!(CloseParen))?;
 
-        self.psess.gated_spans.gate(sym::builtin_syntax, lo.to(self.token.span));
+        self.psess.gated_spans.gate(sym::internal_syntax, lo.to(self.token.span));
 
         Ok(self.mk_expr(span, ExprKind::OffsetOf(container, fields)))
     }
@@ -1929,7 +1929,7 @@ impl<'a> Parser<'a> {
         //        it actually "improves" diagnostics slightly.
         let span = lo.to(self.token.span);
         self.expect(exp!(CloseParen))?;
-        self.psess.gated_spans.gate(sym::builtin_syntax, lo.to(self.token.span));
+        self.psess.gated_spans.gate(sym::internal_syntax, lo.to(self.token.span));
         Ok(self.mk_expr(span, ExprKind::Type(expr, ty)))
     }
 
@@ -1946,7 +1946,7 @@ impl<'a> Parser<'a> {
         //        it actually "improves" diagnostics slightly.
         let span = lo.to(self.token.span);
         self.expect(exp!(CloseParen))?;
-        self.psess.gated_spans.gate(sym::builtin_syntax, lo.to(self.token.span));
+        self.psess.gated_spans.gate(sym::internal_syntax, lo.to(self.token.span));
         Ok(self.mk_expr(span, ExprKind::UnsafeBinderCast(kind, expr, ty)))
     }
 
