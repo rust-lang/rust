2d17a93de657f001d8da6600bc740762f3dbc2f6
León Orell Valerian Liehr <me@fmease.dev>
2026-09-04 00:13:30 +0200

    Simplify recovery from outer attributes on types
    Instead of checking for `#[` before trying to parse outer attributes,
    check for `#` or doc comments instead. This way, we automatically recover
    from leading doc comments and innerattributes on types, too.
    
    Previously, we would only recover from doc comments and inner attributes
    on types if they were preceded by normal outer attributes.
    
    Move the whole recovery into a separate more general function in
    preparation for using it in various other places, too.
    
    This means we drop the recovery from attributes in type contexts that
    aren't ascribed to a valid type (at least until the next sync point).
    For example, `type T = #[a];` and `fn f() -> #[a] {}` are now fatal
    (meaning the parser will only continue parsing from the next sync point).
    
    This could be considered unfortunate but honestly it didn't make much sense:
    Why would we only defatalize syntax errors in type contexts if there are
    preceding attributes (which aren't legal / expected on types in the first
    place, mind you); there's no reason to recover from e.g., `type T = #[a] @;`
    (which used to lead to bad follow-up diagnostics). Defatalizing syntax
    errors isn't inherently bad, it just needs to be done carefully and not
    made dependent on "random" conditions.

--- tests/ui/parser/attribute-on-empty.rs
+++ tests/ui/parser/attribute-on-empty.rs
@@ -1,29 +1,36 @@
-//! Regression test for: <https://github.com/rust-lang/rust/issues/144132>
-//!                      <https://github.com/rust-lang/rust/issues/135017>
+// Exercise outer attributes being applied to "nothing" in invalid contexts.
 
 struct Baz<const N: usize>(i32);
 
-fn main() {
+fn f() {
     let _: Baz<#[cfg(false)]> = todo!();
     //~^ ERROR attributes cannot be applied here
 }
 
-fn f(_param: #[attr]) {}
+fn g(_param: #[attr]) {}
 //~^ ERROR attributes cannot be applied to a function parameter's type
 //~| ERROR expected type, found `)`
 
-fn g() -> #[attr] { 0 }
-//~^ ERROR attributes cannot be applied here
+fn barrier0() {
+    fn f() -> #[attr] { 0 }
+    //~^ ERROR attributes cannot be applied to types
+    //~| ERROR expected type, found `{`
+}
 
 struct S {
     field: #[attr],
-    //~^ ERROR attributes cannot be applied here
-    field1: (#[attr], i32),
-    //~^ ERROR attributes cannot be applied here
+    //~^ ERROR attributes cannot be applied to types
+    //~| ERROR expected type, found `,`
 }
 
-type Tuple = (#[attr], String);
-//~^ ERROR attributes cannot be applied here
+fn barrier1() {
+    type Tuple = (#[attr], String);
+    //~^ ERROR attributes cannot be applied to types
+    //~| ERROR expected type, found `,`
+}
 
 impl #[attr] {}
-//~^ ERROR attributes cannot be applied here
+//~^ ERROR attributes cannot be applied to types
+//~| ERROR expected type, found `{`
+
+fn main() {}
