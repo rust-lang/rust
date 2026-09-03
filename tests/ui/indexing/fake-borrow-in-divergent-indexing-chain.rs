472c1451030a5350cc4319dead0c3d03a2df76ea
dianne <diannes.gm@gmail.com>
2026-09-03 06:08:10 -0700

    schedule fake reads for indexings' fake borrows

--- tests/ui/indexing/fake-borrow-in-divergent-indexing-chain.rs
+++ tests/ui/indexing/fake-borrow-in-divergent-indexing-chain.rs
@@ -1,8 +1,6 @@
 //! Regression test for <https://github.com/rust-lang/rust/issues/161852>: we need to keep fake
 //! borrows on indexed-into slice pointers alive for bounds-checks even if the end of the indexing
 //! chain is unreachable. This prevents bounds-checks from performing out-of-bounds accesses.
-//@ check-pass
-// TODO: this should be check-fail
 
 #![feature(explicit_tail_calls)]
 
@@ -10,7 +8,7 @@ fn main() {
     let mut x: &[&[&[u32]]] = &[&[&[0]]];
     let y: &[&[&[u32]]] = &[];
     x[0][{ x = y; 0 }][{ return; 0 }];
-    // TODO: ERROR: cannot assign `x` in indexing expression
+    //~^ ERROR: cannot assign `x` in indexing expression
 }
 
 // In the following tests, no bounds-checks are reachable after mutating `x`. We keep the fake
@@ -20,14 +18,14 @@ fn always_return_after_mutation() {
     let mut x: &[&[&[u32]]] = &[&[&[0]]];
     let y: &[&[&[u32]]] = &[];
     x[0][{ x = y; return; 0 }];
-    // TODO: ERROR: cannot assign `x` in indexing expression
+    //~^ ERROR: cannot assign `x` in indexing expression
 }
 
 fn always_panic_after_mutation() {
     let mut x: &[&[&[u32]]] = &[&[&[0]]];
     let y: &[&[&[u32]]] = &[];
     x[0][{ x = y; panic!(); 0 }];
-    // TODO: ERROR: cannot assign `x` in indexing expression
+    //~^ ERROR: cannot assign `x` in indexing expression
 }
 
 fn always_break_after_mutation() {
@@ -35,7 +33,7 @@ fn always_break_after_mutation() {
     let y: &[&[&[u32]]] = &[];
     'b: {
         x[0][{ x = y; break 'b; 0 }];
-        // TODO: ERROR: cannot assign `x` in indexing expression
+        //~^ ERROR: cannot assign `x` in indexing expression
     }
 }
 
@@ -44,7 +42,7 @@ fn always_continue_after_mutation() {
     let y: &[&[&[u32]]] = &[];
     loop {
         x[0][{ x = y; continue; 0 }];
-        // TODO: ERROR: cannot assign `x` in indexing expression
+        //~^ ERROR: cannot assign `x` in indexing expression
     }
 }
 
@@ -52,5 +50,5 @@ fn always_become_after_mutation() {
     let mut x: &[&[&[u32]]] = &[&[&[0]]];
     let y: &[&[&[u32]]] = &[];
     x[0][{ x = y; become always_become_after_mutation(); 0 }];
-    // TODO: ERROR: cannot assign `x` in indexing expression
+    //~^ ERROR: cannot assign `x` in indexing expression
 }
