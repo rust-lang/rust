552358cdc471b59b519f1b9ca152c62c0411376b
Flakebi <flakebi@t-online.de>
2026-09-03 09:23:32 +0200

    Properly implement the gpu-kernel ABI for amdgpu
    Add support to pass structs, arrays and vectors to amdgpu kernels.
    Scalars and vectors are taken by value, aggregates are passed by byref
    pointers. Structs containing a single scalar/vector are handled like
    a scalar.
    
    Judging from clang tests, nvptx seems to do somewhat the same, just
    using byval instead of byref: https://github.com/llvm/llvm-project/blob/3a8affeef4da19d39191aac316e189eca3214a8c/clang/test/CodeGenCUDA/kernel-args.cu
    
    I tested a couple of the lit test signatures on real hardware and it
    seems to work fine. Given the relatively simple implementation, I hope
    this amount of testing is enough (the C calling convention seems like
    a worse fit for Rust’s current ABI code, it’s still giving me headaches).

--- tests/codegen-llvm/amdgpu-abi/struct-abi.rs
+++ tests/codegen-llvm/amdgpu-abi/struct-abi.rs
@@ -78,56 +78,56 @@ pub struct DifferentSizeTypePair {
     i: i32,
 }
 
-// CHECK: define amdgpu_kernel void @kernel_single_element_struct_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(4) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_single_element_struct_arg(i32 %0)
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_single_element_struct_arg(_: SingleElementStructArg) {}
 
-// CHECK: define amdgpu_kernel void @kernel_nested_single_element_struct_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(4) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_nested_single_element_struct_arg(i32 %0)
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_nested_single_element_struct_arg(
     _: NestedSingleElementStructArg,
 ) {
 }
 
-// CHECK: define amdgpu_kernel void @kernel_struct_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(12) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_struct_arg(ptr addrspace(4) noalias nofree noundef readnone byref([12 x i8]) align 4 captures(none) dereferenceable(12) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_struct_arg(_: StructArg) {}
 
-// CHECK: define amdgpu_kernel void @kernel_struct_padding_arg(i8 noundef {{%.+}}, i64 noundef {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_struct_padding_arg(ptr addrspace(4) noalias nofree noundef readnone byref([16 x i8]) align 8 captures(none) dereferenceable(16) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_struct_padding_arg(_: StructPaddingArg) {}
 
-// CHECK: define amdgpu_kernel void @kernel_struct_of_arrays_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(44) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_struct_of_arrays_arg(ptr addrspace(4) noalias nofree noundef readnone byref([44 x i8]) align 4 captures(none) dereferenceable(44) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_struct_of_arrays_arg(_: StructOfArraysArg) {}
 
-// CHECK: define amdgpu_kernel void @kernel_struct_of_structs_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(24) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_struct_of_structs_arg(ptr addrspace(4) noalias nofree noundef readnone byref([24 x i8]) align 4 captures(none) dereferenceable(24) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_struct_of_structs_arg(_: StructOfStructsArg) {}
 
-// CHECK: define amdgpu_kernel void @test_kernel_union_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(4) {{%.+}})
+// CHECK: define amdgpu_kernel void @test_kernel_union_arg(ptr addrspace(4) noalias nofree noundef readnone byref([4 x i8]) align 4 captures(none) dereferenceable(4) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn test_kernel_union_arg(_: U) {}
 
-// CHECK: define amdgpu_kernel void @kernel_single_array_element_struct_arg(ptr noalias nofree noundef readnone align 4 captures(none) dead_on_return dereferenceable(16) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_single_array_element_struct_arg(ptr addrspace(4) noalias nofree noundef readnone byref([16 x i8]) align 4 captures(none) dereferenceable(16) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_single_array_element_struct_arg(_: SingleArrayElementStructArg) {}
 
-// CHECK: define amdgpu_kernel void @kernel_single_struct_element_struct_arg(i32 noundef {{%.+}}, i64 noundef {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_single_struct_element_struct_arg(ptr addrspace(4) noalias nofree noundef readnone byref([16 x i8]) align 8 captures(none) dereferenceable(16) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_single_struct_element_struct_arg(
     _: SingleStructElementStructArg,
 ) {
 }
 
-// CHECK: define amdgpu_kernel void @kernel_different_size_type_pair_arg(i64 noundef {{%.+}}, i32 noundef {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_different_size_type_pair_arg(ptr addrspace(4) noalias nofree noundef readnone byref([16 x i8]) align 8 captures(none) dereferenceable(16) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_different_size_type_pair_arg(_: DifferentSizeTypePair) {}
 
-// CHECK: define amdgpu_kernel void @kernel_complex(float noundef {{%.+}}, float noundef {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_complex(ptr addrspace(4) noalias nofree noundef readnone byref([8 x i8]) align 4 captures(none) dereferenceable(8) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_complex(_: Complex<f32>) {}
 
-// CHECK: define amdgpu_kernel void @kernel_slice(ptr noalias nofree noundef nonnull readonly align 4 captures(none) {{%.+}}, i64 noundef range(i64 0, 2305843009213693952) {{%.+}})
+// CHECK: define amdgpu_kernel void @kernel_slice(ptr addrspace(4) noalias nofree noundef readnone byref([16 x i8]) align 8 captures(none) dereferenceable(16) {{%.+}})
 #[no_mangle]
 pub extern "gpu-kernel" fn kernel_slice(_: &[u32]) {}
