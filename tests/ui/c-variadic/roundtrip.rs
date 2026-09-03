3d281d48815e6213ad9911e97d5d8d59d2348b1a
Folkert de Vries <folkert@folkertdev.nl>
2026-09-03 21:25:04 +0200

    implement `VaArgSafe` for `f128`

--- tests/ui/c-variadic/roundtrip.rs
+++ tests/ui/c-variadic/roundtrip.rs
@@ -5,7 +5,8 @@
     c_variadic_va_arg_safe,
     c_variadic_int128,
     const_destruct,
-    const_raw_ptr_comparison
+    const_raw_ptr_comparison,
+    f128
 )]
 #![allow(unused_features)] // c_variadic_int128 is only used on 64-bit targets.
 
@@ -113,7 +114,38 @@ fn main() {
                 roundtrip!(i128, -1, -2);
                 roundtrip!(u128, 1, 2);
             }
-            _ => {}
+            _ => { /* unsupported */ }
+        }
+
+        cfg_select! {
+            any(
+                all(
+                    any(target_arch = "x86_64", target_arch = "x86"),
+                    not(target_vendor = "apple"),
+                    not(target_env = "msvc")
+                ),
+                all(target_arch = "powerpc64", target_feature = "vsx"),
+                all(
+                    not(windows),
+                    not(target_vendor = "apple"),
+                    any(
+                        target_arch = "aarch64",
+                        target_arch = "loongarch32",
+                        target_arch = "loongarch64",
+                        target_arch = "mips64",
+                        target_arch = "mips64r6",
+                        target_arch = "riscv64",
+                        target_arch = "s390x",
+                        target_arch = "sparc",
+                        target_arch = "sparc64",
+                        target_arch = "wasm32",
+                        target_arch = "wasm64",
+                    ),
+                ),
+            ) => {
+                roundtrip!(f128, -1.0, f128::MAX);
+            }
+            _ => { /* unsupported */ }
         }
     }
 }
