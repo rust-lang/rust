6e8947a4a5e790d360cc561167b1b84f0eaa288a
Ralf Jung <post@ralfj.de>
2026-09-04 16:14:36 +0200

    leave a comment for some sve hacks

--- compiler/rustc_codegen_ssa/src/mir/place.rs
+++ compiler/rustc_codegen_ssa/src/mir/place.rs
@@ -112,7 +112,8 @@ impl<'a, 'tcx, V: CodegenObject> PlaceRef<'tcx, V> {
         bx: &mut Bx,
         layout: TyAndLayout<'tcx>,
     ) -> Self {
-        // Scalable vector are never 1-ZST. FIXME: is that correct?
+        // FIXME(rustc_scalable_vector/stdarch_aarch64_sve): Scalable vectors aren't actually sized,
+        // but we pretend they are. Here we have to hack around that.
         if layout.peel_transparent_wrappers_from_non_1zst(bx).deref().is_scalable_vector() {
             Self::alloca_scalable(bx, layout)
         } else {
@@ -160,7 +161,8 @@ impl<'a, 'tcx, V: CodegenObject> PlaceRef<'tcx, V> {
         layout: TyAndLayout<'tcx>,
     ) -> Self {
         PlaceValue::new_sized(
-            // FIXME why is this peeling at all? And why is it redoing the work the caller just did?
+            // FIXME why is this peeling at all? The LLVM type should be the same for the
+            // transparent wrapper and the inner type.
             bx.alloca_with_ty(layout.peel_transparent_wrappers_from_non_1zst(bx)),
             layout.align.abi,
         )
