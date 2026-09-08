9e5d24801ad6c3cc8e5b3bf64614d569dc945134
Trevor Gross <tg@trevorgross.com>
2026-09-08 01:18:40 -0400

    build: Add a disabled assertion for thumb mode config
    We still match on target name because we can't always rely on config
    from the unstable `arm_target_feature`. However, we can still assert
    that things match up when running in the compiler-builtins CI.
    
    This is disabled for now because it fails on three thumb targets:
    
    * thumbv4t-none-eabi
    * thumbv5te-none-eabi
    * thumbv6-none-eabi
    
    This will be resolved in a future commit.
    
    Checking for the verbose build is a good indicator that we are in our
    CI, rather than any other that might set the `CI` env.

--- library/compiler-builtins/libm/configure.rs
+++ library/compiler-builtins/libm/configure.rs
@@ -28,12 +28,14 @@ pub struct Config {
     pub target_features: Vec<String>,
     pub reliable_f128: bool,
     pub reliable_f16: bool,
+    pub verbose_build: bool,
 }
 
 impl Config {
     pub fn from_env(library: Library) -> Self {
         println!("cargo:rerun-if-env-changed=LIBM_BUILD_VERBOSE");
-        if env_flag("LIBM_BUILD_VERBOSE") {
+        let verbose_build = env_flag("LIBM_BUILD_VERBOSE");
+        if verbose_build {
             VERBOSE_BUILD.store(true, Relaxed);
         }
 
@@ -49,7 +51,7 @@ impl Config {
             .filter_map(|(name, _value)| name.strip_prefix("CARGO_FEATURE_").map(ToOwned::to_owned))
             .map(|s| s.to_lowercase().replace("_", "-"))
             .collect();
-        if VERBOSE_BUILD.load(Relaxed) {
+        if verbose_build {
             for feature in &cargo_features {
                 println!("cargo:warning=feature `{feature}` enabled");
             }
@@ -76,6 +78,7 @@ impl Config {
             // with `RUSTC_BOOTSTRAP=1` (which is required to use the types anyway).
             reliable_f128: env::var_os("CARGO_CFG_TARGET_HAS_RELIABLE_F128").is_some(),
             reliable_f16: env::var_os("CARGO_CFG_TARGET_HAS_RELIABLE_F16").is_some(),
+            verbose_build,
         }
     }
 
@@ -125,6 +128,23 @@ pub fn emit(cfg: &Config) {
 
     // If set, enable `no-panic` for `libm`. Requires LTO (`release-opt` profile).
     let assert_no_panic = env_flag("ENSURE_NO_PANIC");
+
+    // Ensure that thumb is set when expected. We match on target name rather than using target
+    // features directly, since `arm_target_feature` is unfortunately not yet stable. If any
+    // target features are present, we are running on nightly and can do these checks.
+    /* FIXME: fails on targets such as thumbv5te-none-eabi
+    if cfg.verbose_build && !cfg.target_features.is_empty() {
+        if thumb {
+            assert!(cfg.has_target_feature("thumb-mode"));
+        }
+        if thumb1_only {
+            assert!(cfg.has_target_feature("thumb-mode"));
+            assert!(!cfg.has_target_feature("thumb2"));
+        } else if thumb {
+            assert!(cfg.has_target_feature("thumb2"));
+        }
+    }
+    */
 
     // Arch shorthand config is used in most crates.
     set_cfg("thumb", thumb);
