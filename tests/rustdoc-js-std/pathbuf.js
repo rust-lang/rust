937203f68f6a44dbf0f6a0c7ec611409af6c3e0c
Michael Howell <michael@notriddle.com>
2026-09-09 08:11:23 -0700

    Add comment to pathbuf.js

--- tests/rustdoc-js-std/pathbuf.js
+++ tests/rustdoc-js-std/pathbuf.js
@@ -1,3 +1,9 @@
+// The PathBuf type is defined in std,
+// but used in proc_macro. This means both crates'
+// search indexes contain TypeData for it,
+// but only std defines EntryData.
+// This test case ensures we can merge them.
+
 const EXPECTED = [
     {
         query: 'PathBuf',
