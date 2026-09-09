641620ef567e79fcb021987e6689a11b75f5493f
panstromek <panstromek@seznam.cz>
2026-09-09 14:20:12 +0200

    fix: index out of bounds

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -2834,9 +2834,9 @@ impl Interner {
 
         for v in values {
             match indices.entry(
-                hasher.hash_one(&v[0..HASH_UP_TO]),
+                hasher.hash_one(&v[0..HASH_UP_TO.min(v.len())]),
                 |&(s, _)| s == v,
-                |&(s, _)| hasher.hash_one(&s[0..HASH_UP_TO]),
+                |&(s, _)| hasher.hash_one(&s[0..HASH_UP_TO.min(s.len())]),
             ) {
                 Entry::Occupied(v) => conflicting_values.push(v.get().0),
                 Entry::Vacant(view) => {
@@ -2867,13 +2867,13 @@ impl Interner {
     #[inline]
     fn intern_inner(&self, byte_str: &[u8]) -> u32 {
         let hasher = FxBuildHasher::default();
-        let hash_of_byte_str = hasher.hash_one(&byte_str[0..HASH_UP_TO]);
+        let hash_of_byte_str = hasher.hash_one(&byte_str[0..HASH_UP_TO.min(byte_str.len())]);
 
         self.0.with_lock(|inner| {
             match inner.indices.entry(
                 hash_of_byte_str,
                 |&(s, _)| s == byte_str,
-                |&(s, _)| hasher.hash_one(&s[0..HASH_UP_TO]),
+                |&(s, _)| hasher.hash_one(&s[0..HASH_UP_TO.min(s.len())]),
             ) {
                 Entry::Occupied(v) => v.get().1,
                 Entry::Vacant(view) => {
