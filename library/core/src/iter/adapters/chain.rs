e6beace3ab63913f5a653b0bc1e62c5a05f2e699
Jakub Beránek <berykubik@gmail.com>
2026-09-09 08:32:31 +0200

    Implement `ExactSizeIterator` for `Chain`

--- library/core/src/iter/adapters/chain.rs
+++ library/core/src/iter/adapters/chain.rs
@@ -304,6 +304,13 @@ where
 {
 }
 
+impl<A, B> ExactSizeIterator for Chain<A, B>
+where
+    A: ExactSizeIterator,
+    B: ExactSizeIterator<Item = A::Item>,
+{
+}
+
 #[stable(feature = "default_iters", since = "1.70.0")]
 impl<A: Default, B: Default> Default for Chain<A, B> {
     /// Creates a `Chain` from the default values for `A` and `B`.
