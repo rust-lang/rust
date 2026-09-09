5a114dd99bc3fe164b9d8eff51e7e8f902105ead
panstromek <panstromek@seznam.cz>
2026-09-09 13:23:17 +0200

    disable mir inlining for PGO instrumented build

--- src/bootstrap/src/core/builder/cargo.rs
+++ src/bootstrap/src/core/builder/cargo.rs
@@ -1543,6 +1543,7 @@ pub(crate) fn apply_pgo(
             // Apparently necessary to avoid overflowing the counters during
             // a Cargo build profile
             cargo.rustflag("-Cllvm-args=-vp-counters-per-site=4");
+            cargo.rustflag("-Zinline-mir=no");
             true
         } else {
             false
