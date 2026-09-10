1adccf221ff9c6c507354395f255aa973c5a3693
León Orell Valerian Liehr <me@fmease.dev>
2026-09-10 10:59:21 +0200

    Inline fns & data types related to parse error recovery from C-style inc/dec ops

--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -470,7 +470,13 @@ impl<'a> Parser<'a> {
                 this.bump();
 
                 let operand = this.parse_expr_dot_or_call(attrs)?;
-                return Err(this.recover_from_prefix_increment(&operand, pre_span, starts_stmt));
+                return Err(this.report_inc_dec_op(
+                    &operand,
+                    starts_stmt,
+                    diagnostics::IncOrDec::Inc,
+                    diagnostics::UnaryFixity::Pre,
+                    pre_span,
+                ));
             }
             token::Ident(..)
                 if this.token.is_keyword(kw::Move)
