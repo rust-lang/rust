6e683158ba3e878ac30d1de9c0267c578300d505
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-09-10 12:34:29 +0200

    Use File::*lock*() instead of manual flock calls

--- compiler/rustc_data_structures/src/flock/linux.rs
+++ compiler/rustc_data_structures/src/flock/linux.rs
@@ -16,13 +16,14 @@ impl Lock {
     pub fn new(p: &Path, wait: bool, create: bool, exclusive: bool) -> io::Result<Lock> {
         let file = OpenOptions::new().read(true).write(true).create(create).mode(0o600).open(p)?;
 
-        let mut operation = if exclusive { libc::LOCK_EX } else { libc::LOCK_SH };
-        if !wait {
-            operation |= libc::LOCK_NB
-        }
+        match (wait, exclusive) {
+            (true, true) => file.lock()?,
+            (true, false) => file.lock_shared()?,
+            (false, true) => file.try_lock()?,
+            (false, false) => file.try_lock_shared()?,
+        };
 
-        let ret = unsafe { libc::flock(file.as_raw_fd(), operation) };
-        if ret == -1 { Err(io::Error::last_os_error()) } else { Ok(Lock { _file: file }) }
+        Ok(Lock { _file: file })
     }
 
     pub fn error_unsupported(err: &io::Error) -> bool {
