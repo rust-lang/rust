3a2fd4c03cf9ba64275f5b291025ab74c9d3d3fc
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-09-10 17:58:30 +0200

    Introduce a global lock registry for the fcntl lock fallback
    This lock registry allows emulating fd based locks (like flock()) on top
    of the fcntl(F_SETLK) process based locks, thereby ensuring that
    concurrent rustc instances inside the same process don't break the incr
    comp locking scheme.

--- compiler/rustc_data_structures/src/flock/unix.rs
+++ compiler/rustc_data_structures/src/flock/unix.rs
@@ -1,22 +1,94 @@
 use std::fs::{File, OpenOptions};
 use std::os::unix::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, LazyLock, Mutex};
 use std::{io, mem};
+
+use rustc_hash::FxHashMap;
+
+static LOCK_REGISTRY: LazyLock<Mutex<FxHashMap<PathBuf, LockState>>> =
+    LazyLock::new(|| Mutex::new(FxHashMap::default()));
+
+enum LockState {
+    /// Lock exclusively held. `Lock.file` contains an `Arc<UnlockGuard>` with a single reference.
+    Exclusive,
+    /// Lock can be shared. When there are N lock holders, the `Arc<UnlockGuard>` has N+1 references
+    /// with the last one being held by `LockState` and getting removed in the drop impl of `Lock`
+    /// if it is the remaining reference.
+    Shared(Arc<UnlockGuard>),
+}
 
 #[derive(Debug)]
 pub struct Lock {
-    file: File,
+    path: PathBuf,
+    file: Option<Arc<UnlockGuard>>,
 }
 
 impl Lock {
     pub fn try_lock(p: &Path, create: bool, exclusive: bool) -> io::Result<Lock> {
-        let file = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(create)
-            .mode(libc::S_IRWXU as u32)
-            .open(p)?;
+        let mut locks = LOCK_REGISTRY.lock().unwrap();
 
+        let file = if let Some(state) = locks.get_mut(p) {
+            // We must not open the file again if there is an existing lock to prevent the close
+            // from unlocking the file even when another `Lock` already had the lock held before
+            // this `Lock::try_lock` call.
+            match state {
+                LockState::Exclusive => return Err(io::ErrorKind::WouldBlock.into()),
+                LockState::Shared(file) => {
+                    if exclusive {
+                        return Err(io::ErrorKind::WouldBlock.into());
+                    } else {
+                        file.clone()
+                    }
+                }
+            }
+        } else {
+            let file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(create)
+                .mode(libc::S_IRWXU as u32)
+                .open(p)?;
+            let file = Arc::new(UnlockGuard::try_lock(file, exclusive)?);
+
+            if exclusive {
+                locks.insert(p.to_owned(), LockState::Exclusive);
+            } else {
+                locks.insert(p.to_owned(), LockState::Shared(file.clone()));
+            }
+
+            file
+        };
+
+        Ok(Lock { path: p.to_owned(), file: Some(file) })
+    }
+}
+
+impl Drop for Lock {
+    fn drop(&mut self) {
+        let mut locks = LOCK_REGISTRY.lock().unwrap();
+        self.file.take().unwrap();
+        match locks.get_mut(&self.path).unwrap() {
+            LockState::Exclusive => {
+                locks.remove(&self.path);
+            }
+            LockState::Shared(file) => {
+                if Arc::strong_count(file) == 1 {
+                    locks.remove(&self.path);
+                }
+            }
+        }
+    }
+}
+
+/// A file guard which will unlock the file when dropped.
+#[derive(Debug)]
+struct UnlockGuard {
+    file: File,
+}
+
+impl UnlockGuard {
+    fn try_lock(file: File, exclusive: bool) -> io::Result<Self> {
         let lock_type = if exclusive { libc::F_WRLCK } else { libc::F_RDLCK };
 
         let mut flock: libc::flock = unsafe { mem::zeroed() };
@@ -35,11 +107,11 @@ impl Lock {
 
         let cmd = libc::F_SETLK;
         let ret = unsafe { libc::fcntl(file.as_raw_fd(), cmd, &flock) };
-        if ret == -1 { Err(io::Error::last_os_error()) } else { Ok(Lock { file }) }
+        if ret == -1 { Err(io::Error::last_os_error()) } else { Ok(Self { file }) }
     }
 }
 
-impl Drop for Lock {
+impl Drop for UnlockGuard {
     fn drop(&mut self) {
         let mut flock: libc::flock = unsafe { mem::zeroed() };
         #[cfg(not(all(target_os = "hurd", target_arch = "x86")))]
