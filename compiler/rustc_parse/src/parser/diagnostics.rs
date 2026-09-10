28af0940b3bbf361f4745e9d9ff45db1abd86963
León Orell Valerian Liehr <me@fmease.dev>
2026-09-10 13:25:49 +0200

    Don't needlessly pass the operand through some recovery functions by value
    These functions didn't actually modifiy the operand or return a new or
    different expression. So essentially the "`fn(Box<Expr>) -> Box<Expr>`
    part" was an identity function. Just change it to "fn(&Expr)".

--- compiler/rustc_parse/src/parser/diagnostics.rs
+++ compiler/rustc_parse/src/parser/diagnostics.rs
@@ -1671,10 +1671,10 @@ impl<'a> Parser<'a> {
 
     pub(super) fn recover_from_prefix_increment(
         &mut self,
-        operand_expr: Box<Expr>,
+        operand_expr: &Expr,
         op_span: Span,
         start_stmt: bool,
-    ) -> PResult<'a, Box<Expr>> {
+    ) -> PResult<'a, ()> {
         let standalone = if start_stmt { IsStandalone::Standalone } else { IsStandalone::Subexpr };
         let kind = IncDecRecovery { standalone, op: IncOrDec::Inc, fixity: UnaryFixity::Pre };
         self.recover_from_inc_dec(operand_expr, kind, op_span)
@@ -1682,10 +1682,10 @@ impl<'a> Parser<'a> {
 
     pub(super) fn recover_from_postfix_increment(
         &mut self,
-        operand_expr: Box<Expr>,
+        operand_expr: &Expr,
         op_span: Span,
         start_stmt: bool,
-    ) -> PResult<'a, Box<Expr>> {
+    ) -> PResult<'a, ()> {
         let kind = IncDecRecovery {
             standalone: if start_stmt { IsStandalone::Standalone } else { IsStandalone::Subexpr },
             op: IncOrDec::Inc,
@@ -1696,10 +1696,10 @@ impl<'a> Parser<'a> {
 
     pub(super) fn recover_from_postfix_decrement(
         &mut self,
-        operand_expr: Box<Expr>,
+        operand_expr: &Expr,
         op_span: Span,
         start_stmt: bool,
-    ) -> PResult<'a, Box<Expr>> {
+    ) -> PResult<'a, ()> {
         let kind = IncDecRecovery {
             standalone: if start_stmt { IsStandalone::Standalone } else { IsStandalone::Subexpr },
             op: IncOrDec::Dec,
@@ -1710,21 +1710,15 @@ impl<'a> Parser<'a> {
 
     fn recover_from_inc_dec(
         &mut self,
-        base: Box<Expr>,
+        base: &Expr,
         kind: IncDecRecovery,
         op_span: Span,
-    ) -> PResult<'a, Box<Expr>> {
+    ) -> PResult<'a, ()> {
         let mut err = self.dcx().struct_span_err(
             op_span,
             format!("Rust has no {} {} operator", kind.fixity, kind.op.name()),
         );
         err.span_label(op_span, format!("not a valid {} operator", kind.fixity));
-
-        let help_base_case = |mut err: Diag<'_, _>, base| {
-            err.help(format!("use `{}= 1` instead", kind.op.chr()));
-            err.emit();
-            Ok(base)
-        };
 
         // (pre, post)
         let spans = match kind.fixity {
@@ -1738,7 +1732,9 @@ impl<'a> Parser<'a> {
             }
             IsStandalone::Subexpr => {
                 let Ok(base_src) = self.span_to_snippet(base.span) else {
-                    return help_base_case(err, base);
+                    err.help(format!("use `{}= 1` instead", kind.op.chr()));
+                    err.emit();
+                    return Ok(());
                 };
                 match kind.fixity {
                     UnaryFixity::Pre => {
--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -191,7 +191,7 @@ impl<'a> Parser<'a> {
                 let op_span = self.prev_token.span.to(self.token.span);
                 // Eat the second `+`
                 self.bump();
-                lhs = self.recover_from_postfix_increment(lhs, op_span, starts_stmt)?;
+                self.recover_from_postfix_increment(&lhs, op_span, starts_stmt)?;
                 continue;
             }
 
@@ -203,7 +203,7 @@ impl<'a> Parser<'a> {
                 let op_span = self.prev_token.span.to(self.token.span);
                 // Eat the second `-`
                 self.bump();
-                lhs = self.recover_from_postfix_decrement(lhs, op_span, starts_stmt)?;
+                self.recover_from_postfix_decrement(&lhs, op_span, starts_stmt)?;
                 continue;
             }
 
@@ -487,7 +487,8 @@ impl<'a> Parser<'a> {
                 this.bump();
 
                 let operand_expr = this.parse_expr_dot_or_call(attrs)?;
-                this.recover_from_prefix_increment(operand_expr, pre_span, starts_stmt)
+                this.recover_from_prefix_increment(&operand_expr, pre_span, starts_stmt)?;
+                Ok(operand_expr)
             }
             token::Ident(..)
                 if this.token.is_keyword(kw::Move)
