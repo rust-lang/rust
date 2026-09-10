356ccce8f8fbdde7f32a98f9b4cb384e207ce00e
Jakub Beránek <berykubik@gmail.com>
2026-09-10 12:09:19 +0200

    WIP

--- compiler/rustc_ast/src/tokenstream.rs
+++ compiler/rustc_ast/src/tokenstream.rs
@@ -249,6 +249,15 @@ impl LazyAttrTokenStreamInner {
                 break_last_token,
                 node_replacements,
             } => {
+                let mut snapshot = cursor_snapshot.clone();
+                let stack_size = snapshot.stack.len();
+                for i in 0..num_calls.saturating_sub(1) {
+                    snapshot.next_and_bump();
+                    if snapshot.stack.len() < stack_size {
+                        assert_eq!(snapshot.stack.len(), stack_size - 1);
+                        assert_eq!(i, num_calls - 2);
+                    }
+                }
                 // The token produced by the final call to `{,inlined_}next_and_bump` was not
                 // actually consumed by the callback. The combination of chaining the
                 // initial token and using `take` produces the desired result - we
@@ -971,6 +980,11 @@ impl TokenCursor {
     #[inline]
     pub fn new(stream: TokenStream) -> Self {
         TokenCursor { curr: TokenTreeCursor::new(stream), stack: vec![] }
+    }
+
+    pub fn clone_without_stack(&self) -> Self {
+        let stack = self.stack.last().cloned().into_iter().collect();
+        Self { curr: self.curr.clone(), stack }
     }
 
     /// Gets the next token and advances the cursor by one.
