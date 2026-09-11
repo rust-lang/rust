c690933f6ce13b8bf460ddb769728118c5a1e559
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-09-11 15:39:20 +0200

    Remove unused argument of `try_match_macro_attr`

--- compiler/rustc_expand/src/mbe/diagnostics.rs
+++ compiler/rustc_expand/src/mbe/diagnostics.rs
@@ -50,7 +50,7 @@ pub(super) fn failed_to_match_macro(
     let try_success_result = match args {
         FailedMacro::Func => try_match_macro(psess, name, body, rules, &mut tracker),
         FailedMacro::Attr(attr_args) => {
-            try_match_macro_attr(psess, name, attr_args, body, rules, &mut tracker)
+            try_match_macro_attr(psess, attr_args, body, rules, &mut tracker)
         }
         FailedMacro::Derive => try_match_macro_derive(psess, body, rules, &mut tracker),
     };
