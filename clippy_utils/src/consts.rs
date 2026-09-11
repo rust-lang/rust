8ec3b71889c43becb8dafae023e4c378c703c603
khyperia <953151+khyperia@users.noreply.github.com>
2026-09-11 17:08:12 +0200

    yeet type const

--- clippy_utils/src/consts.rs
+++ clippy_utils/src/consts.rs
@@ -874,10 +874,8 @@ impl<'tcx> ConstEvalCtxt<'tcx> {
             // TODO: revisit when feature `min_generic_const_args` is stabilized. In the meantime,
             // `TyCtxt::const_eval_resolve()` will trigger an ICE when evaluating the body of the
             // `type const` definition.
-            _ if let Res::Def(
-                DefKind::Const { is_type_const: false } | DefKind::AssocConst { is_type_const: false },
-                did,
-            ) = self.typeck.qpath_res(qpath, id) =>
+            _ if let Res::Def(DefKind::Const | DefKind::AssocConst, did) = self.typeck.qpath_res(qpath, id)
+                && !self.tcx.is_direct_const(did) =>
             {
                 self.source.set(ConstantSource::NonLocal);
                 did
