fc6fa6dd541b8a5f4bdf7e9593d7b38e30e49a3e
León Orell Valerian Liehr <me@fmease.dev>
2026-09-11 17:50:41 +0200

    Syntactically reject leading parenthesized precise capturing lists in bare trait object types (`(use<…>)+`)

--- tests/ui/impl-trait/precise-capturing/parenthesized.rs
+++ tests/ui/impl-trait/precise-capturing/parenthesized.rs
@@ -5,4 +5,14 @@ fn f() -> impl Sized + (use<>) {}
 //~^ ERROR precise capturing lists may not be parenthesized
 //~| HELP remove the parentheses
 
+#[cfg(false)]
+type O = Trait + (use<>);
+//~^ ERROR precise capturing lists may not be parenthesized
+//~| HELP remove the parentheses
+
+// We once used to accidentally accept this.
+#[cfg(false)]
+type O = (use<>) + Trait;
+//~^ ERROR expected a path on the left-hand side of `+`
+
 fn main() {}
