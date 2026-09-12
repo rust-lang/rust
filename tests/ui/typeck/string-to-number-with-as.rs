d9befd9df8d79e79179fb09ee790a611a99bf9c8
Snxhit <amitdgames@gmail.com>
2026-09-13 00:20:20 +0530

    add more tests, bless stderr

--- tests/ui/typeck/string-to-number-with-as.rs
+++ tests/ui/typeck/string-to-number-with-as.rs
@@ -4,4 +4,7 @@ fn main() {
 
     let slice: &str = "123";
     let number2 = slice as u16; //~ ERROR casting `&str` as `u16` is invalid
+
+    let number3 = "8080" as i16; //~ ERROR casting `&'static str` as `i16` is invalid
+    let number4 = &text as i32; //~ ERROR casting `&String` as `i32` is invalid
 }
