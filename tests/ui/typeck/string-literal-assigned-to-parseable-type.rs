c7d02927ca3c0ae874186b4fc92dead14a47ed6b
Snxhit <amitdgames@gmail.com>
2026-09-12 18:31:19 +0530

    add new test case, bless stderr

--- tests/ui/typeck/string-literal-assigned-to-parseable-type.rs
+++ tests/ui/typeck/string-literal-assigned-to-parseable-type.rs
@@ -1,3 +1,7 @@
 fn main() {
-    let test: u16 = "29"; //~ ERROR mismatched types
+    // literal
+    let str1: u16 = "29"; //~ ERROR mismatched types
+
+    // function calls
+    let str2: i32 = String::from("129"); //~ ERROR mismatched types
 }
