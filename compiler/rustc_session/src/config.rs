bb3600a11023d208ff9e90777e6cf137cf8a1780
Esteban Küber <esteban@kuber.com.ar>
2026-09-13 23:31:04 +0000

    Re-add the hability to emit notes exclusively for missing `--edition`

--- compiler/rustc_session/src/config.rs
+++ compiler/rustc_session/src/config.rs
@@ -2378,7 +2378,7 @@ pub fn parse_crate_edition(
         }),
         None => {
             if has_input {
-                early_dcx.early_note(format!(
+                early_dcx.early_note_for_missing_crate_edition(format!(
                     "`--edition` is unspecified, defaulting to `{DEFAULT_EDITION}` while the \
                      latest is `{LATEST_STABLE_EDITION}`; it must be one of: {EDITION_NAME_LIST}",
                 ));
