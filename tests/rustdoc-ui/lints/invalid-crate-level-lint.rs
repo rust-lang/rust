4899f5bbd43942e411b53d4accfd2c800c738268
Eva van Houten <opensource@evavh.net>
2026-09-14 15:37:41 +0200

    Bless rustdoc ui test

--- tests/rustdoc-ui/lints/invalid-crate-level-lint.rs
+++ tests/rustdoc-ui/lints/invalid-crate-level-lint.rs
@@ -6,7 +6,8 @@
 
 pub mod bar {
     #![doc(test(no_crate_inject))]
-    //~^ ERROR can only be applied at the crate level
+    //~^ ERROR unused attribute
+    //~| WARN this was previously accepted by the compiler but is being phased out
 
     #[doc(test(no_crate_inject))]
     //~^ ERROR can only be applied at the crate level
