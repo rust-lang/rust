f7e96ba8aca536ac0a8ba26ddc37f0d7598afd88
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-09-14 13:04:19 +0200

    Add regression tests

--- tests/ui/attributes/attrs-on-params.rs
+++ tests/ui/attributes/attrs-on-params.rs
@@ -5,4 +5,23 @@ fn function(#[inline] param: u32) {
     //~| ERROR allow, cfg, cfg_attr, deny, expect, forbid, and warn are the only allowed built-in attributes
 }
 
+trait Test {
+    fn meow(
+        #[rustc_splat] a1: u32,
+    );
+    fn meow2(
+        #[rustc_splat(invalid)] a4: u32,
+    );
+}
+
+type Meow = fn(
+    #[rustc_splat] a1: u32,
+);
+
+extern "Rust" {
+    fn meow2(
+        #[rustc_splat] a1: u32,
+    );
+}
+
 fn main() {}
