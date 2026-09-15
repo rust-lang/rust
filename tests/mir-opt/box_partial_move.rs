b647fd5714b81a4441361db68192a7a1b895c8c3
Max Dexheimer <maxdexh03@gmail.com>
2026-09-15 10:05:04 +0200

    Replace `Unique` in `Box` with a `(NonNull, PhantomData)` wrapper

--- tests/mir-opt/box_partial_move.rs
+++ tests/mir-opt/box_partial_move.rs
@@ -7,7 +7,7 @@
 fn maybe_move(cond: bool, thing: Box<String>) -> Option<String> {
     // CHECK-LABEL: fn maybe_move(
     // CHECK: let mut [[PTR:_[0-9]+]]: *const std::string::String;
-    // CHECK: [[PTR]] = copy ((_2.0: std::ptr::Unique<std::string::String>).0: std::ptr::NonNull<std::string::String>) as *const std::string::String (Transmute);
+    // CHECK: [[PTR]] = copy ((_2.0: std::boxed::BoxRaw<std::string::String>).0: std::ptr::NonNull<std::string::String>) as *const std::string::String (Transmute);
     // CHECK: drop((*[[PTR]]))
     if cond { Some(*thing) } else { None }
 }
