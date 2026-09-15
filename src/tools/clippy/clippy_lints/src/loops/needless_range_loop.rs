d482705e2a7e5d4c157a4e4a36547ed4bb27d28f
Jamesbarford <james.barford-evans@arm.com>
2026-09-15 09:57:20 +0000

    Use `ConstExt` in clippy

--- src/tools/clippy/clippy_lints/src/loops/needless_range_loop.rs
+++ src/tools/clippy/clippy_lints/src/loops/needless_range_loop.rs
@@ -15,6 +15,7 @@ use rustc_hir::intravisit::{Visitor, walk_expr};
 use rustc_hir::{BinOpKind, BorrowKind, Closure, Expr, ExprKind, HirId, Mutability, Pat, PatKind, QPath};
 use rustc_lint::LateContext;
 use rustc_middle::middle::region;
+use rustc_middle::ty::consts::ConstExt;
 use rustc_middle::ty::{self, Ty};
 use rustc_span::symbol::{Symbol, sym};
 use rustc_span::{Span, SyntaxContext};
