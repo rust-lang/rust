591e0810a23ea02f29ea4792e1f21ee70b3674e0
Esteban Küber <esteban@kuber.com.ar>
2026-09-16 22:35:56 +0000

    Use context span more

--- compiler/rustc_hir_analysis/src/check/compare_impl_item.rs
+++ compiler/rustc_hir_analysis/src/check/compare_impl_item.rs
@@ -2123,11 +2123,11 @@ fn compare_generic_param_kinds<'tcx>(
             };
 
             let trait_header_span = tcx.def_ident_span(tcx.parent(trait_item.def_id)).unwrap();
-            err.span_label(trait_header_span, "");
+            err.span_context(trait_header_span);
             err.span_label(param_trait_span, make_param_message("expected", param_trait));
 
             let impl_header_span = tcx.def_span(tcx.parent(impl_item.def_id));
-            err.span_label(impl_header_span, "");
+            err.span_context(impl_header_span);
             err.span_label(param_impl_span, make_param_message("found", param_impl));
 
             let reported = err.emit_unless_delay(delay);
