482f884c22e5b1e9a1ae57ec327adfd28f6943cf
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-16 14:08:31 -0600

    Use hypothetical seek_read_exact_seek_write_all feature also for .seek_read_exact()

--- library/std/src/os/windows/fs.rs
+++ library/std/src/os/windows/fs.rs
@@ -95,7 +95,7 @@ pub trait FileExt {
     ///     Ok(())
     /// }
     /// ```
-    #[unstable(feature = "seek_read_exact", issue = "none")]
+    #[unstable(feature = "seek_read_exact_seek_write_all", issue = "none")]
     fn seek_read_exact(&self, mut buf: &mut [u8], mut offset: u64) -> io::Result<()> {
         while !buf.is_empty() {
             match self.seek_read(buf, offset) {
