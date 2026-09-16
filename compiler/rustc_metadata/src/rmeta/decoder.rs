a6f5997f955c7f26fc74638143ea563e555f5032
aerooneqq <aerooneq@yandex.ru>
2026-09-16 17:03:15 +0300

    wip

--- compiler/rustc_metadata/src/rmeta/decoder.rs
+++ compiler/rustc_metadata/src/rmeta/decoder.rs
@@ -1522,7 +1522,9 @@ impl CrateMetadata {
 
     fn get_proc_macro_quoted_span(&self, tcx: TyCtxt<'_>, index: usize) -> Span {
         self.root
-            .tables
+            .proc_macro_data
+            .as_ref()
+            .unwrap()
             .proc_macro_quoted_spans
             .get(self, index)
             .unwrap_or_else(|| panic!("Missing proc macro quoted span: {index:?}"))
