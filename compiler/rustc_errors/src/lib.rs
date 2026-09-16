17c9e14d87b31da07204a207d3ef8f6fbd3e16dc
Xuyang Zhang <huan9d0w@gmail.com>
2026-09-16 22:06:48 +0800

    Don't reorder remaining stashed diagnostics when stealing

--- compiler/rustc_errors/src/lib.rs
+++ compiler/rustc_errors/src/lib.rs
@@ -636,10 +636,9 @@ impl<'a> DiagCtxtHandle<'a> {
     /// and [`StashKey`] as the key. Panics if the found diagnostic is an
     /// error.
     pub fn steal_non_err(self, span: Span, key: StashKey) -> Option<Diag<'a, ()>> {
-        // FIXME(#120456) - is `swap_remove` correct?
         let (diag, guar, _) =
             self.dcx.inner.borrow_mut().stashed_diagnostics.get_mut(&key).and_then(
-                |stashed_diagnostics| stashed_diagnostics.swap_remove(&span.with_parent(None)),
+                |stashed_diagnostics| stashed_diagnostics.shift_remove(&span.with_parent(None)),
             )?;
         assert!(!diag.is_error());
         assert!(guar.is_none());
@@ -659,9 +658,8 @@ impl<'a> DiagCtxtHandle<'a> {
     where
         F: FnMut(&mut Diag<'_>),
     {
-        // FIXME(#120456) - is `swap_remove` correct?
         let err = self.dcx.inner.borrow_mut().stashed_diagnostics.get_mut(&key).and_then(
-            |stashed_diagnostics| stashed_diagnostics.swap_remove(&span.with_parent(None)),
+            |stashed_diagnostics| stashed_diagnostics.shift_remove(&span.with_parent(None)),
         );
         err.map(|(err, guar, _)| {
             // The use of `::<ErrorGuaranteed>` is safe because level is `Level::Error`.
@@ -683,9 +681,8 @@ impl<'a> DiagCtxtHandle<'a> {
         key: StashKey,
         new_err: Diag<'_>,
     ) -> ErrorGuaranteed {
-        // FIXME(#120456) - is `swap_remove` correct?
         let old_err = self.dcx.inner.borrow_mut().stashed_diagnostics.get_mut(&key).and_then(
-            |stashed_diagnostics| stashed_diagnostics.swap_remove(&span.with_parent(None)),
+            |stashed_diagnostics| stashed_diagnostics.shift_remove(&span.with_parent(None)),
         );
         match old_err {
             Some((old_err, guar, _)) => {
@@ -1744,3 +1741,7 @@ pub enum TerminalUrl {
     Yes,
     Auto,
 }
+
+#[cfg(test)]
+mod tests;
+
