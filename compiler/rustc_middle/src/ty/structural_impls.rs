e83bfcabca99e7d18ce0a57a6ac8f57345596d65
James Barford-Evans <james.barford-evans@arm.com>
2026-09-17 09:36:39 +0100

    Move `Const` from `rustc_middle` to `rustc_type_ir`

--- compiler/rustc_middle/src/ty/structural_impls.rs
+++ compiler/rustc_middle/src/ty/structural_impls.rs
@@ -9,7 +9,7 @@ use rustc_abi::TyAndLayout;
 use rustc_hir::def::Namespace;
 use rustc_hir::def_id::LocalDefId;
 use rustc_span::Spanned;
-use rustc_type_ir::{ConstKind, PredicateProxy, TypeFolder, Upcast, VisitorResult, try_visit};
+use rustc_type_ir::{PredicateProxy, TypeFolder, Upcast, VisitorResult, try_visit};
 
 use super::{GenericArg, GenericArgKind, Pattern};
 use crate::mir::PlaceElem;
@@ -137,18 +137,6 @@ impl<'tcx> fmt::Debug for ty::consts::Expr<'tcx> {
                 let (value_ty, value, to_ty) = self.cast_args();
                 write!(f, "({kind:?}: ({:?}: {:?}), {:?})", value, value_ty, to_ty)
             }
-        }
-    }
-}
-
-impl<'tcx> fmt::Debug for ty::Const<'tcx> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // If this is a value, we spend some effort to make it look nice.
-        if let ConstKind::Value(cv) = self.kind() {
-            write!(f, "{}", cv)
-        } else {
-            // Fall back to something verbose.
-            write!(f, "{:?}", self.kind())
         }
     }
 }
@@ -648,80 +636,6 @@ impl<'tcx> TypeSuperFoldable<TyCtxt<'tcx>> for ty::Clauses<'tcx> {
 
     fn super_fold_with<F: TypeFolder<TyCtxt<'tcx>>>(self, folder: &mut F) -> Self {
         ty::util::fold_list(self, folder, |tcx, v| tcx.mk_clauses(v))
-    }
-}
-
-impl<'tcx> TypeFoldable<TyCtxt<'tcx>> for ty::Const<'tcx> {
-    fn try_fold_with<F: FallibleTypeFolder<TyCtxt<'tcx>>>(
-        self,
-        folder: &mut F,
-    ) -> Result<Self, F::Error> {
-        folder.try_fold_const(self)
-    }
-
-    fn fold_with<F: TypeFolder<TyCtxt<'tcx>>>(self, folder: &mut F) -> Self {
-        folder.fold_const(self)
-    }
-}
-
-impl<'tcx> TypeVisitable<TyCtxt<'tcx>> for ty::Const<'tcx> {
-    fn visit_with<V: TypeVisitor<TyCtxt<'tcx>>>(&self, visitor: &mut V) -> V::Result {
-        visitor.visit_const(*self)
-    }
-}
-
-impl<'tcx> TypeSuperFoldable<TyCtxt<'tcx>> for ty::Const<'tcx> {
-    fn try_super_fold_with<F: FallibleTypeFolder<TyCtxt<'tcx>>>(
-        self,
-        folder: &mut F,
-    ) -> Result<Self, F::Error> {
-        let kind = match self.kind() {
-            ConstKind::Alias(is_rigid, alias_const) => {
-                ConstKind::Alias(is_rigid, alias_const.try_fold_with(folder)?)
-            }
-            ConstKind::Value(v) => ConstKind::Value(v.try_fold_with(folder)?),
-            ConstKind::Expr(e) => ConstKind::Expr(e.try_fold_with(folder)?),
-
-            ConstKind::Param(_)
-            | ConstKind::Infer(_)
-            | ConstKind::Bound(..)
-            | ConstKind::Placeholder(_)
-            | ConstKind::Error(_) => return Ok(self),
-        };
-        if kind != self.kind() { Ok(folder.cx().mk_ct_from_kind(kind)) } else { Ok(self) }
-    }
-
-    fn super_fold_with<F: TypeFolder<TyCtxt<'tcx>>>(self, folder: &mut F) -> Self {
-        let kind = match self.kind() {
-            ConstKind::Alias(is_rigid, alias_const) => {
-                ConstKind::Alias(is_rigid, alias_const.fold_with(folder))
-            }
-            ConstKind::Value(v) => ConstKind::Value(v.fold_with(folder)),
-            ConstKind::Expr(e) => ConstKind::Expr(e.fold_with(folder)),
-
-            ConstKind::Param(_)
-            | ConstKind::Infer(_)
-            | ConstKind::Bound(..)
-            | ConstKind::Placeholder(_)
-            | ConstKind::Error(_) => return self,
-        };
-        if kind != self.kind() { folder.cx().mk_ct_from_kind(kind) } else { self }
-    }
-}
-
-impl<'tcx> TypeSuperVisitable<TyCtxt<'tcx>> for ty::Const<'tcx> {
-    fn super_visit_with<V: TypeVisitor<TyCtxt<'tcx>>>(&self, visitor: &mut V) -> V::Result {
-        match self.kind() {
-            ConstKind::Alias(_, alias_const) => alias_const.visit_with(visitor),
-            ConstKind::Value(v) => v.visit_with(visitor),
-            ConstKind::Expr(e) => e.visit_with(visitor),
-            ConstKind::Error(e) => e.visit_with(visitor),
-
-            ConstKind::Param(_)
-            | ConstKind::Infer(_)
-            | ConstKind::Bound(..)
-            | ConstKind::Placeholder(_) => V::Result::output(),
-        }
     }
 }
 
