ccaab08634ffa129b9877cb0b5c8ac41706c0056
cyrgani <ansgar.w.zielke@gmail.com>
2026-09-17 20:16:07 +0000

    just use `Self` instead of writing the type out

--- tests/ui/duplicate/multiple-types-with-same-name-and-derive.rs
+++ tests/ui/duplicate/multiple-types-with-same-name-and-derive.rs
@@ -8,12 +8,10 @@
 struct NotSM;
 
 #[derive(PartialEq, Eq)]
-//~^ ERROR: struct takes 0 generic arguments
 struct NotSM<T>(T);
 //~^ ERROR: struct takes 0 generic arguments
 //~| ERROR: struct takes 0 generic arguments
 //~| ERROR: struct takes 0 generic arguments
 //~| ERROR: the name `NotSM` is defined multiple times
-//~| ERROR: no field `0`
 
 fn main() {}
