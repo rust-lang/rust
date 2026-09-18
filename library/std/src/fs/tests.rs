1ee847599812a37d107f6a6a96f90e42faa79e0b
Jason Gerard DeRose <jasonderose@gmail.com>
2026-09-18 12:10:02 -0600

    Split three tests out of file_test_windows_fileext_trait()

--- library/std/src/fs/tests.rs
+++ library/std/src/fs/tests.rs
@@ -831,6 +831,98 @@ fn file_test_io_seek_read_exact_write_all() {
 
 #[test]
 #[cfg(windows)]
+fn file_test_windows_fileext_trait_case_1() {
+    use crate::os::windows::fs::FileExt;
+
+    // Test when seek_read_exact(), seek_write_all() are called with empty buffers.
+    // Importantly, no calls to seek_read() or seek_write() should be made, and therefore
+    // no system calls should be made.
+    struct MockFile {}
+
+    impl FileExt for MockFile {
+        fn seek_read(&self, _buf: &mut [u8], _offset: u64) -> io::Result<usize> {
+            panic!("should not be called");
+        }
+
+        fn seek_write(&self, _buf: &[u8], _offset: u64) -> io::Result<usize> {
+            panic!("should not be called");
+        }
+    }
+
+    let mock_file = MockFile {};
+    check!(mock_file.seek_read_exact(&mut [], 0));
+    check!(mock_file.seek_read_exact(&mut [], 420));
+    check!(mock_file.seek_write_all(&[], 0));
+    check!(mock_file.seek_write_all(&[], 420));
+}
+
+#[test]
+#[cfg(windows)]
+fn file_test_windows_fileext_trait_case_2() {
+    use crate::os::windows::fs::FileExt;
+
+    // Test when seek_read(), seek_write() return Ok(0)
+    struct MockFile {}
+
+    impl FileExt for MockFile {
+        fn seek_read(&self, _buf: &mut [u8], _offset: u64) -> io::Result<usize> {
+            Ok(0)
+        }
+
+        fn seek_write(&self, _buf: &[u8], _offset: u64) -> io::Result<usize> {
+            Ok(0)
+        }
+    }
+
+    let mock_file = MockFile {};
+    let mut buf = [0; 256];
+    assert_eq!(mock_file.seek_read(&mut buf, 0).unwrap_err().kind(), io::ErrorKind::UnexpectedEof);
+    assert_eq!(
+        mock_file.seek_read(&mut buf, 420).unwrap_err().kind(),
+        io::ErrorKind::UnexpectedEof
+    );
+    assert_eq!(mock_file.seek_write(&buf, 0).unwrap_err().kind(), io::ErrorKind::WriteZero);
+    assert_eq!(mock_file.seek_write(&buf, 420).unwrap_err().kind(), io::ErrorKind::WriteZero);
+}
+
+#[test]
+#[cfg(windows)]
+fn file_test_windows_fileext_trait_case_3() {
+    use crate::os::windows::fs::FileExt;
+
+    // Test that Err other than io::ErrorKind::Interrupted are propagated up.
+    struct MockFile {}
+
+    impl FileExt for MockFile {
+        fn seek_read(&self, _buf: &mut [u8], _offset: u64) -> io::Result<usize> {
+            Err(io::Error::new(io::ErrorKind::PermissionDenied, "seek_read"))
+        }
+
+        fn seek_write(&self, _buf: &[u8], _offset: u64) -> io::Result<usize> {
+            Err(io::Error::new(io::ErrorKind::ConnectionRefused, "seek_write"))
+        }
+    }
+
+    let mock_file = MockFile {};
+    let mut buf = [0; 256];
+    assert_eq!(
+        mock_file.seek_read(&mut buf, 0).unwrap_err().kind(),
+        io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(
+        mock_file.seek_read(&mut buf, 420).unwrap_err().kind(),
+        io::ErrorKind::PermissionDenied
+    );
+    assert_eq!(mock_file.seek_write(&buf, 0).unwrap_err().kind(), io::ErrorKind::ConnectionRefused);
+    assert_eq!(
+        mock_file.seek_write(&buf, 420).unwrap_err().kind(),
+        io::ErrorKind::ConnectionRefused
+    );
+    // FIXME: Cover io::ErrorKind::Interrupted, but don't infinite loop ;)
+}
+
+#[test]
+#[cfg(windows)]
 fn file_test_windows_fileext_trait() {
     use crate::io;
     use crate::os::windows::fs::FileExt;
