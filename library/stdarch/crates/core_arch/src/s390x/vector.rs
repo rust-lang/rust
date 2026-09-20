3da3866635c74f7d666422697daba0b759878129
Folkert de Vries <folkert@folkertdev.nl>
2026-09-20 15:09:09 +0200

    s390x: use u128 arithmetic

--- library/stdarch/crates/core_arch/src/s390x/vector.rs
+++ library/stdarch/crates/core_arch/src/s390x/vector.rs
@@ -138,9 +138,6 @@ unsafe extern "llvm-intrinsic" {
     #[link_name = "llvm.s390.vsumqf"] fn vsumqf(a: vector_unsigned_int, b: vector_unsigned_int) -> u128;
     #[link_name = "llvm.s390.vsumqg"] fn vsumqg(a: vector_unsigned_long_long, b: vector_unsigned_long_long) -> u128;
 
-    #[link_name = "llvm.s390.vaccq"] fn vaccq(a: u128, b: u128) -> u128;
-    #[link_name = "llvm.s390.vacccq"] fn vacccq(a: u128, b: u128, c: u128) -> u128;
-
     #[link_name = "llvm.s390.vscbiq"] fn vscbiq(a: u128, b: u128) -> u128;
     #[link_name = "llvm.s390.vsbiq"] fn vsbiq(a: u128, b: u128, c: u128) -> u128;
     #[link_name = "llvm.s390.vsbcbiq"] fn vsbcbiq(a: u128, b: u128, c: u128) -> u128;
@@ -4842,9 +4839,7 @@ pub unsafe fn vec_addc_u128(
 ) -> vector_unsigned_char {
     let a: u128 = transmute(a);
     let b: u128 = transmute(b);
-    // FIXME(llvm) https://github.com/llvm/llvm-project/pull/153557
-    // transmute(a.overflowing_add(b).1 as u128)
-    transmute(vaccq(a, b))
+    transmute(a.overflowing_add(b).1 as u128)
 }
 
 /// Vector Add With Carry unsigned 128-bits
@@ -4879,10 +4874,8 @@ pub unsafe fn vec_addec_u128(
     let a: u128 = transmute(a);
     let b: u128 = transmute(b);
     let c: u128 = transmute(c);
-    // FIXME(llvm) https://github.com/llvm/llvm-project/pull/153557
-    // let (_d, carry) = a.carrying_add(b, c & 1 != 0);
-    // transmute(carry as u128)
-    transmute(vacccq(a, b, c))
+    let (_d, carry) = a.carrying_add(b, c & 1 != 0);
+    transmute(carry as u128)
 }
 
 /// Vector Subtract with Carryout
