93bfeed513279837781482ce88707cb6a18ef8c1
Yukang <moorekang@gmail.com>
2026-09-22 00:18:16 +0800

    Add regression tests for Option predicate suggestion precedence

--- tests/ui/suggestions/option-to-bool.rs
+++ tests/ui/suggestions/option-to-bool.rs
@@ -1,7 +1,34 @@
+// An `is_some()` suggestion must apply to the whole expression, preserving its precedence.
+
 fn foo(x: Option<i32>) {
     if true && x {}
     //~^ ERROR mismatched types
     //~| HELP use `Option::is_some` to test if the `Option` has a value
+
+    let reference = &x;
+    if *reference {}
+    //~^ ERROR mismatched types
+    //~| HELP use `Option::is_some` to test if the `Option` has a value
+
+    if &x {}
+    //~^ ERROR mismatched types
+    //~| HELP use `Option::is_some` to test if the `Option` has a value
+
+    if Value + Value {}
+    //~^ ERROR mismatched types
+    //~| HELP use `Option::is_some` to test if the `Option` has a value
 }
 
-fn main() {}
+struct Value;
+
+impl std::ops::Add for Value {
+    type Output = Option<i32>;
+
+    fn add(self, _: Self) -> Self::Output {
+        Some(1)
+    }
+}
+
+fn main() {
+    foo(Some(1));
+}
