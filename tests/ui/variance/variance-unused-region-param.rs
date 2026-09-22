7779f5cf60bf306f537d1d2fb6c57b7fdf33b7bb
Alice Ryhl <aliceryhl@google.com>
2026-09-22 20:14:32 +0000

    Test unused lifetime params for 'type' and 'union'

--- tests/ui/variance/variance-unused-region-param.rs
+++ tests/ui/variance/variance-unused-region-param.rs
@@ -2,6 +2,8 @@
 
 struct SomeStruct<'a> { x: u32 } //~ ERROR parameter `'a` is never used
 enum SomeEnum<'a> { Nothing } //~ ERROR parameter `'a` is never used
+union SomeUnion<'a> { x: u32 } //~ ERROR parameter `'a` is never used
 trait SomeTrait<'a> { fn foo(&self); } // OK on traits.
+type SomeAlias<'a> = u32; // OK on type aliases.
 
 fn main() {}
