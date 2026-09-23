6387f4b9bcde5a76f05411c9b441396f2458690c
joboet <jonasboettiger@icloud.com>
2026-09-23 13:56:19 +0200

    std: split stack overflow module

--- library/std/src/sys/pal/unix/stack_overflow/mod.rs
+++ library/std/src/sys/pal/unix/stack_overflow/mod.rs
@@ -1,6 +1,46 @@
 #![cfg_attr(test, allow(dead_code))]
 #![forbid(unsafe_op_in_unsafe_fn)]
 
+cfg_select! {
+    // This is intentionally not enabled on iOS/tvOS/watchOS/visionOS, as it uses
+    // several symbols that might lead to rejections from the App Store, namely
+    // `sigaction`, `sigaltstack`, `sysctlbyname`, `mmap`, `munmap` and `mprotect`.
+    //
+    // This might be overly cautious, though it is also what Swift does (and they
+    // usually have fewer qualms about forwards compatibility, since the runtime
+    // is shipped with the OS):
+    // <https://github.com/apple/swift/blob/swift-5.10-RELEASE/stdlib/public/runtime/CrashHandlerMacOS.cpp>
+    //
+    // miri doesn't model signals nor stack overflows and this code has some
+    // synchronization properties that we don't want to expose to user code,
+    // hence we disable it on miri.
+    all(
+        not(miri),
+        any(
+            target_os = "linux",
+            target_os = "freebsd",
+            target_os = "hurd",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+            target_os = "solaris",
+            target_os = "illumos",
+        )
+    ) => {
+        mod handler_signal;
+        mod thread_info;
+        use handler_signal as imp;
+    }
+    target_os = "cygwin" => {
+        mod handler_cygwin;
+        use handler_cygwin as imp;
+    }
+    _ => {
+        mod handler_none;
+        use handler_none as imp;
+    }
+}
+
 pub use self::imp::init;
 use self::imp::{drop_handler, make_handler};
 
@@ -25,63 +65,3 @@ impl Drop for Handler {
         }
     }
 }
-
-#[cfg(all(
-    not(miri),
-    any(
-        target_os = "linux",
-        target_os = "freebsd",
-        target_os = "hurd",
-        target_os = "macos",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris",
-        target_os = "illumos",
-    ),
-))]
-mod thread_info;
-
-// miri doesn't model signals nor stack overflows and this code has some
-// synchronization properties that we don't want to expose to user code,
-// hence we disable it on miri.
-#[cfg(all(
-    not(miri),
-    any(
-        target_os = "linux",
-        target_os = "freebsd",
-        target_os = "hurd",
-        target_os = "macos",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris",
-        target_os = "illumos",
-    )
-))]
-mod imp {}
-
-// This is intentionally not enabled on iOS/tvOS/watchOS/visionOS, as it uses
-// several symbols that might lead to rejections from the App Store, namely
-// `sigaction`, `sigaltstack`, `sysctlbyname`, `mmap`, `munmap` and `mprotect`.
-//
-// This might be overly cautious, though it is also what Swift does (and they
-// usually have fewer qualms about forwards compatibility, since the runtime
-// is shipped with the OS):
-// <https://github.com/apple/swift/blob/swift-5.10-RELEASE/stdlib/public/runtime/CrashHandlerMacOS.cpp>
-#[cfg(any(
-    miri,
-    not(any(
-        target_os = "linux",
-        target_os = "freebsd",
-        target_os = "hurd",
-        target_os = "macos",
-        target_os = "netbsd",
-        target_os = "openbsd",
-        target_os = "solaris",
-        target_os = "illumos",
-        target_os = "cygwin",
-    ))
-))]
-mod imp {}
-
-#[cfg(target_os = "cygwin")]
-mod imp {}
