4de84d7f2a2c490a83a4ff33a61aab9387d967b4
Guillaume Gomez <contact@guillaume-gomez.fr>
2026-09-24 22:15:20 +0200

    Correctly handle `impl` items when checking if an item parents have `doc(hidden)`

--- tests/rustdoc-html/jump-to-def/hidden-links.rs
+++ tests/rustdoc-html/jump-to-def/hidden-links.rs
@@ -11,16 +11,33 @@ extern crate hidden_links;
 
 //@ has 'src/foo/hidden-links.rs.html'
 
+// Because our XPath handler is very bad, I can't look for `not(@id)` so instead,
+// we count how many `<a>` we have in total (line numbers are `<a>`).
+//@ count - '//pre/code/a' 21
+// Then we count the `<a>` of lines. The diff should be the number of non-line `<a>` (no
+// better way to test that either..).
+//@ count - '//pre/code/a[@id]' 13
+
+//@ has - '//pre/code/a[@href="../../hidden_links/trait.PubTrait.html"]' 'PubTrait'
+use hidden_links::{PubTrait, error::HiddenTrait};
+
 fn foo() {
     // In here, only `Bar` should be linked as `Foo` has a `doc(hidden)` parent and
     // is not reexported. So there should be only one link.
 
-    // Because our XPath handler is very bad, I can't look for `not(@id)` so instead,
-    // we count how many `<a>` we have in total (line numbers are `<a>`).
-    //@ count - '//pre/code/a' 7
-    // Then we count the number of lines. The diff should be one (no way to test that either..).
-    //@ count - '//pre/code/a[@id]' 6
-    //@ has - '//pre/code/a[@href="../../hidden_links/struct.Bar.html"]' 'Bar'
+    // This one should not generate any link.
     let x = hidden_links::error::Foo;
-    let x = hidden_links::Bar;
+    //@ has - '//pre/code/a[@href="../../hidden_links/struct.Bar.html"]' 'Bar'
+    let y = hidden_links::Bar;
+
+    // `x` and `y` generate a link every time...
+    //@ count - '//pre/code/a[@href="#29"]' 2
+    //@ count - '//pre/code/a[@href="#31"]' 2
+    // FIXME(GuillaumeGomez): This is a bug, it should link the trait method on the item's page,
+    // not on the trait's (and so `x.public()`) should not link.
+    //@ count - '//pre/code/a[@href="../../hidden_links/trait.PubTrait.html#method.public"]' 2
+    x.hidden();
+    y.hidden();
+    x.public();
+    y.public();
 }
