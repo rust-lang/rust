20f22cc6525cffa55466da6abe727234163f1fa0
Esteban Küber <esteban@kuber.com.ar>
2026-09-26 00:57:01 +0000

    Add more test cases of type not found caused by derive macro

--- tests/ui/derives/auxiliary/derive-no-generics.rs
+++ tests/ui/derives/auxiliary/derive-no-generics.rs
@@ -32,3 +32,53 @@ pub fn derive(item: TokenStream) -> TokenStream {
     };
     tokens
 }
+
+#[proc_macro_derive(B)]
+pub fn derive_2(item: TokenStream) -> TokenStream {
+    let mut tokens = item.into_iter();
+    let _enum = tokens.next();
+    let name = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let TokenTree::Group(group) = tokens.next().unwrap() else { panic!() };
+    let mut group = group.stream().into_iter();
+    let variant = group.next().unwrap();
+    let TokenTree::Group(args) = group.next().unwrap() else { panic!() };
+    let arg = args.stream().into_iter().next().unwrap();
+    let tokens = quote! {
+        trait X {}
+        impl X for $name {}
+
+        impl $name {
+            fn foo(&self) {
+                if let Self :: $variant(val) = self {
+                    let _: T = val;
+                }
+            }
+        }
+
+    };
+    tokens
+}
+
+#[proc_macro_derive(C)]
+pub fn derive_3(item: TokenStream) -> TokenStream {
+    let mut tokens = item.into_iter();
+    let _enum = tokens.next();
+    let name = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let _ = tokens.next().unwrap();
+    let TokenTree::Group(group) = tokens.next().unwrap() else { panic!() };
+    let mut group = group.stream().into_iter();
+    let variant = group.next().unwrap();
+    let TokenTree::Group(args) = group.next().unwrap() else { panic!() };
+    let arg = args.stream().into_iter().next().unwrap();
+    let tokens = quote! {
+        fn foo(val: $name<$arg>) {
+            $name :: $variant($arg) = val;
+        }
+    };
+    tokens
+}
--- tests/ui/derives/derive-macro-unsupported-type-params.rs
+++ tests/ui/derives/derive-macro-unsupported-type-params.rs
@@ -11,3 +11,18 @@ enum A<T> {
     Variant(T),
     //~^ ERROR: cannot find type `T` in this scope
 }
+
+// Resolution error comes entirely from within the derive macro:
+#[derive(B)] //~ ERROR: cannot find type `T` in this scope
+enum B<T> {
+    //~^ ERROR: missing generics for enum `B`
+    //~| ERROR: missing generics for enum `B`
+    Variant(T),
+}
+
+#[derive(C)]
+enum C<T> {
+    Variant(T),
+    //~^ ERROR: cannot find value `T` in this scope
+    //~| ERROR: cannot find type `T` in this scope
+}
